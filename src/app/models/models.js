18384e4b273e39f6a69f369d6ca0f8ab36cce92b
matehat <mathieu@damours.org>
2014-09-01 10:29:29 -0400

    Catch JSON decoding error

--- src/app/models/models.js
+++ src/app/models/models.js
@@ -1,6 +1,13 @@
 var getFromJSONResponse = function (name) {
   return function(data) {
-    var obj = JSON.parse(data);
+    var obj;
+    try {
+      obj = JSON.parse(data);
+    } catch(err) {
+      console.error("JSON decoding error:", err, data);
+      return {};
+    }
+
     if (!obj.error && name) {
       return obj[name];
     }
