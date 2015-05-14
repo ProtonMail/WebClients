429a5765030e443b26714f2041e504fffdcdd821
Richard <richard.tetaz@gmail.com>
2015-05-14 20:27:03 +0200

    fix Display label

--- src/app/controllers/sidebar.js
+++ src/app/controllers/sidebar.js
@@ -1,6 +1,6 @@
 angular.module("proton.controllers.Sidebar", [])
 
-.controller('SidebarController', function($scope, $rootScope, $state, $timeout, authentication) {
+.controller('SidebarController', function($scope, $rootScope, $state, authentication) {
     // Call event to open new composer
     $scope.compose = function() {
         $rootScope.$broadcast('newMessage');
