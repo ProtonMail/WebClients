7f85bcec321ec999c2d9b04e516c9003dd36c602
Jason Stockman <hello@jasonstockman.com>
2015-05-21 13:18:40 -0700

    template for UI

--- conf.build.js
+++ conf.build.js
@@ -34,6 +34,7 @@ module.exports = {
       "vendor/angular-file-upload/dist/angular-file-upload-shim.js",
       "vendor/angular-resource/angular-resource.js",
       "vendor/angular-bootstrap/ui-bootstrap.js",
+      "vendor/angular-bootstrap/ui-bootstrap-tpls.js",
       "vendor/angular-ui-router/release/angular-ui-router.js",
       "vendor/angular-animate/angular-animate.js",
       "vendor/angular-local-storage/dist/angular-local-storage.js",
