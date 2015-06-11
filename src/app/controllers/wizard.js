d69bc65769c8af9791c2e16e208c8dc800cf793f
Dayne Davis <daynedavis5@gmail.com>
2015-06-10 17:14:25 -0700

    added wizard versions

--- src/app/controllers/wizard.js
+++ src/app/controllers/wizard.js
@@ -9,10 +9,19 @@ angular.module("proton.controllers.Wizard", [])
     tools,
     networkActivityTracker
 ) {
-    $scope.pages = [1,2,3];
-    $scope.selection = 1;
+    $scope.pages = [0,1,2];
+    $scope.selection = 0;
+    $scope.versions = {
+        startWizard : [
+            '<ul><li>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li><li>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li><li>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li><li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li></ul>',
+            '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>',
+            '<ol><li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li><li>Aliquam tincidunt mauris eu risus.</li><li>Vestibulum auctor dapibus neque.</li></ol>'
+        ]
+    };
+    $scope.version = $scope.versions[$scope.ctrl.version];
+    console.log($scope.version);
+
     $scope.changePage = function(page) {
-        console.log(page);
         $scope.selection = page;
     };
 });
