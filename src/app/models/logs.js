ef14299124392e06126e63b854a0f498905c25cd
Jason Stockman <hello@jasonstockman.com>
2015-06-18 16:57:38 -0700

    robot and toolbar style

---
+++ src/app/models/logs.js
@@ -0,0 +1,14 @@
+angular.module("proton.models.logs", [])
+
+.factory("Logs", function($http, authentication) {
+    return {
+        // GET
+        getLogs: function() {
+            return $http.get(authentication.baseURL + '/logs/auth');
+        },
+        // DELETE
+        deleteLogs: function(Obj) {
+            return $http.delete(authentication.baseURL + '/logs/auth');
+        } 
+    };
+}); 
