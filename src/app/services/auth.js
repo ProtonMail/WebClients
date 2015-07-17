7bd1f80f779607e1fd23a0bfd4d89b6c6c4fc747
Richard <richard.tetaz@gmail.com>
2015-07-17 17:14:44 +0200

    Oups

--- src/app/services/auth.js
+++ src/app/services/auth.js
@@ -41,11 +41,11 @@ angular.module("proton.authentication", [
         };
 
         auth.savePassword = function(pwd) {
-            window.sessionStorage[MAILBOX_PASSWORD_KEY] = pmcwProvider.encode_utf8_base64(pwd);
+            window.sessionStorage[MAILBOX_PASSWORD_KEY] = pwd;
         };
 
         auth.getPassword = function() {
-            return pmcwProvider.decode_utf8_base64(window.sessionStorage[MAILBOX_PASSWORD_KEY]);
+            return window.sessionStorage[MAILBOX_PASSWORD_KEY];
         };
 
         // CONFIG-TIME API FUNCTIONS
