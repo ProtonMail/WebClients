b0bb50833ae63c429f030115f720e29281811db0
Jason Stockman <hello@jasonstockman.com>
2015-07-23 14:30:02 -0700

    hide login button and add client id and client secret to app

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,7 +102,9 @@ module.exports = function(grunt) {
                         debug: true,
                         apiUrl: apiUrl(),
                         app_version: '2.0.0',
-                        api_version: '1'
+                        api_version: '1',
+                        clientID: 'Angular',
+                        clientSecret: '00a11965ac0b47782ec7359c5af4dd79'
                     }
                 }
             },
@@ -112,7 +114,9 @@ module.exports = function(grunt) {
                         debug: false,
                         apiUrl: apiUrl(),
                         app_version: '2.0.0',
-                        api_version: '1'
+                        api_version: '1',
+                        clientID: 'Angular',
+                        clientSecret: '00a11965ac0b47782ec7359c5af4dd79'
                     }
                 }
             }
