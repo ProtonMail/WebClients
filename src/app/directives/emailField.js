6471e52384c33614ff16f509c029ceb13db77435
Dayne Davis <daynedavis5@gmail.com>
2015-07-27 13:44:57 -0700

    remove tag drag and drop and allo selection of emails in tags. Fixes #732

--- src/app/directives/emailField.js
+++ src/app/directives/emailField.js
@@ -2,179 +2,192 @@
 angular.module("proton.emailField", [])
 
 .constant("EMAIL_REGEXP",
-    /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i
-    )
+  /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i
+)
 
 .directive('emailField', function ($timeout, Contact, EMAIL_REGEXP, $sanitize) {
-    var self = this;
-    var directive = {
-        restrict: "A",
-        require: 'ngModel',
-        link: function ( $scope, $element, $attrs, $ctrl ) {
-            var $$element = $($element[0]);
-            var parent = $$element.parent();
-            var container = $(parent).closest('.input-container');
+  var self = this;
+  var directive = {
+    restrict: "A",
+    require: 'ngModel',
+    link: function ( $scope, $element, $attrs, $ctrl ) {
+      var $$element = $($element[0]);
+      var parent = $$element.parent();
+      var container = $(parent).closest('.input-container');
 
-            $(container).on('click', function() {
-                $$element.focus();
-            });
+      $(container).on('click', function() {
+          var selection = getSelection().toString();
+          if (!selection) {
+              $$element.focus();
+          }
+      });
 
-            var htmlEscape = function(str) {
-                var entityMap = {
-                    "&": "&amp;",
-                    "<": "&lt;",
-                    ">": "&gt;",
-                    '"': '&quot;',
-                    "'": '&#39;',
-                    "/": '&#x2F;'
-                };
+      var htmlEscape = function(str) {
+          var entityMap = {
+              "&": "&amp;",
+              "<": "&lt;",
+              ">": "&gt;",
+              '"': '&quot;',
+              "'": '&#39;',
+              "/": '&#x2F;'
+          };
 
-                return String(str).replace(/[&<>"'\/]/g, function (s) {
-                    return entityMap[s];
-                });
+          return String(str).replace(/[&<>"'\/]/g, function (s) {
+              return entityMap[s];
+          });
+      };
+      
+      $ctrl.$render = function () {
+        _(($ctrl.$viewValue || "").split(","))
+          .map(function (str) { return str.trim(); })
+          .each(function (email) {
+            manager.tagsManager('pushTag', email);
+          });
+      };
+
+      var positionInput = function (argument) {
+        var tt = $$element.closest(".twitter-typeahead");
+        tt.appendTo(tt.parent());
+      };
+
+      var setValue = function () {
+        $ctrl.$setViewValue(_(manager.tagsManager('tags').concat([$$element.val()]))
+          .filter(function (data) {
+            return data && EMAIL_REGEXP.test(data.Email);
+          })
+          .unique()
+          .map(function (element) {
+            return {
+              Name: element.Name.trim(),
+              Address: element.Email.trim()
             };
+          })
+          .value()
+        );
+        $scope.$apply();
+      };
 
-            $ctrl.$render = function () {
-                _(($ctrl.$viewValue || "").split(","))
-                .map(function (str) { return str.trim(); })
-                .each(function (email) {
-                    manager.tagsManager('pushTag', email);
-                });
-            };
+      $timeout(positionInput, 0);
 
-            var positionInput = function (argument) {
-                var tt = $$element.closest(".twitter-typeahead");
-                tt.appendTo(tt.parent());
-            };
+      var emails = [];
+      var tabbing = false;
 
-            var setValue = function () {
-                $ctrl.$setViewValue(_(manager.tagsManager('tags').concat([$$element.val()]))
-                    .filter(function (data) {
-                        return data && EMAIL_REGEXP.test(data.Email);
-                    })
-                    .unique()
-                    .map(function (element) {
-                        return {
-                            Name: element.Name.trim(),
-                            Address: element.Email.trim()
-                        };
-                    })
-                    .value()
-                    );
-                $scope.$apply();
-            };
-
-            $timeout(positionInput, 0);
-
-            var emails = [];
-            var tabbing = false;
-
-            var manager = $$element.tagsManager({
-                tagsContainer: parent[0],
-                tagCloseIcon: "<i class=\"fa fa-times\">",
-                delimiters: [32, 44, 9, 13],
-                validator: function (input) {
-                    return EMAIL_REGEXP.test(input);
-                }
-            });
-
-            receivedTag = function (event, ui) {
-                var currentTags = manager.tagsManager('tags');
-                var item = ui.item[0];
-                var name = item.innerText.trim();
-                var email = $(item).attr('value');
-                if (currentTags.indexOf(email) > -1) {
-                    ui.sender.sortable('cancel');
-                }
-                else {
-                    $(item).find('i').trigger('click');
-                    manager.tagsManager("pushTag", {
-                        Name: name,
-                        Email: email
-                    });
-                }
-            };
-
-            $(parent).closest('.input-container').sortable({
-                items: '.tm-tag',
-                connectWith: '.input-container',
-                receive: receivedTag,
-                containment: $(parent).closest('.composer')
-            });
-
-            manager.on("tm:pushed", function (ev, tag, tagId, $el) {
-
-                positionInput();
-                if (!tabbing) {
-                    $$element.focus();
-                }
-                tabbing = false;
-
-                $($el).on('mouseover', function() {
-                    $(this).css('cursor', 'move');
-                });
-
-                $($el).dblclick(function( ) {
-                    var input = $(parent).find('.tt-input');
-                    $(this).find('i').trigger('click');
-                    $(input).val(tag.Email);
-                    $$element.focus();
-                    $(input).trigger('keydown');
-                });
-
-                setValue();
-            });
-
-            manager.on("tm:popped tm:spliced", setValue);
-
-            $$element
-            .on("keydown", function (e) {
-                if (e.which === 9) {
-                    tabbing = true;
-                }
-            })
-            .on("blur", function () {
-                var val = $$element.val();
-                response = manager.tagsManager("pushTag",{
-                    Name: val,
-                    Email: val
-                });
-
-                if (response === undefined) {
-                    $timeout(function () { $$element.val(""); }, 0);
-                }
-                setValue();
-            })
-            .on("change", setValue)
-            .typeahead(null, {
-                source: Contact.index.ttAdapter(),
-                templates: {
-                    suggestion: function(Contact) {
-                        return "<b>" + $sanitize(htmlEscape(Contact.Name)) + "</b><br>" + $sanitize(htmlEscape(Contact.Email));
-                    }
-                }
-            }).on("typeahead:selected", function (e, d) {
-                if (typeof d.Name === 'undefined' || d.Name === '') {
-                    d.Name = d.Email;
-                }
-                manager.tagsManager("pushTag", d);
-            });
-
-            $$element.autosizeInput();
-
-            var list = ($(parent).hasClass('to-container')) ? "ToList" : ($(parent).hasClass('bcc-container')) ? "BCCList" : "CCList";
-
-            _.forEach($scope.message[list], function(d) {
-                if (typeof d.Name === 'undefined' || d.Name === '') {
-                    d.Name = d.Address;
-                }
-                manager.tagsManager("pushTag", {
-                    Name: d.Name,
-                    Email: d.Address
-                });
-            });
+      var manager = $$element.tagsManager({
+        tagsContainer: parent[0],
+        tagCloseIcon: "<i class=\"fa fa-times\">",
+        delimiters: [32, 44, 9, 13],
+        validator: function (input) {
+          return EMAIL_REGEXP.test(input);
         }
-    };
+      });
 
-    return directive;
+    //   Disable drag and drop of tags
+    //
+    //   receivedTag = function (event, ui) {
+    //     var currentTags = manager.tagsManager('tags');
+    //     var item = ui.item[0];
+    //     var name = item.innerText.trim();
+    //     var email = $(item).attr('value');
+    //     if (currentTags.indexOf(email) > -1) {
+    //       ui.sender.sortable('cancel');
+    //     }
+    //     else {
+    //       $(item).find('i').trigger('click');
+    //       manager.tagsManager("pushTag", {
+    //         Name: name,
+    //         Email: email
+    //       });
+    //     }
+    //   };
+
+    //   $(parent).closest('.input-container').sortable({
+    //     items: '.tm-tag',
+    //     connectWith: '.input-container',
+    //     receive: receivedTag,
+    //     containment: $(parent).closest('.composer')
+    //   });
+
+      manager.on("tm:pushed", function (ev, tag, tagId, $el) {
+
+        positionInput();
+        if (!tabbing) {
+          $$element.focus();
+        }
+        tabbing = false;
+
+        // $($el).on('mouseover', function() {
+        //   $(this).css('cursor', 'move');
+        // });
+
+        $($el).dblclick(function( ) {
+          var input = $(parent).find('.tt-input');
+          $(this).find('i').trigger('click');
+          $(input).val(tag.Email);
+          $$element.focus();
+          $(input).trigger('keydown');
+        });
+
+        setValue();
+      });
+
+      manager.on("tm:popped tm:spliced", setValue);
+      $$element
+        .on("keydown", function (e) {
+          if (e.which === 9) {
+            tabbing = true;
+          }
+        })
+        .on("blur", function () {
+          var val = $$element.val();
+          response = manager.tagsManager("pushTag",{
+            Name: val,
+            Email: val
+          });
+          if (response === undefined) {
+              var input = $(parent).find('.tt-input');
+              $timeout(function () {
+                  $$element.val("");
+                  $(input).val('');
+                  $(input).trigger('keydown');
+              }, 0);
+          }
+        setValue();
+        })
+        .on("focus", function() {
+            $timeout(function() {
+                $('.typeahead-container').scrollTop(0);
+            });
+        })
+        .on("change", setValue)
+        .typeahead(null, {
+          source: Contact.index.ttAdapter(),
+          templates: {
+              suggestion: function(Contact) {
+                  return "<b>" +$sanitize(Contact.Name) + "</b><br>" + $sanitize(Contact.Email);
+                }
+            }
+        }).on("typeahead:selected", function (e, d) {
+            if (typeof d.Name === 'undefined' || d.Name === '') {
+                d.Name = d.Email;
+            }
+            manager.tagsManager("pushTag", d);
+        });
+
+      $$element.autosizeInput();
+
+      var list = ($(parent).hasClass('to-container')) ? "ToList" : ($(parent).hasClass('bcc-container')) ? "BCCList" : "CCList";
+
+      _.forEach($scope.message[list], function(d) {
+          if (typeof d.Name === 'undefined' || d.Name === '') {
+              d.Name = d.Address;
+          }
+          manager.tagsManager("pushTag", {
+            Name: d.Name,
+            Email: d.Address
+          });
+      });
+    }
+  };
+
+  return directive;
 });
