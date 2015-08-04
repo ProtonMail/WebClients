58504c3610918cde948437841adba2385948535f
EpokK <richard.tetaz@gmail.com>
2015-08-04 10:45:08 +0200

    Fix grunt problem

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,16 @@
 
 var _ = require("lodash"),
 util = require("util");
-
+var appVersion = '2.0.4';
+var apiVersion = '1';
+var clientID = 'Angular';
+var clientSecret = '00a11965ac0b47782ec7359c5af4dd79';
+var BROWSERS = ["PhantomJS", "Chrome", "Firefox", "Safari"];
 var API_TARGETS = {
     prod: "https://dev.protonmail.ch/api",
     dev: "https://test-api.protonmail.ch",
     build: "/api"
 };
-
-var appVersion = '2.0.4';
-var apiVersion = '1';
-var clientID = 'Angular';
-var clientSecret = '00a11965ac0b47782ec7359c5af4dd79';
-
-var BROWSERS = ["PhantomJS", "Chrome", "Firefox", "Safari"];
 
 module.exports = function(grunt) {
     grunt.loadTasks("tasks");
@@ -142,7 +139,8 @@ module.exports = function(grunt) {
                 files: [
                     {
                         expand: true,
-                        src: ["<%= build_dir %>/**"],
+                        flatten: true,
+                        src: ["<%= build_dir %>/**/*.html"],
                         dest: "<%= build_dir %>/"
                     }
                 ]
