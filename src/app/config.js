66af10ee35b3ce402fda7e975cbe461ae9cc10ba
Jason Stockman <hello@jasonstockman.com>
2015-08-20 10:49:36 -0700

    Revert "mobile beta update"
    This reverts commit 5abf3953b6d4b2ea64db014cb49cb92d329defdc.

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,5 @@
 angular.module('proton.config', [])
 
-.constant('CONFIG', {debug:true,apiUrl:'https://protonmail.ch/api',app_version:'2.0.9',api_version:'1',date_version:'19 August 2015',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79'})
+.constant('CONFIG', {debug:true,apiUrl:'https://protonmail.ch/api',app_version:'2.0.8',api_version:'1',date_version:'14 August 2015',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79'})
 
 ;
