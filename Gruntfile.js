7ba902f884651aab2df6da4be7cbc69ac541684a
Bart Butler <bart.c.butler@gmail.com>
2015-08-24 10:41:37 -0700

    version changed to 2.0.12

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,9 @@
 
 var _ = require("lodash"),
 util = require("util");
-var appVersion = '2.0.11';
+var appVersion = '2.0.12';
 var apiVersion = '1';
-var dateVersion = '21 August 2015';
+var dateVersion = '24 August 2015';
 var clientID = 'Angular';
 var clientSecret = '00a11965ac0b47782ec7359c5af4dd79';
 var BROWSERS = ["PhantomJS", "Chrome", "Firefox", "Safari"];
