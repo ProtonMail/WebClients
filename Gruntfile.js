e846677ad171ad54ffa7689b1893cd7446af517f
Bart Butler <bart.c.butler@gmail.com>
2015-08-31 16:12:38 -0700

    Version to 2.0.21

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,9 @@
 
 var _ = require("lodash"),
 util = require("util");
-var appVersion = '2.0.20';
+var appVersion = '2.0.21';
 var apiVersion = '1';
-var dateVersion = '30 August 2015';
+var dateVersion = '31 August 2015';
 var clientID = 'Angular';
 var clientSecret = '00a11965ac0b47782ec7359c5af4dd79';
 var BROWSERS = ["PhantomJS", "Chrome", "Firefox", "Safari"];
