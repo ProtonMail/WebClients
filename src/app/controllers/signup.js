dbdce4f352bca8bda63fb4e0e5ce1be17b1b2d93
Jason Stockman <hello@jasonstockman.com>
2015-09-03 19:44:38 -0700

    removed more password melt stuff

--- src/app/controllers/signup.js
+++ src/app/controllers/signup.js
@@ -103,30 +103,6 @@ angular.module("proton.controllers.Signup", ["proton.tools"])
         $log.debug('finishLoginReset');
     };
 
-    $scope.strength = function(password) {
-        var daysToCrack = mellt.CheckPassword(password);
-        var word;
-
-        if (daysToCrack < 100) {
-            word = 'Very Weak';
-        } else if (daysToCrack < 1000) {
-            word = 'Weak';
-        } else if (daysToCrack < 10000) {
-            word = 'Okay';
-        } else if (daysToCrack < 100000) {
-            word = 'Good';
-        } else if (daysToCrack < 1000000) {
-            word = 'Strong';
-        } else {
-            word = 'Very Strong';
-        }
-
-        return {
-            number: daysToCrack,
-            word: word
-        };
-    };
-
     $scope.generateKeys = function(userID, pass) {
         var deferred = $q.defer();
 
