7db34fccc7c5a04b6f2e5840d563de8bfc29bb7e
EpokK <richard.tetaz@gmail.com>
2015-09-07 11:29:07 +0200

    Fix #1229

--- src/app/directives/attachment.js
+++ src/app/directives/attachment.js
@@ -1,29 +1,19 @@
 angular.module("proton.attachmentHeight", [])
-.directive("attachmentHeight", ['$timeout', function($timeout) {
-
+.directive("attachmentHeight", function() {
 
 	function link(scope, element, attrs) {
-
-        var count = parseInt(attrs.attachmentcount)+1;
+        var count = parseInt(attrs.attachmentcount);
         var buttonHeight = 32;
-        var maxHeight = (32*4);
+        var maxHeight = (buttonHeight * 4);
 
-        if (count>6) {
-        	element.css('minHeight', maxHeight);
-        }
-        else {
-        	if (count%2) {
-        		count -= 1;
-        		if (count<2) {
-        			count = 2;
-        		}
-        	}
-        	element.css('minHeight', (count*buttonHeight)+1 );
+        if (count > 6) {
+        	element.css('maxHeight', maxHeight);
+        } else {
+			element.css('minHeight', ((parseInt(count / 2) + count % 2) * buttonHeight) + 1);
         }
 	}
 
 	return {
 		link: link
 	};
-
-}]);
+});
