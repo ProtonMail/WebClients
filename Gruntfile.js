b3fd8699dff572866d98bae4bb75982fb460e8f6
Jason Stockman <hello@jasonstockman.com>
2015-09-24 10:47:46 -0700

    updated versioning

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,9 @@
 
 var _ = require("lodash"),
 util = require("util");
-var appVersion = '2.1.2';
+var appVersion = '2.1.3';
 var apiVersion = '1';
-var dateVersion = '23 September 2015';
+var dateVersion = '24 September 2015';
 var clientID = 'Angular';
 var clientSecret = '00a11965ac0b47782ec7359c5af4dd79';
 var BROWSERS = ["PhantomJS", "Chrome", "Firefox", "Safari"];
