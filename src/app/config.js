a30b5a1c0fceb55ef5d7ab2434a807f01fbf14c9
Bart Butler <bart.c.butler@gmail.com>
2015-09-25 22:22:59 -0700

    Version 2.1.3

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,5 @@
 angular.module('proton.config', [])
 
-.constant('CONFIG', {debug:true,apiUrl:'https://protonmail.ch/api',app_version:'2.1.2',api_version:'1',date_version:'24 September 2015',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79'})
+.constant('CONFIG', {debug:true,apiUrl:'https://protonmail.ch/api',app_version:'2.1.3',api_version:'1',date_version:'25 September 2015',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79'})
 
 ;
