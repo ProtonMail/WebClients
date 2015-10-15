c06f543a03044d616e2389345a6f95db078883c7
EpokK <richard.tetaz@gmail.com>
2015-10-15 15:56:16 +0200

    try to use ng-messages but it seems not worked correctly

--- src/app/controllers/settings/users.js
+++ src/app/controllers/settings/users.js
@@ -1,6 +1,15 @@
 angular.module("proton.controllers.Settings")
 
 .controller('UsersController', function($rootScope, $scope, userModal) {
+    $scope.organization = {
+        name: ''
+    };
+    $scope.users = {};
+
+    $scope.saveOrganizationName = function() {
+        // $scope.organization.name;
+    };
+
     $scope.openUserModal = function() {
         userModal.activate({
             params: {
