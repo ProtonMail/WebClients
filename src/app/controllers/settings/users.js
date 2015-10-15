657f0eeeae8f88cc7e0b9ddb9c6605204960b8b2
EpokK <richard.tetaz@gmail.com>
2015-10-15 18:22:06 +0200

    Establish users logic

--- src/app/controllers/settings/users.js
+++ src/app/controllers/settings/users.js
@@ -1,13 +1,50 @@
 angular.module("proton.controllers.Settings")
 
-.controller('UsersController', function($rootScope, $scope, userModal) {
+.controller('UsersController', function($rootScope, $scope, $translate, userModal) {
+    var MASTER = 0;
+    var SUB = 1;
+
     $scope.organization = {
         name: ''
     };
-    $scope.users = {};
+
+    $scope.roles = [MASTER, SUB];
+
+    // Example
+    $scope.users = [
+        {
+            id: 1,
+            username: 'default@emailaddresses.com',
+            role: MASTER,
+            storage: 123,
+            addresses: [
+                {
+                    id: 1,
+                    address: 'qweqweqwe@qweqwe.fr',
+                    default: true
+                }
+            ]
+        }
+    ];
 
     $scope.saveOrganizationName = function() {
         // $scope.organization.name;
+    };
+
+    $scope.unlinkAddress = function(user, address) {
+
+    };
+
+    $scope.managePasswords = function(user) {
+
+    };
+
+    $scope.generateKeys = function(user) {
+
+    };
+
+    $scope.enterMailbox = function(user) {
+
     };
 
     $scope.openUserModal = function() {
