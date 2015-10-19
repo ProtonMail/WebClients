7423d9b5d07cbe5df67d97537488244ccbbf25bc
EpokK <richard.tetaz@gmail.com>
2015-10-19 13:03:39 +0200

    Add custom domain page

--- src/app/controllers/settings/domains.js
+++ src/app/controllers/settings/domains.js
@@ -1,7 +1,14 @@
 angular.module("proton.controllers.Settings")
 
 .controller('DomainsController', function($rootScope, $scope, domainModal) {
-    $scope.openDomainModal = function() {
+    $scope.domains = [
+        {id: 1, domain: 'example1.com', status: true, verification: true, spf: true, dkim: true, dmarc: true},
+        {id: 2, domain: 'example2.com', status: false, verification: false, spf: false, dkim: false, dmarc: false}
+    ];
+    /**
+     * Open modal process to add a domain
+     */
+    $scope.addDomain = function() {
         domainModal.activate({
             params: {
                 submit: function(datas) {
@@ -13,5 +20,39 @@ angular.module("proton.controllers.Settings")
                 }
             }
         });
+    };
+
+    /**
+     * Open modal process to buy a new domain
+     */
+    $scope.buyDomain = function() {
+
+    };
+
+    $scope.deleteDomain = function(domain) {
+
+    };
+
+    /**
+     * Delete address
+     */
+    $scope.deleteAddress = function(address) {
+
+    };
+
+    $scope.verification = function(domain) {
+
+    };
+
+    $scope.spf = function(domain) {
+
+    };
+
+    $scope.dkim = function(domain) {
+
+    };
+
+    $scope.dmarc = function(domain) {
+
     };
 });
