99d8808be17e21ff5509e2710900d6b4acf41e7d
EpokK <richard.tetaz@gmail.com>
2015-10-22 10:04:38 +0200

    override basic number filter

--- src/app/filters/filters.js
+++ src/app/filters/filters.js
@@ -9,6 +9,20 @@ angular.module("proton.filters",[])
     };
 })
 
+.filter('number', function ($filter) {
+    return function (input, places) {
+        if (isNaN(input)) {
+            return input;
+        }
+        // If we want 1 decimal place, we want to mult/div by 10
+        // If we want 2 decimal places, we want to mult/div by 100, etc
+        // So use the following to create that factor
+        var factor = "1" + Array(+(places > 0 && places + 1)).join("0");
+
+        return Math.round(input * factor) / factor;
+    };
+})
+
 .filter('currency', function() {
     return function(amount, currency) {
         var result;
