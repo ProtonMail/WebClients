ab4e856dbb0a467834666cd8a68508135e6eee19
EpokK <richard.tetaz@gmail.com>
2015-11-02 10:17:27 +0100

    manage stopObserving

--- src/app/directives/transformation.js
+++ src/app/directives/transformation.js
@@ -17,30 +17,26 @@ angular.module("proton.transformation", [])
     return {
         restrict: 'A',
         link: function(scope, element, attributes) {
-            var done = false;
+            var stopObserving = attributes.$observe('hideFirstBlockquote', function(interpolatedValue) {
+                $timeout(function() {
+                    var blockquote = angular.element(element).find('blockquote:first');
+                    var style = 'background-color: #f1f1f1;border: 1px solid #ddd;color: #666;clear: both;line-height: 6px;outline: none;'; // TODO add style (for Jason)
 
-            attributes.$observe('hideFirstBlockquote', function(interpolatedValue) {
-                if(done === false) {
-                    $timeout(function() {
-                        var blockquote = angular.element(element).find('blockquote:first');
-                        var style = 'background-color: #f1f1f1;border: 1px solid #ddd;color: #666;clear: both;line-height: 6px;outline: none;'; // TODO add style (for Jason)
+                    if(blockquote.length > 0) {
+                        var button = angular.element('<button/>', {
+                            class: 'fa fa-ellipsis-h',
+                            style: style,
+                            click: function () {
+                                blockquote.show();
+                                button.remove();
+                            }
+                        });
 
-                        if(blockquote.length > 0) {
-                            var button = angular.element('<button/>', {
-                                class: 'fa fa-ellipsis-h',
-                                style: style,
-                                click: function () {
-                                    blockquote.show();
-                                    button.remove();
-                                }
-                            });
-
-                            blockquote.before(button);
-                            blockquote.hide();
-                            done = true;
-                        }
-                    }, 0, false);
-                }
+                        blockquote.before(button);
+                        blockquote.hide();
+                        stopObserving();
+                    }
+                }, 0, false);
             });
         }
     };
