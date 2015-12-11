6f9e8ba9c5eaa74186def52b7b64887532eea9cc
Jason Stockman <hello@jasonstockman.com>
2015-12-11 13:58:08 -0800

    added temporary function to toggle modes by clicking on the logo

--- src/app/controllers/header.js
+++ src/app/controllers/header.js
@@ -34,6 +34,17 @@ angular.module("proton.controllers.Header", [])
         });
     }
 
+    /* TEMPORARY */
+    $scope.testToggleLayout = function() {
+        if ($rootScope.layoutMode === 'rows') {
+            $rootScope.layoutMode = 'columns';
+        }
+        else {
+            $rootScope.layoutMode = 'rows';
+        }
+    };
+    /* END TEMPORARY */
+
     /**
      * Call event to open new composer
      */
