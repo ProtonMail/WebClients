6ae182fafda2b9aa78fb35d519e696693e761810
Jason Stockman <hello@jasonstockman.com>
2015-12-22 23:15:17 -0800

    merged

--- src/app/config.js
+++ src/app/config.js
@@ -1,9 +1,4 @@
 angular.module('proton.config', [])
 
-<<<<<<< HEAD
 .constant('CONFIG', {debug:true,apiUrl:'https://v2.protonmail.com/api',app_version:'2.1.8',api_version:'1',date_version:'Tue Dec 22 2015',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79'})
-=======
-.constant('CONFIG', {debug:true,apiUrl:'https://52.32.164.147/api',app_version:'3.0.0',api_version:'1',date_version:'Tue Dec 22 2015',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79'})
->>>>>>> b10b41cd01066df0b8e350866dbc863c237d250a
-
 ;
