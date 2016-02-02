fdaaf5747178cb20f3ed7e6eec480d83b3d2a499
Jason Stockman <hello@jasonstockman.com>
2016-02-01 18:03:41 -0800

    more tour stuff

--- src/app/app.js
+++ src/app/app.js
@@ -234,11 +234,25 @@ angular.module('proton', [
 
     // Intro / Tour
     $rootScope.tourStart = function() {
-        $('*[data-tour]').tooltip('show');
+        
+        $rootScope.tourActive = true; // used for body class and CSS.
+
+        $('#tour-jason').tooltip({
+            title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            placement: "right"
+        });
+
+        $('#tour-fromage').tooltip({
+            title: "Lorem ipsum dolor sit amet",
+            placement: "top"
+        });
+
+        $('#tour-jason, #tour-fromage').tooltip('show');
         $('.tooltip:visible').addClass('tour animated rubberBand');
     };
     $rootScope.tourEnd = function() {
-        $('*[data-tour]').tooltip('hide');
+        $rootScope.tourActive = false;
+        $('#tour-jason, #tour-fromage').tooltip('hide');
     };
 
     // Manage page title
