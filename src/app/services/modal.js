c46fdb74f6ffd9b70f145b94b716e11d82cbb7f4
EpokK <richard.tetaz@gmail.com>
2016-02-05 10:28:25 -0800

    Fix dashboard with Sources because it's an array

--- src/app/services/modal.js
+++ src/app/services/modal.js
@@ -509,7 +509,7 @@ angular.module("proton.modals", [])
             this.base = CONSTANTS.BASE_SIZE;
             this.coupon = '';
 
-            if(params.card.data.Code === 1000 && params.card.data.Sources) {
+            if(params.card.data.Code === 1000 && params.card.data.Sources.length > 0) {
                 var card = _.first(params.card.data.Sources);
 
                 this.source = card.ExternalSourceID;
