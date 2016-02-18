e9586886edd1dd40f9f31dcc28a3bfed3b96abc2
EpokK <richard.tetaz@gmail.com>
2016-02-18 18:31:45 +0100

    Needs to be displayed even for free accounts because .ch/com people need it, and even if they only have .com, its an additional chance to upsell

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,5 @@
 angular.module('proton.config', [])
 
-.constant('CONFIG', {debug:true,apiUrl:'https://dev.protonmail.com/api',app_version:'3.0.11',api_version:'1',date_version:'Thu Feb 18 2016',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79'})
+.constant('CONFIG', {debug:false,apiUrl:'/api',app_version:'3.0.11',api_version:'1',date_version:'Thu Feb 18 2016',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79'})
 
 ;
