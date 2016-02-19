67d3b748e4759fb34eb3168f5d9bc8793edf80a0
EpokK <richard.tetaz@gmail.com>
2016-02-19 12:11:39 +0100

    review

--- src/app/controllers/compose.js
+++ src/app/controllers/compose.js
@@ -130,7 +130,7 @@ angular.module("proton.controllers.Compose", ["proton.constants"])
     $scope.$on('newMessage', function() {
         if(
             ($scope.messages.length >= CONSTANTS.MAX_NUMBER_COMPOSER) ||
-            ($scope.messages.length > 0 && $rootScope.mobileMode)
+            ($scope.messages.length === 1 && $rootScope.mobileMode === true)
         ) {
             notify({message: $translate.instant('MAXIMUM_COMPOSER_REACHED'), classes: 'notification-danger'});
         } else {
