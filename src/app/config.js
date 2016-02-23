329fc22d9bb9febb3a4bbf8ea39feb0e1b8fc49a
EpokK <richard.tetaz@gmail.com>
2016-02-23 10:42:44 +0100

    confie

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,4 @@
 angular.module('proton.config', [])
 
 .constant('CONFIG', {debug:true,apiUrl:'https://52.32.164.147/api',app_version:'3.1.0',api_version:'1',date_version:'Tue Feb 23 2016',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79'})
-
 ;
