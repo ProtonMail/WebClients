4e15b7032beb8f4642fd8e1c95c3d0fcd6c339b0
EpokK <richard.tetaz@gmail.com>
2016-02-29 14:18:01 +0100

    prepare invoices table

--- src/app/controllers/header.js
+++ src/app/controllers/header.js
@@ -83,7 +83,6 @@ angular.module("proton.controllers.Header", [])
     };
 
     $scope.openSearchModal = function() {
-        console.log('search?', $scope.advancedSearch);
         $scope.labels = authentication.user.Labels;
         $scope.advancedSearch = !$scope.advancedSearch;
     };
