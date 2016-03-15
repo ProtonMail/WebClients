4bbec30506388ee3bec3a8bde50f22555f4ae967
EpokK <richard.tetaz@gmail.com>
2016-03-15 16:57:36 +0100

    Fix for @violetakochoska (initialize Signature and DisplayName where there are no value inside)

--- src/app/services/modal.js
+++ src/app/services/modal.js
@@ -1706,6 +1706,8 @@ angular.module("proton.modals", [])
             this.defaultDisplayName = authentication.user.DisplayName;
             this.defaultSignature = authentication.user.Signature;
             this.address = params.address;
+            this.address.DisplayName = this.address.DisplayName || authentication.user.DisplayName;
+            this.address.Signature = this.address.Signature || authentication.user.Signature;
             this.address.custom = true;
 
             this.confirm = function() {
