c60140ef94f924a37b6586d4b1d86a303589b469
Jason Stockman <hello@jasonstockman.com>
2016-04-29 18:35:52 -0700

    more controller functions and translations

--- src/app/controllers/settings/filters.js
+++ src/app/controllers/settings/filters.js
@@ -12,10 +12,47 @@ angular.module("proton.controllers.Settings")
     networkActivityTracker,
     Setting,
     incomingDefaults,
+    IncomingDefault,
     notify
 ) {
 
     // Variables
     $scope.IncomingDefaults = incomingDefaults.data.IncomingDefaults;
+
+    $scope.clearDefaults = function() {
+        networkActivityTracker.track(
+            IncomingDefault.clear()
+            .then(function(result) {
+                if (result.data && result.data.Code === 1000) {
+                    $scope.IncomingDefaults = [];
+                    notify({
+                        message: gettextCatalog.getString('Default incomming rules cleared', null), 
+                        classes: 'notification-success'
+                    });
+                }
+            })
+        );
+    };
+
+    $scope.refreshDefaults = function() {
+        networkActivityTracker.track(
+            IncomingDefault.get()
+            .then(function(result) {
+                if (result.data && result.data.Code === 1000) {
+                    $scope.IncomingDefaults = result.data.IncomingDefaults;
+                    notify({
+                        message: gettextCatalog.getString('Default incomming rules refreshed', null), 
+                        classes: 'notification-success'
+                    });
+                }
+            })
+        );
+
+    };
+
+    $scope.deleteSelectedDefaults = function() {
+
+    };
+
 
 });
