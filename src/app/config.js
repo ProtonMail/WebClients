fdb0037f1ba3c2685c29ff83765e82b5f45e3199
EpokK <richard.tetaz@gmail.com>
2016-05-09 19:09:27 +0200

    Fix https://protonmail.zendesk.com/agent/tickets/43649

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,5 @@
 angular.module('proton.config', [])
 
-.constant('CONFIG', {debug:true,apiUrl:'https://dev.protonmail.com/api',app_version:'3.1.7',api_version:'1',date_version:'Mon May 09 2016',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79'})
+.constant('CONFIG', {debug:false,apiUrl:'/api',app_version:'3.1.8',api_version:'1',date_version:'Mon May 09 2016',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79'})
 
 ;
