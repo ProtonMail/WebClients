54f974f9aa26f997ba9247236643a4a889f8e2b3
EpokK <richard.tetaz@gmail.com>
2016-06-03 17:59:12 +0200

    Add a polyfill function to support DOMParser

--- src/app/libraries/polyfill.js
+++ src/app/libraries/polyfill.js
@@ -105,3 +105,34 @@ if (!Array.prototype.some) {
     return false;
   };
 }
+
+(function(DOMParser) {
+	var DOMParser_proto = DOMParser.prototype;
+    var real_parseFromString = DOMParser_proto.parseFromString;
+
+	// Firefox/Opera/IE throw errors on unsupported types
+	try {
+		// WebKit returns null on unsupported types
+		if ((new DOMParser).parseFromString('', 'text/html')) {
+			// text/html parsing is natively supported
+			return;
+		}
+	} catch (ex) {}
+
+	DOMParser_proto.parseFromString = function(markup, type) {
+		if (/^\s*text\/html\s*(?:;|$)/i.test(type)) {
+			var
+			  doc = document.implementation.createHTMLDocument('')
+			;
+	      		if (markup.toLowerCase().indexOf('<!doctype') > -1) {
+        			doc.documentElement.innerHTML = markup;
+      			}
+      			else {
+        			doc.body.innerHTML = markup;
+      			}
+			return doc;
+		} else {
+			return real_parseFromString.apply(this, arguments);
+		}
+	};
+}(DOMParser));
