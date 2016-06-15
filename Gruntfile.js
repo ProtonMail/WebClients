05807acc8637f826411fdf050a6e3d3b9494d2e9
EpokK <richard.tetaz@gmail.com>
2016-06-15 11:26:55 +0200

    v3.3.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@
 
 var _ = require('lodash');
 var util = require('util');
-var appVersion = '3.3.2';
+var appVersion = '3.3.3';
 var apiVersion = '1';
 var dateVersion = new Date().toDateString();
 var clientID = 'Angular';
