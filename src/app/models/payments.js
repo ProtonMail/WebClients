e3434ca5817e214136684884da15f8603429cd59
Richard <EpokK@users.noreply.github.com>
2016-06-24 20:22:20 +0200

    Upsell (#3150)
    * Add a new process to pay during the signup
    
    * Better design for the payment panel
    
    * I missed to add the condition
    
    * Display invoice
    
    * align right icon
    
    * Fix problem reported by @violetakochoska
    
    * Disable the monetize modal
    
    * Apply change for Andy
    
    Now we display a message if the payment fails to allow the user to
    choose a free account.
    
    * Change sentence for @bartbutler
    
    * Rename action button
    
    * Fix #3146
    
    * First comment from Andy, applied
    
    * Apply changes from Keven and Andy proposal
    
    * Add condition
    
    * Little tweaks
    
    * Move information
    
    * Apply some changes requested by Andy
    
    * Fix CSS

--- src/app/models/payments.js
+++ src/app/models/payments.js
@@ -25,6 +25,14 @@ angular.module("proton.models.payments", [])
             return $http.post(url.get() + '/payments/paypal', params);
         },
         /**
+         * Send payment details to subscribe during the signup process
+         * @param {Object} params
+         * @param {Promise}
+         */
+        verify: function(params) {
+            return $http.post(url.get() + '/payments/verify', params);
+        },
+        /**
          * Get payment method status
          * @return {Promise}
          */
