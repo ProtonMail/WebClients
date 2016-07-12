20df8d4514320ffc2d49e8b5461aa0f64581548d
dhoko <dhoko@cyaneus.org>
2016-07-12 15:03:10 +0200

    Fix encoding error errorReporter

--- src/app/services/errorReporter.js
+++ src/app/services/errorReporter.js
@@ -1,30 +1,30 @@
 angular.module("proton.errorReporter", [])
-.factory("errorReporter", ($log, $rootScope, $q, notify) => {
-    ​
+  .factory("errorReporter", ($log, $rootScope, $q, notify) => {
+
     const newNotification = (msg, error) => msg && notify(msg);
     const clear = notify.closeAll;
-    ​
+
     const catcher = (msg, promise) => (error) => {
-        newNotification(msg, error);
-        promise && promise.reject();
+      newNotification(msg, error);
+      promise && promise.reject();
     };
-    ​
+
     const resolve = (msg, promise, defaultResult) => {
-        const q = $q.defer();
-        promise
+      const q = $q.defer();
+      promise
         .then(
-            q.resolve,
-            (error) => {
-                newNotification(msg, error);
-                q.resolve(defaultResult);
-            }
+          q.resolve,
+          (error) => {
+            newNotification(msg, error);
+            q.resolve(defaultResult);
+          }
         );
-        return q.promise;
+      return q.promise;
     };
-    ​
+
     return { clear, catcher, resolve, notify: newNotification };
-    ​
-})
+
+  })
 .run(function($rootScope) {
-    $rootScope.errorReporter = {};
+  $rootScope.errorReporter = {};
 });
