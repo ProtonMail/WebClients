cb86f0a4d30954a0bc6225151ad94a16cf76c44c
Richard Tetaz <richard@MacBook-Air-de-Richard.local>
2016-07-12 14:55:40 +0200

    patch error reporter

--- src/app/services/errorReporter.js
+++ src/app/services/errorReporter.js
@@ -1,44 +1,30 @@
 angular.module("proton.errorReporter", [])
-    .factory("errorReporter", function($log, $rootScope, $q, notify) {
-        var api = {
-            catcher: function(msg, promise) {
-                var self = this;
-
-                return function(error) {
-                    self.notify(msg, error);
-                    if (promise) {
-                        promise.reject();
-                    }
-                };
-            },
-            resolve: function(msg, promise, defaultResult) {
-                var q = $q.defer();
-                var self = this;
-
-                promise.then(
-                    function(result) {
-                        q.resolve(result);
-                    },
-                    function(error) {
-                        self.notify(msg, error);
-                        q.resolve(defaultResult);
-                    }
-                );
-
-                return q.promise;
-            },
-            notify(message, error) {
-                if (angular.isDefined(message)) {
-                    notify(message);
-                }
-            },
-            clear: function() {
-                notify.closeAll();
+.factory("errorReporter", ($log, $rootScope, $q, notify) => {
+    ​
+    const newNotification = (msg, error) => msg && notify(msg);
+    const clear = notify.closeAll;
+    ​
+    const catcher = (msg, promise) => () => {
+        newNotification(msg, error);
+        promise && promise.reject();
+    };
+    ​
+    const resolve = (msg, promise, defaultResult) => {
+        const q = $q.defer();
+        promise
+        .then(
+            q.resolve,
+            (error) => {
+                newNotification(msg, error);
+                q.resolve(defaultResult);
             }
-        };
-
-        return api;
-    })
+        );
+        return q.promise;
+    };
+    ​
+    return { clear, catcher, resolve, notify: newNotification };
+    ​
+})
 .run(function($rootScope) {
     $rootScope.errorReporter = {};
 });
