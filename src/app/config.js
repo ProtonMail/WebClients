9baeda2d729e4f2a57278cb7c8742e7a9cc319df
Richard <richard@protonmail.com>
2016-07-19 16:14:13 +0200

    Ignore src/app/config.js

--- src/app/config.js
+++
@@ -1,5 +0,0 @@
-angular.module('proton.config', [])
-
-.constant('CONFIG', {debug:true,apiUrl:'https://dev.protonmail.com/api',app_version:'3.3.8',api_version:'1',date_version:'Wed Jul 13 2016',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79'})
-
-;
