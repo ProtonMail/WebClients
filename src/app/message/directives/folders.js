3a786ad8db9619f8edbb771bcf52de0504566bdd
dhoko <dhoko@users.noreply.github.com>
2016-07-21 16:29:40 +0200

    Extract proton.message to its own directory ~ update build (#3323)
    * Extract proton.message to its own directory ~ update build
    
    * Grunt compile scripts should work on prod

--- src/app/message/directives/folders.js
+++ src/app/message/directives/folders.js
@@ -1,4 +1,4 @@
-angular.module('proton.message', [])
+angular.module('proton.message')
 .directive('foldersMessage', ($rootScope, gettextCatalog, $compile, mailboxIdentifersTemplate) => {
 
   const MAP_LABELS = {
