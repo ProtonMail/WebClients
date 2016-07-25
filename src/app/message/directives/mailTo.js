e2a7d767b0a20e6158c27e73f802565daabd7278
Richard <richard@protonmail.com>
2016-07-25 11:47:49 +0200

    organize directives and services

---
+++ src/app/message/directives/mailTo.js
@@ -0,0 +1,42 @@
+angular.module('proton.message')
+.directive('mailTo', ($rootScope, regexEmail, Message) => ({
+    restrict: 'A',
+    link(scope, element) {
+        var click = function(event) {
+            if (event.target.tagName === 'A') {
+                var href = event.target.getAttribute('href');
+
+                if (href) {
+                    event.preventDefault();
+
+                    var mailTo = href.indexOf('mailto') === 0;
+                    var emails = event.target.getAttribute('href').match(regexEmail);
+
+                    if (mailTo && emails) {
+                        var message = new Message();
+                        var ToList = [];
+
+                        ToList.push({
+                            Address: emails[0],
+                            Name: emails[0]
+                        });
+
+                        _.defaults(message, {
+                            ToList: ToList,
+                            CCList: [],
+                            BCCList: [],
+                            Subject: '',
+                            PasswordHint: '',
+                            Attachments: []
+                        });
+
+                        $rootScope.$broadcast('loadMessage', message);
+                    }
+                }
+            }
+        };
+
+        element.on('click', click);
+        scope.$on('$destroy', () => { element.off('click', click); });
+    }
+}));
--- src/app/message/directives/toggleMessage.js
+++ src/app/message/directives/toggleMessage.js
@@ -1,5 +1,6 @@
 angular.module('proton.message')
 .directive('toggleMessage', ($rootScope) => ({
+    restrict: 'A',
     link(scope, element) {
         function selection() {
             return window.getSelection().toString().length === 0;
