d2a6b214a1a92a1e3ac57c7032cd793a8981b075
Richard <richard@protonmail.com>
2016-07-25 12:58:59 +0200

    Add toggleBlockquote directive

--- src/app/message/directives/toggleBlockquote.js
+++ src/app/message/directives/toggleBlockquote.js
@@ -1,7 +1,24 @@
 angular.module('proton.message')
-.directive('toggleMessage', ($rootScope) => ({
+.directive('toggleBlockquote', ($rootScope) => ({
     restrict: 'A',
     link(scope, element) {
+        var click = function(event) {
+            var target = event.target;
 
+            event.preventDefault();
+
+            if (target.tagName === 'BUTTON' && target.classList.contains('more')) {
+                var blockquote = target.nextElementSibling;
+
+                if (blockquote.style.display === 'none') {
+                    blockquote.style.display = 'block';
+                } else {
+                    blockquote.style.display = 'none';
+                }
+            }
+        };
+
+        element.on('click', click);
+        scope.$on('$destroy', () => { element.off('click', click); });
     }
 }));
