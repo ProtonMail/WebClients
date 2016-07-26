fc280767b8b22fec46cd9f9daf97b2971a4e9e30
Richard <richard@protonmail.com>
2016-07-26 14:22:52 +0200

    Fix #3361

--- src/app/message/services/transformBlockquotes.js
+++ src/app/message/services/transformBlockquotes.js
@@ -20,18 +20,13 @@ angular.module('proton.message')
         var blockquote = blockquotes[0];
 
         if (blockquote) {
-            var parent = blockquote.parentElement;
-            var textSplitted = parent.textContent.replace(/\s+/g, '').split(blockquote.textContent.replace(/\s+/g, ''));
+            var button = document.createElement('button');
+            var title = gettextCatalog.getString('Show previous message', null, 'Title');
 
-            if (angular.isArray(textSplitted) && textSplitted.length > 0 && textSplitted[0].length > 0) {
-                var button = document.createElement('button');
-                var title = gettextCatalog.getString('Show previous message', null, 'Title');
-
-                button.className = 'fa fa-ellipsis-h pm_button more';
-                button.setAttribute('title', title);
-                blockquote.parentNode.insertBefore(button, blockquote);
-                blockquote.style.display = 'none';
-            }
+            button.className = 'fa fa-ellipsis-h pm_button more';
+            button.setAttribute('title', title);
+            blockquote.parentNode.insertBefore(button, blockquote);
+            blockquote.style.display = 'none';
         }
 
         return html;
