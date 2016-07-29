f8415bd1f910724d77627de1c6009e650760c7fc
dhoko <dhoko@users.noreply.github.com>
2016-07-29 10:30:53 +0200

    Fix #3418 ~ trim content before checking the length (#3421)

--- src/app/message/services/transformBlockquotes.js
+++ src/app/message/services/transformBlockquotes.js
@@ -1,26 +1,27 @@
 angular.module('proton.message')
 .factory('transformBlockquotes', function(gettextCatalog) {
+    const quotes = [
+        '.protonmail_quote',
+        '.gmail_quote',
+        '.yahoo_quoted',
+        '.WordSection1',
+        '#isForwardContent',
+        '#isReplyContent',
+        '#mailcontent',
+        '#origbody',
+        '#reply139content',
+        '#oriMsgHtmlSeperator',
+        '#OLK_SRC_BODY_SECTION',
+        'blockquote[type="cite"]'
+    ].join(',');
+
     return function(html, message) {
-        var quotes = [
-            '.protonmail_quote',
-            '.gmail_quote',
-            '.yahoo_quoted',
-            '.WordSection1',
-            '#isForwardContent',
-            '#isReplyContent',
-            '#mailcontent',
-            '#origbody',
-            '#reply139content',
-            '#oriMsgHtmlSeperator',
-            '#OLK_SRC_BODY_SECTION',
-            'blockquote[type="cite"]'
-        ].join(',');
 
         var blockquotes = html.querySelectorAll(quotes);
         var blockquote = blockquotes[0];
 
         if (blockquote) {
-            if (blockquote.textContent < html.textContent) {
+            if (blockquote.textContent.trim() < html.textContent.trim()) {
                 var button = document.createElement('button');
                 var title = gettextCatalog.getString('Show previous message', null, 'Title');
 
