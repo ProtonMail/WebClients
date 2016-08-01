77d3d247022526ea66e035014665cc80c5a1f4ac
Richard <EpokK@users.noreply.github.com>
2016-08-01 17:11:29 +0200

    ShowEmbedded option (#3431)
    * let's go
    
    * use showEmbedded condition
    
    * We just need to save the ShowEmbedded parameter
    
    * Set properly the parameter
    
    * Remove the link

--- src/app/message/services/transformImages.js
+++ src/app/message/services/transformImages.js
@@ -1,5 +1,5 @@
 angular.module('proton.message')
-.factory('transformImages', function() {
+.factory('transformImages', function(authentication) {
 
     const REGEXP_IS_CID = /^cid:/;
     const wrapImage = (img) => angular.element(img).wrap('<div class="image loading"></div>');
@@ -9,15 +9,19 @@ angular.module('proton.message')
         const images = [].slice.call(html.querySelectorAll('img[src]'));
 
         if (images.length > 0) {
-
             images.forEach(function(image) {
                 const src = image.src;
                 const isEmbedded = REGEXP_IS_CID.test(src);
-            
+
                 if (isEmbedded) {
+                    if (authentication.user.ShowEmbedded || message.showEmbedded) {
+                        image.setAttribute('data-embedded-img', src);
+                        !image.parentElement.classList.contains('loading') && wrapImage(image);
+                    } else {
+                        message.embeddedHidden = true;
+                    }
+
                     image.removeAttribute('src');
-                    image.setAttribute('data-embedded-img', src);
-                    !image.parentElement.classList.contains('loading') && wrapImage(image);
                 }
             });
         }
