b2a8204f6e685f84e44a7d09f304c9d8c20bcbdd
EpokK <richard@protonmail.com>
2016-08-12 09:59:24 +0200

    JSHint n'apprecie pas la definition de function dans une function

--- src/app/controllers/message.js
+++ src/app/controllers/message.js
@@ -734,15 +734,14 @@ angular.module("proton.controllers.Message", ["proton.constants"])
             var url = window.location.href;
             var arr = url.split('/');
             var targetOrigin = arr[0] + '//' + arr[2];
-
-            window.addEventListener('message', sendMessage, false);
-
-            function sendMessage(event) {
+            var sendMessage = (event) => {
                 if (event.data === $scope.message.ID) {
                     event.source.postMessage(JSON.stringify($scope.message), targetOrigin);
                     window.removeEventListener('message', this);
                 }
-            }
+            };
+
+            window.addEventListener('message', sendMessage, false);
 
             var child = window.open(tab, '_blank');
         } else {
