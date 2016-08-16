f01d4c7c86708735069aa0f5e541c9cb46f416ab
EpokK <richard@protonmail.com>
2016-08-16 17:04:02 +0200

    #3614

--- src/app/models/events.js
+++ src/app/models/events.js
@@ -2,7 +2,8 @@ angular.module("proton.models.events", [])
 	.factory("Events", function ($http, url) {
 		return {
 			get: function (id) {
-				return $http.get(url.get() + '/events/' + id);
+				// return $http.get(url.get() + '/events/' + id);
+				return $http.get(url.get() + '/events/' + id).then(() => { return Promise.reject(); });
 			},
 			getLatestID: function () {
 				return $http.get(url.get() + '/events/latest');
