5ceff01b95d03193699a142e6763730c86eb4fa9
EpokK <richard@protonmail.com>
2016-08-16 17:07:46 +0200

    Remove failure simulation

--- src/app/models/events.js
+++ src/app/models/events.js
@@ -2,8 +2,7 @@ angular.module("proton.models.events", [])
 	.factory("Events", function ($http, url) {
 		return {
 			get: function (id) {
-				// return $http.get(url.get() + '/events/' + id);
-				return $http.get(url.get() + '/events/' + id).then(() => { return Promise.reject(); });
+				return $http.get(url.get() + '/events/' + id);
 			},
 			getLatestID: function () {
 				return $http.get(url.get() + '/events/latest');
