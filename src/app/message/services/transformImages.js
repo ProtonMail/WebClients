bf8e467d4045c7730c3ffc2a3e3d430ff7ede363
dhoko <dhoko@users.noreply.github.com>
2016-08-18 10:50:01 +0200

    Fix #3370 ~ bind proton className to external images (#3631)

--- src/app/message/services/transformImages.js
+++ src/app/message/services/transformImages.js
@@ -2,6 +2,7 @@ angular.module('proton.message')
 .factory('transformImages', function(authentication, embedded) {
 
     const REGEXP_IS_CID = /^cid:/;
+    const EMBEDDED_CLASSNAME = 'proton-embedded';
     const wrapImage = (img) => angular.element(img).wrap('<div class="image loading"></div>');
 
     return function(html, message) {
@@ -14,6 +15,10 @@ angular.module('proton.message')
             images.forEach(function(image) {
                 const src = image.src;
                 const isEmbedded = REGEXP_IS_CID.test(src);
+
+                if (!image.classList.contains(EMBEDDED_CLASSNAME)) {
+                    image.classList.add(EMBEDDED_CLASSNAME);
+                }
 
                 if (isEmbedded) {
                     const attach = embedded.getAttachment(message,image.src);
