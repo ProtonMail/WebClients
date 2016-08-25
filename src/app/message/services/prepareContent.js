d2353b9ebef1bb54ef2ece55cd2bde8e7a67a96d
Richard <richard@protonmail.com>
2016-08-25 08:43:58 +0200

    Fix #3693

--- src/app/message/services/prepareContent.js
+++ src/app/message/services/prepareContent.js
@@ -1,11 +1,11 @@
 angular.module('proton.message')
-.factory('prepareContent', function($injector, transformAttachement) {
+.factory('prepareContent', function($injector, transformAttachement, transformRemote) {
     return function(content, message, blacklist = []) {
         const div = document.createElement('div');
-        const filters = ['transformLinks', 'transformImages', 'transformPrefix', 'transformWelcome', 'transformBlockquotes'].map((name) => ({ name, action: $injector.get(name) }));
+        const filters = ['transformLinks', 'transformEmbedded', 'transformWelcome', 'transformBlockquotes'].map((name) => ({ name, action: $injector.get(name) }));
         const transformers = filters.filter(({name}) => blacklist.indexOf(name) === -1);
 
-        div.innerHTML = content;
+        div.innerHTML = transformRemote(content, message);
 
         const output =  transformers.reduceRight((html, transformer) => transformer.action(html, message), div);
 
