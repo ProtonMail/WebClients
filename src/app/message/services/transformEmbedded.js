0ded04996b382e7d6d78075e5ed1921a8b88c16a
Richard <richard@protonmail.com>
2016-08-25 13:06:36 +0200

    Review getAttachment

--- src/app/message/services/transformEmbedded.js
+++ src/app/message/services/transformEmbedded.js
@@ -1,37 +1,34 @@
 angular.module('proton.message')
-.factory('transformEmbedded', function(authentication, embedded) {
-
+.factory('transformEmbedded', (authentication, embedded) => {
     const REGEXP_IS_CID = /^cid:/;
     const EMBEDDED_CLASSNAME = 'proton-embedded';
     const wrapImage = (img) => angular.element(img).wrap('<div class="image loading"></div>');
 
-    return function(html, message) {
+    return (html, message) => {
         const images = [].slice.call(html.querySelectorAll('img[src]'));
         const user = authentication.user || {ShowEmbedded:0};
         const show = message.showEmbedded === true || user.ShowEmbedded === 1;
 
-        if (images.length > 0) {
-            images.forEach(function(image) {
-                const src = image.getAttribute('src');
-                const attach = embedded.getAttachment(message, src);
+        images.forEach((image) => {
+            const src = image.getAttribute('src');
+            const attachment = embedded.getAttachment(message, src);
 
-                if (!image.classList.contains(EMBEDDED_CLASSNAME)) {
-                    image.classList.add(EMBEDDED_CLASSNAME);
-                }
+            if (!image.classList.contains(EMBEDDED_CLASSNAME)) {
+                image.classList.add(EMBEDDED_CLASSNAME);
+            }
 
-                // check if the attachment exist before processing
-                if (attach && Object.keys(attach).length > 0) {
-                    if (show) {
-                        image.setAttribute('data-embedded-img', src);
-                        !image.parentElement.classList.contains('loading') && wrapImage(image);
-                        image.removeAttribute('src');
-                    } else {
-                        message.showEmbedded = false;
-                        image.setAttribute('alt', attach.Name);
-                    }
+            // check if the attachment exist before processing
+            if (attachment && Object.keys(attachment).length > 0) {
+                if (show) {
+                    image.setAttribute('data-embedded-img', src);
+                    !image.parentElement.classList.contains('loading') && wrapImage(image);
+                    image.removeAttribute('src');
+                } else {
+                    message.showEmbedded = false;
+                    image.setAttribute('alt', attachment.Name);
                 }
-            });
-        }
+            }
+        });
 
         return html;
     };
