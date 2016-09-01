32e6e95250486bf3d7ae8edce209b985fa7b4710
Richard <richard@protonmail.com>
2016-09-01 15:05:45 +0200

    Fix

--- src/app/ui/filters/labelAutocomplete.js
+++ src/app/ui/filters/labelAutocomplete.js
@@ -1,4 +1,6 @@
 angular.module('proton.ui')
-.filter('labelAutocomplete', () => {
-    return ({ Name = '', Address = '' } = {}) => Name || Address;
+.filter('labelAutocomplete', (CONSTANTS) => {
+    const { OPEN_TAG_AUTOCOMPLETE, CLOSE_TAG_AUTOCOMPLETE } = CONSTANTS.EMAIL_FORMATING;
+
+    return ({ Name = '', Address = '' } = {}) => (Name === Address) ? Address : `${Name} ${OPEN_TAG_AUTOCOMPLETE}${Address}${CLOSE_TAG_AUTOCOMPLETE}`;
 });
