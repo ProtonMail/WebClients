4d2fcae0953f63669d7b132c0adb0a3d6f86dd6c
Richard <richard@protonmail.com>
2016-09-01 15:12:32 +0200

    Remove chevrons filter

--- src/app/ui/filters/labelAutocomplete.js
+++ src/app/ui/filters/labelAutocomplete.js
@@ -1,6 +1,6 @@
 angular.module('proton.ui')
 .filter('labelAutocomplete', (CONSTANTS) => {
-    const { OPEN_TAG_AUTOCOMPLETE, CLOSE_TAG_AUTOCOMPLETE } = CONSTANTS.EMAIL_FORMATING;
+    const { OPEN_TAG_AUTOCOMPLETE_RAW, CLOSE_TAG_AUTOCOMPLETE_RAW } = CONSTANTS.EMAIL_FORMATING;
 
-    return ({ Name = '', Address = '' } = {}) => (Name === Address) ? Address : `${Name} ${OPEN_TAG_AUTOCOMPLETE}${Address}${CLOSE_TAG_AUTOCOMPLETE}`;
+    return ({ Name = '', Address = '' } = {}) => (Name === Address) ? Address : `${Name} ${OPEN_TAG_AUTOCOMPLETE_RAW}${Address}${CLOSE_TAG_AUTOCOMPLETE_RAW}`;
 });
