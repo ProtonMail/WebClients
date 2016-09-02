0391ed18888ad5755f6d3c8248f014606c6bcce5
Richard <richard@protonmail.com>
2016-09-02 14:10:37 +0200

    Check splitted variable

--- src/app/message/services/transformBlockquotes.js
+++ src/app/message/services/transformBlockquotes.js
@@ -23,9 +23,9 @@ angular.module('proton.message')
         blockquotes.forEach((blockquote) => {
             if (!found) {
                 const child = blockquote.textContent.trim();
-                const splitted = parent.split(child);
+                const [ splitted = '' ] = parent.split(child);
 
-                if (child.length < parent.length && splitted[0].length) {
+                if (child.length < parent.length && splitted.length) {
                     const button = document.createElement('button');
                     const title = gettextCatalog.getString('Show previous message', null, 'Title');
 
