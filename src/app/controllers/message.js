be5d3e329a0dc0bb9c3f98247f96bc47ba5814ad
dhoko <dhoko@users.noreply.github.com>
2016-09-09 10:25:18 +0200

    :warning: Fix/loading long message (#3682)
    * Init fix loading long body
    
    * Fix #3674 ~ Improve rendering of the body and add a loader

--- src/app/controllers/message.js
+++ src/app/controllers/message.js
@@ -328,8 +328,7 @@ angular.module("proton.controllers.Message", ["proton.constants"])
 
                 var showMessage = function(content) {
 
-                    var deferred = $q.defer();
-
+                    // var deferred = $q.defer();
                     // Clear content with DOMPurify before anything happen!
                     content = DOMPurify.sanitize(content, {
                         ADD_ATTR: ['target'],
@@ -342,20 +341,20 @@ angular.module("proton.controllers.Message", ["proton.constants"])
                     }
 
                     // NOTE Plain text detection doesn't work. Check #1701
-                    var isHtml = tools.isHtml(content);
+                    // var isHtml = tools.isHtml(content);
 
                     content = prepareContent(content, $scope.message);
 
                     // Detect type of content
-                    if (isHtml === true) {
+                    // if (isHtml === true) {
                         $scope.isPlain = false;
                         $scope.message.viewMode = 'html';
                         $scope.message.setDecryptedBody(content);
-                        deferred.resolve();
-                    } else {
-                        $scope.isPlain = true;
-                        $scope.message.viewMode = 'plain';
-                    }
+                        // deferred.resolve();
+                    // } else {
+                    //     $scope.isPlain = true;
+                    //     $scope.message.viewMode = 'plain';
+                    // }
 
                     // Broken images
                     $(".email").find("img").error(function () {
@@ -364,7 +363,7 @@ angular.module("proton.controllers.Message", ["proton.constants"])
 
                     $scope.scrollToMe();
 
-                    return deferred.promise;
+                    // return deferred.promise;
                 };
 
                 // PGP/MIME case
