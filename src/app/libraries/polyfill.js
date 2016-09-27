c1c42f6ec555154e9cc0277c129a2d35ca0a6dce
Richard <richard@protonmail.com>
2016-09-27 10:03:04 +0200

    Add polyfill: Array.includes

--- src/app/libraries/polyfill.js
+++ src/app/libraries/polyfill.js
@@ -5,6 +5,39 @@ if (!String.prototype.startsWith) {
     };
 }
 
+if (!Array.prototype.includes) {
+  Array.prototype.includes = function(searchElement /*, fromIndex*/) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('Array.prototype.includes called on null or undefined');
+    }
+
+    var O = Object(this);
+    var len = parseInt(O.length, 10) || 0;
+    if (len === 0) {
+      return false;
+    }
+    var n = parseInt(arguments[1], 10) || 0;
+    var k;
+    if (n >= 0) {
+      k = n;
+    } else {
+      k = len + n;
+      if (k < 0) {k = 0;}
+    }
+    var currentElement;
+    while (k < len) {
+      currentElement = O[k];
+      if (searchElement === currentElement ||
+         (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+        return true;
+      }
+      k++;
+    }
+    return false;
+  };
+}
+
 if (!Array.from) {
   Array.from = (function () {
     var toStr = Object.prototype.toString;
