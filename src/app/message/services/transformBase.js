6c378cec3b6cea144f00c7c03032a7b225d25ef2
emersion <contact@emersion.fr>
2016-10-10 10:38:02 +0200

    Adds support for <base> tag (#3902)
    * Adds support for <base> tag
    
    * Replaces querySelectorAll by querySelector

---
+++ src/app/message/services/transformBase.js
@@ -0,0 +1,34 @@
+angular.module('proton.message')
+.factory('transformBase', () => {
+    const elements = [
+        {
+            selector: 'a:not([href^=http])',
+            attribute: 'href'
+        },
+        {
+            selector: 'img:not([src^=http])',
+            attribute: 'src'
+        }
+    ];
+
+    return (html, message) => {
+        const base = html.querySelector('base');
+        if (!base || !base.href) {
+            return html;
+        }
+
+        // Make sure base has trailing slash
+        let baseUrl = base.href;
+        if (baseUrl.substr(-1, 1) !== '/') {
+            baseUrl += '/';
+        }
+
+        elements.forEach(({selector, attribute}) => {
+            [].slice.call(html.querySelectorAll(selector)).forEach(el => {
+                el[attribute] = baseUrl + el.getAttribute(attribute);
+            });
+        });
+
+        return html;
+    };
+});
