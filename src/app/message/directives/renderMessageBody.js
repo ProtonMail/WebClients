6599d05e3fe4c50100ee69f1a82458c110ab921a
dhoko <dhoko@users.noreply.github.com>
2016-10-11 10:46:33 +0200

    Fix loader always hidden (#3936)

--- src/app/message/directives/renderMessageBody.js
+++ src/app/message/directives/renderMessageBody.js
@@ -1,41 +1,63 @@
 angular.module('proton.message')
-    .directive('renderMessageBody', (networkActivityTracker, $sce) => ({
-        link(scope, el) {
+    .directive('renderMessageBody', (networkActivityTracker, $sce) => {
 
-            const watcherKey = 'body';
+        /**
+         * Dispatch an action to render the loader only if
+         *     - the body is empty
+         *     - the body is big
+         * @param  {String} body
+         * @param  {String} action
+         * @return {void}
+         */
+        const dispatch = (body = '', action = 'load') => {
 
-            // Render the loader
-            networkActivityTracker.dispatch('load');
-
-            const onAnimationStart = ({ animationName }) => {
-
-                /**
-                 * Detect when the animation from the keyFrame nodeInserted is triggered
-                 * cf messageView.css, then hide the activityTracker
-                 */
-                if (animationName === 'nodeInserted') {
+            switch (action) {
+                case 'load':
+                    (body.length > 10000 || !body) && networkActivityTracker.dispatch('load');
+                    break;
+                default:
                     networkActivityTracker.dispatch('close');
-                }
-            };
+                    break;
+            }
+        };
 
-            el[0].addEventListener('animationstart', onAnimationStart, false);
+        return {
+            link(scope, el) {
 
-            // Update the body after every update of the decryptedBody
-            const unsubscribe = scope
-                .$watch(watcherKey, () => {
-                    scope
-                        .$applyAsync(() => {
-                            el[0].innerHTML = $sce.getTrustedHtml($sce.trustAsHtml(scope.body));
-                        });
-                });
 
-            scope
-                .$on('$destroy', () => {
-                    el[0].removeEventListener('animationstart', onAnimationStart, false);
-                    unsubscribe();
-                    // Close the loader
-                    networkActivityTracker.dispatch('close');
-                });
+                // Render the loader
+                dispatch(scope.body);
 
-        }
-    }));
+                const onAnimationStart = ({ animationName }) => {
+
+                    /**
+                     * Detect when the animation from the keyFrame nodeInserted is triggered
+                     * cf messageView.css, then hide the activityTracker
+                     */
+                    if (animationName === 'nodeInserted') {
+                        dispatch(scope.body, 'close');
+                    }
+                };
+
+                el[0].addEventListener('animationstart', onAnimationStart, false);
+
+                // Update the body after every update of the decryptedBody
+                const unsubscribe = scope
+                    .$watch('body', () => {
+                        scope
+                            .$applyAsync(() => {
+                                el[0].innerHTML = $sce.getTrustedHtml($sce.trustAsHtml(scope.body));
+                            });
+                    });
+
+                scope
+                    .$on('$destroy', () => {
+                        el[0].removeEventListener('animationstart', onAnimationStart, false);
+                        unsubscribe();
+                        // Close the loader
+                        networkActivityTracker.dispatch('close');
+                    });
+
+            }
+        };
+    });
