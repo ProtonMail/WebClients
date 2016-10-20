938e4825dd650d276ba300c861fda124ec936b3e
dhoko <dhoko@users.noreply.github.com>
2016-10-20 14:17:12 +0200

    Fix #4039 ~ extract to a filter (#4040)

--- src/app/core/directives/storageProgress.js
+++ src/app/core/directives/storageProgress.js
@@ -2,13 +2,14 @@ angular.module('proton.core')
     .directive('storageProgress', (authentication, $filter) => {
 
         const filter = $filter('humanSize');
-        const pourcentage = () => Math.round(100 * authentication.user.UsedSpace / authentication.user.MaxSpace);
+        const percentage = $filter('percentage');
+        const percentageValue = () => percentage(authentication.user.UsedSpace, authentication.user.MaxSpace);
 
         return {
             templateUrl: 'templates/directives/core/storageProgress.tpl.html',
             replace: true,
             link(scope) {
-                scope.storageStyle = () => ({ width: `${pourcentage()}%` });
+                scope.storageStyle = () => ({ width: `${percentageValue()}%` });
                 scope.storageValue = () => `${filter(authentication.user.UsedSpace)} / ${filter(authentication.user.MaxSpace)}`;
             }
         };
