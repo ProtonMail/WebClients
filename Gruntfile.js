b6cf2a09f15417d5214086f770c0bf6f31d78cc6
Richard <richard@protonmail.com>
2016-10-26 18:11:56 +0200

    3.5.7

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 var _ = require('lodash');
 var util = require('util');
-var appVersion = '3.5.6';
+var appVersion = '3.5.7';
 var apiVersion = '1';
 var dateVersion = new Date().toDateString();
 var clientID = 'Angular';
