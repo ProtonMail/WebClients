fba8266ef83ae070f18f974698b3727db8ca1876
Bart Butler <bart.c.butler@gmail.com>
2016-10-27 13:32:23 -0700

    rebase modal changes

--- src/app/core/factories/addressModal.js
+++ src/app/core/factories/addressModal.js
@@ -1,83 +1,73 @@
 angular.module('proton.core')
-.factory('addressModal', (pmModal, authentication, $rootScope, $state, $q, networkActivityTracker, notify, Address, gettextCatalog, eventManager, pmcw, Key, MemberKey) => {
+.factory('addressModal', (pmModal, CONSTANTS, setupKeys, authentication, $rootScope, $state, $q, networkActivityTracker, notify, Address, gettextCatalog) => {
     return pmModal({
         controllerAs: 'ctrl',
-        templateUrl: 'templates/modals/domain/address.tpl.html',
+        templateUrl: 'templates/modals/addAddress.tpl.html',
         controller(params) {
             // Variables
-            const { domain, organizationPublicKey, step, members = [] } = params;
+            const { domains = [], organizationKey = {}, members = [] } = params;
 
-            this.domain = domain;
-            this.organizationPublicKey = organizationPublicKey;
-            this.step = step;
+            this.domain = domains[0];
+            this.domains = domains;
+            this.organizationKey = organizationKey;
             this.address = '';
-            this.password = '';
             this.size = 2048;
             this.members = members;
             this.member = members[0];
+
+            this.alias = !angular.isDefined(this.domain.ID);
+
+            this.keyPhase = CONSTANTS.KEY_PHASE;
+            this.showMember = this.keyPhase > 3 && !this.alias;
+
+            if (!this.showMember) {
+                this.member = members.filter((member) => member.Self)[0];
+            }
 
             this.open = (name) => {
                 $rootScope.$broadcast(name, params.domain);
             };
 
             // Functions
-            this.add = function () {
+            this.submit = () => {
+
+                const member = this.member;
+                const mailboxPassword = authentication.getPassword();
+
+                if (this.member.Private === 0 && !this.organizationKey) {
+                    notify({ message: gettextCatalog.getString('Cannot decrypt organization key', null, 'Error'), classes: 'notification-danger' });
+                    return;
+                }
+
                 networkActivityTracker.track(
                     Address.create({ Local: this.address, Domain: this.domain.DomainName, MemberID: this.member.ID })
                     .then((result) => {
                         if (angular.isDefined(result.data) && result.data.Code === 1000) {
                             const address = result.data.Address;
-                            const password = this.password;
                             const numBits = this.size;
 
-                            const generate = function () {
-                                const randomString = authentication.randomString(24);
-
-                                return $q.all({
-                                    userKey: pmcw.generateKeysRSA(address.Email, password, numBits),
-                                    memberKey: pmcw.generateKeysRSA(address.Email, randomString, numBits),
-                                    token: pmcw.encryptMessage(randomString, this.organizationPublicKey)
-                                });
-                            }.bind(this);
-
-                            const keyRequest = function (result) {
-                                const deferred = $q.defer();
-
-                                MemberKey.create({
-                                    AddressID: address.ID,
-                                    UserKey: result.userKey.privateKeyArmored,
-                                    MemberKey: result.memberKey,
-                                    Token: result.token
-                                })
-                                .then((result) => {
-                                    if (result.data && result.data.Code === 1000) {
-                                        deferred.resolve();
-                                    } else if (result.data && result.data.Error) {
-                                        deferred.reject(result.data.Error);
-                                    } else {
-                                        deferred.reject('Request error');
-                                    }
-                                });
-
-                                return deferred.promise;
+                            const generate = () => {
+                                return setupKeys.generateAddresses([address], mailboxPassword, numBits);
                             };
 
-                            const finish = function () {
+                            const keyRequest = ([key]) => {
+                                if (this.member.Private === 0) {
+                                    return setupKeys.memberKey(mailboxPassword, key, member, organizationKey);
+                                }
+                                return setupKeys.key(key);
+                            };
+
+                            const finish = () => {
                                 notify({ message: gettextCatalog.getString('Address added', null, 'Info'), classes: 'notification-success' });
-                                this.domain.Addresses.push(address);
+                                params.submit(address);
+                            };
 
-                                return eventManager.call();
-                            }.bind(this);
-
-                            if (this.member.Private === 0) {
+                            if (this.member.Private === 0 || this.member.Self) {
                                 return generate()
                                     .then(keyRequest)
                                     .then(finish);
                             }
                             return finish();
-
-                        } else if (angular.isDefined(result.data) && result.data.Code === 31006) {
-                            notify({ message: gettextCatalog.getString('Domain not found', null, 'Error'), classes: 'notification-danger' });
                         } else if (angular.isDefined(result.data) && result.data.Error) {
                             notify({ message: result.data.Error, classes: 'notification-danger' });
                         } else {
@@ -85,18 +75,13 @@ angular.module('proton.core')
                         }
                     })
                 );
-            }.bind(this);
-
-            this.createMember = function () {
-                params.cancel();
-                $state.go('secured.members', { action: 'new' });
             };
 
-            this.next = function () {
-                params.next();
+            this.addMember = () => {
+                params.addMember();
             };
 
-            this.close = function () {
+            this.cancel = () => {
                 params.cancel();
             };
         }
