ce59c032a7718fc1f25a82919b1fa8f9a4c47e0e
Bart Butler <bart.c.butler@gmail.com>
2016-10-31 14:30:37 -0700

    fix babel for pmcrypto

--- conf.build.js
+++ conf.build.js
@@ -65,8 +65,7 @@ module.exports = {
       "vendor/angular-gettext/dist/angular-gettext.js",
       "vendor/awesomplete/awesomplete.js",
       "vendor/angular-ui-indeterminate/dist/indeterminate.js",
-      "vendor/bcryptjs/dist/bcrypt.js",
-      "vendor/openpgp/dist/openpgp.js"
+      "vendor/bcryptjs/dist/bcrypt.js"
     ],
     bootstrap_components: [
       "affix",
