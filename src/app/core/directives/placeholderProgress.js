4e9a702cdc0d1da53a9f79227135dc6723792aa9
Richard <richard@protonmail.com>
2016-11-14 11:01:36 +0100

    Fix #4151 - used space bar not working inside the right placeholder (welcome page)

--- src/app/core/directives/placeholderProgress.js
+++ src/app/core/directives/placeholderProgress.js
@@ -1,16 +1,16 @@
 angular.module('proton.core')
-    .directive('storageProgress', (authentication, $filter) => {
-
-        const filter = $filter('humanSize');
+    .directive('placeholderProgress', (authentication, $filter) => {
+        const humanSize = $filter('humanSize');
         const percentage = $filter('percentage');
         const percentageValue = () => percentage(authentication.user.UsedSpace, authentication.user.MaxSpace);
 
         return {
-            templateUrl: 'templates/directives/core/storageProgress.tpl.html',
+            templateUrl: 'templates/directives/core/placeholderProgress.tpl.html',
             replace: true,
             link(scope) {
                 scope.storageStyle = () => ({ width: `${percentageValue()}%` });
-                scope.storageValue = () => `${filter(authentication.user.UsedSpace)} / ${filter(authentication.user.MaxSpace)}`;
+                scope.usedSpace = humanSize(authentication.user.UsedSpace);
+                scope.maxSpace = humanSize(authentication.user.MaxSpace);
             }
         };
     });
