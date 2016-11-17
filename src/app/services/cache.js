4b3d4480cdd5f548ec5949528ab3f181f90a0356
dhoko <dhoko@cyaneus.org>
2016-11-17 11:29:16 +0100

    Extract title to its own factory and directive

--- src/app/services/cache.js
+++ src/app/services/cache.js
@@ -903,7 +903,8 @@ angular.module('proton.cache', [])
      */
     api.callRefresh = (messageIDs = [], conversationIDs = []) => {
         $rootScope.$broadcast('refreshElements');
-        $rootScope.$broadcast('updatePageName');
+        $rootScope.$emit('updatePageName');
+        console.trace('Call refresh')
         $rootScope.$emit('refreshConversation', conversationIDs);
         $rootScope.$emit('message.refresh', messageIDs);
     };
