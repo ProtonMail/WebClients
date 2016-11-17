15abffb912f591dbd4e9db5ef26fb842572e2eb5
adrien <adrien@buildog.com>
2016-11-17 20:10:07 +0100

    toggle password

---
+++ src/app/core/directives/togglePassword.js
@@ -0,0 +1,24 @@
+angular.module('proton.core')
+.directive('togglePassword', ($compile, gettextCatalog) => {
+    return {
+        restrict: 'A',
+        link(scope, elem) {
+            if (!$.browser.msie) {
+                const show = gettextCatalog.getString('Show', null, 'Action');
+                const hide = gettextCatalog.getString('Hide', null, 'Action');
+
+                const toggler = document.createElement('a');
+                toggler.innerHTML = show;
+
+                const toggleType = () => {
+                    const type = (elem.attr('type') === 'text' ? 'password' : 'text');
+                    elem.attr('type', type);
+                    toggler.innerHTML = (type === 'password') ? show : hide;
+                };
+
+                toggler.addEventListener('click', toggleType);
+                elem.wrap('<div class="pm_toggle_password"/>').after(toggler);
+            }
+        }
+    };
+});
