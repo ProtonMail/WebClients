3c589270ec63f8f2e2f3d8677bf2d9aeab12507b
Richard <richard@protonmail.com>
2016-11-18 05:19:22 +0100

    Review donate modal and use cardView directive

--- src/app/core/factories/donateModal.js
+++ src/app/core/factories/donateModal.js
@@ -4,105 +4,81 @@ angular.module('proton.core')
         controllerAs: 'ctrl',
         templateUrl: 'templates/modals/donate.tpl.html',
         controller(params) {
-            // Variables
-            this.process = false;
-            this.text = params.message || 'Donate to ProtonMail';
-            this.amount = 25;
-            this.methods = [];
-            this.months = [];
-
-            for (let i = 1; i <= 12; i++) {
-                this.months.push(i);
-            }
-
-            this.month = this.months[0];
-            this.years = [];
-
-            for (let i = 0; i < 12; i++) {
-                this.years.push(new Date().getFullYear() + i);
-            }
-
-            this.currencies = [
+            const self = this;
+            self.process = false;
+            self.amount = 25;
+            self.methods = [];
+            self.currencies = [
                 { label: 'USD', value: 'USD' },
                 { label: 'EUR', value: 'EUR' },
                 { label: 'CHF', value: 'CHF' }
             ];
-            this.currency = _.findWhere(this.currencies, { value: authentication.user.Currency });
-            this.number = '';
-            this.month = this.months[0];
-            this.year = this.years[0];
-            this.cvc = '';
-            this.fullname = '';
-            this.countries = tools.countries;
-            this.country = _.findWhere(this.countries, { value: 'US' });
-            this.zip = '';
-
-            if (angular.isDefined(params.currency)) {
-                this.currency = _.findWhere(this.currencies, { value: params.currency });
-            }
-
+            self.currency = _.findWhere(self.currencies, { value: authentication.user.Currency });
+            self.card = {};
             if (angular.isDefined(params.methods) && params.methods.length > 0) {
-                this.methods = params.methods;
-                this.method = this.methods[0];
+                self.methods = params.methods;
+                self.method = self.methods[0];
             }
 
             // Functions
-            const validateCardNumber = function () {
-                return Payment.validateCardNumber(this.number);
-            }.bind(this);
+            const validateCardNumber = () => {
+                return Payment.validateCardNumber(self.card.number);
+            };
 
-            const validateCardExpiry = function () {
-                return Payment.validateCardExpiry(this.month, this.year);
-            }.bind(this);
+            const validateCardExpiry = () => {
+                return Payment.validateCardExpiry(self.card.month, self.card.year);
+            };
 
-            const validateCardCVC = function () {
-                return Payment.validateCardCVC(this.cvc);
-            }.bind(this);
+            const validateCardCVC = () => {
+                return Payment.validateCardCVC(self.card.cvc);
+            };
 
-            const donatation = function () {
-                const year = (this.year.length === 2) ? '20' + this.year : this.year;
-                const amount = this.otherAmount || this.amount;
+            const donatation = () => {
+                const amount = self.otherAmount || self.amount;
+                const { number, month, year, cvc, fullname, zip } = self.card;
+                const country = self.card.country.value;
+                const currency = self.currency.value;
 
-                this.process = true;
+                self.process = true;
 
                 return Payment.donate({
                     Amount: amount * 100, // Don't be afraid
-                    Currency: this.currency.value,
+                    Currency: currency,
                     Payment: {
                         Type: 'card',
                         Details: {
-                            Number: this.number,
-                            ExpMonth: this.month,
-                            ExpYear: year,
-                            CVC: this.cvc,
-                            Name: this.fullname,
-                            Country: this.country.value,
-                            ZIP: this.zip
+                            Number: number,
+                            ExpMonth: month,
+                            ExpYear: (year.length === 2) ? '20' + year : year,
+                            CVC: cvc,
+                            Name: fullname,
+                            Country: country,
+                            ZIP: zip
                         }
                     }
                 });
-            }.bind(this);
+            };
 
-            const donatationWithMethod = function () {
-                const amount = this.otherAmount || this.amount;
-                this.process = true;
+            const donatationWithMethod = () => {
+                const amount = self.otherAmount || self.amount;
+                self.process = true;
 
                 return Payment.donate({
                     Amount: amount * 100, // Don't be afraid
-                    Currency: this.currency.value,
-                    PaymentMethodID: this.method.ID
+                    Currency: self.currency.value,
+                    PaymentMethodID: self.method.ID
                 });
-            }.bind(this);
+            };
 
-            const finish = function (result) {
+            const finish = (result) => {
                 const deferred = $q.defer();
 
-                this.process = false;
+                self.process = false;
 
                 if (result.data && result.data.Code === 1000) {
                     deferred.resolve();
                     notify({ message: 'Your support is essential to keeping ProtonMail running. Thank you for supporting internet privacy!', classes: 'notification-success' });
-                    this.close();
+                    self.close();
                 } else if (result.data && result.data.Error) {
                     deferred.reject(new Error(result.data.Error));
                 } else {
@@ -110,25 +86,25 @@ angular.module('proton.core')
                 }
 
                 return deferred.promise;
-            }.bind(this);
+            };
 
-            this.label = (method) => {
+            self.label = (method) => {
                 return '•••• •••• •••• ' + method.Details.Last4;
             };
 
-            this.selectAmount = (amount) => {
-                this.otherAmount = null;
-                this.amount = amount;
+            self.selectAmount = (amount) => {
+                self.otherAmount = null;
+                self.amount = amount;
             };
 
-            this.onFocusOtherAmount = () => {
-                this.amount = null;
+            self.onFocusOtherAmount = () => {
+                self.amount = null;
             };
 
-            this.donate = () => {
+            self.donate = () => {
                 let promise;
 
-                if (this.methods.length > 0) {
+                if (self.methods.length > 0) {
                     promise = donatationWithMethod()
                     .then(finish)
                     .catch((error) => {
@@ -148,7 +124,7 @@ angular.module('proton.core')
                 networkActivityTracker.track(promise);
             };
 
-            this.close = function () {
+            self.close = () => {
                 if (angular.isDefined(params.close) && angular.isFunction(params.close)) {
                     params.close();
                 }
