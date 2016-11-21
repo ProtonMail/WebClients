4537e534a1025ae04867f6f4eb4b7904c2ef3f8c
Richard <richard@protonmail.com>
2016-11-21 17:39:11 +0100

    Fix #3989 - New directive to display properly the title to know the number of element selected (conversation or message)

---
+++ src/app/ui/directives/numberElementSelected.js
@@ -0,0 +1,16 @@
+angular.module('proton.ui')
+.directive('numberElementSelected', (gettextCatalog, tools) => {
+    return {
+        scope: { number: '=' },
+        replace: true,
+        template: '<h2 ng-bind="text()"></h2>',
+        link(scope) {
+            scope.text = () => {
+                const { number } = scope;
+                const type = tools.typeList();
+                const element = (type === 'conversation') ? gettextCatalog.getPlural(number, 'conversation selected', 'conversations selected') : gettextCatalog.getPlural(number, 'message selected', 'messages selected');
+                return `${number} ${element}`;
+            };
+        }
+    };
+});
