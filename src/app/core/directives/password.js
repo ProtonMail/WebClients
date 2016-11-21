da30a0c3a519a46fdb8a548f9b9c6d7f1572ddc3
Richard <richard@protonmail.com>
2016-11-21 13:01:48 +0100

    Fix #4174 - Show password directive

---
+++ src/app/core/directives/password.js
@@ -0,0 +1,24 @@
+angular.module('proton.core')
+.directive('password', (CONSTANTS) => {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'templates/directives/core/password.tpl.html',
+        scope: {
+            value: '=',
+            form: '=',
+            compare: '='
+        },
+        compile(element, attrs) {
+            const { id = '', name = '', placeholder = '' } = attrs;
+            const input = element[0].querySelector('input');
+            input.setAttribute('id', id);
+            input.setAttribute('name', name);
+            input.setAttribute('placeholder', placeholder);
+            return (scope) => {
+                scope.message = scope.form[name].$error;
+                scope.max = CONSTANTS.LOGIN_PW_MAX_LEN;
+            };
+        }
+    };
+});
