afe1edddb56a46f79a600ab743f7cef861f640c6
Richard <richard@protonmail.com>
2016-11-21 21:55:07 +0100

    Start lifetimeModal

---
+++ src/app/core/factories/lifetimeModal.js
@@ -0,0 +1,60 @@
+angular.module('proton.core')
+.factory('lifetimeModal', (pmModal, Payment, networkActivityTracker, authentication) => {
+    return pmModal({
+        controllerAs: 'ctrl',
+        templateUrl: 'templates/modals/lifetime.tpl.html',
+        controller(params) {
+            const self = this;
+            self.card = {};
+            self.currencies = [{ label: 'USD', value: 'USD' }, { label: 'EUR', value: 'EUR' }, { label: 'CHF', value: 'CHF' }];
+            self.currency = _.findWhere(self.currencies, { value: authentication.user.Currency });
+            Payment.methods().then((result) => {
+                const { data } = result;
+                if (data.Code === 1000) {
+                    self.methods = data.PaymentMethods;
+                }
+            });
+            self.submit = () => {
+                const promise = Payment.credit(getParameters())
+                .then((result) => {
+                    const { data } = result;
+                    if (data.Code === 1000) {
+                        // Success
+                    } else if (data.Error) {
+                        return Promise.reject(data.Error);
+                    }
+                });
+                networkActivityTracker.track(promise);
+            };
+            self.cancel = () => {
+                params.close();
+            };
+            function getParameters() {
+                const currency = self.currency.value;
+                const parameters = {
+                    Amount: 1337,
+                    Currency: currency
+                };
+                if (self.methods.length) {
+                    parameters.PaymentMethodID = '';
+                } else {
+                    const { number, month, year, cvc, fullname, zip } = self.card;
+                    const country = self.card.country.value;
+                    parameters.Payment = {
+                        Type: 'card',
+                        Details: {
+                            Number: number,
+                            ExpMonth: month,
+                            ExpYear: year,
+                            CVC: cvc,
+                            Name: fullname,
+                            Country: country,
+                            ZIP: zip
+                        }
+                    };
+                }
+                return parameters;
+            }
+        }
+    });
+});
