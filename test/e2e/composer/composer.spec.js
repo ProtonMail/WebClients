0a3e7281033caa041ad9bbae3eda6de3787b9522
dhoko <dhoko@cyaneus.org>
2016-11-22 17:35:11 +0100

    Test 1 ~ simple message

--- test/e2e/composer/composer.spec.js
+++ test/e2e/composer/composer.spec.js
@@ -1,73 +1,16 @@
 const composer = require('./composer.po');
+const simple = require('./scenarii/simple.test');
+
 const message = {
-  Subject: 'JO: Ha que coucou',
-  ToList: 'qatest1@protonmail.com',
-  CCList: 'qatest2@protonmail.com',
-  BCCList: 'qatest3@protonmail.com',
-  body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos aperiam debitis, ipsam numquam eius unde cupiditate atque enim, deleniti amet, quidem itaque. Voluptatum quisquam voluptates neque, numquam molestiae! Molestiae, aliquam?'
+    Subject: 'JO: Ha que coucou',
+    ToList: 'qatest1@protonmail.com',
+    CCList: 'qatest2@protonmail.com',
+    BCCList: 'qatest3@protonmail.com',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos aperiam debitis, ipsam numquam eius unde cupiditate atque enim, deleniti amet, quidem itaque. Voluptatum quisquam voluptates neque, numquam molestiae! Molestiae, aliquam?'
 };
 
 describe('composer tests', () => {
-  const editor = composer();
-
-  describe('Composer simple message', () => {
-
-    let borodin;
-
-    it('should open a the composer', () => {
-      editor.open();
-      browser.sleep(500);
-      editor
-        .isOpened()
-        .then(test => {
-          borodin = editor.compose();
-          expect(test).toEqual(true)
-        });
-    })
-
-    it('should create a new message', () => {
-      borodin
-        .content(message.body)
-        .then((text) => {
-          expect(text).toEqual(message.body);
-        })
-    })
-
-    it('should add a recepient', () => {
-      borodin
-        .fillInput('ToList', message.ToList)
-        .then((text) => {
-          expect(text).toEqual(message.ToList);
-        })
-    })
-
-    it('should add a subject', () => {
-      borodin
-        .fillInput('Subject', message.Subject)
-        .then((text) => {
-          expect(text).toEqual(message.Subject);
-        })
-    })
-
-    it('should send the message', () => {
-      borodin
-        .send()
-        .then(() => browser.sleep(5000))
-        .then(() => {
-          borodin
-            .isOpened()
-            .then((editor) => {
-              expect(editor).toEqual(false);
-            });
-        });
-    })
-
-    // it('should hide the composer', () => {
-    // })
-
-    // it('should add a new conversation', () => {
-    // })
-  })
-
+    const editor = composer();
+    simple(editor, message);
 
 });
