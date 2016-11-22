9a06df6dcfa87d2febf1fc4bf148a0edee364e40
dhoko <dhoko@cyaneus.org>
2016-11-22 15:47:11 +0100

    Do not add the loader inside an outside reply

--- src/app/message/services/transformEscape.js
+++ src/app/message/services/transformEscape.js
@@ -2,15 +2,11 @@ angular.module('proton.message')
 .factory('transformEscape', () => {
 
     const REGEXP_IS_BREAK = new RegExp('(<svg|xlink:href|srcset|src=|background=|poster=)', 'g');
-    // const REGEXP_IS_BREAK = new RegExp('(<svg|xlink:href|srcset|(^(?!proton-)src=)|background=|poster=)', 'g')
     const REGEXP_IS_URL = new RegExp(/url\(/ig);
-    const REGEXP_IS_PROTON = new RegExp(/proton-/ig);
     const replace = (regex, input) => input.replace(regex, (match) => `proton-${match}`);
 
     return (html, message, { content = '' }) => {
-        const input = content.replace(REGEXP_IS_PROTON, '');
-        html.innerHTML = replace(REGEXP_IS_URL, replace(REGEXP_IS_BREAK, input));
+        html.innerHTML = replace(REGEXP_IS_URL, replace(REGEXP_IS_BREAK, content));
         return html;
     };
 });
-// (<svg|xlink:href|srcset|(proton-)?src|background=|poster=)
