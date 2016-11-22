03c762f55bc0b8c162e84f5e8216afc81141f279
dhoko <dhoko@cyaneus.org>
2016-11-22 18:38:47 +0100

    Add test message with a remote content

--- test/e2e/composer/composer.spec.js
+++ test/e2e/composer/composer.spec.js
@@ -1,18 +1,21 @@
 const composer = require('./composer.po');
 const simple = require('./scenarii/simple.test');
 const simpleCCBCC = require('./scenarii/simpleCCBCC.test');
+const addFileLink = require('./scenarii/addFileLink.test');
 
 const message = {
     Subject: 'E2E: Ha que coucou',
     ToList: 'qatest123@protonmail.com',
     CCList: 'qatest123+roberto@protonmail.com',
     BCCList: 'qatest123+monique@protonmail.com',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos aperiam debitis, ipsam numquam eius unde cupiditate atque enim, deleniti amet, quidem itaque. Voluptatum quisquam voluptates neque, numquam molestiae! Molestiae, aliquam?'
+    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos aperiam debitis, ipsam numquam eius unde cupiditate atque enim, deleniti amet, quidem itaque. Voluptatum quisquam voluptates neque, numquam molestiae! Molestiae, aliquam?',
+    linkImage: 'https://i.imgur.com/WScAnHr.jpg'
 };
 
 describe('composer tests', () => {
     const editor = composer();
-    simple(editor, message);
-    simpleCCBCC(editor, message);
+    simple(editor, message, { identifier: 'simple' });
+    simpleCCBCC(editor, message, { identifier: 'simpleCCBCC' });
+    addFileLink(editor, message, { identifier: 'addFileLink' });
 
 });
