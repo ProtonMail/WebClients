894ff92cdffc9319cfa1200cf7cbe53f4db8996d
Richard <richard@protonmail.com>
2016-11-25 19:49:55 +0100

    Refactor country selector

--- src/app/core/directives/cardView.js
+++ src/app/core/directives/cardView.js
@@ -12,11 +12,18 @@ angular.module('proton.core')
             const currentYear = new Date().getFullYear();
             const months = _.range(1, 13);
             const years = _.range(currentYear, currentYear + 12);
-
+            const countries = [
+                { label: 'United States', value: 'US' },
+                { label: 'United Kingdom', value: 'GB' },
+                { label: 'Switzerland', value: 'CH' },
+                { label: 'France', value: 'FR' },
+                { label: 'Canada', value: 'CA' },
+                { label: '------------------', value: '', disabled: true }
+            ].concat(tools.countries);
+            const country = (scope.card.country) ? _.findWhere(countries, { value: scope.card.country.value }) : countries[0];
             scope.months = months;
             scope.years = years;
-            scope.countries = tools.countries;
-            const country = (scope.card.country) ? _.findWhere(scope.countries, { value: scope.card.country.value }) : _.findWhere(tools.countries, { priority: 1 });
+            scope.countries = countries;
             scope.card.country = country;
             scope.card.month = scope.months[0];
             scope.card.year = scope.years[0];
