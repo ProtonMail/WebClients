31d79bd3f6022d92c1d72fb32a6f3fc2f35d4247
dhoko <dhoko@cyaneus.org>
2016-11-25 15:48:13 +0100

    Add tests add attachemnts

--- test/e2e/composer/scenarii/uploadFile.test.js
+++ test/e2e/composer/scenarii/uploadFile.test.js
@@ -1,37 +1,51 @@
+const { isTrue, isFalse } = require('../../../e2e.utils/assertions');
+const simpleFile = require('./attachments/simpleFile.test');
+const addEmbedded = require('./attachments/addEmbedded.test');
 
 module.exports = ({ editor }) => {
-    describe('Upload a file a draft', () => {
+    describe('Attachments', () => {
 
         let borodin, dropzone, listAttachments;
 
-        it('should not display the dropzone', () => {
+        it('should not set the attachment button as active', () => {
             borodin = editor.compose();
             const uploader = borodin.uploader();
             dropzone = uploader.dropzone();
             listAttachments = uploader.attachmentsList();
 
+            uploader.isBtnActive()
+                .then(isFalse);
+        });
+
+        it('should not display the dropzone', () => {
             dropzone.isVisible()
-                .then((visible) => {
-                    expect(visible).toEqual(false);
-                });
+                .then(isFalse);
         });
 
         it('should not display the askEmbedded zone', () => {
             dropzone.isVisibleAsk()
-                .then((visible) => {
-                    expect(visible).toEqual(false);
-                });
+                .then(isFalse);
         });
 
         it('should display the askEmbedded', () => {
             borodin.upload();
-            browser.sleep(1000);
+            browser.sleep(500);
 
             dropzone.isVisibleAsk()
-                .then((visible) => {
-                    expect(visible).toEqual(true);
-                });
+                .then(isTrue);
         });
+
+        it('should hide on close', () => {
+            dropzone.cancel()
+                .then(() => dropzone.isVisibleAsk())
+                .then(isFalse)
+                .then(() => dropzone.isVisible())
+                .then(isFalse);
+        });
+
+        describe('Add an attachment', simpleFile({ editor }));
+        describe('Add an embedded', addEmbedded({ editor }));
+
 
     });
 
