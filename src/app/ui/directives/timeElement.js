3d352351c813ca80cca3241f3855dc86561a8de5
Richard <richard@protonmail.com>
2016-11-28 15:41:47 +0100

    Review time-element directive

--- src/app/ui/directives/timeElement.js
+++ src/app/ui/directives/timeElement.js
@@ -1,26 +1,30 @@
 angular.module('proton.ui')
-.directive('timeElement', ($interval, cache, tools) => {
+.directive('timeElement', ($rootScope, cache, tools) => {
+    /**
+    * Get time from element context
+    * @return {String}
+    */
+    function getTime({ ID, Time }) {
+        const type = tools.typeList();
+        const loc = tools.currentLocation();
+        const time = (type === 'conversation') ? cache.getTime(ID, loc) : Time;
+        const m = moment.unix(time);
+        const format = (m.isSame(moment(), 'day')) ? 'LT' : 'll';
+        return m.format(format);
+    }
     return {
         restrict: 'E',
         replace: true,
-        scope: { model: '=' },
-        template: '<em class="time"></em>',
+        template: '<time class="time"></time>',
         link(scope, element) {
-            const { ID, Time } = scope.model;
-            const minute = 60 * 1000;
-            const promise = $interval(() => displayTime(), minute, false);
-            scope.$on('$destroy', () => $interval.cancel(promise));
+            const unsubscribe = $rootScope.$on('refreshTimeElement', () => displayTime());
+            scope.$on('$destroy', () => unsubscribe());
             displayTime();
+            /**
+             * Insert new time value inside the element
+             */
             function displayTime() {
-                element[0].innerHTML = getTime();
-            }
-            function getTime() {
-                const type = tools.typeList();
-                const loc = tools.currentLocation();
-                const time = (type === 'conversation') ? cache.getTime(ID, loc) : Time;
-                const m = moment.unix(time);
-                const format = (m.isSame(moment(), 'day')) ? 'LT' : 'll';
-                return m.format(format);
+                element[0].textContent = getTime(scope.conversation);
             }
         }
     };
