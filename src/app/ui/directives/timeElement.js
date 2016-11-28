e05bfa4ab46107b8db20ba3994a2940add1c0981
Richard <richard@protonmail.com>
2016-11-28 14:24:36 +0100

    Add  time element directive

---
+++ src/app/ui/directives/timeElement.js
@@ -0,0 +1,29 @@
+angular.module('proton.ui')
+.directive('timeElement', ($interval, cache, tools) => {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: { model: '=' },
+        template: '<em class="time"></em>',
+        link(scope, element) {
+            const { ID, Time } = scope.model;
+            const minute = 60 * 1000;
+            const promise = $interval(() => {
+                element[0].innerHTML = getTime();
+            }, minute);
+            scope.$on('$destroy', () => {
+                $interval.cancel(promise);
+            });
+            function getTime() {
+                const type = tools.typeList();
+                const loc = tools.currentLocation();
+                const time = (type === 'conversation') ? cache.getTime(ID, loc) : Time;
+                const m = moment.unix(time);
+                if (m.isSame(moment(), 'day')) {
+                    return m.format('LT');
+                }
+                return m.format('ll');
+            }
+        }
+    };
+});
