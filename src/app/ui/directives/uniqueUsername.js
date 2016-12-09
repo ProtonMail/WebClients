76413c1c853ab27305bed4d2db0deb8d0cb5ee31
Richard <richard@protonmail.com>
2016-12-09 15:02:20 +0100

    Review uniqueUsername directive

--- src/app/ui/directives/uniqueUsername.js
+++ src/app/ui/directives/uniqueUsername.js
@@ -1,15 +1,14 @@
 angular.module('proton.ui')
-.directive('uniqueUsername', (User, gettextCatalog) => {
+.directive('uniqueUsername', (User) => {
     function isUsernameAvailable(username) {
         return User.available(username)
         .then(({ data = {} }) => {
             const { Code, Error, Available } = data;
             if (Code === 1000) {
-                if (!Available) {
-                    const error = gettextCatalog.getString('Username already taken', null, 'Error');
-                    return Promise.reject(error);
+                if (Available) {
+                    return Promise.resolve();
                 }
-                return Promise.resolve();
+                return Promise.reject('Username already taken');
             } else if (Error) {
                 return Promise.reject(Error);
             }
