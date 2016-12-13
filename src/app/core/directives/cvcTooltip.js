8f97978ec60968263713887acf5610d3bcbb7d59
dhoko <dhoko@cyaneus.org>
2016-12-13 16:43:05 +0100

    Fix #4309 ~ focus instead of trigger as default behaviour

--- src/app/core/directives/cvcTooltip.js
+++ src/app/core/directives/cvcTooltip.js
@@ -1,30 +1,39 @@
 angular.module('proton.core')
-.directive('cvcTooltip', (gettextCatalog) => ({
-    restrict: 'A',
-    link(scope, element) {
-        const line1 = gettextCatalog.getString('For Visa, MasterCard and Discover, the 3 digits on the back of your card.', null);
-        const line2 = gettextCatalog.getString('For American Express, the 4 digits on the front of your card.', null);
-        const title = gettextCatalog.getString('Security Code', null);
-        const options = {
-            placement: 'top',
-            container: 'body',
-            html: true,
-            title,
-            content: `
-                <p>${line1}</p>
-                <p>${line2}</p>
-            `,
-            template: `
-                <div class="popover" role="tooltip">
-                    <div class="arrow"></div>
-                    <div class="popover-title bold"></div>
-                    <div class="popover-content"></div>
-                </div>
-            `
-        };
-        element.popover(options);
-        scope.$on('$destroy', () => {
-            element.popover('destroy'); // it doesn't work, Bootstrap problem?
-        });
-    }
-}));
+.directive('cvcTooltip', (gettextCatalog) => {
+    const line1 = gettextCatalog.getString('For Visa, MasterCard and Discover, the 3 digits on the back of your card.', null);
+    const line2 = gettextCatalog.getString('For American Express, the 4 digits on the front of your card.', null);
+    const title = gettextCatalog.getString('Security Code', null);
+
+    return {
+        restrict: 'A',
+        link(scope, element) {
+
+            element[0].setAttribute('tabindex', 0);
+            element[0].setAttribute('role', 'button');
+
+            const options = {
+                placement: 'top',
+                container: 'body',
+                html: true,
+                title,
+                trigger: 'focus',
+                content: `
+                    <p>${line1}</p>
+                    <p>${line2}</p>
+                `,
+                template: `
+                    <div class="popover" role="tooltip">
+                        <div class="arrow"></div>
+                        <div class="popover-title bold"></div>
+                        <div class="popover-content"></div>
+                    </div>
+                `
+            };
+            element.popover(options);
+            scope.$on('$destroy', () => {
+                element.popover('destroy'); // it doesn't work, Bootstrap problem?
+            });
+        }
+    };
+
+});
