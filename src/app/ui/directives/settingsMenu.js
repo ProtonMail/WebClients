6e354698279a2ca6972115c21b880f31f074edf7
Richard <richard.tetaz@gmail.com>
2016-12-13 10:12:18 +0100

    Missing variable: keyPhase

--- src/app/ui/directives/settingsMenu.js
+++ src/app/ui/directives/settingsMenu.js
@@ -1,11 +1,12 @@
 angular.module('proton.ui')
-.directive('settingsMenu', (authentication) => {
+.directive('settingsMenu', (authentication, CONSTANTS) => {
     return {
         replace: true,
         restrict: 'E',
         templateUrl: 'templates/directives/ui/settingsMenu.tpl.html',
         link(scope) {
             scope.isSubUser = authentication.user.subuser;
+            scope.keyPhase = CONSTANTS.KEY_PHASE;
         }
     };
 });
