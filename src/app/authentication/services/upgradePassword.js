d5879e455505bb292fb8a042455518dffc3e5474
Richard <richard.tetaz@gmail.com>
2016-12-14 10:27:28 +0100

    Remove params

--- src/app/authentication/services/upgradePassword.js
+++ src/app/authentication/services/upgradePassword.js
@@ -13,13 +13,12 @@ angular.module('proton.authentication')
      */
     function send() {
         const Auth = tempStorage.getItem(key);
-        const params = { Auth };
-        const creds = tempStorage.getItem('creds');
 
         if (Auth) {
             tempStorage.removeItem(key);
-            return Setting.passwordUpgrade(params, creds);
+            return Setting.passwordUpgrade({ Auth });
         }
+        return Promise.resolve()
     }
     return { store, send };
 });
