f38b2f4bedec251f533133e08a395d272512c061
Richard <richard@protonmail.com>
2016-12-21 07:21:07 +0100

    Toggle text and icon (spam trash action)

--- src/app/core/directives/toggleTrashSpam.js
+++ src/app/core/directives/toggleTrashSpam.js
@@ -1,5 +1,8 @@
 angular.module('proton.core')
-.directive('toggleTrashSpam', ($state, $stateParams, tools) => {
+.directive('toggleTrashSpam', ($state, $stateParams, tools, gettextCatalog) => {
+    const ON_ICON = 'fa-toggle-on';
+    const OFF_ICON = 'fa-toggle-off';
+
     function onClick() {
         $state.go($state.$current.name, _.extend({}, $state.params, {
             trashspam: $stateParams.trashspam === '0' ? 1 : 0,
@@ -10,20 +13,35 @@ angular.module('proton.core')
         const box = tools.currentMailbox();
         element.style.display = (['label', 'sent', 'drafts'].indexOf(box) > -1) ? 'block' : 'none';
     }
+    function text(element) {
+        const hideTrashSpam = angular.isUndefined($stateParams.trashspam) || $stateParams.trashspam === '0';
+        const text = hideTrashSpam ? gettextCatalog.getString('Show trash and spam', null, 'Action') : gettextCatalog.getString('Hide trash and spam', null, 'Action');
+        element.textContent = text;
+    }
+    function icon(element) {
+        const hideTrashSpam = angular.isUndefined($stateParams.trashspam) || $stateParams.trashspam === '0';
+        const className = hideTrashSpam ? ON_ICON : OFF_ICON;
+        element.classList.add(className);
+    }
     return {
         restrict: 'E',
         replace: true,
         templateUrl: 'templates/directives/core/toggleTrashSpam.tpl.html',
         link(scope, element) {
             const $link = element[0];
+            const $span = element[0].querySelector('span');
+            const $icon = element[0].querySelector('i');
             $link.addEventListener('click', onClick, false);
             scope.$on('$destroy', () => {
                 $link.removeEventListener('click', onClick, false);
             });
             scope.$on('$stateChangeSuccess', () => {
                 display($link);
+                text($span);
             });
             display($link);
+            text($span);
+            icon($icon);
         }
     };
 });
