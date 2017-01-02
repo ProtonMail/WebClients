711fb554078f3bdf9fe61f4193a917364ebe054e
Richard <richard@protonmail.com>
2017-01-02 11:41:40 +0100

    Change organization password with a modal

---
+++ src/app/core/factories/changeOrganizationPassword.js
@@ -0,0 +1,19 @@
+angular.module('proton.core')
+.factory('changeOrganizationPassword', (pmModal) => {
+    return pmModal({
+        controllerAs: 'ctrl',
+        templateUrl: 'templates/modals/changeOrganizationPassword.tpl.html',
+        controller(params) {
+            const self = this;
+            self.newRecoveryPassword = '';
+            self.confirmRecoveryPassword = '';
+            self.submit = () => {
+                params.close(self.newRecoveryPassword);
+            };
+            self.cancel = () => {
+                params.close();
+            };
+            setTimeout(() => document.getElementById('newRecoveryPassword').focus(), 0);
+        }
+    });
+});
