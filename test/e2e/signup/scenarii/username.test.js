080250f38c6ac02da9e1c283a7989325429e69a9
dhoko <dhoko@cyaneus.org>
2017-01-03 10:24:57 +0100

    Add tests for the username and password

---
+++ test/e2e/signup/scenarii/username.test.js
@@ -0,0 +1,115 @@
+const { isTrue, isFalse, isLength, contains, assert } = require('../../../e2e.utils/assertions');
+
+module.exports = (utils) => {
+
+    const USERNAME = `proton${Date.now()}`;
+
+    describe('Username', () => {
+        const errors = utils.getErrors('username');
+
+        describe('Empty', () => {
+            it('should  not check if it is available', () => {
+                utils.fillInput('username', 'a')
+                    .then(() => utils.fillInput('username', ''))
+                    .then(() => browser.sleep(100))
+                    .then(() => utils.usernameMsg())
+                    .then(assert(''));
+                browser.sleep(2000);
+            });
+
+            it('should dislay an error', () => {
+                errors.isVisible()
+                    .then(isTrue);
+            });
+
+            it('should display one message', () => {
+                errors.getMessage()
+                    .then(isLength(1));
+            });
+
+            it('should not be available', () => {
+                errors.getMessage()
+                    .then(contains((t) => t === 'Username required'));
+            });
+
+        });
+
+        describe('Invalid', () => {
+            it('should not check if it is available', () => {
+                ['<', '>', '!', '+'].forEach((key) => {
+                    utils.fillInput('username', key)
+                        .then(() => browser.sleep(100))
+                        .then(() => utils.usernameMsg())
+                        .then(assert(''));
+                });
+                browser.sleep(2000);
+            });
+
+            it('should dislay an error', () => {
+                errors.isVisible()
+                    .then(isTrue);
+            });
+
+            it('should display one message', () => {
+                errors.getMessage()
+                    .then(isLength(1));
+            });
+
+            it('should not be available', () => {
+                errors.getMessage()
+                    .then(contains((t) => t === 'Username invalid'));
+            });
+
+        });
+
+        describe('Non available', () => {
+
+            it('should check if it is available', () => {
+                utils.fillInput('username', browser.params.login)
+                    .then(() => browser.sleep(100))
+                    .then(() => utils.usernameMsg())
+                    .then(assert('Checking username'));
+                browser.sleep(2000);
+            });
+
+            it('should dislay an error', () => {
+                errors.isVisible()
+                    .then(isTrue);
+            });
+
+            it('should display one message', () => {
+                errors.getMessage()
+                    .then(isLength(1));
+            });
+
+            it('should not be available', () => {
+                errors.getMessage()
+                    .then(contains((t) => t === 'Username already taken'));
+            });
+
+        });
+
+        describe('Available', () => {
+
+            it('should check if it is available', () => {
+                utils.fillInput('username', USERNAME)
+                    .then(() => utils.usernameMsg())
+                    .then(assert('Checking username'));
+
+            });
+
+            it('should be available', () => {
+                browser.sleep(1000)
+                    .then(() => utils.usernameMsg())
+                    .then(assert('Username available'));
+            });
+
+            it('should not dislay an error', () => {
+                errors.isVisible()
+                    .then(isFalse);
+            });
+
+        });
+
+    });
+};
--- test/e2e/signup/signup.spec.js
+++ test/e2e/signup/signup.spec.js
@@ -1,8 +1,7 @@
 const webapp = require('../../e2e.utils/webapp');
-const { isTrue, isFalse, contains, assert } = require('../../e2e.utils/assertions');
+const { isTrue, isFalse, isLength, contains, assert } = require('../../e2e.utils/assertions');
 const utils = require('./signup.po')();
 
-const USERNAME = `proton${Date.now()}`;
 
 describe('Create an account', () => {
 
@@ -12,71 +11,8 @@ describe('Create an account', () => {
         browser.sleep(2000);
     });
 
-    describe('Username', () => {
-        const errors = utils.getErrors('username');
-        describe('Available', () => {
-
-            it('should check if it is available', () => {
-                utils.fillInput('username', USERNAME)
-                    .then(() => utils.usernameMsg())
-                    .then(assert('Checking username'))
-                    .then(() => browser.sleep(2000));
-
-            });
-
-            it('should be available', () => {
-                utils.usernameMsg()
-                    .then(assert('Username available'));
-            });
-
-            it('should not dislay an error', () => {
-                errors.isVisible()
-                    .then(isFalse);
-            });
-
-        });
-
-        describe('Non available', () => {
-            it('should check if it is available', () => {
-                utils.fillInput('username', browser.params.login)
-                    .then(() => browser.sleep(100))
-                    .then(() => utils.usernameMsg())
-                    .then(assert('Checking username'));
-                browser.sleep(2000);
-            });
-
-            it('should dislay an error', () => {
-                errors.isVisible()
-                    .then(isTrue);
-            });
-
-            it('should not be available', () => {
-                errors.getMessage()
-                    .then(contains((t) => t === 'Username already taken'));
-            });
-
-        });
-    });
-
-
-    describe('Password', () => {
-        describe('1st set only', () => {
-
-        });
-
-        describe('2sd set only', () => {
-
-        });
-
-        describe('Different passwords', () => {
-
-        });
-
-        describe('Same passwords', () => {
-
-        });
-
-    });
+    // require('./scenarii/username.test')(utils);
+    require('./scenarii/password.test')(utils);
 
     describe('Recovery', () => {
         describe('Invalid email', () => {
