3b69345a1e7a926a6667a0e9469bc396efa8b43f
Richard <richard@protonmail.com>
2017-01-03 17:10:51 +0100

    Review with @dhoko

--- src/app/formUtils/directives/twoFaField.js
+++ src/app/formUtils/directives/twoFaField.js
@@ -3,17 +3,6 @@ angular.module('proton.formUtils')
     return {
         restrict: 'E',
         replace: true,
-        templateUrl: 'templates/formUtils/twoFaField.tpl.html',
-        scope: { value: '=' },
-        compile(element, { id = '', name = '', placeholder = '', tabindex = 0, autofocus = false }) {
-            const input = element[0];
-
-            input.setAttribute('id', id);
-            input.setAttribute('name', name);
-            input.setAttribute('placeholder', placeholder);
-            input.setAttribute('tabindex', tabindex);
-
-            autofocus && input.setAttribute('autofocus', autofocus);
-        }
+        templateUrl: 'templates/formUtils/twoFaField.tpl.html'
     };
 });
