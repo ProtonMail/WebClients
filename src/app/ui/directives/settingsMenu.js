ac0921194c2cfefbb9eead62e9cf26a0e4e97e25
Richard <richard@protonmail.com>
2017-01-06 17:07:57 +0100

    Close #4446 - Add donate inside settings sidebar

--- src/app/ui/directives/settingsMenu.js
+++ src/app/ui/directives/settingsMenu.js
@@ -1,5 +1,5 @@
 angular.module('proton.ui')
-.directive('settingsMenu', (authentication, CONSTANTS) => {
+.directive('settingsMenu', (authentication, CONSTANTS, donateModal, Payment, networkActivityTracker) => {
     return {
         replace: true,
         restrict: 'E',
@@ -7,6 +7,23 @@ angular.module('proton.ui')
         link(scope) {
             scope.isSubUser = authentication.user.subuser;
             scope.keyPhase = CONSTANTS.KEY_PHASE;
+            scope.donate = () => {
+                const promise = Payment.methods()
+                .then(({ data = {} } = {}) => {
+                    const { Code, PaymentMethods } = data;
+                    if (Code === 1000) {
+                        donateModal.activate({
+                            params: {
+                                methods: PaymentMethods,
+                                close() {
+                                    donateModal.deactivate();
+                                }
+                            }
+                        });
+                    }
+                });
+                networkActivityTracker.track(promise);
+            };
         }
     };
 });
