0ed2970cd49424f1e92fa962843f282db8b2dc4c
dhoko <dhoko@cyaneus.org>
2017-01-06 11:46:35 +0100

    Remove extra temp var and add margin copyright

--- src/app/ui/directives/appCopyright.js
+++ src/app/ui/directives/appCopyright.js
@@ -1,13 +1,12 @@
 angular.module('proton.ui')
     .directive('appCopyright', (CONFIG, gettextCatalog) => {
-        const copyright = ` ${CONFIG.year}`;
         const label = gettextCatalog.getString('ProtonMail.com - Made globally, hosted in Switzerland.', null, 'copyright');
 
         return {
             replace: true,
             template: '<span class="appCopyright-container"></span>',
             compile(el) {
-                el.text(`${copyright} ${label}`);
+                el.text(`${CONFIG.year} ${label}`);
             }
         };
     });
