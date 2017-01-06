dd88b6f74f6d60f0092d9993c24a2a9f7128dce6
dhoko <dhoko@cyaneus.org>
2017-01-06 15:48:43 +0100

    Improve promise and logs for drafts/conversations

--- src/app/elements/controllers/elements.js
+++ src/app/elements/controllers/elements.js
@@ -410,9 +410,8 @@ angular.module('proton.elements')
 
 
             deferred.resolve(elements);
-        }, (error) => {
+        }, () => {
             notify({ message: gettextCatalog.getString('Error during quering conversations', null, 'Error'), classes: 'notification-danger' });
-            console.error(error);
         });
 
         if (context === false) {
