184cf0e046fbeaf73e833719f58641e646c48729
Richard <richard@protonmail.com>
2017-01-11 18:43:27 +0100

    Refactor header view

---
+++ src/app/ui/directives/headerView.js
@@ -0,0 +1,17 @@
+angular.module('proton.ui')
+.directive('headerView', (organizationModel, $rootScope) => {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/ui/headerView.tpl.html',
+        link(scope) {
+            const unsubscribes = [];
+            scope.organization = organizationModel.get();
+            unsubscribes.push($rootScope.$on('organizationChange', (event, newOrganization) => {
+                scope.organization = newOrganization;
+            }));
+            scope.$on('$destroy', () => {
+                unsubscribes.forEach((callback) => callback());
+            });
+        }
+    };
+});
