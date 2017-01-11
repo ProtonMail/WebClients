3bc472566947c4dde3becb353498a76eb0b3717d
Richard <richard@protonmail.com>
2017-01-11 17:11:41 +0100

    Review sidebarProgress directive

--- src/app/core/directives/sidebarProgress.js
+++ src/app/core/directives/sidebarProgress.js
@@ -1,15 +1,24 @@
 angular.module('proton.core')
-    .directive('sidebarProgress', (authentication, $filter) => {
+    .directive('sidebarProgress', (authentication, organizationModel, $filter, $rootScope) => {
         const filter = $filter('humanSize');
         const percentage = $filter('percentage');
         const percentageValue = () => percentage(authentication.user.UsedSpace, authentication.user.MaxSpace);
+        const organization = organizationModel.get();
 
         return {
             templateUrl: 'templates/directives/core/sidebarProgress.tpl.html',
             replace: true,
             link(scope) {
+                const unsubscribes = [];
+                scope.notVisionary = organization.PlanName !== 'visionary';
                 scope.storageStyle = () => ({ width: `${percentageValue()}%` });
                 scope.storageValue = () => `${filter(authentication.user.UsedSpace)} / ${filter(authentication.user.MaxSpace)}`;
+                unsubscribes.push($rootScope.$on('organizationChange', (event, newOrganization) => {
+                    scope.notVisionary = newOrganization.PlanName !== 'visionary';
+                }));
+                scope.$on('$destroy', () => {
+                    unsubscribes.forEach((callback) => callback());
+                });
             }
         };
     });
