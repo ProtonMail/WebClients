b6212e0a7174a78149851ac4b066f602b4e608f6
Richard <richard@protonmail.com>
2017-01-12 17:46:44 +0100

    Fix #4476 - use subscription from the scope

--- src/app/ui/directives/protonmailLogo.js
+++ src/app/ui/directives/protonmailLogo.js
@@ -6,8 +6,8 @@ angular.module('proton.ui')
             replace: true,
             link(scope, element) {
                 const img = element[0].querySelector('img');
-                function updateLogo(organization, subscription) {
-                    const isLifetime = subscription.CouponCode === 'LIFETIME';
+                function updateLogo(organization) {
+                    const isLifetime = scope.subscription.CouponCode === 'LIFETIME';
                     const isSubuser = authentication.user.subuser;
                     const isMember = authentication.user.Role === CONSTANTS.PAID_MEMBER_ROLE;
                     let src;
@@ -33,8 +33,8 @@ angular.module('proton.ui')
                     }
                     img.src = src;
                 }
-                scope.$on('organizationChange', (event, organization) => updateLogo(organization, scope.subscription));
-                updateLogo(organizationModel.get(), scope.subscription);
+                scope.$on('organizationChange', (event, organization) => updateLogo(organization));
+                updateLogo(scope.organization);
             }
         };
     });
