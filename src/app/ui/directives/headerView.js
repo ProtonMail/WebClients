b6f96a998f50041d74c0143dec5ab995cacbbcb3
Richard <richard@protonmail.com>
2017-01-13 14:24:06 +0100

    Fix organization in header

--- src/app/ui/directives/headerView.js
+++ src/app/ui/directives/headerView.js
@@ -1,18 +1,7 @@
 angular.module('proton.ui')
-.directive('headerView', (organizationModel, $rootScope) => {
+.directive('headerView', (organizationModel) => {
     return {
         restrict: 'E',
-        templateUrl: 'templates/ui/headerView.tpl.html',
-        scope: {},
-        link(scope) {
-            const unsubscribes = [];
-            scope.organization = organizationModel.get();
-            unsubscribes.push($rootScope.$on('organizationChange', (event, newOrganization) => {
-                scope.organization = newOrganization;
-            }));
-            scope.$on('$destroy', () => {
-                unsubscribes.forEach((callback) => callback());
-            });
-        }
+        templateUrl: 'templates/ui/headerView.tpl.html'
     };
 });
