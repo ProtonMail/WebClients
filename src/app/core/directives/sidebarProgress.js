1019016d75e48ab42c83e27ba154ce4271299c1e
Richard <richard@protonmail.com>
2017-01-13 10:38:57 +0100

    Review sidebarProgress

--- src/app/core/directives/sidebarProgress.js
+++ src/app/core/directives/sidebarProgress.js
@@ -9,15 +9,14 @@ angular.module('proton.core')
             templateUrl: 'templates/directives/core/sidebarProgress.tpl.html',
             replace: true,
             link(scope) {
-                const unsubscribes = [];
                 scope.notVisionary = organization.PlanName !== 'visionary';
                 scope.storageStyle = () => ({ width: `${percentageValue()}%` });
                 scope.storageValue = () => `${filter(authentication.user.UsedSpace)} / ${filter(authentication.user.MaxSpace)}`;
-                unsubscribes.push($rootScope.$on('organizationChange', (event, newOrganization) => {
+                const unsubscribe = $rootScope.$on('organizationChange', (event, newOrganization) => {
                     scope.notVisionary = newOrganization.PlanName !== 'visionary';
-                }));
+                });
                 scope.$on('$destroy', () => {
-                    unsubscribes.forEach((callback) => callback());
+                    unsubscribe();
                 });
             }
         };
