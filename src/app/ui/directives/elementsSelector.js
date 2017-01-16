b3586e1e3532275fe661d4d874aa9af851f1ff54
dhoko <dhoko@cyaneus.org>
2017-01-16 19:51:19 +0100

    Sort actions for select all elements

--- src/app/ui/directives/elementsSelector.js
+++ src/app/ui/directives/elementsSelector.js
@@ -1,19 +1,45 @@
 angular.module('proton.ui')
 .directive('elementsSelector', ($rootScope, authentication, gettextCatalog, dedentTpl) => {
     const isChecked = true;
-    const actions = [
-        { label: gettextCatalog.getString('All unread', null), icon: 'fa-eye-slash', action: 'unread' },
-        { label: gettextCatalog.getString('All read', null), icon: 'fa-eye', action: 'read' },
-        { label: gettextCatalog.getString('All unstarred', null), icon: 'fa-star-o', action: 'unstarred' },
-        { label: gettextCatalog.getString('All starred', null), icon: 'fa-star', action: 'starred' }
-    ];
+    const ORDER_FALSY = ['read', 'unread', 'star', 'unstar'];
+    const ORDER_TRUTHY = ['unread', 'read', 'unstar', 'star'];
 
-    return {
-        replace: true,
-        templateUrl: 'templates/directives/ui/elementsSelector.tpl.html',
-        compile(element) {
-            const dropdown = element[0].querySelector('.pm_dropdown');
-            const template = actions.reduce((prev, { label, icon, action }) => {
+    const ACTIONS = {
+        unread: {
+            label: gettextCatalog.getString('All unread', null),
+            icon: 'fa-eye-slash',
+            action: 'unread'
+        },
+        read: {
+            label: gettextCatalog.getString('All read', null),
+            icon: 'fa-eye',
+            action: 'read'
+        },
+        unstar: {
+            label: gettextCatalog.getString('All unstarred', null),
+            icon: 'fa-star-o',
+            action: 'unstarred'
+        },
+        star: {
+            label: gettextCatalog.getString('All starred', null),
+            icon: 'fa-star',
+            action: 'starred'
+        }
+    };
+
+    const map = (list) => list.map((key) => ACTIONS[key]);
+    const orderActions = () => {
+        const order = +authentication.user.MessageButtons;
+        if (!order) {
+            return map(ORDER_FALSY);
+        }
+
+        return map(ORDER_TRUTHY);
+    };
+
+    const getTemplate = () => {
+        return orderActions()
+            .reduce((prev, { label, icon, action }) => {
                 return prev + dedentTpl(`
                     <button data-action="${action}">
                         <i class="fa ${icon}"></i>
@@ -21,14 +47,19 @@ angular.module('proton.ui')
                     </button>
                 `);
             }, '');
+    };
 
-            dropdown.insertAdjacentHTML('beforeEnd', template);
+    return {
+        replace: true,
+        templateUrl: 'templates/directives/ui/elementsSelector.tpl.html',
+        compile(element) {
+            const dropdown = element[0].querySelector('.pm_dropdown');
+            dropdown.insertAdjacentHTML('beforeEnd', getTemplate());
 
             return (scope, el) => {
                 const $btn = el.find('.element-selector-set-scope button');
 
                 $btn.on('click', onClick);
-                scope.value = 'all';
 
                 function onClick({ currentTarget }) {
                     const value = currentTarget.getAttribute('data-action');
@@ -36,10 +67,7 @@ angular.module('proton.ui')
                     $rootScope.$emit('closeDropdown');
                 }
 
-                scope.checkedSelectorState = () => {
-                    const { conversations = [] } = scope;
-                    return (conversations.length) ? _.every(conversations, { Selected: true }) : false;
-                };
+                scope.checkedSelectorState = () => _.every(scope.conversations, { Selected: true });
 
                 scope.$on('$destroy', () => {
                     $btn.off('click', onClick);
