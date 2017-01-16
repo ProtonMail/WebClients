f19c26c5bfdb58780744e7b47f22ebf9d12c27e5
Richard <richard@protonmail.com>
2017-01-16 18:01:29 +0100

    Review elementsSelector for WWF and Nicolas Hulot

--- src/app/ui/directives/elementsSelector.js
+++ src/app/ui/directives/elementsSelector.js
@@ -2,27 +2,48 @@ angular.module('proton.ui')
 .directive('elementsSelector', ($rootScope, authentication, gettextCatalog) => {
     const isChecked = true;
     const actions = [
-        { label: gettextCatalog.getString('All unread', null), icon: 'fa-eye-slash', scope: 'unread' },
-        { label: gettextCatalog.getString('All read', null), icon: 'fa-eye', scope: 'read' },
-        { label: gettextCatalog.getString('All unstarred', null), icon: 'fa-star-o', scope: 'unstarred' },
-        { label: gettextCatalog.getString('All starred', null), icon: 'fa-star', scope: 'starred' }
+        { label: gettextCatalog.getString('All unread', null), icon: 'fa-eye-slash', action: 'unread' },
+        { label: gettextCatalog.getString('All read', null), icon: 'fa-eye', action: 'read' },
+        { label: gettextCatalog.getString('All unstarred', null), icon: 'fa-star-o', action: 'unstarred' },
+        { label: gettextCatalog.getString('All starred', null), icon: 'fa-star', action: 'starred' }
     ];
 
     return {
         replace: true,
         templateUrl: 'templates/directives/ui/elementsSelector.tpl.html',
-        link(scope) {
-            scope.value = 'all';
-            scope.actions = actions;
+        compile(element) {
+            const dropdown = element[0].querySelector('.pm_dropdown');
+            const template = actions.reduce((prev, { label, icon, action }) => {
+                return prev + `
+                    <button data-action="${action}">
+                        <i class="fa ${icon}"></i>
+                        <span>${label}</span>
+                    </button>
+                `;
+            }, '');
 
-            scope.selectAction = (value) => {
-                $rootScope.$emit('selectElements', { value, isChecked });
-                $rootScope.$emit('closeDropdown');
-            };
+            dropdown.insertAdjacentHTML('beforeEnd', template);
 
-            scope.checkedSelectorState = () => {
-                const { conversations = [] } = scope;
-                return (conversations.length) ? _.every(conversations, { Selected: true }) : false;
+            return (scope, el) => {
+                const $btn = el.find('.element-selector-set-scope button');
+
+                $btn.on('click', onClick);
+                scope.value = 'all';
+
+                function onClick({ currentTarget }) {
+                    const value = currentTarget.getAttribute('data-action');
+                    $rootScope.$emit('selectElements', { value, isChecked });
+                    $rootScope.$emit('closeDropdown');
+                }
+
+                scope.checkedSelectorState = () => {
+                    const { conversations = [] } = scope;
+                    return (conversations.length) ? _.every(conversations, { Selected: true }) : false;
+                };
+
+                scope.$on('$destroy', () => {
+                    $btn.off('click', onClick);
+                });
             };
         }
     };
