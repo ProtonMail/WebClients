158b995768f445f7f28a09ee6845de973a03a3b3
Richard <richard@protonmail.com>
2017-01-20 12:04:57 +0100

    Review code with @dhoko

--- src/app/core/directives/hideUpgrade.js
+++ src/app/core/directives/hideUpgrade.js
@@ -1,23 +1,22 @@
 angular.module('proton.core')
 .directive('hideUpgrade', (organizationModel, $rootScope) => {
+    const HIDE_UPGRADE_CLASS = 'hideUpgrade';
     return {
         restrict: 'A',
         link(scope, element) {
-            const organization = organizationModel.get();
+            // NOTE the type is defined inside hide-upgrade attribute
             const unsubscribe = $rootScope.$on('organizationChange', (event, newOrganization) => update(newOrganization));
 
-            scope.$on('$destroy', () => unsubscribe());
-            update(organization);
-
-            function update(organization) {
-                const isVisionary = organization.PlanName === 'visionary';
-
-                if (isVisionary) {
-                    element[0].style.display = 'none';
-                } else {
-                    element[0].style.display = 'block';
+            function update({ PlanName }) {
+                if (PlanName === 'visionary') {
+                    return element[0].classList.add(HIDE_UPGRADE_CLASS);
                 }
+                return element[0].classList.remove(HIDE_UPGRADE_CLASS);
             }
+
+            update(organizationModel.get());
+
+            scope.$on('$destroy', () => unsubscribe());
         }
     };
 });
