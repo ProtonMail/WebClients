79525169461979cea477f16990b5d3dbba284d98
Richard <richard@protonmail.com>
2017-01-23 16:43:47 +0100

    Review how we hide deletion section

--- src/app/settings/controllers/account.js
+++ src/app/settings/controllers/account.js
@@ -225,6 +225,7 @@ angular.module('proton.settings')
         $scope.hotkeys = authentication.user.Hotkeys;
         $scope.signature = tools.replaceLineBreaks(authentication.user.Signature);
         $scope.passwordMode = authentication.user.PasswordMode;
+        $scope.isMember = authentication.user.Role === CONSTANTS.PAID_MEMBER_ROLE;
     }
 
     function changePMSignature(event, status) {
@@ -305,15 +306,6 @@ angular.module('proton.settings')
                 }
             })
         );
-    };
-
-    $scope.displayDeletion = () => {
-        const organization = organizationModel.get();
-        const isMember = authentication.user.Role === CONSTANTS.PAID_MEMBER_ROLE;
-        const isAdmin = authentication.user.Role === CONSTANTS.PAID_ADMIN_ROLE;
-        const usedMembers = organization.UsedMembers > 1;
-
-        return !isMember && !isAdmin && !usedMembers;
     };
 
     $scope.deleteAccount = () => {
