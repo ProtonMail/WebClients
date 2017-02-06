f301a35999f7377108d64d54f15045936583b56a
dhoko <dhoko@users.noreply.github.com>
2017-02-06 10:07:02 +0100

    Use a class instead of extend to prevent an extra ref :sad: (#4674)

--- src/app/message/factories/messageModel.js
+++ src/app/message/factories/messageModel.js
@@ -24,7 +24,6 @@ angular.module('proton.message')
         LabelIDs: [],
         ExternalID: null
     };
-    const defaultKeys = Object.keys(defaultMessage);
     const encryptionTypes = [
         gettextCatalog.getString('Unencrypted message', null),
         gettextCatalog.getString('End to end encrypted internal message', null),
@@ -36,57 +35,100 @@ angular.module('proton.message')
         gettextCatalog.getString('End to end encrypted using PGP', null),
         gettextCatalog.getString('End to end encrypted using PGP/MIME', null)
     ];
-    const cleanKey = (key) => key.replace(/(\r\n|\n|\r)/gm, '');
-    return (m = defaultMessage) => {
-        const message = angular.copy(m);
-        const isDraft = () => message.Type === 1;
-        const getAttachment = (ID) => _.findWhere(message.Attachments || [], { ID });
-        const getAttachments = () => message.Attachments || [];
-        const getDecryptedBody = () => message.DecryptedBody || '';
-        const encryptionType = () => encryptionTypes[message.IsEncrypted];
-        const plainText = () => message.getDecryptedBody();
-        const labels = () => $filter('labels')(message.LabelIDs);
-        const disableOthers = () => (message.saving && !message.autosaving) || message.sending || message.encrypting || message.askEmbedding;
-        const disableSend = () => message.uploading > 0 || disableOthers();
-        const disableSave = disableSend;
-        const disableDiscard = disableSend;
-        const generateReplyToken = () => pmcw.encode_base64(pmcw.arrayToBinaryString(pmcw.generateKeyAES())); // Use a base64-encoded AES256 session key as the reply token
-        const attachmentsSize = () => (message.Attachments || []).reduce((acc, { Size = 0 } = {}) => acc + (+Size), 0);
-        const getJSON = () => _.pick(message, defaultKeys);
 
-        function setDecryptedBody(input = '', purify = true) {
-            message.DecryptedBody = !purify ? input : DOMPurify.sanitize(input, {
+    class Message {
+
+        constructor(msg) {
+            _.extend(this, angular.copy(msg));
+            return this;
+        }
+
+        isDraft() {
+            return this.Type === 1;
+        }
+
+        generateReplyToken() {
+            // Use a base64-encoded AES256 session key as the reply token
+            return pmcw.encode_base64(pmcw.arrayToBinaryString(pmcw.generateKeyAES()));
+        }
+
+        encryptionType() {
+            return encryptionTypes[this.IsEncrypted];
+        }
+
+        plainText() {
+            return this.getDecryptedBody();
+        }
+
+        labels() {
+            $filter('labels')(this.LabelIDs);
+        }
+
+        disableOthers() {
+            return (this.saving && !this.autosaving) || this.sending || this.encrypting || this.askEmbedding;
+        }
+
+        disableSend() {
+            return this.uploading > 0 || this.disableOthers();
+        }
+
+        disableSave() {
+            return this.disableSend();
+        }
+        disableDiscard() {
+            return this.disableSend();
+        }
+
+        getAttachment(ID) {
+            return _.findWhere(this.Attachments || [], { ID });
+        }
+
+        getAttachments() {
+            return this.Attachments || [];
+        }
+
+        attachmentsSize() {
+            return (this.Attachments || [])
+                .reduce((acc, { Size = 0 } = {}) => acc + (+Size), 0);
+        }
+
+        countEmbedded() {
+            return this.Attachments
+                .filter(({ Headers = {} }) => Headers['content-disposition'] === 'inline')
+                .length;
+        }
+
+        addAttachments(list = []) {
+            this.Attachments = [].concat(this.Attachments, list);
+            this.NumEmbedded = this.countEmbedded();
+        }
+
+        removeAttachment({ ID } = {}) {
+            this.Attachments = (this.Attachments || [])
+                .filter((att) => att.ID !== ID);
+            this.NumEmbedded = this.countEmbedded();
+        }
+
+        setDecryptedBody(input = '', purify = true) {
+            this.DecryptedBody = !purify ? input : DOMPurify.sanitize(input, {
                 ADD_ATTR: ['target'],
                 FORBID_TAGS: ['style', 'input', 'form']
             });
         }
 
-        function countEmbedded() {
-            return message.Attachments
-                .filter(({ Headers = {} }) => Headers['content-disposition'] === 'inline')
-                .length;
+        getDecryptedBody() {
+            return this.DecryptedBody;
         }
 
-        function addAttachments(list = []) {
-            message.Attachments = [].concat(message.Attachments).concat(list);
-            message.NumEmbedded = countEmbedded();
-        }
-
-        function removeAttachment({ ID } = {}) {
-            message.Attachments = (message.Attachments || [])
-                .filter((att) => att.ID !== ID);
-            message.NumEmbedded = countEmbedded();
-        }
-
-        function close() {
-            if (angular.isDefined(message.timeoutSaving)) {
-                $timeout.cancel(message.timeoutSaving);
+        close() {
+            if (angular.isDefined(this.timeoutSaving)) {
+                $timeout.cancel(this.timeoutSaving);
             }
         }
 
-        function encryptBody(pubKey) {
-            const privKey = authentication.getPrivateKeys(message.From.ID);
-            return pmcw.encryptMessage(getDecryptedBody(), pubKey, [], privKey)
+        encryptBody(pubKey) {
+            const privKey = authentication.getPrivateKeys(this.From.ID);
+            return pmcw.encryptMessage(this.getDecryptedBody(), pubKey, [], privKey)
                 .catch((error) => {
                     error.message = gettextCatalog.getString('Error encrypting message');
                     throw error;
@@ -97,33 +139,31 @@ angular.module('proton.message')
          * Decrypt the body
          * @return {Promise}
          */
-        function decryptBody() {
-            const privKey = authentication.getPrivateKeys(message.AddressID);
+        decryptBody() {
+            const privKey = authentication.getPrivateKeys(this.AddressID);
             let pubKeys = null;
-            const sender = [message.Sender.Address];
-
-            message.decrypting = true;
-
-            return getPublicKeys(sender)
+            const sender = [this.Sender.Address];
+            this.decrypting = true;
+            return this.getPublicKeys(sender)
                 .then(({ data = {} } = {}) => {
                     if (data.Code === 1000) {
                         pubKeys = data[sender];
                     }
-                    return pmcw.decryptMessageRSA(message.Body, privKey, message.Time, pubKeys)
-                        .then((rep) => (message.decrypting = false, rep))
+                    return pmcw.decryptMessageRSA(this.Body, privKey, this.Time, pubKeys)
+                        .then((rep) => (this.decrypting = false, rep))
                         .catch((error) => {
-                            message.decrypting = false;
+                            this.decrypting = false;
                             throw error;
                         });
                 });
         }
 
-        function encryptPackets(keys = '', passwords = '') {
+        encryptPackets(keys = '', passwords = '') {
             const keysPackets = keys !== '' ? keys : [];
             const passwordsPackets = passwords !== '' ? passwords : [];
 
-            const promises = message.Attachments.map((attachment) => {
-                return AttachmentLoader.getSessionKey(message, attachment)
+            const promises = this.Attachments.map((attachment) => {
+                return AttachmentLoader.getSessionKey(this, attachment)
                     .then(({ sessionKey = {}, AttachmentID, ID } = {}) => {
                         // Update the ref
                         attachment.sessionKey = sessionKey;
@@ -139,12 +179,12 @@ angular.module('proton.message')
             return Promise.all(promises);
         }
 
-        function clearPackets() {
+        clearPackets() {
             const packets = [];
             const promises = [];
-            const keys = authentication.getPrivateKeys(message.AddressID);
+            const keys = authentication.getPrivateKeys(this.AddressID);
 
-            _.each(message.Attachments, (element) => {
+            _.each(this.Attachments, (element) => {
                 if (element.sessionKey === undefined) {
                     const keyPackets = pmcw.binaryStringToArray(pmcw.decode_base64(element.KeyPackets));
 
@@ -168,84 +208,55 @@ angular.module('proton.message')
             return $q.all(promises).then(() => packets);
         }
 
-        function emailsToString() {
-            return _.map(
-                [].concat(message.ToList, message.CCList, message.BCCList),
-                ({ Address } = {}) => Address
-            );
+        emailsToString() {
+            const list = [].concat(this.ToList, this.CCList, this.BCCList);
+            return _.map(list, ({ Address } = {}) => Address);
         }
 
-        function getPublicKeys(emails) {
+        getPublicKeys(emails) {
             const base64 = pmcw.encode_base64(emails.join(','));
-
             return User.pubkeys(base64);
         }
 
-        function clearTextBody() {
+        clearTextBody() {
             const deferred = $q.defer();
 
-            if (isDraft() || message.IsEncrypted > 0) {
-                if (!getDecryptedBody()) {
+            if (this.isDraft() || this.IsEncrypted > 0) {
+                debugger;
+                if (!this.getDecryptedBody()) {
                     try {
-                        decryptBody()
+                        this.decryptBody()
                         .then((result) => {
-                            setDecryptedBody(result.data);
-                            message.Signature = result.signature;
-                            message.failedDecryption = false;
+                            this.setDecryptedBody(result.data);
+                            this.Signature = result.signature;
+                            this.failedDecryption = false;
                             deferred.resolve(result.data);
                         })
                         .catch((err) => {
-                            setDecryptedBody(message.Body, false);
-                            message.failedDecryption = true;
+                            this.setDecryptedBody(this.Body, false);
+                            this.failedDecryption = true;
 
                             // We need to display the encrypted body to the user if it fails
-                            message.MIMEType = 'text/plain';
+                            this.MIMEType = 'text/plain';
                             deferred.reject(err);
                         });
                     } catch (err) {
-                        setDecryptedBody(message.Body, false);
-                        message.MIMEType = 'text/plain';
-                        message.failedDecryption = true;
+                        this.setDecryptedBody(this.Body, false);
+                        this.MIMEType = 'text/plain';
+                        this.failedDecryption = true;
                         deferred.reject(err);
                     }
                 } else {
-                    deferred.resolve(getDecryptedBody());
+                    deferred.resolve(this.getDecryptedBody());
                 }
             } else {
-                setDecryptedBody(message.Body, false);
-                deferred.resolve(getDecryptedBody());
+                this.setDecryptedBody(this.Body, false);
+                deferred.resolve(this.getDecryptedBody());
             }
 
             return deferred.promise;
         }
-        return _.extend(message, {
-            addAttachments,
-            attachmentsSize,
-            cleanKey,
-            clearPackets,
-            clearTextBody,
-            close,
-            countEmbedded,
-            decryptBody,
-            disableDiscard,
-            disableOthers,
-            disableSave,
-            disableSend,
-            emailsToString,
-            encryptBody,
-            encryptionType,
-            encryptPackets,
-            generateReplyToken,
-            getAttachment,
-            getAttachments,
-            getDecryptedBody,
-            getJSON,
-            getPublicKeys,
-            isDraft,
-            labels,
-            plainText,
-            removeAttachment,
-            setDecryptedBody
-        });
-    };
+    }
+
+    return (m = defaultMessage) => (new Message(m));
 });
