91ada47990b750a0b84c0446fb5deafd04fa7e3d
Richard <richard@protonmail.com>
2017-02-13 10:40:33 +0100

    Fix #4745 - current need to be tested before to change flags

--- src/app/core/services/cache.js
+++ src/app/core/services/cache.js
@@ -756,6 +756,9 @@ angular.module('proton.core')
         // if (!isSend && (!current || (current && event.Message.Time === current.Time))) {
         //     return Promise.resolve();
         // }
+        if (!current) {
+            return Promise.resolve();
+        }
 
         const message = _.extend(current, event.Message);
 
