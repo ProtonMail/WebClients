9a01c08002d265052e829b6781c0807bec0ad2e1
Richard <richard@protonmail.com>
2017-02-21 16:15:03 +0100

    params.password is already a Boolean in that case

--- src/app/vpn/factories/changeVPNPasswordModal.js
+++ src/app/vpn/factories/changeVPNPasswordModal.js
@@ -8,7 +8,7 @@ angular.module('proton.vpn')
         controller(params) {
             const self = this;
             self.VPNPassword = '';
-            self.passwordDefined = typeof params.password !== 'undefined';
+            self.passwordDefined = params.password;
             self.submit = () => {
                 const { VPNPassword } = self;
                 const promise = settingsApi.updateVPNPassword({ VPNPassword })
