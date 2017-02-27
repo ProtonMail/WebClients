caa6d5db7cd0e11c1d952a3c09706a505a97ab3d
Richard <richard@protonmail.com>
2017-02-27 07:35:12 +0100

    Fix #4839 - review bytes filter

--- src/app/utils/filters/bytes.js
+++ src/app/utils/filters/bytes.js
@@ -1,13 +1,14 @@
 angular.module('proton.utils')
-    .filter('bytes', () => {
-        const KB = 1000;
-        const UNITS = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'];
-        return function (bytes, precision = 1) {
+    .filter('bytes', (CONSTANTS) => {
+        const KB = CONSTANTS.BASE_SIZE;
+        const UNITS = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
+        return (bytes, precision = 1) => {
             if (isNaN(parseFloat(bytes)) || !isFinite(bytes) || bytes === 0) {
                 return '-';
             }
             const number = Math.floor(Math.log(bytes) / Math.log(KB));
-            const value = (bytes / Math.pow(KB, Math.floor(number))).toFixed(precision);
+            const digits = (number === 0) ? 0 : precision;
+            const value = (bytes / Math.pow(KB, Math.floor(number))).toFixed(digits);
             return `${value} ${UNITS[number]}`;
         };
     });
