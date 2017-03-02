88088feef4dd619e1b8f596b8b22199d03646e57
dhoko <dhoko@users.noreply.github.com>
2017-03-02 18:20:28 +0100

    Fix #4882 ~ fix custom input config for the component filter message (#4895)

--- src/app/message/directives/filterButton.js
+++ src/app/message/directives/filterButton.js
@@ -1,86 +1,76 @@
 angular.module('proton.message')
-.directive('filterButton', (filterModal) => ({
-    restrict: 'E',
-    replace: true,
-    scope: { message: '=' },
-    template: `
-    <span class="pm_buttons">
-        <a href="#" class="pm_trigger open-label" pt-tooltip-translate="Custom Filter" pt-tooltip-translate-context="Action" pt-tooltip-translate-comment="Tooltip"  dropdown>
-            <small><i class="fa fa-filter"></i> <i class="fa fa-angle-down"></i></small>
-        </a>
-        <span class="pm_dropdown wide">
-            <ul class="filterButton-container">
-                <li><b>Filter on:</b></li>
-                <li class="filterButton-item"><label class="filterButton-label"><span class="filterButton-label-text">Subject</span> <custom-checkbox class="filterButton-checkbox" data-custom-ng-model="model.subject"></custom-checkbox></label></li>
-                <li class="filterButton-item"><label class="filterButton-label"><span class="filterButton-label-text">Sender</span> <custom-checkbox class="filterButton-checkbox" ng-model="model.sender"></custom-checkbox></label></li>
-                <li class="filterButton-item"><label class="filterButton-label"><span class="filterButton-label-text">Recipient</span> <custom-checkbox class="filterButton-checkbox"  ng-model="model.recipient"></custom-checkbox></label></li>
-                <li class="filterButton-item"><label class="filterButton-label"><span class="filterButton-label-text">Attachments</span> <custom-checkbox class="filterButton-checkbox" ng-model="model.attachments"></custom-checkbox></label></li>
-            </ul>
-            <div>
-                <p></p>
-                <button class="pm_button primary" ng-click="next()">Next</button>
-            </div>
-        </span>
-    </span>
-    `,
-    link(scope, element) {
-        scope.model = {};
+.directive('filterButton', (filterModal) => {
+    const recipients = ({ ToList = [], CCList = [], BCCList = [] }) => {
+        return [].concat(ToList, CCList, BCCList).map(({ Address }) => Address);
+    };
 
-        function recipients({ ToList = [], CCList = [], BCCList = [] }) {
-            return [].concat(ToList).concat(CCList).concat(BCCList).map((contact) => contact.Address);
-        }
+    const attachments = ({ Attachments = [] }) => (Attachments.length ? 'contains' : '!contains');
 
-        function attachments({ Attachments = [] }) {
-            return (Attachments.length) ? 'contains' : '!contains';
-        }
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: { message: '=' },
+        templateUrl: 'templates/message/filterButton.tpl.html',
+        link(scope, el) {
+            scope.model = {};
+            const $btn = el[0].querySelector('.filterButton-btn-next');
 
-        function initialize() {
-            scope.model.subject = false;
-            scope.model.sender = false;
-            scope.model.recipient = false;
-            scope.model.attachments = false;
-        }
-
-        scope.next = () => {
-            const conditions = [];
-
-            if (scope.model.subject) {
-                conditions.push({ Type: { value: 'subject' }, Comparator: { value: 'contains' }, Values: [scope.message.Subject] });
+            function initialize() {
+                scope.model.subject = false;
+                scope.model.sender = false;
+                scope.model.recipient = false;
+                scope.model.attachments = false;
             }
+            initialize();
 
-            if (scope.model.sender) {
-                conditions.push({ Type: { value: 'sender' }, Comparator: { value: 'contains' }, Values: [scope.message.Sender.Address] });
-            }
+            const onClick = () => {
+                scope.$applyAsync(() => {
+                    const conditions = [];
 
-            if (scope.model.recipient) {
-                conditions.push({ Type: { value: 'recipient' }, Comparator: { value: 'contains' }, Values: recipients(scope.message) });
-            }
+                    if (scope.model.subject) {
+                        conditions.push({ Type: { value: 'subject' }, Comparator: { value: 'contains' }, Values: [scope.message.Subject] });
+                    }
 
-            if (scope.model.attachments) {
-                conditions.push({ Type: { value: 'attachments' }, Comparator: { value: attachments(scope.message) } });
-            }
+                    if (scope.model.sender) {
+                        conditions.push({ Type: { value: 'sender' }, Comparator: { value: 'contains' }, Values: [scope.message.Sender.Address] });
+                    }
 
-            const filter = {
-                Simple: {
-                    Operator: { value: 'all' },
-                    Actions: [],
-                    Conditions: conditions
-                }
+                    if (scope.model.recipient) {
+                        conditions.push({ Type: { value: 'recipient' }, Comparator: { value: 'contains' }, Values: recipients(scope.message) });
+                    }
+
+                    if (scope.model.attachments) {
+                        conditions.push({ Type: { value: 'attachments' }, Comparator: { value: attachments(scope.message) } });
+                    }
+
+                    const filter = {
+                        Simple: {
+                            Operator: { value: 'all' },
+                            Actions: [],
+                            Conditions: conditions
+                        }
+                    };
+
+                    filterModal.activate({
+                        params: {
+                            mode: 'simple',
+                            filter,
+                            close() { filterModal.deactivate(); }
+                        }
+                    });
+
+                    initialize();
+                    // Close the dropdown
+                    el.click();
+                });
             };
 
-            filterModal.activate({
-                params: {
-                    mode: 'simple',
-                    filter,
-                    close() { filterModal.deactivate(); }
-                }
+
+            $btn.addEventListener('click', onClick);
+
+            scope.$on('$destroy', () => {
+                $btn.removeEventListener('click', onClick);
             });
-
-            initialize();
-            // Close the dropdown
-            element.click();
-        };
-
-        initialize();
-    }
-}));
+        }
+    };
+});
