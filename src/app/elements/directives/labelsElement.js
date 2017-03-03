dc70e1843b23d8d9505ad098f0a2362d3329a4e2
dhoko <dhoko@users.noreply.github.com>
2017-03-03 17:13:39 +0100

    Improve labels message ~ Allow remove from the list (#4904)
    * Add labels edit
    
    * Fix #4890 ~ remove label from the list
    
    * RFR pmlabels
    
    * Remove useless CSS
    
    * Remove useless component labelsMessage
    
    * Fix conversations row mode

--- src/app/elements/directives/labelsElement.js
+++ src/app/elements/directives/labelsElement.js
@@ -27,7 +27,7 @@ angular.module('proton.elements')
         scope: {
             element: '='
         },
-        link(scope, el) {
+        link(scope, el, { limit = 4 }) {
 
             const moreToggle = moreVisibility(el[0].querySelector('.labelsElement-more'));
 
@@ -36,23 +36,41 @@ angular.module('proton.elements')
                 // Check if there is custom labels
                 if (Array.isArray(LabelIDs)) {
                     const labels = toLabels(LabelIDs);
-                    scope.labels = labels.length ? angular.copy(labels.slice(0, 4)) : [];
-                    labels.length > 4 && moreToggle.show();
+
+                    if (limit !== 'none') {
+                        scope.labels = labels.length ? angular.copy(labels.slice(0, limit)) : [];
+                        labels.length > limit && moreToggle.show();
+                        return;
+                    }
+
+                    scope.labels = angular.copy(labels);
                 }
             };
 
             const onClick = (e) => {
                 e.stopPropagation();
 
-                if (e.target.nodeName === 'LABEL') {
+                if (e.target.classList.contains('labelsElement-label')) {
                     const label = e.target.getAttribute('data-label-id');
                     $state.go('secured.label', { label });
+                }
+
+                if (e.target.classList.contains('labelsElement-btn-remove')) {
+                    $rootScope.$emit('messageActions', {
+                        action: 'unlabel',
+                        data: {
+                            messageID: scope.element.ID,
+                            conversationID: scope.element.ConversationID,
+                            labelID: e.target.getAttribute('data-label-id')
+                        }
+                    });
                 }
             };
 
             const unsubscribe = $rootScope.$on('labelsElement.' + scope.element.ID, build);
 
             build(undefined, scope.element);
+            scope.color = ({ Color: color = 'inherit' } = {}) => ({ color });
 
             el.on('click', onClick);
 
