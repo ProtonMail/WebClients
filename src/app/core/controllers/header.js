f16d85eff57578960debf873a87c6f48261416a3
Richard <richard@protonmail.com>
2017-03-07 11:32:17 +0100

    Fix #4921 - Review upgrade link fix

--- src/app/core/controllers/header.js
+++ src/app/core/controllers/header.js
@@ -1,5 +1,6 @@
 angular.module('proton.core')
 .controller('HeaderController', ($rootScope, $scope, $state, authentication, organizationModel, CONSTANTS) => {
+    const unsubscribes = [];
 
     $scope.params = {};
     $scope.user = authentication.user;
@@ -10,11 +11,19 @@ angular.module('proton.core')
     $scope.ctrl = {};
     $scope.starred = 2;
 
-    const unsubscribe = $rootScope.$on('organizationChange', (event, newOrganization) => {
+    unsubscribes.push($rootScope.$on('organizationChange', (event, newOrganization) => {
         $scope.organization = newOrganization;
-    });
-    $scope.$on('$destroy', () => unsubscribe());
+    }));
 
+    unsubscribes.push($rootScope.$on('updateUser', () => {
+        $scope.isAdmin = authentication.user.Role === CONSTANTS.PAID_ADMIN_ROLE;
+        $scope.isFree = authentication.user.Role === CONSTANTS.FREE_USER_ROLE;
+    }));
+
+    $scope.$on('$destroy', () => {
+        unsubscribes.forEach((cb) => cb());
+        unsubscribes.length = 0;
+    });
 
     // $scope.toggleAdvancedSearch = () => {
     //     if ($scope.advancedSearch === false) {
