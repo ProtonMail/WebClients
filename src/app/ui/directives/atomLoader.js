5518a57814d7b485b68462554d1aeac4e832802e
buildog <adrien@buildog.com>
2017-03-07 13:18:03 +0100

    Atom loader translate (#4925)
    * remove scope usage from atomLoader txt translation
    
    * map translations
    
    * review

--- src/app/ui/directives/atomLoader.js
+++ src/app/ui/directives/atomLoader.js
@@ -1,11 +1,21 @@
 angular.module('proton.ui')
 .directive('atomLoader', (gettextCatalog) => {
+
+    const translations = {
+        decrypting: gettextCatalog.getString('Decrypting', null, 'atom text loader')
+    };
+
+    const getTranslatedText = (translationKey) => {
+        return `${translations[translationKey]} ...`;
+    };
+
     return {
-        scope: { text: '@' },
         replace: true,
         templateUrl: 'templates/ui/atomLoader.tpl.html',
-        link(scope) {
-            scope.translated = gettextCatalog.getString(scope.text, null, 'atom text loader');
+        link(scope, el, { translationKey }) {
+
+            const $textLoader = el[0].querySelector('.atomLoader-text');
+            $textLoader.textContent = getTranslatedText(translationKey);
         }
     };
 });
