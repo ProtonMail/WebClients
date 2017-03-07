08cf9192f812f2cf7dee676e78f914a1d6d82577
dhoko <dhoko@cyaneus.org>
2017-03-07 09:37:09 +0100

    Add button upgrade inside the mobile sidebar

--- src/app/sidebar/directives/sidebarMobile.js
+++ src/app/sidebar/directives/sidebarMobile.js
@@ -1,9 +1,11 @@
 angular.module('proton.sidebar')
-    .directive('sidebarMobile', (sidebarModel, $rootScope) => ({
+    .directive('sidebarMobile', (sidebarModel, $rootScope, authentication, CONSTANTS) => ({
         replace: true,
         scope: {},
         templateUrl: 'templates/partials/sidebar-responsive.tpl.html',
         link(scope) {
+            scope.isAdmin = authentication.user.Role === CONSTANTS.PAID_ADMIN_ROLE;
+            scope.isFree = authentication.user.Role === CONSTANTS.FREE_USER_ROLE;
             scope.listStates = Object.keys(sidebarModel.getStateConfig());
 
             // Hide the sidebar with a delay for better perf on repaint
