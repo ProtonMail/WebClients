664353b58f18957eab96321d9a07e02fd754616e
Richard <richard@protonmail.com>
2017-03-08 22:15:45 +0100

    Fix #4936 - VPN for Plus

--- src/app/vpn/directives/vpnView.js
+++ src/app/vpn/directives/vpnView.js
@@ -1,9 +1,13 @@
 angular.module('proton.vpn')
-.directive('vpnView', ($rootScope, $state, CONSTANTS, authentication) => {
+.directive('vpnView', ($rootScope, $state, CONSTANTS, authentication, organizationModel) => {
     const isMember = () => authentication.user.Role === CONSTANTS.PAID_MEMBER_ROLE;
+    const isPlus = () => {
+        const { PlanName } = organizationModel.get();
+        return PlanName === 'plus';
+    };
     const vpnAccess = () => {
-        const { PlanName = '', Status = 0 } = authentication.user.VPN;
-        return PlanName === 'visionary' && Status === 1;
+        const { Status = 0 } = authentication.user.VPN;
+        return Status === 1;
     };
     const getFirstEmail = () => {
         const { Addresses = [] } = authentication.user;
@@ -17,18 +21,20 @@ angular.module('proton.vpn')
         link(scope) {
             isMember() && $state.go('secured.account');
             const unsubscribes = [];
-            scope.vpnEnabled = vpnAccess();
-            scope.VPNLogin = getFirstEmail();
-
-            unsubscribes.push($rootScope.$on('updateUser', () => {
+            const update = () => {
                 scope.VPNLogin = getFirstEmail();
                 scope.vpnEnabled = vpnAccess();
-            }));
+                scope.isPlus = isPlus();
+            };
+
+            unsubscribes.push($rootScope.$on('updateUser', () => update()));
 
             scope.$on('$destroy', () => {
                 unsubscribes.forEach((callback) => callback());
                 unsubscribes.length = 0;
             });
+
+            update();
         }
     };
 });
