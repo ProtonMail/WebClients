28f041d49f611e5104c6a45227c24e4b25b7e9fe
buildog <adrien@buildog.com>
2017-03-14 20:27:32 +0100

    fix #4970 ~ PW toggle > icons (#4971)
    * fix #4970 ~ PW toggle > icons
    
    * review togglePassword and add tooltips
    
    * KISS togglePassword icon

--- src/app/formUtils/directives/togglePassword.js
+++ src/app/formUtils/directives/togglePassword.js
@@ -1,33 +1,41 @@
 angular.module('proton.formUtils')
-.directive('togglePassword', (gettextCatalog) => {
-    const SHOW = gettextCatalog.getString('Show', null, 'Action');
-    const HIDE = gettextCatalog.getString('Hide', null, 'Action');
-    const isIE = $.ua.browser.name === 'IE';
+.directive('togglePassword', (gettextCatalog, dedentTpl) => {
+
+    const hasNativeButton = $.ua.browser.name === 'IE' || $.ua.browser.name === 'EDGE';
+    const CLASS_DISPLAY_PASSWORD = 'togglePassword-btn-display';
+    const TOOLTIPS = {
+        SHOW: gettextCatalog.getString('Show password', null, 'toggle password'),
+        HIDE: gettextCatalog.getString('Hide password', null, 'toggle password')
+    };
+
+    const template = dedentTpl`<button type="button" class="togglePassword-btn-toggle">
+        <i class="togglePassword-icon-toText fa fa-eye" pt-tooltip="${TOOLTIPS.SHOW}"></i>
+        <i class="togglePassword-icon-toPassword fa fa-eye-slash" pt-tooltip="${TOOLTIPS.HIDE}"></i>
+    </button>`;
 
     return {
         restrict: 'A',
-        compile(element) {
-            if (!isIE) {
-                const btn = document.createElement('BUTTON');
-                btn.type = 'button';
-                btn.className = 'togglePassword-btn-toggle';
-
-                const container = element[0].parentElement;
-                container.appendChild(btn);
-
-                return (scope, el) => {
-                    btn.innerHTML = SHOW;
-                    const onClick = () => {
-                        const type = (el[0].getAttribute('type') === 'text') ? 'password' : 'text';
-                        el[0].setAttribute('type', type);
-                        btn.textContent = (type === 'password') ? SHOW : HIDE;
-                    };
-                    btn.addEventListener('click', onClick);
-                    scope.$on('$destroy', () => {
-                        btn.removeEventListener('click', onClick);
-                    });
-                };
+        compile(el) {
+            if (hasNativeButton) {
+                return;
             }
+
+            const container = el[0].parentElement;
+            container.insertAdjacentHTML('beforeEnd', template);
+
+            return (scope, el) => {
+                const btn = el[0].parentElement.querySelector('.togglePassword-btn-toggle');
+                const onClick = () => {
+                    const type = (el[0].type === 'text') ? 'password' : 'text';
+                    el[0].setAttribute('type', type);
+                    btn.classList.toggle(CLASS_DISPLAY_PASSWORD);
+                };
+
+                btn.addEventListener('click', onClick);
+                scope.$on('$destroy', () => {
+                    btn.removeEventListener('click', onClick);
+                });
+            };
         }
     };
 });
