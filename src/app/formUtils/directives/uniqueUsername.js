a156724b0589394c59fc78d22c3af363bdc417e0
Richard <EpokK@users.noreply.github.com>
2017-03-28 11:31:04 +0200

    Handle redis jail properly for username checking (#5068)
    * Handle redis jail properly for username checking
    
    * Review
    
    * handle 429

--- src/app/formUtils/directives/uniqueUsername.js
+++ src/app/formUtils/directives/uniqueUsername.js
@@ -1,31 +1,47 @@
 angular.module('proton.formUtils')
-.directive('uniqueUsername', (User, $stateParams) => {
+.directive('uniqueUsername', ($stateParams, gettextCatalog, User) => {
+    const alreadyTaken = gettextCatalog.getString('Username already taken', null, 'Error');
+    const requestFailed = gettextCatalog.getString('The request failed', null, 'Error');
+    const tooMuch = gettextCatalog.getString('You are doing this too much, please try again later', null, 'Error');
     const cleanUsername = (username = '') => username.toLowerCase().replace('.', '').replace('_', '').replace('-', '');
-    function isUsernameAvailable(username) {
-        const usernameCleaned = cleanUsername(username);
-        // Temporary hack, remove along with $stateParams dependence once invite system migration complete
-        if (usernameCleaned === $stateParams.inviteSelector) {
-            return Promise.resolve();
-        }
 
-        return User.available(username)
-        .then(({ data = {} }) => {
-            const { Code, Error, Available } = data;
-            if (Code === 1000) {
-                if (Available) {
-                    return Promise.resolve();
-                }
-                return Promise.reject('Username already taken');
-            } else if (Error) {
-                return Promise.reject(Error);
-            }
-            return Promise.reject();
-        });
-    }
     return {
         require: 'ngModel',
         restrict: 'A',
+        scope: { uniqueError: '=' },
         link(scope, element, attributes, ngModel) {
+            function isUsernameAvailable(username) {
+                const usernameCleaned = cleanUsername(username);
+                // Temporary hack, remove along with $stateParams dependence once invite system migration complete
+                if (usernameCleaned === $stateParams.inviteSelector) {
+                    return Promise.resolve();
+                }
+
+                return User.available(username)
+                .then(({ data = {} } = {}) => {
+                    if (data.Code === 1000) {
+                        if (data.Available) {
+                            return Promise.resolve();
+                        }
+                        scope.uniqueError = alreadyTaken;
+                        return Promise.reject(alreadyTaken);
+                    }
+                    
+                    if (data.Error === 429) {
+                        scope.uniqueError = tooMuch;
+                        return Promise.reject(tooMuch);
+                    }
+                    
+                    if (data.Error) {
+                        scope.uniqueError = data.Error;
+                        return Promise.reject(data.Error);
+                    }
+                    
+                    scope.uniqueError = requestFailed;
+                    
+                    return Promise.reject(requestFailed);
+                });
+            }
             ngModel.$asyncValidators.unique = isUsernameAvailable;
         }
     };
