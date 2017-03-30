17434da178876d4d04621fff8f644af2c0838cb4
Richard <richard@protonmail.com>
2017-03-30 16:25:25 +0200

    Update link blog post

--- src/app/ui/directives/appVersion.js
+++ src/app/ui/directives/appVersion.js
@@ -1,6 +1,6 @@
 angular.module('proton.ui')
     .directive('appVersion', (CONFIG) => {
-        const URL = 'https://protonmail.com/blog/protonmail-beta/';
+        const URL = 'https://protonmail.com/blog/protonmail-v3-8-release-notes/';
 
         return {
             replace: true,
