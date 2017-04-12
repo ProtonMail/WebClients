02ef0bfecbf745176a657e7be375291a8cff9682
Richard <richard@protonmail.com>
2017-04-12 06:13:01 +0200

    Fix #5160

--- src/app/core/factories/cardModal.js
+++ src/app/core/factories/cardModal.js
@@ -1,68 +1,58 @@
 angular.module('proton.core')
-.factory('cardModal', (pmModal, Payment, notify, pmcw, tools, gettextCatalog, cardModel, networkActivityTracker) => {
-    return pmModal({
-        controllerAs: 'ctrl',
-        templateUrl: 'templates/modals/card.tpl.html',
-        controller(params) {
-            // Variables
-            const self = this;
-            self.process = false;
-            self.card = {};
+    .factory('cardModal', (pmModal, Payment, gettextCatalog, cardModel, networkActivityTracker) => {
+        return pmModal({
+            controllerAs: 'ctrl',
+            templateUrl: 'templates/modals/card.tpl.html',
+            controller(params) {
+                // Variables
+                const self = this;
+                self.card = {};
 
-            if (params.method) {
-                self.text = gettextCatalog.getString('Update your credit card information.', null);
-                self.mode = 'display';
-                self.panel = {
-                    fullname: params.method.Details.Name,
-                    number: '•••• •••• •••• ' + params.method.Details.Last4,
-                    month: params.method.Details.ExpMonth,
-                    year: params.method.Details.ExpYear,
-                    cvc: '•••',
-                    zip: params.method.Details.ZIP,
-                    country: params.method.Details.Country
-                };
-            } else {
-                self.text = gettextCatalog.getString('Add a credit card.', null);
-                self.mode = 'edition';
-            }
-
-            // Functions
-            const method = () => {
-                if (self.mode === 'edition') {
-                    const card = cardModel(self.card);
-
-                    return Payment.updateMethod({ Type: 'card', Details: card.details() })
-                    .then(({ data = {} } = {}) => {
-                        if (data.Code === 1000) {
-                            return data.PaymentMethod;
-                        }
-                        throw new Error(data.Error);
-                    });
+                if (params.method) {
+                    self.text = gettextCatalog.getString('Update your credit card information.', null);
+                    self.mode = 'display';
+                    self.panel = {
+                        fullname: params.method.Details.Name,
+                        number: '•••• •••• •••• ' + params.method.Details.Last4,
+                        month: params.method.Details.ExpMonth,
+                        year: params.method.Details.ExpYear,
+                        cvc: '•••',
+                        zip: params.method.Details.ZIP,
+                        country: params.method.Details.Country
+                    };
+                } else {
+                    self.text = gettextCatalog.getString('Add a credit card.', null);
+                    self.mode = 'edition';
                 }
-                return Promise.resolve();
-            };
 
-            const finish = (method) => params.close(method);
+                // Functions
+                const method = () => {
+                    if (self.mode === 'edition') {
+                        const card = cardModel(self.card);
+                        return Payment.updateMethod({ Type: 'card', Details: card.details() })
+                            .then(({ data = {} } = {}) => {
+                                if (data.Code === 1000) {
+                                    return data.PaymentMethod;
+                                }
+                                throw new Error(data.Error);
+                            });
+                    }
+                    return Promise.resolve();
+                };
 
-            self.edit = () => {
-                self.card.fullname = self.panel.fullname;
-                self.mode = 'edition';
-            };
+                const finish = (method) => params.close(method);
 
-            self.submit = () => {
-                self.process = true;
+                self.edit = () => {
+                    self.card.fullname = self.panel.fullname;
+                    self.mode = 'edition';
+                };
 
-                const promise = method()
-                    .then(finish)
-                    .catch((error) => {
-                        notify({ message: error, classes: 'notification-danger' });
-                        self.process = false;
-                    });
+                self.submit = () => {
+                    const promise = method().then(finish);
+                    networkActivityTracker.track(promise);
+                };
 
-                networkActivityTracker.track(promise);
-            };
-
-            self.cancel = () => params.close();
-        }
+                self.cancel = () => params.close();
+            }
+        });
     });
-});
