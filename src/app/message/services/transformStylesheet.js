703826973e40cdf24f985dccd410a9aa8d107a74
dhoko <dhoko@cyaneus.org>
2017-04-25 10:59:19 +0200

    Hotfix EO does not work after a reply :warning:

--- src/app/message/services/transformStylesheet.js
+++ src/app/message/services/transformStylesheet.js
@@ -9,7 +9,7 @@ angular.module('proton.message')
          * @{@link https://twitter.com/HTeuMeuLeu/status/852110032045035520}
          * Gmail changes the dom too.
          */
-        if (/absolute/.test(firstElement.style.position)) {
+        if (firstElement && /absolute/.test(firstElement.style.position)) {
             firstElement.style.position = 'inherit';
         }
         return html;
