1abda523fbd494d4e0db2ecc0cb1dc46d6c3543c
dhoko <dhoko@cyaneus.org>
2017-04-25 16:21:07 +0200

    Fix i18n task

--- tasks/commitI18n.sh
+++ tasks/commitI18n.sh
@@ -1,2 +1,2 @@
 git add -f po/template.pot
-(git commit -m 'i18n ~ build translation release' && git push origin $(git rev-parse --abbrev-ref HEAD)") || echo "[i18n] No changes to commit"
+(git commit -m 'i18n ~ build translation release' && git push origin $(git rev-parse --abbrev-ref HEAD)) || echo "[i18n] No changes to commit"
