c8454e4b0680b5dd41c81a0cf31dee0b15da5c82
dhoko <dhoko@cyaneus.org>
2017-05-02 12:23:23 +0200

    Create post link inside the config

---
+++ src/app/config.js
@@ -0,0 +1,5 @@
+angular.module('proton.config', [])
+
+.constant('CONFIG', {debug:true,apiUrl:'https://protonmail.host/api',app_version:'3.8.4',api_version:'1',date_version:'Wed Apr 19 2017',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79',year:2017, articleLink: 'https://protonmail.com/blog/protonmail-v3-8-release-notes/'})
+
+;
