e4bb5fdaf3d5c3e5c4c1f5f74a52f5768579ef5b
Richard <richard@protonmail.com>
2017-05-12 14:33:55 +0200

    Fix issues

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,5 @@
 angular.module('proton.config', [])
 
-.constant('CONFIG', {debug:true,apiUrl:'https://dev.protonmail.com/api',app_version:'3.8.7',api_version:'1',date_version:'Fri May 12 2017',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79',articleLink:'https://protonmail.com/blog/protonmail-v3-8-release-notes/',year:2017})
+.constant('CONFIG', {debug:false,apiUrl:'https://beta.protonmail.com/api',app_version:'3.8.8',api_version:'1',date_version:'Fri May 12 2017',clientID:'Angular',clientSecret:'00a11965ac0b47782ec7359c5af4dd79',year:2017})
 
 ;
