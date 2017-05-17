e2c77d22d60b2ffb5b14a20e0c4d06a7fbf3b111
Richard <richard@protonmail.com>
2017-05-17 15:45:44 +0200

    It's finally a bad idea to use the ContextNumMessages

--- src/app/message/factories/messageActions.js
+++ src/app/message/factories/messageActions.js
@@ -536,10 +536,10 @@ angular.module('proton.message')
                 const conversation = cache.getConversationCached(message.ConversationID);
 
                 if (angular.isDefined(conversation)) {
-                    if (conversation.ContextNumMessages === 1) {
+                    if (conversation.NumMessages === 1) {
                         // Delete conversation
                         events.push({ Action: 0, ID: conversation.ID });
-                    } else if (conversation.ContextNumMessages > 1) {
+                    } else if (conversation.NumMessages > 1) {
                         const messages = cache.queryMessagesCached(conversation.ID);
                         const labelIDs = _.chain(messages)
                             .filter(({ ID }) => ID !== id)
@@ -553,7 +553,7 @@ angular.module('proton.message')
                             Conversation: {
                                 ID: conversation.ID,
                                 LabelIDs: labelIDs, // Forge LabelIDs
-                                ContextNumMessages: conversation.ContextNumMessages - 1 // Decrease the number of message
+                                NumMessages: conversation.NumMessages - 1 // Decrease the number of message
                             }
                         });
                     }
