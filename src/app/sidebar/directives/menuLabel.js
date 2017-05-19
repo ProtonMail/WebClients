7a5967c753da4d7638e71e8a2ede3be826c059bb
buildog <adrien@buildog.com>
2017-05-19 16:59:19 +0200

    feat #5261 ~ sidebar display the correct unread number

--- src/app/sidebar/directives/menuLabel.js
+++ src/app/sidebar/directives/menuLabel.js
@@ -51,7 +51,11 @@ angular.module('proton.sidebar')
                 const updateCounter = () => {
                     _.each(el[0].querySelectorAll('.menuLabel-counter'), (node) => {
                         const id = node.getAttribute('data-label-id');
-                        node.textContent = sidebarModel.unread('label', id);
+                        const total = sidebarModel.unread('label', id);
+
+                        if (total) {
+                            node.textContent = `(${total})`;
+                        }
                     });
                 };
 
