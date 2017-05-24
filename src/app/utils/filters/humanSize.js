70ed3935dff26d84268a9964145a8a69495f014e
dhoko <dhoko@cyaneus.org>
2017-05-24 11:57:07 +0200

    Hotfix i18n fileSize format

--- src/app/utils/filters/humanSize.js
+++ src/app/utils/filters/humanSize.js
@@ -1,15 +1,27 @@
 angular.module('proton.utils')
-    .filter('humanSize', (CONSTANTS) => {
+    .filter('humanSize', (CONSTANTS, gettextCatalog) => {
         const units = {
             KB: CONSTANTS.BASE_SIZE,
             MB: CONSTANTS.BASE_SIZE * CONSTANTS.BASE_SIZE,
             GB: CONSTANTS.BASE_SIZE * CONSTANTS.BASE_SIZE * CONSTANTS.BASE_SIZE
         };
+
+        const i18nSuffix = (key) => {
+            const map = {
+                KB: gettextCatalog.getString('KB', null, 'fileSize format'),
+                MB: gettextCatalog.getString('MB', null, 'fileSize format'),
+                GB: gettextCatalog.getString('GB', null, 'fileSize format')
+            };
+
+            return map[key];
+        };
+
         function transformTo(bytes, unit, withoutUnit) {
             const value = (bytes / units[unit]).toFixed(2);
-            const suffix = (withoutUnit) ? '' : ` ${unit}`;
+            const suffix = (withoutUnit) ? '' : ` ${i18nSuffix(unit)}`;
             return value + suffix;
         }
+
         return (input, withoutUnit, forceUnit) => {
             let bytes;
 
