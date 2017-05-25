fc453c5ccc3dd620f72f1af3b29b23dc814a6681
dhoko <dhoko@cyaneus.org>
2017-05-25 09:36:27 +0200

    Upgrade command commit i18n

--- tasks/commitI18n.sh
+++ tasks/commitI18n.sh
@@ -1,2 +1,5 @@
 git add -f po/template.pot
 (git commit -m 'i18n ~ build translation release' && git push origin $(git rev-parse --abbrev-ref HEAD)) || echo "[i18n] No changes to commit"
+
+git add src/i18n/*.json
+(git commit -m 'i18n ~ build translation app' && git push origin $(git rev-parse --abbrev-ref HEAD)) || echo "[i18n] No changes for app to commit"
