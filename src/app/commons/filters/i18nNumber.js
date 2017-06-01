b84322e821b189e455f9e42389197404faf27010
buildog <adrien@buildog.com>
2017-06-01 13:29:13 +0200

    Fix/remove counter format (#5311)
    * remove unread format
    
    * remove unused i18nNumber filter

--- src/app/commons/filters/i18nNumber.js
+++
@@ -1,5 +0,0 @@
-angular.module('proton.commons')
-.filter('i18nNumber', () => {
-    const numberFactory = window.Intl ? new Intl.NumberFormat(document.documentElement.lang) : { format: _.identity };
-    return (input) => numberFactory.format(input);
-});
