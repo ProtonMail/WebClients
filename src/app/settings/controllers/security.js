895359dc872873b6b8aed3742974ff61c8d98b62
Richard <richard@protonmail.com>
2017-06-15 22:02:26 +0200

    Lint

--- src/app/settings/controllers/security.js
+++ src/app/settings/controllers/security.js
@@ -256,7 +256,7 @@ angular.module('proton.settings')
 
         const promise = settingsApi.setLogging({ LogAuth: value })
             .then(() => {
-                $scope.doLogging = value
+                $scope.doLogging = value;
                 notify({
                     message: gettextCatalog.getString('Logging preference updated', null, 'Dashboard/security'),
                     classes: 'notification-success'
