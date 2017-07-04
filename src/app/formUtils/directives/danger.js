a93e7eeea0dc677f0c9993e28d2c4d3a1039e750
buildog <adrien@buildog.com>
2017-07-04 09:46:08 +0200

    fix #5432 ~ DANGER translation (#5440)

--- src/app/formUtils/directives/danger.js
+++ src/app/formUtils/directives/danger.js
@@ -1,13 +1,21 @@
 angular.module('proton.formUtils')
-.directive('danger', () => {
+.directive('danger', (gettextCatalog) => {
+    const dangerWord = gettextCatalog.getString('DANGER');
+    const placeholder = gettextCatalog.getString("Enter the word 'DANGER' here.");
     function isDanger(value) {
-        return value === 'DANGER';
+        return value === dangerWord;
     }
     return {
         require: 'ngModel',
         restrict: 'A',
+        scope: {
+            dangerWord: '='
+        },
         link(scope, element, attributes, ngModel) {
+            scope.dangerWord = dangerWord;
+            element.attr('placeholder', placeholder);
             ngModel.$validators.danger = isDanger;
+
         }
     };
 });
