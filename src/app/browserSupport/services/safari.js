62477a6981c3f95dcf54cbfb97dac9582e52cb8a
KAYLukas <kay.lukas@gmail.com>
2017-07-04 09:58:46 +0200

    Fixed issue #5433 (#5437)
    * Fixed issue #5433
    
     * After downloading an attachment using safari all target=_blank links
    where downloaded instead of opened in an attachment
     * This is an internal safari bug, but the workaround is to open the
    webpage using javascript.
    
    * Don't LowerCase

---
+++ src/app/browserSupport/services/safari.js
@@ -0,0 +1,23 @@
+angular.module('proton.browserSupport')
+    .factory('safari', (aboutClient) => {
+
+        const fixes = [
+            // Issue #5433: Download instead of new tab when clicking on links in emails
+            () => {
+                $(document).on('click.fixSafariDownloadBug', (e) => {
+                    const target = e.target;
+                    if (target &&
+                        target.tagName === 'A' &&
+                        target.getAttribute('target') === '_blank' &&
+                        target.hasAttribute('href')) {
+                        e.preventDefault();
+                        window.open(target.href);
+                    }
+                });
+            }
+        ];
+
+        const applyFixes = () => _.each(fixes, (fix) => fix());
+        
+        return { isCurrentBrowser: aboutClient.isSafari, applyFixes };
+    });
