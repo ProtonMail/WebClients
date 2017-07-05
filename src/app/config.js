9c961fe865f1df1db6b80acdbc5d532cc41f8cec
Richard <richard@protonmail.com>
2017-07-05 15:47:11 +0200

    Update blog post

--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,5 @@
 angular.module('proton.config', [])
 
-.constant('CONFIG', {debug:true,apiUrl:'https://dev.protonmail.com/api',app_version:'3.8.18',api_version:'1',date_version:'Thu Jun 29 2017',clientID:'Web',clientSecret:'4957cc9a2e0a2a49d02475c9d013478d',articleLink:'https://protonmail.com/blog/protonmail-v3-8-release-notes/',year:2017,translations:['de_DE','en_US','es_ES','fr_FR','ru_RU','tr_TR']})
+.constant('CONFIG', {app_version:'3.9.0',api_version:'1',date_version:'Wed Jul 05 2017',year:2017,clientID:'Web',clientSecret:'4957cc9a2e0a2a49d02475c9d013478d',articleLink:'https://protonmail.com/blog/protonmail-v3-9-release-notes/',translations:['de_DE','en_US','es_ES','fr_FR','ru_RU','tr_TR'],debug:true,apiUrl:'https://dev.protonmail.com/api'})
 
 ;
