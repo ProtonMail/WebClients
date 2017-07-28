d84c928e58e45d37f6d454650fbd46135945e4d3
Richard <richard@protonmail.com>
2017-07-28 09:37:50 +0200

    Fix #5525 - autoscroll to plans

--- src/app/settings/controllers/dashboard.js
+++ src/app/settings/controllers/dashboard.js
@@ -1,4 +1,8 @@
 angular.module('proton.settings')
-.controller('DashboardController', () => {
-    console.log('DashboardController loaded');
+.controller('DashboardController', ($stateParams) => {
+    const scrollToPlans = () => $('.settings').animate({ scrollTop: $('#plans').offset().top }, 1000);
+
+    if ($stateParams.scroll === true) {
+        scrollToPlans();
+    }
 });
