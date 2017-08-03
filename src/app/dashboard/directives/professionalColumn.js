a1a9881a918183362f609356a03da39fc5ce00a0
Richard <richard@protonmail.com>
2017-08-03 22:00:58 +0200

    Fix #5577 - Dashboard changes around the professional plan

--- src/app/dashboard/directives/professionalColumn.js
+++ src/app/dashboard/directives/professionalColumn.js
@@ -1,27 +1,12 @@
 angular.module('proton.dashboard')
-    .directive('professionalColumn', ($rootScope, CONSTANTS, customProPlanModal, dashboardConfiguration, dashboardModel, gettextCatalog, subscriptionModel) => {
-        const PROFESSIONAL = CONSTANTS.PLANS.PLAN.PROFESSIONAL;
-        const MEMBER = CONSTANTS.PLANS.ADDON.MEMBER;
-        const BASE = CONSTANTS.BASE_SIZE;
-
+    .directive('professionalColumn', (customProPlanModal) => {
         return {
             restrict: 'E',
             replace: true,
             scope: {},
             templateUrl: 'templates/dashboard/professionalColumn.tpl.html',
             link(scope, element) {
-                const number = subscriptionModel.count('member');
                 const $buttons = element.find('.professionalColumn-customize');
-                const $space = element.find('.professionalColumn-space-value');
-                const $address = element.find('.professionalColumn-address-value');
-                const { addons, plan } = dashboardModel.get(dashboardConfiguration.cycle());
-                const professional = plan[PROFESSIONAL];
-                const member = addons[MEMBER];
-                const unsubscribe = $rootScope.$on('dashboard', (event, { type, data = {} }) => {
-                    if (type === 'addon.updated' && data.addon === 'member') {
-                        update(data.value);
-                    }
-                });
 
                 $buttons.on('click', onClick);
 
@@ -35,15 +20,7 @@ angular.module('proton.dashboard')
                     });
                 }
 
-                function update(n) {
-                    $space.text(gettextCatalog.getPlural((n * member.MaxSpace + professional.MaxSpace) / (BASE * BASE * BASE), '1 GB Storage', '{{$count}} GB Storage', {}));
-                    $address.text(gettextCatalog.getString('{{value}} Addresses', { value: n * member.MaxAddresses + professional.MaxAddresses }));
-                }
-
-                update(number);
-
                 scope.$on('$destroy', () => {
-                    unsubscribe();
                     $buttons.off('click', onClick);
                 });
             }
