f3b78e1ba45fe8122f8acef4c53da72c00f4759c
Kay Lukas <kay.lukas@gmail.com>
2017-08-07 17:40:08 +0200

    ESLint

--- src/app/utils/factories/dateUtils.js
+++ src/app/utils/factories/dateUtils.js
@@ -18,63 +18,63 @@ angular.module('proton.utils')
                 .replace('DD', date);
 
             I18N.days = [
-                    {
-                        narrowLabel: gettextCatalog.getString('M', null, 'Single letter code Monday'),
-                        shortLabel: gettextCatalog.getString('Mon', null, 'Three letter code Monday'),
-                        longLabel: gettextCatalog.getString('Monday', null, 'Full day name'),
-                        value: 1
-                    },
-                    {
-                        narrowLabel: gettextCatalog.getString('T', null, 'Single letter code Tuesday'),
-                        shortLabel: gettextCatalog.getString('Tue', null, 'Three letter code Tuesday'),
-                        longLabel: gettextCatalog.getString('Tuesday', null, 'Full day name'),
-                        value: 2
-                    },
-                    {
-                        narrowLabel: gettextCatalog.getString('W', null, 'Single letter code Wednesday'),
-                        shortLabel: gettextCatalog.getString('Wed', null, 'Three letter code Wednesday'),
-                        longLabel: gettextCatalog.getString('Wednesday', null, 'Full day name'),
-                        value: 3
-                    },
-                    {
-                        narrowLabel: gettextCatalog.getString('T', null, 'Single letter code Thursday'),
-                        shortLabel: gettextCatalog.getString('Thu', null, 'Three letter code Thursday'),
-                        longLabel: gettextCatalog.getString('Thursday', null, 'Full day name'),
-                        value: 4
-                    },
-                    {
-                        narrowLabel: gettextCatalog.getString('F', null, 'Single letter code Friday'),
-                        shortLabel: gettextCatalog.getString('Fri', null, 'Three letter code Friday'),
-                        longLabel: gettextCatalog.getString('Friday', null, 'Full day name'),
-                        value: 5
-                    },
-                    {
-                        narrowLabel: gettextCatalog.getString('S', null, 'Single letter code Saturday'),
-                        shortLabel: gettextCatalog.getString('Sat', null, 'Three letter code Saturday'),
-                        longLabel: gettextCatalog.getString('Saturday', null, 'Full day name'),
-                        value: 6
-                    },
-                    {
-                        narrowLabel: gettextCatalog.getString('S', null, 'Single letter code Sunday'),
-                        shortLabel: gettextCatalog.getString('Sun', null, 'Three letter code Sunday'),
-                        longLabel: gettextCatalog.getString('Sunday', null, 'Full day name'),
-                        value: 0
-                    }
-                ];
+                {
+                    narrowLabel: gettextCatalog.getString('M', null, 'Single letter code Monday'),
+                    shortLabel: gettextCatalog.getString('Mon', null, 'Three letter code Monday'),
+                    longLabel: gettextCatalog.getString('Monday', null, 'Full day name'),
+                    value: 1
+                },
+                {
+                    narrowLabel: gettextCatalog.getString('T', null, 'Single letter code Tuesday'),
+                    shortLabel: gettextCatalog.getString('Tue', null, 'Three letter code Tuesday'),
+                    longLabel: gettextCatalog.getString('Tuesday', null, 'Full day name'),
+                    value: 2
+                },
+                {
+                    narrowLabel: gettextCatalog.getString('W', null, 'Single letter code Wednesday'),
+                    shortLabel: gettextCatalog.getString('Wed', null, 'Three letter code Wednesday'),
+                    longLabel: gettextCatalog.getString('Wednesday', null, 'Full day name'),
+                    value: 3
+                },
+                {
+                    narrowLabel: gettextCatalog.getString('T', null, 'Single letter code Thursday'),
+                    shortLabel: gettextCatalog.getString('Thu', null, 'Three letter code Thursday'),
+                    longLabel: gettextCatalog.getString('Thursday', null, 'Full day name'),
+                    value: 4
+                },
+                {
+                    narrowLabel: gettextCatalog.getString('F', null, 'Single letter code Friday'),
+                    shortLabel: gettextCatalog.getString('Fri', null, 'Three letter code Friday'),
+                    longLabel: gettextCatalog.getString('Friday', null, 'Full day name'),
+                    value: 5
+                },
+                {
+                    narrowLabel: gettextCatalog.getString('S', null, 'Single letter code Saturday'),
+                    shortLabel: gettextCatalog.getString('Sat', null, 'Three letter code Saturday'),
+                    longLabel: gettextCatalog.getString('Saturday', null, 'Full day name'),
+                    value: 6
+                },
+                {
+                    narrowLabel: gettextCatalog.getString('S', null, 'Single letter code Sunday'),
+                    shortLabel: gettextCatalog.getString('Sun', null, 'Three letter code Sunday'),
+                    longLabel: gettextCatalog.getString('Sunday', null, 'Full day name'),
+                    value: 0
+                }
+            ];
             I18N.months = [
-                    gettextCatalog.getString('January', null, 'Pikaday'),
-                    gettextCatalog.getString('February', null, 'Pikaday'),
-                    gettextCatalog.getString('March', null, 'Pikaday'),
-                    gettextCatalog.getString('April', null, 'Pikaday'),
-                    gettextCatalog.getString('May', null, 'Pikaday'),
-                    gettextCatalog.getString('June', null, 'Pikaday'),
-                    gettextCatalog.getString('July', null, 'Pikaday'),
-                    gettextCatalog.getString('August', null, 'Pikaday'),
-                    gettextCatalog.getString('September', null, 'Pikaday'),
-                    gettextCatalog.getString('October', null, 'Pikaday'),
-                    gettextCatalog.getString('November', null, 'Pikaday'),
-                    gettextCatalog.getString('December', null, 'Pikaday')
-                ];
+                gettextCatalog.getString('January', null, 'Pikaday'),
+                gettextCatalog.getString('February', null, 'Pikaday'),
+                gettextCatalog.getString('March', null, 'Pikaday'),
+                gettextCatalog.getString('April', null, 'Pikaday'),
+                gettextCatalog.getString('May', null, 'Pikaday'),
+                gettextCatalog.getString('June', null, 'Pikaday'),
+                gettextCatalog.getString('July', null, 'Pikaday'),
+                gettextCatalog.getString('August', null, 'Pikaday'),
+                gettextCatalog.getString('September', null, 'Pikaday'),
+                gettextCatalog.getString('October', null, 'Pikaday'),
+                gettextCatalog.getString('November', null, 'Pikaday'),
+                gettextCatalog.getString('December', null, 'Pikaday')
+            ];
             I18N.localizedDatePlaceholder = localDatePlaceholder;
         }
 
