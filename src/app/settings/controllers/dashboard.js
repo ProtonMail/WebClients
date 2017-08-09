166691f056c501e0952705e6dab8b40772d5eb96
Richard <richard@protonmail.com>
2017-08-09 09:37:51 +0200

    Hotfix - Donate section was never displayed

--- src/app/settings/controllers/dashboard.js
+++ src/app/settings/controllers/dashboard.js
@@ -1,8 +1,18 @@
 angular.module('proton.settings')
-    .controller('DashboardController', ($stateParams) => {
+    .controller('DashboardController', ($rootScope, $scope, $stateParams, subscriptionModel) => {
         const scrollToPlans = () => $('.settings').animate({ scrollTop: $('#plans').offset().top }, 1000);
+        const updateSubscription = () => $scope.hasPaidMail = subscriptionModel.hasPaid('mail');
+        const unsubscribe = $rootScope.$on('subscription', (event, { type }) => {
+            (type === 'update') && $scope.$applyAsync(() => updateSubscription());
+        });
 
         if ($stateParams.scroll === true) {
             scrollToPlans();
         }
+
+        updateSubscription();
+
+        $scope.$on('$destroy', () => {
+            unsubscribe();
+        });
     });
