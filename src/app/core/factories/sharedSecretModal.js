f4c233dd39548dafb96099a2e19df94289c49d24
Richard <richard@protonmail.com>
2017-08-15 17:26:55 +0200

    Review QR Code modal

--- src/app/core/factories/sharedSecretModal.js
+++ src/app/core/factories/sharedSecretModal.js
@@ -8,8 +8,8 @@ angular.module('proton.core')
                 const randomBytes = webcrypto.getRandomValues(new Uint8Array(20));
                 const sharedSecret = base32.encode(randomBytes);
                 const primaryAddress = _.find(authentication.user.Addresses, ({ Keys }) => Keys);
-                const identifier = (primaryAddress) ? primaryAddress.Email : authentication.user.Name + '@protonmail';
-                const qrURI = 'otpauth://totp/' + identifier + '?secret=' + sharedSecret + '&issuer=ProtonMail&algorithm=SHA1&digits=6&period=30';
+                const identifier = (primaryAddress) ? primaryAddress.Email : `${authentication.user.Name}@protonmail`;
+                const qrURI = `otpauth://totp/${identifier}?secret=${sharedSecret}&issuer=ProtonMail&algorithm=SHA1&digits=6&period=30`;
 
                 self.sharedSecret = params.sharedSecret || sharedSecret;
                 self.qrURI = params.qrURI || qrURI;
@@ -21,10 +21,8 @@ angular.module('proton.core')
                 };
 
                 self.makeCode = () => {
-                    $timeout(() => {
                     /* eslint no-new: "off" */
-                        new QRCode(document.getElementById('qrcode'), self.qrURI);
-                    });
+                    new QRCode(document.getElementById('qrcode'), self.qrURI);
                 };
             }
         });
