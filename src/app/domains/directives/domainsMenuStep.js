27bdde2446f19fbf14892c41f9b480f17da5deb2
dhoko <dhoko@cyaneus.org>
2017-08-23 12:21:06 +0200

    Remove ngInclude and fix names

---
+++ src/app/domains/directives/domainsMenuStep.js
@@ -0,0 +1,5 @@
+angular.module('proton.domains')
+    .directive('domainsMenuStep', () => ({
+        replace: true,
+        templateUrl: 'templates/domains/domainsMenuStep.tpl.html'
+    }));
--- src/app/templates/message/headerSecured.js
+++ src/app/templates/message/headerSecured.js
@@ -2,6 +2,7 @@ angular.module('proton.ui')
     .directive('headerSecured', () => {
         return {
             restrict: 'E',
+            replace: true,
             templateUrl: 'templates/ui/header/headerSecured.tpl.html'
         };
     });
