12d19f3ca16035b6cc7ac62b791ef9174580a8f7
Richard <richard@protonmail.com>
2017-08-31 14:39:26 +0200

    Fix #5603 - disable to go back to inbox when the user is change the view mode

--- src/app/ui/directives/settingsMenu.js
+++ src/app/ui/directives/settingsMenu.js
@@ -1,31 +1,39 @@
 angular.module('proton.ui')
     .directive('settingsMenu', (authentication, CONSTANTS, networkActivityTracker, $rootScope, sidebarSettingsModel, AppModel) => {
-        const CLASS_SUBUSER = 'settingsMenu-is-subuser';
-        const CLASS_MEMBER = 'settingsMenu-is-member';
+        const IS_SUBUSER = 'settingsMenu-is-subuser';
+        const IS_MEMBER = 'settingsMenu-is-member';
+        const BACK_BUTTON = 'sidebar-btn-back';
 
         return {
             replace: true,
             scope: {},
             templateUrl: 'templates/directives/ui/settingsMenu.tpl.html',
-            link(scope, el) {
-                const unsubscribes = [];
+            link(scope, element) {
+                const unsubscribe = [];
                 const isMember = () => authentication.user.Role === CONSTANTS.PAID_MEMBER_ROLE;
+                const $back = element.find(`.${BACK_BUTTON}`);
+
                 scope.listStates = Object.keys(sidebarSettingsModel.getStateConfig());
 
-                authentication.user.subuser && el[0].classList.add(CLASS_SUBUSER);
-                isMember() && el[0].classList.add(CLASS_MEMBER);
+                authentication.user.subuser && element.addClass(IS_SUBUSER);
+                isMember() && element.addClass(IS_MEMBER);
 
-                unsubscribes.push($rootScope.$on('updateUser', () => {
-                    isMember() && el[0].classList.add(CLASS_MEMBER);
+                unsubscribe.push($rootScope.$on('updateUser', () => {
+                    isMember() && element.addClass(IS_MEMBER);
                 }));
 
-                unsubscribes.push($rootScope.$on('$stateChangeStart', () => {
+                unsubscribe.push($rootScope.$on('$stateChangeStart', () => {
                     AppModel.set('showSidebar', false);
                 }));
 
+                unsubscribe.push($rootScope.$on('appearance', (event, { type }) => {
+                    (type === 'changingViewMode') && $back.prop('disabled', true);
+                    (type === 'viewModeChanged') && $back.prop('disabled', false);
+                }));
+
                 scope.$on('$destroy', () => {
-                    unsubscribes.forEach((cb) => cb());
-                    unsubscribes.length = 0;
+                    unsubscribe.forEach((cb) => cb());
+                    unsubscribe.length = 0;
                 });
             }
         };
