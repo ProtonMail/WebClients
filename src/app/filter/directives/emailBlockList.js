07ba79d44752383bfa9a834376e91b4e92e0af85
Kay Lukas <kay.lukas@gmail.com>
2017-09-07 11:05:42 +0200

    use jquery delegates

--- src/app/filter/directives/emailBlockList.js
+++ src/app/filter/directives/emailBlockList.js
@@ -6,9 +6,9 @@ angular.module('proton.filter')
         // fetch when less than TRIGGER_BOUNDARY entries are below the bottom of the table view
         const TRIGGER_BOUNDARY = 50;
 
-        const onEvent = (element, type, callback) => {
-            element.addEventListener(type, callback);
-            return () => element.removeEventListener(type, callback);
+        const onEvent = (element, type, delegate, handler) => {
+            element.on(type, delegate, handler);
+            return () => element.off(type, delegate, handler);
         };
 
         return {
@@ -30,10 +30,27 @@ angular.module('proton.filter')
 
                     const list = spamListModel.getList(listType);
                     const switchList = spamListModel.getList(switchTo);
-                    const tbody = elem[0].querySelector('.block-list > table > tbody');
+                    // we need the jquery for delegates
+                    const tbody = elem.find('.block-list > table > tbody');
 
                     // we need to use the scope here because of ng-repeat.
                     scope.entries = list.getEntries();
+
+                    const onScroll = _.throttle(() => {
+                        if (!list.hasMoreData() || list.isFetchingData()) {
+                            return;
+                        }
+
+                        const elementCount = scope.entries.length;
+                        const triggerFetch = elementCount - TRIGGER_BOUNDARY;
+                        const scrollBottom = tbody.scrollTop() + tbody.innerHeight();
+
+                        // check if we have reached the last TRIGGER_BOUNDARY elements
+                        if (scrollBottom / tbody[0].scrollHeight > triggerFetch / elementCount) {
+                            list.fetchMoreData();
+                        }
+
+                    }, SCROLL_THROTTLE);
 
                     unsubscribe.push($rootScope.$on('filter', (event, { type, data = {} }) => {
                         if (type === 'spamlist.update' && listType === data.name) {
@@ -49,48 +66,19 @@ angular.module('proton.filter')
                             scope.$applyAsync(() => {
                                 scope.entries = data.entries;
 
-                                $('.tooltip').not(this).hide();
+                                $('.tooltip').hide();
                             });
                         }
                     }));
 
+                    const getId = (subElement) => $(subElement).closest('tr').data('entryId');
 
-                    const onScroll = _.throttle(() => {
-                        if (!list.hasMoreData() || list.isFetchingData()) {
-                            return;
-                        }
+                    const onSwitch = ({ target }) => switchList.adopt(getId(target));
+                    const onDelete = ({ target }) => spamListModel.deleteEntry(getId(target));
 
-                        const elementCount = scope.entries.length;
-                        const triggerFetch = elementCount - TRIGGER_BOUNDARY;
-                        const scrollBottom = tbody.scrollTop + tbody.clientHeight;
-
-                        // check if we have reached the last TRIGGER_BOUNDARY elements
-                        if (scrollBottom / tbody.scrollHeight > triggerFetch / elementCount) {
-                            list.fetchMoreData();
-                        }
-
-                    }, SCROLL_THROTTLE);
-
-                    unsubscribe.push(onEvent(tbody, 'scroll', onScroll));
-
-                    const onClick = (event) => {
-
-                        // we calculate the click here: this prevents registering 1000+ click handlers
-                        const target = $(event.target);
-                        const button = target.closest('button');
-                        const row = target.closest('tr');
-
-                        if (button.hasClass('blocklist-email-switch')) {
-                            switchList.adopt(row.data('entryId'));
-                        }
-
-                        if (button.hasClass('blocklist-email-delete')) {
-                            spamListModel.deleteEntry(row.data('entryId'));
-                        }
-                    };
-
-                    unsubscribe.push(onEvent(tbody, 'click', onClick));
-
+                    unsubscribe.push(onEvent(tbody, 'scroll', null, onScroll));
+                    unsubscribe.push(onEvent(tbody, 'click', '.blocklist-email-switch', onSwitch));
+                    unsubscribe.push(onEvent(tbody, 'click', '.blocklist-email-delete', onDelete));
 
                     scope.$on('$destroy', () => {
                         _.each(unsubscribe, (cb) => cb());
