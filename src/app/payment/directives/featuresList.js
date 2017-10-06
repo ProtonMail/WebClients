4413ad9a70e22aab6137ba178c2f074e88361e34
Richard <EpokK@users.noreply.github.com>
2017-10-06 11:40:15 +0200

    Fix #5601 - Features list (final step of the subscription process) (#5763)
    * Add features icons
    
    * Add featuresList component
    
    * Use the featuresList component in the payment modal
    
    * Fix click event
    
    * Update text for the VPN
    
    * Add `!` in the title
    
    * Update vpn logo
    
    * review style

---
+++ src/app/payment/directives/featuresList.js
@@ -0,0 +1,39 @@
+angular.module('proton.payment')
+    .directive('featuresList', ($state, paymentModal) => {
+        const IS_PLUS = 'featuresList-isPlus';
+        const IS_PROFESSIONAL = 'featuresList-isProfessional';
+        const IS_VISIONARY = 'featuresList-isVisionary';
+        const HAS_VPN = 'featuresList-hasVPN';
+
+        function onClick(event) {
+            const target = event.target;
+
+            if (target.nodeName === 'BUTTON') {
+                const route = target.getAttribute('data-route');
+
+                paymentModal.deactivate();
+                $state.go(route);
+            }
+        }
+
+        return {
+            restrict: 'E',
+            scope: { ctrl: '=' },
+            replace: true,
+            templateUrl: 'templates/payment/featuresList.tpl.html',
+            link(scope, element) {
+                const MAP = scope.ctrl.plans.reduce((acc, plan) => {
+                    acc[plan.Name] = plan.Name;
+                    return acc;
+                }, {});
+
+                element.on('click', onClick);
+                MAP.plus && element.addClass(IS_PLUS);
+                MAP.professional && element.addClass(IS_PROFESSIONAL);
+                MAP.visionary && element.addClass(IS_VISIONARY);
+                (MAP.vpnbasic || MAP.vpnplus) && element.addClass(HAS_VPN);
+
+                scope.$on('$destroy', () => element.off('click', onClick));
+            }
+        };
+    });
