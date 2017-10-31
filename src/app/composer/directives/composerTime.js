df0c0669862afc9b0cdfd1d084b2f0e10086e54b
Richard <richard@protonmail.com>
2017-10-31 13:32:17 +0100

    Add the saving state in the composer's footer

--- src/app/composer/directives/composerTime.js
+++ src/app/composer/directives/composerTime.js
@@ -1,7 +1,10 @@
 angular.module('proton.composer')
     .directive('composerTime', ($rootScope, gettextCatalog) => {
         const getTime = ({ Time }) => moment.unix(Time).format('LT');
-        const saveAt = gettextCatalog.getString('Saved at', null, 'Info display in the composer footer');
+        const I18N = {
+            saveAt: gettextCatalog.getString('Saved at', null, 'Info display in the composer footer'),
+            saving: gettextCatalog.getString('Saving', null, 'Info display in the composer footer')
+        };
 
         return {
             restrict: 'E',
@@ -9,8 +12,13 @@ angular.module('proton.composer')
             template: '<time class="composerTime-container"></time>',
             link(scope, element) {
                 const update = (message) => {
+                    if (message.saving) {
+                        element[0].textContent = I18N.saving;
+                        return;
+                    }
+
                     if (message.Time) {
-                        element[0].textContent = `${saveAt} ${getTime(message)}`;
+                        element[0].textContent = `${I18N.saveAt} ${getTime(message)}`;
                     }
                 };
                 const unsubscribe = $rootScope.$on('actionMessage', (event, message) => {
