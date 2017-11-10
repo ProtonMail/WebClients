a2fea2b08946e4f5b82ad2161f1ca65de56090dc
dhoko <aurelien@protonmail.com>
2017-11-10 18:31:10 +0100

    Remove build step of the lib vcard

--- tasks/privateDependencies.sh
+++ tasks/privateDependencies.sh
@@ -1,15 +1,7 @@
 DEST_MAILPARSER='node_modules/pt.mailparser'
-DEST_VCARD='node_modules/vcard'
 
 # Remove previous version
-rm -rf $DEST_MAILPARSER $DEST_VCARD
-
+rm -rf $DEST_MAILPARSER
 mkdir $DEST_MAILPARSER
 cp src/libraries/mailparser.js $DEST_MAILPARSER/mailparser.js
-
-mkdir $DEST_VCARD
-npx browserify node_modules/vcf/lib/vcard.js --standalone vCard -o node_modules/vcard/vcard.js
-
-echo
 echo "--- custom dependencies installed ---"
-echo
