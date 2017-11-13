aaf7d1fdb3cb33e2ff4668758ef4d30634048661
Richard <richard@protonmail.com>
2017-11-13 14:42:26 +0100

    #5961 - Display error when adding bad contact data

--- src/app/contact/modals/contactModal.js
+++ src/app/contact/modals/contactModal.js
@@ -1,5 +1,9 @@
 angular.module('proton.contact')
     .factory('contactModal', ($rootScope, $state, gettextCatalog, notification, pmModal) => {
+        const I18N = {
+            contactAdded: gettextCatalog.getString('Contact added', null, 'Success message for the contact modal'),
+            contactError: gettextCatalog.getString('Error with the request', null, 'Default error for the contact modal')
+        };
         return pmModal({
             controllerAs: 'ctrl',
             templateUrl: 'templates/contact/contactModal.tpl.html',
@@ -7,13 +11,16 @@ angular.module('proton.contact')
                 const self = this;
                 const unsubscribe = $rootScope.$on('contacts', (event, { type = '', data = {} }) => {
                     if (type === 'contactCreated') {
-                        const { created = [] } = data;
+                        const { created = [], errors = [] } = data;
 
-                        notification.success(gettextCatalog.getString('Contact added', null));
-                        params.close();
+                        if (errors.length) {
+                            errors.forEach(({ error = I18N.contactError }) => notification.error(error));
+                        }
 
                         if (created.length) {
+                            notification.success(I18N.contactAdded);
                             $state.go('secured.contacts.details', { id: data.created[0].ID });
+                            params.close();
                         }
                     }
                 });
