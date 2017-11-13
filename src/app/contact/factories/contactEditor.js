84a6fe2a80d956dd735803e5e80d024609eba9c4
Richard <richard@protonmail.com>
2017-11-13 14:54:09 +0100

    Call the eventManager before to resolve the promise

--- src/app/contact/factories/contactEditor.js
+++ src/app/contact/factories/contactEditor.js
@@ -9,10 +9,11 @@ angular.module('proton.contact')
             let promiseCompleted = false;
             const promise = Contact.add(contacts)
                 .then(({ created, total, errors }) => {
-                    promiseCompleted = true;
-                    $rootScope.$emit('contacts', { type: 'contactCreated', data: { created, total, errors, mode } });
-
-                    return eventManager.call();
+                    return eventManager.call()
+                        .then(() => {
+                            promiseCompleted = true;
+                            $rootScope.$emit('contacts', { type: 'contactCreated', data: { created, total, errors, mode } });
+                        });
                 });
 
             // If the promise take too much time, we display a modal to inform the user
