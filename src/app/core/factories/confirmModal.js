dde8849b10eeba08bd1eda60c410ac7deae1e2ea
Richard <richard@protonmail.com>
2017-11-15 09:15:09 +0100

    Add customization for buttons in the confirm modal

--- src/app/core/factories/confirmModal.js
+++ src/app/core/factories/confirmModal.js
@@ -1,5 +1,9 @@
 angular.module('proton.core')
-    .factory('confirmModal', (pmModal) => {
+    .factory('confirmModal', (pmModal, gettextCatalog) => {
+        const I18N = {
+            confirm: gettextCatalog.getString('Confirm', null, 'Default text for the confirm button in the confirm modal'),
+            cancel: gettextCatalog.getString('Cancel', null, 'Default text for the cancel button in the confirm modal')
+        };
         return pmModal({
             controllerAs: 'ctrl',
             templateUrl: 'templates/modals/confirm.tpl.html',
@@ -9,6 +13,8 @@ angular.module('proton.core')
                 hotkeys.unbind(['enter']);
                 this.title = params.title;
                 this.message = params.message;
+                this.confirmText = params.confirmText || I18N.confirm;
+                this.cancelText = params.cancelText || I18N.cancel;
                 this.confirm = () => (hotkeys.bind(['enter']), params.confirm());
                 this.cancel = () => (hotkeys.bind(['enter']), params.cancel());
 
