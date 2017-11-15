08a99a3052563ac8d777f63ea4525cee917e915a
Richard <richard@protonmail.com>
2017-11-15 14:30:30 +0100

    Review contactDownloader

--- src/app/contact/factories/contactDownloader.js
+++ src/app/contact/factories/contactDownloader.js
@@ -1,18 +1,27 @@
 angular.module('proton.contact')
-    .factory('contactDownloader', (CONSTANTS, Contact, contactLoaderModal, downloadFile, vcard) => {
+    .factory('contactDownloader', (CONSTANTS, Contact, contactLoaderModal, contactDetailsModel, downloadFile, vcard) => {
+        const getFileName = (id, [card]) => {
+            const nameProperty = card.get('fn');
+
+            if (id !== 'all' && nameProperty) {
+                return `${contactDetailsModel.unescapeValue(nameProperty.valueOf())}.vcf`;
+            }
+
+            return 'proton.vcf';
+        };
+
         return (id = 'all') => {
             const promise = (id !== 'all') ? Contact.get(id) : Contact.exportAll();
 
             contactLoaderModal.activate({ params: { mode: 'export', close() { contactLoaderModal.deactivate(); } } });
 
-            promise.then((contacts) => {
-                contactLoaderModal.deactivate();
-
-                const FILE_NAME = (id !== 'all' && contacts.vCard.data.fn) ? contacts.vCard.data.fn.valueOf() + '.vcf' : 'proton.vcf';
-                const vCards = (id === 'all') ? contacts.map(({ vCard }) => vCard) : [contacts.vCard];
+            promise.then((result) => {
+                const vCards = (id === 'all') ? result.map(({ vCard }) => vCard) : [result.vCard];
+                const fileName = getFileName(id, vCards);
                 const blob = new Blob([vcard.to(vCards)], { type: 'data:attachment/vcard;' });
 
-                downloadFile(blob, FILE_NAME);
+                contactLoaderModal.deactivate();
+                downloadFile(blob, fileName);
             }, () => {
                 contactLoaderModal.deactivate();
             });
