afa53cdeb08780aff9d7da045d4e5c03b255283c
Richard <EpokK@users.noreply.github.com>
2017-11-30 17:55:48 +0100

    WIP - Fix #6051 - Display discount in the payment modal (#6069)
    * Fix #6051 - Display discount in the payment modal
    
    * Display different discounts when the VPN bundle is applied
    
    * Apply changes asked by Andy

--- src/app/payment/directives/tosubscribeOverview.js
+++ src/app/payment/directives/tosubscribeOverview.js
@@ -1,7 +1,7 @@
 angular.module('proton.payment')
     .directive('tosubscribeOverview', () => {
-
         return {
+            restrict: 'E',
             replace: true,
             templateUrl: 'templates/payment/tosubscribeOverview.tpl.html'
         };
