f5b920d08e10cccfbbb11d7b7d9fa983805a7946
Richard <richard@protonmail.com>
2017-12-01 10:09:33 +0100

    Check gift code value during the signup

--- src/app/payment/directives/giftCodeValidator.js
+++ src/app/payment/directives/giftCodeValidator.js
@@ -1,18 +1,10 @@
 angular.module('proton.payment')
-    .directive('giftCodeValidator', (CONSTANTS) => {
-        const { GIFT_CODE_LENGTH } = CONSTANTS;
-        const IS_VALID = /[a-zA-Z0-9]+/g; // Alpha-Numeric
-        const CLEANER = /-|\s|\t/g; // Remove space, tab and hyphen
-        const cleanCode = (input = '') => input.replace(CLEANER, '');
-
+    .directive('giftCodeValidator', (giftCodeModel) => {
         return {
             restrict: 'A',
             require: 'ngModel',
             link(scope, element, attr, ngModel) {
-                ngModel.$validators.giftCode = (input = '') => {
-                    const code = cleanCode(input);
-                    return code.length === GIFT_CODE_LENGTH && IS_VALID.test(code);
-                };
+                ngModel.$validators.giftCode = (input = '') => giftCodeModel.isValid(input);
             }
         };
     });
