406d71eecb8a07d667299e106af7bb3ee5b64eaf
dhoko <aurelien@protonmail.com>
2017-12-06 18:31:55 +0100

    Fix vendor checksum with MacOS

--- webpack.tasks/vendorCheckSum.sh
+++ webpack.tasks/vendorCheckSum.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-HASH_VENDOR=$(shasum dist/styles.css | awk '{print  $1}');
+HASH_STYLE=$(shasum dist/styles.css | awk '{print  $1}');
 HASH_VENDOR=$(shasum dist/vendor.js | awk '{print  $1}');
 HASH_OPENPGP=$(shasum dist/openpgp.min.js | awk '{print  $1}');
 
-mv dist/vendor.js dist/vendor.$HASH_VENDOR.js
-mv dist/styles.css dist/styles.$HASH_VENDOR.css
-sed -i "s/styles.css/styles.$HASH_VENDOR.css/g" dist/index.html
-sed -i "s/vendor.js/vendor.$HASH_VENDOR.js/g" dist/index.html
-sed -i "s/openpgp.min.js/openpgp.min.js?rel=$HASH_OPENPGP/g" dist/index.html
+CONTENT=$(sed -e "s/styles.css/styles.$HASH_STYLE.css/g;s/vendor.js/vendor.$HASH_VENDOR.js/g;s/openpgp.min.js/openpgp.min.js?rel=$HASH_OPENPGP/g" dist/index.html)
 
+echo HASH_STYLE $HASH_STYLE
+echo HASH_VENDOR $HASH_VENDOR
+echo HASH_OPENPGP $HASH_OPENPGP
+
+mv dist/vendor.js dist/vendor.$HASH_VENDOR.js
+mv dist/styles.css dist/styles.$HASH_STYLE.css
+
+echo $CONTENT > dist/index.html
