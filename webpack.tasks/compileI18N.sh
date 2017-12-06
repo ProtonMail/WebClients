aa62d8ecbfae665975f64ceb1466a51c77755e61
dhoko <aurelien@protonmail.com>
2017-12-06 18:42:11 +0100

    Fix compile i18n for Mac OS

--- webpack.tasks/compileI18N.sh
+++ webpack.tasks/compileI18N.sh
@@ -5,9 +5,12 @@ COMMAND=./node_modules/.bin/angular-gettext-cli;
 for file in $(ls po/*.po)
 do
     lang="${file:3:2}";
+    # ${lang^^} doesn't work with Mac OS
+    langUpper="$(echo $lang | tr '[a-z]' '[A-Z]')"
+
     $COMMAND \
         --files $file \
-        --dest src/i18n/i18n.${lang}_${lang^^}.json \
+        --dest src/i18n/i18n.${lang}_${langUpper}.json \
         --compile \
         --format json;
 done
