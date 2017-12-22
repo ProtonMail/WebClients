49baaa0f5fc1e3eddcb443386a5e9d9f8158b78a
Richard <richard@protonmail.com>
2017-12-22 13:26:09 +0100

    Fix #6167 - Loading state for the contact modal

--- src/app/contact/modals/contactModal.js
+++ src/app/contact/modals/contactModal.js
@@ -9,34 +9,57 @@ function contactModal($rootScope, $state, gettextCatalog, notification, pmModal)
         controllerAs: 'ctrl',
         templateUrl: 'templates/contact/contactModal.tpl.html',
         /* @ngInject */
-        controller: function(params) {
+        controller: function(params, $scope) {
+            const unsubscribe = [];
             let processing = false;
+            this.networkActivity = false;
 
-            const unsubscribe = $rootScope.$on('contacts', (event, { type = '', data = {} }) => {
-                if (type === 'contactCreated') {
-                    const { created = [], errors = [] } = data;
-                    processing = false;
+            unsubscribe.push(
+                $rootScope.$on('contacts', (event, { type = '', data = {} }) => {
+                    if (type === 'contactCreated') {
+                        const { created = [], errors = [] } = data;
+                        processing = false;
 
-                    if (errors.length) {
-                        errors.forEach(({ error = I18N.contactError }) => notification.error(error));
+                        if (errors.length) {
+                            errors.forEach(({ error = I18N.contactError }) => notification.error(error));
+                        }
+
+                        if (created.length) {
+                            notification.success(I18N.contactAdded);
+                            $state.go('secured.contacts.details', { id: created[0].ID });
+                            params.close();
+                        }
+                    }
+                })
+            );
+
+            unsubscribe.push(
+                $rootScope.$on('networkActivity', (e, type) => {
+                    if (type === 'load') {
+                        $scope.$applyAsync(() => {
+                            this.networkActivity = true;
+                        });
                     }
 
-                    if (created.length) {
-                        notification.success(I18N.contactAdded);
-                        $state.go('secured.contacts.details', { id: created[0].ID });
-                        params.close();
+                    if (type === 'close') {
+                        $scope.$applyAsync(() => {
+                            this.networkActivity = false;
+                        });
                     }
-                }
-            });
+                })
+            );
 
             this.contact = params.contact;
             this.cancel = params.close;
-            this.$onDestroy = () => unsubscribe();
             this.submit = () => {
                 if (!processing) {
                     processing = true;
                     $rootScope.$emit('contacts', { type: 'submitContactForm' });
                 }
+            };
+            this.$onDestroy = () => {
+                unsubscribe.forEach((cb) => cb());
+                unsubscribe.length = 0;
             };
         }
     });
