a40c1725d4347124712f7910321657f525cd9d56
dhoko <dhoko@users.noreply.github.com>
2018-01-03 11:00:25 +0100

    Improve contact import component (#6174)

---
+++ src/app/contact/directives/importCardDropzone.js
@@ -0,0 +1,51 @@
+/* @ngInject */
+function importCardDropzone(notification, gettextCatalog, $rootScope) {
+    const I18N = {
+        dictDefaultMessage: gettextCatalog.getString('Drop file or click here', null, 'Info'),
+        dictInvalidFileType: gettextCatalog.getString('Invalid file type', null, 'Error')
+    };
+
+    const CLASS_HAS_FILE = 'importCardDropzone-has-file';
+
+    return {
+        scope: {},
+        replace: true,
+        restrict: 'E',
+        templateUrl: 'templates/contact/importCardDropzone.tpl.html',
+        link(scope, el) {
+            const dropzone = new Dropzone(el[0].firstElementChild, {
+                addRemoveLinks: false,
+                dictDefaultMessage: I18N.dictDefaultMessage,
+                dictInvalidFileType: I18N.dictInvalidFileType,
+                url: '/file/post',
+                autoProcessQueue: false,
+                maxFiles: 1,
+                paramName: 'file', // The name that will be used to transfer the file
+                previewTemplate: '<div style="display:none"></div>',
+                ignoreHiddenFiles: true,
+                acceptedFiles: '.vcf,.csv'
+            });
+
+            dropzone.on('addedfiles', (list) => {
+                const file = list[0];
+                if (!/\.(vcf|csv)$/i.test(file.name)) {
+                    _.defer(() => dropzone.removeAllFiles(true), 100);
+                    return notification.error(I18N.dictInvalidFileType);
+                }
+
+                $rootScope.$emit('importCardDropzone', { data: [file] });
+                scope.$applyAsync(() => {
+                    el[0].classList.add(CLASS_HAS_FILE);
+                    scope.fileDropped = file.name;
+                });
+            });
+
+            scope.$on('$destroy', () => {
+                dropzone.off('dragover');
+                dropzone.off('addedfiles');
+                dropzone.destroy();
+            });
+        }
+    };
+}
+export default importCardDropzone;
---
+++ src/app/contact/modals/importContactModal.js
@@ -0,0 +1,22 @@
+/* @ngInject */
+function importContactModal(pmModal, dispatchers) {
+    return pmModal({
+        controllerAs: 'ctrl',
+        templateUrl: 'templates/contact/importContactModal.tpl.html',
+        /* @ngInject */
+        controller: function(params, $scope) {
+            const { on, unsubscribe } = dispatchers();
+            let files = [];
+
+            this.import = () => params.import(files);
+            this.cancel = params.cancel;
+            this.$onDestroy = unsubscribe;
+
+            on('importCardDropzone', (e, { data }) => {
+                files = data;
+                $scope.$applyAsync(() => (this.fileDropped = true));
+            });
+        }
+    });
+}
+export default importContactModal;
