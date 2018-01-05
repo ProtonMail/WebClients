90f376eab7fb59f5fa43b6883db566d2a00213e9
dhoko <aurelien@protonmail.com>
2018-01-05 14:32:19 +0100

    Fix test suite

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -12,12 +12,9 @@ module.exports = (config) => {
             '!../build/openpgp.worker.min.js',
             '../build/openpgp.min.js',
             '../build/templates.js',
-            // '../build/app.js',
-            // '../build/appLazy.js',
             '../node_modules/angular-mocks/angular-mocks.js',
             '../src/templates/**/*.html',
             'specs/**/*.js'
-            // 'mocks/**/*.js'
         ],
 
         // list of files to exclude
@@ -59,7 +56,6 @@ module.exports = (config) => {
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
         autoWatch: false,
-        // browsers: ['PhantomJS'],
         browsers: ['ChromeHeadless'],
         singleRun: true
     });
