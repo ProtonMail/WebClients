d4f81b487ba6a03922b41406b506a3e8a96e62b3
dhoko <dhoko@users.noreply.github.com>
2018-01-10 10:49:02 +0100

    Download attachment if the decryption failed  (#6205)
    * Download raw attachment if decryption fails
    
    * Fix #6138 ~ Fallback download encrypted file
    
    * Fix #6138 ~ Confirm download via a modal
    
    * Improve txt modal ~ allow custom className on confirmModal
    
    * Fix import lodash

--- src/app/core/factories/confirmModal.js
+++ src/app/core/factories/confirmModal.js
@@ -4,6 +4,12 @@ function confirmModal(pmModal, gettextCatalog) {
         confirm: gettextCatalog.getString('Confirm', null, 'Default text for the confirm button in the confirm modal'),
         cancel: gettextCatalog.getString('Cancel', null, 'Default text for the cancel button in the confirm modal')
     };
+
+    const getClassName = ({ isDanger = false, isWarning = false } = {}) => {
+        const danger = isDanger && 'alert-danger';
+        const warning = isWarning && 'alert-warning';
+        return ['alert', danger, warning].filter(Boolean).join(' ');
+    };
     return pmModal({
         controllerAs: 'ctrl',
         templateUrl: require('../../../templates/modals/confirm.tpl.html'),
@@ -13,6 +19,7 @@ function confirmModal(pmModal, gettextCatalog) {
             this.title = params.title;
             this.message = params.message;
             this.confirmText = params.confirmText || I18N.confirm;
+            this.classNameMessage = getClassName(params);
             this.cancelText = params.cancelText || I18N.cancel;
             this.confirm = () => (hotkeys.bind(['enter']), params.confirm());
             this.cancel = () => (hotkeys.bind(['enter']), params.cancel());
