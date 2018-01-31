c851a4e6137785c247790ea0e2064cf3db1d9eb9
mmso <mmso@protonmail.com>
2018-01-31 09:44:34 +0100

    Fix #6366 - Insert image button remains selected after you insert an image in the composer (#6367)

--- src/app/squire/directives/addFilePopover.js
+++ src/app/squire/directives/addFilePopover.js
@@ -1,5 +1,11 @@
 /* @ngInject */
-function addFilePopover(editorModel, CONSTANTS, squireExecAction, onCurrentMessage) {
+function addFilePopover($rootScope, editorModel, CONSTANTS, squireExecAction, onCurrentMessage) {
+    const dispatch = (data) => {
+        $rootScope.$emit('squire.editor', {
+            type: 'popover.form',
+            data
+        });
+    };
     const { DEFAULT_SQUIRE_VALUE } = CONSTANTS;
     const IMAGE = DEFAULT_SQUIRE_VALUE.IMAGE;
     const CLASS_HIDDEN = 'addFilePopover-hidden';
@@ -33,18 +39,21 @@ function addFilePopover(editorModel, CONSTANTS, squireExecAction, onCurrentMessa
         replace: true,
         templateUrl: require('../../../templates/squire/addFilePopover.tpl.html'),
         link(scope, el, { name }) {
+            const hide = () => {
+                el[0].classList.add(CLASS_HIDDEN);
+                dispatch({ name, form: {}, action: 'close.popover', message: scope.message });
+            };
             const $embedded = el[0].querySelector('.addFilePopover-btn-embedded');
             const $uri = el[0].querySelector('.addFilePopover-btn-uri');
             const $input = el[0].querySelector('.addFilePopover-input-file');
             const onChange = onChangeFile(scope.message, el[0]);
-            const onClick = () => el[0].classList.add(CLASS_HIDDEN);
             const onClickUri = () => {
                 $input.click();
-                el[0].classList.add(CLASS_HIDDEN);
+                hide();
             };
 
             $uri.addEventListener('click', onClickUri, false);
-            $embedded.addEventListener('click', onClick, false);
+            $embedded.addEventListener('click', hide, false);
             $input.addEventListener('change', onChange, false);
 
             const onAction = (type, data = {}) => {
@@ -64,7 +73,7 @@ function addFilePopover(editorModel, CONSTANTS, squireExecAction, onCurrentMessa
             scope.$on('$destroy', () => {
                 unsubscribe();
                 $uri.removeEventListener('click', onClickUri, false);
-                $embedded.removeEventListener('click', onClick, false);
+                $embedded.removeEventListener('click', hide, false);
                 $input.removeEventListener('change', onChange, false);
             });
         }
