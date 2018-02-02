a415ac322a7a51463e73d6d63a2df876c34ba595
dhoko <dhoko@users.noreply.github.com>
2018-02-02 15:41:06 +0100

    Fix #6195 ~ Report bug with stacktrace on error message (#6339)

---
+++ src/app/message/directives/messageDisplayError.js
@@ -0,0 +1,38 @@
+/* @ngInject */
+function messageDisplayError($rootScope, dedentTpl) {
+
+    const CLASS_NAME = {
+        REPORT: 'messageDisplayError-btn-report'
+    };
+
+    return {
+        replace: true,
+        restrict: 'E',
+        templateUrl: require('../../../templates/message/messageDisplayError.tpl.html'),
+        link(scope, el) {
+            const onClick = (e) => {
+                if (e.target.classList.contains(CLASS_NAME.REPORT)) {
+                    e.preventDefault();
+                    $rootScope.$emit('bugReport', {
+                        type: 'new',
+                        data: {
+                            content: dedentTpl`
+
+
+                                === Debug stacktrace for the support ===
+                                ${scope.message.errorInfo.stack}
+                            `
+                        }
+                    });
+                }
+            };
+
+            el.on('click', onClick);
+
+            scope.$on('$destroy', () => {
+                el.off('click', onClick);
+            });
+        }
+    };
+}
+export default messageDisplayError;
