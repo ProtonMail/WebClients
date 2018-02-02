8499d6037e74243fa19d4431d1b4082d721da0c3
Richard <EpokK@users.noreply.github.com>
2018-02-02 12:29:32 +0100

    Fix #6384 - Handle events logic (#6387)
    * Fix #6384 - Handle events logic
    
    * in one place
    * consider `create` and `update` with the same logic
    
    * Vanilla JS FTW
    
    * Move the helper + fix update

---
+++ src/app/utils/helpers/updateCollection.js
@@ -0,0 +1,63 @@
+import _ from 'lodash';
+
+import { CONSTANTS } from '../../constants';
+
+const ACTIONS = {
+    [CONSTANTS.STATUS.DELETE]: 'remove',
+    [CONSTANTS.STATUS.CREATE]: 'create',
+    [CONSTANTS.STATUS.UPDATE]: 'update'
+};
+
+/**
+ * Update collection from events
+ * @param  {Array}  [all=[]]
+ * @param  {Array}  [events=[]]
+ * @param  {String} [type='']
+ * @return {Array}
+ */
+function updateCollection(all = [], events = [], type = '') {
+    const copy = all.slice(0);
+    const todo = events.reduce(
+        (acc, task) => {
+            const { Action, ID } = task;
+            const action = ACTIONS[Action];
+
+            if (action === 'remove') {
+                acc.remove[ID] = true;
+                return acc;
+            }
+
+            acc[action].push(task[type]);
+
+            return acc;
+        },
+        { update: [], create: [], remove: {} }
+    );
+
+    // NOTE We cannot trust Action so "create" and "update" events need to be handle in the way
+    const { collection } = _.reduce([].concat(todo.create, todo.update), (acc, element) => {
+        const index = acc.MAP[element.ID];
+
+        if (index) {
+            // Update
+            acc.collection[index] = element;
+            return acc;
+        }
+
+        // Create
+        const length = acc.collection.push(element);
+        acc.MAP[element.ID] = length - 1; // index
+
+        return acc;
+    }, {
+        collection: copy,
+        MAP: _.reduce(copy, (acc, element, index) => (acc[element.ID] = index, acc), {})
+    });
+
+    return {
+        collection: _.filter(collection, ({ ID }) => !todo.remove[ID]),
+        todo
+    };
+}
+
+export default updateCollection;
