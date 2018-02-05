98fc98377a1569e82d7140107aee40582928d6ea
dhoko <aurelien@protonmail.com>
2018-02-05 15:16:00 +0100

    Fix IIFE fingerprint2

--- env/conf.build.js
+++ env/conf.build.js
@@ -25,7 +25,7 @@ module.exports = {
             'mousetrap/plugins/pause/mousetrap-pause.js',
             'mousetrap/plugins/global-bind/mousetrap-global-bind.js',
             'angular-gettext/dist/angular-gettext.js',
-            'fingerprintjs2/fingerprint2.js',
+            'fingerprintjs2/dist/fingerprint2.js',
             'bcryptjs/dist/bcrypt.js',
             'nouislider/distribute/nouislider.js',
             'ua-parser-js/src/ua-parser.js',
