5510712fc06eedf0e048e48877d5453f9e924dd5
Fabien Salathe <fabacrans@gmail.com>
2018-02-06 16:37:50 +0100

    Fix #6360, improve sorting in contacts page (#6369)
    * Fix #6360, improve sorting in contacts page
    
    * Move the logic in contactArrowSort component
    
    * Remove useless div, add span with translation info for titles
    
    * Move float outside the component

---
+++ src/helpers/urlHelpers.js
@@ -0,0 +1,12 @@
+/**
+ * Get the current sorting state from the URL (?sort=asc|desc)
+ * @param {String} stateParams
+ * @return {Object}
+ */
+export const currentSorting = (stateParams) => {
+    const prefix = (stateParams.sort || '').startsWith('-') ? '-' : '';
+    const sort = (stateParams.sort || '').substr(prefix.length);
+    const order = prefix === '' ? 'asc' : 'desc';
+
+    return { order, sort };
+};
