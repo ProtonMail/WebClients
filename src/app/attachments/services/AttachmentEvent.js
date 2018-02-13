5a2d661cfd05bcb4c942db9c623b0c13fc9fa751
Fabien Salathe <fabacrans@gmail.com>
2018-02-13 20:10:46 +0100

    Fix ics viewer on messages with multiple conversations (#6462)
    * Fix ics viewer on messages with multiple conversations
    
    * Simplify code
    
    * Refactor eventsAttachments
    
    * Fix scope function

--- src/app/attachments/services/AttachmentEvent.js
+++ src/app/attachments/services/AttachmentEvent.js
@@ -1,13 +1,48 @@
 import ICAL from 'ical.js';
 
 /* @ngInject */
-function AttachmentEvent() {
+function AttachmentEvent(
+    attachmentDownloader,
+    notification,
+    AppModel
+) {
+    const REGEXP_EVENT_EXTENSIONS = /\.(?:ics|icalendar|ical|vcard|vcf)$/i;
+
+    const filterAttachmentsForEvents = (attachments = []) =>
+        attachments.filter(({ Name }) => REGEXP_EVENT_EXTENSIONS.test(Name));
+
+    const mapAttachmentsToIcalEvent = (event, model) => {
+        return attachmentDownloader
+            .downloadString(event, model)
+            .then(getIcalEvent)
+            .catch((err) => AppModel.is('onLine') && notification.error(err));
+    };
+
+    const load = (events, model) => {
+        console.log(events, model);
+        return Promise.all(events.map((event) => mapAttachmentsToIcalEvent(event, model)));
+    };
+
+    const dateFormat = ({ startDateMoment: start, endDateMoment: end, startDate }, withTimezone = false) => {
+        if (!moment.isMoment(start) || !moment.isMoment(end)) {
+            return '';
+        }
+
+        const timezone = (withTimezone && startDate.timezone) ? `(${startDate.timezone})` : '';
+
+        if (end.diff(start, 'days') < 1) {
+            return `${start.format('ll')} ${start.format('LT')} – ${end.format('LT')} ${timezone}`;
+        }
+
+        return `${start.format('ll')} ${start.format('LT')} – ${end.format('ll')} ${end.format('LT')} ${timezone}`;
+    };
+
     /**
      * Get ical event from raw string of the event
      * @param {string} event info
      * @return {object}
      */
-    const getIcalEvent = (info) => {
+    function getIcalEvent(info) {
         const jcalData = ICAL.parse(info);
         const vcalendar = new ICAL.Component(jcalData);
         const vevent = vcalendar.getFirstSubcomponent('vevent');
@@ -34,6 +69,12 @@ function AttachmentEvent() {
         return icalEvent;
     };
 
-    return { getIcalEvent };
+    return {
+        dateFormat,
+        getIcalEvent,
+        filterAttachmentsForEvents,
+        mapAttachmentsToIcalEvent,
+        load
+    };
 }
 export default AttachmentEvent;
