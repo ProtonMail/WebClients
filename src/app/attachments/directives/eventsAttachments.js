454b5050624de0de83503b977165edf2293f7ad7
Fabien Salathe <fabacrans@gmail.com>
2018-02-13 20:10:46 +0100

    Fix ics viewer on messages with multiple conversations (#6462)
    * Fix ics viewer on messages with multiple conversations
    
    * Simplify code
    
    * Refactor eventsAttachments
    
    * Fix scope function

--- src/app/attachments/directives/eventsAttachments.js
+++ src/app/attachments/directives/eventsAttachments.js
@@ -6,13 +6,12 @@ function eventsAttachments(
     $rootScope,
     pmcw,
     attachmentDownloader,
-    notification,
     AppModel,
     AttachmentEvent,
     networkActivityTracker,
+    dispatchers,
     downloadFile
 ) {
-    const REGEXP_EVENT_EXTENSIONS = /\.(?:ics|icalendar|ical|vcard|vcf)$/i;
     const LOAD_CLASSNAME = 'eventsAttachments-load';
     const HIDDEN_CLASSNAME = 'eventsAttachments-hidden';
 
@@ -23,18 +22,44 @@ function eventsAttachments(
         replace: true,
         templateUrl: require('../../../templates/attachments/eventsAttachments.tpl.html'),
         link(scope, el) {
-            scope.dateFormat = ({ startDateMoment: start, endDateMoment: end, startDate }, withTimezone = false) => {
-                if (!moment.isMoment(start) || !moment.isMoment(end)) {
-                    return '';
+            const { on, unsubscribe } = dispatchers(['message.open']);
+
+            scope.dateFormat = AttachmentEvent.dateFormat;
+
+            const loadEvents = (attachments = []) => {
+                const events = AttachmentEvent.filterAttachmentsForEvents(attachments);
+
+                if (!events.length) {
+                    return;
                 }
 
-                const timezone = (withTimezone && startDate.timezone) ? `(${startDate.timezone})` : '';
+                el.removeClass(HIDDEN_CLASSNAME);
 
-                if (end.diff(start, 'days') < 1) {
-                    return `${start.format('ll')} ${start.format('LT')} – ${end.format('LT')} ${timezone}`;
-                }
+                // Initialize events for placeholders
+                scope.model.events = events.map((_, uid) => ({ uid }));
 
-                return `${start.format('ll')} ${start.format('LT')} – ${end.format('ll')} ${end.format('LT')} ${timezone}`;
+                el.addClass(LOAD_CLASSNAME);
+
+                const promise = AttachmentEvent.load(events, scope.model).then((events) => {
+                    el.remove(LOAD_CLASSNAME);
+
+                    scope.$applyAsync(() => {
+                        scope.model.events = events
+                            // Remove empty elements (eg. corrupted icals)
+                            .filter(Boolean)
+                            // Remove events with same UID (duplication)
+                            .filter((ev, index, self) => self.findIndex((ev2) => ev2.uid === ev.uid) === index);
+
+                        if (scope.model.events.length === 1) {
+                            _.defer(() => {
+                                // Open the first event panel
+                                el[0].querySelector('.eventsAttachments-details').setAttribute('open', 'open');
+                            });
+                        }
+                    });
+                });
+
+                networkActivityTracker.track(promise);
             };
 
             const onClick = (e) => {
@@ -58,47 +83,17 @@ function eventsAttachments(
                 }
             };
 
-            const events = scope.model.Attachments.filter(({ Name }) => REGEXP_EVENT_EXTENSIONS.test(Name));
-
-            const mapAttachmentsToIcalEvent = (event) =>
-                attachmentDownloader
-                    .downloadString(event, scope.model)
-                    .then(AttachmentEvent.getIcalEvent)
-                    .catch((err) => AppModel.is('onLine') && notification.error(err));
-
-            if (events.length) {
-                el.removeClass(HIDDEN_CLASSNAME);
-
-                // Initialize events for placeholders
-                scope.model.events = Array.from({ length: events.length }, (_, i) => ({ uid: i }));
-
-                el.addClass(LOAD_CLASSNAME);
-
-                const promise = Promise.all(events.map(mapAttachmentsToIcalEvent)).then((events) => {
-                    el.remove(LOAD_CLASSNAME);
-
-                    scope.$applyAsync(() => {
-                        scope.model.events = events
-                            // Remove empty elements (eg. corrupted icals)
-                            .filter(Boolean)
-                            // Remove events with same UID (duplication)
-                            .filter((ev, index, self) => self.findIndex((ev2) => ev2.uid === ev.uid) === index);
-
-                        if (scope.model.events.length === 1) {
-                            _.defer(() => {
-                                // Open the first event panel
-                                el[0].querySelector('.eventsAttachments-details').setAttribute('open', 'open');
-                            });
-                        }
-                    });
-                });
-
-                networkActivityTracker.track(promise);
-            }
+            on('message.open', (e, { type = '', data = {} }) => {
+                if (type === 'render') {
+                    const { message = {} } = data;
+                    loadEvents(message.Attachments);
+                }
+            });
 
             el.on('click', onClick);
 
             scope.$on('$destroy', () => {
+                unsubscribe();
                 el.off('click', onClick);
             });
         }
