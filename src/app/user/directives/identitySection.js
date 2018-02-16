adeec059faa7eebd353eec463a06a1c5b2ca6dc2
dhoko <dhoko@users.noreply.github.com>
2018-02-16 15:39:37 +0100

    Fix #6447 ~ Auto create phone anchor inside the Signature (#6480)
    * Fix #6447 ~ Auto create phone anchor inside the Signature
    
    * Scope submit event to the form itself

--- src/app/user/directives/identitySection.js
+++ src/app/user/directives/identitySection.js
@@ -1,27 +1,40 @@
 import _ from 'lodash';
+import isPhone from 'phone-regex';
 
 /* @ngInject */
-function identitySection($rootScope, authentication, editorModel, gettextCatalog, notification, signatureModel, tools) {
+function identitySection(authentication, editorModel, gettextCatalog, notification, signatureModel, tools, dispatchers) {
+
     const I18N = {
         SUCCESS_SAVE: gettextCatalog.getString('Default Name / Signature saved', null, "User's signature")
     };
     const EDITOR_ID = 'signature';
     const MULTIPLE_ADDRESS_CLASS = 'identitySection-has-multiple-address';
+
+    /**
+     * Find if there is a phone number inside the signature and replace it with
+     * an anchor.
+     * @param  {String} input Signature
+     * @return {String}
+     */
+    const bindPhone = (input = '') => {
+        if (/tel:/.test(input)) {
+            return input;
+        }
+        return input.replace(isPhone(), (match) => {
+            return ` <a href="tel:${match.trim()}">${match.trim()}</a>`;
+        });
+    };
+
     return {
         scope: {},
         replace: true,
         restrict: 'E',
         templateUrl: require('../../../templates/user/identitySection.tpl.html'),
         link(scope, el) {
-            const $form = el;
             const CACHE = {};
-            const unsubscribe = [];
+            const $form = el.find('[name="identityForm"]');
 
-            const onSubmit = () => {
-                const { DisplayName, Signature } = scope.address;
-                signatureModel.save({ ID: CACHE.ID, DisplayName, Signature })
-                    .then(() => notification.success(I18N.SUCCESS_SAVE));
-            };
+            const { on, unsubscribe } = dispatchers();
 
             const updateAddress = ({ ID, DisplayName, Signature }, firstTime = false) => {
                 const signature = tools.replaceLineBreaks(Signature);
@@ -35,16 +48,28 @@ function identitySection($rootScope, authentication, editorModel, gettextCatalog
 
                 scope.address = { DisplayName, Signature: signature };
             };
+
             const updateUser = () => {
                 const { Addresses = [] } = authentication.user || {};
-
                 CACHE.addresses = Addresses.slice(0);
                 el[0].classList[CACHE.addresses.length > 1 ? 'add' : 'remove'](MULTIPLE_ADDRESS_CLASS);
             };
 
+            const onSubmit = async () => {
+                const { DisplayName, Signature } = scope.address;
+                const config = {
+                    ID: CACHE.ID,
+                    DisplayName,
+                    Signature: bindPhone(Signature)
+                 };
+                await signatureModel.save(config);
+                notification.success(I18N.SUCCESS_SAVE);
+                updateAddress(config);
+            };
+
             $form.on('submit', onSubmit);
 
-            unsubscribe.push($rootScope.$on('changeAddressSelection', (event, { ID }) => {
+            on('changeAddressSelection', (event, { ID }) => {
                 const address = _.find(CACHE.addresses, { ID });
 
                 if (address) {
@@ -52,19 +77,16 @@ function identitySection($rootScope, authentication, editorModel, gettextCatalog
                         updateAddress(address);
                     });
                 }
-            }));
+            });
 
-            unsubscribe.push($rootScope.$on('updateUser', () => {
-                updateUser();
-            }));
+            on('updateUser', updateUser);
 
             updateUser();
             updateAddress(CACHE.addresses[0], true);
 
             scope.$on('$destroy', () => {
                 $form.off('submit', onSubmit);
-                unsubscribe.forEach((cb) => cb());
-                unsubscribe.length = 0;
+                unsubscribe();
             });
         }
     };
