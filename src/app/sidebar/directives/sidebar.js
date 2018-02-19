6d9be4fd23f9e80514d7347d81873c7990bc0135
mmso <mmso@protonmail.com>
2018-02-19 11:05:00 +0100

    Fix #5762 - Auto scroll sidebar labels when DnD (#6482)

--- src/app/sidebar/directives/sidebar.js
+++ src/app/sidebar/directives/sidebar.js
@@ -5,30 +5,6 @@ const sidebar = (sidebarModel) => ({
     templateUrl: require('../../../templates/partials/sidebar.tpl.html'),
     link(scope) {
         scope.listStates = Object.keys(sidebarModel.getStateConfig());
-        scope.scrollbarConfig = {
-            advanced: {
-                updateOnContentResize: true
-            },
-            scrollInertia: 0,
-            scrollButtons: {
-                enable: false
-            }
-        };
-
-        /**
-         * For some reason ng-scrollbars doesn't update when the content is resized.
-         * Force update it here.
-         * Related to issue https://github.com/ProtonMail/Angular/issues/6260.
-         */
-        function onResize() {
-            scope.$applyAsync(() => scope.updateScrollbar('update'));
-        }
-
-        window.addEventListener('resize', onResize);
-
-        scope.$on('$destroy', () => {
-            window.removeEventListener('resize', onResize);
-        });
     }
 });
 export default sidebar;
