e700b1df89940560b62fce0b479955d2bc7faf8f
Richard <EpokK@users.noreply.github.com>
2018-02-23 09:17:40 +0100

    Hotfix - Close payment modal if the subscription change (#6532)
    * Hotfix - Close payment modal if the subscription change
    
    * Add comment

--- src/app/payment/modals/paymentModal.js
+++ src/app/payment/modals/paymentModal.js
@@ -1,13 +1,23 @@
 /* @ngInject */
-function paymentModal(pmModal, paymentModalModel) {
+function paymentModal(dispatchers, pmModal, paymentModalModel) {
     return pmModal({
         controllerAs: 'ctrl',
         templateUrl: require('../../../templates/modals/payment/modal.tpl.html'),
         /* @ngInject */
         controller: function(params) {
+            const { on, unsubscribe } = dispatchers(['subscription']);
+
             paymentModalModel.set(params);
             // submit:Function is coming from the form inside the modal
+            // We need to close the payment modal if the subscription change to avoid "Amount Mismatch" issue (concurrency with payment process).
+            on('subscription', (event, { type }) => {
+                if (type === 'update' && this.step === 'payment') {
+                    params.cancel();
+                }
+            });
+
             this.$onDestroy = () => {
+                unsubscribe();
                 paymentModalModel.clear();
             };
         }
