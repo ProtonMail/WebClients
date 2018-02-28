6e230c6f402a7ef62913f92eb12582178a1f7d40
Richard <richard@protonmail.com>
2018-02-28 11:40:57 +0100

    Add other image helper (toImage, downSize)

--- src/helpers/imageHelper.js
+++ src/helpers/imageHelper.js
@@ -1,4 +1,21 @@
 /**
+ * Convert base64 string to Image
+ * @param  {String} base64str
+ * @return {Object}
+ */
+export const toImage = (base64str) => {
+    return new Promise((resolve) => {
+        const image = new Image();
+
+        image.onload = () => {
+            resolve(image);
+        };
+
+        image.src = base64str;
+    });
+};
+
+/**
 * Resizes a picture to a maximum length/width (based on largest dimension)
 * @param {String} original Base64 representation of image to be resized.
 * @param {Number} maxSize Amount of pixels that largest dimention (whether width or length) should have.
@@ -7,10 +24,8 @@
 * @return {Promise} receives base64 string of resized image.
 */
 export const resizeImage = (original, maxSize, finalMimeType = 'image/jpeg', encoderOptions = 1) => {
-    return new Promise((resolve) => {
-        const image = new Image();
-
-        image.onload = () => {
+    return toImage(original)
+        .then((image) => {
             // Resize the image
             const canvas = document.createElement('canvas');
             let width = image.width;
@@ -28,13 +43,8 @@ export const resizeImage = (original, maxSize, finalMimeType = 'image/jpeg', enc
             canvas.height = height;
             canvas.getContext('2d').drawImage(image, 0, 0, width, height);
 
-            const base64str = canvas.toDataURL(finalMimeType, encoderOptions);
-
-            resolve(base64str);
-        };
-
-        image.src = original;
-    });
+            return canvas.toDataURL(finalMimeType, encoderOptions);
+        });
 };
 
 /**
@@ -43,7 +53,7 @@ export const resizeImage = (original, maxSize, finalMimeType = 'image/jpeg', enc
  * @param  {String} filename
  * @return {Object}
  */
-export const base64toFile = (base64str, filename) => {
+export const base64toFile = (base64str, filename = 'file') => {
     const arr = base64str.split(',');
     const mime = arr[0].match(/:(.*?);/)[1];
     const bstr = atob(arr[1]);
@@ -55,4 +65,30 @@ export const base64toFile = (base64str, filename) => {
     }
 
     return new File([u8arr], filename, { type: mime });
+};
+
+/**
+ * Down size image to reach the max size limit
+ * @param  {String} base64str
+ * @param  {} maxSize in bytes
+ * @param  {String} mimeType
+ * @param  {Number} encoderOptions
+ * @return {Promise}
+ */
+export const downSize = (base64str, maxSize, mimeType = 'image/jpeg', encoderOptions = 1) => {
+    const process = (source, max) => {
+        return resizeImage(source, max, mimeType, encoderOptions)
+            .then((resized) => {
+                const { size } = base64toFile(resized);
+
+                if (size <= maxSize) {
+                    return resized;
+                }
+
+                return process(resized, Math.round(max * 0.9));
+            });
+    };
+
+    return toImage(base64str)
+        .then(({ height, width }) => process(base64str, height > width ? height : width));
 };
