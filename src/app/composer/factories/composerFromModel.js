1275e15270957ce6c96276f890eca2804d32e0ba
Richard <EpokK@users.noreply.github.com>
2018-03-20 20:15:14 +0100

    Fix #6677 - Display warning message if we auto-pick a default address… (#6683)
    * Fix #6677 - Display warning message if we auto-pick a default address instead of the premium one
    
    * Review with @mmso
    
    * Review with @mmso 2
    
    * Add translation context
    
    * Review with @dhoko

--- src/app/composer/factories/composerFromModel.js
+++ src/app/composer/factories/composerFromModel.js
@@ -1,9 +1,18 @@
 import _ from 'lodash';
 
 import { flow, filter, sortBy } from 'lodash/fp';
+import { ADDRESS_TYPE } from '../../constants';
 
 /* @ngInject */
-function composerFromModel(addressesModel, authentication, plusAliasModel) {
+function composerFromModel(addressesModel, authentication, confirmModal, gettextCatalog, plusAliasModel) {
+    const PREMIUM_ADDRESS_ITEM = 'protonmail_premium_address';
+    const I18N = {
+        CONFIRM: gettextCatalog.getString("Don't remind me again", null, 'Action'),
+        TITLE: gettextCatalog.getString('Warning', null, 'Title'),
+        error(email) {
+            return gettextCatalog.getString('Sending messages from @pm.me address is a paid feature. Your message will be sent from your default address {{email}}', { email }, 'Error');
+        }
+    };
     /**
      * Return list of addresses available in the FROM select
      * @param  {Object} message
@@ -21,6 +30,23 @@ function composerFromModel(addressesModel, authentication, plusAliasModel) {
         return { addresses, address: find(addresses, AddressID) };
     }
 
+    function displayWarning(email) {
+        confirmModal.activate({
+            params: {
+                title: I18N.TITLE,
+                message: I18N.error(email),
+                confirmText: I18N.CONFIRM,
+                confirm() {
+                    confirmModal.deactivate();
+                    localStorage.setItem(PREMIUM_ADDRESS_ITEM, 'dontRemind');
+                },
+                cancel() {
+                    confirmModal.deactivate();
+                }
+            }
+        });
+    }
+
     /**
      * Return the address selected in the FROM select
      * @param  {Array} addresses
@@ -28,9 +54,18 @@ function composerFromModel(addressesModel, authentication, plusAliasModel) {
      * @return {Object}
      */
     function find(addresses = [], ID) {
+        const isPmMeAddress = _.some(addresses, { ID, Type: ADDRESS_TYPE.PREMIUM });
+
         if (!authentication.hasPaidMail()) {
             const onlySend = _.filter(addresses, { Send: 1 });
-            return _.find(onlySend, { ID }) || onlySend[0];
+            const address = _.find(onlySend, { ID });
+
+            if (!address) {
+                isPmMeAddress && !localStorage.getItem(PREMIUM_ADDRESS_ITEM) && displayWarning(onlySend[0].Email);
+                return onlySend[0];
+            }
+
+            return address;
         }
 
         if (ID) {
