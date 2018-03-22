82641055d98883ac76247b982574df75835196a7
Richard <richard@protonmail.com>
2018-03-22 11:10:23 +0100

    Hotfix - Pick the correct option when the addresses are updated

--- src/app/user/directives/addressesSelector.js
+++ src/app/user/directives/addressesSelector.js
@@ -12,13 +12,20 @@ function addressesSelector(addressesModel, dispatchers) {
             const $select = el.find('.addressesSelector-select');
             const { dispatcher, on, unsubscribe } = dispatchers(['addressSelection']);
             const onChange = ({ target }) => dispatcher.addressSelection('change', { ID: target.value });
+            const updateSelect = () => {
+                const index = $select[0].selectedIndex;
 
-            $select[0].innerHTML = buildOptions();
+                $select[0].innerHTML = buildOptions();
+                $select[0].selectedIndex = Math.max(0, index); // NOTE Make sure we don't use negative value because `selectedIndex` can equals `-1` the first time, when options doesn't exist yet
+            };
+
+            updateSelect();
 
             $select.on('change', onChange);
+
             on('addressesModel', (e, { type = '' }) => {
                 if (type === 'addresses.updated') {
-                    $select[0].innerHTML = buildOptions();
+                    updateSelect();
                 }
             });
 
