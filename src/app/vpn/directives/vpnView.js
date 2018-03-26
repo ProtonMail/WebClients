9cfe4b902f7ff1a8c975a30fa9a322c4ec250af7
Richard <richard@protonmail.com>
2018-03-26 10:28:02 +0200

    Fix #6712 - Update ProtonVPN views

--- src/app/vpn/directives/vpnView.js
+++ src/app/vpn/directives/vpnView.js
@@ -1,13 +1,15 @@
 /* @ngInject */
-function vpnView(dispatchers, $state, addressesModel, authentication) {
+function vpnView(dispatchers, addressesModel, authentication, memberModel) {
     const vpnStatus = () => {
         const { Status = 0 } = authentication.user.VPN;
         return Status;
     };
+
     const getFirstEmail = () => {
         const addresses = addressesModel.get();
         return addresses.length ? addresses[0].Email : '';
     };
+
     return {
         restrict: 'E',
         replace: true,
@@ -17,7 +19,7 @@ function vpnView(dispatchers, $state, addressesModel, authentication) {
             const { on, unsubscribe } = dispatchers();
 
             const update = () => {
-                scope.VPNLogin = getFirstEmail();
+                scope.VPNLogin = memberModel.isMember() ? getFirstEmail() : authentication.user.Name;
                 scope.vpnEnabled = vpnStatus();
             };
 
