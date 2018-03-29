5afdc93a3bac7a34650148f4d78276d4d6f14776
Richard <EpokK@users.noreply.github.com>
2018-03-29 17:13:59 +0200

    Fix #6749 - textContent not reset in the template (#6750)

--- src/app/pmMe/directives/pmMeBtn.js
+++ src/app/pmMe/directives/pmMeBtn.js
@@ -11,8 +11,9 @@ function pmMeBtn(gettextCatalog, pmMeModel) {
     return {
         replace: true,
         restrict: 'E',
-        template: `<button type="button">${I18N.activate(pmMeModel.email())}</button>`,
+        template: '<button class="pmMeBtn-container" type="button"></button>',
         link(scope, el) {
+            el[0].textContent = I18N.activate(pmMeModel.email());
             el.on('click', onClick);
             scope.$on('$destroy', () => {
                 el.off('click', onClick);
