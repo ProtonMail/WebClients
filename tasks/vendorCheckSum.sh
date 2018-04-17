c0b3815c320f41e4e45c3c7d1f75ac74742e9c2e
Bart Butler <bart.c.butler@gmail.com>
2018-04-17 13:25:55 -0700

    Fix OpenPGP cache busting (#6834)

--- tasks/vendorCheckSum.sh
+++ tasks/vendorCheckSum.sh
@@ -20,6 +20,7 @@ HASH_VENDOR_LAZY=$(shasum dist/vendorLazy.js | awk '{print  $1}');
 HASH_VENDOR_LAZY2=$(shasum dist/vendorLazy2.js | awk '{print  $1}');
 HASH_STYLE=$(shasum dist/styles.css | awk '{print  $1}');
 HASH_OPENPGP=$(shasum dist/openpgp.min.js | awk '{print  $1}');
+HASH_WORKER=$(shasum dist/openpgp.worker.min.js | awk '{print  $1}');
 
 echo HASH_STYLE $HASH_STYLE
 echo HASH_APP $HASH_APP
@@ -27,25 +28,27 @@ echo HASH_APP_LAZY $HASH_APP_LAZY
 echo HASH_VENDOR $HASH_VENDOR
 echo HASH_VENDOR_LAZY $HASH_VENDOR_LAZY
 echo HASH_VENDOR_LAZY2 $HASH_VENDOR_LAZY2
-# Don't rename the file => else the worker is broken
 echo HASH_OPENPGP $HASH_OPENPGP
+echo HASH_WORKER $HASH_WORKER
 
 echo
 echo " → Attach shasum to files"
 mv dist/index.html dist/app.html
 mv dist/app.js.map dist/app.$HASH_APP.js.map
 mv dist/appLazy.js.map dist/appLazy.$HASH_APP_LAZY.js.map
-mv dist/vendor.js dist/vendor.$HASH_VENDOR.js
 mv dist/vendorLazy.js dist/vendorLazy.$HASH_VENDOR_LAZY.js
 mv dist/vendorLazy2.js dist/vendorLazy2.$HASH_VENDOR_LAZY2.js
 mv dist/styles.css dist/styles.$HASH_STYLE.css
+mv dist/openpgp.min.js dist/openpgp.$HASH_OPENPGP.js
 
 echo " → Write shasum"
 
-sed -e "s/app.js/app.$HASH_APP.js/g;s/appLazy.js/appLazy.$HASH_APP_LAZY.js/g;s/styles.css/styles.$HASH_STYLE.css/g;s/vendor.js/vendor.$HASH_VENDOR.js/g;s/vendorLazy.js/vendorLazy.$HASH_VENDOR_LAZY.js/g;s/vendorLazy2.js/vendorLazy2.$HASH_VENDOR_LAZY2.js/g;s/openpgp.min.js/openpgp.min.js?rel=$HASH_OPENPGP/g" < dist/app.html > dist/index.html;
+sed -e "s/app.js/app.$HASH_APP.js/g;s/appLazy.js/appLazy.$HASH_APP_LAZY.js/g;s/styles.css/styles.$HASH_STYLE.css/g;s/vendor.js/vendor.$HASH_VENDOR.js/g;s/vendorLazy.js/vendorLazy.$HASH_VENDOR_LAZY.js/g;s/vendorLazy2.js/vendorLazy2.$HASH_VENDOR_LAZY2.js/g;s/openpgp.min.js/openpgp.$HASH_OPENPGP.js/g;" < dist/app.html > dist/index.html;
 sed -e "s/vendorLazy.js/vendorLazy.$HASH_VENDOR_LAZY.js/g;s/vendorLazy2.js/vendorLazy2.$HASH_VENDOR_LAZY2.js/g;s/appLazy.js/appLazy.$HASH_APP_LAZY.js/g;s/app.js.map/app.$HASH_APP.js.map/g;" < dist/app.js > dist/app.$HASH_APP.js;
 sed -e "s/appLazy.js.map/appLazy.$HASH_APP_LAZY.js.map/g;" < dist/appLazy.js > dist/appLazy.$HASH_APP_LAZY.js;
+sed -e "s/openpgp.min.js/openpgp.$HASH_OPENPGP.js/g;" < dist/openpgp.worker.min.js > dist/openpgp.worker.$HASH_WORKER.js
+sed -e "s/openpgp.worker.min.js/openpgp.worker.$HASH_WORKER.js/g;" < dist/vendor.js > dist/vendor.$HASH_VENDOR.js
 
-rm dist/{app.html,app.js,appLazy.js,html.js};
+rm dist/{app.html,app.js,appLazy.js,html.js,openpgp.worker.min.js};
 echo " ✓ Write shasum success"
 echo
