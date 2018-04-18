d0c1b9832cd9a982845e4e7adb4d95185c5ff142
KAYLukas <kay.lukas@gmail.com>
2018-04-18 15:10:48 +0200

    Hotfix #6838 - Optimize sending speed (#6846)

--- src/app/message/factories/keyCache.js
+++ src/app/message/factories/keyCache.js
@@ -6,8 +6,38 @@ const KEY_GET_ADDRESS_NO_RECEIVE = 33103;
 const TIMEOUT = 10 * TIME.MINUTE;
 
 /* @ngInject */
-function keyCache(Key) {
+function keyCache(Key, addressesModel, mailSettingsModel) {
     const CACHE = {};
+
+    /**
+     * Tries to get the user keys from the local user. Will return the user keys in the `/keys` format if the email
+     * is a local email address. Otherwise it returns null
+     * @param email
+     * @return Object keys { RecipientType: number, MIMEType: string, Keys: list }
+     */
+    const getUserAddressesKeys = (email) => {
+        const addresses = addressesModel.get();
+        const address = addresses.find(({ Email }) => Email === email);
+        // Not our address: return undefined
+        if (!address) {
+            return;
+        }
+        const { Receive, Keys: addressKeys } = address;
+
+        const keys = addressKeys.map(({ PublicKey, decrypted }) => {
+            return {
+                Send: decrypted && Receive,
+                PublicKey
+            };
+        });
+
+        // Do not save in the cache: we use the addressesModel cache instead.
+        return {
+            RecipientType: Receive ? RECIPIENT_TYPE.TYPE_INTERNAL : RECIPIENT_TYPE.TYPE_NO_RECEIVE,
+            MIMEType: mailSettingsModel.get('ReceiveMIMEType'),
+            Keys: keys
+        };
+    };
 
     const getKeysFromApi = (email) => {
         return Key.keys(email)
@@ -44,6 +74,10 @@ function keyCache(Key) {
     };
 
     const getKeysPerEmail = async (email) => {
+        const keys = getUserAddressesKeys(email);
+        if (keys) {
+            return keys;
+        }
         const inCache = _.has(CACHE, email) && CACHE[email].time + TIMEOUT > Date.now();
         if (!inCache) {
             CACHE[email] = { time: Date.now(), data: getKeysFromApi(email) };
