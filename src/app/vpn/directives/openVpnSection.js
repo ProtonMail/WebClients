bad557868b490f711357fef8c1a637a15cad5227
Richard <EpokK@users.noreply.github.com>
2018-04-18 15:10:29 +0200

    Fix #6813 - Review UI for OpenVPN section (#6832)
    * Fix #6813 - Review UI for OpenVPN section
    
    * Simplify after review

--- src/app/vpn/directives/openVpnSection.js
+++ src/app/vpn/directives/openVpnSection.js
@@ -1,41 +1,71 @@
 /* @ngInject */
 function openVpnSection(vpnModel, changeVPNNameModal, changeVPNPasswordModal) {
+    const SHOW_PASSWORD_CLASS = 'openVpnSection-show-password';
+    const togglePassword = (element) => element.classList.toggle(SHOW_PASSWORD_CLASS);
+    const changeName = (scope) => {
+        const params = {
+            name: scope.VPNName,
+            close(newName) {
+                if (newName) {
+                    scope.$applyAsync(() => {
+                        scope.VPNName = newName;
+                    });
+                }
+                changeVPNNameModal.deactivate();
+            }
+        };
+        changeVPNNameModal.activate({ params });
+    };
+
+    const changePassword = (scope) => {
+        const params = {
+            password: scope.VPNPassword,
+            close(newPassword) {
+                if (newPassword) {
+                    scope.$applyAsync(() => {
+                        scope.VPNPassword = newPassword;
+                    });
+                }
+                changeVPNPasswordModal.deactivate();
+            }
+        };
+        changeVPNPasswordModal.activate({ params });
+    };
+
     return {
         restrict: 'E',
         replace: true,
         scope: {},
         templateUrl: require('../../../templates/vpn/openVpnSection.tpl.html'),
-        link(scope) {
+        link(scope, el) {
             const { Name, Password } = vpnModel.get();
 
             scope.VPNName = Name;
             scope.VPNPassword = Password;
 
-            scope.changeName = () => {
-                const params = {
-                    name: scope.VPNName,
-                    close(newName) {
-                        if (newName) {
-                            scope.VPNName = newName;
-                        }
-                        changeVPNNameModal.deactivate();
-                    }
-                };
-                changeVPNNameModal.activate({ params });
+            const onClick = ({ target }) => {
+                const action = target.getAttribute('data-action');
+
+                switch (action) {
+                    case 'changeName':
+                        changeName(scope);
+                        break;
+                    case 'changePassword':
+                        changePassword(scope);
+                        break;
+                    case 'togglePassword':
+                        togglePassword(el[0]);
+                        break;
+                    default:
+                        break;
+                }
             };
 
-            scope.changePassword = () => {
-                const params = {
-                    password: scope.VPNPassword,
-                    close(newPassword) {
-                        if (newPassword) {
-                            scope.VPNPassword = newPassword;
-                        }
-                        changeVPNPasswordModal.deactivate();
-                    }
-                };
-                changeVPNPasswordModal.activate({ params });
-            };
+            el.on('click', onClick);
+
+            scope.$on('$destroy', () => {
+                el.off('click', onClick);
+            });
         }
     };
 }
