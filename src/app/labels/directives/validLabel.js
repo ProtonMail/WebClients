e13aab6e0b8dfb32f2cd596149c60178d5434473
dhoko <dhoko@users.noreply.github.com>
2018-04-19 14:40:11 +0200

    Fix #6854 ~ Forbid <> for a label when we edit too (#6855)

--- src/app/labels/directives/validLabel.js
+++ src/app/labels/directives/validLabel.js
@@ -1,23 +1,12 @@
 /* @ngInject */
 function validLabel() {
-    const CACHE = { index: 0 };
-
     /**
-     * Forbid <> for a new input value
-     * Allow <> for an edit, because your label may already contain one
-     * As ngModel.$modelValue is not reliable we store it to detect the edit
+     * Forbid <> for a label
      * @param  {ngModel} ngModel
      * @return {Boolean}
      */
     const validator = (ngModel) => (input) => {
-        if (!CACHE.index) {
-            CACHE.index++;
-            CACHE.value = ngModel.$modelValue;
-            ngModel.$error.validLabel = !!CACHE.value;
-            return ngModel.$error.validLabel;
-        }
-
-        ngModel.$error.validLabel = !CACHE.value ? !/<|>/.test(input) : true;
+        ngModel.$error.validLabel = !/<|>/.test(input || '');
         return ngModel.$error.validLabel;
     };
 
@@ -26,10 +15,6 @@ function validLabel() {
         restrict: 'A',
         link(scope, el, attr, ngModel) {
             ngModel.$validators.validLabel = validator(ngModel);
-            scope.$on('$destroy', () => {
-                delete CACHE.value;
-                CACHE.index = 0;
-            });
         }
     };
 }
