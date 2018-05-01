ed6e983191cafbea5103477f933c83cc36871ccf
Richard <EpokK@users.noreply.github.com>
2018-05-01 20:37:56 +0200

    Review message extra (#6935)
    * Review message extra
    
    * Add min height for each section

---
+++ src/app/message/directives/messageMimeParsing.js
@@ -0,0 +1,10 @@
+/* @ngInject */
+function messageMimeParsing() {
+    return {
+        replace: true,
+        restrict: 'E',
+        templateUrl: require('../../../templates/message/messageMimeParsing.tpl.html')
+    };
+}
+
+export default messageMimeParsing;
