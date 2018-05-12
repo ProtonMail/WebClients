c3fd0d940cb516e67ef4bce4a417ab41afd18685
Richard <richard@protonmail.com>
2018-05-12 19:27:21 +0200

    Fix #6983 - Time can change when a draft become a sent message

--- src/app/utils/directives/timeRefreshed.js
+++ src/app/utils/directives/timeRefreshed.js
@@ -1,25 +1,29 @@
 /* @ngInject */
-function timeRefreshed($filter) {
+function timeRefreshed($filter, dispatchers) {
     const getDate = (time = 0) => {
         const value = +time * 1000;
         return new Date(value || Date.now()).toISOString();
     };
 
     return {
+        restrict: 'A',
         link(scope, element, { timeRefreshed, timeInterval = 1000, timeFilter = 'delay' }) {
+            const { on, unsubscribe } = dispatchers();
             const isTime = element[0].nodeName === 'TIME';
             const filter = $filter(timeFilter);
             isTime && element[0].setAttribute('datetime', getDate(timeRefreshed));
+            const updateTime = () => element.text(filter(timeRefreshed));
             const id = setInterval(updateTime, timeInterval);
-
-            function updateTime() {
-                element.text(filter(timeRefreshed));
-            }
 
             updateTime();
 
+            on('elements', (e, { type }) => {
+                type === 'refresh.time' && updateTime();
+            });
+
             scope.$on('$destroy', () => {
                 clearInterval(id);
+                unsubscribe();
             });
         }
     };
