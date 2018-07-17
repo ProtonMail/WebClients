ProtonMail/WebClients

commit 26578b178476248964530f67c1717593909cfdbf

Refresh post default cache total + fix pagination if messages (#7177)

src/app/conversation/services/cache.js
@@ -317,8 +317,6 @@ function cache(
                 if (data.Code === INVALID_SEARCH_ERROR_CODE) {
                     return [];
                 }
-                cacheCounters.currentState(data.Limit);
-                $rootScope.$emit('elements', { type: 'setLimit', data: { limit: data.Limit, total: data.Total } });
 
                 _.each(data.Conversations, (conversation) => {
                     conversation.loaded = true; // Mark these conversations as loaded
@@ -341,7 +339,7 @@ function cache(
 
                     // Store conversations
                     storeConversations(data.Conversations);
-
+                    refreshStateLimit(data);
                     api.clearDispatcher();
                     // Return conversations ordered
                     return api.orderConversation(data.Conversations.slice(0, ELEMENTS_PER_PAGE), loc);
@@ -359,6 +357,11 @@ function cache(
         networkActivityTracker.track(promise);
 
         return promise;
+    }
+
+    function refreshStateLimit({ Limit: limit, Total: total }) {
+        cacheCounters.currentState(limit);
+        $rootScope.$emit('elements', { type: 'setLimit', data: { limit, total } });
     }
 
     /**
@@ -379,9 +382,7 @@ function cache(
                 if (data.Code === INVALID_SEARCH_ERROR_CODE) {
                     return [];
                 }
-                const { Messages = [], Total = 0, Limit = 0 } = data;
-                cacheCounters.currentState(Limit);
-                $rootScope.$emit('elements', { type: 'setLimit', data: { limit: Limit, total: Total } });
+                const { Messages = [], Limit = 0 } = data;
 
                 _.each(Messages, (message) => {
                     const { ToList = [], CCList = [], BCCList = [] } = message;
@@ -390,7 +391,6 @@ function cache(
                     message.Recipients = _.uniq([].concat(ToList, CCList, BCCList));
                 });
 
-                // Store messages
                 storeMessages(Messages);
 
                 // Only for cache context
@@ -404,9 +404,10 @@ function cache(
                     pages.forEach((page) => !cachePages.inside(page) && cachePages.add(page));
                     // Return messages ordered
                     api.clearDispatcher();
+                    refreshStateLimit(data);
                     return api.orderMessage(Messages.slice(0, ELEMENTS_PER_PAGE));
                 }
-
+                refreshStateLimit(data);
                 api.clearDispatcher();
                 return Messages.slice(0, ELEMENTS_PER_PAGE);
             })
@@ -635,11 +636,11 @@ function cache(
                     number = ELEMENTS_PER_PAGE;
                 }
 
-                cacheCounters.currentState(total);
                 messages = messages.slice(start, end);
 
                 // Supposed total equal to the total cache?
                 if (messages.length === number) {
+                    cacheCounters.currentState(total);
                     return Promise.resolve(messages);
                 }
             }
@@ -692,10 +693,10 @@ function cache(
                     number = ELEMENTS_PER_PAGE;
                 }
 
-                cacheCounters.currentState(total);
                 conversations = conversations.slice(start, end);
                 // Supposed total equal to the total cache?
                 if (conversations.length === number) {
+                    cacheCounters.currentState(total);
                     return Promise.resolve(conversations);
                 }
             }

src/app/elements/controllers/elements.js
@@ -407,6 +407,7 @@ function ElementsController(
     $scope.refreshElements = () => {
         const request = forgeRequestParameters($scope.mailbox);
         const type = tools.getTypeList();
+
         const promise = type === 'message' ? cache.queryMessages(request) : cache.queryConversations(request);
 
         return promise.then(

src/app/paginator/factories/paginationModel.js
@@ -1,22 +1,13 @@
-import _ from 'lodash';
-
-import { ELEMENTS_PER_PAGE, MESSAGE_VIEW_MODE } from '../../constants';
+import { ELEMENTS_PER_PAGE } from '../../constants';
 
 /* @ngInject */
-function paginationModel($injector, dispatchers, $state, $stateParams, mailSettingsModel, tools) {
+function paginationModel($injector, dispatchers, $state, $stateParams, tools) {
     const { on } = dispatchers();
     let currentState = '';
 
     on('$stateChangeSuccess', (e, state) => {
         currentState = state.name.replace('.element', '');
     });
-
-    const getLayout = () => {
-        if (mailSettingsModel.get('ViewMode') === MESSAGE_VIEW_MODE) {
-            return 'message';
-        }
-        return 'conversation';
-    };
 
     /**
      * Get the max page where an user can go
@@ -27,9 +18,11 @@ function paginationModel($injector, dispatchers, $state, $stateParams, mailSetti
     const getMaxPage = () => {
         const cacheCounters = $injector.get('cacheCounters');
         const counter = cacheCounters.getCounter(tools.currentLocation());
+
         if (tools.cacheContext() && counter) {
             const key = $stateParams.filter === 'unread' ? 'unread' : 'total';
-            return Math.ceil(counter[getLayout()][key] / ELEMENTS_PER_PAGE);
+            const type = tools.getTypeList();
+            return Math.ceil(counter[type][key] / ELEMENTS_PER_PAGE);
         }
 
         return Math.ceil(cacheCounters.getCurrentState() / ELEMENTS_PER_PAGE);
@@ -40,7 +33,7 @@ function paginationModel($injector, dispatchers, $state, $stateParams, mailSetti
      * @param  {Object} opts    Custom options
      */
     const switchPage = (opts = {}) => {
-        $state.go(currentState, _.extend({ id: null }, opts));
+        $state.go(currentState, { id: null, ...opts });
     };
 
     /**
@@ -49,7 +42,6 @@ function paginationModel($injector, dispatchers, $state, $stateParams, mailSetti
      */
     const previous = () => {
         const pos = ~~$stateParams.page || 0;
-
         if (pos) {
             const page = pos - 1;
             // If page = 1 remove it from the url
@@ -62,9 +54,9 @@ function paginationModel($injector, dispatchers, $state, $stateParams, mailSetti
      * @return {void}
      */
     const next = () => {
-        const pos = ~~$stateParams.page || 0;
+        const pos = ~~$stateParams.page || 1;
         const page = pos + 1;
-        page <= getMaxPage() + 1 && switchPage({ page });
+        page <= getMaxPage() && switchPage({ page });
     };
 
     /**
@@ -73,8 +65,8 @@ function paginationModel($injector, dispatchers, $state, $stateParams, mailSetti
      * @return {Boolean}
      */
     const isMax = () => {
-        const pos = ~~$stateParams.page || 0;
-        return pos >= getMaxPage() + 1;
+        const page = ~~$stateParams.page || 1;
+        return page >= getMaxPage();
     };
 
     const init = angular.noop;

test/specs/core/services/paginationModel.spec.js
@@ -9,18 +9,17 @@ describe('paginationModel factory', () => {
     const MODULE = generateModuleName();
     const stateParamsMock = {};
 
-    const mailSettingsMock = { ViewMode: 0 };
-    const mailSettingsModel = { get: (k) => mailSettingsMock[k] };
-
-    const state = { go: _.noop };
+    const state = { go: _.noop, is: _.noop };
     const cacheCounters = {
         getCurrentState: _.noop,
-        getCounter: _.noop
+        getCounter: _.noop,
+        getTypeList: _.noop
     };
 
     const tools = {
         currentLocation: _.noop,
-        cacheContext: _.noop
+        cacheContext: _.noop,
+        getTypeList: _.noop
     };
 
     let factory;
@@ -35,7 +34,7 @@ describe('paginationModel factory', () => {
     beforeEach(angular.mock.inject(($injector) => {
         rootScope = $injector.get('$rootScope');
         const dispatchers = dispatchersService(rootScope);
-        factory = service($injector, dispatchers, state, stateParamsMock, mailSettingsModel, tools);
+        factory = service($injector, dispatchers, state, stateParamsMock, tools);
     }));
 
     describe('Switch page', () => {
@@ -83,11 +82,16 @@ describe('paginationModel factory', () => {
         describe('No counters, not a cache context', () => {
             let total;
             beforeEach(() => {
+                spyOn(tools, 'getTypeList');
                 spyOn(tools, 'cacheContext').and.returnValue(false);
                 spyOn(tools, 'currentLocation').and.returnValue('ici');
                 spyOn(cacheCounters, 'getCounter').and.returnValue(0);
                 spyOn(cacheCounters, 'getCurrentState').and.returnValue(10);
                 total = factory.getMaxPage();
+            });
+
+            it('should not check the type of messages', () => {
+                expect(tools.getTypeList).not.toHaveBeenCalled();
             });
 
             it('should get check the cache context', () => {
@@ -114,12 +118,19 @@ describe('paginationModel factory', () => {
         describe('No counters, and a cache context', () => {
             let total;
             beforeEach(() => {
+                spyOn(tools, 'getTypeList');
                 spyOn(tools, 'cacheContext').and.returnValue(true);
                 spyOn(tools, 'currentLocation').and.returnValue('ici');
                 spyOn(cacheCounters, 'getCounter').and.returnValue(0);
                 spyOn(cacheCounters, 'getCurrentState').and.returnValue(10);
                 total = factory.getMaxPage();
             });
+
+
+            it('should not check the type of messages', () => {
+                expect(tools.getTypeList).not.toHaveBeenCalled();
+            });
+
 
             it('should check the cache context', () => {
                 expect(tools.cacheContext).toHaveBeenCalledTimes(1);
@@ -146,12 +157,18 @@ describe('paginationModel factory', () => {
         describe('Counters, and not a cache context', () => {
             let total;
             beforeEach(() => {
+                spyOn(tools, 'getTypeList');
                 spyOn(tools, 'cacheContext').and.returnValue(false);
                 spyOn(tools, 'currentLocation').and.returnValue('ici');
                 spyOn(cacheCounters, 'getCounter').and.returnValue(100);
                 spyOn(cacheCounters, 'getCurrentState').and.returnValue(10);
                 total = factory.getMaxPage();
             });
+
+            it('should not check the type of messages', () => {
+                expect(tools.getTypeList).not.toHaveBeenCalled();
+            });
+
 
             it('should check the cache context', () => {
                 expect(tools.cacheContext).toHaveBeenCalledTimes(1);
@@ -179,7 +196,7 @@ describe('paginationModel factory', () => {
             let total;
 
             beforeEach(() => {
-                mailSettingsMock.ViewMode = CONVERSATION_VIEW_MODE;
+                spyOn(tools, 'getTypeList').and.returnValue('conversation');
                 spyOn(tools, 'cacheContext').and.returnValue(true);
                 spyOn(tools, 'currentLocation').and.returnValue('ici');
                 spyOn(cacheCounters, 'getCounter').and.returnValue({
@@ -197,6 +214,10 @@ describe('paginationModel factory', () => {
                 beforeEach(() => {
                     stateParamsMock.filter = 'unread';
                     total = factory.getMaxPage();
+                });
+
+                it('should not check the type of list', () => {
+                    expect(tools.getTypeList).toHaveBeenCalledTimes(1);
                 });
 
                 it('should check the cache context', () => {
@@ -226,6 +247,11 @@ describe('paginationModel factory', () => {
                 beforeEach(() => {
                     stateParamsMock.filter = 'total';
                     total = factory.getMaxPage();
+                });
+
+
+                it('should not check the type of list', () => {
+                    expect(tools.getTypeList).toHaveBeenCalledTimes(1);
                 });
 
                 it('should check the cache context', () => {
@@ -255,7 +281,8 @@ describe('paginationModel factory', () => {
             let total;
 
             beforeEach(() => {
-                mailSettingsMock.ViewMode = MESSAGE_VIEW_MODE;
+                spyOn(tools, 'getTypeList').and.returnValue('message');
+
                 spyOn(tools, 'cacheContext').and.returnValue(true);
                 spyOn(tools, 'currentLocation').and.returnValue('ici');
                 spyOn(cacheCounters, 'getCounter').and.returnValue({
@@ -275,6 +302,9 @@ describe('paginationModel factory', () => {
                     total = factory.getMaxPage();
                 });
 
+                it('should not check the type of list', () => {
+                    expect(tools.getTypeList).toHaveBeenCalledTimes(1);
+                });
                 it('should check the cache context', () => {
                     expect(tools.cacheContext).toHaveBeenCalledTimes(1);
                 });
@@ -302,6 +332,10 @@ describe('paginationModel factory', () => {
                 beforeEach(() => {
                     stateParamsMock.filter = 'total';
                     total = factory.getMaxPage();
+                });
+
+                it('should not check the type of list', () => {
+                    expect(tools.getTypeList).toHaveBeenCalledTimes(1);
                 });
 
                 it('should check the cache context', () => {
@@ -333,7 +367,7 @@ describe('paginationModel factory', () => {
                 let total;
 
                 beforeEach(() => {
-                    mailSettingsMock.ViewMode = MESSAGE_VIEW_MODE;
+                    spyOn(tools, 'getTypeList').and.returnValue('message');
                     spyOn(tools, 'cacheContext').and.returnValue(true);
                     spyOn(tools, 'currentLocation').and.returnValue('ici');
                     spyOn(cacheCounters, 'getCounter').and.returnValue({

commit d0b8649584e54fc1cdce3fd2a4b940e79f50c56b

Fix #7204 - Set Limit and Total for every context
Bug introduced by https://github.com/ProtonMail/Angular/commit/26578b178476248964530f67c1717593909cfdbf

src/app/conversation/services/cache.js
@@ -318,6 +318,8 @@ function cache(
                     return [];
                 }
 
+                refreshStateLimit(data);
+
                 _.each(data.Conversations, (conversation) => {
                     conversation.loaded = true; // Mark these conversations as loaded
                     storeTime(conversation.ID, loc, conversation.ContextTime); // Store time value
@@ -339,7 +341,6 @@ function cache(
 
                     // Store conversations
                     storeConversations(data.Conversations);
-                    refreshStateLimit(data);
                     api.clearDispatcher();
                     // Return conversations ordered
                     return api.orderConversation(data.Conversations.slice(0, ELEMENTS_PER_PAGE), loc);
@@ -384,6 +385,8 @@ function cache(
                 }
                 const { Messages = [], Limit = 0 } = data;
 
+                refreshStateLimit(data);
+
                 _.each(Messages, (message) => {
                     const { ToList = [], CCList = [], BCCList = [] } = message;
                     message.loaded = true;
@@ -404,10 +407,9 @@ function cache(
                     pages.forEach((page) => !cachePages.inside(page) && cachePages.add(page));
                     // Return messages ordered
                     api.clearDispatcher();
-                    refreshStateLimit(data);
                     return api.orderMessage(Messages.slice(0, ELEMENTS_PER_PAGE));
                 }
-                refreshStateLimit(data);
+
                 api.clearDispatcher();
                 return Messages.slice(0, ELEMENTS_PER_PAGE);
             })