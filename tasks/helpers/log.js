f29d3b0aea3056ee4e258495736753f0e05bb1fd
dhoko <aurelien@protonmail.com>
2018-07-20 17:51:18 +0200

    Hotfix error 33 log

--- tasks/helpers/log.js
+++ tasks/helpers/log.js
@@ -5,7 +5,7 @@ const warn = (msg) => {
     console.log(`${chalk.magenta('⚠')} ${chalk.magenta(msg)}.`);
     console.log();
 };
-const success = (msg, { time }) => {
+const success = (msg, { time } = {}) => {
     const txt = chalk.green(` ${chalk.bold('✓')} ${msg} `);
     const message = [txt, time && `(${time})`].filter(Boolean).join('');
     console.log();
