77c3c2a2709fdb41b02a7d8da291dca617a7fb18
Richard <richard@protonmail.com>
2018-08-03 14:44:34 +0200

    Hotfix - Custom theme was not working

--- src/app/ui/directives/customTheme.js
+++ src/app/ui/directives/customTheme.js
@@ -1,6 +1,14 @@
 /* @ngInject */
-const customTheme = () => ({
+const customTheme = (dispatchers, mailSettingsModel) => ({
     replace: true,
-    template: '<style id="customTheme">{{ user.Theme }}</style>'
+    template: '<style id="customTheme"></style>',
+    link(scope, el) {
+        const { on, unsubscribe } = dispatchers();
+        const update = () => (el[0].textContent = mailSettingsModel.get('Theme'));
+
+        on('mailSettings', update);
+        update();
+        scope.$on('$destroy', unsubscribe);
+    }
 });
 export default customTheme;
