86b03c75e7a36058204740a5c84e5ecaa0fd0daa
dhoko <dhoko@users.noreply.github.com>
2018-08-31 16:28:37 +0200

    Fix #7395 ~ Better date when we build the app (#7396)

--- env/configDefault.js
+++ env/configDefault.js
@@ -7,7 +7,7 @@ i18nLoader.set(i18n);
 module.exports = {
     app_version: PACKAGE.version,
     api_version: '3',
-    date_version: new Date().toDateString(),
+    date_version: new Date().toGMTString(),
     year: new Date().getFullYear(),
     clientID: 'Web',
     clientSecret: '4957cc9a2e0a2a49d02475c9d013478d',
