1288baae23fe113659e57a0bf3ee0dc809a80a2b
Richard <richard@protonmail.com>
2018-09-03 19:02:02 +0200

    Fix #7406 - Custom theme was not injected

--- src/app/ui/directives/customTheme.js
+++ src/app/ui/directives/customTheme.js
@@ -1,13 +1,22 @@
 /* @ngInject */
-const customTheme = (dispatchers, mailSettingsModel) => ({
+const customTheme = (AppModel, dispatchers, mailSettingsModel) => ({
     replace: true,
     template: '<style id="customTheme"></style>',
     link(scope, el) {
         const { on, unsubscribe } = dispatchers();
-        const update = () => (el[0].textContent = mailSettingsModel.get('Theme'));
+        const update = () => {
+            const { isLoggedIn, isLocked } = AppModel.query();
+
+            if (isLoggedIn && !isLocked) {
+                el[0].textContent = mailSettingsModel.get('Theme');
+            }
+        };
 
         on('mailSettings', update);
+        on('AppModel', update);
+
         update();
+
         scope.$on('$destroy', unsubscribe);
     }
 });
