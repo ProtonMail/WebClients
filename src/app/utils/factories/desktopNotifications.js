b8bf51d74411e58726d76bb0be96194060c7861e
dhoko <dhoko@users.noreply.github.com>
2018-09-06 17:11:00 +0200

    Fix #7426 ~ Always check if we can display the notificaiton (#7450)

--- src/app/utils/factories/desktopNotifications.js
+++ src/app/utils/factories/desktopNotifications.js
@@ -1,17 +1,16 @@
 /* @ngInject */
 function desktopNotifications() {
-    const notification = {
-        status() {
-            return Push.Permission.get();
-        },
-        request(onGranted = angular.noop, onDenied = angular.noop) {
-            Push.Permission.request(onGranted, onDenied);
-        },
-        create(title, params) {
-            Push.create(title, params);
-        }
+    const status = () => Push.Permission.get();
+    const isEnabled = () => status() === Push.Permission.GRANTED;
+
+    const request = (onGranted = angular.noop, onDenied = angular.noop) => {
+        Push.Permission.request(onGranted, onDenied);
     };
 
-    return notification;
+    const create = (title, params) => {
+        isEnabled() && Push.create(title, params);
+    };
+
+    return { create, status, isEnabled, request };
 }
 export default desktopNotifications;
