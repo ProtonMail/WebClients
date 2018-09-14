233dff9d59100a2ec0ccd81946b55bc2217e6e6a
dhoko <aurelien@protonmail.com>
2018-09-14 10:40:56 +0200

    Hotifx default config for sentry is empty if does not exit

--- env/config.js
+++ env/config.js
@@ -130,7 +130,7 @@ const sentryConfig = () => {
     }
     const env = getDefaultApiTarget(argv.api);
     process.env.NODE_ENV_SENTRY = env;
-    return SENTRY_CONFIG[env];
+    return SENTRY_CONFIG[env] || {};
 };
 
 const getHostURL = (encoded) => {
