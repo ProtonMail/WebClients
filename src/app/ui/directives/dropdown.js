dcd8dea3a2fcd7e9616982c267405ab504f1a8e4
Richard <EpokK@users.noreply.github.com>
2018-10-08 16:26:30 +0200

    Fix #7594 - Auto-close dropdown when the select an action expect for … (#7596)
    * Fix #7594 - Auto-close dropdown when the select an action expect for label and folder dropdowns
    
    * Review code with @dhoko
    
    Finally I decided to rename the parameter to match with our concerns.

--- src/app/ui/directives/dropdown.js
+++ src/app/ui/directives/dropdown.js
@@ -1,56 +1,60 @@
 /* @ngInject */
-function dropdown($document, AppModel, dispatchers) {
+function dropdown($document, dispatchers) {
     const CLASS_OPEN = 'pm_dropdown-opened';
 
-    return (scope, element) => {
-        const { on, unsubscribe, dispatcher } = dispatchers(['closeDropdown']);
-        const parent = element.parent();
-        const dropdown = parent.find('.pm_dropdown');
+    return {
+        restrict: 'A',
+        scope: {},
+        link(scope, element, { dontAutoClose }) {
+            const { on, unsubscribe, dispatcher } = dispatchers(['closeDropdown']);
+            const parent = element.parent();
+            const dropdown = parent.find('.pm_dropdown');
 
-        // Functions
-        function showDropdown() {
-            element.addClass('active');
-            dropdown.addClass(CLASS_OPEN);
-            $document.on('click', outside);
-        }
+            // Functions
+            function showDropdown() {
+                element.addClass('active');
+                dropdown.addClass(CLASS_OPEN);
+                $document.on('click', outside);
+            }
 
-        function hideDropdown() {
-            element.removeClass('active');
-            dropdown.removeClass(CLASS_OPEN);
-            $document.off('click', outside);
-        }
+            function hideDropdown() {
+                element.removeClass('active');
+                dropdown.removeClass(CLASS_OPEN);
+                $document.off('click', outside);
+            }
 
-        function outside(event) {
-            const isOutsideDropdown = !dropdown[0].contains(event.target);
-            const modalClosed = !AppModel.is('modalOpen');
+            function outside() {
+                if (dontAutoClose) {
+                    return;
+                }
 
-            if (isOutsideDropdown && modalClosed) {
                 dispatcher.closeDropdown('close');
             }
-        }
 
-        function click() {
-            const wasOpen = element.hasClass('active');
-            // Close all dropdowns
-            dispatcher.closeDropdown('close');
-            if (!wasOpen) {
-                // Open only this one
-                showDropdown();
+            function click() {
+                const wasOpen = element.hasClass('active');
+                // Close all dropdowns
+                dispatcher.closeDropdown('close');
+
+                if (!wasOpen) {
+                    // Open only this one
+                    showDropdown();
+                }
+
+                return false;
             }
 
-            return false;
+            // Listeners
+            element.on('click', click);
+
+            on('closeDropdown', hideDropdown);
+
+            scope.$on('$destroy', () => {
+                element.off('click', click);
+                hideDropdown();
+                unsubscribe();
+            });
         }
-
-        // Listeners
-        element.on('click', click);
-
-        on('closeDropdown', hideDropdown);
-
-        scope.$on('$destroy', () => {
-            element.off('click', click);
-            hideDropdown();
-            unsubscribe();
-        });
     };
 }
 export default dropdown;
