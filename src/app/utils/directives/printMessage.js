03fb4291cc80c3343701b560e726a76c167a7c8a
Richard <EpokK@users.noreply.github.com>
2018-10-16 14:00:48 +0200

    Hotfix print rendering issue reported by @RandomAdversary (#7657)
    * Hotfix print rendering issue reported by @RandomAdversary
    
    https://github.com/ProtonMail/Angular/pull/7621#issuecomment-430166078
    
    * Clean content to make sure there is no bad injection that we don't control
    
    * Hotfix
    
    * Use constant
    
    Review with @mmso

--- src/app/utils/directives/printMessage.js
+++ src/app/utils/directives/printMessage.js
@@ -1,5 +1,10 @@
+import { MIME_TYPES } from '../../constants';
+
+const { PLAINTEXT } = MIME_TYPES;
+
 /* @ngInject */
-function printMessage(printMessageModel) {
+function printMessage(printMessageModel, sanitize) {
+    const isPlain = ({ MIMEType } = {}) => MIMEType === PLAINTEXT;
     return {
         scope: {
             config: '=printMessageConfig'
@@ -7,9 +12,13 @@ function printMessage(printMessageModel) {
         replace: true,
         restrict: 'E',
         templateUrl: require('../../../templates/utils/printMessage.tpl.html'),
-        link(scope) {
+        link(scope, el) {
+            const $content = el.find('.printMessage-content');
+
             scope.model = printMessageModel(scope.config);
-            scope.message = scope.config.message;
+
+            $content[0].innerHTML = sanitize.message(scope.config.content); // Can't use ng-bind-html because it removes style
+            isPlain(scope.model) && $content.addClass('printMessage-plain-content');
         }
     };
 }
