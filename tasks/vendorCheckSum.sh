92e6cd2f8c38c5f776fdffc6df8910158f5ac619
dhoko <dhoko@users.noreply.github.com>
2018-10-24 10:13:10 +0200

    Fix #7763 ~ Compute hash for vendor + app only post replace (#7764)

--- tasks/vendorCheckSum.sh
+++ tasks/vendorCheckSum.sh
@@ -3,55 +3,111 @@ set -eo pipefail
 
 COMMAND=./node_modules/.bin/uglifyjs;
 
+#
+#  Process
+#  1. Minify vendor*
+#  2. Compute hash for
+#       - openpgp
+#       - openpgp.worker
+#       - appLazy
+#       - styles
+#  3. Rename styles, vendorLazy* + sourcemaps
+#  4. Edit in place hashes
+#       - hash for the sourcemap inside appLazy
+#       - hash for openpgp inside openpgp.worker
+#       - hash for openpgp.worker inside vendor
+#  5. Compute new hash for
+#       - vendor
+#  6. Edit in place hashes
+#       - hash for vendors* inside app.js
+#       - hash for appLazy inside app.js
+#  7. Bind assets inside index.html
+#  8. Rename files
+#  9. Done
+#
+
+# Edit in place hash for items
+function replace {
+    # Mac OS non-standard
+    if [[ "$OSTYPE" = "darwin"* ]]; then
+        sed -i '' $1 $2
+    else
+        sed -i $1 $2
+    fi
+}
+
+function minify {
+    if [ -e "./dist/$1.js" ]; then
+        echo " → minify $1";
+        $COMMAND --compress --mangle -o dist/$1.js -- dist/$1.js &
+    fi
+}
+
 echo " → Minify vendors + vendorsLazy"
 
-$COMMAND --compress --mangle -o dist/vendor.js -- dist/vendor.js &
-$COMMAND --compress --mangle -o dist/vendorLazy.js -- dist/vendorLazy.js &
-$COMMAND --compress --mangle -o dist/vendorLazy2.js -- dist/vendorLazy2.js &
+minify vendor
+minify vendorLazy
+minify vendorLazy2
 wait
+
 echo " ✓ Minify vendors + vendorsLazy success"
 echo
 echo " → Compute shasum for bundles"
 
-HASH_APP=$(shasum dist/app.js | awk '{print  $1}');
 HASH_APP_LAZY=$(shasum dist/appLazy.js | awk '{print  $1}');
-HASH_VENDOR=$(shasum dist/vendor.js | awk '{print  $1}');
 HASH_VENDOR_LAZY=$(shasum dist/vendorLazy.js | awk '{print  $1}');
 HASH_VENDOR_LAZY2=$(shasum dist/vendorLazy2.js | awk '{print  $1}');
 HASH_STYLE=$(shasum dist/styles.css | awk '{print  $1}');
+
+# No need to recompute them later post edit in place as it comes with the same version of openpgp.
 HASH_OPENPGP=$(shasum dist/openpgp.min.js | awk '{print  $1}');
 HASH_WORKER=$(shasum dist/openpgp.worker.min.js | awk '{print  $1}');
 
-echo HASH_STYLE $HASH_STYLE
+
+echo " → Replace shasum in place"
+
+replace "s/appLazy.js.map/appLazy.$HASH_APP_LAZY.js.map/g;" dist/appLazy.js;
+replace "s/openpgp.min.js/openpgp.$HASH_OPENPGP.js/g;"  dist/openpgp.worker.min.js
+replace "s/openpgp.worker.min.js/openpgp.worker.$HASH_WORKER.js/g;" dist/vendor.js
+
+echo " → Checksum for app + vendor"
+
+HASH_VENDOR=$(shasum dist/vendor.js | awk '{print  $1}');
+
+# Replace assets for lazyLoad inside the app.js. Single source of truth
+replace "s/vendorLazy.js/vendorLazy.$HASH_VENDOR_LAZY.js/g;s/vendorLazy2.js/vendorLazy2.$HASH_VENDOR_LAZY2.js/g;s/appLazy.js/appLazy.$HASH_APP_LAZY.js/g;" dist/app.js;
+
+# No more updates, let's create its hash
+HASH_APP=$(shasum dist/app.js | awk '{print  $1}');
+
+replace "s/app.js.map/app.$HASH_APP.js.map/g;" dist/app.js;
+
+# Last step update index.html with assets
+replace "s/app.js/app.$HASH_APP.js/g;s/appLazy.js/appLazy.$HASH_APP_LAZY.js/g;s/styles.css/styles.$HASH_STYLE.css/g;s/vendor.js/vendor.$HASH_VENDOR.js/g;s/vendorLazy.js/vendorLazy.$HASH_VENDOR_LAZY.js/g;s/vendorLazy2.js/vendorLazy2.$HASH_VENDOR_LAZY2.js/g;s/openpgp.min.js/openpgp.$HASH_OPENPGP.js/g;" dist/index.html;
+
+echo " → Write new files"
+echo
+
+mv dist/appLazy.js dist/appLazy.$HASH_APP_LAZY.js
+mv dist/appLazy.js.map dist/appLazy.$HASH_APP_LAZY.js.map
+mv dist/vendorLazy.js dist/vendorLazy.$HASH_VENDOR_LAZY.js
+mv dist/vendorLazy2.js dist/vendorLazy2.$HASH_VENDOR_LAZY2.js
+mv dist/styles.css dist/styles.$HASH_STYLE.css
+mv dist/openpgp.min.js dist/openpgp.$HASH_OPENPGP.js
+mv dist/openpgp.worker.min.js dist/openpgp.worker.$HASH_WORKER.js
+mv dist/vendor.js dist/vendor.$HASH_VENDOR.js
+mv dist/app.js dist/app.$HASH_APP.js
+mv dist/app.js.map dist/app.$HASH_APP.js.map
+rm dist/html.js
+
 echo HASH_APP $HASH_APP
-echo HASH_APP_LAZY $HASH_APP_LAZY
 echo HASH_VENDOR $HASH_VENDOR
+echo HASH_STYLE $HASH_STYLE
+echo HASH_APP_LAZY $HASH_APP_LAZY
 echo HASH_VENDOR_LAZY $HASH_VENDOR_LAZY
 echo HASH_VENDOR_LAZY2 $HASH_VENDOR_LAZY2
 echo HASH_OPENPGP $HASH_OPENPGP
 echo HASH_WORKER $HASH_WORKER
 
 echo
-echo " → Attach shasum to files"
-mv dist/index.html dist/app.html
-mv dist/app.js.map dist/app.$HASH_APP.js.map
-mv dist/appLazy.js.map dist/appLazy.$HASH_APP_LAZY.js.map
-mv dist/vendorLazy.js dist/vendorLazy.$HASH_VENDOR_LAZY.js
-mv dist/vendorLazy2.js dist/vendorLazy2.$HASH_VENDOR_LAZY2.js
-mv dist/styles.css dist/styles.$HASH_STYLE.css
-mv dist/openpgp.min.js dist/openpgp.$HASH_OPENPGP.js
-
-echo " → Write shasum"
-
-sed -e "s/app.js/app.$HASH_APP.js/g;s/appLazy.js/appLazy.$HASH_APP_LAZY.js/g;s/styles.css/styles.$HASH_STYLE.css/g;s/vendor.js/vendor.$HASH_VENDOR.js/g;s/vendorLazy.js/vendorLazy.$HASH_VENDOR_LAZY.js/g;s/vendorLazy2.js/vendorLazy2.$HASH_VENDOR_LAZY2.js/g;s/openpgp.min.js/openpgp.$HASH_OPENPGP.js/g;" < dist/app.html > dist/index.html;
-sed -e "s/vendorLazy.js/vendorLazy.$HASH_VENDOR_LAZY.js/g;s/vendorLazy2.js/vendorLazy2.$HASH_VENDOR_LAZY2.js/g;s/appLazy.js/appLazy.$HASH_APP_LAZY.js/g;s/app.js.map/app.$HASH_APP.js.map/g;" < dist/app.js > dist/app.$HASH_APP.js;
-sed -e "s/appLazy.js.map/appLazy.$HASH_APP_LAZY.js.map/g;" < dist/appLazy.js > dist/appLazy.$HASH_APP_LAZY.js;
-sed -e "s/openpgp.min.js/openpgp.$HASH_OPENPGP.js/g;" < dist/openpgp.worker.min.js > dist/openpgp.worker.$HASH_WORKER.js
-sed -e "s/openpgp.worker.min.js/openpgp.worker.$HASH_WORKER.js/g;" < dist/vendor.js > dist/vendor.$HASH_VENDOR.js
-
-# if it doesn't find the ref inside vendor it won't create the file
-if [ -d dist/vendor.js ]; then mv dist/vendor.js dist/vendor.$HASH_VENDOR.js; fi
-
-rm dist/{app.html,app.js,appLazy.js,html.js,openpgp.worker.min.js};
 echo " ✓ Write shasum success"
-echo
