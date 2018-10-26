c6bc6dc5b22e89ad42d882cfa65c8c95c67adcf4
dhoko <dhoko@users.noreply.github.com>
2018-10-26 11:20:03 +0200

    Fix #7777 ~ Add overflow for the autocomplete (#7778)
    * Fix #7777 ~ Add overflow for the autocomplete
    
    * Remove outline on up/down arrow
    
    * Fix tooltip position + remove tabs

--- src/app/utils/helpers/tooltipHelper.js
+++ src/app/utils/helpers/tooltipHelper.js
@@ -1,9 +1,17 @@
-const tooltipModel = (reference, options) =>
-    new Tooltip(reference, {
+const tooltipModel = (reference, options) => {
+    return new Tooltip(reference, {
         container: 'body',
         placement: 'top',
-        trigger: 'hover', // The default value for trigger is 'hover focus'
+        trigger: 'hover', // The default value for trigger is 'hover focus',
+        popperOptions: {
+            modifiers: {
+                preventOverflow: {
+                    boundariesElement: 'viewport'
+                }
+            }
+        },
         ...options
     });
+};
 
 export default tooltipModel;
