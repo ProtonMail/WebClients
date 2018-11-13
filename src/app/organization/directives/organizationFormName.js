fe8fb97b36606c62eba2056fec10a68af3e08436
Richard <richard@protonmail.com>
2018-11-13 11:45:17 +0100

    Fix #7898 - Organization name was not updated

--- src/app/organization/directives/organizationFormName.js
+++ src/app/organization/directives/organizationFormName.js
@@ -1,22 +1,32 @@
 /* @ngInject */
-function organizationFormName(organizationModel) {
+function organizationFormName(dispatchers, organizationModel) {
     return {
         replace: true,
         scope: {},
         templateUrl: require('../../../templates/organization/organizationFormName.tpl.html'),
         link(scope, el) {
-            const { DisplayName } = organizationModel.get() || {};
-            scope.value = DisplayName;
+            const { on, unsubscribe } = dispatchers();
+            const { DisplayName = '' } = organizationModel.get() || {};
+            const updateName = (organizationName = '') => (scope.value = organizationName);
 
             const onSubmit = (e) => {
                 e.preventDefault();
                 scope.organizationForm.$valid && organizationModel.saveName(scope.value);
             };
 
+            on('organizationChange', (event, { data: newOrganization }) => {
+                scope.$applyAsync(() => {
+                    updateName(newOrganization.DisplayName);
+                });
+            });
+
             el.on('submit', onSubmit);
+
+            updateName(DisplayName);
 
             scope.$on('$destroy', () => {
                 el.off('submit', onSubmit);
+                unsubscribe();
             });
         }
     };
