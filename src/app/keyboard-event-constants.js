c2de6f6d92f2b4178472e22be1b9557ca9f69ecc
Nicolas Hoffmann <dante3333@gmail.com>
2018-11-20 12:50:17 +0100

    Fix #7964 - buttons focusable/improve speech (#7973)
    * Fix #7964 - buttons focusable/improve speech
    
    * Fix #7964 - updated syntax
    
    * Add constants for keyboard events
    
    * Update dropdown with constants
    
    * Add missing injector
    
    * Ignore new config file

---
+++ src/app/keyboard-event-constants.js
@@ -0,0 +1,5 @@
+export const ESCAPE_KEY = 'Escape';
+export const ESCAPE_KEY_OLD = 'Esc';
+export const RETURN_KEY = 'Enter';
+export const SPACE_KEY = ' ';
+export const SPACE_KEY_OLD = 'Spacebar';
