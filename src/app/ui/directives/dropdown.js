c9641e39afc3b9b2a8f46223b4c7b8b1fd96ee98
Nicolas Hoffmann <dante3333@gmail.com>
2018-11-20 12:50:17 +0100

    Fix #7964 - buttons focusable/improve speech (#7973)
    * Fix #7964 - buttons focusable/improve speech
    
    * Fix #7964 - updated syntax
    
    * Add constants for keyboard events
    
    * Update dropdown with constants
    
    * Add missing injector
    
    * Ignore new config file

--- src/app/ui/directives/dropdown.js
+++ src/app/ui/directives/dropdown.js
@@ -1,6 +1,8 @@
+import { ESCAPE_KEY, ESCAPE_KEY_OLD, RETURN_KEY, SPACE_KEY, SPACE_KEY_OLD } from '../../keyboard-event-constants';
 /* @ngInject */
 function dropdown($document, dispatchers) {
     const CLASS_OPEN = 'pm_dropdown-opened';
+    const ATTR_EXPANDED = 'aria-expanded';
 
     return {
         restrict: 'A',
@@ -13,6 +15,7 @@ function dropdown($document, dispatchers) {
 
             function showDropdown() {
                 element.addClass('active');
+                element.attr(ATTR_EXPANDED, 'true');
                 dropdown.addClass(CLASS_OPEN);
                 $document.on('click', outside);
                 dispatcher.dropdown('show', { type: dropdownType });
@@ -20,6 +23,7 @@ function dropdown($document, dispatchers) {
 
             function hideDropdown() {
                 element.removeClass('active');
+                element.attr(ATTR_EXPANDED, 'false');
                 dropdown.removeClass(CLASS_OPEN);
                 $document.off('click', outside);
             }
@@ -43,7 +47,20 @@ function dropdown($document, dispatchers) {
                 return false;
             }
 
+            const onKeyDown = ({ key }) => {
+                if (key === SPACE_KEY || key === SPACE_KEY_OLD || key === RETURN_KEY) {
+                   click();
+                   return false;
+                }
+                if (key === ESCAPE_KEY || key === ESCAPE_KEY_OLD) {
+                    hideDropdown();
+                    return false;
+                }
+            };
+
             element.on('click', click);
+
+            element.on('keydown', onKeyDown);
 
             on('dropdown', (e, { type }) => {
                 type === 'close' && hideDropdown();
