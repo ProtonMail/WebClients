6c81c3b83083ba4462bbe39b79fe235eb8b02394
dhoko <dhoko@users.noreply.github.com>
2018-11-27 17:19:35 +0100

    Fix #7946 ~ Do not auto load the avatar for a contact (#8079)
    * Fix #7946 ~ Do not auto load the avatar for a contact
    
    * Unsubscribe

--- src/app/contact/directives/contactViewDetail.js
+++ src/app/contact/directives/contactViewDetail.js
@@ -1,5 +1,12 @@
 /* @ngInject */
-function contactViewDetail(contactDetailsModel, dispatchers, userType) {
+function contactViewDetail(contactDetailsModel, dispatchers, userType, mailSettingsModel) {
+    const loadImages = (scope, el) => (value) => {
+        scope.$applyAsync(() => {
+            scope.loadImage = value;
+        });
+        el.classList[value ? 'add' : 'remove']('contactViewDetail-loadRemoteImage');
+    };
+
     /**
      * We don't need to display some keys because it can be displayed
      * from a component
@@ -16,10 +23,14 @@ function contactViewDetail(contactDetailsModel, dispatchers, userType) {
         replace: true,
         restrict: 'E',
         templateUrl: require('../../../templates/contact/contactViewDetail.tpl.html'),
-        link(scope) {
+        link(scope, el) {
             const { on, unsubscribe } = dispatchers();
+            const loadRemoteImage = loadImages(scope, el[0]);
 
             scope.isFree = userType().isFree;
+
+            loadRemoteImage(!!mailSettingsModel.get('ShowImages'));
+
             scope.model = contactDetailsModel.extractHumans(scope.contact.vCard, filterKey);
             scope.model.hasEncrypted = scope.model.encrypted.length;
 
@@ -32,7 +43,20 @@ function contactViewDetail(contactDetailsModel, dispatchers, userType) {
                 }
             });
 
-            scope.$on('$destroy', unsubscribe);
+            const onClick = ({ target }) => {
+                if (target.nodeName !== 'BUTTON') {
+                    return;
+                }
+                const action = target.getAttribute('data-action');
+                action === 'loadRemoteImage' && loadRemoteImage(true);
+            };
+
+            el.on('click', onClick);
+
+            scope.$on('$destroy', () => {
+                unsubscribe();
+                el.off('click', onClick);
+            });
         }
     };
 }
