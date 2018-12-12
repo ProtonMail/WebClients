9a938529a5bf39133746441946ee54b43e2564a1
mmso <mmso@protonmail.com>
2018-12-12 15:20:39 +0100

    Make openpgp compat loading more robust (#8203)
    * Make openpgp compat loading more robust
    
    * Add crossorigin for credentials

--- src/helpers/setupPmcrypto.js
+++ src/helpers/setupPmcrypto.js
@@ -1,9 +1,9 @@
 import { init, createWorker } from 'pmcrypto';
-import { isIE11 } from './browser';
+import { hasModulesSupport } from './browser';
 
 init(window.openpgp);
 
 createWorker({
     // NOTE: THESE VALUES ARE TRANSFORMED BY WEBPACK WITH THE DEFINE PLUGIN.
-    path: isIE11() ? OPENPGP_WORKER_COMPAT : OPENPGP_WORKER // eslint-disable-line no-undef
+    path: hasModulesSupport() ? OPENPGP_WORKER : OPENPGP_WORKER_COMPAT // eslint-disable-line no-undef
 });
