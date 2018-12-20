442b82f326ba15b320157e93795425971382d156
mmso <mmso@protonmail.com>
2018-12-20 12:01:30 +0100

    Hotfix - Update "More info" link to the latest blog post (#8255)

--- env/configDefault.js
+++ env/configDefault.js
@@ -11,7 +11,7 @@ module.exports = {
     year: new Date().getFullYear(),
     clientID: 'Web',
     clientSecret: '4957cc9a2e0a2a49d02475c9d013478d',
-    articleLink: 'https://protonmail.com/blog/protonmail-v3-14-release-notes/',
+    articleLink: 'https://protonmail.com/blog/protonmail-v3-15-release-notes/',
     changelogPath: 'assets/changelog.tpl.html',
     versionPath: 'assets/version.json',
     translations: i18nLoader.get('list')
