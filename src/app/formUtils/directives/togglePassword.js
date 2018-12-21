331bd793b8da65483a6249aec7cc35203af37e7b
Richard <EpokK@users.noreply.github.com>
2018-12-21 20:59:32 +0100

    Hotfix - Comment mac logic on password input (toggle) (#8272)

--- src/app/formUtils/directives/togglePassword.js
+++ src/app/formUtils/directives/togglePassword.js
@@ -1,5 +1,5 @@
 import dedentTpl from '../../../helpers/dedent';
-import { isMac } from '../../../helpers/browser';
+// import { isMac } from '../../../helpers/browser';
 
 /* @ngInject */
 function togglePassword(gettextCatalog) {
@@ -25,17 +25,17 @@ function togglePassword(gettextCatalog) {
                 const btn = el[0].parentElement.querySelector('.togglePassword-btn-toggle');
 
                 const onClick = () => {
-                    if (isMac()) {
-                        const isPw = el[0].classList.contains('password-input-mac');
+                    // if (isMac()) {
+                    //     const isPw = el[0].classList.contains('password-input-mac');
 
-                        /*
-                            Force the input to be text, as we change the type to password onBlur
-                         */
-                        if (isPw) {
-                            el[0].type = 'text';
-                        }
-                        return el[0].classList.toggle('password-input-mac');
-                    }
+                    //     /*
+                    //         Force the input to be text, as we change the type to password onBlur
+                    //      */
+                    //     if (isPw) {
+                    //         el[0].type = 'text';
+                    //     }
+                    //     return el[0].classList.toggle('password-input-mac');
+                    // }
 
                     const type = el[0].type === 'text' ? 'password' : 'text';
                     el[0].setAttribute('type', type);
