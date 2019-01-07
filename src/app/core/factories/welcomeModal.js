cb38aa658c6daabb1dde134d8cf7d8deac227b98
Richard <richard@protonmail.com>
2019-01-07 12:12:47 +0100

    Fix #8321 - Signature is saved in the welcome modal
    People are probably saving the ProtonMail link with the welcome=true parameter saved in the URL. So each time they close the welcome modal, we were saving the identify without the signature.

--- src/app/core/factories/welcomeModal.js
+++ src/app/core/factories/welcomeModal.js
@@ -1,16 +1,21 @@
 /* @ngInject */
-function welcomeModal(pmModal, authentication, signatureModel) {
+function welcomeModal(pmModal, authentication, addressesModel, signatureModel) {
     return pmModal({
         controllerAs: 'ctrl',
         templateUrl: require('../../../templates/modals/welcome.tpl.html'),
         /* @ngInject */
         controller: function(params) {
             const { Name = '' } = authentication.user;
+            const { DisplayName = '', Signature = '' } = addressesModel.getFirst();
 
-            this.displayName = Name;
+            this.displayName = DisplayName || Name;
             this.cancel = params.cancel;
             this.next = () => {
-                this.displayName.length && signatureModel.save({ DisplayName: this.displayName });
+                this.displayName.length &&
+                    signatureModel.save({
+                        DisplayName: this.displayName,
+                        Signature
+                    });
                 params.next();
             };
         }
