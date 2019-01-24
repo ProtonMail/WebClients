a0b9ca4fc739116b94711d048b08c13615b4897a
dhoko <aurelien@protonmail.com>
2019-01-24 14:38:06 +0100

    Create unique i18n release commit build

--- tasks/commitI18n.sh
+++ tasks/commitI18n.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# Store the ref of the commit we use to build the translations
+LAST_COMMIT=$(git rev-parse HEAD);
+# Short version, enough to create a unique commit
+LAST_COMMIT_SHORT="${LAST_COMMIT:0:12}";
+
 if [ "$1" = '--update' ]; then
     git add -f po/template.pot
-    (git commit -m 'i18n ~ build translation release' && git push origin $(git rev-parse --abbrev-ref HEAD)) || echo "[i18n] No changes to commit"
+    (git commit -m "[I18N@$LAST_COMMIT_SHORT] build translation release" -m "Based on the commit: $LAST_COMMIT" && git push origin $(git rev-parse --abbrev-ref HEAD)) || echo "[i18n] No changes to commit"
 fi
 
 if [ "$1" = '--upgrade' ]; then
