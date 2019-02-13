f07f443241c90434a9d046cb2f06ac512895914b
dhoko <aurelien@protonmail.com>
2019-02-13 16:21:27 +0100

    Fix #8516 ~ On Enter validate the human verification form

--- src/app/formUtils/directives/codeVerificator.js
+++ src/app/formUtils/directives/codeVerificator.js
@@ -11,14 +11,22 @@ function codeVerificator(dispatchers, humanVerificationModel, networkActivityTra
         restrict: 'E',
         templateUrl: require('../../../templates/formUtils/codeVerificator.tpl.html'),
         link(scope, el, { method }) {
-            const { on, unsubscribe } = dispatchers(['humanVerification']);
+            const { on, unsubscribe, dispatcher } = dispatchers(['humanVerification']);
+
+            const focusInput = (name) => {
+                scope.$applyAsync(() => {
+                    el[0][name].focus();
+                });
+            };
+
+            focusInput(`${method}Verification`);
 
             const onSubmit = (e) => {
                 // Stop the propagation to the signup form...
                 e.preventDefault();
                 e.stopPropagation();
-                const promise = humanVerificationModel.sendCode(method, scope.contactInformation);
 
+                const promise = humanVerificationModel.sendCode(method, scope.contactInformation);
                 networkActivityTracker.track(promise);
 
                 // reset code value to avoid getting it back a second time
@@ -27,7 +35,6 @@ function codeVerificator(dispatchers, humanVerificationModel, networkActivityTra
 
             const onClick = ({ target }) => {
                 const action = target.getAttribute('data-action');
-
                 if (action === 'sendNewCode') {
                     el[0].classList.add(NEW_CODE_CLASS);
                 }
@@ -38,6 +45,7 @@ function codeVerificator(dispatchers, humanVerificationModel, networkActivityTra
             on('humanVerification', (e, { type, data }) => {
                 if (type === 'code.sent.success' && data.method === method) {
                     el[0].classList.add(CODE_SENT_CLASS);
+                    focusInput('codeValue');
                 }
 
                 if (type === 'code.sent.error' && data.method === method && data.errorMessage) {
@@ -46,12 +54,28 @@ function codeVerificator(dispatchers, humanVerificationModel, networkActivityTra
                 }
             });
 
+            const onKeydown = (e) => {
+                if (e.key !== 'Enter') {
+                    return;
+                }
+
+                // No need to use $applyAsync from here, it's done inside HumanVerification
+                if (scope.code) {
+                    // prevent submit --force
+                    e.preventDefault();
+                    e.stopPropagation();
+                    dispatcher.humanVerification('validate.submit.codeVerification');
+                }
+            };
+
+            el.on('keydown', onKeydown);
             el.on('submit', onSubmit);
             el.on('click', onClick);
 
             scope.$on('$destroy', () => {
                 el.off('submit', onSubmit);
                 el.off('click', onClick);
+                el.off('keydown', onKeydown);
                 unsubscribe();
             });
         }
