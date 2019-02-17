be1c41c727b2491cabe6f090150048427291c624
Richard <EpokK@users.noreply.github.com>
2019-02-17 10:32:27 +0100

    Fix #8502 - Add filter to display properly birthdate (#8531)
    * Fix #8502 - Add filter to display properly birthdate
    
    * Add doc
    
    * Check if the input can be parsed by moment

---
+++ src/app/utils/filters/formatBDay.js
@@ -0,0 +1,32 @@
+/* @ngInject */
+function formatBDay() {
+    return (dateAndOrTime) => {
+        /*
+        https://tools.ietf.org/html/rfc6350#section-6.2.5
+        Input:
+        19961022T140000
+        --1022T1400
+        ---22T14
+        19850412
+        1985-04
+        1985
+        --0412
+        ---12
+        T102200
+        T1022
+        T10
+        T-2200
+        T--00
+        T102200Z
+        T102200-0800
+        */
+        const m = moment(dateAndOrTime);
+        
+        if (m.isValid()) {
+            return m.format('LL');
+        }
+        
+        return dateAndOrTime;
+    };
+}
+export default formatBDay;
