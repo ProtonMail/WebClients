3a3b93561d59731ecbff73d9db58c1f258860d73
dhoko <dhoko@users.noreply.github.com>
2019-02-19 11:20:42 +0100

    Fix #8076 ~ Use new API for vpn settings (#8551)

--- src/app/settings/factories/vpnSettingsApi.js
+++ src/app/settings/factories/vpnSettingsApi.js
@@ -1,6 +1,6 @@
 /* @ngInject */
 function vpnSettingsApi($http, url) {
-    const requestURL = url.build('settings/vpn');
+    const requestURL = url.build('vpn/settings');
     const handleResult = ({ data = {} } = {}) => data.VPNSettings;
     const fetch = () => $http.get(requestURL()).then(handleResult);
     const updateName = (data) => $http.put(requestURL('name'), data).then(handleResult);
