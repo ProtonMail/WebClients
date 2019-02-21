20543fdc85de79ce73c8ca5ca1885bb7c0d83c46
dhoko <dhoko@users.noreply.github.com>
2019-02-21 18:25:20 +0100

    Fix #8563 ~ Missing re render cb on refresh (#8572)
    * Fix #8563 ~ Missing re render cb on refresh
    
    * Fix scope resize composer

--- src/app/composer/directives/responsiveComposer.js
+++ src/app/composer/directives/responsiveComposer.js
@@ -2,37 +2,39 @@ import _ from 'lodash';
 
 /* @ngInject */
 function responsiveComposer(AppModel, mailSettingsModel) {
-    const latestState = {};
-
     /**
      * Factory to compute sizes
      * @param  {$scope} scope
      * @return {Function}        Callback
      */
-    const computeType = (scope) => () => {
-        const width = window.innerWidth;
-        const height = window.innerHeight;
-        const isSmall = width <= 640 || height <= 600;
+    const computeType = (scope) => {
+        const latestState = {};
 
-        // If the composer is maximized do nothing, keep this state
-        if (scope.message.maximized && !latestState.isSmall) {
-            return;
-        }
+        return () => {
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+            const isSmall = width <= 640 || height <= 600;
 
-        // If the composer is minimized do nothing, keep this state
-        if (scope.message.minimized) {
-            return;
-        }
+            // If the composer is maximized do nothing, keep this state
+            if (scope.message.maximized && !latestState.isSmall) {
+                return;
+            }
 
-        latestState.isSmall = isSmall;
+            // If the composer is minimized do nothing, keep this state
+            if (scope.message.minimized) {
+                return;
+            }
 
-        scope.$applyAsync(() => {
-            AppModel.set('small', height < 700 && height >= 600);
-            AppModel.set('mini', height < 600);
+            latestState.isSmall = isSmall;
 
-            isSmall && scope.maximize(scope.message);
-            !isSmall && mailSettingsModel.get('ComposerMode') === 0 && scope.normalize(scope.message);
-        });
+            scope.$applyAsync(() => {
+                AppModel.set('small', height < 700 && height >= 600);
+                AppModel.set('mini', height < 600);
+
+                isSmall && scope.maximize(scope.message);
+                !isSmall && mailSettingsModel.get('ComposerMode') === 0 && scope.normalize(scope.message);
+            });
+        };
     };
 
     return {
