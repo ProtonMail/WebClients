4b6590c673f7973a5144c83cefa893c462e17c47
Richard <richard@protonmail.com>
2019-02-22 13:25:48 +0100

    Add constants

--- packages/shared/lib/constants.js
+++ packages/shared/lib/constants.js
@@ -13,3 +13,21 @@ export const USER_ROLES = {
     MEMBER_ROLE: 1,
     ADMIN_ROLE: 2
 };
+export const ELEMENTS_PER_PAGE = 10;
+export const INVOICE_OWNER = {
+    USER: 0,
+    ORGANIZATION: 1
+};
+export const INVOICE_TYPE = {
+    OTHER: 0,
+    SUBSCRIPTION: 1,
+    CANCELLATION: 2,
+    CREDIT: 3,
+    DONATION: 4
+};
+export const INVOICE_STATE = {
+    UNPAID: 0,
+    PAID: 1,
+    VOID: 2,
+    BILLED: 3
+};
