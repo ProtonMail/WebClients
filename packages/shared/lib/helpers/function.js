02adf77340d222594854fad6e907f3bbd2671072
EpokK <richard.d@protonmail.com>
2019-02-23 14:56:50 +0100

    add debounce

--- packages/shared/lib/helpers/function.js
+++ packages/shared/lib/helpers/function.js
@@ -1,1 +1,12 @@
 export const noop = () => {};
+
+export const debounce = (fn, time) => {
+    let timeout;
+
+    return function() {
+        const functionCall = () => fn.apply(this, arguments);
+
+        clearTimeout(timeout);
+        timeout = setTimeout(functionCall, time);
+    }
+  }
