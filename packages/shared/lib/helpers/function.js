9fc5541a22c0a80aa239504310ce09c1193f84c3
EpokK <richard.d@protonmail.com>
2019-02-23 15:06:35 +0100

    Add throttle helper

--- packages/shared/lib/helpers/function.js
+++ packages/shared/lib/helpers/function.js
@@ -8,5 +8,23 @@ export const debounce = (fn, time) => {
 
         clearTimeout(timeout);
         timeout = setTimeout(functionCall, time);
-    }
-  }
+    };
+};
+
+export const throttle = (func, ms = 50, context = window) => {
+	let wait = false;
+
+    return (...args) => {
+	  const later = () => {
+		  func.apply(context, args);
+	  };
+
+      if (!wait) {
+		  later();
+		  wait = true;
+		  setTimeout(() => {
+		    wait = false;
+		  }, ms);
+	  }
+	};
+};
