f4c6cda4ae204f8b03dc30c0ec0349a67b67fffa
Richard <richard@protonmail.com>
2019-02-25 11:22:46 +0100

    Review function helpers

--- packages/shared/lib/helpers/function.js
+++ packages/shared/lib/helpers/function.js
@@ -1,30 +1,38 @@
 export const noop = () => {};
 
-export const debounce = (fn, time) => {
-    let timeout;
+export const debounce = (func, wait, immediate) => {
+	let timeout;
 
-    return function() {
-        const functionCall = () => fn.apply(this, arguments);
+	return function (...args) {
+		clearTimeout(timeout);
 
-        clearTimeout(timeout);
-        timeout = setTimeout(functionCall, time);
-    };
+		timeout = setTimeout(() => {
+			timeout = null;
+			if (!immediate) {
+				func.apply(this, args);
+			}
+		}, wait);
+
+		if (immediate && !timeout) {
+			func.apply(this, [...args]);
+		}
+	}
 };
 
 export const throttle = (func, ms = 50, context = window) => {
 	let wait = false;
 
-    return (...args) => {
-	  const later = () => {
-		  func.apply(context, args);
-	  };
+	return (...args) => {
+		const later = () => {
+			func.apply(context, args);
+		};
 
-      if (!wait) {
-		  later();
-		  wait = true;
-		  setTimeout(() => {
-		    wait = false;
-		  }, ms);
-	  }
+		if (!wait) {
+			later();
+			wait = true;
+			setTimeout(() => {
+				wait = false;
+			}, ms);
+		}
 	};
 };
