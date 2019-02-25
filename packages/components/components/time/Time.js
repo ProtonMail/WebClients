a9e98ae87146929ef85665cde48e48d6c6e5a508
Richard <richard@protonmail.com>
2019-02-25 11:23:24 +0100

    Review Time to use format

--- packages/components/components/time/Time.js
+++ packages/components/components/time/Time.js
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import readableTime from 'proton-shared/lib/helpers/readableTime';
 
-const Time = ({ children, ...rest }) => <time {...rest}>{readableTime(children)}</time>;
+const Time = ({ children, format, ...rest }) => <time {...rest}>{readableTime(children, format)}</time>;
 
 Time.propTypes = {
-    children: PropTypes.number.isRequired
+    children: PropTypes.number.isRequired,
+    format: PropTypes.string
 };
 
 Time.defaultProps = {
-    children: 0
+    children: 0,
+    format: 'LL'
 };
 
 export default Time;
