821f5c2dd0ed3057896665515ef8e98860bf115a
Richard <richard@protonmail.com>
2019-02-25 12:07:03 +0100

    Add reset for useSearch hook

--- packages/components/components/input/useSearch.js
+++ packages/components/components/input/useSearch.js
@@ -8,14 +8,17 @@ import { normalize } from 'proton-shared/lib/helpers/string';
  * @param {Number} wait delay for debounce, set 0 for no debounce
  * @returns {String} o.keywords
  * @returns {Function} o.set contains debounce to set keywords value
+ * @returns {Function} o.reset directly set an empty string
  */
 const useSearch = (initialKeywords = '', wait = 500) => {
     const [keywords, setKeywords] = useState(normalize(initialKeywords));
     const set = (value = '') => debounce(() => setKeywords(normalize(value)), wait);
+    const reset = () => setKeywords('');
 
     return {
         keywords,
-        set
+        set,
+        reset
     };
 };
 
