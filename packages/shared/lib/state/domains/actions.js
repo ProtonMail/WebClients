171a727b1c2767d096b2699a459d948f1bac86d3
Richard <richard@protonmail.com>
2019-03-01 16:01:03 +0100

    Add fetchDomains action

--- packages/shared/lib/state/domains/actions.js
+++ packages/shared/lib/state/domains/actions.js
@@ -1,6 +1,20 @@
 import { ACTIONS } from './reducer';
+import { queryDomains } from '../../api/domains';
 
 export const loadingAction = () => ({ type: ACTIONS.LOADING });
 export const resetAction = () => ({ type: ACTIONS.RESET });
 export const updateAction = (data) => ({ type: ACTIONS.UPDATE, payload: data });
 export const setAction = (data) => ({ type: ACTIONS.SET, payload: data });
+
+export const fetchDomains = () => {
+    return async (dispatch, getState, { api }) => {
+        dispatch(loadingAction());
+        try {
+            const { Domains = [] } = await api(queryDomains());
+            return dispatch(setAction(Domains));
+        } catch (error) {
+            dispatch(resetAction());
+            throw error;
+        }
+    };
+};
