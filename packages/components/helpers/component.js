c0d6360ca94e3e14e2aa0233ef725382f562d8f4
Richard <richard@protonmail.com>
2019-03-01 17:02:16 +0100

    Helper to generate fake event input

--- packages/components/helpers/component.js
+++ packages/components/helpers/component.js
@@ -2,3 +2,4 @@ let current = 0
 
 export const generateUID = (prefix) => `${prefix || 'id'}-${current++}`;
 export const getClasses = (defaultClass = '', classNames = '') => `${defaultClass.trim()}  ${classNames.trim()}`;
+export const fakeEvent = (value) => ({ target: { value }});
