da9b7971b66b335cc34715e866d3cd9c31650da8
Richard <richard@protonmail.com>
2019-03-04 14:54:05 +0100

    Handle new design for Checkbox and Radio

--- packages/components/components/input/Checkbox.js
+++ packages/components/components/input/Checkbox.js
@@ -3,7 +3,14 @@ import PropTypes from 'prop-types';
 
 import Input from './Input';
 
-const Checkbox = ({ checked, ...rest }) => <Input type="checkbox" checked={checked} {...rest} />;
+const Checkbox = ({ checked, ...rest }) => {
+    return (
+        <>
+            <Input type="checkbox" className="pm-checkbox" checked={checked} {...rest} />
+            <span className="pm-radio-fakecheckbox" />
+        </>
+    );
+};
 
 Checkbox.propTypes = {
     checked: PropTypes.bool.isRequired
--- packages/components/components/input/Radio.js
+++ packages/components/components/input/Radio.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 import Input from './Input';
 
-const Radio = ({ ...rest }) => <Input type="radio" {...rest} />;
+const Radio = ({ ...rest }) => {
+    return (
+        <>
+            <Input type="radio" className="pm-radio" {...rest} />
+            <span className="pm-radio-fakeradio" />
+        </>
+    );
+};
 
 export default Radio;
