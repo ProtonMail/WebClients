6c36f11d1b5a837fe27d9da329bf70db725434eb
Nicolas Hoffmann <dante3333@gmail.com>
2019-03-06 10:14:20 +0100

    Feat - box sizing border box (episode 2) (#8478)
    * Feat - activate box-sizing for all
    
    * Fix no label display
    
    * Fix move to folder display
    
    * Fix display logo
    
    * Fix display attachment
    
    * Fix differences in message/contact view
    
    * Fix difference in signup

--- test/specs/message/helpers/transformEscape.spec.js
+++ test/specs/message/helpers/transformEscape.spec.js
@@ -30,7 +30,7 @@ describe('transformEscape', () => {
 </section>`;
 
     const CODE_HTML_HIGHLIGHT = `
-    <div style="box-sizing: border-box;white-space: normal;" class="pre"><div style="box-sizing: border-box;color: #fff;max-width: 100%;font-size: 16px;line-height: 1.3;" class="code"><span style="box-sizing: border-box;color: #DDDDDF;" class="nt">&lt;script</span> <span style="box-sizing: border-box;color: #84868B;" class="na">src="</span><span style="box-sizing: border-box;color: #68BEA2;" class="s"><span class="s">https<span>://</span>use.fontawesome<span>.</span>com/f0d8991ea9.js</span><span style="box-sizing: border-box;color: #84868B;" class="na">"</span><span style="box-sizing: border-box;color: #DDDDDF;" class="nt">&gt;</span><span style="box-sizing: border-box;color: #DDDDDF;" class="nt">&lt;/script&gt;</span></span></div></div><div class="pre"></div>
+    <div style="white-space: normal;" class="pre"><div style="color: #fff;max-width: 100%;font-size: 16px;line-height: 1.3;" class="code"><span style="color: #DDDDDF;" class="nt">&lt;script</span> <span style="color: #84868B;" class="na">src="</span><span style="color: #68BEA2;" class="s"><span class="s">https<span>://</span>use.fontawesome<span>.</span>com/f0d8991ea9.js</span><span style="color: #84868B;" class="na">"</span><span style="color: #DDDDDF;" class="nt">&gt;</span><span style="color: #DDDDDF;" class="nt">&lt;/script&gt;</span></span></div></div><div class="pre"></div>
 `;
     const HTML_LINKS = `<div>
   <a href="http://www.dewdwesrcset-dewdw.com/?srcset=de&srcset=Dewdwe"></a>
