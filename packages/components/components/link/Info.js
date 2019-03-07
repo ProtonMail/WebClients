077c05066daa9bef2e62047eade1383e6340c20a
Richard <richard@protonmail.com>
2019-03-07 15:15:26 +0100

    Extend Info to handle tooltip and title

--- packages/components/components/link/Info.js
+++ packages/components/components/link/Info.js
@@ -3,17 +3,32 @@ import PropTypes from 'prop-types';
 
 import Href from './Href';
 import Icon from '../icon/Icon';
+import Tooltip from '../tooltip/Tooltip';
 
-const Info = ({ url }) => {
-    return (
-        <Href url={url}>
-            <Icon name="info" />
-        </Href>
-    );
+const Info = ({ url, tooltip, title }) => {
+    if (url) {
+        return (
+            <Href url={url} title={title}>
+                <Icon name="info" />
+            </Href>
+        );
+    }
+
+    if (tooltip) {
+        return (
+            <Tooltip title={tooltip}>
+                <Icon name="info" />
+            </Tooltip>
+        );
+    }
+
+    return null;
 };
 
 Info.propTypes = {
-    url: PropTypes.string.isRequired
+    url: PropTypes.string,
+    tooltip: PropTypes.string,
+    title: PropTypes.string
 };
 
 export default Info;
