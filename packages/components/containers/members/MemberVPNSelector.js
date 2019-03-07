7490990e357135b241c88333ba36545fbdc0794d
Richard <richard@protonmail.com>
2019-03-07 09:04:19 +0100

    Add VPN selector

--- packages/components/containers/members/MemberVPNSelector.js
+++ packages/components/containers/members/MemberVPNSelector.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { Row } from 'react-components';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Select } from 'react-components';
+import { range } from 'proton-shared/lib/helpers/array';
 
-const MemberVPNSelector = () => {
-    return (
-        <Row>
+const MemberVPNSelector = ({ member, organization, onChange }) => {
+    const minPadding = member.MaxVPN || 0;
+    const maxPadding = member.ID
+        ? organization.MaxVPN - organization.UsedVPN + member.MaxVPN
+        : organization.MaxVPN - organization.UsedVPN;
+    const options = range(minPadding, maxPadding).map((value) => ({ text: value, value }));
+    const [vpn, setVpn] = useState(member.ID ? member.MaxVPN : 0);
+    const handleChange = ({ target }) => setVpn(target.value);
 
-        </Row>
-    );
+    useEffect(() => {
+        onChange(vpn);
+    }, vpn);
+
+    return <Select value={vpn} options={options} onChange={handleChange} />;
+};
+
+MemberVPNSelector.propTypes = {
+    member: PropTypes.object.isRequired,
+    onChange: PropTypes.func.isRequired,
+    organization: PropTypes.object.isRequired
+};
+
+MemberVPNSelector.defaultProps = {
+    member: {}
 };
 
 export default MemberVPNSelector;
