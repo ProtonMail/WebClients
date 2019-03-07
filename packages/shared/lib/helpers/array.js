034e5a13631ab77073de64e827804a82cf867e0e
Richard <richard@protonmail.com>
2019-03-07 08:40:11 +0100

    Add step parameter to range helper

--- packages/shared/lib/helpers/array.js
+++ packages/shared/lib/helpers/array.js
@@ -1,7 +1,9 @@
-export const range = (start, end) => {
+export const range = (start = 0, end = 0, step = 1) => {
     const result = [];
-    for (let index = start; index < end; index++) {
+
+    for (let index = start; index < end; index += step) {
         result.push(index);
     }
+
     return result;
 };
