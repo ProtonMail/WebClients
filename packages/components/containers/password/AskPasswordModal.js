a1410a19ed286d14393a17a61d91df11f84c9b3d
mmso <mmso@mmso.se>
2019-03-11 15:51:32 +0100

    Use c in askPasswordModal

--- packages/components/containers/password/AskPasswordModal.js
+++ packages/components/containers/password/AskPasswordModal.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { t } from 'ttag';
+import { c } from 'ttag';
 import { InputModal } from 'react-components';
 
 const AskPasswordModal = ({ onClose, onSubmit }) => {
     return (
         <InputModal
-            label={t`Password`}
-            title={t`Sign in again to continue`}
+            label={c('Label').t`Password`}
+            title={c('Title').t`Sign in again to continue`}
             show={true}
-            cancel={t`Cancel`}
-            confirm={t`Submit`}
+            cancel={c('Label').t`Cancel`}
+            confirm={c('Label').t`Submit`}
             onSubmit={onSubmit}
             onClose={onClose}
         />
