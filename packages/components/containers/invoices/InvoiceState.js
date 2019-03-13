118f60b92fa6c568d62c3eb4a02a990b1ed28c73
Richard <richard@protonmail.com>
2019-03-13 07:41:44 +0100

    Review invoice badge

--- packages/components/containers/invoices/InvoiceState.js
+++ packages/components/containers/invoices/InvoiceState.js
@@ -4,15 +4,22 @@ import { c } from 'ttag';
 import PropTypes from 'prop-types';
 import { INVOICE_STATE } from 'proton-shared/lib/constants';
 
-const InvoiceState = ({ invoice }) => {
-    const STATES = {
-        [INVOICE_STATE.UNPAID]: c('Invoice state display as badge').t`Unpaid`,
-        [INVOICE_STATE.PAID]: c('Invoice state display as badge').t`Paid`,
-        [INVOICE_STATE.VOID]: c('Invoice state display as badge').t`Void`,
-        [INVOICE_STATE.BILLED]: c('Invoice state display as badge').t`Billed`
-    };
+const STATES = {
+    [INVOICE_STATE.UNPAID]: c('Invoice state display as badge').t`Unpaid`,
+    [INVOICE_STATE.PAID]: c('Invoice state display as badge').t`Paid`,
+    [INVOICE_STATE.VOID]: c('Invoice state display as badge').t`Void`,
+    [INVOICE_STATE.BILLED]: c('Invoice state display as badge').t`Billed`
+};
 
-    return <Badge>{STATES[invoice.State]}</Badge>;
+const TYPES = {
+    [INVOICE_STATE.UNPAID]: 'error',
+    [INVOICE_STATE.PAID]: 'success',
+    [INVOICE_STATE.VOID]: 'default',
+    [INVOICE_STATE.BILLED]: 'default'
+};
+
+const InvoiceState = ({ invoice }) => {
+    return <Badge type={TYPES[invoice.State]}>{STATES[invoice.State]}</Badge>;
 };
 
 InvoiceState.propTypes = {
