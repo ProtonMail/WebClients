f535e302029154adb77ce083b22ef840bd9564c8
Richard <richard@protonmail.com>
2019-03-13 13:01:35 +0100

    Add payments routes

--- packages/shared/lib/api/payments.js
+++ packages/shared/lib/api/payments.js
@@ -3,16 +3,33 @@ export const getSubscription = () => ({
     method: 'get'
 });
 
+export const deleteSubscription = () => ({
+    url: 'payments/subscription',
+    method: 'delete'
+});
+
 export const queryInvoices = ({ Page, PageSize, Owner, State, Type }) => ({
     url: 'payments/invoices',
     method: 'get',
     params: { Page, PageSize, Owner, State, Type }
 });
 
-export const getInvoice = (ID) => ({
-    url: `payments/invoices/${ID}`,
+export const queryPlans = (Currency, Cycle) => ({
+    url: 'payments/plans',
+    method: 'get',
+    params: { Currency, Cycle }
+});
+
+export const getInvoice = (invoiceID) => ({
+    url: `payments/invoices/${invoiceID}`,
     method: 'get',
     output: 'arrayBuffer'
+});
+
+export const checkInvoice = (invoiceID, GiftCode) => ({
+    url: `payments/invoices/${invoiceID}/check`,
+    method: 'put',
+    data: { GiftCode }
 });
 
 export const queryPaymentMethods = () => ({
@@ -26,13 +43,19 @@ export const setPaymentMethod = (data) => ({
     data
 });
 
-export const deletePaymentMethod = (ID) => ({
-    url: `payments/methods/${ID}`,
+export const deletePaymentMethod = (methodID) => ({
+    url: `payments/methods/${methodID}`,
     method: 'delete'
 });
 
-export const createBitcoinPayment = ({ Amount, Currency }) => ({
+export const createBitcoinPayment = (Amount, Currency) => ({
     url: 'payments/bcinfo',
+    method: 'post',
+    data: { Amount, Currency }
+});
+
+export const createPayPalPayment = (Amount, Currency) => ({
+    url: 'payments/paypal',
     method: 'post',
     data: { Amount, Currency }
 });
