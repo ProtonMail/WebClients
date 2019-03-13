d91a0eac0e25c126355dbf1fb22a6cd434f4bbb9
Richard <richard@protonmail.com>
2019-03-13 07:41:26 +0100

    Add className for Price

--- packages/components/components/price/Price.js
+++ packages/components/components/price/Price.js
@@ -7,18 +7,23 @@ const CURRENCIES = {
     CHF: 'CHF'
 };
 
-const Price = ({ children: amount = 0, currency = '' }) => {
+const Price = ({ children: amount = 0, currency = '', className = '' }) => {
     const symbol = CURRENCIES[currency] || currency;
     const value = Number(amount).toFixed(2);
     const prefix = value < 0 ? '-' : '';
     const absValue = Math.abs(value);
 
-    return (<span className="price">{currency === 'USD' ? `${prefix}${symbol}${absValue}` : `${prefix}${absValue} ${symbol}`}</span>);
+    return (
+        <span className={`price ${className}`}>
+            {currency === 'USD' ? `${prefix}${symbol}${absValue}` : `${prefix}${absValue} ${symbol}`}
+        </span>
+    );
 };
 
 Price.propTypes = {
     currency: PropTypes.string,
-    children: PropTypes.number.isRequired
+    children: PropTypes.number.isRequired,
+    className: PropTypes.string
 };
 
 export default Price;
