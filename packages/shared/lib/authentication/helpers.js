a32d99a0685053af87e662fc6a893528b607ebe5
mmso <mmso@mmso.se>
2019-03-14 18:01:44 +0100

    Add getAccessToken

--- packages/shared/lib/authentication/helpers.js
+++ packages/shared/lib/authentication/helpers.js
@@ -1,4 +1,5 @@
-import { getMessage, decryptMessage } from 'pmcrypto';
+import { getMessage, decryptMessage, decryptPrivateKey } from 'pmcrypto';
+import { computeKeyPassword } from 'pm-srp';
 
 export const hasTotp = ({ '2FA': { TOTP } }) => {
     return !!TOTP;
@@ -30,4 +31,22 @@ export const decryptAccessToken = async (accessToken, privateKey) => {
         privateKeys: [privateKey]
     });
     return data;
+};
+
+/**
+ * Get access token with forwards compat.
+ * TODO: Not needed once the API has deprecated encrypted access tokens.
+ * @param {String} AccessToken
+ * @param {String} PrivateKey
+ * @param {String} KeySalt
+ * @param {String} password
+ * @returns {Promise<String>}
+ */
+export const getAccessToken = async (AccessToken, PrivateKey, KeySalt, password) => {
+    if (!isPgpMessage(AccessToken)) {
+        return AccessToken;
+    }
+    const keyPassword = await computeKeyPassword(password, KeySalt);
+    const privateKey = await decryptPrivateKey(PrivateKey, keyPassword);
+    return decryptAccessToken(AccessToken, privateKey);
 };
