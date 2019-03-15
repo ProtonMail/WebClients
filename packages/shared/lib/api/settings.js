93f1af309c6b23f6fb2b642341fcfeb09306bf53
Richard <richard@protonmail.com>
2019-03-15 17:45:33 +0100

    Add settings methods

--- packages/shared/lib/api/settings.js
+++ packages/shared/lib/api/settings.js
@@ -3,14 +3,22 @@ export const getSettings = () => ({
     method: 'get'
 });
 
-export const passwordUpgrade = () => ({
-    url: 'settings/password/upgrade',
-    method: 'put'
+export const updatePassword = (data) => ({
+    url: 'settings/password',
+    method: 'put',
+    data
 });
 
-export const updateLocale = () => ({
+export const upgradePassword = (data) => ({
+    url: 'settings/password/upgrade',
+    method: 'put',
+    data
+});
+
+export const updateLocale = (Locale) => ({
     url: 'settings/locale',
-    method: 'put'
+    method: 'put',
+    data: { Locale }
 });
 
 export const updateNews = (News) => ({
@@ -29,4 +37,52 @@ export const updateLogAuth = (LogAuth) => ({
     url: 'settings/logauth',
     method: 'put',
     data: { LogAuth }
+});
+
+export const updateEmail = (data) => ({
+    url: 'settings/email',
+    method: 'put',
+    data
+});
+
+export const updateNotifyEmail = (Notify) => ({
+    url: 'settings/email/notify',
+    method: 'put',
+    data: { Notify }
+});
+
+export const updateResetEmail = (data) => ({
+    url: 'settings/email/reset',
+    method: 'put',
+    data
+});
+
+export const verifyEmail = (Token) => ({
+    url: 'settings/email/verify',
+    method: 'post',
+    data: { Token }
+});
+
+export const updatePhone = (data) => ({
+    url: 'settings/phone',
+    method: 'put',
+    data
+});
+
+export const updateNotifyPhone = (Notify) => ({
+    url: 'settings/phone/notify',
+    method: 'put',
+    data: { Notify }
+});
+
+export const updateResetPhone = (data) => ({
+    url: 'settings/phone/reset',
+    method: 'put',
+    data
+});
+
+export const verifyPhone = (Token) => ({
+    url: 'settings/phone/verify',
+    method: 'post',
+    data: { Token }
 });
