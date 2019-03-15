89833c02df47b4942a444791c8d6b2fca190808d
Richard <richard@protonmail.com>
2019-03-15 13:21:26 +0100

    Redo logic for news section

--- packages/components/containers/news/NewsSection.js
+++ packages/components/containers/news/NewsSection.js
@@ -1,62 +1,53 @@
-import React from 'react';
+import React, { useState, onEffect } from 'react';
 import { c } from 'ttag';
-import { Label, Checkbox, useApiWithoutResult, useSettings, useEventManager } from 'react-components';
+import { Label, Checkbox, useApiWithoutResult, useSettings } from 'react-components';
+import { NEWS } from 'proton-shared/lib/constants';
 import { updateNews } from 'proton-shared/lib/api/settings';
-import { toBitMap } from 'proton-shared/lib/helpers/object';
+import { toggleBit, hasBit } from 'proton-shared/lib/helpers/bitset';
 
-import useNews from './useNews';
+const { ANNOUNCEMENTS, FEATURES, NEWSLETTER, BETA } = NEWS;
 
 const NewsSection = () => {
     const { News } = useSettings();
-    const { call } = useEventManager();
     const { request, loading } = useApiWithoutResult(updateNews);
-    const keys = ['announcements', 'features', 'newsletter', 'beta'];
-    const { news, setNews } = useNews(News, keys);
+    const [news, setNews] = useState(News);
+    const handleChange = (mask) => setNews(toggleBit(news, mask));
 
-    const handleChange = (key) => async (event) => {
-        // Here we need to keep the order of the Object for the bitmap conversion
-        // so we cannot do: { ...new, [key]: event.target.checked }
-        const newState = Object.keys(news).reduce((acc, k) => {
-            acc[k] = k === key ? event.target.checked : acc[k];
-            return acc;
-        }, {});
-        const value = toBitMap(newState);
-        await request(value);
-        await call();
-        setNews(value);
-    };
+    onEffect(() => {
+        request(news); // No need to call the even manager here
+    }, [news]);
 
     return (
         <>
             <Label htmlFor="announcements">
                 <Checkbox
                     id="announcements"
-                    checked={news.announcements}
+                    checked={hasBit(news, ANNOUNCEMENTS)}
                     disabled={loading}
-                    onChange={handleChange('announcements')}
+                    onChange={handleChange(ANNOUNCEMENTS)}
                 />
                 {c('Label for news').t`Major announcements (2-3 per year)`}
             </Label>
             <Label htmlFor="features">
                 <Checkbox
                     id="features"
-                    checked={news.features}
+                    checked={hasBit(news, FEATURES)}
                     disabled={loading}
-                    onChange={handleChange('features')}
+                    onChange={handleChange(FEATURES)}
                 />
                 {c('Label for news').t`Major features (3-4 per year)`}
             </Label>
             <Label htmlFor="newsletter">
                 <Checkbox
                     id="newsletter"
-                    checked={news.newsletter}
+                    checked={hasBit(news, NEWSLETTER)}
                     disabled={loading}
-                    onChange={handleChange('newsletter')}
+                    onChange={handleChange(NEWSLETTER)}
                 />
                 {c('Label for news').t`Proton newsletter (5-6 per year)`}
             </Label>
             <Label htmlFor="beta">
-                <Checkbox id="beta" checked={news.beta} disabled={loading} onChange={handleChange('beta')} />
+                <Checkbox id="beta" checked={hasBit(news, BETA)} disabled={loading} onChange={handleChange(BETA)} />
                 {c('Label for news').t`Proton Beta (10-12 per year)`}
             </Label>
         </>
