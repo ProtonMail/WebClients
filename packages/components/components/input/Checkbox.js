03058661099f15d13618519a91038538b17b8aaf
EpokK <richard.d@protonmail.com>
2019-03-22 11:50:09 +0100

    Review checkbox and radio with Nico

--- packages/components/components/input/Checkbox.js
+++ packages/components/components/input/Checkbox.js
@@ -2,17 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Input from './Input';
+import Icon from '../icon/Icon';
 
-const Checkbox = ({ checked, ...rest }) => {
+const Checkbox = ({ id, checked, ...rest }) => {
     return (
-        <>
-            <Input type="checkbox" className="pm-checkbox" checked={checked} {...rest} />
-            <span className="pm-radio-fakecheckbox" />
-        </>
+        <label htmlFor={id}>
+            <Input id={id} type="checkbox" className="pm-checkbox" checked={checked} {...rest} />
+            <span className="pm-checkbox-fakecheck">
+                <Icon className="pm-checkbox-fakecheck-img" name="on" />
+            </span>
+        </label>
     );
 };
 
 Checkbox.propTypes = {
+    id: PropTypes.string,
     checked: PropTypes.bool.isRequired
 };
 
--- packages/components/components/input/Radio.js
+++ packages/components/components/input/Radio.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Input from './Input';
 
-const Radio = ({ ...rest }) => {
+const Radio = ({ id, ...rest }) => {
     return (
-        <>
-            <Input type="radio" className="pm-radio" {...rest} />
+        <label htmlFor={id}>
+            <Input id={id} type="radio" className="pm-radio" {...rest} />
             <span className="pm-radio-fakeradio" />
-        </>
+        </label>
     );
+};
+
+Radio.propTypes = {
+    id: PropTypes.string
 };
 
 export default Radio;
