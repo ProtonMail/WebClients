227dee199e6e27ff40779be331a683d49a29f1b1
Richard <richard@protonmail.com>
2019-03-25 15:53:10 +0100

    Add minimal test for Tooltip
    I didn't succeed to display the tooltip by firing a "hover" event on the button.

---
+++ packages/components/components/tooltip/Tooltip.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import Tooltip from './Tooltip';
+
+describe('Tooltip component', () => {
+    it('should add a tooltip around the children', async () => {
+        const title = 'panda';
+        const { container } = render(
+            <Tooltip title={title}>
+                <button />
+            </Tooltip>
+        );
+        const buttonNode = container.querySelector('button');
+
+        expect(buttonNode).toBeDefined();
+        expect(container.firstChild.classList.contains('Tooltip')).toBeTruthy();
+    });
+});
