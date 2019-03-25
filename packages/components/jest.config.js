0f309394932006a4ed07bd9c4bb78ad43851e8c6
mmso <mmso@mmso.se>
2019-03-25 14:17:02 +0100

    Fix testing imports

--- packages/components/jest.config.js
+++ packages/components/jest.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-    setupFilesAfterEnv: ['./rtl.setup.js']
+    setupFilesAfterEnv: ['./rtl.setup.js'],
+    verbose: true,
+    moduleDirectories: ['node_modules', 'node_modules/proton-shared'],
+    transform: {
+        '^.+\\.js$': 'babel-jest'
+    },
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+        pmcrypto: '<rootDir>/__mocks__/pmcrypto.js',
+        'pm-srp': '<rootDir>/__mocks__/pm-srp.js'
+    },
+    transformIgnorePatterns: ['node_modules/(?!(proton-shared)/)']
 };
