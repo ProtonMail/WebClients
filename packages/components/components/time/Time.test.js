e82bb305de01035f556c72e3587da8822088cbbf
Richard <richard@protonmail.com>
2019-03-25 15:28:51 +0100

    Add default case for Time component

--- packages/components/components/time/Time.test.js
+++ packages/components/components/time/Time.test.js
@@ -6,13 +6,19 @@ import Time from './Time';
 describe('Time component', () => {
     const unixDate = 1552897937;
 
-    it('should display time', async () => {
+    it('should handle default when time is not set', () => {
+        const { container } = render(<Time />);
+
+        expect(container.firstChild.textContent).toBe('January 1, 1970');
+    });
+
+    it('should display time', () => {
         const { container } = render(<Time>{unixDate}</Time>);
 
         expect(container.firstChild.textContent).toBe('March 18, 2019');
     });
 
-    it('should support format', async () => {
+    it('should support format', () => {
         const { container } = render(<Time format="LLL">{unixDate}</Time>);
 
         expect(container.firstChild.textContent).toBe('March 18, 2019 9:32 AM');
