f9c982b65f4a20c28ea801d9dae50a6dea5123af
Richard <richard@protonmail.com>
2019-04-01 11:31:09 +0200

    Remove AddressModal in members section to match with design decision

--- packages/components/containers/members/MemberAddresses.js
+++ packages/components/containers/members/MemberAddresses.js
@@ -2,14 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { ngettext, msgid, c } from 'ttag';
-import { Dropdown, DropdownMenu, SmallButton, useModal, AddressModal } from 'react-components';
+import { Dropdown, DropdownMenu } from 'react-components';
 
 const MemberAddresses = ({ member }) => {
     const addresses = member.addresses || [];
     const title = addresses.map(({ Email }) => Email).join(', ');
     const list = addresses.map(({ Email: text }) => ({ text }));
     const n = list.length;
-    const { isOpen, open, close } = useModal();
 
     return (
         <>
@@ -19,15 +18,12 @@ const MemberAddresses = ({ member }) => {
                 content={ngettext(msgid`${n} address`, `${n} addresses`, n)}
             >
                 <DropdownMenu list={list} />
-                <div className="flex flex-spacebetween">
+                <div className="alignright">
                     <Link className="pm-button pm-button--small" to="/settings/addresses">{c(
                         'Link for member addresses'
                     ).t`Manage`}</Link>
-                    <SmallButton className="pm-button-primary" onClick={open}>{c('Action for member addresses')
-                        .t`Add`}</SmallButton>
                 </div>
             </Dropdown>
-            <AddressModal show={isOpen} onClose={close} member={member} />
         </>
     );
 };
