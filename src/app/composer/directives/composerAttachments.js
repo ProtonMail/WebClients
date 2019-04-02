ProtonMail/WebClients

commit ef8f5f2176aaaa0d6cb294cf2179e3fb0f1d70c9

Fix #7161 ~ Auto refresh translations service (#7162)
* Fix #7161 ~ Auto refresh translations service

Upgrade I18N

* Improve translator less events

* Fix transaltions signup link

Add tests + fix

Translate new keys and non standard

Fix I18N messages + single translations

Add new translatins

Upgrade latest components

* Fix storage issues + add tests + fix new translations

Fix linter

Fix storage naming + add translations

Add missing translation

Fix access translation ref

Fix test storage

Fix new component

New translations

New translations postcontact groups

* Add new translations

Remove useless service

Fix lint

src/app/composer/directives/composerAttachments.js
@@ -2,13 +2,14 @@ import _ from 'lodash';
 import { ENCRYPTED_STATUS } from '../../constants';
 
 /* @ngInject */
-function composerAttachments(embeddedUtils, gettextCatalog, dispatchers) {
+function composerAttachments(embeddedUtils, gettextCatalog, dispatchers, translator) {
+
     const CLASS_CLOSED = 'composerAttachments-close';
     const CLASS_HIDDEN = 'composerAttachments-hidden';
-    const labelHeader = {
+    const I18N = translator(() => ({
         show: gettextCatalog.getString('Show', null, 'Action'),
         hide: gettextCatalog.getString('Hide', null, 'Action')
-    };
+    }));
 
     /**
      * Toggle the panel
@@ -26,7 +27,7 @@ function composerAttachments(embeddedUtils, gettextCatalog, dispatchers) {
         const doAction = (action, label) => () => {
             action === 'remove' && el.classList.remove(CLASS_HIDDEN);
             el.classList[action](CLASS_CLOSED);
-            scope.$applyAsync(() => (scope.labelHeader = labelHeader[label]));
+            scope.$applyAsync(() => (scope.I18N = I18N[label]));
         };
 
         const open = doAction('remove', 'hide');
@@ -166,7 +167,7 @@ function composerAttachments(embeddedUtils, gettextCatalog, dispatchers) {
             const { on, unsubscribe } = dispatchers();
 
             scope.list = formatAttachments(scope, scope.message.Attachments);
-            scope.labelHeader = labelHeader.show;
+            scope.I18N = I18N.show;
 
             const requestSet = {};
 

commit e19a8080c63f7322eff88e451a77f64d287c2ab6

Fix #8703 - This bug has been introduced by ef8f5f2176aaaa0d6cb294cf2179e3fb0f1d70c9

src/app/composer/directives/composerAttachments.js
@@ -3,7 +3,6 @@ import { ENCRYPTED_STATUS } from '../../constants';
 
 /* @ngInject */
 function composerAttachments(embeddedUtils, gettextCatalog, dispatchers, translator) {
-
     const CLASS_CLOSED = 'composerAttachments-close';
     const CLASS_HIDDEN = 'composerAttachments-hidden';
     const I18N = translator(() => ({
@@ -27,7 +26,7 @@ function composerAttachments(embeddedUtils, gettextCatalog, dispatchers, transla
         const doAction = (action, label) => () => {
             action === 'remove' && el.classList.remove(CLASS_HIDDEN);
             el.classList[action](CLASS_CLOSED);
-            scope.$applyAsync(() => (scope.I18N = I18N[label]));
+            scope.$applyAsync(() => (scope.labelHeader = I18N[label]));
         };
 
         const open = doAction('remove', 'hide');
@@ -167,7 +166,7 @@ function composerAttachments(embeddedUtils, gettextCatalog, dispatchers, transla
             const { on, unsubscribe } = dispatchers();
 
             scope.list = formatAttachments(scope, scope.message.Attachments);
-            scope.I18N = I18N.show;
+            scope.labelHeader = I18N.show;
 
             const requestSet = {};
 