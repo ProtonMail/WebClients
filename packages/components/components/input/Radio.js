c7df6e208a7aa4ff8c5ecc29db3c59d35f563c28
dhoko <dhoko@users.noreply.github.com>
2019-04-08 12:00:44 +0200

    Match design system config for the radio (#33)

--- packages/components/components/input/Radio.js
+++ packages/components/components/input/Radio.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Radio = ({ id, ...rest }) => {
+const Radio = ({ id, children, ...rest }) => {
     return (
         <label htmlFor={id}>
             <input id={id} type="radio" className="pm-radio" {...rest} />
             <span className="pm-radio-fakeradio" />
+            {children}
         </label>
     );
 };
