bb0ce715735437c8c7f39d1b3b48ff383cc6a57c
Richard <richard@protonmail.com>
2019-04-09 15:39:16 +0200

    Add delete user route

--- packages/shared/lib/api/user.js
+++ packages/shared/lib/api/user.js
@@ -7,3 +7,9 @@ export const queryUnlock = () => ({
     url: 'users/unlock',
     method: 'put'
 });
+
+export const deleteUser = (data) => ({
+    url: 'users/delete',
+    method: 'put',
+    data
+});
