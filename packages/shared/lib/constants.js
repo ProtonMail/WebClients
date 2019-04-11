dd2a63e9a303844199b635c72b63033fa6251ede
EpokK <richard.d@protonmail.com>
2019-04-11 16:44:58 +0200

    Add MAX_SIZE_SCREENSHOT constant

--- packages/shared/lib/constants.js
+++ packages/shared/lib/constants.js
@@ -3,6 +3,7 @@ export const SECURE_SESSION_STORAGE_KEY = 'SECURE';
 export const MAILBOX_PASSWORD_KEY = 'proton:mailbox_pwd';
 export const UID_KEY = 'proton:oauth:UID';
 export const INTERVAL_EVENT_TIMER = 30 * 1000;
+export const MAX_SIZE_SCREENSHOT = 500 * 1000;
 export const EVENT_ACTIONS = {
     DELETE: 0,
     CREATE: 1,
