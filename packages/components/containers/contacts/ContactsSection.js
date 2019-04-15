aef2e2a762b57e27dbb192eaa78752e4880bcfb6
Richard <richard@protonmail.com>
2019-04-15 11:39:24 +0200

    Add contacts section component

---
+++ packages/components/containers/contacts/ContactsSection.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { c } from 'ttag';
+import {
+    SubTitle,
+    Row,
+    Label,
+    Toggle,
+    Info,
+    useToggle,
+    useNotifications,
+    useMailSettings,
+    useEventManager,
+    useApiWithoutResult
+} from 'react-components';
+import { updateAutoSaveContacts } from 'proton-shared/lib/api/mailSettings';
+
+const ContactsSection = () => {
+    const { createNotification } = useNotifications();
+    const { call } = useEventManager();
+    const [{ AutoSaveContacts } = {}] = useMailSettings();
+    const { request, loading } = useApiWithoutResult(updateAutoSaveContacts);
+    const { state, toggle } = useToggle(!!AutoSaveContacts);
+    const handleChange = async ({ target }) => {
+        await request(+target.checked);
+        await call();
+        toggle();
+        createNotification({ text: c('Success').t`Preference saved` });
+    };
+    return (
+        <>
+            <SubTitle>{c('Title').t`Contacts`}</SubTitle>
+            <Row>
+                <Label htmlFor="saveContactToggle">
+                    {c('Label').t`Automatically save contacts`}{' '}
+                    <Info url="https://protonmail.com/support/knowledge-base/autosave-contact-list/" />
+                </Label>
+                <Toggle id="saveContactToggle" disabled={loading} checked={state} onChange={handleChange} />
+            </Row>
+        </>
+    );
+};
+
+export default ContactsSection;
