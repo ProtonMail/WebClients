104daf4a2aad0da31c029eebd729b58545b1b65a
Richard <richard@protonmail.com>
2019-04-24 10:43:45 +0200

    Use our proper email regex for `EmailInput` component  to show error

--- packages/components/components/input/EmailInput.js
+++ packages/components/components/input/EmailInput.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { c } from 'ttag';
+import PropTypes from 'prop-types';
 
 import Input from './Input';
+import { isEmail } from '../../helpers/validators';
 
-const Email = ({ ...rest }) => <Input type="email" {...rest} />;
+const EmailInput = ({ value, ...rest }) => {
+    const error = isEmail(value) ? '' : c('Error').t`Email address invalid`;
+    return <Input type="email" error={error} {...rest} />;
+};
 
-export default Email;
+EmailInput.propTypes = {
+    value: PropTypes.string
+};
+
+EmailInput.defaultProps = {
+    value: ''
+};
+
+export default EmailInput;
