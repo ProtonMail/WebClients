03d8ec168f74fa7defe95072c7cf895b530b8e64
Richard <richard@protonmail.com>
2019-04-24 09:49:12 +0200

    Use sass for ColorPicker style

--- packages/components/components/input/ColorPicker.js
+++ packages/components/components/input/ColorPicker.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-components';
 import { ChromePicker } from 'react-color';
-import './ColorPicker.css';
+import './ColorPicker.scss';
 
 const ColorPicker = ({ text, initialRgbaColor, onColorChange, ...rest }) => {
     const [display, setDisplay] = useState(false);
