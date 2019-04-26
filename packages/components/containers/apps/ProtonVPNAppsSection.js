55e1e347d17479ec7f6904af95abef15e0674307
Richard <richard@protonmail.com>
2019-04-26 18:10:57 +0200

    Add spacing around links

--- packages/components/containers/apps/ProtonVPNAppsSection.js
+++ packages/components/containers/apps/ProtonVPNAppsSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { c } from 'ttag';
-import { Alert, SubTitle, Href } from 'react-components';
+import { Alert, SubTitle, Href, Block } from 'react-components';
 
 const ProtonVPNAppsSection = () => {
     return (
@@ -8,10 +8,14 @@ const ProtonVPNAppsSection = () => {
             <SubTitle>ProtonVPN apps</SubTitle>
             <Alert learnMore="https://protonvpn.com/support/">{c('Info')
                 .t`Go to the download page to get ProtonVPN. You can use you ProtonMail credentials to the log into the ProtonVPN native clients.`}</Alert>
-            <Href url="https://protonvpn.com/download/">{c('Link').t`Download page`}</Href>
+            <Block>
+                <Href url="https://protonvpn.com/download/">{c('Link').t`Download page`}</Href>
+            </Block>
             <Alert learnMore="https://protonvpn.com/support/vpn-login/">{c('Info')
                 .t`To connect to ProtonVPN with third party clients (e.g. Tunnelblick on MacOS or OopenVPN on GNU/Linux), you need specific credentials. to get these credentials, go to the Account settings when logged in at ProtonVPN.`}</Alert>
-            <Href url="https://account.protonvpn.com">{c('Link').t`ProtonVPN login`}</Href>
+            <Block>
+                <Href url="https://account.protonvpn.com">{c('Link').t`ProtonVPN login`}</Href>
+            </Block>
         </>
     );
 };
