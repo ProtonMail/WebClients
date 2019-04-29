9f556a5b7f7c34c73ab6c753cf97a8a1630251d3
Richard <richard@protonmail.com>
2019-04-29 11:24:16 +0200

    Add action to Activate bridge

--- packages/components/containers/apps/ProtonMailBridgeSection.js
+++ packages/components/containers/apps/ProtonMailBridgeSection.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { c } from 'ttag';
-import { SubTitle, Alert } from 'react-components';
+import { SubTitle, Alert, PrimaryButton, useUser, useNotifications } from 'react-components';
 
 const ProtonMailBridgeSection = () => {
+    const [{ isPaidMail }] = useUser();
+    const { createNotification } = useNotifications();
+    const handleClick = () =>
+        createNotification({
+            type: 'info',
+            text: c('Info').t`This feature is only available for paid users.`
+        });
     return (
         <>
             <SubTitle>ProtonMail Bridge</SubTitle>
             <Alert learnMore="https://protonmail.com/bridge/">{c('Info')
                 .t`ProtonMail Supports IMAP/SMTP via the ProtonMail Bridge application. Thunderbird, Microsoft Outlook, and Apple Mail are officially supported on both Windows and MacOS.`}</Alert>
+            {isPaidMail ? null : <PrimaryButton onClick={handleClick}>{c('Action').t`Activate`}</PrimaryButton>}
         </>
     );
 };
