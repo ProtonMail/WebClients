f2d35fc7cdccc53a5de228fdc2c711bc4aeb5735
mmso <mmso@mmso.se>
2019-05-02 15:58:09 +0200

    Fix new version of openpgp

--- packages/shared/lib/helpers/setupPmcrypto.js
+++ packages/shared/lib/helpers/setupPmcrypto.js
@@ -1,4 +1,5 @@
 import { init, createWorker } from 'pmcrypto';
+
 import { loadScript } from './dom';
 
 export const initMain = async (openpgpContents) => {
@@ -10,32 +11,12 @@ export const initMain = async (openpgpContents) => {
 
 export const initWorker = async (openpgpContents, openpgpWorkerContents) => {
     const workerUrl = URL.createObjectURL(
-        new Blob(['postMessage({ event: "load" });self.window = self;', openpgpContents, openpgpWorkerContents], {
+        new Blob(['self.window = self;', openpgpContents, openpgpWorkerContents], {
             type: 'text/javascript'
         })
     );
-
-    createWorker({
+    await createWorker({
         path: workerUrl
     });
-
-    if (window.openpgp.getWorker()) {
-        // Wait until all workers are loaded
-        await Promise.all(
-            window.openpgp.getWorker().workers.map(
-                (worker) =>
-                    new Promise((resolve) => {
-                        const { onmessage } = worker;
-                        // eslint-disable-next-line
-                        worker.onmessage = () => {
-                            // eslint-disable-next-line
-                            worker.onmessage = onmessage;
-                            resolve();
-                        };
-                    })
-            )
-        );
-    }
-
     URL.revokeObjectURL(workerUrl);
 };
