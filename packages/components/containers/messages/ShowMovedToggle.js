8317959f8ad3824c2b7a9ef5ad5dc07d296ee4e2
edu san <33841139+econdepe@users.noreply.github.com>
2019-05-15 12:54:46 +0200

    use SHOW_MOVED constant (#73)

--- packages/components/containers/messages/ShowMovedToggle.js
+++ packages/components/containers/messages/ShowMovedToggle.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Toggle, useApiWithoutResult, useEventManager, useToggle, useMailSettings } from 'react-components';
 import { updateShowMoved } from 'proton-shared/lib/api/mailSettings';
+import { SHOW_MOVED } from 'proton-shared/lib/constants';
+
+const { DRAFTS_AND_SENT, NONE } = SHOW_MOVED;
 
 const ShowMovedToggle = ({ id }) => {
     const [{ ShowMoved } = {}] = useMailSettings();
@@ -10,7 +13,7 @@ const ShowMovedToggle = ({ id }) => {
     const { state, toggle } = useToggle(!!ShowMoved);
 
     const handleChange = async ({ target }) => {
-        await request(target.checked ? 3 : 0);
+        await request(target.checked ? DRAFTS_AND_SENT : NONE);
         call();
         toggle();
     };
