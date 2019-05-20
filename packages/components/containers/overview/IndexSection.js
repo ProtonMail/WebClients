2b9ebc9f9044f5114cd6f72dea6cc1f095d5f4a1
Richard <richard@protonmail.com>
2019-05-20 15:05:01 +0200

    Review overview to filter paid features

--- packages/components/containers/overview/IndexSection.js
+++ packages/components/containers/overview/IndexSection.js
@@ -1,44 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useUser } from 'react-components';
 
-const Sections = ({ route, sections = [] }) => {
+const Sections = ({ route, sections = [], text }) => {
     return (
         <ul className="unstyled mt0-5">
-            {sections
-                .reduce((acc, { text, id }) => {
-                    acc.push({ text, id, route: `${route}#${id}` });
-                    return acc;
-                }, [])
-                .map(({ text, id, route }) => {
-                    return (
-                        <li key={id}>
-                            <Link to={route}>{text}</Link>
-                        </li>
-                    );
-                })}
+            {sections.length ? (
+                sections
+                    .reduce((acc, { text, id }) => {
+                        acc.push({ text, id, route: `${route}#${id}` });
+                        return acc;
+                    }, [])
+                    .map(({ text, id, route }) => {
+                        return (
+                            <li key={id}>
+                                <Link to={route}>{text}</Link>
+                            </li>
+                        );
+                    })
+            ) : (
+                <li>
+                    <Link to={route}>{text}</Link>
+                </li>
+            )}
         </ul>
     );
 };
 
 Sections.propTypes = {
     route: PropTypes.string,
-    sections: PropTypes.array
+    sections: PropTypes.array,
+    text: PropTypes.string
 };
 
 const IndexSection = ({ pages }) => {
+    const [{ hasPaidMail } = {}] = useUser();
     return (
         <div className="settings-grid-container">
-            {pages.map(({ text, route, sections = [] }) => {
-                return (
-                    <div key={route} className={`setting-grid ${sections.length > 4 ? 'setting-grid--tall' : ''}`}>
-                        <h2 className="h6 mb0-5">
-                            <strong>{text}</strong>
-                        </h2>
-                        {Sections({ route, sections })}
-                    </div>
-                );
-            })}
+            {pages
+                .filter(({ paidFeature }) => {
+                    if (hasPaidMail) {
+                        return true;
+                    }
+                    return !paidFeature;
+                })
+                .map(({ text, route, sections = [] }) => {
+                    return (
+                        <div key={route} className={`setting-grid ${sections.length > 4 ? 'setting-grid--tall' : ''}`}>
+                            <h2 className="h6 mb0-5">
+                                <strong>{text}</strong>
+                            </h2>
+                            {Sections({ route, sections, text })}
+                        </div>
+                    );
+                })}
         </div>
     );
 };
