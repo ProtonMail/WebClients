21a998338d4cd31e79b9c9ca65443b2dc3ccef4e
Richard <richard@protonmail.com>
2019-05-20 14:07:58 +0200

    Review app sections

--- packages/components/containers/apps/ProtonMailBridgeSection.js
+++ packages/components/containers/apps/ProtonMailBridgeSection.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { c } from 'ttag';
-import { SubTitle, Alert, PrimaryButton, useUser, useNotifications } from 'react-components';
+import { SubTitle, Alert, useUser } from 'react-components';
+import { Link } from 'react-router-dom';
 
 const ProtonMailBridgeSection = () => {
     const [{ hasPaidMail }] = useUser();
-    const { createNotification } = useNotifications();
-    const handleClick = () =>
-        createNotification({
-            type: 'info',
-            text: c('Info').t`This feature is only available for paid users.`
-        });
     return (
         <>
             <SubTitle>ProtonMail Bridge</SubTitle>
             <Alert learnMore="https://protonmail.com/bridge/">{c('Info')
                 .t`ProtonMail Supports IMAP/SMTP via the ProtonMail Bridge application. Thunderbird, Microsoft Outlook, and Apple Mail are officially supported on both Windows and MacOS.`}</Alert>
-            {hasPaidMail ? null : <PrimaryButton onClick={handleClick}>{c('Action').t`Activate`}</PrimaryButton>}
+            {hasPaidMail ? null : (
+                <Link to="/settings/subscription" className="pm-button pm-button--primary">{c('Action')
+                    .t`Upgrade`}</Link>
+            )}
         </>
     );
 };
