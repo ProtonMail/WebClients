8e8a33c02e5af918e2b9220a7f6f3aa6eb82bbe0
Richard <richard@protonmail.com>
2019-05-20 11:28:47 +0200

    Review username section to match with design

--- packages/components/containers/account/UsernameSection.js
+++ packages/components/containers/account/UsernameSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { c } from 'ttag';
-import { SubTitle, Label, Row, Field, useUser } from 'react-components';
+import { SubTitle, Label, Row, Field, Alert, useUser } from 'react-components';
 import { Link } from 'react-router-dom';
 
 const UsernameSection = () => {
@@ -13,11 +13,10 @@ const UsernameSection = () => {
             <Row>
                 <Label>{c('Label').t`Name`}</Label>
                 <Field>
-                    <span className="bold">{Name}</span>
-                    <br />
-                    {c('Info').jt`To manage your display name and signature, go to ${link}.`}
+                    <strong>{Name}</strong>
                 </Field>
             </Row>
+            <Alert>{c('Info').jt`To manage your display name and signature, go to ${link}.`}</Alert>
         </>
     );
 };
