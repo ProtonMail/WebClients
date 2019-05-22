38514329f76d40f6b8a95a51fb4d4ed8ac7fecf3
Richard <richard@protonmail.com>
2019-05-22 10:41:55 +0200

    Add caption for Table component

--- packages/components/components/table/Table.js
+++ packages/components/components/table/Table.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Table = ({ children, className }) => {
-    return <table className={`pm-simple-table ${className}`}>{children}</table>;
+const Table = ({ children, className, caption }) => {
+    return (
+        <table className={`pm-simple-table ${className}`}>
+            {caption ? <caption className="sr-only">{caption}</caption> : null}
+            {children}
+        </table>
+    );
 };
 
 Table.propTypes = {
     className: PropTypes.string,
-    children: PropTypes.node.isRequired
+    children: PropTypes.node.isRequired,
+    caption: PropTypes.string
 };
 
 Table.defaultProps = {
