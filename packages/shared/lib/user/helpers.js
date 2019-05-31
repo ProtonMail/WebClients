dbb5f2329fb98e62ce5adf992585e04a39e47f2a
mmso <mmso@mmso.se>
2019-05-31 08:57:15 +0200

    Add isPrivate flag to user

--- packages/shared/lib/user/helpers.js
+++ packages/shared/lib/user/helpers.js
@@ -5,6 +5,7 @@ const { ADMIN_ROLE, MEMBER_ROLE } = USER_ROLES;
 export const hasPaidMail = ({ Subscribed }) => Subscribed & 1;
 export const hasPaidVpn = ({ Subscribed }) => Subscribed & 4;
 export const isPaid = ({ Subscribed }) => Subscribed;
+export const isPrivate = ({ Private }) => Private === 1;
 export const isFree = (user) => !isPaid(user);
 export const isAdmin = ({ Role }) => Role === ADMIN_ROLE;
 export const isMember = ({ Role }) => Role === MEMBER_ROLE;
@@ -17,6 +18,7 @@ export const getInfo = (User) => {
         isMember: isMember(User),
         isFree: isFree(User),
         isPaid: isPaid(User),
+        isPrivate: isPrivate(User),
         isSubUser: isSubUser(User),
         isDelinquent: isDelinquent(User),
         hasPaidMail: hasPaidMail(User),
