c974512324fe20775db67626d15027a539ec27bb
mmso <mmso@mmso.se>
2019-05-31 10:19:59 +0200

    Add support for sending client secret from the config

--- packages/components/containers/app/createApi.js
+++ packages/components/containers/app/createApi.js
@@ -1,12 +1,13 @@
 import xhr from 'proton-shared/lib/fetch/fetch';
 import configureApi from 'proton-shared/lib/api';
 
-export default ({ CLIENT_ID, APP_VERSION, API_VERSION, API_URL }) => (UID) => {
+export default ({ CLIENT_ID, CLIENT_SECRET, APP_VERSION, API_VERSION, API_URL }) => (UID) => {
     return configureApi({
         xhr,
         UID,
         API_URL,
         CLIENT_ID,
+        CLIENT_SECRET,
         API_VERSION,
         APP_VERSION
     });
