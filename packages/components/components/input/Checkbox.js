52d4b1e7b65311f14c9a247750fe4aeddf4717ee
Richard <richard@protonmail.com>
2019-06-07 15:43:25 +0200

    Add indeterminate state for Checkbox component

--- packages/components/components/input/Checkbox.js
+++ packages/components/components/input/Checkbox.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import Icon from '../icon/Icon';
 
-const Checkbox = ({ id, checked, ...rest }) => {
+const Checkbox = ({ id, checked, indeterminate, ...rest }) => {
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.indeterminate = indeterminate;
+    }, [indeterminate]);
+
     return (
         <label htmlFor={id}>
-            <input id={id} type="checkbox" className="pm-checkbox" checked={checked} {...rest} />
+            <input ref={inputRef} id={id} type="checkbox" className="pm-checkbox" checked={checked} {...rest} />
             <span className="pm-checkbox-fakecheck">
                 <Icon className="pm-checkbox-fakecheck-img" name="on" />
             </span>
@@ -16,7 +22,12 @@ const Checkbox = ({ id, checked, ...rest }) => {
 
 Checkbox.propTypes = {
     id: PropTypes.string,
-    checked: PropTypes.bool.isRequired
+    checked: PropTypes.bool.isRequired,
+    indeterminate: PropTypes.bool
+};
+
+Checkbox.defaultProps = {
+    indeterminate: false
 };
 
 export default Checkbox;
