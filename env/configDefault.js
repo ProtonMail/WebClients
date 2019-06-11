158d73ab5a2c320f742edce05bba89d5cc4a9d02
dhoko <dhoko@pm.me>
2019-06-11 09:58:07 +0200

    Fix #8879 ~ Update article link config

--- env/configDefault.js
+++ env/configDefault.js
@@ -9,7 +9,7 @@ module.exports = {
     year: new Date().getFullYear(),
     clientID: 'Web',
     clientSecret: '4957cc9a2e0a2a49d02475c9d013478d',
-    articleLink: 'https://protonmail.com/blog/protonmail-v3-15-release-notes/',
+    articleLink: 'https://protonmail.com/blog/protonmail-v3-16-release-notes/',
     changelogPath: 'assets/changelog.tpl.html',
     versionPath: 'assets/version.json',
     translations: _.map(i18n, 'lang')
