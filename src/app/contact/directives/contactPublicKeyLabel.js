c4b3544fd6da1884deef420aeee5e9119bb48f06
dhoko <dhoko@pm.me>
2019-06-12 18:40:15 +0200

    Fix broken translations inside the app

--- src/app/contact/directives/contactPublicKeyLabel.js
+++ src/app/contact/directives/contactPublicKeyLabel.js
@@ -1,12 +1,18 @@
 /* @ngInject */
-function contactPublicKeyLabel() {
+function contactPublicKeyLabel(gettextCatalog, translator) {
+    const I18N = translator(() => ({
+        available: gettextCatalog.getString('Available Public Keys', null, 'Label'),
+        public: gettextCatalog.getString('Public Keys', null, 'Laebel'),
+        trusted: gettextCatalog.getString('Trusted Public Keys', null, 'Label')
+    }));
+
     return {
         restrict: 'E',
         replace: true,
         scope: {},
         templateUrl: require('../../../templates/directives/contact/contactPublicKeyLabel.tpl.html'),
         link(scope, el, { textTranslate }) {
-            scope.text = textTranslate;
+            scope.text = I18N[textTranslate];
         }
     };
 }
