9f09d44757882d8c949dc28c5134bd25bfbda98d
Nicolas Hoffmann <dante3333@gmail.com>
2019-06-19 12:10:30 +0200

    Message details/labels/fixes

--- src/app/ui/directives/icon.js
+++ src/app/ui/directives/icon.js
@@ -4,14 +4,14 @@ function icon() {
         restrict: 'E',
         replace: true,
         template: '<svg role="img" focusable="false"></svg>',
-        link(scope, el, { viewBox = '0 0 16 16', size = 16, fill = 'grey', name, style }) {
+        link(scope, el, { viewBox = '0 0 16 16', size = 16, fill = 'global-grey', name, style }) {
             el[0].innerHTML = `<use xlink:href="#shape-${name}"/>`;
             el[0].classList.add(`icon-${size}p`);
 
             style && el.prop('style', style);
 
             if (fill) {
-                el[0].classList.add(`fill-global-${fill}`);
+                el[0].classList.add(`fill-${fill}`);
             }
 
             el[0].setAttribute('viewBox', viewBox);
