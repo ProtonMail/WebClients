5a882cb7c04f92571754369ddd8c33109a9b0171
dhoko <dhoko@pm.me>
2019-06-25 17:09:07 +0200

    Icon default aria-hidden = true

--- src/app/ui/directives/icon.js
+++ src/app/ui/directives/icon.js
@@ -4,11 +4,12 @@ function icon() {
         restrict: 'E',
         replace: true,
         template: '<svg role="img" focusable="false"></svg>',
-        link(scope, el, { viewBox = '0 0 16 16', size = 16, fill = 'global-grey', name }) {
+        link(scope, el, { viewBox = '0 0 16 16', size = 16, fill = 'global-grey', name, ariaHidden = true }) {
             el[0].innerHTML = `<use xlink:href="#shape-${name}"/>`;
             el[0].classList.add(`icon-${size}p`);
             fill && el[0].classList.add(`fill-${fill}`);
             el[0].setAttribute('viewBox', viewBox);
+            el[0].setAttribute('aria-hidden', ariaHidden);
         }
     };
 }
