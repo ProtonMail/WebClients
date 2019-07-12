223472df0d57a7f1f7684878bb9a5f961df14554
mmso <mmso@mmso.se>
2019-07-12 11:22:23 +0200

    Fix test

--- packages/shared/test/keys/keys.spec.js
+++ packages/shared/test/keys/keys.spec.js
@@ -1,41 +1,37 @@
+import { decryptPrivateKey } from 'pmcrypto';
+
 import { DecryptableKey, OrganizationPrivateKey, NotDecryptableKey, SubKey } from './keys.data';
-import { prepareKeys } from '../../lib/keys/keys';
+import { prepareKeys, prepareMemberKeys } from '../../lib/keys/keys';
 
 describe('keys', () => {
     it('should prepare keys', async () => {
-        const preparedKeys = await prepareKeys({
-            Keys: [DecryptableKey, NotDecryptableKey],
-            keyPassword: '123'
-        });
+        const preparedKeys = await prepareKeys([DecryptableKey, NotDecryptableKey], '123');
 
-        expect(preparedKeys)
-            .toEqual([
-                {
-                    Key: DecryptableKey,
-                    privateKey: jasmine.any(Object)
-                },
-                {
-                    Key: NotDecryptableKey,
-                    privateKey: jasmine.any(Object)
-                }
-            ]);
+        expect(preparedKeys).toEqual([
+            {
+                Key: DecryptableKey,
+                privateKey: jasmine.any(Object)
+            },
+            {
+                Key: NotDecryptableKey,
+                privateKey: jasmine.any(Object)
+            }
+        ]);
 
         expect(preparedKeys[0].privateKey.isDecrypted()).toBeTruthy();
         expect(preparedKeys[1].privateKey.isDecrypted()).toBeFalsy();
     });
 
     it('should prepare the keys if a subuser', async () => {
-        const preparedKeys = await prepareKeys({
-            Keys: [SubKey],
-            keyPassword: '123',
-            OrganizationPrivateKey
-        });
+        const orgKey = await decryptPrivateKey(OrganizationPrivateKey, '123');
+        const preparedKeys = await prepareMemberKeys([SubKey], orgKey);
 
-        expect(preparedKeys)
-            .toEqual([{
+        expect(preparedKeys).toEqual([
+            {
                 Key: SubKey,
                 privateKey: jasmine.any(Object)
-            }]);
+            }
+        ]);
 
         expect(preparedKeys[0].privateKey.isDecrypted()).toBeTruthy();
     });
