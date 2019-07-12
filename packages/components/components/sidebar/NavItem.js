00026b7dd509e71a2590fdcea65e4a176ab5b8b3
Richard <richard@protonmail.com>
2019-07-12 15:06:00 +0200

    Review Sidebar

--- packages/components/components/sidebar/NavItem.js
+++ packages/components/components/sidebar/NavItem.js
@@ -2,11 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
-import Button from '../button/Button';
 import Icon from '../icon/Icon';
 import NavMenu from './NavMenu';
 
-const NavItem = ({ type, link, text, onClick, icon, list, color }) => {
+const NavItem = ({ type, link, text, onClick, icon, list, color, className }) => {
     const content = (
         <>
             {icon && <Icon fill="light" name={icon} color={color} className="mr1" />}
@@ -17,7 +16,7 @@ const NavItem = ({ type, link, text, onClick, icon, list, color }) => {
     if (type === 'link') {
         return (
             <li className="navigation__item">
-                <NavLink className="navigation__link ellipsis" to={link}>
+                <NavLink className={`navigation__link ellipsis ${className}`} to={link}>
                     {content}
                 </NavLink>
                 {list.length ? <NavMenu list={list} /> : null}
@@ -28,7 +27,7 @@ const NavItem = ({ type, link, text, onClick, icon, list, color }) => {
     if (type === 'text') {
         return (
             <li className="navigation__item">
-                <span className="navigation__link ellipsis">
+                <span className={`navigation__link ellipsis ${className}`}>
                     {content}
                     {list.length ? <NavMenu list={list} /> : null}
                 </span>
@@ -38,10 +37,10 @@ const NavItem = ({ type, link, text, onClick, icon, list, color }) => {
 
     if (type === 'button') {
         return (
-            <li className="navigation__item">
-                <Button className="w100" onClick={onClick}>
+            <li className="navigation__item pl1 pr1">
+                <button type="button" className={`w100 navigation__link ellipsis ${className}`} onClick={onClick}>
                     {content}
-                </Button>
+                </button>
                 {list.length ? <NavMenu list={list} /> : null}
             </li>
         );
@@ -57,11 +56,13 @@ NavItem.propTypes = {
     type: PropTypes.oneOf(['link', 'button', 'text']),
     link: PropTypes.string,
     text: PropTypes.string,
-    list: PropTypes.arrayOf(PropTypes.object)
+    list: PropTypes.arrayOf(PropTypes.object),
+    className: PropTypes.string
 };
 
 NavItem.defaultProps = {
     type: 'link',
+    className: '',
     list: []
 };
 
