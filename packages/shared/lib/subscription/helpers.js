5cf46aec3e997b47d92402285587e413c2d780d1
Vincas Stonys <vincaslt@gmail.com>
2019-07-18 15:54:38 +0300

    Helper to collect subscription plan titles (#30)

--- packages/shared/lib/subscription/helpers.js
+++ packages/shared/lib/subscription/helpers.js
@@ -1,4 +1,14 @@
+import { PLAN_TYPES } from '../constants';
+
+const { PLAN } = PLAN_TYPES;
+
 export const isManagedByMozilla = ({ CouponCode } = {}) => {
     const coupon = CouponCode || ''; // CouponCode can be null
     return coupon.startsWith('MOZILLA') || coupon.startsWith('MOZTEST');
+};
+
+export const getSubscriptionTitle = ({ Plans = [] }) => {
+    return Plans.filter(({ Type }) => Type === PLAN)
+        .map(({ Title }) => Title)
+        .join(', ');
 };
