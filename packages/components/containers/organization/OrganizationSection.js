ae2520fbfe5c8457b87481b7c56c18112a448c31
Richard <richard@protonmail.com>
2019-07-19 09:47:11 +0200

    Review OrganizationSection

--- packages/components/containers/organization/OrganizationSection.js
+++ packages/components/containers/organization/OrganizationSection.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { c } from 'ttag';
-import { Link } from 'react-router-dom';
 import { SubTitle, Alert, Row, Loader, Field, Label, SmallButton, useModals, useOrganization } from 'react-components';
-import humanSize from 'proton-shared/lib/helpers/humanSize';
 
 import RestoreAdministratorPrivileges from './RestoreAdministratorPrivileges';
 import OrganizationNameModal from './OrganizationNameModal';
@@ -15,18 +13,7 @@ const OrganizationSection = () => {
     if (loadingOrganization) {
         return <Loader />;
     }
-    const {
-        Name,
-        UsedMembers,
-        MaxMembers,
-        MaxSpace,
-        UsedVPN,
-        MaxVPN,
-        UsedDomains,
-        MaxDomains,
-        UsedAddresses,
-        MaxAddresses
-    } = organization;
+    const { Name } = organization;
 
     if (!organization.HasKeys) {
         return (
@@ -43,8 +30,8 @@ const OrganizationSection = () => {
         <>
             <RestoreAdministratorPrivileges />
             <SubTitle>{c('Title').t`Organization`}</SubTitle>
-            <Alert learnMore="todo">{c('Info')
-                .t`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium enim nec massa fringilla, ac ultrices tortor posuere. Fusce sed quam vitae arcu pharetra congue. Quisque in elementum nibh.`}</Alert>
+            <Alert>{c('Info')
+                .t`The name and logo will be visible to your users while logged in at mail.protonmail.com.`}</Alert>
             <Row>
                 <Label>{c('Label').t`Organization name`}</Label>
                 <Field>
@@ -52,36 +39,6 @@ const OrganizationSection = () => {
                     <SmallButton onClick={() => createModal(<OrganizationNameModal organizationName={Name} />)}>{c(
                         'Action'
                     ).t`Edit`}</SmallButton>
-                </Field>
-            </Row>
-            <Row>
-                <Label>{c('Label').t`Number of users`}</Label>
-                <Field>
-                    {`${UsedMembers}/${MaxMembers}`} <Link to="todo">{c('Link').t`Upgrade`}</Link>
-                </Field>
-            </Row>
-            <Row>
-                <Label>{c('Label').t`Organization storage`}</Label>
-                <Field>
-                    {humanSize(MaxSpace)} <Link to="todo">{c('Link').t`Upgrade`}</Link>
-                </Field>
-            </Row>
-            <Row>
-                <Label>{c('Label').t`VPN connections`}</Label>
-                <Field>
-                    {`${UsedVPN}/${MaxVPN}`} <Link to="todo">{c('Link').t`Upgrade`}</Link>
-                </Field>
-            </Row>
-            <Row>
-                <Label>{c('Label').t`Addresses`}</Label>
-                <Field>
-                    {`${UsedAddresses}/${MaxAddresses}`} <Link to="todo">{c('Link').t`Upgrade`}</Link>
-                </Field>
-            </Row>
-            <Row>
-                <Label>{c('Label').t`Domains`}</Label>
-                <Field>
-                    {`${UsedDomains}/${MaxDomains}`} <Link to="todo">{c('Link').t`Upgrade`}</Link>
                 </Field>
             </Row>
         </>
