18e5d28eebb7789b3af8f07f661a719214a1631d
edu san <33841139+econdepe@users.noreply.github.com>
2019-07-23 09:19:23 +0200

    Create helper function to round to decimal places

---
+++ packages/shared/lib/helpers/math.js
@@ -0,0 +1,15 @@
+/**
+ * Round a number, x, to a certain number, n, of decimal places.
+ * If n < 0, keep the significative digits up to 10 ** (-n)
+ */
+export const withDecimalPrecision = (x, n) => {
+    // assume n is an integer. Round to integer otherwise
+    const powerOfTen = 10 ** Math.round(n);
+    if (powerOfTen > Number.MAX_VALUE) {
+        return x;
+    }
+    if (powerOfTen < Number.MIN_VALUE) {
+        return 0;
+    }
+    return Math.round(x * powerOfTen) / powerOfTen;
+};
---
+++ packages/shared/test/helpers/math.spec.js
@@ -0,0 +1,31 @@
+import { withDecimalPrecision } from '../../lib/helpers/math';
+
+describe('math', () => {
+    describe('withDecimalPrecision', () => {
+        it('should return the same number when the precision is infinity', async () => {
+            const list = [2.234, 5, 381712.0001, -1, -1.38, -0.99, -10282.12312];
+            expect(list.map((x) => withDecimalPrecision(x, Infinity))).toEqual(list);
+        });
+    });
+
+    describe('withDecimalPrecision', () => {
+        it('should be equivalent to round when the precision is zero', async () => {
+            const list = [2.234, 5, 0, -1, -1.38, -0.99, -10282.12312];
+            expect(list.map((x) => withDecimalPrecision(x, 0))).toEqual(list.map(Math.round));
+        });
+    });
+
+    describe('withDecimalPrecision', () => {
+        it('should return 0 when the precision is minus infinity', async () => {
+            const list = [2.234, 5, 0, -1, -1.38, -0.99, -10282.12312];
+            expect(list.map((x) => withDecimalPrecision(x, -Infinity))).toEqual(list.map(() => 0));
+        });
+    });
+
+    describe('withDecimalPrecision', () => {
+        it('should return the expected values for some simple cases', async () => {
+            const list = [122.234, 5, -0.54, -1, -1.387, 12.4491];
+            expect(list.map((x, i) => withDecimalPrecision(x, i - 2))).toEqual([100, 10, -1, -1, -1.39, 12.449]);
+        });
+    });
+});
