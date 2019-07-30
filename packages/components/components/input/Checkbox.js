831ae8e2dbdd9ef7c90553a5a6d1d247e75d62a6
Richard <richard@protonmail.com>
2019-07-30 11:32:36 +0200

    Handle colors

--- packages/components/components/input/Checkbox.js
+++ packages/components/components/input/Checkbox.js
@@ -2,8 +2,9 @@ import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import Icon from '../icon/Icon';
+import { classnames } from '../../helpers/component';
 
-const Checkbox = ({ id, className, checked, indeterminate, ...rest }) => {
+const Checkbox = ({ id, className, checked, indeterminate, color, backgroundColor, ...rest }) => {
     const inputRef = useRef(null);
 
     useEffect(() => {
@@ -13,14 +14,16 @@ const Checkbox = ({ id, className, checked, indeterminate, ...rest }) => {
     return (
         <label htmlFor={id} className={className}>
             <input ref={inputRef} id={id} type="checkbox" className="pm-checkbox" checked={checked} {...rest} />
-            <span className="pm-checkbox-fakecheck">
-                <Icon className="pm-checkbox-fakecheck-img" name="on" />
+            <span className="pm-checkbox-fakecheck" style={{ background: backgroundColor, color }}>
+                <Icon className={classnames(['pm-checkbox-fakecheck-img', color && 'fill-currentColor'])} name="on" />
             </span>
         </label>
     );
 };
 
 Checkbox.propTypes = {
+    backgroundColor: PropTypes.string,
+    color: PropTypes.string,
     id: PropTypes.string,
     className: PropTypes.string,
     checked: PropTypes.bool.isRequired,
