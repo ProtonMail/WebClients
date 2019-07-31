2ab871459e668496e6a187a961d38d2810c13f51
mmso <mmso@mmso.se>
2019-07-31 10:08:17 +0200

    Fix condition for when keys for self can be generated

--- packages/components/containers/addresses/helper.js
+++ packages/components/containers/addresses/helper.js
@@ -1,7 +1,7 @@
 import { ADDRESS_STATUS, ADDRESS_TYPE, MEMBER_PRIVATE, RECEIVE_ADDRESS } from 'proton-shared/lib/constants';
 
 const { TYPE_ORIGINAL, TYPE_CUSTOM_DOMAIN, TYPE_PREMIUM } = ADDRESS_TYPE;
-const { READABLE, UNREADABLE } = MEMBER_PRIVATE;
+const { READABLE } = MEMBER_PRIVATE;
 
 export const getStatus = ({ address: { Status, Receive, DomainID, HasKeys }, i }) => {
     const isActive = ADDRESS_STATUS.STATUS_ENABLED && Receive === RECEIVE_ADDRESS.RECEIVE_YES;
@@ -26,9 +26,9 @@ export const getPermissions = ({
 }) => {
     const isSpecialAddress = Type === TYPE_ORIGINAL && Type === TYPE_PREMIUM;
 
+    const canGenerateMember = organizationKey && isAdmin && Private === READABLE;
     return {
-        canGenerate:
-            !HasKeys && organizationKey && ((isAdmin && Private === READABLE) || (Private === UNREADABLE && Self)),
+        canGenerate: !HasKeys && (canGenerateMember || Self),
         canDisable: Status === ADDRESS_STATUS.STATUS_ENABLED && isAdmin && !isSpecialAddress,
         canEnable: Status === ADDRESS_STATUS.STATUS_DISABLED && isAdmin && !isSpecialAddress,
         canDelete: Type === TYPE_CUSTOM_DOMAIN,
