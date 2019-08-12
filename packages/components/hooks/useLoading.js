7460c4e971b6c1790b9cf59e87b0cec23ec8693f
mmso <mmso@mmso.se>
2019-08-12 14:16:01 +0200

    Ensure the latest promise is running for useLoading

--- packages/components/hooks/useLoading.js
+++ packages/components/hooks/useLoading.js
@@ -8,6 +8,7 @@ import { useRef, useState, useEffect, useCallback } from 'react';
 const useLoading = (initialState = false) => {
     const [loading, setLoading] = useState(initialState);
     const unmountedRef = useRef(false);
+    const counterRef = useRef(0);
 
     useEffect(() => {
         return () => (unmountedRef.current = true);
@@ -18,13 +19,22 @@ const useLoading = (initialState = false) => {
             setLoading(false);
             return;
         }
+        const counterNext = counterRef.current + 1;
+        counterRef.current = counterNext;
         setLoading(true);
         return promise
             .then((result) => {
+                // Ensure that the latest promise is setting the new state
+                if (counterRef.current !== counterNext) {
+                    return;
+                }
                 !unmountedRef.current && setLoading(false);
                 return result;
             })
             .catch((e) => {
+                if (counterRef.current !== counterNext) {
+                    return;
+                }
                 !unmountedRef.current && setLoading(false);
                 throw e;
             });
