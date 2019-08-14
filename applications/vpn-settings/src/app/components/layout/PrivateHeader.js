1e48eae27f8403f6b9c99fd7ef7a653f8930bf25
Richard <richard@protonmail.com>
2019-08-14 21:12:23 +0200

    Use currentApp on header components

--- applications/vpn-settings/src/app/components/layout/PrivateHeader.js
+++ applications/vpn-settings/src/app/components/layout/PrivateHeader.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { MainLogo, SupportDropdown, UserDropdown } from 'react-components';
 import { APPS } from 'proton-shared/lib/constants';
 
+const { PROTONVPN_SETTINGS } = APPS;
+
 const PrivateHeader = () => {
     return (
         <header className="header flex flex-nowrap reset4print">
-            <MainLogo currentApp={APPS.PROTONVPN_SETTINGS} url="/account" />
+            <MainLogo currentApp={PROTONVPN_SETTINGS} url="/account" />
             <div className="searchbox-container"></div>
             <div className="topnav-container flex-item-centered-vert flex-item-fluid">
                 <ul className="topnav-list unstyled mt0 mb0 ml1 flex flex-nowrap">
                     <li className="mr1">
-                        <SupportDropdown />
+                        <SupportDropdown currentApp={PROTONVPN_SETTINGS} />
                     </li>
                     <li className="mlauto mtauto mbauto relative flex-item-noshrink">
-                        <UserDropdown />
+                        <UserDropdown currentApp={PROTONVPN_SETTINGS} />
                     </li>
                 </ul>
             </div>
