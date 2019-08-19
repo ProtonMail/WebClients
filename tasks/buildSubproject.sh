0eb8885faf63274f4bd339e63d2c6bc2ebe75898
dhoko <dhoko@pm.me>
2019-08-19 16:06:46 +0200

    Fix forward flag api

--- tasks/buildSubproject.sh
+++ tasks/buildSubproject.sh
@@ -32,6 +32,12 @@ fi;
 
 WEBCLIENT_DIR="$ROOT_DIR/${WEBCLIENT_APP:-Angular}";
 
+# Extract API flag and fallback default if it doesn't exist
+API_FLAG=$(echo "$@" | awk 'match($0, /--api=(\w{3,4})/) {
+    print substr($0, RSTART, RLENGTH)
+}' | awk -F '=' '{print $2}');
+API="${API_FLAG:-build}";
+
 # Output dir where we will store the dist version of protonmail-settings.
 # dist/settings will allow us to access to mail.protonmail.com/settings with protonmail-settings
 SETTINGS_DIST_DIR="dist/settings";
@@ -46,18 +52,24 @@ function addSubProject {
     fi
 
     rm -rf dist;
-    npm run build -- $@
+    npm run build -- $@ "--api=$API"
     cp -r dist/ "$WEBCLIENT_DIR/$2";
 }
 
+echo "[sub.build] $@" >> build.log;
+echo "[sub.build.api] $API" >> build.log;
+
 if [[ "$*" == *--deploy-subproject=settings* ]]; then
+    echo "[build] settings" >> build.log;
     addSubProject "${SETTINGS_APP:-protonmail-settings}" "$SETTINGS_DIST_DIR";
 fi
 
 if [[ "$*" == *--deploy-subproject=contacts* ]]; then
-    addSubProject "${CONTACTS_APP:-proton-contacts}" "$CONTACTS_DIST_DIR";
+    echo "[build] contacts" >> build.log;
+    addSubProject "${CONTACTS_APP:-proton-contacts}" "$CONTACTS_DIST_DIR" ;
 fi
 
 if [[ "$*" == *--deploy-subproject=calendar* ]]; then
+    echo "[build] calendar" >> build.log;
     addSubProject "${CALENDAR_APP:-proton-calendar}" "$CALENDAR_DIST_DIR";
 fi
