774fc8e9c9f3843f32bcd7defd01b58afb3f939d
Richard <richard@protonmail.com>
2019-08-20 19:50:29 +0200

    Remove ProtonVPN link in left sidebar

--- packages/components/containers/app/AppsSidebar.js
+++ packages/components/containers/app/AppsSidebar.js
@@ -2,17 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon, useUser } from 'react-components';
 import { APPS } from 'proton-shared/lib/constants';
-import { c } from 'ttag';
 
-const { PROTONMAIL, PROTONCONTACTS, PROTONCALENDAR, PROTONMAIL_SETTINGS, PROTONVPN_SETTINGS } = APPS;
+const { PROTONMAIL, PROTONCONTACTS, PROTONCALENDAR, PROTONMAIL_SETTINGS } = APPS;
 
 const AppsSidebar = ({ currentApp = '', items = [] }) => {
     const [{ isPaid }] = useUser();
     const apps = [
         { ids: [PROTONMAIL, PROTONMAIL_SETTINGS], icon: 'protonmail', title: 'ProtonMail', link: '/inbox' },
         isPaid && { ids: [PROTONCALENDAR], icon: 'calendar', title: 'ProtonCalendar', link: '/calendar' },
-        { ids: [PROTONCONTACTS], icon: 'contacts', title: 'ProtonContacts', link: '/contacts' },
-        { ids: [PROTONVPN_SETTINGS], icon: 'protonvpn', title: c('Title').t`ProtonVPN settings`, link: '/settings/vpn' }
+        { ids: [PROTONCONTACTS], icon: 'contacts', title: 'ProtonContacts', link: '/contacts' }
     ].filter(Boolean);
 
     return (
