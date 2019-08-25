f3337a7de279710c697d525544243801ece63e60
Nicolas Hoffmann <dante3333@gmail.com>
2019-08-25 20:43:40 +0200

    Fix VPNClientCard component (#216)

--- packages/components/containers/vpn/ProtonVPNClientsSection/VPNClientCard.js
+++ packages/components/containers/vpn/ProtonVPNClientsSection/VPNClientCard.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Bordered, Icon, Button, Block, Href } from 'react-components';
+import { Bordered, Icon, Block, Href } from 'react-components';
 import { c } from 'ttag';
 
 const VPNClientCard = ({ title, link, icon }) => {
     return (
-        <Bordered className="ml0-5 mr0-5 aligncenter">
+        <Bordered className="ml0-5 mr0-5 aligncenter relative">
             <div>
                 <Icon size={25} name={icon} />
             </div>
             <Block>{title}</Block>
-            <Href url={link}>
-                <Button>{c('Action').t`Download`}</Button>
+            <Href url={link} className="pm-button increase-surface-click">
+                {c('Action').t`Download`}
+                <span className="sr-only">{title}</span>
             </Href>
         </Bordered>
     );
