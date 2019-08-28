0843a531d7235d63e75b0427b3750c11d358a830
mmso <mmso@mmso.se>
2019-08-28 13:52:08 +0200

    Fix restore administrator privileges style

--- packages/components/containers/organization/RestoreAdministratorPrivileges.js
+++ packages/components/containers/organization/RestoreAdministratorPrivileges.js
@@ -27,13 +27,16 @@ const RestoreAdministratorPrivileges = () => {
     return (
         <Block>
             <Alert type="error">
-                {c('Restore administrator panel')
-                    .t`Due to a password change, your organization administrator privileges have been restricted. The following actions are no longer permitted:`}
-                <br />
-                <br />
-                {c('Restore administrator panel').t`* Creating new sub-users`}
-                <br />
-                {c('Restore administrator panel').t`* Reading emails of non-private subusers`}
+                <div>
+                    {c('Restore administrator panel')
+                        .t`Due to a password change, your organization administrator privileges have been restricted. The following actions are no longer permitted:`}
+                </div>
+                <ul>
+                    <li>{c('Restore administrator panel').t`Creating new sub-users`}</li>
+                    <li>{c('Restore administrator panel').t`Reading emails of non-private subusers`}</li>
+                    <li>{c('Restore administrator panel').t`Changing organization password`}</li>
+                    <li>{c('Restore administrator panel').t`Changing organization keys`}</li>
+                </ul>
             </Alert>
             <PrimaryButton
                 className="mr1"
