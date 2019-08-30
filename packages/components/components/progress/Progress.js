4d3b917873019fe58ed156cc94171ef580a7c1bf
Richard <richard@protonmail.com>
2019-08-30 17:24:21 +0200

    Use meter style

--- packages/components/components/progress/Progress.js
+++ packages/components/components/progress/Progress.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Progress = ({ value = 50, max = 100, ...rest }) => (
-    <progress className="progressBar" value={value} max={max} {...rest} />
-);
+import { classnames } from '../../helpers/component';
+
+const Progress = ({ value = 50, low = 0, min = 0, max = 100, id, ...rest }) => {
+    const high = value > 80 ? 80 : 50;
+    return (
+        <meter
+            aria-describedby={id}
+            className={classnames(['setting-meterbar inbl w100', value > 80 && 'setting-meterbar--high'])}
+            high={high}
+            low={low}
+            value={value}
+            min={min}
+            max={max}
+            {...rest}
+        />
+    );
+};
 
 Progress.propTypes = {
+    id: PropTypes.string,
     value: PropTypes.number,
-    max: PropTypes.number
+    max: PropTypes.number,
+    min: PropTypes.number,
+    low: PropTypes.number
 };
 
 export default Progress;
