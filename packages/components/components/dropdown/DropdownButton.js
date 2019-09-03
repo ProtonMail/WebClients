455bfce9c65d6fb2cc233230efe4b94f4b30a307
Richard <richard@protonmail.com>
2019-09-03 17:42:09 +0200

    Hotfix - correct size for arrow in dropdown

--- packages/components/components/dropdown/DropdownButton.js
+++ packages/components/components/dropdown/DropdownButton.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Button from '../button/Button';
 import PropTypes from 'prop-types';
 import DropdownCaret from './DropdownCaret';
+import { classnames } from 'react-components';
 
-const DropdownButton = ({ hasCaret = false, isOpen, children, ...rest }) => {
+const DropdownButton = ({ className = '', hasCaret = false, isOpen, children, ...rest }) => {
     return (
-        <Button aria-expanded={isOpen} {...rest}>
+        <Button className={classnames(['flex-item-noshrink', className])} aria-expanded={isOpen} {...rest}>
             <span className="mauto">
                 <span className={hasCaret && children ? 'mr0-5' : ''}>{children}</span>
                 {hasCaret && <DropdownCaret isOpen={isOpen} />}
@@ -17,7 +18,8 @@ const DropdownButton = ({ hasCaret = false, isOpen, children, ...rest }) => {
 DropdownButton.propTypes = {
     hasCaret: PropTypes.bool,
     isOpen: PropTypes.bool,
-    children: PropTypes.node
+    children: PropTypes.node,
+    className: PropTypes.string
 };
 
 export default DropdownButton;
