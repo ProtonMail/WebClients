6557dcafd8865c68807c5fd68841223571d70552
mmso <mmso@mmso.se>
2019-09-03 18:21:17 +0200

    Remove unnecessary wrapping

--- packages/components/containers/app/LoaderPage.js
+++ packages/components/containers/app/LoaderPage.js
@@ -11,14 +11,11 @@ const LoaderPage = () => {
     const { APP_NAME } = useConfig();
     const appName = APP_NAME === PROTONVPN_SETTINGS ? 'ProtonVPN' : 'ProtonMail';
     return (
-        // TODO: h100 does not seem to work.
-        <div className="w100 h100" style={{ height: '100vh' }}>
-            <div className="centered-absolute aligncenter">
-                <FullLoader color="global-light" size={200} />
-                <p className="atomLoader-text">
-                    {c('Info').t`Loading`} {appName}
-                </p>
-            </div>
+        <div className="centered-absolute aligncenter">
+            <FullLoader color="global-light" size={200} />
+            <p className="atomLoader-text">
+                {c('Info').t`Loading`} {appName}
+            </p>
         </div>
     );
 };
