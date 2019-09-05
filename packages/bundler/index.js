db12aea4fa7fe727c535b236cb00d4b08bfb8a6d
dhoko <dhoko@pm.me>
2019-09-05 10:53:16 +0200

    Prevent messages when we deploy

--- packages/bundler/index.js
+++ packages/bundler/index.js
@@ -230,7 +230,8 @@ async function main() {
 
     if (!isCI) {
         return logCommits(branch, flowType).then((data) => {
-            coucou.send(data);
+            const isDev = process.env.APP_KEY === 'Angular' && branch === 'deploy-dev';
+            /deploy-(beta|prod|old)/.test(branch) || (isDev && coucou.send(data));
         });
     }
 }
