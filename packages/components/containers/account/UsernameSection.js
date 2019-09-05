dcc6135da37cf56c65c72f61f4342461ee28c51f
Richard <richard@protonmail.com>
2019-09-05 09:05:59 +0200

    Allow user to activate his ProtonMail address
    Fix https://github.com/ProtonMail/proton-vpn-settings/issues/86

--- packages/components/containers/account/UsernameSection.js
+++ packages/components/containers/account/UsernameSection.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { c } from 'ttag';
-import { SubTitle, Label, Row, Field, useUser } from 'react-components';
+import { SubTitle, Label, Row, Field, Href, Loader, useUser, useAddresses, useConfig } from 'react-components';
+import { CLIENT_TYPES } from 'proton-shared/lib/constants';
+
+const { VPN } = CLIENT_TYPES;
 
 const UsernameSection = () => {
     const [{ Name }] = useUser();
+    const [addresses, loading] = useAddresses();
+    const [{ Email } = {}] = addresses || [];
+    const { CLIENT_TYPE } = useConfig();
 
     return (
         <>
@@ -14,6 +20,25 @@ const UsernameSection = () => {
                     <strong>{Name}</strong>
                 </Field>
             </Row>
+            {CLIENT_TYPE === VPN ? (
+                loading ? (
+                    <Loader />
+                ) : (
+                    <Row>
+                        <Label>{c('Label').t`ProtonMail address`}</Label>
+                        <Field className="pt0-5">
+                            {addresses.length ? (
+                                <strong>{Email}</strong>
+                            ) : (
+                                <Href
+                                    url="https://mail.protonmail.com/login"
+                                    title={c('Info').t`Log in to ProtonMail to activate your address`}
+                                >{c('Link').t`Not activated`}</Href>
+                            )}
+                        </Field>
+                    </Row>
+                )
+            ) : null}
         </>
     );
 };
