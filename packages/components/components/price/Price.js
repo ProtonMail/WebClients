8e42064e10e9ef76533861ac19ce3d97ba8240f6
Richard <richard@protonmail.com>
2019-09-09 12:03:14 +0200

    Review Price to not depend on string format

--- packages/components/components/price/Price.js
+++ packages/components/components/price/Price.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { toPrice } from 'proton-shared/lib/helpers/string';
+
+const CURRENCIES = {
+    USD: '$',
+    EUR: '€',
+    CHF: 'CHF'
+};
 
 const Price = ({ children: amount = 0, currency = '', className = '', divisor = 100, suffix = '' }) => {
-    return <span className={`price ${className}`}>{`${toPrice(amount, currency, divisor)} ${suffix}`.trim()}</span>;
+    const value = Number(amount / divisor).toFixed(2);
+    const c = <span className="currency">{CURRENCIES[currency] || currency}</span>;
+    const p = value < 0 ? <span className="prefix">-</span> : null;
+    const v = <span className="amount">{Math.abs(value)}</span>;
+    const s = suffix ? <span className="suffix">{suffix}</span> : null;
+
+    if (currency === 'USD') {
+        return (
+            <span className={`price ${className}`}>
+                {p}
+                {c}
+                {v}
+                {s}
+            </span>
+        ); // -$2/month
+    }
+
+    return (
+        <span className={`price ${className}`}>
+            {p}
+            {v} {c}
+            {s}
+        </span>
+    ); // -2 EUR/month
 };
 
 Price.propTypes = {
