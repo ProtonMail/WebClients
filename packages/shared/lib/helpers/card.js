c01d7a77c481e2d95170cb25ce208a75889cf570
Richard <richard@protonmail.com>
2019-09-09 17:02:02 +0200

    Review isExpired helper
    Fix https://github.com/ProtonMail/proton-vpn-settings/issues/177

--- packages/shared/lib/helpers/card.js
+++ packages/shared/lib/helpers/card.js
@@ -1,7 +1,13 @@
+/**
+ * Check if card is expired at the end of current month
+ * @param {Number} Details.ExpMonth
+ * @param {Number} Details.ExpYear
+ * @returns {Boolean}
+ */
 export const isExpired = ({ ExpMonth = 12, ExpYear = 9999 } = {}) => {
     const currentTime = new Date();
     const currentMonth = currentTime.getMonth() + 1;
     const currentYear = currentTime.getFullYear();
 
-    return currentMonth >= +ExpMonth && currentYear >= +ExpYear;
+    return currentMonth > +ExpMonth && currentYear >= +ExpYear;
 };
