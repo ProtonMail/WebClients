5a50bf811425025b5819914c0df8f0b8a030772a
Richard <richard@protonmail.com>
2019-09-09 08:55:33 +0200

    Define isBundleEligible subscription helper

--- packages/shared/lib/helpers/subscription.js
+++ packages/shared/lib/helpers/subscription.js
@@ -1,7 +1,8 @@
-import { PLAN_TYPES, PLAN_SERVICES } from '../constants';
+import { PLAN_TYPES, PLAN_SERVICES, PLANS } from '../constants';
 
 const { PLAN, ADDON } = PLAN_TYPES;
 const { MAIL } = PLAN_SERVICES;
+const { PLUS, VPNPLUS, VISIONARY } = PLANS;
 
 /**
  * Get plan from current subscription
@@ -25,4 +26,31 @@ export const getAddons = ({ Plans = [] } = {}) => Plans.filter(({ Type }) => Typ
 export const getPlanName = (subscription = {}, service = MAIL) => {
     const { Name = '' } = getPlan(subscription, service);
     return Name;
+};
+
+/**
+ * Check if a subscription is eligible to BUNDLE coupon
+ * @param {Object} subscription
+ * @returns {Boolean} is eligible to BUNDLE
+ */
+export const isBundleEligible = (subscription = {}) => {
+    const { Plans = [], CouponCode = '' } = subscription;
+
+    if (CouponCode) {
+        return false;
+    }
+
+    if (!Plans.length) {
+        return true;
+    }
+
+    const plans = getPlans(subscription);
+
+    if (plans.length > 1) {
+        return false;
+    }
+
+    const [{ Name = '' }] = plans;
+
+    return [PLUS, VPNPLUS, VISIONARY].includes(Name);
 };
