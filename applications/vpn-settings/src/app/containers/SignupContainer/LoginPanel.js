183263173bf9b1c391f88d8b18e90468936b288c
Richard <richard@protonmail.com>
2019-09-10 12:52:09 +0200

    Spacing

--- applications/vpn-settings/src/app/containers/SignupContainer/LoginPanel.js
+++ applications/vpn-settings/src/app/containers/SignupContainer/LoginPanel.js
@@ -6,7 +6,7 @@ const LoginPanel = () => {
     return (
         <div className="border-top mt2 pt2">
             <h3 className="mb1">{c('Title').t`Already have a Proton account?`}</h3>
-            <div className="mb1">{c('Info')
+            <div className="mb1-5">{c('Info')
                 .t`If you are a ProtonMail user you can use your Proton account to log in to ProtonVPN.`}</div>
             <div>
                 <Link className="pm-button--primaryborder" to="/login">{c('Link').t`Log in`}</Link>
