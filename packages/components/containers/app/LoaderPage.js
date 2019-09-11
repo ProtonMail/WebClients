5c68c320aa466acbb151df1eae8b8bf8f10f65a7
Vincas Stonys <vincaslt@gmail.com>
2019-09-11 12:13:23 +0300

    Extend loader page for use in signup (#269)

--- packages/components/containers/app/LoaderPage.js
+++ packages/components/containers/app/LoaderPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FullLoader, useConfig } from 'react-components';
 import { c } from 'ttag';
 import { APPS } from 'proton-shared/lib/constants';
@@ -7,17 +8,20 @@ import './loaderPage.scss';
 
 const { PROTONVPN_SETTINGS } = APPS;
 
-const LoaderPage = () => {
+const LoaderPage = ({ text, color = 'global-light' }) => {
     const { APP_NAME } = useConfig();
     const appName = APP_NAME === PROTONVPN_SETTINGS ? 'ProtonVPN' : 'ProtonMail';
     return (
         <div className="centered-absolute aligncenter">
-            <FullLoader color="global-light" size={200} />
-            <p className="atomLoader-text">
-                {c('Info').t`Loading`} {appName}
-            </p>
+            <FullLoader color={color} size={200} />
+            <p className="atomLoader-text">{text || c('Info').t`Loading ${appName}`}</p>
         </div>
     );
+};
+
+LoaderPage.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string
 };
 
 export default LoaderPage;
