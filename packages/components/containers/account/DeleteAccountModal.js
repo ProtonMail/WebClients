60d458ca503824dccbc0519c1cfccf502da8177f
Richard <EpokK@users.noreply.github.com>
2019-09-11 10:51:57 +0200

    Report bug first (#268)
    * Report bug first
    
    Fix https://github.com/ProtonMail/proton-vpn-settings/issues/209
    
    * Stop event manager and verify credentials before deleting

--- packages/components/containers/account/DeleteAccountModal.js
+++ packages/components/containers/account/DeleteAccountModal.js
@@ -11,6 +11,7 @@ import {
     TwoFactorInput,
     FormModal,
     Alert,
+    useEventManager,
     useAddresses,
     useApiWithoutResult,
     useUser,
@@ -21,13 +22,14 @@ import {
     useConfig,
     ErrorButton
 } from 'react-components';
-import { deleteUser } from 'proton-shared/lib/api/user';
+import { deleteUser, unlockPasswordChanges } from 'proton-shared/lib/api/user';
 import { reportBug } from 'proton-shared/lib/api/reports';
 import { srpAuth } from 'proton-shared/lib/srp';
 
 const DeleteAccountModal = ({ onClose, ...rest }) => {
     const { createNotification } = useNotifications();
     const { CLIENT_TYPE } = useConfig();
+    const eventManager = useEventManager();
     const api = useApi();
     const authentication = useAuthentication();
     const [{ isAdmin, Name } = {}] = useUser();
@@ -48,10 +50,14 @@ const DeleteAccountModal = ({ onClose, ...rest }) => {
     const handleSubmit = async () => {
         try {
             setLoading(true);
+
+            eventManager.stop();
+
+            // This is just used to verify that the entered password and totp code is correct
             await srpAuth({
                 api,
                 credentials: { password: model.password, totp: model.twoFa },
-                config: deleteUser()
+                config: unlockPasswordChanges()
             });
 
             if (isAdmin) {
@@ -70,15 +76,21 @@ const DeleteAccountModal = ({ onClose, ...rest }) => {
                     Description: model.feedback
                 });
             }
-            setLoading(false);
+
+            await srpAuth({
+                api,
+                credentials: { password: model.password, totp: model.twoFa },
+                config: deleteUser()
+            });
+
+            onClose();
+            createNotification({ text: c('Success').t`Account deleted` });
+            authentication.logout();
         } catch (error) {
+            eventManager.start();
             setLoading(false);
             throw error;
         }
-
-        onClose();
-        createNotification({ text: c('Success').t`Account deleted` });
-        authentication.logout();
     };
 
     return (
