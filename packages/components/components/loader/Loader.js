b8355a83b16ec9334befee422738ee286a49e746
Richard <richard@protonmail.com>
2019-09-11 12:27:29 +0200

    Add size prop on Loader component
    "small" is the default size

--- packages/components/components/loader/Loader.js
+++ packages/components/components/loader/Loader.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { c } from 'ttag';
+import loadingSvg from 'design-system/assets/img/shared/loading-atom.svg';
+import loadingSmallerSvg from 'design-system/assets/img/shared/loading-atom-smaller.svg';
 
-const Loader = () => <div className="square-color center mb2 mt0-5" aria-busy="true" />;
+const IMAGES = {
+    small: loadingSmallerSvg,
+    medium: loadingSvg,
+    big: loadingSvg
+};
+
+const MEDIUM_WIDTH = '80';
+const MEDIUM_HEIGHT = '80';
+
+const Loader = ({ size = 'small' }) => {
+    return (
+        <div className="center mb2 mt2">
+            <img
+                src={IMAGES[size]}
+                width={size === 'medium' && MEDIUM_WIDTH}
+                height={size === 'medium' && MEDIUM_HEIGHT}
+                alt={c('Info').t`Loading`}
+            />
+        </div>
+    );
+};
+
+Loader.propTypes = {
+    size: PropTypes.oneOf(['small', 'medium', 'big'])
+};
 
 export default Loader;
