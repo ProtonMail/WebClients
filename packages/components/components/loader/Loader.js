21b74c9e31d563277b48a73559e5f21408695d8c
Nicolas Hoffmann <dante3333@gmail.com>
2019-09-12 06:36:44 +0200

    Fix #276 - loader centering (#277)

--- packages/components/components/loader/Loader.js
+++ packages/components/components/loader/Loader.js
@@ -15,8 +15,9 @@ const MEDIUM_HEIGHT = '80';
 
 const Loader = ({ size = 'small' }) => {
     return (
-        <div className="center mb2 mt2">
+        <div className="center flex mb2 mt2">
             <img
+                className="mauto"
                 src={IMAGES[size]}
                 width={size === 'medium' ? MEDIUM_WIDTH : undefined}
                 height={size === 'medium' ? MEDIUM_HEIGHT : undefined}
