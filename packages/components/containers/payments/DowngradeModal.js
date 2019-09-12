32582d5b7741fd6f42fd445a03ca7b28f2f31b9c
Richard <richard@protonmail.com>
2019-09-12 13:51:53 +0200

    Update copy for DowngradeModal
    Fix https://github.com/ProtonMail/proton-vpn-settings/issues/225

--- packages/components/containers/payments/DowngradeModal.js
+++ packages/components/containers/payments/DowngradeModal.js
@@ -5,10 +5,9 @@ import { Alert, ConfirmModal } from 'react-components';
 const DowngradeModal = (props) => {
     return (
         <ConfirmModal title={c('Title').t`Confirm downgrade`} confirm={c('Action').t`Downgrade`} {...props}>
-            <Alert>{c('Info')
-                .t`This will downgrade your account to the Free plan. The Free plan is supported by donations and paid accounts. Please consider making a donation so we can continue offering our service for free.`}</Alert>
+            <Alert>{c('Info').t`Your account will be downgraded in a few minutes.`}</Alert>
             <Alert type="warning">{c('Info')
-                .t`Additional addresses, custom domains, and users must be removed/disabled before performing this action.`}</Alert>
+                .t`Additional addresses, custom domains, and users must be removed/disabled before performing this action. Any connections to premium servers will be terminated.`}</Alert>
         </ConfirmModal>
     );
 };
