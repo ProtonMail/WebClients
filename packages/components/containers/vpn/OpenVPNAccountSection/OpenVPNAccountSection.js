926bab25cd72d054eb0734481f00f211dfacc701
mmso <mmso@mmso.se>
2019-09-13 09:16:22 +0200

    Add toggle for OpenVPN password (#281)
    * Add toggle for OpenVPN password
    
    Fix https://github.com/ProtonMail/proton-vpn-settings/issues/233
    
    * Fix button icon size
    
    * Move copy first

--- packages/components/containers/vpn/OpenVPNAccountSection/OpenVPNAccountSection.js
+++ packages/components/containers/vpn/OpenVPNAccountSection/OpenVPNAccountSection.js
@@ -1,5 +1,16 @@
-import React from 'react';
-import { SubTitle, Alert, Row, Field, Label, Copy, PrimaryButton, useUserVPN, useModals } from 'react-components';
+import React, { useState } from 'react';
+import {
+    Button,
+    SubTitle,
+    Alert,
+    Row,
+    Field,
+    Label,
+    Copy,
+    PrimaryButton,
+    useUserVPN,
+    useModals
+} from 'react-components';
 import { c } from 'ttag';
 
 import OpenVPNCredentialsModal from './OpenVPNCredentialsModal';
@@ -9,6 +20,7 @@ const OpenVPNAccountSection = () => {
     const { result = {}, fetch: fetchUserVPN } = useUserVPN();
     const { VPN = {} } = result;
     const { Name = '', Password = '' } = VPN;
+    const [show, setShow] = useState(false);
 
     const handleEditCredentials = () => {
         createModal(<OpenVPNCredentialsModal username={Name} password={Password} fetchUserVPN={fetchUserVPN} />);
@@ -26,7 +38,7 @@ const OpenVPNAccountSection = () => {
                 <Label>{c('Label').t`OpenVPN / IKEv2 username`}</Label>
                 <Field>
                     <div className="pt0-5">
-                        <strong>{Name}</strong>
+                        <code>{Name}</code>
                     </div>
                 </Field>
                 <div className="ml1 flex-item-noshrink onmobile-ml0 onmobile-mt0-5">
@@ -37,13 +49,18 @@ const OpenVPNAccountSection = () => {
                 <Label>{c('Label').t`OpenVPN / IKEv2 password`}</Label>
                 <Field>
                     <div className="mb1 pt0-5">
-                        <strong>{Password}</strong>
+                        <code>{show ? Password : '••••••••••••••••••••'}</code>
                     </div>
                     <PrimaryButton disabled={!Name || !Password} onClick={handleEditCredentials}>{c('Action')
                         .t`Edit credentials`}</PrimaryButton>
                 </Field>
                 <div className="ml1 flex-item-noshrink onmobile-ml0 onmobile-mt0-5">
                     <Copy value={Password} />
+                    <Button
+                        icon={show ? 'unread' : 'read'}
+                        onClick={() => setShow(!show)}
+                        title={show ? c('Action').t`Hide` : c('Action').t`Show`}
+                    />
                 </div>
             </Row>
         </>
