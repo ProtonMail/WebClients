727e72b41eb0a1ecabb6bb4e074020ab19384492
Richard <richard@protonmail.com>
2019-09-13 10:15:29 +0200

    Add title on unsupported browser page
    Fix https://github.com/ProtonMail/proton-vpn-settings/issues/235

--- packages/shared/lib/browser/unsupported.js
+++ packages/shared/lib/browser/unsupported.js
@@ -3,19 +3,20 @@ import unsupportedBrowser from 'design-system/assets/img/shared/unsupported-brow
 
 const showUnsupported = () => {
     document.body.innerHTML = `
-    <div class="w50 p2 mt2 center big automobile">
-        <div class="aligncenter">
-            <h1>Unsupported browser</h1>
-            <p>
-                You are using an unsupported browser. Please update it to the latest version or use a different browser.
-            </p>
-            <a class="primary-link bold" target="_blank" rel="noopener noreferrer" href="https://protonmail.com/support/knowledge-base/browsers-supported/">More info</a>
+        <div class="w50 p2 mt2 center big automobile">
+            <div class="aligncenter">
+                <h1>Unsupported browser</h1>
+                <p>
+                    You are using an unsupported browser. Please update it to the latest version or use a different browser.
+                </p>
+                <a class="primary-link bold" target="_blank" rel="noopener noreferrer" href="https://protonmail.com/support/knowledge-base/browsers-supported/">More info</a>
+            </div>
+            <div class="mt2 aligncenter">
+                <img src=${unsupportedBrowser} alt="Unsupported browser"/>
+            </div>
         </div>
-        <div class="mt2 aligncenter">
-            <img src=${unsupportedBrowser} alt="Unsupported browser"/>
-        </div>
-    </div>
-`;
+    `;
+    document.title = 'Unsupported browser';
 };
 
 if (!window.protonSupportedBrowser) {
