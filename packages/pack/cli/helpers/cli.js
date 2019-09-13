21bc64e082e10834fbe65ca55b43bc954b2dddd4
dhoko <dhoko@pm.me>
2019-09-13 17:28:54 +0200

    Fix #24 ~ Escape path when we run a command

--- packages/pack/cli/helpers/cli.js
+++ packages/pack/cli/helpers/cli.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const execa = require('execa');
 
-const bash = (cli, args = []) => execa.shell(`${cli} ${args.join(' ')}`, { shell: '/bin/bash' });
+const bash = (cli, args = []) => execa.shell(`'${cli}' ${args.join(' ')}`, { shell: '/bin/bash' });
 const script = (cli, args = []) => {
     const cmd = path.resolve(__dirname, '..', '..', 'scripts', cli);
     return bash(cmd, args);
