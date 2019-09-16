962e93e68d20d414384ce6567e88d68e1b0eeb88
Richard <richard@protonmail.com>
2019-09-16 12:11:30 +0200

    Review News checkboxes to add spacing between row
    Fix https://github.com/ProtonMail/proton-vpn-settings/issues/251

--- packages/components/containers/account/NewsCheckboxes.js
+++ packages/components/containers/account/NewsCheckboxes.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import { c } from 'ttag';
-import {
-    Label,
-    Checkbox,
-    useUserSettings,
-    useEventManager,
-    useNotifications,
-    useApi,
-    useLoading
-} from 'react-components';
+import { Checkbox, useUserSettings, useEventManager, useNotifications, useApi, useLoading } from 'react-components';
 import { NEWS } from 'proton-shared/lib/constants';
 import { updateNews } from 'proton-shared/lib/api/settings';
 import { toggleBit, hasBit } from 'proton-shared/lib/helpers/bitset';
@@ -30,58 +22,25 @@ const NewsCheckboxes = () => {
 
     const handleChange = (mask) => () => withLoading(update(mask));
 
+    const checkboxes = [
+        { id: 'announcements', flag: ANNOUNCEMENTS, text: c('Label for news').t`Major announcements (2-3 per year)` },
+        { id: 'features', flag: FEATURES, text: c('Label for news').t`Major features (3-4 per year)` },
+        { id: 'business', flag: BUSINESS, text: c('Label for news').t`Proton Business (4-5 per year)` },
+        { id: 'newsletter', flag: NEWSLETTER, text: c('Label for news').t`Proton newsletter (5-6 per year)` },
+        { id: 'beta', flag: BETA, text: c('Label for news').t`Proton Beta (10-12 per year)` }
+    ];
+
     return (
         <ul className="unstyled">
-            <li>
-                <Label htmlFor="announcements">
-                    <Checkbox
-                        id="announcements"
-                        checked={hasBit(News, ANNOUNCEMENTS)}
-                        disabled={loading}
-                        onChange={handleChange(ANNOUNCEMENTS)}
-                    />
-                    {c('Label for news').t`Major announcements (2-3 per year)`}
-                </Label>
-            </li>
-            <li>
-                <Label htmlFor="features">
-                    <Checkbox
-                        id="features"
-                        checked={hasBit(News, FEATURES)}
-                        disabled={loading}
-                        onChange={handleChange(FEATURES)}
-                    />
-                    {c('Label for news').t`Major features (3-4 per year)`}
-                </Label>
-            </li>
-            <li>
-                <Label htmlFor="business">
-                    <Checkbox
-                        id="business"
-                        checked={hasBit(News, BUSINESS)}
-                        disabled={loading}
-                        onChange={handleChange(BUSINESS)}
-                    />
-                    {c('Label for news').t`Proton Business (4-5 per year)`}
-                </Label>
-            </li>
-            <li>
-                <Label htmlFor="newsletter">
-                    <Checkbox
-                        id="newsletter"
-                        checked={hasBit(News, NEWSLETTER)}
-                        disabled={loading}
-                        onChange={handleChange(NEWSLETTER)}
-                    />
-                    {c('Label for news').t`Proton newsletter (5-6 per year)`}
-                </Label>
-            </li>
-            <li>
-                <Label htmlFor="beta">
-                    <Checkbox id="beta" checked={hasBit(News, BETA)} disabled={loading} onChange={handleChange(BETA)} />
-                    {c('Label for news').t`Proton Beta (10-12 per year)`}
-                </Label>
-            </li>
+            {checkboxes.map(({ id, flag, text }) => {
+                return (
+                    <li key={id} className="mb0-5">
+                        <Checkbox checked={hasBit(News, flag)} disabled={loading} onChange={handleChange(flag)}>
+                            {text}
+                        </Checkbox>
+                    </li>
+                );
+            })}
         </ul>
     );
 };
