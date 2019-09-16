b806791c3e5b092a5b47504a9a862c0ad77c7fe0
Richard <richard@protonmail.com>
2019-09-16 10:28:06 +0200

    Wrap Info icon with a button
    Fix https://github.com/ProtonMail/proton-vpn-settings/issues/250

--- packages/components/components/link/Info.js
+++ packages/components/components/link/Info.js
@@ -1,30 +1,63 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
-import Href from './Href';
+import { generateUID, classnames } from '../../helpers/component';
 import Icon from '../icon/Icon';
-import Tooltip from '../tooltip/Tooltip';
+import { usePopper, Popper, usePopperAnchor } from '../popper';
+import useRightToLeft from '../../containers/rightToLeft/useRightToLeft';
 
-const wrapTooltip = (children, title) => <Tooltip title={title}>{children}</Tooltip>;
-const wrapLink = (children, url) => <Href url={url}>{children}</Href>;
+const Info = ({ url, title, originalPlacement = 'top', scrollContainerClass = 'main', ...rest }) => {
+    const [uid] = useState(generateUID('tooltip'));
 
-const Info = ({ url, title, ...rest }) => {
-    let children = <Icon className="icon-16p fill-primary" name="info" {...rest} />;
+    const { isRTL } = useRightToLeft();
+    const rtlAdjustedPlacement = originalPlacement.includes('right')
+        ? originalPlacement.replace('right', 'left')
+        : originalPlacement.replace('left', 'right');
 
-    if (url) {
-        children = wrapLink(children, url);
-    }
+    const popperRef = useRef();
+    const { anchorRef, open, close, isOpen } = usePopperAnchor();
+    const { position, placement } = usePopper(popperRef, anchorRef, isOpen, {
+        originalPlacement: isRTL ? rtlAdjustedPlacement : originalPlacement,
+        scrollContainerClass
+    });
 
-    if (title) {
-        children = wrapTooltip(children, title);
-    }
+    const handleClick = () => {
+        url && window.open(url);
+    };
 
-    return children;
+    return (
+        <>
+            <button
+                className="inline-flex"
+                onClick={handleClick}
+                ref={anchorRef}
+                onMouseEnter={open}
+                onMouseLeave={close}
+                onFocus={open}
+                onBlur={close}
+                aria-describedby={uid}
+                type="button"
+            >
+                <Icon className="icon-16p fill-primary" name="info" {...rest} />
+            </button>
+            <Popper
+                ref={popperRef}
+                id={uid}
+                isOpen={isOpen}
+                position={position}
+                className={classnames(['tooltip', `tooltip--${placement}`])}
+            >
+                {title}
+            </Popper>
+        </>
+    );
 };
 
 Info.propTypes = {
+    originalPlacement: PropTypes.oneOf(['top', 'bottom', 'left', 'right']),
     url: PropTypes.string,
-    title: PropTypes.node
+    title: PropTypes.node,
+    scrollContainerClass: PropTypes.string
 };
 
 export default Info;
