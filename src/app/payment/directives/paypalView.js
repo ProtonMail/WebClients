2cb1eec33f4081a3dab8810486a88a72adbe49a3
Richard <EpokK@users.noreply.github.com>
2019-09-20 14:04:23 +0200

    Generate Token + ApprovalURL when we display PayPal button (#9022)
    * Generate Token + ApprovalURL when we display PayPal method
    
    * Review

--- src/app/payment/directives/paypalView.js
+++ src/app/payment/directives/paypalView.js
@@ -1,8 +1,8 @@
 import { MIN_PAYPAL_AMOUNT, MAX_PAYPAL_AMOUNT } from '../../constants';
-import { handlePaymentToken } from '../helpers/paymentToken';
+import { process } from '../helpers/paymentToken';
 
 /* @ngInject */
-function paypalView(Payment, notification, paymentVerificationModal) {
+function paypalView(Payment, notification) {
     return {
         replace: true,
         restrict: 'E',
@@ -17,44 +17,60 @@ function paypalView(Payment, notification, paymentVerificationModal) {
             const Currency = scope.currency;
             const setLoading = (status) => scope.$applyAsync(() => (scope.loading = status));
 
-            if (type === 'payment' && Amount < MIN_PAYPAL_AMOUNT) {
-                scope.errorDetails = {
-                    type: 'validator.amount',
-                    validator: 'min',
-                    amount: MIN_PAYPAL_AMOUNT
-                };
-            }
+            const generateToken = async () => {
+                try {
+                    scope.$applyAsync(() => {
+                        scope.loading = true;
+                        scope.errorDetails = false;
+                    });
 
-            if (Amount > MAX_PAYPAL_AMOUNT) {
-                scope.errorDetails = {
-                    type: 'validator.amount',
-                    validator: 'max',
-                    amount: MAX_PAYPAL_AMOUNT
-                };
-            }
+                    const { Token, ApprovalURL } = await Payment.createToken({
+                        Amount,
+                        Currency,
+                        Payment: {
+                            Type: 'paypal'
+                        }
+                    });
+
+                    scope.$applyAsync(() => {
+                        scope.token = Token;
+                        scope.approvalURL = ApprovalURL;
+                        scope.loading = false;
+                    });
+                } catch (error) {
+                    setLoading(false);
+                }
+            };
+
+            scope.retry = () => generateToken();
 
             scope.openTab = async () => {
                 try {
                     setLoading(true);
-                    const requestBody = await handlePaymentToken({
-                        params: {
-                            Amount,
-                            Currency,
-                            Payment: {
-                                Type: 'paypal'
-                            }
-                        },
-                        paymentApi: Payment,
-                        paymentVerificationModal
-                    });
-                    const { Token = '' } = requestBody.Payment.Details;
-                    scope.paypalCallback({ Token });
+                    await process({ Token: scope.token, paymentApi: Payment, ApprovalURL: scope.approvalURL });
+                    scope.paypalCallback({ Token: scope.token });
                     setLoading(false);
                 } catch (error) {
                     setLoading(false);
                     error && error.message && notification.error(error.message);
                 }
             };
+
+            if (type === 'payment' && Amount < MIN_PAYPAL_AMOUNT) {
+                scope.errorDetails = {
+                    type: 'validator.amount',
+                    validator: 'min',
+                    amount: MIN_PAYPAL_AMOUNT
+                };
+            } else if (Amount > MAX_PAYPAL_AMOUNT) {
+                scope.errorDetails = {
+                    type: 'validator.amount',
+                    validator: 'max',
+                    amount: MAX_PAYPAL_AMOUNT
+                };
+            } else {
+                generateToken();
+            }
         }
     };
 }
