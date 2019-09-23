7b4ac7fd1032ec455890f2101274f3c82d5c9af8
Richard <richard@protonmail.com>
2019-09-23 11:34:22 +0200

    toCard only for "card" Type
    Fix https://github.com/ProtonMail/proton-vpn-settings/issues/272

--- packages/components/containers/paymentMethods/PaymentMethodActions.js
+++ packages/components/containers/paymentMethods/PaymentMethodActions.js
@@ -14,16 +14,19 @@ import { deletePaymentMethod, orderPaymentMethods } from 'proton-shared/lib/api/
 import { isExpired } from 'proton-shared/lib/helpers/card';
 import { PAYMENT_METHOD_TYPES } from 'proton-shared/lib/constants';
 
-const toCard = ({ Details = {} }) => {
-    return {
-        fullname: Details.Name,
-        month: Details.ExpMonth,
-        number: '',
-        year: Details.ExpYear.slice(-2),
-        cvc: '',
-        zip: Details.ZIP,
-        country: Details.Country
-    };
+const toCard = ({ Details = {}, Type }) => {
+    if (Type === 'card') {
+        return {
+            fullname: Details.Name,
+            month: Details.ExpMonth,
+            number: '',
+            year: Details.ExpYear.slice(-2),
+            cvc: '',
+            zip: Details.ZIP,
+            country: Details.Country
+        };
+    }
+    return Details;
 };
 
 const PaymentMethodActions = ({ method, onChange, methods, index }) => {
