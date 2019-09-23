1a774eb9f62bc77c1a9c963ce563b145250f4b7e
dhoko <dhoko@pm.me>
2019-09-23 12:01:53 +0200

    Extract issues fixing !current app

--- packages/bundler/scripts/logcommits.sh
+++ packages/bundler/scripts/logcommits.sh
@@ -9,22 +9,38 @@ function logCommit {
     echo
 }
 
-function extractDiffCommit {
+function printLine {
     local issueURL="$2";
+
+    local msg="$(git log "$1" --format=%s -n 1)";
+    local body="$(git log "$1" --format=%b -n 1 | xargs)";
+    local issueID="$(echo $msg | awk 'match($0, /#([0-9]{1,4})/, arr) {
+        print arr[1];
+    }')";
+
+    # Fix issue from another repository
+    if [[ "$body" =~ https:\/\/github\.com\/ProtonMail ]]; then
+        echo "- $msg ~ $body";
+        return 0;
+    fi;
+
+    # Fix issue from the current repository
+    if [ -n "$issueID" ]; then
+        echo "- $msg ~ $issueURL$issueID";
+        return 0;
+    fi;
+
+    # Log message
+    echo "- $msg";
+}
+
+function extractDiffCommit {
     local messageDeploy=$(git log --format=%b -n 1 "$(git rev-parse origin/deploy-$1)");
     local hash="$(echo $messageDeploy | awk -F 'commit' '{print $2}' | xargs | awk '{print $1}')";
 
     # We ignore commits from the CI
     for commit in $(git log "$hash"..HEAD --format=%H --invert-grep --grep="\[I18N@" --grep="Upgrade dependencies"); do
-        local msg="$(git log "$commit" --format=%s -n 1)";
-        local issueID="$(echo $msg | awk 'match($0, /#([0-9]{1,4})/, arr) {
-            print arr[1];
-        }')";
-        if [ -n "$issueID" ]; then
-            echo "- $msg ~ $issueURL$issueID";
-        else
-            echo "- $msg";
-        fi;
+        printLine "$commit" "$2";
     done;
 }
 
@@ -40,6 +56,16 @@ if [ "$1" = 'changelog' ]; then
       esac
       shift
     done;
+
+    if [ -z "$BRANCH" ] || [ -z "$ISSUE_URL" ]; then
+        echo "You must set a branch or an issue URL";
+        echo "Flags:"
+        printf '%-20s' "    --branch" "Name of the branch to use to create a diff changelog";
+        echo ""
+        printf '%-20s' "    --issue-url" "Repository issue URL";
+        echo ""
+        exit 1;
+    fi;
 
     extractDiffCommit $BRANCH $ISSUE_URL;
     exit;
