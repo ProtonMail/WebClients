1452f0328e419a7d17cd03e9f8db73f3984f4723
dhoko <dhoko@pm.me>
2019-09-24 14:23:09 +0200

    Upgrade execa

--- packages/bundler/lib/helpers/cli.js
+++ packages/bundler/lib/helpers/cli.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const execa = require('execa');
+const { debug } = require('./log')('proton-bundler');
 
-const bash = (cli, args = [], stdio) => execa.shell(`${cli} ${args.join(' ')}`, { shell: '/bin/bash', stdio });
+const bash = (cli, args = [], stdio) => {
+    debug({ cli, args, stdio }, 'bash');
+    return execa(cli, args, { shell: '/bin/bash', stdio });
+};
+
 const script = (cli, args = [], stdio) => {
     const cmd = path.resolve(__dirname, '..', '..', 'scripts', cli);
     return bash(cmd, args, stdio);
