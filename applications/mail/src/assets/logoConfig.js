f0d4c6beea5aebb77f6fcc6b4c940676488130ba
Richard <richard@protonmail.com>
2019-09-24 14:42:48 +0200

    Add logoConfig

---
+++ applications/mail/src/assets/logoConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+    logo: 'src/assets/protonmail.svg',
+    favicons: {
+        appName: 'ProtonMail',
+        appDescription:
+            "ProtonMail is the world's largest secure email service, developed by CERN and MIT scientists.We are open source and protected by Swiss privacy law",
+        developerName: 'Proton Technologies AG',
+        developerURL: 'https://github.com/ProtonMail/proton-mail',
+        background: '#262a33',
+        theme_color: '#262a33'
+    }
+};
