8e91dd0dd5c36494cf1066d5986b1e3554737dea
Richard <richard@protonmail.com>
2019-09-25 15:07:08 +0200

    Review with @matstaz

--- packages/components/containers/payments/subscription/Upgrading.js
+++ packages/components/containers/payments/subscription/Upgrading.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { c } from 'ttag';
-import { Loader } from 'react-components';
+import { Alert, Loader } from 'react-components';
 
 const Upgrading = () => {
     return (
         <>
-            <p className="aligncenter">{c('Info')
-                .t`Your account is being upgraded, this may take up to 30 seconds.`}</p>
+            <Alert>{c('Info').t`Your account is being upgraded, this may take up to 30 seconds.`}</Alert>
             <Loader />
         </>
     );
