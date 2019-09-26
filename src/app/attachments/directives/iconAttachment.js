e4656e21b5a0bcdec6624ae2c3856941bfd28886
dhoko <dhoko@users.noreply.github.com>
2019-09-26 17:13:16 +0200

    Icon attachments message (#9036)
    * Fix icon type inside messages
    
    * Fix lint

--- src/app/attachments/directives/iconAttachment.js
+++ src/app/attachments/directives/iconAttachment.js
@@ -1,14 +1,15 @@
 /* @ngInject */
-function iconAttachment(embedded) {
+function iconAttachment($compile) {
     const OUTER_MAP_CLASSNAME = {
-        zip: 'fa-archive-o',
-        mp3: 'fa-audio-o',
-        javascript: 'fa-code-o',
-        xls: 'fa-excel-o',
-        mov: 'fa-movie-o',
-        pdf: 'fa-pdf-o',
-        power: 'fa-powerpoint-o',
-        word: 'fa-word-o'
+        zip: 'file-rar-zip',
+        mp3: 'file-video',
+        javascript: 'file-unknown',
+        vcard: 'file-unknown',
+        xls: 'file-xls',
+        mov: 'file-video',
+        pdf: 'file-pdf',
+        power: 'file-ppt',
+        word: 'file-doc'
     };
 
     const INNER_MAP_CLASSNAME = {
@@ -18,27 +19,11 @@ function iconAttachment(embedded) {
     /**
      * Get list of classNames for a file
      * @param  {String} options.MIMEType
-     * @return {Array}
+     * @return {String}
      */
     const getFileIconsType = ({ MIMEType }) => {
-        return Object.keys(OUTER_MAP_CLASSNAME)
-            .filter((key) => MIMEType.includes(key))
-            .reduce((acc, key) => (acc.push(OUTER_MAP_CLASSNAME[key]), acc), []);
-    };
-
-    /**
-     * Get classNames for an attachemnt depending of its status
-     * @param  {Object} attachment
-     * @return {Array}
-     */
-    const getAttachmentType = (attachment) => {
-        if (embedded.isEmbedded(attachment)) {
-            return ['fa-picture-o', 'embedded'];
-        }
-
-        const list = ['fa-file-o'];
-        attachment.MIMEType.includes('image') && list.push('fa-image-o');
-        return list;
+        const key = Object.keys(OUTER_MAP_CLASSNAME).find((key) => MIMEType.includes(key));
+        return OUTER_MAP_CLASSNAME[key];
     };
 
     /**
@@ -48,26 +33,24 @@ function iconAttachment(embedded) {
      * @return {Array}
      */
     const getInnerFileIconTypes = ({ MIMEType }) => {
-        return Object.keys(INNER_MAP_CLASSNAME)
-            .filter((key) => MIMEType.includes(key))
-            .reduce((acc, key) => (acc.push(INNER_MAP_CLASSNAME[key]), acc), []);
+        const key = Object.keys(INNER_MAP_CLASSNAME).find((key) => MIMEType.includes(key));
+        return INNER_MAP_CLASSNAME[key];
+    };
+
+    const template = (outerIcon = 'file-image', single = true) => {
+        const secondIcon = !single ? '<icon data-name="key" class="file-inner-icon"></icon>`' : '';
+        const singleClass = single ? 'single' : '';
+
+        return `<icon data-name="${outerIcon}" data-size="20" class="mauto file-outer-icon ${singleClass}"></icon>${secondIcon}`;
     };
 
     return {
         replace: true,
         templateUrl: require('../../../templates/attachments/iconAttachment.tpl.html'),
         link(scope, el) {
-            //@todo we need to generate new icons.
-            const outerClassNames = []
-                .concat(getAttachmentType(scope.attachment))
-                .concat(getFileIconsType(scope.attachment));
-            const innerClassNames = getInnerFileIconTypes(scope.attachment);
-            // const fileOuterIcon = el[0].querySelector('.file-outer-icon');
-            const fileInnerIcon = el[0].querySelector('.file-inner-icon');
-
             _rAF(() => {
-                // outerClassNames.forEach((className) => fileOuterIcon.classList.add(className));
-                innerClassNames.forEach((className) => fileInnerIcon.classList.add(className));
+                const tpl = template(getFileIconsType(scope.attachment), !getInnerFileIconTypes(scope.attachment));
+                el.append($compile(tpl)(scope));
             });
         }
     };
