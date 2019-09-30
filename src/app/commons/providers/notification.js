194b9d05371dc52516e1b6d08a4a41ca427588c3
dhoko <dhoko@pm.me>
2019-09-30 14:47:00 +0200

    Fix #9049 ~ Button action for the notification

--- src/app/commons/providers/notification.js
+++ src/app/commons/providers/notification.js
@@ -64,7 +64,7 @@ function notification() {
                 if (options.undo) {
                     const content = sanitize.input(message);
                     options.onClick = options.undo;
-                    options.messageTemplate = `<div>${content} <a href="#" internal-link>${I18N.UNDO}</a></div>`;
+                    options.messageTemplate = `<div>${content} <button class="pm-button--link">${I18N.UNDO}</button></div>`;
                 }
 
                 notify({ message, ...options, onClose });
