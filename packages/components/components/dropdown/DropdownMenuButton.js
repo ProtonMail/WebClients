621c4a0282ad17589d05961def406e4a628016f6
Richard <richard@protonmail.com>
2019-10-01 21:38:22 +0200

    Handle loading state in DropdownMenuButton

--- packages/components/components/dropdown/DropdownMenuButton.js
+++ packages/components/components/dropdown/DropdownMenuButton.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DropdownMenuButton = ({ className = '', children, ...rest }) => {
+const DropdownMenuButton = ({ className = '', disabled, loading, children, ...rest }) => {
     return (
-        <button type="button" className={`w100 pt0-5 pb0-5 ${className}`} {...rest}>
+        <button type="button" disabled={disabled || loading} className={`w100 pt0-5 pb0-5 ${className}`} {...rest}>
             {children}
         </button>
     );
@@ -11,6 +11,8 @@ const DropdownMenuButton = ({ className = '', children, ...rest }) => {
 
 DropdownMenuButton.propTypes = {
     className: PropTypes.string,
+    loading: PropTypes.bool,
+    disabled: PropTypes.bool,
     children: PropTypes.node.isRequired
 };
 
