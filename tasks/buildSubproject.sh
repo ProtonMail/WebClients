1a5f89a84a4a24e4726a8df8cdfdf434d7f0e622
dhoko <dhoko@pm.me>
2019-10-02 13:29:49 +0200

    Lint subprojects

--- tasks/buildSubproject.sh
+++ tasks/buildSubproject.sh
@@ -93,6 +93,7 @@ function addSubProject {
 
     echo "[build.project] npm run build -- $@ "--api=$API" --verbose" >> build.log;
     rm -rf dist;
+    npm run lint;
     npm run build -- $@ "--api=$API" --verbose
     cp -r dist/ "$WEBCLIENT_DIR/$1";
 }
