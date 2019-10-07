561a7136d61d386c741d3cf152b20c55cb6bf787
edu san <33841139+econdepe@users.noreply.github.com>
2019-10-07 11:03:19 +0200

    Add proptypes to confirm modal (#333)
    * add proptypes
    
    * remove redundant proptype

--- packages/components/components/modal/Confirm.js
+++ packages/components/components/modal/Confirm.js
@@ -35,8 +35,8 @@ Confirm.propTypes = {
     onConfirm: PropTypes.func,
     title: PropTypes.string.isRequired,
     children: PropTypes.node.isRequired,
-    cancel: PropTypes.string,
-    confirm: PropTypes.string,
+    cancel: PropTypes.node,
+    confirm: PropTypes.node,
     loading: PropTypes.bool
 };
 
