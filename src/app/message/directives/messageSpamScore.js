a03d21e25334438e8b71cf3415365654d37358cf
Nicolas Hoffmann <dante3333@gmail.com>
2019-10-18 18:19:33 +0200

    Fix #9165 - alignment for mail infos (content/ml/etc.)  (#9169)
    * Fix #9165 - alignment for mail infos (content/ml/etc.)
    
    * Put back conditions = oops
    
    * Fix message buttons

--- src/app/message/directives/messageSpamScore.js
+++ src/app/message/directives/messageSpamScore.js
@@ -14,7 +14,7 @@ function messageSpamScore(gettextCatalog, translator) {
             "This email has failed its domain's authentication requirements. It may be spoofed or improperly forwarded! {{startLink}}Learn more{{endLink}}.",
             {
                 startLink:
-                    '</span><a href="https://protonmail.com/support/knowledge-base/email-has-failed-its-domains-authentication-requirements-warning/" target="_blank">',
+                    '</span><a href="https://protonmail.com/support/knowledge-base/email-has-failed-its-domains-authentication-requirements-warning/" class="flex-item-noshrink color-white bold pl0-5 pr0-5" target="_blank">',
                 endLink: '</a>'
             },
             'Info'
@@ -22,7 +22,8 @@ function messageSpamScore(gettextCatalog, translator) {
         [PHISHING]: gettextCatalog.getString(
             'This message may be a phishing attempt. Please check the sender and contents to make sure they are legitimate. {{startLink}}Learn more{{endLink}}.',
             {
-                startLink: '</span><a href="https://protonmail.com/blog/prevent-phishing-attacks/" target="_blank">',
+                startLink:
+                    '</span><a href="https://protonmail.com/blog/prevent-phishing-attacks/" class="flex-item-noshrink color-white bold pl0-5 pr0-5" target="_blank">',
                 endLink: '</a>'
             },
             'Info'
@@ -32,8 +33,8 @@ function messageSpamScore(gettextCatalog, translator) {
     return {
         replace: true,
         template: `
-            <div class="messageSpamScore-container color-white w100 bg-global-warning rounded p0-5 mb0-5 flex flex-nowrap">
-                <span class="messageSpamScore-notice flex flex-nowrap w100"></span>
+            <div class="messageSpamScore-container color-white w100 bg-global-warning rounded p0-5 mb0-5 flex">
+                <span class="messageSpamScore-notice flex w100"></span>
             </div>
         `,
         link(scope, el, { score }) {
@@ -41,7 +42,10 @@ function messageSpamScore(gettextCatalog, translator) {
 
             // regexp to remove ending . as we want to keep the translation and the new version is coming soon ;)
             if ([DMARC_FAILED, PHISHING].includes(score)) {
-                return ($notice.innerHTML = `<span>${I18N[score].replace(/\.$/, '')}`);
+                return ($notice.innerHTML = `<span class="flex-item-fluid-auto pl0-5 pr0-5">${I18N[score].replace(
+                    /\.$/,
+                    ''
+                )}`);
             }
             $notice.innerHTML = I18N[score].replace(/\.$/, '');
         }
