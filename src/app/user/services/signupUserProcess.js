6e5c4a7ed39a572b46721d788e76813469ab2e36
dhoko <dhoko@pm.me>
2019-10-18 11:43:20 +0200

    Fix #9166 ~ Save the current locale

--- src/app/user/services/signupUserProcess.js
+++ src/app/user/services/signupUserProcess.js
@@ -40,9 +40,7 @@ function signupUserProcess(
     }
 
     function setUserLanguage() {
-        if ($location.search().language) {
-            return settingsApi.updateLocale({ Locale: gettextCatalog.getCurrentLanguage() });
-        }
+        return settingsApi.updateLocale({ Locale: gettextCatalog.getCurrentLanguage() });
     }
 
     async function doLogUserIn() {
