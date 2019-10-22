dc622f96a03f973d3dbded48972556c4de918980
Richard <richard@protonmail.com>
2019-10-22 21:37:52 +0200

    Hotfix - Colorize floating button properly

--- packages/components/components/button/FloatingButton.js
+++ packages/components/components/button/FloatingButton.js
@@ -5,7 +5,7 @@ import { Icon } from 'react-components';
 const FloatingButton = ({ icon, title, ...rest }) => {
     return (
         <button type="button" className="compose-fab pm-button--primary flex" {...rest}>
-            <Icon size={25} fill="white" className="mauto" name={icon} />
+            <Icon size={25} className="fill-white mauto" name={icon} />
             {title ? <span className="sr-only">{title}</span> : null}
         </button>
     );
