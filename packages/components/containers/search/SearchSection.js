64d705fc897db05c9a815c3a42b0a9670ef2ede4
mmso <mmso@mmso.se>
2019-10-22 16:43:42 +0200

    Fix https://github.com/ProtonMail/proton-mail-settings/issues/175
    Add require exact match toggle

--- packages/components/containers/search/SearchSection.js
+++ packages/components/containers/search/SearchSection.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { c } from 'ttag';
-import { SubTitle } from 'react-components';
+import {
+    SubTitle,
+    Row,
+    Label,
+    Info,
+    Toggle,
+    Field,
+    useMailSettings,
+    useEventManager,
+    useLoading,
+    useApi
+} from 'react-components';
+import { updateAutoWildcardSearch } from 'proton-shared/lib/api/mailSettings';
 
 const SearchSection = () => {
+    const api = useApi();
+    const { call } = useEventManager();
+    const [{ AutoWildcardSearch }] = useMailSettings();
+    const [loading, withLoading] = useLoading();
     return (
         <>
             <SubTitle>{c('Title').t`Search`}</SubTitle>
-            TODO
+            <Row>
+                <Label htmlFor="exactMatchToggle">
+                    <span className="mr0-5">{c('Label').t`Require exact match`}</span>
+                    <Info url="https://protonmail.com/support/knowledge-base/search/" />
+                </Label>
+                <Field>
+                    <Toggle
+                        loading={loading}
+                        checked={!!AutoWildcardSearch}
+                        id="exactMatchToggle"
+                        onChange={({ target: { checked } }) => {
+                            withLoading(api(updateAutoWildcardSearch(+checked)).then(call));
+                        }}
+                    />
+                </Field>
+            </Row>
         </>
     );
 };
