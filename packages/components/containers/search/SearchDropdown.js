8f3d3fa1288e57c4a9d31ba31f4b5cb116b24376
Richard <richard@protonmail.com>
2019-10-23 12:21:27 +0200

    Review SimpleDropdown component

--- packages/components/containers/search/SearchDropdown.js
+++ packages/components/containers/search/SearchDropdown.js
@@ -1,44 +1,55 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { SearchInput, SimpleDropdown, Icon } from 'react-components';
-import { PrimaryButton } from '../../components/button';
+import { SearchInput, DropdownButton, Dropdown, Icon, usePopperAnchor, PrimaryButton } from 'react-components';
 import { c } from 'ttag';
+
+import { generateUID } from '../../helpers/component';
 
 const SearchDropdown = ({
     search: initialSearch = '',
     onSearch,
     placeholder = c('Placeholder').t`Search`,
+    content = <Icon name="search" />,
     ...rest
 }) => {
+    const [uid] = useState(generateUID('search-dropdown'));
+    const { anchorRef, isOpen, toggle, close } = usePopperAnchor();
     const [search, updateSearch] = useState(initialSearch);
 
     const handleSubmit = (event) => {
         event.preventDefault();
         onSearch(search);
         updateSearch('');
+        close();
     };
 
     return (
-        <SimpleDropdown autoClose={false} content={<Icon name="search" />} {...rest}>
-            <form className="p1" name="search-dropdown" onSubmit={handleSubmit}>
-                <div className="mb1">
-                    <SearchInput
-                        autoFocus={true}
-                        delay={0}
-                        value={search}
-                        onChange={(newValue) => updateSearch(newValue)}
-                        placeholder={placeholder}
-                    />
-                </div>
-                <div>
-                    <PrimaryButton className="w100" type="submit">{c('Action').t`Search`}</PrimaryButton>
-                </div>
-            </form>
-        </SimpleDropdown>
+        <>
+            <DropdownButton {...rest} buttonRef={anchorRef} isOpen={isOpen} onClick={toggle}>
+                {content}
+            </DropdownButton>
+            <Dropdown id={uid} autoClose={false} isOpen={isOpen} anchorRef={anchorRef} onClose={close}>
+                <form className="p1" name="search-dropdown" onSubmit={handleSubmit}>
+                    <div className="mb1">
+                        <SearchInput
+                            autoFocus={true}
+                            delay={0}
+                            value={search}
+                            onChange={(newValue) => updateSearch(newValue)}
+                            placeholder={placeholder}
+                        />
+                    </div>
+                    <div>
+                        <PrimaryButton className="w100" type="submit">{c('Action').t`Search`}</PrimaryButton>
+                    </div>
+                </form>
+            </Dropdown>
+        </>
     );
 };
 
 SearchDropdown.propTypes = {
+    content: PropTypes.node,
     search: PropTypes.string,
     onSearch: PropTypes.func,
     placeholder: PropTypes.string
