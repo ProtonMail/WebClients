cae6829e1120937643110ecfc02924244a9934c9
Richard <richard@protonmail.com>
2019-10-23 13:36:43 +0200

    Update layout to support new mobile layout

--- applications/vpn-settings/src/app/components/layout/PrivateLayout.js
+++ applications/vpn-settings/src/app/components/layout/PrivateLayout.js
@@ -1,15 +1,15 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { c } from 'ttag';
 import { Route } from 'react-router';
 import { withRouter } from 'react-router-dom';
-import { Sidebar, MainAreaContext } from 'react-components';
+import { Sidebar, MainAreaContext, useToggle } from 'react-components';
 
 import PrivateHeader from './PrivateHeader';
 
 const PrivateLayout = ({ children, location }) => {
     const mainAreaRef = useRef();
-    const [expanded, setExpand] = useState(false);
+    const { state: expanded, toggle: onToggleExpand, set: setExpand } = useToggle();
     const list = [
         {
             icon: 'dashboard',
@@ -42,9 +42,14 @@ const PrivateLayout = ({ children, location }) => {
     return (
         <div className="flex flex-nowrap no-scroll">
             <div className="content flex-item-fluid reset4print">
-                <PrivateHeader expanded={expanded} onToggleExpand={() => setExpand(!expanded)} />
+                <PrivateHeader expanded={expanded} onToggleExpand={onToggleExpand} />
                 <div className="flex flex-nowrap">
-                    <Route path="/:path" render={() => <Sidebar url="/account" expanded={expanded} list={list} />} />
+                    <Route
+                        path="/:path"
+                        render={() => (
+                            <Sidebar url="/account" expanded={expanded} onToggleExpand={onToggleExpand} list={list} />
+                        )}
+                    />
                     <div className="main flex-item-fluid main-area scroll-smooth-touch" ref={mainAreaRef}>
                         <div className="flex flex-reverse">
                             <MainAreaContext.Provider value={mainAreaRef}>{children}</MainAreaContext.Provider>
