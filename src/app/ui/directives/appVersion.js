52330c1c8e14010e9cb6486de2874bbe059aa1b8
dhoko <dhoko@pm.me>
2019-10-24 10:27:23 +0200

    Display v4 inside appVersion

--- src/app/ui/directives/appVersion.js
+++ src/app/ui/directives/appVersion.js
@@ -9,9 +9,7 @@ function appVersion(releaseNotesModal) {
     };
     return {
         replace: true,
-        template: `<a data-prefix="v" href="${CONFIG.articleLink}" title="${
-            CONFIG.date_version
-        }" target="_blank" class="color-white hover-same-color nodecoration">v${CONFIG.app_version}</a>`,
+        template: `<a data-prefix="v" href="${CONFIG.articleLink}" title="${CONFIG.date_version}" target="_blank" class="color-white hover-same-color nodecoration">v4.0.0</a>`,
         link(scope, element) {
             element.on('click', onClick);
 
