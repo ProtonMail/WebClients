08dd0e3ffe79d4cef3348e556b88fbc0b336f1c9
dhoko <dhoko@pm.me>
2019-10-30 17:49:57 +0100

    Fix linter

--- src/app/elements/directives/elementView.js
+++ src/app/elements/directives/elementView.js
@@ -1,7 +1,9 @@
 /* @ngInject */
 const elementView = (tools) => ({
     restrict: 'E',
-    template: `<div><conversation-view ng-if="type === 'conversation'"></conversation-view><message-view ng-if="type === 'message'"></message-view></div>`,
+    template: `<div>
+    <conversation-view ng-if="type === 'conversation'"></conversation-view><message-view ng-if="type === 'message'"></message-view>
+    </div>`,
     replace: true,
     link(scope) {
         scope.type = tools.typeView();
