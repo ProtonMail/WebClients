8a4cc2a36faa950ccd3d106b4e9b381eb6d750e3
dhoko <dhoko@pm.me>
2019-11-08 12:06:12 +0100

    Fix #9288 ~  Add scroll for reset password mobile

--- src/app/routes.js
+++ src/app/routes.js
@@ -277,7 +277,11 @@ export default angular
                     token: null
                 },
                 views: {
-                    'panel@support': {
+                    'main@': {
+                        controller: 'SupportController',
+                        templateUrl: require('../templates/layout/login.tpl.html')
+                    },
+                    'panel@support.reset-password': {
                         templateUrl: require('../templates/views/reset-login-password.tpl.html')
                     }
                 }
