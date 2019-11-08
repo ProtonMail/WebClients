4a93eaa0df2e547f284c012691d84ab4d2a2220a
Nicolas Hoffmann <dante3333@gmail.com>
2019-11-08 18:46:29 +0100

    Fix error in console/wrong type

--- applications/calendar/src/app/containers/calendar/CalendarContainerView.js
+++ applications/calendar/src/app/containers/calendar/CalendarContainerView.js
@@ -159,7 +159,7 @@ const CalendarContainerView = ({
                     />
                     {isLoading ? (
                         <div className="calendar-loader-container aligncenter p1">
-                            <FullLoader color="global-light" size="60" />
+                            <FullLoader color="global-light" size={60} />
                             <TextLoader className="m0">{c('Info').t`Loading events`}</TextLoader>
                         </div>
                     ) : null}
