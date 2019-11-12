511f6596941da9ed8c2c0c6b39ba844b7428be2a
Nicolas Hoffmann <dante3333@gmail.com>
2019-11-12 15:35:17 +0100

    Fix modal icon + positionning (#429)

--- packages/components/components/modal/Header.js
+++ packages/components/components/modal/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { c } from 'ttag';
+import { Icon } from 'react-components';
 import { classnames } from '../../helpers/component';
 
 const Header = ({ children, modalTitleID, className = '', hasClose = true, onClose, ...rest }) => {
@@ -8,7 +9,8 @@ const Header = ({ children, modalTitleID, className = '', hasClose = true, onClo
         <header className={classnames(['pm-modalHeader', className])} {...rest}>
             {hasClose ? (
                 <button type="button" className="pm-modalClose" title={c('Action').t`Close modal`} onClick={onClose}>
-                    ×
+                    <Icon className="pm-modalClose-icon" name="close" />
+                    <span className="sr-only">{c('Action').t`Close modal`}</span>
                 </button>
             ) : null}
             <h1 id={modalTitleID} className="pm-modalTitle">
