b440473155939a60cfc447f077bf8c4ed6387520
Nicolas Hoffmann <dante3333@gmail.com>
2019-11-12 18:22:09 +0100

    Fix popover display info

--- applications/calendar/src/app/components/events/PopoverEventContent.js
+++ applications/calendar/src/app/components/events/PopoverEventContent.js
@@ -8,10 +8,6 @@ import { c } from 'ttag';
 import PopoverNotification from './PopoverNotification';
 import CalendarIcon from '../calendar/CalendarIcon';
 
-const PopoverRow = ({ children }) => <div className="flex flex-items-center flex-nowrap mb0-5">{children}</div>;
-
-PopoverRow.propTypes = { children: PropTypes.node.isRequired };
-
 const PopoverEventContent = ({ targetCalendar = {}, targetEvent = {}, model, formatTime }) => {
     const { Name: calendarName, Color } = targetCalendar;
 
@@ -34,41 +30,41 @@ const PopoverEventContent = ({ targetCalendar = {}, targetEvent = {}, model, for
 
     return (
         <>
-            <PopoverRow>
-                <Icon name="clock" className="flex-item-noshrink mr1" />
-                <div>
+            <div className="flex flex-nowrap mb0-5">
+                <Icon name="clock" className="flex-item-noshrink mr1 mt0-25" />
+                <div className="flex flex-column">
                     {model.isAllDay ? null : <span>{timeString}</span>}
                     <span>{dateString}</span>
                     {model.frequency ? <span></span> : null}
                 </div>
-            </PopoverRow>
+            </div>
             {model.location ? (
-                <PopoverRow>
+                <div className="flex flex-items-center flex-nowrap mb0-5">
                     <Icon title={c('Title').t`Location`} name="address" className="flex-item-noshrink mr1" />
                     <span className="break">{model.location}</span>
-                </PopoverRow>
+                </div>
             ) : null}
             {calendarName ? (
-                <PopoverRow>
+                <div className="flex flex-items-center flex-nowrap mb0-5">
                     <CalendarIcon color={Color} className="mr1" />
                     <span className="break">{calendarName}</span>
-                </PopoverRow>
+                </div>
             ) : null}
             {model.description ? (
-                <PopoverRow>
+                <div className="flex flex-items-center flex-nowrap mb0-5">
                     <Icon title={c('Title').t`Description`} name="note" className="flex-item-noshrink mr1" />
                     <p className="break mt0 mb0">{model.description}</p>
-                </PopoverRow>
+                </div>
             ) : null}
             {model.notifications && Array.isArray(model.notifications) && model.notifications.length ? (
-                <PopoverRow>
+                <div className="flex flex-items-center flex-nowrap mb0-5">
                     <Icon name="notifications-enabled" className="flex-item-noshrink mr1" />
                     <div>
                         {model.notifications.map((notification, i) => {
                             return <PopoverNotification key={i} notification={notification} formatTime={formatTime} />;
                         })}
                     </div>
-                </PopoverRow>
+                </div>
             ) : null}
         </>
     );
