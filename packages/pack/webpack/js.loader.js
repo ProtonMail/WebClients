c48a6a476b4e348c701bc4b5030b2a5dabda0b83
Vincas Stonys <vincaslt@gmail.com>
2019-11-14 16:55:02 +0200

    Update babel package versions and typescript to 3.7.2 (#31)

--- packages/pack/webpack/js.loader.js
+++ packages/pack/webpack/js.loader.js
@@ -63,6 +63,8 @@ module.exports = ({ isProduction }, flow) => {
                 plugins: [
                     '@babel/plugin-syntax-dynamic-import',
                     '@babel/plugin-proposal-object-rest-spread',
+                    '@babel/plugin-proposal-nullish-coalescing-operator',
+                    '@babel/plugin-proposal-optional-chaining',
                     ['@babel/plugin-proposal-class-properties', { loose: true }],
                     require('babel-plugin-lodash'),
                     '@babel/plugin-transform-runtime',
