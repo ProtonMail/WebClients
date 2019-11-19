c60d6c72587bce7a6476719d7c0a74dad36d250e
mmso <mmso@mmso.se>
2019-11-19 15:59:14 +0100

    Fix #125 - Force part day events into one day

--- applications/calendar/src/app/components/calendar/splitDayEventsInInterval.js
+++ applications/calendar/src/app/components/calendar/splitDayEventsInInterval.js
@@ -1,7 +1,7 @@
 import { differenceInCalendarDays, startOfDay, endOfDay, min, max } from 'proton-shared/lib/date-fns-utc';
 
 export const splitDayEventsInInterval = ({ events = [], min: minDate, max: maxDate }) => {
-    return events.reduce((acc, { start, end }, i) => {
+    return events.reduce((acc, { start, end, isAllDay, isAllPartDay }, i) => {
         const startDate = startOfDay(max(start, minDate));
         const endDate = endOfDay(min(end, maxDate));
 
@@ -9,7 +9,8 @@ export const splitDayEventsInInterval = ({ events = [], min: minDate, max: maxDa
             return acc;
         }
 
-        const calendarDaysDifference = differenceInCalendarDays(endDate, startDate);
+        // For part day events, lock it into one day even if it happens to span multiple days, but not considered an all day part event.
+        const calendarDaysDifference = isAllDay || isAllPartDay ? differenceInCalendarDays(endDate, startDate) : 0;
         const startIndex = differenceInCalendarDays(startDate, minDate);
 
         acc.push({
