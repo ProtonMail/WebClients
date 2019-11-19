73a24d2e358c2ff9cfc40adb74b498d5ac23e514
Richard <richard@protonmail.com>
2019-11-19 13:40:56 +0100

    Hotfix date for BF

--- packages/shared/lib/helpers/blackfriday.js
+++ packages/shared/lib/helpers/blackfriday.js
@@ -3,5 +3,5 @@ import { isWithinInterval } from 'date-fns';
 import { BLACK_FRIDAY } from '../constants';
 
 export const isBlackFridayPeriod = () => {
-    return isWithinInterval(new Date(), { start: BLACK_FRIDAY.START, end: BLACK_FRIDAY.END });
+    return isWithinInterval(new Date(), { start: new Date(BLACK_FRIDAY.START), end: new Date(BLACK_FRIDAY.END) });
 };
