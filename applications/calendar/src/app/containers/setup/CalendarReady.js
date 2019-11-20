c032bdcb262c4afa6dc09b05c8532432706b70e5
Nicolas Hoffmann <dante3333@gmail.com>
2019-11-20 15:21:49 +0100

    Fix - icon alignment in onboarding modal (calendar ready)

--- applications/calendar/src/app/containers/setup/CalendarReady.js
+++ applications/calendar/src/app/containers/setup/CalendarReady.js
@@ -4,13 +4,13 @@ import React from 'react';
 import calendarSvg from 'design-system/assets/img/pm-images/calendar.svg';
 
 const CalendarReady = () => {
-    const supportIcon = <Icon key="support-icon" name="support1" />;
+    const supportIcon = <Icon key="support-icon" name="support1" className="alignsub" />;
     return (
         <>
             <Alert>{c('Info')
                 .t`Your new calendar is now ready. All events in your calendar are encrypted and inaccessible to anybody other than you.`}</Alert>
-            <div className="aligncenter">
-                <img src={calendarSvg} alt="Calendar" />
+            <div className="aligncenter mb1">
+                <img src={calendarSvg} alt="" />
             </div>
             <Alert>{c('Info')
                 .jt`If you encounter a problem, you can reach our support team by clicking the ${supportIcon} button.`}</Alert>
