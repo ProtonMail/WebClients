0453cc3a63495615b61ec7a3e80f14c2134a3bda
dhoko <dhoko@pm.me>
2019-11-21 12:15:41 +0100

    Add CI to lint SVG

---
+++ packages/styles/tasks/lint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+for file in $(find . -type f -name "*.svg" -not -path "./node_modules/*"); do
+  echo "[LINT]: $file";
+  npx svgo "$file" -q 2>&1 -o /dev/null;
+done;
