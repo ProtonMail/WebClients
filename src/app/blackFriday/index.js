fbbe9cd71406c5183a853eee4111815062935265
dhoko <dhoko@pm.me>
2019-11-22 14:57:51 +0100

    Menache + hide button if not available

--- src/app/blackFriday/index.js
+++ src/app/blackFriday/index.js
@@ -1,15 +1,7 @@
-import blackFriday from './directives/blackFriday';
-import blackFridayCountdown from './directives/blackFridayCountdown';
 import blackFridayHandler from './factories/blackFridayHandler';
 import blackFridayModel from './factories/blackFridayModel';
-import blackFridayModal from './modals/blackFridayModal';
-import blackFridayModalOpener from './factories/blackFridayModalOpener';
 
 export default angular
     .module('proton.blackFriday', [])
-    .directive('blackFriday', blackFriday)
-    .directive('blackFridayCountdown', blackFridayCountdown)
     .factory('blackFridayModel', blackFridayModel)
-    .factory('blackFridayHandler', blackFridayHandler)
-    .factory('blackFridayModalOpener', blackFridayModalOpener)
-    .factory('blackFridayModal', blackFridayModal).name;
+    .factory('blackFridayHandler', blackFridayHandler).name;
