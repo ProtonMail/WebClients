74ce1752bbff81582a1f141a274f1a47b5042f3f
Richard <richard@protonmail.com>
2019-11-22 11:26:43 +0100

    Add isAfterBlackFriday helper

--- packages/shared/lib/helpers/blackfriday.js
+++ packages/shared/lib/helpers/blackfriday.js
@@ -1,7 +1,11 @@
-import { isWithinInterval } from 'date-fns';
+import { isWithinInterval, isAfter } from 'date-fns';
 
 import { BLACK_FRIDAY } from '../constants';
 
 export const isBlackFridayPeriod = () => {
     return isWithinInterval(new Date(), { start: new Date(BLACK_FRIDAY.START), end: new Date(BLACK_FRIDAY.END) });
+};
+
+export const isAfterBlackFriday = () => {
+    return isAfter(new Date(), new Date(BLACK_FRIDAY.END));
 };
