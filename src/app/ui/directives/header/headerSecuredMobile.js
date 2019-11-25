d0e42c6121d403a199a2bc7c87593c78ed57c511
dhoko <dhoko@pm.me>
2019-11-25 15:38:59 +0100

    Missing condition BF button mobile

--- src/app/ui/directives/header/headerSecuredMobile.js
+++ src/app/ui/directives/header/headerSecuredMobile.js
@@ -1,10 +1,31 @@
 /* @ngInject */
-function headerSecuredMobile() {
+function headerSecuredMobile(dispatchers, blackFridayModel) {
+    const IS_BLACK_FRIDAY_CLASS = 'navigation-is-black-friday';
+
     return {
         restrict: 'E',
         replace: true,
         controller: 'HeaderController',
-        templateUrl: require('../../../../templates/ui/header/headerSecuredMobile.tpl.html')
+        templateUrl: require('../../../../templates/ui/header/headerSecuredMobile.tpl.html'),
+        link(scope, element) {
+            const { on, unsubscribe } = dispatchers();
+            const update = () =>
+                element[0].classList[blackFridayModel.isDealPeriod(true) ? 'add' : 'remove'](IS_BLACK_FRIDAY_CLASS);
+
+            on('subscription', (event, { type = '' }) => {
+                type === 'update' && update();
+            });
+
+            on('updateUser', update);
+
+            on('blackFriday', (event, { type = '' }) => {
+                type === 'run' && update();
+            });
+
+            update();
+
+            scope.$on('$destroy', unsubscribe);
+        }
     };
 }
 export default headerSecuredMobile;
