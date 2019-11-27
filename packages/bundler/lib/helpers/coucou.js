6b2fad27a6a28f399ed929e7b948f20a9702a51f
dhoko <dhoko@users.noreply.github.com>
2019-11-27 10:17:53 +0100

    Issue with Node.JS + proxy (#8)
    * Use curl
    
    * Prevent proxy issue with Node.JS via curl

--- packages/bundler/lib/helpers/coucou.js
+++ packages/bundler/lib/helpers/coucou.js
@@ -1,6 +1,6 @@
-const https = require('https');
 const dedent = require('dedent');
 const { error, success, debug, warn } = require('./log')('proton-bundler');
+const { bash } = require('./cli');
 
 async function send(data, { env, mode = 'deploy', api }) {
     const requests = {
@@ -9,15 +9,13 @@ async function send(data, { env, mode = 'deploy', api }) {
                 return { warning: 'No deploy hook available' };
             }
 
-            const { pathname, host } = new URL(process.env.DEPLOY_MESSAGES_HOOK);
-            return { pathname, host, text: dedent`${data}` };
+            return { text: dedent`${data}`, url: process.env.DEPLOY_MESSAGES_HOOK };
         },
         changelog() {
             if (!process.env.CHANGELOG_QA_HOOK) {
                 return { warning: 'No changelog hook available' };
             }
 
-            const { pathname, host } = new URL(process.env.CHANGELOG_QA_HOOK);
             const scope = env === 'dev' ? `_available in a few minutes on ${env}_` : `_available on ${env}_`;
             const apiRow = api ? `API: *${api}*` : '';
             const text = dedent`
@@ -26,12 +24,12 @@ async function send(data, { env, mode = 'deploy', api }) {
 
                 ${data}
             `.trim();
-            return { pathname, host, text };
+            return { text, url: process.env.CHANGELOG_QA_HOOK };
         }
     };
 
     try {
-        const { text, pathname, host, warning } = requests[mode]();
+        const { warning, url, text } = requests[mode]();
 
         if (warning) {
             return warn(warning);
@@ -44,37 +42,8 @@ async function send(data, { env, mode = 'deploy', api }) {
             text
         });
 
-        const req = https.request(
-            {
-                host: 'proxy',
-                port: 443,
-                path: pathname,
-                method: 'POST',
-                headers: {
-                    host,
-                    'Content-Type': 'application/json',
-                    'Content-Length': Buffer.byteLength(body, 'utf8')
-                }
-            },
-            (res) => {
-                debug(res.statusCode);
-
-                res.on('data', (d) => {
-                    process.stdout.write(d);
-                });
-
-                if (res.statusCode === 200) {
-                    success('Message sent !');
-                }
-            }
-        );
-
-        req.on('error', (e) => {
-            console.error(e);
-            throw e;
-        });
-        req.write(body);
-        req.end();
+        await bash(`curl -X POST -H 'Content-type: application/json' --data '${body}' ${url}`);
+        success('Message sent !');
     } catch (e) {
         error(e);
     }
