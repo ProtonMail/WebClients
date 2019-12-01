5d3bb0ca248b9b73d9a0649e562e6d83cb30bbac
Richard <richard@protonmail.com>
2019-12-01 09:51:55 +0100

    Review how we interact with negative amount

--- packages/components/components/price/Price.js
+++ packages/components/components/price/Price.js
@@ -10,10 +10,9 @@ const CURRENCIES = {
 
 const Price = ({ children: amount = 0, currency = '', className = '', divisor = 100, suffix = '' }) => {
     const fixedValue = Number(amount / divisor).toFixed(2);
-    const absValue = Math.abs(fixedValue);
-    const value = ~~fixedValue === absValue ? absValue : fixedValue;
+    const value = fixedValue.replace('.00', '').replace('-', '');
     const c = <span className="currency">{CURRENCIES[currency] || currency}</span>;
-    const p = value < 0 ? <span className="prefix">-</span> : null;
+    const p = amount < 0 ? <span className="prefix">-</span> : null;
     const v = <span className="amount">{value}</span>;
     const s = suffix ? <span className="suffix">{suffix}</span> : null;
 
