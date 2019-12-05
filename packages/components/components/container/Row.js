5cb2dd9f9900c040aa7cf4099068e893bc545028
mmso <mmso@mmso.se>
2019-12-05 16:55:59 +0100

    Add options to disable collapse row with labels on mobile (#482)

--- packages/components/components/container/Row.js
+++ packages/components/components/container/Row.js
@@ -2,9 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { classnames } from '../../helpers/component';
 
-const Row = ({ children, className = '', ...rest }) => {
+const Row = ({ children, className = '', collapseOnMobile = true, ...rest }) => {
     return (
-        <div className={classnames(['flex flex-nowrap onmobile-flex-column mb1', className])} {...rest}>
+        <div
+            className={classnames(['flex flex-nowrap mb1', collapseOnMobile && 'onmobile-flex-column ', className])}
+            {...rest}
+        >
             {children}
         </div>
     );
@@ -12,6 +15,7 @@ const Row = ({ children, className = '', ...rest }) => {
 
 Row.propTypes = {
     children: PropTypes.node.isRequired,
+    collapseOnMobile: PropTypes.bool,
     className: PropTypes.string
 };
 
