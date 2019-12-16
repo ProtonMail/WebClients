741b269271027b8ef60f26ec61e3678f60f4ef5c
dhoko <dhoko@users.noreply.github.com>
2019-12-16 10:58:17 +0100

    Read i18n config from the config file (#492)

--- packages/components/containers/general/LanguageSection.js
+++ packages/components/containers/general/LanguageSection.js
@@ -8,6 +8,7 @@ import {
     Label,
     Select,
     useApi,
+    useConfig,
     useLoading,
     useNotifications,
     useForceRefresh,
@@ -18,33 +19,11 @@ import { updateLocale } from 'proton-shared/lib/api/settings';
 import loadLocale from 'proton-shared/lib/i18n/loadLocale';
 import { getBrowserLocale, getClosestMatches } from 'proton-shared/lib/i18n/helper';
 
-/* eslint-disable @typescript-eslint/camelcase */
-const LOCALES = {
-    cs_CZ: 'Čeština',
-    de_DE: 'Deutsch',
-    en_US: 'English',
-    es_ES: 'Español',
-    ca_ES: 'Español - català',
-    fr_FR: 'Français',
-    hr_HR: 'Hrvatski',
-    it_IT: 'Italiano',
-    ja_JP: '日本語',
-    nl_NL: 'Nederlands',
-    pl_PL: 'Polski',
-    pt_BR: 'Português, brasileiro',
-    ru_RU: 'Pусский',
-    ro_RO: 'Română',
-    tr_TR: 'Türkçe',
-    uk_UA: 'Українська',
-    zh_CN: '简体中文',
-    zh_TW: '繁體中'
-};
-/* eslint-enable @typescript-eslint/camelcase */
-
 const LanguageSection = ({ locales = {} }) => {
-    const [{ Locale }] = useUserSettings();
     const api = useApi();
     const { call } = useEventManager();
+    const { LOCALES = {} } = useConfig();
+    const [{ Locale }] = useUserSettings();
     const { createNotification } = useNotifications();
     const [loading, withLoading] = useLoading();
     const forceRefresh = useForceRefresh();
