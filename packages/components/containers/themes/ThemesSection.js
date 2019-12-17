eccf24e76441fd400cde84f6f957ead8b9ecbce6
edu san <33841139+econdepe@users.noreply.github.com>
2019-12-17 16:07:02 +0100

    temporarily disallow selecting dark mode (#498)

--- packages/components/containers/themes/ThemesSection.js
+++ packages/components/containers/themes/ThemesSection.js
@@ -14,11 +14,11 @@ import {
 } from 'react-components';
 import { updateTheme } from 'proton-shared/lib/api/mailSettings';
 import { getThemeIdentifier, stripThemeIdentifier } from 'proton-shared/lib/themes/helpers';
-import { DEFAULT_THEME, CUSTOM_THEME, PROTON_THEMES } from 'proton-shared/lib/themes/themes.js';
+import { DEFAULT_THEME, CUSTOM_THEME } from 'proton-shared/lib/themes/themes.js';
 
 import CustomThemeModal from './CustomThemeModal.js';
 
-const availableThemes = [DEFAULT_THEME, PROTON_THEMES.DARK, CUSTOM_THEME];
+const availableThemes = [DEFAULT_THEME, CUSTOM_THEME];
 
 const ThemesSection = () => {
     const api = useApi();
