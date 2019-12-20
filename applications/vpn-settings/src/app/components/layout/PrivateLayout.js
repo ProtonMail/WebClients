28e2454c2065a9e30686a70d7cb5371d30e729c2
Yiin <stanislovas.janonis@gmail.com>
2019-12-20 09:54:00 +0200

    Hide dashboard and payments pages for sub-user (#327)
    * Hide dashboard and payments pages for sub-user
    
    Fixes https://gitlab.protontech.ch/ProtonVPN/web/protonvpn-settings/issues/57
    
    * Update src/app/components/layout/PrivateLayout.js
    
    * Update src/app/components/layout/PrivateLayout.js
    
    * Update src/app/components/layout/PrivateLayout.js
    
    Co-authored-by: Richard <EpokK@users.noreply.github.com>

--- applications/vpn-settings/src/app/components/layout/PrivateLayout.js
+++ applications/vpn-settings/src/app/components/layout/PrivateLayout.js
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 import { c } from 'ttag';
 import { Route } from 'react-router';
 import { withRouter } from 'react-router-dom';
-import { Sidebar, MainAreaContext, useToggle } from 'react-components';
+import { Sidebar, MainAreaContext, useToggle, useUser } from 'react-components';
 
 import PrivateHeader from './PrivateHeader';
 
 const PrivateLayout = ({ children, location }) => {
     const mainAreaRef = useRef();
     const { state: expanded, toggle: onToggleExpand, set: setExpand } = useToggle();
+    const [{ canPay }] = useUser();
     const list = [
-        {
+        canPay && {
             icon: 'dashboard',
             text: c('Link').t`Dashboard`,
             link: '/dashboard'
@@ -26,12 +27,12 @@ const PrivateLayout = ({ children, location }) => {
             text: c('Link').t`Downloads`,
             link: '/downloads'
         },
-        {
+        canPay && {
             icon: 'payments',
             text: c('Link').t`Payments`,
             link: '/payments'
         }
-    ];
+    ].filter(Boolean);
 
     useEffect(() => {
         setExpand(false);
