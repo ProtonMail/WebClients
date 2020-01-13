58f0b818741753c20d062886808c60435fc92c54
mmso <mmso@mmso.se>
2020-01-13 19:21:08 +0100

    Fix refresh logout (#9373)

--- src/app/authentication/services/handle401.js
+++ src/app/authentication/services/handle401.js
@@ -31,10 +31,14 @@ function handle401($http, $q, authentication, authApi) {
                 // Don't display this error, the "invalid access token" error will be displayed.
                 .refresh({ suppress: [AUTH_COOKIES_REFRESH_INVALID, AUTH_REFRESH_TOKEN_INVALID] })
                 .then(clearPromise)
-                .catch(() => {
-                    logout();
-                    // If the refresh call fails, hide the "Refresh cookie fail" error and return "Invalid access token" instead
-                    return $q.reject(rejection);
+                .catch((e) => {
+                    // Any 4xx except 429 (retry) from the refresh route and the session is no longer valid
+                    if (e.status >= 400 && e.status <= 499 && e.status !== 429) {
+                        logout();
+                        // If the refresh call fails, hide the "Refresh cookie fail" error and return "Invalid access token" instead
+                        return $q.reject(rejection);
+                    }
+                    return $q.reject(e);
                 });
         }
 
