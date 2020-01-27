12aed77e1dcf0428d35d31e786c735b6ab49738f
Richard <richard@protonmail.com>
2020-01-27 10:43:47 +0100

    Review bitcoin detail view

--- packages/components/containers/payments/BitcoinDetails.js
+++ packages/components/containers/payments/BitcoinDetails.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { c } from 'ttag';
-import { Row, Label, Field, Copy } from 'react-components';
+import { Copy } from 'react-components';
 
 const BitcoinDetails = ({ amount, address }) => {
     return (
         <figcaption>
-            <Row>
-                <Label>{c('Label').t`Amount BTC`}</Label>
-                <Field>
-                    <span className="mt0-5">{amount}</span>
-                </Field>
-            </Row>
-            <Row>
-                <Label>{c('Label').t`BTC address`}</Label>
-                <Field className="flex">
-                    <span className="mt0-5 mr1 ellipsis" title={address}>
-                        {address}
-                    </span>
-                    <Copy value={address} />
-                </Field>
-            </Row>
+            <div>{c('Label').t`BTC amount`}</div>
+            <div className="flex flex-nowrap flex-items-center mb1">
+                <span className="mr1" title={amount}>
+                    {amount}
+                </span>
+                <Copy className="pm-button--small flex-item-noshrink" value={amount} />
+            </div>
+            <div>{c('Label').t`BTC address`}</div>
+            <div className="flex flex-nowrap flex-items-center mb1">
+                <span className="mr1 ellipsis" title={address}>
+                    {address}
+                </span>
+                <Copy className="pm-button--small flex-item-noshrink" value={address} />
+            </div>
         </figcaption>
     );
 };
