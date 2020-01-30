6b1cee3753dfcafc2a975d8f5adcf8c1e7905eb9
mmso <mmso@mmso.se>
2020-01-30 13:53:53 +0100

    Fix new openpgp version worker loading (#36)

--- packages/pack/webpack/helpers/openpgp.js
+++ packages/pack/webpack/helpers/openpgp.js
@@ -9,8 +9,7 @@ const getDefineObject = (publicPath, { filepath, integrity }) => {
     };
 };
 
-const transformWorkerContents = (path, contents) =>
-    contents.replace('self.window=self,importScripts("openpgp.min.js");', '');
+const transformWorkerContents = (path, contents) => contents.replace('importcripts("openpgp.min.js");', '');
 
 const transformCompatPath = ({ basename, ext, hash }) => [basename, 'compat', hash, ext].join('.');
 
