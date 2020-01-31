b191775be1ce9ea83188d4f71aa9566938ba2239
Richard <richard@protonmail.com>
2020-01-31 16:14:12 +0100

    Add hook to manage payment methods

---
+++ packages/components/hooks/useMethods.js
@@ -0,0 +1,4 @@
+import { PaymentMethodsModel } from 'proton-shared/lib/models/paymentMethodsModel';
+import createUseModelHook from './helpers/createModelHook';
+
+export default createUseModelHook(PaymentMethodsModel);
