d5664feb227755f5aef1f9aaae732cf106c3e70e
mmso <mmso@mmso.se>
2020-02-13 16:58:46 +0100

    Use StandardPrivateApp

--- applications/calendar/src/app/content/PrivateApp.js
+++ applications/calendar/src/app/content/PrivateApp.js
@@ -1,14 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import {
-    EventManagerProvider,
-    EventModelListener,
-    EventNotices,
-    ErrorBoundary,
-    LoaderPage,
-    ThemeInjector,
-    ModalsChildren
-} from 'react-components';
+import { StandardPrivateApp, ErrorBoundary, LoaderPage, useApi, useCache } from 'react-components';
 import { c } from 'ttag';
 import {
     UserModel,
@@ -18,9 +10,11 @@ import {
     AddressesModel,
     MailSettingsModel
 } from 'proton-shared/lib/models';
+import { loadModels } from 'proton-shared/lib/models/helper';
+import updateLongLocale from 'proton-shared/lib/i18n/updateLongLocale';
 
-import CalendarPreload from './CalendarPreload';
 import MainContainer from '../containers/calendar/MainContainer';
+import { SETTINGS_TIME_FORMAT } from '../constants';
 
 const EVENT_MODELS = [
     UserModel,
@@ -31,44 +25,36 @@ const EVENT_MODELS = [
     AddressesModel
 ];
 
-const PRELOAD_MODELS = [UserModel, UserSettingsModel, MailSettingsModel, CalendarsModel, AddressesModel];
+const PRELOAD_MODELS = [UserModel, UserSettingsModel, MailSettingsModel, AddressesModel];
 
 const PrivateApp = ({ onLogout }) => {
-    const [loading, setLoading] = useState(true);
-    const eventManagerRef = useRef();
+    const api = useApi();
+    const cache = useCache();
 
     useEffect(() => {
         document.title = 'ProtonCalendar';
     }, []);
 
-    if (loading) {
-        return (
-            <>
-                <CalendarPreload
-                    locales={{}}
-                    preloadModels={PRELOAD_MODELS}
-                    onSuccess={(ev) => {
-                        eventManagerRef.current = ev;
-                        setLoading(false);
-                    }}
-                    onError={onLogout}
-                />
-                <ModalsChildren />
-                <LoaderPage text={c('Info').t`Loading ProtonCalendar`} />
-            </>
-        );
-    }
-
     return (
-        <EventManagerProvider eventManager={eventManagerRef.current}>
-            <ModalsChildren />
-            <EventModelListener models={EVENT_MODELS} />
-            <EventNotices />
-            <ThemeInjector />
+        <StandardPrivateApp
+            onLogout={onLogout}
+            locales={{} /* todo */}
+            onInit={async () => {
+                const calendars = await loadModels([CalendarsModel], { api, cache });
+                if (calendars && calendars.length) {
+                    // The calendar user settings can only be fetched if calendars have been setup.
+                    const [{ TimeFormat }] = await loadModels([CalendarUserSettingsModel], { api, cache });
+                    updateLongLocale({ displayAMPM: TimeFormat === SETTINGS_TIME_FORMAT.H12 });
+                }
+            }}
+            preloadModels={PRELOAD_MODELS}
+            eventModels={EVENT_MODELS}
+            fallback={<LoaderPage text={c('Info').t`Loading ProtonCalendar`} />}
+        >
             <ErrorBoundary>
                 <MainContainer />
             </ErrorBoundary>
-        </EventManagerProvider>
+        </StandardPrivateApp>
     );
 };
 
