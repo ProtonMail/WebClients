82c5d1eee1dde5312183837b93b50026761397a6
Nico Hoffmann <65-nhoffmann@users.noreply.gitlab.protontech.ch>
2020-02-17 13:09:11 +0000

    Fix #66 - iOS missing margin

--- applications/vpn-settings/src/app/components/page/Main.js
+++ applications/vpn-settings/src/app/components/page/Main.js
@@ -2,7 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Main = ({ className, children }) => {
-    return <main className={`main-area-content bg-white relative flex-item-fluid ${className}`}>{children}</main>;
+    return (
+        <main
+            className={`main-area-content main-area-content--paddingFix bg-white relative flex-item-fluid ${className}`}
+        >
+            {children}
+        </main>
+    );
 };
 
 Main.propTypes = {
