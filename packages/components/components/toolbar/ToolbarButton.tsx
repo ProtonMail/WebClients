6f4b527ac6bfc14f0978f7b66dcf38d9954db54d
Vincas Stonys <vincas.stonys@protonmail.com>
2020-02-24 15:18:20 +0000

    Disabled status improvements for toolbar button

--- packages/components/components/toolbar/ToolbarButton.tsx
+++ packages/components/components/toolbar/ToolbarButton.tsx
@@ -1,14 +1,22 @@
-import React, { HTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 import { classnames } from '../../helpers/component';
 import Icon, { Props as IconProps } from '../icon/Icon';
+import { noop } from 'proton-shared/lib/helpers/function';
 
-interface Props extends HTMLAttributes<HTMLButtonElement> {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     icon: string | IconProps;
 }
 
-const ToolbarButton = ({ icon, className, ...rest }: Props) => {
+const ToolbarButton = ({ icon, className, disabled, tabIndex, onClick, ...rest }: Props) => {
     return (
-        <button type="button" className={classnames([className, 'toolbar-button'])} {...rest}>
+        <button
+            type="button"
+            className={classnames([className, 'toolbar-button'])}
+            onClick={disabled ? noop : onClick}
+            tabIndex={disabled ? -1 : tabIndex}
+            disabled={disabled}
+            {...rest}
+        >
             {typeof icon === 'string' ? (
                 <Icon name={icon} className="toolbar-icon mauto" />
             ) : (
