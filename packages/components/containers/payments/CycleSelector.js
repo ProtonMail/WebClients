d0d2b4393206cec576c129d794f81b18d850ea45
Richard <richard@protonmail.com>
2020-02-24 14:16:33 +0100

    TWO YEARS option should always be available

--- packages/components/containers/payments/CycleSelector.js
+++ packages/components/containers/payments/CycleSelector.js
@@ -3,34 +3,26 @@ import PropTypes from 'prop-types';
 import { c } from 'ttag';
 import { Select } from 'react-components';
 import { CYCLE, DEFAULT_CYCLE } from 'proton-shared/lib/constants';
-import { isBlackFridayPeriod, isAfterBlackFriday } from 'proton-shared/lib/helpers/blackfriday';
 
 const { MONTHLY, YEARLY, TWO_YEARS } = CYCLE;
 
 const CycleSelector = ({
     cycle = DEFAULT_CYCLE,
     onSelect,
-    subscription = {},
     options = [
         { text: c('Billing cycle option').t`Monthly`, value: MONTHLY },
-        { text: c('Billing cycle option').t`Annually`, value: YEARLY }
+        { text: c('Billing cycle option').t`Annually`, value: YEARLY },
+        { text: c('Billing cycle option').t`Two-year`, value: TWO_YEARS }
     ],
     ...rest
 }) => {
-    const { Cycle = DEFAULT_CYCLE } = subscription;
     const handleChange = ({ target }) => onSelect(+target.value);
-
-    if (Cycle === TWO_YEARS || isBlackFridayPeriod() || isAfterBlackFriday()) {
-        options.push({ text: c('Billing cycle option').t`Two-year`, value: TWO_YEARS });
-    }
-
     return <Select value={cycle} options={options} onChange={handleChange} {...rest} />;
 };
 
 CycleSelector.propTypes = {
     cycle: PropTypes.oneOf([MONTHLY, YEARLY, TWO_YEARS]),
     onSelect: PropTypes.func.isRequired,
-    subscription: PropTypes.object,
     options: PropTypes.arrayOf(
         PropTypes.shape({
             text: PropTypes.string.isRequired,
