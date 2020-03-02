1b0bf2f173a830dcd748b61625dcae759fd94a5c
dhoko <dhoko@pm.me>
2020-03-02 13:15:09 +0100

    Upgrade to proton-translations

--- applications/mail/src/app/locales.js
+++ applications/mail/src/app/locales.js
@@ -1,4 +1,4 @@
-const locales = require.context('../i18n', true, /.json$/, 'lazy');
+const locales = require.context('proton-translations', true, /.json$/, 'lazy');
 
 export default locales.keys().reduce((acc, key) => {
     acc[key.slice(2, key.length - 5)] = () => locales(key);
