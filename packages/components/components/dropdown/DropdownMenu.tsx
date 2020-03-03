a2e1a18d9987d608c4987c072e32b5e1299e80da
Nicolas Hoffmann <dante3333@gmail.com>
2020-03-03 17:10:28 +0100

    Fix - MAILWEB-428 - hover effect in dropdown

--- packages/components/components/dropdown/DropdownMenu.tsx
+++ packages/components/components/dropdown/DropdownMenu.tsx
@@ -9,10 +9,10 @@ interface Props {
 const DropdownMenu = ({ children, className = '' }: Props) => {
     return (
         <div className="dropDown-content">
-            <ul className={classnames(['unstyled mt0 mb0 ml1 mr1', className])}>
+            <ul className={classnames(['unstyled mt0 mb0', className])}>
                 {React.Children.toArray(children).map((child, i) => {
                     return React.isValidElement(child) ? (
-                        <li className="dropDown-item" key={child.key || i}>
+                        <li className="dropDown-item pl1 pr1" key={child.key || i}>
                             {child}
                         </li>
                     ) : null;
