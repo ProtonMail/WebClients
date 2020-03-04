87e925d40767caa2840cf14a3952643f315ad2fd
mmso <mmso@mmso.se>
2020-03-04 15:20:11 +0100

    Add useCache option to loadModels

--- packages/shared/lib/models/helper.js
+++ packages/shared/lib/models/helper.js
@@ -1,20 +1,11 @@
 import { STATUS } from './cache';
 
-/**
- * Given a list of models, preload them and set them in the cache.
- * TODO: Add support for dependencies. E.g. to know if we can fetch
- * subscription model or organization model, we have fetch the user model first.
- * @param {Array} models
- * @param {Function} api
- * @param {Map} cache
- * @return {Promise<Array>}
- */
-export const loadModels = async (models = [], { api, cache }) => {
+export const loadModels = async (models = [], { api, cache, useCache = true }) => {
     const result = await Promise.all(
         models.map((model) => {
             // Special case to not re-fetch the model if it exists. This can happen for
             // the user model which is set at login.
-            if (cache.has(model.key)) {
+            if (useCache && cache.has(model.key)) {
                 return cache.get(model.key).value;
             }
             return model.get(api);
