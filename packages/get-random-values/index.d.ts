281de21a3afdfdd7b5c3114c681dbc967a42d4a4
mmso <mmso@mmso.se>
2020-03-10 17:41:15 +0100

    Add typings

---
+++ packages/get-random-values/index.d.ts
@@ -0,0 +1,1 @@
+export default function getRandomValues<T extends Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Uint8ClampedArray | Float32Array | Float64Array | DataView | null>(array: T): T;
