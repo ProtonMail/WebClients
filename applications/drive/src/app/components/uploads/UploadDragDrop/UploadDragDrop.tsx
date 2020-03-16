2cc7c5fa1af6bb7ad3675e800051531480c5e603
Mantvidas M <mantvidasm@Mantvidass-MacBook-Pro.local>
2020-03-16 10:48:43 +0200

    Lint errors fixed.

--- applications/drive/src/app/components/uploads/UploadDragDrop/UploadDragDrop.tsx
+++ applications/drive/src/app/components/uploads/UploadDragDrop/UploadDragDrop.tsx
@@ -10,7 +10,7 @@ interface UploadDragDropProps {
 }
 
 const UploadDragDrop = ({ children }: UploadDragDropProps) => {
-    const dropRef = createRef<HTMLDivElement>();
+    const dropAreaRef = createRef<HTMLDivElement>();
     const overlayRef = createRef<HTMLDivElement>();
 
     const { resource } = useDriveResource();
@@ -21,16 +21,16 @@ const UploadDragDrop = ({ children }: UploadDragDropProps) => {
     const dragDropEvents = [...dragOverEvents, 'dragend', 'dragleave', 'drag', 'drop'];
 
     useEffect(() => {
-        const handleDragOver = (e: any) => {
+        const handleDragOver = () => {
             setOverlayIsVisible(true);
         };
 
-        const handleDragLeave = (e: any) => {
+        const handleDragLeave = () => {
             setOverlayIsVisible(false);
         };
 
         const handleOnDrop = (e: any) => {
-            handleDragLeave(e);
+            handleDragLeave();
 
             const files = e.dataTransfer.files;
             if (!resource || !files) {
@@ -42,18 +42,18 @@ const UploadDragDrop = ({ children }: UploadDragDropProps) => {
             }
         };
 
-        const handlerPrevent = (e: any) => {
+        const handlePrevent = (e: any) => {
             e.preventDefault();
             e.stopPropagation();
         };
 
         dragDropEvents.forEach((eventName) => {
-            dropRef.current?.addEventListener(eventName, handlerPrevent, false);
-            overlayRef.current?.addEventListener(eventName, handlerPrevent, false);
+            dropAreaRef.current?.addEventListener(eventName, handlePrevent, false);
+            overlayRef.current?.addEventListener(eventName, handlePrevent, false);
         });
 
         dragOverEvents.forEach((eventName) => {
-            dropRef.current?.addEventListener(eventName, handleDragOver, false);
+            dropAreaRef.current?.addEventListener(eventName, handleDragOver, false);
         });
 
         overlayRef.current?.addEventListener('dragleave', handleDragLeave, false);
@@ -61,12 +61,12 @@ const UploadDragDrop = ({ children }: UploadDragDropProps) => {
 
         return () => {
             dragDropEvents.forEach((eventName) => {
-                dropRef.current?.removeEventListener(eventName, handlerPrevent, false);
-                overlayRef.current?.removeEventListener(eventName, handlerPrevent, false);
+                dropAreaRef.current?.removeEventListener(eventName, handlePrevent, false);
+                overlayRef.current?.removeEventListener(eventName, handlePrevent, false);
             });
 
             dragOverEvents.forEach((eventName) => {
-                dropRef.current?.removeEventListener(eventName, handleDragOver, false);
+                dropAreaRef.current?.removeEventListener(eventName, handleDragOver, false);
             });
 
             overlayRef.current?.removeEventListener('dragleave', handleDragLeave, false);
@@ -76,7 +76,7 @@ const UploadDragDrop = ({ children }: UploadDragDropProps) => {
 
     return (
         <>
-            <div ref={dropRef}>{children}</div>
+            <div ref={dropAreaRef}>{children}</div>
             <div ref={overlayRef} className={overlayIsVisible ? 'pd-drag-drop' : ''}>
                 {overlayIsVisible && (
                     <div className="pd-drag-drop-infobox">
