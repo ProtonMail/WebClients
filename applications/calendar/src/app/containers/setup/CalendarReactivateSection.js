ca198885627899b69138af3e48b9be7ea38e3bb1
Matthieu Bué <matthieu.bue@protonmail.com>
2020-03-17 11:32:48 +0000

    CALWEB-848 - Add ellipsis for long calendar names

--- applications/calendar/src/app/containers/setup/CalendarReactivateSection.js
+++ applications/calendar/src/app/containers/setup/CalendarReactivateSection.js
@@ -1,30 +1,14 @@
-import { Alert, Icon, Table, TableBody, TableHeader, TableRow } from 'react-components';
+import { Alert } from 'react-components';
 import { c } from 'ttag';
 import React from 'react';
+import CalendarTableRows from './CalendarTableRows';
 
 const CalendarReactivateSection = ({ calendarsToReactivate = [] }) => {
     return (
         <>
             <Alert type="info">{c('Info')
                 .t`You have reactivated your keys and events linked to the following calendars can now be decrypted.`}</Alert>
-            <Table>
-                <TableHeader cells={[c('Header').t`Name`]} />
-                <TableBody>
-                    {calendarsToReactivate.map(({ ID, Color, Name }) => {
-                        return (
-                            <TableRow
-                                key={ID}
-                                cells={[
-                                    <div key={0}>
-                                        <Icon name="calendar" color={Color} className="mr0-5" />
-                                        {Name}
-                                    </div>
-                                ]}
-                            />
-                        );
-                    })}
-                </TableBody>
-            </Table>
+            <CalendarTableRows calendars={calendarsToReactivate} />
         </>
     );
 };
--- applications/calendar/src/app/containers/setup/CalendarResetSection.js
+++ applications/calendar/src/app/containers/setup/CalendarResetSection.js
@@ -1,30 +1,14 @@
-import { Alert, Icon, Table, TableBody, TableHeader, TableRow } from 'react-components';
+import { Alert } from 'react-components';
 import { c } from 'ttag';
 import React from 'react';
+import CalendarTableRows from './CalendarTableRows';
 
 const CalendarResetSection = ({ calendarsToReset = [] }) => {
     return (
         <>
             <Alert type="warning">{c('Info')
                 .t`You have reset your password and events linked to the following calendars couldn't be decrypted.`}</Alert>
-            <Table>
-                <TableHeader cells={[c('Header').t`Name`]} />
-                <TableBody>
-                    {calendarsToReset.map(({ ID, Color, Name }) => {
-                        return (
-                            <TableRow
-                                key={ID}
-                                cells={[
-                                    <div key={0}>
-                                        <Icon name="calendar" color={Color} className="mr0-5" />
-                                        {Name}
-                                    </div>
-                                ]}
-                            />
-                        );
-                    })}
-                </TableBody>
-            </Table>
+            <CalendarTableRows calendars={calendarsToReset} />
         </>
     );
 };
