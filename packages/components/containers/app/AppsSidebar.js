47dd1c7a5ae13bf0d21dfe02c6a7d64ddbf4fb3c
Richard <richard@protonmail.com>
2020-03-18 09:10:11 +0100

    Fix COREWEB-106 - Use React link for current app
    Fix https://jira.protontech.ch/browse/COREWEB-106

--- packages/components/containers/app/AppsSidebar.js
+++ packages/components/containers/app/AppsSidebar.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Icon, useConfig, Tooltip } from 'react-components';
+import { Icon, useConfig, Tooltip, Link } from 'react-components';
 import { APPS } from 'proton-shared/lib/constants';
 
 const { PROTONMAIL, PROTONCONTACTS, PROTONMAIL_SETTINGS, PROTONCALENDAR } = APPS;
 
 const AppsSidebar = ({ items = [] }) => {
     const { APP_NAME } = useConfig();
-
     const apps = [
         { appNames: [PROTONMAIL, PROTONMAIL_SETTINGS], icon: 'protonmail', title: 'ProtonMail', link: '/inbox' },
         { appNames: [PROTONCONTACTS], icon: 'protoncontacts', title: 'ProtonContacts', link: '/contacts' },
@@ -22,21 +21,20 @@ const AppsSidebar = ({ items = [] }) => {
     return (
         <aside className="aside noprint nomobile" id="aside-bar">
             <ul className="unstyled m0 aligncenter flex flex-column h100">
-                {apps.map(({ appNames = [], icon, title, link, target }, index) => {
+                {apps.map(({ appNames = [], icon, title, link }, index) => {
                     const isCurrent = appNames.includes(APP_NAME);
                     const key = `${index}`;
                     return (
                         <li key={key} className="mb0-5">
                             <Tooltip title={title} originalPlacement="right">
-                                <a
-                                    href={link}
-                                    target={target ? target : '_self'}
+                                <Link
+                                    to={link}
                                     className="center flex aside-link"
-                                    disabled={isCurrent}
+                                    external={!isCurrent}
                                     aria-current={isCurrent}
                                 >
                                     <Icon name={icon} className="aside-linkIcon mauto" />
-                                </a>
+                                </Link>
                             </Tooltip>
                         </li>
                     );
