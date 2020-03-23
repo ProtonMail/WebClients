5308ad9eb53957680c8ba0bcd15279ba3f412751
Richard <richard@protonmail.com>
2020-03-23 15:24:20 +0100

    Fix MAILWEB-404 - Update texts for Verify tab (domain config)
    Fix https://jira.protontech.ch/browse/MAILWEB-404

--- packages/components/containers/domains/VerifySection.js
+++ packages/components/containers/domains/VerifySection.js
@@ -10,13 +10,11 @@ const VerifySection = ({ domain }) => {
         <>
             <Alert learnMore="https://protonmail.com/support/knowledge-base/dns-records/">
                 {c('Info for domain modal')
-                    .t`For security reasons, we need to verify that you are the owner of your domain. Please add the following code into your DNS record.`}
+                    .t`For security reasons, we need to verify that you are the owner of $domain. Please add the following DNS TXT record to your domain. This can typically be done in the control panel of your domain name registrar.`}
             </Alert>
             <Alert type="warning">
-                <strong>{c('Warning for domain modal').t`Do not remove it even after successful verification.`}</strong>
-                <br />
                 {c('Warning for domain modal')
-                    .t`This can typically be done in the control panel of your domain name registrar.`}
+                    .t`After successful verification, do not remove this TXT record as it is needed to confirm that you continue to own the domain.`}
             </Alert>
             <Label>{c('Label for domain modal').t`Please add the following TXT record:`}</Label>
             <Table>
