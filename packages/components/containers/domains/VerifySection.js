79b5754dce93fe4094cb2533922e502907b81e17
Richard <richard@protonmail.com>
2020-03-23 15:13:29 +0100

    Fix MAILWEB-387 - Review Verify table in domain config
    Fix https://jira.protontech.ch/browse/MAILWEB-387

--- packages/components/containers/domains/VerifySection.js
+++ packages/components/containers/domains/VerifySection.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { c } from 'ttag';
 import PropTypes from 'prop-types';
-import { Alert, Label, Table, TableHeader, TableBody, TableRow } from 'react-components';
+import { Alert, Label, Table, TableHeader, TableBody, TableRow, Copy, useNotifications } from 'react-components';
 
 const VerifySection = ({ domain }) => {
+    const { createNotification } = useNotifications();
+    const handleCopy = () => createNotification({ text: c('Success').t`Verify code copied to clipboard!` });
     return (
         <>
             <Alert learnMore="https://protonmail.com/support/knowledge-base/dns-records/">
@@ -26,7 +28,22 @@ const VerifySection = ({ domain }) => {
                     ]}
                 />
                 <TableBody>
-                    <TableRow cells={['TXT', '@', domain.VerifyCode]} />
+                    <TableRow
+                        cells={[
+                            <code key="txt">TXT</code>,
+                            <code key="at">@</code>,
+                            <div className="flex flex-nowrap flex-items-center" key="value">
+                                <Copy
+                                    onCopy={handleCopy}
+                                    className="flex-item-noshrink pm-button--small mr0-5"
+                                    value={domain.VerifyCode}
+                                />{' '}
+                                <code className="ellipsis" title={domain.VerifyCode}>
+                                    {domain.VerifyCode}
+                                </code>
+                            </div>
+                        ]}
+                    />
                 </TableBody>
             </Table>
             <Alert type="warning">{c('Warning for domain modal')
