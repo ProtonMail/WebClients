8bfcb30e18fc8a4e8ce0587a3d61ef8337ca1f4a
Richard <richard@protonmail.com>
2020-03-23 15:30:49 +0100

    Fix MAILWEB-406 - Update text for MX tab in domain config
    Fix https://jira.protontech.ch/browse/MAILWEB-406

--- packages/components/containers/domains/MXSection.js
+++ packages/components/containers/domains/MXSection.js
@@ -8,11 +8,6 @@ const MXSection = () => {
             <Alert learnMore="https://protonmail.com/support/knowledge-base/dns-records/">
                 {c('Info')
                     .t`Before you can receive emails for your custom domain addresses at ProtonMail, you need to add the following MX records to your DNS. This can typically be done in the control panel of your domain name registrar.`}
-                <br />
-                {c('Info').t`Delete any other MX records or make sure ProtonMail's Priority is the lowest number.`}
-                <br />
-                {c('Info')
-                    .t`If this domain is currently actively receiving emails, create all used email addresses in Step 3 before changing MX record to ensure a smooth transition.`}
             </Alert>
             <Label>{c('Label')
                 .t`Please add the following MX record. Note, DNS records can take several hours to update.`}</Label>
@@ -30,6 +25,12 @@ const MXSection = () => {
                     <TableRow cells={['MX', '@', 'mailsec.protonmail.ch', '20']} />
                 </TableBody>
             </Table>
+            <Alert>
+                {c('Info').t`Delete any other MX records or make sure ProtonMail's Priority is the lowest number.`}
+                <br />
+                {c('Info')
+                    .t`If this domain is currently actively receiving emails, create all used email addresses in Step 3 before changing MX record to ensure a smooth transition.`}
+            </Alert>
         </>
     );
 };
