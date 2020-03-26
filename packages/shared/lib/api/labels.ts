6ffb274e8c5c053292254d67f26f60d571167454
mmso <mmso@mmso.se>
2020-03-26 13:18:40 +0100

    Fix label types

--- packages/shared/lib/api/labels.ts
+++ packages/shared/lib/api/labels.ts
@@ -8,28 +8,31 @@ export const get = (Type: number) => ({
     params: { Type }
 });
 
-export const order = ({ LabelIDs, ParentID, Type }: { LabelIDs: string[]; ParentID?: string; Type: number }) => ({
+interface PartialLabelOrderArgument {
+    LabelIDs: string[];
+    ParentID?: string;
+}
+interface LabelOrderArgument extends PartialLabelOrderArgument {
+    Type: number;
+}
+export const order = ({ LabelIDs, ParentID, Type }: LabelOrderArgument) => ({
     method: 'put',
     url: 'v4/labels/order',
     data: { LabelIDs, ParentID, Type }
 });
 
-export const create = ({
-    Name,
-    Color,
-    Type,
-    Notify,
-    ParentID,
-    Expanded
-}: {
+interface PartialCreateLabelArgument {
     Name: string;
     Color: string;
-    Type: number;
     ParentID?: string;
     Notify?: number;
     Expanded?: number;
     Sticky?: number;
-}) => ({
+}
+interface CreateLabelArgument extends PartialCreateLabelArgument {
+    Type: number;
+}
+export const create = ({ Name, Color, Type, Notify, ParentID, Expanded }: CreateLabelArgument) => ({
     method: 'post',
     url: 'v4/labels',
     data: { Name, Color, Type, Notify, ParentID, Expanded }
@@ -60,9 +63,9 @@ export const getLabels = () => get(MESSAGE_LABEL);
 export const getFolders = () => get(MESSAGE_FOLDER);
 export const getContactGroup = () => get(CONTACT_GROUP);
 
-export const orderFolders = (opt) => order({ ...opt, Type: MESSAGE_FOLDER });
-export const orderLabels = (opt) => order({ ...opt, Type: MESSAGE_LABEL });
-export const orderContactGroup = (opt) => order({ ...opt, Type: CONTACT_GROUP });
+export const orderFolders = (opt: PartialLabelOrderArgument) => order({ ...opt, Type: MESSAGE_FOLDER });
+export const orderLabels = (opt: PartialLabelOrderArgument) => order({ ...opt, Type: MESSAGE_LABEL });
+export const orderContactGroup = (opt: PartialLabelOrderArgument) => order({ ...opt, Type: CONTACT_GROUP });
 
-export const createLabel = (opt) => create({ ...opt, Type: MESSAGE_LABEL });
-export const createContactGroup = (opt) => create({ ...opt, Type: CONTACT_GROUP });
+export const createLabel = (opt: PartialCreateLabelArgument) => create({ ...opt, Type: MESSAGE_LABEL });
+export const createContactGroup = (opt: PartialCreateLabelArgument) => create({ ...opt, Type: CONTACT_GROUP });
