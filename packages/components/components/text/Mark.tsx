0dcc1401411d34f6713bb1ac597e6be9116dae94
Richard <richard@protonmail.com>
2020-03-26 15:01:52 +0100

    Review TypeScript output with Vincas

--- packages/components/components/text/Mark.tsx
+++ packages/components/components/text/Mark.tsx
@@ -7,25 +7,29 @@ interface Props {
 
 const Mark = ({ children, value }: Props) => {
     if (!value || typeof children !== 'string') {
-        return children;
+        return <>{children}</>;
     }
 
     const splitted = children.split(new RegExp(value, 'i'));
 
     if (splitted.length < 2) {
         // Not found
-        return children;
+        return <>{children}</>;
     }
 
-    return splitted.reduce((acc: ReactNode[], v, index) => {
-        acc.push(v);
-        if (index < splitted.length - 1) {
-            const currentLength = acc.join('').length;
-            const insert = children.substring(currentLength, currentLength + value.length);
-            acc.push(<mark>{insert}</mark>);
-        }
-        return acc;
-    }, []);
+    return (
+        <>
+            {splitted.reduce((acc: ReactNode[], v, index) => {
+                acc.push(v);
+                if (index < splitted.length - 1) {
+                    const currentLength = acc.join('').length;
+                    const insert = children.substring(currentLength, currentLength + value.length);
+                    acc.push(<mark>{insert}</mark>);
+                }
+                return acc;
+            }, [])}
+        </>
+    );
 };
 
 export default Mark;
