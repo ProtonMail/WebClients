b0a71742da036071d953c18d66a2a2f740979938
econdepe <econdepe@protonmail.com>
2020-03-30 13:28:29 +0200

    make new hooks stable

--- packages/components/hooks/useCategories.ts
+++ packages/components/hooks/useCategories.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { LabelsModel } from 'proton-shared/lib/models/labelsModel';
 import { LABEL_TYPE } from 'proton-shared/lib/constants';
 import { Folder } from 'proton-shared/lib/interfaces/Folder';
@@ -19,18 +20,18 @@ const filterCategories = (categories: Category[] | undefined, type: number): Cat
 
 export const useLabels = (): [Label[] | undefined, boolean] => {
     const [categories, loading] = useCategories();
-    const labels = filterCategories(categories, LABEL_TYPE.MESSAGE_LABEL);
+    const labels = useMemo(() => filterCategories(categories, LABEL_TYPE.MESSAGE_LABEL), [categories]);
     return [labels, loading];
 };
 
 export const useFolders = (): [Folder[] | undefined, boolean] => {
     const [categories, loading] = useCategories();
-    const folders = filterCategories(categories, LABEL_TYPE.MESSAGE_FOLDER);
+    const folders = useMemo(() => filterCategories(categories, LABEL_TYPE.MESSAGE_FOLDER), [categories]);
     return [folders, loading];
 };
 
 export const useContactGroups = (): [ContactGroup[] | undefined, boolean] => {
     const [categories, loading] = useCategories();
-    const contactGroups = filterCategories(categories, LABEL_TYPE.CONTACT_GROUP);
+    const contactGroups = useMemo(() => filterCategories(categories, LABEL_TYPE.CONTACT_GROUP), [categories]);
     return [contactGroups, loading];
 };
