469e98a331fa026c5757bd9b537da91dc815e121
mmso <mmso@mmso.se>
2020-04-15 16:01:00 +0200

    Row in TS

--- packages/components/components/container/Row.tsx
+++ packages/components/components/container/Row.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { classnames } from '../../helpers/component';
 
-const Row = ({ children, className = '', collapseOnMobile = true, ...rest }) => {
+interface Props extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+    children?: React.ReactNode;
+    className?: string;
+    collapseOnMobile?: boolean;
+}
+
+const Row = ({ children, className = '', collapseOnMobile = true, ...rest }: Props) => {
     return (
         <div
             className={classnames(['flex flex-nowrap mb1', collapseOnMobile && 'onmobile-flex-column ', className])}
@@ -11,12 +16,6 @@ const Row = ({ children, className = '', collapseOnMobile = true, ...rest }) => 
             {children}
         </div>
     );
-};
-
-Row.propTypes = {
-    children: PropTypes.node.isRequired,
-    collapseOnMobile: PropTypes.bool,
-    className: PropTypes.string
 };
 
 export default Row;
