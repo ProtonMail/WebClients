76957f3338a14b13bc57c59dafc62bdb43d0879e
JP Bochi <jpbochi@pm.me>
2020-04-20 16:54:43 +0200

    proxy calendar, contacts and settings

--- applications/mail/docker/entrypoint.sh
+++ applications/mail/docker/entrypoint.sh
@@ -2,11 +2,13 @@
 set -eu
 
 API_ENDPOINT="${API_ENDPOINT:-https://mail.protonmail.com/api}"
+BASE_OTHER_FE_APPS="${BASE_OTHER_FE_APPS:-https://proton.qa}"
 
 # Ensure url ends with a /
 export API_ENDPOINT="$(echo "$API_ENDPOINT" | sed 's/\/\?$/\//')"
+export BASE_OTHER_FE_APPS="$(echo "$BASE_OTHER_FE_APPS" | sed 's/\/\?$/\//')"
 
-envsubst '${API_ENDPOINT}' \
+envsubst '${API_ENDPOINT} ${BASE_OTHER_FE_APPS}' \
   < /etc/nginx/conf.d/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
