8fa7542dcf68a5d7368156ee76c18c9fe574e4fc
Richard <richard@protonmail.com>
2020-04-27 08:35:33 +0200

    Fix CP-247 - Use headers to send back verification token and tokenType
    Before we were sending these parameters in the body.
    Use also appropriate keys: HumanVerificationMethods and HumanVerificationToken

--- src/app/authentication/services/handle9001.js
+++ src/app/authentication/services/handle9001.js
@@ -1,7 +1,7 @@
 /* @ngInject */
 function handle9001($http, humanVerificationModal) {
     return (config, { Details = {} }) => {
-        const { VerifyMethods: methods = [], Token: token } = Details;
+        const { HumanVerificationMethods: methods = [], HumanVerificationToken: token } = Details;
         const useParams = ['GET', 'DELETE'].includes(config.method);
         return new Promise((resolve) => {
             humanVerificationModal.activate({
@@ -15,9 +15,13 @@ function handle9001($http, humanVerificationModal) {
                             return resolve(
                                 $http({
                                     ...config,
+                                    headers: {
+                                        ...config.headers,
+                                        'X-PM-Human-Verification-Token': parameters.Token,
+                                        'X-PM-Human-Verification-Token-Type': parameters.TokenType
+                                    },
                                     [useParams ? 'params' : 'data']: {
-                                        ...(config[useParams ? 'params' : 'data'] || {}),
-                                        ...parameters
+                                        ...(config[useParams ? 'params' : 'data'] || {})
                                     }
                                 })
                             );
