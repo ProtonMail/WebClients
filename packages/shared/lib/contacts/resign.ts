c921cc5b6c6e9d32d6966ad51bea4601892b1e55
econdepe <econdepe@protonmail.com>
2020-04-28 12:01:00 +0200

    no need of verifying signature

--- packages/shared/lib/contacts/resign.ts
+++ packages/shared/lib/contacts/resign.ts
@@ -1,33 +1,25 @@
 import { OpenPGPKey, signMessage } from 'pmcrypto';
 import { CONTACT_CARD_TYPE } from '../constants';
 import { ContactCard } from '../interfaces/contacts';
-import { readSigned } from './decrypt';
 
 /**
  * Re-sign contact cards
- * Public keys need to be passed to check signature validity of signed contact cards
  * Private keys (typically only the primary one) need to be passed to re-sign the contact cards
+ * No public key is needed as we don't do signature verification (since we are re-signing anyway)
  */
 interface Params {
     contactCards: ContactCard[];
-    publicKeys: OpenPGPKey[];
     privateKeys: OpenPGPKey[];
 }
-export const resignCards = async ({ contactCards, publicKeys, privateKeys }: Params): Promise<ContactCard[]> => {
+export const resignCards = async ({ contactCards, privateKeys }: Params): Promise<ContactCard[]> => {
     const signedCards = contactCards.filter((card) => card.Type === CONTACT_CARD_TYPE.SIGNED);
     const otherCards = contactCards.filter((card) => card.Type !== CONTACT_CARD_TYPE.SIGNED);
-    const signedVcards = await Promise.all(
-        signedCards.map(async (card) => {
-            const { data } = await readSigned(card, { publicKeys });
-            return data;
-        })
-    );
     const reSignedCards = await Promise.all(
-        signedVcards.map(async (vcard) => {
-            const { signature } = await signMessage({ data: vcard, privateKeys, armor: true, detached: true });
+        signedCards.map(async ({ Data }) => {
+            const { signature } = await signMessage({ data: Data, privateKeys, armor: true, detached: true });
             return {
                 Type: CONTACT_CARD_TYPE.SIGNED,
-                Data: vcard,
+                Data,
                 Signature: signature
             };
         })
