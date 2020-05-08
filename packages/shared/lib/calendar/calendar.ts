433b6d681178bdebb2c446b1f9ef18bc9a2659e0
mmso <mmso@mmso.se>
2020-05-08 18:49:59 +0200

    Calendar in ts

--- packages/shared/lib/calendar/calendar.ts
+++ packages/shared/lib/calendar/calendar.ts
@@ -1,5 +1,6 @@
 import { hasBit } from '../helpers/bitset';
 import { CALENDAR_FLAGS } from './constants';
+import { Calendar } from '../interfaces/calendar';
 
 export const getIsCalendarActive = ({ Flags } = { Flags: 0 }) => {
     return hasBit(Flags, CALENDAR_FLAGS.ACTIVE);
@@ -9,17 +10,17 @@ export const getIsCalendarDisabled = ({ Flags } = { Flags: 0 }) => {
     return hasBit(Flags, CALENDAR_FLAGS.SELF_DISABLED) || hasBit(Flags, CALENDAR_FLAGS.SUPER_OWNER_DISABLED);
 };
 
-export const getIsCalendarProbablyActive = (calendar = {}) => {
+export const getIsCalendarProbablyActive = (calendar = { Flags: 0 }) => {
     // Calendars are treated as "active" if flags are undefined, this can happen when a new calendar was created and received through the event manager.
     // In this case, we assume everything went well and treat it as an active calendar.
     return calendar.Flags === undefined || (!getIsCalendarDisabled(calendar) && getIsCalendarActive(calendar));
 };
 
-export const getProbablyActiveCalendars = (calendars = []) => {
+export const getProbablyActiveCalendars = (calendars: Calendar[] = []) => {
     return calendars.filter(getIsCalendarProbablyActive);
 };
 
-export const getDefaultCalendar = (calendars = [], defaultCalendarID) => {
+export const getDefaultCalendar = (calendars: Calendar[] = [], defaultCalendarID = '') => {
     if (!calendars.length) {
         return;
     }
