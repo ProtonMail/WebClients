403c6ee829c31a2ad54fdaf345454ff51222c332
Mantvidas M <mantvidasm@Mantvidass-MacBook-Pro.local>
2020-05-26 15:04:55 +0300

    ToolbarButton extended with children and ref

--- packages/components/components/toolbar/ToolbarButton.tsx
+++ packages/components/components/toolbar/ToolbarButton.tsx
@@ -1,29 +1,34 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes, ReactNode } from 'react';
 import { classnames } from '../../helpers/component';
 import Icon, { Props as IconProps } from '../icon/Icon';
 import { noop } from 'proton-shared/lib/helpers/function';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     icon: string | IconProps;
+    children?: ReactNode;
 }
 
-const ToolbarButton = ({ icon, className, disabled, tabIndex, onClick, ...rest }: Props) => {
-    return (
-        <button
-            type="button"
-            className={classnames([className, 'toolbar-button'])}
-            onClick={disabled ? noop : onClick}
-            tabIndex={disabled ? -1 : tabIndex}
-            disabled={disabled}
-            {...rest}
-        >
-            {typeof icon === 'string' ? (
-                <Icon name={icon} className="toolbar-icon mauto" />
-            ) : (
-                <Icon {...icon} className={classnames([icon.className, 'toolbar-icon mauto'])} />
-            )}
-        </button>
-    );
-};
+const ToolbarButton = React.forwardRef<HTMLButtonElement, Props>(
+    ({ icon, children, className, disabled, tabIndex, onClick, ...rest }: Props, ref) => {
+        return (
+            <button
+                type="button"
+                className={classnames([className, 'toolbar-button'])}
+                onClick={disabled ? noop : onClick}
+                tabIndex={disabled ? -1 : tabIndex}
+                disabled={disabled}
+                ref={ref}
+                {...rest}
+            >
+                {typeof icon === 'string' ? (
+                    <Icon name={icon} className="toolbar-icon mauto" />
+                ) : (
+                    <Icon {...icon} className={classnames([icon.className, 'toolbar-icon mauto'])} />
+                )}
+                {children}
+            </button>
+        );
+    }
+);
 
 export default ToolbarButton;
