9df73a8d0bc8dabc545082b1333ca18c29fb8f0f
Vincas Stonys <vincaslt@gmail.com>
2020-05-27 11:08:26 +0300

    Add threshold to queued function and queue folder creation and name check

--- applications/drive/src/app/hooks/useQueuedFunction.ts
+++ applications/drive/src/app/hooks/useQueuedFunction.ts
@@ -1,22 +1,59 @@
 import { useCache } from 'react-components';
 
+type FunctionQueue<R> = [number, (() => Promise<R>)[]];
+
+/**
+ * Puts function execution into a queue with a threshold of maximum active functions processing at once
+ */
 const useQueuedFunction = () => {
     const cache = useCache();
 
-    const queuedFunction = <R, A extends any[]>(fnKey: string, fn: (...args: A) => Promise<R>) => {
+    const queuedFunction = <R, A extends any[]>(fnKey: string, fn: (...args: A) => Promise<R>, threshold = 1) => {
         const key = `queuedfn_${fnKey}`;
 
-        return (...args: A) => {
-            const existingPromise: Promise<R> | undefined = cache.get(key);
-            if (existingPromise) {
-                const promise = existingPromise.finally(() => fn(...args));
-                cache.set(key, promise);
-                return promise;
-            }
+        if (!cache.has(key)) {
+            cache.set(key, [0, []]);
+        }
 
+        const runNextQueued = () => {
+            const [processing, queued]: FunctionQueue<R> = cache.get(key);
+
+            if (queued.length) {
+                const [next, ...remaining] = queued;
+                next().finally(runNextQueued);
+                cache.set(key, [processing, remaining]);
+            } else {
+                cache.set(key, [processing - 1, []]);
+            }
+        };
+
+        const run = (...args: A) => {
+            const [processing, queued]: FunctionQueue<R> = cache.get(key);
+            cache.set(key, [processing + 1, queued]);
             const promise = fn(...args);
-            cache.set(key, promise);
+            promise.finally(runNextQueued);
             return promise;
+        };
+
+        const enqueue = (...args: A) =>
+            new Promise<R>((resolve) => {
+                const [processing, queued]: FunctionQueue<R> = cache.get(key);
+                cache.set(key, [
+                    processing,
+                    [
+                        ...queued,
+                        () => {
+                            const promise = fn(...args);
+                            resolve(promise);
+                            return promise;
+                        }
+                    ]
+                ]);
+            });
+
+        return (...args: A) => {
+            const [processing]: FunctionQueue<R> = cache.get(key);
+            return processing < threshold ? run(...args) : enqueue(...args);
         };
     };
 
