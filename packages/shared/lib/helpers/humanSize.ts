c6921a4eaf59c5f1d6b2a924a3972c24910b858d
Mantvidas Miskinis <mantvidas.miskinis@protonmail.com>
2020-05-28 12:09:13 +0000

    [DRVWEB-264] Add bytes unit to humanSize

--- packages/shared/lib/helpers/humanSize.ts
+++ packages/shared/lib/helpers/humanSize.ts
@@ -2,14 +2,18 @@ import { c } from 'ttag';
 
 import { BASE_SIZE } from '../constants';
 
+export type SizeUnits = 'B' | 'KB' | 'MB' | 'GB';
+
 const units = {
+    B: 1,
     KB: BASE_SIZE,
     MB: BASE_SIZE * BASE_SIZE,
     GB: BASE_SIZE * BASE_SIZE * BASE_SIZE
 };
 
-const i18nSuffix = (key) => {
+const i18nSuffix = (key: SizeUnits) => {
     const map = {
+        B: c('file size format').t`bytes`,
         KB: c('file size format').t`KB`,
         MB: c('file size format').t`MB`,
         GB: c('file size format').t`GB`
@@ -18,24 +22,22 @@ const i18nSuffix = (key) => {
     return map[key];
 };
 
-const transformTo = (bytes, unit, withoutUnit) => {
-    const value = (bytes / units[unit]).toFixed(2);
+const transformTo = (bytes: number, unit: SizeUnits, withoutUnit: boolean, fractionDigits = 2) => {
+    const value = (bytes / units[unit]).toFixed(fractionDigits);
     const suffix = withoutUnit ? '' : ` ${i18nSuffix(unit)}`;
 
     return value + suffix;
 };
 
-const humanSize = (input = 0, forceUnit = undefined, withoutUnit = false) => {
-    let bytes;
-
-    if (typeof input === 'number') {
-        bytes = input;
-    } else if (Number.isNaN(parseInt(input, 10))) {
-        bytes = 0;
-    }
+const humanSize = (input = 0, forceUnit?: SizeUnits, withoutUnit = false) => {
+    const bytes = input;
 
     if (forceUnit) {
         return transformTo(bytes, forceUnit, withoutUnit);
+    }
+
+    if (bytes < units.KB) {
+        return transformTo(bytes, 'B', withoutUnit, 0);
     }
 
     if (bytes < units.MB) {
