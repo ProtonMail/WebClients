d130b9f2e93a18b6be174693dd5fbc6deef64f4d
Richard <richard@protonmail.com>
2020-05-28 19:47:06 +0000

    [VPNFE-52] Use CircularProgress component

--- packages/components/containers/vpn/OpenVPNConfigurationSection/LoadIndicator.js
+++ packages/components/containers/vpn/OpenVPNConfigurationSection/LoadIndicator.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Tooltip } from 'react-components';
+import { Tooltip, CircularProgress } from 'react-components';
 import { c } from 'ttag';
 
 const LoadIndicator = ({ server: { Load = 0 } }) => {
     // 1-49% load is GREEN color #5db039
     // 50-89% load is YELLOW color #eac819
     // 90-100% load is RED color #ec5858
-    const background = Load < 50 ? '#5db039' : Load < 90 ? '#eac819' : '#ec5858';
+    const className = Load < 50 ? '' : Load < 90 ? 'circle-bar--medium' : 'circle-bar--full';
     return (
         <span className="min-w5e nomobile">
             <Tooltip title={c('Info').t`Server load`}>
                 <div className="flex inline-flex-vcenter">
-                    <div className="load-indicator">
-                        <div className="load-indicator-overlay" style={{ background }}></div>
-                    </div>
+                    <CircularProgress progress={Load} size={22} className={className}>
+                        <g className="circle-chart__info">
+                            <rect x="17" y="14" width="1.55" height="9.1" className="circle-chart__percent"></rect>
+                            <rect x="17" y="11" width="1.55" height="1.53" className="circle-chart__percent"></rect>
+                        </g>
+                    </CircularProgress>
                     <div className="ml0-5">{Load}%</div>
                 </div>
             </Tooltip>
