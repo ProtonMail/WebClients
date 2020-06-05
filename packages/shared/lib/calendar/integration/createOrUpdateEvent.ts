7a54226b7fcac9503f17cbb518decfbd801b2650
mmso <mmso@mmso.se>
2020-06-05 13:08:17 +0200

    More calendar ts

---
+++ packages/shared/lib/calendar/integration/createOrUpdateEvent.ts
@@ -0,0 +1,44 @@
+import { addSharedEvent, CreateCalendarEventBlobData, createEvent, updateEvent } from '../../api/calendars';
+import { CalendarEventSharedData } from '../../interfaces/calendar';
+
+interface SwitchArguments {
+    calendarID: string;
+    UID: string;
+    memberID: string;
+    data: CreateCalendarEventBlobData;
+}
+export const getSwitchCalendarEvent = ({ calendarID, data, UID, memberID }: SwitchArguments) => {
+    return addSharedEvent(calendarID, {
+        ...data,
+        UID,
+        Overwrite: 1,
+        MemberID: memberID,
+        Permissions: 3 // TODO what?
+    });
+};
+
+interface UpdateArguments {
+    memberID: string;
+    data: CreateCalendarEventBlobData;
+    Event: CalendarEventSharedData;
+}
+export const getUpdateCalendarEvent = ({ Event, memberID, data }: UpdateArguments) => {
+    return updateEvent(Event.CalendarID, Event.ID, {
+        ...data,
+        MemberID: memberID,
+        Permissions: 3 // TODO what?
+    });
+};
+
+interface CreateArguments {
+    calendarID: string;
+    memberID: string;
+    data: CreateCalendarEventBlobData;
+}
+export const getCreateCalendarEvent = ({ calendarID, data, memberID }: CreateArguments) => {
+    return createEvent(calendarID, {
+        ...data,
+        MemberID: memberID,
+        Permissions: 3 // TODO what?
+    });
+};
