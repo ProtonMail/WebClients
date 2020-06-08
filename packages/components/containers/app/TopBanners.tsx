7c0b03b46b19d58a47271a0954946e5ec0e31855
Richard <richard@protonmail.com>
2020-06-08 12:07:48 +0200

    Handle delinquent case with top banner

--- packages/components/containers/app/TopBanners.tsx
+++ packages/components/containers/app/TopBanners.tsx
@@ -12,10 +12,14 @@ const TopBanners = () => {
     const [ignoreStorageLimit, setIgnoreStorageLimit] = useState(
         getItem(`${IGNORE_STORAGE_LIMIT_KEY}${user.ID}`) === 'true'
     );
+    // TODO Update links once we have proton-account
     const upgradeLink = (
         <Href key="storage-link" className="color-currentColor" url="/settings/subscription" target="_self">{c('Link')
             .t`Upgrade account`}</Href>
-    ); // TODO Update link once we have proton-account
+    );
+    const payInvoiceLink = (
+        <Href key="pay-invoices" url="/settings/subscription" target="_self">{c('Link').t`Pay invoice`}</Href>
+    );
     const spacePercentage = (user.UsedSpace * 100) / user.MaxSpace;
 
     useEffect(() => {
@@ -33,6 +37,14 @@ const TopBanners = () => {
             {!ignoreStorageLimit && !isNaN(spacePercentage) && spacePercentage >= 90 && spacePercentage < 100 ? (
                 <TopBanner className="bg-global-attention" onClose={() => setIgnoreStorageLimit(true)}>{c('Info')
                     .jt`You reached 90% of your storage capacity. Free up some space or add more storage space. ${upgradeLink}`}</TopBanner>
+            ) : null}
+            {user.isDelinquent && user.canPay ? (
+                <TopBanner className="bg-global-warning">{c('Info')
+                    .jt`Your account has at least one overdue invoice. Your access will soon get restricted. ${payInvoiceLink}`}</TopBanner>
+            ) : null}
+            {user.isDelinquent && user.isMember ? (
+                <TopBanner className="bg-global-warning">{c('Info')
+                    .t`Account access restricted due to unpaid invoices. Please contact your administrator.`}</TopBanner>
             ) : null}
         </>
     );
