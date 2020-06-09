dd618c75f66575a2bd5b06f8404a4f4a60b8a57f
Richard <richard@protonmail.com>
2020-06-09 09:03:22 +0200

    Add VPNServer interface

---
+++ packages/shared/lib/interfaces/VPNServer.ts
@@ -0,0 +1,29 @@
+export interface ServerLocation {
+    Lat: number;
+    Long: number;
+}
+
+interface Server {
+    Domain: string;
+    EntryIP: string;
+    ExitIP: string;
+    ID: string;
+    Status: number;
+}
+
+export interface VPNServer {
+    City: string;
+    Country: string;
+    Domain: string;
+    EntryCountry: string;
+    ExitCountry: string;
+    Features: number;
+    ID: string;
+    Load: number;
+    Location: ServerLocation;
+    Name: string;
+    Score: number;
+    Servers: Server[];
+    Status: number;
+    Tier: number;
+}
