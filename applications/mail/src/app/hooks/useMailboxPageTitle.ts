a3339a49fbc9fcf5827b12ac53fc4a0fec007a63
Jeremy Benaim <jeremybenaim@gmail.com>
2020-06-25 16:07:21 +0100

    Make the inbox title more reactive to the event loop

--- applications/mail/src/app/hooks/useMailboxPageTitle.ts
+++ applications/mail/src/app/hooks/useMailboxPageTitle.ts
@@ -14,32 +14,21 @@ import { toMap } from 'proton-shared/lib/helpers/object';
 import { LabelCount } from 'proton-shared/lib/interfaces/Label';
 
 export const useMailboxPageTitle = (labelID: string, location: Location) => {
-    const [mailSettings, loadingMailSettings] = useMailSettings();
-    const [labels, loadingLabels] = useLabels();
-    const [folders, loadingFolders] = useFolders();
-    const [user, loadingUser] = useUser();
-    const [conversationCounts, loadingConversationCounts] = useConversationCounts() as [LabelCount[], boolean, Error];
-    const [messageCounts, loadingMessageCounts] = useMessageCounts() as [LabelCount[], boolean, Error];
-
-    const loadings = [
-        loadingMailSettings,
-        loadingLabels,
-        loadingFolders,
-        loadingUser,
-        loadingConversationCounts,
-        loadingMessageCounts
-    ];
+    const [mailSettings] = useMailSettings();
+    const [labels] = useLabels();
+    const [folders] = useFolders();
+    const [user] = useUser();
+    const [conversationCounts] = useConversationCounts() as [LabelCount[], boolean, Error];
+    const [messageCounts] = useMessageCounts() as [LabelCount[], boolean, Error];
 
     useEffect(() => {
-        if (loadings.every((loading) => !loading)) {
-            const conversationMode = isConversationMode(labelID, mailSettings, location);
-            const counters = conversationMode ? conversationCounts : messageCounts;
-            const countersMap = toMap(counters, 'LabelID') as { [labelID: string]: LabelCount };
-            const unreads = (countersMap[labelID] || {}).Unread || 0;
-            const unreadString = unreads > 0 ? `(${unreads}) ` : '';
-            const labelName = getLabelName(labelID, labels, folders);
-            const address = user.Email;
-            document.title = `${unreadString}${labelName} | ${address} | ProtonMail`;
-        }
-    }, [labelID, conversationCounts, messageCounts, ...loadings]);
+        const conversationMode = isConversationMode(labelID, mailSettings, location);
+        const counters = conversationMode ? conversationCounts : messageCounts;
+        const countersMap = toMap(counters, 'LabelID') as { [labelID: string]: LabelCount };
+        const unreads = (countersMap[labelID] || {}).Unread || 0;
+        const unreadString = unreads > 0 ? `(${unreads}) ` : '';
+        const labelName = getLabelName(labelID, labels, folders);
+        const address = user.Email;
+        document.title = `${unreadString}${labelName} | ${address} | ProtonMail`;
+    }, [labelID, mailSettings, user, labels, folders, conversationCounts, messageCounts]);
 };
