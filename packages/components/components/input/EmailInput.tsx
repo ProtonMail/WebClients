a65316f71304e6af77771458ec67fdc1244fcad4
econdepe <econdepe@protonmail.com>
2020-06-25 11:39:42 +0200

    remove isEmail helper

--- packages/components/components/input/EmailInput.tsx
+++ packages/components/components/input/EmailInput.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { c } from 'ttag';
-import { isEmail } from 'proton-shared/lib/helpers/validators';
+import { validateEmailAddress } from 'proton-shared/lib/helpers/string';
 
 import Input, { Props } from './Input';
 
 const EmailInput = ({ value = '', ...rest }: Props) => {
-    const error = isEmail(value as string) ? '' : c('Error').t`Email address invalid`;
+    const error = validateEmailAddress(value as string) ? '' : c('Error').t`Email address invalid`;
     return <Input type="email" error={error} value={value} {...rest} />;
 };
 
