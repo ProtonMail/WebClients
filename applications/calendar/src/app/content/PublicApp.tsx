d6d1c37dd0c6be1ee701b77835e796a80cd4c128
mmso <mmso@mmso.se>
2020-07-06 16:01:04 +0200

    Rename login form

--- applications/calendar/src/app/content/PublicApp.tsx
+++ applications/calendar/src/app/content/PublicApp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { StandardPublicApp, LoginForm } from 'react-components';
+import { StandardPublicApp, MinimalLoginContainer } from 'react-components';
 import locales from '../locales';
 
 interface Props {
@@ -10,7 +10,7 @@ const PublicApp = ({ onLogin }: Props) => {
     return (
         <StandardPublicApp locales={locales}>
             <Switch>
-                <Route render={() => <LoginForm onLogin={onLogin} />} />
+                <Route render={() => <MinimalLoginContainer onLogin={onLogin} />} />
             </Switch>
         </StandardPublicApp>
     );
