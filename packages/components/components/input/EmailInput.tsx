9eb4620ce816afd22bb986af1e6df5dcde6c8366
Richard <richard@protonmail.com>
2020-07-07 17:22:53 +0200

    Hotfix - value can by empty

--- packages/components/components/input/EmailInput.tsx
+++ packages/components/components/input/EmailInput.tsx
@@ -5,7 +5,7 @@ import { validateEmailAddress } from 'proton-shared/lib/helpers/string';
 import Input, { Props } from './Input';
 
 const EmailInput = ({ value = '', ...rest }: Props) => {
-    const error = validateEmailAddress(value as string) ? '' : c('Error').t`Email address invalid`;
+    const error = value ? (validateEmailAddress(value as string) ? '' : c('Error').t`Email address invalid`) : '';
     return <Input type="email" error={error} value={value} {...rest} />;
 };
 
