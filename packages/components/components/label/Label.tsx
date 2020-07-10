5034367f1c4eb30f64ebd7adea68a99c8480dbe3
Mattias Svanström <mmso@mmso.se>
2020-07-10 10:09:41 +0000

    Label in ts

--- packages/components/components/label/Label.tsx
+++ packages/components/components/label/Label.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { classnames } from '../../helpers/component';
 
-const Label = ({ htmlFor = undefined, className = '', children, ...rest }) => {
+type Props = React.LabelHTMLAttributes<HTMLLabelElement>;
+
+const Label = ({ htmlFor, className, children, ...rest }: Props) => {
     return (
         <label htmlFor={htmlFor} className={classnames(['pm-label', className])} {...rest}>
             {children}
         </label>
     );
-};
-
-Label.propTypes = {
-    className: PropTypes.string,
-    children: PropTypes.node.isRequired,
-    htmlFor: PropTypes.string
 };
 
 export default Label;
