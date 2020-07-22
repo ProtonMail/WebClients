c7f131f63b1ba3853a65efadd3dda8c6a8793107
Matthieu Lux <matthieu.lux@protonmail.com>
2020-07-22 15:25:02 +0200

    [MAILWEB-1095] move out of an element when it has been moved in another folder

--- applications/mail/src/app/hooks/useShouldMoveOut.ts
+++ applications/mail/src/app/hooks/useShouldMoveOut.ts
@@ -2,17 +2,16 @@ import { useEffect, useRef } from 'react';
 import { MAILBOX_LABEL_IDS } from 'proton-shared/lib/constants';
 
 import { MessageExtended } from '../models/message';
-import { hasLabel } from '../helpers/elements';
+import { hasLabel, getCurrentFolderID } from '../helpers/elements';
 import { useMessageCache } from '../containers/MessageProvider';
 import { ConversationResult } from './useConversation';
 import { useConversationCache } from '../containers/ConversationProvider';
+import { useFolders } from 'react-components';
 
-const { ALL_MAIL, TRASH } = MAILBOX_LABEL_IDS;
+const { ALL_MAIL } = MAILBOX_LABEL_IDS;
 
-const cacheEntryHasLabel = (cacheEntry: MessageExtended | ConversationResult | undefined, labelID: string) => {
-    const element = (cacheEntry as ConversationResult)?.Conversation || (cacheEntry as MessageExtended)?.data || {};
-    return hasLabel(element, labelID);
-};
+const chacheEntryToElement = (cacheEntry: MessageExtended | ConversationResult | undefined) =>
+    (cacheEntry as ConversationResult)?.Conversation || (cacheEntry as MessageExtended)?.data || {};
 
 const cacheEntryIsFailedLoading = (
     conversationMode: boolean,
@@ -35,40 +34,45 @@ export const useShouldMoveOut = (
     const messageCache = useMessageCache();
     const conversationCache = useConversationCache();
     const cache = conversationMode ? conversationCache : messageCache;
+    const [folders = []] = useFolders();
 
     const previousVersionRef = useRef<MessageExtended | ConversationResult | undefined>();
-    if (ID) {
-        previousVersionRef.current = cache.get(ID);
-    } else {
-        previousVersionRef.current = undefined;
-    }
 
-    useEffect(
-        () =>
-            cache.subscribe((changedID: string) => {
-                if (changedID !== ID) {
-                    return;
-                }
+    useEffect(() => {
+        if (ID) {
+            previousVersionRef.current = cache.get(ID);
+        } else {
+            previousVersionRef.current = undefined;
+        }
 
-                const cacheEntry = cache.get(ID);
+        return cache.subscribe((changedID: string) => {
+            if (changedID !== ID) {
+                return;
+            }
 
-                // Move out of a deleted element
-                if (!cacheEntry) {
-                    onBack();
-                    return;
-                }
+            const cacheEntry = cache.get(ID);
 
-                // Move out of trashed message
-                const hasLabels = cacheEntryHasLabel(previousVersionRef.current, ALL_MAIL);
-                const wasTrashed = cacheEntryHasLabel(previousVersionRef.current, TRASH);
-                const isTrashed = cacheEntryHasLabel(cacheEntry, TRASH);
-                if (hasLabels && !wasTrashed && isTrashed) {
-                    onBack();
-                    return;
-                }
-            }),
-        [cache, ID]
-    );
+            // Move out of a deleted element
+            if (!cacheEntry) {
+                onBack();
+                return;
+            }
+
+            // Move out of moved away message
+            const previousElement = chacheEntryToElement(previousVersionRef.current);
+            const currentElement = chacheEntryToElement(cacheEntry);
+            const hadLabels = hasLabel(previousElement, ALL_MAIL);
+            const previousFolderID = getCurrentFolderID(previousElement, folders);
+            const currentFolderID = getCurrentFolderID(currentElement, folders);
+
+            if (hadLabels && previousFolderID !== currentFolderID) {
+                onBack();
+                return;
+            }
+
+            previousVersionRef.current = cacheEntry;
+        });
+    }, [cache, ID]);
 
     useEffect(() => {
         if (!ID) {
