6bc1975735e33f26e49b9db0cbb1c7ba9a327750
mmso <mmso@mmso.se>
2020-07-27 11:19:23 +0200

    Also verify iff keys

--- packages/shared/lib/calendar/decrypt.ts
+++ packages/shared/lib/calendar/decrypt.ts
@@ -30,8 +30,9 @@ export const verifySignedCard = async (
         signature: await getSignature(signature),
         detached: true,
     });
+    const hasPublicKeys = Array.isArray(publicKeys) ? !!publicKeys.length : !!publicKeys;
 
-    if (verified !== VERIFICATION_STATUS.SIGNED_AND_VALID) {
+    if (hasPublicKeys && verified !== VERIFICATION_STATUS.SIGNED_AND_VALID) {
         const error = new Error('Signature verification failed');
         error.name = 'SignatureError';
         throw error;
