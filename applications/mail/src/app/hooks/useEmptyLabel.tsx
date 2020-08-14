1805231719053bb864f449dc1a48cb14a446927e
Matthieu Lux <matthieu.lux@protonmail.com>
2020-08-14 10:44:43 +0200

    [MAILWEB-703] Fix emptying a new label

--- applications/mail/src/app/hooks/useEmptyLabel.tsx
+++ applications/mail/src/app/hooks/useEmptyLabel.tsx
@@ -25,42 +25,45 @@ export const useEmptyLabel = () => {
     const [labels = []] = useLabels();
     const [folders = []] = useFolders();
 
-    const emptyLabel = useCallback(async (labelID: string) => {
-        const isLabel = isCustomLabel(labelID, labels);
-        const labelName = getLabelName(labelID, labels, folders);
-        await new Promise((resolve, reject) => {
-            createModal(
-                <ConfirmModal
-                    title={c('Title').t`Empty ${labelName}`}
-                    confirm={<ErrorButton type="submit">{c('Action').t`Empty`}</ErrorButton>}
-                    onConfirm={resolve}
-                    onClose={reject}
-                >
-                    <Alert type="info">
-                        {isLabel
-                            ? c('Info')
-                                  .t`Please note that if you empty this label, you will permanently delete all the emails with this label.`
-                            : c('Info')
-                                  .t`Please note that if you empty this folder, you will permanently delete all the emails stored in it.`}
-                    </Alert>
-                    <Alert type="error">
-                        {isLabel
-                            ? c('Info').t`Are you sure you want to empty this label?`
-                            : c('Info').t`Are you sure you want to empty this folder?`}
-                    </Alert>
-                </ConfirmModal>
-            );
-        });
-        const rollback = optimisticEmptyLabel(labelID);
-        try {
-            await api(emptyLabelRequest({ LabelID: labelID, AddressID: undefined }));
-        } catch (error) {
-            rollback();
-            throw error;
-        }
-        await call();
-        createNotification({ text: isLabel ? c('Success').t`Label cleared` : c('Success').t`Folder cleared` });
-    }, []);
+    const emptyLabel = useCallback(
+        async (labelID: string) => {
+            const isLabel = isCustomLabel(labelID, labels);
+            const labelName = getLabelName(labelID, labels, folders);
+            await new Promise((resolve, reject) => {
+                createModal(
+                    <ConfirmModal
+                        title={c('Title').t`Empty ${labelName}`}
+                        confirm={<ErrorButton type="submit">{c('Action').t`Empty`}</ErrorButton>}
+                        onConfirm={resolve}
+                        onClose={reject}
+                    >
+                        <Alert type="info">
+                            {isLabel
+                                ? c('Info')
+                                      .t`Please note that if you empty this label, you will permanently delete all the emails with this label.`
+                                : c('Info')
+                                      .t`Please note that if you empty this folder, you will permanently delete all the emails stored in it.`}
+                        </Alert>
+                        <Alert type="error">
+                            {isLabel
+                                ? c('Info').t`Are you sure you want to empty this label?`
+                                : c('Info').t`Are you sure you want to empty this folder?`}
+                        </Alert>
+                    </ConfirmModal>
+                );
+            });
+            const rollback = optimisticEmptyLabel(labelID);
+            try {
+                await api(emptyLabelRequest({ LabelID: labelID, AddressID: undefined }));
+            } catch (error) {
+                rollback();
+                throw error;
+            }
+            await call();
+            createNotification({ text: isLabel ? c('Success').t`Label cleared` : c('Success').t`Folder cleared` });
+        },
+        [labels, folders]
+    );
 
     return emptyLabel;
 };
