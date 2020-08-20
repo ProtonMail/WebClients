b5913e9b249a9dd06a984e54b4f6e87895bcde74
mmso <mmso@mmso.se>
2020-08-20 08:59:32 +0200

    Change pathname to u/x

--- packages/shared/lib/authentication/pathnameHelper.ts
+++ packages/shared/lib/authentication/pathnameHelper.ts
@@ -2,10 +2,10 @@ import { getValidatedLocalID } from './sessionForkValidation';
 import { stripLeadingAndTrailingSlash } from '../helpers/string';
 import { isSSOMode, PUBLIC_PATH } from '../constants';
 
-export const getLocalIDPath = (u?: number) => (u === undefined ? undefined : `u${u}`);
+export const getLocalIDPath = (u?: number) => (u === undefined ? undefined : `u/${u}`);
 
 export const getLocalIDFromPathname = (pathname: string) => {
-    const maybeLocalID = pathname.match(/\/u(\d{0,6})\/?/);
+    const maybeLocalID = pathname.match(/\/u\/(\d{0,6})\/?/);
     return getValidatedLocalID(maybeLocalID?.[1]);
 };
 
