7dc790130f8b86306ccb56b1d7f605f0430bdb58
Richard <richard@protonmail.com>
2020-08-21 18:23:12 +0200

    [MAILWEB-1242] Bring the blockquote on top of the protonmail signature input field

---
+++ packages/components/containers/addresses/PMSignatureField.tsx
@@ -0,0 +1,46 @@
+import React, { ChangeEvent } from 'react';
+import { c } from 'ttag';
+import { PM_SIGNATURE } from 'proton-shared/lib/constants';
+import { updatePMSignature } from 'proton-shared/lib/api/mailSettings';
+import { MailSettings } from 'proton-shared/lib/interfaces';
+
+import { useToggle, useNotifications, useEventManager, useApiWithoutResult } from '../../hooks';
+import { Toggle, Field } from '../../components';
+
+interface Props {
+    id: string;
+    mailSettings: MailSettings;
+}
+
+const PMSignatureField = ({ id, mailSettings }: Props) => {
+    const { call } = useEventManager();
+    const { createNotification } = useNotifications();
+    const { request, loading } = useApiWithoutResult(updatePMSignature);
+    const { state, toggle } = useToggle(!!mailSettings.PMSignature);
+    const isMandatory = mailSettings.PMSignature === 2;
+
+    const handleChange = async ({ target }: ChangeEvent<HTMLInputElement>) => {
+        await request(+target.checked);
+        call();
+        toggle();
+        createNotification({ text: c('Success').t`Preference saved` });
+    };
+
+    return (
+        <>
+            <Field>
+                <div
+                    className="bordered-container pl1 pr1 pt0-5 pb0-5 mb1"
+                    dangerouslySetInnerHTML={{ __html: PM_SIGNATURE }}
+                />
+            </Field>
+            {isMandatory ? null : (
+                <div className="ml1 onmobile-ml0">
+                    <Toggle loading={loading} id={id} checked={state} onChange={handleChange} />
+                </div>
+            )}
+        </>
+    );
+};
+
+export default PMSignatureField;
--- packages/components/containers/addresses/ProtonFooterSection.tsx
+++ packages/components/containers/addresses/ProtonFooterSection.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { c } from 'ttag';
 
-import { Row, Label } from '../../components';
+import { Row, Label, Alert } from '../../components';
+import { useMailSettings } from '../../hooks';
 import PMSignatureField from './PMSignatureField';
 
 const ProtonFooterSection = () => {
+    const [mailSettings] = useMailSettings();
+    const isMandatory = mailSettings.PMSignature === 2;
     return (
-        <Row>
-            <Label htmlFor="pmSignatureToggle">{c('Label').t`ProtonMail signature`}</Label>
-            <PMSignatureField id="pmSignatureToggle" />
-        </Row>
+        <>
+            {isMandatory ? (
+                <Alert>{c('Info')
+                    .t`A paid plan is required to turn off the ProtonMail signature. Paid plan revenue allows us to continue supporting free accounts.`}</Alert>
+            ) : null}
+            <Row>
+                <Label htmlFor="pmSignatureToggle">{c('Label').t`ProtonMail signature`}</Label>
+                <PMSignatureField id="pmSignatureToggle" mailSettings={mailSettings} />
+            </Row>
+        </>
     );
 };
 
