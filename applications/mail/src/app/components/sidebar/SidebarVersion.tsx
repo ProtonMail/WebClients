29347aa4b3afb7e1b6d7411196d569acec81a91e
mmso <mmso@mmso.se>
2020-08-24 15:51:57 +0200

    Normalize app names

--- applications/mail/src/app/components/sidebar/SidebarVersion.tsx
+++ applications/mail/src/app/components/sidebar/SidebarVersion.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppVersion } from 'react-components';
 
 const SidebarVersion = () => {
-    return <AppVersion appName="ProtonMail" />;
+    return <AppVersion />;
 };
 
 export default SidebarVersion;
