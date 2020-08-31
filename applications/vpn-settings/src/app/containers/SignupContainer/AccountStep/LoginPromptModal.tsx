10eea4c6000f7d33bd6f91f7a77a7d8a5c667142
Richard <richard@protonmail.com>
2020-08-31 11:12:54 +0200

    [VPNFE-84] Update existing Proton account modal

---
+++ applications/vpn-settings/src/app/containers/SignupContainer/AccountStep/LoginPromptModal.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { c } from 'ttag';
+import { useHistory } from 'react-router-dom';
+import { ConfirmModal, Alert } from 'react-components';
+
+interface Props {
+    email: string;
+}
+
+const LoginPromptModal = ({ email, ...rest }: Props) => {
+    const history = useHistory();
+    const handleConfirm = () => history.push('/login');
+    return (
+        <ConfirmModal
+            title={c('Title').t`You already have a Proton account`}
+            confirm={c('Action').t`Go to login`}
+            onConfirm={handleConfirm}
+            {...rest}
+        >
+            <Alert type="warning">
+                {c('Info')
+                    .t`Your existing Proton account can be used to access all Proton services. Please login with ${email}`}
+            </Alert>
+        </ConfirmModal>
+    );
+};
+
+export default LoginPromptModal;
