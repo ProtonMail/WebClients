7f4668c8fdf565da0464b4d927dac66c3423fe07
Vincas Stonys <vincaslt@gmail.com>
2020-09-08 16:13:13 +0300

    Buffer rate limiter function

--- packages/shared/lib/helpers/function.ts
+++ packages/shared/lib/helpers/function.ts
@@ -53,6 +53,32 @@ export const throttle = <A extends any[]>(func: (...args: A) => void, ms = 50, c
     };
 };
 
+/** Similar to throttle but ensures first and last calls are made */
+export const buffer = <A extends any[]>(func: (...args: A) => void, ms = 50) => {
+    let cooldown: any;
+    let lastArgs: A | undefined;
+
+    const run = (...args: A) => {
+        if (!cooldown) {
+            func(...args);
+
+            cooldown = setTimeout(() => {
+                cooldown = undefined;
+
+                if (lastArgs) {
+                    const fnArgs = lastArgs;
+                    lastArgs = undefined;
+                    run(...fnArgs);
+                }
+            }, ms);
+        } else {
+            lastArgs = args;
+        }
+    };
+
+    return run;
+};
+
 export function defer(cb: () => void, delay: number) {
     const id = setTimeout(() => {
         cb();
