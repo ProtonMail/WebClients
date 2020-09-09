4851c9352031a9940a33a114c4117d7ebbcb617a
Vincas Stonys <vincas.stonys@protonmail.com>
2020-09-09 07:19:16 +0000

    Allow custom rate limiter function on useElementRect

--- packages/components/hooks/useElementRect.ts
+++ packages/components/hooks/useElementRect.ts
@@ -3,7 +3,7 @@ import { debounce } from 'proton-shared/lib/helpers/function';
 
 // Can't loop over DOMRect keys with getOwnPropertyNames.
 const keys = ['bottom', 'height', 'left', 'right', 'top', 'width', 'x', 'y'];
-const isEquivalent = (aRect, bRect) => {
+const isEquivalent = (aRect?: DOMRect, bRect?: DOMRect) => {
     if (!aRect && bRect) {
         return false;
     }
@@ -11,21 +11,23 @@ const isEquivalent = (aRect, bRect) => {
         return false;
     }
     for (const key of keys) {
-        if (aRect[key] !== bRect[key]) {
+        if (aRect?.[key as keyof DOMRect] !== bRect?.[key as keyof DOMRect]) {
             return false;
         }
     }
     return true;
 };
 
-const getElementRect = (target) => {
+const getElementRect = (target?: HTMLElement | null) => {
     if (!target) {
         return;
     }
     return target.getBoundingClientRect();
 };
 
-const useElementRect = (ref) => {
+type RateLimiter = <A extends any[]>(func: (...args: A) => void, wait: number) => (...args: A) => void;
+
+const useElementRect = <E extends HTMLElement>(ref: React.RefObject<E>, rateLimiter: RateLimiter = debounce) => {
     const [elementRect, setElementRect] = useState(() => getElementRect(ref.current));
 
     useLayoutEffect(() => {
@@ -33,29 +35,25 @@ const useElementRect = (ref) => {
         if (!target) {
             return;
         }
-        const reducer = (old, newRect) => {
-            if (isEquivalent(old, newRect)) {
-                return old;
-            }
-            return newRect;
-        };
+        const reducer = (old?: DOMRect, newRect?: DOMRect) => (isEquivalent(old, newRect) ? old : newRect);
 
         if (typeof ResizeObserver === 'function') {
-            // eslint-disable-next-line no-unused-vars
-            const onResize = debounce(([{ contentRect }]) => {
-                // The contentRect does not give correct global positions?
-                // setElementRect((old) => reducer(old, contentRect));
-                setElementRect((old) => reducer(old, getElementRect(target)));
-            }, 100);
-            const resizeObserver = new ResizeObserver(onResize, { box: 'border-box' });
-            resizeObserver.observe(target);
+            const resizeObserver = new ResizeObserver(
+                // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+                rateLimiter(([{ contentRect }]) => {
+                    // The contentRect does not give correct global positions?
+                    // setElementRect((old) => reducer(old, contentRect));
+                    setElementRect((old) => reducer(old, getElementRect(target)));
+                }, 100)
+            );
+            resizeObserver.observe(target, { box: 'border-box' });
             setElementRect((old) => reducer(old, getElementRect(target)));
             return () => {
-                resizeObserver.disconnect(target);
+                resizeObserver.disconnect();
             };
         }
 
-        const onResize = debounce(() => {
+        const onResize = rateLimiter(() => {
             setElementRect((old) => reducer(old, getElementRect(target)));
         }, 100);
         window.addEventListener('resize', onResize);
