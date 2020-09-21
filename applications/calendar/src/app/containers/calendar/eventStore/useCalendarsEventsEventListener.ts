579694098ff005dcff4c04bb16504a705732f873
Vincas Stonys <vincaslt@gmail.com>
2020-09-21 18:31:09 +0300

    Fix event manager types after refactoring subscribe function types

--- applications/calendar/src/app/containers/calendar/eventStore/useCalendarsEventsEventListener.ts
+++ applications/calendar/src/app/containers/calendar/eventStore/useCalendarsEventsEventListener.ts
@@ -7,63 +7,68 @@ import upsertCalendarApiEventWithoutBlob from './cache/upsertCalendarApiEventWit
 import { CalendarEventManager, CalendarEventsEventManager } from '../../../interfaces/EventManager';
 
 export const useCalendarCacheEventListener = (cacheRef: MutableRefObject<CalendarsEventsCache>) => {
-    const { subscribe } = useEventManager<{
-        CalendarEvents?: CalendarEventsEventManager[];
-        Calendars?: CalendarEventManager[];
-    }>();
+    const { subscribe } = useEventManager();
 
     useEffect(() => {
-        return subscribe(({ CalendarEvents = [], Calendars = [] }) => {
-            const cache = cacheRef.current;
-            if (!cache) {
-                return;
-            }
-            const { calendars } = cache;
-
-            let actions = 0;
-
-            Calendars.forEach(({ ID: CalendarID, Action }) => {
-                if (Action === EVENT_ACTIONS.DELETE) {
-                    delete calendars[CalendarID];
-                    actions++;
+        return subscribe(
+            ({
+                CalendarEvents = [],
+                Calendars = [],
+            }: {
+                CalendarEvents?: CalendarEventsEventManager[];
+                Calendars?: CalendarEventManager[];
+            }) => {
+                const cache = cacheRef.current;
+                if (!cache) {
+                    return;
                 }
-            });
+                const { calendars } = cache;
 
-            CalendarEvents.forEach((CalendarEventsChange) => {
-                if (CalendarEventsChange.Action === EVENT_ACTIONS.DELETE) {
-                    // The API does not send the calendar id to which this event belongs, so find it
-                    const calendarID = Object.keys(calendars).find((calendarID) => {
-                        return calendars[calendarID]?.events.has(CalendarEventsChange.ID);
-                    });
-                    const calendarsEventsCache = calendars[calendarID || 'undefined'];
-                    if (!calendarsEventsCache) {
-                        return;
+                let actions = 0;
+
+                Calendars.forEach(({ ID: CalendarID, Action }) => {
+                    if (Action === EVENT_ACTIONS.DELETE) {
+                        delete calendars[CalendarID];
+                        actions++;
                     }
-                    removeCalendarEventStoreRecord(CalendarEventsChange.ID, calendarsEventsCache);
-                    // TODO: Only increment count if this event happened in the date range we are currently interested in
-                    actions++;
-                }
+                });
 
-                if (
-                    CalendarEventsChange.Action === EVENT_ACTIONS.UPDATE ||
-                    CalendarEventsChange.Action === EVENT_ACTIONS.CREATE
-                ) {
-                    const { CalendarID } = CalendarEventsChange.Event;
-
-                    const calendarsEventsCache = calendars[CalendarID];
-                    if (!calendarsEventsCache) {
-                        return;
+                CalendarEvents.forEach((CalendarEventsChange) => {
+                    if (CalendarEventsChange.Action === EVENT_ACTIONS.DELETE) {
+                        // The API does not send the calendar id to which this event belongs, so find it
+                        const calendarID = Object.keys(calendars).find((calendarID) => {
+                            return calendars[calendarID]?.events.has(CalendarEventsChange.ID);
+                        });
+                        const calendarsEventsCache = calendars[calendarID || 'undefined'];
+                        if (!calendarsEventsCache) {
+                            return;
+                        }
+                        removeCalendarEventStoreRecord(CalendarEventsChange.ID, calendarsEventsCache);
+                        // TODO: Only increment count if this event happened in the date range we are currently interested in
+                        actions++;
                     }
-                    upsertCalendarApiEventWithoutBlob(CalendarEventsChange.Event, calendarsEventsCache);
-                    // TODO: Only increment count if this event happened in the date range we are currently interested in
-                    actions++;
-                }
-            });
 
-            if (actions > 0) {
-                cache.rerender?.();
+                    if (
+                        CalendarEventsChange.Action === EVENT_ACTIONS.UPDATE ||
+                        CalendarEventsChange.Action === EVENT_ACTIONS.CREATE
+                    ) {
+                        const { CalendarID } = CalendarEventsChange.Event;
+
+                        const calendarsEventsCache = calendars[CalendarID];
+                        if (!calendarsEventsCache) {
+                            return;
+                        }
+                        upsertCalendarApiEventWithoutBlob(CalendarEventsChange.Event, calendarsEventsCache);
+                        // TODO: Only increment count if this event happened in the date range we are currently interested in
+                        actions++;
+                    }
+                });
+
+                if (actions > 0) {
+                    cache.rerender?.();
+                }
             }
-        });
+        );
     }, []);
 };
 
