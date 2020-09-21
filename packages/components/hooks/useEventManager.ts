4a674833baa7a885408eb8f94720b3f28ff312c2
Vincas Stonys <vincaslt@gmail.com>
2020-09-21 18:30:41 +0300

    Move event manager type inference to subscribe function

--- packages/components/hooks/useEventManager.ts
+++ packages/components/hooks/useEventManager.ts
@@ -1,15 +1,14 @@
 import { useContext } from 'react';
-import { EventManager } from 'proton-shared/lib/eventManager/eventManager';
 import Context from '../containers/eventManager/context';
 
-function useEventManager<EventModel, ListenerResult = void>() {
+function useEventManager() {
     const eventManager = useContext(Context);
 
     if (!eventManager) {
         throw new Error('Trying to use uninitialized EventManagerContext');
     }
 
-    return eventManager as EventManager<EventModel, ListenerResult>;
+    return eventManager;
 }
 
 export default useEventManager;
