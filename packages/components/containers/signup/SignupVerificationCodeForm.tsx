205203bc86c0fdd0a1585d238045603066b0cedf
Nicolas Hoffmann <dante3333@gmail.com>
2020-09-22 18:26:16 +0200

    [CP-887] fix - dropdown alignment/account verification layout

--- packages/components/containers/signup/SignupVerificationCodeForm.tsx
+++ packages/components/containers/signup/SignupVerificationCodeForm.tsx
@@ -1,10 +1,9 @@
 import React, { ChangeEvent, FormEvent } from 'react';
 import { c } from 'ttag';
 
-import { VerificationCodeInput, InlineLinkButton, Alert } from '../../components';
+import { VerificationCodeInput, InlineLinkButton } from '../../components';
 import { SignupModel, SignupErrors } from './interfaces';
 import { PrimaryButton } from '../../components/button';
-import SignupLabelInputRow from './SignupLabelInputRow';
 import SignupSubmitRow from './SignupSubmitRow';
 
 interface Props {
@@ -21,25 +20,21 @@ const SignupVerificationCodeForm = ({ model, onChange, onSubmit, onResend, error
     const destinationBold = <strong key="destination">{model.email}</strong>;
     return (
         <form name="humanForm" onSubmit={onSubmit}>
-            <Alert>{c('Info').t`For security reasons, please verify that your are not a robot.`}</Alert>
-            <SignupLabelInputRow
-                label={
-                    <label htmlFor="verification-code">{c('Label')
-                        .jt`Enter the verification code that was sent to ${destinationBold}. If you don't find the email in your inbox, please check your spam folder`}</label>
+            <div className="mb1">{c('Info').t`For security reasons, please verify that your are not a robot.`}</div>
+            <div className="mb1">{c('Info')
+                .jt`Enter the verification code that was sent to ${destinationBold}. If you don't find the email in your inbox, please check your spam folder.`}</div>
+
+            <label className="pm-label" htmlFor="verification-code">{c('Label').t`Verification code`}</label>
+            <VerificationCodeInput
+                id="verification-code"
+                className="mb0-5"
+                value={model.verificationCode}
+                error={errors.verificationCode}
+                onChange={({ target }: ChangeEvent<HTMLInputElement>) =>
+                    onChange({ ...model, verificationCode: target.value })
                 }
-                input={
-                    <VerificationCodeInput
-                        id="verification-code"
-                        className="mb1"
-                        value={model.verificationCode}
-                        error={errors.verificationCode}
-                        onChange={({ target }: ChangeEvent<HTMLInputElement>) =>
-                            onChange({ ...model, verificationCode: target.value })
-                        }
-                        autoFocus
-                        required
-                    />
-                }
+                autoFocus
+                required
             />
             <div className="mb1">
                 <InlineLinkButton disabled={loading} onClick={onResend}>{c('Action')
