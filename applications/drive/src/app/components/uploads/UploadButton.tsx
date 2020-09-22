b73fd9d1e9069e815cdcf8d98fadea63cc7fd50b
Matthieu Bué <matthieu.bue@gmail.com>
2020-09-22 19:14:03 +0200

    Move fab button when transfer manager is open

--- applications/drive/src/app/components/uploads/UploadButton.tsx
+++ applications/drive/src/app/components/uploads/UploadButton.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { c } from 'ttag';
 
-import { FloatingButton, SidebarPrimaryButton } from 'react-components';
+import { classnames, FloatingButton, SidebarPrimaryButton } from 'react-components';
 
 import { useDriveActiveFolder } from '../Drive/DriveFolderProvider';
 import useFileUploadInput from '../../hooks/drive/useFileUploadInput';
+import { useDownloadProvider } from '../downloads/DownloadProvider';
+import { useUploadProvider } from './UploadProvider';
 
 interface Props {
     floating?: boolean;
@@ -14,11 +16,16 @@ const UploadButton = ({ floating }: Props) => {
     const { folder } = useDriveActiveFolder();
     const { inputRef: fileInput, handleClick, handleChange: handleFileChange } = useFileUploadInput();
 
+    const { downloads } = useDownloadProvider();
+    const { uploads } = useUploadProvider();
+    const isTransfering = uploads.length > 0 || downloads.length > 0;
+
     return (
         <>
             <input multiple type="file" ref={fileInput} className="hidden" onChange={handleFileChange} />
             {floating ? (
                 <FloatingButton
+                    className={classnames([isTransfering && 'compose-fab--is-higher'])}
                     disabled={!folder?.shareId}
                     onClick={handleClick}
                     title={c('Action').t`New upload`}
