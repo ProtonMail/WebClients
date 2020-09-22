457255fd0826dccdb7383a1323ec0a13ad575850
mmso <mmso@mmso.se>
2020-09-22 10:55:02 +0200

    Revert "Account support dropdown without without decoration"
    This reverts commit 1f8ea10971d97b99052c37a2806826f778fa6b78.

--- packages/components/containers/app/InternalEmailAddressGeneration.tsx
+++ packages/components/containers/app/InternalEmailAddressGeneration.tsx
@@ -24,7 +24,7 @@ const AppAccountPublicLayoutWrapper = ({ children, ...rest }: AccountProps) => {
         <AccountPublicLayout
             center={<ProtonLogo />}
             right={
-                <AccountSupportDropdown noCaret className="link nodecoration">
+                <AccountSupportDropdown noCaret className="link">
                     {c('Action').t`Need help?`}
                 </AccountSupportDropdown>
             }
