cd4ea0631f949d09a45674a50cf58bf733c1846c
mmso <mmso@mmso.se>
2020-09-22 16:27:46 +0200

    Change logout to sign out
    CP-899
    CP-902

--- packages/components/containers/logs/LogsTable.js
+++ packages/components/containers/logs/LogsTable.js
@@ -17,11 +17,11 @@ const ICONS = {
 };
 
 const getEventsI18N = () => ({
-    [LOGIN_FAILURE_PASSWORD]: c('Log event').t`Login failure (password)`,
-    [LOGIN_SUCCESS]: c('Log event').t`Login success`,
-    [LOGOUT]: c('Log event').t`Logout`,
-    [LOGIN_FAILURE_2FA]: c('Log event').t`Login failure (2FA)`,
-    [LOGIN_SUCCESS_AWAIT_2FA]: c('Log event').t`Login success (2FA)`,
+    [LOGIN_FAILURE_PASSWORD]: c('Log event').t`Sign in failure (password)`,
+    [LOGIN_SUCCESS]: c('Log event').t`Sign in success`,
+    [LOGOUT]: c('Log event').t`Sign out`,
+    [LOGIN_FAILURE_2FA]: c('Log event').t`Sign in failure (2FA)`,
+    [LOGIN_SUCCESS_AWAIT_2FA]: c('Log event').t`Sign in success (2FA)`,
 });
 
 const LogsTable = ({ logs, logAuth, loading }) => {
