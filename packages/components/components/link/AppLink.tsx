42204eb86abcde980782fd74bffa909298bc76e8
mmso <mmso@mmso.se>
2020-10-02 11:18:49 +0200

    Force open app links in the same tab in firefox
    CP-949

--- packages/components/components/link/AppLink.tsx
+++ packages/components/components/link/AppLink.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import { APP_NAMES, isSSOMode, isStandaloneMode } from 'proton-shared/lib/constants';
 import { getAppHref, getAppHrefBundle } from 'proton-shared/lib/apps/helper';
+import { isFirefox as getIsFirefox } from 'proton-shared/lib/helpers/browser';
 
 import { useConfig, useAuthentication } from '../../hooks';
 import Tooltip from '../tooltip/Tooltip';
@@ -11,6 +12,8 @@ export interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
     toApp?: APP_NAMES;
 }
 
+const isFirefox = getIsFirefox();
+
 const AppLink = ({ to, toApp, children, ...rest }: Props) => {
     const { APP_NAME } = useConfig();
     const authentication = useAuthentication();
@@ -19,10 +22,20 @@ const AppLink = ({ to, toApp, children, ...rest }: Props) => {
         if (isSSOMode) {
             const localID = authentication.getLocalID?.();
             const href = getAppHref(to, toApp, localID);
+            // TODO: Extend this to private session
+            const isForceOpenSameTab = isFirefox;
+            // Firefox does not preserve localStorage across tabs in certain configurations.
+            // https://bugzilla.mozilla.org/show_bug.cgi?id=1540402
+            const overrides = isForceOpenSameTab
+                ? {
+                      target: '_self',
+                      href,
+                  }
+                : {};
             return (
                 // internal link, trusted
                 // eslint-disable-next-line react/jsx-no-target-blank
-                <a target="_blank" {...rest} href={href}>
+                <a target="_blank" {...rest} {...overrides}>
                     {children}
                 </a>
             );
