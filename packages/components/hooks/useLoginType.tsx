2f117b2af76d8e8f2fcb841753dd2627277be6bc
mmso <mmso@mmso.se>
2020-10-05 12:12:44 +0200

    Revert firefox workaround
    Doesn't work either, storage is cleared when navigating between urls

--- packages/components/hooks/useLoginType.tsx
+++ packages/components/hooks/useLoginType.tsx
@@ -1,14 +1,8 @@
 import { useState } from 'react';
-import { isFirefox } from 'proton-shared/lib/helpers/browser';
 import { LoginTypes } from 'proton-shared/lib/authentication/LoginInterface';
 
 const useLoginType = () => {
     const [loginType] = useState(() => {
-        // Firefox does not preserve localStorage across tabs in certain configurations.
-        // https://bugzilla.mozilla.org/show_bug.cgi?id=1540402
-        if (isFirefox()) {
-            return LoginTypes.PERSISTENT_WORKAROUND;
-        }
         return LoginTypes.PERSISTENT;
     });
     return loginType;
