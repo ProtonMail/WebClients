637fa3ad68adf86b420d8aaf67f9fb77229547a0
Mattias Svanström <mmso@mmso.se>
2020-10-08 15:54:31 +0000

    Get scrollable parent

--- packages/shared/lib/helpers/dom.ts
+++ packages/shared/lib/helpers/dom.ts
@@ -44,3 +44,32 @@ export const isElement = (node: Node | null) => node && node.nodeType === 1;
  * Returns the node if it's an element or the parent element if not
  */
 export const getElement = (node: Node | null) => (isElement(node) ? (node as Element) : node?.parentElement || null);
+
+/**
+ * From https://stackoverflow.com/a/42543908
+ */
+export const getScrollParent = (element: HTMLElement | null | undefined, includeHidden = false) => {
+    if (!element) {
+        return document.body;
+    }
+
+    const style = getComputedStyle(element);
+    const excludeStaticParent = style.position === 'absolute';
+    const overflowRegex = includeHidden ? /(auto|scroll|hidden)/ : /(auto|scroll)/;
+
+    if (style.position === 'fixed') {
+        return document.body;
+    }
+
+    for (let parent = element.parentElement; parent; parent = parent.parentElement) {
+        const style = getComputedStyle(parent);
+        if (excludeStaticParent && style.position === 'static') {
+            continue;
+        }
+        if (overflowRegex.test(style.overflow + style.overflowY + style.overflowX)) {
+            return parent;
+        }
+    }
+
+    return document.body;
+};
