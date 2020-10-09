8c9024792e4ccb71dea1d75c197d38e82a584eba
Eduardo Conde Pena <econdepe@protonmail.com>
2020-10-09 12:01:17 +0000

    Change invitation answer from calendar
    CALWEB-1410

---
+++ packages/components/components/calendar/InviteButtons.tsx
@@ -0,0 +1,86 @@
+import { ICAL_ATTENDEE_STATUS } from 'proton-shared/lib/calendar/constants';
+import { move } from 'proton-shared/lib/helpers/array';
+import { noop } from 'proton-shared/lib/helpers/function';
+import { InviteActions } from 'proton-shared/lib/interfaces/calendar';
+import { c } from 'ttag';
+import React from 'react';
+import { useLoadingMap } from '../../hooks';
+import { SmallButton } from '../button';
+import { DropdownActions } from '../dropdown';
+
+interface Props {
+    actions: InviteActions;
+    partstat?: ICAL_ATTENDEE_STATUS;
+    disabled?: boolean;
+    className?: string;
+}
+const InviteButtons = ({ actions, partstat = ICAL_ATTENDEE_STATUS.NEEDS_ACTION, disabled, className = '' }: Props) => {
+    const [loadingMap, withLoadingMap] = useLoadingMap();
+
+    const { accept, acceptTentatively, decline } = actions;
+    const onAccept = async () => withLoadingMap({ accept: accept() });
+    const onTentative = async () => withLoadingMap({ tentative: acceptTentatively() });
+    const onDecline = async () => withLoadingMap({ decline: decline() });
+
+    const { accept: loadingAccept, tentative: loadingTentative, decline: loadingDecline } = loadingMap;
+    const loadingAnswer = loadingAccept || loadingTentative || loadingDecline;
+
+    if (partstat === ICAL_ATTENDEE_STATUS.NEEDS_ACTION) {
+        return (
+            <div className={className}>
+                <SmallButton
+                    onClick={onAccept}
+                    disabled={loadingAnswer || disabled}
+                    loading={loadingAccept}
+                    className="mr0-5"
+                >
+                    {c('Action').t`Yes`}
+                </SmallButton>
+                <SmallButton
+                    onClick={onTentative}
+                    disabled={loadingAnswer || disabled}
+                    loading={loadingTentative}
+                    className="mr0-5"
+                >
+                    {c('Action').t`Maybe`}
+                </SmallButton>
+                <SmallButton onClick={onDecline} disabled={loadingAnswer || disabled} loading={loadingDecline}>
+                    {c('Action').t`No`}
+                </SmallButton>
+            </div>
+        );
+    }
+    const accepted = partstat === ICAL_ATTENDEE_STATUS.ACCEPTED;
+    const tentative = partstat === ICAL_ATTENDEE_STATUS.TENTATIVE;
+    const declined = partstat === ICAL_ATTENDEE_STATUS.DECLINED;
+    const list = [
+        {
+            text: c('Action').t`Yes, I'm attending`,
+            onClick: accepted ? noop : onAccept,
+        },
+        {
+            text: c('Action').t`Maybe I'm attending`,
+            onClick: tentative ? noop : onTentative,
+        },
+        {
+            text: c('Action').t`No, I'm not attending`,
+            onClick: declined ? noop : onDecline,
+        },
+    ];
+    const answerIndex = [accepted, tentative, declined].findIndex((bool) => bool === true);
+    const orderedList = move(list, answerIndex, 0);
+    list.unshift();
+    return (
+        <div className={className}>
+            <DropdownActions
+                className="pm-button--small"
+                key="actions"
+                list={orderedList}
+                loading={loadingAnswer}
+                disabled={disabled}
+            />
+        </div>
+    );
+};
+
+export default InviteButtons;
---
+++ packages/components/hooks/useSendIcs.ts
@@ -0,0 +1,119 @@
+import { enums } from 'openpgp';
+import { encryptMessage } from 'pmcrypto';
+import { MIME_TYPES } from 'proton-shared/lib/constants';
+import { Recipient } from 'proton-shared/lib/interfaces';
+import { Packages, SendPreferences } from 'proton-shared/lib/interfaces/mail/crypto';
+import { Message, SimpleMessageExtended } from 'proton-shared/lib/interfaces/mail/Message';
+import { RequireSome, SimpleMap } from 'proton-shared/lib/interfaces/utils';
+import { createDraft, sendMessage } from 'proton-shared/lib/api/messages';
+import { splitKeys } from 'proton-shared/lib/keys/keys';
+import { uploadAttachment } from 'proton-shared/lib/api/attachments';
+import { addReceived } from 'proton-shared/lib/mail/messages';
+import getSendPreferences from 'proton-shared/lib/mail/send/getSendPreferences';
+import { encryptAttachment } from 'proton-shared/lib/mail/send/attachments';
+import { encryptPackages } from 'proton-shared/lib/mail/send/sendEncrypt';
+import { attachSubPackages } from 'proton-shared/lib/mail/send/sendSubPackages';
+import { useCallback } from 'react';
+import { useApi, useGetAddressKeys, useGetEncryptionPreferences, useUserSettings } from './index';
+import { generateUID } from '../helpers/component';
+
+const { PLAINTEXT } = MIME_TYPES;
+
+interface Params {
+    ics: string;
+    from: RequireSome<Recipient, 'Address' | 'Name'>;
+    addressID: string;
+    to: RequireSome<Recipient, 'Address' | 'Name'>[];
+    subject: string;
+    plainTextBody?: string;
+}
+
+export const useSendIcs = () => {
+    const api = useApi();
+    const [userSettings] = useUserSettings();
+    const getAddressKeys = useGetAddressKeys();
+    const getEncryptionPreferences = useGetEncryptionPreferences();
+
+    const send = useCallback(
+        async ({ ics, from, addressID, to, subject, plainTextBody = '' }: Params) => {
+            if (!addressID) {
+                throw new Error('Missing addressID');
+            }
+            const { publicKeys: allPublicKeys, privateKeys: allPrivateKeys } = splitKeys(
+                await getAddressKeys(addressID)
+            );
+            const [publicKeys, privateKeys] = [allPublicKeys.slice(0, 1), allPrivateKeys.slice(0, 1)];
+            const inputMessage: SimpleMessageExtended = {
+                localID: generateUID('reply-invitation'),
+                plainText: plainTextBody,
+                publicKeys,
+                privateKeys,
+                data: {
+                    AddressID: addressID,
+                    Subject: subject,
+                    Sender: from,
+                    ToList: to,
+                    CCList: [],
+                    BCCList: [],
+                    MIMEType: MIME_TYPES.PLAINTEXT,
+                },
+            };
+            const { data: Body } = await encryptMessage({
+                data: inputMessage.plainText,
+                publicKeys: [publicKeys?.[0]],
+                privateKeys: [privateKeys?.[0]],
+                compression: enums.compression.zip,
+            });
+            const { Message: updatedMessageData } = await api<{ Message: Message }>(
+                // MESSAGE_ACTIONS.NEW = -1, better not to export that constant for the moment
+                createDraft({
+                    Action: -1,
+                    Message: { ...inputMessage.data, Body },
+                    AttachmentKeyPackets: {},
+                } as any)
+            );
+            const replyAttachment = new File([new Blob([ics])], 'invite.ics', { type: 'text/calendar; method=REPLY' });
+            const packets = await encryptAttachment(ics, replyAttachment, false, publicKeys, privateKeys);
+
+            const { Attachment } = await api(
+                await uploadAttachment({
+                    Filename: packets.Filename,
+                    MessageID: updatedMessageData.ID,
+                    ContentID: '',
+                    MIMEType: packets.MIMEType,
+                    KeyPackets: new Blob([packets.keys] as any),
+                    DataPacket: new Blob([packets.data] as any),
+                    Signature: packets.signature ? new Blob([packets.signature] as any) : undefined,
+                })
+            );
+            const message = {
+                ...inputMessage,
+                data: { ...updatedMessageData, Attachments: [Attachment] },
+            };
+            const emails = to.map(({ Address }) => Address);
+            const mapSendPrefs: SimpleMap<SendPreferences> = {};
+            await Promise.all(
+                emails.map(async (email) => {
+                    const encryptionPreferences = await getEncryptionPreferences(email);
+                    const sendPreferences = getSendPreferences(encryptionPreferences);
+                    mapSendPrefs[email] = sendPreferences;
+                })
+            );
+            let packages: Packages = {
+                [PLAINTEXT]: {
+                    Flags: addReceived(message.data?.Flags),
+                    Addresses: {},
+                    MIMEType: PLAINTEXT,
+                    Body: plainTextBody,
+                },
+            };
+            packages = await attachSubPackages(packages, message.data, emails, mapSendPrefs, api);
+            packages = await encryptPackages(message, packages, getAddressKeys);
+            await api(sendMessage(message.data.ID, { Packages: packages, AutoSaveContacts: 0 } as any));
+        },
+        [api, userSettings, getAddressKeys, getEncryptionPreferences]
+    );
+    return send;
+};
+
+export default useSendIcs;
