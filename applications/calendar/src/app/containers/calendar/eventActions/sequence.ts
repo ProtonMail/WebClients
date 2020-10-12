1be79772ef52bef023003e1831bfb24580db9675
econdepe <econdepe@protonmail.com>
2020-10-12 16:33:11 +0200

    Use new helper in proton-shared

--- applications/calendar/src/app/containers/calendar/eventActions/sequence.ts
+++ applications/calendar/src/app/containers/calendar/eventActions/sequence.ts
@@ -1,42 +1,40 @@
-import { getDtendProperty, propertyToUTCDate } from 'proton-shared/lib/calendar/vcalConverter';
+import { getHasModifiedDateTimes } from 'proton-shared/lib/calendar/vcalConverter';
 import { getIsPropertyAllDay } from 'proton-shared/lib/calendar/vcalHelper';
 import isDeepEqual from 'proton-shared/lib/helpers/isDeepEqual';
 import { VcalVeventComponent } from 'proton-shared/lib/interfaces/calendar/VcalModel';
 
-export const withIncreasedSequence = (event: VcalVeventComponent) => {
-    const sequenceValue = event.sequence?.value;
+export const withIncreasedSequence = (vevent: VcalVeventComponent) => {
+    const sequenceValue = vevent.sequence?.value;
     if (sequenceValue === undefined) {
-        return { ...event, sequence: { value: 0 } };
+        return { ...vevent, sequence: { value: 0 } };
     }
     return {
-        ...event,
+        ...vevent,
         sequence: { value: Math.max(0, sequenceValue) + 1 },
     };
 };
 
 export const withVeventSequence = (
-    event: VcalVeventComponent,
-    oldEvent: VcalVeventComponent,
+    vevent: VcalVeventComponent,
+    oldVevent: VcalVeventComponent,
     hasModifiedRrule?: boolean
 ) => {
-    if (oldEvent.sequence?.value === undefined) {
-        return { ...event, sequence: { value: 0 } };
+    if (oldVevent.sequence?.value === undefined) {
+        return { ...vevent, sequence: { value: 0 } };
     }
-    const { dtstart, rrule } = event;
-    const { dtstart: oldDtstart, rrule: oldRrule, sequence: oldSequence } = oldEvent;
-    const { sequence: newSequence } = event;
-    const [dtend, oldDtend] = [event, oldEvent].map(getDtendProperty);
+    const { dtstart, rrule } = vevent;
+    const { dtstart: oldDtstart, rrule: oldRrule, sequence: oldSequence } = oldVevent;
+    const { sequence: newSequence } = vevent;
     const [isAllDay, oldIsAllDay] = [dtstart, oldDtstart].map(getIsPropertyAllDay);
     const isAllDayPreserved = isAllDay === oldIsAllDay;
-    const isStartPreserved = +propertyToUTCDate(dtstart) === +propertyToUTCDate(oldDtstart);
-    const isEndPreserved = +propertyToUTCDate(dtend) === +propertyToUTCDate(oldDtend);
+    const areDateTimesPreserved = !getHasModifiedDateTimes(vevent, oldVevent);
     const isRrulePreserved = hasModifiedRrule === undefined ? isDeepEqual(rrule, oldRrule) : !hasModifiedRrule;
     const oldSequenceValue = Math.max(0, oldSequence.value);
-    if (isAllDayPreserved && isStartPreserved && isEndPreserved && isRrulePreserved) {
-        return newSequence ? { ...event } : { ...event, sequence: { value: oldSequenceValue } };
+    if (isAllDayPreserved && areDateTimesPreserved && isRrulePreserved) {
+        return newSequence ? { ...vevent } : { ...vevent, sequence: { value: oldSequenceValue } };
     }
     return {
-        ...event,
+        ...vevent,
         sequence: { value: oldSequenceValue + 1 },
     };
 };
