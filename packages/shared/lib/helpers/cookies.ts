85a6d925b6780611b0c396565954fdf61436e47c
Richard <richard@protonmail.com>
2020-10-14 18:57:54 +0200

    Add setCookie helper

--- packages/shared/lib/helpers/cookies.ts
+++ packages/shared/lib/helpers/cookies.ts
@@ -1,3 +1,5 @@
+import isTruthy from './isTruthy';
+
 export const getCookies = (): string[] => {
     try {
         return document.cookie.split(';').map((item) => item.trim());
@@ -8,4 +10,14 @@ export const getCookies = (): string[] => {
 
 export const checkCookie = (name: string, value: string) => {
     return getCookies().some((cookie) => cookie.includes(`${name}=${value}`));
+};
+
+export const setCookie = (cookieName: string, cookieValue: string, expirationDate?: string, cookieDomain?: string) => {
+    document.cookie = [
+        `${cookieName}=${cookieValue}`,
+        expirationDate && `expires=${expirationDate}`,
+        cookieDomain && `domain=${cookieDomain}`,
+    ]
+        .filter(isTruthy)
+        .join(';');
 };
