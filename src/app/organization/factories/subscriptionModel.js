7bd08cb65a2ae5ac17491beea35e175288e3d9ea
Richard <richard@protonmail.com>
2020-10-28 15:26:16 +0100

    Hotfix - Remove people having addons on BF promo

--- src/app/organization/factories/subscriptionModel.js
+++ src/app/organization/factories/subscriptionModel.js
@@ -127,19 +127,21 @@ function subscriptionModel(dispatchers, Payment) {
         return CouponCode === coupon;
     };
 
+    const getAddons = () => {
+        const { Plans = [] } = CACHE.subscription || {};
+        return Plans.filter(({ Type }) => Type === PLANS_TYPE.ADDON);
+    };
+
     const isProductPayer = () => {
+        const addons = getAddons();
         const { CouponCode = '' } = CACHE.subscription || {};
         const noPro = !hasPaid('professional');
         const isPaying = hasPaid('plus') || hasPaid('vpnplus') || hasPaid('vpnbasic');
         const noBundle = !(hasPaid('plus') && hasPaid('vpnplus'));
         const noBFCoupon = ![BLACK_FRIDAY.COUPON_CODE].includes(CouponCode);
+        const noAddons = !addons.length;
 
-        return isPaying && noPro && noBundle && noBFCoupon;
-    };
-
-    const getAddons = () => {
-        const { Plans = [] } = CACHE.subscription || {};
-        return Plans.filter(({ Type }) => Type === PLANS_TYPE.ADDON);
+        return isPaying && noPro && noBundle && noBFCoupon && noAddons;
     };
 
     on('app.event', (event, { type, data }) => {
