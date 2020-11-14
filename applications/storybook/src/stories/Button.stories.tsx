8d25f87d8b28868c35f443c79c5c9e536ed48091
mmso <mmso@mmso.se>
2020-11-14 19:58:55 +0100

    Add loading button

--- applications/storybook/src/stories/Button.stories.tsx
+++ applications/storybook/src/stories/Button.stories.tsx
@@ -4,4 +4,5 @@ import { PrimaryButton, Button } from 'react-components';
 export default { component: Button, title: 'Proton UI / Button' };
 
 export const Basic = () => <Button>Button</Button>;
+export const LoadingButton = () => <Button loading>Button</Button>;
 export const Primary = () => <PrimaryButton>Button</PrimaryButton>;
