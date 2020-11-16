656d746eb00f4f064cb8589e34ae8cd2b06dd4a9
Richard <richard@protonmail.com>
2020-11-16 13:45:39 +0100

    Move cookie functions to browser helpers

--- src/app/user/services/setPaidCookie.js
+++ src/app/user/services/setPaidCookie.js
@@ -1,34 +1,8 @@
+import { getSecondLevelDomain, getCookie, setCookie } from '../../../helpers/browser';
+
 /* @ngInject */
 function setPaidCookie(userType) {
     const COOKIE_NAME = 'is-paid-user';
-
-    const getSecondLevelDomain = () => {
-        const { hostname } = window.location;
-        return hostname.substr(hostname.indexOf('.') + 1);
-    };
-
-    const getCookie = (name, cookies = document.cookie) => {
-        return `; ${cookies}`.match(`;\\s*${name}=([^;]+)`)?.[1];
-    };
-
-    const setCookie = ({
-        cookieName,
-        cookieValue: maybeCookieValue,
-        expirationDate: maybeExpirationDate,
-        path,
-        cookieDomain
-    }) => {
-        const expirationDate = maybeCookieValue === undefined ? new Date(0).toUTCString() : maybeExpirationDate;
-        const cookieValue = maybeCookieValue === undefined ? '' : maybeCookieValue;
-        document.cookie = [
-            `${cookieName}=${cookieValue}`,
-            expirationDate && `expires=${expirationDate}`,
-            cookieDomain && `domain=${cookieDomain}`,
-            path && `path=${path}`
-        ]
-            .filter(Boolean)
-            .join(';');
-    };
 
     return () => {
         const { isPaid } = userType();
--- src/helpers/browser.js
+++ src/helpers/browser.js
@@ -179,3 +179,31 @@ export function patchAtob() {
         })(window.atob);
     }
 }
+
+export const getSecondLevelDomain = () => {
+    const { hostname } = window.location;
+    return hostname.substr(hostname.indexOf('.') + 1);
+};
+
+export const getCookie = (name, cookies = document.cookie) => {
+    return `; ${cookies}`.match(`;\\s*${name}=([^;]+)`)?.[1];
+};
+
+export const setCookie = ({
+    cookieName,
+    cookieValue: maybeCookieValue,
+    expirationDate: maybeExpirationDate,
+    path,
+    cookieDomain
+}) => {
+    const expirationDate = maybeCookieValue === undefined ? new Date(0).toUTCString() : maybeExpirationDate;
+    const cookieValue = maybeCookieValue === undefined ? '' : maybeCookieValue;
+    document.cookie = [
+        `${cookieName}=${cookieValue}`,
+        expirationDate && `expires=${expirationDate}`,
+        cookieDomain && `domain=${cookieDomain}`,
+        path && `path=${path}`
+    ]
+        .filter(Boolean)
+        .join(';');
+};
