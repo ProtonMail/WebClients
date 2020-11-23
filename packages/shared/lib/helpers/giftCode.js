10234e4f1d19a406f281b530a0bf0c76016ca4c2
Richard <richard@protonmail.com>
2020-11-23 11:19:33 +0100

    Remove useless helper

--- packages/shared/lib/helpers/giftCode.js
+++
@@ -1,8 +0,0 @@
-import { GIFT_CODE_LENGTH } from '../constants';
-
-export const clean = (code = '') => code.replace(/-|\s|\t/g, '');
-
-export const isValid = (input = '') => {
-    const code = clean(input);
-    return code.length === GIFT_CODE_LENGTH && /\w\d+/g.test(code);
-};
