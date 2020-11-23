e9600653a754d80f8f6e514d00f4a77f0c8dffa2
Richard <richard@protonmail.com>
2020-11-23 14:50:55 +0000

    [BF2020-69] Don't use cookie, rely only on API state

--- packages/components/containers/heading/usePromoModalState.ts
+++ packages/components/containers/heading/usePromoModalState.ts
@@ -1,51 +1,16 @@
-import { useRef, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getFeature, updateFeatureValue } from 'proton-shared/lib/api/features';
-import { getSHA256String } from 'proton-shared/lib/helpers/hash';
-import { BLACK_FRIDAY } from 'proton-shared/lib/constants';
-import { getCookie, setCookie } from 'proton-shared/lib/helpers/cookies';
-import { getSecondLevelDomain } from 'proton-shared/lib/helpers/url';
 
-import { useApi, useUser, useLoading } from '../../hooks';
+import { useApi, useLoading } from '../../hooks';
 
 const FEATURE_ID = 'BlackFridayPromoShown';
-
-const cookieExpirationDate = BLACK_FRIDAY.END.toUTCString();
-const cookiePath = '/';
-const cookieDomain = `.${getSecondLevelDomain()}`;
-
-const setModalCookie = (key: string, value: string) => {
-    setCookie({
-        cookieName: key,
-        cookieValue: value,
-        expirationDate: cookieExpirationDate,
-        path: cookiePath,
-        cookieDomain,
-    });
-};
 
 const usePromoModalState = () => {
     const [loading, withLoading] = useLoading(true);
     const [state, setState] = useState(false);
     const api = useApi();
-    const [user] = useUser();
-    const keys = useRef<{ blackFridayStateKey?: string; productPayerStateKey?: string }>({});
 
     const fetchFeature = async () => {
-        // Search cookie state first
-        const [newBlackFridayStateKey, newProductPayerStateKey] = await Promise.all([
-            getSHA256String(`${user.ID}${BLACK_FRIDAY.COUPON_CODE}-black-friday-modal`),
-            getSHA256String(`${user.ID}-product-payer-modal`),
-        ]);
-
-        keys.current.blackFridayStateKey = newBlackFridayStateKey.slice(0, 8);
-        keys.current.productPayerStateKey = newProductPayerStateKey.slice(0, 8);
-
-        if (getCookie(keys.current.blackFridayStateKey) || getCookie(keys.current.productPayerStateKey)) {
-            setState(true);
-            return;
-        }
-
-        // Otherwise check API state
         const { Feature } = await api(getFeature(FEATURE_ID));
         const { Value, DefaultValue } = Feature;
         setState(typeof Value === 'undefined' ? DefaultValue : Value);
@@ -58,10 +23,6 @@ const usePromoModalState = () => {
     const onChange = async (Value: boolean) => {
         await api(updateFeatureValue(FEATURE_ID, Value));
         setState(Value);
-
-        if (keys.current.blackFridayStateKey) {
-            setModalCookie(keys.current.blackFridayStateKey, '1');
-        }
     };
 
     return [state, loading, onChange] as const;
