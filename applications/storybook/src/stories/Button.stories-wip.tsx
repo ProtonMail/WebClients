14744607f36580138556ae87092d3a5fecd3e61a
matthiasmargot <margotmatthias@protonmail.com>
2020-11-27 11:49:39 +0100

    A bit of cleanup

--- applications/storybook/src/stories/Button.stories-wip.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { PrimaryButton, Button } from 'react-components';
-
-export const basic = () => <Button>Button</Button>;
-
-export const primary = () => <PrimaryButton>Button</PrimaryButton>;
