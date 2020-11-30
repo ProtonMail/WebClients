df78bcc10e1a22087f710fe68466f78c287c7f43
Richard <richard@protonmail.com>
2020-11-30 18:47:47 +0000

    Add Copy component

---
+++ applications/storybook/src/stories/Copy.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Copy } from 'react-components';
+
+import mdx from './Copy.mdx';
+
+export default {
+    component: Copy,
+    title: 'Components / Copy',
+    parameters: {
+        docs: {
+            page: mdx,
+        },
+    },
+};
+
+export const Basic = () => <Copy value="Pikachu" onCopy={() => alert(`"Pikachu" copied to clipboard`)} />;
