a52c85d00b26b460f360553d89ea90f9dc288a1f
Richard <richard@protonmail.com>
2020-11-30 15:57:23 +0100

    Review button page

--- applications/storybook/src/stories/Button.stories.tsx
+++ applications/storybook/src/stories/Button.stories.tsx
@@ -1,10 +1,63 @@
-import React from 'react';
-import { PrimaryButton, Button, ErrorButton, LinkButton } from 'react-components';
+import React, { useState } from 'react';
+import {
+    PrimaryButton,
+    Button,
+    ErrorButton,
+    WarningButton,
+    LinkButton,
+    LargeButton,
+    SmallButton,
+    Group,
+    ButtonGroup,
+} from 'react-components';
 
-export default { component: Button, title: 'Proton UI / Button' };
+import mdx from './Button.mdx';
 
-export const Basic = () => <Button>Button</Button>;
-export const LoadingButton = () => <Button loading>Button</Button>;
-export const Primary = () => <PrimaryButton>Button</PrimaryButton>;
-export const Error = () => <ErrorButton>Button</ErrorButton>;
-export const Link = () => <LinkButton>Button link</LinkButton>;
+export default {
+    component: Button,
+    title: 'Components / Button',
+    parameters: {
+        docs: {
+            page: mdx,
+        },
+    },
+};
+
+export const Basic = () => (
+    <>
+        <Button className="mr1">Button</Button>
+        <Button className="pm-button--primaryborder mr1">Button</Button>
+        <Button className="pm-button--pill mr1">Button</Button>
+        <PrimaryButton className="mr1">Button</PrimaryButton>
+        <ErrorButton className="mr1">Button</ErrorButton>
+        <WarningButton className="mr1">Button</WarningButton>
+        <LinkButton>Button link</LinkButton>
+    </>
+);
+export const Size = () => (
+    <>
+        <SmallButton className="mr1">Small</SmallButton>
+        <LargeButton>Large</LargeButton>
+    </>
+);
+export const Grouped = () => {
+    const [pokemon, setPokemon] = useState('Bulbasaur');
+    return (
+        <Group>
+            <ButtonGroup className={pokemon === 'Bulbasaur' ? 'is-active' : ''} onClick={() => setPokemon('Bulbasaur')}>
+                Bulbasaur
+            </ButtonGroup>
+            <ButtonGroup
+                className={pokemon === 'Charmander' ? 'is-active' : ''}
+                onClick={() => setPokemon('Charmander')}
+            >
+                Charmander
+            </ButtonGroup>
+            <ButtonGroup className={pokemon === 'Squirtle' ? 'is-active' : ''} onClick={() => setPokemon('Squirtle')}>
+                Squirtle
+            </ButtonGroup>
+        </Group>
+    );
+};
+export const Disabled = () => <Button disabled>Button</Button>;
+export const Loading = () => <Button loading>Button</Button>;
