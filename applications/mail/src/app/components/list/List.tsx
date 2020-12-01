af17875a1741f19b0687d595860e273de603df63
Nicolas Hoffmann <dante3333@gmail.com>
2020-12-01 18:42:32 +0100

    hotfix - l18n improvement for MAILWEB-1660

--- applications/mail/src/app/components/list/List.tsx
+++ applications/mail/src/app/components/list/List.tsx
@@ -193,7 +193,9 @@ const List = ({
                 isCompactView && 'is-compact',
             ])}
         >
-            <h1 className="sr-only">{c('Title').t`Email list`}</h1>
+            <h1 className="sr-only">
+                {conversationMode ? c('Title').t`Conversation list` : c('Title').t`Message list`}
+            </h1>
             <div className="items-column-list-inner flex flex-nowrap flex-column">
                 {expectedLength === 0 ? (
                     <EmptyView labelID={labelID} isSearch={isSearch} />
