57a3d085c02e4a318abf5e010070385c0cf19279
matthiasmargot <margotmatthias@protonmail.com>
2020-12-09 14:50:19 +0100

    Add Scale stories

---
+++ applications/storybook/src/stories/Scale.stories.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Scale } from 'react-components';
+
+import mdx from './Scale.mdx';
+
+export default {
+    component: Scale,
+    title: 'Components / Scale',
+    parameters: {
+        docs: {
+            page: mdx,
+        },
+    },
+};
+
+export const Basic = () => {
+    const [value, setValue] = useState<number>();
+
+    const handleChange = (v: number) => {
+        setValue(v);
+    };
+
+    return (
+        <Scale
+            from={0}
+            to={10}
+            fromLabel="0 - Not at all likely"
+            toLabel="10 - Extremely likely"
+            value={value}
+            onChange={handleChange}
+        />
+    );
+};
