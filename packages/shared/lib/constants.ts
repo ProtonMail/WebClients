16e9e48384289feeb93e46718c9465582bb2225a
Richard <richard@protonmail.com>
2020-12-17 15:38:46 +0100

    Add MONTH constant

--- packages/shared/lib/constants.ts
+++ packages/shared/lib/constants.ts
@@ -8,6 +8,7 @@ export const MINUTE = 60 * SECOND;
 export const HOUR = 60 * MINUTE;
 export const DAY = 24 * HOUR;
 export const WEEK = 7 * DAY;
+export const MONTH = 4 * WEEK;
 
 // Max quantity for all addons
 export const MAX_VPN_ADDON = 496;
