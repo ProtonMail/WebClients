755f8105076d22d0c5aae7c4805f7d5015f14cbe
Zlatko Petreski <petreskizlatko0@protonmail.com>
2020-12-22 15:32:57 +0100

    remove tooltip and change url for android tv download section

--- packages/components/containers/vpn/ProtonVPNClientsSection/VPNClientCard.tsx
+++ packages/components/containers/vpn/ProtonVPNClientsSection/VPNClientCard.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import { c } from 'ttag';
-import { Bordered, Icon, Block, Href, Info } from '../../../components';
+import { Bordered, Icon, Block, Href } from '../../../components';
 
 interface Props {
     title: string;
     link: string;
     icon: string;
-    tooltip?: string;
 }
 
-const VPNClientCard = ({ title, link, icon, tooltip }: Props) => {
+const VPNClientCard = ({ title, link, icon }: Props) => {
     return (
         <Bordered className="mr1 aligncenter relative">
             <div>
                 <Icon size={24} name={icon} />
             </div>
-            <div className="flex flex-justify-center">
-                <Block>{title}</Block>
-                {tooltip ? <Info buttonClass="ml0-25 mb1 relative z667" title={tooltip} /> : null}
-            </div>
+            <Block>{title}</Block>
             <Href url={link} className="pm-button increase-surface-click">
                 {c('Action').t`Download`}
                 <span className="sr-only">{title}</span>
