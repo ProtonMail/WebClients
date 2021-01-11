cf856c62ce2f8938eb9e969ca4ff2eb3defa9b2a
mmso <mmso@mmso.se>
2021-01-11 11:41:17 +0100

    Update forbidden text

--- packages/shared/lib/authlog/i18n.ts
+++ packages/shared/lib/authlog/i18n.ts
@@ -14,7 +14,7 @@ export const getAuthLogEventsI18N = (type: AUTH_LOG_EVENTS) => {
         case AUTH_LOG_EVENTS.LOGIN_SUCCESS_AWAIT_2FA:
             return c('Log event').t`Sign in success (awaiting second factor)`;
         case AUTH_LOG_EVENTS.LOGIN_SUCCESS_FORBIDDEN:
-            return c('Log event').t`Sign in success (forbidden)`;
+            return c('Log event').t`Sign in success (forbidden due to abuse)`;
 
         case AUTH_LOG_EVENTS.REAUTH_FAILURE_PASSWORD:
             return c('Log event').t`Authentication failure (wrong password)`;
