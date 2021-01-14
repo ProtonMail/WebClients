ef29d807083e00e1c12b94c033c475d3af12c9fa
Vincas Stonys <vincaslt@gmail.com>
2021-01-14 12:16:27 +0200

    Change zero-access -> end-to-end encryption

--- applications/drive/src/app/components/layout/DriveSidebar/DriveSidebarFooter.tsx
+++ applications/drive/src/app/components/layout/DriveSidebar/DriveSidebarFooter.tsx
@@ -8,7 +8,7 @@ const DriveSidebarFooter = () => (
     <>
         <div className="aligncenter opacity-50 mr4 ml4">
             <Icon name="lock-check" size={20} />
-            <div className="small m0">{c('Label').t`Zero-Access Encryption by Proton`}</div>
+            <div className="small m0">{c('Label').jt`End-to-End Encryption by Proton`}</div>
             <hr className="opacity-30 m0-25" />
         </div>
         <AppVersion changelog={changelog} />
