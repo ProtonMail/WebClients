0d47ff31a06d2de8b34f06fd47f2397e66013487
Jeremy Benaim <jeremybenaim@gmail.com>
2021-01-24 16:33:45 +0100

    [MAILWEB-1133] - Update CustomFilter dropdown

--- applications/mail/src/app/components/dropdown/CustomFilterDropdown.tsx
+++ applications/mail/src/app/components/dropdown/CustomFilterDropdown.tsx
@@ -8,51 +8,48 @@ import {
     useUser,
     useFilters,
     useNotifications,
+    FilterUtils,
+    FilterConstants,
 } from 'react-components';
 import { c } from 'ttag';
 
-import { newFilter } from 'proton-shared/lib/filters/factory';
-import { computeTree } from 'proton-shared/lib/filters/sieve';
 import { identity } from 'proton-shared/lib/helpers/function';
 import { isPaid } from 'proton-shared/lib/user/helpers';
-import { Filter } from 'proton-shared/lib/filters/interfaces';
 import { FILTER_STATUS } from 'proton-shared/lib/constants';
 
-enum AVAILABLE_FILTERS {
-    SUBJECT = 'subject',
-    SENDER = 'sender',
-    RECIPIENT = 'recipient',
-    ATTACHMENTS = 'attachments',
-}
+import { Filter, ConditionType, ConditionComparator } from 'react-components/containers/filters/interfaces';
+
+const { computeTree, newFilter } = FilterUtils;
+const { OPERATORS } = FilterConstants;
 
 type FiltersState = {
-    [key in AVAILABLE_FILTERS]: boolean;
+    [key in ConditionType]: boolean;
 };
 
 type FilterType = {
     label: string;
-    value: AVAILABLE_FILTERS;
+    value: ConditionType;
     conditionLabel: string;
 };
 
 const FILTER_TYPES: FilterType[] = [
     {
-        value: AVAILABLE_FILTERS.SUBJECT,
+        value: ConditionType.SUBJECT,
         label: c('CustomFilter').t`Subject`,
         conditionLabel: c('Filter modal type').t`If the subject`,
     },
     {
-        value: AVAILABLE_FILTERS.SENDER,
+        value: ConditionType.SENDER,
         label: c('CustomFilter').t`Sender`,
         conditionLabel: c('Filter modal type').t`If the sender`,
     },
     {
-        value: AVAILABLE_FILTERS.RECIPIENT,
+        value: ConditionType.RECIPIENT,
         label: c('CustomFilter').t`Recipient`,
         conditionLabel: c('Filter modal type').t`If the recipient`,
     },
     {
-        value: AVAILABLE_FILTERS.ATTACHMENTS,
+        value: ConditionType.ATTACHMENTS,
         label: c('CustomFilter').t`Attachments`,
         conditionLabel: c('Filter modal type').t`If the attachments`,
     },
@@ -65,55 +62,54 @@ interface Props {
 
 const CustomFilterDropdown = ({ message, onClose }: Props) => {
     const [filtersState, setFiltersState] = useState<FiltersState>({
-        [AVAILABLE_FILTERS.SUBJECT]: false,
-        [AVAILABLE_FILTERS.SENDER]: false,
-        [AVAILABLE_FILTERS.RECIPIENT]: false,
-        [AVAILABLE_FILTERS.ATTACHMENTS]: false,
+        [ConditionType.SELECT]: false,
+        [ConditionType.SUBJECT]: false,
+        [ConditionType.SENDER]: false,
+        [ConditionType.RECIPIENT]: false,
+        [ConditionType.ATTACHMENTS]: false,
     });
     const { createModal } = useModals();
     const { createNotification } = useNotifications();
     const [user] = useUser();
     const [filters = []] = useFilters() as [Filter[], boolean, Error];
 
-    const toggleFilterType = (filterType: AVAILABLE_FILTERS) => {
+    const toggleFilterType = (filterType: ConditionType) => {
         setFiltersState({
             ...filtersState,
             [filterType]: !filtersState[filterType],
         });
     };
 
-    const formatConditions = (conditions: string[]) => {
+    const formatConditions = (conditions: ConditionType[]) => {
         return conditions.map((condition) => {
-            const filterType = FILTER_TYPES.find((f) => f.value === condition);
+            const filterType = FILTER_TYPES.find((f) => f.value === condition) as FilterType;
             let value;
 
             switch (condition) {
-                case AVAILABLE_FILTERS.SUBJECT:
+                case ConditionType.SUBJECT:
                     value = message.Subject;
                     break;
-                case AVAILABLE_FILTERS.SENDER:
+                case ConditionType.SENDER:
                     value = message.Sender ? message.Sender.Address : '';
                     break;
-                case AVAILABLE_FILTERS.RECIPIENT:
+                case ConditionType.RECIPIENT:
                     value = message.ToList && message.ToList.length ? message.ToList[0].Address : '';
                     break;
-                case AVAILABLE_FILTERS.ATTACHMENTS:
+                case ConditionType.ATTACHMENTS:
                 default:
                     value = '';
                     break;
             }
 
-            return (
-                filterType && {
-                    value,
-                    Values: [value],
-                    Type: {
-                        label: filterType.conditionLabel,
-                        value: filterType.value,
-                    },
-                    Comparator: { label: 'contains', value: 'contains' },
-                }
-            );
+            return {
+                value,
+                Values: [value],
+                Type: {
+                    label: filterType?.conditionLabel,
+                    value: filterType?.value,
+                },
+                Comparator: { label: 'contains', value: ConditionComparator.CONTAINS },
+            };
         });
     };
 
@@ -129,7 +125,7 @@ const CustomFilterDropdown = ({ message, onClose }: Props) => {
 
         const filter = newFilter();
         const conditions = [];
-        let filterType: AVAILABLE_FILTERS;
+        let filterType: ConditionType;
 
         for (filterType in filtersState) {
             if (filtersState[filterType]) {
@@ -137,10 +133,27 @@ const CustomFilterDropdown = ({ message, onClose }: Props) => {
             }
         }
 
-        filter.Simple.Conditions = formatConditions(conditions);
-        filter.Tree = computeTree(filter);
+        filter.Simple = {
+            Operator: {
+                label: OPERATORS[0].label,
+                value: OPERATORS[0].value,
+            },
+            Conditions: formatConditions(conditions),
+            Actions: {
+                FileInto: [],
+                Vacation: '',
+                Mark: { Read: false, Starred: false },
+            },
+        };
 
-        createModal(<AddFilterModal filter={filter} />);
+        createModal(
+            <AddFilterModal
+                filter={{
+                    ...filter,
+                    Tree: computeTree(filter),
+                }}
+            />
+        );
     };
 
     const buttonDisabled = !Object.values(filtersState).some(identity);
