266d84eb0a0aab852559bd8245c8cf3976211fc2
Matthieu Lux <matthieu.lux@protonmail.com>
2021-01-28 16:17:30 +0000

    [MAILWEB-1569] Add a link on the contact group label

--- packages/components/components/contacts/ContactGroupLabels.tsx
+++ packages/components/components/contacts/ContactGroupLabels.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
+import { APPS } from 'proton-shared/lib/constants';
 import { ContactGroup } from 'proton-shared/lib/interfaces/contacts/Contact';
 import LabelStack, { LabelDescription } from '../labelStack/LabelStack';
+import { useAppLink } from '../link';
 
 interface Props {
     contactGroups: ContactGroup[];
 }
 
 const ContactGroupLabels = ({ contactGroups }: Props) => {
+    const appLink = useAppLink();
+
     const labels = contactGroups.reduce((acc: LabelDescription[], contactGroup: ContactGroup) => {
         return contactGroup
             ? [
@@ -15,6 +19,10 @@ const ContactGroupLabels = ({ contactGroups }: Props) => {
                       name: contactGroup.Name,
                       color: contactGroup.Color,
                       title: contactGroup.Name,
+                      onClick: (event: MouseEvent) => {
+                          appLink(`/?contactGroupID=${contactGroup.ID}`, APPS.PROTONCONTACTS);
+                          event.stopPropagation();
+                      },
                   },
               ]
             : acc;
