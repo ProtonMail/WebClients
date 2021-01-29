de5334bd9ffdc6cde40f0dca6bbaab801df24fde
matthiasmargot <margotmatthias@protonmail.com>
2021-01-29 19:23:33 +0100

    Control ordering of stories in storybook so that the introduction page will be rendered first when accessign the base url

--- applications/storybook/.storybook/preview.js
+++ applications/storybook/.storybook/preview.js
@@ -34,12 +34,26 @@ export const decorators = [
     ),
 ];
 
+const order = ['introduction-', 'changelog-', 'components-', 'css-', 'design-system-', 'proton-ui-'];
+
+const priority = ['introduction-', 'changelog-'];
+
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: { expanded: true },
     docs: { theme: theme },
     options: {
-        storySort: (a, b) =>
-            a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+        storySort: (a, b) => {
+            const aName = a[0];
+            const bName = b[0];
+
+            if (priority.some((name) => aName.includes(name) || bName.includes(name))) {
+                const aIdx = order.findIndex((i) => aName.indexOf(i) > -1);
+                const bIdx = order.findIndex((i) => bName.indexOf(i) > -1);
+                return aIdx - bIdx;
+            }
+
+            return a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true });
+        },
     },
 };
