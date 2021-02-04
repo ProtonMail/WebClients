05f84a2c0b359725a35c4cf29d3369786882490f
Zlatko Petreski <petreskizlatko0@protonmail.com>
2021-02-04 09:27:50 +0000

    [VPNFE-134]Update the plan-summary in the Dashboard

--- packages/components/containers/payments/subscription/SubscriptionFeatureRow.js
+++ packages/components/containers/payments/subscription/SubscriptionFeatureRow.js
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon, Info } from '../../../components';
 
-const SubscriptionFeatureRow = ({ icon, feature, info }) => {
+const SubscriptionFeatureRow = ({ icon, feature, info, url }) => {
     return (
         <span className="flex flex-nowrap">
             <Icon name={icon} className="mt0-25 mr1 flex-item-noshrink" />
             {feature}
-            {info ? <Info title={info} /> : null}
+            {info ? <Info className="ml0-5 mt0-25" title={info} url={url} /> : null}
         </span>
     );
 };
@@ -16,6 +16,7 @@ SubscriptionFeatureRow.propTypes = {
     icon: PropTypes.string.isRequired,
     feature: PropTypes.node.isRequired,
     info: PropTypes.string,
+    url: PropTypes.string,
 };
 
 export default SubscriptionFeatureRow;
