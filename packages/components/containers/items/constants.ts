7a0ff3fd5d96654ea2aed5fee8050593655a9f64
Matthieu Lux <matthieu.lux@protonmail.com>
2021-02-16 16:08:54 +0000

    [MAILWEB-1215] Create a generic items selection and drag hooks

---
+++ packages/components/containers/items/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * ID used in the drag DataTransfer object to store items ids currently dragged
+ */
+export const DRAG_ITEM_KEY = 'drag-item';
+
+/**
+ * ID used in the drag DataTransfer object to store the HTML id of the dragged element
+ */
+export const DRAG_ITEM_ID_KEY = 'drag-item-id';
