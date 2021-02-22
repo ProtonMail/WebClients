3483d032253354ccb2ef2c7b6247eeb8a4c24167
Matthieu Bué <matthieu.bue@gmail.com>
2021-02-22 21:00:38 +0100

    WIP Buttons for theming
    Co-authored-by: Matthias Margot <MatthiasMargot@users.noreply.github.com>

--- applications/storybook/src/stories/components/Button.stories.tsx
+++ applications/storybook/src/stories/components/Button.stories.tsx
@@ -15,8 +15,7 @@ import {
     TableRow,
     TableCell,
     TableBody,
-    SelectTwo,
-    Option,
+    RadioGroup,
 } from 'react-components';
 
 import mdx from './Button.mdx';
@@ -31,75 +30,66 @@ export default {
     },
 };
 
-export const Primary = ({ ...args }) => <Button {...args}>Loremium</Button>;
+export const Basic = ({ ...args }) => <Button {...args}>Loremium</Button>;
 
-Primary.args = {};
-
-export const Basic = () => (
-    <>
-        <Button className="mr1">Button</Button>
-        <Button className="button--primaryborder mr1">Button</Button>
-        <Button className="button--pill mr1">Button</Button>
-        <PrimaryButton className="mr1">Button</PrimaryButton>
-        <ErrorButton className="mr1">Button</ErrorButton>
-        <WarningButton className="mr1">Button</WarningButton>
-        <LinkButton>Button link</LinkButton>
-    </>
-);
+Basic.args = {};
 
 const shapes: Required<ButtonProps>['shape'][] = ['solid', 'outline', 'ghost'];
 
 const colors: Required<ButtonProps>['color'][] = ['norm', 'weak', 'danger', 'warning', 'success', 'info'];
 
-const buttonContainerClassName =
-    'flex flex-item-fluid flex-align-items-center flex-justify-center bg-global-light ml2 rounded';
+const sizes: Required<ButtonProps>['size'][] = ['small', 'medium', 'large'];
 
-export const New = () => {
+const buttonContainerClassName = 'flex flex-item-fluid flex-align-items-center flex-justify-center bordered-container';
+
+export const Sandbox = () => {
     const [selectedShape, setSelectedShape] = useState<Required<ButtonProps>['shape']>('solid');
-    const [selectedColor, setSelectedColor] = useState<Required<ButtonProps>['color']>('norm');
+    const [selectedColor, setSelectedColor] = useState<Required<ButtonProps>['color']>('weak');
+    const [selectedSize, setSelectedSize] = useState<Required<ButtonProps>['size']>('medium');
 
     const button = (
-        <Button shape={selectedShape} color={selectedColor}>
-            {selectedShape} {selectedColor}
+        <Button shape={selectedShape} color={selectedColor} size={selectedSize}>
+            {selectedShape} {selectedColor} {selectedSize}
         </Button>
     );
 
     return (
-        <div className="flex p2">
-            <div className="w40">
-                <label htmlFor="selected-shape" className="block mb2">
-                    Shape
-                    <SelectTwo
-                        id="selected-shape"
-                        onChange={({ value }) => setSelectedShape(value)}
-                        value={selectedShape}
-                        className="mt0-5"
-                    >
-                        {shapes.map((shape) => (
-                            <Option value={shape} title={shape} />
-                        ))}
-                    </SelectTwo>
-                </label>
-                <label htmlFor="selected-color">
-                    Color
-                    <SelectTwo
-                        id="selected-color"
-                        onChange={({ value }) => setSelectedColor(value)}
+        <div className="p2">
+            <div className="flex flex-align-items-stretch">
+                <div className="mr2">
+                    <strong className="block mb1">Color</strong>
+                    <RadioGroup
+                        name="selected-color"
+                        onChange={(v) => setSelectedColor(v)}
                         value={selectedColor}
-                        className="mt0-5"
-                    >
-                        {colors.map((color) => (
-                            <Option value={color} title={color} />
-                        ))}
-                    </SelectTwo>
-                </label>
+                        options={colors.map((color) => ({ value: color, label: color }))}
+                    />
+                </div>
+                <div className="mr2">
+                    <strong className="block mb1">Shape</strong>
+                    <RadioGroup
+                        name="selected-shape"
+                        onChange={(v) => setSelectedShape(v)}
+                        value={selectedShape}
+                        options={shapes.map((shape) => ({ value: shape, label: shape }))}
+                    />
+                </div>
+                <div>
+                    <strong className="block mb1">Size</strong>
+                    <RadioGroup
+                        name="selected-size"
+                        onChange={(v) => setSelectedSize(v)}
+                        value={selectedSize}
+                        options={sizes.map((size) => ({ value: size, label: size }))}
+                    />
+                </div>
+                <div className={buttonContainerClassName}>{button}</div>
             </div>
-            <div className={buttonContainerClassName}>{button}</div>
         </div>
     );
 };
 
-export const Compositions = () => {
+export const Variants = () => {
     return (
         <Table>
             <TableHeader>
@@ -130,34 +120,53 @@ export const Compositions = () => {
     );
 };
 
-export const Size = () => (
-    <>
-        <SmallButton className="mr1">Small</SmallButton>
-        <Button className="mr1">Normal</Button>
-        <LargeButton>Large</LargeButton>
-    </>
-);
-
-export const Grouped = () => {
+export const Legacy = () => {
     const [pokemon, setPokemon] = useState('Bulbasaur');
+
     return (
-        <Group>
-            <ButtonGroup className={pokemon === 'Bulbasaur' ? 'is-active' : ''} onClick={() => setPokemon('Bulbasaur')}>
-                Bulbasaur
-            </ButtonGroup>
-            <ButtonGroup
-                className={pokemon === 'Charmander' ? 'is-active' : ''}
-                onClick={() => setPokemon('Charmander')}
-            >
-                Charmander
-            </ButtonGroup>
-            <ButtonGroup className={pokemon === 'Squirtle' ? 'is-active' : ''} onClick={() => setPokemon('Squirtle')}>
-                Squirtle
-            </ButtonGroup>
-        </Group>
+        <div>
+            <div className="mb2">
+                <Button className="mr1">Button</Button>
+                <Button className="button--primaryborder mr1">Button</Button>
+                <Button className="button--pill mr1">Button</Button>
+                <PrimaryButton className="mr1">Button</PrimaryButton>
+                <ErrorButton className="mr1">Button</ErrorButton>
+                <WarningButton className="mr1">Button</WarningButton>
+                <LinkButton>Button link</LinkButton>
+            </div>
+            <div className="mb2">
+                <SmallButton className="mr1">Small</SmallButton>
+                <Button className="mr1">Normal</Button>
+                <LargeButton>Large</LargeButton>
+            </div>
+            <div className="mb2">
+                <Group>
+                    <ButtonGroup
+                        className={pokemon === 'Bulbasaur' ? 'is-active' : ''}
+                        onClick={() => setPokemon('Bulbasaur')}
+                    >
+                        Bulbasaur
+                    </ButtonGroup>
+                    <ButtonGroup
+                        className={pokemon === 'Charmander' ? 'is-active' : ''}
+                        onClick={() => setPokemon('Charmander')}
+                    >
+                        Charmander
+                    </ButtonGroup>
+                    <ButtonGroup
+                        className={pokemon === 'Squirtle' ? 'is-active' : ''}
+                        onClick={() => setPokemon('Squirtle')}
+                    >
+                        Squirtle
+                    </ButtonGroup>
+                </Group>
+            </div>
+            <div className="mb2">
+                <Button disabled>Button</Button>
+            </div>
+            <div>
+                <Button loading>Button</Button>
+            </div>
+        </div>
     );
 };
-
-export const Disabled = () => <Button disabled>Button</Button>;
-
-export const Loading = () => <Button loading>Button</Button>;
