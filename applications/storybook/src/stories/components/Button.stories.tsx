d571eff170a8abf53a71c4114f3cff6817a3cd37
Matthias Margot <margotmatthias@protonmail.com>
2021-02-22 17:18:48 +0100

    Add back the Button stories

--- applications/storybook/src/stories/components/Button.stories.tsx
+++ applications/storybook/src/stories/components/Button.stories.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {
     PrimaryButton,
     Button,
+    ButtonProps,
     ErrorButton,
     WarningButton,
     LinkButton,
@@ -9,6 +10,13 @@ import {
     SmallButton,
     Group,
     ButtonGroup,
+    Table,
+    TableHeader,
+    TableRow,
+    TableCell,
+    TableBody,
+    SelectTwo,
+    Option,
 } from 'react-components';
 
 import mdx from './Button.mdx';
@@ -23,6 +31,10 @@ export default {
     },
 };
 
+export const Primary = ({ ...args }) => <Button {...args}>Loremium</Button>;
+
+Primary.args = {};
+
 export const Basic = () => (
     <>
         <Button className="mr1">Button</Button>
@@ -34,6 +46,90 @@ export const Basic = () => (
         <LinkButton>Button link</LinkButton>
     </>
 );
+
+const shapes: Required<ButtonProps>['shape'][] = ['solid', 'outline', 'ghost'];
+
+const colors: Required<ButtonProps>['color'][] = ['norm', 'weak', 'danger', 'warning', 'success', 'info'];
+
+const buttonContainerClassName =
+    'flex flex-item-fluid flex-align-items-center flex-justify-center bg-global-light ml2 rounded';
+
+export const New = () => {
+    const [selectedShape, setSelectedShape] = useState<Required<ButtonProps>['shape']>('solid');
+    const [selectedColor, setSelectedColor] = useState<Required<ButtonProps>['color']>('norm');
+
+    const button = (
+        <Button shape={selectedShape} color={selectedColor}>
+            {selectedShape} {selectedColor}
+        </Button>
+    );
+
+    return (
+        <div className="flex p2">
+            <div className="w40">
+                <label htmlFor="selected-shape" className="block mb2">
+                    Shape
+                    <SelectTwo
+                        id="selected-shape"
+                        onChange={({ value }) => setSelectedShape(value)}
+                        value={selectedShape}
+                        className="mt0-5"
+                    >
+                        {shapes.map((shape) => (
+                            <Option value={shape} title={shape} />
+                        ))}
+                    </SelectTwo>
+                </label>
+                <label htmlFor="selected-color">
+                    Color
+                    <SelectTwo
+                        id="selected-color"
+                        onChange={({ value }) => setSelectedColor(value)}
+                        value={selectedColor}
+                        className="mt0-5"
+                    >
+                        {colors.map((color) => (
+                            <Option value={color} title={color} />
+                        ))}
+                    </SelectTwo>
+                </label>
+            </div>
+            <div className={buttonContainerClassName}>{button}</div>
+        </div>
+    );
+};
+
+export const Compositions = () => {
+    return (
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableCell>
+                        <></>
+                    </TableCell>
+                    {colors.map((color) => (
+                        <TableCell scope="col">{color}</TableCell>
+                    ))}
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {shapes.map((shape) => (
+                    <TableRow>
+                        <TableCell>{shape}</TableCell>
+                        {colors.map((color) => (
+                            <TableCell>
+                                <Button shape={shape} color={color}>
+                                    Loremium
+                                </Button>
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    );
+};
+
 export const Size = () => (
     <>
         <SmallButton className="mr1">Small</SmallButton>
@@ -41,6 +137,7 @@ export const Size = () => (
         <LargeButton>Large</LargeButton>
     </>
 );
+
 export const Grouped = () => {
     const [pokemon, setPokemon] = useState('Bulbasaur');
     return (
@@ -60,5 +157,7 @@ export const Grouped = () => {
         </Group>
     );
 };
+
 export const Disabled = () => <Button disabled>Button</Button>;
+
 export const Loading = () => <Button loading>Button</Button>;
