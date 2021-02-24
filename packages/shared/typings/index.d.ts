b5e02c714c4308795eeca97dc07efbe7f48c30f9
Mattias Svanström <mmso@mmso.se>
2021-02-24 16:43:15 +0000

    Ignore web-streams type

--- packages/shared/typings/index.d.ts
+++ packages/shared/typings/index.d.ts
@@ -5,3 +5,7 @@ declare module 'pm-srp';
 declare module 'is-valid-domain';
 
 declare module 'mimemessage';
+
+type UnderlyingByteSource = undefined;
+type ReadableStreamBYOBReader = undefined;
+declare module '@mattiasbuelens/web-streams-adapter';
