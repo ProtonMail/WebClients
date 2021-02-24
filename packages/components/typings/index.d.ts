5699b456c953a3e8ffc731019ba639ffca5c3666
mmso <mmso@mmso.se>
2021-02-24 15:18:54 +0100

    ResizeObserver now default

--- packages/components/typings/index.d.ts
+++ packages/components/typings/index.d.ts
@@ -12,30 +12,3 @@ declare module 'squire-rte';
 declare module 'pm-srp';
 
 declare module 'is-valid-domain';
-
-declare class ResizeObserver {
-    constructor(callback: ResizeObserverCallback);
-    disconnect: () => void;
-
-    observe: (target: Element, options?: ResizeObserverOptions) => void;
-
-    unobserve: (target: Element) => void;
-}
-
-type ResizeObserverOptions = {
-    box?: 'content-box' | 'border-box';
-};
-
-type ResizeObserverCallback = (entries: ResizeObserverEntry[], observer: ResizeObserver) => void;
-
-interface ResizeObserverEntry {
-    readonly borderBoxSize: ResizeObserverEntryBoxSize;
-    readonly contentBoxSize: ResizeObserverEntryBoxSize;
-    readonly contentRect: DOMRectReadOnly;
-    readonly target: Element;
-}
-
-interface ResizeObserverEntryBoxSize {
-    blockSize: number;
-    inlineSize: number;
-}
