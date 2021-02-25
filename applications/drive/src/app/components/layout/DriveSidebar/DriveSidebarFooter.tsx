ef0e34a95f914f72ecd01fc1ac0a5fec5d781238
Mantvidas M <mantvidasm@Mantvidass-MacBook-Pro.local>
2021-02-25 12:30:37 +0200

    E2EE label removed from sidebar footer

--- applications/drive/src/app/components/layout/DriveSidebar/DriveSidebarFooter.tsx
+++ applications/drive/src/app/components/layout/DriveSidebar/DriveSidebarFooter.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import { c } from 'ttag';
-import { AppVersion, Icon } from 'react-components';
+import { AppVersion } from 'react-components';
 
 import changelog from '../../../../../CHANGELOG.md';
 
-const DriveSidebarFooter = () => (
-    <>
-        <div className="text-center opacity-50 mr4 ml4">
-            <Icon name="lock-check" size={20} />
-            <div className="text-sm m0">{c('Label').t`End-to-end Encryption by Proton`}</div>
-            <hr className="opacity-30 m0-25" />
-        </div>
-        <AppVersion changelog={changelog} />
-    </>
-);
+const DriveSidebarFooter = () => <AppVersion changelog={changelog} />;
 
 export default DriveSidebarFooter;
