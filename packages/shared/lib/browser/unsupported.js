032903d61b139b11d135c71e85f23bed77333863
mmso <mmso@mmso.se>
2021-02-25 23:14:24 +0100

    Fix updated class names

--- packages/shared/lib/browser/unsupported.js
+++ packages/shared/lib/browser/unsupported.js
@@ -8,15 +8,15 @@ const showUnsupported = () => {
         : 'https://protonmail.com/support/knowledge-base/browsers-supported/';
 
     document.body.innerHTML = `
-        <div class="w50 p2 mt2 center big automobile">
-            <div class="aligncenter">
+        <div class="w50 p2 mt2 center big automobile text-center">
+            <div>
                 <h1>Unsupported browser</h1>
                 <p>
                     You are using an unsupported browser. Please update it to the latest version or use a different browser.
                 </p>
                 <a class="primary-link bold" target="_blank" rel="noopener noreferrer" href="${kbUrl}">More info</a>
             </div>
-            <div class="mt2 aligncenter">
+            <div class="mt2">
                 <img src=${unsupportedBrowser} alt="Unsupported browser"/>
             </div>
         </div>
