37c83cf77e5d446f20ef3cd50777e16fb3a0ca46
mmso <mmso@mmso.se>
2021-03-07 10:19:51 +0100

    Allow number or string

--- packages/shared/lib/helpers/card.ts
+++ packages/shared/lib/helpers/card.ts
@@ -1,7 +1,10 @@
 /**
  * Check if card is expired at the end of current month
  */
-export const isExpired = ({ ExpMonth = 12, ExpYear = 9999 }: { ExpMonth?: number; ExpYear?: number } = {}): boolean => {
+export const isExpired = ({
+    ExpMonth = 12,
+    ExpYear = 9999,
+}: { ExpMonth?: number | string; ExpYear?: number | string } = {}): boolean => {
     const currentTime = new Date();
     const currentMonth = currentTime.getMonth() + 1;
     const currentYear = currentTime.getFullYear();
