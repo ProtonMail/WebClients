50ffaa80c7fd8b50cd70ecebee641a6a52355b98
Richard <richard@protonmail.com>
2021-03-11 10:00:22 +0100

    Format image in contact editor

--- packages/components/components/contacts/ContactImageField.tsx
+++ packages/components/components/contacts/ContactImageField.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { c } from 'ttag';
+import { formatImage } from 'proton-shared/lib/helpers/image';
 
 import { Button } from '../button';
 
@@ -12,7 +13,7 @@ const ContactImageField = ({ value, onChange }: Props) => {
     return (
         <div>
             {value ? (
-                <img className="max-w13e" src={value} referrerPolicy="no-referrer" />
+                <img className="max-w13e" src={formatImage(value)} referrerPolicy="no-referrer" />
             ) : (
                 <Button onClick={onChange}>{c('Action').t`Upload picture`}</Button>
             )}
