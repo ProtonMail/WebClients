8f32eafb37a49fa0e16c7af2bd3d2135bdb0107a
Redacted <redacted@invalid.proton.ch>
2021-03-15 21:11:16 +0000

    Allow PasswordsSection to pre-open the change-password modal

--- packages/components/containers/account/PasswordsSection.tsx
+++ packages/components/containers/account/PasswordsSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { c } from 'ttag';
 import { SETTINGS_PASSWORD_MODE } from 'proton-shared/lib/interfaces';
 
@@ -7,39 +7,46 @@ import { useAddresses, useModals, useUserSettings } from '../../hooks';
 
 import ChangePasswordModal, { MODES } from './ChangePasswordModal';
 
-const PasswordsSection = () => {
+interface Props {
+    open?: boolean;
+}
+
+const PasswordsSection = ({ open }: Props) => {
     const [userSettings, loadingUserSettings] = useUserSettings();
     const [addresses, loadingAddresses] = useAddresses();
     const { createModal } = useModals();
 
-    if (loadingUserSettings || loadingAddresses) {
+    const isOnePasswordMode = userSettings?.Password?.Mode === SETTINGS_PASSWORD_MODE.ONE_PASSWORD_MODE;
+    const passwordLabel = isOnePasswordMode ? c('Title').t`Password` : c('Title').t`Login password`;
+    const passwordButtonLabel = isOnePasswordMode ? c('Title').t`Change password` : c('Title').t`Change login password`;
+    const changePasswordMode = isOnePasswordMode
+        ? MODES.CHANGE_ONE_PASSWORD_MODE
+        : MODES.CHANGE_TWO_PASSWORD_LOGIN_MODE;
+    const loading = loadingUserSettings || loadingAddresses;
+
+    const handleChangePassword = (mode: MODES) => {
+        createModal(<ChangePasswordModal mode={mode} />, 'change-password');
+    };
+
+    useEffect(() => {
+        if (open && !loading) {
+            handleChangePassword(changePasswordMode);
+        }
+    }, [loading]);
+
+    if (loading) {
         return <Loader />;
     }
 
     // VPN users are by default in two password mode, even if they don't have any addresses. Don't allow them to change two-password mode.
     const hasAddresses = Array.isArray(addresses) && addresses.length > 0;
-    const isOnePasswordMode = userSettings?.Password?.Mode === SETTINGS_PASSWORD_MODE.ONE_PASSWORD_MODE;
-    const passwordLabel = isOnePasswordMode ? c('Title').t`Password` : c('Title').t`Login password`;
-    const passwordButtonLabel = isOnePasswordMode ? c('Title').t`Change password` : c('Title').t`Change login password`;
-
-    const handleChangePassword = (mode: MODES) => {
-        createModal(<ChangePasswordModal mode={mode} />);
-    };
 
     return (
         <>
             <Row>
                 <Label htmlFor="passwordChange">{passwordLabel}</Label>
                 <Field>
-                    <PrimaryButton
-                        onClick={() =>
-                            handleChangePassword(
-                                isOnePasswordMode
-                                    ? MODES.CHANGE_ONE_PASSWORD_MODE
-                                    : MODES.CHANGE_TWO_PASSWORD_LOGIN_MODE
-                            )
-                        }
-                    >
+                    <PrimaryButton onClick={() => handleChangePassword(changePasswordMode)}>
                         {passwordButtonLabel}
                     </PrimaryButton>
                 </Field>
