f8f304d595ff83be4b40bcb0bfb0ea9fbb0bbb97
Matthieu Lux <matthieu.lux@protonmail.com>
2021-03-16 14:23:12 +0000

    [MAILWEB-1861] Reduce max contacts creation per batch

--- packages/shared/lib/constants.ts
+++ packages/shared/lib/constants.ts
@@ -415,7 +415,7 @@ export const ALL_MEMBERS_ID = -100;
 export const API_SAFE_INTERVAL = 100; // API request limit: 100 requests / 10 seconds, so 1 request every 100 ms is safe
 export const QUERY_EXPORT_MAX_PAGESIZE = 50; // in GET API route /contacts/export
 // Manual limit on number of imported contacts to be sent to the API, so that the response does not take too long
-export const ADD_CONTACTS_MAX_SIZE = 100;
+export const ADD_CONTACTS_MAX_SIZE = 10;
 
 export enum LABEL_EXCLUSIVE {
     FOLDER = 1,
