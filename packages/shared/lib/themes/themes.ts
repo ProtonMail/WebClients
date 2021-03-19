0b5f186d39fbcb5f13e7e7397e56ef0e97ac6cb1
Matthieu Bué <matthieu.bue@gmail.com>
2021-03-19 04:16:01 +0100

    Temp select for theming building

--- packages/shared/lib/themes/themes.ts
+++ packages/shared/lib/themes/themes.ts
@@ -4,6 +4,9 @@ import themeTestSvg from 'design-system/assets/img/pm-images/theme-company.svg';
 import themeDarkmodeSvg from 'design-system/assets/img/pm-images/theme-darkmode.svg';
 import defaultTheme from 'design-system/scss/themes/default-theme.scss';
 import darkTheme from 'design-system/scss/themes/dark-theme.scss';
+import lightTheme from 'design-system/scss/themes/light-theme.scss';
+import monokaiTheme from 'design-system/scss/themes/monokai-theme.scss';
+import contrastTheme from 'design-system/scss/themes/contrast-theme.scss';
 
 import { c } from 'ttag';
 
@@ -19,23 +22,40 @@ export const CUSTOM_THEME = {
 export enum ThemeTypes {
     Default = 0,
     Dark = 1,
+    Light = 2,
+    Monokai = 3,
+    Contrast = 4,
 }
 
 export const PROTON_THEMES = {
     DEFAULT: {
-        getI18NLabel() {
-            return c('Theme').t`Default`;
-        },
+        getI18NLabel: () => c('Theme').t`Default`,
         identifier: ThemeTypes.Default,
         src: themeDarkSvg,
         theme: defaultTheme.toString(),
     },
     DARK: {
-        getI18NLabel() {
-            return c('Theme').t`Dark`;
-        },
+        getI18NLabel: () => c('Theme').t`Dark`,
         identifier: ThemeTypes.Dark,
         src: themeDarkmodeSvg,
         theme: darkTheme.toString(),
+    },
+    LIGHT: {
+        getI18NLabel: () => c('Theme').t`Light`,
+        identifier: ThemeTypes.Light,
+        src: themeDarkSvg,
+        theme: lightTheme.toString(),
+    },
+    MONOKAI: {
+        getI18NLabel: () => c('Theme').t`Monokai`,
+        identifier: ThemeTypes.Monokai,
+        src: themeDarkmodeSvg,
+        theme: monokaiTheme.toString(),
+    },
+    CONTRAST: {
+        getI18NLabel: () => c('Theme').t`Contrast`,
+        identifier: ThemeTypes.Contrast,
+        src: themeDarkSvg,
+        theme: contrastTheme.toString(),
     },
 } as const;
