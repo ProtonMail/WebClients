98001a282096d70d74b417ca0f1a9a9b7323cd8b
Richard <richard@protonmail.com>
2021-03-22 20:46:25 +0100

    Hotfix folder icon: parent was used for root folder

--- packages/components/containers/labels/FolderIcon.tsx
+++ packages/components/containers/labels/FolderIcon.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Folder } from 'proton-shared/lib/interfaces/Folder';
+import { FolderWithSubFolders } from 'proton-shared/lib/interfaces/Folder';
 
 import { Icon } from '../../components';
 import { useFolderColor } from '../../hooks';
 import { Props as IconProps } from '../../components/icon/Icon';
 
 interface Props extends Omit<IconProps, 'name'> {
-    folder: Folder;
+    folder: FolderWithSubFolders;
     name?: string;
 }
 
-const getIconName = (isRoot?: boolean, color?: string) => {
-    if (isRoot) {
+const getIconName = (isParent: boolean, color?: string) => {
+    if (isParent) {
         return color ? 'parent-folder-filled' : 'parent-folder';
     }
     return color ? 'folder-filled' : 'folder';
 };
 
 const FolderIcon = ({ folder, name, ...rest }: Props) => {
-    const isRoot = !folder.ParentID;
+    const isParent = !!folder.subfolders?.length;
     const color = useFolderColor(folder);
 
-    return <Icon name={name || getIconName(isRoot, color)} color={color} alt={folder.Name} {...rest} />;
+    return <Icon name={name || getIconName(isParent, color)} color={color} alt={folder.Name} {...rest} />;
 };
 
 export default FolderIcon;
