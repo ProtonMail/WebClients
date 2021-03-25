42d4e5cfc3a514356ac09125133af3a534d1e099
Nico Hoffmann <65-nhoffmann@users.noreply.gitlab.protontech.ch>
2021-03-25 11:54:56 +0000

    Fix - remove SVG CSS sprite for detail/summary tag

--- packages/components/components/container/Details.tsx
+++ packages/components/components/container/Details.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 interface Props extends React.HTMLProps<HTMLDetailsElement> {
     children: React.ReactNode;
+    className?: string;
     open?: boolean;
 }
 
-const Details = ({ children, open = false, ...props }: Props) => (
-    <details open={open} {...props}>
+const Details = ({ children, className, open = false, ...props }: Props, ref: React.Ref<HTMLDetailsElement>) => (
+    <details className={className} open={open} ref={ref} {...props}>
         {children}
     </details>
 );
 
-export default Details;
+export default React.forwardRef<HTMLDetailsElement, Props>(Details);
