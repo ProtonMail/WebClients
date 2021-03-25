fc16ec32433904d09cfce8c8d2e05ea392f7905e
mmso <mmso@mmso.se>
2021-03-25 10:53:13 +0100

    Fix country select row
    CP-1636

--- packages/components/components/v2/phone/CountrySelectRow.tsx
+++ packages/components/components/v2/phone/CountrySelectRow.tsx
@@ -15,24 +15,18 @@ const CountrySelectRow = ({ data, style, value, onChange }: Props, ref?: Ref<HTM
         <div className="dropdown-item" style={style} ref={ref} role="row">
             <DropdownMenuButton
                 isSelected={false}
-                className={classnames(['block w100 text-ellipsis text-left no-outline', data === value && 'active'])}
+                className={classnames([
+                    'block w100 text-ellipsis text-left no-outline flex',
+                    data === value && 'active',
+                ])}
                 title={data.countryName}
                 onClick={() => {
                     onChange(data);
                 }}
             >
-                <div className="flex">
-                    <img
-                        role="presentation"
-                        className="flex-item-noshrink"
-                        alt={data.countryName}
-                        src={data.countryFlag}
-                        width="30"
-                        height="20"
-                    />
-                    <span className="flex-item-fluid pl0-5 text-ellipsis">{data.countryName}</span>
-                    <span className="flex-item-noshrink text-bold">+{data.countryCallingCode}</span>
-                </div>
+                <img className="flex-item-noshrink" alt="" src={data.countryFlag} width="30" height="20" />
+                <span className="flex-item-fluid pl0-5 text-ellipsis">{data.countryName}</span>
+                <span className="flex-item-noshrink text-bold">+{data.countryCallingCode}</span>
             </DropdownMenuButton>
         </div>
     );
