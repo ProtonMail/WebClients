49740afebe4c3ffd17cd186c9ebce64fb9c5e565
Matthias Margot <margotmatthias@protonmail.com>
2021-03-30 14:29:35 +0200

    Change usage of forwardRef to enable typescript docgen again

--- packages/components/components/button/Button.tsx
+++ packages/components/components/button/Button.tsx
@@ -4,8 +4,10 @@ import ButtonLike, { ButtonLikeProps } from './ButtonLike';
 
 export interface ButtonProps extends Omit<ButtonLikeProps<'button'>, 'as' | 'ref'> {}
 
-const Button = (props: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
-    return <ButtonLike type="button" ref={ref} {...props} as="button" />;
-};
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+    (props: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
+        return <ButtonLike type="button" ref={ref} {...props} as="button" />;
+    }
+);
 
-export default React.forwardRef<HTMLButtonElement, ButtonProps>(Button);
+export default Button;
