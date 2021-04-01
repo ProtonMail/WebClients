8e6b0e3f72cb739a981a5e175c13b767dced93d6
Matthieu Bué <matthieu.bue@gmail.com>
2021-04-01 19:23:52 +0200

    Fix sidebarBackButton alignment

--- packages/components/components/sidebar/SidebarBackButton.tsx
+++ packages/components/components/sidebar/SidebarBackButton.tsx
@@ -6,9 +6,17 @@ import { Icon } from '../icon';
 
 const SidebarBackButton = ({ children, ...rest }: LinkProps) => {
     return (
-        <ButtonLike as={AppLink} size="large" color="weak" shape="solid" fullWidth className="mt0-25" {...rest}>
+        <ButtonLike
+            as={AppLink}
+            size="large"
+            color="weak"
+            shape="solid"
+            fullWidth
+            className="mt0-25 flex flex-align-items-center flex-justify-center"
+            {...rest}
+        >
             <Icon name="arrow-left" />
-            <span className="ml0-5 align-middle">{children}</span>
+            <span className="ml0-5">{children}</span>
         </ButtonLike>
     );
 };
