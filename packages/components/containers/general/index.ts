e2817de1b54dc8cf43ff459b08421cfc436a7fbc
Richard <richard@protonmail.com>
2021-04-01 21:12:56 +0200

    Review ShortcutsToggle

--- packages/components/containers/general/index.ts
+++ packages/components/containers/general/index.ts
@@ -5,3 +5,4 @@ export { default as AutoSaveContactsToggle } from './AutoSaveContactsToggle';
 export { default as TimeFormatSection } from './TimeFormatSection';
 export { default as DateFormatSection } from './DateFormatSection';
 export { default as WeekStartSection } from './WeekStartSection';
+export { default as ShortcutsToggle } from './ShortcutsToggle';
