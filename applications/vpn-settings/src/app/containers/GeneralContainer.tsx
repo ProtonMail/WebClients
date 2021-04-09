b77b1cf29575ccbd9acfaa75ae9a2efe30618637
Matthias Margot <margotmatthias@protonmail.com>
2021-04-09 17:17:32 +0200

    Ensure proton-vpn-settings backwards compatibility with the settings update

--- applications/vpn-settings/src/app/containers/GeneralContainer.tsx
+++ applications/vpn-settings/src/app/containers/GeneralContainer.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { c } from 'ttag';
-import {
-    LanguageSection,
-    ThemesSection,
-    SettingsPropsShared,
-    EarlyAccessSection,
-    useEarlyAccess,
-} from 'react-components';
+import { LanguageSection, ThemesSection, SettingsPropsShared } from 'react-components';
 import locales from 'proton-shared/lib/i18n/locales';
 import isTruthy from 'proton-shared/lib/helpers/isTruthy';
 
 import PrivateMainSettingsAreaWithPermissions from '../components/page/PrivateMainSettingsAreaWithPermissions';
 
-export const getGeneralPage = ({ hasEarlyAccess }: { hasEarlyAccess: boolean }) => {
+export const getGeneralPage = () => {
     return {
         text: c('Title').t`General`,
         to: '/general',
@@ -26,28 +20,19 @@ export const getGeneralPage = ({ hasEarlyAccess }: { hasEarlyAccess: boolean }) 
                 text: c('Title').t`Themes`,
                 id: 'themes',
             },
-            hasEarlyAccess
-                ? {
-                      text: c('Title').t`Early Access`,
-                      id: 'early-access',
-                  }
-                : undefined,
         ].filter(isTruthy),
     };
 };
 
 const GeneralContainer = ({ setActiveSection, location }: SettingsPropsShared) => {
-    const { hasEarlyAccess } = useEarlyAccess();
-
     return (
         <PrivateMainSettingsAreaWithPermissions
             location={location}
-            config={getGeneralPage({ hasEarlyAccess })}
+            config={getGeneralPage()}
             setActiveSection={setActiveSection}
         >
             <LanguageSection locales={locales} />
             <ThemesSection />
-            {hasEarlyAccess ? <EarlyAccessSection /> : null}
         </PrivateMainSettingsAreaWithPermissions>
     );
 };
