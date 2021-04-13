4a9ad0050e631c109d264ccd2fca906843d412af
Eamonn Maguire <eamonnmag@protonmail.ch>
2021-04-13 09:33:27 +0000

    Add a minimum password length validator

--- packages/shared/lib/helpers/formValidators.ts
+++ packages/shared/lib/helpers/formValidators.ts
@@ -4,6 +4,10 @@ import { isNumber } from './validators';
 
 export const requiredValidator = (value: any) =>
     value === undefined || value === null || value?.trim?.() === '' ? c('Error').t`This field is required` : '';
+export const minLengthValidator = (value: string, minimumLength: number) =>
+    value.length < minimumLength ? c('Error').t`This field requires a minimum of ${minimumLength} characters.` : '';
 export const emailValidator = (value: string) => (!validateEmailAddress(value) ? c('Error').t`Email is not valid` : '');
 export const numberValidator = (value: string) => (!isNumber(value) ? c('Error').t`Not a valid number` : '');
 export const confirmPasswordValidator = (a: string, b: string) => (a !== b ? c('Error').t`Passwords do not match` : '');
+export const passwordLengthValidator = (a: string, length = 8) =>
+    a.length < length ? c('Error').t`Password should have at least ${length} characters` : '';
