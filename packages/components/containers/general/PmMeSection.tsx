c9bbb936b9557c02e08421d658a8640bbc577225
Jeremy Benaim <jeremybenaim@gmail.com>
2021-04-15 17:38:58 +0200

    Fix PmMe section

--- packages/components/containers/general/PmMeSection.tsx
+++ packages/components/containers/general/PmMeSection.tsx
@@ -1,31 +1,45 @@
 import React from 'react';
 import { c } from 'ttag';
 
-import { Address } from 'proton-shared/lib/interfaces';
-import { ADDRESS_TYPE } from 'proton-shared/lib/constants';
+import { getAccountSettingsApp } from 'proton-shared/lib/apps/helper';
+
+import { AppLink, ButtonLike } from '../../components';
 
 import { SettingsSection, SettingsParagraph } from '../account';
 import PmMeButton from './PmMeButton';
 
 interface Props {
-    addresses: Address[];
+    isPMAddressActive: boolean;
 }
 
-const PmMeSection = ({ addresses }: Props) => {
-    const hasPremium = addresses.some(({ Type }) => Type === ADDRESS_TYPE.TYPE_PREMIUM);
-
-    return !hasPremium ? (
+const PmMeSection = ({ isPMAddressActive }: Props) => {
+    return (
         <SettingsSection>
-            <SettingsParagraph
-                className="mb1"
-                learnMoreUrl="https://protonmail.com/support/knowledge-base/pm-me-addresses/"
-            >
-                {c('Info')
-                    .t`Add a @pm.me email address to your account. This simple, shorter domain stands for "ProtonMail me" or "Private Message me."`}
-            </SettingsParagraph>
-            <PmMeButton />
+            {!isPMAddressActive ? (
+                <>
+                    <SettingsParagraph
+                        className="mb1"
+                        learnMoreUrl="https://protonmail.com/support/knowledge-base/pm-me-addresses/"
+                    >
+                        {c('Info')
+                            .t`Add a @pm.me email address to your account. This simple, shorter domain stands for "ProtonMail me" or "Private Message me."`}
+                    </SettingsParagraph>
+                    <PmMeButton />
+                </>
+            ) : (
+                <>
+                    <SettingsParagraph className="mb1">
+                        {c('Info')
+                            .t`You can now receive messages from your @pm.me address. Upgrade to a paid plan to also send emails using your @pm.me address and create additional @pm.me addresses.`}
+                    </SettingsParagraph>
+
+                    <ButtonLike color="norm" as={AppLink} to="/subscription" toApp={getAccountSettingsApp()}>
+                        {c('Action').t`Upgrade`}
+                    </ButtonLike>
+                </>
+            )}
         </SettingsSection>
-    ) : null;
+    );
 };
 
 export default PmMeSection;
