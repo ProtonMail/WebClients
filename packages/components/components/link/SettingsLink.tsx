5ca9c0bf4c09aa58916797ca8b82bb32fc639a55
Mattias Svanström <mmso@mmso.se>
2021-04-16 10:20:47 +0000

    Fix subscription links

--- packages/components/components/link/SettingsLink.tsx
+++ packages/components/components/link/SettingsLink.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { APPS, APP_NAMES } from 'proton-shared/lib/constants';
-import { DEFAULT_APP, getAppFromPathnameSafe, getSlugFromApp } from 'proton-shared/lib/apps/slugHelper';
+import { getAppFromPathnameSafe, getSlugFromApp } from 'proton-shared/lib/apps/slugHelper';
 import { useLocation } from 'react-router-dom';
 import AppLink, { Props as AppLinkProps } from './AppLink';
 import { useConfig } from '../../hooks';
@@ -10,26 +10,26 @@ export interface Props extends Omit<AppLinkProps, 'to' | 'toApp'> {
     app?: APP_NAMES;
 }
 
-const SettingsLink = ({ path, app, children, ...rest }: Props) => {
+const SettingsLink = ({ path, app, children, ...rest }: Props, ref: React.Ref<HTMLAnchorElement>) => {
     const location = useLocation();
     const { APP_NAME } = useConfig();
 
     if (APP_NAME === APPS.PROTONVPN_SETTINGS) {
         return (
-            <AppLink to={path} {...rest}>
+            <AppLink to={path} ref={ref} {...rest}>
                 {children}
             </AppLink>
         );
     }
 
     const settingsApp = APP_NAME === APPS.PROTONACCOUNT ? getAppFromPathnameSafe(location.pathname) : undefined;
-    const slug = getSlugFromApp(settingsApp || app || DEFAULT_APP);
+    const slug = getSlugFromApp(settingsApp || app || APP_NAME);
 
     return (
-        <AppLink to={`/${slug}${path}`} toApp={APPS.PROTONACCOUNT} {...rest}>
+        <AppLink to={`/${slug}${path}`} ref={ref} toApp={APPS.PROTONACCOUNT} {...rest}>
             {children}
         </AppLink>
     );
 };
 
-export default SettingsLink;
+export default React.forwardRef<HTMLAnchorElement, Props>(SettingsLink);
