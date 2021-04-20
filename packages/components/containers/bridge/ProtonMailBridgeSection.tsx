44bbca646a67bfd51ffa1508ad1660d8d7590320
Michal Horejsek <horejsek@protonmail.com>
2021-04-20 14:33:12 +0200

    Load Bridge versions dynamically

--- packages/components/containers/bridge/ProtonMailBridgeSection.tsx
+++ packages/components/containers/bridge/ProtonMailBridgeSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { c } from 'ttag';
 
 import { Select, Icon, Button } from '../../components';
@@ -7,78 +7,105 @@ import { SettingsParagraph, SettingsSectionWide } from '../account';
 
 import './ProtonMailBridgeSection.scss';
 
-interface BridgeClients {
+interface BridgeClient {
     id: string;
     icon: string;
     platform: string;
+    versionFile: string;
     version: string;
-    downloads: {
-        link: string;
-        fileType: string;
-    }[];
+    downloads: string[];
 }
 
-const ProtonMailBridgeSection = () => {
-    const bridgeClients: BridgeClients[] = [
-        {
-            id: 'windows',
-            icon: 'windows',
-            platform: 'Windows',
-            version: 'v1.6.3 (64-bit)',
-            downloads: [
-                {
-                    fileType: c('OS compatibility').t`.exe (64-bit)`,
-                    link: 'https://protonmail.com/download/Bridge-Installer.exe',
-                },
-            ],
-        },
-        {
-            id: 'apple',
-            icon: 'apple',
-            platform: 'macOS',
-            version: 'v1.6.3',
-            downloads: [
-                {
-                    fileType: c('OS compatibility').t`.dmg (10.12 or later)`,
-                    link: 'https://protonmail.com/download/Bridge-Installer.dmg',
-                },
-            ],
-        },
-        {
-            id: 'linux',
-            icon: 'linux',
-            platform: 'GNU/Linux',
-            version: 'v1.6.3',
-            downloads: [
-                {
-                    fileType: c('OS compatibility').t`.deb (for Debian/Ubuntu)`,
-                    link: 'https://protonmail.com/download/bridge/protonmail-bridge_1.6.3-1_amd64.deb',
-                },
-                {
-                    fileType: c('OS compatibility').t`.rpm (for Fedora/openSUSE)`,
-                    link: 'https://protonmail.com/download/bridge/protonmail-bridge-1.6.3-1.x86_64.rpm',
-                },
-                {
-                    fileType: c('OS compatibility').t`PKGBUILD (for other distributions)`,
-                    link: 'https://protonmail.com/download/bridge/PKGBUILD',
-                },
-            ],
-        },
-    ];
+const initialBridgeClients: BridgeClient[] = [
+    {
+        id: 'windows',
+        icon: 'windows',
+        platform: 'Windows',
+        versionFile: 'version_windows.json',
+        version: 'Latest',
+        downloads: ['https://protonmail.com/download/bridge/Bridge-Installer.exe'],
+    },
+    {
+        id: 'apple',
+        icon: 'apple',
+        platform: 'macOS',
+        versionFile: 'version_darwin.json',
+        version: 'Latest',
+        downloads: ['https://protonmail.com/download/bridge/Bridge-Installer.dmg'],
+    },
+    {
+        id: 'linux',
+        icon: 'linux',
+        platform: 'GNU/Linux',
+        versionFile: 'version_linux.json',
+        version: 'Latest',
+        downloads: ['https://protonmail.com/bridge/download'],
+    },
+];
 
-    const [linuxLink, setLinuxLink] = useState(bridgeClients[2].downloads[0].link);
+const fetchBridgeVersion = async (bridgeClient: BridgeClient): Promise<BridgeClient> => {
+    try {
+        const response = await fetch(`https://protonmail.com/download/bridge/${bridgeClient.versionFile}`);
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        const jsonResponse = await response.json();
+        return {
+            ...bridgeClient,
+            version: jsonResponse.stable.Version,
+            downloads: jsonResponse.stable.Installers,
+        };
+    } catch (e) {
+        if (bridgeClient.id === 'linux') {
+            return {
+                ...bridgeClient,
+                version: c('Bridge version load failed').t`Failed to find latest version`,
+            };
+        }
+    }
+    return bridgeClient;
+};
+
+const ProtonMailBridgeSection = () => {
+    const fileTypes = new Map([
+        ['.exe', c('OS compatibility').t`.exe (64-bit)`],
+        ['.dmg', c('OS compatibility').t`.dmg (10.12 or later)`],
+        ['.deb', c('OS compatibility').t`.deb (for Debian/Ubuntu)`],
+        ['.rpm', c('OS compatibility').t`.rpm (for Fedora/openSUSE)`],
+        ['PKGBUILD', c('OS compatibility').t`PKGBUILD (for other distributions)`],
+    ]);
+    const getFileType = (link: string): string => {
+        for (const [ext, fileType] of fileTypes) {
+            if (link.endsWith(ext)) {
+                return fileType;
+            }
+        }
+        return '';
+    };
+
+    const [bridgeClients, setBridgeClients] = useState(initialBridgeClients);
+    const [linuxLink, setLinuxLink] = useState(bridgeClients[2].downloads[0]);
+
+    useEffect(() => {
+        async function run() {
+            const newBridgeClients = await Promise.all(bridgeClients.map(fetchBridgeVersion));
+            setBridgeClients(newBridgeClients);
+            setLinuxLink(newBridgeClients[2].downloads[0]);
+        }
+        run();
+    }, []);
 
     const handleSelect = ({ target }: React.ChangeEvent<HTMLSelectElement>) => setLinuxLink(target.value);
 
-    const renderBox = ({ id, icon, platform, version, downloads }: BridgeClients) => {
+    const renderBox = ({ id, icon, platform, version, downloads }: BridgeClient) => {
         const options =
             downloads.map((d) => ({
-                text: d.fileType,
-                value: d.link,
+                text: getFileType(d),
+                value: d,
             })) || [];
 
         const handleClick = () => {
-            window.open(id === 'linux' ? linuxLink : downloads[0].link, '_self');
+            window.open(id === 'linux' ? linuxLink : downloads[0], '_self');
         };
 
         return (
@@ -94,7 +121,10 @@ const ProtonMailBridgeSection = () => {
                         <Select value={linuxLink} options={options} onChange={handleSelect} className="mb1" />
                     )}
 
-                    <Button color="norm" className="w100 mtauto" onClick={handleClick}>{c('Title').t`Download`}</Button>
+                    {downloads.length > 0 && (
+                        <Button color="norm" className="w100 mtauto" onClick={handleClick}>{c('Action')
+                            .t`Download`}</Button>
+                    )}
                 </div>
             </div>
         );
