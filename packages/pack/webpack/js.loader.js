4206926a20bcd72ad83c6f17c753a7a0c92e1162
mmso <mmso@mmso.se>
2021-04-26 11:01:34 +0200

    Keep supporting safari 11 (#76)

--- packages/pack/webpack/js.loader.js
+++ packages/pack/webpack/js.loader.js
@@ -41,10 +41,7 @@ const getBabelLoader = ({ isProduction = false, hasReactRefresh = true, isTtag =
                     {
                         targets: {
                             browsers: isProduction
-                                ? [
-                                      // TODO: Only browsers that support es6
-                                      '> 0.5%, not IE 11, Firefox ESR'
-                                  ]
+                                ? ['> 0.5%, not IE 11, Firefox ESR, Safari 11']
                                 : ['last 1 chrome version', 'last 1 firefox version', 'last 1 safari version']
                         },
                         useBuiltIns: 'entry',
