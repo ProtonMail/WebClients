a70c088116d4c4dcb8a341d5d2c632885f33810d
econdepe <econdepe@protonmail.com>
2021-04-28 10:13:31 +0200

    Move GetCalendarInfo type

--- packages/components/hooks/useGetCalendarInfo.ts
+++ packages/components/hooks/useGetCalendarInfo.ts
@@ -1,22 +1,9 @@
-import { DecryptedKey } from 'proton-shared/lib/interfaces';
-import { CalendarSettings, DecryptedCalendarKey } from 'proton-shared/lib/interfaces/calendar';
 import { useCallback } from 'react';
 import getMemberAndAddress, { getMemberAndAddressID } from 'proton-shared/lib/calendar/integration/getMemberAndAddress';
 import { useGetAddresses } from './useAddresses';
 import { useGetAddressKeys } from './useGetAddressKeys';
 import { useGetCalendarBootstrap } from './useGetCalendarBootstrap';
 import { useGetDecryptedPassphraseAndCalendarKeys } from './useGetDecryptedPassphraseAndCalendarKeys';
-
-export type GetCalendarInfo = (
-    ID: string
-) => Promise<{
-    memberID: string;
-    addressKeys: DecryptedKey[];
-    decryptedCalendarKeys: DecryptedCalendarKey[];
-    calendarSettings: CalendarSettings;
-    decryptedPassphrase: string;
-    passphraseID: string;
-}>;
 
 export const useGetCalendarInfo = () => {
     const getCalendarBootstrap = useGetCalendarBootstrap();
