fca54f1508fe2d88ce07b5a33c71e69bc285c81d
Mattias Svanström <mmso@mmso.se>
2021-04-28 14:00:17 +0000

    Extract challenge error component

--- packages/components/components/challenge/Challenge.tsx
+++ packages/components/components/challenge/Challenge.tsx
@@ -1,15 +1,13 @@
 import React, { useRef, useState } from 'react';
-import { c } from 'ttag';
 import { randomIntFromInterval } from 'proton-shared/lib/helpers/function';
 import { getRelativeApiHostname } from 'proton-shared/lib/helpers/url';
 
-import ChallengeFrame, { Props as ChallengeProps } from './ChallengeFrame';
-import { Alert } from '../alert';
-import { Loader } from '../loader';
 import { classnames } from '../../helpers';
-import { useConfig, useModals } from '../../hooks';
-import { BugModal } from '../../containers';
-import { InlineLinkButton } from '../button';
+import { useConfig } from '../../hooks';
+import { Loader } from '../loader';
+
+import ChallengeFrame, { Props as ChallengeProps } from './ChallengeFrame';
+import ChallengeError from './ChallengeError';
 import { ChallengeLog } from './interface';
 
 interface Props extends Omit<ChallengeProps, 'src' | 'onError' | 'onSuccess'> {
@@ -20,7 +18,6 @@ interface Props extends Omit<ChallengeProps, 'src' | 'onError' | 'onSuccess'> {
 
 const Challenge = ({ children, style, onSuccess, onError, bodyClassName, loaderClassName, type, ...rest }: Props) => {
     const config = useConfig();
-    const { createModal } = useModals();
 
     const [isLoading, setIsLoading] = useState(true);
     const [hasError, setHasError] = useState(false);
@@ -42,34 +39,12 @@ const Challenge = ({ children, style, onSuccess, onError, bodyClassName, loaderC
     const jitter = randomIntFromInterval(0, 3);
     const errorTimeout = (15 + errorRetry * 5 - jitter) * 1000;
 
-    const refresh = (
-        <InlineLinkButton key="refresh" onClick={() => window.location.reload()}>{c('Action')
-            .t`refresh the page`}</InlineLinkButton>
-    );
-
-    const supportTeam = (
-        <InlineLinkButton
-            key="support"
-            title="Contact the ProtonMail support team."
-            onClick={() => {
-                createModal(<BugModal />);
-            }}
-        >
-            {c('Info').t`support team`}
-        </InlineLinkButton>
-    );
-
     return (
         <div style={style}>
             {isLoading ? <Loader className={loaderClassName} /> : null}
 
             {hasError ? (
-                <>
-                    <Alert type="error">
-                        {c('Error')
-                            .jt`Something went wrong, please ${refresh} in order to proceed. If you still see this error message please contact our ${supportTeam}.`}
-                    </Alert>
-                </>
+                <ChallengeError />
             ) : (
                 <ChallengeFrame
                     key={errorRetry}
