9d1958e30d91a02e1990254fc806e3767ef145c5
mmso <mmso@mmso.se>
2021-04-28 18:25:36 +0200

    Remove unused functions

--- packages/components/containers/payments/subscription/helpers.js
+++ packages/components/containers/payments/subscription/helpers.js
@@ -1,7 +1,5 @@
 import { PLAN_SERVICES, PLAN_TYPES, ADDON_NAMES } from 'proton-shared/lib/constants';
 import { hasBit } from 'proton-shared/lib/helpers/bitset';
-import { c, msgid } from 'ttag';
-import { isEquivalent, pick } from 'proton-shared/lib/helpers/object';
 import { getLastCancelledSubscription } from 'proton-shared/lib/api/payments';
 import { getUnixTime } from 'date-fns';
 
@@ -9,126 +7,8 @@ const { PLAN, ADDON } = PLAN_TYPES;
 const { MAIL, VPN } = PLAN_SERVICES;
 const OCTOBER_01 = getUnixTime(new Date('2020-10-01'));
 
-const I18N = {
-    included: c('Option').t`Included`,
-    address(value) {
-        return c('Option').ngettext(msgid`${value} address`, `${value} addresses`, value);
-    },
-    space(value) {
-        return c('Option').ngettext(msgid`${value} GB storage`, `${value} GB storage`, value);
-    },
-    domain(value) {
-        return c('Option').ngettext(msgid`${value} custom domain`, `${value} custom domains`, value);
-    },
-    member(value) {
-        return c('Option').ngettext(msgid`${value} user`, `${value} users`, value);
-    },
-    vpn(value) {
-        return c('Option').ngettext(msgid`${value} VPN connection`, `${value} VPN connections`, value);
-    },
-};
-
-/**
- * Check if a plans map contains at least b plans map
- * @param {Object} a plans map
- * @param {Object} b plans map
- * @returns {Boolean}
- */
-export const containsSamePlans = (a, b) => isEquivalent(pick(a, Object.keys(b)), b);
-
-/**
- * Convert subscription plans to PlanIDs format required by API requests
- * @param {Array} plans coming from Subscription API
- * @returns {Object}
- */
-export const toPlanMap = (plans = [], key = 'ID') => {
-    return plans.reduce((acc, plan) => {
-        acc[plan[key]] = acc[plan[key]] || 0;
-        acc[plan[key]] += 1;
-        return acc;
-    }, Object.create(null));
-};
-
-export const toPlanIDs = toPlanMap;
-export const toPlanNames = (plans = []) => toPlanMap(plans, 'Name');
-
-/**
- * Build plansMap from current subscription and user demand
- * @param {Object} plansMap user demand
- * Possible entries for plansMap
- * {}
- * { vpnplus: 1 }
- * { vpnbasic: 1 }
- * { plus: 1 }
- * { vpnplus: 1, plus: 1 }
- * { professional: 1 }
- * { vpnplus: 1, professional: 1 }
- * { visionary: 1 }
- * @param {Array} subscription.Plans
- * @returns {Object} plansMap
- */
-export const mergePlansMap = (plansMap = {}, { Plans = [] }) => {
-    // Free user subscribing
-    if (!Plans.length) {
-        return plansMap;
-    }
-
-    // Has an existing subscription
-
-    // Visionary includes mail and vpn service with no addons
-    if (plansMap.visionary) {
-        return plansMap;
-    }
-
-    const currentPlansMap = toPlanNames(Plans);
-
-    if (containsSamePlans(plansMap, { vpnplus: 1, plus: 1 })) {
-        return {
-            ...plansMap,
-            [ADDON_NAMES.DOMAIN]: currentPlansMap[ADDON_NAMES.DOMAIN],
-            [ADDON_NAMES.ADDRESS]: currentPlansMap[ADDON_NAMES.ADDRESS],
-            [ADDON_NAMES.SPACE]: currentPlansMap[ADDON_NAMES.SPACE],
-        };
-    }
-
-    if (containsSamePlans(plansMap, { vpnplus: 1, professional: 1 })) {
-        return {
-            ...plansMap,
-            [ADDON_NAMES.DOMAIN]:
-                currentPlansMap[ADDON_NAMES.DOMAIN] > 1 ? currentPlansMap[ADDON_NAMES.DOMAIN] : undefined, // pro starts with 2 custom domain
-            [ADDON_NAMES.MEMBER]: currentPlansMap[ADDON_NAMES.MEMBER],
-            [ADDON_NAMES.VPN]: currentPlansMap[ADDON_NAMES.VPN], // Only possible with vpnplus and professional
-        };
-    }
-
-    // Only concern ProtonVPN dashboard
-    if (containsSamePlans(plansMap, { vpnplus: 1 }) || containsSamePlans(plansMap, { vpnbasic: 1 })) {
-        return {
-            ...plansMap,
-            plus: currentPlansMap.plus,
-            professional: currentPlansMap.professional,
-            [ADDON_NAMES.DOMAIN]: currentPlansMap[ADDON_NAMES.DOMAIN],
-            [ADDON_NAMES.ADDRESS]: currentPlansMap[ADDON_NAMES.ADDRESS],
-            [ADDON_NAMES.SPACE]: currentPlansMap[ADDON_NAMES.SPACE],
-            [ADDON_NAMES.MEMBER]: currentPlansMap[ADDON_NAMES.MEMBER],
-            [ADDON_NAMES.VPN]: currentPlansMap[ADDON_NAMES.VPN],
-        };
-    }
-
-    return plansMap;
-};
-
-export const getTextOption = (type, value, index) => {
-    return `${I18N[type](value)} ${index ? '' : `(${I18N.included})`}`.trim();
-};
-
 /**
  * Calculate total for a specific subscription configuration
- * @param {Object} param.plansMap
- * @param {Number} param.cycle
- * @param {Array} param.plans
- * @param {Number} param.services optional
- * @returns {Number} amount
  */
 export const getSubTotal = ({ plansMap, cycle, plans, services }) => {
     return Object.entries(plansMap).reduce((acc, [planName, quantity]) => {
@@ -149,15 +29,6 @@ export const getSubTotal = ({ plansMap, cycle, plans, services }) => {
 
 /**
  * Merge addon to addition parameters
- * @param {Number} oldAddon.Quantity
- * @param {Number} oldAddon.Amount
- * @param {Number} oldAddon.MaxDomains
- * @param {Number} oldAddon.MaxAddresses
- * @param {Number} oldAddon.MaxSpace
- * @param {Number} oldAddon.MaxMembers
- * @param {Number} oldAddon.MaxVPN
- * @param {Object} addon to merge with
- * @returns {Object} new addon
  */
 const mergeAddons = (
     { Quantity = 0, Amount = 0, MaxDomains = 0, MaxAddresses = 0, MaxSpace = 0, MaxMembers = 0, MaxVPN = 0 } = {},
@@ -175,8 +46,6 @@ const mergeAddons = (
 
 /**
  * Format plans to returns essential structure
- * @param {Array} plans coming from Subscription API
- * @returns {Object} { mailPlan, vpnPlan, addressAddon, vpnAddon, domainAddon, memberAddon, spaceAddon }
  */
 export const formatPlans = (plans = []) => {
     return plans.reduce((acc, plan) => {
@@ -220,78 +89,7 @@ export const formatPlans = (plans = []) => {
 };
 
 /**
- * Helper to find plans from Subscription.Plans or Plans
- * @param {Array} plans from Subscription.Plans or Plans
- * @param {String} params.planName examples: 'plus', 'vpnplus', 'visionary', ADDON_NAMES.ADDRESS, ADDON_NAMES.MEMBER
- * @param {String} params.id plan ID
- * @param {Number} params.type default: plan
- * @returns {Object} plan Object
- */
-export const getPlan = (plans = [], { type = PLAN, name = '', id = '' }) => {
-    if (!plans.length) {
-        throw new Error('plans not defined');
-    }
-
-    if (!name && !id) {
-        throw new Error('name or id not defined to get plan');
-    }
-
-    if (name) {
-        return plans.find(({ Type, Name }) => Type === type && Name === name);
-    }
-
-    return plans.find(({ Type, ID }) => Type === type && ID === id);
-};
-
-/**
- * Alias to getPlan with Type = ADDON
- * @param {Array} plans
- * @param {Object} params
- */
-export const getAddon = (plans, params) => getPlan(plans, { ...params, type: ADDON });
-
-/**
- * Prepare params for /api/subscription/check request
- * @param {Array} param.plans
- * @param {Object} param.plansMap
- * @param {String} param.coupon
- * @param {String} param.currency
- * @param {Number} param.cycle
- * @returns {Object} parameters
- */
-export const getCheckParams = ({
-    plans = [],
-    plansMap = {},
-    coupon: CouponCode,
-    currency: Currency,
-    cycle: Cycle,
-    ...rest
-}) => {
-    if (!plans.length) {
-        throw new Error('plans not defined');
-    }
-
-    const PlanIDs = Object.entries(plansMap).reduce((acc, [planName, quantity]) => {
-        if (quantity) {
-            const { ID } = plans.find((plan) => plan.Name === planName);
-            acc[ID] = quantity;
-        }
-        return acc;
-    }, Object.create(null));
-
-    return {
-        PlanIDs,
-        CouponCode,
-        Currency,
-        Cycle,
-        ...rest,
-    };
-};
-
-/**
  * Check if the current user is eligible to Black Friday discount
- * @param {Function} api useApi hook
- * @returns {Promise<Boolean>}
  */
 export const checkLastCancelledSubscription = async (api) => {
     // Return the latest subscription cancellation time, return null if the user never had any subscription, 0 if the user currently has an active subscription
--- packages/components/containers/payments/subscription/index.ts
+++ packages/components/containers/payments/subscription/index.ts
@@ -4,7 +4,6 @@ export { default as YourPlanSection } from './YourPlanSection';
 export { default as BlackFridayModal } from './BlackFridayModal';
 export { default as MailBlackFridayModal } from './MailBlackFridayModal';
 export { default as VPNBlackFridayModal } from './VPNBlackFridayModal';
-export { default as SubscriptionTable } from './SubscriptionTable';
 export { default as SubscriptionCheckout } from './SubscriptionCheckout';
 export { default as PlanSelection } from './PlanSelection';
 export { default as PlanSelectionComparison } from './PlanSelectionComparison';
