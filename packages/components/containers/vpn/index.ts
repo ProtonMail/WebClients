623a3163d5c0c01e4e9ee1ac52bdd2e1e9ada32d
mmso <mmso@mmso.se>
2021-04-29 09:34:37 +0200

    Update openvpn credentials section

--- packages/components/containers/vpn/index.ts
+++ packages/components/containers/vpn/index.ts
@@ -1,6 +1,6 @@
 export { default as OpenVPNConfigurationSection } from './OpenVPNConfigurationSection/OpenVPNConfigurationSection';
 export { default as ProtonAccountOpenVpnConfigurationFiles } from './OpenVPNConfigurationSection/ProtonAccountOpenVpnConfigurationFiles';
 export { default as ProtonVPNClientsSection } from './ProtonVPNClientsSection/ProtonVPNClientsSection';
-export { default as OpenVPNAccountSection } from './OpenVPNAccountSection/OpenVPNAccountSection';
+export { default as OpenVPNCredentialsSection } from './OpenVPNCredentialsSection';
 export { default as ProtonVPNResourcesSection } from './ProtonVPNResourcesSection/ProtonVPNResourcesSection';
 export { default as ProtonVPNCredentialsSection } from './ProtonVPNCredentialsSection/ProtonVPNCredentialsSection';
