a5994822c9f1501d2313e8ff369271dfd4fa6726
Mattias Svanström <mmso@mmso.se>
2021-04-30 07:57:11 +0000

    Decrypt member token with all user keys

--- packages/components/hooks/useGetOrganizationKeyRaw.ts
+++ packages/components/hooks/useGetOrganizationKeyRaw.ts
@@ -1,6 +1,6 @@
 import { useCallback } from 'react';
 import { OrganizationKey } from 'proton-shared/lib/interfaces';
-import { getDecryptedOrganizationKey } from 'proton-shared/lib/keys';
+import { getCachedOrganizationKey } from 'proton-shared/lib/keys';
 import { getOrganizationKeys } from 'proton-shared/lib/api/organization';
 
 import useApi from './useApi';
@@ -12,7 +12,7 @@ export const useGetOrganizationKeyRaw = () => {
 
     return useCallback(async () => {
         const Key = await api<OrganizationKey>(getOrganizationKeys());
-        return getDecryptedOrganizationKey({ keyPassword: authentication.getPassword(), Key });
+        return getCachedOrganizationKey({ keyPassword: authentication.getPassword(), Key });
     }, []);
 };
 
