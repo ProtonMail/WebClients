0781d4aee1606afaa635617ff468399e60728633
Richard <richard@protonmail.com>
2021-05-05 16:53:11 +0000

    [CP-1578] Review onboarding flow

--- packages/components/containers/onboarding/OnboardingSetDisplayName.tsx
+++ packages/components/containers/onboarding/OnboardingSetDisplayName.tsx
@@ -1,57 +1,34 @@
 import React from 'react';
 import { c } from 'ttag';
-import onboardingWelcome from 'design-system/assets/img/onboarding/proton-welcome.svg';
+import onboardingDisplayName from 'design-system/assets/img/onboarding/display-name.svg';
 
-import { Label } from '../../components/label';
-import { Input, LegacyInputField } from '../../components/input';
+import { InputFieldTwo } from '../../components';
 import OnboardingContent, { Props as OnboardingContentProps } from './OnboardingContent';
 
 interface Props extends Omit<OnboardingContentProps, 'img' | 'text' | 'description'> {
     displayName: string;
-    hideDisplayName?: boolean;
     setDisplayName: (displayName: string) => void;
     displayNameError?: string;
-    isSubmitted?: boolean;
 }
 
-const OnboardingSetDisplayName = ({
-    isSubmitted,
-    displayName,
-    hideDisplayName,
-    setDisplayName,
-    displayNameError,
-    ...rest
-}: Props) => {
+const OnboardingSetDisplayName = ({ displayName, setDisplayName, displayNameError, ...rest }: Props) => {
     return (
         <OnboardingContent
             description={c('Onboarding Proton')
-                .t`Proton is your private space on the Internet. No one is reading your emails, monitoring your calendar events, or scanning your files. Your data is encrypted, and you’re in control.`}
-            img={<img src={onboardingWelcome} alt="Proton" />}
-            text={
-                !hideDisplayName &&
-                c('Onboarding Proton')
-                    .t`Please choose a display name to finish setting up your account. (Other people will see this.)`
-            }
+                .t`This is what people will see when you send them an email, invite them to an event, or share a file with them.`}
+            img={<img src={onboardingDisplayName} alt={c('Onboarding Proton').t`Choose a display name`} />}
+            title={c('Onboarding Proton').t`Choose a display name`}
             {...rest}
         >
-            {!hideDisplayName && (
-                <div className="sign-layout-container">
-                    <LegacyInputField
-                        label={<Label htmlFor="displayName">{c('Label').t`Display name`}</Label>}
-                        input={
-                            <Input
-                                value={displayName}
-                                onChange={({ target }) => setDisplayName(target.value)}
-                                id="displayName"
-                                placeholder={c('Placeholder').t`e.g. Julia Smith`}
-                                isSubmitted={isSubmitted}
-                                error={displayNameError}
-                                autoFocus
-                            />
-                        }
-                    />
-                </div>
-            )}
+            <InputFieldTwo
+                id="displayName"
+                label={c('Label').t`Display name`}
+                autoFocus
+                placeholder={c('Placeholder').t`e.g. Julia Smith`}
+                value={displayName}
+                onValue={setDisplayName}
+                error={displayNameError}
+            />
         </OnboardingContent>
     );
 };
