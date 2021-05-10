5fd29345df77b6e3fa100df36436f12044c1a67d
mmso <mmso@mmso.se>
2021-05-10 13:55:55 +0200

    Fix core js version (#81)
    * Fix core-js version
    
    * Sync lock

--- packages/pack/webpack/js.loader.js
+++ packages/pack/webpack/js.loader.js
@@ -14,7 +14,7 @@ const UNSUPPORTED_JS_LOADER = [
                     {
                         targets: { browsers: ['ie 11'] },
                         useBuiltIns: 'entry',
-                        corejs: 3
+                        corejs: { version: '3.12' }
                     }
                 ]
             ],
@@ -45,7 +45,7 @@ const getBabelLoader = ({ isProduction = false, hasReactRefresh = true, isTtag =
                                 : ['last 1 chrome version', 'last 1 firefox version', 'last 1 safari version']
                         },
                         useBuiltIns: 'entry',
-                        corejs: 3,
+                        corejs: { version: '3.12' },
                         exclude: ['transform-typeof-symbol'] // Exclude transforms that make all code slower
                     }
                 ],
