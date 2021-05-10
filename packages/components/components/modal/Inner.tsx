a223c34b58713218c087fe34e6a102cd3651e584
mmso <mmso@mmso.se>
2021-05-10 15:49:57 +0200

    Fix inner forward ref

--- packages/components/components/modal/Inner.tsx
+++ packages/components/components/modal/Inner.tsx
@@ -5,14 +5,15 @@ import ScrollShadows from '../scroll/ScrollShadows';
 interface Props {
     children: React.ReactNode;
     className?: string;
-    ref?: Ref<HTMLDivElement>;
 }
-const Inner = ({ children, className = '', ref }: Props) => {
-    return (
-        <div ref={ref} className={classnames(['modal-content-inner', className])}>
-            <ScrollShadows>{children}</ScrollShadows>
-        </div>
-    );
-};
+const Inner = React.forwardRef<HTMLDivElement, Props>(
+    ({ children, className = '' }: Props, ref: Ref<HTMLDivElement>) => {
+        return (
+            <div ref={ref} className={classnames(['modal-content-inner', className])}>
+                <ScrollShadows>{children}</ScrollShadows>
+            </div>
+        );
+    }
+);
 
 export default Inner;
