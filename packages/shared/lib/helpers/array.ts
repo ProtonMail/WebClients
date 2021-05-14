23d2590b328b3b3c38f67c49b11e8315ec192bbc
Sergo Vainumäe <sergo.vainumae@protonmail.ch>
2021-05-14 15:06:05 +0000

    [CALWEB-2319] export errors v1

--- packages/shared/lib/helpers/array.ts
+++ packages/shared/lib/helpers/array.ts
@@ -186,3 +186,17 @@ export const updateItem = <T>(array: T[], index: number, newItem: T) => {
 };
 
 export const removeItem = <T>(array: T[], index: number) => array.filter((oldValue, i) => i !== index);
+
+export const partition = <T, K = T>(arr: (T | K)[], predicate: (item: T | K) => item is T): [T[], K[]] =>
+    arr.reduce<[T[], K[]]>(
+        (accumulator, current) => {
+            if (predicate(current)) {
+                accumulator[0].push(current);
+            } else {
+                accumulator[1].push(current);
+            }
+
+            return accumulator;
+        },
+        [[], []]
+    );
