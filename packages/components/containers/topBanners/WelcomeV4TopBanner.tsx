4c49e9bc8a1703dc3aa6701a51bd1bdb43c01e2e
Richard <richard@protonmail.com>
2021-05-25 15:05:09 +0200

    Update welcome v4 banner to includes link

--- packages/components/containers/topBanners/WelcomeV4TopBanner.tsx
+++ packages/components/containers/topBanners/WelcomeV4TopBanner.tsx
@@ -4,6 +4,7 @@ import { APPS, SSO_PATHS } from 'proton-shared/lib/constants';
 
 import TopBanner from './TopBanner';
 import { useConfig } from '../../hooks';
+import { Href } from '../../components';
 
 const IS_INITIAL_LOGIN = [SSO_PATHS.AUTHORIZE, SSO_PATHS.LOGIN, '/'].includes(window.location.pathname);
 
@@ -16,8 +17,11 @@ const WelcomeV4TopBanner = () => {
 
     return (
         <TopBanner className="bg-info">
-            {c('Message display when user visit v4 login first time')
-                .t`Welcome to the new ProtonMail design, modern and easy to use. Sign in to discover more.`}
+            <span className="mr0-5">{c('Message display when user visit v4 login first time')
+                .t`Welcome to the new ProtonMail design, modern and easy to use. Sign in to discover more.`}</span>
+            <Href className="underline inline-block color-inherit" url="https://protonmail.com/blog/new-protonmail">{c(
+                'Link'
+            ).t`Learn more`}</Href>
         </TopBanner>
     );
 };
