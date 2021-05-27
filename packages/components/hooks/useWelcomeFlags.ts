0010e125f8ae932dccc8d1c688544de49d47f5c5
Richard <richard@protonmail.com>
2021-05-27 15:20:25 +0000

    [CP-1763] Update logic to set welcome UserSettings.Flags

--- packages/components/hooks/useWelcomeFlags.ts
+++ packages/components/hooks/useWelcomeFlags.ts
@@ -2,33 +2,24 @@ import { useCallback, useState } from 'react';
 import { UserSettingsModel } from 'proton-shared/lib/models';
 import useCache from './useCache';
 
-export const WELCOME_FLAG_KEY = 'flow';
-
 export interface WelcomeFlagsState {
     hasDisplayNameStep?: boolean;
     isWelcomeFlow?: boolean;
-    isWelcomeFlag?: boolean;
 }
 
 const useWelcomeFlags = (): [WelcomeFlagsState, () => void] => {
     const cache = useCache();
     const [state, setState] = useState<WelcomeFlagsState>(() => {
-        const searchParams = new URLSearchParams(window.location.search);
-        const welcomeParam = searchParams.get('welcome') || '';
-        // Set from ProtonApp
-        const flow = cache.get(WELCOME_FLAG_KEY);
-        const hasDisplayNameStep = flow === 'signup' || welcomeParam === 'true';
-        const hasSeenFlow = flow === 'seen';
         // Assumes that user settings has been pre-loaded. Not using hook to avoid re-renders.
-        const isWelcomeFlag = !hasSeenFlow && cache.get(UserSettingsModel.key)?.value?.WelcomeFlag === 0;
+        const userSettings = cache.get(UserSettingsModel.key)?.value;
+        const hasProductWelcomeStep = userSettings.WelcomeFlag === 0; // Operated directly on API side
+        const hasGenericWelcomeStep = userSettings.Flags.Welcomed === 0;
         return {
-            hasDisplayNameStep,
-            isWelcomeFlag,
-            isWelcomeFlow: hasDisplayNameStep || isWelcomeFlag,
+            hasDisplayNameStep: hasGenericWelcomeStep,
+            isWelcomeFlow: hasGenericWelcomeStep || hasProductWelcomeStep,
         };
     });
     const setDone = useCallback(() => {
-        cache.set(WELCOME_FLAG_KEY, 'seen');
         setState({});
     }, []);
     return [state, setDone];
