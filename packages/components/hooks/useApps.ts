0d5b79e6b3cba672764682c6a4a3a6bf6ed2986c
mmso <mmso@mmso.se>
2021-05-27 16:06:08 +0200

    Display drive to all users

--- packages/components/hooks/useApps.ts
+++ packages/components/hooks/useApps.ts
@@ -1,22 +1,9 @@
-import { COUPON_CODES, APPS } from 'proton-shared/lib/constants';
-import isTruthy from 'proton-shared/lib/helpers/isTruthy';
-
-import useSubscription from './useSubscription';
-import { useUser } from './useUser';
+import { APPS } from 'proton-shared/lib/constants';
 
 const { PROTONMAIL, PROTONCALENDAR, PROTONDRIVE } = APPS;
-const { PROTONTEAM, LIFETIME, VISIONARYFOREVER } = COUPON_CODES;
 
 const useApps = () => {
-    const [subscription] = useSubscription();
-    const [user] = useUser();
-    const coupon = subscription?.CouponCode || ''; // CouponCode can be null
-    return [
-        PROTONMAIL,
-        PROTONCALENDAR,
-        ([PROTONTEAM, LIFETIME, VISIONARYFOREVER].includes(coupon as COUPON_CODES) || user.DriveEarlyAccess) &&
-            PROTONDRIVE,
-    ].filter(isTruthy);
+    return [PROTONMAIL, PROTONCALENDAR, PROTONDRIVE];
 };
 
 export default useApps;
