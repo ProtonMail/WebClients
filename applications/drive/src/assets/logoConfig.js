e887e7160fd5198d7fb7e4e4fcb4fa64bfc510b8
Nicolas Hoffmann <dante3333@gmail.com>
2021-06-03 00:55:03 +0200

    Hotfix - theme_color update

--- applications/drive/src/assets/logoConfig.js
+++ applications/drive/src/assets/logoConfig.js
@@ -6,7 +6,7 @@ module.exports = {
             'ProtonDrive allows you to securely store and share your sensitive documents and access them anywhere.',
         developerName: 'Proton Technologies AG',
         developerURL: 'https://github.com/ProtonMail/proton-drive',
-        background: '#262a33',
-        theme_color: '#262a33',
+        background: '#1c223d',
+        theme_color: '#1c223d',
     },
 };
