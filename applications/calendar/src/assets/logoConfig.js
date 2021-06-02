507a019a92066663942e71f7f9d550b733cd8c3f
Nicolas Hoffmann <dante3333@gmail.com>
2021-06-03 00:56:32 +0200

    Hotfix - theme_color update

--- applications/calendar/src/assets/logoConfig.js
+++ applications/calendar/src/assets/logoConfig.js
@@ -6,7 +6,7 @@ module.exports = {
             'Keep your plans private with an encrypted, open source calendar from the developers of ProtonMail.',
         developerName: 'Proton Technologies AG',
         developerURL: 'https://github.com/ProtonMail/proton-calendar',
-        background: '#262a33',
-        theme_color: '#262a33',
+        background: '#1c223d',
+        theme_color: '#1c223d',
     },
 };
