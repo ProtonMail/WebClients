5c6fe77bd836fcb9300441a5cbada162f86a940c
Matthias Margot <margotmatthias@protonmail.com>
2021-06-03 10:36:53 +0000

    [CP-1950] Add useLoad hook

---
+++ packages/components/hooks/useLoad.ts
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { useRouteMatch } from 'react-router';
+import { load } from 'proton-shared/lib/api/core/load';
+
+import useApi from './useApi';
+
+const useLoad = () => {
+    const api = useApi();
+
+    /*
+     * The "path" property on React Router's "match" object contains the
+     * path pattern that was used to match the current pathname.
+     *
+     * It therefore contains the names of any dynamic parameters rather
+     * than their values.
+     *
+     * This is an important distinction for three reasons:
+     * - it makes it possible to tell which segments of the pathname are parameters (":")
+     * - this path looks the same indifferent of how the parameters are populated
+     * - it allows us to send the load request without leaking any potentially sensitive data
+     */
+    const { path } = useRouteMatch();
+
+    useEffect(() => {
+        api({ ...load(path), silent: true });
+    }, []);
+};
+
+export default useLoad;
