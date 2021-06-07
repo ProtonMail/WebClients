4800cb14201d9c267b84cdfb9ef2518fb17d59b9
Richard <richard@protonmail.com>
2021-06-07 10:48:26 +0200

    Review spacing for onboarding modal

--- packages/components/containers/onboarding/OnboardingContent.tsx
+++ packages/components/containers/onboarding/OnboardingContent.tsx
@@ -9,9 +9,9 @@ export interface Props extends React.HTMLProps<HTMLDivElement> {
 const OnboardingContent = ({ description, img, children, title, ...rest }: Props) => {
     return (
         <div {...rest}>
-            {img && <div className="mb0-5 text-center mauto w66 on-mobile-w100">{img}</div>}
-            {title && <h1 className="mb1 text-2xl text-bold text-center">{title}</h1>}
-            {description && <div className="mb1 text-center">{description}</div>}
+            {img && <div className="pb1 text-center mauto w66 on-mobile-w100">{img}</div>}
+            {title && <h1 className="mb0-5 text-2xl text-bold text-center">{title}</h1>}
+            {description && <div className="mb2 text-center">{description}</div>}
             {children}
         </div>
     );
