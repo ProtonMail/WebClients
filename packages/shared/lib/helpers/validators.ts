b935451e1bee747f4ac1b8d7b0c4cd778f6c40e0
Richard <richard@protonmail.com>
2021-06-08 16:21:41 +0000

    [MAILWEB-2231] Add punycode regex detection

--- packages/shared/lib/helpers/validators.ts
+++ packages/shared/lib/helpers/validators.ts
@@ -5,6 +5,7 @@ export const REGEX_URL = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za
 export const REGEX_HEX_COLOR = /^#([a-f0-9]{3,4}|[a-f0-9]{4}(?:[a-f0-9]{2}){1,2})\b$/i;
 export const REGEX_NUMBER = /^\d+$/;
 export const REGEX_BASE64_IMAGE = /^data:image\/(?:gif|png|jpeg|bmp|webp|svg\+xml|apng|tiff);base64/;
+export const REGEX_PUNYCODE = /^(http|https):\/\/xn--/;
 
 export const isEmpty = (value = '') => !value.length;
 export const maxLength = (value = '', limit = 0) => value.length <= limit;
@@ -14,3 +15,4 @@ export const isDomain = isValidDomain;
 export const isHexColor = (value = '') => REGEX_HEX_COLOR.test(value);
 export const isNumber = (value = '') => REGEX_NUMBER.test(value);
 export const isBase64Image = (value = '') => REGEX_BASE64_IMAGE.test(value);
+export const isPunycode = (value = '') => REGEX_PUNYCODE.test(value);
