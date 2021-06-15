8cfbbf3dae280769d176f21e061784424a8bc008
Jeremy Benaim <jeremybenaim@pm.me>
2021-06-15 09:50:33 +0000

    Fix notification on folder creation

--- applications/mail/src/app/components/sidebar/EmptyFolders.tsx
+++ applications/mail/src/app/components/sidebar/EmptyFolders.tsx
@@ -9,9 +9,6 @@ import {
     LabelModal,
     useModals,
 } from 'react-components';
-import { randomIntFromInterval } from 'proton-shared/lib/helpers/function';
-import { LABEL_COLORS, ROOT_FOLDER, LABEL_TYPE } from 'proton-shared/lib/constants';
-import { Folder } from 'proton-shared/lib/interfaces/Folder';
 
 interface Props {
     onFocus: () => void;
@@ -21,13 +18,7 @@ const EmptyFolders = ({ onFocus }: Props) => {
     const { createModal } = useModals();
 
     const handleClick = () => {
-        const newLabel: Pick<Folder, 'Name' | 'Color' | 'ParentID' | 'Type'> = {
-            Name: '',
-            Color: LABEL_COLORS[randomIntFromInterval(0, LABEL_COLORS.length - 1)],
-            ParentID: ROOT_FOLDER,
-            Type: LABEL_TYPE.MESSAGE_FOLDER,
-        };
-        createModal(<LabelModal label={newLabel} />);
+        createModal(<LabelModal type="folder" />);
     };
 
     return (
