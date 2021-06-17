86d0cbf3a1e533068e978b3d8c99648365b718af
Richard <richard@protonmail.com>
2021-06-17 15:02:43 +0200

    Convert calendarImport and contactsImport to TS

--- packages/shared/lib/api/calendarImport.ts
+++ packages/shared/lib/api/calendarImport.ts
@@ -1,11 +1,11 @@
 // Manage imports of user calendars and events from external providers
-export const createCalendarImport = (data) => ({
+export const createCalendarImport = (data: any) => ({
     url: 'calendar/v1/importers',
     method: 'post',
     data,
 });
 
-export const startCalendarImportJob = (importID, data) => ({
+export const startCalendarImportJob = (importID: string, data: any) => ({
     url: `calendar/v1/importers/${importID}`,
     method: 'post',
     data,
--- packages/shared/lib/api/contactsImport.ts
+++ packages/shared/lib/api/contactsImport.ts
@@ -1,11 +1,11 @@
 // Manage imports of user contacts from external providers
-export const createContactsImport = (data) => ({
+export const createContactsImport = (data: any) => ({
     url: 'contacts/v4/importers',
     method: 'post',
     data,
 });
 
-export const startContactsImportJob = (importID, data) => ({
+export const startContactsImportJob = (importID: string, data: any) => ({
     url: `contacts/v4/importers/${importID}`,
     method: 'post',
     data,
