e1d72bb2e1748f0ef16b798a42510439647780b8
Matthias Margot <margotmatthias@protonmail.com>
2021-06-25 17:14:37 +0200

    Fix lint error: @typescript-eslint/no-unused-vars

--- packages/components/typings/css.d.ts
+++ packages/components/typings/css.d.ts
@@ -1,5 +1,5 @@
 declare module 'csstype' {
-    // eslint-disable-next-line no-unused-vars
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface Properties<T> {
         // allow css variables
         [index: string]: unknown;
