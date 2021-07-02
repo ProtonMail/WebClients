22bc67b20a410737b5d6f5aee74caa836a12ade8
Aurelien <aurelien+ptgitlab@protonmail.com>
2021-07-02 09:51:02 +0000

    Move translations out of node_modules

--- packages/i18n/index.js
+++ packages/i18n/index.js
@@ -1,35 +1,26 @@
 #!/usr/bin/env node
 
-const argv = require('minimist')(process.argv.slice(2));
-
-const CONFIG = require('./config');
-const { error, debug, json } = require('./lib/helpers/log')('proton-i18n');
+const { error } = require('./lib/helpers/log')('proton-i18n');
 const renderHelp = require('./lib/helpers/help');
 
-const is = (command) => argv._.includes(command);
+const argv = process.argv.slice(2);
+const is = (command) => argv.includes(command);
 
 async function main() {
-    debug(CONFIG.getEnv(), 'ENV');
-
-    if (is('post-install')) {
-        await require('./lib/postInstall')();
-    }
-
-    if (is('config')) {
-        return json(CONFIG.getEnv(), true);
-    }
+    const [, ...options] = argv;
 
     if (is('extract')) {
-        await require('./lib/extract')(argv._[1]);
+        await require('./lib/extract')(options[0]);
     }
 
     if (is('validate')) {
-        require('./lib/validate')(argv._[1], { dir: argv._[2] });
+        await require('./lib/validate')(options[0], { dir: options[1] });
     }
 
-    if (is('help') && !is('crowdin')) {
+    if (is('help')) {
         renderHelp();
     }
+    process.exit(0);
 }
 
 main().catch(error);
--- packages/i18n/lib/helpers/cli.js
+++ packages/i18n/lib/helpers/cli.js
@@ -7,22 +7,10 @@ const bash = (cli, args = [], stdio) => {
     return execa(cli, args, { shell: '/bin/bash', stdio });
 };
 
-const curl = (url, optCurl = {}, opt = {}) => {
-    const options = Array.isArray(optCurl) ? optCurl : [];
-
-    if (optCurl.file) {
-        const { input, output } = optCurl.file;
-        options.push(`-F "files[/${output}]=@${input}"`);
-    }
-    debug({ url, opt, optCurl, options }, 'curl');
-    debug(`curl ${options.join(' ')} '${url}'`, 'curl');
-    return execa(`curl ${options.join(' ')} '${url}'`, [], { shell: '/bin/bash', ...opt });
-};
-
 // 'inherit'
 const script = (cli, args = [], stdio) => {
     const cmd = path.resolve(__dirname, '..', '..', 'scripts', cli);
     return bash(cmd, args, stdio);
 };
 
-module.exports = { bash, script, curl };
+module.exports = { bash, script };
