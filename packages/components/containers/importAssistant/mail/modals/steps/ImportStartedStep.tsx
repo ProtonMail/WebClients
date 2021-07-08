cea0e67d136fede9772a80b6514e8c02d01179c1
Jeremy Benaim <jeremybenaim@gmail.com>
2021-07-08 11:16:15 +0200

    Add context for l10n in import assistant last step
    MAILWEB-2423

--- packages/components/containers/importAssistant/mail/modals/steps/ImportStartedStep.tsx
+++ packages/components/containers/importAssistant/mail/modals/steps/ImportStartedStep.tsx
@@ -15,15 +15,20 @@ interface Props {
 const ImportStartedStep = ({ modalModel, addresses }: Props) => {
     const address = addresses.find((addr) => addr.ID === modalModel.payload.AddressID);
 
+    // translator: this is part of a sentence that needed to be cut in the code to respect the design. The full sentence is the following: "Your messages are being imported from ${email1} to ${email2}", where ${email1} and ${email2} are variables.
+    const sentenceFragment1 = c('Info').t`Your messages are being imported from`;
+    // translator: this is part of a sentence that needed to be cut in the code to respect the design. The full sentence is the following: "Your messages are being imported from ${email1} to ${email2}", where ${email1} and ${email2} are variables.
+    const sentenceFragment2 = c('Info').t`to`;
+
     return (
         <div className="text-center">
             <img src={importStartedSvg} alt="" className="max-w80" />
             <h3>{c('Info').t`Your import has started!`}</h3>
-            <div className="mt1">{c('Info').t`Your messages are being imported from`}</div>
+            <div className="mt1">{sentenceFragment1}</div>
             <div className="mt1">
                 <strong>{modalModel.email}</strong>
             </div>
-            <div>{c('Info').t`to`}</div>
+            <div>{sentenceFragment2}</div>
             <div>
                 <strong>{address?.Email}</strong>
             </div>
