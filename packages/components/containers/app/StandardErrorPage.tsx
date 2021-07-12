3aff2f5caf0c95694a39567dc767f4ef72bfac70
StraightOuttaCrompton <StraightOuttaCrompton@protonmail.com>
2021-07-12 15:55:54 +0100

    Update too many child sessions error
    To display a more informative error, we provide a link on how to
    clean cookies. Since links cannot be configured in the backend, we
    do this in the frontend by sniffing for the 'TOO_MANY_CHILDREN' error
    code 8003, and displaying a custom error.
    
    CP-2092

--- packages/components/containers/app/StandardErrorPage.tsx
+++ packages/components/containers/app/StandardErrorPage.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { c } from 'ttag';
 import GenericError from '../error/GenericError';
 import { ProminentContainer } from '../../components';
 import { useDocumentTitle } from '../../hooks';
 
-const StandardErrorPage = () => {
+interface Props {
+    children?: ReactNode;
+}
+
+const StandardErrorPage = ({ children }: Props) => {
     useDocumentTitle(c('Error message').t`Oops, something went wrong`);
 
     return (
         <ProminentContainer className="flex flex-align-items-center pb4 scroll-if-needed">
-            <GenericError />
+            <GenericError>{children}</GenericError>
         </ProminentContainer>
     );
 };
