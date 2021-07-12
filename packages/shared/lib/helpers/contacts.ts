643f0000d61e35f8b478351addd195c8bfbe68bc
LeGrosSancho <monsieurk74@gmail.com>
2021-07-12 14:57:09 +0200

    Removed Name field 2nd dropdown when importing contacts
    We removed Yomi field and the only option left in the second dropdown is Name, so it does not makes sense to keep it
    
    MAILWEB-2084

--- packages/shared/lib/helpers/contacts.ts
+++ packages/shared/lib/helpers/contacts.ts
@@ -140,7 +140,6 @@ export const getTypeLabels = () => ({
 });
 
 export const getAllTypes: () => { [key: string]: { text: string; value: string }[] } = () => ({
-    fn: [{ text: c('Property type').t`Name`, value: '' }],
     email: [
         { text: c('Property type').t`Email`, value: '' },
         { text: c('Property type').t`Home`, value: 'home' },
