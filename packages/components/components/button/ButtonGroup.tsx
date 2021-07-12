889396e079f84de06d270399d5e1b19f98610e0d
Matthieu Bué <matthieu.bue@protonmail.com>
2021-07-12 17:44:25 +0000

    Add new ButtonGroup variants

--- packages/components/components/button/ButtonGroup.tsx
+++ packages/components/components/button/ButtonGroup.tsx
@@ -2,45 +2,51 @@ import React, { Fragment, ReactElement } from 'react';
 import { classnames } from '../../helpers';
 import { Vr } from '../vr';
 
-type Shape = 'outline' | 'ghost';
+type Color = 'norm' | 'weak' | 'danger' | 'warning' | 'success' | 'info';
+
+type Shape = 'solid' | 'outline' | 'ghost';
 
 type Size = 'small' | 'medium' | 'large';
 
 interface Props extends React.ComponentPropsWithoutRef<'div'> {
     children: React.ReactNode;
+    color?: Color;
     shape?: Shape;
     size?: Size;
-    className?: string;
 }
 
-const ButtonGroup = (
-    { children, shape = 'outline', size = 'medium', className = '', ...rest }: Props,
-    ref: React.Ref<HTMLDivElement>
-) => {
-    const childrenWithSeparators = React.Children.toArray(children)
-        .filter((x): x is ReactElement => x !== null && React.isValidElement(x))
-        .map((child, index, array) => {
-            const clonedChild = React.cloneElement(child, { group: true });
-            if (index === array.length - 1) {
-                return clonedChild;
-            }
-            return (
-                // eslint-disable-next-line react/no-array-index-key
-                <Fragment key={clonedChild.key || index}>
-                    {clonedChild}
-                    <Vr aria-hidden="true" />
-                </Fragment>
-            );
-        });
-    return (
-        <div
-            className={classnames(['button-group', `button-group-${shape}`, `button-group-${size}`, className])}
-            ref={ref}
-            {...rest}
-        >
-            {childrenWithSeparators}
-        </div>
-    );
-};
+const ButtonGroup = React.forwardRef<HTMLDivElement, Props>(
+    ({ children, color = 'weak', shape = 'outline', size = 'medium', className = '', ...rest }, ref) => {
+        const childrenWithSeparators = React.Children.toArray(children)
+            .filter((x): x is ReactElement => x !== null && React.isValidElement(x))
+            .map((child, index, array) => {
+                const clonedChild = React.cloneElement(child, { group: true, size });
+                if (index === array.length - 1) {
+                    return clonedChild;
+                }
+                return (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <Fragment key={clonedChild.key || index}>
+                        {clonedChild}
+                        <Vr aria-hidden="true" />
+                    </Fragment>
+                );
+            });
+        return (
+            <div
+                className={classnames([
+                    'button-group',
+                    `button-group-${shape}-${color}`,
+                    `button-group-${size}`,
+                    className,
+                ])}
+                ref={ref}
+                {...rest}
+            >
+                {childrenWithSeparators}
+            </div>
+        );
+    }
+);
 
-export default React.forwardRef<HTMLDivElement, Props>(ButtonGroup);
+export default ButtonGroup;
