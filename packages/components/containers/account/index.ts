46d68587609d95ceb47da13ba8d1b3b897e25d27
StraightOuttaCrompton <StraightOuttaCrompton@protonmail.com>
2021-07-13 18:43:59 +0100

    Add upgrade banners to custom domain and catch all address sections
    Instead of displaying a generic upgrade message, we include descriptions
    for custom domain names and the catch-all address with an upgrade banner
    in place of the actual functionality.
    
    CP-1966

--- packages/components/containers/account/index.ts
+++ packages/components/containers/account/index.ts
@@ -11,3 +11,4 @@ export { default as UsernameSection } from './UsernameSection';
 export { default as DeleteSection } from './DeleteSection';
 export { default as PasswordsSection } from './PasswordsSection';
 export { default as TwoFactorSection } from './TwoFactorSection';
+export { default as UpgradeBanner } from './UpgradeBanner';
