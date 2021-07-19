177c35342dd9d115e61613292ff1030c6925cbe5
Oleg Voloshin <olegwoloschin@protonmail.com>
2021-07-19 13:41:17 +0300

    Fix empty transfer manager window
    Empty trasnfer manager issues was caused by `useElementRect` assinging
    0 width to `FixedSizeList` component.
    
    As the `FixedSizeList` component required width to work correctly, we used
    `useElementRect` hook to observer parent element's dimensions. In one
    particular case `useElementRect` sets list width to 0 (it uses
    `useBoundingClientRect` to get the element's dimensions from a browser),
    causing the transfer manager to look like it's empty.
    
    When a user closes the transfer manager we have a race condition, in which there's
    not certainty whether element deletion from DOM, or its width calculation
    will happen first. Due to caching specificities of the `useElementRect` this
    is what happens when a user witnesses a bug:
    
    - initial state (window is open): transfer manager list width is set to n px
    - user closes transfer manager: race condition ends up unlucky and
    `useBoundingClientRect` gets called *after* the element gets removed from DOM ->
    this sets list width to 0
    - user opens the transfer manager again. when `TransferManager` tries to set
    `FixedSizeList`'s width back to n, cache check inside `equivalentReducer`
    prevent this change from happening. Thus, we are stuck at 0 until the browser
    window resizing is done.
    
    The issue is solved by ensuring that the hook is used *only* when the element's
    rendered.
    
    DRVWEB-1334

--- applications/drive/src/app/components/TransferManager/TransferManager.tsx
+++ applications/drive/src/app/components/TransferManager/TransferManager.tsx
@@ -67,33 +67,45 @@ const ListItemRow = ({ style, index, data }: ListItemRowProps) => {
     );
 };
 
-function TransferManager() {
+const TransferManager = ({
+    downloads,
+    uploads,
+    statsHistory,
+    onClear,
+    allTransfersFinished,
+}: {
+    downloads: Download[];
+    uploads: Upload[];
+    statsHistory: TransfersStats[];
+    onClear: () => void;
+    allTransfersFinished: boolean;
+}) => {
     const containerRef = useRef<HTMLDivElement>(null);
+    /*
+        FixedSizedList (used for virtual scrolling) requires `width` prop to work
+        correcty. This is why we use 'useElementRect' hook here.
+        `useElementRect` utilizes `getBoundingClientRect` method to get dimensions
+        of a given element reference.
+
+        *WARNING:* don't introduce conditional rendering to this component –
+        this will lead to a race condition in which an element could be removed
+        from DOM while React still keeping its reference. In this case
+        `getBoundingClientRect` returns zero for each target's dimension, which in certain
+        cases (here due to useElementRect specifics) cause rendering bugs.
+
+        For more details see TransferManagerContainer component
+    */
     const rect = useElementRect(containerRef, buffer);
     const { state: minimized, toggle: toggleMinimized } = useToggle();
     const { openConfirmModal } = useConfirm();
-    const { downloads, getDownloadsProgresses, clearDownloads } = useDownloadProvider();
-    const { uploads, getUploadsProgresses, clearUploads } = useUploadProvider();
     const { isNarrow } = useActiveBreakpoint();
 
-    const transfers = useMemo(() => [...downloads, ...uploads], [downloads, uploads]);
-    const allTransfersFinished = useMemo(() => transfers.every(isTransferFinished), [transfers]);
-    const getTransferProgresses = useCallback(() => {
-        return { ...getUploadsProgresses(), ...getDownloadsProgresses() };
-    }, [getUploadsProgresses, getDownloadsProgresses]);
-    const statsHistory = useStatsHistory(transfers, getTransferProgresses);
-
-    const clearAllTransfers = useCallback(() => {
-        clearDownloads();
-        clearUploads();
-    }, [clearDownloads, clearUploads]);
-
     useEffect(() => {
-        window.addEventListener('unload', clearAllTransfers);
+        window.addEventListener('unload', onClear);
         return () => {
-            window.removeEventListener('unload', clearAllTransfers);
+            window.removeEventListener('unload', onClear);
         };
-    }, [clearAllTransfers]);
+    }, [onClear]);
 
     const latestStats = statsHistory[0];
 
@@ -120,19 +132,15 @@ function TransferManager() {
         [downloadEntries, uploadEntries]
     );
 
-    if (!latestStats || downloads.length + uploads.length === 0) {
-        return null;
-    }
-
     const handleCloseClick = () => {
         if (allTransfersFinished) {
-            clearAllTransfers();
+            onClear();
         } else {
             openConfirmModal({
                 title: c('Title').t`Cancel all active transfers`,
                 confirm: c('Action').t`Confirm`,
                 message: c('Info').t`Closing transfer manager will cancel all active transfers, are you sure?`,
-                onConfirm: clearAllTransfers,
+                onConfirm: onClear,
             });
         }
     };
@@ -189,6 +197,48 @@ function TransferManager() {
             </div>
         </div>
     );
-}
+};
 
-export default TransferManager;
+/*
+    This component is introduced specifically to address the race condition of
+    `return null` code branch caused by `clearAllTransfers` call and width
+    calculation inside `TransferManager`.
+
+    Separating this chunk of code into its component guaranties that
+    list element will be *always* present in DOM for correct transfer manager list
+    width calculation.
+*/
+const TransferManagerContainer = () => {
+    const { downloads, getDownloadsProgresses, clearDownloads } = useDownloadProvider();
+    const { uploads, getUploadsProgresses, clearUploads } = useUploadProvider();
+
+    const transfers = useMemo(() => [...downloads, ...uploads], [downloads, uploads]);
+    const allTransfersFinished = useMemo(() => transfers.every(isTransferFinished), [transfers]);
+    const getTransferProgresses = useCallback(() => {
+        return { ...getUploadsProgresses(), ...getDownloadsProgresses() };
+    }, [getUploadsProgresses, getDownloadsProgresses]);
+    const statsHistory = useStatsHistory(transfers, getTransferProgresses);
+
+    const clearAllTransfers = useCallback(() => {
+        clearDownloads();
+        clearUploads();
+    }, [clearDownloads, clearUploads]);
+
+    const latestStats = statsHistory[0];
+
+    if (!latestStats || downloads.length + uploads.length === 0) {
+        return null;
+    }
+
+    return (
+        <TransferManager
+            downloads={downloads}
+            uploads={uploads}
+            statsHistory={statsHistory}
+            onClear={clearAllTransfers}
+            allTransfersFinished={allTransfersFinished}
+        />
+    );
+};
+
+export default TransferManagerContainer;
