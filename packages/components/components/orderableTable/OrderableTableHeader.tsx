8819530feaeacde15762304002866ea407c7f9a5
Jeremy Benaim <jeremybenaim@gmail.com>
2021-07-23 16:43:52 +0100

    Translate OrderableTable component to Typescript Optionally disable useDragHandle prop and disable them for label list Adjust debounce value for label sorting
    MAILWEB-2427

--- packages/components/components/orderableTable/OrderableTableHeader.tsx
+++ packages/components/components/orderableTable/OrderableTableHeader.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import { TableHeader } from '../table';
 import './OrderableTableHeader.scss';
 import { classnames } from '../../helpers';
 
-const OrderableTableHeader = ({ cells = [], className = '', children = null, ...rest }) => (
+interface Props {
+    cells?: React.ReactNode[];
+    className?: string;
+    children?: React.ReactNode;
+}
+
+const OrderableTableHeader = ({ cells = [], className = '', children = null, ...rest }: Props) => (
     <TableHeader
         cells={[
             null, // column for icon
@@ -17,11 +22,5 @@ const OrderableTableHeader = ({ cells = [], className = '', children = null, ...
         {children}
     </TableHeader>
 );
-
-OrderableTableHeader.propTypes = {
-    cells: PropTypes.arrayOf(PropTypes.node),
-    className: PropTypes.string,
-    children: PropTypes.node,
-};
 
 export default OrderableTableHeader;
