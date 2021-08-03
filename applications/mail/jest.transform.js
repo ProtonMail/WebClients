116d927a693d0e94003abd0d6e1d429dde3b4272
Matthias Margot <margotmatthias@protonmail.com>
2021-08-03 15:32:22 +0200

    Have react babel jest config run with automatic
    This parameter seems to be a special addition to support the
    new jsx transform

--- applications/mail/jest.transform.js
+++ applications/mail/jest.transform.js
@@ -1,10 +1,10 @@
 // Custom Jest transform implementation that injects test-specific babel presets.
 module.exports = require('babel-jest').createTransformer({
-    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+    presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }], '@babel/preset-typescript'],
     plugins: [
         '@babel/plugin-proposal-object-rest-spread',
         '@babel/plugin-transform-runtime',
         'transform-require-context',
-        '@babel/plugin-proposal-optional-chaining'
-    ]
+        '@babel/plugin-proposal-optional-chaining',
+    ],
 });
