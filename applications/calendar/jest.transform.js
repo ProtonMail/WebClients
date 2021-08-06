5409aa634f78cb50565b410dd5b13f076f8c13bc
Matthias Margot <margotmatthias@protonmail.com>
2021-08-06 12:08:22 +0200

    Upgrade to jest 27 everywhere

--- applications/calendar/jest.transform.js
+++ applications/calendar/jest.transform.js
@@ -1,5 +1,5 @@
 // Custom Jest transform implementation that injects test-specific babel presets.
-module.exports = require('babel-jest').createTransformer({
+module.exports = require('babel-jest').default.createTransformer({
     presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
     plugins: [
         '@babel/plugin-proposal-object-rest-spread',
