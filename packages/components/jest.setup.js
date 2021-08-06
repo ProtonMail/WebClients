b2b3d21da4e0284c5f04f3778d43caa31573c406
mmso <mmso@mmso.se>
2021-08-06 18:38:49 +0200

    Duplicate jest configs

---
+++ packages/components/jest.setup.js
@@ -0,0 +1,6 @@
+import '@testing-library/react/cleanup-after-each';
+import '@testing-library/jest-dom/extend-expect';
+
+// Silence warnings on expect to throw https://github.com/testing-library/react-testing-library/issues/157
+console.error = () => {};
+console.warn = () => {};
