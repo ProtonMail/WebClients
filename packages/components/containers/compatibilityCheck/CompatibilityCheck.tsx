6d110f5a885d5685fbdaeee58786df6441877bbd
mmso <mmso@mmso.se>
2021-08-06 15:03:25 +0200

    Disallow non aes-gcm browsers
    Certain browsers, like pale moon, do not support AES-GCM encryption.
    Currently the user experience is non-optimal because it fails on login.
    With this change we disable support for this browser.
    
    CP-1390

--- packages/components/containers/compatibilityCheck/CompatibilityCheck.tsx
+++ packages/components/containers/compatibilityCheck/CompatibilityCheck.tsx
@@ -1,5 +1,6 @@
-import { useMemo } from 'react';
+import { useEffect, useState } from 'react';
 import * as React from 'react';
+import { decryptData, encryptData, getKey } from '@proton/shared/lib/authentication/cryptoHelper';
 import { useConfig } from '../../hooks';
 import CompatibilityCheckView from './CompatibilityCheckView';
 import { getCompatibilityList } from './compatibilityCheckHelper';
@@ -10,9 +11,26 @@ interface Props {
 
 const CompatibilityCheck = ({ children }: Props) => {
     const { APP_NAME } = useConfig();
-
-    const incompatibilities = useMemo(() => {
+    const [incompatibilities, setIncompatibilities] = useState(() => {
         return getCompatibilityList().filter(({ valid }) => !valid);
+    });
+
+    useEffect(() => {
+        const run = async () => {
+            const key = await getKey(new Uint8Array(16));
+            const data = await encryptData(key, new Uint8Array(16));
+            return decryptData(key, data);
+        };
+        run().catch(() => {
+            setIncompatibilities([
+                ...incompatibilities,
+                {
+                    name: 'WebCrypto',
+                    valid: false,
+                    text: 'Please update to a browser compatible with AES-GCM encryption.',
+                },
+            ]);
+        });
     }, []);
 
     if (!incompatibilities.length) {
