eaa9f29a602306648081e9657c018de94b566a6a
LeGrosSancho <monsieurk74@gmail.com>
2021-08-09 13:55:52 +0200

    Disable delete all button when searching messages
    Not display the button when searching inside trash, spam or custom folder
    
    MAILWEB-2395

--- applications/mail/src/app/components/toolbar/MoreDropdown.tsx
+++ applications/mail/src/app/components/toolbar/MoreDropdown.tsx
@@ -2,9 +2,12 @@ import { c } from 'ttag';
 import { Icon, DropdownMenu, DropdownMenuButton, ToolbarSeparator, useLoading } from '@proton/components';
 import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
 
+import { useLocation } from 'react-router-dom';
 import ToolbarDropdown from './ToolbarDropdown';
 import { useEmptyLabel } from '../../hooks/useEmptyLabel';
 import { labelIncludes } from '../../helpers/labels';
+import { isSearch } from '../../helpers/elements';
+import { extractSearchParameters } from '../../helpers/mailboxUrl';
 
 const { DRAFTS, ALL_DRAFTS, ALL_MAIL, INBOX, SENT, ALL_SENT, ARCHIVE, STARRED, SCHEDULED } = MAILBOX_LABEL_IDS;
 
@@ -17,6 +20,9 @@ interface Props {
 const MoreDropdown = ({ labelID = '', elementIDs = [], selectedIDs = [] }: Props) => {
     const [loading, withLoading] = useLoading();
     const emptyLabel = useEmptyLabel();
+    const location = useLocation();
+    const searchParameters = extractSearchParameters(location);
+
     const cannotEmpty = labelIncludes(
         labelID,
         INBOX,
@@ -30,7 +36,7 @@ const MoreDropdown = ({ labelID = '', elementIDs = [], selectedIDs = [] }: Props
         SCHEDULED
     );
 
-    if (cannotEmpty) {
+    if (cannotEmpty || isSearch(searchParameters)) {
         return null;
     }
 
