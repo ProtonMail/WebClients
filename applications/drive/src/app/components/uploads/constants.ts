62b2721071effb0e8e05315e3d082c3c653ab735
Michal Horejsek <michal.horejsek@proton.ch>
2021-08-16 15:29:45 +0200

    Upload refactor

---
+++ applications/drive/src/app/components/uploads/constants.ts
@@ -0,0 +1,52 @@
+/**
+ * How many times failed request is retried before giving up and failing
+ * the whole upload.
+ */
+export const MAX_RETRIES_BEFORE_FAIL = 3;
+
+/**
+ * MAX_ENCRYPTED_BLOCKS limits the number of blocks in the buffer before
+ * asking API for next batch of links, if not asked sooner by low buffer
+ * of uploading blocks.
+ */
+export const MAX_ENCRYPTED_BLOCKS = 15;
+
+/**
+ * MAX_UPLOADING_BLOCKS limits the number of blocks in the buffer for
+ * upload. It should be a bit bigger than MAX_UPLOAD_JOBS to ensure that
+ * there is always ready next block when any upload job finishes current
+ * upload. When there is not enough uploading jobs, worker asks for link
+ * creation sooner than it hits the ideal size of MAX_ENCRYPTED_BLOCKS.
+ */
+export const MAX_UPLOADING_BLOCKS = 10;
+
+/**
+ * MAX_BLOCKS_PER_UPLOAD is how many blocks can one upload job have buffered
+ * in memory. Used to count the current load for the total limit of ongoing
+ * upload files by MAX_UPLOAD_BLOCKS_LOAD.
+ */
+export const MAX_BLOCKS_PER_UPLOAD = MAX_ENCRYPTED_BLOCKS + MAX_UPLOADING_BLOCKS;
+
+/**
+ * MAX_UPLOAD_BLOCKS_LOAD limits the number of total blocks being uploaded
+ * at one time. If the queue contains only big files, only few of them at
+ * a time is allowed to limit the memory requirements. If the queue contains
+ * small files, more can be uploaded in parallel. But each upload mean extra
+ * worker. Even though browsers support up to hunderds of web workers, still
+ * it spawns threads.
+ */
+export const MAX_UPLOAD_BLOCKS_LOAD = 20;
+
+/**
+ * How many ongoing uploads there can be. Without http2, we cannot do more
+ * than six parallel requests to one host. With http2 (which we use), there
+ * is theretically no limit, but still we should make a reasonable limit
+ * to not kill users device.
+ */
+export const MAX_UPLOAD_JOBS = 5;
+
+/**
+ * WAIT_TIME is used for pauses between checks, such as to check if buffer is
+ * still full or not, or if the upload is paused, and so on.
+ */
+export const WAIT_TIME = 50; // Miliseconds.
---
+++ applications/drive/src/app/components/uploads/workerController.ts
@@ -0,0 +1,266 @@
+import * as openpgp from 'openpgp';
+import { OpenPGPKey, SessionKey } from 'pmcrypto';
+
+import { EncryptedBlock, EncryptedThumbnailBlock, FileRequestBlock, ThumbnailRequestBlock, Link, BlockToken } from './interface';
+import { getErrorString } from './utils';
+
+type StartMessage = {
+    command: 'start';
+    file: File;
+    thumbnailData?: Uint8Array;
+    addressPrivateKey: Uint8Array;
+    addressEmail: string;
+    privateKey: Uint8Array;
+    sessionKey: SessionKey;
+}
+
+type CreatedBlocksMessage = {
+    command: 'created_blocks';
+    fileLinks: Link[];
+    thumbnailLink?: Link;
+}
+
+type PauseMessage = {
+    command: 'pause';
+}
+
+type ResumeMessage = {
+    command: 'resume';
+}
+
+/**
+ * WorkerControllerEvent contains all possible events which can come from
+ * the main thread to the upload web worker.
+ */
+type WorkerControllerEvent = {
+    data: StartMessage | CreatedBlocksMessage | PauseMessage | ResumeMessage;
+}
+
+/**
+ * WorkerHandlers defines what handlers are available to be used in the upload
+ * web worker to messages from the main thread defined in WorkerControllerEvent.
+ */
+interface WorkerHandlers {
+    start: (file: File, thumbnailData: Uint8Array | undefined, addressPrivateKey: OpenPGPKey, addressEmail: string, privateKey: OpenPGPKey, sessionKey: SessionKey) => void;
+    createdBlocks: (fileLinks: Link[], thumbnailLink?: Link) => void;
+    pause: () => void;
+    resume: () => void;
+}
+
+type CreateBlockMessage = {
+    command: 'create_blocks';
+    fileBlocks: FileRequestBlock[],
+    thumbnailBlock?: ThumbnailRequestBlock,
+}
+
+type ProgressMessage = {
+    command: 'progress';
+    increment: number;
+}
+
+type DoneMessage = {
+    command: 'done';
+    blockTokens: BlockToken[],
+    signature: string,
+    signatureAddress: string,
+}
+
+type ErrorMessage = {
+    command: 'error';
+    error: string;
+}
+
+/**
+ * WorkerEvent contains all possible events which can come from the upload
+ * web worker to the main thread.
+ */
+type WorkerEvent = {
+    data: CreateBlockMessage | ProgressMessage | DoneMessage | ErrorMessage;
+}
+
+/**
+ * WorkerControllerHandlers defines what handlers are available to be used
+ * in the main thread to messages from the upload web worked defined in
+ * WorkerEvent.
+ */
+interface WorkerControllerHandlers {
+    createBlocks: (fileBlocks: FileRequestBlock[], thumbnailBlock?: ThumbnailRequestBlock) => void;
+    onProgress: (increment: number) => void;
+    finalize: (blockTokens: BlockToken[], signature: string, signatureAddress: string) => void;
+    onError: (error: string) => void;
+    onCancel: () => void;
+}
+
+async function readOpenPGPKey(binaryKey: Uint8Array): Promise<OpenPGPKey> {
+    const keys = await openpgp.key.read(binaryKey);
+    if (keys.err) {
+        throw new Error(keys.err.join(', '));
+    }
+    if (keys.keys.length === 0) {
+        throw new Error('Missing key');
+    }
+    return keys.keys[0];
+}
+
+/**
+ * UploadWorker provides communication between the main thread and upload web
+ * worker. The class ensures type safety as much as possible.
+ * UploadWorker is meant to be used on the side of the web worker.
+ */
+export class UploadWorker {
+    worker: Worker;
+
+    constructor(worker: Worker, {start, createdBlocks, pause, resume}: WorkerHandlers) {
+        this.worker = worker;
+        worker.addEventListener('message', ({ data }: WorkerControllerEvent) => {
+            switch (data.command) {
+                case 'start':
+                    (async (data) => {
+                        const addressPrivateKey = await readOpenPGPKey(data.addressPrivateKey);
+                        const privateKey = await readOpenPGPKey(data.privateKey);
+                        start(data.file, data.thumbnailData, addressPrivateKey, data.addressEmail, privateKey, data.sessionKey);
+                    })(data).catch((err) => {
+                        this.postError(err);
+                    });
+                    break;
+                case 'created_blocks':
+                    createdBlocks(data.fileLinks, data.thumbnailLink);
+                    break;
+                case 'pause':
+                    pause();
+                    break;
+                case 'resume':
+                    resume();
+                    break;
+                default:
+                    // Type linters should prevent this error.
+                    throw new Error('Unexpected message');
+            }
+        });
+        worker.addEventListener('error', (event: ErrorEvent) => {
+            this.postError(getErrorString(event.error, event.message));
+        });
+        // @ts-ignore
+        worker.addEventListener('unhandledrejection', (event: PromiseRejectionEvent) => {
+            event.preventDefault();
+            this.postError(event.reason);
+        });
+    }
+
+    postCreateBlocks(fileBlocks: EncryptedBlock[], encryptedThumbnailBlock?: EncryptedThumbnailBlock)  {
+        this.worker.postMessage({
+            command: 'create_blocks',
+            fileBlocks: fileBlocks.map((block) => ({
+                index: block.index,
+                signature: block.signature,
+                size: block.encryptedData.byteLength,
+                hash: block.hash,
+            })),
+            thumbnailBlock: !encryptedThumbnailBlock ? undefined : {
+                size: encryptedThumbnailBlock.encryptedData.byteLength,
+                hash: encryptedThumbnailBlock.hash,
+            },
+        } as CreateBlockMessage);
+    }
+
+    postProgress(increment: number) {
+        this.worker.postMessage({
+            command: 'progress',
+            increment,
+        } as ProgressMessage);
+    }
+
+    postDone(blockTokens: BlockToken[], signature: string, signatureAddress: string) {
+        this.worker.postMessage({
+            command: 'done',
+            blockTokens,
+            signature,
+            signatureAddress,
+        } as DoneMessage);
+    }
+
+    postError(error: string) {
+        this.worker.postMessage({
+            command: 'error',
+            error,
+        } as ErrorMessage);
+    }
+}
+
+/**
+ * UploadWorkerController provides communication between the main thread and
+ * upload web worker. The class ensures type safety as much as possible.
+ * UploadWorkerController is meant to be used on the side of the main thread.
+ */
+export class UploadWorkerController {
+    worker: Worker;
+
+    onCancel: () => void;
+
+    constructor(worker: Worker, {createBlocks, onProgress, finalize, onError, onCancel}: WorkerControllerHandlers) {
+        this.worker = worker;
+        this.onCancel = onCancel;
+        worker.addEventListener('message', ({ data }: WorkerEvent) => {
+            switch (data.command) {
+                case 'create_blocks':
+                    createBlocks(data.fileBlocks, data.thumbnailBlock);
+                    break;
+                case 'progress':
+                    onProgress(data.increment);
+                    break;
+                case 'done':
+                    finalize(data.blockTokens, data.signature, data.signatureAddress);
+                    break;
+                case 'error':
+                    onError(data.error);
+                    break;
+                default:
+                    // Type linters should prevent this error.
+                    throw new Error('Unexpected message');
+            }
+        });
+        worker.addEventListener('error', (event: ErrorEvent) => {
+            onError(getErrorString(event.error, event.message));
+        });
+    }
+
+    terminate() {
+        this.worker.terminate();
+    }
+
+    cancel() {
+        this.onCancel();
+    }
+    
+    postStart(file: File, thumbnailData: Uint8Array | undefined, addressPrivateKey: OpenPGPKey, addressEmail: string, privateKey: OpenPGPKey, sessionKey: SessionKey) {
+        this.worker.postMessage({
+            command: 'start',
+            file,
+            thumbnailData,
+            addressPrivateKey: addressPrivateKey.toPacketlist().write(),
+            addressEmail,
+            privateKey: privateKey.toPacketlist().write(),
+            sessionKey,
+        } as StartMessage);
+    }
+    
+    postCreatedBlocks(fileLinks: Link[], thumbnailLink?: Link) {
+        this.worker.postMessage({
+            command: 'created_blocks',
+            fileLinks,
+            thumbnailLink,
+        } as CreatedBlocksMessage);
+    }
+    
+    postPause() {
+        this.worker.postMessage({
+            command: 'pause',
+        } as PauseMessage);
+    }
+    
+    postResume() {
+        this.worker.postMessage({
+            command: 'resume',
+        } as ResumeMessage);
+    }
+}
