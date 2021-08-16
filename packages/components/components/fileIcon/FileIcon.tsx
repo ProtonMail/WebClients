3b71b4450ae8f1b433ef6cab354cd12250a9e3dc
Matthieu Bué <matthieu.bue@gmail.com>
2021-08-16 15:17:22 +0200

    Fix icon name
    This was a renaming mistake
    
    MAILWEB-2550

--- packages/components/components/fileIcon/FileIcon.tsx
+++ packages/components/components/fileIcon/FileIcon.tsx
@@ -33,7 +33,7 @@ const getIconName = (mimeType: string) => {
         // Exception for XML to use it's own icon and not fallback as text
         name = 'xml';
     } else if (isICS(mimeType)) {
-        name = 'calendar-days';
+        name = 'calendar';
     } else if (isSupportedText(mimeType)) {
         name = 'text';
     } else if (isPDF(mimeType)) {
