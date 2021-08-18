7a71777937e1757d167403d19dcd4449ca4b4c5b
Oleg Voloshin <olegwoloschin@protonmail.com>
2021-08-18 13:15:30 +0300

    Restructure ReactivationModal
    DRVWEB-1021

--- applications/drive/src/app/components/ResolveLockedVolumes/KeyReactivation/KeyReactivationModal.tsx
+++ applications/drive/src/app/components/ResolveLockedVolumes/KeyReactivation/KeyReactivationModal.tsx
@@ -14,11 +14,6 @@ import { APPS } from '@proton/shared/lib/constants';
 import { c } from 'ttag';
 import { noop } from '@proton/shared/lib/helpers/function';
 
-const INFO_TEXT = c('Action').t`In order to access your files, \
-you need to reactivate encryption key
-
-You are also able to delete all of your old files`;
-
 interface Props {
     onClose?: () => void;
     onSubmit?: (event: { type: ReactivationOptions }) => void;
@@ -38,26 +33,7 @@ const OptionLabel = ({ title, info }: { title: string; info: string }) => {
     );
 };
 
-const radioOptions = [
-    {
-        label: (
-            <OptionLabel
-                title={c('Label').t`Re-activate keys`}
-                info={c('Info').t`The existing files will be restored`}
-            />
-        ),
-        value: ReactivationOptions.ReactivateKeys,
-    },
-    {
-        label: (
-            <OptionLabel
-                title={c('Label').t`Delete old files`}
-                info={c('Info').t`The old files will be deleted forever`}
-            />
-        ),
-        value: ReactivationOptions.DeleteOldFiles,
-    },
-];
+const br = <br />;
 
 const KeyReactivationModal = ({ onClose = noop, onSubmit = noop, ...rest }: Props) => {
     const [radioGroupValue, setRadioGroupValue] = useState<number>(ReactivationOptions.ReactivateKeys);
@@ -67,6 +43,32 @@ const KeyReactivationModal = ({ onClose = noop, onSubmit = noop, ...rest }: Prop
     };
 
     const modalTitleID = 'KeyReactivationModal';
+
+    const INFO_TEXT = c('Action').jt`In order to access your files,
+        you need to reactivate encryption key.${br}
+        ${br}
+        You are also able to delete all of your old files.`;
+
+    const radioOptions = [
+        {
+            label: (
+                <OptionLabel
+                    title={c('Label').t`Re-activate keys`}
+                    info={c('Info').t`The existing files will be restored`}
+                />
+            ),
+            value: ReactivationOptions.ReactivateKeys,
+        },
+        {
+            label: (
+                <OptionLabel
+                    title={c('Label').t`Delete old files`}
+                    info={c('Info').t`The old files will be deleted forever`}
+                />
+            ),
+            value: ReactivationOptions.DeleteOldFiles,
+        },
+    ];
 
     return (
         <DialogModal modalTitleID={modalTitleID} onClose={onClose} small {...rest}>
