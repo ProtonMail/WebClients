85d26ba9d1a20ce5755c91bebe7fde7647d9a4dd
Matthieu Lux <matthieu.lux@protonmail.com>
2021-08-25 16:55:18 +0200

    Fix conversion from data url to binary file
    MAILWEB-2563

--- applications/mail/src/app/helpers/message/messageDataUrl.ts
+++ applications/mail/src/app/helpers/message/messageDataUrl.ts
@@ -1,6 +1,5 @@
-import { decodeBase64 } from '@proton/shared/lib/helpers/base64';
 import { generateProtonWebUID } from '@proton/shared/lib/helpers/uid';
-
+import { base64StringToUint8Array } from '@proton/shared/lib/helpers/encoding';
 import { MessageExtended } from '../../models/message';
 import { generateCid } from './messageEmbeddeds';
 
@@ -14,9 +13,10 @@ const dataUrlToFile = (fileName: string, dataUrl: string) => {
 
     // separate out the mime component
     const mimeString = mime.split(':')[1].split(';')[0];
-    // write the bytes of the string to an ArrayBuffer
+
     try {
-        const data = decodeBase64(decodeURIComponent(byte));
+        // write the bytes of the string to an ArrayBuffer
+        const data = base64StringToUint8Array(decodeURIComponent(byte));
         // write the ArrayBuffer to a blob, and you're done
         return new File([data], fileName, { type: mimeString });
     } catch {
