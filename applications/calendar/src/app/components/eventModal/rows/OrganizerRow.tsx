c00f672544b1484cc2e147d5c67c76600782d066
Sergo Vainumäe <sergo.vainumae@protonmail.ch>
2021-08-26 10:23:28 +0200

    Fix organizer row display with long email and name
    CALWEB-2786

--- applications/calendar/src/app/components/eventModal/rows/OrganizerRow.tsx
+++ applications/calendar/src/app/components/eventModal/rows/OrganizerRow.tsx
@@ -22,10 +22,10 @@ const OrganizerRow = ({ model, addresses }: Props) => {
         <div key={email} className={classnames(['address-item flex mb0-25 pl0-5 pr0-5'])}>
             <div className="flex flex-item-fluid p0-5" title={displayFull ? `${name} <${email}>` : email}>
                 {displayFull ? (
-                    <>
+                    <div className="flex flex-nowrap w100">
                         <div className="max-w50 text-ellipsis">{name}</div>
                         <div className="ml0-25 max-w50 text-ellipsis">{`<${email}>`}</div>
-                    </>
+                    </div>
                 ) : (
                     <div className="max-w100 text-ellipsis">{email}</div>
                 )}
