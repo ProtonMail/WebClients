2afa3fdb30729604d024f1223780207a716bf5c4
Matthias Margot <margotmatthias@protonmail.com>
2021-08-31 11:15:56 +0200

    Add default values parameters to verification
    CP-2489

--- packages/components/containers/verification/EmbeddedVerification.tsx
+++ packages/components/containers/verification/EmbeddedVerification.tsx
@@ -1,68 +1,80 @@
 import { useEffect, useMemo, useState } from 'react';
 import { APPS, APPS_CONFIGURATION } from '@proton/shared/lib/constants';
 import { HumanVerificationMethodType } from '@proton/shared/lib/interfaces';
-import { message } from '../../../shared/lib/sanitize';
+import { stringifySearchParams } from '@proton/shared/lib/helpers/url';
 
 interface EmbeddedVerificationProps {
-  token: string;
-  methods: HumanVerificationMethodType [];
-  onSuccess: (token: string, tokenType: HumanVerificationMethodType) => void;
+    token: string;
+    methods: HumanVerificationMethodType[];
+    defaultCountry?: string;
+    defaultEmail?: string;
+    defaultPhone?: string;
+    onSuccess: (token: string, tokenType: HumanVerificationMethodType) => void;
 }
 
-const EmbeddedVerification = ({ token, methods, onSuccess }: EmbeddedVerificationProps) => {
-  const [iframeHeight, setIframeHeight] = useState<number | undefined>();
+const EmbeddedVerification = ({
+    token,
+    methods,
+    onSuccess,
+    defaultCountry,
+    defaultEmail,
+    defaultPhone,
+}: EmbeddedVerificationProps) => {
+    const [iframeHeight, setIframeHeight] = useState<number | undefined>();
 
-  const embedUrl = useMemo(
-    () => {
-      const url = new URL(window.location.origin)
-      const segments = url.host.split('.')
-      segments[0] = APPS_CONFIGURATION[APPS.PROTONVERIFICATION].subdomain
-      url.hostname = segments.join('.')
+    const embedUrl = useMemo(() => {
+        const url = new URL(window.location.origin);
+        const segments = url.host.split('.');
+        segments[0] = APPS_CONFIGURATION[APPS.PROTONVERIFICATION].subdomain;
+        url.hostname = segments.join('.');
 
-      return url
-    },
-    []
-  )
+        return url;
+    }, []);
 
-  const handleMessage = (e: MessageEvent) => {
-    if (e.origin !== embedUrl.origin) {
-      /*
-       * Post message from unknown origin, exit to make sure
-       * this handler doesn't do anything with it
-       */
-      return
-    }
+    const handleMessage = (e: MessageEvent) => {
+        if (e.origin !== embedUrl.origin) {
+            /*
+             * Post message from unknown origin, exit to make sure
+             * this handler doesn't do anything with it
+             */
+            return;
+        }
 
-    switch (e.data.type) {
-      case 'verification-height': {
-        setIframeHeight(e.data.height);
-        break
-      }
+        switch (e.data.type) {
+            case 'verification-height': {
+                setIframeHeight(e.data.height);
+                break;
+            }
 
-      case 'verification-success': {
-        onSuccess(e.data.payload.token, e.data.payload.tokenType)
-        break
-      }
+            case 'verification-success': {
+                onSuccess(e.data.payload.token, e.data.payload.tokenType);
+                break;
+            }
 
-      default:
-    }
-  }
+            default:
+        }
+    };
 
-  useEffect(() => {
-    window.addEventListener('message', handleMessage);
+    useEffect(() => {
+        window.addEventListener('message', handleMessage);
 
-    return () => {
-      window.removeEventListener('message', handleMessage);
-    }
-  }, []);
+        return () => {
+            window.removeEventListener('message', handleMessage);
+        };
+    }, []);
 
-  return (
-    <iframe
-        style={{ height: `${iframeHeight}px`, width: '100%' }}
-        src={`${embedUrl.toString()}?methods=${methods.join(',')}&token=${token}&origin=${window.location.origin}`}
-        title="verification-iframe"
-    />
-  )
-}
+    const params = {
+        methods,
+        token,
+        origin: window.location.origin,
+        defaultCountry,
+        defaultEmail,
+        defaultPhone,
+    };
 
-export default EmbeddedVerification
+    const src = `${embedUrl.toString()}?${stringifySearchParams(params)}`;
+
+    return <iframe style={{ height: `${iframeHeight}px`, width: '100%' }} src={src} title="verification-iframe" />;
+};
+
+export default EmbeddedVerification;
