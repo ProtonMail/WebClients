2e775f9adc2fa65f681b45566666293f2d4d1e37
mmso <mmso@mmso.se>
2021-09-03 13:31:26 +0200

    Use built-in webpack5 mechanism to load the service worker

--- applications/drive/typings/index.d.ts
+++ applications/drive/typings/index.d.ts
@@ -1,15 +1,3 @@
 declare module '*.png';
 
 declare module '@transcend-io/conflux';
-
-declare module 'service-worker-loader*' {
-    import {
-        ServiceWorkerRegister,
-        ScriptUrl as scriptUrl,
-        ServiceWorkerNoSupportError,
-    } from 'service-worker-loader/types.d';
-
-    const register: ServiceWorkerRegister;
-    export default register;
-    export { ServiceWorkerNoSupportError, scriptUrl };
-}
