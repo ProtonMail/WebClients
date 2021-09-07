405caec79de5e9deb89d6046beeb4676162a4b5d
econdepe <econdepe@protonmail.com>
2021-09-07 11:59:56 +0200

    Rework participants list

--- applications/mail/src/app/components/message/extras/calendar/ExtraEventParticipants.tsx
+++ applications/mail/src/app/components/message/extras/calendar/ExtraEventParticipants.tsx
@@ -1,10 +1,8 @@
 import { LinkButton } from '@proton/components';
 import { Participant } from '@proton/shared/lib/interfaces/calendar';
 import { useState } from 'react';
-import { c } from 'ttag';
+import { c, msgid } from 'ttag';
 import ExtraEventParticipant from './ExtraEventParticipant';
-
-const DEFAULT_NUMBER_VISIBLE_PARTICIPANTS = 5;
 
 interface Props {
     list: Participant[];
@@ -16,25 +14,50 @@ const ExtraEventParticipants = ({ list }: Props) => {
         return null;
     }
 
-    const totalParticipants = list.length;
-    const totalHiddenParticipants = totalParticipants - DEFAULT_NUMBER_VISIBLE_PARTICIPANTS;
-    const listOfVisibleParticipants = isShowingMore ? list : list.slice(0, DEFAULT_NUMBER_VISIBLE_PARTICIPANTS);
+    const [organizer, ...attendees] = list;
+    const totalAttendees = attendees.length;
+    const toggleShowMore = () => setIsShowingMore((prevState) => !prevState);
+
+    if (totalAttendees === 1) {
+        return (
+            <>
+                {list.map((participant, i) => {
+                    return (
+                        <ExtraEventParticipant
+                            participant={participant}
+                            isOrganizer={i === 0}
+                            key={participant.emailAddress}
+                        />
+                    );
+                })}
+            </>
+        );
+    }
 
     return (
         <>
-            {listOfVisibleParticipants.map((participant, i) => {
-                return (
-                    <ExtraEventParticipant
-                        participant={participant}
-                        isOrganizer={i === 0}
-                        key={participant.emailAddress}
-                    />
-                );
-            })}
-            {totalParticipants > DEFAULT_NUMBER_VISIBLE_PARTICIPANTS && (
-                <LinkButton onClick={() => setIsShowingMore((prevState) => !prevState)}>
-                    {isShowingMore ? c('Action').t`Show less` : c('Action').t`Show ${totalHiddenParticipants} more`}
-                </LinkButton>
+            <ExtraEventParticipant participant={organizer} isOrganizer />
+            {!isShowingMore && (
+                <div className="flex flex-align-items-center">
+                    <span className="mr0-5">
+                        {c('ICS widget label for event details').ngettext(
+                            msgid`${totalAttendees} Participant`,
+                            `${totalAttendees} Participants`,
+                            totalAttendees
+                        )}
+                    </span>
+                    <LinkButton onClick={toggleShowMore}>{c('Action').t`Show`}</LinkButton>
+                </div>
+            )}
+            {isShowingMore &&
+                attendees.map((participant) => {
+                    return <ExtraEventParticipant participant={participant} key={participant.emailAddress} />;
+                })}
+            {}
+            {isShowingMore && (
+                <div>
+                    <LinkButton onClick={toggleShowMore}>{c('Action').t`Show less`}</LinkButton>
+                </div>
             )}
         </>
     );
