b2b0b2068308ee360a57c67bb4234be2a42dbce4
LeGrosSancho <monsieurk74@gmail.com>
2021-09-14 15:37:40 +0200

    Workaround to have a plural form in a string with a strong tag
    In selection pane, we need to display a tag and having a plural form which is not possible with ttag
    Instead of having the tag directly in a string, we are surrounding the bold part with ** so that we can have plural forms
    Then, from the code we are able to replace ** by a strong tag
    
    MAILWEB-2586

--- applications/mail/src/app/components/view/SelectionPane.tsx
+++ applications/mail/src/app/components/view/SelectionPane.tsx
@@ -33,44 +33,94 @@ const SelectionPane = ({ labelID, mailSettings, location, labelCount, checkedIDs
 
     const count = checkeds || total;
 
-    const countOfConversationOrMessage = conversationMode ? (
-        <strong key="conversation">
-            {c('Info').ngettext(msgid`${count} conversation`, `${count} conversations`, count)}
-        </strong>
-    ) : (
-        <strong key="email">{c('Info').ngettext(msgid`${count} message`, `${count} messages`, count)}</strong>
-    );
-
-    // Need to have 2 similar strings for each case, in order to translate them correctly depending on message and conversation gender
+    /*
+     * With ttag we cannot have JSX in plural forms
+     * We need a workaround to have the number of messages/conversations in a strong tag
+     * So we are surrounding the bold part with ** to replace it later by a strong tag
+     */
     const getFolderText = () => {
         if (checkeds) {
-            // translator: the variable is an HTML tag containing the number of conversations selected, already formatted in plural form - ex: 1 conversation, 2 conversations
+            // translator: To have plural forms AND a part in bold, we need to surround the bold part with "**" so that we can replace it by a <strong> tag in the code. Here, "{count} conversation/s" will be bold. You need to put them in your translation too.
             return conversationMode
-                ? c('Info').jt`You selected ${countOfConversationOrMessage} from this folder`
-                : // translator: the variable is an HTML tag containing the number of messages selected, already formatted in plural form - ex: 1 message, 2 messages
-                  c('Info').jt`You selected ${countOfConversationOrMessage} from this folder`;
+                ? c('Info').ngettext(
+                      msgid`You selected **${count} conversation** from this folder`,
+                      `You selected **${count} conversations** from this folder`,
+                      count
+                  )
+                : // translator: To have plural forms AND a part in bold, we need to surround the bold part with "**" so that we can replace it by a <strong> tag in the code. Here, "{count} message/s" will be bold. You need to put them in your translation too.
+                  c('Info').ngettext(
+                      msgid`You selected **${count} message** from this folder`,
+                      `You selected **${count} messages** from this folder`,
+                      count
+                  );
         }
-        // translator: the variable is an HTML tag containing the number of conversations stored, already formatted in plural form - ex: 1 conversation, 2 conversations
+        // translator: To have plural forms AND a part in bold, we need to surround the bold part with "**" so that we can replace it by a <strong> tag in the code. Here, "{count} conversation/s" will be bold. You need to put them in your translation too.
         return conversationMode
-            ? c('Info').jt`You have ${countOfConversationOrMessage} stored in this folder`
-            : // translator: the variable is an HTML tag containing the number of conversations stored, already formatted in plural form - ex: 1 message, 2 messages
-              c('Info').jt`You have ${countOfConversationOrMessage} stored in this folder`;
+            ? c('Info').ngettext(
+                  msgid`You have **${count} conversation** stored in this folder`,
+                  `You have **${count} conversations** stored in this folder`,
+                  count
+              )
+            : // translator: To have plural forms AND a part in bold, we need to surround the bold part with "**" so that we can replace it by a <strong> tag in the code. Here, "{count} message/s" will be bold. You need to put them in your translation too.
+              c('Info').ngettext(
+                  msgid`You have **${count} message** stored in this folder`,
+                  `You have **${count} messages** stored in this folder`,
+                  count
+              );
     };
 
-    // Need to have 2 similar strings for each case, in order to translate them correctly depending on message and conversation gender
+    /*
+     * With ttag we cannot have JSX in plural forms
+     * We need a workaround to have the number of messages/conversations in a strong tag
+     * So we are surrounding the bold part with ** to replace it later by a strong tag
+     */
     const getLabelText = () => {
         if (checkeds) {
-            // translator: the variable is an HTML tag containing the number of conversations selected, already formatted in plural form - ex: 1 conversation, 2 conversations
+            // translator: To have plural forms AND a part in bold, we need to surround the bold part with "**" so that we can replace it by a <strong> tag in the code. Here, "{count} conversation/s" will be bold. You need to put them in your translation too.
             return conversationMode
-                ? c('Info').jt`You selected ${countOfConversationOrMessage} with this label`
-                : // translator: the variable is an HTML tag containing the number of messages selected, already formatted in plural form - ex: 1 message, 2 messages
-                  c('Info').jt`You selected ${countOfConversationOrMessage} with this label`;
+                ? c('Info').ngettext(
+                      msgid`You selected **${count} conversation** with this label`,
+                      `You selected **${count} conversations** with this label`,
+                      count
+                  )
+                : // translator: To have plural forms AND a part in bold, we need to surround the bold part with "**" so that we can replace it by a <strong> tag in the code. Here, "{count} message/s" will be bold. You need to put them in your translation too.
+                  c('Info').ngettext(
+                      msgid`You selected **${count} message** with this label`,
+                      `You selected **${count} messages** with this label`,
+                      count
+                  );
         }
-        // translator: the variable is an HTML tag containing the number of conversations tagged, already formatted in plural form - ex: 1 conversation, 2 conversations
+        // translator: To have plural forms AND a part in bold, we need to surround the bold part with "**" so that we can replace it by a <strong> tag in the code. Here, "{count} conversation/s" will be bold. You need to put them in your translation too.
         return conversationMode
-            ? c('Info').jt`You have ${countOfConversationOrMessage} tagged with this label`
-            : // translator: the variable is an HTML tag containing the number of messages tagged, already formatted in plural form - ex: 1 message, 2 messages
-              c('Info').jt`You have ${countOfConversationOrMessage} tagged with this label`;
+            ? c('Info').ngettext(
+                  msgid`You have **${count} conversation** tagged with this label`,
+                  `You have **${count} conversations** tagged with this label`,
+                  count
+              )
+            : // translator: To have plural forms AND a part in bold, we need to surround the bold part with "**" so that we can replace it by a <strong> tag in the code. Here, "{count} message/s" will be bold. You need to put them in your translation too.
+              c('Info').ngettext(
+                  msgid`You have **${count} message** tagged with this label`,
+                  `You have **${count} messages** tagged with this label`,
+                  count
+              );
+    };
+
+    // Replace "**" by strong tags
+    const getFormattedText = (text: string) => {
+        const splitText = text.split('**');
+        // If the length is odd, it means that we can apply the change on the text
+        if (splitText.length > 2 && splitText.length % 2 !== 0) {
+            const formattedText = splitText.map((s, index) => {
+                // All odd indexes corresponds to a part to surround with a tag
+                if (index % 2 !== 0) {
+                    return <strong key={`formattedText-${s}`}>{s}</strong>;
+                }
+                return <span key={`formattedText-${s}`}>{s}</span>;
+            });
+
+            return <>{formattedText}</>;
+        }
+        return text;
     };
 
     const text = isCustomLabel ? getLabelText() : getFolderText();
@@ -84,7 +134,7 @@ const SelectionPane = ({ labelID, mailSettings, location, labelCount, checkedIDs
                     {labelName}
                 </h3>
             )}
-            <p className="mb2 text-keep-space">{showText ? text : null}</p>
+            <p className="mb2 text-keep-space">{showText ? getFormattedText(text) : null}</p>
             <div className="mb2">
                 <img
                     src={conversationSvg}
