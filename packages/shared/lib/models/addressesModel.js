c214df4816ae77cd1ec5b152d31b856e42d06a3c
mmso <mmso@mmso.se>
2021-09-16 18:00:06 +0200

    Paginate get addresses request

--- packages/shared/lib/models/addressesModel.js
+++ packages/shared/lib/models/addressesModel.js
@@ -1,8 +1,27 @@
 import { queryAddresses } from '../api/addresses';
+import queryPagesThrottled from '../api/helpers/queryPagesThrottled';
 import updateCollection from '../helpers/updateCollection';
 
 export const getAddressesModel = (api) => {
-    return api(queryAddresses()).then(({ Addresses }) => Addresses);
+    const pageSize = 100;
+
+    const requestPage = (page) => {
+        return api(
+            queryAddresses({
+                Page: page,
+                PageSize: pageSize,
+            })
+        );
+    };
+
+    return queryPagesThrottled({
+        requestPage,
+        pageSize,
+        pagesPerChunk: 10,
+        delayPerChunk: 100,
+    }).then((pages) => {
+        return pages.map(({ Addresses }) => Addresses).flat();
+    });
 };
 
 export const AddressesModel = {
