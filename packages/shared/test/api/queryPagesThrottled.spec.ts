9bdc948837f203d61d814344d1309652930f38f8
mmso <mmso@mmso.se>
2021-09-17 12:14:34 +0200

    Set default total if none

---
+++ packages/shared/test/api/queryPagesThrottled.spec.ts
@@ -0,0 +1,53 @@
+import queryPagesThrottled from '../../lib/api/helpers/queryPagesThrottled';
+
+describe('query pages', () => {
+    it('should be able to process multiple pages', async () => {
+        const result = await queryPagesThrottled({
+            requestPage: async (n) => {
+                return {
+                    Value: n,
+                    Total: 5,
+                };
+            },
+            pageSize: 1,
+            pagesPerChunk: 1,
+            delayPerChunk: 1,
+        }).then((pages) => {
+            return pages.map(({ Value }) => Value);
+        });
+        expect(result).toEqual([0, 1, 2, 3, 4]);
+    });
+
+    it('should return the first page without a total', async () => {
+        const result = await queryPagesThrottled({
+            requestPage: async () => {
+                return {
+                    Value: 0,
+                } as any;
+            },
+            pageSize: 100,
+            pagesPerChunk: 20,
+            delayPerChunk: 1,
+        }).then((pages) => {
+            return pages.map(({ Value }) => Value);
+        });
+        expect(result).toEqual([0]);
+    });
+
+    it('should return several pages', async () => {
+        const result = await queryPagesThrottled({
+            requestPage: async (n) => {
+                return {
+                    Value: n,
+                    Total: 4,
+                };
+            },
+            pageSize: 2,
+            pagesPerChunk: 20,
+            delayPerChunk: 1,
+        }).then((pages) => {
+            return pages.map(({ Value }) => Value);
+        });
+        expect(result).toEqual([0, 1]);
+    });
+});
