e581cfad5d94f0c5fab88400ddd1fc3cfe963a42
mmso <mmso@mmso.se>
2021-09-17 12:09:41 +0200

    Paginate get members request

--- packages/shared/lib/models/membersModel.js
+++ packages/shared/lib/models/membersModel.js
@@ -1,8 +1,27 @@
 import { queryMembers } from '../api/members';
 import updateCollection from '../helpers/updateCollection';
+import queryPagesThrottled from '../api/helpers/queryPagesThrottled';
 
 export const getMembersModel = (api) => {
-    return api(queryMembers()).then(({ Members }) => Members);
+    const pageSize = 100;
+
+    const requestPage = (page) => {
+        return api(
+            queryMembers({
+                Page: page,
+                PageSize: pageSize,
+            })
+        );
+    };
+
+    return queryPagesThrottled({
+        requestPage,
+        pageSize,
+        pagesPerChunk: 10,
+        delayPerChunk: 100,
+    }).then((pages) => {
+        return pages.map(({ Members }) => Members).flat();
+    });
 };
 
 export const MembersModel = {
