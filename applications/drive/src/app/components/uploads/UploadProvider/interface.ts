54d3a1c69d4a14bfad68c697ff394d453eab801e
Michal Horejsek <michal.horejsek@proton.ch>
2021-09-22 08:15:38 +0200

    Set proper state to added new uploads

--- applications/drive/src/app/components/uploads/UploadProvider/interface.ts
+++ applications/drive/src/app/components/uploads/UploadProvider/interface.ts
@@ -44,7 +44,7 @@ export interface FolderUploadReady extends FolderUpload {
 
 export interface UploadQueue {
     shareId: string;
-    parentId: string;
+    linkId: string;
     files: FileUpload[];
     folders: FolderUpload[];
 }
