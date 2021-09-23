717a53ac94af4efc1addff3b549e22a4a28ef5a1
Oleg Voloshin <olegwoloschin@protonmail.com>
2021-09-23 12:02:29 +0300

    Update back button title
    DRVWEB-1520

--- applications/drive/src/app/components/sections/Drive/ToolbarButtons/BackButton.tsx
+++ applications/drive/src/app/components/sections/Drive/ToolbarButtons/BackButton.tsx
@@ -1,9 +1,11 @@
 import { c } from 'ttag';
-
-import { Icon, ToolbarButton } from '@proton/components';
+import { Icon, ToolbarButton, FileNameDisplay } from '@proton/components';
+import { useEffect, useState } from 'react';
 
 import useNavigate from '../../../../hooks/drive/useNavigate';
+import useWorkingDirectory from '../../../../hooks/drive/useWorkingDirectory';
 import { LinkType } from '../../../../interfaces/link';
+import useDrive from '../../../../hooks/drive/useDrive';
 
 interface Props {
     shareId: string;
@@ -13,16 +15,44 @@ interface Props {
 
 const BackButton = ({ shareId, parentLinkId, disabled }: Props) => {
     const { navigateToLink } = useNavigate();
+    const { getLinkMeta } = useDrive();
+    const path = useWorkingDirectory();
+
+    const [parentLinkName, setParentLinkName] = useState<string | null>(null);
     const handleBackClick = () => {
         if (parentLinkId) {
             navigateToLink(shareId, parentLinkId, LinkType.FOLDER);
         }
     };
 
+    useEffect(() => {
+        setParentLinkName(null);
+
+        if (!parentLinkId) {
+            return;
+        }
+
+        getLinkMeta(shareId, parentLinkId)
+            .then((meta) => {
+                setParentLinkName(path.getLinkName(meta));
+            })
+            .catch(console.error);
+    }, [shareId, parentLinkId]);
+
+    const getTitleText = () => {
+        if (!parentLinkName) {
+            return c('Label').t`Up to the parent folder}`;
+        }
+
+        return c('Label').t`Up to ${parentLinkName}`;
+    };
+
+    const title = parentLinkId && <FileNameDisplay className="w100" text={getTitleText()} />;
+
     return (
         <ToolbarButton
             disabled={disabled}
-            title={c('Action').t`Back`}
+            title={title}
             onClick={handleBackClick}
             data-testid="toolbar-back"
             icon={<Icon name="arrow-side-up" className="on-rtl-mirror" />}
