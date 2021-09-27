4ed05edef0418fc9374a2d066e530f4990afa70f
LeGrosSancho <monsieurk74@gmail.com>
2021-09-27 18:02:15 +0200

    Added loading remote images through proxy test
    MAILWEB-2577

--- applications/mail/src/app/helpers/transforms/tests/transformRemote.test.ts
+++ applications/mail/src/app/helpers/transforms/tests/transformRemote.test.ts
@@ -1,19 +1,15 @@
-import { SHOW_IMAGES } from '@proton/shared/lib/constants';
+import { IMAGE_PROXY_FLAGS, SHOW_IMAGES } from '@proton/shared/lib/constants';
 import { MailSettings } from '@proton/shared/lib/interfaces';
 import { Message } from '@proton/shared/lib/interfaces/mail/Message';
+import { wait } from '@proton/shared/lib/helpers/promise';
 import { MessageExtended } from '../../../models/message';
 import { messageCache } from '../../test/cache';
-import { api } from '../../test/api';
+import { addApiMock, api } from '../../test/api';
 import { transformRemote } from '../transformRemote';
 import { createDocument } from '../../test/message';
 
-const mailSettings = {
-    ShowImages: SHOW_IMAGES.REMOTE,
-    // ImageProxy: IMAGE_PROXY_FLAGS.PROXY
-} as MailSettings;
-
 describe('transformEmbedded', () => {
-    const setup = (message: MessageExtended) => {
+    const setup = (message: MessageExtended, mailSettings: MailSettings) => {
         return transformRemote(message, mailSettings, api, messageCache);
     };
 
@@ -31,11 +27,49 @@ describe('transformEmbedded', () => {
             document: createDocument(content),
         };
 
-        const { showRemoteImages, remoteImages, hasRemoteImages } = await setup(message);
+        const mailSettings = {
+            ShowImages: SHOW_IMAGES.REMOTE,
+        } as MailSettings;
+
+        const { showRemoteImages, remoteImages, hasRemoteImages } = await setup(message, mailSettings);
 
         expect(showRemoteImages).toBeTruthy();
         expect(hasRemoteImages).toBeTruthy();
         expect(remoteImages[0].type).toEqual('remote');
         expect(remoteImages[0].url).toEqual(imageURL);
+    });
+
+    it('should load remote images through proxy', async () => {
+        const proxyCall = jest.fn();
+
+        addApiMock('images', proxyCall, 'get');
+
+        const imageURL = 'imageURL';
+        const content = `<div>
+                            <img proton-src='${imageURL}'/>
+                        </div>`;
+
+        const message: MessageExtended = {
+            localID: 'messageWithEmbedded',
+            data: {
+                ID: 'messageID',
+            } as Message,
+            document: createDocument(content),
+        };
+
+        const mailSettings = {
+            ShowImages: SHOW_IMAGES.REMOTE,
+            ImageProxy: IMAGE_PROXY_FLAGS.PROXY,
+        } as MailSettings;
+
+        const { showRemoteImages, remoteImages, hasRemoteImages } = await setup(message, mailSettings);
+
+        await wait(2000);
+
+        expect(showRemoteImages).toBeTruthy();
+        expect(hasRemoteImages).toBeTruthy();
+        expect(remoteImages[0].type).toEqual('remote');
+        expect(remoteImages[0].url).toEqual(imageURL);
+        expect(proxyCall).toHaveBeenCalled();
     });
 });
