c775bdc7795a0f2baf5f638bda82e22d1cc488b5
mmso <mmso@mmso.se>
2021-09-30 12:31:26 +0200

    Assert key updates are still decryptable

--- packages/components/containers/app/KeyBackgroundManager.tsx
+++ packages/components/containers/app/KeyBackgroundManager.tsx
@@ -8,9 +8,11 @@ import {
     migrateMemberAddressKeys,
     getHasKeyMigrationRunner,
     getHasMigratedAddressKeys,
+    getDecryptedUserKeysHelper,
 } from '@proton/shared/lib/keys';
 import { traceError } from '@proton/shared/lib/helpers/sentry';
 import { USER_ROLES } from '@proton/shared/lib/constants';
+import { User } from '@proton/shared/lib/interfaces';
 
 import {
     useAuthentication,
@@ -43,7 +45,7 @@ const KeyBackgroundManager = ({
     const getAddressKeys = useGetAddressKeys();
     const getOrganization = useGetOrganization();
     const authentication = useAuthentication();
-    const { call } = useEventManager();
+    const { subscribe, call } = useEventManager();
     const normalApi = useApi();
     const silentApi = <T,>(config: any) => normalApi<T>({ ...config, silence: true });
     const keyMigrationFeature = useFeature(FeatureCode.KeyMigration, false);
@@ -118,6 +120,28 @@ const KeyBackgroundManager = ({
         run()
             .then(() => (hasMemberKeyMigration ? runMigration() : undefined))
             .catch(noop);
+    }, []);
+
+    useEffect(() => {
+        const assertDecryptable = async (user: User) => {
+            if (!user?.Keys?.length) {
+                return;
+            }
+            // This asserts that when a key update arrives, the client still has the correct key password for it.
+            // Otherwise, it may invalidly be used. This is dangerous because it could potentially break keys.
+            // One example where this happened was during a password reset where from the client perspective
+            // it looked like an API call to update user keys failed (timed out) however, it actually succeeded
+            // and the client kept using the old password. Since this is rare and should not happen, we just
+            // sign out the user in lack of a better UX.
+            const copy = { ...user, Keys: [user.Keys[0]] }; // Only interested in the primary key.
+            const result = await getDecryptedUserKeysHelper(copy, authentication.getPassword());
+            if (!result.length) {
+                authentication.logout();
+            }
+        };
+        return subscribe(({ User }) => {
+            assertDecryptable(User).catch(noop);
+        });
     }, []);
 
     return <>{null}</>;
