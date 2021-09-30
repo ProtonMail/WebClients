204ba1ed2ec56e953de87af954b10500e7c7df56
mmso <mmso@mmso.se>
2021-09-30 22:01:26 +0200

    Simplify query pages api

--- packages/shared/lib/api/helpers/queryPages.ts
+++ packages/shared/lib/api/helpers/queryPages.ts
@@ -1,19 +1,17 @@
 import { runChunksDelayed } from '../../helpers/promise';
 import { chunk } from '../../helpers/array';
 
-interface Arguments<T> {
-    requestPage: (page: number) => Promise<T>;
+interface Arguments {
     pageSize: number;
     pagesPerChunk: number;
     delayPerChunk: number;
 }
-const queryPagesThrottled = async <T extends { Total: number }>({
-    requestPage,
-    pageSize,
-    pagesPerChunk,
-    delayPerChunk,
-}: Arguments<T>) => {
-    const firstPage = await requestPage(0);
+
+const queryPages = async <T extends { Total: number }>(
+    requestPage: (page: number, pageSize: number) => Promise<T>,
+    { pageSize = 50, pagesPerChunk = 10, delayPerChunk = 100 }: Partial<Arguments> = {}
+) => {
+    const firstPage = await requestPage(0, pageSize);
     const n = Math.ceil((firstPage?.Total || 0) / pageSize) - 1; // First page already loaded
 
     if (n <= 0) {
@@ -23,9 +21,10 @@ const queryPagesThrottled = async <T extends { Total: number }>({
     const pages = Array.from({ length: n }, (a, i) => i + 1);
     const chunks = chunk(pages, pagesPerChunk);
 
-    const restPages = chunks.length > 0 ? await runChunksDelayed(chunks, requestPage, delayPerChunk) : [];
+    const restPages =
+        chunks.length > 0 ? await runChunksDelayed(chunks, (idx) => requestPage(idx, pageSize), delayPerChunk) : [];
 
     return [firstPage].concat(restPages);
 };
 
-export default queryPagesThrottled;
+export default queryPages;
