ee5d06c2aa332640ca18092310d28dc748ade030
Michal Horejsek <michal.horejsek@proton.ch>
2021-09-30 14:42:44 +0200

    Add block sizes to extended attributes

--- applications/drive/src/app/utils/drive/extendedAttributes.test.ts
+++ applications/drive/src/app/utils/drive/extendedAttributes.test.ts
@@ -1,3 +1,4 @@
+import { FILE_CHUNK_SIZE } from '../../constants';
 import { mockGlobalFile, testFile } from '../../helpers/test/file';
 import { createExtendedAttributes, parseExtendedAttributes } from './extendedAttributes';
 
@@ -5,6 +6,7 @@ const emptyExtendedAttributes = {
     Common: {
         ModificationTime: undefined,
         Size: undefined,
+        BlockSizes: undefined,
     },
 };
 
@@ -14,13 +16,31 @@ describe('extended attrbiutes', () => {
     });
 
     it('creates the struct from the file', () => {
-        const file = testFile('testfile.txt', 123);
-        const xattrs = createExtendedAttributes(file);
-        expect(xattrs).toMatchObject({
-            Common: {
-                ModificationTime: '2009-02-13T23:31:30.000Z',
-                Size: 123,
-            },
+        const testCases: [File, object][] = [
+            [
+                testFile('testfile.txt', 123),
+                {
+                    Common: {
+                        ModificationTime: '2009-02-13T23:31:30.000Z',
+                        Size: 123,
+                        BlockSizes: [123],
+                    },
+                },
+            ],
+            [
+                testFile('testfile.txt', FILE_CHUNK_SIZE * 2 + 123),
+                {
+                    Common: {
+                        ModificationTime: '2009-02-13T23:31:30.000Z',
+                        Size: FILE_CHUNK_SIZE * 2 + 123,
+                        BlockSizes: [FILE_CHUNK_SIZE, FILE_CHUNK_SIZE, 123],
+                    },
+                },
+            ],
+        ];
+        testCases.forEach(([input, expectedAttributes]) => {
+            const xattrs = createExtendedAttributes(input);
+            expect(xattrs).toMatchObject(expectedAttributes);
         });
     });
 
@@ -35,6 +55,7 @@ describe('extended attrbiutes', () => {
                     Common: {
                         ModificationTime: 1234567890,
                         Size: undefined,
+                        BlockSizes: undefined,
                     },
                 },
             ],
@@ -44,15 +65,17 @@ describe('extended attrbiutes', () => {
                     Common: {
                         ModificationTime: undefined,
                         Size: 123,
+                        BlockSizes: undefined,
                     },
                 },
             ],
             [
-                '{"Common": {"ModificationTime": "2009-02-13T23:31:30+0000", "Size": 123}}',
+                '{"Common": {"ModificationTime": "2009-02-13T23:31:30+0000", "Size": 123, "BlockSizes": [1, 2, 3]}}',
                 {
                     Common: {
                         ModificationTime: 1234567890,
                         Size: 123,
+                        BlockSizes: [1, 2, 3],
                     },
                 },
             ],
@@ -62,6 +85,7 @@ describe('extended attrbiutes', () => {
                     Common: {
                         ModificationTime: undefined,
                         Size: 123,
+                        BlockSizes: undefined,
                     },
                 },
             ],
@@ -71,6 +95,17 @@ describe('extended attrbiutes', () => {
                     Common: {
                         ModificationTime: 1234567890,
                         Size: undefined,
+                        BlockSizes: undefined,
+                    },
+                },
+            ],
+            [
+                '{"Common": {"ModificationTime": "2009-02-13T23:31:30+0000", "BlockSizes": "aaa"}}',
+                {
+                    Common: {
+                        ModificationTime: 1234567890,
+                        Size: undefined,
+                        BlockSizes: undefined,
                     },
                 },
             ],
