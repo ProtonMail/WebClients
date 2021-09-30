e5c5e316494ed09a2381390d839ed804be3fd18e
Michal Horejsek <michal.horejsek@proton.ch>
2021-09-30 14:42:44 +0200

    Add block sizes to extended attributes

--- applications/drive/src/app/utils/drive/extendedAttributes.ts
+++ applications/drive/src/app/utils/drive/extendedAttributes.ts
@@ -2,10 +2,13 @@ import { encryptMessage, OpenPGPKey } from 'pmcrypto';
 
 import { decryptUnsigned } from '@proton/shared/lib/keys/driveKeys';
 
+import { FILE_CHUNK_SIZE } from '../../constants';
+
 interface ExtendedAttributes {
     Common: {
         ModificationTime: string;
         Size: number;
+        BlockSizes: number[];
     };
 }
 
@@ -13,6 +16,7 @@ interface ParsedExtendedAttributes {
     Common: {
         ModificationTime?: number;
         Size?: number;
+        BlockSizes?: number[];
     };
 }
 
@@ -28,10 +32,14 @@ export async function ecryptExtendedAttributes(file: File, nodePrivateKey: OpenP
 }
 
 export function createExtendedAttributes(file: File): ExtendedAttributes {
+    const blockSizes = new Array(Math.floor(file.size / FILE_CHUNK_SIZE));
+    blockSizes.fill(FILE_CHUNK_SIZE);
+    blockSizes.push(file.size % FILE_CHUNK_SIZE);
     return {
         Common: {
             ModificationTime: new Date(file.lastModified).toISOString(),
             Size: file.size,
+            BlockSizes: blockSizes,
         },
     };
 }
@@ -55,6 +63,7 @@ export function parseExtendedAttributes(xattrString: string) {
         Common: {
             ModificationTime: parseModificationTime(xattr),
             Size: parseSize(xattr),
+            BlockSizes: parseBlockSizes(xattr),
         },
     };
 }
@@ -80,4 +89,17 @@ function parseSize(xattr: any): number | undefined {
         return undefined;
     }
     return size;
+}
+
+function parseBlockSizes(xattr: any): number[] | undefined {
+    const blockSizes = xattr?.Common?.BlockSizes;
+    if (!Array.isArray(blockSizes)) {
+        console.warn(`XAttr block sizes "${blockSizes}" is not valid`);
+        return undefined;
+    }
+    if (!blockSizes.every((item) => typeof item === 'number')) {
+        console.warn(`XAttr block sizes "${blockSizes}" is not valid`);
+        return undefined;
+    }
+    return blockSizes;
 }
