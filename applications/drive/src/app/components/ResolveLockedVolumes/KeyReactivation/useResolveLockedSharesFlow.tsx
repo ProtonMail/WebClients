307b89d0f34c74703f3378f51c6a8fbe4d4b61b9
Oleg Voloshin <oleg.voloshin@proton.ch>
2021-10-04 14:12:14 +0200

    Locked volumes modal: update design
    DRVWEB-1534

--- applications/drive/src/app/components/ResolveLockedVolumes/KeyReactivation/useResolveLockedSharesFlow.tsx
+++ applications/drive/src/app/components/ResolveLockedVolumes/KeyReactivation/useResolveLockedSharesFlow.tsx
@@ -1,10 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { c } from 'ttag';
 
 import { useModals, useNotifications } from '@proton/components';
 
-import DeleteOldFilesConfirmModal from './DeleteOldFilesConfirmModal';
-import KeyReactivationModal, { ReactivationOptions } from './KeyReactivationModal';
+import DeleteOldFilesConfirmModal from './DeleteLockedVolumesConfirmModal';
+import UnlockDriveConfirmationDialog from './UnlockDriveConfirmationDialog';
+import KeyReactivationModal, { LockedVolumeResolveMethod } from './LockedVolumesResolveMethodModal';
 import useFiles from '../../../hooks/drive/useFiles';
 
 interface ReactivationParams {
@@ -13,13 +14,22 @@ interface ReactivationParams {
 }
 
 const useKeyReactivationFlow = ({ onSuccess, onError }: ReactivationParams) => {
-    const keyReactivationModalId = useRef<string>();
-    const deleteOldFilesModalId = useRef<string>();
+    const lastResolveMethod = useRef<LockedVolumeResolveMethod>(LockedVolumeResolveMethod.ReactivateKeys);
     const volumesToDelete = useRef<string[]>([]);
 
     const { deleteOldFiles } = useFiles();
     const { createModal, removeModal } = useModals();
     const { createNotification } = useNotifications();
+
+    const [currentModalType, setCurrentModalType] = useState<LockedVolumeResolveMethod | null>(null);
+    const currentModalRef = useRef<string | null>(null);
+
+    const removeCurrentModal = () => {
+        if (currentModalRef.current !== null) {
+            removeModal(currentModalRef.current);
+            currentModalRef.current = null;
+        }
+    };
 
     const handleConfirmation = async () => {
         try {
@@ -27,28 +37,74 @@ const useKeyReactivationFlow = ({ onSuccess, onError }: ReactivationParams) => {
             createNotification({
                 text: c('Notification').t`Your old files will be deleted in 72 hours`,
             });
+            removeCurrentModal();
             onSuccess?.();
         } catch (e) {
             onError?.();
         }
-        removeModal(deleteOldFilesModalId.current);
     };
 
-    const handleKeyReactivationModalSubmit = ({ type }: { type: ReactivationOptions }) => {
-        removeModal(keyReactivationModalId.current);
+    const handleResolveMethodSelection = (type: LockedVolumeResolveMethod) => {
+        removeCurrentModal();
+        lastResolveMethod.current = type;
+        setCurrentModalType(type);
+    };
 
-        if (type === ReactivationOptions.ReactivateKeys) {
-            onSuccess?.();
-        } else if (type === ReactivationOptions.DeleteOldFiles) {
-            deleteOldFilesModalId.current = createModal(<DeleteOldFilesConfirmModal onSubmit={handleConfirmation} />);
+    const handleBackButtonClick = () => {
+        removeCurrentModal();
+        setCurrentModalType(LockedVolumeResolveMethod.ResolveMethodSelection);
+    };
+
+    useEffect(() => {
+        if (currentModalType === null) {
+            return;
         }
-    };
+
+        const volumeCount = volumesToDelete.current.length;
+
+        switch (currentModalType) {
+            case LockedVolumeResolveMethod.ResolveMethodSelection:
+                currentModalRef.current = createModal(
+                    <KeyReactivationModal
+                        onSubmit={handleResolveMethodSelection}
+                        defaultResolveMethod={lastResolveMethod.current}
+                        onClose={() => setCurrentModalType(null)}
+                        volumeCount={volumeCount}
+                    />
+                );
+                break;
+            case LockedVolumeResolveMethod.DeleteOldFiles:
+                currentModalRef.current = createModal(
+                    <DeleteOldFilesConfirmModal
+                        onSubmit={handleConfirmation}
+                        onBack={handleBackButtonClick}
+                        onClose={() => setCurrentModalType(null)}
+                        volumeCount={volumeCount}
+                    />
+                );
+                break;
+            case LockedVolumeResolveMethod.ReactivateKeys:
+                currentModalRef.current = createModal(
+                    <UnlockDriveConfirmationDialog
+                        onBack={handleBackButtonClick}
+                        onClose={() => setCurrentModalType(null)}
+                    />
+                );
+                break;
+            case LockedVolumeResolveMethod.UnlockLater:
+                setCurrentModalType(null);
+                onSuccess?.();
+                break;
+            default:
+                break;
+        }
+    }, [currentModalType]);
 
     const openKeyReactivationModal = (volumeIds: string[]) => {
         volumesToDelete.current = volumeIds;
-        keyReactivationModalId.current = createModal(
-            <KeyReactivationModal onSubmit={handleKeyReactivationModalSubmit} />
-        );
+        lastResolveMethod.current = LockedVolumeResolveMethod.ReactivateKeys;
+        setCurrentModalType(null);
+        setCurrentModalType(LockedVolumeResolveMethod.ResolveMethodSelection);
     };
 
     return {
