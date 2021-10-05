cd4810ba4b84448460852857dccaa35276c5bd52
Guillaume Gonzalez <guillaume.gonzalez@proton.ch>
2021-10-05 12:32:14 +0200

    Add search filter to location dropdown
    MAILWEB-2656

--- applications/mail/src/app/components/header/AdvancedSearchFields/LocationFieldDropdown.tsx
+++ applications/mail/src/app/components/header/AdvancedSearchFields/LocationFieldDropdown.tsx
@@ -1,5 +1,5 @@
 import { c } from 'ttag';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {
     classnames,
     Dropdown,
@@ -7,6 +7,8 @@ import {
     DropdownMenu,
     DropdownMenuButton,
     generateUID,
+    Mark,
+    SearchInput,
     usePopperAnchor,
 } from '@proton/components';
 
@@ -18,9 +20,22 @@ interface Props {
 }
 
 const LocationFieldDropdown = ({ value, onChange }: Props) => {
-    const { current: uid } = useRef(generateUID('advanced-search-location-dropdown'));
-    const { anchorRef, isOpen, toggle, close } = usePopperAnchor<HTMLButtonElement>();
     const { grouped: optionGroups } = useLocationFieldOptions();
+    const [search, setSearch] = useState('');
+    const [options, setOptions] = useState(optionGroups);
+
+    const { current: uid } = useRef(generateUID('advanced-search-location-dropdown'));
+    const { current: searchInputID } = useRef(generateUID('dropdown'));
+    const { anchorRef, isOpen, toggle, close } = usePopperAnchor<HTMLButtonElement>();
+
+    useEffect(() => {
+        const lowerCaseSearch = search.toLowerCase();
+        const nextOptions = optionGroups.map((optionGroup) => ({
+            ...optionGroup,
+            items: optionGroup.items.filter((item) => item.text.toLowerCase().includes(lowerCaseSearch)),
+        }));
+        setOptions(nextOptions);
+    }, [search]);
 
     return (
         <>
@@ -39,30 +54,50 @@ const LocationFieldDropdown = ({ value, onChange }: Props) => {
             <Dropdown
                 anchorRef={anchorRef}
                 autoClose
+                onClosed={() => setSearch('')}
                 id={uid}
                 isOpen={isOpen}
                 onClose={close}
                 originalPlacement="bottom-left"
+                contentProps={{
+                    className: 'flex flex-column flex-nowrap',
+                }}
             >
-                <DropdownMenu>
-                    {optionGroups.map((group) =>
-                        group.items.length ? (
-                            <div key={group.id}>
-                                <span className="button text-bold pl1">{group.title}</span>
-                                {group.items.map((item) => (
-                                    <DropdownMenuButton
-                                        className="text-left pl2 text-ellipsis"
-                                        isSelected={item.value === value}
-                                        key={item.value}
-                                        onClick={() => onChange(item.value)}
-                                    >
-                                        {item.text}
-                                    </DropdownMenuButton>
-                                ))}
-                            </div>
-                        ) : null
-                    )}
-                </DropdownMenu>
+                <div className="p1 flex-item-noshrink">
+                    <SearchInput
+                        value={search}
+                        onChange={setSearch}
+                        onClick={(e) => {
+                            // Prevent dropdown closing
+                            e.stopPropagation();
+                        }}
+                        id={searchInputID}
+                        placeholder={c('Placeholder').t`Filter folders`}
+                        autoFocus
+                        data-prevent-arrow-navigation
+                    />
+                </div>
+                <div className="scroll-if-needed scroll-smooth-touch">
+                    <DropdownMenu>
+                        {options.map((group) =>
+                            group.items.length ? (
+                                <div key={group.id}>
+                                    <span className="button text-bold pl1">{group.title}</span>
+                                    {group.items.map((item) => (
+                                        <DropdownMenuButton
+                                            className="text-left pl2 text-ellipsis"
+                                            isSelected={item.value === value}
+                                            key={item.value}
+                                            onClick={() => onChange(item.value)}
+                                        >
+                                            <Mark value={search}>{item.text}</Mark>
+                                        </DropdownMenuButton>
+                                    ))}
+                                </div>
+                            ) : null
+                        )}
+                    </DropdownMenu>
+                </div>
             </Dropdown>
         </>
     );
