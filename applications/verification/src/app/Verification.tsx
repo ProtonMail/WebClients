b35749ee170eb767d936a1cc561ed07e8c5815d7
Matthias Margot <margotmatthias@protonmail.com>
2021-10-05 13:52:16 +0200

    Comment VerificationSearchParameter fields
    CP-2489

--- applications/verification/src/app/Verification.tsx
+++ applications/verification/src/app/Verification.tsx
@@ -9,12 +9,36 @@ import broadcast, { MessageType } from './broadcast';
 import './Verification.scss';
 
 interface VerificationSearchParameters {
-    methods?: HumanVerificationMethodType[];
-    embed?: boolean;
-    theme?: ThemeTypes;
+    /*
+     * Human verification start token
+     */
     token?: string;
+    /*
+     * Available human verification methods
+     * captcha | payment | sms | email | invite | coupon
+     */
+    methods?: HumanVerificationMethodType[];
+    /*
+     * Force embed in case the checks fail to detect it for some reason
+     */
+    embed?: boolean;
+    /*
+     * Which theme to render the hv app in
+     * Default - 0
+     * Dark - 1
+     * Light - 2
+     * Monokai - 3
+     * Contrast - 4
+     * Legacy - 5
+     */
+    theme?: ThemeTypes;
+    /* Which language to display the app in */
+    locale?: string;
+    /* Default country, used in defaulting the phone number for example */
     defaultCountry?: string;
+    /* Default email value, used in email input display */
     defaultEmail?: string;
+    /* Default phone number, used in the phone input display */
     defaultPhone?: string;
 }
 
