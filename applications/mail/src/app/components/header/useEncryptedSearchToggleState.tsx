68e848f83735d724553ef9d23b6cb226bdc9c5b6
Guillaume Gonzalez <guillaume.gonzalez@proton.ch>
2021-10-06 19:48:31 +0200

    Move esState to a hook

---
+++ applications/mail/src/app/components/header/useEncryptedSearchToggleState.tsx
@@ -0,0 +1,95 @@
+import { useEffect, useRef, useState } from 'react';
+import { useUser } from '@proton/components';
+import { wait } from '@proton/shared/lib/helpers/promise';
+import { SECOND } from '@proton/shared/lib/constants';
+
+import { getOldestTime, wasIndexingDone } from '../../helpers/encryptedSearch/esUtils';
+import { useEncryptedSearchContext } from '../../containers/EncryptedSearchProvider';
+import { ESIndexingState } from '../../models/encryptedSearch';
+import { defaultESIndexingState } from '../../constants';
+import { estimateIndexingTime } from '../../helpers/encryptedSearch/esBuild';
+
+const useEncryptedSearchToggleState = (isOpen: boolean) => {
+    const [user] = useUser();
+    const { getProgressRecorderRef, getESDBStatus } = useEncryptedSearchContext();
+    const { isBuilding, isDBLimited, isRefreshing } = getESDBStatus();
+    const [esState, setESState] = useState<ESIndexingState>(defaultESIndexingState);
+
+    const abortProgressRef = useRef<AbortController>(new AbortController());
+
+    const setProgress = async () => {
+        while (!abortProgressRef.current.signal.aborted) {
+            setESState((esState) => {
+                const [esProgress, esTotal] = getProgressRecorderRef().current;
+                const endTime = performance.now();
+
+                const { estimatedMinutes, currentProgressValue } = estimateIndexingTime(
+                    esProgress,
+                    esTotal,
+                    endTime,
+                    esState
+                );
+
+                return {
+                    ...esState,
+                    endTime,
+                    esProgress,
+                    totalIndexingMessages: esTotal,
+                    estimatedMinutes: estimatedMinutes || esState.estimatedMinutes,
+                    currentProgressValue: currentProgressValue || esState.currentProgressValue,
+                };
+            });
+            await wait(2 * SECOND);
+        }
+    };
+
+    const setOldestTime = async () => {
+        if (wasIndexingDone(user.ID) && isDBLimited) {
+            const oldestTime = await getOldestTime(user.ID, 1000);
+            setESState((esState) => {
+                return {
+                    ...esState,
+                    oldestTime,
+                };
+            });
+        }
+    };
+
+    const startProgress = async () => {
+        abortProgressRef.current = new AbortController();
+        const [esPrevProgress, totalIndexingMessages] = getProgressRecorderRef().current;
+        setESState((esState) => {
+            return {
+                ...esState,
+                startTime: performance.now(),
+                esPrevProgress,
+                totalIndexingMessages,
+            };
+        });
+        void setProgress();
+    };
+
+    const stopProgress = () => {
+        abortProgressRef.current.abort();
+        setESState(() => defaultESIndexingState);
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+            void setOldestTime();
+        }
+    }, [isOpen]);
+
+    useEffect(() => {
+        if (isBuilding || isRefreshing) {
+            void startProgress();
+        } else {
+            stopProgress();
+        }
+        void setOldestTime();
+    }, [isBuilding, isRefreshing]);
+
+    return esState;
+};
+
+export default useEncryptedSearchToggleState;
