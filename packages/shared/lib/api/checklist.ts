f1402c6c463d39e640bc628d45f86936485c2a05
Matthias Margot <margotmatthias@protonmail.com>
2021-10-13 11:47:13 +0200

    Mark get started checklist as seen on ui mount
    CP-2526

--- packages/shared/lib/api/checklist.ts
+++ packages/shared/lib/api/checklist.ts
@@ -2,3 +2,8 @@ export const getChecklist = (checklistId: string) => ({
     method: 'get',
     url: `core/v4/checklist/${checklistId}`,
 });
+
+export const seenCompletedChecklist = (checklistId: string) => ({
+    method: 'post',
+    url: `core/v4/checklist/${checklistId}/seen-completed-list`,
+});
