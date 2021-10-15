407ec3192d4b27413544849dabbdccbf55acb455
Michal Horejsek <michal.horejsek@proton.ch>
2021-10-15 09:00:26 +0200

    Fix upload on Firefox mobile

--- applications/drive/src/app/components/uploads/useUploadInput.ts
+++ applications/drive/src/app/components/uploads/useUploadInput.ts
@@ -1,4 +1,7 @@
 import { useRef, useEffect, ChangeEvent } from 'react';
+import { c } from 'ttag';
+
+import { useNotifications } from '@proton/components';
 
 import useActiveShare from '../../hooks/drive/useActiveShare';
 import { isTransferCancelError } from '../../utils/transfer';
@@ -8,6 +11,7 @@ import { useUploadProvider } from './UploadProvider';
 const useUploadInput = (forFolders?: boolean) => {
     const { activeFolder } = useActiveShare();
     const { uploadFiles } = useUploadProvider();
+    const { createNotification } = useNotifications();
 
     const inputRef = useRef<HTMLInputElement>(null);
 
@@ -18,26 +22,37 @@ const useUploadInput = (forFolders?: boolean) => {
         }
     }, [forFolders]);
 
-    const getFolderItemsToUpload = (files: FileList): UploadFileList => {
+    const getItemsToUpload = (files: FileList): UploadFileList => {
         const foldersCreated = new Set<string>();
         const filesToUpload: UploadFileList = [];
 
         for (let i = 0; i < files.length; i++) {
             const file = files[i];
 
-            if ('webkitRelativePath' in file) {
-                const path = ((file as any).webkitRelativePath as string).split('/');
-                for (let j = 1; j < path.length; j++) {
-                    const folderPath = path.slice(0, j);
-                    const folderPathStr = folderPath.join('/');
-                    if (!foldersCreated.has(folderPathStr)) {
-                        foldersCreated.add(folderPathStr);
-                        filesToUpload.push({ path: folderPath.slice(0, -1), folder: folderPath.slice(-1)[0] });
+            if (forFolders) {
+                // webkitRelativePath might be available only if property
+                // webkitdirectory is set, or not at all. It is important
+                // to not use it if its not for folders so at least just
+                // files without structure can be uploaded.
+                if ('webkitRelativePath' in file) {
+                    const path = ((file as any).webkitRelativePath as string).split('/');
+                    for (let j = 1; j < path.length; j++) {
+                        const folderPath = path.slice(0, j);
+                        const folderPathStr = folderPath.join('/');
+                        if (!foldersCreated.has(folderPathStr)) {
+                            foldersCreated.add(folderPathStr);
+                            filesToUpload.push({ path: folderPath.slice(0, -1), folder: folderPath.slice(-1)[0] });
+                        }
                     }
+                    filesToUpload.push({ path: path.slice(0, -1), file });
+                } else {
+                    createNotification({
+                        type: 'error',
+                        text: c('Error').t`Your browser does not support uploading folders`,
+                    });
                 }
-                filesToUpload.push({ path: path.slice(0, -1), file });
             } else {
-                console.error('No relative path to determine folder structure from');
+                filesToUpload.push({ path: [], file });
             }
         }
 
@@ -59,7 +74,7 @@ const useUploadInput = (forFolders?: boolean) => {
             return;
         }
 
-        let filesToUpload = getFolderItemsToUpload(files);
+        let filesToUpload = getItemsToUpload(files);
         if (!forFolders) {
             // MacOS has bug, where you can select folders when uploading files in some cases.
             filesToUpload = filesToUpload.filter((item) => !!(item as UploadFileItem).file);
