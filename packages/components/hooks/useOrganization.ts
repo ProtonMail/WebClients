5e05d3dcfd8da2ad69a12a421addfc494420f14f
mmso <mmso@mmso.se>
2021-10-19 19:54:58 +0200

    Use async user hook in organization

--- packages/components/hooks/useOrganization.ts
+++ packages/components/hooks/useOrganization.ts
@@ -2,19 +2,18 @@ import { useCallback } from 'react';
 import { Organization } from '@proton/shared/lib/interfaces';
 import { FREE_ORGANIZATION } from '@proton/shared/lib/constants';
 import { OrganizationModel } from '@proton/shared/lib/models/organizationModel';
-import { UserModel } from '@proton/shared/lib/models/userModel';
 
 import useCachedModelResult, { getPromiseValue } from './useCachedModelResult';
 import useApi from './useApi';
 import useCache from './useCache';
+import { useGetUser } from './useUser';
 
 export const useGetOrganization = (): (() => Promise<Organization>) => {
     const api = useApi();
     const cache = useCache();
-    const miss = useCallback(() => {
-        // Not using use user since it's better to read from the cache
-        // It will be updated from the event manager.
-        const user = cache.get(UserModel.key).value;
+    const getUser = useGetUser();
+    const miss = useCallback(async () => {
+        const user = await getUser();
         if (user.isPaid) {
             return OrganizationModel.get(api);
         }
