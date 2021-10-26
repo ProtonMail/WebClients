94c05c048be4804b10c21b6ba05f6e827d17111e
Jeremy Benaim <jeremybenaim@gmail.com>
2021-10-26 10:12:18 +0200

    Move Easy Switch sources to constants + review

--- packages/components/containers/importAssistant/mail/ImportCsvSection.tsx
+++ packages/components/containers/importAssistant/mail/ImportCsvSection.tsx
@@ -1,6 +1,7 @@
 import { c } from 'ttag';
 import { SettingsParagraph, SettingsSection } from '@proton/components';
 import ImportCsvContactButton from '@proton/components/containers/contacts/widget/ImportCsvContactButton';
+import { EASY_SWITCH_SOURCE } from '@proton/shared/lib/interfaces/EasySwitch';
 
 interface Props {
     hideEasySwitch?: boolean;
@@ -14,7 +15,10 @@ const ImportCsvSection = ({ hideEasySwitch = false }: Props) => (
         </SettingsParagraph>
 
         <div>
-            <ImportCsvContactButton easySwitchSource="import-contacts-settings" hideEasySwitch={hideEasySwitch} />
+            <ImportCsvContactButton
+                easySwitchSource={EASY_SWITCH_SOURCE.IMPORT_CONTACT_SETTINGS}
+                hideEasySwitch={hideEasySwitch}
+            />
         </div>
     </SettingsSection>
 );
