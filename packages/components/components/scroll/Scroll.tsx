2329ec6e9b05dfa96afac870b27904d0fe8e32df
Matthias Margot <matthias.margot@proton.ch>
2021-10-26 18:49:02 +0200

    Bake resize-observer into Scroll
    For use-cases where no scrolling and no mounting is
    going on, but the container is shrunk or grown from
    resize events, whatever might have triggered them.
    
    CP-2753
    CP-2812

--- packages/components/components/scroll/Scroll.tsx
+++ packages/components/components/scroll/Scroll.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { classnames } from '../../helpers';
 import './Scroll.scss';
@@ -10,12 +10,16 @@ interface ScrollProps extends React.ComponentPropsWithoutRef<'div'> {
 }
 
 const Scroll = ({ children, horizontal, className, ...rest }: ScrollProps) => {
-    const scrollContainerRef = useRef<null | HTMLDivElement>(null);
-    const scrollChildRef = useRef<null | HTMLDivElement>(null);
+    const scrollContainerRef = useRef<HTMLDivElement>(null);
+    const scrollChildRef = useRef<HTMLDivElement>(null);
     const [showStartShadow, setShowStartShadow] = useState(false);
     const [showEndShadow, setShowEndShadow] = useState(false);
 
     const setShadows = (container: HTMLDivElement, child: HTMLDivElement) => {
+        if (!container || !child) {
+            return;
+        }
+
         const containerRect = container.getBoundingClientRect();
 
         const childRect = child.getBoundingClientRect();
@@ -28,38 +32,47 @@ const Scroll = ({ children, horizontal, className, ...rest }: ScrollProps) => {
             ? childRect.right - containerRect.right < TOLERANCE
             : childRect.bottom - containerRect.bottom < TOLERANCE;
 
-        if (isOnStartEdge === showStartShadow) {
-            setShowStartShadow(!isOnStartEdge);
-        }
+        /*
+         * We're passing a callback here so that we can have access to the previous
+         * respective show shadow value. If we reference "showStartShadow" or
+         * "showEndShadow" directly here, we run into a stale closure issue because
+         * "setShadows" is passed to the resize observer in the useEffect below.
+         *
+         * I was cautious at first because I was afraid that calling useState's set
+         * functions every time like this just to receive access to the real previous
+         * state value might have some negative impact on rendering performance. However,
+         * useState seems to internally handle reference equality comparison optimizations.
+         * These setState's do not trigger re-renders if they return the exact same value
+         * as what was already set.
+         *
+         * This is mostly an effort to be able to re-use only one resize observer.
+         * An alternative would've been to clean up and re-create a resize observer in the
+         * useEffect below, passing setShadows (wrapped in useCallback that is) into the
+         * dependency array, but I thought this would be the cleaner solution, albeit a bit
+         * weird imo.
+         */
+        setShowStartShadow((previousShowStartShadow) =>
+            isOnStartEdge === previousShowStartShadow ? !previousShowStartShadow : previousShowStartShadow
+        );
 
-        if (isOnEndEdge === showEndShadow) {
-            setShowEndShadow(!isOnEndEdge);
-        }
+        setShowEndShadow((previousShowEndShadow) =>
+            isOnEndEdge === previousShowEndShadow ? !previousShowEndShadow : previousShowEndShadow
+        );
     };
 
-    const setScrollContainerRef = (node: HTMLDivElement) => {
-        if (!node) {
-            return;
-        }
+    useEffect(() => {
+        const resizeObserver = new ResizeObserver(() => {
+            setShadows(scrollContainerRef.current!, scrollChildRef.current!);
+        });
 
-        scrollContainerRef.current = node;
+        resizeObserver.observe(scrollChildRef.current!);
 
-        if (scrollChildRef.current) {
-            setShadows(scrollContainerRef.current, scrollChildRef.current);
-        }
-    };
+        resizeObserver.observe(scrollContainerRef.current!);
 
-    const setScrollChildRef = (node: HTMLDivElement) => {
-        if (!node) {
-            return;
-        }
-
-        scrollChildRef.current = node;
-
-        if (scrollContainerRef.current) {
-            setShadows(scrollContainerRef.current, scrollChildRef.current);
-        }
-    };
+        return () => {
+            resizeObserver.disconnect();
+        };
+    }, []);
 
     const handleScroll = ({ currentTarget: scrollContainer }: React.UIEvent<HTMLDivElement, UIEvent>) => {
         const { current: scrollChild } = scrollChildRef;
@@ -78,6 +91,7 @@ const Scroll = ({ children, horizontal, className, ...rest }: ScrollProps) => {
     };
 
     const outerClassName = classnames([className, horizontal ? 'scroll-outer-horizontal' : 'scroll-outer-vertical']);
+
     const startShadowClassName = classnames([
         'scroll-start-shadow no-pointer-events',
         showStartShadow && 'scroll-start-shadow-visible',
@@ -91,8 +105,8 @@ const Scroll = ({ children, horizontal, className, ...rest }: ScrollProps) => {
         <div {...rest} className={outerClassName}>
             <div className={startShadowClassName} aria-hidden="true" />
             <div className={endShadowClassName} aria-hidden="true" />
-            <div className="scroll-inner" ref={setScrollContainerRef} onScroll={handleScroll}>
-                <div className="scroll-child" ref={setScrollChildRef}>
+            <div className="scroll-inner" ref={scrollContainerRef} onScroll={handleScroll}>
+                <div className="scroll-child" ref={scrollChildRef}>
                     {children}
                 </div>
             </div>
