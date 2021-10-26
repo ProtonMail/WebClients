c4e2cde841cc037cad64cfcf3962970130a56bd1
mmso <mmso@mmso.se>
2021-10-26 14:38:54 +0200

    Upgrade keys in v2 flow when key migration has been done

--- packages/shared/test/keys/keyDataHelper.ts
+++ packages/shared/test/keys/keyDataHelper.ts
@@ -5,7 +5,7 @@ import { ENCRYPTION_CONFIGS, ENCRYPTION_TYPES } from '../../lib/constants';
 
 const encryptionConfig = ENCRYPTION_CONFIGS[ENCRYPTION_TYPES.CURVE25519];
 
-export const getUserKey = async (ID: string, keyPassword: string) => {
+export const getUserKey = async (ID: string, keyPassword: string, version = 3) => {
     const { privateKey, privateKeyArmored } = await generateUserKey({
         passphrase: keyPassword,
         encryptionConfig,
@@ -19,12 +19,12 @@ export const getUserKey = async (ID: string, keyPassword: string) => {
         Key: {
             ID,
             PrivateKey: privateKeyArmored,
-            Version: 3,
+            Version: version,
         } as Key,
     };
 };
 
-export const getAddressKeyHelper = async (ID: string, userKey: OpenPGPKey, privateKey: OpenPGPKey) => {
+export const getAddressKeyHelper = async (ID: string, userKey: OpenPGPKey, privateKey: OpenPGPKey, version = 3) => {
     const result = await generateAddressKeyTokens(userKey);
     const privateKeyArmored = await encryptPrivateKey(privateKey, result.token);
     return {
@@ -38,19 +38,19 @@ export const getAddressKeyHelper = async (ID: string, userKey: OpenPGPKey, priva
             PrivateKey: privateKeyArmored,
             Signature: result.signature,
             Token: result.encryptedToken,
-            Version: 3,
+            Version: version,
             Flags: 3,
         } as Key,
     };
 };
 
-export const getAddressKey = async (ID: string, userKey: OpenPGPKey, email: string) => {
+export const getAddressKey = async (ID: string, userKey: OpenPGPKey, email: string, version?: number) => {
     const key = await generateAddressKey({
         email,
         passphrase: 'tmp',
         encryptionConfig,
     });
-    return getAddressKeyHelper(ID, userKey, key.privateKey);
+    return getAddressKeyHelper(ID, userKey, key.privateKey, version);
 };
 
 export const getLegacyAddressKey = async (ID: string, password: string, email: string) => {
