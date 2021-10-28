f4673608fbdb6a6190ddfa6b95cd8d6b763fb677
Matthias Margot <matthias.margot@proton.ch>
2021-10-28 18:27:51 +0200

    Lift restrictions on slot/children apis
    CP-2753

--- packages/components/components/modalTwo/ModalTitle.tsx
+++ packages/components/components/modalTwo/ModalTitle.tsx
@@ -1,11 +1,16 @@
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, useContext } from 'react';
 
 import { classnames } from '../../helpers';
+import { ModalContext } from './Modal';
 
-const ModalTitle = ({ className, children, ...rest }: ComponentPropsWithoutRef<'div'>) => (
-    <h3 className={classnames([className, 'text-bold'])} {...rest}>
-        {children}
-    </h3>
-);
+const ModalTitle = ({ className, children, ...rest }: ComponentPropsWithoutRef<'div'>) => {
+    const { id } = useContext(ModalContext);
+
+    return (
+        <h3 id={id} className={classnames([className, 'text-bold'])} {...rest}>
+            {children}
+        </h3>
+    );
+};
 
 export default ModalTitle;
