aa342fb9ca4c8b878649664500436cb5294040fa
Jack Crompton <jack.crompton@proton.ch>
2021-11-01 12:09:10 +0000

    Call mnemonic reactivation from GenerateMnemonicModal

--- packages/components/containers/mnemonic/GenerateMnemonicModal.tsx
+++ packages/components/containers/mnemonic/GenerateMnemonicModal.tsx
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react';
 import { c } from 'ttag';
-import { updateMnemonicPhrase } from '@proton/shared/lib/api/settingsMnemonic';
+import { reactivateMnemonicPhrase, updateMnemonicPhrase } from '@proton/shared/lib/api/settingsMnemonic';
 import { getApiErrorMessage } from '@proton/shared/lib/api/helpers/apiErrorHelper';
 import { PASSWORD_WRONG_ERROR } from '@proton/shared/lib/api/auth';
 import { noop } from '@proton/shared/lib/helpers/function';
 import { srpAuth } from '@proton/shared/lib/srp';
 import { generateMnemonicPayload, generateMnemonicWithSalt, MnemonicData } from '@proton/shared/lib/mnemonic';
 
+import { MNEMONIC_STATUS } from '@proton/shared/lib/interfaces';
 import { Button, FormModal, Loader } from '../../components';
-import { useApi, useGetUserKeys, useUser } from '../../hooks';
+import { useApi, useGetUserKeys, useLoading, useUser } from '../../hooks';
 import { PasswordTotpInputs, useAskAuth } from '../password';
 import { MnemonicPhraseStepButtons, MnemonicPhraseStepContent } from './MnemonicPhraseStep';
 
@@ -24,9 +25,15 @@ interface Props {
     onSuccess: () => void;
 }
 
-const GenerateMnemonicModal = (props: Props) => {
-    const { confirmStep = false, onClose = () => {}, onSuccess, ...rest } = props;
-    const [step, setStep] = useState(confirmStep ? STEPS.CONFIRM : STEPS.AUTH);
+const GenerateMnemonicModal = ({ confirmStep = false, onClose = () => {}, onSuccess, ...rest }: Props) => {
+    const [{ Name, MnemonicStatus }] = useUser();
+    const callReactivateEndpoint =
+        MnemonicStatus === MNEMONIC_STATUS.ENABLED ||
+        MnemonicStatus === MNEMONIC_STATUS.OUTDATED ||
+        MnemonicStatus === MNEMONIC_STATUS.PROMPT;
+
+    const nonConfirmStep = callReactivateEndpoint ? STEPS.MNEMONIC_PHRASE : STEPS.AUTH;
+    const [step, setStep] = useState(confirmStep ? STEPS.CONFIRM : nonConfirmStep);
 
     const api = useApi();
     const [submittingAuth, setSubmittingAuth] = useState(false);
@@ -35,18 +42,49 @@ const GenerateMnemonicModal = (props: Props) => {
 
     const [password, setPassword] = useState('');
     const [totp, setTotp] = useState('');
-    const [{ Name }] = useUser();
     const [hasTOTPEnabled, isLoadingAuth] = useAskAuth();
 
+    const [generating, withGenerating] = useLoading();
+    const [reactivating, withReactivating] = useLoading();
+
     const [mnemonicData, setMnemonicData] = useState<MnemonicData>();
+
+    const getPayload = async (data: MnemonicData) => {
+        const userKeys = await getUserKeys();
+        const { randomBytes, salt } = data;
+
+        return generateMnemonicPayload({ randomBytes, salt, userKeys, api, username: Name });
+    };
+
+    const handleReactivate = async (data: MnemonicData | undefined) => {
+        if (!data) {
+            return;
+        }
+
+        try {
+            const payload = await getPayload(data);
+            await api(reactivateMnemonicPhrase(payload));
+
+            onSuccess();
+            if (confirmStep) {
+                setStep(STEPS.MNEMONIC_PHRASE);
+            }
+        } catch (error: any) {
+            onClose?.();
+        }
+    };
 
     useEffect(() => {
         const generateMnemonicData = async () => {
             const data = await generateMnemonicWithSalt();
             setMnemonicData(data);
+
+            if (!confirmStep && callReactivateEndpoint) {
+                await handleReactivate(data);
+            }
         };
 
-        void generateMnemonicData();
+        void withGenerating(generateMnemonicData());
     }, []);
 
     const { section, ...modalProps } = (() => {
@@ -64,10 +102,22 @@ const GenerateMnemonicModal = (props: Props) => {
                 ),
                 footer: (
                     <div className="w100">
-                        <Button fullWidth color="norm" loading={!mnemonicData} onClick={() => setStep(STEPS.AUTH)}>
-                            {c('Action').t`Create new recovery phrase`}
+                        <Button
+                            fullWidth
+                            color="norm"
+                            disabled={!mnemonicData}
+                            loading={reactivating}
+                            onClick={() => {
+                                if (callReactivateEndpoint) {
+                                    void withReactivating(handleReactivate(mnemonicData));
+                                } else {
+                                    setStep(STEPS.AUTH);
+                                }
+                            }}
+                        >
+                            {c('Action').t`Create recovery phrase`}
                         </Button>
-                        <Button className="mt1" fullWidth onClick={onClose}>
+                        <Button className="mt1" fullWidth disabled={reactivating} onClick={onClose}>
                             {c('Action').t`Cancel`}
                         </Button>
                     </div>
@@ -83,10 +133,7 @@ const GenerateMnemonicModal = (props: Props) => {
 
                 try {
                     setSubmittingAuth(true);
-
-                    const userKeys = await getUserKeys();
-                    const { randomBytes, salt } = mnemonicData;
-                    const payload = await generateMnemonicPayload({ randomBytes, salt, userKeys, api, username: Name });
+                    const payload = await getPayload(mnemonicData);
 
                     await srpAuth({
                         api,
@@ -132,17 +179,14 @@ const GenerateMnemonicModal = (props: Props) => {
             };
         }
 
-        if (!mnemonicData) {
-            throw new Error('No mnemonic data');
-        }
-
         if (step === STEPS.MNEMONIC_PHRASE) {
-            const { mnemonic } = mnemonicData;
+            const mnemonic = mnemonicData?.mnemonic;
 
             return {
+                hasClose: !generating,
                 title: c('Info').t`Your recovery phrase`,
-                section: <MnemonicPhraseStepContent mnemonic={mnemonic} />,
-                footer: <MnemonicPhraseStepButtons mnemonic={mnemonic} onDone={onClose} />,
+                section: <MnemonicPhraseStepContent mnemonic={mnemonic} loading={generating} />,
+                footer: <MnemonicPhraseStepButtons mnemonic={mnemonic} disabled={generating} onDone={onClose} />,
             };
         }
 
