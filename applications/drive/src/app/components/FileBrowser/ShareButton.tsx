13da8bcc3f335a9e2e71870f492794fc3681903e
Michal Horejsek <michal.horejsek@proton.ch>
2021-11-02 14:40:19 +0100

    Remove share button on hover

--- applications/drive/src/app/components/FileBrowser/ShareButton.tsx
+++ applications/drive/src/app/components/FileBrowser/ShareButton.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { PrimaryButton, useModals, classnames } from '@proton/components';
 import { c } from 'ttag';
 import { FileBrowserItem } from '@proton/shared/lib/interfaces/drive/fileBrowser';
-import { LinkType } from '@proton/shared/lib/interfaces/drive/link';
 
 import ShareLinkModal from '../ShareLinkModal/ShareLinkModal';
 
@@ -11,9 +10,6 @@ interface Props {
     item: FileBrowserItem;
     className?: string;
 }
-
-export const shouldRenderShareButton = (item: FileBrowserItem) =>
-    item.Type === LinkType.FILE && item.Trashed === null && !item.SharedUrl;
 
 const ShareButton = ({ shareId, item, className }: Props) => {
     const { createModal } = useModals();
