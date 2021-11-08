d62c5a76948a0e325ecd1f0344ee8e1b8d22d0f4
Matthias Margot <matthias.margot@proton.ch>
2021-11-08 13:57:12 +0100

    A bunch of modal updates

--- packages/components/components/modalTwo/ModalHeader.tsx
+++ packages/components/components/modalTwo/ModalHeader.tsx
@@ -23,15 +23,10 @@ interface ModalHeaderProps extends Omit<ComponentPropsWithRef<'div'>, 'children'
      *
      */
     actions?: JSX.Element | [JSX.Element] | [JSX.Element, JSX.Element];
-    /**
-     * If passed, renders a back button in the Modal's header.
-     * Fires on click of said back button.
-     */
-    onBack?: () => void;
 }
 
-const ModalHeader = ({ title, subline, actions, onBack, ...rest }: ModalHeaderProps) => {
-    const { id, onClose } = useContext(ModalContext);
+const ModalHeader = ({ title, subline, actions, ...rest }: ModalHeaderProps) => {
+    const { id, onClose, large, full } = useContext(ModalContext);
 
     const [firstAction, secondAction] = Array.isArray(actions) ? actions : [actions];
 
@@ -39,22 +34,13 @@ const ModalHeader = ({ title, subline, actions, onBack, ...rest }: ModalHeaderPr
         <div
             className={classnames([
                 'modal-two-header flex flex-nowrap flex-item-noshrink flex-align-items-start',
-                onBack && 'modal-two-header--with-back',
-                onBack || title ? 'flex-justify-space-between' : 'flex-justify-end',
+                title ? 'flex-justify-space-between' : 'flex-justify-end',
             ])}
             {...rest}
         >
-            {onBack && (
-                <div className="flex-item-noshrink">
-                    <Button className="flex-item-noshrink" icon shape="ghost" onClick={onBack}>
-                        <Icon className="modal-close-icon" name="arrow-left" />
-                    </Button>
-                </div>
-            )}
-
             {title && (
-                <div className={classnames(['mt0-5', onBack && 'text-center'])}>
-                    <h3 id={id} className="text-lg text-bold">
+                <div className="modal-two-header-title mt0-25">
+                    <h3 id={id} className={classnames(['text-bold', large || full ? 'text-2xl' : 'text-xl'])}>
                         {title}
                     </h3>
                     {subline && <div className="color-weak">{subline}</div>}
