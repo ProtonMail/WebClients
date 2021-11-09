c0df31686a159a46c6b05d929159101479741694
Matthieu Lux <matthieu.lux@protonmail.com>
2021-11-09 17:56:46 +0100

    Various sentry reports fixes

--- packages/pack/webpack.config.js
+++ packages/pack/webpack.config.js
@@ -98,6 +98,12 @@ const getConfig = (env) => {
                         onProxyRes: (proxyRes) => {
                             delete proxyRes.headers['content-security-policy'];
                             delete proxyRes.headers['x-frame-options'];
+                            if (proxyRes.headers['set-cookie']) {
+                                const cookies = proxyRes.headers['set-cookie'].map((cookie) =>
+                                    cookie.replace(/; secure/gi, '')
+                                );
+                                proxyRes.headers['set-cookie'] = cookies;
+                            }
                         },
                     },
                 },
