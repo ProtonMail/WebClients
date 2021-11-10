1f3640e645f665a35094e760f61c65873732b0d9
Guillaume Gonzalez <guillaume.gonzalez@proton.ch>
2021-11-10 11:20:33 +0100

    Stop hearing DOM changes on contact widget dropdown

--- packages/components/containers/contacts/widget/TopNavbarListItemContactsDropdown.tsx
+++ packages/components/containers/contacts/widget/TopNavbarListItemContactsDropdown.tsx
@@ -84,6 +84,7 @@ const TopNavbarListItemContactsDropdown = ({ className, onCompose, onMailTo, cus
                 noMaxWidth
                 noMaxHeight
                 disableDefaultArrowNavigation
+                updatePositionOnDOMChange={false}
             >
                 {/* Translator: this text is "visually"-hidden, it's for helping blind people */}
                 <h1 className="sr-only">{c('Header').t`Contacts, groups and settings`}</h1>
