41b84836c4e93f8abb3d9e912183244b3625360b
Jack Crompton <jack.crompton@proton.ch>
2021-11-10 13:31:46 +0000

    Handle early access desynchronization automatically
    Remove the EarlyAccessDesynchronizedBanner, instead we set the version
    cookie on desynchronization and refresh the page automatically removing
    the need for the user to click the banner to refresh the page.
    
    Co-authored-by: Mattias Svanström <mattias.svanstrom@proton.ch>
    
    CP-2821

---
+++ packages/components/helpers/earlyAccessDesynchronization.ts
@@ -0,0 +1,74 @@
+import { doesNotSupportEarlyAccessVersion } from '@proton/shared/lib/helpers/browser';
+import { getItem, setItem, removeItem } from '@proton/shared/lib/helpers/sessionStorage';
+import { APPS, APP_NAMES } from '@proton/shared/lib/constants';
+import { UserSettings } from '@proton/shared/lib/interfaces';
+import {
+    Environment,
+    getTargetEnvironment,
+    getVersionCookieIsValid,
+    updateVersionCookie,
+    versionCookieAtLoad,
+} from '../hooks/useEarlyAccess';
+import { Feature } from '../containers/features/FeaturesContext';
+
+const MAX_NUMBER_OF_EARLY_ACCESS_DESYNCHRONIZATION_RETRIES = 2;
+
+// Set to -1 in browser to disable auto refresh
+const EARLY_ACCESS_DESYNCHRONIZATION_RETRIES_STORAGE_KEY = 'EARLY_ACCESS_DESYNCHRONIZATION_RETRIES_STORAGE_KEY';
+
+const getCurrentRetries = () => {
+    const currentRetries = Number(getItem(EARLY_ACCESS_DESYNCHRONIZATION_RETRIES_STORAGE_KEY));
+    if (Number.isNaN(currentRetries)) {
+        return undefined;
+    }
+
+    return currentRetries;
+};
+
+const setCurrentRetries = (currentRetries: number) => {
+    setItem(EARLY_ACCESS_DESYNCHRONIZATION_RETRIES_STORAGE_KEY, `${currentRetries}`);
+};
+
+const removeCurrentRetries = () => {
+    removeItem(EARLY_ACCESS_DESYNCHRONIZATION_RETRIES_STORAGE_KEY);
+};
+
+export const handleEarlyAccessDesynchronization = async ({
+    userSettings,
+    earlyAccessScope,
+    appName,
+}: {
+    userSettings: UserSettings;
+    earlyAccessScope: Feature<Environment> | undefined;
+    appName: APP_NAMES;
+}) => {
+    if (doesNotSupportEarlyAccessVersion()) {
+        return;
+    }
+
+    const currentRetries = getCurrentRetries() || 0;
+    if (currentRetries >= MAX_NUMBER_OF_EARLY_ACCESS_DESYNCHRONIZATION_RETRIES || currentRetries < 0) {
+        return;
+    }
+
+    const normalizedVersionCookieAtLoad = getVersionCookieIsValid(versionCookieAtLoad, earlyAccessScope)
+        ? versionCookieAtLoad
+        : undefined;
+    const targetEnvironment = getTargetEnvironment(earlyAccessScope, Boolean(userSettings.EarlyAccess));
+    const environmentIsDesynchronized = normalizedVersionCookieAtLoad !== targetEnvironment;
+    const isVpnSettings = appName === APPS.PROTONVPN_SETTINGS;
+
+    const shouldUpdateToEarlyAccessVersion = !isVpnSettings && environmentIsDesynchronized;
+    if (shouldUpdateToEarlyAccessVersion) {
+        setCurrentRetries(currentRetries + 1);
+        updateVersionCookie(targetEnvironment, earlyAccessScope);
+
+        // Allow awaiting on handleEarlyAccessDesynchronization to prevent
+        // further code executing before the page has chance to reload
+        return new Promise<void>(() => {
+            window.location.reload();
+        });
+    }
+
+    removeCurrentRetries();
+};
