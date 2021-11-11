5b36872207b733e06fe6b6cd12bb51ff56da08f9
mmso <mmso@mmso.se>
2021-11-11 17:00:57 +0100

    Use provider instead of detached state
    Must be a react context for storybook compatibility

--- packages/components/components/modalTwo/BackdropContainer.tsx
+++ packages/components/components/modalTwo/BackdropContainer.tsx
@@ -1,24 +1,12 @@
-import { useLayoutEffect, useState } from 'react';
 import Backdrop from './Backdrop';
 import { Portal } from '../portal';
-import { getModalsLength, subscribe } from './useModalPosition';
+import { useModalPositions } from './modalPositions';
 
 const BackdropContainer = () => {
-    const [modalsLength, setModalsLength] = useState(() => getModalsLength());
-
-    useLayoutEffect(() => {
-        const sync = () => {
-            setModalsLength(getModalsLength());
-        };
-        const unsubscribe = subscribe(sync);
-        return () => {
-            unsubscribe();
-        };
-    }, []);
-
+    const [modalPositions] = useModalPositions();
     return (
         <Portal>
-            <Backdrop entering={modalsLength >= 1} exiting={modalsLength === 0} />
+            <Backdrop entering={modalPositions.length >= 1} exiting={modalPositions.length === 0} />
         </Portal>
     );
 };
