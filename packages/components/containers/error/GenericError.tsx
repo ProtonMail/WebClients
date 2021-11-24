43500ab00b0efcc63b7c3057fc7416800f5a2e1f
Matthieu Lux <matthieu.lux@protonmail.com>
2021-11-24 15:21:48 +0100

    Add error boundaries on all part of mail app, improve generic error with a few options
    MAILWEB-2707

--- packages/components/containers/error/GenericError.tsx
+++ packages/components/containers/error/GenericError.tsx
@@ -5,31 +5,43 @@ import errorImg from '@proton/styles/assets/img/errors/generic-error.svg';
 
 import IllustrationPlaceholder from '../illustration/IllustrationPlaceholder';
 import InlineLinkButton from '../../components/button/InlineLinkButton';
+import { classnames } from '../../helpers';
 
 interface Props {
     className?: string;
     children?: ReactNode;
+    small?: boolean;
 }
 
-const GenericError = ({ className, children }: Props) => {
+const GenericError = ({ children, className, small = false }: Props) => {
     const refresh = (
         <InlineLinkButton key="1" onClick={() => window.location.reload()}>{c('Action')
             .t`refresh the page`}</InlineLinkButton>
     );
 
+    const title = c('Error message').t`Oops, something went wrong`;
+    const line1 = c('Error message').t`Brace yourself till we get the error fixed.`;
+    const line2 = c('Error message').jt`You may also ${refresh} or try again later.`;
+
     return (
-        <IllustrationPlaceholder
-            className={className}
-            title={c('Error message').t`Oops, something went wrong`}
-            url={errorImg}
-        >
-            {children || (
+        <div className={classnames(['mauto', className])}>
+            {small ? (
                 <>
-                    <span>{c('Error message').t`Brace yourself till we get the error fixed.`}</span>
-                    <span>{c('Error message').jt`You may also ${refresh} or try again later.`}</span>
+                    <h2 className="text-bold">{title}</h2>
+                    <div className="text-center">{line1}</div>
+                    <div className="text-center">{line2}</div>
                 </>
+            ) : (
+                <IllustrationPlaceholder title={title} url={errorImg}>
+                    {children || (
+                        <>
+                            <span>{line1}</span>
+                            <span>{line2}</span>
+                        </>
+                    )}
+                </IllustrationPlaceholder>
             )}
-        </IllustrationPlaceholder>
+        </div>
     );
 };
 
