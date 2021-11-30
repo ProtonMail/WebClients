51e7ff606473c9f0e35aca725886d8beae41a0cf
Nicolas Hoffmann <dante3333@gmail.com>
2021-11-30 16:36:37 +0100

    Fix Details/Summary icon when opened
    Spotted by Drive QA, the icon was not rotating when opened.
    
    - fix styles
    - added storybook examples
    
    CP-2993 DRVWEB-1587

---
+++ applications/storybook/src/stories/components/Details.stories.tsx
@@ -0,0 +1,35 @@
+import { Details, Summary } from '@proton/components';
+import { getTitle } from '../../helpers/title';
+import mdx from './Details.mdx';
+
+export default {
+    component: Details,
+    title: getTitle(__filename, false),
+    parameters: {
+        docs: {
+            page: mdx,
+        },
+    },
+};
+
+export const Basic = () => {
+    return (
+        <>
+            <Details>
+                <Summary>Here a summary</Summary>
+                Lorem Ipsum dolor si amet.
+            </Details>
+        </>
+    );
+};
+
+export const Opened = () => {
+    return (
+        <>
+            <Details open>
+                <Summary>Here a summary opened</Summary>
+                Lorem Ipsum dolor si amet.
+            </Details>
+        </>
+    );
+};
