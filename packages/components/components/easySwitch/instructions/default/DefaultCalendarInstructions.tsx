092b7bf1ab84be779e7328a8882524976daa7a5b
Jeremy Benaim <jeremybenaim@gmail.com>
2021-12-07 11:13:47 +0000

    Remove useless punctuation

--- packages/components/components/easySwitch/instructions/default/DefaultCalendarInstructions.tsx
+++ packages/components/components/easySwitch/instructions/default/DefaultCalendarInstructions.tsx
@@ -14,7 +14,7 @@ const DefaultCalendarInstructions = () => {
     );
     // translator: full sentence: "To import a calendar to Proton, you need the ICS file. Download this before you start the import process."
     const defaultCalendarMessage = c('Import instructions')
-        .jt`To import a calendar to Proton, you need the ${knowledgeBaseLink}. Download this before you start the import process.`;
+        .jt`To import a calendar to Proton, you need the ${knowledgeBaseLink}. Download this before you start the import process`;
 
     return <div className="mb1">{defaultCalendarMessage}</div>;
 };
