9418780a7c0b48aad9277beb6dc93089f0403466
Jeremy Benaim <jeremybenaim@gmail.com>
2021-12-07 09:56:40 +0000

    Update missing links

--- packages/components/components/easySwitch/instructions/default/DefaultCalendarInstructions.tsx
+++ packages/components/components/easySwitch/instructions/default/DefaultCalendarInstructions.tsx
@@ -1,8 +1,20 @@
 import { c } from 'ttag';
 
+import { Href } from '../../../link';
+
 const DefaultCalendarInstructions = () => {
+    // translator: full sentence: "To import a calendar to Proton, you need the ICS file. Download this before you start the import process."
+    const knowledgeBaseLink = (
+        <Href
+            url="https://protonmail.com/support/knowledge-base/import-calendar-to-protoncalendar/"
+            key="knowledgeBaseLink"
+        >
+            {c('Import instructions link').t`ICS file`}
+        </Href>
+    );
+    // translator: full sentence: "To import a calendar to Proton, you need the ICS file. Download this before you start the import process."
     const defaultCalendarMessage = c('Import instructions')
-        .t`To import a calendar to Proton, you need the ICS file. Download this before you start the import process.`;
+        .jt`To import a calendar to Proton, you need the ${knowledgeBaseLink}. Download this before you start the import process.`;
 
     return <div className="mb1">{defaultCalendarMessage}</div>;
 };
--- packages/components/components/easySwitch/instructions/default/DefaultMailInstructions.tsx
+++ packages/components/components/easySwitch/instructions/default/DefaultMailInstructions.tsx
@@ -3,19 +3,19 @@ import { c } from 'ttag';
 import { Href } from '../../../link';
 
 const DefaultMailInstructions = () => {
-    // translator: full sentence: "Depending on your email service provider, you may need to generate an app password or enable IMAP first before you can import to Proton. Learn more about importing emails to Proton."
+    // translator: full sentence: "Depending on your email service provider, you may need to generate an app password or enable IMAP first before you can import to Proton."
     const knowledgeBaseLink = (
         <Href
             url="https://protonmail.com/support/knowledge-base/allowing-imap-access-and-entering-imap-details/"
             key="knowledgeBaseLink"
         >
-            {c('Import instructions link').t`importing emails`}
+            {c('Import instructions link').t`app password or enable IMAP`}
         </Href>
     );
 
-    // translator: full sentence: "Depending on your email service provider, you may need to generate an app password or enable IMAP first before you can import to Proton. Learn more about importing emails to Proton."
+    // translator: full sentence: "Depending on your email service provider, you may need to generate an app password or enable IMAP first before you can import to Proton."
     const defaultAppPasswordMessage = c('Import instructions')
-        .jt`Depending on your email service provider, you may need to generate an app password or enable IMAP first before you can import to Proton. Learn more about ${knowledgeBaseLink} to Proton.`;
+        .jt`Depending on your email service provider, you may need to generate an ${knowledgeBaseLink} first before you can import to Proton.`;
 
     return <div className="mb1">{defaultAppPasswordMessage}</div>;
 };
