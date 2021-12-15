c611a8e06c002bee52d1c0a312bad273960c2b9b
Eduardo Conde Pena <econdepe@proton.ch>
2021-12-15 13:45:39 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- packages/shared/lib/contacts/vcard.ts
+++ packages/shared/lib/contacts/vcard.ts
@@ -120,7 +120,7 @@ export const toICAL = (properties: ContactProperties = []) => {
         const fieldWithGroup = [group, field].filter(isTruthy).join('.');
         const property = new ICAL.Property(fieldWithGroup);
 
-        if (['bday', 'anniversary'].includes(field) && !isValidDate(parseISO(value as string))) {
+        if (['bday', 'anniversary'].includes(field) && !isValidDate(parseISO(value))) {
             property.resetType('text');
         }
 
