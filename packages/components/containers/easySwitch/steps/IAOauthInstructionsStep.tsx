c1d589d52a6b65eef827046d8895950ca3317325
Jeremy Benaim <jeremybenaim@gmail.com>
2021-12-21 10:55:59 +0100

    Oauth instructions copy change

--- packages/components/containers/easySwitch/steps/IAOauthInstructionsStep.tsx
+++ packages/components/containers/easySwitch/steps/IAOauthInstructionsStep.tsx
@@ -5,19 +5,28 @@ import oauthInstructionsMp4 from '@proton/styles/assets/videos/easySwitch/oauth-
 
 import { VideoInstructions } from '../../../components';
 
-const IAOauthInstructionsStep = () => (
-    <>
-        <div className="mb2">
-            {c('Oauth instructions')
-                .t`Please note that you need to accept all the permissions requested by the application to be able to start your import.`}
-        </div>
-        <div className="text-center mb1 relative">
-            <VideoInstructions>
-                <source src={oauthInstructionsWebm} type="video/webm" />
-                <source src={oauthInstructionsMp4} type="video/mp4" />
-            </VideoInstructions>
-        </div>
-    </>
-);
+const IAOauthInstructionsStep = () => {
+    // translator: full sentence: "Please note that in the next step you need to select all the permissions requested by the application to start your import"
+    const boldNextStep = (
+        <strong key="boldNextStep">{c('Import instructions emphasis')
+            .t`in the next step you need to select all the permissions`}</strong>
+    );
+
+    // translator: full sentence: "Please note that in the next step you need to select all the permissions requested by the application to start your import"
+    const instructions = c('Oauth instructions')
+        .jt`Please note that ${boldNextStep} requested by the application to start your import.`;
+
+    return (
+        <>
+            <div className="mb2">{instructions}</div>
+            <div className="text-center mb1 relative">
+                <VideoInstructions>
+                    <source src={oauthInstructionsWebm} type="video/webm" />
+                    <source src={oauthInstructionsMp4} type="video/mp4" />
+                </VideoInstructions>
+            </div>
+        </>
+    );
+};
 
 export default IAOauthInstructionsStep;
