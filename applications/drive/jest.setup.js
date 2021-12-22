b6e253f9ede8ed892004a0eaee12e692be3bc2f1
mmso <mmso@mmso.se>
2021-12-22 12:25:44 +0100

    Add srp mocks

--- applications/drive/jest.setup.js
+++ applications/drive/jest.setup.js
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
+import { TextEncoder, TextDecoder } from 'util';
+
+// Getting ReferenceError: TextDecoder is not defined without
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
