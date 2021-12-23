90da42247b3ec07fd2245290f6030726c340175f
econdepe <econdepe@protonmail.com>
2021-12-23 23:34:45 +0100

    Deprecate PersonalEvent in favor of PersonalEvents in CalendarEvent API interface
    CALWEB-3186

--- packages/shared/lib/interfaces/calendar/Event.ts
+++ packages/shared/lib/interfaces/calendar/Event.ts
@@ -46,7 +46,7 @@ export interface CalendarEventBlobData {
     CalendarEvents: CalendarEventData[];
     SharedKeyPacket: string;
     SharedEvents: CalendarEventData[];
-    PersonalEvent: CalendarPersonalEventData[];
+    PersonalEvents: CalendarPersonalEventData[];
     AttendeesEvents: CalendarEventData[];
     Attendees: Attendee[];
 }
