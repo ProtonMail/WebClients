7c8bc88d4532d4510a49f26f7c871e5ef7c835e2
mmso <mmso@mmso.se>
2021-12-28 18:10:55 +0100

    Fix account test setup

--- applications/account/jest.setup.js
+++ applications/account/jest.setup.js
@@ -1,5 +1,4 @@
-import '@testing-library/react/cleanup-after-each';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 
 // Silence warnings on expect to throw https://github.com/testing-library/react-testing-library/issues/157
 console.error = () => {};
