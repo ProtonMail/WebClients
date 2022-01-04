61b621b15e7d66aa4590341c73c0f9c1cf5f6635
mmso <mmso@mmso.se>
2022-01-04 11:50:58 +0100

    Update request new code with new modal

--- packages/components/containers/api/humanVerification/RequestNewCodeModal.tsx
+++ packages/components/containers/api/humanVerification/RequestNewCodeModal.tsx
@@ -1,6 +1,6 @@
 import { c } from 'ttag';
 
-import { Button, FormModal } from '../../../components';
+import { AlertModal, Button } from '../../../components';
 import { useLoading } from '../../../hooks';
 import { VerificationModel } from './interface';
 
@@ -8,62 +8,55 @@ interface Props {
     verificationModel: VerificationModel;
     onEdit: () => void;
     onResend: () => Promise<void>;
-
-    [key: string]: any;
+    onClose: () => void;
+    open: boolean;
 }
 
-const RequestNewCodeModal = ({ verificationModel, onEdit, onResend, ...rest }: Props) => {
+const RequestNewCodeModal = ({ open, verificationModel, onEdit, onResend, onClose }: Props) => {
     const strong = <strong key="email">{verificationModel.value}</strong>;
     const [loading, withLoading] = useLoading();
     return (
-        <FormModal
+        <AlertModal
+            open={open}
             title={c('Title').t`Request new code`}
-            mode="alert"
-            loading={loading}
-            noTitleEllipsis
-            footer={
-                <>
-                    <Button
-                        size="large"
-                        color="norm"
-                        type="button"
-                        fullWidth
-                        loading={loading}
-                        onClick={async () => {
-                            await withLoading(onResend());
-                            rest.onClose?.();
-                        }}
-                    >
-                        {c('Action').t`Request new code`}
-                    </Button>
-                    <Button
-                        size="large"
-                        color="weak"
-                        type="button"
-                        onClick={() => {
-                            rest.onClose?.();
-                            onEdit();
-                        }}
-                        disabled={loading}
-                        fullWidth
-                    >
-                        {verificationModel.method === 'email'
-                            ? c('Action').t`Edit email address`
-                            : c('Action').t`Edit phone number`}
-                    </Button>
-                    <Button size="large" color="weak" type="button" onClick={rest.onClose} disabled={loading} fullWidth>
-                        {c('Action').t`Cancel`}
-                    </Button>
-                </>
-            }
-            {...rest}
+            buttons={[
+                <Button
+                    color="norm"
+                    type="button"
+                    fullWidth
+                    loading={loading}
+                    onClick={async () => {
+                        await withLoading(onResend());
+                        onClose();
+                    }}
+                >
+                    {c('Action').t`Request new code`}
+                </Button>,
+                <Button
+                    color="weak"
+                    type="button"
+                    onClick={() => {
+                        onClose();
+                        onEdit();
+                    }}
+                    disabled={loading}
+                    fullWidth
+                >
+                    {verificationModel.method === 'email'
+                        ? c('Action').t`Edit email address`
+                        : c('Action').t`Edit phone number`}
+                </Button>,
+                <Button color="weak" type="button" onClick={onClose} disabled={loading} fullWidth>
+                    {c('Action').t`Cancel`}
+                </Button>,
+            ]}
         >
             {verificationModel.method === 'email'
                 ? c('Info')
                       .jt`Click "Request new code" to have a new verification code sent to ${strong}. If this email address is incorrect, click "Edit" to correct it.`
                 : c('Info')
                       .jt`Click "Request new code" to have a new verification code sent to ${strong}. If this phone number is incorrect, click "Edit" to correct it.`}
-        </FormModal>
+        </AlertModal>
     );
 };
 
