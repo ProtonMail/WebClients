6933b1d5df9d7b3e56569f14cc894571450eae13
Marco <marco.martinoli@protonmail.com>
2022-01-10 18:25:35 +0100

    Update VRF

--- packages/key-transparency/lib/constants.ts
+++ packages/key-transparency/lib/constants.ts
@@ -11,4 +11,10 @@ export enum KT_STATUS {
     KTERROR_MINEPOCHID_NULL,
 }
 
+// VRF constants
 export const vrfHexKey = '2d7688feb429f714f102f758412cd4b81337b307122770f620ad9e4ac898a2eb';
+export const N = 16;
+export const ptLen = 2 * N; // = qLen
+export const CO_FACTOR = 8;
+export const LEFT_N = 1; // left neighbour
+export const KT_LEN = 32;
