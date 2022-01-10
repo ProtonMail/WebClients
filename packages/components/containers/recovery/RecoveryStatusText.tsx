84889be3e954d764afcf904a52ea4ac995ae9914
Jack Crompton <jack.crompton@proton.ch>
2022-01-10 11:06:11 +0000

    Update recovery status color to warning
    This keeps the notification color and status color consistent

--- packages/components/containers/recovery/RecoveryStatusText.tsx
+++ packages/components/containers/recovery/RecoveryStatusText.tsx
@@ -13,7 +13,7 @@ const RecoveryStatusText = ({ status, className, ...rest }: Props) => {
 
     if (status === 'intermediate' || status === 'incomplete') {
         config = {
-            className: 'color-danger',
+            className: 'color-warning',
         };
     }
 
