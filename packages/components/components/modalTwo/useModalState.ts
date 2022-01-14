d6cb14e44cf4111ba091761ffb918ba5218d543a
WebAtProton <matthias.margot@proton.ch>
2022-01-14 17:31:28 +0100

    Add useModalState hook to force-clear local child state

---
+++ packages/components/components/modalTwo/useModalState.ts
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+import { useControlled } from '../../hooks';
+import { ModalProps } from './Modal';
+
+const useModalState = (options?: { open?: boolean; onClose?: () => void; onExit?: () => void }) => {
+    const { open: controlledOpen, onClose, onExit } = options || {};
+
+    const [key, setKey] = useState(0);
+
+    const [open, setOpen] = useControlled(controlledOpen);
+
+    const handleClose = () => {
+        setOpen(false);
+        onClose?.();
+    };
+
+    const handleExit = () => {
+        setKey(key + 1);
+        onExit?.();
+    };
+
+    const modalProps: Partial<ModalProps> = {
+        key,
+        open,
+        onClose: handleClose,
+        onExit: handleExit,
+    };
+
+    return [modalProps, setOpen] as const;
+};
+
+export default useModalState;
