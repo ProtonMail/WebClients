350ebb66035b24066f47acb9c8768dd50bc80832
Guillaume Gonzalez <guillaume.gonzalez@proton.ch>
2022-01-18 15:58:54 +0100

    Add documentation about sandbox attribute

--- applications/mail/src/app/components/message/helpers/getIframeSandboxAttributes.ts
+++ applications/mail/src/app/components/message/helpers/getIframeSandboxAttributes.ts
@@ -2,11 +2,23 @@ import { isSafari } from '@proton/shared/lib/helpers/browser';
 
 const getIframeSandboxAttributes = (isPrint: boolean) => {
     /**
-     * "allow-modals" : For being able to print emails
+     * "allow-same-origin"
+     *      Because iframe origin is set to protonmail.com we need
+     *      to be able to access it's origin
+     * "allow-modals"
+     *      Allow to be able to print emails
+     * "allow-scripts"
+     *      allows react portals to execute correctly in Safari
+     * "allow-popups"
+     *      allows target="_blank" links to work
+     * "allow-popups-to-escape-sandbox"
+     *      allows to open links in non sandboxed mode
+     *      for ex : if allow-scripts is not present opened links are allowed to execute scripts
      *
-     * "allow-scripts" : allows react portals to execute correctly in Safari
-     *
-     * "allow-popups-to-escape-sandbox" allows to open links in non modal sandboxed mode
+     * ATM we do not need to allow the following options
+     * - allow-forms
+     * - allow-pointer-lock
+     * - allow-top-navigation
      */
     const sandboxAttributes: string = [
         'allow-same-origin',
