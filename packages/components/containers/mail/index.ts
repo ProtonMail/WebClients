b4135c4ed1ba0ed3d6346e161546b916ab362fc2
mmso <mmso@mmso.se>
2022-01-20 11:47:45 +0100

    Move mail settings dropdown to mail

--- packages/components/containers/mail/index.ts
+++ packages/components/containers/mail/index.ts
@@ -1,1 +1,4 @@
 export { default as MailShortcutsModal } from './MailShortcutsModal';
+export { default as MailDensityModal } from './MailDensityModal';
+export { default as MailViewLayoutModal } from './MailViewLayoutModal';
+export { default as MailComposerModeModal } from './MailComposerModeModal';
