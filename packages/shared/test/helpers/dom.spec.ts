9e41cceac80fd28bf10833f70f5c7a74c8fa67ca
Richard <richard@protonmail.com>
2022-01-23 20:41:05 +0100

    Add dom tests

--- packages/shared/test/helpers/dom.spec.ts
+++ packages/shared/test/helpers/dom.spec.ts
@@ -1,7 +1,43 @@
-import { countAllNodesRecursively } from '../../lib/helpers/dom';
+import { countAllNodesRecursively, getMaxDepth } from '../../lib/helpers/dom';
 
 describe('countAllNodesRecursively', () => {
     it('should count node', () => {
         expect(countAllNodesRecursively(document.createElement('div'))).toBe(1);
+    });
+
+    it('should count node with children', () => {
+        expect(countAllNodesRecursively(document.createElement('div').appendChild(document.createElement('div')))).toBe(
+            2
+        );
+    });
+
+    it('should count node with children and grandchildren', () => {
+        expect(
+            countAllNodesRecursively(
+                document
+                    .createElement('div')
+                    .appendChild(document.createElement('div').appendChild(document.createElement('div')))
+            )
+        ).toBe(3);
+    });
+});
+
+describe('getMaxDepth', () => {
+    it('should get max depth', () => {
+        expect(getMaxDepth(document.createElement('div'))).toBe(1);
+    });
+
+    it('should get max depth with children', () => {
+        expect(getMaxDepth(document.createElement('div').appendChild(document.createElement('div')))).toBe(2);
+    });
+
+    it('should get max depth with children and grandchildren', () => {
+        expect(
+            getMaxDepth(
+                document
+                    .createElement('div')
+                    .appendChild(document.createElement('div').appendChild(document.createElement('div')))
+            )
+        ).toBe(3);
     });
 });
