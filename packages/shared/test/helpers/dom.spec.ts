cc8d96532092fdee6eaf4c001f2ab536c9cc1d76
Richard <richard@protonmail.com>
2022-01-24 08:42:51 +0100

    Add tests for getMaxDepth

--- packages/shared/test/helpers/dom.spec.ts
+++ packages/shared/test/helpers/dom.spec.ts
@@ -1,57 +1,61 @@
-import { countAllNodesRecursively, getMaxDepth, hasChildren } from '../../lib/helpers/dom';
-
-describe('countAllNodesRecursively', () => {
-    it('should count node', () => {
-        expect(countAllNodesRecursively(document.createElement('div'))).toBe(1);
-    });
-
-    it('should count node with children', () => {
-        expect(countAllNodesRecursively(document.createElement('div').appendChild(document.createElement('div')))).toBe(
-            2
-        );
-    });
-
-    it('should count node with children and grandchildren', () => {
-        expect(
-            countAllNodesRecursively(
-                document
-                    .createElement('div')
-                    .appendChild(document.createElement('div').appendChild(document.createElement('div')))
-            )
-        ).toBe(3);
-    });
-});
-
-describe('getMaxDepth', () => {
-    it('should get max depth', () => {
-        expect(getMaxDepth(document.createElement('div'))).toBe(1);
-    });
-
-    it('should get max depth with children', () => {
-        expect(getMaxDepth(document.createElement('div').appendChild(document.createElement('div')))).toBe(2);
-    });
-
-    it('should get max depth with children and grandchildren', () => {
-        expect(
-            getMaxDepth(
-                document
-                    .createElement('div')
-                    .appendChild(document.createElement('div').appendChild(document.createElement('div')))
-            )
-        ).toBe(3);
-    });
-});
+import { getMaxDepth, hasChildren } from '../../lib/helpers/dom';
 
 describe('hasChildren', () => {
     it('should return false for text node', () => {
         expect(hasChildren(document.createTextNode('text'))).toBe(false);
     });
 
-    it('should return true for element', () => {
-        expect(hasChildren(document.createElement('div'))).toBe(true);
+    it('should return false for element', () => {
+        expect(hasChildren(document.createElement('div'))).toBe(false);
     });
 
-    it('should return false for element without children', () => {
-        expect(hasChildren(document.createElement('div').appendChild(document.createTextNode('text')))).toBe(false);
+    it('should return true for element with children', () => {
+        const div = document.createElement('div');
+        div.appendChild(document.createElement('div'));
+        expect(hasChildren(div)).toBe(true);
+    });
+});
+
+describe('getMaxDepth', () => {
+    beforeEach(() => {
+        document.body.innerHTML = `
+            <div id="parent">
+                <div id="child">
+                    <div id="grandchild">
+                        <div id="greatgrandchild">
+                        </div>
+                    </div>
+                </div>
+                <div></div>
+            </div>
+        `;
+    });
+
+    it('should return 1 for text node', () => {
+        expect(getMaxDepth(document.createTextNode('text'))).toBe(1);
+    });
+
+    it('should return 1 for element', () => {
+        expect(getMaxDepth(document.createElement('div'))).toBe(1);
+    });
+
+    it('should return 4 for parent with children', () => {
+        const div = document.getElementById('parent');
+        expect(getMaxDepth(div as HTMLDivElement)).toBe(4);
+    });
+
+    it('should return 3 for element with children', () => {
+        const div = document.getElementById('child');
+        expect(getMaxDepth(div as HTMLDivElement)).toBe(3);
+    });
+
+    it('should return 2 for element with grandchildren', () => {
+        const div = document.getElementById('grandchild');
+        expect(getMaxDepth(div as HTMLDivElement)).toBe(2);
+    });
+
+    it('should return 1 for element with greatgrandchildren', () => {
+        const div = document.getElementById('greatgrandchild');
+        expect(getMaxDepth(div as HTMLDivElement)).toBe(1);
     });
 });
