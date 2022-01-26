daef076da7e71f3169c46844900ca04132389c8e
mmso <mmso@mmso.se>
2022-01-26 16:43:00 +0100

    Preload phone input
    To prevent users not being able to dynamically load the phone input
    component during deployments, let's preload it in the browser

--- packages/components/components/v2/phone/LazyPhoneInput.tsx
+++ packages/components/components/v2/phone/LazyPhoneInput.tsx
@@ -2,7 +2,7 @@ import { lazy, Suspense } from 'react';
 import Loader from '../../loader/Loader';
 import { Props as PhoneInputProps } from './PhoneInput';
 
-const PhoneInput = lazy(() => import('./PhoneInput'));
+const PhoneInput = lazy(() => import(/* webpackPreload: true */ './PhoneInput'));
 
 const LazyPhoneInput = (props: PhoneInputProps) => {
     return (
