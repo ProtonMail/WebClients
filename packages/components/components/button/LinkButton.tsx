0550e676c37b155fb1ca7455bad8a49f34e83f69
Matthieu Bué <matthieu.bue@gmail.com>
2022-02-02 18:28:06 +0100

    Rename `link` shape to `underline`
    To avoid any misunderstanding between a `Button` with a `link` shape
    and an `inlineLinkButton`, we have to rename that shape,
    so let's rename it to `underline`, exactly what it looks like.
    
    The corresponding classname is renamed as well. The doc is updated.

--- packages/components/components/button/LinkButton.tsx
+++ packages/components/components/button/LinkButton.tsx
@@ -4,7 +4,7 @@ import Button, { ButtonProps } from './Button';
 export type LinkButtonProps = Omit<ButtonProps, 'shape'>;
 
 const LinkButton = (props: LinkButtonProps, ref: Ref<HTMLButtonElement>) => {
-    return <Button shape="link" color="norm" ref={ref} {...props} />;
+    return <Button shape="underline" color="norm" ref={ref} {...props} />;
 };
 
 export default forwardRef<HTMLButtonElement, LinkButtonProps>(LinkButton);
