42ef76bd45344f9be37a07ac14f074dd887e5741
Jack Crompton <jack.crompton@proton.ch>
2022-02-08 11:42:51 +0000

    Rename 'Proton Technologies AG' to 'Proton AG'

--- applications/account/src/assets/logoConfig.js
+++ applications/account/src/assets/logoConfig.js
@@ -4,7 +4,7 @@ module.exports = {
         appName: 'ProtonAccount',
         appDescription:
             "ProtonMail is the world's largest secure email service, developed by CERN and MIT scientists. We are open source and protected by Swiss privacy law",
-        developerName: 'Proton Technologies AG',
+        developerName: 'Proton AG',
         developerURL: 'https://github.com/ProtonMail/proton-account',
         background: '#1c223d',
         theme_color: '#1c223d',
