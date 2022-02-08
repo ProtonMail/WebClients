2dc1da0c2a49b7fbd60e24e5071242c9900c6778
Jack Crompton <jack.crompton@proton.ch>
2022-02-08 11:52:09 +0000

    Update developerURL in logoConfig.js files

--- applications/calendar/src/assets/logoConfig.js
+++ applications/calendar/src/assets/logoConfig.js
@@ -5,7 +5,7 @@ module.exports = {
         appDescription:
             'Keep your plans private with an encrypted, open source calendar from the developers of ProtonMail.',
         developerName: 'Proton AG',
-        developerURL: 'https://github.com/ProtonMail/proton-calendar',
+        developerURL: 'https://github.com/ProtonMail/WebClients',
         background: '#1c223d',
         theme_color: '#1c223d',
     },
--- applications/vpn-settings/src/assets/logoConfig.js
+++ applications/vpn-settings/src/assets/logoConfig.js
@@ -4,7 +4,7 @@ module.exports = {
         appName: 'ProtonVPN Settings',
         appDescription: 'Use ProtonVPN and protect your online activity',
         developerName: 'Proton AG',
-        developerURL: 'https://github.com/ProtonMail/protonvpn-settings', // prevent retrieving from the nearest package.json
+        developerURL: 'https://github.com/ProtonMail/WebClients',
         background: '#ddd',
         theme_color: '#262a33',
     },
