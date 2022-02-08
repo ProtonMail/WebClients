576cbf91d423270258003c988e6ae537c01319f1
Jack Crompton <jack.crompton@proton.ch>
2022-02-08 11:52:09 +0000

    Update developerURL in logoConfig.js files

--- applications/verify/src/assets/logoConfig.js
+++ applications/verify/src/assets/logoConfig.js
@@ -5,9 +5,8 @@ module.exports = {
         appDescription:
             "ProtonMail is the world's largest secure email service, developed by CERN and MIT scientists. We are open source and protected by Swiss privacy law",
         developerName: 'Proton AG',
-        developerURL: 'https://github.com/ProtonMail/proton-mail',
+        developerURL: 'https://github.com/ProtonMail/WebClients',
         background: '#1c223d',
-        // eslint-disable-next-line @typescript-eslint/naming-convention
         theme_color: '#1c223d',
     },
 };
