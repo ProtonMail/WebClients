0bffd9db2a0eaac1155bd3df57787efdb1dabbc1
Matthieu Bué <matthieu.bue@gmail.com>
2022-02-08 11:53:33 +0100

    Still cleaning up
    CP-2881

---
+++ applications/storybook/src/stories/components/InlineLinkButton.stories.tsx
@@ -0,0 +1,36 @@
+import { InlineLinkButton } from '@proton/components';
+
+import { getTitle } from '../../helpers/title';
+import mdx from './InlineLinkButton.mdx';
+
+export default {
+    component: InlineLinkButton,
+    title: getTitle(__filename, false),
+    parameters: {
+        docs: {
+            page: mdx,
+        },
+    },
+};
+
+export const Basic = () => (
+    <p>
+        Mr. Levet this day shewed me Dr. Johnsonʼs library, which was contained in two garrets{' '}
+        <InlineLinkButton onClick={() => alert('See? This is a real button.')}>over his Chambers</InlineLinkButton>,
+        where Lintot, son of the celebrated bookseller of that name, had formerly his warehouse. I found a number of
+        good books, but very dusty and in great confusion. The floor was strewed with manuscript leaves, in Johnsonʼs
+        own handwriting, which I beheld with a degree of veneration, supposing they perhaps might contain portions of
+        The Rambler or of Rasselas. I observed an apparatus for{' '}
+        <InlineLinkButton onClick={() => alert('See? This is a real button.')}>chymical experiments</InlineLinkButton>,
+        of which Johnson was all his life very fond. The place seemed to be very favourable for{' '}
+        <InlineLinkButton onClick={() => alert('See? This is a real button.')}>
+            retirement and meditation
+        </InlineLinkButton>
+        . Johnson told me, that he went up thither without mentioning it to his servant, when he wanted to study, secure
+        from interruption; for he would not allow his servant to say he was not at home when he really was. "A servantʼs
+        strict regard for truth, — said he — must be weakened by such a practice. that it is merely a form of denial{' '}
+        <InlineLinkButton onClick={() => alert('See? This is a real button.')}>A philosopher may know</InlineLinkButton>
+        ; but few servants are such nice distinguishers. If I accustom a servant to tell a lie for ME, have I not reason
+        to apprehend that he will tell many lies for HIMSELF."
+    </p>
+);
