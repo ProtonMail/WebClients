4483f78451e48f7ed03e1281ac9a97de84a88190
Jack Crompton <jack.crompton@proton.ch>
2022-02-09 17:21:24 +0000

    Add recovery settings kb article links

--- packages/components/containers/recovery/RecoverDataCard.tsx
+++ packages/components/containers/recovery/RecoverDataCard.tsx
@@ -1,7 +1,7 @@
 import { c } from 'ttag';
 
 import { classnames } from '../../helpers/component';
-import { Card, Icon } from '../../components';
+import { Card, Icon, LearnMore } from '../../components';
 import { SettingsSectionTitle } from '../account';
 import ReactivateKeysButton from '../keys/reactivateKeys/ReactivateKeysButton';
 
@@ -16,8 +16,12 @@ const RecoverDataCard = ({ className }: Props) => {
                 <Icon className="flex-item-noshrink color-danger" name="circle-exclamation-filled" size={18} />
                 <span className="ml0-5">{c('Title').t`Data locked`}</span>
             </SettingsSectionTitle>
-            <p>{c('Info')
-                .t`It appears some of your data is encrypted and locked. This is probably due to a recent or previous password reset. We suggest you use a data recovery method to unlock your data.`}</p>
+            <p>
+                {c('Info')
+                    .t`It appears some of your data is encrypted and locked. This is probably due to a recent or previous password reset. We suggest you use a data recovery method to unlock your data.`}
+                <br />
+                <LearnMore url="https://protonmail.com/support/knowledge-base/recover-encrypted-messages-files" />
+            </p>
             <ReactivateKeysButton>{c('Action').t`Unlock data`}</ReactivateKeysButton>
         </Card>
     );
