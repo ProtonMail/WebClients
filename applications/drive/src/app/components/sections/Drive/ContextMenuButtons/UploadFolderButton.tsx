3f5d3413b6a34cc1ddb64a1dc08af5476dc3c1af
Michal Horejsek <michal.horejsek@proton.ch>
2022-02-09 10:12:18 +0100

    Fix upload from context menu

--- applications/drive/src/app/components/sections/Drive/ContextMenuButtons/UploadFolderButton.tsx
+++ applications/drive/src/app/components/sections/Drive/ContextMenuButtons/UploadFolderButton.tsx
@@ -1,32 +1,21 @@
 import { c } from 'ttag';
 
-import useActiveShare from '../../../../hooks/drive/useActiveShare';
-import { useFolderUploadInput } from '../../../../store';
 import { ContextMenuButton } from '../../ContextMenu';
 
 interface Props {
     close: () => void;
+    onClick: () => void;
 }
 
-const UploadFolderButton = ({ close }: Props) => {
-    const { activeFolder } = useActiveShare();
-    const {
-        inputRef: fileInput,
-        handleClick: handleUploadFolder,
-        handleChange,
-    } = useFolderUploadInput(activeFolder.shareId, activeFolder.linkId);
-
+const UploadFolderButton = ({ close, onClick }: Props) => {
     return (
-        <>
-            <input multiple type="file" ref={fileInput} className="hidden" onChange={handleChange} />
-            <ContextMenuButton
-                testId="toolbar-upload-folder"
-                icon="folder-arrow-up"
-                name={c('Action').t`Upload folder`}
-                action={handleUploadFolder}
-                close={close}
-            />
-        </>
+        <ContextMenuButton
+            testId="toolbar-upload-folder"
+            icon="folder-arrow-up"
+            name={c('Action').t`Upload folder`}
+            action={onClick}
+            close={close}
+        />
     );
 };
 
