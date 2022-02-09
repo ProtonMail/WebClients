b7e873013d67127a093d221fb5b91d764f70b75e
Jack Crompton <jack.crompton@proton.ch>
2022-02-09 15:39:56 +0000

    Move NotificationsHijack to components/containers

--- applications/account/src/lite/App.tsx
+++ applications/account/src/lite/App.tsx
@@ -14,6 +14,7 @@ import {
     ApiProvider,
     CacheProvider,
     NotificationsChildren,
+    NotificationsHijack,
     CreateNotificationOptions,
 } from '@proton/components';
 import { initLocales } from '@proton/shared/lib/i18n/locales';
@@ -29,7 +30,6 @@ import * as config from '../app/config';
 import '../app/app.scss';
 import Setup from './Setup';
 import broadcast, { MessageType } from './broadcast';
-import NotificationsHijack from './NotificationsHijack';
 
 initLocales(require.context('../../locales', true, /.json$/, 'lazy'));
 
