e2dc48de6cef5b0c317fb7f256509f0bdee002a8
romain sanchez <romain.sanchez@proton.ch>
2022-02-11 13:14:11 +0100

    Update mail navbar settings modals to new modal structure
    MAILWEB-3050

--- packages/components/containers/mail/MailComposerModeModal.tsx
+++ packages/components/containers/mail/MailComposerModeModal.tsx
@@ -3,16 +3,12 @@ import { COMPOSER_MODE } from '@proton/shared/lib/constants';
 import { updateComposerMode } from '@proton/shared/lib/api/mailSettings';
 
 import { useApi, useLoading, useEventManager, useNotifications, useMailSettings } from '../../hooks';
-import { FormModal } from '../../components';
+import { Button, ModalProps, ModalTwo, ModalTwoContent, ModalTwoFooter, ModalTwoHeader } from '../../components';
 import ComposerModeCards from '../layouts/ComposerModeCards';
 
 import './ModalSettingsLayoutCards.scss';
 
-interface Props {
-    onClose?: () => void;
-}
-
-const MailComposerModeModal = ({ ...rest }: Props) => {
+const MailComposerModeModal = (props: ModalProps) => {
     const api = useApi();
     const { call } = useEventManager();
     const [{ ComposerMode = 0 } = {}] = useMailSettings();
@@ -20,30 +16,38 @@ const MailComposerModeModal = ({ ...rest }: Props) => {
     const { createNotification } = useNotifications();
     const title = c('Title').t`Composer mode`;
 
+    const { onClose } = props;
+
     const handleChangeComposerMode = async (mode: COMPOSER_MODE) => {
         await api(updateComposerMode(mode));
         await call();
         createNotification({ text: c('Success').t`Preference saved` });
     };
 
-    const handleSubmit = () => rest.onClose?.();
+    const handleSubmit = () => onClose?.();
 
     return (
-        <FormModal title={title} intermediate close={null} submit={c('Action').t`OK`} onSubmit={handleSubmit} {...rest}>
-            <div className="flex flex-nowrap mb1 on-mobile-flex-column flex-column">
-                <span className="mb1" id="composerMode_desc">
-                    {c('Label').t`Select how your composer opens by default.`}
-                </span>
-                <ComposerModeCards
-                    describedByID="composerMode_desc"
-                    composerMode={ComposerMode}
-                    onChange={(value) => withLoading(handleChangeComposerMode(value))}
-                    loading={loading}
-                    liClassName="w100"
-                    className="layoutCards-two-per-row"
-                />
-            </div>
-        </FormModal>
+        <ModalTwo {...props}>
+            <ModalTwoHeader title={title} />
+            <ModalTwoContent>
+                <div className="flex flex-nowrap mb1 on-mobile-flex-column flex-column">
+                    <span className="mb1" id="composerMode_desc">
+                        {c('Label').t`Select how your composer opens by default.`}
+                    </span>
+                    <ComposerModeCards
+                        describedByID="composerMode_desc"
+                        composerMode={ComposerMode}
+                        onChange={(value) => withLoading(handleChangeComposerMode(value))}
+                        loading={loading}
+                        liClassName="w100"
+                        className="layoutCards-two-per-row"
+                    />
+                </div>
+            </ModalTwoContent>
+            <ModalTwoFooter>
+                <Button className="mlauto" color="norm" onClick={handleSubmit}>{c('Action').t`OK`}</Button>
+            </ModalTwoFooter>
+        </ModalTwo>
     );
 };
 
