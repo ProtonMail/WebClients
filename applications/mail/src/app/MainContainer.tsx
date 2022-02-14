530632c964edd6ae145722ee0710d218772bf56e
Matthieu Lux <matthieu.lux@protonmail.com>
2022-02-14 12:02:18 +0100

    Add a feature flag to be able to unregister SW worker quickly if pb

--- applications/mail/src/app/MainContainer.tsx
+++ applications/mail/src/app/MainContainer.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import {
@@ -20,7 +20,8 @@ import { store } from './logic/store';
 const MainContainer = () => {
     const breakpoints = useActiveBreakpoint();
     const mailContentRef = useRef<HTMLDivElement>(null);
-    useFeatures([
+    const [{ feature: featureSw, loading: loadingSw }] = useFeatures([
+        FeatureCode.MailServiceWorker,
         FeatureCode.EarlyAccessScope,
         FeatureCode.ScheduledSend,
         FeatureCode.SpotlightScheduledSend,
@@ -31,6 +32,33 @@ const MainContainer = () => {
         FeatureCode.CalendarEmailNotification,
         FeatureCode.SpyTrackerProtection,
     ]);
+
+    // Service Worker registration
+    // Including a kill switch with a feature flag
+    useEffect(() => {
+        if ('serviceWorker' in navigator && !loadingSw) {
+            const unregister = async () => {
+                const registrations = await navigator.serviceWorker.getRegistrations();
+                registrations.forEach((registration) => {
+                    if (registration.scope === window.location.origin + '/') {
+                        void registration.unregister();
+                    }
+                });
+            };
+
+            const register = async () => {
+                try {
+                    await navigator.serviceWorker.register('/service-worker.js');
+                } catch {
+                    console.log('No service worker found');
+                }
+            };
+
+            const action = featureSw?.Value === true ? register : unregister;
+
+            void action();
+        }
+    }, [featureSw, loadingSw]);
 
     return (
         <ReduxProvider store={store}>
