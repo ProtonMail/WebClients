faa20db595239fd1d7d8ed80206a9224500b547f
romain sanchez <romain.sanchez@proton.ch>
2022-02-14 10:55:01 +0100

    Update filter modal to new modal structure MAILWEB-3071

--- packages/components/containers/filters/ActionsFilterToolbar.tsx
+++ packages/components/containers/filters/ActionsFilterToolbar.tsx
@@ -1,5 +1,5 @@
 import { c } from 'ttag';
-import { Button } from '../../components';
+import { Button, useModalState } from '../../components';
 import { useModals } from '../../hooks';
 
 import FilterModal from './modal/FilterModal';
@@ -8,15 +8,24 @@ import AdvancedFilterModal from './modal/advanced/AdvancedFilterModal';
 function ActionsFilterToolbar() {
     const { createModal } = useModals();
 
+    const [{ open: isFilterModalOpen, ...filterModalProps }, setFilterModalOpen] = useModalState();
+
     return (
-        <div className="mb1">
-            <Button color="norm" onClick={() => createModal(<FilterModal />)} className="on-mobile-mb0-5 mr1">
-                {c('Action').t`Add filter`}
-            </Button>
-            <Button shape="outline" onClick={() => createModal(<AdvancedFilterModal />)} className="on-mobile-mb0-5">
-                {c('Action').t`Add sieve filter`}
-            </Button>
-        </div>
+        <>
+            <div className="mb1">
+                <Button color="norm" onClick={() => setFilterModalOpen(true)} className="on-mobile-mb0-5 mr1">
+                    {c('Action').t`Add filter`}
+                </Button>
+                <Button
+                    shape="outline"
+                    onClick={() => createModal(<AdvancedFilterModal />)}
+                    className="on-mobile-mb0-5"
+                >
+                    {c('Action').t`Add sieve filter`}
+                </Button>
+            </div>
+            <FilterModal {...filterModalProps} isOpen={isFilterModalOpen} />
+        </>
     );
 }
 
