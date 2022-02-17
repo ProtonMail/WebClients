e424ed77b5da9e896e27e6fae285a224013ac094
MatthiasMargot <matthias.margot@proton.ch>
2022-02-17 16:28:34 +0100

    Add @proton/colors package
    CP-2430

---
+++ packages/colors/tint.ts
@@ -0,0 +1,15 @@
+import tiny, { Instance as Color } from 'tinycolor2';
+
+import percentOf from '@proton/shared/lib/helpers/percentOf';
+
+function tint(color: Color, percent: number) {
+    const rgb = color.toRgb();
+
+    rgb.r = rgb.r + percentOf(percent, 255 - rgb.r);
+    rgb.g = rgb.g + percentOf(percent, 255 - rgb.g);
+    rgb.b = rgb.b + percentOf(percent, 255 - rgb.b);
+
+    return tiny(rgb);
+}
+
+export default tint;
