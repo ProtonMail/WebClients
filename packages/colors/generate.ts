b75d555623dbaf9e514cac551b4d835b9082ed7b
MatthiasMargot <matthias.margot@proton.ch>
2022-02-18 17:01:50 +0100

    Clean up the output of generate
    CP-2430

--- packages/colors/generate.ts
+++ packages/colors/generate.ts
@@ -1,45 +1,78 @@
 import fs from 'fs';
 import tiny from 'tinycolor2';
 import prettier from 'prettier';
-import { parse, walk, generate } from 'css-tree';
+import cssTree from 'css-tree';
 
 import genButtonShades from './gen-button-shades';
 
+const buttonBases = [
+    'primary',
+    'signal-danger',
+    'signal-warning',
+    'signal-info',
+    'signal-success'
+];
+
+const buttonShadeNames = [
+    '-l2',
+    '-l1',
+    '',
+    '-d1',
+    '-d2',
+    '-d3',
+]
+
 const baseThemeCssFile = fs.readFileSync('./themes/default.theme.base.css', { encoding: 'utf-8' });
 
-const ast = parse(baseThemeCssFile);
+const ast = cssTree.parse(baseThemeCssFile);
 
-const bases = ['--primary', '--signal-danger', '--signal-warning', '--signal-info', '--signal-success'];
+cssTree.walk(ast, (node, item, list) => {
+    if (node.type !== 'Declaration') {
+        return
+    }
 
-walk(ast, (node, item, list) => {
-    if (node.type === 'Declaration' && bases.includes(node.property)) {
-        if (node.value.type !== 'Raw') {
-            return;
-        }
+    if (node.value.type !== 'Raw') {
+        return;
+    }
 
-        const declarations = genButtonShades(tiny(node.value.value)).map((color, i) =>
+    const baseName = node.property.substring(2)
+
+    if (!buttonBases.includes(baseName)) {
+        return
+    }
+
+    /*
+     * make sure we don't visit the same base name again
+     * by removing it fro the array of button base names
+     */
+    buttonBases.splice(buttonBases.indexOf(baseName), 1)
+
+    const declarations = genButtonShades(tiny(node.value.value)).map(
+        (color, i) =>
             list.createItem({
                 type: 'Declaration',
                 important: false,
-                property: '--primary-' + i,
-                value: { type: 'Raw', value: color.toHexString() },
-            })
-        );
-
-        declarations.reverse()
-
-        if (!item.next) {
-            for (const declaration of declarations) {
-                list.append(declaration);
+                property: '--' + baseName + buttonShadeNames[i],
+                value: { type: 'Raw', value: color.toHexString() }
             }
-        } else {
-            for (const declaration of declarations) {
-                list.insert(declaration, item.next);
-            }
+        )
+    );
+
+    if (!item.next) {
+        for (const declaration of declarations) {
+            list.append(declaration);
+        }
+    } else {
+        /* list.insert() inserts after the next element so we reverse insertion order */
+        for (let i = declarations.length - 1; i >= 0; i--) {
+            list.insert(declarations[i], item.next);
         }
     }
+
+    /* base is consumed, we don't need it any more and we don't want to re-visit */
+    list.remove(item)
 });
 
-const css = prettier.format(generate(ast), { parser: 'css' });
+const css = prettier.format(cssTree.generate(ast), { parser: 'css' });
 
 fs.writeFileSync('./themes/default.theme.css', css);
