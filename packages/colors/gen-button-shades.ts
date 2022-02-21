ae8e2238f4ba773957ad741745a7c200a0d00459
MatthiasMargot <matthias.margot@proton.ch>
2022-02-21 19:13:14 +0100

    Add support for type 'dark' generated colors
    CP-2430

--- packages/colors/gen-button-shades.ts
+++ packages/colors/gen-button-shades.ts
@@ -1,4 +1,4 @@
-import tinycolor, { Instance as Color } from 'tinycolor2';
+import { Instance as Color } from 'tinycolor2';
 
 import isBetween from '@proton/shared/lib/helpers/isBetween';
 
@@ -10,26 +10,42 @@ function genMutation(color: Color) {
     return function (mutation: number) {
         const clone = color.clone();
 
-        return mutation > 0 ? tint(clone, mutation) : shade(clone, Math.abs(mutation));
+        return (mutation > 0 ? tint(clone, mutation) : shade(clone, Math.abs(mutation)));
     };
 }
 
-function genButtonShades(base: Color) {
+function genButtonShades(base: Color, light: boolean) {
     const hsl = base.toHsl();
 
     if (hsl.s <= 0.3) {
-        return [70, 50, 0, -5, -10, -15].map(genMutation(base));
+        if (light) {
+            return [70, 50, 0, -5, -10, -15].map(genMutation(base))
+        } else {
+            return [-70, -50, 0, 10, 20, 30].map(genMutation(base))
+        }
     }
 
     if (isBetween(hsl.h, 30, 60)) {
-        const tinted = [90, 80, 0].map(genMutation(base));
-
-        const shaded = [-5, -10, -15].map(genMutation(base)).map((c) => hue(c, -5));
-
-        return tinted.concat(shaded);
+        if (light) {
+            const tinted = [90, 80, 0].map(genMutation(base));
+    
+            const shaded = [-5, -10, -15].map(genMutation(base)).map(hue(-5));
+    
+            return [ ...tinted, ...shaded ];
+        } else {
+            const shaded = [-80, -70, 0].map(genMutation(base)).map(hue(-5));
+            
+            const tinted = [10, 20, 30].map(genMutation(base));
+    
+            return [ ...shaded, ...tinted ];
+        }
     }
 
-    return [90, 80, 0, -10, -20, -30].map(genMutation(base));
+    if (light) {
+        return [90, 80, 0, -10, -20, -30].map(genMutation(base));
+    } else {
+        return [-80, -70, 0, 10, 20, 30].map(genMutation(base));
+    }
 }
 
 export default genButtonShades;
