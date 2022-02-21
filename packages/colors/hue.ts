f39b4190af4146760ebac1e3a3e628b8f5f39740
MatthiasMargot <matthias.margot@proton.ch>
2022-02-21 19:13:14 +0100

    Add support for type 'dark' generated colors
    CP-2430

--- packages/colors/hue.ts
+++ packages/colors/hue.ts
@@ -2,12 +2,14 @@ import tinycolor, { Instance as Color } from 'tinycolor2';
 
 import percentOf from '@proton/shared/lib/helpers/percentOf';
 
-function hue(color: Color, percent: number) {
-    const hsl = color.toHsl();
-
-    hsl.h = hsl.h + percentOf(percent, hsl.h);
-
-    return tinycolor(hsl);
+function hue(percent: number) {
+    return function (color: Color) {
+        const hsl = color.toHsl();
+    
+        hsl.h = hsl.h + percentOf(percent, hsl.h);
+    
+        return tinycolor(hsl);
+    }
 }
 
 export default hue;
