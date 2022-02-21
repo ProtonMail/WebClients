d59f31b6330ad6b56ffeee044315fd381585deef
MatthiasMargot <matthias.margot@proton.ch>
2022-02-21 15:18:58 +0100

    Create minimal color preview tool
    CP-2430

---
+++ packages/colors/tool/tool.ts
@@ -0,0 +1,38 @@
+import tinycolor from 'tinycolor2'
+
+import genButtonShades from '../gen-button-shades'
+
+const form = document.getElementById('form') as HTMLFormElement
+const input = document.getElementById('input') as HTMLInputElement
+const colors = document.getElementById('colors') as HTMLDivElement
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const { value } = input
+
+  if (!value) { return }
+
+  const shades = genButtonShades(tinycolor(value))
+
+  const children = shades.map(
+    shade => {
+      const hex = shade.toHexString()
+
+      const child = document.createElement('div')
+
+      child.setAttribute(
+        'style',
+        `background: ${hex}; padding: 24px;`
+      )
+
+      child.innerText = hex
+
+      return child
+    }
+  )
+
+  colors.innerHTML = null
+
+  children.forEach(colors.appendChild.bind(colors))
+})
---
+++ packages/colors/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './tool/tool.ts',
+  stats: 'minimal',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'tool'),
+  },
+};
