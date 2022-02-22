7564a93527ea1fd97aae3c0ba04d9c0acac18a24
mmso <mmso@mmso.se>
2022-02-22 08:25:51 +0100

    Add modal tests

--- packages/components/jest.setup.js
+++ packages/components/jest.setup.js
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom';
+import './jest.mock';
 
 // Silence warnings on expect to throw https://github.com/testing-library/react-testing-library/issues/157
 console.error = () => {};
