b58cf10a4c1a7bb921081c56cc1d548a4d117722
Guillaume Gonzalez <guillaume.gonzalez@proton.ch>
2022-02-22 15:20:52 +0100

    Improve invite email design

--- packages/components/containers/referral/invite/InviteSendEmailRecipient.tsx
+++ packages/components/containers/referral/invite/InviteSendEmailRecipient.tsx
@@ -2,6 +2,7 @@ import { c } from 'ttag';
 import { MouseEventHandler } from 'react';
 import { Icon, classnames, Tooltip } from '@proton/components';
 import { Recipient } from '@proton/shared/lib/interfaces';
+import { isProtonAddress } from './helpers';
 
 interface Props {
     recipient: Recipient;
@@ -9,18 +10,32 @@ interface Props {
     isValid: boolean;
 }
 
-const InviteSendEmailRecipient = ({ recipient, onDeleteRecipient, isValid }: Props) => (
-    <Tooltip title={!isValid ? c('Info').t`This address is invalid or this is a protonmail address` : undefined}>
-        <div
-            className={classnames([isValid ? 'bg-weak' : 'bg-danger text-white opacity-65', ' m0-25 p0-25'])}
-            key={recipient.Address}
-        >
-            <span className="text-no-wrap pr0-5">{recipient.Address}</span>
-            <span className="cursor-pointer" onClick={onDeleteRecipient}>
-                <Icon name="xmark" />
-            </span>
-        </div>
-    </Tooltip>
-);
+const getErrorMessage = (emailAddress: string) => {
+    if (isProtonAddress(emailAddress)) {
+        return c('Info').t`${emailAddress} is a protonmail address`;
+    }
+    return c('Info').t`${emailAddress} is invalid`;
+};
+
+const InviteSendEmailRecipient = ({ recipient, onDeleteRecipient, isValid }: Props) => {
+    const emailAddress = recipient.Address;
+
+    return (
+        <Tooltip title={!isValid ? getErrorMessage(emailAddress) : undefined}>
+            <div
+                className={classnames([
+                    'm0-25 p0-25 flex flex-nowrap',
+                    isValid ? 'bg-weak' : 'bg-danger text-white opacity-65',
+                ])}
+                key={recipient.Address}
+            >
+                <span className="text-no-wrap pr0-5 text-ellipsis">{recipient.Address}</span>
+                <span className="cursor-pointer" onClick={onDeleteRecipient}>
+                    <Icon name="xmark" />
+                </span>
+            </div>
+        </Tooltip>
+    );
+};
 
 export default InviteSendEmailRecipient;
