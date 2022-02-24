97ff941cdddd7073c5b09b2b9fa278a96e79b55a
MatthiasMargot <matthias.margot@proton.ch>
2022-02-24 15:49:57 +0100

    Set up appropriate validation
    CP-2430

--- packages/colors/gen-themes.ts
+++ packages/colors/gen-themes.ts
@@ -4,66 +4,56 @@ import prettier from 'prettier';
 import cssTree from 'css-tree';
 
 import genButtonShades from './gen-button-shades';
-import config, { ThemeConfig, ThemeFileType } from './themes.config'
+import config, { ThemeConfig, ThemeFileType } from './themes.config';
 
-function generateTheme ({ source, type }: { source: string, type: ThemeFileType }) {
+function generateTheme({ source, type }: { source: string; type: ThemeFileType }) {
     const buttonBases = [
         'signal-danger',
         'signal-warning',
         'signal-info',
         'signal-success',
         'interaction-norm',
-        'interaction-weak'
+        'interaction-weak',
     ];
 
-    const lightButtonShadeNames = [
-        '-tint-90',
-        '-tint-80',
-        '',
-        '-shade-10',
-        '-shade-20',
-        '-shade-30',
-    ]
+    const lightButtonShadeNames = ['-tint-90', '-tint-80', '', '-shade-10', '-shade-20', '-shade-30'];
 
-    const darkButtonShadeNames = [
-        '-shade-80',
-        '-shade-70',
-        '',
-        '-tint-10',
-        '-tint-20',
-        '-tint-30',
-    ]
+    const darkButtonShadeNames = ['-shade-80', '-shade-70', '', '-tint-10', '-tint-20', '-tint-30'];
 
     const ast = cssTree.parse(source);
 
     cssTree.walk(ast, (node, item, list) => {
-        if (node.type !== 'Declaration') { return; }
+        if (node.type !== 'Declaration') {
+            return;
+        }
 
-        if (node.value.type !== 'Raw') { return; }
+        if (node.value.type !== 'Raw') {
+            return;
+        }
 
-        const baseName = node.property.substring(2)
+        const baseName = node.property.substring(2);
 
-        if (!buttonBases.includes(baseName)) { return }
+        if (!buttonBases.includes(baseName)) {
+            return;
+        }
 
         /*
-        * make sure we don't visit the same base name again
-        * by removing it from the array of button base names
-        */
-        buttonBases.splice(buttonBases.indexOf(baseName), 1)
+         * make sure we don't visit the same base name again
+         * by removing it from the array of button base names
+         */
+        buttonBases.splice(buttonBases.indexOf(baseName), 1);
 
-        const isLight = type === 'light'
+        const isLight = type === 'light';
 
-        const buttonShadeNames = type === 'light' ? lightButtonShadeNames : darkButtonShadeNames
+        const buttonShadeNames = type === 'light' ? lightButtonShadeNames : darkButtonShadeNames;
 
-        const declarations = genButtonShades(tiny(node.value.value), isLight).map(
-            (color, i) =>
-                list.createItem({
-                    type: 'Declaration',
-                    important: false,
-                    property: '--' + baseName + buttonShadeNames[i],
-                    value: { type: 'Raw', value: color.toHexString() }
-                }
-            )
+        const declarations = genButtonShades(tiny(node.value.value), isLight).map((color, i) =>
+            list.createItem({
+                type: 'Declaration',
+                important: false,
+                property: '--' + baseName + buttonShadeNames[i],
+                value: { type: 'Raw', value: color.toHexString() },
+            })
         );
 
         if (!item.next) {
@@ -78,27 +68,25 @@ function generateTheme ({ source, type }: { source: string, type: ThemeFileType 
         }
 
         /* base is consumed, we don't need it any more and we don't want to re-visit */
-        list.remove(item)
+        list.remove(item);
     });
 
     return cssTree.generate(ast);
 }
 
-function main ({ output, files }: ThemeConfig) {
-    const sources = files.map(
-        ({ path, type }) => ({
-            source: fs.readFileSync(path, { encoding: 'utf-8' }),
-            type
-        })
-    )
+function main({ output, files }: ThemeConfig) {
+    const sources = files.map(({ path, type }) => ({
+        source: fs.readFileSync(path, { encoding: 'utf-8' }),
+        type,
+    }));
 
-    const generatedCssFiles = sources.map(generateTheme)
+    const generatedCssFiles = sources.map(generateTheme);
 
-    const cssFile = generatedCssFiles.join('\n\n')
+    const cssFile = generatedCssFiles.join('\n\n');
 
-    const prettierCssFile = prettier.format(cssFile, { parser: 'css' })
+    const prettierCssFile = prettier.format(cssFile, { parser: 'css' });
 
-    fs.writeFileSync(output, prettierCssFile)
+    fs.writeFileSync(output, prettierCssFile);
 }
 
-config.forEach(main)
+config.forEach(main);
--- packages/colors/hue.ts
+++ packages/colors/hue.ts
@@ -5,11 +5,11 @@ import percentOf from '@proton/shared/lib/helpers/percentOf';
 function hue(percent: number) {
     return function (color: Color) {
         const hsl = color.toHsl();
-    
+
         hsl.h = hsl.h + percentOf(percent, hsl.h);
-    
+
         return tinycolor(hsl);
-    }
+    };
 }
 
 export default hue;
