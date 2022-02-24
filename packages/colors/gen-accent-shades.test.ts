d3aa67465e78ffe7b60df578c4905343a9178e8a
MatthiasMargot <matthias.margot@proton.ch>
2022-02-24 15:49:57 +0100

    Set up appropriate validation
    CP-2430

--- packages/colors/gen-accent-shades.test.ts
+++ packages/colors/gen-accent-shades.test.ts
@@ -2,7 +2,7 @@ import tinycolor, { Instance as Color } from 'tinycolor2';
 
 import genAccentShades from './gen-accent-shades';
 
-const getOriginal = (c: Color) => c.getOriginalInput()
+const getOriginal = (c: Color) => c.getOriginalInput();
 
 describe('genAccentShades', () => {
     it('generates the necessary shades for an accent color', () => {
@@ -11,17 +11,17 @@ describe('genAccentShades', () => {
         expect(output.map(getOriginal)).toEqual([
             { h: 240, s: 1, l: 0.75 },
             { h: 234, s: 0.9, l: 0.65 },
-            { h: 232, s: 0.9, l: 0.58 }
+            { h: 232, s: 0.9, l: 0.58 },
         ]);
     });
 
-    it('doesn\'t allow values to drop below 0', () => {
-      const output = genAccentShades(tinycolor({ h: 5, s: 5, l: 5 }));
+    it("doesn't allow values to drop below 0", () => {
+        const output = genAccentShades(tinycolor({ h: 5, s: 5, l: 5 }));
 
-      expect(output.map(getOriginal)).toEqual([
-          { h: 5, s: 5, l: 5 },
-          { h: 0, s: 0, l: 0 },
-          { h: 0, s: 0, l: 0 }
-      ]);
-  });
+        expect(output.map(getOriginal)).toEqual([
+            { h: 5, s: 5, l: 5 },
+            { h: 0, s: 0, l: 0 },
+            { h: 0, s: 0, l: 0 },
+        ]);
+    });
 });
--- packages/colors/tool/tool.ts
+++ packages/colors/tool/tool.ts
@@ -1,38 +1,35 @@
-import tinycolor from 'tinycolor2'
+import tinycolor from 'tinycolor2';
 
-import genButtonShades from '../gen-button-shades'
+import genButtonShades from '../gen-button-shades';
 
-const form = document.getElementById('form') as HTMLFormElement
-const input = document.getElementById('input') as HTMLInputElement
-const colors = document.getElementById('colors') as HTMLDivElement
+const form = document.getElementById('form') as HTMLFormElement;
+const input = document.getElementById('input') as HTMLInputElement;
+const colors = document.getElementById('colors') as HTMLDivElement;
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  const { value } = input
+    const { value } = input;
 
-  if (!value) { return }
-
-  const shades = genButtonShades(tinycolor(value), true)
-
-  const children = shades.map(
-    shade => {
-      const hex = shade.toHexString()
-
-      const child = document.createElement('div')
-
-      child.setAttribute(
-        'style',
-        `background: ${hex}; padding: 24px;`
-      )
-
-      child.innerText = hex
-
-      return child
+    if (!value) {
+        return;
     }
-  )
 
-  colors.innerHTML = null
+    const shades = genButtonShades(tinycolor(value), true);
 
-  children.forEach(colors.appendChild.bind(colors))
-})
+    const children = shades.map((shade) => {
+        const hex = shade.toHexString();
+
+        const child = document.createElement('div');
+
+        child.setAttribute('style', `background: ${hex}; padding: 24px;`);
+
+        child.innerText = hex;
+
+        return child;
+    });
+
+    colors.innerHTML = '';
+
+    children.forEach(colors.appendChild.bind(colors));
+});
