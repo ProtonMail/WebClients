e619232f1ad4a2ad7551fe874a8f5421b4d48035
mmso <mmso@mmso.se>
2022-02-28 13:04:48 +0100

    Replace PaymentsDisabled Feature Flag with legacy plan

--- packages/components/containers/payments/subscription/SubscriptionModalDisabled.tsx
+++ packages/components/containers/payments/subscription/SubscriptionModalDisabled.tsx
@@ -1,19 +1,20 @@
 import { c } from 'ttag';
-import { BRAND_NAME } from '@proton/shared/lib/constants';
-import { AlertModal, AlertModalProps, Button } from '../../../components';
+import { AlertModal, AlertModalProps, Button, Href } from '../../../components';
 
 const SubscriptionModalDisabled = (props: Omit<AlertModalProps, 'title' | 'buttons' | 'children'>) => {
+    const learnMore = (
+        <Href url="https://proton.me/support/upgrading-to-new-proton-plan">{c('Link').t`Learn more`}</Href>
+    );
     return (
         <AlertModal
             {...props}
-            title={c('Title').t`Refresh your browser`}
-            buttons={
-                <Button color="norm" onClick={() => window.location.reload()}>{c('Action').t`Refresh browser`}</Button>
-            }
+            title={c('new_plans: title').t`Improved plans coming soon!`}
+            buttons={<Button color="norm" onClick={props.onClose}>{c('new_plans: action').t`Got it`}</Button>}
         >
             <div className="text-pre-wrap">
                 {c('Info')
-                    .t`Get the latest version of ${BRAND_NAME} and all relevant updates by refreshing your browser.`}
+                    .t`We’re upgrading your current plan to an improved plan that offers more for the same price. While our systems are updating, you won’t be able to change it. We’ll email you as soon as it’s done.`}{' '}
+                {learnMore}
             </div>
         </AlertModal>
     );
