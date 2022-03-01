d8d92d31955061bde0876feaaa6e8112dd640011
Michal Horejsek <michal.horejsek@proton.ch>
2022-03-01 08:21:33 +0100

    Fix creating default share

--- applications/drive/src/app/store/shares/useDefaultShare.ts
+++ applications/drive/src/app/store/shares/useDefaultShare.ts
@@ -3,6 +3,7 @@ import { useCallback } from 'react';
 import { UserShareResult } from '@proton/shared/lib/interfaces/drive/share';
 import { queryUserShares } from '@proton/shared/lib/api/drive/share';
 
+import { useDebouncedFunction } from '../utils';
 import { useDebouncedRequest, shareMetaShortToShare } from '../api';
 import { ShareWithKey } from './interface';
 import useSharesState from './useSharesState';
@@ -13,6 +14,7 @@ import useVolume from './useVolume';
  * useDefaultShare provides access to main default user's share.
  */
 export default function useDefaultShare() {
+    const debouncedFunction = useDebouncedFunction();
     const debouncedRequest = useDebouncedRequest();
     const sharesState = useSharesState();
     const { getShareWithKey } = useShare();
@@ -26,21 +28,29 @@ export default function useDefaultShare() {
 
     const getDefaultShare = useCallback(
         async (abortSignal?: AbortSignal): Promise<ShareWithKey> => {
-            let defaultShareId = sharesState.getDefaultShareId();
+            return debouncedFunction(
+                async (abortSignal: AbortSignal) => {
+                    let defaultShareId = sharesState.getDefaultShareId();
 
-            // First try to load fresh list of shares from API to make sure
-            // we don't create second default share.
-            if (!defaultShareId) {
-                await loadUserShares();
-                defaultShareId = sharesState.getDefaultShareId();
-            }
+                    // First try to load fresh list of shares from API to make sure
+                    // we don't create second default share.
+                    if (!defaultShareId) {
+                        await loadUserShares();
+                        defaultShareId = sharesState.getDefaultShareId();
+                    }
 
-            if (!defaultShareId) {
-                const { shareId } = await createVolume();
-                defaultShareId = shareId;
-            }
+                    if (!defaultShareId) {
+                        const { shareId } = await createVolume();
+                        defaultShareId = shareId;
+                        // Load shares to the cache.
+                        await loadUserShares();
+                    }
 
-            return getShareWithKey(abortSignal || new AbortController().signal, defaultShareId);
+                    return getShareWithKey(abortSignal || new AbortController().signal, defaultShareId);
+                },
+                ['getDefaultShare'],
+                abortSignal
+            );
         },
         [sharesState.getDefaultShareId, getShareWithKey]
     );
