078f54f39b806e56a767b6d0b8db92702935241c
Matthieu Bué <matthieu.bue@gmail.com>
2022-03-03 12:43:47 +0100

    Update theme switcher

--- packages/shared/lib/themes/themes.ts
+++ packages/shared/lib/themes/themes.ts
@@ -1,20 +1,26 @@
 // @ts-nocheck Disable import errors from ds
 import themeDefaultSvg from '@proton/styles/assets/img/themes/theme-thumb-proton.svg';
+import themeDefaultSvgSmall from '@proton/styles/assets/img/themes/theme-thumb-proton-small.svg';
 import defaultTheme from '@proton/colors/themes/proton.theme.css';
 
 import themeCarbonSvg from '@proton/styles/assets/img/themes/theme-thumb-carbon.svg';
+import themeCarbonSvgSmall from '@proton/styles/assets/img/themes/theme-thumb-carbon-small.svg';
 import carbonTheme from '@proton/colors/themes/carbon.theme.css';
 
 import themeMonokaiSvg from '@proton/styles/assets/img/themes/theme-thumb-monokai.svg';
+import themeMonokaiSvgSmall from '@proton/styles/assets/img/themes/theme-thumb-monokai-small.svg';
 import monokaiTheme from '@proton/colors/themes/monokai.theme.css';
 
 import themeDuotoneSvg from '@proton/styles/assets/img/themes/theme-thumb-duotone.svg';
+import themeDuotoneSvgSmall from '@proton/styles/assets/img/themes/theme-thumb-duotone-small.svg';
 import duotoneTheme from '@proton/colors/themes/duotone.theme.css';
 
 import themeContrastSvg from '@proton/styles/assets/img/themes/theme-thumb-contrast.svg';
+import themeContrastSvgSmall from '@proton/styles/assets/img/themes/theme-thumb-contrast-small.svg';
 import contrastTheme from '@proton/colors/themes/contrast.theme.css';
 
 import themeLegacySvg from '@proton/styles/assets/img/themes/theme-thumb-legacy.svg';
+import themeLegacySvgSmall from '@proton/styles/assets/img/themes/theme-thumb-legacy-small.svg';
 import legacyTheme from '@proton/colors/themes/legacy.theme.css';
 
 export enum HistoricThemeTypes {
@@ -42,36 +48,42 @@ export const PROTON_THEMES_MAP = {
         label: 'Proton',
         identifier: ThemeTypes.Default,
         src: themeDefaultSvg,
+        thumb: themeDefaultSvgSmall,
         theme: defaultTheme.toString(),
     },
     [ThemeTypes.Carbon]: {
         label: 'Carbon',
         identifier: ThemeTypes.Carbon,
         src: themeCarbonSvg,
+        thumb: themeCarbonSvgSmall,
         theme: carbonTheme.toString(),
     },
     [ThemeTypes.Monokai]: {
         label: 'Monokai',
         identifier: ThemeTypes.Monokai,
         src: themeMonokaiSvg,
+        thumb: themeMonokaiSvgSmall,
         theme: monokaiTheme.toString(),
     },
     [ThemeTypes.Duotone]: {
         label: 'Duotone',
         identifier: ThemeTypes.Duotone,
         src: themeDuotoneSvg,
+        thumb: themeDuotoneSvgSmall,
         theme: duotoneTheme.toString(),
     },
     [ThemeTypes.Contrast]: {
         label: 'Contrast',
         identifier: ThemeTypes.Contrast,
         src: themeContrastSvg,
+        thumb: themeContrastSvgSmall,
         theme: contrastTheme.toString(),
     },
     [ThemeTypes.Legacy]: {
         label: 'Legacy',
         identifier: ThemeTypes.Legacy,
         src: themeLegacySvg,
+        thumb: themeLegacySvgSmall,
         theme: legacyTheme.toString(),
     },
 } as const;
