59e97dc9517126dfe4d1a16ca38c40057291ee22
Matthieu Lux <matthieu.lux@protonmail.com>
2022-03-07 16:55:34 +0100

    Show blockquote during dark style computation, force recalculate between messages
    MAILWEB-3116

--- applications/mail/src/app/components/message/MessageView.tsx
+++ applications/mail/src/app/components/message/MessageView.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useMemo, useRef, useState, memo, forwardRef, Ref, useImperativeHandle } from 'react';
-
-import * as React from 'react';
 import { hasAttachments, isDraft, isSent, isOutbox } from '@proton/shared/lib/mail/messages';
 import { Message } from '@proton/shared/lib/interfaces/mail/Message';
 import { classnames } from '@proton/components';
