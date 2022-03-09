a325e42595a82155f8f1969f96a6fdd1d33b95b4
Jack Crompton <jack.crompton@proton.ch>
2022-03-09 15:28:15 +0000

    Migrate VoidRecoveryFilesModal
    CP-3536

--- packages/components/containers/recovery/VoidRecoveryFilesButton.tsx
+++ packages/components/containers/recovery/VoidRecoveryFilesButton.tsx
@@ -1,25 +1,29 @@
 import { c } from 'ttag';
-import { useEventManager, useModals } from '../../hooks';
+import { useEventManager } from '../../hooks';
 import Button, { ButtonProps } from '../../components/button/Button';
 import VoidRecoveryFilesModal from './VoidRecoveryFilesModal';
+import { useModalState } from '../../components';
 
 interface Props extends Omit<ButtonProps, 'onClick' | 'children'> {}
 
 const VoidRecoveryFilesButton = ({ loading, ...rest }: Props) => {
-    const { createModal } = useModals();
     const { call } = useEventManager();
-
-    const handleClick = async () => {
-        await new Promise<void>((resolve, reject) => {
-            createModal(<VoidRecoveryFilesModal onClose={reject} onSuccess={resolve} />);
-        });
-        await call();
-    };
+    const [voidRecoveryFilesModal, setVoidRecoveryFilesModalOpen, renderVoidRecoveryFilesModal] = useModalState();
 
     return (
-        <Button onClick={handleClick} loading={loading} {...rest}>
-            {c('Action').t`Void all recovery files`}
-        </Button>
+        <>
+            {renderVoidRecoveryFilesModal && (
+                <VoidRecoveryFilesModal
+                    {...voidRecoveryFilesModal}
+                    onSuccess={async () => {
+                        await call();
+                    }}
+                />
+            )}
+            <Button onClick={() => setVoidRecoveryFilesModalOpen(true)} loading={loading} {...rest}>
+                {c('Action').t`Void all recovery files`}
+            </Button>
+        </>
     );
 };
 
