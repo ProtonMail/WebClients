2e2b7853c228f0eb6762c16d5b3620ac7a450f56
Jack Crompton <jack.crompton@proton.ch>
2022-03-09 17:43:05 +0000

    Migrate TwoFactorSection to use useModalState

--- packages/components/containers/account/TwoFactorSection.tsx
+++ packages/components/containers/account/TwoFactorSection.tsx
@@ -2,8 +2,8 @@ import { c } from 'ttag';
 import { APPS } from '@proton/shared/lib/constants';
 import { getHasTOTPSettingEnabled } from '@proton/shared/lib/settings/twoFactor';
 
-import { Info, Toggle } from '../../components';
-import { useConfig, useModals, useUserSettings } from '../../hooks';
+import { Info, Toggle, useModalState } from '../../components';
+import { useConfig, useUserSettings } from '../../hooks';
 
 import EnableTOTPModal from './EnableTOTPModal';
 import DisableTOTPModal from './DisableTOTPModal';
@@ -14,15 +14,16 @@ import SettingsLayoutRight from './SettingsLayoutRight';
 const TwoFactorSection = () => {
     const { APP_NAME } = useConfig();
     const [userSettings] = useUserSettings();
-    const { createModal } = useModals();
+    const [enableTOTPModal, setEnableTOTPModalopen, renderEnableTOTPModal] = useModalState();
+    const [disableTOTPModal, setDisableTOTPModalopen, renderDisableTOTPModal] = useModalState();
 
     const hasTOTPEnabled = getHasTOTPSettingEnabled(userSettings);
 
     const handleChange = () => {
         if (hasTOTPEnabled) {
-            createModal(<DisableTOTPModal />);
+            setDisableTOTPModalopen(true);
         } else {
-            createModal(<EnableTOTPModal />);
+            setEnableTOTPModalopen(true);
         }
     };
 
@@ -32,21 +33,25 @@ const TwoFactorSection = () => {
             : 'https://protonmail.com/support/knowledge-base/two-factor-authentication';
 
     return (
-        <SettingsLayout>
-            <SettingsLayoutLeft>
-                <label htmlFor="twoFactorToggle" className="text-semibold">
-                    <span className="mr0-5">{c('Label').t`Two-factor authentication`}</span>
-                    <Info
-                        url={twoFactorAuthLink}
-                        title={c('Info')
-                            .t`Two-factor authentication adds an extra layer of security to your account in case your password is compromised.`}
-                    />
-                </label>
-            </SettingsLayoutLeft>
-            <SettingsLayoutRight className="pt0-5">
-                <Toggle checked={hasTOTPEnabled} id="twoFactorToggle" onChange={handleChange} />
-            </SettingsLayoutRight>
-        </SettingsLayout>
+        <>
+            {renderEnableTOTPModal && <EnableTOTPModal {...enableTOTPModal} />}
+            {renderDisableTOTPModal && <DisableTOTPModal {...disableTOTPModal} />}
+            <SettingsLayout>
+                <SettingsLayoutLeft>
+                    <label htmlFor="twoFactorToggle" className="text-semibold">
+                        <span className="mr0-5">{c('Label').t`Two-factor authentication`}</span>
+                        <Info
+                            url={twoFactorAuthLink}
+                            title={c('Info')
+                                .t`Two-factor authentication adds an extra layer of security to your account in case your password is compromised.`}
+                        />
+                    </label>
+                </SettingsLayoutLeft>
+                <SettingsLayoutRight className="pt0-5">
+                    <Toggle checked={hasTOTPEnabled} id="twoFactorToggle" onChange={handleChange} />
+                </SettingsLayoutRight>
+            </SettingsLayout>
+        </>
     );
 };
 
