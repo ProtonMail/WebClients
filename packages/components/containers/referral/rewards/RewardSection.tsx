3170c94c2cca261d8d213668d8b22c9ae3c698a4
Guillaume Gonzalez <guillaume.gonzalez@proton.ch>
2022-03-10 11:08:47 +0100

    Remove duplicate string in reward section

--- packages/components/containers/referral/rewards/RewardSection.tsx
+++ packages/components/containers/referral/rewards/RewardSection.tsx
@@ -1,6 +1,6 @@
 import { c } from 'ttag';
 
-import { classnames, Loader, SettingsSectionWide } from '@proton/components';
+import { Loader, SettingsSectionWide } from '@proton/components';
 
 import { useReferralInvitesContext } from '../ReferralInvitesContext';
 import RewardsProgress from './RewardsProgress';
@@ -22,16 +22,10 @@ const RewardSection = () => {
         <SettingsSectionWide>
             <p className="color-weak">{c('Description')
                 .t`Track how many people click on your link, sign up, and become paid subscribers. Watch your free months add up.`}</p>
-
-            {loadingRewards ? (
-                <Loader />
-            ) : (
-                <div className={classnames([showRewardSection && 'border-bottom pb1', 'mb4'])}>
-                    {showRewardSection ? (
-                        <RewardsProgress rewardsLimit={rewardsLimit} rewards={rewards} />
-                    ) : (
-                        <p className="color-weak">{c('Description').t`Track your referral link activities here.`}</p>
-                    )}
+            {loadingRewards && <Loader />}
+            {showRewardSection && (
+                <div className="border-bottom pb1 mb4">
+                    <RewardsProgress rewardsLimit={rewardsLimit} rewards={rewards} />
                 </div>
             )}
             <RewardsTable referrals={dedupReferrals} loading={loadingReferrals} />
