960c7368c180f566ab2bf4e547f6092124ea259b
Guillaume Gonzalez <guillaume.gonzalez@proton.ch>
2022-03-10 12:12:51 +0100

    Change reward cell wording

--- packages/components/containers/referral/rewards/table/RewardCell.tsx
+++ packages/components/containers/referral/rewards/table/RewardCell.tsx
@@ -3,21 +3,30 @@ import { Referral, ReferralState } from '@proton/shared/lib/interfaces';
 
 interface Props {
     referral: Referral;
+    hasReachedRewardLimit: boolean;
 }
 
-const RewardCell = ({ referral }: Props) => {
+const RewardCell = ({ referral, hasReachedRewardLimit }: Props) => {
     let reward: string | React.ReactNode = '-';
 
     const monthsRewarded = referral.RewardMonths || 0;
 
     switch (referral.State) {
+        case ReferralState.SIGNED_UP:
+        case ReferralState.TRIAL:
+            if (!hasReachedRewardLimit) {
+                reward = c('Label').t`Waiting for subscription`;
+            }
+            break;
         case ReferralState.COMPLETED:
-            /*
-             * translator : We are in a table cell.
-             * A user referee have signed up or completed a subscription
-             * We show the reward user would be allowed to get.
-             */
-            reward = c('Label').t`Credits pending`;
+            if (!hasReachedRewardLimit) {
+                /*
+                 * translator : We are in a table cell.
+                 * A user referee have signed up or completed a subscription
+                 * We show the reward user would be allowed to get.
+                 */
+                reward = c('Label').t`Credits pending`;
+            }
             break;
         case ReferralState.REWARDED:
             /*
