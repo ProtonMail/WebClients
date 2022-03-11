f7174ad6650088fcb876b0d4a577d936f7e155e0
romain sanchez <romain.sanchez@proton.ch>
2022-03-11 14:43:22 +0100

    Update Sending original message modal to new modal structure MAILWEB-3097

---
+++ applications/mail/src/app/components/composer/modals/SendingOriginalMessageModal.tsx
@@ -0,0 +1,25 @@
+import { AlertModal, Button, ErrorButton } from '@proton/components';
+import { c } from 'ttag';
+
+interface Props {
+    onResolve: () => void;
+    onReject: () => void;
+}
+
+const SendingOriginalMessageModal = ({ onResolve, onReject, ...rest }: Props) => {
+    return (
+        <AlertModal
+            title={c('Title').t`Sending original message`}
+            buttons={[
+                <ErrorButton onClick={onResolve}>{c('Action').t`OK`}</ErrorButton>,
+                <Button onClick={onReject}>{c('Action').t`Close`}</Button>,
+            ]}
+            {...rest}
+        >
+            {c('Info')
+                .t`The original message you are trying to forward / reply to is in the process of being sent. If you continue, you will not be able to undo sending of the original message any longer.`}
+        </AlertModal>
+    );
+};
+
+export default SendingOriginalMessageModal;
