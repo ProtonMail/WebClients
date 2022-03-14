1ceb4c96ff95711f44177d18857645ce33529d28
econdepe <econdepe@protonmail.com>
2022-03-14 23:14:55 +0100

    Fix wrong Booleans

--- packages/shared/lib/keys/keyFlags.ts
+++ packages/shared/lib/keys/keyFlags.ts
@@ -4,9 +4,9 @@ import { hasBit } from '../helpers/bitset';
 export const getDefaultKeyFlags = () => KEY_FLAG.FLAG_NOT_OBSOLETE + KEY_FLAG.FLAG_NOT_COMPROMISED;
 
 export const getKeyHasFlagsToVerify = (flags: KEY_FLAG) => {
-    return !hasBit(flags, KEY_FLAG.FLAG_NOT_COMPROMISED);
+    return hasBit(flags, KEY_FLAG.FLAG_NOT_COMPROMISED);
 };
 
 export const getKeyHasFlagsToEncrypt = (flags: KEY_FLAG) => {
-    return !hasBit(flags, KEY_FLAG.FLAG_NOT_OBSOLETE);
+    return hasBit(flags, KEY_FLAG.FLAG_NOT_OBSOLETE);
 };
