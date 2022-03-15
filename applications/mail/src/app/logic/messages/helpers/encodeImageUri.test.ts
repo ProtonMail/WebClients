4e2e1b491c794988ce670f16e49092e23f911e55
Guillaume Gonzalez <guillaume.gonzalez@proton.ch>
2022-03-15 16:24:02 +0100

    Update encode image uri function

---
+++ applications/mail/src/app/logic/messages/helpers/encodeImageUri.test.ts
@@ -0,0 +1,15 @@
+import encodeImageUri from './encodeImageUri';
+
+const domain = 'https://test.com';
+
+describe('encode image url', () => {
+    it.each`
+        url                              | expectedUrl
+        ${domain + '/a space.png'}       | ${domain + '/a%20space.png'}
+        ${domain + '/a%20space.png'}     | ${domain + '/a%20space.png'}
+        ${' ' + domain + '/logo.png%22'} | ${domain + '/logo.png%22'}
+        ${domain + '/logo.png&quot'}     | ${domain + '/logo.png&quot'}
+    `('Encoding $url should return $expectedUrl', ({ url, expectedUrl }) => {
+        expect(encodeImageUri(url)).toBe(expectedUrl);
+    });
+});
