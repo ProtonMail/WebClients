ProtonMail/WebClients

commit 31e68f73eb8186adce7b5baf2e5f4c041840318c

Reactivate calendar keys from ICS widget
CALWEB-3030

applications/mail/src/app/helpers/calendar/inviteApi.ts
@@ -1,5 +1,7 @@
 import { getLinkedDateTimeProperty } from '@proton/shared/lib/calendar/icsSurgery/vevent';
+import { reactivateCalendarsKeys } from '@proton/shared/lib/calendar/keys/reactivateCalendarKeys';
 import { getIsPersonalCalendar } from '@proton/shared/lib/calendar/subscribe/helpers';
+import { toggleBit } from '@proton/shared/lib/helpers/bitset';
 import { getUnixTime } from 'date-fns';
 import { syncMultipleEvents, updateAttendeePartstat, updatePersonalEventPart } from '@proton/shared/lib/api/calendars';
 import { processApiRequestsSafe } from '@proton/shared/lib/api/helpers/safeApiRequests';
@@ -9,8 +11,17 @@ import {
     toApiPartstat,
     withPmAttendees,
 } from '@proton/shared/lib/calendar/attendees';
-import { getDoesCalendarNeedUserAction, getIsCalendarDisabled } from '@proton/shared/lib/calendar/calendar';
-import { ICAL_ATTENDEE_STATUS, ICAL_EVENT_STATUS, ICAL_METHOD } from '@proton/shared/lib/calendar/constants';
+import {
+    getDoesCalendarHaveInactiveKeys,
+    getDoesCalendarNeedUserAction,
+    getIsCalendarDisabled,
+} from '@proton/shared/lib/calendar/calendar';
+import {
+    CALENDAR_FLAGS,
+    ICAL_ATTENDEE_STATUS,
+    ICAL_EVENT_STATUS,
+    ICAL_METHOD,
+} from '@proton/shared/lib/calendar/constants';
 import {
     CreateCalendarEventSyncData,
     CreateLinkedCalendarEventsSyncData,
@@ -218,6 +229,7 @@ export const fetchAllEventsByUID: FetchAllEventsByUID = async ({ uid, api, recur
 type FetchEventInvitation = (args: {
     veventComponent: VcalVeventComponent;
     api: Api;
+    getAddressKeys: GetAddressKeys;
     getCalendarInfo: GetCalendarInfo;
     getCalendarEventRaw: (event: CalendarEvent) => Promise<DecryptedVeventResult>;
     getCalendarEventPersonal: (event: CalendarEvent) => Promise<DecryptedPersonalVeventMapResult>;
@@ -238,6 +250,7 @@ type FetchEventInvitation = (args: {
 export const fetchEventInvitation: FetchEventInvitation = async ({
     veventComponent,
     api,
+    getAddressKeys,
     getCalendarInfo,
     getCalendarEventRaw,
     getCalendarEventPersonal,
@@ -258,8 +271,28 @@ export const fetchEventInvitation: FetchEventInvitation = async ({
     if (!calendar) {
         return {};
     }
+    let hasReactivatedCalendar = false;
+    if (getDoesCalendarHaveInactiveKeys(calendar)) {
+        try {
+            const silentApi = <T>(config: any) => api<T>({ ...config, silence: true });
+            await reactivateCalendarsKeys({
+                calendars: [calendar],
+                api: silentApi,
+                addresses: ownAddresses,
+                getAddressKeys,
+            });
+            // we need to update the calendar data manually. We update the flags here,
+            // while the calendar keys will be updated in the step after the catch
+            calendar.Flags = toggleBit(calendar.Flags, CALENDAR_FLAGS.UPDATE_PASSPHRASE);
+            hasReactivatedCalendar = true;
+        } catch (e) {
+            // fail silently, proceed with the calendar in a passphrase-needs-update state
+            noop();
+        }
+    }
     const { memberID, addressID, addressKeys, decryptedCalendarKeys, calendarSettings } = await getCalendarInfo(
-        calendar.ID
+        calendar.ID,
+        hasReactivatedCalendar
     );
     const calendarData = {
         calendar,
@@ -271,6 +304,7 @@ export const fetchEventInvitation: FetchEventInvitation = async ({
         addressKeys,
         calendarKeys: decryptedCalendarKeys,
     };
+
     // if we retrieved a single edit when not looking for one, or looking for another one, do not return it
     if (!calendarEvent || getIsNonSoughtEvent(calendarEvent, veventComponent, supportedRecurrenceId)) {
         return { calendarData };
@@ -470,7 +504,7 @@ export const updateEventInvitation = async ({
         !attendeeApi ||
         getIsProtonInvite({
             invitationIcs,
-            invitationApi,
+            calendarEvent,
             pmData,
         })
     ) {

applications/mail/src/app/components/message/extras/ExtraEvents.tsx
@@ -1,3 +1,5 @@
+import useGetCalendarEventPersonal from '@proton/components/hooks/useGetCalendarEventPersonal';
+import { useGetCanonicalEmailsMap } from '@proton/components/hooks/useGetCanonicalEmailsMap';
 import { arrayToBinaryString, decodeUtf8, DecryptResultPmcrypto } from 'pmcrypto';
 import {
     getCanCreateCalendar,
@@ -28,6 +30,9 @@ import {
     useLoading,
     useUser,
     useUserSettings,
+    useCalendarEmailNotificationsFeature,
+    useGetCalendarInfo,
+    useGetCalendarEventRaw,
 } from '@proton/components';
 import { useDispatch } from 'react-redux';
 import { formatDownload } from '../../../helpers/attachment/attachmentDownloader';
@@ -64,6 +69,11 @@ const ExtraEvents = ({ message }: Props) => {
     const [user, loadingUser] = useUser();
     const [userSettings, loadingUserSettings] = useUserSettings();
     const getCalendarUserSettings = useGetCalendarUserSettings();
+    const enabledEmailNotifications = useCalendarEmailNotificationsFeature();
+    const getCalendarInfo = useGetCalendarInfo();
+    const getCalendarEventRaw = useGetCalendarEventRaw();
+    const getCalendarEventPersonal = useGetCalendarEventPersonal();
+    const getCanonicalEmailsMap = useGetCanonicalEmailsMap();
 
     const [loadingWidget, withLoadingWidget] = useLoading();
     const [loadedWidget, setLoadedWidget] = useState('');
@@ -73,8 +83,7 @@ const ExtraEvents = ({ message }: Props) => {
         defaultCalendar?: Calendar;
         canCreateCalendar: boolean;
         maxUserCalendarsDisabled: boolean;
-        mustReactivateCalendars: boolean;
-    }>({ calendars: [], canCreateCalendar: true, maxUserCalendarsDisabled: false, mustReactivateCalendars: false });
+    }>({ calendars: [], canCreateCalendar: true, maxUserCalendarsDisabled: false });
 
     const loadingConfigs = loadingContactEmails || loadingAddresses || loadingUserSettings || loadingUser;
     const messageHasDecryptionError = !!message.errors?.decryption?.length;
@@ -120,13 +129,12 @@ const ExtraEvents = ({ message }: Props) => {
                         user.isFree
                     );
                     const maxUserCalendarsDisabled = getMaxUserCalendarsDisabled(disabledCalendars, user.isFree);
-                    const mustReactivateCalendars = !defaultCalendar && !canCreateCalendar && !maxUserCalendarsDisabled;
+
                     return {
                         calendars,
                         defaultCalendar,
                         canCreateCalendar,
                         maxUserCalendarsDisabled,
-                        mustReactivateCalendars,
                     };
                 };
                 const getInvitations = async () => {
@@ -245,10 +253,15 @@ const ExtraEvents = ({ message }: Props) => {
                         defaultCalendar={calData.defaultCalendar}
                         canCreateCalendar={calData.canCreateCalendar}
                         maxUserCalendarsDisabled={calData.maxUserCalendarsDisabled}
-                        mustReactivateCalendars={calData.mustReactivateCalendars}
                         contactEmails={contactEmails}
                         ownAddresses={addresses}
                         userSettings={userSettings}
+                        enabledEmailNotifications={enabledEmailNotifications}
+                        getAddressKeys={getAddressKeys}
+                        getCalendarInfo={getCalendarInfo}
+                        getCalendarEventRaw={getCalendarEventRaw}
+                        getCalendarEventPersonal={getCalendarEventPersonal}
+                        getCanonicalEmailsMap={getCanonicalEmailsMap}
                     />
                 );
             })}

applications/mail/src/app/components/message/extras/calendar/EmailReminderWidget.test.tsx
@@ -13,7 +13,6 @@ import { getEvent } from '@proton/shared/lib/api/calendars';
 import { CALENDAR_APP_NAME } from '@proton/shared/lib/calendar/constants';
 import {
     getDefaultCalendar,
-    getCanCreateCalendar,
     getMaxUserCalendarsDisabled,
     getDoesCalendarNeedUserAction,
 } from '@proton/shared/lib/calendar/calendar';
@@ -99,7 +98,6 @@ const mockedGetParsedHeadersFirstValue = getParsedHeadersFirstValue as jest.Mock
     ReturnType<typeof getParsedHeadersFirstValue>
 >;
 const mockedGetDefaultCalendar = getDefaultCalendar as jest.Mock<any>;
-const mockedGetCanCreateCalendar = getCanCreateCalendar as jest.Mock<ReturnType<typeof getCanCreateCalendar>>;
 const mockedGetDoesCalendarNeedUserAction = getDoesCalendarNeedUserAction as jest.Mock<
     ReturnType<typeof getDoesCalendarNeedUserAction>
 >;
@@ -362,7 +360,6 @@ describe('EmailReminderWidget', () => {
 
             it('displays an error instead of the widget when calendars must be reactivated', async () => {
                 mockedGetDefaultCalendar.mockImplementation(() => false);
-                mockedGetCanCreateCalendar.mockImplementation(() => false);
                 mockedGetMaxUserCalendarsDisabled.mockImplementation(() => false);
 
                 render(renderComponent());

applications/mail/src/app/components/message/extras/calendar/ExtraEvent.tsx
@@ -3,24 +3,19 @@ import { canonizeEmailByGuess } from '@proton/shared/lib/helpers/email';
 import { Address, UserSettings } from '@proton/shared/lib/interfaces';
 import { Calendar } from '@proton/shared/lib/interfaces/calendar';
 import { ContactEmail } from '@proton/shared/lib/interfaces/contacts';
+import { GetAddressKeys } from '@proton/shared/lib/interfaces/hooks/GetAddressKeys';
+import { GetCalendarEventPersonal } from '@proton/shared/lib/interfaces/hooks/GetCalendarEventPersonal';
+import { GetCalendarEventRaw } from '@proton/shared/lib/interfaces/hooks/GetCalendarEventRaw';
+import { GetCalendarInfo } from '@proton/shared/lib/interfaces/hooks/GetCalendarInfo';
+import { GetCanonicalEmailsMap } from '@proton/shared/lib/interfaces/hooks/GetCanonicalEmailsMap';
 import { getWeekStartsOn } from '@proton/shared/lib/settings/helper';
 import {
     EVENT_INVITATION_ERROR_TYPE,
     EventInvitationError,
 } from '@proton/shared/lib/calendar/icsSurgery/EventInvitationError';
 import { useEffect, useState } from 'react';
-import {
-    Banner,
-    InlineLinkButton,
-    useApi,
-    useGetCalendarEventRaw,
-    useGetCalendarInfo,
-    useLoading,
-    useCalendarEmailNotificationsFeature,
-} from '@proton/components';
-import { useGetCanonicalEmailsMap } from '@proton/components/hooks/useGetCanonicalEmailsMap';
+import { Banner, InlineLinkButton, useApi, useLoading } from '@proton/components';
 import { c } from 'ttag';
-import useGetCalendarEventPersonal from '@proton/components/hooks/useGetCalendarEventPersonal';
 import { BannerBackgroundColor } from '@proton/components/components/banner/Banner';
 import {
     EventInvitation,
@@ -34,6 +29,7 @@ import {
     getIsInvitationOutdated,
     InvitationModel,
     UPDATE_ACTION,
+    getIsProtonInvite,
 } from '../../../../helpers/calendar/invite';
 import { fetchEventInvitation, updateEventInvitation } from '../../../../helpers/calendar/inviteApi';
 import ExtraEventButtons from './ExtraEventButtons';
@@ -60,12 +56,17 @@ interface Props {
     invitationOrError: EventInvitation | EventInvitationError;
     canCreateCalendar: boolean;
     maxUserCalendarsDisabled: boolean;
-    mustReactivateCalendars: boolean;
     calendars: Calendar[];
     defaultCalendar?: Calendar;
     contactEmails: ContactEmail[];
     ownAddresses: Address[];
     userSettings: UserSettings;
+    enabledEmailNotifications: boolean;
+    getAddressKeys: GetAddressKeys;
+    getCalendarInfo: GetCalendarInfo;
+    getCalendarEventRaw: GetCalendarEventRaw;
+    getCalendarEventPersonal: GetCalendarEventPersonal;
+    getCanonicalEmailsMap: GetCanonicalEmailsMap;
 }
 const ExtraEvent = ({
     invitationOrError,
@@ -74,10 +75,15 @@ const ExtraEvent = ({
     defaultCalendar,
     canCreateCalendar,
     maxUserCalendarsDisabled,
-    mustReactivateCalendars,
     contactEmails,
     ownAddresses,
     userSettings,
+    enabledEmailNotifications,
+    getAddressKeys,
+    getCalendarInfo,
+    getCalendarEventRaw,
+    getCalendarEventPersonal,
+    getCanonicalEmailsMap,
 }: Props) => {
     const [model, setModel] = useState<InvitationModel>(() =>
         getInitialInvitationModel({
@@ -89,17 +95,11 @@ const ExtraEvent = ({
             hasNoCalendars: calendars.length === 0,
             canCreateCalendar,
             maxUserCalendarsDisabled,
-            mustReactivateCalendars,
         })
     );
     const [loading, withLoading] = useLoading(true);
     const [retryCount, setRetryCount] = useState<number>(0);
     const api = useApi();
-    const enabledEmailNotifications = useCalendarEmailNotificationsFeature();
-    const getCalendarInfo = useGetCalendarInfo();
-    const getCalendarEventRaw = useGetCalendarEventRaw();
-    const getCalendarEventPersonal = useGetCalendarEventPersonal();
-    const getCanonicalEmailsMap = useGetCanonicalEmailsMap();
 
     const handleRetry = () => {
         setRetryCount((count) => count + 1);
@@ -113,7 +113,6 @@ const ExtraEvent = ({
                 hasNoCalendars: calendars.length === 0,
                 canCreateCalendar,
                 maxUserCalendarsDisabled,
-                mustReactivateCalendars,
             })
         );
     };
@@ -147,6 +146,7 @@ const ExtraEvent = ({
                 } = await fetchEventInvitation({
                     veventComponent: invitationIcs.vevent,
                     api,
+                    getAddressKeys,
                     getCalendarInfo,
                     getCalendarEventRaw,
                     getCalendarEventPersonal,
@@ -160,13 +160,15 @@ const ExtraEvent = ({
                 calendarData = calData;
                 singleEditData = singleData;
                 hasDecryptionError = hasDecryptError;
-                if (getIsReinvite({ invitationIcs, invitationApi, isOrganizerMode })) {
-                    reinviteEventID = invitationApi?.calendarEvent.ID;
+                const isOutdated = getIsInvitationOutdated({ invitationIcs, invitationApi, isOrganizerMode });
+                const isFromFuture = getIsInvitationFromFuture({ invitationIcs, invitationApi, isOrganizerMode });
+                const isProtonInvite = getIsProtonInvite({ invitationIcs, calendarEvent, pmData });
+                const isReinvite = getIsReinvite({ invitationIcs, calendarEvent, isOrganizerMode, isOutdated });
+                if (isReinvite) {
+                    reinviteEventID = calendarEvent?.ID;
                     // ignore existing partstat
                     delete invitationApi?.attendee?.partstat;
                 }
-                const isOutdated = getIsInvitationOutdated({ invitationIcs, invitationApi, isOrganizerMode });
-                const isFromFuture = getIsInvitationFromFuture({ invitationIcs, invitationApi, isOrganizerMode });
                 if (parentInvitation) {
                     parentInvitationApi = parentInvitation;
                 }
@@ -195,6 +197,8 @@ const ExtraEvent = ({
                         parentInvitationApi,
                         isOutdated,
                         isFromFuture,
+                        isProtonInvite,
+                        isReinvite,
                         reinviteEventID,
                         calendarData,
                         singleEditData,
@@ -210,7 +214,7 @@ const ExtraEvent = ({
                 !invitationApi ||
                 !getInvitationHasEventID(invitationApi) ||
                 !getHasFullCalendarData(calendarData) ||
-                calendarData.calendarNeedsUserAction ||
+                calendarData?.calendarNeedsUserAction ||
                 unmounted
             ) {
                 // treat as a new invitation
@@ -247,6 +251,17 @@ const ExtraEvent = ({
                     invitationApi: newInvitationApi,
                     isOrganizerMode,
                 });
+                const isProtonInvite = getIsProtonInvite({
+                    invitationIcs: supportedInvitationIcs,
+                    calendarEvent: newInvitationApi.calendarEvent,
+                    pmData,
+                });
+                const isReinvite = getIsReinvite({
+                    invitationIcs: supportedInvitationIcs,
+                    calendarEvent: newInvitationApi.calendarEvent,
+                    isOrganizerMode,
+                    isOutdated,
+                });
                 if (!unmounted) {
                     setModel({
                         ...model,
@@ -258,6 +273,8 @@ const ExtraEvent = ({
                         timeStatus: getEventTimeStatus(newInvitationApi.vevent, Date.now()),
                         isOutdated,
                         isFromFuture,
+                        isProtonInvite,
+                        isReinvite,
                         reinviteEventID,
                         updateAction,
                         hasDecryptionError,

applications/mail/src/app/components/message/extras/calendar/ExtraEventAttendeeButtons.tsx
@@ -16,13 +16,7 @@ import { useCallback, Dispatch, SetStateAction } from 'react';
 import { InlineLinkButton, Loader, useLoading, useNotifications, CalendarInviteButtons } from '@proton/components';
 import { c } from 'ttag';
 import Banner, { BannerBackgroundColor } from '@proton/components/components/banner/Banner';
-import {
-    getDisableButtons,
-    getIsProtonInvite,
-    getIsReinvite,
-    InvitationModel,
-    UPDATE_ACTION,
-} from '../../../../helpers/calendar/invite';
+import { getDisableButtons, InvitationModel, UPDATE_ACTION } from '../../../../helpers/calendar/invite';
 import useInviteButtons from '../../../../hooks/useInviteButtons';
 import { MessageState } from '../../../../logic/messages/messagesTypes';
 
@@ -35,13 +29,14 @@ interface Props {
 }
 const ExtraEventAttendeeButtons = ({ model, setModel, message }: Props) => {
     const {
-        isOrganizerMode,
         invitationIcs,
         invitationIcs: { method },
         invitationApi,
         calendarData,
         pmData,
         singleEditData,
+        isProtonInvite,
+        isReinvite,
         error,
         hasDecryptionError,
         reinviteEventID,
@@ -152,9 +147,6 @@ const ExtraEventAttendeeButtons = ({ model, setModel, message }: Props) => {
     };
 
     const buttonsDisabled = getDisableButtons(model);
-    const isProtonInvite =
-        getIsProtonInvite({ invitationIcs, invitationApi, pmData }) ||
-        (pmData && getIsReinvite({ invitationIcs, invitationApi, isOrganizerMode }));
 
     const actions = useInviteButtons({
         veventIcs: invitationIcs.vevent,
@@ -165,7 +157,7 @@ const ExtraEventAttendeeButtons = ({ model, setModel, message }: Props) => {
         subject: formatSubject(message.data?.Subject, RE_PREFIX),
         messageID: message.data?.ID,
         calendarData,
-        pmData: isProtonInvite ? pmData : undefined,
+        pmData: isProtonInvite || (pmData && isReinvite) ? pmData : undefined,
         singleEditData,
         onEmailSuccess: handleEmailSuccess,
         onCreateEventSuccess: handleCreateEventSuccess,

applications/mail/src/app/helpers/calendar/invite.ts
@@ -106,10 +106,11 @@ export interface InvitationModel {
     isAddressDisabled: boolean;
     canCreateCalendar: boolean;
     maxUserCalendarsDisabled: boolean;
-    mustReactivateCalendars: boolean;
     hasNoCalendars: boolean;
     isOutdated?: boolean;
     isFromFuture?: boolean;
+    isProtonInvite?: boolean;
+    isReinvite?: boolean;
     reinviteEventID?: string;
     updateAction?: UPDATE_ACTION;
     hideSummary?: boolean;
@@ -280,21 +281,21 @@ export const getIsInvitationFromFuture = ({
  */
 export const getIsProtonInvite = ({
     invitationIcs,
-    invitationApi,
+    calendarEvent,
     pmData,
 }: {
     invitationIcs: RequireSome<EventInvitation, 'method'>;
-    invitationApi?: RequireSome<EventInvitation, 'calendarEvent'>;
+    calendarEvent?: CalendarEventWithMetadata;
     pmData?: PmInviteData;
 }) => {
     const { method } = invitationIcs;
     const { sharedEventID, isProtonReply } = pmData || {};
 
     if ([ICAL_METHOD.REQUEST, ICAL_METHOD.CANCEL].includes(method)) {
-        if (!invitationApi) {
+        if (!calendarEvent) {
             return !!sharedEventID;
         }
-        return sharedEventID === invitationApi.calendarEvent.SharedEventID;
+        return sharedEventID === calendarEvent.SharedEventID;
     }
     if (method === ICAL_METHOD.REPLY) {
         return !!isProtonReply;
@@ -307,19 +308,19 @@ export const getIsProtonInvite = ({
  */
 export const getIsReinvite = ({
     invitationIcs,
-    invitationApi,
+    calendarEvent,
     isOrganizerMode,
+    isOutdated,
 }: {
     invitationIcs: EventInvitation;
-    invitationApi?: RequireSome<EventInvitation, 'calendarEvent'>;
+    calendarEvent?: CalendarEventWithMetadata;
     isOrganizerMode: boolean;
+    isOutdated: boolean;
 }) => {
     const { method } = invitationIcs;
-    const isOutdated = getIsInvitationOutdated({ invitationIcs, invitationApi, isOrganizerMode });
-    if (isOrganizerMode || method !== ICAL_METHOD.REQUEST || !invitationApi || isOutdated) {
+    if (isOrganizerMode || method !== ICAL_METHOD.REQUEST || !calendarEvent || isOutdated) {
         return false;
     }
-    const { calendarEvent } = invitationApi;
     return getIsEventCancelled(calendarEvent);
 };
 
@@ -480,7 +481,6 @@ interface GetInitialInvitationModelArgs {
     hasNoCalendars: boolean;
     canCreateCalendar: boolean;
     maxUserCalendarsDisabled: boolean;
-    mustReactivateCalendars: boolean;
 }
 export const getInitialInvitationModel = ({
     invitationOrError,
@@ -491,7 +491,6 @@ export const getInitialInvitationModel = ({
     hasNoCalendars,
     canCreateCalendar,
     maxUserCalendarsDisabled,
-    mustReactivateCalendars,
 }: GetInitialInvitationModelArgs) => {
     const errorModel = {
         isImport: false,
@@ -501,7 +500,6 @@ export const getInitialInvitationModel = ({
         isAddressActive: true,
         canCreateCalendar,
         maxUserCalendarsDisabled,
-        mustReactivateCalendars,
         hasNoCalendars,
         timeStatus: EVENT_TIME_STATUS.FUTURE,
     };
@@ -536,7 +534,6 @@ export const getInitialInvitationModel = ({
         isAddressDisabled,
         canCreateCalendar,
         maxUserCalendarsDisabled,
-        mustReactivateCalendars,
         hasNoCalendars,
         invitationIcs: invitation,
         isPartyCrasher: isOrganizerMode || isImport ? false : !invitation.attendee,

applications/mail/src/app/helpers/calendar/inviteLink.tsx
@@ -24,7 +24,6 @@ export const getCalendarEventLink = (model: RequireSome<InvitationModel, 'invita
         invitationApi,
         hasNoCalendars,
         canCreateCalendar,
-        mustReactivateCalendars,
         hasDecryptionError,
     } = model;
 
@@ -51,7 +50,7 @@ export const getCalendarEventLink = (model: RequireSome<InvitationModel, 'invita
 
     const safeCalendarNeedsUserAction = calendarData?.calendarNeedsUserAction && !(isPartyCrasher && !isOrganizerMode);
     // the calendar needs a user action to be active
-    if (safeCalendarNeedsUserAction || mustReactivateCalendars) {
+    if (safeCalendarNeedsUserAction) {
         if (isImport) {
             return (
                 <AppLink to="/" toApp={APPS.PROTONCALENDAR}>{c('Link')

packages/components/hooks/useGetCalendarBootstrap.ts
@@ -22,11 +22,11 @@ export const useGetCalendarBootstrap = () => {
     );
 
     return useCallback(
-        (calendarID: string) => {
+        (calendarID: string, noCache?: boolean) => {
             if (!cache.has(KEY)) {
                 cache.set(KEY, createCache());
             }
-            return getPromiseValue(cache.get(KEY), calendarID, miss);
+            return getPromiseValue(cache.get(KEY), calendarID, miss, noCache ? 0 : undefined);
         },
         [cache, miss]
     );

commit 8472bf79a5eb965555e59eef31a642994112444a

Fix bug that broke Proton reinvite flow
We introduced a bug with commit 31e68f73eb8186adce7b5baf2e5f4c041840318c, due to which the ICS widget
would not recognize any longer a Proton reinvite as such.

applications/mail/src/app/helpers/calendar/inviteApi.ts
@@ -306,6 +306,7 @@ export const fetchEventInvitation: FetchEventInvitation = async ({
         const [vevent, parentVevent] = vevents;
         const result: Unwrap<ReturnType<FetchEventInvitation>> = {
             calendarData,
+            calendarEvent,
             singleEditData,
             supportedRecurrenceId,
         };