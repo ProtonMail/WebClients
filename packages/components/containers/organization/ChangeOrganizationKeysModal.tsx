88ab9ba1fedc7e0fc6018f39c3900034148a18d9
Jack Crompton <jack.crompton@proton.ch>
2022-03-21 22:15:15 +0000

    Migrate ChangeOrganizationKeysModal inputs
    CP-3575

--- packages/components/containers/organization/ChangeOrganizationKeysModal.tsx
+++ packages/components/containers/organization/ChangeOrganizationKeysModal.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 import { c } from 'ttag';
 import { updateOrganizationKeysLegacy, updateOrganizationKeysV2 } from '@proton/shared/lib/api/organization';
 import { DEFAULT_ENCRYPTION_CONFIG, ENCRYPTION_CONFIGS } from '@proton/shared/lib/constants';
@@ -10,19 +10,19 @@ import {
 } from '@proton/shared/lib/keys';
 import { CachedOrganizationKey, Member } from '@proton/shared/lib/interfaces';
 import { noop } from '@proton/shared/lib/helpers/function';
+import { confirmPasswordValidator, passwordLengthValidator } from '@proton/shared/lib/helpers/formValidators';
 import {
     Alert,
-    Field,
-    Label,
+    InputFieldTwo,
     ModalProps,
     ModalTwo as Modal,
     ModalTwoHeader as ModalHeader,
     ModalTwoContent as ModalContent,
     ModalTwoFooter as ModalFooter,
-    PasswordInput,
-    Row,
+    PasswordInputTwo,
     Button,
     Form,
+    useFormErrors,
 } from '../../components';
 import {
     useEventManager,
@@ -58,21 +58,16 @@ const ChangeOrganizationKeysModal = ({
     const { createModal } = useModals();
     const { createNotification } = useNotifications();
     const authentication = useAuthentication();
+    const { validator, onFormSubmit } = useFormErrors();
 
     const getAddresses = useGetAddresses();
     const { step, next, previous } = useStep();
     const [loading, withLoading] = useLoading();
     const [newPassword, setNewPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
-    const [confirmError, setConfirmError] = useState('');
     const [encryptionType, setEncryptionType] = useState(DEFAULT_ENCRYPTION_CONFIG);
 
     const handleSubmit = async () => {
-        if (confirmPassword !== newPassword) {
-            return setConfirmError(c('Error').t`Passwords do not match`);
-        }
-        setConfirmError('');
-
         const { privateKey, privateKeyArmored, backupKeySalt, backupArmoredPrivateKey } =
             await generateOrganizationKeys({
                 keyPassword: authentication.getPassword(),
@@ -132,8 +127,10 @@ const ChangeOrganizationKeysModal = ({
             return {
                 section: (
                     <>
-                        <Alert className="mb1">{c('Info')
-                            .t`This will create an encryption key for your organization. 4096-bit keys only work on high performance computers, for most users, we recommend using 2048-bit keys.`}</Alert>
+                        <div className="mb1">
+                            {c('Info')
+                                .t`This will create an encryption key for your organization. 4096-bit keys only work on high performance computers, for most users, we recommend using 2048-bit keys.`}
+                        </div>
                         <SelectEncryption encryptionType={encryptionType} setEncryptionType={setEncryptionType} />
                     </>
                 ),
@@ -152,38 +149,6 @@ const ChangeOrganizationKeysModal = ({
                             <Alert className="mb1">{c('Info')
                                 .t`Other administrators exist in your organization, you are responsible for communicating the new password to them.`}</Alert>
                         )}
-                        <Row>
-                            <Label htmlFor="organizationPassword">{c('Label').t`New organization password`}</Label>
-                            <Field>
-                                <PasswordInput
-                                    id="organizationPassword"
-                                    value={newPassword}
-                                    onChange={({ target: { value } }: ChangeEvent<HTMLInputElement>) =>
-                                        setNewPassword(value)
-                                    }
-                                    error={confirmError}
-                                    placeholder={c('Placeholder').t`Password`}
-                                    autoComplete="new-password"
-                                    required
-                                />
-                            </Field>
-                        </Row>
-                        <Row>
-                            <Label htmlFor="confirmPassword">{c('Label').t`Confirm organization password`}</Label>
-                            <Field>
-                                <PasswordInput
-                                    id="confirmPassword"
-                                    value={confirmPassword}
-                                    onChange={({ target: { value } }: ChangeEvent<HTMLInputElement>) =>
-                                        setConfirmPassword(value)
-                                    }
-                                    error={confirmError}
-                                    placeholder={c('Placeholder').t`Confirm`}
-                                    autoComplete="new-password"
-                                    required
-                                />
-                            </Field>
-                        </Row>
                         <Alert className="mb1" type="warning">
                             {c('Info')
                                 .t`Do NOT forget this password. If you forget it, you will not be able to manage your organization.`}
@@ -191,10 +156,40 @@ const ChangeOrganizationKeysModal = ({
                             {c('Info')
                                 .t`Save your password somewhere safe. Click on icon to confirm that you have typed your password correctly.`}
                         </Alert>
+
+                        <InputFieldTwo
+                            id="organizationPassword"
+                            as={PasswordInputTwo}
+                            label={c('Label').t`New organization password`}
+                            placeholder={c('Placeholder').t`Password`}
+                            value={newPassword}
+                            onValue={setNewPassword}
+                            error={validator([passwordLengthValidator(newPassword)])}
+                            autoComplete="new-password"
+                            autoFocus
+                        />
+
+                        <InputFieldTwo
+                            id="confirmPassword"
+                            as={PasswordInputTwo}
+                            label={c('Label').t`Confirm organization password`}
+                            placeholder={c('Placeholder').t`Confirm`}
+                            value={confirmPassword}
+                            onValue={setConfirmPassword}
+                            error={validator([
+                                passwordLengthValidator(newPassword),
+                                confirmPasswordValidator(newPassword, confirmPassword),
+                            ])}
+                            autoComplete="new-password"
+                        />
                     </>
                 ),
                 submitText: c('Action').t`Save`,
                 onSubmit() {
+                    if (!onFormSubmit()) {
+                        return;
+                    }
+
                     void withLoading(handleSubmit());
                 },
             };
