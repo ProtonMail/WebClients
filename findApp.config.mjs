d217e6382fa7ba36bbe5b210a2d25215334940a9
dhoko <agarroux@proton.ch>
2022-03-21 16:37:58 +0100

    Add configuration file find-app-depedencies

---
+++ findApp.config.mjs
@@ -0,0 +1,54 @@
+const TEST_FILES_GLOB = '**/*.{test,spec}.{js,ts,tsx,jsx}';
+
+export default {
+  packages: {
+    glob: 'packages/*',
+    directory: 'packages',
+    globIgnore: [TEST_FILES_GLOB, '__mocks__'],
+    alias: true,
+    scope: '@proton',
+    config: [
+      {
+        name: 'polyfill',
+        glob: 'index.js'
+      },
+      {
+        name: 'get-random-values',
+        glob: 'index.js'
+      },
+      {
+        name: 'srp',
+        glob: 'lib/**/*.{js,jsx,ts,tsx}'
+      },
+      {
+        name: 'shared',
+        glob: 'lib/**/*.{js,jsx,ts,tsx}'
+      },
+      {
+        name: 'key-transparency',
+        glob: 'lib/**/*.{js,jsx,ts,tsx}'
+      },
+      {
+        name: 'cross-storage',
+        glob: '{lib,*-impl}/**/*.{js,jsx,ts,tsx}'
+      },
+      {
+        name: 'components',
+        glob: '{{components,containers,helpers,hooks}/**/*,index}.{js,jsx,ts,tsx}'
+      },
+      {
+        name: 'encrypted-search',
+        glob: 'lib/**/*.{js,jsx,ts,tsx}'
+      }
+    ],
+    ignore: ['pack', 'config', 'i18n', 'eslint-config-proton', 'stylelint-config-proton', 'styles', 'testing']
+  },
+
+  applications: {
+    glob: 'applications/*',
+    directory: 'applications',
+    globIgnore: [TEST_FILES_GLOB],
+    sourcesGlob: 'src/app/**/*.{js,jsx,ts,tsx}',
+    ignore: ['storybook']
+  }
+};
