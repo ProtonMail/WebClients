70d25506f5e0cedc09ef6e50406fd0bb8683436b
MatthiasMargot <matthias.margot@proton.ch>
2022-03-22 14:58:00 +0100

    Improvements
    - Remove some unnecessary translations
    - Revert app name updates for logo config files
    - Fix typo (trully -> truly)
    
    CP-2476

--- applications/verify/src/assets/logoConfig.js
+++ applications/verify/src/assets/logoConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
     logo: 'src/assets/mail.svg',
     favicons: {
-        appName: 'Proton Mail',
+        appName: 'ProtonMail',
         appDescription:
-            "Proton Mail is the world's largest secure email service, developed by CERN and MIT scientists. We are open source and protected by Swiss privacy law",
+            "ProtonMail is the world's largest secure email service, developed by CERN and MIT scientists. We are open source and protected by Swiss privacy law",
         developerName: 'Proton AG',
         developerURL: 'https://github.com/ProtonMail/WebClients',
         background: '#1c223d',
