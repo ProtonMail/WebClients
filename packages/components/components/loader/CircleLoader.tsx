2b14bb21604de47a8e14ade5c8f03839f1c5a869
Matthieu Bué <matthieu.bue@gmail.com>
2022-03-23 19:08:29 +0100

    Improve circle loader rendering
    It seems that on Safari, the rendering of this loading spinner is a bit
    pixelated or blurry on rotating, so I removed the transformation
    and keep the animation using `stroke-dasharray` only.
    
    Adding more flexibility in the same time.
    
    CP-2427

--- packages/components/components/loader/CircleLoader.tsx
+++ packages/components/components/loader/CircleLoader.tsx
@@ -1,19 +1,27 @@
+import { useRef } from 'react';
 import { c } from 'ttag';
-import { classnames } from '../../helpers';
+import { classnames, generateUID } from '../../helpers';
 
 interface Props {
     className?: string;
 }
 const CircleLoader = ({ className }: Props) => {
+    const uid = generateUID('circle-loader');
+    const loaderCircle = useRef<SVGCircleElement>(null);
+
     return (
         <>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className={classnames(['circle-loader', className])}
-                viewBox="0 0 200 200"
+                viewBox="0 0 16 16"
+                style={{ '--total-length': loaderCircle.current?.getTotalLength() }}
             >
-                <circle cx="100" cy="100" r="70" className="circle-loader-track" />
-                <circle cx="100" cy="100" r="70" className="circle-loader-circle" />
+                <defs>
+                    <circle ref={loaderCircle} id={uid} cx="8" cy="8" r="7" />
+                </defs>
+                <use href={`#${uid}`} className="circle-loader-track" />
+                <use href={`#${uid}`} className="circle-loader-circle" />
             </svg>
             <span className="sr-only">{c('Info').t`Loading`}</span>
         </>
