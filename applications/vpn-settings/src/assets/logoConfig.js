658151580201974e7bd0b1de399260526e5786cb
MatthiasMargot <matthias.margot@proton.ch>
2022-03-23 11:56:03 +0100

    Put a space between app name and brand name
    CP-2476

--- applications/vpn-settings/src/assets/logoConfig.js
+++ applications/vpn-settings/src/assets/logoConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
     logo: 'src/assets/favicon-vpn-32.svg',
     favicons: {
-        appName: 'ProtonVPN Settings',
-        appDescription: 'Use ProtonVPN and protect your online activity',
+        appName: 'Proton VPN Settings',
+        appDescription: 'Use Proton VPN and protect your online activity',
         developerName: 'Proton AG',
         developerURL: 'https://github.com/ProtonMail/WebClients',
         background: '#ddd',
