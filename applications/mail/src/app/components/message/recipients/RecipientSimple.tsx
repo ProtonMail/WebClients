31f7199e3a955d0d171547352205a725c0562195
romain sanchez <romain.sanchez@proton.ch>
2022-03-29 14:01:58 +0200

    Message view feedbacks MAILWEB-3167

--- applications/mail/src/app/components/message/recipients/RecipientSimple.tsx
+++ applications/mail/src/app/components/message/recipients/RecipientSimple.tsx
@@ -7,11 +7,10 @@ import { RecipientOrGroup } from '../../../models/address';
 interface Props {
     recipientsOrGroup: RecipientOrGroup[];
     isLoading: boolean;
-    showAddress?: boolean;
     isOutside?: boolean;
 }
 
-const RecipientSimple = ({ isLoading, recipientsOrGroup, showAddress, isOutside }: Props) => {
+const RecipientSimple = ({ isLoading, recipientsOrGroup, isOutside }: Props) => {
     return (
         <div className="flex flex-nowrap flex-align-items-center" data-testid="message-header:to">
             <RecipientType label={c('Label').t`To`}>
@@ -23,11 +22,12 @@ const RecipientSimple = ({ isLoading, recipientsOrGroup, showAddress, isOutside 
                                       <RecipientItem
                                           recipientOrGroup={recipientOrGroup}
                                           isLoading={isLoading}
-                                          showAddress={showAddress}
                                           isOutside={isOutside}
+                                          isRecipient={true}
+                                          isExpanded={false}
                                       />
                                       {index < recipientsOrGroup.length - 1 && (
-                                          <span className="message-recipient-item-separator">,</span>
+                                          <span className="message-recipient-item-separator mr0-2">,</span>
                                       )}
                                   </span>
                               );
