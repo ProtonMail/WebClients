8236771a9337ea07599b4bd1f1c0754b01704b1e
Matthieu Bué <matthieu.bue@gmail.com>
2022-04-03 13:32:22 +0200

    Update icons documentation

--- applications/storybook/src/stories/coreConcepts/Icons.stories.tsx
+++ applications/storybook/src/stories/coreConcepts/Icons.stories.tsx
@@ -1,5 +1,5 @@
 import { useState, useMemo } from 'react';
-import { Icon, IconName, InputTwo, Mark } from '@proton/components';
+import { Button, ButtonGroup, Icon, IconName, InputTwo, Mark } from '@proton/components';
 import iconSvg from '@proton/styles/assets/img/icons/sprite-icons.svg';
 
 import { getTitle } from '../../helpers/title';
@@ -14,7 +14,12 @@ export default {
     },
 };
 
+type IconProps = React.ComponentProps<typeof Icon>;
+
+const sizes: Required<IconProps>['size'][] = [16, 20, 28, 40, 60];
+
 export const PrimaryIcons = () => {
+    const [selectedSize, setSelectedSize] = useState<Required<IconProps>['size']>(40);
     const primaryIconNames: IconName[] = iconSvg.match(/id="ic-([^"]+)/g).map((x: string) => x.replace('id="ic-', ''));
     const [search, setSearch] = useState('');
     const iconResults = useMemo(() => {
@@ -25,18 +30,32 @@ export const PrimaryIcons = () => {
     }, [search]);
     return (
         <>
-            <InputTwo
-                placeholder="Seach by name…"
-                value={search}
-                onChange={({ target: { value } }) => setSearch(value)}
-            />
-            <div className="icon-grid mt1">
+            <div className="flex flex-nowrap flex-gap-1">
+                <InputTwo
+                    placeholder={`Seach by name in those ${primaryIconNames.length} icons…`}
+                    value={search}
+                    onChange={({ target: { value } }) => setSearch(value)}
+                    className="flex-item-fluid"
+                />
+                <ButtonGroup>
+                    {sizes.map((size) => (
+                        <Button
+                            onClick={() => setSelectedSize(size)}
+                            selected={size === selectedSize}
+                            title={`Set icon size to ${size}`}
+                        >
+                            {size}
+                        </Button>
+                    ))}
+                </ButtonGroup>
+            </div>
+            <div className="icon-grid mt2">
                 {iconResults.map((iconName) => (
                     <div className="border rounded text-center p1" key={iconName}>
-                        <Icon name={iconName} size={40} />
-                        <code className="block mt1 user-select">
+                        <Icon name={iconName} size={selectedSize} />
+                        <div className="mt1 text-monospace lh120 user-select">
                             <Mark value={search}>{iconName}</Mark>
-                        </code>
+                        </div>
                     </div>
                 ))}
             </div>
