173ea248ad974b16f5a315db236c4aead012e431
Nicolas Hoffmann <dante3333@gmail.com>
2022-04-04 18:11:40 +0200

    Update theme modal to modalTwo
    - Updated theme modal
    - Slightly adapted theme cards - modal is wider
    
    CP-3673

--- packages/components/containers/themes/ThemeCard.tsx
+++ packages/components/containers/themes/ThemeCard.tsx
@@ -20,6 +20,7 @@ const ThemeCard = ({ label, id, src, selected, onChange, disabled }: Props) => {
             shape="outline"
             color={selected ? 'norm' : 'weak'}
             id={id}
+            fullWidth
             className={classnames([selected && 'is-active no-pointer-events text-bold'])}
             aria-pressed={selected}
             onClick={onChange}
@@ -28,7 +29,7 @@ const ThemeCard = ({ label, id, src, selected, onChange, disabled }: Props) => {
             aria-label={c('Action').t`Use ${label} theme`}
             title={c('Action').t`Use ${label} theme`}
         >
-            <span className="flex flex-nowrap flex-column">
+            <span className="flex flex-nowrap flex-column flex-align-items-center">
                 <img alt="" src={src} className="mb0-5 theme-card-image on-rtl-mirror" />
                 <span>{label}</span>
             </span>
