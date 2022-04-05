27690f6dd1778e7334e4e08e6667e50f7194e59f
Jack Crompton <jack.crompton@proton.ch>
2022-04-05 10:22:16 +0100

    Migrate DecryptFileKeyModal

--- packages/components/containers/keys/shared/DecryptFileKeyModal.tsx
+++ packages/components/containers/keys/shared/DecryptFileKeyModal.tsx
@@ -1,64 +1,94 @@
-import { ChangeEvent, useState } from 'react';
-import { decryptPrivateKey, OpenPGPKey } from 'pmcrypto';
+import { useState } from 'react';
 import { c } from 'ttag';
-import { Alert, Row, Label, Field, PasswordInput, FormModal } from '../../../components';
-
+import { decryptPrivateKey, OpenPGPKey } from 'pmcrypto';
+import { noop } from '@proton/shared/lib/helpers/function';
+import { requiredValidator } from '@proton/shared/lib/helpers/formValidators';
+import {
+    Button,
+    Form,
+    InputFieldTwo,
+    ModalProps,
+    ModalTwo as Modal,
+    ModalTwoHeader as ModalHeader,
+    ModalTwoContent as ModalContent,
+    ModalTwoFooter as ModalFooter,
+    PasswordInputTwo,
+    useFormErrors,
+} from '../../../components';
 import { generateUID } from '../../../helpers';
+import { useLoading } from '../../../hooks';
 
-interface Props {
+interface Props extends ModalProps {
     privateKey: OpenPGPKey;
     onSuccess: (privateKey: OpenPGPKey) => void;
-    onClose?: () => void;
 }
+
 const DecryptFileKeyModal = ({ privateKey, onSuccess, onClose, ...rest }: Props) => {
     const id = generateUID('decryptKey');
     const fingerprint = privateKey.getFingerprint();
     const fingerprintCode = <code key="0">{fingerprint}</code>;
 
     const [password, setPassword] = useState('');
-    const [decrypting, setDecrypting] = useState(false);
     const [error, setError] = useState('');
+
+    const [loading, withLoading] = useLoading();
+    const { validator, onFormSubmit } = useFormErrors();
 
     const handleSubmit = async () => {
         try {
-            setDecrypting(true);
-            setError('');
-
             const decryptedPrivateKey = await decryptPrivateKey(privateKey.armor(), password);
-
             onSuccess(decryptedPrivateKey);
             onClose?.();
         } catch (e: any) {
             setError(e.message);
-            setDecrypting(false);
         }
     };
 
+    const handleClose = loading ? noop : onClose;
+
     return (
-        <FormModal
-            title={c('Title').t`Decrypt key`}
-            loading={decrypting}
-            submit={c('Action').t`Decrypt`}
-            onSubmit={handleSubmit}
-            onClose={onClose}
+        <Modal
+            as={Form}
+            onSubmit={() => {
+                if (!onFormSubmit()) {
+                    return;
+                }
+
+                void withLoading(handleSubmit());
+            }}
+            onClose={handleClose}
             {...rest}
         >
-            <Alert className="mb1">{c('Label')
-                .jt`Enter the password for key with fingerprint: ${fingerprintCode}`}</Alert>
-            <Row>
-                <Label htmlFor={id}>{c('Label').t`Enter password`}</Label>
-                <Field>
-                    <PasswordInput
-                        id={id}
-                        value={password}
-                        error={error}
-                        onChange={({ target: { value } }: ChangeEvent<HTMLInputElement>) => setPassword(value)}
-                        autoFocus
-                        required
-                    />
-                </Field>
-            </Row>
-        </FormModal>
+            <ModalHeader title={c('Title').t`Decrypt key`} />
+            <ModalContent>
+                <div className="mb1">
+                    {c('Label').jt`Enter the password for key with fingerprint: ${fingerprintCode}`}
+                </div>
+
+                <InputFieldTwo
+                    id={id}
+                    as={PasswordInputTwo}
+                    label={c('Label').t`Enter password`}
+                    placeholder={c('Placeholder').t`Password`}
+                    value={password}
+                    onValue={(value: string) => {
+                        setError('');
+                        setPassword(value);
+                    }}
+                    error={validator([requiredValidator(password), error])}
+                    autoFocus
+                />
+            </ModalContent>
+            <ModalFooter>
+                <Button onClick={handleClose} disabled={loading}>
+                    {c('Action').t`Close`}
+                </Button>
+
+                <Button loading={loading} type="submit" color="norm">
+                    {c('Action').t`Decrypt`}
+                </Button>
+            </ModalFooter>
+        </Modal>
     );
 };
 
