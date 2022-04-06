290e15169ec1cb4e9bbad149051a99633d75f46b
Jack Crompton <jack.crompton@proton.ch>
2022-04-06 20:48:18 +0100

    Migrate MemberDowngradeModal

--- packages/components/containers/payments/MemberDowngradeModal.tsx
+++ packages/components/containers/payments/MemberDowngradeModal.tsx
@@ -3,52 +3,72 @@ import { c } from 'ttag';
 import { Organization } from '@proton/shared/lib/interfaces';
 import { requiredValidator } from '@proton/shared/lib/helpers/formValidators';
 
-import { Card, ErrorButton, FormModal, InputFieldTwo, useFormErrors } from '../../components';
+import {
+    Button,
+    Card,
+    Form,
+    InputFieldTwo,
+    ModalProps,
+    ModalTwo as Modal,
+    ModalTwoHeader as ModalHeader,
+    ModalTwoContent as ModalContent,
+    ModalTwoFooter as ModalFooter,
+    useFormErrors,
+} from '../../components';
 
-interface Props {
+interface Props extends ModalProps {
     organization: Organization;
     onConfirm: () => void;
-    onClose?: () => void;
 }
 
-const MemberDowngradeModal = ({ organization, onConfirm, ...rest }: Props) => {
+const MemberDowngradeModal = ({ organization, onConfirm, onClose, ...rest }: Props) => {
     const { validator, onFormSubmit } = useFormErrors();
     const [confirmText, setConfirmText] = useState('');
     const organizationName = organization.Name;
+
     return (
-        <FormModal
-            small
-            title={c('Title').t`Delete organization?`}
-            close={c('Action').t`Cancel`}
-            submit={<ErrorButton type="submit">{c('Action').t`Delete`}</ErrorButton>}
+        <Modal
+            as={Form}
             onSubmit={(event: FormEvent) => {
                 event.preventDefault();
                 if (!onFormSubmit()) {
                     return;
                 }
                 onConfirm();
-                rest.onClose?.();
+                onClose?.();
             }}
+            onClose={onClose}
             {...rest}
         >
-            <div className="mb1">{c('Member downgrade modal')
-                .t`This will permanently delete all sub-users, accounts, and data associated with your organization.`}</div>
-            <Card rounded className="text-break user-select mb1">
-                {organizationName}
-            </Card>
-            <InputFieldTwo
-                id="confirm-text"
-                bigger
-                label={c('Label').t`Enter organization name to confirm`}
-                error={validator([
-                    requiredValidator(confirmText),
-                    confirmText !== organizationName ? c('Error').t`Organization not recognized. Try again.` : '',
-                ])}
-                autoFocus
-                value={confirmText}
-                onValue={setConfirmText}
-            />
-        </FormModal>
+            <ModalHeader title={c('Title').t`Delete organization?`} />
+            <ModalContent>
+                <div className="mb1">
+                    {c('Member downgrade modal')
+                        .t`This will permanently delete all sub-users, accounts, and data associated with your organization.`}
+                </div>
+                <Card rounded className="text-break user-select mb1">
+                    {organizationName}
+                </Card>
+                <InputFieldTwo
+                    id="confirm-text"
+                    bigger
+                    label={c('Label').t`Enter organization name to confirm`}
+                    error={validator([
+                        requiredValidator(confirmText),
+                        confirmText !== organizationName ? c('Error').t`Organization not recognized. Try again.` : '',
+                    ])}
+                    autoFocus
+                    value={confirmText}
+                    onValue={setConfirmText}
+                />
+            </ModalContent>
+            <ModalFooter>
+                <Button onClick={onClose}>{c('Action').t`Cancel`}</Button>
+                <Button type="submit" color="danger">
+                    {c('Action').t`Delete`}
+                </Button>
+            </ModalFooter>
+        </Modal>
     );
 };
 
