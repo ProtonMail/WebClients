3ef14f4ccc152efb0324123fdaab75944729718e
Jack Crompton <jack.crompton@proton.ch>
2022-04-06 21:33:23 +0100

    Migrate CalendarDowngradeModal

--- packages/components/containers/payments/subscription/CalendarDowngradeModal.tsx
+++ packages/components/containers/payments/subscription/CalendarDowngradeModal.tsx
@@ -1,21 +1,15 @@
 import { c } from 'ttag';
 import { APPS } from '@proton/shared/lib/constants';
 import { getAppName } from '@proton/shared/lib/apps/helper';
-import { FormModal, AppLink, Alert } from '../../../components';
+import { AlertModal, AppLink, Button, ModalProps } from '../../../components';
 
-interface Props {
-    onClose: () => void;
-    onSubmit: () => void;
+interface Props extends ModalProps {
+    onConfirm: () => void;
 }
 
 const CALENDAR_APP_NAME = getAppName(APPS.PROTONCALENDAR);
 
-const CalendarDowngradeModal = ({ onSubmit, onClose, ...rest }: Props) => {
-    const handleSubmit = () => {
-        onSubmit();
-        onClose();
-    };
-
+const CalendarDowngradeModal = ({ onConfirm, onClose, ...rest }: Props) => {
     const linkButton = (
         <AppLink toApp={APPS.PROTONACCOUNT} to="/calendar/calendars" onClick={onClose}>
             {c('Action').t`Open ${CALENDAR_APP_NAME} settings`}
@@ -23,19 +17,30 @@ const CalendarDowngradeModal = ({ onSubmit, onClose, ...rest }: Props) => {
     );
 
     return (
-        <FormModal
+        <AlertModal
             title={c('Title').t`Downgrade account`}
-            submit={c('Action').t`OK`}
-            onSubmit={handleSubmit}
+            buttons={[
+                <Button
+                    onClick={() => {
+                        onConfirm();
+                        onClose?.();
+                    }}
+                    color="norm"
+                >
+                    {c('Action').t`OK`}
+                </Button>,
+                <Button onClick={onClose}>{c('Action').t`Cancel`}</Button>,
+            ]}
             onClose={onClose}
-            close={null}
             {...rest}
         >
-            <Alert className="mb1" type="warning">
+            <div>
                 {c('Info')
-                    .jt`You must remove any additional personal calendars and any shared calendar links before you can cancel your subscription. ${linkButton}`}
-            </Alert>
-        </FormModal>
+                    .t`You must remove any additional personal calendars and any shared calendar links before you can cancel your subscription.`}
+                <br />
+                {linkButton}
+            </div>
+        </AlertModal>
     );
 };
 
