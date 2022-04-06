a9b7a80b4aa565959e704cbfb8cbc2e3df1fe546
Sergo Vainumäe <sergo.vainumae@protonmail.ch>
2022-04-06 11:15:56 +0200

    Added auto-grow functionality to TextareaTwo

--- packages/components/components/v2/input/TextArea.tsx
+++ packages/components/components/v2/input/TextArea.tsx
@@ -1,29 +1,53 @@
-import { ComponentPropsWithRef, forwardRef, ReactNode, Ref } from 'react';
+import { ComponentPropsWithRef, forwardRef, ReactNode, Ref, useEffect, useRef } from 'react';
+import { useCombinedRefs } from '../../..';
 import { classnames } from '../../../helpers';
+import useAutoGrow from '../../../hooks/useAutoGrow';
 
 export interface TextAreaTwoProps extends Omit<ComponentPropsWithRef<'textarea'>, 'prefix'> {
     error?: ReactNode | boolean;
     disableChange?: boolean;
+    rows?: number;
+    minRows?: number;
+    autoGrow?: boolean;
     onValue?: (value: string) => void;
 }
 
 const TextAreaTwo = (props: TextAreaTwoProps, ref: Ref<HTMLTextAreaElement>) => {
-    const { error, className: classNameProp, onValue, disableChange, ...rest } = props;
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+    const {
+        error,
+        className: classNameProp,
+        onValue,
+        disableChange,
+        rows: maxRows = 5,
+        minRows = 1,
+        autoGrow = false,
+        ...rest
+    } = props;
 
     const className = classnames([classNameProp, 'w100 inputform-field', Boolean(error) && 'error']);
+    const { rows, updateTextArea } = useAutoGrow({ maxRows, minRows, autoGrow });
+
+    useEffect(() => {
+        if (textAreaRef.current) {
+            updateTextArea?.(textAreaRef.current);
+        }
+    }, [updateTextArea]);
 
     return (
         <textarea
             aria-invalid={!!error}
             {...rest}
-            ref={ref}
+            ref={useCombinedRefs(textAreaRef, ref)}
             onChange={(e) => {
                 if (disableChange) {
                     return;
                 }
                 onValue?.(e.target.value);
                 rest.onChange?.(e);
+                updateTextArea?.(textAreaRef.current);
             }}
+            rows={rows}
             className={className}
         />
     );
