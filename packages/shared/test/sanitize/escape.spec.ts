0862a97caa2d59c8c45c76f989dc7dbbed994d01
Guillaume Gonzalez <guillaume.gonzalez@proton.ch>
2022-04-11 12:06:21 +0200

    Improve escapeForbiddenStyles regex

---
+++ packages/shared/test/sanitize/escape.spec.ts
@@ -0,0 +1,19 @@
+import { escapeForbiddenStyle } from '../../lib/sanitize/escape';
+
+describe('Escape', () => {
+    describe('escapeForbiddenStyles', () => {
+        it('Should replace absolute styles by relative', () => {
+            expect(escapeForbiddenStyle('position: absolute')).toBe('position: relative');
+        });
+
+        it('Should replace percentage height by unset', () => {
+            expect(escapeForbiddenStyle('height: 100%; min-height: 30% ; max-height:  50%;')).toBe(
+                'height: unset; min-height: unset ; max-height:  unset;'
+            );
+        });
+
+        it('Should not replace percent line-height by unset', () => {
+            expect(escapeForbiddenStyle('line-height: 100%;')).toBe('line-height: 100%;');
+        });
+    });
+});
