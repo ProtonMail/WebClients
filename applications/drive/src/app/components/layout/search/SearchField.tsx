8b038f1125967ce5f0c90f0d52479a2a967dd692
Matthieu Bué <matthieu.bue@proton.ch>
2022-04-19 17:30:33 +0200

    Remove duplicated asset

--- applications/drive/src/app/components/layout/search/SearchField.tsx
+++ applications/drive/src/app/components/layout/search/SearchField.tsx
@@ -3,7 +3,7 @@ import { c } from 'ttag';
 
 import { Href, Searchbox, Spotlight, usePopperAnchor } from '@proton/components';
 // TODO: replace this with placeholders/star.svg icon after April 2022
-import esSpotlightIcon from '@proton/styles/assets/img/onboarding/drive-search-spotlight.svg';
+import esSpotlightIcon from '@proton/styles/assets/img/illustrations/spotlight-stars.svg';
 
 import { useSearchControl } from '../../../store';
 import useNavigate from '../../../hooks/drive/useNavigate';
