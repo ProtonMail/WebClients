94d849e9395b472765ad0ac68c1094da7cb9d3a0
romain sanchez <romain.sanchez@proton.ch>
2022-04-20 18:08:02 +0200

    Add unit tests for ExtraPinkKey, ExtraAskResign and TrustPublicKeyModal MAILWEB-3204

---
+++ applications/mail/src/app/components/message/modals/ContactResignModal.test.tsx
@@ -0,0 +1,103 @@
+import { Address } from '@proton/shared/lib/interfaces';
+import { ContactEmail } from '@proton/shared/lib/interfaces/contacts';
+import { fireEvent } from '@testing-library/dom';
+import { render, tick } from '../../../helpers/test/render';
+import ContactResignModal from './ContactResignModal';
+import { addKeysToUserKeysCache } from '../../../helpers/test/crypto';
+import { contactID, receiver, sender, setupContactsForPinKeys } from '../../../helpers/test/pinKeys';
+import { addToCache, minimalCache } from '../../../helpers/test/cache';
+import { addApiMock } from '../../../helpers/test/api';
+
+const contacts = [{ contactID: contactID }];
+const title = 'Contact Resign Modal title';
+const children = 'Contact Resign Modal children';
+
+describe('Contact resign modal', () => {
+    const setup = async (hasFingerprint: boolean) => {
+        minimalCache();
+        addToCache('Addresses', [{ Email: receiver.Address } as Address]);
+
+        const { receiverKeys, senderKeys } = await setupContactsForPinKeys(hasFingerprint);
+
+        addApiMock('keys', () => ({ Keys: [senderKeys] }));
+        addApiMock(
+            'contacts/v4/contacts/emails',
+            () => ({
+                ContactEmails: [{ Email: sender.Address, ContactID: contactID } as ContactEmail] as ContactEmail[],
+            }),
+            'get'
+        );
+
+        const updateSpy = jest.fn();
+        addApiMock(`contacts/v4/contacts/${contactID}`, updateSpy, 'put');
+
+        addKeysToUserKeysCache(receiverKeys);
+
+        const onResignSpy = jest.fn();
+
+        const container = await render(
+            <ContactResignModal title={title} contacts={contacts} onResign={onResignSpy} open>
+                {children}
+            </ContactResignModal>,
+            false
+        );
+
+        return { container, senderKeys, updateSpy, onResignSpy };
+    };
+
+    it('should resign the contact and render email rows', async () => {
+        const {
+            container: { getByText, getByTestId },
+            senderKeys,
+            updateSpy,
+            onResignSpy,
+        } = await setup(true);
+
+        // Modal and content are displayed
+        getByText(title);
+        getByText(children);
+
+        // Email rows are rendered (Email + fingerprints)
+        getByText(`${sender.Address}:`);
+        const expectedFingerPrint = senderKeys.publicKeys[0].getFingerprint();
+        getByText(expectedFingerPrint);
+
+        // Click on Re-sign button
+        const resignButton = getByTestId('resign-contact');
+        fireEvent.click(resignButton);
+        await tick();
+
+        // Contact update is called
+        expect(updateSpy).toHaveBeenCalled();
+
+        // Optional on Resign action is called
+        expect(onResignSpy).toHaveBeenCalled();
+    });
+
+    it('should resign the contact not render email rows', async () => {
+        const {
+            container: { getByText, queryByText, getByTestId },
+            updateSpy,
+            onResignSpy,
+        } = await setup(false);
+
+        // Modal and content are displayed
+        getByText(title);
+        getByText(children);
+
+        // Email rows are not rendered
+        const emailRow = queryByText(`${sender.Address}:`);
+        expect(emailRow).toBeNull();
+
+        // Click on Re-sign button
+        const resignButton = getByTestId('resign-contact');
+        fireEvent.click(resignButton);
+        await tick();
+
+        // Contact update is called
+        expect(updateSpy).toHaveBeenCalled();
+
+        // Optional on Resign action is called
+        expect(onResignSpy).toHaveBeenCalled();
+    });
+});
