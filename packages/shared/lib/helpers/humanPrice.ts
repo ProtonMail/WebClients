ceb9589ec1837a50173682114befc59e36d333ab
Jack Crompton <jack.crompton@proton.ch>
2022-04-25 13:58:16 +0100

    Convert helpers to TypeScript

--- packages/shared/lib/helpers/humanPrice.ts
+++ packages/shared/lib/helpers/humanPrice.ts
@@ -1,11 +1,8 @@
 /**
  * Make amount readable
  * 600 -> 6, 650 -> 6.50, 633 -> 6.33
- * @param {Number} amount
- * @param {Number} divisor
- * @return {String}
  */
-const humanPrice = (amount = 0, divisor = 100) => {
+const humanPrice = (amount: number = 0, divisor: number = 100) => {
     const fixedValue = Number(amount / divisor).toFixed(2);
     return fixedValue.replace('.00', '').replace('-', '');
 };
