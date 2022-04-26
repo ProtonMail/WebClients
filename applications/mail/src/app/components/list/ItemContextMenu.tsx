6435c0e29ecd9f3da8503b30e3c2b6e481181be8
Richard <richard@protonmail.com>
2022-04-26 17:50:54 +0200

    Review

--- applications/mail/src/app/components/list/ItemContextMenu.tsx
+++ applications/mail/src/app/components/list/ItemContextMenu.tsx
@@ -1,5 +1,5 @@
 import { c } from 'ttag';
-import { RefObject } from 'react';
+import { RefObject, ReactNode } from 'react';
 import { MESSAGE_BUTTONS, MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
 import { Folder } from '@proton/shared/lib/interfaces/Folder';
 import { ContextMenu, ContextSeparator, ContextMenuButton } from '@proton/components';
@@ -7,7 +7,8 @@ import { Label, MailSettings } from '@proton/shared/lib/interfaces';
 import { MARK_AS_STATUS, useMarkAs } from '../../hooks/useMarkAs';
 import { useGetElementsFromIDs } from '../../hooks/mailbox/useElements';
 import { useMoveToFolder } from '../../hooks/useApplyLabels';
-import { getFolderName } from '../../helpers/labels';
+import { getFolderName, isCustomFolder, isCustomLabel } from '../../helpers/labels';
+import { usePermanentDelete } from '../../hooks/usePermanentDelete';
 
 interface Props {
     mailSettings: MailSettings;
@@ -41,6 +42,8 @@ const ItemContextMenu = ({
     const markAs = useMarkAs();
     const moveToFolder = useMoveToFolder();
     const getElementsFromIDs = useGetElementsFromIDs();
+    const { handleDelete: permanentDelete, modal: deleteModal } = usePermanentDelete(labelID);
+
     const labelIDs = labels.map(({ ID }) => ID);
 
     const handleMarkAs = async (status: MARK_AS_STATUS) => {
@@ -62,6 +65,90 @@ const ItemContextMenu = ({
         }
     };
 
+    const handleDelete = async () => {
+        await permanentDelete(checkedIDs);
+    };
+
+    const inboxButton = (
+        <ContextMenuButton
+            testId="context-menu-inbox"
+            icon="inbox"
+            name={c('Action').t`Move to inbox`}
+            action={() => handleMove(MAILBOX_LABEL_IDS.INBOX)}
+        />
+    );
+
+    const nospamButton = (
+        <ContextMenuButton
+            testId="context-menu-nospam"
+            icon="fire-slash"
+            name={c('Action').t`Move to inbox (not spam)`}
+            action={() => handleMove(MAILBOX_LABEL_IDS.INBOX)}
+        />
+    );
+
+    const archiveButton = (
+        <ContextMenuButton
+            testId="context-menu-archive"
+            icon="archive-box"
+            name={c('Action').t`Move to archive`}
+            action={() => handleMove(MAILBOX_LABEL_IDS.ARCHIVE)}
+        />
+    );
+
+    const trashButton = (
+        <ContextMenuButton
+            testId="context-menu-trash"
+            icon="trash"
+            name={c('Action').t`Move to trash`}
+            action={() => handleMove(MAILBOX_LABEL_IDS.TRASH)}
+        />
+    );
+
+    const spamButton = (
+        <ContextMenuButton
+            testId="context-menu-spam"
+            icon="fire"
+            name={c('Action').t`Move to spam`}
+            action={() => handleMove(MAILBOX_LABEL_IDS.SPAM)}
+        />
+    );
+
+    const deleteButton = (
+        <ContextMenuButton
+            testId="context-menu-delete"
+            icon="cross-circle"
+            name={c('Action').t`Delete`}
+            action={() => handleDelete()}
+        />
+    );
+
+    let moveButtons: ReactNode[] = [];
+
+    if (labelID === MAILBOX_LABEL_IDS.INBOX) {
+        moveButtons = [trashButton, archiveButton, spamButton];
+    } else if (labelID === MAILBOX_LABEL_IDS.DRAFTS || labelID === MAILBOX_LABEL_IDS.ALL_DRAFTS) {
+        moveButtons = [trashButton, archiveButton, deleteButton];
+    } else if (labelID === MAILBOX_LABEL_IDS.SENT || labelID === MAILBOX_LABEL_IDS.ALL_SENT) {
+        moveButtons = [trashButton, archiveButton, deleteButton];
+    } else if (labelID === MAILBOX_LABEL_IDS.SCHEDULED) {
+        moveButtons = [trashButton, archiveButton];
+    } else if (labelID === MAILBOX_LABEL_IDS.STARRED) {
+        moveButtons = [trashButton, archiveButton, spamButton];
+    } else if (labelID === MAILBOX_LABEL_IDS.ARCHIVE) {
+        moveButtons = [trashButton, inboxButton, spamButton];
+    } else if (labelID === MAILBOX_LABEL_IDS.SPAM) {
+        moveButtons = [trashButton, nospamButton, deleteButton];
+    } else if (labelID === MAILBOX_LABEL_IDS.TRASH) {
+        moveButtons = [inboxButton, archiveButton, deleteButton];
+    } else if (labelID === MAILBOX_LABEL_IDS.ALL_MAIL) {
+        moveButtons = [trashButton, archiveButton, spamButton];
+    } else if (isCustomFolder(labelID, folders)) {
+        moveButtons = [trashButton, archiveButton, spamButton];
+    } else if (isCustomLabel(labelID, labels)) {
+        moveButtons = [trashButton, archiveButton, spamButton];
+    }
+
     const readButtons = [
         <ContextMenuButton
             testId="context-menu-read"
@@ -82,34 +169,14 @@ const ItemContextMenu = ({
     }
 
     return (
-        <ContextMenu noMaxHeight autoClose {...rest}>
-            <ContextMenuButton
-                testId="context-menu-inbox"
-                icon="inbox"
-                name={c('Action').t`Move to inbox`}
-                action={() => handleMove(MAILBOX_LABEL_IDS.INBOX)}
-            />
-            <ContextMenuButton
-                testId="context-menu-archive"
-                icon="archive-box"
-                name={c('Action').t`Move to archive`}
-                action={() => handleMove(MAILBOX_LABEL_IDS.ARCHIVE)}
-            />
-            <ContextMenuButton
-                testId="context-menu-trash"
-                icon="trash"
-                name={c('Action').t`Move to trash`}
-                action={() => handleMove(MAILBOX_LABEL_IDS.TRASH)}
-            />
-            <ContextMenuButton
-                testId="context-menu-spam"
-                icon="fire"
-                name={c('Action').t`Move to spam`}
-                action={() => handleMove(MAILBOX_LABEL_IDS.SPAM)}
-            />
-            <ContextSeparator />
-            {readButtons}
-        </ContextMenu>
+        <>
+            <ContextMenu noMaxHeight autoClose {...rest}>
+                {moveButtons}
+                <ContextSeparator />
+                {readButtons}
+            </ContextMenu>
+            {deleteModal}
+        </>
     );
 };
 
