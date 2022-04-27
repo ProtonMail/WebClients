ce9d308965703b5890eab13ee1d9940060f90b41
Richard Tetaz <richard.tetaz@proton.ch>
2022-04-27 10:42:02 +0200

    [IA-229] Remove percentage in Paused Badge

--- packages/components/containers/easySwitch/list/ActiveImportStatus.tsx
+++ packages/components/containers/easySwitch/list/ActiveImportStatus.tsx
@@ -20,7 +20,7 @@ const ActiveImportStatus = ({ processed, total, state, errorCode }: Props) => {
         case ImportStatus.PAUSED:
             return (
                 <>
-                    <Badge type="warning">{c('Import status').t`${percentageValue}% paused`}</Badge>
+                    <Badge type="warning">{c('Import status').t`Paused`}</Badge>
 
                     {errorCode === ImportError.ERROR_CODE_IMAP_CONNECTION && (
                         <Tooltip title={c('Tooltip').t`Account is disconnected`}>
