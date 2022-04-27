8ac535e9ab97660cbf93dd0d4df53380f3c564e7
MatthiasMargot <matthias.margot@proton.ch>
2022-04-27 22:15:22 +0200

    Add @proton/hooks package
    CP-3824

---
+++ packages/hooks/useSynchronizingState.test.ts
@@ -0,0 +1,39 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
+import useSynchronizingState from './useSynchronizingState';
+
+describe('useSynchronizingState()', () => {
+    it('initiates with the value passed to it', () => {
+        const hook = renderHook(() => useSynchronizingState('initial'));
+
+        const [value] = hook.result.current;
+
+        expect(value).toBe('initial');
+    });
+
+    it('sets the state just as useState would', () => {
+        const initial = 0;
+
+        const hook = renderHook(() => useSynchronizingState(initial));
+
+        act(() => {
+            const [, setState] = hook.result.current;
+
+            setState(1);
+        });
+
+        const [value] = hook.result.current;
+
+        expect(value).toBe(1);
+    });
+
+    it('synchronizes with the value passed to it on updates', () => {
+        const hook = renderHook(({ initial }) => useSynchronizingState(initial), { initialProps: { initial: 0 } });
+
+        hook.rerender({ initial: 1 });
+
+        const [value] = hook.result.current;
+
+        expect(value).toBe(1);
+    });
+});
