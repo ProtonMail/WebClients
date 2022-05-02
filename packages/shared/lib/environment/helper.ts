e84e826535980cf504b5b009eb95bd6fff530113
mmso <mmso@mmso.se>
2022-05-02 11:03:44 +0200

    Add relaunch to environment bits

--- packages/shared/lib/environment/helper.ts
+++ packages/shared/lib/environment/helper.ts
@@ -7,12 +7,16 @@ enum EnvironmentBits {
     beta = 0b010,
     default = 0b001,
 }
+
 // How many bits are allocated. Having one bit per environment, we end up with 3.
 // We dynamically calculate that from the enum, and since typescript generates
 // a reverse mapping, the value is divided by half.
 const maxBits = Object.keys(EnvironmentBits).length / 2;
 
 const getBit = (env?: Environment) => {
+    if (env === ('relaunch' as Environment)) {
+        return EnvironmentBits.alpha;
+    }
     return EnvironmentBits[env as keyof typeof EnvironmentBits] ?? EnvironmentBits.default;
 };
 
