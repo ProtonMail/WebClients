2af877e499d430e2df9801c116516c0fc08e39f1
mmso <mmso@mmso.se>
2022-05-03 18:18:51 +0200

    Update upsell panel and feature list

--- packages/components/containers/payments/subscription/SubscriptionPanel.tsx
+++ packages/components/containers/payments/subscription/SubscriptionPanel.tsx
@@ -25,7 +25,7 @@ import { MAX_CALENDARS_PER_USER } from '@proton/shared/lib/calendar/constants';
 import { getFreeServers, getPlusServers } from '@proton/shared/lib/vpn/features';
 
 import { useConfig } from '../../../hooks';
-import { Price, StrippedList, StrippedItem, Meter, Button, IconName } from '../../../components';
+import { Price, StrippedList, StrippedItem, Meter, Button, IconName, Icon } from '../../../components';
 import { OpenSubscriptionModalCallback } from './SubscriptionModalProvider';
 import { SUBSCRIPTION_STEPS } from './constants';
 import {
@@ -129,7 +129,7 @@ const SubscriptionPanel = ({
                     ] as Item[]
                 ).map((item) => {
                     return (
-                        <StrippedItem key={item.text} icon={item.icon}>
+                        <StrippedItem left={<Icon className="color-success" name={item.icon} size={20} />}>
                             {item.text}
                         </StrippedItem>
                     );
@@ -166,7 +166,7 @@ const SubscriptionPanel = ({
             <StrippedList>
                 {items.map((item) => {
                     return (
-                        <StrippedItem key={item.text} icon={item.icon}>
+                        <StrippedItem left={<Icon className="color-success" name={item.icon} size={20} />}>
                             {item.text}
                         </StrippedItem>
                     );
@@ -246,13 +246,13 @@ const SubscriptionPanel = ({
         ];
         return (
             <StrippedList>
-                <StrippedItem icon="storage">
+                <StrippedItem left={<Icon className="color-success" name="storage" size={20} />}>
                     <span className="block">{c('Label').t`${humanUsedSpace} of ${humanMaxSpace}`}</span>
                     <Meter className="mt1 mb1" aria-hidden="true" value={Math.ceil(percentage(MaxSpace, UsedSpace))} />
                 </StrippedItem>
                 {items.filter(isTruthy).map((item) => {
                     return (
-                        <StrippedItem key={item.text} icon={item.icon}>
+                        <StrippedItem left={<Icon className="color-success" name={item.icon} size={20} />}>
                             {item.text}
                         </StrippedItem>
                     );
--- packages/components/containers/referral/ReferralFeaturesList.tsx
+++ packages/components/containers/referral/ReferralFeaturesList.tsx
@@ -1,5 +1,5 @@
 import { c } from 'ttag';
-import { StrippedItem, StrippedList, Info } from '@proton/components';
+import { StrippedItem, StrippedList, Info, Icon } from '@proton/components';
 import { CALENDAR_APP_NAME, DRIVE_APP_NAME, MAIL_APP_NAME } from '@proton/shared/lib/constants';
 import humanSize from '@proton/shared/lib/helpers/humanSize';
 
@@ -7,7 +7,7 @@ const ReferralFeaturesList = () => {
     const storageSize = humanSize(5 * 1024 ** 3, undefined, undefined, 0);
     return (
         <StrippedList>
-            <StrippedItem icon="lock">
+            <StrippedItem left={<Icon className="color-success" name="lock" size={20} />}>
                 {c('Info').t`${storageSize} storage`}
                 <Info
                     className="ml0-5"
@@ -15,12 +15,24 @@ const ReferralFeaturesList = () => {
                         .t`Storage space is shared across ${MAIL_APP_NAME}, ${CALENDAR_APP_NAME}, and ${DRIVE_APP_NAME}.`}
                 />
             </StrippedItem>
-            <StrippedItem icon="inbox">{c('Info').t`5 email addresses`}</StrippedItem>
-            <StrippedItem icon="folders">{c('Info').t`Unlimited folders, labels, and filters`}</StrippedItem>
-            <StrippedItem icon="envelopes">{c('Info').t`Unlimited messages`}</StrippedItem>
-            <StrippedItem icon="speech-bubble">{c('Info').t`Support for 1 custom email domain`}</StrippedItem>
-            <StrippedItem icon="life-ring">{c('Info').t`Priority support`}</StrippedItem>
-            <StrippedItem icon="brand-proton-calendar">{CALENDAR_APP_NAME}</StrippedItem>
+            <StrippedItem left={<Icon className="color-success" name="inbox" size={20} />}>
+                {c('Info').t`5 email addresses`}
+            </StrippedItem>
+            <StrippedItem left={<Icon className="color-success" name="folders" size={20} />}>
+                {c('Info').t`Unlimited folders, labels, and filters`}
+            </StrippedItem>
+            <StrippedItem left={<Icon className="color-success" name="envelopes" size={20} />}>
+                {c('Info').t`Unlimited messages`}
+            </StrippedItem>
+            <StrippedItem left={<Icon className="color-success" name="speech-bubble" size={20} />}>
+                {c('Info').t`Support for 1 custom email domain`}
+            </StrippedItem>
+            <StrippedItem left={<Icon className="color-success" name="life-ring" size={20} />}>
+                {c('Info').t`Priority support`}
+            </StrippedItem>
+            <StrippedItem left={<Icon className="color-success" name="brand-proton-calendar" size={20} />}>
+                {CALENDAR_APP_NAME}
+            </StrippedItem>
         </StrippedList>
     );
 };
