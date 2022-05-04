f2333985bf6d4011ca433b9140a3d74253da1ba3
Jack Crompton <jack.crompton@proton.ch>
2022-05-04 14:21:34 +0100

    Remove unused Legend component

--- packages/components/components/text/index.ts
+++ packages/components/components/text/index.ts
@@ -1,4 +1,3 @@
-export { default as Legend } from './Legend';
 export { default as ErrorZone } from './ErrorZone';
 export { default as Mark } from './Mark';
 export { default as Marks } from './Marks';
