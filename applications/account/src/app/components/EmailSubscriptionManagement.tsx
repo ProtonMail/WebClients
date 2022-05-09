57b5e5be4127e14c5de091f1b946dca79ddd0388
MatthiasMargot <matthias.margot@proton.ch>
2022-05-09 17:28:10 +0200

    Replace old mail logo with new proton logo in lobby
    CP-3951

--- applications/account/src/app/components/EmailSubscriptionManagement.tsx
+++ applications/account/src/app/components/EmailSubscriptionManagement.tsx
@@ -1,9 +1,9 @@
-import { EmailSubscriptionCheckboxes } from '@proton/components';
 import { c } from 'ttag';
-import protonLogoSvg from '@proton/styles/assets/img/illustrations/proton-logo.svg';
+
+import { EmailSubscriptionCheckboxes, ProtonLogo } from '@proton/components';
+import { BRAND_NAME } from '@proton/shared/lib/constants';
 
 import PublicContainer from './PublicContainer';
-import './PublicLayout.scss';
 
 interface EmailSubscriptionManagementProps {
     News: number;
@@ -14,13 +14,9 @@ interface EmailSubscriptionManagementProps {
 const EmailSubscriptionManagement = ({ News, disabled, onChange }: EmailSubscriptionManagementProps) => {
     return (
         <PublicContainer className="mrauto mlauto">
-            <img
-                src={protonLogoSvg}
-                alt={c('Title').t`Proton Logo`}
-                className="public-layout--logo block mlauto mrauto mb2"
-            />
+            <ProtonLogo className="mb2" />
 
-            {c('Email Unsubscribe').jt`Which emails do you want to receive from Proton?`}
+            {c('Email Unsubscribe').jt`Which emails do you want to receive from ${BRAND_NAME}?`}
 
             <div className="mt2">
                 <EmailSubscriptionCheckboxes News={News} disabled={disabled} onChange={onChange} />
