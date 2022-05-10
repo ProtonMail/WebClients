2b6e5307bdcb65a4fc9c82171b7135d1be697953
MatthiasMargot <matthias.margot@proton.ch>
2022-05-10 14:51:00 +0200

    Fix the mnemonic modal closing automatically when opened
    The mnemonic modal wasn't actually closing itself, rather its parent container
    the checklist modal, which was instantiated using createModal(), was being un-
    mounted on item selection and since the mnemonic modal was a child of it, it
    couldn't render without the parent being in the tree itself.
    
    CP-3954

--- applications/mail/src/app/components/checklist/GetStartedChecklistModal.tsx
+++ applications/mail/src/app/components/checklist/GetStartedChecklistModal.tsx
@@ -1,79 +1,76 @@
 import {
-    DialogModal,
-    ModalCloseButton,
-    InnerModal,
-    ModalPropsInjection,
     useModals,
-    useModalState,
     useIsMnemonicAvailable,
     useSettingsLink,
     useUser,
     getCanReactiveMnemonic,
+    ModalProps,
+    ModalTwo as Modal,
+    ModalTwoHeader as ModalHeader,
+    ModalTwoContent as ModalContent,
 } from '@proton/components';
-import { MnemonicPromptModal } from '@proton/components/containers/mnemonic';
 import { ChecklistKey } from '@proton/shared/lib/interfaces';
 
 import GetStartedChecklist from './GetStartedChecklist';
 import ModalGetMobileApp from './ModalGetMobileApp';
 import ModalImportEmails from './ModalImportEmails';
 
-interface MailGetStartedChecklistModalProps extends Partial<ModalPropsInjection> {
+interface MailGetStartedChecklistModalProps extends ModalProps {
     onSendMessage: () => void;
+    onMnemonicItemSelection: () => void;
 }
 
-const MailGetStartedChecklistModal = ({ onClose, onSendMessage, ...rest }: MailGetStartedChecklistModalProps) => {
+const MailGetStartedChecklistModal = ({
+    onSendMessage,
+    onMnemonicItemSelection,
+    ...rest
+}: MailGetStartedChecklistModalProps) => {
     const [user] = useUser();
     const { createModal } = useModals();
-    const [mnemonicPromptModal, setMnemonicPromptModalOpen] = useModalState();
     const goToSettings = useSettingsLink();
 
     const [isMnemonicAvailable] = useIsMnemonicAvailable();
     const canReactivateMnemonic = getCanReactiveMnemonic(user);
     const displayMnemonicPrompt = isMnemonicAvailable && canReactivateMnemonic;
 
+    const handleItemSelection = (key: ChecklistKey) => () => {
+        /* eslint-disable default-case */
+        switch (key) {
+            case ChecklistKey.SendMessage: {
+                onSendMessage();
+                break;
+            }
+
+            case ChecklistKey.MobileApp: {
+                createModal(<ModalGetMobileApp />);
+                break;
+            }
+
+            case ChecklistKey.RecoveryMethod: {
+                if (displayMnemonicPrompt) {
+                    onMnemonicItemSelection();
+                } else {
+                    goToSettings('/recovery', undefined, true);
+                }
+                break;
+            }
+
+            case ChecklistKey.Import: {
+                createModal(<ModalImportEmails />);
+                break;
+            }
+        }
+
+        rest.onClose?.();
+    };
+
     return (
-        <>
-            <MnemonicPromptModal {...mnemonicPromptModal} />
-            {/* TODO Modal refactor */}
-            {/* eslint-disable-next-line deprecation/deprecation */}
-            <DialogModal intermediate onClose={onClose} {...rest}>
-                <ModalCloseButton onClose={onClose} />
-                <InnerModal className="modal-content pb2 pt2">
-                    <GetStartedChecklist
-                        onItemSelection={(key: ChecklistKey) => () => {
-                            onClose?.();
-
-                            /* eslint-disable default-case */
-                            switch (key) {
-                                case ChecklistKey.SendMessage: {
-                                    onSendMessage();
-                                    break;
-                                }
-
-                                case ChecklistKey.MobileApp: {
-                                    createModal(<ModalGetMobileApp />);
-                                    break;
-                                }
-
-                                case ChecklistKey.RecoveryMethod: {
-                                    if (displayMnemonicPrompt) {
-                                        setMnemonicPromptModalOpen(true);
-                                    } else {
-                                        goToSettings('/recovery', undefined, true);
-                                    }
-                                    break;
-                                }
-
-                                case ChecklistKey.Import: {
-                                    createModal(<ModalImportEmails />);
-                                    break;
-                                }
-                            }
-                        }}
-                    />
-                </InnerModal>
-            </DialogModal>
-        </>
+        <Modal size="medium" {...rest}>
+            <ModalHeader />
+            <ModalContent>
+                <GetStartedChecklist onItemSelection={handleItemSelection} />
+            </ModalContent>
+        </Modal>
     );
 };
 
