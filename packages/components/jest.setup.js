da07a0640771b1c323bfb2b13e0ee3a5ece3ade4
larabr <7375870+larabr@users.noreply.github.com>
2022-05-12 18:21:26 +0200

    Fix @proton/components tests

--- packages/components/jest.setup.js
+++ packages/components/jest.setup.js
@@ -10,3 +10,9 @@ window.ResizeObserver = jest.fn().mockImplementation(() => ({
     unobserve: jest.fn(),
     disconnect: jest.fn(),
 }));
+
+// Do not start crypto worker pool, let the single tests setup/mock the CryptoProxy as needed
+jest.mock('@proton/shared/lib/helpers/setupCryptoWorker', () => ({
+    __esModule: true,
+    loadCryptoWorker: jest.fn(),
+}));
