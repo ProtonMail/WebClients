87824c16fc1e2269751ae0e6d03854287cf023fd
Jack Crompton <jack.crompton@proton.ch>
2022-05-13 15:40:19 +0100

    Extract unary util into new file
    CP-3824

--- packages/util/function.test.ts
+++ packages/util/function.test.ts
@@ -1,5 +1,4 @@
 import { randomIntFromInterval } from './function';
-import unary from './unary';
 
 describe('Functions', () => {
     describe('Random int', () => {
@@ -7,17 +6,6 @@ describe('Functions', () => {
             const list = Array.from({ length: 100 }, () => randomIntFromInterval(1, 100));
             const test = list.some((n) => n < 1 || n > 100);
             expect(test).toEqual(false);
-        });
-    });
-
-    describe('unary', () => {
-        it('should ensure only one argument is passed', () => {
-            const myFunction = (name: string, index: number) => {
-                return index !== undefined ? `Ola ${name} - ${index}` : `Ola ${name}`;
-            };
-            const names = ['Joao', 'Felix', 'Tareixa'];
-            expect(names.map(myFunction)).toEqual(['Ola Joao - 0', 'Ola Felix - 1', 'Ola Tareixa - 2']);
-            expect(names.map(unary(myFunction))).toEqual(['Ola Joao', 'Ola Felix', 'Ola Tareixa']);
         });
     });
 });
