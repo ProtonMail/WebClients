28c1fccb3bdfca59c868e605f3ee392bfac716ae
Jack Crompton <jack.crompton@proton.ch>
2022-05-13 15:25:02 +0100

    Extract identity util into new file
    CP-3824

--- applications/mail/src/app/helpers/send/sendEncrypt.ts
+++ applications/mail/src/app/helpers/send/sendEncrypt.ts
@@ -1,5 +1,5 @@
 import { encryptMessage, splitMessage, generateSessionKey, encryptSessionKey, SessionKey, OpenPGPKey } from 'pmcrypto';
-import { identity } from '@proton/util/function';
+import identity from '@proton/util/identity';
 import { Package, Packages } from '@proton/shared/lib/interfaces/mail/crypto';
 import { Attachment } from '@proton/shared/lib/interfaces/mail/Message';
 import { hasBit } from '@proton/shared/lib/helpers/bitset';
