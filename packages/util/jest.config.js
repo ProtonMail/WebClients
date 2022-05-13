2ee71084bb5c8e679999ea009f192e659771bb5a
MatthiasMargot <matthias.margot@proton.ch>
2022-05-13 11:32:14 +0200

    Add coverage requirements to @proton/util
    CP-3824

--- packages/util/jest.config.js
+++ packages/util/jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    collectCoverageFrom: ['*.ts'],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
 };
