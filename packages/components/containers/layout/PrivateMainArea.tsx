acce02b2958f875b800365b43661f083b46c9a61
Jack Crompton <jack.crompton@proton.ch>
2022-05-18 13:11:05 +0100

    Fix PrivateMainArea react/display-name eslint warning

--- packages/components/containers/layout/PrivateMainArea.tsx
+++ packages/components/containers/layout/PrivateMainArea.tsx
@@ -7,23 +7,27 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
     hasToolbar?: boolean;
     hasRowMode?: boolean;
 }
-const PrivateMainArea = forwardRef(
-    ({ className, hasToolbar = false, children, hasRowMode = false, ...rest }: Props, ref: Ref<HTMLDivElement>) => {
-        return (
-            <main
-                className={classnames([
-                    hasToolbar ? 'main-area--with-toolbar' : 'main-area',
-                    hasRowMode ? 'main-area--row-mode' : '',
-                    'flex-item-fluid-auto relative',
-                    className,
-                ])}
-                ref={ref}
-                {...rest}
-            >
-                {children}
-            </main>
-        );
-    }
-);
+
+const PrivateMainAreaBase = (
+    { className, hasToolbar = false, children, hasRowMode = false, ...rest }: Props,
+    ref: Ref<HTMLDivElement>
+) => {
+    return (
+        <main
+            className={classnames([
+                hasToolbar ? 'main-area--with-toolbar' : 'main-area',
+                hasRowMode ? 'main-area--row-mode' : '',
+                'flex-item-fluid-auto relative',
+                className,
+            ])}
+            ref={ref}
+            {...rest}
+        >
+            {children}
+        </main>
+    );
+};
+
+const PrivateMainArea = forwardRef(PrivateMainAreaBase);
 
 export default PrivateMainArea;
