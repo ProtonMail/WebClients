67eea63f135679efd89d99c15075d5562cdf81b0
MatthiasMargot <matthias.margot@proton.ch>
2022-05-18 11:56:18 +0200

    Extract remove util into separate file
    CP-3824

--- packages/util/array.ts
+++ packages/util/array.ts
@@ -1,17 +1,4 @@
 /**
- * Remove the first occurrence of an item from an array. Return a copy of the updated array
- */
-export const remove = <T>(arr: T[], item: T) => {
-    const i = arr.indexOf(item);
-    if (i === -1) {
-        return arr;
-    }
-    const result = arr.slice();
-    result.splice(i, 1);
-    return result;
-};
-
-/**
  * Replace the first occurrence of an item from an array by another item. Return a copy of the updated array
  */
 export const replace = <T>(arr: T[], item: T, replacement: T) => {
