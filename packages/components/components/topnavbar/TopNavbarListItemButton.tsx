c68445fd6761b37294982f9a1dec407f52099c96
Jack Crompton <jack.crompton@proton.ch>
2022-05-18 13:03:53 +0100

    Fix TopNavbarListItemButton react/display-name eslint warning

--- packages/components/components/topnavbar/TopNavbarListItemButton.tsx
+++ packages/components/components/topnavbar/TopNavbarListItemButton.tsx
@@ -18,49 +18,49 @@ export type TopNavbarListItemButtonProps<E extends ElementType> = PolymorphicCom
 
 const defaultElement = 'button';
 
+const TopNavbarListItemButtonBase = <E extends ElementType = typeof defaultElement>(
+    {
+        color = 'weak',
+        shape = 'ghost',
+        text,
+        icon,
+        disabled,
+        className,
+        hasRedDot,
+        tabIndex,
+        children,
+        ...rest
+    }: TopNavbarListItemButtonProps<E>,
+    ref: typeof rest.ref
+) => {
+    const isDisabled = disabled;
+
+    return (
+        <ButtonLike
+            as={defaultElement}
+            color={color}
+            shape={shape}
+            className={classnames([
+                'topnav-link inline-flex flex-nowrap flex-align-items-center',
+                hasRedDot && 'relative topnav-link--blackfriday',
+                className,
+            ])}
+            disabled={isDisabled}
+            tabIndex={isDisabled ? -1 : tabIndex}
+            ref={ref}
+            {...rest}
+        >
+            {cloneElement(icon, {
+                className: classnames([icon.props.className, 'topnav-icon mr0-5']),
+            })}
+            <span className="navigation-title">{text}</span>
+            {children}
+        </ButtonLike>
+    );
+};
+
 const TopNavbarListItemButton: <E extends ElementType = typeof defaultElement>(
     props: TopNavbarListItemButtonProps<E>
-) => ReactElement | null = forwardRef(
-    <E extends ElementType = typeof defaultElement>(
-        {
-            color = 'weak',
-            shape = 'ghost',
-            text,
-            icon,
-            disabled,
-            className,
-            hasRedDot,
-            tabIndex,
-            children,
-            ...rest
-        }: TopNavbarListItemButtonProps<E>,
-        ref: typeof rest.ref
-    ) => {
-        const isDisabled = disabled;
-
-        return (
-            <ButtonLike
-                as={defaultElement}
-                color={color}
-                shape={shape}
-                className={classnames([
-                    'topnav-link inline-flex flex-nowrap flex-align-items-center',
-                    hasRedDot && 'relative topnav-link--blackfriday',
-                    className,
-                ])}
-                disabled={isDisabled}
-                tabIndex={isDisabled ? -1 : tabIndex}
-                ref={ref}
-                {...rest}
-            >
-                {cloneElement(icon, {
-                    className: classnames([icon.props.className, 'topnav-icon mr0-5']),
-                })}
-                <span className="navigation-title">{text}</span>
-                {children}
-            </ButtonLike>
-        );
-    }
-);
+) => ReactElement | null = forwardRef(TopNavbarListItemButtonBase);
 
 export default TopNavbarListItemButton;
