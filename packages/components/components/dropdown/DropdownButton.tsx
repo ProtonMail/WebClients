921e74529e75b79f4b2e4cf1be27c5046ebf8169
Jack Crompton <jack.crompton@proton.ch>
2022-05-18 12:55:15 +0100

    Fix DropdownButton react/display-name eslint warning

--- packages/components/components/dropdown/DropdownButton.tsx
+++ packages/components/components/dropdown/DropdownButton.tsx
@@ -16,48 +16,48 @@ export type DropdownButtonProps<E extends ElementType> = PolymorphicComponentPro
 
 const defaultElement = Button;
 
+const DropdownButtonBase = <E extends ElementType = typeof defaultElement>(
+    {
+        children,
+        className,
+        hasCaret = false,
+        isOpen = false,
+        caretClassName = '',
+        loading = false,
+        disabled,
+        ...rest
+    }: DropdownButtonProps<E>,
+    ref: typeof rest.ref
+) => {
+    return (
+        <Box
+            as={defaultElement}
+            ref={ref}
+            aria-expanded={isOpen}
+            aria-busy={loading}
+            disabled={loading ? true : disabled}
+            data-testid="dropdown-button"
+            className={classnames([children && hasCaret && 'flex flex-align-items-center flex-nowrap', className])}
+            {...rest}
+        >
+            {children}
+            {loading && (
+                <span className="button-loader-container">
+                    <CircleLoader />
+                </span>
+            )}
+            {hasCaret && (
+                <DropdownCaret
+                    className={classnames(['flex-item-noshrink', children ? 'ml0-5' : '', caretClassName])}
+                    isOpen={isOpen}
+                />
+            )}
+        </Box>
+    );
+};
+
 export const DropdownButton: <E extends ElementType = typeof defaultElement>(
     props: DropdownButtonProps<E>
-) => ReactElement | null = forwardRef(
-    <E extends ElementType = typeof defaultElement>(
-        {
-            children,
-            className,
-            hasCaret = false,
-            isOpen = false,
-            caretClassName = '',
-            loading = false,
-            disabled,
-            ...rest
-        }: DropdownButtonProps<E>,
-        ref: typeof rest.ref
-    ) => {
-        return (
-            <Box
-                as={defaultElement}
-                ref={ref}
-                aria-expanded={isOpen}
-                aria-busy={loading}
-                disabled={loading ? true : disabled}
-                data-testid="dropdown-button"
-                className={classnames([children && hasCaret && 'flex flex-align-items-center flex-nowrap', className])}
-                {...rest}
-            >
-                {children}
-                {loading && (
-                    <span className="button-loader-container">
-                        <CircleLoader />
-                    </span>
-                )}
-                {hasCaret && (
-                    <DropdownCaret
-                        className={classnames(['flex-item-noshrink', children ? 'ml0-5' : '', caretClassName])}
-                        isOpen={isOpen}
-                    />
-                )}
-            </Box>
-        );
-    }
-);
+) => ReactElement | null = forwardRef(DropdownButtonBase);
 
 export default DropdownButton;
