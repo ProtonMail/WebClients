3accac5db1143570038d276dc642fb5380dc6b5a
mmso <mmso@mmso.se>
2022-05-20 09:13:49 +0200

    Update meta tags

---
+++ applications/calendar/src/assets/favicon.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    logo: 'src/assets/favicon-calendar-static-32.svg',
+    favicons: {
+        appName: 'Proton Calendar',
+        appDescription:
+            'Proton Calendar helps you stay on top of your schedule while protecting your data. Apps available for Android, iOS, and the web.',
+    },
+};
--- applications/drive/src/assets/favicon.config.js
+++ applications/drive/src/assets/favicon.config.js
@@ -4,9 +4,5 @@ module.exports = {
         appName: 'Proton Drive',
         appDescription:
             'Proton Drive allows you to securely store and share your sensitive documents and access them anywhere.',
-        developerName: 'Proton AG',
-        developerURL: 'https://github.com/ProtonMail/WebClients',
-        background: '#1c223d',
-        theme_color: '#1c223d',
     },
 };
