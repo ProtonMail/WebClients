3e0351e3778677158b70146e1582628267ffa336
Matthieu Lux <matthieu.lux@protonmail.com>
2022-05-23 17:07:12 +0200

    Update row view and add unread indicator

---
+++ applications/mail/src/app/components/list/ItemUnread.tsx
@@ -0,0 +1,25 @@
+import { Icon } from '@proton/components';
+import { isUnread } from '../../helpers/elements';
+import { Element } from '../../models/element';
+
+interface Props {
+    element: Element;
+    labelID: string;
+    className?: string;
+}
+
+const ItemUnread = ({ element, labelID, className }: Props) => {
+    const unread = isUnread(element, labelID);
+
+    if (!unread) {
+        return null;
+    }
+
+    return (
+        <span className={className}>
+            <Icon name="circle-filled" size={12} className="color-primary" />
+        </span>
+    );
+};
+
+export default ItemUnread;
