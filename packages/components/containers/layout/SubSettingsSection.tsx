ee58b77d1af9771d7dde075c6062df4feab76b88
Jack Crompton <jack.crompton@proton.ch>
2022-05-24 14:10:53 +0100

    Add github like anchor icons to sub settings section title
    CP-4091
    
    Co-authored-by: Nicolas Hoffmann <nicolas.hoffmann@proton.ch>

--- packages/components/containers/layout/SubSettingsSection.tsx
+++ packages/components/containers/layout/SubSettingsSection.tsx
@@ -1,5 +1,7 @@
 import { ComponentPropsWithoutRef, ReactNode, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import { SettingsSectionTitle } from '../account';
+import { Icon } from '../../components';
 
 export interface SubSettingsSectionProps extends ComponentPropsWithoutRef<'div'> {
     id: string;
@@ -29,7 +31,19 @@ const SubSettingsSection = ({ id, observer, title, children, ...rest }: SubSetti
                 <div id={id} className="header-height-anchor" />
             </div>
             <section {...rest} id={id} ref={ref} data-target-id={id}>
-                {title && <SettingsSectionTitle>{title}</SettingsSectionTitle>}
+                {title && (
+                    <SettingsSectionTitle className="opacity-on-hover-container relative">
+                        <Link
+                            to={`#${id}`}
+                            className="sub-settings-section-anchor absolute opacity-on-hover"
+                            aria-hidden="true"
+                            tabIndex={-1}
+                        >
+                            <Icon name="link" />
+                        </Link>
+                        {title}
+                    </SettingsSectionTitle>
+                )}
                 {children}
             </section>
         </>
