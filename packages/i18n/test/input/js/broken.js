2ec652ef64cf69c0541f24774a5dd1edeef5f016
dhoko <agarroux@proton.ch>
2022-05-24 14:37:53 +0200

    Add new linter script to handle multilines + faster

--- packages/i18n/test/input/js/broken.js
+++ packages/i18n/test/input/js/broken.js
@@ -9,3 +9,11 @@ c('Action').c`Nope monique 2`
 c('Action').ngettext(msgid`Day`, 'Days', modifiedValue)
 c('Action').ngettext(msgid`Miinute`, "Miinutes", modifiedValue)
 c('Action').ngettext(msgid('Miinute'), "Miinutes", modifiedValue)
+
+mailpro2022: {
+    label: c('new_plans_mailpro2022_team_management_session_management_label')
+        .t`User session management`,
+    tooltip: c(`new_plans_mailpro2022_team_management_session_management_tooltip`)
+        .t`Force sign-out of user sessions when user credentials are believed to be compromised.`,
+    icon: <DynamicHeroIcon className="w-5 h-5" icon="CheckIcon" />,
+},
