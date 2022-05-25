45d9ec407fbf059aa28a593ef25f10cfb04ee437
Jack Crompton <jack.crompton@proton.ch>
2022-05-25 15:32:46 +0100

    update uncapitalize type

--- packages/util/uncapitalize.ts
+++ packages/util/uncapitalize.ts
@@ -1,8 +1,8 @@
 /**
  * Uncapitalize the first letter in a string.
  */
-export default function uncapitalize(str: any) {
-    if (str === '' || typeof str !== 'string') {
+export default function uncapitalize(str: string) {
+    if (str === '') {
         return '';
     }
     return str[0].toLowerCase() + str.slice(1);
