83d43a2876cb03f282a7ae37ba54014a189e659b
Matthieu Lux <matthieu.lux@protonmail.com>
2022-05-27 15:02:47 +0200

    New paging controls

--- applications/mail/src/app/components/toolbar/PagingControls.tsx
+++ applications/mail/src/app/components/toolbar/PagingControls.tsx
@@ -1,4 +1,13 @@
-import { Icon, DropdownMenu, DropdownMenuButton, EllipsisLoader, ToolbarButton } from '@proton/components';
+import {
+    Icon,
+    DropdownMenu,
+    DropdownMenuButton,
+    EllipsisLoader,
+    ToolbarButton,
+    ToolbarSeparator,
+    Scroll,
+    classnames,
+} from '@proton/components';
 import { c } from 'ttag';
 import { useLocation } from 'react-router';
 import ToolbarDropdown from './ToolbarDropdown';
@@ -37,8 +46,12 @@ const PagingControls = ({ loading, page: inputPage, total: inputTotal, onPage: i
         </DropdownMenuButton>
     );
 
+    // translator: Used for pagination, both values are number. Ex: "3 of 15"
+    const paginationLabel = c('Pagination').t`${page} of ${total}`;
+
     return (
         <>
+            <ToolbarSeparator />
             <ToolbarButton
                 disabled={loading || page <= 1}
                 title={c('Action').t`Previous page`}
@@ -49,29 +62,55 @@ const PagingControls = ({ loading, page: inputPage, total: inputTotal, onPage: i
             />
             <ToolbarDropdown
                 title={c('Action').t`Change page`}
-                content={String(page)}
+                content={paginationLabel}
                 disabled={total <= 1}
                 size="narrow"
                 data-testid="toolbar:page-number-dropdown"
+                hasCaret={false}
             >
                 {() => (
                     <DropdownMenu>
-                        {[...Array(total)].map((_, i) => {
-                            const pageNumber = i + 1; // paging tooling is 0 based
-                            return (
-                                <DropdownMenuButton
-                                    key={i} // eslint-disable-line react/no-array-index-key
-                                    loading={loading}
-                                    disabled={page - 1 === i}
-                                    onClick={() => onPage(i + 1)}
-                                    aria-label={c('Action').t`Page ${pageNumber}`}
-                                    data-testid={`toolbar:page-number-${pageNumber}`}
-                                >
-                                    {pageNumber}
-                                </DropdownMenuButton>
-                            );
-                        })}
-                        {useLoadMore && loadMore}
+                        <DropdownMenuButton
+                            loading={loading}
+                            disabled={page === 1}
+                            onClick={() => onPage(1)}
+                            aria-label={c('Action').t`Go to first page`}
+                            data-testid={`toolbar:first-page`}
+                        >
+                            {c('Action').t`Go to first page`}
+                        </DropdownMenuButton>
+                        <DropdownMenuButton
+                            loading={loading}
+                            disabled={page === total}
+                            onClick={() => onPage(total)}
+                            aria-label={c('Action').t`Go to last page`}
+                            data-testid={`toolbar:last-page`}
+                            className="border-bottom"
+                        >
+                            {c('Action').t`Go to last page`}
+                        </DropdownMenuButton>
+                        <Scroll style={{ height: '120px' }}>
+                            {[...Array(total)].map((_, i) => {
+                                const pageNumber = i + 1; // paging tooling is 0 based
+                                const active = page === pageNumber;
+                                return (
+                                    <DropdownMenuButton
+                                        key={i} // eslint-disable-line react/no-array-index-key
+                                        loading={loading}
+                                        disabled={active}
+                                        isSelected={active}
+                                        onClick={() => onPage(i + 1)}
+                                        aria-label={c('Action').t`Page ${pageNumber}`}
+                                        data-testid={`toolbar:page-number-${pageNumber}`}
+                                        className={classnames(['flex flex-row'])}
+                                    >
+                                        <span className="text-left flex-item-fluid">{pageNumber}</span>
+                                        {active ? <Icon name="checkmark" /> : null}
+                                    </DropdownMenuButton>
+                                );
+                            })}
+                            {useLoadMore && loadMore}
+                        </Scroll>
                     </DropdownMenu>
                 )}
             </ToolbarDropdown>
