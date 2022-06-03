1917b16ac5425fe896cddbc446ed276d7ec3cf71
Nicolas Hoffmann <dante3333@gmail.com>
2022-06-03 16:46:04 +0200

    Responsive fixes

--- applications/drive/src/app/components/SharedPage/SharedPageLayout.tsx
+++ applications/drive/src/app/components/SharedPage/SharedPageLayout.tsx
@@ -22,11 +22,11 @@ export default function SharedPageLayout({ reportAbuseButton, small, children }:
             </header>
             <div
                 className={classnames([
-                    'shared-page-layout-container flex flex-nowrap',
+                    'shared-page-layout-container flex-no-min-children flex-nowrap on-mobile-flex-column',
                     small && 'shared-page-layout-container-small',
                 ])}
             >
-                <div className="flex-item-fluid">{children}</div>
+                <div className="flex-item-fluid on-mobile-mt1">{children}</div>
                 {!small && <SharedPageGetDriveSidebar />}
             </div>
             <footer className="shared-page-layout-footer">
@@ -39,22 +39,23 @@ export default function SharedPageLayout({ reportAbuseButton, small, children }:
 
 function SharedPageGetDriveSidebar() {
     return (
-        <div className="flex flex-column flex-justify-center flex-nowrap w40 ml2">
+        <div className="flex flex-column flex-justify-center flex-nowrap w40 ml2 on-mobile-ml0">
             <h3>{c('Title').t`You've got files`}</h3>
             <p>{c('Info')
                 .t`These files were shared using end-to-end encryption. To ensure their security — and the security of all your documents, photos, and files — store them in Proton Drive, the cloud storage service you can trust.`}</p>
             <p>
                 <ButtonLike color="norm" shape="outline" as="a" href="/">{c('Action').t`Get Proton Drive`}</ButtonLike>
             </p>
-            <div className="flex flex-justify-space-between">
-                <span>
-                    <Icon name="lock-check-filled" /> {c('Info').t`Encrypted`}
+            <div className="flex flex-justify-space-between on-mobile-flex-column">
+                <span className="inline-flex flex-align-items-center flex-nowrap mr1">
+                    <Icon name="lock-check-filled" className="mr0-25" /> <span>{c('Info').t`Encrypted`}</span>
                 </span>
-                <span>
-                    <Icon name="plus-circle-filled" color="red" /> {c('Info').t`Swiss privacy`}
+                <span className="inline-flex flex-align-items-center flex-nowrap mr1">
+                    <Icon name="plus-circle-filled" color="red" className="mr0-25" />{' '}
+                    <span>{c('Info').t`Swiss privacy`}</span>
                 </span>
-                <span>
-                    <Icon name="shield-half-filled" /> {c('Info').t`Secure sharing`}
+                <span className="inline-flex flex-align-items-center flex-nowrap mr1">
+                    <Icon name="shield-half-filled" className="mr0-25" /> <span>{c('Info').t`Secure sharing`}</span>
                 </span>
             </div>
         </div>
