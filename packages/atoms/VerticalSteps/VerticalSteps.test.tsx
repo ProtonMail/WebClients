91070b5b0f58101fd14c31feae3b3093189a34f0
Richard <richard@protonmail.com>
2022-06-13 18:32:56 +0200

    Review

--- packages/atoms/VerticalSteps/VerticalSteps.test.tsx
+++ packages/atoms/VerticalSteps/VerticalSteps.test.tsx
@@ -1,20 +1,52 @@
 import { render } from '@testing-library/react';
 
-import { Basic } from './VerticalSteps.stories';
+import VerticalSteps from './VerticalSteps';
+import VerticalStep from './VerticalStep';
+
+function renderBasicVerticalSteps() {
+    return render(
+        <VerticalSteps>
+            <VerticalStep
+                icon="checkmark"
+                title="Choose a username"
+                description="You successfully selected your new email address."
+                status="passed"
+            />
+            <VerticalStep
+                icon="lock"
+                title="Today: get instant access"
+                description="15 GB secure mailbox with unlimited personalisation."
+                status="done"
+            />
+            <VerticalStep
+                icon="bell"
+                title="Day 24: Trial end reminder"
+                description="We’ll send you a notice. Cancel anytime."
+            />
+            <VerticalStep
+                icon="calendar-row"
+                title="Day 30: Trial ends"
+                description="Your subscription will start Jan 16th. Cancel anytime before."
+            />
+        </VerticalSteps>
+    );
+}
 
 describe('<VerticalSteps /> with Basic render', () => {
+    const output = renderBasicVerticalSteps();
+
     it('should display 4 steps', () => {
-        const { container } = render(<Basic />);
+        const { container } = output;
         expect(container.querySelectorAll('li')).toHaveLength(4);
     });
 
     it('should display icons', () => {
-        const { container } = render(<Basic />);
+        const { container } = output;
         expect(container.querySelectorAll('li > svg')).toHaveLength(4);
     });
 
     it('should display titles', () => {
-        const { container } = render(<Basic />);
+        const { container } = output;
         const titles = container.querySelectorAll('.steps-vertical-item-text > span:first-child');
         expect(titles).toHaveLength(4);
         expect(titles[0].textContent).toBe('Choose a username');
@@ -24,7 +56,7 @@ describe('<VerticalSteps /> with Basic render', () => {
     });
 
     it('should display descriptions', () => {
-        const { container } = render(<Basic />);
+        const { container } = output;
         const descriptions = container.querySelectorAll('.steps-vertical-item-text > span:last-child');
         expect(descriptions).toHaveLength(4);
         expect(descriptions[0].textContent).toBe('You successfully selected your new email address.');
