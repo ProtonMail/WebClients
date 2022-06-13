afef281a1717db453c17a6274af8e4af3880d1b5
Richard <richard@protonmail.com>
2022-06-13 18:15:53 +0200

    Review class names

--- packages/atoms/VerticalSteps/VerticalStep.tsx
+++ packages/atoms/VerticalSteps/VerticalStep.tsx
@@ -5,7 +5,7 @@ import { ComponentPropsWithoutRef, ReactNode } from 'react';
 export interface VerticalStepProps extends ComponentPropsWithoutRef<'li'> {
     description?: ReactNode;
     icon: IconName;
-    alt?: string;
+    alt?: string; // define `alt` only for non decorative icon
     status?: 'next' | 'done' | 'passed';
 }
 
@@ -22,12 +22,17 @@ const VerticalStep = ({ title, description, icon, alt, status = 'next', classNam
             <span
                 className={classnames([
                     'flex-item-noshrink rounded-50 vertical-steps-icon-container flex',
-                    ['done', 'passed'].includes(status) && 'bg-success',
+                    ['done', 'passed'].includes(status) ? 'bg-success' : 'bg-strong',
                 ])}
             >
                 <Icon name={icon} className="mauto" size={16} alt={alt} />
             </span>
-            <span className="flex-item-fluid pl0-5 flex flex-column flex-nowrap relative vertical-steps-item-text color-disabled">
+            <span
+                className={classnames([
+                    'flex-item-fluid pl0-5 flex flex-column flex-nowrap relative vertical-steps-item-text',
+                    status === 'passed' && 'color-disabled',
+                ])}
+            >
                 {title ? <span className={classnames([status !== 'passed' && 'text-semibold'])}>{title}</span> : null}
                 {description ? (
                     <span className={classnames(['text-sm', status !== 'passed' && 'color-weak'])}>{description}</span>
