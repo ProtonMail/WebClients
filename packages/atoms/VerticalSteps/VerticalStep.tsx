d5300e34909db2e489a646c862a6ad84653c677a
Richard <richard@protonmail.com>
2022-06-13 17:44:45 +0200

    Review

--- packages/atoms/VerticalSteps/VerticalStep.tsx
+++ packages/atoms/VerticalSteps/VerticalStep.tsx
@@ -14,20 +14,20 @@ const VerticalStep = ({ title, description, icon, alt, status = 'next', classNam
         <li
             {...rest}
             className={classnames([
-                'flex flex-nowrap flex-align-items-start steps-vertical-item',
-                status === 'passed' && 'steps-vertical-item--next-done',
+                'flex flex-nowrap flex-align-items-start vertical-steps-item',
+                status === 'passed' && 'vertical-steps-item--next-done',
                 className,
             ])}
         >
             <span
                 className={classnames([
-                    'flex-item-noshrink rounded-50 steps-vertical-icon-container flex',
+                    'flex-item-noshrink rounded-50 vertical-steps-icon-container flex',
                     ['done', 'passed'].includes(status) && 'bg-success',
                 ])}
             >
                 <Icon name={icon} className="mauto" size={16} alt={alt} />
             </span>
-            <span className="flex-item-fluid pl0-5 flex flex-column flex-nowrap relative steps-vertical-item-text color-disabled">
+            <span className="flex-item-fluid pl0-5 flex flex-column flex-nowrap relative vertical-steps-item-text color-disabled">
                 {title ? <span className={classnames([status !== 'passed' && 'text-semibold'])}>{title}</span> : null}
                 {description ? (
                     <span className={classnames(['text-sm', status !== 'passed' && 'color-weak'])}>{description}</span>
