392d5d521f0503d2dc05f36beaaa64582a9ae0e5
Richard <richard@protonmail.com>
2022-06-14 08:12:58 +0200

    Use clsx instead of classnames helper

--- packages/atoms/VerticalSteps/VerticalSteps.tsx
+++ packages/atoms/VerticalSteps/VerticalSteps.tsx
@@ -1,12 +1,12 @@
-import { classnames } from '@proton/components/helpers';
 import { ComponentPropsWithoutRef } from 'react';
 
+import clsx from '../clsx';
 import './VerticalSteps.scss';
 
 export interface VerticalStepsProps extends ComponentPropsWithoutRef<'ul'> {}
 
 const VerticalSteps = ({ className, ...rest }: VerticalStepsProps) => {
-    return <ul {...rest} className={classnames(['unstyled flex flex-column flex-nowrap vertical-steps', className])} />;
+    return <ul {...rest} className={clsx(['unstyled flex flex-column flex-nowrap vertical-steps', className])} />;
 };
 
 export default VerticalSteps;
