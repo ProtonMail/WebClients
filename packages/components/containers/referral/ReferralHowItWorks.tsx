295c9d7781d11be54c3281998ed1466de4681b28
Richard <richard@protonmail.com>
2022-06-21 13:51:55 +0200

    Update copy

--- packages/components/containers/referral/ReferralHowItWorks.tsx
+++ packages/components/containers/referral/ReferralHowItWorks.tsx
@@ -1,36 +1,35 @@
 import { c } from 'ttag';
 import { VerticalSteps, VerticalStep } from '@proton/atoms/VerticalSteps';
+import { addMonths, format } from 'date-fns';
+import { dateLocale } from '@proton/shared/lib/i18n';
 
 const ReferralHowItWorks = () => {
+    const endDate = addMonths(new Date(), 1);
+    const endText = format(endDate, 'PP', { locale: dateLocale });
+
     return (
         <VerticalSteps>
             <VerticalStep
                 icon="checkmark"
-                title={c('info').t`Choose a username`}
-                description={c('info').t`You successfully selected your new email address.`}
+                title={c('info').t`Create an email address`}
+                description={c('info').t`Your new email address is ready to go!`}
                 status="passed"
             />
             <VerticalStep
                 icon="lock"
-                title={c('info').t`Today: get instant access`}
-                description={c('info').t`15 GB secure mailbox with unlimited personalisation.`}
+                title={c('info').t`Try Mail Plus`}
+                description={c('info').t`Enjoy secure, easy-to-use emailing with plenty of premium features.`}
                 status="done"
             />
             <VerticalStep
                 icon="bell"
-                title={c('info').t`Day 24: Trial end reminder`}
-                description={c('info').t`We’ll send you a notice. Cancel anytime.`}
+                title={c('info').t`Day 27: Trial reminder`}
+                description={c('info').t`We’ll email you before your trial ends. Cancel anytime.`}
             />
             <VerticalStep
                 icon="calendar-today"
                 title={c('info').t`Day 30: Trial ends`}
-                description={
-                    <>
-                        {c('info').t`Your subscription will start after your 30-day trial.`}
-                        <br />
-                        {c('info').t`Cancel anytime before`}
-                    </>
-                }
+                description={c('info').t`Your subscription starts on ${endText}.`}
             />
         </VerticalSteps>
     );
