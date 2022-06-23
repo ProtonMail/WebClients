6ef5ff890872633516d8e6bd49057959a4091929
Michal Horejsek <michal.horejsek@proton.ch>
2022-06-23 10:12:33 +0200

    Add noindex tag for shared URLs

--- applications/drive/webpack.config.js
+++ applications/drive/webpack.config.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack');
 const getConfig = require('@proton/pack/webpack.config');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 /**
  * There are some specific references to Buffer in the drive application,
@@ -7,8 +9,31 @@ const getConfig = require('@proton/pack/webpack.config');
  */
 module.exports = (...env) => {
     const config = getConfig(...env);
+
+    const htmlIndex = config.plugins.findIndex((plugin) => {
+        return plugin instanceof HtmlWebpackPlugin;
+    });
+    const htmlPlugin = config.plugins[htmlIndex];
+
+    config.plugins.splice(
+        htmlIndex,
+        0,
+        new HtmlWebpackPlugin({
+            filename: 'urls.html',
+            template: path.resolve('./src/urls.ejs'),
+            templateParameters: htmlPlugin.userOptions.templateParameters,
+            scriptLoading: 'defer',
+            excludeChunks: ['index'],
+            inject: 'body',
+        })
+    );
+
     return {
         ...config,
+        entry: {
+            urls: [path.resolve('./src/app/index.tsx'), require.resolve('@proton/shared/lib/browser/supported.js')],
+            ...config.urls,
+        },
         resolve: {
             ...config.resolve,
             extensions: ['.js', '.tsx', '.ts', '...'], // ... is there to include default extensions for proper building of type script web workers.
