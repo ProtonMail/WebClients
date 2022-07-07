b9edc4b7c378adeec09e60662d27a244715a8ba5
Jack Crompton <jack.crompton@proton.ch>
2022-07-07 22:23:04 +0100

    Migrate shuffle to @proton/utils

--- packages/utils/array.ts
+++ packages/utils/array.ts
@@ -51,16 +51,3 @@ export const partition = <T, K = T>(arr: (T | K)[], predicate: (item: T | K) => 
         },
         [[], []]
     );
-
-/**
- * Fisher–Yates shuffle implementation - https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
- */
-export const shuffle = <T>(array: T[]): T[] => {
-    const arrayCopy = [...array];
-    for (let i = arrayCopy.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
-    }
-
-    return arrayCopy;
-};
