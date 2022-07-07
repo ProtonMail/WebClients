00b3d10f8611e12bac60a55c1bf5b7038508a753
Guillaume Gonzalez <guillaume.gonzalez@proton.ch>
2022-07-07 22:59:52 +0200

    Settings - Block sender list

---
+++ packages/components/containers/filters/spams/SpamsButtonDropdown.tsx
@@ -0,0 +1,49 @@
+import { useRef, useState } from 'react';
+import { Button, Dropdown, DropdownButton, DropdownMenu, DropdownMenuButton } from '@proton/components/components';
+import { DropdownButtonProps } from '@proton/components/components/dropdown/DropdownButton';
+import useUid from '@proton/components/hooks/useUid';
+import { SpamListAction } from './Spams.interfaces';
+
+interface Props {
+    title: string;
+    actions: SpamListAction[];
+    buttonProps?: DropdownButtonProps<typeof Button>;
+}
+
+const SpamsButtonDropdown = ({ title, actions, buttonProps }: Props) => {
+    const uid = useUid();
+    const anchorRef = useRef<HTMLButtonElement>(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+            <DropdownButton
+                as={Button}
+                isOpen={isOpen}
+                onClick={() => setIsOpen(!isOpen)}
+                ref={anchorRef}
+                type="button"
+                {...buttonProps}
+            >
+                {title}
+            </DropdownButton>
+            <Dropdown
+                anchorRef={anchorRef}
+                disableDefaultArrowNavigation
+                id={uid}
+                isOpen={isOpen}
+                onClose={() => setIsOpen(!isOpen)}
+            >
+                <DropdownMenu>
+                    {actions.map((action, index) => (
+                        <DropdownMenuButton key={index} className="text-left" onClick={action.onClick}>
+                            {action.name}
+                        </DropdownMenuButton>
+                    ))}
+                </DropdownMenu>
+            </Dropdown>
+        </>
+    );
+};
+
+export default SpamsButtonDropdown;
---
+++ packages/components/containers/filters/spams/hooks/useSpamApi.ts
@@ -0,0 +1,102 @@
+import { useApi } from '@proton/components/hooks';
+import {
+    addIncomingDefault,
+    deleteIncomingDefaults,
+    getIncomingDefaults,
+    updateIncomingDefault,
+} from '@proton/shared/lib/api/incomingDefaults';
+import { INCOMING_DEFAULTS_LOCATION } from '@proton/shared/lib/constants';
+import { IncomingDefault } from '@proton/shared/lib/interfaces';
+
+import { SpamItem, SpamLocation, SpamNavItem } from '../Spams.interfaces';
+
+const LOCATION_BY_TYPE: Record<SpamLocation, number> = {
+    SPAM: INCOMING_DEFAULTS_LOCATION.SPAM,
+    NON_SPAM: INCOMING_DEFAULTS_LOCATION.INBOX,
+    BLOCKED: INCOMING_DEFAULTS_LOCATION.BLOCKED,
+};
+
+interface IncomingDefaultsApiResults {
+    Code: number;
+    IncomingDefaults: IncomingDefault[];
+    Total: number;
+    GlobalTotal: number;
+}
+
+const getHumanReadableLocation = (location: INCOMING_DEFAULTS_LOCATION): SpamLocation => {
+    if (location === LOCATION_BY_TYPE.NON_SPAM) {
+        return 'NON_SPAM';
+    }
+    if (location === LOCATION_BY_TYPE.SPAM) {
+        return 'SPAM';
+    }
+
+    if (location === LOCATION_BY_TYPE.BLOCKED) {
+        return 'BLOCKED';
+    }
+
+    throw new Error('location is not valid');
+};
+
+export type FetchSpams = (
+    location: SpamNavItem,
+    search: string | undefined,
+    page: number,
+    limit: number,
+    abortController: AbortController
+) => Promise<{ list: SpamItem[]; total: number; globalTotal: number }>;
+
+const useSpamApi = () => {
+    const api = useApi();
+
+    const fetchSpams: FetchSpams = async (location, search, page, limit, abortController) => {
+        const result = await api<IncomingDefaultsApiResults>({
+            ...getIncomingDefaults({
+                Page: page,
+                Keyword: search,
+                PageSize: limit,
+                Location: location === 'ALL' ? undefined : LOCATION_BY_TYPE[location],
+            }),
+            signal: abortController ? abortController.signal : undefined,
+        });
+
+        const noResult = !result || !result?.IncomingDefaults || !Array.isArray(result.IncomingDefaults);
+        if (noResult) {
+            return { list: [], total: 0, globalTotal: 0 };
+        }
+
+        const nextList: SpamItem[] = result.IncomingDefaults.map((item) => {
+            const base = { id: item.ID, location: getHumanReadableLocation(item.Location) };
+
+            if (item.Domain) {
+                return { ...base, domain: item.Domain };
+            }
+
+            if (item.Email) {
+                return { ...base, email: item.Email };
+            }
+
+            throw new Error('Item is invalid');
+        });
+
+        return { list: nextList, total: result.Total, globalTotal: result.GlobalTotal };
+    };
+
+    const insertSpam = (location: SpamLocation, type: 'email' | 'domain', name: string) =>
+        api<{ Email: string; Location: number; Domain: string }>(
+            addIncomingDefault({
+                Location: LOCATION_BY_TYPE[location],
+                Domain: type === 'domain' ? name : undefined,
+                Email: type === 'email' ? name : undefined,
+            })
+        );
+
+    const updateSpam = (id: string, nextLocation: SpamLocation) =>
+        api<IncomingDefaultsApiResults>(updateIncomingDefault(id, { Location: LOCATION_BY_TYPE[nextLocation] }));
+
+    const deleteSpam = (id: string) => api(deleteIncomingDefaults([id]));
+
+    return { fetchSpams, insertSpam, updateSpam, deleteSpam };
+};
+
+export default useSpamApi;
