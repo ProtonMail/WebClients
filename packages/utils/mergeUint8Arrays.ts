768df24b2fd1f4bb0c432ffc678ce931e856f681
Jack Crompton <jack.crompton@proton.ch>
2022-07-08 17:18:50 +0100

    Migrate mergeUint8Arrays to @proton/utils

---
+++ packages/utils/mergeUint8Arrays.ts
@@ -0,0 +1,9 @@
+export default function mergeUint8Arrays(arrays: Uint8Array[]) {
+    const length = arrays.reduce((sum, arr) => sum + arr.length, 0);
+    const chunksAll = new Uint8Array(length);
+    arrays.reduce((position, arr) => {
+        chunksAll.set(arr, position);
+        return position + arr.length;
+    }, 0);
+    return chunksAll;
+}
