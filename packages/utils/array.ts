f59fee9667738d9aeda0a5045b21b25a0c89713d
Jack Crompton <jack.crompton@proton.ch>
2022-07-08 16:44:11 +0100

    Migrate addItem to @proton/utils

--- packages/utils/array.ts
+++ packages/utils/array.ts
@@ -27,8 +27,6 @@ export function areUint8Arrays(arr: any[]): arr is Uint8Array[] {
     return arr.every((el) => el instanceof Uint8Array);
 }
 
-export const addItem = <T>(array: T[], item: T) => array.concat(item);
-
 export const updateItem = <T>(array: T[], index: number, newItem: T) => {
     return array.map((item, i) => {
         if (i !== index) {
