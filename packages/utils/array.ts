7d4b86c9604efa7c38226538d9eced021db210b4
Jack Crompton <jack.crompton@proton.ch>
2022-07-08 17:27:01 +0100

    Migrate shallowEqual to @proton/utils

--- packages/utils/array.ts
+++ packages/utils/array.ts
@@ -1,18 +1,3 @@
-/**
- * Determine if two arrays are shallowy equal (i.e. they have the same length and the same elements)
- */
-export const shallowEqual = <T>(a: T[], b: T[]) => {
-    if (a.length !== b.length) {
-        return false;
-    }
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) {
-            return false;
-        }
-    }
-    return true;
-};
-
 export const partition = <T, K = T>(arr: (T | K)[], predicate: (item: T | K) => item is T): [T[], K[]] =>
     arr.reduce<[T[], K[]]>(
         (accumulator, current) => {
