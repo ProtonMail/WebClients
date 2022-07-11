76e0530dc434cc10c34153dba29558ba6986eda8
Richard <richard@protonmail.com>
2022-07-11 16:00:29 +0200

    Add experiment logic

--- applications/account/src/app/signup/ReferralStep.tsx
+++ applications/account/src/app/signup/ReferralStep.tsx
@@ -1,9 +1,18 @@
 import { useState } from 'react';
 import { c } from 'ttag';
-import { Button, ReferralFeaturesList, ReferralHowItWorks, useLoading } from '@proton/components';
+import {
+    Button,
+    ExperimentCode,
+    Loader,
+    ReferralFeaturesList,
+    ReferralHowItWorks,
+    useExperiment,
+    useLoading,
+} from '@proton/components';
 import { APPS, PLANS } from '@proton/shared/lib/constants';
 import { getAppName } from '@proton/shared/lib/apps/helper';
 import { PlanIDs } from '@proton/shared/lib/interfaces';
+
 import Header from '../public/Header';
 import Content from '../public/Content';
 import Text from '../public/Text';
@@ -21,47 +30,14 @@ interface Props {
 const ReferralStep = ({ onPlan, onBack }: Props) => {
     const [type, setType] = useState<PlanType | undefined>(undefined);
     const [loading, withLoading] = useLoading();
-    return (
-        <>
-            <Main>
-                <Header title={c('Title').t`Try the best of ${MAIL_APP_NAME} for free`} onBack={onBack} />
-                <Content>
-                    <Text>
-                        {c('Subtitle for trial plan')
-                            .t`The privacy-first Mail and Calendar solution for your everyday communications needs.`}
-                    </Text>
-                    <ReferralFeaturesList />
-                    <Button
-                        loading={loading && type === 'trial'}
-                        disabled={loading}
-                        color="norm"
-                        shape="solid"
-                        size="large"
-                        className="mb0-5"
-                        onClick={() => {
-                            setType('trial');
-                            withLoading(onPlan({ [PLANS.MAIL]: 1 }));
-                        }}
-                        fullWidth
-                    >{c('Action in trial plan').t`Try free for 30 days`}</Button>
-                    <p className="text-center mt0 mb0-5">
-                        <small className="color-weak">{c('Info').t`No credit card required`}</small>
-                    </p>
-                    <Button
-                        loading={loading && type === 'free'}
-                        disabled={loading}
-                        size="large"
-                        color="norm"
-                        shape="ghost"
-                        onClick={() => {
-                            setType('free');
-                            withLoading(onPlan({}));
-                        }}
-                        fullWidth
-                    >{c('Action in trial plan').t`No, thanks`}</Button>
-                </Content>
-            </Main>
+    const { experiment, loading: loadingExperiment } = useExperiment(ExperimentCode.ReferralProgramSignup);
 
+    if (loadingExperiment) {
+        return <Loader />;
+    }
+
+    if (experiment === 'B') {
+        return (
             <Main>
                 <Header title={c('Title').t`How your free trial works`} onBack={onBack} />
                 <Content>
@@ -97,7 +73,48 @@ const ReferralStep = ({ onPlan, onBack }: Props) => {
                     >{c('Action in trial plan').t`Continue with free plan`}</Button>
                 </Content>
             </Main>
-        </>
+        );
+    }
+
+    return (
+        <Main>
+            <Header title={c('Title').t`Try the best of ${MAIL_APP_NAME} for free`} onBack={onBack} />
+            <Content>
+                <Text>
+                    {c('Subtitle for trial plan')
+                        .t`The privacy-first Mail and Calendar solution for your everyday communications needs.`}
+                </Text>
+                <ReferralFeaturesList />
+                <Button
+                    loading={loading && type === 'trial'}
+                    disabled={loading}
+                    color="norm"
+                    shape="solid"
+                    size="large"
+                    className="mb0-5"
+                    onClick={() => {
+                        setType('trial');
+                        withLoading(onPlan({ [PLANS.MAIL]: 1 }));
+                    }}
+                    fullWidth
+                >{c('Action in trial plan').t`Try free for 30 days`}</Button>
+                <p className="text-center mt0 mb0-5">
+                    <small className="color-weak">{c('Info').t`No credit card required`}</small>
+                </p>
+                <Button
+                    loading={loading && type === 'free'}
+                    disabled={loading}
+                    size="large"
+                    color="norm"
+                    shape="ghost"
+                    onClick={() => {
+                        setType('free');
+                        withLoading(onPlan({}));
+                    }}
+                    fullWidth
+                >{c('Action in trial plan').t`No, thanks`}</Button>
+            </Content>
+        </Main>
     );
 };
 
