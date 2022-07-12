fd2268c5cab895762509b3decafff60c9131e49f
Richard Tetaz <richard.tetaz@proton.ch>
2022-07-12 10:14:21 +0200

    Review test

--- packages/atoms/VerticalSteps/VerticalStep.tsx
+++ packages/atoms/VerticalSteps/VerticalStep.tsx
@@ -1,53 +1,47 @@
 import { ComponentPropsWithoutRef, ReactNode } from 'react';
-import { Icon, IconName } from '@proton/components/components';
 
 import clsx from '../clsx';
 
 export interface VerticalStepProps extends ComponentPropsWithoutRef<'li'> {
     description?: ReactNode;
-    icon: IconName;
-    /** 
-     * define `alt` only for non decorative icons
-     * if the icon does not bring any added value to the text, leave it empty
-     */
-    alt?: string;
+    icon: ReactNode;
     /**
      * 'next': default state
-     * 'done': icon will be colored as success 
+     * 'done': icon will be colored as success
      * 'passed': icon will be colored as success, text will look like disabled, and green will be applied to the "path" to the next icon
      */
     status?: 'next' | 'done' | 'passed';
 }
 
-const VerticalStep = ({ title, description, icon, alt, status = 'next', className, ...rest }: VerticalStepProps) => {
+const VerticalStep = ({ title, description, icon, status = 'next', className, ...rest }: VerticalStepProps) => {
     return (
         <li
             {...rest}
-            className={clsx([
+            className={clsx(
                 'flex flex-nowrap flex-align-items-start vertical-steps-item',
                 status === 'passed' && 'vertical-steps-item--next-done',
-                className,
-            ])}
+                className
+            )}
         >
-            <span
-                className={clsx([
+            <div
+                className={clsx(
                     'flex-item-noshrink rounded-50 vertical-steps-icon-container flex',
-                    ['done', 'passed'].includes(status) ? 'bg-success' : 'bg-strong',
-                ])}
+                    ['done', 'passed'].includes(status) ? 'bg-success' : 'bg-strong'
+                )}
             >
-                <Icon name={icon} className="mauto" size={16} alt={alt} />
-            </span>
-            <span
-                className={clsx([
+                {icon}
+            </div>
+            <div
+                className={clsx(
                     'flex-item-fluid pl0-5 flex flex-column flex-nowrap relative vertical-steps-item-text',
-                    status === 'passed' && 'color-disabled',
-                ])}
+                    status === 'passed' && 'color-disabled'
+                )}
             >
                 {title ? <span className={clsx([status !== 'passed' && 'text-semibold'])}>{title}</span> : null}
                 {description ? (
                     <span className={clsx(['text-sm', status !== 'passed' && 'color-weak'])}>{description}</span>
                 ) : null}
-            </span>
+            </div>
         </li>
     );
 };
