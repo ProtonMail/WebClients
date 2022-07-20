3b79428f85a895ff1a5a123ba04050395b735763
romain sanchez <romain.sanchez@proton.ch>
2022-07-20 12:58:22 +0200

    Hide notification banner if the user is seeing the app for the first time MAILWEB-3400

--- packages/components/containers/topBanners/DesktopNotificationTopBanner.tsx
+++ packages/components/containers/topBanners/DesktopNotificationTopBanner.tsx
@@ -1,18 +1,36 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useLayoutEffect } from 'react';
 import { c } from 'ttag';
 import { getStatus, request, Status } from '@proton/shared/lib/helpers/desktopNotification';
 import { APP_NAMES, APPS } from '@proton/shared/lib/constants';
 import { getAppName } from '@proton/shared/lib/apps/helper';
 import { isMobile } from '@proton/shared/lib/helpers/browser';
+import { UserSettings } from '@proton/shared/lib/interfaces';
 
 import TopBanner from './TopBanner';
-import { useLocalState, useConfig } from '../../hooks';
+import { useLocalState, useConfig, useUserSettings } from '../../hooks';
 
 const DesktopNotificationTopBanner = () => {
     const [status, setStatus] = useState<Status>(getStatus());
     const [dontAsk, setDontAsk] = useLocalState(false, 'dont-ask-desktop-notification');
     const { APP_NAME } = useConfig();
     const onMobile = useRef(isMobile());
+    const [userSettings] = useUserSettings();
+
+    const initialUserSettings = useRef<UserSettings>();
+
+    const { AppWelcome = {} } = initialUserSettings.current || {};
+    const { Calendar = [], Mail = [] } = AppWelcome;
+
+    // We don't want to display this banner to a new user on the first use, we are waiting for a refresh to do it
+    const hasSeenAppBefore =
+        APP_NAME === APPS.PROTONMAIL ? !!Mail.length : APP_NAME === APPS.PROTONCALENDAR ? !!Calendar.length : false;
+    const canShowBanner = !!initialUserSettings && hasSeenAppBefore;
+
+    useLayoutEffect(() => {
+        if (!initialUserSettings.current && userSettings) {
+            initialUserSettings.current = userSettings;
+        }
+    }, [userSettings]);
 
     if (onMobile.current) {
         // NOTE: we could support mobile notification but this require to run a service worker
@@ -30,6 +48,10 @@ const DesktopNotificationTopBanner = () => {
     }
 
     if ([Status.GRANTED, Status.DENIED].includes(status)) {
+        return null;
+    }
+
+    if (!canShowBanner) {
         return null;
     }
 
