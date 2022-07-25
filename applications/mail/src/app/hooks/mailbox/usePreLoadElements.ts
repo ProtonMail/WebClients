43343b2439d1891c733c00a1748c86ea22dbe074
Richard <richard@protonmail.com>
2022-07-25 10:13:31 +0200

    Preload only conversations

--- applications/mail/src/app/hooks/mailbox/usePreLoadElements.ts
+++ applications/mail/src/app/hooks/mailbox/usePreLoadElements.ts
@@ -4,35 +4,28 @@ import { useDispatch } from 'react-redux';
 import { useApi } from '@proton/components/hooks';
 
 import { load as loadConversation } from '../../logic/conversations/conversationsActions';
-import { useGetMessage } from '../message/useMessage';
 
 const NUM_ELEMENT_TO_PRELOADED = 5;
 
 const usePreloadElements = (elementIDs: string[], isConversation: boolean) => {
-    // TODO review logic to preload messages
     const api = useApi();
     const dispatch = useDispatch();
-    const getMessage = useGetMessage();
     const firstElementIDs = elementIDs.slice(0, NUM_ELEMENT_TO_PRELOADED);
 
     useEffect(() => {
         const preload = async () => {
             try {
-                const elements = await Promise.all(
+                await Promise.all(
                     firstElementIDs.map(async (ID) => {
-                        if (isConversation) {
-                            return dispatch(loadConversation({ api, conversationID: ID, messageID: undefined }));
-                        }
-                        return getMessage(ID);
+                        return dispatch(loadConversation({ api, conversationID: ID, messageID: undefined }));
                     })
                 );
-                console.log(elements);
             } catch {
                 // ignore
             }
         };
 
-        if (firstElementIDs.length > 0) {
+        if (isConversation && firstElementIDs.length > 0) {
             void preload();
         }
     }, [elementIDs, isConversation]);
