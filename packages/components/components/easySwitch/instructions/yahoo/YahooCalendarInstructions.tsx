dbf91bdfac2cf12db6f23d98571cf46c8bf50ce4
Eduardo Conde Pena <econdepe@proton.ch>
2022-07-26 18:35:46 +0300

    Fix broken link for import-calendar knowledge base article
    CALWEB-3656

--- packages/components/components/easySwitch/instructions/yahoo/YahooCalendarInstructions.tsx
+++ packages/components/components/easySwitch/instructions/yahoo/YahooCalendarInstructions.tsx
@@ -1,12 +1,14 @@
 import { c } from 'ttag';
 
+import { IMPORT_CALENDAR_FAQ_URL } from '@proton/shared/lib/calendar/constants';
 import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
+
 import { Href } from '../../../link';
 
 const YahooCalendarInstructions = () => {
     // translator: full sentence: "To import a calendar to Proton, you need the ICS file. Download it from Yahoo in 3 easy steps:"
     const knowledgeBaseLink = (
-        <Href url={getKnowledgeBaseUrl('/how-to-import-calendar-to-proton-calendar')} key="knowledgeBaseLink">
+        <Href url={getKnowledgeBaseUrl(IMPORT_CALENDAR_FAQ_URL)} key="knowledgeBaseLink">
             {c('Import instructions link').t`ICS file`}
         </Href>
     );
