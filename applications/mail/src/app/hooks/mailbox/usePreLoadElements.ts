a38a831be19142d6247dd1707c8288329d9f9ecd
Richard <richard@protonmail.com>
2022-07-26 18:17:53 +0200

    Review

--- applications/mail/src/app/hooks/mailbox/usePreLoadElements.ts
+++ applications/mail/src/app/hooks/mailbox/usePreLoadElements.ts
@@ -7,7 +7,6 @@ import { findMessageToExpand } from '../../helpers/message/messageExpandable';
 import { load as loadConversation } from '../../logic/conversations/conversationsActions';
 import { initialize as initializeMessage } from '../../logic/messages/read/messagesReadActions';
 import { useGetConversation } from '../conversation/useConversation';
-import { useInitializeMessage } from '../message/useInitializeMessage';
 import { useGetMessage } from '../message/useMessage';
 
 const NUM_ELEMENT_TO_PRELOADED = 5;
@@ -16,7 +15,6 @@ const usePreLoadElements = (elementIDs: string[], isConversation: boolean, label
     const api = useApi();
     const dispatch = useDispatch();
     const firstElementIDs = elementIDs.slice(0, NUM_ELEMENT_TO_PRELOADED);
-    const initialize = useInitializeMessage();
     const getConversation = useGetConversation();
     const getMessage = useGetMessage();
 
@@ -25,19 +23,21 @@ const usePreLoadElements = (elementIDs: string[], isConversation: boolean, label
             try {
                 await Promise.all(
                     firstElementIDs.map(async (ID) => {
-                        await dispatch(loadConversation({ api, conversationID: ID, messageID: undefined }));
-                        const conversation = getConversation(ID);
-                        const messageToExpand = findMessageToExpand(labelID, conversation?.Messages);
+                        const cachedConversation = getConversation(ID);
 
-                        if (messageToExpand) {
-                            const cachedMessage = getMessage(messageToExpand.ID);
-                            // If not yet in cache, we set it
-                            if (!cachedMessage?.messageDocument?.initialized) {
-                                const messageState = { localID: messageToExpand.ID, data: messageToExpand };
-                                await dispatch(initializeMessage(messageState));
+                        if (!cachedConversation) {
+                            await dispatch(loadConversation({ api, conversationID: ID, messageID: undefined }));
+                            const conversation = getConversation(ID);
+                            const messageToExpand = findMessageToExpand(labelID, conversation?.Messages);
+
+                            if (messageToExpand) {
+                                const cachedMessage = getMessage(messageToExpand.ID);
+                                // If not yet in cache, we set it
+                                if (!cachedMessage?.messageDocument?.initialized) {
+                                    const messageState = { localID: messageToExpand.ID, data: messageToExpand };
+                                    await dispatch(initializeMessage(messageState));
+                                }
                             }
-
-                            return initialize(messageToExpand.ID, labelID);
                         }
                     })
                 );
