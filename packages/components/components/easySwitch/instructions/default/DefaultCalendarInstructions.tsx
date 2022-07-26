901404d36926d453d763c77e1bd30d84e0574000
Eduardo Conde Pena <econdepe@proton.ch>
2022-07-26 18:35:46 +0300

    Fix broken link for import-calendar knowledge base article
    CALWEB-3656

--- packages/components/components/easySwitch/instructions/default/DefaultCalendarInstructions.tsx
+++ packages/components/components/easySwitch/instructions/default/DefaultCalendarInstructions.tsx
@@ -1,12 +1,14 @@
 import { c } from 'ttag';
 
+import { IMPORT_CALENDAR_FAQ_URL } from '@proton/shared/lib/calendar/constants';
 import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
+
 import { Href } from '../../../link';
 
 const DefaultCalendarInstructions = () => {
     // translator: full sentence: "To import a calendar to Proton, you need the ICS file. Download this before you start the import process."
     const knowledgeBaseLink = (
-        <Href url={getKnowledgeBaseUrl('/how-to-import-calendar-to-proton-calendar')} key="knowledgeBaseLink">
+        <Href url={getKnowledgeBaseUrl(IMPORT_CALENDAR_FAQ_URL)} key="knowledgeBaseLink">
             {c('Import instructions link').t`ICS file`}
         </Href>
     );
