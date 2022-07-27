40b63d6ba74a83d8d111fe4d6e265c19117c11f4
Jack Crompton <jack.crompton@proton.ch>
2022-07-27 11:31:29 +0100

    Remove unecessary prop items from SidebarListItem

--- packages/components/components/collapsingBreadcrumbs/interfaces.ts
+++ packages/components/components/collapsingBreadcrumbs/interfaces.ts
@@ -1,13 +1,12 @@
 import { HTMLAttributes, ReactNode } from 'react';
 
 export interface BreadcrumbInfo
-    extends Omit<HTMLAttributes<HTMLButtonElement | HTMLLIElement>, 'children' | 'onClick' | 'className'> {
+    extends Omit<HTMLAttributes<HTMLButtonElement | HTMLLIElement>, 'children' | 'onClick'> {
     key: string | number;
     text: string;
     richText?: ReactNode;
     collapsedText?: ReactNode;
     noShrink?: boolean;
     highlighted?: boolean;
-    className?: string;
     onClick?: () => void;
 }
