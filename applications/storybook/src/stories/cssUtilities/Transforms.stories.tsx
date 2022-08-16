3c27c5c1c402c1e17ed4700d5df293792bd105de
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2022-08-16 11:47:46 +0100

    Split transforms and opacity into separate pages
    Opacity classes are not transforms
    
    UXE-34

---
+++ applications/storybook/src/stories/cssUtilities/Transforms.stories.tsx
@@ -0,0 +1,56 @@
+import { getTitle } from '../../helpers/title';
+import mdx from './Transforms.mdx';
+
+export default {
+    title: getTitle(__filename, false),
+    parameters: {
+        docs: {
+            page: mdx,
+        },
+    },
+};
+
+const demoItemClasses =
+    'user-select flex flex-align-items-center p1 flex-justify-center bg-primary rounded-sm text-center';
+
+const demoContainerClasses = 'border rounded w100 relative flex p4 flex-align-items-center flex-justify-space-around';
+
+export const Mirror = () => {
+    return (
+        <div className={demoContainerClasses}>
+            <div className={`${demoItemClasses} mirror`} style={{ width: '5rem', height: '5rem' }}>
+                Mirror
+            </div>
+        </div>
+    );
+};
+
+export const RotateX180 = () => {
+    return (
+        <div className={demoContainerClasses}>
+            <div className={`${demoItemClasses} rotateX-180`} style={{ height: '5rem' }}>
+                RotateX(180deg)
+            </div>
+        </div>
+    );
+};
+
+export const RotateZ90 = () => {
+    return (
+        <div className={demoContainerClasses}>
+            <div className={`${demoItemClasses} rotateZ-90`} style={{ height: '5rem' }}>
+                RotateZ(90deg)
+            </div>
+        </div>
+    );
+};
+
+export const RotateZ270 = () => {
+    return (
+        <div className={demoContainerClasses}>
+            <div className={`${demoItemClasses} rotateZ-270`} style={{ height: '5rem' }}>
+                RotateZ(270deg)
+            </div>
+        </div>
+    );
+};
