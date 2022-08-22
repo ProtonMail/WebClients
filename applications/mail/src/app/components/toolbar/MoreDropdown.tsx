428cd9b5a06a8568e4ff11905aaca45000869243
Richard Tetaz <richard.tetaz@proton.ch>
2022-08-22 10:56:57 +0200

    Remove vertical separator

--- applications/mail/src/app/components/toolbar/MoreDropdown.tsx
+++ applications/mail/src/app/components/toolbar/MoreDropdown.tsx
@@ -1,6 +1,5 @@
 import { c } from 'ttag';
 
-import { Vr } from '@proton/atoms';
 import { DropdownMenu, DropdownMenuButton, Icon } from '@proton/components';
 import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
 
@@ -156,7 +155,6 @@ const MoreDropdown = ({
 
     return (
         <>
-            <Vr />
             <ToolbarDropdown
                 title={c('Action').t`More`}
                 content={<Icon className="toolbar-icon" name="three-dots-horizontal" alt={c('Action').t`More`} />}
