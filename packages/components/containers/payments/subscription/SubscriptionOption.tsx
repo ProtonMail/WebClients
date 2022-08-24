1b68d2aa162662c07f30e1e8455a89d37d185a9d
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2022-08-24 16:43:50 +0100

    Remove flex-autogrid
    Replace autogrid with suitable flex classes
    UXE-81

--- packages/components/containers/payments/subscription/SubscriptionOption.tsx
+++ packages/components/containers/payments/subscription/SubscriptionOption.tsx
@@ -14,7 +14,7 @@ interface SubscriptionOptionProps {
 }
 
 const SubscriptionOption = ({ name, title, price, description, features = [], onSelect }: SubscriptionOptionProps) => (
-    <div className="flex-autogrid-item" data-plan-name={name}>
+    <div className="flex-item-fluid" data-plan-name={name}>
         <div className="border-container p2">
             <h3 className="h1 text-bold text-ellipsis">{title}</h3>
 
