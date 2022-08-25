c5cfda5ccd8819bb4a87cea53d069dab7b109cb2
Oleg Voloshin <oleg.voloshin@proton.ch>
2022-08-25 18:41:58 +0300

    Enable ES for everyone
    DRVWEB-2372

--- applications/drive/src/app/store/_search/useSearchEnabledFeature.tsx
+++ applications/drive/src/app/store/_search/useSearchEnabledFeature.tsx
@@ -1,11 +1,6 @@
-import { FeatureCode, useFeature, useUser } from '@proton/components';
 import { isMobile, isSafari } from '@proton/shared/lib/helpers/browser';
-import { isPaid } from '@proton/shared/lib/user/helpers';
 
 export default function useSearchEnabledFeature() {
-    const [user] = useUser();
-    const { feature } = useFeature(FeatureCode.DriveSearchEnabled);
-
     // Safari has several issues.
     // One: it is throttling a lot. First tens of items are done fast but
     // after ~ 500 items it goes very slowly and after ~ 2500 items it
@@ -15,5 +10,5 @@ export default function useSearchEnabledFeature() {
     // reported cases and we haven't found the issue yet.
     // Because of that, its better to not allow search on Safari at all
     // until we find some way around it.
-    return !isSafari() && !isMobile() && !!feature?.Value && !!isPaid(user);
+    return !isSafari() && !isMobile();
 }
