c902e9efd097b007c4f90ef0ed1a54a4870444e1
Marco <marco.martinoli@protonmail.com>
2022-08-31 14:03:39 +0200

    Add partial ES

--- applications/mail/src/app/components/header/search/MailSearch.tsx
+++ applications/mail/src/app/components/header/search/MailSearch.tsx
@@ -7,7 +7,7 @@ import {
     TopNavbarListItemSearchButton,
     generateUID,
     useAddresses,
-    useFeature,
+    useFeatures,
     useFolders,
     useLabels,
     useMailSettings,
@@ -47,12 +47,15 @@ const MailSearch = ({ breakpoints, labelID, location }: Props) => {
     const [, loadingLabels] = useLabels();
     const [, loadingFolders] = useFolders();
     const [, loadingAddresses] = useAddresses();
-    const { loading: loadingScheduledFeature } = useFeature(FeatureCode.ScheduledSend);
+    const [{ loading: loadingScheduledFeature }, { feature: partialES }] = useFeatures([
+        FeatureCode.ScheduledSend,
+        FeatureCode.PartialEncryptedSearch,
+    ]);
     const { getESDBStatus, cacheMailContent, closeDropdown } = useEncryptedSearchContext();
     const { dropdownOpened } = getESDBStatus();
     const esState = useEncryptedSearchToggleState(isOpen);
 
-    const showEncryptedSearch = !isMobile() && !!isPaid(user);
+    const showEncryptedSearch = !isMobile() && (!!isPaid(user) || (!!partialES && partialES.Value));
 
     // Show more from inside AdvancedSearch to persist the state when the overlay is closed
     const { state: showMore, toggle: toggleShowMore } = useToggle(false);
