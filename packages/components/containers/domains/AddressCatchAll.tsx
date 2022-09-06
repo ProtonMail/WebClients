08aea1b5c7f7bd339eaf36a8cb2cae770e3f2082
Matthieu Bué <matthieu.bue@proton.ch>
2022-09-06 19:40:18 +0200

    Update catch all layout and `disabled` condition
    UXE-47
    UXE-63

--- packages/components/containers/domains/AddressCatchAll.tsx
+++ packages/components/containers/domains/AddressCatchAll.tsx
@@ -1,9 +1,10 @@
 import { c } from 'ttag';
 
 import { updateCatchAll } from '@proton/shared/lib/api/domains';
+import { ADDRESS_STATUS, RECEIVE_ADDRESS } from '@proton/shared/lib/constants';
 import { Domain, DomainAddress } from '@proton/shared/lib/interfaces';
 
-import { Checkbox } from '../../components';
+import { Radio } from '../../components';
 import { useApiWithoutResult, useNotifications } from '../../hooks';
 
 interface Props {
@@ -15,11 +16,15 @@ interface Props {
 const AddressCatchAll = ({ address, domain, onChange }: Props) => {
     const { request, loading } = useApiWithoutResult(updateCatchAll);
     const { createNotification } = useNotifications();
+    const { Status, Receive } = address;
+
+    const isAddressActive = Status === ADDRESS_STATUS.STATUS_ENABLED && Receive === RECEIVE_ADDRESS.RECEIVE_YES;
 
     return (
-        <Checkbox
+        <Radio
+            name="catchAllRadioGroup"
             id={address.ID}
-            disabled={loading}
+            disabled={loading || !isAddressActive}
             checked={!!address.CatchAll}
             onChange={async ({ target }) => {
                 const newValue = target.checked;
