2331ac4710c1c5ad05293025d9392081ff7a9da8
Jack Crompton <jack.crompton@proton.ch>
2022-09-13 19:12:52 +0100

    Refactor LoadingStep

--- applications/account/src/app/signup/LoadingStep.tsx
+++ applications/account/src/app/signup/LoadingStep.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { c } from 'ttag';
 
 import { CircleLoader, Icon, ProtonLogo, classnames } from '@proton/components';
+import useInterval from '@proton/hooks/useInterval';
+import isTruthy from '@proton/utils/isTruthy';
 import noop from '@proton/utils/noop';
 
 import Content from '../public/Content';
@@ -14,51 +16,22 @@ interface Props {
 }
 
 const LoadingStep = ({ onSetup, hasPayment }: Props) => {
-    const [steps, setSteps] = useState(() => {
-        return [
-            {
-                loading: true,
-                text: c('Info').t`Creating your account`,
-            },
-        ];
-    });
+    const steps: string[] = [
+        c('Info').t`Creating your account`,
+        c('Info').t`Securing your account`,
+        hasPayment && c('Info').t`Verifying your payment`,
+    ].filter(isTruthy);
+
+    const [stepIndex, setStepIndex] = useState(0);
 
     useEffect(() => {
         onSetup().catch(noop);
-
-        const interval = setInterval(() => {
-            setSteps((steps) => {
-                const stepsWithoutLoading = steps.map((step) => ({ ...step, loading: false }));
-
-                if (stepsWithoutLoading.length === 1) {
-                    return [
-                        ...stepsWithoutLoading,
-                        {
-                            loading: true,
-                            text: c('Info').t`Securing your account`,
-                        },
-                    ];
-                }
-
-                clearInterval(interval);
-
-                if (!hasPayment) {
-                    return steps;
-                }
-
-                return [
-                    ...stepsWithoutLoading,
-                    {
-                        loading: true,
-                        text: c('Info').t`Verifying your payment`,
-                    },
-                ];
-            });
-        }, 2500);
-        return () => {
-            clearInterval(interval);
-        };
     }, []);
+
+    useInterval(() => {
+        const nextIndex = stepIndex + 1;
+        setStepIndex(nextIndex);
+    }, 2500);
 
     return (
         <Main>
@@ -67,23 +40,31 @@ const LoadingStep = ({ onSetup, hasPayment }: Props) => {
                     <ProtonLogo size={60} variant="glyph-only" />
                     <hr className="mb2 mt2" />
                     <div className="inline-block">
-                        {steps.map(({ loading, text }) => {
+                        {steps.map((step, i) => {
+                            const isPreviousStep = i < stepIndex;
+                            const isCurrentStep = i === stepIndex;
+
+                            const isVisibleStep = isPreviousStep || isCurrentStep;
+                            if (!isVisibleStep) {
+                                return null;
+                            }
+
                             return (
-                                <div className="text-lg" key={text}>
+                                <div className="text-lg" key={step}>
                                     <div
                                         className={classnames([
                                             'flex-no-min-children flex-align-items-center flex-nowrap',
-                                            loading && 'color-primary',
+                                            isCurrentStep && 'color-primary',
                                         ])}
                                     >
                                         <div className="mr0-5 min-w2e flex flex-item-noshrink">
-                                            {loading ? (
+                                            {isCurrentStep ? (
                                                 <CircleLoader size="small" className="ml0-25" />
                                             ) : (
                                                 <Icon size={24} className="color-success" name="checkmark" />
                                             )}
                                         </div>
-                                        <div className="flex-item-fluid p0-5 on-tiny-mobile-text-left">{text}</div>
+                                        <div className="flex-item-fluid p0-5 on-tiny-mobile-text-left">{step}</div>
                                     </div>
                                 </div>
                             );
