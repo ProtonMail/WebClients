75c43eed1a2fa5e573e7def76ad3f6fb5dd1d4ad
Richard <richard@protonmail.com>
2022-09-13 17:53:07 +0200

    Display progress

--- packages/components/containers/referral/rewards/RewardsProgress.tsx
+++ packages/components/containers/referral/rewards/RewardsProgress.tsx
@@ -1,7 +1,8 @@
 import { c } from 'ttag';
 
-import { Meter } from '@proton/components';
-import { PLANS, PLAN_NAMES } from '@proton/shared/lib/constants';
+import { Meter, usePlans, useUser } from '@proton/components';
+import { CYCLE, PLANS, PLAN_NAMES } from '@proton/shared/lib/constants';
+import { humanPriceWithCurrency } from '@proton/shared/lib/helpers/humanPrice';
 
 interface Props {
     rewards: number;
@@ -9,26 +10,24 @@ interface Props {
 }
 
 const RewardsProgress = ({ rewards, rewardsLimit }: Props) => {
+    const [user] = useUser();
+    const [plans = []] = usePlans();
+
     const planName = PLAN_NAMES[PLANS.MAIL];
+    const mailPlusPlan = plans.find(({ Name }) => Name === PLANS.MAIL);
+    const price = mailPlusPlan?.Pricing[CYCLE.MONTHLY] || 0;
+    const current = humanPriceWithCurrency(rewards * price, user.Currency);
+    const total = humanPriceWithCurrency(rewardsLimit * price, user.Currency);
 
     return (
         <div className="flex flex-justify-space-between flex-align-items-center flex-gap-1 on-tablet-flex-column">
             <div className="flex-item-fluid">
                 <b>{c('Info').t`${planName} credits earned`}</b>
             </div>
-            <div className="flex-item-fluid">
-                {rewards > 0 && (
-                    <Meter
-                        value={rewards}
-                        max={rewardsLimit}
-                        // translator: The months are free months of mailplus subscription. Full sentence can be something like this : "Earned 5 of 18 Mail Plus months"
-                        title={c('Info').t`Earned ${rewards} of ${rewardsLimit} ${planName} months.`}
-                    />
-                )}
-            </div>
+            <div className="flex-item-fluid">{rewards > 0 && <Meter value={rewards} max={rewardsLimit} />}</div>
             <div className="flex-item-fluid text-right">{
-                // translator: The months are free months of mailplus subscription. Full sentence can be something like this : "8 of 18 months"
-                c('Info').t`${rewards} of ${rewardsLimit} months`
+                // translator: The months are free months of mailplus subscription. Full sentence can be something like this : "$30 of $90"
+                c('Info').t`${current} of ${total}`
             }</div>
         </div>
     );
