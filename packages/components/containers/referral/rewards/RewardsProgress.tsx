b784a746272885f1409df7bb105bd49121bc17a4
Richard <richard@protonmail.com>
2022-09-14 09:17:10 +0200

    Apply feedback

--- packages/components/containers/referral/rewards/RewardsProgress.tsx
+++ packages/components/containers/referral/rewards/RewardsProgress.tsx
@@ -15,14 +15,14 @@ const RewardsProgress = ({ rewards, rewardsLimit }: Props) => {
 
     const planName = PLAN_NAMES[PLANS.MAIL];
     const mailPlusPlan = plans.find(({ Name }) => Name === PLANS.MAIL);
-    const price = mailPlusPlan?.Pricing[CYCLE.MONTHLY] || 0;
+    const price = Math.round(mailPlusPlan?.Pricing[CYCLE.MONTHLY] || 0); // Price rounded to 5
     const current = humanPriceWithCurrency(rewards * price, user.Currency);
     const total = humanPriceWithCurrency(rewardsLimit * price, user.Currency);
 
     return (
         <div className="flex flex-justify-space-between flex-align-items-center flex-gap-1 on-tablet-flex-column">
             <div className="flex-item-fluid">
-                <b>{c('Info').t`${planName} credits earned`}</b>
+                <b>{c('Info').t`Proton ${planName} credits earned`}</b>
             </div>
             <div className="flex-item-fluid">{rewards > 0 && <Meter value={rewards} max={rewardsLimit} />}</div>
             <div className="flex-item-fluid text-right">{
