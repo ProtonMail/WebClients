04004221e31e551a79e5373055a1414cd1e8a9e8
Edvin CANDON <edvin.candon@proton.ch>
2022-09-15 11:59:19 +0200

    Replace window.crypto references in crypto helpers
    - Agnostic global access to crypto (needed for usage in service workers)

--- packages/shared/lib/authentication/cryptoHelper.ts
+++ packages/shared/lib/authentication/cryptoHelper.ts
@@ -4,12 +4,12 @@ const IV_LENGTH = 16;
 const ALGORITHM = 'AES-GCM';
 
 export const getKey = (key: Uint8Array, keyUsage: KeyUsage[] = ['decrypt', 'encrypt']) => {
-    return window.crypto.subtle.importKey('raw', key.buffer, ALGORITHM, false, keyUsage);
+    return crypto.subtle.importKey('raw', key.buffer, ALGORITHM, false, keyUsage);
 };
 
 export const encryptData = async (key: CryptoKey, data: Uint8Array) => {
-    const iv = window.crypto.getRandomValues(new Uint8Array(IV_LENGTH));
-    const cipher = await window.crypto.subtle.encrypt(
+    const iv = crypto.getRandomValues(new Uint8Array(IV_LENGTH));
+    const cipher = await crypto.subtle.encrypt(
         {
             name: ALGORITHM,
             iv,
@@ -23,6 +23,6 @@ export const encryptData = async (key: CryptoKey, data: Uint8Array) => {
 export const decryptData = async (key: CryptoKey, data: Uint8Array) => {
     const iv = data.slice(0, IV_LENGTH);
     const cipher = data.slice(IV_LENGTH, data.length);
-    const result = await window.crypto.subtle.decrypt({ name: ALGORITHM, iv }, key, cipher);
+    const result = await crypto.subtle.decrypt({ name: ALGORITHM, iv }, key, cipher);
     return new Uint8Array(result);
 };
