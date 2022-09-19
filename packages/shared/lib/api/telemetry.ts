53660e5f584f36b177f57328dede15822c63bea9
romain sanchez <romain.sanchez@proton.ch>
2022-09-19 15:29:36 +0200

    Improve SL Telemetry for spam view MAILWEB-3577

--- packages/shared/lib/api/telemetry.ts
+++ packages/shared/lib/api/telemetry.ts
@@ -6,7 +6,7 @@ export enum TelemetryMeasurementGroups {
 
 export enum TelemetrySimpleLoginEvents {
     spam_view = 'spam_view',
-    newsletter_unsubscribe = 'unsubscribe',
+    newsletter_unsubscribe = 'newsletter_unsubscribe',
     simplelogin_modal_view = 'simplelogin_modal_view',
     go_to_simplelogin = 'go_to_simplelogin',
 }
