58abcf2275f16d7c18932e473cef56170f82c724
Jack Crompton <jack.crompton@proton.ch>
2022-09-20 16:20:15 +0100

    Remove DriveBeta feature flag

--- packages/components/containers/app/AppsDropdown.tsx
+++ packages/components/containers/app/AppsDropdown.tsx
@@ -6,8 +6,7 @@ import { getAppName } from '@proton/shared/lib/apps/helper';
 import { APPS } from '@proton/shared/lib/constants';
 
 import { AppLink, Icon, Logo, SettingsLink, SimpleDropdown, VpnLogo } from '../../components';
-import { useApps, useFeature } from '../../hooks';
-import { FeatureCode } from '../features';
+import { useApps } from '../../hooks';
 
 interface AppsDropdownProps {
     onDropdownClick?: () => void;
@@ -25,9 +24,6 @@ const AppsDropdown = ({ onDropdownClick, ...rest }: AppsDropdownProps, ref: Forw
 
     const itemClassName =
         'dropdown-item-link w100 flex flex-nowrap flex-align-items-center py0-25 pl1 pr1-5 color-norm text-no-decoration';
-
-    const driveBetaFeature = useFeature(FeatureCode.DriveBeta);
-    const showDriveBeta = driveBetaFeature.feature?.Value === true;
 
     return (
         <SimpleDropdown
@@ -52,11 +48,6 @@ const AppsDropdown = ({ onDropdownClick, ...rest }: AppsDropdownProps, ref: Forw
                                 <AppLink key={index} to="/" toApp={app} className={itemClassName} title={name}>
                                     <Logo appName={app} variant="glyph-only" className="flex-item-noshrink mr0-5" />
                                     {name}
-                                    {showDriveBeta && app === APPS.PROTONDRIVE && (
-                                        <span className="inline-block flex-item-noshrink bg-primary rounded-full text-sm px0-75 ml1">
-                                            Beta
-                                        </span>
-                                    )}
                                 </AppLink>
                             </li>
                             <li className="dropdown-item-hr my0-25" aria-hidden="true" />
