7fc4c4f77f6bf4389eb662f23a09fabc621c37ad
romain sanchez <romain.sanchez@proton.ch>
2022-09-26 10:20:18 +0200

    Prevent sending screen size to telemetry if app is inside an iframe MAILWEB-3587

--- packages/components/hooks/useTelemetryScreenSize.ts
+++ packages/components/hooks/useTelemetryScreenSize.ts
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-import { useApi, useUser } from '@proton/components/hooks/index';
+import { useApi, useSideApp, useUser } from '@proton/components/hooks/index';
 import { TelemetryMeasurementGroups, TelemetryScreenSizeEvents } from '@proton/shared/lib/api/telemetry';
 import { SECOND } from '@proton/shared/lib/constants';
 import { sendTelemetryReport } from '@proton/shared/lib/helpers/metrics';
@@ -9,8 +9,14 @@ import debounce from '@proton/utils/debounce';
 const useTelemetryScreenSize = () => {
     const api = useApi();
     const [user] = useUser();
+    const { parentApp } = useSideApp();
 
     const sendScreenSizeToTelemetry = async (event: TelemetryScreenSizeEvents) => {
+        // If app is displayed inside an iframe, we don't want to send the screensize
+        if (parentApp) {
+            return;
+        }
+
         // We need to send monitor size and view size
         const values = {
             screenWidth: screen.width,
