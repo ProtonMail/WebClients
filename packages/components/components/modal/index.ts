9f97c4a30d1c85bf0dbcd1dfdfddabdd3052600a
Jack Crompton <jack.crompton@proton.ch>
2022-09-27 12:38:59 +0100

    Remove unused BackButtonModal

--- packages/components/components/modal/index.ts
+++ packages/components/components/modal/index.ts
@@ -3,7 +3,6 @@ export { default as ContentModal } from './Content';
 export { default as InnerModal } from './Inner';
 export { default as HeaderModal } from './Header';
 export { default as TitleModal } from './Title';
-export { default as BackButtonModal } from './BackButton';
 export { default as FormModal } from './FormModal';
 export { default as OverlayModal } from './Overlay';
 export { default as ConfirmModal } from './Confirm';
