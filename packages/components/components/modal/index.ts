1627354d1561547172ae8a4035ded9a018902fdb
Jack Crompton <jack.crompton@proton.ch>
2022-09-27 12:42:50 +0100

    Remove unused DeleteModal

--- packages/components/components/modal/index.ts
+++ packages/components/components/modal/index.ts
@@ -7,7 +7,6 @@ export { default as FormModal } from './FormModal';
 export { default as OverlayModal } from './Overlay';
 export { default as ConfirmModal } from './Confirm';
 export * from './Confirm';
-export { default as DeleteModal } from './DeleteModal';
 export { default as SimpleFormModal } from './SimpleFormModal';
 export { default as PreviewPDFModal } from './PreviewPDFModal';
 export { default as FooterModal } from './Footer';
