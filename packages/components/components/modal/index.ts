bcd647f8aabb119e7ec7c653e66bdc2999ea2f57
Jack Crompton <jack.crompton@proton.ch>
2022-09-27 12:48:02 +0100

    Remove unused SimpleFormModal

--- packages/components/components/modal/index.ts
+++ packages/components/components/modal/index.ts
@@ -6,7 +6,6 @@ export { default as TitleModal } from './Title';
 export { default as FormModal } from './FormModal';
 export { default as ConfirmModal } from './Confirm';
 export * from './Confirm';
-export { default as SimpleFormModal } from './SimpleFormModal';
 export { default as PreviewPDFModal } from './PreviewPDFModal';
 export { default as FooterModal } from './Footer';
 export { default as ModalCloseButton } from './ModalCloseButton';
