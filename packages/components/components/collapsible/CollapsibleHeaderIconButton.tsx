46268d9aba3ca7e2ea215488871bd83774340782
Eduardo Conde Pena <econdepe@proton.ch>
2022-09-29 12:37:14 +0200

    Handle loading state more gracefully

--- packages/components/components/collapsible/CollapsibleHeaderIconButton.tsx
+++ packages/components/components/collapsible/CollapsibleHeaderIconButton.tsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+
+import CollapsibleContext from '@proton/components/components/collapsible/CollapsibleContext';
 import clsx from '@proton/utils/clsx';
 
 import CollapsibleHeaderButton, { CollapsibleHeaderButtonProps } from './CollapsibleHeaderButton';
@@ -10,11 +13,14 @@ export interface CollapsibleHeaderIconButtonProps extends Omit<CollapsibleHeader
  * Icon button which rotates the icon by 180° when collapsible is toggled.
  */
 const CollapsibleHeaderIconButton = ({ children, className, ...rest }: CollapsibleHeaderIconButtonProps) => {
+    const { disabled } = useContext(CollapsibleContext);
+
     return (
         <CollapsibleHeaderButton
             className={clsx('collapsible-header-icon-button', className)}
             shape="ghost"
             color="weak"
+            disabled={disabled}
             {...rest}
             icon
         >
