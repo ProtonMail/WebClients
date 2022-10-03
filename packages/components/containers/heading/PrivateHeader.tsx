d37bf471d7ba1baf41ec854a2b2988eaf7f09b57
Richard Tetaz <richard.tetaz@proton.ch>
2022-10-03 15:16:58 +0200

    Review

--- packages/components/containers/heading/PrivateHeader.tsx
+++ packages/components/containers/heading/PrivateHeader.tsx
@@ -4,7 +4,7 @@ import { c } from 'ttag';
 
 import { Vr } from '@proton/atoms';
 
-import { useBFCookie } from '../..';
+import { useNoBFCookie } from '../..';
 import { AppLink, Hamburger, Icon } from '../../components';
 import Header, { Props as HeaderProps } from '../../components/header/Header';
 import { TopNavbar, TopNavbarList, TopNavbarListItem, TopNavbarUpsell } from '../../components/topnavbar';
@@ -44,7 +44,7 @@ const PrivateHeader = ({
     onToggleExpand,
     title,
 }: Props) => {
-    useBFCookie();
+    useNoBFCookie();
 
     if (backUrl) {
         return (
