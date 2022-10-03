f9c642e076b47ad3f21cdce2ef218c5dbcbb55eb
Richard Tetaz <richard.tetaz@proton.ch>
2022-10-03 15:14:38 +0200

    Review

--- packages/components/hooks/useBFCookie.ts
+++ packages/components/hooks/useBFCookie.ts
@@ -1,23 +1,34 @@
 import { useEffect } from 'react';
 
+import { fromUnixTime, isAfter } from 'date-fns';
+
 import { getCookie, setCookie } from '@proton/shared/lib/helpers/cookies';
 import { getSecondLevelDomain } from '@proton/shared/lib/helpers/url';
 
+import useLastSubscriptionEnd from './useLastSubscriptionEnd';
 import { useUser } from './useUser';
 
-const COOKIE_NAME = 'is-paid-user';
-
+const COOKIE_NAME = 'no-bf-2022';
 const today = new Date();
+const FIRST_OCTOBER_2022 = new Date(2022, 9, 1);
 const lastDayOfTheYear = new Date(today.getFullYear(), 11, 31, 23, 59, 59);
 const cookieDomain = `.${getSecondLevelDomain(window.location.hostname)}`;
 
-const usePaidCookie = () => {
-    const [user] = useUser();
+const useBFCookie = () => {
+    const [user, loadingUser] = useUser();
+    const [lastSubscriptionEnd, loadingLastSubscriptionEnd] = useLastSubscriptionEnd();
+    const loading = loadingUser || loadingLastSubscriptionEnd;
 
     useEffect(() => {
-        const cookie = getCookie(COOKIE_NAME);
+        if (loading) {
+            return;
+        }
 
-        if (user?.isPaid && cookie !== '1') {
+        const cookie = getCookie(COOKIE_NAME);
+        const isPaid = user.isPaid;
+        const isInvalidFree = user.isFree && isAfter(fromUnixTime(lastSubscriptionEnd), FIRST_OCTOBER_2022);
+
+        if ((isPaid || isInvalidFree) && cookie !== '1') {
             setCookie({
                 cookieName: COOKIE_NAME,
                 cookieValue: '1',
@@ -25,7 +36,7 @@ const usePaidCookie = () => {
                 expirationDate: lastDayOfTheYear.toUTCString(),
                 path: '/',
             });
-        } else if (user?.isFree && cookie === '1') {
+        } else if (cookie === '1') {
             setCookie({
                 cookieName: COOKIE_NAME,
                 cookieValue: undefined,
@@ -34,7 +45,7 @@ const usePaidCookie = () => {
                 path: '/',
             });
         }
-    }, [user]);
+    }, [user, lastSubscriptionEnd, loading]);
 };
 
-export default usePaidCookie;
+export default useBFCookie;
