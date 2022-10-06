c4719cb6a313b8e09b58d2fe0de3f8410c035831
mmso <mmso@mmso.se>
2022-10-06 13:38:27 +0200

    Disable security key on the onion domain

--- packages/components/containers/password/getAuthTypes.ts
+++ packages/components/containers/password/getAuthTypes.ts
@@ -1,12 +1,13 @@
 import { InfoAuthedResponse } from '@proton/shared/lib/authentication/interface';
-import { APPS, APP_NAMES } from '@proton/shared/lib/constants';
+import { APP_NAMES } from '@proton/shared/lib/constants';
 import { UserSettings } from '@proton/shared/lib/interfaces';
 import { getHasFIDO2Enabled, getHasTOTPEnabled } from '@proton/shared/lib/settings/twoFactor';
+import { getHasFIDO2Support } from '@proton/shared/lib/webauthn/helper';
 
 export const getAuthTypes = (info: InfoAuthedResponse | undefined, userSettings: UserSettings, app: APP_NAMES) => {
     const Enabled = info?.['2FA']?.Enabled || userSettings?.['2FA']?.Enabled || 0;
     const hasTOTPEnabled = getHasTOTPEnabled(Enabled);
-    const hasFido2Enabled = getHasFIDO2Enabled(Enabled) && app === APPS.PROTONACCOUNT;
+    const hasFido2Enabled = getHasFIDO2Support(app, location.hostname) && getHasFIDO2Enabled(Enabled);
 
     return { totp: hasTOTPEnabled, fido2: hasFido2Enabled, twoFactor: hasFido2Enabled || hasTOTPEnabled };
 };
