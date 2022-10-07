81ad7c952480b2f83f51c6ec9ce1be1954d50e18
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2022-10-07 15:22:36 +0100

    Added adaptive button and button group, new positioning for action buttons

--- applications/mail/src/app/components/list/ItemColumnLayout.tsx
+++ applications/mail/src/app/components/list/ItemColumnLayout.tsx
@@ -108,86 +108,103 @@ const ItemColumnLayout = ({
             className="flex-item-fluid flex flex-nowrap flex-column flex-justify-center item-titlesender pr0-25"
             data-testid="message-list:message"
         >
-            <div className="flex flex-align-items-center item-firstline">
-                <div className="item-senders flex-item-fluid flex flex-nowrap pr1">
-                    <ItemUnread
-                        element={element}
-                        labelID={labelID}
-                        className={clsx('item-unread-dot flex-item-noshrink', isCompactView && 'mr0-25')}
-                        isSelected={isSelected}
-                    />
-                    <ItemAction element={element} className="mr0-25 myauto flex-item-noshrink" />
-                    <span
-                        className="inline-block max-w100 text-ellipsis"
-                        title={addresses}
-                        data-testid="message-column:sender-address"
-                    >
-                        {sendersContent}
-                    </span>
-                </div>
-
-                <span className="item-firstline-infos flex-item-noshrink flex flex-nowrap flex-align-items-center">
-                    <ItemDate element={element} labelID={labelID} className="item-senddate-col text-sm" useTooltip />
-                </span>
-            </div>
-
-            <div className="flex flex-nowrap flex-align-items-center item-secondline max-w100">
-                <div className="item-subject flex-item-fluid flex flex-nowrap flex-align-items-center">
-                    {showIcon && (
-                        <span className="flex flex-item-noshrink">
-                            <ItemLocation element={element} labelID={labelID} />
-                        </span>
-                    )}
-                    {conversationMode && <NumMessages className="mr0-25 flex-item-noshrink" conversation={element} />}
-                    <span
-                        role="heading"
-                        aria-level={2}
-                        className="inline-block max-w100 mr0-25 text-ellipsis"
-                        title={Subject}
-                        data-testid="message-column:subject"
-                    >
-                        {subjectContent}
-                    </span>
-                </div>
-
-                <div className="item-icons flex flex-item-noshrink flex-nowrap no-mobile">
-                    <ItemHoverButtons element={element} labelID={labelID} elementID={elementID} onBack={onBack} />
-                    <span className="flex item-meta-infos">
-                        {hasLabels && isCompactView && (
-                            <ItemLabels
-                                className="ml0-5"
-                                labels={labels}
+            <div className="flex flex-align-items-center flex-nowrap">
+                <div className="flex-item-fluid">
+                    <div className="flex flex-align-items-center item-firstline">
+                        <div className="item-senders flex-item-fluid flex flex-nowrap pr1">
+                            <ItemUnread
                                 element={element}
                                 labelID={labelID}
-                                maxNumber={1}
+                                className={clsx('item-unread-dot flex-item-noshrink', isCompactView && 'mr0-25')}
+                                isSelected={isSelected}
                             />
-                        )}
-                        {hasExpiration && (
-                            <ItemExpiration expirationTime={expirationTime} className="ml0-25 flex-align-self-center" />
-                        )}
-                        <ItemAttachmentIcon
-                            icon={hasOnlyIcsAttachments ? 'calendar-grid' : undefined}
-                            element={element}
-                            className="ml0-25 flex-align-self-center"
-                        />
-                        <span className="ml0-25 flex-flex-children flex-item-centered-vert hidden-empty">
-                            {isStarred && <ItemStar element={element} />}
+                            <ItemAction element={element} className="mr0-25 myauto flex-item-noshrink" />
+                            <span
+                                className="inline-block max-w100 text-ellipsis"
+                                title={addresses}
+                                data-testid="message-column:sender-address"
+                            >
+                                {sendersContent}
+                            </span>
+                        </div>
+
+                        <span className="item-firstline-infos flex-item-noshrink flex flex-nowrap flex-align-items-center">
+                            <ItemDate
+                                element={element}
+                                labelID={labelID}
+                                className="item-senddate-col text-sm"
+                                useTooltip
+                            />
                         </span>
-                    </span>
+                    </div>
+
+                    <div className="flex flex-nowrap flex-align-items-center item-secondline max-w100">
+                        <div className="item-subject flex-item-fluid flex flex-nowrap flex-align-items-center">
+                            {showIcon && (
+                                <span className="flex flex-item-noshrink">
+                                    <ItemLocation element={element} labelID={labelID} />
+                                </span>
+                            )}
+                            {conversationMode && (
+                                <NumMessages className="mr0-25 flex-item-noshrink" conversation={element} />
+                            )}
+                            <span
+                                role="heading"
+                                aria-level={2}
+                                className="inline-block max-w100 mr0-25 text-ellipsis"
+                                title={Subject}
+                                data-testid="message-column:subject"
+                            >
+                                {subjectContent}
+                            </span>
+                        </div>
+
+                        <div className="item-icons flex flex-item-noshrink flex-nowrap no-mobile">
+                            <span className="flex item-meta-infos">
+                                {hasLabels && isCompactView && (
+                                    <ItemLabels
+                                        className="ml0-5"
+                                        labels={labels}
+                                        element={element}
+                                        labelID={labelID}
+                                        maxNumber={1}
+                                    />
+                                )}
+                                {hasExpiration && (
+                                    <ItemExpiration
+                                        expirationTime={expirationTime}
+                                        className="ml0-25 flex-align-self-center"
+                                    />
+                                )}
+                                <ItemAttachmentIcon
+                                    icon={hasOnlyIcsAttachments ? 'calendar-grid' : undefined}
+                                    element={element}
+                                    className="ml0-25 flex-align-self-center"
+                                />
+                                <span className="ml0-25 flex-flex-children flex-item-centered-vert hidden-empty">
+                                    {isStarred && <ItemStar element={element} />}
+                                </span>
+                            </span>
+                        </div>
+                        <div className="item-icons flex flex-row flex-item-noshrink flex-nowrap no-desktop no-tablet on-mobile-flex">
+                            {hasExpiration && (
+                                <ItemExpiration
+                                    expirationTime={expirationTime}
+                                    className="ml0-25 flex-align-self-center"
+                                />
+                            )}
+                            <ItemAttachmentIcon
+                                icon={hasOnlyIcsAttachments ? 'calendar-grid' : undefined}
+                                element={element}
+                                className="ml0-25 flex-align-self-center"
+                            />
+                            <span className="ml0-25 flex-flex-children flex-item-centered-vert hidden-empty">
+                                <ItemStar element={element} />
+                            </span>
+                        </div>
+                    </div>
                 </div>
-                <div className="item-icons flex flex-row flex-item-noshrink flex-nowrap no-desktop no-tablet on-mobile-flex">
-                    {hasExpiration && (
-                        <ItemExpiration expirationTime={expirationTime} className="ml0-25 flex-align-self-center" />
-                    )}
-                    <ItemAttachmentIcon
-                        icon={hasOnlyIcsAttachments ? 'calendar-grid' : undefined}
-                        element={element}
-                        className="ml0-25 flex-align-self-center"
-                    />
-                    <span className="ml0-25 flex-flex-children flex-item-centered-vert hidden-empty">
-                        <ItemStar element={element} />
-                    </span>
-                </div>
+                <ItemHoverButtons element={element} labelID={labelID} elementID={elementID} onBack={onBack} />
             </div>
 
             {hasLabels && !isCompactView && (
