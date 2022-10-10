51d874d975af03a0b95b9d824bd9b1661c7ba300
romain sanchez <romain.sanchez@proton.ch>
2022-10-10 13:34:30 +0200

    Hide remote images by default if hideRemoteImages setting is undefined MAILWEB-3521

--- packages/shared/lib/mail/images.ts
+++ packages/shared/lib/mail/images.ts
@@ -4,5 +4,5 @@ import { MailSettings } from '@proton/shared/lib/interfaces';
 export const hasShowEmbedded = ({ HideEmbeddedImages = SHOW_IMAGES.SHOW }: Partial<MailSettings> = {}) =>
     HideEmbeddedImages === SHOW_IMAGES.SHOW;
 
-export const hasShowRemote = ({ HideRemoteImages = SHOW_IMAGES.SHOW }: Partial<MailSettings> = {}) =>
+export const hasShowRemote = ({ HideRemoteImages = SHOW_IMAGES.HIDE }: Partial<MailSettings> = {}) =>
     HideRemoteImages === SHOW_IMAGES.SHOW;
