559ff4affa45bf4ab9e8d3f3aa31ba7c12aa48f0
mmso <mmso@mmso.se>
2022-10-24 14:51:17 +0200

    Add new external key flags

--- packages/shared/lib/keys/keyFlags.ts
+++ packages/shared/lib/keys/keyFlags.ts
@@ -1,12 +1,24 @@
 import { Address } from '@proton/shared/lib/interfaces';
 
 import { ADDRESS_TYPE, KEY_FLAG } from '../constants';
-import { hasBit } from '../helpers/bitset';
+import { clearBit, hasBit, setBit } from '../helpers/bitset';
+
+export const setExternalFlags = (flags: number) => {
+    flags = setBit(flags, KEY_FLAG.NO_EMAIL_ENCRYPT);
+    flags = setBit(flags, KEY_FLAG.NO_EMAIL_SIGN);
+    return flags;
+};
+
+export const clearExternalFlags = (flags: number) => {
+    flags = clearBit(flags, KEY_FLAG.NO_EMAIL_ENCRYPT);
+    flags = clearBit(flags, KEY_FLAG.NO_EMAIL_SIGN);
+    return flags;
+};
 
 export const getDefaultKeyFlags = (address: Address | undefined) => {
     let flags = KEY_FLAG.FLAG_NOT_OBSOLETE + KEY_FLAG.FLAG_NOT_COMPROMISED;
     if (address?.Type === ADDRESS_TYPE.TYPE_EXTERNAL) {
-        flags += KEY_FLAG.FLAG_EXTERNAL;
+        flags = setExternalFlags(flags);
     }
     return flags;
 };
