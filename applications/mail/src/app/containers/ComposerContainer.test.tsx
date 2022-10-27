acfd444fd5fa2459ac501159ee75fd8c69e2208a
romain sanchez <romain.sanchez@proton.ch>
2022-10-27 14:18:36 +0200

    Add CC recipients in forwarded messages blockquotes MAILWEB-3675'

--- applications/mail/src/app/containers/ComposerContainer.test.tsx
+++ applications/mail/src/app/containers/ComposerContainer.test.tsx
@@ -3,6 +3,7 @@ import { act } from '@testing-library/react';
 import { getAppName } from '@proton/shared/lib/apps/helper';
 import { APPS, MIME_TYPES } from '@proton/shared/lib/constants';
 import { Recipient } from '@proton/shared/lib/interfaces';
+import { FORWARDED_MESSAGE, ORIGINAL_MESSAGE } from '@proton/shared/lib/mail/messages';
 
 import { MESSAGE_ACTIONS } from '../constants';
 import { formatFullDate } from '../helpers/date';
@@ -81,8 +82,87 @@ ${Signature}
 
 Sent with ${protonmailAppName} secure email.
 
-------- Original Message -------
+${ORIGINAL_MESSAGE}
 On ${formatFullDate(new Date(0))}, ${Sender.Name} <${Sender.Address}> wrote:
+
+
+> mail content
+> with a link -> https://protonmail.com/`);
+
+        // Wait for Address focus action
+        await tick();
+
+        // Unmount unless the container will listen the reset action
+        unmount();
+    });
+
+    it('should forward to a plaintext message with the right content', async () => {
+        let onCompose: OnCompose;
+
+        const content = `mail content
+with a link -> https://protonmail.com/`;
+
+        minimalCache();
+        addAddressToCache({ Email, Signature });
+
+        const me = { Name: 'me', Address: 'me@protonmail.com' };
+        const toRecipient = { Name: 'toRecipient', Address: 'toRecipient@protonmail.com' };
+        const ccRecipient = { Name: 'ccRecipient', Address: 'ccRecipient@protonmail.com' };
+        const ccRecipient2 = { Name: '', Address: 'ccRecipient2@protonmail.com' };
+        const messageSubject = 'Message subject';
+
+        const message = {
+            localID: ID,
+            data: {
+                ID,
+                MIMEType: 'text/plain' as MIME_TYPES,
+                Subject: messageSubject,
+                Sender,
+                ToList: [me, toRecipient] as Recipient[],
+                CCList: [ccRecipient, ccRecipient2] as Recipient[],
+            },
+            decryption: {
+                decryptedBody: content,
+            },
+            messageDocument: {
+                initialized: true,
+            },
+            ...(await preparePlainText(content, false)),
+        } as MessageState;
+
+        const Inside = () => {
+            onCompose = useOnCompose();
+            return null;
+        };
+
+        const { findByTestId, unmount } = await render(
+            <ComposerContainer breakpoints={{} as Breakpoints}>{() => <Inside />}</ComposerContainer>,
+            false
+        );
+
+        await act(async () => {
+            onCompose({
+                action: MESSAGE_ACTIONS.FORWARD,
+                referenceMessage: message,
+            });
+        });
+
+        const textarea = (await findByTestId('editor-textarea')) as HTMLTextAreaElement;
+
+        expect(textarea.value).toBe(`
+
+
+
+${Signature}
+
+Sent with ${protonmailAppName} secure email.
+
+${FORWARDED_MESSAGE}
+From: ${Sender.Name} <${Sender.Address}>
+Date: On ${formatFullDate(new Date(0))}
+Subject: ${messageSubject}
+To: ${me.Name} <${me.Address}>, ${toRecipient.Name} <${toRecipient.Address}>
+CC: ${ccRecipient.Name} <${ccRecipient.Address}>, ${ccRecipient2.Address} <${ccRecipient2.Address}>
 
 
 > mail content
