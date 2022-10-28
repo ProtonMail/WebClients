5018d3744a73654f1e1facf5ffae6eca6746aa7f
mmso <mmso@mmso.se>
2022-10-28 15:57:24 +0200

    Revoke discarded child sessions during fork consumption

--- packages/shared/lib/api/auth.ts
+++ packages/shared/lib/api/auth.ts
@@ -23,9 +23,10 @@ export const authJwt = (data: { Token: String; ClientSecret?: String }) => ({
     data,
 });
 
-export const revoke = () => ({
+export const revoke = (query?: { Child?: 1 }) => ({
     method: 'delete',
     url: 'auth',
+    query,
 });
 
 interface RefreshArgs {
