59bb93f83d509eb39e9c7e925bb40efec6cb3d6d
Jack Crompton <jack.crompton@proton.ch>
2022-10-31 13:38:17 +0000

    Workaround possible modal backdrop race condition
    CP-4415

--- packages/components/components/modalTwo/Backdrop.tsx
+++ packages/components/components/modalTwo/Backdrop.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import { isDialogOpen, isModalOpen } from '@proton/shared/lib/busy';
 
 import { classnames } from '../../helpers';
 
@@ -11,6 +13,29 @@ interface Props {
 
 const Backdrop = ({ entering, exiting }: Props) => {
     const [hidden, setHidden] = useState(true);
+
+    /**
+     * Fix for CP-4425
+     * There is a possible race condition which means that the backdrop remains visible
+     * after opening a link from LinkConfirmationModal
+     */
+    useEffect(() => {
+        const handleVisibilityChange = () => {
+            if (document.visibilityState === 'visible') {
+                /**
+                 * Remove all modals if no modals are open.
+                 * This removes the backdrop if it remains visible.
+                 */
+                if (!isDialogOpen() && !isModalOpen()) {
+                    setHidden(true);
+                }
+            }
+        };
+        document.addEventListener('visibilitychange', handleVisibilityChange);
+        return () => {
+            document.removeEventListener('visibilitychange', handleVisibilityChange);
+        };
+    }, []);
 
     const backdropClassName = classnames([
         'modal-two-backdrop',
