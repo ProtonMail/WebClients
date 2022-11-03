def216bad79cd7795ddab67e69bf884369568b4b
mmso <mmso@mmso.se>
2022-11-03 12:49:29 +0100

    Fix notification hijack in lite app

--- applications/account/src/lite/AppB.tsx
+++ applications/account/src/lite/AppB.tsx
@@ -14,11 +14,13 @@ import {
     ModalsProvider,
     NotificationsChildren,
     NotificationsHijack,
+    NotificationsProvider,
     PreventLeaveProvider,
     RightToLeftProvider,
     StandardErrorPage,
     ThemeProvider,
     getSessionTrackingEnabled,
+    useActiveBreakpoint,
 } from '@proton/components';
 import AuthenticationProvider from '@proton/components/containers/authentication/Provider';
 import authentication from '@proton/shared/lib/authentication/authentication';
@@ -26,11 +28,14 @@ import { APPS } from '@proton/shared/lib/constants';
 import createCache from '@proton/shared/lib/helpers/cache';
 import sentry from '@proton/shared/lib/helpers/sentry';
 import { initLocales } from '@proton/shared/lib/i18n/locales';
+import { getRedirect } from '@proton/shared/lib/subscription/redirect';
 import noop from '@proton/utils/noop';
 
 import * as config from '../app/config';
 import Setup from './Setup';
 import broadcast, { MessageType } from './broadcast';
+import MainContainer from './containers/MainContainer';
+import { FullscreenOption, SupportedActions, getApp, getFullscreenOption } from './containers/helper';
 
 initLocales(require.context('../../locales', true, /.json$/, 'lazy'));
 
@@ -46,6 +51,32 @@ const cache = createCache<string, any>();
 const App = () => {
     const [UID, setUID] = useState<string | undefined>(() => authentication.getUID());
     const [isLogout, setLogout] = useState(false);
+
+    const { isNarrow } = useActiveBreakpoint();
+
+    const searchParams = new URLSearchParams(window.location.search);
+    const action = searchParams.get('action') as SupportedActions | null;
+    const client = searchParams.get('client');
+
+    const defaultValues =
+        {
+            macOS: {
+                redirect: 'protonvpn://refresh',
+                fullscreen: 'off',
+            },
+        }[client || ''] || {};
+
+    const redirect = getRedirect(searchParams.get('redirect') || defaultValues.redirect || undefined);
+    const fullscreenOption = getFullscreenOption(
+        searchParams.get('fullscreen') || defaultValues.fullscreen || undefined
+    );
+    const app = getApp(searchParams.get('app'), redirect);
+    const fullscreen = (() => {
+        if (fullscreenOption === FullscreenOption.Auto) {
+            return isNarrow;
+        }
+        return fullscreenOption !== FullscreenOption.Off;
+    })();
 
     const handleLogin = (UID: string) => {
         authentication.setUID(UID);
@@ -95,26 +126,37 @@ const App = () => {
                         <ThemeProvider>
                             <Router>
                                 <PreventLeaveProvider>
-                                    <NotificationsHijack onCreate={handleNotificationCreate}>
-                                        <ModalsProvider>
-                                            <ApiProvider UID={UID} config={enhancedConfig} onLogout={handleLogout}>
-                                                <AuthenticationProvider store={authenticationValue}>
-                                                    <CacheProvider cache={cache}>
-                                                        <FeaturesProvider>
-                                                            <ExperimentsProvider>
-                                                                <NotificationsChildren />
-                                                                <ErrorBoundary component={<StandardErrorPage />}>
-                                                                    {isLogout ? null : (
-                                                                        <Setup UID={UID} onLogin={handleLogin} />
-                                                                    )}
-                                                                </ErrorBoundary>
-                                                            </ExperimentsProvider>
-                                                        </FeaturesProvider>
-                                                    </CacheProvider>
-                                                </AuthenticationProvider>
-                                            </ApiProvider>
-                                        </ModalsProvider>
-                                    </NotificationsHijack>
+                                    <NotificationsProvider>
+                                        <NotificationsHijack onCreate={redirect ? undefined : handleNotificationCreate}>
+                                            <ModalsProvider>
+                                                <ApiProvider UID={UID} config={enhancedConfig} onLogout={handleLogout}>
+                                                    <AuthenticationProvider store={authenticationValue}>
+                                                        <CacheProvider cache={cache}>
+                                                            <FeaturesProvider>
+                                                                <ExperimentsProvider>
+                                                                    <NotificationsChildren />
+                                                                    <ErrorBoundary component={<StandardErrorPage />}>
+                                                                        {isLogout ? null : (
+                                                                            <Setup UID={UID} onLogin={handleLogin}>
+                                                                                <MainContainer
+                                                                                    config={enhancedConfig}
+                                                                                    action={action}
+                                                                                    fullscreen={fullscreen}
+                                                                                    redirect={redirect}
+                                                                                    app={app}
+                                                                                    searchParams={searchParams}
+                                                                                />
+                                                                            </Setup>
+                                                                        )}
+                                                                    </ErrorBoundary>
+                                                                </ExperimentsProvider>
+                                                            </FeaturesProvider>
+                                                        </CacheProvider>
+                                                    </AuthenticationProvider>
+                                                </ApiProvider>
+                                            </ModalsProvider>
+                                        </NotificationsHijack>
+                                    </NotificationsProvider>
                                 </PreventLeaveProvider>
                             </Router>
                         </ThemeProvider>
