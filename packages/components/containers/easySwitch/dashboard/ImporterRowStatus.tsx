b57101c0594748bc861dd78aabf39732ef2c6188
Guillaume Gonzalez <guillaume.gonzalez@proton.ch>
2022-11-04 22:54:56 +0100

    Add new imports list with redux

---
+++ packages/components/containers/easySwitch/dashboard/ImporterRowStatus.tsx
@@ -0,0 +1,75 @@
+import { c } from 'ttag';
+
+import { Badge, Icon, Progress, Tooltip } from '@proton/components';
+import { MAIL_APP_NAME } from '@proton/shared/lib/constants';
+
+import { ApiImporterError, ApiImporterState } from '../logic/types/api.types';
+
+interface Props {
+    processed: number;
+    total: number;
+    state: ApiImporterState | undefined;
+    errorCode: ApiImporterError | undefined;
+}
+
+const ImporterRowStatus = ({ processed, total, state, errorCode }: Props) => {
+    const percentage = total === 0 ? 0 : (processed * 100) / total;
+    const percentageValue = Number.isNaN(percentage) ? 0 : Math.floor(percentage);
+
+    switch (state) {
+        case ApiImporterState.PAUSED:
+            return (
+                <>
+                    <Badge type="warning">
+                        {total === 0 ? c('Import status').t`Paused` : c('Import status').t`${percentageValue}% paused`}
+                    </Badge>
+
+                    {errorCode === ApiImporterError.ERROR_CODE_IMAP_CONNECTION && (
+                        <Tooltip title={c('Tooltip').t`Account is disconnected`}>
+                            <Icon name="exclamation-circle-filled" />
+                        </Tooltip>
+                    )}
+                    {errorCode === ApiImporterError.ERROR_CODE_QUOTA_LIMIT && (
+                        <Tooltip title={c('Tooltip').t`Your ${MAIL_APP_NAME} inbox is almost full`}>
+                            <Icon name="exclamation-circle-filled" />
+                        </Tooltip>
+                    )}
+                </>
+            );
+        case ApiImporterState.QUEUED:
+            return <Badge type="primary">{c('Import status').t`Started`}</Badge>;
+        case ApiImporterState.CANCELED:
+            return <Badge type="error">{c('Import status').t`Canceling`}</Badge>;
+        case ApiImporterState.DELAYED:
+            return (
+                <>
+                    <Badge type="warning">{c('Import status').t`Delayed`}</Badge>
+                    <Tooltip
+                        title={c('Tooltip')
+                            .t`Your external account may have reached its 24-hour bandwidth limit. Proton will try to resume the import as soon as possible.`}
+                    >
+                        <Icon name="exclamation-circle-filled" />
+                    </Tooltip>
+                </>
+            );
+        /*
+         * Default case is "in progress", if we have a total for the percentage
+         * we can show a progress bar, otherwise just show "in progress"
+         */
+        default:
+            return total !== 0 ? (
+                <span className="inline-flex flex-align-items-center w100">
+                    <Progress
+                        aria-labelledby="progressLabel"
+                        value={percentageValue}
+                        className="flex-item-fluid progress-bar--success"
+                    />
+                    <span id="progressLabel" className="ml0-5">{`${percentageValue}%`}</span>
+                </span>
+            ) : (
+                <Badge type="primary">{c('Import status').t`In progress`}</Badge>
+            );
+    }
+};
+
+export default ImporterRowStatus;
---
+++ packages/components/containers/easySwitch/dashboard/ReportRowStatus.tsx
@@ -0,0 +1,36 @@
+import { c } from 'ttag';
+
+import { Badge } from '@proton/components';
+
+import { ApiImporterState, ApiReportRollbackState } from '../logic/types/api.types';
+
+interface Props {
+    status: ApiImporterState;
+    rollbackState: ApiReportRollbackState | undefined;
+}
+
+const ReportRowStatus = ({ status, rollbackState }: Props) => {
+    if (rollbackState === ApiReportRollbackState.ROLLED_BACK || rollbackState === ApiReportRollbackState.ROLLING_BACK) {
+        switch (rollbackState) {
+            case ApiReportRollbackState.ROLLED_BACK:
+                return <Badge type="success" className="m0">{c('Import rollback status').t`Undo finished`}</Badge>;
+            case ApiReportRollbackState.ROLLING_BACK:
+                return <Badge type="warning" className="m0">{c('Import rollback status').t`Undo in progress`}</Badge>;
+        }
+    }
+
+    switch (status) {
+        case ApiImporterState.PAUSED:
+            return <Badge type="warning" className="m0">{c('Import status').t`Paused`}</Badge>;
+        case ApiImporterState.CANCELED:
+            return <Badge type="error" className="m0">{c('Import status').t`Canceled`}</Badge>;
+        case ApiImporterState.DONE:
+            return <Badge type="success" className="m0">{c('Import status').t`Completed`}</Badge>;
+        case ApiImporterState.FAILED:
+            return <Badge type="error" className="m0">{c('Import status').t`Failed`}</Badge>;
+    }
+
+    return null;
+};
+
+export default ReportRowStatus;
