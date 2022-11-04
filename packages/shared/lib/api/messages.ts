5382a8cd5c71d6e486d20a6456fe15f73eb72769
Eduardo Conde Pena <econdepe@proton.ch>
2022-11-04 11:55:14 +0100

    Do not use Limit and PageSize simultaneously when querying messages metadata
    Clients are meant to use one or the other, and the API behavior is unpredictable when passing both

--- packages/shared/lib/api/messages.ts
+++ packages/shared/lib/api/messages.ts
@@ -13,7 +13,7 @@ interface QueryMessageMetadataParams {
     Page?: number;
     PageSize?: number;
     Limit?: number;
-    LabelID?: string[];
+    LabelID?: string | string[];
     Sort?: string;
     Desc?: number;
     Begin?: number;
@@ -35,8 +35,8 @@ interface QueryMessageMetadataParams {
 export const queryMessageMetadata = ({
     Location,
     Page = 0,
-    PageSize = 50,
-    Limit = 50,
+    PageSize,
+    Limit,
     LabelID,
     Sort = 'Time',
     Desc = 1,
