6d4edd73cede77facd9f8f210cf994472d61d3de
mmso <mmso@mmso.se>
2022-11-08 15:31:37 +0100

    Use same sidebar settings in vpn as in account

--- packages/components/components/sidebar/index.tsx
+++ packages/components/components/sidebar/index.tsx
@@ -18,3 +18,5 @@ export { default as SidebarListItemLink } from './SidebarListItemLink';
 export { default as SidebarListItemButton } from './SidebarListItemButton';
 export { default as SidebarNav } from './SidebarNav';
 export { default as SidebarPrimaryButton } from './SidebarPrimaryButton';
+
+export { default as SettingsListItem } from './SettingsListItem';
