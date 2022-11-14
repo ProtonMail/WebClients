28e8bc440355a10ef2f5d35d5f8757995c6dc787
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2022-11-14 12:49:26 +0000

    New build your own section for Storybook
    UXE-167

--- applications/storybook/src/stories/components/Notification.stories.tsx
+++ applications/storybook/src/stories/components/Notification.stories.tsx
@@ -1,5 +1,14 @@
+import { SetStateAction, useState } from 'react';
+
 import { Button, CircleLoader } from '@proton/atoms';
-import { CreateNotificationOptions, NotificationButton, useNotifications } from '@proton/components';
+import {
+    Checkbox,
+    CreateNotificationOptions,
+    InputFieldTwo,
+    NotificationButton,
+    RadioGroup,
+    useNotifications,
+} from '@proton/components';
 
 import { getTitle } from '../../helpers/title';
 import mdx from './Notification.mdx';
@@ -37,8 +46,123 @@ export const Basic = () => {
         </>
     );
 
+    const types = ['info', 'warning'] as const;
+
+    const BuildYourOwn = () => {
+        const [byoExpiration, setByoExpiration] = useState(5000);
+        const [byoText, setByoText] = useState('Lorem ipsum');
+        const [byoType, setByoType] = useState(types[0]);
+        const [byoCloseButton, setCloseButton] = useState(true);
+
+        const [byoButtonShow, setByoButtonShow] = useState(false);
+        const [byoButtonText, setByoButtonText] = useState('Undo');
+
+        const [byoLoader, setByoLoader] = useState(false);
+
+        const ByoContent = () => {
+            return (
+                <>
+                    <span>{byoText}</span>
+                    {byoButtonShow && (
+                        <NotificationButton notificationType={byoType}>{byoButtonText}</NotificationButton>
+                    )}
+                    {byoLoader && <CircleLoader />}
+                </>
+            );
+        };
+
+        return (
+            <div className="block">
+                <form>
+                    <div className="flex flex-column flex-gap-1 mb1">
+                        <InputFieldTwo
+                            name="content"
+                            id="content"
+                            label="Content"
+                            value={byoText}
+                            onChange={(e: { target: { value: SetStateAction<string> } }) => setByoText(e.target.value)}
+                        />
+
+                        <div>
+                            <strong className="block mb1">Type</strong>
+                            <RadioGroup
+                                name="type"
+                                onChange={(v) => setByoType(v)}
+                                value={byoType}
+                                options={types.map((type) => ({ value: type, label: type }))}
+                            />
+                        </div>
+
+                        <Checkbox
+                            id="closebutton"
+                            checked={byoCloseButton}
+                            onChange={() => {
+                                setCloseButton(!byoCloseButton);
+                                setByoLoader(false);
+                            }}
+                        >
+                            Show Close Button
+                        </Checkbox>
+
+                        <Checkbox
+                            id="loader"
+                            checked={byoLoader}
+                            onChange={() => {
+                                setByoLoader(!byoLoader);
+                                setCloseButton(false);
+                            }}
+                        >
+                            Show Loader
+                        </Checkbox>
+
+                        <Checkbox
+                            id="byoButtonShow"
+                            checked={byoButtonShow}
+                            onChange={() => setByoButtonShow(!byoButtonShow)}
+                        >
+                            Show Button
+                        </Checkbox>
+                        <InputFieldTwo
+                            name="byoButtonText"
+                            id="byoButtonText"
+                            label="Button Text"
+                            className="w10e"
+                            value={byoButtonText}
+                            onChange={(e: { target: { value: SetStateAction<string> } }) =>
+                                setByoButtonText(e.target.value)
+                            }
+                        />
+
+                        <InputFieldTwo
+                            name="expiration"
+                            id="expiration"
+                            label="Expiration"
+                            className="w10e"
+                            value={byoExpiration}
+                            type="number"
+                            onChange={(e: { target: { value: number } }) => setByoExpiration(Number(e.target.value))}
+                        />
+                    </div>
+
+                    <Button
+                        onClick={handleClick({
+                            type: byoType,
+                            text: <ByoContent />,
+                            showCloseButton: byoCloseButton,
+                            expiration: byoExpiration,
+                        })}
+                        className="mr1 mb1"
+                    >
+                        Trigger
+                    </Button>
+                </form>
+            </div>
+        );
+    };
+
     return (
         <div>
+            <h4 className="mb1">Presets</h4>
             <Button
                 color="success"
                 onClick={handleClick({
@@ -113,6 +237,10 @@ export const Basic = () => {
             >
                 Warning without close button but with an action
             </Button>
+
+            <hr />
+            <h4 className="mb1">Build your own</h4>
+            <BuildYourOwn />
         </div>
     );
 };
