93278204baed116ef280e70133c6ae573defda8f
mmso <mmso@mmso.se>
2022-11-16 16:02:05 +0100

    Use ResizeObserver to calculate positions

--- packages/components/containers/notifications/Container.tsx
+++ packages/components/containers/notifications/Container.tsx
@@ -1,7 +1,37 @@
-import { Key, useLayoutEffect, useRef, useState } from 'react';
+import { Key, useEffect, useRef, useState } from 'react';
 
 import Notification from './Notification';
 import { NotificationOptions } from './interfaces';
+
+const notificationGap = 4;
+
+const getRects = (notifications: { [key: Key]: HTMLDivElement }) => {
+    return Object.entries(notifications).reduce<{ [key: Key]: DOMRect }>((acc, [key, el]) => {
+        if (!el) {
+            return acc;
+        }
+        acc[key] = el.getBoundingClientRect();
+        return acc;
+    }, {});
+};
+
+type Position = number;
+
+const getPositions = (notifications: NotificationOptions[], rects: ReturnType<typeof getRects>) => {
+    let top = 0;
+
+    return notifications.reduce<{ [key: Key]: Position }>((acc, notification) => {
+        acc[notification.key] = top;
+
+        const height = rects[notification.key]?.height;
+        if (height === undefined) {
+            return acc;
+        }
+        top += height + notificationGap;
+
+        return acc;
+    }, {});
+};
 
 interface Props {
     notifications: NotificationOptions[];
@@ -9,46 +39,59 @@ interface Props {
     hideNotification: (id: number) => void;
 }
 
-interface Position {
-    top: number;
-}
-
 const NotificationsContainer = ({ notifications, removeNotification, hideNotification }: Props) => {
     const containerRef = useRef<HTMLDivElement>(null);
     const notificationRefs = useRef<{ [key: Key]: HTMLDivElement }>({});
-    const [positions, setPositions] = useState<{ [key: Key]: Position }>({});
+    const [rects, setRects] = useState<{ [key: Key]: DOMRect }>({});
+    const resizeObserverRef = useRef<ResizeObserver | null>(null);
+    const callbackRefs = useRef<{ [key: Key]: (el: HTMLDivElement | null) => void }>({});
 
-    useLayoutEffect(() => {
-        const rects = Object.entries(notificationRefs.current).reduce<{ [key: Key]: DOMRect }>((acc, [key, el]) => {
-            if (!el) {
-                return acc;
+    useEffect(() => {
+        const observer = new ResizeObserver(() => {
+            setRects(getRects(notificationRefs.current));
+        });
+        resizeObserverRef.current = observer;
+        Object.values(notificationRefs.current).forEach((el) => {
+            observer.observe(el);
+        });
+        return () => {
+            observer.disconnect();
+            resizeObserverRef.current = null;
+        };
+    }, []);
+
+    useEffect(() => {
+        const notificationIds = Object.fromEntries(notifications.map((notification) => [notification.key, true]));
+        const callbackKeys = Object.keys(callbackRefs.current);
+        callbackKeys.forEach((callbackKey) => {
+            if (!notificationIds[callbackKey]) {
+                delete callbackRefs.current[callbackKey];
             }
-            acc[key] = el.getBoundingClientRect();
-            return acc;
-        }, {});
-        let top = 0;
-        const gap = 4;
-        setPositions(
-            notifications.reduce<{ [key: Key]: Position }>((acc, notification) => {
-                acc[notification.key] = { top };
-                top += rects[notification.key].height + gap;
-                return acc;
-            }, {})
-        );
+        });
     }, [notifications]);
 
+    const positions = getPositions(notifications, rects);
+
     const list = notifications.map(({ id, key, type, text, isClosing, showCloseButton, icon }) => {
+        if (!callbackRefs.current[key]) {
+            callbackRefs.current[key] = (el: HTMLDivElement | null) => {
+                if (el === null) {
+                    const oldEl = notificationRefs.current[key];
+                    if (oldEl) {
+                        resizeObserverRef.current?.unobserve(oldEl);
+                    }
+                    delete notificationRefs.current[key];
+                } else {
+                    resizeObserverRef.current?.observe(el);
+                    notificationRefs.current[key] = el;
+                }
+            };
+        }
         return (
             <Notification
                 key={key}
-                top={positions[key]?.top}
-                ref={(el: HTMLDivElement | null) => {
-                    if (el === null) {
-                        delete notificationRefs.current[key];
-                        return;
-                    }
-                    notificationRefs.current[key] = el;
-                }}
+                top={positions[key]}
+                ref={callbackRefs.current[key]}
                 isClosing={isClosing}
                 icon={icon}
                 type={type}
