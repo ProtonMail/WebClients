90a14adb15e1c7f0185781f7b21b8ceb328f58e5
mmso <mmso@mmso.se>
2022-11-18 13:07:25 +0100

    Optimize floating ui hook

--- packages/components/components/popper/interface.ts
+++ packages/components/components/popper/interface.ts
@@ -1,9 +1,9 @@
-import type { Placement as FloatingUiPlacement } from '@floating-ui/react-dom';
+import type { Placement as FloatingUiPlacement } from '@floating-ui/dom';
 
 export type ArrowOffset = 0 | string;
 
 export type PopperPlacement = FloatingUiPlacement;
 
-export type PopperPosition = { top: number; left: number; };
+export type PopperPosition = { top: number; left: number };
 
 export type PopperArrow = { '--arrow-offset': ArrowOffset };
--- packages/components/components/popper/usePopper.ts
+++ packages/components/components/popper/usePopper.ts
@@ -1,6 +1,18 @@
-import { RefObject, useLayoutEffect, useMemo } from 'react';
+import { RefObject, useCallback, useLayoutEffect, useMemo, useRef, useState } from 'react';
 
-import { UseFloatingReturn, autoUpdate, flip, hide, offset, shift, useFloating } from '@floating-ui/react-dom';
+import {
+    Placement,
+    Strategy,
+    VirtualElement,
+    autoUpdate,
+    computePosition,
+    flip,
+    hide,
+    offset,
+    shift,
+} from '@floating-ui/dom';
+
+import isDeepEqual from '@proton/shared/lib/helpers/isDeepEqual';
 
 import { PopperArrow, PopperPlacement, PopperPosition } from './interface';
 import {
@@ -12,14 +24,24 @@ import {
     rtlPlacement,
 } from './utils';
 
+type ReferenceType = Element | VirtualElement;
+
 const hiddenPosition: PopperPosition = {
     top: -9999,
     left: -9999,
 };
 
+interface Data {
+    x: number | null;
+    y: number | null;
+    strategy: Strategy;
+    placement: Placement;
+    middlewareData: any;
+}
+
 interface PopperReturnValue {
-    floating: UseFloatingReturn['floating'] | null;
-    reference: UseFloatingReturn['reference'] | null;
+    reference: ((el: ReferenceType | null) => void) | null;
+    floating: ((el: HTMLElement | null) => void) | null;
     position: PopperPosition;
     arrow: PopperArrow;
     placement: PopperPlacement | 'hidden';
@@ -44,6 +66,14 @@ interface Props {
     updateAnimationFrame?: boolean;
 }
 
+const defaultState: Data = {
+    x: null,
+    y: null,
+    strategy: 'fixed',
+    placement: 'top',
+    middlewareData: {},
+};
+
 const usePopper = ({
     isOpen = false,
     originalPlacement = 'top',
@@ -53,28 +83,75 @@ const usePopper = ({
     reference: anchorReference,
     updateAnimationFrame = false,
 }: Props): PopperReturnValue => {
+    const [data, setData] = useState<Data>(defaultState);
+
+    const reference = useRef<ReferenceType | null>(null);
+    const floating = useRef<HTMLElement | null>(null);
+    const cleanupRef = useRef<(() => void) | void | null>(null);
+    const isMountedRef = useRef(false);
+
     const fallbackPlacements = useMemo(() => {
         return getFallbackPlacements(originalPlacement, availablePlacements);
     }, []);
-    const { reference, floating, x, y, middlewareData, placement } = useFloating({
-        strategy: 'fixed',
-        placement: originalPlacement,
-        middleware: [
-            anchorOffset(relativeReference),
-            offset(offsetPx),
-            flip({ fallbackPlacements }),
-            shift(),
-            hide(),
-            arrowOffset(),
-            rtlPlacement(),
-        ],
-        whileElementsMounted: (reference, floating, update) => {
-            const unsubscribe = autoUpdate(reference, floating, update, {
+
+    const update = useCallback(() => {
+        const referenceEl = reference.current;
+        const floatingEl = floating.current;
+        if (!referenceEl || !floatingEl) {
+            return;
+        }
+
+        // NOTE: This hook assumes that props never change during the lifetime of the component.
+        computePosition(referenceEl, floatingEl, {
+            placement: originalPlacement,
+            middleware: [
+                anchorOffset(relativeReference),
+                offset(offsetPx),
+                flip({ fallbackPlacements }),
+                shift(),
+                hide(),
+                arrowOffset(),
+                rtlPlacement(),
+            ],
+            strategy: 'fixed',
+        }).then((data) => {
+            if (!isMountedRef.current) {
+                return;
+            }
+            setData((oldData) => {
+                if (isDeepEqual(oldData, data)) {
+                    return oldData;
+                }
+                return data;
+            });
+        });
+    }, []);
+
+    useLayoutEffect(() => {
+        isMountedRef.current = true;
+        return () => {
+            isMountedRef.current = false;
+        };
+    }, []);
+
+    const runElementMountCallback = useCallback(() => {
+        cleanupRef.current?.();
+        cleanupRef.current = null;
+
+        const referenceEl = reference.current;
+        const floatingEl = floating.current;
+
+        if (!referenceEl || !floatingEl) {
+            return;
+        }
+
+        const autoUpdater = () => {
+            const unsubscribe = autoUpdate(referenceEl, floatingEl, update, {
                 animationFrame: updateAnimationFrame,
             });
             let relativeUnsubscribe: (() => void) | undefined;
             if (relativeReference?.current) {
-                relativeUnsubscribe = autoUpdate(relativeReference.current, floating, update, {
+                relativeUnsubscribe = autoUpdate(relativeReference.current, floatingEl, update, {
                     animationFrame: updateAnimationFrame,
                 });
             }
@@ -82,56 +159,62 @@ const usePopper = ({
                 unsubscribe();
                 relativeUnsubscribe?.();
             };
-        },
-    });
+        };
+
+        cleanupRef.current = autoUpdater();
+    }, []);
+
+    const setReference = useCallback((node: ReferenceType | null) => {
+        reference.current = node;
+        runElementMountCallback();
+    }, []);
+
+    const setFloating = useCallback((node: HTMLElement | null) => {
+        floating.current = node;
+        runElementMountCallback();
+    }, []);
 
     useLayoutEffect(() => {
         if (!isOpen) {
-            reference(null);
-            floating(null);
+            setReference(null);
+            setFloating(null);
+            setData(defaultState);
         }
-    }, [isOpen]);
-
-    useLayoutEffect(() => {
         if (!anchorReference) {
             return;
         }
-        if (!isOpen) {
-            reference(null);
-            return;
-        }
         if (anchorReference.mode === 'element') {
-            reference(anchorReference.value || null);
+            setReference(anchorReference.value || null);
             return;
         }
         if (anchorReference.mode === 'position') {
             if (!anchorReference.value) {
-                reference(null);
+                setReference(null);
                 return;
             }
             const clickRect = getClickRect(anchorReference.value);
-            reference({
+            setReference({
                 getBoundingClientRect: () => {
                     return clickRect;
                 },
             });
             return;
         }
-    }, [reference, anchorReference?.value, anchorReference?.mode, isOpen]);
+    }, [anchorReference?.value, anchorReference?.mode, isOpen]);
 
     // x and y are null initially, before the layout effect has fired
-    const hidden = Boolean(middlewareData.hide?.referenceHidden) || x === null || y === null;
-    const arrowOffsetValue: string | number = middlewareData.arrowOffset?.value;
-    const adjustedPlacement: PopperPlacement = middlewareData.rtlPlacement?.placement || placement;
+    const hidden = Boolean(data.middlewareData.hide?.referenceHidden) || data.x === null || data.y === null;
+    const arrowOffsetValue: string | number = data.middlewareData.arrowOffset?.value;
+    const adjustedPlacement: PopperPlacement = data.middlewareData.rtlPlacement?.placement || data.placement;
 
     return {
-        reference: isOpen ? reference : null,
-        floating: isOpen ? floating : null,
+        reference: isOpen ? setReference : null,
+        floating: isOpen ? setFloating : null,
         position: hidden
             ? hiddenPosition
             : {
-                  top: y || 0,
-                  left: x || 0,
+                  top: data.y || 0,
+                  left: data.x || 0,
               },
         arrow: {
             '--arrow-offset': !arrowOffsetValue ? 0 : `${arrowOffsetValue}px`,
