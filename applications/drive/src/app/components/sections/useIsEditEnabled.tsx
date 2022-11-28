ce783bf504a7fd71f6e2ea857ef3e37e987ce041
Michal Horejsek <michal.horejsek@proton.ch>
2022-11-28 10:49:45 +0100

    Allow edits only internally

--- applications/drive/src/app/components/sections/useIsEditEnabled.tsx
+++ applications/drive/src/app/components/sections/useIsEditEnabled.tsx
@@ -2,5 +2,5 @@ import { useEarlyAccess } from '@proton/components';
 
 export default function useIsEditEnabled() {
     const { currentEnvironment } = useEarlyAccess();
-    return currentEnvironment === 'alpha' || currentEnvironment === 'beta';
+    return currentEnvironment === 'alpha';
 }
