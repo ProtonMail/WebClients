5513d6b3f5208e594868dbc6400356b200731f3a
Oleg Voloshin <oleg.voloshin@proton.ch>
2022-11-28 15:32:47 +0200

    Add a placeholder for empty device root
    DRVWEB-2607

--- applications/drive/src/app/components/sections/ToolbarButtons/index.ts
+++ applications/drive/src/app/components/sections/ToolbarButtons/index.ts
@@ -3,5 +3,5 @@ export { default as DownloadButton } from './DownloadButton';
 export { default as LayoutButton } from './LayoutButton';
 export { default as PreviewButton } from './PreviewButton';
 export { default as RenameButton } from './RenameButton';
-export { default as ShareFileButton } from './ShareFileButton';
+export { default as ShareButton } from './ShareButton';
 export { default as ShareLinkButton } from './ShareLinkButton';
