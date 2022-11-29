6252e71b92e1b5b2bfc2955645a39510a446c8ef
Redacted <redacted@invalid.proton.ch>
2022-11-29 19:37:46 +0100

    Setup signup workflow for VPN Browser Extension

--- applications/account/src/app/public/helper.ts
+++ applications/account/src/app/public/helper.ts
@@ -1,4 +1,4 @@
-import { APPS, APPS_CONFIGURATION, APP_NAMES } from '@proton/shared/lib/constants';
+import { APPS, APPS_CONFIGURATION, APP_NAMES, CLIENT_TYPES } from '@proton/shared/lib/constants';
 
 export const getToAppName = (toApp?: APP_NAMES) => {
     if (!toApp || toApp === APPS.PROTONACCOUNT) {
@@ -17,10 +17,16 @@ export const requiresNonDelinquent: APP_NAMES[] = [
     APPS.PROTONDRIVE,
 ];
 
-export const externalApps = [APPS.PROTONVPN_SETTINGS, APPS.PROTONDRIVE];
+export const vpnApps = [APPS.PROTONVPN_SETTINGS, APPS.PROTONVPNBROWSEREXTENSION];
+
+export const externalApps = [...vpnApps, APPS.PROTONDRIVE, APPS.PROTONEXTENSION];
 
 export const getHasAppExternalSignup = (toApp?: APP_NAMES) => {
     return externalApps.includes(toApp as any);
+};
+
+export const getIsVPNApp = (toApp?: APP_NAMES, clientType?: CLIENT_TYPES) => {
+    return vpnApps.includes(toApp as any) || clientType === CLIENT_TYPES.VPN;
 };
 
 export const defaultPersistentKey = 'default-persistent';
