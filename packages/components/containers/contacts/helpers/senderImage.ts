59c837dd9e362688945abbd85337509bfa67aaad
Richard <richard@protonmail.com>
2022-11-30 11:18:19 +0100

    Load sender image with direct url (uid as param)

--- packages/components/containers/contacts/helpers/senderImage.ts
+++ packages/components/containers/contacts/helpers/senderImage.ts
@@ -1,4 +1,5 @@
 import { SenderImageMode, getLogo } from '@proton/shared/lib/api/images';
+import { createUrl } from '@proton/shared/lib/fetch/helpers';
 import { Api } from '@proton/shared/lib/interfaces';
 
 const CACHE = {} as { [domain: string]: Promise<string> };
@@ -35,4 +36,28 @@ export const getSenderLogo = (
     }
 
     return CACHE[emailAddress];
+};
+
+export const getImageSize = () => {
+    if (window.devicePixelRatio >= 4) {
+        return 128;
+    }
+
+    if (window.devicePixelRatio > 1) {
+        return 64;
+    }
+
+    return 32;
+};
+
+export const getSenderImageUrl = (
+    UID: string,
+    emailAddress: string,
+    size?: number,
+    bimiSelector?: string,
+    mode?: SenderImageMode
+) => {
+    const config = getLogo(emailAddress, size, bimiSelector, mode, UID);
+    const url = createUrl(config.url, config.params);
+    return url.toString();
 };
