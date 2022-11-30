f183063bd019685e28b5be6bcf0e11d92c660c9e
mmso <mmso@mmso.se>
2022-11-30 11:03:32 +0100

    Disable size cache in useElementRect
    Move this logic to the specific use case in calendar with the aspect
    ratio problem.

--- packages/components/hooks/useElementRect.ts
+++ packages/components/hooks/useElementRect.ts
@@ -1,7 +1,6 @@
 import { RefObject, useLayoutEffect, useState } from 'react';
 
-import useInstance from '@proton/hooks/useInstance';
-import debounce from '@proton/utils/debounce';
+import throttle from '@proton/utils/throttle';
 
 // Can't loop over DOMRect keys with getOwnPropertyNames.
 const keys = ['bottom', 'height', 'left', 'right', 'top', 'width', 'x', 'y'];
@@ -20,69 +19,66 @@ const isEquivalent = (aRect?: DOMRect, bRect?: DOMRect) => {
     return true;
 };
 
-const getElementRect = (target?: HTMLElement | null) => {
+function getElementRect(target: HTMLElement): DOMRect;
+function getElementRect(target?: HTMLElement | null) {
     if (!target) {
         return;
     }
     return target.getBoundingClientRect();
-};
+}
 
-type RateLimiter = <A extends any[]>(func: (...args: A) => void, wait: number) => (...args: A) => void;
+type RateLimiter = <A extends any[]>(func: (...args: A) => void) => (...args: A) => void;
 
 export const equivalentReducer = (oldRect?: DOMRect, newRect?: DOMRect) => {
     return isEquivalent(oldRect, newRect) ? oldRect : newRect;
 };
-export const defaultReducer = (
-    oldRect: DOMRect | undefined,
-    newRect: DOMRect | undefined,
-    sizeCache: (DOMRect | undefined)[]
-) => {
-    const [prevOld, prevNew] = sizeCache;
-    // If it's flipping back and forth, settle on the previous value to prevent jiggling effects
-    if (isEquivalent(prevOld, newRect) && isEquivalent(prevNew, oldRect)) {
-        return oldRect;
-    }
-    sizeCache[0] = oldRect;
-    sizeCache[1] = newRect;
-    return equivalentReducer(oldRect, newRect);
-};
-type Reducer = typeof defaultReducer;
 
-const useElementRect = <E extends HTMLElement>(
-    ref: RefObject<E>,
-    rateLimiter: RateLimiter = debounce,
-    reducer: Reducer = defaultReducer
+export const createObserver = (
+    target: HTMLElement,
+    onResize: (rect: DOMRect) => void,
+    maybeRateLimiter?: RateLimiter | null
 ) => {
-    const sizeCache = useInstance((): (DOMRect | undefined)[] => []);
-    const [elementRect, setElementRect] = useState(() => getElementRect(ref.current));
+    if (maybeRateLimiter === undefined) {
+        maybeRateLimiter = (cb) => throttle(cb, 16, { leading: true, trailing: true });
+    }
+
+    let cache = {} as DOMRect;
+    const handleResizeCallback = (rect: DOMRect) => {
+        if (isEquivalent(cache, rect)) {
+            return;
+        }
+        cache = rect;
+        onResize(rect);
+    };
+
+    const handleResize = maybeRateLimiter ? maybeRateLimiter(handleResizeCallback) : handleResizeCallback;
+
+    const handleResizeObserver = () => {
+        handleResize(getElementRect(target));
+    };
+    const resizeObserver = new ResizeObserver(handleResizeObserver);
+    resizeObserver.observe(target, { box: 'border-box' });
+    handleResizeObserver();
+    return () => {
+        resizeObserver.disconnect();
+    };
+};
+
+const useElementRect = <E extends HTMLElement>(ref: RefObject<E>, maybeRateLimiter?: RateLimiter | null) => {
+    const [elementRect, setElementRect] = useState<DOMRect | null>(null);
 
     useLayoutEffect(() => {
         const target = ref.current;
         if (!target) {
             return;
         }
-
-        if (typeof ResizeObserver === 'function') {
-            const resizeObserver = new ResizeObserver(
-                rateLimiter(() => {
-                    setElementRect((old) => reducer(old, getElementRect(target), sizeCache));
-                }, 100)
-            );
-            resizeObserver.observe(target, { box: 'border-box' });
-            setElementRect((old) => reducer(old, getElementRect(target), sizeCache));
-            return () => {
-                resizeObserver.disconnect();
-            };
-        }
-
-        const onResize = rateLimiter(() => {
-            setElementRect((old) => reducer(old, getElementRect(target), sizeCache));
-        }, 100);
-        window.addEventListener('resize', onResize);
-        setElementRect((old) => reducer(old, getElementRect(target), sizeCache));
-        return () => {
-            window.removeEventListener('resize', onResize);
-        };
+        return createObserver(
+            target,
+            (rect: DOMRect) => {
+                setElementRect(rect);
+            },
+            maybeRateLimiter
+        );
     }, [ref.current]);
 
     return elementRect;
