76af2cda8ac62c32a5b4a764f08e49cabdd74457
romain sanchez <romain.sanchez@proton.ch>
2022-12-06 11:17:08 +0100

    Use navigator to copy email to clipboard CALWEB-4079

--- packages/shared/lib/helpers/browser.ts
+++ packages/shared/lib/helpers/browser.ts
@@ -12,12 +12,16 @@ export const isFileSaverSupported = () => !!new Blob();
 
 export const textToClipboard = (text = '', target = document.body) => {
     const oldActiveElement = document.activeElement as HTMLElement;
-    const dummy = document.createElement('textarea');
-    target.appendChild(dummy);
-    dummy.value = text;
-    dummy.select();
-    document.execCommand('copy');
-    target.removeChild(dummy);
+    if (navigator.clipboard) {
+        void navigator.clipboard.writeText(text);
+    } else {
+        const dummy = document.createElement('textarea');
+        target.appendChild(dummy);
+        dummy.value = text;
+        dummy.select();
+        document.execCommand('copy');
+        target.removeChild(dummy);
+    }
     oldActiveElement?.focus?.();
 };
 
