6e4224314d830019ab62f1d969db4b99585e09cd
Oleg Voloshin <oleg.voloshin@proton.ch>
2022-12-08 14:23:41 +0200

    Fix read-only link placeholder
    DRVWEB-2695

--- applications/drive/src/app/store/_views/utils/useShareType.ts
+++ applications/drive/src/app/store/_views/utils/useShareType.ts
@@ -4,17 +4,27 @@ import { ShareType } from '../..';
 import { sendErrorReport } from '../../../utils/errorHandling';
 import { useShare } from '../../_shares';
 
+/**
+ * Returns following values:
+ * * `ShareType` - type of the share
+ * * `undefined` – loading/not set yet
+ * * `null` - failed to identify shareType
+ */
 export const useShareType = (shareId: string) => {
-    const [shareType, setShareType] = useState<ShareType>();
+    const [shareType, setShareType] = useState<ShareType | undefined | null>(undefined);
     const { getShare } = useShare();
 
     useEffect(() => {
+        setShareType(undefined);
         const ac = new AbortController();
         getShare(ac.signal, shareId)
             .then((share) => {
                 setShareType(share.type);
             })
-            .catch(sendErrorReport);
+            .catch((e) => {
+                sendErrorReport(e);
+                setShareType(null);
+            });
     }, [shareId]);
 
     return shareType;
