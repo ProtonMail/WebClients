3525a82f606da1081d65d0e49f6c3694b2400e8e
Oleg Voloshin <oleg.voloshin@proton.ch>
2022-12-09 14:23:47 +0200

    Update empty devices link URL

--- applications/drive/src/app/components/sections/Devices/EmptyDevices.tsx
+++ applications/drive/src/app/components/sections/Devices/EmptyDevices.tsx
@@ -1,14 +1,14 @@
 import { c } from 'ttag';
 
 import { EmptyViewContainer } from '@proton/components';
+import { DRIVE_APP_NAME } from '@proton/shared/lib/constants';
 import noContentSvg from '@proton/styles/assets/img/illustrations/empty-devices.svg';
 
 import './EmptyDevices.scss';
-import { DRIVE_APP_NAME } from '@proton/shared/lib/constants';
 
 const EmptyDevices = () => {
     const LearnMore = (
-        <a href={''} target="_blank" key="learn-more-link">
+        <a href={'https://drive.proton.me/urls/3SF8FZV8B0#grWxkmu9q4NP'} target="_blank" key="learn-more-link">
             {c('Action').t`Learn more`}
         </a>
     );
@@ -18,7 +18,7 @@ const EmptyDevices = () => {
             <h3 className="text-bold">{c('Info').t`No synced computers`}</h3>
             <p className="empty-devices-placeholder-info">
                 {c('Info')
-                    .jt`Use Drive for Desktop to sync folders on your computer with ${ DRIVE_APP_NAME }. They’ll show up here and be accessible from anywhere. ${ LearnMore }`}
+                    .jt`Use Drive for Desktop to sync folders on your computer with ${DRIVE_APP_NAME}. They’ll show up here and be accessible from anywhere. ${LearnMore}`}
             </p>
         </EmptyViewContainer>
     );
