25fb6f471f0f8d91d354145e889518d71ec7d250
Alexey Karpov <alexey.karpov@proton.ch>
2022-12-12 13:33:36 +0100

    Add new impementation for readableTime()

--- packages/shared/lib/helpers/time.ts
+++ packages/shared/lib/helpers/time.ts
@@ -2,20 +2,45 @@ import { format as formatDate, fromUnixTime, getUnixTime, isSameDay } from 'date
 
 import { serverTime } from '@proton/crypto';
 
-type Options = Parameters<typeof formatDate>[2];
+export type Options = Parameters<typeof formatDate>[2] & {
+    sameDayFormat?: string;
+    differentDayFormat?: string;
+};
+
 /**
  * Convert UNIX timestamp into a readable time. The format of the readable time is:
  * ** Hours and minutes if the UNIX timestamp is from the same day,
  * ** Day, month and year otherwise
  */
-export const readableTime = (unixTime: number, format = 'PP', options?: Options, forceFormat = false) => {
+export const readableTime = (unixTime: number, options?: Options) => {
+    const sameDayFormat = options?.sameDayFormat ?? 'p';
+    const differentDayFormat = options?.differentDayFormat ?? 'PP';
+
     const date = fromUnixTime(unixTime);
 
-    if (isSameDay(date, Date.now()) && !forceFormat) {
-        return formatDate(date, 'p', options);
+    if (isSameDay(date, Date.now())) {
+        return formatDate(date, sameDayFormat, options);
     }
 
-    return formatDate(date, format, options);
+    return formatDate(date, differentDayFormat, options);
+};
+
+/**
+ * Convert UNIX timestamp into a readable time. The format of the readable time is:
+ * ** Hours and minutes if the UNIX timestamp is from the same day,
+ * ** Day, month and year otherwise
+ */
+export const readableTimeLegacy = (unixTime: number, format = 'PP', options?: Options, forceFormat = false) => {
+    const localOptions = {
+        ...options,
+        differentDayFormat: format,
+    };
+
+    if (forceFormat) {
+        localOptions.sameDayFormat = localOptions.differentDayFormat;
+    }
+
+    return readableTime(unixTime, localOptions);
 };
 
 export const getCurrentUnixTimestamp = () => {
