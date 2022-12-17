6235ae4f281090d754a90053a63236016f58d1a5
Richard Tetaz <richard.tetaz@proton.ch>
2022-12-17 20:47:52 +0100

    Update message view from fetch messages
    [MAILWEB-3822]

--- applications/mail/src/app/logic/messages/helpers/messagesReducer.ts
+++ applications/mail/src/app/logic/messages/helpers/messagesReducer.ts
@@ -1,7 +1,10 @@
+import { PayloadAction } from '@reduxjs/toolkit';
 import { Draft } from 'immer';
 
 import { Message } from '@proton/shared/lib/interfaces/mail/Message';
 
+import { isMessage } from '../../../helpers/elements';
+import { QueryParams, QueryResults, TaskRunningInfo } from '../../elements/elementsTypes';
 import { RootState } from '../../store';
 import { localID as localIDSelector, messageByID } from '../messagesSelectors';
 import { MessagesState } from '../messagesTypes';
@@ -23,3 +26,34 @@ export const getLocalID = (state: Draft<MessagesState>, ID: string) =>
 
 export const getMessage = (state: Draft<MessagesState>, ID: string) =>
     messageByID({ messages: state } as RootState, { ID });
+
+export const updateFromElements = (
+    state: Draft<MessagesState>,
+    action: PayloadAction<
+        {
+            result: QueryResults;
+            taskRunning: TaskRunningInfo;
+        },
+        string,
+        {
+            arg: QueryParams;
+            requestId: string;
+            requestStatus: 'fulfilled';
+        },
+        never
+    >
+) => {
+    const { Elements } = action.payload.result;
+
+    if (Elements && Elements.length) {
+        Elements.forEach((element) => {
+            if (isMessage(element)) {
+                const messageState = getMessage(state, element.ID);
+
+                if (messageState) {
+                    messageState.data = element as Message;
+                }
+            }
+        });
+    }
+};
