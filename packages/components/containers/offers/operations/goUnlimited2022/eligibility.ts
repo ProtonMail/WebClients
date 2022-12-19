135f72e485e61af88cb95acc2532c3d0ffa0edb3
Richard Tetaz <richard.tetaz@proton.ch>
2022-12-19 13:18:14 +0100

    Remove time condition for testing

--- packages/components/containers/offers/operations/goUnlimited2022/eligibility.ts
+++ packages/components/containers/offers/operations/goUnlimited2022/eligibility.ts
@@ -1,5 +1,4 @@
-import { addDays, fromUnixTime, isBefore } from 'date-fns';
-
+// import { addDays, fromUnixTime, isBefore } from 'date-fns';
 import { APPS, PLANS } from '@proton/shared/lib/constants';
 import { getPlan, isExternal, isTrial } from '@proton/shared/lib/helpers/subscription';
 import { ProtonConfig, Subscription, UserModel } from '@proton/shared/lib/interfaces';
@@ -12,12 +11,12 @@ interface Props {
 
 const isEligible = ({ user, subscription, protonConfig }: Props) => {
     const isValidApp = protonConfig?.APP_NAME === APPS.PROTONMAIL || protonConfig?.APP_NAME === APPS.PROTONACCOUNT;
-    const createDate = fromUnixTime(subscription?.CreateTime || subscription?.PeriodStart || 0); // Subscription.CreateTime is not yet available
+    // const createDate = fromUnixTime(subscription?.CreateTime || subscription?.PeriodStart || 0); // Subscription.CreateTime is not yet available
     const plan = getPlan(subscription);
     return (
         [PLANS.MAIL, PLANS.VPN].includes(plan?.Name as PLANS) &&
         !isTrial(subscription) &&
-        isBefore(createDate, addDays(new Date(), -7)) &&
+        // isBefore(createDate, addDays(new Date(), -7)) &&
         user.canPay &&
         isValidApp &&
         !user.isDelinquent &&
