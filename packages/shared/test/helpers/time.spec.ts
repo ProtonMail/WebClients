25113ae6171da2552dc4bb055d440a6422883c43
Alexey Karpov <alexey.karpov@proton.ch>
2022-12-20 16:10:08 +0100

    Support setting sameDayFormat to false

--- packages/shared/test/helpers/time.spec.ts
+++ packages/shared/test/helpers/time.spec.ts
@@ -1,7 +1,7 @@
 import { readableTime } from '@proton/shared/lib/helpers/time';
 import { enUSLocale } from '@proton/shared/lib/i18n/dateFnLocales';
 
-describe('readableTime()', () => {
+fdescribe('readableTime()', () => {
     const unixTime = 1666438769; // Sat Oct 22 2022 13:39:29 GMT+0200 (Central European Summer Time)
 
     let originalDateNow: any;
@@ -52,5 +52,16 @@ describe('readableTime()', () => {
                 format: 'p',
             })
         ).toMatch(/AM|PM/); // esentially checking that it's X:XX AM or X:XX PM. By using regex, I bypass the problem of the timezones
+    });
+
+    it('should use format parameter if sameDayFormat is false', () => {
+        setMockedTime(unixTime * 1000);
+
+        let result = readableTime(unixTime, {
+            locale: enUSLocale,
+            sameDayFormat: false,
+        });
+
+        expect(result).toEqual('Oct 22, 2022');
     });
 });
