6e16fb9b685c7e6a8ce6de0685b4c869b4721b74
Jack Crompton <jack.crompton@proton.ch>
2022-12-20 11:28:56 +0000

    Remove Scroll test

--- packages/atoms/Scroll/Scroll.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-
-import Scroll from './Scroll';
-
-describe('<Scroll />', () => {
-    it('should do something', () => {
-        const { container } = render(<Scroll />);
-    });
-});
