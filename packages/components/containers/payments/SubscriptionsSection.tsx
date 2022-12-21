d79e13db2bdd7326fcc98e5a5660bb487e7a1f90
Alexey Karpov <alexey.karpov@proton.ch>
2022-12-21 13:37:16 +0100

    Show the full date even if today is the day of renewal

--- packages/components/containers/payments/SubscriptionsSection.tsx
+++ packages/components/containers/payments/SubscriptionsSection.tsx
@@ -16,7 +16,7 @@ import MozillaInfoPanel from '../account/MozillaInfoPanel';
 import { getShortBillingText } from './helper';
 
 const getRenewalText = (periodEnd: number) => {
-    const formattedEndTime = <Time key="time-text">{periodEnd}</Time>;
+    const formattedEndTime = <Time sameDayFormat={false}>{periodEnd}</Time>;
     // translator: formattedEndTime uses a "long localized date" format ('PP' format in https://date-fns.org/v2.29.3/docs/format). E.g.: formattedEndTime = "Dec 12, 2022"
     return c('Billing cycle').jt`Renews automatically on ${formattedEndTime}`;
 };
