528f7a25a3e301e49329ad961352182cd7736944
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2022-12-28 18:35:53 +0100

    Split linkConfirmationModal into small components

---
+++ packages/components/components/notifications/LinkConfirmationModalPhishing.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { c } from 'ttag';
+
+import { Checkbox } from '../input';
+import { Label } from '../label';
+
+interface Props {
+    link: string;
+    value: boolean;
+    onToggle: () => void;
+}
+
+const LinkConfirmationModalPhishing = ({ link, onToggle, value }: Props) => (
+    <>
+        {`${c('Info')
+            .t`This link leads to a website that might be trying to steal your information, such as passwords and credit card details.`} `}
+        <br />
+        <span className="text-bold text-break">{link}</span>
+
+        <Label className="flex">
+            <Checkbox className="mr0-5" checked={value} onChange={onToggle} />
+            {c('Label').t`I understand the risk`}
+        </Label>
+    </>
+);
+
+export default LinkConfirmationModalPhishing;
