5f8ee8193e928a8dc79395f52584155e0a68e989
Oleg Voloshin <oleg.voloshin@proton.ch>
2023-01-03 16:59:39 +0200

    Add test for useTrashedLinksListing
    DRVWEB-2714

--- packages/shared/lib/api/drive/devices.ts
+++ packages/shared/lib/api/drive/devices.ts
@@ -1,36 +1,9 @@
-import { CreateDeviceVolume, DevicesResult } from '../../interfaces/drive/device';
+import { CreateDeviceVolume } from '../../interfaces/drive/device';
 
 export const queryDevices = () => ({
     method: 'get',
     url: 'drive/devices',
 });
-
-export const fetchDevicesMock = async (): Promise<DevicesResult> => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve({
-                Devices: [
-                    {
-                        Device: {
-                            DeviceID: '1',
-                            VolumeID: '1',
-                            CreateTime: Date.now(),
-                            ModifyTime: Date.now(),
-                            Type: 1,
-                            SyncState: 1,
-                        },
-                        Share: {
-                            ShareID:
-                                'iCh9uK5GOkusxvJk68rP81v7udtFxkvyhe_0Z6Fg7Ot37C3A5R5V_dt5OqBuIGnXrfX9ILrPkWNVB9gsPwNbtg==',
-                            Name: 'HOME-DESKTOP',
-                            LinkID: 'hqtFxkbSeO1bLlLzdn5_kkaSAV4aapKlQVsb5mdktrVtxFWEVg8nBvDLL08UbTDROsj5n5s6HlW5DSe_0N17fg==',
-                        },
-                    },
-                ],
-            });
-        }, 5000);
-    });
-};
 
 export const queryDeviceDeletion = (deviceId: string) => ({
     method: 'delete',
