3992d66533babc07dea205a5399a872e33a7fdd1
Eduardo Conde Pena <econdepe@proton.ch>
2023-01-03 12:09:19 +0100

    Restore previous URL regex and check for spaces within a URL in a different way

--- packages/shared/test/helpers/validators.spec.ts
+++ packages/shared/test/helpers/validators.spec.ts
@@ -2,7 +2,9 @@ import { isURL } from '@proton/shared/lib/helpers/validators';
 
 describe('isURL', () => {
     it('should return true for valid URLs', () => {
-        const urls = ['https://www.proton.me', 'http://www.proton.me', 'www.proton.me'];
+        const calendarLink =
+            'https://calendar.proton.me/api/calendar/v1/url/mGwA0H_CF5I8hNQU-p4FqUxFCFW3AsJE7BxzMzQHe0wmCiT_dyd3G0xl0WUCQmtHSQSfQ4rhACeFIVpsVjO3Kw==/calendar.ics?CacheKey=LRO6Cif1PdTj0Ys-Ox0Idw%3D%3D&PassphraseKey=vNIxDirYWB7r3f6BpgA5JT_flDIFQs0xh0uhImvTcY0%3D';
+        const urls = ['https://www.proton.me', 'http://www.proton.me', 'www.proton.me', calendarLink];
         const results = urls.map((url) => isURL(url));
 
         expect(results.every((result) => result === true)).toBe(true);
