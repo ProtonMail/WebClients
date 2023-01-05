ec61d8d3987fc61dff77ae4525ce0567351bdffe
Richard Tetaz <richard.tetaz@proton.ch>
2023-01-05 11:43:32 +0100

    Add tests on clearValue

--- packages/components/components/v2/addressesAutomplete/AddressesAutocomplete.helper.test.ts
+++ packages/components/components/v2/addressesAutomplete/AddressesAutocomplete.helper.test.ts
@@ -1,4 +1,4 @@
-import { splitBySeparator } from './AddressesAutocomplete.helper';
+import { clearValue, splitBySeparator } from './AddressesAutocomplete.helper';
 
 describe('splitBySeparator', () => {
     it('should omit separators present at the beginning and the end', () => {
@@ -11,5 +11,25 @@ describe('splitBySeparator', () => {
         const input = 'plus@debye.proton.black, visionary@debye.proton.black, iamblueuser@gmail.com,,';
         const result = splitBySeparator(input);
         expect(result).toEqual(['plus@debye.proton.black', 'visionary@debye.proton.black', 'iamblueuser@gmail.com']);
+    });
+});
+
+describe('clearValue', () => {
+    it('should trim the value', () => {
+        const input = ' panda@proton.me ';
+        const result = clearValue(input);
+        expect(result).toEqual('panda@proton.me');
+    });
+
+    it('should remove surrounding chevrons', () => {
+        const input = '<panda@proton.me>';
+        const result = clearValue(input);
+        expect(result).toEqual('panda@proton.me');
+    });
+
+    it('should keep chevrons when needed', () => {
+        const input = 'Panda <panda@proton.me>';
+        const result = clearValue(input);
+        expect(result).toEqual('Panda <panda@proton.me>');
     });
 });
