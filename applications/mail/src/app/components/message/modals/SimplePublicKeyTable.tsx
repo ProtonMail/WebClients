406caf0685fd0e9f712c2f123d3651386584ff2f
romain sanchez <romain.sanchez@proton.ch>
2023-01-11 17:47:41 +0100

    Make Trust key modal bigger so that we can see the whole fingerprint MAILWEB-3221

--- applications/mail/src/app/components/message/modals/SimplePublicKeyTable.tsx
+++ applications/mail/src/app/components/message/modals/SimplePublicKeyTable.tsx
@@ -69,9 +69,9 @@ const SimplePublicKeyTable = ({ contact }: Props) => {
             <thead>
                 <tr>
                     <th scope="col" className="text-ellipsis">{c('Table header').t`Fingerprint`}</th>
-                    {!isNarrow && <th scope="col" className="text-ellipsis">{c('Table header').t`Created`}</th>}
-                    {!isTinyMobile && <th scope="col" className="text-ellipsis">{c('Table header').t`Expires`}</th>}
-                    {!isNarrow && <th scope="col" className="text-ellipsis">{c('Table header').t`Type`}</th>}
+                    {!isNarrow && <th scope="col" className="text-ellipsis w20">{c('Table header').t`Created`}</th>}
+                    {!isTinyMobile && <th scope="col" className="text-ellipsis w15">{c('Table header').t`Expires`}</th>}
+                    {!isNarrow && <th scope="col" className="text-ellipsis w15">{c('Table header').t`Type`}</th>}
                 </tr>
             </thead>
             <TableBody>
