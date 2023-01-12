3c637567b1e57ff404cfdf368ad2818fbfbfd61d
Alexey Karpov <alexey.karpov@proton.ch>
2023-01-12 14:55:13 +0100

    Add onClose handler for the Pay Invoice modal
    PAY-768

--- packages/components/containers/invoices/PayInvoiceModal.tsx
+++ packages/components/containers/invoices/PayInvoiceModal.tsx
@@ -69,6 +69,7 @@ const PayInvoiceModal = ({ invoice, fetchInvoices, onClose, ...rest }: Props) =>
             close={c('Action').t`Close`}
             submit={submit}
             title={c('Title').t`Pay invoice`}
+            onClose={() => onClose?.()}
             {...rest}
         >
             {loadingCheck ? null : (
