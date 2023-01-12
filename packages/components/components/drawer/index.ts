624441bb93acc8c3ce1c210c674e8ec03b42d5d4
mmso <mmso@mmso.se>
2023-01-12 14:59:32 +0200

    Remove calendar from drawer for EAs

--- packages/components/components/drawer/index.ts
+++ packages/components/components/drawer/index.ts
@@ -1,4 +1,5 @@
 export { default as DrawerApp } from './DrawerApp';
 export { default as DrawerSidebar } from './DrawerSidebar';
+export { default as DrawerVisibilityButton } from './DrawerVisibilityButton';
 export { default as CalendarDrawerAppButton } from './drawerAppButtons/CalendarDrawerAppButton';
 export { default as ContactDrawerAppButton } from './drawerAppButtons/ContactDrawerAppButton';
