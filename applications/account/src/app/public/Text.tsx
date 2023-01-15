1a31172ec9305e2a000154ed1443b1979c3bfadc
kjurevic <kristina.jureviciute@proton.ch>
2023-01-15 14:52:53 +0200

    External account tests

--- applications/account/src/app/public/Text.tsx
+++ applications/account/src/app/public/Text.tsx
@@ -1,16 +1,19 @@
-import { ReactNode } from 'react';
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
 
 import { classnames } from '@proton/components';
 
-interface Props {
+interface Props extends ComponentPropsWithoutRef<'div'> {
     margin?: 'small';
     children: ReactNode;
     className?: string;
 }
 
-const Text = ({ children, margin, className }: Props) => {
+const Text = ({ children, margin, className, ...rest }: Props) => {
     return (
-        <div className={classnames([margin === 'small' ? 'mb1' : 'mb1-75', 'text-break color-weak', className])}>
+        <div
+            className={classnames([margin === 'small' ? 'mb1' : 'mb1-75', 'text-break color-weak', className])}
+            {...rest}
+        >
             {children}
         </div>
     );
