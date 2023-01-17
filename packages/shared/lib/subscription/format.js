35536a69c9f42dcbe11fa5a58b86eb29b6abd489
Alexey Karpov <alexey.karpov@proton.ch>
2023-01-17 16:22:10 +0100

    Fix refresh logic of subscriptions section
    PAY-798

--- packages/shared/lib/subscription/format.js
+++ packages/shared/lib/subscription/format.js
@@ -1,15 +1,14 @@
 import { isManagedByMozilla } from './helpers';
 
-const format = (subscription = {}, upcoming = undefined) => {
-    // it covers the case when UpcomingSubscription arrives from the Events API. In this case UpcomingSubscription is a child of Subscription.
-    // In case if it arrived through GET subscription then UpcomingSubscription is a sibling of Subscription
-    if (!upcoming) {
-        upcoming = subscription.UpcomingSubscription;
-    }
-
+/**
+ * @param {*} subscription
+ * @param {*} [UpcomingSubscription]
+ * @returns {import('../interfaces').SubscriptionModel}
+ */
+const format = (subscription = {}, UpcomingSubscription) => {
     return {
         ...subscription,
-        upcoming,
+        UpcomingSubscription,
         isManagedByMozilla: isManagedByMozilla(subscription),
     };
 };
