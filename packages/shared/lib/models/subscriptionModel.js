b5ab9d33268fb454ca9e9da9a79b7ee6077d71d9
Alexey Karpov <alexey.karpov@proton.ch>
2023-01-17 16:22:10 +0100

    Fix refresh logic of subscriptions section
    PAY-798

--- packages/shared/lib/models/subscriptionModel.js
+++ packages/shared/lib/models/subscriptionModel.js
@@ -2,6 +2,10 @@ import { getSubscription } from '../api/payments';
 import updateObject from '../helpers/updateObject';
 import formatSubscription from '../subscription/format';
 
+/**
+ * @param {*} api
+ * @returns {Promise<import('../interfaces').SubscriptionModel>}
+ */
 export const getSubscriptionModel = (api) => {
     return api(getSubscription()).then(({ Subscription, UpcomingSubscription }) =>
         formatSubscription(Subscription, UpcomingSubscription)
@@ -11,5 +15,23 @@ export const getSubscriptionModel = (api) => {
 export const SubscriptionModel = {
     key: 'Subscription',
     get: getSubscriptionModel,
-    update: (model, events) => formatSubscription(updateObject(model, events)),
+    update: (model, events) => {
+        let eventsSubscription = updateObject(model, events);
+
+        /**
+         * There are two possible cases in the events: UpcomingSubscription created and UpcomingSubscription deleted.
+         * This branch handles the deletion case, whereas {@link updateObject()} above handles the creation case.
+         */
+        if (!events.UpcomingSubscription) {
+            delete eventsSubscription.UpcomingSubscription;
+        }
+
+        /**
+         * In contrast to {@link getSubscriptionModel()}, events have a different structure for the
+         * UpcomingSubscription. For example, {@link getSubscription()} endpoint returns the both properties on the top
+         * level: { Subscription: { ... }, UpcomingSubscription: { ... }} Events make the upcoming subscription nested:
+         * { Subscription: { UpcomingSubscription: { ... }, ...} }
+         */
+        return formatSubscription(eventsSubscription, eventsSubscription.UpcomingSubscription);
+    },
 };
---
+++ packages/shared/test/subscription/subscriptionModel.spec.ts
@@ -0,0 +1,102 @@
+import { SubscriptionModel as SubscriptionModelInterface } from '@proton/shared/lib/interfaces';
+import { SubscriptionModel, getSubscriptionModel } from '@proton/shared/lib/models/subscriptionModel';
+
+type DeepPartial<T> = T extends object
+    ? {
+          [P in keyof T]?: DeepPartial<T[P]>;
+      }
+    : T;
+
+type PartialSubscriptionModel = DeepPartial<SubscriptionModelInterface>;
+
+describe('subscriptionModel', () => {
+    describe('getSubscriptionModel()', () => {
+        it('should expect the API response with two top-level properties: Subscription and UpcomingSubscription', async () => {
+            const apiMock = () =>
+                Promise.resolve({
+                    Subscription: { ID: 'Subscription1' },
+                    UpcomingSubscription: { ID: 'Subscription2' },
+                });
+
+            const result: PartialSubscriptionModel = await getSubscriptionModel(apiMock);
+            expect(result).toEqual({
+                ID: 'Subscription1',
+                UpcomingSubscription: { ID: 'Subscription2' },
+                isManagedByMozilla: false,
+            });
+        });
+
+        it('should handle the case when there is no UpcomingSubscription', async () => {
+            const apiMock = () =>
+                Promise.resolve({
+                    Subscription: { ID: 'Subscription1' },
+                });
+
+            const result: PartialSubscriptionModel = await getSubscriptionModel(apiMock);
+            expect(result).toEqual({
+                ID: 'Subscription1',
+                UpcomingSubscription: undefined,
+                isManagedByMozilla: false,
+            });
+        });
+    });
+
+    describe('SubscriptionModel.update', () => {
+        it('should add the UpcomingSubscription from the events', () => {
+            const model = {
+                ID: 'Subscription1',
+                isManagedByMozilla: false,
+            };
+
+            const events = {
+                ID: 'Subscription1',
+                UpcomingSubscription: { ID: 'Subscription2' },
+            };
+
+            const result: PartialSubscriptionModel = SubscriptionModel.update(model, events);
+            expect(result).toEqual({
+                ID: 'Subscription1',
+                isManagedByMozilla: false,
+                UpcomingSubscription: { ID: 'Subscription2' },
+            });
+        });
+
+        it('should remove UpcomingSubscription when events does not have it', () => {
+            const model = {
+                ID: 'Subscription1',
+                UpcomingSubscription: { ID: 'Subscription2' },
+                isManagedByMozilla: false,
+            };
+
+            const events = {
+                ID: 'Subscription1',
+            };
+
+            const result: PartialSubscriptionModel = SubscriptionModel.update(model, events);
+
+            expect(result).toEqual({
+                ID: 'Subscription1',
+                UpcomingSubscription: undefined,
+                isManagedByMozilla: false,
+            });
+        });
+
+        it('should do nothing if model does not have UpcomingSubscription, nor events', () => {
+            const model = {
+                ID: 'Subscription1',
+                isManagedByMozilla: false,
+            };
+
+            const events = {
+                ID: 'Subscription1',
+            };
+
+            const result: PartialSubscriptionModel = SubscriptionModel.update(model, events);
+            expect(result).toEqual({
+                ID: 'Subscription1',
+                UpcomingSubscription: undefined,
+                isManagedByMozilla: false,
+            });
+        });
+    });
+});
