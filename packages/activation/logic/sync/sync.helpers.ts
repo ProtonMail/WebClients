79dcbf6e5f4c35d65ae6b7a9d38b4431ca279faf
Flavien Bonvin <flavien.bonvin@proton.ch>
2023-01-18 10:34:10 +0100

    Add sync item support on events update

--- packages/activation/logic/sync/sync.helpers.ts
+++ packages/activation/logic/sync/sync.helpers.ts
@@ -2,15 +2,19 @@ import { ApiSync } from '@proton/activation/api/api.interface';
 
 import { Sync } from './sync.interface';
 
-export const formatApiSync = (syncs: ApiSync[]): Sync[] => {
+export const formatApiSync = (sync: ApiSync): Sync => {
+    return {
+        account: sync.Account,
+        id: sync.ID,
+        importerID: sync.ImporterID,
+        product: sync.Product,
+        state: sync.State,
+        startDate: sync.CreateTime,
+    };
+};
+
+export const formatApiSyncs = (syncs: ApiSync[]): Sync[] => {
     return syncs.map((item) => {
-        return {
-            account: item.Account,
-            id: item.ID,
-            importerID: item.ImporterID,
-            product: item.Product,
-            state: item.State,
-            startDate: item.CreateTime,
-        };
+        return formatApiSync(item);
     });
 };
