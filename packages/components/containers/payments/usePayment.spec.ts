549ac10f26f4d879a3084a08fed936364c3e3980
Alexey Karpov <alexey.karpov@proton.ch>
2023-01-20 14:36:31 +0100

    Add isExistingPaymentMethod() helper

--- packages/components/containers/payments/usePayment.spec.ts
+++ packages/components/containers/payments/usePayment.spec.ts
@@ -2,6 +2,7 @@ import { renderHook } from '@testing-library/react-hooks';
 
 import { PAYMENT_METHOD_TYPES } from '@proton/shared/lib/constants';
 
+import { CardPayment, PaymentParameters } from './interface';
 import usePayment from './usePayment';
 
 jest.mock('./usePayPal', () =>
@@ -13,7 +14,7 @@ jest.mock('./usePayPal', () =>
 
 describe('usePayment', () => {
     it('should render', () => {
-        let { result } = renderHook(() =>
+        const { result } = renderHook(() =>
             usePayment({
                 amount: 1000,
                 currency: 'EUR',
@@ -25,7 +26,7 @@ describe('usePayment', () => {
     });
 
     it('should be able to pay if amount is 0', () => {
-        let { result } = renderHook(() =>
+        const { result } = renderHook(() =>
             usePayment({
                 amount: 0,
                 currency: 'EUR',
@@ -37,7 +38,7 @@ describe('usePayment', () => {
     });
 
     it('should not able to pay if method is not defined', () => {
-        let { result } = renderHook(() =>
+        const { result } = renderHook(() =>
             usePayment({
                 amount: 1000,
                 currency: 'EUR',
@@ -48,34 +49,24 @@ describe('usePayment', () => {
         expect(result.current.canPay).toEqual(false);
     });
 
-    it('should not able to pay if method is cash', () => {
-        let { result } = renderHook(() =>
-            usePayment({
-                amount: 1000,
-                defaultMethod: PAYMENT_METHOD_TYPES.CASH,
-                currency: 'EUR',
-                onPay: () => {},
-            })
-        );
+    it.each([PAYMENT_METHOD_TYPES.CASH, PAYMENT_METHOD_TYPES.BITCOIN])(
+        'should not able to pay if method is %s',
+        (defaultMethod) => {
+            const { result } = renderHook(() =>
+                usePayment({
+                    amount: 1000,
+                    defaultMethod,
+                    currency: 'EUR',
+                    onPay: () => {},
+                })
+            );
 
-        expect(result.current.canPay).toEqual(false);
-    });
-
-    it('should not able to pay if method is bitcoin', () => {
-        let { result } = renderHook(() =>
-            usePayment({
-                amount: 1000,
-                defaultMethod: PAYMENT_METHOD_TYPES.BITCOIN,
-                currency: 'EUR',
-                onPay: () => {},
-            })
-        );
-
-        expect(result.current.canPay).toEqual(false);
-    });
+            expect(result.current.canPay).toEqual(false);
+        }
+    );
 
     it('should not be able to pay of method is paypal and there is no payment token', () => {
-        let { result } = renderHook(() =>
+        const { result } = renderHook(() =>
             usePayment({
                 amount: 1000,
                 defaultMethod: PAYMENT_METHOD_TYPES.PAYPAL,
@@ -85,5 +76,79 @@ describe('usePayment', () => {
         );
 
         expect(result.current.canPay).toEqual(false);
+    });
+
+    describe('payment parameters', () => {
+        it('should return empty object if there is no method', () => {
+            const { result } = renderHook(() =>
+                usePayment({
+                    amount: 1000,
+                    currency: 'EUR',
+                    onPay: () => {},
+                })
+            );
+
+            expect(result.current.parameters).toEqual({});
+        });
+
+        it('should return PaymentMethodID if user selected an existing method', () => {
+            const { result } = renderHook(() =>
+                usePayment({
+                    amount: 1000,
+                    currency: 'EUR',
+                    onPay: () => {},
+                })
+            );
+
+            result.current.setMethod('existing-payment-method-id');
+
+            expect(result.current.parameters).toEqual({
+                PaymentMethodID: 'existing-payment-method-id',
+            });
+        });
+
+        it('should return CardPayment if method is card', () => {
+            const { result } = renderHook(() =>
+                usePayment({
+                    amount: 1000,
+                    currency: 'EUR',
+                    onPay: () => {},
+                    defaultMethod: PAYMENT_METHOD_TYPES.CARD,
+                })
+            );
+
+            const expectedPaymentParameters: PaymentParameters<CardPayment> = {
+                Payment: {
+                    Type: PAYMENT_METHOD_TYPES.CARD,
+                    Details: {
+                        CVC: '',
+                        Country: 'US',
+                        ExpMonth: '',
+                        ExpYear: '',
+                        Name: '',
+                        Number: '',
+                        ZIP: '',
+                    },
+                },
+            };
+
+            expect(result.current.parameters).toEqual(expectedPaymentParameters);
+        });
+
+        it.each([PAYMENT_METHOD_TYPES.PAYPAL, PAYMENT_METHOD_TYPES.CASH, PAYMENT_METHOD_TYPES.BITCOIN])(
+            'should return empty paymentMethods for %s',
+            (defaultMethod) => {
+                const { result } = renderHook(() =>
+                    usePayment({
+                        amount: 1000,
+                        currency: 'EUR',
+                        onPay: () => {},
+                        defaultMethod,
+                    })
+                );
+
+                expect(result.current.parameters).toEqual({});
+            }
+        );
     });
 });
