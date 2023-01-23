747696a8eb7a03da45cc70bc23f68735634feb2f
mmso <mmso@mmso.se>
2023-01-23 17:39:30 +0100

    Improve unsupported browser detection mechanism

--- packages/pack/webpack.config.js
+++ packages/pack/webpack.config.js
@@ -54,9 +54,10 @@ const getConfig = (env) => {
             },
         },
         entry: {
-            // The order is important. The supported.js file sets a global variable that is used by unsupported.js to detect if the main bundle could be parsed.
-            index: [path.resolve('./src/app/index.tsx'), require.resolve('@proton/shared/lib/browser/supported.js')],
-            unsupported: [require.resolve('@proton/shared/lib/browser/unsupported.js')],
+            // The order is important. The pre.js listens to index.js, and supported.js file sets a global variable that is used by unsupported.js to detect if the main bundle could be parsed.
+            pre: [require.resolve('@proton/shared/lib/supported/pre.ts')],
+            index: [path.resolve('./src/app/index.tsx'), require.resolve('@proton/shared/lib/supported/supported.ts')],
+            unsupported: [require.resolve('@proton/shared/lib/supported/unsupported.ts')],
         },
         output: {
             filename: options.isProduction ? '[name].[contenthash:8].js' : '[name].js',
@@ -124,3 +125,12 @@ const getConfig = (env) => {
 };
 
 module.exports = getConfig;
+
+module.exports.mergeEntry = (originalEntry, entry) => {
+    const { pre, ...rest } = originalEntry;
+    return {
+        pre,
+        ...entry,
+        ...rest,
+    };
+};
--- packages/shared/lib/supported/supported.ts
+++ packages/shared/lib/supported/supported.ts
@@ -1,14 +1,16 @@
+import { SupportedBrowserValue } from './interface';
+
 /**
  * This file is included in the main bundle. Its main purpose is to find out if the main bundle could execute,
  * or if it errored out due to a Syntax Error since the main bundle is only compiled against a specific list
  * of browsers. We also check some specific browsers here.
  * The unsupported.js script is included as another script tag and relies on this variable.
  */
-const isNotSupported = () => {
+const isUnsupported = () => {
     // IE11 or old edge (not chromium based) are not supported.
     const isOldEdgeOrIE = !('reversed' in document.createElement('ol'));
     // If these function get polyfilled they'll exist, this is a safety mechanism for when we stop supporting it
     return isOldEdgeOrIE || !Object.fromEntries || !''.trimStart || !window.crypto.subtle;
 };
 
-window.protonSupportedBrowser = !isNotSupported();
+window.protonSupportedBrowser = isUnsupported() ? SupportedBrowserValue.Unsupported : SupportedBrowserValue.Supported;
