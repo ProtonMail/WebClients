9aaab206b674d8da36dbbb781b5c49a87006c743
Richard Tetaz <richard.tetaz@proton.ch>
2023-01-25 14:47:23 +0100

    Special offer requires 7 days of subscription

--- packages/components/containers/offers/operations/specialOffer2022/eligibility.ts
+++ packages/components/containers/offers/operations/specialOffer2022/eligibility.ts
@@ -1,3 +1,5 @@
+import { addDays, fromUnixTime, isBefore } from 'date-fns';
+
 import { APPS, PLANS } from '@proton/shared/lib/constants';
 import { getPlan, hasMonthly, isExternal } from '@proton/shared/lib/helpers/subscription';
 import { ProtonConfig, Subscription, UserModel } from '@proton/shared/lib/interfaces';
@@ -10,10 +12,12 @@ interface Props {
 
 const isEligible = ({ user, subscription, protonConfig }: Props) => {
     const isValidApp = protonConfig?.APP_NAME === APPS.PROTONMAIL || protonConfig?.APP_NAME === APPS.PROTONACCOUNT;
+    const createDate = subscription?.CreateTime ? fromUnixTime(subscription.CreateTime) : new Date();
     const plan = getPlan(subscription);
     return (
         plan?.Name === PLANS.BUNDLE &&
         hasMonthly(subscription) &&
+        isBefore(createDate, addDays(new Date(), -7)) &&
         user.canPay &&
         isValidApp &&
         !user.isDelinquent &&
