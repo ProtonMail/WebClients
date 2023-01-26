6202050d3a7cd5a6cc11a4bef4c8de531c8c4dd8
Richard Tetaz <richard.tetaz@proton.ch>
2023-01-26 08:59:03 +0100

    Always use OfferTitle

--- packages/components/containers/offers/operations/blackFridayVPN1Deal2022/Layout.tsx
+++ packages/components/containers/offers/operations/blackFridayVPN1Deal2022/Layout.tsx
@@ -5,6 +5,7 @@ import OfferFooter from '../../components/shared/OfferFooter';
 import OfferHeader from '../../components/shared/OfferHeader';
 import OfferLayout from '../../components/shared/OfferLayout';
 import OfferLoader from '../../components/shared/OfferLoader';
+import OfferTitle from '../../components/shared/OfferTitle';
 import Deals from '../../components/shared/deal/Deals';
 import hasOffer from '../../helpers/hasOffer';
 import { OfferLayoutProps } from '../../interface';
@@ -13,7 +14,7 @@ const Layout = (props: OfferLayoutProps) => {
     return hasOffer(props) ? (
         <OfferLayout {...props}>
             <OfferHeader {...props}>
-                <h1 className="h2 text-center text-bold">{c('specialoffer: Title').t`End of year offer`}</h1>
+                <OfferTitle>{c('specialoffer: Title').t`End of year offer`}</OfferTitle>
             </OfferHeader>
 
             <Deals {...props} />
