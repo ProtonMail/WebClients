7f5b80c14e4a71aa418576a242fd34520ed1e24a
Jack Crompton <jack.crompton@proton.ch>
2023-01-30 15:26:20 +0100

    Add @proton/metrics unit tests
    CP-5179

---
+++ packages/metrics/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    setupFilesAfterEnv: ['./jest.setup.js'],
+    preset: 'ts-jest',
+    collectCoverage: true,
+    clearMocks: true,
+};
---
+++ packages/metrics/tests/MetricsBase.test.ts
@@ -0,0 +1,102 @@
+import MetricsApi from '../lib/MetricsApi';
+import MetricsBase from '../lib/MetricsBase';
+import MetricsRequestService from '../lib/MetricsRequestService';
+
+jest.mock('../lib/MetricsApi');
+const metricsApi = new MetricsApi();
+
+jest.mock('../lib/MetricsRequestService', () => {
+    return jest.fn().mockImplementation(() => {
+        return { api: metricsApi, setReportMetrics: jest.fn(), processAllRequests: jest.fn() };
+    });
+});
+
+const metricsRequestService = new MetricsRequestService(metricsApi, { reportMetrics: true });
+
+describe('MetricsBase', () => {
+    describe('init', () => {
+        it('sets auth headers', () => {
+            const uid = 'uid';
+
+            const clientID = 'clientID';
+            const appVersion = 'appVersion';
+            const metrics = new MetricsBase(metricsRequestService);
+
+            metrics.init({ uid, clientID, appVersion });
+
+            expect(metricsRequestService.api.setAuthHeaders).toHaveBeenCalledWith(uid);
+        });
+
+        it('sets version headers', () => {
+            const uid = 'uid';
+            const clientID = 'clientID';
+            const appVersion = 'appVersion';
+            const metrics = new MetricsBase(metricsRequestService);
+
+            metrics.init({ uid, clientID, appVersion });
+
+            expect(metricsRequestService.api.setVersionHeaders).toHaveBeenCalledWith(clientID, appVersion);
+        });
+    });
+
+    describe('setVersionHeaders', () => {
+        it('sets version headers', () => {
+            const clientID = 'clientID';
+            const appVersion = 'appVersion';
+            const metrics = new MetricsBase(metricsRequestService);
+
+            metrics.setVersionHeaders(clientID, appVersion);
+
+            expect(metricsRequestService.api.setVersionHeaders).toHaveBeenCalledWith(clientID, appVersion);
+        });
+    });
+
+    describe('setAuthHeaders', () => {
+        it('sets auth headers', () => {
+            const uid = 'uid';
+            const metrics = new MetricsBase(metricsRequestService);
+
+            metrics.setAuthHeaders(uid);
+
+            expect(metricsRequestService.api.setAuthHeaders).toHaveBeenCalledWith(uid);
+        });
+    });
+
+    describe('clearAuthHeaders', () => {
+        it(`sets auth headers with uid ''`, () => {
+            const metrics = new MetricsBase(metricsRequestService);
+
+            metrics.clearAuthHeaders();
+
+            expect(metricsRequestService.api.setAuthHeaders).toHaveBeenCalledWith('');
+        });
+    });
+
+    describe('setReportMetrics', () => {
+        it('sets report metrics to false', () => {
+            const metrics = new MetricsBase(metricsRequestService);
+
+            metrics.setReportMetrics(false);
+
+            expect(metricsRequestService.setReportMetrics).toHaveBeenCalledWith(false);
+        });
+
+        it('sets report metrics to true', () => {
+            const metrics = new MetricsBase(metricsRequestService);
+
+            metrics.setReportMetrics(true);
+
+            expect(metricsRequestService.setReportMetrics).toHaveBeenCalledWith(true);
+        });
+    });
+
+    describe('processAllRequests', () => {
+        it('calls processAllRequests', async () => {
+            const metrics = new MetricsBase(metricsRequestService);
+
+            await metrics.processAllRequests();
+
+            expect(metricsRequestService.processAllRequests).toHaveBeenCalledTimes(1);
+        });
+    });
+});
