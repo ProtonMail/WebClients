f164a7b9000595dcd21ac24fed410a338ad44107
Alexey Karpov <alexey.karpov@proton.ch>
2023-01-30 14:45:04 +0100

    Remove unused helper

--- packages/components/containers/payments/helper.ts
+++ packages/components/containers/payments/helper.ts
@@ -1,26 +1,6 @@
 import { c, msgid } from 'ttag';
 
-import { CYCLE } from '@proton/shared/lib/constants';
 import { Cycle } from '@proton/shared/lib/interfaces';
-
-export const getDueCycleText = (cycle: Cycle) => {
-    if (cycle === CYCLE.MONTHLY) {
-        return c('Billing cycle').t`Payable every month`;
-    }
-    if (cycle === CYCLE.YEARLY) {
-        return c('Billing cycle').t`Payable every 12 months`;
-    }
-    if (cycle === CYCLE.TWO_YEARS) {
-        return c('Billing cycle').t`Payable every 24 months`;
-    }
-    if (cycle === CYCLE.THIRTY) {
-        return c('Billing cycle').t`Payable every 30 months`;
-    }
-    if (cycle === CYCLE.FIFTEEN) {
-        return c('Billing cycle').t`Payable every 15 months`;
-    }
-    return '';
-};
 
 export const getTotalBillingText = (cycle: Cycle) => {
     const n = cycle;
