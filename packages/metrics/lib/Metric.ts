cee5d9e79cd48ae55fab032149578fb286c14bbf
Jack Crompton <jack.crompton@proton.ch>
2023-01-31 16:25:01 +0100

    Remove label name validation

--- packages/metrics/lib/Metric.ts
+++ packages/metrics/lib/Metric.ts
@@ -1,18 +1,17 @@
 import { IMetricsRequestService } from './MetricsRequestService';
 
-// These are from https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels
-const metricRegexp = /^[a-zA-Z_:][a-zA-Z0-9_:]*$/;
-const labelRegexp = /^[a-zA-Z_][a-zA-Z0-9_]*$/;
+/**
+ * Custom regex based on the following
+ * - https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels
+ * - https://gitlab.protontech.ch/proton/be/json-schema-registry
+ */
+const metricRegexp = /^[a-zA-Z]+(?:_[a-zA-Z0-9]+)*$/;
 
 function validateMetricName(name: string) {
     return metricRegexp.test(name);
 }
 
-function validateLabelName(names: string[]) {
-    return names.every((name) => labelRegexp.test(name));
-}
-
-export type MetricVersions = '1' | '2';
+export type MetricVersions = 1 | 2;
 
 export interface MetricSchema {
     Value: number;
@@ -32,19 +31,11 @@ abstract class Metric<D extends MetricSchema> {
         this.requestService = requestService;
 
         if (!validateMetricName(this.name)) {
-            throw new Error('Invalid metric name');
+            throw new Error(`Invalid metric name ${this.name}`);
         }
     }
 
     protected addToRequestQueue(data: D) {
-        const labelNames = Object.keys(data.Labels);
-        if (!validateLabelName(labelNames)) {
-            /**
-             * TODO: should we really throw here?
-             */
-            throw new Error('Invalid label name');
-        }
-
         this.requestService.report({
             Name: this.name,
             Version: this.version,
