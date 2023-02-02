2282462ea1c730d79057fa02c2406c3f7cdcc547
Alexey Karpov <alexey.karpov@proton.ch>
2023-02-02 17:22:35 +0100

    Add tests for isExpired

--- packages/shared/lib/helpers/card.ts
+++ packages/shared/lib/helpers/card.ts
@@ -1,13 +1,18 @@
+import { CardDetails, isCardDetails } from '../interfaces';
+
 /**
  * Check if card is expired at the end of current month
  */
-export const isExpired = ({
-    ExpMonth = 12,
-    ExpYear = 9999,
-}: { ExpMonth?: number | string; ExpYear?: number | string } = {}): boolean => {
+export function isExpired(cardDetails: CardDetails | unknown): boolean {
+    if (!isCardDetails(cardDetails)) {
+        return false;
+    }
+
+    const { ExpMonth, ExpYear } = cardDetails;
+
     const currentTime = new Date();
     const currentMonth = currentTime.getMonth() + 1;
     const currentYear = currentTime.getFullYear();
 
     return currentMonth > +ExpMonth && currentYear >= +ExpYear;
-};
+}
