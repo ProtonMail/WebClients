9e293d081b58cfbbb59a609646db5704723c7b18
romain sanchez <romain.sanchez@proton.ch>
2023-02-08 16:09:16 +0100

    Make it possible to detect addresses separated by a space in AddressesAutocomplete v2 MAILWEB-3961

--- packages/components/components/v2/index.ts
+++ packages/components/components/v2/index.ts
@@ -5,4 +5,4 @@ export { default as PhoneInput } from './phone/LazyPhoneInput';
 export { default as PasswordInputTwo } from './input/PasswordInput';
 export { default as InputFieldTwo } from './field/InputField';
 export { default as useFormErrors } from './useFormErrors';
-export { default as AddressesAutocompleteTwo } from './addressesAutomplete/AddressesAutocomplete';
+export { default as AddressesAutocompleteTwo } from './addressesAutocomplete/AddressesAutocomplete';
