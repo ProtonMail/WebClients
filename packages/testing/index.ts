130fc99e7d7df0ab1cf56b2dccc346717baff269
Alexey Karpov <alexey.karpov@proton.ch>
2023-02-13 16:54:14 +0100

    Rename mockApi to mockApiWithServer

--- packages/testing/index.ts
+++ packages/testing/index.ts
@@ -2,7 +2,7 @@ export { rest } from 'msw';
 export * from './lib/api';
 export * from './lib/builders';
 export * from './lib/cache';
-export * from './lib/mockApi';
+export * from './lib/mockApiWithServer';
 export * from './lib/mockModals';
 export * from './lib/mockNotifications';
 export * from './lib/mockRandomValues';
