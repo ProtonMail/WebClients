92eaead5d67d3d97829b06c7118ba251b44814a5
Eduardo Conde Pena <econdepe@proton.ch>
2023-02-14 18:55:22 +0100

    Fix bad sequences on export
    CALWEB-4199

--- packages/shared/lib/calendar/export/export.ts
+++ packages/shared/lib/calendar/export/export.ts
@@ -2,6 +2,7 @@ import { fromUnixTime } from 'date-fns';
 import { c } from 'ttag';
 
 import { CryptoProxy } from '@proton/crypto';
+import { withSupportedSequence } from '@proton/shared/lib/calendar/icsSurgery/vevent';
 import partition from '@proton/utils/partition';
 import unique from '@proton/utils/unique';
 
@@ -164,7 +165,7 @@ const decryptEvent = async ({
         });
 
         // SUMMARY is mandatory in a PUBLISH ics
-        return withSummary(veventComponent);
+        return withSupportedSequence(withSummary(veventComponent));
     } catch (error: any) {
         const inactiveKeys = addresses.flatMap(({ Keys }) => Keys.filter(({ Active }) => !Active));
         return getError({
