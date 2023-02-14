55f0f0e9cc126ddc2cf0a1dd20bcbb6ede86c20a
Richard Tetaz <richard.tetaz@proton.ch>
2023-02-14 21:22:43 +0100

    Follow Redux good practice

--- applications/mail/src/app/logic/messages/expire/messagesExpireActions.ts
+++ applications/mail/src/app/logic/messages/expire/messagesExpireActions.ts
@@ -4,7 +4,7 @@ import { setExpiration } from '@proton/shared/lib/api/messages';
 import { Api } from '@proton/shared/lib/interfaces';
 
 export const expireMessages = createAsyncThunk<void, { IDs: string[]; expirationTime: number | null; api: Api }>(
-    'messages/expire',
+    'messages/setExpiration',
     async ({ IDs, expirationTime, api }) => {
         await api(setExpiration(IDs, expirationTime));
     }
