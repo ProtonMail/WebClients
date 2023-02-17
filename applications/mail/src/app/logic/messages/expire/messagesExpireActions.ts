1ce81e0b03379ef32a1a6eea6a5208bf5c22a3da
Richard Tetaz <richard.tetaz@proton.ch>
2023-02-17 18:18:40 +0100

    Improve optimistic logic

--- applications/mail/src/app/logic/messages/expire/messagesExpireActions.ts
+++ applications/mail/src/app/logic/messages/expire/messagesExpireActions.ts
@@ -5,7 +5,7 @@ import { Api } from '@proton/shared/lib/interfaces';
 
 export const expireMessages = createAsyncThunk<
     Promise<void>,
-    { IDs: string[]; expirationTime: number | null; api: Api; call: () => Promise<void> }
+    { IDs: string[]; conversationID?: string; expirationTime: number | null; api: Api; call: () => Promise<void> }
 >('messages/setExpiration', async ({ IDs, expirationTime, api, call }) => {
     await api(setExpiration(IDs, expirationTime));
     await call();
