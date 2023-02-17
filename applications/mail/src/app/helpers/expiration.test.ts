6d9cf868e7a7a64b1cd391cf705ef8d331bec483
Richard Tetaz <richard.tetaz@proton.ch>
2023-02-17 18:07:50 +0100

    Add self-destruction action in message

--- applications/mail/src/app/helpers/expiration.test.ts
+++ applications/mail/src/app/helpers/expiration.test.ts
@@ -1,6 +1,6 @@
 import { UserModel } from '@proton/shared/lib/interfaces';
 
-import { canSetExpiration } from './expiration';
+import { canSetExpiration, getExpirationTime } from './expiration';
 
 describe('canSetExpiration', () => {
     it('should return false if feature flag is false', () => {
@@ -13,5 +13,15 @@ describe('canSetExpiration', () => {
 
     it('should return true if feature flag is true and user is paid', () => {
         expect(canSetExpiration(true, { isFree: false } as UserModel)).toBe(true);
+    });
+});
+
+describe('getExpirationTime', () => {
+    it('should return null if days is 0', () => {
+        expect(getExpirationTime(0)).toBe(null);
+    });
+
+    it('should return a Unix timestamp if days is > 0', () => {
+        expect(getExpirationTime(1)).toBeGreaterThan(0);
     });
 });
