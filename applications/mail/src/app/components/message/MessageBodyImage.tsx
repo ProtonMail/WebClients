72204cfed99a734bfb3158705a0931f15b321da0
romain sanchez <romain.sanchez@proton.ch>
2023-02-20 16:15:10 +0100

    Prevent displaying Load anyway banner when loading without proxy failed MAILWEB-4022

--- applications/mail/src/app/components/message/MessageBodyImage.tsx
+++ applications/mail/src/app/components/message/MessageBodyImage.tsx
@@ -8,7 +8,7 @@ import { SimpleMap } from '@proton/shared/lib/interfaces';
 import clsx from '@proton/utils/clsx';
 
 import { getAnchor } from '../../helpers/message/messageImages';
-import { loadRemoteProxy } from '../../logic/messages/images/messagesImagesActions';
+import { failedRemoteDirectLoading, loadRemoteProxy } from '../../logic/messages/images/messagesImagesActions';
 import { MessageImage } from '../../logic/messages/messagesTypes';
 import { useAppDispatch } from '../../logic/store';
 
@@ -83,6 +83,8 @@ const MessageBodyImage = ({
     const dispatch = useAppDispatch();
     const api = useApi();
     const imageRef = useRef<HTMLImageElement>(null);
+    // Ref used to trigger an action after a onError while trying to load the image
+    const hasLoadedAfterError = useRef({ hasLoadedProxy: false, hasLoadedDirect: false });
     const { type, error, url, status, original } = image;
     const showPlaceholder =
         error || status !== 'loaded' || (type === 'remote' ? !showRemoteImages : !showEmbeddedImages);
@@ -115,8 +117,14 @@ const MessageBodyImage = ({
         // Only make this call when user is using proxy.
         // - Without proxy we are already trying to load direct
         // - With EO, we are also already trying to load direct
-        if (type === 'remote' && useProxy) {
+        // However, if we are trying to load the image without the proxy, we don't want to trigger the load remote onError
+        if (type === 'remote' && useProxy && !hasLoadedAfterError.current.hasLoadedProxy) {
+            hasLoadedAfterError.current.hasLoadedProxy = true;
             await dispatch(loadRemoteProxy({ ID: localID, imageToLoad: image, api }));
+        } else if (type === 'remote' && !hasLoadedAfterError.current.hasLoadedDirect) {
+            // Instead, we want to add an error to the image in the state to display a placeholder
+            hasLoadedAfterError.current.hasLoadedDirect = true;
+            await dispatch(failedRemoteDirectLoading({ ID: localID, image }));
         }
     };
 
