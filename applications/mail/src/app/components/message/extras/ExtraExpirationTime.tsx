1e7ebb8b372fe11f47a93c1c87c542115990c963
Richard Tetaz <richard.tetaz@proton.ch>
2023-02-21 17:18:01 +0100

    French touch

--- applications/mail/src/app/components/message/extras/ExtraExpirationTime.tsx
+++ applications/mail/src/app/components/message/extras/ExtraExpirationTime.tsx
@@ -11,6 +11,7 @@ import {
     useNotifications,
     useUser,
 } from '@proton/components';
+import clsx from '@proton/utils/clsx';
 
 import { canSetExpiration } from '../../../helpers/expiration';
 import { useExpiration } from '../../../hooks/useExpiration';
@@ -56,7 +57,11 @@ const ExtraExpirationTime = ({ message }: Props) => {
         <div className="bg-norm rounded border pl0-5 pr0-25 on-mobile-pr0-5 on-mobile-pb0-5 py0-25 mb0-85 flex flex-nowrap on-mobile-flex-column">
             <div className="flex-item-fluid flex flex-nowrap on-mobile-mb0-5" data-testid="expiration-banner">
                 <Icon name="hourglass" className="mt0-4 flex-item-noshrink ml0-2" />
-                <span className="pl0-5 pr0-5 flex flex-item-fluid flex-align-items-center">{delayMessage}</span>
+                <span
+                    className={clsx(canExpire && 'mt0-25', 'pl0-5 pr0-5 flex flex-item-fluid flex-align-items-center')}
+                >
+                    {delayMessage}
+                </span>
             </div>
             {canExpire ? (
                 <span className="flex-item-noshrink flex-align-items-start flex on-mobile-w100 pt0-1">
