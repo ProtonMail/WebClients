5d821ff38c0ba0337b78d96d33a240ec4831fa0c
Richard Tetaz <richard.tetaz@proton.ch>
2023-02-27 17:47:10 +0100

    Review

--- applications/mail/src/app/components/list/useItemExpiration.ts
+++ applications/mail/src/app/components/list/useItemExpiration.ts
@@ -3,13 +3,13 @@ import { useEffect, useState } from 'react';
 import {
     differenceInDays,
     differenceInHours,
-    differenceInMonths,
-    differenceInWeeks,
     differenceInYears,
+    endOfDay,
     fromUnixTime,
+    isAfter,
     isToday,
 } from 'date-fns';
-import { c } from 'ttag';
+import { c, msgid } from 'ttag';
 
 import { formatFullDate } from '../../helpers/date';
 
@@ -17,6 +17,7 @@ const EVERY_THIRTY_MINUTES = 30 * 60 * 1000;
 
 const useItemExpiration = (expirationTime?: number) => {
     const date = fromUnixTime(expirationTime || 0);
+    const end = endOfDay(date);
     const formattedDate = formatFullDate(date);
     const tooltipMessage = c('Info').t`This message will expire ${formattedDate}`;
     const [willExpireToday, setWillExpireToday] = useState(isToday(date));
@@ -24,28 +25,42 @@ const useItemExpiration = (expirationTime?: number) => {
 
     useEffect(() => {
         if (expirationTime) {
-            const now = new Date();
-            const remainingHours = differenceInHours(date, now);
-            const remainingDays = differenceInDays(date, now);
-            const remainingWeeks = differenceInWeeks(date, now);
-            const remainingMonths = differenceInMonths(date, now);
-            const remaingYears = differenceInYears(date, now);
-
             const callback = () => {
+                const now = new Date();
+                const remainingHours = differenceInHours(date, now);
+                const remainingDays = differenceInDays(end, now); // Using endOfDay since differenceInDays count full days
+                const remainingYears = differenceInYears(date, now);
+                const hasEnded = isAfter(now, date);
                 const expiringToday = isToday(date);
 
                 setWillExpireToday(expiringToday);
 
-                if (remaingYears > 1) {
-                    setShortMessage(c('Remaining years before the message expire').t`${remaingYears} years`);
-                } else if (remainingMonths > 1) {
-                    setShortMessage(c('Remaining months before the message expire').t`${remainingMonths} months`);
-                } else if (remainingWeeks > 1) {
-                    setShortMessage(c('Remaining weeks before the message expire').t`${remainingWeeks} weeks`);
-                } else if (remainingDays > 1) {
-                    setShortMessage(c('Remaining days before the message expire').t`${remainingDays} days`);
-                } else if (remainingHours > 1) {
-                    setShortMessage(c('Remaining hours before the message expire').t`${remainingHours} hours`);
+                if (hasEnded) {
+                    setShortMessage('');
+                } else if (remainingYears) {
+                    setShortMessage(
+                        c('Remaining years before the message expire').ngettext(
+                            msgid`${remainingYears} year`,
+                            `${remainingYears} years`,
+                            remainingYears
+                        )
+                    );
+                } else if (remainingDays) {
+                    setShortMessage(
+                        c('Remaining days before the message expire').ngettext(
+                            msgid`${remainingDays} day`,
+                            `${remainingDays} days`,
+                            remainingDays
+                        )
+                    );
+                } else if (remainingHours) {
+                    setShortMessage(
+                        c('Remaining hours before the message expire').ngettext(
+                            msgid`${remainingHours} hour`,
+                            `${remainingHours} hours`,
+                            remainingHours
+                        )
+                    );
                 } else {
                     setShortMessage(c('Remaining time before the message expire').t`<1 hour`);
                 }
