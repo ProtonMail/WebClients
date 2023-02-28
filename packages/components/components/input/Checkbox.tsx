ae85167b4478882dbf2f696d6dacb391d71563f7
Jack Crompton <jack.crompton@proton.ch>
2023-02-28 16:52:34 +0100

    Disable jsx-a11y rules in Checkbox.tsx
    After consulting with Nico, this warning is probably unecessary
    in this case.

--- packages/components/components/input/Checkbox.tsx
+++ packages/components/components/input/Checkbox.tsx
@@ -57,6 +57,7 @@ const Checkbox = (
     }, [indeterminate]);
 
     return (
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
         <label
             {...labelProps}
             htmlFor={id}
