9e41cfb48fc2a3839499b71de6b1c893e684bbdb
Richard Tetaz <richard.tetaz@proton.ch>
2023-03-02 09:06:48 +0100

    Set expiration only on message, not spam/trash

--- applications/mail/src/app/helpers/expiration.ts
+++ applications/mail/src/app/helpers/expiration.ts
@@ -1,13 +1,18 @@
 import { getUnixTime } from 'date-fns';
 
+import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
 import { UserModel } from '@proton/shared/lib/interfaces';
 
-export const canSetExpiration = (featureFlagValue: boolean, user: UserModel) => {
+export const canSetExpiration = (featureFlagValue: boolean, user: UserModel, labelIDs?: string[]) => {
     if (!featureFlagValue) {
         return false;
     }
 
     if (user.isFree) {
+        return false;
+    }
+
+    if (!labelIDs || labelIDs.includes(MAILBOX_LABEL_IDS.SPAM) || labelIDs.includes(MAILBOX_LABEL_IDS.TRASH)) {
         return false;
     }
 
