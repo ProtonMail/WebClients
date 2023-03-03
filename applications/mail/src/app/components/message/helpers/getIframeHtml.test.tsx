7418acc4c6ac20a165fc3019d84355c0177565fc
Nicolas Hoffmann <dante3333@gmail.com>
2023-03-03 17:49:07 +0100

    Remove mark style from print version
    - reseted `mark` styles from print version and from modal
    - added proton-print class to message content root div (to attach styles)
    
    UXE-295
    
    Co-authored-by: @ggonzalez

---
+++ applications/mail/src/app/components/message/helpers/getIframeHtml.test.tsx
@@ -0,0 +1,73 @@
+import { createDocument } from '../../../helpers/test/message';
+import {
+    MESSAGE_IFRAME_PRINT_CLASS,
+    MESSAGE_IFRAME_PRINT_FOOTER_ID,
+    MESSAGE_IFRAME_PRINT_HEADER_ID,
+} from '../constants';
+import getIframeHtml from './getIframeHtml';
+
+describe('getIframeHTML', () => {
+    describe('rich text', () => {
+        it('Should not contain print classes and elements', () => {
+            const document = createDocument('hello buddy');
+            const htmlString = getIframeHtml({
+                emailContent: 'dude',
+                messageDocument: document,
+                isPlainText: false,
+                isPrint: false,
+                themeCSSVariables: '',
+            });
+
+            expect(htmlString).not.toContain(MESSAGE_IFRAME_PRINT_CLASS);
+            expect(htmlString).not.toContain(MESSAGE_IFRAME_PRINT_HEADER_ID);
+            expect(htmlString).not.toContain(MESSAGE_IFRAME_PRINT_FOOTER_ID);
+        });
+
+        it('Should contain print classes and elements', () => {
+            const document = createDocument('hello buddy');
+            const htmlString = getIframeHtml({
+                emailContent: 'dude',
+                messageDocument: document,
+                isPlainText: false,
+                isPrint: true,
+                themeCSSVariables: '',
+            });
+
+            expect(htmlString).toContain(MESSAGE_IFRAME_PRINT_CLASS);
+            expect(htmlString).toContain(MESSAGE_IFRAME_PRINT_HEADER_ID);
+            expect(htmlString).toContain(MESSAGE_IFRAME_PRINT_FOOTER_ID);
+        });
+    });
+
+    describe('plain text', () => {
+        it('Should not contain print classes and elements', () => {
+            const document = createDocument('hello buddy');
+            const htmlString = getIframeHtml({
+                emailContent: 'dude',
+                messageDocument: document,
+                isPlainText: true,
+                isPrint: false,
+                themeCSSVariables: '',
+            });
+
+            expect(htmlString).not.toContain(MESSAGE_IFRAME_PRINT_CLASS);
+            expect(htmlString).not.toContain(MESSAGE_IFRAME_PRINT_HEADER_ID);
+            expect(htmlString).not.toContain(MESSAGE_IFRAME_PRINT_FOOTER_ID);
+        });
+
+        it('Should not contain print classes and elements', () => {
+            const document = createDocument('hello buddy');
+            const htmlString = getIframeHtml({
+                emailContent: 'dude',
+                messageDocument: document,
+                isPlainText: true,
+                isPrint: true,
+                themeCSSVariables: '',
+            });
+
+            expect(htmlString).toContain(MESSAGE_IFRAME_PRINT_CLASS);
+            expect(htmlString).toContain(MESSAGE_IFRAME_PRINT_HEADER_ID);
+            expect(htmlString).toContain(MESSAGE_IFRAME_PRINT_FOOTER_ID);
+        });
+    });
+});
