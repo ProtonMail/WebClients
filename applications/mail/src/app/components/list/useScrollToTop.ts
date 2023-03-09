b2af899f5c794fbdd138568869b5b06f9cc6879c
Richard Tetaz <richard.tetaz@proton.ch>
2023-03-09 09:48:55 +0100

    Review

--- applications/mail/src/app/components/list/useScrollToTop.ts
+++ applications/mail/src/app/components/list/useScrollToTop.ts
@@ -3,7 +3,7 @@ import { DependencyList, RefObject, useEffect } from 'react';
 // Scroll to top of list when page or label changes
 const useScrollToTop = (ref: RefObject<HTMLElement>, dependencies: DependencyList) => {
     const scrollTop = () => {
-        if (ref.current) {
+        if (ref.current && ref.current.scrollTop !== 0) {
             ref.current.scrollTop = 0;
         }
     };
