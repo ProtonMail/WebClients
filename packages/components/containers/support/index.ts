6428c949668fc8328bf36f5b49a09fee6ddaba47
Zlatko Petreski <petreskizlatko0@protonmail.com>
2023-03-15 16:50:11 +0100

    Add free user live chat modal
    IENG-4159

--- packages/components/containers/support/index.ts
+++ packages/components/containers/support/index.ts
@@ -1,2 +1,3 @@
 export { default as BugModal } from './BugModal';
 export { default as AuthenticatedBugModal } from './AuthenticatedBugModal';
+export { default as FreeUserLiveChatModal } from './FreeUserLiveChatModal';
