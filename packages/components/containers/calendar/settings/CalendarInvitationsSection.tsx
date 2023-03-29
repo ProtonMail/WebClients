2a45d5b734055ab81b46304c5c4887a01d55b8cf
Eduardo Conde Pena <econdepe@proton.ch>
2023-03-29 12:18:10 +0200

    Deprecate feature flags for invite locale and auto import invite

--- packages/components/containers/calendar/settings/CalendarInvitationsSection.tsx
+++ packages/components/containers/calendar/settings/CalendarInvitationsSection.tsx
@@ -16,16 +16,9 @@ import InviteLocaleSelector from './InviteLocaleSelector';
 interface Props {
     calendarUserSettings: CalendarUserSettings;
     locales: TtagLocaleMap;
-    hasInviteLocaleFeature: boolean;
-    hasAutoImportInviteFeature: boolean;
 }
 
-const CalendarInvitationsSection = ({
-    calendarUserSettings: { InviteLocale, AutoImportInvite },
-    locales,
-    hasInviteLocaleFeature,
-    hasAutoImportInviteFeature,
-}: Props) => {
+const CalendarInvitationsSection = ({ calendarUserSettings: { InviteLocale, AutoImportInvite }, locales }: Props) => {
     const api = useApi();
     const { call } = useEventManager();
     const { createNotification } = useNotifications();
@@ -43,60 +36,56 @@ const CalendarInvitationsSection = ({
 
     return (
         <SettingsSection>
-            {hasInviteLocaleFeature && (
-                <SettingsLayout>
-                    <SettingsLayoutLeft>
-                        <label className="text-semibold" htmlFor="invite-locale">
-                            {
-                                // translator: the full sentence will be "Send invites in [LANGUAGE]", where [LANGUAGE] is a dropdown with language options. E.g. "Send invites in [ENGLISH]"
-                                c('Label').t`Send in`
-                            }{' '}
-                            <Info
-                                buttonClass="ml0-5 inline-flex"
-                                title={c('Info').t`Event invitations and RSVPs will be sent in this language.`}
-                            />
-                        </label>
-                    </SettingsLayoutLeft>
-                    <SettingsLayoutRight>
-                        <InviteLocaleSelector
-                            id="invite-locale"
-                            locale={displayedLocale}
-                            locales={locales}
-                            loading={loadingInviteLocale}
-                            onChange={(InviteLocale) => withLoadingInviteLocale(handleChange({ InviteLocale }))}
+            <SettingsLayout>
+                <SettingsLayoutLeft>
+                    <label className="text-semibold" htmlFor="invite-locale">
+                        {
+                            // translator: the full sentence will be "Send invites in [LANGUAGE]", where [LANGUAGE] is a dropdown with language options. E.g. "Send invites in [ENGLISH]"
+                            c('Label').t`Send in`
+                        }{' '}
+                        <Info
+                            buttonClass="ml0-5 inline-flex"
+                            title={c('Info').t`Event invitations and RSVPs will be sent in this language.`}
                         />
-                    </SettingsLayoutRight>
-                </SettingsLayout>
-            )}
-            {hasAutoImportInviteFeature && (
-                <SettingsLayout>
-                    <SettingsLayoutLeft>
-                        <label className="text-semibold inline-block" htmlFor="auto-import-invitations">
-                            <span>{c('Auto import invitations setting').jt`Add to calendar and mark as pending`}</span>
-                            <Info
-                                buttonClass="ml0-5 inline-flex"
-                                title={c('Info')
-                                    .t`You still need to reply to invitations for the host to see your response.`}
-                            />
-                        </label>
-                    </SettingsLayoutLeft>
-                    <SettingsLayoutRight className="pt0-5 flex flex-align-items-center">
-                        <Toggle
-                            id="auto-import-invitations"
-                            aria-describedby="auto-import-invitations"
-                            loading={loadingAutoImportInvite}
-                            checked={!!AutoImportInvite}
-                            onChange={({ target }) =>
-                                withLoadingAutoImportInvite(
-                                    handleChange({
-                                        AutoImportInvite: +target.checked,
-                                    })
-                                )
-                            }
+                    </label>
+                </SettingsLayoutLeft>
+                <SettingsLayoutRight>
+                    <InviteLocaleSelector
+                        id="invite-locale"
+                        locale={displayedLocale}
+                        locales={locales}
+                        loading={loadingInviteLocale}
+                        onChange={(InviteLocale) => withLoadingInviteLocale(handleChange({ InviteLocale }))}
+                    />
+                </SettingsLayoutRight>
+            </SettingsLayout>
+            <SettingsLayout>
+                <SettingsLayoutLeft>
+                    <label className="text-semibold inline-block" htmlFor="auto-import-invitations">
+                        <span>{c('Auto import invitations setting').jt`Add to calendar and mark as pending`}</span>
+                        <Info
+                            buttonClass="ml0-5 inline-flex"
+                            title={c('Info')
+                                .t`You still need to reply to invitations for the host to see your response.`}
                         />
-                    </SettingsLayoutRight>
-                </SettingsLayout>
-            )}
+                    </label>
+                </SettingsLayoutLeft>
+                <SettingsLayoutRight className="pt0-5 flex flex-align-items-center">
+                    <Toggle
+                        id="auto-import-invitations"
+                        aria-describedby="auto-import-invitations"
+                        loading={loadingAutoImportInvite}
+                        checked={!!AutoImportInvite}
+                        onChange={({ target }) =>
+                            withLoadingAutoImportInvite(
+                                handleChange({
+                                    AutoImportInvite: +target.checked,
+                                })
+                            )
+                        }
+                    />
+                </SettingsLayoutRight>
+            </SettingsLayout>
         </SettingsSection>
     );
 };
