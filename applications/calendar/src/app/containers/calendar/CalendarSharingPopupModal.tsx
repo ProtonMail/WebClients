7471cfbf358d4f3f6d77f9971a34ee448dd83711
Eduardo Conde Pena <econdepe@proton.ch>
2023-04-03 11:00:14 +0200

    Add image for calendar sharing popup
    CALWEB-4264

--- applications/calendar/src/app/containers/calendar/CalendarSharingPopupModal.tsx
+++ applications/calendar/src/app/containers/calendar/CalendarSharingPopupModal.tsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 
 import { c } from 'ttag';
 
-import { Button } from '@proton/atoms/Button';
 import { Href } from '@proton/atoms/Href';
 import {
     ModalTwo as Modal,
@@ -10,9 +9,11 @@ import {
     ModalTwoFooter as ModalFooter,
     ModalTwoHeader as ModalHeader,
     ModalProps,
+    PrimaryButton,
 } from '@proton/components/components';
 import { BRAND_NAME } from '@proton/shared/lib/constants';
 import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
+import img from '@proton/styles/assets/img/illustrations/calendar-share.svg';
 
 interface Props extends ModalProps {
     onDisplayed: () => void;
@@ -26,15 +27,16 @@ const CalendarSharingPopupModal = ({ onDisplayed, ...rest }: Props) => {
     }, [rest.open]);
 
     return (
-        <Modal {...rest} size="medium">
+        <Modal {...rest} size="small">
             <ModalHeader title={c('Title; calendar sharing pop-up').t`Planning just got easier!`} />
             <ModalContent>
-                <p>{c('Info; calendar sharing pop-up')
-                    .t`Invite others with a ${BRAND_NAME} account to view or edit events in your calendar.`}</p>
+                <img className="w100" src={img} alt="" />
+                <div className="my1">{c('Info; calendar sharing pop-up')
+                    .t`Invite others with a ${BRAND_NAME} account to view or edit events in your calendar.`}</div>
                 <Href href={getKnowledgeBaseUrl('/share-calendar-with-proton-users')}>{c('Link').t`Learn more`}</Href>
             </ModalContent>
             <ModalFooter className="flex-justify-end">
-                <Button onClick={rest.onClose}>{c('Action').t`Got it`}</Button>
+                <PrimaryButton onClick={rest.onClose}>{c('Action').t`Got it`}</PrimaryButton>
             </ModalFooter>
         </Modal>
     );
