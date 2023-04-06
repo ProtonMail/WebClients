7eb309b44bacb063cf2c4a91c2d0c36d1eb599b5
lpeslier <lucien.peslier@proton.ch>
2023-04-06 18:00:18 +0200

    Refactor to remove view logic from store

--- applications/drive/src/app/store/_revisions/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './interface';
-
-export { RevisionsProvider, useRevisionsProvider as useRevisions } from './RevisionsProvider';
--- applications/drive/src/app/store/_revisions/interface.ts
+++
@@ -1,7 +0,0 @@
-import { DriveFileRevision } from '@proton/shared/lib/interfaces/drive/file';
-
-export interface RevisionsProviderState {
-    openRevisionPreview: (revision: DriveFileRevision) => void;
-    downloadRevision: (revision: DriveFileRevision) => void;
-    havePreviewAvailable: boolean;
-}
