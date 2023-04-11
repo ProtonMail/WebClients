440f18dad0b4d4db57197ffa031ee1a35369dbf8
romain sanchez <romain.sanchez@proton.ch>
2023-04-11 14:10:27 +0200

    Add user.ID in localStorage so that we reopen the drawer only when previous user is the current user CALWEB-4331

--- packages/components/hooks/drawer/useDrawerLocalStorage.ts
+++ packages/components/hooks/drawer/useDrawerLocalStorage.ts
@@ -1,10 +1,13 @@
 import { useEffect, useRef } from 'react';
 
+import { useGetUser } from '@proton/components/hooks';
 import { LOCALSTORAGE_DRAWER_KEY } from '@proton/shared/lib/drawer/constants';
 import { DRAWER_APPS, DrawerLocalStorageValue, IframeSrcMap } from '@proton/shared/lib/drawer/interfaces';
 import { removeItem, setItem } from '@proton/shared/lib/helpers/storage';
 
 const useDrawerLocalStorage = (iframeSrcMap: IframeSrcMap, appInView?: DRAWER_APPS) => {
+    const getUser = useGetUser();
+
     /**
      * Use a ref to allow deletion of the localStorage value only if we opened an app during the session.
      * Otherwise, when loading the app, no app is opened in the drawer, and we would delete the value stored.
@@ -16,16 +19,21 @@ const useDrawerLocalStorage = (iframeSrcMap: IframeSrcMap, appInView?: DRAWER_AP
         setItem(LOCALSTORAGE_DRAWER_KEY, JSON.stringify(item));
     };
 
-    useEffect(() => {
+    const handleSetLocalStorage = async () => {
         if (appInView) {
             const url = iframeSrcMap[appInView];
+            const user = await getUser();
             hasSetAppInView.current = true;
-            const item = { app: appInView, url } as DrawerLocalStorageValue;
+            const item = { app: appInView, url, userID: user.ID } as DrawerLocalStorageValue;
             setDrawerLocalStorageKey(item);
         } else if (hasSetAppInView.current) {
             // When closing the drawer, clean up the value from the drawer local storage item
             removeItem(LOCALSTORAGE_DRAWER_KEY);
         }
+    };
+
+    useEffect(() => {
+        void handleSetLocalStorage();
     }, [appInView]);
 
     return { setDrawerLocalStorageKey };
