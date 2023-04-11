8fa65782bf8ebc3bd8139dfc2b66c6bcf974a7d2
M. Thiercelin <marin.thiercelin@proton.ch>
2023-04-11 13:00:31 +0200

    Read feature flag when checking absence proofs.
    The KT feature flag was bypassed when
    checking absence proof during key migration.

--- packages/components/containers/keyTransparency/index.ts
+++ packages/components/containers/keyTransparency/index.ts
@@ -1,4 +1,5 @@
 export { default as KeyTransparencyManager } from './KeyTransparencyManager';
 export { default as createPreAuthKTVerifier } from './createPreAuthKTVerifier';
 export { default as useKTVerifier } from './useKTVerifier';
+export { default as useKTAbsenceVerifier } from './useKTAbsenceVerifier';
 export * from './useKeyTransparencyContext';
