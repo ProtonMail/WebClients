ebf1affb5e75c30143fa4e9e963fbb11799b618c
Krzysztof Stępniak <krzysztof.stepniak@proton.ch>
2023-04-12 14:02:10 +0200

    Buy vpn plus with braintree card e2e test

--- packages/components/containers/paymentMethods/PaymentMethodState.tsx
+++ packages/components/containers/paymentMethods/PaymentMethodState.tsx
@@ -14,14 +14,14 @@ interface Props {
 const PaymentMethodState = ({ method, index }: Props) => {
     if (method.Type === PAYMENT_METHOD_TYPES.CARD && isExpired(method.Details)) {
         return (
-            <Badge type="error">{`${c('Label on payment method').t`Expired`} ${method.Details.ExpMonth}/${
-                method.Details.ExpYear
-            }`}</Badge>
+            <Badge type="error" data-testid="expired">{`${c('Label on payment method').t`Expired`} ${
+                method.Details.ExpMonth
+            }/${method.Details.ExpYear}`}</Badge>
         );
     }
 
     if (!index) {
-        return <Badge type="primary">{c('Label on payment method').t`Default`}</Badge>;
+        return <Badge type="primary" data-testid="default-badge">{c('Label on payment method').t`Default`}</Badge>;
     }
 
     return null;
