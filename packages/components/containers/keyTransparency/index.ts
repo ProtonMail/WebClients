4bbbb728102bc25d790e4800934815ebe8cf5d72
M. Thiercelin <marin.thiercelin@proton.ch>
2023-04-13 17:52:34 +0200

    Fix the KT checks before key migration and key upgrade.
    When the client triggers key migration, it
    needs to check that the KT state is correct before
    submitting a new SKL to the server.
    The checks were wrongly assuming that all non migrated
    addresses were absent in the KT epoch.

--- packages/components/containers/keyTransparency/index.ts
+++ packages/components/containers/keyTransparency/index.ts
@@ -1,5 +1,6 @@
 export { default as KeyTransparencyManager } from './KeyTransparencyManager';
 export { default as createPreAuthKTVerifier } from './createPreAuthKTVerifier';
 export { default as useKTVerifier } from './useKTVerifier';
-export { default as useKTAbsenceVerifier } from './useKTAbsenceVerifier';
+export { default as useKeyMigrationKTVerifier } from './useKeyMigrationKTVerifier';
+export { default as createKeyMigrationKTVerifier } from './createKeyMigrationKTVerifier';
 export * from './useKeyTransparencyContext';
