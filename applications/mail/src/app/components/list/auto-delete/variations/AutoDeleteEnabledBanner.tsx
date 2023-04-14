8f8132acd2d4ecb69b262efc83eda5e3b584554b
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2023-04-14 18:00:37 +0200

    Fix auto-delete banner spacing
    Co-authored-by: Nicolas Hoffmann <dante3333@gmail.com>

--- applications/mail/src/app/components/list/auto-delete/variations/AutoDeleteEnabledBanner.tsx
+++ applications/mail/src/app/components/list/auto-delete/variations/AutoDeleteEnabledBanner.tsx
@@ -1,14 +1,17 @@
 import { c } from 'ttag';
 
 import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
+import clsx from '@proton/utils/clsx';
 
 import { AutoDeleteLabelIDs } from '../interface';
 
 interface Props {
     labelID: AutoDeleteLabelIDs;
+    columnLayout: boolean;
+    isCompactView: boolean;
 }
 
-const AutoDeleteEnabledBanner = ({ labelID }: Props) => {
+const AutoDeleteEnabledBanner = ({ labelID, columnLayout, isCompactView }: Props) => {
     const message: string = (() => {
         switch (labelID) {
             case MAILBOX_LABEL_IDS.TRASH:
@@ -19,7 +22,14 @@ const AutoDeleteEnabledBanner = ({ labelID }: Props) => {
     })();
 
     return (
-        <div data-testid="auto-delete:banner:enabled" className="p0-5 color-hint text-center text-sm">
+        <div
+            data-testid="auto-delete:banner:enabled"
+            className={clsx(
+                'p0-5 color-hint text-center text-sm auto-delete-banner-enabled',
+                columnLayout && 'auto-delete-banner-enabled--column',
+                isCompactView && 'auto-delete-banner-enabled--compact'
+            )}
+        >
             {message}
         </div>
     );
