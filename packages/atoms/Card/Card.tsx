b55082b2b506804aa2a2b9b47b4fa47ebbf3b9d3
Jack Crompton <jack.crompton@proton.ch>
2023-04-18 17:00:52 +0200

    Add upgrade banner to PassDownloadsSettingsPage

--- packages/atoms/Card/Card.tsx
+++ packages/atoms/Card/Card.tsx
@@ -24,7 +24,7 @@ const Card = <E extends ElementType = typeof defaultElement>({
     const Element: ElementType = as || defaultElement;
     return (
         <Element
-            className={clsx('p1', className, rounded && 'rounded', bordered && 'border', background && 'bg-weak')}
+            className={clsx('p-4', className, rounded && 'rounded', bordered && 'border', background && 'bg-weak')}
             {...rest}
         />
     );
