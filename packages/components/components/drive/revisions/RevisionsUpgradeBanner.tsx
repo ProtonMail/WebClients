d069e0544a970481b3aebc3d2c0d180a004ef6ad
lpeslier <lucien.peslier@proton.ch>
2023-04-18 15:30:57 +0200

    Set upgrade link to account setting page

--- packages/components/components/drive/revisions/RevisionsUpgradeBanner.tsx
+++ packages/components/components/drive/revisions/RevisionsUpgradeBanner.tsx
@@ -1,14 +1,21 @@
 import { c } from 'ttag';
 
-import { ButtonLike } from '@proton/atoms/Button';
-import { DRIVE_PRICING_PAGE } from '@proton/shared/lib/drive/urls';
+import { ButtonLike } from '@proton/atoms';
+import { AppLink } from '@proton/components';
+import { APPS } from '@proton/shared/lib/constants';
 
 const RevisionsUpgradeBanner = () => {
     return (
         <div className="bg-weak py-4 px-4 rounded flex flex-align-items-center flex-justify-space-between">
             <p className="m0 my-1">{c('Info').t`Upgrade to extended version history beyond 7 days`}</p>
-            <ButtonLike className="my-1" as="a" href={DRIVE_PRICING_PAGE} color="norm" size="small">{c('Action')
-                .t`Upgrade`}</ButtonLike>
+            <ButtonLike
+                className="my-1"
+                as={AppLink}
+                to="/drive/upgrade"
+                toApp={APPS.PROTONACCOUNT}
+                color="norm"
+                size="small"
+            >{c('Action').t`Upgrade`}</ButtonLike>
         </div>
     );
 };
