3fbeb78e943a32d774f434b65797d5904d04692e
Edvin CANDON <edvin.candon@proton.ch>
2023-04-20 17:41:27 +0200

    Support optional `note` column in chrome importer
    [IDTEAM-1145]

--- packages/pass/import/providers/chrome.reader.spec.ts
+++ packages/pass/import/providers/chrome.reader.spec.ts
@@ -5,10 +5,12 @@ import { ItemImportIntent } from '@proton/pass/types';
 import { readChromeData } from './chrome.reader';
 
 describe('Import Chrome CSV', () => {
-    let sourceData: string;
+    let chromeExport: string;
+    let chromeExportWindows: string;
 
     beforeAll(async () => {
-        sourceData = await fs.promises.readFile(__dirname + '/mocks/chrome.csv', 'utf8');
+        chromeExport = await fs.promises.readFile(__dirname + '/mocks/chrome.csv', 'utf8');
+        chromeExportWindows = await fs.promises.readFile(__dirname + '/mocks/chrome-windows.csv', 'utf8');
     });
 
     it('should handle corrupted files', async () => {
@@ -16,7 +18,45 @@ describe('Import Chrome CSV', () => {
     });
 
     it('transforms chrome csv into ImportPayload', async () => {
-        const payload = await readChromeData(sourceData);
+        const payload = await readChromeData(chromeExport);
+        const [vaultData] = payload;
+
+        expect(payload.length).toEqual(1);
+        expect(vaultData.type).toEqual('new');
+        expect(vaultData.type === 'new' && vaultData.vaultName).not.toBeUndefined();
+
+        const { items } = vaultData;
+
+        /* Login */
+        const loginItem1 = items[0] as ItemImportIntent<'login'>;
+        expect(loginItem1.type).toEqual('login');
+        expect(loginItem1.metadata.name).toEqual('proton.me');
+        expect(loginItem1.metadata.note).toEqual('');
+        expect(loginItem1.content.username).toEqual('nobody@proton.me');
+        expect(loginItem1.content.password).toEqual('proton123');
+        expect(loginItem1.content.urls[0]).toEqual('https://account.proton.me');
+
+        /* Login */
+        const loginItem2 = items[1] as ItemImportIntent<'login'>;
+        expect(loginItem2.type).toEqual('login');
+        expect(loginItem2.metadata.name).toEqual('missing url');
+        expect(loginItem2.metadata.note).toEqual('');
+        expect(loginItem2.content.username).toEqual('missingurl@proton.me');
+        expect(loginItem2.content.password).toEqual('proton123');
+        expect(loginItem2.content.urls).toStrictEqual([]);
+
+        /* Login */
+        const loginItem3 = items[2] as ItemImportIntent<'login'>;
+        expect(loginItem3.type).toEqual('login');
+        expect(loginItem3.metadata.name).toEqual('missing password');
+        expect(loginItem3.metadata.note).toEqual('');
+        expect(loginItem3.content.username).toEqual('missingpw@proton.me');
+        expect(loginItem3.content.password).toEqual('');
+        expect(loginItem3.content.urls[0]).toEqual('https://account.proton.me');
+    });
+
+    it('transforms chrome csv (windows) into ImportPayload', async () => {
+        const payload = await readChromeData(chromeExportWindows);
         const [vaultData] = payload;
 
         expect(payload.length).toEqual(1);
