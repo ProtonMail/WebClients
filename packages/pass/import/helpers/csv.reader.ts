9351fe0b05a78125070d1c8f5af4e17451f21177
Edvin CANDON <edvin.candon@proton.ch>
2023-04-22 16:13:50 +0200

    Improve CSV parser
    - Avoid throwing on header index mistmatch
    - Give more context to errors during CSV parsing
    - Throw when expected headers are missing
    - Add unit tests
    
    [IDTEAM-1168]

--- packages/pass/import/helpers/csv.reader.ts
+++ packages/pass/import/helpers/csv.reader.ts
@@ -1,18 +1,40 @@
 import Papa from 'papaparse';
 import { c } from 'ttag';
 
-export const readCSV = async <T>(data: string, headers: (keyof T)[]): Promise<T[]> =>
-    new Promise<T[]>((resolve, reject) =>
-        Papa.parse<T>(data, {
-            header: true,
-            skipEmptyLines: true,
-            transformHeader: (header, index) => {
-                if (header !== headers?.[index]) {
-                    throw new Error(c('Error').t`Invalid .csv file`);
-                }
-                return header;
-            },
-            complete: ({ errors, data }) => (errors.length > 0 ? reject() : resolve(data)),
-            error: (err: Error) => reject(err),
-        })
-    );
+import { ImportReaderError } from './reader.error';
+
+export const readCSV = async <T extends Record<string, any>>(
+    data: string,
+    expectedHeaders: (keyof T)[]
+): Promise<T[]> => {
+    try {
+        return await new Promise<T[]>((resolve, reject) => {
+            Papa.parse<T>(data, {
+                header: true,
+                skipEmptyLines: true,
+                delimiter: ',',
+                complete: ({ data, errors }) => {
+                    if (errors.length > 0) {
+                        const errorDetails = errors.map((err) => err.message).join(', ');
+                        return reject(errorDetails);
+                    }
+
+                    if (data.length === 0) return reject(c('Error').t`Empty CSV file`);
+
+                    const headers = Object.keys(data[0]);
+                    const missed = (expectedHeaders as string[]).filter((header) => !headers.includes(header));
+
+                    if (missed.length > 0) {
+                        const missingHeaders = missed.join(', ');
+                        return reject(c('Error').t`CSV file is missing expected headers: ${missingHeaders}`);
+                    }
+
+                    return resolve(data);
+                },
+                error: (err: Error) => reject(err.message),
+            });
+        });
+    } catch (err: any) {
+        throw new ImportReaderError(err);
+    }
+};
