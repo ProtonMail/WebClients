532dc07610a6035dea73361c17912f59e6685b17
mmso <mmso@mmso.se>
2023-04-24 13:28:13 +0200

    Add VPN SEO pages

---
+++ applications/vpn-settings/src/pages/reset-password.js
@@ -0,0 +1,5 @@
+const data = require('proton-account/src/pages/reset-password.json');
+
+module.exports = {
+    ...data,
+};
---
+++ applications/vpn-settings/src/pages/signup.js
@@ -0,0 +1,6 @@
+const data = require('proton-account/src/pages/vpn.signup.json');
+
+module.exports = {
+    ...data,
+    pathname: '/signup',
+};
