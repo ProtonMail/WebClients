efe90d3d2b8617c14a8664acda6d0e2eeeffb92d
lpeslier <lucien.peslier@proton.ch>
2023-04-24 11:23:04 +0200

    Create NewFeatureTag component

--- packages/components/components/index.ts
+++ packages/components/components/index.ts
@@ -42,6 +42,7 @@ export * from './logo';
 export * from './miniCalendar';
 export * from './modal';
 export * from './modalTwo';
+export * from './newFeatureTag';
 export * from './option';
 export * from './orderable';
 export * from './orderableTable';
