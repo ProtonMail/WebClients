46aa3bab7b44f955e2f9aee338227bea3e8ac389
lpeslier <lucien.peslier@proton.ch>
2023-04-25 18:53:28 +0200

    Add Revision restore feature

--- applications/drive/src/app/store/_views/useRevisionsView.ts
+++ applications/drive/src/app/store/_views/useRevisionsView.ts
@@ -1,8 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { useLoading } from '@proton/components/hooks';
-import { queryDeleteFileRevision, queryFileRevisions } from '@proton/shared/lib/api/drive/files';
 import {
+    queryDeleteFileRevision,
+    queryFileRevisions,
+    queryRestoreFileRevision,
+} from '@proton/shared/lib/api/drive/files';
+import {
+    DriveFileRestoreRevisionResult,
     DriveFileRevision,
     DriveFileRevisionsResult,
     FileRevisionState,
@@ -20,15 +25,22 @@ export default function useRevisionsView(shareId: string, linkId: string) {
     const [isLoading, withLoading] = useLoading(true);
     const [revisions, setRevisions] = useState<DriveFileRevision[]>([]);
 
+    const loadRevisions = useCallback(
+        (abortSignal: AbortSignal) => {
+            void withLoading(
+                debouncedRequest<DriveFileRevisionsResult>(queryFileRevisions(shareId, linkId), abortSignal).then(
+                    (result) => {
+                        setRevisions(filterRevisions(result.Revisions));
+                    }
+                )
+            );
+        },
+        [shareId, linkId]
+    );
+
     useEffect(() => {
         const ac = new AbortController();
-        void withLoading(
-            debouncedRequest<DriveFileRevisionsResult>(queryFileRevisions(shareId, linkId), ac.signal).then(
-                (result) => {
-                    setRevisions(filterRevisions(result.Revisions));
-                }
-            )
-        );
+        loadRevisions(ac.signal);
         return () => {
             ac.abort();
         };
@@ -38,9 +50,22 @@ export default function useRevisionsView(shareId: string, linkId: string) {
         setRevisions(revisions.filter((revision) => revision.ID !== revisionId));
     };
 
+    const restoreRevision = async (abortSignal: AbortSignal, revisionId: string) => {
+        const { Code } = await debouncedRequest<DriveFileRestoreRevisionResult>(
+            queryRestoreFileRevision(shareId, linkId, revisionId),
+            abortSignal
+        );
+        // If restore happened immediately fetch updated revision list
+        if (Code === 1000) {
+            loadRevisions(abortSignal);
+        }
+        return Code;
+    };
+
     return {
         isLoading,
         revisions,
         deleteRevision,
+        restoreRevision,
     };
 }
