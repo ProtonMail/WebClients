ca8e92791cb89f22936fb26d009b0ee0f9c70c82
Edvin CANDON <edvin.candon@proton.ch>
2023-04-25 17:25:17 +0200

    Show import recap in Settings export tab
    - Reflect ImportPayload changes accross UI codebase
    - Update `useImportForm` to return import result
    
    [IDTEAM-1201]

--- applications/pass-extension/src/pages/settings/views/Import.tsx
+++ applications/pass-extension/src/pages/settings/views/Import.tsx
@@ -1,7 +1,7 @@
 import { type VFC, useCallback, useRef, useState } from 'react';
 
 import { Form, FormikProvider } from 'formik';
-import { c } from 'ttag';
+import { c, msgid } from 'ttag';
 
 import { Button } from '@proton/atoms/Button';
 import { Card } from '@proton/atoms/Card';
@@ -11,7 +11,12 @@ import type { MaybeNull } from '@proton/pass/types';
 import { pipe, tap } from '@proton/pass/utils/fp';
 import { PASS_APP_NAME } from '@proton/shared/lib/constants';
 
-import { ImportForm, ImportVaultsPicker, type ImportVaultsPickerHandle } from '../../../shared/components/import';
+import {
+    ImportForm,
+    ImportVaultsPicker,
+    type ImportVaultsPickerHandle,
+    PROVIDER_TITLE_MAP,
+} from '../../../shared/components/import';
 import {
     type UseImportFormBeforeSubmit,
     type UseImportFormBeforeSubmitValue,
@@ -39,50 +44,105 @@ export const Import: VFC = () => {
         []
     );
 
-    const { form, dropzone, busy } = useImportForm({ beforeSubmit });
+    const { form, dropzone, busy, result } = useImportForm({ beforeSubmit });
 
     return (
-        <Card rounded className="mb-4 p-3 relative">
-            <strong className="color-norm block">{c('Label').t`Import`}</strong>
-            <em className="block text-sm color-weak mt-1">
-                {c('Info')
-                    .t`In order to migrate your data to ${PASS_APP_NAME} from another password manager, export your passwords from your current provider and import them using the form below. Once all your items have been imported, delete the previously exported file.`}
-            </em>
+        <>
+            <Card rounded className="mb-4 p-3 relative">
+                <strong className="color-norm block">{c('Label').t`Import`}</strong>
+                <em className="block text-sm color-weak mt-1">
+                    {c('Info')
+                        .t`In order to migrate your data to ${PASS_APP_NAME} from another password manager, export your passwords from your current provider and import them using the form below. Once all your items have been imported, delete the previously exported file.`}
+                </em>
 
-            <hr className="mt-2 mb-4 border-weak" />
+                <hr className="mt-2 mb-4 border-weak" />
 
-            <FormikProvider value={form}>
-                <Form className="modal-two-dialog-container">
-                    <ImportForm form={form} dropzone={dropzone} busy={busy} />
-                    <Button className="mt-3" type="submit" disabled={busy || !form.isValid} loading={busy} color="norm">
-                        {busy ? c('Action').t`Importing` : c('Action').t`Import`}
-                    </Button>
-                </Form>
-            </FormikProvider>
-
-            {importData && (
-                <ModalTwo onClose={reset} onReset={reset} open size={'medium'} className="mt-10">
-                    <ModalTwoHeader title={c('Title').t`Import to vaults`} />
-                    <ModalTwoContent>
-                        <ImportVaultsPicker
-                            ref={vaultPickerRef}
-                            payload={importData}
-                            onSubmit={(payload) =>
-                                beforeSubmitResolver?.current?.(
-                                    payload.length === 0 ? { ok: false } : { ok: true, payload }
-                                )
-                            }
-                        />
-                    </ModalTwoContent>
-                    <ModalTwoFooter>
-                        <Button type="reset" onClick={reset} color="danger">
-                            {c('Action').t`Cancel`}
+                <FormikProvider value={form}>
+                    <Form className="modal-two-dialog-container">
+                        <ImportForm form={form} dropzone={dropzone} busy={busy} />
+                        <Button
+                            className="mt-3"
+                            type="submit"
+                            disabled={busy || !form.isValid}
+                            loading={busy}
+                            color="norm"
+                        >
+                            {busy ? c('Action').t`Importing` : c('Action').t`Import`}
                         </Button>
-                        <Button type="button" color="norm" onClick={() => vaultPickerRef.current?.submit()}>{c('Action')
-                            .t`Proceed`}</Button>
-                    </ModalTwoFooter>
-                </ModalTwo>
+                    </Form>
+                </FormikProvider>
+
+                {importData !== null && (
+                    <ModalTwo onClose={reset} onReset={reset} open size={'medium'} className="mt-10">
+                        <ModalTwoHeader title={c('Title').t`Import to vaults`} />
+                        <ModalTwoContent>
+                            <ImportVaultsPicker
+                                ref={vaultPickerRef}
+                                payload={importData}
+                                onSubmit={(payload) =>
+                                    beforeSubmitResolver?.current?.(
+                                        payload.vaults.length === 0 ? { ok: false } : { ok: true, payload }
+                                    )
+                                }
+                            />
+                        </ModalTwoContent>
+                        <ModalTwoFooter>
+                            <Button type="reset" onClick={reset} color="danger">
+                                {c('Action').t`Cancel`}
+                            </Button>
+                            <Button type="button" color="norm" onClick={() => vaultPickerRef.current?.submit()}>{c(
+                                'Action'
+                            ).t`Proceed`}</Button>
+                        </ModalTwoFooter>
+                    </ModalTwo>
+                )}
+            </Card>
+
+            {result && (
+                <Card rounded className="mt-4 p-3">
+                    <strong className="color-weak block">{c('Label').t`Latest import`}</strong>
+                    <hr className="mt-2 mb-4 border-weak" />
+                    <div className="flex flex-column gap-y-1 text-sm">
+                        <div>
+                            <span className="color-weak">{c('Label').t`Imported from : `}</span>
+                            <span className="rounded bg-primary px-1 user-select-none">
+                                {PROVIDER_TITLE_MAP[result.provider]}
+                            </span>
+                        </div>
+
+                        <div>
+                            <span className="color-weak">{c('Label').t`Imported on : `}</span>
+                            <span>{new Date(result.importedAt * 1000).toLocaleString()}</span>
+                        </div>
+
+                        <div>
+                            <span className="color-weak">{c('Label').t`Total imported items : `}</span>
+                            <span>
+                                {c('Info').ngettext(msgid`${result.total} item`, `${result.total} items`, result.total)}
+                            </span>
+                        </div>
+
+                        {result.ignored.length > 0 && (
+                            <div className="bg-norm rounded-sm p-3 mt-2">
+                                <span className="mb-2 block">
+                                    {c('Info').ngettext(
+                                        msgid`The following item could not be imported :`,
+                                        `The following ${result.ignored.length} items could not be imported`,
+                                        result.ignored.length
+                                    )}
+                                </span>
+                                <div className="color-weak scroll-if-needed" style={{ maxHeight: 150 }}>
+                                    {result.ignored.map((description, i) => (
+                                        <span className="block" key={i}>
+                                            {description}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </Card>
             )}
-        </Card>
+        </>
     );
 };
