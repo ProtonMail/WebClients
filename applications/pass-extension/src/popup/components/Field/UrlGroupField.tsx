e2577e7a2fd291d40b8e1bacc3f5f58cb7cd2e4d
Edvin CANDON <edvin.candon@proton.ch>
2023-04-26 19:14:11 +0200

    Simplify `UrlGroupField`
    - Handle duplicate url detection in final urls
    - Improve UX: handle onKeyPress enter & blur events
    - Improve design: avoid flickering glitches due to close button size
    - Simplify flow: only show add button when active field is being written
      to and handle disabled state when active field is in error
    
    [IDTEAM-1167]

--- applications/pass-extension/src/popup/components/Field/UrlGroupField.tsx
+++ applications/pass-extension/src/popup/components/Field/UrlGroupField.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { type KeyboardEvent, useRef } from 'react';
 
 import { FieldArray, type FormikContextType, type FormikErrors } from 'formik';
 import { c } from 'ttag';
@@ -6,56 +6,42 @@ import uniqid from 'uniqid';
 
 import { Button } from '@proton/atoms';
 import { Icon, InputFieldTwo } from '@proton/components/';
+import { duplicates } from '@proton/pass/utils/array';
 import { isEmptyString } from '@proton/pass/utils/string';
 import { isValidURL } from '@proton/pass/utils/url';
 
 import { FieldBox } from './Layout/FieldBox';
 
-type UrlItem = { url: string; id: string };
+export type UrlItem = { url: string; id: string };
+export type UrlGroupValues = { url: string; urls: UrlItem[] };
+export type UrlGroupProps<V extends UrlGroupValues = UrlGroupValues> = { form: FormikContextType<V> };
 
-export type UrlGroupValues = {
-    url: string;
-    urls: UrlItem[];
-};
+export const createNewUrl = (url: string) => ({ id: uniqid(), url: isValidURL(url).valid ? url : '' });
 
-type UrlGroupProps<V extends UrlGroupValues = UrlGroupValues> = {
-    form: FormikContextType<V>;
-};
-
-export const createNewUrl = (url: string) => ({
-    id: uniqid(),
-    url: isValidURL(url).url,
-});
-
+/* validates the active URL input field */
 export const validateUrl = <V extends UrlGroupValues>({ url, urls }: V) => {
     if (!isEmptyString(url)) {
         const { valid: validURL, url: safeUrl } = isValidURL(url);
         const urlExists = urls.map(({ url }) => url).includes(safeUrl);
 
-        if (!validURL) {
-            return { url: c('Validation').t`Url is invalid` };
-        }
-
-        if (urlExists) {
-            return { url: c('Validation').t`Url already exists` };
-        }
+        if (!validURL) return { url: c('Validation').t`Url is invalid` };
+        if (urlExists) return { url: c('Validation').t`Url already exists` };
     }
 
     return {};
 };
 
+/* validates the actual URLs list */
 export const validateUrls = <V extends UrlGroupValues>({ urls }: V) => {
+    const duplicatesCount = duplicates(urls.map((item) => item.url));
+
     const urlsErrors = urls.map(({ url }) => {
         const isEmpty = isEmptyString(url);
-        const { valid: validURL } = isValidURL(url);
+        const { valid: validURL, url: safeUrl } = isValidURL(url);
 
-        if (isEmpty) {
-            return { url: c('Validation').t`Url cannot be empty` };
-        }
-
-        if (!validURL) {
-            return { url: c('Validation').t`Url is invalid` };
-        }
+        if ((duplicatesCount.get(safeUrl) ?? 0) > 1) return { url: c('Validation').t`Duplicated url` };
+        if (isEmpty) return { url: c('Validation').t`Url cannot be empty` };
+        if (!validURL) return { url: c('Validation').t`Url is invalid` };
 
         return {};
     });
@@ -64,14 +50,15 @@ export const validateUrls = <V extends UrlGroupValues>({ urls }: V) => {
 };
 
 export const UrlGroupField = <T extends UrlGroupValues>({ form }: UrlGroupProps<T>) => {
-    const { values, errors, handleChange } = form;
     const inputRef = useRef<HTMLInputElement>(null);
-    const [showExtraField, setShowExtraField] = useState(!values.urls.length);
-    const showAddButton = !!values.url || values.urls.length > 0;
+    const { values, errors, handleChange } = form;
 
-    useEffect(() => {
-        setShowExtraField(!values.urls.length);
-    }, [values.urls.length]);
+    const onKeyEnter = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            event.preventDefault(); /* avoid submitting the form */
+            event.currentTarget.blur();
+        }
+    };
 
     return (
         <FieldBox icon={<Icon name="earth" size={20} style={{ color: 'var(--fieldset-cluster-icon-color)' }} />}>
@@ -81,25 +68,21 @@ export const UrlGroupField = <T extends UrlGroupValues>({ form }: UrlGroupProps<
             >
                 <span className="field-two-label color-weak text-sm">{c('Label').t`Websites`}</span>
             </label>
+
             <FieldArray
                 name="urls"
                 render={(helpers) => {
-                    const handleReplace = (index: number) => (url: string) => {
-                        const { id } = values.urls[index];
-
-                        helpers.replace(index, { id, url });
-                    };
-
                     const handleRemove = helpers.handleRemove;
 
-                    const handleAdd = () => {
-                        if (values.url && !errors.url) {
-                            helpers.push(createNewUrl(values.url));
-                            form.setFieldValue('url', '');
-                        }
+                    const handleReplace = (index: number) => (url: string) =>
+                        helpers.replace(index, { id: values.urls[index].id, url });
 
-                        setShowExtraField(true);
-                        inputRef.current?.focus();
+                    const handleAdd = (url: string) => {
+                        if (!errors.url) {
+                            helpers.push(createNewUrl(isValidURL(url).url));
+                            form.setFieldValue('url', '');
+                            inputRef.current?.focus();
+                        }
                     };
 
                     return (
@@ -111,22 +94,24 @@ export const UrlGroupField = <T extends UrlGroupValues>({ form }: UrlGroupProps<
                                             <InputFieldTwo
                                                 error={(errors.urls?.[index] as FormikErrors<UrlItem>)?.url}
                                                 onValue={handleReplace(index)}
-                                                onBlur={() => helpers.replace(index, { id, url: isValidURL(url).url })}
+                                                onBlur={() => handleReplace(index)(isValidURL(url).url)}
                                                 value={url}
                                                 unstyled
                                                 assistContainerClassName="hidden-empty"
                                                 inputClassName="color-norm p-0 rounded-none"
                                                 placeholder="https://"
+                                                onKeyDown={onKeyEnter}
                                             />
                                             <Button
                                                 icon
                                                 pill
-                                                className="flex-item-noshrink ml-2"
+                                                className="flex-item-noshrink flex-align-self-start ml-2"
                                                 color="weak"
                                                 shape="ghost"
                                                 size="small"
                                                 title={c('Action').t`Delete`}
                                                 onClick={handleRemove(index)}
+                                                style={{ padding: 0 }}
                                             >
                                                 <Icon name="cross" />
                                             </Button>
@@ -134,23 +119,23 @@ export const UrlGroupField = <T extends UrlGroupValues>({ form }: UrlGroupProps<
                                     </li>
                                 ))}
                             </ul>
-                            {showExtraField && (
-                                <InputFieldTwo
-                                    id="next-url-field"
-                                    unstyled
-                                    assistContainerClassName="hidden-empty"
-                                    inputClassName="color-norm p-0 rounded-none"
-                                    placeholder="https://"
-                                    name="url"
-                                    value={values.url}
-                                    error={errors.url}
-                                    onChange={handleChange}
-                                    onBlur={() => !errors.url && form.setFieldValue('url', isValidURL(values.url).url)}
-                                    ref={inputRef}
-                                />
-                            )}
 
-                            {showAddButton && (
+                            <InputFieldTwo
+                                id="next-url-field"
+                                unstyled
+                                assistContainerClassName="hidden-empty"
+                                inputClassName="color-norm p-0 rounded-none"
+                                placeholder="https://"
+                                name="url"
+                                value={values.url}
+                                error={errors.url}
+                                onChange={handleChange}
+                                onBlur={() => values.url && !errors.url && handleAdd(values.url)}
+                                onKeyDown={onKeyEnter}
+                                ref={inputRef}
+                            />
+
+                            {values.url && (
                                 <Button
                                     icon
                                     color="norm"
@@ -158,7 +143,8 @@ export const UrlGroupField = <T extends UrlGroupValues>({ form }: UrlGroupProps<
                                     size="small"
                                     title={c('Action').t`Add`}
                                     className="flex flex-align-items-center gap-1 mt-1"
-                                    onClick={handleAdd}
+                                    disabled={Boolean(errors.url)}
+                                    /*  clicking will trigger onBlur on active field */
                                 >
                                     <Icon name="plus" /> {c('Action').t`Add`}
                                 </Button>
