ceb6a63e8e825f887f03fe38fdeb370b8a0abcfa
Edvin CANDON <edvin.candon@proton.ch>
2023-04-27 16:54:48 +0200

    Improve error types in PassCrypto
    - In PassCrypto::openShare always wrap in PassCryptoError
    - Add `isPassCryptoError` utility

--- packages/pass/crypto/utils/errors.ts
+++ packages/pass/crypto/utils/errors.ts
@@ -1,5 +1,8 @@
-export class PassCryptoNotHydratedError extends Error {}
-export class PassCryptoHydrationError extends Error {}
-export class PassCryptoShareError extends Error {}
-export class PassCryptoVaultError extends Error {}
-export class PassCryptoItemError extends Error {}
+export class PassCryptoError extends Error {}
+export class PassCryptoNotHydratedError extends PassCryptoError {}
+export class PassCryptoHydrationError extends PassCryptoError {}
+export class PassCryptoShareError extends PassCryptoError {}
+export class PassCryptoVaultError extends PassCryptoError {}
+export class PassCryptoItemError extends PassCryptoError {}
+
+export const isPassCryptoError = (err: any) => err instanceof PassCryptoError;
