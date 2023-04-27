f5a48ec727080c74987b39ed18fbd9c82ff5a262
Edvin CANDON <edvin.candon@proton.ch>
2023-04-27 10:04:35 +0200

    Split `useNavigateToLogin` hook
    - Create base `useAccountFork` hook for content-script sub-apps which
      either cannot use the `NotificationContext` or do not require the
      extension permissions check to work correctly
    
    [IDTEAM-1226]

--- applications/pass-extension/src/shared/hooks/useNavigateToLogin.ts
+++ applications/pass-extension/src/shared/hooks/useNavigateToLogin.ts
@@ -10,23 +10,28 @@ import { SSO_URL } from '../../app/config';
 import { promptForPermissions } from '../extension/permissions';
 import { usePermissionsGranted } from './usePermissionsGranted';
 
+export const useAccountFork = () => async (type: FORK_TYPE) => {
+    const url = await requestFork(SSO_URL, type);
+    return browser.tabs.create({ url });
+};
+
 /* before navigating to login we should prompt the
  * user for any extension permissions required for PASS
  * to work correctly. IE: on FF we absolutely need the user
  * to do so for fallback account communication to work  */
 export const useNavigateToLogin = () => {
     const { createNotification } = useNotifications();
+    const accountFork = useAccountFork();
     const permissionsGranted = usePermissionsGranted();
 
     return async (type: FORK_TYPE) => {
         if (permissionsGranted || (await promptForPermissions())) {
-            const url = await requestFork(SSO_URL, type);
-            return browser.tabs.create({ url });
+            return accountFork(type);
         }
 
         createNotification({
             type: 'error',
-            text: c('Error').t`Please grant ${PASS_APP_NAME} the required extension permissions`,
+            text: c('Error').t`Please grant ${PASS_APP_NAME} the necessary extension permissions in order to continue`,
             expiration: -1,
         });
     };
