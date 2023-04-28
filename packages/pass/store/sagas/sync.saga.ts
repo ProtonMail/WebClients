21ef3fd6087a2047fdc168d33fdc6757fd5a289a
Edvin CANDON <edvin.candon@proton.ch>
2023-04-28 08:34:53 +0200

    Refactor `sync` saga and worker
    - `sync` saga now can be cancelled by any state resetting action : this
      fixed the edge-case where the user session is revoked while a sync is
      underway : we want to avoid dispatching `syncFailure` in that case as
      it will re-spawn the event channels
    - Handle inactive shares in `sync` worker - reconciliation now accounts
      for disabled shares (deleted or inactive).
    
    [IDTEAM-1171]

--- packages/pass/store/sagas/sync.saga.ts
+++ packages/pass/store/sagas/sync.saga.ts
@@ -1,8 +1,10 @@
-import { call, put, select, takeLeading } from 'redux-saga/effects';
+import { call, put, race, select, take } from 'redux-saga/effects';
 
+import { logger } from '@proton/pass/utils/logger';
 import { wait } from '@proton/shared/lib/helpers/promise';
 
 import { syncFailure, syncIntent, syncSuccess } from '../actions';
+import { isStateResetAction } from '../actions/utils';
 import { asIfNotOptimistic } from '../optimistic/selectors/select-is-optimistic';
 import { reducerMap } from '../reducers';
 import type { State, WorkerRootSagaOptions } from '../types';
@@ -12,18 +14,37 @@ function* syncWorker(options: WorkerRootSagaOptions) {
     const state = (yield select()) as State;
     try {
         yield wait(1500);
+
         const sync: SynchronizationResult = yield call(
             synchronize,
             asIfNotOptimistic(state, reducerMap),
             SyncType.FULL,
             options
         );
+
         yield put(syncSuccess(sync));
     } catch (e: unknown) {
         yield put(syncFailure(e));
+    } finally {
+        logger.info('[Saga::Sync] Cancelling sync task');
     }
 }
 
+/* The `syncWorker` function can take a long time to complete. In order
+ * to avoid conflicts with any state resetting actions, we race the `sync`
+ * against such actions. For example, if the user's session is evoked during
+ * a sync, we want to avoid dispatching the `syncFailure` action as it will
+ * re-spawn the event channels. Instead, we cancel the sync task using the
+ * `isStateResetAction` predicate, which checks if the given action is a state
+ * resetting action (e.g. stateDestroy, stateLock, signout) */
 export default function* watcher(options: WorkerRootSagaOptions): Generator {
-    yield takeLeading(syncIntent.match, syncWorker, options);
+    while (true) {
+        yield call(function* () {
+            yield take(syncIntent.match);
+            yield race({
+                sync: syncWorker(options),
+                cancel: take(isStateResetAction),
+            });
+        });
+    }
 }
