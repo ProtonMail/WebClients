f80a7769234256cc2610021fa16eace2c93c411b
Edvin CANDON <edvin.candon@proton.ch>
2023-04-28 20:13:49 +0200

    Clear notifications only when `Lobby` unmounts
    [IDTEAM-1233]

--- applications/pass-extension/src/popup/views/Main.tsx
+++ applications/pass-extension/src/popup/views/Main.tsx
@@ -1,5 +1,7 @@
-import { type VFC, memo } from 'react';
+import { type VFC, memo, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
+
+import { useNotifications } from '@proton/components/hooks';
 
 import { FadeIn } from '../../shared/components/animation/FadeIn';
 import { Header } from '../components/Header/Header';
@@ -14,6 +16,9 @@ import { TrashItemsList } from './Sidebar/TrashItemsList';
 import './Main.scss';
 
 const MainRaw: VFC = () => {
+    const { clearNotifications } = useNotifications();
+    useEffect(() => () => clearNotifications(), []);
+
     return (
         <FadeIn id="main" className="flex flex-column flex-nowrap w100 h100 overflow-hidden" key="main">
             <Header />
