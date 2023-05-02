463e5aca285c15d117f595f7db767b86a751fc26
Alexander Lemons <alexander.lemons@proton.ch>
2023-05-02 16:35:18 +0200

    keepass import spec
    [IDTEAM-781]

--- packages/pass/import/providers/keepass.types.ts
+++ packages/pass/import/providers/keepass.types.ts
@@ -1,15 +1,15 @@
 import type { Maybe, MaybeArray } from '@proton/pass/types';
 
-export type KeyPassEntryValue = { Key: string; Value: { __text: string } | string };
+export type KeyPassEntryValue = { __text: string } | string;
 
 export type KeePassEntry = {
     String: [
-        { Key: 'Title'; Value: string },
-        { Key: 'UserName'; Value: string },
+        { Key: 'Title'; Value: KeyPassEntryValue },
+        { Key: 'UserName'; Value: KeyPassEntryValue },
         { Key: 'Password'; Value: KeyPassEntryValue },
-        { Key: 'URL'; Value: string },
-        { Key: 'Notes'; Value: string },
-        { Key: 'TimeOtp-Secret-Base32'; Value: string }
+        { Key: 'URL'; Value: KeyPassEntryValue },
+        { Key: 'Notes'; Value: KeyPassEntryValue },
+        { Key: 'otp'; Value: KeyPassEntryValue }
     ];
 };
 
