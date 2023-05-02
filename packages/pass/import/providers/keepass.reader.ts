33357b5d72bcf50c8fa0a2a42d36fe775a313892
Alexander Lemons <alexander.lemons@proton.ch>
2023-05-02 10:32:43 +0200

    refactor keepass reader
    [IDTEAM-781]

--- packages/pass/import/providers/keepass.reader.ts
+++ packages/pass/import/providers/keepass.reader.ts
@@ -12,90 +12,92 @@ import { isValidURL } from '@proton/pass/utils/url';
 
 import { ImportReaderError } from '../helpers/reader.error';
 import { type ImportPayload, ImportVault } from '../types';
-import { KeePassEntry, KeePassFile, KeePassGroup, KeePassItem } from './keepass.types';
+import { KeePassEntry, KeePassFile, KeePassGroup, KeePassItem, KeyPassEntryValue } from './keepass.types';
 
-export const readKeePassData = async (data: string): Promise<ImportPayload> => {
-    function entryToItem(entry: KeePassEntry): ItemImportIntent<'login'> {
-        const item = entry.String.reduce<KeePassItem>((acc, { Key, Value }) => {
-            switch (Key) {
-                case 'Password':
-                    acc.password = Value.__text;
-                    break;
-                case 'Title':
-                    acc.name = Value;
-                    break;
-                case 'Notes':
-                    acc.note = Value;
-                    break;
-                case 'TimeOtp-Secret-Base32':
-                    acc.totp = Value;
-                    break;
-                default:
-                    acc[Key.toLowerCase() as keyof KeePassItem] = Value;
-            }
+const getKeyPassEntryValue = ({ Value }: KeyPassEntryValue) => (typeof Value === 'string' ? Value : Value.__text);
 
-            return acc;
-        }, {} as KeePassItem);
-
-        const urlResult = isValidURL(item.url ?? '');
-        const url = urlResult.valid ? new URL(urlResult.url) : undefined;
-        const name = item.name || url?.hostname || c('Title').t`Unnamed item`;
-
-        return {
-            type: 'login',
-            metadata: {
-                name,
-                itemUuid: uniqid(),
-                note: item.note || '',
-            },
-            content: {
-                username: item.username || '',
-                password: item.password || '',
-                urls: [url?.origin].filter(Boolean) as string[],
-                totpUri: item.totp ? parseOTPValue(item.totp, { label: name }) : '',
-            },
-            extraFields: [],
-            trashed: false,
-        };
-    }
-
-    function groupToVault(group: KeePassGroup): ImportVault | null {
-        const entry = get(group, 'Entry') as KeePassEntry | KeePassEntry[] | undefined;
-
-        if (!entry) return null;
-
-        const vaultName = group.Name || c('Title').t`Import - ${getFormattedDayFromTimestamp(getEpoch())}`;
-
-        return {
-            type: 'new',
-            vaultName,
-            id: uniqid(),
-            items: Array.isArray(entry) ? entry.map(entryToItem) : [entryToItem(entry)],
-        };
-    }
-
-    function extractVaults(group: KeePassGroup) {
-        const vaults: ImportVault[] = [];
-
-        function extract(group: KeePassGroup) {
-            const vault = groupToVault(group);
-            if (vault) vaults.push(vault);
-
-            const nestedGroup = get(group, 'Group') as KeePassGroup | KeePassGroup[] | undefined;
-
-            if (!nestedGroup) return;
-
-            if (Array.isArray(nestedGroup)) {
-                nestedGroup.forEach(extract);
-            } else {
-                extract(nestedGroup);
-            }
+function entryToItem(entry: KeePassEntry): ItemImportIntent<'login'> {
+    const item = entry.String.reduce<KeePassItem>((acc, { Key, Value }) => {
+        switch (Key) {
+            case 'Password':
+                acc.password = getKeyPassEntryValue(Value);
+                break;
+            case 'Title':
+                acc.name = Value;
+                break;
+            case 'Notes':
+                acc.note = Value;
+                break;
+            case 'TimeOtp-Secret-Base32':
+                acc.totp = Value;
+                break;
+            default:
+                acc[Key.toLowerCase() as keyof KeePassItem] = Value;
         }
 
-        extract(group);
-        return vaults;
+        return acc;
+    }, {} as KeePassItem);
+
+    const urlResult = isValidURL(item.url ?? '');
+    const url = urlResult.valid ? new URL(urlResult.url) : undefined;
+    const name = item.name || url?.hostname || c('Title').t`Unnamed item`;
+
+    return {
+        type: 'login',
+        metadata: {
+            name,
+            itemUuid: uniqid(),
+            note: item.note || '',
+        },
+        content: {
+            username: item.username || '',
+            password: item.password || '',
+            urls: [url?.origin].filter(Boolean) as string[],
+            totpUri: item.totp ? parseOTPValue(item.totp, { label: name }) : '',
+        },
+        extraFields: [],
+        trashed: false,
+    };
+}
+
+function groupToVault(group: KeePassGroup): ImportVault | null {
+    const entry = get(group, 'Entry');
+
+    if (!entry) return null;
+
+    const vaultName = group.Name || c('Title').t`Import - ${getFormattedDayFromTimestamp(getEpoch())}`;
+
+    return {
+        type: 'new',
+        vaultName,
+        id: uniqid(),
+        items: Array.isArray(entry) ? entry.map(entryToItem) : [entryToItem(entry)],
+    };
+}
+
+function extractVaults(group: KeePassGroup) {
+    const vaults: ImportVault[] = [];
+
+    function extract(group: KeePassGroup) {
+        const vault = groupToVault(group);
+        if (vault) vaults.push(vault);
+
+        const nestedGroup = get(group, 'Group');
+
+        if (!nestedGroup) return;
+
+        if (Array.isArray(nestedGroup)) {
+            nestedGroup.forEach(extract);
+        } else {
+            extract(nestedGroup);
+        }
     }
 
+    extract(group);
+    return vaults;
+}
+
+export const readKeePassData = async (data: string): Promise<ImportPayload> => {
     try {
         const x2js = new X2JS();
         const importXml: KeePassFile = x2js.xml2js(data);
