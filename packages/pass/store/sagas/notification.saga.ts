b7e43eacefea9e30c3f01df319811c01e832d516
Edvin CANDON <edvin.candon@proton.ch>
2023-05-02 16:22:08 +0200

    Deduplicate notification from actions using request id

--- packages/pass/store/sagas/notification.saga.ts
+++ packages/pass/store/sagas/notification.saga.ts
@@ -2,10 +2,20 @@ import { AnyAction } from 'redux';
 import { takeEvery } from 'redux-saga/effects';
 
 import { WithNotification, isActionWithNotification } from '../actions/with-notification';
+import { isActionWithRequest } from '../actions/with-request';
 import { WorkerRootSagaOptions } from '../types';
 
 function* notificationWorker({ onNotification }: WorkerRootSagaOptions, action: WithNotification<AnyAction>) {
     const { notification } = action.meta;
+
+    /* if the action has request metadata - use the
+     * request id as the notification key in order to
+     * correctly de-duplicate them */
+    if (isActionWithRequest(action)) {
+        notification.deduplicate = true;
+        notification.key = action.meta.request.id;
+    }
+
     onNotification?.(notification);
 }
 
