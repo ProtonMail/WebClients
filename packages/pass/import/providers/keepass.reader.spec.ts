cbc08bdcd5c0125e7ac1ff85ccbd1944e97db924
Edvin CANDON <edvin.candon@proton.ch>
2023-05-02 18:16:59 +0200

    Improve keepass reader unit tests
    - ensure broken urls are not parsed
    - test all group contents
    
    [IDTEAM-781]

--- packages/pass/import/providers/keepass.reader.spec.ts
+++ packages/pass/import/providers/keepass.reader.spec.ts
@@ -25,33 +25,106 @@ describe('Import KeePass xml', () => {
         expect(() => readKeePassData('not-an-xml-file')).toThrow();
     });
 
-    it('extracts vaults from groups', () => {
+    it('should extract vaults from groups', () => {
         expect(payload.vaults.length).toEqual(4);
+
         expect(payload.vaults[0].type).toEqual('new');
         expect(payload.vaults[0].vaultName).toEqual('Group A');
+
+        expect(payload.vaults[1].type).toEqual('new');
+        expect(payload.vaults[1].vaultName).toEqual('Group B');
+
+        expect(payload.vaults[2].type).toEqual('new');
+        expect(payload.vaults[2].vaultName).toEqual('Group C');
+
         expect(payload.vaults[3].type).toEqual('new');
         expect(payload.vaults[3].vaultName).toEqual('Import (27 Apr 2023)');
     });
 
-    it('extracts items from entries', () => {
-        expect(payload.vaults[0].items.length).toEqual(1);
+    it('should extract items from `Group A`', () => {
+        const groupA = payload.vaults[0];
+        expect(groupA.items.length).toEqual(1);
 
-        /* Login */
-        const itemA1 = payload.vaults[0].items[0] as ItemImportIntent<'login'>;
-        expect(itemA1.type).toEqual('login');
-        expect(itemA1.metadata.name).toEqual('Entry A1');
-        expect(itemA1.content.username).toEqual('nobodyA@proton.me');
-        expect(itemA1.content.password).toEqual('proton123');
-        expect(itemA1.content.urls[0]).toEqual('https://account.proton.me');
-        expect(itemA1.metadata.note).toEqual('Entry A1 note');
-        const totpUri = new URL(itemA1.content.totpUri).searchParams;
-        expect(totpUri.get('secret')).toEqual('VZKDI2A4UP2NG5BB');
+        const loginItem = groupA.items[0] as ItemImportIntent<'login'>;
+        expect(loginItem.type).toEqual('login');
+        expect(loginItem.metadata.name).toEqual('Login item with note');
+        expect(loginItem.metadata.note).toEqual('Login item');
+        expect(loginItem.metadata.itemUuid).not.toBeUndefined();
+        expect(loginItem.content.username).toEqual('nobodyA@proton.me');
+        expect(loginItem.content.password).toEqual('proton123');
+        expect(loginItem.content.urls).toEqual(['https://account.proton.me']);
+        expect(loginItem.content.totpUri).toEqual(
+            'otpauth://totp/Entry%20A:nobodyA%40proton.me%40account.proton.me?issuer=Entry%20A&secret=VZKDI2A4UP2NG5BB&algorithm=SHA1&digits=6&period=30'
+        );
+        expect(loginItem.extraFields).toEqual([]);
+        expect(loginItem.platformSpecific).toBeUndefined();
+        expect(loginItem.trashed).toEqual(false);
+    });
 
-        /* Login */
-        const itemD1 = payload.vaults[3].items[0] as ItemImportIntent<'login'>;
-        expect(itemD1.type).toEqual('login');
-        expect(itemD1.metadata.name).toEqual('Entry D1');
-        expect(itemD1.content.username).toEqual('nobodyD@proton.me');
-        expect(itemD1.content.password).toEqual('proton123');
+    it('should extract items from `Group B`', () => {
+        const groupB = payload.vaults[1];
+        expect(groupB.items.length).toEqual(1);
+
+        const loginItem = groupB.items[0] as ItemImportIntent<'login'>;
+        expect(loginItem.type).toEqual('login');
+        expect(loginItem.metadata.name).toEqual('Broken URL');
+        expect(loginItem.metadata.note).toEqual('');
+        expect(loginItem.metadata.itemUuid).not.toBeUndefined();
+        expect(loginItem.content.username).toEqual('');
+        expect(loginItem.content.password).toEqual('');
+        expect(loginItem.content.urls).toEqual([]);
+        expect(loginItem.content.totpUri).toEqual('');
+        expect(loginItem.extraFields).toEqual([]);
+        expect(loginItem.platformSpecific).toBeUndefined();
+        expect(loginItem.trashed).toEqual(false);
+    });
+
+    it('should extract items from `Group C`', () => {
+        const groupC = payload.vaults[2];
+        expect(groupC.items.length).toEqual(1);
+
+        const loginItem = groupC.items[0] as ItemImportIntent<'login'>;
+        expect(loginItem.type).toEqual('login');
+        expect(loginItem.metadata.name).toEqual('Login item');
+        expect(loginItem.metadata.note).toEqual('');
+        expect(loginItem.metadata.itemUuid).not.toBeUndefined();
+        expect(loginItem.content.username).toEqual('nobody@proton.me');
+        expect(loginItem.content.password).toEqual('proton123');
+        expect(loginItem.content.urls).toEqual([]);
+        expect(loginItem.content.totpUri).toEqual('');
+        expect(loginItem.extraFields).toEqual([]);
+        expect(loginItem.platformSpecific).toBeUndefined();
+        expect(loginItem.trashed).toEqual(false);
+    });
+
+    it('should extract items from `Group D`', () => {
+        const groupC = payload.vaults[3];
+        expect(groupC.items.length).toEqual(2);
+
+        const loginItem1 = groupC.items[0] as ItemImportIntent<'login'>;
+        expect(loginItem1.type).toEqual('login');
+        expect(loginItem1.metadata.name).toEqual('Login item');
+        expect(loginItem1.metadata.note).toEqual('some note');
+        expect(loginItem1.metadata.itemUuid).not.toBeUndefined();
+        expect(loginItem1.content.username).toEqual('nobody@proton.me');
+        expect(loginItem1.content.password).toEqual('proton123');
+        expect(loginItem1.content.urls).toEqual(['https://account.proton.me']);
+        expect(loginItem1.content.totpUri).toEqual('');
+        expect(loginItem1.extraFields).toEqual([]);
+        expect(loginItem1.platformSpecific).toBeUndefined();
+        expect(loginItem1.trashed).toEqual(false);
+
+        const loginItem2 = groupC.items[1] as ItemImportIntent<'login'>;
+        expect(loginItem2.type).toEqual('login');
+        expect(loginItem2.metadata.name).toEqual('Broken URL');
+        expect(loginItem2.metadata.note).toEqual('');
+        expect(loginItem2.metadata.itemUuid).not.toBeUndefined();
+        expect(loginItem2.content.username).toEqual('nobody@proton.me');
+        expect(loginItem2.content.password).toEqual('proton123');
+        expect(loginItem2.content.urls).toEqual([]);
+        expect(loginItem2.content.totpUri).toEqual('');
+        expect(loginItem2.extraFields).toEqual([]);
+        expect(loginItem2.platformSpecific).toBeUndefined();
+        expect(loginItem2.trashed).toEqual(false);
     });
 });
