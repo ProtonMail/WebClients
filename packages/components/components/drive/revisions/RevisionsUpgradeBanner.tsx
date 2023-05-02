af02510488dfc5e79607d5e8ec5d326d11f0d8ce
lpeslier <lucien.peslier@proton.ch>
2023-05-02 12:28:35 +0200

    Update copy for free user upgrade banner

--- packages/components/components/drive/revisions/RevisionsUpgradeBanner.tsx
+++ packages/components/components/drive/revisions/RevisionsUpgradeBanner.tsx
@@ -7,7 +7,7 @@ import { APPS } from '@proton/shared/lib/constants';
 const RevisionsUpgradeBanner = () => {
     return (
         <div className="bg-weak py-4 px-4 rounded flex flex-align-items-center flex-justify-space-between">
-            <p className="m0 my-1">{c('Info').t`Upgrade to extended version history beyond 7 days`}</p>
+            <p className="m0 my-1">{c('Info').t`Your plan is limited to 10 versions for up to 7 days`}</p>
             <ButtonLike
                 className="my-1"
                 as={AppLink}
