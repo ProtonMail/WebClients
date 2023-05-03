21d36f4a743f2a18fa5a270c985f26e1c5365629
Eduardo Conde Pena <econdepe@proton.ch>
2023-05-03 15:11:05 +0200

    Control type of fetch with feature flag

--- applications/calendar/src/app/containers/calendar/eventStore/getPaginatedEvents.ts
+++ applications/calendar/src/app/containers/calendar/eventStore/getPaginatedEvents.ts
@@ -1,13 +1,14 @@
 import { getUnixTime } from 'date-fns';
 
 import { queryEvents } from '@proton/shared/lib/api/calendars';
+import { booleanToNumber } from '@proton/shared/lib/helpers/boolean';
 import { Api } from '@proton/shared/lib/interfaces';
-import { CalendarEventWithoutBlob } from '@proton/shared/lib/interfaces/calendar';
+import { CalendarEvent, CalendarEventWithoutBlob } from '@proton/shared/lib/interfaces/calendar';
 import { CalendarEventsQueryType } from '@proton/shared/lib/interfaces/calendar/Api';
 
-interface ApiResponse {
+interface ApiResponse<T> {
     More: 0 | 1;
-    Events: CalendarEventWithoutBlob[];
+    Events: T[];
 }
 
 const PageSize = 100;
@@ -18,28 +19,39 @@ const QUERY_TYPES = [
     CalendarEventsQueryType.FullDayBeforeWindow,
 ];
 
-const getPaginatedEvents = async (
-    api: Api,
-    calendarID: string,
-    dateRange: Date[],
-    Timezone: string,
-    upsertEvent: (event: CalendarEventWithoutBlob) => void
-) => {
+interface GetPaginatedEventParams<T> {
+    calendarID: string;
+    dateRange: Date[];
+    tzid: string;
+    metadataOnly: boolean;
+    api: Api;
+    upsertEvent: (event: T) => void;
+}
+export const getPaginatedEvents = async <T>({
+    calendarID,
+    dateRange,
+    tzid,
+    metadataOnly,
+    api,
+    upsertEvent,
+}: GetPaginatedEventParams<T>) => {
     const params = {
         // Special case, not using our boundaries, since the API does not accept negative values.
         Start: Math.max(0, getUnixTime(dateRange[0])),
         End: Math.max(0, getUnixTime(dateRange[1])),
-        Timezone,
+        Timezone: tzid,
         PageSize,
     };
-    const eventsOfType: CalendarEventWithoutBlob[][] = QUERY_TYPES.map(() => []);
+    const eventsOfType: T[][] = QUERY_TYPES.map(() => []);
 
     async function* getEventsOfType({ Type }: { Type: CalendarEventsQueryType }) {
         let More = 1;
         let Page = 0;
-        let Events: CalendarEventWithoutBlob[] = [];
+        let Events: T[] = [];
         while (More === 1) {
-            ({ Events, More } = await api<ApiResponse>(queryEvents(calendarID, { ...params, Type, Page })));
+            ({ Events, More } = await api<ApiResponse<T>>(
+                queryEvents(calendarID, { ...params, Type, Page, MetaDataOnly: booleanToNumber(metadataOnly) })
+            ));
             Page += 1;
             yield Events;
         }
@@ -59,4 +71,12 @@ const getPaginatedEvents = async (
     return eventsOfType.flat();
 };
 
-export default getPaginatedEvents;
+export const getPaginatedCalendarEventsWithoutBlob = (
+    params: Omit<GetPaginatedEventParams<CalendarEventWithoutBlob>, 'metadataOnly'>
+) => {
+    return getPaginatedEvents<CalendarEventWithoutBlob>({ ...params, metadataOnly: true });
+};
+
+export const getPaginatedCalendarEvents = (params: Omit<GetPaginatedEventParams<CalendarEvent>, 'metadataOnly'>) => {
+    return getPaginatedEvents<CalendarEvent>({ ...params, metadataOnly: false });
+};
