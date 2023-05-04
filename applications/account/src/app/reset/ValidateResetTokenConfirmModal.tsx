7075072af61c55b74698a5cc314783a7f7867d01
Jack Crompton <jack.crompton@proton.ch>
2023-05-04 17:43:26 +0200

    Add email address to ValidateResetTokenConfirmModal subline
    CP-5622

--- applications/account/src/app/reset/ValidateResetTokenConfirmModal.tsx
+++ applications/account/src/app/reset/ValidateResetTokenConfirmModal.tsx
@@ -1,25 +1,61 @@
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms';
-import { Prompt } from '@proton/components';
-import { BRAND_NAME } from '@proton/shared/lib/constants';
+import { ModalTwo, ModalTwoContent, ModalTwoFooter, ModalTwoHeader } from '@proton/components';
+import { RecoveryMethod } from '@proton/components/containers/resetPassword/interface';
 
 interface ModalProps {
     onClose: () => void;
     onConfirm: () => void;
+    recoveryMethods: RecoveryMethod[];
     open: boolean;
+    address?: string;
 }
 
-const ValidateResetTokenConfirmModal = ({ onClose, onConfirm, open }: ModalProps) => {
-    const loseAllData = (
-        <span className="text-bold" key="lose-access">{c('Info').t`lose access to all current encrypted data`}</span>
+const ValidateResetTokenConfirmModal = ({ onClose, onConfirm, open, address, recoveryMethods }: ModalProps) => {
+    const oldPassword = (
+        <span className="text-bold" key="lose-access">{
+            // translator: full sentence "Your existing messages, calendars, or contacts will be locked. To recover this data, you’ll need your recovery phrase or your old password."
+            c('Info').t`old password`
+        }</span>
     );
+
+    const twoFa = (
+        <span className="text-bold" key="lose-access">{
+            // translator: full sentence "Any two-factor authentication (2FA) method on your account will be disabled."
+            c('Info').t`two-factor authentication (2FA)`
+        }</span>
+    );
+
     return (
-        <Prompt
-            open={open}
-            onClose={onClose}
-            title={c('Title').t`Warning!`}
-            buttons={[
+        <ModalTwo open={open} onClose={onClose}>
+            <ModalTwoHeader title={c('Title').t`Reset password?`} subline={address} />
+            <ModalTwoContent>
+                <div>{c('Info').jt`Once you reset your password:`}</div>
+                <ul>
+                    <li className="mb-2">
+                        {(() => {
+                            if (recoveryMethods.includes('mnemonic')) {
+                                // translator: full sentence "Your existing messages, calendars, or contacts will be locked. To recover this data, you’ll need your recovery phrase or your old password."
+                                return c('Info')
+                                    .jt`Your existing messages, calendars, or contacts will be locked. To recover this data, you’ll need your recovery phrase or your ${oldPassword}.`;
+                            }
+
+                            // translator: full sentence "Your existing messages, calendars, or contacts will be locked. To recover this data, you’ll need your old password."
+                            return c('Info')
+                                .jt`Your existing messages, calendars, or contacts will be locked. To recover this data, you’ll need your ${oldPassword}.`;
+                        })()}
+                    </li>
+                    <li>
+                        {
+                            // translator: full sentence "Any two-factor authentication (2FA) method on your account will be disabled."
+                            c('Info').jt`Any ${twoFa} method on your account will be disabled.`
+                        }
+                    </li>
+                </ul>
+            </ModalTwoContent>
+            <ModalTwoFooter>
+                <Button onClick={onClose}>{c('Action').t`Cancel`}</Button>
                 <Button
                     color="danger"
                     onClick={() => {
@@ -28,18 +64,9 @@ const ValidateResetTokenConfirmModal = ({ onClose, onConfirm, open }: ModalProps
                     }}
                 >
                     {c('Action').t`Reset password`}
-                </Button>,
-                <Button onClick={onClose}>{c('Action').t`Cancel`}</Button>,
-            ]}
-        >
-            <div>
-                <p className="mt-0">{c('Info')
-                    .jt`You will ${loseAllData} in your ${BRAND_NAME} Account. To restore it, you will need to enter your old password.`}</p>
-                <p className="mt-0">{c('Info')
-                    .t`This will also disable any two-factor authentication method associated with this account.`}</p>
-                <p className="my-0">{c('Info').t`Continue anyway?`}</p>
-            </div>
-        </Prompt>
+                </Button>
+            </ModalTwoFooter>
+        </ModalTwo>
     );
 };
 
