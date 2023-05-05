912516bd5ebefa97ae657d4adf51c289104357c8
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2023-05-05 13:12:52 +0200

    Improvements

--- packages/components/containers/error/GenericError.tsx
+++ packages/components/containers/error/GenericError.tsx
@@ -7,7 +7,7 @@ import errorImg from '@proton/styles/assets/img/errors/error-generic.svg';
 import networkErrorImg from '@proton/styles/assets/img/errors/error-network.svg';
 import clsx from '@proton/utils/clsx';
 
-import { Icon } from '../..';
+import Icon from '../../components/icon/Icon';
 import IllustrationPlaceholder from '../illustration/IllustrationPlaceholder';
 
 interface Props {
@@ -18,41 +18,36 @@ interface Props {
 }
 
 const GenericError = ({ children, className, big, isNetworkError }: Props) => {
-    const handleRefresh = () => window.location.reload();
-
     const title = c('Error message').t`Something went wrong`;
     const line1 = c('Error message').jt`Please refresh the page or try again later.`;
 
-    const status: 'child' | 'big' | 'small' = (() => {
-        if (!children && big) {
-            return 'big';
+    const display: 'default' | 'with-refresh' | 'custom' = (() => {
+        if (children) {
+            return 'custom';
         }
-        if (!children && !big) {
-            return 'small';
-        }
-        return 'child';
+        return big ? 'with-refresh' : 'default';
     })();
 
     return (
-        <div className={clsx('m-auto', status === 'big' ? 'p-1' : 'p-2', className)}>
+        <div className={clsx('m-auto', big ? 'p-1' : 'p-2', className)}>
             <IllustrationPlaceholder
                 title={title}
                 titleSize={big ? 'big' : 'regular'}
                 url={isNetworkError ? networkErrorImg : errorImg}
             >
-                {status === 'child' && children}
-                {status === 'big' && (
+                {display === 'default' && <div className="text-weak text-sm">{line1}</div>}
+                {display === 'with-refresh' && (
                     <>
                         <div className="text-weak text-rg">{line1}</div>
                         <div className="mt-8">
-                            <Button onClick={handleRefresh}>
+                            <Button onClick={() => window.location.reload()}>
                                 <Icon name="arrow-rotate-right" />
                                 <span className="ml-4">{c('Action').t`Refresh the page`}</span>
                             </Button>
                         </div>
                     </>
                 )}
-                {status === 'small' && <div className="text-weak text-sm">{line1}</div>}
+                {display === 'custom' && children}
             </IllustrationPlaceholder>
         </div>
     );
