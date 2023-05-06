03b2839898d8bfeef725b51e3e2281ca3af675a5
Edvin CANDON <edvin.candon@proton.ch>
2023-05-06 17:22:20 +0200

    Fix `onSort` direction parameter

--- packages/pass/utils/fp/sort.ts
+++ packages/pass/utils/fp/sort.ts
@@ -1,4 +1,4 @@
 export const sortOn =
     <T extends { [key: string]: any }, K extends keyof T>(key: K, order: 'ASC' | 'DESC' = 'DESC') =>
     (a: T, b: T): number =>
-        order === 'ASC' ? b[key] - a[key] : a[key] - b[key];
+        order === 'DESC' ? b[key] - a[key] : a[key] - b[key];
