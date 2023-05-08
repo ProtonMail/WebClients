f6dba45c8484b88df73087ce7ad99d81200cd08f
Alexey Karpov <alexey.karpov@proton.ch>
2023-05-08 12:44:09 +0200

    Add basic PaymentStep test

--- packages/utils/lastItem.ts
+++ packages/utils/lastItem.ts
@@ -1,6 +1,6 @@
 /**
  * Returns the last item in an array
  */
-export default function lastItem<T>(array: T[]): T | undefined {
+export default function lastItem<T>(array: ArrayLike<T>): T | undefined {
     return array[array.length - 1];
 }
