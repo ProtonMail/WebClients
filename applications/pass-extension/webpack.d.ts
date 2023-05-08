5cbb27682bbb70724cf4302b34bbc4a9abc7ff23
Edvin CANDON <edvin.candon@proton.ch>
2023-05-08 12:21:27 +0200

    Move `webpack.d.ts` to packages/pass
    - Udpate tsconfig.json

--- applications/pass-extension/webpack.d.ts
+++ applications/pass-extension/webpack.d.ts
@@ -1,4 +1,1 @@
-declare const ENV: string;
-declare const BUILD_TARGET: string;
-declare const RUNTIME_RELOAD: boolean;
-declare const RESUME_FALLBACK: boolean;
+export * from '@proton/pass/webpack.d';
