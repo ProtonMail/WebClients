55170055e90ce7305555f913ac8fb1260f4a4ba1
Edvin CANDON <edvin.candon@proton.ch>
2023-05-10 22:20:37 +0200

    Split content-script into orchestrator & client
    - Use extension message passing for injecting client
    - Reflect changes in webpack config & dev-server
    - Add LOAD|UNLOAD|START content script messages
    - Injection service now broadcasts back cs messages to relevant tabs
    - Only inject `orchestrator` in manifest declaration
    
    [IDTEAM-1237]

--- applications/pass-extension/src/worker/services/injection.ts
+++ applications/pass-extension/src/worker/services/injection.ts
@@ -1,3 +1,5 @@
+import type { Runtime } from 'webextension-polyfill';
+
 import { backgroundMessage } from '@proton/pass/extension/message';
 import browser from '@proton/pass/globals/browser';
 import { type TabId, WorkerMessageType } from '@proton/pass/types';
@@ -6,44 +8,59 @@ import { logger } from '@proton/pass/utils/logger';
 import { createDevReloader } from '../../shared/extension/dev-reload';
 import WorkerMessageBroker from '../channel';
 
-const CSS_FILES = ['styles/content.css'];
-const JS_FILES = ['content.js'];
+const withTabEffect =
+    (fn: (tabId: TabId) => Promise<void>) =>
+    async (_: any, { tab }: Runtime.MessageSender) => {
+        if (!tab?.id) return false;
+        await fn(tab.id);
+        return true;
+    };
 
 export const createInjectionService = () => {
-    const inject = async (options: { tabId: TabId }) => {
+    const inject = async (options: { tabId: TabId; js: string[]; css?: string[] }) => {
         const { tabId } = options;
         Promise.all([
-            browser.scripting.insertCSS({ target: { tabId, allFrames: false }, files: CSS_FILES }),
-            browser.scripting.executeScript({ target: { tabId, allFrames: false }, files: JS_FILES }),
+            options.css && browser.scripting.insertCSS({ target: { tabId, allFrames: false }, files: options.css }),
+            options.js &&
+                browser.scripting.executeScript({
+                    target: { tabId, allFrames: false },
+                    files: options.js,
+                }),
         ]).catch((e) => logger.info(`[InjectionService::inject] Injection error on tab ${tabId}`, e));
     };
 
     const updateInjections = async () => {
-        WorkerMessageBroker.ports.broadcast(backgroundMessage({ type: WorkerMessageType.UNLOAD_CONTENT_SCRIPT }));
-
         const tabs = await browser.tabs.query({ url: ['https://*/*', 'http://*/*'] });
         await Promise.all(
             tabs.map((tab) => {
                 logger.info(`[InjectionService::update] Re-injecting script on tab ${tab.id}`);
                 if (tab.id !== undefined) {
-                    void inject({ tabId: tab.id });
+                    void inject({ tabId: tab.id, js: ['orchestrator.js'] });
                 }
             })
         );
     };
 
-    if (ENV === 'development') {
-        if (BUILD_TARGET === 'chrome') void updateInjections();
+    WorkerMessageBroker.registerMessage(
+        WorkerMessageType.LOAD_CONTENT_SCRIPT,
+        withTabEffect((tabId) => inject({ tabId, js: ['client.js'], css: ['styles/client.css'] }))
+    );
 
-        createDevReloader(() => {
-            WorkerMessageBroker.ports.broadcast(
-                backgroundMessage({
-                    type: WorkerMessageType.UNLOAD_CONTENT_SCRIPT,
-                })
-            );
-            setTimeout(() => browser.runtime.reload(), 100);
-        }, '[DEV] Reloading runtime');
-    }
+    WorkerMessageBroker.registerMessage(
+        WorkerMessageType.UNLOAD_CONTENT_SCRIPT,
+        withTabEffect((tabId) =>
+            browser.tabs.sendMessage(tabId, backgroundMessage({ type: WorkerMessageType.UNLOAD_CONTENT_SCRIPT }))
+        )
+    );
+
+    WorkerMessageBroker.registerMessage(
+        WorkerMessageType.START_CONTENT_SCRIPT,
+        withTabEffect((tabId) =>
+            browser.tabs.sendMessage(tabId, backgroundMessage({ type: WorkerMessageType.START_CONTENT_SCRIPT }))
+        )
+    );
+
+    if (ENV === 'development') createDevReloader(() => browser.runtime.reload(), '[DEV] Reloading runtime');
 
     return { updateInjections };
 };
