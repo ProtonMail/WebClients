8dda2a0e967693537a61ef759878e2ba2702e3c3
Edvin CANDON <edvin.candon@proton.ch>
2023-05-10 15:50:21 +0200

    Clean-up vaults panel in settings
    [IDTEAM-1150]

--- applications/pass-extension/src/pages/settings/views/General.tsx
+++ applications/pass-extension/src/pages/settings/views/General.tsx
@@ -1,14 +1,14 @@
 import { type VFC } from 'react';
 
 import { ApplicationLogs } from '../component/ApplicationLogs';
-import { PrimaryVaultSelect } from '../component/PrimaryVaultSelect';
 import { SettingsPanel } from '../component/SettingsPanel';
+import { VaultsPanel } from '../component/VaultsPanel';
 
 export const General: VFC = () => {
     return (
         <>
+            <VaultsPanel />
             <SettingsPanel />
-            <PrimaryVaultSelect />
             <ApplicationLogs />
         </>
     );
