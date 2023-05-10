8580dbfb9bb23211e4735bae46555aca0a75283d
Edvin CANDON <edvin.candon@proton.ch>
2023-05-10 10:06:24 +0200

    Add webpack hook to bump dev manifest version on code-reload

--- applications/pass-extension/tools/dev:server.js
+++ applications/pass-extension/tools/dev:server.js
@@ -79,11 +79,38 @@ const server = new WebpackDevServer(
 const main = async () => {
     await server.start();
     await createReduxDevTools({ key, cert, port: REDUX_DEVTOOLS_PORT });
+    let devVersion = 0;
 
     if (RUNTIME_RELOAD) {
         const { reload } = createReloadRuntimeServer({ cert, key });
         compiler.hooks.afterEmit.tap('ProtonPassExtensionReloader', reload);
     }
+
+    compiler.hooks.compilation.tap('ProtonPassUpdateManifest', (compilation) => {
+        console.info(`[ProtonPassUpdateManifest] - Updating manifest version..`);
+        compilation.hooks.processAssets.tap(
+            {
+                name: 'ProtonPassUpdateManifest',
+                stage: webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONS,
+            },
+            () => {
+                devVersion += 1;
+                const buffer = compilation.assets[`manifest.json`].source();
+                const source = buffer.toString('utf8');
+                const manifest = JSON.parse(source);
+                const version = `${manifest.version}.${devVersion}`;
+                manifest.version = version;
+
+                const newBuffer = Buffer.from(JSON.stringify(manifest), 'utf8');
+                compilation.updateAsset(`manifest.json`, new webpack.sources.RawSource(newBuffer));
+
+                console.info(`[ProtonPassUpdateManifest] - Updated manifest version to ${version}`);
+            }
+        );
+    });
 };
 
-main().catch(() => process.exit(0));
+main().catch((err) => {
+    console.warn(err);
+    process.exit(0);
+});
