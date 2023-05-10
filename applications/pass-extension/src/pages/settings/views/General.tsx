5269d404789c848d49481b01be0300084f775cb0
Alexander Lemons <alexander.lemons@proton.ch>
2023-05-10 08:22:59 +0200

    set primary vault
    [IDTEAM-1150]

--- applications/pass-extension/src/pages/settings/views/General.tsx
+++ applications/pass-extension/src/pages/settings/views/General.tsx
@@ -1,12 +1,14 @@
 import { type VFC } from 'react';
 
 import { ApplicationLogs } from '../component/ApplicationLogs';
+import { PrimaryVaultSelect } from '../component/PrimaryVaultSelect';
 import { SettingsPanel } from '../component/SettingsPanel';
 
 export const General: VFC = () => {
     return (
         <>
             <SettingsPanel />
+            <PrimaryVaultSelect />
             <ApplicationLogs />
         </>
     );
