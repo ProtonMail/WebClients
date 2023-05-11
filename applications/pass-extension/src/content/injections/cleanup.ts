d957866833af2375970dffd4c6887cbab0bb2a55
Edvin CANDON <edvin.candon@proton.ch>
2023-05-11 09:20:38 +0200

    Clean-up added DOM attributes
    [IDTEAM-1237]

--- applications/pass-extension/src/content/injections/cleanup.ts
+++ applications/pass-extension/src/content/injections/cleanup.ts
@@ -1,4 +1,4 @@
-import { INPUT_STYLES_ATTR } from '../constants';
+import { DETECTED_FORM_ID_ATTR, INPUT_STYLES_ATTR, PROCESSED_INPUT_ATTR } from '../constants';
 import { cleanupInputInjectedStyles } from './icon';
 import { getIFrameRoot } from './iframe/create-iframe-root';
 
@@ -15,4 +15,9 @@ export const DOMCleanUp = () =>
         /* reset input field styles */
         const inputs = document.querySelectorAll<HTMLInputElement>(`input[${INPUT_STYLES_ATTR}]`);
         inputs.forEach((input) => cleanupInputInjectedStyles(input));
+
+        /* remove extension attributes */
+        [DETECTED_FORM_ID_ATTR, PROCESSED_INPUT_ATTR, PROCESSED_INPUT_ATTR].forEach((attr) => {
+            document.querySelectorAll(`[${attr}]`).forEach((node) => node.removeAttribute(attr));
+        });
     });
