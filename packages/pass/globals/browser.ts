04279058ec7f9744e87662ec26886cd25136c4c0
Edvin CANDON <edvin.candon@proton.ch>
2023-05-11 15:32:37 +0200

    Disable browser proxy trap in content-scripts

--- packages/pass/globals/browser.ts
+++ packages/pass/globals/browser.ts
@@ -1,11 +1,9 @@
-/**
- * We create a proxy around the `chrome` or `browser` global object to hide them
+/* We create a proxy around the `chrome` or `browser` global object to hide them
  * from the devtools of our extension. This is done to prevent developers from
  * accidentally or intentionally modifying the extension's messaging system, which
  * could cause unexpected behavior or security issues. By hiding these globals,
  * we ensure that the extension's messaging system is only accessed through the
- * provided APIs, and not directly through the devtools console or other means.
- */
+ * provided APIs, and not directly through the devtools console or other means */
 import browser from 'webextension-polyfill';
 
 import { logger } from '../utils/logger';
@@ -27,24 +25,24 @@ export default (() => {
 
     setTimeout(
         () =>
+            !self.__DISABLE_BROWSER_TRAP__ &&
             Object.entries(HIDDEN_GLOBALS)
                 .filter(([, value]) => value)
-                .forEach(
-                    ([key, value]) =>
-                        ((globalThis as any)[key] = new Proxy(value, {
-                            get(target, prop, receiver) {
-                                if (process.env.NODE_ENV !== 'development') {
-                                    return logger.error(`[Extension::Error] extension API is protected`);
-                                }
+                .forEach(([key, value]) => {
+                    self[key] = new Proxy(value, {
+                        get(target, prop, receiver) {
+                            if (process.env.NODE_ENV !== 'development') {
+                                return logger.error(`[Extension::Error] extension API is protected`);
+                            }
 
-                                return Reflect.get(target, prop, receiver);
-                            },
-                            set() {
-                                logger.error(`[Extension::Error] extension API is read-only`);
-                                return false;
-                            },
-                        }))
-                ),
+                            return Reflect.get(target, prop, receiver);
+                        },
+                        set() {
+                            logger.error(`[Extension::Error] extension API is read-only`);
+                            return false;
+                        },
+                    });
+                }),
         0
     );
 
