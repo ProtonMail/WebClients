f3d7222ca75e7104a263934361a9b21134a9c4a9
lpeslier <lucien.peslier@proton.ch>
2023-05-12 11:39:44 +0200

    Adapt drive userSettings for Defaults API value

--- packages/components/hooks/useRevisionRetentionDays.tsx
+++ packages/components/hooks/useRevisionRetentionDays.tsx
@@ -4,16 +4,16 @@ import { c } from 'ttag';
 
 import { queryUpdateUserSettings, queryUserSettings } from '@proton/shared/lib/api/drive/userSettings';
 import { DRIVE_APP_NAME } from '@proton/shared/lib/constants';
-import { DEFAULT_PAID_USER_SETTINGS, DEFAULT_USER_SETTINGS } from '@proton/shared/lib/drive/constants';
-import type { RevisionRetentionDaysSetting, UserSettings } from '@proton/shared/lib/interfaces/drive/userSettings';
+import type {
+    RevisionRetentionDaysSetting,
+    UserSettingsResponse,
+} from '@proton/shared/lib/interfaces/drive/userSettings';
 
 import { useConfirmActionModal } from '../components/confirmActionModal';
 import { getRetentionLabel } from '../containers/drive/settings/retentionLabels';
 import useApi from './useApi';
 import useLoading from './useLoading';
 import useNotifications from './useNotifications';
-
-type UserSettingsResponse = { UserSettings: Partial<UserSettings> };
 
 const useRevisionRetentionDays = (
     hasPaidDrive: boolean,
@@ -23,13 +23,8 @@ const useRevisionRetentionDays = (
     const [isLoading, withLoading] = useLoading(hasPaidDrive);
     const [isSubmitLoading, withSubmitLoading] = useLoading();
     const { createNotification } = useNotifications();
-    const defaultUserRetentionsDays = hasPaidDrive
-        ? DEFAULT_PAID_USER_SETTINGS.RevisionRetentionDays
-        : DEFAULT_USER_SETTINGS.RevisionRetentionDays;
-    const [originalRevisionRetentionDays, setOriginalRevisionRetentionDays] =
-        useState<RevisionRetentionDaysSetting>(defaultUserRetentionsDays);
-    const [revisionRetentionDays, setRevisionRetentionDay] =
-        useState<RevisionRetentionDaysSetting>(defaultUserRetentionsDays);
+    const [originalRevisionRetentionDays, setOriginalRevisionRetentionDays] = useState<RevisionRetentionDaysSetting>();
+    const [revisionRetentionDays, setRevisionRetentionDay] = useState<RevisionRetentionDaysSetting>();
 
     const hasValueChanged = originalRevisionRetentionDays !== revisionRetentionDays;
 
@@ -38,10 +33,13 @@ const useRevisionRetentionDays = (
             return;
         }
         void withLoading(
-            api<UserSettingsResponse>(queryUserSettings()).then(({ UserSettings }) => {
+            api<UserSettingsResponse>(queryUserSettings()).then(({ UserSettings, Defaults }) => {
                 if (UserSettings.RevisionRetentionDays !== null && UserSettings.RevisionRetentionDays !== undefined) {
                     setOriginalRevisionRetentionDays(UserSettings.RevisionRetentionDays);
                     setRevisionRetentionDay(UserSettings.RevisionRetentionDays);
+                } else {
+                    setOriginalRevisionRetentionDays(Defaults.RevisionRetentionDays);
+                    setRevisionRetentionDay(Defaults.RevisionRetentionDays);
                 }
                 return;
             })
@@ -77,7 +75,10 @@ const useRevisionRetentionDays = (
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
-        if (originalRevisionRetentionDays > revisionRetentionDays) {
+        if (revisionRetentionDays === undefined) {
+            return;
+        }
+        if (originalRevisionRetentionDays && originalRevisionRetentionDays > revisionRetentionDays) {
             const rententionLabel = getRetentionLabel(revisionRetentionDays);
             const message =
                 revisionRetentionDays === 0
