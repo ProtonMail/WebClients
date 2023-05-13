34e95005d6f9c8a805b5ffac5fc175646011566d
Jack Crompton <jack.crompton@proton.ch>
2023-05-13 16:43:41 +0200

    Collect @proton/colors coverage
    CP-5825

--- packages/colors/jest.config.js
+++ packages/colors/jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    collectCoverageFrom: ['*.ts', '!tool/*', '!index.ts', '!*.config.ts', '!types.ts'],
+    coverageReporters: ['text', 'lcov', 'cobertura'],
 };
