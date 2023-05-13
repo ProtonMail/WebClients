bf27ea2e36e7bd8520481d765642edbfa2c39fb3
Jack Crompton <jack.crompton@proton.ch>
2023-05-13 16:44:32 +0200

    Add coverageThreshold to @proton/colors
    CP-5504

--- packages/colors/jest.config.js
+++ packages/colors/jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
     testEnvironment: 'node',
     collectCoverageFrom: ['*.ts', '!tool/*', '!index.ts', '!*.config.ts', '!types.ts'],
     coverageReporters: ['text', 'lcov', 'cobertura'],
+    coverageThreshold: {
+        global: {
+            branches: 33,
+            functions: 43,
+            lines: 42,
+            statements: 41,
+        },
+    },
 };
