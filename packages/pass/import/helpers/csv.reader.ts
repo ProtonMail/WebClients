df6089435bb36c7a5a9334e610f8fea85551794d
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2023-05-17 16:39:05 +0200

    Add option to ignore importer CSV parsing errors
    [IDTEAM-1283]

--- packages/pass/import/helpers/csv.reader.ts
+++ packages/pass/import/helpers/csv.reader.ts
@@ -1,11 +1,14 @@
 import Papa from 'papaparse';
 import { c } from 'ttag';
 
+import { logger } from '@proton/pass/utils/logger';
+
 import { ImportReaderError } from './reader.error';
 
 export const readCSV = async <T extends Record<string, any>>(
     data: string,
-    expectedHeaders: (keyof T)[]
+    expectedHeaders: (keyof T)[],
+    ignoreCsvParsingErrors: boolean = false
 ): Promise<T[]> => {
     try {
         return await new Promise<T[]>((resolve, reject) => {
@@ -17,7 +20,11 @@ export const readCSV = async <T extends Record<string, any>>(
                 complete: ({ data, errors }) => {
                     if (errors.length > 0) {
                         const errorDetails = errors.map((err) => err.message).join(', ');
-                        return reject(errorDetails);
+                        if (ignoreCsvParsingErrors) {
+                            logger.warn('[Importer::LastPass]', errorDetails);
+                        } else {
+                            return reject(errorDetails);
+                        }
                     }
 
                     if (data.length === 0) return reject(c('Error').t`Empty CSV file`);
