5dde84f3b65363532fd13ea062866dfc87bad427
Edvin CANDON <edvin.candon@proton.ch>
2023-05-17 15:56:19 +0200

    Check fork message extension target in auth-fallback

--- applications/pass-extension/src/content/firefox/auth-fallback.ts
+++ applications/pass-extension/src/content/firefox/auth-fallback.ts
@@ -1,10 +1,15 @@
 import { contentScriptMessage, sendMessage } from '@proton/pass/extension/message';
 import { WorkerMessageType } from '@proton/pass/types';
 import { logger } from '@proton/pass/utils/logger';
+import { APPS, EXTENSIONS } from '@proton/shared/lib/constants';
 
 window.addEventListener('message', async (message) => {
     try {
-        if (message.data && message.data.type === 'fork') {
+        if (
+            message.data &&
+            message.data?.type === 'fork' &&
+            message.data?.extension === EXTENSIONS[APPS.PROTONPASSBROWSEREXTENSION]
+        ) {
             const { keyPassword, selector, state, persistent, trusted } = message.data.payload;
             await sendMessage.on(
                 contentScriptMessage({
