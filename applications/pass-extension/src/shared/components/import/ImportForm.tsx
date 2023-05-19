ff9de0b159ad0ece40227f2057b48246df6fe6ea
Edvin CANDON <edvin.candon@proton.ch>
2023-05-19 12:41:52 +0200

    Add single provider option for ProtonPass import provider
    - Remove PROTONPASS_PGP
    - Add file extension extract utility
    - Reflect changes in ImportForm
    
    [IDTEAM-1195]

--- applications/pass-extension/src/shared/components/import/ImportForm.tsx
+++ applications/pass-extension/src/shared/components/import/ImportForm.tsx
@@ -1,10 +1,10 @@
-import { type VFC } from 'react';
+import { type VFC, useMemo } from 'react';
 
 import { Field } from 'formik';
 import { c } from 'ttag';
 
 import { AttachedFile, Bordered, Dropzone, FileInput, Option, SelectTwo } from '@proton/components/components';
-import { ImportProvider } from '@proton/pass/import';
+import { ImportProvider, extractFileExtension } from '@proton/pass/import';
 import { PASS_APP_NAME } from '@proton/shared/lib/constants';
 import clsx from '@proton/utils/clsx';
 
@@ -18,72 +18,81 @@ export const PROVIDER_TITLE_MAP = {
     [ImportProvider.EDGE]: 'Edge (csv)',
     [ImportProvider.KEEPASS]: 'KeePass (xml)',
     [ImportProvider.LASTPASS]: 'LastPass (csv)',
-    [ImportProvider.ONEPASSWORD]: '1Password (1pux or 1pif)',
-    [ImportProvider.PROTONPASS]: PASS_APP_NAME,
-    [ImportProvider.PROTONPASS_PGP]: `${PASS_APP_NAME} (PGP encrypted)`,
+    [ImportProvider.ONEPASSWORD]: '1Password (1pux, 1pif)',
+    [ImportProvider.PROTONPASS]: `${PASS_APP_NAME} (zip, pgp)`,
 };
 
-export const ImportForm: VFC<Omit<ImportFormContext, 'reset' | 'result'>> = ({ form, dropzone, busy }) => (
-    <>
-        <label className="block mb-2 text-bold">{c('Label').t`Provider`}</label>
+export const ImportForm: VFC<Omit<ImportFormContext, 'reset' | 'result'>> = ({ form, dropzone, busy }) => {
+    const needsPassphrase = useMemo(
+        () =>
+            form.values.file &&
+            extractFileExtension(form.values.file.name) &&
+            form.values.provider === ImportProvider.PROTONPASS,
+        [form.values]
+    );
 
-        <SelectTwo
-            value={form.values.provider}
-            name="provider"
-            onValue={(provider) => form.setFieldValue('provider', provider)}
-            className="mb-4"
-            disabled={busy}
-        >
-            {Object.values(ImportProvider).map((provider) => (
-                <Option key={provider} value={provider} title={PROVIDER_TITLE_MAP[provider]}>
-                    {PROVIDER_TITLE_MAP[provider]}
-                </Option>
-            ))}
-        </SelectTwo>
+    return (
+        <>
+            <label className="block mb-2 text-bold">{c('Label').t`Provider`}</label>
 
-        <label className="block mb-2 text-bold">{c('Label').t`File`}</label>
-
-        <Bordered
-            className={clsx([
-                'relative border-dashed mb-4 rounded border-norm',
-                (form.values.file || (dropzone.hovered && !busy)) && 'border-weak',
-                form.errors.file && 'border-danger',
-            ])}
-        >
-            <Dropzone
-                isHovered={dropzone.hovered}
-                onDragEnter={dropzone.onDragEnter}
-                onDragLeave={dropzone.onDragLeave}
-                onDrop={dropzone.onDrop}
-                isDisabled={busy}
+            <SelectTwo
+                value={form.values.provider}
+                name="provider"
+                onValue={(provider) => form.setFieldValue('provider', provider)}
+                className="mb-4"
+                disabled={busy}
             >
-                {form.values.file ? (
-                    <AttachedFile
-                        file={form.values.file}
-                        className={clsx('border-none', busy && 'no-pointer-events')}
-                        iconName="file-lines"
-                        clear={c('Action').t`Delete`}
-                        onClear={() => form.setFieldValue('file', undefined)}
-                    />
-                ) : (
-                    <FileInput
-                        accept={SUPPORTED_IMPORT_FILE_TYPES.map((ext) => `.${ext}`).join(',')}
-                        onChange={dropzone.onAttach}
-                        disabled={busy}
-                        shape="solid"
-                        color="weak"
-                    >
-                        {c('Action').t`Choose a file or drag it here`}
-                    </FileInput>
-                )}
-            </Dropzone>
-        </Bordered>
+                {Object.values(ImportProvider).map((provider) => (
+                    <Option key={provider} value={provider} title={PROVIDER_TITLE_MAP[provider]}>
+                        {PROVIDER_TITLE_MAP[provider]}
+                    </Option>
+                ))}
+            </SelectTwo>
 
-        <Field
-            name="passphrase"
-            label={c('Label').t`Passphrase`}
-            component={PasswordField}
-            disabled={form.values.provider !== ImportProvider.PROTONPASS_PGP}
-        />
-    </>
-);
+            <label className="block mb-2 text-bold">{c('Label').t`File`}</label>
+
+            <Bordered
+                className={clsx([
+                    'relative border-dashed mb-4 rounded border-norm',
+                    (form.values.file || (dropzone.hovered && !busy)) && 'border-weak',
+                    form.errors.file && 'border-danger',
+                ])}
+            >
+                <Dropzone
+                    isHovered={dropzone.hovered}
+                    onDragEnter={dropzone.onDragEnter}
+                    onDragLeave={dropzone.onDragLeave}
+                    onDrop={dropzone.onDrop}
+                    isDisabled={busy}
+                >
+                    {form.values.file ? (
+                        <AttachedFile
+                            file={form.values.file}
+                            className={clsx('border-none', busy && 'no-pointer-events')}
+                            iconName="file-lines"
+                            clear={c('Action').t`Delete`}
+                            onClear={() => form.setFieldValue('file', undefined)}
+                        />
+                    ) : (
+                        <FileInput
+                            accept={SUPPORTED_IMPORT_FILE_TYPES.map((ext) => `.${ext}`).join(',')}
+                            onChange={dropzone.onAttach}
+                            disabled={busy}
+                            shape="solid"
+                            color="weak"
+                        >
+                            {c('Action').t`Choose a file or drag it here`}
+                        </FileInput>
+                    )}
+                </Dropzone>
+            </Bordered>
+
+            <Field
+                name="passphrase"
+                label={c('Label').t`Passphrase`}
+                component={PasswordField}
+                disabled={!needsPassphrase}
+            />
+        </>
+    );
+};
