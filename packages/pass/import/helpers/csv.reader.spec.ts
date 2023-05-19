f911e0e50739dec03b2a2391738b8e4b45f6fefd
Edvin CANDON <edvin.candon@proton.ch>
2023-05-19 12:34:40 +0200

    Improve csv reader error handling and ignored row detection
    [IDTEAM-1283]

--- packages/pass/import/helpers/csv.reader.spec.ts
+++ packages/pass/import/helpers/csv.reader.spec.ts
@@ -6,24 +6,27 @@ type CSVItem = { id: string; name: string };
 describe('readCSV', () => {
     it('should read CSV data with expected headers', async () => {
         const csvContent = 'id,name\n1,John Doe\n2,Jane Doe\n3,Bob Smith\n';
-        const data = await readCSV<CSVItem>(csvContent, ['id', 'name']);
+        const result = await readCSV<CSVItem>(csvContent, ['id', 'name']);
 
-        expect(data).toEqual([
+        expect(result.items).toEqual([
             { id: '1', name: 'John Doe' },
             { id: '2', name: 'Jane Doe' },
             { id: '3', name: 'Bob Smith' },
         ]);
+
+        expect(result.ignored).toEqual([]);
     });
 
     it('should read CSV data with extra headers not in expected headers', async () => {
         const csvContent = 'id,name,age\n1,John Doe,20\n2,Jane Doe,21\n3,Bob Smith,22\n';
-        const data = await readCSV<CSVItem>(csvContent, ['id', 'name']);
+        const result = await readCSV<CSVItem>(csvContent, ['id', 'name']);
 
-        expect(data).toEqual([
+        expect(result.items).toEqual([
             { id: '1', name: 'John Doe', age: '20' },
             { id: '2', name: 'Jane Doe', age: '21' },
             { id: '3', name: 'Bob Smith', age: '22' },
         ]);
+        expect(result.ignored).toEqual([]);
     });
 
     it('should throw error if CSV is empty', async () => {
@@ -38,10 +41,15 @@ describe('readCSV', () => {
         );
     });
 
-    it('should throw error if CSV file has invalid content', async () => {
+    it('should hydrate ignored array if some entries are invalid', async () => {
         const csvContent = 'id,name\n1,John Doe\n2\n3,Bob Smith\n';
-        await expect(readCSV<CSVItem>(csvContent, ['id', 'name'])).rejects.toThrow(
-            new ImportReaderError('Too few fields: expected 2 fields but parsed 1')
-        );
+        const result = await readCSV<CSVItem>(csvContent, ['id', 'name']);
+
+        expect(result.items).toEqual([
+            { id: '1', name: 'John Doe' },
+            { id: '3', name: 'Bob Smith' },
+        ]);
+
+        expect(result.ignored).toEqual([{ id: '2' }]);
     });
 });
