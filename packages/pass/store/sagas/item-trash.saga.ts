db6de115d232ba6c49362b86876173fdbaf62fa8
Edvin CANDON <edvin.candon@proton.ch>
2023-05-20 14:53:27 +0200

    Show custom notification when trashing alias item
    [IDTEAM-1330]

--- packages/pass/store/sagas/item-trash.saga.ts
+++ packages/pass/store/sagas/item-trash.saga.ts
@@ -1,12 +1,16 @@
 import { put, takeEvery } from 'redux-saga/effects';
+import { c } from 'ttag';
 
-import { itemTrashFailure, itemTrashIntent, itemTrashSuccess } from '../actions';
+import { getItemActionId } from '@proton/pass/utils/pass/items';
+
+import { itemTrashFailure, itemTrashIntent, itemTrashSuccess, notification } from '../actions';
+import { WithSenderAction } from '../actions/with-receiver';
 import { WorkerRootSagaOptions } from '../types';
 import { trashItem } from './workers/items';
 
 function* trashItemWorker(
     { onItemsChange }: WorkerRootSagaOptions,
-    { payload, meta }: ReturnType<typeof itemTrashIntent>
+    { payload, meta }: WithSenderAction<ReturnType<typeof itemTrashIntent>>
 ) {
     const { item, shareId } = payload;
     const { callback: onItemTrashProcessed } = meta;
@@ -15,6 +19,21 @@ function* trashItemWorker(
         yield trashItem(payload.item);
         const itemTrashSuccessAction = itemTrashSuccess({ itemId: item.itemId, shareId });
         yield put(itemTrashSuccessAction);
+
+        /* alias specific notification on trash :
+         * this will override the default trash success
+         * notification by deduplicating a new notification
+         * with the same key */
+        yield item.data.type === 'alias' &&
+            put(
+                notification({
+                    type: 'info',
+                    key: getItemActionId(payload),
+                    receiver: meta.sender?.endpoint,
+                    text: c('Info')
+                        .t`Alias "${item.aliasEmail}" moved to trash - it will stop forwarding emails to your mailbox`,
+                })
+            );
 
         onItemTrashProcessed?.(itemTrashSuccessAction);
         onItemsChange?.();
