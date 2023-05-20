71e8c338cd38d15b2d00da53ba165d893edc14c9
Edvin CANDON <edvin.candon@proton.ch>
2023-05-20 22:34:51 +0200

    Rename `canFocus` prop to `didMount` in `DiscardableModalPanel`

--- applications/pass-extension/src/popup/components/Panel/DiscardableModalPanel.tsx
+++ applications/pass-extension/src/popup/components/Panel/DiscardableModalPanel.tsx
@@ -7,27 +7,40 @@ import type { Callback } from '@proton/pass/types';
 import { ConfirmationModal } from '../../../shared/components/confirmation';
 import { SidebarModal } from '../../../shared/components/sidebarmodal/SidebarModal';
 
-export type DiscardableModalRenderProps = { confirm: (effect?: Callback) => void; canFocus: boolean };
+export type DiscardableModalRenderProps = { confirm: (effect?: Callback) => void; didMount: boolean };
 export type DiscardableModalProps = {
     discardable: boolean;
     onDiscard: () => void;
     children: (props: DiscardableModalRenderProps) => ReactNode;
 };
+
+/* The didMount state is passed as a render prop to the children so
+ * that they are informed when the modal has appeared on the screen
+ * and completed its reveal animation. This is particularly useful
+ * when the children contain input elements that need to be autofocused
+ * after the modal has finished appearing. By doing this, we can avoid
+ * animation glitches related to the "flickering caret" problem that
+ * can occur with CSS animations. The "flickering caret" problem refers
+ * to the flickering effect that can happen when the input's text cursor
+ * rapidly gains and loses focus, leading to visual inconsistencies. */
 export const DiscardableModalPanel: FC<DiscardableModalProps> = ({ discardable, onDiscard, children }) => {
+    const timer = useRef<NodeJS.Timeout>();
     const [confirm, setConfirm] = useState<{ opened: boolean; effect?: Callback }>({ opened: false });
-    const [canFocus, setCanFocus] = useState<boolean>(false);
+    const [didMount, setDidMount] = useState<boolean>(false);
     const doConfirm = useCallback((effect?: Callback) => setConfirm({ opened: true, effect }), []);
     const onBackdropClick = useCallback(discardable ? onDiscard : () => setConfirm({ opened: true }), [discardable]);
+
+    useEffect(() => () => clearTimeout(timer.current), []);
 
     return (
         <div>
             <SidebarModal
                 open
                 onBackdropClick={onBackdropClick}
-                onEnter={() => setTimeout(() => setCanFocus(true), 250)}
+                onEnter={() => (timer.current = setTimeout(() => setDidMount(true), 50))}
                 rootClassName="pass-modal-two--sidebar-content"
             >
-                {children({ confirm: doConfirm, canFocus })}
+                {children({ confirm: doConfirm, didMount })}
 
                 <ConfirmationModal
                     title={c('Title').t`Discard changes ?`}
