31d43712dac73063b8505ae21b699ef0656f8012
Edvin CANDON <edvin.candon@proton.ch>
2023-05-21 11:44:44 +0200

    Add upselling hooks in injected dropdown
    - Prompt for upgrade in alias autosuggest if limits reached
    - Show prompt for upgrade in autofill dropdown if candidates filtered

--- applications/pass-extension/src/content/injections/apps/dropdown/views/ItemsList.tsx
+++ applications/pass-extension/src/content/injections/apps/dropdown/views/ItemsList.tsx
@@ -1,25 +1,47 @@
-import type { VFC } from 'react';
+import { VFC, useMemo } from 'react';
+
+import { c } from 'ttag';
 
 import { SafeLoginItem } from '@proton/pass/types';
+import { truthy } from '@proton/pass/utils/fp';
+import { PASS_APP_NAME } from '@proton/shared/lib/constants';
 
+import { navigateToUpgrade } from '../../../../../shared/components/upgrade/UpgradeButton';
 import { DropdownItem } from '../components/DropdownItem';
 import { DropdownItemsList } from '../components/DropdownItemsList';
 
-export const ItemsList: VFC<{ items: SafeLoginItem[]; onSubmit: (item: SafeLoginItem) => void }> = ({
-    items,
-    onSubmit,
-}) => {
-    return (
-        <DropdownItemsList>
-            {items.map((item) => (
-                <DropdownItem
-                    key={item.itemId}
-                    icon="key"
-                    title={item.name}
-                    subTitle={item.username}
-                    onClick={() => onSubmit(item)}
-                />
-            ))}
-        </DropdownItemsList>
+type Props = {
+    items: SafeLoginItem[];
+    needsUpgrade: boolean;
+    onSubmit: (item: SafeLoginItem) => void;
+};
+
+export const ItemsList: VFC<Props> = ({ items, needsUpgrade, onSubmit }) => {
+    const dropdownItems = useMemo(
+        () =>
+            [
+                needsUpgrade && (
+                    <DropdownItem
+                        key={'upgrade-autofill'}
+                        icon="arrow-out-square"
+                        title={c('Info').t`Upgrade ${PASS_APP_NAME}`}
+                        subTitle={c('Warning').t`Your plan only allows you to autofill from your primary vault`}
+                        onClick={navigateToUpgrade}
+                        autogrow
+                    />
+                ),
+                ...items.map((item) => (
+                    <DropdownItem
+                        key={item.itemId}
+                        icon="key"
+                        title={item.name}
+                        subTitle={item.username}
+                        onClick={() => onSubmit(item)}
+                    />
+                )),
+            ].filter(truthy),
+        [items, needsUpgrade, onSubmit]
     );
+
+    return <DropdownItemsList>{dropdownItems}</DropdownItemsList>;
 };
