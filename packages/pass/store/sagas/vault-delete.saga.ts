afebcc92ff24d2a8dc673d6e9e5542b60b48d637
Edvin CANDON <edvin.candon@proton.ch>
2023-05-22 17:39:35 +0200

    Handle item move before vault delete
    [IDTEAM-1199]

--- packages/pass/store/sagas/vault-delete.saga.ts
+++ packages/pass/store/sagas/vault-delete.saga.ts
@@ -1,20 +1,32 @@
-import { put, takeEvery } from 'redux-saga/effects';
+import { put } from 'redux-saga/effects';
 
 import { api } from '@proton/pass/api';
 import { PassCrypto } from '@proton/pass/crypto';
+import type { ItemRevision } from '@proton/pass/types';
 
 import { acknowledgeRequest, vaultDeleteFailure, vaultDeleteIntent, vaultDeleteSuccess } from '../actions';
-import type { WorkerRootSagaOptions } from '../types';
+import { selectItemsByShareId } from '../selectors';
+import type { State, WorkerRootSagaOptions } from '../types';
+import { takeEveryBefore } from './utils/take.before';
+import { moveItems } from './workers/items';
 
 function* deleteVault(
-    { onItemsChange }: WorkerRootSagaOptions,
-    { payload: { id, content }, meta }: ReturnType<typeof vaultDeleteIntent>
+    { payload: { id, content, destinationShareId }, meta }: ReturnType<typeof vaultDeleteIntent>,
+    stateBeforeAction: State,
+    { onItemsChange }: WorkerRootSagaOptions
 ): Generator {
     try {
+        const items = selectItemsByShareId(stateBeforeAction, id);
+
+        const movedItems = (yield destinationShareId !== null
+            ? moveItems(items, destinationShareId)
+            : []) as ItemRevision[];
+
         yield api({ url: `pass/v1/vault/${id}`, method: 'delete' });
+
         PassCrypto.removeShare(id);
 
-        yield put(vaultDeleteSuccess({ id, content }));
+        yield put(vaultDeleteSuccess({ id, content, movedItems }));
         onItemsChange?.();
     } catch (e) {
         yield put(vaultDeleteFailure({ id, content }, e));
@@ -24,5 +36,5 @@ function* deleteVault(
 }
 
 export default function* watcher(options: WorkerRootSagaOptions) {
-    yield takeEvery(vaultDeleteIntent.match, deleteVault, options);
+    yield takeEveryBefore(vaultDeleteIntent.match, deleteVault, options);
 }
