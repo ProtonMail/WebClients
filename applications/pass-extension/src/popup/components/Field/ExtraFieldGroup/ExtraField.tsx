8d66e149c4336133381985515b209f6585ef51fc
Edvin CANDON <edvin.candon@proton.ch>
2023-05-23 22:22:37 +0200

    Improvements for ExtraFields
    - basic error handling
    - add shared validation

--- applications/pass-extension/src/popup/components/Field/ExtraFieldGroup/ExtraField.tsx
+++ applications/pass-extension/src/popup/components/Field/ExtraFieldGroup/ExtraField.tsx
@@ -1,17 +1,18 @@
 import type { ChangeEvent, VFC } from 'react';
 
-import type { FieldInputProps } from 'formik';
+import type { FieldInputProps, FormikErrors } from 'formik';
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms/Button';
 import type { IconName } from '@proton/components/components';
 import { Icon } from '@proton/components/components';
-import type { ExtraFieldType, ItemExtraField } from '@proton/pass/types';
+import type { ExtraFieldType } from '@proton/pass/types';
 
 import type { FieldBoxProps } from '../Layout/FieldBox';
 import { FieldBox } from '../Layout/FieldBox';
 import type { BaseTextFieldProps } from '../TextField';
 import { BaseTextField } from '../TextField';
+import type { ExtraFieldFormValue } from './ExtraFieldGroup';
 
 type ExtraFieldOption = {
     icon: IconName;
@@ -21,17 +22,18 @@ type ExtraFieldOption = {
 };
 
 export type ExtraFieldProps = FieldBoxProps &
-    Omit<BaseTextFieldProps, 'field' | 'placeholder'> & {
-        field: FieldInputProps<ItemExtraField>;
-        onDelete: () => void;
+    Omit<BaseTextFieldProps, 'field' | 'placeholder' | 'error'> & {
         type: ExtraFieldType;
+        field: FieldInputProps<ExtraFieldFormValue>;
+        error?: FormikErrors<ExtraFieldFormValue>;
+        onDelete: () => void;
     };
 
 export const EXTRA_FIELD_OPTIONS: { [key in ExtraFieldType]: ExtraFieldOption } = {
     totp: {
         icon: 'lock',
         label: c('Label').t`2FA secret (TOTP)`,
-        labelEdit: false,
+        labelEdit: true,
         placeholder: c('Placeholder').t`Add 2FA secret`,
     },
     text: {
@@ -55,14 +57,14 @@ const DeleteButton: VFC<{ onDelete: () => void }> = ({ onDelete }) => (
 );
 
 export const ExtraFieldComponent: VFC<ExtraFieldProps> = (props) => {
-    const { className, field, onDelete, type, ...rest } = props;
+    const { className, field, onDelete, type, error, ...rest } = props;
     const { icon, label, labelEdit, placeholder } = EXTRA_FIELD_OPTIONS[type];
 
-    const getField = (input: keyof ItemExtraField) => ({
+    const getFieldFor = (input: keyof ExtraFieldFormValue) => ({
         ...field,
+        value: field.value[input],
         onChange: (e: ChangeEvent<HTMLInputElement>) =>
             props.form.setFieldValue(field.name, { ...field.value, [input]: e.target.value }),
-        value: field.value[input],
     });
 
     return (
@@ -70,15 +72,17 @@ export const ExtraFieldComponent: VFC<ExtraFieldProps> = (props) => {
             {labelEdit && (
                 <BaseTextField
                     className="field-two-label color-weak text-sm"
-                    field={getField('fieldName')}
+                    field={getFieldFor('fieldName')}
                     placeholder={label}
+                    error={error?.fieldName}
                     {...rest}
                 />
             )}
             <BaseTextField
-                field={getField('content')}
+                field={getFieldFor('value')}
                 label={labelEdit ? undefined : label}
                 placeholder={placeholder}
+                error={error?.value}
                 {...rest}
             />
         </FieldBox>
