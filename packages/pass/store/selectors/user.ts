223db2920fe2104627867fdde6d3fa3290b4a2e0
Edvin CANDON <edvin.candon@proton.ch>
2023-05-23 08:47:42 +0200

    Add selectors for pass plan type & days remaining for trial
    - Define UserPassPlan enum with trial
    
    [IDTEAM-1355]

--- packages/pass/store/selectors/user.ts
+++ packages/pass/store/selectors/user.ts
@@ -1,4 +1,7 @@
-import type { Maybe } from '@proton/pass/types';
+import type { Maybe, MaybeNull } from '@proton/pass/types';
+import { PlanType } from '@proton/pass/types';
+import { UserPassPlan } from '@proton/pass/types/api/plan';
+import { UNIX_DAY, getEpoch } from '@proton/pass/utils/time';
 import { type Address, UserType } from '@proton/shared/lib/interfaces';
 
 import type { State } from '../types';
@@ -6,14 +9,32 @@ import type { State } from '../types';
 export const selectUserState = ({ user }: State) => user;
 
 export const selectUser = ({ user: { user } }: State) => user;
-
 export const selectUserPlan = ({ user: { plan } }: State) => plan;
 
+/* Specification for pass specific plans in `/user/access` response :
+ * `paid` -> Plan: Plus | Trial: null | Limits: none
+ * `trial` -> Plan: Free | Trial: unix timestamp end | Limits
+ * `free` -> Plan: Free | Trial: null | Limits */
+export const selectPassPlan = ({ user: { plan } }: State): UserPassPlan => {
+    switch (plan?.Type) {
+        case PlanType.plus:
+            return UserPassPlan.PLUS;
+        default: {
+            return plan?.TrialEnd !== null ? UserPassPlan.TRIAL : UserPassPlan.FREE;
+        }
+    }
+};
+
+export const selectTrialDaysLeft = ({ user: { plan } }: State): MaybeNull<number> => {
+    if (!plan?.TrialEnd) return null;
+    const remaining = plan.TrialEnd - getEpoch();
+    return Math.ceil(remaining / UNIX_DAY);
+};
+/* user tier is only used for telemetry */
 export const selectUserTier = ({ user: { user, plan } }: State) =>
     user?.Type === UserType.MANAGED ? 'subuser' : plan?.InternalName;
 
 export const selectAllAddresses = ({ user: { addresses } }: State): Address[] => Object.values(addresses);
-
 export const selectAddress =
     (addressId: string) =>
     ({ user: { addresses } }: State): Maybe<Address> =>
