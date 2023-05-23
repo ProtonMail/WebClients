4b1ac9d4465ea9a96848dfe6df65aaa363fc3bf3
Edvin CANDON <edvin.candon@proton.ch>
2023-05-23 18:53:02 +0200

    Add feature flags state in store
    [IDTEAM-1339]

--- packages/pass/store/actions/creators/user.ts
+++ packages/pass/store/actions/creators/user.ts
@@ -2,4 +2,7 @@ import { createAction } from '@reduxjs/toolkit';
 
 import type { PassPlanResponse } from '@proton/pass/types';
 
+import type { UserFeatureState } from '../../reducers';
+
 export const setUserPlan = createAction<PassPlanResponse>('set plan');
+export const setUserFeatures = createAction<UserFeatureState>('set user features');
