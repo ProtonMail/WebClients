fc12035845c87eea57e079e91bfff5e62de841c6
larabr <7375870+larabr@users.noreply.github.com>
2023-05-24 16:18:00 +0200

    Log to Sentry whether serverTime() falls back to device time

--- packages/crypto/lib/serverTime.ts
+++ packages/crypto/lib/serverTime.ts
@@ -1,3 +1,14 @@
-import { serverTime, updateServerTime } from 'pmcrypto/lib/serverTime';
+import { updateServerTime as internalUpdateServerTime, serverTime } from 'pmcrypto/lib/serverTime';
 
-export { serverTime, updateServerTime };
+let updateCalled = false;
+/**
+ * If `updateServerTime()` is never called, then `serverTime()` returns the device time.
+ * This helpers is for debugging purposes and returns whether the serverTime is indeed being used.
+ */
+const wasServerTimeEverUpdated = () => updateCalled;
+const updateServerTime = (serverDate: Date) => {
+    updateCalled = true;
+    return internalUpdateServerTime(serverDate);
+};
+
+export { serverTime, updateServerTime, wasServerTimeEverUpdated };
