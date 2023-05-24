bc306ee4ab27198262f2aa29b2382dc4bacddf5f
Richard <richard@protonmail.com>
2023-05-24 14:29:56 +0200

    Review edit invite modal conditions
    We can open the invite modal if the user is a Proton member (excluding logged user and subscriber)

--- packages/components/containers/members/UsersAndAddressesSection/UsersAndAddressesSection.tsx
+++ packages/components/containers/members/UsersAndAddressesSection/UsersAndAddressesSection.tsx
@@ -155,8 +155,8 @@ const UsersAndAddressesSection = ({ app }: { app: APP_NAMES }) => {
     const handleEditUser = (member: Member) => {
         setTmpMember(member);
 
-        //We can edit member if we have a family plan or if the user is a Proton member (excluding logged user)
-        if (hasFamily(subscription) || (member.Type === MEMBER_TYPE.PROTON && !member.Self)) {
+        // We can open the invite modal if the user is a Proton member (excluding logged user and subscriber)
+        if (member.Type === MEMBER_TYPE.PROTON && !member.Self && !member.Subscriber) {
             setUserInviteOrEditModalOpen(true);
         } else {
             setSubUserEditModalOpen(true);
