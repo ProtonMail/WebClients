375d04c630710491171fc56ff2d727d98722e328
Edvin CANDON <edvin.candon@proton.ch>
2023-05-24 14:53:19 +0200

    Use new postMessage message format with account
    - disable proxy trap in FF content-script

--- applications/pass-extension/src/content/firefox/auth-fallback.ts
+++ applications/pass-extension/src/content/firefox/auth-fallback.ts
@@ -3,29 +3,31 @@ import { WorkerMessageType } from '@proton/pass/types';
 import { logger } from '@proton/pass/utils/logger';
 import { APPS, EXTENSIONS } from '@proton/shared/lib/constants';
 
-window.addEventListener('message', async (message) => {
-    try {
-        if (
-            message.data &&
-            message.data?.type === 'fork' &&
-            message.data?.extension === EXTENSIONS[APPS.PROTONPASSBROWSEREXTENSION]
-        ) {
-            const { keyPassword, selector, state, persistent, trusted } = message.data.payload;
-            await sendMessage.on(
-                contentScriptMessage({
-                    type: WorkerMessageType.FORK,
-                    payload: {
-                        selector,
-                        state,
-                        keyPassword,
-                        persistent,
-                        trusted,
-                    },
-                }),
-                (message) => window.postMessage({ fork: 'success', ...message })
-            );
+export const authFallback = () => {
+    window.addEventListener('message', async (message) => {
+        try {
+            if (
+                message.data &&
+                message.data?.type === 'fork' &&
+                message.data?.extension === EXTENSIONS[APPS.PROTONPASSBROWSEREXTENSION].ID
+            ) {
+                const { keyPassword, selector, state, persistent, trusted } = message.data.payload;
+                await sendMessage.on(
+                    contentScriptMessage({
+                        type: WorkerMessageType.FORK,
+                        payload: {
+                            selector,
+                            state,
+                            keyPassword,
+                            persistent,
+                            trusted,
+                        },
+                    }),
+                    (response) => window.postMessage({ token: message.data?.token, ...response })
+                );
+            }
+        } catch (e) {
+            logger.warn('[ContentScript::Fork]', e);
         }
-    } catch (e) {
-        logger.warn('[ContentScript::Fork]', e);
-    }
-});
+    });
+};
