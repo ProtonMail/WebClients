69b7e94e112f03be1578c281695ef2e7d6ddeee4
Edvin CANDON <edvin.candon@proton.ch>
2023-05-25 15:56:02 +0200

    Leverage generics by pattern matching in ExtraField components

--- applications/pass-extension/src/popup/components/Field/ExtraFieldGroup/ExtraField.tsx
+++ applications/pass-extension/src/popup/components/Field/ExtraFieldGroup/ExtraField.tsx
@@ -7,6 +7,7 @@ import { Button } from '@proton/atoms/Button';
 import type { IconName } from '@proton/components/components';
 import { Icon } from '@proton/components/components';
 import type { ExtraFieldType, ItemExtraField } from '@proton/pass/types';
+import { partialMerge } from '@proton/pass/utils/object';
 import clsx from '@proton/utils/clsx';
 
 import type { FieldBoxProps } from '../Layout/FieldBox';
@@ -20,15 +21,19 @@ type ExtraFieldOption = {
     placeholder: string;
 };
 
+type ExtraFieldError<T extends ExtraFieldType> = FormikErrors<ItemExtraField<T>>;
+
 export type ExtraFieldProps = FieldBoxProps &
     Omit<BaseTextFieldProps, 'field' | 'placeholder' | 'error'> & {
         type: ExtraFieldType;
         field: FieldInputProps<ItemExtraField>;
-        error?: FormikErrors<ItemExtraField>;
+        error?: ExtraFieldError<ExtraFieldType>;
+        touched?: boolean;
+        autoFocus?: boolean;
         onDelete: () => void;
     };
 
-export const getExtraFieldOptions = (): { [key in ExtraFieldType]: ExtraFieldOption } => ({
+export const getExtraFieldOptions = (): Record<ExtraFieldType, ExtraFieldOption> => ({
     totp: {
         icon: 'lock',
         label: c('Label').t`2FA secret (TOTP)`,
@@ -46,6 +51,8 @@ export const getExtraFieldOptions = (): { [key in ExtraFieldType]: ExtraFieldOpt
     },
 });
 
+export const getExtraFieldOption = (type: ExtraFieldType) => getExtraFieldOptions()[type];
+
 const DeleteButton: VFC<{ onDelete: () => void }> = ({ onDelete }) => (
     <Button icon pill color="weak" onClick={onDelete} shape="solid" size="medium" title={c('Action').t`Delete`}>
         <Icon name="cross" size={20} />
@@ -53,32 +60,69 @@ const DeleteButton: VFC<{ onDelete: () => void }> = ({ onDelete }) => (
 );
 
 export const ExtraFieldComponent: VFC<ExtraFieldProps> = (props) => {
-    const { className, field, onDelete, type, error, ...rest } = props;
-    const { icon, placeholder } = getExtraFieldOptions()[type];
+    const { className, field, onDelete, type, error, touched, autoFocus, ...rest } = props;
+    const { icon, placeholder } = getExtraFieldOption(type);
 
-    const getFieldFor = (input: keyof ItemExtraField) => ({
-        ...field,
-        value: field.value[input],
-        onChange: (e: ChangeEvent<HTMLInputElement>) =>
-            props.form.setFieldValue(field.name, { ...field.value, [input]: e.target.value }),
-    });
+    const onChangeHandler =
+        (merge: (evt: ChangeEvent<HTMLInputElement>, field: ItemExtraField) => ItemExtraField) =>
+        (evt: ChangeEvent<HTMLInputElement>) => {
+            props.form.setFieldValue(field.name, merge(evt, props.field.value));
+        };
 
     return (
         <FieldBox actions={[<DeleteButton onDelete={onDelete} />]} className={className} icon={icon}>
             <BaseTextField
-                autoFocus={true}
                 className="field-two-label color-weak text-sm"
-                field={getFieldFor('fieldName')}
-                inputClassName={clsx(error?.fieldName && 'placeholder-danger')}
+                inputClassName={clsx(touched && error?.fieldName && 'placeholder-danger')}
                 placeholder={c('Label').t`Label`}
+                autoFocus={autoFocus}
+                field={{
+                    ...field,
+                    value: field.value.fieldName,
+                    onChange: onChangeHandler((evt, values) => partialMerge(values, { fieldName: evt.target.value })),
+                }}
                 {...rest}
             />
-            <BaseTextField
-                field={getFieldFor('value')}
-                placeholder={placeholder}
-                error={error?.fieldName || error?.value}
-                {...rest}
-            />
+
+            {(() => {
+                switch (field.value.type) {
+                    case 'text':
+                    case 'hidden': {
+                        const fieldError = error as ExtraFieldError<'text' | 'hidden'>;
+                        return (
+                            <BaseTextField
+                                placeholder={placeholder}
+                                error={touched && fieldError?.data?.content}
+                                field={{
+                                    ...field,
+                                    value: field.value.data.content,
+                                    onChange: onChangeHandler((evt, values) =>
+                                        partialMerge(values, { data: { content: evt.target.value } })
+                                    ),
+                                }}
+                                {...rest}
+                            />
+                        );
+                    }
+                    case 'totp': {
+                        const fieldError = error as ExtraFieldError<'totp'>;
+                        return (
+                            <BaseTextField
+                                placeholder={placeholder}
+                                error={touched && fieldError?.data?.totpUri}
+                                field={{
+                                    ...field,
+                                    value: field.value.data.totpUri,
+                                    onChange: onChangeHandler((evt, values) =>
+                                        partialMerge(values, { data: { totpUri: evt.target.value } })
+                                    ),
+                                }}
+                                {...rest}
+                            />
+                        );
+                    }
+                }
+            })()}
         </FieldBox>
     );
 };
