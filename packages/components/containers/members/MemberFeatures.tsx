ed6df47f3ec0181da5389f93faefe8c5a362d3ce
Richard Tetaz <richard.tetaz@proton.ch>
2023-05-25 16:03:31 +0200

    Hide VPN and Space row if missing in Organization

--- packages/components/containers/members/MemberFeatures.tsx
+++ packages/components/containers/members/MemberFeatures.tsx
@@ -1,31 +1,35 @@
 import { c, msgid } from 'ttag';
 
 import humanSize from '@proton/shared/lib/helpers/humanSize';
-import { Member } from '@proton/shared/lib/interfaces';
+import { Member, Organization } from '@proton/shared/lib/interfaces';
 
 import { Icon } from '../../components';
 
-const MemberFeatures = ({ member }: { member: Member }) => {
+const MemberFeatures = ({ member, organization }: { member: Member; organization: Organization }) => {
     const { UsedSpace, MaxSpace, MaxVPN } = member;
 
     return (
         <>
-            <span className="mb-2 flex flex-nowrap">
-                <span className="flex-item-noshrink flex mt-0.5">
-                    <Icon name="filing-cabinet" />
+            {organization.MaxSpace > 0 ? (
+                <span className="mb-2 flex flex-nowrap">
+                    <span className="flex-item-noshrink flex mt-0.5">
+                        <Icon name="filing-cabinet" />
+                    </span>
+                    <span className="flex-item-fluid pl-1" data-testid="users-and-addresses-table:storage">
+                        {humanSize(UsedSpace, 'GB')} / {humanSize(MaxSpace, 'GB')}
+                    </span>
                 </span>
-                <span className="flex-item-fluid pl-1" data-testid="users-and-addresses-table:storage">
-                    {humanSize(UsedSpace, 'GB')} / {humanSize(MaxSpace, 'GB')}
+            ) : null}
+            {organization.MaxVPN > 0 ? (
+                <span className="flex flex-nowrap">
+                    <span className="flex-item-noshrink flex mt-0.5">
+                        <Icon name="brand-proton-vpn" />
+                    </span>
+                    <span className="flex-item-fluid pl-1" data-testid="users-and-addresses-table:vpnConnections">
+                        {c('Feature').ngettext(msgid`${MaxVPN} VPN connection`, `${MaxVPN} VPN connections`, MaxVPN)}
+                    </span>
                 </span>
-            </span>
-            <span className="flex flex-nowrap">
-                <span className="flex-item-noshrink flex mt-0.5">
-                    <Icon name="brand-proton-vpn" />
-                </span>
-                <span className="flex-item-fluid pl-1" data-testid="users-and-addresses-table:vpnConnections">
-                    {c('Feature').ngettext(msgid`${MaxVPN} VPN connection`, `${MaxVPN} VPN connections`, MaxVPN)}
-                </span>
-            </span>
+            ) : null}
         </>
     );
 };
