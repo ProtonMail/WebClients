953036b35bad0161b4341ac53b450033b044d98e
Richard <richard@protonmail.com>
2023-05-26 05:38:42 +0200

    Let family admin user promote other admins
    Add add VPN connections

--- packages/components/containers/members/UserInviteOrEditModal.tsx
+++ packages/components/containers/members/UserInviteOrEditModal.tsx
@@ -1,24 +1,27 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useMemo, useState } from 'react';
 
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms';
-import { editMemberInvitation, inviteMember, updateQuota } from '@proton/shared/lib/api/members';
-import { GIGA, MAIL_APP_NAME } from '@proton/shared/lib/constants';
+import { editMemberInvitation, inviteMember, updateQuota, updateRole, updateVPN } from '@proton/shared/lib/api/members';
+import { GIGA, MAIL_APP_NAME, MEMBER_ROLE, MEMBER_SUBSCRIBER, VPN_CONNECTIONS } from '@proton/shared/lib/constants';
 import { emailValidator, requiredValidator } from '@proton/shared/lib/helpers/formValidators';
 import { Domain, FAMILY_PLAN_INVITE_STATE, Member, Organization } from '@proton/shared/lib/interfaces';
 import clamp from '@proton/utils/clamp';
 
 import {
+    Alert,
+    ConfirmModal,
     InputFieldTwo,
     ModalTwo as Modal,
     ModalTwoContent as ModalContent,
     ModalTwoFooter as ModalFooter,
     ModalTwoHeader as ModalHeader,
     ModalStateProps,
+    Toggle,
     useFormErrors,
 } from '../../components';
-import { useApi, useEventManager, useLoading, useNotifications } from '../../hooks';
+import { useApi, useEventManager, useLoading, useModals, useNotifications } from '../../hooks';
 import Addresses from '../addresses/Addresses';
 import MemberStorageSelector, { getStorageRange, getTotalStorage } from './MemberStorageSelector';
 
@@ -31,20 +34,30 @@ interface Props extends ModalStateProps {
 const UserInviteOrEditModal = ({ organization, domains, member, ...modalState }: Props) => {
     const api = useApi();
     const { call } = useEventManager();
+    const { createModal } = useModals();
     const [submitting, withLoading] = useLoading();
     const { createNotification } = useNotifications();
     const { validator, onFormSubmit } = useFormErrors();
     const totalStorage = getTotalStorage(member ?? {}, organization);
     const storageRange = getStorageRange(member ?? {}, organization);
     const storageSizeUnit = GIGA;
-
     const isEditing = !!member?.ID;
     const isInvitationPending = member?.State === FAMILY_PLAN_INVITE_STATE.STATUS_INVITED;
+    const hasVPN = Boolean(organization.MaxVPN);
+    const canMakeAdmin = !member?.Self && !isInvitationPending && member?.Role === MEMBER_ROLE.ORGANIZATION_MEMBER;
+    const canRevokeAdmin = !member?.Self && !isInvitationPending && member?.Role === MEMBER_ROLE.ORGANIZATION_ADMIN;
+    const canUpdateVPNConnection = isEditing && !isInvitationPending && hasVPN;
 
-    const [model, setModel] = useState({
-        address: '',
-        storage: member ? member.MaxSpace : clamp(500 * GIGA, storageRange.min, storageRange.max),
-    });
+    const initialModel = useMemo(
+        () => ({
+            address: '',
+            storage: member ? member.MaxSpace : clamp(500 * GIGA, storageRange.min, storageRange.max),
+            vpn: !!member?.MaxVPN,
+            admin: member?.Role === MEMBER_ROLE.ORGANIZATION_ADMIN,
+        }),
+        [member]
+    );
+    const [model, setModel] = useState(initialModel);
 
     const handleClose = () => {
         if (submitting) {
@@ -63,12 +76,48 @@ const UserInviteOrEditModal = ({ organization, domains, member, ...modalState }:
         createNotification({ text: c('Success').t`Invitation sent` });
     };
 
+    const revokeAdmin = async () => {
+        try {
+            await new Promise((resolve, reject) => {
+                createModal(
+                    <ConfirmModal
+                        onClose={reject}
+                        onConfirm={() => resolve(undefined)}
+                        title={c('Title').t`Change role`}
+                    >
+                        <Alert className="mb-4">
+                            {member!.Subscriber === MEMBER_SUBSCRIBER.PAYER
+                                ? c('Info')
+                                      .t`This user is currently responsible for payments for your organization. By demoting this member, you will become responsible for payments for your organization.`
+                                : c('Info')
+                                      .t`Are you sure you want to remove administrative privileges from this user?`}
+                        </Alert>
+                    </ConfirmModal>
+                );
+            });
+
+            await api(updateRole(member!.ID, MEMBER_ROLE.ORGANIZATION_MEMBER));
+        } catch (e) {
+            /* do nothing, user declined confirm-modal to revoke admin rights from member */
+        }
+    };
+
     const editInvitation = async () => {
         //Editing a pending invitation uses a different endpoint than updating a user that accepted the invite
         if (isInvitationPending) {
             await api(editMemberInvitation(member!.ID, model.storage));
         } else {
-            await api(updateQuota(member!.ID, model.storage));
+            await Promise.all([
+                initialModel.storage !== model.storage && api(updateQuota(member!.ID, model.storage)),
+                canUpdateVPNConnection &&
+                    initialModel.vpn !== model.vpn &&
+                    api(updateVPN(member!.ID, model.vpn ? VPN_CONNECTIONS : 0)),
+                canMakeAdmin &&
+                    model.admin &&
+                    initialModel.admin !== model.admin &&
+                    api(updateRole(member!.ID, MEMBER_ROLE.ORGANIZATION_ADMIN)),
+                canRevokeAdmin && !model.admin && initialModel.admin !== model.admin && revokeAdmin(),
+            ]);
         }
 
         createNotification({ text: c('familyOffer_2023:Success').t`Member updated` });
@@ -138,6 +187,32 @@ const UserInviteOrEditModal = ({ organization, domains, member, ...modalState }:
                     totalStorage={totalStorage}
                     onChange={handleChange('storage')}
                 />
+
+                {canUpdateVPNConnection ? (
+                    <div className="flex flex-align-center mb-5">
+                        <label className="text-semibold mr-4" htmlFor="vpn-toggle">
+                            {c('Label for new member').t`VPN connections`}
+                        </label>
+                        <Toggle
+                            id="vpn-toggle"
+                            checked={model.vpn}
+                            onChange={({ target }) => setModel({ ...model, vpn: target.checked })}
+                        />
+                    </div>
+                ) : null}
+
+                {(canMakeAdmin || canRevokeAdmin) && (
+                    <div className="flex flex-align-center mb-6">
+                        <label className="text-semibold mr-4" htmlFor="admin-toggle">
+                            {c('Label for new member').t`Admin`}
+                        </label>
+                        <Toggle
+                            id="admin-toggle"
+                            checked={model.admin}
+                            onChange={({ target }) => setModel({ ...model, admin: target.checked })}
+                        />
+                    </div>
+                )}
 
                 {isEditing && member.State === FAMILY_PLAN_INVITE_STATE.STATUS_ENABLED && (
                     <div>
