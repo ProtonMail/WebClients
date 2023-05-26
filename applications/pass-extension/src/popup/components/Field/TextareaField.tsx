ff0a1702c08e441c8a8916056cd40efa04c8da25
Alexander Lemons <alexander.lemons@proton.ch>
2023-05-26 15:10:18 +0200

    single line masked textarea field
    [IDTEAM-1264]

--- applications/pass-extension/src/popup/components/Field/TextareaField.tsx
+++ applications/pass-extension/src/popup/components/Field/TextareaField.tsx
@@ -1,11 +1,11 @@
-import { type VFC, useState } from 'react';
+import { type VFC, forwardRef, useEffect, useRef, useState } from 'react';
 
 import { type FieldProps } from 'formik';
 
 import { InputFieldTwo, TextAreaTwo } from '@proton/components';
 import { type InputFieldProps } from '@proton/components/components/v2/field/InputField';
 import { pipe } from '@proton/pass/utils/fp';
-import { replaceAllRetainFormat } from '@proton/pass/utils/string';
+import { isEmptyString } from '@proton/pass/utils/string';
 import clsx from '@proton/utils/clsx';
 
 import { useFieldControl } from '../../hooks/useFieldControl';
@@ -15,55 +15,61 @@ import { FieldBox, type FieldBoxProps } from './Layout/FieldBox';
 
 export type BaseTextAreaFieldProps = FieldProps & InputFieldProps<typeof TextAreaTwo>;
 
-export const BaseTextAreaField: VFC<BaseTextAreaFieldProps> = ({
-    className,
-    form,
-    field,
-    labelContainerClassName,
-    meta,
-    onKeyDown,
-    onPaste,
-    ...props
-}) => {
-    const { error } = useFieldControl({ form, field, meta });
-    const pasteLengthLimiter = usePasteLengthLimiter();
-    const maxLengthLimiter = useMaxLengthLimiter();
+export const BaseTextAreaField: VFC<BaseTextAreaFieldProps> = forwardRef(
+    ({ className, form, field, labelContainerClassName, meta, onKeyDown, onPaste, ...props }, ref) => {
+        const { error } = useFieldControl({ form, field, meta });
+        const pasteLengthLimiter = usePasteLengthLimiter();
+        const maxLengthLimiter = useMaxLengthLimiter();
 
-    return (
-        <InputFieldTwo
-            as={TextAreaTwo}
-            autoGrow
-            unstyled
-            assistContainerClassName="hidden-empty"
-            className={clsx('border-none flex p-0 resize-none', className)}
-            error={error}
-            labelContainerClassName={clsx(
-                'm-0 text-normal text-sm',
-                error ? 'color-danger' : 'color-weak',
-                labelContainerClassName
-            )}
-            minRows={1}
-            rows={5}
-            {...field}
-            {...props}
-            onPaste={props.maxLength ? pasteLengthLimiter(props.maxLength, onPaste) : onPaste}
-            onKeyDown={props.maxLength ? maxLengthLimiter(props.maxLength, onKeyDown) : onKeyDown}
-        />
-    );
-};
+        return (
+            <InputFieldTwo
+                as={TextAreaTwo}
+                autoGrow
+                unstyled
+                assistContainerClassName="hidden-empty"
+                className={clsx('border-none flex p-0 resize-none', className)}
+                error={error}
+                labelContainerClassName={clsx(
+                    'm-0 text-normal text-sm',
+                    error ? 'color-danger' : 'color-weak',
+                    labelContainerClassName
+                )}
+                minRows={1}
+                ref={ref}
+                rows={5}
+                {...field}
+                {...props}
+                onPaste={props.maxLength ? pasteLengthLimiter(props.maxLength, onPaste) : onPaste}
+                onKeyDown={props.maxLength ? maxLengthLimiter(props.maxLength, onKeyDown) : onKeyDown}
+            />
+        );
+    }
+);
 
 export const BaseMaskedTextAreaField: VFC<BaseTextAreaFieldProps> = ({ form, field, ...rest }) => {
+    const { value } = field;
     const [masked, setMasked] = useState<boolean>(true);
-    const maskedValue = replaceAllRetainFormat(field.value, '•');
+    const ref = useRef<HTMLTextAreaElement>(null);
+    const isEmpty = isEmptyString(value);
+
+    useEffect(() => {
+        if (!masked) {
+            setTimeout(() => ref?.current?.scrollIntoView({ behavior: 'smooth' }), 50);
+        }
+    }, [masked]);
+
+    const maskedValue = !masked || isEmpty ? value : '••••••••••••••••';
 
     return (
         <BaseTextAreaField
-            className={clsx(masked && 'text-monospace')}
+            className={clsx(!isEmpty && masked && 'text-monospace')}
             onFocus={() => setMasked(false)}
             onBlur={pipe(field.onBlur, () => setMasked(true))}
             form={form}
             field={field}
-            value={masked ? maskedValue : field.value}
+            ref={ref}
+            rows={masked ? 1 : rest.rows}
+            value={maskedValue}
             {...rest}
         />
     );
