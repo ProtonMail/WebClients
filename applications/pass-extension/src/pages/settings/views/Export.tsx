bdf3a3ab3d8c9744a78fee6566bfa07cb671c8c2
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2023-05-30 13:35:50 +0200

    Default PGP export encryption to disabled
    [IDTEAM-1380]

--- applications/pass-extension/src/pages/settings/views/Export.tsx
+++ applications/pass-extension/src/pages/settings/views/Export.tsx
@@ -3,7 +3,6 @@ import type { FC } from 'react';
 import { c } from 'ttag';
 
 import { Card } from '@proton/atoms/Card';
-import { PASS_APP_NAME } from '@proton/shared/lib/constants';
 
 import { Exporter } from '../../../shared/components/export';
 
@@ -12,11 +11,6 @@ export const Export: FC = () => {
         <>
             <Card rounded className="mb-4 p-3 relative">
                 <strong className="color-norm block">{c('Label').t`Export`}</strong>
-                <em className="block text-sm color-weak mt-1">
-                    {c('Info')
-                        .t`To securely export your ${PASS_APP_NAME} data, please choose a strong password. ${PASS_APP_NAME} exports use PGP encryption : if you lose your passphrase, you will not be able to access your exported data.`}
-                </em>
-
                 <hr className="mt-2 mb-4 border-weak" />
                 <Exporter />
             </Card>
