68a57189c7c1bac4758c003d205c399567688bb9
mmso <mmso@mmso.se>
2023-05-30 09:54:34 +0200

    Add start and end username character validator

--- packages/shared/lib/helpers/formValidators.ts
+++ packages/shared/lib/helpers/formValidators.ts
@@ -1,13 +1,19 @@
 import { c, msgid } from 'ttag';
 
 import { validateEmailAddress } from './email';
-import { REGEX_USERNAME, isNumber } from './validators';
+import { REGEX_USERNAME, REGEX_USERNAME_END, REGEX_USERNAME_START, isNumber } from './validators';
 
 export const requiredValidator = (value: any) =>
     value === undefined || value === null || value?.trim?.() === '' ? c('Error').t`This field is required` : '';
 
 export const usernameCharacterValidator = (value: string) =>
     !REGEX_USERNAME.test(value) ? c('Error').t`Try using only letters, numerals, and _.-` : '';
+
+export const usernameStartCharacterValidator = (value: string) =>
+    !REGEX_USERNAME_START.test(value) ? c('Error').t`Username must begin with a letter or digit` : '';
+
+export const usernameEndCharacterValidator = (value: string) =>
+    !REGEX_USERNAME_END.test(value) ? c('Error').t`Username must end with a letter or digit` : '';
 
 const defaultUsernameLength = 40;
 export const usernameLengthValidator = (value: string, n = defaultUsernameLength) =>
