dc1c2f16a90c5907be9a9a3452046183a4699b40
Edvin CANDON <edvin.candon@proton.ch>
2023-05-31 22:58:49 +0200

    Fine-tune autofill creator

--- applications/pass-extension/src/shared/form/autofill.ts
+++ applications/pass-extension/src/shared/form/autofill.ts
@@ -7,18 +7,21 @@
  * attach their event listeners not directly on the input
  * elements (ie: account.google.com)
  */
-export const createAutofill = (input: HTMLInputElement) => (data: string) => {
-    const focused = input === document.activeElement;
-    input.value = data;
+export const createAutofill =
+    (input: HTMLInputElement) =>
+    (data: string): HTMLInputElement => {
+        const focused = input === document.activeElement;
+        input.value = data;
 
-    const events = [
-        !focused && new FocusEvent('focusin'),
-        new KeyboardEvent('keydown', { bubbles: true }),
-        new InputEvent('input', { data, bubbles: true }),
-        new KeyboardEvent('keyup', { bubbles: true }),
-        new KeyboardEvent('keypress', { bubbles: true }) /* support legacy websites */,
-        !focused && new FocusEvent('blur'),
-    ].filter(Boolean) as Event[];
+        const events = [
+            !focused && new FocusEvent('focusin'),
+            new KeyboardEvent('keydown', { bubbles: true }),
+            new InputEvent('input', { data, bubbles: true }),
+            new KeyboardEvent('keyup', { bubbles: true }),
+            new KeyboardEvent('keypress', { bubbles: true }) /* support legacy websites */,
+            !focused && new FocusEvent('blur'),
+        ].filter(Boolean) as Event[];
 
-    events.forEach((event) => input.dispatchEvent(event));
-};
+        events.forEach((event) => input.dispatchEvent(event));
+        return input;
+    };
