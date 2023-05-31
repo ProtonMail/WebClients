85781f679683e9e27747f2894ab5219f9d4041f2
Krzysztof Stępniak <krzysztof.stepniak@proton.ch>
2023-05-31 08:59:35 +0200

    Update bitcoin e2e payments

--- packages/components/containers/payments/BitcoinDetails.tsx
+++ packages/components/containers/payments/BitcoinDetails.tsx
@@ -23,7 +23,7 @@ const BitcoinDetails = ({ amount, address }: Props) => {
             ) : null}
             <div className="flex max-w100 flex-nowrap flex-align-items-center p-4 border-bottom">
                 <span className="flex-item-noshrink">{c('Label').t`BTC address:`}</span>
-                <strong className="ml-1 mr-4 text-ellipsis" title={address}>
+                <strong className="ml-1 mr-4 text-ellipsis" title={address} data-testid="btc-address">
                     {address}
                 </strong>
                 <Copy value={address} />
