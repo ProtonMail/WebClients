53c0d74234ea08e4df669fef10db013f7a0a8f6b
Edvin CANDON <edvin.candon@proton.ch>
2023-05-31 23:03:18 +0200

    Add flagging utilities for forms & fields

--- applications/pass-extension/src/content/constants.ts
+++ applications/pass-extension/src/content/constants.ts
@@ -1,19 +1,20 @@
 import browser from '@proton/pass/globals/browser';
+import { FormField, FormType } from '@proton/pass/types';
 
 import type { FormTrackerFieldConfig } from './types';
-import { DropdownAction, FieldInjectionRule, FormField, FormType } from './types';
+import { DropdownAction, FieldInjectionRule } from './types';
 
 export const EXTENSION_PREFIX = 'protonpass';
 export const CONTENT_SCRIPT_INJECTED_MESSAGE = `${EXTENSION_PREFIX}-cs-injected`;
 export const INPUT_STYLES_ATTR = `data-${EXTENSION_PREFIX}-styles-reset`;
 export const DETECTED_FORM_ID_ATTR = `data-${EXTENSION_PREFIX}-form-id`;
-export const PROCESSED_INPUT_ATTR = `data-${EXTENSION_PREFIX}-processed`;
+export const PROCESSED_ATTR = `data-${EXTENSION_PREFIX}-processed`;
 export const ICON_ROOT_CLASSNAME = `${EXTENSION_PREFIX}-input`;
 export const ICON_WRAPPER_CLASSNAME = `${ICON_ROOT_CLASSNAME}--wrapper`;
 export const ICON_CLASSNAME = `${ICON_ROOT_CLASSNAME}--icon`;
 export const ICON_SVG = `${ICON_ROOT_CLASSNAME}--svg`;
 
-export const ICON_PADDING = 8;
+export const ICON_PADDING = 5;
 export const ICON_MAX_HEIGHT = 25;
 export const ICON_MIN_HEIGHT = 18;
 export const DROPDOWN_WIDTH = 250;
