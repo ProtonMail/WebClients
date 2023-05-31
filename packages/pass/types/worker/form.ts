1c88c6f6d694f8d045dbd5fa994476d95dd8502d
Edvin CANDON <edvin.candon@proton.ch>
2023-05-31 23:02:00 +0200

    Move form|field type definitions to packages/pass

--- packages/pass/types/worker/form.ts
+++ packages/pass/types/worker/form.ts
@@ -3,6 +3,26 @@ import type { WithAutoSavePromptOptions } from './autosave';
 import type { TabId } from './runtime';
 
 export type FormIdentifier = `${TabId}:${string}`;
+/* Form types based on protonpass-fathom
+ * predicted form types */
+export enum FormType {
+    LOGIN = 'login',
+    REGISTER = 'register',
+    PASSWORD_CHANGE = 'password-change',
+    RECOVERY = 'recovery',
+    MFA = 'mfa',
+    NOOP = 'noop',
+}
+
+/* Form field types based on protonpass-fathom
+ * predicted form field */
+export enum FormField {
+    EMAIL = 'email',
+    USERNAME = 'username',
+    USERNAME_HIDDEN = 'username-hidden',
+    PASSWORD_CURRENT = 'password',
+    PASSWORD_NEW = 'new-password',
+}
 
 export enum FormEntryStatus {
     STAGING,
@@ -12,7 +32,7 @@ export enum FormEntryStatus {
 export type FormEntryBase = {
     domain: string;
     subdomain: MaybeNull<string>;
-    type: 'login' | 'register';
+    type: FormType;
     action?: string /* form action attribute */;
 };
 
