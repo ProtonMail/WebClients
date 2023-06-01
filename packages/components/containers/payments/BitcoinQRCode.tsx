6c3d130bf3f99f81e33c68819a2344abfc8da029
Alexey Karpov <alexey.karpov@proton.ch>
2023-06-01 15:20:12 +0200

    New design and simpler UX for Bitcoin payments
    PAY-719

--- packages/components/containers/payments/BitcoinQRCode.tsx
+++ packages/components/containers/payments/BitcoinQRCode.tsx
@@ -1,14 +1,37 @@
 import { ComponentProps } from 'react';
 
-import { QRCode } from '../../components';
+import { CircleLoader } from '@proton/atoms';
+import clsx from '@proton/utils/clsx';
 
-interface OwnProps {
+import { Icon, QRCode } from '../../components';
+
+import './BitcoinQRCode.scss';
+
+export interface OwnProps {
     amount: number;
     address: string;
+    status: 'initial' | 'pending' | 'confirmed';
 }
-const BitcoinQRCode = ({ amount, address, ...rest }: OwnProps & Omit<ComponentProps<typeof QRCode>, 'value'>) => {
+
+const BitcoinQRCode = ({
+    amount,
+    address,
+    status,
+    className,
+    ...rest
+}: OwnProps & Omit<ComponentProps<typeof QRCode>, 'value'>) => {
     const url = `bitcoin:${address}?amount=${amount}`;
-    return <QRCode value={url} {...rest} />;
+    const blurred = status === 'pending' || status === 'confirmed' ? 'blurred' : null;
+
+    return (
+        <div className="border rounded relative p-6">
+            <QRCode value={url} className={clsx(className, blurred)} {...rest} />
+            {status === 'pending' && <CircleLoader size="medium" className="absolute-center" />}
+            {status === 'confirmed' && (
+                <Icon name="checkmark-circle" size={36} className="absolute-center color-success" />
+            )}
+        </div>
+    );
 };
 
 export default BitcoinQRCode;
