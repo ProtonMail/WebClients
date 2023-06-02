8f8c32c0782266352fea9fb9b05550a8af25a6c5
Jack Crompton <jack.crompton@proton.ch>
2023-06-02 14:08:03 +0200

    Add @proton/recovery-kit
    CP-6026

---
+++ packages/recovery-kit/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    setupFilesAfterEnv: ['./jest.setup.js'],
+    preset: 'ts-jest',
+    testRegex: 'tests/.*\\.test\\.ts$',
+    clearMocks: true,
+    collectCoverage: true,
+    coverageReporters: ['text', 'lcov', 'cobertura'],
+    coverageThreshold: {
+        global: {
+            branches: 90,
+            functions: 100,
+            lines: 97,
+            statements: 97,
+        },
+    },
+};
