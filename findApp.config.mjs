dce9e978eba99eec34e4e6b883004109ea85d336
Jack Crompton <jack.crompton@proton.ch>
2023-06-02 14:14:27 +0200

    Add pdf-ui application
    CP-6026

--- findApp.config.mjs
+++ findApp.config.mjs
@@ -73,6 +73,6 @@ export default {
         directory: 'applications',
         globIgnore: [TEST_FILES_GLOB],
         sourcesGlob: 'src/app/**/*.{js,jsx,ts,tsx}',
-        ignore: ['storybook', 'preview-sandbox'],
+        ignore: ['storybook', 'preview-sandbox', 'pdf-ui'],
     },
 };
