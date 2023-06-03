413486e7e07b1a4f1341578471a07daa9d3628c5
Edvin CANDON <edvin.candon@proton.ch>
2023-06-03 21:36:47 +0200

    Exclude secure protocols in autofill candidates when on `http` [PRO-01-009]
    [IDTEAM-1415]

--- packages/pass/utils/search/match-realm.ts
+++ packages/pass/utils/search/match-realm.ts
@@ -1,13 +1,17 @@
-import { parse } from 'tldts';
-
-import type { Item } from '@proton/pass/types';
+import type { Item, ItemType } from '@proton/pass/types';
 import type { ItemMatchFunc } from '@proton/pass/utils/search';
 import { matchAny } from '@proton/pass/utils/search';
 
+import { prop, truthy } from '../fp';
 import { isLoginItem } from '../pass/items';
+import { parseUrl } from '../url';
 
-export const parseLoginItemUrls = ({ content }: Item<'login'>) =>
-    content.urls.map((url) => parse(url).hostname).filter(Boolean) as string[];
+export const parseLoginItemUrls = ({ content }: Item<'login'>, isSecure: boolean) =>
+    content.urls
+        .map((url) => parseUrl(url))
+        .filter((itemUrl) => isSecure || !itemUrl.isSecure)
+        .map(prop('hostname'))
+        .filter(truthy);
 
-export const matchLoginItemsByUrl: ItemMatchFunc = (item) => (url: string) =>
-    isLoginItem(item) && matchAny(parseLoginItemUrls(item))(url);
+export const matchLoginItemsByUrl: ItemMatchFunc<ItemType, boolean> = (item) => (url: string, isSecure?: boolean) =>
+    isLoginItem(item) && matchAny(parseLoginItemUrls(item, isSecure ?? true))(url);
