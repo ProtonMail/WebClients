96703755de2806cd81a2213acd5d3ba435d71c3b
Edvin CANDON <edvin.candon@proton.ch>
2023-06-04 10:10:06 +0200

    Improve protocol / top-level domain matching [PRO-01-009]
    - Add protocol property to `parseUrl` result
    - Support direct protocol matching via `protocolFilter`
    - Handle `isPrivate` flag in `matchLoginItemByDomain`
    - Reflect changes in autofill candidate selection
    
    [IDTEAM-1415]

--- packages/pass/utils/search/match-any.ts
+++ packages/pass/utils/search/match-any.ts
@@ -2,6 +2,5 @@ import { normalize } from '@proton/shared/lib/helpers/string';
 
 export const matchAny = (terms: string[]) => (match: string) => {
     const normalizedSearchTerm = normalize(match, true);
-
     return terms.map((s) => normalize(s, true)).some((s) => s.includes(normalizedSearchTerm));
 };
