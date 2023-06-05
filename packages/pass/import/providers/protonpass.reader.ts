403b1099424453d49b420f978852172799351edd
Edvin CANDON <edvin.candon@proton.ch>
2023-06-05 12:55:29 +0200

    Only import aliases which belong to user in import sequence
    - Add `userId` to export payload
    - When trying to import aliases ensure the export userId matches the
      currnet userId
    - Report ignored aliases in import report

--- packages/pass/import/providers/protonpass.reader.ts
+++ packages/pass/import/providers/protonpass.reader.ts
@@ -1,20 +1,22 @@
 import JSZip from 'jszip';
 import { c } from 'ttag';
 
-import type { ExportPayload } from '@proton/pass/export/types';
+import type { ExportPayload, ExportedItem } from '@proton/pass/export/types';
 import { pageMessage, sendMessage } from '@proton/pass/extension/message';
 import { type ItemImportIntent, ItemState, WorkerMessageType } from '@proton/pass/types';
+import { partition } from '@proton/pass/utils/array';
+import { prop } from '@proton/pass/utils/fp';
 import { logger } from '@proton/pass/utils/logger';
 import { uniqueId } from '@proton/pass/utils/string';
 import { PASS_APP_NAME } from '@proton/shared/lib/constants';
 import { base64StringToUint8Array } from '@proton/shared/lib/helpers/encoding';
 
 import { ImportReaderError } from '../helpers/reader.error';
-import type { ImportPayload } from '../types';
+import type { ImportPayload, ImportVault } from '../types';
 
 type ProtonPassReaderPayload =
-    | { data: ArrayBuffer; encrypted: false }
-    | { data: string; encrypted: true; passphrase: string };
+    | { data: ArrayBuffer; encrypted: false; userId?: string }
+    | { data: string; encrypted: true; passphrase: string; userId?: string };
 
 export const readProtonPassData = async (payload: ProtonPassReaderPayload): Promise<ImportPayload> => {
     const zipBuffer = payload.encrypted
@@ -53,25 +55,45 @@ export const readProtonPassData = async (payload: ProtonPassReaderPayload): Prom
 
         if (exportData === undefined) throw new Error();
 
-        const { vaults } = JSON.parse(exportData) as ExportPayload;
+        const parsedExport = JSON.parse(exportData) as ExportPayload;
+        const { userId } = parsedExport;
+
+        /* when trying to import alias items : make sure the userId between
+         * the exported file and the current userId match. We won't be able
+         * to re-create the aliases if they're not user-owned */
+        const aliasOwnedByUser = (item: ExportedItem) =>
+            item.data.type === 'alias' ? userId === payload.userId : true;
+
+        const vaults = Object.values(parsedExport.vaults).map<{ vault: ImportVault; ignored: string[] }>(
+            ({ name, items }) => {
+                const [itemsToImport, ignoredAliases] = partition(items, aliasOwnedByUser);
+
+                return {
+                    vault: {
+                        type: 'new',
+                        vaultName: name,
+                        id: uniqueId(),
+                        items: itemsToImport.map(
+                            (item) =>
+                                ({
+                                    ...item.data,
+                                    ...(item.data.type === 'alias'
+                                        ? { extraData: { aliasEmail: item.aliasEmail! } }
+                                        : {}),
+                                    trashed: item.state === ItemState.Trashed,
+                                    createTime: item.createTime,
+                                    modifyTime: item.modifyTime,
+                                } as ItemImportIntent)
+                        ),
+                    },
+                    ignored: ignoredAliases.map((item) => `[Alias] ${item.aliasEmail}`),
+                };
+            }
+        );
 
         return {
-            vaults: Object.values(vaults).map(({ name, items }) => ({
-                type: 'new',
-                vaultName: name,
-                id: uniqueId(),
-                items: items.map(
-                    (item) =>
-                        ({
-                            ...item.data,
-                            ...(item.data.type === 'alias' ? { extraData: { aliasEmail: item.aliasEmail! } } : {}),
-                            trashed: item.state === ItemState.Trashed,
-                            createTime: item.createTime,
-                            modifyTime: item.modifyTime,
-                        } as ItemImportIntent)
-                ),
-            })),
-            ignored: [],
+            vaults: vaults.map(prop('vault')),
+            ignored: vaults.flatMap(prop('ignored')),
             warnings: [],
         };
     } catch (e) {
