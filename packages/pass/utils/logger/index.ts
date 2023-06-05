e6e955d88c42b9406ed184f1f81d076347c38d5b
Edvin CANDON <edvin.candon@proton.ch>
2023-06-05 11:12:26 +0200

    Rollback loglevel to info

--- packages/pass/utils/logger/index.ts
+++ packages/pass/utils/logger/index.ts
@@ -2,7 +2,7 @@ import log from 'loglevel';
 
 import type { Callback } from '@proton/pass/types';
 
-log.setLevel('debug');
+log.setLevel('info');
 
 export const logId = (id: string) =>
     id.length > 10 ? `[${id.slice(0, 5)}…${id.slice(id.length - 5, id.length)}]` : `[${id}]`;
