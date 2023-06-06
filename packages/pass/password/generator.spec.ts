226911546c6bf318429fc23365aa694ded7e479a
Edvin CANDON <edvin.candon@proton.ch>
2023-06-06 10:04:19 +0200

    Support interpolating and capitalization options in memorable password generator
    [IDTEAM-1261]

--- packages/pass/password/generator.spec.ts
+++ packages/pass/password/generator.spec.ts
@@ -4,60 +4,79 @@ import WORD_LIST from './wordlist.json';
 
 const WORDS = Object.values(WORD_LIST);
 
+const wordsInList = (words: string[], options: WordListOptions) =>
+    words.every((word) => {
+        let originalWord = word.toLowerCase();
+        if (options.extraNumbers) originalWord = originalWord.slice(0, -1);
+        return WORDS.includes(originalWord);
+    });
+
+const wordsMatchCapitalization = (words: string[], options: WordListOptions) =>
+    words.every((word) => (options.capitalize ? /[A-Z]/.test(word[0]) : true));
+
+const wordsMatchExtraNumbers = (words: string[], options: WordListOptions) =>
+    words.every((word) => (options.extraNumbers ? /[0-9]/.test(word[word.length - 1]) : true));
+
 describe('password generators', () => {
     test('should throw if wordCount is 0', () => {
-        expect(() => generateFromWordList({ wordCount: 0, seperator: SeperatorOptions.HYPHEN })).toThrow();
+        expect(() =>
+            generateFromWordList({ wordCount: 0, seperator: SeperatorOptions.HYPHEN, capitalize: false })
+        ).toThrow();
     });
 
     test('generate from wordlist with basic seperators', () => {
         const options: WordListOptions[] = [
-            { wordCount: 1, seperator: SeperatorOptions.HYPHEN },
-            { wordCount: 4, seperator: SeperatorOptions.HYPHEN },
-            { wordCount: 4, seperator: SeperatorOptions.COMMA },
-            { wordCount: 4, seperator: SeperatorOptions.PERIOD },
-            { wordCount: 4, seperator: SeperatorOptions.UNDERSCORE },
-            { wordCount: 4, seperator: SeperatorOptions.SPACE },
-            { wordCount: 5, seperator: SeperatorOptions.HYPHEN },
-            { wordCount: 10, seperator: SeperatorOptions.SPACE },
-            { wordCount: 15, seperator: SeperatorOptions.UNDERSCORE },
+            { wordCount: 1, seperator: SeperatorOptions.HYPHEN, capitalize: false },
+            { wordCount: 4, seperator: SeperatorOptions.HYPHEN, capitalize: false },
+            { wordCount: 4, seperator: SeperatorOptions.COMMA, capitalize: true },
+            { wordCount: 4, seperator: SeperatorOptions.PERIOD, capitalize: false },
+            { wordCount: 4, seperator: SeperatorOptions.UNDERSCORE, capitalize: false },
+            { wordCount: 4, seperator: SeperatorOptions.SPACE, capitalize: false },
+            { wordCount: 5, seperator: SeperatorOptions.HYPHEN, capitalize: true },
+            { wordCount: 10, seperator: SeperatorOptions.SPACE, capitalize: false },
+            { wordCount: 15, seperator: SeperatorOptions.UNDERSCORE, capitalize: true },
         ];
 
-        options.forEach(({ wordCount, seperator }) => {
-            const result = generateFromWordList({ wordCount, seperator });
-            const words = result.split(seperator);
+        options.forEach((options) => {
+            const result = generateFromWordList(options);
+            const words = result.split(options.seperator);
             expect(result).toBeDefined();
-            expect(words.length).toEqual(wordCount);
-            expect(words.every((word) => WORDS.includes(word))).toBe(true);
+            expect(words.length).toEqual(options.wordCount);
+            expect(wordsInList(words, options)).toBe(true);
+            expect(wordsMatchCapitalization(words, options)).toBe(true);
+            expect(wordsMatchExtraNumbers(words, options)).toBe(true);
         });
     });
 
     test('generate from wordlist with random number seperator', () => {
         const options: WordListOptions[] = [
-            { wordCount: 4, seperator: SeperatorOptions.NUMBER },
-            { wordCount: 5, seperator: SeperatorOptions.NUMBER },
-            { wordCount: 10, seperator: SeperatorOptions.NUMBER },
-            { wordCount: 15, seperator: SeperatorOptions.NUMBER },
+            { wordCount: 4, seperator: SeperatorOptions.NUMBER, capitalize: false },
+            { wordCount: 5, seperator: SeperatorOptions.NUMBER, capitalize: true },
+            { wordCount: 10, seperator: SeperatorOptions.NUMBER, capitalize: true },
+            { wordCount: 15, seperator: SeperatorOptions.NUMBER, capitalize: false },
         ];
 
-        options.forEach(({ wordCount, seperator }) => {
-            const result = generateFromWordList({ wordCount, seperator });
+        options.forEach((options) => {
+            const result = generateFromWordList(options);
             const words = result.split(new RegExp(digits.split('').join('|')));
             expect(result).toBeDefined();
-            expect(words.length).toEqual(wordCount);
-            expect(words.every((word) => WORDS.includes(word))).toBe(true);
+            expect(words.length).toEqual(options.wordCount);
+            expect(wordsInList(words, options)).toBe(true);
+            expect(wordsMatchCapitalization(words, options)).toBe(true);
+            expect(wordsMatchExtraNumbers(words, options)).toBe(true);
         });
     });
 
     test('generate from wordlist with random number or symbol seperator', () => {
         const options: WordListOptions[] = [
-            { wordCount: 4, seperator: SeperatorOptions.NUMBER_OR_SYMBOL },
-            { wordCount: 5, seperator: SeperatorOptions.NUMBER_OR_SYMBOL },
-            { wordCount: 10, seperator: SeperatorOptions.NUMBER_OR_SYMBOL },
-            { wordCount: 15, seperator: SeperatorOptions.NUMBER_OR_SYMBOL },
+            { wordCount: 4, seperator: SeperatorOptions.NUMBER_OR_SYMBOL, capitalize: false },
+            { wordCount: 5, seperator: SeperatorOptions.NUMBER_OR_SYMBOL, capitalize: true },
+            { wordCount: 10, seperator: SeperatorOptions.NUMBER_OR_SYMBOL, capitalize: true },
+            { wordCount: 15, seperator: SeperatorOptions.NUMBER_OR_SYMBOL, capitalize: false },
         ];
 
-        options.forEach(({ wordCount, seperator }) => {
-            const result = generateFromWordList({ wordCount, seperator });
+        options.forEach((options) => {
+            const result = generateFromWordList(options);
             const words = result.split(
                 new RegExp(
                     digitsAndSymbols
@@ -67,8 +86,34 @@ describe('password generators', () => {
                 )
             );
             expect(result).toBeDefined();
-            expect(words.length).toEqual(wordCount);
-            expect(words.every((word) => WORDS.includes(word))).toBe(true);
+            expect(words.length).toEqual(options.wordCount);
+            expect(wordsInList(words, options)).toBe(true);
+            expect(wordsMatchCapitalization(words, options)).toBe(true);
+            expect(wordsMatchExtraNumbers(words, options)).toBe(true);
+        });
+    });
+
+    test('should support interpolating extra numbers', () => {
+        const options: WordListOptions[] = [
+            { wordCount: 1, seperator: SeperatorOptions.HYPHEN, capitalize: false, extraNumbers: true },
+            { wordCount: 4, seperator: SeperatorOptions.HYPHEN, capitalize: false, extraNumbers: true },
+            { wordCount: 4, seperator: SeperatorOptions.COMMA, capitalize: true, extraNumbers: true },
+            { wordCount: 4, seperator: SeperatorOptions.PERIOD, capitalize: false, extraNumbers: true },
+            { wordCount: 4, seperator: SeperatorOptions.UNDERSCORE, capitalize: false, extraNumbers: true },
+            { wordCount: 4, seperator: SeperatorOptions.SPACE, capitalize: false, extraNumbers: true },
+            { wordCount: 5, seperator: SeperatorOptions.HYPHEN, capitalize: true, extraNumbers: true },
+            { wordCount: 10, seperator: SeperatorOptions.SPACE, capitalize: false, extraNumbers: true },
+            { wordCount: 15, seperator: SeperatorOptions.UNDERSCORE, capitalize: true, extraNumbers: true },
+        ];
+
+        options.forEach((options) => {
+            const result = generateFromWordList(options);
+            const words = result.split(options.seperator);
+            expect(result).toBeDefined();
+            expect(words.length).toEqual(options.wordCount);
+            expect(wordsInList(words, options)).toBe(true);
+            expect(wordsMatchCapitalization(words, options)).toBe(true);
+            expect(wordsMatchExtraNumbers(words, options)).toBe(true);
         });
     });
 });
