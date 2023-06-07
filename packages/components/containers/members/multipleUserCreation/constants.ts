b91a5065134c523be55540c9fbb0214d1bd701ec
Jack Crompton <jack.crompton@proton.ch>
2023-06-07 15:36:27 +0200

    Increase multi user creation MAX_NUMBER_OF_USER_ROWS to 750

--- packages/components/containers/members/multipleUserCreation/constants.ts
+++ packages/components/containers/members/multipleUserCreation/constants.ts
@@ -3,4 +3,4 @@ import { BASE_SIZE } from '@proton/shared/lib/constants';
 export const MAX_FILENAME_CHARS_DISPLAY = 100;
 export const MAX_IMPORT_FILE_SIZE = 10 * BASE_SIZE ** 2;
 export const MAX_IMPORT_FILE_SIZE_STRING = '10 MB';
-export const MAX_NUMBER_OF_USER_ROWS = 500;
+export const MAX_NUMBER_OF_USER_ROWS = 750;
