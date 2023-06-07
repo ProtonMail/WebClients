3f11bf952632315937e6824ba2dfe44024a31b1f
lpeslier <lucien.peslier@proton.ch>
2023-06-07 16:01:13 +0200

    Rename from root link

--- applications/drive/src/app/store/_devices/useDevicesActions.ts
+++ applications/drive/src/app/store/_devices/useDevicesActions.ts
@@ -1,11 +1,14 @@
 import { useApi, usePreventLeave } from '@proton/components';
 import { queryDeviceDeletion, queryDeviceRename } from '@proton/shared/lib/api/drive/devices';
 
+import useDevicesListing from './useDevicesListing';
+
 /**
  * useDevicesActions provides actions for manipulating with devices.
  */
 export default function useDevicesActions() {
     const { preventLeave } = usePreventLeave();
+    const { renameCachedDevice, removeCachedDevice } = useDevicesListing();
     const api = useApi();
 
     const remove = async (deviceId: string, abortSignal?: AbortSignal) => {
@@ -13,19 +16,26 @@ export default function useDevicesActions() {
             api({
                 ...queryDeviceDeletion(deviceId),
                 signal: abortSignal,
+            }).then(() => {
+                removeCachedDevice(deviceId);
             })
         );
-        // TODO: events polling
     };
 
-    const rename = async (params: { deviceId: string; newName: string }, abortSignal?: AbortSignal) => {
-        await preventLeave(
-            api({
-                ...queryDeviceRename(params.deviceId, { Name: params.newName }),
-                signal: abortSignal,
-            })
-        );
-        // TODO: events polling
+    const rename = async (
+        params: { deviceId: string; newName: string; haveLegacyName: boolean },
+        abortSignal?: AbortSignal
+    ) => {
+        if (params.haveLegacyName) {
+            await preventLeave(
+                api({
+                    ...queryDeviceRename(params.deviceId, { Name: '' }),
+                    signal: abortSignal,
+                }).then(() => {
+                    renameCachedDevice(params.deviceId, params.newName);
+                })
+            );
+        }
     };
 
     return {
