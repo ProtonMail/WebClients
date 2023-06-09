79ab0c6cfd0377b3e4f54de5ab7655ea76161f35
Edvin CANDON <edvin.candon@proton.ch>
2023-06-09 10:59:08 +0200

    Clean-up keeper importer

--- packages/pass/import/providers/keeper.types.ts
+++ packages/pass/import/providers/keeper.types.ts
@@ -1,11 +1,20 @@
 export type KeeperItem = [
-    string, // folder name
-    string, // title (can't be an empty string)
-    string, // username
-    string, // url
-    string, // password
-    string, // note
-    string, // shared folder name
-    // custom fields, KeeperItem[i] is the label of a custom field and KeeperItem[i + 1] is the value of that custom field, for 7 < i < KeeperItem.length-1
+    /* folder name */
+    string,
+    /* title (can't be an empty string) */
+    string,
+    /* username */
+    string,
+    /* url */
+    string,
+    /* password */
+    string,
+    /* note */
+    string,
+    /* shared folder name */
+    string,
+    /* custom fields:
+     * KeeperItem[7 + n]: label of a custom field
+     * KeeperItem[7 + (n + 1)]: value of that custom field */
     ...string[]
 ];
