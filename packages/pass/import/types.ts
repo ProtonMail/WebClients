afd7014c9be5adb6053c90e65e25c27ef21bf3f8
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2023-06-12 14:53:50 +0200

    Refactor ImportForm
    [IDTEAM-1474]

--- packages/pass/import/types.ts
+++ packages/pass/import/types.ts
@@ -1,3 +1,5 @@
+import { PASS_APP_NAME } from '@proton/shared/lib/constants';
+
 import type { ItemImportIntent } from '../types';
 
 export enum ImportProvider {
@@ -14,6 +16,75 @@ export enum ImportProvider {
     SAFARI = 'safari',
     KEEPER = 'keeper',
 }
+
+export const PROVIDER_INFO_MAP: Record<ImportProvider, { title: string; fileExtension: string; tutorialUrl: string }> =
+    {
+        [ImportProvider.BITWARDEN]: {
+            title: 'Bitwarden',
+            fileExtension: 'json',
+            tutorialUrl: 'https://proton.me/support/pass-import-bitwarden',
+        },
+        [ImportProvider.KEEPER]: {
+            title: 'Keeper',
+            fileExtension: 'csv',
+            tutorialUrl: '',
+        },
+        [ImportProvider.BRAVE]: {
+            title: 'Brave',
+            fileExtension: 'csv',
+            tutorialUrl: 'https://proton.me/support/pass-import-brave',
+        },
+        [ImportProvider.CHROME]: {
+            title: 'Chrome',
+            fileExtension: 'csv',
+            tutorialUrl: 'https://proton.me/support/pass-import-chrome',
+        },
+        [ImportProvider.EDGE]: {
+            title: 'Edge',
+            fileExtension: 'csv',
+            tutorialUrl: 'https://proton.me/support/pass-import-edge',
+        },
+        [ImportProvider.FIREFOX]: {
+            title: 'Firefox',
+            fileExtension: 'csv',
+            tutorialUrl: 'https://proton.me/support/pass-import-firefox',
+        },
+        [ImportProvider.KEEPASS]: {
+            title: 'KeePass',
+            fileExtension: 'xml',
+            tutorialUrl: 'https://proton.me/support/pass-import-keepass',
+        },
+        [ImportProvider.LASTPASS]: {
+            title: 'LastPass',
+            fileExtension: 'csv',
+            tutorialUrl: 'https://proton.me/support/pass-import-lastpass',
+        },
+        [ImportProvider.ONEPASSWORD]: {
+            title: '1Password',
+            fileExtension: '1pux, 1pif',
+            tutorialUrl: 'https://proton.me/support/pass-import-1password',
+        },
+        [ImportProvider.DASHLANE]: {
+            title: 'Dashlane',
+            fileExtension: 'zip',
+            tutorialUrl: 'https://proton.me/support/pass-import-dashlane',
+        },
+        [ImportProvider.PROTONPASS]: {
+            title: PASS_APP_NAME,
+            fileExtension: 'zip, pgp',
+            tutorialUrl: '',
+        },
+        [ImportProvider.SAFARI]: {
+            title: 'Safari',
+            fileExtension: 'csv',
+            tutorialUrl: 'https://proton.me/support/pass-import-safari',
+        },
+        [ImportProvider.KEEPER]: {
+            title: 'Keeper',
+            fileExtension: 'csv',
+            tutorialUrl: '',
+        },
+    };
 
 export const ImportProviderValues = Object.values(ImportProvider).sort((a, b) => a.localeCompare(b));
 
