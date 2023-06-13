822e6e0fe7a015ddba9ffc85bd41cb6a66a1f5ec
Edvin CANDON <edvin.candon@proton.ch>
2023-06-13 17:14:01 +0200

    Parametrize worker message broker creator
    - Specify allowed external messages via `allowExternal`
    - Allow adding `onDisconnect` callback : will be used to detect pop-up
      disconnecting in order to cache any draft state
    
    [IDTEAM-1115]

--- applications/pass-extension/src/worker/channel.ts
+++ applications/pass-extension/src/worker/channel.ts
@@ -1,5 +1,22 @@
 import { createMessageBroker } from '@proton/pass/extension/message';
+import { stateCache } from '@proton/pass/store';
+import { WorkerMessageType } from '@proton/pass/types';
 
-const WorkerMessageBroker = createMessageBroker();
+import store from './store';
+
+/* For security reasons : limit the type of messages that
+ * can be processed via externally connectable resources.
+ * When we detect a popup port being disconnected : this
+ * likely means the popup was closed : dispatch a cache request
+ * in order to save the latest popup state */
+const WorkerMessageBroker = createMessageBroker({
+    allowExternal: [
+        WorkerMessageType.ACCOUNT_FORK,
+        WorkerMessageType.ACCOUNT_EXTENSION,
+        WorkerMessageType.ACCOUNT_PROBE,
+        WorkerMessageType.ACCOUNT_ONBOARDING,
+    ],
+    onDisconnect: (portName) => portName.startsWith('popup') && store.dispatch(stateCache()),
+});
 
 export default WorkerMessageBroker;
