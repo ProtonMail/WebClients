adadc0bfd2fa479e52303aab43e7184b6193878f
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2023-06-13 17:55:18 +0200

    Add new colors and icons to dropdown items
    [IDTEAM-1500]

--- applications/pass-extension/src/content/injections/apps/dropdown/components/DropdownItem.tsx
+++ applications/pass-extension/src/content/injections/apps/dropdown/components/DropdownItem.tsx
@@ -9,29 +9,62 @@ import clsx from '@proton/utils/clsx';
 
 export const DROPDOWN_ITEM_HEIGHT = 60;
 
+const renderIcon = (icon: IconName | 'proton-pass-locked' | 'proton-pass-inactive') => {
+    switch (icon) {
+        case 'proton-pass-locked':
+            return (
+                <div className="mr-4 w-custom text-align-center" style={{ '--width-custom': '32px' }}>
+                    <img
+                        src={'/assets/protonpass-icon-padding-locked-light.svg'}
+                        width={32}
+                        height={32}
+                        className="ml-1"
+                        alt={c('Action').t`Toggle ${PASS_APP_NAME}`}
+                    />
+                </div>
+            );
+        case 'proton-pass-inactive':
+            return (
+                <div className="mr-4 w-custom text-align-center" style={{ '--width-custom': '32px' }}>
+                    <img
+                        src={'/assets/protonpass-icon-padding-inactive.svg'}
+                        width={32}
+                        height={32}
+                        className="ml-1"
+                        alt={c('Action').t`Toggle ${PASS_APP_NAME}`}
+                    />
+                </div>
+            );
+        default:
+            return <Icon name={icon} className="mr-4 item-icon" size={32} color="var(--interaction-norm)" />;
+    }
+};
+
 export const DropdownItem: VFC<{
     onClick?: () => void;
-    title: string;
+    title?: string;
     subTitle: ReactNode;
-    icon?: IconName;
+    icon?: IconName | 'proton-pass-locked' | 'proton-pass-inactive';
     disabled?: boolean;
+    removePointer?: boolean;
     autogrow?: boolean;
-}> = ({ onClick, title, subTitle, icon, disabled, autogrow }) => (
+    className?: 'ui-login' | 'ui-alias' | 'ui-password';
+}> = ({ onClick, title, subTitle, icon, disabled, removePointer, autogrow, className = 'ui-login' }) => (
     <DropdownMenuButton
-        className="text-left h-custom"
+        className={clsx('text-left h-custom', className, removePointer && 'cursor-default')}
         style={autogrow ? {} : { '--custom-height': `${DROPDOWN_ITEM_HEIGHT}px` }}
         onClick={onClick}
         disabled={disabled}
     >
         <div className="flex flex-align-items-center py-1">
             {icon !== undefined ? (
-                <Icon name={icon} className="mr-4 item-icon" size={24} color="#6D4AFF" />
+                renderIcon(icon)
             ) : (
-                <div className="mr-4 w-custom text-align-center" style={{ '--w-custom': '24px' }}>
+                <div className="mr-4 w-custom text-align-center" style={{ '--w-custom': '32px' }}>
                     <img
-                        src={'/assets/protonpass-icon.svg'}
-                        width={18}
-                        height={18}
+                        src={'/assets/protonpass-icon-padding.svg'}
+                        width={32}
+                        height={32}
                         className="ml-1"
                         alt={c('Action').t`Toggle ${PASS_APP_NAME}`}
                     />
@@ -39,7 +72,7 @@ export const DropdownItem: VFC<{
             )}
 
             <div className="flex-item-fluid">
-                <span className="block text-ellipsis">{title}</span>
+                {title && <span className="block text-ellipsis">{title}</span>}
                 <span className={clsx('block color-weak text-sm', !autogrow && 'text-ellipsis')}>{subTitle}</span>
             </div>
         </div>
