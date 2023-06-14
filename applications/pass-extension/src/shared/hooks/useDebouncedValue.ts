8cac800945e2b7ea3134ecc0b8a4f59dc79c518a
Alexander Lemons <alexander.lemons@proton.ch>
2023-06-14 08:34:40 +0200

    useDebouncedValue
    [IDTEAM-1459]

---
+++ applications/pass-extension/src/shared/hooks/useDebouncedValue.ts
@@ -0,0 +1,14 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import _debounce from '@proton/utils/debounce';
+
+export const useDebouncedValue = <T extends string | number>(valueIn: T, time: number): T => {
+    const [valueOut, setValueOut] = useState(valueIn);
+    const debounce = useCallback(
+        _debounce((value) => setValueOut(value), time, { leading: true }),
+        [time]
+    );
+    useEffect(() => debounce(valueIn), [valueIn]);
+    useEffect(() => () => debounce.cancel(), []);
+    return valueOut;
+};
