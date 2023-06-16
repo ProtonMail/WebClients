cd9ee33c846942db7a0b906491b69e794f3c0649
Edvin CANDON <edvin.candon@proton.ch>
2023-06-16 14:14:44 +0200

    Update popup tab state on item select & filtering options change
    [IDTEAM-1515]

--- applications/pass-extension/src/popup/context/items/ItemsFilteringContext.tsx
+++ applications/pass-extension/src/popup/context/items/ItemsFilteringContext.tsx
@@ -1,5 +1,7 @@
 import { type FC, createContext, useEffect, useMemo, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
+import { popupTabStateSave } from '@proton/pass/store/actions/creators/popup';
 import type { ItemSortFilter, ItemTypeFilter, MaybeNull } from '@proton/pass/types';
 import noop from '@proton/utils/noop';
 
@@ -40,6 +42,7 @@ const SEARCH_DEBOUNCE_TIME = 150;
 
 export const ItemsFilteringContextProvider: FC = ({ children }) => {
     const popup = usePopupContext();
+    const dispatch = useDispatch();
     const [search, setSearch] = useState<string>(popup.state.initialSearch);
     const debouncedSearch = useDebouncedValue(search, SEARCH_DEBOUNCE_TIME);
 
@@ -48,7 +51,18 @@ export const ItemsFilteringContextProvider: FC = ({ children }) => {
     const [shareId, setShareId] = useState<MaybeNull<string>>(null);
     const [shareBeingDeleted, setShareBeingDeleted] = useState<MaybeNull<string>>(null);
 
-    useEffect(() => {}, []);
+    /* when updating the sort filters : discard any
+     * selected item from the current popup tab */
+    useEffect(() => {
+        dispatch(
+            popupTabStateSave({
+                tabId: popup.context!.tabId,
+                domain: popup.url.subdomain ?? popup.url.domain ?? null,
+                selectedItem: null,
+                filters: { sort, type },
+            })
+        );
+    }, [sort, type]);
 
     const context: ItemsFilteringContextType = useMemo(
         () => ({
