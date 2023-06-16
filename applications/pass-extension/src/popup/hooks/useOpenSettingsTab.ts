7457ee0daf2aac294e3119b9fd49899e6608456b
Edvin CANDON <edvin.candon@proton.ch>
2023-06-16 16:20:01 +0200

    Ensure all browser api call errors are caught

--- applications/pass-extension/src/popup/hooks/useOpenSettingsTab.ts
+++ applications/pass-extension/src/popup/hooks/useOpenSettingsTab.ts
@@ -4,14 +4,16 @@ import browser from '@proton/pass/globals/browser';
 
 export const useOpenSettingsTab = () => {
     return useCallback(async (page?: string) => {
-        const settingsUrl = browser.runtime.getURL('/settings.html');
-        const url = `${settingsUrl}#/${page ?? ''}`;
-        const match = await browser.tabs.query({ url: settingsUrl });
+        try {
+            const settingsUrl = browser.runtime.getURL('/settings.html');
+            const url = `${settingsUrl}#/${page ?? ''}`;
+            const match = await browser.tabs.query({ url: settingsUrl });
 
-        await (match.length > 0 && match[0].id
-            ? browser.tabs.update(match[0].id, { highlighted: true, url })
-            : browser.tabs.create({ url }));
+            await (match.length > 0 && match[0].id
+                ? browser.tabs.update(match[0].id, { highlighted: true, url })
+                : browser.tabs.create({ url }));
 
-        window.close();
+            window.close();
+        } catch (_) {}
     }, []);
 };
