2ded13acba82f9c3de61b1d0d4a2149ad059c52c
Edvin CANDON <edvin.candon@proton.ch>
2023-06-16 12:19:04 +0200

    Standardise item filter names in `ItemsFilteringContext`

--- applications/pass-extension/src/popup/context/items/ItemsFilteringContext.tsx
+++ applications/pass-extension/src/popup/context/items/ItemsFilteringContext.tsx
@@ -1,4 +1,4 @@
-import { type FC, createContext, useMemo, useState } from 'react';
+import { type FC, createContext, useEffect, useMemo, useState } from 'react';
 
 import type { ItemSortFilter, ItemTypeFilter, MaybeNull } from '@proton/pass/types';
 import noop from '@proton/utils/noop';
@@ -10,12 +10,12 @@ export type ItemsFilteringContextType = {
     search: string;
     debouncedSearch: string;
     sort: ItemSortFilter;
-    filter: ItemTypeFilter;
+    type: ItemTypeFilter;
     shareId: MaybeNull<string>;
     shareBeingDeleted: MaybeNull<string>;
     setSearch: (value: string) => void;
     setSort: (value: ItemSortFilter) => void;
-    setFilter: (value: ItemTypeFilter) => void;
+    setType: (value: ItemTypeFilter) => void;
     setShareId: (shareId: MaybeNull<string>) => void;
     setShareBeingDeleted: (shareId: MaybeNull<string>) => void;
 };
@@ -26,12 +26,12 @@ export const ItemsFilteringContext = createContext<ItemsFilteringContextType>({
     search: '',
     debouncedSearch: '',
     sort: INITIAL_SORT,
-    filter: '*',
+    type: '*',
     shareId: null,
     shareBeingDeleted: null,
     setSearch: noop,
     setSort: noop,
-    setFilter: noop,
+    setType: noop,
     setShareId: noop,
     setShareBeingDeleted: noop,
 });
@@ -44,25 +44,27 @@ export const ItemsFilteringContextProvider: FC = ({ children }) => {
     const debouncedSearch = useDebouncedValue(search, SEARCH_DEBOUNCE_TIME);
 
     const [sort, setSort] = useState<ItemSortFilter>(INITIAL_SORT);
-    const [filter, setFilter] = useState<ItemTypeFilter>('*');
+    const [type, setType] = useState<ItemTypeFilter>('*');
     const [shareId, setShareId] = useState<MaybeNull<string>>(null);
     const [shareBeingDeleted, setShareBeingDeleted] = useState<MaybeNull<string>>(null);
+
+    useEffect(() => {}, []);
 
     const context: ItemsFilteringContextType = useMemo(
         () => ({
             search,
             debouncedSearch,
             sort,
-            filter,
+            type,
             shareId,
             shareBeingDeleted,
             setSearch,
             setSort,
-            setFilter,
+            setType,
             setShareId,
             setShareBeingDeleted,
         }),
-        [search, debouncedSearch, sort, filter, shareId, shareBeingDeleted]
+        [search, debouncedSearch, sort, type, shareId, shareBeingDeleted]
     );
 
     return <ItemsFilteringContext.Provider value={context}>{children}</ItemsFilteringContext.Provider>;
