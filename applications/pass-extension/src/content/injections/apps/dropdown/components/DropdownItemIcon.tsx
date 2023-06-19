6e0e9557a7e3300b9e8b9b823a99f041b0484e96
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2023-06-19 17:49:21 +0200

    Refactor DropdownItemIcon into subcomponents
    [IDTEAM-1500]

--- applications/pass-extension/src/content/injections/apps/dropdown/components/DropdownItemIcon.tsx
+++ applications/pass-extension/src/content/injections/apps/dropdown/components/DropdownItemIcon.tsx
@@ -1,120 +1,23 @@
 import type { VFC } from 'react';
-import { useCallback, useState } from 'react';
-
-import { c } from 'ttag';
 
 import type { IconName } from '@proton/components/components';
-import { Icon } from '@proton/components/components';
-import type { MaybeNull } from '@proton/pass/types';
-import { PassIcon } from '@proton/pass/types/data/pass-icon';
 import type { DropdownIcon } from '@proton/pass/types/data/pass-icon';
-import { PASS_APP_NAME } from '@proton/shared/lib/constants';
-import clsx from '@proton/utils/clsx';
 
-import { ImageStatus, ProxiedDomainImage } from '../../../../../shared/components/icon/ProxiedDomainImage';
+import { DropdownItemIconDomain } from './DropdownItemIconDomain';
+import { DropdownItemIconSvg } from './DropdownItemIconSvg';
 
 import '../../../../../popup/components/Item/ItemIcon.scss';
 
-export const DropdownItemIcon: VFC<{
-    icon?: DropdownIcon;
-    url?: string;
-    canLoadFavicons?: boolean;
-    className?: string;
-}> = ({ icon, url, canLoadFavicons, className }) => {
-    const [imageStatus, setImageStatus] = useState<ImageStatus>(ImageStatus.LOADING);
-    const handleStatusChange = useCallback((status: ImageStatus) => setImageStatus(status), []);
-    const iconFullSize = 36;
-    const faviconSize = Math.round(iconFullSize * 0.6);
-    const resolvePassIconAsset = (icon: DropdownIcon): MaybeNull<string> =>
-        Object.values<DropdownIcon>(PassIcon).includes(icon) ? `/assets/${icon}.svg` : null;
+export const ICON_SIZE = 36;
 
-    return (
-        <>
-            {(() => {
-                if (url) {
-                    return (
-                        <div
-                            className={clsx(
-                                'pass-item-icon w-custom h-custom rounded-xl overflow-hidden relative',
-                                className,
-                                url && imageStatus === ImageStatus.READY && 'pass-item-icon--has-image'
-                            )}
-                            style={{ '--width-custom': `${iconFullSize}px`, '--height-custom': `${iconFullSize}px` }}
-                        >
-                            {canLoadFavicons && (
-                                <ProxiedDomainImage
-                                    className={clsx(
-                                        'absolute-center w-custom h-custom',
-                                        imageStatus !== ImageStatus.READY && 'hidden'
-                                    )}
-                                    style={{
-                                        '--width-custom': `${faviconSize}px`,
-                                        '--height-custom': `${faviconSize}px`,
-                                    }}
-                                    onStatusChange={handleStatusChange}
-                                    status={imageStatus}
-                                    url={url ?? ''}
-                                />
-                            )}
-                            {!canLoadFavicons ||
-                                (imageStatus !== ImageStatus.READY && (
-                                    <Icon
-                                        className="absolute-center"
-                                        color="var(--interaction-norm)"
-                                        name="user"
-                                        size={20}
-                                    />
-                                ))}
-                        </div>
-                    );
-                } else if (icon) {
-                    const passIconAsset = resolvePassIconAsset(icon);
-                    return passIconAsset ? (
-                        <div
-                            className={clsx('w-custom text-align-center', className)}
-                            style={{ '--width-custom': `${iconFullSize}px` }}
-                        >
-                            <img
-                                src={passIconAsset}
-                                width={iconFullSize}
-                                height={iconFullSize}
-                                className="ml-1"
-                                alt={c('Action').t`Toggle ${PASS_APP_NAME}`}
-                            />
-                        </div>
-                    ) : (
-                        <div
-                            className={clsx(
-                                'pass-item-icon w-custom h-custom rounded-xl overflow-hidden relative',
-                                className
-                            )}
-                            style={{ '--width-custom': `${iconFullSize}px`, '--height-custom': `${iconFullSize}px` }}
-                        >
-                            <Icon
-                                className="absolute-center"
-                                name={icon as IconName}
-                                size={20}
-                                color="var(--interaction-norm)"
-                            />
-                        </div>
-                    );
-                } else {
-                    return (
-                        <div
-                            className={clsx('w-custom text-align-center', className)}
-                            style={{ '--width-custom': `${iconFullSize}px` }}
-                        >
-                            <img
-                                src={resolvePassIconAsset(PassIcon.ACTIVE) as string}
-                                width={iconFullSize}
-                                height={iconFullSize}
-                                className="ml-1"
-                                alt={c('Action').t`Toggle ${PASS_APP_NAME}`}
-                            />
-                        </div>
-                    );
-                }
-            })()}
-        </>
+export const DropdownItemIcon: VFC<{
+    icon: DropdownIcon;
+    url?: string;
+    className?: string;
+}> = ({ icon, url, className }) => {
+    return url ? (
+        <DropdownItemIconDomain url={url} icon={icon as IconName} className={className} size={ICON_SIZE} />
+    ) : (
+        <DropdownItemIconSvg icon={icon} className={className} size={ICON_SIZE} />
     );
 };
---
+++ applications/pass-extension/src/content/injections/apps/dropdown/components/DropdownItemIconDomain.tsx
@@ -0,0 +1,46 @@
+import type { VFC } from 'react';
+import { useCallback, useState } from 'react';
+
+import type { IconName } from '@proton/components/components';
+import { Icon } from '@proton/components/components';
+import clsx from '@proton/utils/clsx';
+
+import { ImageStatus, ProxiedDomainImage } from '../../../../../shared/components/icon/ProxiedDomainImage';
+
+import '../../../../../popup/components/Item/ItemIcon.scss';
+
+export const DropdownItemIconDomain: VFC<{
+    url: string;
+    icon: IconName;
+    size?: number;
+    className?: string;
+}> = ({ url, icon, size = 36, className }) => {
+    const [imageStatus, setImageStatus] = useState<ImageStatus>(ImageStatus.LOADING);
+    const handleStatusChange = useCallback((status: ImageStatus) => setImageStatus(status), []);
+    const faviconSize = Math.round(size * 0.6);
+
+    return (
+        <div
+            className={clsx(
+                'pass-item-icon w-custom h-custom rounded-xl overflow-hidden relative',
+                className,
+                url && imageStatus === ImageStatus.READY && 'pass-item-icon--has-image'
+            )}
+            style={{ '--width-custom': `${size}px`, '--height-custom': `${size}px` }}
+        >
+            <ProxiedDomainImage
+                className={clsx('absolute-center w-custom h-custom', imageStatus !== ImageStatus.READY && 'hidden')}
+                style={{
+                    '--width-custom': `${faviconSize}px`,
+                    '--height-custom': `${faviconSize}px`,
+                }}
+                onStatusChange={handleStatusChange}
+                status={imageStatus}
+                url={url ?? ''}
+            />
+            {imageStatus !== ImageStatus.READY && (
+                <Icon className="absolute-center" color="var(--interaction-norm)" name={icon} size={20} />
+            )}
+        </div>
+    );
+};
