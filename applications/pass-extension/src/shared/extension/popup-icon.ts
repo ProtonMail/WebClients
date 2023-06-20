91a3f5e3176b017edc85e50ffbef7cb0f0e3db1b
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2023-06-20 15:07:02 +0200

    Display Pass locked icon in browser toolbar
    [IDTEAM-1500]

--- applications/pass-extension/src/shared/extension/popup-icon.ts
+++ applications/pass-extension/src/shared/extension/popup-icon.ts
@@ -1,13 +1,20 @@
 import browser from '@proton/pass/globals/browser';
 import noop from '@proton/utils/noop';
 
-export const setPopupIcon = (options: { loggedIn: boolean }): Promise<void> =>
-    browser.action.setIcon({
+export const setPopupIcon = (options: { loggedIn: boolean; locked: boolean }): Promise<void> => {
+    let iconStatus = '';
+    if (options.locked) {
+        iconStatus = '-locked';
+    } else if (!options.loggedIn) {
+        iconStatus = '-disabled';
+    }
+    return browser.action.setIcon({
         path: {
-            16: `./assets/protonpass-icon-16${options.loggedIn ? '' : '-disabled'}.png`,
-            32: `./assets/protonpass-icon-32${options.loggedIn ? '' : '-disabled'}.png`,
+            16: `./assets/protonpass-icon-16${iconStatus}.png`,
+            32: `./assets/protonpass-icon-32${iconStatus}.png`,
         },
     });
+};
 
 /* this function should gracefully fail if the tabId
  * does not exist or has been discarded when calling it*/
