0790ed5dae3776f742600a0d27d63572e646a0b8
Edvin CANDON <edvin.candon@proton.ch>
2023-06-22 00:49:50 +0200

    Fix icon flickering glitch on worker status change
    [IDTEAM-1500]

--- applications/pass-extension/src/worker/context/factory.ts
+++ applications/pass-extension/src/worker/context/factory.ts
@@ -1,9 +1,16 @@
 import { backgroundMessage } from '@proton/pass/extension/message';
 import type { WorkerStatus } from '@proton/pass/types';
 import { type Api, WorkerMessageType } from '@proton/pass/types';
-import { waitUntil } from '@proton/pass/utils/fp';
+import { or, waitUntil } from '@proton/pass/utils/fp';
 import { logger } from '@proton/pass/utils/logger';
-import { workerLocked, workerLoggedOut, workerReady, workerStatusResolved } from '@proton/pass/utils/worker';
+import {
+    workerErrored,
+    workerLocked,
+    workerLoggedOut,
+    workerReady,
+    workerStale,
+    workerStatusResolved,
+} from '@proton/pass/utils/worker';
 import { setUID as setSentryUID } from '@proton/shared/lib/helpers/sentry';
 
 import { setPopupIcon } from '../../shared/extension';
@@ -83,9 +90,10 @@ export const createWorkerContext = (options: { api: Api; status: WorkerStatus })
             logger.info(`[Worker::Context] Status update : ${context.status} -> ${status}`);
             context.status = status;
 
-            if (workerLoggedOut(status)) void setPopupIcon({ loggedIn: false, locked: false });
-            if (workerLocked(status)) void setPopupIcon({ loggedIn: false, locked: true });
-            if (workerReady(status)) void setPopupIcon({ loggedIn: true, locked: false });
+            void setPopupIcon({
+                disabled: or(workerLoggedOut, workerErrored, workerStale)(status),
+                locked: workerLocked(status),
+            });
 
             WorkerMessageBroker.ports.broadcast(
                 backgroundMessage({
