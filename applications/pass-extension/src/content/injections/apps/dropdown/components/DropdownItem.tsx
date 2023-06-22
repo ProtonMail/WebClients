cfc67131fa76e69acd79a0cbd8d9ba63026d19c7
Edvin CANDON <edvin.candon@proton.ch>
2023-06-22 09:08:35 +0200

    Fine-tune autofill|autosuggest revamp
    - Move `userEmail` resolution to `IFrameContextProvider`
    - Simplify and enforce type-safety in `DropdownItemIcon` props
    - Avoid flickering glitches in `DropdownItem` subtitle renders
    
    [IDTEAM-1500]

--- applications/pass-extension/src/content/injections/apps/dropdown/components/DropdownItem.tsx
+++ applications/pass-extension/src/content/injections/apps/dropdown/components/DropdownItem.tsx
@@ -1,25 +1,26 @@
 import type { ReactNode, VFC } from 'react';
 
 import { DropdownMenuButton } from '@proton/components/components';
-import type { DropdownIcon } from '@proton/pass/types/data/pass-icon';
 import clsx from '@proton/utils/clsx';
 
+import type { DropdownItemprops } from './DropdownItemIcon';
 import { DropdownItemIcon } from './DropdownItemIcon';
 
 export const DROPDOWN_ITEM_HEIGHT = 60;
 
-export const DropdownItem: VFC<{
-    onClick?: () => void;
-    title?: string;
-    subTitle: ReactNode;
-    icon: DropdownIcon;
-    url?: string;
-    disabled?: boolean;
-    autogrow?: boolean;
-    /* FIXME once theming is implemented: rename className prop to itemType or similar
-     * and automatically derive the className using something similar to itemTypeToItemClassName[itemType] */
-    className?: 'ui-login' | 'ui-alias' | 'ui-password';
-}> = ({ onClick, title, subTitle, icon, url, disabled, autogrow, className = 'ui-login' }) => (
+export const DropdownItem: VFC<
+    {
+        onClick?: () => void;
+        title?: string;
+        subTitle: ReactNode;
+        disabled?: boolean;
+        autogrow?: boolean;
+        /* FIXME once theming is implemented: rename className prop to
+         * itemType or similar and automatically derive the className
+         * using something similar to itemTypeToItemClassName[itemType] */
+        className?: 'ui-login' | 'ui-alias' | 'ui-password';
+    } & DropdownItemprops
+> = ({ onClick, title, subTitle, icon, url, disabled, autogrow, className = 'ui-login' }) => (
     <DropdownMenuButton
         className={clsx('text-left h-custom', className)}
         style={autogrow ? {} : { '--h-custom': `${DROPDOWN_ITEM_HEIGHT}px` }}
@@ -27,7 +28,7 @@ export const DropdownItem: VFC<{
         disabled={disabled}
     >
         <div className="flex flex-align-items-center py-1">
-            <DropdownItemIcon icon={icon} url={url} className="mr-4" />
+            <DropdownItemIcon {...(url ? { url, icon } : { icon })} />
             <div className="flex-item-fluid">
                 {title && <span className="block text-ellipsis">{title}</span>}
                 <span className={clsx('block color-weak text-sm', !autogrow && 'text-ellipsis')}>{subTitle}</span>
