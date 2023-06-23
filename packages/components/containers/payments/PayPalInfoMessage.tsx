1eb0350084d29dcc3b68a75a2efdb0ef0c3d71e9
Alexey Karpov <alexey.karpov@proton.ch>
2023-06-23 12:04:55 +0200

    Add short paypal view
    PAY-1040

---
+++ packages/components/containers/payments/PayPalInfoMessage.tsx
@@ -0,0 +1,10 @@
+import { c } from 'ttag';
+
+const PayPalInfoMessage = () => {
+    const payPalInfoMessage = c('Info')
+        .t`We will redirect you to PayPal in a new browser tab to complete this transaction. If you use any pop-up blockers, please disable them to continue.`;
+
+    return <div className="mb-4">{payPalInfoMessage}</div>;
+};
+
+export default PayPalInfoMessage;
