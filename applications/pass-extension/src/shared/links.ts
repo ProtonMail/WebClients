7deb1c1ccc5666ca987447d653d4f35b2a1aa496
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2023-06-23 12:49:27 +0200

    Refactor pin detection and moved urls to constants
    [IDTEAM-1537]

---
+++ applications/pass-extension/src/shared/links.ts
@@ -0,0 +1,5 @@
+export enum OnboardingLink {
+    ANDROID = 'https://play.google.com/store/apps/details?id=proton.android.pass',
+    IOS = 'https://apps.apple.com/us/app/proton-pass-password-manager/id6443490629',
+    YOUTUBE = 'https://www.youtube.com/embed/Nm4DCAjePOM?cc_load_policy=1',
+}
