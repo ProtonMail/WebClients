c2fe4b81e285998fe3fff1f3e77b2a7e03b1902b
Edvin CANDON <edvin.candon@proton.ch>
2023-06-26 14:29:38 +0200

    HOTFIX: only try to sync features & user plan if logged in

--- packages/pass/store/sagas/feature-flags.saga.ts
+++ packages/pass/store/sagas/feature-flags.saga.ts
@@ -2,20 +2,22 @@ import { put, select, takeLeading } from 'redux-saga/effects';
 
 import { setUserFeatures, wakeupSuccess } from '../actions';
 import type { UserFeatureState } from '../reducers';
-import type { State } from '../types';
+import type { State, WorkerRootSagaOptions } from '../types';
 import { getUserFeatures } from './workers/user';
 
 /* Try to sync the user feature flags on each wakeup success :
 /* `getUserFeatures` will only request pass feature flags from the api
  * if the `requestedAt` timestamp is more than a day old */
-function* syncFeatures() {
+function* syncFeatures({ getAuth }: WorkerRootSagaOptions) {
     try {
-        const { user }: State = yield select();
-        const features: UserFeatureState = yield getUserFeatures(user);
-        yield features !== user.features && put(setUserFeatures(features));
+        if (getAuth().hasSession()) {
+            const { user }: State = yield select();
+            const features: UserFeatureState = yield getUserFeatures(user);
+            yield features !== user.features && put(setUserFeatures(features));
+        }
     } catch (err) {}
 }
 
-export default function* watcher() {
-    yield takeLeading(wakeupSuccess.match, syncFeatures);
+export default function* watcher(options: WorkerRootSagaOptions) {
+    yield takeLeading(wakeupSuccess.match, syncFeatures, options);
 }
