f4031686ab6da7d51a4816b31ba01699f14beb0e
Edvin CANDON <edvin.candon@proton.ch>
2023-06-26 14:28:34 +0200

    Avoid relying on global import for authentication in sagas

--- packages/pass/store/types.ts
+++ packages/pass/store/types.ts
@@ -1,3 +1,5 @@
+import type { AuthenticationStore } from '@proton/shared/lib/authentication/createAuthenticationStore';
+
 import type { ExtensionEndpoint } from '../types';
 import type { TelemetryEvent } from '../types/data/telemetry';
 import type * as actions from './actions';
@@ -9,6 +11,7 @@ export type State = ReturnType<typeof rootReducer>;
 export type Action = ReturnType<(typeof actions)[keyof typeof actions]>;
 
 export interface WorkerRootSagaOptions {
+    getAuth: () => AuthenticationStore;
     onBoot?: (result: { ok: true } | { ok: false; clearCache: boolean }) => void;
     onCacheRequest: () => boolean;
     onItemsChange?: () => void;
