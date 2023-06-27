ec029d3f032482567be299b19ca8501bd546f60e
mmso <mmso@mmso.se>
2023-06-27 22:13:50 +0200

    Fix pass signup url

--- applications/account/src/app/content/helper.ts
+++ applications/account/src/app/content/helper.ts
@@ -1,9 +1,10 @@
 import type { ActiveSessionData } from '@proton/components/containers/app/SSOForkProducer';
 import { SSOType } from '@proton/components/containers/app/SSOForkProducer';
-import { APPS, SSO_PATHS } from '@proton/shared/lib/constants';
+import { getIsPassApp } from '@proton/shared/lib/authentication/apps';
+import { APP_NAMES, SSO_PATHS } from '@proton/shared/lib/constants';
 import { stringifySearchParams } from '@proton/shared/lib/helpers/url';
 
-export const getSignupUrl = (forkState: ActiveSessionData | undefined) => {
+export const getSignupUrl = (forkState: ActiveSessionData | undefined, app: APP_NAMES | undefined) => {
     const { path, params } = (() => {
         if (forkState?.type === SSOType.OAuth) {
             return { path: SSO_PATHS.SIGNUP, params: {} };
@@ -11,15 +12,16 @@ export const getSignupUrl = (forkState: ActiveSessionData | undefined) => {
 
         if (forkState?.type === SSOType.Proton) {
             const params = { plan: forkState.payload.plan || undefined };
-            if (
-                [APPS.PROTONPASS, APPS.PROTONEXTENSION, APPS.PROTONPASSBROWSEREXTENSION].includes(
-                    forkState.payload.app as any
-                )
-            ) {
+
+            if (getIsPassApp(forkState.payload.app)) {
                 return { path: SSO_PATHS.PASS_SIGNUP, params };
             }
 
             return { path: SSO_PATHS.SIGNUP, params };
+        }
+
+        if (getIsPassApp(app)) {
+            return { path: SSO_PATHS.PASS_SIGNUP, params: {} };
         }
 
         return { path: SSO_PATHS.SIGNUP, params: {} };
