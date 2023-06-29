6d6db66847dde6acca50275625fca0c7a03c170b
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-06-29 12:40:25 +0200

    Add create new account button to pass signup

--- applications/account/src/app/single-signup-v2/modals/SubUserModal.tsx
+++ applications/account/src/app/single-signup-v2/modals/SubUserModal.tsx
@@ -1,7 +1,7 @@
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms/Button';
-import { ModalProps, ModalTwo, ModalTwoContent, ModalTwoHeader } from '@proton/components';
+import { ModalProps, ModalTwo, ModalTwoContent, ModalTwoHeader, useLoading } from '@proton/components';
 import { Plan, PlansMap } from '@proton/shared/lib/interfaces';
 
 import { getFreeTitle } from '../helper';
@@ -13,10 +13,23 @@ interface Props extends ModalProps {
     currentPlan: Plan | undefined;
     upsellPlan: Plan | undefined;
     unlockPlan: Plan | undefined;
+    onContinue: () => void;
+    onSignOut: () => Promise<void>;
 }
 
-const SubUserModal = ({ appName, currentPlan, unlockPlan, plansMap, upsellPlan, ...rest }: Props) => {
+const SubUserModal = ({
+    appName,
+    currentPlan,
+    unlockPlan,
+    plansMap,
+    onContinue,
+    onClose,
+    onSignOut,
+    upsellPlan,
+    ...rest
+}: Props) => {
     const free = getFreeTitle(appName);
+    const [loading, withLoading] = useLoading();
     return (
         <ModalTwo {...rest} size="small">
             <ModalTwoHeader
@@ -35,8 +48,27 @@ const SubUserModal = ({ appName, currentPlan, unlockPlan, plansMap, upsellPlan, 
                     currentPlan={currentPlan}
                     unlockPlan={unlockPlan}
                 >
-                    <Button color="norm" fullWidth onClick={rest.onClose}>
+                    <Button
+                        color="norm"
+                        fullWidth
+                        className="mb-2"
+                        onClick={() => {
+                            onContinue();
+                            onClose?.();
+                        }}
+                    >
                         {c('pass_signup_2023: Action').t`Continue to ${free}`}
+                    </Button>
+                    <Button
+                        shape="ghost"
+                        color="norm"
+                        loading={loading}
+                        onClick={async () => {
+                            await withLoading(onSignOut().then(() => onClose?.()));
+                        }}
+                        fullWidth
+                    >
+                        {c('pass_signup_2023: Action').t`Create another account instead`}
                     </Button>
                 </PlanComparison>
             </ModalTwoContent>
