441b31975a96a276780534cb441d7635eab8ca76
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2023-06-30 16:08:17 +0200

    Fix unstable test
    Co-authored-by: Romain Sanchez <romain.sanchez@proton.ch>

--- applications/mail/src/app/components/eo/message/EOExpirationTime.test.tsx
+++ applications/mail/src/app/components/eo/message/EOExpirationTime.test.tsx
@@ -1,4 +1,4 @@
-import { addDays, addHours, addMinutes, addSeconds, getUnixTime } from 'date-fns';
+import { add, addHours, addMinutes, addSeconds, getUnixTime } from 'date-fns';
 
 import { Message } from '@proton/shared/lib/interfaces/mail/Message';
 
@@ -6,7 +6,26 @@ import { clearAll, render, tick } from '../../../helpers/test/helper';
 import EOExpirationTime from './EOExpirationTime';
 
 describe('EOExpirationTime', () => {
+    const date = new Date(2023, 0, 1, 10, 0, 1);
     const seconds = 50;
+
+    beforeAll(() => {
+        jest.useFakeTimers();
+    });
+
+    beforeEach(() => {
+        jest.clearAllTimers();
+        jest.setSystemTime(date);
+    });
+
+    afterAll(() => {
+        jest.useRealTimers();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+        clearAll();
+    });
 
     const setup = async (ExpirationTime: number) => {
         const result = await render(
@@ -21,14 +40,15 @@ describe('EOExpirationTime', () => {
         return { banner: result.queryByTestId('expiration-banner'), rerender };
     };
 
-    afterEach(() => {
-        jest.clearAllMocks();
-        clearAll();
-    });
-
     it('should display expiration banner as button and expected content', async () => {
         // Set expiration time to two days
-        const daysInSeconds = getUnixTime(addDays(new Date(), 1));
+        const daysInSeconds = getUnixTime(
+            add(date, {
+                hours: 23,
+                minutes: 59,
+                seconds: 59,
+            })
+        );
         const result = await setup(daysInSeconds);
         let { banner } = result;
 
@@ -36,25 +56,25 @@ describe('EOExpirationTime', () => {
         expect(banner?.textContent).toBe('Expires in less than 24 hours');
 
         // The message will expire in 0 day 3h 59min 59s, so we display "Expires in less than 4 hours"
-        const hoursInSeconds1 = getUnixTime(addHours(new Date(), 4));
+        const hoursInSeconds1 = getUnixTime(addHours(date, 4));
         banner = await result.rerender(hoursInSeconds1);
         await tick();
         expect(banner?.textContent).toBe('Expires in less than 4 hours');
 
         // The message will expire in 0 day 1h 59min 59s, so we display "Expires in less than 2 hour"
-        const hoursInSeconds2 = getUnixTime(addHours(new Date(), 2));
+        const hoursInSeconds2 = getUnixTime(addHours(date, 2));
         banner = await result.rerender(hoursInSeconds2);
         await tick();
         expect(banner?.textContent).toBe('Expires in less than 2 hours');
 
         // The message will expire in 0 day 0h 1min 59s, so we display "Expires in 2 minutes"
-        const minutesInSeconds = getUnixTime(addMinutes(new Date(), 2));
+        const minutesInSeconds = getUnixTime(addMinutes(date, 2));
         banner = await result.rerender(minutesInSeconds);
         await tick();
         expect(banner?.textContent).toBe('Expires in 2 minutes');
 
         // The message will expire in 0 day 0h 0min Xs, so we display "Expires in less than X seconds"
-        banner = await result.rerender(getUnixTime(addSeconds(new Date(), seconds)));
+        banner = await result.rerender(getUnixTime(addSeconds(date, seconds)));
         const value = Number(/\d+/.exec(banner?.textContent || '')?.[0]);
         await tick();
         expect(value).toBeLessThanOrEqual(seconds);
