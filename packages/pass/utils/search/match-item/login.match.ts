e9d77bef0e5b66d32500cc74b110a1c3b9d588a6
Alexander Lemons <alexander.lemons@proton.ch>
2023-07-04 16:38:15 +0200

    update credit card matchItem
    [IDTEAM-1479]

--- packages/pass/utils/search/match-item/login.match.ts
+++ packages/pass/utils/search/match-item/login.match.ts
@@ -1,7 +1,7 @@
 import type { ItemMatchFunc } from '@proton/pass/utils/search';
 import { matchAny } from '@proton/pass/utils/search';
 
-const matchesLoginItem: ItemMatchFunc<'login'> = ({ metadata: { name }, content: { username, urls } }) =>
-    matchAny([name, username, ...urls]);
+const matchesLoginItem: ItemMatchFunc<'login'> = ({ metadata: { name, note }, content: { username, urls } }) =>
+    matchAny([name, note, username, ...urls]);
 
 export default matchesLoginItem;
