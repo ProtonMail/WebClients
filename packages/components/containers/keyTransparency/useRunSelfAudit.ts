5ae00ed27501d94e2c8b8232d6bc9a8fe94876fc
M. Thiercelin <marin.thiercelin@proton.ch>
2023-07-07 17:55:14 +0200

    Add retry mechanism for self audit

--- packages/components/containers/keyTransparency/useRunSelfAudit.ts
+++ packages/components/containers/keyTransparency/useRunSelfAudit.ts
@@ -4,23 +4,19 @@ import {
     getAuditResult,
     getKTLocalStorage,
     getSelfAuditInterval,
-    ktSentryReportError,
     selfAudit,
     storeAuditResult,
 } from '@proton/key-transparency/lib';
 import { getSilentApi } from '@proton/shared/lib/api/helpers/customConfig';
-import { KeyTransparencyActivation, KeyTransparencyState } from '@proton/shared/lib/interfaces';
 import { getPrimaryKey } from '@proton/shared/lib/keys';
 
 import { useApi, useConfig, useGetAddresses, useGetUserKeys, useUser } from '../../hooks';
 import useGetLatestEpoch from './useGetLatestEpoch';
-import useKTActivation from './useKTActivation';
 import useReportSelfAuditErrors from './useReportSelfAuditErrors';
 import useSaveSKLToLS from './useSaveSKLToLS';
 import useUploadMissingSKL from './useUploadMissingSKL';
 
-const useRunSelfAudit = (setKTState: (state: KeyTransparencyState) => void) => {
-    const ktActivation = useKTActivation();
+const useRunSelfAudit = () => {
     const getAddresses = useGetAddresses();
     const getUserKeys = useGetUserKeys();
     const getLatestEpoch = useGetLatestEpoch();
@@ -34,54 +30,33 @@ const useRunSelfAudit = (setKTState: (state: KeyTransparencyState) => void) => {
     const selfAuditInterval = getSelfAuditInterval();
 
     const runSelfAudit = async () => {
-        try {
-            if (ktActivation === KeyTransparencyActivation.DISABLED) {
-                return;
-            }
-            const userKeys = await getUserKeys();
-            const userPrivateKeys = userKeys.map(({ privateKey }) => privateKey);
-            const { publicKey: userPrimaryPublicKey } = getPrimaryKey(userKeys) || {};
-            if (!userPrimaryPublicKey) {
-                throw new Error('User has no user keys');
-            }
-            const lastSelfAudit = await getAuditResult(userID, userPrivateKeys, ktLSAPI);
-            const lastSelfAuditTime = lastSelfAudit?.auditTime ?? 0;
-            const elapsedTime = +serverTime() - lastSelfAuditTime;
-            let timer = selfAuditInterval;
-            if (elapsedTime > selfAuditInterval) {
-                try {
-                    const addresses = await getAddresses();
-                    const selfAuditResult = await selfAudit(
-                        userID,
-                        api,
-                        addresses,
-                        userKeys,
-                        ktLSAPI,
-                        saveSKLToLS,
-                        getLatestEpoch,
-                        uploadMissingSKL
-                    );
-                    await reportSelfAuditErrors(selfAuditResult);
-                    if (selfAuditResult) {
-                        await storeAuditResult(userID, selfAuditResult, userPrimaryPublicKey, ktLSAPI);
-                        setKTState({ selfAuditResult });
-                    }
-                } catch (error: any) {
-                    ktSentryReportError(error, { context: 'SelfAudit' });
-                    return;
-                }
-            } else {
-                setKTState({ selfAuditResult: lastSelfAudit ?? undefined });
-                timer = elapsedTime;
-            }
-
-            // Repeat every expectedEpochInterval (4h)
-            setTimeout(() => {
-                void runSelfAudit();
-            }, timer);
-        } catch (error: any) {
-            ktSentryReportError(error, { context: 'runSelfAudit' });
+        const userKeys = await getUserKeys();
+        const userPrivateKeys = userKeys.map(({ privateKey }) => privateKey);
+        const { publicKey: userPrimaryPublicKey } = getPrimaryKey(userKeys) || {};
+        if (!userPrimaryPublicKey) {
+            throw new Error('User has no user keys');
         }
+        const lastSelfAudit = await getAuditResult(userID, userPrivateKeys, ktLSAPI);
+        const lastSelfAuditTime = lastSelfAudit?.auditTime ?? 0;
+        const elapsedTime = +serverTime() - lastSelfAuditTime;
+        let timer = selfAuditInterval;
+        if (elapsedTime > selfAuditInterval) {
+            const addresses = await getAddresses();
+            const selfAuditResult = await selfAudit(
+                userID,
+                api,
+                addresses,
+                userKeys,
+                ktLSAPI,
+                saveSKLToLS,
+                getLatestEpoch,
+                uploadMissingSKL
+            );
+            await reportSelfAuditErrors(selfAuditResult);
+            await storeAuditResult(userID, selfAuditResult, userPrimaryPublicKey, ktLSAPI);
+            return { selfAuditResult, nextSelfAuditInterval: timer };
+        }
+        return { selfAuditResult: lastSelfAudit, nextSelfAuditInterval: timer };
     };
 
     return runSelfAudit;
