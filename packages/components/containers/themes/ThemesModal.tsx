a91061243a257117f09e046068940092f096a0d2
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-07-07 11:16:13 +0200

    Remove accessibility settings feature flag

--- packages/components/containers/themes/ThemesModal.tsx
+++ packages/components/containers/themes/ThemesModal.tsx
@@ -5,41 +5,35 @@ import { BRAND_NAME } from '@proton/shared/lib/constants';
 import { ColorScheme, ThemeModeSetting, getThemes } from '@proton/shared/lib/themes/themes';
 
 import { Info, ModalProps, ModalTwo, ModalTwoContent, ModalTwoFooter, ModalTwoHeader, Toggle } from '../../components';
-import { FeatureCode } from '../../containers/features/FeaturesContext';
-import { useFeature } from '../../hooks';
 import ThemeCards from './ThemeCards';
 import { useTheme } from './ThemeProvider';
 import ThemeSyncModeCard from './ThemeSyncModeCard';
 
 const ThemesModal = (props: ModalProps) => {
-    const isAccessibilitySettingsEnabled =
-        useFeature<boolean>(FeatureCode.AccessibilitySettings)?.feature?.Value === true;
     const { information, settings, setTheme, setAutoTheme } = useTheme();
 
-    const themes = getThemes(isAccessibilitySettingsEnabled);
+    const themes = getThemes();
 
     return (
         <ModalTwo size="large" {...props}>
             <ModalTwoHeader title={c('Title').t`Select a theme`} />
             <ModalTwoContent>
                 <p className="mb-4">{c('Info').t`Customize the look and feel of ${BRAND_NAME} applications.`}</p>
-                {isAccessibilitySettingsEnabled && (
-                    <div className="inline-flex flex-align-item-center mb-4">
-                        <label htmlFor="themeSyncToggle" className="text-semibold">
-                            <span className="mr-2">{c('Label').t`Synchronize with system`}</span>
-                            <Info
-                                title={c('Tooltip')
-                                    .t`Automatically switch between your preferred themes for day and night in sync with your system’s day and night modes`}
-                            />
-                        </label>
-                        <Toggle
-                            id="themeSyncToggle"
-                            className="ml-6"
-                            checked={settings.Mode === ThemeModeSetting.Auto}
-                            onChange={(e) => setAutoTheme(e.target.checked)}
+                <div className="inline-flex flex-align-item-center mb-4">
+                    <label htmlFor="themeSyncToggle" className="text-semibold">
+                        <span className="mr-2">{c('Label').t`Synchronize with system`}</span>
+                        <Info
+                            title={c('Tooltip')
+                                .t`Automatically switch between your preferred themes for day and night in sync with your system’s day and night modes`}
                         />
-                    </div>
-                )}
+                    </label>
+                    <Toggle
+                        id="themeSyncToggle"
+                        className="ml-6"
+                        checked={settings.Mode === ThemeModeSetting.Auto}
+                        onChange={(e) => setAutoTheme(e.target.checked)}
+                    />
+                </div>
                 {settings.Mode === ThemeModeSetting.Auto ? (
                     <div className="flex flex-nowrap gap-4 on-mobile-flex-column">
                         <ThemeSyncModeCard
