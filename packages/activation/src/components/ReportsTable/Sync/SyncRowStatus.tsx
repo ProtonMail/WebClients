1a984c94088fa89194ad90295474230a352e771c
Flavien Bonvin <flavien.bonvin@proton.ch>
2023-07-10 11:04:53 +0200

    Add support for paused sync

--- packages/activation/src/components/ReportsTable/Sync/SyncRowStatus.tsx
+++ packages/activation/src/components/ReportsTable/Sync/SyncRowStatus.tsx
@@ -11,7 +11,8 @@ const SyncRowStatus = ({ state }: Props) => {
     switch (state) {
         case ApiSyncState.ACTIVE:
             return <Badge type="primary">{c('Import status').t`Active`}</Badge>;
-        case ApiSyncState.STOPPED:
+        case ApiSyncState.OFFLINE:
+        case ApiSyncState.EXPIRED:
             return <Badge type="warning">{c('Import status').t`Paused`}</Badge>;
     }
 
