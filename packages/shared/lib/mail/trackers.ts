88c5c72518675f0a3023c402ef67ee9ab84ce3b8
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2023-07-10 11:06:11 +0200

    Fix UTM tracker parser issue
    Check if URL is valid and add case for when parsing library throws.
    
    Co-authored-by: Romain Sanchez <romain.sanchez@proton.ch>

--- packages/shared/lib/mail/trackers.ts
+++ packages/shared/lib/mail/trackers.ts
@@ -3,16 +3,19 @@ import { TidyURL } from '@protontech/tidy-url';
 import { MessageUTMTracker } from '@proton/shared/lib/models/mailUtmTrackers';
 
 export const getUTMTrackersFromURL = (originalURL: string) => {
-    const { url, info } = TidyURL.clean(originalURL);
+    if (TidyURL.validate(originalURL)) {
+        const { url, info } = TidyURL.clean(originalURL);
 
-    let utmTracker: MessageUTMTracker | undefined;
-    if (info.removed.length > 0) {
-        utmTracker = {
-            originalURL: info.original,
-            cleanedURL: url,
-            removed: info.removed,
-        };
+        let utmTracker: MessageUTMTracker | undefined;
+        if (info.removed.length > 0) {
+            utmTracker = {
+                originalURL: info.original,
+                cleanedURL: url,
+                removed: info.removed,
+            };
+        }
+
+        return { url, info, utmTracker };
     }
-
-    return { url, info, utmTracker };
+    return undefined;
 };
