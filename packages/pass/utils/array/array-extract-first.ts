c67d43a08e89bdcc5638b75b477e6c6753f7d3f4
Edvin CANDON <edvin.candon@proton.ch>
2023-07-13 14:56:33 +0200

    Improve type inference in `extractFirst`

--- packages/pass/utils/array/array-extract-first.ts
+++ packages/pass/utils/array/array-extract-first.ts
@@ -1,6 +1,9 @@
 import type { MaybeNull } from '@proton/pass/types';
 
-export const extractFirst = <K extends T, T>(arr: T[], condition: (entry: T) => boolean) =>
+export const extractFirst = <T, K extends T>(
+    arr: T[],
+    condition: ((entry: T) => entry is K) | ((entry: T) => boolean)
+) =>
     arr.reduce<[MaybeNull<K>, T[]]>(
         (acc, entry) => {
             if (acc[0] === null && condition(entry)) acc[0] = entry as MaybeNull<K>;
