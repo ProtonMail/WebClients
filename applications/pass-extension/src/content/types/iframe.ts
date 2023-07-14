d640154e6475279f93e6b22ed46fd58dc356e0bc
Edvin CANDON <edvin.candon@proton.ch>
2023-07-14 08:48:50 +0200

    Improve iframe app creator
    - Store current app action in iframe state
    - Reflect changes in type definitions
    - Pass IframeState to iframe effect callbacks
    - Reflect changes in dropdown & notification services
    
    [IDTEAM-788]

--- applications/pass-extension/src/content/types/iframe.ts
+++ applications/pass-extension/src/content/types/iframe.ts
@@ -3,45 +3,48 @@ import type { Runtime } from 'webextension-polyfill';
 import type { ProxiedSettings } from '@proton/pass/store/reducers/settings';
 import type { MaybeNull, SafeLoginItem, WorkerState } from '@proton/pass/types';
 
-import type { DropdownSetActionPayload } from './dropdown';
-import type { NotificationSetActionPayload } from './notification';
+import type { DropdownActions } from './dropdown';
+import type { NotificationActions } from './notification';
 
 export type IFramePosition = { top: number; left?: number; right?: number; zIndex?: number };
 export type IFrameDimensions = { width: number; height: number };
 
-export type IFrameState = {
+export type IFrameState<A> = {
     visible: boolean;
     ready: boolean;
     loaded: boolean;
     port: MaybeNull<Runtime.Port>;
     framePort: MaybeNull<string>;
     position: IFramePosition;
+    action: MaybeNull<A>;
 };
 
 export type IFramePortMessageHandler<T extends IFrameMessageType = IFrameMessageType> = (
     message: IFrameMessageWithSender<T>
 ) => void;
 
-export interface IFrameApp {
+export type IFrameCloseOptions = { event?: Event; userInitiated: boolean; refocus?: boolean };
+
+export interface IFrameApp<A = any> {
     element: HTMLIFrameElement;
-    state: IFrameState;
+    state: IFrameState<A>;
     sendPortMessage: (message: IFrameMessage) => void;
     registerMessageHandler: <M extends IFrameMessage['type']>(type: M, handler: IFramePortMessageHandler<M>) => void;
     getPosition: () => IFramePosition;
     updatePosition: () => void;
-    open: (scrollRef?: HTMLElement) => void;
-    close: (options?: { event?: Event; userInitiated: boolean; refocus?: boolean }) => void;
+    open: (action: A, scrollRef?: HTMLElement) => void;
+    close: (options?: IFrameCloseOptions) => void;
     init: (port: Runtime.Port) => void;
     reset: (workerState: WorkerState, settings: ProxiedSettings) => void;
     destroy: () => void;
 }
 
-export interface IFrameAppService<OpenOptions = {}> {
-    getState: () => IFrameState;
-    open: (options: OpenOptions) => IFrameAppService<OpenOptions>;
-    close: () => IFrameAppService<OpenOptions>;
-    init: (port: Runtime.Port) => IFrameAppService<OpenOptions>;
-    reset: (workerState: WorkerState, settings: ProxiedSettings) => IFrameAppService<OpenOptions>;
+export interface IFrameAppService<T extends { action: any }> {
+    getState: () => IFrameState<T['action']>;
+    open: (options: T) => IFrameAppService<T>;
+    close: () => IFrameAppService<T>;
+    init: (port: Runtime.Port) => IFrameAppService<T>;
+    reset: (workerState: WorkerState, settings: ProxiedSettings) => IFrameAppService<T>;
     destroy: () => void;
 }
 
@@ -55,9 +58,8 @@ export enum IFrameMessageType {
     IFRAME_DIMENSIONS = 'IFRAME_DIMENSIONS',
     DROPDOWN_ACTION = 'DROPDOWN_ACTION',
     DROPDOWN_AUTOFILL_LOGIN = 'DROPDOWN_AUTOFILL_LOGIN',
-    DROPDOWN_AUTOSUGGEST_PASSWORD = 'DROPDOWN_AUTOSUGGEST_PASSWORD',
-    DROPDOWN_AUTOSUGGEST_ALIAS = 'DROPDOWN_AUTOSUGGEST_ALIAS',
-    DROPDOWN_AUTOFILL_USER_EMAIL = 'DROPDOWN_AUTOFILL_USER_EMAIL',
+    DROPDOWN_AUTOFILL_GENERATED_PW = 'DROPDOWN_AUTOFILL_GENERATED_PASSWORD',
+    DROPDOWN_AUTOFILL_EMAIL = 'DROPDOWN_AUTOFILL_EMAIL',
     NOTIFICATION_ACTION = 'NOTIFICATION_ACTION',
     NOTIFICATION_AUTOSAVE_REQUEST = 'NOTIFICATION_AUTOSAVE_REQUEST',
     NOTIFICATION_AUTOSAVE_SUCCESS = 'NOTIFICATION_AUTOSAVE_SUCCESS',
@@ -75,12 +77,11 @@ export type IFrameMessage<T extends IFrameMessageType = IFrameMessageType> = Ext
     | { type: IFrameMessageType.IFRAME_CLOSE; payload?: { refocus: boolean } }
     | { type: IFrameMessageType.IFRAME_HIDDEN }
     | { type: IFrameMessageType.IFRAME_DIMENSIONS; payload: { height: number; width?: number } }
-    | { type: IFrameMessageType.DROPDOWN_ACTION; payload: DropdownSetActionPayload }
+    | { type: IFrameMessageType.DROPDOWN_ACTION; payload: DropdownActions }
     | { type: IFrameMessageType.DROPDOWN_AUTOFILL_LOGIN; payload: { item: SafeLoginItem } }
-    | { type: IFrameMessageType.DROPDOWN_AUTOSUGGEST_PASSWORD; payload: { password: string } }
-    | { type: IFrameMessageType.DROPDOWN_AUTOSUGGEST_ALIAS; payload: { aliasEmail: string } }
-    | { type: IFrameMessageType.DROPDOWN_AUTOFILL_USER_EMAIL; payload: { userEmail: string } }
-    | { type: IFrameMessageType.NOTIFICATION_ACTION; payload: NotificationSetActionPayload }
+    | { type: IFrameMessageType.DROPDOWN_AUTOFILL_GENERATED_PW; payload: { password: string } }
+    | { type: IFrameMessageType.DROPDOWN_AUTOFILL_EMAIL; payload: { email: string } }
+    | { type: IFrameMessageType.NOTIFICATION_ACTION; payload: NotificationActions }
     | { type: IFrameMessageType.NOTIFICATION_AUTOFILL_OTP; payload: { code: string } },
     { type: T }
 >;
