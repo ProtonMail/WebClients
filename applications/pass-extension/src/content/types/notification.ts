213e1ef89e57f58807910dfaaabf7d260bf490d0
Edvin CANDON <edvin.candon@proton.ch>
2023-07-14 08:48:50 +0200

    Improve iframe app creator
    - Store current app action in iframe state
    - Reflect changes in type definitions
    - Pass IframeState to iframe effect callbacks
    - Reflect changes in dropdown & notification services
    
    [IDTEAM-788]

--- applications/pass-extension/src/content/types/notification.ts
+++ applications/pass-extension/src/content/types/notification.ts
@@ -7,15 +7,10 @@ export enum NotificationAction {
     AUTOFILL_OTP_PROMPT = 'AUTOFILL_OTP_PROMPT',
 }
 
-export type NotificationSetActionPayload =
+export type NotificationActions =
     | { action: NotificationAction.AUTOSAVE_PROMPT; submission: FormEntryPrompt }
     | { action: NotificationAction.AUTOFILL_OTP_PROMPT; item: SelectedItem };
 
 export type NotificationAutosaveRequestPayload = { item: Item<'login'>; submission: FormEntryPrompt };
 
-export type OpenNotificationOptions = {
-    action: NotificationAction;
-    submission: FormEntryPrompt;
-};
-
-export interface InjectedNotification extends IFrameAppService<OpenNotificationOptions> {}
+export interface InjectedNotification extends IFrameAppService<NotificationActions> {}
