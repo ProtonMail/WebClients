25fdddbf54ed8c2b7f01b48e7feaeb7487dae471
Edvin CANDON <edvin.candon@proton.ch>
2023-07-18 10:02:58 +0200

    Fix children overlap offset computation
    [IDTEAM-788]

---
+++ packages/pass/types/utils/dom.ts
@@ -0,0 +1,3 @@
+export type Rect = { left: number; right: number; top: number; bottom: number };
+export type RectOffset = { x: number; y: number };
+export type Dimensions = { width: number; height: number };
