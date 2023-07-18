2c6442c8a7f8283ac88947008144607d44c8387f
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2023-07-18 10:42:10 +0200

    Add calendar participant dropdown

--- applications/calendar/src/app/components/events/Participant.tsx
+++ applications/calendar/src/app/components/events/Participant.tsx
@@ -1,38 +1,74 @@
-import * as React from 'react';
-
-import { Tooltip } from '@proton/components';
+import { Tooltip, usePopperAnchor } from '@proton/components';
 import clsx from '@proton/utils/clsx';
+
+import ParticipantDropdown from './ParticipantDropdown';
 
 interface Props {
     icon: React.ReactNode;
-    text: string;
+    name: string;
     extraText?: string;
     title: string;
     initials: string;
     tooltip: string;
     className?: string;
+    email: string;
+    isContact: boolean;
+    onCreateOrEditContact: () => void;
 }
 
-const Participant = ({ icon, text, title, tooltip, initials, extraText, className }: Props) => {
+const Participant = ({
+    icon,
+    name,
+    title,
+    tooltip,
+    initials,
+    extraText,
+    className,
+    email,
+    isContact,
+    onCreateOrEditContact,
+}: Props) => {
+    const { anchorRef, isOpen, toggle, close } = usePopperAnchor<HTMLButtonElement>();
+    const displayDropdown = !!email;
+
     return (
-        <div className={clsx(['participant flex flex-nowrap flex-align-items-center', className])}>
+        <button
+            type="button"
+            ref={anchorRef}
+            onClick={() => {
+                toggle();
+            }}
+            className={clsx([
+                'participant flex flex-nowrap flex-align-items-center w100 relative interactive-pseudo interactive--no-background text-left',
+                className,
+            ])}
+        >
             <Tooltip title={tooltip}>
                 <div className="participant-display item-icon relative flex flex-item-noshrink flex-align-items-center flex-justify-center">
                     <div className="item-abbr">{initials}</div>
                     <span className="participant-status">{icon}</span>
                 </div>
             </Tooltip>
-            <Tooltip title={title}>
-                <div className="ml-4">
-                    <div className="max-w100 text-ellipsis participant-text text-ellipsis">{text}</div>
-                    {!!extraText && (
-                        <div className="max-w100 text-ellipsis participant-extra-text color-weak text-sm m-0">
-                            {extraText}
-                        </div>
-                    )}
-                </div>
-            </Tooltip>
-        </div>
+            <div className="ml-4 flex-item-fluid" title={title}>
+                <div className="max-w100 participant-text text-ellipsis">{name}</div>
+                {!!extraText && (
+                    <div className="max-w100 text-ellipsis participant-extra-text color-weak text-sm m-0">
+                        {extraText}
+                    </div>
+                )}
+            </div>
+            {displayDropdown && (
+                <ParticipantDropdown
+                    anchorRef={anchorRef}
+                    close={close}
+                    email={email}
+                    isOpen={isOpen}
+                    toggle={toggle}
+                    isContact={isContact}
+                    onCreateOrEditContact={onCreateOrEditContact}
+                />
+            )}
+        </button>
     );
 };
 
