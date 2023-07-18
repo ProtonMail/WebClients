43bd6dfe2e0505589d1be3716dbffb11ec99d440
Flavien Bonvin <flavien.bonvin@proton.ch>
2023-07-18 17:31:34 +0200

    IA-782 - Improve contact modal design IA-779 - Add first and last name field

--- packages/components/containers/contacts/edit/fields/ContactFieldFn.tsx
+++ packages/components/containers/contacts/edit/fields/ContactFieldFn.tsx
@@ -4,28 +4,41 @@ import { c } from 'ttag';
 
 import { Input, InputProps } from '@proton/atoms';
 import { isContactNameValid } from '@proton/shared/lib/contacts/property';
-import { getAllFieldLabels } from '@proton/shared/lib/helpers/contacts';
 import { requiredValidator } from '@proton/shared/lib/helpers/formValidators';
-import { VCardProperty } from '@proton/shared/lib/interfaces/contacts/VCard';
+import { VCardContact, VCardProperty } from '@proton/shared/lib/interfaces/contacts/VCard';
 
 import { ErrorZone } from '../../../../components';
 
 interface Props extends Omit<InputProps, 'onChange'> {
+    contactID?: string;
+    vCardContact: VCardContact;
     vCardProperty: VCardProperty<string>;
     isSubmitted: boolean;
     onChange: (vCardProperty: VCardProperty) => void;
 }
 
-const ContactFieldFn = ({ vCardProperty, isSubmitted, onChange, ...rest }: Props, ref: Ref<HTMLInputElement>) => {
-    const label = getAllFieldLabels().fn;
+const ContactFieldFn = (
+    { contactID, vCardContact, vCardProperty, isSubmitted, onChange, ...rest }: Props,
+    ref: Ref<HTMLInputElement>
+) => {
+    const label = c('Contact field label').t`Enter a display name or nickname`;
 
     const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
         const newValue = target.value;
         onChange({ ...vCardProperty, value: newValue });
     };
 
-    const requiredError = isSubmitted && requiredValidator(vCardProperty.value);
-    const nameTooLongError = !isContactNameValid(vCardProperty.value);
+    const isFirstAndLastNameArray = Array.isArray(vCardContact.n?.value);
+    const firstAndLastName = isFirstAndLastNameArray ? vCardContact.n?.value.join('').trim() : '';
+
+    const nameTooLongError =
+        !isContactNameValid(vCardProperty.value) ||
+        (!contactID && firstAndLastName && !isContactNameValid(firstAndLastName));
+    // The fn field is required for existing contacts but can be left empty for new contacts
+    const requiredError = contactID && isSubmitted && requiredValidator(vCardProperty.value);
+    // Display an error border when the fn field and the n field is empty during contact creation
+    const noNameInfoDuringCreationError =
+        !contactID && isSubmitted && !firstAndLastName && requiredValidator(vCardProperty.value);
 
     return (
         <>
@@ -35,11 +48,14 @@ const ContactFieldFn = ({ vCardProperty, isSubmitted, onChange, ...rest }: Props
                 placeholder={label}
                 onChange={handleChange}
                 data-testid={label}
-                error={!!requiredError || nameTooLongError}
+                error={!!requiredError || nameTooLongError || noNameInfoDuringCreationError}
                 {...rest}
             />
             {!!requiredError ? <ErrorZone>{requiredError}</ErrorZone> : null}
             {nameTooLongError ? <ErrorZone>{c('Error').t`Contact name is too long`}</ErrorZone> : null}
+            {noNameInfoDuringCreationError ? (
+                <ErrorZone>{c('Error').t`Please provide either a first name, a last name or a display name`}</ErrorZone>
+            ) : null}
         </>
     );
 };
--- packages/components/containers/contacts/import/steps/ContactImportCsvSelectField.tsx
+++ packages/components/containers/contacts/import/steps/ContactImportCsvSelectField.tsx
@@ -5,16 +5,17 @@ import { SelectChangeEvent } from '../../../../components/selectTwo/select';
 
 interface Props {
     value?: string;
-    onChangeField: (field: string) => void;
+    disabled?: boolean;
+    onChangeField?: (field: string) => void;
 }
-const ContactImportCsvSelectField = ({ value = '', onChangeField }: Props) => {
+const ContactImportCsvSelectField = ({ value = '', disabled, onChangeField }: Props) => {
     const fields = getAllFields();
 
-    const handleChangeField = ({ value }: SelectChangeEvent<string>) => onChangeField(value);
+    const handleChangeField = ({ value }: SelectChangeEvent<string>) => onChangeField?.(value);
 
     return (
         <Label className="pt-0">
-            <SelectTwo value={value} onChange={handleChangeField}>
+            <SelectTwo value={value} onChange={handleChangeField} disabled={disabled}>
                 {fields.map((field) => (
                     <Option key={field.value} title={field.text} value={field.value} />
                 ))}
