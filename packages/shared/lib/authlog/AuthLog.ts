b1a93a0728fc4fb235c2f4d8b0352b7396042e5b
yzhang <yanfeng.zhang@proton.ch>
2023-07-18 17:31:54 -0700

    Update sentinel protection icon

--- packages/shared/lib/authlog/AuthLog.ts
+++ packages/shared/lib/authlog/AuthLog.ts
@@ -58,6 +58,18 @@ export enum AuthLogStatus {
     Failure = 'failure',
 }
 
+export enum ProtectionType {
+    BLOCKED = 1,
+    CAPTCHA = 2,
+    OWNERSHIP_VERIFICATION = 3,
+    DEVICE_VERIFICATION = 4,
+    /**
+     * AuthLog action was protected by anti-abuse systems
+     * and was evaluated as safe.
+     */
+    OK = 5,
+}
+
 export interface AuthLog {
     AppVersion: string;
     Description: string;
