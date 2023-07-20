2b8ebd6b3176867c2e5d69d825df037a99f03a10
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-07-20 12:14:44 +0200

    Ignore HV errors in metrics error observation

--- packages/metrics/tests/observeApiError.test.ts
+++ packages/metrics/tests/observeApiError.test.ts
@@ -3,32 +3,52 @@ import observeApiError from '../lib/observeApiError';
 const metricObserver = jest.fn();
 
 describe('observeApiError', () => {
-    it('returns failure if error is undefined', () => {
+    it('ignores reporting if error is undefined', () => {
         observeApiError(undefined, metricObserver);
 
-        expect(metricObserver).toHaveBeenCalledTimes(1);
-        expect(metricObserver).toHaveBeenCalledWith('failure');
+        expect(metricObserver).toHaveBeenCalledTimes(0);
     });
 
-    it('returns failure if error is null', () => {
+    it('ignores reporting if error is null', () => {
         observeApiError(null, metricObserver);
 
-        expect(metricObserver).toHaveBeenCalledTimes(1);
-        expect(metricObserver).toHaveBeenCalledWith('failure');
+        expect(metricObserver).toHaveBeenCalledTimes(0);
     });
 
-    it('returns failure if error is not an object', () => {
+    it('ignores reporting if error is not an object', () => {
         observeApiError('string', metricObserver);
 
-        expect(metricObserver).toHaveBeenCalledTimes(1);
-        expect(metricObserver).toHaveBeenCalledWith('failure');
+        expect(metricObserver).toHaveBeenCalledTimes(0);
     });
 
-    it('returns failure if error does not contain a status property', () => {
+    it('ignores reporting if status is -1 (offline)', () => {
+        observeApiError({ status: -1 }, metricObserver);
+
+        expect(metricObserver).toHaveBeenCalledTimes(0);
+    });
+
+    it('ignores reporting if status is 0', () => {
+        observeApiError({ status: 0 }, metricObserver);
+
+        expect(metricObserver).toHaveBeenCalledTimes(0);
+    });
+
+    it('ignores reporting if error does not contain a status property', () => {
         observeApiError('', metricObserver);
 
-        expect(metricObserver).toHaveBeenCalledTimes(1);
-        expect(metricObserver).toHaveBeenCalledWith('failure');
+        expect(metricObserver).toHaveBeenCalledTimes(0);
+    });
+
+    it('ignores reporting if it is an abort error', () => {
+        observeApiError({ name: 'AbortError ' }, metricObserver);
+
+        expect(metricObserver).toHaveBeenCalledTimes(0);
+    });
+
+    it('ignores reporting if it is an HV error', () => {
+        observeApiError({ status: 422, data: { Error: 'HV required', Code: 9001 } }, metricObserver);
+
+        expect(metricObserver).toHaveBeenCalledTimes(0);
     });
 
     it('returns 5xx if error.status is 500', () => {
