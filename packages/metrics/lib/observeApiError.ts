daa4c834fefdc784f290509683e3364712ac1398
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-07-20 12:14:44 +0200

    Ignore HV errors in metrics error observation

--- packages/metrics/lib/observeApiError.ts
+++ packages/metrics/lib/observeApiError.ts
@@ -1,15 +1,34 @@
+import { getApiError, getIsConnectionIssue } from '@proton/shared/lib/api/helpers/apiErrorHelper';
+import { API_CUSTOM_ERROR_CODES } from '@proton/shared/lib/errors';
+
 export type MetricsApiStatusTypes = '4xx' | '5xx' | 'failure';
 
-export default function observeApiError(error: any, metricObserver: (status: MetricsApiStatusTypes) => void) {
+export default function observeApiError(
+    error: any,
+    metricObserver: (status: MetricsApiStatusTypes) => void,
+    ignoreCodes = [API_CUSTOM_ERROR_CODES.CARD_DECLINED, API_CUSTOM_ERROR_CODES.HUMAN_VERIFICATION_REQUIRED]
+) {
     if (!error) {
-        return metricObserver('failure');
+        return;
     }
 
-    if (error.status >= 500) {
+    const { status, code } = getApiError(error);
+
+    if (
+        !status ||
+        status === -1 ||
+        error?.name === 'AbortError' ||
+        getIsConnectionIssue(error) ||
+        ignoreCodes.includes(code)
+    ) {
+        return;
+    }
+
+    if (status >= 500) {
         return metricObserver('5xx');
     }
 
-    if (error.status >= 400) {
+    if (status >= 400) {
         return metricObserver('4xx');
     }
 
