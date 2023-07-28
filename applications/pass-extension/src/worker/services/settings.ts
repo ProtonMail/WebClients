f9162522c770717bbdc136bebeeb070cd3688c90
Edvin CANDON <edvin.candon@proton.ch>
2023-07-28 10:33:45 +0200

    Remove Pass controlling browser password manager settings
    - Reflect changes in settings app
    
    [IDTEAM-1761]

--- applications/pass-extension/src/worker/services/settings.ts
+++ applications/pass-extension/src/worker/services/settings.ts
@@ -1,8 +1,5 @@
-import type { Types } from 'webextension-polyfill';
-
 import { backgroundMessage } from '@proton/pass/extension/message';
 import { browserLocalStorage } from '@proton/pass/extension/storage';
-import browser from '@proton/pass/globals/browser';
 import { selectProxiedSettings } from '@proton/pass/store';
 import type { ProxiedSettings } from '@proton/pass/store/reducers/settings';
 import { WorkerMessageType } from '@proton/pass/types';
@@ -12,68 +9,20 @@ import { INITIAL_SETTINGS } from '../../shared/constants';
 import WorkerMessageBroker from '../channel';
 import store from '../store';
 
-type ExtensionSetting = {
-    platform: 'all' | 'chrome' | 'firefox';
-    defaultValue: boolean;
-    set: (value: boolean) => Promise<boolean>;
-    get: () => Promise<boolean>;
-};
-
-const createBrowserSetting = (id: string, setting: Types.Setting, defaultValue: boolean): ExtensionSetting => {
-    return {
-        platform: 'all',
-        defaultValue,
-        get: async () => (await setting.get({})).value,
-        set: async (value: boolean): Promise<boolean> => {
-            try {
-                const { levelOfControl } = await setting.get({});
-                if (
-                    levelOfControl === 'controlled_by_this_extension' ||
-                    levelOfControl === 'controllable_by_this_extension'
-                ) {
-                    await setting.set({ value });
-                    return true;
-                }
-
-                return false;
-            } catch (e) {
-                logger.warn(`[Worker::Settings] could not update ${id}`, e);
-                return false;
-            }
-        },
-    };
-};
-
 export type SettingsService = ReturnType<typeof createSettingsService>;
 
 export const createSettingsService = () => {
-    const browserSettings: Record<string, ExtensionSetting> = {
-        'autosave.browserDefault': createBrowserSetting(
-            'passwordSavingEnabled',
-            browser.privacy.services.passwordSavingEnabled,
-            true
-        ),
-    };
-
-    /* on extension install : try to set the extension's
-     * browser settings to their default values. Set the initial
-     * proxied locally stored settings with the results */
+    /* on extension install : Set the initial proxied
+     * locally stored settings with the results */
     const onInstall = async () => {
-        await Promise.all(Object.values(browserSettings).map(({ set, defaultValue }) => set(defaultValue)));
         const initialSettings = selectProxiedSettings(store.getState());
-        initialSettings.autosave.browserDefault = !(await browserSettings['autosave.browserDefault'].get());
-
         return browserLocalStorage.setItem('settings', JSON.stringify(initialSettings));
     };
-
-    const syncBrowserSettings = async (settings: ProxiedSettings) =>
-        Promise.all([browserSettings['autosave.browserDefault'].set(!settings.autosave.browserDefault)]);
 
     /* We have to proxy the redux store settings in local storage
      * in case the user is logged out (session invalidated, locked etc..)
      * but need to preserve the user settings in the content-script */
     const sync = async (settings: ProxiedSettings) => {
-        await syncBrowserSettings(settings);
         await browserLocalStorage.setItem('settings', JSON.stringify(settings));
         logger.info('[Worker::Settings] synced settings');
 
