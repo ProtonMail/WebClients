847959049eac74d25fe9ecc9c22ce8107bdf8a84
Edvin CANDON <edvin.candon@proton.ch>
2023-07-28 09:51:45 +0200

    Move popup state dispatch to `ItemEffects` component
    [IDTEAM-1762]

--- applications/pass-extension/src/popup/hooks/useSelectItemClick.ts
+++ applications/pass-extension/src/popup/hooks/useSelectItemClick.ts
@@ -1,8 +1,6 @@
 import { type MouseEvent, useCallback } from 'react';
-import { useDispatch } from 'react-redux';
 
 import { popupMessage, sendMessage } from '@proton/pass/extension/message';
-import { popupTabStateSave } from '@proton/pass/store/actions/creators/popup';
 import { createTelemetryEvent } from '@proton/pass/telemetry/events';
 import { type ItemRevision, WorkerMessageType } from '@proton/pass/types';
 import { TelemetryEventName } from '@proton/pass/types/data/telemetry';
@@ -11,11 +9,8 @@ import { isEmptyString } from '@proton/pass/utils/string';
 import type { NavigationOptions } from '../context/navigation/NavigationContext';
 import { useItemsFilteringContext } from './useItemsFilteringContext';
 import { useNavigationContext } from './useNavigationContext';
-import { usePopupContext } from './usePopupContext';
 
 export const useSelectItemClick = () => {
-    const popup = usePopupContext();
-    const dispatch = useDispatch();
     const { selectItem } = useNavigationContext();
     const { search } = useItemsFilteringContext();
 
@@ -23,15 +18,6 @@ export const useSelectItemClick = () => {
         (item: ItemRevision, options?: NavigationOptions) => (evt: MouseEvent<HTMLElement>) => {
             evt.preventDefault();
             selectItem(item.shareId, item.itemId, options);
-
-            dispatch(
-                popupTabStateSave({
-                    tabId: popup.context!.tabId,
-                    domain: popup.url.subdomain ?? popup.url.domain ?? null,
-                    selectedItem: { shareId: item.shareId, itemId: item.itemId },
-                    search,
-                })
-            );
 
             void sendMessage(
                 popupMessage({
