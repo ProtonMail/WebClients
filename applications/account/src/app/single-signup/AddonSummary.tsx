2261c936ad296dc15e2485650baa29fe277fff24
Jack Crompton <jack.crompton@proton.ch>
2023-07-31 11:33:11 +0200

    Add addon pricing calculation

---
+++ applications/account/src/app/single-signup/AddonSummary.tsx
@@ -0,0 +1,29 @@
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
+
+import { Price } from '@proton/components/components';
+import { Currency } from '@proton/shared/lib/interfaces';
+import clsx from '@proton/utils/clsx';
+
+interface Props extends ComponentPropsWithoutRef<'div'> {
+    label: string;
+    numberOfItems: number;
+    currency: Currency;
+    price: number;
+    subline?: ReactNode;
+}
+
+const AddonSummary = ({ label, numberOfItems, currency, price, subline, className, ...rest }: Props) => {
+    return (
+        <div className={clsx('flex', className)} {...rest}>
+            <span className="flex-item-fluid">{label}</span>
+            <div>
+                <div>
+                    {numberOfItems} x <Price currency={currency}>{price}</Price>
+                </div>
+                {subline !== undefined && <div className="color-weak text-right text-sm">{subline}</div>}
+            </div>
+        </div>
+    );
+};
+
+export default AddonSummary;
