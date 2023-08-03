2c4d7f88505d442c6260334c25b0219d729e4fa7
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-08-03 15:45:41 +0200

    Update asset fetching scheme

--- packages/shared/lib/supported/pre.ts
+++ packages/shared/lib/supported/pre.ts
@@ -1,10 +1,10 @@
 import { SupportedBrowserValue } from './interface';
 
-const preScriptNode = document.querySelector<HTMLScriptElement>('script[src^="/pre"]');
+const preScriptNode = document.querySelector<HTMLScriptElement>('script[src*="/pre"]');
 // The tag that we're interested in is the main script file. Normally that's index.js but there's also lite and eo take
 // into account and we fallback to the nextSibling in those scenarios since pre.js is supposed to be first.
 const scriptNode =
-    document.querySelector<HTMLScriptElement>('script[src^="/index"]') ||
+    document.querySelector<HTMLScriptElement>('script[src*="/index"]') ||
     (preScriptNode?.nextSibling as HTMLScriptElement);
 
 if (scriptNode) {
