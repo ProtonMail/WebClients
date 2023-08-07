6456f758d2f14a77f075790a8861e53288480abe
Alexey Karpov <alexey.karpov@proton.ch>
2023-08-07 16:09:12 +0200

    Added tests

--- packages/components/components/price/Price.test.tsx
+++ packages/components/components/price/Price.test.tsx
@@ -5,21 +5,26 @@ import Price from './Price';
 describe('Price component', () => {
     it('should render negative price with USD currency', () => {
         const { container } = render(<Price currency="USD">{-1500}</Price>);
-        expect(container.firstChild.textContent).toBe('-$15');
+        expect((container.firstChild as any).textContent).toBe('-$15');
     });
 
     it('should render price with EUR in suffix', () => {
         const { container } = render(<Price currency="EUR">{1500}</Price>);
-        expect(container.firstChild.textContent).toBe('15 €');
+        expect((container.firstChild as any).textContent).toBe('15 €');
     });
 
     it('should render price with CHF in prefix', () => {
         const { container } = render(<Price currency="CHF">{1500}</Price>);
-        expect(container.firstChild.textContent).toBe('CHF 15');
+        expect((container.firstChild as any).textContent).toBe('CHF 15');
     });
 
     it('should use the divisor defined', () => {
         const { container } = render(<Price divisor={1}>{1500}</Price>);
-        expect(container.firstChild.textContent).toBe('1500');
+        expect((container.firstChild as any).textContent).toBe('1500');
+    });
+
+    it('should render string values as is', () => {
+        const { container } = render(<Price>{'Let us talk'}</Price>);
+        expect((container.firstChild as any).textContent).toBe('Let us talk');
     });
 });
