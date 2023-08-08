1988a70cb62934417fb076148ddb75bcd87e2311
M. Thiercelin <marin.thiercelin@proton.ch>
2023-08-08 11:20:14 +0200

    Fix flags for new keys on address with disabled e2ee

--- packages/shared/lib/keys/keyFlags.ts
+++ packages/shared/lib/keys/keyFlags.ts
@@ -1,6 +1,6 @@
 import { Address } from '@proton/shared/lib/interfaces';
 
-import { ADDRESS_TYPE, KEY_FLAG } from '../constants';
+import { ADDRESS_FLAGS, ADDRESS_TYPE, KEY_FLAG } from '../constants';
 import { clearBit, hasBit, setBit } from '../helpers/bitset';
 
 export const setExternalFlags = (flags: number) => {
@@ -19,6 +19,12 @@ export const getDefaultKeyFlags = (address: Address | undefined) => {
     let flags = KEY_FLAG.FLAG_NOT_OBSOLETE + KEY_FLAG.FLAG_NOT_COMPROMISED;
     if (address?.Type === ADDRESS_TYPE.TYPE_EXTERNAL) {
         flags = setExternalFlags(flags);
+    }
+    if (hasBit(address?.Flags, ADDRESS_FLAGS.FLAG_DISABLE_E2EE)) {
+        flags = setBit(flags, KEY_FLAG.FLAG_EMAIL_NO_ENCRYPT);
+    }
+    if (hasBit(address?.Flags, ADDRESS_FLAGS.FLAG_DISABLE_EXPECTED_SIGNED)) {
+        flags = setBit(flags, KEY_FLAG.FLAG_EMAIL_NO_SIGN);
     }
     return flags;
 };
