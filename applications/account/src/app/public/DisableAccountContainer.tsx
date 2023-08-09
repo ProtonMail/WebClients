cb6532a1be194fe2d2cfd5787bbb3458bc917206
Jack Crompton <jack.crompton@proton.ch>
2023-08-09 22:35:52 +0200

    Add confirmation step to disable account flow
    CP-6400

--- applications/account/src/app/public/DisableAccountContainer.tsx
+++ applications/account/src/app/public/DisableAccountContainer.tsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 
 import { c } from 'ttag';
 
-import { CircleLoader, Href } from '@proton/atoms';
+import { Button, CircleLoader, Href } from '@proton/atoms';
 import { GenericError, useApi, useErrorHandler } from '@proton/components';
 import { useLoading } from '@proton/hooks';
 import { getApiError } from '@proton/shared/lib/api/helpers/apiErrorHelper';
@@ -17,12 +17,30 @@ import PublicLayout from '../components/PublicLayout';
 import ExpiredError from './ExpiredError';
 import accountIllustration from './account-illustration.svg';
 
+const Footer = () => {
+    return (
+        <PublicFooter center={false}>
+            <div className="color-weak">
+                {c('Info')
+                    .t`${BRAND_NAME} is privacy you can trust, ensured by strong encryption, open-source code, and Swiss privacy laws. We believe nobody should be able to exploit your data, period. Our technology and business are based upon this fundamentally stronger definition of privacy.`}
+            </div>
+            <br />
+            <div className="mb-6 color-weak">
+                {c('Info').t`Over 100 million people and businesses have signed up for ${BRAND_NAME}.`}{' '}
+                <Href className="color-weak" href={getStaticURL('')}>
+                    {c('Link').t`Learn more`}
+                </Href>
+            </div>
+        </PublicFooter>
+    );
+};
+
 enum ErrorType {
     Expired,
     API,
 }
 
-const DisableAccountContainer = () => {
+const DisableAccount = () => {
     const api = useApi();
     const [error, setError] = useState<{ type: ErrorType } | null>(null);
     const [loading, withLoading] = useLoading(true);
@@ -49,7 +67,7 @@ const DisableAccountContainer = () => {
             {(() => {
                 const signIn = (
                     <a key="1" href={SSO_PATHS.SWITCH} target="_self">
-                        {c('Recovery Email').t`sign in`}
+                        {c('Disable account').t`sign in`}
                     </a>
                 );
                 if (error) {
@@ -65,9 +83,9 @@ const DisableAccountContainer = () => {
                         <div className="absolute-center text-center">
                             <GenericError>
                                 <span>
-                                    {c('Error message, recovery').t`There was a problem disabling your account.`}
+                                    {c('Error message, Disable account').t`There was a problem disabling your account.`}
                                 </span>
-                                <span>{c('Recovery Email').jt`Back to ${signIn}.`}</span>
+                                <span>{c('Disable account').jt`Back to ${signIn}.`}</span>
                             </GenericError>
                         </div>
                     );
@@ -83,36 +101,62 @@ const DisableAccountContainer = () => {
                     <PublicLayout
                         className="h100"
                         img={<img src={accountIllustration} alt="" />}
-                        header={c('Email').t`Thanks for letting us know`}
+                        header={c('Disable account').t`Thanks for letting us know`}
                         main={
                             <div className="text-center">
                                 <div className="mb-2">
-                                    {c('Email')
+                                    {c('Disable account')
                                         .t`We've noted that this account doesn't belong to you. We'll investigate and act accordingly.`}
                                 </div>
                             </div>
                         }
-                        below={
-                            <PublicFooter center={false}>
-                                <div className="color-weak">
-                                    {c('Info')
-                                        .t`${BRAND_NAME} is privacy you can trust, ensured by strong encryption, open-source code, and Swiss privacy laws. We believe nobody should be able to exploit your data, period. Our technology and business are based upon this fundamentally stronger definition of privacy.`}
-                                </div>
-                                <br />
-                                <div className="mb-6 color-weak">
-                                    {c('Info')
-                                        .t`Over 100 million people and businesses have signed up for ${BRAND_NAME}.`}{' '}
-                                    <Href className="color-weak" href={getStaticURL('')}>
-                                        {c('Link').t`Learn more`}
-                                    </Href>
-                                </div>
-                            </PublicFooter>
-                        }
+                        below={<Footer />}
                     />
                 );
             })()}
         </main>
     );
+};
+
+enum Step {
+    CONFIRM,
+    DISABLE_ACCOUNT,
+}
+
+const DisableAccountContainer = () => {
+    const [step, setStep] = useState(Step.CONFIRM);
+
+    if (step === Step.CONFIRM) {
+        return (
+            <main className="main-area h100">
+                <PublicLayout
+                    className="h100"
+                    img={<img src={accountIllustration} alt="" />}
+                    header={c('Disable account').t`Didn't create an account?`}
+                    main={
+                        <div className="text-center">
+                            <div className="mb-2">
+                                {c('Disable account')
+                                    .t`This is usually nothing to worry about. Just click below to confirm that this ${BRAND_NAME} Account isn't yours, and we'll disable it.`}
+                            </div>
+                            <div className="mb-12">{c('Disable account').t`You can safely close this tab.`}</div>
+                            <Button
+                                fullWidth
+                                color="danger"
+                                shape="outline"
+                                onClick={() => setStep(Step.DISABLE_ACCOUNT)}
+                            >
+                                {c('Disable account').t`I didn't create this account`}
+                            </Button>
+                        </div>
+                    }
+                    below={<Footer />}
+                />
+            </main>
+        );
+    }
+
+    return <DisableAccount />;
 };
 
 export default DisableAccountContainer;
