28e05d969f56778db97220c04d1d6df476d41474
Pierre Bondoerffer <pierre.bondoerffer@proton.ch>
2023-08-10 14:57:39 +0200

    v6 design update

--- applications/drive/src/app/components/sections/Photos/PhotosGrid.tsx
+++ applications/drive/src/app/components/sections/Photos/PhotosGrid.tsx
@@ -1,4 +1,7 @@
-import React, { FC, useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { FC, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
+
+import { useElementRect } from '@proton/components';
+import { rootFontSize } from '@proton/shared/lib/helpers/dom';
 
 import type { Photo } from '../../../store/_photos/interfaces';
 import { usePhotosView } from '../../../store/_views/usePhotosView';
@@ -11,16 +14,9 @@ type Props = {
     getPhotoLink: ReturnType<typeof usePhotosView>['getPhotoLink'];
 };
 
-type ItemDimensions = {
-    width: number;
-    height: number;
-    gap: number;
-    groupHeight: number;
-};
-
 export const PhotosGrid: FC<Props> = ({ data, getPhotoLink }) => {
     const containerRef = useRef<HTMLDivElement>(null);
-    const [itemDimensions, setItemDimensions] = useState<ItemDimensions>();
+    const containerRect = useElementRect(containerRef);
     const [itemsPerLine, setItemsPerLine] = useState(0);
     const [itemScaling, setItemScaling] = useState(0);
     const [containerHeight, setContainerHeight] = useState(0);
@@ -28,57 +24,35 @@ export const PhotosGrid: FC<Props> = ({ data, getPhotoLink }) => {
     const [posX, setPosX] = useState<number[]>([]);
     const [posY, setPosY] = useState<number[]>([]);
 
-    useLayoutEffect(() => {
-        if (!containerRef.current) {
-            return;
-        }
+    const itemDimensions = useMemo(() => {
+        const emRatio = rootFontSize();
 
-        // Get the em-to-pixel ratio to calculate item dimensions
-        let div = document.createElement('div');
-
-        div.classList.add('w-4');
-        document.body.appendChild(div);
-
-        let emRatio = div.getBoundingClientRect().width;
-        const dimensions: ItemDimensions = {
+        return {
             width: 10 * emRatio,
             height: 13.75 * emRatio,
             gap: 0.25 * emRatio,
             groupHeight: 2 * emRatio,
         };
-        setItemDimensions(dimensions);
-
-        document.body.removeChild(div);
-
-        // Calculate how many elements we can have per line
-        const calc = (rect: DOMRect | DOMRectReadOnly) => {
-            const containerWidth = rect.width;
-
-            let maxItems = (containerWidth + dimensions.gap) / (dimensions.width + dimensions.gap);
-            let itemsPerLine = Math.max(1, Math.floor(maxItems));
-            let scaling = (containerWidth - (itemsPerLine - 1) * dimensions.gap) / (itemsPerLine * dimensions.width);
-
-            setItemsPerLine(itemsPerLine);
-            setItemScaling(scaling);
-        };
-
-        calc(containerRef.current.getBoundingClientRect());
-
-        // Set up an observer for when we resize the element
-        const resizeObserver = new ResizeObserver(([elem]) => {
-            calc(elem.contentRect);
-        });
-
-        resizeObserver.observe(containerRef.current);
-
-        // Clean up observer
-        return () => {
-            resizeObserver.disconnect();
-        };
     }, []);
 
-    const itemHeight = (itemDimensions?.height || 0) * itemScaling;
-    const itemWidth = (itemDimensions?.width || 0) * itemScaling;
+    useLayoutEffect(() => {
+        if (!containerRect) {
+            return;
+        }
+
+        const { width, gap } = itemDimensions;
+        const containerWidth = containerRect.width;
+
+        const maxItems = (containerWidth + gap) / (width + gap);
+        const itemsPerLine = Math.max(1, Math.floor(maxItems));
+        const scaling = (containerWidth - (itemsPerLine - 1) * gap) / (itemsPerLine * width);
+
+        setItemsPerLine(itemsPerLine);
+        setItemScaling(scaling);
+    }, [containerRect]);
+
+    const itemHeight = itemDimensions.height * itemScaling;
+    const itemWidth = itemDimensions.width * itemScaling;
     const safetyMargin = itemHeight * 3;
 
     useEffect(() => {
@@ -86,8 +60,8 @@ export const PhotosGrid: FC<Props> = ({ data, getPhotoLink }) => {
             return;
         }
 
-        let posX = [];
-        let posY = [];
+        const posX = [];
+        const posY = [];
 
         let currentX = 0;
         let currentY = 0;
@@ -118,10 +92,10 @@ export const PhotosGrid: FC<Props> = ({ data, getPhotoLink }) => {
         setPosY(posY);
     }, [data, itemDimensions, itemScaling, itemsPerLine]);
 
-    let gridItems = [];
+    const gridItems = [];
 
-    let startIndex = posY.findIndex((value) => value >= scrollPosition - itemHeight - safetyMargin);
-    let endIndex = posY.findLastIndex((value) => value <= scrollPosition + containerHeight + safetyMargin);
+    const startIndex = posY.findIndex((value) => value >= scrollPosition - itemHeight - safetyMargin);
+    const endIndex = posY.findLastIndex((value) => value <= scrollPosition + containerHeight + safetyMargin);
 
     if (startIndex >= 0 && endIndex >= 0) {
         for (let i = startIndex; i <= endIndex; i++) {
@@ -134,7 +108,7 @@ export const PhotosGrid: FC<Props> = ({ data, getPhotoLink }) => {
                     <div
                         style={{
                             position: 'absolute',
-                            height: `${itemDimensions?.groupHeight || 0}px`,
+                            height: `${itemDimensions.groupHeight}px`,
                             width: '100%',
                             top: `${y}px`,
                             backgroundColor: 'blue',
