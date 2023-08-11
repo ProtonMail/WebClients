12905c409232e230460c242f2e8c02de568c86b0
Edvin CANDON <edvin.candon@proton.ch>
2023-08-11 11:51:05 +0200

    Fix z-index resolution edge-case
    [IDTEAM-1804]

--- packages/pass/utils/dom/zindex.ts
+++ packages/pass/utils/dom/zindex.ts
@@ -1,3 +1,7 @@
+/* Recursively determines the maximum z-index value required for overlaying
+ * an element on the DOM. During traversal, z-index evaluation occurs only if :
+ * - The current element's position is not 'static' (ie: relatively, absolutely, or fixedly positioned)
+ * - Or if the maximum z-index value is still 0 (indicating no z-index value has been resolved yet) */
 const zTraverse = (el: HTMLElement, max: number = 0): number => {
     const styles = getComputedStyle(el);
     const { position, zIndex } = styles;
@@ -5,7 +9,7 @@ const zTraverse = (el: HTMLElement, max: number = 0): number => {
     const parent = el.parentElement;
     if (!parent) return max;
 
-    if (position !== 'static') {
+    if (position !== 'static' || max === 0) {
         const value = parseInt(zIndex, 10);
         if (!isNaN(value)) return zTraverse(parent, Math.max(value, max));
     }
