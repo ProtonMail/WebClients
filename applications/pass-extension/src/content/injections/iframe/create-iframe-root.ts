6b0037b58818a061b77cb29f563b88e8b07fd32b
Edvin CANDON <edvin.candon@proton.ch>
2023-08-14 14:42:21 +0200

    Fix flickering glitch due to `StyledShadowHost` styles loading

--- applications/pass-extension/src/content/injections/iframe/create-iframe-root.ts
+++ applications/pass-extension/src/content/injections/iframe/create-iframe-root.ts
@@ -9,14 +9,13 @@ export const createIframeRoot = (): ProtonPassRoot => {
     const root = getIFrameRoot();
     if (root !== null) return root;
 
-    const iframeRoot = createElement<ProtonPassRoot>({ type: 'protonpass-root', parent: document.body });
+    const iframeRoot = createElement<ProtonPassRoot>({
+        type: 'protonpass-root',
+        parent: document.body,
+    });
 
-    /* we may hit a case where the content-script is reloaded - on
-     * firefox, this will cause our injected stylesheets to be removed
-     * from the DOM and induce a small flickering glitch. To be safe
-     * hide the iframe root by default, this value is override in the
-     * `iframe.scss` */
     iframeRoot.style.display = 'none';
+    iframeRoot.addEventListener('ready', () => iframeRoot.style.removeProperty('display'));
 
     return iframeRoot;
 };
