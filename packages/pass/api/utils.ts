0d411074d9502382607e38a59de4f5bbaa47189a
Edvin CANDON <edvin.candon@proton.ch>
2023-08-16 19:02:17 +0200

    Fix `LockedSessionError` trigger in api handlers

--- packages/pass/api/utils.ts
+++ packages/pass/api/utils.ts
@@ -1,2 +1,4 @@
 export const getSilenced = ({ silence }: any = {}, code: string | number): boolean =>
     Array.isArray(silence) ? silence.includes(code) : !!silence;
+
+export const isPassSessionRoute = (url?: string): boolean => Boolean(url?.startsWith('pass/v1/user/session/lock'));
