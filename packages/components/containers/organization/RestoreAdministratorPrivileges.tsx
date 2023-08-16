4f2d7e84f31ee0cbf31a3fbc3f794a9c59692e0a
Jack Crompton <jack.crompton@proton.ch>
2023-08-16 11:36:20 +0200

    Enable org key reactivation on /users-addresses page
    Opens ReactivateOrganizationKeysModal instead of redirecting to
    multi user support. This is a step towards dropping the multi user
    support page for vpn-settings.

--- packages/components/containers/organization/RestoreAdministratorPrivileges.tsx
+++ packages/components/containers/organization/RestoreAdministratorPrivileges.tsx
@@ -1,25 +1,56 @@
 import { c } from 'ttag';
 
-import { ButtonLike } from '@proton/atoms';
+import { Button, Href } from '@proton/atoms';
 import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
-import { getOrganizationKeyInfo } from '@proton/shared/lib/organization/helper';
+import { CachedOrganizationKey, Member } from '@proton/shared/lib/interfaces';
+import {
+    getHasOtherAdmins,
+    getNonPrivateMembers,
+    getOrganizationKeyInfo,
+} from '@proton/shared/lib/organization/helper';
 
-import { Alert, Block, LearnMore, SettingsLink } from '../../components';
-import { useOrganization, useOrganizationKey } from '../../hooks';
+import { Alert, Block, useModalState } from '../../components';
+import { useMembers, useOrganization, useOrganizationKey } from '../../hooks';
+import ChangeOrganizationKeysModal from './ChangeOrganizationKeysModal';
+import ReactivateOrganizationKeysModal from './ReactivateOrganizationKeysModal';
 import { getActivationText } from './helper';
 
-const RestoreAdministratorPrivileges = () => {
-    const [organization, loadingOrganization] = useOrganization();
-    const [organizationKey, loadingOrganizationKey] = useOrganizationKey(organization);
+const UserNeedsToReactivate = ({
+    members,
+    organizationKey,
+}: {
+    members: Member[];
+    organizationKey: CachedOrganizationKey;
+}) => {
+    const [
+        reactivateOrganizationKeysModalProps,
+        setReactivateOrganizationKeysModalOpen,
+        renderReactivateOrganizationKeysModal,
+    ] = useModalState();
+    const [changeOrganizationKeysModalProps, setChangeOrganizationKeysModalOpen, renderChangeOrganizationKeysModal] =
+        useModalState();
 
-    const organizationKeyInfo = getOrganizationKeyInfo(organization, organizationKey);
+    const hasOtherAdmins = members ? getHasOtherAdmins(members) : false;
+    const publicMembers = members ? getNonPrivateMembers(members) : [];
 
-    if (loadingOrganization || loadingOrganizationKey || !organization?.HasKeys) {
-        return null;
-    }
-
-    if (organizationKeyInfo.userNeedsToReactivateKey) {
-        return (
+    return (
+        <>
+            {renderReactivateOrganizationKeysModal && (
+                <ReactivateOrganizationKeysModal
+                    mode="reactivate"
+                    onResetKeys={() => setChangeOrganizationKeysModalOpen(true)}
+                    {...reactivateOrganizationKeysModalProps}
+                />
+            )}
+            {renderChangeOrganizationKeysModal && (
+                <ChangeOrganizationKeysModal
+                    mode="reset"
+                    hasOtherAdmins={hasOtherAdmins}
+                    organizationKey={organizationKey}
+                    publicMembers={publicMembers}
+                    {...changeOrganizationKeysModalProps}
+                />
+            )}
             <Block>
                 <Alert className="mb-4" type="error">
                     <div>
@@ -33,25 +64,89 @@ const RestoreAdministratorPrivileges = () => {
                         <li>{c('Restore administrator panel').t`Changing organization keys`}</li>
                     </ul>
                 </Alert>
-                <ButtonLike as={SettingsLink} path="/organization-keys" color="norm" className="mr-4">
+                <Button color="norm" className="mr-4" onClick={() => setReactivateOrganizationKeysModalOpen(true)}>
                     {c('Action').t`Restore administrator privileges`}
-                </ButtonLike>
-                <LearnMore className="inline-block" url={getKnowledgeBaseUrl('/proton-for-business')} />
+                </Button>
+                <Href href={getKnowledgeBaseUrl('/proton-for-business')} className="inline-block">
+                    {c('Link').t`Learn more`}
+                </Href>
             </Block>
-        );
-    }
+        </>
+    );
+};
 
-    if (organizationKeyInfo.userNeedsToActivateKey) {
-        return (
+const UserNeedsToActivate = ({
+    members,
+    organizationKey,
+}: {
+    members: Member[];
+    organizationKey: CachedOrganizationKey;
+}) => {
+    const [
+        activateOrganizationKeysModalProps,
+        setActivateOrganizationKeysModalOpen,
+        renderActivateOrganizationKeysModal,
+    ] = useModalState();
+    const [changeOrganizationKeysModalProps, setChangeOrganizationKeysModalOpen, renderChangeOrganizationKeysModal] =
+        useModalState();
+
+    const hasOtherAdmins = members ? getHasOtherAdmins(members) : false;
+    const publicMembers = members ? getNonPrivateMembers(members) : [];
+
+    return (
+        <>
+            {renderActivateOrganizationKeysModal && (
+                <ReactivateOrganizationKeysModal
+                    mode="activate"
+                    onResetKeys={() => setChangeOrganizationKeysModalOpen(true)}
+                    {...activateOrganizationKeysModalProps}
+                />
+            )}
+            {renderChangeOrganizationKeysModal && (
+                <ChangeOrganizationKeysModal
+                    mode="reset"
+                    hasOtherAdmins={hasOtherAdmins}
+                    organizationKey={organizationKey}
+                    publicMembers={publicMembers}
+                    {...changeOrganizationKeysModalProps}
+                />
+            )}
             <Block>
                 <Alert className="mb-4" type="error">
                     {getActivationText()}
                 </Alert>
-                <ButtonLike as={SettingsLink} path="/organization-keys" color="norm">
+                <Button
+                    color="norm"
+                    onClick={() => {
+                        console.log('hey');
+                        setActivateOrganizationKeysModalOpen(true);
+                    }}
+                >
                     {c('Action').t`Activate organization key`}
-                </ButtonLike>
+                </Button>
             </Block>
-        );
+        </>
+    );
+};
+
+const RestoreAdministratorPrivileges = () => {
+    const [members, loadingMembers] = useMembers();
+
+    const [organization, loadingOrganization] = useOrganization();
+    const [organizationKey, loadingOrganizationKey] = useOrganizationKey(organization);
+
+    const organizationKeyInfo = getOrganizationKeyInfo(organization, organizationKey);
+
+    if (!organizationKey || loadingOrganization || loadingOrganizationKey || !organization?.HasKeys || loadingMembers) {
+        return null;
+    }
+
+    if (organizationKeyInfo.userNeedsToReactivateKey) {
+        return <UserNeedsToReactivate members={members} organizationKey={organizationKey} />;
+    }
+
+    if (organizationKeyInfo.userNeedsToActivateKey) {
+        return <UserNeedsToActivate members={members} organizationKey={organizationKey} />;
     }
 
     return null;
