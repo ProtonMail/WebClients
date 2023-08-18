9f1ed83076da7a5849c4daed9f336375619ec9cc
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2023-08-18 09:42:39 +0100

    De-optimising the generic login page
    CP-6424

--- applications/account/src/pages/login.ts
+++ applications/account/src/pages/login.ts
@@ -2,11 +2,10 @@ import { c } from 'ttag';
 
 import { BRAND_NAME, CALENDAR_SHORT_APP_NAME, DRIVE_SHORT_APP_NAME, MAIL_APP_NAME } from '@proton/shared/lib/constants';
 
-import { Parameters } from './interface';
+import { Parameters, getLoginTitle } from './interface';
 
 const data = (): Parameters => ({
-    title: c('Metadata title')
-        .t`Sign in | ${BRAND_NAME} Account - Access ${MAIL_APP_NAME}, ${DRIVE_SHORT_APP_NAME}, and more`,
+    title: getLoginTitle(c('Metadata title').t`${BRAND_NAME} Account`),
     description: c('Metadata title')
         .t`Sign in to your ${BRAND_NAME} Account to access all encrypted ${BRAND_NAME} services such as ${MAIL_APP_NAME}, ${DRIVE_SHORT_APP_NAME}, ${CALENDAR_SHORT_APP_NAME}, and more. Don't have an account? Create one for FREE.`,
 });
