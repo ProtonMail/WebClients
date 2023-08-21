c5b48fabd93a7b773e9fb36f7580c6a094fe5581
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2023-08-21 19:21:18 +0200

    Add workaround to get flags ready

--- packages/components/containers/unleash/index.ts
+++ packages/components/containers/unleash/index.ts
@@ -1,3 +1,4 @@
-export { useFlag, useFlags, useFlagsStatus } from '@unleash/proxy-client-react';
 export * from './UnleashContext';
 export { default as UnleashFlagProvider } from './UnleashFlagProvider';
+export { default as useFlagsReady } from './useFlagsReady';
+export { useFlag, useFlags } from '@unleash/proxy-client-react';
