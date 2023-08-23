a59ce83df0b4762a4b43fa2b267f3a228752bf0a
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2023-08-23 17:53:43 +0200

    Revert "Merge branch 'IA-789-fix-importer-name-error' into 'main'"
    This reverts commit 4334176319931dc1b0657609549fd6037e0c1da6, reversing
    changes made to a364b577a08137bf69f148ac74d7957f0fe98e5b.

--- packages/activation/src/helpers/errorsMapping.test.ts
+++ packages/activation/src/helpers/errorsMapping.test.ts
@@ -1,3 +1,6 @@
+import { Folder } from '@proton/shared/lib/interfaces/Folder';
+import { Label } from '@proton/shared/lib/interfaces/Label';
+
 import { MailImportFolder } from './MailImportFoldersParser/MailImportFoldersParser';
 import {
     isNameAlreadyUsed,
@@ -6,6 +9,7 @@ import {
     isNameTooLong,
     mappingHasNameTooLong,
     mappingHasReservedNames,
+    mappingHasUnavailableNames,
 } from './errorsMapping';
 
 const smallString = '6NLaLHynY3YPM8gGLncefo5PP7n2Db';
@@ -72,7 +76,7 @@ describe('Activation errors mapping', () => {
         });
         it('Should return true if the name present in an array contains a space before and after and is capitalized', () => {
             const paths = ['path1', 'path2', ' path3 '];
-            const res = isNameAlreadyUsed('path3', paths);
+            const res = isNameAlreadyUsed('Path3', paths);
             expect(res).toBe(true);
         });
         it('Should return true if the name present in an array contains two space before', () => {
@@ -137,6 +141,165 @@ describe('Activation errors mapping', () => {
             ];
 
             const res = mappingHasNameTooLong(mapping);
+            expect(res).toBe(true);
+        });
+    });
+
+    describe('mappingHasUnavailableNames', () => {
+        it('Should return false if the mapping has no duplicate folder names', () => {
+            const mapping = [
+                {
+                    protonPath: ['parent', 'child'],
+                    separator: '/',
+                } as MailImportFolder,
+                {
+                    protonPath: ['parent', 'child2'],
+                    separator: '/',
+                } as MailImportFolder,
+            ];
+
+            const collection: Folder[] = [
+                {
+                    ID: 'folder1',
+                    Name: 'folder1',
+                    Color: 'folder1',
+                    Path: 'folder1',
+                    Expanded: 1,
+                    Type: 1,
+                    Order: 1,
+                    ParentID: 'folder1',
+                    Notify: 1,
+                },
+                {
+                    ID: 'folder2',
+                    Name: 'folder2',
+                    Color: 'folder2',
+                    Path: 'folder2',
+                    Expanded: 2,
+                    Type: 2,
+                    Order: 2,
+                    ParentID: 'folder2',
+                    Notify: 2,
+                },
+            ];
+
+            const res = mappingHasUnavailableNames(mapping, collection, false);
+            expect(res).toBe(false);
+        });
+        it('Should return false if the mapping has no duplicate label names', () => {
+            const mapping = [
+                {
+                    protonPath: ['parent', 'child'],
+                    separator: '/',
+                } as MailImportFolder,
+                {
+                    protonPath: ['parent', 'child2'],
+                    separator: '/',
+                } as MailImportFolder,
+            ];
+
+            const collection: Label[] = [
+                {
+                    ID: 'label1',
+                    Name: 'label1',
+                    Color: 'label1',
+                    ContextTime: 1,
+                    Type: 1,
+                    Order: 1,
+                    Path: 'label1',
+                    Display: 1,
+                },
+                {
+                    ID: 'label2',
+                    Name: 'label2',
+                    Color: 'label2',
+                    ContextTime: 2,
+                    Type: 2,
+                    Order: 2,
+                    Path: 'label2',
+                    Display: 2,
+                },
+            ];
+
+            const res = mappingHasUnavailableNames(mapping, collection, true);
+            expect(res).toBe(false);
+        });
+        it('Should return true if the mapping has duplicate folder names', () => {
+            const mapping = [
+                {
+                    protonPath: ['folder1'],
+                    separator: '/',
+                } as MailImportFolder,
+                {
+                    protonPath: ['folder2'],
+                    separator: '/',
+                } as MailImportFolder,
+            ];
+
+            const collection: Folder[] = [
+                {
+                    ID: 'folder1',
+                    Name: 'folder1',
+                    Color: 'folder1',
+                    Path: 'folder1',
+                    Expanded: 1,
+                    Type: 1,
+                    Order: 1,
+                    ParentID: 'folder1',
+                    Notify: 1,
+                },
+                {
+                    ID: 'folder2',
+                    Name: 'folder2',
+                    Color: 'folder2',
+                    Path: 'folder2',
+                    Expanded: 2,
+                    Type: 2,
+                    Order: 2,
+                    ParentID: 'folder2',
+                    Notify: 2,
+                },
+            ];
+
+            const res = mappingHasUnavailableNames(mapping, collection, false);
+            expect(res).toBe(true);
+        });
+        it('Should return false if the mapping has duplicate label names', () => {
+            const mapping = [
+                {
+                    protonPath: ['label1'],
+                    separator: '/',
+                } as MailImportFolder,
+                {
+                    protonPath: ['label2'],
+                    separator: '/',
+                } as MailImportFolder,
+            ];
+
+            const collection: Label[] = [
+                {
+                    ID: 'label1',
+                    Name: 'label1',
+                    Color: 'label1',
+                    ContextTime: 1,
+                    Type: 1,
+                    Order: 1,
+                    Path: 'label1',
+                    Display: 1,
+                },
+                {
+                    ID: 'label2',
+                    Name: 'label2',
+                    Color: 'label2',
+                    ContextTime: 2,
+                    Type: 2,
+                    Order: 2,
+                    Path: 'label2',
+                    Display: 2,
+                },
+            ];
+
+            const res = mappingHasUnavailableNames(mapping, collection, true);
             expect(res).toBe(true);
         });
     });
