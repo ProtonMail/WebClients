5a95d53d763ed584f0c9c7df03cd794d7e286dd0
romain sanchez <romain.sanchez@proton.ch>
2023-08-23 16:26:12 +0200

    Do not show the extra modal when unsubscribing an official proton message MAILWEB-4441

--- packages/shared/lib/mail/messages.ts
+++ packages/shared/lib/mail/messages.ts
@@ -117,7 +117,8 @@ export const isBounced = (message: Pick<Message, 'Sender' | 'Subject'>) => {
 
 export const getSender = (message?: Pick<Message, 'Sender'>) => message?.Sender;
 
-export const hasSimpleLoginSender = (message: Pick<Message, 'Sender'>) => !!message.Sender?.IsSimpleLogin;
+export const hasSimpleLoginSender = (message?: Pick<Message, 'Sender'>) => !!message?.Sender?.IsSimpleLogin;
+export const hasProtonSender = (message?: Pick<Message, 'Sender'>) => !!message?.Sender?.IsProton;
 
 export const getRecipients = (message?: Partial<Message>) => {
     const { ToList = [], CCList = [], BCCList = [] } = message || {};
