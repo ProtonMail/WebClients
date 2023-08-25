0b69a18adacdd3125aa339f293722a6b52ecace1
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2023-08-25 10:52:21 +0100

    Move search to V6 header

--- applications/calendar/src/app/containers/calendar/search/CalendarSearch.tsx
+++ applications/calendar/src/app/containers/calendar/search/CalendarSearch.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
 
-import { TopNavbarListItemSearchButton, generateUID, usePopperAnchor, useToggle } from '@proton/components';
+import { c } from 'ttag';
 
-import { extractSearchParameters } from '../../../helpers/encryptedSearch/esUtils';
+import { Icon, ToolbarButton, generateUID, usePopperAnchor, useToggle } from '@proton/components';
+
 import { useEncryptedSearchLibrary } from '../../EncryptedSearchLibraryProvider';
 import AdvancedSearch from './AdvancedSearch';
 import CalendarSearchInput from './CalendarSearchInput';
@@ -14,24 +14,23 @@ import './SearchOverlay.scss';
 
 interface Props {
     isNarrow: boolean;
+    showSearchField: boolean;
     containerRef: HTMLDivElement | null;
     onSearch: () => void;
     onBackFromSearch: () => void;
 }
 
-const CalendarSearch = ({ isNarrow, containerRef, onSearch, onBackFromSearch }: Props) => {
-    const history = useHistory();
+const CalendarSearch = ({ isNarrow, showSearchField, containerRef, onSearch, onBackFromSearch }: Props) => {
     const [uid] = useState(generateUID('advanced-search-overlay'));
-    const { anchorRef, isOpen, open, close } = usePopperAnchor<HTMLInputElement>();
-    const searchParams = extractSearchParameters(history.location);
-    const [searchInputValue, setSearchInputValue] = useState(searchParams.keyword || '');
+    const { anchorRef, isOpen, open, close } = usePopperAnchor<HTMLButtonElement>();
 
     const { cacheIndexedDB } = useEncryptedSearchLibrary();
-    const { isActive, isIndexing, loading } = useCalendarSearch();
+    const { searchParams, isActive, isIndexing, loading } = useCalendarSearch();
+    const keyword = searchParams.keyword || '';
+
+    const [searchInputValue, setSearchInputValue] = useState(keyword);
     // Show more from inside AdvancedSearch to persist the state when the overlay is closed
     const { state: showMore, toggle: toggleShowMore } = useToggle(false);
-
-    const keyword = searchParams?.keyword || '';
 
     const handleOpen = () => {
         if (isOpen) {
@@ -43,37 +42,45 @@ const CalendarSearch = ({ isNarrow, containerRef, onSearch, onBackFromSearch }: 
         open();
     };
 
+    const handleClear = () => {
+        setSearchInputValue('');
+        open();
+    };
+
     useEffect(() => {
         setSearchInputValue(keyword);
-        if (keyword) {
-            onSearch();
-        }
-        close();
     }, [keyword]);
 
     return (
         <>
-            {isNarrow ? (
-                <TopNavbarListItemSearchButton onClick={handleOpen} />
-            ) : (
+            {showSearchField ? (
                 <CalendarSearchInput
                     ref={anchorRef}
                     value={searchInputValue}
                     onOpen={handleOpen}
-                    onClear={onBackFromSearch}
+                    onClear={handleClear}
+                    onBack={onBackFromSearch}
                     loading={loading}
+                />
+            ) : (
+                <ToolbarButton
+                    icon={<Icon name="magnifier" />}
+                    title={c('Header').t`Search`}
+                    onClick={handleOpen}
+                    ref={anchorRef}
                 />
             )}
             <SearchOverlay id={uid} isOpen={isOpen} anchorRef={anchorRef} onClose={close}>
                 <AdvancedSearch
+                    searchInputParams={{ keyword: searchInputValue }}
                     isNarrow={isNarrow}
                     containerRef={containerRef}
                     isIndexing={isIndexing}
                     isSearchActive={isActive}
+                    onSearch={onSearch}
                     onClose={close}
                     showMore={showMore}
                     toggleShowMore={toggleShowMore}
-                    searchInputValue={searchInputValue}
                 />
             </SearchOverlay>
         </>
