b47393b8a2623c2d76acdfa9f4912ca5e37c287c
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-08-28 17:47:43 +0200

    Fix custom fetch in unleash provider

--- packages/components/containers/unleash/UnleashFlagProvider.tsx
+++ packages/components/containers/unleash/UnleashFlagProvider.tsx
@@ -1,32 +1,43 @@
-import { ReactElement } from 'react';
+import { ReactNode } from 'react';
 
 import FlagProvider from '@unleash/proxy-client-react';
 import { IConfig } from 'unleash-proxy-client';
 
-import { ProtonConfig } from '@proton/shared/lib/interfaces';
+import { Api } from '@proton/shared/lib/interfaces';
 
-import { useApi } from '../../hooks';
+import useApi from '../../hooks/useApi';
+
+// Just something dummy to have a valid domain because the library does new URL
+const prefix = 'https://proton.me/';
+const url = new URL(prefix);
+
+const customFetch =
+    (api: Api): typeof window.fetch =>
+    (url, config) => {
+        if (typeof url === 'string') {
+            return api({
+                url: `feature/v2/frontend${url.replace(prefix, '')}`,
+                headers: config?.headers,
+                silence: true,
+                output: 'raw',
+            });
+        }
+        throw new Error('not supported');
+    };
 
 interface Props {
-    UID?: string;
-    config: ProtonConfig;
-    children: ReactElement;
+    children: ReactNode;
 }
 
-const UnleashFlagProvider = ({ config, children }: Props) => {
+const UnleashFlagProvider = ({ children }: Props) => {
     const api = useApi();
-    const unleashUrl = 'feature/v2/frontend';
     const unleashConfig: IConfig = {
-        // URL needs to be valid for Unleash provider
-        url: `${window.location.origin}${config.API_URL}/${unleashUrl}`,
+        url,
         clientKey: '-', // set by the server
         appName: '-', // set by the server
-        refreshInterval: 600, // refreshInterval in seconds, 10 mins,
+        refreshInterval: 600, // refreshInterval in seconds, 10 mins
         disableMetrics: true,
-        /**
-         * Here we use proton API to fetch the data (needed for the drawer)
-         */
-        fetch: (url: string, options: any) => api({ url: unleashUrl, ...options }),
+        fetch: customFetch(api),
     };
 
     return <FlagProvider config={unleashConfig}>{children}</FlagProvider>;
