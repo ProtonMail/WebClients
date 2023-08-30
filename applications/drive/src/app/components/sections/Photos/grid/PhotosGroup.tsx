bf195f76b7ceb82eeddccb3e13552caf38b7d248
Pierre Bondoerffer <pierre.bondoerffer@proton.ch>
2023-08-30 18:28:24 +0200

    Finish photos components & connect to store nicely
    Co-authored-by: Lucien Peslier <lucien.peslier@proton.ch>

--- applications/drive/src/app/components/sections/Photos/grid/PhotosGroup.tsx
+++ applications/drive/src/app/components/sections/Photos/grid/PhotosGroup.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
-import { FC } from 'react';
+import { CSSProperties, FC } from 'react';
 
-type Props = {};
+type Props = {
+    style: CSSProperties;
+    text: string;
+    showSeparatorLine: boolean;
+};
 
-export const PhotosGroup: FC<Props> = ({}) => {
-    return <div></div>;
+export const PhotosGroup: FC<Props> = ({ style, text, showSeparatorLine }) => {
+    return (
+        <div style={style} className="text-semibold text-no-wrap py-3 flex flex-align-items-center flex-nowrap">
+            {text}
+            {showSeparatorLine && <hr className="w100 m-0 ml-3 h0 border-bottom border-weak" />}
+        </div>
+    );
 };
