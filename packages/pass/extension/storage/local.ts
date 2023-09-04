10ce2342d155773408d2174081552048c9c3540e
Edvin CANDON <edvin.candon@proton.ch>
2023-09-04 13:32:25 +0200

    Simplify storage types & generics
    [IDTEAM-1903]

--- packages/pass/extension/storage/local.ts
+++ packages/pass/extension/storage/local.ts
@@ -1,26 +1,16 @@
 import browser from '@proton/pass/globals/browser';
 
-import type { Storage, StorageData } from './types';
+import type { GetItem, GetItems, RemoveItem, RemoveItems, SetItem, SetItems, StorageInterface } from './types';
 
-const getItems = <T extends StorageData, K = keyof T>(keys: K[]) =>
-    browser.storage.local.get(keys) as Promise<Partial<T>>;
-
-const getItem = async <T extends StorageData, K extends keyof T = keyof T>(key: K): Promise<T[K] | null> =>
-    (await getItems<T>([key]))?.[key] ?? null;
-
-const setItems = <T extends StorageData>(items: T): Promise<void> => browser.storage.local.set(items);
-
-const setItem = <T extends StorageData, K extends keyof T = keyof T>(key: K, value: T[K]): Promise<void> =>
-    setItems({ [key]: value });
-
-const removeItems = <T extends StorageData, K = keyof T>(keys: K[]): Promise<void> =>
-    browser.storage.local.remove(keys as string[]);
-
-const removeItem = <T extends StorageData, K = keyof T>(key: K): Promise<void> => removeItems([key]);
-
+const getItems: GetItems = (keys) => browser.storage.local.get(keys);
+const getItem: GetItem = async (key) => (await getItems([key]))?.[key] ?? null;
+const setItems: SetItems = (items) => browser.storage.local.set(items);
+const setItem: SetItem = (key, value) => setItems({ [key]: value });
+const removeItems: RemoveItems = (keys) => browser.storage.local.remove(keys);
+const removeItem: RemoveItem = (key) => removeItems([key]);
 const clear = (): Promise<void> => browser.storage.local.clear();
 
-const browserLocalStorage: Storage = {
+const browserLocalStorage: StorageInterface = {
     getItems,
     getItem,
     setItems,
