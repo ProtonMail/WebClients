0570f8f2e17dadd7ac82734b37eb2c6863868ee8
Edvin CANDON <edvin.candon@proton.ch>
2023-09-04 13:32:25 +0200

    Simplify storage types & generics
    [IDTEAM-1903]

--- packages/pass/extension/storage/index.ts
+++ packages/pass/extension/storage/index.ts
@@ -1,8 +1,5 @@
-import type { ExtensionLocalData, ExtensionSessionData } from '@proton/pass/types';
-
 import { default as localStorage } from './local';
 import { default as sessionStorage } from './session';
-import type { Storage } from './types';
 
-export const browserSessionStorage = sessionStorage as Storage<ExtensionSessionData>;
-export const browserLocalStorage = localStorage as Storage<ExtensionLocalData>;
+export const browserSessionStorage = sessionStorage;
+export const browserLocalStorage = localStorage;
