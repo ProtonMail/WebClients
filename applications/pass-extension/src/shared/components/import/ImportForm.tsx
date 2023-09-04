b7a4a4335b0b2d96bf42a29bc91942ec705817a5
Edvin CANDON <edvin.candon@proton.ch>
2023-09-04 07:57:16 +0200

    Fix reported copy errors
    [IDTEAM-1934]

--- applications/pass-extension/src/shared/components/import/ImportForm.tsx
+++ applications/pass-extension/src/shared/components/import/ImportForm.tsx
@@ -133,7 +133,7 @@ export const ImportForm: VFC<Omit<ImportFormContext, 'reset' | 'result'>> = ({ f
                     {providerHasUnsupportedItemTypes(form.values.provider) && (
                         <em className="block text-sm color-weak mb-2">
                             {c('Info')
-                                .t`${PASS_APP_NAME} will only import logins and notes. Credit cards, passports, attached files, etc. are not supported at the moment.`}
+                                .t`${PASS_APP_NAME} will only import logins and notes. Passports, attached files, etc. are not supported at the moment.`}
                         </em>
                     )}
                 </>
