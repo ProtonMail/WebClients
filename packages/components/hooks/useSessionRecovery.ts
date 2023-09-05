cd391b332d03ed4477ebcf144a2948c7360c4c59
Jack Crompton <jack.crompton@proton.ch>
2023-09-05 13:43:14 +0200

    Add useSessionRecovery hooks

--- packages/components/hooks/useSessionRecovery.ts
+++ packages/components/hooks/useSessionRecovery.ts
@@ -1,8 +1,20 @@
-import { HOUR } from '@proton/shared/lib/constants';
+import { DAY, HOUR } from '@proton/shared/lib/constants';
 import { MNEMONIC_STATUS, SessionRecoveryState } from '@proton/shared/lib/interfaces';
 
+import useAuthentication from './useAuthentication';
 import useUser from './useUser';
 import useUserSettings from './useUserSettings';
+
+export const useIsSessionRecoveryInitiatedByCurrentSession = () => {
+    const [user] = useUser();
+    const authentication = useAuthentication();
+
+    if (!user?.AccountRecovery) {
+        return null;
+    }
+
+    return user.AccountRecovery.UID === authentication.getUID();
+};
 
 export const useSessionRecoveryState = () => {
     const [user] = useUser();
@@ -52,6 +64,33 @@ export const useIsSessionRecoveryInitiationAvailable = () => {
     return isSessionRecoveryAvailable && isSessionRecoveryEnabled && !sessionRecoveryInitiated;
 };
 
+/**
+ * Determines whether applications should display session recovery in progress "notifications".
+ * Notifications here means banners or modals and not the browser notifications.
+ */
+export const useShouldNotifySessionRecoveryInProgress = () => {
+    const isSessionRecoveryAvailable = useIsSessionRecoveryAvailable();
+    const sessionRecoveryState = useSessionRecoveryState();
+    const isSessionRecoveryInitiatedByCurrentSession = useIsSessionRecoveryInitiatedByCurrentSession();
+
+    return (
+        isSessionRecoveryAvailable &&
+        sessionRecoveryState === SessionRecoveryState.GRACE_PERIOD &&
+        !isSessionRecoveryInitiatedByCurrentSession
+    );
+};
+
+/**
+ * Determines whether applications should display password reset available "notifications".
+ * Notifications here means banners or modals and not the browser notifications.
+ */
+export const useShouldNotifyPasswordResetAvailable = () => {
+    const isSessionRecoveryAvailable = useIsSessionRecoveryAvailable();
+    const sessionRecoveryState = useSessionRecoveryState();
+
+    return isSessionRecoveryAvailable && sessionRecoveryState === SessionRecoveryState.INSECURE;
+};
+
 export const useSessionRecoveryGracePeriodHoursRemaining = () => {
     const [user] = useUser();
 
@@ -62,4 +101,22 @@ export const useSessionRecoveryGracePeriodHoursRemaining = () => {
     const msRemaining = user.AccountRecovery.EndTime * 1000 - Date.now();
 
     return Math.floor(msRemaining / HOUR);
+};
+
+export const useSessionRecoveryInsecureTimeRemaining = () => {
+    const [user] = useUser();
+
+    if (user.AccountRecovery === null || user.AccountRecovery.State !== SessionRecoveryState.INSECURE) {
+        return null;
+    }
+
+    const msRemaining = user.AccountRecovery.EndTime * 1000 - Date.now();
+
+    const inHours = Math.floor(msRemaining / HOUR);
+    const inDays = Math.floor(msRemaining / DAY);
+
+    return {
+        inHours,
+        inDays,
+    };
 };
