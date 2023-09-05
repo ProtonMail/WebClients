5cf775a319bb973d3800fd3ab3ffde2473f34d62
Richard Tetaz <richard.tetaz@proton.ch>
2023-09-05 14:00:02 +0200

    Add KT toggle

--- packages/shared/lib/api/mailSettings.ts
+++ packages/shared/lib/api/mailSettings.ts
@@ -121,6 +121,12 @@ export const updatePromptPin = (PromptPin: number) => ({
     data: { PromptPin },
 });
 
+export const updateKT = (KT: number) => ({
+    url: 'mail/v4/settings/kt',
+    method: 'put',
+    data: { KT },
+});
+
 export const updateAutocrypt = (Autocrypt: any) => ({
     url: 'mail/v4/settings/autocrypt',
     method: 'put',
