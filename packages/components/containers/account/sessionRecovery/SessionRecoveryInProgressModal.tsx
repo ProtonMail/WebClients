a8e33e569f9b1bd076429f5836f4a902e9e4008d
Jack Crompton <jack.crompton@proton.ch>
2023-09-05 21:20:08 +0200

    Update SessionRecoveryInProgressModal

--- packages/components/containers/account/sessionRecovery/SessionRecoveryInProgressModal.tsx
+++ packages/components/containers/account/sessionRecovery/SessionRecoveryInProgressModal.tsx
@@ -2,8 +2,7 @@ import { ReactNode, useState } from 'react';
 
 import { c, msgid } from 'ttag';
 
-import { Button, Href } from '@proton/atoms';
-import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
+import { Button } from '@proton/atoms';
 
 import {
     ModalTwo as Modal,
@@ -11,6 +10,7 @@ import {
     ModalTwoFooter as ModalFooter,
     ModalTwoHeader as ModalHeader,
     ModalProps,
+    SettingsLink,
 } from '../../../components';
 import {
     useHasConfirmedSessionRecoveryInProgress,
@@ -19,15 +19,13 @@ import {
 } from '../../../hooks';
 import ConfirmSessionRecoveryCancellationModal from './ConfirmSessionRecoveryCancellationModal';
 import SessionRecoveryInProgressModalIllustration from './SessionRecoveryInProgressModalIllustration';
-import SessionRecoveryResetConfirmedPrompt from './SessionRecoveryResetConfirmedPrompt';
 
 enum STEP {
     INFO,
     CONFIRM_CANCELLATION,
-    RESET_CONFIRMED,
 }
 
-const SessionRecoveryInProgressModal = ({ ...rest }: ModalProps) => {
+const SessionRecoveryInProgressModal = ({ onClose, ...rest }: ModalProps) => {
     const [user] = useUser();
     const [step, setStep] = useState(STEP.INFO);
     const { confirmSessionRecoveryInProgress } = useHasConfirmedSessionRecoveryInProgress();
@@ -42,38 +40,38 @@ const SessionRecoveryInProgressModal = ({ ...rest }: ModalProps) => {
         return <ConfirmSessionRecoveryCancellationModal open={rest.open} onBack={() => setStep(STEP.INFO)} />;
     }
 
-    if (step === STEP.RESET_CONFIRMED) {
-        return <SessionRecoveryResetConfirmedPrompt open={rest.open} onClose={rest.onClose} />;
-    }
-
     const { title, content, footer }: { title: string; content: ReactNode; footer: ReactNode } = (() => {
         const boldEmail = (
-            <b key="bold-user-email" className="text-break">
-                {user.Email}
-            </b>
+            <>
+                <br />
+                <b key="bold-user-email" className="text-break">
+                    {user.Email}
+                </b>
+            </>
         );
 
         if (step === STEP.INFO) {
             const boldTimeLeft = (
                 <b key="bold-time-left">
-                    {c('Info').ngettext(
-                        msgid`${gracePeriodHoursRemaining} hour`,
-                        `${gracePeriodHoursRemaining} hours`,
-                        gracePeriodHoursRemaining
-                    )}
+                    {
+                        // translator: Full sentence is "To make sure it’s really you trying to reset your password, we wait 72 hours before approving requests. You can change your password in XX more hours."
+                        c('Info').ngettext(
+                            msgid`${gracePeriodHoursRemaining} more hour`,
+                            `${gracePeriodHoursRemaining} more hours`,
+                            gracePeriodHoursRemaining
+                        )
+                    }
                 </b>
             );
 
-            const secureYourAccount = (
-                <Href key="secure-your-account" href={getKnowledgeBaseUrl('/new-account-owner-security-checklist')}>
-                    {
-                        // translator: Full sentence is "If you didn't make this request, or if you no longer need help signing in, please cancel it and take steps to secure your account."
-                        c('Info').t`take steps to secure your account`
-                    }
-                </Href>
+            const viewRequest = (
+                <SettingsLink key="view-request-link" path="/account-password" onClick={onClose}>
+                    {c('Link').t`View request`}
+                </SettingsLink>
             );
+
             return {
-                title: c('Title').t`Do you want to reset your password?`,
+                title: c('Title').t`Password reset requested`,
                 content: (
                     <>
                         <div className="flex flex-justify-center">
@@ -81,18 +79,19 @@ const SessionRecoveryInProgressModal = ({ ...rest }: ModalProps) => {
                         </div>
                         <p>
                             {
-                                // translator: Full sentence is "We received a request to reset your password for account@proton.me. To keep your account safe, we want to make sure it's really you trying to reset your password. You'll be able to reset your password in 72 hours.""
-                                c('Info')
-                                    .jt`We received a request to reset your password for ${boldEmail}. To keep your account safe, we want to make sure it's really you trying to reset your password. You'll be able to reset your password in ${boldTimeLeft}.`
+                                // translator: Full sentence is "We received a password reset request for account@proton.me."
+                                c('Info').jt`We received a password reset request for ${boldEmail}.`
                             }
+                            <br />
                         </p>
                         <p>
                             {
-                                // translator: Full sentence is "If you didn't make this request, or if you no longer need help signing in, please cancel it and take steps to secure your account."
+                                // translator: Full sentence is "To make sure it’s really you trying to reset your password, we wait 72 hours before approving requests. You can change your password in XX more hours."
                                 c('Info')
-                                    .jt`If you didn't make this request, or if you no longer need help signing in, please cancel it and ${secureYourAccount}.`
+                                    .jt`To make sure it’s really you trying to reset your password, we wait 72 hours before approving requests. You can change your password in ${boldTimeLeft}. ${viewRequest}`
                             }
                         </p>
+                        <p>{c('Info').t`If you didn’t ask to reset your password, cancel this request now.`}</p>
                     </>
                 ),
                 footer: (
@@ -107,10 +106,10 @@ const SessionRecoveryInProgressModal = ({ ...rest }: ModalProps) => {
                             color="norm"
                             onClick={() => {
                                 confirmSessionRecoveryInProgress();
-                                setStep(STEP.RESET_CONFIRMED);
+                                onClose?.();
                             }}
                         >
-                            {c('Action').t`It was me, continue`}
+                            {c('Action').t`Got it`}
                         </Button>
                     </>
                 ),
@@ -121,7 +120,7 @@ const SessionRecoveryInProgressModal = ({ ...rest }: ModalProps) => {
     })();
 
     return (
-        <Modal {...rest}>
+        <Modal onClose={onClose} {...rest}>
             <ModalHeader title={title} />
             <ModalContent>{content}</ModalContent>
             <ModalFooter>{footer}</ModalFooter>
