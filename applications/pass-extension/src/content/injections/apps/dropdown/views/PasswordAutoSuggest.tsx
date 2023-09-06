00dd3cf26a2071b1e23e010e7d1d4dc319a4d2a2
Edvin CANDON <edvin.candon@proton.ch>
2023-09-06 14:11:06 +0200

    Copy to clipboard from password autosuggest dropdown
    [IDTEAM-1945]

--- applications/pass-extension/src/content/injections/apps/dropdown/views/PasswordAutoSuggest.tsx
+++ applications/pass-extension/src/content/injections/apps/dropdown/views/PasswordAutoSuggest.tsx
@@ -1,4 +1,5 @@
-import type { VFC } from 'react';
+/* eslint-disable deprecation/deprecation */
+import { type VFC, useEffect, useRef, useState } from 'react';
 
 import { c } from 'ttag';
 
@@ -6,25 +7,58 @@ import { generatePassword } from '@proton/pass/password';
 
 import { DEFAULT_RANDOM_PW_OPTIONS, getCharsGroupedByColor } from '../../../../../shared/hooks/usePasswordGenerator';
 import { SubTheme } from '../../../../../shared/theme/sub-theme';
-import type { IFrameMessage } from '../../../../types';
+import type { IFrameCloseOptions, IFrameMessage } from '../../../../types';
 import { IFrameMessageType } from '../../../../types';
 import { DropdownItem } from '../components/DropdownItem';
 
-export const PasswordAutoSuggest: VFC<{ onMessage?: (message: IFrameMessage) => void }> = ({ onMessage }) => {
+type Props = {
+    onClose?: (options?: IFrameCloseOptions) => void;
+    onMessage?: (message: IFrameMessage) => void;
+    visible?: boolean;
+};
+
+export const PasswordAutoSuggest: VFC<Props> = ({ onMessage, onClose, visible }) => {
+    const timer = useRef<NodeJS.Timer>();
+    const inputRef = useRef<HTMLInputElement>(null);
     const password = generatePassword(DEFAULT_RANDOM_PW_OPTIONS);
+    const [copied, setCopied] = useState(false);
+
+    useEffect(() => {
+        if (copied) timer.current = setTimeout(() => onClose?.(), 1_500);
+        return () => clearTimeout(timer.current);
+    }, [copied]);
+
+    useEffect(() => setCopied(false), [visible]);
+
+    /* FIXME: move away from from `execCommand` and
+     * prefer `navigator.clipboard` API  */
+    const handleClick = async () => {
+        inputRef.current?.select();
+        document.execCommand('copy');
+
+        setCopied(true);
+        onMessage?.({
+            type: IFrameMessageType.DROPDOWN_AUTOFILL_GENERATED_PW,
+            payload: { password },
+        });
+    };
 
     return (
-        <DropdownItem
-            icon="key"
-            subTheme={SubTheme.RED}
-            title={c('Title').t`Use secure password`}
-            subTitle={<span className="text-monospace">{getCharsGroupedByColor(password)}</span>}
-            onClick={() =>
-                onMessage?.({
-                    type: IFrameMessageType.DROPDOWN_AUTOFILL_GENERATED_PW,
-                    payload: { password },
-                })
-            }
-        />
+        <>
+            <DropdownItem
+                icon={copied ? 'checkmark' : 'key'}
+                subTheme={SubTheme.RED}
+                title={copied ? undefined : c('Title').t`Fill & copy password`}
+                subTitle={
+                    copied ? (
+                        c('Info').t`Password copied`
+                    ) : (
+                        <span className="text-monospace">{getCharsGroupedByColor(password)}</span>
+                    )
+                }
+                onClick={handleClick}
+            />
+            <input ref={inputRef} className="absolute" style={{ top: -9999, left: -9990 }} value={password} readOnly />
+        </>
     );
 };
