46c6a69def14f48eed05fe7c4f700e32d70bb207
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-09-06 12:54:54 +0200

    Optimise memory usage of page generation

---
+++ applications/account/pages/interface.ts
@@ -0,0 +1,13 @@
+import { Parameters } from '../src/pages/interface';
+
+export interface HrefLang {
+    hreflang: string;
+    pathname: string;
+}
+
+export interface LocalizedPage {
+    shortLocalizedPathname: string;
+    filename: string;
+    rewrite: { from: RegExp; to: string };
+    parameters: Parameters & { pathname: string; lang: string };
+}
