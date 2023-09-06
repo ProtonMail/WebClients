f9c55f59a4d31219ed0ce0d5ac055d394eafdd69
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2023-09-06 15:28:18 +0100

    Calendar search results style fixes
    UXE-562 UXE-563

--- applications/calendar/src/app/containers/calendar/search/CalendarSearchViewDayEvents.tsx
+++ applications/calendar/src/app/containers/calendar/search/CalendarSearchViewDayEvents.tsx
@@ -40,13 +40,19 @@ const CalendarSearchViewDayEvents = ({ dailyEvents = [], onClickSearchItem, clos
         >
             <div
                 data-testid="month-day-block"
-                className="flex-no-min-children flex-item-noshrink my-1 py-1"
+                className="flex-no-min-children flex-item-noshrink flex-align-items-baseline search-month-day"
                 aria-current={isToday ? `date` : undefined}
             >
-                <div className="text-lg text-semibold min-w5e text-center">
-                    <span className="search-day-number rounded-sm p-1">{day}</span>
+                <div
+                    className="text-lg text-semibold text-center min-w-custom lg:min-w-custom mr-2"
+                    style={{
+                        '--min-w-custom': '2rem',
+                        '--lg-min-w-custom': '5rem',
+                    }}
+                >
+                    <span className="search-day-number rounded-sm p-1 inline-block">{day}</span>
                 </div>
-                <div className="text-lg text-weak min-w9e">{formattedDate}</div>
+                <div className="color-weak min-w9e">{formattedDate}</div>
             </div>
             {Boolean(dailyEvents.length) && (
                 <div className="flex-item-grow search-day flex flex-nowrap flex-column pl-7 lg:pl-0 mt-2 lg:mt-0">
@@ -101,12 +107,10 @@ const CalendarSearchViewDayEvents = ({ dailyEvents = [], onClickSearchItem, clos
                                     className="flex-no-min-children flex-nowrap flex-item-fluid search-event-time-details on-tablet-flex-column"
                                     ref={isClosestToDate ? closestToDateRef : null}
                                 >
-                                    <span className="text-lg min-w14e pl-2 lg:pl-0 search-event-time">
-                                        {timeString}
-                                    </span>
+                                    <span className="min-w14e pl-2 lg:pl-0 pr-2 search-event-time">{timeString}</span>
                                     <span
                                         className={clsx(
-                                            'text-lg text-ellipsis flex-item-fluid pl-2 lg:pl-0 search-event-summary text-bold'
+                                            'text-ellipsis flex-item-fluid pl-2 lg:pl-0 search-event-summary text-bold'
                                         )}
                                     >
                                         {getDisplayTitle(Summary)}
