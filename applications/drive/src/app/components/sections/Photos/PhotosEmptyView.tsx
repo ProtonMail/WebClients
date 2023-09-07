64d32e82d470e9f631ce8040af55ec4f8431f6b6
lpeslier <lucien.peslier@proton.ch>
2023-09-07 14:29:21 +0200

    Move urls to specific const

--- applications/drive/src/app/components/sections/Photos/PhotosEmptyView.tsx
+++ applications/drive/src/app/components/sections/Photos/PhotosEmptyView.tsx
@@ -4,6 +4,7 @@ import { c } from 'ttag';
 
 import { Href } from '@proton/atoms';
 import { EmptyViewContainer } from '@proton/components';
+import { DRIVE_ANDROID_APP, DRIVE_IOS_APP } from '@proton/shared/lib/drive/urls';
 import appStoreSvg from '@proton/styles/assets/img/illustrations/app-store.svg';
 import emptyPhotosSvg from '@proton/styles/assets/img/illustrations/empty-photos.svg';
 import playStoreSvg from '@proton/styles/assets/img/illustrations/play-store.svg';
@@ -24,10 +25,10 @@ export const PhotosEmptyView: FC<Props> = () => {
                     </p>
                 </div>
                 <div className="mt-10 flex flex-align-items-center flex-justify-center gap-4">
-                    <Href href={'https://play.google.com/store/apps/details?id=me.proton.android.drive'}>
+                    <Href href={DRIVE_ANDROID_APP}>
                         <img width="140" src={playStoreSvg} alt="Play Store" />
                     </Href>
-                    <Href href={'https://apps.apple.com/app/proton-drive-cloud-storage/id1509667851'}>
+                    <Href href={DRIVE_IOS_APP}>
                         <img width="140" src={appStoreSvg} alt="App Store" />
                     </Href>
                 </div>
