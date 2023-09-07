ea8ec2b7c3a2c1969cdf9814267c5afde5ae5a13
lpeslier <lucien.peslier@proton.ch>
2023-09-07 14:29:21 +0200

    Move urls to specific const

--- packages/shared/lib/drive/urls.ts
+++ packages/shared/lib/drive/urls.ts
@@ -2,3 +2,5 @@ import { getStaticURL } from '@proton/shared/lib/helpers/url';
 
 export const DRIVE_LANDING_PAGE = getStaticURL('/drive');
 export const DRIVE_PRICING_PAGE = getStaticURL('/drive/pricing?product=drive ');
+export const DRIVE_IOS_APP = 'https://apps.apple.com/app/proton-drive-cloud-storage/id1509667851';
+export const DRIVE_ANDROID_APP = 'https://play.google.com/store/apps/details?id=me.proton.android.drive';
