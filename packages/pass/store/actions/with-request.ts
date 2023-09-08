1d13d42b9b163bb1c809038d4b807621a26ae78f
Edvin CANDON <edvin.candon@proton.ch>
2023-09-08 10:44:29 +0200

    Lay ground work for namespacing request metadata in actions
    [IDTEAM-1669]

--- packages/pass/store/actions/with-request.ts
+++ packages/pass/store/actions/with-request.ts
@@ -3,21 +3,26 @@ import type { AnyAction } from 'redux';
 import { merge } from '@proton/pass/utils/object';
 
 export type RequestType = 'start' | 'failure' | 'success';
+export type NamespaceSeperator = typeof NS_SEPERATOR;
+export type NamespacedRequest = `${string}${NamespaceSeperator}${string}`;
+export type RequestOptions<T extends string = string> = { type: RequestType; id: T };
+export type WithRequest<T = AnyAction, R extends string = string> = T & { meta: { request: RequestOptions<R> } };
 
-export type RequestOptions = {
-    type: RequestType;
-    id: string;
-};
+const NS_SEPERATOR = '::' as const;
 
-export type WithRequest<T = AnyAction> = T & { meta: { request: RequestOptions } };
+export const withRequestNamespace =
+    (request: string) =>
+    (ns: string): NamespacedRequest =>
+        `${request}${NS_SEPERATOR}${ns}`;
 
-/* type guard utility */
+export const getRequestNamespace = (req: NamespacedRequest) => req.split(NS_SEPERATOR)[1];
+
 export const isActionWithRequest = <T extends AnyAction>(action?: T): action is WithRequest<T> =>
     (action as any)?.meta?.request !== undefined;
 
 const withRequest =
-    (request: RequestOptions) =>
-    <T extends object>(action: T): WithRequest<T> =>
+    <U extends string>(request: RequestOptions<U>) =>
+    <T extends object>(action: T): WithRequest<T, U> =>
         merge(action, { meta: { request } });
 
 export default withRequest;
