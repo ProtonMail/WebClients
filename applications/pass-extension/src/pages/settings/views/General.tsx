86d39c0eadfb0d9423229a68968c08d2d57efc71
Edvin CANDON <edvin.candon@proton.ch>
2023-09-08 16:32:01 +0200

    Add language selector in settings general tab
    [IDTEAM-1669]

--- applications/pass-extension/src/pages/settings/views/General.tsx
+++ applications/pass-extension/src/pages/settings/views/General.tsx
@@ -2,11 +2,13 @@ import { type VFC } from 'react';
 
 import { ApplicationLogs } from '../component/ApplicationLogs';
 import { Behaviors } from '../component/Behaviors';
+import { Locale } from '../component/Locale';
 import { PrimaryVault } from '../component/PrimaryVault';
 
 export const General: VFC = () => {
     return (
         <>
+            <Locale />
             <PrimaryVault />
             <Behaviors />
             <ApplicationLogs />
