8dee571e3317e17e7efaf98b990707ce40ec0936
Jack Crompton <jack.crompton@proton.ch>
2023-09-13 18:05:37 +0200

    Add password input to ConfirmSessionRecoveryCancellationModal

--- packages/components/containers/account/sessionRecovery/ConfirmSessionRecoveryCancellationModal.tsx
+++ packages/components/containers/account/sessionRecovery/ConfirmSessionRecoveryCancellationModal.tsx
@@ -1,57 +1,105 @@
+import { useState } from 'react';
+
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms';
+import useLoading from '@proton/hooks/useLoading';
 import { abortSessionRecovery } from '@proton/shared/lib/api/sessionRecovery';
+import { requiredValidator } from '@proton/shared/lib/helpers/formValidators';
+import { srpAuth } from '@proton/shared/lib/srp';
+import noop from '@proton/utils/noop';
 
-import { ModalProps, Prompt, useModalState } from '../../../components';
-import AuthModal from '../../../containers/password/AuthModal';
-import { useEventManager, useNotifications, useUser } from '../../../hooks';
+import {
+    Form,
+    InputFieldTwo,
+    ModalTwo as Modal,
+    ModalTwoContent as ModalContent,
+    ModalTwoFooter as ModalFooter,
+    ModalTwoHeader as ModalHeader,
+    ModalProps,
+    PasswordInputTwo,
+    useFormErrors,
+} from '../../../components';
+import { useApi, useEventManager, useNotifications, useUser } from '../../../hooks';
+import { useSessionRecoveryLocalStorage } from './SessionRecoveryLocalStorageManager';
 
 interface Props extends ModalProps {
     onBack?: () => void;
 }
 
 const ConfirmSessionRecoveryCancellationModal = ({ onBack, onClose, ...rest }: Props) => {
-    const { call } = useEventManager();
-    const [authModal, setAuthModalOpen, renderAuthModal] = useModalState();
-    const { createNotification } = useNotifications();
+    const api = useApi();
     const [user] = useUser();
+    const { call } = useEventManager();
+    const { createNotification } = useNotifications();
+    const { validator, onFormSubmit } = useFormErrors();
+    const [password, setPassword] = useState('');
+    const [submitting, withSubmitting] = useLoading();
+    const { dismissSessionRecoveryCancelled } = useSessionRecoveryLocalStorage();
+
+    const handleSubmit = () => {
+        if (submitting || !onFormSubmit()) {
+            return;
+        }
+
+        const run = async () => {
+            await srpAuth({
+                api,
+                credentials: {
+                    password,
+                },
+                config: abortSessionRecovery(),
+            });
+
+            await call();
+            dismissSessionRecoveryCancelled();
+            createNotification({ text: c('Info').t`Password reset cancelled`, showCloseButton: false });
+            onClose?.();
+        };
+
+        void withSubmitting(run());
+    };
+
+    const handleClose = submitting ? noop : onClose;
 
     return (
-        <>
-            {renderAuthModal && (
-                <AuthModal
-                    config={abortSessionRecovery()}
-                    {...authModal}
-                    onCancel={() => setAuthModalOpen(false)}
-                    onSuccess={async () => {
-                        await call();
-                        createNotification({ text: c('Info').t`Password reset cancelled`, showCloseButton: false });
-                        onClose?.();
-                    }}
+        <Modal
+            as={Form}
+            onSubmit={handleSubmit}
+            onClose={handleClose}
+            disableCloseOnEscape={!!onBack}
+            size="small"
+            {...rest}
+        >
+            <ModalHeader title={c('Title').t`Cancel password reset?`} subline={user.Email} hasClose={!onBack} />
+
+            <ModalContent>
+                <p>{c('Info')
+                    .t`Enter your current password to cancel the password reset process. No other changes will take effect.`}</p>
+
+                <InputFieldTwo
+                    id="password"
+                    bigger
+                    label={c('Label').t`Password`}
+                    error={validator([requiredValidator(password)])}
+                    as={PasswordInputTwo}
+                    disableChange={submitting}
+                    autoComplete="current-password"
+                    value={password}
+                    onValue={setPassword}
                 />
-            )}
-            <Prompt
-                title={c('Title').t`Cancel password reset?`}
-                subline={
-                    // translator: variable here is the users email address
-                    c('Title').t`for ${user.Email}`
-                }
-                buttons={[
-                    <Button color="danger" onClick={() => setAuthModalOpen(true)}>
-                        {c('Action').t`Cancel password reset`}
-                    </Button>,
-                    onBack ? (
-                        <Button onClick={onBack}>{c('Action').t`Back`}</Button>
-                    ) : (
-                        <Button onClick={onClose}>{c('Action').t`Close`}</Button>
-                    ),
-                ]}
-                {...rest}
-            >
-                <p>{c('Info').t`This will cancel the password reset process. No other changes will take effect.`}</p>
-            </Prompt>
-        </>
+            </ModalContent>
+            <ModalFooter>
+                {onBack ? (
+                    <Button onClick={onBack} disabled={submitting}>{c('Action').t`Back`}</Button>
+                ) : (
+                    <Button onClick={onClose} disabled={submitting}>{c('Action').t`Close`}</Button>
+                )}
+                <Button color="danger" type="submit" loading={submitting}>
+                    {c('Action').t`Cancel password reset`}
+                </Button>
+            </ModalFooter>
+        </Modal>
     );
 };
 
