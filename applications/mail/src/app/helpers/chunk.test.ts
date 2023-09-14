ad6b3d69a422f8637c430a77e3ae8273f0a5bd41
romain sanchez <romain.sanchez@proton.ch>
2023-09-14 16:34:17 +0200

    Improve error handling in chunk actions MAILWEB-4444

--- applications/mail/src/app/helpers/chunk.test.ts
+++ applications/mail/src/app/helpers/chunk.test.ts
@@ -1,47 +1,45 @@
-import {
-    getFilteredUndoTokens,
-    runParallelChunkedActions,
-    runParallelUndoableChunkedActions,
-} from 'proton-mail/helpers/chunk';
+import { getFilteredUndoTokens, runParallelChunkedActions } from 'proton-mail/helpers/chunk';
 import { clearAll } from 'proton-mail/helpers/test/helper';
 
-const api = jest.fn();
+const api = jest.fn((any) => any);
 const items: number[] = [...Array(50).keys()];
 
 describe('chunk', () => {
     beforeEach(() => clearAll());
 
     describe('runParallelChunkedActions', () => {
-        const action = jest.fn();
+        const action = jest.fn(() => ({ UndoToken: { Token: 'fake' } }));
 
         it('should chunk actions with expected size', async () => {
-            await runParallelChunkedActions({ api, items, chunkSize: 10, action });
-
-            expect(action).toHaveBeenCalledTimes(5);
-        });
-
-        it('should chunk actions with default size', async () => {
-            await runParallelChunkedActions({ api, items, action });
-
-            expect(action).toHaveBeenCalledTimes(2);
-        });
-    });
-
-    describe('runParallelUndoableChunkedActions', () => {
-        const action = jest.fn(() => ({ Undo: 'fake' }));
-
-        it('should chunk actions with expected size', async () => {
-            const tokens = await runParallelUndoableChunkedActions({ api, items, chunkSize: 10, action });
+            const tokens = await runParallelChunkedActions({ api, items, chunkSize: 10, action });
 
             expect(action).toHaveBeenCalledTimes(5);
             expect(tokens.length).toEqual(5);
         });
 
         it('should chunk actions with default size', async () => {
-            const tokens = await runParallelUndoableChunkedActions({ api, items, action });
+            const tokens = await runParallelChunkedActions({ api, items, action });
 
             expect(action).toHaveBeenCalledTimes(2);
             expect(tokens.length).toEqual(2);
+        });
+
+        it('should rollback changes when a request on a chunk has failed', async () => {
+            const action = jest.fn(() => {
+                throw new Error('FAILS');
+            });
+            const handleChunkFailed = jest.fn();
+
+            try {
+                await runParallelChunkedActions({ api, items, action, optimisticAction: handleChunkFailed });
+            } catch (e: any) {
+                expect(e.message).toEqual('Something went wrong. Please try again.');
+            }
+
+            expect(action).toHaveBeenCalledTimes(2);
+            expect(handleChunkFailed).toHaveBeenCalledTimes(2);
+            expect(handleChunkFailed.mock.calls[0]).toEqual([items.slice(0, 25)]);
+            expect(handleChunkFailed.mock.calls[1]).toEqual([items.splice(25, 49)]);
         });
     });
 
--- applications/mail/src/app/helpers/chunk.ts
+++ applications/mail/src/app/helpers/chunk.ts
@@ -1,3 +1,5 @@
+import { c } from 'ttag';
+
 import { Api } from '@proton/shared/lib/interfaces';
 import chunk from '@proton/utils/chunk';
 import isTruthy from '@proton/utils/isTruthy';
@@ -7,64 +9,74 @@ import { MAIL_ACTION_DEFAULT_CHUNK_SIZE } from 'proton-mail/constants';
 /**
  * Takes an api action and split it in several requests to have smaller actions on the api side
  * By default, split on 25 elements.
- * If there is an "Undo" on the request use "runParallelUndoableChunkedActions" instead
+ * Also, if one of the request fails, an error will be thrown with UndoTokens of the request which did work.
+ * It allows us to Undo all the requests and rollback optimistically the UI for the user.
+ *
+ * However, if the request does not return UndoToken, optimisticAction needs to be used.
+ * It allows to rollback optimistically the UI only on the request which did fail.
+ * In that case, request that passed and the ones that failed will be reflected on the UI.
  */
-export const runParallelChunkedActions = ({
-    api,
-    items,
-    chunkSize,
-    action,
-}: {
-    api: Api;
-    items: any[];
-    chunkSize?: number;
-    action: (chunkedItem: any) => any;
-}) => {
-    const chunks = chunk(items, chunkSize || MAIL_ACTION_DEFAULT_CHUNK_SIZE);
-
-    return Promise.allSettled(
-        chunks.map(async (chunk) => {
-            try {
-                await api(action(chunk));
-            } catch (error: any) {
-                throw error;
-            }
-        })
-    );
-};
-
-/**
- * Takes an api action and split it in several requests to have smaller actions on the api side
- * By default, split on 25 elements.
- * If there is no "Undo" on the request use "runParallelChunkedActions" instead
- */
-export const runParallelUndoableChunkedActions = ({
+export const runParallelChunkedActions = async ({
     api,
     items,
     chunkSize,
     action,
     canUndo = true,
+    optimisticAction,
 }: {
     api: Api;
     items: any[];
     chunkSize?: number;
     action: (chunkedItem: any) => any;
     canUndo?: boolean;
+    optimisticAction?: (items: any) => any;
 }) => {
     const chunks = chunk(items, chunkSize || MAIL_ACTION_DEFAULT_CHUNK_SIZE);
 
-    return Promise.allSettled(
+    // Store failed chunks in case we need to rollback them
+    const rollbacks: (() => any)[] = [];
+
+    const tokens = await Promise.allSettled(
         chunks.map(async (chunk) => {
+            let rollback;
             try {
-                const { UndoToken } = await api<{ UndoToken: { Token: string } }>(action(chunk));
+                // For requests that we cannot undo, we first need to apply the optimistic action.
+                if (optimisticAction) {
+                    rollback = optimisticAction(chunk);
+                }
+
+                // Do the action on the chunk
+                const res = await api<{ UndoToken: { Token: string } }>({ ...action(chunk), silence: true });
+
+                // If we can undo the request, return the Undo token
                 if (canUndo) {
+                    const { UndoToken } = res;
                     return UndoToken.Token;
                 }
             } catch (error: any) {
+                // If an error happened, store the "rollback" function of the current chunk
+                if (rollback) {
+                    rollbacks.push(rollback);
+                }
                 throw error;
             }
         })
     );
+
+    // Throw an error if one of the chunked request has failed
+    const isPartialFail = tokens.some((res) => res.status === 'rejected');
+
+    if (isPartialFail) {
+        // If the request cannot be undone, and some of them failed, undo all the optimistic updates done
+        if (optimisticAction) {
+            rollbacks.forEach((rollback) => rollback());
+        }
+        const newError = new Error(c('Error').t`Something went wrong. Please try again.`);
+        Object.assign(newError, { data: tokens });
+        throw newError;
+    }
+
+    return tokens;
 };
 
 export const getFilteredUndoTokens = (tokens: PromiseSettledResult<string | undefined>[]) => {
