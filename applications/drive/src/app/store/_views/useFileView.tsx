2c97702d735247c60fc740327c4df7fec70e58ab
lpeslier <lucien.peslier@proton.ch>
2023-09-15 14:59:18 +0200

    Add fallback for non supported preview images

--- applications/drive/src/app/store/_views/useFileView.tsx
+++ applications/drive/src/app/store/_views/useFileView.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 
 import { useLoading } from '@proton/hooks';
+import { SupportedMimeTypes } from '@proton/shared/lib/drive/constants';
 import { isPreviewAvailable } from '@proton/shared/lib/helpers/preview';
 
 import { isIgnoredError } from '../../utils/errorHandling';
@@ -17,7 +18,7 @@ import { SortParams } from './utils/useSorting';
  */
 export default function useFileView(shareId: string, linkId: string, useNavigation = false, revisionId?: string) {
     const { downloadStream } = useDownload();
-    const { isLinkLoading, isContentLoading, error, link, contents, downloadFile } = useFileViewBase(
+    const { isLinkLoading, isContentLoading, error, link, contents, contentsMimeType, downloadFile } = useFileViewBase(
         shareId,
         linkId,
         downloadStream,
@@ -25,7 +26,7 @@ export default function useFileView(shareId: string, linkId: string, useNavigati
     );
     const navigation = useFileViewNavigation(useNavigation, shareId, link?.parentLinkId, linkId);
 
-    return { navigation, isLinkLoading, isContentLoading, error, link, contents, downloadFile };
+    return { navigation, isLinkLoading, isContentLoading, error, link, contents, contentsMimeType, downloadFile };
 }
 
 export function usePublicFileView(
@@ -35,14 +36,14 @@ export function usePublicFileView(
     sortParams: SortParams = DEFAULT_SORT
 ) {
     const { downloadStream } = usePublicDownload();
-    const { isLinkLoading, isContentLoading, error, link, contents, downloadFile } = useFileViewBase(
+    const { isLinkLoading, isContentLoading, error, link, contents, contentsMimeType, downloadFile } = useFileViewBase(
         shareId,
         linkId,
         downloadStream
     );
     const navigation = usePublicFileViewNavigation(useNavigation, shareId, sortParams, link?.parentLinkId, linkId);
 
-    return { navigation, isLinkLoading, isContentLoading, error, link, contents, downloadFile };
+    return { navigation, isLinkLoading, isContentLoading, error, link, contents, contentsMimeType, downloadFile };
 }
 
 function useFileViewBase(
@@ -53,18 +54,25 @@ function useFileViewBase(
 ) {
     const { getLink } = useLink();
     const { download } = useDownloadProvider();
-
+    const { getPreviewThumbnail } = useDownload();
     const [isContentLoading, withContentLoading] = useLoading(true);
 
     const [error, setError] = useState<any>();
     const [link, setLink] = useState<DecryptedLink>();
     const [contents, setContents] = useState<Uint8Array[]>();
+    const [contentsMimeType, setContentsMimeType] = useState<string>();
 
     const preloadFile = async (abortSignal: AbortSignal) => {
         const link = await getLink(abortSignal, shareId, linkId);
 
-        if (link && isPreviewAvailable(link.mimeType, link.size)) {
-            setLink(link);
+        if (!link) {
+            setContents(undefined);
+            return;
+        }
+
+        setLink(link);
+        if (isPreviewAvailable(link.mimeType, link.size)) {
+            setContentsMimeType(link.mimeType);
             const { stream, controls } = downloadStream([
                 {
                     ...link,
@@ -77,15 +85,23 @@ function useFileViewBase(
             });
 
             setContents(await streamToBuffer(stream));
-            // After file content download is finished we have complete information
-            // about signatures
-            setLink(await getLink(abortSignal, shareId, linkId));
+            // Fallback is only available for photos
+        } else if (!!link.activeRevision?.photo) {
+            // We force jpg type as thumbnails are always jpg for photos
+            setContentsMimeType(SupportedMimeTypes.jpg);
+            try {
+                const hdThumbnail = await getPreviewThumbnail(abortSignal, shareId, linkId);
+                setContents(await hdThumbnail.contents);
+            } catch {
+                setContents(undefined);
+            }
         } else {
             setContents(undefined);
-            if (link) {
-                setLink(link);
-            }
+            return;
         }
+        // After file content download is finished we have complete information
+        // about signatures
+        setLink(await getLink(abortSignal, shareId, linkId));
     };
 
     useEffect(() => {
@@ -123,6 +139,7 @@ function useFileViewBase(
         error,
         link,
         contents,
+        contentsMimeType,
         downloadFile,
     };
 }
