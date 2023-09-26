54142f7c8fd79f836147c5142d92e29abe9aefe8
lpeslier <lucien.peslier@proton.ch>
2023-09-26 11:29:01 +0200

    Refactor FeatureFlag context

--- packages/components/containers/unleash/index.ts
+++ packages/components/containers/unleash/index.ts
@@ -1,4 +1,11 @@
+import { useFlag as useUnleashFlag } from '@unleash/proxy-client-react';
+
+import type { FeatureFlag } from '@proton/components/containers';
+
 export * from './UnleashContext';
 export { default as UnleashFlagProvider } from './UnleashFlagProvider';
 export { default as useFlagsReady } from './useFlagsReady';
-export { useFlag, useFlags } from '@unleash/proxy-client-react';
+
+const useFlag: (name: FeatureFlag) => boolean = useUnleashFlag;
+
+export { useFlag };
