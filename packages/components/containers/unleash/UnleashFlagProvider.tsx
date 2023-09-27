2f65b39baab793da90fc929a9a72b593d599e2ce
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2023-09-27 18:17:10 +0200

    Use api helper to make calls to Unleash
    Before we were using fetch directly, but the fetch calls from the drawer would be unauthorized
    due to them being sent with the Mail UID but the Calendar cookie
    
    CALWEB-4624

--- packages/components/containers/unleash/UnleashFlagProvider.tsx
+++ packages/components/containers/unleash/UnleashFlagProvider.tsx
@@ -3,9 +3,9 @@ import { ReactElement } from 'react';
 import FlagProvider from '@unleash/proxy-client-react';
 import { IConfig } from 'unleash-proxy-client';
 
-import { getClientID } from '@proton/shared/lib/apps/helper';
-import { getAppVersionHeaders, getUIDHeaders } from '@proton/shared/lib/fetch/headers';
 import { ProtonConfig } from '@proton/shared/lib/interfaces';
+
+import { useApi } from '../../hooks';
 
 interface Props {
     UID?: string;
@@ -13,23 +13,20 @@ interface Props {
     children: ReactElement;
 }
 
-const UnleashFlagProvider = ({ UID, config, children }: Props) => {
-    const clientId = getClientID(config.APP_NAME);
-    const authHeaders = UID ? getUIDHeaders(UID) : undefined;
-    const appVersionHeaders = getAppVersionHeaders(clientId, config.APP_VERSION);
+const UnleashFlagProvider = ({ config, children }: Props) => {
+    const api = useApi();
+    const unleashUrl = 'feature/v2/frontend';
     const unleashConfig: IConfig = {
-        url: `${window.location.origin}${config.API_URL}/feature/v2/frontend`,
+        // URL needs to be valid for Unleash provider
+        url: `${window.location.origin}${config.API_URL}/${unleashUrl}`,
         clientKey: '-', // set by the server
         appName: '-', // set by the server
-        refreshInterval: 600, // refreshInterval in seconds, 10 mins
-        customHeaders: {
-            'Content-Type': 'application/json;charset=utf-8',
-            accept: 'application/vnd.protonmail.v1+json',
-            ...authHeaders,
-            ...appVersionHeaders,
-        },
-        // Route feature/v2/frontend/client/metrics is not set by the server
+        refreshInterval: 600, // refreshInterval in seconds, 10 mins,
         disableMetrics: true,
+        /**
+         * Here we use proton API to fetch the data (needed for the drawer)
+         */
+        fetch: (url: string, options: any) => api({ url: unleashUrl, ...options }),
     };
 
     return <FlagProvider config={unleashConfig}>{children}</FlagProvider>;
