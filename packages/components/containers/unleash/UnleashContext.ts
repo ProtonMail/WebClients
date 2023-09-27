a37b8c6654f11075da2ea64c71769b75b1df49a5
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-09-27 11:03:27 +0200

    Drop edit email address flag

--- packages/components/containers/unleash/UnleashContext.ts
+++ packages/components/containers/unleash/UnleashContext.ts
@@ -4,7 +4,6 @@
  */
 export enum FeatureFlag {
     AttachmentThumbnails = 'AttachmentThumbnails',
-    EditEmailAddress = 'EditEmailAddress',
     MaintenanceImporter = 'MaintenanceImporter',
     WebMailPageSizeSetting = 'WebMailPageSizeSetting',
 }
