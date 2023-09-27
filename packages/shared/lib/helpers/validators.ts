1203472935a5146624ce3e18b33638638f268834
romain sanchez <romain.sanchez@proton.ch>
2023-09-27 14:04:54 +0200

    Improve b64 image detection in contacts CLIENT-5430

--- packages/shared/lib/helpers/validators.ts
+++ packages/shared/lib/helpers/validators.ts
@@ -5,7 +5,6 @@ export const REGEX_URL =
     /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
 export const REGEX_HEX_COLOR = /^#([a-f0-9]{3,4}|[a-f0-9]{4}(?:[a-f0-9]{2}){1,2})\b$/i;
 export const REGEX_NUMBER = /^\d+$/;
-export const REGEX_BASE64_IMAGE = /^data:image\/(?:gif|png|jpeg|bmp|webp|svg\+xml|apng|tiff);base64/;
 export const REGEX_PUNYCODE = /^(http|https):\/\/xn--/;
 export const REGEX_USERNAME = /^[A-Za-z0-9]+(?:[_.-][A-Za-z0-9]+)*$/;
 export const REGEX_USERNAME_START = /^[A-Za-z0-9]/;
@@ -25,5 +24,5 @@ export const isURL = (value = '') => {
 export const isDomain = isValidDomain;
 export const isHexColor = (value = '') => REGEX_HEX_COLOR.test(value);
 export const isNumber = (value = '') => REGEX_NUMBER.test(value);
-export const isBase64Image = (value = '') => REGEX_BASE64_IMAGE.test(value);
+export const isBase64Image = (value = '') => value.startsWith('data:image') && value.includes(';base64');
 export const isPunycode = (value = '') => REGEX_PUNYCODE.test(value);
