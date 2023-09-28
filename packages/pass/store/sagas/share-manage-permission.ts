f0ff5e4d69f35a57071b07b3c896981e2107caa7
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2023-09-28 10:15:23 +0200

    WIP: remove user access to vault
    [IDTEAM-1697]

---
+++ packages/pass/store/sagas/share-manage-permission.ts
@@ -0,0 +1,19 @@
+import { put, takeEvery } from 'redux-saga/effects';
+
+
+import { vaultRemoveAccessFailure, vaultRemoveAccessIntent, vaultRemoveAccessSuccess } from '../actions';
+import { removeUserAccess } from './workers/shares';
+
+function* removeUserAccessWorker({ payload, meta: { request } }: ReturnType<typeof vaultRemoveAccessIntent>) {
+    try {
+        yield removeUserAccess(payload);
+        yield put(vaultRemoveAccessSuccess(request.id));
+    } catch (err) {
+        yield put(vaultRemoveAccessFailure(request.id, err));
+    }
+}
+
+
+export default function* watcher() {
+    yield takeEvery(vaultRemoveAccessIntent.match, removeUserAccessWorker);
+}
