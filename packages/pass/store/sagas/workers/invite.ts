fd5088689dfb1b0f1e73a9d4106d7ad4b22b0655
Edvin CANDON <edvin.candon@proton.ch>
2023-09-28 18:34:01 +0200

    Add pending user invite worker in sagas

--- packages/pass/store/sagas/workers/invite.ts
+++ packages/pass/store/sagas/workers/invite.ts
@@ -1,23 +1,21 @@
-import { c } from 'ttag';
-
 import { type PendingInvite } from '@proton/pass/types/data/invites';
-import { getPublicKeys } from '@proton/shared/lib/api/keys';
 
 import { api } from '../../../api';
 import { PassCrypto } from '../../../crypto';
-import type { PublicKeysResponse, VaultInviteCreateRequest } from '../../../types';
+import type { VaultInviteCreateRequest } from '../../../types';
+import { getPrimaryPublicKeyForEmail } from './address';
 
-export const createVaultInvite = async ({ shareId, email, role }: VaultInviteCreateRequest) => {
-    const inviteeKeys = await api<PublicKeysResponse>(getPublicKeys({ Email: email }));
-    const publicKey = inviteeKeys.Keys[0]?.PublicKey;
-    if (!publicKey) throw new Error(c(`Error`).t`Could not resolve recipient's address key`);
-
-    return api({
+export const createVaultInvite = async ({ shareId, email, role }: VaultInviteCreateRequest) =>
+    api({
         url: `pass/v1/share/${shareId}/invite`,
         method: 'post',
-        data: await PassCrypto.createVaultInvite({ shareId, email, role, inviteePublicKey: publicKey }),
+        data: await PassCrypto.createVaultInvite({
+            shareId,
+            email,
+            role,
+            inviteePublicKey: await getPrimaryPublicKeyForEmail(email),
+        }),
     });
-};
 
 export const loadPendingShareInvites = async (shareId: string): Promise<PendingInvite[]> => {
     const { Invites } = await api({
