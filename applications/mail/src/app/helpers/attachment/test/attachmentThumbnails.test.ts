228d80ecac44c15727ab23779791552145f01fa6
romain sanchez <romain.sanchez@proton.ch>
2023-09-29 11:09:18 +0200

    Enable attachment preview in message mode MAILWEB-4525

--- applications/mail/src/app/helpers/attachment/test/attachmentThumbnails.test.ts
+++ applications/mail/src/app/helpers/attachment/test/attachmentThumbnails.test.ts
@@ -33,28 +33,28 @@ const getMessage = (isSpam = false) => {
 describe('attachmentThumbnails', () => {
     describe('canShowAttachmentThumbnails', () => {
         it('should show attachment thumbnails', () => {
-            expect(canShowAttachmentThumbnails(false, true, getConversation(), true)).toBeTruthy();
-            expect(canShowAttachmentThumbnails(false, true, getMessage(), true)).toBeTruthy();
+            expect(canShowAttachmentThumbnails(false, getConversation(), true)).toBeTruthy();
+            expect(canShowAttachmentThumbnails(false, getMessage(), true)).toBeTruthy();
         });
 
         it('should not show attachment thumbnails when feature flag is off', () => {
-            expect(canShowAttachmentThumbnails(false, true, getConversation(), false)).toBeFalsy();
-            expect(canShowAttachmentThumbnails(false, true, getMessage(), false)).toBeFalsy();
+            expect(canShowAttachmentThumbnails(false, getConversation(), false)).toBeFalsy();
+            expect(canShowAttachmentThumbnails(false, getMessage(), false)).toBeFalsy();
         });
 
         it('should not show attachment thumbnails on compact view', () => {
-            expect(canShowAttachmentThumbnails(true, true, getConversation(), true)).toBeFalsy();
-            expect(canShowAttachmentThumbnails(true, true, getMessage(), true)).toBeFalsy();
+            expect(canShowAttachmentThumbnails(true, getConversation(), true)).toBeFalsy();
+            expect(canShowAttachmentThumbnails(true, getMessage(), true)).toBeFalsy();
         });
 
         it('should not show attachment thumbnails when no attachment metadata is attached to the element', () => {
-            expect(canShowAttachmentThumbnails(false, true, {} as Conversation, true)).toBeFalsy();
-            expect(canShowAttachmentThumbnails(false, true, {} as Message, true)).toBeFalsy();
+            expect(canShowAttachmentThumbnails(false, {} as Conversation, true)).toBeFalsy();
+            expect(canShowAttachmentThumbnails(false, {} as Message, true)).toBeFalsy();
         });
 
         it('should not show attachment thumbnails when element is in SPAM', () => {
-            expect(canShowAttachmentThumbnails(false, true, getConversation(true), true)).toBeFalsy();
-            expect(canShowAttachmentThumbnails(false, true, getMessage(true), true)).toBeFalsy();
+            expect(canShowAttachmentThumbnails(false, getConversation(true), true)).toBeFalsy();
+            expect(canShowAttachmentThumbnails(false, getMessage(true), true)).toBeFalsy();
         });
     });
 
