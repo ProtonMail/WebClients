0d092f644920f4f6001cf1e9456f6c8346028cf7
Edvin CANDON <edvin.candon@proton.ch>
2023-09-29 17:51:23 +0200

    Accept invite sequence

--- packages/pass/store/reducers/invites.ts
+++ packages/pass/store/reducers/invites.ts
@@ -1,13 +1,16 @@
 import type { Reducer } from 'redux';
 
 import type { Invite } from '@proton/pass/types/data/invites';
+import { objectDelete } from '@proton/pass/utils/object';
 
-import { syncInvites } from '../actions';
+import { inviteAcceptSuccess, syncInvites } from '../actions';
 
 export type InviteState = Record<string, Invite>;
 
 const reducer: Reducer<InviteState> = (state = {}, action) => {
     if (syncInvites.match(action)) return action.payload;
+    if (inviteAcceptSuccess.match(action)) return objectDelete(state, action.payload.token);
+
     return state;
 };
 
