6ca5f8da85f6d9e750e6c00a42dd912a04444baf
Edvin CANDON <edvin.candon@proton.ch>
2023-10-03 17:34:10 +0200

    Fix failing pass tests

--- packages/pass/jest.setup.js
+++ packages/pass/jest.setup.js
@@ -11,6 +11,18 @@ jest.mock('@proton/shared/lib/helpers/setupCryptoWorker', () => ({
     loadCryptoWorker: jest.fn(),
 }));
 
+jest.mock('@proton/shared/lib/i18n/dateFnLocales', () => ({
+    __esModule: true,
+}));
+
+jest.mock('@proton/shared/lib/pow/wasmWorkerWrapper.ts', () => ({
+    __esModule: true,
+}));
+
+jest.mock('@proton/shared/lib/pow/pbkdfWorkerWrapper.ts', () => ({
+    __esModule: true,
+}));
+
 jest.mock('loglevel');
 
 // JSDom does not include webcrypto
