7613244892bd5f3a03b251852f92242d29b3a9f3
Edvin CANDON <edvin.candon@proton.ch>
2023-10-03 10:08:43 +0200

    Adjust empty vault view scrollbar position

--- applications/pass-extension/src/popup/views/Sidebar/ItemsList.tsx
+++ applications/pass-extension/src/popup/views/Sidebar/ItemsList.tsx
@@ -41,7 +41,7 @@ export const ItemsList: VFC = () => {
             )}
 
             {filtered.length === 0 ? (
-                <div className="flex flex-justify-center flex-align-items-center w70 m-auto overflow-x-auto">
+                <div className="flex flex-justify-center flex-align-items-center w100 m-auto overflow-x-auto py-3">
                     <ItemsListPlaceholder />
                 </div>
             ) : (
