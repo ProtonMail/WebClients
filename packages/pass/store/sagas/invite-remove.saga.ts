cd0cfc4b8a1c5830e632e835cacc43ded28c0c5a
Edvin CANDON <edvin.candon@proton.ch>
2023-10-03 15:46:40 +0200

    Fine-tune invite sequences

--- packages/pass/store/sagas/invite-remove.saga.ts
+++ packages/pass/store/sagas/invite-remove.saga.ts
@@ -1,11 +1,11 @@
 import { put, takeEvery } from 'redux-saga/effects';
 
 import { inviteRemoveFailure, inviteRemoveIntent, inviteRemoveSuccess } from '../actions';
-import { resendInvite } from './workers/invite';
+import { removeInvite } from './workers/invite';
 
-function* resendInviteWorker({ payload, meta: { request } }: ReturnType<typeof inviteRemoveIntent>) {
+function* removeInviteWorker({ payload, meta: { request } }: ReturnType<typeof inviteRemoveIntent>) {
     try {
-        yield resendInvite(payload);
+        yield removeInvite(payload);
         yield put(inviteRemoveSuccess(request.id, payload.shareId, payload.inviteId));
     } catch (err) {
         yield put(inviteRemoveFailure(request.id, err));
@@ -13,5 +13,5 @@ function* resendInviteWorker({ payload, meta: { request } }: ReturnType<typeof i
 }
 
 export default function* watcher() {
-    yield takeEvery(inviteRemoveIntent.match, resendInviteWorker);
+    yield takeEvery(inviteRemoveIntent.match, removeInviteWorker);
 }
