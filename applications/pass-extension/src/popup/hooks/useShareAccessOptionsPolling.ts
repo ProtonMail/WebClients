59f1aeda387f0a2359ba12bf5e8b636bf8240d3a
Edvin CANDON <edvin.candon@proton.ch>
2023-10-03 15:49:54 +0200

    Improve loading states in vault response modal and manager views

--- applications/pass-extension/src/popup/hooks/useShareAccessOptionsPolling.ts
+++ applications/pass-extension/src/popup/hooks/useShareAccessOptionsPolling.ts
@@ -1,5 +1,6 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
+import usePrevious from '@proton/hooks/usePrevious';
 import { ACTIVE_POLLING_TIMEOUT } from '@proton/pass/events/constants';
 import { getShareAccessOptionsIntent } from '@proton/pass/store';
 import { shareAccessOptionsRequest } from '@proton/pass/store/actions/requests';
@@ -9,17 +10,24 @@ import { useActionWithRequest } from '../../shared/hooks/useActionWithRequest';
 export const useShareAccessOptionsPolling = (shareId: string) => {
     const timer = useRef<NodeJS.Timer>();
 
-    const getShareAccessOptions = useActionWithRequest({
+    const { loading, dispatch, revalidate } = useActionWithRequest({
         action: getShareAccessOptionsIntent,
         requestId: shareAccessOptionsRequest(shareId),
     });
 
+    const wasLoading = usePrevious(loading);
+    const [didLoad, setDidLoad] = useState(false);
+
     useEffect(() => {
-        timer.current = setInterval(() => getShareAccessOptions.dispatch(shareId), ACTIVE_POLLING_TIMEOUT);
-        getShareAccessOptions.revalidate(shareId);
+        timer.current = setInterval(() => dispatch(shareId), ACTIVE_POLLING_TIMEOUT);
+        revalidate(shareId);
 
         return () => clearInterval(timer.current);
     }, [shareId]);
 
-    return getShareAccessOptions.loading;
+    useEffect(() => {
+        if (wasLoading === true && !loading) setDidLoad(true);
+    }, [loading]);
+
+    return didLoad ? false : loading;
 };
