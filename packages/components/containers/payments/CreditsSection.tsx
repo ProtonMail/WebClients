409732776ca89432bc2205363e4a65f4ae81b1e6
Alexey Karpov <alexey.karpov@proton.ch>
2023-10-06 12:50:44 +0900

    Simplify subscription section
    PAY-1277

--- packages/components/containers/payments/CreditsSection.tsx
+++ packages/components/containers/payments/CreditsSection.tsx
@@ -1,20 +1,46 @@
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms';
+import { isFreeSubscription } from '@proton/shared/lib/constants';
 import { getHasVpnB2BPlan, isManagedExternally } from '@proton/shared/lib/helpers/subscription';
+import { External } from '@proton/shared/lib/interfaces';
+import noop from '@proton/utils/noop';
 
+import { Loader } from '../../components';
 import { useModalState, useModalTwo } from '../../components/modalTwo';
-import { useSubscription, useUser } from '../../hooks';
+import { useTypedSubscription, useUser } from '../../hooks';
 import { SettingsParagraph, SettingsSection } from '../account';
 import CreditsModal from './CreditsModal';
 import InAppPurchaseModal from './subscription/InAppPurchaseModal';
 
 const CreditsSection = () => {
-    const [subscription, loadingSubscription] = useSubscription();
+    const [subscription] = useTypedSubscription();
     const [creditModalProps, setCreditModalOpen, renderCreditModal] = useModalState();
     const [externalSubscriptionModal, showExternalSubscriptionModal] = useModalTwo(InAppPurchaseModal);
 
     const [{ Credit }] = useUser();
+
+    if (!subscription) {
+        return <Loader />;
+    }
+
+    let upcomingSubscriptionPrice: number = 0;
+    // In case of Chargebee, they don't display the balance for the upcoming subscription.
+    // Instead, they count it as already paid, and they return credits balance already taking into account
+    // the upcoming subscription.
+    if (
+        !isFreeSubscription(subscription) &&
+        subscription.External !== External.Chargebee &&
+        subscription?.UpcomingSubscription
+    ) {
+        upcomingSubscriptionPrice =
+            subscription.UpcomingSubscription.Amount - subscription.UpcomingSubscription.Discount;
+    }
+
+    let availableCredits = (Credit - upcomingSubscriptionPrice) / 100;
+    if (availableCredits < 0) {
+        availableCredits = 0;
+    }
 
     const hasVpnB2B = getHasVpnB2BPlan(subscription);
 
@@ -27,11 +53,13 @@ const CreditsSection = () => {
             {hasVpnB2B ? null : (
                 <div className="mb-7">
                     <Button
-                        loading={loadingSubscription}
                         shape="outline"
                         onClick={() => {
                             if (isManagedExternally(subscription)) {
-                                showExternalSubscriptionModal({ subscription, key: 'external-subscription-modal' });
+                                showExternalSubscriptionModal({
+                                    subscription,
+                                    key: 'external-subscription-modal',
+                                }).catch(noop);
                                 return;
                             }
 
@@ -43,7 +71,7 @@ const CreditsSection = () => {
             <div className="px-4 mb-4 flex flex-justify-space-between">
                 <span className="text-bold" data-testid="unused-credits">{c('Credits').t`Available credits`}</span>
                 <span className="text-bold" data-testid="avalaible-credits">
-                    {Credit / 100}
+                    {availableCredits}
                 </span>
             </div>
             <hr />
