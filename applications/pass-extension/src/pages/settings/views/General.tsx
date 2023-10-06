79cbe2092ec0c7576446a80fd040c758a6e87342
Irena <irena.zazoska@proton.ch>
2023-10-06 12:14:16 +0200

    Remove PrimaryVault and add Autosave vault in Settings
    [IDTEAM-2059]

--- applications/pass-extension/src/pages/settings/views/General.tsx
+++ applications/pass-extension/src/pages/settings/views/General.tsx
@@ -3,13 +3,11 @@ import { type VFC } from 'react';
 import { ApplicationLogs } from '../component/ApplicationLogs';
 import { Behaviors } from '../component/Behaviors';
 import { Locale } from '../component/Locale';
-import { PrimaryVault } from '../component/PrimaryVault';
 
 export const General: VFC = () => {
     return (
         <>
             {ENV === 'development' && <Locale />}
-            <PrimaryVault />
             <Behaviors />
             <ApplicationLogs />
         </>
