9ca8f9de7639c4f843c8b464b638763063c2d89b
Pierre Bondoerffer <pierre.bondoerffer@proton.ch>
2023-10-09 14:25:58 +0200

    Remove cached photo linkIds directly in trashLinks

--- applications/drive/src/app/components/sections/Drive/ToolbarButtons/MoveToTrashButton.tsx
+++ applications/drive/src/app/components/sections/Drive/ToolbarButtons/MoveToTrashButton.tsx
@@ -7,10 +7,9 @@ import { DecryptedLink, useActions } from '../../../../store';
 
 interface Props {
     selectedLinks: DecryptedLink[];
-    onTrash?: (linkIds: string[]) => void;
 }
 
-const MoveToTrashButton = ({ selectedLinks, onTrash }: Props) => {
+const MoveToTrashButton = ({ selectedLinks }: Props) => {
     const [isLoading, withLoading] = useLoading();
     const { trashLinks } = useActions();
 
@@ -19,15 +18,7 @@ const MoveToTrashButton = ({ selectedLinks, onTrash }: Props) => {
             disabled={isLoading}
             title={c('Action').t`Move to trash`}
             icon={<Icon name="trash" alt={c('Action').t`Move to trash`} />}
-            onClick={() =>
-                withLoading(
-                    trashLinks(new AbortController().signal, selectedLinks).then((result) => {
-                        if (onTrash && result) {
-                            onTrash(result.successes);
-                        }
-                    })
-                )
-            }
+            onClick={() => withLoading(trashLinks(new AbortController().signal, selectedLinks))}
             data-testid="toolbar-trash"
         />
     );
