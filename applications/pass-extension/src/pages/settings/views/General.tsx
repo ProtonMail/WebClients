1d51f1a180d13201559bb9d446afacc5e18c751a
Edvin CANDON <edvin.candon@proton.ch>
2023-10-10 13:00:33 +0200

    Handle both legacy primary vaults and new autosave vault in settings
    [IDTEAM-2059]

--- applications/pass-extension/src/pages/settings/views/General.tsx
+++ applications/pass-extension/src/pages/settings/views/General.tsx
@@ -1,13 +1,34 @@
 import { type VFC } from 'react';
+import { useDispatch } from 'react-redux';
 
+import { c } from 'ttag';
+
+import { selectDefaultVault, selectOwnWritableVaults, vaultSetPrimaryIntent } from '@proton/pass/store';
+import { PassFeature } from '@proton/pass/types/api/features';
+
+import { useFeatureFlag } from '../../../shared/hooks/useFeatureFlag';
 import { ApplicationLogs } from '../component/ApplicationLogs';
 import { Behaviors } from '../component/Behaviors';
 import { Locale } from '../component/Locale';
+import { VaultSetting } from '../component/VaultSetting';
 
 export const General: VFC = () => {
+    const dispatch = useDispatch();
+    const primaryVaultDisabled = useFeatureFlag(PassFeature.PassRemovePrimaryVault);
+
     return (
         <>
             {ENV === 'development' && <Locale />}
+            {!primaryVaultDisabled && (
+                <VaultSetting
+                    label={c('Label').t`Primary vault`}
+                    optionsSelector={selectOwnWritableVaults}
+                    valueSelector={selectDefaultVault}
+                    onSubmit={({ shareId, content }) =>
+                        dispatch(vaultSetPrimaryIntent({ id: shareId, name: content.name }))
+                    }
+                />
+            )}
             <Behaviors />
             <ApplicationLogs />
         </>
