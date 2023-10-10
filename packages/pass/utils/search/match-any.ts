6682c2ac46fdbdf3517ff77321ae38092774ab03
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2023-10-10 11:50:08 +0200

    Match words in search query in any order
    [IDTEAM-2128]

--- packages/pass/utils/search/match-any.ts
+++ packages/pass/utils/search/match-any.ts
@@ -2,5 +2,5 @@ import { normalize } from '@proton/shared/lib/helpers/string';
 
 export const matchAny = (terms: string[]) => (match: string) => {
     const normalizedSearchTerm = normalize(match, true);
-    return terms.map((s) => normalize(s, true)).some((s) => s.includes(normalizedSearchTerm));
+    return normalizedSearchTerm.split(' ').every((word) => terms.some((s) => normalize(s, true).includes(word)));
 };
