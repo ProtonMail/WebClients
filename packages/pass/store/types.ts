949c5bb9aee35c14efbe188f66c95de8d6e78672
Edvin CANDON <edvin.candon@proton.ch>
2023-10-11 08:49:41 +0200

    Broadcast feature flags update message from saga when user features are updated

--- packages/pass/store/types.ts
+++ packages/pass/store/types.ts
@@ -4,7 +4,7 @@ import type { TelemetryEvent } from '../types/data/telemetry';
 import type { EncryptedExtensionCache } from '../types/worker/cache';
 import type * as actions from './actions';
 import type { Notification } from './actions/with-notification';
-import type { rootReducer } from './reducers';
+import type { FeatureFlagState, rootReducer } from './reducers';
 import type { ProxiedSettings } from './reducers/settings';
 
 export type State = ReturnType<typeof rootReducer>;
@@ -13,22 +13,21 @@ export type Action = ReturnType<(typeof actions)[keyof typeof actions]>;
 export interface WorkerRootSagaOptions {
     getAuth: () => AuthStore;
     getCache: () => Promise<Partial<EncryptedExtensionCache>>;
-    setCache: (encrypted: EncryptedExtensionCache) => Promise<void>;
     getEventInterval: () => number;
     getLocalSettings: () => Promise<ProxiedSettings>;
     getWorkerState: () => WorkerState;
-
     onBoot?: (result: { ok: true } | { ok: false; clearCache: boolean }) => void;
-    onItemsChange?: () => void;
+    onFeatureFlagsUpdate?: (features: FeatureFlagState) => void;
     onImportProgress?: (progress: number, endpoint?: ExtensionEndpoint) => void;
+    onItemsChange?: () => void;
     onNotification?: (notification: Notification) => void;
+    onSessionLockChange?: (sessionLockToken?: string, sessionLockTTL?: number) => Promise<void>;
     onSessionLocked?: () => Promise<void>;
     onSessionUnlocked?: (sessionLockToken: string) => Promise<void>;
-    onSessionLockChange?: (sessionLockToken?: string, sessionLockTTL?: number) => Promise<void>;
     onSettingUpdate?: (settings: ProxiedSettings) => Promise<void>;
     onShareEventDisabled?: (shareId: string) => void;
     onShareEventItemsDeleted?: (shareId: string, itemIds: string[]) => void;
     onSignout?: () => void;
-
+    setCache: (encrypted: EncryptedExtensionCache) => Promise<void>;
     telemetry?: (message: TelemetryEvent) => void;
 }
