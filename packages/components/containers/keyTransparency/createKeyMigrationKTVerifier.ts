6e1c6fead7f9f1cba231eccaaf9c59d0120443d6
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2023-10-11 11:43:43 +0200

    Allow an obsolescence proof when migrating an address

--- packages/components/containers/keyTransparency/createKeyMigrationKTVerifier.ts
+++ packages/components/containers/keyTransparency/createKeyMigrationKTVerifier.ts
@@ -5,18 +5,37 @@ import {
     ktSentryReport,
     ktSentryReportError,
     verifyProofOfAbsenceForAllRevision,
-} from '@proton/key-transparency/lib';
-import { Api, KeyMigrationKTVerifier, KeyTransparencyActivation } from '@proton/shared/lib/interfaces';
+    verifyProofOfObsolescenceLatest,
+} from "@proton/key-transparency/lib";
+import {
+    Api,
+    KeyMigrationKTVerifier,
+    KeyTransparencyActivation,
+    FetchedSignedKeyList,
+} from "@proton/shared/lib/interfaces";
 
 const createKeyMigrationKTVerifier = (ktActivation: KeyTransparencyActivation, api: Api): KeyMigrationKTVerifier => {
-    return async (email: string) => {
+    return async (email: string, signedKeyList: Partial<FetchedSignedKeyList> | null | undefined) => {
         if (ktActivation === KeyTransparencyActivation.DISABLED) {
             return;
         }
         try {
             const epoch = await fetchLatestEpoch(api);
-            const proof = await fetchProof(epoch.EpochID, email, 1, api);
-            await verifyProofOfAbsenceForAllRevision(proof, email, epoch.TreeHash);
+            if (signedKeyList?.ObsolescenceToken) {
+                const revisionToCheck = signedKeyList?.Revision ?? 1;
+                const obsolescenceProof = await fetchProof(epoch.EpochID, email, revisionToCheck, api);
+                const absenceProof = await fetchProof(epoch.EpochID, email, revisionToCheck + 1, api);
+                await verifyProofOfObsolescenceLatest(
+                    obsolescenceProof,
+                    absenceProof,
+                    email,
+                    epoch.TreeHash,
+                    signedKeyList
+                );
+            } else {
+                const absenceProof = await fetchProof(epoch.EpochID, email, 1, api);
+                await verifyProofOfAbsenceForAllRevision(absenceProof, email, epoch.TreeHash);
+            }
         } catch (error: any) {
             if (error instanceof KeyTransparencyError) {
                 ktSentryReport('KT error during key migration', { error: error.message });
