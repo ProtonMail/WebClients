fdeef9a1077157046d131a3d39869ebf4626c5ee
Edvin CANDON <edvin.candon@proton.ch>
2023-10-16 08:32:59 +0200

    Refactor user data request flow
    - Leverage max age in request meta data
    - Reflect changes in sagas and auth service

--- packages/pass/store/sagas/user/user-plan.ts
+++ packages/pass/store/sagas/user/user-plan.ts
@@ -1,25 +1,24 @@
-import { put, select, takeLeading } from 'redux-saga/effects';
+import { put, takeEvery } from 'redux-saga/effects';
 
 import { getUserPlan } from '@proton/pass/lib/user/user.requests';
-import { setUserPlan, wakeupSuccess } from '@proton/pass/store/actions';
-import type { UserPlanState } from '@proton/pass/store/reducers';
-import type { State, WorkerRootSagaOptions } from '@proton/pass/store/types';
+import { getUserPlanFailure, getUserPlanIntent, getUserPlanSuccess } from '@proton/pass/store/actions';
+import type { WorkerRootSagaOptions } from '@proton/pass/store/types';
+import type { PassPlanResponse } from '@proton/pass/types';
 import { SessionLockStatus } from '@proton/pass/types';
 
-/* Try to sync the user plan on each wakeup success */
-function* syncPlan({ getAuth }: WorkerRootSagaOptions) {
+function* syncPlan({ getAuth }: WorkerRootSagaOptions, { meta }: ReturnType<typeof getUserPlanIntent>) {
     try {
         const loggedIn = getAuth().hasSession();
         const locked = getAuth().getLockStatus() === SessionLockStatus.LOCKED;
+        if (!loggedIn || locked) throw new Error('Cannot fetch user plan');
 
-        if (loggedIn && !locked) {
-            const { user }: State = yield select();
-            const plan: UserPlanState = yield getUserPlan(user);
-            yield plan !== user.plan && put(setUserPlan(plan));
-        }
-    } catch (_) {}
+        const plan: PassPlanResponse = yield getUserPlan();
+        yield put(getUserPlanSuccess(meta.request.id, plan));
+    } catch (error) {
+        yield put(getUserPlanFailure(meta.request.id, error));
+    }
 }
 
 export default function* watcher(options: WorkerRootSagaOptions) {
-    yield takeLeading(wakeupSuccess.match, syncPlan, options);
+    yield takeEvery(getUserPlanIntent.match, syncPlan, options);
 }
