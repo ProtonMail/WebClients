a4b69f416ac12fc3d5b5a967785bb6cd9819fe65
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2023-10-17 17:13:28 +0200

    Add iconSize prop to VaultTag
    [IDTEAM-2164]

--- packages/pass/components/Vault/VaultTag.tsx
+++ packages/pass/components/Vault/VaultTag.tsx
@@ -1,6 +1,6 @@
 import type { VFC } from 'react';
 
-import type { IconName } from '@proton/components/components';
+import type { IconName, IconSize } from '@proton/components/components';
 import { Icon } from '@proton/components/components';
 import { VaultColor } from '@proton/pass/types/protobuf/vault-v1';
 import clsx from '@proton/utils/clsx';
@@ -15,9 +15,10 @@ type Props = {
     icon: IconName;
     color?: VaultColor;
     count?: number;
+    iconSize?: IconSize;
 };
 
-export const VaultTag: VFC<Props> = ({ title, shared = false, icon, color, count }) => {
+export const VaultTag: VFC<Props> = ({ title, shared = false, icon, color, count, iconSize = 12 }) => {
     return (
         <div
             className={clsx(
@@ -26,7 +27,7 @@ export const VaultTag: VFC<Props> = ({ title, shared = false, icon, color, count
             )}
             style={shared ? { '--vault-icon-color': VAULT_COLOR_MAP[color ?? VaultColor.COLOR1] } : undefined}
         >
-            {<Icon className="mr-1" name={icon} size={12} />}
+            {<Icon className="mr-1" name={icon} size={iconSize} />}
             <span className="text-ellipsis">{title}</span>
             {count !== undefined && count > 1 && <span className="flex-item-noshrink">• {count}</span>}
         </div>
