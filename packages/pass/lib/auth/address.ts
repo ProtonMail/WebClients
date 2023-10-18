81f5df944f71a3198a464617e481e7ee7788d3cb
Edvin CANDON <edvin.candon@proton.ch>
2023-10-18 15:06:22 +0200

    Allow inviting non-existing proton users

--- packages/pass/lib/auth/address.ts
+++ packages/pass/lib/auth/address.ts
@@ -1,6 +1,8 @@
 import { api } from '@proton/pass/lib/api/api';
 import type { Maybe, PublicKeysResponse } from '@proton/pass/types';
+import { getApiError } from '@proton/shared/lib/api/helpers/apiErrorHelper';
 import { getPublicKeys } from '@proton/shared/lib/api/keys';
+import { API_CUSTOM_ERROR_CODES } from '@proton/shared/lib/errors';
 
 export const getPublicKeysForEmail = async (email: string): Promise<string[]> => {
     const publicKeys = await api<PublicKeysResponse>(getPublicKeys({ Email: email }));
@@ -8,6 +10,12 @@ export const getPublicKeysForEmail = async (email: string): Promise<string[]> =>
 };
 
 export const getPrimaryPublicKeyForEmail = async (email: string): Promise<Maybe<string>> => {
-    const publicKeys = await getPublicKeysForEmail(email);
-    return publicKeys?.[0];
+    try {
+        const publicKeys = await getPublicKeysForEmail(email);
+        return publicKeys?.[0];
+    } catch (err) {
+        const { code } = getApiError(err);
+        if (code === API_CUSTOM_ERROR_CODES.KEY_GET_ADDRESS_MISSING) return undefined;
+        throw err;
+    }
 };
