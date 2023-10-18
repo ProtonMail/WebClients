cfcb3cfba5252e9f3121fe569bbe93d500b124c5
Edvin CANDON <edvin.candon@proton.ch>
2023-10-18 07:15:51 +0200

    Improve boot saga user state hydration
    - Dispatch appropriate actions in order to set request max-age for user access & features
    - Move user state resolution to boot saga
    
    [IDTEAM-2142]

--- packages/pass/lib/user/user.requests.ts
+++ packages/pass/lib/user/user.requests.ts
@@ -1,24 +1,10 @@
 import { api } from '@proton/pass/lib/api/api';
-import type {
-    AddressState,
-    FeatureFlagState,
-    UserAccessState,
-    UserSettingsState,
-    UserState,
-} from '@proton/pass/store/reducers';
-import { selectUserState } from '@proton/pass/store/selectors';
-import type { State } from '@proton/pass/store/types';
-import type { RequiredNonNull } from '@proton/pass/types';
+import type { FeatureFlagState, SafeUserAccessState, UserSettingsState } from '@proton/pass/store/reducers';
 import type { FeatureFlagsResponse } from '@proton/pass/types/api/features';
 import { PassFeaturesValues } from '@proton/pass/types/api/features';
 import { logger } from '@proton/pass/utils/logger';
-import { getAllAddresses } from '@proton/shared/lib/api/addresses';
-import { getLatestID } from '@proton/shared/lib/api/events';
 import { getSettings } from '@proton/shared/lib/api/settings';
-import { getUser } from '@proton/shared/lib/api/user';
-import { toMap } from '@proton/shared/lib/helpers/object';
 import type { UserSettings } from '@proton/shared/lib/interfaces';
-import type { User } from '@proton/shared/lib/interfaces/User';
 
 export const getFeatureFlags = async (): Promise<FeatureFlagState> => {
     logger.info(`[Saga::UserFeatures] syncing user feature flags`);
@@ -30,7 +16,7 @@ export const getFeatureFlags = async (): Promise<FeatureFlagState> => {
     }, {});
 };
 
-export const getUserAccess = async (): Promise<RequiredNonNull<UserAccessState>> => {
+export const getUserAccess = async (): Promise<SafeUserAccessState> => {
     logger.info(`[Saga::UserPlan] syncing user access`);
     const { Access } = await api({ url: 'pass/v1/user/access', method: 'get' });
     return { plan: Access!.Plan, waitingNewUserInvites: Access!.WaitingNewUserInvites };
@@ -47,25 +33,4 @@ export const getUserSettings = async (): Promise<UserSettingsState> => {
     } catch {
         return {};
     }
-};
-
-export const getUserData = async (state: State): Promise<RequiredNonNull<UserState>> => {
-    const cached = selectUserState(state);
-
-    const [user, eventId, features, userSettings, addresses, access] = await Promise.all([
-        cached.user ?? api<{ User: User }>(getUser()).then(({ User }) => User),
-        cached.eventId ?? api<{ EventID: string }>(getLatestID()).then(({ EventID }) => EventID),
-        cached.features ?? (await getFeatureFlags()),
-        cached.userSettings ?? (await getUserSettings()) ?? {},
-
-        Object.keys(cached.addresses).length > 0
-            ? cached.addresses
-            : getAllAddresses(api).then((addresses): AddressState => toMap(addresses, 'ID')),
-
-        cached.plan && cached.waitingNewUserInvites
-            ? { plan: cached.plan, waitingNewUserInvites: cached.waitingNewUserInvites }
-            : await getUserAccess(),
-    ]);
-
-    return { user, ...access, addresses, eventId, features, userSettings };
 };
