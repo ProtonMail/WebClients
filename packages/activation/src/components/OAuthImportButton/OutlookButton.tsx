9152e93d77355e9a91915613f1c07c89c354e451
Flavien Bonvin <flavien.bonvin@proton.ch>
2023-10-18 19:28:31 +0200

    Update import CSV modal and add Outlook button in settings
    IA-900

---
+++ packages/activation/src/components/OAuthImportButton/OutlookButton.tsx
@@ -0,0 +1,33 @@
+import { c } from 'ttag';
+
+import { Button } from '@proton/atoms';
+import outlookLogo from '@proton/styles/assets/img/import/providers/outlook.svg';
+import clsx from '@proton/utils/clsx';
+
+interface Props {
+    className?: string;
+    disabled?: boolean;
+    label?: string;
+    onClick: () => void;
+}
+
+const OutlookButton = ({ className, disabled, onClick, label }: Props) => {
+    return (
+        <Button
+            className={clsx(['inline-flex flex-justify-center', className])}
+            disabled={disabled}
+            onClick={onClick}
+            data-testid="OAuthImportButton:button:outlook"
+        >
+            <img
+                alt=""
+                src={outlookLogo}
+                className="mr-2 w-custom flex-align-self-center"
+                style={{ '--w-custom': '18px' }}
+            />
+            {label ?? c('Action').t`Continue with Outlook`}
+        </Button>
+    );
+};
+
+export default OutlookButton;
