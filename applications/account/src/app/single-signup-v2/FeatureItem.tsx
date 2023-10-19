2527c9bed59108895bd0c4759cf368b901419fdb
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-10-19 17:19:51 +0200

    Add support for mail signup in pass
    Co-authored-by: Nicolas Hoffmann <dante3333@gmail.com>

--- applications/account/src/app/single-signup-v2/FeatureItem.tsx
+++ applications/account/src/app/single-signup-v2/FeatureItem.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 interface Props {
     left: ReactNode;
-    text: string;
+    text: ReactNode;
 }
 
 const FeatureItem = ({ left, text }: Props) => {
---
+++ applications/account/src/app/single-signup-v2/mail/CustomStep.tsx
@@ -0,0 +1,109 @@
+import { useRef, useState } from 'react';
+
+import { useErrorHandler, useMyCountry } from '@proton/components/hooks';
+import { getLocalPart } from '@proton/shared/lib/keys';
+
+import CongratulationsStep from '../../signup/CongratulationsStep';
+import RecoveryStep from '../../signup/RecoveryStep';
+import { getPlanFromPlanIDs } from '../../signup/helper';
+import { SignupCacheResult, SignupType } from '../../signup/interfaces';
+import { handleDisplayName, handleSaveRecovery } from '../../signup/signupActions';
+import { useFlowRef } from '../../useFlowRef';
+import Layout from '../Layout';
+import { SignupCustomStepProps } from '../interface';
+
+enum Step {
+    Congratulations,
+    SaveRecovery,
+}
+
+const CustomStep = ({ model, onSetup, theme, logo }: SignupCustomStepProps) => {
+    const [step, setStep] = useState(Step.Congratulations);
+    const createFlow = useFlowRef();
+
+    if (model.cache?.type !== 'signup') {
+        throw new Error('Unknown type');
+    }
+
+    const cacheRef = useRef<SignupCacheResult>(model.cache);
+    const cache = cacheRef.current!;
+    const accountData = cache.accountData;
+    const [defaultCountry] = useMyCountry();
+    const handleError = useErrorHandler();
+    const verificationModel = cache.humanVerificationResult?.verificationModel;
+
+    const plan = getPlanFromPlanIDs(model.plansMap, model.subscriptionData.planIDs);
+    const planName = plan?.Title;
+
+    return (
+        <Layout theme={theme} logo={logo} hasDecoration={false}>
+            {step === Step.Congratulations && (
+                <CongratulationsStep
+                    defaultName={
+                        accountData.username ||
+                        (accountData?.signupType === SignupType.Email && getLocalPart(accountData.email)) ||
+                        ''
+                    }
+                    planName={planName}
+                    onSubmit={async ({ displayName }) => {
+                        const validateFlow = createFlow();
+                        try {
+                            if (!cache || cache.type !== 'signup') {
+                                throw new Error('Missing cache');
+                            }
+                            const signupActionResponse = await handleDisplayName({
+                                displayName,
+                                cache,
+                            });
+
+                            if (validateFlow()) {
+                                cacheRef.current = signupActionResponse.cache;
+                                setStep(Step.SaveRecovery);
+                            }
+                        } catch (error) {
+                            handleError(error);
+                        } finally {
+                            createFlow.reset();
+                        }
+                    }}
+                />
+            )}
+            {step === Step.SaveRecovery && (
+                <RecoveryStep
+                    onBack={() => setStep(Step.Congratulations)}
+                    defaultCountry={defaultCountry}
+                    defaultEmail={
+                        (verificationModel?.method === 'email' && verificationModel?.value) ||
+                        (accountData?.signupType === SignupType.Email && accountData.email) ||
+                        ''
+                    }
+                    defaultPhone={verificationModel?.method === 'sms' ? verificationModel?.value : ''}
+                    onSubmit={async ({ recoveryEmail, recoveryPhone }) => {
+                        const validateFlow = createFlow();
+                        try {
+                            if (!cache || cache.type !== 'signup') {
+                                throw new Error('Missing cache');
+                            }
+                            const signupActionResponse = await handleSaveRecovery({
+                                cache,
+                                recoveryEmail,
+                                recoveryPhone,
+                            });
+
+                            if (validateFlow()) {
+                                cacheRef.current = signupActionResponse.cache;
+                                await onSetup(signupActionResponse.cache);
+                            }
+                        } catch (error) {
+                            handleError(error);
+                        } finally {
+                            createFlow.reset();
+                        }
+                    }}
+                />
+            )}
+        </Layout>
+    );
+};
+
+export default CustomStep;
