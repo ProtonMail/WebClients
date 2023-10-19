534ae392f0291a17820aa3b65e636a55e71ccc91
Dominik Jankovic <dominik.jankovic@proton.ch>
2023-10-19 14:28:16 +0200

    Add Roboform reader
    [IDTEAM-1951]

---
+++ packages/pass/lib/import/providers/roboform.reader.ts
@@ -0,0 +1,95 @@
+import type { Maybe } from '@proton/pass/types';
+import { logger } from '@proton/pass/utils/logger';
+import groupWith from '@proton/utils/groupWith';
+import lastItem from '@proton/utils/lastItem';
+
+import { readCSV } from '../helpers/csv.reader';
+import { ImportProviderError } from '../helpers/error';
+import { getImportedVaultName, importLoginItem, importNoteItem } from '../helpers/transformers';
+import type { ImportPayload, ImportVault } from '../types';
+import type { RoboformItem, RoboformVariadicItem } from './roboform.types';
+
+function formatOtpAuthUri(item: RoboformItem): Maybe<string> {
+    const totpDefinition = item.RfFieldsV2?.find((e) => e.startsWith('TOTP KEY$'));
+    if (!totpDefinition) return;
+    const secret = lastItem(totpDefinition.split(','));
+    return `otpauth://totp/${item.Name}:none?secret=${secret}`;
+}
+
+export const readRoboformData = async (data: string): Promise<ImportPayload> => {
+    const ignored: string[] = [];
+    const warnings: string[] = [];
+
+    try {
+        const result = await readCSV<RoboformVariadicItem>({
+            data,
+            onError: (error) => warnings.push(error),
+        });
+
+        /*
+         * Skips the first row (headers) and maps results to an array of RoboformItem's.
+         *
+         * In Roboform exports, fields beginning with `@` or `'` have a single quote (`'`) prepended.
+         * To account for this, occurences of `'@` and `''` are replaced with their actual values,
+         * ie. `@` and `'` respectively.
+         */
+        const items: RoboformItem[] = result.items
+            .slice(1)
+            .map(([Name, Url, MatchUrl, Login, Pwd, Note, Folder, ...RfFieldsV2]) => ({
+                Name,
+                Url,
+                MatchUrl,
+                Login: Login.replace(/^'@/, '').replace(/^''/, ''),
+                Pwd: Pwd.replace(/^'@/, '').replace(/^''/, ''),
+                Note,
+                Folder: lastItem(Folder.split('/')),
+                RfFieldsV2,
+            }));
+
+        const groupedByVault = groupWith<RoboformItem>((a, b) => a.Folder === b.Folder, items);
+
+        const vaults: ImportVault[] = groupedByVault.map((roboformItems) => {
+            const vaultName = roboformItems[0].Folder;
+
+            const items = roboformItems.map((item) => {
+                /* Bookmark */
+                if (item.Url && !item.MatchUrl && !item.Login && !item.Pwd) {
+                    return importLoginItem({
+                        name: item.Name,
+                        urls: [item.Url],
+                        note: item.Note,
+                    });
+                }
+
+                /* Note */
+                if (!item.Url && !item.MatchUrl && !item.Login && !item.Pwd) {
+                    return importNoteItem({
+                        name: item.Name,
+                        note: item.Note,
+                    });
+                }
+
+                /* Login */
+                return importLoginItem({
+                    name: item.Name,
+                    urls: [item.MatchUrl],
+                    note: item.Note,
+                    username: item.Login,
+                    password: item.Pwd,
+                    totp: formatOtpAuthUri(item),
+                });
+            });
+
+            return {
+                shareId: null,
+                name: getImportedVaultName(vaultName),
+                items,
+            };
+        });
+
+        return { vaults, ignored, warnings };
+    } catch (e) {
+        logger.warn('[Importer::Roboform]', e);
+        throw new ImportProviderError('Roboform', e);
+    }
+};
