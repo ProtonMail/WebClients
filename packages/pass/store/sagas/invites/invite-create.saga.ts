ec023380241c1f21b62ba63373902170462bc29d
Edvin CANDON <edvin.candon@proton.ch>
2023-10-20 00:10:29 +0200

    Handle vault creation and item move flow in invite creation saga
    [IDTEAN-2164]

--- packages/pass/store/sagas/invites/invite-create.saga.ts
+++ packages/pass/store/sagas/invites/invite-create.saga.ts
@@ -2,9 +2,11 @@ import { put, select, takeEvery } from 'redux-saga/effects';
 
 import { getPrimaryPublicKeyForEmail } from '@proton/pass/lib/auth/address';
 import { createInvite, createNewUserInvite } from '@proton/pass/lib/invites/invite.requests';
+import { moveItem } from '@proton/pass/lib/items/item.requests';
+import { createVault } from '@proton/pass/lib/vaults/vault.requests';
 import { inviteCreationFailure, inviteCreationIntent, inviteCreationSuccess } from '@proton/pass/store/actions';
-import { selectFeatureFlag } from '@proton/pass/store/selectors';
-import { type Maybe } from '@proton/pass/types';
+import { selectFeatureFlag, selectItemByShareIdAndId } from '@proton/pass/store/selectors';
+import type { ItemRevision, Maybe, Share, ShareType } from '@proton/pass/types';
 import { PassFeature } from '@proton/pass/types/api/features';
 
 /* Depending on the result of the public key for the invited email,
@@ -13,11 +15,37 @@ function* createInviteWorker({ payload, meta: { request } }: ReturnType<typeof i
     try {
         const allowNewUser: boolean = yield select(selectFeatureFlag(PassFeature.PassSharingNewUsers));
         const invitedPublicKey: Maybe<string> = yield getPrimaryPublicKeyForEmail(payload.email);
-
         if (!invitedPublicKey && !allowNewUser) throw new Error();
 
+        if (payload.withVaultCreation) {
+            const { name, description, icon, color, item, ...inviteCreate } = payload;
+            const itemToMove: Maybe<ItemRevision> = yield select(selectItemByShareIdAndId(item.shareId, item.itemId));
+            if (itemToMove === undefined) throw new Error();
+
+            const vaultContent = { name, description, display: { icon, color } };
+
+            const createdShare: Share<ShareType.Vault> = yield createVault({ content: vaultContent });
+            const movedItem: ItemRevision = yield moveItem(itemToMove, item.shareId, createdShare.shareId);
+
+            yield invitedPublicKey
+                ? createInvite({ ...inviteCreate, shareId: createdShare.shareId, invitedPublicKey })
+                : createNewUserInvite({ ...inviteCreate, shareId: createdShare.shareId });
+
+            yield put(
+                inviteCreationSuccess(request.id, {
+                    item,
+                    movedItem,
+                    share: createdShare,
+                    shareId: createdShare.shareId,
+                    withVaultCreation: true,
+                })
+            );
+
+            return;
+        }
+
         yield invitedPublicKey ? createInvite({ ...payload, invitedPublicKey }) : createNewUserInvite(payload);
-        yield put(inviteCreationSuccess(request.id, payload.shareId));
+        yield put(inviteCreationSuccess(request.id, { withVaultCreation: false, shareId: payload.shareId }));
     } catch (err) {
         yield put(inviteCreationFailure(request.id, err));
     }
--- packages/pass/types/forms/invite.ts
+++ packages/pass/types/forms/invite.ts
@@ -1,10 +1,10 @@
-import type { ShareRole, VaultFormValues } from '@proton/pass/types';
+import type { ShareRole, UniqueItem, VaultFormValues } from '@proton/pass/types';
 
 export type InviteFormStep = 'email' | 'vault' | 'permissions';
 
 export type InviteFormValues<T extends boolean = boolean> = Extract<
     { email: string; role: ShareRole; step: InviteFormStep } & (
-        | ({ withVaultCreation: true } & VaultFormValues)
+        | ({ withVaultCreation: true; item: UniqueItem } & VaultFormValues)
         | { withVaultCreation: false; shareId: string }
     ),
     { withVaultCreation: T }
