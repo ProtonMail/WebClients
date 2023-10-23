abf470709403167afdb757ff668dacef441f6377
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2023-10-23 12:02:05 +0200

    Use another proxy client

--- packages/components/containers/unleash/index.ts
+++ packages/components/containers/unleash/index.ts
@@ -1,11 +1,4 @@
-import { useFlag as useUnleashFlag } from '@unleash/proxy-client-react';
-
-import type { FeatureFlag } from '@proton/components/containers';
-
 export * from './UnleashContext';
 export { default as UnleashFlagProvider } from './UnleashFlagProvider';
 export { default as useFlagsReady } from './useFlagsReady';
-
-const useFlag: (name: FeatureFlag) => boolean = useUnleashFlag;
-
-export { useFlag };
+export { default as useFlag } from './useFlag';
