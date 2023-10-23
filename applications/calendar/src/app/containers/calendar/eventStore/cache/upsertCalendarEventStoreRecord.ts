3b5445ff746da71e3a3761c97d23e553e7929d6b
Eduardo Conde Pena <econdepe@proton.ch>
2023-10-23 23:37:30 +0200

    Handle attendee UpdateTime properly with event manager
    Because attendee UpdateTime is updated asynchronously in the BE, it's possible to receive model events with
    higher ModifyTime but lower attendee UpdateTime. We should not update attendees in that case.
    
    CALWEB-4668

--- applications/calendar/src/app/containers/calendar/eventStore/cache/upsertCalendarEventStoreRecord.ts
+++ applications/calendar/src/app/containers/calendar/eventStore/cache/upsertCalendarEventStoreRecord.ts
@@ -1,17 +1,101 @@
-import { getIsRecurring, getRecurrenceIdDate, getUidValue} from '@proton/shared/lib/calendar/veventHelper';
-import { CalendarEvent, CalendarEventSharedData } from '@proton/shared/lib/interfaces/calendar';
-import {getViewEventDateProperties} from '../../eventHelper';
+import { getIsRecurring, getRecurrenceIdDate, getUidValue } from '@proton/shared/lib/calendar/veventHelper';
+import { RequireSome, SimpleMap } from '@proton/shared/lib/interfaces';
+import { Attendee, CalendarEvent, CalendarEventSharedData } from '@proton/shared/lib/interfaces/calendar';
 
+import { getViewEventDateProperties } from '../../eventHelper';
 import { CalendarEventStoreRecord, CalendarEventsCache, SharedVcalVeventComponent } from '../interface';
 import { getIsCalendarEvent } from './helper';
 import { setEventInRecurrenceInstances, setEventInRecurringCache } from './recurringCache';
 import upsertCalendarEventInTree from './upsertCalendarEventInTree';
 
+export const getHasUpdatedAttendees = (
+    newEventData: CalendarEvent | CalendarEventSharedData,
+    oldEventData?: CalendarEvent | CalendarEventSharedData
+) => {
+    if (!oldEventData || !getIsCalendarEvent(oldEventData)) {
+        if (!getIsCalendarEvent(newEventData)) {
+            return {
+                hasUpdatedAttendees: false,
+            };
+        }
+        return {
+            attendees: newEventData.Attendees,
+            hasUpdatedAttendees: !!newEventData.Attendees.length,
+        };
+    }
+
+    if (!getIsCalendarEvent(newEventData)) {
+        return {
+            hasUpdatedAttendees: false,
+        };
+    }
+
+    let hasUpdatedAttendees = oldEventData.Attendees.length !== newEventData.Attendees.length;
+    const attendeeMap = oldEventData.Attendees.reduce<SimpleMap<Attendee>>((acc, attendee) => {
+        acc[attendee.Token] = attendee;
+        return acc;
+    }, {});
+
+    const attendees = newEventData.Attendees.map((attendee) => {
+        const oldAttendee = attendeeMap[attendee.Token];
+        const oldUpdateTime = oldAttendee?.UpdateTime || 0;
+        if (!oldUpdateTime) {
+            hasUpdatedAttendees = true;
+            return attendee;
+        }
+        const newUpdateTime = attendee.UpdateTime || 0;
+        if (oldUpdateTime >= newUpdateTime) {
+            // we should return old attendee data
+            return oldAttendee;
+        }
+        hasUpdatedAttendees = true;
+        return attendee;
+    });
+
+    return { attendees, hasUpdatedAttendees };
+};
+
+/**
+ * Different event parts are updated asynchronously in the BE^**. Ideally we would have a ModifyTime per event part.
+ * Since we don't atm, we rely on the global ModifyTime plus attendee update times.
+ *
+ * ** E.g.: a Proton organizer makes a modification to shared event data (shared with the attendee)
+ * but the attendee makes a change to e.g. notifications before receiving the notification of the organizer change
+ */
+export const getHasUpdatedEventData = (
+    newEventData: CalendarEvent | CalendarEventSharedData,
+    oldEventData?: CalendarEvent | CalendarEventSharedData
+) => {
+    const isOldEventDataFull = oldEventData && getIsCalendarEvent(oldEventData);
+    const isNewEventFull = getIsCalendarEvent(newEventData);
+    const oldModifyTime = oldEventData?.ModifyTime || 0;
+    const newModifyTime = newEventData.ModifyTime;
+
+    const isNewEventLastEditTime = newModifyTime > oldModifyTime ? true : !isOldEventDataFull && isNewEventFull;
+    const isNewEvent = !oldEventData;
+
+    const hasUpdatedEventDataGlobal = isNewEvent || isNewEventLastEditTime;
+    const { hasUpdatedAttendees, attendees } = getHasUpdatedAttendees(newEventData, oldEventData);
+
+    const eventData: CalendarEvent | CalendarEventSharedData | undefined = hasUpdatedEventDataGlobal
+        ? newEventData
+        : oldEventData;
+
+    if (attendees && eventData && 'Attendees' in eventData) {
+        eventData.Attendees = attendees;
+    }
+
+    return {
+        hasUpdatedEventData: hasUpdatedEventDataGlobal || hasUpdatedAttendees,
+        eventData,
+    };
+};
+
 export const getCalendarEventStoreRecord = (
     eventComponent: SharedVcalVeventComponent,
     eventData: CalendarEvent | CalendarEventSharedData
-): CalendarEventStoreRecord => {
-    const { utcStart, utcEnd, isAllDay, isAllPartDay} = getViewEventDateProperties(eventComponent)
+): RequireSome<CalendarEventStoreRecord, 'eventData'> => {
+    const { utcStart, utcEnd, isAllDay, isAllPartDay } = getViewEventDateProperties(eventComponent);
 
     return {
         utcStart,
@@ -103,35 +187,23 @@ const upsertCalendarEventStoreRecordHelper = (
 
 export const upsertCalendarEventStoreRecord = (
     eventID: string,
-    calendarEventStoreRecord: CalendarEventStoreRecord,
+    calendarEventStoreRecord: RequireSome<CalendarEventStoreRecord, 'eventData'>,
     calendarEventsCache: CalendarEventsCache
 ) => {
     const oldEventRecord = calendarEventsCache.events.get(eventID);
-
     const oldEventData = oldEventRecord?.eventData;
-    const newEventData = calendarEventStoreRecord?.eventData;
-    const isOldEventDataFull = oldEventData && getIsCalendarEvent(oldEventData);
+    const newEventData = calendarEventStoreRecord.eventData;
+    const { hasUpdatedEventData, eventData } = getHasUpdatedEventData(newEventData, oldEventData);
 
-    const oldModifyTime = oldEventData?.ModifyTime || 0;
-    const newModifyTime = newEventData?.ModifyTime || 0;
-
-    /**
-     * There are instances of linked events where the API may inform us of a change with
-     * a ModifyTime such that newModifyTime < oldModifyTime . We still need to update in that case.
-     * This can happen if an organizer makes a modification to shared event data (shared with the attendee)
-     * but the attendee made a change to e.g. notifications before receiving the notification of the organizer change.
-     * Both modifications happen asynchronously on the BE and they need not come in order.
-     * This will be improved later by BE with a ModifyTime per calendar part
-     */
-    const isNewEventLastEditTime = newModifyTime !== oldModifyTime ? true : !isOldEventDataFull;
-
-    const isNewEvent = !oldEventRecord || !oldEventData;
-    const shouldUpsert = isNewEvent || isNewEventLastEditTime;
-
-    if (!shouldUpsert) {
+    if (!hasUpdatedEventData) {
         return false;
     }
 
-    upsertCalendarEventStoreRecordHelper(eventID, calendarEventStoreRecord, calendarEventsCache);
+    const newCalendarEventStoreRecord: CalendarEventStoreRecord = {
+        ...calendarEventStoreRecord,
+        eventData,
+    };
+
+    upsertCalendarEventStoreRecordHelper(eventID, newCalendarEventStoreRecord, calendarEventsCache);
     return true;
 };
