82306ccbbd1bc3e84eab730c82bb729eb4d3b7b5
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2023-10-25 16:56:02 +0200

    Create usePassConfig hook to avoid type casting
    [IDTEAM-2174]

--- packages/pass/hooks/useNavigateToUpgrade.ts
+++ packages/pass/hooks/useNavigateToUpgrade.ts
@@ -1,11 +1,10 @@
 import { useCallback } from 'react';
 
-import { useConfig } from '@proton/components/hooks';
+import usePassConfig from '@proton/pass/hooks/usePassConfig';
 import browser from '@proton/pass/lib/globals/browser';
-import type { ProtonConfig } from '@proton/shared/lib/interfaces';
 import noop from '@proton/utils/noop';
 
 export const useNavigateToUpgrade = () => {
-    const { SSO_URL } = useConfig() as ProtonConfig & { SSO_URL: string };
+    const { SSO_URL } = usePassConfig();
     return useCallback(() => browser.tabs.create({ url: `${SSO_URL}/pass/upgrade` }).catch(noop), []);
 };
