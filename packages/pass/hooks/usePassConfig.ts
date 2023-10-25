677e63b91ef083cb15c45dfb2f28e7d8756cdc39
Edvin CANDON <edvin.candon@proton.ch>
2023-10-25 18:54:32 +0200

    Fine-tune BF2023 spotlight message

--- packages/pass/hooks/usePassConfig.ts
+++ packages/pass/hooks/usePassConfig.ts
@@ -3,10 +3,5 @@ import { useContext } from 'react';
 import ConfigContext from '@proton/components/containers/config/configContext';
 import type { ProtonConfig } from '@proton/shared/lib/interfaces';
 
-type PassConfig = ProtonConfig & { SSO_URL: string };
-
-const usePassConfig = () => {
-    return useContext(ConfigContext) as PassConfig;
-};
-
-export default usePassConfig;
+export type PassConfig = ProtonConfig & { SSO_URL: string };
+export const usePassConfig = () => useContext(ConfigContext) as PassConfig;
