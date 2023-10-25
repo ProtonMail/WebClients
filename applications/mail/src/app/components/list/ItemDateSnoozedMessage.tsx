2914db7a66854cd4c68234a1f386a164cd8c284d
Flavien Bonvin <flavien.bonvin@proton.ch>
2023-10-25 09:49:16 +0200

    Unify ItemDate rendering

--- applications/mail/src/app/components/list/ItemDateSnoozedMessage.tsx
+++ applications/mail/src/app/components/list/ItemDateSnoozedMessage.tsx
@@ -3,7 +3,7 @@ import { useMemo } from 'react';
 import { format, isToday, isTomorrow } from 'date-fns';
 import { c } from 'ttag';
 
-import { Icon, Tooltip } from '@proton/components';
+import { Icon } from '@proton/components';
 import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
 import { dateLocale } from '@proton/shared/lib/i18n';
 import clsx from '@proton/utils/clsx';
@@ -11,6 +11,8 @@ import clsx from '@proton/utils/clsx';
 import { formatFullDate, formatScheduledTimeString } from 'proton-mail/helpers/date';
 import { isElementReminded } from 'proton-mail/logic/snoozehelpers';
 import { Element } from 'proton-mail/models/element';
+
+import ItemDateRender from './ItemDateRender';
 
 interface Props {
     snoozeTime: number;
@@ -37,30 +39,15 @@ const SnoozedDate = ({ snoozeTime, className, useTooltip = false }: Props) => {
 
     const fullDate = formatFullDate(snoozeDate);
 
-    const itemDate = (
-        <>
-            <time
-                dateTime={fullDate}
-                className={clsx(className, 'color-warning')}
-                title={useTooltip ? undefined : fullDate}
-                aria-hidden="true"
-                data-testid="item-date-snoozed"
-            >
-                {snoozeShortDate}
-            </time>
-            <span className="sr-only">{fullDate}</span>
-        </>
+    return (
+        <ItemDateRender
+            className={clsx(className, 'color-warning')}
+            useTooltip={useTooltip}
+            fullDate={fullDate}
+            formattedDate={snoozeShortDate}
+            dataTestId="item-date-snoozed"
+        />
     );
-
-    if (useTooltip) {
-        return (
-            <Tooltip title={fullDate}>
-                <span>{itemDate}</span>
-            </Tooltip>
-        );
-    }
-
-    return <>{itemDate}</>;
 };
 
 const RemindedConversation = ({ className }: { className?: string }) => {
