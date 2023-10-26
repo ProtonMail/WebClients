394530dee828ad75b6329056c08bc46f7582b4f2
Nicolas Hoffmann <dante3333@gmail.com>
2023-10-26 17:57:49 +0200

    Replacement of old box-shadow helpers to new ones
    We need to replace helper by their new responsive syntax
    
    - just replaced box-shadow helpers by new syntax
    
    UXE-621

--- applications/account/src/app/single-signup/Box.tsx
+++ applications/account/src/app/single-signup/Box.tsx
@@ -8,7 +8,7 @@ const Box = ({ children, className }: { children: ReactNode; className?: string 
     return (
         <div
             className={clsx(
-                'pricing-box ui-standard relative shadow-lifted shadow-color-primary on-tiny-mobile-no-box-shadow rounded-xl',
+                'pricing-box ui-standard relative sm:shadow-lifted shadow-color-primary rounded-xl',
                 className
             )}
         >
