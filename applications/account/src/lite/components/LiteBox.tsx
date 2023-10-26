be2a2630d023605cfc1591bd6eaa725c3d6e89bc
Nicolas Hoffmann <dante3333@gmail.com>
2023-10-26 17:57:49 +0200

    Replacement of old box-shadow helpers to new ones
    We need to replace helper by their new responsive syntax
    
    - just replaced box-shadow helpers by new syntax
    
    UXE-621

--- applications/account/src/lite/components/LiteBox.tsx
+++ applications/account/src/lite/components/LiteBox.tsx
@@ -8,7 +8,7 @@ interface Props {
 const LiteBox = ({ children, maxWidth = 52 }: Props) => {
     return (
         <div
-            className="m-0 sm:m-4 p-4 sm:p-8 rounded shadow-lifted on-tiny-mobile-no-box-shadow bg-norm w-full max-w-custom"
+            className="m-0 sm:m-4 p-4 sm:p-8 rounded sm:shadow-lifted bg-norm w-full max-w-custom"
             style={{ '--max-w-custom': `${maxWidth}rem` }}
         >
             {children}
