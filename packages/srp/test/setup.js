022989c139e4ce0bfc4d8e825677bf166091cea8
msvanstr <mattias.svanstrom@proton.ch>
2023-10-27 00:23:45 +0200

    Fix srp test

--- packages/srp/test/setup.js
+++ packages/srp/test/setup.js
@@ -3,9 +3,6 @@ import { after, before } from 'mocha';
 import { CryptoProxy } from '@proton/crypto';
 import { Api as CryptoApi } from '@proton/crypto/lib/worker/api';
 
-// JSDom does not include webcrypto
-global.crypto = require('crypto').webcrypto;
-
 before(async () => {
     await CryptoProxy.setEndpoint(new CryptoApi(), (endpoint) => endpoint.clearKeyStore());
 });
