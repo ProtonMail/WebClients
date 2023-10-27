a3b2d8db8101cf484b7416c0e46f59616d5994ac
Pierre Bondoerffer <pierre.bondoerffer@proton.ch>
2023-10-27 13:56:52 +0200

    Use new sizing helpers

--- applications/drive/src/app/components/sections/Photos/PhotosEmptyView.tsx
+++ applications/drive/src/app/components/sections/Photos/PhotosEmptyView.tsx
@@ -13,14 +13,14 @@ type Props = {};
 
 export const PhotosEmptyView: FC<Props> = () => {
     return (
-        <div role="presentation" className="flex w100 flex-item-fluid">
+        <div role="presentation" className="flex w-full flex-item-fluid">
             <EmptyViewContainer
                 imageProps={{ src: emptyPhotosSvg, title: c('Info').t`No photos yet` }}
                 data-testid="photos-empty-placeholder"
             >
                 <div className="flex flex-column flex-align-items-center flex-justify-center">
                     <h3 className="text-bold">{c('Info').t`No photos yet`}</h3>
-                    <p className="text-center w24e max-w100">
+                    <p className="text-center w-custom max-w-full" style={{ '--w-custom': '24em' }}>
                         {c('Info').t`As soon as you have photos or videos on your device, they will appear here.`}
                     </p>
                 </div>
