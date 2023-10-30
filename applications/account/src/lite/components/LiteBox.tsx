ebc299b68ebf771db176eba596503af01d1c69ce
Jack Crompton <jack.crompton@proton.ch>
2023-10-30 14:46:06 +0100

    Update mobile stylings for subscribe account

--- applications/account/src/lite/components/LiteBox.tsx
+++ applications/account/src/lite/components/LiteBox.tsx
@@ -12,7 +12,7 @@ const LiteBox = ({ center = true, children, maxWidth = 52 }: Props) => {
     return (
         <div className={clsx('flex flex-justify-center', center ? 'flex-align-items-center h-full' : '')}>
             <div
-                className="m-0 sm:m-2 p-4 sm:p-8 rounded shadow-lifted on-tiny-mobile-no-box-shadow bg-norm w-full max-w-custom"
+                className="m-0 sm:m-4 p-4 sm:p-8 rounded shadow-lifted on-tiny-mobile-no-box-shadow bg-norm w-full max-w-custom"
                 style={{ '--max-w-custom': `${maxWidth}rem` }}
             >
                 {children}
