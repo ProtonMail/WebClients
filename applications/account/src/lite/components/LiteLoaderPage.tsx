27b6b95ec867df9995a58a72113faebfd1379909
Jack Crompton <jack.crompton@proton.ch>
2023-10-30 16:22:38 +0100

    Add sticky footer to lite account subscriptions

--- applications/account/src/lite/components/LiteLoaderPage.tsx
+++ applications/account/src/lite/components/LiteLoaderPage.tsx
@@ -2,7 +2,7 @@ import { CircleLoader } from '@proton/atoms';
 
 const LiteLoaderPage = () => {
     return (
-        <div className="flex flex-justify-center flex-align-items-center h100">
+        <div className="flex flex-justify-center flex-align-items-center h-full">
             <CircleLoader className="color-primary" size="large" />
         </div>
     );
