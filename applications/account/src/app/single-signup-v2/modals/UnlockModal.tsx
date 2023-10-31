e6251624c35fef69fd06347689f0f2476e7b7ed7
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-10-31 18:04:41 +0100

    Improve unlock modal

--- applications/account/src/app/single-signup-v2/modals/UnlockModal.tsx
+++ applications/account/src/app/single-signup-v2/modals/UnlockModal.tsx
@@ -6,8 +6,11 @@ import { Button } from '@proton/atoms/Button';
 import { ModalProps, ModalTwo, ModalTwoContent, ModalTwoHeader } from '@proton/components';
 import getBoldFormattedText from '@proton/components/helpers/getBoldFormattedText';
 import { BRAND_NAME } from '@proton/shared/lib/constants';
+import { hasBit } from '@proton/shared/lib/helpers/bitset';
+import { getCheckout } from '@proton/shared/lib/helpers/checkout';
 import { Plan, PlansMap } from '@proton/shared/lib/interfaces';
 
+import { SubscriptionData } from '../../signup/interfaces';
 import PlanComparison from './PlanComparison';
 
 interface Props extends Omit<ModalProps, 'title'> {
@@ -18,6 +21,7 @@ interface Props extends Omit<ModalProps, 'title'> {
     relativePrice: string | undefined;
     appName: string;
     plansMap: PlansMap;
+    subscriptionData: SubscriptionData | undefined;
     onUpgrade: () => void;
     onFree: () => void;
 }
@@ -29,6 +33,7 @@ const UnlockModal = ({
     upsellPlan,
     relativePrice,
     appName,
+    subscriptionData,
     plansMap,
     onUpgrade,
     onFree,
@@ -37,6 +42,13 @@ const UnlockModal = ({
     const currentPlanTitle = currentPlan?.Title || '';
     const unlockPlanTitle = unlockPlan?.Title || '';
     const upsellPlanTitle = upsellPlan?.Title || '';
+    const checkout = subscriptionData
+        ? getCheckout({
+              planIDs: subscriptionData.planIDs,
+              plansMap,
+              checkResult: subscriptionData.checkResult,
+          })
+        : undefined;
     return (
         <ModalTwo {...rest} size="small" disableCloseOnEscape={true}>
             <ModalTwoHeader title={title} className="text-center" hasClose={false} />
@@ -45,17 +57,38 @@ const UnlockModal = ({
                     {c('pass_signup_2023: Info')
                         .t`The offer you selected is not available for ${currentPlanTitle} subscribers.`}
                 </div>
-                <div className="mb-4 color-weak">
-                    {relativePrice
-                        ? getBoldFormattedText(
-                              c('pass_signup_2023: Info')
-                                  .t`But you can unlock **${unlockPlanTitle}** and all premium ${BRAND_NAME} services by upgrading to **${upsellPlanTitle}** — it’s just ${relativePrice} extra per month.`
-                          )
-                        : getBoldFormattedText(
-                              c('pass_signup_2023: Info')
-                                  .t`But you can unlock **${unlockPlanTitle}** and all premium ${BRAND_NAME} services by upgrading to **${upsellPlanTitle}**.`
-                          )}
-                </div>
+                {(() => {
+                    const hasUnlockPlan = hasBit(currentPlan?.Services, unlockPlan?.Services || 0);
+                    return (
+                        <div className="mb-4 color-weak">
+                            {(() => {
+                                if (hasUnlockPlan && checkout) {
+                                    const discount = `${checkout.discountPercent}%`;
+                                    if (relativePrice) {
+                                        return getBoldFormattedText(
+                                            c('pass_signup_2023: Info')
+                                                .t`But you can get ${discount} off by upgrading to **${upsellPlanTitle}** — it’s just ${relativePrice} extra per month.`
+                                        );
+                                    }
+                                    return getBoldFormattedText(
+                                        c('pass_signup_2023: Info')
+                                            .t`But you can get ${discount} off by upgrading to **${upsellPlanTitle}**.`
+                                    );
+                                }
+                                if (relativePrice) {
+                                    return getBoldFormattedText(
+                                        c('pass_signup_2023: Info')
+                                            .t`But you can unlock **${unlockPlanTitle}** and all premium ${BRAND_NAME} services by upgrading to **${upsellPlanTitle}** — it’s just ${relativePrice} extra per month.`
+                                    );
+                                }
+                                return getBoldFormattedText(
+                                    c('pass_signup_2023: Info')
+                                        .t`But you can unlock **${unlockPlanTitle}** and all premium ${BRAND_NAME} services by upgrading to **${upsellPlanTitle}**.`
+                                );
+                            })()}
+                        </div>
+                    );
+                })()}
                 <PlanComparison
                     currentPlan={currentPlan}
                     upsellPlan={upsellPlan}
