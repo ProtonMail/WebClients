09b45027f05b2434006f600187cd1749b73f7f5b
Dominik Jankovic <dominik.jankovic@proton.ch>
2023-10-31 12:06:41 +0100

    Verity Firefox reproducibility
    [IDTEAM-2065]

--- applications/pass-extension/tools/build-all.sh
+++ applications/pass-extension/tools/build-all.sh
@@ -10,28 +10,48 @@ COMMIT="$(git rev-parse --short HEAD)"
 BUILD_ID="ProtonPass-${VERSION}-${COMMIT}"
 
 
-echo "Building for Chrome & Firefox... This may take a while."
+echo "Building for all platforms... This may take a while."
+printf "\tNode\t%s (%s)\n" "$(node --version)" "$(which node)"
+printf "\tnpm\tv%s (%s)\n" "$(npm --version)" "$(which npm)"
+printf "\tYarn\tv%s (%s)\n" "$(yarn --version)" "$(which yarn)"
+for var in "REPODIR" "PASSDIR" "VERSION" "OUTDIR" "COMMIT" "BUILD_ID"; do
+    printf "\t%s = %s\n" "${var}" "${!var}"
+done
+
+
+# Validate dependencies
+echo "Validating yarn.lock..."
+if [ -z ${CI+n} ]; then
+    cd "$REPODIR"
+    yarn install --immutable --immutable-cache > /dev/null
+    printf "\t✅ %s\n" "OK"
+else
+    printf "\t⚠️ Skipped, \$CI var is already set\n"
+fi
+
+
 mkdir -p "$OUTDIR"
 cd "$PASSDIR"
 
 
 # Chrome production release
+echo "Building Chrome (Prod)..."
 BUILD_TARGET=chrome yarn run build > /dev/null
 cd dist
 tar -cf "$OUTDIR/$BUILD_ID-chrome.tar" "."
 cd ..
-echo "✅ Built Chrome (Prod)"
-printf "\t%s\n" "$OUTDIR/$BUILD_ID-chrome.tar"
+printf "\t✅ %s\n" "$OUTDIR/$BUILD_ID-chrome.tar"
 
 
 # Chrome development (Black) release
+echo "Building Chrome (Black)..."
 BUILD_TARGET=chrome yarn run build:dev > /dev/null
-mv "./dist" "$OUTDIR/$BUILD_ID-chrome-black"
-echo "✅ Built Chrome (Black)"
-printf "\t%s\n" "$OUTDIR/$BUILD_ID-chrome-black"
+mv "./dist" "$OUTDIR/black-$BUILD_ID-chrome"
+printf "\t✅ %s\n" "$OUTDIR/black-$BUILD_ID-chrome"
 
 
 # Firefox production release
+echo "Building Firefox (Sources)..."
 # Firefox sources
 BUILD_TARGET=firefox NODE_ENV=production yarn run config
 # Preserve config.ts because the `yarn` postinstall script will overwrite it
@@ -56,21 +76,41 @@ tar -c                                             \
     ./*.mjs
 cd "$PASSDIR"
 tar rf "$OUTDIR/$BUILD_ID-FF-sources.tar" "FIREFOX_REVIEWERS.md"
-echo "✅ Built Firefox (Sources)"
-printf "\t%s\n" "$OUTDIR/$BUILD_ID-FF-sources.tar"
+printf "\t✅ %s\n" "$OUTDIR/$BUILD_ID-FF-sources.tar"
 
 
 # Firefox build
-BUILD_TARGET=firefox yarn run build:ff > /dev/null
+echo "Building Firefox (Prod)..."
+yarn run build:ff > /dev/null
 cd dist
 tar -cf "$OUTDIR/$BUILD_ID-FF.tar" "."
 cd ..
-echo "✅ Built Firefox (Prod)"
-printf "\t%s\n" "$OUTDIR/$BUILD_ID-FF.tar"
+printf "\t✅ %s\n" "$OUTDIR/$BUILD_ID-FF.tar"
+
+
+# Verify FF reproducibility
+echo "Verifying Firefox reproducibility..."
+# Extract build
+mkdir -p "$OUTDIR/$BUILD_ID-FF"
+cd "$OUTDIR/$BUILD_ID-FF"
+tar xf "$OUTDIR/$BUILD_ID-FF.tar"
+
+# Extract sources
+mkdir -p "$OUTDIR/$BUILD_ID-FF-sources"
+cd "$OUTDIR/$BUILD_ID-FF-sources"
+tar xf "$OUTDIR/$BUILD_ID-FF-sources.tar"
+
+# Build and diff
+yarn install > /dev/null
+cd applications/pass-extension
+yarn run build:ff > /dev/null
+diff -qr dist "$OUTDIR/$BUILD_ID-FF"
+cd "$PASSDIR"
+printf "\t✅ %s\n" "OK"
 
 
 # Firefox development (Black) release
+echo "Building Firefox (Black)..."
 BUILD_TARGET=firefox yarn run build:dev > /dev/null
-mv "./dist" "$OUTDIR/$BUILD_ID-FF-black"
-echo "✅ Built Firefox (Black)"
-printf "\t%s\n" "$OUTDIR/$BUILD_ID-FF-black"
+mv "./dist" "$OUTDIR/black-$BUILD_ID-FF"
+printf "\t✅ %s\n" "$OUTDIR/black-$BUILD_ID-FF"
