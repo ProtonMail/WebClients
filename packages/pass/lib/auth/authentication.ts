ff4aaff4e86614138d217a19cefaa88204715db8
Edvin CANDON <edvin.candon@proton.ch>
2023-11-03 18:24:49 +0100

    Keep a reference to LocalID in auth store and persisted session
    - Improve persisted session validation
    
    [IDTEAM-2250]

--- packages/pass/lib/auth/authentication.ts
+++ packages/pass/lib/auth/authentication.ts
@@ -6,16 +6,17 @@ import type { AuthSession } from './session';
 
 export type AuthStore = ReturnType<typeof createAuthStore>;
 
-const PASS_MAILBOX_PWD_KEY = 'pass:mailbox_pwd';
-const PASS_UID_KEY = 'pass:uid';
-const PASS_USER_ID_KEY = 'pass:user_id';
+const PASS_ACCESS_TOKEN_KEY = 'pass:access_token';
+const PASS_LOCAL_ID_KEY = 'pass:local_id';
+const PASS_LOCK_LAST_EXTEND_TIME = 'pass:lock_extend_time';
 const PASS_LOCK_STATUS_KEY = 'pass:lock_status';
 const PASS_LOCK_TOKEN_KEY = 'pass:lock_token';
 const PASS_LOCK_TTL_KEY = 'pass:lock_ttl';
-const PASS_LOCK_LAST_EXTEND_TIME = 'pass:lock_extend_time';
-const PASS_ACCESS_TOKEN_KEY = 'pass:access_token';
-const PASS_REFRESH_TOKEN_KEY = 'pass:refresh_token';
+const PASS_MAILBOX_PWD_KEY = 'pass:mailbox_pwd';
 const PASS_REFRESH_TIME_KEY = 'pass:refresh_time';
+const PASS_REFRESH_TOKEN_KEY = 'pass:refresh_token';
+const PASS_UID_KEY = 'pass:uid';
+const PASS_USER_ID_KEY = 'pass:user_id';
 
 export const createAuthStore = (store: Store) => {
     const authStore = {
@@ -25,8 +26,9 @@ export const createAuthStore = (store: Store) => {
         getSession: (): AuthSession => ({
             AccessToken: authStore.getAccessToken() ?? '',
             keyPassword: authStore.getPassword(),
-            RefreshToken: authStore.getRefreshToken() ?? '',
+            LocalID: authStore.getLocalID(),
             RefreshTime: authStore.getRefreshTime(),
+            RefreshToken: authStore.getRefreshToken() ?? '',
             sessionLockToken: authStore.getLockToken(),
             UID: authStore.getUID() ?? '',
             UserID: authStore.getUserID() ?? '',
@@ -43,9 +45,10 @@ export const createAuthStore = (store: Store) => {
         getUID: (): Maybe<string> => store.get(PASS_UID_KEY),
         setUserID: (UserID: Maybe<string>): void => store.set(PASS_USER_ID_KEY, UserID),
         getUserID: (): Maybe<string> => store.get(PASS_USER_ID_KEY),
-
         setPassword: encodedSetter(store)(PASS_MAILBOX_PWD_KEY),
         getPassword: encodedGetter(store)(PASS_MAILBOX_PWD_KEY, ''),
+        setLocalID: (LocalID: Maybe<number>): void => store.set(PASS_LOCAL_ID_KEY, LocalID),
+        getLocalID: (): Maybe<number> => store.get(PASS_LOCAL_ID_KEY),
 
         setLockStatus: (status: Maybe<SessionLockStatus>): void => store.set(PASS_LOCK_STATUS_KEY, status),
         getLockStatus: (): Maybe<SessionLockStatus> => store.get(PASS_LOCK_STATUS_KEY),
