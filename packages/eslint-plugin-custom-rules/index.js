c28d8c9f4e083fd53bcdfb6c080c398b6880184a
Pierre Bondoerffer <pierre.bondoerffer@proton.ch>
2023-11-06 23:51:44 +0100

    Add eslint rule to forbid template strings in translator context

--- packages/eslint-plugin-custom-rules/index.js
+++ packages/eslint-plugin-custom-rules/index.js
@@ -4,5 +4,6 @@ module.exports = {
         'deprecate-responsive-utility-classes': require('./deprecate-responsive-utility-classes'),
         'deprecate-sizing-classes': require('./deprecate-sizing-classes'),
         'deprecate-classes': require('./deprecate-classes'),
+        'no-template-in-translator-context': require('./no-template-in-translator-context'),
     },
 };
