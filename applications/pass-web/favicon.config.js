aa66d07fb4dfb72df9cbeef48edde3d3d5202f04
Irena <irena.zazoska@proton.ch>
2023-11-07 15:17:41 +0100

    Fix Pass web build [IDTEAM-2060]

--- applications/pass-web/favicon.config.js
+++ applications/pass-web/favicon.config.js
@@ -4,11 +4,14 @@ const url = 'https://pass.proton.me/';
 
 module.exports = {
     logo: path.resolve('./src/favicon.svg'),
+    // TODO provide real maskable icon
+    logoMaskable: path.resolve('./src/favicon.svg'),
     favicons: {
-        appName: 'Proton Pass Web',
-        appDescription: 'Proton Pass web is comming soon!',
+        appName: 'Proton Pass Web App',
+        appDescription:
+            'Proton Pass is an open source, end-to-end encrypted password manager app. Create and store passwords, email aliases, 2FA codes, and notes on all your devices',
     },
     url,
     ogImage: `${url}assets/proton-og-image.png`,
-    //locales: Object.keys(require('./locales/config/locales.json')),
+    // locales: Object.keys(require('./locales/config/locales.json')),
 };
