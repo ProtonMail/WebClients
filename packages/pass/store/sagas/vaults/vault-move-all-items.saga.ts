1885fbc44636dd0c33b531e90a215c36412beebb
Edvin CANDON <edvin.candon@proton.ch>
2023-11-08 08:10:29 +0100

    Remove `takeEveryBefore` usage in vault delete and move items sagas
    - Unnecessary as optimistics where removed from these vault actions

--- packages/pass/store/sagas/vaults/vault-move-all-items.saga.ts
+++ packages/pass/store/sagas/vaults/vault-move-all-items.saga.ts
@@ -1,4 +1,4 @@
-import { put } from 'redux-saga/effects';
+import { put, select, takeEvery } from 'redux-saga/effects';
 
 import { moveItems } from '@proton/pass/lib/items/item.requests';
 import {
@@ -6,19 +6,16 @@ import {
     vaultMoveAllItemsIntent,
     vaultMoveAllItemsSuccess,
 } from '@proton/pass/store/actions';
-import { takeEveryBefore } from '@proton/pass/store/sagas/utils/take.before';
 import { selectItemsByShareId } from '@proton/pass/store/selectors';
-import type { State, WorkerRootSagaOptions } from '@proton/pass/store/types';
+import type { WorkerRootSagaOptions } from '@proton/pass/store/types';
 import type { ItemRevision } from '@proton/pass/types';
 
 function* moveAllItemsWorker(
-    { payload: { shareId, content, destinationShareId }, meta }: ReturnType<typeof vaultMoveAllItemsIntent>,
-    stateBeforeAction: State,
-    { onItemsChange }: WorkerRootSagaOptions
-): Generator {
+    { onItemsChange }: WorkerRootSagaOptions,
+    { payload: { shareId, content, destinationShareId }, meta }: ReturnType<typeof vaultMoveAllItemsIntent>
+) {
     try {
-        const items = selectItemsByShareId(stateBeforeAction, shareId);
-
+        const items: ItemRevision[] = yield select(selectItemsByShareId(shareId));
         const movedItems = (yield moveItems(items, destinationShareId)) as ItemRevision[];
 
         yield put(vaultMoveAllItemsSuccess(meta.request.id, { shareId, content, movedItems }));
@@ -29,5 +26,5 @@ function* moveAllItemsWorker(
 }
 
 export default function* watcher(options: WorkerRootSagaOptions) {
-    yield takeEveryBefore(vaultMoveAllItemsIntent.match, moveAllItemsWorker, options);
+    yield takeEvery(vaultMoveAllItemsIntent.match, moveAllItemsWorker, options);
 }
