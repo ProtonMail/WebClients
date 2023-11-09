8a22accaa502fd1a7a44e4ca26f237a3c1c3a14a
larabr <7375870+larabr@users.noreply.github.com>
2023-11-09 19:54:44 +0100

    Add support for fetching keys with e2ee disabled flags in getPublicKeys and getEncryptionPreferences
    The caller can now request that address keys with e2ee-disabled flag be returned.
    Note that internal address keys from external accounts are still always filtered out, since these keys are not used by Inbox apps.
    While the helpers are in principle not Inbox-specific, they are currently not used by other apps, and full support of external accounts
    would require more extended refactoring - and different interfaces - to enable the caller to distinguish between WKD keys and address keys
    for this type of accounts.

--- packages/shared/lib/interfaces/hooks/GetPublicKeys.ts
+++ packages/shared/lib/interfaces/hooks/GetPublicKeys.ts
@@ -4,10 +4,16 @@ export type GetPublicKeys = ({
     email,
     lifetime,
     internalKeysOnly,
+    includeInternalKeysWithE2EEDisabledForMail,
     noCache,
 }: {
     email: string;
     lifetime?: number;
     internalKeysOnly?: boolean;
+    /**
+     * Whether to return internal keys which cannot be used for email encryption, as the owner has disabled E2EE.
+     * These keys may still be used for e.g. calendar sharing or message verification.
+     **/
+    includeInternalKeysWithE2EEDisabledForMail?: boolean;
     noCache?: boolean;
 }) => Promise<ApiKeysConfig>;
