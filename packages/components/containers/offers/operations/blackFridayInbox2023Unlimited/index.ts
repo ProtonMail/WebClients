da8d99a41e922dbe5befaca76aa9e93fb94fce74
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-11-09 17:16:09 +0100

    Use eligibility instead of hook

--- packages/components/containers/offers/operations/blackFridayInbox2023Unlimited/index.ts
+++ packages/components/containers/offers/operations/blackFridayInbox2023Unlimited/index.ts
@@ -1,2 +1,3 @@
 export { default as blackFriday2023InboxUnlimitedConfig } from './configuration';
 export { default as useBlackFriday2023InboxUnlimited } from './useOffer';
+export { default as blackFriday2023InboxUnlimitedEligibility } from './eligibility';
--- packages/components/containers/payments/subscription/AutomaticSubscriptionModal.tsx
+++ packages/components/containers/payments/subscription/AutomaticSubscriptionModal.tsx
@@ -8,6 +8,8 @@ import {
     ModalProps,
     OpenCallbackProps,
     Prompt,
+    useConfig,
+    useLastSubscriptionEnd,
     useLoad,
     useModalState,
     usePlans,
@@ -15,16 +17,46 @@ import {
     useSubscriptionModal,
     useUser,
 } from '@proton/components';
-import { useBlackFriday2023DriveFree } from '@proton/components/containers/offers/operations/blackFridayDrive2023Free';
-import { useBlackFriday2023DrivePlus } from '@proton/components/containers/offers/operations/blackFridayDrive2023Plus';
-import { useBlackFriday2023DriveUnlimited } from '@proton/components/containers/offers/operations/blackFridayDrive2023Unlimited';
-import { useBlackFriday2023InboxFree } from '@proton/components/containers/offers/operations/blackFridayInbox2023Free';
-import { useBlackFriday2023InboxMail } from '@proton/components/containers/offers/operations/blackFridayInbox2023Plus';
-import { useBlackFriday2023InboxUnlimited } from '@proton/components/containers/offers/operations/blackFridayInbox2023Unlimited';
-import { useBlackFriday2023VPNFree } from '@proton/components/containers/offers/operations/blackFridayVPN2023Free';
-import { useBlackFriday2023VPNMonthly } from '@proton/components/containers/offers/operations/blackFridayVPN2023Monthly';
-import { useBlackFriday2023VPNTwoYears } from '@proton/components/containers/offers/operations/blackFridayVPN2023TwoYears';
-import { useBlackFriday2023VPNYearly } from '@proton/components/containers/offers/operations/blackFridayVPN2023Yearly';
+import {
+    blackFriday2023DriveFreeConfig,
+    blackFriday2023DriveFreeEligibility,
+} from '@proton/components/containers/offers/operations/blackFridayDrive2023Free';
+import {
+    blackFriday2023DrivePlusConfig,
+    blackFriday2023DrivePlusEligibility,
+} from '@proton/components/containers/offers/operations/blackFridayDrive2023Plus';
+import {
+    blackFriday2023DriveUnlimitedConfig,
+    blackFriday2023DriveUnlimitedEligibility,
+} from '@proton/components/containers/offers/operations/blackFridayDrive2023Unlimited';
+import {
+    blackFriday2023InboxFreeConfig,
+    blackFriday2023InboxFreeEligibility,
+} from '@proton/components/containers/offers/operations/blackFridayInbox2023Free';
+import {
+    blackFriday2023InboxMailConfig,
+    blackFriday2023InboxMailEligibility,
+} from '@proton/components/containers/offers/operations/blackFridayInbox2023Plus';
+import {
+    blackFriday2023InboxUnlimitedConfig,
+    blackFriday2023InboxUnlimitedEligibility,
+} from '@proton/components/containers/offers/operations/blackFridayInbox2023Unlimited';
+import {
+    blackFriday2023VPNFreeConfig,
+    blackFriday2023VPNFreeEligibility,
+} from '@proton/components/containers/offers/operations/blackFridayVPN2023Free';
+import {
+    blackFriday2023VPNMonthlyConfig,
+    blackFriday2023VPNMonthlyEligibility,
+} from '@proton/components/containers/offers/operations/blackFridayVPN2023Monthly';
+import {
+    blackFriday2023VPNTwoYearsConfig,
+    blackFriday2023VPNTwoYearsEligibility,
+} from '@proton/components/containers/offers/operations/blackFridayVPN2023TwoYears';
+import {
+    blackFriday2023VPNYearlyConfig,
+    blackFriday2023VPNYearlyEligibility,
+} from '@proton/components/containers/offers/operations/blackFridayVPN2023Yearly';
 import { getMonths } from '@proton/components/containers/payments/SubscriptionsSection';
 import { SUBSCRIPTION_STEPS } from '@proton/components/containers/payments/subscription/constants';
 import getBoldFormattedText from '@proton/components/helpers/getBoldFormattedText';
@@ -32,6 +64,7 @@ import { CURRENCIES, DEFAULT_CYCLE, PLANS } from '@proton/shared/lib/constants';
 import { toMap } from '@proton/shared/lib/helpers/object';
 import { getValidCycle } from '@proton/shared/lib/helpers/subscription';
 import { Currency, Plan, PlansMap, Subscription, UserModel } from '@proton/shared/lib/interfaces';
+import isTruthy from '@proton/utils/isTruthy';
 
 import { getCurrency } from './helpers';
 import { Eligibility, PlanCombinationWithDiscount, getEligibility } from './subscriptionEligbility';
@@ -146,10 +179,12 @@ const UpsellPrompt = ({ planCombination: { discount, plan, cycle }, onConfirm, .
 const AutomaticSubscriptionModal = () => {
     const history = useHistory();
     const location = useLocation();
+    const protonConfig = useConfig();
 
     const [open, loadingModal] = useSubscriptionModal();
     const [plans, loadingPlans] = usePlans();
     const [subscription, loadingSubscription] = useSubscription();
+    const [lastSubscriptionEnd, loadingLastSubscriptionEnd] = useLastSubscriptionEnd();
     const [user] = useUser();
     const tmpProps = useRef<{ props: OpenCallbackProps; eligibility: Eligibility } | undefined>(undefined);
     const [upsellModalProps, setUpsellModal, renderUpsellModal] = useModalState();
@@ -158,34 +193,15 @@ const AutomaticSubscriptionModal = () => {
 
     useLoad();
 
-    const blackFriday2023InboxFree = useBlackFriday2023InboxFree();
-    const blackFriday2023InboxMail = useBlackFriday2023InboxMail();
-    const blackFriday2023InboxUnlimited = useBlackFriday2023InboxUnlimited();
-    const blackFriday2023VPNFree = useBlackFriday2023VPNFree();
-    const blackFriday2023VPNMonthly = useBlackFriday2023VPNMonthly();
-    const blackFriday2023VPNYearly = useBlackFriday2023VPNYearly();
-    const blackFriday2023VPNTwoYears = useBlackFriday2023VPNTwoYears();
-    const blackFriday2023DriveFree = useBlackFriday2023DriveFree();
-    const blackFriday2023DrivePlus = useBlackFriday2023DrivePlus();
-    const blackFriday2023DriveUnlimited = useBlackFriday2023DriveUnlimited();
-
-    const allOffers = [
-        blackFriday2023InboxFree,
-        blackFriday2023InboxMail,
-        blackFriday2023InboxUnlimited,
-        blackFriday2023VPNFree,
-        blackFriday2023VPNMonthly,
-        blackFriday2023VPNYearly,
-        blackFriday2023VPNTwoYears,
-        blackFriday2023DriveFree,
-        blackFriday2023DrivePlus,
-        blackFriday2023DriveUnlimited,
-    ];
-
-    const loadingOffer = allOffers.some((offer) => offer.isLoading);
-
     useEffect(() => {
-        if (!plans || !subscription || loadingPlans || loadingSubscription || loadingModal || loadingOffer) {
+        if (
+            !plans ||
+            !subscription ||
+            loadingPlans ||
+            loadingSubscription ||
+            loadingModal ||
+            loadingLastSubscriptionEnd
+        ) {
             return;
         }
 
@@ -202,7 +218,24 @@ const AutomaticSubscriptionModal = () => {
             return;
         }
 
-        const eligibleBlackFridayOperations = allOffers.filter((offer) => offer.isEligible);
+        const options = {
+            subscription,
+            protonConfig,
+            user,
+            lastSubscriptionEnd,
+        };
+        const eligibleBlackFridayConfigs = [
+            blackFriday2023InboxFreeEligibility(options) && blackFriday2023InboxFreeConfig,
+            blackFriday2023InboxMailEligibility(options) && blackFriday2023InboxMailConfig,
+            blackFriday2023InboxUnlimitedEligibility(options) && blackFriday2023InboxUnlimitedConfig,
+            blackFriday2023VPNFreeEligibility(options) && blackFriday2023VPNFreeConfig,
+            blackFriday2023VPNMonthlyEligibility(options) && blackFriday2023VPNMonthlyConfig,
+            blackFriday2023VPNYearlyEligibility(options) && blackFriday2023VPNYearlyConfig,
+            blackFriday2023VPNTwoYearsEligibility(options) && blackFriday2023VPNTwoYearsConfig,
+            blackFriday2023DriveFreeEligibility(options) && blackFriday2023DriveFreeConfig,
+            blackFriday2023DrivePlusEligibility(options) && blackFriday2023DrivePlusConfig,
+            blackFriday2023DriveUnlimitedEligibility(options) && blackFriday2023DriveUnlimitedConfig,
+        ].filter(isTruthy);
 
         const eligibility = getEligibility({
             plansMap,
@@ -213,7 +246,7 @@ const AutomaticSubscriptionModal = () => {
             },
             subscription,
             user,
-            eligibleBlackFridayOperations,
+            eligibleBlackFridayConfigs,
         });
 
         history.replace({ search: undefined });
@@ -257,7 +290,7 @@ const AutomaticSubscriptionModal = () => {
         if (eligibility.type === 'pass-through') {
             open(openProps);
         }
-    }, [loadingPlans, loadingSubscription, loadingModal, loadingOffer, location.search]);
+    }, [loadingPlans, loadingSubscription, loadingModal, loadingLastSubscriptionEnd, location.search]);
 
     const tmp = tmpProps.current;
 
