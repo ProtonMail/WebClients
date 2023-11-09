46849310940eb926f08ffaf149a74aa146b814ae
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-11-09 17:16:09 +0100

    Use eligibility instead of hook

--- packages/components/containers/offers/operations/blackFridayVPN2023TwoYears/index.ts
+++ packages/components/containers/offers/operations/blackFridayVPN2023TwoYears/index.ts
@@ -1,2 +1,3 @@
 export { default as blackFriday2023VPNTwoYearsConfig } from './configuration';
 export { default as useBlackFriday2023VPNTwoYears } from './useOffer';
+export { default as blackFriday2023VPNTwoYearsEligibility } from './eligibility';
