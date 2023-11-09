68b2041cf3a2eca0a89ee120f0ae342a224e1fdf
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-11-09 17:16:09 +0100

    Use eligibility instead of hook

--- packages/components/containers/offers/operations/blackFridayDrive2023Unlimited/index.ts
+++ packages/components/containers/offers/operations/blackFridayDrive2023Unlimited/index.ts
@@ -1,2 +1,3 @@
 export { default as blackFriday2023DriveUnlimitedConfig } from './configuration';
 export { default as useBlackFriday2023DriveUnlimited } from './useOffer';
+export { default as blackFriday2023DriveUnlimitedEligibility } from './eligibility';
--- packages/components/containers/offers/operations/blackFridayInbox2023Plus/index.ts
+++ packages/components/containers/offers/operations/blackFridayInbox2023Plus/index.ts
@@ -1,2 +1,3 @@
 export { default as blackFriday2023InboxMailConfig } from './configuration';
 export { default as useBlackFriday2023InboxMail } from './useOffer';
+export { default as blackFriday2023InboxMailEligibility } from './eligibility';
