db897220927a8097bff36f4c09fd16c392fc2cfe
Edvin CANDON <edvin.candon@proton.ch>
2023-11-12 10:13:29 +0100

    Prepare common pass authentication utilities for web-app
    - Handle 'sso' and 'secure' mode in fork consuming
    - Remove references to extension in auth types
    - Rename fork hooks to `useRequestFork`
    
    [IDTEAM-2238]

--- packages/pass/lib/auth/fork.ts
+++ packages/pass/lib/auth/fork.ts
@@ -1,54 +1,85 @@
 import { c } from 'ttag';
 
-import { browserSessionStorage } from '@proton/pass/lib/extension/storage';
-import type { Api } from '@proton/pass/types';
+import type { Api, MaybeNull } from '@proton/pass/types';
 import { pullForkSession, setRefreshCookies as refreshTokens } from '@proton/shared/lib/api/auth';
 import { getUser } from '@proton/shared/lib/api/user';
 import type { FORK_TYPE } from '@proton/shared/lib/authentication/ForkInterface';
+import { getKey } from '@proton/shared/lib/authentication/cryptoHelper';
+import { InvalidForkConsumeError } from '@proton/shared/lib/authentication/error';
 import type { PullForkResponse, RefreshSessionResponse } from '@proton/shared/lib/authentication/interface';
-import { APPS, MAIL_APP_NAME, PASS_APP_NAME, SSO_PATHS } from '@proton/shared/lib/constants';
+import { getForkDecryptedBlob } from '@proton/shared/lib/authentication/sessionForkBlob';
+import { type getConsumeForkParameters } from '@proton/shared/lib/authentication/sessionForking';
+import type { APP_NAMES } from '@proton/shared/lib/constants';
+import { MAIL_APP_NAME, PASS_APP_NAME, SSO_PATHS } from '@proton/shared/lib/constants';
 import { withAuthHeaders, withUIDHeaders } from '@proton/shared/lib/fetch/headers';
 import { encodeBase64URL, uint8ArrayToString } from '@proton/shared/lib/helpers/encoding';
 import type { User } from '@proton/shared/lib/interfaces';
 
 import type { AuthSession } from './session';
 
-/* FIXME: update to `APPS.PROTONPASSBROWSEREXTENSION` */
-export const requestFork = async (host: string, type?: FORK_TYPE) => {
+type RequestForkOptions = { host: string; app: APP_NAMES; type?: FORK_TYPE };
+type RequestForkResult = { url: string; state: string };
+
+export const requestFork = ({ host, type, app }: RequestForkOptions): RequestForkResult => {
     const state = encodeBase64URL(uint8ArrayToString(crypto.getRandomValues(new Uint8Array(32))));
 
     const searchParams = new URLSearchParams();
-    searchParams.append('app', APPS.PROTONEXTENSION);
+    searchParams.append('app', app);
     searchParams.append('state', state);
     searchParams.append('independent', '0');
     if (type) searchParams.append('t', type);
 
-    await browserSessionStorage.setItem(`f${state}`, JSON.stringify({}));
-
-    return `${host}${SSO_PATHS.AUTHORIZE}?${searchParams.toString()}`;
+    return { url: `${host}${SSO_PATHS.AUTHORIZE}?${searchParams.toString()}`, state };
 };
 
-type ConsumeForkOptions = {
-    api: Api;
-    apiUrl?: string;
-    state: string;
-    selector: string;
-    keyPassword: string;
-};
+export type ConsumeForkParameters = ReturnType<typeof getConsumeForkParameters>;
+export type ConsumeForkPayload =
+    | { mode: 'sso'; localState: MaybeNull<string>; state: string; selector: string; key?: Uint8Array }
+    | { mode: 'secure'; state: string; selector: string; keyPassword: string };
 
-export const consumeFork = async ({ api, apiUrl, selector, keyPassword }: ConsumeForkOptions): Promise<AuthSession> => {
-    const pullForkParams = pullForkSession(selector);
+export type ConsumeForkOptions = { api: Api; apiUrl?: string; payload: ConsumeForkPayload };
+
+/**
+ * If `keyPassword` is not provided to `ConsumeForkOptions`, it will attempt to recover it from
+ * the `Payload` property of the `PullForkResponse`. `keyPassword` will always be omitted when
+ * retrieving the fork options from the url parameters. This is not the case when using secure
+ * extension messaging where `keyPassword` can safely be passed.
+ * ⚠️ Only validates the fork state in SSO mode.
+ */
+export const consumeFork = async (options: ConsumeForkOptions): Promise<AuthSession> => {
+    const { payload, apiUrl, api } = options;
+
+    const validFork =
+        (payload.mode === 'secure' || (payload.localState !== null && payload.key)) &&
+        payload.selector &&
+        payload.state;
+
+    if (!validFork) throw new InvalidForkConsumeError(`Invalid fork state`);
+
+    const pullForkParams = pullForkSession(payload.selector);
     pullForkParams.url = apiUrl ? `${apiUrl}/${pullForkParams.url}` : pullForkParams.url;
 
-    const { UID, RefreshToken, LocalID } = await api<PullForkResponse>(pullForkParams);
+    const { UID, RefreshToken, LocalID, Payload } = await api<PullForkResponse>(pullForkParams);
     const refresh = await api<RefreshSessionResponse>(withUIDHeaders(UID, refreshTokens({ RefreshToken })));
     const { User } = await api<{ User: User }>(withAuthHeaders(UID, refresh.AccessToken, getUser()));
+
+    const keyPassword =
+        payload.mode === 'secure'
+            ? payload.keyPassword
+            : await (async () => {
+                  try {
+                      const key = await getKey(payload.key!);
+                      return (await getForkDecryptedBlob(key, Payload))?.keyPassword ?? '';
+                  } catch (_) {
+                      throw new InvalidForkConsumeError('Failed to decrypt fork payload');
+                  }
+              })();
 
     return {
         AccessToken: refresh.AccessToken,
         keyPassword,
-        RefreshToken: refresh.RefreshToken,
         LocalID,
+        RefreshToken: refresh.RefreshToken,
         UID,
         UserID: User.ID,
     };
