9d1f0d0a8072559466ec8224f4b670bef916a4ea
Edvin CANDON <edvin.candon@proton.ch>
2023-11-12 09:54:00 +0100

    Improve pass-web setup
    - Setup eslint rules
    - Prepare tsconfig
    - Expose `ENV` var via DefinePlugin
    - Clean-up dependencies
    
    [IDTEAM-2238]

--- applications/pass/.eslintrc.js
+++ applications/pass/.eslintrc.js
@@ -5,5 +5,10 @@ module.exports = {
         tsconfigRootDir: __dirname,
         project: './tsconfig.json',
     },
+    rules: {
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        '@typescript-eslint/consistent-type-imports': 'error',
+        curly: ['error', 'multi-line'],
+    },
     ignorePatterns: ['.eslintrc.js'],
 };
