d878ee5f9569aa39bd2d13c780bf4c179a364105
Edvin CANDON <edvin.candon@proton.ch>
2023-11-14 13:40:31 +0100

    Leverage auth service and store in sagas
    [IDTEAM-2238]

--- packages/pass/store/types.ts
+++ packages/pass/store/types.ts
@@ -1,3 +1,4 @@
+import type { AuthService } from '@proton/pass/lib/auth/service';
 import type { AuthStore } from '@proton/pass/lib/auth/store';
 import type { AppState, MaybeNull } from '@proton/pass/types';
 import type { TelemetryEvent } from '@proton/pass/types/data/telemetry';
@@ -13,22 +14,19 @@ export type Action = ReturnType<(typeof actions)[keyof typeof actions]>;
 export type Telemetry = { start: () => void; stop: () => void; pushEvent: (event: TelemetryEvent) => Promise<boolean> };
 
 export interface WorkerRootSagaOptions {
-    getAuth: () => AuthStore;
+    getAuthStore: () => AuthStore;
+    getAuthService: () => AuthService;
     getCache: () => Promise<Partial<EncryptedExtensionCache>>;
     getEventInterval: () => number;
     getLocalSettings: () => Promise<ProxiedSettings>;
     getTelemetry: () => MaybeNull<Telemetry>;
-    getWorkerState: () => AppState;
+    getAppState: () => AppState;
     onBoot?: (result: { ok: true } | { ok: false; clearCache: boolean }) => void;
     onFeatureFlagsUpdate?: (features: FeatureFlagState) => void;
     onItemsChange?: () => void;
     onNotification?: (notification: Notification) => void;
-    onSessionLockChange?: (sessionLockToken?: string, sessionLockTTL?: number) => Promise<void>;
-    onSessionLocked?: () => Promise<void>;
-    onSessionUnlocked?: (sessionLockToken: string) => Promise<void>;
     onSettingUpdate?: (settings: ProxiedSettings) => Promise<void>;
     onShareEventDisabled?: (shareId: string) => void;
     onShareEventItemsDeleted?: (shareId: string, itemIds: string[]) => void;
-    onSignout?: () => void;
     setCache: (encrypted: EncryptedExtensionCache) => Promise<void>;
 }
