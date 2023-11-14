8c972cc203f2bfc86797651d017003de62ee01ca
Irena <irena.zazoska@proton.ch>
2023-11-14 17:34:19 +0100

    Fix pass web-app package name

--- applications/pass/src/app/App.tsx
+++ applications/pass/src/app/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
     return (
         <>
             <div className="text-bold text-4xl text-center w-full py-14">{c('Info').t`Hello from Pass web app`}</div>
-            <div className="text-xl text-center w-full h-full pb-14">{c('Info').t`Comming soon...`}</div>
+            <div className="text-xl text-center w-full h-full pb-14">{c('Info').t`Coming soon...`}</div>
         </>
     );
 };
