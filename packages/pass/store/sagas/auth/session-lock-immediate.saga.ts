8ef4067d7c910c217aa4a3e0a72a54e4801a9e67
Edvin CANDON <edvin.candon@proton.ch>
2023-11-14 13:40:31 +0100

    Leverage auth service and store in sagas
    [IDTEAM-2238]

--- packages/pass/store/sagas/auth/session-lock-immediate.saga.ts
+++ packages/pass/store/sagas/auth/session-lock-immediate.saga.ts
@@ -1,28 +1,20 @@
 import { fork, put, takeLeading } from 'redux-saga/effects';
 
-import { lockSessionImmediate } from '@proton/pass/lib/auth/session-lock';
 import { sessionLockIntent, stateCache } from '@proton/pass/store/actions';
 import type { WorkerRootSagaOptions } from '@proton/pass/store/types';
-import { logger } from '@proton/pass/utils/logger';
+import noop from '@proton/utils/noop';
 
 /* If we the user has not registered a lock yet (ie: has
  * a sessionLockToken saved) then this saga should have
  * no effect */
-function* lockSessionImmediateWorker({ onSessionLocked, getAuth }: WorkerRootSagaOptions) {
-    const sessionLockToken = getAuth().getLockToken();
-
-    if (sessionLockToken !== undefined) {
+function* lockSessionImmediateWorker({ getAuthService, getAuthStore: getAuth }: WorkerRootSagaOptions) {
+    if (getAuth().getLockToken() !== undefined) {
         yield put(stateCache());
-        /* fork for non-blocking action -> immediate UI effect */
-        yield fork(function* () {
-            try {
-                yield lockSessionImmediate();
-            } catch (e) {
-                logger.info('[Saga::SessionLock] Could not lock session on back-end');
-            }
-        });
 
-        yield onSessionLocked?.();
+        yield fork(function* () {
+            /* fork for non-blocking action -> immediate UI effect */
+            yield getAuthService().lock({ soft: false }).catch(noop);
+        });
     }
 }
 
