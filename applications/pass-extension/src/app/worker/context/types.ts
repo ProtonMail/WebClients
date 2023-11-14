96b9808b4a5b0abf0cfeb60298b6cde0c609dbbb
Edvin CANDON <edvin.candon@proton.ch>
2023-11-14 13:43:10 +0100

    Leverage abstract authentication service in pass extension

--- applications/pass-extension/src/app/worker/context/types.ts
+++ applications/pass-extension/src/app/worker/context/types.ts
@@ -1,23 +1,23 @@
-import type { AppStatus, MaybeNull } from '@proton/pass/types';
-import { type AppState } from '@proton/pass/types';
+import type { AuthService } from '@proton/pass/lib/auth/service';
+import type { AuthStore } from '@proton/pass/lib/auth/store';
+import type { AppState, AppStatus, MaybeNull } from '@proton/pass/types';
 
-import { type ActivationService } from '../services/activation';
-import { type AliasService } from '../services/alias';
-import { type AuthService } from '../services/auth';
-import { type AutoFillService } from '../services/autofill';
-import { type AutoSaveService } from '../services/autosave';
-import { type CacheProxyService } from '../services/cache-proxy';
-import { type ExportService } from '../services/export';
-import { type FormTrackerService } from '../services/form.tracker';
+import type { ActivationService } from '../services/activation';
+import type { AliasService } from '../services/alias';
+import type { AutoFillService } from '../services/autofill';
+import type { AutoSaveService } from '../services/autosave';
+import type { CacheProxyService } from '../services/cache-proxy';
+import type { ExportService } from '../services/export';
+import type { FormTrackerService } from '../services/form.tracker';
 import type { I18NService } from '../services/i18n';
-import { type InjectionService } from '../services/injection';
-import { type LoggerService } from '../services/logger';
-import { type OnboardingService } from '../services/onboarding';
-import { type OTPService } from '../services/otp';
-import { type SettingsService } from '../services/settings';
+import type { InjectionService } from '../services/injection';
+import type { LoggerService } from '../services/logger';
+import type { OnboardingService } from '../services/onboarding';
+import type { OTPService } from '../services/otp';
+import type { SettingsService } from '../services/settings';
 import type { StorageService } from '../services/storage';
-import { type StoreService } from '../services/store';
-import { type TelemetryService } from '../services/telemetry';
+import type { StoreService } from '../services/store';
+import type { TelemetryService } from '../services/telemetry';
 
 export type WorkerInitOptions = {
     sync?: boolean /* will clear local storage */;
@@ -26,6 +26,7 @@ export type WorkerInitOptions = {
 
 export interface WorkerContextInterface {
     status: AppStatus;
+    authStore: AuthStore;
     service: {
         auth: AuthService;
         activation: ActivationService;
