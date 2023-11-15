730b25b3f311f701058a49feada47653e405dd12
Matthieu Bué <matthieu.bue@proton.ch>
2023-11-15 18:27:19 +0100

    Replace remaining responsive flex utilities
    UXE-489

--- applications/account/src/app/single-signup-v2/Layout.tsx
+++ applications/account/src/app/single-signup-v2/Layout.tsx
@@ -69,7 +69,10 @@ const Layout = ({
                                 {footer}
                                 <div className="w-full flex flex-justify-space-between flex-column md:flex-row">
                                     <div className="flex gap-1 flex-column">
-                                        <LayoutLogosV2 size={20} className="on-mobile-flex-justify-center" />
+                                        <LayoutLogosV2
+                                            size={20}
+                                            className="flex-justify-center md:flex-justify-start"
+                                        />
                                         <span className="text-sm color-weak text-center mb-4 lg:mb-0">
                                             {
                                                 // translator: full sentence 'Proton. Privacy by default.'
