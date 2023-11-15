24029f90f533e65a382efb72e907842f1c4a5255
Matthieu Bué <matthieu.bue@proton.ch>
2023-11-15 18:27:19 +0100

    Replace remaining responsive flex utilities
    UXE-489

--- packages/components/containers/payments/subscription/PlanSelection.tsx
+++ packages/components/containers/payments/subscription/PlanSelection.tsx
@@ -388,7 +388,7 @@ const PlanSelection = ({
     const currencySelectorRow = (
         <div className="flex flex-justify-space-between flex-column md:flex-row-nostretch">
             <div className="hidden md:inline-block visibility-hidden">{currencyItem}</div>
-            <div className="flex on-mobile-flex on-mobile-flex-justify-center w-full md:w-auto">
+            <div className="flex flex-justify-center md:flex-justify-start w-full md:w-auto">
                 <CycleSelector
                     mode="buttons"
                     cycle={cycle}
