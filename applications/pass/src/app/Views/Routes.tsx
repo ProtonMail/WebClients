5ed7ce86e51ab29a20b00a28be1637957c3fc023
Irena <irena.zazoska@proton.ch>
2023-11-16 17:44:50 +0100

    wip: setup web app routing
    [IDTEAM-2295]

--- applications/pass/src/app/Views/Routes.tsx
+++ applications/pass/src/app/Views/Routes.tsx
@@ -1,6 +1,8 @@
 import { type FC } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import { ErrorBoundary, StandardErrorPage } from '@proton/components';
+
 import { useClient } from '../Context/ClientProvider';
 import { Lobby } from './Lobby';
 import { Main } from './Main';
@@ -9,8 +11,10 @@ export const Routes: FC = () => {
     const client = useClient();
 
     return (
-        <Switch>
-            <Route path="*" render={() => (client.state.loggedIn ? <Main /> : <Lobby />)} />
-        </Switch>
+        <ErrorBoundary component={<StandardErrorPage big />}>
+            <Switch>
+                <Route path="*" render={() => (client.state.loggedIn ? <Main /> : <Lobby />)} />
+            </Switch>
+        </ErrorBoundary>
     );
 };
