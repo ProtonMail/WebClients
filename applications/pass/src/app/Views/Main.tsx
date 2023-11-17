a81b5e83523af7fdf2dda23543e671fa76adb52f
Edvin CANDON <edvin.candon@proton.ch>
2023-11-17 17:59:12 +0100

    Setup nested route matching
    [IDTEAM-2295]

--- applications/pass/src/app/Views/Main.tsx
+++ applications/pass/src/app/Views/Main.tsx
@@ -1,28 +1,28 @@
 import { type FC } from 'react';
 
 import { Button } from '@proton/atoms/Button';
-import { LobbyLayout } from '@proton/pass/components/Layout/Lobby/LobbyLayout';
+import { FadeIn } from '@proton/pass/components/Layout/Animation/FadeIn';
 import { Content } from '@proton/pass/components/Layout/Section/Content';
+import { Sidebar } from '@proton/pass/components/Layout/Section/Sidebar';
 
 import { useAuthService } from '../Context/AuthServiceProvider';
-import { PrivateRoutes } from './PrivateRoutes';
+import { PrivateRoutes } from '../Routing/PrivateRoutes';
 
 export const Main: FC = () => {
     const authService = useAuthService();
 
     return (
-        <LobbyLayout overlay>
-            <main className="h-full w-full flex flex-column gap-4 flex-align-items-center flex-justify-center">
-                <h4>Logged in to Pass</h4>
-
+        <FadeIn id="main" className="flex flex-column flex-nowrap w-full h-full overflow-hidden" key="main" delay={50}>
+            <main className="flex flex-align-items-center flex-justify-center flex-nowrap w-full h-full">
+                <Sidebar>
+                    <Button shape="ghost" color="weak" onClick={() => authService.logout({ soft: false })}>
+                        Logout
+                    </Button>
+                </Sidebar>
                 <Content>
                     <PrivateRoutes />
                 </Content>
-
-                <Button pill shape="solid" color="weak" onClick={() => authService.logout({ soft: false })}>
-                    Logout
-                </Button>
             </main>
-        </LobbyLayout>
+        </FadeIn>
     );
 };
