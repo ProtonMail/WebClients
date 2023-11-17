be13a06fbf42988d985765661e6294acfc89de98
Edvin CANDON <edvin.candon@proton.ch>
2023-11-17 08:38:47 +0100

    Handle visibility changes
    - When document is hidden: destroy state and reset client status to IDLE
    - When document is visible: call auth init to re-boot
    
    [IDTEAM-2307]

--- applications/pass/src/app/Store/StoreProvider.tsx
+++ applications/pass/src/app/Store/StoreProvider.tsx
@@ -1,4 +1,4 @@
-import { type FC, useEffect, useRef } from 'react';
+import { type FC, useEffect } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 
 import { useNotifications } from '@proton/components/hooks';
@@ -11,30 +11,27 @@ import { pipe } from '@proton/pass/utils/fp/pipe';
 import { authStore } from '../../lib/core';
 import { getDBCache, writeDBCache } from '../../lib/database';
 import { useAuthService } from '../Context/AuthServiceProvider';
-import { useClient } from '../Context/ClientProvider';
+import { useClientRef } from '../Context/ClientProvider';
 import { rootSaga } from './root-saga';
 import { sagaMiddleware, store } from './store';
 
 export const StoreProvider: FC = ({ children }) => {
     const authService = useAuthService();
-    const client = useClient();
+    const client = useClientRef();
     const { createNotification } = useNotifications();
     const notificationEnhancer = useNotificationEnhancer({ onLink: (url) => window.open(url, '_blank') });
-
-    const clientRef = useRef(client);
-    clientRef.current = client;
 
     useEffect(() => {
         sagaMiddleware.run(
             rootSaga.bind(null, {
-                getAppState: () => clientRef.current.state,
+                getAppState: () => client.current.state,
                 getAuthService: () => authService,
                 getAuthStore: () => authStore,
                 getCache: () => getDBCache(authStore.getUserID()!),
                 getEventInterval: () => ACTIVE_POLLING_TIMEOUT,
                 getLocalSettings: async () => INITIAL_SETTINGS,
                 getTelemetry: () => null,
-                onBoot: ({ ok }) => client.setStatus(ok ? AppStatus.READY : AppStatus.ERROR),
+                onBoot: ({ ok }) => client.current.setStatus(ok ? AppStatus.READY : AppStatus.ERROR),
                 onNotification: pipe(notificationEnhancer, createNotification),
                 setCache: (encryptedCache) => writeDBCache(authStore.getUserID()!, encryptedCache),
             })
