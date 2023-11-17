ee666a9153a9ddff6f578f718fc883fb119e910e
Matthieu Bué <matthieu.bue@proton.ch>
2023-11-17 18:27:12 +0100

    Rename `flex-items-center` to `children-items-center`

--- packages/components/components/v2/addressesAutocomplete/AddressesAutocomplete.tsx
+++ packages/components/components/v2/addressesAutocomplete/AddressesAutocomplete.tsx
@@ -253,7 +253,7 @@ const AddressesAutocompleteTwo = forwardRef<HTMLInputElement, Props>(
                                 onChange={handleSelect}
                             >
                                 {option.type === 'group' ? (
-                                    <div className="flex flex-nowrap flex-items-center">
+                                    <div className="flex flex-nowrap children-items-center">
                                         <Icon
                                             name="circle-filled"
                                             color={option.value.Color}
