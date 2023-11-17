62fb981c7598a3bf2db43d91c566d6ec094cf778
Edvin CANDON <edvin.candon@proton.ch>
2023-11-17 14:22:39 +0100

    Decouple OTP generation from extension API
    - Define `OTPProvider` to set the OTP generation method for the client
    - In extension leverage worker messaging
    - In web-app leverage OTP utilities in-place
    - Move OTP generation to custom utility
    - Adapt OTPRequest payloads to always include totpUri
    
    [IDTEAM-2295]

--- packages/pass/components/Form/Field/Control/OTPValueControl.tsx
+++ packages/pass/components/Form/Field/Control/OTPValueControl.tsx
@@ -3,20 +3,23 @@ import { type VFC } from 'react';
 import { c } from 'ttag';
 
 import { OTPDonut } from '@proton/pass/components/Otp/OTPDonut';
+import { useOTPGenerate } from '@proton/pass/components/Otp/OTPProvider';
 import { OTPValue } from '@proton/pass/components/Otp/OTPValue';
-import type { UsePeriodOtpCodeOptions } from '@proton/pass/hooks/usePeriodicOtpCode';
 import { usePeriodicOtpCode } from '@proton/pass/hooks/usePeriodicOtpCode';
+import { type OtpRequest } from '@proton/pass/types';
 
 import { ValueControl } from './ValueControl';
+
+type Props = { label?: string; payload: OtpRequest };
 
 /* This component handles the period otp code generation
  * to avoid cluttering the render cycle of a component in
  * need of the OTP code generation as it involves alot of
  * re-rendering. eg: we do not want to re-render `Login.view`
- * everytime the OTP countdown updates
- */
-export const OTPValueControl: VFC<UsePeriodOtpCodeOptions & { label?: string }> = ({ totpUri, label, ...request }) => {
-    const [otp, percent] = usePeriodicOtpCode({ totpUri, ...request });
+ * everytime the OTP countdown updates */
+export const OTPValueControl: VFC<Props> = ({ label, payload }) => {
+    const generate = useOTPGenerate();
+    const [otp, percent] = usePeriodicOtpCode({ generate, payload });
 
     return (
         <ValueControl
