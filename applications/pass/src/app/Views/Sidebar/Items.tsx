8f4dcfa0142a899e11f71f37a7499d339fda91a2
Edvin CANDON <edvin.candon@proton.ch>
2023-11-19 11:42:33 +0100

    Handle trash routing in pass web-app
    - Adapt navigation provider API to support trash routes
    - Fix ItemView::doMoveItem refactoring error
    - Create generic sidebar placeholder
    
    [IDTEAM-2295]

--- applications/pass/src/app/Views/Sidebar/Items.tsx
+++ applications/pass/src/app/Views/Sidebar/Items.tsx
@@ -1,20 +1,21 @@
 import { type FC } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 
-import { c } from 'ttag';
-
 import { useNavigation } from '@proton/pass/components/Core/NavigationProvider';
+import { getItemRoute, getTrashRoute } from '@proton/pass/components/Core/routing';
 import { SortFilter } from '@proton/pass/components/Item/Filters/Sort';
 import { TypeFilter } from '@proton/pass/components/Item/Filters/Type';
 import { ItemsList } from '@proton/pass/components/Item/List/ItemsList';
 import { type ItemRouteParams } from '@proton/pass/components/Views/types';
 import { useFilteredItems } from '@proton/pass/hooks/useFilteredItems';
 
+import { ItemsPlaceholder } from './Placeholder';
+
 export const Items: FC = () => {
     const { filters, setFilters, selectItem } = useNavigation();
-    const match = useRouteMatch<ItemRouteParams>(`/u/*/share/:shareId/item/:itemId`);
-    const items = useFilteredItems(filters);
-    const search = filters.search ?? '';
+    const inTrash = useRouteMatch(getTrashRoute()) !== null;
+    const itemMatch = useRouteMatch<ItemRouteParams>(getItemRoute(':shareId', ':itemId', inTrash));
+    const items = useFilteredItems({ ...filters, trashed: inTrash });
 
     return (
         <>
@@ -30,19 +31,10 @@ export const Items: FC = () => {
                 items={items.filtered}
                 totalCount={items.totalCount}
                 onFilter={setFilters}
-                onSelect={selectItem}
-                selectedItem={match ? match.params : undefined}
+                onSelect={(shareId, itemId) => selectItem(shareId, itemId, { inTrash })}
+                selectedItem={itemMatch ? itemMatch.params : undefined}
                 placeholder={() => (
-                    <span className="block text-break color-weak text-sm p-2 text-center text-break">
-                        {search?.trim() ? (
-                            <span>
-                                {c('Warning').t`No items matching`}
-                                <br />"{search}"
-                            </span>
-                        ) : (
-                            c('Warning').t`No items`
-                        )}
-                    </span>
+                    <ItemsPlaceholder inTrash={inTrash} search={filters.search} totalCount={items.totalCount} />
                 )}
             />
         </>
