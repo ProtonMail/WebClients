785410ab1cea41756a4e46159cca4c91d64b534b
Edvin CANDON <edvin.candon@proton.ch>
2023-11-19 11:45:07 +0100

    Refactor `selectItemsSearchResult` selector to account for trashed parameter

--- applications/pass-extension/src/lib/hooks/useItems.ts
+++ applications/pass-extension/src/lib/hooks/useItems.ts
@@ -1,31 +1,27 @@
 import { useContext } from 'react';
 import { useSelector } from 'react-redux';
 
-import { selectItemsSearchResult, selectTrashedItemsSearchResults } from '@proton/pass/store/selectors';
+import { selectItemsSearchResult } from '@proton/pass/store/selectors';
 
 import { ItemsFilteringContext } from '../components/Context/Items/ItemsFilteringContext';
 
-export const useItems = () => {
+export const useItems = (options?: { trashed: boolean }) => {
     const filtering = useContext(ItemsFilteringContext);
     const { search, sort, type, shareId } = filtering;
 
     const matchedAndFilteredItems = useSelector(
-        selectItemsSearchResult({
-            itemType: type === '*' ? null : type,
-            search,
-            shareId,
-            sort,
-        })
+        selectItemsSearchResult(
+            options?.trashed
+                ? { search, trashed: true }
+                : {
+                      search,
+                      shareId,
+                      sort,
+                      trashed: false,
+                      type: type === '*' ? null : type,
+                  }
+        )
     );
 
     return { filtering, ...matchedAndFilteredItems };
-};
-
-export const useTrashItems = () => {
-    const filtering = useContext(ItemsFilteringContext);
-    const { search } = filtering;
-
-    const matched = useSelector(selectTrashedItemsSearchResults(search));
-
-    return { filtering, ...matched };
 };
