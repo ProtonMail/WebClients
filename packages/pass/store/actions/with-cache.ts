fd18dc39cd726510365f4d8db024c5cec0cac3ce
Edvin CANDON <edvin.candon@proton.ch>
2023-11-20 09:56:53 +0100

    Allow forcing immediate cache with `withCacheImmediate` enhancer
    - Fixes initial caching request being debounced on bootSuccess due to event loop triggers
    
    [IDTEAM-2320]

--- packages/pass/store/actions/with-cache.ts
+++ packages/pass/store/actions/with-cache.ts
@@ -2,10 +2,12 @@ import type { AnyAction } from 'redux';
 
 import { merge } from '@proton/pass/utils/object/merge';
 
-export type WithCache<T = AnyAction> = T & { meta: { cache: true } };
+export type WithCache<T = AnyAction> = T & { meta: { cache: boolean; immediate?: boolean } };
 
 export const isCachingAction = <T extends AnyAction>(action?: T): action is WithCache<T> =>
     (action as any)?.meta?.cache === true;
 
-export const withCache = <T extends object>(action: T): WithCache<T> =>
-    merge(action, { meta: { cache: true as const } });
+export const withCache = <T extends object>(action: T): WithCache<T> => merge(action, { meta: { cache: true } });
+
+export const withCacheImmediate = <T extends object>(action: T): WithCache<T> =>
+    merge(action, { meta: { cache: true, immediate: true } });
