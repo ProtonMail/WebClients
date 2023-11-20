cce9f67c1a46ff8bbc9fa2dc0f5d90d29cc5806c
Edvin CANDON <edvin.candon@proton.ch>
2023-11-20 08:20:05 +0100

    Improve timeout error in Lobby both for extension & webapp
    - Increase error timeout to 15s
    - Reset timeout error on stale status change

--- applications/pass/src/app/Views/Lobby.tsx
+++ applications/pass/src/app/Views/Lobby.tsx
@@ -1,28 +1,28 @@
-import { type FC, useCallback } from 'react';
+import { type FC } from 'react';
 
 import { LobbyContent } from '@proton/pass/components/Layout/Lobby/LobbyContent';
 import { LobbyLayout } from '@proton/pass/components/Layout/Lobby/LobbyLayout';
 import { usePassConfig } from '@proton/pass/hooks/usePassConfig';
+import { FORK_TYPE } from '@proton/shared/lib/authentication/ForkInterface';
 import { APPS } from '@proton/shared/lib/constants';
-import noop from '@proton/utils/noop';
 
 import { useAuthService } from '../Context/AuthServiceProvider';
 import { useClient } from '../Context/ClientProvider';
 
+const app = APPS.PROTONPASS;
+
 export const Lobby: FC = () => {
-    const { SSO_URL } = usePassConfig();
+    const { SSO_URL: host } = usePassConfig();
     const client = useClient();
     const authService = useAuthService();
-
-    const handleLogin = useCallback(() => authService.requestFork({ host: SSO_URL, app: APPS.PROTONPASS }), []);
 
     return (
         <LobbyLayout overlay>
             <LobbyContent
                 status={client.state.status}
-                onLogin={handleLogin}
-                onLogout={noop}
-                onRegister={noop}
+                onLogin={() => authService.requestFork({ host, app })}
+                onLogout={() => authService.logout({ soft: true })}
+                onRegister={() => authService.requestFork({ host, app, type: FORK_TYPE.SIGNUP })}
                 renderError={() => <></>}
             />
         </LobbyLayout>
