d451174a6e6476ef053f9e3466ce378723a8b3d6
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2023-11-20 12:19:47 +0100

    Setup global sidebar menu for pass web
    [IDTEAM-2087]

--- applications/pass/src/app/Views/Main.tsx
+++ applications/pass/src/app/Views/Main.tsx
@@ -1,44 +1,59 @@
 import { type FC, useCallback } from 'react';
 
 import { Button } from '@proton/atoms/Button';
+import { Hamburger } from '@proton/components';
+import { useToggle } from '@proton/components/hooks';
 import { useNavigation } from '@proton/pass/components/Core/NavigationProvider';
 import { InviteContextProvider } from '@proton/pass/components/Invite/InviteContextProvider';
 import { FadeIn } from '@proton/pass/components/Layout/Animation/FadeIn';
 import { Content } from '@proton/pass/components/Layout/Section/Content';
 import { Sidebar } from '@proton/pass/components/Layout/Section/Sidebar';
 import { PasswordContextProvider } from '@proton/pass/components/PasswordGenerator/PasswordContext';
+import { VaultActionsProvider } from '@proton/pass/components/Vault/VaultActionsProvider';
+import noop from '@proton/utils/noop';
 
 import { useAuthService } from '../Context/AuthServiceProvider';
 import { Header } from './Header/Header';
 import { ItemSwitch } from './Item/ItemSwitch';
 import { Items } from './Sidebar/Items';
+import { SidebarLayout } from './Sidebar/SidebarLayout';
+import { SidebarMenu } from './Sidebar/SidebarMenu';
 
 export const Main: FC = () => {
     const { setFilters } = useNavigation();
     const authService = useAuthService();
+    const { state: expanded, toggle } = useToggle();
 
     return (
         <InviteContextProvider onVaultCreated={useCallback((shareId) => setFilters({ selectedShareId: shareId }), [])}>
             <PasswordContextProvider initial={null}>
-                <FadeIn
-                    id="main"
-                    className="flex flex-column flex-nowrap w-full h-full overflow-hidden"
-                    key="main"
-                    delay={50}
-                >
-                    <Header />
-                    <main className="flex flex-align-items-center flex-justify-center flex-nowrap w-full h-full">
-                        <Sidebar>
-                            <Items />
-                            <Button shape="ghost" color="weak" onClick={() => authService.logout({ soft: false })}>
-                                Logout
-                            </Button>
-                        </Sidebar>
-                        <Content>
-                            <ItemSwitch />
-                        </Content>
-                    </main>
-                </FadeIn>
+                <div className="content-container flex flex-row flex-nowrap no-scroll flex-item-fluid relative w-full h-full">
+                    <SidebarLayout expanded={expanded} onToggleExpand={toggle}>
+                        <VaultActionsProvider onVaultCreated={noop} onVaultDeleted={noop}>
+                            <SidebarMenu />
+                        </VaultActionsProvider>
+                    </SidebarLayout>
+
+                    <FadeIn
+                        id="main"
+                        className="content flex flex-nowrap flex-column flex-item-fluid overflow-hidden"
+                        key="main"
+                        delay={50}
+                    >
+                        <Header hamburger={<Hamburger expanded={expanded} onToggle={toggle} />} />
+                        <main className="flex flex-align-items-center flex-justify-center flex-nowrap w-full h-full">
+                            <Sidebar>
+                                <Items />
+                                <Button shape="ghost" color="weak" onClick={() => authService.logout({ soft: false })}>
+                                    Logout
+                                </Button>
+                            </Sidebar>
+                            <Content>
+                                <ItemSwitch />
+                            </Content>
+                        </main>
+                    </FadeIn>
+                </div>
             </PasswordContextProvider>
         </InviteContextProvider>
     );
--- packages/pass/components/PasswordGenerator/PasswordContext.tsx
+++ packages/pass/components/PasswordGenerator/PasswordContext.tsx
@@ -1,10 +1,10 @@
-import type { Dispatch, SetStateAction } from 'react';
 import { type FC, createContext, useContext, useMemo, useState } from 'react';
 
 import type { UseAsyncModalHandle } from '@proton/pass/hooks/useAsyncModalHandles';
 import { useAsyncModalHandles } from '@proton/pass/hooks/useAsyncModalHandles';
 import type { GeneratePasswordOptions } from '@proton/pass/lib/password/generator';
 import type { MaybeNull } from '@proton/pass/types';
+import noop from '@proton/utils/noop';
 
 import { PasswordGeneratorModal, type BaseProps as PasswordGeneratorModalProps } from './PasswordGeneratorModal';
 import { PasswordHistoryModal } from './PasswordHistoryModal';
@@ -14,13 +14,13 @@ type ModalState = Omit<PasswordGeneratorModalProps, 'onSubmit'>;
 type PasswordContextValue = {
     options: MaybeNull<GeneratePasswordOptions>;
     generatePassword: UseAsyncModalHandle<string, ModalState>;
-    openPasswordHistory: Dispatch<SetStateAction<boolean>>;
+    openPasswordHistory: () => void;
 };
 
 const PasswordContext = createContext<PasswordContextValue>({
     options: null,
     generatePassword: async () => {},
-    openPasswordHistory: () => {},
+    openPasswordHistory: noop,
 });
 
 const getInitialModalState = (): ModalState => ({ actionLabel: '' });
