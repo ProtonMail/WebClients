af8735c4777092958d9c22b1159f214a7d0ecdc4
Flavien Bonvin <flavien.bonvin@proton.ch>
2023-11-21 09:21:16 +0100

    Update copy

--- applications/mail/src/app/components/list/snooze/components/SnoozeSpotlight.tsx
+++ applications/mail/src/app/components/list/snooze/components/SnoozeSpotlight.tsx
@@ -2,10 +2,11 @@ import React, { useRef } from 'react';
 
 import { c } from 'ttag';
 
-import { SettingsLink, Spotlight, useSpotlightShow } from '@proton/components/components';
+import { Href } from '@proton/atoms/Href';
+import { Spotlight, useSpotlightShow } from '@proton/components/components';
 import { FeatureCode } from '@proton/components/containers';
 import { useSpotlightOnFeature } from '@proton/components/hooks';
-import { APPS } from '@proton/shared/lib/constants';
+import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
 
 import useSnooze from 'proton-mail/hooks/actions/useSnooze';
 
@@ -24,16 +25,19 @@ const SnoozeSpotlight = ({ children }: Props) => {
             originalPlacement="bottom-end"
             show={show}
             onDisplayed={onDisplayed}
-            onClose={onClose}
+            onClose={(e) => {
+                e.stopPropagation();
+                onClose();
+            }}
             anchorRef={ref}
             type="new"
             content={
                 <>
-                    <p className="text-lg text-bold mb-1 mt-0">{c('Spotlight').t`Snooze is now available!`}</p>
+                    <p className="text-lg text-bold mb-1 mt-0">{c('Spotlight').t`Snooze it for later`}</p>
                     <p className="m-0">{c('Spotlight')
-                        .t`Set aside emails, and be reminded at a more convenient time.`}</p>
-                    <SettingsLink path="/auto-reply" app={APPS.PROTONMAIL}>{c('Link')
-                        .t`Set up email forwarding`}</SettingsLink>
+                        .t`Set when an email should reappear in your inbox with the new snooze feature.`}</p>
+                    <Href href={getKnowledgeBaseUrl('/snooze-emails')} onClick={(e) => e.stopPropagation()}>{c('Link')
+                        .t`Learn more`}</Href>
                 </>
             }
         >
