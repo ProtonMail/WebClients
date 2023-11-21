828940c0ccd3b192a1b981a6f27784bcce073278
Edvin CANDON <edvin.candon@proton.ch>
2023-11-21 19:56:27 +0100

    Fine-tune navigation provider and routing
    - Move selectedItem resolution to NavigationProvider
    - Handle common route matchers
    - Improve auto-selections and re-routing on filter change
    
    [IDTEAM-2087]

--- applications/pass/src/app/Views/Item/Autoselect.tsx
+++ applications/pass/src/app/Views/Item/Autoselect.tsx
@@ -1,25 +1,18 @@
 import { type FC } from 'react';
-import { Redirect, useRouteMatch } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 import { useNavigation } from '@proton/pass/components/Core/NavigationProvider';
-import {
-    getItemRoute,
-    getLocalPath,
-    getTrashRoute,
-    maybeTrash,
-    preserveSearch,
-} from '@proton/pass/components/Core/routing';
+import { getItemRoute, getLocalPath, maybeTrash, preserveSearch } from '@proton/pass/components/Core/routing';
 import { useFilteredItems } from '@proton/pass/hooks/useFilteredItems';
 
 export const Autoselect: FC = () => {
-    const inTrash = useRouteMatch(getTrashRoute()) !== null;
-    const { filters } = useNavigation();
-    const { filtered } = useFilteredItems({ ...filters, trashed: inTrash });
+    const { filters, matchTrash } = useNavigation();
+    const { filtered } = useFilteredItems({ ...filters, trashed: matchTrash });
     const autoselect = filtered[0];
 
     const to = autoselect
-        ? getItemRoute(autoselect.shareId, autoselect.itemId, inTrash)
-        : getLocalPath(maybeTrash('empty', inTrash));
+        ? getItemRoute(autoselect.shareId, autoselect.itemId, matchTrash)
+        : getLocalPath(maybeTrash('empty', matchTrash));
 
     return <Redirect exact to={preserveSearch(to)} />;
 };
