b7744ba52f5b8836ad6ad7cb55f9d90a8ebcf6ae
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-11-21 11:14:05 +0100

    Fix pass test

--- packages/pass/jest.config.js
+++ packages/pass/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     moduleDirectories: ['<rootDir>/node_modules', 'node_modules'],
     testEnvironment: './jest.env.js',
     transformIgnorePatterns: [
-        'node_modules/(?!(@proton/shared|@proton/components|@protontech/mutex-browser|pmcrypto|openpgp|@openpgp/web-stream-tools|asmcrypto.js|otpauth)/)',
+        'node_modules/(?!(@proton/shared|@proton/components|jsmimeparser|@protontech/mutex-browser|pmcrypto|openpgp|@openpgp/web-stream-tools|asmcrypto.js|otpauth)/)',
     ],
     transform: { '^.+\\.(m?js|tsx?)$': 'babel-jest' },
     reporters: ['default', ['jest-junit', { outputName: 'test-report.xml' }]],
