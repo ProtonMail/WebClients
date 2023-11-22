6eb5404e76f053aa2b13757e591ea65c32e682b0
Matthieu Bué <matthieu.bue@proton.ch>
2023-11-22 11:02:11 +0100

    Update documentation
    UXE-489

--- applications/storybook/src/stories/cssUtilities/flexbox/Flex.stories.tsx
+++ applications/storybook/src/stories/cssUtilities/flexbox/Flex.stories.tsx
@@ -17,3 +17,11 @@ export const Flex = () => (
         <em className="bg-primary p-4">em</em>
     </div>
 );
+
+export const InlineFlex = () => (
+    <div className="inline-flex rounded overflow-hidden border">
+        <div className="bg-primary p-4">div</div>
+        <span className="bg-primary p-4">span</span>
+        <em className="bg-primary p-4">em</em>
+    </div>
+);
