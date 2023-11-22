c4e60cb9115b90fae58240cfe6c04f79a3a12e06
Edvin CANDON <edvin.candon@proton.ch>
2023-11-22 21:03:10 +0100

    Fix selected shareId selection in web item new

--- applications/pass/src/app/Views/Item/ItemSwitch.tsx
+++ applications/pass/src/app/Views/Item/ItemSwitch.tsx
@@ -1,5 +1,5 @@
 import { type FC } from 'react';
-import type { RouteChildrenProps} from 'react-router-dom';
+import type { RouteChildrenProps } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
 
 import { ItemEdit } from '@proton/pass/components/Item/Containers/ItemEdit';
@@ -27,7 +27,7 @@ export const ItemSwitch: FC<RouteChildrenProps> = ({ match }) => {
                     <Switch>
                         <Route exact path={sub('(trash/)?share/:shareId/item/:itemId')} component={ItemView} />
                         <Route exact path={sub('share/:shareId/item/:itemId/edit')} component={ItemEdit} />
-                        <Route exact path={sub('item/new/:itemType')} component={ItemNew} />
+                        <Route exact path={sub('item/new/:type')} component={ItemNew} />
                         <Route exact path={sub('(trash/)?empty')} component={Empty} />
                         <Route component={Autoselect} />
                     </Switch>
