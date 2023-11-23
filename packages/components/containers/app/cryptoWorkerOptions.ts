fcc70d52dc19ce69d6b76b6728472b4a1a8206ca
larabr <7375870+larabr@users.noreply.github.com>
2023-11-23 18:38:08 +0100

    Add EdDSA faulty signature check behind feature flag
    And report errors to Sentry.
    
    CRYPTO-198

--- packages/components/containers/app/cryptoWorkerOptions.ts
+++ packages/components/containers/app/cryptoWorkerOptions.ts
@@ -1,13 +1,20 @@
 import { APPS, APP_NAMES } from '@proton/shared/lib/constants';
-import { loadCryptoWorker } from '@proton/shared/lib/helpers/setupCryptoWorker';
+import { CryptoWorkerOptions } from '@proton/shared/lib/helpers/setupCryptoWorker';
 import clamp from '@proton/utils/clamp';
 
-export const getCryptoWorkerOptions = (appName: APP_NAMES): Parameters<typeof loadCryptoWorker>[0] => {
+
+export const getCryptoWorkerOptions = (
+    appName: APP_NAMES,
+    openpgpConfigOptions: NonNullable<CryptoWorkerOptions['openpgpConfigOptions']>
+): CryptoWorkerOptions => {
     // The account and vpn app typically requires less crypto workers than others, mainly for SRP and key management.
     // This is to avoid loading too many workers to prevent load issues.
     if (appName === APPS.PROTONACCOUNT || appName === APPS.PROTONVPN_SETTINGS) {
         return {
             poolSize: clamp(navigator.hardwareConcurrency, 1, 2),
+            openpgpConfigOptions
         };
     }
+
+    return { openpgpConfigOptions };
 };
