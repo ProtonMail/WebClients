daed63ac527415baba0c6cfc10ea59780bae9445
seraph <929-seraph@users.noreply.gitlab.protontech.ch>
2023-11-23 15:20:27 +0000

    Create 'explore proton wallet' section

--- applications/wallet/src/app/components/index.ts
+++ applications/wallet/src/app/components/index.ts
@@ -2,3 +2,4 @@ export * from './layout';
 export * from './BitcoinReceiveInfoGenerator';
 export * from './BalanceOverview';
 export * from './YourWalletsSection';
+export * from './ExploreProtonWalletSection';
