82a8ccbadd279e4772e796337928ff759724a261
Edvin CANDON <edvin.candon@proton.ch>
2023-11-23 18:27:50 +0100

    Setup onboarding service in web-app
    - Integrate spotlight provider and spotlight zone in header
    
    [IDTEAM-2330]

--- applications/pass/src/app/Views/Header/Header.tsx
+++ applications/pass/src/app/Views/Header/Header.tsx
@@ -1,17 +1,32 @@
-import { type FC, type ReactElement } from 'react';
+import { type FC, type ReactElement, useEffect } from 'react';
 
 import { Header as CoreHeader } from '@proton/components';
 import { useNavigation } from '@proton/pass/components/Core/NavigationProvider';
 import { getLocalPath } from '@proton/pass/components/Core/routing';
 import { SearchBar } from '@proton/pass/components/Item/Search/SearchBar';
 import { ItemQuickActions } from '@proton/pass/components/Menu/Item/ItemQuickActions';
-import type { ItemType } from '@proton/pass/types';
+import { SpotlightContent } from '@proton/pass/components/Spotlight/SpotlightContent';
+import { useSpotlight } from '@proton/pass/components/Spotlight/SpotlightProvider';
+import { useOnboardingMessages } from '@proton/pass/hooks/useOnboardingMessages';
+import { type ItemType, OnboardingMessage } from '@proton/pass/types';
+import clsx from '@proton/utils/clsx';
+
+import { onboarding } from '../../../lib/onboarding';
 
 type Props = { hamburger?: ReactElement; searchable?: boolean; title?: string };
 
 export const Header: FC<Props> = ({ hamburger, searchable = true }) => {
     const { filters, setFilters, navigate } = useNavigation();
     const onNewItem = (type: ItemType) => navigate(getLocalPath(`item/new/${type}`));
+
+    const spotlight = useSpotlight();
+    const definitions = useOnboardingMessages();
+
+    useEffect(() => {
+        const messageType = onboarding.getMessage().message;
+        if (messageType === OnboardingMessage.PENDING_SHARE_ACCESS) spotlight.setPendingShareAccess(true);
+        else if (messageType) spotlight.setOnboardingMessage(definitions[messageType] ?? null);
+    }, []);
 
     return (
         <CoreHeader className="border-bottom h-auto p-2">
@@ -23,6 +38,16 @@ export const Header: FC<Props> = ({ hamburger, searchable = true }) => {
                         <ItemQuickActions onNewItem={onNewItem} />
                     </>
                 )}
+                <div className="flex-item-fluid-auto w-full">
+                    <div
+                        className={clsx(
+                            'pass-spotlight-panel',
+                            !spotlight.state.open && 'pass-spotlight-panel--hidden'
+                        )}
+                    >
+                        {spotlight.state.message && <SpotlightContent {...spotlight.state.message} />}
+                    </div>
+                </div>
             </div>
         </CoreHeader>
     );
