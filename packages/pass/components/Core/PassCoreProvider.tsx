04a412ef6adff813411c9a027a233436df89a694
Edvin CANDON <edvin.candon@proton.ch>
2023-11-23 18:24:22 +0100

    Refactor spotlight and onboarding to accomodate for web-app
    - Move extension specifics to PassExtensionCore
    - Move onboarding message definitions to `useOnboardingMessages` hook
    - Simplify orchestration betweem InviteProvider and SpotlightProvider
    - Refactor `useOnboardingListener`
    - Clean-up typings
    
    [IDTEAN-2330]

--- packages/pass/components/Core/PassCoreProvider.tsx
+++ packages/pass/components/Core/PassCoreProvider.tsx
@@ -4,7 +4,7 @@ import { createContext, useContext, useMemo } from 'react';
 import { ConfigProvider } from '@proton/components/containers/config';
 import type { PassConfig } from '@proton/pass/hooks/usePassConfig';
 import type { UsePeriodOtpCodeOptions } from '@proton/pass/hooks/usePeriodicOtpCode';
-import type { Maybe, MaybeNull } from '@proton/pass/types';
+import type { Maybe, MaybeNull, OnboardingMessage } from '@proton/pass/types';
 import type { TelemetryEvent } from '@proton/pass/types/data/telemetry';
 
 type PassCoreContextValue = {
@@ -12,16 +12,24 @@ type PassCoreContextValue = {
     /** In the extension: leverage worker communication to generate
      * a token. In the web-app: use the OTP utils in-place */
     generateOTP: UsePeriodOtpCodeOptions['generate'];
-    /* Defines how the item domain image URLs should be built */
+    /**  Defines how the item domain image URLs should be built */
     getDomainImageURL: (domain?: string) => Maybe<string>;
+    /** Returns the URL that should be opened when prompting for rating */
+    getRatingURL?: () => string;
     /** defines how a client handles external links.
      * In extension, this will leverage the `browser.tabs` API
      * whereas in the web-app, we can use `window.location` */
     onLink: (url: string) => void;
     /** Processes a telemetry event */
     onTelemetry: (event: TelemetryEvent) => void;
+    /** Defines side-effect when user acknowledges a message. */
+    onOnboardingAck?: (type: OnboardingMessage) => void;
+    /* Will get called when user tries to update a client manually */
+    onForceUpdate?: () => void;
     /** Open the settings view at a particular page */
     openSettings?: (page?: string) => void;
+    /** Prompts for client specific permissions */
+    promptForPermissions?: () => void;
 };
 
 const PassCoreContext = createContext<MaybeNull<PassCoreContextValue>>(null);
