56af394388bd7480b5f84920130e6a3ed4ff2a85
seraph <929-seraph@users.noreply.gitlab.protontech.ch>
2023-11-23 12:53:06 +0000

    Create your wallets section

--- applications/wallet/src/app/components/BalanceOverview/index.tsx
+++ applications/wallet/src/app/components/BalanceOverview/index.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 
 import { c } from 'ttag';
 
-import { Price } from '@proton/components/components';
 import clsx from '@proton/utils/clsx';
 
-import { Transaction, Wallet } from '../../types';
-import { satsToBitcoin, toFiat } from '../../utils';
+import { BitcoinAmount } from '../../atoms';
+import { BitcoinUnit, Transaction, Wallet } from '../../types';
 import { DoughnutChart } from '../charts/DoughnutChart';
 import { LineChart } from '../charts/LineChart';
 import { useBalanceOverview } from './useBalanceOverview';
 
 // TODO: change this when wallet settings API is ready
 const fiatCurrency = 'USD';
-const bitcoinUnit = 'BTC';
+const bitcoinUnit = BitcoinUnit.BTC;
 
 interface Props {
     wallets: Wallet[];
@@ -36,12 +35,9 @@ export const BalanceOverview = ({ wallets, transactions }: Props) => {
                 <div className="flex flex-row mr-14 mt-8">
                     <div data-testid="balance" className="mr-3 w-custom text-no-wrap" style={{ '--w-custom': '10rem' }}>
                         <p className="color-hint m-0">{c('Wallet Dashboard').t`My assets`}</p>
-                        <p className="my-0.5 text-2xl">
-                            {satsToBitcoin(totalBalance).toFixed(6)} {bitcoinUnit}
-                        </p>
-                        <Price className="color-hint m-0 text-sm" currency={fiatCurrency}>
-                            {toFiat(totalBalance).toFixed(2)}
-                        </Price>
+                        <BitcoinAmount unit={bitcoinUnit} fiat={fiatCurrency} className="my-0.5 text-2xl">
+                            {totalBalance}
+                        </BitcoinAmount>
                     </div>
 
                     <div className="h-custom flex flex-column flex-justify-center" style={{ '--h-custom': '7rem' }}>
@@ -56,17 +52,16 @@ export const BalanceOverview = ({ wallets, transactions }: Props) => {
                         style={{ '--w-custom': '10rem' }}
                     >
                         <p className="color-hint m-0">{c('Wallet Dashboard').t`Last 7 days`}</p>
-                        <p
+                        <BitcoinAmount
+                            unit={bitcoinUnit}
+                            fiat={fiatCurrency}
                             className={clsx(
                                 'my-0.5 text-2xl',
                                 last7DaysBalanceDifference < 0 ? 'color-danger' : 'color-success'
                             )}
                         >
-                            {satsToBitcoin(last7DaysBalanceDifference).toFixed(6)} {bitcoinUnit}
-                        </p>
-                        <Price className="color-hint m-0 text-sm" currency={fiatCurrency}>
-                            {toFiat(last7DaysBalanceDifference).toFixed(2)}
-                        </Price>
+                            {last7DaysBalanceDifference}
+                        </BitcoinAmount>
                     </div>
 
                     <div
--- applications/wallet/src/app/utils/bitcoin.ts
+++ applications/wallet/src/app/utils/bitcoin.ts
@@ -1,4 +1,4 @@
-import { BITCOIN } from '../constants';
+import { BITCOIN, mBITCOIN } from '../constants';
 
 export const bitcoinToSats = (btc: number) => {
     return btc * BITCOIN;
@@ -6,4 +6,8 @@ export const bitcoinToSats = (btc: number) => {
 
 export const satsToBitcoin = (sats: number) => {
     return sats / BITCOIN;
+};
+
+export const satsToMBitcoin = (sats: number) => {
+    return sats / mBITCOIN;
 };
