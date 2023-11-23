e43a8017a760821a77bc8597105a884580dbfa7f
Edvin CANDON <edvin.candon@proton.ch>
2023-11-23 18:21:49 +0100

    Move onboarding rules to shared pass code
    [IDTEAM-2330]

--- applications/pass-extension/src/app/worker/services/onboarding.ts
+++ applications/pass-extension/src/app/worker/services/onboarding.ts
@@ -1,29 +1,19 @@
-import { PASS_BF_2023_DATES } from '@proton/pass/constants';
-import { api } from '@proton/pass/lib/api/api';
 import browser from '@proton/pass/lib/globals/browser';
-import type {
-    OnboardingAck,
-    OnboardingRule,
-    OnboardingStorageData,
-    OnboardingWhen,
-} from '@proton/pass/lib/onboarding/service';
 import {
-    createOnboardingService as createCoreOnboardingService,
-    createOnboardingRule,
-} from '@proton/pass/lib/onboarding/service';
-import {
-    selectFeatureFlag,
-    selectHasRegisteredLock,
-    selectPassPlan,
-    selectUserState,
-} from '@proton/pass/store/selectors';
-import type { MaybeNull, Storage, TabId } from '@proton/pass/types';
-import { OnboardingMessage, WorkerMessageType } from '@proton/pass/types';
-import { PassFeature } from '@proton/pass/types/api/features';
-import { UserPassPlan } from '@proton/pass/types/api/plan';
+    createBlackFridayRule,
+    createPendingShareAccessRule,
+    createPermissionsRule,
+    createSecurityRule,
+    createStorageIssueRule,
+    createTrialRule,
+    createUpdateRule,
+    createUserRatingRule,
+} from '@proton/pass/lib/onboarding/rules';
+import type { OnboardingStorageData } from '@proton/pass/lib/onboarding/service';
+import { createOnboardingService as createCoreOnboardingService } from '@proton/pass/lib/onboarding/service';
+import type { Storage, TabId } from '@proton/pass/types';
+import { WorkerMessageType } from '@proton/pass/types';
 import { withPayloadLens } from '@proton/pass/utils/fp/lens';
-import { merge } from '@proton/pass/utils/object/merge';
-import { UNIX_DAY } from '@proton/pass/utils/time/constants';
 import { getEpoch } from '@proton/pass/utils/time/get-epoch';
 import noop from '@proton/utils/noop';
 
@@ -31,103 +21,24 @@ import WorkerMessageBroker from '../channel';
 import { withContext } from '../context';
 import store from '../store';
 
-/* Define the onboarding rules here :
- * - each rule must be registered on a specific `OnboardingMessage` type
- * - define an optional predicate for when to show the message
- * - order is important: we will apply the first matched rule */
-const ONBOARDING_RULES: OnboardingRule[] = [
-    createOnboardingRule({
-        message: OnboardingMessage.PENDING_SHARE_ACCESS,
-        when: (previous) => {
-            if (previous) return false;
-            const state = store.getState();
-            const newUserSharingEnabled = selectFeatureFlag(PassFeature.PassSharingNewUsers)(state);
-            const { waitingNewUserInvites } = selectUserState(state);
-            return newUserSharingEnabled && (waitingNewUserInvites ?? 0) > 0;
-        },
-    }),
-    createOnboardingRule({
-        message: OnboardingMessage.PERMISSIONS_REQUIRED,
-        when: withContext<OnboardingWhen>((ctx) => !ctx.service.activation.getPermissionsGranted()),
-    }),
-    createOnboardingRule({
-        message: OnboardingMessage.STORAGE_ISSUE,
-        when: withContext<OnboardingWhen>((ctx) => ctx.service.storage.getState().storageFull),
-    }),
-    createOnboardingRule({
-        message: OnboardingMessage.UPDATE_AVAILABLE,
-        /* keep a reference to the current available update so as
-         * not to re-prompt the user with this update if ignored */
-        onAcknowledge: withContext<OnboardingAck>((ctx, ack) =>
-            merge(ack, { extraData: { version: ctx.service.activation.getAvailableUpdate() } })
-        ),
-        when: withContext<OnboardingWhen>((ctx, previous) => {
-            const availableVersion = ctx.service.activation.getAvailableUpdate();
-            const previousAckVersion = previous?.extraData?.version as MaybeNull<string>;
-            const shouldPrompt = !previous || previousAckVersion !== availableVersion;
-
-            return availableVersion !== null && shouldPrompt;
-        }),
-    }),
-    createOnboardingRule({
-        message: OnboardingMessage.BLACK_FRIDAY_OFFER,
-        when: (previous) => {
-            const passPlan = selectPassPlan(store.getState());
-            if (passPlan === UserPassPlan.PLUS) return false;
-
-            const now = api.getState().serverTime?.getTime() ?? Date.now();
-            return !previous && now > PASS_BF_2023_DATES[0] && now < PASS_BF_2023_DATES[1];
-        },
-    }),
-    createOnboardingRule({
-        message: OnboardingMessage.TRIAL,
-        when: (previous) => {
-            const passPlan = selectPassPlan(store.getState());
-            return !previous && passPlan === UserPassPlan.TRIAL;
-        },
-    }),
-    createOnboardingRule({
-        message: OnboardingMessage.SECURE_EXTENSION,
-        when: (previous, { installedOn }) => {
-            /* should prompt only if user has no extension lock AND
-             * message was not previously acknowledged AND user has
-             * installed at least one day ago */
-            const now = getEpoch();
-            const hasLock = selectHasRegisteredLock(store.getState());
-            const shouldPrompt = !previous && now - installedOn > UNIX_DAY;
-
-            return !hasLock && shouldPrompt;
-        },
-    }),
-    createOnboardingRule({
-        message: OnboardingMessage.USER_RATING,
-        when: (previous) => {
-            const PROMPT_ITEM_COUNT = 10;
-            const { createdItemsCount } = store.getState().settings;
-            return !previous && createdItemsCount >= PROMPT_ITEM_COUNT;
-        },
-    }),
-];
-
-export const createOnboardingService = (store: Storage<OnboardingStorageData>) => {
-    const { acknowledge, init, setState, state } = createCoreOnboardingService({ store, rules: ONBOARDING_RULES });
+export const createOnboardingService = (storage: Storage<OnboardingStorageData>) => {
+    const { acknowledge, init, setState, getMessage } = createCoreOnboardingService({
+        storage,
+        rules: [
+            createPendingShareAccessRule(store),
+            createPermissionsRule(withContext((ctx) => ctx.service.activation.getPermissionsGranted())),
+            createStorageIssueRule(withContext((ctx) => ctx.service.storage.getState().storageFull)),
+            createUpdateRule(withContext((ctx) => ctx.service.activation.getAvailableUpdate())),
+            createBlackFridayRule(store),
+            createTrialRule(store),
+            createSecurityRule(store),
+            createUserRatingRule(store),
+        ],
+    });
 
     const onInstall = () => setState({ installedOn: getEpoch() });
     const onUpdate = () => setState({ updatedOn: getEpoch() });
     const reset = () => setState({ acknowledged: [] });
-
-    /* Define extra rules in the `ONBOARDING_RULES` constant :
-     * we will resolve the first message that matches the rule's
-     * `when` condition */
-    const getOnboardingMessage = () => ({
-        message: ONBOARDING_RULES.find(
-            ({ message, when }) =>
-                when?.(
-                    state.acknowledged.find((ack) => message === ack.message),
-                    state
-                )
-        )?.message,
-    });
 
     const navigateToOnboarding = async (tabId: TabId): Promise<boolean> => {
         const welcomePage = browser.runtime.getURL('/onboarding.html#/welcome');
@@ -136,7 +47,7 @@ export const createOnboardingService = (store: Storage<OnboardingStorageData>) =
     };
 
     WorkerMessageBroker.registerMessage(WorkerMessageType.ONBOARDING_ACK, withPayloadLens('message', acknowledge));
-    WorkerMessageBroker.registerMessage(WorkerMessageType.ONBOARDING_REQUEST, () => getOnboardingMessage());
+    WorkerMessageBroker.registerMessage(WorkerMessageType.ONBOARDING_REQUEST, getMessage);
 
     /* when reaching `account.proton.me/auth-ext` we want to
      * redirect the user to the welcome page iif user has logged in.
