29a26187d872bbc2be9fda40a237d94a25ede319
Jack Crompton <jack.crompton@proton.ch>
2023-11-23 14:06:07 +0000

    Add SAML configuration setup
    CP-5869

--- packages/components/containers/organization/sso/SamlAuthenticationSection.tsx
+++ packages/components/containers/organization/sso/SamlAuthenticationSection.tsx
@@ -1,72 +1,122 @@
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms/Button';
-import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
+import useSAMLConfigs from '@proton/components/hooks/useSAMLConfigs';
+import { Domain, SSO } from '@proton/shared/lib/interfaces';
 
-import { Info, InputFieldTwo, Loader, useModalState } from '../../../components';
+import { Info, InputFieldTwo, Loader, ModalStateProps, useModalState } from '../../../components';
 import { useDomains } from '../../../hooks';
 import {
     SettingsLayout,
     SettingsLayoutLeft,
     SettingsLayoutRight,
     SettingsParagraph,
-    SettingsSection,
+    SettingsSectionWide,
 } from '../../account';
 import ConfigureSamlModal from './ConfigureSamlModal';
 import DomainVerificationState from './DomainVerificationState';
+import SetupSSODomainModal from './SetupSSODomainModal';
+
+const ConfigureSamlContent = ({
+    domain,
+    ssoConfigs,
+    configureSamlModalProps,
+    setConfigureSamlModalOpen,
+    renderConfigureSamlModal,
+}: {
+    domain: Domain;
+    ssoConfigs: SSO[];
+    configureSamlModalProps: ModalStateProps;
+    setConfigureSamlModalOpen: (newValue: boolean) => void;
+    renderConfigureSamlModal: boolean | undefined;
+}) => {
+    const ssoConfigForDomain = ssoConfigs.find(({ DomainID }) => DomainID === domain.ID);
+
+    return (
+        <>
+            {renderConfigureSamlModal && <ConfigureSamlModal domain={domain} {...configureSamlModalProps} />}
+            <SettingsLayout>
+                <SettingsLayoutLeft>
+                    <label htmlFor="domainName" className="text-semibold align-top">
+                        <span className="mr-2">{c('Label').t`Allowed Domain`}</span>
+                        <Info
+                            title={c('Tooltip').t`Specify the domain which is allowed to authenticate with SAML SSO`}
+                        />
+                    </label>
+                </SettingsLayoutLeft>
+                <SettingsLayoutRight className="w-full max-w-custom" style={{ '--max-w-custom': '25rem' }}>
+                    <InputFieldTwo
+                        id="domainName"
+                        value={domain.DomainName}
+                        readOnly
+                        inputContainerClassName="bg-weak rounded w-full"
+                        unstyled
+                        assistiveText={<DomainVerificationState domain={domain} />}
+                    />
+                </SettingsLayoutRight>
+            </SettingsLayout>
+
+            {!ssoConfigForDomain && (
+                <Button
+                    color="norm"
+                    onClick={() => {
+                        setConfigureSamlModalOpen(true);
+                    }}
+                >
+                    {c('Action').t`Configure SAML`}
+                </Button>
+            )}
+        </>
+    );
+};
 
 const SamlAuthenticationSection = () => {
-    const [configureSamlModalProps, setConfigureSamlModalOpen, renderConfigureSamlModal] = useModalState();
     const [domains, loadingDomains] = useDomains();
+    const [ssoConfigs, loadingSSOConfigs] = useSAMLConfigs();
 
-    const configureSaml = () => {
-        setConfigureSamlModalOpen(true);
-    };
+    const [setupSSODomainModalProps, setSetupSSODomainModalOpen, renderSetupSSODomainModal] = useModalState();
+    const [configureSamlModalProps, setConfigureSamlModalOpen, renderConfigureSamlModal] = useModalState();
 
-    if (loadingDomains) {
+    if (loadingDomains || loadingSSOConfigs) {
         return <Loader />;
     }
 
     return (
         <>
-            {renderConfigureSamlModal && <ConfigureSamlModal {...configureSamlModalProps} />}
-            <SettingsSection>
-                <SettingsParagraph
-                    learnMoreUrl={
-                        // TODO:
-                        getKnowledgeBaseUrl('')
-                    }
-                >
+            {renderSetupSSODomainModal && (
+                <SetupSSODomainModal
+                    onContinue={() => {
+                        setupSSODomainModalProps.onClose();
+                        setConfigureSamlModalOpen(true);
+                    }}
+                    {...setupSSODomainModalProps}
+                />
+            )}
+            <SettingsSectionWide>
+                <SettingsParagraph learnMoreUrl="https://protonvpn.com/support/sso">
                     {c('Info')
                         .t`Configure SAML authentication for your organization through an identity provider like Okta, Azure or Onelogin. This will enable SAML for the whole organization.`}
                 </SettingsParagraph>
 
                 {domains.length > 0 ? (
-                    <SettingsLayout>
-                        <SettingsLayoutLeft>
-                            <label htmlFor="domainName" className="text-semibold align-top">
-                                <span className="mr-2">{c('Label').t`Allowed Domain`}</span>
-                                <Info
-                                    title={c('Tooltip')
-                                        .t`Specify the domain which is allowed to authenticate with SAML SSO`}
-                                />
-                            </label>
-                        </SettingsLayoutLeft>
-                        <SettingsLayoutRight className="w-full">
-                            <InputFieldTwo
-                                id="domainName"
-                                value={domains[0].DomainName}
-                                readOnly
-                                inputContainerClassName="bg-weak rounded w-full"
-                                unstyled
-                                assistiveText={<DomainVerificationState domain={domains[0]} />}
-                            />
-                        </SettingsLayoutRight>
-                    </SettingsLayout>
+                    <ConfigureSamlContent
+                        domain={domains[0]}
+                        ssoConfigs={ssoConfigs}
+                        configureSamlModalProps={configureSamlModalProps}
+                        setConfigureSamlModalOpen={setConfigureSamlModalOpen}
+                        renderConfigureSamlModal={renderConfigureSamlModal}
+                    />
                 ) : (
-                    <Button color="norm" onClick={configureSaml}>{c('Action').t`Configure SAML`}</Button>
+                    <Button
+                        color="weak"
+                        onClick={() => {
+                            setSetupSSODomainModalOpen(true);
+                        }}
+                    >
+                        {c('Action').t`Configure SAML`}
+                    </Button>
                 )}
-            </SettingsSection>
+            </SettingsSectionWide>
         </>
     );
 };
