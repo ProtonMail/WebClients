bccc231e84bc52a42ac3e7e8cd74e6201e4c1e19
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2023-11-23 11:25:34 +0000

    Add domain setup to vpn-setting sso configuration
    CP-6880

---
+++ packages/components/containers/organization/sso/TXTRecordModal.tsx
@@ -0,0 +1,33 @@
+import { c } from 'ttag';
+
+import { Button } from '@proton/atoms';
+import { Domain } from '@proton/shared/lib/interfaces';
+
+import {
+    ModalTwo as Modal,
+    ModalTwoContent as ModalContent,
+    ModalTwoFooter as ModalFooter,
+    ModalTwoHeader as ModalHeader,
+    ModalProps,
+} from '../../../components';
+import TXTSection from './TXTSection';
+
+interface Props extends ModalProps {
+    domain?: Domain;
+}
+
+const TXTRecordModal = ({ domain, ...rest }: Props) => {
+    return (
+        <Modal size="large" {...rest}>
+            <ModalHeader title={c('Info').t`Verify domain`} />
+            <ModalContent>
+                <TXTSection domain={domain} />
+            </ModalContent>
+            <ModalFooter className="justify-end">
+                <Button color="norm" onClick={rest.onClose}>{c('Action').t`Done`}</Button>
+            </ModalFooter>
+        </Modal>
+    );
+};
+
+export default TXTRecordModal;
