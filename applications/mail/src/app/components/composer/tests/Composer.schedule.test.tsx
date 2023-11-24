f812fa612d9d6a511e55b8b9c6877ed87e93f70b
Flavien Bonvin <flavien.bonvin@proton.ch>
2023-11-24 12:26:08 +0100

    Fix failing tests for schedule send modal

--- applications/mail/src/app/components/composer/tests/Composer.schedule.test.tsx
+++ applications/mail/src/app/components/composer/tests/Composer.schedule.test.tsx
@@ -6,7 +6,7 @@ import {
     screen,
     waitFor,
 } from '@testing-library/react';
-import { format, getUnixTime } from 'date-fns';
+import { format, getUnixTime, set } from 'date-fns';
 import { enUS } from 'date-fns/locale';
 import loudRejection from 'loud-rejection';
 
@@ -222,7 +222,9 @@ describe('Composer scheduled messages', () => {
             fireEvent.click(dropdownButton);
 
             const customDropdownButton = screen.getByTestId('composer:schedule-send:custom');
-            fireEvent.click(customDropdownButton);
+            await act(async () => {
+                fireEvent.click(customDropdownButton);
+            });
 
             expect(screen.queryByTestId('composer:schedule-send:upsell-modal')).toBeNull();
         });
@@ -283,113 +285,180 @@ describe('Composer scheduled messages', () => {
         getByTextDefault(modal, 'Subject missing');
     });
 
-    it('should open schedule send modal and change date', async () => {
-        const { composerID } = setupMessage('Subject', [user as Recipient]);
-        setupTest({ hasPaidMail: true });
-
-        const dateSpy = jest.spyOn(Date, 'now').mockImplementation(() => new Date().setHours(12).valueOf());
-
-        const { getByTestId } = await render(<Composer {...props} composerID={composerID} />, false);
-
-        const sendActions = getByTestId('composer:send-actions');
-        const dropdownButton = getByTestIdDefault(sendActions, 'composer:scheduled-send:open-dropdown');
-        fireEvent.click(dropdownButton);
-
-        const dropdown = await getDropdown();
-        const scheduledSendButton = getByTestIdDefault(dropdown, 'composer:schedule-send:custom');
-        await act(async () => {
-            fireEvent.click(scheduledSendButton);
+    describe('Scheduled send modal', () => {
+        const mockedDate = set(new Date(), {
+            hours: 7,
+            minutes: 10,
+            seconds: 0,
+            milliseconds: 0,
+            month: 0,
+            date: 1,
+            year: 2023,
         });
 
-        getByTestId('composer:schedule-send:custom-modal:title');
-        const dateInput = getByTestId('composer:schedule-date-input') as HTMLInputElement;
-        const timeInput = getByTestId('composer:schedule-time-input') as HTMLInputElement;
-
-        // Check if default date is Tomorrow, 8:00 AM
-        expect(dateInput.value).toEqual('Tomorrow');
-        expect(timeInput.value).toEqual('8:00 AM');
-
-        // format today date to change the input
-        const todayDate = format(new Date(Date.now()), 'PP', { locale: enUS });
-
-        // set date input to today
-        fireEvent.change(dateInput, { target: { value: todayDate } });
-        fireEvent.keyDown(dateInput, { key: 'Enter' });
-
-        // When choosing today with a time before the current time, the time should be set to the next available time
-        const nextAvailableTime = getMinScheduleTime(new Date()) ?? new Date();
-        const nextAvailableTimeFormatted = format(nextAvailableTime, 'p', { locale: enUS });
-        expect(dateInput.value).toEqual('Today');
-        expect(timeInput.value).toEqual(nextAvailableTimeFormatted);
-
-        const laterDate = format(addDays(new Date(Date.now()), 5), 'PP', { locale: enUS });
-
-        // set date input to 5 days later
-        fireEvent.change(dateInput, { target: { value: laterDate } });
-        fireEvent.keyDown(dateInput, { key: 'Enter' });
-
-        expect(dateInput.value).toEqual(laterDate);
-        expect(timeInput.value).toEqual(nextAvailableTimeFormatted);
-
-        dateSpy.mockRestore();
-    });
-
-    it('should disable schedule send button display if date is not valid', async () => {
-        const { composerID } = setupMessage('Subject', [user as Recipient]);
-        setupTest({ hasPaidMail: true });
-
-        const dateSpy = jest.spyOn(Date, 'now').mockImplementation(() => new Date().setHours(12).valueOf());
-
-        const { getByTestId } = await render(<Composer {...props} composerID={composerID} />, false);
-
-        const sendActions = getByTestId('composer:send-actions');
-        const dropdownButton = getByTestIdDefault(sendActions, 'composer:scheduled-send:open-dropdown');
-        fireEvent.click(dropdownButton);
-
-        const dropdown = await getDropdown();
-        const scheduledSendButton = getByTestIdDefault(dropdown, 'composer:schedule-send:custom');
-        await act(async () => {
-            fireEvent.click(scheduledSendButton);
+        beforeEach(() => {
+            jest.useFakeTimers({ now: mockedDate.getTime() });
         });
 
-        const dateInput = getByTestId('composer:schedule-date-input') as HTMLInputElement;
-        const timeInput = getByTestId('composer:schedule-time-input') as HTMLInputElement;
-        const button = getByTestId('modal-footer:set-button') as HTMLButtonElement;
+        afterEach(() => {
+            jest.useRealTimers();
+        });
 
-        // set date input to the past
-        const previousDate = format(addDays(new Date(), -5), 'PP', { locale: enUS });
+        it('should open schedule time and see tomorrow as default value', async () => {
+            const { composerID } = setupMessage('Subject', [user as Recipient]);
+            setupTest({ hasPaidMail: true });
 
-        fireEvent.change(dateInput, { target: { value: previousDate } });
-        fireEvent.keyDown(dateInput, { key: 'Enter' });
+            const { getByTestId } = await render(<Composer {...props} composerID={composerID} />, false);
 
-        expect(dateInput.value).toEqual(previousDate);
-        expect(timeInput.value).toEqual('8:00 AM');
-        expect(button.disabled).toBeTruthy();
+            const sendActions = getByTestId('composer:send-actions');
+            const dropdownButton = getByTestIdDefault(sendActions, 'composer:scheduled-send:open-dropdown');
+            fireEvent.click(dropdownButton);
 
-        // set date input too far in the future
-        const laterDate = format(addDays(new Date(), 91), 'PP', { locale: enUS });
+            const dropdown = await getDropdown();
+            const scheduledSendButton = getByTestIdDefault(dropdown, 'composer:schedule-send:custom');
+            await act(async () => {
+                fireEvent.click(scheduledSendButton);
+            });
 
-        fireEvent.change(dateInput, { target: { value: laterDate } });
-        fireEvent.keyDown(dateInput, { key: 'Enter' });
+            getByTestId('composer:schedule-send:custom-modal:title');
+            const dateInput = getByTestId('composer:schedule-date-input') as HTMLInputElement;
+            const timeInput = getByTestId('composer:schedule-time-input') as HTMLInputElement;
 
-        expect(dateInput.value).toEqual(laterDate);
-        expect(timeInput.value).toEqual('8:00 AM');
-        expect(button.disabled).toBeTruthy();
+            // Check if default date is Tomorrow, 8:00 AM
+            expect(dateInput.value).toEqual('Tomorrow');
+            expect(timeInput.value).toEqual('8:00 AM');
+        });
 
-        // set date input to today, and time in the past
-        const todayDate = format(Date.now(), 'PP', { locale: enUS });
-        const todayTime = format(addMinutes(new Date(Date.now()), -5), 'p', { locale: enUS });
+        it('should open schedule send modal and change date to today', async () => {
+            const { composerID } = setupMessage('Subject', [user as Recipient]);
+            setupTest({ hasPaidMail: true });
 
-        fireEvent.change(dateInput, { target: { value: todayDate } });
-        fireEvent.keyDown(dateInput, { key: 'Enter' });
+            const { getByTestId } = await render(<Composer {...props} composerID={composerID} />, false);
 
-        fireEvent.change(timeInput, { target: { value: todayTime } });
-        fireEvent.keyDown(timeInput, { key: 'Enter' });
+            const sendActions = getByTestId('composer:send-actions');
+            const dropdownButton = getByTestIdDefault(sendActions, 'composer:scheduled-send:open-dropdown');
+            fireEvent.click(dropdownButton);
 
-        expect(dateInput.value).toEqual('Today');
-        expect(timeInput.value).toEqual(todayTime);
-        expect(button.disabled).toBeTruthy();
+            const dropdown = await getDropdown();
+            const scheduledSendButton = getByTestIdDefault(dropdown, 'composer:schedule-send:custom');
+            await act(async () => {
+                fireEvent.click(scheduledSendButton);
+            });
 
-        dateSpy.mockRestore();
+            getByTestId('composer:schedule-send:custom-modal:title');
+            const dateInput = getByTestId('composer:schedule-date-input') as HTMLInputElement;
+
+            const timeInput = getByTestId('composer:schedule-time-input') as HTMLInputElement;
+
+            // format today date to change the input
+            const todayDate = format(new Date(Date.now()), 'PP', { locale: enUS });
+
+            // set date input to today
+            fireEvent.change(dateInput, { target: { value: todayDate } });
+            fireEvent.keyDown(dateInput, { key: 'Enter' });
+
+            // When choosing today with a time before the current time, the time should be set to the next available time
+            const nextAvailableTime = getMinScheduleTime(new Date()) ?? new Date();
+            const nextAvailableTimeFormatted = format(nextAvailableTime, 'p', { locale: enUS });
+
+            expect(dateInput.value).toEqual('Today');
+            expect(timeInput.value).toEqual(nextAvailableTimeFormatted);
+
+            const laterDate = format(addDays(new Date(Date.now()), 5), 'PP', { locale: enUS });
+
+            // set date input to 5 days later
+            fireEvent.change(dateInput, { target: { value: laterDate } });
+            fireEvent.keyDown(dateInput, { key: 'Enter' });
+
+            expect(dateInput.value).toEqual(laterDate);
+            expect(timeInput.value).toEqual(nextAvailableTimeFormatted);
+        });
+
+        it('should open schedule send modal and change date to future', async () => {
+            const { composerID } = setupMessage('Subject', [user as Recipient]);
+            setupTest({ hasPaidMail: true });
+
+            const { getByTestId } = await render(<Composer {...props} composerID={composerID} />, false);
+
+            const sendActions = getByTestId('composer:send-actions');
+            const dropdownButton = getByTestIdDefault(sendActions, 'composer:scheduled-send:open-dropdown');
+            fireEvent.click(dropdownButton);
+
+            const dropdown = await getDropdown();
+            const scheduledSendButton = getByTestIdDefault(dropdown, 'composer:schedule-send:custom');
+            await act(async () => {
+                fireEvent.click(scheduledSendButton);
+            });
+
+            getByTestId('composer:schedule-send:custom-modal:title');
+            const dateInput = getByTestId('composer:schedule-date-input') as HTMLInputElement;
+
+            const timeInput = getByTestId('composer:schedule-time-input') as HTMLInputElement;
+
+            const laterDate = format(addDays(new Date(Date.now()), 5), 'PP', { locale: enUS });
+
+            // set date input to 5 days later
+            fireEvent.change(dateInput, { target: { value: laterDate } });
+            fireEvent.keyDown(dateInput, { key: 'Enter' });
+
+            // Default time is 08:00 AM
+            expect(dateInput.value).toEqual(laterDate);
+            expect(timeInput.value).toEqual('8:00 AM');
+        });
+
+        it('should disable schedule send button display if date is not valid', async () => {
+            const { composerID } = setupMessage('Subject', [user as Recipient]);
+            setupTest({ hasPaidMail: true });
+
+            const { getByTestId } = await render(<Composer {...props} composerID={composerID} />, false);
+
+            const sendActions = getByTestId('composer:send-actions');
+            const dropdownButton = getByTestIdDefault(sendActions, 'composer:scheduled-send:open-dropdown');
+            fireEvent.click(dropdownButton);
+
+            const dropdown = await getDropdown();
+            const scheduledSendButton = getByTestIdDefault(dropdown, 'composer:schedule-send:custom');
+            await act(async () => {
+                fireEvent.click(scheduledSendButton);
+            });
+
+            const dateInput = getByTestId('composer:schedule-date-input') as HTMLInputElement;
+            const timeInput = getByTestId('composer:schedule-time-input') as HTMLInputElement;
+            const button = getByTestId('modal-footer:set-button') as HTMLButtonElement;
+
+            // set date input to the past
+            const previousDate = format(addDays(new Date(), -5), 'PP', { locale: enUS });
+
+            fireEvent.change(dateInput, { target: { value: previousDate } });
+            fireEvent.keyDown(dateInput, { key: 'Enter' });
+
+            expect(dateInput.value).toEqual(previousDate);
+            expect(timeInput.value).toEqual('8:00 AM');
+            expect(button.disabled).toBeTruthy();
+
+            // set date input too far in the future
+            const laterDate = format(addDays(new Date(), 91), 'PP', { locale: enUS });
+
+            fireEvent.change(dateInput, { target: { value: laterDate } });
+            fireEvent.keyDown(dateInput, { key: 'Enter' });
+
+            expect(dateInput.value).toEqual(laterDate);
+            expect(timeInput.value).toEqual('8:00 AM');
+            expect(button.disabled).toBeTruthy();
+
+            // set date input to today, and time in the past
+            const todayDate = format(Date.now(), 'PP', { locale: enUS });
+            const todayTime = format(addMinutes(new Date(Date.now()), -5), 'p', { locale: enUS });
+
+            fireEvent.change(dateInput, { target: { value: todayDate } });
+            fireEvent.keyDown(dateInput, { key: 'Enter' });
+
+            fireEvent.change(timeInput, { target: { value: todayTime } });
+            fireEvent.keyDown(timeInput, { key: 'Enter' });
+
+            expect(dateInput.value).toEqual('Today');
+            expect(timeInput.value).toEqual(todayTime);
+            expect(button.disabled).toBeTruthy();
+        });
     });
 });
