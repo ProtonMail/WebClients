5b7a826a22dc855dbe5c0119b877cd2acb338b45
Flavien Bonvin <flavien.bonvin@proton.ch>
2023-11-24 08:51:20 +0100

    Add pass in app discover and remove non-existing CSS class

--- packages/components/containers/onboarding/OnboardingDiscoverApps.tsx
+++ packages/components/containers/onboarding/OnboardingDiscoverApps.tsx
@@ -5,6 +5,7 @@ import {
     CALENDAR_SHORT_APP_NAME,
     DRIVE_SHORT_APP_NAME,
     MAIL_SHORT_APP_NAME,
+    PASS_SHORT_APP_NAME,
     VPN_SHORT_APP_NAME,
 } from '@proton/shared/lib/constants';
 import onboardingDiscover from '@proton/styles/assets/img/onboarding/discover.svg';
@@ -22,7 +23,7 @@ const OnboardingDiscoverApps = (props: Props) => {
             style={{ '--h-custom': props.maxContentHeight ?? 'auto' }}
             title={c('Onboarding Proton').t`Discover all ${BRAND_NAME} services`}
             description={c('Onboarding Proton')
-                .t`Use the app selector in the top left to access all ${BRAND_NAME} services: ${MAIL_SHORT_APP_NAME}, ${VPN_SHORT_APP_NAME}, ${CALENDAR_SHORT_APP_NAME} and ${DRIVE_SHORT_APP_NAME}.`}
+                .t`Use the app selector in the top left to access all ${BRAND_NAME} services: ${MAIL_SHORT_APP_NAME}, ${VPN_SHORT_APP_NAME}, ${CALENDAR_SHORT_APP_NAME}, ${DRIVE_SHORT_APP_NAME} and ${PASS_SHORT_APP_NAME}.`}
             img={<img src={onboardingDiscover} alt={c('Onboarding Proton').t`Discover all ${BRAND_NAME} services`} />}
             {...props}
         />
