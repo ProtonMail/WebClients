42ea437fa607dd92d57c96b79c0dcf9e055d5b4f
lpeslier <lucien.peslier@proton.ch>
2023-11-27 12:37:43 +0100

    Manage conflict with Photos upload
    - Check ContentHash duplicity
    - Add (auto) Skipped state if duplicate is found

--- packages/shared/lib/interfaces/drive/photos.ts
+++ packages/shared/lib/interfaces/drive/photos.ts
@@ -1,3 +1,5 @@
+import { LinkState } from '@proton/shared/lib/interfaces/drive/link';
+
 export interface Photo {
     LinkID: string;
     CaptureTime: number;
@@ -5,4 +7,13 @@ export interface Photo {
     Exif: string | null;
     Hash: string | null;
     ContentHash: string | null;
+}
+
+export interface DuplicatePhotosHash {
+    Hash: string;
+    ContentHash: string;
+    LinkState: LinkState;
+    ClientUID: string;
+    LinkID: string;
+    RevisionID: number;
 }
