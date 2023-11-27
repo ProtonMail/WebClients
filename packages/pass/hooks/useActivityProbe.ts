4b6bdc98e4ae648f118767258b07f9815e2694fb
Edvin CANDON <edvin.candon@proton.ch>
2023-11-27 13:08:54 +0100

    Remove extension specifics from `useActivityProbe` hook
    - Create useExtensionActivityProbe and use in IFrameContextProvider &
      ExtensionConnect providers
    - Stop probe on unmount
    
    [IDTEAM-2335]

--- packages/pass/hooks/useActivityProbe.ts
+++ packages/pass/hooks/useActivityProbe.ts
@@ -1,28 +1,25 @@
-import { useRef } from 'react';
-
-import type { MessageWithSenderFactory } from '@proton/pass/lib/extension/message';
-import { sendMessage } from '@proton/pass/lib/extension/message';
-import { WorkerMessageType } from '@proton/pass/types';
-import noop from '@proton/utils/noop';
+import { useEffect, useRef } from 'react';
 
 export type ActivityProbe = ReturnType<typeof createActivityProbe>;
 
 const ACTIVITY_PROBE_TIMEOUT = 5_000;
 
-const createActivityProbe = (messageFactory: MessageWithSenderFactory) => {
+const createActivityProbe = (onProbe: () => void) => {
     const timer: { interval?: ReturnType<typeof setInterval> } = {};
 
     const cancel = () => clearInterval(timer?.interval);
-    const ping = () => sendMessage(messageFactory({ type: WorkerMessageType.ACTIVITY_PROBE })).catch(noop);
 
     const start = () => {
         cancel();
-        timer.interval = setInterval(ping, ACTIVITY_PROBE_TIMEOUT);
-        void ping();
+        timer.interval = setInterval(onProbe, ACTIVITY_PROBE_TIMEOUT);
+        void onProbe();
     };
 
     return { start, cancel };
 };
 
-export const useActivityProbe = (messageFactory: MessageWithSenderFactory): ActivityProbe =>
-    useRef(createActivityProbe(messageFactory)).current;
+export const useActivityProbe = (onProbe: () => void): ActivityProbe => {
+    const probe = useRef(createActivityProbe(onProbe)).current;
+    useEffect(() => () => probe.cancel());
+    return probe;
+};
