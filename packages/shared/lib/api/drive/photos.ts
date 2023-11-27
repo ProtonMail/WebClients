a1c160c5624cbe66480513a8eb754edf09bec026
lpeslier <lucien.peslier@proton.ch>
2023-11-27 12:37:43 +0100

    Manage conflict with Photos upload
    - Check ContentHash duplicity
    - Add (auto) Skipped state if duplicate is found

--- packages/shared/lib/api/drive/photos.ts
+++ packages/shared/lib/api/drive/photos.ts
@@ -19,3 +19,11 @@ export const queryDeletePhotosShare = (volumeId: string, shareId: string) => ({
     method: 'delete',
     url: `drive/volumes/${volumeId}/photos/share/${shareId}`,
 });
+
+export const queryPhotosDuplicates = (volumeId: string, { nameHashes }: { nameHashes: string[] }) => ({
+    method: 'post',
+    url: `drive/volumes/${volumeId}/photos/duplicates`,
+    data: {
+        NameHashes: nameHashes,
+    },
+});
