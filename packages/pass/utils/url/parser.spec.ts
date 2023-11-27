31d1fd44b471d534f06708aca3e7775ee978e803
Dominik Jankovic <dominik.jankovic@proton.ch>
2023-11-27 21:23:16 +0100

    Short-circuit favicon fetch on reserved domains
    [IDTEAM]

--- packages/pass/utils/url/parser.spec.ts
+++ packages/pass/utils/url/parser.spec.ts
@@ -11,6 +11,7 @@ describe('URL parsers', () => {
             expect(result.isPrivate).toEqual(false);
             expect(result.isSecure).toEqual(true);
             expect(result.protocol).toEqual('https:');
+            expect(result.isUnknownOrReserved).toEqual(false);
         });
 
         test('should handle standard nested subdomain url', () => {
@@ -22,6 +23,7 @@ describe('URL parsers', () => {
             expect(result.isPrivate).toEqual(false);
             expect(result.isSecure).toEqual(true);
             expect(result.protocol).toEqual('https:');
+            expect(result.isUnknownOrReserved).toEqual(false);
         });
 
         test('should consider `www` as top-level url', () => {
@@ -33,6 +35,7 @@ describe('URL parsers', () => {
             expect(result.isPrivate).toEqual(false);
             expect(result.isSecure).toEqual(true);
             expect(result.protocol).toEqual('https:');
+            expect(result.isUnknownOrReserved).toEqual(false);
         });
 
         test('should handle private sub-domains', () => {
@@ -44,6 +47,7 @@ describe('URL parsers', () => {
             expect(result.isPrivate).toEqual(true);
             expect(result.isSecure).toEqual(true);
             expect(result.protocol).toEqual('https:');
+            expect(result.isUnknownOrReserved).toEqual(false);
         });
 
         test('should handle private nested sub-domains', () => {
@@ -55,6 +59,7 @@ describe('URL parsers', () => {
             expect(result.isPrivate).toEqual(true);
             expect(result.isSecure).toEqual(true);
             expect(result.protocol).toEqual('https:');
+            expect(result.isUnknownOrReserved).toEqual(false);
         });
 
         test('should handle non-private sub-domains', () => {
@@ -66,6 +71,7 @@ describe('URL parsers', () => {
             expect(result.isPrivate).toEqual(false);
             expect(result.isSecure).toEqual(true);
             expect(result.protocol).toEqual('https:');
+            expect(result.isUnknownOrReserved).toEqual(false);
         });
 
         test('should handle localhost', () => {
@@ -77,6 +83,7 @@ describe('URL parsers', () => {
             expect(result.isPrivate).toEqual(false);
             expect(result.isSecure).toEqual(false);
             expect(result.protocol).toEqual('http:');
+            expect(result.isUnknownOrReserved).toEqual(true);
         });
 
         test('should handle IP addresses', () => {
@@ -88,6 +95,7 @@ describe('URL parsers', () => {
             expect(result.isPrivate).toEqual(false);
             expect(result.isSecure).toEqual(false);
             expect(result.protocol).toEqual('http:');
+            expect(result.isUnknownOrReserved).toEqual(true);
         });
 
         test('should handle non-standard http protocols', () => {
@@ -99,6 +107,7 @@ describe('URL parsers', () => {
             expect(result.isPrivate).toEqual(false);
             expect(result.isSecure).toEqual(false);
             expect(result.protocol).toEqual('ftp:');
+            expect(result.isUnknownOrReserved).toEqual(false);
         });
 
         test('should return defaults if non valid url', () => {
@@ -110,6 +119,74 @@ describe('URL parsers', () => {
             expect(result.isPrivate).toEqual(false);
             expect(result.isSecure).toEqual(false);
             expect(result.protocol).toEqual(null);
+            expect(result.isUnknownOrReserved).toEqual(true);
+        });
+
+        const reservedDomains = [
+            'alt',
+            '6tisch.arpa',
+            '10.in-addr.arpa',
+            '16.172.in-addr.arpa',
+            '17.172.in-addr.arpa',
+            '18.172.in-addr.arpa',
+            '19.172.in-addr.arpa',
+            '20.172.in-addr.arpa',
+            '21.172.in-addr.arpa',
+            '22.172.in-addr.arpa',
+            '23.172.in-addr.arpa',
+            '24.172.in-addr.arpa',
+            '25.172.in-addr.arpa',
+            '26.172.in-addr.arpa',
+            '27.172.in-addr.arpa',
+            '28.172.in-addr.arpa',
+            '29.172.in-addr.arpa',
+            '30.172.in-addr.arpa',
+            '31.172.in-addr.arpa',
+            '168.192.in-addr.arpa',
+            '170.0.0.192.in-addr.arpa',
+            '171.0.0.192.in-addr.arpa',
+            'ipv4only.arpa',
+            '254.169.in-addr.arpa',
+            '8.e.f.ip6.arpa',
+            '9.e.f.ip6.arpa',
+            'a.e.f.ip6.arpa',
+            'b.e.f.ip6.arpa',
+            'home.arpa',
+            'example',
+            'example.com',
+            'example.net',
+            'example.org',
+            'invalid',
+            'intranet',
+            'internal',
+            'private',
+            'corp',
+            'home',
+            'lan',
+            'local',
+            'localhost',
+            'onion',
+            'test',
+        ];
+
+        test.each(reservedDomains)('should flag %s as unknown or reserved', (domain) => {
+            // Raw hosts
+            expect(parseUrl(domain).isUnknownOrReserved).toEqual(true);
+            expect(parseUrl(`${domain}:3000`).isUnknownOrReserved).toEqual(true);
+            expect(parseUrl(`sub.${domain}`).isUnknownOrReserved).toEqual(true);
+            expect(parseUrl(`sub.${domain}:3000`).isUnknownOrReserved).toEqual(true);
+
+            // Full origins
+            const url = `http://${domain}`;
+            expect(parseUrl(url).isUnknownOrReserved).toEqual(true);
+            expect(parseUrl(`${url}:3000`).isUnknownOrReserved).toEqual(true);
+            expect(parseUrl(`sub.${url}`).isUnknownOrReserved).toEqual(true);
+            expect(parseUrl(`sub.${url}:3000`).isUnknownOrReserved).toEqual(true);
+        });
+
+        test.each(reservedDomains)('shouldnt flag %s subdomains as unknown or reserved', (domain) => {
+            expect(parseUrl(`https://${domain}.dev`).isUnknownOrReserved).toEqual(false);
+            expect(parseUrl(`${domain}.proton.me`).isUnknownOrReserved).toEqual(false);
         });
     });
 });
