732134036373b66f631f8108b8cee70ebe648ca3
Flavien Bonvin <flavien.bonvin@proton.ch>
2023-11-28 14:37:43 +0100

    Remove fixed height in onboarding modals

--- packages/components/containers/onboarding/OnboardingDiscoverApps.tsx
+++ packages/components/containers/onboarding/OnboardingDiscoverApps.tsx
@@ -12,15 +12,9 @@ import onboardingDiscover from '@proton/styles/assets/img/onboarding/discover.sv
 
 import OnboardingContent, { Props as OnboardingContentProps } from './OnboardingContent';
 
-interface Props extends Omit<OnboardingContentProps, 'decription' | 'img'> {
-    maxContentHeight?: string;
-}
-
-const OnboardingDiscoverApps = (props: Props) => {
+const OnboardingDiscoverApps = (props: Omit<OnboardingContentProps, 'decription' | 'img'>) => {
     return (
         <OnboardingContent
-            className="h-custom"
-            style={{ '--h-custom': props.maxContentHeight ?? 'auto' }}
             title={c('Onboarding Proton').t`Discover all ${BRAND_NAME} services`}
             description={c('Onboarding Proton')
                 .t`Use the app selector in the top left to access all ${BRAND_NAME} services: ${MAIL_SHORT_APP_NAME}, ${VPN_SHORT_APP_NAME}, ${CALENDAR_SHORT_APP_NAME}, ${DRIVE_SHORT_APP_NAME} and ${PASS_SHORT_APP_NAME}.`}
