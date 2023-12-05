bb92824bfb9286c101918f7bf67f6d78c7c39aed
lpeslier <lucien.peslier@proton.ch>
2023-12-05 12:07:50 +0100

    Add coming soon for ios app

--- applications/drive/src/app/components/sections/Photos/EmptyPhotos.tsx
+++ applications/drive/src/app/components/sections/Photos/EmptyPhotos.tsx
@@ -4,10 +4,11 @@ import { c } from 'ttag';
 
 import { Href } from '@proton/atoms';
 import { DRIVE_APP_NAME } from '@proton/shared/lib/constants';
-import { DRIVE_ANDROID_APP, DRIVE_IOS_APP } from '@proton/shared/lib/drive/urls';
+import { DRIVE_ANDROID_APP } from '@proton/shared/lib/drive/urls';
 import appStoreSvg from '@proton/styles/assets/img/illustrations/app-store.svg';
 import emptyPhotosSvg from '@proton/styles/assets/img/illustrations/empty-photos.svg';
 import playStoreSvg from '@proton/styles/assets/img/illustrations/play-store.svg';
+import clsx from '@proton/utils/clsx';
 
 import { DriveEmptyView } from '../../layout/DriveEmptyView';
 
@@ -26,16 +27,23 @@ export const EmptyPhotos: FC<Props> = () => {
                 c('Info').t`Get the app to backup photos from your phone.`
             }
         >
-            <div
-                className="mt-10 flex items-center justify-center gap-4"
-                data-testid="photos-empty-page:mobile-apps"
-            >
+            <div className="mt-10 flex items-center justify-center gap-4" data-testid="photos-empty-page:mobile-apps">
                 <Href href={DRIVE_ANDROID_APP}>
                     <img width="140" src={playStoreSvg} alt={c('Info').t`${DRIVE_APP_NAME} on Play Store`} />
                 </Href>
-                <Href href={DRIVE_IOS_APP}>
-                    <img width="140" src={appStoreSvg} alt={c('Info').t`${DRIVE_APP_NAME} on App Store`} />
-                </Href>
+                <div className="relative">
+                    <div className="absolute left right top-custom" style={{ '--top-custom': '-15px' }}>
+                        <span className={clsx('bg-weak color-primary px-2 py-0.5 rounded-full text-semibold')}>{c(
+                            'Info'
+                        ).t`Coming soon`}</span>
+                    </div>
+                    <img
+                        className="opacity-30"
+                        width="140"
+                        src={appStoreSvg}
+                        alt={c('Info').t`${DRIVE_APP_NAME} on App Store`}
+                    />
+                </div>
             </div>
         </DriveEmptyView>
     );
