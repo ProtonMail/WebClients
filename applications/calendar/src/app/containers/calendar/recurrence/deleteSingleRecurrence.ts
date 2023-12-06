b0808b0eb4cc571c678cfaad4d47fca97a833f3e
Eduardo Conde Pena <econdepe@proton.ch>
2023-12-06 14:24:04 +0100

    Handle properly deletion of single cancellation attendee-side when all events are cancelled
    When deleting a single deletion in a cancelled series, there's no need to update the parent event
    with an EXDATE, as the EXDATE was already present. The API will complain if we try to do so.
    
    Notice in this case there's a competing RECURRENCE-ID and an EXDATE. This was not possible before,
    so the code needs to be adapted.
    
    CALWEB-4755

--- applications/calendar/src/app/containers/calendar/recurrence/deleteSingleRecurrence.ts
+++ applications/calendar/src/app/containers/calendar/recurrence/deleteSingleRecurrence.ts
@@ -1,9 +1,13 @@
 import { toExdate } from '@proton/shared/lib/calendar/exdate';
+import { propertyToUTCDate } from '@proton/shared/lib/calendar/vcalConverter';
 import { getIsPropertyAllDay, getPropertyTzid } from '@proton/shared/lib/calendar/vcalHelper';
 import { fromUTCDate, toUTCDate } from '@proton/shared/lib/date/timezone';
 import { VcalVeventComponent } from '@proton/shared/lib/interfaces/calendar/VcalModel';
 import uniqueBy from '@proton/utils/uniqueBy';
 
+/**
+ * Returns a new event with an EXDATE for the cancelled occurrence. If the EXDATE was already present, returns undefined
+ */
 const deleteSingleRecurrence = (component: VcalVeventComponent, localStartToExclude: Date) => {
     const { dtstart, exdate: oldExdate = [] } = component;
 
@@ -12,6 +16,14 @@ const deleteSingleRecurrence = (component: VcalVeventComponent, localStartToExcl
         getIsPropertyAllDay(dtstart),
         getPropertyTzid(dtstart)
     );
+
+    const isExistingExdate = oldExdate.some((oldExdate) => {
+        return +propertyToUTCDate(oldExdate) === +propertyToUTCDate(singleExdate);
+    });
+
+    if (isExistingExdate) {
+        return;
+    }
 
     const newExdates = uniqueBy([...oldExdate, singleExdate], (property) => {
         return +toUTCDate(property.value);
