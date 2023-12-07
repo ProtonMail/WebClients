f83bd6dc766fd05614a3e0382e98f5555b55751b
Nicolas Hoffmann <dante3333@gmail.com>
2023-12-07 10:43:03 +0100

    Made ellipsis removable from quick settings
    Some language like portuguese need more room for "Sync with system"
    
    - made ellipsis removable from quick settings
    
    L10N-2751

--- packages/components/components/drawer/views/quickSettings/QuickSettingsSectionRow.tsx
+++ packages/components/components/drawer/views/quickSettings/QuickSettingsSectionRow.tsx
@@ -1,18 +1,21 @@
 import { ReactNode } from 'react';
 
+import clsx from '@proton/utils/clsx';
+
 import './QuickSettingsSectionRow.scss';
 
 interface Props {
     label: string;
     labelInfo?: ReactNode;
     action: ReactNode;
+    ellipsisOnText?: boolean;
 }
 
-const QuickSettingsSectionRow = ({ label, labelInfo, action }: Props) => {
+const QuickSettingsSectionRow = ({ label, labelInfo, action, ellipsisOnText = true }: Props) => {
     return (
         <label className="quickSettingsSectionRow flex flex-nowrap justify-space-between items-center gap-2 w-full">
             <span className="flex items-center gap-2">
-                <span className="flex-1 text-left text-ellipsis">{label}</span>
+                <span className={clsx('flex-1 text-left', ellipsisOnText && 'text-ellipsis')}>{label}</span>
                 {labelInfo}
             </span>
             <span className="quickSettingsSectionRow-action shrink-0">{action}</span>
