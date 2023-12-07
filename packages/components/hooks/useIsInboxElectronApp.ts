cc4e3fa9b66ef1c96bf7d69cb87fa96cece00952
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2023-12-07 14:54:03 +0000

    electron app styling
    UXE-775

--- packages/components/hooks/useIsInboxElectronApp.ts
+++ packages/components/hooks/useIsInboxElectronApp.ts
@@ -1,8 +1,9 @@
 import useIsElectronApp from './useIsElectronApp';
 
 const useIsInboxElectronApp = () => {
-    const { isElectron, isElectronDisabled, isElectronOnMac } = useIsElectronApp('DisableElectronMail');
-    return { isElectron, isElectronDisabled, isElectronOnMac };
+    const { isElectron, isElectronDisabled, isElectronOnMac, isElectronOnWindows } =
+        useIsElectronApp('DisableElectronMail');
+    return { isElectron, isElectronDisabled, isElectronOnMac, isElectronOnWindows };
 };
 
 export default useIsInboxElectronApp;
