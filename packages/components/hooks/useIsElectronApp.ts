c908eabfb3907935c52e3eb2c9ca2195ffe05fb5
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2023-12-07 14:54:03 +0000

    electron app styling
    UXE-775

--- packages/components/hooks/useIsElectronApp.ts
+++ packages/components/hooks/useIsElectronApp.ts
@@ -1,14 +1,19 @@
 import { useFlag } from '@protontech/proxy-client-react';
 
-import { isElectronApp, isElectronOnMac as testIsElectronOnMac } from '@proton/shared/lib/helpers/desktop';
+import {
+    isElectronApp,
+    isElectronOnMac as testIsElectronOnMac,
+    isElectronOnWindows as testIsElectronOnWindows,
+} from '@proton/shared/lib/helpers/desktop';
 
 const useIsElectronApp = (flag: string) => {
     const isElectron = isElectronApp();
     const electronFlag = useFlag(flag);
     const isElectronDisabled = isElectron && electronFlag;
     const isElectronOnMac = testIsElectronOnMac();
+    const isElectronOnWindows = testIsElectronOnWindows();
 
-    return { isElectron, isElectronDisabled, isElectronOnMac };
+    return { isElectron, isElectronDisabled, isElectronOnMac, isElectronOnWindows };
 };
 
 export default useIsElectronApp;
