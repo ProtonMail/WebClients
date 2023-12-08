f198f9f2ffbc13d54a623e691db7b65831371978
Flavien Bonvin <flavien.bonvin@proton.ch>
2023-12-08 12:12:06 +0100

    Add method in index file instead of App

--- applications/account/src/app/index.tsx
+++ applications/account/src/app/index.tsx
@@ -1,7 +1,20 @@
 import ReactDOM from 'react-dom';
 
 import '@proton/polyfill';
+import { isElectronApp, isElectronOnMac, isElectronOnWindows } from '@proton/shared/lib/helpers/desktop';
 
 import App from './App';
+
+if (isElectronApp()) {
+    document.body.classList.add('is-electron');
+}
+
+if (isElectronOnMac()) {
+    document.body.classList.add('is-electron-mac');
+}
+
+if (isElectronOnWindows()) {
+    document.body.classList.add('is-electron-windows');
+}
 
 ReactDOM.render(<App />, document.querySelector('.app-root'));
