eb6a8098403a05de5e9964e6a308f37ce2615016
Flavien Bonvin <flavien.bonvin@pm.me>
2023-12-10 18:40:23 +0100

    Add logs on macos uninstaller

--- src/macos/uninstall.ts
+++ src/macos/uninstall.ts
@@ -1,20 +1,25 @@
 import { exec } from "child_process";
 import { app } from "electron";
+import log from "electron-log/main";
 import { join } from "path";
 import { isMac } from "../utils/helpers";
 
 export const uninstallProton = () => {
+    log.info("Uninstalling ProtonMail");
     app.quit();
 
     const file = join(process.resourcesPath, "uninstall.sh");
+
     exec(`sh "${file}"`);
 };
 
 export const moveUninstaller = () => {
     if (!isMac) return;
 
+    log.info("Copying uninstaller to Applications folder");
     const file = join(process.resourcesPath, "Uninstall Proton Mail.app");
     if (file) {
+        log.info("Uninstaller present, copy to Applications folder");
         exec(`mv -f "${file}" /Applications`);
     }
 };
