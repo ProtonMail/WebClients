4bdd9b60736d19ccf97a3504920cafb8e97d09b0
Dominik Jankovic <dominik.jankovic@proton.ch>
2023-12-12 18:14:33 +0100

    Support payloads larger than 65kB when obfuscating (eg. notes)
    [IDTEAM]

--- packages/pass/utils/obfuscate/xor.ts
+++ packages/pass/utils/obfuscate/xor.ts
@@ -1,15 +1,18 @@
 import { stringToUtf8Array, utf8ArrayToString } from '@proton/crypto/lib/utils';
 import { base64StringToUint8Array, uint8ArrayToBase64String } from '@proton/shared/lib/helpers/encoding';
 
+// Browser-imposed limitation - maximum byte length for crypto.getRandomValues
+const MAX_ENTROPY = 65536;
+
 export type XorObfuscation = { v: string; m: string };
 
 export const obfuscate = (str: string): XorObfuscation => {
     const bytes = stringToUtf8Array(str);
-    const xor = new Uint8Array(bytes.length);
+    const xor = new Uint8Array(Math.min(bytes.length, MAX_ENTROPY));
     crypto.getRandomValues(xor);
 
     for (let i = 0, len = bytes.length; i < len; i++) {
-        bytes[i] ^= xor[i];
+        bytes[i] ^= xor[i % xor.length];
     }
 
     return {
@@ -23,7 +26,7 @@ export const deobfuscate = (obfuscation: XorObfuscation): string => {
     const xor = base64StringToUint8Array(obfuscation.m);
 
     for (var i = 0, len = data.length; i < len; i++) {
-        data[i] ^= xor[i];
+        data[i] ^= xor[i % xor.length];
     }
 
     return utf8ArrayToString(data);
