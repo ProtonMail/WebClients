914ef943cb8c7a32fa90388f8ddd831ff779f7b4
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2023-12-12 11:19:18 +0100

    Add search to parent folders

--- packages/components/containers/labels/ParentFolderSelector.tsx
+++ packages/components/containers/labels/ParentFolderSelector.tsx
@@ -1,10 +1,12 @@
+import { useMemo } from 'react';
+
 import { c } from 'ttag';
 
 import { ROOT_FOLDER } from '@proton/shared/lib/constants';
 import { buildTreeview, formatFolderName } from '@proton/shared/lib/helpers/folder';
 import { FolderWithSubFolders } from '@proton/shared/lib/interfaces/Folder';
 
-import { InputFieldTwo, Loader, Option, SelectTwo } from '../../components';
+import { InputFieldTwo, Loader, Option, SearchableSelect } from '../../components';
 import { OptionProps } from '../../components/select/Select';
 import { useFolders } from '../../hooks';
 
@@ -20,44 +22,52 @@ interface Props {
 const ParentFolderSelector = ({ id, value, label, onChange, className, disableOptions = [] }: Props) => {
     const [folders, loading] = useFolders();
 
+    const options = useMemo(() => {
+        if (loading) {
+            return [];
+        }
+
+        const formatOption = ({ Name, ID }: FolderWithSubFolders, level = 0): OptionProps => ({
+            disabled: disableOptions.includes(ID),
+            value: ID,
+            text: formatFolderName(level, Name, ' ∙ '),
+        });
+
+        const reducer = (acc: OptionProps[] = [], folder: FolderWithSubFolders, level = 0) => {
+            acc.push(formatOption(folder, level));
+
+            if (Array.isArray(folder.subfolders)) {
+                folder.subfolders.forEach((folder) => reducer(acc, folder, level + 1));
+            }
+
+            return acc;
+        };
+
+        const treeview = buildTreeview(folders);
+        return treeview.reduce<OptionProps[]>(
+            (acc, folder) => reducer(acc, folder),
+            [{ value: ROOT_FOLDER, text: c('Option').t`No parent folder` }]
+        );
+    }, [loading]);
+
     if (loading) {
         return <Loader />;
     }
 
-    const formatOption = ({ Name, ID }: FolderWithSubFolders, level = 0): OptionProps => ({
-        disabled: disableOptions.includes(ID),
-        value: ID,
-        text: formatFolderName(level, Name, ' ∙ '),
-    });
-
-    const reducer = (acc: OptionProps[] = [], folder: FolderWithSubFolders, level = 0) => {
-        acc.push(formatOption(folder, level));
-
-        if (Array.isArray(folder.subfolders)) {
-            folder.subfolders.forEach((folder) => reducer(acc, folder, level + 1));
-        }
-
-        return acc;
-    };
-
-    const treeview = buildTreeview(folders);
-    const options = treeview.reduce<OptionProps[]>(
-        (acc, folder) => reducer(acc, folder),
-        [{ value: ROOT_FOLDER, text: c('Option').t`No parent folder` }]
-    );
-
     return (
         <InputFieldTwo
-            as={SelectTwo}
-            label={label}
-            id={id}
+            as={SearchableSelect<string | number>}
             className={className}
-            value={value}
+            data-testid="parent-folder-select"
+            id={id}
+            label={label}
             onChange={({ value }: any) => onChange?.(value)}
+            placeholder={c('Placeholder').t`Search parent folder`}
+            value={value}
         >
-            {options.map((option) => {
-                return <Option key={option.value} value={option.value} title={option.text.toString()} />;
-            })}
+            {options.map((option) => (
+                <Option key={option.value} value={option.value} title={option.text.toString()} />
+            ))}
         </InputFieldTwo>
     );
 };
