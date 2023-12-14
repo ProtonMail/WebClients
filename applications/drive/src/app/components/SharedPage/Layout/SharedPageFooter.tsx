25d5fc684ce4a325443ac75b63debdb29df0f628
lpeslier <lucien.peslier@proton.ch>
2023-12-14 18:55:09 +0100

    First version of download share transfer manager

--- applications/drive/src/app/components/SharedPage/Layout/SharedPageFooter.tsx
+++ applications/drive/src/app/components/SharedPage/Layout/SharedPageFooter.tsx
@@ -1,13 +1,19 @@
 import { useActiveBreakpoint } from '@proton/components/hooks';
 
+import { useDownload } from '../../../store';
 import { DownloadButton, DownloadButtonProps } from './DownloadButton';
 import ReportAbuseButton from './ReportAbuseButton';
 
 interface Props extends DownloadButtonProps {}
 const SharedPageFooter = ({ rootItem, items }: Props) => {
     const { viewportWidth } = useActiveBreakpoint();
+    const { hasDownloads } = useDownload();
 
-    if (viewportWidth['<=small'] && items.length > 0) {
+    if (viewportWidth['<=small']) {
+        // Hide download button if transfer modal is present
+        if (hasDownloads) {
+            return null;
+        }
         return (
             <div className="fixed bottom-0 p-4 flex justify-center bg-weak w-full">
                 <DownloadButton className="flex-1" rootItem={rootItem} items={items} />
