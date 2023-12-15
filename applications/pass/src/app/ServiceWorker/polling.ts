97a84d24c2b9dfed260d85fb4baedde2a3846bbd
Edvin CANDON <edvin.candon@proton.ch>
2023-12-15 18:17:09 +0100

    Improve async lock util to allow parametrized lock
    - Reflect changes in polling and refresh locks in service worker
    
    [IDTEAM-2456]

--- applications/pass/src/app/ServiceWorker/polling.ts
+++ applications/pass/src/app/ServiceWorker/polling.ts
@@ -1,12 +1,13 @@
 import { ACTIVE_POLLING_TIMEOUT } from '@proton/pass/lib/events/constants';
-import { type AsyncLockedFunc, asyncLock } from '@proton/pass/utils/fp/promises';
+import { asyncLock } from '@proton/pass/utils/fp/promises';
 import { logger } from '@proton/pass/utils/logger';
 import { globToRegExp } from '@proton/pass/utils/url/glob';
 import noop from '@proton/utils/noop';
 
+import { getUID } from './utils';
+
 const POLLING_CACHE_KEY = 'polling';
 const POLLING_REQ_MAX_AGE = ACTIVE_POLLING_TIMEOUT;
-const POLLING_HANDLERS = new Map<string, AsyncLockedFunc<(event: FetchEvent) => Promise<Response>>>();
 
 const EVENT_ROUTES = ['/api/pass/v1/share/*/event/*', '/api/pass/v1/invite', '/api/core/v4/events/*'].map(globToRegExp);
 
@@ -20,33 +21,30 @@ const isValidEventCache = (cached: Response) => {
 export const matchPollingRoute = (pathname: string): boolean => EVENT_ROUTES.some((route) => route.test(pathname));
 export const clearPollingCache = () => caches.delete(POLLING_CACHE_KEY).catch(noop);
 
+const pollingLock = asyncLock(
+    async (event: FetchEvent): Promise<Response> => {
+        const { url } = event.request;
+
+        const cache = await caches.open(POLLING_CACHE_KEY);
+        const hit = await cache.match(url);
+
+        if (hit && isValidEventCache(hit)) {
+            logger.info(`[Polling] Cache hit for ${new URL(url).pathname.slice(0, 25)}…`);
+            return hit;
+        }
+
+        await cache.delete(url);
+
+        const response = await fetch(event.request);
+        if (response.ok && response.status === 200) cache.put(url, response.clone()).catch(noop);
+
+        return response;
+    },
+    { key: (event) => getUID(event) + event.request.url }
+);
+
 /** Event routes will be polled every `ACTIVE_POLLING_TIMEOUT` in worst case scenarios.
  * This service worker fetch handler allows handling concurrent requests to the same
  * event route through an "async lock" and also caching responses accordingly. */
-export const processPolling = async (event: FetchEvent): Promise<void> => {
-    const { url } = event.request;
-
-    const handlerForRoute = POLLING_HANDLERS.get(url);
-
-    const handler =
-        handlerForRoute ??
-        asyncLock(async (evt: FetchEvent) => {
-            const cache = await caches.open(POLLING_CACHE_KEY);
-            const hit = await cache.match(evt.request);
-
-            if (hit && isValidEventCache(hit)) {
-                logger.debug(`[Polling] Cache hit for ${new URL(url).pathname.slice(0, 25)}…`);
-                return hit;
-            }
-
-            await cache.delete(evt.request).catch(noop);
-            const response = await fetch(evt.request);
-            if (response.ok && response.status === 200) cache.put(evt.request, response.clone());
-            POLLING_HANDLERS.delete(url);
-
-            return response;
-        });
-
-    POLLING_HANDLERS.set(url, handler);
-    event.respondWith(handler(event).then((response) => response.clone()));
-};
+export const processPolling = (event: FetchEvent): void =>
+    event.respondWith(pollingLock(event).then((res) => res.clone()));
--- applications/pass/src/app/ServiceWorker/refresh.ts
+++ applications/pass/src/app/ServiceWorker/refresh.ts
@@ -1,19 +1,20 @@
-import { type AsyncLockedFunc, asyncLock } from '@proton/pass/utils/fp/promises';
+import { asyncLock } from '@proton/pass/utils/fp/promises';
 import { logger } from '@proton/pass/utils/logger';
 import { wait } from '@proton/shared/lib/helpers/promise';
+import randomIntFromInterval from '@proton/utils/randomIntFromInterval';
 
-const REFRESH_HANDLERS = new Map<string, AsyncLockedFunc<(event: FetchEvent) => Promise<Response>>>();
+import { getUID } from './utils';
 
-export const processRefresh = (event: FetchEvent) => {
-    const requestHeaders = event.request.headers;
-    const UID = requestHeaders.get('X-Pm-Uid');
+const refreshLock = asyncLock(
+    async (event: FetchEvent) => {
+        await wait(randomIntFromInterval(500, 1000));
+        return fetch(event.request);
+    },
+    { key: (event) => getUID(event)! }
+);
 
-    if (UID) {
-        const handlerForUID = REFRESH_HANDLERS.get(UID);
-        const handler = handlerForUID ?? asyncLock(async (evt: FetchEvent) => wait(500).then(() => fetch(evt.request)));
-        REFRESH_HANDLERS.set(UID, handler);
-
-        logger.info('[ServiceWorker] Refresh request intercepted');
-        event.respondWith(handler(event).then((response) => response.clone()));
-    }
+export const processRefresh = async (event: FetchEvent) => {
+    const UID = getUID(event);
+    logger.info(`[ServiceWorker] Refresh request intercepted for UID[${UID}]`);
+    if (UID) event.respondWith(refreshLock(event).then((res) => res.clone()));
 };
