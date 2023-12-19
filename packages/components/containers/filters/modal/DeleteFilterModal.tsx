1fec49ffe109ee4e97eac81c6d1ebb493296df9b
Nicolas Hoffmann <dante3333@gmail.com>
2023-12-19 18:01:31 +0100

    Fix delete modal
    Delete modal were putting the folder/label/filter name in the title, so when it's long, display is not good.
    
    On top of this, designers/copy teams don't want us to do it.
    
    - removed user-generated content from title
    - wrapped them with ellipsis in the content
    - small update of copy for folder, given by copy team in a discussion.
    
    Please note: I did not use quotes (even if a grammarNazi like me would love them), because of this:
    - in case of super long names it will give break after the quote opening
    - and given the complexity of localizing quotes (https://en.wikipedia.org/wiki/Quotation_mark#Summary_table), I went for something simpler (I can't predict the length of quotes, for some language, it might be longer).
    
    UXE-801

--- packages/components/containers/filters/modal/DeleteFilterModal.tsx
+++ packages/components/containers/filters/modal/DeleteFilterModal.tsx
@@ -17,17 +17,26 @@ const DeleteFilterModal = ({ filterName, handleDelete, ...rest }: Props) => {
         handleDelete();
     };
 
+    const nameDeleted = (
+        <strong className="text-ellipsis inline-block max-w-full align-bottom" title={filterName}>
+            {filterName}
+        </strong>
+    );
+
     return (
         <Prompt
-            title={c('Title').t`Delete ${filterName}`}
+            title={c('Title').t`Delete filter?`}
             buttons={[
                 <Button color="danger" onClick={deleteFilter}>{c('Action').t`Delete`}</Button>,
                 <Button onClick={onClose}>{c('Action').t`Cancel`}</Button>,
             ]}
             {...rest}
         >
-            {c('Info')
-                .t`Please note that if you delete this filter, we will stop processing all the automated actions it triggers.`}
+            {
+                // translator: ${nameDeleted} contain the filter name.
+                c('Info')
+                    .jt`Please note that if you delete this filter ${nameDeleted} we will stop processing all the automated actions it triggers.`
+            }
             <br />
             {c('Info').t`Are you sure you want to delete this filter?`}
         </Prompt>
