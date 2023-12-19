afe4d6ccd4beb59ade08b6c5e20e653868f79f82
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2023-12-19 17:23:10 +0100

    Check if element is visible on screen before scrolling

---
+++ applications/mail/src/app/components/message/header/constants.ts
@@ -0,0 +1,3 @@
+export const MESSAGE_LABEL_DROPDOWN_ID = 'message-header-expanded:label-dropdown';
+export const MESSAGE_FOLDER_DROPDOWN_ID = 'message-header-expanded:folder-dropdown';
+export const MESSAGE_FILTER_DROPDOWN_ID = 'message-header-expanded:filter-dropdown';
