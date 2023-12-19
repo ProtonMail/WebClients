bcb92e7dccce0c68a397328e29336e233c2a48ad
Edvin CANDON <edvin.candon@proton.ch>
2023-12-19 15:21:18 +0100

    Handle unlock anomaly when session lock is removed between clients
    [IDTEAM-2486]

--- packages/pass/lib/api/utils.ts
+++ packages/pass/lib/api/utils.ts
@@ -1,4 +1,5 @@
 export const API_BODYLESS_STATUS_CODES = [101, 204, 205, 304];
+export const SESSION_LOCK_CODE = 300008;
 
 export const getSilenced = ({ silence }: any = {}, code: string | number): boolean =>
     Array.isArray(silence) ? silence.includes(code) : !!silence;
