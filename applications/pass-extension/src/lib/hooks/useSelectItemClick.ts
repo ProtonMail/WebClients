be56a1abf69099ded6c89386f8b9ae3be8c7d4e3
Irena <irena.zazoska@proton.ch>
2023-12-21 11:12:06 +0100

    Fix telemetry item types
    [IDTEAM-2505]

--- applications/pass-extension/src/lib/hooks/useSelectItemClick.ts
+++ applications/pass-extension/src/lib/hooks/useSelectItemClick.ts
@@ -1,11 +1,11 @@
-import { type MouseEvent, useCallback } from 'react';
+import { useCallback, type MouseEvent } from 'react';
 
 import type { NavigationOptions } from 'proton-pass-extension/lib/components/Context/Navigation/NavigationContext';
 
 import { popupMessage, sendMessage } from '@proton/pass/lib/extension/message';
 import { createTelemetryEvent } from '@proton/pass/lib/telemetry/event';
-import { type ItemRevision, WorkerMessageType } from '@proton/pass/types';
-import { TelemetryEventName } from '@proton/pass/types/data/telemetry';
+import { WorkerMessageType, type ItemRevision } from '@proton/pass/types';
+import { TelemetryEventName, TelemetryItemType } from '@proton/pass/types/data/telemetry';
 import { isEmptyString } from '@proton/pass/utils/string/is-empty-string';
 
 import { useItemsFilteringContext } from './useItemsFilteringContext';
@@ -24,7 +24,7 @@ export const useSelectItemClick = () => {
                 popupMessage({
                     type: WorkerMessageType.TELEMETRY_EVENT,
                     payload: {
-                        event: createTelemetryEvent(TelemetryEventName.ItemRead, {}, { type: item.data.type }),
+                        event: createTelemetryEvent(TelemetryEventName.ItemRead, {}, { type: TelemetryItemType[item.data.type] }),
                     },
                 })
             );
@@ -35,7 +35,7 @@ export const useSelectItemClick = () => {
                     popupMessage({
                         type: WorkerMessageType.TELEMETRY_EVENT,
                         payload: {
-                            event: createTelemetryEvent(TelemetryEventName.SearchClick, {}, { type: item.data.type }),
+                            event: createTelemetryEvent(TelemetryEventName.SearchClick, {}, { type: TelemetryItemType[item.data.type] }),
                         },
                     })
                 )
