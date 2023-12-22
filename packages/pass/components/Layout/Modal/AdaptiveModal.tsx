ac7d71bda99a8dcc3980b4b044bf2c89c8a607be
Edvin CANDON <edvin.candon@proton.ch>
2023-12-22 15:30:03 +0100

    Allow overiding display type of AdapativeModal
    [IDTEAM-2513]

--- packages/pass/components/Layout/Modal/AdaptiveModal.tsx
+++ packages/pass/components/Layout/Modal/AdaptiveModal.tsx
@@ -5,27 +5,44 @@ import { c } from 'ttag';
 import { Button } from '@proton/atoms';
 import { Icon, ModalTwo, ModalTwoContent, ModalTwoFooter, ModalTwoHeader } from '@proton/components';
 import type { ModalProps } from '@proton/components/components/modalTwo/Modal';
-import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
 import { PanelHeader } from '@proton/pass/components/Layout/Panel/PanelHeader';
+import clsx from '@proton/utils/clsx';
 
+import { usePassCore } from '../../Core/PassCoreProvider';
 import { Panel } from '../Panel/Panel';
 import { SidebarModal } from './SidebarModal';
 
 import './AdaptiveModal.scss';
 
-export type SidebarModalProps = Omit<ModalProps, 'children'> & { actions?: ReactNode[] };
+export type AdapativeModalProps = Omit<ModalProps, 'children'> & {
+    actions?: ReactNode[];
+    /** only used when in `overlay` mode to show a
+     * close button defaults to true. */
+    closable?: boolean;
+    type?: 'overlay' | 'sidebar';
+};
 
 /** The AdaptiveModal dynamically adjusts its presentation based on the client environment.
  * When accessed through the extension, it renders as a sidebar modal - on web, it takes the
  * form of a conventional modal, relocating quick actions to the footer. */
-export const AdaptiveModal: FC<SidebarModalProps> = ({ children, content, actions = [], size, ...props }) => {
+export const AdaptiveModal: FC<AdapativeModalProps> = ({
+    actions = [],
+    children,
+    closable = true,
+    content,
+    size,
+    type,
+    ...props
+}) => {
     const { endpoint } = usePassCore();
 
-    return endpoint === 'web' ? (
+    return type === 'overlay' || endpoint === 'web' ? (
         <ModalTwo {...props} size={size} className="text-center">
-            <ModalTwoHeader closeButtonProps={{ pill: true, icon: true }} />
-            <ModalTwoContent className="pb-4">{children}</ModalTwoContent>
-            <ModalTwoFooter className="pass-onboarding-modal--actions flex justify-center">{actions}</ModalTwoFooter>
+            {closable && <ModalTwoHeader closeButtonProps={{ pill: true, icon: true }} />}
+            <ModalTwoContent className={clsx(!closable && 'pt-4')}>{children}</ModalTwoContent>
+            <ModalTwoFooter className="pass-onboarding-modal--actions flex flex-column justify-center">
+                {actions}
+            </ModalTwoFooter>
         </ModalTwo>
     ) : (
         <SidebarModal {...props} className="text-center">
