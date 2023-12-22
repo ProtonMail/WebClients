3f91d60388749258675d0136081a378f67d5cde9
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-12-22 14:47:59 +0100

    Add account switch to signup

---
+++ applications/account/src/app/single-signup-v2/SwitchModal.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+
+import { c } from 'ttag';
+
+import { CircleLoader } from '@proton/atoms/CircleLoader';
+import { ModalProps, ModalTwo, ModalTwoContent, ModalTwoHeader } from '@proton/components/components';
+import { useErrorHandler } from '@proton/components/hooks';
+import { LocalSessionResponse } from '@proton/shared/lib/authentication/interface';
+import { LocalSessionPersisted } from '@proton/shared/lib/authentication/persistedSessionHelper';
+import { User } from '@proton/shared/lib/interfaces';
+
+import AccountSwitcherItem from './AccountSwitcherItem';
+
+interface Props extends ModalProps {
+    onSwitchSession: (session: LocalSessionPersisted) => Promise<void>;
+    sessions: LocalSessionPersisted[] | undefined;
+    user: User | undefined;
+}
+
+const SwitchModal = ({ onSwitchSession, user, sessions = [], ...rest }: Props) => {
+    const errorHandler = useErrorHandler();
+    const [tmpUser, setTmpUser] = useState<LocalSessionResponse | null>(null);
+    return (
+        <ModalTwo size="small" {...rest} onClose={tmpUser ? undefined : rest.onClose}>
+            <ModalTwoHeader title={c('Title').t`Switch account`} titleClassName="text-4xl mb-1" />
+            <ModalTwoContent>
+                <div className="flex flex-column gap-1 pb-2">
+                    {sessions
+                        .filter((session) => session.remote.UserID !== user?.ID)
+                        .map((session) => {
+                            const user = {
+                                ID: session.remote.UserID,
+                                Name: session.remote.Username,
+                                Email: session.remote.PrimaryEmail,
+                                DisplayName: session.remote.DisplayName,
+                            };
+                            return (
+                                <AccountSwitcherItem
+                                    key={session.remote.LocalID}
+                                    border={false}
+                                    onClick={() => {
+                                        if (tmpUser) {
+                                            return;
+                                        }
+                                        const run = async () => {
+                                            setTmpUser(session.remote);
+                                            try {
+                                                await onSwitchSession(session);
+                                            } catch (e) {
+                                                errorHandler(e);
+                                            } finally {
+                                                setTmpUser(null);
+                                            }
+                                        };
+
+                                        void run();
+                                    }}
+                                    as="button"
+                                    user={user}
+                                    className="w-full interactive"
+                                    right={tmpUser?.LocalID === session.remote.LocalID ? <CircleLoader /> : undefined}
+                                />
+                            );
+                        })}
+                </div>
+            </ModalTwoContent>
+        </ModalTwo>
+    );
+};
+
+export default SwitchModal;
