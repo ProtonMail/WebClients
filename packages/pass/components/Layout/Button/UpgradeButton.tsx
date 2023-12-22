7a934f56a88d80f5274dc241709d5c674a612e4e
Edvin CANDON <edvin.candon@proton.ch>
2023-12-22 17:25:36 +0100

    Improve PersistentUpsellBar
    - Correct relative prices
    - Add limited time badge
    
    [IDTEAM-2513]

--- packages/pass/components/Layout/Button/UpgradeButton.tsx
+++ packages/pass/components/Layout/Button/UpgradeButton.tsx
@@ -1,22 +1,35 @@
-import { type VFC } from 'react';
+import type { CSSProperties, VFC } from 'react';
 
 import { c } from 'ttag';
 
-import { Button, InlineLinkButton } from '@proton/atoms';
+import { Button, type ButtonLikeSize, InlineLinkButton } from '@proton/atoms';
+import type { IconSize } from '@proton/components/components';
 import { Icon } from '@proton/components/components';
 import { type UpsellRef } from '@proton/pass/constants';
 import { useNavigateToUpgrade } from '@proton/pass/hooks/useNavigateToUpgrade';
 import clsx from '@proton/utils/clsx';
 
 type UpgradeButtonProps = {
+    buttonSize?: ButtonLikeSize;
     className?: string;
     inline?: boolean;
+    iconSize?: IconSize;
     label?: string;
     path?: string;
+    style?: CSSProperties;
     upsellRef: UpsellRef;
 };
 
-export const UpgradeButton: VFC<UpgradeButtonProps> = ({ className, inline = false, label, path, upsellRef }) => {
+export const UpgradeButton: VFC<UpgradeButtonProps> = ({
+    buttonSize,
+    className,
+    inline = false,
+    label,
+    path,
+    iconSize,
+    style,
+    upsellRef,
+}) => {
     const ButtonComponent = inline ? InlineLinkButton : Button;
     const buttonProps = { pill: true, shape: 'solid' } as const;
     const navigateToUpgrade = useNavigateToUpgrade({ upsellRef, path });
@@ -26,10 +39,12 @@ export const UpgradeButton: VFC<UpgradeButtonProps> = ({ className, inline = fal
             className={clsx('items-center flex-nowrap shrink-0', inline ? 'inline-flex' : 'flex', className)}
             color="norm"
             onClick={navigateToUpgrade}
+            size={buttonSize}
+            style={style}
             {...(!inline && buttonProps)}
         >
             {label || c('Action').t`Upgrade`}
-            <Icon className="ml-2" name="arrow-out-square" />
+            <Icon className="ml-2" name="arrow-out-square" size={iconSize} />
         </ButtonComponent>
     );
 };
