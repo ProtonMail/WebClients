6c2ce7655438eb84c09bbbf1790b7f9c491a82b4
Mattias Svanström <mattias.svanstrom@proton.ch>
2023-12-26 12:07:01 +0100

    Update srp tests

--- packages/srp/lib/getAuthVersion.test.ts
+++ packages/srp/lib/getAuthVersion.test.ts
@@ -1,6 +1,3 @@
-import assert from 'assert';
-import { describe, it } from 'mocha';
-
 import '../test/setup';
 import getAuthVersionWithFallback from './getAuthVersionWithFallback';
 
@@ -8,7 +5,7 @@ describe('auth version fallback', () => {
     for (let i = 1; i <= 4; ++i) {
         it(`should get specified auth version ${i}`, () => {
             const result = getAuthVersionWithFallback({ Version: 4 }, '');
-            assert.deepStrictEqual(result, {
+            expect(result).toEqual({
                 version: 4,
                 done: true,
             });
@@ -17,7 +14,7 @@ describe('auth version fallback', () => {
 
     it('should get specified auth version if a version has been attempted', () => {
         const result = getAuthVersionWithFallback({ Version: 4 }, '', 2);
-        assert.deepStrictEqual(result, {
+        expect(result).toEqual({
             version: 4,
             done: true,
         });
@@ -25,7 +22,7 @@ describe('auth version fallback', () => {
 
     it('should fall back to auth version 2 when it is unknown', () => {
         const result = getAuthVersionWithFallback({ Version: 0 }, '');
-        assert.deepStrictEqual(result, {
+        expect(result).toEqual({
             version: 2,
             done: false,
         });
@@ -33,7 +30,7 @@ describe('auth version fallback', () => {
 
     it('should fall back to auth version 1 when it has attempted 2 and the username contains underscore', () => {
         const result = getAuthVersionWithFallback({ Version: 0 }, 'test_100', 2);
-        assert.deepStrictEqual(result, {
+        expect(result).toEqual({
             version: 1,
             done: false,
         });
@@ -41,21 +38,18 @@ describe('auth version fallback', () => {
 
     it('should fall back to auth version 0 when it has attempted 2', () => {
         const result = getAuthVersionWithFallback({ Version: 0 }, '', 2);
-        assert.deepStrictEqual(result, {
+        expect(result).toEqual({
             version: 0,
             done: true,
         });
     });
 
     it('should throw when all attempts have been made', () => {
-        assert.throws(
-            () => {
-                getAuthVersionWithFallback({ Version: 0 }, '', 0);
-            },
-            {
-                name: 'Error',
-                message: 'Can not provide any other auth version',
-            }
-        );
+        expect(() => {
+            getAuthVersionWithFallback({ Version: 0 }, '', 0);
+        }).toThrow({
+            name: 'Error',
+            message: 'Can not provide any other auth version',
+        });
     });
 });
