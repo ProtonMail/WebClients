834112b4edebf7795b69e315765d08013f78c7c2
Edvin CANDON <edvin.candon@proton.ch>
2023-12-27 12:29:46 +0100

    Improve unlock view loading state UI

--- packages/pass/components/Lock/Unlock.tsx
+++ packages/pass/components/Lock/Unlock.tsx
@@ -1,24 +1,29 @@
-import { type VFC, useState } from 'react';
+import { type FC, useState } from 'react';
 
 import { PinCodeInput } from '@proton/pass/components/Lock/PinCodeInput';
 import { useActionRequest } from '@proton/pass/hooks/useActionRequest';
 import { useSessionLockPinSubmitEffect } from '@proton/pass/hooks/useSessionLockPinSubmitEffect';
 import { sessionUnlockIntent } from '@proton/pass/store/actions';
 import { sessionUnlockRequest } from '@proton/pass/store/actions/requests';
+import noop from '@proton/utils/noop';
 
-export const Unlock: VFC = () => {
+type Props = {
+    loading?: boolean;
+    onFailure?: () => void;
+    onStart?: () => void;
+};
+
+export const Unlock: FC<Props> = ({ loading, onFailure = noop, onStart = noop }) => {
     const [value, setValue] = useState('');
-
-    const [disabled, setDisabled] = useState(false);
 
     const unlock = useActionRequest({
         action: sessionUnlockIntent,
         initialRequestId: sessionUnlockRequest(),
-        onStart: () => setDisabled(true),
-        onFailure: () => setDisabled(false),
+        onStart,
+        onFailure,
     });
 
     useSessionLockPinSubmitEffect(value, { onSubmit: (pin) => unlock.dispatch({ pin }) });
 
-    return <PinCodeInput loading={disabled} value={value} onValue={setValue} />;
+    return <PinCodeInput loading={loading} value={value} onValue={setValue} />;
 };
