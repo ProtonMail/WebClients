2ba253d8de903e7d1cea2834a8d013002cbe5b6c
Edvin CANDON <edvin.candon@proton.ch>
2023-12-28 08:47:54 +0100

    Add `lastCalledAt` flag when using `withCallCount`
    [IDTEAM-2544]

--- packages/pass/utils/fp/with-call-count.ts
+++ packages/pass/utils/fp/with-call-count.ts
@@ -1,14 +1,25 @@
-import type { Callback } from '@proton/pass/types';
+import type { Callback, MaybeNull } from '@proton/pass/types';
 
-type WithCallCount<F extends Callback> = F & { callCount: number; resetCount: () => void };
+import { getEpoch } from '../time/get-epoch';
+
+type WithCallCount<F extends Callback> = F & {
+    /** Number of function calls since last `resetCount` call */
+    callCount: number;
+    /** Epoch in seconds of the last function call */
+    lastCalledAt: MaybeNull<number>;
+    /** Resets the internal call count */
+    resetCount: () => void;
+};
 
 export const withCallCount = <F extends Callback>(fn: F): WithCallCount<F> => {
     const wrappedFn = ((...args) => {
+        wrappedFn.lastCalledAt = getEpoch();
         wrappedFn.callCount += 1;
         return fn(...args);
     }) as WithCallCount<F>;
 
     wrappedFn.callCount = 0;
+    wrappedFn.lastCalledAt = null;
     wrappedFn.resetCount = () => (wrappedFn.callCount = 0);
 
     return wrappedFn;
