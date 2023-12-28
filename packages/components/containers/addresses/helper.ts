f6963b1f348cbd3c5d2decb74dd8ab55f4b1ffe6
Kévin Ta <kevin.ta@proton.ch>
2023-12-28 11:10:17 +0000

    MSA-39 Update helpers and address interfaces

--- packages/components/containers/addresses/helper.ts
+++ packages/components/containers/addresses/helper.ts
@@ -1,5 +1,9 @@
+import { c } from 'ttag';
+
 import {
     ADDRESS_FLAGS,
+    ADDRESS_PERMISSIONS,
+    ADDRESS_PERMISSION_TYPE,
     ADDRESS_RECEIVE,
     ADDRESS_SEND,
     ADDRESS_STATUS,
@@ -13,6 +17,7 @@ import {
     AddressConfirmationState,
     CachedOrganizationKey,
     Member,
+    PartialMemberAddress,
     UserModel,
 } from '@proton/shared/lib/interfaces';
 
@@ -148,3 +153,164 @@ export const formatAddresses = (addresses?: Address[]) => {
 
 export type AddressPermissions = ReturnType<typeof getPermissions>;
 export type AddressStatuses = ReturnType<typeof getStatus>;
+
+export interface AddressWithMemberID extends PartialMemberAddress {
+    MemberID: string;
+    MemberName?: string;
+}
+
+export interface MembersMap {
+    [MemberID: string]: Member;
+}
+
+export interface SwitchAddressPermissionMultiResponses {
+    Responses: SwitchAddressPermissionResponse[];
+}
+
+export interface SwitchAddressPermissionResponse {
+    AddressID: string;
+    Response: {
+        Code: number;
+    };
+}
+
+export interface PermissionOption {
+    text: string;
+    value: ADDRESS_PERMISSIONS;
+    testid: string;
+}
+
+export const canReceive = (permissions: number): boolean => {
+    return (
+        hasBit(permissions, ADDRESS_PERMISSIONS.PERMISSIONS_RECEIVE_ALL) ||
+        hasBit(permissions, ADDRESS_PERMISSIONS.PERMISSIONS_RECEIVE_ORG)
+    );
+};
+
+export const canSend = (permissions: number): boolean => {
+    return (
+        hasBit(permissions, ADDRESS_PERMISSIONS.PERMISSIONS_SEND_ALL) ||
+        hasBit(permissions, ADDRESS_PERMISSIONS.PERMISSIONS_SEND_ORG)
+    );
+};
+
+export const hasIncompleteSetup = (permissions: number): boolean => {
+    return !canReceive(permissions) && !canSend(permissions);
+};
+
+export const noPermissionMap = (): PermissionOption[] => {
+    return [
+        {
+            text: c('Option').t`No permission`,
+            value: ADDRESS_PERMISSIONS.NO_PERMISSION,
+            testid: 'permission-map:none',
+        },
+    ];
+};
+
+export const setupIncompletePermissionMap = (): PermissionOption[] => {
+    return [
+        {
+            text: c('Option').t`Setup incomplete`,
+            value: ADDRESS_PERMISSIONS.NO_PERMISSION,
+            testid: 'permission-map:setup-incomplete',
+        },
+    ];
+};
+
+export const permissionsReceiveMap = (): PermissionOption[] => {
+    return [
+        {
+            text: c('Option').t`Receive from all`,
+            value: ADDRESS_PERMISSIONS.PERMISSIONS_RECEIVE_ALL,
+            testid: 'permission-dropdown:receive-all',
+        },
+        {
+            text: c('Option').t`Organization only`,
+            value: ADDRESS_PERMISSIONS.PERMISSIONS_RECEIVE_ORG,
+            testid: 'permission-dropdown:receive-org',
+        },
+    ];
+};
+
+export const permissionsSendMap = (): PermissionOption[] => {
+    return [
+        {
+            text: c('Option').t`Send to all`,
+            value: ADDRESS_PERMISSIONS.PERMISSIONS_SEND_ALL,
+            testid: 'permission-dropdown:send-all',
+        },
+        {
+            text: c('Option').t`Organization only`,
+            value: ADDRESS_PERMISSIONS.PERMISSIONS_SEND_ORG,
+            testid: 'permission-dropdown:send-org',
+        },
+    ];
+};
+
+export const getReceivePermission = (permissions: number) => {
+    if (hasBit(permissions, ADDRESS_PERMISSIONS.PERMISSIONS_RECEIVE_ALL)) {
+        return ADDRESS_PERMISSIONS.PERMISSIONS_RECEIVE_ALL;
+    }
+    if (hasBit(permissions, ADDRESS_PERMISSIONS.PERMISSIONS_RECEIVE_ORG)) {
+        return ADDRESS_PERMISSIONS.PERMISSIONS_RECEIVE_ORG;
+    }
+    return ADDRESS_PERMISSIONS.NO_PERMISSION;
+};
+
+export const getSendPermission = (permissions: number) => {
+    if (hasBit(permissions, ADDRESS_PERMISSIONS.PERMISSIONS_SEND_ALL)) {
+        return ADDRESS_PERMISSIONS.PERMISSIONS_SEND_ALL;
+    }
+    if (hasBit(permissions, ADDRESS_PERMISSIONS.PERMISSIONS_SEND_ORG)) {
+        return ADDRESS_PERMISSIONS.PERMISSIONS_SEND_ORG;
+    }
+    return ADDRESS_PERMISSIONS.NO_PERMISSION;
+};
+
+export const permissionsMap = (addressPermissions: number | undefined, type: ADDRESS_PERMISSION_TYPE) => {
+    if (addressPermissions && !hasIncompleteSetup(addressPermissions)) {
+        if (
+            type === ADDRESS_PERMISSION_TYPE.RECEIVE &&
+            getReceivePermission(addressPermissions) !== ADDRESS_PERMISSIONS.NO_PERMISSION
+        ) {
+            return permissionsReceiveMap();
+        }
+        if (
+            type === ADDRESS_PERMISSION_TYPE.SEND &&
+            getSendPermission(addressPermissions) !== ADDRESS_PERMISSIONS.NO_PERMISSION
+        ) {
+            return permissionsSendMap();
+        }
+    }
+    if (!addressPermissions || hasIncompleteSetup(addressPermissions)) {
+        return setupIncompletePermissionMap();
+    }
+    return noPermissionMap();
+};
+
+export const getPermission = (permissions: number, type: ADDRESS_PERMISSION_TYPE): string => {
+    if (type === ADDRESS_PERMISSION_TYPE.RECEIVE) {
+        if (hasBit(permissions, ADDRESS_PERMISSIONS.PERMISSIONS_RECEIVE_ALL)) {
+            return c('Permission').t`Receive from all`;
+        }
+        if (hasBit(permissions, ADDRESS_PERMISSIONS.PERMISSIONS_RECEIVE_ORG)) {
+            return c('Permission').t`Organization only`;
+        }
+        if (hasIncompleteSetup(permissions)) {
+            return c('Permission').t`Setup incomplete`;
+        }
+    } else {
+        if (hasBit(permissions, ADDRESS_PERMISSIONS.PERMISSIONS_SEND_ALL)) {
+            return c('Permission').t`Send to all`;
+        }
+        if (hasBit(permissions, ADDRESS_PERMISSIONS.PERMISSIONS_SEND_ORG)) {
+            return c('Permission').t`Organization only`;
+        }
+        if (hasIncompleteSetup(permissions)) {
+            return c('Permission').t`Setup incomplete`;
+        }
+    }
+
+    return c('Permission').t`No permission`;
+};
