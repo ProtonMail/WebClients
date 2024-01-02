bfdadf909f6504487c396b785eec7cc028bb816d
Eduardo Conde Pena <econdepe@proton.ch>
2024-01-02 11:13:34 +0100

    Fix remaining duplicate translations to avoid linter warnings

--- packages/shared/lib/helpers/premium.ts
+++ packages/shared/lib/helpers/premium.ts
@@ -1,3 +1,11 @@
 import { c } from 'ttag';
 
-export const getPremium = (appName: string) => c('bf2023: Deal details').t`Premium ${appName}`;
+export const getPremium = (appName1: string, appName2?: string, appName3?: string) => {
+    if (!appName2 && !appName3) {
+        return c('specialoffer: Deal details').t`Premium ${appName1}`;
+    }
+    if (!appName3) {
+        return c('specialoffer: Deal details').t`Premium ${appName1} & ${appName2}`;
+    }
+    return c('specialoffer: Deal details').t`Premium ${appName1} & ${appName2} & ${appName3}`;
+};
