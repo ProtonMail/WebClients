0e9b768abb1cc8774019866125df069b1fe1def8
Edvin CANDON <edvin.candon@proton.ch>
2024-01-02 11:17:11 +0100

    Fix duplicate checkLock during export sequence in extension
    - Check lock status after password confirmation

--- applications/pass-extension/src/app/worker/services/export.ts
+++ applications/pass-extension/src/app/worker/services/export.ts
@@ -1,6 +1,6 @@
 import { createPassExport } from '@proton/pass/lib/export/export';
 import { selectExportData } from '@proton/pass/store/selectors/export';
-import { SessionLockStatus, WorkerMessageType } from '@proton/pass/types';
+import { WorkerMessageType } from '@proton/pass/types';
 
 import * as config from '../../config';
 import WorkerMessageBroker from '../channel';
@@ -10,10 +10,7 @@ import store from '../store';
 export const createExportService = () => {
     WorkerMessageBroker.registerMessage(
         WorkerMessageType.EXPORT_REQUEST,
-        onContextReady(async (ctx, { payload: options }) => {
-            const lock = await ctx.service.auth.checkLock();
-            if (lock.status === SessionLockStatus.LOCKED) throw Error('Session locked');
-
+        onContextReady(async (_, { payload: options }) => {
             const state = store.getState();
             const data = selectExportData({ config, encrypted: options.encrypted })(state);
             const file = await createPassExport(data, options);
