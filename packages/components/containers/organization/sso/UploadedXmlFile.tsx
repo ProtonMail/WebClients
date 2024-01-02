df028f6fad25669b07bdf8a1640ad8c4e6ddc463
Jack Crompton <jack.crompton@proton.ch>
2024-01-02 12:19:07 +0000

    Account for long file names for XML sso upload

--- packages/components/containers/organization/sso/UploadedXmlFile.tsx
+++ packages/components/containers/organization/sso/UploadedXmlFile.tsx
@@ -9,13 +9,13 @@ interface Props {
 
 const UploadedXmlFile = ({ file, onRemoveClick }: Props) => {
     return (
-        <div className="rounded border border-weak flex items-center">
-            <div className="p-1.5 flex items-center gap-2">
-                <div className="p-1 bg-strong rounded-sm flex">
+        <div className="rounded border border-weak flex flex-nowrap items-center max-w-full">
+            <div className="p-1.5 flex flex-nowrap items-center gap-2" title={file.name}>
+                <div className="p-1 bg-strong rounded-sm flex shrink-0">
                     <Icon name="code" />
                 </div>
-                <span>{file.name}</span>
-                <span className="color-weak">{shortHumanSize(file.size)}</span>
+                <span className="text-ellipsis">{file.name}</span>
+                <span className="color-weak shrink-0">{shortHumanSize(file.size)}</span>
             </div>
             <Button className="shrink-0" icon shape="ghost" onClick={onRemoveClick}>
                 <Icon name="cross-big" />
