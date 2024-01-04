74687d3c8b61a5051a9ce76160cc38d53ac72e0c
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-01-04 13:29:34 +0100

    Improve handling of mailto links in the anchor

---
+++ applications/mail/src/app/hooks/useMailtoHash.ts
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+
+import { useOnMailTo } from '../containers/ComposeProvider';
+
+/**
+ * URLs with a mailto protocol handler can be used to prefill the composer.
+ * The mailto link is in the format `#mailto=mailto:...&subject=...&body=...`
+ *
+ */
+const useMailtoHash = ({ isSearch }: { isSearch: boolean }) => {
+    const onMailTo = useOnMailTo();
+
+    useEffect(() => {
+        if (isSearch || !location.hash) {
+            return;
+        }
+
+        const { hash } = location;
+
+        try {
+            const mailtoIndex = hash.indexOf('mailto=mailto:');
+            if (mailtoIndex >= 0) {
+                // We don't want to select the #mailto= but just the mailto: part
+                const mailto = hash.substring(mailtoIndex + 'mailto='.length, hash.length);
+                onMailTo(decodeURIComponent(mailto));
+            }
+        } catch (e: any) {
+            console.error(e);
+        }
+    }, [location.hash, isSearch]);
+};
+
+export default useMailtoHash;
