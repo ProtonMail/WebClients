a3ef147bc9d0977abb70dbe9c4c7f775dfe1d857
Edvin CANDON <edvin.candon@proton.ch>
2024-01-04 16:39:45 +0100

    Factorise navigation/filtering components & utils
    - Move `ItemSwitch` to packages/pass and remove layout specifics
    - Move `Autoselect` to packages/pass for re-use in extension
    - Create `useSelectItemAction` for telemetry effects on item select click
    - Create base `Spotlight` component
    - Remove unused routing utils
    - Simplify filter resolution from location.search
    - Move filtered items resolution to `ItemsProvider`
    - Remove unnecessary empty route
    - Move vault selection logic to `VaultActionsProvider`
    
    [IDTEAM-2561]

--- packages/pass/components/Item/Containers/ItemNew.tsx
+++ packages/pass/components/Item/Containers/ItemNew.tsx
@@ -2,12 +2,12 @@ import type { VFC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 
-import { useNavigation } from '@proton/pass/components/Core/NavigationProvider';
-import type { ItemNewRouteParams } from '@proton/pass/components/Core/routing';
 import { AliasNew } from '@proton/pass/components/Item/Alias/Alias.new';
 import { CreditCardNew } from '@proton/pass/components/Item/CreditCard/CreditCard.new';
 import { LoginNew } from '@proton/pass/components/Item/Login/Login.new';
 import { NoteNew } from '@proton/pass/components/Item/Note/Note.new';
+import { useNavigation } from '@proton/pass/components/Navigation/NavigationProvider';
+import type { ItemNewRouteParams } from '@proton/pass/components/Navigation/routing';
 import type { ItemNewViewProps } from '@proton/pass/components/Views/types';
 import { itemCreationIntent } from '@proton/pass/store/actions';
 import { selectDefaultVault, selectVaultLimits } from '@proton/pass/store/selectors';
