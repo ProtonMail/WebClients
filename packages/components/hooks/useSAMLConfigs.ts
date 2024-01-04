3676f5988aada24a7906694fcfbdbf857c75a0c6
Mattias Svanström <mattias.svanstrom@proton.ch>
2024-01-04 17:25:03 +0100

    Add SAML SSO state

--- packages/components/hooks/useSAMLConfigs.ts
+++ packages/components/hooks/useSAMLConfigs.ts
@@ -1,6 +1,1 @@
-import { SSO } from '@proton/shared/lib/interfaces/SSO';
-import { SamlSSOModel } from '@proton/shared/lib/models/samlSSOModel';
-
-import createUseModelHook from './helpers/createModelHook';
-
-export default createUseModelHook<SSO[]>(SamlSSOModel);
+export { useSamlSSO, useGetSamlSSO } from '@proton/account/samlSSO/hooks';
