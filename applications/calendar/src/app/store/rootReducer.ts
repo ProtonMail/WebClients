322aa4f366b848f016495df737d141210182bc55
Eduardo Conde Pena <econdepe@proton.ch>
2024-01-04 16:25:43 +0100

    Rename helpers for holidays directory calendars

--- applications/calendar/src/app/store/rootReducer.ts
+++ applications/calendar/src/app/store/rootReducer.ts
@@ -1,11 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
-import { calendarSettingsReducer, calendarsReducer, holidayCalendarsReducer } from '@proton/calendar';
+import { calendarSettingsReducer, calendarsReducer, holidaysDirectoryReducer } from '@proton/calendar';
 import { sharedReducers } from '@proton/redux-shared-store';
 
 export const rootReducer = combineReducers({
     ...sharedReducers,
     ...calendarsReducer,
     ...calendarSettingsReducer,
-    ...holidayCalendarsReducer,
+    ...holidaysDirectoryReducer,
 });
