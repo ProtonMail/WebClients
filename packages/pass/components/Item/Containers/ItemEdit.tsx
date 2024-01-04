d0da9006ebd30675c8c4c29ee405f2c7d0ee0be3
Edvin CANDON <edvin.candon@proton.ch>
2024-01-04 16:39:45 +0100

    Factorise navigation/filtering components & utils
    - Move `ItemSwitch` to packages/pass and remove layout specifics
    - Move `Autoselect` to packages/pass for re-use in extension
    - Create `useSelectItemAction` for telemetry effects on item select click
    - Create base `Spotlight` component
    - Remove unused routing utils
    - Simplify filter resolution from location.search
    - Move filtered items resolution to `ItemsProvider`
    - Remove unnecessary empty route
    - Move vault selection logic to `VaultActionsProvider`
    
    [IDTEAM-2561]

--- packages/pass/components/Item/Containers/ItemEdit.tsx
+++ packages/pass/components/Item/Containers/ItemEdit.tsx
@@ -2,12 +2,12 @@ import { type VFC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, useParams } from 'react-router-dom';
 
-import { useNavigation } from '@proton/pass/components/Core/NavigationProvider';
-import { getLocalPath, preserveSearch } from '@proton/pass/components/Core/routing';
 import { AliasEdit } from '@proton/pass/components/Item/Alias/Alias.edit';
 import { CreditCardEdit } from '@proton/pass/components/Item/CreditCard/CreditCard.edit';
 import { LoginEdit } from '@proton/pass/components/Item/Login/Login.edit';
 import { NoteEdit } from '@proton/pass/components/Item/Note/Note.edit';
+import { useNavigation } from '@proton/pass/components/Navigation/NavigationProvider';
+import { getLocalPath, preserveSearch } from '@proton/pass/components/Navigation/routing';
 import type { ItemEditViewProps } from '@proton/pass/components/Views/types';
 import { itemEditIntent } from '@proton/pass/store/actions';
 import { selectItemByShareIdAndId, selectShare } from '@proton/pass/store/selectors';
@@ -33,16 +33,17 @@ export const ItemEdit: VFC = () => {
         selectItem(shareId, itemId, { mode: 'replace' });
     };
 
-    if (!(item && vault)) return <Redirect to={preserveSearch(getLocalPath('/'))} push={false} />;
+    if (!(item && vault)) return <Redirect to={preserveSearch(getLocalPath())} push={false} />;
 
     const EditViewComponent = itemEditMap[item.data.type] as VFC<ItemEditViewProps>;
+
     return (
         <EditViewComponent
             vault={vault}
             revision={item}
             onSubmit={handleSubmit}
             url={null}
-            onCancel={() => selectItem(shareId, itemId, { mode: 'replace' })}
+            onCancel={() => selectItem(shareId, itemId)}
         />
     );
 };
--- packages/pass/components/Navigation/authenticated-path.ts
+++
@@ -1,3 +0,0 @@
-import { stripLeadingAndTrailingSlash, stripTrailingSlash } from '@proton/shared/lib/helpers/string';
-
-export const authenticatedPath = (path: string) => stripTrailingSlash(`/u/*/${stripLeadingAndTrailingSlash(path)}`);
