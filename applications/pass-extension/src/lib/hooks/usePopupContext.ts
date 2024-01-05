726daaee02dbc82252bb26cda0b72c1a9cc56d29
Edvin CANDON <edvin.candon@proton.ch>
2024-01-05 11:19:09 +0100

    Clean-up and migrate extension lib for router migration
    - Remove extraneous hooks and context providers
    - Clean-up file structure
    
    [IDTEAM-2561]

--- applications/pass-extension/src/lib/hooks/usePopupContext.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from 'react';
-
-import { PopupContext } from 'proton-pass-extension/lib/components/Context/Popup/PopupContext';
-
-export const usePopupContext = () => useContext(PopupContext);
