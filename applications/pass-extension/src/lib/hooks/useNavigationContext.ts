da0e3f793f65e8cc547c2edfeac1199df14d8517
Edvin CANDON <edvin.candon@proton.ch>
2024-01-05 11:19:09 +0100

    Clean-up and migrate extension lib for router migration
    - Remove extraneous hooks and context providers
    - Clean-up file structure
    
    [IDTEAM-2561]

--- applications/pass-extension/src/lib/hooks/useNavigationContext.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from 'react';
-
-import { NavigationContext } from 'proton-pass-extension/lib/components/Context/Navigation/NavigationContext';
-
-export const useNavigationContext = () => useContext(NavigationContext);
