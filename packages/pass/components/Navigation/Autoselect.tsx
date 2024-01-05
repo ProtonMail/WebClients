2184c6310511d40f089a5a68f1a259e0bfbfbbdd
Dominik Jankovic <dominik.jankovic@proton.ch>
2024-01-05 13:24:19 +0100

    Move routing utils to NavigationProvider
    - Adds support for HashRouter
    
    [IDTEAM-2561]

--- packages/pass/components/Navigation/Autoselect.tsx
+++ packages/pass/components/Navigation/Autoselect.tsx
@@ -3,10 +3,10 @@ import { Redirect } from 'react-router-dom';
 
 import { useItems } from '@proton/pass/components/Item/Context/ItemsProvider';
 import { useNavigation } from '@proton/pass/components/Navigation/NavigationProvider';
-import { getItemRoute, preserveSearch } from '@proton/pass/components/Navigation/routing';
+import { getItemRoute } from '@proton/pass/components/Navigation/routing';
 
 export const Autoselect: FC = () => {
-    const { matchTrash } = useNavigation();
+    const { matchTrash, preserveSearch } = useNavigation();
     const { filtered } = useItems();
     const autoselect = filtered[0];
 
