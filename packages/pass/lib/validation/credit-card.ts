2ca8e30724567ffd2e080dffdf30b5b0b8b60546
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-01-05 13:33:37 +0100

    Display YY instead of YYYY for card expiration year
    - Change UI display as MMYY instead of MMYYYY
    - but keep storing the data as MMYYYY
    - Handle invalid card expiration date display
    - Validate card expiration date during import
    - Improve expiration date parsing for Enpass import
    
    [IDTEAM-1889]

--- packages/pass/lib/validation/credit-card.ts
+++ packages/pass/lib/validation/credit-card.ts
@@ -14,12 +14,53 @@ export type CreditCardItemFormValues = {
     note: string;
 };
 
+const isExpirationDateValid = (expirationDate: string) => {
+    if (expirationDate.length === 4) {
+        const month = parseInt(expirationDate.slice(0, 2));
+        const year = parseInt(expirationDate.slice(-2));
+        return !isNaN(month) && !isNaN(year) && month >= 1 && month <= 12 && year >= 0 && year <= 99;
+    }
+    if (expirationDate.length === 6) {
+        const month = parseInt(expirationDate.slice(0, 2));
+        const year = parseInt(expirationDate.slice(-4));
+        return !isNaN(month) && !isNaN(year) && month >= 1 && month <= 12 && year >= 0 && year <= 9999;
+    }
+    return false;
+};
+
 export const validateCreditCardForm = (values: CreditCardItemFormValues): FormikErrors<CreditCardItemFormValues> => {
     const errors: FormikErrors<CreditCardItemFormValues> = validateItemErrors(values);
 
-    if (values.expirationDate.length && values.expirationDate.length < 6) {
-        errors.expirationDate = c('Warning').t`Expiration Date is incomplete`;
+    if (values.expirationDate.length && !isExpirationDateValid(values.expirationDate)) {
+        errors.expirationDate = c('Warning').t`Expiration Date is not in the format MM/YY`;
     }
 
     return { ...errors };
+};
+
+export const expirationDateMMYY = (expirationDate: string) => {
+    if (!expirationDate) return '';
+    if (!isExpirationDateValid(expirationDate)) {
+        throw new Error('Invalid card expiration date');
+    }
+    if (expirationDate.length === 4) return expirationDate;
+
+    const month = expirationDate.slice(0, 2);
+    const year = expirationDate.slice(-2);
+
+    return `${month}${year}`;
+};
+
+export const expirationDateMMYYYY = (expirationDate: string) => {
+    if (!expirationDate) return '';
+    if (!isExpirationDateValid(expirationDate)) {
+        throw new Error('Invalid card expiration date');
+    }
+    if (expirationDate.length === 6) return expirationDate;
+
+    const currentYear = new Date().getFullYear().toString();
+    const month = expirationDate.slice(0, 2);
+    const year = expirationDate.slice(-2);
+
+    return `${month}${currentYear.slice(0, 2)}${year}`;
 };
