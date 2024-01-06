90ad5b1a10d2e36cef4f50df861404d13b3c4e2e
Edvin CANDON <edvin.candon@proton.ch>
2024-01-06 10:04:31 +0100

    Standardise item requests
    [IDTEAM-2281]

--- packages/pass/store/actions/requests.ts
+++ packages/pass/store/actions/requests.ts
@@ -4,6 +4,10 @@ export const bootRequest = () => 'worker::boot';
 export const syncRequest = () => 'worker::sync';
 export const wakeupRequest = ({ endpoint, tabId }: EndpointOptions) => `worker::wakeup-${endpoint}-${tabId}`;
 
+export const itemPinRequest = (shareId: string, itemId: string) => `item::pin::${shareId}::${itemId}`;
+export const itemUnpinRequest = (shareId: string, itemId: string) => `item::unpin::${shareId}::${itemId}`;
+export const itemsImportRequest = () => `items::import`;
+
 export const vaultCreateRequest = (optimisticId: string) => `vault::create::${optimisticId}`;
 export const vaultEditRequest = (shareId: string) => `vault::edit::${shareId}`;
 export const vaultDeleteRequest = (shareId: string) => `vault::delete::${shareId}`;
@@ -11,8 +15,6 @@ export const vaultMoveAllItemsRequest = (shareId: string) => `vault::move::all::
 export const vaultTransferOwnerRequest = (userShareId: string) => `vault::transfer:owner::${userShareId}`;
 export const trashEmptyRequest = () => `trash::empty`;
 export const trashRestoreRequest = () => `trash::restore`;
-
-export const importItemsRequest = () => `import::items`;
 
 export const sessionUnlockRequest = () => `session::unlock`;
 export const sessionLockEnableRequest = () => `session::enable`;
@@ -35,9 +37,6 @@ export const inviteRemoveRequest = (inviteId: string) => `invite::remove::${invi
 
 export const newUserInvitePromoteRequest = (newUserInviteId: string) => `new-user-invite::promote::${newUserInviteId}`;
 export const newUserInviteRemoveRequest = (newUserInviteId: string) => `new-user-invite::remove::${newUserInviteId}`;
-
-export const pinItemRequest = (itemId: string) => `pin::item::${itemId}`;
-export const unpinItemRequest = (itemId: string) => `unpin::item::${itemId}`;
 
 export const userAccessRequest = (userId: string) => `user::access::${userId}`;
 export const userFeaturesRequest = (userId: string) => `user::features::${userId}`;
