2ee7cbcc63c879eb5a71e823f42354c96f23e2cf
Edvin CANDON <edvin.candon@proton.ch>
2024-01-08 13:38:34 +0100

    Sanitize filters if selected vault does not exist

--- packages/pass/components/Navigation/Autoselect.tsx
+++ packages/pass/components/Navigation/Autoselect.tsx
@@ -1,20 +1,26 @@
 import { type FC } from 'react';
+import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
 import { useItems } from '@proton/pass/components/Item/Context/ItemsProvider';
 import { useNavigation } from '@proton/pass/components/Navigation/NavigationProvider';
-import { getItemRoute } from '@proton/pass/components/Navigation/routing';
+import { getItemRoute, getLocalPath } from '@proton/pass/components/Navigation/routing';
+import { selectShare } from '@proton/pass/store/selectors';
 
 export const Autoselect: FC = () => {
-    const { matchTrash, preserveSearch } = useNavigation();
+    const { matchTrash, preserveSearch, filters } = useNavigation();
     const { filtered } = useItems();
     const autoselect = filtered[0];
 
-    return autoselect ? (
-        <Redirect
-            exact
-            to={preserveSearch(getItemRoute(autoselect.shareId, autoselect.itemId, matchTrash))}
-            push={false}
-        />
-    ) : null;
+    /** Check if we should preserve the current filters (selected vault exists)  */
+    const selectedVault = useSelector(selectShare(filters.selectedShareId));
+    const clearFilters = filters.selectedShareId !== null && selectedVault === undefined;
+
+    const to = (() => {
+        if (autoselect) return preserveSearch(getItemRoute(autoselect.shareId, autoselect.itemId, matchTrash));
+        if (clearFilters) return getLocalPath();
+        return null;
+    })();
+
+    return to ? <Redirect exact to={to} push={false} /> : null;
 };
