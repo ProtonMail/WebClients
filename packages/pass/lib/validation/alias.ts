1847fea6e3ac4aee5f38291733a4763e2e8da21b
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-01-09 17:11:10 +0100

    Improve title field for new alias
    - remove dots at the beginning
    
    [IDTEAM-1889]

--- packages/pass/lib/validation/alias.ts
+++ packages/pass/lib/validation/alias.ts
@@ -16,13 +16,14 @@ import { validateItemErrors } from './item';
 
 /* Normalize unicode representation of the string
  * Remove diacritics (accents) + 20 max characters length
- * for the auto-derived alias name. Removes trailing dots. */
+ * for the auto-derived alias name.
+ * Removes trailing dots and dots at the beginning. */
 export const deriveAliasPrefix = (name: string) => {
     const prefix = normalize(name, true)
         .replace(/[^a-z0-9\-\_.]/g, '')
         .slice(0, 20);
 
-    return prefix.replace(/\.*$/, '');
+    return prefix.replace(/\.*$/, '').replace(/^\.+/, '');
 };
 
 export const validateAliasForm = ({
