239a89e664ddd89c0a9ed18f427ab71402baacba
Edvin CANDON <edvin.candon@proton.ch>
2024-01-11 13:41:35 +0100

    Fix upgrade link and remove all EOY references

--- packages/pass/hooks/useNavigateToUpgrade.ts
+++ packages/pass/hooks/useNavigateToUpgrade.ts
@@ -1,14 +1,20 @@
 import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
-import { getUpgradePath } from '@proton/pass/lib/onboarding/upselling';
 
-import { type UpsellRef, UpsellRefPrefix } from '../constants';
+import { PASS_UPGRADE_PATH, type UpsellRef, UpsellRefPrefix } from '../constants';
+import { authStore } from '../lib/auth/store';
 
 /** `pathRef` will be passed to the upgrade link */
 export const useNavigateToUpgrade = (options: { path?: string; upsellRef: UpsellRef }) => {
     const { onLink, config, endpoint } = usePassCore();
+    const searchParams = new URLSearchParams();
 
     const refPrefix = UpsellRefPrefix[endpoint === 'web' ? 'Web' : 'Extension'];
-    const refSearch = options?.upsellRef ? `&ref=${refPrefix}_${options.upsellRef}` : '';
+    if (options.upsellRef) searchParams.append('ref', `${refPrefix}_${options.upsellRef}`);
 
-    return () => onLink(`${config.SSO_URL}/${options.path ?? getUpgradePath()}${refSearch}`, { replace: true });
+    const localID = authStore?.getLocalID();
+    if (localID !== undefined) searchParams.append('u', localID.toString());
+
+    const upgradeHref = `${config.SSO_URL}/${options.path ?? PASS_UPGRADE_PATH}?${searchParams.toString()}`;
+
+    return () => onLink(upgradeHref, { replace: true });
 };
