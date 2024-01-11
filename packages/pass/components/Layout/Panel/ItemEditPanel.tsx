51c863e3295a7a9d19005fac16d70a82044c5f50
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-01-11 15:55:31 +0100

    Add keyboard shortcut to save item
    [IDTEAM-2214]

--- packages/pass/components/Layout/Panel/ItemEditPanel.tsx
+++ packages/pass/components/Layout/Panel/ItemEditPanel.tsx
@@ -1,9 +1,10 @@
 import { c } from 'ttag';
 
-import { Button } from '@proton/atoms';
-import { Icon } from '@proton/components';
+import { Button, Kbd } from '@proton/atoms';
+import { Icon, Tooltip } from '@proton/components';
 import { itemTypeToSubThemeClassName } from '@proton/pass/components/Layout/Theme/types';
 import type { ItemType } from '@proton/pass/types';
+import { metaKey } from '@proton/shared/lib/helpers/browser';
 
 import { DiscardableModalPanel, type DiscardableModalProps } from './DiscardableModalPanel';
 import { Panel } from './Panel';
@@ -35,18 +36,28 @@ export const ItemEditPanel = ({ type, formId, valid, discardable, handleCancelCl
                             >
                                 <Icon name="cross" alt={c('Action').t`Cancel`} />
                             </Button>,
-                            <Button
-                                className="text-sm"
+                            <Tooltip
                                 key="submit-button"
-                                pill
-                                shape="solid"
-                                color="norm"
-                                type="submit"
-                                form={formId}
-                                disabled={!valid}
+                                openDelay={500}
+                                originalPlacement={'bottom'}
+                                title={
+                                    <>
+                                        <Kbd shortcut={metaKey} /> + <Kbd shortcut="S" />
+                                    </>
+                                }
                             >
-                                {c('Action').t`Save`}
-                            </Button>,
+                                <Button
+                                    className="text-sm"
+                                    pill
+                                    shape="solid"
+                                    color="norm"
+                                    type="submit"
+                                    form={formId}
+                                    disabled={!valid}
+                                >
+                                    {c('Action').t`Save`}
+                                </Button>
+                            </Tooltip>,
                         ]}
                     />
                 }
