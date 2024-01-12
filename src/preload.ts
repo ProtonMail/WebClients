cab99688578ea0f14827196cc2dac3e8759e4d85
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-01-12 12:37:28 +0100

    Remove the desktop boolean from IPC

--- src/preload.ts
+++ src/preload.ts
@@ -2,6 +2,5 @@ import { contextBridge, ipcRenderer } from "electron";
 import { IPC_CALLS } from "./ipc/ipcConstants";
 
 contextBridge.exposeInMainWorld("protonDesktopAPI", {
-    desktop: true,
     updateNotification: (count: number) => ipcRenderer.send(IPC_CALLS.updateNotification, count),
 });
