70d6c43d99d87d442fd04dfe91bf3d562d49dd63
Jack Crompton <jack.crompton@proton.ch>
2024-01-12 12:45:09 +0100

    Add observability metrics for vpn settings SSO
    CP-7121

--- packages/metrics/lib/observeApiError.ts
+++ packages/metrics/lib/observeApiError.ts
@@ -1,4 +1,5 @@
 import { getApiError, getIsConnectionIssue } from '@proton/shared/lib/api/helpers/apiErrorHelper';
+import { API_CODES } from '@proton/shared/lib/constants';
 import { API_CUSTOM_ERROR_CODES } from '@proton/shared/lib/errors';
 
 export type MetricsApiStatusTypes = '4xx' | '5xx' | 'failure';
@@ -6,7 +7,13 @@ export type MetricsApiStatusTypes = '4xx' | '5xx' | 'failure';
 export default function observeApiError(
     error: any,
     metricObserver: (status: MetricsApiStatusTypes) => void,
-    ignoreCodes = [API_CUSTOM_ERROR_CODES.CARD_DECLINED, API_CUSTOM_ERROR_CODES.HUMAN_VERIFICATION_REQUIRED]
+    ignoreCodes = [
+        API_CUSTOM_ERROR_CODES.CARD_DECLINED,
+        API_CUSTOM_ERROR_CODES.HUMAN_VERIFICATION_REQUIRED,
+        API_CUSTOM_ERROR_CODES.INVALID_DOMAIN_NAME,
+        API_CUSTOM_ERROR_CODES.INVALID_URL,
+        API_CODES.ALREADY_EXISTS_ERROR,
+    ]
 ) {
     if (!error) {
         return;
