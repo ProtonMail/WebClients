546281217079a007adbba6dce57df29bc37e9c71
Edvin CANDON <edvin.candon@proton.ch>
2024-01-15 09:42:34 +0100

    Improve core auth service
    - Ensure we always merge the `AuthSession` with the latest session tokens
    - Set up single API listener and trigger effects accordingly
    - Leverage API listener for locked and inactive session detection
    - Remove `isPassSessionRoute` utils - prefer resetting api state instead
    - Create `AuthStore::setSession` utility for hydrating session
    - Persist session when initial forked session is locked
    
    [IDTEAM-2633]

--- packages/pass/lib/api/utils.ts
+++ packages/pass/lib/api/utils.ts
@@ -3,5 +3,3 @@ export const SESSION_LOCK_CODE = 300008;
 
 export const getSilenced = ({ silence }: any = {}, code: string | number): boolean =>
     Array.isArray(silence) ? silence.includes(code) : !!silence;
-
-export const isPassSessionRoute = (url?: string): boolean => Boolean(url?.startsWith('pass/v1/user/session/lock'));
