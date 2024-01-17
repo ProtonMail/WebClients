7321a99bd643f9c6d0a5584e45cf2922da0d2467
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-01-17 15:32:23 +0100

    Refactor extension i18n to use coreI18nService
    [IDTEAM-2384]

---
+++ packages/pass/lib/i18n/service.ts
@@ -0,0 +1,19 @@
+import { logger } from '@proton/pass/utils/logger';
+import { loadLocale } from '@proton/shared/lib/i18n/loadLocale';
+import { setTtagLocales } from '@proton/shared/lib/i18n/locales';
+import { type TtagLocaleMap } from '@proton/shared/lib/interfaces/Locale';
+import noop from '@proton/utils/noop';
+
+export const createCoreI18nService = (locales: TtagLocaleMap, getLocale: () => Promise<string>) => {
+    const setLocale = async (locale?: string) => {
+        logger.info(`[I18nService] changing locale to ${locale}`);
+        await loadLocale(locale ?? (await getLocale()), locales).catch(noop);
+    };
+
+    const init = async () => {
+        setTtagLocales(locales);
+        await setLocale(await getLocale());
+    };
+
+    return { init, setLocale };
+};
