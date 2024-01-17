62827f0e20423d7ff32ebc3dd49c014de74ef9e6
Richard Tetaz <richard.tetaz@proton.ch>
2024-01-17 17:50:59 +0100

    Wrap label functions with useCallback

--- applications/mail/src/app/hooks/actions/delete/usePermanentDelete.tsx
+++ applications/mail/src/app/hooks/actions/delete/usePermanentDelete.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+
 import { usePermanentDeleteAll } from 'proton-mail/hooks/actions/delete/usePermanentDeleteAll';
 import { usePermanentDeleteSelection } from 'proton-mail/hooks/actions/delete/usePermanentDeleteSelection';
 
@@ -5,13 +7,13 @@ export const usePermanentDelete = (labelID: string) => {
     const { handleDeleteSelection, deleteSelectionModal } = usePermanentDeleteSelection(labelID);
     const { handleDeleteAll, deleteAllModal } = usePermanentDeleteAll(labelID);
 
-    const handleDelete = async (selectedIDs: string[], selectAll?: boolean) => {
+    const handleDelete = useCallback(async (selectedIDs: string[], selectAll?: boolean) => {
         if (selectAll) {
             await handleDeleteAll(selectedIDs);
         } else {
             await handleDeleteSelection(selectedIDs);
         }
-    };
+    }, []);
 
     return { handleDelete, deleteSelectionModal, deleteAllModal };
 };
