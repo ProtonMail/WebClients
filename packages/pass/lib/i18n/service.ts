6ef4a630470f5a648eb2dd454a8b3cc43a9dc20c
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-01-17 15:42:00 +0100

    Create i18n for web app
    [IDTEAM-2384] [IDTEAM-2397]

--- packages/pass/lib/i18n/service.ts
+++ packages/pass/lib/i18n/service.ts
@@ -4,7 +4,7 @@ import { setTtagLocales } from '@proton/shared/lib/i18n/locales';
 import { type TtagLocaleMap } from '@proton/shared/lib/interfaces/Locale';
 import noop from '@proton/utils/noop';
 
-export const createCoreI18nService = (locales: TtagLocaleMap, getLocale: () => Promise<string>) => {
+export const createCoreI18nService = (locales: TtagLocaleMap, getLocale: () => Promise<string> | string) => {
     const setLocale = async (locale?: string) => {
         logger.info(`[I18nService] changing locale to ${locale}`);
         await loadLocale(locale ?? (await getLocale()), locales).catch(noop);
