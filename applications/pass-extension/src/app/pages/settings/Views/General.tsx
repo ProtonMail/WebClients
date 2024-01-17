aff01f03cb88886b46f1e85e7a2e9fdd8db41e6e
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-01-17 15:54:00 +0100

    Fix language selector default value
    [IDTEAM-2384] [IDTEAM-2397]

--- applications/pass-extension/src/app/pages/settings/Views/General.tsx
+++ applications/pass-extension/src/app/pages/settings/Views/General.tsx
@@ -2,7 +2,8 @@ import { type FC } from 'react';
 
 import { ApplicationLogs } from 'proton-pass-extension/lib/components/Settings/ApplicationLogs';
 import { Behaviors } from 'proton-pass-extension/lib/components/Settings/Behaviors';
-import { Locale } from 'proton-pass-extension/lib/components/Settings/Locale';
+
+import { Locale } from '@proton/pass/components/Settings/Locale';
 
 export const General: FC = () => {
     return (
