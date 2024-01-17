e480c2a2efc91894590887877790819742c5606b
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-01-17 15:17:41 +0100

    Avoid showing onboarding modals when users access desktop app

--- applications/calendar/src/app/containers/calendar/MainContainer.tsx
+++ applications/calendar/src/app/containers/calendar/MainContainer.tsx
@@ -16,6 +16,7 @@ import { useInstance } from '@proton/hooks/index';
 import { getVisualCalendars, groupCalendarsByTaxonomy, sortCalendars } from '@proton/shared/lib/calendar/calendar';
 import { CALENDAR_FLAGS } from '@proton/shared/lib/calendar/constants';
 import { hasBit } from '@proton/shared/lib/helpers/bitset';
+import { isElectronApp } from '@proton/shared/lib/helpers/desktop';
 
 import { getIsCalendarAppInDrawer } from '../../helpers/views';
 import useCalendarFavicon from '../../hooks/useCalendarFavicon';
@@ -101,7 +102,7 @@ const MainContainer = () => {
         return <CalendarSetupContainer calendars={calendarsToSetup} onDone={() => setCalendarsToSetup([])} />;
     }
 
-    if (!isDone) {
+    if (!isDone && !isElectronApp()) {
         return <CalendarOnboardingContainer onDone={() => setWelcomeFlagsDone()} />;
     }
 
