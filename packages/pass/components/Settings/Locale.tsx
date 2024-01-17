bb125a82beb44a44b729ea7f9190998b92efdd3c
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-01-17 15:54:00 +0100

    Fix language selector default value
    [IDTEAM-2384] [IDTEAM-2397]

--- packages/pass/components/Settings/Locale.tsx
+++ packages/pass/components/Settings/Locale.tsx
@@ -6,7 +6,6 @@ import { c } from 'ttag';
 import { LocaleSelect } from '@proton/pass/components/Form/legacy/LocaleSelect';
 import { settingsEditIntent } from '@proton/pass/store/actions';
 import { selectLocale } from '@proton/pass/store/selectors';
-import { DEFAULT_LOCALE } from '@proton/shared/lib/constants';
 
 import { SettingsPanel } from './SettingsPanel';
 
@@ -14,10 +13,13 @@ export const Locale: FC = () => {
     const locale = useSelector(selectLocale);
     const dispatch = useDispatch();
 
+    const getLocaleFromDocumentLang = (lang: string) => lang.replace(/-/g, '_').replace('es', 'es_LA');
+    const currentPageLocale = getLocaleFromDocumentLang(document.documentElement.lang);
+
     return (
         <SettingsPanel title={c('Label').t`Language`}>
             <LocaleSelect
-                value={locale ?? DEFAULT_LOCALE}
+                value={locale ?? currentPageLocale}
                 onChange={(locale) => dispatch(settingsEditIntent('locale', { locale }))}
             />
         </SettingsPanel>
