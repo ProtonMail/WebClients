e4f20bb020e48e25dcd761243c328f1e3b455430
Nicolas Hoffmann <dante3333@gmail.com>
2024-01-18 17:41:06 +0100

    Remove outline when commander is closed
    Commander was activating focus styles on `main` tag even if it's supposed not getting it with `tabindex="-1"`
    
    - removed it using `outline-none--at-all` helper

--- packages/components/containers/layout/PrivateMainArea.tsx
+++ packages/components/containers/layout/PrivateMainArea.tsx
@@ -39,7 +39,7 @@ const PrivateMainAreaBase = (
                 hasToolbar ? 'main-area--with-toolbar' : 'main-area',
                 hasRowMode ? 'main-area--row-mode' : undefined,
                 !drawerSidebar && !isProminent ? 'main-area-border' : undefined,
-                'flex-auto relative h-full reset4print',
+                'flex-auto relative h-full reset4print outline-none--at-all',
                 className,
             ])}
             ref={ref}
