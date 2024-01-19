e6066fe0297e5fc1da4248f7b1140f66e9c82b07
seraph <929-seraph@users.noreply.gitlab.protontech.ch>
2024-01-19 14:36:27 +0000

    Merge branch 'wallets-dashboard' into 'proton-wallet'
    Display wallets instead of onchain/lightning distribution in multi wallet dashbaord
    
    See merge request web/clients!8680

--- applications/wallet/src/app/components/YourWalletsSection/index.test.tsx
+++ applications/wallet/src/app/components/YourWalletsSection/index.test.tsx
@@ -2,35 +2,38 @@ import { fireEvent, render, screen, within } from '@testing-library/react';
 import { vi } from 'vitest';
 
 import { YourWalletsSection } from '.';
-import { mockUseOnchainWalletContext } from '../../tests';
+import { mockUseOnchainWalletContext, walletsWithAccountsWithBalanceAndTxs } from '../../tests';
 
 describe('YourWalletsSection', () => {
     beforeEach(() => {
         mockUseOnchainWalletContext();
     });
 
-    it('should display lightning wallets total balance and fiat amount', () => {
+    it('should display wallets with their balance and their type', () => {
         render(<YourWalletsSection onAddWallet={vi.fn()} />);
 
-        const lightningBalanceContainer = screen.getByTestId('lightning-balance-card');
-        expect(lightningBalanceContainer).toBeInTheDocument();
+        const balanceCards = screen.getAllByTestId('wallet-balance-card');
+        expect(balanceCards).toHaveLength(walletsWithAccountsWithBalanceAndTxs.length);
 
-        expect(within(lightningBalanceContainer).getByRole('heading', { level: 3, name: 'Your checking' }));
-        expect(within(lightningBalanceContainer).getByText('Lightning'));
-        expect(lightningBalanceContainer).toHaveTextContent('0.088753 BTC');
-        expect(lightningBalanceContainer).toHaveTextContent('$324.62');
-    });
+        expect(within(balanceCards[0]).getByRole('heading', { level: 3, name: 'Bitcoin 01' }));
+        expect(within(balanceCards[0]).getByText('OnChain'));
+        expect(balanceCards[0]).toHaveTextContent('0.118841 BTC');
+        expect(balanceCards[0]).toHaveTextContent('$434.67');
 
-    it('should display onchain wallets total balance and fiat amount', () => {
-        render(<YourWalletsSection onAddWallet={vi.fn()} />);
+        expect(within(balanceCards[1]).getByRole('heading', { level: 3, name: 'Savings on Jade' }));
+        expect(within(balanceCards[1]).getByText('OnChain'));
+        expect(balanceCards[1]).toHaveTextContent('0.083848 BTC');
+        expect(balanceCards[1]).toHaveTextContent('$306.68');
 
-        const onchainBalanceContainer = screen.getByTestId('onchain-balance-card');
-        expect(onchainBalanceContainer).toBeInTheDocument();
+        expect(within(balanceCards[2]).getByRole('heading', { level: 3, name: 'Savings on Electrum' }));
+        expect(within(balanceCards[2]).getByText('OnChain'));
+        expect(balanceCards[2]).toHaveTextContent('0.026124 BTC');
+        expect(balanceCards[2]).toHaveTextContent('$95.55');
 
-        expect(within(onchainBalanceContainer).getByRole('heading', { level: 3, name: 'Your saving' }));
-        expect(within(onchainBalanceContainer).getByText('OnChain'));
-        expect(onchainBalanceContainer).toHaveTextContent('0.228812 BTC');
-        expect(onchainBalanceContainer).toHaveTextContent('$836.90');
+        expect(within(balanceCards[3]).getByRole('heading', { level: 3, name: 'Lightning 01' }));
+        expect(within(balanceCards[3]).getByText('Lightning'));
+        expect(balanceCards[3]).toHaveTextContent('0.088753 BTC');
+        expect(balanceCards[3]).toHaveTextContent('$324.62');
     });
 
     describe('when user clicks on `Add wallet` card', () => {
