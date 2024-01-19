fa4ba7a7045a78574d0911ebfd03bea1551f77ff
Edvin CANDON <edvin.candon@proton.ch>
2024-01-19 13:52:18 +0100

    Add all password generation settings in autosuggest dropdown
    [IDTEAM-2679]

--- applications/pass-extension/src/app/content/injections/apps/dropdown/views/PasswordAutoSuggest.tsx
+++ applications/pass-extension/src/app/content/injections/apps/dropdown/views/PasswordAutoSuggest.tsx
@@ -8,44 +8,67 @@ import type { IFrameCloseOptions, IFrameMessage } from 'proton-pass-extension/ap
 import { IFrameMessageType } from 'proton-pass-extension/app/content/types';
 import { c } from 'ttag';
 
+import { Button } from '@proton/atoms/Button';
+import { Icon } from '@proton/components/index';
 import { SubTheme } from '@proton/pass/components/Layout/Theme/types';
-import { getCharsGroupedByColor } from '@proton/pass/hooks/usePasswordGenerator';
+import { PasswordMemorableOptions } from '@proton/pass/components/Password/PasswordMemorableOptions';
+import { PasswordRandomOptions } from '@proton/pass/components/Password/PasswordRandomOptions';
+import { PasswordTypeSelect } from '@proton/pass/components/Password/PasswordTypeSelect';
+import {
+    getCharsGroupedByColor,
+    isUsingMemorablePassword,
+    isUsingRandomPassword,
+    usePasswordGenerator,
+} from '@proton/pass/hooks/usePasswordGenerator';
 import type { GeneratePasswordConfig } from '@proton/pass/lib/password/generator';
-import { generatePassword } from '@proton/pass/lib/password/generator';
 import { type Maybe } from '@proton/pass/types';
+import noop from '@proton/utils/noop';
 
 type Props = {
     hostname: string;
-    passwordOptions: GeneratePasswordConfig;
+    config: GeneratePasswordConfig;
     visible?: boolean;
     onClose?: (options?: IFrameCloseOptions) => void;
     onMessage?: (message: IFrameMessage) => void;
 };
 
-export const PasswordAutoSuggest: VFC<Props> = ({ hostname, passwordOptions, visible, onMessage, onClose }) => {
+export const PasswordAutoSuggest: VFC<Props> = ({ hostname, config: initial, visible, onMessage, onClose }) => {
     const timer = useRef<Maybe<ReturnType<typeof setTimeout>>>();
     const inputRef = useRef<HTMLInputElement>(null);
-    const password = generatePassword(passwordOptions);
+
+    const [showAdvanced, setShowAdvanced] = useState(false);
     const [copied, setCopied] = useState(false);
 
+    const generator = usePasswordGenerator({ initial, onConfigChange: noop });
+
     useEffect(() => {
-        if (copied) timer.current = setTimeout(() => onClose?.(), 1_500);
+        if (copied) {
+            timer.current = setTimeout(() => {
+                setCopied(false);
+                onClose?.();
+            }, 1_500);
+        }
         return () => clearTimeout(timer.current);
     }, [copied]);
 
-    useEffect(() => setCopied(false), [visible]);
+    useEffect(() => {
+        setCopied(false);
+        setShowAdvanced(false);
+    }, [visible]);
 
     /* FIXME: move away from from `execCommand` and
      * prefer `navigator.clipboard` API  */
-    const handleClick = async () => {
+    const copyPassword = async (immediate: boolean) => {
         inputRef.current?.select();
         document.execCommand('copy');
 
-        setCopied(true);
         onMessage?.({
             type: IFrameMessageType.DROPDOWN_AUTOFILL_GENERATED_PW,
-            payload: { password },
+            payload: { password: generator.password },
         });
+
+        if (immediate) onClose?.();
+        else setCopied(true);
     };
 
     return (
@@ -53,14 +76,28 @@ export const PasswordAutoSuggest: VFC<Props> = ({ hostname, passwordOptions, vis
             <DropdownHeader
                 title={c('Title').t`Password`}
                 extra={
-                    <PauseListDropdown
-                        criteria="Autosuggest"
-                        dense
-                        hostname={hostname}
-                        label={c('Action').t`Do not suggest on this website`}
-                        onClose={onClose}
-                        visible={visible}
-                    />
+                    <div className="flex gap-1">
+                        <Button
+                            className="shrink-0 button-xs"
+                            icon
+                            color="weak"
+                            shape="solid"
+                            pill
+                            onClick={() => setShowAdvanced((prev) => !prev)}
+                            size="small"
+                            title={c('Action').t`Show advanced options`}
+                        >
+                            <Icon name="cog-drawer" alt={c('Action').t`More options`} size={12} />
+                        </Button>
+                        <PauseListDropdown
+                            criteria="Autosuggest"
+                            dense
+                            hostname={hostname}
+                            label={c('Action').t`Do not suggest on this website`}
+                            onClose={onClose}
+                            visible={visible}
+                        />
+                    </div>
                 }
             />
 
@@ -70,16 +107,36 @@ export const PasswordAutoSuggest: VFC<Props> = ({ hostname, passwordOptions, vis
                     ? {
                           icon: 'checkmark',
                           subTitle: c('Info').t`Password copied`,
-                          onClick: () => onClose?.(),
+                          onClick: onClose,
                       }
                     : {
                           icon: 'key',
                           title: c('Title').t`Fill & copy password`,
-                          subTitle: <span className="text-monospace">{getCharsGroupedByColor(password)}</span>,
-                          onClick: handleClick,
+                          subTitle: (
+                              <span className="text-monospace">{getCharsGroupedByColor(generator.password)}</span>
+                          ),
+                          onClick: () => copyPassword(false),
                       })}
             />
-            <input ref={inputRef} className="invisible" value={password} readOnly />
+            <input ref={inputRef} className="invisible" value={generator.password} readOnly />
+            {showAdvanced && (
+                <div className="flex-column flex gap-y-2 px-4 pb-3 text-sm ui-red">
+                    <hr className="m-0" />
+                    <PasswordTypeSelect dense {...generator} />
+                    <hr className="m-0" />
+                    {isUsingRandomPassword(generator) && <PasswordRandomOptions advanced dense {...generator} />}
+                    {isUsingMemorablePassword(generator) && <PasswordMemorableOptions advanced dense {...generator} />}
+                    <hr className="m-0" />
+                    <div className="flex gap-x-2">
+                        <Button className="flex-1" pill shape="solid" onClick={() => copyPassword(true)}>
+                            {c('Title').t`Fill & copy password`}
+                        </Button>
+                        <Button icon pill shape="solid" className="shrink-0" onClick={generator.regeneratePassword}>
+                            <Icon name="arrows-rotate" alt={c('Action').t`Regenerate`} />
+                        </Button>
+                    </div>
+                </div>
+            )}
         </>
     );
 };
