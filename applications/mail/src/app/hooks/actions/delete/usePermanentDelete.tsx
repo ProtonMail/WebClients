5d0167701e632891e744521641c368c8322bd42d
romain sanchez <romain.sanchez@proton.ch>
2024-01-19 17:20:38 +0100

    Use sub actions in mail actions callbacks

--- applications/mail/src/app/hooks/actions/delete/usePermanentDelete.tsx
+++ applications/mail/src/app/hooks/actions/delete/usePermanentDelete.tsx
@@ -7,13 +7,16 @@ export const usePermanentDelete = (labelID: string) => {
     const { handleDeleteSelection, deleteSelectionModal } = usePermanentDeleteSelection(labelID);
     const { handleDeleteAll, deleteAllModal } = usePermanentDeleteAll(labelID);
 
-    const handleDelete = useCallback(async (selectedIDs: string[], selectAll?: boolean) => {
-        if (selectAll) {
-            await handleDeleteAll(selectedIDs);
-        } else {
-            await handleDeleteSelection(selectedIDs);
-        }
-    }, []);
+    const handleDelete = useCallback(
+        async (selectedIDs: string[], selectAll?: boolean) => {
+            if (selectAll) {
+                await handleDeleteAll(selectedIDs);
+            } else {
+                await handleDeleteSelection(selectedIDs);
+            }
+        },
+        [handleDeleteAll, handleDeleteSelection]
+    );
 
     return { handleDelete, deleteSelectionModal, deleteAllModal };
 };
