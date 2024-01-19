094afd28c1b8f0814c2b4f14f206ca2111593dab
seraph <929-seraph@users.noreply.gitlab.protontech.ch>
2024-01-19 14:36:27 +0000

    Merge branch 'wallets-dashboard' into 'proton-wallet'
    Display wallets instead of onchain/lightning distribution in multi wallet dashbaord
    
    See merge request web/clients!8680

--- applications/wallet/src/app/components/YourWalletsSection/index.tsx
+++ applications/wallet/src/app/components/YourWalletsSection/index.tsx
@@ -9,8 +9,8 @@ import { Pill } from '@proton/atoms/Pill';
 import { WasmBitcoinUnit } from '../../../pkg';
 import { BitcoinAmount } from '../../atoms';
 import { useOnchainWalletContext } from '../../contexts';
-import { useBalanceDistribution } from '../../hooks/useBalanceDistribution';
 import { WalletType } from '../../types/api';
+import { getWalletBalance } from '../../utils';
 
 interface Props {
     onAddWallet: () => void;
@@ -22,55 +22,40 @@ const bitcoinUnit = WasmBitcoinUnit.BTC;
 
 export const YourWalletsSection = ({ onAddWallet }: Props) => {
     const { wallets } = useOnchainWalletContext();
-    const distribution = useBalanceDistribution(wallets);
-
-    const lightningBalance = distribution[WalletType.Lightning];
-    const onchainBalance = distribution[WalletType.OnChain];
 
     return (
         <div className="mt-14">
             <h2 className="h4 text-semibold">{c('Wallet Dashboard').t`Your wallets`}</h2>
 
             <div className="grid-auto-fill mt-4 gap-4" style={{ '--min-grid-template-column-size': '16rem' }}>
-                <Card
-                    data-testid="lightning-balance-card"
-                    rounded
-                    className="w-full bg-gradient-weak-norm border-weak py-3"
-                >
-                    <div className="flex justify-space-between w-full">
-                        <h3 className="text-lg">{c('Wallet Dashboard').t`Your checking`}</h3>
-                        <Pill color="#AD7406">Lightning</Pill>
-                    </div>
+                {wallets?.map((wallet) => {
+                    return (
+                        <Card
+                            key={wallet.WalletID}
+                            data-testid="wallet-balance-card"
+                            rounded
+                            className="w-full bg-gradient-weak-norm border-weak py-3"
+                        >
+                            <div className="flex flex-row justify-space-between w-full">
+                                <h3 className="text-lg">{wallet.Name}</h3>
+                                {wallet.Type === WalletType.OnChain ? (
+                                    <Pill color="#12869F">Onchain</Pill>
+                                ) : (
+                                    <Pill color="#AD7406">Lightning</Pill>
+                                )}
+                            </div>
 
-                    <BitcoinAmount
-                        unit={bitcoinUnit}
-                        fiat={fiatCurrency}
-                        className="mt-4 text-2xl"
-                        fiatClassName="mb-1"
-                    >
-                        {lightningBalance ?? 0}
-                    </BitcoinAmount>
-                </Card>
-
-                <Card
-                    data-testid="onchain-balance-card"
-                    rounded
-                    className="w-full bg-gradient-weak-norm border-weak py-3"
-                >
-                    <div className="flex justify-space-between w-full">
-                        <h3 className="text-lg">{c('Wallet Dashboard').t`Your saving`}</h3>
-                        <Pill color="#12869F">OnChain</Pill>
-                    </div>
-
-                    <BitcoinAmount
-                        unit={bitcoinUnit}
-                        fiat={fiatCurrency}
-                        className="mt-4 text-2xl"
-                        fiatClassName="mb-1"
-                    >
-                        {onchainBalance ?? 0}
-                    </BitcoinAmount>
-                </Card>
+                            <BitcoinAmount
+                                unit={bitcoinUnit}
+                                fiat={fiatCurrency}
+                                className="mt-auto text-2xl"
+                                fiatClassName="mb-1"
+                            >
+                                {getWalletBalance(wallet)}
+                            </BitcoinAmount>
+                        </Card>
+                    );
+                })}
 
                 <Button
                     shape="outline"
