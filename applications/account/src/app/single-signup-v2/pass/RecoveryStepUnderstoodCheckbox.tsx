40dcc0d12904da6e85168570119e31f83c78874d
Jack Crompton <jack.crompton@proton.ch>
2024-01-22 14:45:26 +0100

    Add "understood" checkbox to recovery step on pass signup
    CP-7243

---
+++ applications/account/src/app/single-signup-v2/pass/RecoveryStepUnderstoodCheckbox.tsx
@@ -0,0 +1,24 @@
+import { c } from 'ttag';
+
+import { Checkbox, Label } from '@proton/components/index';
+import clsx from '@proton/utils/clsx';
+
+interface Props {
+    className?: string;
+    checked: boolean;
+    onChange: () => void;
+}
+
+const RecoveryStepUnderstoodCheckbox = ({ className, checked, onChange }: Props) => {
+    return (
+        <div className={clsx('flex flex-row items-start', className)}>
+            <Checkbox id="understood-recovery-necessity" className="mt-2 mr-2" checked={checked} onChange={onChange} />
+            <Label htmlFor="understood-recovery-necessity" className="flex-1">
+                {c('pass_signup_2023: Info')
+                    .t`I understand that if I lose my recovery details, I may permanently lose access to my account.`}
+            </Label>
+        </div>
+    );
+};
+
+export default RecoveryStepUnderstoodCheckbox;
