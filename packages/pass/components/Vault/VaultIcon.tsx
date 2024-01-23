2746bce6248a96ffb689fd4fba924bd5ccad3154
Edvin CANDON <edvin.candon@proton.ch>
2024-01-23 14:17:40 +0100

    Conform pass to new icon taxonomy
    [UXE-783]

--- packages/pass/components/Vault/VaultIcon.tsx
+++ packages/pass/components/Vault/VaultIcon.tsx
@@ -2,12 +2,12 @@ import type { FC } from 'react';
 
 import type { IconSize } from '@proton/components/components';
 import { Icon } from '@proton/components/components';
+import { getIconSizePx } from '@proton/pass/components/Layout/Icon/IconBox';
+import { VAULT_COLOR_MAP, VAULT_ICON_MAP } from '@proton/pass/components/Vault/constants';
 import type { VaultIcon as VaultIconEnum } from '@proton/pass/types/protobuf/vault-v1';
 import { VaultColor as VaultColorEnum } from '@proton/pass/types/protobuf/vault-v1';
 import { rootFontSize } from '@proton/shared/lib/helpers/dom';
 import clsx from '@proton/utils/clsx';
-
-import { VAULT_COLOR_MAP, VAULT_ICON_MAP } from '../../components/Vault/constants';
 
 import './VaultIcon.scss';
 
@@ -43,8 +43,8 @@ export const VaultIcon: FC<Props> = ({
             '--vault-icon-color': highlighted
                 ? 'var(--interaction-norm-contrast)'
                 : VAULT_COLOR_MAP[color ?? VaultColorEnum.COLOR1],
-            '--w-custom': rem(background ? size * 2 : size),
-            '--h-custom': rem(background ? size * 2 : size),
+            '--w-custom': rem(getIconSizePx(background ? size * 2 : size)),
+            '--h-custom': rem(getIconSizePx(background ? size * 2 : size)),
         }}
     >
         <Icon
