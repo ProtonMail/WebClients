1a2a8b8dc9c1ecd3a915a0862df393929c425dd3
Edvin CANDON <edvin.candon@proton.ch>
2024-01-23 09:05:49 +0100

    Improve BulkSelectProvider
    - Allow toggling bulk selection via shift key
    - Rename bulk enabled flag
    
    [IDTEAM-2648]

--- packages/pass/components/Bulk/BulkSelectProvider.tsx
+++ packages/pass/components/Bulk/BulkSelectProvider.tsx
@@ -1,52 +1,73 @@
-import React, { createContext, useContext, useMemo, useState } from 'react';
+import { type FC, type PropsWithChildren, createContext, useContext, useEffect, useMemo, useState } from 'react';
 
 import type { MaybeNull, SelectedItem } from '@proton/pass/types';
 
+export type BulkSelection = Map<string, Set<string>>;
+
 type BulkSelectContextType = {
     count: number;
-    isBulk: boolean;
-    selection: Map<string, Set<string>>;
+    enabled: boolean;
+    selection: BulkSelection;
     clear: () => void;
     disable: () => void;
     enable: () => void;
     isSelected: (item: { shareId: string; itemId: string }) => boolean;
-    selectItem: ({ shareId, itemId }: SelectedItem) => void;
-    unselectItem: ({ shareId, itemId }: SelectedItem) => void;
+    select: ({ shareId, itemId }: SelectedItem) => void;
+    unselect: ({ shareId, itemId }: SelectedItem) => void;
 };
 
 const BulkSelectContext = createContext<MaybeNull<BulkSelectContextType>>(null);
 
-export const BulkSelectProvider: React.FC = ({ children }) => {
-    const [selection, setSelection] = useState<Map<string, Set<string>>>(new Map());
-    const [isBulk, setIsBulk] = useState(false);
+export const BulkSelectProvider: FC<PropsWithChildren> = ({ children }) => {
+    const [selection, setSelection] = useState<BulkSelection>(new Map());
+    const [enabled, setEnabled] = useState(false);
 
     const context = useMemo<BulkSelectContextType>(() => {
         const clear = () => setSelection(new Map());
 
         return {
             count: Array.from(selection.values()).reduce((count, { size }) => count + size, 0),
-            isBulk,
+            enabled,
             selection,
             clear,
             disable: () => {
-                setIsBulk(false);
+                setEnabled(false);
                 clear();
             },
-            enable: () => setIsBulk(true),
+            enable: () => setEnabled(true),
             isSelected: ({ shareId, itemId }) => selection.get(shareId)?.has(itemId) ?? false,
-            selectItem: ({ shareId, itemId }) =>
+            select: ({ shareId, itemId }) =>
                 setSelection((curr) => {
                     const shareSet = curr.get(shareId) ?? new Set();
                     shareSet.add(itemId);
                     return new Map(curr.set(shareId, shareSet));
                 }),
-            unselectItem: ({ shareId, itemId }) =>
+            unselect: ({ shareId, itemId }) =>
                 setSelection((curr) => {
                     curr.get(shareId)?.delete(itemId);
                     return new Map(curr);
                 }),
         };
-    }, [selection, isBulk]);
+    }, [selection, enabled]);
+
+    useEffect(() => {
+        const handleKeyUp = (event: KeyboardEvent) => {
+            if (enabled && context.count === 0) context.disable();
+            if (event.key === 'Shift') document.removeEventListener('keyup', handleKeyUp);
+        };
+
+        const handleKeyDown = (event: KeyboardEvent) => {
+            if (!enabled && event.key === 'Shift') context.enable();
+        };
+
+        document.addEventListener('keydown', handleKeyDown);
+        document.addEventListener('keyup', handleKeyUp);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('keyup', handleKeyUp);
+        };
+    }, [enabled, context.count]);
 
     return <BulkSelectContext.Provider value={context}>{children}</BulkSelectContext.Provider>;
 };
