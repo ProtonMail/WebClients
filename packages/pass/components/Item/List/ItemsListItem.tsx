61aaaece455a19ba0b4776bf4f78d74a953da4cc
Edvin CANDON <edvin.candon@proton.ch>
2024-01-24 17:18:39 +0100

    Fine-tune bulk selection styles in items list
    [IDTEAM-2282]

--- packages/pass/components/Item/List/ItemsListItem.tsx
+++ packages/pass/components/Item/List/ItemsListItem.tsx
@@ -35,77 +35,93 @@ const ItemsListItemRaw: FC<Props> = ({ item, search = '', active = false, ...res
     const vault = useSelector(selectShare<ShareType.Vault>(shareId));
     const pinningEnabled = useFeatureFlag(PassFeature.PassPinningV1);
     const bulk = useBulkSelect();
+    const bulkSelected = bulk.isSelected(item);
     const bulkInFlight = useBulkInFlight(item);
     const loading = optimistic || bulkInFlight;
 
     return (
-        <ButtonLike
-            as={Link}
-            to="#"
-            className={clsx(['pass-item-list--item interactive-pseudo w-full relative', active && 'is-active'])}
-            color={failed ? 'warning' : 'weak'}
-            shape="ghost"
-            style={{ '--anime-opacity': loading ? '0.5' : '1' }}
-            {...rest}
-        >
-            <div className="flex-nowrap flex w-full px-3 py-2 items-center">
-                <SafeItemIcon
-                    item={item}
-                    size={5}
-                    className={clsx('mr-3  shrink-0', itemTypeToSubThemeClassName[data.type])}
-                    iconClassName={clsx(loading && 'opacity-50')}
-                    renderIndicators={(size) => (
-                        <ItemIconIndicators size={size} loading={loading} error={item.failed} />
-                    )}
-                />
-                {bulk.enabled && (
-                    <ItemIcon
-                        icon="checkmark"
-                        alt=""
-                        size={20}
-                        className={clsx(
-                            'pass-item-icon mr-3 shrink-0 absolute bulk-select-check',
-                            bulk.isSelected(item) && 'selected'
-                        )}
-                        pill
-                        loadImage={false}
-                        normColor={false}
-                    />
-                )}
-                {pinningEnabled && item.pinned && (
-                    <IconBox
-                        size={2.5}
-                        mode="transparent"
-                        className={clsx(
-                            'pass-item-list--item--pin absolute top-custom left-custom flex items-center justify-center',
-                            itemTypeToSubThemeClassName[data.type]
-                        )}
-                        style={{ '--top-custom': '32px', '--left-custom': '36px' }}
-                    >
-                        <Icon name="pin-angled-filled" size={2.75} className="absolute-center" color="black" />
-                    </IconBox>
-                )}
+        <div className={clsx(bulk.enabled && 'px-1 py-0.5')}>
+            <ButtonLike
+                as={Link}
+                to="#"
+                className={clsx([
+                    'pass-item-list--item interactive-pseudo w-full',
+                    bulk.enabled && 'pass-item-list--item-bulk',
+                    bulkSelected && 'pass-item-list--item-bulk-selected',
+                    active && 'is-active',
+                ])}
+                color={failed ? 'warning' : 'weak'}
+                shape="ghost"
+                style={{ '--anime-opacity': loading ? '0.5' : '1' }}
+                {...rest}
+            >
+                <div
+                    className={clsx('flex-nowrap flex w-full items-center', bulk.enabled ? 'px-2 py-1.5' : 'px-3 py-2')}
+                >
+                    <SafeItemIcon
+                        item={item}
+                        size={5}
+                        className={clsx('mr-3 shrink-0 relative', itemTypeToSubThemeClassName[data.type])}
+                        iconClassName={clsx(loading && 'opacity-50')}
+                        renderIndicators={(size) => (
+                            <>
+                                {bulk.enabled ? (
+                                    <ItemIcon
+                                        alt=""
+                                        className={'ui-standard absolute bulk-select-check pass-item-list--checkmark'}
+                                        icon={'checkmark'}
+                                        loadImage={false}
+                                        normColor={false}
+                                        pill
+                                        size={5}
+                                    />
+                                ) : (
+                                    <ItemIconIndicators size={size} loading={loading} error={item.failed} />
+                                )}
 
-                <div className={clsx('text-left', loading && !failed && 'opacity-50')}>
-                    <span className="flex items-center">
-                        {search && (
-                            <VaultIcon size={3} icon={vault?.content.display.icon} className="color-weak mr-1" />
+                                {pinningEnabled && item.pinned && (
+                                    <IconBox
+                                        size={2.5}
+                                        mode="transparent"
+                                        className={clsx(
+                                            'pass-item-list--item--pin absolute bottom-custom right-custom flex items-center justify-center',
+                                            itemTypeToSubThemeClassName[data.type]
+                                        )}
+                                        style={{ '--bottom-custom': '-6px', '--right-custom': '-6px' }}
+                                    >
+                                        <Icon
+                                            name="pin-angled-filled"
+                                            size={2.75}
+                                            className="absolute-center"
+                                            color="black"
+                                        />
+                                    </IconBox>
+                                )}
+                            </>
                         )}
-                        <span className="flex-1 text-ellipsis">
-                            <Marks chunks={matchChunks(heading, search)}>{heading}</Marks>
+                    />
+
+                    <div className={clsx('text-left', loading && !failed && 'opacity-50')}>
+                        <span className="flex items-center">
+                            {search && (
+                                <VaultIcon size={3} icon={vault?.content.display.icon} className="color-weak mr-1" />
+                            )}
+                            <span className="flex-1 text-ellipsis">
+                                <Marks chunks={matchChunks(heading, search)}>{heading}</Marks>
+                            </span>
                         </span>
-                    </span>
-                    <div
-                        className={clsx([
-                            'pass-item-list--subtitle block color-weak text-sm text-ellipsis',
-                            item.data.type === 'note' && isEmptyString(item.data.metadata.note.v) && 'text-italic',
-                        ])}
-                    >
-                        <Marks chunks={matchChunks(subheading, search)}>{subheading}</Marks>
+                        <div
+                            className={clsx([
+                                'pass-item-list--subtitle block color-weak text-sm text-ellipsis',
+                                item.data.type === 'note' && isEmptyString(item.data.metadata.note.v) && 'text-italic',
+                            ])}
+                        >
+                            <Marks chunks={matchChunks(subheading, search)}>{subheading}</Marks>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </ButtonLike>
+            </ButtonLike>
+        </div>
     );
 };
 
