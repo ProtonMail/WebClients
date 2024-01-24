2820f89e87fb8cd9ef671f875f3c0a69172bf466
Riste Bozhinov <r.bozhinov@proton.ch>
2024-01-24 11:24:37 +0200

    Drive e2e fixes and updated locators

--- applications/drive/src/app/components/sections/ContextMenu/buttons/ShareLinkButton.tsx
+++ applications/drive/src/app/components/sections/ContextMenu/buttons/ShareLinkButton.tsx
@@ -18,7 +18,7 @@ const ShareLinkButton = ({ shareId, link, showLinkSharingModal, close }: Props) 
         <ContextMenuButton
             name={hasSharedLink ? c('Action').t`Manage link` : c('Action').t`Get link`}
             icon={hasSharedLink ? 'link-pen' : 'link'}
-            testId="context-menu-manage-link"
+            testId={hasSharedLink ? 'context-menu-manage-link' : 'context-menu-share-link'}
             action={() => showLinkSharingModal({ shareId, linkId: link.linkId })}
             close={close}
         />
