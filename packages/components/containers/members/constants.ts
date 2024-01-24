6020716d7b8ca8701bc18463c7399ce29216eca1
Jack Crompton <jack.crompton@proton.ch>
2024-01-24 16:53:18 +0100

    Rename vpnB2bAdminTooltipTitle to adminTooltipText
    CP-7246

--- packages/components/containers/members/constants.ts
+++ packages/components/containers/members/constants.ts
@@ -1,6 +1,6 @@
 import { c } from 'ttag';
 
-export const vpnB2bAdminTooltipTitle = (() => {
+export const adminTooltipText = (() => {
     return c('Tooltip')
         .t`Admins can make changes to your subscription, manage user accounts and passwords, and give admin privileges to other users.`;
 })();
