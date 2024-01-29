8d43c2c786f666f6c0d0f90c030886b8da62f945
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-01-29 15:10:00 +0100

    Improve application switcher design on macos

--- packages/components/containers/desktop/InboxDesktopAppSwitcher.tsx
+++ packages/components/containers/desktop/InboxDesktopAppSwitcher.tsx
@@ -14,37 +14,31 @@ const InboxDesktopAppSwitcher = ({ appToLinkTo }: Props) => {
     const isAppCalendar = APP_NAME === APPS.PROTONCALENDAR || APPS.PROTONCALENDAR === appToLinkTo;
 
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-0.5">
             <ButtonLike
                 as={AppLink}
                 to="/"
                 toApp={APPS.PROTONMAIL}
                 target="_blank"
+                className="flex items-center"
                 shape={isAppMail ? 'solid' : 'ghost'}
-                size="small"
                 aria-current={isAppMail}
             >
-                <Icon
-                    name="inbox"
-                    alt={MAIL_APP_NAME}
-                    className={clsx(isAppMail ? 'color-norm' : 'color-weak')}
-                    strokeWidth={2}
-                />
+                <Icon name="inbox" alt={MAIL_APP_NAME} className={clsx(isAppMail ? 'color-norm' : 'color-weak')} />
             </ButtonLike>
             <ButtonLike
                 as={AppLink}
                 to="/"
                 toApp={APPS.PROTONCALENDAR}
                 target="_blank"
+                className="flex items-center"
                 shape={isAppCalendar ? 'solid' : 'ghost'}
-                size="small"
                 aria-current={isAppCalendar}
             >
                 <Icon
                     name="calendar-grid"
                     alt={CALENDAR_APP_NAME}
                     className={clsx(isAppCalendar ? 'color-norm' : 'color-weak')}
-                    strokeWidth={2}
                 />
             </ButtonLike>
         </div>
