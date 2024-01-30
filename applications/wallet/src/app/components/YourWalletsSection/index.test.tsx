f6b7b18f9933819e1f5ed61a123e6cdc3edf0cf5
seraph <929-seraph@users.noreply.gitlab.protontech.ch>
2024-01-30 13:53:33 +0100

    Bridge rebase gap

--- applications/wallet/src/app/components/YourWalletsSection/index.test.tsx
+++ applications/wallet/src/app/components/YourWalletsSection/index.test.tsx
@@ -16,17 +16,17 @@ describe('YourWalletsSection', () => {
         expect(balanceCards).toHaveLength(walletsWithAccountsWithBalanceAndTxs.length);
 
         expect(within(balanceCards[0]).getByRole('heading', { level: 3, name: 'Bitcoin 01' }));
-        expect(within(balanceCards[0]).getByText('OnChain'));
+        expect(within(balanceCards[0]).getByText('Onchain'));
         expect(balanceCards[0]).toHaveTextContent('0.118841 BTC');
         expect(balanceCards[0]).toHaveTextContent('$434.67');
 
         expect(within(balanceCards[1]).getByRole('heading', { level: 3, name: 'Savings on Jade' }));
-        expect(within(balanceCards[1]).getByText('OnChain'));
+        expect(within(balanceCards[1]).getByText('Onchain'));
         expect(balanceCards[1]).toHaveTextContent('0.083848 BTC');
         expect(balanceCards[1]).toHaveTextContent('$306.68');
 
         expect(within(balanceCards[2]).getByRole('heading', { level: 3, name: 'Savings on Electrum' }));
-        expect(within(balanceCards[2]).getByText('OnChain'));
+        expect(within(balanceCards[2]).getByText('Onchain'));
         expect(balanceCards[2]).toHaveTextContent('0.026124 BTC');
         expect(balanceCards[2]).toHaveTextContent('$95.55');
 
