f860a4e28ebe935ecb9a2e5e09c143cf702b49f6
Edvin CANDON <edvin.candon@proton.ch>
2024-01-31 17:06:31 +0100

    Fine-tune pass default locale resolution

--- packages/pass/components/Settings/Locale.tsx
+++ packages/pass/components/Settings/Locale.tsx
@@ -6,7 +6,6 @@ import { c } from 'ttag';
 import { LocaleSelect } from '@proton/pass/components/Form/legacy/LocaleSelect';
 import { settingsEditIntent } from '@proton/pass/store/actions';
 import { selectLocale } from '@proton/pass/store/selectors';
-import { DEFAULT_LOCALE } from '@proton/shared/lib/constants';
 
 import { SettingsPanel } from './SettingsPanel';
 
@@ -17,7 +16,7 @@ export const Locale: FC = () => {
     return (
         <SettingsPanel title={c('Label').t`Language`}>
             <LocaleSelect
-                value={locale ?? DEFAULT_LOCALE}
+                value={locale}
                 onChange={(locale) => dispatch(settingsEditIntent('locale', { locale }, true))}
             />
         </SettingsPanel>
