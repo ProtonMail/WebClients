d50df5067561b7ccfb72b2adea47d31cd1f371ca
Edvin CANDON <edvin.candon@proton.ch>
2024-02-01 15:47:07 +0100

    Avoid re-triggering context provider effects on locale switch

--- applications/pass-extension/src/app/content/injections/apps/notification/Notification.tsx
+++ applications/pass-extension/src/app/content/injections/apps/notification/Notification.tsx
@@ -1,5 +1,7 @@
 import { type FC } from 'react';
 
+import { ExtensionCore } from 'proton-pass-extension/lib/components/Extension/ExtensionCore';
+
 import { Icons, NotificationsProvider } from '@proton/components';
 import { ThemeProvider } from '@proton/pass/components/Layout/Theme/ThemeProvider';
 
@@ -9,11 +11,13 @@ import { NotificationContent } from './views/NotificationContent';
 import './Notification.scss';
 
 export const Notification: FC = () => (
-    <IFrameContextProvider endpoint="notification">
-        <Icons />
-        <ThemeProvider />
-        <NotificationsProvider>
-            <NotificationContent />
-        </NotificationsProvider>
-    </IFrameContextProvider>
+    <ExtensionCore endpoint="notification">
+        <IFrameContextProvider endpoint="notification">
+            <Icons />
+            <ThemeProvider />
+            <NotificationsProvider>
+                <NotificationContent />
+            </NotificationsProvider>
+        </IFrameContextProvider>
+    </ExtensionCore>
 );
--- applications/pass-extension/src/app/pages/onboarding/Onboarding.tsx
+++ applications/pass-extension/src/app/pages/onboarding/Onboarding.tsx
@@ -2,6 +2,7 @@ import { type FC } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import { ExtensionApp } from 'proton-pass-extension/lib/components/Extension/ExtensionApp';
+import { ExtensionLocalized } from 'proton-pass-extension/lib/components/Extension/ExtensionLocalized';
 
 import { InstallationSuccess } from './Views/InstallationSuccess';
 import { ResumeSession } from './Views/ResumeSession';
@@ -13,19 +14,21 @@ export const Onboarding: FC = () => (
     <ExtensionApp endpoint="page">
         {(ready) =>
             ready && (
-                <HashRouter>
-                    <Switch>
-                        <Route path="/resume">
-                            <ResumeSession />
-                        </Route>
-                        <Route path="/success">
-                            <InstallationSuccess />
-                        </Route>
-                        <Route path="/welcome">
-                            <Welcome />
-                        </Route>
-                    </Switch>
-                </HashRouter>
+                <ExtensionLocalized>
+                    <HashRouter>
+                        <Switch>
+                            <Route path="/resume">
+                                <ResumeSession />
+                            </Route>
+                            <Route path="/success">
+                                <InstallationSuccess />
+                            </Route>
+                            <Route path="/welcome">
+                                <Welcome />
+                            </Route>
+                        </Switch>
+                    </HashRouter>
+                </ExtensionLocalized>
             )
         }
     </ExtensionApp>
