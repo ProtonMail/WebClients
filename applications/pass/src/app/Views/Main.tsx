d887ac997ae5f575e1dad7be7b952bda513643ef
Irena <irena.zazoska@proton.ch>
2024-02-01 12:27:49 +0100

    Add basic onboarding components
    [IDTEAM-2696]

--- applications/pass/src/app/Views/Main.tsx
+++ applications/pass/src/app/Views/Main.tsx
@@ -1,4 +1,5 @@
 import { type FC } from 'react';
+import { useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
 import { Hamburger } from '@proton/components';
@@ -16,12 +17,16 @@ import { PasswordProvider } from '@proton/pass/components/Password/PasswordProvi
 import { SpotlightProvider } from '@proton/pass/components/Spotlight/SpotlightProvider';
 import { VaultActionsProvider } from '@proton/pass/components/Vault/VaultActionsProvider';
 import { getExtensionSupportedBrowser } from '@proton/pass/lib/extension/utils/browser';
+import { selectPassPlan, selectUser } from '@proton/pass/store/selectors';
+import { UserPassPlan } from '@proton/pass/types/api/plan';
 import { getLocalIDPath } from '@proton/shared/lib/authentication/pathnameHelper';
 import { isDesktop } from '@proton/shared/lib/helpers/browser';
+import { isAdmin } from '@proton/shared/lib/user/helpers';
 
 import { useClient } from '../Context/ClientProvider';
 import { Header } from './Header/Header';
 import { TopBar } from './Header/TopBar';
+import { Onboarding } from './Onboarding/Onboarding';
 import { Settings } from './Settings/Settings';
 import { Menu } from './Sidebar/Menu';
 
@@ -31,6 +36,11 @@ const showTopBar = isDesktop() && extensionClient !== undefined;
 export const Main: FC = () => {
     const client = useClient();
     const { state: expanded, toggle } = useToggle();
+    const plan = useSelector(selectPassPlan);
+    const user = useSelector(selectUser);
+    const isOrgAdmin = user ? isAdmin(user) : false;
+
+    /* if there is onboarding going and are not 1 of 4 actions redirect to onboarding */
 
     return (
         <ItemsProvider>
@@ -60,6 +70,13 @@ export const Main: FC = () => {
                                                         <div className="flex items-center justify-center flex-nowrap w-full h-full">
                                                             {route.match && (
                                                                 <Switch>
+                                                                    {plan === UserPassPlan.BUSINESS && isOrgAdmin && (
+                                                                        <Route
+                                                                            exact
+                                                                            path={`${route.match.path}/onboarding`}
+                                                                            component={Onboarding}
+                                                                        />
+                                                                    )}
                                                                     <Route
                                                                         exact
                                                                         path={`${route.match.path}/settings`}
