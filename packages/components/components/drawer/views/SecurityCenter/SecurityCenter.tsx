2b0a9bfbe437ca88a0f03f7bef231a1807d16321
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2024-02-02 16:31:39 +0100

    Feedbacks

--- packages/components/components/drawer/views/SecurityCenter/SecurityCenter.tsx
+++ packages/components/components/drawer/views/SecurityCenter/SecurityCenter.tsx
@@ -1,17 +1,21 @@
 import { useFlag } from '@proton/components/containers';
+import { baseUseSelector } from '@proton/redux-shared-store';
 
 import { DrawerAppScrollContainer } from '../shared';
 import AccountSecurity from './AccountSecurity/AccountSecurity';
+import { selectCanDisplayAccountSecuritySection } from './AccountSecurity/slice/accountSecuritySlice';
 import PassAliasesContainer from './PassAliases/PassAliasesContainer';
 import ProtonSentinel from './ProtonSentinel/ProtonSentinel';
 
 const SecurityCenter = () => {
     const canDisplayPassAliases = useFlag('DrawerSecurityCenterDisplayPassAliases');
+    const canDisplayAccountSecurity = baseUseSelector(selectCanDisplayAccountSecuritySection);
+
     return (
         <DrawerAppScrollContainer>
             {canDisplayPassAliases && <PassAliasesContainer />}
             <ProtonSentinel />
-            <AccountSecurity />
+            {canDisplayAccountSecurity && <AccountSecurity />}
         </DrawerAppScrollContainer>
     );
 };
