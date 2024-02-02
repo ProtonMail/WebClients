576af017935194c77b45918c335a3162bbb9f6e6
Irena Zazoska <irena.zazoska@proton.ch>
2024-02-02 09:06:13 +0000

    [IDTEAM-2387] Add TopBar Install Pass Extension

---
+++ packages/pass/hooks/usePassExtensionInstalled.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+import { type AccountProbeMessage, WorkerMessageType } from '@proton/pass/types';
+import { getExtension } from '@proton/shared/lib/apps/helper';
+import { sendExtensionMessage } from '@proton/shared/lib/browser/extension';
+import { APPS } from '@proton/shared/lib/constants';
+import noop from '@proton/utils/noop';
+
+export const usePassExtensionInstalled = (): boolean => {
+    const [installed, setInstalled] = useState(true);
+
+    useEffect(() => {
+        const extension = getExtension(APPS.PROTONPASSBROWSEREXTENSION);
+
+        if (extension) {
+            sendExtensionMessage<AccountProbeMessage>(
+                { type: WorkerMessageType.ACCOUNT_PROBE },
+                { extensionId: extension.ID, maxTimeout: 1000 }
+            )
+                .then((result) => setInstalled(result?.type === 'success'))
+                .catch(noop);
+        }
+    }, []);
+
+    return installed;
+};
