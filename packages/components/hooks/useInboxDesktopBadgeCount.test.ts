1372c47568abc75b796e6b442a3a4e7124188e59
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-02-02 13:33:40 +0000

    Send IPC call when user logs out

--- packages/components/hooks/useInboxDesktopBadgeCount.test.ts
+++ packages/components/hooks/useInboxDesktopBadgeCount.test.ts
@@ -1,26 +1,23 @@
 import { renderHook } from '@testing-library/react-hooks';
 
-import { useConversationCounts, useMailSettings } from '@proton/components';
+import { useConversationCounts } from '@proton/components';
 import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
-import * as desktop from '@proton/shared/lib/helpers/desktop';
-import { UNREAD_FAVICON } from '@proton/shared/lib/mail/mailSettings';
+import * as ipcHelpers from '@proton/shared/lib/desktop/ipcHelpers';
 
 import { useInboxDesktopBadgeCount } from './';
 
 jest.mock('@proton/components/hooks/useConversationCounts');
-jest.mock('@proton/components/hooks/useMailSettings');
-jest.mock('@proton/shared/lib/helpers/desktop');
-const desktopMock = desktop as jest.MockedObject<typeof desktop>;
+jest.mock('@proton/shared/lib/desktop/ipcHelpers');
+const ipcHelpersMock = ipcHelpers as jest.MockedObject<typeof ipcHelpers>;
 
 declare const global: {
-    protonDesktopAPI?: any;
+    ipcInboxMessageBroker?: any;
 };
 
 const originalWindow = { ...window };
 
 describe('useInboxDesktopBadgeCount', () => {
     const useConversationCountsMock = useConversationCounts as jest.Mock;
-    const useMailSettingsMock = useMailSettings as jest.Mock;
     const ipcInboxMessageBrokerMock = {
         send: jest.fn(),
     };
@@ -36,56 +33,34 @@ describe('useInboxDesktopBadgeCount', () => {
     });
 
     it('should not call when not on desktop', () => {
-        desktopMock.isElectronApp = false;
+        ipcHelpersMock.canInvokeInboxDesktopIPC = false;
         useConversationCountsMock.mockReturnValue([]);
-        useMailSettingsMock.mockReturnValue([{}]);
 
         renderHook(() => useInboxDesktopBadgeCount());
         expect(ipcInboxMessageBrokerMock.send).not.toHaveBeenCalled();
     });
 
-    it('should call with 0 when unread favicon is disabled', () => {
-        desktopMock.isElectronApp = true;
+    it('should call with not call when no count', () => {
+        ipcHelpersMock.canInvokeInboxDesktopIPC = true;
         useConversationCountsMock.mockReturnValue([]);
-        useMailSettingsMock.mockReturnValue([{ UnreadFavicon: UNREAD_FAVICON.DISABLED }]);
-
-        renderHook(() => useInboxDesktopBadgeCount());
-        expect(ipcInboxMessageBrokerMock.send).toHaveBeenCalledWith('updateNotification', 0);
-    });
-
-    it('should call with not call when unread enabled but no count', () => {
-        desktopMock.isElectronApp = true;
-        useConversationCountsMock.mockReturnValue([]);
-        useMailSettingsMock.mockReturnValue([{ UnreadFavicon: UNREAD_FAVICON.ENABLED }]);
 
         renderHook(() => useInboxDesktopBadgeCount());
         expect(ipcInboxMessageBrokerMock.send).not.toHaveBeenCalled();
     });
 
-    it('should call with 1 when unread enabled and count', () => {
-        desktopMock.isElectronApp = true;
+    it('should call with 1 when 1 unread', () => {
+        ipcHelpersMock.canInvokeInboxDesktopIPC = true;
         useConversationCountsMock.mockReturnValue([[{ LabelID: MAILBOX_LABEL_IDS.INBOX, Unread: 1, Total: 1 }]]);
-        useMailSettingsMock.mockReturnValue([{ UnreadFavicon: UNREAD_FAVICON.ENABLED }]);
 
         renderHook(() => useInboxDesktopBadgeCount());
         expect(ipcInboxMessageBrokerMock.send).toHaveBeenCalledWith('updateNotification', 1);
     });
 
-    it('should call with 100 when unread enabled and count', () => {
-        desktopMock.isElectronApp = true;
+    it('should call with 100 when 100 unread', () => {
+        ipcHelpersMock.canInvokeInboxDesktopIPC = true;
         useConversationCountsMock.mockReturnValue([[{ LabelID: MAILBOX_LABEL_IDS.INBOX, Unread: 100, Total: 100 }]]);
-        useMailSettingsMock.mockReturnValue([{ UnreadFavicon: UNREAD_FAVICON.ENABLED }]);
 
         renderHook(() => useInboxDesktopBadgeCount());
         expect(ipcInboxMessageBrokerMock.send).toHaveBeenCalledWith('updateNotification', 100);
-    });
-
-    it('should call with 0 when unread enabled and no unread', () => {
-        desktopMock.isElectronApp = true;
-        useConversationCountsMock.mockReturnValue([[{ LabelID: MAILBOX_LABEL_IDS.INBOX, Total: 1 }]]);
-        useMailSettingsMock.mockReturnValue([{ UnreadFavicon: UNREAD_FAVICON.ENABLED }]);
-
-        renderHook(() => useInboxDesktopBadgeCount());
-        expect(ipcInboxMessageBrokerMock.send).toHaveBeenCalledWith('updateNotification', 0);
     });
 });
--- packages/components/hooks/useInboxDesktopBadgeCount.ts
+++ packages/components/hooks/useInboxDesktopBadgeCount.ts
@@ -1,10 +1,9 @@
 import { useEffect } from 'react';
 
-import { useConversationCounts, useMailSettings } from '@proton/components/hooks';
+import { useConversationCounts } from '@proton/components/hooks';
 import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
 import { IPCInboxMessageBroker } from '@proton/shared/lib/desktop/desktopTypes';
-import { isElectronApp } from '@proton/shared/lib/helpers/desktop';
-import { UNREAD_FAVICON } from '@proton/shared/lib/mail/mailSettings';
+import { canInvokeInboxDesktopIPC } from '@proton/shared/lib/desktop/ipcHelpers';
 
 declare global {
     interface Window {
@@ -14,25 +13,18 @@ declare global {
 
 const useInboxDesktopBadgeCount = () => {
     const [conversationCounts] = useConversationCounts();
-    const [settings] = useMailSettings();
 
     // Updates the notification badge on the desktop app icon depending on the unread count
-    // Respect user settings for the unread favicon
     useEffect(() => {
-        if (!isElectronApp || !window.ipcInboxMessageBroker) {
-            return;
-        }
-
-        if (settings?.UnreadFavicon === UNREAD_FAVICON.DISABLED) {
-            window.ipcInboxMessageBroker.send('updateNotification', 0);
+        if (!canInvokeInboxDesktopIPC) {
             return;
         }
 
         const inboxConvCount = conversationCounts?.find(({ LabelID }) => LabelID === MAILBOX_LABEL_IDS.INBOX);
-        if (inboxConvCount && settings?.UnreadFavicon === UNREAD_FAVICON.ENABLED) {
-            window.ipcInboxMessageBroker.send('updateNotification', inboxConvCount.Unread ?? 0);
+        if (inboxConvCount) {
+            window.ipcInboxMessageBroker!.send('updateNotification', inboxConvCount.Unread ?? 0);
         }
-    }, [conversationCounts, settings]);
+    }, [conversationCounts]);
 };
 
 export default useInboxDesktopBadgeCount;
