f29e96f86903dfb42f57521d0d2dfb0a37b22746
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-02-02 17:19:25 +0100

    Add IPC calls to inform Electron of the Easy Switch process

--- packages/shared/lib/desktop/desktopTypes.ts
+++ packages/shared/lib/desktop/desktopTypes.ts
@@ -2,7 +2,8 @@
 export type IPCInboxMessage =
     | { type: 'updateNotification'; payload: number }
     | { type: 'userLogout'; payload: undefined }
-    | { type: 'clearAppData'; payload: undefined };
+    | { type: 'clearAppData'; payload: undefined }
+    | { type: 'oauthPopupOpened'; payload: 'oauthPopupStarted' | 'oauthPopupFinished' };
 export type IPCInboxMessageType = IPCInboxMessage['type'];
 
 /**
