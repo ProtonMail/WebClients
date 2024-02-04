544b7e7731ed873ae0a1f6565c0a1f4808d47baa
Edvin CANDON <edvin.candon@proton.ch>
2024-02-04 11:11:19 +0100

    Add all extension detection logic to `PassExtensionLink` provider

--- packages/pass/hooks/usePassExtensionInstalled.ts
+++ packages/pass/hooks/usePassExtensionInstalled.ts
@@ -6,19 +6,21 @@ import { sendExtensionMessage } from '@proton/shared/lib/browser/extension';
 import { APPS } from '@proton/shared/lib/constants';
 import noop from '@proton/utils/noop';
 
-export const usePassExtensionInstalled = (): boolean => {
+export const usePassExtensionInstalled = (supported: boolean): boolean => {
     const [installed, setInstalled] = useState(true);
 
     useEffect(() => {
-        const extension = getExtension(APPS.PROTONPASSBROWSEREXTENSION);
+        if (supported) {
+            const extension = getExtension(APPS.PROTONPASSBROWSEREXTENSION);
 
-        if (extension) {
-            sendExtensionMessage<AccountProbeMessage>(
-                { type: WorkerMessageType.ACCOUNT_PROBE },
-                { extensionId: extension.ID, maxTimeout: 1000 }
-            )
-                .then((result) => setInstalled(result?.type === 'success'))
-                .catch(noop);
+            if (extension) {
+                sendExtensionMessage<AccountProbeMessage>(
+                    { type: WorkerMessageType.ACCOUNT_PROBE },
+                    { extensionId: extension.ID, maxTimeout: 1000 }
+                )
+                    .then((result) => setInstalled(result?.type === 'success'))
+                    .catch(noop);
+            }
         }
     }, []);
 
