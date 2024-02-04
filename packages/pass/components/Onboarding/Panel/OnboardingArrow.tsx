a4db2ba6a121fc16feb3a0a4ada90c2c9e9c9ae2
Edvin CANDON <edvin.candon@proton.ch>
2024-02-04 12:31:18 +0100

    Fine-tune onboarding responsive layout
    [IDTEAM-2696]

--- packages/pass/components/Onboarding/Panel/OnboardingArrow.tsx
+++ packages/pass/components/Onboarding/Panel/OnboardingArrow.tsx
@@ -8,7 +8,7 @@ import createItemArrow from '@proton/pass/assets/b2b-onboarding/create-arrow.svg
 export const OnboardingArrow: FC = () => {
     return (
         <div className="relative flex flex-nowrap items-end">
-            <Button pill shape="outline" color="norm" className="border-primary">{c('Action')
+            <Button pill shape="outline" color="norm" className="border-primary pointer-events-none">{c('Action')
                 .t`Create new items`}</Button>
             <img src={createItemArrow} alt="" className="pb-4" />
         </div>
