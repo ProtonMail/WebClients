3017d6e802898167838c3242d076c0bc355c9c2a
Eduardo Conde Pena <econdepe@proton.ch>
2024-02-06 19:22:56 +0100

    Refactor
    Now that we allow editing single occurrences, we can't compute the corrected invite actions outside of getSaveRecurringEventActions

--- applications/calendar/src/app/containers/calendar/eventActions/getSaveEventActionsHelpers.ts
+++ applications/calendar/src/app/containers/calendar/eventActions/getSaveEventActionsHelpers.ts
@@ -1,10 +1,13 @@
+import { ICAL_METHOD } from '@proton/shared/lib/calendar/constants';
 import { getBase64SharedSessionKey } from '@proton/shared/lib/calendar/crypto/keys/helpers';
+import { getInviteVeventWithUpdatedParstats } from '@proton/shared/lib/calendar/mailIntegration/invite';
 import { omit } from '@proton/shared/lib/helpers/object';
 import { SyncMultipleApiResponse, VcalVeventComponent } from '@proton/shared/lib/interfaces/calendar';
 import { GetCalendarKeys } from '@proton/shared/lib/interfaces/hooks/GetCalendarKeys';
 
-import { InviteActions, OnSendPrefsErrors } from '../../../interfaces/Invite';
+import { INVITE_ACTION_TYPES, InviteActions, OnSendPrefsErrors } from '../../../interfaces/Invite';
 import { SyncEventActionOperations, getCreateSyncOperation } from '../getSyncMultipleEventsPayload';
+import { getCorrectedSaveInviteActions } from './inviteActions';
 
 /**
  * Helper that saves in the DB an intermediate event without attendees, taking into account send preferences errors
@@ -77,4 +80,40 @@ export const createIntermediateEvent = async ({
     }
 
     return { intermediateEvent, vevent: cleanVevent, inviteActions: cleanInviteActions };
+};
+
+export const getCorrectedSendInviteData = async ({
+    newVevent,
+    oldVevent,
+    inviteActions,
+    hasModifiedDateTimes,
+    onEquivalentAttendees,
+    isCreatingSingleEdit,
+}: {
+    newVevent: VcalVeventComponent;
+    oldVevent: VcalVeventComponent;
+    inviteActions: InviteActions;
+    hasModifiedDateTimes?: boolean;
+    onEquivalentAttendees: (veventComponent: VcalVeventComponent, inviteActions: InviteActions) => Promise<void>;
+    isCreatingSingleEdit?: boolean;
+}) => {
+    const correctedSaveInviteActions = getCorrectedSaveInviteActions({
+        inviteActions,
+        newVevent,
+        oldVevent,
+        hasModifiedDateTimes,
+    });
+    if (isCreatingSingleEdit && correctedSaveInviteActions.type === INVITE_ACTION_TYPES.SEND_UPDATE) {
+        // We are creating a single edit, which is like creating a new event, not updating an existing one
+        correctedSaveInviteActions.type = INVITE_ACTION_TYPES.SEND_INVITATION;
+    }
+    await onEquivalentAttendees(newVevent, correctedSaveInviteActions);
+    const isSendInviteType = [INVITE_ACTION_TYPES.SEND_INVITATION, INVITE_ACTION_TYPES.SEND_UPDATE].includes(
+        correctedSaveInviteActions.type
+    );
+
+    const method = isSendInviteType ? ICAL_METHOD.REQUEST : undefined;
+    const correctedVevent = getInviteVeventWithUpdatedParstats(newVevent, oldVevent, method);
+
+    return { vevent: correctedVevent, inviteActions: correctedSaveInviteActions, isSendInviteType };
 };
