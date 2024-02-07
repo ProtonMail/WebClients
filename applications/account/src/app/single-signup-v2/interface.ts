47ea9cabbe918d1c18855543edb51692c6212e80
Alexey Karpov <alexey.karpov@proton.ch>
2024-02-07 16:48:55 +0100

    Fix optimistic handling of billing address for Pass signup

--- applications/account/src/app/single-signup-v2/interface.ts
+++ applications/account/src/app/single-signup-v2/interface.ts
@@ -42,6 +42,7 @@ export interface OptimisticOptions {
     currency: Currency;
     plan: Plan;
     planIDs: PlanIDs;
+    billingAddress: BillingAddress;
     checkResult: RequiredCheckResponse;
 }
 
