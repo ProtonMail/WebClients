bafd5ea5da663884b194374be9822993f3accf45
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-02-07 22:27:32 +0100

    Enable log access from Lobby
    - Logs are now accessible without user being logged in
    - Move ApplicationLogs component to @proton/pass
    - Fine-tune settings layout
    
    [IDTEAM-2433]
    
    Co-authored-by: Edvin CANDON <edvin.candon@proton.ch>

--- applications/pass/src/app/Views/Settings/Tabs/General.tsx
+++ applications/pass/src/app/Views/Settings/Tabs/General.tsx
@@ -1,11 +1,7 @@
 import { type FC } from 'react';
 
+import { ApplicationLogs } from '@proton/pass/components/Settings/ApplicationLogs';
 import { Display } from '@proton/pass/components/Settings/Display';
 import { Locale } from '@proton/pass/components/Settings/Locale';
 
-export const General: FC = () => (
-    <>
-        <Locale />
-        <Display />
-    </>
-);
+export const General: FC = () => [<Locale />, <Display />, <ApplicationLogs style={{ '--h-custom': '18.75rem' }} />];
