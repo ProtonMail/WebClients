e1c0318e03b4ab835468d7576b215f47ff1eab77
Alexey Karpov <alexey.karpov@proton.ch>
2024-02-08 08:46:12 +0100

    Updated the text for the credit invoice

--- packages/components/containers/invoices/InvoiceType.tsx
+++ packages/components/containers/invoices/InvoiceType.tsx
@@ -1,10 +1,11 @@
 import { c } from 'ttag';
 
 import { INVOICE_TYPE } from '@proton/shared/lib/constants';
+import { ChargebeeEnabled, UserModel } from '@proton/shared/lib/interfaces';
 
 import { Invoice } from './interface';
 
-const getType = (type: INVOICE_TYPE) => {
+const getType = (type: INVOICE_TYPE, chargebeeUser?: ChargebeeEnabled) => {
     switch (type) {
         case INVOICE_TYPE.OTHER:
             return c('Invoice type display as badge').t`Other`;
@@ -13,6 +14,9 @@ const getType = (type: INVOICE_TYPE) => {
         case INVOICE_TYPE.CANCELLATION:
             return c('Invoice type display as badge').t`Cancellation`;
         case INVOICE_TYPE.CREDIT:
+            if (chargebeeUser === ChargebeeEnabled.CHARGEBEE_FORCED) {
+                return c('Invoice type display as badge').t`Credit note`;
+            }
             return c('Invoice type display as badge').t`Credit`;
         case INVOICE_TYPE.DONATION:
             return c('Invoice type display as badge').t`Donation`;
@@ -35,10 +39,11 @@ const getType = (type: INVOICE_TYPE) => {
 
 interface Props {
     invoice: Invoice;
+    user?: UserModel;
 }
 
-const InvoiceType = ({ invoice }: Props) => {
-    return <>{getType(invoice.Type)}</>;
+const InvoiceType = ({ invoice, user }: Props) => {
+    return <>{getType(invoice.Type, user?.ChargebeeUser)}</>;
 };
 
 export default InvoiceType;
