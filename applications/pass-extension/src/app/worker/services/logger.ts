0e6f89c09744218f5a83dda36419593612666880
Edvin CANDON <edvin.candon@proton.ch>
2024-02-08 11:02:15 +0100

    Standardise logger effects
    - Create standard loginfo plugin
    
    [IDTEAM-2433]

--- applications/pass-extension/src/app/worker/services/logger.ts
+++ applications/pass-extension/src/app/worker/services/logger.ts
@@ -1,19 +1,14 @@
 import { type LogStorageData, createLogStore } from '@proton/pass/lib/logger/store';
 import { type ExtensionStorage, WorkerMessageType } from '@proton/pass/types';
 import { withPayloadLens } from '@proton/pass/utils/fp/lens';
-import { logger } from '@proton/pass/utils/logger';
+import { registerLoggerEffect } from '@proton/pass/utils/logger';
 
 import WorkerMessageBroker from '../channel';
 
 export const createLoggerService = (storage: ExtensionStorage<LogStorageData>) => {
     const { push, read } = createLogStore(storage);
 
-    logger.setLogOptions({
-        onLog: (log, originalLog) => {
-            if (ENV === 'development') originalLog(log);
-            push(log);
-        },
-    });
+    registerLoggerEffect(push);
 
     WorkerMessageBroker.registerMessage(WorkerMessageType.LOG_EVENT, withPayloadLens('log', push));
     WorkerMessageBroker.registerMessage(WorkerMessageType.LOG_REQUEST, async () => ({ logs: await read() }));
