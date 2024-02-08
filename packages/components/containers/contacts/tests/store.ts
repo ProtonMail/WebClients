77a71c8745302a3db00d1e806e3b812b39da2a8a
Mattias Svanström <mattias.svanstrom@proton.ch>
2024-02-08 15:31:01 +0100

    Unify cancel subscription flow

--- packages/components/containers/contacts/tests/store.ts
+++ packages/components/containers/contacts/tests/store.ts
@@ -1,6 +1,7 @@
 import { combineReducers, configureStore, createListenerMiddleware } from '@reduxjs/toolkit';
 
 import { plansReducer } from '@proton/account';
+import { calendarsReducer } from '@proton/calendar';
 import { type ProtonThunkArguments, sharedReducers } from '@proton/redux-shared-store';
 import { ignoredActions, ignoredPaths } from '@proton/redux-shared-store/sharedSerializable';
 
@@ -11,6 +12,7 @@ export const listenerMiddleware = createListenerMiddleware();
 const rootReducer = combineReducers({
     ...sharedReducers,
     ...plansReducer,
+    ...calendarsReducer,
 });
 export type RootState = ReturnType<typeof rootReducer>;
 export const setupStore = ({ preloadedState }: { preloadedState: Partial<RootState> }) => {
