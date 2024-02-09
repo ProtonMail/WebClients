2a1ba5cd9966d9b5b02e814ab1674c6f6bc0e85b
Irena <irena.zazoska@proton.ch>
2024-02-09 14:53:08 +0100

    B2B Add org info
    [IDTEAM-2790]

--- packages/pass/store/sagas/user/user-access.saga.ts
+++ packages/pass/store/sagas/user/user-access.saga.ts
@@ -1,10 +1,12 @@
 import { put, takeEvery } from 'redux-saga/effects';
 
-import { getUserAccess } from '@proton/pass/lib/user/user.requests';
+import { getUserAccess, getUserOrganization } from '@proton/pass/lib/user/user.requests';
 import { getUserAccessFailure, getUserAccessIntent, getUserAccessSuccess } from '@proton/pass/store/actions';
 import type { SafeUserAccessState } from '@proton/pass/store/reducers';
 import type { RootSagaOptions } from '@proton/pass/store/types';
-import { SessionLockStatus } from '@proton/pass/types';
+import type { MaybeNull} from '@proton/pass/types';
+import { PlanType, SessionLockStatus } from '@proton/pass/types';
+import type { Organization } from '@proton/shared/lib/interfaces';
 
 function* syncPlan({ getAuthStore }: RootSagaOptions, { meta }: ReturnType<typeof getUserAccessIntent>) {
     try {
@@ -13,7 +15,12 @@ function* syncPlan({ getAuthStore }: RootSagaOptions, { meta }: ReturnType<typeo
         if (!loggedIn || locked) throw new Error('Cannot fetch user plan');
 
         const access: SafeUserAccessState = yield getUserAccess();
-        yield put(getUserAccessSuccess(meta.request.id, access));
+        // we get organisation only for B2B users;
+        const organization: MaybeNull<Organization> = yield access.plan?.Type === PlanType.business
+            ? getUserOrganization()
+            : null;
+
+        yield put(getUserAccessSuccess(meta.request.id, { ...access, organization: organization }));
     } catch (error) {
         yield put(getUserAccessFailure(meta.request.id, error));
     }
