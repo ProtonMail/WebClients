3d8fdc6d9b5da6874076123424b1fa08f97e289f
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-02-12 10:51:04 +0100

    Add option to export data as CSV
    [IDTEAM-2751]

--- packages/pass/lib/export/types.ts
+++ packages/pass/lib/export/types.ts
@@ -1,18 +1,51 @@
-import type { ItemType, UnsafeItemRevision, VaultShareContent } from '@proton/pass/types';
+import type { ItemType, Maybe, UnsafeItemRevision, VaultShareContent } from '@proton/pass/types';
 
 export type ExportedItem<T extends ItemType = ItemType> = Omit<
     UnsafeItemRevision<T>,
     'revision' | 'revisionTime' | 'lastUseTime'
 >;
 
+export type ExportedCsvItem = {
+    type: ItemType;
+    name: string;
+    url: Maybe<string>;
+    username: Maybe<string>;
+    password: Maybe<string>;
+    note: Maybe<string>;
+    totp: Maybe<string>;
+    createTime: Maybe<string>;
+    modifyTime: Maybe<string>;
+};
+
 export type ExportedVault = VaultShareContent & { items: ExportedItem[] };
 
-export type ExportData = {
+export type ExportDataDefault = {
     encrypted: boolean;
     userId?: string;
     vaults: Record<string, ExportedVault>;
     version: string;
 };
 
-export type ExportOptions = { encrypted: true; passphrase: string } | { encrypted: false };
-export type ExportFormValues = { encrypted: boolean; passphrase: string };
+export type ExportData<T extends ExportFormat | undefined = undefined> = T extends ExportFormat.CSV
+    ? string
+    : T extends ExportFormat
+      ? ExportDataDefault
+      : string | ExportDataDefault;
+
+export enum ExportFormat {
+    DEFAULT = 'default',
+    ENCRYPTED = 'encrypted',
+    CSV = 'csv',
+}
+
+export const EXPORT_FORMAT_MAP: Record<ExportFormat, string> = {
+    [ExportFormat.DEFAULT]: 'zip',
+    [ExportFormat.ENCRYPTED]: 'pgp',
+    [ExportFormat.CSV]: 'csv',
+};
+
+export type ExportOptions =
+    | { format: ExportFormat.ENCRYPTED; passphrase: string }
+    | { format: Exclude<ExportFormat, ExportFormat.ENCRYPTED> };
+
+export type ExportFormValues = { format: ExportFormat; passphrase: string };
--- packages/pass/lib/validation/export.ts
+++ packages/pass/lib/validation/export.ts
@@ -1,8 +1,10 @@
 import type { FormikErrors } from 'formik';
 import { c } from 'ttag';
 
-import type { ExportFormValues } from '@proton/pass/lib/export/types';
+import { type ExportFormValues, ExportFormat } from '@proton/pass/lib/export/types';
 import { isEmptyString } from '@proton/pass/utils/string/is-empty-string';
 
 export const validateExportForm = (values: ExportFormValues): FormikErrors<ExportFormValues> =>
-    values.encrypted && isEmptyString(values.passphrase) ? { passphrase: c('Warning').t`Passphrase is required` } : {};
+    values.format === ExportFormat.ENCRYPTED && isEmptyString(values.passphrase)
+        ? { passphrase: c('Warning').t`Passphrase is required` }
+        : {};
