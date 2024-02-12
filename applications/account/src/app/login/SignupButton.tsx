de9582c06e8d6d0c2f55c2723034bff0db402562
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-02-12 07:40:40 +0100

    Use IPC for  "Create account" link instead of internal link

---
+++ applications/account/src/app/login/SignupButton.tsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+
+import { c } from 'ttag';
+
+import { Button } from '@proton/atoms';
+import { canInvokeInboxDesktopIPC } from '@proton/shared/lib/desktop/ipcHelpers';
+import { isElectronApp } from '@proton/shared/lib/helpers/desktop';
+
+import { type Paths } from '../content/helper';
+
+interface Props {
+    paths: Paths;
+}
+
+const SignupButton = ({ paths }: Props) => {
+    if (!paths.signup) {
+        return null;
+    }
+
+    const handleSignupDesktop = () => {
+        if (canInvokeInboxDesktopIPC) {
+            window.ipcInboxMessageBroker?.send('openExternal', 'https://account.proton.me/mail/signup');
+        }
+    };
+
+    if (isElectronApp) {
+        return (
+            <Button
+                key="desktop-signup"
+                shape="underline"
+                className="link link-focus text-nowrap"
+                onClick={handleSignupDesktop}
+            >
+                {c('Link').t`Create account`}
+            </Button>
+        );
+    }
+
+    return (
+        <Link key="signup" className="link link-focus text-nowrap" to={paths.signup}>
+            {c('Link').t`Create account`}
+        </Link>
+    );
+};
+
+export default SignupButton;
