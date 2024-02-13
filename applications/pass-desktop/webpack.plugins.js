bf950ef2fab3ce6b323418e9b420f5ca0ccc6d05
Dominik Jankovic <dominik.jankovic@proton.ch>
2024-02-13 15:07:26 +0100

    Disable ForkTsCheckerPlugin, OOMs when building on shared runner

--- applications/pass-desktop/webpack.plugins.js
+++ applications/pass-desktop/webpack.plugins.js
@@ -1,29 +1,8 @@
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const { DefinePlugin } = require('webpack');
 
 module.exports = [
     new DefinePlugin({
         ENV: JSON.stringify(process.env.NODE_ENV ?? 'development'),
         BUILD_TARGET: JSON.stringify('desktop'),
-    }),
-    new ForkTsCheckerWebpackPlugin({
-        async: true,
-        formatter: 'basic',
-        issue: {
-            include: (issue) => {
-                const warningLogs = true;
-                const errorLogs = true;
-
-                if (warningLogs && issue.severity === 'warning') {
-                    return true;
-                }
-
-                if (errorLogs && issue.severity === 'error') {
-                    return true;
-                }
-
-                return false;
-            },
-        },
     }),
 ];
