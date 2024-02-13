7f487f14adbabbe8ede274a3f6e57d40e23ccee9
Alexey Karpov <alexey.karpov@proton.ch>
2024-02-13 14:12:34 +0100

    Remove paypal credit option for VPN subscribers
    PAY-1546

--- packages/components/containers/payments/PayPalInfoMessage.tsx
+++ packages/components/containers/payments/PayPalInfoMessage.tsx
@@ -4,7 +4,7 @@ const PayPalInfoMessage = () => {
     const payPalInfoMessage = c('Info')
         .t`We will redirect you to PayPal in a new browser tab to complete this transaction. If you use any pop-up blockers, please disable them to continue.`;
 
-    return <div className="mb-4">{payPalInfoMessage}</div>;
+    return <div>{payPalInfoMessage}</div>;
 };
 
 export default PayPalInfoMessage;
