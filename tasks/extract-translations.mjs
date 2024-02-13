624fffd96f72a1137e1642a01ca617d621fe06c4
dhoko <agarroux@proton.ch>
2024-02-13 09:45:24 +0100

    doc

--- tasks/extract-translations.mjs
+++ tasks/extract-translations.mjs
@@ -1,3 +1,9 @@
+/**
+ * Command to extract source strings for translations
+ * @link {https://confluence.protontech.ch/display/CP/Localisation+on+Front-end+-+rules+and+best+practices}
+ * ex:
+ * `node tasks/extract-translations.mjs` output -> `po/template.pot`
+ */
 import path from "node:path";
 import { constants as FS_CONSTANTS } from "node:fs";
 import { mkdir, access, rm } from "node:fs/promises";
@@ -6,6 +12,11 @@ import { execaCommand } from "execa";
 const DEFAULT_LANGUAGE = "en";
 const PO_OUTPUT_FILE = "po/template.pot";
 
+/**
+ * Test if a path exists
+ * @param {string} itemPath path to a file/directory
+ * @return {Promise<bool>} True if it exists
+ */
 export const pathExists = async (itemPath) => {
     try {
         await access(itemPath, FS_CONSTANTS.R_OK);
@@ -18,6 +29,7 @@ export const pathExists = async (itemPath) => {
 async function main() {
     const rootDir = path.dirname(PO_OUTPUT_FILE);
 
+    // always clean previous file -> if we test on our own pc
     if (await pathExists(PO_OUTPUT_FILE)) {
         await rm(PO_OUTPUT_FILE);
     }
