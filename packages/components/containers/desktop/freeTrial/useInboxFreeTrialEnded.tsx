63c87c90109ffe7e957453a284ff24768e0102c8
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-02-13 21:06:48 +0100

    Store data on local storage when user reach end of trial

--- packages/components/containers/desktop/freeTrial/useInboxFreeTrialEnded.tsx
+++ packages/components/containers/desktop/freeTrial/useInboxFreeTrialEnded.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react';
+
+import { END_OF_TRIAL_KEY } from '@proton/shared/lib/desktop/desktopTypes';
+import { getItem } from '@proton/shared/lib/helpers/storage';
+
 // TODO do stuff
 const useInboxFreeTrialEnded = () => {
+    const [hasTrialEnded, setHasTrialEnded] = useState(false);
+
+    useEffect(() => {
+        const item = !!getItem(END_OF_TRIAL_KEY);
+        setHasTrialEnded(item);
+    }, []);
+
     return {
-        hasTrialEnded: true,
+        hasTrialEnded,
     };
 };
 
