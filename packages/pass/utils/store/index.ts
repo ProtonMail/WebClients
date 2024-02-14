525f7e6e76c901f036ad76fc6ec9c0f5b781322a
Edvin CANDON <edvin.candon@proton.ch>
2024-02-14 11:40:56 +0100

    Setup offline encryption scheme
    [IDTEAM-2766]

--- packages/pass/utils/store/index.ts
+++ packages/pass/utils/store/index.ts
@@ -1,13 +1,10 @@
 import { decodeUtf8Base64, encodeUtf8Base64 } from '@proton/crypto/lib/utils';
 import type { Maybe, Store } from '@proton/pass/types';
 
-export const encodedGetter =
-    (store: Store) =>
-    <T>(key: string, defaultValue: T) =>
-    (): string | T => {
-        const value = store.get(key);
-        return decodeUtf8Base64(value) ?? defaultValue;
-    };
+export const encodedGetter = (store: Store) => (key: string) => (): Maybe<string> => {
+    const value = store.get(key);
+    return decodeUtf8Base64(value);
+};
 
 export const encodedSetter = (store: Store) => (key: string) => (value: Maybe<string>) =>
     store.set(key, encodeUtf8Base64(value) ?? undefined);
