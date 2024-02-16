3ac84c0a0442837945c943d7d8b907f4eac1706a
Edvin CANDON <edvin.candon@proton.ch>
2024-02-16 19:29:47 +0100

    Offline service-worker support
    - behind build defined environment variable
    - cache critical offline assets on SW install
    
    [IDTEAM-2766]

--- applications/pass-desktop/webpack.plugins.js
+++ applications/pass-desktop/webpack.plugins.js
@@ -4,5 +4,6 @@ module.exports = [
     new DefinePlugin({
         ENV: JSON.stringify(process.env.NODE_ENV ?? 'development'),
         BUILD_TARGET: JSON.stringify('desktop'),
+        OFFLINE_SUPPORTED: false,
     }),
 ];
