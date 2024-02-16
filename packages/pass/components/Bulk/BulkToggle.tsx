9a07bca6dd1965f382dbc0c518c17dd65468aec2
Irena Zazoska <zazoska@protonmail.com>
2024-02-16 08:47:58 +0100

    Improve bulk items edit
    [IDTEAM-2784]

--- packages/pass/components/Bulk/BulkToggle.tsx
+++ packages/pass/components/Bulk/BulkToggle.tsx
@@ -6,7 +6,7 @@ import { Button } from '@proton/atoms/Button';
 import { Kbd } from '@proton/atoms/Kbd';
 import { Icon, Tooltip } from '@proton/components';
 import { useBulkSelect } from '@proton/pass/components/Bulk/BulkSelectProvider';
-import { shiftKey } from '@proton/shared/lib/helpers/browser';
+import { metaKey } from '@proton/shared/lib/helpers/browser';
 
 type Props = { disabled?: boolean };
 
@@ -18,24 +18,33 @@ export const BulkToggle: FC<Props> = ({ disabled }) => {
     }, [disabled]);
 
     return (
-        <Tooltip
-            key="bulk-toggle"
-            openDelay={500}
-            isOpen={bulk.enabled ? false : undefined}
-            originalPlacement={'bottom'}
-            title={<Kbd shortcut={shiftKey} />}
-        >
-            <Button
-                shape="solid"
-                size="small"
-                color="weak"
-                icon={!bulk.enabled}
-                onClick={bulk[bulk.enabled ? 'disable' : 'enable']}
-                title={c('Action').t`Bulk select items`}
-                disabled={disabled}
+        <div className="flex flex-row flex-nowrap overflow-hidden justify-end">
+            <Tooltip
+                key="bulk-toggle"
+                openDelay={500}
+                isOpen={bulk.enabled ? false : undefined}
+                originalPlacement={'bottom'}
+                title={<Kbd shortcut={metaKey} />}
             >
-                {bulk.enabled ? c('Action').t`Cancel` : <Icon name={'checkmark-triple'} />}
-            </Button>
-        </Tooltip>
+                <Button
+                    shape="solid"
+                    size="small"
+                    color="weak"
+                    onClick={bulk[bulk.enabled ? 'disable' : 'enable']}
+                    title={c('Action').t`Bulk select items`}
+                    disabled={disabled}
+                    className="flex gap-2 text-ellipsis text-sm text-semibold"
+                >
+                    {bulk.enabled ? (
+                        c('Action').t`Cancel`
+                    ) : (
+                        <>
+                            <Icon name="checkmark-triple" className="inline shrink-0" />
+                            <span className="hidden md:block">{c('Action').t`Multiple select`}</span>
+                        </>
+                    )}
+                </Button>
+            </Tooltip>
+        </div>
     );
 };
