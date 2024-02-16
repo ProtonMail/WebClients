67fbb5353ae55ee33ba446a2b1cf9e3bf9ed769d
Alexey Karpov <alexey.karpov@proton.ch>
2024-02-16 15:08:54 +0100

    Add pass-signup-upgrade type
    It allows more precise control of Chargebee upgrades

--- packages/components/payments/client-extensions/usePaymentFacade.ts
+++ packages/components/payments/client-extensions/usePaymentFacade.ts
@@ -242,7 +242,13 @@ export const usePaymentFacade = ({
             PAYMENT_METHOD_TYPES.CHARGEBEE_CARD,
             PAYMENT_METHOD_TYPES.CHARGEBEE_PAYPAL,
         ];
-        const flowsWithTaxCountry: PaymentMethodFlows[] = ['signup', 'signup-pass', 'signup-vpn', 'subscription'];
+        const flowsWithTaxCountry: PaymentMethodFlows[] = [
+            'signup',
+            'signup-pass',
+            'signup-pass-upgrade',
+            'signup-vpn',
+            'subscription',
+        ];
 
         const isNewAllowedMethod = methodsWithTaxCountry.includes(method);
         const isSavedAllowedMethod = !!savedMethod && methodsWithTaxCountry.includes(savedMethod.Type);
