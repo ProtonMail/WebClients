8ce7ab8fb7c880276fbf69abcc4b906c71a8014e
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-02-19 17:50:53 +0100

    Support viewing previous item revision
    - Refactor [ItemType]View components by creating [ItemType]Content components to reuse
    - Add history stats and button to credit card and alias items
    
    [IDTEAM-2621]

--- packages/pass/components/Item/CreditCard/CreditCard.view.tsx
+++ packages/pass/components/Item/CreditCard/CreditCard.view.tsx
@@ -1,87 +1,49 @@
 import { type FC } from 'react';
-import { useSelector } from 'react-redux';
 
-import { c } from 'ttag';
+import { c, msgid } from 'ttag';
 
-import { MaskedValueControl } from '@proton/pass/components/Form/Field/Control/MaskedValueControl';
-import { UpgradeControl } from '@proton/pass/components/Form/Field/Control/UpgradeControl';
-import { ValueControl } from '@proton/pass/components/Form/Field/Control/ValueControl';
-import { FieldsetCluster } from '@proton/pass/components/Form/Field/Layout/FieldsetCluster';
-import {
-    cardNumberHiddenValue,
-    cardNumberMask,
-    expDateMask,
-} from '@proton/pass/components/Form/Field/masks/credit-card';
-import { TextAreaReadonly } from '@proton/pass/components/Form/legacy/TextAreaReadonly';
+import { CreditCardContent } from '@proton/pass/components/Item/CreditCard/CreditCardContent';
+import { ItemViewHistoryStats } from '@proton/pass/components/Item/History/ItemViewHistoryStats';
+import { MoreInfoDropdown } from '@proton/pass/components/Layout/Dropdown/MoreInfoDropdown';
 import { ItemViewPanel } from '@proton/pass/components/Layout/Panel/ItemViewPanel';
 import type { ItemViewProps } from '@proton/pass/components/Views/types';
-import { UpsellRef } from '@proton/pass/constants';
-import { useDeobfuscatedItem } from '@proton/pass/hooks/useDeobfuscatedItem';
-import { formatExpirationDateMMYY } from '@proton/pass/lib/validation/credit-card';
-import { selectPassPlan } from '@proton/pass/store/selectors';
-import { UserPassPlan } from '@proton/pass/types/api/plan';
 
 export const CreditCardView: FC<ItemViewProps<'creditCard'>> = (itemViewProps) => {
-    const { data: item } = itemViewProps.revision;
-
-    const {
-        metadata: { note },
-        content: { cardholderName, expirationDate, number, verificationNumber, pin },
-    } = useDeobfuscatedItem(item);
-
-    const isFreePlan = useSelector(selectPassPlan) === UserPassPlan.FREE;
+    const { revision, handleHistoryClick } = itemViewProps;
+    const { createTime, modifyTime, revision: revisionNumber, shareId, itemId } = revision;
 
     return (
         <ItemViewPanel type="creditCard" {...itemViewProps}>
-            <FieldsetCluster mode="read" as="div">
-                <ValueControl clickToCopy icon="user" label={c('Label').t`Name on card`} value={cardholderName} />
-                {isFreePlan ? (
-                    <UpgradeControl
-                        icon="credit-card"
-                        label={c('Label').t`Card number`}
-                        upsellRef={UpsellRef.LIMIT_CC}
-                    />
-                ) : (
-                    <MaskedValueControl
-                        clickToCopy
-                        hidden
-                        hiddenValue={cardNumberHiddenValue(number)}
-                        icon="credit-card"
-                        label={c('Label').t`Card number`}
-                        mask={cardNumberMask(number)}
-                        value={number}
-                        clipboardValue={number}
-                    />
-                )}
-                <MaskedValueControl
-                    clickToCopy
-                    icon="calendar-today"
-                    label={c('Label').t`Expiration date`}
-                    mask={expDateMask}
-                    value={formatExpirationDateMMYY(expirationDate)}
-                />
-                <ValueControl
-                    clickToCopy
-                    hidden
-                    hiddenValue="••••"
-                    icon="shield"
-                    label={c('Label').t`Security code`}
-                    value={verificationNumber}
-                />
-                <ValueControl hidden hiddenValue="••••" icon="grid-3" label={c('Label').t`PIN`} value={pin} />
-            </FieldsetCluster>
-
-            {note && (
-                <FieldsetCluster mode="read" as="div">
-                    <ValueControl
-                        clickToCopy
-                        as={TextAreaReadonly}
-                        icon="note"
-                        label={c('Label').t`Note`}
-                        value={note}
-                    />
-                </FieldsetCluster>
-            )}
+            <CreditCardContent revision={revision} />
+            <ItemViewHistoryStats
+                createTime={createTime}
+                modifyTime={modifyTime}
+                handleHistoryClick={handleHistoryClick}
+            />
+            <MoreInfoDropdown
+                info={[
+                    {
+                        label: c('Label').t`Modified`,
+                        values: [
+                            c('Info').ngettext(
+                                msgid`${revisionNumber} time`,
+                                `${revisionNumber} times`,
+                                revisionNumber
+                            ),
+                        ],
+                    },
+                    {
+                        // translator: label for item identification number
+                        label: c('Label').t`Item ID`,
+                        values: [itemId],
+                    },
+                    {
+                        // translator: label for vault identification number
+                        label: c('Label').t`Vault ID`,
+                        values: [shareId],
+                    },
+                ]}
+            />
         </ItemViewPanel>
     );
 };
