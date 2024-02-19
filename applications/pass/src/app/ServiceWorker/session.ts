e58055c1be37f4f9ad15fb5ec4123d8848dc639f
Edvin CANDON <edvin.candon@proton.ch>
2024-02-19 16:50:29 +0100

    Fix setLocalKey queue in service worker

--- applications/pass/src/app/ServiceWorker/session.ts
+++ applications/pass/src/app/ServiceWorker/session.ts
@@ -1,7 +1,7 @@
 /**
  * Implements handlers for session-specific routes to manage concurrency
  * - Prevent concurrent requests to `refresh` route
- * - Prevent concurrent requests to `setLocalKey` route
+ * - Queue concurrent requests to `setLocalKey` route
  * - Queue session lock requests to avoid parallel calls
  *
  * These measures mitigate potential pitfalls and race conditions that may arise
@@ -18,8 +18,9 @@ const SESSION_LOCK_ROUTES = globToRegExp('/api/pass/v1/user/session/lock/*');
 
 export const matchRefreshRoute = (pathname: string): boolean => pathname === REFRESH_ROUTE;
 export const matchLockRoute = (pathname: string): boolean => SESSION_LOCK_ROUTES.test(pathname);
-export const matchSetLocalKeyRoute = (pathname: string): boolean => pathname === SET_LOCAL_KEY_ROUTE;
+export const matchSetLocalKeyRoute = (pathname: string, method: string): boolean =>
+    method === 'PUT' && pathname === SET_LOCAL_KEY_ROUTE;
 
 export const handleRefresh = fetchController.register(requestLockFactory());
 export const handleLock = fetchController.register(requestQueueFactory());
-export const handleSetLocalKey = fetchController.register(requestLockFactory());
+export const handleSetLocalKey = fetchController.register(requestQueueFactory());
