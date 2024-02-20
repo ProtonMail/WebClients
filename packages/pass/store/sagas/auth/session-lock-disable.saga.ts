5659437decfd91c8f9d990ec7a24dff0369b5a02
Edvin CANDON <edvin.candon@proton.ch>
2024-02-20 11:16:02 +0100

    Improve cross-tab cache synchronisation when deleting/mutating session lock
    [IDTEAM-2766]

--- packages/pass/store/sagas/auth/session-lock-disable.saga.ts
+++ packages/pass/store/sagas/auth/session-lock-disable.saga.ts
@@ -1,19 +1,37 @@
-import { put, takeLeading } from 'redux-saga/effects';
+import { put, select, takeLeading } from 'redux-saga/effects';
 
+import { generateCache } from '@proton/pass/lib/cache/generate';
 import {
     sessionLockDisableFailure,
     sessionLockDisableIntent,
     sessionLockDisableSuccess,
 } from '@proton/pass/store/actions';
 import type { WithSenderAction } from '@proton/pass/store/actions/enhancers/endpoint';
-import type { RootSagaOptions } from '@proton/pass/store/types';
+import type { RootSagaOptions, State } from '@proton/pass/store/types';
 
 function* disableSessionLockWorker(
-    { getAuthService }: RootSagaOptions,
+    { getAuthService, getAuthStore, setCache }: RootSagaOptions,
     { meta, payload }: WithSenderAction<ReturnType<typeof sessionLockDisableIntent>>
 ) {
     try {
-        yield getAuthService().deleteLock(payload.pin);
+        const state = (yield select()) as State;
+
+        /** Update the cache immediately after deleting the lock on the back-end.
+         * In the web application, when a lock is deleted, this change is propagated
+         * to other active tabs. Therefore, we need to ensure that the cache is
+         * refreshed and encrypted without the sessionLockToken as soon as possible */
+        yield getAuthService().deleteLock(payload.pin, {
+            onLockDeleted: async () => {
+                try {
+                    const authStore = getAuthStore();
+                    const keyPassword = authStore.getPassword() ?? '';
+                    const offlineKD = authStore.getOfflineKD();
+                    const cache = await generateCache({ keyPassword, offlineKD })(state);
+                    await setCache(cache);
+                } catch {}
+            },
+        });
+
         yield put(sessionLockDisableSuccess(meta.request.id, meta.sender?.endpoint));
     } catch (error) {
         yield put(sessionLockDisableFailure(meta.request.id, error, meta.sender?.endpoint));
