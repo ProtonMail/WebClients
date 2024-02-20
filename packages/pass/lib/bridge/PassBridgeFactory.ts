d9a65d857614a1cc071813462de28d0b50cebcc1
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2024-02-20 18:39:05 +0100

    Memoise share

--- packages/pass/lib/bridge/PassBridgeFactory.ts
+++ packages/pass/lib/bridge/PassBridgeFactory.ts
@@ -12,6 +12,7 @@ import { createVault } from '@proton/pass/lib/vaults/vault.requests';
 import type { ItemRevision, Api as PassApi } from '@proton/pass/types';
 import { first } from '@proton/pass/utils/array/first';
 import { prop } from '@proton/pass/utils/fp/lens';
+import { maxAgeMemoize } from '@proton/pass/utils/fp/memo';
 import { pipe } from '@proton/pass/utils/fp/pipe';
 import { and, truthy } from '@proton/pass/utils/fp/predicates';
 import { sortOn } from '@proton/pass/utils/fp/sort';
@@ -24,83 +25,97 @@ import unary from '@proton/utils/unary';
 
 import type { PassBridge, PassBridgeAliasItem, PassBridgeInitOptions } from './types';
 
+let passBridgeInstance: PassBridge | undefined;
+let hydrated = false;
+
 export const createPassBridge = (api: Api): PassBridge => {
-    exposeApi(api as PassApi);
+    return (
+        passBridgeInstance ||
+        (() => {
+            exposeApi(api as PassApi);
+            const PassCrypto = exposePassCrypto(createPassCrypto());
 
-    const PassCrypto = exposePassCrypto(createPassCrypto());
+            passBridgeInstance = {
+                ready: () => waitUntil(() => PassCrypto.ready, 250).then(() => true),
+                hydrate: async ({ user, addresses, authStore }: PassBridgeInitOptions) => {
+                    if (hydrated) return;
 
-    return {
-        ready: () => waitUntil(() => PassCrypto.ready, 250).then(() => true),
-        hydrate: async ({ user, addresses, authStore }: PassBridgeInitOptions) => {
-            await PassCrypto.hydrate({ user, addresses, keyPassword: authStore.getPassword(), clear: false });
-        },
-        user: {
-            getUserAccess,
-        },
-        vault: {
-            getDefault: async (hadVaultCallback) => {
-                const encryptedShares = await requestShares();
-                const shares = (await Promise.all(encryptedShares.map(unary(parseShareResponse)))).filter(truthy);
-                const candidates = shares
-                    .filter(and(isActiveVault, isWritableVault, isOwnVault))
-                    .sort(sortOn('createTime', 'ASC'));
+                    await PassCrypto.hydrate({ user, addresses, keyPassword: authStore.getPassword(), clear: false });
+                    hydrated = true;
+                },
+                user: {
+                    getUserAccess,
+                },
+                vault: {
+                    getDefault: maxAgeMemoize(async (hadVaultCallback) => {
+                        const encryptedShares = await requestShares();
+                        const shares = (await Promise.all(encryptedShares.map(unary(parseShareResponse)))).filter(
+                            truthy
+                        );
+                        const candidates = shares
+                            .filter(and(isActiveVault, isWritableVault, isOwnVault))
+                            .sort(sortOn('createTime', 'ASC'));
 
-                const defaultVault = first(candidates);
-                if (defaultVault) {
-                    hadVaultCallback?.(true);
-                    return defaultVault;
-                } else {
-                    hadVaultCallback?.(false);
-                    const newVault = await createVault({
-                        content: {
-                            name: 'Personal',
-                            description: 'Personal vault (created from Mail)',
-                            display: {},
-                        },
-                    });
-                    return newVault;
-                }
-            },
-        },
-        alias: {
-            create: async ({ shareId, name, note, alias: { aliasEmail, mailbox, prefix, signedSuffix } }) => {
-                const itemUuid = uniqueId();
+                        const defaultVault = first(candidates);
+                        if (defaultVault) {
+                            hadVaultCallback?.(true);
+                            return defaultVault;
+                        } else {
+                            hadVaultCallback?.(false);
+                            const newVault = await createVault({
+                                content: {
+                                    name: 'Personal',
+                                    description: 'Personal vault (created from Mail)',
+                                    display: {},
+                                },
+                            });
+                            return newVault;
+                        }
+                    }),
+                },
+                alias: {
+                    create: async ({ shareId, name, note, alias: { aliasEmail, mailbox, prefix, signedSuffix } }) => {
+                        const itemUuid = uniqueId();
 
-                const encryptedItem = await createAlias({
-                    content: {},
-                    createTime: getEpoch(),
-                    extraData: { aliasEmail, mailboxes: [mailbox], prefix, signedSuffix },
-                    extraFields: [],
-                    metadata: { itemUuid, name, note: obfuscate(note ?? '') },
-                    optimisticId: itemUuid,
-                    shareId,
-                    type: 'alias',
-                });
+                        const encryptedItem = await createAlias({
+                            content: {},
+                            createTime: getEpoch(),
+                            extraData: { aliasEmail, mailboxes: [mailbox], prefix, signedSuffix },
+                            extraFields: [],
+                            metadata: { itemUuid, name, note: obfuscate(note ?? '') },
+                            optimisticId: itemUuid,
+                            shareId,
+                            type: 'alias',
+                        });
 
-                const item = (await parseItemRevision(shareId, encryptedItem)) as ItemRevision<'alias'>;
+                        const item = (await parseItemRevision(shareId, encryptedItem)) as ItemRevision<'alias'>;
 
-                return {
-                    item: { ...item, aliasEmail },
-                    aliasDetails: { aliasEmail, mailboxes: [mailbox] },
-                };
-            },
-            getAliasOptions,
-            getAllByShareId: async (shareId) => {
-                const aliases = (await Promise.all(
-                    (await requestAllItemsForShareId({ shareId, OnlyAlias: true }))
-                        .filter(pipe(prop('AliasEmail'), truthy))
-                        .map((item) => parseItemRevision(shareId, item))
-                )) as ItemRevision<'alias'>[];
+                        return {
+                            item: { ...item, aliasEmail },
+                            aliasDetails: { aliasEmail, mailboxes: [mailbox] },
+                        };
+                    },
+                    getAliasOptions,
+                    getAllByShareId: async (shareId) => {
+                        const aliases = (await Promise.all(
+                            (await requestAllItemsForShareId({ shareId, OnlyAlias: true }))
+                                .filter(pipe(prop('AliasEmail'), truthy))
+                                .map((item) => parseItemRevision(shareId, item))
+                        )) as ItemRevision<'alias'>[];
 
-                return Promise.all(
-                    aliases.map(
-                        async (item): Promise<PassBridgeAliasItem> => ({
-                            item,
-                            aliasDetails: await getAliasDetails(shareId, item.itemId),
-                        })
-                    )
-                );
-            },
-        },
-    };
+                        return Promise.all(
+                            aliases.map(
+                                async (item): Promise<PassBridgeAliasItem> => ({
+                                    item,
+                                    aliasDetails: await getAliasDetails(shareId, item.itemId),
+                                })
+                            )
+                        );
+                    },
+                },
+            };
+
+            return passBridgeInstance;
+        })()
+    );
 };
