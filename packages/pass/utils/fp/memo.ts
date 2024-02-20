7e01029bda88f1d2b90098e94c438c6da43872f8
Edvin CANDON <edvin.candon@proton.ch>
2024-02-20 11:49:13 +0100

    Add `maxAgeMemoize` utility

---
+++ packages/pass/utils/fp/memo.ts
@@ -0,0 +1,26 @@
+import type { AsyncCallback, Callback } from '@proton/pass/types';
+import { getEpoch } from '@proton/pass/utils/time/epoch';
+import lastItem from '@proton/utils/lastItem';
+
+type MaxAgeOptions = { maxAge: number };
+
+/** Memoizes the result of an asynchronous function with a specified maximum age.
+ * Returns a wrapped function that accepts a trailing `maxAge` parameter in seconds */
+export const maxAgeMemoize = <F extends AsyncCallback>(fn: F) => {
+    const cache: Map<string, { calledAt: number; result: ReturnType<F> }> = new Map();
+
+    return (async (...args: [...Parameters<F>, options: MaxAgeOptions]) => {
+        const calledAt = getEpoch();
+        const { maxAge } = lastItem(args) as MaxAgeOptions;
+
+        const memoArgs = JSON.stringify(args.slice(0, -1));
+        const cached = cache.get(memoArgs);
+
+        if (cached && calledAt - cached.calledAt < maxAge) return cached.result;
+        else {
+            const result = await fn(...args);
+            cache.set(memoArgs, { calledAt, result });
+            return result;
+        }
+    }) as F extends Callback ? (...args: [...Parameters<F>, options: MaxAgeOptions]) => ReturnType<F> : never;
+};
