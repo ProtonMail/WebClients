931be7668158de88980339313eecbf17173cf8cb
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2024-02-20 18:39:05 +0100

    Memoise share

--- packages/components/components/drawer/views/SecurityCenter/PassAliases/PassAliases.helpers.ts
+++ packages/components/components/drawer/views/SecurityCenter/PassAliases/PassAliases.helpers.ts
@@ -1,5 +1,6 @@
 import type { PassBridge, PassBridgeAliasItem } from '@proton/pass/lib/bridge/types';
 import { isTrashed } from '@proton/pass/lib/items/item.predicates';
+import { UNIX_DAY } from '@proton/pass/utils/time/constants';
 
 export const filterPassAliases = (aliases: PassBridgeAliasItem[]) => {
     const filterNonTrashedItems = ({ item }: PassBridgeAliasItem) => !isTrashed(item);
@@ -16,9 +17,12 @@ export const filterPassAliases = (aliases: PassBridgeAliasItem[]) => {
 export const fetchPassBridgeInfos = async (PassBridge: PassBridge) => {
     let userHadVault = false;
 
-    const defaultVault = await PassBridge.vault.getDefault((hadVault) => {
-        userHadVault = hadVault;
-    });
+    const defaultVault = await PassBridge.vault.getDefault(
+        (hadVault) => {
+            userHadVault = hadVault;
+        },
+        { maxAge: UNIX_DAY * 1 }
+    );
 
     const aliases = await PassBridge.alias.getAllByShareId(defaultVault.shareId);
     return { aliases, vault: defaultVault, userHadVault };
