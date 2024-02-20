6b505b8bd63f8b067fb33a5c6dd9ad7cf0050996
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2024-02-20 16:38:17 +0100

    Export memo type

--- packages/pass/utils/fp/memo.ts
+++ packages/pass/utils/fp/memo.ts
@@ -2,16 +2,16 @@ import type { AsyncCallback, Callback } from '@proton/pass/types';
 import { getEpoch } from '@proton/pass/utils/time/epoch';
 import lastItem from '@proton/utils/lastItem';
 
-type MaxAgeOptions = { maxAge: number };
+export type MaxAgeMemoizeOptions = { maxAge: number };
 
 /** Memoizes the result of an asynchronous function with a specified maximum age.
  * Returns a wrapped function that accepts a trailing `maxAge` parameter in seconds */
 export const maxAgeMemoize = <F extends AsyncCallback>(fn: F) => {
     const cache: Map<string, { calledAt: number; result: ReturnType<F> }> = new Map();
 
-    return (async (...args: [...Parameters<F>, options: MaxAgeOptions]) => {
+    return (async (...args: [...Parameters<F>, options: MaxAgeMemoizeOptions]) => {
         const calledAt = getEpoch();
-        const { maxAge } = lastItem(args) as MaxAgeOptions;
+        const { maxAge } = lastItem(args) as MaxAgeMemoizeOptions;
 
         const memoArgs = JSON.stringify(args.slice(0, -1));
         const cached = cache.get(memoArgs);
@@ -22,5 +22,5 @@ export const maxAgeMemoize = <F extends AsyncCallback>(fn: F) => {
             cache.set(memoArgs, { calledAt, result });
             return result;
         }
-    }) as F extends Callback ? (...args: [...Parameters<F>, options: MaxAgeOptions]) => ReturnType<F> : never;
+    }) as F extends Callback ? (...args: [...Parameters<F>, options: MaxAgeMemoizeOptions]) => ReturnType<F> : never;
 };
