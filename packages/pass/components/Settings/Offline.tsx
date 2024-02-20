3e84aa5391219da4ffb10e761a91494860b6b949
Edvin CANDON <edvin.candon@proton.ch>
2024-02-20 16:54:39 +0100

    Offline setup available for paid users only
    [IDTEAM-2766]

--- packages/pass/components/Settings/Offline.tsx
+++ packages/pass/components/Settings/Offline.tsx
@@ -4,20 +4,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { c } from 'ttag';
 
 import { Checkbox } from '@proton/components/index';
+import { ConfirmPasswordModal } from '@proton/pass/components/Confirmation/ConfirmPasswordModal';
+import { UpgradeButton } from '@proton/pass/components/Layout/Button/UpgradeButton';
+import { UpsellRef } from '@proton/pass/constants';
 import { useActionRequest } from '@proton/pass/hooks/useActionRequest';
 import { useAsyncModalHandles } from '@proton/pass/hooks/useAsyncModalHandles';
+import { isPaidPlan } from '@proton/pass/lib/user/user.predicates';
 import { offlineDisable, offlineSetupIntent } from '@proton/pass/store/actions';
-import { selectOfflineSupported } from '@proton/pass/store/selectors';
+import { selectOfflineEnabled, selectPassPlan } from '@proton/pass/store/selectors';
 import { BRAND_NAME, PASS_APP_NAME } from '@proton/shared/lib/constants';
 
-import { ConfirmPasswordModal } from '../Confirmation/ConfirmPasswordModal';
 import { SettingsPanel } from './SettingsPanel';
 
 export const Offline: FC = () => {
     const dispatch = useDispatch();
     const passwordConfirmModal = useAsyncModalHandles<string>();
-    const supported = useSelector(selectOfflineSupported);
+    const supported = useSelector(selectOfflineEnabled);
     const setup = useActionRequest({ action: offlineSetupIntent });
+    const plan = useSelector(selectPassPlan);
+    const disabled = !isPaidPlan(plan);
 
     const toggleOffline = async (enable: boolean) => {
         if (enable) {
@@ -31,11 +36,22 @@ export const Offline: FC = () => {
 
     return (
         <>
-            <SettingsPanel title={c('Label').t`Offline mode`}>
+            <SettingsPanel
+                title={c('Label').t`Offline mode`}
+                {...(disabled
+                    ? {
+                          subTitle: c('Info').t`Please upgrade your plan to enable offline support.`,
+                          contentClassname: 'opacity-50 pointer-events-none py-4',
+                          actions: [
+                              <UpgradeButton upsellRef={UpsellRef.SETTING} inline className="text-sm" key="upgrade" />,
+                          ],
+                      }
+                    : {})}
+            >
                 <Checkbox
                     checked={supported}
-                    disabled={false}
-                    loading={false}
+                    disabled={setup.loading || disabled}
+                    loading={setup.loading}
                     onChange={(e) => toggleOffline(e.target.checked)}
                 >
                     <span>
