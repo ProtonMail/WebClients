fce1873a05767f9ff19e6fdb19ca926703e35db8
Edvin CANDON <edvin.candon@proton.ch>
2024-02-20 11:16:02 +0100

    Improve cross-tab cache synchronisation when deleting/mutating session lock
    [IDTEAM-2766]

--- packages/pass/store/sagas/auth/session-lock-enable.saga.ts
+++ packages/pass/store/sagas/auth/session-lock-enable.saga.ts
@@ -1,6 +1,7 @@
 import { put, select, takeLeading } from 'redux-saga/effects';
 
 import { deleteSessionLock } from '@proton/pass/lib/auth/session-lock';
+import { generateCache } from '@proton/pass/lib/cache/generate';
 import {
     sessionLockEnableFailure,
     sessionLockEnableIntent,
@@ -8,10 +9,10 @@ import {
 } from '@proton/pass/store/actions';
 import type { WithSenderAction } from '@proton/pass/store/actions/enhancers/endpoint';
 import { selectHasRegisteredLock } from '@proton/pass/store/selectors';
-import type { RootSagaOptions } from '@proton/pass/store/types';
+import type { RootSagaOptions, State } from '@proton/pass/store/types';
 
 function* enableSessionLockWorker(
-    { getAuthService }: RootSagaOptions,
+    { getAuthService, getAuthStore, setCache }: RootSagaOptions,
     { meta, payload: { pin, ttl } }: WithSenderAction<ReturnType<typeof sessionLockEnableIntent>>
 ) {
     try {
@@ -19,7 +20,24 @@ function* enableSessionLockWorker(
          * previous one. This will happen during a lock TTL update. */
         if ((yield select(selectHasRegisteredLock)) as boolean) yield deleteSessionLock(pin);
 
-        const sessionLockToken: string = yield getAuthService().createLock(pin, ttl);
+        const state: State = yield select();
+
+        /** Update the cache immediately after enavling the lock on the back-end.
+         * In the web application, when a lock is created or updated, this change is
+         * propagated to other active tabs. Therefore, we need to ensure that the cache is
+         * refreshed and encrypted with the new sessionLockToken as soon as possible */
+        const sessionLockToken: string = yield getAuthService().createLock(pin, ttl, {
+            onLockCreated: async (sessionLockToken) => {
+                try {
+                    const authStore = getAuthStore();
+                    const keyPassword = authStore.getPassword() ?? '';
+                    const offlineKD = authStore.getOfflineKD();
+                    const cache = await generateCache({ keyPassword, offlineKD, sessionLockToken })(state);
+                    await setCache(cache);
+                } catch {}
+            },
+        });
+
         yield put(sessionLockEnableSuccess(meta.request.id, { sessionLockToken, ttl }, meta.sender?.endpoint));
     } catch (error) {
         yield put(sessionLockEnableFailure(meta.request.id, error, meta.sender?.endpoint));
