c7154f09e114a5457689c92d58f759ad2054b242
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2024-02-21 15:55:22 +0100

    Re-hydrate pass crypto on user or adresses change

--- packages/pass/lib/bridge/PassBridgeProvider.tsx
+++ packages/pass/lib/bridge/PassBridgeProvider.tsx
@@ -1,7 +1,7 @@
 import type { FC, PropsWithChildren } from 'react';
-import { createContext, useContext, useEffect } from 'react';
+import { createContext, useContext } from 'react';
 
-import { useAddresses, useApi, useAuthentication, useUser } from '@proton/components/hooks';
+import { useApi } from '@proton/components/hooks';
 import useInstance from '@proton/hooks/useInstance';
 import type { MaybeNull } from '@proton/pass/types';
 
@@ -12,15 +12,7 @@ export const PassBridgeContext = createContext<MaybeNull<PassBridge>>(null);
 
 export const PassBridgeProvider: FC<PropsWithChildren> = ({ children }) => {
     const api = useApi();
-    const [user] = useUser();
-    const [addresses] = useAddresses();
-    const authStore = useAuthentication();
-
     const bridge = useInstance(() => createPassBridge(api));
-
-    useEffect(() => {
-        void bridge.hydrate({ user, addresses: addresses ?? [], authStore });
-    }, []);
 
     return <PassBridgeContext.Provider value={bridge}>{children}</PassBridgeContext.Provider>;
 };
