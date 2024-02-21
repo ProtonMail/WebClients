06a38900e5b7c0665718c513948fe167c3d5399a
Alexey Karpov <alexey.karpov@proton.ch>
2024-02-21 22:04:56 +0300

    Load chargebee script synchronously

--- packages/chargebee/src/main.ts
+++ packages/chargebee/src/main.ts
@@ -1,7 +1,3 @@
 import { initialize } from './chargebee-entry';
 
-async function main() {
-    void initialize();
-}
-
-void main();
+document.addEventListener('DOMContentLoaded', () => initialize());
