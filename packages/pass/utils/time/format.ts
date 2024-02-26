f6c9e43e99f0d2b330afd92cd113467c1460d079
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-02-26 17:33:49 +0100

    Improve item history dates format
    [IDTEAM-2621]

--- packages/pass/utils/time/format.ts
+++ packages/pass/utils/time/format.ts
@@ -1,4 +1,7 @@
-import { format } from 'date-fns';
+import { format, formatRelative, isThisWeek } from 'date-fns';
+
+import { dateLocale } from '@proton/shared/lib/i18n';
+import capitalize from '@proton/utils/capitalize';
 
 export const getFormattedDateFromTimestamp = (timestamp: number) => {
     return `${format(new Date(timestamp * 1000), 'dd MMM yyyy, HH:mmaaa')}`;
@@ -6,4 +9,12 @@ export const getFormattedDateFromTimestamp = (timestamp: number) => {
 
 export const getFormattedDayFromTimestamp = (timestamp: number) => {
     return format(new Date(timestamp * 1000), 'dd MMM yyyy');
+};
+
+export const getRelativeDateFromTimestamp = (timestamp: number) => {
+    const date = new Date(timestamp * 1000);
+    if (isThisWeek(date)) {
+        return `${capitalize(formatRelative(date, new Date(), { locale: dateLocale }))}`;
+    }
+    return getFormattedDateFromTimestamp(timestamp);
 };
