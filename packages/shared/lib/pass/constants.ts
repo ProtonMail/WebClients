7a72f7fa027f380b16e9257840b46904b1378b00
Jack Crompton <jack.crompton@proton.ch>
2024-02-28 14:52:43 +0100

    Remove PassWindowsDownload feature flag

--- packages/shared/lib/pass/constants.ts
+++ packages/shared/lib/pass/constants.ts
@@ -21,48 +21,40 @@ export enum Clients {
     Edge,
 }
 
-export const getDownloadablePassClients = ({
-    includeWindows = true,
-}: {
-    includeWindows?: boolean;
-} = {}): { [key in Clients]: Client } => {
-    return {
-        [Clients.Windows]: {
-            title: 'Windows',
-            link: includeWindows ? 'https://proton.me/download/PassDesktop/win32/x64/ProtonPass_Setup.exe' : '',
-            icon: 'brand-windows',
-        },
-        [Clients.Android]: {
-            title: 'Android',
-            link: 'https://play.google.com/store/apps/details?id=proton.android.pass',
-            icon: 'brand-android',
-        },
-        [Clients.iOS]: {
-            title: 'iOS',
-            link: 'https://apps.apple.com/us/app/id6443490629',
-            icon: 'brand-apple',
-        },
-        [Clients.Chrome]: {
-            title: 'Chrome',
-            link: 'https://chrome.google.com/webstore/detail/proton-pass/ghmbeldphafepmbegfdlkpapadhbakde',
-            icon: 'brand-chrome',
-        },
-        [Clients.Brave]: {
-            title: 'Brave',
-            link: 'https://chrome.google.com/webstore/detail/proton-pass/ghmbeldphafepmbegfdlkpapadhbakde',
-            icon: 'brand-brave',
-        },
-        [Clients.Edge]: {
-            title: 'Edge',
-            link: 'https://chrome.google.com/webstore/detail/proton-pass/ghmbeldphafepmbegfdlkpapadhbakde',
-            icon: 'brand-edge',
-        },
-        [Clients.Firefox]: {
-            title: 'Firefox',
-            link: 'https://addons.mozilla.org/en-US/firefox/addon/proton-pass',
-            icon: 'brand-firefox',
-        },
-    } as const;
-};
-
-export const clients = getDownloadablePassClients();
+export const clients: { [key in Clients]: Client } = {
+    [Clients.Windows]: {
+        title: 'Windows',
+        link: 'https://proton.me/download/PassDesktop/win32/x64/ProtonPass_Setup.exe',
+        icon: 'brand-windows',
+    },
+    [Clients.Android]: {
+        title: 'Android',
+        link: 'https://play.google.com/store/apps/details?id=proton.android.pass',
+        icon: 'brand-android',
+    },
+    [Clients.iOS]: {
+        title: 'iOS',
+        link: 'https://apps.apple.com/us/app/id6443490629',
+        icon: 'brand-apple',
+    },
+    [Clients.Chrome]: {
+        title: 'Chrome',
+        link: 'https://chrome.google.com/webstore/detail/proton-pass/ghmbeldphafepmbegfdlkpapadhbakde',
+        icon: 'brand-chrome',
+    },
+    [Clients.Brave]: {
+        title: 'Brave',
+        link: 'https://chrome.google.com/webstore/detail/proton-pass/ghmbeldphafepmbegfdlkpapadhbakde',
+        icon: 'brand-brave',
+    },
+    [Clients.Edge]: {
+        title: 'Edge',
+        link: 'https://chrome.google.com/webstore/detail/proton-pass/ghmbeldphafepmbegfdlkpapadhbakde',
+        icon: 'brand-edge',
+    },
+    [Clients.Firefox]: {
+        title: 'Firefox',
+        link: 'https://addons.mozilla.org/en-US/firefox/addon/proton-pass',
+        icon: 'brand-firefox',
+    },
+} as const;
