10a6f20eed3260b1975249f037a14b2f348f6acf
Dominik Jankovic <dominik.jankovic@proton.ch>
2024-02-28 17:59:35 +0100

    Set correct download category

--- applications/pass-desktop/tools/deploy.sh
+++ applications/pass-desktop/tools/deploy.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 set -euo pipefail
 
+if [ -z "${CI_COMMIT_TAG}" ]; then
+  echo "CI_COMMIT_TAG is not set."
+  exit 1
+fi
+
+if [ -z "${PASS_DESKTOP_DEPLOY_REPO}" ]; then
+  echo "PASS_DESKTOP_DEPLOY_REPO is not set."
+  exit 1
+fi
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_ROOT="${SCRIPT_DIR//\/tools/}"
 GIT_COMMIT_AUTHOR="${GITLAB_USER_NAME}"
 GIT_COMMIT_EMAIL="${GITLAB_USER_EMAIL}"
 VERSION="$(jq -r .version < "$PROJECT_ROOT/package.json")"
+# shellcheck disable=SC2001
+TAG_VERSION=$( echo "$CI_COMMIT_TAG" | sed 's/.*@//' )
 
-if [ -z "${PASS_DESKTOP_DEPLOY_REPO}" ]; then
-  echo "PASS_DESKTOP_DEPLOY_REPO is not set."
-  exit 1
+# Deployment is EarlyAccess if its version contains a dash, eg. 1.0.0-rc1
+CATEGORY="Stable"
+if [[ "${TAG_VERSION}" == *"-"* ]]; then
+  CATEGORY="EarlyAccess"
 fi
 
 # Check if git-lfs is available (or auto-install it under CI)
@@ -36,7 +49,9 @@ git config --local user.email "${GIT_COMMIT_EMAIL}"
 # Copy Windows artefacts
 mkdir -p "${repoDir}/PassDesktop/win32/x64"
 cp "${PROJECT_ROOT}/out/make/squirrel.windows/x64/"* "${repoDir}/PassDesktop/win32/x64/"
-cp "${PROJECT_ROOT}/out/make/squirrel.windows/x64/ProtonPass_Setup_${VERSION}.exe" "${repoDir}/PassDesktop/win32/x64/ProtonPass_Setup.exe"
+if [[ "${CATEGORY}" == "Stable" ]]; then
+  cp "${PROJECT_ROOT}/out/make/squirrel.windows/x64/ProtonPass_Setup_${VERSION}.exe" "${repoDir}/PassDesktop/win32/x64/ProtonPass_Setup.exe"
+fi
 git lfs track "*.exe" "*.nupkg"
 
 # Update version.json
@@ -50,7 +65,7 @@ if grep -q "$sha" "${repoDir}/PassDesktop/win32/x64/version.json"; then
   echo "SHA already exists in version.json"
   exit 1
 fi
-newrelease="{\"CategoryName\":\"EarlyAccess\", \"Version\": \"${VERSION}\", \"ReleaseDate\": \"${now}\", \"RolloutPercentage\": 0, \"File\": { \"Url\": \"https://proton.me/download/PassDesktop/win32/x64/ProtonPass_Setup_${VERSION}.exe\", \"Sha512CheckSum\": \"${sha}\", \"Args\": \"\" }}"
+newrelease="{\"CategoryName\":\"${CATEGORY}\", \"Version\": \"${VERSION}\", \"ReleaseDate\": \"${now}\", \"RolloutPercentage\": 0, \"File\": { \"Url\": \"https://proton.me/download/PassDesktop/win32/x64/ProtonPass_Setup_${VERSION}.exe\", \"Sha512CheckSum\": \"${sha}\", \"Args\": \"\" }}"
 < "${repoDir}/PassDesktop/win32/x64/version.json" jq ".Releases |= [$newrelease] + ." > version.json.tmp
 mv version.json.tmp "${repoDir}/PassDesktop/win32/x64/version.json"
 
