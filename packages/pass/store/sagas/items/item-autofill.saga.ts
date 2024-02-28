7457e602f57f4f08843aac100a120681438c9b93
Edvin CANDON <edvin.candon@proton.ch>
2024-02-28 17:54:27 +0100

    Fix autofill candidates sort

--- packages/pass/store/sagas/items/item-autofill.saga.ts
+++ packages/pass/store/sagas/items/item-autofill.saga.ts
@@ -3,12 +3,18 @@ import { put, takeEvery } from 'redux-saga/effects';
 import { parseItemRevision } from '@proton/pass/lib/items/item.parser';
 import { updateItemLastUseTime } from '@proton/pass/lib/items/item.requests';
 import { itemAutofilled, itemEditSync } from '@proton/pass/store/actions';
+import type { RootSagaOptions } from '@proton/pass/store/types';
 import type { ItemRevision, ItemRevisionContentsResponse } from '@proton/pass/types';
 import { logId, logger } from '@proton/pass/utils/logger';
 
-function* itemAutofilledWorker({ payload: { shareId, itemId } }: ReturnType<typeof itemAutofilled>) {
+function* itemAutofilledWorker(
+    { onItemsUpdated }: RootSagaOptions,
+    { payload: { shareId, itemId } }: ReturnType<typeof itemAutofilled>
+) {
     try {
         logger.info(`[Item::Autofill] used item ${logId(itemId)} on share ${logId(shareId)}`);
+        onItemsUpdated?.(); /* will re-order items for autofill suggestions */
+
         const encryptedItem: ItemRevisionContentsResponse = yield updateItemLastUseTime(shareId, itemId);
         const item: ItemRevision = yield parseItemRevision(shareId, encryptedItem);
         yield put(itemEditSync({ shareId, itemId, item }));
@@ -20,6 +26,6 @@ function* itemAutofilledWorker({ payload: { shareId, itemId } }: ReturnType<type
     }
 }
 
-export default function* watcher() {
-    yield takeEvery(itemAutofilled.match, itemAutofilledWorker);
+export default function* watcher(options: RootSagaOptions) {
+    yield takeEvery(itemAutofilled.match, itemAutofilledWorker, options);
 }
