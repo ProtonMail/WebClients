4279cd50179e6cc9de7653627b091e25bb2a447d
Dominik Jankovic <dominik.jankovic@proton.ch>
2024-03-01 11:49:39 +0100

    Disable undismissible HMR overlay

--- applications/pass-desktop/forge.config.js
+++ applications/pass-desktop/forge.config.js
@@ -34,6 +34,9 @@ const config = {
         new WebpackPlugin({
             devContentSecurityPolicy: `default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;`,
             devServer: {
+                client: {
+                    overlay: false,
+                },
                 liveReload: false,
             },
             mainConfig,
