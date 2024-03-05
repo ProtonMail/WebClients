7182b36fc2729b1298b226fbd808b1ffa26aa316
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-03-05 16:59:05 +0100

    Fix totp code not updating in item history diff
    [IDTEAM-2621]

--- packages/pass/types/worker/otp.ts
+++ packages/pass/types/worker/otp.ts
@@ -2,7 +2,7 @@ import type { SelectedItem } from '../data';
 
 export type OtpCode = { token: string; period: number; expiry: number };
 
-export type OtpRequest = SelectedItem & { totpUri?: string } & (
+export type OtpRequest = SelectedItem & { totpUri?: string; revisionNumber?: number } & (
         | { type: 'item' }
         | { type: 'extraField'; index: number }
     );
