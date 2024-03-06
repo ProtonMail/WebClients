bc654aa63c58fadf9f39aed474ed1905e2ff5daa
Edvin CANDON <edvin.candon@proton.ch>
2024-03-06 10:56:50 +0100

    Add conditional address validation in vault invite create modal
    [IDTEAM-2792]

--- packages/pass/lib/validation/vault-invite.ts
+++ packages/pass/lib/validation/vault-invite.ts
@@ -1,4 +1,4 @@
-import type { RefObject } from 'react';
+import type { MutableRefObject, RefObject } from 'react';
 
 import { type FormikErrors } from 'formik';
 
@@ -10,56 +10,63 @@ export enum InviteEmailsError {
     DUPLICATE = 'DUPLICATE',
     INVALID = 'INVALID',
     EMPTY = 'EMPTY',
-    LIMITED = 'LIMITED', // Limited to organization members
+    EXTERNAL = 'EXTERNAL',
 }
 
-export const validateShareInviteValues = (emailFieldRef: RefObject<HTMLInputElement>) => (values: InviteFormValues) => {
-    if (values.step === 'vault' && values.withVaultCreation) return validateVaultValues(values);
+type ValidateShareInviteOptions = {
+    emailField: RefObject<HTMLInputElement>;
+    validateAddresses: boolean;
+    validationMap: MutableRefObject<Map<string, boolean>>;
+};
 
-    let errors: FormikErrors<InviteFormValues> = {};
+export const validateShareInviteValues =
+    ({ emailField, validateAddresses, validationMap }: ValidateShareInviteOptions) =>
+    (values: InviteFormValues) => {
+        if (values.step === 'vault' && values.withVaultCreation) return validateVaultValues(values);
 
-    if (values.step === 'members') {
-        const emails = values.members.reduce<{ errors: string[]; pass: boolean; seen: Set<string> }>(
-            (acc, { value }) => {
-                if (values.orgEmails && values.orgEmails?.indexOf(value.email) === -1) {
-                    acc.errors.push(InviteEmailsError.LIMITED);
-                    acc.pass = false;
+        let errors: FormikErrors<InviteFormValues> = {};
+
+        if (values.step === 'members') {
+            const emails = values.members.reduce<{ errors: string[]; pass: boolean; seen: Set<string> }>(
+                (acc, { value }) => {
+                    if (acc.seen.has(value.email)) {
+                        acc.errors.push(InviteEmailsError.DUPLICATE);
+                        acc.pass = false;
+                    } else if (!validateEmailAddress(value.email)) {
+                        acc.pass = false;
+                        acc.errors.push(InviteEmailsError.INVALID);
+                    } else if (validateAddresses && validationMap.current.get(value.email) === false) {
+                        acc.errors.push(InviteEmailsError.EXTERNAL);
+                        acc.pass = false;
+                    } else acc.errors.push('');
+
+                    acc.seen.add(value.email);
+
+                    return acc;
+                },
+                { errors: [], pass: true, seen: new Set() }
+            );
+
+            errors.members = emails.errors;
+
+            /** Validate the trailing input value only when it is not
+             * focused : this value lives outside of the formik state
+             * values - as such adapt the validation logic accordingly */
+            if (emailField.current !== document.activeElement) {
+                const trailing = emailField.current?.value?.trim() ?? '';
+                const validTrailingEmail = validateEmailAddress(trailing);
+
+                if (emails.errors.length === 0 && !validTrailingEmail) {
+                    emails.pass = false;
+                    errors.members.push(InviteEmailsError.EMPTY);
+                } else if (trailing && !validTrailingEmail) {
+                    emails.pass = false;
+                    errors.members.push(InviteEmailsError.INVALID);
                 }
-                if (acc.seen.has(value.email)) {
-                    acc.errors.push(InviteEmailsError.DUPLICATE);
-                    acc.pass = false;
-                } else if (!validateEmailAddress(value.email)) {
-                    acc.pass = false;
-                    acc.errors.push(InviteEmailsError.INVALID);
-                } else acc.errors.push('');
-
-                acc.seen.add(value.email);
-
-                return acc;
-            },
-            { errors: [], pass: true, seen: new Set() }
-        );
-
-        errors.members = emails.errors;
-
-        /** Validate the trailing input value only when it is not
-         * focused : this value lives outside of the formik state
-         * values - as such adapt the validation logic accordingly */
-        if (emailFieldRef.current !== document.activeElement) {
-            const trailing = emailFieldRef.current?.value?.trim() ?? '';
-            const validTrailingEmail = validateEmailAddress(trailing);
-
-            if (emails.errors.length === 0 && !validTrailingEmail) {
-                emails.pass = false;
-                errors.members.push(InviteEmailsError.EMPTY);
-            } else if (trailing && !validTrailingEmail) {
-                emails.pass = false;
-                errors.members.push(InviteEmailsError.INVALID);
             }
+
+            if (emails.pass) delete errors.members;
         }
 
-        if (emails.pass) delete errors.members;
-    }
-
-    return errors;
-};
+        return errors;
+    };
