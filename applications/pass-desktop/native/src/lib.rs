54a27952f6aa9dea30f160248c2ee6fcfd1cfb88
Dominik Jankovic <dominik.jankovic@proton.ch>
2024-03-06 14:18:05 +0100

    Add Rust biometrics bindings (NAPI)

---
+++ applications/pass-desktop/native/src/lib.rs
@@ -0,0 +1,49 @@
+#![deny(clippy::all)]
+
+mod biometrics;
+
+#[macro_use]
+extern crate napi_derive;
+
+#[napi]
+pub mod biometric {
+    use napi::bindgen_prelude::Buffer;
+
+    use super::biometrics::*;
+
+    #[napi]
+    pub async fn can_check_presence() -> napi::Result<bool> {
+        Biometrics::can_check_presence().map_err(|e| napi::Error::from_reason(e.to_string()))
+    }
+    
+    #[napi]
+    pub async fn check_presence(handle: Buffer, reason: String) -> napi::Result<bool> {
+        Biometrics::check_presence(handle.into(), reason).map_err(|e| napi::Error::from_reason(e.to_string()))
+    }
+
+    #[napi]
+    pub async fn get_decryption_key(challenge: Option<&str>) -> napi::Result<Vec<String>> {
+        Biometrics::get_decryption_key(challenge)
+            .map(|v| v.into())
+            .map_err(|e| napi::Error::from_reason(e.to_string()))
+    }
+
+    #[napi]
+    pub async fn get_secret(key: String) -> napi::Result<String> {
+        Biometrics::get_secret(key).map_err(|e| napi::Error::from_reason(e.to_string()))
+    }
+
+    #[napi]
+    pub async fn set_secret(
+        key: String,
+        secret: String,
+    ) -> napi::Result<()> {
+        Biometrics::set_secret(key, secret).map_err(|e| napi::Error::from_reason(e.to_string()))
+    }
+
+    #[napi]
+    pub async fn delete_secret(key: String) -> napi::Result<()> {
+        Biometrics::delete_secret(key).map_err(|e| napi::Error::from_reason(e.to_string()))
+    }
+
+}
--- applications/pass-desktop/src/preload.ts
+++ applications/pass-desktop/src/preload.ts
@@ -2,10 +2,25 @@ import { contextBridge, ipcRenderer } from 'electron';
 
 export type ContextBridgeApi = {
     writeToClipboard: (text: string) => Promise<void>;
+    canCheckPresence: () => Promise<boolean>;
+    checkPresence: (reason?: string) => Promise<boolean>;
+    getDecryptionKey: (challenge: string) => Promise<Buffer | null>;
+    getSecret: (key: string) => Promise<string>;
+    setSecret: (key: string, data: string) => Promise<void>;
+    deleteSecret: (key: string) => Promise<void>;
 };
 
 const contextBridgeApi: ContextBridgeApi = {
+    // clipboard
     writeToClipboard: (text: string) => ipcRenderer.invoke('clipboard:writeText', text),
+
+    // secrets
+    canCheckPresence: () => ipcRenderer.invoke('biometrics:canCheckPresence'),
+    checkPresence: (reason?: string) => ipcRenderer.invoke('biometrics:checkPresence', reason),
+    getDecryptionKey: (challenge: string) => ipcRenderer.invoke('biometrics:getDecryptionKey', challenge),
+    getSecret: (key: string) => ipcRenderer.invoke('biometrics:getSecret', key),
+    setSecret: (key: string, data: string) => ipcRenderer.invoke('biometrics:setSecret', key, data),
+    deleteSecret: (key: string) => ipcRenderer.invoke('biometrics:deleteSecret', key),
 };
 
 contextBridge.exposeInMainWorld('ctxBridge', contextBridgeApi);
