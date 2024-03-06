9c0687ae919db9d46a7a6f3f5241e2f23a943122
Jack Crompton <jack.crompton@proton.ch>
2024-03-06 17:21:36 +0100

    Persist password scope when enabling 2 password mode
    CP-7306

--- packages/components/containers/account/changePasswordHelper.ts
+++ packages/components/containers/account/changePasswordHelper.ts
@@ -2,12 +2,20 @@ import { updatePassword } from '@proton/shared/lib/api/settings';
 import { Api } from '@proton/shared/lib/interfaces';
 import { srpVerify } from '@proton/shared/lib/srp';
 
-export const handleChangeLoginPassword = async ({ api, newPassword }: { api: Api; newPassword: string }) => {
+export const handleChangeLoginPassword = async ({
+    api,
+    newPassword,
+    persistPasswordScope = false,
+}: {
+    api: Api;
+    newPassword: string;
+    persistPasswordScope?: boolean;
+}) => {
     return srpVerify({
         api,
         credentials: {
             password: newPassword,
         },
-        config: updatePassword(),
+        config: updatePassword({ PersistPasswordScope: persistPasswordScope }),
     });
 };
