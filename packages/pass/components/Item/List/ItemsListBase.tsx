35c85d440380ccb1a8c38739761c196dc6eedfc0
Edvin CANDON <edvin.candon@proton.ch>
2024-03-07 21:22:14 +0100

    Improve `ItemsListPlaceholder` UX
    - Add scrollable content
    - Fix alignments
    
    [PP-99]

--- packages/pass/components/Item/List/ItemsListBase.tsx
+++ packages/pass/components/Item/List/ItemsListBase.tsx
@@ -1,12 +1,16 @@
 import { type FC, type ReactElement, useEffect, useMemo, useRef } from 'react';
 import type { List } from 'react-virtualized';
 
+import { Scroll } from '@proton/atoms/Scroll';
 import { useBulkSelect } from '@proton/pass/components/Bulk/BulkSelectProvider';
 import { ItemsListItem } from '@proton/pass/components/Item/List/ItemsListItem';
 import { VirtualList } from '@proton/pass/components/Layout/List/VirtualList';
 import { itemEq } from '@proton/pass/lib/items/item.predicates';
 import { interpolateRecentItems } from '@proton/pass/lib/items/item.utils';
 import type { ItemFilters, ItemRevision, ItemRevisionWithOptimistic, SelectedItem } from '@proton/pass/types';
+import clsx from '@proton/utils/clsx';
+
+import './ItemsListBase.scss';
 
 type Props = {
     filters: ItemFilters;
@@ -32,9 +36,11 @@ export const ItemsListBase: FC<Props> = ({ items, filters, selectedItem, onSelec
     return (
         <>
             {items.length === 0 ? (
-                <div className="flex justify-center items-center w-full m-auto overflow-x-auto p-3">
-                    {placeholder()}
-                </div>
+                <Scroll className="pass-items-list--placeholder">
+                    <div className={clsx('flex justify-center items-center w-full m-auto pt-8 pb-14 min-h-full')}>
+                        {placeholder()}
+                    </div>
+                </Scroll>
             ) : (
                 <VirtualList
                     interpolationIndexes={interpolationIndexes}
