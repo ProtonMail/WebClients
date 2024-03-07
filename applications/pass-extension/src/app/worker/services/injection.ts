11551e965c323b55faa39541f637145b5dac05cb
Edvin CANDON <edvin.candon@proton.ch>
2024-03-07 09:33:07 +0100

    Allow passing cancellation callback to `waitUntil`

--- applications/pass-extension/src/app/worker/services/injection.ts
+++ applications/pass-extension/src/app/worker/services/injection.ts
@@ -5,10 +5,12 @@ import { backgroundMessage } from '@proton/pass/lib/extension/message';
 import browser from '@proton/pass/lib/globals/browser';
 import type { Maybe } from '@proton/pass/types';
 import { type TabId, WorkerMessageType } from '@proton/pass/types';
+import type { PassElementsConfig } from '@proton/pass/types/utils/dom';
 import { logger } from '@proton/pass/utils/logger';
-import noop from '@proton/utils/noop';
+import { uniqueId } from '@proton/pass/utils/string/unique-id';
 
 import WorkerMessageBroker from '../channel';
+import { PASS_ELEMENTS_KEY } from '../constants';
 
 const withTabEffect =
     (fn: (tabId: TabId, frameId: Maybe<number>) => Promise<void>) =>
@@ -31,6 +33,7 @@ export const createInjectionService = () => {
         frameId?: Maybe<number>;
         js: string[];
         css?: string[];
+        world?: 'MAIN' | 'ISOLATED';
     }) => {
         const { tabId } = options;
         const frameIds = options.frameId ? [options.frameId] : undefined;
@@ -45,8 +48,12 @@ export const createInjectionService = () => {
                 browser.scripting.executeScript({
                     target: { tabId, allFrames: options.allFrames ?? false, frameIds },
                     files: options.js,
+                    world: options.world as any,
                 }),
-        ]).catch((e) => logger.info(`[InjectionService::inject] Injection error on tab ${tabId}`, e));
+        ]).catch((e) => {
+            console.warn(e);
+            logger.info(`[InjectionService::inject] Injection error on tab ${tabId}`, e);
+        });
     };
 
     const updateInjections = async () => {
@@ -54,19 +61,41 @@ export const createInjectionService = () => {
         await Promise.all(
             tabs
                 .filter((tab) => !tab.url?.includes('pass.proton.'))
-                .map((tab) => {
+                .map(async (tab) => {
                     logger.info(`[InjectionService::update] Re-injecting script on tab ${tab.id}`);
                     if (tab.id !== undefined) {
                         /* FIXME: re-inject in all frames when supporting iframes */
-                        inject({ tabId: tab.id, allFrames: false, js: ['orchestrator.js'] }).catch(noop);
+                        // await inject({ tabId: tab.id, allFrames: false, js: ['elements.js'], world: 'MAIN' });
+                        await inject({ tabId: tab.id, allFrames: false, js: ['orchestrator.js'] });
                     }
                 })
         );
     };
 
+    WorkerMessageBroker.registerMessage(WorkerMessageType.REGISTER_ELEMENTS, async (_, { tab }) => {
+        const target = { tabId: tab?.id!, allFrames: false };
+        const world = 'MAIN' as any;
+
+        const elements: PassElementsConfig = {
+            root: `protonpass-root-${uniqueId(4)}`,
+            control: `protonpass-control-${uniqueId(4)}`,
+        };
+
+        await browser.scripting.executeScript({
+            target,
+            world,
+            args: [{ [PASS_ELEMENTS_KEY]: elements }],
+            func: (key) => Object.assign(self, key),
+        });
+
+        await browser.scripting.executeScript({ target, world, files: ['elements.js'] });
+
+        return { elements };
+    });
+
     WorkerMessageBroker.registerMessage(
         WorkerMessageType.LOAD_CONTENT_SCRIPT,
-        withTabEffect((tabId, frameId) => inject({ tabId, frameId, js: ['client.js'], css: ['styles/client.css'] }))
+        withTabEffect((tabId, frameId) => inject({ tabId, frameId, js: ['client.js'] }))
     );
 
     WorkerMessageBroker.registerMessage(
