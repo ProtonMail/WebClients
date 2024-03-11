04e9e1d8a67352591b29f2dfb7d5e5998e88ec3b
romain sanchez <romain.sanchez@proton.ch>
2024-03-11 10:14:41 +0100

    Hide Add participant button in ICS widget for single edits CALWEB-5038

--- applications/mail/src/app/components/message/extras/calendar/ExtraEventOrganizerButtons.tsx
+++ applications/mail/src/app/components/message/extras/calendar/ExtraEventOrganizerButtons.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from 'react';
 
 import { ICAL_METHOD } from '@proton/shared/lib/calendar/constants';
+import { getHasRecurrenceId } from '@proton/shared/lib/calendar/vcalHelper';
 import { RequireSome } from '@proton/shared/lib/interfaces/utils';
 
 import { InvitationModel, getHasInvitationApi } from '../../../../helpers/calendar/invite';
@@ -12,18 +13,19 @@ interface Props {
 }
 const ExtraEventOrganizerButtons = ({ model, setModel }: Props) => {
     const {
-        invitationIcs: { method },
+        invitationIcs: { method, vevent },
         isPartyCrasher,
         singleEditData,
     } = model;
 
-    const hasSingleEdits = singleEditData && singleEditData.length > 0;
+    const hasSingleEdits = !!singleEditData?.length;
+    const isSingleEdit = getHasRecurrenceId(vevent);
 
     if (!getHasInvitationApi(model)) {
         return null;
     }
 
-    if (method === ICAL_METHOD.REPLY && isPartyCrasher && !hasSingleEdits) {
+    if (method === ICAL_METHOD.REPLY && isPartyCrasher && !hasSingleEdits && !isSingleEdit) {
         return <ExtraEventAddParticipantButton model={model} setModel={setModel} />;
     }
 
