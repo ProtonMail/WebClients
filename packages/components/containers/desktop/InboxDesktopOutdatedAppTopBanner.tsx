1b1507d71a3c4fdba4ff79d546dcb402514b9722
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-03-11 13:30:05 +0100

    Update version.json format

--- packages/components/containers/desktop/InboxDesktopOutdatedAppTopBanner.tsx
+++ packages/components/containers/desktop/InboxDesktopOutdatedAppTopBanner.tsx
@@ -1,14 +1,31 @@
 import { c } from 'ttag';
 
+import { getAppHref } from '@proton/shared/lib/apps/helper';
+import { APPS } from '@proton/shared/lib/constants';
 import {
     electronAppVersion,
     isElectronApp,
-    isElectronOnMac,
-    isElectronOnWindows,
+    isElectronOnLinux as isLinux,
+    isElectronOnMac as isMac,
+    isElectronOnWindows as isWindows,
 } from '@proton/shared/lib/helpers/desktop';
 
 import TopBanner from '../topBanners/TopBanner';
-import useInboxDesktopVersion, { DesktopClient } from './useInboxDesktopVersion';
+import useInboxDesktopVersion, { DesktopVersion } from './useInboxDesktopVersion';
+
+/**
+ * Linux is different from Windows and MacOS. There is no auto updates, this means that any user running a verison that is not the latest
+ * is running an outdated version. A manual update is required if that's the case.
+ * The latests version is always the first element in the array, if the current version is not at index 0 then it's outdated.
+ *
+ * @param currentVersion Current version of the electron app
+ * @param linuxVersions Array of all Linux versions coming from the version.json file
+ * @returns
+ */
+const isLinuxOutdated = (currentVersion: string, linuxVersions: DesktopVersion[]) => {
+    const currentVersionIndex = linuxVersions.findIndex(({ Version }) => Version === currentVersion);
+    return currentVersionIndex > 0;
+};
 
 /**
  * Returns true if the application needs to be manually updated.
@@ -16,54 +33,68 @@ import useInboxDesktopVersion, { DesktopClient } from './useInboxDesktopVersion'
  * We return false
  * 1. If the latests version in version.json is the same as the installed version
  * 2. If the latests version in version.json also requires ManualUpdate array
- *    For example, if the latest version is 0.9.1 and the ManualUpdate is [0.9.0, 0.9.1], no prompt since current version requires manual update.
+ *
+ * For example, if the latest version is 0.9.1 and the ManualUpdate is [0.9.0, 0.9.1], no prompt since current version requires manual update.
  */
-const doesEarlyVersionNeedsManualUpdate = (app: DesktopClient, version: string) => {
-    if (app.early.Version === version || (app.early.Version && app.early.ManualUpdate?.includes(app.early.Version))) {
+const doesEarlyVersionNeedsManualUpdate = (app: DesktopVersion, version: string) => {
+    if (!app.ManualUpdate || app.Version === version || app.ManualUpdate.includes(app.Version)) {
         return false;
     }
 
-    return app?.early.ManualUpdate?.includes(version);
+    return app?.ManualUpdate?.includes(version);
 };
 
-const DownloadButton = ({ app }: { app: DesktopClient }) => {
+const DownloadButton = ({ link }: { link: string }) => {
     return (
-        <a
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link align-baseline text-left"
-            href={app.early.File.Url}
-        >
+        <a target="_blank" rel="noopener noreferrer" className="link align-baseline text-left" href={link}>
             {c('Action').t`Download now`}
         </a>
     );
 };
 
+const DisplayTopBanner = ({ displayTopBanner, link }: { displayTopBanner: boolean; link?: string }) => {
+    if (!link || !displayTopBanner) {
+        return null;
+    }
+
+    const downloadUpdate = <DownloadButton link={link} />;
+    return (
+        <TopBanner className="bg-info">{c('Action')
+            .jt`Important update available. To continue to use the app, please update to the latest version. ${downloadUpdate}`}</TopBanner>
+    );
+};
+
 const InboxDesktopOutdatedAppTopBanner = () => {
     const version = electronAppVersion;
-    const { windowsApp, macosApp, loading } = useInboxDesktopVersion();
+    const { windowsApp, macosApp, linuxApp, allLinuxVersions, loading } = useInboxDesktopVersion();
 
     if (!isElectronApp || !version || loading) {
         return null;
     }
 
-    if (isElectronOnMac && macosApp && doesEarlyVersionNeedsManualUpdate(macosApp, version)) {
-        const downloadUpdate = <DownloadButton app={macosApp} key="download-update-macos" />;
-        return (
-            <TopBanner className="bg-info">{c('Action')
-                .jt`Important update available. To continue to use the app, please update to the latest version. ${downloadUpdate}`}</TopBanner>
-        );
-    }
+    const displayMac = (isMac && macosApp && doesEarlyVersionNeedsManualUpdate(macosApp, version)) || false;
+    const displayWindows = (isWindows && windowsApp && doesEarlyVersionNeedsManualUpdate(windowsApp, version)) || false;
+    const displayLinux = (isLinux && linuxApp && isLinuxOutdated(version, allLinuxVersions)) || false;
 
-    if (isElectronOnWindows && windowsApp && doesEarlyVersionNeedsManualUpdate(windowsApp, version)) {
-        const downloadUpdate = <DownloadButton app={windowsApp} key="download-update-windows" />;
-        return (
-            <TopBanner className="bg-info">{c('Action')
-                .jt`Important update available. To continue to use the app, please update to the latest version. ${downloadUpdate}`}</TopBanner>
-        );
-    }
-
-    return null;
+    return (
+        <>
+            <DisplayTopBanner
+                displayTopBanner={displayMac}
+                link={macosApp?.File?.[0].Url}
+                key="download-update-macos"
+            />
+            <DisplayTopBanner
+                displayTopBanner={displayWindows}
+                link={macosApp?.File?.[0].Url}
+                key="download-update-windows"
+            />
+            <DisplayTopBanner
+                displayTopBanner={displayLinux}
+                link={getAppHref('/get-the-apps#proton-mail-desktop-apps', APPS.PROTONACCOUNT)}
+                key="download-update-linux"
+            />
+        </>
+    );
 };
 
 export default InboxDesktopOutdatedAppTopBanner;
