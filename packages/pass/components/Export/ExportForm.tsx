2b42e4e99cc8d639e2e1c22eb821ac29b8b38a8b
Irena Zazoska <irena.zazoska@proton.ch>
2024-03-12 15:28:41 +0100

    B2B Admin can prevent exports
    [IDTEAM-2892]

--- packages/pass/components/Export/ExportForm.tsx
+++ packages/pass/components/Export/ExportForm.tsx
@@ -8,13 +8,17 @@ import { Button } from '@proton/atoms/Button';
 import { RadioGroupField } from '@proton/pass/components/Form/Field/RadioGroupField';
 import { PasswordField } from '@proton/pass/components/Form/legacy/PasswordField';
 import { Card } from '@proton/pass/components/Layout/Card/Card';
+import { useOrganization } from '@proton/pass/components/Organization/OrganizationProvider';
 import { type ExportFormValues, ExportFormat } from '@proton/pass/lib/export/types';
 import { selectNonOwnedVaults } from '@proton/pass/store/selectors';
+import { BitField } from '@proton/pass/types';
 
 type ExporterProps = { form: FormikContextType<ExportFormValues>; loading: boolean };
 
 export const ExportForm: FC<ExporterProps> = ({ form, loading = false }) => {
     const hasNonOwnedVaults = useSelector(selectNonOwnedVaults).length > 0;
+    const org = useOrganization();
+    const organizationExportLimit = org?.settings.ExportMode === BitField.ACTIVE;
 
     return (
         <FormikProvider value={form}>
@@ -40,6 +44,7 @@ export const ExportForm: FC<ExporterProps> = ({ form, loading = false }) => {
                         ]}
                         checked={form.values.format}
                         label={c('Label').t`File format`}
+                        disabled={organizationExportLimit}
                     />
                 </div>
 
@@ -76,11 +81,17 @@ export const ExportForm: FC<ExporterProps> = ({ form, loading = false }) => {
                     </em>
                 )}
 
+                {organizationExportLimit && (
+                    <Card className="mb-4 p-1">
+                        <div>{c('Info').t`This setting is disabled on the organization level`}</div>
+                    </Card>
+                )}
+
                 <Button
                     type="submit"
                     color="norm"
                     loading={loading}
-                    disabled={!form.isValid || loading}
+                    disabled={!form.isValid || loading || organizationExportLimit}
                     className="mt-3 w-full"
                 >
                     {c('Action').t`Export`}
