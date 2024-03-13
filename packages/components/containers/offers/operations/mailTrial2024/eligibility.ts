b6e3d5b5644dc0a4222aefad1198df2370b3a8a2
Richard Tetaz <richard.tetaz@proton.ch>
2024-03-13 20:01:23 +0100

    Review eligibility of March offer

--- packages/components/containers/offers/operations/mailTrial2024/eligibility.ts
+++ packages/components/containers/offers/operations/mailTrial2024/eligibility.ts
@@ -1,20 +1,34 @@
+import { fromUnixTime, isBefore } from 'date-fns';
+
 import { getAppFromPathnameSafe } from '@proton/shared/lib/apps/slugHelper';
 import { APPS } from '@proton/shared/lib/constants';
 import { ProtonConfig, UserModel } from '@proton/shared/lib/interfaces';
 
+const FREE_DOWNGRADER_LIMIT = new Date(Date.UTC(2024, 2, 20, 0, 0, 0));
+
 interface Props {
-    user?: UserModel;
+    user: UserModel;
     protonConfig: ProtonConfig;
+    lastSubscriptionEnd?: number;
 }
 
-const isEligible = ({ user, protonConfig }: Props) => {
+const isEligible = ({ user, protonConfig, lastSubscriptionEnd = 0 }: Props) => {
     const parentApp = getAppFromPathnameSafe(window.location.pathname);
     const hasValidApp =
         (protonConfig.APP_NAME === APPS.PROTONACCOUNT && parentApp === APPS.PROTONMAIL) ||
         (protonConfig.APP_NAME === APPS.PROTONACCOUNT && parentApp === APPS.PROTONCALENDAR) ||
         protonConfig.APP_NAME === APPS.PROTONCALENDAR ||
         protonConfig.APP_NAME === APPS.PROTONMAIL;
-    return hasValidApp && !!user?.isFree;
+    const { canPay, isDelinquent, isFree } = user;
+    const notDelinquent = !isDelinquent;
+
+    return (
+        hasValidApp &&
+        canPay &&
+        notDelinquent &&
+        isFree &&
+        isBefore(fromUnixTime(lastSubscriptionEnd), FREE_DOWNGRADER_LIMIT)
+    );
 };
 
 export default isEligible;
