35b768ebfb1dd877f4e21037f16a262c028cc34d
Edvin CANDON <edvin.candon@proton.ch>
2024-03-14 16:54:07 +0100

    Remove passkey domains for proxied settings
    [IDTEAM-2916]

--- packages/pass/store/reducers/settings.ts
+++ packages/pass/store/reducers/settings.ts
@@ -37,7 +37,6 @@ export type SettingsState = {
     sessionLockRegistered: boolean;
     sessionLockTTL?: number;
     offlineEnabled?: boolean;
-    passkeyDomains?: string[];
 };
 
 /* proxied settings will also be copied on local
