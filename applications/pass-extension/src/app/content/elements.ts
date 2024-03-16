0bc8722cca52a2f709fc6d2c96dc5841de0e1ef3
Edvin CANDON <edvin.candon@proton.ch>
2024-03-16 09:08:43 +0100

    Workaround for firefox custom elements registration
    - Rollback script injection strategy for firefox content-scripts
    - Firefox does not allow properly registrating custom elements in the
      main page-script realm : in order to alleviate this, use the
      script tag technique

--- applications/pass-extension/src/app/content/elements.ts
+++ applications/pass-extension/src/app/content/elements.ts
@@ -1,6 +1,7 @@
 /* This script should not be loaded in the content-script realm :
  * we need access to the customElements API which is either missing
  * or limited in the content-script execution context */
+import { exporter } from './bridge/utils';
 import { ProtonPassControl } from './injections/custom-elements/ProtonPassControl';
 import { ProtonPassRoot } from './injections/custom-elements/ProtonPassRoot';
 
@@ -8,9 +9,14 @@ import { ProtonPassRoot } from './injections/custom-elements/ProtonPassRoot';
  * global object to facilitate its invocation from an `executeScript`
  * function within the service worker. This setup enables passing the
  * `PassElementsConfig` to this script operating in the MAIN world */
-window.registerPassElements = (config) => {
-    const { root, control } = config;
-    if (!customElements.get(root)) customElements.define(root, ProtonPassRoot);
-    if (!customElements.get(control)) customElements.define(control, ProtonPassControl);
-    delete window.registerPassElements;
-};
+exporter(
+    (hash: string) => {
+        const root = ProtonPassRoot.getTagName(hash);
+        const control = ProtonPassControl.getTagName(hash);
+        if (!customElements.get(root)) customElements.define(root, ProtonPassRoot);
+        if (!customElements.get(control)) customElements.define(control, ProtonPassControl);
+        delete window.registerPassElements;
+    },
+    window,
+    { defineAs: 'registerPassElements' }
+);
