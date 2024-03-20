e0c3729285bd33b95d417fedfb3148e4d325136d
Pablo Molina <contact@pablomolina.me>
2024-03-20 10:24:04 +0100

    Refactor IPC communication to use a single channel
    Instead of creating a channel for IPC message type, we can reuse a single
    "clientUpdate" message and then put the payload type inside the message
    itself. This improves type safe when handling the received message.

--- src/ipc/main.ts
+++ src/ipc/main.ts
@@ -3,36 +3,56 @@ import { saveTrialStatus } from "../store/trialStore";
 import { clearStorage } from "../utils/helpers";
 import { setTrialEnded, updateView } from "../utils/view/viewManagement";
 import { handleIPCBadge, resetBadge, showNotification } from "./notification";
+import Logger from "electron-log";
+import { IPCMessage } from "./ipcConstants";
+
+function isValidMessage(message: unknown): message is IPCMessage {
+    return message && typeof message === "object" && "type" in message && "payload" in message;
+}
 
 export const handleIPCCalls = () => {
-    ipcMain.on("updateNotification", (_e, count: number) => {
-        handleIPCBadge(count);
-    });
-    ipcMain.on("userLogout", () => {
-        clearStorage(true, 500);
-        resetBadge();
-    });
-    ipcMain.on("clearAppData", () => {
-        clearStorage(true, 500);
-        resetBadge();
-    });
-    ipcMain.on("oauthPopupOpened", (_e, payload) => {
-        global.oauthProcess = payload === "oauthPopupStarted";
-    });
-    ipcMain.on("openExternal", (_e, url) => {
-        shell.openExternal(url);
-    });
-    ipcMain.on("trialEnd", (_e, payload) => {
-        saveTrialStatus(payload);
-
-        if (payload === "trialEnded") {
-            setTrialEnded();
+    ipcMain.on("clientUpdate", (_e, message: unknown) => {
+        if (!isValidMessage(message)) {
+            Logger.error(`Invalid clientUpdate message: ${message}`);
+            return;
         }
-    });
-    ipcMain.on("changeView", (_e, target) => {
-        updateView(target);
-    });
-    ipcMain.on("showNotification", (_e, payload) => {
-        showNotification(payload);
+
+        const { type, payload } = message;
+
+        switch (type) {
+            case "updateNotification":
+                handleIPCBadge(payload);
+                break;
+            case "userLogout":
+                clearStorage(true, 500);
+                resetBadge();
+                break;
+            case "clearAppData":
+                clearStorage(true, 500);
+                resetBadge();
+                break;
+            case "oauthPopupOpened":
+                global.oauthProcess = payload === "oauthPopupStarted";
+                break;
+            case "openExternal":
+                shell.openExternal(payload);
+                break;
+            case "trialEnd":
+                saveTrialStatus(payload);
+
+                if (payload === "trialEnded") {
+                    setTrialEnded();
+                }
+                break;
+            case "changeView":
+                updateView(payload);
+                break;
+            case "showNotification":
+                showNotification(payload);
+                break;
+            default:
+                Logger.error(`unknown message type: ${message}`);
+                break;
+        }
     });
 };
--- src/preload.ts
+++ src/preload.ts
@@ -5,34 +5,6 @@ import { IPCMessagePayload, IPCMessageType } from "./ipc/ipcConstants";
 contextBridge.exposeInMainWorld("ipcInboxMessageBroker", {
     send: <T extends IPCMessageType>(type: IPCMessageType, payload: IPCMessagePayload<T>) => {
         Logger.info(`Sending IPC message: ${type}`);
-        switch (type) {
-            case "updateNotification":
-                ipcRenderer.send("updateNotification", payload);
-                break;
-            case "userLogout":
-                ipcRenderer.send("userLogout");
-                break;
-            case "clearAppData":
-                ipcRenderer.send("userLogout");
-                break;
-            case "oauthPopupOpened":
-                ipcRenderer.send("oauthPopupOpened", payload);
-                break;
-            case "openExternal":
-                ipcRenderer.send("openExternal", payload);
-                break;
-            case "trialEnd":
-                ipcRenderer.send("trialEnd", payload);
-                break;
-            case "changeView":
-                ipcRenderer.send("changeView", payload);
-                break;
-            case "showNotification":
-                ipcRenderer.send("showNotification", payload);
-                break;
-            default:
-                Logger.error(`Unknown IPC message type: ${type}`);
-                break;
-        }
+        ipcRenderer.send("clientUpdate", { type, payload });
     },
 });
