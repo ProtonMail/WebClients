9f3b1918d9dc5b793358e2fd8547cc605a5a3fb9
Edvin CANDON <edvin.candon@proton.ch>
2024-03-21 16:28:51 +0100

    Prefer CSSStyleSheet API to circumvent CSP policies
    [IDTEAM-3012]

--- applications/pass-extension/src/app/content/injections/custom-elements/StyledShadowHost.ts
+++ applications/pass-extension/src/app/content/injections/custom-elements/StyledShadowHost.ts
@@ -1,11 +1,12 @@
 export class StyledShadowHost extends HTMLElement {
     constructor(styles: string) {
         super();
-
         const shadowRoot = this.attachShadow({ mode: 'open' });
-        const styleElement = document.createElement('style');
-        styleElement.innerText = styles;
 
-        shadowRoot.appendChild(styleElement);
+        /** Utilize CSSStyleSheet API to circumvent CSP restrictions
+         * on inline styles that could exist in target websites  */
+        const sheet = new CSSStyleSheet();
+        sheet.replaceSync(styles);
+        shadowRoot.adoptedStyleSheets.push(sheet);
     }
 }
