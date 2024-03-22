f543d5b5a77acb3dc9b299b259b6688164cb1511
Edvin CANDON <edvin.candon@proton.ch>
2024-03-22 16:49:07 +0100

    Fix key warnings in extension settings pages

--- applications/pass-extension/src/app/pages/settings/Views/Developer.tsx
+++ applications/pass-extension/src/app/pages/settings/Views/Developer.tsx
@@ -9,51 +9,49 @@ import Icon from '@proton/components/components/icon/Icon';
 import { pageMessage, sendMessage } from '@proton/pass/lib/extension/message';
 import { WorkerMessageType } from '@proton/pass/types';
 
-export const Developer: FC = () => {
-    return (
-        <>
-            <SettingsPanel title="Extension triggers">
-                <Button
-                    icon
-                    shape="ghost"
-                    className="w-full"
-                    onClick={() =>
-                        sendMessage(
-                            pageMessage({
-                                type: WorkerMessageType.DEBUG,
-                                payload: { debug: 'update_trigger' },
-                            })
-                        )
-                    }
-                >
-                    <div className="flex items-center flex items-center">
-                        <Icon name="brand-chrome" className="mr-2" />
-                        <span className="flex-1 text-left">Trigger update</span>
-                        <span className="text-xs color-weak">Triggers a fake update (keep popup opened)</span>
-                    </div>
-                </Button>
-                <Button
-                    icon
-                    shape="ghost"
-                    className="w-full"
-                    onClick={() =>
-                        sendMessage(
-                            pageMessage({
-                                type: WorkerMessageType.DEBUG,
-                                payload: { debug: 'storage_full' },
-                            })
-                        )
-                    }
-                >
-                    <div className="flex items-center flex items-center">
-                        <Icon name="drive" className="mr-2" />
-                        <span className="flex-1 text-left">Trigger full disk</span>
-                        <span className="text-xs color-weak">Triggers a fake disk full event (open popup after)</span>
-                    </div>
-                </Button>
-            </SettingsPanel>
-            <DropdownDebug />
-            <NotificationDebug />
-        </>
-    );
-};
+export const Developer: FC = () => (
+    <>
+        <SettingsPanel title="Extension triggers">
+            <Button
+                icon
+                shape="ghost"
+                className="w-full"
+                onClick={() =>
+                    sendMessage(
+                        pageMessage({
+                            type: WorkerMessageType.DEBUG,
+                            payload: { debug: 'update_trigger' },
+                        })
+                    )
+                }
+            >
+                <div className="flex items-center flex items-center">
+                    <Icon name="brand-chrome" className="mr-2" />
+                    <span className="flex-1 text-left">Trigger update</span>
+                    <span className="text-xs color-weak">Triggers a fake update (keep popup opened)</span>
+                </div>
+            </Button>
+            <Button
+                icon
+                shape="ghost"
+                className="w-full"
+                onClick={() =>
+                    sendMessage(
+                        pageMessage({
+                            type: WorkerMessageType.DEBUG,
+                            payload: { debug: 'storage_full' },
+                        })
+                    )
+                }
+            >
+                <div className="flex items-center flex items-center">
+                    <Icon name="drive" className="mr-2" />
+                    <span className="flex-1 text-left">Trigger full disk</span>
+                    <span className="text-xs color-weak">Triggers a fake disk full event (open popup after)</span>
+                </div>
+            </Button>
+        </SettingsPanel>
+        <DropdownDebug />
+        <NotificationDebug />
+    </>
+);
