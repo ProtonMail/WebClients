a9195d26b7cd414bdca874764713ba735b3a4378
Pablo Molina <contact@pablomolina.me>
2024-03-27 14:39:48 +0100

    Check desktop theme instead of just media queries

--- packages/shared/lib/helpers/initElectronClassnames.ts
+++ packages/shared/lib/helpers/initElectronClassnames.ts
@@ -1,8 +1,32 @@
+import { canInvokeInboxDesktopIPC } from '../desktop/ipcHelpers';
+import { ThemeModeSetting, ThemeSetting } from '../themes/themes';
 import { isElectronApp, isElectronOnMac, isElectronOnWindows } from './desktop';
+
+export const updateElectronThemeModeClassnames = (theme: ThemeSetting) => {
+    if (!isElectronApp) {
+        return;
+    }
+
+    const prefersDark =
+        theme.Mode === ThemeModeSetting.Dark ||
+        (theme.Mode === ThemeModeSetting.Auto && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+    if (prefersDark) {
+        document.body.classList.add('is-electron-dark');
+        document.body.classList.remove('is-electron-light');
+    } else {
+        document.body.classList.remove('is-electron-dark');
+        document.body.classList.add('is-electron-light');
+    }
+};
 
 export const initElectronClassnames = () => {
     if (isElectronApp) {
         document.body.classList.add('is-electron');
+
+        if (canInvokeInboxDesktopIPC) {
+            updateElectronThemeModeClassnames(window.ipcInboxMessageBroker!.getTheme());
+        }
     }
 
     if (isElectronOnMac) {
