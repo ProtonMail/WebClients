e08257f5908d8816ae599b2b9f7cb29309eefd90
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-03-27 15:21:11 +0100

    Add feature flag for web app offline mode
    [IDTEAM-2997]

--- applications/pass/src/app/Views/Settings/Tabs/Security.tsx
+++ applications/pass/src/app/Views/Settings/Tabs/Security.tsx
@@ -2,10 +2,16 @@ import { type FC } from 'react';
 
 import { Offline } from '@proton/pass/components/Settings/Offline';
 import { PinLockSetting } from '@proton/pass/components/Settings/PinLockSetting';
+import { useFeatureFlag } from '@proton/pass/hooks/useFeatureFlag';
+import { PassFeature } from '@proton/pass/types/api/features';
 
-export const Security: FC = () => (
-    <>
-        <PinLockSetting key="pin" />
-        <Offline key="offline" />
-    </>
-);
+export const Security: FC = () => {
+    const canToggleOfflineMode = useFeatureFlag(PassFeature.PassWebOfflineMode);
+
+    return (
+        <>
+            <PinLockSetting key="pin" />
+            {canToggleOfflineMode && <Offline key="offline" />}
+        </>
+    );
+};
