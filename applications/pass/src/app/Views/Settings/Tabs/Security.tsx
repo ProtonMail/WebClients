fd5e6cc7018060fd8d14e8263e323858e09a7c2b
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-03-27 14:37:11 +0100

    Enable offline mode for web app
    [IDTEAM-2997]

--- applications/pass/src/app/Views/Settings/Tabs/Security.tsx
+++ applications/pass/src/app/Views/Settings/Tabs/Security.tsx
@@ -3,5 +3,9 @@ import { type FC } from 'react';
 import { Offline } from '@proton/pass/components/Settings/Offline';
 import { PinLockSetting } from '@proton/pass/components/Settings/PinLockSetting';
 
-export const Security: FC = () =>
-    [<PinLockSetting key="pin" />].concat(OFFLINE_SUPPORTED ? [<Offline key="offline" />] : []);
+export const Security: FC = () => (
+    <>
+        <PinLockSetting key="pin" />
+        <Offline key="offline" />
+    </>
+);
