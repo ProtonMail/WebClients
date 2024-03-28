dc9ed30d93f6cdcbf16a932405df1cdbdc635f84
Edvin CANDON <edvin.candon@proton.ch>
2024-03-28 09:48:39 +0100

    Handle bridge disconnection
    - This is to support the main content-script being destroyed
      after the webauthn content-script was injected
    
    [IDTEAM-3048]

--- applications/pass-extension/src/app/content/bridge/constants.ts
+++ applications/pass-extension/src/app/content/bridge/constants.ts
@@ -3,5 +3,6 @@ import { WorkerMessageType } from '@proton/pass/types';
 export const BRIDGE_REQUEST = 'Pass::MainWorld::Message';
 export const BRIDGE_RESPONSE = 'Pass::MainWorld::Response';
 export const BRIDGE_ABORT = 'Pass::MainWorld::Abort';
+export const BRIDGE_DISCONNECT = 'Pass::MainWorld::Disconnect';
 
 export const ALLOWED_MESSAGES = [WorkerMessageType.PASSKEY_CREATE, WorkerMessageType.PASSKEY_GET] as const;
