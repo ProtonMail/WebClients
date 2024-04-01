6c44d0a64edf9fc384079d9bb45ec896a107a91d
Edvin CANDON <edvin.candon@proton.ch>
2024-04-01 18:32:47 +0200

    Fix offline support detection
    - Add `useOfflineSupported` hook
    - Enabled in desktop builds
    - Enabled via feature flag in web-app
    - Disabled in extension
    
    [IDTEAM-2997]

--- applications/pass/src/app/Views/Settings/Tabs/Security.tsx
+++ applications/pass/src/app/Views/Settings/Tabs/Security.tsx
@@ -2,16 +2,10 @@ import { type FC } from 'react';
 
 import { Offline } from '@proton/pass/components/Settings/Offline';
 import { PinLockSetting } from '@proton/pass/components/Settings/PinLockSetting';
-import { useFeatureFlag } from '@proton/pass/hooks/useFeatureFlag';
-import { PassFeature } from '@proton/pass/types/api/features';
+import { useOfflineSupported } from '@proton/pass/hooks/useOfflineSupported';
+import { truthy } from '@proton/pass/utils/fp/predicates';
 
 export const Security: FC = () => {
-    const canToggleOfflineMode = useFeatureFlag(PassFeature.PassWebOfflineMode);
-
-    return (
-        <>
-            <PinLockSetting key="pin" />
-            {canToggleOfflineMode && <Offline key="offline" />}
-        </>
-    );
+    const offlineSupported = useOfflineSupported();
+    return [<PinLockSetting key="pin" />, offlineSupported && <Offline key="offline" />].filter(truthy);
 };
