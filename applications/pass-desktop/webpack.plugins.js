105e05a01ceca7c3bc2d5da50c6357e0bd285a66
Edvin CANDON <edvin.candon@proton.ch>
2024-04-01 18:48:38 +0200

    Remove `OFFLINE_SUPPORTED` build flag
    [IDTEAM-2997]

--- applications/pass-desktop/webpack.plugins.js
+++ applications/pass-desktop/webpack.plugins.js
@@ -5,6 +5,5 @@ module.exports = [
     new DefinePlugin({
         ENV: JSON.stringify(process.env.NODE_ENV ?? 'development'),
         BUILD_TARGET: JSON.stringify(platform()),
-        OFFLINE_SUPPORTED: true,
     }),
 ];
