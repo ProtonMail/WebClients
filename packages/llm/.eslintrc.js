b34172b8858a0c78b8b586a6e83cf4914e09df37
Marc Dupont <marc.dupont@proton.ch>
2024-04-02 11:50:25 +0200

    Setup llm package

---
+++ packages/llm/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: ['@proton/eslint-config-proton'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
+    },
+    rules: {
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        '@typescript-eslint/consistent-type-imports': 'error',
+        curly: ['error', 'multi-line'],
+    },
+    ignorePatterns: ['.eslintrc.js'],
+};
