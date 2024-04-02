9e0f00f79be770c216e7f6a8b6aae3152d264e25
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2024-04-02 17:58:58 +0200

    Busy slots beta patches
    Full day events
    Design changes
    Fixes

--- applications/calendar/src/app/components/eventModal/rows/BusyParticipantRow.tsx
+++ applications/calendar/src/app/components/eventModal/rows/BusyParticipantRow.tsx
@@ -18,47 +18,61 @@ import BusyParticipantRowDot from './BusyPartipantRowDot';
 interface Props {
     attendee: AttendeeModel;
     contactEmailsMap: SimpleMap<ContactEmail>;
-    displayBusySlotsActions: boolean;
     onDelete: (attendee: AttendeeModel) => void;
     onHighlight: (attendeeEmail: string, highlighted: boolean) => void;
     onToggleOptional: (attendee: AttendeeModel) => void;
 }
 
-const BusyParticipantRow = ({
-    attendee,
-    contactEmailsMap,
-    displayBusySlotsActions,
-    onDelete,
-    onHighlight,
-    onToggleOptional,
-}: Props) => {
+const BusyParticipantRow = ({ attendee, contactEmailsMap, onDelete, onHighlight, onToggleOptional }: Props) => {
     const dispatch = useCalendarDispatch();
     const { email: attendeeEmail, role } = attendee;
     const canonicalizedEmail = canonicalizeEmail(attendeeEmail);
-    const attendeeBusyData = useCalendarSelector((state) => selectAttendeeBusyData(state, canonicalizedEmail));
+    const { status, hasAvailability, isVisible, color } = useCalendarSelector((state) =>
+        selectAttendeeBusyData(state, canonicalizedEmail)
+    );
     const isOptional = role === ICAL_ATTENDEE_ROLE.OPTIONAL;
     const { Name: contactName, Email: contactEmail } = contactEmailsMap[canonicalizedEmail] || {};
     const email = contactEmail || attendeeEmail;
 
     const { nameEmail, displayOnlyEmail } = getContactDisplayNameEmail({ name: contactName, email });
-    const nameEmailDisplay = contactName ? (
-        <>
-            <span className="text-semibold text-sm">{contactName}</span>
-            <span className="color-weak ml-1 text-sm">{email}</span>
-        </>
-    ) : (
-        <span className="text-semibold text-sm">{email}</span>
-    );
 
-    const optionalText = isOptional
-        ? c('Action').t`Make this participant required`
-        : c('Action').t`Make this participant optional`;
+    const handleVisibilityToggle = () => {
+        if (status === 'loading' || status === 'not-available') {
+            return;
+        }
+        dispatch(
+            busyTimeSlotsActions.setAttendeeVisibility({
+                email: attendeeEmail,
+                visible: !isVisible,
+            })
+        );
+    };
 
     const visibilityText = (() => {
-        if (!attendeeBusyData.hasAvailability) {
-            return c('Action').t`Availability is unknown`;
+        if (status === 'not-available') {
+            return c('Description').t`Availability unknown`;
         }
-        return attendeeBusyData.isVisible ? c('Action').t`Hide busy times` : c('Action').t`Show busy times`;
+        if (status === 'loading') {
+            return c('Description').t`Loading availability`;
+        }
+        return isVisible ? c('Action').t`Hide busy times` : c('Action').t`Show busy times`;
+    })();
+
+    const dotDisplay = (() => {
+        if (status === 'loading') {
+            return 'loader';
+        }
+        if (!hasAvailability) {
+            return 'half-circle';
+        }
+        return isVisible ? 'circle' : 'bordered-circle';
+    })();
+
+    const dotClassName = (() => {
+        if (status !== 'loading' && status !== 'not-available') {
+            return 'cursor-pointer';
+        }
+        return '';
     })();
 
     return (
@@ -72,47 +86,51 @@ const BusyParticipantRow = ({
                 onHighlight(email, false);
             }}
         >
-            {displayBusySlotsActions && (
-                <div className="shrink-0 flex mt-0.5 pt-2 w-custom" style={{ width: '1rem' }}>
-                    <BusyParticipantRowDot
-                        color={attendeeBusyData.color}
-                        hasAvailability={attendeeBusyData.hasAvailability}
-                        isLoading={attendeeBusyData.isLoading}
-                        tooltipText={visibilityText}
-                    />
-                </div>
-            )}
-
-            <div className={clsx('flex flex-1', displayBusySlotsActions ? 'p-1' : 'py-1 pr-1')} title={nameEmail}>
-                <div className={clsx(['text-ellipsis', displayOnlyEmail && 'max-w-full'])}>{nameEmailDisplay}</div>
-                {isOptional ? <span className="color-weak w-full text-sm">{c('Label').t`Optional`}</span> : null}
+            <div className="shrink-0 flex mt-0.5 pt-2 w-custom" style={{ width: '1rem' }}>
+                <BusyParticipantRowDot
+                    display={dotDisplay}
+                    color={color}
+                    tooltipText={visibilityText}
+                    onClick={handleVisibilityToggle}
+                    classname={dotClassName}
+                />
             </div>
 
-            {displayBusySlotsActions && (
-                <Tooltip title={visibilityText}>
-                    <div>
-                        <Button
-                            icon
-                            shape="ghost"
-                            type="button"
-                            size="small"
-                            className="flex shrink-0 group-hover:opacity-100 group-hover:opacity-100-no-width"
-                            disabled={!attendeeBusyData.hasAvailability}
-                            onClick={() => {
-                                dispatch(
-                                    busyTimeSlotsActions.setAttendeeVisibility({
-                                        email: attendeeEmail,
-                                        visible: !attendeeBusyData.isVisible,
-                                    })
-                                );
-                            }}
-                        >
-                            <Icon name={attendeeBusyData.isVisible ? 'eye' : 'eye-slash'} alt={visibilityText} />
-                        </Button>
-                    </div>
-                </Tooltip>
-            )}
-            <Tooltip title={optionalText}>
+            <div className={clsx('flex flex-1 p-1')} title={nameEmail}>
+                <div className={clsx(['text-ellipsis', displayOnlyEmail && 'max-w-full'])}>
+                    {contactName ? (
+                        <>
+                            <span className="text-semibold text-sm">{contactName}</span>
+                            <span className="color-weak ml-1 text-sm">{email}</span>
+                        </>
+                    ) : (
+                        <span className="text-semibold text-sm">{email}</span>
+                    )}
+                </div>
+                {isOptional && <span className="color-weak w-full text-sm">{c('Label').t`Optional`}</span>}
+            </div>
+
+            <Tooltip title={visibilityText}>
+                <div className={clsx(status === 'not-available' && 'hidden')}>
+                    <Button
+                        icon
+                        shape="ghost"
+                        type="button"
+                        size="small"
+                        className="flex shrink-0 group-hover:opacity-100 group-hover:opacity-100-no-width"
+                        onClick={handleVisibilityToggle}
+                    >
+                        <Icon name={isVisible ? 'eye' : 'eye-slash'} alt={visibilityText} />
+                    </Button>
+                </div>
+            </Tooltip>
+            <Tooltip
+                title={
+                    isOptional
+                        ? c('Action').t`Make this participant required`
+                        : c('Action').t`Make this participant optional`
+                }
+            >
                 <Button
                     icon
                     shape="ghost"
