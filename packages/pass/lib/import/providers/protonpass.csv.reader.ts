86a7bc015817577e09d1f21f8941628d8a568c69
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-04-03 16:36:57 +0200

    Support vault names in Pass CSV export/import
    [IDTEAM-2941]

--- packages/pass/lib/import/providers/protonpass.csv.reader.ts
+++ packages/pass/lib/import/providers/protonpass.csv.reader.ts
@@ -1,6 +1,7 @@
-import type { ExportedCsvItem } from '@proton/pass/lib/export/types';
+import type { ExportCSVItem } from '@proton/pass/lib/export/types';
 import type { ItemImportIntent } from '@proton/pass/types';
 import type { ItemCreditCard } from '@proton/pass/types/protobuf/item-v1';
+import { groupByKey } from '@proton/pass/utils/array/group-by-key';
 import { truthy } from '@proton/pass/utils/fp/predicates';
 import { logger } from '@proton/pass/utils/logger';
 
@@ -11,7 +12,7 @@ import type { ImportPayload } from '../types';
 
 type CreditCardCsvItem = ItemCreditCard & { note: string };
 
-const PASS_EXPECTED_HEADERS: (keyof ExportedCsvItem)[] = [
+const PASS_EXPECTED_HEADERS: (keyof ExportCSVItem)[] = [
     'type',
     'name',
     'url',
@@ -23,7 +24,7 @@ const PASS_EXPECTED_HEADERS: (keyof ExportedCsvItem)[] = [
     'modifyTime',
 ];
 
-const processCreditCardItem = (item: ExportedCsvItem): ItemImportIntent<'creditCard'> => {
+const processCreditCardItem = (item: ExportCSVItem): ItemImportIntent<'creditCard'> => {
     const creditCardItem: CreditCardCsvItem = JSON.parse(item.note as string);
 
     return importCreditCardItem({
@@ -44,46 +45,46 @@ export const readProtonPassCSV = async (data: string): Promise<ImportPayload> =>
     const warnings: string[] = [];
 
     try {
-        const result = await readCSV<ExportedCsvItem>({
+        const result = await readCSV<ExportCSVItem>({
             data,
             headers: PASS_EXPECTED_HEADERS,
             onError: (error) => warnings.push(error),
         });
 
+        const groupByVaults = groupByKey(result.items, 'vault');
+
         return {
-            vaults: [
-                {
-                    name: getImportedVaultName(),
-                    shareId: null,
-                    items: result.items
-                        .filter((item) => item.type !== 'alias')
-                        .map((item) => {
-                            switch (item.type) {
-                                case 'login':
-                                    return importLoginItem({
-                                        name: item.name,
-                                        note: item.note,
-                                        username: item.username,
-                                        password: item.password,
-                                        urls: item.url?.split(', '),
-                                        totp: item.totp,
-                                        createTime: Number(item.createTime),
-                                        modifyTime: Number(item.modifyTime),
-                                    });
-                                case 'creditCard':
-                                    return processCreditCardItem(item);
-                                default:
-                                    return importNoteItem({
-                                        name: item.name,
-                                        note: item.note,
-                                        createTime: Number(item.createTime),
-                                        modifyTime: Number(item.modifyTime),
-                                    });
-                            }
-                        })
-                        .filter(truthy),
-                },
-            ],
+            vaults: groupByVaults.map((items) => ({
+                name: getImportedVaultName(items[0].vault),
+                shareId: null,
+                items: items
+                    .filter((item) => item.type !== 'alias')
+                    .map((item) => {
+                        switch (item.type) {
+                            case 'login':
+                                return importLoginItem({
+                                    name: item.name,
+                                    note: item.note,
+                                    username: item.username,
+                                    password: item.password,
+                                    urls: item.url?.split(', '),
+                                    totp: item.totp,
+                                    createTime: Number(item.createTime),
+                                    modifyTime: Number(item.modifyTime),
+                                });
+                            case 'creditCard':
+                                return processCreditCardItem(item);
+                            default:
+                                return importNoteItem({
+                                    name: item.name,
+                                    note: item.note,
+                                    createTime: Number(item.createTime),
+                                    modifyTime: Number(item.modifyTime),
+                                });
+                        }
+                    })
+                    .filter(truthy),
+            })),
             ignored,
             warnings,
         };
