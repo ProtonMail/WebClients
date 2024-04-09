9efaf872ae4cf99ac196506026229f3f762b65d3
Pablo Molina <contact@pablomolina.me>
2024-04-09 08:56:05 +0200

    Use isElectronMail flag

--- packages/components/containers/themes/ThemeProvider.tsx
+++ packages/components/containers/themes/ThemeProvider.tsx
@@ -4,7 +4,7 @@ import { APP_NAMES } from '@proton/shared/lib/constants';
 import { hasInboxDesktopFeature, invokeInboxDesktopIPC } from '@proton/shared/lib/desktop/ipcHelpers';
 import { clearBit, hasBit, setBit } from '@proton/shared/lib/helpers/bitset';
 import { getCookie, setCookie } from '@proton/shared/lib/helpers/cookies';
-import { isElectronApp, isElectronOnSupportedApps } from '@proton/shared/lib/helpers/desktop';
+import { isElectronMail, isElectronOnSupportedApps } from '@proton/shared/lib/helpers/desktop';
 import { updateElectronThemeModeClassnames } from '@proton/shared/lib/helpers/initElectronClassnames';
 import isDeepEqual from '@proton/shared/lib/helpers/isDeepEqual';
 import createListeners from '@proton/shared/lib/helpers/listeners';
@@ -335,7 +335,7 @@ const ThemeProvider = ({ children, appName }: Props) => {
     }, [themeSetting]);
 
     useEffect(() => {
-        if (isElectronApp) {
+        if (isElectronMail) {
             updateElectronThemeModeClassnames(themeSetting);
         }
     }, [colorScheme, themeSetting]);
--- packages/shared/lib/desktop/ipcHelpers.ts
+++ packages/shared/lib/desktop/ipcHelpers.ts
@@ -1,21 +1,21 @@
-import { isElectronApp } from '../helpers/desktop';
+import { isElectronMail } from '../helpers/desktop';
 import { IPCInboxClientUpdateMessage, IPCInboxDesktopFeature, IPCInboxGetInfoMessage } from './desktopTypes';
 
 export function invokeInboxDesktopIPC({ type, payload }: IPCInboxClientUpdateMessage) {
-    if (isElectronApp && window.ipcInboxMessageBroker?.send) {
+    if (isElectronMail && window.ipcInboxMessageBroker?.send) {
         window.ipcInboxMessageBroker!.send!(type, payload);
     }
 }
 
 export const canGetInboxDesktopInfo =
-    isElectronApp && !!window.ipcInboxMessageBroker && !!window.ipcInboxMessageBroker!.getInfo;
+    isElectronMail && !!window.ipcInboxMessageBroker && !!window.ipcInboxMessageBroker!.getInfo;
 
 export function getInboxDesktopInfo<T extends IPCInboxGetInfoMessage['type']>(key: T) {
     return window.ipcInboxMessageBroker!.getInfo!<T>(key);
 }
 
 export const hasInboxDesktopFeature = (feature: IPCInboxDesktopFeature) =>
-    isElectronApp &&
+    isElectronMail &&
     !!window.ipcInboxMessageBroker &&
     !!window.ipcInboxMessageBroker!.hasFeature &&
     window.ipcInboxMessageBroker!.hasFeature(feature);
