86cbd3aef00210b5e87ce1d8ed97833521724de1
Edvin CANDON <edvin.candon@proton.ch>
2024-04-09 11:52:33 +0200

    Fix password options change not triggering settings persist

--- packages/pass/store/actions/creators/password.ts
+++ packages/pass/store/actions/creators/password.ts
@@ -2,7 +2,9 @@ import { createAction } from '@reduxjs/toolkit';
 
 import { type GeneratePasswordConfig } from '@proton/pass/lib/password/generator';
 import { withCache } from '@proton/pass/store/actions/enhancers/cache';
+import { withSettings } from '@proton/pass/store/actions/enhancers/settings';
 import type { PasswordHistoryEntry } from '@proton/pass/store/reducers';
+import { pipe } from '@proton/pass/utils/fp/pipe';
 
 export const passwordSave = createAction('password::save', (payload: PasswordHistoryEntry) => withCache({ payload }));
 export const passwordDelete = createAction('password::delete', (payload: { id: string }) => withCache({ payload }));
@@ -10,5 +12,5 @@ export const passwordHistoryClear = createAction('password::history::clear', () 
 export const passwordHistoryGarbageCollect = createAction('password::history::gc', () => withCache({ payload: {} }));
 
 export const passwordOptionsEdit = createAction('password::options::edit', (payload: GeneratePasswordConfig) =>
-    withCache({ payload })
+    pipe(withCache, withSettings)({ payload })
 );
