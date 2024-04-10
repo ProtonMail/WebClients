cc877be226d2da0f887aad168cf563f33ee1ce3c
Irena Zazoska <zazoska@protonmail.com>
2024-04-10 07:23:42 +0200

    Add new common components for PassMonitor layout

--- packages/pass/components/Layout/Card/InfoCard.tsx
+++ packages/pass/components/Layout/Card/InfoCard.tsx
@@ -1,20 +1,33 @@
-import type { FC } from 'react';
+import type { FC, ReactNode } from 'react';
 
 import { Icon, type IconName } from '@proton/components/index';
 import clsx from '@proton/utils/clsx';
 
 export type InfoCardProps = {
     className?: string;
-    icon: IconName;
-    subtitle?: string;
-    title: string;
+    icon: IconName | (() => ReactNode);
+    subtitle?: ReactNode;
+    subtitleClassname?: string;
+    title: ReactNode;
+    titleClassname?: string;
+    actions?: ReactNode;
 };
-export const InfoCard: FC<InfoCardProps> = ({ icon, title, subtitle, className }) => (
+
+export const InfoCard: FC<InfoCardProps> = ({
+    actions,
+    className,
+    icon,
+    subtitle,
+    subtitleClassname,
+    title,
+    titleClassname,
+}) => (
     <div className={clsx('flex items-center flex-nowrap w-full gap-4 text-sm', className)}>
-        <Icon name={icon} size={5} />
-        <div className="flex flex-column flex-nowrap justify-start w-full">
-            <span className="text-left text-ellipsis color-norm">{title}</span>
-            {subtitle && <span className="text-left text-ellipsis color-weak">{subtitle}</span>}
+        {typeof icon === 'function' ? icon() : <Icon name={icon} size={5} />}
+        <div className="flex flex-column flex-nowrap justify-start w-full text-left">
+            <span className={titleClassname ?? 'color-norm text-ellipsis'}>{title}</span>
+            {subtitle && <span className={subtitleClassname ?? 'color-weak text-ellipsis'}>{subtitle}</span>}
         </div>
+        {actions && <div className="shrink-0">{actions}</div>}
     </div>
 );
