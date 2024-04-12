a1489f36be0a69b76b23552e7a2dd0ec69a8cfe5
Irena Zazoska <zazoska@protonmail.com>
2024-04-12 08:46:38 +0200

    Move PassMonitor views behind feature flag
    [IDTEAM-2867]

--- applications/pass/src/app/Views/Monitor/Monitor.tsx
+++ applications/pass/src/app/Views/Monitor/Monitor.tsx
@@ -3,16 +3,22 @@ import { Route, type RouteChildrenProps, Switch } from 'react-router-dom';
 
 import { DuplicatePasswords } from '@proton/pass/components/Monitor/Password/DuplicatePasswords';
 import { Summary } from '@proton/pass/components/Monitor/Summary';
+import { useFeatureFlag } from '@proton/pass/hooks/useFeatureFlag';
+import { PassFeature } from '@proton/pass/types/api/features';
 
 export const Monitor: FC<RouteChildrenProps> = ({ match }) => {
+    const enabled = useFeatureFlag(PassFeature.PassMonitor);
+
     return (
-        <div className="w-full h-full">
-            <div className="flex flex-1 flex-column items-start w-full h-full">
-                <Switch>
-                    <Route exact path={`${match?.path}/duplicates`} component={DuplicatePasswords} />
-                    <Route component={Summary} />
-                </Switch>
+        enabled && (
+            <div className="w-full h-full">
+                <div className="flex flex-1 flex-column items-start w-full h-full">
+                    <Switch>
+                        <Route exact path={`${match?.path}/duplicates`} component={DuplicatePasswords} />
+                        <Route component={Summary} />
+                    </Switch>
+                </div>
             </div>
-        </div>
+        )
     );
 };
