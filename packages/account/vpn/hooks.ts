3e6626f67039a5627fbc8162d384beb409dd32c4
Mattias Svanström <mattias.svanstrom@proton.ch>
2024-04-15 10:35:38 +0200

    Add support for periodic queueing of thunks in hooks

--- packages/account/vpn/hooks.ts
+++ packages/account/vpn/hooks.ts
@@ -2,6 +2,6 @@ import { createHooks } from '@proton/redux-utilities';
 
 import { selectVpnServersCount, vpnServersCountThunk } from './serversCount';
 
-const hooks = createHooks(vpnServersCountThunk, selectVpnServersCount);
+const hooks = createHooks(vpnServersCountThunk, selectVpnServersCount, { periodic: true });
 
 export const useVPNServersCount = hooks.useValue;
