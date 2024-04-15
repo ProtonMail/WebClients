e4c8b6700743b43536c80cc6c1ac4956231ce13e
Edvin CANDON <edvin.candon@proton.ch>
2024-04-15 10:37:23 +0200

    Improve form submission detection
    - Support detecting form submission loading|submitted|error state service-worker side
    - Reflect request tracker API changes and only start tracking when close submission
    - Add `formId` to FormEntry in order to properly reconciliate autosave
    - Add `FORM_STATUS` tab message from worker and handle in content-script
    - `FORM_STATUS` only processed if form is not in a busy state
    - Ensures that dynamic forms with appearing/disppearing fields
      triggering multiple submissions as the form is filled out are
      correctly processed for autosave
    - Add sanity checks : observe idle state and last interactions
    - Improve form detach submission detection
    - Let content-script stash on XMLHttpRequest errors

--- applications/pass-extension/src/app/worker/services/form.tracker.ts
+++ applications/pass-extension/src/app/worker/services/form.tracker.ts
@@ -1,12 +1,15 @@
 import { isFormEntryCommitted, setFormEntryStatus } from 'proton-pass-extension/lib/utils/form-entry';
 
-import type { FormEntry, FormEntryBase, Maybe, TabId } from '@proton/pass/types';
+import { backgroundMessage } from '@proton/pass/lib/extension/message';
+import browser from '@proton/pass/lib/globals/browser';
+import type { FormEntry, FormEntryBase, FormStatusPayload, Maybe, TabId } from '@proton/pass/types';
 import { FormEntryStatus, WorkerMessageType } from '@proton/pass/types';
+import { waitUntil } from '@proton/pass/utils/fp/wait-until';
 import { logger } from '@proton/pass/utils/logger';
 import { merge } from '@proton/pass/utils/object/merge';
 import { requestHasBodyFormData } from '@proton/pass/utils/requests';
 import { parseSender } from '@proton/pass/utils/url/parser';
-import { wait } from '@proton/shared/lib/helpers/promise';
+import noop from '@proton/utils/noop';
 
 import WorkerMessageBroker from '../channel';
 import { withContext } from '../context';
@@ -16,6 +19,16 @@ import { createXMLHTTPRequestTracker } from './xmlhttp-request.tracker';
 export const createFormTrackerService = () => {
     /** Track form entries for each tab */
     const submissions: Map<TabId, FormEntry> = new Map();
+
+    /** Notifies the tab of the current status of the form. If no XMLHttpRequests
+     * were intercepted, we will rely on a content-script timeout to infer the
+     * submission. Else we ping the tab to clear the timeout and wait for the
+     * form loading state to resolve via the `onIdle` callback of the tracker */
+    const syncFormStatus = (tabId: TabId, payload: FormStatusPayload) => {
+        browser.tabs
+            .sendMessage(tabId, backgroundMessage({ type: WorkerMessageType.FORM_STATUS, payload }))
+            .catch(noop);
+    };
 
     const get = (tabId: TabId, domain: string): Maybe<FormEntry> => {
         const submission = submissions.get(tabId);
@@ -32,34 +45,84 @@ export const createFormTrackerService = () => {
     const stage = (tabId: TabId, submission: FormEntryBase, reason: string): FormEntry => {
         logger.info(`[FormTracker::Stage]: on tab ${tabId} for domain "${submission.domain}" {${reason}}`);
         const pending = get(tabId, submission.domain);
+        const updatedAt = Date.now();
+        const submittedAt = submission.submit ? updatedAt : null;
 
         const staging = ((): FormEntry => {
             if (pending && pending.status === FormEntryStatus.STAGING) {
                 pending.action = submission.action;
                 pending.data = merge(pending.data, submission.data, { excludeEmpty: true });
-                pending.submitted = pending.submitted || submission.submitted;
                 pending.type = submission.type;
-                pending.updatedAt = Date.now();
+                pending.updatedAt = updatedAt;
+                pending.submit = submission.submit;
+                pending.submittedAt = submittedAt;
+
                 return pending;
-            } else return { ...submission, status: FormEntryStatus.STAGING, updatedAt: Date.now() };
+            } else {
+                return {
+                    ...submission,
+                    status: FormEntryStatus.STAGING,
+                    submittedAt,
+                    updatedAt,
+                };
+            }
         })();
 
         submissions.set(tabId, staging);
         return staging;
     };
-
-    /** Comitting a `FormEntry` will automatically flag it as `submitted` */
     const commit = (tabId: TabId, domain: string, reason: string): Maybe<FormEntry<FormEntryStatus.COMMITTED>> => {
         const pending = get(tabId, domain);
 
         if (pending && pending.status === FormEntryStatus.STAGING) {
             logger.info(`[FormTracker::Commit] on tab ${tabId} for domain "${domain}" {${reason}}`);
-            pending.submitted = true;
             return setFormEntryStatus(pending, FormEntryStatus.COMMITTED);
         } else stash(tabId, 'INVALID_COMMIT');
     };
 
+    const XMLHttpTracker = createXMLHTTPRequestTracker({
+        acceptRequest: (details) => {
+            const submission = submissions.get(details.tabId);
+            if (!submission) return false;
+
+            /* only start tracking requests if the form entry was submitted
+             * recently and is not committed. This avoids tracking validation
+             * API calls that may occur on websites validating fields */
+            const submitted = submission.loading || Date.now() - (submission?.submittedAt ?? 0) < 500;
+            if (isFormEntryCommitted(submission) || !submitted) return false;
+
+            if (requestHasBodyFormData(details)) {
+                if (!submission.loading) submission.loading = true;
+                syncFormStatus(details.tabId, { formId: submission.formId, status: 'loading' });
+                return true;
+            }
+
+            return false;
+        },
+        onFailed: ({ tabId, domain }) => {
+            const submission = get(tabId, domain);
+            if (submission && submission.status === FormEntryStatus.STAGING) {
+                submission.submittedAt = null;
+                syncFormStatus(tabId, { formId: submission.formId, status: 'error' });
+            }
+        },
+        onIdle: (tabId) => {
+            /** If there are no more tracked requests for a tab and there's a valid form
+             * entry, notify the tab that the form may have been successfully submitted.
+             * At this point, failure inference may not be possible. Add a small timout
+             * between each resolved request in order to handle concurrent requests */
+            const submission = submissions.get(tabId);
+
+            if (submission && submission.loading) {
+                submission.loading = false;
+                logger.info(`[FormTracker] Inferred submission on tab ${tabId}`);
+                syncFormStatus(tabId, { formId: submission.formId, status: 'submitted' });
+            }
+        },
+    });
+
     createMainFrameRequestTracker({
+        onTabUpdate: (tabId) => XMLHttpTracker.reset(tabId),
         onTabDelete: (tabId) => stash(tabId, 'TAB_DELETED'),
         onTabError: (tabId) => stash(tabId, 'TAB_ERRORED'),
         onTabLoaded: (tabId, method, domain) => {
@@ -68,34 +131,9 @@ export const createFormTrackerService = () => {
              * was submitted as part of the form action */
             const submission = get(tabId, domain ?? '');
             if (submission?.domain !== domain) stash(tabId, 'DOMAIN_SWITCH');
-            else if (method === 'POST') submission.submitted = true;
-        },
-    });
-
-    createXMLHTTPRequestTracker({
-        acceptRequest: (details) => {
-            const submission = submissions.get(details.tabId);
-            if (!submission) return false;
-
-            if (requestHasBodyFormData(details)) {
-                /** if the form was not flagged as submitted - infer submission from this potential
-                 * network request interception. If it is "close" enough to the submission's `updatedAt`
-                 * timestamp, we can consider something of interest happened during this timeframe and
-                 * as such flag the submission as `submitted` */
-                if (Date.now() - submission.updatedAt < 500) {
-                    logger.info(`[FormTracker::XMLHttp] inferred submission on tab ${details.tabId}`);
-                    submission.submitted = true;
-                }
-                return true;
-            }
-
-            return false;
-        },
-        onFailedRequest: ({ tabId, domain }) => {
-            const submission = get(tabId, domain);
-            if (submission && submission.status === FormEntryStatus.STAGING) {
-                submission.submitted = false;
-                stash(tabId, 'XMLHTTP_ERROR_DETECTED');
+            else {
+                submission.loading = false;
+                if (method === 'POST') submission.submit = true;
             }
         },
     });
@@ -175,9 +213,10 @@ export const createFormTrackerService = () => {
                         return { submission: null };
                     }
 
-                    /** If the form was not submitted, add a short timeout before
-                     * resolving the response to intercept possible XMLHttpRequests */
-                    await wait(submission.submitted ? 0 : 250);
+                    /* If we intercepted XMLHttpRequests for this submission,
+                     * wait for them to resolve to an idle state */
+                    await waitUntil(() => !submission.loading, 100).catch(noop);
+
                     const autosave = isFormEntryCommitted(submission)
                         ? ctx.service.autosave.resolve(submission)
                         : { shouldPrompt: false as const };
