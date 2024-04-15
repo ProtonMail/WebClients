4601feb06451758ba726964a8758a192ae8d1640
Mattias Svanström <mattias.svanstrom@proton.ch>
2024-04-15 16:02:23 +0200

    Remove VPN intro pricing A/B support

--- applications/account/src/app/single-signup/state.ts
+++ applications/account/src/app/single-signup/state.ts
@@ -1,19 +1,10 @@
-import { VPNIntroPricingVariant } from '@proton/components/containers';
-import { getIsVpn2024, getIsVpn2024Deal } from '@proton/components/containers/payments/subscription/helpers';
+import { getIsVpn2024Deal } from '@proton/components/containers/payments/subscription/helpers';
 import { CYCLE, PLANS } from '@proton/shared/lib/constants';
 
 import { defaultSignupModel } from '../single-signup-v2/SingleSignupContainerV2';
 import { VPNSignupModel } from './interface';
 
-export const getCycleData = ({
-    plan,
-    coupon,
-    vpnIntroPricingVariant,
-}: {
-    plan: PLANS;
-    coupon?: string;
-    vpnIntroPricingVariant?: VPNIntroPricingVariant;
-}) => {
+export const getCycleData = ({ plan, coupon }: { plan: PLANS; coupon?: string }) => {
     if (getIsVpn2024Deal(plan, coupon)) {
         return {
             upsellCycle: CYCLE.THIRTY,
@@ -21,23 +12,10 @@ export const getCycleData = ({
         };
     }
 
-    const defaultValue = {
+    return {
         upsellCycle: CYCLE.TWO_YEARS,
         cycles: [CYCLE.MONTHLY, CYCLE.YEARLY, CYCLE.TWO_YEARS],
     };
-
-    if (vpnIntroPricingVariant === VPNIntroPricingVariant.New2024) {
-        return defaultValue;
-    }
-
-    if (getIsVpn2024(plan)) {
-        return {
-            upsellCycle: CYCLE.EIGHTEEN,
-            cycles: [CYCLE.MONTHLY, CYCLE.THREE, CYCLE.EIGHTEEN],
-        };
-    }
-
-    return defaultValue;
 };
 
 export const defaultVPNSignupModel: VPNSignupModel = {
