2378bf038ddaff4f1bb5ad347f027a0eddf7b252
Alexey Karpov <alexey.karpov@proton.ch>
2024-04-16 15:44:57 +0200

    Revert "Merge branch 'fix-dark-theme-cc' into 'main'"
    This reverts commit 4d741b2ad98b9667c4a1289798533bd5a5a1f53f, reversing
    changes made to 10d36c24ea5c912a8cb107a4ca4e34ce311f5e8c.

--- packages/chargebee/lib/types.ts
+++ packages/chargebee/lib/types.ts
@@ -33,13 +33,10 @@ export type CbCardConfig = {
         '--focus-outline': string;
         '--focus-ring': string;
         '--field-norm': string;
-        '--field-background-color': string;
         '--field-focus-background-color': string;
         '--field-focus-text-color': string;
         '--field-placeholder-color': string;
         '--field-text-color': string;
-        '--selection-text-color': string;
-        '--selection-background-color': string;
     };
     translations: {
         cardNumberPlaceholder: string;
@@ -59,13 +56,10 @@ export const chargebeeCssVariables: ChargebeeCssVariable[] = [
     '--focus-outline',
     '--focus-ring',
     '--field-norm',
-    '--field-background-color',
     '--field-focus-background-color',
     '--field-focus-text-color',
     '--field-placeholder-color',
     '--field-text-color',
-    '--selection-text-color',
-    '--selection-background-color',
 ];
 
 export type CbPaypalConfig = {
