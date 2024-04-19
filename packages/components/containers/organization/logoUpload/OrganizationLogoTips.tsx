52d7a1561413e72e80af0c183ca5b93799d721cb
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2024-04-19 13:28:07 +0000

    Light labelling text updates

--- packages/components/containers/organization/logoUpload/OrganizationLogoTips.tsx
+++ packages/components/containers/organization/logoUpload/OrganizationLogoTips.tsx
@@ -4,7 +4,8 @@ const OrganizationLogoTips = () => {
     return (
         <ul className="my-1">
             <li className="my-2">{c('Organization logo upload').t`Use a graphical icon or image rather than text.`}</li>
-            <li className="my-2">{c('Organization logo upload').t`Ensure your icon has padding space around it.`}</li>
+            <li className="my-2">{c('Organization logo upload').t`Ensure your logo has padding space around it.`}</li>
+            <li className="my-2">{c('Organization logo upload').t`Use a solid background color.`}</li>
             <li className="my-2">{c('Organization logo upload').t`Avoid overly compressed images.`}</li>
             <li className="my-2">{c('Organization logo upload')
                 .t`Opt for a PNG image that is a 128x128 pixels for clarity.`}</li>
