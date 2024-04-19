1c7cf2e69d7063fd9a880f2c99c5c3c3be5d12ed
Edvin CANDON <edvin.candon@proton.ch>
2024-04-19 12:26:58 +0200

    Refactor PassMonitor list views to support split item views

--- packages/pass/components/Monitor/Password/DuplicatePasswords.tsx
+++ packages/pass/components/Monitor/Password/DuplicatePasswords.tsx
@@ -1,17 +1,18 @@
-import { type FC, useMemo, useRef } from 'react';
+import { type FC, useEffect, useMemo, useRef } from 'react';
 import { useSelector } from 'react-redux';
+import { useRouteMatch } from 'react-router-dom';
 import type { List } from 'react-virtualized';
 
 import { c, msgid } from 'ttag';
 
 import { ItemsListItem } from '@proton/pass/components/Item/List/ItemsListItem';
 import { VirtualList } from '@proton/pass/components/Layout/List/VirtualList';
-import { SubHeader } from '@proton/pass/components/Layout/Section/SubHeader';
+import { getItemRoute } from '@proton/pass/components/Navigation/routing';
 import { useSelectItemAction } from '@proton/pass/hooks/useSelectItemAction';
-import { isTrashed } from '@proton/pass/lib/items/item.predicates';
+import { isTrashed, itemEq } from '@proton/pass/lib/items/item.predicates';
 import { getItemKey } from '@proton/pass/lib/items/item.utils';
 import { selectDuplicatePasswordItems } from '@proton/pass/store/selectors/monitor';
-import type { ItemRevision } from '@proton/pass/types';
+import type { ItemRevision, SelectedItem } from '@proton/pass/types';
 
 export type InterpolationItem = { type: 'divider'; label: string } | { type: 'item'; item: ItemRevision };
 
@@ -42,66 +43,63 @@ const flattenWithLabelsAndIndexes = (
 };
 
 export const DuplicatePasswords: FC = () => {
-    const duplicatePasswordItems = useSelector(selectDuplicatePasswordItems);
     const selectItem = useSelectItemAction();
+    const itemRoute = getItemRoute(':shareId', ':itemId', { prefix: 'monitor/duplicates(/trash)?' });
+    const selectedItem = useRouteMatch<SelectedItem>(itemRoute)?.params;
+    const duplicatePasswordItems = useSelector(selectDuplicatePasswordItems);
+
     const listRef = useRef<List>(null);
     const { interpolation, interpolationIndexes } = useMemo(
         () => flattenWithLabelsAndIndexes(duplicatePasswordItems),
         [duplicatePasswordItems]
     );
 
-    return (
-        <div className="flex-1 w-full">
-            <div className="h-full flex flex-column gap-4 max-w-custom pt-6 px-6" style={{ '--max-w-custom': '74em' }}>
-                <SubHeader
-                    title={c('Title').t`Reused passwords`}
-                    description={c('Description').t`Generate unique passwords to increase your security.`}
-                    className="shrink-0"
-                />
+    useEffect(() => {
+        if (duplicatePasswordItems.length && !selectedItem) {
+            const item = duplicatePasswordItems[0][0];
+            selectItem(item, { inTrash: isTrashed(item), prefix: 'monitor/duplicates', mode: 'replace' });
+        }
+    }, [selectedItem, duplicatePasswordItems]);
 
-                {duplicatePasswordItems.length > 0 && (
-                    <div className="flex-auto">
-                        <VirtualList
-                            interpolationIndexes={interpolationIndexes}
-                            ref={listRef}
-                            rowCount={interpolation.length}
-                            rowHeight={(idx) => (interpolationIndexes.includes(idx) ? 60 : 54)}
-                            rowRenderer={({ style, index, key }) => {
-                                const row = interpolation[index];
-                                switch (row.type) {
-                                    case 'item': {
-                                        const item = row.item;
-                                        const id = getItemKey(item);
-                                        return (
-                                            <div style={style} key={key}>
-                                                <ItemsListItem
-                                                    id={id}
-                                                    item={item}
-                                                    key={id}
-                                                    onClick={() =>
-                                                        selectItem(row.item, { inTrash: isTrashed(row.item) })
-                                                    }
-                                                />
-                                            </div>
-                                        );
-                                    }
-                                    case 'divider': {
-                                        return (
-                                            <div
-                                                style={style}
-                                                key={key}
-                                                className="flex color-weak text-xl pt-6 pb-3 pl-3 items-center"
-                                            >
-                                                {row.label}
-                                            </div>
-                                        );
-                                    }
-                                }
-                            }}
-                        />
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+    return interpolation.length > 0 ? (
+        <VirtualList
+            interpolationIndexes={interpolationIndexes}
+            ref={listRef}
+            rowCount={interpolation.length}
+            rowHeight={(idx) => (interpolationIndexes.includes(idx) ? 28 : 54)}
+            rowRenderer={({ style, index, key }) => {
+                const row = interpolation[index];
+                switch (row.type) {
+                    case 'item': {
+                        const item = row.item;
+                        const id = getItemKey(item);
+                        return (
+                            <div style={style} key={key}>
+                                <ItemsListItem
+                                    active={selectedItem && itemEq(selectedItem)(item)}
+                                    id={id}
+                                    item={item}
+                                    key={id}
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        selectItem(item, {
+                                            inTrash: isTrashed(item),
+                                            prefix: 'monitor/duplicates',
+                                        });
+                                    }}
+                                />
+                            </div>
+                        );
+                    }
+                    case 'divider': {
+                        return (
+                            <div style={style} key={key} className="flex color-weak text-sm pt-2 pb-1 pl-3">
+                                {row.label}
+                            </div>
+                        );
+                    }
+                }
+            }}
+        />
+    ) : null;
 };
