05994bea6276a7b92682a3ed6992c8b1aa586330
lpeslier <lucien.peslier@proton.ch>
2024-04-23 15:57:50 +0200

    Remove Changelogs + ChangelogModals
    Changes:
    - Remove ChangelogModal related work
    - Remove CHANGELOG.MD from drive
    - Add fullVersion params to AppVersion
    - Parse appVersion for Drive to only show short one

--- applications/drive/src/app/components/layout/sidebar/DriveSidebar/DriveSidebarFooter.tsx
+++ applications/drive/src/app/components/layout/sidebar/DriveSidebar/DriveSidebarFooter.tsx
@@ -1,7 +1,11 @@
-import { AppVersion } from '@proton/components';
+import { AppVersion, getAppVersion, useConfig } from '@proton/components';
 
-import changelog from '../../../../../../CHANGELOG.md';
+const DriveSidebarFooter = () => {
+    const { APP_VERSION } = useConfig();
+    const appVersion = getAppVersion(APP_VERSION);
 
-const DriveSidebarFooter = () => <AppVersion changelog={changelog} />;
+    // On Drive we have versioning that include last commit hash, ex. 5.1.0+58eb37d11e. We do not want to show that in sidebar
+    return <AppVersion appVersion={appVersion.split('+')[0]} fullVersion={appVersion} />;
+};
 
 export default DriveSidebarFooter;
