897e01e45666b25c62b5a1a3f405dbc19455ed65
Nico Hoffmann <65-nhoffmann@users.noreply.gitlab.protontech.ch>
2024-04-25 14:05:50 +0000

    Harmonized participant display

--- applications/calendar/src/app/components/eventModal/rows/OrganizerRow.tsx
+++ applications/calendar/src/app/components/eventModal/rows/OrganizerRow.tsx
@@ -1,7 +1,6 @@
 import { c } from 'ttag';
 
 import { OrganizerModel } from '@proton/shared/lib/interfaces/calendar';
-import clsx from '@proton/utils/clsx';
 
 interface Props {
     organizer: OrganizerModel;
@@ -18,7 +17,7 @@ const getDisplayNames = (name: string, email: string) => {
     };
 };
 
-const BusySlotsOrganiserRow = ({ organizer }: Props) => {
+const OrganizerRow = ({ organizer }: Props) => {
     const { email, cn: name } = organizer;
     const { nameEmail, displayFull } = getDisplayNames(name, email);
 
@@ -38,28 +37,6 @@ const BusySlotsOrganiserRow = ({ organizer }: Props) => {
             </div>
             <div className="color-weak w-full text-sm">{c('Label').t`Organizer`}</div>
         </div>
-    );
-};
-
-const RegularOrganiserRow = ({ organizer }: Props) => {
-    const { email, cn: name } = organizer;
-    const { nameEmail, displayFull } = getDisplayNames(name, email);
-
-    return (
-        <div key={email} className={clsx(['address-item flex mb-1 px-2'])}>
-            <div className="flex flex-1 p-1" title={nameEmail}>
-                <div className={clsx(['text-ellipsis', !displayFull && 'max-w-full'])}>{nameEmail}</div>
-                <span className="color-weak w-full">{c('Label').t`Organizer`}</span>
-            </div>
-        </div>
-    );
-};
-
-const OrganizerRow = ({ organizer, isBusyTimeSlotsAvailable }: Props & { isBusyTimeSlotsAvailable: boolean }) => {
-    return isBusyTimeSlotsAvailable ? (
-        <BusySlotsOrganiserRow organizer={organizer} />
-    ) : (
-        <RegularOrganiserRow organizer={organizer} />
     );
 };
 
