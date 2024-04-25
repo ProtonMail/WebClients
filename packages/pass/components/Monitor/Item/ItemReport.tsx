3abf2a6b00fe5b947ca27e7ce8a0dbe58b12dd34
Edvin CANDON <edvin.candon@proton.ch>
2024-04-25 09:05:15 +0200

    Fine-tune item reports from monitor views

--- packages/pass/components/Monitor/Item/ItemReport.tsx
+++ packages/pass/components/Monitor/Item/ItemReport.tsx
@@ -1,44 +1,90 @@
 import { type FC } from 'react';
 import { useSelector } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
 
 import { c, msgid } from 'ttag';
 
-import { ActionCard } from '@proton/pass/components/Layout/Card/ActionCard';
-import { selectItemReport } from '@proton/pass/store/selectors';
+import { PinnedItem } from '@proton/pass/components/Item/Pinned/PinnedItem';
+import { Card } from '@proton/pass/components/Layout/Card/Card';
+import { CardContent } from '@proton/pass/components/Layout/Card/CardContent';
+import { CardIcon } from '@proton/pass/components/Layout/Card/CardIcon';
+import { useMonitor } from '@proton/pass/components/Monitor/MonitorProvider';
+import { useNavigation } from '@proton/pass/components/Navigation/NavigationProvider';
+import { getLocalPath } from '@proton/pass/components/Navigation/routing';
+import { itemEq } from '@proton/pass/lib/items/item.predicates';
+import { getItemKey } from '@proton/pass/lib/items/item.utils';
+import { selectSelectedItems } from '@proton/pass/store/selectors';
 import type { SelectedItem } from '@proton/pass/types';
+import { invert } from '@proton/pass/utils/fp/predicates';
 
-export const ItemReport: FC<SelectedItem> = (item) => {
-    const report = useSelector(selectItemReport(item));
-    const duplicatesCount = report.password.duplicates.length;
+const DuplicatePasswordReport: FC<SelectedItem> = (item) => {
+    const { duplicates } = useMonitor();
+    const { selectItem } = useNavigation();
 
-    return (
-        <>
-            {duplicatesCount > 1 && (
-                <div className="py-4">
-                    <ActionCard
-                        icon="exclamation-filled"
-                        title={c('Title').t`Reused passwords`}
-                        subtitle={
-                            <>
-                                <div>
-                                    {duplicatesCount < 5
-                                        ? c('Description').ngettext(
-                                              msgid`${duplicatesCount} other login use this password:`,
-                                              `${duplicatesCount} other logins use this password:`,
-                                              duplicatesCount
-                                          )
-                                        : // translator full sentence is "X other login use this password", in this usecase X is allwas >5 and the text "X other login" is displayed as pill
-                                          c('Description').jt`${duplicatesCount} other logins use this password`}
-                                </div>
-                                <div>{c('Description')
-                                    .t`Visit the website and generate a unique password for this item.`}</div>
-                            </>
-                        }
-                        pillLabel={duplicatesCount}
-                        type="warning"
-                    />
-                </div>
-            )}
-        </>
-    );
+    const group = duplicates.data.find((group) => group.some(itemEq(item))) ?? [];
+    const others = useSelector(selectSelectedItems(group?.filter(invert(itemEq(item))) ?? []));
+    const total = others.length;
+
+    return total > 0 ? (
+        <Card className="mb-2" type="warning">
+            <CardContent
+                icon={() => <CardIcon icon="exclamation-filled" className="self-start mt-1" />}
+                titleClassname="color-interaction-norm-major-2 text-lg text-semibold"
+                subtitleClassname="color-interaction-norm-major-2"
+                title={c('Title').t`Reused passwords`}
+                subtitle={
+                    <>
+                        <div>
+                            {total <= 5 ? (
+                                <>
+                                    {c('Description').ngettext(
+                                        msgid`${total} other login uses this password:`,
+                                        `${total} other logins use this password:`,
+                                        total
+                                    )}
+                                    <div className="flex gap-2 my-2 flex-nowrap overflow-overlay">
+                                        {others.map((item) => (
+                                            <PinnedItem
+                                                onClick={() =>
+                                                    selectItem(item.shareId, item.itemId, {
+                                                        prefix: 'monitor/duplicates',
+                                                    })
+                                                }
+                                                item={item}
+                                                key={getItemKey(item)}
+                                                active
+                                            />
+                                        ))}
+                                    </div>
+                                </>
+                            ) : (
+                                // translator full sentence is "X other login use this password", in this usecase X is allwas >5 and the text "X other login" is displayed as pill
+                                c('Description').jt`${total} other logins use this password`
+                            )}
+                        </div>
+                        <div>{c('Description').t`Visit the website and generate a unique password for this item.`}</div>
+                    </>
+                }
+            />
+        </Card>
+    ) : null;
 };
+
+const WeakPasswordReport: FC = () => (
+    <Card className="mb-2" type="warning">
+        <CardContent
+            icon={() => <CardIcon icon="exclamation-filled" className="self-start mt-1" />}
+            titleClassname="color-interaction-norm-major-2 text-lg text-semibold"
+            subtitleClassname="color-interaction-norm-major-2"
+            title={c('Title').t`Insecure password`}
+            subtitle={c('Description').t`This account is vulnerable, visit the service and change your password.`}
+        />
+    </Card>
+);
+
+export const ItemReport: FC<SelectedItem> = (item) => (
+    <Switch>
+        <Route path={getLocalPath('monitor/duplicates')} render={() => <DuplicatePasswordReport {...item} />} />
+        <Route path={getLocalPath('monitor/weak')} render={() => <WeakPasswordReport />} />
+    </Switch>
+);
