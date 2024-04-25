802333cf2e6e82d2b04a3b56db0d089d6b38badb
Ricardas Vaitkus <ricardas.vaitkus@proton.ch>
2024-04-25 14:47:09 +0000

    DRVWEB-3948 Allow update permissions for share invite

--- applications/drive/src/app/components/modals/ShareLinkModal/DirectSharing/DirectSharingListInvitation.tsx
+++ applications/drive/src/app/components/modals/ShareLinkModal/DirectSharing/DirectSharingListInvitation.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 import { c } from 'ttag';
 
@@ -13,6 +13,8 @@ import {
     usePopperAnchor,
 } from '@proton/components';
 import { useNotifications } from '@proton/components/hooks';
+import { SHARE_MEMBER_PERMISSIONS } from '@proton/shared/lib/drive/constants';
+import { MEMBER_PERMISSIONS } from '@proton/shared/lib/drive/permissions';
 import { textToClipboard } from '@proton/shared/lib/helpers/browser';
 import { getInitials } from '@proton/shared/lib/helpers/string';
 
@@ -22,16 +24,35 @@ interface Props {
     linkId: string;
     contactEmail: string;
     contactName?: string;
+    selectedPermissions: SHARE_MEMBER_PERMISSIONS;
     onInvitationRemove: (invitationId: string) => void;
+    onInvitationPermissionsChange: (invitationId: string, permissions: SHARE_MEMBER_PERMISSIONS) => Promise<void>;
 }
 
-const MenuItem = ({ label, iconName, onClick }: { label: string; iconName: IconName; onClick: () => void }) => {
+interface PermissionOption {
+    icon: IconName;
+    label: string;
+    value: number;
+}
+
+const MenuItem = ({
+    label,
+    iconName,
+    isSelected,
+    onClick,
+}: {
+    label: string;
+    iconName: IconName;
+    isSelected?: boolean;
+    onClick: () => void;
+}) => {
     return (
-        <DropdownMenuButton onClick={onClick}>
+        <DropdownMenuButton onClick={onClick} className="text-left flex justify-space-between items-center">
             <span className="flex items-center mr-14">
                 <Icon name={iconName} className="mr-2" />
                 {label}
             </span>
+            {isSelected ? <Icon name="checkmark" /> : null}
         </DropdownMenuButton>
     );
 };
@@ -42,10 +63,28 @@ export const DirectSharingListInvitation = ({
     linkId,
     contactEmail,
     contactName,
+    selectedPermissions,
     onInvitationRemove,
+    onInvitationPermissionsChange,
 }: Props) => {
     const { anchorRef, isOpen, toggle, close } = usePopperAnchor<HTMLButtonElement>();
+    const [isLoading, setIsLoading] = useState(false);
     const { createNotification } = useNotifications();
+
+    const permissionsOptions: PermissionOption[] = [
+        {
+            icon: 'eye',
+            label: c('Label').t`Viewer`,
+            value: MEMBER_PERMISSIONS.VIEWER,
+        },
+        {
+            icon: 'pencil',
+            label: c('Label').t`Editor`,
+            value: MEMBER_PERMISSIONS.EDITOR,
+        },
+    ];
+
+    const dropdownLabel = permissionsOptions.find((permission) => permission.value === selectedPermissions)?.label;
 
     const copyShareInviteLinkUrl = useCallback(() => {
         textToClipboard(`${window.location.origin}/${volumeId}/${linkId}?invitation=${invitationId}`);
@@ -57,6 +96,13 @@ export const DirectSharingListInvitation = ({
     const handleInviteRemove = useCallback(() => {
         onInvitationRemove(invitationId);
     }, []);
+
+    const handleInvitationPermissionsChange = (invitationId: string, permissions: SHARE_MEMBER_PERMISSIONS) => {
+        setIsLoading(true);
+        onInvitationPermissionsChange(invitationId, permissions).finally(() => {
+            setIsLoading(false);
+        });
+    };
 
     return (
         <div className="flex my-4 justify-space-between items-center">
@@ -79,11 +125,24 @@ export const DirectSharingListInvitation = ({
                     hasCaret
                     shape="ghost"
                     size="small"
+                    loading={isLoading}
                 >
-                    {c('Info').t`Pending`}
+                    {dropdownLabel}
                 </DropdownButton>
                 <Dropdown isOpen={isOpen} anchorRef={anchorRef} onClose={close}>
                     <DropdownMenu>
+                        <MenuItem
+                            iconName={permissionsOptions[0].icon}
+                            label={permissionsOptions[0].label}
+                            isSelected={permissionsOptions[0].value === selectedPermissions}
+                            onClick={() => handleInvitationPermissionsChange(invitationId, permissionsOptions[0].value)}
+                        />
+                        <MenuItem
+                            iconName={permissionsOptions[1].icon}
+                            label={permissionsOptions[1].label}
+                            isSelected={permissionsOptions[1].value === selectedPermissions}
+                            onClick={() => handleInvitationPermissionsChange(invitationId, permissionsOptions[1].value)}
+                        />
                         <MenuItem
                             iconName="link"
                             label={c('Action').t`Copy invite link`}
