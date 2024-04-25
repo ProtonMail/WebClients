043e8341cc840a8249a42f7dce26a9cb912bfffa
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2024-04-25 08:34:01 +0000

    add light labelling metrics

---
+++ packages/components/containers/organization/logoUpload/useOrgLogoUploadTelemetry.ts
@@ -0,0 +1,42 @@
+import { useApi } from '@proton/components/hooks';
+import {
+    TelemetryAccountOrganizationLogoUploadEvents,
+    TelemetryMeasurementGroups,
+} from '@proton/shared/lib/api/telemetry';
+import { sendTelemetryReport } from '@proton/shared/lib/helpers/metrics';
+
+const useOrgLogoUploadTelemetry = () => {
+    const api = useApi();
+
+    const sendOrgLogoUploadStartProcessReport = () => {
+        void sendTelemetryReport({
+            api,
+            measurementGroup: TelemetryMeasurementGroups.accountOrgLogoUpload,
+            event: TelemetryAccountOrganizationLogoUploadEvents.processStart,
+        });
+    };
+
+    const sendOrgLogoUploadSuccessReport = () => {
+        void sendTelemetryReport({
+            api,
+            measurementGroup: TelemetryMeasurementGroups.mailComposerAssistant,
+            event: TelemetryAccountOrganizationLogoUploadEvents.processSuccess,
+        });
+    };
+
+    const sendOrgLogoUploadFailureReport = () => {
+        void sendTelemetryReport({
+            api,
+            measurementGroup: TelemetryMeasurementGroups.mailComposerAssistant,
+            event: TelemetryAccountOrganizationLogoUploadEvents.processFailure,
+        });
+    };
+
+    return {
+        sendOrgLogoUploadStartProcessReport,
+        sendOrgLogoUploadSuccessReport,
+        sendOrgLogoUploadFailureReport,
+    };
+};
+
+export default useOrgLogoUploadTelemetry;
