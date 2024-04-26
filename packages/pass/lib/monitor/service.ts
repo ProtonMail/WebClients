38bf2733e397e0436a9661b072b3de520f45ecdf
Edvin CANDON <edvin.candon@proton.ch>
2024-04-26 09:16:54 +0200

    Use single wasm call when resolving missing 2FAs and weak passwords

--- packages/pass/lib/monitor/service.ts
+++ packages/pass/lib/monitor/service.ts
@@ -2,9 +2,11 @@ import type { WasmPasswordScoreResult } from '@protontech/pass-rust-core';
 import type { Store } from 'redux';
 
 import type { PassCoreService } from '@proton/pass/lib/core/types';
-import { isHealthCheckSkipped } from '@proton/pass/lib/items/item.predicates';
+import { hasDomain, hasOTP, isHealthCheckSkipped } from '@proton/pass/lib/items/item.predicates';
+import { intoSelectedItem } from '@proton/pass/lib/items/item.utils';
 import { selectLoginItems } from '@proton/pass/store/selectors';
 import { type UniqueItem } from '@proton/pass/types';
+import { and, invert } from '@proton/pass/utils/fp/predicates';
 import { deobfuscate } from '@proton/pass/utils/obfuscate/xor';
 
 export interface MonitorService {
@@ -16,55 +18,28 @@ export interface MonitorService {
 /** MonitorService provides `PassMonitor` methods that rely
  * on the `PassRustCore` module */
 export const createMonitorService = (core: PassCoreService, store: Store): MonitorService => {
-    const getLoginItems = () => selectLoginItems(store.getState());
+    const getLoginItems = () => selectLoginItems(store.getState()).filter(invert(isHealthCheckSkipped));
 
     const service: MonitorService = {
         analyzePassword: (password) => core.exec('analyze_password', password),
 
         checkMissing2FAs: async () => {
-            let items: UniqueItem[] = [];
+            const logins = getLoginItems();
+            const candidates = logins.filter(and(hasDomain, invert(hasOTP)));
+            const domains = candidates.flatMap((item) => item.data.content.urls);
+            const eligible = await core.exec('twofa_domains_eligible', domains);
+            const eligibleDomain = (url: string) => eligible.get(url) === true;
 
-            for (const item of getLoginItems()) {
-                const { shareId, itemId } = item;
-                if (isHealthCheckSkipped(item)) continue;
-                if (!item.data.content.urls) continue;
-
-                const hasOTP =
-                    item.data.content.totpUri.v ||
-                    item.data.extraFields.some((field) => field.type === 'totp' && field.data.totpUri.v);
-
-                if (!hasOTP) {
-                    for (const url of item.data.content.urls) {
-                        if (await core.exec('twofa_domain_eligible', url)) {
-                            items.push({ shareId, itemId });
-                            break;
-                        }
-                    }
-                }
-            }
-
-            return items;
+            return candidates.filter((item) => item.data.content.urls.some(eligibleDomain)).map(intoSelectedItem);
         },
 
         checkWeakPasswords: async () => {
-            let items: UniqueItem[] = [];
+            const logins = getLoginItems();
+            const candidates = logins.filter((item) => item.data.content.password.v);
+            const passwords = candidates.map((item) => deobfuscate(item.data.content.password));
+            const scores = await core.exec('check_password_scores', passwords);
 
-            for (const item of getLoginItems()) {
-                if (isHealthCheckSkipped(item)) continue;
-                if (item.data.content.password.v) {
-                    const { shareId, itemId } = item;
-                    const password = deobfuscate(item.data.content.password);
-                    const score = (await service.analyzePassword(password)).password_score;
-
-                    switch (score) {
-                        case 'Vulnerable':
-                        case 'Weak':
-                            items.push({ shareId, itemId });
-                    }
-                }
-            }
-
-            return items;
+            return candidates.filter((_, idx) => scores[idx] !== 'Strong').map(intoSelectedItem);
         },
     };
 
