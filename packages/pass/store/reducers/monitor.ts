534bf72cf015208eb55a8b8cedfa0405c85b6923
Edvin CANDON <edvin.candon@proton.ch>
2024-04-27 11:15:12 +0200

    Avoid caching monitor data

--- packages/pass/store/reducers/monitor.ts
+++ packages/pass/store/reducers/monitor.ts
@@ -14,64 +14,65 @@ import {
     resolveProtonBreach,
     verifyCustomAddress,
 } from '@proton/pass/store/actions';
+import type { MaybeNull } from '@proton/pass/types';
 import { partialMerge } from '@proton/pass/utils/object/merge';
 
-const INITIAL_MONITOR_STATE: MonitorState = { total: 0, proton: [], custom: [], preview: [] };
-
-export type MonitorState = {
+export type MonitorState = MaybeNull<{
     custom: MonitorAddress<AddressType.CUSTOM>[];
     preview: MonitorDomain[];
     proton: MonitorAddress<AddressType.PROTON>[];
     total: number;
-};
+}>;
 
-const monitorReducer: Reducer<MonitorState> = (state = INITIAL_MONITOR_STATE, action) => {
+const monitorReducer: Reducer<MonitorState> = (state = null, action) => {
     if (getBreaches.success.match(action)) {
-        return partialMerge(state, {
+        return {
             custom: action.payload.CustomEmails?.map(intoCustomMonitorAddress) ?? [],
             preview: action.payload.DomainsPeek?.map(intoMonitorDomain) ?? [],
             proton: action.payload.Addresses?.map(intoProtonMonitorAddress) ?? [],
             total: action.payload.EmailsCount,
-        });
+        };
     }
 
-    if (resolveCustomBreach.success.match(action)) {
-        return partialMerge(state, {
-            custom: state.custom.map((breach) => {
-                if (breach.addressId !== action.payload) return breach;
-                return { ...breach, breachCount: 0 };
-            }),
-        });
-    }
+    if (state) {
+        if (resolveCustomBreach.success.match(action)) {
+            return partialMerge(state, {
+                custom: state.custom.map((breach) => {
+                    if (breach.addressId !== action.payload) return breach;
+                    return { ...breach, breachCount: 0 };
+                }),
+            });
+        }
 
-    if (resolveProtonBreach.success.match(action)) {
-        return partialMerge(state, {
-            proton: state.proton.map((breach) => {
-                if (breach.addressId !== action.payload) return breach;
-                return { ...breach, breachCount: 0 };
-            }),
-        });
-    }
+        if (resolveProtonBreach.success.match(action)) {
+            return partialMerge(state, {
+                proton: state.proton.map((breach) => {
+                    if (breach.addressId !== action.payload) return breach;
+                    return { ...breach, breachCount: 0 };
+                }),
+            });
+        }
 
-    if (addCustomAddress.success.match(action)) {
-        return partialMerge(state, {
-            custom: state.custom.concat(intoCustomMonitorAddress(action.payload)),
-        });
-    }
+        if (addCustomAddress.success.match(action)) {
+            return partialMerge(state, {
+                custom: state.custom.concat(intoCustomMonitorAddress(action.payload)),
+            });
+        }
 
-    if (verifyCustomAddress.success.match(action)) {
-        return partialMerge(state, {
-            custom: state.custom.map((breach) => {
-                if (breach.addressId !== action.payload) return breach;
-                return { ...breach, verified: true };
-            }),
-        });
-    }
+        if (verifyCustomAddress.success.match(action)) {
+            return partialMerge(state, {
+                custom: state.custom.map((breach) => {
+                    if (breach.addressId !== action.payload) return breach;
+                    return { ...breach, verified: true };
+                }),
+            });
+        }
 
-    if (deleteCustomAddress.success.match(action)) {
-        return partialMerge(state, {
-            custom: state.custom.filter((breach) => breach.addressId !== action.payload),
-        });
+        if (deleteCustomAddress.success.match(action)) {
+            return partialMerge(state, {
+                custom: state.custom.filter((breach) => breach.addressId !== action.payload),
+            });
+        }
     }
 
     return state;
