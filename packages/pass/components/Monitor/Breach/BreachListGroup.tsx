9823344a7ac219becc94452b84b2792f105ef128
Edvin CANDON <edvin.candon@proton.ch>
2024-04-27 10:09:58 +0200

    Allow adding/verifying/removing custom addresses from provider
    [IDTEAM-3180]

--- packages/pass/components/Monitor/Breach/BreachListGroup.tsx
+++ packages/pass/components/Monitor/Breach/BreachListGroup.tsx
@@ -1,4 +1,5 @@
-import type { FC } from 'react';
+import type { FC, ReactNode } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { c, msgid } from 'ttag';
@@ -14,21 +15,32 @@ import {
     TableHeaderCell,
     TableRow,
 } from '@proton/components/components/table';
+import { DropdownMenuButton } from '@proton/pass/components/Layout/Dropdown/DropdownMenuButton';
+import { QuickActionsDropdown } from '@proton/pass/components/Layout/Dropdown/QuickActionsDropdown';
+import { TableRowLoading } from '@proton/pass/components/Layout/Table/TableRowLoading';
+import { useMonitor } from '@proton/pass/components/Monitor/MonitorProvider';
 import { getLocalPath } from '@proton/pass/components/Navigation/routing';
 import type { MonitorTableItem } from '@proton/pass/hooks/monitor/useBreachesTable';
 import { getAddressId } from '@proton/pass/lib/monitor/monitor.utils';
 import { AddressType } from '@proton/pass/lib/monitor/types';
+import { addCustomAddressRequest, deleteCustomAddressRequest } from '@proton/pass/store/actions/requests';
+import { selectRequestInFlight } from '@proton/pass/store/selectors';
 import { formatEpoch } from '@proton/pass/utils/time/format';
 import isTruthy from '@proton/utils/isTruthy';
 
 const BreachListRow: FC<{ address: MonitorTableItem }> = ({ address }) => {
+    const monitor = useMonitor();
+    const adding = useSelector(selectRequestInFlight(addCustomAddressRequest(address.email)));
+    const deleting = useSelector(selectRequestInFlight(deleteCustomAddressRequest(address.email)));
+    const loading = adding || deleting;
+
     const { breached, breachedAt, breachCount, email, monitored, type, usedIn, verified } = address;
 
     const className = monitored && verified && breached ? 'color-danger' : '';
 
     const statusClassName = (() => {
         if (monitored && verified && !breached) return 'color-success';
-        if (!monitored && !verified) return 'color-weak';
+        if (!verified && monitored) return 'color-weak';
     })();
 
     const status = (() => {
@@ -59,7 +71,15 @@ const BreachListRow: FC<{ address: MonitorTableItem }> = ({ address }) => {
         if (address.type === AddressType.CUSTOM) {
             if (!monitored) {
                 return (
-                    <Button pill shape="solid" color="weak" size="small">
+                    <Button
+                        className="shrink-0"
+                        pill
+                        shape="solid"
+                        color="weak"
+                        size="small"
+                        onClick={() => monitor.addAddress(address.email)}
+                        loading={adding}
+                    >
                         {c('Action').t`Add`}
                     </Button>
                 );
@@ -67,15 +87,32 @@ const BreachListRow: FC<{ address: MonitorTableItem }> = ({ address }) => {
 
             if (!verified) {
                 return (
-                    <Button pill size="small" shape="ghost">
-                        <Icon name="three-dots-vertical" />
-                    </Button>
+                    <QuickActionsDropdown
+                        icon="three-dots-vertical"
+                        size="small"
+                        shape="ghost"
+                        className="shrink-0"
+                        originalPlacement="bottom-start"
+                        disabled={loading}
+                    >
+                        <DropdownMenuButton
+                            onClick={() => monitor.verifyAddress(address)}
+                            label={c('Label').t`Verify`}
+                            icon="envelope-open"
+                        />
+                        <DropdownMenuButton
+                            onClick={() => monitor.deleteAddress(address.addressId)}
+                            label={c('Label').t`Remove`}
+                            icon="trash"
+                            loading={deleting}
+                        />
+                    </QuickActionsDropdown>
                 );
             }
         }
 
         return (
-            <Link to={getLocalPath(`monitor/dark-web/${address.type}/${getAddressId(address)}`)}>
+            <Link className="shrink-0" to={getLocalPath(`monitor/dark-web/${address.type}/${getAddressId(address)}`)}>
                 <Button pill size="small" shape="ghost" type="button">
                     <Icon name="chevron-right" />
                 </Button>
@@ -95,38 +132,55 @@ const BreachListRow: FC<{ address: MonitorTableItem }> = ({ address }) => {
     );
 };
 
-export const BreachListGroup: FC<{
+type Props = {
+    actions?: ReactNode;
     className?: string;
-    groupName?: string;
-    seeAllHref?: string;
-    displayLimit?: number;
     data: MonitorTableItem[];
-}> = ({ className, groupName, seeAllHref, displayLimit, data }) => {
-    return (
-        <section className={className}>
-            {groupName && (
-                <header className="mb-2 flex justify-space-between">
-                    <span className="text-bold">{groupName}</span>
-                    {displayLimit && data.length > displayLimit && seeAllHref && <Link to={seeAllHref}>See all</Link>}
-                </header>
-            )}
-            <Card rounded>
-                <Table hasActions responsive="stacked">
-                    <TableHeader>
-                        <TableRow>
-                            <TableHeaderCell>{c('Label').t`Email`}</TableHeaderCell>
-                            <TableHeaderCell>{c('Label').t`Status`}</TableHeaderCell>
-                            <TableHeaderCell>{c('Label').t`Used in`}</TableHeaderCell>
-                            <TableHeaderCell className="w-custom" style={{ '--w-custom': '4rem' }} />
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {data.slice(0, displayLimit).map((address) => (
-                            <BreachListRow key={address.email} address={address} />
-                        ))}
-                    </TableBody>
-                </Table>
-            </Card>
-        </section>
-    );
+    displayLimit?: number;
+    groupName?: string;
+    loading: boolean;
+    seeAllHref?: string;
 };
+
+export const BreachListGroup: FC<Props> = ({
+    actions,
+    className,
+    data,
+    displayLimit,
+    groupName,
+    loading,
+    seeAllHref,
+}) => (
+    <section className={className}>
+        {groupName && (
+            <header className="mb-2 flex justify-space-between">
+                <div className="flex gap-2 items-center">
+                    <span className="text-bold">{groupName}</span>
+                    {actions}
+                </div>
+                {displayLimit && data.length > displayLimit && seeAllHref && <Link to={seeAllHref}>See all</Link>}
+            </header>
+        )}
+        <Card rounded>
+            <Table hasActions responsive="stacked">
+                <TableHeader>
+                    <TableRow>
+                        <TableHeaderCell>{c('Label').t`Email`}</TableHeaderCell>
+                        <TableHeaderCell>{c('Label').t`Status`}</TableHeaderCell>
+                        <TableHeaderCell>{c('Label').t`Used in`}</TableHeaderCell>
+                        <TableHeaderCell className="w-custom" style={{ '--w-custom': '4rem' }} />
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {loading ? (
+                        <TableRowLoading rows={3} cells={4} />
+                    ) : (
+                        data
+                            .slice(0, displayLimit)
+                            .map((address) => <BreachListRow key={address.email} address={address} />)
+                    )}
+                </TableBody>
+            </Table>
+        </Card>
+    </section>
+);
