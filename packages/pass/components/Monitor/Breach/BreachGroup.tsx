1e0bc3e5e35bd58aabe1712a19fe3a73adf4dcc0
Edvin CANDON <edvin.candon@proton.ch>
2024-04-29 10:07:27 +0200

    Show warning when user has SL custom domains in hide-my-email monitor view

--- packages/pass/components/Monitor/Breach/BreachGroup.tsx
+++ packages/pass/components/Monitor/Breach/BreachGroup.tsx
@@ -1,11 +1,16 @@
 import { type FC, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { type RouteChildrenProps, useHistory } from 'react-router-dom';
 
+import { c } from 'ttag';
+
+import { Card } from '@proton/atoms/Card';
 import { SubHeader } from '@proton/pass/components/Layout/Section/SubHeader';
 import { CustomAddressAddButton } from '@proton/pass/components/Monitor/Address/CustomAddressAddButton';
 import { getLocalPath } from '@proton/pass/components/Navigation/routing';
 import { useBreachesTable } from '@proton/pass/hooks/monitor/useBreachesTable';
 import { AddressType } from '@proton/pass/lib/monitor/types';
+import { selectHasCustomDomains } from '@proton/pass/store/selectors';
 
 import { BreachGroupList } from './Group/BreachGroupList';
 import { BreachGroupToggleButton } from './Group/BreachGroupToggleButton';
@@ -19,6 +24,7 @@ const validateType = (type?: string): type is AddressType =>
 
 export const BreachGroup: FC<Props> = ({ match }) => {
     const history = useHistory();
+    const customDomains = useSelector(selectHasCustomDomains);
     const type = validateType(match?.params.type) ? match.params.type : null;
     const { title, data, loading } = useBreachesTable(type ?? FALLBACK);
 
@@ -39,6 +45,10 @@ export const BreachGroup: FC<Props> = ({ match }) => {
                         </>
                     }
                 />
+                {type === AddressType.ALIAS && customDomains && (
+                    <Card rounded background={false} className="mb-3 color-weak">{c('Info')
+                        .t`Aliases with custom domains are not monitored, please add them as a custom email for monitoring.`}</Card>
+                )}
                 <BreachGroupList data={data} loading={loading} />
             </>
         )
