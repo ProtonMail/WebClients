c0d7e68020a0e8e7b267c66c8f6b5bf0ddc8a96d
Edvin CANDON <edvin.candon@proton.ch>
2024-04-29 14:07:38 +0200

    Add custom monitored emails limits

--- packages/pass/components/Monitor/Address/CustomAddressAddButton.tsx
+++ packages/pass/components/Monitor/Address/CustomAddressAddButton.tsx
@@ -3,11 +3,14 @@ import type { FC } from 'react';
 import { Button } from '@proton/atoms/Button';
 import { Icon } from '@proton/components/index';
 import { useMonitor } from '@proton/pass/components/Monitor/MonitorProvider';
+import { MAX_CUSTOM_ADDRESSES } from '@proton/pass/constants';
 
 export const CustomAddressAddButton: FC = () => {
-    const monitor = useMonitor();
+    const { breaches, addAddress } = useMonitor();
+    const disabled = breaches.data.custom.length >= MAX_CUSTOM_ADDRESSES;
+
     return (
-        <Button icon pill size="small" shape="solid" color="weak" onClick={() => monitor.addAddress()}>
+        <Button icon pill size="small" shape="solid" color="weak" disabled={disabled} onClick={addAddress}>
             <Icon name="plus" />
         </Button>
     );
