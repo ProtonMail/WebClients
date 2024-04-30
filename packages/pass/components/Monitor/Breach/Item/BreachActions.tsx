7914c961f0661ac456b6be16a36b3485e3958463
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-04-30 09:39:46 +0200

    Add confirmation modal for breach actions
    [IDTEAM-3225]

--- packages/pass/components/Monitor/Breach/Item/BreachActions.tsx
+++ packages/pass/components/Monitor/Breach/Item/BreachActions.tsx
@@ -4,11 +4,14 @@ import { useSelector } from 'react-redux';
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms/Button';
+import { Alert } from '@proton/components/index';
+import { ConfirmationModal } from '@proton/pass/components/Confirmation/ConfirmationModal';
 import { DropdownMenuButton } from '@proton/pass/components/Layout/Dropdown/DropdownMenuButton';
 import { QuickActionsDropdown } from '@proton/pass/components/Layout/Dropdown/QuickActionsDropdown';
 import { useMonitor } from '@proton/pass/components/Monitor/MonitorProvider';
 import { useResolveBreach } from '@proton/pass/hooks/monitor/useResolveBreach';
 import { useRequest } from '@proton/pass/hooks/useActionRequest';
+import { useConfirm } from '@proton/pass/hooks/useConfirm';
 import { getAddressId } from '@proton/pass/lib/monitor/monitor.utils';
 import { AddressType, type MonitorAddress } from '@proton/pass/lib/monitor/types';
 import { toggleAddressMonitor } from '@proton/pass/store/actions';
@@ -16,6 +19,7 @@ import { deleteCustomAddressRequest, toggleAddressMonitorRequest } from '@proton
 import { selectMonitorSettingByType, selectRequestInFlight } from '@proton/pass/store/selectors';
 
 type Props = { resolved: boolean; disabled: boolean } & MonitorAddress;
+
 export const BreachActions: FC<Props> = ({ resolved, disabled, ...address }) => {
     const { monitored, type, email } = address;
     const monitor = useMonitor();
@@ -27,10 +31,14 @@ export const BreachActions: FC<Props> = ({ resolved, disabled, ...address }) => 
     const initialRequestId = toggleAddressMonitorRequest(getAddressId(address));
     const toggle = useRequest(toggleAddressMonitor, { initialRequestId });
 
+    const confirmResolve = useConfirm(resolve);
+    const confirmToggle = useConfirm(() => toggle.dispatch({ ...address, monitor: !monitored }));
+    const confirmDelete = useConfirm(() => monitor.deleteAddress(getAddressId(address)));
+
     return (
         <div className="flex flex-nowrap gap-2">
             {!resolved && (
-                <Button pill shape="solid" color="weak" onClick={resolve} loading={loading}>
+                <Button pill shape="solid" color="weak" onClick={confirmResolve.prompt} loading={loading}>
                     {c('Action').t`Mark as resolved`}
                 </Button>
             )}
@@ -49,17 +57,59 @@ export const BreachActions: FC<Props> = ({ resolved, disabled, ...address }) => 
                     icon={monitored ? 'eye-slash' : 'eye'}
                     label={monitored ? c('Action').t`Pause monitoring` : c('Action').t`Resume monitoring`}
                     loading={toggle.loading}
-                    onClick={() => toggle.dispatch({ ...address, monitor: !monitored })}
+                    onClick={confirmToggle.prompt}
                 />
                 {address.type === AddressType.CUSTOM && (
                     <DropdownMenuButton
                         icon="trash"
                         label={c('Action').t`Remove`}
                         loading={deleting}
-                        onClick={() => monitor.deleteAddress(getAddressId(address))}
+                        onClick={confirmDelete.prompt}
                     />
                 )}
             </QuickActionsDropdown>
+
+            {confirmResolve.pending && (
+                <ConfirmationModal
+                    open
+                    onClose={confirmResolve.cancel}
+                    onSubmit={confirmResolve.confirm}
+                    title={c('Title').t`Mark as resolved?`}
+                    submitText={c('Action').t`Confirm`}
+                >
+                    <Alert className="mb-4" type="info">
+                        {c('Info')
+                            .t`All current breaches will be marked as resolved. In case of future data breaches, you'll be notified.`}
+                    </Alert>
+                </ConfirmationModal>
+            )}
+
+            {confirmToggle.pending && (
+                <ConfirmationModal
+                    open
+                    onClose={confirmToggle.cancel}
+                    onSubmit={confirmToggle.confirm}
+                    title={monitored ? c('Title').t`Pause monitoring?` : c('Title').t`Resume monitoring?`}
+                    submitText={c('Action').t`Confirm`}
+                >
+                    <Alert className="mb-4" type="info">
+                        {monitored
+                            ? c('Info').t`You will no longer be notified of data breaches for this email address.`
+                            : c('Info').t`You will be notified of data breaches for this email address.`}
+                    </Alert>
+                </ConfirmationModal>
+            )}
+
+            {confirmDelete.pending && (
+                <ConfirmationModal
+                    open
+                    onClose={confirmDelete.cancel}
+                    onSubmit={confirmDelete.confirm}
+                    title={c('Title').t`Remove email address?`}
+                    submitText={c('Action').t`Confirm`}
+                    alertText={c('Warning').t`This email address will no longer be monitored for data breaches.`}
+                />
+            )}
         </div>
     );
 };
