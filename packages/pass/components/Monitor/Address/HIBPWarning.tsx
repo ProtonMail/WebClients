c2bbc6d00de07656a85849550b9e996d0346a8ae
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-05-02 15:19:52 +0200

    Show HIBP info when adding email to monitor
    [IDTEAM-3239]

---
+++ packages/pass/components/Monitor/Address/HIBPWarning.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react';
+
+import { c } from 'ttag';
+
+import { Href } from '@proton/atoms/Href';
+
+export const HIBPWarning: FC = () => {
+    const href = <Href href="https://haveibeenpwned.com/">HIBP</Href>;
+    return (
+        // translation: the variable {href} is a hyperlink, full sentence is: "The custom email will be sent to HIBP for monitoring. No other data will be sent to HIBP."
+        c('Info').jt`The custom email will be sent to ${href} for monitoring. No other data will be sent to HIBP.`
+    );
+};
