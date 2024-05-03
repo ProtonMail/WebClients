7d0725f7b59d3b5e1f5fa2ca788202200b8b369c
Lucien Peslier <lucien.peslier@proton.ch>
2024-05-03 10:02:00 +0000

    [DRVWEB-3942] List shared without shared link

--- applications/drive/src/app/store/_links/useLinksListing/useSharedLinksListing.test.tsx
+++ applications/drive/src/app/store/_links/useLinksListing/useSharedLinksListing.test.tsx
@@ -9,6 +9,10 @@ jest.mock('@proton/shared/lib/api/drive/volume', () => ({
     queryVolumeSharedLinks: jest.fn(),
 }));
 
+jest.mock('@proton/shared/lib/api/drive/sharing', () => ({
+    querySharedByMeLinks: jest.fn(),
+}));
+
 const mockRequest = jest.fn();
 jest.mock('../../_api/useDebouncedRequest', () => {
     const useDebouncedRequest = () => {
@@ -36,11 +40,12 @@ jest.mock('../useLink', () => {
 });
 
 const queryVolumeSharedLinksMock = require('@proton/shared/lib/api/drive/volume').queryVolumeSharedLinks as jest.Mock;
+const querySharedByMeLinksMock = require('@proton/shared/lib/api/drive/sharing').querySharedByMeLinks as jest.Mock;
 const generateArrayOfRandomStrings = (size: number): string[] => {
     return Array.from({ length: size }, () => Math.random().toString(36).substring(2));
 };
 
-describe('useSharedLinksListing', () => {
+describe('useSharedLinksListing LEGACY', () => {
     let hook: {
         current: ReturnType<typeof useSharedLinksListing>;
     };
@@ -75,7 +80,7 @@ describe('useSharedLinksListing', () => {
         mockRequest.mockResolvedValue(response);
 
         await act(async () => {
-            await hook.current.loadSharedLinks(new AbortController().signal, volumeId, () =>
+            await hook.current.loadSharedLinksLEGACY(new AbortController().signal, volumeId, () =>
                 Promise.resolve({ links: [], parents: [], errors: [] })
             );
         });
@@ -106,10 +111,10 @@ describe('useSharedLinksListing', () => {
         };
         mockRequest.mockResolvedValueOnce(firstResponse).mockResolvedValueOnce(secondResponse);
 
-        const { loadSharedLinks } = hook.current;
+        const { loadSharedLinksLEGACY } = hook.current;
 
         await act(async () => {
-            await loadSharedLinks(new AbortController().signal, volumeId, () =>
+            await loadSharedLinksLEGACY(new AbortController().signal, volumeId, () =>
                 Promise.resolve({ links: [], parents: [], errors: [] })
             );
         });
@@ -117,5 +122,85 @@ describe('useSharedLinksListing', () => {
         expect(queryVolumeSharedLinksMock).toHaveBeenCalledWith(volumeId, { Page: page + 1, PageSize: PAGE_SIZE });
         // verify that the script terminates successfully
         expect(queryVolumeSharedLinksMock).toBeCalledTimes(2);
+    });
+});
+
+describe('useSharedLinksListing', () => {
+    let hook: {
+        current: ReturnType<typeof useSharedLinksListing>;
+    };
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+        const wrapper = ({ children }: { children: React.ReactNode }) => (
+            <VolumesStateProvider>
+                <LinksStateProvider>{children}</LinksStateProvider>
+            </VolumesStateProvider>
+        );
+
+        const { result } = renderHook(() => useSharedLinksListing(), { wrapper });
+        hook = result;
+
+        jest.resetAllMocks();
+    });
+
+    it('should fetch the first batch of shared by me links for a given volume', async () => {
+        const volumeId = '1';
+        const response = {
+            Links: [
+                {
+                    LinkID: 'linkID',
+                    ShareID: 'shareID',
+                    ContextShareID: 'contextShareID',
+                },
+            ],
+        };
+
+        mockRequest.mockResolvedValue(response);
+
+        await act(async () => {
+            await hook.current.loadSharedByMeLinks(new AbortController().signal, volumeId, () =>
+                Promise.resolve({ links: [], parents: [], errors: [] })
+            );
+        });
+        expect(querySharedByMeLinksMock).toHaveBeenCalledWith(volumeId, { AnchorID: undefined });
+    });
+
+    it('should call again if there is more shared links', async () => {
+        const volumeId = '1';
+        let firstResponse = {
+            More: true,
+            AnchorID: 'linkID',
+            Links: [
+                {
+                    LinkID: 'linkID',
+                    ShareID: 'shareID',
+                    ContextShareID: 'contextShareID',
+                },
+            ],
+        };
+        let secondResponse = {
+            AnchorID: 'linkID2',
+            Links: [
+                {
+                    LinkID: 'linkID2',
+                    ShareID: 'shareID2',
+                    ContextShareID: 'contextShareID2',
+                },
+            ],
+        };
+        mockRequest.mockResolvedValueOnce(firstResponse).mockResolvedValueOnce(secondResponse);
+
+        const { loadSharedByMeLinks } = hook.current;
+
+        await act(async () => {
+            await loadSharedByMeLinks(new AbortController().signal, volumeId, () =>
+                Promise.resolve({ links: [], parents: [], errors: [] })
+            );
+        });
+
+        expect(querySharedByMeLinksMock).toHaveBeenCalledWith(volumeId, { AnchorID: 'linkID' });
+        // verify that the script terminates successfully
+        expect(querySharedByMeLinksMock).toBeCalledTimes(2);
     });
 });
