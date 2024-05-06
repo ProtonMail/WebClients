739f7b91c4deb4ca7d018053f7ad43b6fead5cd1
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-05-06 09:17:20 +0000

    Delete desktop spotlight

--- packages/components/components/drawer/views/SecurityCenter/BreachAlertsSpotlight.tsx
+++ packages/components/components/drawer/views/SecurityCenter/BreachAlertsSpotlight.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 
-import { differenceInDays, differenceInMilliseconds, fromUnixTime } from 'date-fns';
+import { differenceInDays, fromUnixTime } from 'date-fns';
 import { c } from 'ttag';
 
 import { Spotlight, useSpotlightShow } from '@proton/components/components';
@@ -12,36 +12,13 @@ import {
     useUser,
     useWelcomeFlags,
 } from '@proton/components/hooks';
-import { FeatureCode, useFeature } from '@proton/features';
-import { FeatureContextValue } from '@proton/features/useFeatures';
-import { DARK_WEB_MONITORING_NAME, MINUTE, PLANS } from '@proton/shared/lib/constants';
+import { FeatureCode } from '@proton/features';
+import { DARK_WEB_MONITORING_NAME, PLANS } from '@proton/shared/lib/constants';
 import spotlightImg from '@proton/styles/assets/img/illustrations/sentinel-shield-bolt-breach-alert.svg';
 
 interface Props {
     children: React.ReactNode;
 }
-
-const TIME_THRESHOLD = 1 * MINUTE;
-const hasFeatureBeenDisplayedOrRemoved = (overlappingFeature: FeatureContextValue<any>, currentTime: number) => {
-    const currentTimeInSeconds = Math.floor(currentTime / 1000);
-
-    if (!overlappingFeature) {
-        return true;
-    }
-    const { feature } = overlappingFeature;
-
-    if (feature) {
-        if (
-            Object.keys(feature).length === 0 ||
-            (feature.Value === false &&
-                differenceInMilliseconds(fromUnixTime(currentTimeInSeconds), fromUnixTime(feature.UpdateTime)) >=
-                    TIME_THRESHOLD)
-        ) {
-            return true;
-        }
-    }
-    return false;
-};
 
 const BreachAlertsSpotlight = ({ children }: Props) => {
     const anchorRef = useRef<HTMLDivElement>(null);
@@ -50,7 +27,6 @@ const BreachAlertsSpotlight = ({ children }: Props) => {
     const [organization] = useOrganization();
     const { viewportWidth } = useActiveBreakpoint();
     const [{ isDone }] = useWelcomeFlags();
-    const inboxDesktopFeature = useFeature(FeatureCode.SpotlightInboxDesktop);
 
     /**
      * Display conditions:
@@ -58,10 +34,8 @@ const BreachAlertsSpotlight = ({ children }: Props) => {
      * 2. User has done the welcome flow
      * 3. User has a plan that can utilize breach alerts: free, mail2022, bundle2022, family2022, visionary2022
      * 4. User does not have a custom domain
-     * 5. Any overlapping Feature Spotlights have already been shown for active users and have been shown at least a minute before for inative users
-     * 6. Account is older than 4 days
+     * 5. Account is older than 4 days
      */
-
     const isPlanAllowed =
         user.isFree ||
         [PLANS.MAIL, PLANS.BUNDLE, PLANS.FAMILY, PLANS.NEW_VISIONARY].some((plan) =>
@@ -70,17 +44,8 @@ const BreachAlertsSpotlight = ({ children }: Props) => {
     const hasCustomDomains = organization && organization?.UsedDomains > 0;
     const accountIsOlderThanFourDays = differenceInDays(new Date(), fromUnixTime(user.CreateTime)) >= 4;
 
-    // we want to prevent breach alerts spotlight from overlapping with the sec center and desktop spotlights
-    const currentTime = Date.now();
-    const noOverlapping = hasFeatureBeenDisplayedOrRemoved(inboxDesktopFeature, currentTime);
-
     const displaySpotlight =
-        !viewportWidth['<=small'] &&
-        isDone &&
-        isPlanAllowed &&
-        !hasCustomDomains &&
-        noOverlapping &&
-        accountIsOlderThanFourDays;
+        !viewportWidth['<=small'] && isDone && isPlanAllowed && !hasCustomDomains && accountIsOlderThanFourDays;
 
     const { show, onDisplayed, onClose } = useSpotlightOnFeature(
         FeatureCode.SpotlightBreachAlertSecurityCenter,
