cdbc6e93e9901ce537bfdd631a1e3c6cc819a377
Edvin CANDON <edvin.candon@proton.ch>
2024-05-06 09:11:13 +0200

    Move `MonitorProvider` lower in component tree
    - Avoid having the MonitorContext set up on all routes
    - Prefer local context for specific monitor routes
    - Reflect changes in `MonitorButton`
    - Remove `refreshedAt` on MonitorContext

--- packages/pass/components/Menu/Monitor/MonitorButton.tsx
+++ packages/pass/components/Menu/Monitor/MonitorButton.tsx
@@ -1,31 +1,40 @@
 import { type FC } from 'react';
+import { useSelector } from 'react-redux';
 import { useRouteMatch } from 'react-router-dom';
 
 import { c } from 'ttag';
 
+import type { IconName } from '@proton/components/index';
 import { PillBadge } from '@proton/pass/components/Layout/Badge/PillBadge';
 import { DropdownMenuButton } from '@proton/pass/components/Layout/Dropdown/DropdownMenuButton';
-import { useMonitor } from '@proton/pass/components/Monitor/MonitorProvider';
-import { getMonitorIcon } from '@proton/pass/components/Monitor/utils';
 import { useNavigation } from '@proton/pass/components/Navigation/NavigationProvider';
 import { getLocalPath } from '@proton/pass/components/Navigation/routing';
+import { useFeatureFlag } from '@proton/pass/hooks/useFeatureFlag';
+import { selectTotalBreaches } from '@proton/pass/store/selectors';
+import { PassFeature } from '@proton/pass/types/api/features';
 import clsx from '@proton/utils/clsx';
 
 export const MonitorButton: FC = () => {
     const { navigate } = useNavigation();
-    const monitor = useMonitor();
+    const enabled = useFeatureFlag(PassFeature.PassMonitor);
+    const breachCount = useSelector(selectTotalBreaches) ?? 0;
     const isActive = useRouteMatch(getLocalPath('monitor'));
+
+    const monitorIcon = ((): IconName => {
+        if (breachCount > 0) return enabled ? 'pass-shield-monitoring-warning' : 'pass-shield-warning';
+        else return enabled ? 'pass-shield-monitoring-ok' : 'pass-shield-ok';
+    })();
 
     return (
         <DropdownMenuButton
-            icon={getMonitorIcon(monitor)}
+            icon={monitorIcon}
             className={clsx('rounded', isActive && 'color-primary bg-weak')}
             ellipsis
             label={c('Action').t`Pass Monitor`}
             extra={
-                monitor.breaches.count > 0 && (
+                breachCount > 0 && (
                     <PillBadge
-                        label={monitor.breaches.count}
+                        label={breachCount}
                         color="var(--signal-danger-contrast)"
                         backgroundColor="var(--signal-danger)"
                     />
