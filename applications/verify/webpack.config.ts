0fa099b06be90e94bbc9e9070f87a4be0a1789c7
Mattias Svanström <mattias.svanstrom@proton.ch>
2024-05-07 13:16:38 +0200

    Fix browserslists override

--- applications/verify/webpack.config.ts
+++ applications/verify/webpack.config.ts
@@ -3,15 +3,11 @@ import webpack from 'webpack';
 import getConfig from '@proton/pack/webpack.config';
 
 const result = (env: any): webpack.Configuration => {
-    const result = getConfig(env);
-
-    return {
-        ...result,
-        target:
-            result.mode === 'production'
-                ? 'browserslist:>0.1%, IE 11, Firefox ESR, Safari 10, Chrome 50'
-                : result.target,
-    };
+    const isProduction = process.env.NODE_ENV === 'production';
+    return getConfig({
+        ...env,
+        browserslist: isProduction ? '>0.1%, IE 11, Firefox ESR, Safari 10, Chrome 50' : undefined,
+    });
 };
 
 export default result;
