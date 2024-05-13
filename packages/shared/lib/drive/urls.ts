99b7c3fd6e0d3405ff05e517f26db15e0c06c1d1
Gregory Linford <gregory.linford@proton.ch>
2024-05-13 10:10:46 +0000

    [DRVWEB-3973] Change CTA link to Sign-Up

--- packages/shared/lib/drive/urls.ts
+++ packages/shared/lib/drive/urls.ts
@@ -1,6 +1,10 @@
 import { getStaticURL } from '@proton/shared/lib/helpers/url';
 
+import { getAppHref } from '../apps/helper';
+import { APPS } from '../constants';
+
 export const DRIVE_LANDING_PAGE = getStaticURL('/drive');
 export const DRIVE_PRICING_PAGE = getStaticURL('/drive/pricing?product=drive');
+export const DRIVE_SIGNUP = getAppHref('/drive/signup', APPS.PROTONACCOUNT);
 export const DRIVE_IOS_APP = 'https://apps.apple.com/app/proton-drive-cloud-storage/id1509667851';
 export const DRIVE_ANDROID_APP = 'https://play.google.com/store/apps/details?id=me.proton.android.drive';
