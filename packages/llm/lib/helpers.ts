032e474ef99daf007bac95340f6db4520cfb623a
Marc Dupont <marc.dupont@proton.ch>
2024-05-15 11:53:25 +0200

    Detox prompt: reject harmful queries from the user

--- packages/llm/lib/helpers.ts
+++ packages/llm/lib/helpers.ts
@@ -54,24 +54,66 @@ export const getAssistantStatus = ({
 };
 
 export const cleanAssistantEmailGeneration = (inputText: string) => {
-    /* Assistant often generates content with a subject and a body with the following format
+    /* The LLM generates text that contains extraneous data, such as:
      *
-     *      Subject: email subject
-     *      Body: some text
+     *   - a token to tell if the prompt was harmful (yes/no)
+     *   - a subject (we drop it)
+     *   - a prefix "Body:" (we drop it)
+     *   - a signature "[Your Name]" (we drop it)
      *
-     *      some other text
-     *
-     * We need to clean this in order to display the correct content to the user
+     * This method preprocesses the LLM generated text and return only the extracted email content without
+     * the extraneous text.
      */
-    let text = inputText
-        .split('\n')
-        .filter((line) => !line.startsWith('Subject:'))
-        .join('\n')
-        .trim();
-    if (text.startsWith('Body:')) {
-        text = text.substring('Body:'.length).trim();
+
+    function isLast(i: number, lines: any[]) {
+        return i + 1 === lines.length;
     }
-    return text;
+
+    let text = inputText.trim();
+
+    // The LLM begins with an answer to the question: "Harmful? (yes/no):"
+    // If the prompt is harmful, we do not let the text pass through.
+    if (text.toLowerCase().startsWith('yes')) {
+        return '[Rejected]';
+    }
+
+    // Split lines
+    let lines = text.split('\n');
+    lines = lines.map((line) => line.trim());
+
+    // The LLM should have replied "no" to the harmful prompt.
+    // We drop this answer since it's not part of the actual email we want to generate.
+    lines = lines.filter((line, i) => i > 0 || line !== 'no');
+
+    // Drop the subject.
+    // The LLM often wants to generates a subject line before the email content. We're not using it at
+    // the moment, so we just get rid of this line altogether.
+    lines = lines.filter((line) => !line.startsWith('Subject:'));
+    // Do not show a partially generated line, like "Subj"
+    lines = lines.filter((line, i) => !isLast(i, lines) || !'Subject:'.startsWith(line));
+
+    // Drop the signature.
+    // It's very difficult to ask the LLM to **not sign**, but dropping [Your Name] seems to work better, so
+    // in the instructions, we ask explicitly the LLM to sign with "[Your Name]" so we can look for it
+    // and remove it.
+    lines = lines.filter((line) => !line.startsWith('[Your Name]'));
+    // Do not show a partially generated line, like "[", "[Your", "[Your Name"
+    lines = lines.filter((line, i) => !isLast(i, lines) || !'[Your Name]'.startsWith(line));
+
+    // Drop the "Body:" prefix, but keep the rest of the line.
+    // Do not show a partially generated line, like "Bo"
+    lines = lines.filter((line, i) => !isLast(i, lines) || !'Body:'.startsWith(line));
+    lines = lines.map((line) => line.replace(/^Body:\s*/, ''));
+
+    // Drop spaces at the beginning of lines (not sure why it happens, but it does).
+    lines = lines.map((line) => line.replace(/^ +/, ''));
+
+    // Join back the lines into a string. If some lines have been removed, we collapse the newlines to 2 max.
+    const outputText = lines
+        .join('\n')
+        .replaceAll(/\n{3,}/g, '\n\n')
+        .trim();
+    return outputText;
 };
 
 export const queryAssistantModels = async (api: Api) => {
