5b506e757c4cc8a366bdc63debc16d4287af9841
Pierre Bondoerffer <pierre.bondoerffer@proton.ch>
2024-05-16 16:51:25 +0000

    Integrate documents into Drive

---
+++ applications/drive/src/app/store/_documents/useDocumentActions.ts
@@ -0,0 +1,59 @@
+import { c } from 'ttag';
+
+import { EnrichedError } from '../../utils/errorHandling/EnrichedError';
+import { useVolumesState } from '../_volumes';
+import { useOpenDocument } from './useOpenDocument';
+
+export const useDocumentActions = () => {
+    const volumeState = useVolumesState();
+    const { openDocumentWindow } = useOpenDocument();
+
+    const findVolume = (shareId: string) => {
+        // We can't pass volumeId easily everywhere in Drive, so let's try to
+        // resolve it from cache to avoid having to write volume-finding logic everywhere
+        const volumeId = volumeState.findVolumeId(shareId);
+
+        if (!volumeId) {
+            throw new EnrichedError(
+                // translator: Error message when failure to open Proton Docs document
+                c('Notification').t`Failed to open document`,
+                {
+                    tags: { shareId },
+                },
+                'No volume ID found when opening document'
+            );
+        }
+
+        return volumeId;
+    };
+
+    const openDocument = ({ shareId, linkId }: { shareId: string; linkId: string }) => {
+        openDocumentWindow({
+            mode: 'open',
+            volumeId: findVolume(shareId),
+            linkId,
+        });
+    };
+
+    const createDocument = ({ shareId, parentLinkId }: { shareId: string; parentLinkId: string }) => {
+        openDocumentWindow({
+            mode: 'create',
+            volumeId: findVolume(shareId),
+            parentLinkId,
+        });
+    };
+
+    const convertDocument = ({ shareId, linkId }: { shareId: string; linkId: string }) => {
+        openDocumentWindow({
+            mode: 'convert',
+            volumeId: findVolume(shareId),
+            linkId,
+        });
+    };
+
+    return {
+        openDocument,
+        createDocument,
+        convertDocument,
+    };
+};
