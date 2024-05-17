851ed58268b7d1317ca1be0f8b3df82da46e5471
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-05-17 17:23:13 +0200

    Show both email & username fields in UI
    [IDTEAM-3200]

---
+++ packages/pass/hooks/useDisplayEmailUsernameFields.ts
@@ -0,0 +1,25 @@
+import { validateEmailAddress } from '@proton/shared/lib/helpers/email';
+
+type Props = {
+    itemEmail: string;
+    itemUsername: string;
+};
+
+/** Handle username & email display taking into account users coming from ContentFormatVersion < 5:
+ * itemEmail may be an invalid email and should be displayed as username, unless itemUsername already exists */
+export const useDisplayEmailUsernameFields = ({ itemEmail, itemUsername }: Props) => {
+    // TODO: use Rust's email validation
+    const emailDisplay = validateEmailAddress(itemEmail) || itemUsername ? itemEmail : '';
+    const usernameDisplay = (() => {
+        if (itemUsername) {
+            return itemUsername;
+        } else if (itemEmail && !emailDisplay) {
+            // itemEmail is not a valid email, so display itemEmail as username
+            return itemEmail;
+        } else {
+            return '';
+        }
+    })();
+
+    return { emailDisplay, usernameDisplay };
+};
