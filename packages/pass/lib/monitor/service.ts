bd18984ee60a2fe50436a09a89047841a692efca
Edvin CANDON <edvin.candon@proton.ch>
2024-05-18 10:38:49 +0200

    Reflect changes in passkey and monitor services
    - Expose proxy on PassCoreProvider

--- packages/pass/lib/monitor/service.ts
+++ packages/pass/lib/monitor/service.ts
@@ -1,7 +1,6 @@
-import type { WasmPasswordScoreResult } from '@protontech/pass-rust-core';
 import type { Store } from 'redux';
 
-import type { PassCoreService } from '@proton/pass/lib/core/types';
+import type { PassCoreProxy } from '@proton/pass/lib/core/types';
 import { hasDomain, hasOTP } from '@proton/pass/lib/items/item.predicates';
 import { intoSelectedItem } from '@proton/pass/lib/items/item.utils';
 import { selectMonitoredLogins } from '@proton/pass/store/selectors';
@@ -10,24 +9,21 @@ import { and, invert } from '@proton/pass/utils/fp/predicates';
 import { deobfuscate } from '@proton/pass/utils/obfuscate/xor';
 
 export interface MonitorService {
-    analyzePassword: (password: string) => Promise<WasmPasswordScoreResult>;
     checkMissing2FAs: () => Promise<UniqueItem[]>;
     checkWeakPasswords: () => Promise<UniqueItem[]>;
 }
 
 /** MonitorService provides `PassMonitor` methods that rely
  * on the `PassRustCore` module */
-export const createMonitorService = (core: PassCoreService, store: Store): MonitorService => {
+export const createMonitorService = (core: PassCoreProxy, store: Store): MonitorService => {
     const getLoginItems = () => selectMonitoredLogins(store.getState());
 
     const service: MonitorService = {
-        analyzePassword: (password) => core.exec('analyze_password', password),
-
         checkMissing2FAs: async () => {
             const logins = getLoginItems();
             const candidates = logins.filter(and(hasDomain, invert(hasOTP)));
             const domains = candidates.flatMap((item) => item.data.content.urls);
-            const eligible = await core.exec('twofa_domains_eligible', domains);
+            const eligible = await core.twofa_domains_eligible(domains);
             const eligibleDomain = (url: string) => eligible.get(url) === true;
 
             return candidates.filter((item) => item.data.content.urls.some(eligibleDomain)).map(intoSelectedItem);
@@ -37,7 +33,7 @@ export const createMonitorService = (core: PassCoreService, store: Store): Monit
             const logins = getLoginItems();
             const candidates = logins.filter((item) => item.data.content.password.v);
             const passwords = candidates.map((item) => deobfuscate(item.data.content.password));
-            const scores = await core.exec('check_password_scores', passwords);
+            const scores = await core.check_password_scores(passwords);
 
             return candidates.filter((_, idx) => scores[idx] !== 'Strong').map(intoSelectedItem);
         },
