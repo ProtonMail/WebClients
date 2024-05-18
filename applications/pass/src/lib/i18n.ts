54ce544192b3b3a5214ffaf0582fa8903394582f
Edvin CANDON <edvin.candon@proton.ch>
2024-05-18 11:32:39 +0200

    Setup PassCoreProxy in web-app
    - Clean-up web app `lib` in order to share with pass-desktop

--- applications/pass/src/lib/i18n.ts
+++ applications/pass/src/lib/i18n.ts
@@ -1,20 +1,20 @@
-import locales from 'proton-pass-web/app/locales';
-
 import { authStore } from '@proton/pass/lib/auth/store';
 import { createI18nService } from '@proton/pass/lib/i18n/service';
+import type { TtagLocaleMap } from '@proton/shared/lib/interfaces';
 import noop from '@proton/utils/noop';
 
 import { settings } from './settings';
 
-export const i18n = createI18nService({
-    locales,
-    getLocale: async () => (await settings.resolve()).locale,
-    onLocaleChange: (locale) => {
-        if (authStore.hasSession()) {
-            settings
-                .resolve()
-                .then((localSettings) => settings.sync({ ...localSettings, locale }))
-                .catch(noop);
-        }
-    },
-});
+export const i18n = (locales: TtagLocaleMap) =>
+    createI18nService({
+        locales,
+        getLocale: async () => (await settings.resolve()).locale,
+        onLocaleChange: (locale) => {
+            if (authStore.hasSession()) {
+                settings
+                    .resolve()
+                    .then((localSettings) => settings.sync({ ...localSettings, locale }))
+                    .catch(noop);
+            }
+        },
+    });
