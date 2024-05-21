c5b5e6a664ee5439ccc98dfaf9245b899b1f1d17
larabr <7375870+larabr@users.noreply.github.com>
2024-05-21 18:00:23 +0200

    Drop JSBI, use BigInt instead

--- packages/crypto/lib/bigInteger.ts
+++ packages/crypto/lib/bigInteger.ts
@@ -1,1 +1,13 @@
 export * from 'pmcrypto-v6-canary/lib/bigInteger';
+
+/**
+ * Get this value as an exact Number (max 53 bits)
+ * Fails if this value is too large
+ * @throws if input is larger than `Number.MAX_SAFE_INTEGER`
+ */
+export function bigIntToNumber(x: bigint) {
+    if (x > BigInt(Number.MAX_SAFE_INTEGER)) {
+        throw new Error('Number can only safely store up to 53 bits');
+    }
+    return Number(x);
+}
