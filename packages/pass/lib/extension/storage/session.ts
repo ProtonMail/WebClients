8f87966426274fb5dd46d954a3179ad4782db769
Edvin CANDON <edvin.candon@proton.ch>
2024-05-21 20:07:15 +0200

    Support browser.storage.session on firefox build

--- packages/pass/lib/extension/storage/session.ts
+++ packages/pass/lib/extension/storage/session.ts
@@ -7,7 +7,7 @@
  * extensions. Once FF has full MV3 support we can
  * safely port it to webextension-polyfill
  * ⚠️ ⚠️ ⚠️ */
-import { chromeAPI } from '@proton/pass/lib/globals/browser';
+import browser from '@proton/pass/lib/globals/browser';
 import type {
     GetItem,
     GetItems,
@@ -20,39 +20,15 @@ import type {
 
 import { createMemoryStorage } from './memory';
 
-const getItems: GetItems = (keys) =>
-    new Promise((resolve, reject) => {
-        chromeAPI.storage.session.get(keys, (items) => {
-            const err = chromeAPI.runtime.lastError;
-            return err ? reject(err) : resolve(items);
-        });
-    });
-
-export const getItem: GetItem = async (key) => (await getItems([key]))?.[key] ?? null;
-
-const setItems: SetItems = (items) =>
-    new Promise((resolve, reject) => {
-        chromeAPI.storage.session.set(items, () => {
-            const err = chromeAPI.runtime.lastError;
-            return err ? reject(err) : resolve();
-        });
-    });
-
-export const setItem: SetItem = (key, value) => setItems({ [key]: value });
-
-export const removeItems: RemoveItems = (keys) =>
-    new Promise((resolve, reject) => {
-        chromeAPI.storage.session.remove(keys, () => {
-            const err = chromeAPI.runtime.lastError;
-            return err ? reject(err) : resolve();
-        });
-    });
-
+const getItems: GetItems = (keys) => browser.storage.session.get(keys);
+const getItem: GetItem = async (key) => (await getItems([key]))?.[key] ?? null;
+const setItems: SetItems = (items) => browser.storage.session.set(items);
+const setItem: SetItem = (key, value) => setItems({ [key]: value });
+const removeItems: RemoveItems = (keys) => browser.storage.session.remove(keys);
 const removeItem: RemoveItem = (key) => removeItems([key]);
+const clear = (): Promise<void> => browser.storage.session.clear();
 
-const clear = (): Promise<void> => chromeAPI.storage.session.clear();
-
-const chromeSessionStorage: StorageInterface = {
+const browserSessionStorage: StorageInterface = {
     getItems,
     getItem,
     setItems,
@@ -62,4 +38,4 @@ const chromeSessionStorage: StorageInterface = {
     clear,
 };
 
-export default BUILD_TARGET === 'firefox' ? createMemoryStorage() : chromeSessionStorage;
+export default browser.storage?.session === undefined ? createMemoryStorage() : browserSessionStorage;
