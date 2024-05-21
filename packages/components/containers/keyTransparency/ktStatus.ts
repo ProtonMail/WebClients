e0b3834f218c08b410665a1749474305374b23b6
larabr <7375870+larabr@users.noreply.github.com>
2024-05-21 17:44:31 +0200

    Drop leftover BigInt and Safari 11 checks
    We now only support Safari 14+

--- packages/components/containers/keyTransparency/ktStatus.ts
+++ packages/components/containers/keyTransparency/ktStatus.ts
@@ -1,7 +1,6 @@
 import { serverTime } from '@proton/crypto';
 import { KT_DATA_VALIDITY_PERIOD, KT_DOMAINS, ctLogs, getBaseDomain } from '@proton/key-transparency';
 import { HOUR } from '@proton/shared/lib/constants';
-import { isIos11, isSafari11 } from '@proton/shared/lib/helpers/browser';
 
 export enum KtFeatureEnum {
     DISABLE,
@@ -14,9 +13,6 @@ export const isKTActive = (feature: KT_FF) => {
     // Do not activate KT if
     //  - feature flag is off;
     //  - the api is not prod's or proton.black's
-    //  - safari 11 or ios 11 is used due to issues with the CryptoProxy
-    //  - BigInt is not supported (it is needed for VRF verification);
-    //  - BigInt is only partially implemented and does not support -- (it is needed for VRF verification)
     //  - the hardcoded KT certificate data is older than 6 months.
     //  - the server time compared to the client time is off by more than 24 hours -- (UI warning is shown to prevent attacks)
     //  - Web Crypto isn't fully supported (it is needed for certificate verification in pki.js)
@@ -26,23 +22,6 @@ export const isKTActive = (feature: KT_FF) => {
 
     const domain = getBaseDomain(false);
     if (domain === KT_DOMAINS.UNKNOWN) {
-        return false;
-    }
-
-    if (isSafari11() || isIos11()) {
-        return false;
-    }
-
-    if (typeof BigInt === 'undefined') {
-        return false;
-    }
-
-    try {
-        // Test if the -- operand is supported by BigInt
-        // because it is used by VRF verification.
-        let check = BigInt('0x1'); // eslint-disable-line @typescript-eslint/no-unused-vars
-        check--; // eslint-disable-line @typescript-eslint/no-unused-vars
-    } catch {
         return false;
     }
 
