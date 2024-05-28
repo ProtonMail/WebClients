eef6e81e1517fd5c863d9a0e32f8552042b2da0a
Alexey Karpov <alexey.karpov@proton.ch>
2024-05-28 09:42:09 +0000

    Added JCB logo

--- packages/components/containers/payments/Alert3ds.tsx
+++ packages/components/containers/payments/Alert3ds.tsx
@@ -2,6 +2,7 @@ import { c } from 'ttag';
 
 import americanExpressSafekeySvg from '@proton/styles/assets/img/bank-icons/amex-safekey-colored.svg';
 import discoverProtectBuySvg from '@proton/styles/assets/img/bank-icons/discover-protectbuy-colored.svg';
+import jcbLogoSvg from '@proton/styles/assets/img/bank-icons/jcb-logo.png';
 import mastercardSecurecodeSvg from '@proton/styles/assets/img/bank-icons/mastercard-securecode-colored.svg';
 import verifiedByVisaSvg from '@proton/styles/assets/img/bank-icons/visa-secure-colored.svg';
 
@@ -11,7 +12,7 @@ const Alert3ds = () => {
             <div className="mb-2 text-sm" data-testid="3ds-info">
                 {c('Info').t`We use 3-D Secure to protect your payments`}
             </div>
-            <div className="flex flex-nowrap items-center justify-center">
+            <div className="flex items-center justify-center">
                 <img
                     alt={c('Info').t`Visa Secure logo`}
                     className="mr-4"
@@ -36,6 +37,7 @@ const Alert3ds = () => {
                     style={{ maxHeight: '44px' }}
                     src={americanExpressSafekeySvg}
                 />
+                <img alt={c('Info').t`JCB logo`} className="mr-4" style={{ maxHeight: '44px' }} src={jcbLogoSvg} />
             </div>
         </div>
     );
