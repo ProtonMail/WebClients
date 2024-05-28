ba8d03a9c4eaf64346f383a85ba2719e9c20eddb
Haroun Butt <haroun.butt@proton.ch>
2024-05-28 07:51:28 +0000

    Pass AddressKeyId to endpoints for Volume Creation, Volume Restore, Device...

--- applications/drive/src/app/store/_shares/useCreatePhotos.ts
+++ applications/drive/src/app/store/_shares/useCreatePhotos.ts
@@ -15,7 +15,10 @@ export function useCreatePhotos() {
     const createPhotosShare = async () => {
         const abortController = new AbortController();
         const defaultShare = await getDefaultShare();
-        const { address, privateKey } = await getShareCreatorKeys(abortController.signal, defaultShare.shareId);
+        const { address, privateKey, addressKeyID } = await getShareCreatorKeys(
+            abortController.signal,
+            defaultShare.shareId
+        );
         const { bootstrap, folderPrivateKey } = await generateDriveBootstrap(privateKey);
         const { NodeHashKey: FolderHashKey } = await generateNodeHashKey(folderPrivateKey, folderPrivateKey);
 
@@ -24,6 +27,7 @@ export function useCreatePhotos() {
                 Share: {
                     Name: 'Photos',
                     AddressID: address.ID,
+                    AddressKeyID: addressKeyID,
                     Key: bootstrap.ShareKey,
                     Passphrase: bootstrap.SharePassphrase,
                     PassphraseSignature: bootstrap.SharePassphraseSignature,
