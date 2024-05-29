5a88c25b482a20d4876e0e0673f4db71fa4fc51c
Gregory Linford <gregory.linford@proton.ch>
2024-05-29 15:19:25 +0000

    Cleanup and refactor isProtonUser cookie for future usage

--- packages/components/hooks/useIsProtonUserCookie.ts
+++ packages/components/hooks/useIsProtonUserCookie.ts
@@ -1,28 +1,12 @@
 import { useEffect } from 'react';
 
-import { getCookie, setCookie } from '@proton/shared/lib/helpers/cookies';
-import { getSecondLevelDomain } from '@proton/shared/lib/helpers/url';
-
-export const IS_PROTON_USER_COOKIE_NAME = 'is-proton-user';
-const today = new Date();
-const lastDayOfTheYear = new Date(today.getFullYear(), 11, 31, 23, 59, 59);
-const cookieDomain = `.${getSecondLevelDomain(window.location.hostname)}`;
+import { setIsProtonUserCookie } from '../helpers/protonUserCookie';
 
 // Set a cookie for Proton users, used by proton.me website
 // Also use in Drive download page to show/hide some marketing info
 const useIsProtonUserCookie = () => {
     useEffect(() => {
-        const cookie = getCookie(IS_PROTON_USER_COOKIE_NAME);
-
-        if (cookie !== '1') {
-            setCookie({
-                cookieName: IS_PROTON_USER_COOKIE_NAME,
-                cookieValue: '1',
-                cookieDomain,
-                expirationDate: lastDayOfTheYear.toUTCString(),
-                path: '/',
-            });
-        }
+        setIsProtonUserCookie();
     }, []);
 };
 
