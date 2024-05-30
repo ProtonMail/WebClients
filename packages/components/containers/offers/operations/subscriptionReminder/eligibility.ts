da367a757dbe299b68487f29a0bf8cee458c0625
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-05-30 14:16:18 +0200

    Delete SubscriptionReminderMinimalCreateTime flag and adapt offer config

--- packages/components/containers/offers/operations/subscriptionReminder/eligibility.ts
+++ packages/components/containers/offers/operations/subscriptionReminder/eligibility.ts
@@ -1,4 +1,4 @@
-import { differenceInDays, fromUnixTime, isAfter } from 'date-fns';
+import { differenceInDays, fromUnixTime } from 'date-fns';
 
 import { domIsBusy } from '@proton/shared/lib/busy';
 import { APPS } from '@proton/shared/lib/constants';
@@ -7,26 +7,21 @@ import { ProtonConfig, UserModel } from '@proton/shared/lib/interfaces';
 interface Props {
     user: UserModel;
     protonConfig: ProtonConfig;
-    minimalCreationTime: number;
 }
 
 const ACCOUNT_AGE_DAY_THESHOLD = 14;
 
-const isEligible = ({ user, protonConfig, minimalCreationTime }: Props) => {
+const isEligible = ({ user, protonConfig }: Props) => {
     const { isFree, isDelinquent, CreateTime } = user;
     const isNotDelinquent = !isDelinquent;
     const hasValidApp = protonConfig.APP_NAME === APPS.PROTONMAIL;
 
     const createTimeDate = fromUnixTime(CreateTime);
-    const minimalCreationTimeDate = fromUnixTime(minimalCreationTime);
     const isAccountOlderThanTheshold = differenceInDays(new Date(), createTimeDate) >= ACCOUNT_AGE_DAY_THESHOLD;
-    const isAccountCreatedBeforeMinimalCreationTime = isAfter(createTimeDate, minimalCreationTimeDate);
-    //True if the account is older than the threshold and was created after the minimal creation time
-    const isAccountOldEnough = isAccountOlderThanTheshold && isAccountCreatedBeforeMinimalCreationTime;
 
     const isDomBusy = domIsBusy();
 
-    return isFree && isNotDelinquent && hasValidApp && isAccountOldEnough && !isDomBusy;
+    return isFree && isNotDelinquent && hasValidApp && isAccountOlderThanTheshold && !isDomBusy;
 };
 
 export default isEligible;
