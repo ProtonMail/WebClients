84d3dce36fb98a99aba3f15f7cd2632364cdbd5d
Michal Horejsek <michal.horejsek@proton.ch>
2024-05-30 17:04:18 +0200

    Fix loading shared links

--- applications/drive/src/app/store/_links/useLinksListing/interface.ts
+++ applications/drive/src/app/store/_links/useLinksListing/interface.ts
@@ -1,10 +1,14 @@
 import { DecryptedLink } from '../interface';
+import { FetchMeta } from './useLinksListingHelpers';
 
 type LoadLinksMetaOptions = {
     /**
      * Whether or not to request thumbnail tokens from the API
      */
     loadThumbnails?: boolean;
+    // It is importing inteface from inside the module which is ugly, but this is for quick fix.
+    // No problem with cyclic imports. If there is problem in the future, please refactor.
+    fetchMeta?: FetchMeta;
 };
 
 export type FetchLoadLinksMeta = (
