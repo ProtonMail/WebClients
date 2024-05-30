e849ca80076d65881d17b35c9db8cb80882aee3e
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-05-30 14:16:18 +0200

    Delete SubscriptionReminderMinimalCreateTime flag and adapt offer config

--- packages/components/containers/offers/operations/subscriptionReminder/useOffer.ts
+++ packages/components/containers/offers/operations/subscriptionReminder/useOffer.ts
@@ -1,5 +1,4 @@
-import { FeatureCode } from '@proton/components';
-import { useConfig, useFeature, useUser } from '@proton/components/hooks';
+import { useConfig, useUser } from '@proton/components/hooks';
 
 import useOfferFlags from '../../hooks/useOfferFlags';
 import { Operation } from '../../interface';
@@ -12,17 +11,14 @@ import getIsEligible from './eligibility';
  **/
 const useOffer = (): Operation => {
     const protonConfig = useConfig();
-    const { feature } = useFeature(FeatureCode.SubscriptionReminderMinimalCreateTime);
     const [user, loadingUser] = useUser();
     const { isActive, loading: flagsLoading } = useOfferFlags(config);
 
-    const today = new Date();
     const isLoading = flagsLoading || loadingUser;
     // We use today time if the feature flag is not set to avoid showing the value to all users or unexpected behavior
     const isEligible = getIsEligible({
         user,
         protonConfig,
-        minimalCreationTime: feature?.Value ?? today.getTime(),
     });
     const isValid = isEligible && isActive;
 
