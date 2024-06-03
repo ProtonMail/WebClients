63043364a3ccd519a31d6c80cc51922df8a1dde6
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-06-03 13:10:21 +0000

    Fix copy of the cancellation flow if policy is not enabled

--- packages/components/containers/payments/subscription/cancelSubscription/CancelSubscriptionSection.tsx
+++ packages/components/containers/payments/subscription/cancelSubscription/CancelSubscriptionSection.tsx
@@ -1,3 +1,4 @@
+import { useFlag } from '@unleash/proxy-client-react';
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms/Button';
@@ -8,6 +9,7 @@ import { useB2CCancellationFlow } from '../b2cCancellationFlow';
 import { useCancelSubscriptionFlow } from './useCancelSubscriptionFlow';
 
 export const CancelSubscriptionSection = ({ app }: { app: APP_NAMES }) => {
+    const newCancellationPolicy = useFlag('ExtendCancellationProcess');
     const { redirectToCancellationFlow, hasAccess: hasAccessToNewCancellationFlow } = useB2CCancellationFlow();
     const { loadingCancelSubscription, cancelSubscriptionModals, cancelSubscription } = useCancelSubscriptionFlow({
         app,
@@ -25,21 +27,26 @@ export const CancelSubscriptionSection = ({ app }: { app: APP_NAMES }) => {
         }
     };
 
+    const description = newCancellationPolicy
+        ? c('Info')
+              .t`When you cancel, your subscription won't be renewed, but you can still enjoy plan benefits until the end of the subscription period. After that, you will be downgraded to the ${BRAND_NAME} Free plan.`
+        : c('Info')
+              .t`This will cancel your current paid subscription and you will lose any loyalty benefits you have accumulated.`;
+
+    const cta = newCancellationPolicy ? c('Action').t`Continue` : c('Action').t`Cancel subscription`;
+
     return (
         <>
             {cancelSubscriptionModals}
             <SettingsSection>
-                <SettingsParagraph>
-                    {c('Info')
-                        .t`When you cancel, your subscription won't be renewed, but you can still enjoy plan benefits until the end of the subscription period. After that, you will be downgraded to the ${BRAND_NAME} Free plan.`}
-                </SettingsParagraph>
+                <SettingsParagraph>{description}</SettingsParagraph>
                 <Button
                     onClick={handleContinueClick}
                     data-testid="CancelSubsriptionButton"
                     shape="outline"
                     disabled={loadingCancelSubscription}
                 >
-                    {c('Action').t`Continue`}
+                    {cta}
                 </Button>
             </SettingsSection>
         </>
