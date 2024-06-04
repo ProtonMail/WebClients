8a7ef317e8c92cf69dc7680060941e4b7080c32c
Gregory Linford <gregory.linford@proton.ch>
2024-06-04 07:31:04 +0000

    [DRVDOC-314] Send errors to Sentry / no more direct console.error

--- applications/docs-editor/src/app/Utils/errorMessage.ts
+++ applications/docs-editor/src/app/Utils/errorMessage.ts
@@ -6,12 +6,14 @@ import {
 import { GenerateUUID } from '@proton/docs-core'
 import { type ErrorInfo } from 'react'
 
-export const sendErrorMessage = (error: Error, errorInfo?: ErrorInfo) => {
+export const sendErrorMessage = (error: unknown, errorInfo?: ErrorInfo) => {
+  console.error(error, errorInfo)
   if (window.parent) {
+    let errorArg: Error = error instanceof Error ? error : new Error(String(error))
     const message: EditorToClientInvokationMessage<keyof EditorRequiresClientMethods> = {
       messageId: GenerateUUID(),
       functionName: 'reportError',
-      args: [error, errorInfo],
+      args: [errorArg, errorInfo],
       type: EditorBridgeMessageType.EditorToClientInvokation,
     }
     window.parent.postMessage(message, '*')
