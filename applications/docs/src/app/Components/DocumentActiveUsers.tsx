6f9c8ce9ea2da5d553c1072b6cdde580f54a4093
Aman Harwara <aman.harwara@proton.ch>
2024-06-05 16:04:40 +0000

    [DRVDOC-387] Use same avatar component for both the header avatar display and comments

--- applications/docs/src/app/Components/DocumentActiveUsers.tsx
+++ applications/docs/src/app/Components/DocumentActiveUsers.tsx
@@ -3,9 +3,9 @@ import { UserState } from '@lexical/yjs'
 import { Tooltip } from '@proton/components'
 import clsx from '@proton/utils/clsx'
 import { useApplication } from '../Containers/ApplicationProvider'
-import { DocAwarenessEvent, DocsAwarenessStateChangeData } from '@proton/docs-shared'
+import { DocAwarenessEvent, DocsAwarenessStateChangeData, UserAvatar } from '@proton/docs-shared'
 
-export function DocumentActiveUsers() {
+export function DocumentActiveUsers({ className }: { className?: string }) {
   const application = useApplication()
   const [states, setStates] = useState<UserState[]>([])
 
@@ -15,27 +15,22 @@ export function DocumentActiveUsers() {
     }, DocAwarenessEvent.AwarenessStateChange)
   }, [application.eventBus])
 
+  if (states.length <= 1) {
+    return null
+  }
+
   return (
-    <div className="flex items-center gap-2">
+    <div className={clsx('flex items-center gap-2', className)}>
       {states.map(({ name, color, focusing }, index) => {
-        let textColor = '#000'
-        let backgroundColor = color
-        try {
-          const hue = parseInt(color.replace('hsl(', '').split(',')[0])
-          backgroundColor = `hsl(${hue}, 100%, 80%)`
-          textColor = `hsl(${hue}, 100%, 20%)`
-        } catch {}
         return (
           <Tooltip title={name} key={index}>
-            <div
-              className={clsx(
-                'flex h-8 w-8 select-none items-center justify-center rounded-lg text-center font-bold',
-                !focusing && 'opacity-50',
-              )}
-              style={{ backgroundColor, color: textColor }}
-            >
-              {name.substring(0, 1).toUpperCase()}
-            </div>
+            <UserAvatar
+              name={name}
+              className={!focusing ? 'opacity-50' : ''}
+              color={{
+                hsl: color,
+              }}
+            />
           </Tooltip>
         )
       })}
