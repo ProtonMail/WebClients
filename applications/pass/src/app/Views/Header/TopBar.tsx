ff12a33d68be8300c0016663f8493c09f5f772e5
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-06-06 10:59:59 +0200

    Fix Pass linux update bar
    [IDTEAM]

--- applications/pass/src/app/Views/Header/TopBar.tsx
+++ applications/pass/src/app/Views/Header/TopBar.tsx
@@ -8,16 +8,15 @@ type TopBarProps = {
     children: ReactNode;
     visible: boolean;
     className?: string;
-    closable?: boolean;
     onClose?: () => void;
 };
 
-export const TopBar: FC<TopBarProps> = ({ children, visible, className, closable = false, onClose }) => {
+export const TopBar: FC<TopBarProps> = ({ children, visible, className, onClose }) => {
     return (
         <div className={clsx('anime-reveal hidden md:block text-sm', !visible && 'anime-reveal--hidden', className)}>
             <div className="flex gap-2 shrink-0 flex-1 items-center px-3 py-2 pass-spotlight-content weak">
                 {children}
-                {closable && (
+                {onClose && (
                     <Button className="ml-auto" pill size="small" shape="ghost" onClick={onClose}>
                         <Icon name="cross" />
                     </Button>
