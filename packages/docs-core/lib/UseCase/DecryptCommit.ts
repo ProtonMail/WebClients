21a2d159eba1d2bddd9675aee9d68b59f0f9f330
Mo Bitar <mo.bitar@proton.ch>
2024-06-07 14:11:47 +0000

    [DRVDOC-381] feat(docs): perform signature verification on squash

--- packages/docs-core/lib/UseCase/DecryptCommit.ts
+++ packages/docs-core/lib/UseCase/DecryptCommit.ts
@@ -4,25 +4,17 @@ import { Result, UseCaseInterface } from '@standardnotes/domain-core'
 import { DocumentKeys } from '@proton/drive-store'
 import { DecryptedMessage } from '../Models/DecryptedMessage'
 import { DecryptedCommit } from '../Models/DecryptedCommit'
-import { VerifyCommit } from './VerifyCommit'
-import { InternalEventBusInterface } from '@proton/docs-shared'
-import { CommitVerificationFailedPayload, DocControllerEvent } from '../Controller/Document/DocControllerEvent'
 
 type DecryptCommitDTO = {
   commit: Commit
   commitId: string
   keys: DocumentKeys
-  verificationMode: 'deferred' | 'immediate'
 }
 
-const VERIFY_VALUE_FALSE_DUE_TO_COMMIT_HAVING_DEFERRED_VERIFICATION = false
+const VERIFY_VALUE_FALSE_DUE_TO_COMMIT_HAVING_SEPARATE_VERIFICATION = false
 
 export class DecryptCommit implements UseCaseInterface<DecryptedCommit> {
-  constructor(
-    private decryptMessage: DecryptMessage,
-    private verifyCommit: VerifyCommit,
-    private eventBus: InternalEventBusInterface,
-  ) {}
+  constructor(private decryptMessage: DecryptMessage) {}
 
   async execute(dto: DecryptCommitDTO): Promise<Result<DecryptedCommit>> {
     const updates = dto.commit.updates.documentUpdates
@@ -32,7 +24,7 @@ export class DecryptCommit implements UseCaseInterface<DecryptedCommit> {
         this.decryptMessage.execute({
           message: update,
           keys: dto.keys,
-          verify: VERIFY_VALUE_FALSE_DUE_TO_COMMIT_HAVING_DEFERRED_VERIFICATION,
+          verify: VERIFY_VALUE_FALSE_DUE_TO_COMMIT_HAVING_SEPARATE_VERIFICATION,
         }),
       ),
     )
@@ -47,45 +39,6 @@ export class DecryptCommit implements UseCaseInterface<DecryptedCommit> {
       decryptedResults.map((result) => result.getValue()),
     )
 
-    if (dto.verificationMode === 'immediate') {
-      const verificationResult = await this.verifyCommit.execute({
-        commit,
-      })
-
-      if (verificationResult.isFailed()) {
-        return Result.fail(verificationResult.getError())
-      }
-
-      const verificationValue = verificationResult.getValue()
-
-      if (!verificationValue.allVerified) {
-        return Result.fail('Failed to verify commit')
-      }
-    } else if (dto.verificationMode === 'deferred') {
-      void this.performDeferredVerificationOfInitialCommit(commit)
-    }
-
     return Result.ok(commit)
-  }
-
-  async performDeferredVerificationOfInitialCommit(commit: DecryptedCommit): Promise<void> {
-    const result = await this.verifyCommit.execute({
-      commit,
-    })
-
-    if (result.isFailed()) {
-      throw new Error('Failed to verify commit')
-    }
-
-    const resultValue = result.getValue()
-
-    if (!resultValue.allVerified) {
-      this.eventBus.publish({
-        type: DocControllerEvent.CommitVerificationFailed,
-        payload: <CommitVerificationFailedPayload>{
-          commitId: commit.commitId,
-        },
-      })
-    }
   }
 }
