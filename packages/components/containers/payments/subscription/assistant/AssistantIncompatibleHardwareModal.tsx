35ba8c0c6fe8f87bcee388c2eb8460ff5e01dceb
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-06-10 09:42:49 +0000

    fix/account-copy

--- packages/components/containers/payments/subscription/assistant/AssistantIncompatibleHardwareModal.tsx
+++ packages/components/containers/payments/subscription/assistant/AssistantIncompatibleHardwareModal.tsx
@@ -41,7 +41,7 @@ const AssistantIncompatibleHardwareModal = ({ modalProps }: Props) => {
             {...modalProps}
         >
             <span>{modalText}</span>
-            <Href className="ml-2" href={getKnowledgeBaseUrl('/proton-scribe-writing-assistant')}>
+            <Href className="ml-2" href={getKnowledgeBaseUrl('/proton-scribe-writing-assistant#local-or-server')}>
                 {c('Info').t`Learn more`}
             </Href>
         </Prompt>
