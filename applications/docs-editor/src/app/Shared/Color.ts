25c02ce934786b23340ef33a9bb8796a277dd54b
Aman Harwara <aman.harwara@proton.ch>
2024-06-10 15:48:07 +0000

    [DRVDOC-354] Docs: Added text and background color options

---
+++ applications/docs-editor/src/app/Shared/Color.ts
@@ -0,0 +1,13 @@
+const CommonColorOptions = Object.entries({
+  Grey: '92, 89, 88',
+  Purple: '109, 74, 255',
+  Orange: '230, 99, 0',
+  Yellow: '255, 153, 0',
+  Green: '30, 168, 133',
+  Blue: '35, 158, 206',
+  Red: '220, 50, 81',
+  Pink: '249, 124, 161',
+})
+
+export const PredefinedTextColorOptions = [['Default', '12, 12, 20']].concat(CommonColorOptions)
+export const PredefinedHighlightColorOptions = [['Default', '255, 255, 255']].concat(CommonColorOptions)
