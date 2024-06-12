fd2c8fbc7b0104763093896d6e5acd29bc64ac21
Aman Harwara <aman.harwara@proton.ch>
2024-06-12 09:53:06 +0000

    Initial work to make Docs UI responsive to screen size

--- applications/docs-editor/src/app/Shared/Fonts.ts
+++ applications/docs-editor/src/app/Shared/Fonts.ts
@@ -2,3 +2,5 @@ import { FONT_FACES } from '@proton/components/components/editor/constants'
 
 export const FontOptions = Object.values(FONT_FACES)
 export const DefaultFont = FONT_FACES.ARIAL
+
+export const FontSizes = [8, 10, 11, 12, 14, 16, 18, 20, 22, 30, 36, 48, 60, 72, 96]
