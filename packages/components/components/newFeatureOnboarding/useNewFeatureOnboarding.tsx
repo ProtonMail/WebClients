7f06bb7bef974fab448636c60f17e9cabc87e3a6
Ricardas Vaitkus <ricardas.vaitkus@proton.ch>
2024-06-13 10:26:38 +0300

    DRVWEB-4062 Add sharing onboarding expiration date

--- packages/components/components/newFeatureOnboarding/useNewFeatureOnboarding.tsx
+++ packages/components/components/newFeatureOnboarding/useNewFeatureOnboarding.tsx
@@ -7,18 +7,31 @@ type Props = {
     key: string;
     featureFlagsEnabled?: boolean;
     shouldWelcomeFlowBeDone?: boolean;
+    expirationDate?: string;
 };
+
+const getIsExpired = (expirationDate?: string) => {
+    if (!expirationDate) {
+        return false;
+    }
+    const expirationTimestamp = new Date(expirationDate).getTime();
+    const currentTimestamp = new Date().getTime();
+    return expirationTimestamp < currentTimestamp;
+};
+
 export default function useNewFeatureOnboarding({
     key,
     featureFlagsEnabled = true,
     shouldWelcomeFlowBeDone = true,
+    expirationDate,
 }: Props) {
     const onboardingKey = `onboarding-${key}`;
     const [wasShown, setWasShown] = useState<boolean>(Boolean(getItem(onboardingKey, 'false')));
     const [welcomeFlags] = useWelcomeFlags();
 
+    const isExpired = getIsExpired(expirationDate);
     const showOnboarding =
-        !wasShown && featureFlagsEnabled && (!shouldWelcomeFlowBeDone || !welcomeFlags.isWelcomeFlow);
+        !wasShown && !isExpired && featureFlagsEnabled && (!shouldWelcomeFlowBeDone || !welcomeFlags.isWelcomeFlow);
 
     const onWasShown = () => {
         if (!wasShown) {
