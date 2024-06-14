b04a9e81aea7c4b99c17d84d9ce9961ac011f69e
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-06-14 13:41:42 +0000

    Add bundlepro add-on

--- packages/shared/lib/helpers/planIDs.ts
+++ packages/shared/lib/helpers/planIDs.ts
@@ -97,6 +97,7 @@ export function getSupportedB2BAddons(planIDs: PlanIDs): SupportedAddons {
     if (planIDs[BUNDLE_PRO_2024]) {
         supported[ADDON_NAMES.MEMBER_BUNDLE_PRO_2024] = true;
         supported[ADDON_NAMES.DOMAIN_BUNDLE_PRO_2024] = true;
+        supported[ADDON_NAMES.MEMBER_SCRIBE_BUNDLE_PRO_2024] = true;
     }
 
     if (planIDs[ENTERPRISE]) {
