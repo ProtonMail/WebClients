42c2ee1c8de0f098b4bdf62a1b842b13c214a463
Redacted <redacted@invalid.proton.ch>
2024-06-20 18:23:52 +0200

    Count newly ack only

--- packages/metrics/lib/Counter.ts
+++ packages/metrics/lib/Counter.ts
@@ -2,9 +2,9 @@ import Metric from './Metric';
 import MetricSchema from './types/MetricSchema';
 
 class Counter<D extends MetricSchema> extends Metric<D> {
-    public increment(labels: D['Labels']) {
+    public increment(labels: D['Labels'], value: number = 1) {
         this.addToRequestQueue({
-            Value: 1,
+            Value: value,
             Labels: labels,
         } as D);
     }
