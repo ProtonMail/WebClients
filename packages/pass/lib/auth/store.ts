9223f10e6b942740a9adba3a0d08b7d63e3fac25
Edvin CANDON <edvin.candon@proton.ch>
2024-06-20 12:49:59 +0200

    Use offline verifier instead of encrypted cache key to password unlock
    - This avoids edge-case scenarios when trying to password unlock a session
      while another tab is booting and the encrypted cache key has not been
      computed yet

--- packages/pass/lib/auth/store.ts
+++ packages/pass/lib/auth/store.ts
@@ -11,18 +11,19 @@ export type AuthStore = ReturnType<typeof createAuthStore>;
 const PASS_ACCESS_TOKEN_KEY = 'pass:access_token';
 const PASS_LOCAL_ID_KEY = 'pass:local_id';
 const PASS_LOCK_LAST_EXTEND_TIME = 'pass:lock_extend_time';
+const PASS_LOCK_MODE_KEY = 'pass:lock_mode';
 const PASS_LOCK_STATE_KEY = 'pass:lock_state';
 const PASS_LOCK_TOKEN_KEY = 'pass:lock_token';
-const PASS_LOCK_MODE_KEY = 'pass:lock_mode';
 const PASS_LOCK_TTL_KEY = 'pass:lock_ttl';
-const PASS_UNLOCK_RETRY_KEY = 'pass:unlock_retry_count';
 const PASS_MAILBOX_PWD_KEY = 'pass:mailbox_pwd';
-const PASS_OFFLINE_KD_KEY = 'pass:offline_kd';
 const PASS_OFFLINE_CONFIG_KEY = 'pass:offline_config';
+const PASS_OFFLINE_KD_KEY = 'pass:offline_kd';
+const PASS_OFFLINE_VERIFIER = 'pass:offline_verifier';
 const PASS_REFRESH_TIME_KEY = 'pass:refresh_time';
 const PASS_REFRESH_TOKEN_KEY = 'pass:refresh_token';
 const PASS_SESSION_VERSION_KEY = 'pass:session_version';
 const PASS_UID_KEY = 'pass:uid';
+const PASS_UNLOCK_RETRY_KEY = 'pass:unlock_retry_count';
 const PASS_USER_ID_KEY = 'pass:user_id';
 
 export const createAuthStore = (store: Store) => {
@@ -32,7 +33,8 @@ export const createAuthStore = (store: Store) => {
         hasSession: (localID?: number) =>
             Boolean(authStore.getUID() && (localID === undefined || authStore.getLocalID() === localID)),
 
-        hasOfflinePassword: () => Boolean(authStore.getOfflineConfig() && authStore.getOfflineKD()),
+        hasOfflinePassword: () =>
+            Boolean(authStore.getOfflineConfig() && authStore.getOfflineKD() && authStore.getOfflineVerifier()),
 
         getSession: (): AuthSession => ({
             AccessToken: authStore.getAccessToken() ?? '',
@@ -42,6 +44,7 @@ export const createAuthStore = (store: Store) => {
             lockTTL: authStore.getLockTTL(),
             offlineConfig: authStore.getOfflineConfig(),
             offlineKD: authStore.getOfflineKD(),
+            offlineVerifier: authStore.getOfflineVerifier(),
             payloadVersion: authStore.getSessionVersion(),
             RefreshTime: authStore.getRefreshTime(),
             RefreshToken: authStore.getRefreshToken() ?? '',
@@ -59,12 +62,13 @@ export const createAuthStore = (store: Store) => {
             if (session.lockTTL) authStore.setLockTTL(session.lockTTL);
             if (session.offlineConfig) authStore.setOfflineConfig(session.offlineConfig);
             if (session.offlineKD) authStore.setOfflineKD(session.offlineKD);
+            if (session.offlineVerifier) authStore.setOfflineVerifier(session.offlineVerifier);
             if (session.payloadVersion !== undefined) authStore.setSessionVersion(session.payloadVersion);
             if (session.RefreshTime) authStore.setRefreshTime(session.RefreshTime);
             if (session.RefreshToken) authStore.setRefreshToken(session.RefreshToken);
             if (session.sessionLockToken) authStore.setLockToken(session.sessionLockToken);
-            if (session.unlockRetryCount !== undefined) authStore.setUnlockRetryCount(session.unlockRetryCount);
             if (session.UID) authStore.setUID(session.UID);
+            if (session.unlockRetryCount !== undefined) authStore.setUnlockRetryCount(session.unlockRetryCount);
             if (session.UserID) authStore.setUserID(session.UserID);
         },
 
@@ -88,6 +92,8 @@ export const createAuthStore = (store: Store) => {
         getOfflineKD: encodedGetter(store)(PASS_OFFLINE_KD_KEY),
         setOfflineConfig: (config: Maybe<OfflineConfig>) => store.set(PASS_OFFLINE_CONFIG_KEY, config),
         getOfflineConfig: (): Maybe<OfflineConfig> => store.get(PASS_OFFLINE_CONFIG_KEY),
+        setOfflineVerifier: encodedSetter(store)(PASS_OFFLINE_VERIFIER),
+        getOfflineVerifier: encodedGetter(store)(PASS_OFFLINE_VERIFIER),
 
         setLockMode: (mode: LockMode): void => store.set(PASS_LOCK_MODE_KEY, mode),
         getLockMode: (): LockMode => store.get(PASS_LOCK_MODE_KEY) ?? LockMode.NONE,
---
+++ packages/pass/lib/cache/crypto.spec.ts
@@ -0,0 +1,35 @@
+import { ARGON2_PARAMS } from 'pmcrypto-v6-canary/lib/constants';
+
+import { CryptoProxy } from '@proton/crypto/lib';
+import { decryptData, generateKey, getSymmetricKey } from '@proton/pass/lib/crypto/utils/crypto-helpers';
+import { releaseCryptoProxy, setupCryptoProxyForTesting } from '@proton/pass/lib/crypto/utils/testing';
+import { PassEncryptionTag } from '@proton/pass/types';
+import { stringToUint8Array, uint8ArrayToString } from '@proton/shared/lib/helpers/encoding';
+
+import { getOfflineComponents } from './crypto';
+
+describe('cache crypto operations', () => {
+    beforeAll(() => setupCryptoProxyForTesting());
+    afterAll(() => releaseCryptoProxy());
+
+    describe('getOfflineComponents', () => {
+        test('Should compute offline components correctly for a given password', async () => {
+            /** mock argon2 to avoid jest keeping a dangling worker alive */
+            const argon2 = jest.spyOn(CryptoProxy, 'computeArgon2').mockImplementation(async () => generateKey());
+
+            const randomPassword = uint8ArrayToString(generateKey());
+            const components = await getOfflineComponents(randomPassword);
+
+            expect(components.offlineConfig.salt).toBeDefined();
+            expect(components.offlineConfig.params).toStrictEqual(ARGON2_PARAMS.RECOMMENDED);
+            expect(components.offlineKD).toBeDefined();
+            expect(components.offlineVerifier).toBeDefined();
+
+            const offlineKey = await getSymmetricKey(stringToUint8Array(components.offlineKD));
+            const verifier = stringToUint8Array(components.offlineVerifier);
+            await expect((() => decryptData(offlineKey, verifier, PassEncryptionTag.Offline))()).resolves.toBeDefined();
+
+            argon2.mockRestore();
+        });
+    });
+});
