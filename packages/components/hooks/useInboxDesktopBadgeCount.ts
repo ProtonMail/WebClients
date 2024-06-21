a96313b60b2a1c579c0838adc125e84613060e59
Pablo Molina <pablo.molina@proton.ch>
2024-06-21 07:47:51 +0000

    INDA-104 & INDA-161 Fix badge count value and update speed

--- packages/components/hooks/useInboxDesktopBadgeCount.ts
+++ packages/components/hooks/useInboxDesktopBadgeCount.ts
@@ -1,12 +1,16 @@
 import { useEffect } from 'react';
 
-import { useConversationCounts } from '@proton/components/hooks';
+import { useConversationCounts, useMailSettings, useMessageCounts } from '@proton/components/hooks';
 import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
 import { invokeInboxDesktopIPC } from '@proton/shared/lib/desktop/ipcHelpers';
 import { isElectronMail } from '@proton/shared/lib/helpers/desktop';
+import { VIEW_MODE } from '@proton/shared/lib/mail/mailSettings';
 
 const useInboxDesktopBadgeCount = () => {
+    const [mailSettings] = useMailSettings();
     const [conversationCounts] = useConversationCounts();
+    const [messageCounts] = useMessageCounts();
+    const counts = mailSettings?.ViewMode === VIEW_MODE.GROUP ? conversationCounts : messageCounts;
 
     // Updates the notification badge on the desktop app icon depending on the unread count
     useEffect(() => {
@@ -14,14 +18,13 @@ const useInboxDesktopBadgeCount = () => {
             return;
         }
 
-        const inboxConvCount = conversationCounts?.find(({ LabelID }) => LabelID === MAILBOX_LABEL_IDS.INBOX);
-        if (inboxConvCount) {
-            invokeInboxDesktopIPC({
-                type: 'updateNotification',
-                payload: inboxConvCount.Unread ?? 0,
-            });
-        }
-    }, [conversationCounts]);
+        const inboxConvCount = counts?.find(({ LabelID }) => LabelID === MAILBOX_LABEL_IDS.INBOX);
+
+        invokeInboxDesktopIPC({
+            type: 'updateNotification',
+            payload: inboxConvCount?.Unread ?? 0,
+        });
+    }, [counts]);
 };
 
 export default useInboxDesktopBadgeCount;
