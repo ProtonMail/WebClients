3bbf23d244d94d7017c91710f6591c409b42234d
Pablo Molina <contact@pablomolina.me>
2024-06-24 11:38:52 +0200

    chore(INDA-187): define WindowBounds

--- applications/inbox-desktop/src/store/boundsStore.ts
+++ applications/inbox-desktop/src/store/boundsStore.ts
@@ -1,9 +1,13 @@
-import { BrowserWindow, Rectangle } from "electron";
+import { BrowserWindow } from "electron";
 import Store from "electron-store";
 import { ensureWindowIsVisible } from "../utils/view/windowBounds";
-import { updateDownloaded } from "../update";
 
-const store = new Store();
+interface WindowBounds {
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+}
 
 export const DEFAULT_WIDTH = 1024;
 export const DEFAULT_HEIGHT = 768;
@@ -13,23 +17,24 @@ export const DEFAULT_HEIGHT = 768;
 export const MINIMUM_WIDTH = 900;
 export const MINIMUM_HEIGHT = 300;
 
-export const getWindowBounds = () => {
-    // Get stored window size and position
-    const windowBounds = store.get("windowBounds", {
-        width: DEFAULT_WIDTH,
-        height: DEFAULT_HEIGHT,
-        x: undefined,
-        y: undefined,
-    }) as Rectangle;
+const DEFAULT_WINDOW_BOUNDS = {
+    width: DEFAULT_WIDTH,
+    height: DEFAULT_HEIGHT,
+    x: 0,
+    y: 0,
+} satisfies WindowBounds;
 
-    // Ensure the window is fully visible
+const store = new Store<{ windowBounds: WindowBounds }>();
+
+export const getWindowBounds = () => {
+    const windowBounds = store.get("windowBounds", DEFAULT_WINDOW_BOUNDS);
     return ensureWindowIsVisible(windowBounds);
 };
 
-export const saveWindowBounds = (window?: BrowserWindow) => {
-    if (!window || window.isFullScreen() || window.isDestroyed() || updateDownloaded) {
-        return;
-    }
+export const saveWindowBounds = (window: BrowserWindow) => {
+    const newBounds = {
+        ...window.getBounds(),
+    } satisfies WindowBounds;
 
-    store.set("windowBounds", window.getBounds());
+    store.set("windowBounds", newBounds);
 };
