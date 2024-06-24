3fd49fee9e3b0b55c094e4cbd25e944cfa8976af
Edvin CANDON <edvin.candon@proton.ch>
2024-06-24 13:37:44 +0200

    Improve `useAsyncModalHandles` hook
    - Fix initial modal state reset
    - Add key re-generation on handler calls
    
    [IDTEAM-3375]

--- packages/pass/hooks/useAsyncModalHandles.ts
+++ packages/pass/hooks/useAsyncModalHandles.ts
@@ -4,9 +4,11 @@ import type { ModalProps } from '@proton/components/components/modalTwo/Modal';
 import type { MaybePromise } from '@proton/pass/types';
 import noop from '@proton/utils/noop';
 
+import { useRerender } from './useRerender';
+
 export class AsyncModalAbortedError extends Error {}
 type ModalState<T> = T & Omit<ModalProps, 'onSubmit'> & { loading: boolean };
-type HookOptions<T> = { getInitialModalState?: () => T };
+type HookOptions<T> = { getInitialModalState: () => T };
 export type UseAsyncModalHandle<V, T> = (options: UseAsyncModalHandlerOptions<V, T>) => Promise<void>;
 
 type UseAsyncModalHandlerOptions<V, T> = Partial<T> & {
@@ -15,26 +17,26 @@ type UseAsyncModalHandlerOptions<V, T> = Partial<T> & {
     onSubmit: (value: V) => MaybePromise<unknown>;
 };
 
-export const useAsyncModalHandles = <V, T = {}>(options?: HookOptions<T>) => {
-    const getInitialState = useCallback(
-        (): ModalState<T> => ({ ...(options?.getInitialModalState?.() ?? ({} as T)), open: false, loading: false }),
-        [options?.getInitialModalState]
-    );
-    const [state, setState] = useState<ModalState<T>>(getInitialState());
+export const useAsyncModalHandles = <V, T = {}>(options: HookOptions<T>) => {
+    const [key, next] = useRerender();
+    const [state, setState] = useState<ModalState<T>>({
+        ...options.getInitialModalState(),
+        open: false,
+        loading: false,
+    });
 
     const resolver = useRef<(value: V) => void>(noop);
     const rejector = useRef<(error: unknown) => void>(noop);
     const resolve = useCallback((value: V) => resolver.current?.(value), []);
 
-    const abort = useCallback(() => {
-        rejector.current?.(new AsyncModalAbortedError());
-        setState(getInitialState());
-    }, [getInitialState]);
+    const abort = useCallback(() => rejector.current?.(new AsyncModalAbortedError()), []);
 
     const handler = useCallback<UseAsyncModalHandle<V, T>>(
         async (opts) => {
+            next();
+
             const { onSubmit, onError, onAbort, ...modalOptions } = opts;
-            setState((state) => ({ ...state, ...modalOptions, open: true, loading: false }));
+            setState({ ...options.getInitialModalState(), ...modalOptions, open: true, loading: false });
 
             try {
                 const value = await new Promise<V>((resolve, reject) => {
@@ -50,11 +52,11 @@ export const useAsyncModalHandles = <V, T = {}>(options?: HookOptions<T>) => {
                 if (error instanceof AsyncModalAbortedError) await onAbort?.();
                 else await onError?.(error);
             } finally {
-                setState({ ...getInitialState(), ...modalOptions });
+                setState((state) => ({ ...state, open: false, loading: false }));
             }
         },
-        [getInitialState]
+        [options.getInitialModalState]
     );
 
-    return useMemo(() => ({ handler, abort, state, resolver: resolve }), [handler, abort, state]);
+    return useMemo(() => ({ handler, abort, state, resolver: resolve, key }), [handler, abort, state, key]);
 };
