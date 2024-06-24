9c23cee5927ba95287ac04136e662250a4e2c257
Pablo Molina <contact@pablomolina.me>
2024-06-24 11:55:36 +0200

    feat(INDA-187): put the initial window in the center of the cursor screen

--- applications/inbox-desktop/src/store/boundsStore.ts
+++ applications/inbox-desktop/src/store/boundsStore.ts
@@ -1,4 +1,4 @@
-import { BrowserWindow } from "electron";
+import { BrowserWindow, screen } from "electron";
 import Store from "electron-store";
 import { ensureWindowIsVisible } from "../utils/view/windowBounds";
 
@@ -20,14 +20,21 @@ export const MINIMUM_HEIGHT = 300;
 const DEFAULT_WINDOW_BOUNDS = {
     width: DEFAULT_WIDTH,
     height: DEFAULT_HEIGHT,
-    x: 0,
-    y: 0,
+    x: -1,
+    y: -1,
 } satisfies WindowBounds;
 
 const store = new Store<{ windowBounds: WindowBounds }>();
 
 export const getWindowBounds = () => {
     const windowBounds = store.get("windowBounds", DEFAULT_WINDOW_BOUNDS);
+
+    if (windowBounds.x === DEFAULT_WINDOW_BOUNDS.x || windowBounds.y === DEFAULT_WINDOW_BOUNDS.y) {
+        const currentDisplay = screen.getDisplayNearestPoint(screen.getCursorScreenPoint());
+        windowBounds.x = currentDisplay.workArea.width / 2 - windowBounds.width / 2;
+        windowBounds.y = currentDisplay.workArea.height / 2 - windowBounds.height / 2;
+    }
+
     return ensureWindowIsVisible(windowBounds);
 };
 
