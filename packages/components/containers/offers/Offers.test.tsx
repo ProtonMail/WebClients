73c75ad022642b156052ced5bd00a2147f79fc73
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2024-06-25 14:50:14 +0200

    Fix tests

--- packages/components/containers/offers/Offers.test.tsx
+++ packages/components/containers/offers/Offers.test.tsx
@@ -70,7 +70,12 @@ describe('Offers', () => {
     describe('Offers display', () => {
         it('Should display upgrade button for free users', () => {
             renderWithProviders(<TopNavbarComponent />, {
-                preloadedState: { user: getModelState({ isFree: true } as any) },
+                preloadedState: {
+                    user: getModelState({ isFree: true } as any),
+                    features: {
+                        [FeatureCode.UpgradeButtonExpiration]: getModelState({ Value: 0 } as any),
+                    },
+                },
             });
 
             const link = screen.getByTestId('cta:upgrade-plan');
