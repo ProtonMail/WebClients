ef562f334b1ea334245c4ba96ff8992a9754a92a
Pablo Molina <contact@pablomolina.me>
2024-06-26 09:31:46 +0200

    feat(INDA-187): try to move the window as few as possible

--- applications/inbox-desktop/src/utils/view/windowBounds.ts
+++ applications/inbox-desktop/src/utils/view/windowBounds.ts
@@ -1,25 +1,35 @@
 import { Rectangle, screen } from "electron";
 
 export const ensureWindowIsVisible = (bounds: Rectangle) => {
-    const { width, height, x, y } = bounds;
     const nearestDisplay = screen.getDisplayMatching(bounds) || screen.getPrimaryDisplay();
     const { workArea } = nearestDisplay;
 
     // Ensure the window is not larger than the work area
-    const safeWidth = Math.min(width, workArea.width);
-    const safeHeight = Math.min(height, workArea.height);
+    const safeWidth = Math.min(bounds.width, workArea.width);
+    const safeHeight = Math.min(bounds.height, workArea.height);
 
     // Ensure the window is within the horizontal bounds of the work area
-    let safeX = x;
-    if (x < workArea.x || x > workArea.x + workArea.width) {
+    let safeX = bounds.x;
+
+    if (bounds.x < workArea.x) {
         safeX = workArea.x;
+    } else if (bounds.x + safeWidth > workArea.x + workArea.width) {
+        safeX = workArea.x + workArea.width - safeWidth;
     }
 
     // Ensure the window is within the vertical bounds of the work area
-    let safeY = y;
-    if (y < workArea.y || y > workArea.y + workArea.height) {
+    let safeY = bounds.y;
+
+    if (bounds.y < workArea.y) {
         safeY = workArea.y;
+    } else if (bounds.y + safeHeight > workArea.y + workArea.height) {
+        safeY = workArea.y + workArea.height - safeHeight;
     }
 
-    return { x: safeX, y: safeY, width: safeWidth, height: safeHeight };
+    return {
+        x: safeX,
+        y: safeY,
+        width: safeWidth,
+        height: safeHeight,
+    };
 };
