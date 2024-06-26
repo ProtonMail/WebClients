3969dacef913784a78628353db078cea83825ada
Edvin CANDON <edvin.candon@proton.ch>
2024-06-26 11:04:27 +0200

    Improve Pass policies settings view
    - Only show loading toggle for setting being mutated
    - Handle errors when requesting policies
    - Avoid re-requesting org settings on update
    
    [IDTEAM-3467]

--- packages/components/containers/organization/PassPolicies.tsx
+++ packages/components/containers/organization/PassPolicies.tsx
@@ -1,33 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { c } from 'ttag';
 
+import { CircleLoader } from '@proton/atoms/CircleLoader';
 import { Info, Toggle } from '@proton/components/components';
 import { useErrorHandler } from '@proton/components/hooks';
 import useLoading from '@proton/hooks/useLoading';
 import { usePassBridge } from '@proton/pass/lib/bridge/PassBridgeProvider';
-import { BitField, OrganizationGetResponse } from '@proton/pass/types';
-import { type OrganizationSettings } from '@proton/pass/types/data/organization';
-import { partialMerge } from '@proton/pass/utils/object/merge';
+import { BitField, type Maybe, OrganizationGetResponse } from '@proton/pass/types';
+import type { OrganizationSettings } from '@proton/pass/types/data/organization';
 import { PASS_APP_NAME } from '@proton/shared/lib/constants';
-import noop from '@proton/utils/noop';
 
 import { SettingsParagraph, SettingsSection } from '../account';
+import { GenericError } from '../error';
 
-const INITIAL_STATE: OrganizationGetResponse = {
-    CanUpdate: false,
-    Settings: {
-        ExportMode: 0,
-        ForceLockSeconds: 0,
-        ShareMode: 0,
-    },
-};
-
-const getPolicies = (): {
-    setting: keyof OrganizationSettings;
-    label: string;
-    tooltip: string;
-}[] => [
+const getPolicies = (): { setting: keyof OrganizationSettings; label: string; tooltip: string }[] => [
     {
         setting: 'ShareMode',
         label: c('Label').t`Disable sharing outside the organization`,
@@ -43,62 +30,61 @@ const getPolicies = (): {
 ];
 
 const PassPolicies = () => {
-    const {
-        organization: { get, set },
-    } = usePassBridge();
-    const [organizationSettings, setOrganizationSettings] = useState<OrganizationGetResponse>(INITIAL_STATE);
-    const [loading, withLoading] = useLoading();
+    const { organization } = usePassBridge();
+    const [loading, withLoading] = useLoading(true);
     const handleError = useErrorHandler();
 
     const policies = getPolicies();
+    const [organizationSettings, setOrganizationSettings] = useState<Maybe<OrganizationGetResponse>>();
+
+    const touched = useRef<keyof OrganizationSettings>();
+    const didLoad = useRef(false);
 
     useEffect(() => {
-        const fetchOrganizationSettings = async () => {
-            try {
-                const settings = await get();
+        const fetchOrganizationSettings = () =>
+            organization.settings.get().then((settings) => {
                 setOrganizationSettings(settings);
-            } catch (error) {
-                handleError(error);
-            }
-        };
-        withLoading(fetchOrganizationSettings()).catch(noop);
+                didLoad.current = true;
+            });
+
+        withLoading(fetchOrganizationSettings()).catch(handleError);
     }, []);
 
     const handleToggle = async (checked: boolean, setting: keyof OrganizationSettings) => {
-        try {
-            const value = checked ? BitField.ACTIVE : BitField.DISABLED;
-            await withLoading(set(setting, value));
-            setOrganizationSettings((prev) => {
-                return partialMerge(prev, { Settings: { [setting]: value } });
-            });
-        } catch (error) {
-            handleError(error);
-        }
+        touched.current = setting;
+        const value = checked ? BitField.ACTIVE : BitField.DISABLED;
+        withLoading(organization.settings.set(setting, value).then(setOrganizationSettings)).catch(handleError);
     };
 
     return (
-        <SettingsSection>
-            <SettingsParagraph>
-                {c('Info').t`You can define the policies of ${PASS_APP_NAME} for the organization members.`}
-            </SettingsParagraph>
-            <ul className="unstyled relative">
-                {policies.map(({ setting, label, tooltip }) => (
-                    <li key={setting} className="mb-4 flex items-center flex-nowrap gap-4">
-                        <Toggle
-                            checked={organizationSettings?.Settings?.[setting] === BitField.ACTIVE}
-                            id={`${setting}-toggle`}
-                            onChange={({ target }) => handleToggle(target.checked, setting)}
-                            disabled={loading || !organizationSettings.CanUpdate}
-                            loading={loading}
-                        />
-                        <label htmlFor={`${setting}-toggle`}>
-                            <span className="mr-1">{label}</span>
-                            {tooltip && <Info title={tooltip} />}
-                        </label>
-                    </li>
-                ))}
-            </ul>
-        </SettingsSection>
+        <>
+            <SettingsSection>
+                <SettingsParagraph>
+                    {c('Info').t`You can define the policies of ${PASS_APP_NAME} for the organization members.`}
+                </SettingsParagraph>
+                <ul className="unstyled relative">
+                    {organizationSettings &&
+                        policies.map(({ setting, label, tooltip }) => (
+                            <li key={setting} className="mb-4 flex items-center flex-nowrap gap-4">
+                                <Toggle
+                                    checked={organizationSettings.Settings?.[setting] === BitField.ACTIVE}
+                                    id={`${setting}-toggle`}
+                                    onChange={({ target }) => handleToggle(target.checked, setting)}
+                                    disabled={loading || !organizationSettings.CanUpdate}
+                                    loading={touched.current === setting && loading}
+                                />
+                                <label htmlFor={`${setting}-toggle`}>
+                                    <span className="mr-1">{label}</span>
+                                    {tooltip && <Info title={tooltip} />}
+                                </label>
+                            </li>
+                        ))}
+                </ul>
+            </SettingsSection>
+
+            {!didLoad.current && loading && <CircleLoader />}
+            {!loading && !organizationSettings && <GenericError className="mt-16" />}
+        </>
     );
 };
 
