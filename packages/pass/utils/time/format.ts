64f83576e1c997d7fd2445835ea7edadcaebb6a5
Edvin CANDON <edvin.candon@proton.ch>
2024-06-26 18:32:53 +0200

    Add remaining time utility
    [IDTEAM-3344]

--- packages/pass/utils/time/format.ts
+++ packages/pass/utils/time/format.ts
@@ -1,4 +1,5 @@
-import { format, formatRelative, isThisWeek } from 'date-fns';
+import { format, formatRelative, intervalToDuration, isThisWeek } from 'date-fns';
+import { c, msgid } from 'ttag';
 
 import { dateLocale } from '@proton/shared/lib/i18n';
 import capitalize from '@proton/utils/capitalize';
@@ -17,4 +18,25 @@ export const epochToRelativeDate = (epoch: number) => {
     return isThisWeek(date)
         ? `${capitalize(formatRelative(date, new Date(), { locale: dateLocale }))}`
         : epochToDateTime(epoch);
+};
+
+export const timeRemaining = (epoch: number) => {
+    const start = Date.now();
+    const end = new Date(epochToMs(epoch));
+
+    let { days = 0, hours = 0, minutes = 0 } = intervalToDuration({ start, end });
+
+    if (days) {
+        if (hours >= 23) days++;
+        return c('Label').ngettext(msgid`${days} day`, `${days} days`, days);
+    }
+
+    if (hours) {
+        if (minutes >= 59) hours++;
+        return c('Label').ngettext(msgid`${hours} hour`, `${hours} hours`, hours);
+    }
+
+    if (minutes) return c('Label').ngettext(msgid`${minutes} minute`, `${minutes} minutes`, minutes);
+
+    return c('Label').t`Expired link`;
 };
