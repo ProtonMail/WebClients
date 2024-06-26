e42aa61a63453649cadd6248407864ab748588bc
Aman Harwara <aman.harwara@proton.ch>
2024-06-26 15:18:51 +0000

    Docs: Add tests for table utils

--- applications/docs-editor/src/app/Plugins/Table/TableUtils/clearCellsInTableSelection.ts
+++ applications/docs-editor/src/app/Plugins/Table/TableUtils/clearCellsInTableSelection.ts
@@ -1,31 +1,24 @@
 import { $isTableCellNode, $isTableSelection } from '@lexical/table'
-import { $createParagraphNode, $createTextNode, $getSelection, $isElementNode, LexicalEditor } from 'lexical'
+import { $createParagraphNode, $createTextNode, $getSelection, $isElementNode } from 'lexical'
 
-export function clearCellsInTableSelection(editor: LexicalEditor) {
-  editor.update(
-    () => {
-      const selection = $getSelection()
-      if (!$isTableSelection(selection)) {
-        return
-      }
-      const cells = selection.getNodes().filter($isTableCellNode)
-      for (const cell of cells) {
-        if ($isElementNode(cell)) {
-          const paragraphNode = $createParagraphNode()
-          const textNode = $createTextNode()
-          paragraphNode.append(textNode)
-          cell.append(paragraphNode)
-          cell.getChildren().forEach((child) => {
-            if (child !== paragraphNode) {
-              child.remove()
-            }
-          })
+export function $clearCellsInTableSelection() {
+  const selection = $getSelection()
+  if (!$isTableSelection(selection)) {
+    return
+  }
+  const cells = selection.getNodes().filter($isTableCellNode)
+  for (const cell of cells) {
+    if ($isElementNode(cell)) {
+      const paragraphNode = $createParagraphNode()
+      const textNode = $createTextNode()
+      paragraphNode.append(textNode)
+      cell.append(paragraphNode)
+      cell.getChildren().forEach((child) => {
+        if (child !== paragraphNode) {
+          child.remove()
         }
-      }
-      cells[0].selectStart()
-    },
-    {
-      onUpdate: () => editor.focus(),
-    },
-  )
+      })
+    }
+  }
+  cells[0].selectStart()
 }
