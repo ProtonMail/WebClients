29345ce155cec7cfb6be0a59a38c61e9cf6ee13e
Redacted <redacted@invalid.proton.ch>
2024-06-26 15:39:27 +0000

    [DRVDOC-627] Handle ServerIsReadyToAcceptClientMessages event

--- packages/docs-shared/lib/Realtime/WebsocketConnectionInterface.ts
+++ packages/docs-shared/lib/Realtime/WebsocketConnectionInterface.ts
@@ -4,5 +4,6 @@ export interface WebsocketConnectionInterface {
   connect(): Promise<void>
   destroy(): void
   disconnect(): void
+  markAsReadyToAcceptMessages(): void
   broadcastMessage(data: Uint8Array, source: BroadcastSource): Promise<void>
 }
