1dd517e4008087eadeb36931ecbe17701d52299b
Kévin Ta <kevin.ta@proton.ch>
2024-06-26 12:28:07 +0200

    Disable more password managers in EmailOrBitcoinAddressInput

---
+++ applications/wallet/src/app/constants/attribute.ts
@@ -0,0 +1,7 @@
+export const PASSWORD_MANAGER_IGNORE_PROPS = {
+    'data-protonpass-ignore': true,
+    'data-1p-ignore': true,
+    'data-lpignore': true,
+    'data-form-type': 'other',
+    'data-bwignore': true,
+};
--- applications/wallet/src/app/constants/index.ts
+++ applications/wallet/src/app/constants/index.ts
@@ -2,3 +2,4 @@ export * from './charts';
 export * from './bitcoin';
 export * from './pagination';
 export * from './explorer';
+export * from './attribute';
