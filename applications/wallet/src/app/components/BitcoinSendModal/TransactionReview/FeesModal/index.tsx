e31e948d3077bc0f3bb65d71dc817e3de741c33b
seraph <929-seraph@users.noreply.gitlab.protontech.ch>
2024-06-26 16:05:07 +0000

    Fix transaction review text inputs

---
+++ applications/wallet/src/app/components/BitcoinSendModal/TransactionReview/FeesModal/index.tsx
@@ -0,0 +1,108 @@
+import { c } from 'ttag';
+
+import { WasmApiExchangeRate, WasmBitcoinUnit, WasmTxBuilder } from '@proton/andromeda';
+import { Icon, IconName, Tooltip } from '@proton/components/components';
+import { ModalOwnProps } from '@proton/components/components/modalTwo/Modal';
+
+import { BitcoinAmount, Modal } from '../../../../atoms';
+import { Button } from '../../../../atoms/Button';
+import { usePsbt } from '../../../../hooks/usePsbt';
+import { TxBuilderUpdater } from '../../../../hooks/useTxBuilder';
+import { useUserWalletSettings } from '../../../../store/hooks/useUserWalletSettings';
+import { isExchangeRate } from '../../../../utils';
+import { useAsyncValue } from '../../../../utils/hooks/useAsyncValue';
+import { useFeesInput } from '../useFeesInput';
+
+import './FeesModal.scss';
+
+interface Props extends ModalOwnProps {
+    unit: WasmBitcoinUnit | WasmApiExchangeRate;
+    txBuilder: WasmTxBuilder;
+    updateTxBuilder: (updater: TxBuilderUpdater) => void;
+}
+
+export const FeesModal = ({ unit, txBuilder, updateTxBuilder, ...modalProps }: Props) => {
+    const [settings] = useUserWalletSettings();
+    const { getFeesByBlockTarget } = useFeesInput(txBuilder);
+    const { createDraftPsbt } = usePsbt({ txBuilder });
+
+    const getTransactionFeesAtFeeRate = async (feeRate: number) => {
+        const updatedTxBuilder = await txBuilder.setFeeRate(BigInt(feeRate));
+        const psbt = await createDraftPsbt(updatedTxBuilder);
+        return Number(psbt?.total_fees ?? 0);
+    };
+
+    const getFeeOption = async (
+        icon: IconName,
+        text: string,
+        blockTarget: number
+    ): Promise<[IconName, string, number, number]> => {
+        const feeRate = getFeesByBlockTarget(blockTarget);
+        //  expected to be non-deterministic since input selection during PSBT creation is random
+        return [icon, text, feeRate, await getTransactionFeesAtFeeRate(feeRate)];
+    };
+
+    const feeOptions = useAsyncValue<[IconName, string, number, number][]>(
+        Promise.all([
+            getFeeOption('chevron-up', c('Wallet send').t`High priority`, 2),
+            getFeeOption('minus', c('Wallet send').t`Median priority`, 5),
+            getFeeOption('chevron-down', c('Wallet send').t`Low priority`, 10),
+        ]),
+        []
+    );
+
+    return (
+        <Modal
+            {...modalProps}
+            className="fees-selection-modal"
+            size="small"
+            header={
+                <div className="flex flex-row justify-space-between px-6 items-center my-4">
+                    <h1 className="text-lg text-semibold">{c('Wallet send').t`Network fees`}</h1>
+
+                    <Tooltip title={c('Action').t`Close`}>
+                        <Button
+                            className="shrink-0 rounded-full bg-norm"
+                            icon
+                            size="small"
+                            shape="ghost"
+                            data-testid="modal:close"
+                            onClick={() => {
+                                modalProps.onClose?.();
+                            }}
+                        >
+                            <Icon className="modal-close-icon" name="cross-big" alt={c('Action').t`Close`} />
+                        </Button>
+                    </Tooltip>
+                </div>
+            }
+        >
+            <div className="flex flex-column">
+                {feeOptions.map(([iconName, text, feeRate, feesAtFeeRate]) => (
+                    <button
+                        key={feeRate}
+                        onClick={() => {
+                            updateTxBuilder((txBuilder) => txBuilder.setFeeRate(BigInt(feeRate)));
+                            modalProps.onClose?.();
+                        }}
+                        className="fees-selection-button unstyled flex flex-row py-4 px-6 items-center"
+                    >
+                        <div className="flex items-center justify-center bg-norm rounded-full p-4">
+                            <Icon name={iconName} />
+                        </div>
+                        <div className="mx-4">{text}</div>
+                        <div className="flex flex-column items-end ml-auto">
+                            <BitcoinAmount
+                                bitcoin={feesAtFeeRate}
+                                unit={{ value: settings.BitcoinUnit }}
+                                exchangeRate={isExchangeRate(unit) ? { value: unit } : undefined}
+                                firstClassName="text-right"
+                                secondClassName="text-right"
+                            />
+                        </div>
+                    </button>
+                ))}
+            </div>
+        </Modal>
+    );
+};
--- applications/wallet/src/app/components/TransactionNoteModal/index.tsx
+++ applications/wallet/src/app/components/TransactionNoteModal/index.tsx
@@ -1,15 +1,15 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 
 import { c } from 'ttag';
 
 import { WasmApiWalletTransaction } from '@proton/andromeda';
-import { ModalPropsWithData, TextAreaTwo } from '@proton/components/components';
+import { ModalPropsWithData } from '@proton/components/components';
 import { useNotifications, useUserKeys } from '@proton/components/hooks';
 import { IWasmApiWalletData, encryptWalletDataWithWalletKey, useWalletApi } from '@proton/wallet';
 
-import { Button, Input, Modal } from '../../atoms';
 import { useBitcoinBlockchainContext } from '../../contexts';
 import { TransactionData } from '../../hooks/useWalletTransactions';
+import { TextAreaModal } from '../TextAreaModal';
 
 interface Props extends ModalPropsWithData<{ transaction: TransactionData }> {
     apiWalletData: IWasmApiWalletData;
@@ -18,80 +18,72 @@ interface Props extends ModalPropsWithData<{ transaction: TransactionData }> {
 
 export const TransactionNoteModal = ({ onUpdate, apiWalletData, data, ...modalProps }: Props) => {
     const baseLabel = data?.transaction.apiData?.Label ?? '';
-    const [label, setLabel] = useState('');
+
     const [userKeys] = useUserKeys();
     const { createNotification } = useNotifications();
     const { walletMap } = useBitcoinBlockchainContext();
     const walletApi = useWalletApi();
 
-    useEffect(() => {
-        setLabel(baseLabel);
-    }, [baseLabel]);
+    const handleSaveNote = useCallback(
+        async (label: string) => {
+            const apiData = data?.transaction?.apiData;
 
-    const handleSaveNote = useCallback(async () => {
-        const apiData = data?.transaction?.apiData;
+            if (!apiData || !userKeys) {
+                return;
+            }
 
-        if (!apiData || !userKeys) {
-            return;
-        }
+            const { WalletID, WalletAccountID, ID: TransactionID, Label } = apiData;
 
-        const { WalletID, WalletAccountID, ID: TransactionID, Label } = apiData;
-
-        // TODO: later WalletAccountID won't be nullable anymore, typeguard can be removed then
-        const account = WalletAccountID && walletMap[WalletID]?.accounts[WalletAccountID];
-
-        if (account && WalletAccountID && apiWalletData?.WalletKey?.DecryptedKey && Label !== label) {
             // TODO: later WalletAccountID won't be nullable anymore, typeguard can be removed then
+            const account = WalletAccountID && walletMap[WalletID]?.accounts[WalletAccountID];
 
-            try {
-                const [encryptedLabel] = await encryptWalletDataWithWalletKey(
-                    [label],
-                    apiWalletData.WalletKey.DecryptedKey
-                );
+            if (account && WalletAccountID && apiWalletData?.WalletKey?.DecryptedKey && Label !== label) {
+                // TODO: later WalletAccountID won't be nullable anymore, typeguard can be removed then
 
-                const { Data: updatedTx } = await walletApi
-                    .clients()
-                    .wallet.updateWalletTransactionLabel(
-                        WalletID,
-                        WalletAccountID,
-                        TransactionID,
-                        encryptedLabel ?? ''
+                try {
+                    const [encryptedLabel] = await encryptWalletDataWithWalletKey(
+                        [label],
+                        apiWalletData.WalletKey.DecryptedKey
                     );
 
-                createNotification({ text: c('Wallet Transaction').t`Transaction label successfully updated` });
-                onUpdate(updatedTx, data.transaction);
-            } catch (e) {
-                createNotification({
-                    type: 'error',
-                    text: c('Wallet Transaction').t`Could not update transaction label`,
-                });
+                    const { Data: updatedTx } = await walletApi
+                        .clients()
+                        .wallet.updateWalletTransactionLabel(
+                            WalletID,
+                            WalletAccountID,
+                            TransactionID,
+                            encryptedLabel ?? ''
+                        );
+
+                    createNotification({ text: c('Wallet Transaction').t`Transaction label successfully updated` });
+                    onUpdate(updatedTx, data.transaction);
+                } catch (e) {
+                    createNotification({
+                        type: 'error',
+                        text: c('Wallet Transaction').t`Could not update transaction label`,
+                    });
+                }
             }
-        }
-    }, [
-        createNotification,
-        data?.transaction,
-        label,
-        onUpdate,
-        userKeys,
-        apiWalletData.WalletKey?.DecryptedKey,
-        walletApi,
-        walletMap,
-    ]);
+        },
+        [
+            createNotification,
+            data?.transaction,
+            onUpdate,
+            userKeys,
+            apiWalletData.WalletKey?.DecryptedKey,
+            walletApi,
+            walletMap,
+        ]
+    );
 
     return (
-        <Modal title={c('Wallet transaction').t`Transaction note`} {...modalProps}>
-            <Input as={TextAreaTwo} rows={3} label="Note to self" value={label} onValue={(v: string) => setLabel(v)} />
-
-            <Button
-                color="norm"
-                shape="solid"
-                className="mt-6"
-                fullWidth
-                disabled={label === baseLabel || !data?.transaction?.apiData}
-                onClick={() => handleSaveNote()}
-            >
-                {c('Wallet transaction').t`Save note`}
-            </Button>
-        </Modal>
+        <TextAreaModal
+            title={c('Wallet transaction').t`Transaction note`}
+            inputLabel={c('Wallet transaction').t`Note to self`}
+            buttonText={c('Wallet transaction').t`Save note`}
+            value={baseLabel}
+            onSubmit={(value) => handleSaveNote(value)}
+            {...modalProps}
+        />
     );
 };
