ae4518dd325c6f3d45e6efdb7f3eb7a0c0d081ba
Aman Harwara <aman.harwara@proton.ch>
2024-06-26 10:05:43 +0000

    [DRVDOC-681] Make sure all icons are using latest versions from Figma spec

--- applications/docs-editor/src/app/Icons/OutdentIcon.tsx
+++ applications/docs-editor/src/app/Icons/OutdentIcon.tsx
@@ -1,6 +1,14 @@
 const OutdentIcon = ({ className }: { className?: string }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={className}>
-    <path fill="currentColor" d="M7 16V8l-4 4zm4-9h10v2H11zm0 4h10v2H11zm0 4h10v2H11zm-8 4h18v2H3zM3 3h18v2H3z" />
+  <svg viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+    <rect x="3.25" y="9.57324" width="14" height="2" rx="1" fill="currentColor" />
+    <rect x="3.25" y="15.5557" width="14" height="2" rx="1" fill="currentColor" />
+    <rect x="3.25" y="21.5381" width="14" height="2" rx="1" fill="currentColor" />
+    <rect x="3.25" y="3.55566" width="26" height="2" rx="1" fill="currentColor" />
+    <rect x="3.25" y="27.5557" width="26" height="2" rx="1" fill="currentColor" />
+    <path
+      d="M28.2383 19.7088L28.2383 13.3624C28.2383 12.8288 27.5941 12.5607 27.2155 12.9366L23.9734 16.1558C23.7346 16.3928 23.7374 16.7798 23.9796 17.0134L27.2217 20.1406C27.6027 20.5081 28.2383 20.2381 28.2383 19.7088Z"
+      fill="currentColor"
+    />
   </svg>
 )
 
