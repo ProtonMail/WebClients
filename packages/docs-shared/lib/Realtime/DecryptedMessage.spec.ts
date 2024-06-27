0c94afd3dd8a125e974c04a57aa048a64946c6b6
Mo Bitar <mo.bitar@proton.ch>
2024-06-27 16:34:45 +0000

    [DRVDOC-480] Restrict posting updates that would exceed a local or global limit

---
+++ packages/docs-shared/lib/Realtime/DecryptedMessage.spec.ts
@@ -0,0 +1,25 @@
+import { DecryptedMessage } from './DecryptedMessage'
+
+describe('DecryptedMessage', () => {
+  it('should create an instance', () => {
+    const decryptedMessage = new DecryptedMessage({
+      content: new Uint8Array([1, 2, 3]),
+      signature: new Uint8Array([4, 5, 6]),
+      authorAddress: 'authorAddress',
+      aad: 'aad',
+      timestamp: 123456,
+    })
+    expect(decryptedMessage).toBeDefined()
+  })
+
+  it('should return byte size', () => {
+    const decryptedMessage = new DecryptedMessage({
+      content: new Uint8Array([1, 2, 3]),
+      signature: new Uint8Array([4, 5, 6]),
+      authorAddress: 'authorAddress',
+      aad: 'aad',
+      timestamp: 123456,
+    })
+    expect(decryptedMessage.byteSize()).toBe(18)
+  })
+})
