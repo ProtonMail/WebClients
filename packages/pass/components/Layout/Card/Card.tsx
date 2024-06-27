ab077ab4b3aaf9f0a4610901a7f75322b527a63e
Alejandro Tidele <alejandro.tidele@proton.ch>
2024-06-27 15:56:22 +0200

    UI/UX improvements
    - Improve skeleton loaders
    - Fix button styles when success class
    - Allow cards to be clickable
    - Fixed hidden logo styles in Lobby
    
    [IDTEAM-3344]

--- packages/pass/components/Layout/Card/Card.tsx
+++ packages/pass/components/Layout/Card/Card.tsx
@@ -7,15 +7,16 @@ import { type CardType, getCardTheme } from './utils';
 
 import './Card.scss';
 
-type ItemCardProps = { className?: string; style?: CSSProperties; type: CardType };
+type ItemCardProps = { className?: string; style?: CSSProperties; type?: CardType; onClick?: () => void };
 
-export const Card: FC<PropsWithChildren<ItemCardProps>> = ({ children, className, style, type }) => (
+export const Card: FC<PropsWithChildren<ItemCardProps>> = ({ children, className, style, type, onClick }) => (
     <CoreCard
         style={style}
         rounded
         background={false}
         bordered={false}
         className={clsx(getCardTheme(type), 'pass-card', `pass-card:${type}`, className)}
+        onClick={onClick}
     >
         {children}
     </CoreCard>
