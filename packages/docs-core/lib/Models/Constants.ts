8151189f47428dfc45b02ef93dd25c7738d333d9
Mo Bitar <mo.bitar@proton.ch>
2024-06-27 16:34:45 +0000

    [DRVDOC-480] Restrict posting updates that would exceed a local or global limit

---
+++ packages/docs-core/lib/Models/Constants.ts
@@ -0,0 +1,5 @@
+import { RTSConfig } from '@proton/docs-proto'
+
+export const MAX_DOC_SIZE = 25_000_000
+
+export const MAX_UPDATE_SIZE = RTSConfig.MAX_DU_SIZE
---
+++ packages/docs-core/lib/Models/DecryptedCommit.spec.ts
@@ -0,0 +1,56 @@
+import { DecryptedMessage } from '@proton/docs-shared'
+import { DecryptedCommit } from './DecryptedCommit'
+
+describe('DecryptedCommit', () => {
+  it('should create an instance', () => {
+    const decryptedCommit = new DecryptedCommit('commitId', [
+      new DecryptedMessage({
+        content: new Uint8Array([1, 2, 3]),
+        signature: new Uint8Array([4, 5, 6]),
+        authorAddress: 'authorAddress',
+        aad: 'aad',
+        timestamp: 123456,
+      }),
+    ])
+    expect(decryptedCommit).toBeDefined()
+  })
+
+  it('should return byte size', () => {
+    const decryptedCommit = new DecryptedCommit('commitId', [
+      new DecryptedMessage({
+        content: new Uint8Array([1, 2, 3]),
+        signature: new Uint8Array([4, 5, 6]),
+        authorAddress: 'authorAddress',
+        aad: 'aad',
+        timestamp: 123456,
+      }),
+    ])
+    expect(decryptedCommit.byteSize).toBe(22)
+  })
+
+  it('should return number of updates', () => {
+    const decryptedCommit = new DecryptedCommit('commitId', [
+      new DecryptedMessage({
+        content: new Uint8Array([1, 2, 3]),
+        signature: new Uint8Array([4, 5, 6]),
+        authorAddress: 'authorAddress',
+        aad: 'aad',
+        timestamp: 123456,
+      }),
+    ])
+    expect(decryptedCommit.numberOfUpdates()).toBe(1)
+  })
+
+  it('should return needs squash', () => {
+    const decryptedCommit = new DecryptedCommit('commitId', [
+      new DecryptedMessage({
+        content: new Uint8Array([1, 2, 3]),
+        signature: new Uint8Array([4, 5, 6]),
+        authorAddress: 'authorAddress',
+        aad: 'aad',
+        timestamp: 123456,
+      }),
+    ])
+    expect(decryptedCommit.needsSquash()).toBe(false)
+  })
+})
