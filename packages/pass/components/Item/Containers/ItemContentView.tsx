149bed2e563055e0846ec714e019f35cacec42d4
Alejandro Tidele <alejandro.tidele@proton.ch>
2024-06-27 16:03:43 +0200

    Adapt ItemContent views for public secure-link mode
    [IDTEAM-3344]

---
+++ packages/pass/components/Item/Containers/ItemContentView.tsx
@@ -0,0 +1,35 @@
+import { type FC } from 'react';
+
+import { AliasContent } from '@proton/pass/components/Item/Alias/Alias.content';
+import { CreditCardContent } from '@proton/pass/components/Item/CreditCard/CreditCard.content';
+import { presentListItem } from '@proton/pass/components/Item/List/utils';
+import { LoginContent } from '@proton/pass/components/Item/Login/Login.content';
+import { NoteContent } from '@proton/pass/components/Item/Note/Note.content';
+import { Card } from '@proton/pass/components/Layout/Card/Card';
+import { itemTypeToSubThemeClassName } from '@proton/pass/components/Layout/Theme/types';
+import type { ItemContentProps } from '@proton/pass/components/Views/types';
+import type { Item, ItemType } from '@proton/pass/types';
+import capitalize from '@proton/utils/capitalize';
+import clsx from '@proton/utils/clsx';
+
+const itemContentViewMap: { [T in ItemType]: FC<ItemContentProps<T>> } = {
+    alias: AliasContent,
+    creditCard: CreditCardContent,
+    login: LoginContent,
+    note: NoteContent,
+};
+
+export const ItemContentView: FC<ItemContentProps> = ({ revision, secureLinkItem }) => {
+    const item = revision.data as Item;
+    const Component = itemContentViewMap[item.type] as FC<ItemContentProps>;
+    const { heading } = presentListItem(revision);
+
+    return (
+        <Card className="border border-norm">
+            <section className={clsx('text-left', itemTypeToSubThemeClassName[item.type])}>
+                <h3 className="text-bold mb-4">{capitalize(heading)}</h3>
+                <Component revision={revision} secureLinkItem={secureLinkItem} />
+            </section>
+        </Card>
+    );
+};
