a4a9f7d81634474db68d25ea0d911c9c025deaf6
Richard Tetaz <richard.tetaz@proton.ch>
2024-06-27 15:36:40 +0200

    Add Proton Wallet in the drawer

--- applications/mail/src/app/hooks/drawer/useMailDrawer.tsx
+++ applications/mail/src/app/hooks/drawer/useMailDrawer.tsx
@@ -2,20 +2,25 @@ import {
     CalendarDrawerAppButton,
     ContactDrawerAppButton,
     SecurityCenterDrawerAppButton,
+    WalletDrawerAppButton,
 } from '@proton/components/components';
+import { useFlag } from '@proton/components/containers';
 import { useDrawer } from '@proton/components/hooks';
 import { APPS } from '@proton/shared/lib/constants';
 import { isAppInView } from '@proton/shared/lib/drawer/helpers';
 import { DRAWER_NATIVE_APPS } from '@proton/shared/lib/drawer/interfaces';
+import isTruthy from '@proton/utils/isTruthy';
 
 const useMailDrawer = () => {
     const { appInView, showDrawerSidebar } = useDrawer();
+    const canAccessWallet = useFlag('WalletAccess'); // This includes a restriction to visionary2022 plan controlled remotely
 
     const drawerSidebarButtons = [
         <ContactDrawerAppButton aria-expanded={isAppInView(DRAWER_NATIVE_APPS.CONTACTS, appInView)} />,
         <CalendarDrawerAppButton aria-expanded={isAppInView(APPS.PROTONCALENDAR, appInView)} />,
+        canAccessWallet && <WalletDrawerAppButton />,
         <SecurityCenterDrawerAppButton aria-expanded={isAppInView(DRAWER_NATIVE_APPS.SECURITY_CENTER, appInView)} />,
-    ];
+    ].filter(isTruthy);
 
     return { drawerSidebarButtons, showDrawerSidebar };
 };
