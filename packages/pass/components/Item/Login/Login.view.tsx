e12e0b75beb4b6c58ad2a54f01aa47b60bf43bbe
Edvin CANDON <edvin.candon@proton.ch>
2024-06-28 07:51:15 +0200

    Fix duplicate translation msgid

--- packages/pass/components/Item/Login/Login.view.tsx
+++ packages/pass/components/Item/Login/Login.view.tsx
@@ -1,6 +1,6 @@
 import { type FC } from 'react';
 
-import { c, msgid } from 'ttag';
+import { c } from 'ttag';
 
 import { ItemHistoryStats } from '@proton/pass/components/Item/History/ItemHistoryStats';
 import { LoginContent } from '@proton/pass/components/Item/Login/Login.content';
@@ -10,6 +10,7 @@ import { ItemReport } from '@proton/pass/components/Monitor/Item/ItemReport';
 import { SecureLinkCardList } from '@proton/pass/components/SecureLink/SecureLinkCardList';
 import type { ItemViewProps } from '@proton/pass/components/Views/types';
 import { useFeatureFlag } from '@proton/pass/hooks/useFeatureFlag';
+import { getOccurrenceString } from '@proton/pass/lib/i18n/helpers';
 import { isMonitored } from '@proton/pass/lib/items/item.predicates';
 import { PassFeature } from '@proton/pass/types/api/features';
 
@@ -35,22 +36,9 @@ export const LoginView: FC<ItemViewProps<'login'>> = (itemViewProps) => {
 
             <MoreInfoDropdown
                 info={[
-                    {
-                        label: c('Label').t`Modified`,
-                        values: [
-                            c('Info').ngettext(msgid`${modifiedCount} time`, `${modifiedCount} times`, modifiedCount),
-                        ],
-                    },
-                    {
-                        label: c('Label').t`Item ID`,
-                        // translator: label for item identification number
-                        values: [itemId],
-                    },
-                    {
-                        label: c('Label').t`Vault ID`,
-                        // translator: label for vault identification number
-                        values: [shareId],
-                    },
+                    { label: c('Label').t`Modified`, values: [getOccurrenceString(modifiedCount)] },
+                    { label: c('Label').t`Item ID`, values: [itemId] },
+                    { label: c('Label').t`Vault ID`, values: [shareId] },
                 ]}
             />
         </ItemViewPanel>
