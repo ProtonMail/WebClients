f32d5af2d88826bf504e1b64f76ff6d1bad56672
Richard Tetaz <richard.tetaz@proton.ch>
2024-06-28 10:24:50 +0200

    Review access condition
    We target free accounts and non-multi-member organizations.

--- applications/mail/src/app/hooks/drawer/useMailDrawer.tsx
+++ applications/mail/src/app/hooks/drawer/useMailDrawer.tsx
@@ -5,7 +5,7 @@ import {
     WalletDrawerAppButton,
 } from '@proton/components/components';
 import { useFlag } from '@proton/components/containers';
-import { useDrawer } from '@proton/components/hooks';
+import { useDrawer, useOrganization, useUser } from '@proton/components/hooks';
 import { APPS } from '@proton/shared/lib/constants';
 import { isAppInView } from '@proton/shared/lib/drawer/helpers';
 import { DRAWER_NATIVE_APPS } from '@proton/shared/lib/drawer/interfaces';
@@ -13,7 +13,10 @@ import isTruthy from '@proton/utils/isTruthy';
 
 const useMailDrawer = () => {
     const { appInView, showDrawerSidebar } = useDrawer();
-    const canAccessWallet = useFlag('WalletAccess'); // This includes a restriction to visionary2022 plan controlled remotely
+    const [user] = useUser();
+    const [organization] = useOrganization();
+    // TODO: add UserSettings."WalletAccess" condition once available
+    const canAccessWallet = useFlag('WalletAccess') && (user.isFree || organization?.MaxMembers === 1);
 
     const drawerSidebarButtons = [
         <ContactDrawerAppButton aria-expanded={isAppInView(DRAWER_NATIVE_APPS.CONTACTS, appInView)} />,
