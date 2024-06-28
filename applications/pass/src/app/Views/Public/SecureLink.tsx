9d7a337339aca5831bbeb7fc1f051daea67de21a
Alejandro Tidele <alejandro.tidele@proton.ch>
2024-06-28 13:17:31 +0200

    Add new floating modal in Secure Links View
    [IDTEAM-3443]

--- applications/pass/src/app/Views/Public/SecureLink.tsx
+++ applications/pass/src/app/Views/Public/SecureLink.tsx
@@ -1,7 +1,11 @@
 import type { FC } from 'react';
 import { Suspense, lazy } from 'react';
 
+import { c } from 'ttag';
+
 import { Loader } from '@proton/components/components';
+import { UpsellFloatingModal } from '@proton/pass/components/Upsell/UpsellFloatingModal';
+import { PASS_APP_NAME } from '@proton/shared/lib/constants';
 
 import { PublicLayout } from './PublicLayout';
 
@@ -14,5 +18,11 @@ export const SecureLink: FC = () => (
         <Suspense fallback={<Loader />}>
             <SecureLinkView />
         </Suspense>
+        <UpsellFloatingModal
+            badgeText={c('Label').t`Free forever`}
+            title={c('Label').t`Open-source password manager for effortless protection.`}
+            subtitle={c('Label')
+                .t`Securely store, share and auto-login your accounts with ${PASS_APP_NAME}, using end-to-end encryption trusted by millions.`}
+        />
     </PublicLayout>
 );
