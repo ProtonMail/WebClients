997dfb3d28865a16969062772687687327f59ca9
Pablo Molina <contact@pablomolina.me>
2024-06-28 12:09:16 +0200

    chore(INDA-187): extract safeCoordinate

--- applications/inbox-desktop/src/utils/view/windowBounds.ts
+++ applications/inbox-desktop/src/utils/view/windowBounds.ts
@@ -1,29 +1,27 @@
 import { Rectangle, screen } from "electron";
 
+function safeCoordinate(
+    [workAreaPosition, workAreaSize]: [number, number],
+    [windowPosition, windowSize]: [number, number],
+) {
+    const safeSize = Math.min(workAreaSize, windowSize);
+    let safePosition = windowPosition;
+
+    if (windowPosition < workAreaPosition) {
+        safePosition = workAreaPosition;
+    } else if (windowPosition + windowSize > workAreaPosition + workAreaSize) {
+        safePosition = workAreaPosition + workAreaSize - safeSize;
+    }
+
+    return [safePosition, safeSize];
+}
+
 export const ensureWindowIsVisible = (bounds: Rectangle) => {
     const { workArea } = screen.getDisplayMatching(bounds) || screen.getPrimaryDisplay();
 
     // Ensure the window is not larger than the work area
-    const safeWidth = Math.min(bounds.width, workArea.width);
-    const safeHeight = Math.min(bounds.height, workArea.height);
-
-    // Ensure the window is within the horizontal bounds of the work area
-    let safeX = bounds.x;
-
-    if (bounds.x < workArea.x) {
-        safeX = workArea.x;
-    } else if (bounds.x + safeWidth > workArea.x + workArea.width) {
-        safeX = workArea.x + workArea.width - safeWidth;
-    }
-
-    // Ensure the window is within the vertical bounds of the work area
-    let safeY = bounds.y;
-
-    if (bounds.y < workArea.y) {
-        safeY = workArea.y;
-    } else if (bounds.y + safeHeight > workArea.y + workArea.height) {
-        safeY = workArea.y + workArea.height - safeHeight;
-    }
+    const [safeX, safeWidth] = safeCoordinate([workArea.x, workArea.width], [bounds.x, bounds.width]);
+    const [safeY, safeHeight] = safeCoordinate([workArea.y, workArea.height], [bounds.y, bounds.height]);
 
     return {
         x: safeX,
