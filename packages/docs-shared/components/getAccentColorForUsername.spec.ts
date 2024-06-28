714d4be9d85f4f4a6eb43c6c993732744a647eec
Aman Harwara <aman.harwara@proton.ch>
2024-06-28 14:24:52 +0000

    [DRVDOC-424] Use new avatar design from Figma and make sure color is chosen from accent colors

---
+++ packages/docs-shared/components/getAccentColorForUsername.spec.ts
@@ -0,0 +1,11 @@
+import { ACCENT_COLORS_IN_HSL, getAccentColorForUsername } from './getAccentColorForUsername'
+
+describe('GetAccentColorForUsername', () => {
+  it('should deterministically return a color for a given username', () => {
+    expect(getAccentColorForUsername('foo')).toBe(ACCENT_COLORS_IN_HSL[4])
+
+    expect(getAccentColorForUsername('bar')).toBe(ACCENT_COLORS_IN_HSL[9])
+
+    expect(getAccentColorForUsername('baz')).toBe(ACCENT_COLORS_IN_HSL[17])
+  })
+})
