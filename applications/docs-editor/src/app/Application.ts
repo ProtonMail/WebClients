07b56c3eb0ec9cf28d2bb97265dab8c8b9019697
Mo Bitar <mo.bitar@proton.ch>
2024-07-03 18:50:11 +0000

    [DRVDOC-878] Doc client debouncing of presence events

--- applications/docs-editor/src/app/Application.ts
+++ applications/docs-editor/src/app/Application.ts
@@ -1,8 +1,14 @@
 import { DocumentRole, DocumentRoleType, InternalEventBus } from '@proton/docs-shared'
+import { Logger, LoggerInterface } from '@proton/utils/logs'
 
 export class Application {
   public readonly eventBus = new InternalEventBus()
   private role: DocumentRole = new DocumentRole('Viewer')
+  public readonly logger: LoggerInterface
+
+  constructor() {
+    this.logger = new Logger('docs-editor')
+  }
 
   public setRole(roleType: DocumentRoleType) {
     this.role = new DocumentRole(roleType)
