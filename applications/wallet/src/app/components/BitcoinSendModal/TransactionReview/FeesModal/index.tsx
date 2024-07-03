b4c463d746a2f7094234bacb421eb05c28105eb0
seraph <929-seraph@users.noreply.gitlab.protontech.ch>
2024-07-03 11:53:22 +0000

    Fix default fees in transaction review

--- applications/wallet/src/app/components/BitcoinSendModal/TransactionReview/FeesModal/index.tsx
+++ applications/wallet/src/app/components/BitcoinSendModal/TransactionReview/FeesModal/index.tsx
@@ -1,3 +1,6 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import { noop } from 'lodash';
 import { c } from 'ttag';
 
 import { WasmApiExchangeRate, WasmBitcoinUnit, WasmTxBuilder } from '@proton/andromeda';
@@ -5,12 +8,11 @@ import { Icon, IconName, Tooltip } from '@proton/components/components';
 import { ModalOwnProps } from '@proton/components/components/modalTwo/Modal';
 
 import { BitcoinAmount, Modal } from '../../../../atoms';
-import { Button } from '../../../../atoms/Button';
+import { CoreButton } from '../../../../atoms/Button';
 import { usePsbt } from '../../../../hooks/usePsbt';
 import { TxBuilderUpdater } from '../../../../hooks/useTxBuilder';
 import { useUserWalletSettings } from '../../../../store/hooks/useUserWalletSettings';
 import { isExchangeRate } from '../../../../utils';
-import { useAsyncValue } from '../../../../utils/hooks/useAsyncValue';
 
 import './FeesModal.scss';
 
@@ -25,30 +27,37 @@ export const FeesModal = ({ unit, txBuilder, updateTxBuilder, getFeesByBlockTarg
     const [settings] = useUserWalletSettings();
     const { createDraftPsbt } = usePsbt({ txBuilder });
 
-    const getTransactionFeesAtFeeRate = async (feeRate: number) => {
-        const updatedTxBuilder = await txBuilder.setFeeRate(BigInt(feeRate));
-        const psbt = await createDraftPsbt(updatedTxBuilder);
-        return Number(psbt?.total_fees ?? 0);
-    };
+    const getTransactionFeesAtFeeRate = useCallback(
+        async (feeRate: number) => {
+            const updatedTxBuilder = await txBuilder.setFeeRate(BigInt(feeRate));
+            const psbt = await createDraftPsbt(updatedTxBuilder);
+            return Number(psbt?.total_fees ?? 0);
+        },
+        [createDraftPsbt, txBuilder]
+    );
 
-    const getFeeOption = async (
-        icon: IconName,
-        text: string,
-        blockTarget: number
-    ): Promise<[IconName, string, number, number]> => {
-        const feeRate = getFeesByBlockTarget(blockTarget) ?? 0;
-        //  expected to be non-deterministic since input selection during PSBT creation is random
-        return [icon, text, feeRate, await getTransactionFeesAtFeeRate(feeRate)];
-    };
+    const getFeeOption = useCallback(
+        async (icon: IconName, text: string, blockTarget: number): Promise<[IconName, string, number, number]> => {
+            const feeRate = getFeesByBlockTarget(blockTarget) ?? 0;
+            //  expected to be non-deterministic since input selection during PSBT creation is random
+            return [icon, text, feeRate, await getTransactionFeesAtFeeRate(feeRate)];
+        },
+        [getFeesByBlockTarget, getTransactionFeesAtFeeRate]
+    );
 
-    const feeOptions = useAsyncValue<[IconName, string, number, number][]>(
+    const [feeOptions, setFeeOptions] = useState<[IconName, string, number, number][]>([]);
+
+    useEffect(() => {
         Promise.all([
             getFeeOption('chevron-up', c('Wallet send').t`High priority`, 2),
             getFeeOption('minus', c('Wallet send').t`Median priority`, 5),
             getFeeOption('chevron-down', c('Wallet send').t`Low priority`, 10),
-        ]),
-        []
-    );
+        ])
+            .then((options) => {
+                setFeeOptions(options);
+            })
+            .catch(noop);
+    }, [getFeeOption]);
 
     return (
         <Modal
@@ -60,7 +69,7 @@ export const FeesModal = ({ unit, txBuilder, updateTxBuilder, getFeesByBlockTarg
                     <h1 className="text-lg text-semibold">{c('Wallet send').t`Network fees`}</h1>
 
                     <Tooltip title={c('Action').t`Close`}>
-                        <Button
+                        <CoreButton
                             className="shrink-0 rounded-full bg-norm"
                             icon
                             size="small"
@@ -71,7 +80,7 @@ export const FeesModal = ({ unit, txBuilder, updateTxBuilder, getFeesByBlockTarg
                             }}
                         >
                             <Icon className="modal-close-icon" name="cross-big" alt={c('Action').t`Close`} />
-                        </Button>
+                        </CoreButton>
                     </Tooltip>
                 </div>
             }
