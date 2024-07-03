1e8872572c6aebd44a8c1dbf57040d4d767e7edb
Edvin CANDON <edvin.candon@proton.ch>
2024-07-03 16:18:03 +0200

    Improve pass web service worker
    - Split sw client from worker code
    - Add retry mechanism in case of `redundant` state
    - Improve version mismatch detection and expose `updateAvailable`
    - Clean-up service worker registration sequence

--- applications/pass/src/app/ServiceWorker/types.ts
+++ applications/pass/src/app/ServiceWorker/types.ts
@@ -3,10 +3,9 @@ import type { Action } from 'redux';
 import type { LockMode } from '@proton/pass/lib/auth/lock/types';
 import type { AuthSession } from '@proton/pass/lib/auth/session';
 import type { MaybePromise } from '@proton/pass/types';
-import { logger } from '@proton/pass/utils/logger';
 
 export type ServiceWorkerMessageBase = {
-    /**  set this flag when sending out messages from clients to
+    /** set this flag when sending out messages from clients to
      * the service worker in order to broadcast the message back to
      * every claimed clients */
     broadcast?: boolean;
@@ -27,6 +26,12 @@ export type ServiceWorkerMessage = ServiceWorkerMessageBase &
         | { type: 'unauthorized' }
     );
 
+export type WithOrigin<T> = T & {
+    /** origin of the message, in the case of broadcasted messages
+     *  this will be the original `ServiceWorkerClientID` */
+    origin: string;
+};
+
 export type ServiceWorkerMessageType = ServiceWorkerMessage['type'];
 export type ServiceWorkerMessageResponseMap = {};
 
@@ -38,37 +43,3 @@ export type ServiceWorkerMessageHandler<
     T extends ServiceWorkerMessageType = ServiceWorkerMessageType,
     R = ServiceWorkerResponse<T>,
 > = (message: Extract<ServiceWorkerMessage, { type: T }>) => MaybePromise<R>;
-
-export type WithOrigin<T> = T & {
-    /** origin of the message, in the case of broadcasted messages
-     *  this will be the original `ServiceWorkerClientID` */
-    origin: string;
-};
-
-export const CLIENT_CHANNEL = new BroadcastChannel('pass::clients');
-
-export const ServiceWorkerMessageBroker = (() => {
-    const handlers: Map<ServiceWorkerMessageType, ServiceWorkerMessageHandler<any>> = new Map();
-
-    return {
-        register: <T extends ServiceWorkerMessageType>(type: T, handler: ServiceWorkerMessageHandler<T>) =>
-            handlers.set(type, handler),
-
-        onMessage: async (event: ExtendableMessageEvent) => {
-            const port = event.ports?.[0];
-            const message = event.data as WithOrigin<ServiceWorkerMessage>;
-            const handler = handlers.get(message.type);
-
-            if (message.broadcast) CLIENT_CHANNEL.postMessage(message);
-
-            if (handler) {
-                try {
-                    const response = (await handler(message)) as any;
-                    if (port && response) port.postMessage(response);
-                } catch (err) {
-                    logger.error(`[ServiceWorker] Error processing [${message.type}]`, err);
-                }
-            }
-        },
-    };
-})();
