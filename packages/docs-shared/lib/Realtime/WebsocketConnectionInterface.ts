8f3b7ed56cd74c64f477c0ed0e14d4a8fab30207
Mo Bitar <mo.bitar@proton.ch>
2024-07-04 13:02:00 +0000

    [DRVDOC-881] Client should aggressively close idle connections when user goes to a different tab

--- packages/docs-shared/lib/Realtime/WebsocketConnectionInterface.ts
+++ packages/docs-shared/lib/Realtime/WebsocketConnectionInterface.ts
@@ -3,7 +3,7 @@ import { BroadcastSource } from '../Bridge/BroadcastSource'
 export interface WebsocketConnectionInterface {
   connect(): Promise<void>
   destroy(): void
-  disconnect(): void
+  disconnect(code: number): void
   markAsReadyToAcceptMessages(): void
   broadcastMessage(data: Uint8Array, source: BroadcastSource): Promise<void>
 }
