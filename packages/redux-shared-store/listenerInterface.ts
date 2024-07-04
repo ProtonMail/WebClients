eedbc55e2a330c929ed57c80820e1752a566c6b1
Jack Crompton <jack.crompton@proton.ch>
2024-07-04 16:17:10 +0200

    Import dispatch from @proton/redux-shared-store-types

--- packages/redux-shared-store/listenerInterface.ts
+++ packages/redux-shared-store/listenerInterface.ts
@@ -1,6 +1,6 @@
 import type { TypedStartListening } from '@reduxjs/toolkit';
 
-import type { ProtonDispatch, ProtonThunkArguments } from './interface';
+import type { ProtonDispatch, ProtonThunkArguments } from '@proton/redux-shared-store-types';
 
 export type SharedStartListening<RequiredState> = TypedStartListening<
     RequiredState,
