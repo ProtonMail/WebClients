7a7275a5eebd28fadfa748b32f49d75e3896688e
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-07-04 20:29:25 +0200

    Replace babel-loader with swc-loader

---
+++ packages/pack/webpack/js.loader.swc.js
@@ -0,0 +1,56 @@
+const { excludeNodeModulesExcept, excludeFiles, createRegex } = require('./helpers/regex');
+const { BABEL_EXCLUDE_FILES, BABEL_INCLUDE_NODE_MODULES } = require('./constants');
+
+const getUnsupportedLoader = () => {
+    return {
+        loader: require.resolve('swc-loader'),
+        options: {
+            env: { coreJs: '3.22.1', mode: 'usage', targets: 'ie 11' },
+        },
+    };
+};
+
+const getSwcLoader = ({ browserslist, isProduction = false, hasReactRefresh = true } = {}) => {
+    return {
+        loader: require.resolve('swc-loader'),
+        options: {
+            env: { coreJs: '3.22.1', mode: 'usage', targets: browserslist },
+            jsc: {
+                parser: {
+                    syntax: 'typescript',
+                    tsx: true,
+                    dynamicImport: true,
+                },
+                transform: {
+                    react: {
+                        runtime: 'automatic',
+                        refresh: !isProduction && hasReactRefresh,
+                    },
+                },
+            },
+        },
+    };
+};
+
+const getJsLoader = (options) => {
+    return {
+        test: /\.js$|\.tsx?$/,
+        exclude: createRegex(
+            excludeNodeModulesExcept(BABEL_INCLUDE_NODE_MODULES),
+            excludeFiles([...BABEL_EXCLUDE_FILES, 'pre.ts', 'unsupported.ts'])
+        ),
+        use: getSwcLoader(options),
+    };
+};
+
+const getJsLoaders = (options) => {
+    return [
+        {
+            test: /(unsupported|pre)\.ts$/,
+            use: getUnsupportedLoader(options),
+        },
+        getJsLoader(options),
+    ];
+};
+
+module.exports = { getJsLoader, getJsLoaders };
