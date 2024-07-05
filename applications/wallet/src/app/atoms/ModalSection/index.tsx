9152b22067a509ffe7319a636f85f8602aaddf6c
eatencio <elena.atencio@proton.ch>
2024-07-05 12:34:04 +0200

    add shared components ModalParagraph and ModalSection

---
+++ applications/wallet/src/app/atoms/ModalSection/index.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+
+interface ModalSection {
+    header: string;
+    children: ReactNode;
+}
+export const ModalSectionHeader = ({ header, children }: ModalSection) => {
+    return (
+        <>
+            <h3 className="text-center mt-6 mb-2 text-semibold">{header}</h3>
+            <p className="mt-1 mb-6 color-weak text-center">{children}</p>
+        </>
+    );
+};
