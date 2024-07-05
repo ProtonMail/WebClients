a20899de83559bee8db90425c8733b6094827bb7
Edvin CANDON <edvin.candon@proton.ch>
2024-07-05 18:04:55 +0200

    Use event dispatcher factory for telemetry service
    [IDTEAM-3425]

--- applications/pass/src/lib/telemetry.ts
+++ applications/pass/src/lib/telemetry.ts
@@ -1,34 +1,12 @@
 import { store } from 'proton-pass-web/app/Store/store';
 
-import { type TelemetryAlarmHandles, createCoreTelemetryService } from '@proton/pass/lib/telemetry/service';
+import { createCoreTelemetryService } from '@proton/pass/lib/telemetry/service';
 import { selectTelemetryEnabled, selectUserTier } from '@proton/pass/store/selectors';
-import { epochToMs, getEpoch } from '@proton/pass/utils/time/epoch';
+import { createDispatcherAlarm } from '@proton/pass/utils/event/alarm';
 
 export const telemetry = createCoreTelemetryService({
-    alarm: ((): TelemetryAlarmHandles => {
-        type TimeoutAlarm = { timer?: NodeJS.Timeout; scheduledTime?: number };
-        const alarm: TimeoutAlarm = {};
-
-        return {
-            reset: () => {
-                clearTimeout(alarm.timer);
-                delete alarm.timer;
-                delete alarm.scheduledTime;
-            },
-            when: () => alarm?.scheduledTime,
-            set: (when, onAlarm) => {
-                /** convert the UNIX milliseconds timestamp back to
-                 * a standard timeout delay value in milliseconds */
-                const now = epochToMs(getEpoch());
-                const ms = when - now;
-
-                alarm.scheduledTime = when;
-                clearTimeout(alarm.timer);
-                alarm.timer = setTimeout(onAlarm, ms);
-            },
-        };
-    })(),
+    alarm: createDispatcherAlarm(),
+    storage: localStorage,
     getEnabled: () => selectTelemetryEnabled(store.getState()),
     getUserTier: () => selectUserTier(store.getState()),
-    storage: localStorage,
 });
