0668a26621e7a84d5fb2b28c490f1890e59a70ae
Pierre Bondoerffer <pierre.bondoerffer@proton.ch>
2024-07-05 12:39:32 +0200

    drive-docs: Make Open in Docs translation dynamic based on mimeType

--- packages/shared/lib/drive/translations.ts
+++ packages/shared/lib/drive/translations.ts
@@ -1,7 +1,23 @@
 import { c } from 'ttag';
 
+import { DOCS_SHORT_APP_NAME } from '../constants';
+import { isProtonDocument } from '../helpers/mimetype';
+
 export const getNumAccessesTooltipMessage = () =>
     c('Info').t`The download count includes both actual downloads and instances when files are previewed.`;
 export const getSizeTooltipMessage = () =>
     c('Info')
         .t`The encrypted data is slightly larger due to the overhead of the encryption and signatures, which ensure the security of your data.`;
+
+/**
+ * Returns `Open` or `Open in Docs` depending on passed `mimeType`.
+ */
+export const getOpenInDocsString = (mimeType: string = '') => {
+    if (isProtonDocument(mimeType)) {
+        // translator: 'Open' action for documents
+        return c('Action').t`Open`;
+    }
+
+    // translator: 'Open in Docs' action
+    return c('Action').t`Open in ${DOCS_SHORT_APP_NAME}`;
+};
