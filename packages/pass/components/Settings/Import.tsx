e18a97b2b74532a5a9f0a95be322a763f8391bc6
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-07-05 17:00:16 +0200

    Move import result to the top of the page
    [IDTEAM-3512]

--- packages/pass/components/Settings/Import.tsx
+++ packages/pass/components/Settings/Import.tsx
@@ -70,41 +70,6 @@ export const Import: FC = () => {
 
     return (
         <>
-            <SettingsPanel
-                title={c('Label').t`Import`}
-                subTitle={c('Info')
-                    .t`To migrate data from another password manager, go to the password manager, export your data, then upload it to ${PASS_APP_NAME}. Once your data has been imported, delete the exported file.`}
-            >
-                <FormikProvider value={form}>
-                    <Form>
-                        <ImportForm form={form} dropzone={dropzone} busy={busy} />
-                        {form.values.provider && (
-                            <Button
-                                className="w-full mt-2"
-                                type="submit"
-                                disabled={busy || !form.isValid}
-                                loading={busy}
-                                color="norm"
-                            >
-                                {busy ? c('Action').t`Importing` : c('Action').t`Import`}
-                            </Button>
-                        )}
-                    </Form>
-                </FormikProvider>
-
-                {importData !== null && (
-                    <ImportVaultsPickerModal
-                        onClose={() => beforeSubmitResolver.current?.({ ok: false })}
-                        payload={importData}
-                        onSubmit={(payload) =>
-                            beforeSubmitResolver?.current?.(
-                                payload.vaults.length === 0 ? { ok: false } : { ok: true, payload }
-                            )
-                        }
-                    />
-                )}
-            </SettingsPanel>
-
             {result && (
                 <SettingsPanel title={c('Label').t`Latest import`}>
                     <div className="flex flex-column gap-y-1 text-sm">
@@ -165,6 +130,41 @@ export const Import: FC = () => {
                     </div>
                 </SettingsPanel>
             )}
+
+            <SettingsPanel
+                title={c('Label').t`Import`}
+                subTitle={c('Info')
+                    .t`To migrate data from another password manager, go to the password manager, export your data, then upload it to ${PASS_APP_NAME}. Once your data has been imported, delete the exported file.`}
+            >
+                <FormikProvider value={form}>
+                    <Form>
+                        <ImportForm form={form} dropzone={dropzone} busy={busy} />
+                        {form.values.provider && (
+                            <Button
+                                className="w-full mt-2"
+                                type="submit"
+                                disabled={busy || !form.isValid}
+                                loading={busy}
+                                color="norm"
+                            >
+                                {busy ? c('Action').t`Importing` : c('Action').t`Import`}
+                            </Button>
+                        )}
+                    </Form>
+                </FormikProvider>
+
+                {importData !== null && (
+                    <ImportVaultsPickerModal
+                        onClose={() => beforeSubmitResolver.current?.({ ok: false })}
+                        payload={importData}
+                        onSubmit={(payload) =>
+                            beforeSubmitResolver?.current?.(
+                                payload.vaults.length === 0 ? { ok: false } : { ok: true, payload }
+                            )
+                        }
+                    />
+                )}
+            </SettingsPanel>
         </>
     );
 };
