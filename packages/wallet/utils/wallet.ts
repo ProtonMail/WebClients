6a9e2a4a60c1428b5e07ae718430896f59a6b79c
seraph <929-seraph@users.noreply.gitlab.protontech.ch>
2024-07-05 14:09:51 +0200

    Scope more i18n string

--- packages/wallet/utils/wallet.ts
+++ packages/wallet/utils/wallet.ts
@@ -24,8 +24,8 @@ export const getDefaultWalletName = (imported: boolean, wallets: IWasmApiWalletD
     const indexStr = index ? ` ${index}` : '';
 
     const name = imported
-        ? c('Wallet setup').t`My imported wallet${indexStr}`
-        : c('Wallet setup').t`My wallet${indexStr}`;
+        ? c('wallet_signup_2024:Wallet setup').t`My imported wallet${indexStr}`
+        : c('wallet_signup_2024:Wallet setup').t`My wallet${indexStr}`;
 
     if (wallets.some((wallet) => wallet.Wallet.Name === name)) {
         return getDefaultWalletName(imported, wallets, index + 1);
@@ -36,7 +36,7 @@ export const getDefaultWalletName = (imported: boolean, wallets: IWasmApiWalletD
 
 export const getDefaultWalletAccountName = (walletAccounts: WasmApiWalletAccount[], index = 0): string => {
     const indexStr = index.toString().padStart(2, '0');
-    const label = c('Wallet setup').t`My wallet account ${indexStr}`;
+    const label = c('wallet_signup_2024:Wallet setup').t`My wallet account ${indexStr}`;
 
     if (walletAccounts.some((account) => account.Label === label)) {
         return getDefaultWalletAccountName(walletAccounts, index + 1);
