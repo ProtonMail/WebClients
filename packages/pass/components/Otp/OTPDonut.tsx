ec54efe76e4b731ea2dd3f20c92c1a0bced13949
Edvin CANDON <edvin.candon@proton.ch>
2024-07-08 15:00:32 +0200

    Migrate OTPDonut to canvas element
    - Addresses rendering bottlenecks on linux desktop

--- packages/pass/components/Otp/OTPDonut.tsx
+++ packages/pass/components/Otp/OTPDonut.tsx
@@ -1,20 +1,73 @@
-import type { FC } from 'react';
+import { type FC, useEffect, useRef } from 'react';
 
-import { Donut } from '@proton/atoms/Donut';
-import { ThemeColor } from '@proton/colors/types';
 import clsx from '@proton/utils/clsx';
 
 import './OTPDonut.scss';
 
-type Props = { enabled: boolean; percent: number; period?: number };
+type CSSVarColor = `--${string}`;
 
-export const OTPDonut: FC<Props> = ({ enabled, percent, period = 0 }) => (
-    <div
-        className={clsx('pass-otp--donut pointer-events-none anime-fade-in')}
-        style={{ '--countdown-value': `"${Math.round(percent * period)}"` }}
-    >
-        {enabled && (
-            <Donut segments={[[percent * 100, ThemeColor.Success]]} backgroundSegmentColor="var(--text-hint)" />
-        )}
-    </div>
-);
+type Props = {
+    colorEmpty?: CSSVarColor;
+    colorFilled?: CSSVarColor;
+    enabled: boolean;
+    percent: number;
+    period?: number;
+    thickness?: number;
+};
+
+export const OTPDonut: FC<Props> = ({
+    colorEmpty = '--text-hint',
+    colorFilled = '--signal-success',
+    enabled,
+    percent,
+    period = 0,
+    thickness = 3,
+}) => {
+    const ref = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        const canvas = ref.current;
+        const ctx = canvas?.getContext('2d');
+
+        if (!(ctx && canvas)) return;
+
+        const size = 36;
+        const computedStyle = window.getComputedStyle(canvas);
+        const filled = computedStyle.getPropertyValue(colorFilled);
+        const empty = computedStyle.getPropertyValue(colorEmpty);
+        const dpr = window.devicePixelRatio || 1;
+
+        canvas.width = size * dpr;
+        canvas.height = size * dpr;
+        ctx.scale(dpr, dpr);
+        ctx.imageSmoothingEnabled = true;
+
+        const center = size / 2;
+        const radius = center - thickness;
+        const startAngle = -Math.PI / 2;
+        const endAngle = startAngle + (1 - percent) * (2 * Math.PI);
+
+        ctx.clearRect(0, 0, size, size);
+
+        ctx.strokeStyle = empty;
+        ctx.lineWidth = thickness;
+        ctx.beginPath();
+        ctx.arc(center, center, radius, 0, 2 * Math.PI);
+        ctx.stroke();
+
+        ctx.strokeStyle = filled;
+        ctx.lineWidth = thickness;
+        ctx.beginPath();
+        ctx.arc(center, center, radius, startAngle, endAngle);
+        ctx.stroke();
+    }, [percent, thickness, colorEmpty, colorFilled]);
+
+    return (
+        <div
+            className={clsx('pass-otp--donut pointer-events-none anime-fade-in')}
+            style={{ '--countdown-value': `"${Math.round(percent * period)}"` }}
+        >
+            {enabled && <canvas ref={ref} className="pass-otp--donut" />}
+        </div>
+    );
+};
