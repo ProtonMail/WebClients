d557d4cdc097968e3f24774bb6c123e6bf7079da
Pedro Amaro <pedro.lavarinhas.amaro@proton.ch>
2024-07-11 10:57:02 +0200

    [MSA-161] Get and render groups, view group details

---
+++ packages/atoms/Panel/Panel.tsx
@@ -0,0 +1,32 @@
+import type { FC, ReactNode } from 'react';
+
+import { CircleLoader } from '@proton/atoms/CircleLoader';
+import { Scroll } from '@proton/atoms/Scroll';
+import clsx from '@proton/utils/clsx';
+
+import './Panel.scss';
+
+type Props = {
+    header?: ReactNode;
+    footer?: ReactNode;
+    children?: ReactNode;
+    className?: string;
+    loading?: boolean;
+};
+
+export const Panel: FC<Props> = ({ header, footer, children, className, loading }) => {
+    return (
+        <section className={clsx('panel-section flex flex-column flex-nowrap', className)}>
+            {header && <div className="shrink-0 px-4 py-3">{header}</div>}
+            <div className="flex-auto h-full overflow-hidden relative">
+                {loading && <CircleLoader size="small" className="z-up absolute inset-center" />}
+                <Scroll className={clsx('absolute w-full h-full', loading && 'opacity-30 pointer-events-none')}>
+                    <div className="px-4 pt-1 pb-3">{children}</div>
+                </Scroll>
+            </div>
+            {footer && <div className="shrink-0 px-4 py-3">{footer}</div>}
+        </section>
+    );
+};
+
+export default Panel;
