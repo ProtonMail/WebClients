058ee3892e39f37552304677dc2e34099601e72e
Edvin CANDON <edvin.candon@proton.ch>
2024-07-11 20:34:32 +0200

    Move session migration QA flag to local storage

--- packages/pass/lib/auth/flags.ts
+++ packages/pass/lib/auth/flags.ts
@@ -1,7 +1,6 @@
 import { AuthMode } from '@proton/pass/types';
-import { getCookie } from '@proton/shared/lib/helpers/cookies';
 
 export const AUTH_MODE =
-    !(typeof EXTENSION_BUILD !== 'undefined' && EXTENSION_BUILD) && getCookie('ps_legacy') === undefined
+    !(typeof EXTENSION_BUILD !== 'undefined' && EXTENSION_BUILD) && !localStorage.getItem('ps_legacy')
         ? AuthMode.COOKIE
         : AuthMode.TOKEN;
