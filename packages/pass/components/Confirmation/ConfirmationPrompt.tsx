2ab783dd1e6661596dbd586b67b546ff6ac59ed9
Edvin CANDON <edvin.candon@proton.ch>
2024-07-13 10:44:14 +0200

    Decouple `ItemActionsProvider` from secure link state
    - Prefer connecting confirmation prompts to redux state
    - Add generic `ConfirmationPrompt` (TODO: use instead of `ConfirmationModal`)
    
    [IDTEAM-3564]

---
+++ packages/pass/components/Confirmation/ConfirmationPrompt.tsx
@@ -0,0 +1,36 @@
+import type { FC, ReactNode } from 'react';
+
+import { c } from 'ttag';
+
+import { Button } from '@proton/atoms/Button';
+import { Prompt } from '@proton/components/index';
+
+export type ConfirmationPromptHandles = {
+    open: boolean;
+    onCancel: () => void;
+    onConfirm: () => void;
+};
+
+type Props = ConfirmationPromptHandles & {
+    title: ReactNode;
+    message: ReactNode;
+};
+
+export const ConfirmationPrompt: FC<Props> = ({ title, message, open, onCancel, onConfirm }) => {
+    return (
+        <Prompt
+            open={open}
+            title={<span className="text-break">{title}</span>}
+            buttons={[
+                <Button onClick={onConfirm} color="norm" pill>
+                    {c('Action').t`Confirm`}
+                </Button>,
+                <Button onClick={onCancel} shape="outline" color="norm" pill>
+                    {c('Action').t`Cancel`}
+                </Button>,
+            ]}
+        >
+            {message}
+        </Prompt>
+    );
+};
