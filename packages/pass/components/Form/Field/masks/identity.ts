b1feb804a07bf7c9dca737911fb0358e642712c6
Alejandro Tidele <alejandro.tidele@proton.ch>
2024-07-15 12:54:34 +0200

    Handle custom fields addition
    [IDTEAM-3558]

---
+++ packages/pass/components/Form/Field/masks/identity.ts
@@ -0,0 +1,23 @@
+import type { FactoryOpts } from 'imask';
+import MaskedRange from 'imask/masked/range';
+
+export const birthdateMask: FactoryOpts = {
+    mask: 'DD/MM/YYYY',
+    blocks: {
+        DD: {
+            mask: MaskedRange,
+            from: 1,
+            to: 31,
+        },
+        MM: {
+            mask: MaskedRange,
+            from: 1,
+            to: 12,
+        },
+        YY: {
+            mask: MaskedRange,
+            from: 0,
+            to: 9999,
+        },
+    },
+};
