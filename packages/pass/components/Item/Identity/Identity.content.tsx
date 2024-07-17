d1fe44753a4a436d1000d29cb576fd7ebcc00beb
Alejandro Tidele <alejandro.tidele@proton.ch>
2024-07-17 23:02:24 +0200

    Identity can be edited
    [IDTEAM-3558]

--- packages/pass/components/Item/Identity/Identity.content.tsx
+++ packages/pass/components/Item/Identity/Identity.content.tsx
@@ -1,5 +1,6 @@
 import { type FC, useMemo } from 'react';
 
+import { MaskedValueControl } from '@proton/pass/components/Form/Field/Control/MaskedValueControl';
 import { ValueControl } from '@proton/pass/components/Form/Field/Control/ValueControl';
 import { FieldBox } from '@proton/pass/components/Form/Field/Layout/FieldBox';
 import { FieldsetCluster } from '@proton/pass/components/Form/Field/Layout/FieldsetCluster';
@@ -14,15 +15,24 @@ export const IdentityContent: FC<ItemContentProps<'identity'>> = ({
     const { sections, getFilteredSections } = useIdentityFormSections();
     const fieldSections = useMemo(() => getFilteredSections(content), [content, sections]);
 
-    return fieldSections.map(({ name, fields }) => (
-        <section key={name}>
+    return fieldSections.map(({ name, fields }, sectionIndex) => (
+        <section key={`${name}${sectionIndex}`}>
             <FieldBox className="color-weak my-4" unstyled>
                 {name}
             </FieldBox>
             <FieldsetCluster mode="read" as="div">
-                {fields.map((field) => (
-                    <ValueControl key={field.name} clickToCopy label={field.label} value={field.value} />
-                ))}
+                {fields.map((field, fieldIndex) => {
+                    const Component = field.mask ? MaskedValueControl : ValueControl;
+                    return (
+                        <Component
+                            key={`${field.name}${fieldIndex}`}
+                            clickToCopy
+                            label={field.label}
+                            value={field.value}
+                            mask={field.mask}
+                        />
+                    );
+                })}
             </FieldsetCluster>
         </section>
     ));
