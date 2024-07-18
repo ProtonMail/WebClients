9aa0f7b0da840c96c88277250569a1d062bacb9a
Redacted <redacted@invalid.proton.ch>
2024-07-18 08:49:30 +0000

    [DRVWEB-3964][ff:DriveShareURLBookmarksDisabled] Signup Modal on Public Share URL

--- applications/drive/src/app/hooks/drive/usePublicToken.tsx
+++ applications/drive/src/app/hooks/drive/usePublicToken.tsx
@@ -1,10 +1,12 @@
 import { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 
+import { getUrlPassword } from '../../utils/url/password';
+
 export default function usePublicToken() {
-    const { pathname, hash } = useLocation();
+    const { pathname } = useLocation();
     const token = useMemo(() => pathname.replace(/\/urls\/?/, ''), [pathname]);
-    const urlPassword = useMemo(() => hash.replace('#', ''), [hash]);
+    const urlPassword = getUrlPassword();
 
     return {
         token,
