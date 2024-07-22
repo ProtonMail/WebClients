ec8e250e6b0a0cc2391ecd87b885a9f0fa1e27b7
Edvin CANDON <edvin.candon@proton.ch>
2024-07-22 10:59:25 +0200

    Replace `matchAny` in favor of `matchEvery`

--- packages/pass/lib/search/match-any.ts
+++
@@ -1,6 +0,0 @@
-import { normalize } from '@proton/shared/lib/helpers/string';
-
-export const matchAny = (terms: string[]) => (match: string) => {
-    const normalizedSearchTerm = normalize(match, true);
-    return normalizedSearchTerm.split(' ').every((word) => terms.some((s) => normalize(s, true).includes(word)));
-};
