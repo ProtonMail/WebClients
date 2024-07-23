07c6135233b679f456bcf0d3e8ef4646c47ab0d3
Mattias Svanström <mmso@mmso.se>
2024-07-23 11:28:28 +0000

    Disable create account in appletv

--- applications/vpn-settings/src/app/containers/LoginContainer.tsx
+++ applications/vpn-settings/src/app/containers/LoginContainer.tsx
@@ -6,41 +6,41 @@ import AccountLoginContainer from 'proton-account/src/app/login/LoginContainer';
 import type { MetaTags } from 'proton-account/src/app/useMetaTags';
 
 import type { OnLoginCallback } from '@proton/components';
-import { APPS } from '@proton/shared/lib/constants';
+import { APPS, VPN_TV_PATHS_MAP } from '@proton/shared/lib/constants';
 import { isMember } from '@proton/shared/lib/user/helpers';
 
 interface Props {
     onLogin: OnLoginCallback;
     paths: Paths;
     metaTags: MetaTags;
+    initialLocation?: H.Location;
 }
 
-const LoginContainer = ({ metaTags, onLogin, paths }: Props) => {
+const LoginContainer = ({ metaTags, onLogin, paths, initialLocation }: Props) => {
     const location = useLocation<{ from?: H.Location }>();
     const searchParams = new URLSearchParams(location.search);
     const vpnTestflight = searchParams.get('redirect') === 'ios-beta';
-
-    const previousLocation = location.state?.from;
 
     return (
         <AccountLoginContainer
             testflight={vpnTestflight ? 'vpn' : undefined}
             metaTags={metaTags}
-            paths={paths}
+            // Disable create account for /appletv because it has payments
+            paths={initialLocation?.pathname === VPN_TV_PATHS_MAP.apple ? { ...paths, signup: '' } : paths}
             toApp={APPS.PROTONVPN_SETTINGS}
             productParam={APPS.PROTONVPN_SETTINGS}
             setupVPN={false}
             hasRemember={false}
-            externalRedirect={previousLocation?.pathname || ''}
+            externalRedirect={initialLocation?.pathname || ''}
             onLogin={async (data) => {
                 if (vpnTestflight) {
                     document.location.assign('https://testflight.apple.com/join/3yl2MSbw');
                     return { state: 'complete' };
                 }
                 const { User } = data;
-                const previousHash = previousLocation?.hash || '';
-                const previousSearch = previousLocation?.search || '';
-                const path = previousLocation?.pathname || (User && isMember(User) ? '/account' : '/dashboard');
+                const previousHash = initialLocation?.hash || '';
+                const previousSearch = initialLocation?.search || '';
+                const path = initialLocation?.pathname || (User && isMember(User) ? '/account' : '/dashboard');
                 const pathWithSearch = `${path}${previousSearch}${previousHash}`;
                 return onLogin({ ...data, path: pathWithSearch });
             }}
