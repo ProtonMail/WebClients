5c5b56e82e977258f71c6d23e1f6f6d05f145db1
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-07-24 14:17:21 +0200

    Use same lock values as mobile clients
    [IDTEAM-2889]

--- packages/pass/components/Lock/LockTTLField.tsx
+++ packages/pass/components/Lock/LockTTLField.tsx
@@ -12,27 +12,42 @@ type Props = {
 };
 
 const getSessionLockTTLOptions = () => [
-    { title: c('Label').t`30 seconds`, value: 30 },
     { title: c('Label').t`1 minute`, value: 60 },
+    { title: c('Label').t`2 minutes`, value: 120 },
     { title: c('Label').t`5 minutes`, value: 300 },
     { title: c('Label').t`10 minutes`, value: 600 },
-    { title: c('Label').t`15 minutes`, value: 900 },
-    { title: c('Label').t`30 minutes`, value: 1800 },
     { title: c('Label').t`1 hour`, value: 3600 },
+    { title: c('Label').t`4 hours`, value: 14400 },
 ];
 
-export const LockTTLField: FC<Props> = ({ ttl, disabled, onChange, label }) => (
-    <InputFieldTwo
-        as={SelectTwo<number>}
-        label={label}
-        disabled={disabled}
-        placeholder={c('Label').t`15 minutes`}
-        onValue={onChange}
-        value={ttl}
-        dense
-    >
-        {getSessionLockTTLOptions().map(({ title, value }) => (
-            <Option key={value} title={title} value={value} />
-        ))}
-    </InputFieldTwo>
-);
+export const LockTTLField: FC<Props> = ({ ttl, disabled, onChange, label }) => {
+    /* Legacy values selectable prior to v1.20.1 */
+    const legacyPlaceholder = (() => {
+        switch (ttl) {
+            case 30:
+                return c('Label').t`30 seconds`;
+            case 900:
+                return c('Label').t`15 minutes`;
+            case 1800:
+                return c('Label').t`30 minutes`;
+            default:
+                return undefined;
+        }
+    })();
+
+    return (
+        <InputFieldTwo
+            as={SelectTwo<number>}
+            label={label}
+            disabled={disabled}
+            placeholder={legacyPlaceholder ?? c('Label').t`10 minutes`}
+            onValue={onChange}
+            value={ttl}
+            dense
+        >
+            {getSessionLockTTLOptions().map(({ title, value }) => (
+                <Option key={value} title={title} value={value} />
+            ))}
+        </InputFieldTwo>
+    );
+};
