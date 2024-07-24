732428440ddd509ad83ad885f59437bc21583bf7
Edvin CANDON <edvin.candon@proton.ch>
2024-07-24 20:32:05 +0200

    Add unit-tests for identity hook internals
    [IDTEAM-3331]

--- packages/pass/hooks/identity/useIdentityContent.ts
+++ packages/pass/hooks/identity/useIdentityContent.ts
@@ -14,33 +14,35 @@ const presentExtraField = (field: UnsafeItemExtraField): IdentitySectionField =>
     hidden: field.type === 'hidden',
 });
 
+export const buildContentSections = (values: IdentityValues): IdentitySectionContent[] => {
+    const fields = getIdentityFields();
+    const formSections = getInitialSections(fields);
+
+    return formSections
+        .map<IdentitySectionContent>((section) => {
+            const { optionalFields = [], customFieldsKey } = section;
+
+            const fields: IdentitySectionField[] = section.fields
+                .concat(optionalFields)
+                .filter((field) => values[field.name])
+                .map<IdentitySectionField>((field) => ({
+                    ...field,
+                    label: field.label,
+                    value: values[field.name],
+                }));
+
+            const customFields = customFieldsKey ? values[customFieldsKey].map(presentExtraField) : [];
+
+            return { name: section.name, fields: fields.concat(customFields) };
+        })
+        .filter((sections) => sections.fields.length)
+        .concat(
+            values.extraSections.map<IdentitySectionContent>(({ sectionFields, sectionName }) => ({
+                name: sectionName,
+                fields: sectionFields.map(presentExtraField),
+            }))
+        );
+};
+
 export const useIdentityContent = (values: IdentityValues): IdentitySectionContent[] =>
-    useMemo(() => {
-        const fields = getIdentityFields();
-        const formSections = getInitialSections(fields);
-
-        return formSections
-            .map<IdentitySectionContent>((section) => {
-                const { optionalFields = [], customFieldsKey } = section;
-
-                const fields: IdentitySectionField[] = section.fields
-                    .concat(optionalFields)
-                    .filter((field) => values[field.name])
-                    .map<IdentitySectionField>((field) => ({
-                        ...field,
-                        label: field.label,
-                        value: values[field.name],
-                    }));
-
-                const customFields = customFieldsKey ? values[customFieldsKey].map(presentExtraField) : [];
-
-                return { name: section.name, fields: fields.concat(customFields) };
-            })
-            .filter((sections) => sections.fields.length)
-            .concat(
-                values.extraSections.map<IdentitySectionContent>(({ sectionFields, sectionName }) => ({
-                    name: sectionName,
-                    fields: sectionFields.map(presentExtraField),
-                }))
-            );
-    }, [values]);
+    useMemo(() => buildContentSections(values), [values]);
