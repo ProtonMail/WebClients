47ee153df13a44f4ae0fc4478647fb89dd71aea7
Edvin CANDON <edvin.candon@proton.ch>
2024-07-30 08:28:22 +0200

    Improve OTP rendering performance
    [IDTEAM-3663]

--- packages/pass/components/Otp/OTPDonut.tsx
+++ packages/pass/components/Otp/OTPDonut.tsx
@@ -1,4 +1,4 @@
-import { type FC, useEffect, useRef } from 'react';
+import { type FC, useCallback, useEffect, useRef } from 'react';
 
 import clsx from '@proton/utils/clsx';
 
@@ -24,43 +24,67 @@ export const OTPDonut: FC<Props> = ({
     thickness = 3,
 }) => {
     const ref = useRef<HTMLCanvasElement>(null);
+    const animationRef = useRef<number>();
+    const colorsRef = useRef<[empty: string, filled: string]>();
+
+    const getColors = useCallback(
+        (canvas: HTMLCanvasElement): [empty: string, filled: string] => {
+            if (colorsRef.current) return colorsRef.current;
+
+            const computedStyle = window.getComputedStyle(canvas);
+            const filled = computedStyle.getPropertyValue(colorFilled);
+            const empty = computedStyle.getPropertyValue(colorEmpty);
+
+            return [empty, filled];
+        },
+        [colorEmpty, colorFilled]
+    );
+
+    const draw = useCallback(
+        (percent: number) => {
+            const canvas = ref.current;
+            const ctx = canvas?.getContext('2d');
+            if (!(canvas && ctx)) return;
+
+            const size = 36;
+            const dpr = window.devicePixelRatio || 1;
+            const [empty, filled] = getColors(canvas);
+
+            canvas.width = size * dpr;
+            canvas.height = size * dpr;
+            ctx.scale(dpr, dpr);
+
+            const center = size / 2;
+            const radius = center - thickness;
+            const startAngle = -Math.PI / 2;
+            const endAngle = startAngle + (1 - percent) * (2 * Math.PI);
+
+            ctx.clearRect(0, 0, size, size);
+
+            ctx.strokeStyle = empty;
+            ctx.lineWidth = thickness;
+            ctx.beginPath();
+            ctx.arc(center, center, radius, 0, 2 * Math.PI);
+            ctx.stroke();
+
+            ctx.strokeStyle = filled;
+            ctx.lineWidth = thickness;
+            ctx.beginPath();
+            ctx.arc(center, center, radius, startAngle, endAngle);
+            ctx.stroke();
+        },
+        [thickness, getColors]
+    );
 
     useEffect(() => {
-        const canvas = ref.current;
-        const ctx = canvas?.getContext('2d');
+        cancelAnimationFrame(animationRef.current!);
+        animationRef.current = requestAnimationFrame(() => draw(percent));
+        return () => cancelAnimationFrame(animationRef.current!);
+    }, [percent]);
 
-        if (!(ctx && canvas)) return;
-
-        const size = 36;
-        const computedStyle = window.getComputedStyle(canvas);
-        const filled = computedStyle.getPropertyValue(colorFilled);
-        const empty = computedStyle.getPropertyValue(colorEmpty);
-        const dpr = window.devicePixelRatio || 1;
-
-        canvas.width = size * dpr;
-        canvas.height = size * dpr;
-        ctx.scale(dpr, dpr);
-        ctx.imageSmoothingEnabled = true;
-
-        const center = size / 2;
-        const radius = center - thickness;
-        const startAngle = -Math.PI / 2;
-        const endAngle = startAngle + (1 - percent) * (2 * Math.PI);
-
-        ctx.clearRect(0, 0, size, size);
-
-        ctx.strokeStyle = empty;
-        ctx.lineWidth = thickness;
-        ctx.beginPath();
-        ctx.arc(center, center, radius, 0, 2 * Math.PI);
-        ctx.stroke();
-
-        ctx.strokeStyle = filled;
-        ctx.lineWidth = thickness;
-        ctx.beginPath();
-        ctx.arc(center, center, radius, startAngle, endAngle);
-        ctx.stroke();
-    }, [percent, thickness, colorEmpty, colorFilled]);
+    useEffect(() => {
+        colorsRef.current = undefined;
+    }, [colorFilled, colorEmpty]);
 
     return (
         <div
