7d019653d4c599861af515fbf7ec61f10c804da1
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2024-07-31 09:41:52 +0100

    Wallet invite prompt styling

--- applications/wallet/src/app/components/InviteSentConfirmModal/index.tsx
+++ applications/wallet/src/app/components/InviteSentConfirmModal/index.tsx
@@ -6,6 +6,7 @@ import { WALLET_APP_NAME } from '@proton/shared/lib/constants';
 import walletSendingPlane from '@proton/styles/assets/img/wallet/wallet-paper-plane.png';
 
 import { Button } from '../../atoms';
+import { ModalParagraph } from '../../atoms/ModalParagraph';
 
 interface InviteSentConfirmModalOwnProps {
     email: string;
@@ -23,7 +24,7 @@ export const InviteSentConfirmModal = ({ email, ...modalProps }: Props) => {
             }
         >
             <div className="flex flex-column">
-                <div className="flex items-center flex-column">
+                <div className="flex items-center flex-column mb-4">
                     <div className="shrink-0 mb-4">
                         <img src={walletSendingPlane} alt="" />
                     </div>
@@ -33,11 +34,11 @@ export const InviteSentConfirmModal = ({ email, ...modalProps }: Props) => {
                     </div>
                 </div>
 
-                <p className="my-4 text-center color-weak">{c('Wallet invite')
-                    .t`Please let them know to check their email and follow the instruction to set up their ${WALLET_APP_NAME} with Bitcoin via Email.`}</p>
-
-                <p className="my-4 text-center color-weak">{c('Wallet invite')
-                    .t`Thank you for supporting ${WALLET_APP_NAME}!`}</p>
+                <ModalParagraph>
+                    <p>{c('Wallet invite')
+                        .t`Please let them know to check their email and follow the instruction to set up their ${WALLET_APP_NAME} with Bitcoin via Email.`}</p>
+                    <p>{c('Wallet invite').t`Thank you for supporting ${WALLET_APP_NAME}!`}</p>
+                </ModalParagraph>
             </div>
         </Prompt>
     );
