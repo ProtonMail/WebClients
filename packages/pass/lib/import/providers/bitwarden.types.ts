e7c238dc4e1c322a20fab44ed5b17cd0787086f8
Alejandro Tidele <alejandro.tidele@proton.ch>
2024-08-05 12:49:01 +0200

    Add Bitwarden identity importer
    [IDTEAM-3666]

--- packages/pass/lib/import/providers/bitwarden.types.ts
+++ packages/pass/lib/import/providers/bitwarden.types.ts
@@ -57,6 +57,25 @@ export type BitwardenCCItem = BitwardenBaseItem & {
 
 export type BitwardenIdentityItem = BitwardenBaseItem & {
     type: BitwardenType.IDENTITY;
+    identity: {
+        firstName: string;
+        middleName: string;
+        lastName: string;
+        address1: string;
+        address2: string;
+        address3: string;
+        city: string;
+        state: string;
+        postalCode: string;
+        country: string;
+        company: string;
+        email: string;
+        phone: string;
+        ssn: string;
+        username: string;
+        passportNumber: string;
+        licenseNumber: string;
+    };
 };
 
 export type BitwardenNoteItem = BitwardenBaseItem & { type: BitwardenType.NOTE };
