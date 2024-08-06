6a630b28df6c08527f8885d0746abec231f9897f
Alejandro Tidele <alejandro.tidele@proton.ch>
2024-08-06 21:56:01 +0200

    Add Dashlane ZIP identity importer
    [IDTEAM-3666]
    
    Improve code and tests
    
    [IDTEAM-3666]
    
    Improve code and tests
    
    [IDTEAM-3666]
    
    Add number as a dictionary possible key
    
    [IDTEAM-3666]

--- packages/pass/lib/import/builders/builders.types.ts
+++ packages/pass/lib/import/builders/builders.types.ts
@@ -1,3 +1,3 @@
 import type { IdentityFieldName } from '@proton/pass/hooks/identity/useIdentityForm';
 
-export type IdentityDictionary = { [key: string]: IdentityFieldName };
+export type IdentityDictionary = { [key: string | number]: IdentityFieldName };
