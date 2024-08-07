6314ccd815cc1afa09525e820ccf2979537252a4
Pedro Lavarinhas Amaro <pedro.lavarinhas.amaro@proton.ch>
2024-08-07 14:11:25 +0000

    [MSA-135] Set member permissions dropdown

--- packages/components/containers/organization/groups/GroupMemberList.tsx
+++ packages/components/containers/organization/groups/GroupMemberList.tsx
@@ -3,20 +3,21 @@ import { c, msgid } from 'ttag';
 import { Scroll } from '@proton/atoms/Scroll';
 import { Loader } from '@proton/components';
 import { useMembers } from '@proton/components/hooks';
-import type { GroupMember } from '@proton/shared/lib/interfaces';
+import type { Group, GroupMember } from '@proton/shared/lib/interfaces';
 
 import { GroupMemberItem } from './GroupMemberItem';
 
 interface Props {
     groupMembers: GroupMember[];
     loading: boolean;
+    group: Group | undefined; // needs to be removed once GroupMemberItem doesn't need it
     edit?: boolean;
 }
 
-const GroupMemberList = ({ groupMembers, loading, edit = false }: Props) => {
+const GroupMemberList = ({ groupMembers, loading, group, edit = false }: Props) => {
     const [members] = useMembers();
 
-    if (loading) {
+    if (loading || group === undefined) {
         return <Loader />;
     }
 
@@ -35,13 +36,12 @@ const GroupMemberList = ({ groupMembers, loading, edit = false }: Props) => {
             )}
             <Scroll>
                 <div className="flex flex-column gap-3">
-                    {groupMembers &&
-                        groupMembers.map((memberData: GroupMember) => {
-                            const memberName = members?.find(({ Addresses }) =>
-                                Addresses?.some(({ ID }) => ID === (memberData?.AddressID ?? memberData?.AddressId))
-                            )?.Name;
-                            return <GroupMemberItem groupMember={memberData} memberName={memberName} />;
-                        })}
+                    {groupMembers.map((memberData: GroupMember) => {
+                        const memberName = members?.find(({ Addresses }) =>
+                            Addresses?.some(({ ID }) => ID === (memberData?.AddressID ?? memberData?.AddressId))
+                        )?.Name;
+                        return <GroupMemberItem groupMember={memberData} memberName={memberName} group={group} />;
+                    })}
                 </div>
             </Scroll>
         </>
