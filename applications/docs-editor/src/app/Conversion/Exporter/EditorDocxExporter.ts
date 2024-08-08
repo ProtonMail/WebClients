bf24a77d83fa4d09aebe56ea4aac5683bb368ee0
Mo Bitar <mo.bitar@proton.ch>
2024-08-08 12:38:03 +0000

    DRVDOC-592: Duplicating a document should strip comment marks

--- applications/docs-editor/src/app/Conversion/Exporter/EditorDocxExporter.ts
+++ applications/docs-editor/src/app/Conversion/Exporter/EditorDocxExporter.ts
@@ -3,9 +3,10 @@ import { EditorExporter } from './EditorExporter'
 import { generateDocxFromEditor } from '../Docx/LexicalToDocx/GenerateDocxFromEditor'
 
 export class EditorDocxExporter extends EditorExporter {
-  async export(): Promise<Blob> {
+  async export(): Promise<Uint8Array> {
     const docx = await generateDocxFromEditor(this.editor, this.callbacks)
     const buffer = await Packer.toBlob(docx)
-    return buffer
+
+    return new Uint8Array(await buffer.arrayBuffer())
   }
 }
