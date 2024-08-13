e4971fb8150077f46988c99e7ff4934219ad3b95
Mo Bitar <mo.bitar@proton.ch>
2024-08-13 11:08:07 +0000

    DRVDOC-1027: Download remote images for PDF export

--- applications/docs-editor/src/app/Conversion/Exporter/PDFExport/PDFExport.tsx
+++ applications/docs-editor/src/app/Conversion/Exporter/PDFExport/PDFExport.tsx
@@ -5,13 +5,18 @@ import type { PDFWorker } from './PDFWorker'
 import type { PDFPageSize } from './PDFPageSize'
 import { getPDFDataNodeFromLexicalNode } from './LexicalNodeToPDFNode/getPDFDataNodeFromLexicalNode'
 import type { PDFDataNode } from './PDFDataNode'
+import type { ExporterRequiredCallbacks } from '../EditorExporter'
 
 const PDFWorkerComlink = wrap<PDFWorker>(new Worker(new URL('./PDFWorker.tsx', import.meta.url)))
 
 /**
  * @returns The PDF as an object url
  */
-export async function $generatePDFFromEditor(editor: LexicalEditor, pageSize: PDFPageSize): Promise<string> {
+export async function $generatePDFFromEditor(
+  editor: LexicalEditor,
+  pageSize: PDFPageSize,
+  callbacks: ExporterRequiredCallbacks,
+): Promise<string> {
   const state = editor.getEditorState()
   const nodes = state.read(() => {
     const root = $getRoot()
@@ -20,7 +25,7 @@ export async function $generatePDFFromEditor(editor: LexicalEditor, pageSize: PD
 
   const pdfDataNodes: PDFDataNode[] = []
   for (const node of nodes) {
-    const pdfDataNode = await getPDFDataNodeFromLexicalNode(node, state)
+    const pdfDataNode = await getPDFDataNodeFromLexicalNode(node, state, callbacks)
     if (pdfDataNode) {
       pdfDataNodes.push(pdfDataNode)
     }
