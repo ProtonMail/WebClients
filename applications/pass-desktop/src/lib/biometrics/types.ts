f285cbcf0d158a59ca58c6b70207985e85f7f9d8
Edvin CANDON <edvin.candon@proton.ch>
2024-08-13 16:26:23 +0200

    Clean-up pass desktop
    - Remove circular dependencies

--- applications/pass-desktop/src/lib/biometrics/types.ts
+++ applications/pass-desktop/src/lib/biometrics/types.ts
@@ -1,3 +1,9 @@
+import { type BrowserWindow } from 'electron';
+
+import type { MaybeNull } from '@proton/pass/types';
+
+export type BiometricsFactory = (getWindow: () => MaybeNull<BrowserWindow>) => BiometricsPlatformHandler;
+
 export type BiometricsPlatformHandler = {
     canCheckPresence: () => Promise<boolean>;
     checkPresence: (e: Electron.IpcMainInvokeEvent, reason: string) => Promise<boolean>;
