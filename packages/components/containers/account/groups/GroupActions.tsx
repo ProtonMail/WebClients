ec6b3cc8e30dc612c3acadf714a406c5a33127a7
Elena Atencio <elena.atencio@proton.ch>
2024-08-14 15:26:26 +0000

    Disable leave button for non-private group members

--- packages/components/containers/account/groups/GroupActions.tsx
+++ packages/components/containers/account/groups/GroupActions.tsx
@@ -1,18 +1,32 @@
 import { c } from 'ttag';
 
+import { Button } from '@proton/atoms/Button';
+import { Tooltip } from '@proton/components/components';
 import useEventManager from '@proton/components/hooks/useEventManager';
-import type { GroupMembership } from '@proton/shared/lib/interfaces';
+import { GROUP_MEMBERSHIP_STATUS, type GroupMembership } from '@proton/shared/lib/interfaces';
 import isTruthy from '@proton/utils/isTruthy';
 
 import DropdownActions from '../../../components/dropdown/DropdownActions';
 import useGroupActions from './useGroupActions';
 
-const GroupActions = ({ membership }: { membership: GroupMembership }) => {
+const GroupActions = ({ membership, isPrivateUser }: { membership: GroupMembership; isPrivateUser: boolean }) => {
     const { acceptInvitation, declineInvitation, leaveMembership } = useGroupActions();
     const { call } = useEventManager();
 
+    if (!isPrivateUser && membership.Status === GROUP_MEMBERSHIP_STATUS.ACTIVE) {
+        return (
+            <Tooltip title={c('Info').t`You do not have permission to perform this action`}>
+                <div className="inline-block">
+                    <Button size="small" disabled={true}>
+                        {c('Action').t`Leave`}
+                    </Button>
+                </div>
+            </Tooltip>
+        );
+    }
+
     const list = [
-        membership.Status === 'active' && {
+        membership.Status === GROUP_MEMBERSHIP_STATUS.ACTIVE && {
             text: c('Action').t`Leave`,
             'data-testid': 'leaveGroup',
             onClick: async () => {
@@ -20,7 +34,7 @@ const GroupActions = ({ membership }: { membership: GroupMembership }) => {
                 await call();
             },
         }, // accept group
-        membership.Status === 'unanswered' && {
+        membership.Status === GROUP_MEMBERSHIP_STATUS.UNANSWERED && {
             text: c('Action').t`Accept`,
             'data-testid': 'acceptGroup',
             onClick: async () => {
@@ -28,7 +42,7 @@ const GroupActions = ({ membership }: { membership: GroupMembership }) => {
                 await call();
             },
         }, // decline group
-        membership.Status === 'unanswered' && {
+        membership.Status === GROUP_MEMBERSHIP_STATUS.UNANSWERED && {
             text: c('Action').t`Decline`,
             'data-testid': 'declineGroup',
             onClick: async () => {
