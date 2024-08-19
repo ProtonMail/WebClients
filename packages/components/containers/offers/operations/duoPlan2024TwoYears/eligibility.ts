7e8cc8dbbfa334773e7854eb5c2cbf539de8fc99
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2024-08-19 10:03:17 +0000

    Merge branch 'duo-hotfix' into 'main'
    Remove storage and 6 months conditions
    
    See merge request web/clients!12351

--- packages/components/containers/offers/operations/duoPlan2024TwoYears/eligibility.ts
+++ packages/components/containers/offers/operations/duoPlan2024TwoYears/eligibility.ts
@@ -1,11 +1,7 @@
-import { differenceInMonths, fromUnixTime } from 'date-fns';
-
 import { getAppFromPathnameSafe } from '@proton/shared/lib/apps/slugHelper';
 import { APPS } from '@proton/shared/lib/constants';
 import { hasBundle, hasTwoYears } from '@proton/shared/lib/helpers/subscription';
 import type { ProtonConfig, Subscription, UserModel } from '@proton/shared/lib/interfaces';
-import { getAppSpace, getSpace } from '@proton/shared/lib/user/storage';
-import percentage from '@proton/utils/percentage';
 
 interface Props {
     subscription?: Subscription;
@@ -18,11 +14,6 @@ const isEligible = ({ subscription, user, protonConfig }: Props) => {
     const hasUnlimited = hasBundle(subscription);
     const hasTwoYear = hasTwoYears(subscription);
 
-    const { maxSpace, usedSpace } = getAppSpace(getSpace(user), APPS.PROTONMAIL);
-    const isUsingMoreThan80PercentStorage = percentage(maxSpace, usedSpace) > 80;
-
-    const moreThan6Monthsubscription = differenceInMonths(Date.now(), fromUnixTime(subscription?.CreateTime || 0)) >= 6;
-
     // check storage and duration
     const hasValidApp =
         (protonConfig.APP_NAME === APPS.PROTONACCOUNT && parentApp === APPS.PROTONMAIL) ||
@@ -32,14 +23,7 @@ const isEligible = ({ subscription, user, protonConfig }: Props) => {
     const { canPay, isDelinquent } = user;
     const notDelinquent = !isDelinquent;
 
-    return (
-        hasValidApp &&
-        canPay &&
-        notDelinquent &&
-        hasUnlimited &&
-        hasTwoYear &&
-        (isUsingMoreThan80PercentStorage || moreThan6Monthsubscription)
-    );
+    return hasValidApp && canPay && notDelinquent && hasUnlimited && hasTwoYear;
 };
 
 export default isEligible;
