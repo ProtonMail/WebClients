1feba3858e035aaab34e793ed826b4a0db294bdc
Richard Tetaz <richard.tetaz@proton.ch>
2024-08-19 10:03:17 +0000

    Remove storage and 6 months conditions

--- packages/components/containers/offers/operations/duoPlan2024Yearly/eligibility.ts
+++ packages/components/containers/offers/operations/duoPlan2024Yearly/eligibility.ts
@@ -1,11 +1,7 @@
-import { differenceInMonths, fromUnixTime } from 'date-fns';
-
 import { getAppFromPathnameSafe } from '@proton/shared/lib/apps/slugHelper';
 import { APPS } from '@proton/shared/lib/constants';
 import { hasBundle, hasMonthly, hasYearly } from '@proton/shared/lib/helpers/subscription';
 import type { ProtonConfig, Subscription, UserModel } from '@proton/shared/lib/interfaces';
-import { getAppSpace, getSpace } from '@proton/shared/lib/user/storage';
-import percentage from '@proton/utils/percentage';
 
 interface Props {
     subscription?: Subscription;
@@ -17,12 +13,6 @@ const isEligible = ({ subscription, user, protonConfig }: Props) => {
     const parentApp = getAppFromPathnameSafe(window.location.pathname);
     const hasUnlimited = hasBundle(subscription);
     const hasOneYear = hasYearly(subscription) || hasMonthly(subscription); // 1 month also goes to 1 year, but not for 2 years
-
-    const { maxSpace, usedSpace } = getAppSpace(getSpace(user), APPS.PROTONMAIL);
-    const isUsingMoreThan80PercentStorage = percentage(maxSpace, usedSpace) > 80;
-
-    const moreThan6Monthsubscription = differenceInMonths(Date.now(), fromUnixTime(subscription?.CreateTime || 0)) >= 6;
-
     const hasValidApp =
         (protonConfig.APP_NAME === APPS.PROTONACCOUNT && parentApp === APPS.PROTONMAIL) ||
         (protonConfig.APP_NAME === APPS.PROTONACCOUNT && parentApp === APPS.PROTONCALENDAR) ||
@@ -31,14 +21,7 @@ const isEligible = ({ subscription, user, protonConfig }: Props) => {
     const { canPay, isDelinquent } = user;
     const notDelinquent = !isDelinquent;
 
-    return (
-        hasValidApp &&
-        canPay &&
-        notDelinquent &&
-        hasUnlimited &&
-        hasOneYear &&
-        (isUsingMoreThan80PercentStorage || moreThan6Monthsubscription)
-    );
+    return hasValidApp && canPay && notDelinquent && hasUnlimited && hasOneYear;
 };
 
 export default isEligible;
