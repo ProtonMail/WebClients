4b67275ddad234a527f96317ca1c4ce944e0ab50
Pierre Bondoerffer <pierre.bondoerffer@proton.ch>
2024-08-22 10:20:34 +0000

    Implement Drive Lite plan

--- packages/components/hooks/drive/useDrivePlan.ts
+++ packages/components/hooks/drive/useDrivePlan.ts
@@ -11,14 +11,20 @@ export const useDrivePlan = () => {
     const [organization] = useOrganization();
     const plan = organization?.PlanName || PLANS.FREE;
 
+    // B2B plans
     // Note: Enterprise plan is currently *NOT* implemented anywhere,
     //       so it should not be handled.
-
     const isDriveProfessional = plan === PLANS.DRIVE_BUSINESS;
     const isProtonBusinessSuite = plan === PLANS.BUNDLE_PRO || plan === PLANS.BUNDLE_PRO_2024;
 
     const isB2B = isDriveProfessional || isProtonBusinessSuite;
 
+    // B2C plans
+    const isDriveLite = plan === PLANS.DRIVE_LITE;
+
+    const isB2C = hasPaidDrive && !isB2B;
+
+    // Upsells
     const canUpsellFree = !hasPaidDrive;
     const canUpsellB2B = isB2B && !isProtonBusinessSuite;
 
@@ -45,11 +51,22 @@ export const useDrivePlan = () => {
          */
         isB2B,
 
+        /**
+         * `true` if the user is on a Drive B2C plan.
+         *
+         * For Drive, only Drive-specific and ecosystem B2C plans are considered. \
+         * i.e. Mail Plus is not be considered B2C in a Drive context.
+         */
+        isB2C,
+
         /** `true` is plan is *Drive Professional*. */
         isDriveProfessional,
 
         /** `true` if plan is *Proton Business Suite*. */
         isProtonBusinessSuite,
+
+        /** `true` if plan is *Drive Lite*. */
+        isDriveLite,
 
         /** `true` if a B2C upsell is available. */
         canUpsellFree,
