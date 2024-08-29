188e721d582a0929aad264624f68e09cb58596f5
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-08-29 10:02:30 +0200

    Allow toggling alias status from alias views
    [IDTEAM-3568]

--- packages/pass/components/Item/Alias/Alias.content.tsx
+++ packages/pass/components/Item/Alias/Alias.content.tsx
@@ -4,15 +4,20 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { c } from 'ttag';
 
+import { Toggle, Tooltip } from '@proton/components/index';
 import { ValueControl } from '@proton/pass/components/Form/Field/Control/ValueControl';
 import { FieldsetCluster } from '@proton/pass/components/Form/Field/Layout/FieldsetCluster';
 import { TextAreaReadonly } from '@proton/pass/components/Form/legacy/TextAreaReadonly';
 import type { ItemContentProps } from '@proton/pass/components/Views/types';
-import { useActionRequest } from '@proton/pass/hooks/useActionRequest';
+import { useActionRequest, useRequest } from '@proton/pass/hooks/useActionRequest';
 import { useDeobfuscatedValue } from '@proton/pass/hooks/useDeobfuscatedValue';
-import { getAliasDetailsIntent, notification } from '@proton/pass/store/actions';
+import { useFeatureFlag } from '@proton/pass/hooks/useFeatureFlag';
+import { isAliasDisabled } from '@proton/pass/lib/items/item.predicates';
+import { aliasSyncStatusToggle, getAliasDetailsIntent, notification } from '@proton/pass/store/actions';
 import { aliasDetailsRequest } from '@proton/pass/store/actions/requests';
 import { selectAliasDetails } from '@proton/pass/store/selectors';
+import { PassFeature } from '@proton/pass/types/api/features';
+import { not } from '@proton/pass/utils/fp/predicates';
 
 export const AliasContent: FC<ItemContentProps<'alias', { optimistic: boolean; actions: ReactNode }>> = ({
     revision,
@@ -23,7 +28,12 @@ export const AliasContent: FC<ItemContentProps<'alias', { optimistic: boolean; a
 
     const { data: item, shareId, itemId } = revision;
     const aliasEmail = revision.aliasEmail!;
+    const aliasEnabled = not(isAliasDisabled)(revision);
     const note = useDeobfuscatedValue(item.metadata.note);
+    const mailboxesForAlias = useSelector(selectAliasDetails(aliasEmail!));
+
+    const toggleStatus = useRequest(aliasSyncStatusToggle, {});
+    const canToggleStatus = useFeatureFlag(PassFeature.PassSimpleLoginAliasesSync);
 
     const getAliasDetails = useActionRequest(getAliasDetailsIntent, {
         initialRequestId: aliasDetailsRequest(aliasEmail),
@@ -38,7 +48,6 @@ export const AliasContent: FC<ItemContentProps<'alias', { optimistic: boolean; a
         },
     });
 
-    const mailboxesForAlias = useSelector(selectAliasDetails(aliasEmail!));
     const ready = !(getAliasDetails.loading && mailboxesForAlias === undefined);
 
     useEffect(() => {
@@ -48,13 +57,53 @@ export const AliasContent: FC<ItemContentProps<'alias', { optimistic: boolean; a
     return (
         <>
             <FieldsetCluster mode="read" as="div">
-                <ValueControl
-                    clickToCopy
-                    icon="alias"
-                    label={c('Label').t`Alias address`}
-                    value={aliasEmail ?? undefined}
-                    extra={actions}
-                />
+                {canToggleStatus ? (
+                    <div className="relative">
+                        <ValueControl
+                            clickToCopy
+                            icon="alias"
+                            label={c('Label').t`Alias address`}
+                            value={aliasEmail ?? undefined}
+                            extra={actions}
+                            valueClassName="mr-12"
+                        />
+                        <div className="absolute flex items-center h-full right-0 top-0 mr-4">
+                            <Tooltip
+                                openDelay={100}
+                                originalPlacement={'bottom'}
+                                title={
+                                    aliasEnabled
+                                        ? c('Action').t`Disable this alias to stop receiving emails sent to this alias`
+                                        : c('Action').t`Enable this alias to receive emails sent to this alias`
+                                }
+                            >
+                                {/* FIXME: adding a div because Tooltip doesn't seem to appear or is
+                                 * wrongly positioned if <Toggle> is the direct child element */}
+                                <div>
+                                    <Toggle
+                                        checked={aliasEnabled}
+                                        loading={toggleStatus.loading}
+                                        onChange={({ target: { checked } }) =>
+                                            toggleStatus.dispatch({
+                                                shareId,
+                                                itemId,
+                                                enabled: checked,
+                                            })
+                                        }
+                                    ></Toggle>
+                                </div>
+                            </Tooltip>
+                        </div>
+                    </div>
+                ) : (
+                    <ValueControl
+                        clickToCopy
+                        icon="alias"
+                        label={c('Label').t`Alias address`}
+                        value={aliasEmail ?? undefined}
+                        extra={actions}
+                    />
+                )}
 
                 <ValueControl as="ul" loading={!ready} icon="arrow-up-and-right-big" label={c('Label').t`Forwards to`}>
                     {mailboxesForAlias?.map(({ email }) => (
