690ca1df00e56069206b89688b67ad24e7ef808a
Jason Dolbeau <jason.dolbeau@proton.ch>
2024-09-02 14:48:37 +0000

    TPE-742 Fix E2E extraneous dependencies

--- applications/mail/src/app/components/message/constants.ts
+++ applications/mail/src/app/components/message/constants.ts
@@ -1,3 +1,4 @@
+// Update the E2E in tests/mail/poms/mailbox/message/MessageBody.ts when changing these values.
 export const MESSAGE_IFRAME_ROOT_ID = 'proton-root';
 export const MESSAGE_IFRAME_BLOCKQUOTE_ID = 'proton-blockquote';
 export const MESSAGE_IFRAME_TOGGLE_ID = 'proton-toggle';
