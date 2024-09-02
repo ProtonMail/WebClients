82c9c3c102cc9d0145de84fd20aca501732c7427
Edvin CANDON <edvin.candon@proton.ch>
2024-09-02 08:41:44 +0200

    Fix empty alias settings panel when disabled & no aliases to sync
    [IDTEAM-3568]

--- packages/pass/components/Settings/AliasSyncDisabled.tsx
+++ packages/pass/components/Settings/AliasSyncDisabled.tsx
@@ -20,25 +20,36 @@ export const AliasSyncDisabled: FC = () => {
 
     useEffect(dispatch, []);
 
-    return aliasCount ? (
+    return (
         <SettingsPanel
             title={c('Label').t`Sync aliases with SimpleLogin`}
             actions={loading ? [<CircleLoader size="small" />] : undefined}
         >
-            <Card className="mb-4" type="primary">
-                <strong className="color-norm block mb-1 flex-1 text-ellipsis">
-                    {c('Info').t`Aliases out of sync (${aliasCount})`}
-                </strong>
-                <div className="text-sm mb-4">
-                    {c('Info').ngettext(
-                        msgid`We detected that you have ${aliasCount} alias that is present in SimpleLogin but missing in ${PASS_APP_NAME}. Would you like to import it?`,
-                        `We detected that you have ${aliasCount} aliases that are present in SimpleLogin but missing in ${PASS_APP_NAME}. Would you like to import them?`,
-                        aliasCount
-                    )}
-                </div>
-                <Button fullWidth color="norm" onClick={() => setConfirm(true)}>{c('Action').t`Sync aliases`}</Button>
-                {confirm && <AliasSyncModal aliasCount={aliasCount} onClose={() => setConfirm(false)} />}
+            <Card type="primary">
+                {aliasCount > 0 ? (
+                    <>
+                        <strong className="color-norm block mb-1 flex-1 text-ellipsis">
+                            {c('Info').t`Aliases out of sync (${aliasCount})`}
+                        </strong>
+
+                        <div className="text-sm mb-4">
+                            {c('Info').ngettext(
+                                msgid`We detected that you have ${aliasCount} alias that is present in SimpleLogin but missing in ${PASS_APP_NAME}. Would you like to import it?`,
+                                `We detected that you have ${aliasCount} aliases that are present in SimpleLogin but missing in ${PASS_APP_NAME}. Would you like to import them?`,
+                                aliasCount
+                            )}
+                        </div>
+
+                        <Button fullWidth color="norm" onClick={() => setConfirm(true)}>
+                            {c('Action').t`Sync aliases`}
+                        </Button>
+
+                        {confirm && <AliasSyncModal aliasCount={aliasCount} onClose={() => setConfirm(false)} />}
+                    </>
+                ) : (
+                    <span>{c('Info').t`All aliases are synced`}</span>
+                )}
             </Card>
         </SettingsPanel>
-    ) : null;
+    );
 };
