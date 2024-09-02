00e9a90c778fb194760ab5f73cb4934ac7cfcf62
Mattias Svanström <mmso@mmso.se>
2024-09-02 13:58:24 +0000

    Fix wallet bootstrap

--- applications/wallet/src/app/App.tsx
+++ applications/wallet/src/app/App.tsx
@@ -30,15 +30,10 @@ import {
     SubscriptionModalProvider,
     useNotifications,
 } from '@proton/components';
-import { setupGuestCrossStorage } from '@proton/cross-storage/account-impl/guestInstance';
 import useEffectOnce from '@proton/hooks/useEffectOnce';
-import metrics from '@proton/metrics';
 import { ProtonStoreProvider } from '@proton/redux-shared-store';
-import { getClientID } from '@proton/shared/lib/apps/helper';
-import { newVersionUpdater } from '@proton/shared/lib/busy';
 import { WALLET_APP_NAME } from '@proton/shared/lib/constants';
 import { getNonEmptyErrorMessage } from '@proton/shared/lib/helpers/error';
-import { setTtagLocales } from '@proton/shared/lib/i18n/locales';
 import { DRAWER_VISIBILITY } from '@proton/shared/lib/interfaces';
 import { FlagProvider } from '@proton/unleash';
 import ExtendedApiProvider from '@proton/wallet/contexts/ExtendedApiContext/ExtendedApiProvider';
@@ -47,14 +42,7 @@ import { isWasmSupported } from '@proton/wallet/utils/wasm';
 
 import { type bootstrapApp } from './bootstrap';
 import * as config from './config';
-import locales from './locales';
 import type { WalletStore } from './store/store';
-
-setTtagLocales(locales);
-setupGuestCrossStorage();
-newVersionUpdater(config);
-
-metrics.setVersionHeaders(getClientID(config.APP_NAME), config.APP_VERSION);
 
 ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, PointElement, LineElement, Title, Filler);
 
