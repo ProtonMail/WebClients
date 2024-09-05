278254f7f7adc60a1f1ddd93d2786117eb0f3ae0
Jack Crompton <jack.crompton@proton.ch>
2024-09-05 22:09:31 +0200

    Use @proton/icons package

--- packages/components/components/icon/Icons.tsx
+++
@@ -1,8 +0,0 @@
-import svg from '@proton/icons/assets/sprite-icons.svg';
-import svgFiles from '@proton/styles/assets/img/icons/file-icons.svg';
-
-export const ICONS_ID = 'icons-root';
-
-const Icons = () => <div id={ICONS_ID} dangerouslySetInnerHTML={{ __html: `${svg}${svgFiles}` }} />;
-
-export default Icons;
