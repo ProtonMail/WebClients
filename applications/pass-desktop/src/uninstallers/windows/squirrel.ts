4e91f8acf447d899bcdcbe8c3017ea9674be0f51
Dominik Jankovic <dominik.jankovic@proton.ch>
2024-09-09 14:02:58 +0100

    Adopt sharred Squirrel event handling

--- applications/pass-desktop/src/uninstallers/windows/squirrel.ts
+++ applications/pass-desktop/src/uninstallers/windows/squirrel.ts
@@ -2,10 +2,6 @@ import { app } from 'electron';
 import { spawn } from 'node:child_process';
 import { appendFileSync, copyFileSync, existsSync, openSync } from 'node:fs';
 import { resolve } from 'node:path';
-import path from 'path';
-
-import { squirrelLogger } from '../../utils/logger';
-import { isWindows } from '../../utils/platform';
 
 const getLogger =
     (logPath: string, scriptPath: string) =>
@@ -22,7 +18,7 @@ const getLogger =
         appendFileSync(logPath, `${prefix}${message}`, 'utf8');
     };
 
-const spawnUninstallProcess = async () => {
+export const spawnUninstallProcess = async () => {
     const logPath = resolve(app.getPath('temp'), 'proton-pass-uninstall.log');
     const scriptPath = resolve(app.getPath('temp'), 'proton-pass-uninstall.bat');
     const uninstallLogFileDescriptor = openSync(logPath, 'a');
@@ -62,30 +58,4 @@ const spawnUninstallProcess = async () => {
     // exiting with `app.quit()` or `app.exit()` to avoid any side effect.
     logger('Uninstall process started');
     await new Promise(() => {});
-};
-
-export const handleSquirrelEvents = async () => {
-    const run = (args: string[], done: (args: any) => void) => {
-        const updateExe = path.resolve(path.dirname(process.execPath), '..', 'Update.exe');
-        spawn(updateExe, args, { detached: true }).on('close', done);
-    };
-
-    if (isWindows) {
-        squirrelLogger.info('startup');
-        const cmd = process.argv[1] ?? null;
-        const target = path.basename(process.execPath);
-
-        if (['--squirrel-install', '--squirrel-updated'].includes(cmd)) {
-            squirrelLogger.info('creating shortcut');
-            run(['--createShortcut=' + target], app.quit);
-        }
-
-        if ('--squirrel-uninstall' === cmd) {
-            squirrelLogger.info('removing shortcut');
-            run(['--removeShortcut=' + target], app.quit);
-            await spawnUninstallProcess();
-        }
-
-        if ('--squirrel-obsolete' === cmd) app.quit();
-    }
 };
