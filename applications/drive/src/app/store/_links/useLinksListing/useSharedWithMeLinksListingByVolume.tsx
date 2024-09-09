408a7d91dd17621f5783025b196e34337e604477
Michal Horejsek <michal.horejsek@proton.ch>
2024-09-09 14:41:32 +0200

    Faster loading of shared with me

--- applications/drive/src/app/store/_links/useLinksListing/useSharedWithMeLinksListingByVolume.tsx
+++ applications/drive/src/app/store/_links/useLinksListing/useSharedWithMeLinksListingByVolume.tsx
@@ -1,9 +1,12 @@
 import { useCallback, useRef } from 'react';
 
 import { querySharedWithMeLinks } from '@proton/shared/lib/api/drive/sharing';
+import runInQueue from '@proton/shared/lib/helpers/runInQueue';
 import type { ListDriveSharedWithMeLinksPayload } from '@proton/shared/lib/interfaces/drive/sharing';
 
 import { useDebouncedRequest } from '../../_api';
+import { useDriveCrypto } from '../../_crypto';
+import { useShare } from '../../_shares';
 import { useVolumesState } from '../../_volumes';
 import type { DecryptedLink } from '../interface';
 import useLinksState from '../useLinksState';
@@ -23,6 +26,8 @@ export function useSharedWithMeLinksListingByVolume() {
     const debouncedRequest = useDebouncedRequest();
     const linksState = useLinksState();
     const volumesState = useVolumesState();
+    const { getVerificationKey } = useDriveCrypto();
+    const { getSharePrivateKey } = useShare();
     const shareIdsState = useRef<Set<string>>();
 
     const setShareIdsState = (shareIds: string[]) => {
@@ -38,7 +43,7 @@ export function useSharedWithMeLinksListingByVolume() {
 
     const getShareIdsState = (): string[] => Array.from(shareIdsState.current || new Set());
 
-    const { loadFullListingWithAnchor, getDecryptedLinksAndDecryptRest } = useLinksListingHelpers();
+    const { cacheLoadedLinks, loadFullListingWithAnchor, getDecryptedLinksAndDecryptRest } = useLinksListingHelpers();
     const fetchMeta = useRef<FetchSharedLinksMeta>({
         lastPage: 0,
         lastSorting: DEFAULT_SORTING,
@@ -51,11 +56,75 @@ export function useSharedWithMeLinksListingByVolume() {
         },
         loadLinksMetaByVolume: FetchLoadLinksMetaByVolume
     ) => {
-        for (const volumeId in transformedResponse) {
-            await loadLinksMetaByVolume(signal, volumeId, transformedResponse[volumeId], {
-                fetchMeta: fetchMeta.current,
-                removeParentLinkId: true,
-            });
+        /*
+        The store with state in React hooks was not ready for "shared with me".
+
+        The store can load dynamically anything what is missing and update its
+        state as needed. But it comes with a performance and edge cases price.
+        The state must be updated frequently, causing re-renders of everything
+        that must count with that and ensure computation is not done twice etc.
+
+        That doesn't work well in all cases, killing the app or React due to
+        recursion on re-renders. Its better to avoid doing state update in
+        parallel to avoid such issues. But that has very slow performance.
+
+        Alternatively, we could keep it at series, but fetch everything we know
+        (even if its not concern of this module) in advance (even if its
+        blocking to decrypt and show anything until that is done). That ensures
+        that state will be updated less frequently and will not be blocking
+        itself, avoid edge cases that the same resource is fetched or decrypted
+        twice, and then the serial aproach is used only to actualy node
+        decryption and updating the cache to avoid causing re-renders.
+
+        With that, the load of shared with me page goes down to ~25% of the
+        original time.
+
+        Note: this solution will work enough for less than around 100 of items.
+        For more, major refactor will be required to change completely how the
+        loading is done. The reason is that the whole page must be loaded
+        first before we show anything.
+
+        Future consideration: we verify signatures together with decryption.
+        For refactor, we should do it async on background that doesnt block
+        loading. Validating signatures for own files is easier than for others
+        as that requires loading public address keys which own keys are in
+        memory ready to be used.
+        */
+
+        // We cannot let fetch all resources at once, that can be 100s of requests.
+        const BATCH_PROCESSING = 10;
+
+        const nodeQueue = Object.keys(transformedResponse).map((volumeId) => {
+            return () =>
+                loadLinksMetaByVolume(signal, volumeId, transformedResponse[volumeId], {
+                    fetchMeta: fetchMeta.current,
+                    removeParentLinkId: true,
+                });
+        });
+        const results = await runInQueue(nodeQueue, BATCH_PROCESSING);
+
+        const addresses = new Set(
+            Object.values(results).flatMap((result) => {
+                return Object.values(result).flatMap(({ links }) => {
+                    return [...links.map(({ signatureAddress }) => signatureAddress)];
+                });
+            })
+        );
+        const addressesQueue = [...addresses].map((address) => {
+            return () => getVerificationKey(address);
+        });
+        await runInQueue(addressesQueue, BATCH_PROCESSING);
+
+        const shareIds = new Set(Object.values(results).flatMap(Object.keys));
+        const sharesQueue = [...shareIds].map((shareId) => {
+            return () => getSharePrivateKey(signal, shareId);
+        });
+        await runInQueue(sharesQueue, BATCH_PROCESSING);
+
+        for (const result of results) {
+            for (const [shareId, { links, parents }] of Object.entries(result)) {
+                await cacheLoadedLinks(signal, shareId, links, parents);
+            }
         }
     };
 
