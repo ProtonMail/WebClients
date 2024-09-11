2abde3e3300bb68ec832a2aafc5ab6a08286e5a9
Jack Crompton <jack.crompton@proton.ch>
2024-09-11 11:34:49 +0200

    Memoise Icons component

--- packages/icons/Icons.tsx
+++ packages/icons/Icons.tsx
@@ -1,7 +1,11 @@
+import { memo } from 'react';
+
 import svgFiles from './assets/file-icons.svg';
 import svg from './assets/sprite-icons.svg';
 import { ICONS_ID } from './constants';
 
-const Icons = () => <div id={ICONS_ID} dangerouslySetInnerHTML={{ __html: `${svg}${svgFiles}` }} />;
+const Icons = memo(function Icons() {
+    return <div id={ICONS_ID} dangerouslySetInnerHTML={{ __html: `${svg}${svgFiles}` }} />;
+});
 
 export default Icons;
