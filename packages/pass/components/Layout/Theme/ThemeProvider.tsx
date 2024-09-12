307d03e2bd6ea71d5b37f95167af8c73285e5742
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-09-12 13:09:49 +0200

    Add initial theme hydration from `PassCoreProvider`
    - Allows hydrating theme from proxied settings
    - Reflect changes in `ThemeProvider`
    
    [IDTEAM-1975]

--- packages/pass/components/Layout/Theme/ThemeProvider.tsx
+++ packages/pass/components/Layout/Theme/ThemeProvider.tsx
@@ -1,13 +1,15 @@
-import { type FC, type PropsWithChildren, useLayoutEffect } from 'react';
+import { type FC, type PropsWithChildren, useEffect, useLayoutEffect, useState } from 'react';
 
 // @ts-ignore
 import passDarkTheme from '@proton/colors/themes/dist/pass-dark.theme.css';
 // @ts-ignore
 import passLightTheme from '@proton/colors/themes/dist/pass-light.theme.css';
+import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
+import type { Maybe } from '@proton/pass/types';
 import { ThemeTypes } from '@proton/shared/lib/themes/themes';
 
 export const THEME_ID = 'pass-theme';
-type Props = { theme: ThemeTypes };
+type Props = { theme?: ThemeTypes };
 
 type PassTheme = {
     className: string;
@@ -25,18 +27,35 @@ export const PASS_THEMES_MAP: Partial<Record<ThemeTypes, PassTheme>> = {
     },
 };
 
-export const ThemeProvider: FC<PropsWithChildren<Props>> = ({ theme, children }) => {
-    const { className, styles } = PASS_THEMES_MAP[theme] ?? PASS_THEMES_MAP[ThemeTypes.PassDark]!;
+export const ThemeProvider: FC<PropsWithChildren<Props>> = (props) => {
+    const { getInitialTheme } = usePassCore();
+    const [theme, setTheme] = useState<Maybe<ThemeTypes>>(props.theme);
+
+    const config = theme ? PASS_THEMES_MAP[theme] : null;
+
+    useEffect(() => {
+        if (props.theme) {
+            setTheme(props.theme);
+        }
+    }, [props.theme]);
 
     useLayoutEffect(() => {
-        document.body.classList.add(className);
-        return () => document.body.classList.remove(className);
-    }, [className]);
+        if (config) {
+            document.body.classList.add(config.className);
+            return () => document.body.classList.remove(config.className);
+        }
+    }, [config]);
+
+    useEffect(() => {
+        (async () => (await getInitialTheme?.()) ?? ThemeTypes.PassDark)()
+            .then(setTheme)
+            .catch(() => setTheme(ThemeTypes.PassDark));
+    }, []);
 
     return (
         <>
-            <style id={THEME_ID}>{styles}</style>
-            {children}
+            {config && <style id={THEME_ID}>{config.styles}</style>}
+            {props.children}
         </>
     );
 };
