eb30a90ada86f2a35cb588ef6548bc2b94099033
Jakub Cuth <jcuth@proton.ch>
2024-09-13 10:37:41 +0200

    chore(INDA-91): forgotten cosmetics.

--- applications/inbox-desktop/src/utils/protocol/default_mailto_macos.ts
+++ applications/inbox-desktop/src/utils/protocol/default_mailto_macos.ts
@@ -1,11 +1,11 @@
 import { app } from "electron";
 import { DefaultProtocol } from "./types";
 
-// Electron app protocol isDefaultProtocolClient for mailto works only for macOS.
-export const checkDefaultMailtoClientMac = (): DefaultProtocol =>
-    ({
-        isDefault: app.isDefaultProtocolClient("mailto"),
-        isChecked: true,
-    }) as DefaultProtocol;
+// Electron app protocol isDefaultProtocolClient for mailto works correctly only for macOS.
+export const checkDefaultMailtoClientMac = (): DefaultProtocol => ({
+    isDefault: app.isDefaultProtocolClient("mailto"),
+    isChecked: true,
+});
 
+// Electron app protocol setAsDefaultProtocolClient for mailto works correctly only for macOS.
 export const setDefaultMailtoMac = () => app.setAsDefaultProtocolClient("mailto");
