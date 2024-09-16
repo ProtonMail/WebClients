fc611ba5a614c1486813bf65b9a33c20d4bcbd30
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-09-16 11:11:06 +0200

    Rebase fixes and polish

--- applications/pass-extension/src/lib/components/Extension/ExtensionError.tsx
+++ applications/pass-extension/src/lib/components/Extension/ExtensionError.tsx
@@ -4,8 +4,8 @@ import { RUNTIME_RELOAD_THROTTLE } from 'proton-pass-extension/app/worker/servic
 import { c } from 'ttag';
 
 import { Button, CircleLoader } from '@proton/atoms';
-import type { IconName } from '@proton/components/components/icon';
-import { Icon } from '@proton/components/components/icon';
+import type { IconName } from '@proton/components';
+import { Icon } from '@proton/components';
 import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
 import { LobbyLayout } from '@proton/pass/components/Layout/Lobby/LobbyLayout';
 import browser from '@proton/pass/lib/globals/browser';
