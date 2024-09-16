49f8cd0ad8e75f599293d2eabcbae33cf73891fd
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-09-16 15:07:44 +0000

    Remind free users to upsell every 6 months

--- packages/components/containers/offers/operations/subscriptionReminder/useOffer.ts
+++ packages/components/containers/offers/operations/subscriptionReminder/useOffer.ts
@@ -1,4 +1,7 @@
-import { useConfig, useUser } from '@proton/components/hooks';
+import { FeatureCode } from '@proton/components/containers/features';
+import useConfig from '@proton/components/hooks/useConfig';
+import useFeature from '@proton/components/hooks/useFeature';
+import { useUser } from '@proton/components/hooks/useUser';
 
 import useOfferFlags from '../../hooks/useOfferFlags';
 import type { Operation } from '../../interface';
@@ -12,17 +15,21 @@ import getIsEligible from './eligibility';
 const useOffer = (): Operation => {
     const protonConfig = useConfig();
     const [user, loadingUser] = useUser();
-    const { isActive, loading: flagsLoading } = useOfferFlags(config);
+    const { isActive, loading: flagsLoading, isVisited } = useOfferFlags(config);
+    const { feature: lastReminderDate, loading: lastReminderDateLoading } = useFeature(
+        FeatureCode.SubscriptionLastReminderDate
+    );
 
-    const isLoading = flagsLoading || loadingUser;
+    const isLoading = flagsLoading || loadingUser || lastReminderDateLoading;
     // We use today time if the feature flag is not set to avoid showing the value to all users or unexpected behavior
     const isEligible = getIsEligible({
         user,
         protonConfig,
+        lastReminderTimestamp: lastReminderDate?.Value,
+        isVisited,
     });
-    const isValid = isEligible && isActive;
 
-    return { isValid, config, isEligible, isLoading };
+    return { isValid: isEligible && isActive, config, isEligible, isLoading: !!isLoading };
 };
 
 export default useOffer;
