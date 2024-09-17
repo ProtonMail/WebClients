5cd7daf490d4b7bc5ef7daa9a3c576c421d2e0f7
Fedora Lepcheska <fedora.lepcheska@proton.ch>
2024-09-17 14:04:57 +0000

    Drag move container dependencies

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -62,6 +62,7 @@ export { default as ContextMenu } from './components/contextMenu/ContextMenu';
 export { default as ContextMenuButton } from './components/contextMenu/ContextMenuButton';
 export { default as ContextSeparator } from './components/contextMenu/ContextSeparator';
 export { default as DownloadClientCard } from './components/downloadClientCard/DownloadClientCard';
+export { default as DragMoveContainer } from './components/dragMoveContainer/DragMoveContainer';
 export { default as Dropdown } from './components/dropdown/Dropdown';
 export type { DropdownProps } from './components/dropdown/Dropdown';
 export { default as DropdownActions } from './components/dropdown/DropdownActions';
