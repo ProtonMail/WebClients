9fd80415c9918c2cbb3dce7bce46ffaeaa1ba951
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-09-17 11:46:34 +0200

    Handle edge case where the SL sync vault is deleted
    [IDTEAM-3568]

--- packages/pass/store/sagas/vaults/vault-delete.saga.ts
+++ packages/pass/store/sagas/vaults/vault-delete.saga.ts
@@ -1,17 +1,30 @@
-import { put, takeEvery } from 'redux-saga/effects';
+import { put, select, takeEvery } from 'redux-saga/effects';
 
 import { PassCrypto } from '@proton/pass/lib/crypto';
 import { deleteVault } from '@proton/pass/lib/vaults/vault.requests';
-import { vaultDeleteFailure, vaultDeleteIntent, vaultDeleteSuccess } from '@proton/pass/store/actions';
+import {
+    getUserAccessIntent,
+    vaultDeleteFailure,
+    vaultDeleteIntent,
+    vaultDeleteSuccess,
+} from '@proton/pass/store/actions';
+import { withRevalidate } from '@proton/pass/store/request/enhancers';
+import { selectUserDefaultShareId } from '@proton/pass/store/selectors';
 import type { RootSagaOptions } from '@proton/pass/store/types';
 
 function* deleteVaultWorker(
-    { onItemsUpdated }: RootSagaOptions,
+    { onItemsUpdated, getAuthStore }: RootSagaOptions,
     { payload: { shareId, content }, meta }: ReturnType<typeof vaultDeleteIntent>
-): Generator {
+) {
     try {
         yield deleteVault(shareId);
         PassCrypto.removeShare(shareId);
+
+        /* Handle edge case when the alias sync vault is deleted:
+         * we check the new alias sync vault from BE in the user access route */
+        const userID = getAuthStore().getUserID();
+        const aliasSyncShareId: string = yield select(selectUserDefaultShareId);
+        if (shareId === aliasSyncShareId) yield put(withRevalidate(getUserAccessIntent(userID!)));
 
         yield put(vaultDeleteSuccess(meta.request.id, { shareId, content }));
         onItemsUpdated?.();
