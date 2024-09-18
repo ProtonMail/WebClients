0f7fb73a215a379e79b4b35ff520d79fb622b5d4
flepches <fedora.lepcheska@proton.ch>
2024-09-18 14:07:43 +0200

    Fix keys circular dependencies

--- packages/components/containers/keys/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AddressKeysSection } from './AddressKeysSection';
-export { default as UserKeysSection } from './UserKeysSection';
-export { default as SelectKeyFiles } from './shared/SelectKeyFiles';
