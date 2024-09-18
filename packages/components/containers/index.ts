3feb27cbe6b1561df77638bc81cdd10ee9aa8568
flepches <fedora.lepcheska@proton.ch>
2024-09-18 16:23:25 +0200

    Fix mail circular dependencies

--- packages/components/containers/index.ts
+++ packages/components/containers/index.ts
@@ -25,7 +25,6 @@ export * from './labels';
 export * from './layout';
 export * from './layouts';
 export * from './logs';
-export * from './mail';
 export * from './members';
 export * from './messages';
 export * from './mnemonic';
