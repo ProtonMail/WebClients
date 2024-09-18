5c1b9819111c2e30a67332bd2bb6bccc926ec5bf
flepches <fedora.lepcheska@proton.ch>
2024-09-18 16:16:05 +0200

    Fix login circular dependencies

--- packages/components/containers/login/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as AbuseModal } from './AbuseModal';
-export { default as MinimalForgotUsernameContainer } from '../forgotUsername/MinimalForgotUsernameContainer';
-export { default as MinimalLoginContainer } from './MinimalLoginContainer';
-export { default as FooterDetails } from './FooterDetails';
-export { default as UnlockModal } from './UnlockModal';
