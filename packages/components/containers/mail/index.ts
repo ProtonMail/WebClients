c32d119dce3e9592f440f44be8af1605510d98e6
flepches <fedora.lepcheska@proton.ch>
2024-09-18 16:23:25 +0200

    Fix mail circular dependencies

--- packages/components/containers/mail/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MailShortcutsModal } from './MailShortcutsModal';
-export { default as MailDensityModal } from './MailDensityModal';
-export { default as MailViewLayoutModal } from './MailViewLayoutModal';
-export { default as MailComposerModeModal } from './MailComposerModeModal';
