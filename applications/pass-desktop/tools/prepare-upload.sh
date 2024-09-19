f01d41a958c1c883e1daf0fe4d647a07fb5ff78e
Dominik Jankovic <dominik.jankovic@proton.ch>
2024-09-19 15:49:16 +0100

    Fix Git tag channel resolution

--- applications/pass-desktop/tools/prepare-upload.sh
+++ applications/pass-desktop/tools/prepare-upload.sh
@@ -7,8 +7,10 @@ CWD=$(pwd)
 BUILD_DIR="applications/pass-desktop/out/make"
 PLATFORM="$1"
 
-# Deployment is EarlyAccess if its CI tag contains a dash, eg. 1.0.0-rc1
-if [ -n "${CI_COMMIT_TAG:-}" ] && [ "${CI_COMMIT_TAG#*-}" = "$CI_COMMIT_TAG" ]; then
+# Deployment is EarlyAccess if its CI tag contains a dash, eg. proton-pass@1.0.0-rc1
+TAG_VERSION="${CI_COMMIT_TAG:-}"
+TAG_VERSION="${TAG_VERSION##*@}"
+if [ -n "${TAG_VERSION}" ] && [ "${TAG_VERSION#*-}" = "${TAG_VERSION}" ]; then
   CHANNEL="Stable"
 else
   CHANNEL="EarlyAccess"
@@ -16,7 +18,7 @@ fi
 
 # Currently, we're only supporting the stable channel
 if [ "$CHANNEL" != "Stable" ]; then
-  echoerr "Channels other than 'Stable' are currently unsupported"
+  echoerr "Channels other than 'Stable' are currently unsupported (v=${TAG_VERSION},c=${CHANNEL})"
   exit 1
 fi
 
