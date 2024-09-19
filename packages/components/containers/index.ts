cdb7cb5aa2d453c1f1f35df2c3708cf3a3ac2766
flepches <fedora.lepcheska@proton.ch>
2024-09-19 11:16:57 +0200

    Fix notifications circular dependencies

--- packages/components/containers/index.ts
+++ packages/components/containers/index.ts
@@ -20,7 +20,6 @@ export * from './features';
 export * from './filePreview';
 export * from './filters';
 export * from './general';
-export * from './notifications';
 export * from './offers';
 export * from './onboarding';
 export * from './organization';
