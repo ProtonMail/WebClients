c363809fbafc520815383817f682c84a9b93e9e6
Lucien Peslier <lucien.peslier@proton.ch>
2024-09-19 08:32:42 +0000

    Load last persisted session from any app on public page

--- applications/drive/src/app/store/_user/usePublicSessionUser.ts
+++ applications/drive/src/app/store/_user/usePublicSessionUser.ts
@@ -1,11 +1,13 @@
 import { useMemo } from 'react';
 
-import { getLastPersistedLocalID } from '../../utils/lastActivePersistedUserSession';
+import { useAuthentication } from '@proton/components/hooks';
+
 import { usePublicSession } from '../_api';
 
 export const usePublicSessionUser = () => {
+    const auth = useAuthentication();
     const { user } = usePublicSession();
-    const localID = useMemo(() => getLastPersistedLocalID(), []);
+    const localID: number | undefined = useMemo(() => auth.getLocalID(), []);
 
-    return { user, localID: localID ?? undefined };
+    return { user, localID };
 };
