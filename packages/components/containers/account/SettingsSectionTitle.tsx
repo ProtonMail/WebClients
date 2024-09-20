8235b5e5cf8e2c97309ea24585d399d9a6afce0f
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-09-20 15:39:40 +0200

    delete title

--- packages/components/containers/account/SettingsSectionTitle.tsx
+++ packages/components/containers/account/SettingsSectionTitle.tsx
@@ -1,10 +1,11 @@
+import type { HTMLAttributes } from 'react';
+
 import clsx from '@proton/utils/clsx';
 
-import type { SubTitleProps } from '../../components/title/SubTitle';
-import SubTitle from '../../components/title/SubTitle';
-
-const SettingsSectionTitle = ({ className, ...rest }: SubTitleProps) => (
-    <SubTitle className={clsx(className, 'text-bold mb-1')} {...rest} />
+const SettingsSectionTitle = ({ className, ...rest }: HTMLAttributes<HTMLHeadingElement>) => (
+    <h2 className={clsx(className, 'text-bold mb-1')} {...rest}>
+        {rest.children}
+    </h2>
 );
 
 export default SettingsSectionTitle;
