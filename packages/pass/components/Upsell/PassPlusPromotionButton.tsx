6a3103044c4b0507b4d4fc14720ab67a332d4aaf
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-09-23 15:38:17 +0200

    Redesign Pass Plus upsell button
    [IDTEAM-1975]

--- packages/pass/components/Upsell/PassPlusPromotionButton.tsx
+++ packages/pass/components/Upsell/PassPlusPromotionButton.tsx
@@ -2,20 +2,23 @@ import type { FC } from 'react';
 
 import { PromotionButton } from '@proton/components/components/button/PromotionButton';
 import type { PromotionButtonProps } from '@proton/components/components/button/PromotionButton/PromotionButton';
+import { Icon } from '@proton/components/index';
 import clsx from '@proton/utils/clsx';
 
 type Props = Omit<PromotionButtonProps<'span'>, 'size'>;
 
 export const PassPlusPromotionButton: FC<Props> = ({ className, ...props }) => (
     <PromotionButton
-        iconName="brand-proton-pass"
+        iconName="brand-proton-pass-filled"
         iconGradient={false}
-        icon
         size="small"
         shape="outline"
         as="span"
         {...props}
         className={clsx(className, 'flex items-center button-xs')}
         iconSize={3.5}
-    />
+        style={{ '--upgrade-color-stop-1': '#fcd38d', '--upgrade-color-stop-2': '#9834ff' }}
+    >
+        <Icon name="plus" size={3} className="mb-0.5" />
+    </PromotionButton>
 );
