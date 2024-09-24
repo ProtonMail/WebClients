aa257468691e26077167675fa2fa88f15dc96337
Alejandro Tidele <alejandro.tidele@proton.ch>
2024-09-24 12:15:45 +0200

    Improve URL utilities
    - Add port resolution in url parser
    - Add unit-tests for all URL utils
    - Rename `isValidURL` to `sanitizeURL`
    - Simplify `globToRegExp` based on current usage
    
    [IDTEAM-1688]
    
    Co-authored-by: Edvin CANDON <edvin.candon@proton.ch>

--- packages/pass/utils/url/sanitize.spec.ts
+++ packages/pass/utils/url/sanitize.spec.ts
@@ -1,77 +1,72 @@
-import {
-    MAX_HOSTNAME_LENGTH,
-    UNSUPPORTED_SCHEMES,
-    intoCleanHostname,
-    intoDomainImageHostname,
-    isValidURL,
-} from './is-valid-url';
+import { sanitizeURL } from './sanitize';
+import { MAX_HOSTNAME_LENGTH, UNSUPPORTED_SCHEMES, intoCleanHostname, intoDomainImageHostname } from './utils';
 
 const VALID_SCHEMES = ['http:', 'https:', 'ftp:', 'ssh:', 'telnet:', 'irc:', 'magnet:'];
 
 describe('URL validation', () => {
     describe('`isValidURL`', () => {
         test('should try to append `https://` when scheme is missing', () => {
-            const { valid, url } = isValidURL('google.com');
+            const { valid, url } = sanitizeURL('google.com');
             expect(valid).toBe(true);
             expect(url).toEqual('https://google.com/');
         });
 
         test('should auto-fix scheme through URL constructor', () => {
-            const { valid, url } = isValidURL('https:/proton.me');
+            const { valid, url } = sanitizeURL('https:/proton.me');
             expect(valid).toBe(true);
             expect(url).toEqual('https://proton.me/');
         });
 
         test('should invalidate unsupported schemes', () => {
             UNSUPPORTED_SCHEMES.forEach((scheme) => {
-                expect(isValidURL(`${scheme}someurl`).valid).toBe(false);
-                expect(isValidURL(`${scheme}/someurl`).valid).toBe(false);
-                expect(isValidURL(`${scheme}//someurl`).valid).toBe(false);
-                expect(isValidURL('someurl', scheme).valid).toBe(false);
-                expect(isValidURL('someurl', `${scheme}/`).valid).toBe(false);
-                expect(isValidURL('someurl', `${scheme}//`).valid).toBe(false);
+                expect(sanitizeURL(`${scheme}someurl`).valid).toBe(false);
+                expect(sanitizeURL(`${scheme}/someurl`).valid).toBe(false);
+                expect(sanitizeURL(`${scheme}//someurl`).valid).toBe(false);
+                expect(sanitizeURL('someurl', scheme).valid).toBe(false);
+                expect(sanitizeURL('someurl', `${scheme}/`).valid).toBe(false);
+                expect(sanitizeURL('someurl', `${scheme}//`).valid).toBe(false);
             });
         });
 
         test('should invalidate URLs over `MAX_HOSTNAME_LENGTH`', () => {
             const url = `https://${'a'.repeat(MAX_HOSTNAME_LENGTH + 1)}.com`;
-            expect(isValidURL(url).valid).toBe(false);
+            expect(sanitizeURL(url).valid).toBe(false);
         });
 
         test('should accept any valid scheme', () => {
             VALID_SCHEMES.forEach((scheme) => {
-                expect(isValidURL(`${scheme}someurl`).valid).toBe(true);
-                expect(isValidURL(`${scheme}//someurl`).valid).toBe(true);
-                expect(isValidURL('someurl', scheme).valid).toBe(true);
-                expect(isValidURL('someurl', scheme).valid).toBe(true);
+                expect(sanitizeURL(`${scheme}someurl`).valid).toBe(true);
+                expect(sanitizeURL(`${scheme}//someurl`).valid).toBe(true);
+                expect(sanitizeURL('someurl', scheme).valid).toBe(true);
+                expect(sanitizeURL('someurl', scheme).valid).toBe(true);
             });
         });
 
         test('should keep `www` in URL if present', () => {
-            expect(isValidURL('www.proton.me').url).toEqual('https://www.proton.me/');
-            expect(isValidURL('https://www.proton.me').url).toEqual('https://www.proton.me/');
-            expect(isValidURL('https://www.proton.me/').url).toEqual('https://www.proton.me/');
-            expect(isValidURL('www.proton.me/login').url).toEqual('https://www.proton.me/login');
+            expect(sanitizeURL('www.proton.me').url).toEqual('https://www.proton.me/');
+            expect(sanitizeURL('https://www.proton.me').url).toEqual('https://www.proton.me/');
+            expect(sanitizeURL('https://www.proton.me/').url).toEqual('https://www.proton.me/');
+            expect(sanitizeURL('www.proton.me/login').url).toEqual('https://www.proton.me/login');
         });
 
         test('should preserve query strings and full path', () => {
-            expect(isValidURL('proton.me/login/?foo=bar').url).toEqual('https://proton.me/login/?foo=bar');
-            expect(isValidURL('proton.me/login/?foo=bar', 'wss:').url).toEqual('wss://proton.me/login/?foo=bar');
-            expect(isValidURL('https://proton.me/login/?foo=bar').url).toEqual('https://proton.me/login/?foo=bar');
+            expect(sanitizeURL('proton.me/login/?foo=bar').url).toEqual('https://proton.me/login/?foo=bar');
+            expect(sanitizeURL('proton.me/login/?foo=bar', 'wss:').url).toEqual('wss://proton.me/login/?foo=bar');
+            expect(sanitizeURL('https://proton.me/login/?foo=bar').url).toEqual('https://proton.me/login/?foo=bar');
         });
 
         test('should preserve ports', () => {
-            expect(isValidURL('http://localhost:3000').url).toEqual('http://localhost:3000/');
-            expect(isValidURL('https://localhost:3001?foo=bar').url).toEqual('https://localhost:3001/?foo=bar');
+            expect(sanitizeURL('http://localhost:3000').url).toEqual('http://localhost:3000/');
+            expect(sanitizeURL('https://localhost:3001?foo=bar').url).toEqual('https://localhost:3001/?foo=bar');
         });
 
         test('should invalidate URLs containing internal white-spaces', () => {
-            expect(isValidURL('https://www.proton    .me').valid).toBe(false);
-            expect(isValidURL('proton.me  /').valid).toBe(false);
-            expect(isValidURL('www.proton.me/foo/   ?bar=10').valid).toBe(false);
+            expect(sanitizeURL('https://www.proton    .me').valid).toBe(false);
+            expect(sanitizeURL('proton.me  /').valid).toBe(false);
+            expect(sanitizeURL('www.proton.me/foo/   ?bar=10').valid).toBe(false);
 
-            expect(isValidURL('proton.me  ').valid).toBe(true);
-            expect(isValidURL('www.proton.me/foo/   ').valid).toBe(true);
+            expect(sanitizeURL('proton.me  ').valid).toBe(true);
+            expect(sanitizeURL('www.proton.me/foo/   ').valid).toBe(true);
         });
     });
 
