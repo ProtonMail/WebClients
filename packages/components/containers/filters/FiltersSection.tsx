d77625f5c162d7bf3d8fcd15e1f17054b2524a55
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-09-25 19:16:24 +0200

    remove useFilters

--- packages/components/containers/filters/FiltersSection.tsx
+++ packages/components/containers/filters/FiltersSection.tsx
@@ -8,10 +8,11 @@ import { Href } from '@proton/atoms';
 import Loader from '@proton/components/components/loader/Loader';
 import SettingsParagraph from '@proton/components/containers/account/SettingsParagraph';
 import SettingsSection from '@proton/components/containers/account/SettingsSection';
+import { useFilters } from '@proton/mail/filters/hooks';
 import { applyFilters, updateFilterOrder } from '@proton/shared/lib/api/filters';
 import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
 
-import { useApi, useEventManager, useFilters, useNotifications } from '../../hooks';
+import { useApi, useEventManager, useNotifications } from '../../hooks';
 import ActionsFilterToolbar from './ActionsFilterToolbar';
 import FilterSortableList from './FilterSortableList';
 import type { Filter } from './interfaces';
