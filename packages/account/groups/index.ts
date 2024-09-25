3398198308f7a35e676955937bc583d89084a6e3
Pedro Lavarinhas Amaro <pedro.lavarinhas.amaro@proton.ch>
2024-09-25 02:07:22 +0000

    Add server event reducer case to groupThunk

--- packages/account/groups/index.ts
+++ packages/account/groups/index.ts
@@ -4,9 +4,11 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 import type { ProtonThunkArguments } from '@proton/redux-shared-store-types';
 import { createAsyncModelThunk, handleAsyncModel, previousSelector } from '@proton/redux-utilities';
 import { getGroups } from '@proton/shared/lib/api/groups';
+import updateCollection from '@proton/shared/lib/helpers/updateCollection';
 import type { Group } from '@proton/shared/lib/interfaces';
 
 import type { DomainsState } from '../domains';
+import { serverEvent } from '../eventLoop';
 import { getInitialModelState } from '../initialModelState';
 import type { ModelState } from '../interface';
 import type { OrganizationKeyState } from '../organizationKey';
@@ -62,6 +64,19 @@ const slice = createSlice({
     },
     extraReducers: (builder) => {
         handleAsyncModel(builder, modelThunk);
+        builder.addCase(serverEvent, (state, action) => {
+            if (!state.value) {
+                return;
+            }
+
+            if (action.payload.Groups) {
+                state.value = updateCollection({
+                    model: state.value,
+                    events: action.payload.Groups,
+                    itemKey: 'Group',
+                });
+            }
+        });
     },
 });
 export const { addGroup, updateGroup, removeGroup } = slice.actions;
