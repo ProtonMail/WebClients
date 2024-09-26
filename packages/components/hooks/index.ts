8fe2863e48498e1cc8bdd78053c98456d736c56a
flepches <fedora.lepcheska@proton.ch>
2024-09-26 10:15:45 +0200

    Fix useAppTitle circular dependencies

--- packages/components/hooks/index.ts
+++ packages/components/hooks/index.ts
@@ -2,7 +2,6 @@ export { default as useAddresses, useGetAddresses } from './useAddresses';
 export { useAddressesKeys, useGetAddressKeys } from './useAddressesKeys';
 export { default as useApi } from './useApi';
 export { default as useApiWithoutResult } from './useApiWithoutResult';
-export { default as useAppTitle } from './useAppTitle';
 export { default as useAuthentication } from './useAuthentication';
 export { default as useAutocompleteAriaProps } from './useAutocompleteAriaProps';
 export { default as useBeforeUnload } from './useBeforeUnload';
