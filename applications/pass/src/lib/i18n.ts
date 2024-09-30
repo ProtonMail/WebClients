334a9eff9ce96207a0fc37d995a7b768687fb626
Edvin CANDON <edvin.candon@proton.ch>
2024-09-30 07:56:53 +0200

    Set locale as soon as persisted session is resolved
    - In `ExtensionCore` read primary locale from settings directly
    - Remove `LOCALE_REQUEST` message
    
    [IDTEAM-3758]

--- applications/pass/src/lib/i18n.ts
+++ applications/pass/src/lib/i18n.ts
@@ -1,5 +1,6 @@
 import { authStore } from '@proton/pass/lib/auth/store';
 import { createI18nService } from '@proton/pass/lib/i18n/service';
+import { prop } from '@proton/pass/utils/fp/lens';
 import type { TtagLocaleMap } from '@proton/shared/lib/interfaces';
 import noop from '@proton/utils/noop';
 
@@ -9,12 +10,13 @@ export const i18n = (locales: TtagLocaleMap) =>
     createI18nService({
         locales,
         loadDateLocale: true,
-        getLocale: async () => (await settings.resolve()).locale,
+        getLocale: () => settings.resolve(authStore.getLocalID()).then(prop('locale')).catch(noop),
         onLocaleChange: (locale) => {
+            const localID = authStore.getLocalID();
             if (authStore.hasSession()) {
                 settings
-                    .resolve()
-                    .then((localSettings) => settings.sync({ ...localSettings, locale }))
+                    .resolve(localID)
+                    .then((prev) => settings.sync({ ...prev, locale }, localID))
                     .catch(noop);
             }
         },
