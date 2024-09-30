59298107aecda37ec065ce5cc7f29ba72111f2a5
Dominik Jankovic <dominik.jankovic@proton.ch>
2024-09-30 09:34:31 +0100

    Track install source

---
+++ applications/pass-desktop/src/lib/install-info.ts
@@ -0,0 +1,40 @@
+import logger from 'electron-log/main';
+import Store from 'electron-store';
+
+const store = new Store<{
+    installInfo?: {
+        source: string | null;
+        reported: boolean;
+    };
+}>({});
+
+export function setInstallSource(installSource: string) {
+    logger.info('set install source', installSource);
+    store.set('installInfo', {
+        source: installSource,
+        reported: false,
+    });
+}
+
+export function getInstallSource() {
+    const installInfo = store.get('installInfo');
+
+    if (installInfo?.reported === false) {
+        logger.debug('get install source', installInfo.source);
+        return installInfo.source;
+    }
+
+    return null;
+}
+
+export function setInstallSourceReported() {
+    const installInfo = store.get('installInfo');
+
+    if (installInfo && installInfo.reported === false) {
+        logger.info('set install source as reported');
+        store.set('installInfo', {
+            source: installInfo.source,
+            reported: true,
+        });
+    }
+}
