75a66e1ac21707d79788dfd590da039bb4ed38fb
Edvin CANDON <edvin.candon@proton.ch>
2024-10-01 10:40:31 +0200

    Allow passing custom component to `SpotlightContent`
    [IDTEAM-3928]

--- packages/pass/components/Spotlight/SpotlightContent.tsx
+++ packages/pass/components/Spotlight/SpotlightContent.tsx
@@ -1,5 +1,4 @@
-import type { ElementType } from 'react';
-import { type FC, type MouseEvent, useMemo } from 'react';
+import type { ComponentType, ElementType, FC, MouseEvent, ReactNode } from 'react';
 
 import { c } from 'ttag';
 
@@ -9,92 +8,115 @@ import clsx from '@proton/utils/clsx';
 
 import './SpotlightContent.scss';
 
-export type SpotlightMessageDefinition = {
-    action?: { label: string; onClick: (e: MouseEvent<HTMLElement>) => void; type: 'link' | 'button' };
+export type BaseSpotlightMessage = {
     className?: string;
-    dense?: boolean;
-    icon?: ElementType;
-    id: string;
-    message: string;
-    title: string;
-    weak?: boolean;
     hidden?: boolean;
+    id: string;
+    weak?: boolean;
     onClose?: () => void;
 };
 
+export type DefaultSplotlightMessage = BaseSpotlightMessage & {
+    type: 'default';
+    action?: { label: string; onClick: (e: MouseEvent<HTMLElement>) => void; type: 'link' | 'button' };
+    dense?: boolean;
+    icon?: ElementType;
+    message: ReactNode;
+    title: ReactNode;
+};
+
+export type CustomSplotlightMessage = BaseSpotlightMessage & {
+    type: 'custom';
+    component: ComponentType<BaseSpotlightMessage>;
+};
+
+export type SpotlightMessageDefinition = DefaultSplotlightMessage | CustomSplotlightMessage;
+
 type Props = SpotlightMessageDefinition;
 
-export const SpotlightContent: FC<Props> = ({
-    action,
-    className,
-    dense = true,
-    icon,
-    message,
-    title,
-    weak,
-    onClose,
-}) => {
-    const actionNode = useMemo(() => {
-        switch (action?.type) {
-            case 'link':
-                return (
-                    <button onClick={action.onClick} className="unstyled text-sm color-invert px-3">
-                        <span className="text-underline">{action.label}</span>
-                    </button>
-                );
-            case 'button':
-                return (
-                    <Button
-                        pill
-                        shape="solid"
-                        color="norm"
-                        size="small"
-                        className="text-sm px-3"
-                        onClick={action.onClick}
-                        style={{ backgroundColor: 'var(--interaction-norm-major-3)' }}
-                    >
-                        {action.label}
-                    </Button>
-                );
-        }
-    }, [action]);
-
-    const CustomIcon = icon;
-
+export const SpotlightContent: FC<Props> = (props) => {
     return (
         <div
             className={clsx(
-                `pass-spotlight-content flex items-center gap-4 p-4 pr-6 rounded relative mt-2`,
-                className,
-                weak && 'weak'
+                props.className,
+                props.type === 'default' && props.weak && 'weak',
+                `pass-spotlight-content flex items-center gap-4 p-4 pr-6 rounded relative mt-2`
             )}
         >
-            {onClose && (
+            {props.onClose && (
                 <Button
                     icon
+                    pill
                     shape="ghost"
                     color="weak"
                     size="small"
                     className="absolute top-0 right-0"
-                    onClick={onClose}
+                    onClick={props.onClose}
                 >
-                    <Icon name="cross" color="var(--interaction-norm-contrast)" alt={c('Action').t`Close`} />
+                    <Icon
+                        name="cross-circle-filled"
+                        color="var(--interaction-norm-contrast)"
+                        alt={c('Action').t`Close`}
+                    />
                 </Button>
             )}
 
-            <div className="flex-1">
-                <strong className={clsx('block', !weak && 'color-invert')}>{title}</strong>
-                <span className={clsx('block text-sm', !weak && 'color-invert')}>{message}</span>
-                {!dense && <div className="mt-2">{actionNode}</div>}
-            </div>
+            {(() => {
+                switch (props.type) {
+                    case 'custom': {
+                        const { component, ...rest } = props;
+                        const Component = component;
+                        return <Component {...rest} />;
+                    }
 
-            {CustomIcon && (
-                <div {...(dense ? { style: { order: -1 } } : { className: 'mr-2' })}>
-                    <CustomIcon />
-                </div>
-            )}
+                    case 'default': {
+                        const { action, dense, icon: CustomIcon, message, title, weak } = props;
 
-            {dense && actionNode}
+                        const actionNode = (() => {
+                            switch (action?.type) {
+                                case 'link':
+                                    return (
+                                        <button onClick={action.onClick} className="unstyled text-sm color-invert px-3">
+                                            <span className="text-underline">{action.label}</span>
+                                        </button>
+                                    );
+                                case 'button':
+                                    return (
+                                        <Button
+                                            pill
+                                            shape="solid"
+                                            color="norm"
+                                            size="small"
+                                            className="text-sm px-3"
+                                            onClick={action.onClick}
+                                            style={{ backgroundColor: 'var(--interaction-norm-major-3)' }}
+                                        >
+                                            {action.label}
+                                        </Button>
+                                    );
+                            }
+                        })();
+
+                        return (
+                            <>
+                                <div className="flex-1">
+                                    <strong className={clsx('block', !weak && 'color-invert')}>{title}</strong>
+                                    <span className={clsx('block text-sm', !weak && 'color-invert')}>{message}</span>
+                                    {!dense && <div className="mt-2">{actionNode}</div>}
+                                </div>
+
+                                {CustomIcon && (
+                                    <div {...(dense ? { style: { order: -1 } } : { className: 'mr-2' })}>
+                                        <CustomIcon />
+                                    </div>
+                                )}
+
+                                {dense && actionNode}
+                            </>
+                        );
+                    }
+                }
+            })()}
         </div>
     );
 };
