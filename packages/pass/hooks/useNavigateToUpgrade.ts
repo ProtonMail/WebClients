0314649ef4918c056224f88f005ebfd57c2a3a15
Edvin CANDON <edvin.candon@proton.ch>
2024-10-01 10:44:33 +0200

    Allow passing additional parameters to `useNavigateToUpgrade` hook
    - Passing an email value will override the LocalID parameter
    - Add offer options for promos
    
    [IDTEAM-3928]

--- packages/pass/hooks/useNavigateToUpgrade.ts
+++ packages/pass/hooks/useNavigateToUpgrade.ts
@@ -3,10 +3,17 @@ import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
 import { PASS_UPGRADE_PATH, SAFARI_URL_SCHEME, type UpsellRef, UpsellRefPrefix } from '@proton/pass/constants';
 
 /** `pathRef` will be passed to the upgrade link */
-export const useNavigateToUpgrade = (options: { path?: string; upsellRef: UpsellRef }) => {
+export const useNavigateToUpgrade = (options: {
+    coupon?: string;
+    cycle?: string;
+    path?: string;
+    plan?: string;
+    offer?: boolean;
+    upsellRef: UpsellRef;
+    email?: string;
+}) => {
     const { onLink, config, endpoint } = usePassCore();
     const authStore = useAuthStore();
-
     const searchParams = new URLSearchParams();
 
     const refPrefix: UpsellRefPrefix = (() => {
@@ -19,10 +26,18 @@ export const useNavigateToUpgrade = (options: { path?: string; upsellRef: Upsell
                 return UpsellRefPrefix.Extension;
         }
     })();
+
+    if (options.coupon) searchParams.append('coupon', options.coupon);
+    if (options.cycle) searchParams.append('cycle', options.cycle);
+    if (options.email) searchParams.append('email', options.email);
+    if (options.offer) searchParams.append('offer', 'true');
+    if (options.plan) searchParams.append('plan', options.plan);
     if (options.upsellRef) searchParams.append('ref', `${refPrefix}_${options.upsellRef}`);
 
-    const localID = authStore?.getLocalID();
-    if (localID !== undefined) searchParams.append('u', localID.toString());
+    if (!options.email) {
+        const localID = authStore?.getLocalID();
+        if (localID !== undefined) searchParams.append('u', localID.toString());
+    }
 
     const upgradeHref =
         BUILD_TARGET === 'safari'
