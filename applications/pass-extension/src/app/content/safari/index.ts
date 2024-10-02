840d50e8646ec39fe9810e2f08084271513827e5
Edvin CANDON <edvin.candon@proton.ch>
2024-10-02 10:32:13 +0200

    Add pull fork fallback via content-script for safari extension
    - Addresses safari limitations not attaching cookies from service-worker requests
    - This would block users trying to log during the pull fork call
    
    [IDTEAM-3930]

---
+++ applications/pass-extension/src/app/content/safari/index.ts
@@ -0,0 +1,42 @@
+import * as config from 'proton-pass-extension/app/config';
+
+import { contentScriptMessage, sendMessage } from '@proton/pass/lib/extension/message/send-message';
+import browser from '@proton/pass/lib/globals/browser';
+import type { ApiCallFn } from '@proton/pass/types';
+import { type Maybe, WorkerMessageType, type WorkerMessageWithSender } from '@proton/pass/types';
+import { getErrorMessage } from '@proton/pass/utils/errors/get-error-message';
+import configureApi from '@proton/shared/lib/api';
+import { pullForkSession } from '@proton/shared/lib/api/auth';
+import { getClientID } from '@proton/shared/lib/apps/helper';
+import xhr from '@proton/shared/lib/fetch/fetch';
+
+const api = configureApi({ ...config, clientID: getClientID(config.APP_NAME), xhr } as any) as ApiCallFn;
+
+/** In Safari, there's a known problem with cross-domain cookies in service workers.
+ * As a work-around, we execute the `pullFork` request in the content script instead
+ * of the service worker to ensure that cookies are properly attached. */
+browser.runtime.onMessage.addListener(async (message: Maybe<WorkerMessageWithSender>) => {
+    if (message?.sender === 'background' && message.type === WorkerMessageType.AUTH_PULL_FORK_REQ) {
+        const pullForkParams = pullForkSession(message.payload.selector);
+        pullForkParams.url = `${config.SSO_URL}/api/${pullForkParams.url}`;
+
+        return api(pullForkParams)
+            .then(async (res) => {
+                const data = await res.json();
+                void sendMessage(
+                    contentScriptMessage({
+                        type: WorkerMessageType.AUTH_PULL_FORK_RES,
+                        payload: { ok: true, ...data },
+                    })
+                );
+            })
+            .catch((err) => {
+                void sendMessage(
+                    contentScriptMessage({
+                        type: WorkerMessageType.AUTH_PULL_FORK_RES,
+                        payload: { ok: false, error: getErrorMessage(err) },
+                    })
+                );
+            });
+    }
+});
