81792944913cd9d9acd0004fbe1106efdaffdc9f
Edvin CANDON <edvin.candon@proton.ch>
2024-10-03 16:38:33 +0200

    Add Pass family plan promo modal
    - Requires `pass-family-plan-2024-yearly` offer to be defined in legacy `Offers` feature
    - Requires `OfferPassFamilyPlan2024Yearly` legacy feature
    - Eligible users : pass2023 or free
    
    [IDTEAM-3928]

---
+++ packages/components/containers/offers/components/passFamilyPlan/Layout.tsx
@@ -0,0 +1,134 @@
+import { type FC, useMemo } from 'react';
+
+import { c } from 'ttag';
+
+import { useUser } from '@proton/account/user/hooks';
+import { Button } from '@proton/atoms';
+import { Price } from '@proton/components';
+import type { Props as PriceProps } from '@proton/components/components/price/Price';
+import { usePreferredPlansMap, useSubscription } from '@proton/components/hooks';
+import { COUPON_CODES, PASS_APP_NAME, PLANS, PLAN_NAMES } from '@proton/shared/lib/constants';
+
+import OfferDisableButton from '../../components/shared/OfferDisableButton';
+import OfferLoader from '../../components/shared/OfferLoader';
+import hasOffer from '../../helpers/hasOffer';
+import type { OfferLayoutProps } from '../../interface';
+import img from './upsell_pass-family-plan.png';
+
+import './Layout.scss';
+
+const getPriceJSX = (price: number, props: Omit<PriceProps, 'children'>, key: string) => (
+    <Price key={key} {...props} isDisplayedInSentence>
+        {price}
+    </Price>
+);
+
+const Layout: FC<OfferLayoutProps> = (props) => {
+    const { offer, currency, onSelectDeal } = props;
+    const [user] = useUser();
+    const [subscription] = useSubscription();
+    const { getPlansMap, plansMapLoading } = usePreferredPlansMap();
+
+    const data = useMemo(() => {
+        /** This offer can only be shown to `free` and `pass2023` users
+         * - Cohort 1 : `pass2023` with `passlaunch` offer => PASSEARLYSUPPORTER (299)
+         * - Cohort 2 : `pass2023` => PASSFAMILYLAUNCH (399)
+         * - Cohort 3 : `free` => PASSFAMILYLAUNCH (399) */
+        const isPassLaunch = subscription?.Plans?.some((plan) => plan.Offer === 'passlaunch') ?? false;
+        const coupon = !user.isFree && isPassLaunch ? COUPON_CODES.PASSEARLYSUPPORTER : COUPON_CODES.PASSFAMILYLAUNCH;
+        const userDeal = offer?.deals.find((deal) => deal.couponCode === coupon);
+        const baseDeal = offer?.deals.find((deal) => deal.couponCode === COUPON_CODES.PASSFAMILYLAUNCH);
+
+        if (userDeal && baseDeal) {
+            const { plansMap } = getPlansMap({ paramCurrency: currency });
+            const passPlusYearlyPrice = plansMap[PLANS.PASS]?.Pricing?.[userDeal.cycle];
+            const earlySupporter = userDeal !== baseDeal;
+
+            if (passPlusYearlyPrice) {
+                const ref = user.isFree ? 'pass_family_free_399_webmodal' : userDeal.ref;
+
+                const subtitle = earlySupporter
+                    ? c('PassFamilyPlan2024').t`Exclusive offer available only to ${PASS_APP_NAME} early supporters.`
+                    : c('PassFamilyPlan2024').t`Limited-time deal.`;
+
+                const yearlyBasePrice = earlySupporter ? baseDeal.prices.withCoupon : null;
+                const monthlyBasePrice = yearlyBasePrice ? yearlyBasePrice / userDeal.cycle : null;
+
+                const yearlyPrice = userDeal.prices.withCoupon;
+                const monthlyPrice = yearlyPrice / userDeal.cycle;
+                const yearlySavedPrice = passPlusYearlyPrice * 6 - yearlyPrice;
+
+                return {
+                    deal: { ...userDeal, ref },
+                    subtitle,
+                    yearlyPrice: getPriceJSX(yearlyPrice, { currency }, 'yearly-price'),
+                    yearlySavedPrice: getPriceJSX(yearlySavedPrice, { currency }, 'yearly-saved-price'),
+                    monthlyPrice: getPriceJSX(
+                        monthlyPrice,
+                        { currency, suffix: c('Suffix').t`/month` },
+                        'monthly-price'
+                    ),
+                    monthlyBasePrice: monthlyBasePrice
+                        ? getPriceJSX(
+                              monthlyBasePrice,
+                              { currency, suffix: c('Suffix').t`/month`, className: 'text-strike' },
+                              'monthly-base-price'
+                          )
+                        : null,
+                };
+            }
+        }
+    }, [subscription, user, offer, plansMapLoading]);
+
+    if (!(hasOffer(props) && offer && data)) {
+        return <OfferLoader />;
+    }
+
+    const { deal, monthlyPrice, monthlyBasePrice, subtitle, yearlyPrice, yearlySavedPrice } = data;
+    const passFamilyPlanName = <strong key="plan-name">{PLAN_NAMES[PLANS.PASS_FAMILY]}</strong>;
+    const passPlusPlanName = PLAN_NAMES[PLANS.PASS];
+
+    return (
+        props.offer && (
+            <div className="flex flex-row flex-nowrap py-4">
+                <div className="flex items-center flex-1 text-center lg:text-left">
+                    <header className="flex flex-column gap-3">
+                        <h1 className="block lh120">{c('PassFamilyPlan2024').jt`Introducing ${passFamilyPlanName}`}</h1>
+                        <h4>{c('PassFamilyPlan2024').t`All premium features. 6 users. 1 easy subscription.`}</h4>
+
+                        <div className="lg:inline-flex text-center offer-plan my-4">
+                            <div>
+                                <Button
+                                    className="button button-promotion"
+                                    size="large"
+                                    type="button"
+                                    onClick={() => onSelectDeal(offer, deal, currency)}
+                                >
+                                    <span className="flex flex-nowrap text-ellipsis gap-1">
+                                        {c('PassFamilyPlan2024').jt`Get it for ${monthlyPrice}`}
+                                    </span>
+                                </Button>
+                                {monthlyBasePrice && <div className="text-sm mt-1">{monthlyBasePrice}</div>}
+                            </div>
+                        </div>
+
+                        <h4 className="block text-bold">{subtitle}</h4>
+                        <span>
+                            {c('PassFamilyPlan2024')
+                                .jt`Billed yearly at ${yearlyPrice}. Save ${yearlySavedPrice} with ${passFamilyPlanName} (vs 6 individual ${passPlusPlanName} subscriptions).`}
+                        </span>
+
+                        <div className="w-full lg:w-auto offer-disable-button-container mt-4">
+                            <OfferDisableButton {...props} />
+                        </div>
+                    </header>
+                </div>
+                <div className="offer-right-img-container flex-1 flex">
+                    <img src={img} alt="" className="m-auto" />
+                </div>
+            </div>
+        )
+    );
+};
+
+export default Layout;
---
+++ packages/components/containers/offers/operations/passFamilyPlan2024Yearly/eligibility.ts
@@ -0,0 +1,24 @@
+import { getAppFromPathnameSafe } from '@proton/shared/lib/apps/slugHelper';
+import { APPS, PLANS } from '@proton/shared/lib/constants';
+import { isManagedExternally } from '@proton/shared/lib/helpers/subscription';
+import type { ProtonConfig, Subscription, UserModel } from '@proton/shared/lib/interfaces';
+
+interface Props {
+    subscription?: Subscription;
+    user: UserModel;
+    protonConfig: ProtonConfig;
+}
+
+const isEligible = ({ subscription, user, protonConfig }: Props): boolean => {
+    const parentApp = getAppFromPathnameSafe(window.location.pathname);
+    const hasValidApp = protonConfig.APP_NAME === APPS.PROTONACCOUNT && parentApp === APPS.PROTONPASS;
+
+    const { canPay, isDelinquent, isFree } = user;
+    const notDelinquent = !isDelinquent;
+    const isNotExternal = !isManagedExternally(subscription);
+    const cohortPass2023 = subscription?.Plans?.some((plan) => plan.Name === PLANS.PASS) ?? false;
+
+    return hasValidApp && canPay && notDelinquent && isNotExternal && (isFree || cohortPass2023);
+};
+
+export default isEligible;
