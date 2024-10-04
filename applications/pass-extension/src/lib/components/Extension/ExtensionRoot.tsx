586eceb5def47c815f3dfa84ff64a57fc2c56bf7
Edvin CANDON <edvin.candon@proton.ch>
2024-10-04 16:45:43 +0200

    Allow passing theme override to `ExtensionCore`
    [IDTEAM-1975]

--- applications/pass-extension/src/lib/components/Extension/ExtensionRoot.tsx
+++ applications/pass-extension/src/lib/components/Extension/ExtensionRoot.tsx
@@ -3,6 +3,7 @@ import { type FC, type ReactNode } from 'react';
 import { ModalsChildren, ModalsProvider, NotificationsChildren, NotificationsProvider } from '@proton/components';
 import { Portal } from '@proton/components/components/portal';
 import Icons from '@proton/icons/Icons';
+import type { PassThemeOption } from '@proton/pass/components/Layout/Theme/types';
 import { type ClientEndpoint } from '@proton/pass/types';
 
 import { ExtensionCore } from './ExtensionCore';
@@ -10,11 +11,11 @@ import { ExtensionCore } from './ExtensionCore';
 type Props = {
     endpoint: ClientEndpoint;
     children: ReactNode;
-    darkThemeOnly?: boolean;
+    theme?: PassThemeOption;
 };
 
-export const ExtensionRoot: FC<Props> = ({ endpoint, children, darkThemeOnly }) => (
-    <ExtensionCore endpoint={endpoint} darkThemeOnly={darkThemeOnly}>
+export const ExtensionRoot: FC<Props> = ({ endpoint, children, theme }) => (
+    <ExtensionCore endpoint={endpoint} theme={theme}>
         <Icons />
         <NotificationsProvider>
             <ModalsProvider>
