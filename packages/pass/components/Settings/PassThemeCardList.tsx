7786c04724df34352faec1a5bd0fd7337fb4d13d
Edvin CANDON <edvin.candon@proton.ch>
2024-10-04 17:01:31 +0200

    Fine-tune themed components
    [IDTEAM-1975]

--- packages/pass/components/Settings/PassThemeCardList.tsx
+++ packages/pass/components/Settings/PassThemeCardList.tsx
@@ -3,55 +3,56 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { c } from 'ttag';
 
+import { Info } from '@proton/components';
 import automaticThemeImg from '@proton/pass/assets/settings/theme-automatic.svg';
 import darkThemeImg from '@proton/pass/assets/settings/theme-dark.svg';
 import lightThemeImg from '@proton/pass/assets/settings/theme-light.svg';
 import { PassThemeOption } from '@proton/pass/components/Layout/Theme/types';
+import type { PassThemeCardProps } from '@proton/pass/components/Settings/PassThemeCard';
 import { PassThemeCard } from '@proton/pass/components/Settings/PassThemeCard';
 import { PASS_DEFAULT_THEME } from '@proton/pass/constants';
 import { settingsEditIntent } from '@proton/pass/store/actions';
 import { selectTheme } from '@proton/pass/store/selectors';
+import { PASS_APP_NAME } from '@proton/shared/lib/constants';
 
-const THEME_IMG_SRC_MAP = {
-    [PassThemeOption.OS]: automaticThemeImg,
-    [PassThemeOption.PassDark]: darkThemeImg,
-    [PassThemeOption.PassLight]: lightThemeImg,
-};
-
-const getThemes = () => [
+const getThemeCards = (): PassThemeCardProps[] => [
     {
-        identifier: PassThemeOption.PassDark,
+        theme: PassThemeOption.PassDark,
         label: c('Theme').t`Dark`,
+        src: darkThemeImg,
     },
     {
-        identifier: PassThemeOption.PassLight,
+        theme: PassThemeOption.PassLight,
         label: c('Theme').t`Light`,
+        src: lightThemeImg,
     },
     {
-        identifier: PassThemeOption.OS,
+        theme: PassThemeOption.OS,
         label: c('Theme').t`Automatic`,
+        src: automaticThemeImg,
+        info: (
+            <Info
+                className="color-weak"
+                questionMark
+                title={c('Info').t`${PASS_APP_NAME} will follow your system theme.`}
+            />
+        ),
     },
 ];
 
 export const PassThemeCardList: FC = () => {
     const dispatch = useDispatch();
     const currentTheme = useSelector(selectTheme) ?? PASS_DEFAULT_THEME;
-    const themes = getThemes();
-
-    const onChange = (theme: PassThemeOption) => {
-        dispatch(settingsEditIntent('theme', { theme }, true));
-    };
+    const onChange = (theme: PassThemeOption) => dispatch(settingsEditIntent('theme', { theme }, true));
 
     return (
         <div className="flex gap-4">
-            {themes.map(({ identifier, label }) => (
+            {getThemeCards().map((cardProps) => (
                 <PassThemeCard
-                    key={identifier}
-                    theme={identifier}
-                    selected={currentTheme === identifier}
+                    key={cardProps.theme}
+                    selected={currentTheme === cardProps.theme}
                     onChange={onChange}
-                    imageSrc={THEME_IMG_SRC_MAP[identifier]}
-                    label={label}
+                    {...cardProps}
                 />
             ))}
         </div>
