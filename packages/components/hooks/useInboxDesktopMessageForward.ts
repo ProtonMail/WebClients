599955af364b6a363b02c8969b35e43b3a5f801d
Pablo Molina <contact@pablomolina.me>
2024-10-07 13:39:03 +0200

    fix(INDA-195): move functions to ipcHelpers and preload

--- packages/components/hooks/useInboxDesktopMessageForward.ts
+++ packages/components/hooks/useInboxDesktopMessageForward.ts
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 
-import { addIPCHostUpdateListener } from '@proton/shared/lib/desktop/desktopTypes';
-import { canListenInboxDesktopHostMessages } from '@proton/shared/lib/desktop/ipcHelpers';
+import { addIPCHostUpdateListener, canListenInboxDesktopHostMessages } from '@proton/shared/lib/desktop/ipcHelpers';
 import { captureMessage } from '@proton/shared/lib/helpers/sentry';
 
 export function useInboxDesktopMessageForward() {
