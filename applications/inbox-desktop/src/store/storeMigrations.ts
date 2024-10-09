a7a5b6a9f1e127de9874702f725de1f4fc4814ab
Pablo Molina <contact@pablomolina.me>
2024-10-09 10:09:49 +0200

    chore(INDA-195): migrate serialized theme mode

--- applications/inbox-desktop/src/store/storeMigrations.ts
+++ applications/inbox-desktop/src/store/storeMigrations.ts
@@ -1,4 +1,7 @@
 import Store from "electron-store";
+import { getSettings, saveSettings } from "./settingsStore";
+import { SERIALIZED_THEME_MODE } from "../utils/themes";
+import { electronAppTheme, ThemeModeSetting } from "@proton/shared/lib/themes/themes";
 
 const store = new Store();
 
@@ -19,8 +22,38 @@ const deleteTrialEndStore = () => {
     store.delete("trialEnd");
 };
 
+// Delete settings.theme.mode as we are now saving the full theme
+const deleteSerializedThemeMode = () => {
+    const settings = getSettings();
+
+    if (settings && settings.theme && "mode" in settings.theme) {
+        let Mode = electronAppTheme.Mode;
+
+        if (settings.theme.mode === SERIALIZED_THEME_MODE[ThemeModeSetting.Auto]) {
+            Mode = ThemeModeSetting.Auto;
+        } else if (settings.theme.mode === SERIALIZED_THEME_MODE[ThemeModeSetting.Light]) {
+            Mode = ThemeModeSetting.Light;
+        } else if (settings.theme.mode === SERIALIZED_THEME_MODE[ThemeModeSetting.Dark]) {
+            Mode = ThemeModeSetting.Dark;
+        }
+
+        saveSettings({
+            ...settings,
+            theme: {
+                DarkTheme: settings.theme.DarkTheme,
+                LightTheme: settings.theme.LightTheme,
+                Mode,
+                FontFace: settings.theme.FontFace,
+                FontSize: settings.theme.FontSize,
+                Features: settings.theme.Features,
+            },
+        });
+    }
+};
+
 export const performStoreMigrations = () => {
     deleteWindowStore(); // Introduced in v0.9.4
     deleteURLStore(); // Introduced in v1.0.0
     deleteTrialEndStore(); // Introduced in v1.0.0
+    deleteSerializedThemeMode(); // Introduced in v.1.2.4
 };
