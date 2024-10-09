67bfc5768afa0b291d1b986843352b25d1a3e4cc
Pablo Molina <contact@pablomolina.me>
2024-10-09 10:20:21 +0200

    chore(INDA-195): define IPCInboxHostUpdateMessageSchema

--- applications/inbox-desktop/src/preload.ts
+++ applications/inbox-desktop/src/preload.ts
@@ -1,9 +1,9 @@
 import { contextBridge, ipcRenderer, IpcRendererEvent } from "electron";
-import type {
-    IPCInboxMessageBroker,
-    IPCInboxHostUpdateMessageType,
-    IPCInboxHostUpdateListener,
-    IPCInboxHostUpdateMessage,
+import {
+    type IPCInboxMessageBroker,
+    type IPCInboxHostUpdateMessageType,
+    type IPCInboxHostUpdateListener,
+    IPCInboxHostUpdateMessageSchema,
 } from "@proton/shared/lib/desktop/desktopTypes";
 import Logger from "electron-log";
 
@@ -25,41 +25,10 @@ contextBridge.exposeInMainWorld("ipcInboxMessageBroker", {
     },
 } satisfies IPCInboxMessageBroker);
 
-// THIS NEEDS REFACTOR: inda-refactor-001
-// Either avoid this function completelly or at least implemet it in zod.
-function isValidHostUpdateMessage(
-    data: unknown,
-): { success: false; error: string } | { success: true; data: IPCInboxHostUpdateMessage } {
-    if (!data) {
-        return { success: false, error: "is null" };
-    }
-    if (typeof data !== "object") {
-        return { success: false, error: "not an object" };
-    }
-
-    if (!("type" in data)) {
-        return { success: false, error: "not have type" };
-    }
-
-    if (typeof data.type !== "string") {
-        return { success: false, error: "have non-string type" };
-    }
-
-    if (!("payload" in data)) {
-        return { success: false, error: "not have payload" };
-    }
-
-    const allowedTypes = ["captureMessage", "defaultMailtoChecked"];
-    if (allowedTypes.indexOf(data.type) > -1) {
-        return { success: true, data: data as IPCInboxHostUpdateMessage };
-    }
-
-    return { success: false, error: `unknown type ${data.type}` };
-} // Assuming that broker was added as window object.
-
 function addHostUpdateListener(eventType: IPCInboxHostUpdateMessageType, callback: IPCInboxHostUpdateListener) {
     const handleHostUpdate = (_event: IpcRendererEvent, message: unknown) => {
-        const parsed = isValidHostUpdateMessage(message);
+        const parsed = IPCInboxHostUpdateMessageSchema.safeParse(message);
+
         if (!parsed.success) {
             preloadLogger.error("Invalid host update message format:", parsed.error);
             return;
