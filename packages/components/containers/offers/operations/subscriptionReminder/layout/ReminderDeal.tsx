64738a652d8ccaae692c20145fa582476e774d3c
Germain <germain.souquet@proton.ch>
2024-10-09 17:22:01 +0100

    Fix react key warning

--- packages/components/containers/offers/operations/subscriptionReminder/layout/ReminderDeal.tsx
+++ packages/components/containers/offers/operations/subscriptionReminder/layout/ReminderDeal.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { type IconName } from '@proton/components/components/icon/Icon';
 
 export interface DealItem {
@@ -13,13 +15,13 @@ const ReminderDeal = ({ items }: Props) => {
     return (
         <ul className="unstyled">
             {items.map(({ icon, text }, index) => (
-                <>
-                    <li key={icon} className="border-top items-center border-weak py-4 flex gap-2 flex-nowrap">
+                <React.Fragment key={icon}>
+                    <li className="border-top items-center border-weak py-4 flex gap-2 flex-nowrap">
                         <img src={icon} alt="" className="shrink-0" />
                         <span>{text}</span>
                     </li>
                     {index === items.length - 1 && <div className="border-bottom border-weak" />}
-                </>
+                </React.Fragment>
             ))}
         </ul>
     );
