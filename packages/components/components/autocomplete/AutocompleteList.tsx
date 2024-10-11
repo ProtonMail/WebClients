f89555e04679a429e80aac13c53a262c4ff1555e
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2024-10-11 11:49:14 +0200

    Fix accessibility

--- packages/components/components/autocomplete/AutocompleteList.tsx
+++ packages/components/components/autocomplete/AutocompleteList.tsx
@@ -17,9 +17,21 @@ interface Props<V> {
     highlightedIndex: number;
     anchorRef: RefObject<HTMLElement>;
     dataTestId?: string;
+    /**
+     * In case we want to filter the number of items in the list for accessibility purposes
+     */
+    searchResultsCount?: number;
 }
 
-const AutocompleteList = <V,>({ id, children, onClose, isOpen, highlightedIndex, anchorRef }: Props<V>) => {
+const AutocompleteList = <V,>({
+    id,
+    children,
+    onClose,
+    isOpen,
+    highlightedIndex,
+    anchorRef,
+    searchResultsCount,
+}: Props<V>) => {
     const items = Children.map(children, (child, index) => {
         return cloneElement(child, {
             active: highlightedIndex === index,
@@ -44,9 +56,9 @@ const AutocompleteList = <V,>({ id, children, onClose, isOpen, highlightedIndex,
 
             <div className="sr-only" aria-atomic="true" aria-live="assertive">
                 {c('Hint').ngettext(
-                    msgid`Found ${items.length} suggestion, use keyboard to navigate.`,
-                    `Found ${items.length} suggestions, use keyboard to navigate.`,
-                    items.length
+                    msgid`Found ${searchResultsCount || items.length} suggestion, use keyboard to navigate.`,
+                    `Found ${searchResultsCount || items.length} suggestions, use keyboard to navigate.`,
+                    searchResultsCount || items.length
                 )}
             </div>
 
