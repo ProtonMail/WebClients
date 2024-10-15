47dd5f6a1c6332e876138e7e4fa8cea17cc093c6
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2024-10-15 10:41:43 +0200

    Report web vitals on mail and calendar

--- applications/calendar/src/app/index.tsx
+++ applications/calendar/src/app/index.tsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client';
 import '@proton/polyfill';
 
 import App from './App';
+import { initializePerformanceMetrics } from './helpers/metrics/initializePerformanceMetrics';
 import './style';
+
+initializePerformanceMetrics();
 
 const container = document.querySelector('.app-root');
 const root = createRoot(container!);
