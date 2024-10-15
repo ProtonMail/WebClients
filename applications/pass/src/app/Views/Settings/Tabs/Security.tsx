5d3f3f691755aef62eda60bb9149cdf80bb706d6
Dominik Jankovic <dominik.jankovic@proton.ch>
2024-10-15 14:48:09 +0200

    Clear clipboard settings UI

--- applications/pass/src/app/Views/Settings/Tabs/Security.tsx
+++ applications/pass/src/app/Views/Settings/Tabs/Security.tsx
@@ -1,6 +1,11 @@
 import { type FC } from 'react';
 
+import { ClipboardSettings } from '@proton/pass/components/Settings/ClipboardSettings';
 import { ExtraPassword } from '@proton/pass/components/Settings/ExtraPassword';
 import { LockSettings } from '@proton/pass/components/Settings/LockSettings';
 
-export const Security: FC = () => [<LockSettings key="lock" />, <ExtraPassword key="extra-pwd" />];
+export const Security: FC = () => [
+    <LockSettings key="lock" />,
+    <ExtraPassword key="extra-pwd" />,
+    ...(DESKTOP_BUILD ? [<ClipboardSettings key="clipboard" />] : []),
+];
