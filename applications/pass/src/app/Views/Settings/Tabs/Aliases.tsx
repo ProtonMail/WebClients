f5aa6287505ffa5b2168d6759b1e602b0591d95b
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-10-15 18:09:29 +0200

    Add domain creation modal and table UI
    [IDTEAM-3953]

--- applications/pass/src/app/Views/Settings/Tabs/Aliases.tsx
+++ applications/pass/src/app/Views/Settings/Tabs/Aliases.tsx
@@ -2,9 +2,11 @@ import { type FC } from 'react';
 
 import { AliasSyncToggle } from '@proton/pass/components/Settings/AliasSyncToggle';
 import { AliasMailboxes } from '@proton/pass/components/Settings/Aliases/AliasMailboxes';
+import { Domains } from '@proton/pass/components/Settings/Aliases/Domains';
 
 export const Aliases: FC = () => (
     <>
+        <Domains />
         <AliasMailboxes />
         <AliasSyncToggle />
     </>
---
+++ packages/pass/components/Settings/Aliases/DomainsTable.tsx
@@ -0,0 +1,75 @@
+import { type FC } from 'react';
+import { useSelector } from 'react-redux';
+
+import { c } from 'ttag';
+
+import { Badge, Table, TableBody, TableCell, TableHeader, TableHeaderCell, TableRow } from '@proton/components/index';
+import { DropdownMenuButton } from '@proton/pass/components/Layout/Dropdown/DropdownMenuButton';
+import { QuickActionsDropdown } from '@proton/pass/components/Layout/Dropdown/QuickActionsDropdown';
+import { selectUserPlan } from '@proton/pass/store/selectors';
+import type { CustomDomainOutput } from '@proton/pass/types';
+
+type Props = {
+    domains: CustomDomainOutput[];
+    openModalDNS: (domain: CustomDomainOutput) => void;
+};
+
+const getDomainStatusLabel = ({ OwnershipVerified, MxVerified }: CustomDomainOutput) => {
+    if (!OwnershipVerified) {
+        return c('Title').t`Unverified`;
+    }
+    if (!MxVerified) {
+        return c('Info').t`Unconfigured`;
+    }
+    return null;
+};
+
+export const DomainsTable: FC<Props> = ({ domains, openModalDNS }) => {
+    const canManageAlias = useSelector(selectUserPlan)?.ManageAlias;
+
+    return (
+        <>
+            <Table responsive="cards" hasActions>
+                <TableHeader>
+                    <TableRow>
+                        <TableHeaderCell className="w-1/2">{c('Title').t`Domain`}</TableHeaderCell>
+                        <TableHeaderCell>{c('Title').t`Aliases`}</TableHeaderCell>
+                        <TableHeaderCell>{c('Title').t`Status`}</TableHeaderCell>
+                        <TableHeaderCell>{c('Title').t`Actions`}</TableHeaderCell>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {domains.map((domain) => {
+                        const statusLabel = getDomainStatusLabel(domain);
+                        return (
+                            <TableRow key={domain.ID}>
+                                <TableCell>{domain.Domain}</TableCell>
+                                <TableCell>{domain.AliasCount}</TableCell>
+                                <TableCell>{statusLabel && <Badge type="primary">{statusLabel}</Badge>}</TableCell>
+                                <TableCell>
+                                    <div className="flex justify-end">
+                                        <QuickActionsDropdown
+                                            icon="three-dots-horizontal"
+                                            color="weak"
+                                            shape="solid"
+                                            size="small"
+                                            className="button-xs ui-purple"
+                                            pill={false}
+                                            originalPlacement="bottom-end"
+                                            disabled={!canManageAlias}
+                                        >
+                                            <DropdownMenuButton
+                                                label={c('Action').t`Check DNS`}
+                                                onClick={() => openModalDNS(domain)}
+                                            />
+                                        </QuickActionsDropdown>
+                                    </div>
+                                </TableCell>
+                            </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
+        </>
+    );
+};
