5aaa72da2afec75c6f7a52469cd0bcc9a76e77d2
Lucien Peslier <lucien.peslier@proton.ch>
2024-10-17 12:43:57 +0000

    [DRVWEB-4278] Add action on User info public page to allow account switch

--- applications/drive/src/app/components/SharedPage/Layout/UserInfo.tsx
+++ applications/drive/src/app/components/SharedPage/Layout/UserInfo.tsx
@@ -1,5 +1,14 @@
+import { Button } from '@proton/atoms/index';
+import { getAppHref } from '@proton/shared/lib/apps/helper';
+import { APPS, SSO_PATHS } from '@proton/shared/lib/constants';
+import { replaceUrl } from '@proton/shared/lib/helpers/browser';
 import { getInitials } from '@proton/shared/lib/helpers/string';
+import { getUrlWithReturnUrl } from '@proton/shared/lib/helpers/url';
 import type { UserModel } from '@proton/shared/lib/interfaces';
+
+import usePublicToken from '../../../hooks/drive/usePublicToken';
+import { RedirectionReason, drivePublicRedirectionReasonKey } from '../../../hooks/util/useRedirectToPublicPage';
+import { saveUrlPasswordForRedirection } from '../../../utils/url/password';
 
 import './UserInfo.scss';
 
@@ -13,9 +22,32 @@ export const UserInfo = ({ user }: Props) => {
     const { Email, DisplayName, Name } = user || {};
     const nameToDisplay = DisplayName || Name || ''; // nameToDisplay can be falsy for external account
     const initials = getInitials(nameToDisplay || Email || '');
+    const { token, urlPassword } = usePublicToken();
+
+    const accountSwitchUrl = new URL(getAppHref(SSO_PATHS.SWITCH, APPS.PROTONACCOUNT));
+    accountSwitchUrl.searchParams.append('product', 'drive');
+    const returnUrlSearchParams = new URLSearchParams();
+    returnUrlSearchParams.append('token', token);
+    // We need to pass by the private app to set latest active session, then be redirected to public page.
+    // This will be done in MainContainer.tsx on page loading
+    returnUrlSearchParams.append(drivePublicRedirectionReasonKey, RedirectionReason.ACCOUNT_SWITCH);
+    const returnUrl = `/?`.concat(returnUrlSearchParams.toString());
+    const urlWithReturnUrl = getUrlWithReturnUrl(accountSwitchUrl.toString(), {
+        returnUrl: returnUrl,
+        context: 'private',
+    });
 
     return (
-        <div className="max-w-full flex items-center flex-nowrap gap-3 user-info relative interactive-pseudo-protrude rounded interactive--no-background">
+        <Button
+            onClick={() => {
+                // Save password before going to account switch page
+                saveUrlPasswordForRedirection(urlPassword);
+                // We replace the url to prevent any bad action from the user,
+                // like returning back into the history after signout all sessions
+                replaceUrl(urlWithReturnUrl);
+            }}
+            className="user-info border-none max-w-full flex items-center flex-nowrap gap-3 user-info relative interactive-pseudo-protrude rounded interactive--no-background"
+        >
             {nameToDisplay ? (
                 <span className="flex-1 lh130 text-right hidden md:inline">
                     <span className="block text-ellipsis text-sm">{nameToDisplay}</span>
@@ -36,6 +68,6 @@ export const UserInfo = ({ user }: Props) => {
             >
                 <span className="m-auto">{initials}</span>
             </span>
-        </div>
+        </Button>
     );
 };
