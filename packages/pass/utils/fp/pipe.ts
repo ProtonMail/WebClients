94f18f4b91911d7a860a9ca138c12b9bbe8dc99b
Edvin CANDON <edvin.candon@proton.ch>
2024-10-18 11:36:23 +0200

    Patch `pipe` type definition

--- packages/pass/utils/fp/pipe.ts
+++ packages/pass/utils/fp/pipe.ts
@@ -1,6 +1,46 @@
-import { default as pipe } from 'lodash/flow';
+import { default as flow } from 'lodash/flow';
 
-export { default as pipe } from 'lodash/flow';
+/** The default `flow` type is unsafe, allowing invalid compositions due to:
+ * `flow(...func: Array<Many<(...args: any[]) => any>>): (...args: any[]) => any`
+ * This can cause runtime errors TypeScript can't catch. We define a stricter
+ * variadic 'Pipe' interface to address this issue. */
+interface Pipe {
+    <A extends any[], R1, R2, R3, R4, R5, R6, R7>(
+        f1: (...args: A) => R1,
+        f2: (a: R1) => R2,
+        f3: (a: R2) => R3,
+        f4: (a: R3) => R4,
+        f5: (a: R4) => R5,
+        f6: (a: R5) => R6,
+        f7: (a: R6) => R7
+    ): (...args: A) => R7;
+    <A extends any[], R1, R2, R3, R4, R5, R6>(
+        f1: (...args: A) => R1,
+        f2: (a: R1) => R2,
+        f3: (a: R2) => R3,
+        f4: (a: R3) => R4,
+        f5: (a: R4) => R5,
+        f6: (a: R5) => R6
+    ): (...args: A) => R6;
+    <A extends any[], R1, R2, R3, R4, R5>(
+        f1: (...args: A) => R1,
+        f2: (a: R1) => R2,
+        f3: (a: R2) => R3,
+        f4: (a: R3) => R4,
+        f5: (a: R4) => R5
+    ): (...args: A) => R5;
+    <A extends any[], R1, R2, R3, R4>(
+        f1: (...args: A) => R1,
+        f2: (a: R1) => R2,
+        f3: (a: R2) => R3,
+        f4: (a: R3) => R4
+    ): (...args: A) => R4;
+    <A extends any[], R1, R2, R3>(f1: (...args: A) => R1, f2: (a: R1) => R2, f3: (a: R2) => R3): (...args: A) => R3;
+    <A extends any[], R1, R2>(f1: (...args: A) => R1, f2: (a: R1) => R2): (...args: A) => R2;
+    <A extends any[], R1>(f1: (...args: A) => R1): (...args: A) => R1;
+}
+
+export const pipe = flow as Pipe;
 
 export const tap =
     <A, P = A extends Promise<infer U> ? U : A>(effect: (arg: P) => void) =>
