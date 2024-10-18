dce0fd61bee3701f933d1df34ec939c5dd942e3b
Pablo Molina <contact@pablomolina.me>
2024-10-18 09:27:52 +0200

    chore(INDA-329): add sentry for electron

---
+++ applications/inbox-desktop/src/utils/telemetry.ts
@@ -0,0 +1,78 @@
+import { captureMessage, init, setTag, setTags, setUser, SeverityLevel } from "@sentry/electron/main";
+import { appSession, updateSession } from "./session";
+import pkg from "../../package.json";
+import { getConfig } from "./config";
+import { app } from "electron";
+import { getAppID } from "../store/idStore";
+import { DESKTOP_FEATURES } from "../ipc/ipcConstants";
+import { isLinux, isMac, isWindows } from "./helpers";
+import { LogMessage, Transport } from "electron-log";
+
+const MAX_TAG_LENGTH = 32;
+
+const LOG_LEVEL_TO_SEVERITY: Record<LogMessage["level"], SeverityLevel> = {
+    error: "error",
+    warn: "warning",
+    info: "info",
+    debug: "debug",
+    verbose: "debug",
+    silly: "debug",
+};
+
+export async function initializeTelemetry() {
+    const appID = await getAppID();
+    const config = getConfig();
+    const environment = new URL(config.url.mail).hostname.replace(/^mail./i, "");
+    const release = `${pkg.name}@${pkg.version}+${app.isPackaged ? "packaged" : "unpackaged"}`;
+
+    init({
+        debug: !app.isPackaged,
+        enabled: true,
+        dsn: "https://8c45c7615ac54aed9a6747430e0a5d8e@mail.proton.me/api/core/v4/reports/sentry/54",
+        getSessions: () => [appSession(), updateSession()],
+        maxBreadcrumbs: 100,
+        attachStacktrace: true,
+        autoSessionTracking: true,
+        environment,
+        release,
+    });
+
+    setUser({
+        id: appID.id,
+    });
+
+    setTags({
+        "appID.distribution": appID.distribution,
+        "appID.hash": appID.hash,
+        "os.isMac": isMac,
+        "os.isWindows": isWindows,
+        "os.isLinux": isLinux,
+    });
+
+    for (const [feature, enabled] of Object.entries(DESKTOP_FEATURES)) {
+        const tagName = `flag.${feature}`.substring(0, MAX_TAG_LENGTH);
+        setTag(tagName, enabled);
+    }
+}
+
+export function reportToTelemetry(message: unknown, level: SeverityLevel = "info") {
+    captureMessage(serialize(message), { level });
+}
+
+function serialize(value: unknown): string {
+    if (typeof value === "string") {
+        return value;
+    } else if (typeof value === "number") {
+        return value.toString();
+    } else if (Array.isArray(value)) {
+        return value.map((chunk) => serialize(chunk)).join(" ");
+    }
+
+    return JSON.stringify(value);
+}
+
+export const sentryTransport: Transport = ({ data, level }: LogMessage) => {
+    reportToTelemetry(data, LOG_LEVEL_TO_SEVERITY[level]);
+};
+sentryTransport.level = "info";
+sentryTransport.transforms = [];
