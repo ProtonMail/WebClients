2aa4923adae6c8eaf5ad9d9aa9750c36efec248b
Pablo Molina <contact@pablomolina.me>
2024-10-21 13:32:10 +0200

    chore(INDA-329): jest setup

--- applications/inbox-desktop/src/utils/testUtils.ts
+++ applications/inbox-desktop/src/utils/testUtils.ts
@@ -1,8 +1,25 @@
-export interface Singleton<T> {
+import Store from "electron-store";
+import { WindowBounds } from "../store/boundsStore";
+
+interface Singleton<T> {
     INSTANCE: T;
 }
 
-export function getSingleton<T extends Record<string, unknown>>(getInstance: () => T) {
+jest.mock("electron", () => ({
+    screen: {
+        getDisplayNearestPoint: jest.fn(),
+        getCursorScreenPoint: () => {},
+    },
+}));
+
+jest.mock("electron-store", () =>
+    getSingleton<(typeof MockedStore)["INSTANCE"]>(() => ({
+        get: jest.fn(),
+        set: jest.fn(),
+    })),
+);
+
+function getSingleton<T extends Record<string, unknown>>(getInstance: () => T) {
     return class SingletonImpl {
         static INSTANCE: T;
 
@@ -16,3 +33,8 @@ export function getSingleton<T extends Record<string, unknown>>(getInstance: () 
         }
     };
 }
+
+export const MockedStore = Store as unknown as Singleton<{
+    get: jest.MockedFn<() => WindowBounds>;
+    set: () => void;
+}>;
