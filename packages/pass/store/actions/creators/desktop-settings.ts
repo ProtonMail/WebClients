3a826588eca3ccdfb98e18f4bdf26c97a649b386
Dominik Jankovic <dominik.jankovic@proton.ch>
2024-10-22 14:23:21 +0100

    Notify on desktop settings changes

--- packages/pass/store/actions/creators/desktop-settings.ts
+++ packages/pass/store/actions/creators/desktop-settings.ts
@@ -1,4 +1,7 @@
+import { c } from 'ttag';
+
 import type { DesktopSettingsDTO } from '@proton/pass/lib/desktop-settings/types';
+import { withNotification } from '@proton/pass/store/actions/enhancers/notification';
 import { desktopSettingsSetRequest, desktopSettingsSyncRequest } from '@proton/pass/store/actions/requests';
 import type { DesktopSettingsState } from '@proton/pass/store/reducers/desktop-settings';
 import { requestActionsFactory } from '@proton/pass/store/request/flow';
@@ -7,6 +10,21 @@ export const setDesktopSettings = requestActionsFactory<DesktopSettingsDTO, Desk
     'desktop-settings::set'
 )({
     requestId: desktopSettingsSetRequest,
+    success: {
+        prepare: (payload) =>
+            withNotification({
+                type: 'success',
+                text: c('Info').t`Settings successfully updated`,
+            })({ payload }),
+    },
+    failure: {
+        prepare: (error, payload) =>
+            withNotification({
+                type: 'error',
+                text: c('Error').t`Could not update settings at the moment. Please try again later.`,
+                error: error,
+            })({ payload }),
+    },
 });
 
 export const syncDesktopSettings = requestActionsFactory<void, DesktopSettingsState>('desktop-settings::sync')({
