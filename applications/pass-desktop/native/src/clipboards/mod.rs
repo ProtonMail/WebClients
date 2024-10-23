4800c5bde9eb9e71508422224c40d0ed34905c57
Edvin CANDON <edvin.candon@proton.ch>
2024-10-23 08:00:57 +0200

    Fix cargo clippy warnings
    [IDTEAM-3541]
    [IDTEAM-1884]

--- applications/pass-desktop/native/src/clipboards/mod.rs
+++ applications/pass-desktop/native/src/clipboards/mod.rs
@@ -3,11 +3,11 @@ use anyhow::Result;
 #[cfg_attr(target_os = "windows", path = "windows.rs")]
 #[cfg_attr(target_os = "macos", path = "macos.rs")]
 #[cfg_attr(target_os = "linux", path = "linux.rs")]
-mod clipboards;
+mod clipboard_platform;
 
 pub trait ClipboardTrait {
     fn write(text: &str, sensitive: bool) -> Result<(), anyhow::Error>;
     fn read() -> Result<String, anyhow::Error>;
 }
 
-pub use clipboards::*;
+pub use clipboard_platform::*;
