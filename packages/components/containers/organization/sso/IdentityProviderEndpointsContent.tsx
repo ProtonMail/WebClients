d6b78ee6f8b692740e6cdf8954ca7ebc6abe2984
Mattias Svanström <mmso@mmso.se>
2024-10-23 12:19:10 +0000

    Generalize SSO configuration texts

--- packages/components/containers/organization/sso/IdentityProviderEndpointsContent.tsx
+++ packages/components/containers/organization/sso/IdentityProviderEndpointsContent.tsx
@@ -1,7 +1,7 @@
 import { c } from 'ttag';
 
 import { Href } from '@proton/atoms';
-import { VPN_APP_NAME } from '@proton/shared/lib/constants';
+import { BRAND_NAME } from '@proton/shared/lib/constants';
 
 import ReadonlyFieldWithCopy from './ReadonlyFieldWithCopy';
 
@@ -10,18 +10,18 @@ export interface IdentityProviderEndpointsContentProps {
     callbackURL: string;
 }
 
-const IdentityProviderEndpointsContent = ({ issuerID, callbackURL }: IdentityProviderEndpointsContentProps) => {
-    // Using a function here so that we can extend to other applications without requiring retranslation of this string
-    const getDescriptionString = (appName: typeof VPN_APP_NAME) => {
-        // translator: variable here is an application name. Example full sentence "When enabling SAML for Proton VPN in your identity provider, you might be prompted to use the callback (ACS) URL and issuer ID. Just copy-paste the data below into your identity provider fields."
-        return c('Info')
-            .t`When enabling SAML for ${appName} in your identity provider, you might be prompted to use the callback (ACS) URL and issuer ID. Just copy-paste the data below into your identity provider fields.`;
-    };
+// Using a function here so that we can extend to other applications without requiring retranslation of this string
+const getDescriptionString = (appName: string) => {
+    // translator: variable here is an application name. Example full sentence "When enabling SAML for Proton VPN in your identity provider, you might be prompted to use the callback (ACS) URL and issuer ID. Just copy-paste the data below into your identity provider fields."
+    return c('Info')
+        .t`When enabling SAML for ${appName} in your identity provider, you might be prompted to use the callback (ACS) URL and issuer ID. Just copy-paste the data below into your identity provider fields.`;
+};
 
+const IdentityProviderEndpointsContent = ({ issuerID, callbackURL }: IdentityProviderEndpointsContentProps) => {
     return (
         <div className="flex flex-column gap-4">
             <div>
-                {getDescriptionString(VPN_APP_NAME)}
+                {getDescriptionString(BRAND_NAME)}
                 <br />
                 <Href href="https://protonvpn.com/support/sso">{c('Info').t`Learn more`}</Href>
             </div>
