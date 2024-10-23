069d1c63eb6a622ed51dc80ca826587f64ad80de
Edvin CANDON <edvin.candon@proton.ch>
2024-10-23 08:00:57 +0200

    Fix cargo clippy warnings
    [IDTEAM-3541]
    [IDTEAM-1884]

--- applications/pass-desktop/native/src/clipboards/macos.rs
+++ applications/pass-desktop/native/src/clipboards/macos.rs
@@ -1,13 +1,13 @@
+use super::ClipboardTrait;
 use anyhow::Result;
-
 pub struct Clipboard {}
 
-impl super::ClipboardTrait for Clipboard { 
+impl ClipboardTrait for Clipboard {
     fn read() -> Result<String, anyhow::Error> {
         let mut clipboard = arboard::Clipboard::new()?;
         clipboard.get_text().map_err(|e| e.into())
     }
-    
+
     fn write(text: &str, _: bool) -> Result<(), anyhow::Error> {
         let mut clipboard = arboard::Clipboard::new()?;
         let set = clipboard.set();
