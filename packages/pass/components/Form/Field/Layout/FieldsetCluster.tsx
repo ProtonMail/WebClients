4f0ce9fad623aa3c63a91c221f8193ef66c85fee
Alejandro Tidele <alejandro.tidele@proton.ch>
2024-10-24 14:16:23 +0200

    Split the Autosave components in two and optimise the height when updating candidates
    [IDTEAM-3385]

--- packages/pass/components/Form/Field/Layout/FieldsetCluster.tsx
+++ packages/pass/components/Form/Field/Layout/FieldsetCluster.tsx
@@ -10,18 +10,15 @@ type Props = PropsWithChildren<{
     mode?: 'read' | 'write';
 }>;
 
-export const FieldsetCluster: FC<Props> = ({ as = 'fieldset', children, className, mode = 'write' }) => {
-    const Component = as;
-    return (
-        <Component
-            className={clsx(
-                'pass-fieldset-cluster max-w-full border-none p-0 m-0 mb-2',
-                mode === 'write' ? 'pass-fieldset-cluster--write' : 'pass-fieldset-cluster--read',
-                className
-            )}
-            style={mode === 'write' ? { background: 'var(--field-norm)' } : undefined}
-        >
-            {children}
-        </Component>
-    );
-};
+export const FieldsetCluster: FC<Props> = ({ as: Component = 'fieldset', children, className, mode = 'write' }) => (
+    <Component
+        className={clsx(
+            'pass-fieldset-cluster max-w-full border-none p-0 m-0 rounded-lg mb-2',
+            mode === 'write' ? 'pass-fieldset-cluster--write' : 'pass-fieldset-cluster--read',
+            className
+        )}
+        style={mode === 'write' ? { background: 'var(--field-norm)' } : undefined}
+    >
+        {children}
+    </Component>
+);
