548314ada56325717ea53110d2b0d96c4574c885
Mattias Svanström <mattias.svanstrom@proton.ch>
2024-10-24 16:59:41 +0200

    Prevent unleash from blocking render

--- packages/components/containers/publicAppSetup/PublicAppSetup.tsx
+++ packages/components/containers/publicAppSetup/PublicAppSetup.tsx
@@ -1,10 +1,6 @@
 import type { ReactNode } from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-import useApi from '@proton/components/hooks/useApi';
-import { getInboxDesktopInfo, hasInboxDesktopFeature } from '@proton/shared/lib/desktop/ipcHelpers';
-import { reportClientLaunch } from '@proton/shared/lib/desktop/reportClientLaunch';
-import { isElectronMail } from '@proton/shared/lib/helpers/desktop';
 import { useFlagsReady } from '@proton/unleash';
 
 interface Props {
@@ -13,21 +9,15 @@ interface Props {
 }
 
 const PublicAppSetup = ({ loader, children }: Props) => {
-    const flagsReadyPromise = useFlagsReady();
-    const [loaded, setLoaded] = useState(false);
     const api = useApi();
 
     useEffect(() => {
-        void flagsReadyPromise.finally(() => {
-            setLoaded(true);
-        });
-
         if (isElectronMail && hasInboxDesktopFeature('InstallSource')) {
             void reportClientLaunch(getInboxDesktopInfo('installSource'), 'mail', api);
         }
     }, []);
 
-    return <>{loaded ? children : loader}</>;
+    return children;
 };
 
 export default PublicAppSetup;
