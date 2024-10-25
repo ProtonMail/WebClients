3d9432cf532804da1041d652f6aee3f09fcabe18
larabr <7375870+larabr@users.noreply.github.com>
2024-10-25 16:55:42 +0200

    TimeOutOfSyncTopBanner: do not warn on stale server time update
    The server time update might be stale if e.g. the device has been asleep/idle,
    and it's processing the update after a delay.
    The event loop runs every 30s, so we expect the server time to be updated at
    least with that frequency (with a margin of a few seconds)

--- packages/components/containers/topBanners/TimeOutOfSyncTopBanner.tsx
+++ packages/components/containers/topBanners/TimeOutOfSyncTopBanner.tsx
@@ -3,27 +3,45 @@ import { useEffect, useRef, useState } from 'react';
 import { c } from 'ttag';
 
 import { Href } from '@proton/atoms';
-import { HOUR } from '@proton/shared/lib/constants';
+import { HOUR, SECOND } from '@proton/shared/lib/constants';
 import { captureMessage } from '@proton/shared/lib/helpers/sentry';
 import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
 
 import useApiServerTime from '../../hooks/useApiServerTime';
 import TopBanner from './TopBanner';
 
-const isOutOfSync = (serverTime: Date) => {
-    const timeDifference = Math.abs(serverTime.getTime() - Date.now());
+const isOutOfSync = (serverTime: Date, localTime: Date) => {
+    const timeDifference = Math.abs(serverTime.getTime() - localTime.getTime());
     // We should allow at least a 14-hour time difference,
     // because of potential internal clock issues when using dual-boot with Windows and a different OS
     return timeDifference > 24 * HOUR;
 };
 
+/**
+ * The serverTime update might be stale if e.g. the device has been asleep/idle, and it's processing the update after a delay.
+ */
+const isStaleServerTimeUpdate = (previousUpdateLocalTime: Date, localTime: Date) => {
+    const timeDifference = Math.abs(previousUpdateLocalTime.getTime() - localTime.getTime());
+    // The event loop runs every 30s, so we expect the server time to be updated at least with that frequency
+    // (with a margin of a few seconds)
+    return timeDifference > 35 * SECOND;
+};
+
 const TimeOutOfSyncTopBanner = () => {
     const [ignore, setIgnore] = useState(false);
+    const previousUpdateLocalTime = useRef(new Date());
+
     const serverTime = useApiServerTime();
+    const currentUpdateLocalTime = new Date();
+    const isStaleServerTime = isStaleServerTimeUpdate(previousUpdateLocalTime.current, currentUpdateLocalTime);
+
+    useEffect(() => {
+        previousUpdateLocalTime.current = currentUpdateLocalTime;
+    });
 
     // We warn the user if the server time is too far off from local time.
     // We do not want the server to set arbitrary times (either past or future), to avoid signature replay issues and more.
-    const showWarning = !ignore && serverTime && isOutOfSync(serverTime);
+    const showWarning = !ignore && serverTime && !isStaleServerTime && isOutOfSync(serverTime, currentUpdateLocalTime);
 
     // Log warning to have an idea of how many clients might be affected
     const onceRef = useRef(false);
@@ -37,7 +55,8 @@ const TimeOutOfSyncTopBanner = () => {
             level: 'info',
             extra: {
                 serverTime: serverTime?.toString(),
-                localTime: new Date().toString(),
+                localTime: currentUpdateLocalTime.toString(),
+                isStaleServerTime,
             },
         });
     }, [showWarning]);
