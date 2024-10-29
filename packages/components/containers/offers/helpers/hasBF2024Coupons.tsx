84e97aaceed00daeedd2a36da9838e7fbb1318cd
Mattias Svanström <mattias.svanstrom@proton.ch>
2024-10-29 11:55:14 +0100

    Check more BF2024 coupons

--- packages/components/containers/offers/helpers/hasBF2024Coupons.tsx
+++ packages/components/containers/offers/helpers/hasBF2024Coupons.tsx
@@ -1,11 +1,10 @@
-import { COUPON_CODES } from '@proton/shared/lib/constants';
-import { getHasCoupon } from '@proton/shared/lib/helpers/subscription';
+import { getHas2024OfferCoupon } from '@proton/shared/lib/helpers/subscription';
 import type { Subscription } from '@proton/shared/lib/interfaces';
 
 const hasOneBF2024Coupon = (subscription?: Subscription) => {
     return (
-        getHasCoupon(subscription, COUPON_CODES.BLACK_FRIDAY_2024) ||
-        getHasCoupon(subscription, COUPON_CODES.BLACK_FRIDAY_2024_MONTH)
+        getHas2024OfferCoupon(subscription?.CouponCode) ||
+        getHas2024OfferCoupon(subscription?.UpcomingSubscription?.CouponCode)
     );
 };
 
