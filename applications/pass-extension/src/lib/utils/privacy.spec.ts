2cced9f652497515f4b0f8e3904c420da5480e02
Edvin CANDON <edvin.candon@proton.ch>
2024-10-29 23:16:03 +0100

    Make `privacy` permission optional
    - Check & request permission accordingly
    - Auto-reload page if privacy API is unavailable after accepting
    - Update unit-tests
    
    [IDTEAM-4060]

--- applications/pass-extension/src/lib/utils/privacy.spec.ts
+++ applications/pass-extension/src/lib/utils/privacy.spec.ts
@@ -5,6 +5,12 @@ import * as privacy from './privacy';
 const setBuildTarget = (value: string) => ((global as any).BUILD_TARGET = value);
 
 describe('Privacy', () => {
+    beforeEach(() => {
+        setBuildTarget('chrome');
+        browser.permissions.contains.mockClear();
+        browser.permissions.request.mockClear();
+    });
+
     describe('`BROWSER_AUTOFILL_SETTINGS`', () => {
         test('should return correct settings for chrome', () => {
             setBuildTarget('chrome');
@@ -23,7 +29,7 @@ describe('Privacy', () => {
     });
 
     describe('`settingControlledAndDisabled`', () => {
-        test('should return true when controlled by extension and value is false', () => {
+        test('should return `true` when controlled by extension and value is false', () => {
             expect(
                 privacy.settingControlledAndDisabled({
                     levelOfControl: 'controlled_by_this_extension',
@@ -32,7 +38,7 @@ describe('Privacy', () => {
             ).toBe(true);
         });
 
-        test('should return false when controlled by extension but value is true', () => {
+        test('should return `false` when controlled by extension but value is true', () => {
             expect(
                 privacy.settingControlledAndDisabled({
                     levelOfControl: 'controlled_by_this_extension',
@@ -41,7 +47,7 @@ describe('Privacy', () => {
             ).toBe(false);
         });
 
-        test('should return false when not controlled by extension', () => {
+        test('should return `false` when not controlled by extension', () => {
             expect(
                 privacy.settingControlledAndDisabled({
                     levelOfControl: 'not_controllable',
@@ -59,8 +65,14 @@ describe('Privacy', () => {
             expect(await privacy.checkBrowserAutofillCapabilities()).toBe(false);
         });
 
+        test('should return `false` if "privacy" permission not granted', async () => {
+            privacy.BROWSER_AUTOFILL_SETTINGS.push(...privacy.getBrowserAutofillSettings());
+            browser.permissions.contains.mockResolvedValueOnce(false);
+
+            expect(await privacy.checkBrowserAutofillCapabilities()).toBe(false);
+        });
+
         test('should return `true` when all settings are controlled and disabled', async () => {
-            setBuildTarget('chrome');
             privacy.BROWSER_AUTOFILL_SETTINGS.push(...privacy.getBrowserAutofillSettings());
 
             const details = { levelOfControl: 'controlled_by_this_extension', value: false };
@@ -71,7 +83,6 @@ describe('Privacy', () => {
         });
 
         test('should return `false` when any setting is not controlled or enabled', async () => {
-            setBuildTarget('chrome');
             privacy.BROWSER_AUTOFILL_SETTINGS.push(...privacy.getBrowserAutofillSettings());
 
             const detailsActive = { levelOfControl: 'controlled_by_this_extension', value: false };
@@ -84,8 +95,16 @@ describe('Privacy', () => {
     });
 
     describe('`setBrowserAutofillCapabilities`', () => {
+        /** Keep a reference for tests mocking `undefined`
+         * browser.privacy API after granting permission */
+        const privacyAPI = browser.privacy;
+        const reload = jest.fn();
+        jest.spyOn(window, 'location', 'get').mockImplementation(() => ({ reload }) as unknown as Location);
+
+        afterAll(() => (window.location = location));
+        beforeEach(() => (browser.privacy = privacyAPI));
+
         test('should set all settings and return the input value', async () => {
-            setBuildTarget('chrome');
             privacy.BROWSER_AUTOFILL_SETTINGS.push(...privacy.getBrowserAutofillSettings());
 
             const result = await privacy.setBrowserAutofillCapabilities(true);
@@ -94,8 +113,39 @@ describe('Privacy', () => {
             expect(result).toBe(true);
         });
 
+        test('should set settings after granting permission', async () => {
+            privacy.BROWSER_AUTOFILL_SETTINGS.push(...privacy.getBrowserAutofillSettings());
+
+            const result = await privacy.setBrowserAutofillCapabilities(true);
+            expect(browser.privacy.services.autofillAddressEnabled.set).toHaveBeenCalledWith({ value: false });
+            expect(browser.privacy.services.passwordSavingEnabled.set).toHaveBeenCalledWith({ value: false });
+            expect(result).toBe(true);
+        });
+
+        test('should early return if "privacy" permission not granted and user rejects', async () => {
+            privacy.BROWSER_AUTOFILL_SETTINGS.push(...privacy.getBrowserAutofillSettings());
+            browser.permissions.contains.mockResolvedValueOnce(false);
+            browser.permissions.request.mockResolvedValueOnce(false);
+            const result = await privacy.setBrowserAutofillCapabilities(true);
+
+            expect(browser.privacy.services.autofillAddressEnabled.set).toHaveBeenCalledWith({ value: false });
+            expect(browser.privacy.services.passwordSavingEnabled.set).toHaveBeenCalledWith({ value: false });
+            expect(result).toBe(false);
+        });
+
+        test('should reload window if "privacy" permission not granted, user accepts but API unavailable', async () => {
+            delete (browser as any).privacy; /* mock API not available */
+
+            privacy.BROWSER_AUTOFILL_SETTINGS.push(...privacy.getBrowserAutofillSettings());
+            browser.permissions.contains.mockResolvedValueOnce(false);
+            browser.permissions.request.mockResolvedValueOnce(true);
+            const result = await privacy.setBrowserAutofillCapabilities(true);
+
+            expect(result).toBe(false);
+            expect(reload).toHaveBeenCalled();
+        });
+
         test('should handle errors gracefully', async () => {
-            setBuildTarget('chrome');
             privacy.BROWSER_AUTOFILL_SETTINGS.push(...privacy.getBrowserAutofillSettings());
             (browser.privacy.services.autofillAddressEnabled.set as jest.Mock).mockRejectedValue(new Error());
 
