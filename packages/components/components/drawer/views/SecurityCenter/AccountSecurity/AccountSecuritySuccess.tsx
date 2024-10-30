8d881824c83c3144362ab3b2a9f0a40c4e6e2100
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-10-30 15:34:30 +0000

    Update mail placeholders

--- packages/components/components/drawer/views/SecurityCenter/AccountSecurity/AccountSecuritySuccess.tsx
+++ packages/components/components/drawer/views/SecurityCenter/AccountSecurity/AccountSecuritySuccess.tsx
@@ -1,8 +1,10 @@
 import { c } from 'ttag';
 
 import { useTheme } from '@proton/components/containers/themes/ThemeProvider';
-import imgSuccessDarkTheme from '@proton/styles/assets/img/illustrations/account-security-success-dark.svg';
-import imgSuccessWhiteTheme from '@proton/styles/assets/img/illustrations/account-security-success-white.svg';
+import { getPlaceholderSrc } from '@proton/mail';
+import imgSuccessDark from '@proton/styles/assets/img/placeholders/lock-cool-dark.svg';
+import imgSuccessLight from '@proton/styles/assets/img/placeholders/lock-cool-light.svg';
+import imgSuccessWarm from '@proton/styles/assets/img/placeholders/lock-warm-light.svg';
 
 interface Props {
     twoFactorAuthSet: boolean;
@@ -10,11 +12,19 @@ interface Props {
 
 const AccountSecuritySuccess = ({ twoFactorAuthSet }: Props) => {
     const theme = useTheme();
-    const { dark } = theme.information;
-
     return (
         <div className="text-center pt-2">
-            <img src={dark ? imgSuccessDarkTheme : imgSuccessWhiteTheme} alt="" />
+            <img
+                height={128}
+                className="w-auto"
+                src={getPlaceholderSrc({
+                    theme: theme.information.theme,
+                    warmLight: imgSuccessWarm,
+                    coolLight: imgSuccessLight,
+                    coolDark: imgSuccessDark,
+                })}
+                alt=""
+            />
             <p className="mt-2 mb-1">{c('Title').t`Your account is protected`}</p>
             <p className="m-0 text-sm color-weak">
                 {twoFactorAuthSet
