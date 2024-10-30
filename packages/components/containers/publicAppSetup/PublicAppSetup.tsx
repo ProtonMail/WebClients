65f0a2c27f5ed4c6d9869cd477aca71660270972
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2024-10-30 09:56:37 +0000

    Merge branch 'feat/inda-314-report-install-source' into 'main'
    feat(INDA-314): use public app to report install source.
    
    See merge request web/clients!13693

--- packages/components/containers/publicAppSetup/PublicAppSetup.tsx
+++ packages/components/containers/publicAppSetup/PublicAppSetup.tsx
@@ -1,6 +1,10 @@
 import type { ReactNode } from 'react';
 import { useEffect, useState } from 'react';
 
+import useApi from '@proton/components/hooks/useApi';
+import { getInboxDesktopInfo, hasInboxDesktopFeature } from '@proton/shared/lib/desktop/ipcHelpers';
+import { reportClientLaunch } from '@proton/shared/lib/desktop/reportClientLaunch';
+import { isElectronMail } from '@proton/shared/lib/helpers/desktop';
 import { useFlagsReady } from '@proton/unleash';
 
 interface Props {
@@ -11,11 +15,16 @@ interface Props {
 const PublicAppSetup = ({ loader, children }: Props) => {
     const flagsReadyPromise = useFlagsReady();
     const [loaded, setLoaded] = useState(false);
+    const api = useApi();
 
     useEffect(() => {
         void flagsReadyPromise.finally(() => {
             setLoaded(true);
         });
+
+        if (isElectronMail && hasInboxDesktopFeature('InstallSource')) {
+            void reportClientLaunch(getInboxDesktopInfo('installSource'), 'mail', api);
+        }
     }, []);
 
     return <>{loaded ? children : loader}</>;
