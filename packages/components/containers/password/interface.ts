73de772a4ea915198a824457f18f29af3bab0d13
Mattias Svanström <mattias.svanstrom@proton.ch>
2024-10-31 16:25:36 +0100

    Update usage of createModal in setup organization modal

--- packages/components/containers/password/interface.ts
+++ packages/components/containers/password/interface.ts
@@ -1,16 +1,18 @@
 import type { Credentials, SrpConfig } from '@proton/shared/lib/srp';
 
+
+
+export type AuthModalResult =
+    | { type: 'sso'; response: Response }
+    | {
+          type: 'srp';
+          credentials: Credentials;
+          response: Response;
+      };
+
 export interface OwnAuthModalProps {
     config: SrpConfig;
-    onSuccess?: (
-        data:
-            | { type: 'sso'; response: Response }
-            | {
-                  type: 'srp';
-                  credentials: Credentials;
-                  response: Response;
-              }
-    ) => Promise<void> | void;
+    onSuccess?: (data: AuthModalResult) => Promise<void> | void;
     onCancel: (() => void) | undefined;
     onError?: (error: any) => void;
     prioritised2FAItem?: 'fido2' | 'totp';
