f79bc41925d207cdffe9e9e8af1add0ba6450eeb
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-10-31 12:16:40 +0100

    Add upsell for alias management
    [IDTEAM-3953]

---
+++ packages/pass/components/Upsell/PassPlusIcon.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react';
+
+import { Icon } from '@proton/components/index';
+import clsx from '@proton/utils/clsx';
+
+type Props = { className?: string };
+
+export const PassPlusIcon: FC<Props> = ({ className }) => (
+    <span className={clsx('flex items-center flex-nowrap', className)}>
+        <Icon name="brand-proton-pass-filled" size={3} className="mt-0.5 mr-0.5" />
+        <span>+</span>
+    </span>
+);
