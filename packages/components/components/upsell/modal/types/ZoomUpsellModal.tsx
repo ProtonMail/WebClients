6435c227cc38c7878d66541886376165b24db6d1
Coral Avraham <coral.avraham@proton.ch>
2024-10-31 16:12:19 +0200

    IA-1452-zoom-upsell-modal

--- packages/components/components/upsell/modal/types/ZoomUpsellModal.tsx
+++ packages/components/components/upsell/modal/types/ZoomUpsellModal.tsx
@@ -1,13 +1,10 @@
 import { c } from 'ttag';
 
-import { useUser } from '@proton/account/user/hooks';
+import { useUpsellConfig } from '@proton/components';
 import type { ModalStateProps } from '@proton/components/components/modalTwo/useModalState';
-import Price from '@proton/components/components/price/Price';
-import { usePlans } from '@proton/components/hooks';
-import type { Currency } from '@proton/payments';
-import { PLANS } from '@proton/payments';
-import { APP_UPSELL_REF_PATH, CYCLE, MAIL_UPSELL_PATHS, UPSELL_COMPONENT } from '@proton/shared/lib/constants';
-import { addUpsellPath, getUpgradePath, getUpsellRef } from '@proton/shared/lib/helpers/upsell';
+import useOneDollarConfig from '@proton/components/components/upsell/useOneDollarPromo';
+import { APP_UPSELL_REF_PATH, MAIL_UPSELL_PATHS, UPSELL_COMPONENT } from '@proton/shared/lib/constants';
+import { getUpsellRef } from '@proton/shared/lib/helpers/upsell';
 import zoomUpsellSvg from '@proton/styles/assets/img/illustrations/upsell-zoom-header.svg';
 
 import NewUpsellModal from '../NewUpsellModal';
@@ -17,24 +14,13 @@ interface Props {
 }
 
 const ZoomUpsellModal = ({ modalProps }: Props) => {
-    const [user] = useUser();
-
-    const currency: Currency = user?.Currency || 'USD';
-    const [plansResult] = usePlans();
-    const mailPlus = plansResult?.plans?.find(({ Name }) => Name === PLANS.MAIL);
-
-    const amount = (mailPlus?.DefaultPricing?.[CYCLE.YEARLY] || 0) / CYCLE.YEARLY;
-
-    const priceUnlimited = (
-        <Price
-            key="monthly-price"
-            currency={currency}
-            suffix={c('specialoffer: Offers').t`/month`}
-            isDisplayedInSentence
-        >
-            {amount}
-        </Price>
-    );
+    const upsellRef = getUpsellRef({
+        app: APP_UPSELL_REF_PATH.CALENDAR_UPSELL_REF_PATH,
+        component: UPSELL_COMPONENT.MODAL,
+        feature: MAIL_UPSELL_PATHS.ZOOM_MEETING,
+    });
+    const oneDollarConfig = useOneDollarConfig();
+    const upsellConfig = useUpsellConfig({ upsellRef, ...oneDollarConfig });
 
     return (
         <NewUpsellModal
@@ -44,16 +30,8 @@ const ZoomUpsellModal = ({ modalProps }: Props) => {
             modalProps={modalProps}
             illustration={zoomUpsellSvg}
             submitText={c('Action').t`Upgrade to Mail Plus`}
-            footerText={c('Action').jt`Starting from ${priceUnlimited}`}
             sourceEvent="BUTTON_ZOOM"
-            upgradePath={addUpsellPath(
-                getUpgradePath({ user }),
-                getUpsellRef({
-                    app: APP_UPSELL_REF_PATH.CALENDAR_UPSELL_REF_PATH,
-                    component: UPSELL_COMPONENT.MODAL,
-                    feature: MAIL_UPSELL_PATHS.ZOOM_MEETING,
-                })
-            )}
+            {...upsellConfig}
         />
     );
 };
