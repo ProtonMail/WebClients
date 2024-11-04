14777aa84f6889f12303dfb356d5c79c2a75659d
Alejandro Tidele <alejandro.tidele@proton.ch>
2024-11-04 13:14:43 +0100

    Allow passing size to `InlineFieldBox`
    [IDTEAM-3878]

--- packages/pass/components/Form/Field/Layout/InlineFieldBox.tsx
+++ packages/pass/components/Form/Field/Layout/InlineFieldBox.tsx
@@ -1,11 +1,13 @@
 import type { FC, PropsWithChildren, ReactNode } from 'react';
 
+import clsx from '@proton/utils/clsx';
+
 import './InlineFieldBox.scss';
 
-type Props = PropsWithChildren & { label: ReactNode };
+type Props = PropsWithChildren & { label: ReactNode; size?: 'sm' | 'md' | 'lg' };
 
-export const InlineFieldBox: FC<Props> = ({ label, children }) => (
-    <div className="pass-inline-field-box">
+export const InlineFieldBox: FC<Props> = ({ label, children, size = 'sm' }) => (
+    <div className={clsx('pass-inline-field-box', size)}>
         <label>{label}</label>
         <div className="flex items-center justify-end">{children}</div>
     </div>
