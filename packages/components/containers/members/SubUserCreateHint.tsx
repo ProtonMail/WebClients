1e80e46657d8d7a3b05e48e9600e4d46478b8a6f
Mattias Svanström <mattias.svanstrom@proton.ch>
2024-11-04 14:42:59 +0100

    Update pending unprivatization UX
    Add a new text indicating the current state

--- packages/components/containers/members/SubUserCreateHint.tsx
+++ packages/components/containers/members/SubUserCreateHint.tsx
@@ -5,7 +5,7 @@ import clsx from '@proton/utils/clsx';
 
 const SubUserCreateHint = ({ className, children }: { className?: string; children?: ReactNode }) => {
     return (
-        <div className={clsx([className, 'py-2 px-3 border border-weak rounded flex flex-nowrap'])}>
+        <div className={clsx([className, 'py-2 px-2 border border-weak rounded flex flex-nowrap'])}>
             <Icon name="info-circle" className="mr-2 mt-0.5 shrink-0" />
             <div>{children}</div>
         </div>
