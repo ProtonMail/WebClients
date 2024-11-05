dafb842ba777711ac544bc0916a7b0e31d2a8398
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2024-11-05 17:07:50 +0100

    Update pm.me upsell modal design
    - Add list of features on the "new" modals

--- packages/components/components/upsell/modal/types/PmMeUpsellModal.tsx
+++ packages/components/components/upsell/modal/types/PmMeUpsellModal.tsx
@@ -3,13 +3,13 @@ import { c } from 'ttag';
 import { useUser } from '@proton/account/user/hooks';
 import type { ModalStateProps } from '@proton/components/components/modalTwo/useModalState';
 import NewUpsellModal from '@proton/components/components/upsell/modal/NewUpsellModal';
+import UpsellFeatureList from '@proton/components/components/upsell/modal/UpsellFeatureList';
 import UpsellModal from '@proton/components/components/upsell/modal/UpsellModal';
+import useOneDollarConfig from '@proton/components/components/upsell/useOneDollarPromo';
+import useUpsellConfig from '@proton/components/components/upsell/useUpsellConfig';
 import { APP_UPSELL_REF_PATH, MAIL_UPSELL_PATHS, UPSELL_COMPONENT } from '@proton/shared/lib/constants';
 import { getUpsellRef, useNewUpsellModalVariant } from '@proton/shared/lib/helpers/upsell';
 import pmMeImg from '@proton/styles/assets/img/illustrations/new-upsells-img/pm-me.svg';
-
-import useOneDollarConfig from '../../useOneDollarPromo';
-import useUpsellConfig from '../../useUpsellConfig';
 
 interface Props {
     modalProps: ModalStateProps;
@@ -35,8 +35,28 @@ const FiltersUpsellModal = ({ modalProps, upsellComponent }: Props) => {
             <NewUpsellModal
                 titleModal={c('Title').t`Same inbox, shorter email address`}
                 description={
-                    // translator: full sentence is Unlock <address@pm.me> for a catchy and easy-to-type email address.
-                    c('Description').t`Unlock ${activatePmUser} for a catchy and easy-to-type email address.`
+                    <>
+                        <p className="text-wrap-balance color-weak mt-0 mb-6">
+                            {
+                                // translator: full sentence is Upgrade to get <address@pm.me> for a shorter, easy-to-remember email address in addition to your current one.
+                                c('Description')
+                                    .t`Upgrade to get ${activatePmUser} for a shorter, easy-to-remember email address in addition to your current one.`
+                            }
+                        </p>
+                        <p className="text-left">
+                            <strong className="block mb-2">{c('Description').t`Also included`}</strong>
+                            <UpsellFeatureList
+                                hideInfo
+                                features={[
+                                    'more-storage',
+                                    'more-email-addresses',
+                                    'unlimited-folders-and-labels',
+                                    'custom-email-domains',
+                                    'more-premium-features',
+                                ]}
+                            />
+                        </p>
+                    </>
                 }
                 modalProps={modalProps}
                 illustration={pmMeImg}
