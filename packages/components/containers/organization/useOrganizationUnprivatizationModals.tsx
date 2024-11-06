8321382010a722d17949c57dcd0f6bf0cbe5c022
Mattias Svanström <mattias.svanstrom@proton.ch>
2024-11-06 16:18:28 +0100

    Improve user settings section member list

--- packages/components/containers/organization/useOrganizationUnprivatizationModals.tsx
+++ packages/components/containers/organization/useOrganizationUnprivatizationModals.tsx
@@ -1,4 +1,5 @@
-import type { ReactNode } from 'react';
+import type { ReactNode} from 'react';
+import { useState } from 'react';
 
 import { c, msgid } from 'ttag';
 
@@ -9,7 +10,8 @@ import {
     unprivatizeApprovalMembers,
     unprivatizeMembersBackground,
 } from '@proton/account/members/unprivatizeMembers';
-import { Button, Card } from '@proton/atoms';
+import { Button, Card, InlineLinkButton } from '@proton/atoms';
+import type { IconName } from '@proton/components/components/icon/Icon';
 import Icon from '@proton/components/components/icon/Icon';
 import useModalState from '@proton/components/components/modalTwo/useModalState';
 import Prompt, { type PromptProps } from '@proton/components/components/prompt/Prompt';
@@ -23,7 +25,7 @@ import { getMemberEmailOrName } from '@proton/shared/lib/keys/memberHelper';
 
 const Wrap = ({ children }: { children: ReactNode }) => {
     return (
-        <Card rounded className="mb-4">
+        <Card rounded className="mb-4 p-2" padded={false}>
             {children}
         </Card>
     );
@@ -58,6 +60,75 @@ const ConfirmDeleteMembers = ({ members, onConfirm, ...rest }: Props) => {
         </Prompt>
     );
 };
+
+const AccordionButton = ({ onClick, children }: { onClick: () => void; children: ReactNode }) => {
+    return (
+        <InlineLinkButton type="button" className="mt-2 color-weak hover:color-weak" onClick={onClick}>
+            {children}
+        </InlineLinkButton>
+    );
+};
+
+const MembersListItem = ({ member }: { member: Member }) => {
+    const name = member.Name;
+    const email = getMemberEmailOrName(member);
+    return (
+        <div className="text-ellipsis w-full">
+            <span className="text-bold" title={name}>
+                {name}
+            </span>{' '}
+            {email !== name && <span title={email}>({email})</span>}
+        </div>
+    );
+};
+
+const MembersList = ({ members }: { members: Member[] }) => {
+    const [toggled, setToggled] = useState(false);
+
+    const max = 10;
+    const total = members.length;
+    const canHide = total > max;
+
+    const firstMembers = toggled ? members : members.slice(0, max);
+    const lastMembers = toggled ? [] : members.slice(max);
+
+    return (
+        <div>
+            <ul className="m-0 flex flex-column gap-1 pt-2 unstyled">
+                {firstMembers.map((member) => {
+                    return (
+                        <li key={member.ID} className="block w-full">
+                            <MembersListItem member={member} />
+                        </li>
+                    );
+                })}
+            </ul>
+            {lastMembers.length > 0 && (
+                <AccordionButton onClick={() => setToggled(true)}>{c('Action').t`Show all`}</AccordionButton>
+            )}
+            {toggled && canHide && (
+                <AccordionButton onClick={() => setToggled(false)}>{c('Action').t`Hide`}</AccordionButton>
+            )}
+        </div>
+    );
+};
+
+const MemberListBanner = ({ icon, members, action }: { icon: IconName; members: ReactNode; action: ReactNode }) => {
+    return (
+        <Wrap>
+            <div className="flex flex-column md:flex-row flex-nowrap gap-2">
+                <div className="md:flex-1 gap-2 flex flex-nowrap">
+                    <span className="shrink-0">
+                        <Icon name={icon} className="align-text-top" />
+                    </span>
+                    <div>{members}</div>
+                </div>
+                <div className="md:shrink-0">{action}</div>
+            </div>
+        </Wrap>
+    );
+};
+
 const useOrganizationUnprivatizationModals = () => {
     const [organizationKey] = useOrganizationKey();
 
@@ -75,43 +146,30 @@ const useOrganizationUnprivatizationModals = () => {
         }
         const n = membersToUnprivatize.length;
         return (
-            <Wrap>
-                <div className="flex flex-column md:flex-row flex-nowrap gap-2">
-                    <div className="md:flex-1">
-                        <Icon name="info-circle" className="align-text-top" />{' '}
+            <MemberListBanner
+                icon="info-circle"
+                members={
+                    <>
                         {c('sso').ngettext(
                             msgid`${n} user has joined your organization through your Identity Provider. Review the account now:`,
                             `${n} users have joined your organization through your Identity Provider. Review their accounts now:`,
                             n
                         )}
-                    </div>
-                    <div className="md:shrink-0">
-                        <Button
-                            size="small"
-                            loading={joinedUnprivatizationState.loading.approval}
-                            onClick={() => {
-                                dispatch(unprivatizeApprovalMembers({ membersToUnprivatize }));
-                            }}
-                        >
-                            {c('unprivatization').t`Approve all`}
-                        </Button>
-                    </div>
-                </div>
-                <ul className="m-0">
-                    {membersToUnprivatize.map((member) => {
-                        const name = member.Name;
-                        const email = getMemberEmailOrName(member);
-                        return (
-                            <li key={member.ID} className="mt-2">
-                                <span className="text-ellipsis text-bold" title={name}>
-                                    {name}
-                                </span>{' '}
-                                <span>({email})</span>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </Wrap>
+                        <MembersList members={membersToUnprivatize} />
+                    </>
+                }
+                action={
+                    <Button
+                        size="small"
+                        loading={joinedUnprivatizationState.loading.approval}
+                        onClick={() => {
+                            dispatch(unprivatizeApprovalMembers({ membersToUnprivatize }));
+                        }}
+                    >
+                        {c('unprivatization').t`Approve all`}
+                    </Button>
+                }
+            />
         );
     })();
 
@@ -122,54 +180,41 @@ const useOrganizationUnprivatizationModals = () => {
         }
         const n = membersToUnprivatize.length;
         return (
-            <Wrap>
-                <div className="flex flex-column md:flex-row flex-nowrap gap-2">
-                    <div className="md:flex-1">
-                        <Icon name="exclamation-triangle-filled" className="align-text-top" />{' '}
+            <MemberListBanner
+                icon="exclamation-triangle-filled"
+                members={
+                    <>
                         {c('unprivatization').ngettext(
                             msgid`Could not automatically enable administrator access for ${n} user, because their encryption keys have been updated in the meanwhile:`,
                             `Could not automatically enable administrator access for ${n} users, because their encryption keys have been updated in the meanwhile:`,
                             n
                         )}
-                    </div>
-                    <div className="md:shrink-0">
-                        <Button
-                            size="small"
-                            loading={joinedUnprivatizationState.loading.automatic}
-                            onClick={() => {
-                                dispatch(
-                                    unprivatizeMembersBackground({
-                                        verifyOutboundPublicKeys,
-                                        target: {
-                                            type: 'action',
-                                            members: membersToUnprivatize,
-                                        },
-                                        options: {
-                                            ignoreRevisionCheck: true,
-                                        },
-                                    })
-                                );
-                            }}
-                        >
-                            {c('unprivatization').t`Enable manually`}
-                        </Button>
-                    </div>
-                </div>
-                <ul className="m-0">
-                    {membersToUnprivatize.map((member) => {
-                        const name = member.Name;
-                        const email = getMemberEmailOrName(member);
-                        return (
-                            <li key={member.ID} className="mt-2">
-                                <span className="text-ellipsis text-bold" title={name}>
-                                    {name}
-                                </span>{' '}
-                                <span>({email})</span>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </Wrap>
+                        <MembersList members={membersToUnprivatize} />
+                    </>
+                }
+                action={
+                    <Button
+                        size="small"
+                        loading={joinedUnprivatizationState.loading.automatic}
+                        onClick={() => {
+                            dispatch(
+                                unprivatizeMembersBackground({
+                                    verifyOutboundPublicKeys,
+                                    target: {
+                                        type: 'action',
+                                        members: membersToUnprivatize,
+                                    },
+                                    options: {
+                                        ignoreRevisionCheck: true,
+                                    },
+                                })
+                            );
+                        }}
+                    >
+                        {c('unprivatization').t`Enable manually`}
+                    </Button>
+                }
+            />
         );
     })();
 
@@ -181,43 +226,30 @@ const useOrganizationUnprivatizationModals = () => {
         }
         const n = disabledMembers.length;
         return (
-            <Wrap>
-                <div className="flex flex-column md:flex-row flex-nowrap gap-2 items-center">
-                    <div className="md:flex-1">
-                        <Icon name="info-circle" className="align-text-top" />{' '}
+            <MemberListBanner
+                icon="info-circle"
+                members={
+                    <>
                         {c('sso').ngettext(
                             msgid`${n} user is inactive. You can safely remove this user from your organization.`,
                             `${n} users are inactive. You can safely remove them from your organization.`,
                             n
                         )}
-                    </div>
-                    <div className="md:shrink-0">
-                        <Button
-                            size="small"
-                            loading={loadingDelete}
-                            onClick={() => {
-                                setConfirmDelete(true);
-                            }}
-                        >
-                            {c('sso').ngettext(msgid`Delete ${n} user`, `Delete ${n} users`, n)}
-                        </Button>
-                    </div>
-                </div>
-                <ul className="m-0">
-                    {disabledMembers.map((member) => {
-                        const name = member.Name;
-                        const email = getMemberEmailOrName(member);
-                        return (
-                            <li key={member.ID} className="mt-2">
-                                <span className="text-ellipsis text-bold" title={name}>
-                                    {name}
-                                </span>{' '}
-                                <span>({email})</span>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </Wrap>
+                        <MembersList members={disabledMembers} />
+                    </>
+                }
+                action={
+                    <Button
+                        size="small"
+                        loading={loadingDelete}
+                        onClick={() => {
+                            setConfirmDelete(true);
+                        }}
+                    >
+                        {c('sso').ngettext(msgid`Delete ${n} user`, `Delete ${n} users`, n)}
+                    </Button>
+                }
+            />
         );
     })();
 
