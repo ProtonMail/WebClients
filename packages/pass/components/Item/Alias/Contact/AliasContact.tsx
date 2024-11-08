25f4552dc7b1c13dc8fa47a285293f0dc72bb57c
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-11-08 16:45:36 +0100

    Minor alias contact UI improvement
    [IDTEAM-3954]

--- packages/pass/components/Item/Alias/Contact/AliasContact.tsx
+++ packages/pass/components/Item/Alias/Contact/AliasContact.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react';
 import { c } from 'ttag';
 
 import { Icon, useModalState } from '@proton/components';
-import { FieldBox } from '@proton/pass/components/Form/Field/Layout/FieldBox';
+import { ValueControl } from '@proton/pass/components/Form/Field/Control/ValueControl';
 import type { UniqueItem } from '@proton/pass/types';
 
 import { SidebarContactsView } from './SidebarContactsView';
@@ -15,14 +15,13 @@ export const AliasContact: FC<Props> = ({ shareId, itemId }) => {
 
     return (
         <>
-            <FieldBox
-                className="py-4 cursor-pointer"
+            <ValueControl
                 actions={<Icon name="chevron-right" size={4} />}
-                icon={<Icon name="filing-cabinet" size={5} />}
+                icon={<Icon name="filing-cabinet" className="mt-0.5" size={4} />}
                 onClick={() => openViewContactSidebar(true)}
-            >
-                <div>{c('Label').t`Contacts`}</div>
-            </FieldBox>
+                value={c('Label').t`Contacts`}
+                label=""
+            />
             {viewContacts.open && (
                 <>
                     <SidebarContactsView onClose={viewContacts.onClose} shareId={shareId} itemId={itemId} />
