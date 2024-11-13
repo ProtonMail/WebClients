642961bbb7e387cce8505cbcc9c19e1fde58376a
Edvin CANDON <edvin.candon@proton.ch>
2024-11-13 19:46:51 +0100

    Leverage AliasContactsProvider for state management
    [IDTEAM-3954]

--- packages/pass/components/Item/Alias/Contact/AliasContactsProvider.tsx
+++ packages/pass/components/Item/Alias/Contact/AliasContactsProvider.tsx
@@ -1,19 +1,55 @@
 import type { FC, PropsWithChildren } from 'react';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
-import { useNavigation } from '@proton/pass/components/Navigation/NavigationProvider';
+import { useRequest } from '@proton/pass/hooks/useActionRequest';
+import { aliasGetContactsList } from '@proton/pass/store/actions';
+import type { AliasContactWithStatsGetResponse, SelectedItem } from '@proton/pass/types';
+import { partition } from '@proton/pass/utils/array/partition';
+import { prop } from '@proton/pass/utils/fp/lens';
+import { sortOn } from '@proton/pass/utils/fp/sort';
+import { objectDelete } from '@proton/pass/utils/object/delete';
+import { fullMerge, partialMerge } from '@proton/pass/utils/object/merge';
+import { toMap } from '@proton/shared/lib/helpers/object';
 
 import { AliasContactsContext, type AliasContactsContextValue } from './AliasContactsContext';
 
-export const AliasContactsProvider: FC<PropsWithChildren> = ({ children }) => {
-    const { selectedItem } = useNavigation();
+export const AliasContactsProvider: FC<PropsWithChildren<SelectedItem>> = ({ children, shareId, itemId }) => {
+    const [contacts, setContacts] = useState<Record<number, AliasContactWithStatsGetResponse>>({});
+
+    const sync = useRequest(aliasGetContactsList, {
+        initial: { shareId, itemId },
+        loading: true,
+        onSuccess: ({ data }) => setContacts(toMap(data, 'ID')),
+    });
+
+    const [blocked, active] = useMemo(() => {
+        const sorted = Object.values(contacts).sort(sortOn('CreateTime', 'DESC'));
+        return partition(sorted, prop('Blocked'));
+    }, [contacts]);
 
     const context = useMemo<AliasContactsContextValue>(
         () => ({
-            shareId: selectedItem?.shareId!,
-            itemId: selectedItem?.itemId!,
+            itemId,
+            shareId,
+            contacts: { active, blocked },
+            loading: sync.loading,
+            sync: () => sync.dispatch({ shareId, itemId }),
+            onCreate: (contact) =>
+                setContacts((contacts) =>
+                    fullMerge(contacts, {
+                        [contact.ID]: {
+                            ...contact,
+                            ForwardedEmails: 0,
+                            RepliedEmails: 0,
+                            BlockedEmails: 0,
+                        },
+                    })
+                ),
+            onDelete: (contactID) => setContacts((contacts) => objectDelete(contacts, contactID)),
+            onUpdate: (contact) => setContacts((contacts) => partialMerge(contacts, { [contact.ID]: contact })),
         }),
-        [selectedItem]
+        [itemId, shareId, blocked, active, sync.loading]
     );
+
     return <AliasContactsContext.Provider value={context}>{children}</AliasContactsContext.Provider>;
 };
