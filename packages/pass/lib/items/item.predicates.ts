2e5ca4d8d0cc1fcd7c78b012b59ae987736b1444
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-11-13 13:47:35 +0100

    Update disabled alias icon
    [IDTEAM-3568]

--- packages/pass/lib/items/item.predicates.ts
+++ packages/pass/lib/items/item.predicates.ts
@@ -62,6 +62,7 @@ export const isHealthCheckSkipped = hasItemFlag(ItemFlag.SkipHealthCheck);
 export const isBreached = hasItemFlag(ItemFlag.EmailBreached);
 export const isMonitored = not(isHealthCheckSkipped);
 export const isAliasDisabled = hasItemFlag(ItemFlag.AliasDisabled);
+export const isAliasAndDisabled = (item: ItemRevision) => isAliasItem(item.data) && isAliasDisabled(item);
 
 export const isActiveMonitored = and(isActive, isMonitored);
 export const isExcluded = and(isActive, not(isMonitored));
