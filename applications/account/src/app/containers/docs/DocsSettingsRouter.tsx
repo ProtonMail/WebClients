31c57b02b3b90df50e8cbdc4dea20962a4e79fa8
moughxyz <mo@standardnotes.com>
2024-11-13 07:00:43 -0600

    DRVDOC-1241: Settings pane in Account for Docs including comment preferences

--- applications/account/src/app/containers/docs/DocsSettingsRouter.tsx
+++ applications/account/src/app/containers/docs/DocsSettingsRouter.tsx
@@ -1,15 +1,35 @@
 import type { ReactNode } from 'react';
-import { Switch } from 'react-router-dom';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+
+import { CommentEmailSection, PrivateMainSettingsArea } from '@proton/components';
+import { getSectionPath } from '@proton/components/containers/layout/helper';
 
 import type { getDocsAppRoutes } from './routes';
 
 const DocsSettingsRouter = ({
+    docsAppRoutes,
     redirect,
 }: {
     docsAppRoutes: ReturnType<typeof getDocsAppRoutes>;
     redirect: ReactNode;
 }) => {
-    return <Switch>{redirect}</Switch>;
+    const { path } = useRouteMatch();
+
+    const {
+        routes: { comments },
+    } = docsAppRoutes;
+
+    return (
+        <Switch>
+            <Route path={getSectionPath(path, comments)}>
+                <PrivateMainSettingsArea config={comments}>
+                    <CommentEmailSection />
+                </PrivateMainSettingsArea>
+            </Route>
+
+            {redirect}
+        </Switch>
+    );
 };
 
 export default DocsSettingsRouter;
