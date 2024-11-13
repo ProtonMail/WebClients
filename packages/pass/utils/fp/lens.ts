1536c0acd033c4000f41b10b44aeee1d2e3abdaa
Edvin CANDON <edvin.candon@proton.ch>
2024-11-13 18:07:33 +0100

    Increase type-safety of `prop` utility

--- packages/pass/utils/fp/lens.ts
+++ packages/pass/utils/fp/lens.ts
@@ -4,8 +4,11 @@ interface PropFn {
     /* `<V extends T>` constraint ensures the input object satisfies the same
      * constraints as `T`. This maintains type safety when used in function
      * point-free style where the concrete type isn't known at the call site */
-    <T extends Obj, K1 extends keyof T>(key: K1): <V extends T>(obj: V) => V[K1];
-    <T extends Obj, K1 extends keyof T, K2 extends keyof T[K1]>(key: K1, key2: K2): <V extends T>(obj: V) => V[K1][K2];
+    <T extends Obj, K1 extends keyof T>(key: K1): <V extends T & { [Key in K1]?: any }>(obj: V) => V[K1];
+    <T extends Obj, K1 extends keyof T, K2 extends keyof T[K1]>(
+        key: K1,
+        key2: K2
+    ): <V extends T & { [Key in K1]?: { [Key in K2]?: any } }>(obj: V) => V[K1][K2];
     /* Add variadic overloads as necessary */
 }
 
