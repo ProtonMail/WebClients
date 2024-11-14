476505fb6a94330204b984cf0c607bf891be5650
Dominik Jankovic <dominik.jankovic@proton.ch>
2024-11-14 15:45:36 +0000

    [fix] Avoid nesting a <button> inside a <button>

--- packages/pass/components/Onboarding/OnboardingState.tsx
+++ packages/pass/components/Onboarding/OnboardingState.tsx
@@ -1,4 +1,4 @@
-import type { FC, MouseEventHandler } from 'react';
+import type { FC } from 'react';
 
 import { c } from 'ttag';
 
@@ -11,33 +11,38 @@ import { useOnboarding } from './OnboardingProvider';
 export const OnboardingState: FC<{ className?: string }> = ({ className }) => {
     const { steps, launch, acknowledge, completed } = useOnboarding();
 
-    const onDismiss: MouseEventHandler = (e) => {
-        e.stopPropagation();
-        acknowledge();
-    };
-
     return (
-        <Button
-            onClick={launch}
-            shape="ghost"
-            className={clsx('relative shrink-0 mx-3 border border-weak rounded-lg bg-weak ', className)}
-        >
-            <div className="flex justify-space-between items-center">
-                <div className="text-lg text-semibold">{c('Label').t`Get Started`}</div>
-                <Button icon pill shape="ghost" color="weak" size="small" onClick={onDismiss}>
-                    <Icon name="cross-circle-filled" alt={c('Action').t`Close`} />
-                </Button>
-            </div>
-            {steps.map((s) => (
-                <div key={s.key} className="w-full text-left flex gap-2 items-center mt-2">
-                    {completed.includes(s.key) ? (
-                        <Icon name="checkmark-circle-filled" color="var(--signal-success)" />
-                    ) : (
-                        <Icon name="circle" />
-                    )}
-                    {s.shortTitle}
+        <div className="relative px-3">
+            <Button
+                icon
+                pill
+                shape="ghost"
+                color="weak"
+                size="small"
+                onClick={acknowledge}
+                className="absolute top-0 right-0 mx-4 mt-1"
+            >
+                <Icon name="cross-circle-filled" alt={c('Action').t`Close`} />
+            </Button>
+            <Button
+                onClick={launch}
+                shape="ghost"
+                className={clsx('w-full shrink-0 border border-weak rounded-lg bg-weak ', className)}
+            >
+                <div className="flex justify-space-between items-center">
+                    <div className="text-lg text-semibold">{c('Label').t`Get Started`}</div>
                 </div>
-            ))}
-        </Button>
+                {steps.map((s) => (
+                    <div key={s.key} className="w-full text-left flex gap-2 items-center mt-2">
+                        {completed.includes(s.key) ? (
+                            <Icon name="checkmark-circle-filled" color="var(--signal-success)" />
+                        ) : (
+                            <Icon name="circle" />
+                        )}
+                        {s.shortTitle}
+                    </div>
+                ))}
+            </Button>
+        </div>
     );
 };
