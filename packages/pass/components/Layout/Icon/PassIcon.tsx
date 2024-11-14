3adcdaf4cc1820df532aedcf8c3af3ccd90fb916
Edvin CANDON <edvin.candon@proton.ch>
2024-11-14 13:47:15 +0100

    Adapt dropdown locked icon on light theme
    - There is no standard way to pass a custom parameter to an img
      svg so we resort to `protonpass-icon-locked-dropdown-light.svg`
      which patches the lock icon fill color.

--- packages/pass/components/Layout/Icon/PassIcon.tsx
+++ packages/pass/components/Layout/Icon/PassIcon.tsx
@@ -1,18 +1,29 @@
 import type { FC } from 'react';
 
 import type { IconSize } from '@proton/components';
-import type { PassIconStatus } from '@proton/pass/types/data/pass-icon';
+import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
+import { PassThemeOption } from '@proton/pass/components/Layout/Theme/types';
+import { PassIconStatus } from '@proton/pass/types/data/pass-icon';
 
 import { getIconSizePx } from './IconBox';
 
 type Props = { status: PassIconStatus; size: IconSize; className?: string };
 
-export const PassIcon: FC<Props> = ({ status, size, className }) => (
-    <img
-        src={`/assets/${status}.svg`}
-        width={getIconSizePx(size)}
-        height={getIconSizePx(size)}
-        alt=""
-        className={className}
-    />
-);
+export const PassIcon: FC<Props> = ({ status, size, className }) => {
+    const { theme } = usePassCore();
+
+    const icon = (() => {
+        if (status === PassIconStatus.LOCKED_DROPDOWN && theme === PassThemeOption.PassLight) return `${status}-light`;
+        return status;
+    })();
+
+    return (
+        <img
+            src={`/assets/${icon}.svg`}
+            width={getIconSizePx(size)}
+            height={getIconSizePx(size)}
+            alt=""
+            className={className}
+        />
+    );
+};
