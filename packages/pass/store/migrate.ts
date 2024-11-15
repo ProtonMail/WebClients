b72d68385060d9fb174c1b971aa35ee6a3519558
Edvin CANDON <edvin.candon@proton.ch>
2024-11-15 08:35:14 +0100

    Add migration for `state.filters`
    [IDTEAM-4035]

--- packages/pass/store/migrate.ts
+++ packages/pass/store/migrate.ts
@@ -1,4 +1,5 @@
 import { isB2BAdmin } from '@proton/pass/lib/organization/helpers';
+import type { FiltersState } from '@proton/pass/store/reducers';
 import { type MaybeNull } from '@proton/pass/types';
 import type { EncryptedPassCache } from '@proton/pass/types/worker/cache';
 import { type XorObfuscation, obfuscate } from '@proton/pass/utils/obfuscate/xor';
@@ -80,8 +81,15 @@ export const migrate = (state: State) => {
         };
     }
 
-    /** SSO migration */
+    /** v1.24.0 migration */
     if (!state.user.devices) state.user.devices = [];
+
+    /** v1.25.1 migration */
+    if ('popup' in state) {
+        const filters = state.popup as FiltersState;
+        delete state.popup;
+        state.filters = filters;
+    }
 
     return state;
 };
