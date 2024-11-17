a13787fbb7f7c71fbb99e9432eae7dadd91e1a56
Edvin CANDON <edvin.candon@proton.ch>
2024-11-17 13:41:01 +0100

    Promisify `useRequest` hooks

--- packages/pass/hooks/useRequest.spec.tsx
+++ packages/pass/hooks/useRequest.spec.tsx
@@ -8,13 +8,16 @@ import { withRequest } from '@proton/pass/store/request/enhancers';
 import { requestMiddleware } from '@proton/pass/store/request/middleware';
 import request from '@proton/pass/store/request/reducer';
 
-import { useActionRequest } from './useActionRequest';
+import { useActionRequest } from './useRequest';
 
 const requestId = 'test::id';
 
 const start = createAction('test::start', () => withRequest({ status: 'start', id: requestId })({ payload: {} }));
 const success = createAction('test::success', () => withRequest({ status: 'success', id: requestId })({ payload: {} }));
 const failure = createAction('test::failure', () => withRequest({ status: 'failure', id: requestId })({ payload: {} }));
+const progress = createAction('test::progress', (progress: number) =>
+    withRequest({ status: 'progress', id: requestId, progress })({ payload: {} })
+);
 
 const successCache = createAction('test::success::cache', () =>
     withRequest({ status: 'success', id: requestId, maxAge: 1 })({ payload: {} })
@@ -22,6 +25,7 @@ const successCache = createAction('test::success::cache', () =>
 
 const buildHook = (useInitialRequestId: boolean = true, initialActions: UnknownAction[] = []) => {
     const store = configureStore({ reducer: { request }, middleware: (mw) => mw().concat(requestMiddleware) });
+    const dispatch = jest.spyOn(store, 'dispatch');
     initialActions.forEach((action) => store.dispatch(action));
 
     const onStart = jest.fn();
@@ -30,6 +34,7 @@ const buildHook = (useInitialRequestId: boolean = true, initialActions: UnknownA
 
     return {
         store,
+        dispatch,
         onStart,
         onSuccess,
         onFailure,
@@ -51,7 +56,7 @@ const buildHook = (useInitialRequestId: boolean = true, initialActions: UnknownA
 };
 
 describe('useActionRequest', () => {
-    it('Handles request sequence', async () => {
+    test('Handles request sequence', async () => {
         const { hook, onStart, onSuccess, onFailure, store } = buildHook();
 
         expect(hook.result.current.loading).toBe(false);
@@ -60,7 +65,7 @@ describe('useActionRequest', () => {
         expect(onSuccess).not.toHaveBeenCalled();
         expect(onFailure).not.toHaveBeenCalled();
 
-        act(() => {
+        await act(async () => {
             hook.result.current.dispatch();
         });
 
@@ -70,17 +75,37 @@ describe('useActionRequest', () => {
         expect(onSuccess).not.toHaveBeenCalled();
         expect(onFailure).not.toHaveBeenCalled();
 
-        act(() => {
+        await act(async () => {
+            store.dispatch(progress(42));
+        });
+
+        expect(hook.result.current.loading).toBe(true);
+        expect(hook.result.current.progress).toEqual(42);
+        expect(onStart).toHaveBeenCalledTimes(1);
+        expect(onSuccess).not.toHaveBeenCalled();
+        expect(onFailure).not.toHaveBeenCalled();
+
+        await act(async () => {
+            store.dispatch(progress(100));
+        });
+
+        expect(hook.result.current.loading).toBe(true);
+        expect(hook.result.current.progress).toEqual(100);
+        expect(onStart).toHaveBeenCalledTimes(1);
+        expect(onSuccess).not.toHaveBeenCalled();
+        expect(onFailure).not.toHaveBeenCalled();
+
+        await act(async () => {
             store.dispatch(success());
         });
 
         expect(hook.result.current.loading).toBe(false);
-        expect(hook.result.current.progress).toBe(100);
+        expect(hook.result.current.progress).toBe(0);
         expect(onStart).toHaveBeenCalledTimes(1);
         expect(onSuccess).toHaveBeenCalledTimes(1);
         expect(onFailure).not.toHaveBeenCalled();
 
-        act(() => {
+        await act(async () => {
             hook.result.current.dispatch();
         });
 
@@ -90,89 +115,110 @@ describe('useActionRequest', () => {
         expect(onSuccess).toHaveBeenCalledTimes(1);
         expect(onFailure).not.toHaveBeenCalled();
 
-        act(() => {
+        await act(async () => {
             store.dispatch(failure());
         });
 
         expect(hook.result.current.loading).toBe(false);
-        expect(hook.result.current.progress).toBe(100);
+        expect(hook.result.current.progress).toBe(0);
         expect(onStart).toHaveBeenCalledTimes(2);
         expect(onSuccess).toHaveBeenCalledTimes(1);
         expect(onFailure).toHaveBeenCalledTimes(1);
     });
 
-    test('Revalidate should re-trigger sequence', () => {
+    test('Should handle cached request', async () => {
         const { hook, onStart, onSuccess, onFailure } = buildHook(true, [successCache()]);
 
-        act(() => {
+        await act(async () => {
+            await hook.result.current.dispatch();
+        });
+
+        expect(hook.result.current.loading).toBe(false);
+        expect(hook.result.current.progress).toEqual(0);
+        expect(onStart).toHaveBeenCalledTimes(1);
+        expect(onSuccess).toHaveBeenCalledTimes(1);
+        expect(onFailure).not.toHaveBeenCalled();
+    });
+
+    test('Should handle revalidation if cached request', async () => {
+        const { hook, onStart, onSuccess, onFailure } = buildHook(true, [successCache()]);
+
+        await act(async () => {
+            await hook.result.current.revalidate();
+        });
+
+        expect(hook.result.current.loading).toBe(true);
+        expect(hook.result.current.progress).toEqual(0);
+        expect(onStart).toHaveBeenCalledTimes(1);
+        expect(onSuccess).not.toHaveBeenCalled();
+        expect(onFailure).not.toHaveBeenCalled();
+    });
+
+    test('Dispatching the same action should be throttled', async () => {
+        const { hook, store, onStart, onSuccess, onFailure } = buildHook(false, []);
+
+        await act(async () => {
+            hook.result.current.dispatch();
+            hook.result.current.dispatch();
+            hook.result.current.dispatch();
+            hook.result.current.dispatch();
+        });
+
+        expect(hook.result.current.loading).toBe(true);
+        expect(hook.result.current.progress).toEqual(0);
+        expect(onStart).toHaveBeenCalledTimes(1);
+        expect(onSuccess).not.toHaveBeenCalled();
+        expect(onFailure).not.toHaveBeenCalled();
+
+        await act(async () => {
+            store.dispatch(success());
+        });
+
+        expect(hook.result.current.loading).toBe(false);
+        expect(hook.result.current.progress).toBe(0);
+        expect(onStart).toHaveBeenCalledTimes(1);
+        expect(onSuccess).toHaveBeenCalledTimes(1);
+        expect(onFailure).not.toHaveBeenCalled();
+    });
+
+    test('Revalidating after dispatch while in-flight should noop', async () => {
+        const { hook, store, onStart, onSuccess, onFailure } = buildHook(false, []);
+
+        await act(async () => {
+            hook.result.current.dispatch();
             hook.result.current.revalidate();
         });
 
         expect(hook.result.current.loading).toBe(true);
         expect(hook.result.current.progress).toEqual(0);
         expect(onStart).toHaveBeenCalledTimes(1);
+        expect(onSuccess).not.toHaveBeenCalled();
+        expect(onFailure).not.toHaveBeenCalled();
+
+        await act(async () => {
+            store.dispatch(success());
+        });
+
+        expect(hook.result.current.loading).toBe(false);
+        expect(hook.result.current.progress).toBe(0);
+        expect(onStart).toHaveBeenCalledTimes(1);
         expect(onSuccess).toHaveBeenCalledTimes(1);
         expect(onFailure).not.toHaveBeenCalled();
     });
 
-    test('dispatching should noop if request success with maxAge', () => {
-        const { hook, onStart, onSuccess, onFailure } = buildHook(false, [successCache()]);
+    test('Should handle aysnc dispatch throwing', async () => {
+        const { hook, dispatch, onStart, onSuccess, onFailure } = buildHook(false, []);
 
-        act(() => {
+        dispatch.mockImplementation(() => Promise.reject(new Error()) as any);
+
+        await act(async () => {
             hook.result.current.dispatch();
         });
 
         expect(hook.result.current.loading).toBe(false);
-        expect(hook.result.current.progress).toEqual(100);
-        expect(onStart).not.toHaveBeenCalled();
-        expect(onSuccess).toHaveBeenCalledTimes(1);
-        expect(onFailure).not.toHaveBeenCalled();
-    });
-
-    test('dispatching the same action should only trigger effect once', () => {
-        const { hook, onStart, onSuccess, onFailure } = buildHook(false, []);
-
-        act(() => {
-            hook.result.current.dispatch();
-            hook.result.current.dispatch();
-            hook.result.current.dispatch();
-            hook.result.current.dispatch();
-        });
-
-        expect(hook.result.current.loading).toBe(true);
         expect(hook.result.current.progress).toEqual(0);
         expect(onStart).toHaveBeenCalledTimes(1);
         expect(onSuccess).not.toHaveBeenCalled();
         expect(onFailure).not.toHaveBeenCalled();
-    });
-
-    test('Setting `initialRequestId` should trigger `onStart`', () => {
-        const { hook, onStart, onSuccess, onFailure } = buildHook(true, [start()]);
-
-        expect(hook.result.current.loading).toBe(true);
-        expect(hook.result.current.progress).toEqual(0);
-        expect(onStart).toHaveBeenCalledTimes(1);
-        expect(onSuccess).not.toHaveBeenCalled();
-        expect(onFailure).not.toHaveBeenCalled();
-    });
-
-    test('Setting `initialRequestId` should trigger `onSuccess`', () => {
-        const { hook, onStart, onSuccess, onFailure } = buildHook(true, [success()]);
-
-        expect(hook.result.current.loading).toBe(false);
-        expect(hook.result.current.progress).toEqual(100);
-        expect(onStart).not.toHaveBeenCalled();
-        expect(onSuccess).toHaveBeenCalledTimes(1);
-        expect(onFailure).not.toHaveBeenCalled();
-    });
-
-    test('Setting `initialRequestId` should trigger `onFailure`', () => {
-        const { hook, onStart, onSuccess, onFailure } = buildHook(true, [failure()]);
-
-        expect(hook.result.current.loading).toBe(false);
-        expect(hook.result.current.progress).toEqual(100);
-        expect(onStart).not.toHaveBeenCalled();
-        expect(onSuccess).not.toHaveBeenCalled();
-        expect(onFailure).toHaveBeenCalledTimes(1);
     });
 });
