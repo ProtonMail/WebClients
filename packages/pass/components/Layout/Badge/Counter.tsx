84d007a031fbe99ddae177f3e18fa51e383b5f33
Edvin CANDON <edvin.candon@proton.ch>
2024-11-18 09:53:43 +0100

    Fine-tune badge Counter sizing

--- packages/pass/components/Layout/Badge/Counter.tsx
+++ packages/pass/components/Layout/Badge/Counter.tsx
@@ -3,5 +3,9 @@ import { type FC, type PropsWithChildren } from 'react';
 import './Counter.scss';
 
 export const Counter: FC<PropsWithChildren> = ({ children }) => {
-    return <div className="pass--counter-circle border rounded-full py-1 px-3">{children}</div>;
+    return (
+        <div className="pass--counter-circle border rounded-full flex items-center justify-center shrink-0">
+            <span>{children}</span>
+        </div>
+    );
 };
