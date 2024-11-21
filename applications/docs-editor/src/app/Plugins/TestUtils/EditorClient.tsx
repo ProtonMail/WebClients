fde58131c3a2fe2202d7743f5174c9486f70e3cb
Mo Bitar <mo.bitar@proton.ch>
2024-11-21 11:39:08 +0000

    DRVDOC-1251: Safe mode for restoring versions

---
+++ applications/docs-editor/src/app/Plugins/TestUtils/EditorClient.tsx
@@ -0,0 +1,222 @@
+import type { LexicalEditor } from 'lexical'
+import { $createParagraphNode, $createTextNode, $getRoot, CLEAR_HISTORY_COMMAND } from 'lexical'
+import { AllNodes } from '../../AllNodes'
+import type { Root } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import { LexicalComposer } from '@lexical/react/LexicalComposer'
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin'
+import { ProtonContentEditable } from '../../ContentEditable/ProtonContentEditable'
+import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary'
+import * as ReactTestUtils from '../../Utils/react-test-utils'
+import type { Logger } from '@proton/utils/logs'
+import { YjsReadonlyPlugin } from '../YjsReadonly/YjsReadonlyPlugin'
+import { Provider } from '@lexical/yjs'
+import { LexicalDocProvider } from '@proton/docs-shared/lib/Doc/LexicalDocProvider'
+import { DocStateCallbacks } from '@proton/docs-shared/lib/Doc/DocStateCallbacks'
+import { DocState, DocWillInitializeWithEmptyNodeEvent, RtsMessagePayload, YDocMap } from '@proton/docs-shared'
+import { CollaborationContext } from '@lexical/react/LexicalCollaborationContext'
+
+const logger = {
+  info: jest.fn(),
+  debug: jest.fn(),
+} as unknown as Logger
+
+export class EditorClient {
+  editor: LexicalEditor
+  state: DocState
+  provider: () => Provider
+  docMap: YDocMap
+  propagatedMessages: RtsMessagePayload[]
+  container: HTMLElement
+  reactRoot: Root
+  private safeMode: boolean = false
+  docId = `${Math.random()}`
+
+  constructor(
+    editor: LexicalEditor,
+    state: DocState,
+    provider: () => Provider,
+    docMap: YDocMap,
+    propagatedMessages: RtsMessagePayload[],
+    container: HTMLElement,
+    reactRoot: Root,
+  ) {
+    this.editor = editor
+    this.state = state
+    this.provider = provider
+    this.docMap = docMap
+    this.propagatedMessages = propagatedMessages
+    this.container = container
+    this.reactRoot = reactRoot
+    this.safeMode = false
+  }
+
+  async enableSafeMode(enabled: boolean = true) {
+    await ReactTestUtils.act(async () => {
+      this.safeMode = enabled
+      this.reactRoot.render(this.createTestBase())
+    })
+  }
+
+  createTestBase() {
+    const TestBase = () => {
+      const onLoadResult = jest.fn()
+      const lexicalError = undefined
+
+      const TestPlugin = () => {
+        ;[this.editor] = useLexicalComposerContext()
+        return null
+      }
+
+      return (
+        <CollaborationContext.Provider
+          value={{
+            yjsDocMap: this.docMap,
+            name: `name-${this.state.getClientId()}`,
+            color: 'color-123',
+            clientID: this.state.getClientId(),
+            isCollabActive: false,
+          }}
+        >
+          <LexicalComposer
+            initialConfig={{
+              namespace: 'test',
+              nodes: AllNodes,
+              editorState: null,
+              onError: console.error,
+            }}
+          >
+            <RichTextPlugin
+              contentEditable={<ProtonContentEditable isSuggestionMode={false} />}
+              placeholder={null}
+              ErrorBoundary={LexicalErrorBoundary}
+            />
+            <YjsReadonlyPlugin
+              id={this.docId}
+              providerFactory={this.provider}
+              onLoadResult={onLoadResult}
+              lexicalError={lexicalError}
+              logger={logger}
+              safeMode={this.safeMode}
+            />
+            <TestPlugin />
+          </LexicalComposer>
+        </CollaborationContext.Provider>
+      )
+    }
+    return <TestBase />
+  }
+
+  async updateEditor(fn: () => void) {
+    await ReactTestUtils.act(async () => {
+      this.editor.update(fn)
+    })
+  }
+
+  async bootstrapWithEmptyParagraph() {
+    this.state.doc.emit(DocWillInitializeWithEmptyNodeEvent, [true, this.state.doc])
+
+    await this.updateEditor(() => {
+      this.editor.update(
+        () => {
+          const root = $getRoot()
+          if (!root.isEmpty()) {
+            throw new Error('Root is not empty')
+          }
+
+          root.clear()
+
+          const paragraph = $createParagraphNode()
+          root.append(paragraph)
+        },
+        {
+          discrete: true,
+        },
+      )
+    })
+
+    this.editor.dispatchCommand(CLEAR_HISTORY_COMMAND, undefined)
+  }
+
+  async insertText(text: string) {
+    await this.updateEditor(() => {
+      const root = $getRoot()
+      const paragraph = $createParagraphNode()
+      paragraph.append($createTextNode(text))
+      root.append(paragraph)
+    })
+
+    return text
+  }
+
+  getStateAsJson() {
+    return JSON.stringify(this.editor.getEditorState().toJSON())
+  }
+
+  async cleanup() {
+    this.state.destroy()
+    await ReactTestUtils.act(async () => {
+      this.reactRoot.unmount()
+      await Promise.resolve().then()
+    })
+    document.body.removeChild(this.container)
+  }
+}
+
+export async function createEditorClient(): Promise<EditorClient> {
+  const container = document.createElement('div')
+  document.body.appendChild(container)
+  const reactRoot = createRoot(container)
+  let editor: LexicalEditor | null = null
+
+  const docMap = new Map()
+  const propagatedMessages: RtsMessagePayload[] = []
+
+  const state = new DocState(
+    {
+      handleAwarenessStateUpdate: jest.fn(),
+      docStateRequestsPropagationOfUpdate: (update: RtsMessagePayload) => {
+        if (update.type.wrapper === 'du') {
+          propagatedMessages.push(update)
+        }
+      },
+    } as unknown as DocStateCallbacks,
+    logger,
+  )
+
+  state.onEditorReadyToReceiveUpdates()
+
+  const provider = (): Provider => {
+    return new LexicalDocProvider(state)
+  }
+
+  let client: EditorClient
+
+  await ReactTestUtils.act(async () => {
+    const TestBase = () => {
+      const [lexicalEditor] = useLexicalComposerContext()
+      // Assign the editor when the component mounts
+      editor = lexicalEditor
+      return client.createTestBase()
+    }
+
+    client = new EditorClient(editor!, state, provider, docMap, propagatedMessages, container, reactRoot)
+    docMap.set(client.docId, state.getDoc())
+
+    reactRoot.render(
+      <LexicalComposer
+        initialConfig={{
+          namespace: 'test',
+          nodes: AllNodes,
+          editorState: null,
+          onError: console.error,
+        }}
+      >
+        <TestBase />
+      </LexicalComposer>,
+    )
+  })
+
+  return client!
+}
---
+++ applications/docs-editor/src/app/Plugins/YjsReadonly/useYjsReadonly.tsx
@@ -0,0 +1,162 @@
+import type { Binding, Provider } from '@lexical/yjs'
+import { $getRoot, type LexicalEditor } from 'lexical'
+
+import { createBinding, initLocalState, syncLexicalUpdateToYjs, syncYjsChangesToLexical } from '@lexical/yjs'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import type { Doc, Transaction, YEvent } from 'yjs'
+import { UndoManager } from 'yjs'
+
+import { TranslatedResult } from '@proton/docs-shared'
+import type { EditorLoadResult } from '../../EditorLoadResult'
+import type { LoggerInterface } from '@proton/utils/logs'
+
+/**
+ * Based on useYjsCollaboration, but without awareness and cursors, and the addition of Safe Mode.
+ */
+export function useYjsReadonly(
+  editor: LexicalEditor,
+  id: string,
+  provider: Provider,
+  docMap: Map<string, Doc>,
+  onLoadResult: EditorLoadResult,
+  logger: LoggerInterface,
+  safeMode?: boolean,
+  lexicalError?: Error,
+): Binding {
+  const [doc] = useState(() => docMap.get(id))
+  const didPostReadyEvent = useRef(false)
+
+  const safeModeEventIndex = useRef(-1)
+  const needsDecrementDeltaArray = useRef(false)
+  const lastSyncedEvents = useRef<YEvent<any>[]>()
+
+  const binding = useMemo(
+    () => createBinding(editor, provider, id, doc, docMap, undefined),
+    [editor, provider, id, docMap, doc],
+  )
+
+  const applyEventsBasedOnCurrentIndices = useCallback(
+    (isFromUndoManager: boolean) => {
+      if (safeModeEventIndex.current < 0) {
+        logger.info('Safe mode event index is less than 0. Not applying events.')
+        return
+      }
+
+      const events = lastSyncedEvents.current!.slice(0, safeModeEventIndex.current + 1)
+
+      if (needsDecrementDeltaArray.current) {
+        const event = events[events.length - 1]
+        event._delta = event.delta.slice(0, -1)
+        needsDecrementDeltaArray.current = false
+      }
+
+      logger.info(
+        `Applying ${events.length} events to lexical. Current event deltas: ${events[events.length - 1]?.delta?.length}. Safe mode: ${safeMode}`,
+      )
+
+      syncYjsChangesToLexical(binding, provider, events, isFromUndoManager)
+    },
+    [binding, logger, provider, safeMode],
+  )
+
+  useEffect(() => {
+    /**
+     * If we receive a lexical error and we are in safe mode, we will begin trying to render as many updates as we can
+     * until we receive a new error. This allows us to eventually exclude the delta that may be causing the error.
+     *
+     * For now, safe mode is only activated when viewing a revision.
+     *
+     * Each time we receive a new error, we will set needsDecrementDeltaIndex to true re-sync to lexical.
+     * The reason we don't use an index but rather a bool is that we modify the current event's delta in place,
+     * as I wasn't able to conveniently find a way to make a copy of an event and its deltas.
+     *
+     * Each time we resync, we mutate the current event's delta to exclude the last delta, in place.
+     *
+     * If we try all deltas of the current event, we now decrement the event index.
+     *
+     */
+    if (lexicalError && safeMode) {
+      logger.info('Lexical is in safe mode. Attempting to reset editor state and apply one less delta.')
+
+      /** Clear the editor first as we will be reapplying all state. */
+      editor.update(
+        () => {
+          const root = $getRoot()
+          root.clear()
+        },
+        {
+          onUpdate: () => {
+            if (!lastSyncedEvents.current) {
+              return
+            }
+
+            const currentEvent = lastSyncedEvents.current[safeModeEventIndex.current]
+            if (currentEvent.delta.length > 1) {
+              needsDecrementDeltaArray.current = true
+            } else {
+              safeModeEventIndex.current -= 1
+            }
+
+            applyEventsBasedOnCurrentIndices(false)
+          },
+        },
+      )
+    }
+  }, [lexicalError, binding, safeMode, provider, editor, applyEventsBasedOnCurrentIndices, logger])
+
+  useEffect(() => {
+    const { root } = binding
+
+    const onYjsTreeChanges = (
+      // The below `any` type is taken directly from the vendor types for YJS.
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      events: YEvent<any>[],
+      transaction: Transaction,
+    ) => {
+      const origin = transaction.origin
+      if (origin !== binding) {
+        const isFromUndoManger = origin instanceof UndoManager
+
+        lastSyncedEvents.current = events
+        safeModeEventIndex.current = events.length - 1
+
+        applyEventsBasedOnCurrentIndices(isFromUndoManger)
+      }
+    }
+
+    initLocalState(provider, '', '', document.activeElement === editor.getRootElement(), {})
+
+    // This updates the local editor state when we recieve updates from other clients
+    root.getSharedType().observeDeep(onYjsTreeChanges)
+
+    const removeListener = editor.registerUpdateListener(
+      ({ prevEditorState, editorState, dirtyLeaves, dirtyElements, normalizedNodes, tags }) => {
+        if (tags.has('skip-collab') === false) {
+          syncLexicalUpdateToYjs(
+            binding,
+            provider,
+            prevEditorState,
+            editorState,
+            dirtyElements,
+            dirtyLeaves,
+            normalizedNodes,
+            tags,
+          )
+        }
+      },
+    )
+
+    if (!didPostReadyEvent.current) {
+      onLoadResult(TranslatedResult.ok())
+      didPostReadyEvent.current = true
+    }
+
+    return () => {
+      root.getSharedType().unobserveDeep(onYjsTreeChanges)
+      docMap.delete(id)
+      removeListener()
+    }
+  }, [applyEventsBasedOnCurrentIndices, binding, docMap, editor, id, onLoadResult, provider])
+
+  return binding
+}
