8d2dba23a5c36484d06427f1d35ca002cb4ca236
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-11-21 08:03:16 +0000

    Add new page to settings to handle the Zoom integration tokens

---
+++ packages/components/containers/thirdParty/ThirdPartySection.tsx
@@ -0,0 +1,47 @@
+import { c } from 'ttag';
+
+import { useOAuthToken } from '@proton/activation';
+import { OAUTH_PROVIDER } from '@proton/activation/src/interface';
+import Table from '@proton/components/components/table/Table';
+import TableBody from '@proton/components/components/table/TableBody';
+import TableCell from '@proton/components/components/table/TableCell';
+import TableHeader from '@proton/components/components/table/TableHeader';
+import TableHeaderCell from '@proton/components/components/table/TableHeaderCell';
+import TableRow from '@proton/components/components/table/TableRow';
+
+import SettingsSectionWide from '../account/SettingsSectionWide';
+import { ProviderAction, ProviderIcon, ProviderReason } from './ThirdPartyComponents';
+
+export enum SupportedProviders {
+    zoom = 'zoom',
+}
+
+export const ThirdPartySection = () => {
+    const [token, loading] = useOAuthToken();
+    const hasZoom = !loading && token?.some(({ Provider }) => Provider === OAUTH_PROVIDER.ZOOM);
+
+    return (
+        <SettingsSectionWide>
+            <Table hasActions responsive="cards">
+                <TableHeader>
+                    <TableHeaderCell className="w-2/10">{c('Title').t`Service`}</TableHeaderCell>
+                    <TableHeaderCell className="w-6/10">{c('Title').t`Description`}</TableHeaderCell>
+                    <TableHeaderCell className="w-2/10">{c('Title').t`Connection status`}</TableHeaderCell>
+                </TableHeader>
+                <TableBody colSpan={4} loading={loading}>
+                    <TableRow>
+                        <TableCell>
+                            <ProviderIcon provider={SupportedProviders.zoom} />
+                        </TableCell>
+                        <TableCell>
+                            <ProviderReason provider={SupportedProviders.zoom} />
+                        </TableCell>
+                        <TableCell>
+                            <ProviderAction provider={SupportedProviders.zoom} connected={hasZoom} />
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </SettingsSectionWide>
+    );
+};
