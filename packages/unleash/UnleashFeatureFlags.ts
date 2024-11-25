935f7703d1da1711fcbc6580d8e10720d7724276
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2024-11-25 10:36:15 +0000

    Add Edugain SAML sso setup

--- packages/unleash/UnleashFeatureFlags.ts
+++ packages/unleash/UnleashFeatureFlags.ts
@@ -51,6 +51,7 @@ enum AccountFlag {
     B2BAuthenticationLogs = 'B2BAuthenticationLogs',
     GlobalSSO = 'GlobalSSO',
     EasySwitchConsentExperiment = 'EasySwitchConsentExperiment',
+    EduGainSSO = 'EduGainSSO',
 }
 
 enum PaymentsFlag {
