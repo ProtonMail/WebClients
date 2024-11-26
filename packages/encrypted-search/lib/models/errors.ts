42e99569d4643268fd8a64c7aaf0cd7d02d25468
Divyam Rastogi <divyam.rastogi@proton.ch>
2024-11-26 17:37:11 +0000

    Mailweb 6076 add error to index key writes during initialize config

--- packages/encrypted-search/lib/models/errors.ts
+++ packages/encrypted-search/lib/models/errors.ts
@@ -21,3 +21,38 @@ export class ESParseError extends ESError {
         this.name = 'ESParseError';
     }
 }
+
+export class ESTransactionError extends ESError {
+    constructor(message: string, cause?: Error) {
+        super(message, cause);
+        this.name = 'ESTransactionError';
+    }
+}
+
+export class ESTransactionInactiveError extends ESTransactionError {
+    constructor(message: string, cause?: Error) {
+        super(message, cause);
+        this.name = 'ESTransactionInactiveError';
+    }
+}
+
+export class ESInvalidStateError extends ESTransactionError {
+    constructor(message: string, cause?: Error) {
+        super(message, cause);
+        this.name = 'ESInvalidStateError';
+    }
+}
+
+export class ESInvalidAccessError extends ESTransactionError {
+    constructor(message: string, cause?: Error) {
+        super(message, cause);
+        this.name = 'ESInvalidAccessError';
+    }
+}
+
+export class ESDbTableNotFound extends ESError {
+    constructor(message: string, cause?: Error) {
+        super(message, cause);
+        this.name = 'ESDbTableNotFound';
+    }
+}
