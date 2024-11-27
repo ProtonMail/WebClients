0461409891c8c4602bc4045760151ff6cf60f22d
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2024-11-27 18:17:50 +0100

    Remove old onboarding code

--- applications/mail/src/app/components/onboarding/checklist/messageListPlaceholder/UserOnboardingMessageListPlaceholder.tsx
+++ applications/mail/src/app/components/onboarding/checklist/messageListPlaceholder/UserOnboardingMessageListPlaceholder.tsx
@@ -3,9 +3,7 @@ import { useMailSettings } from '@proton/mail/mailSettings/hooks';
 
 import { isColumnMode } from 'proton-mail/helpers/mailSettings';
 
-import useMailOnboardingVariant from '../../useMailOnboardingVariant';
-import UsersOnboardingChecklistPlaceholder from './variants/new/UsersOnboardingReplaceAccountPlaceholder';
-import OldOnboardingChecklistPlaceholder from './variants/old/OldOnboardingChecklistPlaceholder';
+import UsersOnboardingChecklistPlaceholder from './UsersOnboardingReplaceAccountPlaceholder';
 
 interface Props {
     location: 'list' | 'emptyPlaceholder' | 'mailboxContainerPlaceholder';
@@ -18,19 +16,9 @@ interface Props {
  */
 const UserOnboardingMessageListPlaceholder = ({ location }: Props) => {
     const [mailSettings] = useMailSettings();
-    const { variant } = useMailOnboardingVariant();
     const { viewportWidth } = useActiveBreakpoint();
     const hidden = location === 'list' && viewportWidth['>=large'] ? isColumnMode(mailSettings) : undefined;
-
-    if (variant === 'old') {
-        return <OldOnboardingChecklistPlaceholder displayOnMobile={hidden} />;
-    }
-
-    if (variant === 'new') {
-        return hidden ? null : <UsersOnboardingChecklistPlaceholder />;
-    }
-
-    return null;
+    return hidden ? null : <UsersOnboardingChecklistPlaceholder />;
 };
 
 export default UserOnboardingMessageListPlaceholder;
--- applications/mail/src/app/components/onboarding/modal/MailOnboardingModal.tsx
+++ applications/mail/src/app/components/onboarding/modal/MailOnboardingModal.tsx
@@ -1,19 +1,10 @@
 import { useCallback, useEffect } from 'react';
 
 import { useWelcomeFlags } from '@proton/account';
-import { useUserSettings } from '@proton/account/userSettings/hooks';
-import { useApi } from '@proton/components';
-import { updateFlags, updateWelcomeFlags } from '@proton/shared/lib/api/settings';
 import { TelemetryMailOnboardingEvents } from '@proton/shared/lib/api/telemetry';
-import { CHECKLIST_DISPLAY_TYPE } from '@proton/shared/lib/interfaces';
-import noop from '@proton/utils/noop';
-
-import { useGetStartedChecklist } from 'proton-mail/containers/onboardingChecklist/provider/GetStartedChecklistProvider';
 
 import { useMailOnboardingTelemetry } from '../useMailOnboardingTelemetry';
-import useMailOnboardingVariant from '../useMailOnboardingVariant';
 import NewMailOnboardingModalVariant from './variants/new/NewMailOnboardingVariant';
-import OldMailOnboardingModal from './variants/old/OldMailOnboardingVariant';
 
 export interface MailOnboardingProps {
     hideDiscoverApps?: boolean;
@@ -25,11 +16,7 @@ export interface MailOnboardingProps {
 }
 
 const MailOnboardingModal = (props: MailOnboardingProps) => {
-    const api = useApi();
-    const { changeChecklistDisplay } = useGetStartedChecklist();
-    const { welcomeFlags, endReplay } = useWelcomeFlags();
-    const [userSettings] = useUserSettings();
-    const { variant, isEnabled } = useMailOnboardingVariant();
+    const { endReplay } = useWelcomeFlags();
     const [sendMailOnboardingTelemetry, loadingTelemetryDeps] = useMailOnboardingTelemetry();
 
     const handleDone = useCallback(() => {
@@ -39,57 +26,21 @@ const MailOnboardingModal = (props: MailOnboardingProps) => {
 
     // Specific for Telemetry
     useEffect(() => {
-        if (!isEnabled || loadingTelemetryDeps || variant === 'none') {
+        if (loadingTelemetryDeps) {
             return;
         }
 
         void sendMailOnboardingTelemetry(TelemetryMailOnboardingEvents.start_onboarding_modals, {});
     }, [loadingTelemetryDeps]);
 
-    // Specific for 'NONE' variant
-    useEffect(() => {
-        // For the none variant, can directly call onDone to update welcome flags
-        if (variant === 'none') {
-            if (welcomeFlags.isWelcomeFlow) {
-                // Set generic welcome to true
-                void api(updateFlags({ Welcomed: 1 })).catch(noop);
-            }
-            if (!userSettings.WelcomeFlag) {
-                // Set product specific welcome to true
-                void api(updateWelcomeFlags()).catch(noop);
-            }
-
-            changeChecklistDisplay(CHECKLIST_DISPLAY_TYPE.REDUCED);
-
-            // Using orignal onDone callback as 'none' variant does not need start or end
-            props.onDone?.();
-        }
-    }, []);
-
-    if (variant === 'new') {
-        return (
-            <NewMailOnboardingModalVariant
-                {...props}
-                onClose={endReplay}
-                onDone={handleDone}
-                data-testid="new-onboarding-variant"
-            />
-        );
-    }
-
-    if (variant === 'old') {
-        return (
-            <OldMailOnboardingModal
-                {...props}
-                onClose={endReplay}
-                onDone={handleDone}
-                data-testid="old-onboarding-variant"
-            />
-        );
-    }
-
-    // Render nothing if variant is none
-    return null;
+    return (
+        <NewMailOnboardingModalVariant
+            {...props}
+            onClose={endReplay}
+            onDone={handleDone}
+            data-testid="new-onboarding-variant"
+        />
+    );
 };
 
 export default MailOnboardingModal;
