31c5d8ceca3cf159af5e64b87bccbd2c37eaca93
Derek Henderson <derek.henderson22@proton.me>
2024-11-28 15:41:56 +0000

    MAILWEB-6065 Remove onboarding replay button from desktop app

--- applications/mail/src/app/hooks/useCanReplayOnboarding.ts
+++ applications/mail/src/app/hooks/useCanReplayOnboarding.ts
@@ -1,6 +1,7 @@
 import { differenceInMonths, fromUnixTime } from 'date-fns';
 
 import { useUser } from '@proton/account/user/hooks';
+import useIsInboxElectronApp from '@proton/components/hooks/useIsInboxElectronApp';
 import useFlag from '@proton/unleash/useFlag';
 
 import useMailOnboardingVariant from 'proton-mail/components/onboarding/useMailOnboardingVariant';
@@ -10,9 +11,10 @@ export const useCanReplayOnboarding = () => {
     const mailOnboarding = useMailOnboardingVariant();
     const isMailOnboardingEnabled = mailOnboarding.isEnabled && mailOnboarding.variant !== 'none';
     const isOnboardingReplayEnabled = useFlag('ReplayOnboardingModal');
+    const { isElectron: isDesktopApp } = useIsInboxElectronApp();
 
     const now = new Date();
     const monthsSinceAccountCreation = differenceInMonths(now, fromUnixTime(user.CreateTime));
 
-    return isMailOnboardingEnabled && isOnboardingReplayEnabled && monthsSinceAccountCreation <= 6;
+    return isMailOnboardingEnabled && isOnboardingReplayEnabled && !isDesktopApp && monthsSinceAccountCreation <= 6;
 };
