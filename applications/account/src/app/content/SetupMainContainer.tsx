350473606cbf40821472fd29b28d0620bb371896
Pedro Lavarinhas Amaro <pedro.lavarinhas.amaro@proton.ch>
2024-11-28 11:33:06 +0000

    Add Porkbun partner components/routes

--- applications/account/src/app/content/SetupMainContainer.tsx
+++ applications/account/src/app/content/SetupMainContainer.tsx
@@ -1,13 +1,17 @@
 import type { FunctionComponent } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
-import { SECURITY_CHECKUP_PATHS, SETUP_ADDRESS_PATH } from '@proton/shared/lib/constants';
+import { SubscriptionModalProvider } from '@proton/components/index';
+import { APPS, SECURITY_CHECKUP_PATHS, SETUP_ADDRESS_PATH } from '@proton/shared/lib/constants';
 
+import PartnerPaymentContainer from '../containers/PartnerPaymentContainer';
 import SetupAddressContainer from '../containers/SetupAddressContainer';
 import SecurityCheckupContainer from '../containers/securityCheckup/SecurityCheckupContainer';
 import MainContainer from './MainContainer';
 
 const SetupMainContainer: FunctionComponent = () => {
+    const isPartnerEnabled = false;
+
     return (
         <Switch>
             <Route path={SETUP_ADDRESS_PATH}>
@@ -19,6 +23,13 @@ const SetupMainContainer: FunctionComponent = () => {
             <Route path="/security-checkup">
                 <Redirect to={`${SECURITY_CHECKUP_PATHS.ROOT}${location.search}`} />
             </Route>
+            {isPartnerEnabled && (
+                <Route path="/partner/porkbun/payment">
+                    <SubscriptionModalProvider app={APPS.PROTONMAIL}>
+                        <PartnerPaymentContainer />
+                    </SubscriptionModalProvider>
+                </Route>
+            )}
             <Route path="*">
                 <MainContainer />
             </Route>
