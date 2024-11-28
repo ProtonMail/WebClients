ee8ef682afef706f4951c35e1bb6cd2bd712be91
Edvin CANDON <edvin.candon@proton.ch>
2024-11-28 09:56:29 +0100

    Add `chainSort` to compose `sortOn` functions

--- packages/pass/utils/fp/sort.ts
+++ packages/pass/utils/fp/sort.ts
@@ -1,4 +1,4 @@
-const compare = <T>(a: T, b: T): number => {
+export const compare = <T>(a: T, b: T): number => {
     if (typeof a === 'string' && typeof b === 'string') return a.localeCompare(b);
     if (typeof a === 'number' && typeof b === 'number') return a - b;
     if (typeof a === 'boolean' && typeof b === 'boolean') return Number(a) - Number(b);
@@ -10,7 +10,7 @@ export const sortOn =
     (a: T, b: T): number =>
         order === 'DESC' ? compare(b[key], a[key]) : compare(a[key], b[key]);
 
-export const liftSort =
-    <T, V>(compare: (a: T, b: T) => number, map: (a: V) => T): ((a: V, b: V) => number) =>
-    (a, b) =>
-        compare(map(a), map(b));
+export const chainSort =
+    <T>(...comparators: ((a: T, b: T) => number)[]) =>
+    (a: T, b: T): number =>
+        comparators.reduce((result, comparator) => (result !== 0 ? result : comparator(a, b)), 0);
