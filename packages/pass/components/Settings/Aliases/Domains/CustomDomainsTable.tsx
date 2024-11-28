52e6c0c36b987b44ef3f0c54b05bdf3329759673
Edvin CANDON <edvin.candon@proton.ch>
2024-11-28 08:30:44 +0100

    Improve loading states in domain/mailbox components

--- packages/pass/components/Settings/Aliases/Domains/CustomDomainsTable.tsx
+++ packages/pass/components/Settings/Aliases/Domains/CustomDomainsTable.tsx
@@ -5,6 +5,7 @@ import { c } from 'ttag';
 import { Badge, Table, TableBody, TableCell, TableHeader, TableHeaderCell, TableRow } from '@proton/components';
 import { DropdownMenuButton } from '@proton/pass/components/Layout/Dropdown/DropdownMenuButton';
 import { QuickActionsDropdown } from '@proton/pass/components/Layout/Dropdown/QuickActionsDropdown';
+import { TableRowLoading } from '@proton/pass/components/Layout/Table/TableRowLoading';
 import type { CustomDomainOutput } from '@proton/pass/types';
 
 import { useAliasDomains } from './DomainsProvider';
@@ -16,7 +17,7 @@ const getDomainStatusLabel = ({ OwnershipVerified, MxVerified }: CustomDomainOut
 };
 
 export const CustomDomainsTable: FC = () => {
-    const { canManage, customDomains, setAction } = useAliasDomains();
+    const { canManage, customDomains, setAction, defaultAliasDomain, loading } = useAliasDomains();
 
     return (
         <>
@@ -30,43 +31,55 @@ export const CustomDomainsTable: FC = () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {customDomains.map((domain) => {
-                        const statusLabel = getDomainStatusLabel(domain);
-                        return (
-                            <TableRow key={domain.ID}>
-                                <TableCell>{domain.Domain}</TableCell>
-                                <TableCell>{domain.AliasCount}</TableCell>
-                                <TableCell>{statusLabel && <Badge type="light">{statusLabel}</Badge>}</TableCell>
-                                <TableCell>
-                                    <div className="flex justify-end">
-                                        <QuickActionsDropdown
-                                            icon="three-dots-horizontal"
-                                            color="weak"
-                                            shape="solid"
-                                            size="small"
-                                            className="button-xs ui-purple"
-                                            pill={false}
-                                            originalPlacement="bottom-end"
-                                            disabled={!canManage}
-                                        >
-                                            <DropdownMenuButton
-                                                label={c('Action').t`Check settings`}
-                                                onClick={() => setAction({ type: 'info', domainID: domain.ID })}
-                                            />
-                                            <DropdownMenuButton
-                                                label={c('Action').t`Check DNS`}
-                                                onClick={() => setAction({ type: 'dns', domainID: domain.ID })}
-                                            />
-                                            <DropdownMenuButton
-                                                label={c('Action').t`Delete`}
-                                                onClick={() => setAction({ type: 'delete', domainID: domain.ID })}
-                                            />
-                                        </QuickActionsDropdown>
-                                    </div>
-                                </TableCell>
-                            </TableRow>
-                        );
-                    })}
+                    {loading ? (
+                        <TableRowLoading rows={1} cells={4} />
+                    ) : (
+                        customDomains.map((domain) => {
+                            return (
+                                <TableRow key={domain.ID}>
+                                    <TableCell>{domain.Domain}</TableCell>
+                                    <TableCell>{domain.AliasCount}</TableCell>
+                                    <TableCell>
+                                        {(() => {
+                                            if (domain.Domain === defaultAliasDomain) {
+                                                return <Badge type="primary">{c('Title').t`Default`}</Badge>;
+                                            }
+
+                                            const statusLabel = getDomainStatusLabel(domain);
+                                            return statusLabel && <Badge type="light">{statusLabel}</Badge>;
+                                        })()}
+                                    </TableCell>
+                                    <TableCell>
+                                        <div className="flex justify-end">
+                                            <QuickActionsDropdown
+                                                icon="three-dots-horizontal"
+                                                color="weak"
+                                                shape="solid"
+                                                size="small"
+                                                className="button-xs ui-purple"
+                                                pill={false}
+                                                originalPlacement="bottom-end"
+                                                disabled={!canManage}
+                                            >
+                                                <DropdownMenuButton
+                                                    label={c('Action').t`Check settings`}
+                                                    onClick={() => setAction({ type: 'info', domainID: domain.ID })}
+                                                />
+                                                <DropdownMenuButton
+                                                    label={c('Action').t`Check DNS`}
+                                                    onClick={() => setAction({ type: 'dns', domainID: domain.ID })}
+                                                />
+                                                <DropdownMenuButton
+                                                    label={c('Action').t`Delete`}
+                                                    onClick={() => setAction({ type: 'delete', domainID: domain.ID })}
+                                                />
+                                            </QuickActionsDropdown>
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        })
+                    )}
                 </TableBody>
             </Table>
         </>
