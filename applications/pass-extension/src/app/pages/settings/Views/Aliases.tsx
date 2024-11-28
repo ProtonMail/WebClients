8a7b2cef10df2002d1e08326078547e5af3b56d0
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2024-11-28 17:00:23 +0100

    Fix upselling not working on extension aliases settings
    [IDTEAM-3878]

--- applications/pass-extension/src/app/pages/settings/Views/Aliases.tsx
+++ applications/pass-extension/src/app/pages/settings/Views/Aliases.tsx
@@ -1,9 +1,10 @@
 import { type FC } from 'react';
 
 import { Aliases as AliasesCore } from '@proton/pass/components/Settings/Aliases';
+import { SpotlightProvider } from '@proton/pass/components/Spotlight/SpotlightProvider';
 
 export const Aliases: FC = () => (
-    <>
+    <SpotlightProvider>
         <AliasesCore />
-    </>
+    </SpotlightProvider>
 );
