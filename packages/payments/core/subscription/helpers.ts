dbf8ca431df4c151b5aa43de3916116a6ba6c6d1
Alexey Karpov <alexey.karpov@proton.ch>
2024-12-02 17:31:25 +0000

    Allow Pass Lifetime users to enable DWM

--- packages/payments/core/subscription/helpers.ts
+++ packages/payments/core/subscription/helpers.ts
@@ -1,4 +1,10 @@
-import { ADDON_NAMES, PLANS } from '../constants';
+import { getPrimaryPlan, hasLifetimeCoupon } from '@proton/shared/lib/helpers/subscription';
+import { type Subscription, type UserModel } from '@proton/shared/lib/interfaces';
+import { FREE_PLAN } from '@proton/shared/lib/subscription/freePlans';
+
+import { ADDON_NAMES, PLANS, PLAN_NAMES } from '../constants';
+import { isFreeSubscription } from '../helpers';
+import { type FreeSubscription } from '../interface';
 
 export const getScribeAddonNameByPlan = (planName: PLANS) => {
     switch (planName) {
@@ -12,3 +18,31 @@ export const getScribeAddonNameByPlan = (planName: PLANS) => {
             return ADDON_NAMES.MEMBER_SCRIBE_MAIL_BUSINESS;
     }
 };
+
+export function getSubscriptionPlanTitle(
+    user: UserModel,
+    subscription: Subscription | FreeSubscription | undefined
+): {
+    planTitle?: string;
+    planName?: PLANS;
+} {
+    const primaryPlan = (() => {
+        if (user.isPaid && !isFreeSubscription(subscription)) {
+            return getPrimaryPlan(subscription) ?? FREE_PLAN;
+        } else if (user.hasPassLifetime) {
+            return {
+                Title: PLAN_NAMES[PLANS.PASS_LIFETIME],
+                Name: PLANS.PASS_LIFETIME,
+            };
+        } else if (user.isFree) {
+            return FREE_PLAN;
+        }
+    })();
+
+    const planTitle = hasLifetimeCoupon(subscription) ? 'Lifetime' : primaryPlan?.Title;
+
+    return {
+        planTitle,
+        planName: primaryPlan?.Name,
+    };
+}
