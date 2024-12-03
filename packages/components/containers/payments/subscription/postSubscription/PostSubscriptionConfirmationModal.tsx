36cca60c08c02bafa21ed3e8a7b810a31252575b
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2024-12-03 14:25:57 +0100

    Add telemetry

--- packages/components/containers/payments/subscription/postSubscription/PostSubscriptionConfirmationModal.tsx
+++ packages/components/containers/payments/subscription/postSubscription/PostSubscriptionConfirmationModal.tsx
@@ -1,17 +1,35 @@
+import { useEffect } from 'react';
+
 import ModalTwo from '@proton/components/components/modalTwo/Modal';
 import type { ModalStateProps } from '@proton/components/components/modalTwo/useModalState';
+import { TelemetryMailPostSubscriptionEvents } from '@proton/shared/lib/api/telemetry';
 
 import type { SUBSCRIPTION_STEPS } from '../constants';
 import type { PostSubscriptionFlowName } from './interface';
 import postSubscriptionConfig from './postSubscriptionConfig';
+import { usePostSubscriptionTelemetry } from './usePostSubscriptionTelemetry';
 
 interface PostSubscriptionModalProps extends ModalStateProps {
-    name: PostSubscriptionFlowName | null;
+    name: PostSubscriptionFlowName;
     step: SUBSCRIPTION_STEPS;
 }
 
 const PostSubscriptionModal = ({ name, step, ...modalProps }: PostSubscriptionModalProps) => {
     const config = name ? postSubscriptionConfig[name] : null;
+
+    const sendTelemetryEvent = usePostSubscriptionTelemetry();
+
+    useEffect(() => {
+        if (config) {
+            void sendTelemetryEvent({
+                event: TelemetryMailPostSubscriptionEvents.post_subscription_start,
+                dimensions: {
+                    modal: name,
+                },
+            });
+        }
+    }, []);
+
     if (!config) {
         return null;
     }
