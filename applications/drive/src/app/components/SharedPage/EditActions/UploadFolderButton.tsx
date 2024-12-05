22448922dadda64a73626e4e750e93823b31a888
Lucien Peslier <lucien.peslier@proton.ch>
2024-12-05 14:37:59 +0000

    [DRVWEB-4412] Support Drag-n-Drop + set default conflict to new name

--- applications/drive/src/app/components/SharedPage/EditActions/UploadFolderButton.tsx
+++ applications/drive/src/app/components/SharedPage/EditActions/UploadFolderButton.tsx
@@ -3,28 +3,28 @@ import { c } from 'ttag';
 import { Button } from '@proton/atoms/index';
 import { Icon } from '@proton/components/index';
 
-import { useFileUploadInput } from '../../../store';
+import { useFolderUploadInput } from '../../../store';
 
-interface UploadFileButtonProps {
+interface UploadFolderButtonProps {
     token: string;
     linkId: string;
 }
-export const UploadFileButton = ({ token, linkId }: UploadFileButtonProps) => {
-    const { inputRef: fileInput, handleClick, handleChange } = useFileUploadInput(token, linkId);
+export const UploadFolderButton = ({ token, linkId }: UploadFolderButtonProps) => {
+    const { inputRef: fileInput, handleClick: handleUploadFolder, handleChange } = useFolderUploadInput(token, linkId);
 
     return (
         <>
             <input multiple type="file" ref={fileInput} className="hidden" onChange={handleChange} />
             <Button
-                onClick={handleClick}
+                onClick={handleUploadFolder}
                 className="flex flex-column py-3 w-custom"
                 style={{
                     '--w-custom': '8.25rem',
                 }}
                 size="medium"
             >
-                <Icon name="arrow-up-line" size={4} className="mb-4" />
-                {c('Action').t`Upload`}
+                <Icon name="folder-arrow-up" size={4} className="mb-4" />
+                {c('Action').t`Upload folder`}
             </Button>
         </>
     );
