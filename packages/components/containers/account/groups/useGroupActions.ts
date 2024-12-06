2aa8695beada63d7f098892e8ee3ef4511c3b673
Redacted <redacted@invalid.proton.ch>
2024-12-06 16:08:59 +0000

    Fix `acceptIncomingForwarding` SKL update with multiple incoming forwarding keys

--- packages/components/containers/account/groups/useGroupActions.ts
+++ packages/components/containers/account/groups/useGroupActions.ts
@@ -15,82 +15,20 @@ import { CryptoProxy } from '@proton/crypto';
 import { baseUseDispatch } from '@proton/react-redux-store';
 import { deleteGroupMember } from '@proton/shared/lib/api/groups';
 import { getAndVerifyApiKeys } from '@proton/shared/lib/api/helpers/getAndVerifyApiKeys';
-import { createAddressKeyRouteV2, replaceAddressTokens } from '@proton/shared/lib/api/keys';
-import type {
-    ActiveAddressKeysByVersion,
-    Address,
-    Api,
-    GroupMembership,
-    KeyTransparencyVerify,
-} from '@proton/shared/lib/interfaces';
+import { replaceAddressTokens } from '@proton/shared/lib/api/keys';
+import type { GroupMembership } from '@proton/shared/lib/interfaces';
 import {
     decryptMemberToken,
     getAddressKeyToken,
-    getDefaultKeyFlags,
     getEmailFromKey,
     getHasMigratedAddressKeys,
     getReplacedAddressKeyTokens,
-    getSignedKeyListWithDeferredPublish,
     splitKeys,
 } from '@proton/shared/lib/keys';
-import {
-    getActiveAddressKeys,
-    getActiveKeyObject,
-    getNormalizedActiveAddressKeys,
-} from '@proton/shared/lib/keys/getActiveKeys';
+import { getActiveAddressKeys } from '@proton/shared/lib/keys/getActiveKeys';
 
+import { generateForwardingAddressKey as generateGroupMemberAddressKey } from '../../forward/helpers';
 import useVerifyOutboundPublicKeys from '../../keyTransparency/useVerifyOutboundPublicKeys';
-
-interface ForwardingAddressKeyParameters {
-    api: Api;
-    privateKey: PrivateKeyReferenceV4;
-    address: Address;
-    activeKeys: ActiveAddressKeysByVersion;
-    privateKeyArmored: string;
-    signature: string;
-    encryptedToken: string;
-    groupMemberID: string;
-    keyTransparencyVerify: KeyTransparencyVerify;
-}
-
-const generateGroupMemberAddressKey = async ({
-    api,
-    privateKey,
-    address,
-    activeKeys,
-    privateKeyArmored,
-    signature,
-    encryptedToken,
-    groupMemberID,
-    keyTransparencyVerify,
-}: ForwardingAddressKeyParameters) => {
-    const newActiveKey = await getActiveKeyObject(privateKey, {
-        ID: 'tmp',
-        primary: 0,
-        flags: getDefaultKeyFlags(address),
-    });
-    const updatedActiveKeys = getNormalizedActiveAddressKeys(address, { v4: [newActiveKey], v6: [] });
-    const [SignedKeyList, onSKLPublishSuccess] = await getSignedKeyListWithDeferredPublish(
-        activeKeys,
-        address,
-        keyTransparencyVerify
-    );
-    const { Key } = await api(
-        createAddressKeyRouteV2({
-            AddressID: address.ID,
-            Primary: newActiveKey.primary,
-            PrivateKey: privateKeyArmored,
-            SignedKeyList,
-            Signature: signature,
-            Token: encryptedToken,
-            GroupMemberID: groupMemberID,
-        })
-    );
-    await onSKLPublishSuccess();
-    newActiveKey.ID = Key.ID;
-
-    return [newActiveKey, updatedActiveKeys] as const;
-};
 
 const useGroupActions = () => {
     const api = useApi();
@@ -166,48 +104,46 @@ const useGroupActions = () => {
 
             let activeKeys = await getActiveAddressKeys(address, address.SignedKeyList, address.Keys, addressKeys);
 
-            for (const forwardingKey of [membership.Keys]) {
-                const decryptedToken = await decryptMemberToken(
-                    forwardingKey.ActivationToken,
-                    splitAddressKeys.privateKeys,
-                    publicKeys
-                );
-                let privateKey = (await CryptoProxy.importPrivateKey({
-                    armoredKey: forwardingKey.PrivateKey,
-                    passphrase: decryptedToken,
-                })) as PrivateKeyReferenceV4; // v6 keys do not support forwarding yet
-                const extractedEmail = getEmailFromKey(privateKey);
+            const forwardingKey = membership.Keys;
+            const decryptedToken = await decryptMemberToken(
+                forwardingKey.ActivationToken,
+                splitAddressKeys.privateKeys,
+                publicKeys
+            );
+            let privateKey = (await CryptoProxy.importPrivateKey({
+                armoredKey: forwardingKey.PrivateKey,
+                passphrase: decryptedToken,
+            })) as PrivateKeyReferenceV4; // v6 keys do not support forwarding yet
+            const extractedEmail = getEmailFromKey(privateKey);
 
-                // The forwardee email address can change before the user has accepted the forwarding
-                // So we need to update the private key with the email address returned by the API
-                // Use strict comparison because capitalization matters
-                if (extractedEmail !== forwardeeEmail) {
-                    const updatedPrivateKey = await CryptoProxy.cloneKeyAndChangeUserIDs({
-                        userIDs: [{ name: forwardeeEmail, email: forwardeeEmail }],
-                        privateKey,
-                    });
-                    await CryptoProxy.clearKey({ key: privateKey });
-                    privateKey = updatedPrivateKey as PrivateKeyReferenceV4;
-                }
-
-                const armoredPrivateKey = await CryptoProxy.exportPrivateKey({
-                    privateKey,
-                    passphrase: decryptedPrimaryAddressKeyToken,
-                });
-                const [, updatedActiveKeys] = await generateGroupMemberAddressKey({
-                    api,
-                    address,
-                    keyTransparencyVerify,
-                    groupMemberID: membership.ID,
-                    encryptedToken: primaryAddressKey.Token,
-                    signature: primaryAddressKey.Signature,
-                    privateKeyArmored: armoredPrivateKey,
-                    activeKeys,
+            // The forwardee email address can change before the user has accepted the forwarding
+            // So we need to update the private key with the email address returned by the API
+            // Use strict comparison because capitalization matters
+            if (extractedEmail !== forwardeeEmail) {
+                const updatedPrivateKey = await CryptoProxy.cloneKeyAndChangeUserIDs({
+                    userIDs: [{ name: forwardeeEmail, email: forwardeeEmail }],
                     privateKey,
                 });
-                await keyTransparencyCommit(userKeys);
-                activeKeys = updatedActiveKeys;
+                await CryptoProxy.clearKey({ key: privateKey });
+                privateKey = updatedPrivateKey as PrivateKeyReferenceV4;
             }
+
+            const armoredPrivateKey = await CryptoProxy.exportPrivateKey({
+                privateKey,
+                passphrase: decryptedPrimaryAddressKeyToken,
+            });
+            await generateGroupMemberAddressKey({
+                api,
+                address,
+                keyTransparencyVerify,
+                groupMemberID: membership.ID,
+                encryptedToken: primaryAddressKey.Token,
+                signature: primaryAddressKey.Signature,
+                privateKeyArmored: armoredPrivateKey,
+                activeKeys,
+                privateKey,
+            });
+            await keyTransparencyCommit(userKeys);
             dispatch(acceptMembership(membership));
             createNotification({ text: c('group_invitation: Success').t`Group invitation accepted` });
         } catch (error) {
