9585b0338d88155a0d997c11254d9241b700e778
Alejandro Tidele <alejandro.tidele@proton.ch>
2024-12-09 13:00:56 +0100

    Uppercase expected "ID's"
    [IDTEAM-4230]

--- applications/pass/src/app/DeepLinks/configuration.ts
+++ applications/pass/src/app/DeepLinks/configuration.ts
@@ -3,11 +3,11 @@ import type { DeepLinkRoutes } from './types';
 export const getURLPrefix = (path: string) => path.substring(0, path.indexOf('/internal'));
 
 export const deepLinkConfig: Partial<Record<keyof DeepLinkRoutes, (p: URLSearchParams) => string>> = {
-    share_members: (p) => `/share/${p.get('ShareId')}`,
-    alias_breach: (p) => `/monitor/dark-web/alias/${p.get('ShareId')}/${p.get('ItemId')}/${p.get('Email')}`,
+    share_members: (p) => `/share/${p.get('ShareID')}`,
+    alias_breach: (p) => `/monitor/dark-web/alias/${p.get('ShareID')}/${p.get('ItemID')}/${p.get('Email')}`,
     custom_email_breach: (p) => `/monitor/dark-web/custom/${p.get('CustomEmailID')}`,
     address_breach: (p) => `/monitor/dark-web/proton/${p.get('AddressID')}}`,
-    view_item: (p) => `/share/${p.get('ShareId')}/item/${p.get('ItemId')}`,
+    view_item: (p) => `/share/${p.get('ShareID')}/item/${p.get('ItemID')}`,
 };
 
 export const fallback = () => '/';
