353bc137b84dd8522f178af6f024f490c4893a4e
Aman Harwara <aman.harwara@proton.ch>
2024-12-09 15:36:10 +0000

    Move text formatting option to start of toolbar on mobile

---
+++ applications/docs-editor/src/app/Toolbar/ToolbarItem.tsx
@@ -0,0 +1,49 @@
+import { Button } from '@proton/atoms'
+import { ShortcutLabel } from '../Plugins/KeyboardShortcuts/ShortcutLabel'
+import type { ToolbarItemInterface } from './ToolbarItemInterface'
+import ToolbarTooltip from './ToolbarTooltip'
+import { ToolbarButton } from './ToolbarButton'
+import { SimpleDropdown } from '@proton/components'
+
+export function ToolbarItem({ item }: { item: ToolbarItemInterface }) {
+  if (item.type === 'button') {
+    return (
+      <ToolbarButton
+        label={item.shortcut ? <ShortcutLabel shortcut={item.shortcut} label={item.label} /> : item.label}
+        data-testid={item.id}
+        active={item.active}
+        disabled={item.disabled}
+        onClick={item.onClick}
+      >
+        {item.icon}
+      </ToolbarButton>
+    )
+  }
+
+  const dropdown = (
+    <SimpleDropdown
+      as={item.useToolbarButton ? ToolbarButton : Button}
+      shape="ghost"
+      type="button"
+      color="norm"
+      className="px-2 text-left text-sm text-[--text-norm]"
+      content={item.label('toolbar')}
+      disabled={item.disabled}
+      contentProps={item.dropdownProps}
+      active={item.active}
+      data-testid={item.id}
+    >
+      {item.menu}
+    </SimpleDropdown>
+  )
+
+  if (item.tooltip) {
+    return (
+      <ToolbarTooltip originalPlacement="bottom" title={item.tooltip}>
+        {dropdown}
+      </ToolbarTooltip>
+    )
+  }
+
+  return dropdown
+}
