bda28dcaad8c8ba43bca2c8fcaa194bb3ec4df5c
Alejandro Tidele <alejandro.tidele@proton.ch>
2024-12-10 17:03:47 +0100

    Fix URL's redirection
    [IDTEAM-4230]

--- applications/pass/src/app/DeepLinks/configuration.ts
+++ applications/pass/src/app/DeepLinks/configuration.ts
@@ -4,9 +4,9 @@ export const getURLPrefix = (path: string) => path.substring(0, path.indexOf('/i
 
 export const deepLinkConfig: Partial<Record<keyof DeepLinkRoutes, (p: URLSearchParams) => string>> = {
     share_members: (p) => `/share/${p.get('ShareID')}`,
-    alias_breach: (p) => `/monitor/dark-web/alias/${p.get('ShareID')}/${p.get('ItemID')}/${p.get('Email')}`,
+    alias_breach: (p) => `/monitor/dark-web/alias/${p.get('ShareID')}:${p.get('ItemID')}`,
     custom_email_breach: (p) => `/monitor/dark-web/custom/${p.get('CustomEmailID')}`,
-    address_breach: (p) => `/monitor/dark-web/proton/${p.get('AddressID')}}`,
+    address_breach: (p) => `/monitor/dark-web/proton/${p.get('AddressID')}`,
     view_item: (p) => `/share/${p.get('ShareID')}/item/${p.get('ItemID')}`,
 };
 
