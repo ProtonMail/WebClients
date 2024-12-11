26435875ddadc7ce7a4ad57444aa3f399be044af
moughxyz <mo@standardnotes.com>
2024-12-11 07:41:54 -0600

    Persist docs state

--- applications/docs/src/app/ReduxStore/listener.ts
+++ applications/docs/src/app/ReduxStore/listener.ts
@@ -1,7 +1,18 @@
 import { startSharedListening } from '@proton/redux-shared-store/sharedListeners'
 
 import type { AppStartListening } from './store'
+import { startPersistListener } from '@proton/account/persist/listener'
+import type { DocsState } from './rootReducer'
 
-export const start = (startListening: AppStartListening) => {
+export const start = ({
+  startListening,
+  persistTransformer,
+}: {
+  startListening: AppStartListening
+  persistTransformer?: (state: DocsState) => any
+}) => {
   startSharedListening(startListening)
+  if (persistTransformer) {
+    startPersistListener(startListening, persistTransformer)
+  }
 }
