867493d8ea983ecb42de190fa1b306f0f48cb662
Flavien Bonvin <flavien.bonvin@proton.ch>
2024-12-16 13:01:31 +0000

    Rename the experimental fields parameters to start with X-PM-

--- packages/calendar/components/videoConferencing/modelHelpers.ts
+++ packages/calendar/components/videoConferencing/modelHelpers.ts
@@ -1,27 +1,43 @@
 import type { EventModelReadView, VcalVeventComponent } from '@proton/shared/lib/interfaces/calendar';
 
+const isVcalEvent = (model: EventModelReadView | VcalVeventComponent): model is VcalVeventComponent => {
+    return 'component' in model;
+};
+
 export const getVideoConferencingData = (model: EventModelReadView | VcalVeventComponent) => {
     // The model is a VcalVeventComponent if it has a 'component' property
-    if ('component' in model) {
+    if (isVcalEvent(model)) {
         return {
-            description: (model as VcalVeventComponent).description?.value.trim(),
-            location: (model as VcalVeventComponent).location?.value.trim(),
-            meetingId: (model as VcalVeventComponent)?.['x-pm-conference-id']?.value,
-            creator: (model as VcalVeventComponent)?.['x-pm-conference-id']?.parameters?.creator,
-            meetingUrl: (model as VcalVeventComponent)?.['x-pm-conference-url']?.value,
-            password: (model as VcalVeventComponent)?.['x-pm-conference-url']?.parameters?.password,
-            meetingHost: (model as VcalVeventComponent)?.['x-pm-conference-url']?.parameters?.host,
+            description: model.description?.value.trim(),
+            location: model.location?.value.trim(),
+            meetingId: model?.['x-pm-conference-id']?.value,
+            creator:
+                model?.['x-pm-conference-id']?.parameters?.['x-pm-creator'] ||
+                model?.['x-pm-conference-id']?.parameters?.creator,
+            meetingUrl: model?.['x-pm-conference-url']?.value,
+            password:
+                model?.['x-pm-conference-url']?.parameters?.['x-pm-password'] ||
+                model?.['x-pm-conference-url']?.parameters?.password,
+            meetingHost:
+                model?.['x-pm-conference-url']?.parameters?.['x-pm-host'] ||
+                model?.['x-pm-conference-url']?.parameters?.host,
         };
     }
 
     // We return both the description and location as trimmed strings with the custom fields
     return {
-        description: (model as EventModelReadView).description.trim(),
-        location: (model as EventModelReadView).location.trim(),
-        meetingId: (model as EventModelReadView).rest['x-pm-conference-id']?.value,
-        creator: (model as EventModelReadView).rest['x-pm-conference-id']?.parameters?.creator,
-        meetingUrl: (model as EventModelReadView).rest['x-pm-conference-url']?.value,
-        password: (model as EventModelReadView).rest['x-pm-conference-url']?.parameters?.password,
-        meetingHost: (model as EventModelReadView).rest['x-pm-conference-url']?.parameters?.host,
+        description: model.description.trim(),
+        location: model.location.trim(),
+        meetingId: model.rest['x-pm-conference-id']?.value,
+        creator:
+            model.rest['x-pm-conference-id']?.parameters?.['x-pm-creator'] ||
+            model.rest['x-pm-conference-id']?.parameters?.creator,
+        meetingUrl: model.rest['x-pm-conference-url']?.value,
+        password:
+            model.rest['x-pm-conference-url']?.parameters?.['x-pm-password'] ||
+            model.rest['x-pm-conference-url']?.parameters?.password,
+        meetingHost:
+            model.rest['x-pm-conference-url']?.parameters?.['x-pm-host'] ||
+            model.rest['x-pm-conference-url']?.parameters?.host,
     };
 };
