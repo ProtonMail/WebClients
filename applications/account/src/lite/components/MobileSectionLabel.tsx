03d0e17bf2ed8a3c64f5476ecce24c02f39c60df
Nico Hoffmann <nicolas.hoffmann@proton.ch>
2024-12-18 13:28:28 +0000

    Update mobile settings

--- applications/account/src/lite/components/MobileSectionLabel.tsx
+++ applications/account/src/lite/components/MobileSectionLabel.tsx
@@ -10,7 +10,7 @@ interface Props {
 const MobileSectionLabel = ({ htmlFor, children, description, small = false }: Props) => {
     return (
         <label htmlFor={htmlFor} className="flex-1 flex flex-column flex-nowrap mr-4">
-            <span className={clsx([small ? 'color-weak' : 'text-semibold text-lg'])}>{children}</span>
+            <span className={clsx([small ? 'color-weak' : 'text-lg'])}>{children}</span>
             {description && <div className="color-weak">{description}</div>}
         </label>
     );
