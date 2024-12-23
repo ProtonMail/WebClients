a71678627dd97a252000fb4ab276bb3f0d31d6ae
Edvin CANDON <edvin.candon@proton.ch>
2024-12-23 11:18:02 +0100

    Migrate alias options actions to request flow

--- packages/pass/store/sagas/alias/alias-options-request.saga.ts
+++ packages/pass/store/sagas/alias/alias-options-request.saga.ts
@@ -1,27 +1,5 @@
-import { put, takeEvery } from 'redux-saga/effects';
-
 import { getAliasOptions } from '@proton/pass/lib/alias/alias.requests';
-import { getAliasOptionsFailure, getAliasOptionsIntent, getAliasOptionsSuccess } from '@proton/pass/store/actions';
-import type { AliasOptions } from '@proton/pass/types';
+import { requestAliasOptions } from '@proton/pass/store/actions';
+import { createRequestSaga } from '@proton/pass/store/request/sagas';
 
-function* requestAliasOptions(action: ReturnType<typeof getAliasOptionsIntent>) {
-    const {
-        payload: { shareId },
-        meta: { callback: onAliasOptionsIntentProcessed, request },
-    } = action;
-
-    try {
-        const options: AliasOptions = yield getAliasOptions(shareId);
-        const aliasOptionsSuccessAction = getAliasOptionsSuccess(request.id, { options });
-        yield put(aliasOptionsSuccessAction);
-        onAliasOptionsIntentProcessed?.(aliasOptionsSuccessAction);
-    } catch (e) {
-        const aliasOptionsFailureAction = getAliasOptionsFailure(request.id, e);
-        yield put(aliasOptionsFailureAction);
-        onAliasOptionsIntentProcessed?.(aliasOptionsFailureAction);
-    }
-}
-
-export default function* watcher() {
-    yield takeEvery(getAliasOptionsIntent.match, requestAliasOptions);
-}
+export default createRequestSaga({ actions: requestAliasOptions, call: getAliasOptions });
