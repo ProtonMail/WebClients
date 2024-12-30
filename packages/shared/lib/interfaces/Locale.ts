5124301f797b5458ba86c08821cc9f52b7c0083f
Mattias Svanström <mmso@mmso.se>
2024-12-30 14:30:40 +0000

    Fix date format options in locale injector

--- packages/shared/lib/interfaces/Locale.ts
+++ packages/shared/lib/interfaces/Locale.ts
@@ -1,10 +1,17 @@
 import type { Locale } from 'date-fns';
 import type { LocaleData } from 'ttag';
 
+import type { SETTINGS_DATE_FORMAT, SETTINGS_TIME_FORMAT, SETTINGS_WEEK_START } from './UserSettings';
 import type { SimpleMap } from './utils';
 
 export type TtagLocaleMap = SimpleMap<() => Promise<LocaleData>>;
 
 export interface DateFnsLocaleMap {
     [key: string]: () => Promise<Locale>;
+}
+
+export interface DateFormatOptions {
+    TimeFormat: SETTINGS_TIME_FORMAT;
+    DateFormat: SETTINGS_DATE_FORMAT;
+    WeekStart: SETTINGS_WEEK_START;
 }
