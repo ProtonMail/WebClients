06cc2c1481be2311c3d7508ca7de9e4d3446119e
Edvin CANDON <edvin.candon@proton.ch>
2024-12-31 07:43:31 +0100

    Ensure `PassCore` is stable
    - Previously : updating the locale or theme would cause a full PassCoreContext
      invalidation which would trigger cascading re-renders in every connected components
    - Use observable state in `ThemeProvider` and expose `PassThemeService` on PassCore
    - Move locale state to `Localized` component

--- packages/pass/components/Core/Localized.tsx
+++ packages/pass/components/Core/Localized.tsx
@@ -1,7 +1,9 @@
 import type { PropsWithChildren } from 'react';
-import { type FC, Fragment } from 'react';
+import { type FC, Fragment, useEffect, useState } from 'react';
 
 import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
+import { DEFAULT_LOCALE } from '@proton/shared/lib/constants';
+import noop from '@proton/utils/noop';
 
 /** Trigger localization change through the React "key" technique to induce a
  * re-render of the specific sub-tree, avoiding a full reload. Note that this
@@ -9,5 +11,12 @@ import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
  * potentially causing a loss of the current context. */
 export const Localized: FC<PropsWithChildren> = ({ children }) => {
     const core = usePassCore();
-    return <Fragment key={core.locale}>{children}</Fragment>;
+    const [appLocale, setAppLocale] = useState(DEFAULT_LOCALE);
+
+    useEffect(() => {
+        core.i18n.setLocale().catch(noop);
+        core.i18n.subscribe(({ locale }) => setAppLocale(locale));
+    }, []);
+
+    return <Fragment key={appLocale}>{children}</Fragment>;
 };
