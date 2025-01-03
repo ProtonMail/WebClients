736554f13d70e3d836ad9abbc31be924984306c5
Dominik Jankovic <dominik.jankovic@proton.ch>
2025-01-03 10:03:41 +0000

    Fix open-item tests

--- packages/pass/lib/crypto/processes/item/open-item.ts
+++ packages/pass/lib/crypto/processes/item/open-item.ts
@@ -1,4 +1,5 @@
 import { decryptData } from '@proton/pass/lib/crypto/utils/crypto-helpers';
+import { PassCryptoItemError } from '@proton/pass/lib/crypto/utils/errors';
 import type { ItemKey, ItemRevisionContentsResponse, OpenedItem } from '@proton/pass/types';
 import { PassEncryptionTag } from '@proton/pass/types';
 import { base64StringToUint8Array } from '@proton/shared/lib/helpers/encoding';
@@ -9,6 +10,12 @@ type OpenItemKeyProcessParams = {
 };
 
 export const openItem = async ({ encryptedItem, itemKey }: OpenItemKeyProcessParams): Promise<OpenedItem> => {
+    if (itemKey.rotation !== encryptedItem.KeyRotation) {
+        throw new PassCryptoItemError(
+            `Invalid key rotation : received ${itemKey.rotation} / expected ${encryptedItem.KeyRotation}`
+        );
+    }
+
     const decryptedContent = await decryptData(
         itemKey.key,
         base64StringToUint8Array(encryptedItem.Content),
