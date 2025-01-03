3ea20cbc50eed01ed46b7220d9d1f27970127d7e
Edvin CANDON <edvin.candon@proton.ch>
2025-01-03 09:49:07 +0100

    Remove unused action callback usages in item trash/restore actions

--- packages/pass/store/sagas/items/item-restore.saga.ts
+++ packages/pass/store/sagas/items/item-restore.saga.ts
@@ -4,22 +4,16 @@ import { restoreItems } from '@proton/pass/lib/items/item.requests';
 import { itemRestoreFailure, itemRestoreIntent, itemRestoreSuccess } from '@proton/pass/store/actions';
 import type { RootSagaOptions } from '@proton/pass/store/types';
 
-function* restoreItem({ onItemsUpdated }: RootSagaOptions, { payload, meta }: ReturnType<typeof itemRestoreIntent>) {
+function* restoreItem({ onItemsUpdated }: RootSagaOptions, { payload }: ReturnType<typeof itemRestoreIntent>) {
     const { item, shareId } = payload;
     const { itemId } = item;
-    const { callback: onItemRestoreProcessed } = meta;
 
     try {
         yield restoreItems([item]);
-        const itemRestoreSuccessAction = itemRestoreSuccess({ itemId, shareId });
-        yield put(itemRestoreSuccessAction);
-
-        onItemRestoreProcessed?.(itemRestoreSuccessAction);
+        yield put(itemRestoreSuccess({ itemId, shareId }));
         onItemsUpdated?.();
     } catch (e) {
-        const itemRestoreFailureAction = itemRestoreFailure({ itemId, shareId }, e);
-        yield put(itemRestoreFailureAction);
-        onItemRestoreProcessed?.(itemRestoreFailureAction);
+        yield put(itemRestoreFailure({ itemId, shareId }, e));
     }
 }
 
