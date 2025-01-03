207608237159faa62871541087ed442c89822231
Edvin CANDON <edvin.candon@proton.ch>
2025-01-03 09:23:58 +0100

    Remove action callback usage from vault creation saga
    - Prefer calling the underlying vault-creation.saga from the import saga

--- packages/pass/store/sagas/vaults/vault-creation.saga.ts
+++ packages/pass/store/sagas/vaults/vault-creation.saga.ts
@@ -2,23 +2,19 @@ import { put, takeEvery } from 'redux-saga/effects';
 
 import { createVault } from '@proton/pass/lib/vaults/vault.requests';
 import { vaultCreationFailure, vaultCreationIntent, vaultCreationSuccess } from '@proton/pass/store/actions';
-import type { ShareType } from '@proton/pass/types';
+import type { Maybe, ShareType } from '@proton/pass/types';
 import { type Share } from '@proton/pass/types';
 
-function* createVaultWorker({ payload, meta }: ReturnType<typeof vaultCreationIntent>) {
-    const { callback: onCreateVaultProcessed } = meta;
+export function* createVaultWorker({
+    payload,
+    meta,
+}: ReturnType<typeof vaultCreationIntent>): Generator<unknown, Maybe<string>> {
     try {
         const share: Share<ShareType.Vault> = yield createVault({ content: payload.content });
-
-        const vaultCreationSuccessAction = vaultCreationSuccess(meta.request.id, { share });
-        yield put(vaultCreationSuccessAction);
-
-        onCreateVaultProcessed?.(vaultCreationSuccessAction);
+        yield put(vaultCreationSuccess(meta.request.id, { share }));
+        return share.shareId;
     } catch (e) {
-        const vaultCreationFailureAction = vaultCreationFailure(meta.request.id, payload, e);
-        yield put(vaultCreationFailureAction);
-
-        onCreateVaultProcessed?.(vaultCreationFailureAction);
+        yield put(vaultCreationFailure(meta.request.id, payload, e));
     }
 }
 
