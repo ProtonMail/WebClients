31e65c2748d4ff32da88582eb73132af1a41c212
Edvin CANDON <edvin.candon@proton.ch>
2025-01-04 19:26:08 +0100

    Refactor item routing to accomodate for future filters
    - Compute item route scope in `NavigationMatches`
    - Remove `trash` optional path segment
    - Define global item route scope prefixes for matching and resolving
      selected item from route params

--- packages/pass/components/Navigation/NavigationMatches.tsx
+++ packages/pass/components/Navigation/NavigationMatches.tsx
@@ -1,46 +1,31 @@
 import type { PropsWithChildren } from 'react';
 import { type FC, createContext, useMemo } from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { matchPath, useLocation, useRouteMatch } from 'react-router-dom';
 
 import { createUseContext } from '@proton/pass/hooks/useContextFactory';
-import type { MaybeNull } from '@proton/pass/types';
+import type { Maybe, MaybeNull } from '@proton/pass/types';
 
-import { getItemRoute, getLocalPath, getMonitorRoute, getOnboardingRoute, getTrashRoute } from './routing';
+import type { ItemScope } from './routing';
+import { ItemScopes, getLocalPath } from './routing';
 
 export type NavigationMatchesContextValue = {
-    /** Flag indicating whether we are currently on an item list page  */
-    matchItemList: boolean;
-    /** Flag indicating whether we are currently on the onboarding route */
-    matchOnboarding: boolean;
-    /** Flag indicating whether we are currently on any monitor route */
-    matchMonitor: boolean;
-    /** Flag indicating whether we are currently on a trash route */
-    matchTrash: boolean;
+    /** Item route scope */
+    itemScope: Maybe<ItemScope>;
 };
 
 const NavigationMatchesContext = createContext<MaybeNull<NavigationMatchesContextValue>>(null);
 
 export const NavigationMatches: FC<PropsWithChildren> = ({ children }) => {
-    const matchTrash = useRouteMatch(getTrashRoute()) !== null;
-    const matchOnboarding = useRouteMatch(getOnboardingRoute()) !== null;
-    const matchMonitor = useRouteMatch(getMonitorRoute()) !== null;
-    const matchRootExact = useRouteMatch({ exact: true, path: getLocalPath() }) !== null;
+    const { pathname } = useLocation();
 
-    const itemRoute = getItemRoute(':shareId', ':itemId', { trashed: matchTrash });
-    const matchItemExact = useRouteMatch({ exact: true, path: itemRoute }) !== null;
-    const matchItemList = matchRootExact || matchItemExact || matchTrash;
+    const matchRootExact = useRouteMatch({ exact: true, path: getLocalPath() }) !== null;
+    const itemScope: Maybe<ItemScope> = matchRootExact
+        ? 'share' /** root scope is "all vaults" scope */
+        : ItemScopes.find((prefix) => matchPath(pathname, getLocalPath(prefix)) !== null);
 
     return (
         <NavigationMatchesContext.Provider
-            value={useMemo<NavigationMatchesContextValue>(
-                () => ({
-                    matchItemList,
-                    matchOnboarding,
-                    matchMonitor,
-                    matchTrash,
-                }),
-                [matchItemList, matchOnboarding, matchMonitor, matchTrash]
-            )}
+            value={useMemo<NavigationMatchesContextValue>(() => ({ itemScope }), [itemScope])}
         >
             {children}
         </NavigationMatchesContext.Provider>
@@ -48,3 +33,4 @@ export const NavigationMatches: FC<PropsWithChildren> = ({ children }) => {
 };
 
 export const useNavigationMatches = createUseContext(NavigationMatchesContext);
+export const useItemScope = () => useNavigationMatches().itemScope;
--- packages/pass/components/Navigation/routing.ts
+++ packages/pass/components/Navigation/routing.ts
@@ -2,19 +2,38 @@ import { type Location, createBrowserHistory } from 'history';
 
 import { decodeUtf8Base64, encodeUtf8Base64 } from '@proton/crypto/lib/utils';
 import { authStore } from '@proton/pass/lib/auth/store';
-import type { ItemFilters, ItemType, MaybeNull } from '@proton/pass/types';
+import type { ItemFilters, ItemType, MaybeNull, Unpack } from '@proton/pass/types';
 import { partialMerge } from '@proton/pass/utils/object/merge';
 import { getLocalIDPath, stripLocalBasenameFromPathname } from '@proton/shared/lib/authentication/pathnameHelper';
 import { APPS } from '@proton/shared/lib/constants';
 import { getAppUrlFromApiUrl } from '@proton/shared/lib/helpers/url';
 
 export type ItemNewRouteParams = { type: ItemType };
-export type ItemRouteOptions = { trashed?: boolean; prefix?: string };
+export type ItemRouteOptions = { scope?: ItemScope };
 export type AuthRouteState = { error?: string; userInitiatedLock?: boolean };
+export type ItemScope = Unpack<typeof ItemScopes>;
+export type PremiumRoute = Unpack<typeof PremiumRoutes>;
 
-export enum UnauthorizedRoutes {
+export enum PublicRoutes {
     SecureLink = '/secure-link/:token',
 }
+
+/** URL prefixes that define different item scopes.
+ * These are prepended to base item routes to create
+ * full paths over `:shareId/item/:itemId` */
+export const ItemScopes = [
+    'share',
+    'trash',
+    'secure-links',
+    'monitor/weak',
+    'monitor/duplicates',
+    'monitor/2fa',
+    'monitor/excluded',
+] as const;
+
+/** Route segments that should only be accessible
+ * to paid users - they should redirect otherwise. */
+export const PremiumRoutes = ['secure-links'] as const;
 
 export const history = createBrowserHistory();
 
@@ -36,20 +55,26 @@ export const removeLocalPath = (path: string) => {
 };
 
 export const subPath = (path: string, sub: string) => `${path}/${sub}`;
-export const maybeTrash = (path: string, inTrash?: boolean) => (inTrash ? subPath('trash', path) : path);
 
 /** Resolves the item route given a shareId and an itemId. */
 export const getItemRoute = (shareId: string, itemId: string, options?: ItemRouteOptions) => {
-    const basePath = maybeTrash(`share/${shareId}/item/${itemId}`, options?.trashed);
-    const prefixed = options?.prefix ? subPath(options.prefix, basePath) : basePath;
+    const basePath = `${shareId}/item/${itemId}`;
+    const prefixed = subPath(options?.scope ?? 'share', basePath);
     return getLocalPath(prefixed);
 };
+
+export const getItemRoutes = () => ItemScopes.map((scope) => getItemRoute(':shareId', ':itemId', { scope }));
 
 export const getItemHistoryRoute = (shareId: string, itemId: string, options?: ItemRouteOptions) =>
     `${getItemRoute(shareId, itemId, options)}/history`;
 
 /** Resolves the new item route given an item type. */
-export const getNewItemRoute = (type: ItemType) => getLocalPath(`item/new/${type}`);
+export const getNewItemRoute = (type: ItemType, scope: ItemScope = 'share'): string => {
+    const basePath = `item/new/${type}`;
+    const prefixed = subPath(scope, basePath);
+    return getLocalPath(prefixed);
+};
+
 export const getTrashRoute = () => getLocalPath('trash');
 export const getOnboardingRoute = () => getLocalPath('onboarding');
 export const getSecureLinksRoute = () => getLocalPath('secure-links');
@@ -121,7 +146,7 @@ const pathMatchesRoute = (path: string, routeTemplate: string): boolean => {
 };
 
 export const isUnauthorizedPath = ({ pathname }: Location): boolean =>
-    Object.values(UnauthorizedRoutes).some((route) => pathMatchesRoute(pathname, route));
+    Object.values(PublicRoutes).some((route) => pathMatchesRoute(pathname, route));
 
 /** In Electron, direct `location.href` mutations don't work due
  * to custom URL schemes. We use IPC via the `ContextBridgeApi` to
