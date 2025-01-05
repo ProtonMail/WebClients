43d699a9e5b97adcabb478769d6a2e31dde048ad
Edvin CANDON <edvin.candon@proton.ch>
2025-01-05 09:29:19 +0100

    Fine-tune ItemsList
    - Move item draggable check away from `useItemDrag`
    - Split item list sub-components (placeholders, headers, actions)
    - Allow customizing items list header (secure-links)

---
+++ packages/pass/components/Item/List/Placeholder/QuickActionsPlaceholder.tsx
@@ -0,0 +1,126 @@
+import type { FC } from 'react';
+import { type MouseEvent, useCallback, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+
+import { c } from 'ttag';
+
+import { Button, type ButtonLikeShape } from '@proton/atoms';
+import type { IconName } from '@proton/components';
+import { Icon } from '@proton/components';
+import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
+import { itemTypeToIconName } from '@proton/pass/components/Layout/Icon/ItemIcon';
+import { SubTheme } from '@proton/pass/components/Layout/Theme/types';
+import { useNavigate } from '@proton/pass/components/Navigation/NavigationActions';
+import { useNavigationFilters } from '@proton/pass/components/Navigation/NavigationFilters';
+import { useItemScope } from '@proton/pass/components/Navigation/NavigationMatches';
+import { getNewItemRoute } from '@proton/pass/components/Navigation/routing';
+import { isWritableVault } from '@proton/pass/lib/vaults/vault.predicates';
+import { selectAllVaults, selectShare } from '@proton/pass/store/selectors';
+import type { ItemType } from '@proton/pass/types';
+import clsx from '@proton/utils/clsx';
+
+type ItemQuickAction = {
+    hidden?: boolean;
+    icon: IconName;
+    label: string;
+    shape?: ButtonLikeShape;
+    subTheme?: SubTheme;
+    type: ItemType | 'import';
+    onClick: (event: MouseEvent<HTMLElement>) => void;
+};
+
+export const QuickActionsPlaceholder: FC = () => {
+    const { openSettings } = usePassCore();
+    const { filters } = useNavigationFilters();
+    const scope = useItemScope();
+
+    const navigate = useNavigate();
+    const hasMultipleVaults = useSelector(selectAllVaults).length > 1;
+    const { selectedShareId } = filters;
+
+    const selectedShare = useSelector(selectShare(selectedShareId));
+    const onCreate = useCallback((type: ItemType) => navigate(getNewItemRoute(type, scope)), [scope]);
+
+    const quickActions = useMemo<ItemQuickAction[]>(
+        () => [
+            {
+                icon: itemTypeToIconName.login,
+                label: c('Label').t`Create a login`,
+                subTheme: SubTheme.VIOLET,
+                type: 'login',
+                onClick: () => onCreate('login'),
+            },
+            {
+                icon: itemTypeToIconName.alias,
+                label: c('Label').t`Create a hide-my-email alias`,
+                subTheme: SubTheme.TEAL,
+                type: 'alias',
+                onClick: () => onCreate('alias'),
+            },
+            {
+                icon: itemTypeToIconName.creditCard,
+                label: c('Label').t`Create a credit card`,
+                subTheme: SubTheme.LIME,
+                type: 'creditCard',
+                onClick: () => onCreate('creditCard'),
+            },
+            {
+                icon: itemTypeToIconName.note,
+                label: c('Label').t`Create an encrypted note`,
+                subTheme: SubTheme.ORANGE,
+                type: 'note',
+                onClick: () => onCreate('note'),
+            },
+            {
+                icon: itemTypeToIconName.identity,
+                label: c('Label').t`Create an Identity`,
+                type: 'identity',
+                onClick: () => onCreate('identity'),
+            },
+            {
+                type: 'import',
+                icon: 'arrow-up-line',
+                shape: 'outline',
+                label: c('Label').t`Import passwords`,
+                onClick: () => openSettings?.('import'),
+            },
+        ],
+        [onCreate]
+    );
+
+    return (
+        <div className="flex flex-column gap-3 text-center">
+            <div className="flex flex-column gap-1">
+                <strong className="inline-block">{c('Title').t`Your vault is empty`}</strong>
+                <span className="color-weak inline-block mb-2">
+                    {hasMultipleVaults
+                        ? c('Info').t`Switch to another vault or create an item in this vault`
+                        : c('Info').t`Let's get you started by creating your first item`}
+                </span>
+            </div>
+
+            {quickActions
+                .filter(({ hidden }) => !hidden)
+                .map(({ type, icon, label, shape, subTheme, onClick }) => (
+                    <Button
+                        pill
+                        shape={shape ?? 'solid'}
+                        color="weak"
+                        key={`quick-action-${type}`}
+                        className={clsx('pass-sub-sidebar--hidable w-full relative', subTheme)}
+                        onClick={onClick}
+                        disabled={selectedShare && !isWritableVault(selectedShare)}
+                        size={EXTENSION_BUILD ? 'small' : 'medium'}
+                    >
+                        <Icon
+                            name={icon}
+                            color="var(--interaction-norm)"
+                            className="absolute left-custom top-0 bottom-0 my-auto"
+                            style={{ '--left-custom': '1rem' }}
+                        />
+                        <span className="max-w-full px-8 text-ellipsis">{label}</span>
+                    </Button>
+                ))}
+        </div>
+    );
+};
