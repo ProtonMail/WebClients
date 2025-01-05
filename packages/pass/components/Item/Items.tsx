f001a5fc36fd7b5e3fc72a2a1852eae76b38e2fe
Edvin CANDON <edvin.candon@proton.ch>
2025-01-05 09:34:57 +0100

    Move secure links data resolution to `ItemsProvider`
    - Allows proper filtering of secure link items

--- packages/pass/components/Item/Items.tsx
+++ packages/pass/components/Item/Items.tsx
@@ -1,20 +1,28 @@
 import { type FC } from 'react';
-import { type RouteChildrenProps } from 'react-router-dom';
+import { Route, type RouteChildrenProps } from 'react-router-dom';
 
+import { ItemsProvider } from '@proton/pass/components/Item/Context/ItemsProvider';
 import { Content } from '@proton/pass/components/Layout/Section/Content';
 import { SubSidebar } from '@proton/pass/components/Layout/Section/SubSidebar';
 import { Autoselect } from '@proton/pass/components/Navigation/Autoselect';
 import { ItemSwitch } from '@proton/pass/components/Navigation/ItemSwitch';
+import { ItemScopes } from '@proton/pass/components/Navigation/routing';
 
 import { ItemsList } from './List/ItemsList';
 
-export const Items: FC<RouteChildrenProps> = (subRoute) => (
-    <>
-        <SubSidebar>
-            <ItemsList />
-        </SubSidebar>
-        <Content>
-            <ItemSwitch fallback={Autoselect} {...subRoute} />
-        </Content>
-    </>
-);
+export const Items: FC<RouteChildrenProps> = (subRoute) => {
+    const { match } = subRoute;
+
+    const itemRoutes = match ? ItemScopes.map((route) => `${match.path}/${route}`).concat(match.path) : [];
+
+    return (
+        <ItemsProvider>
+            <SubSidebar>
+                <ItemsList />
+            </SubSidebar>
+            <Content>
+                <Route path={itemRoutes}>{(itemRoute) => <ItemSwitch fallback={Autoselect} {...itemRoute} />}</Route>
+            </Content>
+        </ItemsProvider>
+    );
+};
