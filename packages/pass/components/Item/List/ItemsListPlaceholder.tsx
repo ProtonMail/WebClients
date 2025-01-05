790a7995af47053d71a0bc3d4a87fa210c30c579
Edvin CANDON <edvin.candon@proton.ch>
2025-01-05 09:29:19 +0100

    Fine-tune ItemsList
    - Move item draggable check away from `useItemDrag`
    - Split item list sub-components (placeholders, headers, actions)
    - Allow customizing items list header (secure-links)

--- packages/pass/components/Item/List/ItemsListPlaceholder.tsx
+++ packages/pass/components/Item/List/ItemsListPlaceholder.tsx
@@ -1,189 +1,39 @@
-import { type FC, type MouseEvent, useMemo } from 'react';
-import { useSelector } from 'react-redux';
+import { memo } from 'react';
 
 import { c } from 'ttag';
 
-import { Button, type ButtonLikeShape } from '@proton/atoms';
-import type { IconName } from '@proton/components';
-import { Icon } from '@proton/components';
-import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
-import { useItems } from '@proton/pass/components/Item/Context/ItemsProvider';
-import { UpgradeButton } from '@proton/pass/components/Layout/Button/UpgradeButton';
-import { Card } from '@proton/pass/components/Layout/Card/Card';
-import { itemTypeToIconName } from '@proton/pass/components/Layout/Icon/ItemIcon';
-import { SubTheme } from '@proton/pass/components/Layout/Theme/types';
-import { useNavigate } from '@proton/pass/components/Navigation/NavigationActions';
-import { useNavigationFilters } from '@proton/pass/components/Navigation/NavigationFilters';
-import { useNavigationMatches } from '@proton/pass/components/Navigation/NavigationMatches';
-import { getNewItemRoute } from '@proton/pass/components/Navigation/routing';
-import { UpsellRef } from '@proton/pass/constants';
-import { isWritableVault } from '@proton/pass/lib/vaults/vault.predicates';
-import { selectAllVaults, selectOwnReadOnlyVaults, selectShare, selectVaultLimits } from '@proton/pass/store/selectors';
-import type { ItemType } from '@proton/pass/types';
-import { prop } from '@proton/pass/utils/fp/lens';
-import clsx from '@proton/utils/clsx';
+import { SearchableListPlaceholder } from '@proton/pass/components/Item/List/Placeholder/SearchableListPlaceholder';
+import { VaultPlaceholder } from '@proton/pass/components/Item/List/Placeholder/VaultPlaceholder';
+import { useItemScope } from '@proton/pass/components/Navigation/NavigationMatches';
+import { SecureLinksLoading } from '@proton/pass/components/SecureLink/SecureLinksLoading';
 
-type ItemQuickAction = {
-    hidden?: boolean;
-    icon: IconName;
-    label: string;
-    shape?: ButtonLikeShape;
-    subTheme?: SubTheme;
-    type: ItemType | 'import';
-    onClick: (event: MouseEvent<HTMLElement>) => void;
-};
+export const ItemsListPlaceholder = memo(() => {
+    const scope = useItemScope();
 
-type Props = {
-    /** When `noActions` is `true`, item quick actions will not render */
-    noActions?: boolean;
-};
-
-export const ItemsListPlaceholder: FC<Props> = ({ noActions }) => {
-    const { openSettings } = usePassCore();
-    const { filters } = useNavigationFilters();
-    const { matchTrash } = useNavigationMatches();
-    const navigate = useNavigate();
-    const { search, selectedShareId } = filters;
-    const { totalCount } = useItems();
-
-    const { didDowngrade } = useSelector(selectVaultLimits);
-    const selectedShare = useSelector(selectShare(selectedShareId));
-    const ownedReadOnlyShareIds = useSelector(selectOwnReadOnlyVaults).map(prop('shareId'));
-    const isOwnedReadOnly = selectedShareId && ownedReadOnlyShareIds.includes(selectedShareId);
-    const hasMultipleVaults = useSelector(selectAllVaults).length > 1;
-
-    const empty = totalCount === 0;
-    const hasSearch = Boolean(search.trim());
-    const showUpgrade = isOwnedReadOnly && totalCount === 0 && didDowngrade;
-
-    const quickActions = useMemo<ItemQuickAction[]>(
-        () => [
-            {
-                icon: itemTypeToIconName.login,
-                label: c('Label').t`Create a login`,
-                subTheme: SubTheme.VIOLET,
-                type: 'login',
-                onClick: () => navigate(getNewItemRoute('login')),
-            },
-            {
-                icon: itemTypeToIconName.alias,
-                label: c('Label').t`Create a hide-my-email alias`,
-                subTheme: SubTheme.TEAL,
-                type: 'alias',
-                onClick: () => navigate(getNewItemRoute('alias')),
-            },
-            {
-                icon: itemTypeToIconName.creditCard,
-                label: c('Label').t`Create a credit card`,
-                subTheme: SubTheme.LIME,
-                type: 'creditCard',
-                onClick: () => navigate(getNewItemRoute('creditCard')),
-            },
-            {
-                icon: itemTypeToIconName.note,
-                label: c('Label').t`Create an encrypted note`,
-                subTheme: SubTheme.ORANGE,
-                type: 'note',
-                onClick: () => navigate(getNewItemRoute('note')),
-            },
-            {
-                icon: itemTypeToIconName.identity,
-                label: c('Label').t`Create an Identity`,
-                type: 'identity',
-                onClick: () => navigate(getNewItemRoute('identity')),
-            },
-            {
-                type: 'import',
-                icon: 'arrow-up-line',
-                shape: 'outline',
-                label: c('Label').t`Import passwords`,
-                onClick: () => openSettings?.('import'),
-            },
-        ],
-        []
-    );
-
-    if (showUpgrade && !matchTrash) {
-        return (
-            <div
-                className="flex flex-column items-center gap-3 text-center p-2 w-2/3 max-w-custom"
-                style={{ '--max-w-custom': '20rem' }}
-            >
-                <span className="text-semibold inline-block">{c('Title').t`Your vault is empty`}</span>
-                <Card type="primary" className="text-sm">
-                    {c('Info')
-                        .t`You have exceeded the number of vaults included in your subscription. New items can only be created in your first two vaults. To create new items in all vaults upgrade your subscription.`}
-                </Card>
-                <UpgradeButton upsellRef={UpsellRef.LIMIT_VAULT} className="pass-sub-sidebar--hidable" />
-            </div>
-        );
-    }
-
-    if (empty && !matchTrash) {
-        return (
-            <div className="flex flex-column gap-3 text-center">
-                <div className="flex flex-column gap-1">
-                    <strong className="inline-block">{c('Title').t`Your vault is empty`}</strong>
-                    <span className="color-weak inline-block mb-2">
-                        {hasMultipleVaults
-                            ? c('Info').t`Switch to another vault or create an item in this vault`
-                            : c('Info').t`Let's get you started by creating your first item`}
-                    </span>
-                </div>
-
-                {!noActions &&
-                    quickActions
-                        .filter(({ hidden }) => !hidden)
-                        .map(({ type, icon, label, shape, subTheme, onClick }) => (
-                            <Button
-                                pill
-                                shape={shape ?? 'solid'}
-                                color="weak"
-                                key={`quick-action-${type}`}
-                                className={clsx('pass-sub-sidebar--hidable w-full relative', subTheme)}
-                                onClick={onClick}
-                                disabled={selectedShare && !isWritableVault(selectedShare)}
-                                size={EXTENSION_BUILD ? 'small' : 'medium'}
-                            >
-                                <Icon
-                                    name={icon}
-                                    color="var(--interaction-norm)"
-                                    className="absolute left-custom top-0 bottom-0 my-auto"
-                                    style={{ '--left-custom': '1rem' }}
-                                />
-                                <span className="max-w-full px-8 text-ellipsis">{label}</span>
-                            </Button>
-                        ))}
-            </div>
-        );
-    }
-
-    return (
-        <span className="block color-weak text-sm p-2 text-center text-break">
-            {(() => {
-                if (matchTrash) {
-                    return empty || !hasSearch ? (
-                        <span>
-                            <strong>{c('Title').t`Trash empty`}</strong>
-                            <br /> {c('Info').t`Deleted items will be moved here first`}
-                        </span>
-                    ) : (
-                        <span>
-                            {c('Warning').t`No items in trash matching`}
-                            <br />"{search}"
-                        </span>
-                    );
-                }
-
-                return hasSearch ? (
-                    <span>
-                        {c('Warning').t`No items matching`}
-                        <br />"{search}"
-                    </span>
-                ) : (
-                    c('Warning').t`No items`
+    return (() => {
+        switch (scope) {
+            case 'trash':
+                return (
+                    <SearchableListPlaceholder
+                        emptyListTitle={c('Title').t`Trash empty`}
+                        emptyListMessage={c('Info').t`Deleted items will be moved here first`}
+                        noResultsMessage={c('Warning').t`No items in trash matching`}
+                    />
                 );
-            })()}
-        </span>
-    );
-};
+            case 'secure-links':
+                return (
+                    <SecureLinksLoading>
+                        <SearchableListPlaceholder
+                            emptyListTitle={c('Title').t`No shared secure links`}
+                            noResultsMessage={c('Warning').t`No secure links matching`}
+                        />
+                    </SecureLinksLoading>
+                );
+
+            default:
+                return <VaultPlaceholder />;
+        }
+    })();
+});
+
+ItemsListPlaceholder.displayName = 'ItemsListPlaceholderMemo';
