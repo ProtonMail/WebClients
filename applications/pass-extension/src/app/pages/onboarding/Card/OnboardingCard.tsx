85d33024ec018531117db349850ece19684ac26b
Edvin CANDON <edvin.candon@proton.ch>
2025-01-06 12:36:49 +0100

    Fine-tune pending share access notification in extension welcome screen

---
+++ applications/pass-extension/src/app/pages/onboarding/Card/OnboardingCard.tsx
@@ -0,0 +1,13 @@
+import type { PropsWithChildren } from 'react';
+import { type FC } from 'react';
+
+import clsx from '@proton/utils/clsx';
+
+import './OnboardingCard.scss';
+
+type Props = { className?: string };
+export const OnboardingCard: FC<PropsWithChildren<Props>> = ({ className, children }) => (
+    <div className={clsx('pass-onboarding--card flex rounded-xl flex-nowrap py-4 px-6 border border-weak', className)}>
+        {children}
+    </div>
+);
