4c128825583b58e7c499fda0800486ff106f8b95
Edvin CANDON <edvin.candon@proton.ch>
2025-01-06 12:11:51 +0100

    Fix onboarding header not showing pass text logo

---
+++ applications/pass-extension/src/app/pages/onboarding/Header/OnboardingHeader.tsx
@@ -0,0 +1,20 @@
+import { type FC } from 'react';
+
+import { PassTextLogo } from '@proton/pass/components/Layout/Logo/PassTextLogo';
+import { PASS_APP_NAME } from '@proton/shared/lib/constants';
+
+import './OnboardingHeader.scss';
+
+export const OnboardingHeader: FC = () => (
+    <div className="flex items-center gap-2 mb-5">
+        {
+            <img
+                src="/assets/protonpass-icon.svg"
+                className="h-custom"
+                style={{ '--h-custom': '2.25rem' }}
+                alt={PASS_APP_NAME}
+            />
+        }
+        <PassTextLogo key="pass-text-logo" className="pass-onboarding--brand-text ml-2 shrink-0 logo" />
+    </div>
+);
