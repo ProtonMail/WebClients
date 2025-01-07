085351253b1e9dcc39d2dbb8348e71b2c16daac3
Edvin CANDON <edvin.candon@proton.ch>
2025-01-07 08:31:50 +0100

    Revamped `Shared` section in sidebar menu

---
+++ packages/pass/components/Menu/Shared/SharedMenu.tsx
@@ -0,0 +1,58 @@
+import { memo } from 'react';
+import { useSelector } from 'react-redux';
+
+import { c } from 'ttag';
+
+import { SharedMenuItem } from '@proton/pass/components/Menu/Shared/SharedMenuItem';
+import { useItemScope } from '@proton/pass/components/Navigation/NavigationMatches';
+import { PassPlusPromotionButton } from '@proton/pass/components/Upsell/PassPlusPromotionButton';
+import { UpsellRef } from '@proton/pass/constants';
+import { selectAllSecureLinks, selectPassPlan } from '@proton/pass/store/selectors';
+import { UserPassPlan } from '@proton/pass/types/api/plan';
+
+export const SharedMenu = memo(() => {
+    const scope = useItemScope();
+
+    const secureLinks = useSelector(selectAllSecureLinks);
+    const passPlan = useSelector(selectPassPlan);
+    const free = passPlan === UserPassPlan.FREE;
+
+    return (
+        <div className="flex flex-column gap-2 w-full">
+            <div className="mx-2 flex flex-nowrap items-center gap-3">
+                <span className="text-ellipsis color-weak"> {c('Label').t`Shared`}</span>
+                {free && <PassPlusPromotionButton style={{ '--background-norm': 'var(--background-strong)' }} />}
+            </div>
+            <div className="flex">
+                <SharedMenuItem
+                    upsellRef={free ? UpsellRef.ITEM_SHARING : undefined}
+                    label={c('Label').t`Shared with me`}
+                    count={0}
+                    selected={scope === 'shared-with-me'}
+                    to="shared-with-me"
+                    icon="user-arrow-left"
+                />
+
+                <SharedMenuItem
+                    upsellRef={free ? UpsellRef.ITEM_SHARING : undefined}
+                    label={c('Label').t`Shared by me`}
+                    count={0}
+                    selected={scope === 'shared-by-me'}
+                    to="shared-by-me"
+                    icon="user-arrow-right"
+                />
+
+                <SharedMenuItem
+                    upsellRef={free ? UpsellRef.SECURE_LINKS : undefined}
+                    label={c('Action').t`Secure links`}
+                    count={secureLinks.length}
+                    selected={scope === 'secure-links'}
+                    to="secure-links"
+                    icon="link"
+                />
+            </div>
+        </div>
+    );
+});
+
+SharedMenu.displayName = 'SharedMenuMenuMemo';
--- packages/pass/components/Menu/Shared/SharedMenuItem.tsx
+++ packages/pass/components/Menu/Shared/SharedMenuItem.tsx
@@ -3,32 +3,31 @@ import { memo } from 'react';
 import Icon, { type IconName } from '@proton/components/components/icon/Icon';
 import { DropdownMenuButton } from '@proton/pass/components/Layout/Dropdown/DropdownMenuButton';
 import { useNavigate } from '@proton/pass/components/Navigation/NavigationActions';
-import { getLocalPath } from '@proton/pass/components/Navigation/routing';
+import { getInitialFilters, getLocalPath } from '@proton/pass/components/Navigation/routing';
 import { useUpselling } from '@proton/pass/components/Upsell/UpsellingProvider';
-import { VaultIcon } from '@proton/pass/components/Vault/VaultIcon';
 import type { UpsellRef } from '@proton/pass/constants';
 import { pipe } from '@proton/pass/utils/fp/pipe';
 import clsx from '@proton/utils/clsx';
 import noop from '@proton/utils/noop';
 
 type Props = {
-    to: string;
     count: number;
     dense?: boolean;
-    selected: boolean;
+    icon: IconName;
     label?: string;
-    onAction?: () => void;
-    icon?: IconName;
+    selected: boolean;
+    to: string;
     upsellRef?: UpsellRef;
+    onAction?: () => void;
 };
 
-export const VaultMenuLink = memo(({ to, count, dense, selected, label, icon, upsellRef, onAction = noop }: Props) => {
+export const SharedMenuItem = memo(({ to, count, dense, selected, label, icon, upsellRef, onAction = noop }: Props) => {
     const navigate = useNavigate();
     const upsell = useUpselling();
 
     const onSelect = upsellRef
         ? () => upsell({ type: 'pass-plus', upsellRef })
-        : pipe(() => navigate(getLocalPath(to)), onAction);
+        : pipe(() => navigate(getLocalPath(to), { filters: getInitialFilters() }), onAction);
 
     return (
         <DropdownMenuButton
@@ -38,15 +37,9 @@ export const VaultMenuLink = memo(({ to, count, dense, selected, label, icon, up
             className={clsx(selected && 'is-selected', !dense && 'py-3')}
             style={{ '--max-h-custom': '1.25rem' }}
             extra={<span className="pass-vault--count shrink-0 color-weak mx-1">{count}</span>}
-            icon={
-                icon ? (
-                    <Icon className="shrink-0" name={icon} size={3.5} />
-                ) : (
-                    <VaultIcon className="shrink-0" size={3.5} />
-                )
-            }
+            icon={<Icon className="shrink-0" name={icon} size={3.5} />}
         />
     );
 });
 
-VaultMenuLink.displayName = 'VaultMenuTrashMemo';
+SharedMenuItem.displayName = 'SharedMenuItemMemo';
