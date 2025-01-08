90dd9f68533b24a90eefece8e4a68fe63ea3ec70
Edvin CANDON <edvin.candon@proton.ch>
2025-01-08 18:57:45 +0100

    Refactor item loading state derivation

--- packages/pass/hooks/useItemLoading.ts
+++ packages/pass/hooks/useItemLoading.ts
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 
 import { isTrashed } from '@proton/pass/lib/items/item.predicates';
+import { itemDelete, itemMove, itemRestore, itemTrash } from '@proton/pass/store/actions';
 import {
     itemsBulkDeleteRequest,
     itemsBulkMoveRequest,
@@ -18,23 +19,32 @@ const bulkSelectionContains =
     (dto: MaybeNull<BulkSelectionDTO>) =>
         dto?.[shareId]?.[itemId] ?? false;
 
-export const useBulkInFlight = (item: ItemRevision) => {
+export const useItemLoading = (item: ItemRevision) => {
+    const trashing = useSelector(selectRequestInFlight(itemTrash.requestID(item)));
+    const moving = useSelector(selectRequestInFlight(itemMove.requestID(item)));
+    const restoring = useSelector(selectRequestInFlight(itemRestore.requestID(item)));
+    const deleting = useSelector(selectRequestInFlight(itemDelete.requestID(item)));
+
     const bulkMoveDTO = useSelector(selectRequestInFlightData<BulkSelectionDTO>(itemsBulkMoveRequest()));
     const bulkTrashDTO = useSelector(selectRequestInFlightData<BulkSelectionDTO>(itemsBulkTrashRequest()));
     const bulkRestoreDTO = useSelector(selectRequestInFlightData<BulkSelectionDTO>(itemsBulkRestoreRequest()));
     const bulkRemoveDTO = useSelector(selectRequestInFlightData<BulkSelectionDTO>(itemsBulkDeleteRequest()));
 
     const trashed = isTrashed(item);
-    const trashEmptyInFlight = useSelector(selectRequestInFlight(trashEmptyRequest())) && trashed;
-    const trashRestoreInFlight = useSelector(selectRequestInFlight(trashRestoreRequest())) && trashed;
-    const vaultMoveInFlight = Boolean(useSelector(selectRequestInFlight(vaultMoveAllItemsRequest(item.shareId))));
+    const emptyingTrash = useSelector(selectRequestInFlight(trashEmptyRequest())) && trashed;
+    const restoringTrash = useSelector(selectRequestInFlight(trashRestoreRequest())) && trashed;
+    const movingVault = Boolean(useSelector(selectRequestInFlight(vaultMoveAllItemsRequest(item.shareId))));
 
     const inBulk = bulkSelectionContains(item);
 
     return (
-        trashEmptyInFlight ||
-        trashRestoreInFlight ||
-        vaultMoveInFlight ||
+        trashing ||
+        moving ||
+        restoring ||
+        deleting ||
+        emptyingTrash ||
+        restoringTrash ||
+        movingVault ||
         inBulk(bulkTrashDTO) ||
         inBulk(bulkMoveDTO) ||
         inBulk(bulkRestoreDTO) ||
