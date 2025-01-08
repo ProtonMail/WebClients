021398e6cec34d4f38703feddc2ea3e383a8dfa9
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-01-08 11:03:20 +0100

    Integrate in-app account sessions switcher in drive & docs

--- applications/drive/src/app/redux-store/listener.ts
+++ applications/drive/src/app/redux-store/listener.ts
@@ -1,7 +1,9 @@
+import { startAccountSessionsListener } from '@proton/account';
 import { startSharedListening } from '@proton/redux-shared-store/sharedListeners';
 
 import type { AppStartListening } from './store';
 
 export const start = (startListening: AppStartListening) => {
     startSharedListening(startListening);
+    startAccountSessionsListener(startListening);
 };
