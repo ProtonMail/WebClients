bc7ff4033d3a44401129980d6c277e70c069d121
Jack Crompton <jack.crompton@proton.ch>
2025-01-08 14:06:25 +0100

    Add discount banner to single signup v2
    CP-9181

--- packages/components/components/skeletonLoader/SkeletonLoader.tsx
+++ packages/components/components/skeletonLoader/SkeletonLoader.tsx
@@ -6,19 +6,21 @@ import './SkeletonLoader.scss';
 
 interface Props extends ComponentPropsWithoutRef<'div'> {
     width?: string | number;
+    height?: string | number;
     className?: string;
     style?: CSSProperties;
     index?: number;
 }
 
-const SkeletonLoader = ({ width, className, style, index, ...rest }: Props) => {
+const SkeletonLoader = ({ width, height, className, style, index, ...rest }: Props) => {
     return (
         <div
             aria-hidden="true"
             data-testid="skeleton-loader"
-            className={clsx('skeleton-loader w-custom', className)}
+            className={clsx('skeleton-loader', !!width && 'w-custom', !!height && 'h-custom', className)}
             style={{
                 '--w-custom': width,
+                '--h-custom': height,
                 '--index': index,
                 ...style,
             }}
