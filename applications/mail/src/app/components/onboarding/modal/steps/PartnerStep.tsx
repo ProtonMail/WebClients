b3318fbaefe95f7675699bd15a7974d10508160a
Pedro Lavarinhas Amaro <pedro.lavarinhas.amaro@proton.ch>
2025-01-09 16:11:18 +0000

    Add onboarding modal for Partner integration

---
+++ applications/mail/src/app/components/onboarding/modal/steps/PartnerStep.tsx
@@ -0,0 +1,42 @@
+import { c } from 'ttag';
+
+import { useCustomDomains } from '@proton/account/domains/hooks';
+import { useSubscription } from '@proton/account/subscription/hooks';
+import { Button } from '@proton/atoms';
+import { OnboardingStep, type OnboardingStepRenderCallback } from '@proton/components';
+import { MAIL_APP_NAME } from '@proton/shared/lib/constants';
+import checkConfirmation from '@proton/styles/assets/img/onboarding/img-check-confirmation.svg';
+
+import OnboardingContent from '../layout/OnboardingContent';
+
+const PartnerStep = ({ onNext }: OnboardingStepRenderCallback) => {
+    const [subscription] = useSubscription();
+    const [domains] = useCustomDomains();
+    const domain = domains ? domains[0]?.DomainName : undefined;
+    const plan = subscription ? subscription.Plans : undefined;
+    const subscriptionName = plan ? plan[0].Title : undefined;
+
+    const description =
+        domain === undefined || subscriptionName === undefined
+            ? c('Onboarding modal').t`Your domain and subscription are ready to use.`
+            : c('Onboarding modal').t`Your domain ${domain} and ${subscriptionName} subscription are ready to use.`;
+
+    return (
+        <OnboardingStep>
+            <OnboardingContent
+                topChildren={<img src={checkConfirmation} alt="" />}
+                title={c('Onboarding modal').t`You are all set!`}
+                description={description}
+                className="mb-16 h-custom flex flex-column items-center justify-center"
+                style={{ '--h-custom': '25.625rem' }}
+            ></OnboardingContent>
+            <footer>
+                <Button size="large" color="norm" fullWidth onClick={onNext}>
+                    {c('Onboarding modal').t`Discover ${MAIL_APP_NAME}`}
+                </Button>
+            </footer>
+        </OnboardingStep>
+    );
+};
+
+export default PartnerStep;
