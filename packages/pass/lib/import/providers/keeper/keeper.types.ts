f9a3ecd775c411ca4ecc405c13d89b8d51872681
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2025-01-14 14:15:59 +0100

    Support Keeper JSON import
    [IDTEAM-4234]

--- packages/pass/lib/import/providers/keeper/keeper.types.ts
+++ packages/pass/lib/import/providers/keeper/keeper.types.ts
@@ -1,20 +1,44 @@
-export type KeeperItem = [
-    /* folder name */
-    string,
-    /* title (can't be an empty string) */
-    string,
-    /* username */
-    string,
-    /* url */
-    string,
-    /* password */
-    string,
-    /* note */
-    string,
-    /* shared folder name */
-    string,
-    /* custom fields:
-     * KeeperItem[7 + n]: label of a custom field
-     * KeeperItem[7 + (n + 1)]: value of that custom field */
-    ...string[],
-];
+import type { MaybeArray } from '@proton/pass/types';
+
+export type KeeperCustomFieldValue = MaybeArray<string | Record<string, string>>;
+
+export type KeeperCustomFields = Record<string, KeeperCustomFieldValue> & {
+    '$note::1'?: string;
+    '$oneTimeCode::1'?: string;
+    '$text:cardholderName:1'?: string;
+    '$pinCode::1'?: string;
+    '$text:company:1'?: string;
+    '$email::1': string;
+    '$paymentCard::1'?: {
+        cardNumber?: string;
+        cardExpirationDate?: string;
+        cardSecurityCode?: string;
+    };
+    '$name::1'?: {
+        first: string;
+        middle: string;
+        last: string;
+    };
+};
+
+export type KeeperFolder = {
+    folder?: string;
+};
+
+export type KeeperItem = {
+    uid: string;
+    title: string;
+    /** This list of item types is not exhaustive */
+    $type?: 'login' | 'encryptedNotes' | 'bankCard' | 'ssnCard' | 'contact' | 'file' | 'address' | 'sshKeys';
+    login?: string;
+    password?: string;
+    login_url?: string;
+    notes?: string;
+    custom_fields?: KeeperCustomFields;
+    folders?: KeeperFolder[] | null;
+};
+
+export type KeeperData = {
+    shared_folders: any[];
+    records: KeeperItem[];
+};
