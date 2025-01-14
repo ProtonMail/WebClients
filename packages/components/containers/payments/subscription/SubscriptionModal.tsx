c3b3b62f074d46ce7bf040555e8e6e3cf745f0fe
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-01-14 15:42:30 +0000

    Merge branch 'refac-subscription-modal' into 'main'
    Create SubscriptionModal component
    
    See merge request web/clients!15000

---
+++ packages/components/containers/payments/subscription/SubscriptionModal.tsx
@@ -0,0 +1,189 @@
+import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
+
+import { useOrganization } from '@proton/account/organization/hooks';
+import { usePaymentStatus } from '@proton/account/paymentStatus/hooks';
+import { usePlans } from '@proton/account/plans/hooks';
+import { useSubscription } from '@proton/account/subscription/hooks';
+import { useUser } from '@proton/account/user/hooks';
+import ModalTwo from '@proton/components/components/modalTwo/Modal';
+import ModalTwoContent from '@proton/components/components/modalTwo/ModalContent';
+import ModalTwoFooter from '@proton/components/components/modalTwo/ModalFooter';
+import ModalTwoHeader from '@proton/components/components/modalTwo/ModalHeader';
+import useModalState from '@proton/components/components/modalTwo/useModalState';
+import { BilledUserModal } from '@proton/components/payments/client-extensions/billed-user';
+import { APPS, type APP_NAMES } from '@proton/shared/lib/constants';
+import { invokeInboxDesktopIPC } from '@proton/shared/lib/desktop/ipcHelpers';
+import { getHas2024OfferCoupon, isManagedExternally } from '@proton/shared/lib/helpers/subscription';
+import { isBilledUser } from '@proton/shared/lib/interfaces';
+import { FREE_PLAN } from '@proton/shared/lib/subscription/freePlans';
+import clsx from '@proton/utils/clsx';
+
+import { useHasInboxDesktopInAppPayments } from '../../desktop/useHasInboxDesktopInAppPayments';
+import InAppPurchaseModal from './InAppPurchaseModal';
+import SubscriptionContainer from './SubscriptionContainer';
+import type { OpenCallbackProps, SubscriptionOverridableStep } from './SubscriptionModalProvider';
+import { SUBSCRIPTION_STEPS, subscriptionModalClassName } from './constants';
+
+interface Props {
+    app: APP_NAMES;
+    onClose?: () => void;
+    onDepsLoaded: () => void;
+}
+
+export interface SubscriptionModalFowardedRefProps {
+    isOpened: boolean;
+    /** Opens the subscription modal */
+    open: (subscriptionProps: OpenCallbackProps) => void;
+}
+
+const isOverridablableStep = (step: SUBSCRIPTION_STEPS): step is SubscriptionOverridableStep =>
+    [SUBSCRIPTION_STEPS.UPGRADE, SUBSCRIPTION_STEPS.THANKS].includes(step);
+
+const SubscriptionModal = forwardRef<SubscriptionModalFowardedRefProps, Props>(
+    ({ app, onClose, onDepsLoaded }, ref) => {
+        const [user] = useUser();
+        const subscriptionPropsRef = useRef<OpenCallbackProps | null>(null);
+        const [subscription, loadingSubscription] = useSubscription();
+        const [plansResult, loadingPlans] = usePlans();
+        const plans = plansResult?.plans || [];
+        const freePlan = plansResult?.freePlan || FREE_PLAN;
+        const [status, statusLoading] = usePaymentStatus();
+        const hasInboxDesktopInAppPayments = useHasInboxDesktopInAppPayments();
+        const [organization, loadingOrganization] = useOrganization();
+        const [modalState, setModalState, render] = useModalState();
+
+        const depsLoading = loadingSubscription || loadingPlans || loadingOrganization || statusLoading;
+
+        useImperativeHandle(ref, () => {
+            return {
+                isOpened: modalState.open,
+                open: (subscriptionProps) => {
+                    subscriptionPropsRef.current = subscriptionProps;
+                    setModalState(true);
+                },
+            };
+        });
+
+        useEffect(() => {
+            if (!depsLoading) {
+                onDepsLoaded();
+            }
+        }, [depsLoading]);
+
+        if (depsLoading || !subscriptionPropsRef.current || !(organization && subscription && render && status)) {
+            return null;
+        }
+
+        if (isManagedExternally(subscription)) {
+            return <InAppPurchaseModal subscription={subscription} {...modalState} />;
+        } else if (isBilledUser(user)) {
+            return <BilledUserModal user={user} {...modalState} />;
+        }
+
+        const {
+            hasClose,
+            onClose: subscriptionPropsOnClose,
+            disableCloseOnEscape,
+            fullscreen,
+            onSubscribed,
+            onUnsubscribed,
+            mode,
+            currency,
+            renderCustomStepModal,
+            ...rest
+        } = subscriptionPropsRef.current;
+
+        if (hasInboxDesktopInAppPayments && modalState.open) {
+            void invokeInboxDesktopIPC({ type: 'subscriptionModalOpened', payload: 'subscriptionModalStarted' });
+        }
+
+        const handleClose = () => {
+            if (hasInboxDesktopInAppPayments) {
+                void invokeInboxDesktopIPC({
+                    type: 'subscriptionModalOpened',
+                    payload: 'subscriptionModalFinished',
+                });
+            }
+
+            onClose?.();
+            subscriptionPropsOnClose?.();
+            modalState.onClose();
+        };
+
+        let blurBackdrop = true;
+        let rootClassName: string | undefined;
+        if (getHas2024OfferCoupon(rest.coupon)) {
+            blurBackdrop = false;
+            if (app === APPS.PROTONVPN_SETTINGS) {
+                rootClassName = 'subscription-modal-bf-bg subscription-modal--vpn-bg';
+            } else if (app === APPS.PROTONPASS) {
+                rootClassName = 'subscription-modal-bf-bg subscription-modal--pass-bg';
+            } else if (app === APPS.PROTONDRIVE) {
+                rootClassName = 'subscription-modal-bf-bg subscription-modal--drive-bg';
+            } else {
+                rootClassName = 'subscription-modal-bf-bg subscription-modal--mail-bg';
+            }
+        }
+
+        return (
+            <SubscriptionContainer
+                parent="subscription-modal"
+                app={app}
+                subscription={subscription}
+                plans={plans}
+                freePlan={freePlan}
+                organization={organization}
+                onSubscribed={() => {
+                    handleClose();
+                    onSubscribed?.();
+                }}
+                onUnsubscribed={() => {
+                    handleClose();
+                    onUnsubscribed?.();
+                }}
+                onCancel={handleClose}
+                mode={mode}
+                currency={currency}
+                paymentsStatus={status}
+                {...rest}
+                render={({ onSubmit, title, content, footer, step }) => {
+                    const modal = (
+                        <ModalTwo
+                            blurBackdrop={blurBackdrop}
+                            className={clsx([
+                                subscriptionModalClassName,
+                                [SUBSCRIPTION_STEPS.PLAN_SELECTION, SUBSCRIPTION_STEPS.CHECKOUT].includes(step) &&
+                                    'subscription-modal--fixed-height',
+                                [SUBSCRIPTION_STEPS.PLAN_SELECTION].includes(step) && 'subscription-modal--large-width',
+                                [SUBSCRIPTION_STEPS.CHECKOUT].includes(step) && 'subscription-modal--medium-width',
+                            ])}
+                            rootClassName={rootClassName}
+                            data-testid="plansModal"
+                            {...modalState}
+                            onClose={handleClose}
+                            disableCloseOnEscape={disableCloseOnEscape}
+                            fullscreen={fullscreen}
+                            as="form"
+                            size="large"
+                            onSubmit={onSubmit}
+                        >
+                            <ModalTwoHeader title={title} hasClose={hasClose} />
+                            <ModalTwoContent>{content}</ModalTwoContent>
+                            {footer && <ModalTwoFooter>{footer}</ModalTwoFooter>}
+                        </ModalTwo>
+                    );
+
+                    if (isOverridablableStep(step)) {
+                        return renderCustomStepModal?.(step, modalState) || modal;
+                    }
+
+                    return modal;
+                }}
+            />
+        );
+    }
+);
+
+SubscriptionModal.displayName = 'SubscriptionModal';
+
+export default SubscriptionModal;
--- packages/components/containers/payments/subscription/SubscriptionModalProvider.tsx
+++ packages/components/containers/payments/subscription/SubscriptionModalProvider.tsx
@@ -1,37 +1,17 @@
 import type { ReactNode } from 'react';
-import { createContext, useContext, useRef } from 'react';
+import { createContext, useCallback, useContext, useRef, useState } from 'react';
 
-import { useOrganization } from '@proton/account/organization/hooks';
-import { usePaymentStatus } from '@proton/account/paymentStatus/hooks';
-import { usePlans } from '@proton/account/plans/hooks';
-import { useSubscription } from '@proton/account/subscription/hooks';
-import { useUser } from '@proton/account/user/hooks';
-import ModalTwo from '@proton/components/components/modalTwo/Modal';
-import ModalTwoContent from '@proton/components/components/modalTwo/ModalContent';
-import ModalTwoFooter from '@proton/components/components/modalTwo/ModalFooter';
-import ModalTwoHeader from '@proton/components/components/modalTwo/ModalHeader';
-import useModalState, { type ModalStateProps } from '@proton/components/components/modalTwo/useModalState';
-import { BilledUserModal } from '@proton/components/payments/client-extensions/billed-user';
+import { type ModalStateProps } from '@proton/components/components/modalTwo/useModalState';
 import type { APP_NAMES } from '@proton/shared/lib/constants';
-import { APPS } from '@proton/shared/lib/constants';
-import { invokeInboxDesktopIPC } from '@proton/shared/lib/desktop/ipcHelpers';
-import { getHas2024OfferCoupon, isManagedExternally } from '@proton/shared/lib/helpers/subscription';
-import type { Nullable } from '@proton/shared/lib/interfaces';
-import { isBilledUser } from '@proton/shared/lib/interfaces';
-import { FREE_PLAN } from '@proton/shared/lib/subscription/freePlans';
-import clsx from '@proton/utils/clsx';
 import noop from '@proton/utils/noop';
 
-import { useHasInboxDesktopInAppPayments } from '../../desktop/useHasInboxDesktopInAppPayments';
 import { useRedirectToAccountApp } from '../../desktop/useRedirectToAccountApp';
-import InAppPurchaseModal from './InAppPurchaseModal';
 import type { SubscriptionContainerProps } from './SubscriptionContainer';
-import SubscriptionContainer from './SubscriptionContainer';
-import { SUBSCRIPTION_STEPS, subscriptionModalClassName } from './constants';
+import type { SubscriptionModalFowardedRefProps } from './SubscriptionModal';
+import SubscriptionModal from './SubscriptionModal';
+import { type SUBSCRIPTION_STEPS } from './constants';
 
 export type SubscriptionOverridableStep = SUBSCRIPTION_STEPS.UPGRADE | SUBSCRIPTION_STEPS.THANKS;
-const isOverridablableStep = (step: SUBSCRIPTION_STEPS): step is SubscriptionOverridableStep =>
-    [SUBSCRIPTION_STEPS.UPGRADE, SUBSCRIPTION_STEPS.THANKS].includes(step);
 
 export interface OpenCallbackProps
     extends Pick<
@@ -80,137 +60,17 @@ interface Props {
 }
 
 const SubscriptionModalProvider = ({ children, app, onClose }: Props) => {
-    const [subscription, loadingSubscription] = useSubscription();
-    const [plansResult, loadingPlans] = usePlans();
-    const plans = plansResult?.plans || [];
-    const freePlan = plansResult?.freePlan || FREE_PLAN;
-    const [organization, loadingOrganization] = useOrganization();
     const redirectToAccountApp = useRedirectToAccountApp();
-    const hasInboxDesktopInAppPayments = useHasInboxDesktopInAppPayments();
-    const [user] = useUser();
+    const [modalDepsLoading, setModalDepsLoading] = useState(true);
+    const subscriptionModalRef = useRef<SubscriptionModalFowardedRefProps>(null);
 
-    const [status, statusLoading] = usePaymentStatus();
-
-    const loading = loadingSubscription || loadingPlans || loadingOrganization || statusLoading;
-
-    const subscriptionProps = useRef<OpenCallbackProps | null>(null);
-    const [modalState, setModalState, render] = useModalState();
-
-    let subscriptionModal: Nullable<JSX.Element> = null;
-    if (organization && subscription && render && subscriptionProps.current && status) {
-        if (isManagedExternally(subscription)) {
-            subscriptionModal = <InAppPurchaseModal subscription={subscription} {...modalState} />;
-        } else if (isBilledUser(user)) {
-            subscriptionModal = <BilledUserModal user={user} {...modalState} />;
-        } else {
-            const {
-                hasClose,
-                onClose: subscriptionPropsOnClose,
-                disableCloseOnEscape,
-                fullscreen,
-                onSubscribed,
-                onUnsubscribed,
-                mode,
-                currency,
-                renderCustomStepModal,
-                ...rest
-            } = subscriptionProps.current;
-
-            if (hasInboxDesktopInAppPayments && modalState.open) {
-                void invokeInboxDesktopIPC({ type: 'subscriptionModalOpened', payload: 'subscriptionModalStarted' });
-            }
-
-            const handleClose = () => {
-                if (hasInboxDesktopInAppPayments) {
-                    void invokeInboxDesktopIPC({
-                        type: 'subscriptionModalOpened',
-                        payload: 'subscriptionModalFinished',
-                    });
-                }
-
-                onClose?.();
-                subscriptionPropsOnClose?.();
-                modalState.onClose();
-            };
-
-            let blurBackdrop = true;
-            let rootClassName: string | undefined;
-            if (getHas2024OfferCoupon(rest.coupon)) {
-                blurBackdrop = false;
-                if (app === APPS.PROTONVPN_SETTINGS) {
-                    rootClassName = 'subscription-modal-bf-bg subscription-modal--vpn-bg';
-                } else if (app === APPS.PROTONPASS) {
-                    rootClassName = 'subscription-modal-bf-bg subscription-modal--pass-bg';
-                } else if (app === APPS.PROTONDRIVE) {
-                    rootClassName = 'subscription-modal-bf-bg subscription-modal--drive-bg';
-                } else {
-                    rootClassName = 'subscription-modal-bf-bg subscription-modal--mail-bg';
-                }
-            }
-
-            subscriptionModal = (
-                <SubscriptionContainer
-                    parent="subscription-modal"
-                    app={app}
-                    subscription={subscription}
-                    plans={plans}
-                    freePlan={freePlan}
-                    organization={organization}
-                    onSubscribed={() => {
-                        handleClose();
-                        onSubscribed?.();
-                    }}
-                    onUnsubscribed={() => {
-                        handleClose();
-                        onUnsubscribed?.();
-                    }}
-                    onCancel={handleClose}
-                    mode={mode}
-                    currency={currency}
-                    paymentsStatus={status}
-                    {...rest}
-                    render={({ onSubmit, title, content, footer, step }) => {
-                        const modal = (
-                            <ModalTwo
-                                blurBackdrop={blurBackdrop}
-                                className={clsx([
-                                    subscriptionModalClassName,
-                                    [SUBSCRIPTION_STEPS.PLAN_SELECTION, SUBSCRIPTION_STEPS.CHECKOUT].includes(step) &&
-                                        'subscription-modal--fixed-height',
-                                    [SUBSCRIPTION_STEPS.PLAN_SELECTION].includes(step) &&
-                                        'subscription-modal--large-width',
-                                    [SUBSCRIPTION_STEPS.CHECKOUT].includes(step) && 'subscription-modal--medium-width',
-                                ])}
-                                rootClassName={rootClassName}
-                                data-testid="plansModal"
-                                {...modalState}
-                                onClose={handleClose}
-                                disableCloseOnEscape={disableCloseOnEscape}
-                                fullscreen={fullscreen}
-                                as="form"
-                                size="large"
-                                onSubmit={onSubmit}
-                            >
-                                <ModalTwoHeader title={title} hasClose={hasClose} />
-                                <ModalTwoContent>{content}</ModalTwoContent>
-                                {footer && <ModalTwoFooter>{footer}</ModalTwoFooter>}
-                            </ModalTwo>
-                        );
-
-                        if (isOverridablableStep(step)) {
-                            return renderCustomStepModal?.(step, modalState) || modal;
-                        }
-
-                        return modal;
-                    }}
-                />
-            );
-        }
-    }
+    const handleDepsLoaded = useCallback(() => {
+        setModalDepsLoading(false);
+    }, []);
 
     return (
         <>
-            {subscriptionModal}
+            <SubscriptionModal ref={subscriptionModalRef} onClose={onClose} app={app} onDepsLoaded={handleDepsLoaded} />
             <SubscriptionModalContext.Provider
                 value={[
                     (props) => {
@@ -218,14 +78,13 @@ const SubscriptionModalProvider = ({ children, app, onClose }: Props) => {
                             return;
                         }
 
-                        if (loading || render) {
+                        if (modalDepsLoading || subscriptionModalRef.current?.isOpened) {
                             return;
                         }
 
-                        subscriptionProps.current = props;
-                        setModalState(true);
+                        subscriptionModalRef.current?.open(props);
                     },
-                    loading,
+                    modalDepsLoading,
                 ]}
             >
                 {children}
