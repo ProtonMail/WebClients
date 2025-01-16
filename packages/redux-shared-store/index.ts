f7f808e5132566418b08cd57e918a951984addb2
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-01-16 09:40:35 +0000

    Add settings heartbeat for calendar

--- packages/redux-shared-store/index.ts
+++ packages/redux-shared-store/index.ts
@@ -3,3 +3,4 @@ export * from './sharedProvider';
 export * from './sharedReducers';
 
 export { mailSettingsHeartbeatListener } from './settingsHeartbeat/mailSettingsHeartbeatListener';
+export { calendarSettingsHeartbeatListener } from './settingsHeartbeat/calendarSettingsHeartbeatListener';
