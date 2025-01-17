58ffa8f9bfae730b5eef19a615d3f0a81323b8f2
Germain Souquet <germain.souquet@proton.ch>
2025-01-17 09:43:55 +0000

    Upsell paid users to Unlimited instead of  Mail Plus promo

--- packages/components/components/upsell/useOneDollarPromo.tsx
+++ packages/components/components/upsell/useOneDollarPromo.tsx
@@ -3,18 +3,26 @@ import { useEffect, useState } from 'react';
 import { c } from 'ttag';
 
 import { usePlans } from '@proton/account/plans/hooks';
+import { useUser } from '@proton/account/user/hooks';
 import { CircleLoader } from '@proton/atoms/index';
 import Price from '@proton/components/components/price/Price';
 import { useAutomaticCurrency } from '@proton/components/payments/client-extensions';
 import { usePaymentsApi } from '@proton/components/payments/react-extensions/usePaymentsApi';
 import { useLoading } from '@proton/hooks/index';
-import { PLANS, getPlanByName } from '@proton/payments';
-import { COUPON_CODES, CYCLE, MAIL_SHORT_APP_NAME } from '@proton/shared/lib/constants';
+import { PLANS, PLAN_NAMES, getPlanByName } from '@proton/payments';
+import { BRAND_NAME, COUPON_CODES, CYCLE } from '@proton/shared/lib/constants';
 import { useNewUpsellModalVariant } from '@proton/shared/lib/helpers/upsell';
 
 const OFFER_DEFAULT_AMOUNT_DUE = 100;
 
+/**
+ * Offers a $1 promo to free users
+ * And a normal Unlimited upsell for the paid users.
+ * We can't offer $1 Mail Plus to paid users as it might override their existing
+ * subscription (e.g. VPN Plus).
+ */
 const useOneDollarConfig = () => {
+    const [user] = useUser();
     const [currency, loadingCurrency] = useAutomaticCurrency();
     const displayNewUpsellModalsVariant = useNewUpsellModalVariant();
     const { paymentsApi } = usePaymentsApi();
@@ -22,39 +30,44 @@ const useOneDollarConfig = () => {
     const [amountDue, setAmountDue] = useState<number>();
 
     const [plansResult] = usePlans();
-    const mailPlus = getPlanByName(plansResult?.plans ?? [], PLANS.MAIL, currency);
 
-    const mailPlusPrice = mailPlus?.Pricing?.[CYCLE.MONTHLY] || 0;
+    let planName = PLAN_NAMES[PLANS.MAIL];
+    let planID: PLANS = PLANS.MAIL;
+    if (user.isPaid) {
+        planID = PLANS.BUNDLE;
+        planName = PLAN_NAMES[PLANS.BUNDLE];
+    }
+    const plan = getPlanByName(plansResult?.plans ?? [], planID, currency);
+    const cycle = user.isFree ? CYCLE.MONTHLY : CYCLE.YEARLY;
+    const planPricePerMonth = (plan?.Pricing?.[cycle] || 0) / cycle;
 
     useEffect(() => {
         const handleGetPlanAmount = async () => {
-            if (mailPlus && currency && !loadingCurrency && !amountDue) {
+            if (plan && currency && !loadingCurrency && !amountDue) {
                 // For USD, CHF and EUR, default value is "1 dollar"
                 const isDefaultAmountDue = ['USD', 'CHF', 'EUR'].includes(currency);
                 if (isDefaultAmountDue) {
-                    setAmountDue(OFFER_DEFAULT_AMOUNT_DUE);
+                    setAmountDue(user.isFree ? OFFER_DEFAULT_AMOUNT_DUE : planPricePerMonth);
                     return;
                 }
 
-                await paymentsApi
-                    .checkWithAutomaticVersion({
-                        Plans: { [PLANS.MAIL]: 1 },
+                try {
+                    const { AmountDue } = await paymentsApi.checkWithAutomaticVersion({
+                        Plans: { [planID]: 1 },
                         Currency: currency,
                         Cycle: CYCLE.MONTHLY,
-                        CouponCode: COUPON_CODES.TRYMAILPLUS0724,
-                    })
-                    .then(({ AmountDue }) => {
-                        setAmountDue(AmountDue);
-                    })
-                    .catch(() => {
-                        // If request fails, default on plan full price
-                        setAmountDue(mailPlusPrice);
+                        CouponCode: user.isFree ? COUPON_CODES.TRYMAILPLUS0724 : undefined,
                     });
+                    setAmountDue(AmountDue);
+                } catch (e) {
+                    // If request fails, default on plan full price
+                    setAmountDue(planPricePerMonth);
+                }
             }
         };
 
         void withLoading(handleGetPlanAmount);
-    }, [mailPlus, currency, loadingCurrency]);
+    }, [plan, currency, loadingCurrency]);
 
     const priceMailPlus = (
         <Price
@@ -63,7 +76,7 @@ const useOneDollarConfig = () => {
             suffix={c('specialoffer: Offers').t`/month`}
             isDisplayedInSentence
         >
-            {mailPlusPrice}
+            {planPricePerMonth}
         </Price>
     );
 
@@ -78,19 +91,32 @@ const useOneDollarConfig = () => {
 
     if (displayNewUpsellModalsVariant) {
         return {
-            submitText: c('Action').jt`Get ${MAIL_SHORT_APP_NAME} Plus for ${priceCoupon}`,
-            footerText: c('new_plans: Title')
-                .jt`The discounted price of ${priceCoupon} is valid for the first month. Then it will automatically be renewed at ${priceMailPlus}. You can cancel at any time.`,
-            cycle: CYCLE.MONTHLY,
-            coupon: COUPON_CODES.TRYMAILPLUS0724,
+            submitText: user.isFree
+                ? c('Action').jt`Get ${planName} for ${priceCoupon}`
+                : c('Action').jt`Get ${planName}`,
+            footerText: user.isFree
+                ? c('new_plans: Subtext')
+                      .jt`The discounted price of ${priceCoupon} is valid for the first month. Then it will automatically be renewed at ${priceMailPlus}. You can cancel at any time.`
+                : c('new_plans: Subtext')
+                      .jt`Unlock all ${BRAND_NAME} premium products and features for just ${priceMailPlus}. Cancel anytime.`,
+            cycle,
+            coupon: user.isFree ? COUPON_CODES.TRYMAILPLUS0724 : undefined,
+            planIDs: {
+                [planID]: 1,
+            },
         };
     }
 
     return {
-        submitText: c('new_plans: Action').jt`Get started for ${priceCoupon}`,
-        title: c('new_plans: Title').jt`Get Mail Plus for ${priceCoupon}`,
-        cycle: CYCLE.MONTHLY,
-        coupon: COUPON_CODES.TRYMAILPLUS0724,
+        submitText: user.isFree
+            ? c('new_plans: Action').jt`Get started for ${priceCoupon}`
+            : c('new_plans: Action').jt`Upgrade`,
+        title: user.isFree ? c('Action').jt`Get ${planName} for ${priceCoupon}` : c('Action').jt`Get ${planName}`,
+        cycle,
+        coupon: user.isFree ? COUPON_CODES.TRYMAILPLUS0724 : undefined,
+        planIDs: {
+            [planID]: 1,
+        },
     };
 };
 
