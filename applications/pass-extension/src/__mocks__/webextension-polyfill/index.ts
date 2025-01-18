4f45b791065de5aae449997b5b5fc070330fede4
Edvin CANDON <edvin.candon@proton.ch>
2025-01-18 15:01:37 +0100

    Refactor content-script orchestration
    - Goal: properly support BFCache resumed pages
    - Main orchestrator script: handles clean-up to support content-script
      re-injection. In all browsers: should try to remove injected DOM nodes
      and unload any previously injected content-script via broadcast.
    - Client orchestrtor: takes care of handling page visibility changes
      to correctly start/stop the underlying content script client instance
    - This removes the need for extension messaging between the two orchestrators
      on page visibility changes making browser BFCaching more stable
    - Add functional tests for client content-script runner

--- applications/pass-extension/src/__mocks__/webextension-polyfill/index.ts
+++ applications/pass-extension/src/__mocks__/webextension-polyfill/index.ts
@@ -3,11 +3,16 @@ import { sender } from 'proton-pass-extension/__mocks__/mocks';
 
 const browser = {
     runtime: {
+        getURL: jest.fn((asset: string) => `test://${asset}`),
         sendMessage: jest.fn((_, message) => {
             const handler = mockHandlers.get(message.type);
             if (handler) return handler(message, sender);
             return false;
         }),
+        onMessage: {
+            addListener: jest.fn(),
+            removeListener: jest.fn(),
+        },
     },
     permissions: {
         request: jest.fn(async () => true),
@@ -19,6 +24,19 @@ const browser = {
             passwordSavingEnabled: { get: jest.fn(), set: jest.fn() },
         },
     },
+};
+
+export const clearBrowserMocks = () => {
+    browser.runtime.getURL.mockClear();
+    browser.runtime.sendMessage.mockClear();
+    browser.runtime.onMessage.addListener.mockClear();
+    browser.runtime.onMessage.removeListener.mockClear();
+    browser.permissions.request.mockClear();
+    browser.permissions.contains.mockClear();
+    browser.privacy.services.autofillAddressEnabled.get.mockClear();
+    browser.privacy.services.autofillAddressEnabled.set.mockClear();
+    browser.privacy.services.passwordSavingEnabled.get.mockClear();
+    browser.privacy.services.passwordSavingEnabled.set.mockClear();
 };
 
 export default browser;
