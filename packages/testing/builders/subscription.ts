45668d779c1c6ea67f61875b1dd4c297280e4191
Alexey Karpov <alexey.karpov@proton.ch>
2025-01-20 09:48:30 +0000

    Added unit tests for ProtonPlanCustomizer

--- packages/testing/builders/subscription.ts
+++ packages/testing/builders/subscription.ts
@@ -1,46 +1,39 @@
-import { PLANS, PLAN_TYPES } from '@proton/payments';
+import { PLANS, type PlanIDs } from '@proton/payments';
 import { CYCLE } from '@proton/shared/lib/constants';
-import { External, Renew, type Subscription } from '@proton/shared/lib/interfaces';
+import { External, type Plan, Renew, type Subscription } from '@proton/shared/lib/interfaces';
 
-export const buildSubscription = (value?: Partial<Subscription>): Subscription => {
+import { getTestPlans } from '../data';
+
+export const buildSubscription = (
+    value?: Partial<Subscription>,
+    planIDs: PlanIDs = {
+        [PLANS.BUNDLE]: 1,
+    }
+): Subscription => {
+    const Cycle = value?.Cycle ?? CYCLE.YEARLY;
+    const Currency = value?.Currency ?? 'EUR';
+
+    const Plans = Object.entries(planIDs).map(([planName, quantity]) => ({
+        ...(getTestPlans(Currency).find((plan) => plan.Name === planName) as Plan),
+        Quantity: quantity,
+    }));
+
     return {
+        Cycle,
+        Currency,
+        Plans,
         ID: 'subscriptionId123',
         InvoiceID: 'invoiceId123',
-        Cycle: CYCLE.YEARLY,
         PeriodStart: 1685966060,
         PeriodEnd: 1717588460,
         CreateTime: 1685966060,
         CouponCode: null,
-        Currency: 'EUR',
         Amount: 11988,
         Discount: 0,
         RenewAmount: 11988,
         RenewDiscount: 0,
         Renew: Renew.Enabled,
         External: External.Default,
-        Plans: [
-            {
-                ID: 'planId123',
-                Name: PLANS.BUNDLE,
-                Type: PLAN_TYPES.PLAN,
-                Title: 'Proton Unlimited',
-                MaxDomains: 3,
-                MaxAddresses: 15,
-                MaxCalendars: 25,
-                MaxSpace: 536870912000,
-                MaxMembers: 1,
-                MaxVPN: 10,
-                MaxTier: 2,
-                Services: 15,
-                Features: 1,
-                State: 1,
-                Cycle: 12,
-                Currency: 'EUR',
-                Amount: 11988,
-                Quantity: 1,
-                Offer: 'default',
-            },
-        ],
         ...value,
     };
 };
