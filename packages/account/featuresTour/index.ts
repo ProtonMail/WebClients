e7aef9842c38853dd984ec7a1b93fe7518965dbf
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2025-01-22 09:35:07 +0100

    Feat: Add post subscription flow and feature tour telemetry
    - Delete `mailPostSubscriptionEvents` group and create a new one called `postSubscriptionTourEvents`.
    - Cover telemetry needs for this feature

--- packages/account/featuresTour/index.ts
+++ packages/account/featuresTour/index.ts
@@ -4,9 +4,21 @@ import type { FeatureTourStepId } from '@proton/components/components/featureTou
 
 const name = 'featureTour' as const;
 
+export type FeatureTourTelemetryFeature = 'short-domain' | 'auto-delete' | 'dark-web-monitoring';
+
 interface State {
     display: boolean;
     steps: FeatureTourStepId[];
+    /**
+     * Features activated during feature tour.
+     */
+    activatedFeatures: FeatureTourTelemetryFeature[];
+    /**
+     * Origin of the feature tour display.
+     * For ex it helps know if user triggered the feature tour from post subscription modal or drawer.
+     * @default undefined
+     */
+    origin: 'postSubscription' | 'drawer' | undefined;
 }
 
 export interface FeaturesTourState {
@@ -15,6 +27,8 @@ export interface FeaturesTourState {
 
 const initialState: State = {
     display: false,
+    origin: undefined,
+    activatedFeatures: [],
     steps: [],
 };
 
@@ -22,13 +36,16 @@ export const slice = createSlice({
     name,
     initialState,
     reducers: {
-        display: (state, action: PayloadAction<{ steps: FeatureTourStepId[] }>) => {
+        display: (state, action: PayloadAction<{ steps: State['steps']; origin: State['origin'] }>) => {
             state.display = true;
+            state.origin = action.payload.origin;
             state.steps = action.payload.steps || [];
         },
         hide: (state) => {
-            state.display = false;
-            state.steps = [];
+            Object.assign(state, initialState);
+        },
+        activateFeature: (state, action: PayloadAction<{ feature: State['activatedFeatures'][number] }>) => {
+            state.activatedFeatures = [...state.activatedFeatures, action.payload.feature];
         },
     },
 });
