2e04cd06ba08e73a99c6a9bcb700e05fe0b6a0ad
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-01-23 06:38:15 +0000

    Merge branch 'update-post-signup-desing' into 'main'
    Update the post signup desing after design review
    
    See merge request web/clients!15227

--- packages/components/components/topnavbar/TopNavbarPostSignupPromo/PostSignupOneDollar/components/OfferContent.tsx
+++ packages/components/components/topnavbar/TopNavbarPostSignupPromo/PostSignupOneDollar/components/OfferContent.tsx
@@ -10,24 +10,16 @@ import type { FeatureProps, SUPPORTED_PRODUCTS } from './interface';
 interface Props {
     product: SUPPORTED_PRODUCTS;
     features: FeatureProps[];
-    imgSrc: string;
     onClose: () => void;
     onUpsellClick: () => void;
-    extended?: boolean;
 }
 
-export const OfferContent = ({ product, features, imgSrc, onUpsellClick, onClose, extended }: Props) => {
+export const OfferContent = ({ product, features, onUpsellClick, onClose }: Props) => {
     return (
         <section className="p-6 pt-12">
-            {extended && (
-                <div className="text-center">
-                    <img alt="" src={imgSrc} />
-                </div>
-            )}
             <PostSignupOneDollarTitle product={product} />
-            {extended && <PostSignupOneDollarFooter onClick={onUpsellClick} extended />}
             <PostSignupOneDollarTable features={features} />
-            {!extended && <PostSignupOneDollarFooter onClick={onUpsellClick} />}
+            <PostSignupOneDollarFooter onClick={onUpsellClick} />
             <div className="text-center">
                 <Button onClick={onClose} shape="underline" color="norm" className="p-0">{c('Offer')
                     .t`Maybe later`}</Button>
