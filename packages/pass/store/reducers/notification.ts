6893f8fad36e6bb02ad75224b96fd4c4b8e9bc29
Edvin CANDON <edvin.candon@proton.ch>
2025-01-24 11:08:17 +0100

    Optimistically mutate in-app notification state without waiting for API response

--- packages/pass/store/reducers/notification.ts
+++ packages/pass/store/reducers/notification.ts
@@ -6,9 +6,7 @@ import { partialMerge } from '@proton/pass/utils/object/merge';
 import { UNIX_MINUTE } from '@proton/pass/utils/time/constants';
 import { getEpoch } from '@proton/pass/utils/time/epoch';
 
-export type NotificationReducerState = InAppNotifications & {
-    nextDisplayTime: number;
-};
+export type NotificationReducerState = InAppNotifications & { nextDisplayTime: number };
 
 const getInitialState = (): NotificationReducerState => ({
     notifications: [],
@@ -20,21 +18,21 @@ const getInitialState = (): NotificationReducerState => ({
 const reducer: Reducer<NotificationReducerState> = (state = getInitialState(), action) => {
     if (getInAppNotifications.success.match(action)) {
         const notifications = action.payload;
-
         if (!notifications) return state;
 
         return partialMerge(state, notifications);
     }
 
-    if (updateInAppNotificationState.success.match(action)) {
+    if (updateInAppNotificationState.intent.match(action)) {
         const { id, state: notificationState } = action.payload;
 
         const notifications = state.notifications.map((notification) =>
             notification.id === id ? { ...notification, state: notificationState } : notification
         );
 
-        // "nextDisplayTime" manages the display timing for notifications on the client side.
-        // If there are two or more notifications, we should enforce a 30-minute interval between displays.
+        /** "nextDisplayTime" manages the display timing for notifications
+         * on the client side. If there are two or more notifications, we
+         * should enforce a 30-minute interval between displays. */
         return partialMerge(state, { notifications, nextDisplayTime: getEpoch() + UNIX_MINUTE * 30 });
     }
 
