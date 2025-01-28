105ff093ccd0977166486f83a6b886aab696e13e
Kévin Ta <kevin.ta@proton.ch>
2025-01-28 10:40:56 +0100

    Allow to sweep a paper wallet

---
+++ applications/wallet/src/app/contexts/WalletDrawerContext/WalletReceiveExtraContent/index.tsx
@@ -0,0 +1,71 @@
+import { c } from 'ttag';
+
+import type { WasmApiWalletAccount } from '@proton/andromeda';
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleHeader,
+    CollapsibleHeaderIconButton,
+    Icon,
+    useModalState,
+} from '@proton/components/index';
+import { useFlag } from '@proton/unleash/index';
+
+import { Button } from '../../../atoms';
+import { ImportPaperWalletModal } from '../../../components/ImportPaperWalletModal';
+
+interface Props {
+    account: WasmApiWalletAccount;
+    onClose?: () => void;
+}
+
+export const WalletReceiveExtraContent = ({ account, onClose }: Props) => {
+    const [importPaperWalletModal, setImportPaperWalletModal, renderImportPaperWalletModal] = useModalState();
+
+    const isImportPaperWalletAllowed = useFlag('ImportPaperWallet');
+
+    if (!isImportPaperWalletAllowed) {
+        return <></>;
+    }
+
+    return (
+        <>
+            <div className="flex flex-column mt-5 w-full">
+                <Collapsible>
+                    <CollapsibleHeader
+                        className="color-weak"
+                        suffix={
+                            <CollapsibleHeaderIconButton className="color-weak">
+                                <Icon name="chevron-down" />
+                            </CollapsibleHeaderIconButton>
+                        }
+                    >
+                        {c('Wallet Receive').t`More options`}
+                    </CollapsibleHeader>
+                    <CollapsibleContent>
+                        <div className="flex flex-column items-center gap-2 mt-5">
+                            <Button
+                                data-testid={'import-paper-wallet'}
+                                fullWidth
+                                shape="ghost"
+                                size="large"
+                                onClick={() => {
+                                    setImportPaperWalletModal(true);
+                                }}
+                                style={{ pointerEvents: 'auto' }}
+                            >
+                                <span className="flex gap-2 items-center text-center justify-center">
+                                    {c('Wallet Receive').t`Import paper wallet`}
+                                </span>
+                            </Button>
+                        </div>
+                    </CollapsibleContent>
+                </Collapsible>
+            </div>
+
+            {renderImportPaperWalletModal && (
+                <ImportPaperWalletModal account={account} onCloseDrawer={onClose} {...importPaperWalletModal} />
+            )}
+        </>
+    );
+};
