9df68d1cffe2c023d9960bf91e8ebcace8f81bf7
Redacted <redacted@invalid.proton.ch>
2025-01-29 11:21:14 +0000

    Remove yetch unnecessary polyfill since it's useless

--- packages/polyfill/index.js
+++ packages/polyfill/index.js
@@ -1,7 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { install } from 'resize-observer';
-import 'yetch/polyfill';
 
 if (!window.ResizeObserver) {
     install();
