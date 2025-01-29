f4da8d01f2b3d52dd6bd381e5410e4dc1b240f4d
Edvin CANDON <edvin.candon@proton.ch>
2025-01-29 08:38:44 +0100

    Stabilize `WithSpotlight` utility
    - Extract common logic into `useSpotlightFor` hook
      if HOC isn't sufficient

--- packages/pass/components/Onboarding/OnboardingSSO.tsx
+++ packages/pass/components/Onboarding/OnboardingSSO.tsx
@@ -7,21 +7,21 @@ import { ModalTwoContent, ModalTwoFooter, ModalTwoHeader } from '@proton/compone
 import { useConnectivity } from '@proton/pass/components/Core/ConnectivityProvider';
 import { PassModal } from '@proton/pass/components/Layout/Modal/PassModal';
 import { OnboardingLockSetup } from '@proton/pass/components/Onboarding/OnboardingLockSetup';
-import type { SpotlightModalProps } from '@proton/pass/components/Spotlight/WithSpotlight';
+import type { WithSpotlightRenderProps } from '@proton/pass/components/Spotlight/WithSpotlight';
 import { useLockSetup } from '@proton/pass/hooks/useLockSetup';
 import { LockMode } from '@proton/pass/lib/auth/lock/types';
 import { oneOf } from '@proton/pass/utils/fp/predicates';
 
 import './OnboardingModal.scss';
 
-export const OnboardingSSO: FC<SpotlightModalProps> = ({ closeAndAcknowledge }) => {
+export const OnboardingSSO: FC<WithSpotlightRenderProps> = ({ close }) => {
     const { lock } = useLockSetup();
     const online = useConnectivity();
     const lockModeIsNotPreferred = oneOf(LockMode.PASSWORD, LockMode.NONE)(lock.mode);
 
     useEffect(() => {
-        // Only display the modal if the Lock Mode is Password or None
-        if (!lockModeIsNotPreferred) closeAndAcknowledge();
+        /* Only display the modal if the Lock Mode is Password or None */
+        if (!lockModeIsNotPreferred) close();
     }, []);
 
     return (
@@ -47,7 +47,7 @@ export const OnboardingSSO: FC<SpotlightModalProps> = ({ closeAndAcknowledge }) 
                         <Button
                             pill
                             shape="solid"
-                            onClick={closeAndAcknowledge}
+                            onClick={close}
                             disabled={!online || lock.mode === LockMode.PASSWORD}
                         >
                             {c('Action').t`Accept`}
--- packages/pass/components/Spotlight/WithSpotlight.tsx
+++ packages/pass/components/Spotlight/WithSpotlight.tsx
@@ -1,31 +1,38 @@
 import type { FC, ReactNode } from 'react';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 
-import { type ModalStateProps, useModalState } from '@proton/components/index';
+import { useModalState } from '@proton/components/index';
 import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
 import type { SpotlightMessage } from '@proton/pass/types';
 import { pipe } from '@proton/pass/utils/fp/pipe';
 import noop from '@proton/utils/noop';
 
-export type SpotlightModalProps = {
-    onClose: ModalStateProps['onClose'];
+export type WithSpotlightRenderProps = {
     acknowledge: () => void;
-    closeAndAcknowledge: () => void;
+    close: () => void;
+    open: boolean;
 };
 
-type Props = { type: SpotlightMessage; children: (props: SpotlightModalProps) => ReactNode };
+type Props = {
+    type: SpotlightMessage;
+    children: (props: WithSpotlightRenderProps) => ReactNode;
+};
 
-export const WithSpotlight: FC<Props> = ({ type, children }) => {
+export const useSpotlightFor = (type: SpotlightMessage) => {
     const { spotlight } = usePassCore();
     const [{ open, onClose }, setOpen] = useModalState();
-
-    const acknowledge = () => spotlight.acknowledge(type);
-
-    const closeAndAcknowledge = pipe(onClose, acknowledge);
 
     useEffect(() => {
         (async () => (await spotlight.check(type)) ?? false)().then(setOpen).catch(noop);
     }, [type]);
 
-    return open && children({ acknowledge, onClose, closeAndAcknowledge });
+    return useMemo<WithSpotlightRenderProps>(() => {
+        const acknowledge = () => spotlight.acknowledge(type);
+        return { open, acknowledge, close: pipe(onClose, acknowledge) };
+    }, [onClose, type, open]);
+};
+
+export const WithSpotlight: FC<Props> = ({ type, children }) => {
+    const spotlightFor = useSpotlightFor(type);
+    return spotlightFor.open && children(spotlightFor);
 };
