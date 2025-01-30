df01437c3b811828da3ccefea0d6c48cb241ada9
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2025-01-30 21:54:42 +0100

    refactor: change post subscription steps to match new modal props

--- packages/components/containers/payments/subscription/postSubscription/modals/PostSubscriptionModalsComponents.tsx
+++ packages/components/containers/payments/subscription/postSubscription/modals/PostSubscriptionModalsComponents.tsx
@@ -3,12 +3,10 @@ import { type ReactNode } from 'react';
 import { Button } from '@proton/atoms';
 import Modal from '@proton/components/components/modalTwo/Modal';
 import ModalContent from '@proton/components/components/modalTwo/ModalContent';
-import ModalHeader from '@proton/components/components/modalTwo/ModalHeader';
+import { ModalHeaderCloseButton } from '@proton/components/components/modalTwo/ModalHeader';
 import type { ModalStateProps } from '@proton/components/components/modalTwo/useModalState';
 import checkoutLoader from '@proton/styles/assets/img/post-subscription/checkout-loader.svg';
 import clsx from '@proton/utils/clsx';
-
-import { POST_SUBSCRIPTION_MODAL_CLOSE_BUTTON_ID } from '../constants';
 
 export const PostSubscriptionModalWrapper = ({
     children,
@@ -22,28 +20,39 @@ export const PostSubscriptionModalWrapper = ({
                 modalProps.onClose();
             }
         }}
-        className="modal-two--twocolors"
+        size="xsmall"
     >
-        {children}
+        <div>{children}</div>
     </Modal>
 );
 
 export const PostSubscriptionModalHeader = ({ illustration }: { illustration: string }) => (
-    <>
-        <ModalHeader closeButtonProps={{ id: POST_SUBSCRIPTION_MODAL_CLOSE_BUTTON_ID }} />
-        <div className="modal-two-illustration-container relative text-center fade-in-up">
-            <img src={illustration} alt="" width="128" height="128" />
-        </div>
-    </>
+    <div
+        className="relative flex justify-center items-center fade-in-up h-custom custom-bg"
+        style={{ '--h-custom': '12rem', '--custom-bg': 'var(--optional-background-lowered)' }}
+    >
+        <ModalHeaderCloseButton
+            buttonProps={{
+                className: 'absolute right-0 top-0 mt-3 mr-3',
+            }}
+        />
+        <img src={illustration} alt="" width="128" height="128" />
+    </div>
 );
 
-export const PostSubscriptionModalContentWrapper = ({ children }: { children: ReactNode }) => (
-    <div className="modal-two-content-container fade-in-up">
-        <ModalContent>
-            <div>
-                <div className="mt-8 mb-6">{children}</div>
-            </div>
-        </ModalContent>
+export const PostSubscriptionModalContentWrapper = ({
+    children,
+    footer,
+}: {
+    children: ReactNode;
+    footer: ReactNode;
+}) => (
+    <div className="fade-in-up bg-norm p-8 flex flex-col justify-space-between">
+        <div className="min-h-custom flex justify-center items-center" style={{ '--min-h-custom': '10.5rem' }}>
+            <div>{children}</div>
+        </div>
+
+        <div className="mt-6 w-full">{footer}</div>
     </div>
 );
 
@@ -70,24 +79,21 @@ export const PostSubscriptionModalContent = ({
 }: ConfirmationModalContentProps) => (
     <>
         <PostSubscriptionModalHeader illustration={illustration} />
-        <PostSubscriptionModalContentWrapper>
-            <div className="flex items-center min-h-custom" style={{ '--min-h-custom': '10rem' }}>
-                <div>
-                    <h1 className={clsx('text-bold text-center', subtitle ? 'text-lg mb-1' : 'text-3xl mb-2')}>
-                        {title}
-                    </h1>
-                    {!!subtitle && <h2 className="text-lg text-center mt-0 mb-2">{subtitle}</h2>}
-                    <p className="text-center m-0 color-weak">{description}</p>
-                </div>
-            </div>
-            <div className="mt-6">
-                <Button className="mb-2" color="norm" fullWidth onClick={primaryButtonCallback}>
-                    {primaryButtonText}
-                </Button>
-                <Button fullWidth onClick={secondaryButtonCallback}>
-                    {secondaryButtonText}
-                </Button>
-            </div>
+        <PostSubscriptionModalContentWrapper
+            footer={
+                <>
+                    <Button className="mb-2" color="norm" fullWidth onClick={primaryButtonCallback}>
+                        {primaryButtonText}
+                    </Button>
+                    <Button fullWidth onClick={secondaryButtonCallback}>
+                        {secondaryButtonText}
+                    </Button>
+                </>
+            }
+        >
+            <h1 className={clsx('text-bold text-center', subtitle ? 'text-lg mb-1' : 'text-3xl mb-2')}>{title}</h1>
+            {!!subtitle && <h2 className="text-lg text-center mt-0 mb-2">{subtitle}</h2>}
+            <p className="text-center m-0 color-weak">{description}</p>
         </PostSubscriptionModalContentWrapper>
     </>
 );
