60875f600a7f78bcf4b9d2cc29fe50b865f99100
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2025-01-31 15:47:23 +0100

    refactor: update FeatureTourStepsContent to get rid of custom scss file

--- packages/components/components/featureTour/steps/components/FeatureTourStepsContent.tsx
+++ packages/components/components/featureTour/steps/components/FeatureTourStepsContent.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from 'react';
 
-import ModalContent from '@proton/components/components/modalTwo/ModalContent';
-import ModalHeader from '@proton/components/components/modalTwo/ModalHeader';
+import { ModalHeaderCloseButton } from '@proton/components/components/modalTwo/ModalHeader';
 import clsx from '@proton/utils/clsx';
 
 import type { FeatureTourStepProps } from '../../interface';
@@ -28,7 +27,6 @@ const FeatureTourStepsContent = ({
     const illustrationWidth = illustrationSize === 'medium' ? '180' : undefined;
     const illustrationHeight = illustrationSize === 'full' ? undefined : '128';
     const illustrationClassName = illustrationSize === 'full' ? 'w-full' : 'mt-8';
-    const closeButtonClassName = illustrationSize === 'full' ? 'modal-two-close-button--illustration-cover' : undefined;
 
     const hasTwoCTAs = !!mainCTA && !!extraCTA;
 
@@ -42,9 +40,23 @@ const FeatureTourStepsContent = ({
 
     return (
         <>
-            <ModalHeader closeButtonProps={{ className: closeButtonClassName }} />
-            {illustration && (
-                <div className={'relative text-center modal-two-illustration-container'}>
+            <div
+                className={illustration ? 'relative flex justify-center items-center h-custom custom-bg' : undefined}
+                style={
+                    illustration
+                        ? { '--h-custom': '12rem', '--custom-bg': 'var(--optional-background-lowered)' }
+                        : undefined
+                }
+            >
+                <ModalHeaderCloseButton
+                    buttonProps={{
+                        className: 'absolute right-0 top-0 mt-3 mr-3',
+                    }}
+                    iconProps={{
+                        style: illustrationSize === 'full' ? { fill: 'white' } : undefined,
+                    }}
+                />
+                {illustration && (
                     <img
                         src={illustration}
                         alt=""
@@ -53,29 +65,25 @@ const FeatureTourStepsContent = ({
                         height={illustrationHeight}
                         className={illustrationClassName}
                     />
+                )}
+            </div>
+            <div className="p-8">
+                <div
+                    className={clsx(
+                        'min-h-custom w-full flex flex-column flex-nowrap grow-0 justify-center items-center'
+                    )}
+                    style={{ '--min-h-custom': minContentHeight }}
+                >
+                    <h1 className="text-lg text-bold text-center mb-2">{title}</h1>
+                    <div className="text-center color-weak w-full">{children}</div>
                 </div>
-            )}
-            <div className="modal-two-content-container">
-                <ModalContent className="my-8">
-                    <div>
-                        <div
-                            className={clsx(
-                                'min-h-custom max-w-custom flex flex-column flex-nowrap grow-0 justify-center items-center'
-                            )}
-                            style={{ '--min-h-custom': minContentHeight, '--max-w-custom': '18.5rem' }}
-                        >
-                            <h1 className="text-lg text-bold text-center mb-2">{title}</h1>
-                            <div className="text-center color-weak w-full">{children}</div>
-                        </div>
 
-                        <div className="flex flex-column gap-2 mt-6 mb-2">
-                            {mainCTA}
-                            {extraCTA}
-                        </div>
+                <div className="flex flex-column gap-2 mt-6 mb-2">
+                    {mainCTA}
+                    {extraCTA}
+                </div>
 
-                        {bullets}
-                    </div>
-                </ModalContent>
+                {bullets}
             </div>
         </>
     );
