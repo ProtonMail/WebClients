9177a09c6328d590fd0040f412a510d6574db257
Edvin CANDON <edvin.candon@proton.ch>
2025-02-02 08:23:42 +0100

    Fine-tune ValentinesDayPromo
    - Move to `packages/pass/components/Promo`
    - Use `WithFeatureFlag` HOC to avoid unecessary renders
    - Add memoisation & fine-tune extension renders
    
    [IDTEAM-4409]

--- packages/pass/components/Promo/ValentinesDayPromo.tsx
+++ packages/pass/components/Promo/ValentinesDayPromo.tsx
@@ -1,15 +1,14 @@
 import type { ReactElement } from 'react';
-import { type FC, useState } from 'react';
+import { type FC, memo, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms';
 import { Icon, Tooltip } from '@proton/components';
+import { WithFeatureFlag } from '@proton/pass/components/Core/WithFeatureFlag';
 import { SubTheme } from '@proton/pass/components/Layout/Theme/types';
-import { ValentinesDayPromoModal } from '@proton/pass/components/Onboarding/ValentinesDayPromoModal';
 import { PASS_VALENTINES_DAY_END_DATE, UpsellRef } from '@proton/pass/constants';
-import { useFeatureFlag } from '@proton/pass/hooks/useFeatureFlag';
 import { useNavigateToUpgrade } from '@proton/pass/hooks/useNavigateToUpgrade';
 import { selectInAppNotificationsEnabled, selectUser, selectUserPlan } from '@proton/pass/store/selectors';
 import { PassFeature } from '@proton/pass/types/api/features';
@@ -17,6 +16,14 @@ import { PLANS, PLAN_NAMES } from '@proton/payments';
 import { COUPON_CODES } from '@proton/shared/lib/constants';
 import { isDelinquent } from '@proton/shared/lib/user/helpers';
 import clsx from '@proton/utils/clsx';
+
+import { ValentinesDayPromoModal } from './ValentinesDayPromoModal';
+
+const getUpsellRef = (isFreePlan: boolean) => {
+    const freeRef = EXTENSION_BUILD ? UpsellRef.VD_25_PASS_FREE_EXTENSION : UpsellRef.VD_25_PASS_FREE_WEB;
+    const plusRef = EXTENSION_BUILD ? UpsellRef.VD_25_PASS_PLUS_EXTENSION : UpsellRef.VD_25_PASS_PLUS_WEB;
+    return isFreePlan ? freeRef : plusRef;
+};
 
 type MaybeToolTipProps = { children: ReactElement; planName: string };
 
@@ -33,31 +40,24 @@ const MaybeToolTip: FC<MaybeToolTipProps> = ({ children, planName }) =>
         children
     );
 
-export const ValentinesDayPromo: FC = () => {
+const ValentinesDayPromo: FC = memo(() => {
     const user = useSelector(selectUser);
     const plan = useSelector(selectUserPlan);
     const inAppNotificationEnabled = useSelector(selectInAppNotificationsEnabled);
 
     const [showModal, setShowModal] = useState(false);
 
+    const isFreePlan = plan?.InternalName === 'free';
+    const planToUpsell = isFreePlan ? PLANS.PASS : PLANS.BUNDLE;
     /** Upsell free users to Pass Plus and upsell Pass Plus users to Proton Unlimited */
     const isPlanEligible = plan?.InternalName === 'free' || plan?.InternalName === 'pass2023';
     const isUserEligible = isPlanEligible && inAppNotificationEnabled && user && !isDelinquent(user);
 
-    const canShowPromo =
-        useFeatureFlag(PassFeature.PassValentinePromo2025) &&
-        new Date().getTime() < PASS_VALENTINES_DAY_END_DATE && // in case updated feature flag could not be fetched
-        BUILD_TARGET !== 'safari' &&
-        isUserEligible;
-
-    const isFreePlan = plan?.InternalName === 'free';
-    const planToUpsell = isFreePlan ? PLANS.PASS : PLANS.BUNDLE;
-
-    const getUpsellRef = () => {
-        const freeRef = EXTENSION_BUILD ? UpsellRef.VD_25_PASS_FREE_EXTENSION : UpsellRef.VD_25_PASS_FREE_WEB;
-        const plusRef = EXTENSION_BUILD ? UpsellRef.VD_25_PASS_PLUS_EXTENSION : UpsellRef.VD_25_PASS_PLUS_WEB;
-        return isFreePlan ? freeRef : plusRef;
-    };
+    const canShowPromo = useMemo(
+        /** Safe-guard promo end date in case feature flagscould not be revalidated */
+        () => isUserEligible && new Date().getTime() < PASS_VALENTINES_DAY_END_DATE,
+        [isUserEligible]
+    );
 
     const upgrade = useNavigateToUpgrade({
         coupon: COUPON_CODES.LOVEPRIVACY25,
@@ -65,18 +65,17 @@ export const ValentinesDayPromo: FC = () => {
         email: user?.Email,
         plan: planToUpsell,
         offer: planToUpsell === PLANS.PASS ? 'valentine-2025-pass-plus' : 'valentine-2025-pass-bundle',
-        upsellRef: getUpsellRef(),
+        upsellRef: getUpsellRef(isFreePlan),
         type: 'offer',
         disableEdit: true,
     });
 
-    /** On web app we open the promo modal,
-     * on extension there is not enough space to show the modal so we directly open the upgrade page */
+    /** On the web app we open the promo modal, on the
+     * extension there is not enough space to show the
+     * modal so we directly open the upgrade page */
     const handlePromoButtonClick = EXTENSION_BUILD ? upgrade : () => setShowModal(true);
 
-    if (!canShowPromo) return null;
-
-    return (
+    return canShowPromo ? (
         <>
             <MaybeToolTip planName={PLAN_NAMES[planToUpsell]}>
                 <Button
@@ -90,7 +89,8 @@ export const ValentinesDayPromo: FC = () => {
                     <span className="hidden md:block">{c('Valentine_2025').t`VALENTINE'S DEAL`}</span>
                 </Button>
             </MaybeToolTip>
-            {showModal && (
+
+            {!EXTENSION_BUILD && showModal && (
                 <ValentinesDayPromoModal
                     onClose={() => setShowModal(false)}
                     onUpgradeClick={upgrade}
@@ -98,5 +98,9 @@ export const ValentinesDayPromo: FC = () => {
                 />
             )}
         </>
-    );
-};
+    ) : null;
+});
+
+ValentinesDayPromo.displayName = 'ValentinesDayPromoMemo';
+
+export const ValentinesDayPromoButton = WithFeatureFlag(ValentinesDayPromo, PassFeature.PassValentinePromo2025);
