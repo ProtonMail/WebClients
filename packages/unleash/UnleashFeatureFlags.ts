2d15b33658430f022bfa2a3c0d36c04626890429
Alexey Karpov <alexey.karpov@proton.ch>
2025-02-03 15:57:47 +0000

    Disable invoice editing for inhouse invoices

--- packages/unleash/UnleashFeatureFlags.ts
+++ packages/unleash/UnleashFeatureFlags.ts
@@ -65,6 +65,7 @@ enum PaymentsFlag {
     TransactionsView = 'TransactionsView',
     ScheduledDowncycling = 'ScheduledDowncycling',
     BillingAddressModal = 'BillingAddressModal',
+    EditInvoiceDetails = 'EditInvoiceDetails',
 }
 
 enum CalendarFeatureFlag {
