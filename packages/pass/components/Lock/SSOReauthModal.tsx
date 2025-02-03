000b067b5d34366375e5362824495dd5e058d27b
Edvin CANDON <edvin.candon@proton.ch>
2025-02-03 10:33:39 +0100

    Support SSO reauth flow from `PasswordUnlockProvider`
    - Allows configuring the fork request options
    - SSO reauth flow only triggered when sso && no offline components
    
    [IDTEAM-4400]

---
+++ packages/pass/components/Lock/SSOReauthModal.tsx
@@ -0,0 +1,40 @@
+import { type FC } from 'react';
+
+import { c } from 'ttag';
+
+import { Button } from '@proton/atoms';
+import { Form, ModalTwo, ModalTwoContent, ModalTwoFooter, ModalTwoHeader } from '@proton/components';
+import { Card } from '@proton/pass/components/Layout/Card/Card';
+
+import type { PasswordModalProps } from './PasswordModal';
+
+export const SSOReauthModal: FC<PasswordModalProps> = ({ loading, open, reauth, onClose, onReauth }) => {
+    if (!reauth) return null;
+    const { fork, ...payload } = reauth;
+
+    return (
+        <ModalTwo
+            as={Form}
+            onClose={onClose}
+            onReset={onClose}
+            onSubmit={() => (reauth ? onReauth?.(payload, fork) : onClose?.())}
+            open={open}
+            size="small"
+        >
+            <ModalTwoHeader title={c('Title').t`Confirm identity`} closeButtonProps={{ pill: true }} />
+            <ModalTwoContent>
+                <Card className="mb-4 text-sm" type="primary">
+                    {c('Info').t`For security reasons, this action requires identity verification.`}
+                </Card>
+            </ModalTwoContent>
+            <ModalTwoFooter>
+                <Button color="danger" shape="outline" onClick={onClose}>
+                    {c('Action').t`Cancel`}
+                </Button>
+                <Button type="submit" color="norm" loading={loading}>
+                    {c('Action').t`Continue`}
+                </Button>
+            </ModalTwoFooter>
+        </ModalTwo>
+    );
+};
