03d640cf1bbb6dbd231f7e9bf860ae965b395cb7
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2025-02-03 11:26:51 +0100

    feat: Add unstyled prop to ModalContent

--- packages/components/components/modalTwo/ModalContent.tsx
+++ packages/components/components/modalTwo/ModalContent.tsx
@@ -8,14 +8,19 @@ import { ModalContext } from './Modal';
 
 import './ModalContent.scss';
 
-export type ModalContentProps = ComponentPropsWithoutRef<'div'>;
+export type ModalContentProps = { unstyled?: boolean } & ComponentPropsWithoutRef<'div'>;
 
-const ModalContent = ({ className, ...rest }: ModalContentProps) => {
+/**
+ * Wraps content into a scrollable element.
+ *
+ * Default margins applied unless `unstyled` prop is set to true.
+ */
+const ModalContent = ({ className, unstyled, ...rest }: ModalContentProps) => {
     const { id } = useContext(ModalContext);
 
     return (
         <Scroll className="overflow-hidden" scrollContained>
-            <div id={`${id}-description`} className={clsx([className, 'modal-two-content'])} {...rest} />
+            <div id={`${id}-description`} className={clsx([className, !unstyled && 'modal-two-content'])} {...rest} />
         </Scroll>
     );
 };
