cca90eff275dd350eb91056244d83e173a0fc51a
Richard <richard.tetaz@proton.ch>
2025-02-05 12:02:54 +0100

    Skip event loop for inherit folder color updates

--- packages/components/containers/labels/ToggleInheritParentFolderColor.tsx
+++ packages/components/containers/labels/ToggleInheritParentFolderColor.tsx
@@ -4,11 +4,13 @@ import { c } from 'ttag';
 
 import Toggle from '@proton/components/components/toggle/Toggle';
 import useApi from '@proton/components/hooks/useApi';
-import useEventManager from '@proton/components/hooks/useEventManager';
 import useNotifications from '@proton/components/hooks/useNotifications';
 import { useLoading } from '@proton/hooks';
+import { mailSettingsActions } from '@proton/mail/mailSettings';
 import { useMailSettings } from '@proton/mail/mailSettings/hooks';
+import { useDispatch } from '@proton/redux-shared-store';
 import { updateInheritParentFolderColor } from '@proton/shared/lib/api/mailSettings';
+import type { MailSettings } from '@proton/shared/lib/interfaces';
 
 interface Props {
     id?: string;
@@ -17,14 +19,16 @@ interface Props {
 
 const ToggleInheritParentFolderColor = ({ id, className }: Props) => {
     const api = useApi();
-    const { call } = useEventManager();
     const { createNotification } = useNotifications();
     const [loading, withLoading] = useLoading();
     const [mailSettings] = useMailSettings();
+    const dispatch = useDispatch();
 
     const handleChange = async ({ target }: ChangeEvent<HTMLInputElement>) => {
-        await api(updateInheritParentFolderColor(+target.checked));
-        await call();
+        const { MailSettings } = await api<{ MailSettings: MailSettings }>(
+            updateInheritParentFolderColor(+target.checked)
+        );
+        dispatch(mailSettingsActions.updateMailSettings(MailSettings));
         createNotification({
             text: c('label/folder notification').t`Preference saved`,
         });
