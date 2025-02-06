59c9001889d258394d0f7eefca165135fec91fce
Edvin CANDON <edvin.candon@proton.ch>
2025-02-06 11:42:16 +0100

    Clean-up org lock selectors

--- packages/pass/store/selectors/organization.ts
+++ packages/pass/store/selectors/organization.ts
@@ -1,13 +1,8 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-import { LockMode } from '@proton/pass/lib/auth/lock/types';
 import type { OrganizationState } from '@proton/pass/store/reducers/organization';
 import type { State } from '@proton/pass/store/types';
 import type { MaybeNull, OrganizationUpdatePasswordPolicyRequest } from '@proton/pass/types';
 import type { OrganizationSettings } from '@proton/pass/types/data/organization';
 import type { Organization } from '@proton/shared/lib/interfaces';
-
-import { selectLockMode } from './settings';
 
 export const selectOrganizationState = ({ organization }: State): MaybeNull<OrganizationState> => organization;
 
@@ -18,12 +13,6 @@ export const selectOrganizationSettings = ({ organization }: State): MaybeNull<O
     organization?.settings ?? null;
 
 export const selectCanUpdateOrganization = ({ organization }: State): boolean => organization?.canUpdate ?? false;
-
-export const selectLockSetupRequired = createSelector(
-    [selectOrganizationSettings, selectLockMode],
-    (orgSettings, lockMode) =>
-        Boolean(orgSettings?.ForceLockSeconds && orgSettings.ForceLockSeconds > 0 && lockMode === LockMode.NONE)
-);
 
 export const selectOrganizationPasswordGeneratorPolicy = ({
     organization,
