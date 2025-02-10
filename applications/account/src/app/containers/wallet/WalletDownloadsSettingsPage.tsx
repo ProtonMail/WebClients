ce0eb74a2dbf50551a27331a0dd8594fa701bf85
Kévin Ta <kevin.ta@proton.ch>
2025-02-10 11:05:12 +0100

    Add APK link

--- applications/account/src/app/containers/wallet/WalletDownloadsSettingsPage.tsx
+++ applications/account/src/app/containers/wallet/WalletDownloadsSettingsPage.tsx
@@ -1,24 +1,15 @@
-import { c } from 'ttag';
-
-import { DownloadClientCard, SettingsParagraph, SettingsSectionWide, Tooltip } from '@proton/components';
-import { WALLET_APP_NAME } from '@proton/shared/lib/constants';
+import { DownloadClientCard, SettingsSectionWide } from '@proton/components';
 import { WALLET_CLIENTS } from '@proton/wallet';
 
 export const WalletDownloadsSettingsPage = () => {
     return (
         <SettingsSectionWide>
-            <SettingsParagraph>
-                {c('wallet_signup_2024:Info')
-                    .t`${WALLET_APP_NAME} is in Early Access. You can download the iOS app through Apple TestFlight and the Android app through Google Open Testing.`}
-            </SettingsParagraph>
             <div className="flex gap-4 flex-column md:flex-row">
-                {Object.values(WALLET_CLIENTS).map(({ title, icon, link }) => {
+                {Object.values(WALLET_CLIENTS).map(({ title, icon, link, items }) => {
                     return (
-                        <Tooltip key={title} title={!link && c('wallet_signup_2024:Info').t`Coming soon`}>
-                            <div>
-                                <DownloadClientCard title={title} icon={icon} link={link} />
-                            </div>
-                        </Tooltip>
+                        <div>
+                            <DownloadClientCard title={title} icon={icon} link={link} items={items} />
+                        </div>
                     );
                 })}
             </div>
