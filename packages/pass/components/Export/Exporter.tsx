47b344789743f6e08d6453774d01c24b27c7bc36
Edvin CANDON <edvin.candon@proton.ch>
2025-02-11 18:02:04 +0100

    Force online-only export

--- packages/pass/components/Export/Exporter.tsx
+++ packages/pass/components/Export/Exporter.tsx
@@ -4,6 +4,7 @@ import { useFormik } from 'formik';
 import { c } from 'ttag';
 
 import { useNotifications } from '@proton/components';
+import { useConnectivity } from '@proton/pass/components/Core/ConnectivityProvider';
 import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
 import { ExportForm } from '@proton/pass/components/Export/ExportForm';
 import { usePasswordTypeSwitch, usePasswordUnlock } from '@proton/pass/components/Lock/PasswordUnlockProvider';
@@ -25,6 +26,7 @@ type Props = {
 export const Exporter: FC<Props> = ({ onConfirm }) => {
     const { exportData } = usePassCore();
     const { createNotification } = useNotifications();
+    const online = useConnectivity();
 
     const initialValues: ExportFormValues = { format: ExportFormat.PGP, passphrase: '' };
     const [loading, setLoading] = useState(false);
@@ -39,59 +41,61 @@ export const Exporter: FC<Props> = ({ onConfirm }) => {
         validateOnMount: true,
         validate: validateExportForm,
         onSubmit: async (values) => {
-            try {
-                setLoading(true);
+            if (online) {
+                try {
+                    setLoading(true);
 
-                await confirmPassword({
-                    reauth: {
-                        type: ReauthAction.SSO_EXPORT,
-                        data: values,
-                        fork: { promptBypass: 'none', promptType: 'default' },
-                    },
-                    message: passwordTypeSwitch({
-                        extra: c('Info')
-                            .t`Please confirm your extra password in order to export your ${PASS_APP_NAME} data`,
-                        sso: c('Info')
-                            .t`Please confirm your backup password in order to export your ${PASS_APP_NAME} data`,
-                        default: c('Info')
-                            .t`Please confirm your ${BRAND_NAME} password in order to export your ${PASS_APP_NAME} data`,
-                    }),
-                    onSubmit: (password) => onConfirm(password),
-                    onError: () => throwError({ name: 'AuthConfirmInvalidError' }),
-                    onAbort: () => throwError({ name: 'AuthConfirmAbortError' }),
-                });
+                    await confirmPassword({
+                        reauth: {
+                            type: ReauthAction.SSO_EXPORT,
+                            data: values,
+                            fork: { promptBypass: 'none', promptType: 'default' },
+                        },
+                        message: passwordTypeSwitch({
+                            extra: c('Info')
+                                .t`Please confirm your extra password in order to export your ${PASS_APP_NAME} data`,
+                            sso: c('Info')
+                                .t`Please confirm your backup password in order to export your ${PASS_APP_NAME} data`,
+                            default: c('Info')
+                                .t`Please confirm your ${BRAND_NAME} password in order to export your ${PASS_APP_NAME} data`,
+                        }),
+                        onSubmit: (password) => onConfirm(password),
+                        onError: () => throwError({ name: 'AuthConfirmInvalidError' }),
+                        onAbort: () => throwError({ name: 'AuthConfirmAbortError' }),
+                    });
 
-                download(await exportData(values));
+                    download(await exportData(values));
 
-                form.resetForm({ values: { ...form.values, passphrase: '' } });
-                void form.validateForm();
+                    form.resetForm({ values: { ...form.values, passphrase: '' } });
+                    void form.validateForm();
 
-                createNotification({ type: 'success', text: c('Info').t`Successfully exported all your items` });
-            } catch (error) {
-                const notification = (() => {
-                    if (error instanceof Error) {
-                        switch (error.name) {
-                            case 'AuthConfirmInvalidError':
-                                return passwordTypeSwitch({
-                                    sso: c('Error')
-                                        .t`Authentication failed. Confirm your backup password in order to proceed with the export`,
-                                    extra: c('Error')
-                                        .t`Authentication failed. Confirm your extra password in order to proceed with the export`,
-                                    default: c('Error')
-                                        .t`Authentication failed. Confirm your ${BRAND_NAME} password in order to proceed with the export`,
-                                });
-                            case 'AuthConfirmAbortError':
-                                return null;
+                    createNotification({ type: 'success', text: c('Info').t`Successfully exported all your items` });
+                } catch (error) {
+                    const notification = (() => {
+                        if (error instanceof Error) {
+                            switch (error.name) {
+                                case 'AuthConfirmInvalidError':
+                                    return passwordTypeSwitch({
+                                        sso: c('Error')
+                                            .t`Authentication failed. Confirm your backup password in order to proceed with the export`,
+                                        extra: c('Error')
+                                            .t`Authentication failed. Confirm your extra password in order to proceed with the export`,
+                                        default: c('Error')
+                                            .t`Authentication failed. Confirm your ${BRAND_NAME} password in order to proceed with the export`,
+                                    });
+                                case 'AuthConfirmAbortError':
+                                    return null;
+                            }
                         }
-                    }
 
-                    return c('Warning').t`An error occurred while exporting your data`;
-                })();
+                        return c('Warning').t`An error occurred while exporting your data`;
+                    })();
 
-                logger.warn(`[Settings::Exporter] export failed`, error);
-                if (notification) createNotification({ type: 'error', text: notification });
-            } finally {
-                setLoading(false);
+                    logger.warn(`[Settings::Exporter] export failed`, error);
+                    if (notification) createNotification({ type: 'error', text: notification });
+                } finally {
+                    setLoading(false);
+                }
             }
         },
     });
