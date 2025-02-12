1e5504a75058225da111a9ca96a629bebca0ea8e
Leopold Gattinger <leopold.gattinger@proton.ch>
2025-02-12 11:51:03 +0000

    VPNBE-1159: Add shared server creation modal & listing

---
+++ packages/components/containers/vpn/sharedServers/constants.ts
@@ -0,0 +1,21 @@
+export enum PolicyType {
+    /**
+     * An ACTIVE NONE policy means that no users of the org can access any location.
+     */
+    None = 0,
+
+    /**
+     * An ACTIVE ALL policy means that all users of the org can access every location.
+     */
+    All = 1,
+
+    /**
+     * If only CUSTOM policies are ACTIVE, the locations rights are cumulative.
+     */
+    Custom = 2,
+}
+
+export enum PolicyState {
+    Inactive = 0,
+    Active = 1,
+}
