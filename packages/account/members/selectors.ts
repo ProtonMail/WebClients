04aa13bababd241bc006e9185cf6f4ebd00720ad
Mattias Svanström <mmso@mmso.se>
2025-02-12 09:50:24 +0000

    Global SSO admin management improvements

--- packages/account/members/selectors.ts
+++ packages/account/members/selectors.ts
@@ -1,9 +1,9 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-import { getIsMemberInactive } from '@proton/shared/lib/keys/memberHelper';
+import { getIsMemberDisabled } from '@proton/shared/lib/keys/memberHelper';
 
 import { selectMembers } from './index';
 
 export const selectDisabledMembers = createSelector(selectMembers, (membersState) => {
-    return membersState.value?.filter(getIsMemberInactive) || [];
+    return membersState.value?.filter(getIsMemberDisabled) || [];
 });
