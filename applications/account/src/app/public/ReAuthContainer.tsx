b9ebc5dfe0a350d180cd26e76bd5898ffb4edce4
Edvin CANDON <edvin.candon@proton.ch>
2025-02-14 18:22:50 +0100

    Fix SSO bypass condition
    - No need to check of `offlineKey` as it will be undefined on first SSO login

--- applications/account/src/app/public/ReAuthContainer.tsx
+++ applications/account/src/app/public/ReAuthContainer.tsx
@@ -4,16 +4,18 @@ import { Link } from 'react-router-dom';
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms';
-import { type OnLoginCallback, type OnLoginCallbackArguments, useModalTwoPromise } from '@proton/components';
 import {
     DropdownMenu,
     DropdownMenuButton,
     Icon,
     InputFieldTwo,
+    type OnLoginCallback,
+    type OnLoginCallbackArguments,
     PasswordInputTwo,
     SimpleDropdown,
     useErrorHandler,
     useFormErrors,
+    useModalTwoPromise,
 } from '@proton/components';
 import type { AuthSession } from '@proton/components/containers/login/interface';
 import { handleReAuthKeyPassword } from '@proton/components/containers/login/loginActions';
@@ -59,13 +61,8 @@ export const getReAuthState = (
     let reAuthType = forkParameters?.promptType ?? 'default';
 
     // Normalize the reauth type to 'default' (auth with IdP - instead of auth with backup password) for SSO accounts
-    // when the offline key exists and 'sso' bypass is requested
-    if (
-        forkParameters?.promptBypass === 'sso' &&
-        session.offlineKey &&
-        getIsGlobalSSOAccount(session.User) &&
-        reAuthType !== 'default'
-    ) {
+    // ignoring if the offline key exists or not and 'sso' bypass is requested
+    if (forkParameters?.promptBypass === 'sso' && getIsGlobalSSOAccount(session.User) && reAuthType !== 'default') {
         reAuthType = 'default';
     }
 
