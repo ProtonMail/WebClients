e73f59502c5af1dabf7f74b2327d7f75732fcbba
Edvin CANDON <edvin.candon@proton.ch>
2025-02-18 08:51:17 +0100

    Clean-up firefox external messaging fallback content-script

--- applications/pass-extension/src/app/content/firefox/index.ts
+++ applications/pass-extension/src/app/content/firefox/index.ts
@@ -1,5 +1,69 @@
-import { authFallback } from './auth-fallback';
-import { otherFallback } from './other-fallback';
+import { contentScriptMessage, sendMessage, successMessage } from '@proton/pass/lib/extension/message/send-message';
+import { WorkerMessageType } from '@proton/pass/types';
+import { logger } from '@proton/pass/utils/logger';
+import { APPS } from '@proton/shared/lib/constants';
 
-authFallback();
-otherFallback();
+window.addEventListener('message', async (message) => {
+    try {
+        if (message.data && message.data?.type !== undefined && message.data?.app === APPS.PROTONPASSBROWSEREXTENSION) {
+            switch (message.data.type) {
+                case WorkerMessageType.ACCOUNT_FORK:
+                    const { keyPassword, selector, state, persistent, trusted } = message.data.payload;
+                    return await sendMessage.on(
+                        contentScriptMessage({
+                            type: WorkerMessageType.ACCOUNT_FORK,
+                            payload: {
+                                selector,
+                                state,
+                                keyPassword,
+                                persistent,
+                                trusted,
+                            },
+                        }),
+                        (response) =>
+                            window.postMessage({
+                                token: message.data?.token,
+                                ...response,
+                            })
+                    );
+
+                case WorkerMessageType.ACCOUNT_EXTENSION:
+                    return await sendMessage(
+                        contentScriptMessage({
+                            type: WorkerMessageType.ACCOUNT_EXTENSION,
+                        })
+                    );
+
+                case WorkerMessageType.ACCOUNT_ONBOARDING:
+                    return await sendMessage(
+                        contentScriptMessage({
+                            type: WorkerMessageType.ACCOUNT_ONBOARDING,
+                        })
+                    );
+
+                case WorkerMessageType.ACCOUNT_PROBE:
+                    return window.postMessage(
+                        successMessage({
+                            token: message.data?.token,
+                        })
+                    );
+
+                case WorkerMessageType.LOAD_CONTENT_SCRIPT_EXTERNAL:
+                    return await sendMessage(
+                        contentScriptMessage({
+                            type: WorkerMessageType.LOAD_CONTENT_SCRIPT_EXTERNAL,
+                        })
+                    );
+
+                case WorkerMessageType.UNLOAD_CONTENT_SCRIPT_EXTERNAL:
+                    return await sendMessage(
+                        contentScriptMessage({
+                            type: WorkerMessageType.UNLOAD_CONTENT_SCRIPT_EXTERNAL,
+                        })
+                    );
+            }
+        }
+    } catch (e) {
+        logger.warn('[ContentScript::Fallback]', e);
+    }
+});
