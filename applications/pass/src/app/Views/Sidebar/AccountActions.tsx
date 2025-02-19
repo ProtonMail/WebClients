86b70330be4a47135842ee27591d69f24bd09fa8
Edvin CANDON <edvin.candon@proton.ch>
2025-02-19 12:12:01 +0100

    Feature flag clean-up
    - Remove pass family promo onboarding rule
    - Remove auth switch feature flag proxy
    - Remove `useOfflineSupported` hook in favor of OFFLINE_SUPPORTED

--- applications/pass/src/app/Views/Sidebar/AccountActions.tsx
+++ applications/pass/src/app/Views/Sidebar/AccountActions.tsx
@@ -1,19 +1,14 @@
-import { memo, useMemo } from 'react';
+import { memo } from 'react';
 import { useSelector } from 'react-redux';
 
 import { AccountSwitcherTooltip } from 'proton-pass-web/app/Auth/AccountSwitcher';
-import { checkAuthSwitch, useAvailableSessions } from 'proton-pass-web/app/Auth/AuthSwitchProvider';
+import { useAvailableSessions } from 'proton-pass-web/app/Auth/AuthSwitchProvider';
 
 import { ButtonLike } from '@proton/atoms';
 import { UserPanel } from '@proton/pass/components/Account/UserPanel';
-import { useFeatureFlag } from '@proton/pass/hooks/useFeatureFlag';
 import { selectPassPlan, selectPlanDisplayName, selectUser } from '@proton/pass/store/selectors';
-import { PassFeature } from '@proton/pass/types/api/features';
-import clsx from '@proton/utils/clsx';
 
 export const AccountActions = memo(() => {
-    const accountSwitchEnabled = useFeatureFlag(PassFeature.PassAccountSwitchV1);
-    const authSwitchEnabled = useMemo(() => accountSwitchEnabled || checkAuthSwitch(), [accountSwitchEnabled]);
     const sessions = useAvailableSessions();
 
     const user = useSelector(selectUser);
@@ -21,15 +16,9 @@ export const AccountActions = memo(() => {
     const planDisplayName = useSelector(selectPlanDisplayName);
 
     return (
-        <AccountSwitcherTooltip sessions={authSwitchEnabled ? sessions : []}>
+        <AccountSwitcherTooltip sessions={sessions}>
             {({ anchorRef, toggle }) => (
-                <ButtonLike
-                    ref={anchorRef}
-                    onClick={toggle}
-                    shape="ghost"
-                    className={clsx('flex-1', !authSwitchEnabled && 'pointer-events-none')}
-                    size="small"
-                >
+                <ButtonLike ref={anchorRef} onClick={toggle} shape="ghost" className="flex-1" size="small">
                     <UserPanel
                         email={user?.Email ?? ''}
                         name={user?.DisplayName ?? user?.Name ?? ''}
