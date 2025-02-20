bc860dd5034a3723d9975fa8e6e3434442a3694b
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2025-02-20 17:35:45 +0100

    Hide in-app notification during onboarding or if any modal is open
    [IDTEAM-4522]

--- packages/pass/components/Notifications/InAppNotifications.tsx
+++ packages/pass/components/Notifications/InAppNotifications.tsx
@@ -1,4 +1,4 @@
-import { type FC, useMemo } from 'react';
+import { type FC, useEffect, useMemo, useState } from 'react';
 
 import { WithFeatureFlag } from '@proton/pass/components/Core/WithFeatureFlag';
 import type { InAppNotificationRenderProps } from '@proton/pass/components/Notifications/WithInAppNotification';
@@ -7,6 +7,8 @@ import { selectNextNotification } from '@proton/pass/store/selectors';
 import { InAppNotificationDisplayType } from '@proton/pass/types';
 import { PassFeature } from '@proton/pass/types/api/features';
 import { getEpoch } from '@proton/pass/utils/time/epoch';
+import { isModalOpen } from '@proton/shared/lib/busy';
+import { wait } from '@proton/shared/lib/helpers/promise';
 
 import { InAppNotificationBanner } from './InAppNotificationBanner';
 import { InAppNotificationModal } from './InAppNotificationModal';
@@ -23,8 +25,20 @@ const getNotificationComponent = (displayType: InAppNotificationDisplayType): FC
 export const InAppNotifications: FC = WithFeatureFlag(() => {
     const now = useMemo(() => getEpoch(), []);
     const notification = useMemoSelector(selectNextNotification, [now]);
+    const [showNotification, setShowNotification] = useState(false);
 
-    if (!notification) return null;
+    useEffect(() => {
+        const checkNotificationDisplay = async () => {
+            if (notification?.content.displayType === InAppNotificationDisplayType.MODAL) {
+                await wait(1_500);
+                setShowNotification(!isModalOpen());
+            } else setShowNotification(true);
+        };
+
+        void checkNotificationDisplay();
+    }, []);
+
+    if (!notification || !showNotification) return null;
 
     const Component = getNotificationComponent(notification?.content.displayType);
     return <Component dense={EXTENSION_BUILD} notification={notification} />;
