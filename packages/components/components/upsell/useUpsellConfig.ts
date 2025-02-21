457953f5561ccb6dc2cb5196870acc4d666b4094
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2025-02-21 08:53:41 +0100

    refactor: Remove OneDollarPromoComponent and upsellConfigHelpers

--- packages/components/components/upsell/useUpsellConfig.ts
+++ packages/components/components/upsell/useUpsellConfig.ts
@@ -1,25 +1,21 @@
 import { useSubscription } from '@proton/account/subscription/hooks';
 import { useUser } from '@proton/account/user/hooks';
+import { getHasInboxDesktopInAppPayments } from '@proton/components/containers/desktop/useHasInboxDesktopInAppPayments';
 import {
-    getHasInboxDesktopInAppPayments,
-    useHasInboxDesktopInAppPayments,
-} from '@proton/components/containers/desktop/useHasInboxDesktopInAppPayments';
-import {
+    type OpenCallbackProps,
     type OpenSubscriptionModalCallback,
     useSubscriptionModal,
 } from '@proton/components/containers/payments/subscription/SubscriptionModalProvider';
-import type { SUBSCRIPTION_STEPS } from '@proton/components/containers/payments/subscription/constants';
+import { SUBSCRIPTION_STEPS } from '@proton/components/containers/payments/subscription/constants';
 import useConfig from '@proton/components/hooks/useConfig';
-import { type CYCLE } from '@proton/payments';
+import { CYCLE } from '@proton/payments';
 import type { ADDON_NAMES, PLANS } from '@proton/payments';
 import { APPS, type APP_NAMES } from '@proton/shared/lib/constants';
 import { addUpsellPath, getUpgradePath } from '@proton/shared/lib/helpers/upsell';
 import { formatURLForAjaxRequest } from '@proton/shared/lib/helpers/url';
 import type { Subscription, UserModel } from '@proton/shared/lib/interfaces';
-import { useFlag, type useGetFlag } from '@proton/unleash';
+import { useGetFlag } from '@proton/unleash';
 import noop from '@proton/utils/noop';
-
-import getUpsellSubscriptionModalConfig from './getUpsellSubscriptionModalConfig';
 
 interface Props {
     upsellRef?: string;
@@ -42,6 +38,8 @@ export const getUpsellConfig = ({
     appName,
     coupon,
     cycle,
+    maximumCycle,
+    minimumCycle,
     getFlag,
     onSubscribed,
     openSubscriptionModal,
@@ -55,7 +53,7 @@ export const getUpsellConfig = ({
     appName: APP_NAMES;
     getFlag: ReturnType<typeof useGetFlag>;
     openSubscriptionModal: OpenSubscriptionModalCallback;
-    subscription: Subscription;
+    subscription: Subscription | undefined;
     user: UserModel;
 }): { upgradePath: string; onUpgrade?: () => void } => {
     const hasSubscriptionModal = openSubscriptionModal !== noop;
@@ -65,7 +63,23 @@ export const getUpsellConfig = ({
     const hasInAppPayments = appsWithInApp.has(appName) || hasInboxDesktopInAppPayments;
 
     if (hasSubscriptionModal && hasInAppPayments && inboxUpsellFlowEnabled && upsellRef && !preventInApp) {
-        const subscriptionCallBackProps = getUpsellSubscriptionModalConfig({ coupon, cycle, step, upsellRef, plan });
+        const subscriptionCallBackProps: OpenCallbackProps = {
+            coupon,
+            cycle: cycle || CYCLE.YEARLY,
+            maximumCycle,
+            minimumCycle,
+            disablePlanSelection: step === SUBSCRIPTION_STEPS.CHECKOUT,
+            metrics: {
+                source: 'upsells',
+            },
+            mode: 'upsell-modal', // hide the Free plan
+            step: step || SUBSCRIPTION_STEPS.CHECKOUT,
+            upsellRef: upsellRef,
+        };
+
+        if (plan) {
+            subscriptionCallBackProps.planIDs = { [plan]: 1 };
+        }
 
         // The subscription modal will open in inbox app
         return {
@@ -103,40 +117,25 @@ const useUpsellConfig = ({
     const [user] = useUser();
     const [subscription] = useSubscription();
     const [openSubscriptionModal] = useSubscriptionModal();
-    const hasSubscriptionModal = openSubscriptionModal !== noop;
-    const inboxUpsellFlowEnabled = useFlag('InboxUpsellFlow');
-    const { APP_NAME } = useConfig();
-    const hasInboxDesktopInAppPayments = useHasInboxDesktopInAppPayments();
-    const hasInAppPayments = appsWithInApp.has(APP_NAME) || hasInboxDesktopInAppPayments;
+    const { APP_NAME: appName } = useConfig();
+    const getFlag = useGetFlag();
 
-    if (hasSubscriptionModal && hasInAppPayments && inboxUpsellFlowEnabled && upsellRef && !preventInApp) {
-        const subscriptionCallBackProps = getUpsellSubscriptionModalConfig({
-            coupon,
-            cycle,
-            step,
-            upsellRef,
-            plan,
-            maximumCycle,
-            minimumCycle,
-        });
-
-        // The subscription modal will open in inbox app
-        return {
-            upgradePath: '',
-            onUpgrade() {
-                // Generate a mocked request to track upsell activity
-                const urlParameters = { ref: upsellRef, load: 'modalOpen' };
-                const url = formatURLForAjaxRequest(window.location.href, urlParameters);
-                fetch(url).catch(noop);
-
-                // Open the subscription modal
-                openSubscriptionModal({ ...subscriptionCallBackProps, onSubscribed });
-            },
-        };
-    }
-
-    // The user will be redirected to account app
-    return { upgradePath: addUpsellPath(getUpgradePath({ user, subscription, app: APP_NAME }), upsellRef) };
+    return getUpsellConfig({
+        appName,
+        getFlag,
+        openSubscriptionModal,
+        subscription,
+        user,
+        upsellRef,
+        step,
+        coupon,
+        cycle,
+        plan,
+        onSubscribed,
+        preventInApp,
+        maximumCycle,
+        minimumCycle,
+    });
 };
 
 export default useUpsellConfig;
