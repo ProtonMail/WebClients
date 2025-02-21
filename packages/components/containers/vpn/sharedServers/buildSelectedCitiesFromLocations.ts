7beffead69eaa90ea478085c0322387ba0f4e6e8
Juan Francisco Granados <juan-francisco.granados@proton.ch>
2025-02-21 09:39:28 +0000

    VPNBE-1160 Edit & delete custom policy

---
+++ packages/components/containers/vpn/sharedServers/buildSelectedCitiesFromLocations.ts
@@ -0,0 +1,16 @@
+import type { SharedServerLocation } from '@proton/components/containers/vpn/sharedServers/useSharedServers';
+
+export function buildSelectedCitiesFromLocations(locations: SharedServerLocation[] = []): Record<string, string[]> {
+    const citiesMap: Record<string, string[]> = {};
+
+    for (const { Country, City } of locations) {
+        if (!citiesMap[Country]) {
+            citiesMap[Country] = [];
+        }
+        if (!citiesMap[Country].includes(City)) {
+            citiesMap[Country].push(City);
+        }
+    }
+
+    return citiesMap;
+}
