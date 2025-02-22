59dcb639693f006fe19f1cca554d025258c7cc45
Edvin CANDON <edvin.candon@proton.ch>
2025-02-22 11:04:16 +0100

    Disable autosave for `PasswordModal`

--- packages/pass/components/Lock/PasswordConfirm.tsx
+++ packages/pass/components/Lock/PasswordConfirm.tsx
@@ -16,6 +16,7 @@ export const PasswordConfirm: FC<Props> = ({ mode, onSuccess }) => {
 
     return (
         <PasswordForm
+            autosavable
             id="password-confirm"
             loading={confirm.loading}
             submitLabel={c('Action').t`Continue`}
