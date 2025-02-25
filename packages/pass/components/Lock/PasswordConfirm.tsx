bff0dda6e0d4d872595d13f77bf895b1bb87d8b5
Edvin CANDON <edvin.candon@proton.ch>
2025-02-25 08:17:50 +0100

    Disable extra password form if offline

--- packages/pass/components/Lock/PasswordConfirm.tsx
+++ packages/pass/components/Lock/PasswordConfirm.tsx
@@ -2,6 +2,7 @@ import { type FC } from 'react';
 
 import { c } from 'ttag';
 
+import { useConnectivity } from '@proton/pass/components/Core/ConnectivityProvider';
 import { useRequest } from '@proton/pass/hooks/useRequest';
 import { type PasswordVerification } from '@proton/pass/lib/auth/password';
 import { validateCurrentPassword } from '@proton/pass/lib/validation/auth';
@@ -12,11 +13,13 @@ import { PasswordForm } from './PasswordForm';
 type Props = { mode?: PasswordVerification; onSuccess: () => void };
 
 export const PasswordConfirm: FC<Props> = ({ mode, onSuccess }) => {
+    const online = useConnectivity();
     const confirm = useRequest(passwordConfirm, { initial: true, onSuccess });
 
     return (
         <PasswordForm
             autosavable
+            disabled={!online}
             id="password-confirm"
             loading={confirm.loading}
             submitLabel={c('Action').t`Continue`}
