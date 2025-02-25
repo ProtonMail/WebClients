c23fb2476a1a94e4c8c7bcec479dcfb1e8c0df5f
Gregory Linford <gregory.linford@proton.ch>
2025-02-25 06:01:21 +0000

    [CS] Fix TransformStream polyfill support

--- applications/drive/src/app/utils/webStreamsPolyfill.ts
+++ applications/drive/src/app/utils/webStreamsPolyfill.ts
@@ -9,13 +9,15 @@ const needsStreamsPolyfill = (): boolean => {
         firefoxVersion = parseInt(firefoxMatch[1], 10);
     }
 
-    const isOldFirefox = navigator.userAgent.includes('Firefox') && firefoxVersion < 100;
+    const isOldFirefox = navigator.userAgent.includes('Firefox') && firefoxVersion < 102;
 
     return !hasReadableStream || !hasWritableStream || !hasTransformStream || isOldFirefox;
 };
 
+const needStreamsPolyfill = needsStreamsPolyfill();
+
 export const loadStreamsPolyfill = async (): Promise<void> => {
-    if (needsStreamsPolyfill()) {
+    if (needStreamsPolyfill) {
         try {
             await import(
                 /* webpackChunkName: "web-streams-polyfill" */
@@ -26,4 +28,18 @@ export const loadStreamsPolyfill = async (): Promise<void> => {
             throw error;
         }
     }
+};
+
+export const loadCreateReadableStreamWrapper = async (
+    stream: ReadableStream<Uint8Array<ArrayBufferLike>>
+): Promise<ReadableStream<Uint8Array<ArrayBufferLike>>> => {
+    if (needStreamsPolyfill) {
+        const { createReadableStreamWrapper } = await import(
+            /* webpackChunkName: "web-streams-adapter" */
+            '@mattiasbuelens/web-streams-adapter'
+        );
+        const toPolyfillReadable = createReadableStreamWrapper(ReadableStream);
+        return toPolyfillReadable(stream) as ReadableStream<Uint8Array<ArrayBufferLike>>;
+    }
+    return stream;
 };
