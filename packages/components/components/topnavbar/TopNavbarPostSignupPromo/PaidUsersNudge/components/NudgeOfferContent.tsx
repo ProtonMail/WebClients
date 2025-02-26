5d6a7486ac3ed2f4ae0fd5a3fdcacdf0aa21d539
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-02-26 13:13:44 +0000

    Addapt offer for $1 promo users

--- packages/components/components/topnavbar/TopNavbarPostSignupPromo/PaidUsersNudge/components/NudgeOfferContent.tsx
+++ packages/components/components/topnavbar/TopNavbarPostSignupPromo/PaidUsersNudge/components/NudgeOfferContent.tsx
@@ -1,12 +1,16 @@
+import { differenceInDays, format, fromUnixTime } from 'date-fns';
 import { c } from 'ttag';
 
+import { useSubscription } from '@proton/account/subscription/hooks';
 import { Button } from '@proton/atoms/index';
 import Price from '@proton/components/components/price/Price';
 import { useAutomaticCurrency } from '@proton/components/payments/client-extensions';
+import { dateLocale } from '@proton/shared/lib/i18n';
 import clsx from '@proton/utils/clsx';
 
 import { NudeOfferCTA } from './NudeOfferCTA';
 import { type PriceData } from './interface';
+import { getWindowEndDate } from './paidUserNudgeHelper';
 
 interface Props {
     imgSrc: string;
@@ -18,13 +22,26 @@ interface Props {
 
 export const NudgeOfferContent = ({ imgSrc, variant, onClick, onNeverShow, prices }: Props) => {
     const [currency] = useAutomaticCurrency();
+    const [subscription] = useSubscription();
+
+    const isOneDollarUser = subscription?.Amount === 100;
+    const subscriptionAge = differenceInDays(Date.now(), fromUnixTime(subscription?.PeriodStart ?? Date.now() / 1000));
+
+    const offerEndDate = getWindowEndDate(subscriptionAge);
+    const formattedDate = offerEndDate ? format(offerEndDate, 'PPP', { locale: dateLocale }) : undefined;
 
     return (
         <section className="p-8">
             <div className="mb-4 text-center">
                 <img src={imgSrc} alt="" className="mb-2" />
-                <p className="color-weak text-sm m-0 mb-2">{c('Offer').t`Limited-time offer`}</p>
-                <p className="text-lg text-semibold m-0">{c('Offer').t`Save big when you pay annually`}</p>
+                <p className="color-weak text-sm m-0 mb-2">
+                    {isOneDollarUser ? c('Offer').t`Your subscription renews soon` : c('Offer').t`Limited-time offer`}
+                </p>
+                <p className="text-lg text-semibold m-0">
+                    {isOneDollarUser
+                        ? c('Offer').t`Pay annually, secure our best offer`
+                        : c('Offer').t`Save big when you pay annually`}
+                </p>
             </div>
             <div className="rounded-lg border p-3 mb-4">
                 {prices?.discountedPrice ? (
@@ -48,8 +65,14 @@ export const NudgeOfferContent = ({ imgSrc, variant, onClick, onNeverShow, price
                 <Button color="norm" className="mb-4" onClick={onClick} fullWidth>
                     <NudeOfferCTA prices={prices} variant={variant} />
                 </Button>
-                <Button shape="underline" color="norm" onClick={onNeverShow}>{c('Offer')
-                    .t`Don’t show this offer again`}</Button>
+                <div className="flex gap-1 text-sm flex-column">
+                    {offerEndDate ? (
+                        // translator: Example string: Offer expires March 4th, 2025.
+                        <span className="m-0 color-weak">{c('Offer').t`Offer expires ${formattedDate}.`}</span>
+                    ) : null}
+                    <Button shape="underline" color="norm" onClick={onNeverShow}>{c('Offer')
+                        .t`Don’t show this offer again`}</Button>
+                </div>
             </div>
         </section>
     );
