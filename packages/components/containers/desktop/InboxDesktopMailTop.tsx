284f1bbba7f0ff1e316290481cc82443ce440f8b
Pablo Molina <contact@pablomolina.me>
2025-02-26 11:46:33 +0100

    feat(INDA-428): app cache killswitch

--- packages/components/containers/desktop/InboxDesktopMailTop.tsx
+++ packages/components/containers/desktop/InboxDesktopMailTop.tsx
@@ -1,10 +1,13 @@
 import { useUser } from '@proton/account/user/hooks';
 
 import InboxDesktopOutdatedAppTopBanner from './InboxDesktopOutdatedAppTopBanner';
+import { useInboxDesktopAppCache } from './appCache/useInboxDesktopAppCache';
 import InboxDesktopDefaultAppTopBanner from './defaultApp/InboxDesktopDefaultAppTopBanner';
 import InboxDesktopFreeTrialTopBanner from './freeTrial/InboxDesktopFreeTrialTopBanner';
 
 export default function InboxDesktopMailTop() {
+    useInboxDesktopAppCache();
+
     const [user] = useUser();
 
     return (
