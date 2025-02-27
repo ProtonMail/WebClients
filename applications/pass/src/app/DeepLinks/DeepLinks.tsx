abf5fcd1d1e55c020e5b177075e0f7b5dceb8585
Edvin CANDON <edvin.candon@proton.ch>
2025-02-27 08:10:30 +0100

    Add unit tests for deeplink URLs

--- applications/pass/src/app/DeepLinks/DeepLinks.tsx
+++ applications/pass/src/app/DeepLinks/DeepLinks.tsx
@@ -1,10 +1,10 @@
-import { type FC, useMemo } from 'react';
+import { type FC } from 'react';
 import { Redirect, type RouteChildrenProps, useParams } from 'react-router-dom';
 
 import { UpsellRef } from '@proton/pass/constants';
 import { useNavigateToUpgrade } from '@proton/pass/hooks/useNavigateToUpgrade';
 
-import { DEEPLINK_CONFIG, fallback, getURLPrefix } from './configuration';
+import { DEEPLINK_CONFIG, getDeeplinkFallbackURL, getDeeplinkURLPrefix } from './configuration';
 import type { DeepLinkRoutes } from './types';
 
 export const DeepLinks: FC<RouteChildrenProps> = ({ match, location }) => {
@@ -14,12 +14,9 @@ export const DeepLinks: FC<RouteChildrenProps> = ({ match, location }) => {
 
     // Redirect to external link when 'upgrade' event is received
     if (key === 'upgrade') navigateToUpgrade();
-
-    const targetUrl = useMemo(() => {
-        const params = new URLSearchParams(location.search);
-        const config = DEEPLINK_CONFIG[key] || fallback;
-        return `${getURLPrefix(path)}${config(params)}`;
-    }, [key, location, match]);
+    const params = new URLSearchParams(location.search);
+    const config = DEEPLINK_CONFIG[key] || getDeeplinkFallbackURL;
+    const targetUrl = `${getDeeplinkURLPrefix(path)}${config(params)}`;
 
     return <Redirect to={targetUrl} />;
 };
