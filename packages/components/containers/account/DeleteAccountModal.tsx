3360bf8a84d666e8d931c5960b8006a3bf71c1c2
Jack Crompton <jack.crompton@proton.ch>
2025-02-28 14:22:22 +0100

    Add merge account reason to delete account modal

--- packages/components/containers/account/DeleteAccountModal.tsx
+++ packages/components/containers/account/DeleteAccountModal.tsx
@@ -7,8 +7,6 @@ import { signoutAction } from '@proton/account/authenticationService';
 import { useGetOrganization } from '@proton/account/organization/hooks';
 import { useUser } from '@proton/account/user/hooks';
 import { Button, Href } from '@proton/atoms';
-import Alert from '@proton/components/components/alert/Alert';
-import ErrorButton from '@proton/components/components/button/ErrorButton';
 import Form from '@proton/components/components/form/Form';
 import Checkbox from '@proton/components/components/input/Checkbox';
 import type { ModalProps } from '@proton/components/components/modalTwo/Modal';
@@ -33,12 +31,12 @@ import { canDelete, deleteUser, unlockPasswordChanges } from '@proton/shared/lib
 import { ACCOUNT_DELETION_REASONS, BRAND_NAME } from '@proton/shared/lib/constants';
 import { minLengthValidator, requiredValidator } from '@proton/shared/lib/helpers/formValidators';
 import { wait } from '@proton/shared/lib/helpers/promise';
-import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
 import { getOrganizationDenomination } from '@proton/shared/lib/organization/helper';
 import isTruthy from '@proton/utils/isTruthy';
 import noop from '@proton/utils/noop';
 
-const { DIFFERENT_ACCOUNT, TOO_EXPENSIVE, MISSING_FEATURE, USE_OTHER_SERVICE, OTHER } = ACCOUNT_DELETION_REASONS;
+const { DIFFERENT_ACCOUNT, TOO_EXPENSIVE, MISSING_FEATURE, USE_OTHER_SERVICE, MERGE_ACCOUNT, OTHER } =
+    ACCOUNT_DELETION_REASONS;
 
 interface Props extends Omit<ModalProps<'form'>, 'as'> {
     onSuccess?: () => Promise<void>;
@@ -46,25 +44,14 @@ interface Props extends Omit<ModalProps<'form'>, 'as'> {
     hideHeader?: boolean;
 }
 
-const SupportParagraph = () => {
-    {
-        /* translator: Full sentence: "This message won't reach the support team, if you have an issue with our service or need further action from our side please open a support ticket." */
-    }
-    const supportLink = (
-        <Href key="link-to-support" href="https://proton.me/support/contact-product">{c('Info')
-            .t`please open a support ticket`}</Href>
-    );
-
-    return (
-        <p className="text-sm color-weak mt-0">
-            {/* translator: Full sentence: "This message won't reach the support team, if you have an issue with our service or need further action from our side please open a support ticket." */}
-            {c('Info')
-                .jt`This message won't reach the support team, if you have an issue with our service or need further action from our side ${supportLink}.`}
-        </p>
-    );
-};
+enum STEP {
+    REASON_SELECTION,
+    CONFIRM_ACCOUNT_DELETION,
+}
 
 const DeleteAccountModal = (props: Props) => {
+    const [step, setStep] = useState<STEP>(STEP.REASON_SELECTION);
+
     const [authModalProps, setAuthModalOpen, renderAuthModal] = useModalState();
     const { createNotification } = useNotifications();
 
@@ -75,26 +62,21 @@ const DeleteAccountModal = (props: Props) => {
         await wait(2500);
     };
 
-    const {
-        onSuccess = defaultOnSuccess,
-        hideHeader = false,
-        onClose,
-        disableCloseOnEscape,
-        size = 'large',
-        ...rest
-    } = props;
+    const { onSuccess = defaultOnSuccess, hideHeader = false, onClose, disableCloseOnEscape, ...rest } = props;
     const eventManager = useEventManager();
     const api = useApi();
-    const [{ isAdmin }] = useUser();
+    const [{ isAdmin, Email }] = useUser();
     const getOrganization = useGetOrganization();
     const dispatch = useDispatch();
     const [loading, withLoading] = useLoading();
+
     const [model, setModel] = useState({
         check: false,
+        mergeUnderstanding: false,
         reason: '',
         feedback: '',
     });
-    const { validator, onFormSubmit } = useFormErrors();
+    const { validator, onFormSubmit, reset } = useFormErrors();
 
     const reasons = [
         <Option
@@ -112,6 +94,11 @@ const DeleteAccountModal = (props: Props) => {
             title={c('Option').t`I found another service that I like better`}
             value={USE_OTHER_SERVICE}
             key={USE_OTHER_SERVICE}
+        />,
+        <Option
+            title={c('Option').t`I'd like to merge this account into another one`}
+            value={MERGE_ACCOUNT}
+            key={MERGE_ACCOUNT}
         />,
         <Option title={c('Option').t`My reason isn't listed`} value={OTHER} key={OTHER} />,
     ].filter(isTruthy);
@@ -149,6 +136,140 @@ const DeleteAccountModal = (props: Props) => {
         }
     };
 
+    const { onSubmit, content, footer } = (() => {
+        if (step === STEP.REASON_SELECTION) {
+            const supportLink = (
+                <Href key="link-to-support" href="https://proton.me/support/contact-product">
+                    {
+                        // translator: Full sentence: "This message won't reach the support team, if you have an issue with our service or need further action from our side please open a support ticket."
+                        c('Info').t`please open a support ticket`
+                    }
+                </Href>
+            );
+
+            return {
+                onSubmit: (event: FormEvent<HTMLFormElement>) => {
+                    if (!onFormSubmit(event.currentTarget)) {
+                        return;
+                    }
+
+                    reset();
+                    setStep(STEP.CONFIRM_ACCOUNT_DELETION);
+                },
+                content: (
+                    <>
+                        <InputFieldTwo
+                            as={SelectTwo}
+                            label={c('Label').t`What is the main reason you are deleting your account?`}
+                            placeholder={c('Placeholder').t`Select a reason`}
+                            id="reason"
+                            autoFocus
+                            value={model.reason}
+                            onValue={(value: unknown) => setModel({ ...model, reason: value as string })}
+                            error={validator([requiredValidator(model.reason)])}
+                            disabled={loading}
+                        >
+                            {reasons}
+                        </InputFieldTwo>
+
+                        <InputFieldTwo
+                            id="feedback"
+                            as={TextAreaTwo}
+                            rows={3}
+                            label={c('Label').t`To help us improve, please explain why you are leaving.`}
+                            placeholder={c('Placeholder').t`Feedback`}
+                            value={model.feedback}
+                            onValue={(value: string) => setModel({ ...model, feedback: value })}
+                            error={validator([
+                                requiredValidator(model.feedback),
+                                minLengthValidator(model.feedback, 10),
+                            ])}
+                            disabled={loading}
+                        />
+
+                        <p className="text-sm color-weak mt-0">
+                            {
+                                // translator: Full sentence: "This message won't reach the support team, if you have an issue with our service or need further action from our side please open a support ticket."
+                                c('Info')
+                                    .jt`This message won't reach the support team, if you have an issue with our service or need further action from our side ${supportLink}.`
+                            }
+                        </p>
+                    </>
+                ),
+                footer: (
+                    <>
+                        <Button onClick={onClose} disabled={loading}>{c('Action').t`Cancel`}</Button>
+                        <Button color="norm" loading={loading} type="submit">
+                            {c('Action').t`Continue`}
+                        </Button>
+                    </>
+                ),
+            };
+        }
+
+        if (step === STEP.CONFIRM_ACCOUNT_DELETION) {
+            return {
+                onSubmit: (event: FormEvent<HTMLFormElement>) => {
+                    if (!onFormSubmit(event.currentTarget)) {
+                        return;
+                    }
+                    setAuthModalOpen(true);
+                },
+                content: (
+                    <>
+                        <div className="text-bold">{c('Info').t`Warning: deleting your account is permanent.`}</div>
+
+                        <div>
+                            {c('Info')
+                                .t`This will remove your access to all connected services and delete all of your data, including contacts, attachments, events, passwords, and files.`}
+                        </div>
+
+                        <InputFieldTwo
+                            className="mt-4"
+                            id="check"
+                            as={Checkbox}
+                            error={validator([!model.check ? requiredValidator(undefined) : ''])}
+                            checked={model.check}
+                            onChange={({ target }: ChangeEvent<HTMLInputElement>) =>
+                                setModel({ ...model, check: target.checked })
+                            }
+                            disabled={loading}
+                        >
+                            {c('Label').t`Yes, I want to permanently delete this account and all its data.`}
+                        </InputFieldTwo>
+
+                        {model.reason === MERGE_ACCOUNT && (
+                            <InputFieldTwo
+                                id="check"
+                                as={Checkbox}
+                                error={validator([!model.mergeUnderstanding ? requiredValidator(undefined) : ''])}
+                                checked={model.mergeUnderstanding}
+                                onChange={({ target }: ChangeEvent<HTMLInputElement>) =>
+                                    setModel({ ...model, mergeUnderstanding: target.checked })
+                                }
+                                disabled={loading}
+                            >
+                                {c('Label')
+                                    .t`I understand that I'll need to add this email address to the other account immediately, to avoid losing this address permanently.`}
+                            </InputFieldTwo>
+                        )}
+                    </>
+                ),
+                footer: (
+                    <>
+                        <Button onClick={() => setStep(STEP.REASON_SELECTION)} disabled={loading}>{c('Action')
+                            .t`Back`}</Button>
+                        <Button color="danger" loading={loading} type="submit">
+                            {c('Action').t`Delete account`}
+                        </Button>
+                    </>
+                ),
+            };
+        }
+
+        throw Error(`Invalid step`);
+    })();
+
     return (
         <>
             {renderAuthModal && (
@@ -158,7 +279,7 @@ const DeleteAccountModal = (props: Props) => {
                     {...authModalProps}
                     onCancel={undefined}
                     onSuccess={() => {
-                        withLoading(handleSubmit());
+                        void withLoading(handleSubmit());
                     }}
                 />
             )}
@@ -166,81 +287,13 @@ const DeleteAccountModal = (props: Props) => {
                 as={Form}
                 onClose={hideHeader ? undefined : onClose}
                 disableCloseOnEscape={disableCloseOnEscape || loading}
-                size={size}
-                onSubmit={
-                    loading
-                        ? noop
-                        : (event: FormEvent<HTMLFormElement>) => {
-                              if (!onFormSubmit(event.currentTarget)) {
-                                  return;
-                              }
-                              setAuthModalOpen(true);
-                          }
-                }
+                size="medium"
+                onSubmit={loading ? noop : onSubmit}
                 {...rest}
             >
-                {!hideHeader && <ModalTwoHeader title={c('Title').t`Delete account`} />}
-                <ModalTwoContent>
-                    <Alert className="mb-4" type="warning">
-                        <div className="text-bold text-uppercase">
-                            {c('Info')
-                                .t`Warning: deletion is permanent. This also removes access to all connected services and deletes all of your contacts.`}
-                        </div>
-                        <div>
-                            {c('Info').t`If you wish to combine this account with another one, do NOT delete it.`}
-                        </div>
-                        <div>
-                            <Href href={getKnowledgeBaseUrl('/combine-accounts')}>{c('Link').t`Learn more`}</Href>
-                        </div>
-                    </Alert>
-                    <InputFieldTwo
-                        as={SelectTwo}
-                        label={c('Label').t`What is the main reason you are deleting your account?`}
-                        placeholder={c('Placeholder').t`Select a reason`}
-                        id="reason"
-                        autoFocus
-                        value={model.reason}
-                        onValue={(value: unknown) => setModel({ ...model, reason: value as string })}
-                        error={validator([requiredValidator(model.reason)])}
-                        disabled={loading}
-                    >
-                        {reasons}
-                    </InputFieldTwo>
-
-                    <InputFieldTwo
-                        id="feedback"
-                        as={TextAreaTwo}
-                        rows={3}
-                        label={c('Label')
-                            .t`We are sorry to see you go. Please explain why you are leaving to help us improve.`}
-                        placeholder={c('Placeholder').t`Feedback`}
-                        value={model.feedback}
-                        onValue={(value: string) => setModel({ ...model, feedback: value })}
-                        error={validator([requiredValidator(model.feedback), minLengthValidator(model.feedback, 10)])}
-                        disabled={loading}
-                    />
-
-                    <InputFieldTwo
-                        id="check"
-                        as={Checkbox}
-                        error={validator([!model.check ? requiredValidator(undefined) : ''])}
-                        checked={model.check}
-                        onChange={({ target }: ChangeEvent<HTMLInputElement>) =>
-                            setModel({ ...model, check: target.checked })
-                        }
-                        disabled={loading}
-                    >
-                        {c('Label').t`Yes, I want to permanently delete this account and all its data.`}
-                    </InputFieldTwo>
-
-                    <SupportParagraph />
-                </ModalTwoContent>
-                <ModalTwoFooter>
-                    <Button onClick={onClose} disabled={loading}>{c('Action').t`Cancel`}</Button>
-                    <ErrorButton loading={loading} type="submit">
-                        {c('Action').t`Delete`}
-                    </ErrorButton>
-                </ModalTwoFooter>
+                {!hideHeader && <ModalTwoHeader title={c('Title').t`Delete account`} subline={Email} />}
+                <ModalTwoContent>{content}</ModalTwoContent>
+                <ModalTwoFooter>{footer}</ModalTwoFooter>
             </ModalTwo>
         </>
     );
