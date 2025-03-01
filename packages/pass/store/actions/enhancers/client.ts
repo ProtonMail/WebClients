89b0fe7e9e47b91d2e6a3a4b9f2eaba292889dab
Edvin CANDON <edvin.candon@proton.ch>
2025-03-01 13:53:19 +0100

    Add streamable action enhancer

--- packages/pass/store/actions/enhancers/client.ts
+++ packages/pass/store/actions/enhancers/client.ts
@@ -3,9 +3,15 @@ import type { Action } from 'redux';
 import { type WithMeta, withMetaFactory } from './meta';
 
 export type SynchronousActionMeta = { sync: true };
-export type WithSynchronousAction<A = Action> = WithMeta<SynchronousActionMeta, A>;
+export type SynchronousAction<A = Action> = WithMeta<SynchronousActionMeta, A>;
 
 export const withSynchronousAction = withMetaFactory<SynchronousActionMeta>({ sync: true });
-
-export const isSynchronousAction = <T extends Action>(action?: T): action is WithSynchronousAction<T> =>
+export const isSynchronousAction = <T extends Action>(action?: T): action is SynchronousAction<T> =>
     (action as any)?.meta?.sync === true;
+
+export type StreamableActionMeta = { stream: true };
+export type StreamableAction<A = Action> = WithMeta<SynchronousActionMeta, A>;
+
+export const withStreamableAction = withMetaFactory<StreamableActionMeta>({ stream: true });
+export const isStreamableAction = <T extends Action>(action?: T): action is StreamableAction<T> =>
+    (action as any)?.meta?.stream === true;
