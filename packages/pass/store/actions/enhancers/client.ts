1849c9053da9dd3672c8e95deb652b450b869c6a
Edvin CANDON <edvin.candon@proton.ch>
2025-03-03 09:57:03 +0100

    Avoid broadcasting background-only actions

--- packages/pass/store/actions/enhancers/client.ts
+++ packages/pass/store/actions/enhancers/client.ts
@@ -15,3 +15,10 @@ export type StreamableAction<A = Action> = WithMeta<SynchronousActionMeta, A>;
 export const withStreamableAction = withMetaFactory<StreamableActionMeta>({ stream: true });
 export const isStreamableAction = <T extends Action>(action?: T): action is StreamableAction<T> =>
     (action as any)?.meta?.stream === true;
+
+export type BackgroundActionMeta = { background: true };
+export type BackgroundAction<A = Action> = WithMeta<SynchronousActionMeta, A>;
+
+export const withBackgroundAction = withMetaFactory<BackgroundActionMeta>({ background: true });
+export const isBackgroundAction = <T extends Action>(action?: T): action is BackgroundAction<T> =>
+    (action as any)?.meta?.background === true;
