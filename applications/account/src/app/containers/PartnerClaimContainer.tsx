fff0755f137b740fc3d8bc3ccd29f51b1824f3ad
Pedro Amaro <pedro.lavarinhas.amaro@proton.ch>
2025-03-06 16:17:32 +0100

    Async verify the domain after claiming it

--- applications/account/src/app/containers/PartnerClaimContainer.tsx
+++ applications/account/src/app/containers/PartnerClaimContainer.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 
 import { addressesThunk } from '@proton/account/addresses';
+import { domainsThunk } from '@proton/account/domains';
+import { syncDomain } from '@proton/account/domains/actions';
 import { convertExternalAddress } from '@proton/account/organizationKey/convertAddresses';
 import useAuthentication from '@proton/components/hooks/useAuthentication';
 import { StandardLoadErrorPage, useApi, useErrorHandler, useKTVerifier } from '@proton/components/index';
@@ -9,6 +11,7 @@ import { getSilentApi } from '@proton/shared/lib/api/helpers/customConfig';
 import { claimOrphanDomain } from '@proton/shared/lib/api/partner';
 import { getAppHref } from '@proton/shared/lib/apps/helper';
 import { ADDRESS_TYPE, APPS } from '@proton/shared/lib/constants';
+import { getEmailParts } from '@proton/shared/lib/helpers/email';
 import { getNonEmptyErrorMessage } from '@proton/shared/lib/helpers/error';
 import noop from '@proton/utils/noop';
 
@@ -37,6 +40,20 @@ const PartnerClaimContainer = () => {
             if (!address) {
                 throw new Error('Address not found');
             }
+
+            // re-verify the domain async
+            // sync domain not only returns the domain; but does a DNS query to check if DNS records are ok
+            const reverifyDomain = async () => {
+                const domains = await dispatch(domainsThunk());
+                const [, domainPart] = getEmailParts(address.Email);
+                const domain = domains.find((domain) => domain.DomainName === domainPart);
+                if (domain) {
+                    await dispatch(syncDomain(domain));
+                }
+            };
+
+            reverifyDomain().catch(noop);
+
             const { keyTransparencyVerify, keyTransparencyCommit } = await createKtVerifier();
             if (address.Type === ADDRESS_TYPE.TYPE_EXTERNAL) {
                 await dispatch(
