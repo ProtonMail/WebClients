10e011a9fc326691508d05b995b7064c973b8732
Dominik Jankovic <dominik.jankovic@proton.ch>
2025-03-06 14:32:49 +0000

    Account for shared menu item icons in MenuHamburger

--- packages/pass/components/Menu/Vault/utils.tsx
+++ packages/pass/components/Menu/Vault/utils.tsx
@@ -1,25 +1,28 @@
 import { c } from 'ttag';
 
+import { type IconName } from '@proton/icons';
+import { VAULT_ICON_MAP } from '@proton/pass/components/Vault/constants';
 import type { VaultShareItem } from '@proton/pass/store/reducers';
 import type { MaybeNull } from '@proton/pass/types';
 import { VaultColor as VaultColorEnum } from '@proton/pass/types/protobuf/vault-v1';
 
-import { type VaultIconName } from '../../Vault/VaultIcon';
-
 export type VaultMenuOption = {
     id: MaybeNull<string>;
     label: string;
-    color?: VaultColorEnum;
-    icon?: VaultIconName;
+    color: VaultColorEnum;
+    icon: IconName;
 };
 
-export const getVaultOptionInfo = (vault: 'all' | 'trash' | VaultShareItem): VaultMenuOption => {
+export const getVaultOptionInfo = (
+    vault: 'all' | 'trash' | 'shared-with-me' | 'shared-by-me' | 'secure-links' | VaultShareItem
+): VaultMenuOption => {
     switch (vault) {
         case 'all':
             return {
                 id: null,
                 label: c('Label').t`All items`,
                 color: VaultColorEnum.COLOR_CUSTOM,
+                icon: 'pass-all-vaults',
             };
         case 'trash':
             return {
@@ -28,12 +31,33 @@ export const getVaultOptionInfo = (vault: 'all' | 'trash' | VaultShareItem): Vau
                 icon: 'pass-trash',
                 color: VaultColorEnum.COLOR_UNSPECIFIED,
             };
+        case 'secure-links':
+            return {
+                id: null,
+                label: c('Label').t`Secure links`,
+                icon: 'link',
+                color: VaultColorEnum.COLOR_CUSTOM,
+            };
+        case 'shared-by-me':
+            return {
+                id: null,
+                label: c('Label').t`Shared by me`,
+                icon: 'user-arrow-right',
+                color: VaultColorEnum.COLOR_CUSTOM,
+            };
+        case 'shared-with-me':
+            return {
+                id: null,
+                label: c('Label').t`Shared with me`,
+                icon: 'user-arrow-left',
+                color: VaultColorEnum.COLOR_CUSTOM,
+            };
         default:
             return {
                 id: vault.shareId,
                 label: vault.content.name,
-                color: vault.content.display.color,
-                icon: vault.content.display.icon,
+                color: vault.content.display.color ?? VaultColorEnum.COLOR1,
+                icon: vault.content.display.icon ? VAULT_ICON_MAP[vault.content.display.icon] : 'pass-all-vaults',
             };
     }
 };
