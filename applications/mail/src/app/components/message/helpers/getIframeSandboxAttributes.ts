dce184096824cac146f1a3cb8bd9e569f9ee1857
Germain <germain.souquet@proton.ch>
2025-03-06 08:58:43 +0000

    Clarify allow-scripts usage on iframe sandbox

--- applications/mail/src/app/components/message/helpers/getIframeSandboxAttributes.ts
+++ applications/mail/src/app/components/message/helpers/getIframeSandboxAttributes.ts
@@ -1,33 +1,45 @@
 import { isDuckDuckGo, isSafari } from '@proton/shared/lib/helpers/browser';
 
 const getIframeSandboxAttributes = (isPrint: boolean) => {
+    const sandboxAttributes: string = [
+        /**
+         * Because iframe origin is set to protonmail.com we need
+         * to be able to access it's origin
+         */
+        'allow-same-origin',
+        /**
+         * allows target="_blank" links to work
+         */
+        'allow-popups',
+        /**
+         * Allows to open links in non sandboxed mode
+         * for ex : if allow-scripts is not present opened links are allowed to execute scripts
+         */
+        'allow-popups-to-escape-sandbox',
+        /**
+         * Modals are a requirement when printing
+         */
+        ...(isPrint ? ['allow-modals'] : []),
+        /**
+         * Allows react portals to execute correctly in Safari
+         * WebKit is smart enought to allow parent script execution over
+         * sandboxed child frames with the same origin.
+         * This works, even though an error is shown in the console. It allows us
+         * to benefit from an extra protection about potential child frame to
+         * parent frame script execution
+         * It is not reported in Sentry and does not impede image loading.
+         * If this becomes a problem in the future we should consider `allow-scripts`
+         * on all engines
+         */
+        ...(isSafari() || isDuckDuckGo() ? ['allow-scripts'] : []),
+    ].join(' ');
+
     /**
-     * "allow-same-origin"
-     *      Because iframe origin is set to protonmail.com we need
-     *      to be able to access it's origin
-     * "allow-modals"
-     *      Allow to be able to print emails
-     * "allow-scripts"
-     *      allows react portals to execute correctly in Safari
-     * "allow-popups"
-     *      allows target="_blank" links to work
-     * "allow-popups-to-escape-sandbox"
-     *      allows to open links in non sandboxed mode
-     *      for ex : if allow-scripts is not present opened links are allowed to execute scripts
-     *
-     * ATM we do not need to allow the following options
+     * The following options do not currently need to be allowed
      * - allow-forms
      * - allow-pointer-lock
      * - allow-top-navigation
      */
-    const sandboxAttributes: string = [
-        'allow-same-origin',
-        'allow-popups',
-        'allow-popups-to-escape-sandbox',
-        ...(isPrint ? ['allow-modals'] : []),
-        ...(isSafari() || isDuckDuckGo() ? ['allow-scripts'] : []),
-    ].join(' ');
-
     return sandboxAttributes;
 };
 
