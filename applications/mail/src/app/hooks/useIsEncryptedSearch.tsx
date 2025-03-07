4c00f96950121be9e209808f26871d3c1c0be586
romain sanchez <romain.sanchez@proton.ch>
2025-03-07 09:58:57 +0100

    Use search value from state

--- applications/mail/src/app/hooks/useIsEncryptedSearch.tsx
+++ applications/mail/src/app/hooks/useIsEncryptedSearch.tsx
@@ -1,16 +1,11 @@
-import { useLocation } from 'react-router-dom';
-
-import type { SearchParameters } from '@proton/shared/lib/mail/search';
-
 import { useEncryptedSearchContext } from 'proton-mail/containers/EncryptedSearchProvider';
 import { isSearch } from 'proton-mail/helpers/elements';
-import { extractSearchParameters } from 'proton-mail/helpers/mailboxUrl';
-import { useDeepMemo } from 'proton-mail/hooks/useDeepMemo';
+import { paramsSelector } from 'proton-mail/store/elements/elementsSelectors';
+import { useMailSelector } from 'proton-mail/store/hooks';
 
 const useIsEncryptedSearch = () => {
-    const location = useLocation();
+    const { search } = useMailSelector(paramsSelector);
     const { esStatus } = useEncryptedSearchContext();
-    const search = useDeepMemo<SearchParameters>(() => extractSearchParameters(location), [location]);
     return isSearch(search) && esStatus.esEnabled;
 };
 
