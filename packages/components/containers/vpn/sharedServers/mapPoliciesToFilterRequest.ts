a774bdece0351b451fe9b9b99a1817e3599f0e5b
Juan Francisco Granados <juan-francisco.granados@proton.ch>
2025-03-10 15:36:10 +0000

    VPNBE-1157 Change submission logic

--- packages/components/containers/vpn/sharedServers/mapPoliciesToFilterRequest.ts
+++ packages/components/containers/vpn/sharedServers/mapPoliciesToFilterRequest.ts
@@ -2,25 +2,20 @@ import type { FilterPolicyRequest } from '@proton/components/containers/vpn/shar
 import { PolicyState, PolicyType } from '@proton/components/containers/vpn/sharedServers/constants';
 import type { VpnLocationFilterPolicy } from '@proton/components/containers/vpn/sharedServers/useSharedServers';
 
-export const mapPoliciesToFilterRequest = (
-    policies: VpnLocationFilterPolicy[],
-    isEditing: boolean,
-    policy?: VpnLocationFilterPolicy,
-    updatedPolicy?: FilterPolicyRequest
-): FilterPolicyRequest[] => {
-    return policies.flatMap((p) => {
-        // If editing and the policy matches, return the updated policy
-        if (isEditing && policy && p.LocationFilterPolicyID === policy.LocationFilterPolicyID) {
-            return updatedPolicy ? [updatedPolicy] : [];
-        } else if (policy && p.LocationFilterPolicyID === policy.LocationFilterPolicyID) {
-            return [];
-        }
+export const mapPoliciesToFilterRequest = (policies: VpnLocationFilterPolicy[]): FilterPolicyRequest[] => {
+    const isRemovingLastCustomPolicy = policies.filter((policy) => policy.Type === PolicyType.Custom).length === 0;
+    const isOnPolicyActive =
+        policies.filter((policy) => policy.Type === PolicyType.All && policy.State === PolicyState.Active).length === 1;
 
+    return policies.flatMap((p) => {
         return {
             ID: p.LocationFilterPolicyID,
             Name: p.Name,
             Type: p.Type,
-            State: p.Type === PolicyType.Custom ? p.State : PolicyState.Inactive,
+            State:
+                p.Type === PolicyType.None && isRemovingLastCustomPolicy && !isOnPolicyActive
+                    ? PolicyState.Active
+                    : p.State,
             Locations: p.Locations.map((loc) => ({ Country: loc.Country, City: loc.City })),
             UserIds: p.Users.length ? p.Users.map((user) => user.UserID) : null,
             GroupIds: p.Groups.length ? p.Groups.map((g) => g.GroupID) : null,
