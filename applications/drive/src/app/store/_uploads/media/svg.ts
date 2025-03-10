4f911646a59cace731f575199c99644ad9bb8489
Redacted <redacted@invalid.proton.ch>
2025-03-10 15:53:28 +0000

    [DRVWEB-4579] HEIC Support for Thumbnails + Thumbnail generation RFC compliance

--- applications/drive/src/app/store/_uploads/media/svg.ts
+++ applications/drive/src/app/store/_uploads/media/svg.ts
@@ -1,4 +1,4 @@
-import { SupportedMimeTypes, THUMBNAIL_MAX_SIDE } from '@proton/shared/lib/drive/constants';
+import { THUMBNAIL_MAX_SIDE } from '@proton/shared/lib/drive/constants';
 import { isFirefox } from '@proton/shared/lib/helpers/browser';
 import { parseStringToDOM } from '@proton/shared/lib/helpers/dom';
 
@@ -38,5 +38,5 @@ export async function scaleSvgFile(file: Blob) {
         fileToScale = await setSvgSize(file, THUMBNAIL_MAX_SIDE);
     }
 
-    return scaleImageFile({ file: fileToScale, mimeType: SupportedMimeTypes.svg });
+    return scaleImageFile({ file: fileToScale });
 }
