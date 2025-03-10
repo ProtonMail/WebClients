1271f0e384a688a95c7c0b22f7095306cbf2abed
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2025-03-10 11:04:29 +0100

    refactor: Change calendarSignatureContext to function

---
+++ packages/shared/lib/calendar/crypto/helpers.ts
@@ -0,0 +1,15 @@
+type SignatureContext = 'calendar.sharing.invite' | 'calendar.rsvp.comment';
+
+export function getSignatureContext(context: 'calendar.sharing.invite'): 'calendar.sharing.invite';
+export function getSignatureContext<TEventID extends string>(
+    context: 'calendar.rsvp.comment',
+    eventID: TEventID
+): `calendar.rsvp.comment.${TEventID}`;
+export function getSignatureContext(context: SignatureContext, eventID?: string) {
+    switch (context) {
+        case 'calendar.sharing.invite':
+            return 'calendar.sharing.invite';
+        case 'calendar.rsvp.comment':
+            return `calendar.rsvp.comment.${eventID}`;
+    }
+}
