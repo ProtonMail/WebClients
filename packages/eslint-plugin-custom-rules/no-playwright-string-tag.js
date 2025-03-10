f756e977c053f1feac8b00161e7d4d6fb8f71309
Paolo Chiabrera <paolo.chiabrera@proton.ch>
2025-03-10 13:02:09 +0000

    [MAILWEB-6404] Centralize test tags in one place for calendar

---
+++ packages/eslint-plugin-custom-rules/no-playwright-string-tag.js
@@ -0,0 +1,73 @@
+/* eslint-env es6 */
+const isObjectTagField = (node) => {
+    return node && node.type === 'Property' && node.key && node.key.type === 'Identifier' && node.key.name === 'tag';
+};
+
+const isTestFunctionOptionsObject = (node) => {
+    const isObject = node && node.type === 'ObjectExpression' && node.parent;
+
+    if (!isObject) {
+        return false;
+    }
+
+    const isFunctionWithOptionsObject = node.parent.type === 'CallExpression' && node.parent.callee;
+
+    if (isFunctionWithOptionsObject) {
+        if (node.parent.callee.name === 'test') {
+            return true;
+        }
+
+        if (node.parent.callee.type === 'MemberExpression' && node.parent.callee.object) {
+            const { object, name } = node.parent.callee.object;
+
+            if (name) {
+                return name === 'test';
+            }
+
+            return object && object.name === 'test';
+        }
+    }
+
+    return false;
+};
+
+const reportIssue = (context, node) => {
+    context.report({
+        node: node,
+        message: 'Test tags cannot be strings, please use the TEST_TAGS enum constant instead.',
+    });
+};
+
+module.exports = {
+    meta: {
+        type: 'suggestion',
+        docs: {
+            description: 'Try to use the TEST_TAGS enum constant.',
+        },
+    },
+    create: (context) => {
+        return {
+            Literal(node) {
+                const { parent, value } = node;
+
+                if (!parent || !value || !value.split) {
+                    return;
+                }
+
+                if (value.trim().startsWith('@')) {
+                    if (parent.type === 'ArrayExpression') {
+                        if (isObjectTagField(parent.parent)) {
+                            if (isTestFunctionOptionsObject(parent.parent.parent)) {
+                                reportIssue(context, node);
+                            }
+                        }
+                    } else if (isObjectTagField(parent)) {
+                        if (isTestFunctionOptionsObject(parent.parent)) {
+                            reportIssue(context, node);
+                        }
+                    }
+                }
+            },
+        };
+    },
+};
