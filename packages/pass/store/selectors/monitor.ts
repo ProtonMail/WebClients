ede33bbfef8b39707cf922129e985cd11241358f
Edvin CANDON <edvin.candon@proton.ch>
2025-03-11 09:57:45 +0100

    Simplify owned vaults filtering in Pass Monitor reporting
    [IDTEAM-4211]

--- packages/pass/store/selectors/monitor.ts
+++ packages/pass/store/selectors/monitor.ts
@@ -1,16 +1,16 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-import { isActiveMonitored, isExcluded, isItemType } from '@proton/pass/lib/items/item.predicates';
+import { belongsToShares, isActiveMonitored, isExcluded } from '@proton/pass/lib/items/item.predicates';
 import { intoSelectedItem } from '@proton/pass/lib/items/item.utils';
 import { getDuplicatePasswords } from '@proton/pass/lib/monitor/monitor.utils';
 import type { MonitorDomain } from '@proton/pass/lib/monitor/types';
 import { AddressType } from '@proton/pass/lib/monitor/types';
 import type { State } from '@proton/pass/store/types';
-import type { Maybe } from '@proton/pass/types';
+import type { Maybe, ShareId } from '@proton/pass/types';
 import { first } from '@proton/pass/utils/array/first';
+import { and } from '@proton/pass/utils/fp/predicates';
 
-import { selectItems, selectLoginItems } from './items';
-import { selectOwnedVaults } from './shares';
+import { selectLoginItems } from './items';
 
 export const selectMonitorState = (state: State) => state.monitor;
 export const selectMonitorSettings = ({ user }: State) => user.monitor;
@@ -20,24 +20,16 @@ export const selectCustomBreaches = (state: State) => state.monitor?.custom;
 export const selectProtonBreaches = (state: State) => state.monitor?.proton;
 export const selectMonitorPreview = (state: State): Maybe<MonitorDomain> => first(state.monitor?.preview ?? []);
 
-const selectOwnLoginItems = createSelector([selectOwnedVaults, selectItems], (ownedVaults, itemsByShareId) => {
-    const ownItems = ownedVaults.flatMap(({ shareId }) => Object.values(itemsByShareId[shareId]));
-    const ownLogins = ownItems.filter(isItemType('login'));
-    return ownLogins;
-});
+export const selectMonitoredLogins = (shareIds?: ShareId[]) =>
+    createSelector(selectLoginItems, (items) => items.filter(and(isActiveMonitored, belongsToShares(shareIds))));
 
-export const selectMonitoredLogins = createSelector(selectLoginItems, (items) => items.filter(isActiveMonitored));
-export const selectOwnMonitoredLogins = createSelector(selectOwnLoginItems, (items) => items.filter(isActiveMonitored));
+export const selectDuplicatePasswords = (shareIds?: ShareId[]) =>
+    createSelector(selectMonitoredLogins(shareIds), getDuplicatePasswords);
 
-export const selectDuplicatePasswords = createSelector(selectMonitoredLogins, getDuplicatePasswords);
-export const selectOwnDuplicatePasswords = createSelector(selectOwnMonitoredLogins, getDuplicatePasswords);
-
-export const selectExcludedItems = createSelector(selectLoginItems, (items) =>
-    items.filter(isExcluded).map(intoSelectedItem)
-);
-export const selectOwnExcludedItems = createSelector(selectOwnLoginItems, (items) =>
-    items.filter(isExcluded).map(intoSelectedItem)
-);
+export const selectExcludedItems = (shareIds?: ShareId[]) =>
+    createSelector(selectLoginItems, (items) =>
+        items.filter(and(isExcluded, belongsToShares(shareIds))).map(intoSelectedItem)
+    );
 
 export const selectMonitorSettingByType = (type: AddressType) =>
     createSelector(selectMonitorSettings, (settings) => {
