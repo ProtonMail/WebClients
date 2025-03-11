abdbcb15e5f3c785bb8887f338baff7d90b38719
Edvin CANDON <edvin.candon@proton.ch>
2025-03-11 11:47:27 +0100

    Only show `ContactAdminWarning` when B2B policy is enforced
    [IDTEAM-3651]

--- packages/pass/components/Item/List/Placeholder/ContactAdminWarning.tsx
+++ packages/pass/components/Item/List/Placeholder/ContactAdminWarning.tsx
@@ -7,7 +7,7 @@ import { Card } from '@proton/pass/components/Layout/Card/Card';
 
 export const ContactAdminWarning: FC = () => {
     return (
-        <Card className="h5 flex items-center flex-nowrap w-full gap-3" type="primary">
+        <Card className="flex items-center flex-nowrap w-full gap-3" type="primary">
             <Icon name="info-circle-filled" size={5} className="shrink-0 mt-0.5" />
             <span className="text-left">{c('Info').t`Please contact your administrator`}</span>
         </Card>
