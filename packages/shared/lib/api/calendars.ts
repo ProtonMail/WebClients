ecd7988ec85093304e467a4f2c87c2b727ad1780
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2025-03-12 15:57:44 +0100

    feat: Add attendees infos pagination

--- packages/shared/lib/api/calendars.ts
+++ packages/shared/lib/api/calendars.ts
@@ -241,6 +241,12 @@ export const updatePersonalEventPart = (calendarID: string, eventID: string, dat
     data,
 });
 
+export const getPaginatedAttendeesInfo = (calendarID: string, eventID: string, Page = 0) => ({
+    url: `${CALENDAR_V1}/${calendarID}/events/${eventID}/attendees`,
+    method: 'get',
+    params: { Page },
+});
+
 export const updateAttendeePartstat = (
     calendarID: string,
     eventID: string,
