81568462417fc2920d40d9b5cd1b49162beb6a77
Coral Avraham <coral.avraham@proton.ch>
2025-03-13 11:37:35 +0200

    View user's note and edit note

---
+++ packages/components/containers/calendar/RsvpSection/RsvpSectionButtons.tsx
@@ -0,0 +1,41 @@
+import { c } from 'ttag';
+
+import { ButtonLike } from '@proton/atoms/index';
+import Icon from '@proton/components/components/icon/Icon';
+import Tooltip from '@proton/components/components/tooltip/Tooltip';
+import noop from '@proton/utils/noop';
+
+interface EditButtonProps {
+    onEdit: () => void;
+}
+
+export const EditNoteButton = ({ onEdit }: EditButtonProps) => {
+    return (
+        <Tooltip title={c('Edit note button tooltip').t`Edit Note`}>
+            <ButtonLike className="shrink-0" shape="ghost" onClick={onEdit} icon size="small">
+                <Icon name="pen" alt={c('Edit note button tooltip').t`Edit note`} />
+            </ButtonLike>
+        </Tooltip>
+    );
+};
+
+interface DeleteButtonProps {
+    onDelete: () => void;
+    loading: boolean;
+}
+export const DeleteNoteButton = ({ loading, onDelete }: DeleteButtonProps) => {
+    return (
+        <Tooltip title={c('Remove note button tooltip').t`Remove note`}>
+            <ButtonLike
+                className="shrink-0"
+                shape="ghost"
+                onClick={loading ? noop : onDelete}
+                loading={loading}
+                icon
+                size="small"
+            >
+                <Icon name="cross-big" alt={c('Remove note button tooltip').t`Remove note`} />
+            </ButtonLike>
+        </Tooltip>
+    );
+};
