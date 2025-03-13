bfdf0f3a1f0067227f1981807b7d75031c51ce51
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2025-03-13 14:40:05 +0000

    Add EdmOptOut settings section

--- packages/components/containers/recovery/AccountRecoverySection.tsx
+++ packages/components/containers/recovery/AccountRecoverySection.tsx
@@ -1,6 +1,7 @@
 import { c } from 'ttag';
 
 import { useUserSettings } from '@proton/account/userSettings/hooks';
+import Info from '@proton/components/components/link/Info';
 import Loader from '@proton/components/components/loader/Loader';
 import { useModalTwoPromise } from '@proton/components/components/modalTwo/useModalTwo';
 import Toggle from '@proton/components/components/toggle/Toggle';
@@ -10,11 +11,14 @@ import SettingsLayoutRight from '@proton/components/containers/account/SettingsL
 import SettingsSection from '@proton/components/containers/account/SettingsSection';
 import AuthModal from '@proton/components/containers/password/AuthModal';
 import type { AuthModalResult } from '@proton/components/containers/password/interface';
+import useApi from '@proton/components/hooks/useApi';
 import useEventManager from '@proton/components/hooks/useEventManager';
 import useMyCountry from '@proton/components/hooks/useMyCountry';
 import useNotifications from '@proton/components/hooks/useNotifications';
 import { useLoading } from '@proton/hooks';
 import { updateResetEmail, updateResetPhone } from '@proton/shared/lib/api/settings';
+import { updateFlags } from '@proton/shared/lib/api/settings';
+import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
 import noop from '@proton/utils/noop';
 
 import RecoveryEmail from './email/RecoveryEmail';
@@ -24,10 +28,12 @@ export const AccountRecoverySection = ({ divider = true }: { divider?: boolean }
     const [userSettings, loadingUserSettings] = useUserSettings();
     const [loadingEmailReset, withLoadingEmailReset] = useLoading();
     const [loadingPhoneReset, withLoadingPhoneReset] = useLoading();
+    const [loadingEDM, withLoadingEDM] = useLoading();
     const { createNotification } = useNotifications();
     const { call } = useEventManager();
     const defaultCountry = useMyCountry();
     const [authModal, showAuthModal] = useModalTwoPromise<{ config: any }, AuthModalResult>();
+    const api = useApi();
 
     if (loadingUserSettings || !userSettings) {
         return <Loader />;
@@ -52,6 +58,15 @@ export const AccountRecoverySection = ({ divider = true }: { divider?: boolean }
         await call();
     };
 
+    const handleEDMToggle = async (value: number) => {
+        await api(updateFlags({ EdmOptOut: value }));
+        await call();
+        createNotification({
+            type: 'info',
+            text: value ? c('Info').t`QR code sign-in disabled` : c('Info').t`QR code sign-in enabled`,
+        });
+    };
+
     return (
         <>
             {authModal((props) => {
@@ -66,6 +81,36 @@ export const AccountRecoverySection = ({ divider = true }: { divider?: boolean }
                 );
             })}
             <SettingsSection>
+                <SettingsLayout>
+                    <SettingsLayoutLeft>
+                        <label className="text-semibold">
+                            <span className="mr-2">{c('Label').t`Sign in with QR code`}</span>
+                        </label>
+                    </SettingsLayoutLeft>
+                    <SettingsLayoutRight isToggleContainer>
+                        <div className="flex items-center gap-2">
+                            <Toggle
+                                loading={loadingEDM}
+                                checked={!userSettings?.Flags.EdmOptOut}
+                                id="edmToggle"
+                                onChange={({ target: { checked } }) =>
+                                    withLoadingEDM(handleEDMToggle(+!checked).catch(noop))
+                                }
+                            />
+                            <label htmlFor="edmToggle" className="flex-1">
+                                {c('Label').t`Allow scanning a QR code to sign in`}
+                                <Info
+                                    url={getKnowledgeBaseUrl('/')}
+                                    title={c('Info').t`Scan QR code on your mobile device to sign in`}
+                                    className="ml-1"
+                                />
+                            </label>
+                        </div>
+                    </SettingsLayoutRight>
+                </SettingsLayout>
+
+                <hr className="my-8" />
+
                 <SettingsLayout>
                     <SettingsLayoutLeft>
                         <label className="pt-0 mb-2 md:mb-0 text-semibold" htmlFor="recovery-email-input">
--- packages/shared/lib/api/settings.ts
+++ packages/shared/lib/api/settings.ts
@@ -232,7 +232,7 @@ export const updateEarlyAccess = (data: { EarlyAccess: number }) => ({
     data,
 });
 
-export const updateFlags = (data: { Welcomed: number }) => ({
+export const updateFlags = (data: { Welcomed?: number; EdmOptOut?: number }) => ({
     url: 'core/v4/settings/flags',
     method: 'put',
     data,
