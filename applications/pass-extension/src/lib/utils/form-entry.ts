9c2c4701bb70e0ca51d399a5c7bef36f9eaa66c6
Edvin CANDON <edvin.candon@proton.ch>
2025-03-17 17:26:38 +0100

    Revert "Merge branch IDTEAM-pass-ml-retrained into main"

--- applications/pass-extension/src/lib/utils/form-entry.ts
+++ applications/pass-extension/src/lib/utils/form-entry.ts
@@ -1,5 +1,4 @@
-import type { FormType } from '@protontech/autofill';
-
+import type { FormType } from '@proton/pass/fathom';
 import { contentScriptMessage, sendMessage } from '@proton/pass/lib/extension/message/send-message';
 import type { AutosaveFormEntry, FormCredentials, FormSubmitPayload } from '@proton/pass/types';
 import { type FormEntry, FormEntryStatus, WorkerMessageType } from '@proton/pass/types';
