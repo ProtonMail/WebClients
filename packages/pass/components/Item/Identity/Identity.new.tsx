5c35f9aecb2aac18cf1f3d7a59d5b2be403fc8b9
Alejandro Tidele <alejandro.tidele@proton.ch>
2025-03-18 10:48:27 +0100

    File attachments support in item create/edit panels
    [IDTEAM-4144]

--- packages/pass/components/Item/Identity/Identity.new.tsx
+++ packages/pass/components/Item/Identity/Identity.new.tsx
@@ -4,6 +4,7 @@ import { useFormik } from 'formik';
 
 import type { ItemNewViewProps } from '@proton/pass/components/Views/types';
 import { useItemDraft } from '@proton/pass/hooks/useItemDraft';
+import { filesFormInitializer } from '@proton/pass/lib/file-attachments/helpers';
 import { itemBuilder } from '@proton/pass/lib/items/item.builder';
 import { validateIdentityForm } from '@proton/pass/lib/validation/identity';
 import type { IdentityItemFormValues } from '@proton/pass/types';
@@ -14,20 +15,21 @@ import { IdentityForm } from './Identity.form';
 
 export const IdentityNew: FC<ItemNewViewProps<'identity'>> = ({ shareId, onSubmit, onCancel }) => {
     const initialValues: IdentityItemFormValues = useMemo(
-        () => ({ shareId, name: '', note: '', ...itemBuilder('identity').data.content }),
+        () => ({ shareId, name: '', note: '', files: filesFormInitializer(), ...itemBuilder('identity').data.content }),
         []
     );
 
     const form = useFormik<IdentityItemFormValues>({
         initialValues,
         initialErrors: validateIdentityForm(initialValues),
-        onSubmit: ({ shareId, name, note, ...content }) => {
+        onSubmit: ({ shareId, name, note, files, ...content }) => {
             const id = uniqueId();
             onSubmit({
                 type: 'identity',
                 optimisticId: id,
                 shareId,
                 metadata: { name, note: obfuscate(note), itemUuid: id },
+                files,
                 content,
                 extraData: {},
                 extraFields: [],
