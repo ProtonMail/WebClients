897a365a9fd4111c64c321efa195815e37c51725
Alejandro Tidele <alejandro.tidele@proton.ch>
2025-03-18 10:59:18 +0100

    Add file-attachments support in export utilities
    [IDTEAM-4144]

--- packages/pass/lib/validation/export.ts
+++ packages/pass/lib/validation/export.ts
@@ -5,6 +5,6 @@ import { type ExportFormValues, ExportFormat } from '@proton/pass/lib/export/typ
 import { isEmptyString } from '@proton/pass/utils/string/is-empty-string';
 
 export const validateExportForm = (values: ExportFormValues): FormikErrors<ExportFormValues> =>
-    values.format === ExportFormat.PGP && isEmptyString(values.passphrase)
+    values.format === ExportFormat.EPEX && isEmptyString(values.passphrase)
         ? { passphrase: c('Warning').t`Passphrase is required` }
         : {};
