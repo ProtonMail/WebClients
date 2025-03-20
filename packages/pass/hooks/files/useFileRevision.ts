40ace36cc262d35d1bdd736a2ac7e3dcab90cdd0
Edvin CANDON <edvin.candon@proton.ch>
2025-03-20 11:02:38 +0100

    Handle item restore reconciliation
    [IDTEAM-4144]

--- packages/pass/hooks/files/useFileRevision.ts
+++ packages/pass/hooks/files/useFileRevision.ts
@@ -1,27 +1,39 @@
 import { useMemo } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useStore } from 'react-redux';
 
 import { useMemoSelector } from '@proton/pass/hooks/useMemoSelector';
 import { filesFormInitializer } from '@proton/pass/lib/file-attachments/helpers';
 import { fileRestore } from '@proton/pass/store/actions';
+import { selectItem } from '@proton/pass/store/selectors';
 import { selectItemFilesForRevision } from '@proton/pass/store/selectors/files';
+import type { State } from '@proton/pass/store/types';
 import type { FileRestoreDTO, SelectedRevision } from '@proton/pass/types';
+import { prop } from '@proton/pass/utils/fp/lens';
 
 export const useFileRevision = ({ shareId, itemId, revision }: SelectedRevision) => {
-    const dispatch = useDispatch();
+    const store = useStore<State>();
     const files = useMemoSelector(selectItemFilesForRevision, [shareId, itemId, revision]);
+    const dispatch = useDispatch();
 
     return useMemo(
         () => ({
             files,
             getFilesToRestore: () => {
-                return filesFormInitializer({});
-                // TODO: reconciliate here
-                // const filesToKeep = filteredFiles.filter(prop('disabled')).map(prop('fileUID'));
-                // return filesFormInitializer({
-                //     toRemove: files.filter((file) => !filesToKeep.includes(file.fileUID)).map(prop('fileID')),
-                //     toRestore: filteredFiles.filter(not(prop('disabled'))).map(prop('fileID')),
-                // });
+                const state = store.getState();
+
+                const latestRevision = selectItem(shareId, itemId)(state)?.revision;
+                if (!latestRevision) return filesFormInitializer({});
+
+                const latestFiles = selectItemFilesForRevision(shareId, itemId, latestRevision)(state);
+
+                /** Using `fileUIDs` to detect if file has already been restored or not. */
+                const fileUIDs = files.map(prop('fileUID'));
+                const latestFileUIDs = latestFiles.map(prop('fileUID'));
+
+                const toRestore = files.filter((file) => !latestFileUIDs.includes(file.fileUID)).map(prop('fileID'));
+                const toRemove = latestFiles.filter((file) => !fileUIDs.includes(file.fileUID)).map(prop('fileID'));
+
+                return filesFormInitializer({ toRemove, toRestore });
             },
             restoreFile: (dto: FileRestoreDTO) => dispatch(fileRestore.intent(dto)),
         }),
