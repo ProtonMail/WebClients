be2ba4a20e709856484afb6447f8f22984888f8c
Edvin CANDON <edvin.candon@proton.ch>
2025-03-20 15:29:52 +0100

    Handle `IFRAME_INIT` race condition
    - content-script context could have updated during the iframe init sequence
    - Make sure we always attach an up-to-date `IFrameInitPayload`

--- packages/pass/components/Core/AppStateProvider.tsx
+++ packages/pass/components/Core/AppStateProvider.tsx
@@ -1,4 +1,4 @@
-import { type FC, type PropsWithChildren, createContext, useEffect, useMemo, useState } from 'react';
+import { type FC, type PropsWithChildren, createContext, useEffect, useState } from 'react';
 
 import { AppStateManager } from '@proton/pass/components/Core/AppStateManager';
 import { createUseContext } from '@proton/pass/hooks/useContextFactory';
@@ -11,5 +11,5 @@ export const AppStateProvider: FC<PropsWithChildren> = ({ children }) => {
     const [state, setState] = useState(AppStateManager.getState());
     useEffect(() => AppStateManager.subscribe(setState), []);
 
-    return <AppStateContext.Provider value={useMemo(() => state, [state])}>{children}</AppStateContext.Provider>;
+    return <AppStateContext.Provider value={state}>{children}</AppStateContext.Provider>;
 };
