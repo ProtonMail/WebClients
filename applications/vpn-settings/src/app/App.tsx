ea43d88fa7889d125733bc6a441921ac627d522d
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-03-22 18:07:35 +0100

    Add support for persisting sessions in VPN app

--- applications/vpn-settings/src/app/App.tsx
+++ applications/vpn-settings/src/app/App.tsx
@@ -3,17 +3,19 @@ import { Router } from 'react-router-dom';
 import { createBrowserHistory as createHistory } from 'history';
 
 import * as bootstrap from '@proton/account/bootstrap';
+import { initStandaloneSession, removeSessions } from '@proton/account/bootstrap/standaloneSession';
 import { ApiProvider, AuthenticationProvider, ErrorBoundary, ProtonApp, StandardErrorPage } from '@proton/components';
 import useInstance from '@proton/hooks/useInstance';
 import { ProtonStoreProvider } from '@proton/redux-shared-store';
 import createApi from '@proton/shared/lib/api/createApi';
+import { handleLogoutFromURL } from '@proton/shared/lib/authentication/handleLogoutFromURL';
 import { getLoginPath } from '@proton/shared/lib/authentication/loginPath';
 import { replaceUrl } from '@proton/shared/lib/helpers/browser';
 import { initSafariFontFixClassnames } from '@proton/shared/lib/helpers/initSafariFontFixClassnames';
 import initLogicalProperties from '@proton/shared/lib/logical/logical';
 
 import PrivateApp from './PrivateApp';
-import PublicApp from './PublicApp';
+import PublicApp, { publicRoutes } from './PublicApp';
 import * as config from './config';
 import locales from './locales';
 import { extendStore, setupStore } from './store/store';
@@ -22,16 +24,26 @@ import { extraThunkArguments } from './store/thunk';
 const bootstrapApp = () => {
     const history = createHistory();
     const api = createApi({ config });
+    handleLogoutFromURL({ api });
     const authentication = bootstrap.createAuthentication();
     bootstrap.init({ config, authentication, locales });
     initLogicalProperties();
     initSafariFontFixClassnames();
     extendStore({ authentication, api, history, config });
-    return setupStore({ mode: authentication.UID ? 'default' : 'public' });
+    // Remove all sessions if a public route is opened, for example signup or login
+    if (Object.values(publicRoutes).some((value) => location.pathname.endsWith(value))) {
+        removeSessions({ api });
+    }
+    const session = initStandaloneSession({ api });
+    const privateApp = Boolean(authentication.UID || session);
+    return {
+        store: setupStore({ mode: privateApp ? 'default' : 'public' }),
+        privateApp,
+    };
 };
 
 const App = () => {
-    const store = useInstance(() => {
+    const { privateApp, store } = useInstance(() => {
         return bootstrapApp();
     });
     return (
@@ -41,7 +53,7 @@ const App = () => {
                     <Router history={extraThunkArguments.history}>
                         <ProtonStoreProvider store={store}>
                             <ApiProvider api={extraThunkArguments.api}>
-                                {extraThunkArguments.authentication.UID ? (
+                                {privateApp ? (
                                     <PrivateApp store={store} locales={locales} />
                                 ) : (
                                     <PublicApp
