ad50ec6e5cacf6b55ad5d6e8d26c5054648e8e1e
Edvin CANDON <edvin.candon@proton.ch>
2025-03-23 04:15:52 +0100

    Fine-tune storage layer
    - Add support for `FileBuffer` as Uint8Arrays directly to avoid
      overhead of having to create a Blob for an array buffer
    - Add support for abort signal in storage write operations
    - Add file auto-removal utils (1s/50MB - 30s user buffer - max 2min)
    
    [IDTEAM-4144]

--- packages/pass/lib/file-storage/fs.opfs.ts
+++ packages/pass/lib/file-storage/fs.opfs.ts
@@ -1,6 +1,6 @@
 import { logger } from '@proton/pass/utils/logger';
 
-import type { FileStorage } from './types';
+import type { FileBuffer, FileStorage } from './types';
 
 export class FileStorageOPFS implements FileStorage {
     async readFile(filename: string) {
@@ -14,13 +14,16 @@ export class FileStorageOPFS implements FileStorage {
         }
     }
 
-    async writeFile(filename: string, data: Blob | ReadableStream<Blob>) {
+    async writeFile(filename: string, data: FileBuffer | ReadableStream<FileBuffer>, signal?: AbortSignal) {
         try {
             const root = await navigator.storage.getDirectory();
             const fileHandle = await root.getFileHandle(filename, { create: true });
             const writable = await fileHandle.createWritable();
 
-            if (data instanceof Blob) await writable.write(data);
+            if (signal) {
+                if (signal.aborted) await writable.abort();
+                signal.addEventListener('abort', () => writable.abort());
+            }
 
             if (data instanceof ReadableStream) {
                 const reader = data.getReader();
@@ -29,7 +32,7 @@ export class FileStorageOPFS implements FileStorage {
                     if (done) break;
                     await writable.write(value);
                 }
-            }
+            } else await writable.write(data);
 
             await writable.close();
         } catch (err) {
