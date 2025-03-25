d878a736d9bc6ea3a07012f81ac2dd7f43067ba3
Edvin CANDON <edvin.candon@proton.ch>
2025-03-25 16:41:19 +0100

    Add base zip reader
    [IDTEAM-4144]

---
+++ packages/pass/lib/import/helpers/zip.reader.ts
@@ -0,0 +1,21 @@
+import type { ImportFileReader } from '@proton/pass/lib/import/types';
+import { prop } from '@proton/pass/utils/fp/lens';
+
+export const readZIP = async (file: File): Promise<ImportFileReader> => {
+    const zip = await import('@zip.js/zip.js');
+    const reader = new zip.ZipReader(new zip.BlobReader(file));
+    const entries = await reader.getEntries();
+    const files = new Set(entries.map(prop('filename')));
+
+    return {
+        files,
+        getFile: async (filename: string) => {
+            try {
+                const match = entries.find((entry) => entry.filename === filename);
+                return match ? ((await match.getData?.(new zip.BlobWriter())) ?? null) : null;
+            } catch {
+                return null;
+            }
+        },
+    };
+};
