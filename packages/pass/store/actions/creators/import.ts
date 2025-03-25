360ba9b11f81c0fed711cbeec2eb18b9e5809654
Edvin CANDON <edvin.candon@proton.ch>
2025-03-25 16:34:09 +0100

    Refactor import saga
    - Remove file upload from saga
    - Reconcile files to items via uuid
    
    [IDTEAM-4144]

--- packages/pass/store/actions/creators/import.ts
+++ packages/pass/store/actions/creators/import.ts
@@ -1,35 +1,27 @@
 import { createAction } from '@reduxjs/toolkit';
-import { c, msgid } from 'ttag';
+import { c } from 'ttag';
 
 import type { ImportPayload, ImportProvider } from '@proton/pass/lib/import/types';
-import { itemsImportRequest } from '@proton/pass/store/actions//requests';
 import { withCache } from '@proton/pass/store/actions/enhancers/cache';
 import { withNotification } from '@proton/pass/store/actions/enhancers/notification';
 import type { ImportEntry } from '@proton/pass/store/reducers';
 import { withRequestProgress } from '@proton/pass/store/request/enhancers';
 import { requestActionsFactory } from '@proton/pass/store/request/flow';
-import type { ClientEndpoint, ItemRevision } from '@proton/pass/types';
+import type { ClientEndpoint, IndexedByShareIdAndItemId, ItemRevision, UniqueItem } from '@proton/pass/types';
+
+export type ImportFile = UniqueItem & { filename: string };
 
 type ImportIntentDTO = { data: ImportPayload; provider: ImportProvider };
-type ImportSuccessDTO = { data: ImportEntry; endpoint?: ClientEndpoint };
 type ImportFailureDTO = { endpoint?: ClientEndpoint };
+type ImportSuccessDTO = {
+    data: ImportEntry;
+    files: IndexedByShareIdAndItemId<string[]>;
+    endpoint?: ClientEndpoint;
+};
+
 type ImportProgressDTO = { shareId: string; items: ItemRevision[] };
 
 export const importItems = requestActionsFactory<ImportIntentDTO, ImportSuccessDTO, ImportFailureDTO>('import::items')({
-    key: itemsImportRequest,
-    success: {
-        prepare: (payload) =>
-            withNotification({
-                type: 'info',
-                endpoint: payload.endpoint,
-                text: c('Info').ngettext(
-                    // translator: ${payload.total} is a number
-                    msgid`Imported ${payload.data.total} item`,
-                    `Imported ${payload.data.total} items`,
-                    payload.data.total
-                ),
-            })({ payload }),
-    },
     failure: {
         prepare: (error, payload) =>
             withNotification({
