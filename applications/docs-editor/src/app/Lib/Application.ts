35505d37e3d3631b7a99c78fc2fe2e94b12b07d1
Aman Harwara <aman.harwara@proton.ch>
2025-03-25 17:18:40 +0000

    Docs: Basic table of contents dropdown

--- applications/docs-editor/src/app/Lib/Application.ts
+++ applications/docs-editor/src/app/Lib/Application.ts
@@ -1,5 +1,7 @@
+import { versionCookieAtLoad } from '@proton/components/helpers/versionCookie'
 import type { DocumentRoleType } from '@proton/docs-shared'
 import { DocumentRole, InternalEventBus, SyncedEditorState } from '@proton/docs-shared'
+import type { Environment } from '@proton/shared/lib/interfaces'
 import type { LoggerInterface } from '@proton/utils/logs'
 import { Logger } from '@proton/utils/logs'
 
@@ -10,6 +12,13 @@ export class Application {
   private role: DocumentRole = new DocumentRole('Viewer')
   public readonly logger: LoggerInterface
   public languageCode: Intl.LocalesArgument = 'en'
+
+  /**
+   * `useEarlyAccess` throws an error when used without a <ProtonStoreProvider />
+   * somewhere up the tree and we only need to use its `currentEnvironment` to check
+   * whether we're in an alpha or beta environment, so we just use that value here directly.
+   */
+  public environment: Environment | undefined = versionCookieAtLoad
 
   constructor() {
     this.logger = new Logger('docs-editor')
