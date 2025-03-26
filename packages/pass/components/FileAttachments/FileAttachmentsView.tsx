64e536e57c72997a61bb665f48996b3f50809c8d
Edvin CANDON <edvin.candon@proton.ch>
2025-03-26 13:43:11 +0100

    Fine-tune file attachments panel display condition
    [IDTEAM-4144]

--- packages/pass/components/FileAttachments/FileAttachmentsView.tsx
+++ packages/pass/components/FileAttachments/FileAttachmentsView.tsx
@@ -6,7 +6,7 @@ import { useMemoSelector } from '@proton/pass/hooks/useMemoSelector';
 import { hasAttachments } from '@proton/pass/lib/items/item.predicates';
 import { filesResolve } from '@proton/pass/store/actions';
 import { selectRequestInFlight } from '@proton/pass/store/selectors';
-import { selectItemFilesCount, selectItemFilesForRevision } from '@proton/pass/store/selectors/files';
+import { selectItemFilesForRevision } from '@proton/pass/store/selectors/files';
 import type { ItemRevision } from '@proton/pass/types';
 
 import { FileAttachmentsList } from './FileAttachmentsList';
@@ -28,12 +28,12 @@ export const FileAttachmentsView: FC<FileAttachmentsViewProps> = ({ children, fi
 
 export const FileAttachmentsContentView: FC<{ revision: ItemRevision }> = ({ revision }) => {
     const { shareId, itemId } = revision;
-    const filesCount = useMemoSelector(selectItemFilesCount, [shareId, itemId]);
     const files = useMemoSelector(selectItemFilesForRevision, [shareId, itemId, revision.revision]);
+    const filesCount = files.length;
     const loading = useSelector(selectRequestInFlight(filesResolve.requestID(revision))) && !filesCount;
 
     return (
-        hasAttachments(revision) && (
+        (filesCount > 0 || hasAttachments(revision)) && (
             <FileAttachmentsView filesCount={filesCount} loading={loading}>
                 <FileAttachmentsList shareId={shareId} itemId={itemId} files={files} />
             </FileAttachmentsView>
