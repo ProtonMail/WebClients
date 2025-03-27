e60a8cea4624739fb783a41f5853a8f5fe7eb02d
Lucien Peslier <lucien.peslier@proton.ch>
2025-03-27 15:36:58 +0000

    [DRVWEB-4596] Add photos to album from album allow to select gallery items

--- applications/drive/src/app/photos/PhotosWithAlbums/toolbar/PhotosAddAlbumPhotosButton.tsx
+++ applications/drive/src/app/photos/PhotosWithAlbums/toolbar/PhotosAddAlbumPhotosButton.tsx
@@ -1,21 +1,42 @@
 import { c } from 'ttag';
 
-import { Icon, ToolbarButton } from '@proton/components/index';
+import { Button } from '@proton/atoms/index';
+import { Icon, ToolbarButton, useActiveBreakpoint } from '@proton/components/index';
 import clsx from '@proton/utils/clsx';
 
-export interface PhotosAddToAlbumButtonProps {
+export interface PhotosAddAlbumPhotosButtonProps {
     onClick: () => void;
-    showIconOnly: boolean;
+    type?: 'toolbar' | 'norm';
 }
 
-export const PhotosAddAlbumPhotosButton = ({ onClick, showIconOnly }: PhotosAddToAlbumButtonProps) => (
-    <ToolbarButton
-        title={c('Action').t`Add to album`}
-        onClick={onClick}
-        data-testid="toolbar-add-to-album"
-        className="inline-flex flex-nowrap flex-row items-center"
-    >
-        <Icon name="plus" className={clsx(!showIconOnly && 'mr-2')} />
-        <span className={clsx(showIconOnly && 'sr-only')}>{c('Action').t`Add to album`}</span>
-    </ToolbarButton>
-);
+export const PhotosAddAlbumPhotosButton = ({ onClick, type = 'norm' }: PhotosAddAlbumPhotosButtonProps) => {
+    const { viewportWidth } = useActiveBreakpoint();
+
+    if (type === 'toolbar') {
+        return (
+            <ToolbarButton
+                onClick={onClick}
+                data-testid="toolbar-photos-upload"
+                title={c('Action').t`Add photos`}
+                className="inline-flex flex-nowrap flex-row items-center"
+            >
+                <Icon name="plus-circle" className={clsx(!viewportWidth.xsmall && 'mr-2')} />
+                <span className={clsx(viewportWidth.xsmall && 'sr-only')}>{c('Action').t`Add photos`}</span>
+            </ToolbarButton>
+        );
+    }
+    return (
+        <Button
+            color="norm"
+            shape="solid"
+            size="small"
+            onClick={onClick}
+            data-testid="toolbar-album-add-photos"
+            title={c('Action').t`Add photos`}
+            className="inline-flex flex-nowrap flex-row items-center"
+        >
+            <Icon name="plus-circle" className="mr-2" />
+            <span>{c('Action').t`Add photos`}</span>
+        </Button>
+    );
+};
---
+++ applications/drive/src/app/photos/PhotosWithAlbums/toolbar/PhotosAddAlbumPhotosToolbar.tsx
@@ -0,0 +1,41 @@
+import { c } from 'ttag';
+
+import { Button } from '@proton/atoms/index';
+import { Icon, Toolbar } from '@proton/components';
+import useLoading from '@proton/hooks/useLoading';
+
+import { PhotosUploadButton } from './PhotosUploadButton';
+
+export interface PhotosAddAlbumPhotosToolbarProps {
+    shareId: string;
+    linkId: string;
+    selectedCount: number;
+    onAddAlbumPhotos: () => Promise<void>;
+}
+
+export const PhotosAddAlbumPhotosToolbar = ({
+    shareId,
+    linkId,
+    selectedCount,
+    onAddAlbumPhotos,
+}: PhotosAddAlbumPhotosToolbarProps) => {
+    const [isLoading, withLoading] = useLoading();
+    return (
+        <Toolbar className="py-1 px-2 toolbar--heavy toolbar--in-container toolbar--no-bg">
+            <div className="gap-2 flex items-center">
+                <PhotosUploadButton shareId={shareId} linkId={linkId} type="toolbar" isAddAlbumPhotosView />
+                <Button
+                    className="flex items-center"
+                    color="norm"
+                    shape="solid"
+                    loading={isLoading}
+                    disabled={selectedCount === 0}
+                    onClick={() => withLoading(onAddAlbumPhotos)}
+                >
+                    <Icon className="mr-2" name="checkmark" />
+                    <span>{c('Action').t`Add to album`}</span>
+                </Button>
+            </div>
+        </Toolbar>
+    );
+};
