4d838eed3e3b261888c57f74eef3e898aac28972
Edvin CANDON <edvin.candon@proton.ch>
2025-03-28 09:34:13 +0100

    Reflect import changes in UI layer
    - Send out import report from `useImportForm`
    - Support file result reporting
    
    [IDTEAM-4144]

--- packages/pass/components/Settings/Import.tsx
+++ packages/pass/components/Settings/Import.tsx
@@ -2,11 +2,10 @@ import { type FC, useCallback, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Form, FormikProvider } from 'formik';
-import { c, msgid } from 'ttag';
+import { c } from 'ttag';
 
 import { Button } from '@proton/atoms';
 import { Icon } from '@proton/components';
-import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
 import { ProgressModal } from '@proton/pass/components/FileAttachments/ProgressModal';
 import { ImportForm } from '@proton/pass/components/Import/ImportForm';
 import { ImportVaultsPickerModal } from '@proton/pass/components/Import/ImportVaultsPickerModal';
@@ -20,17 +19,15 @@ import {
 } from '@proton/pass/hooks/import/useImportForm';
 import { useAsyncModalHandles } from '@proton/pass/hooks/useAsyncModalHandles';
 import type { ImportPayload } from '@proton/pass/lib/import/types';
-import { PROVIDER_INFO_MAP } from '@proton/pass/lib/import/types';
-import { formatItemsCount } from '@proton/pass/lib/items/item.utils';
 import { selectCanCreateItems } from '@proton/pass/store/selectors';
 import type { MaybeNull } from '@proton/pass/types';
 import { pipe, tap } from '@proton/pass/utils/fp/pipe';
 import { PASS_APP_NAME } from '@proton/shared/lib/constants';
 
+import { ImportReport } from './ImportReport';
 import { SettingsPanel } from './SettingsPanel';
 
 export const Import: FC = () => {
-    const { endpoint } = usePassCore();
     const [importData, setImportData] = useState<MaybeNull<ImportPayload>>(null);
     const willSubmitResolver = useRef<(value: OnWillSubmitImportResult) => void>();
     const passphraseModal = useAsyncModalHandles<OnPassphraseImportResult>({ getInitialModalState: () => ({}) });
@@ -50,16 +47,13 @@ export const Import: FC = () => {
         []
     );
 
-    const { form, dropzone, busy, result, progress, cancel } = useImportForm({
+    const { form, dropzone, busy, progress, cancel } = useImportForm({
         onWillSubmit,
         onPassphrase: () =>
             new Promise((onSubmit) => passphraseModal.handler({ onSubmit }).catch(() => ({ ok: false }))),
     });
 
     const canCreateItem = useSelector(selectCanCreateItems);
-    const showResultDetails = (result?.ignored.length ?? 0) > 0 || (result?.warnings?.length ?? 0) > 0;
-    const totalImportedItems = result?.total ?? 0;
-    const totalItems = totalImportedItems + (result?.ignored.length ?? 0);
 
     if (!canCreateItem) {
         return (
@@ -94,66 +88,7 @@ export const Import: FC = () => {
                 />
             )}
 
-            {result && (
-                <SettingsPanel title={c('Label').t`Latest import`}>
-                    <div className="flex flex-column gap-y-1 text-sm">
-                        <div>
-                            <span className="color-weak">{c('Label').t`Imported from: `}</span>
-                            <span className="rounded bg-primary px-1 user-select-none">
-                                {PROVIDER_INFO_MAP[result.provider].title}
-                            </span>
-                        </div>
-
-                        <div>
-                            <span className="color-weak">{c('Label').t`Imported on : `}</span>
-                            <span>{new Date(result.importedAt * 1000).toLocaleString()}</span>
-                        </div>
-
-                        <div>
-                            <span className="color-weak">{c('Label').t`Total items: `}</span>
-                            <span>{formatItemsCount(totalItems)}</span>
-                        </div>
-
-                        <div>
-                            <span className="color-weak">{c('Label').t`Total imported items: `}</span>
-                            <span>{formatItemsCount(totalImportedItems)}</span>
-                        </div>
-
-                        {showResultDetails && (
-                            <div className="bg-norm rounded-sm p-3 mt-2">
-                                {result.ignored.length > 0 && (
-                                    <span className="mb-2 block">
-                                        {c('Info').ngettext(
-                                            msgid`The following ${result.ignored.length} item could not be imported:`,
-                                            `The following ${result.ignored.length} items could not be imported:`,
-                                            result.ignored.length
-                                        )}
-                                    </span>
-                                )}
-                                <div className="color-weak overflow-auto" style={{ maxHeight: 150 }}>
-                                    {result.ignored.map((description, idx) => (
-                                        <span className="block" key={`ignored-${idx}`}>
-                                            {description}
-                                        </span>
-                                    ))}
-                                    {result.warnings?.map((warning, idx) => (
-                                        <span className="block" key={`warning-${idx}`}>
-                                            {warning}
-                                        </span>
-                                    ))}
-                                </div>
-                            </div>
-                        )}
-
-                        {endpoint === 'page' && (
-                            <div className="mt-2">
-                                {c('Info')
-                                    .t`To review your imported data, click on the ${PASS_APP_NAME} icon in your browser toolbar.`}
-                            </div>
-                        )}
-                    </div>
-                </SettingsPanel>
-            )}
+            <ImportReport />
 
             <SettingsPanel
                 title={c('Label').t`Import`}
