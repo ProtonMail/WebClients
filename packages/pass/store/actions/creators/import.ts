f6681e7e9e7ab1cbabcb14e944e5c83aab82fb0e
Edvin CANDON <edvin.candon@proton.ch>
2025-03-28 09:31:47 +0100

    Refactor import saga for proper reporting during cancellation or failure
    [IDTEAM-4144]

--- packages/pass/store/actions/creators/import.ts
+++ packages/pass/store/actions/creators/import.ts
@@ -1,20 +1,21 @@
 import { createAction } from '@reduxjs/toolkit';
 import { c } from 'ttag';
 
+import type { ImportReport } from '@proton/pass/lib/import/helpers/report';
 import type { ImportPayload, ImportProvider } from '@proton/pass/lib/import/types';
 import { withCache } from '@proton/pass/store/actions/enhancers/cache';
 import { withNotification } from '@proton/pass/store/actions/enhancers/notification';
-import type { ImportEntry } from '@proton/pass/store/reducers';
 import { withRequestProgress } from '@proton/pass/store/request/enhancers';
 import { requestActionsFactory } from '@proton/pass/store/request/flow';
 import type { ClientEndpoint, IndexedByShareIdAndItemId, ItemRevision, UniqueItem } from '@proton/pass/types';
 
 export type ImportFile = UniqueItem & { filename: string };
+export type ImportFilesReport = { totalFiles: number; ignoredFiles: string[] };
 
 type ImportIntentDTO = { data: ImportPayload; provider: ImportProvider };
-type ImportFailureDTO = { endpoint?: ClientEndpoint };
+type ImportFailureDTO = { report: ImportReport; endpoint?: ClientEndpoint };
 type ImportSuccessDTO = {
-    data: ImportEntry;
+    report: ImportReport;
     files: IndexedByShareIdAndItemId<string[]>;
     endpoint?: ClientEndpoint;
 };
@@ -27,7 +28,6 @@ export const importItems = requestActionsFactory<ImportIntentDTO, ImportSuccessD
             withNotification({
                 type: 'error',
                 endpoint: payload.endpoint,
-                expiration: -1,
                 text: c('Error').t`Importing items failed`,
                 error,
             })({ payload, error }),
@@ -37,4 +37,8 @@ export const importItems = requestActionsFactory<ImportIntentDTO, ImportSuccessD
 export const importItemsProgress = createAction(
     'import::items::progress',
     withRequestProgress((payload: ImportProgressDTO) => withCache({ payload }))
+);
+
+export const importReport = createAction('import::files::report', (report: ImportReport) =>
+    withCache({ payload: { report } })
 );
