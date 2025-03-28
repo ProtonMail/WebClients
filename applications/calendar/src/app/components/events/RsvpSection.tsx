a1ff8039efd2c97bfba3261039d5c7e8f994aca6
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2025-03-28 13:27:54 +0100

    fix: Improve UI
    - CALWEB-5696: Drawer is not updated when response is changed from mail
    - CALWEB-5668: Note should be displayed under response block
    - CALWEB-5658: Mail widget is broken for newly received invitations

--- applications/calendar/src/app/components/events/RsvpSection.tsx
+++ applications/calendar/src/app/components/events/RsvpSection.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { c } from 'ttag';
 
@@ -11,10 +11,12 @@ import {
     EditNoteButton,
 } from '@proton/components/containers/calendar/RsvpSection/RsvpSectionButtons';
 import useLoading from '@proton/hooks/useLoading';
-import { ICAL_ATTENDEE_STATUS } from '@proton/shared/lib/calendar/constants';
+import { ICAL_ATTENDEE_STATUS, VIEWS } from '@proton/shared/lib/calendar/constants';
 import { wait } from '@proton/shared/lib/helpers/promise';
 import type { PartstatData } from '@proton/shared/lib/interfaces/calendar';
+import clsx from '@proton/utils/clsx';
 
+import { getIsCalendarAppInDrawer } from '../../helpers/views';
 import { INVITE_ACTION_TYPES } from '../../interfaces/Invite';
 import { RsvpSpotlight } from './RsvpSpotlight';
 
@@ -27,7 +29,7 @@ interface Props {
     userPartstat: ICAL_ATTENDEE_STATUS;
     userComment?: string;
     disabled: boolean;
-    isSearchView: boolean;
+    view: VIEWS;
 }
 
 const RsvpButtons = ({
@@ -50,7 +52,7 @@ const RsvpButtons = ({
                     retryCreateEvent: () => wait(0),
                     retryUpdateEvent: () => wait(0),
                 }}
-                originalPatstat={userPartstat}
+                originalPartstat={userPartstat}
                 partstat={attendeeStatus}
                 disabled={disabled}
             />
@@ -58,8 +60,10 @@ const RsvpButtons = ({
     );
 };
 
-const RsvpSection = ({ handleChangePartstat, userPartstat, userComment, disabled, isSearchView }: Props) => {
+const RsvpSection = ({ handleChangePartstat, userPartstat, userComment, disabled, view }: Props) => {
     const [displayNoteOverlay, setDisplayNoteOverlay] = useState(false);
+    const isDrawerApp = getIsCalendarAppInDrawer(view);
+    const isSearchView = view === VIEWS.SEARCH;
     const [model, setModel] = useState<PartstatData>({
         Status: userPartstat,
         Comment: userComment ?? undefined,
@@ -128,49 +132,79 @@ const RsvpSection = ({ handleChangePartstat, userPartstat, userComment, disabled
         );
     };
 
+    useEffect(() => {
+        if (userPartstat !== model.Status) {
+            setModel((currentModel) => ({
+                ...currentModel,
+                Status: userPartstat,
+            }));
+        }
+    }, [userPartstat]);
+
+    useEffect(() => {
+        // Update user comment in background only if not actively editing
+        if (userPartstat !== model.Status && !displayNoteOverlay) {
+            setModel((currentModel) => ({
+                ...currentModel,
+                Comment: userComment,
+            }));
+        }
+    }, [userComment]);
+
     // Rely on userComment instead of model.Comment to avoid line jump
     const canReplyWithNote = !userComment && !isSearchView;
 
     return (
         <>
-            <RsvpButtons
-                handleResponse={handleResponse}
-                userPartstat={userPartstat}
-                disabled={disabled}
-                // As no inner state required for this part i'm
-                // not passing model.Status and rely on parent state
-                attendeeStatus={userPartstat}
-            />
+            {
+                // We display it all the time when we're not in drawer to avoid line jump when open/close note overlay
+                // If in drawer app and notes are opened we hide it to not duplicate content
+                (!isDrawerApp || (isDrawerApp && !displayNoteOverlay)) && (
+                    <>
+                        <RsvpButtons
+                            handleResponse={handleResponse}
+                            userPartstat={userPartstat}
+                            disabled={disabled}
+                            // As no inner state required for this part i'm
+                            // not passing model.Status and rely on parent state
+                            attendeeStatus={userPartstat}
+                        />
 
-            {canReplyWithNote && (
-                <RsvpSpotlight>
-                    <Button
-                        className="flex flex-auto justify-end text-sm color-weak"
-                        shape="underline"
-                        onClick={() => setDisplayNoteOverlay(true)}
-                    >
-                        {c('Action').t`Reply with a note`}
-                    </Button>
-                </RsvpSpotlight>
-            )}
+                        {canReplyWithNote && (
+                            <RsvpSpotlight>
+                                <Button
+                                    className={clsx(
+                                        'flex flex-auto justify-end text-sm color-weak',
+                                        isDrawerApp && 'mt-2'
+                                    )}
+                                    shape="underline"
+                                    onClick={() => setDisplayNoteOverlay(true)}
+                                >
+                                    {c('Action').t`Reply with a note`}
+                                </Button>
+                            </RsvpSpotlight>
+                        )}
 
-            {!!userComment && (
-                <div className="mt-2 flex flex-auto justify-space-between flex-nowrap items-center gap-2">
-                    <div className="text-ellipsis color-weak text-sm">
-                        <span className="text-bold color-weak">{c('Note').t`Note: `}</span>
-                        {userComment}
-                    </div>
-                    {!isSearchView && (
-                        <div className="flex flex-none">
-                            <EditNoteButton onEdit={() => setDisplayNoteOverlay(!displayNoteOverlay)} />
-                            <DeleteNoteButton onDelete={handleDeleteComment} loading={loadingDelete} />
-                        </div>
-                    )}
-                </div>
-            )}
+                        {!!userComment && (
+                            <div className="mt-2 flex flex-auto justify-space-between flex-nowrap items-center gap-2">
+                                <div className="text-ellipsis color-weak text-sm">
+                                    <span className="text-bold color-weak">{c('Note').t`Note: `}</span>
+                                    {userComment}
+                                </div>
+                                {!isSearchView && (
+                                    <div className="flex flex-none">
+                                        <EditNoteButton onEdit={() => setDisplayNoteOverlay(!displayNoteOverlay)} />
+                                        <DeleteNoteButton onDelete={handleDeleteComment} loading={loadingDelete} />
+                                    </div>
+                                )}
+                            </div>
+                        )}
+                    </>
+                )
+            }
 
             {displayNoteOverlay && (
-                <div className="absolute bg-norm event-popover-rsvp-section border-top py-4">
+                <div className={clsx('bg-norm', !isDrawerApp && 'py-4 absolute border-top event-popover-rsvp-section')}>
                     <RsvpButtons
                         handleResponse={handleResponse}
                         userPartstat={userPartstat}
@@ -178,7 +212,7 @@ const RsvpSection = ({ handleChangePartstat, userPartstat, userComment, disabled
                         attendeeStatus={model.Status}
                     />
                     <InputFieldTwo
-                        className="mt-4 resize-none"
+                        className={clsx('mt-4 resize-none')}
                         assistContainerClassName="m-0"
                         id="rsvp-modal"
                         as={TextAreaWithCounter}
