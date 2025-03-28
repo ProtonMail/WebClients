56f3b72631aa0240e25f6727ed3feaa42a4817cd
Alejandro Tidele <alejandro.tidele@proton.ch>
2025-03-28 14:46:53 +0100

    Remove download file action when clicking on the file name
    [IDTEAM-4144]

--- packages/pass/components/Layout/Button/ClickableDiv.tsx
+++ packages/pass/components/Layout/Button/ClickableDiv.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions,jsx-a11y/prefer-tag-over-role,jsx-a11y/interactive-supports-focus */
-import { type FC, type PropsWithChildren, useEffect, useRef } from 'react';
+import { type FC, type ReactNode, useEffect, useRef, useState } from 'react';
 
 import type { Maybe, MaybeNull } from '@proton/pass/types';
 
@@ -7,14 +7,11 @@ type ClickableDivProps = {
     className?: string;
     onClick?: () => void;
     onDoubleClick?: Maybe<() => void>;
+    children: ((props: { hovering: boolean }) => ReactNode) | ReactNode;
 };
 
-export const ClickableDiv: FC<PropsWithChildren<ClickableDivProps>> = ({
-    children,
-    className,
-    onClick,
-    onDoubleClick,
-}) => {
+export const ClickableDiv: FC<ClickableDivProps> = ({ children, className, onClick, onDoubleClick }) => {
+    const [hovering, setHovering] = useState(false);
     const clickTimeoutRef = useRef<MaybeNull<NodeJS.Timeout>>(null);
 
     const handleClick = () => {
@@ -51,8 +48,15 @@ export const ClickableDiv: FC<PropsWithChildren<ClickableDivProps>> = ({
     );
 
     return (
-        <div className={className} onClick={handleClick} onDoubleClick={handleDoubleClick} role="button">
-            {children}
+        <div
+            className={className}
+            onClick={handleClick}
+            onDoubleClick={handleDoubleClick}
+            onMouseEnter={() => setHovering(true)}
+            onMouseLeave={() => setHovering(false)}
+            role="button"
+        >
+            {typeof children === 'function' ? children({ hovering }) : children}
         </div>
     );
 };
