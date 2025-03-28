73d1eaadbb5313b98f5c369ab1e15e8cce704ae0
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2025-03-28 17:14:00 +0100

    Fix file upload on firefox extension
    [IDTEAM-4144]

--- applications/pass-extension/src/lib/hooks/useExpandPopup.ts
+++ applications/pass-extension/src/lib/hooks/useExpandPopup.ts
@@ -4,13 +4,13 @@ import browser from '@proton/pass/lib/globals/browser';
 import { pixelParser } from '@proton/pass/utils/dom/computed-styles';
 import noop from '@proton/utils/noop';
 
-export const useExpandPopup = () =>
+export const useExpandPopup = (subpath = '') =>
     useCallback(() => {
         const rootStyles = getComputedStyle(document.documentElement);
 
         browser.windows
             .create({
-                url: browser.runtime.getURL('popup.html'),
+                url: browser.runtime.getURL(`popup.html#${subpath}`),
                 type: 'popup',
                 width: pixelParser(rootStyles.getPropertyValue('--popup-width')),
                 height: pixelParser(rootStyles.getPropertyValue('--popup-height')),
