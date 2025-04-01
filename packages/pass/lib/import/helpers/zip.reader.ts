5b061663f7d3d49d0b062230793f964cade005b6
Edvin CANDON <edvin.candon@proton.ch>
2025-04-01 18:12:49 +0200

    Support 1pif archives with file attachments
    [IDTEAM-4144]

--- packages/pass/lib/import/helpers/zip.reader.ts
+++ packages/pass/lib/import/helpers/zip.reader.ts
@@ -1,14 +1,17 @@
 import type { ImportFileReader } from '@proton/pass/lib/import/types';
 import { prop } from '@proton/pass/utils/fp/lens';
+import { not } from '@proton/pass/utils/fp/predicates';
 
 export const readZIP = async (file: File): Promise<ImportFileReader> => {
     const zip = await import(/* webpackChunkName: "zip.reader" */ '@zip.js/zip.js');
     const reader = new zip.ZipReader(new zip.BlobReader(file));
     const entries = await reader.getEntries();
-    const files = new Set(entries.map(prop('filename')));
+    const files = new Set(entries.filter(not(prop('directory'))).map(prop('filename')));
+    const dirs = new Set(entries.filter(prop('directory')).map(prop('filename')));
 
     return {
         files,
+        dirs,
         getFile: async (filename: string) => {
             try {
                 const match = entries.find((entry) => entry.filename === filename);
