6a1f0a5f5335886a25d89480db732db16a794052
LisaBot <service_account_4adb237cb9e6ed41ddc75bf6b371d140@noreply.gitlab.protontech.ch>
2025-04-01 02:42:17 +0000

    Remove deprecated packages
    * @babel/plugin-proposal-optional-chaining: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. This plugin is included in @babel/preset-env, in ES2020
    * @babel/plugin-proposal-object-rest-spread: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. This plugin is included in @babel/preset-env, in ES2018
    * @types/dompurify: This is a stub types definition. dompurify provides its own type definitions, so you do not need this installed.
    * @types/prettier: This is a stub types definition. prettier provides its own type definitions, so you do not need this installed.
    * @types/pkijs: This is a stub types definition. pkijs provides its own type definitions, so you do not need this installed.
    * @types/qrcode.react: This is a stub types definition. qrcode.react provides its own type definitions, so you do not need this installed.
    * @types/credit-card-type: This is a stub types definition. credit-card-type provides its own type definitions, so you do not need this installed.

--- packages/shared/babel.config.js
+++ packages/shared/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     presets: ['@babel/preset-env'],
-    plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime'],
+    plugins: ['@babel/plugin-transform-runtime'],
 };
