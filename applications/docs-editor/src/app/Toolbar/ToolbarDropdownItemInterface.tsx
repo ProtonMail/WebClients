0d3ef499eab4395ef957fddfe7635f2c2afd2201
Aman Harwara <aman.harwara@proton.ch>
2025-04-01 15:07:05 +0000

    Docs: Updated color picker UI

--- applications/docs-editor/src/app/Toolbar/ToolbarDropdownItemInterface.tsx
+++ applications/docs-editor/src/app/Toolbar/ToolbarDropdownItemInterface.tsx
@@ -1,12 +1,12 @@
+import type { DropdownProps } from '@proton/components'
 import type { ReactNode } from 'react'
 
 export type ToolbarDropdownItemInterface = {
   type: 'dropdown'
   menu: ReactNode
-  dropdownProps: {
-    onClosed: () => void
-  }
+  dropdownProps: Partial<DropdownProps>
   useToolbarButton?: boolean
   overflowBehavior: 'submenu' | 'spread-items'
   label: (target: 'toolbar' | 'overflow') => ReactNode
+  hasCaret?: boolean
 }
