907c24fd8ae328d57960aaf1f91301802e31ef5a
Edvin CANDON <edvin.candon@proton.ch>
2025-04-01 14:34:52 +0200

    Factorize request cancellation utils
    [IDTEAM-4144]

--- packages/pass/store/actions/creators/export.ts
+++ packages/pass/store/actions/creators/export.ts
@@ -1,15 +1,7 @@
-import { isAbortError } from '@proton/pass/lib/api/errors';
 import type { ExportRequestOptions, ExportResult } from '@proton/pass/lib/export/types';
+import { withAbortPayload } from '@proton/pass/store/actions/creators/utils';
 import { requestActionsFactory } from '@proton/pass/store/request/flow';
-import { getErrorMessage } from '@proton/pass/utils/errors/get-error-message';
 
 export const exportData = requestActionsFactory<ExportRequestOptions, ExportResult>('export::data')({
-    failure: {
-        prepare: (error) => ({
-            payload: {
-                aborted: isAbortError(error),
-                error: getErrorMessage(error),
-            },
-        }),
-    },
+    failure: { prepare: withAbortPayload },
 });
