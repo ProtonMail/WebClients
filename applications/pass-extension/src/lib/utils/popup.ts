f4140ea08b23c3573a371263317b234589ffa449
Edvin CANDON <edvin.candon@proton.ch>
2025-04-02 08:03:41 +0200

    Fix safari extension expanded popup resizing issues

--- applications/pass-extension/src/lib/utils/popup.ts
+++ applications/pass-extension/src/lib/utils/popup.ts
@@ -56,17 +56,19 @@ export const popupSizeSurgery = () => {
 export const isExpandedPopup = async (): Promise<boolean> => {
     const currentTab = (await browser.tabs.getCurrent()) as Maybe<Tabs.Tab>;
 
-    if (BUILD_TARGET === 'safari') {
-        const url = currentTab?.url;
-        const popupPath = `${window.location.origin}/popup.html`;
-        /** In Safari, `currentTab` returns the tab behind the extension popup frame.
-         * If it matches the current location, then we're expanded in a dedicated tab */
-        return Boolean(url?.toLowerCase().startsWith(popupPath));
-    }
+    const expanded = (() => {
+        if (BUILD_TARGET === 'safari') {
+            const url = currentTab?.url;
+            const popupPath = `${window.location.origin}/popup.html`;
+            /** In Safari, `currentTab` returns the tab behind the extension popup frame.
+             * If it matches the current location, then we're expanded in a dedicated tab */
+            return Boolean(url?.toLowerCase().startsWith(popupPath));
+        }
 
-    /** On chromium based browsers : the current tab will be `undefined`
-     * when called from the extension popup frame */
-    const expanded = currentTab !== undefined;
+        /** On chromium based browsers : the current tab will be `undefined`
+         * when called from the extension popup frame */
+        return currentTab !== undefined;
+    })();
 
     if (expanded) {
         /* when pop-up is expanded: reset the dimension constraints */
