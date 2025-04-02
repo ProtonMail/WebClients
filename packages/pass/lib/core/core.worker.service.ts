7062e2500ad984c71991f24fd3cfe10d8a728738
Edvin CANDON <edvin.candon@proton.ch>
2025-04-02 08:01:53 +0200

    Add support for loading PassUI in a dedicated worker
    - Clean up @proton/pass/lib/core workers/services/proxies
    - Reflect changes in critical offline assets and extension imported scripts
    
    [IDTEAM-4144]

--- packages/pass/lib/core/core.worker.service.ts
+++ packages/pass/lib/core/core.worker.service.ts
@@ -1,19 +1,34 @@
+import type { PassCoreMethod, PassCoreRPC, PassCoreResult, PassCoreService } from '@proton/pass/lib/core/core.types';
 import { throwError } from '@proton/pass/utils/fp/throw';
+import { waitUntil } from '@proton/pass/utils/fp/wait-until';
+import { logger } from '@proton/pass/utils/logger';
 
 import { WASM_PROCEDURE_TIMEOUT } from './constants';
-import type { PassCoreMethod, PassCoreRPC, PassCoreResult, PassCoreService } from './types';
 
 /** Creates a `PassCoreService` instance which spawns the `PassRustCore`
  * binary in a dedicated worker. Communication occurs via postMessaging
- * through MessageChannels (see: `core.worker.ts`) This service should
+ * through MessageChannels (see: `core.worker.ts`). This service should
  * be used to ensure the WASM module will not block the main UI thread */
 export const createPassCoreWorkerService = (): PassCoreService => {
+    const state = { ready: false };
+
     const worker = new Worker(
-        /* webpackChunkName: "core.worker" */
+        /* webpackChunkName: "pass-core.worker" */
         new URL('@proton/pass/lib/core/core.worker', import.meta.url)
     );
 
-    const send = <T extends PassCoreMethod>(message: PassCoreRPC<T>) => {
+    const onReady = (event: MessageEvent) => {
+        if (event.data?.type === 'PassCoreUI::worker_ready') {
+            logger.info('[PassCoreWorker] Worker is ready');
+            worker.removeEventListener('message', onReady);
+            state.ready = true;
+        }
+    };
+
+    worker.addEventListener('message', onReady);
+
+    const send = async <T extends PassCoreMethod>(message: PassCoreRPC<T>) => {
+        await waitUntil(() => state.ready, 250);
         const channel = new MessageChannel();
 
         return new Promise<PassCoreResult<T>>((resolve, reject) => {
