f64e04ec410843590be9a9eba3cccb8ddaf6fbfb
Edvin CANDON <edvin.candon@proton.ch>
2025-04-03 16:39:54 +0200

    Add FileStorage stub for legacy browsers
    [IDTEAM-4144]

--- packages/pass/lib/file-storage/fs.ts
+++ packages/pass/lib/file-storage/fs.ts
@@ -1,11 +1,17 @@
+import { FileStorageStub } from '@proton/pass/lib/file-storage/fs.stub';
 import { isSafari } from '@proton/shared/lib/helpers/browser';
 
 import { FileStorageIDB } from './fs.idb';
 import { FileStorageOPFS } from './fs.opfs';
 import type { FileStorage } from './types';
 
-// isSafari() works on web app but not on extension background script, so we have to use BUILD_TARGET too
+/** isSafari() works on web app but not on extension
+ * background script, so we have to use BUILD_TARGET */
 const isOPFSSupported = () =>
     BUILD_TARGET !== 'safari' && !isSafari() && Boolean(navigator.storage && navigator.storage.getDirectory);
 
-export const fileStorage: FileStorage = isOPFSSupported() ? new FileStorageOPFS() : new FileStorageIDB();
+export const fileStorage: FileStorage = (() => {
+    if (isOPFSSupported()) return new FileStorageOPFS();
+    if ('indexedDB' in globalThis) return new FileStorageIDB();
+    return new FileStorageStub();
+})();
