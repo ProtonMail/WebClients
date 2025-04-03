255160dbacb8a25a8e589351189df47db91f65e5
Edvin CANDON <edvin.candon@proton.ch>
2025-04-03 14:19:16 +0200

    Support importing raw PGP file from pass encrypted archive
    [IDTEAM-4144]

---
+++ packages/pass/lib/import/providers/protonpass/utils.ts
@@ -0,0 +1,30 @@
+import { c } from 'ttag';
+
+import { decryptPassExport } from '@proton/pass/lib/crypto/utils/export';
+import { ExportFormat } from '@proton/pass/lib/export/types';
+import { ImportReaderError } from '@proton/pass/lib/import/helpers/error';
+import { PASS_APP_NAME } from '@proton/shared/lib/constants';
+
+export const getProtonPassImportPGPType = async (buffer: Uint8Array): Promise<ExportFormat> => {
+    const head = buffer.slice(0, 5);
+    if (new TextDecoder().decode(head).startsWith('{')) return ExportFormat.JSON;
+    else return ExportFormat.ZIP;
+};
+
+export const decryptProtonPassImport = async (file: File, passphrase?: string): Promise<Uint8Array> => {
+    try {
+        return await decryptPassExport(await file.text(), passphrase ?? '');
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            const errorDetail = err.message.includes('Error decrypting message')
+                ? c('Error').t`Passphrase is incorrect`
+                : c('Error').t`File could not be parsed`;
+
+            throw new ImportReaderError(
+                c('Error').t`Decrypting your ${PASS_APP_NAME} export file failed. ${errorDetail}`
+            );
+        }
+
+        throw err;
+    }
+};
