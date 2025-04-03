ede691eaf0c991dfce1efa259b0a8d95f35356ec
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2025-04-03 09:37:58 +0200

    refactor: remove unexpected white space in translation

--- applications/calendar/src/app/components/events/RsvpSection.tsx
+++ applications/calendar/src/app/components/events/RsvpSection.tsx
@@ -234,7 +234,7 @@ const RsvpSection = ({ handleChangePartstat, userPartstat, userComment, disabled
                         {!!formattedUserComment && (
                             <div className="mt-2 flex flex-auto justify-space-between flex-nowrap items-center gap-2">
                                 <div className="text-ellipsis color-weak text-sm">
-                                    <span className="text-bold color-weak">{c('Note').t`Note: `}</span>
+                                    <span className="text-bold color-weak">{c('Note').t`Note:`} </span>
                                     <span dangerouslySetInnerHTML={{ __html: formattedUserComment }} />
                                 </div>
                                 {!isSearchView && (
