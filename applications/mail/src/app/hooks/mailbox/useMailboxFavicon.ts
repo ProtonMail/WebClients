bcedbd56d00114cd06724e9a772af68fb6727d86
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-04-04 11:26:12 +0000

    Remove UnreadFavicon feature flag

--- applications/mail/src/app/hooks/mailbox/useMailboxFavicon.ts
+++ applications/mail/src/app/hooks/mailbox/useMailboxFavicon.ts
@@ -2,11 +2,10 @@ import { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { useDynamicFavicon } from '@proton/components';
-import { FeatureCode, useFeature } from '@proton/features';
 import { useConversationCounts } from '@proton/mail/counts/conversationCounts';
 import { useMessageCounts } from '@proton/mail/counts/messageCounts';
-
-import useMailModel from 'proton-mail/hooks/useMailModel';
+import { useMailSettings } from '@proton/mail/mailSettings/hooks';
+import { DEFAULT_MAILSETTINGS } from '@proton/shared/lib/mail/mailSettings';
 
 import favicons, { baseFavicon } from '../../../assets/favicons';
 import { getCountersByLabelId } from '../../helpers/counter';
@@ -15,7 +14,7 @@ import { isConversationMode } from '../../helpers/mailSettings';
 export const useMailboxFavicon = (labelID: string) => {
     const location = useLocation();
 
-    const mailSettings = useMailModel('MailSettings');
+    const [mailSettings = DEFAULT_MAILSETTINGS] = useMailSettings();
     const [conversationCounts] = useConversationCounts();
     const [messageCounts] = useMessageCounts();
 
@@ -23,12 +22,9 @@ export const useMailboxFavicon = (labelID: string) => {
     const counters = conversationMode ? conversationCounts : messageCounts;
 
     const countersByLabelId = useMemo(() => getCountersByLabelId(counters), [counters]);
-    const unreads = countersByLabelId[labelID]?.Unread ?? 0;
+    const unreadEmails = countersByLabelId[labelID]?.Unread ?? 0;
 
-    const shouldDisplayUnreadFavicon =
-        useFeature(FeatureCode.UnreadFavicon).feature?.Value && mailSettings.UnreadFavicon;
-
-    const unreadFavicon = !unreads ? baseFavicon : unreads > 99 ? favicons[100] : favicons[unreads];
-    const faviconSrc = !shouldDisplayUnreadFavicon ? baseFavicon : unreadFavicon;
+    const unreadFavicon = !unreadEmails ? baseFavicon : unreadEmails > 99 ? favicons[100] : favicons[unreadEmails];
+    const faviconSrc = mailSettings.UnreadFavicon ? unreadFavicon : baseFavicon;
     useDynamicFavicon(faviconSrc);
 };
