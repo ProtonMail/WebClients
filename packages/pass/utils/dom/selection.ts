90b0385322a69164e20463c2ff6709ef84a20463
Edvin CANDON <edvin.candon@proton.ch>
2025-04-08 20:20:24 +0200

    Fine-tune SelectionManager for firefox support

--- packages/pass/utils/dom/selection.ts
+++ packages/pass/utils/dom/selection.ts
@@ -1,24 +1,13 @@
-import type { MaybeNull } from '@proton/pass/types';
-
 export const SelectionManager = (() => {
-    let selection: MaybeNull<Selection> = null;
+    const hasChildOf = (el: Element) => {
+        const selection = window.getSelection();
+        if (selection === null || selection.toString().length === 0) return false;
 
-    const onSelectionChange = () => {
-        selection = window.getSelection();
+        const { anchorNode, focusNode } = selection;
+        return selection.containsNode(el) || el.contains(anchorNode) || el.contains(focusNode);
     };
 
-    document.addEventListener('selectionchange', onSelectionChange);
-
-    const hasAny = () => selection !== null && selection.toString().length > 0;
-
-    const hasChildOf = (el: Element) =>
-        hasAny() && selection !== null && (el.contains(selection.anchorNode) || el.contains(selection.focusNode));
-
-    const disconnect = () => document.removeEventListener('selectionchange', onSelectionChange);
-
     return {
-        hasAny,
         hasChildOf,
-        disconnect,
     };
 })();
