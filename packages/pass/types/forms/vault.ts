3fab84575e50c50b3f42a63fe29b55c0d3b62d62
Edvin CANDON <edvin.candon@proton.ch>
2025-04-10 11:01:10 +0200

    Reflect static proto type imports

--- packages/pass/types/forms/vault.ts
+++ packages/pass/types/forms/vault.ts
@@ -1,4 +1,4 @@
-import type { VaultColor, VaultIcon } from '@proton/pass/types/protobuf/vault-v1';
+import type { VaultColor, VaultIcon } from '@proton/pass/types/protobuf/vault-v1.static';
 
 export type VaultFormValues = {
     name: string;
