84bfe8c8d6fe8881010c60df4a6619745a9d975c
Edvin CANDON <edvin.candon@proton.ch>
2025-04-10 11:02:30 +0200

    Regenerate pass protos with enum extraction

---
+++ packages/pass/types/protobuf/item-v1.static.ts
@@ -0,0 +1,23 @@
+/* eslint-disable */
+export enum CardType {
+    /**
+     * @generated from protobuf enum value: Unspecified = 0;
+     */
+    Unspecified = 0,
+    /**
+     * @generated from protobuf enum value: Other = 1;
+     */
+    Other = 1,
+    /**
+     * @generated from protobuf enum value: Visa = 2;
+     */
+    Visa = 2,
+    /**
+     * @generated from protobuf enum value: Mastercard = 3;
+     */
+    Mastercard = 3,
+    /**
+     * @generated from protobuf enum value: AmericanExpress = 4;
+     */
+    AmericanExpress = 4,
+}
