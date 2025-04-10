ef7386c99c6017eac96a51efb878cf7a9e5c8c7c
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-04-10 16:00:51 +0200

    Refactor ForwardModal
    Move key info out of React's State
    
    Co-authored-by: larabr <7375870+larabr@users.noreply.github.com>

---
+++ packages/components/containers/forward/ForwardModalInterface.ts
@@ -0,0 +1,28 @@
+import type { Condition, FilterStatement } from '@proton/components/containers/filters/interfaces';
+import type { PublicKeyReference } from '@proton/crypto';
+
+export enum ForwardModalStep {
+    Setup,
+    UserConfirmation,
+    FinalizeForwardingSetup,
+}
+
+export interface ForwardModalState {
+    step: ForwardModalStep;
+    loading?: boolean;
+    addressID: string;
+    isExternal?: boolean;
+    isInternal?: boolean;
+    forwardeeEmail: string;
+    keyErrors?: string[];
+    statement: FilterStatement;
+    conditions: Condition[];
+}
+
+export interface ForwardModalKeyState {
+    forwardeePublicKey: PublicKeyReference | undefined;
+    forwarderPrimaryKeysInfo: {
+        v4: { ID: string; supportsE2EEForwarding: boolean };
+        v6?: { ID: string; supportsE2EEForwarding: false };
+    };
+}
