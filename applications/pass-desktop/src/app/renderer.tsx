9c37015f2621f8b0b4698a20b116aa0907dacac8
Edvin CANDON <edvin.candon@proton.ch>
2025-04-11 12:45:02 +0200

    Improve OPFS detection with storage instance update

--- applications/pass-desktop/src/app/renderer.tsx
+++ applications/pass-desktop/src/app/renderer.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
-import { fileStorage } from '@proton/pass/lib/file-storage/fs';
+import { fileStorage, fileStorageReady } from '@proton/pass/lib/file-storage/fs';
 import { initElectronClassnames } from '@proton/shared/lib/helpers/initElectronClassnames';
 
 import { App } from './App';
@@ -9,8 +9,10 @@ import reportClientLaunch from './reportClientLaunch';
 initElectronClassnames();
 void reportClientLaunch();
 
-fileStorage.attachGarbageCollector(localStorage);
-void fileStorage.clearAll();
+void fileStorageReady.then(() => {
+    fileStorage.attachGarbageCollector(localStorage);
+    return fileStorage.clearAll();
+});
 
 const container = document.querySelector('.app-root');
 const root = createRoot(container!);
--- packages/pass/lib/file-storage/fs.ts
+++ packages/pass/lib/file-storage/fs.ts
@@ -1,18 +1,40 @@
 import { FileStorageMemory } from '@proton/pass/lib/file-storage/fs.memory';
 import { FileStorageStub } from '@proton/pass/lib/file-storage/fs.stub';
+import { awaiter } from '@proton/pass/utils/fp/promises';
+import { logger } from '@proton/pass/utils/logger';
 import { isSafari } from '@proton/shared/lib/helpers/browser';
+import noop from '@proton/utils/noop';
 
 import { FileStorageIDB } from './fs.idb';
 import { FileStorageOPFS } from './fs.opfs';
 import type { FileStorage } from './types';
+
+export const fileStorageReady = awaiter<boolean>();
 
 /** isSafari() works on web app but not on extension
  * background script, so we have to use BUILD_TARGET */
 const isOPFSSupported = () =>
     BUILD_TARGET !== 'safari' && !isSafari() && Boolean(navigator.storage && navigator.storage.getDirectory);
 
-export const fileStorage: FileStorage = (() => {
-    if (isOPFSSupported()) return new FileStorageOPFS();
+export const getSupportedFileStorage = (options: { tryOPFS: boolean }) => {
+    if (options.tryOPFS && isOPFSSupported()) return new FileStorageOPFS();
     if ('indexedDB' in globalThis) return new FileStorageIDB();
     return !EXTENSION_BUILD ? new FileStorageMemory() : new FileStorageStub();
-})();
+};
+
+export let fileStorage: FileStorage = getSupportedFileStorage({ tryOPFS: true });
+
+(async () => {
+    if (fileStorage.type === 'OPFS') {
+        try {
+            /** Checking if OPFS is indeed supported can only happen
+             * asynchronously to check for browser permissions. */
+            await navigator.storage.getDirectory();
+        } catch {
+            fileStorage = getSupportedFileStorage({ tryOPFS: false });
+            logger.info(`[fs::OPFS] OPFS not supported - switching to ${fileStorage.type}`);
+        } finally {
+            fileStorageReady.resolve(true);
+        }
+    }
+})().catch(noop);
