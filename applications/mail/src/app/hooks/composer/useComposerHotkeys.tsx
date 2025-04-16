05f401d2bd9bb5157d93d74050b5d576c5ed81f3
Romain Sanchez <romain.sanchez@proton.ch>
2025-04-16 14:12:28 +0000

    Remove quick reply

--- applications/mail/src/app/hooks/composer/useComposerHotkeys.tsx
+++ applications/mail/src/app/hooks/composer/useComposerHotkeys.tsx
@@ -1,17 +1,23 @@
 import type { MutableRefObject, RefObject } from 'react';
 import { useRef } from 'react';
 
-import type { EditorActions, HotkeyTuple } from '@proton/components';
+import type { HotkeyTuple } from '@proton/components';
 import { useHotkeys } from '@proton/components';
 import { isSafari as checkIsSafari } from '@proton/shared/lib/helpers/browser';
 import { editorShortcuts } from '@proton/shared/lib/shortcuts/mail';
 import noop from '@proton/utils/noop';
 
 import type { ExternalEditorActions } from '../../components/composer/editor/EditorWrapper';
-import { EditorTypes } from './useComposerContent';
 
-interface ComposerHotkeysHandlers {
-    type: EditorTypes.composer;
+export type EditorHotkeysHandlers = {
+    composerRef: RefObject<HTMLDivElement>;
+    handleSend: () => Promise<void>;
+    handleDelete: () => Promise<void>;
+    handleManualSave: () => Promise<void>;
+    toggleMaximized: () => void;
+    lock: boolean;
+    saving: boolean;
+    hasHotkeysEnabled: boolean;
     handleClose: () => Promise<void>;
     toggleMinimized: () => void;
     handlePassword: () => void;
@@ -21,22 +27,6 @@ interface ComposerHotkeysHandlers {
     isAssistantExpanded: boolean;
     closeAssistant: () => void;
     collapseAssistant: () => void;
-}
-
-interface QuickReplyHotkeysHandlers {
-    type: EditorTypes.quickReply;
-    editorRef: MutableRefObject<EditorActions | undefined>;
-}
-
-export type EditorHotkeysHandlers = (ComposerHotkeysHandlers | QuickReplyHotkeysHandlers) & {
-    composerRef: RefObject<HTMLDivElement>;
-    handleSend: () => Promise<void>;
-    handleDelete: () => Promise<void>;
-    handleManualSave: () => Promise<void>;
-    toggleMaximized: () => void;
-    lock: boolean;
-    saving: boolean;
-    hasHotkeysEnabled: boolean;
 };
 
 export const useComposerHotkeys = (args: EditorHotkeysHandlers) => {
@@ -49,29 +39,25 @@ export const useComposerHotkeys = (args: EditorHotkeysHandlers) => {
         lock,
         saving,
         hasHotkeysEnabled,
-        type,
     } = args;
     const isSafari = checkIsSafari();
 
-    const isComposer = type === EditorTypes.composer;
-    const isAssistantExpanded = isComposer && !!args.isAssistantExpanded;
+    const isAssistantExpanded = args.isAssistantExpanded;
 
     const attachmentTriggerRef = useRef<() => void>(noop);
 
     const keyHandlers = {
         close: async (e: KeyboardEvent) => {
-            if (isComposer) {
-                e.preventDefault();
-                e.stopPropagation();
-                if (args.isAssistantExpanded) {
-                    // When composer assistant is expanded, we want to close it when using "Esc"
-                    args.collapseAssistant();
-                } else {
-                    // We still need to close the assistant in that case to clean the context,
-                    // because we allow one assistant opened at the same time
-                    args.closeAssistant();
-                    await args.handleClose();
-                }
+            e.preventDefault();
+            e.stopPropagation();
+            if (args.isAssistantExpanded) {
+                // When composer assistant is expanded, we want to close it when using "Esc"
+                args.collapseAssistant();
+            } else {
+                // We still need to close the assistant in that case to clean the context,
+                // because we allow one assistant opened at the same time
+                args.closeAssistant();
+                await args.handleClose();
             }
         },
         send: async (e: KeyboardEvent) => {
@@ -94,13 +80,11 @@ export const useComposerHotkeys = (args: EditorHotkeysHandlers) => {
             }
         },
         minimize: (e: KeyboardEvent) => {
-            if (isComposer) {
-                e.preventDefault();
-                e.stopPropagation();
-                args.toggleMinimized();
-                // Focus minimize button on minimize with shortcut, otherwise focus would still be on Composer, and it's still possible to edit fields
-                args.minimizeButtonRef.current?.focus();
-            }
+            e.preventDefault();
+            e.stopPropagation();
+            args.toggleMinimized();
+            // Focus minimize button on minimize with shortcut, otherwise focus would still be on Composer, and it's still possible to edit fields
+            args.minimizeButtonRef.current?.focus();
         },
         maximize: (e: KeyboardEvent) => {
             e.preventDefault();
@@ -115,21 +99,21 @@ export const useComposerHotkeys = (args: EditorHotkeysHandlers) => {
             }
         },
         encrypt: (e: KeyboardEvent) => {
-            if (isComposer && !isAssistantExpanded) {
+            if (!isAssistantExpanded) {
                 e.preventDefault();
                 e.stopPropagation();
                 args.handlePassword();
             }
         },
         addExpiration: (e: KeyboardEvent) => {
-            if (isComposer && !isAssistantExpanded) {
+            if (!isAssistantExpanded) {
                 e.preventDefault();
                 e.stopPropagation();
                 args.handleExpiration();
             }
         },
         linkModal: (e: KeyboardEvent) => {
-            if (isComposer && args.editorRef?.current && !isAssistantExpanded) {
+            if (args.editorRef?.current && !isAssistantExpanded) {
                 e.preventDefault();
                 e.stopPropagation();
                 args.editorRef.current?.showLinkModal?.();
