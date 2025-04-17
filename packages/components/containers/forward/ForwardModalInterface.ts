bcfd8f3b475c923185150dd454c08e5bbdb7eb67
larabr <7375870+larabr@users.noreply.github.com>
2025-04-17 20:37:34 +0200

    ForwardModal: unify UI logic dealing with e2ee disabled info and primary key changes

--- packages/components/containers/forward/ForwardModalInterface.ts
+++ packages/components/containers/forward/ForwardModalInterface.ts
@@ -1,3 +1,5 @@
+import type { ReactNode } from 'react';
+
 import type { Condition, FilterStatement } from '@proton/components/containers/filters/interfaces';
 import type { PublicKeyReference } from '@proton/crypto';
 
@@ -19,6 +21,7 @@ export interface ForwardModalState {
     keyErrors?: string[];
     statement: FilterStatement;
     conditions: Condition[];
+    encryptionFixupDetails?: { setup: ReactNode; success: ReactNode } | null;
 }
 
 export interface ForwardModalKeyState {
