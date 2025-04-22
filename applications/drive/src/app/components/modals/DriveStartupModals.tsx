64b2ecc69a185c75fe52bd9de2fd16999a7c5b96
Pedro Lavarinhas Amaro <pedro.lavarinhas.amaro@proton.ch>
2025-04-22 14:05:42 +0000

    Refactor StartupModals

--- applications/drive/src/app/components/modals/DriveStartupModals.tsx
+++ applications/drive/src/app/components/modals/DriveStartupModals.tsx
@@ -1,115 +1,60 @@
-import { useEffect, useRef } from 'react';
-
 import { useWelcomeFlags } from '@proton/account';
-import { useSubscription } from '@proton/account/subscription/hooks';
 import {
-    CancellationReminderModal,
-    LightLabellingFeatureModal,
-    getShouldOpenReferralModal,
-    shouldOpenReminderModal,
+    StartupModals,
+    useCancellationReminderModal,
+    useLightLabellingFeatureModal,
     useModalState,
     useNewFeatureOnboarding,
-    useShowLightLabellingFeatureModal,
+    useReferralModal,
 } from '@proton/components';
-import type { ReminderFlag } from '@proton/components/containers/payments/subscription/cancellationReminder/cancellationReminderHelper';
-import { FeatureCode, useFeature } from '@proton/features';
-import { OPEN_OFFER_MODAL_EVENT } from '@proton/shared/lib/constants';
+import type { StartupModal } from '@proton/components';
 
 import { useDriveDocsFeatureFlag } from '../../store/_documents';
 import { DocsSuggestionsOnboardingModal } from './DocsSuggestionsOnboardingModal';
 import { DriveOnboardingV2Modal } from './DriveOnboardingV2Modal';
 
-const DriveStartupModals = () => {
+const useDriveOnboardingV2Modal: () => StartupModal = () => {
     const { welcomeFlags } = useWelcomeFlags();
+    const [modal, setModal, renderModal] = useModalState();
 
-    // Drive onboarding V2
-    const showWelcomeV2Modal = !welcomeFlags.isDone;
-    const [welcomeV2Modal, setWelcomeV2Modal, renderWelcomeV2Modal] = useModalState();
-
-    // Docs welcome modal
-    const { isDocsEnabled } = useDriveDocsFeatureFlag();
-    const { showOnboarding: showDocsSuggestionModeOnboarding, onWasShown: onDocsSuggestionModeOnboardingShown } =
-        useNewFeatureOnboarding({
-            key: 'drive-docs-suggestion-mode',
-            featureFlagsEnabled: isDocsEnabled,
-            shouldWelcomeFlowBeDone: true,
-            startDate: '2024-11-13',
-            expirationDate: '2024-12-15',
-        });
-    const [docsModal, setDocsModal, renderDocsModal] = useModalState({
-        onClose: onDocsSuggestionModeOnboardingShown,
-    });
-
-    // Referral modal
-    const [subscription, subscriptionLoading] = useSubscription();
-    const seenReferralModal = useFeature<boolean>(FeatureCode.SeenReferralModal);
-    const { open: showReferralModal } = getShouldOpenReferralModal({
-        subscription,
-        feature: seenReferralModal.feature,
-    });
-    const setReferralModal = () => {
-        document.dispatchEvent(new CustomEvent(OPEN_OFFER_MODAL_EVENT));
+    return {
+        showModal: !welcomeFlags.isDone,
+        activateModal: () => setModal(true),
+        component: renderModal ? <DriveOnboardingV2Modal {...modal} /> : null,
     };
+};
 
-    // Cancellation reminder modal
-    const { feature } = useFeature<ReminderFlag>(FeatureCode.AutoDowngradeReminder);
-    const [reminderModal, setReminderModal, renderReminderModal] = useModalState();
-    const showReminderModal = shouldOpenReminderModal(subscriptionLoading, subscription, feature);
+const useDocsSuggestionsOnboardingModal: () => StartupModal = () => {
+    const { isDocsEnabled } = useDriveDocsFeatureFlag();
+    const { showOnboarding, onWasShown: onDocsSuggestionModeOnboardingShown } = useNewFeatureOnboarding({
+        key: 'drive-docs-suggestion-mode',
+        featureFlagsEnabled: isDocsEnabled,
+        shouldWelcomeFlowBeDone: true,
+        startDate: '2024-11-13',
+        expirationDate: '2024-12-15',
+    });
+    const [modal, setModal, renderModal] = useModalState({ onClose: onDocsSuggestionModeOnboardingShown });
 
-    // Light labelling modal
-    const showLightLabellingFeatureModal = useShowLightLabellingFeatureModal();
-    const [lightLabellingFeatureModalProps, setLightLabellingFeatureModal, renderLightLabellingFeatureModal] =
-        useModalState();
+    return {
+        showModal: showOnboarding,
+        activateModal: () => setModal(true),
+        component: renderModal ? <DocsSuggestionsOnboardingModal {...modal} /> : null,
+    };
+};
 
-    // This ref ensures only one modal is shown at a time
-    const onceRef = useRef(false);
+const useStartupModals = () => {
+    const welcomeModal = useDriveOnboardingV2Modal();
+    const docsModal = useDocsSuggestionsOnboardingModal();
+    const reminderModal = useCancellationReminderModal();
+    const referralModal = useReferralModal();
+    const lightLabellingFeatureModal = useLightLabellingFeatureModal();
 
-    useEffect(() => {
-        if (onceRef.current) {
-            return;
-        }
+    return [welcomeModal, docsModal, reminderModal, referralModal, lightLabellingFeatureModal];
+};
 
-        // Modals by order of priority, top-most will be prioritized
-        const modals: [boolean, (value: boolean) => void][] = [
-            // Drive modals
-            [showWelcomeV2Modal, setWelcomeV2Modal],
-            // Docs modals
-            [showDocsSuggestionModeOnboarding, setDocsModal],
-            // Account modals
-            [showReminderModal, setReminderModal],
-            [showReferralModal, setReferralModal],
-            [showLightLabellingFeatureModal, setLightLabellingFeatureModal],
-        ];
-
-        for (const [show, setModalOpen] of modals) {
-            if (show) {
-                onceRef.current = true;
-                setModalOpen(true);
-
-                break;
-            }
-        }
-    }, [
-        showWelcomeV2Modal,
-        showReminderModal,
-        showReferralModal,
-        showLightLabellingFeatureModal,
-        showDocsSuggestionModeOnboarding,
-    ]);
-
-    return (
-        <>
-            {/* Drive modals */}
-            {renderWelcomeV2Modal && <DriveOnboardingV2Modal {...welcomeV2Modal} />}
-
-            {/* Docs modals */}
-            {renderDocsModal && <DocsSuggestionsOnboardingModal {...docsModal} />}
-
-            {/* Account modals */}
-            {renderReminderModal && <CancellationReminderModal {...reminderModal} />}
-            {renderLightLabellingFeatureModal && <LightLabellingFeatureModal {...lightLabellingFeatureModalProps} />}
-        </>
-    );
+const DriveStartupModals = () => {
+    const modals = useStartupModals();
+    return <StartupModals modals={modals} />;
 };
 
 export default DriveStartupModals;
--- packages/components/containers/desktop/freeTrial/InboxDesktopFreeTrialOnboardingModal.tsx
+++ packages/components/containers/desktop/freeTrial/InboxDesktopFreeTrialOnboardingModal.tsx
@@ -1,13 +1,10 @@
-import { useEffect } from 'react';
-
 import { c } from 'ttag';
 
-import { useUser } from '@proton/account/user/hooks';
 import { Button } from '@proton/atoms';
+import { type ModalProps } from '@proton/components/components/modalTwo/Modal';
 import ModalTwo from '@proton/components/components/modalTwo/Modal';
 import ModalTwoContent from '@proton/components/components/modalTwo/ModalContent';
 import ModalTwoFooter from '@proton/components/components/modalTwo/ModalFooter';
-import useModalState from '@proton/components/components/modalTwo/useModalState';
 import { APP_UPSELL_REF_PATH, MAIL_APP_NAME, MAIL_UPSELL_PATHS, UPSELL_COMPONENT } from '@proton/shared/lib/constants';
 import { getUpsellRef } from '@proton/shared/lib/helpers/upsell';
 import protonDesktop from '@proton/styles/assets/img/illustrations/proton-desktop.svg';
@@ -15,10 +12,9 @@ import protonDesktop from '@proton/styles/assets/img/illustrations/proton-deskto
 import { freeTrialUpgradeClick } from '../openExternalLink';
 import useInboxFreeTrial from './useInboxFreeTrial';
 
-export const InboxDesktopFreeTrialOnboardingModal = () => {
-    const { firstLogin, startFreeTrial } = useInboxFreeTrial();
-    const [user] = useUser();
-    const [modalState, setModalState, render] = useModalState();
+export const InboxDesktopFreeTrialOnboardingModal = ({ ...modalState }: ModalProps) => {
+    const { onClose } = modalState;
+    const { startFreeTrial } = useInboxFreeTrial();
 
     const upsellRef = getUpsellRef({
         app: APP_UPSELL_REF_PATH.INBOX_DESKTOP_REF_PATH,
@@ -26,55 +22,40 @@ export const InboxDesktopFreeTrialOnboardingModal = () => {
         feature: MAIL_UPSELL_PATHS.TRIAL_WELCOME,
     });
 
-    useEffect(() => {
-        if (firstLogin) {
-            setModalState(true);
-        }
-    }, [firstLogin]);
-
-    if (user.hasPaidMail) {
-        return null;
-    }
-
     const handleClose = () => {
-        setModalState(false);
+        onClose?.();
         startFreeTrial();
     };
 
     return (
-        <>
-            {render && (
-                <ModalTwo size="small" {...modalState} onClose={startFreeTrial}>
-                    <ModalTwoContent className="text-center mt-8 mb-4 flex gap-6 flex-column">
-                        <img src={protonDesktop} alt={c('Free trial desktop').t`${MAIL_APP_NAME} desktop app`} />
-                        <div>
-                            <h1 className="text-bold mb-2 text-2xl">{c('Free trial desktop')
-                                .t`Try the new desktop app`}</h1>
-                            <p className="my-0">{c('Free trial desktop')
-                                .t`Enjoy fast, secure, and distraction-free access to your inbox and calendar.`}</p>
-                        </div>
-                        <div>
-                            <Button color="norm" size="large" onClick={handleClose} fullWidth>{c('Free trial desktop')
-                                .t`Start free trial`}</Button>
-                            <div className="text-center flex flex-column color-weak text-sm gap-0.5 mt-4">
-                                <p className="m-0">{c('Free trial desktop').t`14-day desktop app trial.`}</p>
-                                <p className="m-0">{c('Free trial desktop').t`No credit card required.`}</p>
-                            </div>
-                        </div>
-                    </ModalTwoContent>
-                    <ModalTwoFooter className="flex text-sm text-center mb-8 flex-column mt-0">
-                        <hr className="mb-4" />
-                        <p className="m-0 color-weak">{c('Free trial desktop')
-                            .t`Get unlimited access with any ${MAIL_APP_NAME} plan.`}</p>
-                        <Button
-                            color="norm"
-                            shape="underline"
-                            className="m-0 p-0 text-semibold"
-                            onClick={() => freeTrialUpgradeClick(upsellRef)}
-                        >{c('Free trial desktop').t`Upgrade now`}</Button>
-                    </ModalTwoFooter>
-                </ModalTwo>
-            )}
-        </>
+        <ModalTwo size="small" {...modalState} onClose={startFreeTrial}>
+            <ModalTwoContent className="text-center mt-8 mb-4 flex gap-6 flex-column">
+                <img src={protonDesktop} alt={c('Free trial desktop').t`${MAIL_APP_NAME} desktop app`} />
+                <div>
+                    <h1 className="text-bold mb-2 text-2xl">{c('Free trial desktop').t`Try the new desktop app`}</h1>
+                    <p className="my-0">{c('Free trial desktop')
+                        .t`Enjoy fast, secure, and distraction-free access to your inbox and calendar.`}</p>
+                </div>
+                <div>
+                    <Button color="norm" size="large" onClick={handleClose} fullWidth>{c('Free trial desktop')
+                        .t`Start free trial`}</Button>
+                    <div className="text-center flex flex-column color-weak text-sm gap-0.5 mt-4">
+                        <p className="m-0">{c('Free trial desktop').t`14-day desktop app trial.`}</p>
+                        <p className="m-0">{c('Free trial desktop').t`No credit card required.`}</p>
+                    </div>
+                </div>
+            </ModalTwoContent>
+            <ModalTwoFooter className="flex text-sm text-center mb-8 flex-column mt-0">
+                <hr className="mb-4" />
+                <p className="m-0 color-weak">{c('Free trial desktop')
+                    .t`Get unlimited access with any ${MAIL_APP_NAME} plan.`}</p>
+                <Button
+                    color="norm"
+                    shape="underline"
+                    className="m-0 p-0 text-semibold"
+                    onClick={() => freeTrialUpgradeClick(upsellRef)}
+                >{c('Free trial desktop').t`Upgrade now`}</Button>
+            </ModalTwoFooter>
+        </ModalTwo>
     );
 };
