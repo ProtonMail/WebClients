ee0f2e46860b5f967ffe845a8e0e76a1a5d95f9c
Riste Bozhinov <risto.bozhinov@proton.ch>
2025-04-23 16:08:15 +0000

    Update photos migration copy

--- applications/drive/src/app/photos/PhotosWithAlbums/components/PhotosMigrationView.tsx
+++ applications/drive/src/app/photos/PhotosWithAlbums/components/PhotosMigrationView.tsx
@@ -4,14 +4,13 @@ import drivePhotosMigration from '@proton/styles/assets/img/drive/drive-photos-m
 
 export const PhotosMigrationView = () => (
     <div
-        className="m-auto flex flex-column items-center justify-center gap-2 max-w-custom"
+        className="m-auto flex flex-column items-center justify-center gap-2 max-w-custom text-center"
         style={{
-            '--max-w-custom': '19.375rem',
+            '--max-w-custom': '21rem',
         }}
     >
-        <img src={drivePhotosMigration} alt="" />
-        <h3 className="text-bold text-xxl">{c('Title').t`Updating your photos`}</h3>
-        <h4 className="text-lg text-center">{c('Info')
-            .t`Please wait while we update your data this may take a few seconds.`}</h4>
+        <img src={drivePhotosMigration} className="px-3" alt="" />
+        <h3 className="text-bold text-xxl">{c('Title').t`Upgrading your Photos experience`}</h3>
+        <h4 className="text-lg">{c('Info').t`Setting up Albums & Filters. This might take a few minutes.`}</h4>
     </div>
 );
