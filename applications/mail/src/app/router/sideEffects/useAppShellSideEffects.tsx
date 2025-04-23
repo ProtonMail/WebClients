ec6273adf1e79f6c25b4f38824ff44b537c938e0
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-04-23 11:39:00 +0200

    Create side effect hooks to have common logic

---
+++ applications/mail/src/app/router/sideEffects/useAppShellSideEffects.tsx
@@ -0,0 +1,41 @@
+import { useOpenDrawerOnLoad } from '@proton/components/index';
+
+import { useContactsListener } from 'proton-mail/hooks/contact/useContactsListener';
+import { useConversationsEvent } from 'proton-mail/hooks/events/useConversationsEvents';
+import { useMessagesEvents } from 'proton-mail/hooks/events/useMessagesEvents';
+import useIncomingDefaultsEvents from 'proton-mail/hooks/incomingDefaults/useIncomingDefaultsEvents';
+import useIncomingDefaultsLoad from 'proton-mail/hooks/incomingDefaults/useIncomingDefaultsLoad';
+import { usePageHotkeys } from 'proton-mail/hooks/mailbox/usePageHotkeys';
+import useComposerEvent from 'proton-mail/hooks/useComposerEvent';
+import { useMailPTTMetric } from 'proton-mail/metrics/useMailPTTMetric';
+
+interface Props {
+    openShortcutsModal: (value: boolean) => void;
+}
+
+/**
+ * This hook is temporary, it's used to ensure the behavior
+ * is the same while we release the mailbox refactoring.
+ * The hook will be removed once we delete the `MailboxContainer`
+ */
+export const useAppShellSideEffects = ({ openShortcutsModal }: Props) => {
+    useOpenDrawerOnLoad();
+
+    useContactsListener();
+    useConversationsEvent();
+    useMessagesEvents();
+
+    useMailPTTMetric();
+
+    /**
+     * Incoming defaults
+     * - cache loading
+     * - events subscription
+     */
+    useIncomingDefaultsLoad();
+    useIncomingDefaultsEvents();
+
+    useComposerEvent();
+
+    usePageHotkeys({ onOpenShortcutsModal: () => openShortcutsModal(true) });
+};
