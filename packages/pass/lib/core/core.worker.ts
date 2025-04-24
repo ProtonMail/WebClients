ffc163c50e1a4cc38e032c5a3673eaeadabfaf37
Edvin CANDON <edvin.candon@proton.ch>
2025-04-24 09:10:13 +0200

    Create generic `WasmWorkerService` factory
    - Properly forward WASM error messages from workers
    - Leverage `wasmWorkerServiceFactory` for PassUI and PassCore
    
    [IDTEAM-4698]

--- packages/pass/lib/core/core.worker.ts
+++ packages/pass/lib/core/core.worker.ts
@@ -1,12 +1,25 @@
 import * as PassRustWorker from '@protontech/pass-rust-core/worker';
 
 import { WASM_WORKER_READY_EVENT } from '@proton/pass/lib/core/constants';
+import { getErrorMessage } from '@proton/pass/utils/errors/get-error-message';
 
-import type { PassCoreMethod, PassCoreRPC } from './core.types';
+import type { PassCoreMessageEvent, PassCoreMethod, PassCoreRPC, PassCoreResult } from './core.types';
 
 self.postMessage({ type: WASM_WORKER_READY_EVENT });
 
 self.onmessage = ({ data: { method, args }, ports }: MessageEvent<PassCoreRPC<PassCoreMethod>>) => {
     const port = ports?.[0];
-    if (port) port.postMessage((PassRustWorker[method] as any)(...args));
+
+    if (port) {
+        port.postMessage(
+            ((): PassCoreMessageEvent<PassCoreMethod> => {
+                try {
+                    const value = (PassRustWorker[method] as any)(...args) as PassCoreResult<PassCoreMethod>;
+                    return { ok: true, value };
+                } catch (err) {
+                    return { ok: false, error: getErrorMessage(err) };
+                }
+            })()
+        );
+    }
 };
