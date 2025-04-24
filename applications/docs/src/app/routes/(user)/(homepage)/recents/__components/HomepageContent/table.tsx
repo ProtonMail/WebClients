7de30c7bca621180e3af69aa89958c5d2348b4d2
Daniel Guardiola <daniel.guardiola@proton.ch>
2025-04-24 13:16:46 +0000

    Docs homepage: 1.0 RC1

--- applications/docs/src/app/routes/(user)/(homepage)/recents/__components/HomepageContent/table.tsx
+++ applications/docs/src/app/routes/(user)/(homepage)/recents/__components/HomepageContent/table.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable jsx-a11y/prefer-tag-over-role */
 import clsx from '@proton/utils/clsx'
 import type { ComponentPropsWithoutRef, ReactNode } from 'react'
 
 const POSITIONAL_CELL_CLASSES = [
-  '[&:nth-child(1)]:w-full [&:nth-child(1)]:px-6',
+  '[&:nth-child(1)]:grow [&:nth-child(1)]:px-6 [&:nth-child(1)]:basis-0',
   '[&:nth-child(2)]:w-[9rem] xlarge:[&:nth-child(2)]:w-[12rem] xxlarge:[&:nth-child(2)]:w-[18rem] [&:nth-child(2)]:px-2',
   '[&:nth-child(3)]:w-[14rem] large:[&:nth-child(3)]:w-[16rem] xlarge:[&:nth-child(3)]:w-[20rem] xxlarge:[&:nth-child(3)]:w-[26rem] [&:nth-child(3)]:px-2',
   '[&:nth-child(4)]:w-[13rem] large:[&:nth-child(4)]:w-[14rem] xlarge:[&:nth-child(4)]:w-[16rem] xxlarge:[&:nth-child(4)]:w-[22rem] [&:nth-child(4)]:pe-5 [&:nth-child(4)]:ps-2',
@@ -15,11 +16,34 @@ const TARGET_CLASSES: Record<Target, string | undefined> = {
   large: 'hidden large:!table-cell',
 }
 
-export type HeaderProps = ComponentPropsWithoutRef<'th'> & { target?: Target }
+export interface TableProps extends ComponentPropsWithoutRef<'div'> {}
+
+export function Table(props: TableProps) {
+  return <div role="table" {...props} className={clsx('text-[14px]', props.className)} />
+}
+
+export interface HeadProps extends ComponentPropsWithoutRef<'div'> {
+  children: ReactNode
+}
+
+export function Head({ children, ...props }: HeadProps) {
+  return (
+    <div role="rowgroup" {...props} className={clsx('sticky left-0 top-0', props.className)}>
+      <div role="row" className="flex h-[3.75rem] items-center text-left">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export interface HeaderProps extends ComponentPropsWithoutRef<'div'> {
+  target?: Target
+}
 
 export function Header({ target = 'all', ...props }: HeaderProps) {
   return (
-    <th
+    <div
+      role="columnheader"
       {...props}
       className={clsx(
         'whitespace-nowrap font-semibold before:absolute before:inset-0 before:z-[-1] before:rounded-xl before:bg-[#fff] [&:nth-child(1)]:text-[1rem]',
@@ -31,28 +55,34 @@ export function Header({ target = 'all', ...props }: HeaderProps) {
   )
 }
 
-export type HeadProps = ComponentPropsWithoutRef<'thead'> & { children: ReactNode }
+export interface BodyProps extends ComponentPropsWithoutRef<'div'> {}
 
-export function Head({ children, ...props }: HeadProps) {
-  return (
-    <thead {...props} className={clsx('sticky left-0 top-0 h-[3.75rem]', props.className)}>
-      <tr className="text-left">{children}</tr>
-    </thead>
-  )
+export function Body(props: BodyProps) {
+  return <div role="rowgroup" {...props} />
 }
 
-export type DataCellProps = ComponentPropsWithoutRef<'td'> & { target?: Target }
-
-export function DataCell({ target = 'all', ...props }: DataCellProps) {
-  return <td {...props} className={clsx(...POSITIONAL_CELL_CLASSES, TARGET_CLASSES[target], props.className)} />
+export interface RowProps extends ComponentPropsWithoutRef<'div'> {
+  children: ReactNode
 }
-
-export type RowProps = ComponentPropsWithoutRef<'tr'> & { children: ReactNode }
 
 export function Row({ children, ...props }: RowProps) {
   return (
-    <tr {...props} className={clsx('h-[3.375rem]', props.className)}>
+    <div role="row" {...props} className={clsx('flex h-[3.375rem] flex-nowrap items-center', props.className)}>
       {children}
-    </tr>
+    </div>
+  )
+}
+
+export interface DataCellProps extends ComponentPropsWithoutRef<'div'> {
+  target?: Target
+}
+
+export function DataCell({ target = 'all', ...props }: DataCellProps) {
+  return (
+    <div
+      role="cell"
+      {...props}
+      className={clsx('min-w-0', ...POSITIONAL_CELL_CLASSES, TARGET_CLASSES[target], props.className)}
+    />
   )
 }
