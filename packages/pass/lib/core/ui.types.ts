453bfbf611b7676775277e187c186d3b2b88f81c
Edvin CANDON <edvin.candon@proton.ch>
2025-04-24 09:10:13 +0200

    Create generic `WasmWorkerService` factory
    - Properly forward WASM error messages from workers
    - Leverage `wasmWorkerServiceFactory` for PassUI and PassCore
    
    [IDTEAM-4698]

--- packages/pass/lib/core/ui.types.ts
+++ packages/pass/lib/core/ui.types.ts
@@ -1,15 +1,22 @@
 import type * as PassUIWorker from '@protontech/pass-rust-core/ui';
 
+import type { WasmWorkerService } from '@proton/pass/lib/core/wasm.worker.service';
+import type { Result } from '@proton/pass/types';
+
 export type PassUI = typeof PassUIWorker;
 export type PassUIMethod = keyof { [K in keyof PassUI]: PassUI[K] extends Function ? K : never };
 export type PassUIParams<T extends PassUIMethod> = Parameters<PassUI[T]>;
 export type PassUIResult<T extends PassUIMethod> = ReturnType<PassUI[T]>;
+export type PassUIMessageEvent<T extends PassUIMethod> = Result<{ value: PassUIResult<T> }>;
 export type PassUIRPC<T extends PassUIMethod> = { method: T; args: PassUIParams<T> };
 export type PassUIProxy = { [K in PassUIMethod]: (...params: PassUIParams<K>) => Promise<PassUIResult<K>> };
 
-export interface PassUIService {
-    exec: <T extends PassUIMethod>(method: T, ...params: PassUIParams<T>) => Promise<PassUIResult<T>>;
-    transfer: (
-        transferable: Transferable[]
-    ) => <T extends PassUIMethod>(method: T, ...params: PassUIParams<T>) => Promise<PassUIResult<T>>;
-}
+export type PassUIMethodMap = {
+    [K in PassUIMethod]: {
+        args: PassUIParams<K>;
+        return: PassUIResult<K>;
+    };
+};
+
+export type PassUIWorkerService = WasmWorkerService<PassUIMethodMap>;
+export type PassUIService = Pick<PassUIWorkerService, 'exec'>;
