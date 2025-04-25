3c696cde72d5284af6b9777827e267843894b3d5
Qwerty (Vítězslav Ackermann Ferko) <qwerty@qwerty.xyz>
2025-04-25 03:11:19 +0200

    ﻿QA fixes
    1)
    - fix publish banner responsive height
    - fix null component key in Policy list
    - count countries and not cities for total
    - add Discard Modal when clicking on Cancel during editing policy
    
    2)
    - prevent word wrap in Dropdowns
    - allow Edit button to grow
    - improve Publish Banner alignment

---
+++ packages/components/containers/vpn/sharedServers/PolicyModal/DiscardModal.tsx
@@ -0,0 +1,38 @@
+import { useCallback } from 'react';
+
+import { c } from 'ttag';
+
+import { Button } from '@proton/atoms';
+import Form from '@proton/components/components/form/Form';
+import type { ModalProps } from '@proton/components/components/modalTwo/Modal';
+import ModalTwo from '@proton/components/components/modalTwo/Modal';
+import ModalTwoFooter from '@proton/components/components/modalTwo/ModalFooter';
+import ModalTwoHeader from '@proton/components/components/modalTwo/ModalHeader';
+
+interface DiscardModalProps extends ModalProps {
+    onSuccess: () => void;
+}
+
+const DiscardModal = ({ onSuccess, ...rest }: DiscardModalProps) => {
+    const handleConfirm = useCallback(async () => {
+        onSuccess();
+        rest.onClose?.();
+    }, [onSuccess, rest]);
+
+    return (
+        <ModalTwo size="small" as={Form} {...rest}>
+            <ModalTwoHeader title={c('Title').t`Discard changes?`} />
+
+            <ModalTwoFooter className="flex flex-column">
+                <Button color="danger" type="button" onClick={handleConfirm}>
+                    {c('Action').t`Discard changes`}
+                </Button>
+                <Button color="weak" type="button" onClick={rest.onClose}>
+                    {c('Action').t`Keep editing`}
+                </Button>
+            </ModalTwoFooter>
+        </ModalTwo>
+    );
+};
+
+export default DiscardModal;
