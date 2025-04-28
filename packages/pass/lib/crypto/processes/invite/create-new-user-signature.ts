4777c4458fc94044058e7a99da9805b6e8c805ca
Edvin CANDON <edvin.candon@proton.ch>
2025-04-28 08:58:25 +0200

    Use rust `create_new_user_invite_signature_body` when creating new user invites
    [IDTEAM-4705]

--- packages/pass/lib/crypto/processes/invite/create-new-user-signature.ts
+++ packages/pass/lib/crypto/processes/invite/create-new-user-signature.ts
@@ -1,6 +1,7 @@
 import { CryptoProxy, type PrivateKeyReference } from '@proton/crypto/lib';
+import type { PassCoreProxy } from '@proton/pass/lib/core/core.types';
 import { PassSignatureContext, type VaultShareKey } from '@proton/pass/types';
-import { stringToUint8Array, uint8ArrayToBase64String } from '@proton/shared/lib/helpers/encoding';
+import { uint8ArrayToBase64String } from '@proton/shared/lib/helpers/encoding';
 
 type CreateNewUserSignatureProcessParams = {
     invitedEmail: string;
@@ -8,37 +9,24 @@ type CreateNewUserSignatureProcessParams = {
     shareKey: VaultShareKey;
 };
 
-type CreateNewUserSignatureBodyProcessParams = Omit<CreateNewUserSignatureProcessParams, 'inviterPrivateKey'>;
+export const createNewUserSignatureFactory =
+    (core: PassCoreProxy) =>
+    async (params: CreateNewUserSignatureProcessParams): Promise<string> => {
+        const signatureBody = await core.create_new_user_invite_signature_body(
+            params.invitedEmail,
+            params.shareKey.raw
+        );
 
-/* FIXME: use common rust library once integrated for generating
- * the signature body payload  */
-export const createNewUserSignatureBody = ({
-    invitedEmail,
-    shareKey,
-}: CreateNewUserSignatureBodyProcessParams): Uint8Array => {
-    const bEmail = stringToUint8Array(invitedEmail);
-    const bSeperator = stringToUint8Array('|');
-    const bShareKey = shareKey.raw;
+        const signature = await CryptoProxy.signMessage({
+            binaryData: signatureBody,
+            signingKeys: [params.inviterPrivateKey],
+            format: 'binary',
+            detached: true,
+            signatureContext: {
+                value: PassSignatureContext.VaultInviteNewUser,
+                critical: true,
+            },
+        });
 
-    const binaryData = new Uint8Array(bEmail.length + bSeperator.length + bShareKey.length);
-    binaryData.set(bEmail, 0);
-    binaryData.set(bSeperator, bEmail.length);
-    binaryData.set(bShareKey, bEmail.length + bSeperator.length);
-
-    return binaryData;
-};
-
-export const createNewUserSignature = async (params: CreateNewUserSignatureProcessParams): Promise<string> => {
-    const signature = await CryptoProxy.signMessage({
-        binaryData: createNewUserSignatureBody(params),
-        signingKeys: [params.inviterPrivateKey],
-        format: 'binary',
-        detached: true,
-        signatureContext: {
-            value: PassSignatureContext.VaultInviteNewUser,
-            critical: true,
-        },
-    });
-
-    return uint8ArrayToBase64String(signature);
-};
+        return uint8ArrayToBase64String(signature);
+    };
