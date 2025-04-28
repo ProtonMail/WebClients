e3e201c7adceb4a583e77adef6ced12e2dba0562
Paolo Chiabrera <paolo.chiabrera@proton.ch>
2025-04-28 18:56:24 +0000

    MAILWEB-6560: Add visual testing to Storybook v8

---
+++ packages/storybook/tests/atoms-visual.test.ts
@@ -0,0 +1,41 @@
+import { expect, test } from '@playwright/test';
+import { existsSync, readFileSync } from 'node:fs';
+import { resolve } from 'node:path';
+
+function getStorybooIds() {
+    const storybookStaticFolder = resolve(__dirname, '../storybook-static');
+
+    if (!existsSync(storybookStaticFolder)) {
+        throw new Error('storybook-static folder does not exist, try to run "yarn storybook:build" first.');
+    }
+
+    const content = readFileSync(resolve(storybookStaticFolder, './index.json'), 'utf-8');
+    let parsedContent;
+
+    try {
+        parsedContent = JSON.parse(content);
+    } catch (err) {
+        throw new Error('Storybook index.json is not valid JSON.');
+    }
+
+    const ids = Object.keys(parsedContent.entries).filter((id: string) => id.endsWith('--docs'));
+
+    return ids;
+}
+
+test.describe('visual tests', () => {
+    const ids = getStorybooIds();
+
+    for (const id of ids) {
+        // NOTE: this is necessary otherwise the snapshot file name will be invalid
+        const testName = id.replaceAll('-', '_');
+
+        test(testName, async ({ page }) => {
+            await page.goto(`/iframe.html?viewMode=docs&id=${id}`);
+
+            await expect(page).toHaveScreenshot({
+                fullPage: true,
+            });
+        });
+    }
+});
