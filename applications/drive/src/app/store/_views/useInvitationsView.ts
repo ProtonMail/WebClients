94a71932bdd2f97f4baa6e10adc2c9de6accb28c
Lucien Peslier <lucien.peslier@proton.ch>
2025-04-29 12:55:29 +0000

    [DRVWEB-4716] Check if invitee have access to album feature to list and accept invite via link

--- applications/drive/src/app/store/_views/useInvitationsView.ts
+++ applications/drive/src/app/store/_views/useInvitationsView.ts
@@ -1,23 +1,35 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import useLoading from '@proton/hooks/useLoading';
 import { EVENT_ACTIONS } from '@proton/shared/lib/constants';
+import { LinkType } from '@proton/shared/lib/interfaces/drive/link';
+import { VolumeType } from '@proton/shared/lib/interfaces/drive/volume';
 
 import type { SharedWithMeItem } from '../../components/sections/SharedWithMe/SharedWithMe';
 import { sendErrorReport } from '../../utils/errorHandling';
 import { useDriveEventManager } from '../_events';
 import { useInvitationsListing } from '../_invitations/useInvitationsListing';
+import { useUserSettings } from '../_settings';
+import { useDefaultShare } from '../_shares';
+import { useMemoArrayNoMatterTheOrder } from './utils';
 
 export const useInvitationsView = () => {
     const [isLoading, withLoading] = useLoading(true);
     const { getCachedInvitations, loadInvitations } = useInvitationsListing();
     const driveEventManager = useDriveEventManager();
+    const [showPhotosWithAlbums, setShowPhotosWithAlbums] = useState<undefined | boolean>();
 
     const cachedInvitations = getCachedInvitations();
+    const invitations = useMemoArrayNoMatterTheOrder(
+        cachedInvitations.filter((invitation) => invitation.link.type !== LinkType.ALBUM || showPhotosWithAlbums)
+    );
+    const { getDefaultPhotosShare } = useDefaultShare();
+
+    const { photosWithAlbumsEnabled } = useUserSettings();
 
     const invitationsBrowserItems: SharedWithMeItem[] = useMemo(
         () =>
-            cachedInvitations.reduce<SharedWithMeItem[]>((acc, item) => {
+            invitations.reduce<SharedWithMeItem[]>((acc, item) => {
                 acc.push({
                     isFile: item.link.isFile,
                     trashed: null,
@@ -37,7 +49,7 @@ export const useInvitationsView = () => {
                 });
                 return acc;
             }, []),
-        [cachedInvitations]
+        [invitations]
     );
 
     useEffect(() => {
@@ -57,10 +69,18 @@ export const useInvitationsView = () => {
         const abortController = new AbortController();
         void withLoading(loadInvitations(abortController.signal).catch(sendErrorReport));
 
+        if (photosWithAlbumsEnabled) {
+            setShowPhotosWithAlbums(true);
+        } else {
+            void getDefaultPhotosShare(abortController.signal).then((photosShare) =>
+                setShowPhotosWithAlbums(photosShare?.volumeType === VolumeType.Photos)
+            );
+        }
+
         return () => {
             abortController.abort();
         };
     }, []);
 
-    return { invitations: cachedInvitations, invitationsBrowserItems, isLoading };
+    return { invitations, invitationsBrowserItems, isLoading };
 };
