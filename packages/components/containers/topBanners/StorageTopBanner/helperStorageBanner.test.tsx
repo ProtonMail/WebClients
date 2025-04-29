30d2370846bb2fa5824e3277361f54eea6ccafd6
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-04-29 14:22:04 +0200

    Add unit test on existing copy

---
+++ packages/components/containers/topBanners/StorageTopBanner/helperStorageBanner.test.tsx
@@ -0,0 +1,189 @@
+import { getStorageFull } from './helperStorageBanner';
+
+describe('getStorageFull', () => {
+    describe('Mode drive', () => {
+        it('Should return drive string for full storage', () => {
+            const cta = 'Upgrade';
+
+            const string = getStorageFull({
+                percentage: 110,
+                mode: 'drive',
+                app: 'proton-drive',
+                upgrade: cta,
+            });
+
+            expect((string as string[]).join('')).toBe(
+                `Your Drive storage is full. To upload or sync files, free up space or ${cta}.`
+            );
+        });
+
+        it('Should return drive string for near full storage', () => {
+            const cta = 'Upgrade';
+
+            const string = getStorageFull({
+                percentage: 90,
+                mode: 'drive',
+                app: 'proton-drive',
+                upgrade: cta,
+            });
+
+            expect((string as string[]).join('')).toBe(
+                `Your Drive storage is 90% full. To upload or sync files, free up space or ${cta}.`
+            );
+        });
+    });
+
+    describe('Mode mail', () => {
+        it('Should return mail string for full storage', () => {
+            const cta = 'Upgrade';
+
+            const string = getStorageFull({
+                percentage: 110,
+                mode: 'mail',
+                app: 'proton-mail',
+                upgrade: cta,
+            });
+
+            expect((string as string[]).join('')).toBe(
+                `Your Mail storage is full. To send or receive emails, free up space or ${cta}.`
+            );
+        });
+
+        it('Should return mail string for near full storage', () => {
+            const cta = 'Upgrade';
+
+            const string = getStorageFull({
+                percentage: 90,
+                mode: 'mail',
+                app: 'proton-mail',
+                upgrade: cta,
+            });
+
+            expect((string as string[]).join('')).toBe(
+                `Your Mail storage is 90% full. To send or receive emails, free up space or ${cta}.`
+            );
+        });
+    });
+
+    describe('Mode both', () => {
+        it('Should return drive string for full storage', () => {
+            const cta = 'Upgrade';
+
+            const string = getStorageFull({
+                percentage: 110,
+                mode: 'drive',
+                app: 'proton-drive',
+                upgrade: cta,
+            });
+
+            expect((string as string[]).join('')).toBe(
+                `Your Drive storage is full. To upload or sync files, free up space or ${cta}.`
+            );
+        });
+
+        it('Should return drive string for near full storage', () => {
+            const cta = 'Upgrade';
+
+            const string = getStorageFull({
+                percentage: 90,
+                mode: 'drive',
+                app: 'proton-drive',
+                upgrade: cta,
+            });
+
+            expect((string as string[]).join('')).toBe(
+                `Your Drive storage is 90% full. To upload or sync files, free up space or ${cta}.`
+            );
+        });
+
+        it('Should return drive string for full storage when on Mail app', () => {
+            const cta = 'Upgrade';
+
+            const string = getStorageFull({
+                percentage: 110,
+                mode: 'drive',
+                app: 'proton-mail',
+                upgrade: cta,
+            });
+
+            expect((string as string[]).join('')).toBe(
+                `Your Drive storage is full. To upload or sync files, free up space or ${cta}.`
+            );
+        });
+
+        it('Should return drive string for near full storage when on Mail app', () => {
+            const cta = 'Upgrade';
+
+            const string = getStorageFull({
+                percentage: 90,
+                mode: 'drive',
+                app: 'proton-mail',
+                upgrade: cta,
+            });
+
+            expect((string as string[]).join('')).toBe(
+                `Your Drive storage is 90% full. To upload or sync files, free up space or ${cta}.`
+            );
+        });
+
+        it('Should return mail string for full storage', () => {
+            const cta = 'Upgrade';
+
+            const string = getStorageFull({
+                percentage: 110,
+                mode: 'mail',
+                app: 'proton-mail',
+                upgrade: cta,
+            });
+
+            expect((string as string[]).join('')).toBe(
+                `Your Mail storage is full. To send or receive emails, free up space or ${cta}.`
+            );
+        });
+
+        it('Should return mail string for near full storage', () => {
+            const cta = 'Upgrade';
+
+            const string = getStorageFull({
+                percentage: 90,
+                mode: 'mail',
+                app: 'proton-mail',
+                upgrade: cta,
+            });
+
+            expect((string as string[]).join('')).toBe(
+                `Your Mail storage is 90% full. To send or receive emails, free up space or ${cta}.`
+            );
+        });
+
+        it('Should return mail string for full storage when on Drive app', () => {
+            const cta = 'Upgrade';
+
+            const string = getStorageFull({
+                percentage: 110,
+                mode: 'mail',
+                app: 'proton-drive',
+                upgrade: cta,
+            });
+
+            expect((string as string[]).join('')).toBe(
+                `Your Mail storage is full. To send or receive emails, free up space or ${cta}.`
+            );
+        });
+
+        it('Should return drive string for near full storage when on Mail app', () => {
+            const cta = 'Upgrade';
+
+            const string = getStorageFull({
+                percentage: 90,
+                mode: 'mail',
+                app: 'proton-drive',
+                upgrade: cta,
+            });
+
+            expect((string as string[]).join('')).toBe(
+                `Your Mail storage is 90% full. To send or receive emails, free up space or ${cta}.`
+            );
+        });
+    });
+});
