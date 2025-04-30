131bde875781d119b9dcffedbd63c202afc0717b
Lucien Peslier <lucien.peslier@proton.ch>
2025-04-30 08:43:03 +0000

    [DRVWEB-4730] Update copy for Migration view

--- applications/drive/src/app/photos/PhotosWithAlbums/components/PhotosMigrationView.tsx
+++ applications/drive/src/app/photos/PhotosWithAlbums/components/PhotosMigrationView.tsx
@@ -3,14 +3,19 @@ import { c } from 'ttag';
 import drivePhotosMigration from '@proton/styles/assets/img/drive/drive-photos-migration.svg';
 
 export const PhotosMigrationView = () => (
-    <div
-        className="m-auto flex flex-column items-center justify-center gap-2 max-w-custom text-center"
-        style={{
-            '--max-w-custom': '21rem',
-        }}
-    >
-        <img src={drivePhotosMigration} className="px-3" alt="" />
-        <h3 className="text-bold text-xxl">{c('Title').t`Upgrading your Photos experience`}</h3>
-        <h4 className="text-lg">{c('Info').t`Setting up Albums & Filters. This might take a few minutes.`}</h4>
+    <div className="m-auto flex flex-column items-center justify-center gap-2 text-center">
+        <img
+            src={drivePhotosMigration}
+            className="px-3 max-w-custom"
+            style={{
+                '--max-w-custom': '21rem',
+            }}
+            alt=""
+        />
+        <h3 className="text-bold text-xxl">{c('Title').t`Setting up Albums & Filters`}</h3>
+        <p className="text-lg m-0 flex flex-column items-center">
+            <span>{c('Info').t`If you have a large photo library, it might take some time.`}</span>
+            <span>{c('Info').t`You can close the tab. We'll work on it in the background.`}</span>
+        </p>
     </div>
 );
