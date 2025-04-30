1e0ea429ba5fcdce318be528c12e31a96715e279
Edvin CANDON <edvin.candon@proton.ch>
2025-04-30 11:56:18 +0200

    Improve type-safety on IPC messaging layer
    [IDTEAM-4721]

--- applications/pass-desktop/src/lib/install-info.ts
+++ applications/pass-desktop/src/lib/install-info.ts
@@ -1,6 +1,15 @@
+import type { MaybeNull } from '@proton/pass/types';
+
 import { store } from '../store';
 import logger from '../utils/logger';
 import { setupIpcHandler } from './ipc';
+
+declare module 'proton-pass-desktop/lib/ipc' {
+    interface IPCChannels {
+        'installInfo:getInfo': IPCChannel<[], MaybeNull<string>>;
+        'installInfo:setInstallSourceReported': IPCChannel<[], void>;
+    }
+}
 
 export type StoreInstallProperties = {
     source: string | null;
@@ -40,6 +49,6 @@ export function setInstallSourceReported() {
 
 export function setupIpcHandlers() {
     const installSource = getInstallSource();
-    setupIpcHandler('installInfo:getInfo', () => Promise.resolve({ installSource }));
+    setupIpcHandler('installInfo:getInfo', () => installSource);
     setupIpcHandler('installInfo:setInstallSourceReported', () => setInstallSourceReported());
 }
--- applications/pass-desktop/src/lib/theming.ts
+++ applications/pass-desktop/src/lib/theming.ts
@@ -5,6 +5,13 @@ import type { DesktopTheme } from '@proton/pass/types';
 import { store } from '../store';
 import { setupIpcHandler } from './ipc';
 
+declare module 'proton-pass-desktop/lib/ipc' {
+    interface IPCChannels {
+        'theming:setTheme': IPCChannel<[theme: DesktopTheme], void>;
+        'theming:getTheme': IPCChannel<[], DesktopTheme>;
+    }
+}
+
 export const setTheme = (theme: DesktopTheme) => {
     store.set('theme', theme);
     nativeTheme.themeSource = theme;
@@ -13,6 +20,6 @@ export const setTheme = (theme: DesktopTheme) => {
 export const getTheme = () => store.get('theme') ?? 'system';
 
 export const setupIpcHandlers = () => {
-    setupIpcHandler('theming:setTheme', (_, theme: DesktopTheme) => setTheme(theme));
+    setupIpcHandler('theming:setTheme', (_, theme) => setTheme(theme));
     setupIpcHandler('theming:getTheme', getTheme);
 };
