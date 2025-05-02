00f706f2946bdad0f64c1e40153ea3f4d549703f
Dominik Jankovic <dominik.jankovic@proton.ch>
2025-05-02 16:06:23 +0100

    Increase retryCount when biometric key fetching fails

--- packages/pass/hooks/useLockSetup.ts
+++ packages/pass/hooks/useLockSetup.ts
@@ -118,6 +118,7 @@ export const useLockSetup = (): LockSetup => {
                 case LockMode.BIOMETRICS: {
                     /** Confirm the biometric key before proceeding */
                     const secret = (await getBiometricsKey?.(authStore!).catch(noop)) ?? '';
+                    if (!secret) return resolve(undefined);
                     return unlock({ mode: currentLockMode, secret })
                         .then(() => resolve({ secret }))
                         .catch(() => resolve(undefined));
