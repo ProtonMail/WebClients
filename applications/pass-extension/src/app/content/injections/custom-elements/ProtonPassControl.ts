d6e0a13b3bf1902e3f99d45dae25c09ae22bd74e
Edvin CANDON <edvin.candon@proton.ch>
2025-05-05 14:03:26 +0200

    Transition to closed shadowDOM for extension custom elements
    [IDTEAM-4699]
    [SECBTY-1086]

--- applications/pass-extension/src/app/content/injections/custom-elements/ProtonPassControl.ts
+++ applications/pass-extension/src/app/content/injections/custom-elements/ProtonPassControl.ts
@@ -1,14 +1,7 @@
 import { PASS_ELEMENT_ROLE, ProtonPassElement } from './ProtonPassElement';
 
-// @ts-ignore
-import styles from './ProtonPassControl.raw.scss';
-
 export class ProtonPassControl extends ProtonPassElement {
     static getTagName = (hash: string) => `protonpass-control-${hash}`;
-
-    constructor() {
-        super(styles);
-    }
 
     connectedCallback() {
         super.connectedCallback();
--- packages/pass/utils/dom/create-element.ts
+++ packages/pass/utils/dom/create-element.ts
@@ -1,9 +1,8 @@
 export const createElement = <T extends HTMLElement = HTMLElement>(options: {
     type: string;
     classNames?: string[];
-    parent?: HTMLElement;
+    parent?: HTMLElement | ShadowRoot;
     attributes?: { [K in keyof T]?: string };
-    shadow?: boolean;
 }): T => {
     const el = document.createElement(options.type);
     options.classNames?.forEach((className) => el.classList.add(className));
@@ -12,8 +11,41 @@ export const createElement = <T extends HTMLElement = HTMLElement>(options: {
         Object.entries(options.attributes).forEach(([key, attr]) => el.setAttribute(key, attr));
     }
 
-    if (options.parent && !options.shadow) options.parent.appendChild(el);
-    if (options.parent && options.shadow) options.parent.shadowRoot?.appendChild(el);
+    if (options.parent) options.parent.appendChild(el);
 
     return el as T;
+};
+
+export type CustomElementRef<T extends HTMLElement> = {
+    customElement: T;
+    shadowRoot: ShadowRoot;
+};
+
+export const createCustomElement = <T extends HTMLElement>(options: {
+    type: string;
+    classNames?: string[];
+    parent?: HTMLElement | ShadowRoot;
+    attributes?: { [K in keyof T]?: string };
+    styles?: string;
+}): CustomElementRef<T> => {
+    const customElement = document.createElement(options.type) as T;
+    const shadowRoot = customElement.attachShadow({ mode: 'closed' });
+
+    options.classNames?.forEach((className) => customElement.classList.add(className));
+
+    if (options.attributes) {
+        Object.entries(options.attributes).forEach(([key, attr]) => {
+            customElement.setAttribute(key, attr);
+        });
+    }
+
+    if (options.styles) {
+        const sheet = new CSSStyleSheet();
+        sheet.replaceSync(options.styles);
+        shadowRoot.adoptedStyleSheets.push(sheet);
+    }
+
+    if (options.parent) options.parent.appendChild(customElement);
+
+    return { customElement, shadowRoot };
 };
