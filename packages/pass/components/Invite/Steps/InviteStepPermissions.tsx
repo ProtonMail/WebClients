42e7fc5721b15ad875dd7c5476f24ec2b67796d3
Edvin CANDON <edvin.candon@proton.ch>
2025-05-06 13:56:45 +0200

    Virtualize permissions/review members lists

--- packages/pass/components/Invite/Steps/InviteStepPermissions.tsx
+++ packages/pass/components/Invite/Steps/InviteStepPermissions.tsx
@@ -1,11 +1,13 @@
-import type { ReactNode } from 'react';
+import { type ReactNode, useRef } from 'react';
+import type { List } from 'react-virtualized';
 
 import { c } from 'ttag';
 
 import { AccessRoleToggle } from '@proton/pass/components/Invite/Access/AccessRoleToggle';
 import { InviteMember } from '@proton/pass/components/Invite/Steps/InviteMember';
+import { VirtualList } from '@proton/pass/components/Layout/List/VirtualList';
 import type { AccessTarget } from '@proton/pass/lib/access/types';
-import type { InviteFormMemberItem, InviteFormStep } from '@proton/pass/types';
+import type { InviteFormMemberItem, InviteFormStep, ShareRole } from '@proton/pass/types';
 
 type Props = {
     heading?: ReactNode;
@@ -15,46 +17,71 @@ type Props = {
     onUpdate: (members: InviteFormMemberItem[]) => Promise<void>;
 };
 
-export const InviteStepPermissions = ({ heading, members, target, onStep, onUpdate }: Props) => (
-    <div className="anime-fade-in h-full flex flex-column gap-y-3">
-        {heading}
-        <h2 className="text-xl text-bold">{c('Title').t`Set access level`}</h2>
+const ROW_HEIGHT = 64;
 
-        <div className="w-full flex flex-nowrap items-center justify-center justify-between gap-3 text-lg">
-            <div className="flex-auto shrink-0">
-                <button className="text-break-all text-left color-weak text-semibold" onClick={() => onStep('members')}>
-                    {c('Title').t`Members`} ({members.length})
-                </button>
-            </div>
+export const InviteStepPermissions = ({ heading, members, target, onStep, onUpdate }: Props) => {
+    const listRef = useRef<List>(null);
 
-            <AccessRoleToggle
-                onRoleChange={(role) => onUpdate(members.map(({ id, value }) => ({ id, value: { ...value, role } })))}
-            />
-        </div>
-        <div className="max-w-full">
-            {members.map((member) => (
-                <InviteMember
-                    {...member}
-                    target={target}
-                    key={`member-${member.id}`}
-                    onRemove={() => {
-                        const update = members.filter(({ id }) => id !== member.id);
-                        onUpdate(update);
-                        onStep(update.length === 0 ? 'members' : 'permissions');
-                    }}
+    return (
+        <div className="anime-fade-in h-full flex flex-column gap-y-3 flex-nowrap *:shrink-0">
+            {heading}
+            <h2 className="text-xl text-bold">{c('Title').t`Set access level`}</h2>
+
+            <div className="w-full flex flex-nowrap items-center justify-center justify-between gap-3 text-lg">
+                <div className="flex-auto shrink-0">
+                    <button
+                        className="text-break-all text-left color-weak text-semibold"
+                        onClick={() => onStep('members')}
+                    >
+                        {c('Title').t`Members`} ({members.length})
+                    </button>
+                </div>
+
+                <AccessRoleToggle
                     onRoleChange={(role) =>
-                        onUpdate(
-                            members.map(({ id, value }) => ({
-                                id,
-                                value: {
-                                    ...value,
-                                    role: id === member.id ? role : value.role,
-                                },
-                            }))
-                        )
+                        onUpdate(members.map(({ id, value }) => ({ id, value: { ...value, role } })))
                     }
                 />
-            ))}
+            </div>
+            <div
+                className="flex-1 min-h-custom overflow-hidden rounded-lg"
+                style={{ '--min-h-custom': `${ROW_HEIGHT * 2}px` }}
+            >
+                <VirtualList
+                    ref={listRef}
+                    rowHeight={() => ROW_HEIGHT}
+                    rowRenderer={({ style, index, key }) => {
+                        const member = members[index];
+
+                        const onRemove = async () => {
+                            const update = members.filter(({ id }) => id !== member.id);
+                            await onUpdate(update);
+                            await onStep(update.length === 0 ? 'members' : 'permissions');
+                        };
+
+                        const onRoleChange = (role: ShareRole) =>
+                            onUpdate(
+                                members.map(({ id, value }) => ({
+                                    id,
+                                    value: { ...value, role: id === member.id ? role : value.role },
+                                }))
+                            );
+
+                        return (
+                            <div style={style} key={key}>
+                                <InviteMember
+                                    {...member}
+                                    target={target}
+                                    key={`member-${member.id}`}
+                                    onRemove={onRemove}
+                                    onRoleChange={onRoleChange}
+                                />
+                            </div>
+                        );
+                    }}
+                    rowCount={members.length}
+                />
+            </div>
         </div>
-    </div>
-);
+    );
+};
