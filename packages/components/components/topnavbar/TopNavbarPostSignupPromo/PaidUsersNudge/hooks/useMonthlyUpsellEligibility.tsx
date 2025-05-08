4d9fa5f9bbcb9ee35c2aaa2a2be5fda578122176
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-05-08 08:39:54 +0000

    Merge branch 'explude-migrated' into 'main'
    exclude migrated users
    
    See merge request web/clients!16926

--- packages/components/components/topnavbar/TopNavbarPostSignupPromo/PaidUsersNudge/hooks/useMonthlyUpsellEligibility.tsx
+++ packages/components/components/topnavbar/TopNavbarPostSignupPromo/PaidUsersNudge/hooks/useMonthlyUpsellEligibility.tsx
@@ -4,7 +4,7 @@ import { useSubscription } from '@proton/account/subscription/hooks';
 import useConfig from '@proton/components/hooks/useConfig';
 import { type FeatureCode } from '@proton/features/interface';
 import useFeature from '@proton/features/useFeature';
-import { CYCLE, type PLANS } from '@proton/payments';
+import { CYCLE, type PLANS, hasMigrationDiscount } from '@proton/payments';
 import { isManagedExternally } from '@proton/payments';
 import { getAppFromPathnameSafe } from '@proton/shared/lib/apps/slugHelper';
 import { APPS } from '@proton/shared/lib/constants';
@@ -42,6 +42,7 @@ export const useMonthlyUpsellEligibility = ({ eligiblePlan, allowedApps, offerFl
     const isNextSubscriptionYearly = subscription.UpcomingSubscription?.Cycle === CYCLE.YEARLY;
 
     const isEligiblePlan = subscription.Plans?.some(({ Name }) => Name === eligiblePlan);
+    const isMigratedUser = hasMigrationDiscount(subscription);
 
     const isMobileSubscriber = isManagedExternally(subscription);
     const isInEligbilityWindow = isInWindow(differenceInDays(Date.now(), fromUnixTime(subscription.PeriodStart)));
@@ -52,6 +53,7 @@ export const useMonthlyUpsellEligibility = ({ eligiblePlan, allowedApps, offerFl
         isEligiblePlan &&
         isInEligbilityWindow &&
         !isMobileSubscriber &&
+        !isMigratedUser &&
         !isNextSubscriptionYearly
     );
 };
