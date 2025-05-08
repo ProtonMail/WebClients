24b47d78ba4cb916afab819d2a129da4dd98a4d8
Alejandro Tidele <alejandro.tidele@proton.ch>
2025-05-08 12:12:43 +0200

    Reuse PassIconLogo and remove unnecessary overlay prop
    [IDTEAM-4640]

--- packages/pass/components/Lock/LockOnboarding.tsx
+++ packages/pass/components/Lock/LockOnboarding.tsx
@@ -12,7 +12,7 @@ import { PASS_APP_NAME } from '@proton/shared/lib/constants';
 type Props = { onCancel: () => void };
 
 export const LockOnboarding: FC<Props> = ({ onCancel }) => (
-    <LobbyLayout overlay>
+    <LobbyLayout>
         <div
             key="lock-setup"
             className="anime-fade-in flex flex-nowrap shrink-0 flex-column gap-5"
