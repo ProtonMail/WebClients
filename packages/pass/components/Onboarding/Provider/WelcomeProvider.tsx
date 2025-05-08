366fb6b18fd78ca5ac15619fdd3194aa20f6ef75
Alejandro Tidele <alejandro.tidele@proton.ch>
2025-05-08 12:07:49 +0200

    Refactor Onboarding Context: - Add new Screen to the flow - Unify Desktop and Web onboarding (B2C only) - Improve styles
    [IDTEAM-4640]

--- packages/pass/components/Onboarding/Provider/WelcomeProvider.tsx
+++ packages/pass/components/Onboarding/Provider/WelcomeProvider.tsx
@@ -1,111 +1,78 @@
 import type { FC, PropsWithChildren } from 'react';
 import { useEffect, useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
 
-import { c } from 'ttag';
-
-import onboardingExtension from '@proton/pass/assets/desktop-onboarding/onboarding-extension.svg';
 import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
-import { OnboardingLockSetup } from '@proton/pass/components/Onboarding/OnboardingLockSetup';
 import { OnboardingModal } from '@proton/pass/components/Onboarding/OnboardingModal';
-import { OnboardingThemeSelect } from '@proton/pass/components/Onboarding/OnboardingThemeSelect';
-import { PASS_DOWNLOAD_URL } from '@proton/pass/constants';
+import { usePassExtensionInstalled } from '@proton/pass/hooks/usePassExtensionInstalled';
+import { selectAllItems, selectPassPlan } from '@proton/pass/store/selectors';
 import { SpotlightMessage } from '@proton/pass/types';
-import { PASS_SHORT_APP_NAME } from '@proton/shared/lib/constants';
+import { UserPassPlan } from '@proton/pass/types/api/plan';
 import noop from '@proton/utils/noop';
 
 import {
     OnboardingContext,
     type OnboardingContextValue,
-    type OnboardingStep,
     OnboardingType,
+    useOnboardingSteps,
 } from './OnboardingContext';
 
-export const WelcomeProvider: FC<PropsWithChildren> = ({ children }) => {
-    const { spotlight } = usePassCore();
-    const [enabled, setEnabled] = useState(false);
-    const [isActive, setIsActive] = useState(false);
-    const [completed, setCompleted] = useState<string[]>([]);
-
-    const steps = useMemo<OnboardingStep[]>(
-        () => [
-            {
-                shortTitle: c('Label').t`Select your theme`,
-                actionText: c('Label').t`Select theme`,
-                description: c('Label').t`Choose your preferred look and feel.`,
-                group: c('Label').t`Personalize`,
-                key: 'look-and-feel',
-                title: c('Label').t`Make it your own.`,
-                component: <OnboardingThemeSelect />,
-            },
-            {
-                shortTitle: c('Label').t`Choose unlock method`,
-                component: (
-                    <div className="pass-onboarding-modal--lock">
-                        <p className="text-bold mt-0">{c('Label').t`Unlock with:`}</p>
-                        <OnboardingLockSetup />
-                    </div>
-                ),
-                description: (
-                    <>
-                        {c('Label')
-                            .t`For security reasons, ${PASS_SHORT_APP_NAME} automatically locks itself after 10 minutes of inactivity.`}
-                        <br />
-                        <br />
-                        {c('Label').t`You can choose between PIN code, biometrics, or your account password to unlock.`}
-                    </>
-                ),
-                group: c('Label').t`Security`,
-                key: 'unlock',
-                title: c('Label').t`How to unlock ${PASS_SHORT_APP_NAME}`,
-            },
-            {
-                shortTitle: c('Label').t`Install extension`,
-                action: () => window.open(PASS_DOWNLOAD_URL, '_blank'),
-                actionText: c('Label').t`Install and continue`,
-                component: <img src={onboardingExtension} className="w-full" alt="" />,
-                description: c('Label').t`Get the extension for your browser.`,
-                group: c('Label').t`Browse faster, smarter`,
-                key: 'extension',
-                title: c('Label').t`Your passwords. Everywhere.`,
-            },
-        ],
-        []
-    );
-
-    useEffect(() => {
-        (async () => (await spotlight.check(SpotlightMessage.WELCOME)) ?? false)()
-            .then((enabled) => {
-                /* Auto-lauch on provider mount */
-                setEnabled(enabled);
-                setIsActive(enabled);
-            })
-            .catch(noop);
-    }, []);
-
-    const context = useMemo<OnboardingContextValue>(
-        () => ({
-            acknowledge: () => {
-                setEnabled(false);
-                void spotlight.acknowledge(SpotlightMessage.WELCOME);
-            },
-            launch: () => {
-                setEnabled(true);
-                setIsActive(true);
-            },
-            markCompleted: (step: string) => setCompleted((steps) => Array.from(new Set(steps.concat(step)))),
-            enabled,
-            isActive,
-            steps,
-            completed,
-            type: OnboardingType.WELCOME,
-        }),
-        [enabled, isActive, steps, completed]
-    );
-
-    return (
-        <OnboardingContext.Provider value={context}>
-            {children}
-            <OnboardingModal open={enabled && isActive} onClose={() => setIsActive(false)} />
-        </OnboardingContext.Provider>
-    );
+type WelcomeProviderParams = {
+    message: SpotlightMessage;
+    type: OnboardingType;
 };
+
+export const WelcomeProvider: (p: WelcomeProviderParams) => FC<PropsWithChildren> =
+    ({ message, type }) =>
+    ({ children }) => {
+        const { spotlight } = usePassCore();
+        const hasExtension = usePassExtensionInstalled(!DESKTOP_BUILD);
+        const [enabled, setEnabled] = useState(false);
+        const [isActive, setIsActive] = useState(false);
+        const [completed, setCompleted] = useState<string[]>([]);
+        const hasItems = useSelector(selectAllItems).length > 0;
+        const isFreePlan = useSelector(selectPassPlan) === UserPassPlan.FREE;
+        const { steps } = useOnboardingSteps([
+            'look-and-feel',
+            hasItems && 'unlock',
+            isFreePlan && 'upgrade',
+            !hasExtension && 'extension',
+        ]);
+
+        useEffect(() => {
+            (async () => (await spotlight.check(message)) ?? false)()
+                .then((enabled) => {
+                    /* Auto-lauch on provider mount */
+                    setEnabled(enabled);
+                    setIsActive(enabled);
+                })
+                .catch(noop);
+        }, []);
+
+        const context = useMemo<OnboardingContextValue>(
+            () => ({
+                acknowledge: () => {
+                    setEnabled(false);
+                    void spotlight.acknowledge(message);
+                },
+                launch: () => {
+                    setEnabled(true);
+                    setIsActive(true);
+                },
+                markCompleted: (step: string) => setCompleted((steps) => Array.from(new Set(steps.concat(step)))),
+                enabled,
+                isActive,
+                steps,
+                completed,
+                type,
+            }),
+            [enabled, isActive, steps, completed]
+        );
+
+        return (
+            <OnboardingContext.Provider value={context}>
+                {children}
+                <OnboardingModal open={enabled && isActive} onClose={() => setIsActive(false)} />
+            </OnboardingContext.Provider>
+        );
+    };
