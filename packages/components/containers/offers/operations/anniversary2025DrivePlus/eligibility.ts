167adaf79640cf33e939ce150d6a76fd2a7a1bd6
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-05-13 13:32:59 +0000

    Merge branch 'proton-anniversary-2025-fixes' into 'main'
    Proton anniversary 2025 fixes
    
    See merge request web/clients!16969

--- packages/components/containers/offers/operations/anniversary2025DrivePlus/eligibility.ts
+++ packages/components/containers/offers/operations/anniversary2025DrivePlus/eligibility.ts
@@ -1,25 +1,37 @@
 import { fromUnixTime, isBefore } from 'date-fns';
 
+import { type Subscription } from '@proton/payments';
 import { getAppFromPathnameSafe } from '@proton/shared/lib/apps/slugHelper';
 import { APPS } from '@proton/shared/lib/constants';
 import type { ProtonConfig, UserModel } from '@proton/shared/lib/interfaces';
 
+import isCheckAllowed from '../../helpers/isCheckAllowed';
 import { FREE_DOWNGRADER_LIMIT } from '../../helpers/offerPeriods';
+import { type OfferConfig } from '../../interface';
 
 interface Props {
     protonConfig: ProtonConfig;
     user: UserModel;
     lastSubscriptionEnd: number;
+    offerConfig: OfferConfig;
+    subscription?: Subscription;
 }
 
-export const getIsEligible = ({ user, protonConfig, lastSubscriptionEnd = 0 }: Props) => {
+export const getIsEligible = ({ user, protonConfig, lastSubscriptionEnd = 0, offerConfig, subscription }: Props) => {
+    if (!subscription) {
+        return false;
+    }
+
     const parentApp = getAppFromPathnameSafe(window.location.pathname);
     const hasValidApp =
         protonConfig.APP_NAME === APPS.PROTONDRIVE ||
         (protonConfig.APP_NAME === APPS.PROTONACCOUNT && parentApp === APPS.PROTONDRIVE);
 
+    const checkAllowed = isCheckAllowed(subscription, offerConfig);
+
     return (
         hasValidApp &&
+        checkAllowed &&
         user.isFree &&
         user.canPay &&
         !user.isDelinquent &&
