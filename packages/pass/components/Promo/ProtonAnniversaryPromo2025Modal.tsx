57a047a687522290049ac4bb847ebb11b7aef557
Edvin CANDON <edvin.candon@proton.ch>
2025-05-13 07:50:53 +0200

    Clean-up `ProtonAnniversaryPromo2025`
    [IDTEAM-4733]

--- packages/pass/components/Promo/ProtonAnniversaryPromo2025Modal.tsx
+++ packages/pass/components/Promo/ProtonAnniversaryPromo2025Modal.tsx
@@ -1,5 +1,5 @@
+/* Mirrors packages/components/containers/offers/components/anniversary2025/Anniversary2025Layout.tsx */
 import { type FC, useMemo } from 'react';
-import { useSelector } from 'react-redux';
 
 import { c } from 'ttag';
 
@@ -12,9 +12,9 @@ import { ModalTwoContent } from '@proton/components/index';
 import type { FeatureCode } from '@proton/features/interface';
 import { PassModal } from '@proton/pass/components/Layout/Modal/PassModal';
 import { useNavigateToUpgrade } from '@proton/pass/hooks/useNavigateToUpgrade';
-import { selectUser } from '@proton/pass/store/selectors';
 import { pipe } from '@proton/pass/utils/fp/pipe';
 import { CYCLE, DEFAULT_CURRENCY, PLANS, PLAN_NAMES } from '@proton/payments/core/constants';
+import type { Currency } from '@proton/payments/core/interface';
 import noop from '@proton/utils/noop';
 
 import { type EligiblePlan, UPSELL_MAP } from './ProtonAnniversaryPromo2025.utils';
@@ -23,39 +23,32 @@ import { Anniversary2025Pricing } from './ProtonAnniversaryPromo2025Pricing';
 import './ProtonAnniversaryPromo2025Modal.scss';
 
 type Props = {
-    onClose: () => void;
-    onNeverShowAgain: () => void;
+    currency: Currency;
     currentPlan: EligiblePlan;
+    email?: string;
+    onClose: () => void;
+    onDiscard: () => void;
 };
 
-/* Copied from packages/components/containers/offers/components/anniversary2025/Anniversary2025Layout.tsx
- * with modifications due to it using incompatible hooks */
-export const ProtonAnniversaryPromo2025Modal: FC<Props> = ({ onClose, onNeverShowAgain, currentPlan }) => {
-    const user = useSelector(selectUser);
-
+export const ProtonAnniversaryPromo2025Modal: FC<Props> = ({ currency, currentPlan, email, onClose, onDiscard }) => {
     const { coupon, planToUpsell, upsellRef, priceWithCoupon, priceWithoutCoupon, features } = UPSELL_MAP[currentPlan];
-
     const dealName = PLAN_NAMES[planToUpsell];
-
-    const userCurrency = user?.Currency ?? DEFAULT_CURRENCY;
-    /** Prices are only correct for USD, EUR and CHF. If user uses another currency, defaults to EUR */
-    const displayedCurrency = ['USD', 'EUR', 'CHF'].includes(userCurrency) ? userCurrency : DEFAULT_CURRENCY;
 
     const upgrade = useNavigateToUpgrade({
         coupon,
         cycle: '12',
-        email: user?.Email,
+        email,
         plan: planToUpsell,
         upsellRef,
         type: 'offer',
         disableEdit: true,
     });
 
-    const handleCTAClick = pipe(upgrade, onClose);
-
-    const offerProps: OfferProps = useMemo(
+    const offer: OfferProps = useMemo<OfferProps>(
         () => ({
-            currency: displayedCurrency,
+            /** Prices are only correct for USD, EUR and CHF.
+             * If user uses another currency, defaults to EUR */
+            currency: ['USD', 'EUR', 'CHF'].includes(currency) ? currency : DEFAULT_CURRENCY,
             offer: {
                 title:
                     planToUpsell === PLANS.PASS
@@ -72,12 +65,10 @@ export const ProtonAnniversaryPromo2025Modal: FC<Props> = ({ onClose, onNeverSho
                             withoutCouponMonthly: priceWithoutCoupon,
                         },
                         cycle: CYCLE.YEARLY,
-                        // variables below not used by the component but required by TypeScript
                         ref: '',
                         planIDs: {},
                     },
                 ],
-                // variables below not used by the component but required by TypeScript
                 featureCode: '' as FeatureCode,
                 ID: '' as OfferId,
                 layout: () => <></>,
@@ -86,7 +77,7 @@ export const ProtonAnniversaryPromo2025Modal: FC<Props> = ({ onClose, onNeverSho
             onCloseModal: noop,
             onSelectDeal: noop,
         }),
-        [currentPlan, displayedCurrency]
+        [currentPlan, currency]
     );
 
     return (
@@ -98,18 +89,18 @@ export const ProtonAnniversaryPromo2025Modal: FC<Props> = ({ onClose, onNeverSho
             <ModalTwoContent>
                 <OfferCloseButton onClose={onClose} darkBackground />
                 <section className="flex flex-column flex-nowrap *:min-size-auto w-full px-2 py-12 anniversary2025">
-                    <Anniversary2025Header {...offerProps} />
-                    <Anniversary2025Pricing {...offerProps} />
+                    <Anniversary2025Header {...offer} />
+                    <Anniversary2025Pricing {...offer} />
                     <Button
                         size="large"
                         fullWidth
                         color="norm"
                         className="text-semibold gradient-highlight"
-                        onClick={handleCTAClick}
+                        onClick={pipe(upgrade, onClose)}
                     >
                         {c('anniversary_2025').t`Get the deal`}
                     </Button>
-                    <Anniversary2025FeatureList {...offerProps} />
+                    <Anniversary2025FeatureList {...offer} />
                     <>
                         <p className="color-weak text-sm mt-0 mb-2 text-center">{c('anniversary_2025: Offer')
                             .t`Discounts are based on standard monthly pricing. At the end of the billing cycle, your subscription will renew at the standard annual rate.`}</p>
@@ -120,7 +111,7 @@ export const ProtonAnniversaryPromo2025Modal: FC<Props> = ({ onClose, onNeverSho
                                 color="norm"
                                 data-testid="cta:hide-offer"
                                 className="offer-disable-button color-weak text-sm hover:color-weak"
-                                onClick={onNeverShowAgain}
+                                onClick={onDiscard}
                             >{c('specialoffer: Action').t`Don't show this offer again`}</Button>
                         </div>
                     </>
