04faffb73a85ab2370db9b2e9328ce409f485fcb
Alejandro Tidele <alejandro.tidele@proton.ch>
2025-05-14 14:02:00 +0200

    Remove unnecessary double casting
    [IDTEAM-4509]

--- packages/pass/hooks/useDeobfuscatedItem.ts
+++ packages/pass/hooks/useDeobfuscatedItem.ts
@@ -5,7 +5,7 @@ import type { DeobfuscatedItem, Item, ItemType } from '@proton/pass/types';
 import type { DeobfuscateMode } from '@proton/pass/types/data/obfuscation';
 
 export const useDeobfuscatedItem = <T extends ItemType>(item: Item<T>) =>
-    useMemo(() => deobfuscateItem(item as Item) as unknown as DeobfuscatedItem<T>, [item]);
+    useMemo(() => deobfuscateItem(item as Item) as DeobfuscatedItem<T>, [item]);
 
 export const usePartialDeobfuscatedItem = <T extends ItemType>(item: Item<T>) =>
     useMemo(() => deobfuscateItemPartial(item as Item) as DeobfuscatedItem<T, DeobfuscateMode.AUTO>, [item]);
