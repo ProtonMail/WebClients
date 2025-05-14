e6c255a05a71379b6a299b4ef19c9c1d482b678c
Jack Crompton <jack.crompton@proton.ch>
2025-05-14 15:14:25 +0200

    Add event queue

--- packages/shared/lib/telemetry/index.ts
+++ packages/shared/lib/telemetry/index.ts
@@ -5,12 +5,22 @@ import { getAppVersionStr } from '../fetch/headers';
 import { captureMessage } from '../helpers/sentry';
 import type { ProtonConfig } from '../interfaces';
 
+type EventArgs = Parameters<ReturnType<typeof createTelemetry>['sendCustomEvent']>;
+
 class ProtonTelemetry {
     private telemetry: ReturnType<typeof createTelemetry> | undefined;
 
     private config: Omit<TelemetryConfig, 'uidHeader'> | undefined;
 
     private UID: string | undefined;
+
+    private readonly maxQueueLength: number = 10;
+
+    /**
+     * This queue is pushed to if telemetry has not been initialised. Then processed once initialisation has begun.
+     * This is to avoid events being dropped before initialisation
+     */
+    private eventQueue: EventArgs[] = [];
 
     public init({ config, uid }: { config: ProtonConfig; uid: string }) {
         this.UID = uid;
@@ -29,23 +39,42 @@ class ProtonTelemetry {
         this.setTelemetry();
     }
 
+    public sendCustomEvent(...args: EventArgs): ReturnType<ReturnType<typeof createTelemetry>['sendCustomEvent']> {
+        if (!this.telemetry || !this.UID) {
+            captureMessage('Attempted to send a custom event when @protontech/telemetry has not been initialised.');
+            this.addToEventQueue(args);
+            return;
+        }
+
+        this.telemetry.sendCustomEvent(...args);
+    }
+
+    private addToEventQueue(args: EventArgs) {
+        /**
+         * Ensure queue never exceeds max length
+         */
+        if (this.eventQueue.length >= this.maxQueueLength) {
+            this.eventQueue.shift();
+        }
+
+        this.eventQueue.push(args);
+    }
+
+    private processQueue() {
+        for (let i = 0; i < this.eventQueue.length; i++) {
+            const args = this.eventQueue[i];
+            this.sendCustomEvent(...args);
+        }
+    }
+
     private setTelemetry() {
         if (!this.config || !this.UID) {
             return;
         }
 
         this.telemetry = createTelemetry({ ...this.config, uidHeader: this.UID });
-    }
 
-    public sendCustomEvent(
-        ...args: Parameters<ReturnType<typeof createTelemetry>['sendCustomEvent']>
-    ): ReturnType<ReturnType<typeof createTelemetry>['sendCustomEvent']> {
-        if (!this.telemetry || !this.UID) {
-            captureMessage('Attempted to send a custom event when @protontech/telemetry has not been initialised.');
-            return;
-        }
-
-        this.telemetry.sendCustomEvent(...args);
+        this.processQueue();
     }
 }
 
