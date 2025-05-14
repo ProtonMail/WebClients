a55dc972ff599b82dba69ff743e7ab106422367b
msvanstr <mattias.svanstrom@proton.ch>
2025-05-14 09:21:26 +0200

    Revert "Merge branch 'add-telem-to-account-public-app' into 'main'"
    This reverts commit 358f24d1478a8e10d7339e9b2e9c440efffa60cd, reversing
    changes made to e451ad2c734067e3863c334815760a76b139ba90.

--- packages/shared/lib/telemetry/index.ts
+++ packages/shared/lib/telemetry/index.ts
@@ -2,46 +2,42 @@ import { type TelemetryConfig, ProtonTelemetry as createTelemetry } from '@proto
 
 import { getClientID } from '../apps/helper';
 import { getAppVersionStr } from '../fetch/headers';
-import { captureMessage } from '../helpers/sentry';
+// import { captureMessage } from '../helpers/sentry';
 import type { ProtonConfig } from '../interfaces';
 
 class ProtonTelemetry {
     private telemetry: ReturnType<typeof createTelemetry> | undefined;
 
-    private config: Omit<TelemetryConfig, 'uidHeader'> | undefined;
-
-    private UID: string | undefined;
+    private config: TelemetryConfig | undefined;
 
     public init({ config, uid }: { config: ProtonConfig; uid: string }) {
-        this.UID = uid;
         this.config = {
             endpoint: `${config.API_URL}/data/v1/telemetry`,
             appVersion: getAppVersionStr(getClientID(config.APP_NAME), config.APP_VERSION),
             debug: process.env.NODE_ENV !== 'production',
+            uidHeader: uid,
         };
-
-        this.setTelemetry();
     }
 
     public setAuthHeaders(UID: string) {
-        this.UID = UID;
-
-        this.setTelemetry();
-    }
-
-    private setTelemetry() {
-        if (!this.config || !this.UID) {
+        if (!this.config) {
             return;
         }
 
-        this.telemetry = createTelemetry({ ...this.config, uidHeader: this.UID });
+        this.config = {
+            ...this.config,
+            uidHeader: UID,
+        };
+
+        this.telemetry = createTelemetry(this.config);
     }
 
     public sendCustomEvent(
         ...args: Parameters<ReturnType<typeof createTelemetry>['sendCustomEvent']>
     ): ReturnType<ReturnType<typeof createTelemetry>['sendCustomEvent']> {
-        if (!this.telemetry || !this.UID) {
-            captureMessage('Attempted to send a custom event when @proton/telemetry has not been initialised.');
+        if (!this.telemetry || !this.config?.uidHeader) {
+            // TODO: report when telem has not been initialised
+            // captureMessage('Attempted to send a custom event when @proton/telemetry has not been initialised.');
             return;
         }
 
