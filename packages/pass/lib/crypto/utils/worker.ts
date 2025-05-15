4d0708379fdd8c770e458501d6742029c22f75ff
Lara Bruseghini <lara.bruseghini@proton.ch>
2025-05-15 11:37:41 +0000

    Always enable OpenPGP grammar check for pass

--- packages/pass/lib/crypto/utils/worker.ts
+++ packages/pass/lib/crypto/utils/worker.ts
@@ -3,6 +3,6 @@ import { loadCryptoWorker } from '@proton/shared/lib/helpers/setupCryptoWorker';
 import { PassCryptoError } from './errors';
 
 export const loadCoreCryptoWorker = () =>
-    loadCryptoWorker().catch(() => {
+    loadCryptoWorker({ openpgpConfigOptions: { enforceOpenpgpGrammar: true } }).catch(() => {
         throw new PassCryptoError('Could not load crypto worker');
     });
