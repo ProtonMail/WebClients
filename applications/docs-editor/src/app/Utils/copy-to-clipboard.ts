9b339f705bf042c081010e896ae61c17dd44ec80
Aman Harwara <aman.harwara@proton.ch>
2025-05-15 08:48:44 +0000

    Docs: Add "Copy as markdown" option

---
+++ applications/docs-editor/src/app/Utils/copy-to-clipboard.ts
@@ -0,0 +1,16 @@
+export function copyTextToClipboard(text: string) {
+  const textArea = document.createElement('textarea')
+  textArea.value = text
+  textArea.style.top = '0'
+  textArea.style.left = '0'
+  textArea.style.position = 'fixed'
+  document.body.appendChild(textArea)
+  textArea.focus()
+  textArea.select()
+  try {
+    document.execCommand('copy')
+  } catch (err) {
+    console.error('Unable to copy', err)
+  }
+  document.body.removeChild(textArea)
+}
