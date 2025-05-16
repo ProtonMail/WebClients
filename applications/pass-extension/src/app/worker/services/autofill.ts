f61feb60332f11675805148c232e8eced363ac62
Alejandro Tidele <alejandro.tidele@proton.ch>
2025-05-16 12:29:10 +0200

    Fine-tune basic auth handler
    - Wrap the listener to make it work only in non-safari builds
    - Non-blocking handler for authentication retries
    
    [IDTEAM-4637]

--- applications/pass-extension/src/app/worker/services/autofill.ts
+++ applications/pass-extension/src/app/worker/services/autofill.ts
@@ -209,38 +209,40 @@ export const createAutoFillService = () => {
         })
     );
 
-    browser.webRequest.onAuthRequired.addListener(
-        (details) => {
-            const items = getLoginCandidates(parseUrl(details.url));
+    if (BUILD_TARGET !== 'safari') {
+        browser.webRequest.onAuthRequired.addListener(
+            (details) => {
+                // If url already has the credentials embedded, do nothing
+                if (/:\/\/(.)+:(.)+@/.test(details.url)) return { cancel: true };
 
-            // If there are no items, do nothing
-            if (!items.length) return { cancel: false };
+                const items = getLoginCandidates(parseUrl(details.url));
 
-            // If url already has the credentials embedded, do nothing
-            if (/:\/\/(.)+:(.)+@/.test(details.url)) return { cancel: false };
+                // If there are no items, do nothing
+                if (!items.length) return { cancel: true };
 
-            // Mechanism to prevent infinite loop when credentials are incorrect
-            const requestAttempt = attemptedBasicAuthRequests.get(details.requestId) ?? 0;
-            if (requestAttempt >= items.length) return { cancel: false };
+                // Mechanism to prevent infinite loop when credentials are incorrect
+                const requestAttempt = attemptedBasicAuthRequests.get(details.requestId) ?? 0;
+                if (requestAttempt >= items.length) return { cancel: true };
 
-            attemptedBasicAuthRequests.set(details.requestId, requestAttempt + 1);
+                attemptedBasicAuthRequests.set(details.requestId, requestAttempt + 1);
 
-            // Clear the Map after 2 seconds
-            setTimeout(() => attemptedBasicAuthRequests.delete(details.requestId), 2000);
+                // Clear the Map after 2 seconds
+                setTimeout(() => attemptedBasicAuthRequests.delete(details.requestId), 2000);
 
-            // If there are more than 1 login credentials, try with each one
-            const item = items[requestAttempt];
+                // If there are more than 1 login credentials, try with each one
+                const item = items[requestAttempt];
 
-            return {
-                authCredentials: {
-                    username: intoUserIdentifier(item),
-                    password: deobfuscate(item.data.content.password),
-                },
-            };
-        },
-        { urls: ['<all_urls>'] },
-        ['blocking']
-    );
+                return {
+                    authCredentials: {
+                        username: intoUserIdentifier(item),
+                        password: deobfuscate(item.data.content.password),
+                    },
+                };
+            },
+            { urls: ['<all_urls>'] },
+            ['blocking']
+        );
+    }
 
     return {
         clear,
