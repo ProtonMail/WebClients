9e10aace1cb84a967c307ee85884f101025067a6
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-05-16 11:30:49 +0000

    Merge branch 'MAILWEB-6538-pagination' into 'main'
    Add pagination UI
    
    See merge request web/clients!17090

--- applications/mail/src/app/components/view/NewsletterSubscription/SubscriptionsList/NewsletterSubscriptionList.tsx
+++ applications/mail/src/app/components/view/NewsletterSubscription/SubscriptionsList/NewsletterSubscriptionList.tsx
@@ -1,19 +1,46 @@
-import { useMailSelector } from 'proton-mail/store/hooks';
-import { selectTabSubscriptionsList } from 'proton-mail/store/newsletterSubscriptions/newsletterSubscriptionsSelector';
+import { c } from 'ttag';
+
+import { Button } from '@proton/atoms';
+import useLoading from '@proton/hooks/useLoading';
+
+import { useMailDispatch, useMailSelector } from 'proton-mail/store/hooks';
+import { fetchNextNewsletterSubscriptionsPage } from 'proton-mail/store/newsletterSubscriptions/newsletterSubscriptionsActions';
+import {
+    selectTabSubscriptionPagination,
+    selectTabSubscriptionsList,
+} from 'proton-mail/store/newsletterSubscriptions/newsletterSubscriptionsSelector';
 
 import { NewsletterSubscriptionCard } from './NewsletterSubscriptionCard';
 import { NewsletterSubscriptionListPlaceholder } from './NewsletterSubscriptionListPlaceholder';
 import { NewsletterSubscriptionListWrapper } from './NewsletterSubscriptionListWrapper';
 
 export const NewsletterSubscriptionList = () => {
+    const dispatch = useMailDispatch();
+
     const subscriptionList = useMailSelector(selectTabSubscriptionsList);
+    const pagination = useMailSelector(selectTabSubscriptionPagination);
+
+    const [loading, withLoading] = useLoading();
+
+    const handleNextPageClick = () => {
+        void withLoading(dispatch(fetchNextNewsletterSubscriptionsPage()));
+    };
 
     return (
         <NewsletterSubscriptionListWrapper>
             {subscriptionList.length === 0 ? (
                 <NewsletterSubscriptionListPlaceholder />
             ) : (
-                subscriptionList.map((sub) => <NewsletterSubscriptionCard key={sub.ID} subscription={sub} />)
+                <>
+                    {subscriptionList.map((sub) => (
+                        <NewsletterSubscriptionCard key={sub.ID} subscription={sub} />
+                    ))}
+                    {pagination ? (
+                        <Button className="w-fit-content mx-auto" loading={loading} onClick={handleNextPageClick}>
+                            {c('Action').t`Load more`}
+                        </Button>
+                    ) : null}
+                </>
             )}
         </NewsletterSubscriptionListWrapper>
     );
