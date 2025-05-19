c07d91dea8dc4739fb936db24b9f4f194ca229d5
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-05-19 16:20:23 +0200

    MAILWEB-6737 - fix scroll behaviour in the list of mail subscriptions

--- applications/mail/src/app/components/view/NewsletterSubscription/SubscriptionsList/NewsletterSubscriptionListWrapper.tsx
+++ applications/mail/src/app/components/view/NewsletterSubscription/SubscriptionsList/NewsletterSubscriptionListWrapper.tsx
@@ -1,13 +1,17 @@
 import { type PropsWithChildren } from 'react';
 
+import { Scroll } from '@proton/atoms';
+
 import { NewsletterSubscriptionListHeader } from './NewsletterSubscriptionListHeader';
 
 export const NewsletterSubscriptionListWrapper = ({ children }: PropsWithChildren) => {
     return (
         <>
-            <div className="flex-1 border-right border-weak h-full overflow-auto">
+            <div className="flex-1 border-right border-weak h-full">
                 <NewsletterSubscriptionListHeader />
-                <section className="flex px-6 flex-column flex-nowrap gap-4 pb-4 px-1">{children}</section>
+                <Scroll className="h-custom" style={{ '--h-custom': 'calc(100% - 4.5rem)' }}>
+                    <section className="flex px-6 flex-column flex-nowrap gap-4 pb-4 px-1 ">{children}</section>
+                </Scroll>
             </div>
         </>
     );
