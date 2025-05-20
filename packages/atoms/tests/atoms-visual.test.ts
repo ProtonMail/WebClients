c3f24463ffcf8357dbf5f982099172cba419deac
Paolo Chiabrera <paolo.chiabrera@proton.ch>
2025-05-20 12:59:59 +0000

    MAILWEB-6753: Extract getStorybooIds helper

--- packages/atoms/tests/atoms-visual.test.ts
+++ packages/atoms/tests/atoms-visual.test.ts
@@ -1,29 +1,6 @@
 import { expect, test } from '@playwright/test';
-import { existsSync, readFileSync } from 'node:fs';
-import { resolve } from 'node:path';
 
-function getStorybooIds() {
-    const storybookStaticFolder = resolve(__dirname, '../storybook-static');
-
-    if (!existsSync(storybookStaticFolder)) {
-        throw new Error('storybook-static folder does not exist, try to run "yarn storybook:build" first.');
-    }
-
-    const content = readFileSync(resolve(storybookStaticFolder, './index.json'), 'utf-8');
-    let parsedContent;
-
-    try {
-        parsedContent = JSON.parse(content);
-    } catch (err) {
-        throw new Error('Storybook index.json is not valid JSON.');
-    }
-
-    const ids = Object.keys(parsedContent.entries).filter(
-        (id: string) => parsedContent.entries[id].type.toLowerCase() === 'story'
-    );
-
-    return ids;
-}
+import { getStorybooIds } from './get-storybook-ids';
 
 const ids = getStorybooIds();
 
