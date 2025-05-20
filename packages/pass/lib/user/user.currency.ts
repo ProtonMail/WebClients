7a9244a389d94aee7e33459aaf3be0a9e4a35d0a
Alejandro Tidele <alejandro.tidele@proton.ch>
2025-05-20 15:58:38 +0200

    Hide Lifetime card if user currency is not supported

--- packages/pass/lib/user/user.currency.ts
+++ packages/pass/lib/user/user.currency.ts
@@ -2,5 +2,7 @@ import type { Maybe } from '@proton/pass/types';
 import { DEFAULT_CURRENCY } from '@proton/payments/core/constants';
 import type { Currency } from '@proton/payments/core/interface';
 
+export const supportedCurrencies = ['USD', 'EUR', 'CHF'];
+
 export const getUserCurrency = (currency: Maybe<Currency>) =>
-    currency && ['USD', 'EUR', 'CHF'].includes(currency) ? currency : DEFAULT_CURRENCY;
+    currency && supportedCurrencies.includes(currency) ? currency : DEFAULT_CURRENCY;
