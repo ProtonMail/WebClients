53f4310a419a1b552c2fec232f7423483819b4be
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-05-22 16:24:09 +0000

    Merge branch 'remote-pm-signature' into 'main'
    Get PM signature content from the API
    
    See merge request web/clients!17083

--- packages/shared/lib/mail/signature.ts
+++ packages/shared/lib/mail/signature.ts
@@ -1,31 +1,35 @@
 import { c } from 'ttag';
 
-import { APPS, APPS_CONFIGURATION } from '../constants';
-import type { MAIL_VERIFICATION_STATUS } from './constants';
 import type { VERIFICATION_STATUS } from '@proton/crypto/lib/constants';
 
-interface Options {
-    isReferralProgramLinkEnabled?: boolean;
-    referralProgramUserLink?: string;
-}
+import { APPS, APPS_CONFIGURATION } from '../constants';
+import type { MAIL_VERIFICATION_STATUS } from './constants';
 
-export const getProtonMailSignature = ({
-    isReferralProgramLinkEnabled = false,
-    referralProgramUserLink,
-}: Options = {}) => {
-    const link =
-        isReferralProgramLinkEnabled && referralProgramUserLink
-            ? referralProgramUserLink
-            : 'https://proton.me/mail/home';
+const getPmSignatureLink = (pmSignatureReferralLinkEnabled: boolean, referralLink: string) => {
+    if (pmSignatureReferralLinkEnabled && referralLink) {
+        return referralLink;
+    }
+    return 'https://proton.me/mail/home';
+};
+
+export const getProtonMailSignature = (
+    pmSignatureReferralLinkEnabled = false,
+    referralLink = 'https://proton.me/mail/home',
+    pmSignatureContent = ''
+): string => {
+    if (pmSignatureContent) {
+        return pmSignatureContent;
+    }
+
+    const link = getPmSignatureLink(pmSignatureReferralLinkEnabled, referralLink);
 
     // translator: full sentence is: "Sent with Proton Mail secure email"
-    const signature = c('Info').t`Sent with <a href="${link}" target="_blank">${
+    return c('Info').t`Sent with <a href="${link}" target="_blank">${
         APPS_CONFIGURATION[APPS.PROTONMAIL].name
     }</a> secure email.`;
-
-    return signature;
 };
 
 // MAIL_VERIFICATION_STATUS should be a superset of VERIFICATION_STATUS returned by the CryptoAPI.
 // This converter is necessary since TS requires explicit casting between enums.
-export const getMailVerificationStatus = (verificationStatus: VERIFICATION_STATUS): MAIL_VERIFICATION_STATUS => verificationStatus.valueOf();
+export const getMailVerificationStatus = (verificationStatus: VERIFICATION_STATUS): MAIL_VERIFICATION_STATUS =>
+    verificationStatus.valueOf();
