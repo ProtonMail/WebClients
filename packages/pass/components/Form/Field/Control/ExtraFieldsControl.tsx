6a425d07785cffd0850ee2629132ac9841d120a4
Edvin CANDON <edvin.candon@proton.ch>
2025-05-23 08:58:57 +0200

    Clean-up `onCopy` handlers for extension telemetry
    - Extract conditional telemetry to `useLoginClipboardTelemetry`
    - Update `ValueControl` props to be more expressive (onCopy)
    - Simplify message types
    
    [IDTEAM-4737]

--- packages/pass/components/Form/Field/Control/ExtraFieldsControl.tsx
+++ packages/pass/components/Form/Field/Control/ExtraFieldsControl.tsx
@@ -18,10 +18,10 @@ type ExtraFieldsControlProps = {
     extraFields: DeobfuscatedItemExtraField[];
     itemId: string;
     shareId: string;
-    onClick?: () => void;
+    onCopy?: () => void;
 };
 
-export const ExtraFieldsControl: FC<ExtraFieldsControlProps> = ({ extraFields, itemId, shareId, onClick }) => {
+export const ExtraFieldsControl: FC<ExtraFieldsControlProps> = ({ extraFields, itemId, shareId, onCopy }) => {
     const { needsUpgrade } = useSelector(selectExtraFieldLimits);
 
     const getControlByType = useCallback(
@@ -38,13 +38,13 @@ export const ExtraFieldsControl: FC<ExtraFieldsControlProps> = ({ extraFields, i
             switch (type) {
                 case 'totp':
                     return isEmptyString(data.totpUri) ? (
-                        <ValueControl icon={icon} key={key} label={fieldName} value={undefined} onClick={onClick} />
+                        <ValueControl icon={icon} key={key} label={fieldName} />
                     ) : (
                         <OTPValueControl
                             key={key}
                             label={fieldName}
                             payload={{ totpUri: data.totpUri, type: 'uri' }}
-                            onClick={onClick}
+                            onCopy={onCopy}
                         />
                     );
                 case 'timestamp':
@@ -73,7 +73,7 @@ export const ExtraFieldsControl: FC<ExtraFieldsControlProps> = ({ extraFields, i
                             icon={icon}
                             label={fieldName}
                             value={data.content}
-                            onClick={onClick}
+                            onCopy={onCopy}
                         />
                     );
             }
