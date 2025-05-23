708091531c0a2a1f938f5c88a9d861d70c2a84f2
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-05-23 13:36:53 +0000

    Load more fix

--- packages/shared/lib/api/newsletterSubscription.ts
+++ packages/shared/lib/api/newsletterSubscription.ts
@@ -17,6 +17,8 @@ export const getNewsletterSubscription = ({ pagination, sort }: GetNewslettersPr
     params: {
         ...pagination,
         ...getSortParams(sort),
+        // We hardcode this sort because it's used as fallback in case of coliding sorting
+        'Sort[ID]': 'DESC',
         // We hardcode this params for the moment as we don't want to see spam subscription
         Spam: 0,
     },
