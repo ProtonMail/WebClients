50cf50e5afe1a4ca1573c915144bf07f41b00f45
Edvin CANDON <edvin.candon@proton.ch>
2025-05-26 18:42:34 +0200

    Improve requestIdleCallback shim for safari

--- applications/pass-extension/src/lib/utils/polyfills.ts
+++ applications/pass-extension/src/lib/utils/polyfills.ts
@@ -1,4 +1,21 @@
 if (BUILD_TARGET === 'safari') {
-    globalThis.requestIdleCallback = (fn) => setTimeout(fn, 0);
-    globalThis.cancelIdleCallback = clearTimeout;
+    globalThis.requestIdleCallback =
+        globalThis.requestIdleCallback ||
+        function (cb) {
+            const start = Date.now();
+            return setTimeout(
+                () =>
+                    cb({
+                        didTimeout: false,
+                        timeRemaining: () => Math.max(0, 50 - (Date.now() - start)),
+                    }),
+                1
+            );
+        };
+
+    globalThis.cancelIdleCallback =
+        globalThis.cancelIdleCallback ||
+        function (id) {
+            clearTimeout(id);
+        };
 }
