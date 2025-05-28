a569056357b26aaf8a1795465ee823ac39fe77ff
Dominik Jankovic <dominik.jankovic@proton.ch>
2025-05-28 15:53:43 +0100

    Show item history for custom items

--- packages/pass/components/Item/Custom/Custom.view.tsx
+++ packages/pass/components/Item/Custom/Custom.view.tsx
@@ -1,6 +1,7 @@
 import { c } from 'ttag';
 
 import { FileAttachmentsContentView } from '@proton/pass/components/FileAttachments/FileAttachmentsView';
+import { ItemHistoryStats } from '@proton/pass/components/Item/History/ItemHistoryStats';
 import { MoreInfoDropdown } from '@proton/pass/components/Layout/Dropdown/MoreInfoDropdown';
 import { ItemViewPanel } from '@proton/pass/components/Layout/Panel/ItemViewPanel';
 import { SecureLinkCardList } from '@proton/pass/components/SecureLink/SecureLinkCardList';
@@ -11,8 +12,8 @@ import type { ItemCustomType, ItemType } from '@proton/pass/types';
 import { CustomContent } from './Custom.content';
 
 export const CustomView = <T extends ItemCustomType>(itemViewProps: ItemViewProps<T>) => {
-    const { revision } = itemViewProps;
-    const { revision: revisionNumber, shareId, itemId } = revision;
+    const { revision, handleHistoryClick } = itemViewProps;
+    const { revision: revisionNumber, shareId, itemId, createTime, modifyTime } = revision;
     const modifiedCount = revisionNumber - 1;
 
     return (
@@ -20,6 +21,7 @@ export const CustomView = <T extends ItemCustomType>(itemViewProps: ItemViewProp
             <SecureLinkCardList shareId={shareId} itemId={itemId} />
             <CustomContent revision={revision} />
             <FileAttachmentsContentView revision={revision} />
+            <ItemHistoryStats createTime={createTime} modifyTime={modifyTime} handleHistoryClick={handleHistoryClick} />
             <MoreInfoDropdown
                 info={[
                     { label: c('Label').t`Modified`, values: [getOccurrenceString(modifiedCount)] },
