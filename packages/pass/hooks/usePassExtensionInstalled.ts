43f0fdae11fde8ca4ae95bfb867c2e1dae4fce93
Edvin CANDON <edvin.candon@proton.ch>
2025-05-30 13:11:54 +0200

    Cache `usePassExtensionInstalled` result to avoid redundant calls

--- packages/pass/hooks/usePassExtensionInstalled.ts
+++ packages/pass/hooks/usePassExtensionInstalled.ts
@@ -1,20 +1,29 @@
 import { useEffect, useState } from 'react';
 
+import type { MaybeNull } from '@proton/pass/types';
 import type { PassInstalledMessage } from '@proton/shared/lib/browser/extension';
 import { sendExtensionMessage } from '@proton/shared/lib/browser/extension';
 import { APPS } from '@proton/shared/lib/constants';
 import noop from '@proton/utils/noop';
 
+/** Cache the detection result to avoid redundant extension
+ * communication if the hook is used multiple times. Assumes
+ * the extension is installed by default. */
+let extensionInstalled: MaybeNull<boolean> = null;
+
 export const usePassExtensionInstalled = (supported: boolean): boolean => {
-    const [installed, setInstalled] = useState(true);
+    const [installed, setInstalled] = useState(extensionInstalled ?? true);
 
     useEffect(() => {
-        if (supported) {
+        if (supported && extensionInstalled === null) {
             sendExtensionMessage({ type: 'pass-installed' } satisfies PassInstalledMessage, {
                 app: APPS.PROTONPASSBROWSEREXTENSION,
                 maxTimeout: 1_000,
             })
-                .then((result) => setInstalled(result?.type === 'success'))
+                .then((result) => {
+                    extensionInstalled = result?.type === 'success';
+                    setInstalled(extensionInstalled);
+                })
                 .catch(noop);
         }
     }, []);
