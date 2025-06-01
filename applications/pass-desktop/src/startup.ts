9247eefb60d554d58020ee31f9088750f8a31fcb
Edvin CANDON <edvin.candon@proton.ch>
2025-06-01 11:07:06 +0200

    Disable uninstall MacOS daemon

--- applications/pass-desktop/src/startup.ts
+++ applications/pass-desktop/src/startup.ts
@@ -4,15 +4,11 @@ import { setupIpcHandlers as info } from './lib/install-info';
 import { setupIpcHandlers as navigation } from './lib/navigation';
 import { setupIpcHandlers as theming } from './lib/theming';
 import type { PassElectronContext } from './types';
-import { installDaemon } from './uninstallers/macos/daemon/launcher';
 import { handleSquirrelEvents } from './utils/squirrel';
 
 export const startup = async (ctx: PassElectronContext) => {
     // Handle creating/removing shortcuts on Windows when installing/uninstalling.
     await handleSquirrelEvents();
-
-    // Handle the creation of a Daemon to wipe data after manual deletion
-    installDaemon();
 
     // Initialize IPC handlers
     biometrics(() => ctx.window);
