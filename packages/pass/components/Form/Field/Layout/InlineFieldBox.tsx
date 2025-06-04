d654eefd47496c47bb37228b228a090f08d07b6d
Dominik Jankovic <dominik.jankovic@proton.ch>
2025-06-04 13:30:24 +0100

    Allow custom utility classes on InlineFieldBox

--- packages/pass/components/Form/Field/Layout/InlineFieldBox.tsx
+++ packages/pass/components/Form/Field/Layout/InlineFieldBox.tsx
@@ -4,10 +4,10 @@ import clsx from '@proton/utils/clsx';
 
 import './InlineFieldBox.scss';
 
-type Props = PropsWithChildren & { label: ReactNode; size?: 'sm' | 'md' | 'lg' };
+type Props = PropsWithChildren & { className?: string; label: ReactNode; size?: 'sm' | 'md' | 'lg' };
 
-export const InlineFieldBox: FC<Props> = ({ label, children, size = 'sm' }) => (
-    <div className={clsx('pass-inline-field-box', size)}>
+export const InlineFieldBox: FC<Props> = ({ label, children, size = 'sm', className = '' }) => (
+    <div className={clsx('pass-inline-field-box', size, className)}>
         <label>{label}</label>
         <div className="flex items-center justify-end">{children}</div>
     </div>
