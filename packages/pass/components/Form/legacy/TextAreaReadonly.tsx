39a2915c55c57738fc83294f05e7c4e69b281cd7
Alejandro Tidele <alejandro.tidele@proton.ch>
2025-06-10 11:10:55 +0200

    Extend TextAreaReadonly component to support contained content
    [IDTEAM-4799]

--- packages/pass/components/Form/legacy/TextAreaReadonly.tsx
+++ packages/pass/components/Form/legacy/TextAreaReadonly.tsx
@@ -1,38 +1,91 @@
-import type { FC } from 'react';
-import { useLayoutEffect, useRef, useState } from 'react';
+import { type FC, type PropsWithChildren, useCallback, useLayoutEffect, useMemo, useRef, useState } from 'react';
 
+import { c } from 'ttag';
+
+import { Button } from '@proton/atoms/src';
+import { FieldBox } from '@proton/pass/components/Form/Field/Layout/FieldBox';
+import { FieldsetCluster } from '@proton/pass/components/Form/Field/Layout/FieldsetCluster';
 import clsx from '@proton/utils/clsx';
 
 import './TextAreaReadonly.scss';
 
-type Props = { children: string; className?: string };
+const CONTAINED_HEIGHT = EXTENSION_BUILD ? 120 : 240;
 
-export const TextAreaReadonly: FC<Props> = ({ children, className }) => {
+type Props = { children: string; className?: string; contained?: boolean };
+
+export const TextAreaReadonly: FC<Props> = ({ children, className, contained }) => {
     const [height, setHeight] = useState(0);
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [needsExpansion, setNeedsExpansion] = useState(false);
     const ref = useRef<HTMLTextAreaElement>(null);
+    const textAreaHeight = useMemo(
+        () => (!contained || isExpanded || height < CONTAINED_HEIGHT ? height : CONTAINED_HEIGHT),
+        [isExpanded, height]
+    );
+
+    const Container: FC<PropsWithChildren> = useCallback(
+        (props) =>
+            contained ? (
+                <FieldsetCluster mode="read">
+                    <FieldBox>{props.children}</FieldBox>
+                </FieldsetCluster>
+            ) : (
+                props.children
+            ),
+        [contained]
+    );
 
     useLayoutEffect(() => {
-        if (ref.current) setHeight(ref.current.scrollHeight);
-    }, [children]);
+        if (ref.current) {
+            const scrollHeight = ref.current.scrollHeight;
+            setHeight(scrollHeight);
+            if (contained && !isExpanded) setNeedsExpansion(scrollHeight > CONTAINED_HEIGHT);
+        }
+    }, [children, contained, isExpanded]);
 
     return (
-        <textarea
-            ref={ref}
-            readOnly
-            value={children}
-            className={clsx('w-full h-full text-pre-wrap resize-none h-custom pass-textarea--readonly', className)}
-            style={{ '--h-custom': `${height}px`, opacity: 1 }}
-            onClick={(evt) => {
-                if (ref.current) {
-                    const { selectionStart, selectionEnd } = ref.current;
-                    const hasSelection = selectionStart !== selectionEnd;
+        <Container>
+            <textarea
+                ref={ref}
+                readOnly
+                value={children}
+                className={clsx(
+                    'w-full h-full text-pre-wrap resize-none h-custom pass-textarea--readonly anime-fade-in',
+                    className
+                )}
+                style={{
+                    '--h-custom': `${textAreaHeight}px`,
+                    transition: 'height 0.3s ease-out',
+                    opacity: 1,
+                }}
+                onClick={(evt) => {
+                    if (ref.current) {
+                        const { selectionStart, selectionEnd } = ref.current;
+                        const hasSelection = selectionStart !== selectionEnd;
 
-                    if (hasSelection) {
-                        evt.preventDefault();
-                        evt.stopPropagation();
+                        if (hasSelection) {
+                            evt.preventDefault();
+                            evt.stopPropagation();
+                        }
                     }
-                }
-            }}
-        />
+                }}
+            />
+            {contained && needsExpansion && (
+                <>
+                    {!isExpanded && <span className="mr-1">...</span>}
+                    <Button
+                        pill
+                        shape="underline"
+                        className="link link-focus text-nowrap"
+                        color="weak"
+                        onClick={() => setIsExpanded((prev) => !prev)}
+                    >
+                        <span className="line-height-1">
+                            {isExpanded ? c('Action').t`Show Less` : c('Action').t`Read More`}
+                        </span>
+                    </Button>
+                </>
+            )}
+        </Container>
     );
 };
