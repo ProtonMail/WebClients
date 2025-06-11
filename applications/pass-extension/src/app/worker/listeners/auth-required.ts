7072c331a5f944e664bc224f9fc1ac3f14988dd1
Edvin CANDON <edvin.candon@proton.ch>
2025-06-11 15:05:55 +0200

    Add dynamic basic auth controller to avoid runtime reload

--- applications/pass-extension/src/app/worker/listeners/auth-required.ts
+++ applications/pass-extension/src/app/worker/listeners/auth-required.ts
@@ -1,10 +1,12 @@
 import { withContext } from 'proton-pass-extension/app/worker/context/inject';
+import { WEB_REQUEST_PERMISSIONS, hasPermissions } from 'proton-pass-extension/lib/utils/permissions';
 import type { WebRequest } from 'webextension-polyfill';
 
 import browser from '@proton/pass/lib/globals/browser';
 import { intoUserIdentifier } from '@proton/pass/lib/items/item.utils';
 import { selectAutofillSettings } from '@proton/pass/store/selectors/settings';
 import type { ItemRevision } from '@proton/pass/types';
+import { logger } from '@proton/pass/utils/logger';
 import { deobfuscate } from '@proton/pass/utils/obfuscate/xor';
 import { parseUrl } from '@proton/pass/utils/url/parser';
 
@@ -14,7 +16,7 @@ type AttemptCount = number;
 
 export const BASIC_AUTH_URL_RE = /^(https?:\/\/)(.+?):(.+?)@/;
 
-export const onAuthRequired = ({ items, url, attempt }: OnAuthRequiredParams): WebRequest.BlockingResponse => {
+export const resolveCredentials = ({ items, url, attempt }: OnAuthRequiredParams): WebRequest.BlockingResponse => {
     /** When we've exhausted all available credentials, allow
      * the request to proceed (will likely result in 401/403) */
     if (attempt >= items.length) return { cancel: false };
@@ -36,29 +38,64 @@ export const onAuthRequired = ({ items, url, attempt }: OnAuthRequiredParams): W
     };
 };
 
-export const createBasicAuthListener = () => {
+export type BasicAuthController = {
+    active: boolean;
+    listen: () => void;
+    destroy: () => void;
+};
+
+const basicAuthEnabled = async (): Promise<boolean> =>
+    BUILD_TARGET !== 'safari' && browser.webRequest.onAuthRequired && (await hasPermissions(WEB_REQUEST_PERMISSIONS));
+
+export const createBasicAuthController = (): BasicAuthController => {
     const authFilter = { urls: ['https://*/*', 'http://*/*'] };
     const authRequests = new Map<RequestID, AttemptCount>();
 
-    browser.webRequest.onAuthRequired.addListener(
-        withContext((ctx, { url, requestId }) => {
-            const attempt = authRequests.get(requestId) ?? 0;
+    const onAuthRequired = withContext((ctx, { url, requestId }: WebRequest.OnAuthRequiredDetailsType) => {
+        const attempt = authRequests.get(requestId) ?? 0;
 
-            try {
-                const authorized = ctx.getState().authorized;
-                const enabled = selectAutofillSettings(ctx.service.store.getState()).basicAuth ?? false;
-                if (!(authorized && enabled)) return { cancel: false };
+        try {
+            const authorized = ctx.getState().authorized;
+            const enabled = selectAutofillSettings(ctx.service.store.getState()).basicAuth ?? false;
+            if (!(authorized && enabled)) return { cancel: false };
 
-                const items = ctx.service.autofill.getLoginCandidates(parseUrl(url));
-                return onAuthRequired({ items, url, attempt });
-            } finally {
-                authRequests.set(requestId, attempt + 1);
+            const items = ctx.service.autofill.getLoginCandidates(parseUrl(url));
+            return resolveCredentials({ items, url, attempt });
+        } finally {
+            authRequests.set(requestId, attempt + 1);
+        }
+    });
+
+    const onCompleted = ({ requestId }: WebRequest.OnCompletedDetailsType) => authRequests.delete(requestId);
+    const onErrored = ({ requestId }: WebRequest.OnErrorOccurredDetailsType) => authRequests.delete(requestId);
+
+    const controller = {
+        active: false,
+        listen: () => {
+            if (!controller.active) {
+                controller.active = true;
+                basicAuthEnabled()
+                    .then((enabled) => {
+                        if (!enabled) throw new Error();
+                        logger.info('[BasicAuth] Listening for basic auth..');
+                        browser.webRequest.onAuthRequired.addListener(onAuthRequired, authFilter, ['blocking']);
+                        browser.webRequest.onCompleted.addListener(onCompleted, authFilter);
+                        browser.webRequest.onErrorOccurred.addListener(onErrored, authFilter);
+                    })
+                    .catch(() => logger.info('[BasicAuth] Failed initializating basic auth listeners'));
             }
-        }),
-        authFilter,
-        [BUILD_TARGET === 'firefox' ? 'blocking' : 'asyncBlocking']
-    );
+        },
+        destroy: () => {
+            if (controller.active) {
+                logger.info('[BasicAuth] Stopped listeners');
+                authRequests.clear();
+                controller.active = false;
+                browser.webRequest.onAuthRequired.removeListener(onAuthRequired);
+                browser.webRequest.onCompleted.removeListener(onCompleted);
+                browser.webRequest.onErrorOccurred.removeListener(onErrored);
+            }
+        },
+    };
 
-    browser.webRequest.onCompleted.addListener(({ requestId }) => authRequests.delete(requestId), authFilter);
-    browser.webRequest.onErrorOccurred.addListener(({ requestId }) => authRequests.delete(requestId), authFilter);
+    return controller;
 };
