ad4891e294155d1ca83ac6208c48e4e9b54d5ea5
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2025-06-12 16:02:12 +0200

    refactor: Move locateHead helper to @proton/mail/helpers and update imports

--- applications/mail/src/app/helpers/message/messageHead.ts
+++
@@ -1,9 +0,0 @@
-export const locateHead = (inputDocument: Element | Document | undefined): string | undefined => {
-    if (!inputDocument) {
-        return undefined;
-    }
-
-    const head = inputDocument.querySelector('head');
-
-    return head?.innerHTML;
-};
