3c6a1428a7121a09bd438653156fc52b95bc9590
Edvin CANDON <edvin.candon@proton.ch>
2025-06-12 14:43:32 +0200

    Improve `TextAreaReadonly`
    - Define max-height depending on window height
    - Clamp to line-height to avoid partial line cropping
    - Enable animation only when collapsing

--- packages/pass/components/Form/legacy/TextAreaReadonly.tsx
+++ packages/pass/components/Form/legacy/TextAreaReadonly.tsx
@@ -1,76 +1,84 @@
-import { type FC, type PropsWithChildren, useCallback, useLayoutEffect, useMemo, useRef, useState } from 'react';
+import type { FC, MouseEventHandler } from 'react';
+import { useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 import { c } from 'ttag';
 
-import { Button } from '@proton/atoms/src';
-import { FieldBox } from '@proton/pass/components/Form/Field/Layout/FieldBox';
-import { FieldsetCluster } from '@proton/pass/components/Form/Field/Layout/FieldsetCluster';
+import { Button } from '@proton/atoms';
 import clsx from '@proton/utils/clsx';
+import debounce from '@proton/utils/debounce';
 
 import './TextAreaReadonly.scss';
 
-const CONTAINED_HEIGHT = EXTENSION_BUILD ? 120 : 240;
+type Props = { children: string; className?: string };
+type ExpansionState = 'initial' | 'collapsed' | 'expanded';
 
-type Props = { children: string; className?: string; contained?: boolean };
+/** Clamp to multiple of body's line-height to
+ * prevent partial line cropping. */
+const getMaxHeight = () => {
+    const maxHeight = Math.max(window.innerHeight / 2, EXTENSION_BUILD ? 120 : 240);
+    const computedStyle = window.getComputedStyle(document.body);
+    const lineHeight = parseFloat(computedStyle.lineHeight);
 
-export const TextAreaReadonly: FC<Props> = ({ children, className, contained }) => {
-    const [height, setHeight] = useState(0);
-    const [isExpanded, setIsExpanded] = useState(false);
-    const [needsExpansion, setNeedsExpansion] = useState(false);
+    if (isNaN(lineHeight)) return maxHeight;
+    else return Math.floor(maxHeight / lineHeight) * lineHeight;
+};
+
+export const TextAreaReadonly: FC<Props> = ({ children, className }) => {
     const ref = useRef<HTMLTextAreaElement>(null);
-    const textAreaHeight = useMemo(
-        () => (!contained || isExpanded || height < CONTAINED_HEIGHT ? height : CONTAINED_HEIGHT),
-        [isExpanded, height]
-    );
+    const [scrollHeight, setScrollHeight] = useState(0);
+    const [expansionState, setExpansionState] = useState<ExpansionState>('initial');
+    const [maxHeight, setMaxHeight] = useState(getMaxHeight);
 
-    const Container: FC<PropsWithChildren> = useCallback(
-        (props) =>
-            contained ? (
-                <FieldsetCluster mode="read">
-                    <FieldBox>{props.children}</FieldBox>
-                </FieldsetCluster>
-            ) : (
-                props.children
-            ),
-        [contained]
-    );
+    const isExpanded = expansionState === 'expanded';
+    const needsExpansion = scrollHeight > maxHeight;
+    const shouldAnimate = expansionState !== 'initial';
+    const height = !needsExpansion || isExpanded ? scrollHeight : maxHeight;
+
+    const toggleExpansion = useCallback<MouseEventHandler>((evt) => {
+        evt.preventDefault();
+        evt.stopPropagation();
+        setExpansionState((prev) => (prev === 'expanded' ? 'collapsed' : 'expanded'));
+    }, []);
+
+    const preventSelectionClick = useCallback<MouseEventHandler>((evt) => {
+        if (ref.current) {
+            const { selectionStart, selectionEnd } = ref.current;
+            const hasSelection = selectionStart !== selectionEnd;
+            if (hasSelection) {
+                evt.preventDefault();
+                evt.stopPropagation();
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        const onResize = debounce(() => setMaxHeight(getMaxHeight), 50);
+        window.addEventListener('resize', onResize);
+        return () => window.removeEventListener('resize', onResize);
+    }, []);
 
     useLayoutEffect(() => {
         if (ref.current) {
             const scrollHeight = ref.current.scrollHeight;
-            setHeight(scrollHeight);
-            if (contained && !isExpanded) setNeedsExpansion(scrollHeight > CONTAINED_HEIGHT);
+            setScrollHeight(scrollHeight);
         }
-    }, [children, contained, isExpanded]);
+    }, [children, expansionState, maxHeight]);
 
     return (
-        <Container>
+        <>
             <textarea
                 ref={ref}
                 readOnly
                 value={children}
                 className={clsx(
-                    'w-full h-full text-pre-wrap resize-none h-custom pass-textarea--readonly anime-fade-in',
+                    'w-full h-full text-pre-wrap resize-none h-custom pass-textarea--readonly overflow-hidden',
+                    shouldAnimate && 'pass-textarea--animate',
                     className
                 )}
-                style={{
-                    '--h-custom': `${textAreaHeight}px`,
-                    transition: 'height 0.3s ease-out',
-                    opacity: 1,
-                }}
-                onClick={(evt) => {
-                    if (ref.current) {
-                        const { selectionStart, selectionEnd } = ref.current;
-                        const hasSelection = selectionStart !== selectionEnd;
-
-                        if (hasSelection) {
-                            evt.preventDefault();
-                            evt.stopPropagation();
-                        }
-                    }
-                }}
+                style={{ '--h-custom': `${height}px` }}
+                onClick={preventSelectionClick}
             />
-            {contained && needsExpansion && (
+            {needsExpansion && (
                 <>
                     {!isExpanded && <span className="mr-1">...</span>}
                     <Button
@@ -78,7 +86,7 @@ export const TextAreaReadonly: FC<Props> = ({ children, className, contained }) 
                         shape="underline"
                         className="link link-focus text-nowrap"
                         color="weak"
-                        onClick={() => setIsExpanded((prev) => !prev)}
+                        onClick={toggleExpansion}
                     >
                         <span className="line-height-1">
                             {isExpanded ? c('Action').t`Show Less` : c('Action').t`Read More`}
@@ -86,6 +94,6 @@ export const TextAreaReadonly: FC<Props> = ({ children, className, contained }) 
                     </Button>
                 </>
             )}
-        </Container>
+        </>
     );
 };
