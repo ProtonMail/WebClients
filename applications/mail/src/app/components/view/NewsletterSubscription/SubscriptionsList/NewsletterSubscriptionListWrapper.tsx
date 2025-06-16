e01bc92d29ceedf52e0f57e1dcd00879e40b8e42
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-06-16 12:38:06 +0000

    Reset scroll height on tab change

--- applications/mail/src/app/components/view/NewsletterSubscription/SubscriptionsList/NewsletterSubscriptionListWrapper.tsx
+++ applications/mail/src/app/components/view/NewsletterSubscription/SubscriptionsList/NewsletterSubscriptionListWrapper.tsx
@@ -1,4 +1,4 @@
-import { type PropsWithChildren } from 'react';
+import { type PropsWithChildren, useRef } from 'react';
 
 import { Scroll } from '@proton/atoms';
 
@@ -11,16 +11,23 @@ interface Props extends PropsWithChildren {
 const customHeight = 'calc(100% - 4.5rem)';
 
 export const NewsletterSubscriptionListWrapper = ({ isDisplayingPlaceholder = false, children }: Props) => {
+    const scrollRef = useRef<HTMLDivElement>(null);
+
+    // We scroll to the top when we change tab
+    const handleScrollToTop = () => {
+        scrollRef.current?.scrollTo({ top: 0, behavior: 'instant' });
+    };
+
     return (
         <>
             <div className="flex-1 border-right border-weak h-full">
-                <NewsletterSubscriptionListHeader />
+                <NewsletterSubscriptionListHeader tabClickCallback={handleScrollToTop} />
                 {isDisplayingPlaceholder ? (
                     <div className="h-custom flex flex-nowrap" style={{ '--h-custom': customHeight }}>
                         {children}
                     </div>
                 ) : (
-                    <Scroll className="h-custom" style={{ '--h-custom': customHeight }}>
+                    <Scroll className="h-custom" style={{ '--h-custom': customHeight }} customContainerRef={scrollRef}>
                         <div className="flex px-6 flex-column flex-nowrap pb-4 px-1">{children}</div>
                     </Scroll>
                 )}
