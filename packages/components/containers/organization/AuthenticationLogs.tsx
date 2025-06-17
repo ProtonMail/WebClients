1f66a809a2e01ff6f3bc138f2f0ec76da4342e12
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-06-17 14:51:43 +0200

    Revert "Merge branch 'activity-monitor' into 'main'"
    This reverts commit 1ed23b08ea5775fe6de06e8444782fb99abaf19a.

--- packages/components/containers/organization/AuthenticationLogs.tsx
+++ packages/components/containers/organization/AuthenticationLogs.tsx
@@ -4,7 +4,6 @@ import { endOfDay, startOfDay } from 'date-fns';
 import { c } from 'ttag';
 
 import { useMembers } from '@proton/account/members/hooks';
-import { organizationActions } from '@proton/account/organization';
 import { Button } from '@proton/atoms';
 import AddressesInput, { AddressesInputItem } from '@proton/components/components/addressesInput/AddressesInput';
 import DateInput from '@proton/components/components/input/DateInput';
@@ -21,10 +20,9 @@ import useActiveBreakpoint from '@proton/components/hooks/useActiveBreakpoint';
 import useApi from '@proton/components/hooks/useApi';
 import useNotifications from '@proton/components/hooks/useNotifications';
 import useLoading from '@proton/hooks/useLoading';
-import { useDispatch } from '@proton/redux-shared-store/sharedProvider';
 import type { B2BAuthLog } from '@proton/shared/lib/authlog';
 import { validateEmailAddress } from '@proton/shared/lib/helpers/email';
-import type { OrganizationExtended, OrganizationSettings, Recipient } from '@proton/shared/lib/interfaces';
+import type { OrganizationExtended, Recipient } from '@proton/shared/lib/interfaces';
 import clsx from '@proton/utils/clsx';
 import noop from '@proton/utils/noop';
 
@@ -46,7 +44,6 @@ interface Props {
     detailedMonitoringOption?: boolean;
     onLogsLoaded: (logs: B2BAuthLog[]) => void;
 }
-
 export interface AuthLogsQueryParams {
     Emails: string[];
     StartTime?: string;
@@ -60,6 +57,7 @@ const AuthenticationLogs = ({
     wipeLogs = false,
     reloadTrigger = 0,
     monitoring = false,
+    detailedMonitoringOption = false,
     onLogsLoaded,
 }: Props) => {
     const api = useApi();
@@ -72,11 +70,9 @@ const AuthenticationLogs = ({
     const { filter, handleStartDateChange, handleEndDateChange } = useAuthLogsDateFilter();
     const [recipients, setRecipient] = useState<Recipient[]>([]);
     const addressesAutocompleteRef = useRef<HTMLInputElement>(null);
+    const [detailedMonitoring, setDetailedMonitoring] = useState(detailedMonitoringOption);
     const [detailedMonitoringLoading, withLoadingDetailedMonitoring] = useLoading();
     const [recipientsModalProps, recipientsModalOpen, recipientsModalRender] = useModalState();
-    const dispatch = useDispatch();
-
-    const detailedMonitoring = organization?.Settings?.LogAuth === 2;
 
     const { viewportWidth } = useActiveBreakpoint();
 
@@ -97,13 +93,26 @@ const AuthenticationLogs = ({
         });
     }, [recipients]);
 
+    useEffect(() => {
+        const loadMonitoringSetting = async () => {
+            try {
+                if (organization?.Settings?.LogAuth === 2) {
+                    setDetailedMonitoring(true);
+                } else {
+                    setDetailedMonitoring(false);
+                }
+            } catch (error) {}
+        };
+
+        void loadMonitoringSetting();
+    }, []);
+
     const handleSetDetailedMonitoring = async () => {
         const enabling = !detailedMonitoring;
 
         try {
-            const newValue = enabling ? 2 : 1;
-            await api<OrganizationSettings>(updateMonitoringSetting(newValue));
-            dispatch(organizationActions.updateOrganizationSettings({ value: { LogAuth: newValue } }));
+            await api(updateMonitoringSetting(enabling ? 2 : 0));
+            setDetailedMonitoring(enabling);
         } catch (e) {
             createNotification({ type: 'error', text: c('Error').t`Failed to update detailed monitoring.` });
         }
