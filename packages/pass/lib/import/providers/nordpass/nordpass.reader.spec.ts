9a8c87aacd41dd56c15fd9532a757f58ff09daf4
Edvin CANDON <edvin.candon@proton.ch>
2025-06-17 13:31:16 +0200

    Clean-up NordPass importer tests
    [IDTEAM-4854]

--- packages/pass/lib/import/providers/nordpass/nordpass.reader.spec.ts
+++ packages/pass/lib/import/providers/nordpass/nordpass.reader.spec.ts
@@ -2,129 +2,151 @@ import fs from 'fs';
 
 import type { ImportPayload } from '@proton/pass/lib/import/types';
 import { deobfuscateItem } from '@proton/pass/lib/items/item.obfuscation';
-import type { ItemImportIntent } from '@proton/pass/types';
+import type { ItemType } from '@proton/pass/types';
 import * as epochUtils from '@proton/pass/utils/time/epoch';
 
 import { readNordPassData } from './nordpass.reader';
 
-describe('Import NordPass csv', () => {
-    let payload: ImportPayload;
+const sourceFiles = {
+    'nordpass-legacy.csv': `${__dirname}/mocks/nordpass-legacy.csv`,
+    'nordpass.csv': `${__dirname}/mocks/nordpass.csv`,
+};
 
-    const dateMock = jest.spyOn(epochUtils, 'getEpoch').mockImplementation(() => 1682585156);
+const data: Record<string, ImportPayload> = {};
+const dateMock = jest.spyOn(epochUtils, 'getEpoch').mockImplementation(() => 1682585156);
 
+const getNordPassData = (sourceKey: string) => data[sourceKey];
+const getNordPassItem = <T extends ItemType>(sourceKey: string, vaultIndex: number = 0, itemIndex: number = 0) =>
+    deobfuscateItem<T>(data[sourceKey].vaults[vaultIndex].items[itemIndex]);
+
+describe('NordPass CSV Importer', () => {
     beforeAll(async () => {
-        const sourceData = fs.readFileSync(__dirname + '/mocks/nordpass.csv');
-        const file = new File([sourceData], 'nordpass.csv');
-        payload = await readNordPassData(file);
+        for (const [key, sourceFile] of Object.entries(sourceFiles)) {
+            const sourceData = fs.readFileSync(sourceFile);
+            const file = new File([sourceData], sourceFile);
+            data[key] = await readNordPassData(file);
+        }
     });
 
     afterAll(() => dateMock.mockRestore());
 
-    it('should throw on corrupted files', async () => {
+    test('should throw on corrupted files', async () => {
         await expect(readNordPassData(new File([], 'corrupted'))).rejects.toThrow();
     });
 
-    it('converts NordPass folders to vaults correctly', () => {
-        const [primary, secondary] = payload.vaults;
-        expect(payload.vaults.length).toEqual(2);
-        expect(primary.name).toEqual('company services');
-        expect(secondary.name).toEqual('Import - 27 Apr 2023');
-    });
+    describe('Nordpass legacy CSV', () => {
+        test('converts NordPass folders to vaults correctly', () => {
+            const { vaults } = getNordPassData('nordpass-legacy.csv');
+            const [primary, secondary] = vaults;
+            expect(vaults.length).toEqual(2);
+            expect(primary.name).toEqual('company services');
+            expect(secondary.name).toEqual('Import - 27 Apr 2023');
+            expect(primary.items.length).toEqual(4);
+            expect(secondary.items.length).toEqual(5);
+        });
 
-    it('parses primary vault items correctly', async () => {
-        const [primary] = payload.vaults;
-        expect(primary.items.length).toEqual(4);
-        /* Login */
-        const loginItem2 = deobfuscateItem(primary.items[0]) as unknown as ItemImportIntent<'login'>;
-        expect(loginItem2.type).toEqual('login');
-        expect(loginItem2.metadata.name).toEqual('Admin');
-        expect(loginItem2.metadata.note).toEqual('');
-        expect(loginItem2.content.itemEmail).toEqual('');
-        expect(loginItem2.content.itemUsername).toEqual('admin');
-        expect(loginItem2.content.password).toEqual('proton123');
-        expect(loginItem2.content.urls[0]).toEqual('https://proton.me/');
+        test('should support login items [vault 1]', () => {
+            const item = getNordPassItem<'login'>('nordpass-legacy.csv', 0, 0);
+            expect(item.type).toEqual('login');
+            expect(item.metadata.name).toEqual('Admin');
+            expect(item.metadata.note).toEqual('');
+            expect(item.content.itemEmail).toEqual('');
+            expect(item.content.itemUsername).toEqual('admin');
+            expect(item.content.password).toEqual('proton123');
+            expect(item.content.urls[0]).toEqual('https://proton.me/');
+        });
 
-        /* Login */
-        const loginItem3 = deobfuscateItem(primary.items[1]) as unknown as ItemImportIntent<'login'>;
-        expect(loginItem3.type).toEqual('login');
-        expect(loginItem3.metadata.name).toEqual('Twitter');
-        expect(loginItem3.metadata.note).toEqual('This is a twitter note');
-        expect(loginItem3.content.itemEmail).toEqual('');
-        expect(loginItem3.content.itemUsername).toEqual('@nobody');
-        expect(loginItem3.content.password).toEqual('proton123');
-        expect(loginItem3.content.urls[0]).toEqual('https://twitter.com/login');
-        expect(loginItem3.content.totpUri).toEqual('');
+        test('should support login items with notes [vault 1]', () => {
+            const item = getNordPassItem<'login'>('nordpass-legacy.csv', 0, 1);
+            expect(item.type).toEqual('login');
+            expect(item.metadata.name).toEqual('Twitter');
+            expect(item.metadata.note).toEqual('This is a twitter note');
+            expect(item.content.itemEmail).toEqual('');
+            expect(item.content.itemUsername).toEqual('@nobody');
+            expect(item.content.password).toEqual('proton123');
+            expect(item.content.urls[0]).toEqual('https://twitter.com/login');
+            expect(item.content.totpUri).toEqual('');
+        });
 
-        /* Login */
-        const loginItem4 = deobfuscateItem(primary.items[2]) as unknown as ItemImportIntent<'login'>;
-        expect(loginItem4.type).toEqual('login');
-        expect(loginItem4.metadata.name).toEqual('fb.com');
-        expect(loginItem4.metadata.note).toEqual('');
-        expect(loginItem4.content.itemEmail).toEqual('');
-        expect(loginItem4.content.itemUsername).toEqual('@nobody');
-        expect(loginItem4.content.password).toEqual('proton123');
-        expect(loginItem4.content.urls[0]).toEqual('https://fb.com/login');
-        expect(loginItem4.content.totpUri).toEqual('');
+        test('should support login items [vault 1]', () => {
+            const item = getNordPassItem<'login'>('nordpass-legacy.csv', 0, 2);
+            expect(item.type).toEqual('login');
+            expect(item.metadata.name).toEqual('fb.com');
+            expect(item.metadata.note).toEqual('');
+            expect(item.content.itemEmail).toEqual('');
+            expect(item.content.itemUsername).toEqual('@nobody');
+            expect(item.content.password).toEqual('proton123');
+            expect(item.content.urls[0]).toEqual('https://fb.com/login');
+            expect(item.content.totpUri).toEqual('');
+        });
 
-        /* Login broken url */
-        const loginItem5 = deobfuscateItem(primary.items[3]) as unknown as ItemImportIntent<'login'>;
-        expect(loginItem5.type).toEqual('login');
-        expect(loginItem5.metadata.name).toEqual('htts');
-        expect(loginItem5.metadata.note).toEqual('');
-        expect(loginItem5.content.itemEmail).toEqual('');
-        expect(loginItem5.content.itemUsername).toEqual('');
-        expect(loginItem5.content.password).toEqual('');
-        expect(loginItem5.content.urls).toEqual([]);
-    });
+        test('should support login items with malformed urls [vault 1]', () => {
+            const item = getNordPassItem<'login'>('nordpass-legacy.csv', 0, 3);
+            expect(item.type).toEqual('login');
+            expect(item.metadata.name).toEqual('htts');
+            expect(item.metadata.note).toEqual('');
+            expect(item.content.itemEmail).toEqual('');
+            expect(item.content.itemUsername).toEqual('');
+            expect(item.content.password).toEqual('');
+            expect(item.content.urls).toEqual([]);
+        });
 
-    it('parses secondary vault items correctly', () => {
-        const [, secondary] = payload.vaults;
-        expect(secondary.items.length).toEqual(5);
+        test('should support login items [vault 2]', () => {
+            const item = getNordPassItem<'login'>('nordpass-legacy.csv', 1, 0);
+            expect(item.type).toEqual('login');
+            expect(item.metadata.name).toEqual('nobody');
+            expect(item.metadata.note).toEqual('Secure note');
+            expect(item.content.itemEmail).toEqual('nobody@proton.me');
+            expect(item.content.itemUsername).toEqual('');
+            expect(item.content.password).toEqual('proton123');
+            expect(item.content.urls[0]).toEqual('https://account.proton.me/');
+        });
 
-        /* Login */
-        const loginItem1 = deobfuscateItem(secondary.items[0]) as unknown as ItemImportIntent<'login'>;
-        expect(loginItem1.type).toEqual('login');
-        expect(loginItem1.metadata.name).toEqual('nobody');
-        expect(loginItem1.metadata.note).toEqual('Secure note');
-        expect(loginItem1.content.itemEmail).toEqual('nobody@proton.me');
-        expect(loginItem1.content.itemUsername).toEqual('');
-        expect(loginItem1.content.password).toEqual('proton123');
-        expect(loginItem1.content.urls[0]).toEqual('https://account.proton.me/');
+        test('should support note items [vault 2]', () => {
+            const item = getNordPassItem<'note'>('nordpass-legacy.csv', 1, 1);
+            expect(item.type).toEqual('note');
+            expect(item.metadata.name).toEqual('Secure note');
+            expect(item.metadata.note).toEqual('This is a secure note');
+        });
 
-        /* Note */
-        const noteItem1 = deobfuscateItem(secondary.items[1]) as unknown as ItemImportIntent<'login'>;
-        expect(noteItem1.type).toEqual('note');
-        expect(noteItem1.metadata.name).toEqual('Secure note');
-        expect(noteItem1.metadata.note).toEqual('This is a secure note');
+        test('should support multi-line note items [vault 2]', () => {
+            const item = getNordPassItem<'note'>('nordpass-legacy.csv', 1, 2);
+            expect(item.type).toEqual('note');
+            expect(item.metadata.name).toEqual('test');
+            expect(item.metadata.note).toEqual('Bank Name: test\n\nAccount Type: test\n\nAccount Number: 333333');
+        });
 
-        /* Credit Card */
-        const creditCardItem1 = deobfuscateItem(secondary.items[3]) as unknown as ItemImportIntent<'creditCard'>;
-        expect(creditCardItem1.type).toEqual('creditCard');
-        expect(creditCardItem1.metadata.name).toEqual('Credit Card Item with note');
-        expect(creditCardItem1.metadata.note).toEqual('this is a note for the credit card');
-        expect(creditCardItem1.content.cardholderName).toEqual('A B');
-        expect(creditCardItem1.content.number).toEqual('4242424242424242');
-        expect(creditCardItem1.content.expirationDate).toEqual('012025');
-        expect(creditCardItem1.content.verificationNumber).toEqual('123');
-        expect(creditCardItem1.content.pin).toEqual('');
+        test('should support credit-card items [vault 2]', () => {
+            const item = getNordPassItem<'creditCard'>('nordpass-legacy.csv', 1, 3);
+            expect(item.type).toEqual('creditCard');
+            expect(item.metadata.name).toEqual('Credit Card Item with note');
+            expect(item.metadata.note).toEqual('this is a note for the credit card');
+            expect(item.content.cardholderName).toEqual('A B');
+            expect(item.content.number).toEqual('4242424242424242');
+            expect(item.content.expirationDate).toEqual('012025');
+            expect(item.content.verificationNumber).toEqual('123');
+            expect(item.content.pin).toEqual('');
+        });
 
-        /* Identity */
-        const identityItem = deobfuscateItem(secondary.items[4]) as unknown as ItemImportIntent<'identity'>;
-        expect(identityItem.type).toEqual('identity');
-        expect(identityItem.metadata.name).toEqual(':title:');
-        expect(identityItem.metadata.note).toEqual(':note:');
-        expect(identityItem.content.fullName).toEqual(':full-name:');
-        expect(identityItem.content.email).toEqual(':email:');
-        expect(identityItem.content.phoneNumber).toEqual(':phone-number:');
-        expect(identityItem.content.streetAddress).toEqual(':address-1:');
-        expect(identityItem.content.zipOrPostalCode).toEqual(':zip:');
-        expect(identityItem.content.city).toEqual(':city:');
-        expect(identityItem.content.stateOrProvince).toEqual(':state:');
-        expect(identityItem.content.countryOrRegion).toEqual("Côte d'Ivoire");
+        test('should support identity items [vault 2]', () => {
+            const item = getNordPassItem<'identity'>('nordpass-legacy.csv', 1, 4);
+            expect(item.type).toEqual('identity');
+            expect(item.metadata.name).toEqual(':title:');
+            expect(item.metadata.note).toEqual(':note:');
+            expect(item.content.fullName).toEqual(':full-name:');
+            expect(item.content.email).toEqual(':email:');
+            expect(item.content.phoneNumber).toEqual(':phone-number:');
+            expect(item.content.streetAddress).toEqual(':address-1:');
+            expect(item.content.zipOrPostalCode).toEqual(':zip:');
+            expect(item.content.city).toEqual(':city:');
+            expect(item.content.stateOrProvince).toEqual(':state:');
+            expect(item.content.countryOrRegion).toEqual("Côte d'Ivoire");
+        });
     });
 
     test('correctly keeps a reference to ignored items', () => {
-        expect(payload.ignored).not.toEqual([]);
-        expect(payload.ignored[0]).toEqual('[folder] company services');
+        const { ignored } = getNordPassData('nordpass-legacy.csv');
+        expect(ignored).not.toEqual([]);
+        expect(ignored[0]).toEqual('[folder] company services');
     });
 });
