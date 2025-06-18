d3dbe8c8e3c6f95ae8fb70935db97e652e92b2e5
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2025-06-18 12:47:34 +0200

    Improve autotype Rust performance
    [IDTEAM-4336]

--- applications/pass-desktop/native/src/autotypes.rs
+++ applications/pass-desktop/native/src/autotypes.rs
@@ -1,21 +1,26 @@
 use anyhow::{Error, Result};
 use enigo::{Direction::Click, Enigo, Key, Keyboard, Settings};
 
-pub struct Autotype;
+pub struct Autotype {
+    enigo: Enigo,
+}
 
 impl Autotype {
-    pub fn text(text: &str) -> Result<(), Error> {
-        let mut enigo = Enigo::new(&Settings::default())?;
-        enigo.text(text).map_err(|e| e.into())
+    pub fn new() -> Result<Self, Error> {
+        Ok(Self {
+            enigo: Enigo::new(&Settings::default())?,
+        })
     }
 
-    pub fn tab() -> Result<(), Error> {
-        let mut enigo = Enigo::new(&Settings::default())?;
-        enigo.key(Key::Tab, Click).map_err(|e| e.into())
+    pub fn text(&mut self, text: &str) -> Result<(), Error> {
+        self.enigo.text(text).map_err(|e| e.into())
     }
 
-    pub fn enter() -> Result<(), Error> {
-        let mut enigo = Enigo::new(&Settings::default())?;
-        enigo.key(Key::Return, Click).map_err(|e| e.into())
+    pub fn tab(&mut self) -> Result<(), Error> {
+        self.enigo.key(Key::Tab, Click).map_err(|e| e.into())
+    }
+
+    pub fn enter(&mut self) -> Result<(), Error> {
+        self.enigo.key(Key::Return, Click).map_err(|e| e.into())
     }
 }
