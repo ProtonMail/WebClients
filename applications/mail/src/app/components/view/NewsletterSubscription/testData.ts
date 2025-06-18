22e9efd9dc3a3ebbbb5f74e8572dd849945088fb
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-06-18 12:48:50 +0000

    Newsletter card and related components tests

---
+++ applications/mail/src/app/components/view/NewsletterSubscription/testData.ts
@@ -0,0 +1,32 @@
+import type { NewsletterSubscription } from '@proton/shared/lib/interfaces/NewsletterSubscription';
+
+export const activeSubscription: NewsletterSubscription = {
+    ID: 'active-1',
+    UserId: 'user-1',
+    AddressId: 'address-1',
+    ListId: 'list-1',
+    SenderAddress: 'sender-1@proton.me',
+    BimiSelector: 'bimi-1',
+    Name: 'Active Subscription',
+    UnsubscribedTime: 0,
+    FirstReceivedTime: '2021-01-01',
+    LastReceivedTime: '2021-01-01',
+    LastReadTime: '2021-01-01',
+    ReceivedMessageCount: 10,
+    UnreadMessageCount: 5,
+    MarkAsRead: false,
+    MoveToFolder: null,
+    ReceivedMessages: {
+        Total: 10,
+        Last30Days: 5,
+        Last90Days: 3,
+    },
+    UnsubscribeMethods: {
+        HttpClient: '',
+    },
+};
+
+export const unsubscribedSubscription: NewsletterSubscription = {
+    ...activeSubscription,
+    UnsubscribedTime: 1718745600,
+};
