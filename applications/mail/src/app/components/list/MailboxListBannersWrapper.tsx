40ffcfa9013aadce7aead75d64a613962a9c1169
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-06-18 14:05:50 +0000

    Merge branch 'MAILWEB-6949-Show-select-all-banner-in-list-refactor' into 'main'
    Show Select all banner in list refactor
    
    See merge request web/clients!17612

--- applications/mail/src/app/components/list/MailboxListBannersWrapper.tsx
+++ applications/mail/src/app/components/list/MailboxListBannersWrapper.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import SelectAllBanner from 'proton-mail/components/list/select-all/SelectAllBanner';
+import { getCanDisplaySelectAllBanner } from 'proton-mail/helpers/selectAll';
+import { useSelectAll } from 'proton-mail/hooks/useSelectAll';
 
 import { showLabelTaskRunningBanner } from '../../store/elements/elementsSelectors';
 import { useMailSelector } from '../../store/hooks';
@@ -7,20 +9,42 @@ import { useMailboxListContext } from './MailboxListProvider';
 
 interface MailboxListBannersWrapperProps {
     columnLayout: boolean;
+    checkedIDs: string[];
+    onCheckAll: (check: boolean) => void;
 }
 
-const MailboxListBannersWrapper = ({ columnLayout }: MailboxListBannersWrapperProps) => {
-    const { labelID = '', isESLoading, isSearch, showESSlowToolbar } = useMailboxListContext();
+const MailboxListBannersWrapper = ({ columnLayout, checkedIDs, onCheckAll }: MailboxListBannersWrapperProps) => {
+    const { labelID = '', isESLoading, isSearch, showESSlowToolbar, pageSize, filter } = useMailboxListContext();
+    const { selectAllAvailable } = useSelectAll({ labelID });
 
     const canDisplayTaskRunningBanner = useMailSelector((state) => showLabelTaskRunningBanner(state, { labelID }));
 
+    const hasFilter = Object.keys(filter).length > 0;
+
+    const canShowSelectAllBanner = getCanDisplaySelectAllBanner({
+        selectAllFeatureAvailable: selectAllAvailable,
+        mailPageSize: pageSize,
+        checkedIDs,
+        labelID,
+        isSearch,
+        hasFilter,
+    });
+
     return (
-        <ListBanners
-            labelID={labelID}
-            columnLayout={columnLayout}
-            esState={{ isESLoading, isSearch, showESSlowToolbar }}
-            canDisplayTaskRunningBanner={canDisplayTaskRunningBanner}
-        />
+        <>
+            {canShowSelectAllBanner && (
+                <div className="shrink-0">
+                    <SelectAllBanner labelID={labelID} onCheckAll={onCheckAll} />
+                </div>
+            )}
+
+            <ListBanners
+                labelID={labelID}
+                columnLayout={columnLayout}
+                esState={{ isESLoading, isSearch, showESSlowToolbar }}
+                canDisplayTaskRunningBanner={canDisplayTaskRunningBanner}
+            />
+        </>
     );
 };
 
