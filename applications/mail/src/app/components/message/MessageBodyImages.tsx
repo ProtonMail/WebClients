4e806be4e522789e7a423b6893584aa75a285f60
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2025-06-19 16:55:17 +0200

    refactor: Move MessageBodyImage to @proton/mail-renderer and update references in MessageBodyImages

--- applications/mail/src/app/components/message/MessageBodyImages.tsx
+++ applications/mail/src/app/components/message/MessageBodyImages.tsx
@@ -1,11 +1,10 @@
 import type { RefObject } from 'react';
 import { useEffect, useRef } from 'react';
 
+import MessageBodyImage from '@proton/mail-renderer/components/MessageBodyImage';
 import type { MessageImages } from '@proton/mail/store/messages/messagesTypes';
 
 import type { OnMessageImageLoadError } from 'proton-mail/components/message/interface';
-
-import MessageBodyImage from './MessageBodyImage';
 
 interface Props {
     messageImages: MessageImages | undefined;
