701687cc53b5de12b790ddfb7c20dda4e90d50b2
Matthieu Lux <matthieu.lux@proton.ch>
2025-06-19 12:06:43 +0000

    Stop search reset on vault navigation

--- packages/pass/jest.config.js
+++ packages/pass/jest.config.js
@@ -5,9 +5,12 @@ module.exports = {
     transformIgnorePatterns: [
         'node_modules/(?!(@proton/shared|@proton/components|@protontech/telemetry|jsmimeparser|@protontech/mutex-browser|pmcrypto|openpgp|@openpgp/web-stream-tools|@protontech/bip39|otpauth|@protontech/pass-rust-core/ui|@protontech/pass-rust-core/worker|client-zip)/)',
     ],
-    transform: { '^.+\\.(m?js|tsx?)$': 'babel-jest' },
+    transform: {
+        '^.+\\.(m?js|tsx?)$': '<rootDir>/jest.transform.js',
+    },
     coverageReporters: ['text-summary', 'json'],
     moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm)$': '@proton/components/__mocks__/fileMock.js',
         '\\.(css|scss|less)$': '@proton/components/__mocks__/styleMock.js',
     },
     reporters: ['default', ['jest-junit', { suiteNameTemplate: '{filepath}', outputName: 'test-report.xml' }]],
