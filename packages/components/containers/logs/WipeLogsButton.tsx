52cb95ee45c92ef0cee727faa50d71b33bc19962
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-06-20 16:38:13 +0200

    Remove deprecated modal usage in WipeLogsButton

--- packages/components/containers/logs/WipeLogsButton.tsx
+++ packages/components/containers/logs/WipeLogsButton.tsx
@@ -1,9 +1,8 @@
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms';
-import Alert from '@proton/components/components/alert/Alert';
-import ConfirmModal from '@proton/components/components/modal/Confirm';
-import useModals from '@proton/components/hooks/useModals';
+import useModalState from '@proton/components/components/modalTwo/useModalState';
+import Prompt from '@proton/components/components/prompt/Prompt';
 
 interface Props {
     onWipe: () => Promise<void>;
@@ -12,24 +11,36 @@ interface Props {
 }
 
 const WipeLogsButton = ({ onWipe, className, loading }: Props) => {
-    const { createModal } = useModals();
-
-    const handleOpenModal = () => {
-        createModal(
-            <ConfirmModal
-                title={c('Title').t`Delete logs`}
-                onConfirm={onWipe}
-                confirm={<Button color="danger" type="submit">{c('Action').t`Delete`}</Button>}
-            >
-                <Alert className="mb-4" type="error">{c('Info')
-                    .t`Are you sure you want to permanently delete all your logs?`}</Alert>
-            </ConfirmModal>
-        );
-    };
+    const [modalProps, setModal, renderModal] = useModalState();
 
     return (
-        <Button shape="outline" className={className} loading={loading} onClick={handleOpenModal}>{c('Action')
-            .t`Wipe`}</Button>
+        <>
+            {renderModal && (
+                <Prompt
+                    {...modalProps}
+                    onClose={modalProps.onClose}
+                    title={c('Action').t`Delete logs`}
+                    buttons={[
+                        <Button color="danger" onClick={onWipe}>
+                            {c('Action').t`Delete logs`}
+                        </Button>,
+                        <Button onClick={modalProps.onClose}>{c('Action').t`Cancel`}</Button>,
+                    ]}
+                >
+                    {c('Info').t`Are you sure you want to permanently delete all your logs?`}
+                </Prompt>
+            )}
+            <Button
+                shape="outline"
+                className={className}
+                loading={loading}
+                onClick={() => {
+                    setModal(true);
+                }}
+            >
+                {c('Action').t`Wipe`}
+            </Button>
+        </>
     );
 };
 
