9be04e12a3eb4ea5e540c738c6887183222f3bb1
Guillaume GONZALEZ <guillaume.gonzalez@proton.ch>
2025-06-20 13:19:34 +0200

    refactor: Move `cleanProxyImagesFromClipboardContent` and related tests to @proton/mail package

--- applications/mail/src/app/helpers/message/messageContent.ts
+++ applications/mail/src/app/helpers/message/messageContent.ts
@@ -2,11 +2,9 @@ import { DEFAULT_FONT_FACE_ID, DEFAULT_FONT_SIZE } from '@proton/components/comp
 import type { MESSAGE_ACTIONS } from '@proton/mail-renderer/constants';
 import { MESSAGE_IFRAME_ROOT_ID } from '@proton/mail-renderer/constants';
 import type { MessageState, PartialMessageState } from '@proton/mail/store/messages/messagesTypes';
-import { PROXY_IMG_URL } from '@proton/shared/lib/api/images';
 import { checkContrast, parseStringToDOM } from '@proton/shared/lib/helpers/dom';
 import type { Address, MailSettings, UserSettings } from '@proton/shared/lib/interfaces';
 import type { Message } from '@proton/shared/lib/interfaces/mail/Message';
-import { removeProxyUrlsFromContent } from '@proton/shared/lib/mail/images';
 import { isPlainText, isPlainText as testIsPlainText } from '@proton/shared/lib/mail/messages';
 import { unescape } from '@proton/shared/lib/sanitize/escape';
 
@@ -235,63 +233,4 @@ export const insertTextBeforeContent = (
             : `<div style="${fontStyles}">${textToInsertHTML}</div><br>${messageBody}`;
     }
     return newBody;
-};
-
-/**
- * When the user has the setting "Block email tracking" enabled, we are loading images through the Proton proxy.
- * However, when the user is copying content from an email (including images) and pasting them somewhere else, they
- * won't be loaded. Additionally, we don't want to leak the UID.
- */
-export const cleanProxyImagesFromClipboardContent = (
-    type: 'copy' | 'drag',
-    event: Event,
-    selection: Selection | null | undefined
-) => {
-    const range = selection?.rangeCount ? selection.getRangeAt(0) : null;
-
-    if (range) {
-        // Clone content so that we can apply transformations to it
-        const clonedSelection = range.cloneContents();
-
-        // Serialize the cloned content to an HTML string
-        const serializer = new XMLSerializer();
-        const html = Array.from(clonedSelection.childNodes)
-            .map((node) => serializer.serializeToString(node))
-            .join('');
-
-        // Convert the HTML string into a document for manipulation
-        const selectionContent = parseStringToDOM(html);
-
-        // Replace all proxy images with their original URL
-        const updatedContent = removeProxyUrlsFromContent(selectionContent);
-
-        // Update clipboard data / data transfer with the updated HTML and plaintext
-        if (type === 'copy') {
-            const updatedEvent = event as ClipboardEvent;
-            updatedEvent.clipboardData?.setData('text/html', updatedContent.body.innerHTML);
-            updatedEvent.clipboardData?.setData('text/plain', toText(updatedContent.body.innerHTML));
-            event.preventDefault();
-        } else if (type === 'drag') {
-            const updatedEvent = event as DragEvent;
-            updatedEvent.dataTransfer?.setData('text/html', updatedContent.body.innerHTML);
-            updatedEvent.dataTransfer?.setData('text/plain', toText(updatedContent.body.innerHTML));
-        }
-    } else if (type === 'drag' && event.target && (event.target as HTMLElement).tagName === 'IMG') {
-        // When the user is doing a drag&drop on a single image, we cannot get a range, of course the range's content.
-        // In that case, we need to replace the proxy url manually
-        const updatedEvent = event as DragEvent;
-        const clonedImg = (event.target as HTMLImageElement).cloneNode(true) as HTMLImageElement;
-
-        const isProxyUrl = clonedImg.src.includes(PROXY_IMG_URL);
-        if (isProxyUrl) {
-            const originalUrl = new URL(clonedImg.src).searchParams.get('Url');
-
-            if (originalUrl) {
-                clonedImg.setAttribute('src', originalUrl);
-
-                updatedEvent.dataTransfer?.setData('text/html', clonedImg.outerHTML);
-                updatedEvent.dataTransfer?.setData('text/plain', toText(clonedImg.outerHTML));
-            }
-        }
-    }
 };
