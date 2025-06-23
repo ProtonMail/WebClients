b6f76391ab7eeba9730ddcbbe7bea26e94b89e32
Edvin CANDON <edvin.candon@proton.ch>
2025-06-23 18:49:37 +0200

    Fix proto enum extractions

--- packages/pass/types/protobuf/item-v1.static.ts
+++ packages/pass/types/protobuf/item-v1.static.ts
@@ -1,4 +1,27 @@
 /* eslint-disable */
+export enum WifiSecurity {
+    /**
+     * @generated from protobuf enum value: UnspecifiedWifiSecurity = 0;
+     */
+    UnspecifiedWifiSecurity = 0,
+    /**
+     * @generated from protobuf enum value: WPA = 1;
+     */
+    WPA = 1,
+    /**
+     * @generated from protobuf enum value: WPA2 = 2;
+     */
+    WPA2 = 2,
+    /**
+     * @generated from protobuf enum value: WPA3 = 3;
+     */
+    WPA3 = 3,
+    /**
+     * @generated from protobuf enum value: WEP = 4;
+     */
+    WEP = 4,
+}
+
 export enum CardType {
     /**
      * @generated from protobuf enum value: Unspecified = 0;
