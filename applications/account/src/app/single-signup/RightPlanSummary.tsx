68ce56437e58f946da3ce421c3cc548aeafb764f
Pedro Lavarinhas Amaro <pedro.lavarinhas.amaro@proton.ch>
2025-06-25 11:58:44 +0000

    [MSA-683] B2B Trials: reflect single sign up v1 figma

--- applications/account/src/app/single-signup/RightPlanSummary.tsx
+++ applications/account/src/app/single-signup/RightPlanSummary.tsx
@@ -1,7 +1,5 @@
 import type { ReactNode } from 'react';
 
-import { c } from 'ttag';
-
 import type { PlanCardFeatureDefinition } from '@proton/components/containers/payments/features/interface';
 import { PlanCardFeatureList } from '@proton/components/containers/payments/subscription/PlanCardFeatures';
 
@@ -10,22 +8,15 @@ interface Props {
     title: ReactNode;
     features: PlanCardFeatureDefinition[];
     bundle?: { title: string; features: PlanCardFeatureDefinition[] }[];
-    trial?: boolean;
-    isB2bPlan: boolean;
 }
 
-const RightPlanSummary = ({ logo, title, features, bundle, trial, isB2bPlan }: Props) => {
+const RightPlanSummary = ({ logo, title, features, bundle }: Props) => {
     return (
         <div className="flex flex-column gap-4">
             <div>
                 <div className="inline-block border rounded-lg p-2">{logo}</div>
             </div>
             <div className="color-weak text-semibold">{title}</div>
-            {isB2bPlan && trial && (
-                <span className="color-success text-left text-sm text-bold">
-                    {c('b2b_trials_2025_Info').t`Try it free for 14 days`}
-                </span>
-            )}
             <PlanCardFeatureList
                 odd={false}
                 margin={false}
