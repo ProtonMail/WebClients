deefe6062dc1edaf1c86a8d7a4f3cb7bb3003850
Daniel Guardiola <daniel.guardiola@proton.ch>
2025-06-26 12:33:30 +0000

    Sheets: ui work 3 - The Return of the King

--- applications/docs-editor/src/app/Containers/Spreadsheet/components/ui.tsx
+++ applications/docs-editor/src/app/Containers/Spreadsheet/components/ui.tsx
@@ -1,14 +1,45 @@
 import * as Ariakit from '@ariakit/react'
 import type { IconName } from '@proton/components'
-import { Icon } from '@proton/components'
 import clsx from '@proton/utils/clsx'
-import { forwardRef } from 'react'
+import type { ReactElement } from 'react'
+import { type ComponentPropsWithoutRef, forwardRef, isValidElement } from 'react'
+import { Icon as LegacyIcon } from '@proton/components'
+import * as Atoms from './atoms'
 
-const SELECT_AND_MENU_ITEM_CLASSES =
-  'flex h-9 select-none items-center px-4 text-[.875rem] text-[#0C0C14] hover:bg-[#C2C1C0]/20'
-const SELECT_AND_MENU_POPOVER_CLASSES =
-  'border-weak z-50 max-h-[--popover-available-height] min-w-[10.25rem] overflow-auto overscroll-contain rounded-[.5rem] border bg-[white] py-2 shadow-[0px_8px_24px_0px_rgba(0,0,0,0.16)]'
-const SELECT_AND_POPOVER_GUTTER = 4
+// icon
+// ----
+
+export type IconData = string | ReactElement
+export type IconOptions = {
+  data?: IconData
+  /** @deprecated Use `data` instead. */
+  legacyName?: IconName
+}
+export interface IconProps extends ComponentPropsWithoutRef<'svg'>, IconOptions {}
+export const Icon = forwardRef<SVGSVGElement, IconProps>(function Icon({ legacyName, data, ...props }: IconProps, ref) {
+  if (legacyName) {
+    return (
+      <LegacyIcon ref={ref} {...props} name={legacyName} rotate={props.rotate ? Number(props.rotate) : undefined} />
+    )
+  }
+  if (!data) {
+    throw new Error('Icon component: either `data` or `legacyName` must be provided')
+  }
+  let content
+  if (typeof data === 'string') {
+    content = <path fill="currentColor" d={data} />
+  } else if (isValidElement(data)) {
+    content = data
+  } else {
+    throw new Error('Icon component: `data` must be a string or a valid ReactElement with SVG content')
+  }
+  return (
+    // TODO: if extracted into a CSS layer, no need for :where, probably
+    <svg ref={ref} viewBox="0 0 16 16" {...props} className="[:where(&)]:h-4 [:where(&)]:w-4">
+      {content}
+    </svg>
+  )
+})
 
 // menu
 // ----
@@ -31,28 +62,37 @@ export const Menu = forwardRef<HTMLDivElement, MenuProps>(function Menu(props, r
   return (
     <Ariakit.Menu
       ref={ref}
-      portal // TODO: Ariakit JSDoc might be wrong
-      gutter={SELECT_AND_POPOVER_GUTTER}
+      {...Atoms.DROPDOWN_POPOVER_DEFAULTS}
       {...props}
-      className={clsx(SELECT_AND_MENU_POPOVER_CLASSES, props.className)}
+      render={<Atoms.DropdownPopover render={props.render} />}
     />
   )
 })
 
-export interface MenuItemProps extends Ariakit.MenuItemProps {
-  isSubmenuTrigger?: boolean
-  icon?: IconName
+export interface SubMenuProps extends MenuProps {
+  /** @default 0 */
+  gutter?: MenuProps['gutter']
+  /** @default -9 */
+  shift?: MenuProps['shift']
 }
-export const MenuItem = forwardRef<HTMLDivElement, MenuItemProps>(function MenuItem(
-  { isSubmenuTrigger, children, icon, ...props },
+export const SubMenu = forwardRef<HTMLDivElement, SubMenuProps>(function SubMenu(props, ref) {
+  return <Menu ref={ref} {...Atoms.DROPDOWN_SUB_POPOVER_DEFAULTS} {...props} />
+})
+
+export interface MenuItemOptions extends Atoms.DropdownItemOptions {}
+export interface MenuItemProps extends Ariakit.MenuItemProps, MenuItemOptions {}
+export const MenuItem = forwardRef<HTMLDivElement, MenuItemProps>(function MenuItem(props, ref) {
+  return <Ariakit.MenuItem ref={ref} {...props} render={<Atoms.DropdownItem render={props.render} />} />
+})
+
+export interface SubMenuButtonProps extends Ariakit.MenuButtonProps, MenuItemOptions {
+  menuItemProps?: MenuItemProps
+}
+export const SubMenuButton = forwardRef<HTMLButtonElement, SubMenuButtonProps>(function SubMenuButton(
+  { menuItemProps, ...props },
   ref,
 ) {
-  return (
-    <Ariakit.MenuItem ref={ref} {...props} className={clsx(SELECT_AND_MENU_ITEM_CLASSES, props.className)}>
-      <span className="grow">{children}</span>
-      {isSubmenuTrigger && <Icon name="chevron-right-filled" className="text-[#0C0C14]" />}
-    </Ariakit.MenuItem>
-  )
+  return <Ariakit.MenuButton ref={ref} submenuIndicator render={<MenuItem {...menuItemProps} />} {...props} />
 })
 
 export interface MenuSeparatorProps extends Ariakit.MenuSeparatorProps {}
@@ -62,6 +102,43 @@ export const MenuSeparator = forwardRef<HTMLHRElement, MenuSeparatorProps>(funct
       ref={ref}
       {...props}
       className={clsx('border-weak my-[.4375rem] h-px border-t', props.className)}
+    />
+  )
+})
+
+export interface MenuItemCheckboxOptions extends Atoms.DropdownItemOptions {}
+export interface MenuItemCheckboxProps extends Ariakit.MenuItemCheckboxProps, MenuItemCheckboxOptions {}
+export const MenuItemCheckbox = forwardRef<HTMLDivElement, MenuItemCheckboxProps>(
+  function MenuItemCheckbox(props, ref) {
+    const menu = Ariakit.useMenuContext()
+    const isChecked = Ariakit.useStoreState(menu, (state) => {
+      const group = state?.values[props.name]
+      return Array.isArray(group) && group.includes(props.value as any)
+    })
+    return (
+      <Ariakit.MenuItemCheckbox
+        ref={ref}
+        leadingIndent
+        selectedIndicator={isChecked}
+        {...props}
+        render={<Atoms.DropdownItem render={props.render} />}
+      />
+    )
+  },
+)
+
+export interface MenuItemRadioOptions extends Atoms.DropdownItemOptions {}
+export interface MenuItemRadioProps extends Ariakit.MenuItemRadioProps, MenuItemRadioOptions {}
+export const MenuItemRadio = forwardRef<HTMLDivElement, MenuItemRadioProps>(function MenuItemRadio(props, ref) {
+  const menu = Ariakit.useMenuContext()
+  const isChecked = Ariakit.useStoreState(menu, (state) => state?.values[props.name] === props.value)
+  return (
+    <Ariakit.MenuItemRadio
+      ref={ref}
+      leadingIndent
+      selectedIndicator={isChecked}
+      {...props}
+      render={<Atoms.DropdownItem render={props.render} />}
     />
   )
 })
@@ -87,17 +164,27 @@ export const SelectPopover = forwardRef<HTMLDivElement, SelectPopoverProps>(func
   return (
     <Ariakit.SelectPopover
       ref={ref}
-      portal // TODO: Ariakit JSDoc might be wrong
-      gutter={SELECT_AND_POPOVER_GUTTER}
+      {...Atoms.DROPDOWN_POPOVER_DEFAULTS}
       {...props}
-      className={clsx(SELECT_AND_MENU_POPOVER_CLASSES, props.className)}
+      render={<Atoms.DropdownPopover render={props.render} />}
     />
   )
 })
 
-export interface SelectItemProps extends Ariakit.SelectItemProps {}
+export interface SelectItemOptions extends Atoms.DropdownItemOptions {}
+export interface SelectItemProps extends Ariakit.SelectItemProps, SelectItemOptions {}
 export const SelectItem = forwardRef<HTMLDivElement, SelectItemProps>(function SelectItem(props, ref) {
-  return <Ariakit.SelectItem ref={ref} {...props} className={clsx(SELECT_AND_MENU_ITEM_CLASSES, props.className)} />
+  const select = Ariakit.useSelectContext()
+  const isChecked = Ariakit.useStoreState(select, (state) => state?.value === props.value)
+  return (
+    <Ariakit.SelectItem
+      ref={ref}
+      leadingIndent
+      selectedIndicator={isChecked}
+      {...props}
+      render={<Atoms.DropdownItem render={props.render} />}
+    />
+  )
 })
 
 export interface SelectGroupProps extends Ariakit.SelectGroupProps {
@@ -117,8 +204,30 @@ export const SelectGroup = forwardRef<HTMLDivElement, SelectGroupProps>(function
   )
 })
 
-// TODO:
-// - menu item icons
-// - submenu trigger active style
-// - menu item checkbox/radio
-// - select checkmark
+// combobox
+
+import type { ComboboxProviderProps } from '@ariakit/react'
+export type { ComboboxProviderProps }
+export const ComboboxProvider = Ariakit.ComboboxProvider
+
+import type { ComboboxProps } from '@ariakit/react'
+export type { ComboboxProps }
+export const Combobox = Ariakit.Combobox
+
+export interface ComboboxPopoverProps extends Ariakit.ComboboxPopoverProps {}
+export const ComboboxPopover = forwardRef<HTMLDivElement, ComboboxPopoverProps>(function ComboboxPopover(props, ref) {
+  return (
+    <Ariakit.ComboboxPopover
+      ref={ref}
+      {...Atoms.DROPDOWN_POPOVER_DEFAULTS}
+      {...props}
+      render={<Atoms.DropdownPopover render={props.render} />}
+    />
+  )
+})
+
+export interface ComboboxItemOptions extends Atoms.DropdownItemOptions {}
+export interface ComboboxItemProps extends Ariakit.ComboboxItemProps, ComboboxItemOptions {}
+export const ComboboxItem = forwardRef<HTMLDivElement, ComboboxItemProps>(function ComboboxItem(props, ref) {
+  return <Ariakit.ComboboxItem ref={ref} {...props} render={<Atoms.DropdownItem render={props.render} />} />
+})
