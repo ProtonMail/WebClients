5418851b551c5d23f92f5d59f050a28cae8cea73
Alexey Karpov <alexey.karpov@proton.ch>
2025-06-27 09:23:49 +0000

    Add config for TLDR coupon

---
+++ packages/components/containers/payments/subscription/coupon-config/tldrNewsletter.tsx
@@ -0,0 +1,24 @@
+import Price from '@proton/components/components/price/Price';
+import { COUPON_CODES, CYCLE } from '@proton/payments';
+import { getCheckout } from '@proton/shared/lib/helpers/checkout';
+
+import { type CouponConfig } from './interface';
+
+export const tldrNewsletterConfig: CouponConfig = {
+    coupons: [COUPON_CODES.TLDRPROMO072025],
+    hidden: true,
+    cyclePriceCompare: ({ cycle, suffix }, config) => {
+        if (cycle !== CYCLE.YEARLY) {
+            return null;
+        }
+
+        const checkout = getCheckout(config);
+
+        return (
+            <Price className="ml-2 text-strike" currency={checkout.currency} suffix={suffix}>
+                {checkout.withoutDiscountPerMonth}
+            </Price>
+        );
+    },
+    availableCycles: [CYCLE.YEARLY],
+};
