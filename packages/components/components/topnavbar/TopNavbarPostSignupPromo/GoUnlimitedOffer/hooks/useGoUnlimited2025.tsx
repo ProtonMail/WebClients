68649197a3ca42450580d2bc9af86f62df5026b9
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-06-27 14:56:23 +0000

    Merge branch 'show-unlimited-offer-using-unleash-feature-flag' into 'main'
    Control go unlimited offer 2025 with unleash feature flag
    
    See merge request web/clients!17879

--- packages/components/components/topnavbar/TopNavbarPostSignupPromo/GoUnlimitedOffer/hooks/useGoUnlimited2025.tsx
+++ packages/components/components/topnavbar/TopNavbarPostSignupPromo/GoUnlimitedOffer/hooks/useGoUnlimited2025.tsx
@@ -4,6 +4,7 @@ import useConfig from '@proton/components/hooks/useConfig';
 import { FeatureCode } from '@proton/features/interface';
 import useFeature from '@proton/features/useFeature';
 import { getAppFromPathnameSafe } from '@proton/shared/lib/apps/slugHelper';
+import { useFlag } from '@proton/unleash/index';
 
 import { type OfferHookReturnValue } from '../../common/interface';
 import { getIsEligible } from '../helpers/eligibility';
@@ -13,10 +14,14 @@ export const useGoUnlimited2025 = (): OfferHookReturnValue => {
     const [user, userLoading] = useUser();
     const [subscription, subscriptionLoading] = useSubscription();
     const parentApp = getAppFromPathnameSafe(window.location.pathname);
+    const isGoUnlimitedOffer2025Enabled = useFlag('GoUnlimitedOffer2025');
     const { feature } = useFeature(FeatureCode.OfferGoUnlimited2025);
 
     return {
-        isEligible: feature?.Value === 0 && getIsEligible({ user, subscription, protonConfig, parentApp }),
+        isEligible:
+            isGoUnlimitedOffer2025Enabled &&
+            feature?.Value === 0 &&
+            getIsEligible({ user, subscription, protonConfig, parentApp }),
         isLoading: userLoading || subscriptionLoading,
         openSpotlight: false,
     };
