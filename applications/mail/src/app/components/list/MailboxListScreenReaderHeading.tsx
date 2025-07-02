4e34c2f938785cd986eec77fa76dad7f10aa2ca2
Divyam Rastogi <divyam.rastogi@proton.ch>
2025-07-02 13:16:24 +0000

    bug: add screen reader support for mailbox list

---
+++ applications/mail/src/app/components/list/MailboxListScreenReaderHeading.tsx
@@ -0,0 +1,30 @@
+import { useMemo } from 'react';
+
+import { c, msgid } from 'ttag';
+
+import { useConversationCounts } from '@proton/mail/store/counts/conversationCounts';
+import { useMessageCounts } from '@proton/mail/store/counts/messageCounts';
+
+interface MailboxListScreenReaderHeadingProps {
+    conversationMode: boolean;
+    labelID?: string;
+}
+
+const MailboxListScreenReaderHeading = ({ conversationMode, labelID }: MailboxListScreenReaderHeadingProps) => {
+    const [conversationCounts] = useConversationCounts();
+    const [messageCounts] = useMessageCounts();
+
+    const unreads = useMemo(() => {
+        const counters = conversationMode ? conversationCounts : messageCounts;
+        return (counters || []).find((counter) => counter.LabelID === labelID)?.Unread || 0;
+    }, [conversationMode, labelID, conversationCounts, messageCounts]);
+
+    return (
+        <h1 className="sr-only">
+            {conversationMode ? c('Title').t`Conversation list` : c('Title').t`Message list`}{' '}
+            {c('Title').ngettext(msgid`${unreads} unread message`, `${unreads} unread messages`, unreads)}
+        </h1>
+    );
+};
+
+export default MailboxListScreenReaderHeading;
