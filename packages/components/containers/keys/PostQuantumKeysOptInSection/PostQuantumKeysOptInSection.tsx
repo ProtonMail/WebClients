c0e9f63cfeb795d554d7f5d856959fec4e3fbb94
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-07-02 16:52:55 +0200

    PostQuantumOptInModal: refetch user and address keys at each step
    To ensure latest state is always loaded

--- packages/components/containers/keys/PostQuantumKeysOptInSection/PostQuantumKeysOptInSection.tsx
+++ packages/components/containers/keys/PostQuantumKeysOptInSection/PostQuantumKeysOptInSection.tsx
@@ -2,46 +2,41 @@ import { c } from 'ttag';
 
 import { useUserSettings } from '@proton/account';
 import { Button } from '@proton/atoms';
+import { useModalTwoStatic } from '@proton/components/components/modalTwo/useModalTwo';
 import SettingsParagraph from '@proton/components/containers/account/SettingsParagraph';
 import SettingsSectionWide from '@proton/components/containers/account/SettingsSectionWide';
+
 import PostQuantumOptInModal from './PostQuantumOptInModal';
-import { useModalTwoStatic } from '@proton/components/components/modalTwo/useModalTwo';
+
 const PostQuantumKeysOptInSection = () => {
     const [userSettings] = useUserSettings(); // loading state not needed since settings are prefetched in bootstrap
     const [optInModal, showOptInModal] = useModalTwoStatic(PostQuantumOptInModal);
 
-    if (!!userSettings.Flags.SupportPgpV6Keys) {
-        return (
-            <SettingsSectionWide>
-                <SettingsParagraph>
-                    {c('Info').t`Support for post-quantum keys is enabled for your account.`}
-                </SettingsParagraph>
-            </SettingsSectionWide>
-        );
-    }
-
     return (
         <>
-            <SettingsSectionWide>
-                <SettingsParagraph>
-                    {c('Info')
-                        .t`By enabling post-quantum protection, keys that are resistant against quantum computers will be generated for your account.`}
-                </SettingsParagraph>
-                {
-                    <div className="mb-4">
-                        <Button
-                            shape="outline"
-                            onClick={() => showOptInModal({})}
-                            data-testid="postQuantumOptIn"
-                        >
-                            {c('Action').t`Enable post-quantum protection`}
-                        </Button>
-                    </div>
-                }
-            </SettingsSectionWide>
             {optInModal}
+            {!!userSettings.Flags.SupportPgpV6Keys ? (
+                <SettingsSectionWide>
+                    <SettingsParagraph>
+                        {c('Info').t`Support for post-quantum keys is enabled for your account.`}
+                    </SettingsParagraph>
+                </SettingsSectionWide>
+            ) : (
+                <SettingsSectionWide>
+                    <SettingsParagraph>
+                        {c('Info')
+                            .t`By enabling post-quantum protection, keys that are resistant against quantum computers will be generated for your account.`}
+                    </SettingsParagraph>
+                    {
+                        <div className="mb-4">
+                            <Button shape="outline" onClick={() => showOptInModal({})} data-testid="postQuantumOptIn">
+                                {c('Action').t`Enable post-quantum protection`}
+                            </Button>
+                        </div>
+                    }
+                </SettingsSectionWide>
+            )}
         </>
     );
 };
-
 export default PostQuantumKeysOptInSection;
