c865e528fb7a43582a9edcfd209e3250554a4042
Aman Harwara <aman.harwara@proton.ch>
2025-07-04 15:50:22 +0000

    Docs/Sheets: Show modal if there is an error when applying update

--- packages/docs-shared/lib/Doc/DocStateCallbacks.ts
+++ packages/docs-shared/lib/Doc/DocStateCallbacks.ts
@@ -5,4 +5,5 @@ import type { SafeDocsUserState } from './DocsAwareness'
 export type DocStateCallbacks = {
   docStateRequestsPropagationOfUpdate: (message: RtsMessagePayload, debugSource: BroadcastSource) => void
   handleAwarenessStateUpdate: (states: SafeDocsUserState[]) => void
+  handleErrorWhenReceivingDocumentUpdate: (error: unknown) => void
 }
