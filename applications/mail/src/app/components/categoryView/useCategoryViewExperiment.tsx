fc9c64c9bc6c631c279de4a30a4e996347e927de
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-07-08 13:42:47 +0000

    Merge branch 'P3-25-remove-backend-flag' into 'main'
    Remove dependency on BE flag
    
    See merge request web/clients!18061

--- applications/mail/src/app/components/categoryView/useCategoryViewExperiment.tsx
+++ applications/mail/src/app/components/categoryView/useCategoryViewExperiment.tsx
@@ -2,9 +2,8 @@ import useFlag from '@proton/unleash/useFlag';
 
 export const useCategoryViewExperiment = () => {
     const isAvailable = useFlag('ShowMessageCategory');
-    const mailCategoryView = useFlag('MailCategoryView');
 
     return {
-        canSeeCategoryLabel: isAvailable && mailCategoryView,
+        canSeeCategoryLabel: isAvailable,
     };
 };
--- packages/unleash/UnleashFeatureFlags.ts
+++ packages/unleash/UnleashFeatureFlags.ts
@@ -170,7 +170,6 @@ enum MailFeatureFlag {
     MailboxOptimisticRefactoring = 'MailboxOptimisticRefactoring',
     // Category view
     ShowMessageCategory = 'ShowMessageCategory',
-    MailCategoryView = 'MailCategoryView',
 }
 
 enum AdminFeatureFlag {
