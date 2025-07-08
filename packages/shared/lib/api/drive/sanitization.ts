1d6b5ce44b5b66f285072d0760db03e96ae57605
Lucien Peslier <lucien.peslier@proton.ch>
2025-07-08 13:19:02 +0000

    [DRVWEB-4822] Remove autoRestore /asv endpoint

--- packages/shared/lib/api/drive/sanitization.ts
+++ packages/shared/lib/api/drive/sanitization.ts
@@ -1,18 +1,3 @@
-export const queryListShareAutoRestore = () => {
-    return {
-        method: 'get',
-        url: `drive/sanitization/asv`,
-    };
-};
-
-export const querySendShareAutoRestoreStatus = ({ Shares }: { Shares: { ShareID: string; Reason: string }[] }) => {
-    return {
-        method: 'post',
-        url: `drive/sanitization/asv`,
-        data: { Shares },
-    };
-};
-
 export const queryListNodesWithMissingNodeHashKeys = () => {
     return {
         method: 'get',
