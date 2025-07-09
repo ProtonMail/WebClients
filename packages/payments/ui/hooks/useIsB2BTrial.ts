06a1fbc8fb6e7dd541bdadcb3571539a1c5f7f38
Pedro Amaro <pedro.lavarinhas.amaro@proton.ch>
2025-07-09 16:23:32 +0200

    [MSA-702] B2B Trials: reflect figma on subscription container

--- packages/payments/ui/hooks/useIsB2BTrial.ts
+++ packages/payments/ui/hooks/useIsB2BTrial.ts
@@ -3,8 +3,9 @@ import { useFlag } from '@proton/unleash/index';
 
 import { isTrial } from '../../core/subscription/helpers';
 import type { Subscription } from '../../core/subscription/interface';
+import type { FreeSubscription } from '../../core/interface';
 
-const useIsB2BTrial = (subscription?: Subscription, organization?: Organization): boolean => {
+const useIsB2BTrial = (subscription?: Subscription | FreeSubscription, organization?: Organization): boolean => {
     const isB2BTrialEnabled = useFlag('ManualTrialsFE');
     return isTrial(subscription) && !!organization?.IsBusiness && isB2BTrialEnabled;
 };
