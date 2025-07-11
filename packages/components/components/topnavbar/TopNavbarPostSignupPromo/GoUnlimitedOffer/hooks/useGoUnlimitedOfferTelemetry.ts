5ab2d3d7ca9091ce1742d279c8b40f805f7ddd9e
romain sanchez <romain.sanchez@proton.ch>
2025-07-11 11:16:32 +0200

    Rename unlimited offer measurement group

--- packages/components/components/topnavbar/TopNavbarPostSignupPromo/GoUnlimitedOffer/hooks/useGoUnlimitedOfferTelemetry.ts
+++ packages/components/components/topnavbar/TopNavbarPostSignupPromo/GoUnlimitedOffer/hooks/useGoUnlimitedOfferTelemetry.ts
@@ -2,12 +2,8 @@ import { useUserSettings } from '@proton/account';
 import { useSubscription } from '@proton/account/subscription/hooks';
 import { useUser } from '@proton/account/user/hooks';
 import useApi from '@proton/components/hooks/useApi';
-import type {
-    TelemetryUnlimitedOffer2025} from '@proton/shared/lib/api/telemetry';
-import {
-    type TelemetryEvents,
-    TelemetryMeasurementGroups
-} from '@proton/shared/lib/api/telemetry';
+import type { TelemetryUnlimitedOffer2025 } from '@proton/shared/lib/api/telemetry';
+import { type TelemetryEvents, TelemetryMeasurementGroups } from '@proton/shared/lib/api/telemetry';
 import type { APP_NAMES } from '@proton/shared/lib/constants';
 import { sendTelemetryReportWithBaseDimensions } from '@proton/shared/lib/helpers/metrics';
 import { type SimpleMap } from '@proton/shared/lib/interfaces';
@@ -26,7 +22,7 @@ export const useGoUnlimitedOfferTelemetry = (app: APP_NAMES) => {
             user,
             subscription,
             userSettings,
-            measurementGroup: TelemetryMeasurementGroups.mailUnlimitedOffer2025,
+            measurementGroup: TelemetryMeasurementGroups.unlimitedOffer2025,
             event,
             dimensions,
             delay: false,
