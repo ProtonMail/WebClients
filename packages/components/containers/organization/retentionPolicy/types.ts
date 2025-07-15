18e17a4b10f5234438bd9d762e2158541f827cc8
Thong-Nguyen The <thong-nguyen.the@proton.ch>
2025-07-15 10:25:00 +0000

    Retention UI [02]: add hook to CRUD retention rules

---
+++ packages/components/containers/organization/retentionPolicy/types.ts
@@ -0,0 +1,22 @@
+import type {
+    RetentionRuleAction,
+    RetentionRuleProduct,
+    RetentionRuleScopeType,
+} from '@proton/shared/lib/interfaces/RetentionRule';
+
+export type ProductScopeFilterOptions = Record<RetentionRuleProduct, RetentionRuleScopeType[]>;
+
+export interface RetentionRuleScopeFormData {
+    id: string;
+    entityType: RetentionRuleScopeType;
+    entityID: string;
+}
+
+export interface RetentionRuleFormData {
+    id: string | null;
+    name: string;
+    products: RetentionRuleProduct;
+    lifetime: number | null;
+    action: RetentionRuleAction;
+    scopes: RetentionRuleScopeFormData[];
+}
