48eb7defdf45707ff24d261d5aea4cafae7f7024
Matthieu Lux <matthieu.lux@protonmail.com>
2025-07-18 15:33:18 +0200

    Checkpoint adding webapp support

--- packages/pass/components/Form/Field/Control/ClickToCopy.tsx
+++ packages/pass/components/Form/Field/Control/ClickToCopy.tsx
@@ -3,7 +3,7 @@ import { type FC, useRef } from 'react';
 
 import { c } from 'ttag';
 
-import { useCopyToClipboard } from '@proton/pass/hooks/useCopyToClipboard';
+import { ClipboardProvider, useOnCopyToClipboard } from '@proton/pass/components/Settings/Clipboard/ClipboardProvider';
 import { SelectionManager } from '@proton/pass/utils/dom/selection';
 import clsx from '@proton/utils/clsx';
 
@@ -13,28 +13,28 @@ export type ClickToCopyProps = {
     onCopy?: () => void;
 };
 
-export const ClickToCopy: FC<PropsWithChildren<ClickToCopyProps>> = ({ className, children, onCopy, value = '' }) => {
+const ClickToCopyInternal: FC<PropsWithChildren<ClickToCopyProps>> = ({ className, children, onCopy, value = '' }) => {
     const ref = useRef<HTMLDivElement>(null);
-    const copyToClipboard = useCopyToClipboard();
+    // const { showModal, onCloseModal, onCopyToClipboard } = useCopyToClipboard();
+    const onCopyToClipboard = useOnCopyToClipboard();
     const getValue = () => (value instanceof Function ? value() : value);
 
     const handleClick = (evt: MouseEvent) => {
         if (ref.current && SelectionManager.hasChildOf(ref.current)) evt.preventDefault();
         else if (value) {
-            void copyToClipboard(getValue());
+            void onCopyToClipboard(getValue());
             onCopy?.();
         }
     };
 
     return (
-        /* disabling `prefer-tag-over-role` as we cannot use a
-         * `button` element here - children may include buttons and
-         * this would lead to invalid DOM structure */
+        /* disabling `prefer-tag-over-role` as we cannot use a * `button` element here - children may include
+            buttons and * this would lead to invalid DOM structure */
         /* eslint-disable-next-line jsx-a11y/prefer-tag-over-role */
         <div
             ref={ref}
             className={clsx('cursor-pointer overflow-hidden', className)}
-            onKeyDown={(evt) => evt.key === 'Enter' && copyToClipboard(getValue())}
+            onKeyDown={(evt) => evt.key === 'Enter' && onCopyToClipboard(getValue())}
             onClick={handleClick}
             tabIndex={0}
             role="button"
@@ -42,5 +42,13 @@ export const ClickToCopy: FC<PropsWithChildren<ClickToCopyProps>> = ({ className
             <span className="sr-only"> {c('Info').t`Press Enter to copy`}</span>
             {children}
         </div>
+    );
+};
+
+export const ClickToCopy: FC<PropsWithChildren<ClickToCopyProps>> = (props) => {
+    return (
+        <ClipboardProvider>
+            <ClickToCopyInternal {...props} />
+        </ClipboardProvider>
     );
 };
