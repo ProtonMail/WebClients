98435665602ca1d3c6db581984a7f992844fbb13
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-07-18 15:01:46 +0000

    Add navigation on the categories tabs

--- applications/mail/src/app/components/categoryView/categoriesStringHelpers.ts
+++ applications/mail/src/app/components/categoryView/categoriesStringHelpers.ts
@@ -1,22 +1,22 @@
 import { c } from 'ttag';
 
-import { CATEGORY_LABEL_IDS } from './categoriesConstants';
+import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
 
-export const getLabelFromCategoryId = (id: CATEGORY_LABEL_IDS) => {
+export const getLabelFromCategoryId = (id: MAILBOX_LABEL_IDS) => {
     switch (id) {
-        case CATEGORY_LABEL_IDS.CATEGORY_DEFAULT:
+        case MAILBOX_LABEL_IDS.CATEGORY_DEFAULT:
             return c('Label').t`Primary`;
-        case CATEGORY_LABEL_IDS.CATEGORY_SOCIAL:
+        case MAILBOX_LABEL_IDS.CATEGORY_SOCIAL:
             return c('Label').t`Social`;
-        case CATEGORY_LABEL_IDS.CATEGORY_PROMOTIONS:
+        case MAILBOX_LABEL_IDS.CATEGORY_PROMOTIONS:
             return c('Label').t`Promotions`;
-        case CATEGORY_LABEL_IDS.CATEGORY_NEWSLETTER:
+        case MAILBOX_LABEL_IDS.CATEGORY_NEWSLETTERS:
             return c('Label').t`Newsletters`;
-        case CATEGORY_LABEL_IDS.CATEGORY_TRANSACTIONS:
+        case MAILBOX_LABEL_IDS.CATEGORY_TRANSACTIONS:
             return c('Label').t`Transactions`;
-        case CATEGORY_LABEL_IDS.CATEGORY_UPDATES:
+        case MAILBOX_LABEL_IDS.CATEGORY_UPDATES:
             return c('Label').t`Updates`;
-        case CATEGORY_LABEL_IDS.CATEGORY_FORUMS:
+        case MAILBOX_LABEL_IDS.CATEGORY_FORUMS:
             return c('Label').t`Forums`;
         default:
             return c('Label').t`Unknown`;
--- applications/mail/src/app/metrics/mailMetricsHelper.ts
+++ applications/mail/src/app/metrics/mailMetricsHelper.ts
@@ -1,10 +1,21 @@
-import { isCustomLabelOrFolder, isStringHumanLabelID } from '@proton/mail/store/labels/helpers';
+import { isCategoryLabel, isCustomLabelOrFolder, isStringHumanLabelID } from '@proton/mail/store/labels/helpers';
 import type { EmailListDisplayTime } from '@proton/metrics/types/web_mail_performance_email_list_display_time_histogram_v1.schema';
-import type { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
+import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
 import type { MailSettings } from '@proton/shared/lib/interfaces';
 import { MAIL_PAGE_SIZE } from '@proton/shared/lib/mail/mailSettings';
 
 export type LabelType = EmailListDisplayTime['Labels']['loaded'];
+
+type SupportedLabelID = Exclude<
+    MAILBOX_LABEL_IDS,
+    | MAILBOX_LABEL_IDS.CATEGORY_DEFAULT
+    | MAILBOX_LABEL_IDS.CATEGORY_FORUMS
+    | MAILBOX_LABEL_IDS.CATEGORY_NEWSLETTERS
+    | MAILBOX_LABEL_IDS.CATEGORY_UPDATES
+    | MAILBOX_LABEL_IDS.CATEGORY_PROMOTIONS
+    | MAILBOX_LABEL_IDS.CATEGORY_SOCIAL
+    | MAILBOX_LABEL_IDS.CATEGORY_TRANSACTIONS
+>;
 
 export const getPageSizeString = (settings: MailSettings | undefined) => {
     const { PageSize } = settings || {};
@@ -20,12 +31,16 @@ export const getPageSizeString = (settings: MailSettings | undefined) => {
     return '50';
 };
 
-export const getLabelID = (labelID: string) => {
+export const getLabelID = (labelID: string): SupportedLabelID | 'custom' => {
     if (isCustomLabelOrFolder(labelID)) {
         return 'custom';
     }
 
-    return labelID as MAILBOX_LABEL_IDS;
+    if (isCategoryLabel(labelID)) {
+        return MAILBOX_LABEL_IDS.INBOX;
+    }
+
+    return labelID as SupportedLabelID;
 };
 
 export const pathnameToLabelName = (pathname: string) => {
