2a21d821b483f4409ebc1f24ef8eb09bb7f75707
Edvin CANDON <edvin.candon@proton.ch>
2025-07-18 11:19:02 +0200

    Respect tldts for custom private domains
    [IDTEAM-4654]

--- packages/pass/utils/url/parser.ts
+++ packages/pass/utils/url/parser.ts
@@ -1,9 +1,26 @@
 import { parse } from 'tldts';
 import type { Runtime } from 'webextension-polyfill';
 
+import type { MaybeNull } from '@proton/pass/types';
+
 import { sanitizeURL } from './sanitize';
 import type { ParsedSender, ParsedUrl } from './types';
 import { isSupportedSenderUrl } from './utils';
+
+export const generateDomainCombinations = function* (domain: string, subdomain: MaybeNull<string>) {
+    if (!subdomain) {
+        yield [domain];
+        return;
+    }
+
+    const parts = subdomain.split('.').reverse();
+    yield [domain, ...parts];
+
+    while (parts.length > 0) {
+        domain = `${parts.shift()}.${domain}`;
+        yield [domain, ...parts];
+    }
+};
 
 export const parseUrl = (url?: string, customPrivateDomains?: Set<string>): ParsedUrl => {
     const check = sanitizeURL(url ?? '');
@@ -22,17 +39,22 @@ export const parseUrl = (url?: string, customPrivateDomains?: Set<string>): Pars
         };
     }
 
-    let { domain, subdomain, domainWithoutSuffix, hostname, isPrivate } = parse(url ?? '', {
+    let { domain, subdomain, domainWithoutSuffix, hostname, isPrivate, publicSuffix } = parse(url ?? '', {
         allowIcannDomains: true,
         allowPrivateDomains: true,
         detectIp: true,
     });
-    // if there is no subdomain or domain, pointless to check for private domain
-    if (subdomain && domain && customPrivateDomains?.has(domain)) {
-        domain = [subdomain, domain].join('.');
-        domainWithoutSuffix = [subdomain, domainWithoutSuffix].join('.');
-        subdomain = null;
-        isPrivate = true;
+
+    if (customPrivateDomains && !isPrivate && domain) {
+        for (const [nDomain, nSubdomain, ...rest] of generateDomainCombinations(domain, subdomain)) {
+            if (customPrivateDomains.has(nDomain)) {
+                domain = nSubdomain ? `${nSubdomain}.${nDomain}` : nDomain;
+                domainWithoutSuffix = nSubdomain || nDomain.replace(new RegExp(`\\.${publicSuffix}$`), '');
+                subdomain = rest.join('.') || null;
+                isPrivate = true;
+                break;
+            }
+        }
     }
 
     return {
