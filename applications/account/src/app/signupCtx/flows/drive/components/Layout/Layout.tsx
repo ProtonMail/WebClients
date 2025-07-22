6bbeab61e16c93e918c643b27292eead088514fe
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2025-07-22 11:40:11 +0100

    Refactor generic, drive and referral signup styles, use helpers, rename class names

--- applications/account/src/app/signupCtx/flows/drive/components/Layout/Layout.tsx
+++ applications/account/src/app/signupCtx/flows/drive/components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import './layout.scss';
 
 const Layout = ({ children }: { children: ReactNode }) => {
     return (
-        <div className="flex *:min-size-auto flex-nowrap flex-column h-full overflow-auto relative drive-signup drive-signup-bg-gradient">
+        <div className="flex *:min-size-auto flex-nowrap flex-column h-full overflow-auto relative scroll-smooth drive-signup drive-signup-bg-gradient">
             <div>{children}</div>
         </div>
     );
