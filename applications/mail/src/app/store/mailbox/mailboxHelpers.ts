0d43910e71eabdf537ccb1dc6ddaf75d206916c7
Richard Tetaz <richard.tetaz@proton.ch>
2025-07-22 13:16:30 +0200

    Define Async Thunk to apply/remove labels on messages

--- applications/mail/src/app/store/mailbox/mailboxHelpers.ts
+++ applications/mail/src/app/store/mailbox/mailboxHelpers.ts
@@ -1,5 +1,6 @@
 import { c, msgid } from 'ttag';
 
+import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
 import { MARK_AS_STATUS } from '@proton/shared/lib/mail/constants';
 
 export const getNotificationTextMarked = (isMessage: boolean, elementsCount: number, status: MARK_AS_STATUS) => {
@@ -40,4 +41,114 @@ export const getNotificationTextMarked = (isMessage: boolean, elementsCount: num
               `${elementsCount} conversations marked as unread.`,
               elementsCount
           );
+};
+
+export const getNotificationTextStarred = (isMessage: boolean, elementsCount: number) => {
+    if (isMessage) {
+        if (elementsCount === 1) {
+            return c('Success').t`Message marked as Starred.`;
+        }
+        return c('Success').ngettext(
+            msgid`${elementsCount} message marked as Starred.`,
+            `${elementsCount} messages marked as Starred.`,
+            elementsCount
+        );
+    }
+
+    if (elementsCount === 1) {
+        return c('Success').t`Conversation marked as Starred.`;
+    }
+
+    return c('Success').ngettext(
+        msgid`${elementsCount} conversation marked as Starred.`,
+        `${elementsCount} conversations marked as Starred.`,
+        elementsCount
+    );
+};
+
+export const getNotificationTextUnstarred = (isMessage: boolean, elementsCount: number) => {
+    if (isMessage) {
+        if (elementsCount === 1) {
+            return c('Success').t`Message removed from Starred.`;
+        }
+        return c('Success').ngettext(
+            msgid`${elementsCount} message removed from Starred.`,
+            `${elementsCount} messages removed from Starred.`,
+            elementsCount
+        );
+    }
+
+    if (elementsCount === 1) {
+        return c('Success').t`Conversation removed from Starred.`;
+    }
+
+    return c('Success').ngettext(
+        msgid`${elementsCount} conversation removed from Starred.`,
+        `${elementsCount} conversations removed from Starred.`,
+        elementsCount
+    );
+};
+
+export const getNotificationTextLabelRemoved = (
+    isMessage: boolean,
+    elementsCount: number,
+    labelID: string,
+    labelName: string
+) => {
+    if (labelID === MAILBOX_LABEL_IDS.STARRED) {
+        return getNotificationTextUnstarred(isMessage, elementsCount);
+    }
+
+    if (isMessage) {
+        if (elementsCount === 1) {
+            return c('Success').t`Message removed from ${labelName}.`;
+        }
+        return c('Success').ngettext(
+            msgid`${elementsCount} message removed from ${labelName}.`,
+            `${elementsCount} messages removed from ${labelName}.`,
+            elementsCount
+        );
+    }
+
+    if (elementsCount === 1) {
+        return c('Success').t`Conversation removed from ${labelName}.`;
+    }
+
+    return c('Success').ngettext(
+        msgid`${elementsCount} conversation removed from ${labelName}.`,
+        `${elementsCount} conversations removed from ${labelName}.`,
+        elementsCount
+    );
+};
+
+export const getNotificationTextLabelAdded = (
+    isMessage: boolean,
+    elementsCount: number,
+    labelID: string,
+    labelName: string
+) => {
+    if (labelID === MAILBOX_LABEL_IDS.STARRED) {
+        return getNotificationTextStarred(isMessage, elementsCount);
+    }
+
+    if (isMessage) {
+        if (elementsCount === 1) {
+            return c('Success').t`Message added to ${labelName}.`;
+        }
+        return c('Success').ngettext(
+            msgid`${elementsCount} message added to ${labelName}.`,
+            `${elementsCount} messages added to ${labelName}.`,
+            elementsCount
+        );
+    }
+
+    if (elementsCount === 1) {
+        return c('Success').t`Conversation added to ${labelName}.`;
+    }
+
+    return c('Success').ngettext(
+        msgid`${elementsCount} conversation added to ${labelName}.`,
+        `${elementsCount} conversations added to ${labelName}.`,
+        elementsCount
+    );
 };
