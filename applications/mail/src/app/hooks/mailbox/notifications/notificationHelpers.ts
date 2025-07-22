e722039abb62e2da11df110bb283103b4444b1f6
Atanas Janeshliev <atanas.janeshliev@proton.ch>
2025-07-22 11:17:03 +0200

    Revert "Open corresponding message from desktop notification"
    This reverts commit c727887d94400465ea07aca64e97b46a9be0d924.

--- applications/mail/src/app/hooks/mailbox/notifications/notificationHelpers.ts
+++ applications/mail/src/app/hooks/mailbox/notifications/notificationHelpers.ts
@@ -11,33 +11,6 @@ import notificationIcon from '../../../assets/notification.png';
 import { isConversationMode } from '../../../helpers/mailSettings';
 import { setParamsInLocation } from '../../../helpers/mailboxUrl';
 
-export const prepareNotificationData = ({
-    message,
-    history,
-    mailSettings,
-    notifier,
-}: {
-    message: Message;
-    history: History<unknown>;
-    mailSettings: any;
-    notifier: any;
-}) => {
-    const { Subject, Sender, ID, ConversationID, LabelIDs } = message;
-    const sender = Sender.Name || Sender.Address;
-    const title = c('Desktop notification title').t`New email received`;
-    const body = c('Desktop notification body').t`From: ${sender} - ${Subject}`;
-    const labelID = LabelIDs.find((labelID) => notifier.includes(labelID)) || MAILBOX_LABEL_IDS.ALL_MAIL;
-
-    // Remove the search keyword from the URL to find the message or conversation. Otherwise we can have a 'Conversation does not exists' error.
-    const cleanHistoryLocation = { ...history.location, hash: '' };
-    const conversationMode = isConversationMode(labelID, mailSettings, cleanHistoryLocation);
-    const elementID = conversationMode ? ConversationID : ID;
-    const messageID = conversationMode ? ID : undefined;
-    const location = setParamsInLocation(cleanHistoryLocation, { labelID, elementID, messageID });
-
-    return { title, body, location, ID, labelID, elementID, messageID };
-};
-
 export const displayNotification = ({
     message,
     history,
@@ -51,17 +24,19 @@ export const displayNotification = ({
     notifier: any;
     onOpenElement: () => void;
 }) => {
-    const notificationData = prepareNotificationData({
-        message,
-        history,
-        mailSettings,
-        notifier,
-    });
+    const { Subject, Sender, ID, ConversationID, LabelIDs } = message;
+    const sender = Sender.Name || Sender.Address;
+    const title = c('Desktop notification title').t`New email received`;
+    const body = c('Desktop notification body').t`From: ${sender} - ${Subject}`;
+    const labelID = LabelIDs.find((labelID) => notifier.includes(labelID)) || MAILBOX_LABEL_IDS.ALL_MAIL;
 
-    const { title, body, location, ID } = notificationData;
+    // Remove the search keyword from the URL to find the message or conversation. Otherwise we can have a 'Conversation does not exists' error.
+    const cleanHistoryLocation = { ...history.location, hash: '' };
+    const elementID = isConversationMode(labelID, mailSettings, cleanHistoryLocation) ? ConversationID : ID;
+    const location = setParamsInLocation(cleanHistoryLocation, { labelID, elementID });
 
     if (isElectronMail) {
-        return createElectronNotification({ app: 'mail', ...notificationData });
+        return createElectronNotification({ title, body, app: 'mail', labelID, elementID });
     }
 
     return create(title, {
