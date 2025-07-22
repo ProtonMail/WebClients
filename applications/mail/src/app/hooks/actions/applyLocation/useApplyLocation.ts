afd92735d4111ea9d94a09e8bee9eb44e7f2fb8f
Richard Tetaz <richard.tetaz@proton.ch>
2025-07-22 13:59:43 +0200

    Connect async thunk to react hook

--- applications/mail/src/app/hooks/actions/applyLocation/useApplyLocation.ts
+++ applications/mail/src/app/hooks/actions/applyLocation/useApplyLocation.ts
@@ -1,18 +1,51 @@
 import useFlag from '@proton/unleash/useFlag';
 
+import { isMessage as testIsMessage } from 'proton-mail/helpers/elements';
+import { getLabelName } from 'proton-mail/helpers/labels';
 import type { Element } from 'proton-mail/models/element';
+import { useMailDispatch } from 'proton-mail/store/hooks';
+import { labelMessages, unlabelMessages } from 'proton-mail/store/mailbox/mailboxActions';
 
 export interface ApplyLocationParams {
     elements: Element[];
-    labelChanges: { [labelID: string]: boolean }; // true: add, false: remove
+    destinationLabelID: string;
+    action: 'label' | 'unlabel';
     createFilters?: boolean;
+    showSuccessNotification?: boolean;
 }
 
 export const useApplyLocation = () => {
     const enabled = useFlag('ApplyLabelsOptimisticRefactoring');
+    const dispatch = useMailDispatch();
 
-    const applyLocation = ({ elements, labelChanges, createFilters }: ApplyLocationParams): Promise<void> => {
-        console.log('applyLocation', { elements, labelChanges, createFilters });
+    const applyLocation = ({ elements, action, destinationLabelID, showSuccessNotification }: ApplyLocationParams) => {
+        const [firstElement] = elements;
+        const isMessage = testIsMessage(firstElement);
+
+        if (isMessage) {
+            if (action === 'label') {
+                void dispatch(
+                    labelMessages({
+                        elements,
+                        labelID: destinationLabelID,
+                        labelName: getLabelName(destinationLabelID),
+                        isEncryptedSearch: false,
+                        showSuccessNotification,
+                    })
+                );
+            } else {
+                void dispatch(
+                    unlabelMessages({
+                        elements,
+                        labelID: destinationLabelID,
+                        labelName: getLabelName(destinationLabelID),
+                        isEncryptedSearch: false,
+                        showSuccessNotification,
+                    })
+                );
+            }
+        }
+
         throw new Error('Not implemented');
     };
 
