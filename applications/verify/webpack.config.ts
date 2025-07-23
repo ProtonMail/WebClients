2b51278112d3f6b2a46d53e7158931b46af72bc0
Paolo Chiabrera <paolo.chiabrera@proton.ch>
2025-07-23 15:55:03 +0200

    INWEB-199: Remove proton-pack config from verify

--- applications/verify/webpack.config.ts
+++ applications/verify/webpack.config.ts
@@ -1,12 +1,18 @@
-import webpack from 'webpack';
+import type { Configuration } from 'webpack';
 
-import getConfig from '@proton/pack/webpack.config';
+import { type WebpackEnvArgumentsV2, getWebpackOptions } from '@proton/pack/lib/configV2';
+import { getConfigV2 } from '@proton/pack/webpack.config';
 
-const result = (env: any): webpack.Configuration => {
-    const isProduction = process.env.NODE_ENV === 'production';
-    return getConfig({
-        ...env,
-        browserslist: isProduction ? '>0.1%, IE 11, Firefox ESR, Safari 10, Chrome 50' : undefined,
+import appConfig from './appConfig';
+
+const result = (opts: WebpackEnvArgumentsV2): Configuration => {
+    const webpackOptions = getWebpackOptions(opts, { appConfig });
+
+    return getConfigV2({
+        ...webpackOptions,
+        browserslist: webpackOptions.isProduction
+            ? '>0.1%, IE 11, Firefox ESR, Safari 10, Chrome 50'
+            : webpackOptions.browserslist,
     });
 };
 
