fe8ba58c90c09280267529318d693e876adf47dd
Paolo Chiabrera <paolo.chiabrera@proton.ch>
2025-07-23 14:50:02 +0200

    INWEB-201: Remove proton-pack config from wallet

--- applications/wallet/src/app/utils/legal.tsx
+++ applications/wallet/src/app/utils/legal.tsx
@@ -3,9 +3,9 @@ import { c } from 'ttag';
 import { Href } from '@proton/atoms';
 import { getAppStaticUrl } from '@proton/shared/lib/helpers/url';
 
-import { APP_NAME } from '../config';
+import config from '../config';
 
-const termsLink = `${getAppStaticUrl(APP_NAME)}/legal/terms`;
+const termsLink = `${getAppStaticUrl(config.APP_NAME)}/legal/terms`;
 
 export const getTermAndConditionsSentence = () => {
     const termsAndConditionsLink = <Href className="" href={termsLink}>{`terms and conditions`}</Href>;
