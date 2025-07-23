7561d96447f53f324f5d961be0bcc56f63d15f53
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-07-23 13:07:29 +0200

    Migrate VPN settings to config v2

---
+++ applications/vpn-settings/appConfig.ts
@@ -0,0 +1,8 @@
+import type { AppConfigV2 } from '@proton/pack/lib/interface';
+
+const appConfig: AppConfigV2 = {
+    clientType: 2,
+    sentry: 'https://38dd62570d3b4791a4feca7bae54bd20@sentry/21',
+};
+
+export default appConfig;
