e7a163dbb4ffc22acb56324c5bd15b649b08fa29
Divyam Rastogi <divyam.rastogi@proton.ch>
2025-07-23 10:45:11 +0000

    Add global errors & exception logging to IDB Logger

---
+++ packages/shared/lib/logger/storage/index.ts
@@ -0,0 +1,4 @@
+export type { LogEntry, Storage } from './types';
+export { IndexedDBStorage } from './IndexedDBStorage';
+export { LocalStorage } from './LocalStorage';
+export { MemoryStorage } from './MemoryStorage';
