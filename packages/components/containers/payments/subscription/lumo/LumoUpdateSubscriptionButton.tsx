b02356f0fca6fb6fc932df15bb547f8295c023cb
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-07-23 07:40:51 +0000

    Merge branch 'account-lumo' into 'main'
    Account lumo integration 2 - electric boogaloo
    
    See merge request web/clients!17521

--- packages/components/containers/payments/subscription/lumo/LumoUpdateSubscriptionButton.tsx
+++ packages/components/containers/payments/subscription/lumo/LumoUpdateSubscriptionButton.tsx
@@ -1,3 +1,5 @@
+import { c } from 'ttag';
+
 import { useSubscription } from '@proton/account/subscription/hooks';
 import { useUser } from '@proton/account/user/hooks';
 import SettingsLink from '@proton/components/components/link/SettingsLink';
@@ -8,12 +10,8 @@ const LumoUpdateSubscriptionButton = () => {
     const [subscription] = useSubscription();
 
     return (
-        // When it's time then simply uncomment the below code and remove the add button above;
-        // <SettingsLink target="_blank" path={getUpgradePath({ user, subscription, target: 'checkout' })}>
-        //     {c('Link').t`Add to your subscription`}
-        // </SettingsLink>
         <SettingsLink target="_blank" path={getUpgradePath({ user, subscription, target: 'checkout' })}>
-            Add to your subscription
+            {c('Link').t`Add to your subscription`}
         </SettingsLink>
     );
 };
