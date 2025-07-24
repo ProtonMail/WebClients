4db6c987dd24b6cc4446404984033891cd89d947
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-07-24 11:28:22 +0200

    Fix pass globals declarations

--- packages/pass/globals.d.ts
+++ packages/pass/globals.d.ts
@@ -1,18 +1,18 @@
 import type { ContextBridgeApi } from './types/desktop';
 
 declare global {
-    declare const BETA: boolean;
-    declare const BUILD_TARGET: string;
-    declare const BUILD_STORE_TARGET: string;
-    declare const DESKTOP_BUILD: boolean;
-    declare const ENV: string;
-    declare const EXTENSION_BUILD: boolean;
-    declare const OFFLINE_SUPPORTED: boolean;
-    declare const REDUX_DEVTOOLS_PORT: number;
-    declare const RESUME_FALLBACK: boolean;
-    declare const RUNTIME_RELOAD_PORT: number;
-    declare const RUNTIME_RELOAD: boolean;
-    declare const VERSION: string;
+    const BETA: boolean;
+    const BUILD_TARGET: string;
+    const BUILD_STORE_TARGET: string;
+    const DESKTOP_BUILD: boolean;
+    const ENV: string;
+    const EXTENSION_BUILD: boolean;
+    const OFFLINE_SUPPORTED: boolean;
+    const REDUX_DEVTOOLS_PORT: number;
+    const RESUME_FALLBACK: boolean;
+    const RUNTIME_RELOAD_PORT: number;
+    const RUNTIME_RELOAD: boolean;
+    const VERSION: string;
 
     interface Window {
         ctxBridge?: ContextBridgeApi;
