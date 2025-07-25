48f1801d1cc3d7cc06483e282951d2bc1efcb342
Edvin CANDON <edvin.candon@proton.ch>
2025-07-25 09:23:23 +0200

    Handle malformed webauthn requests using Int8Array
    - bpifrance.com uses Int8Array buffers instead of Uint8Arrays
    - This would evade proper serialization when posting the request to the service-worker
    
    [PP-253]

--- packages/pass/utils/buffer/sanitization.ts
+++ packages/pass/utils/buffer/sanitization.ts
@@ -19,6 +19,9 @@ const isArrayBuffer = (value: unknown): value is ArrayBuffer =>
 const isUint8Array = (value: unknown): value is Uint8Array =>
     value instanceof Uint8Array || (typeof window !== 'undefined' && value instanceof window.Uint8Array);
 
+const isInt8Array = (value: unknown): value is Int8Array =>
+    value instanceof Int8Array || (typeof window !== 'undefined' && value instanceof window.Int8Array);
+
 export const SafeUint8Array = typeof window !== 'undefined' ? window.Uint8Array : Uint8Array;
 
 /** If this function is invoked within a content-script, use constructors
@@ -26,6 +29,7 @@ export const SafeUint8Array = typeof window !== 'undefined' ? window.Uint8Array 
 export const sanitizeBuffers = <T>(value: T): SanitizedBuffers<T> => {
     if (isArrayBuffer(value)) return uint8ArrayToBase64String(new SafeUint8Array(value)) as SanitizedBuffers<T>;
     else if (isUint8Array(value)) return uint8ArrayToBase64String(value) as SanitizedBuffers<T>;
+    else if (isInt8Array(value)) return uint8ArrayToBase64String(new SafeUint8Array(value)) as SanitizedBuffers<T>;
     else if (Array.isArray(value)) return value.map(sanitizeBuffers) as SanitizedBuffers<T>;
     else if (isObject(value)) return objectMap(value, (_, val) => sanitizeBuffers(val)) as SanitizedBuffers<T>;
     return value as SanitizedBuffers<T>;
