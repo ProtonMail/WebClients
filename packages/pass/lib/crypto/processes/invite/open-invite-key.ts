48c800fb54ecaac37349899bf5b2693858d77c6c
Edvin CANDON <edvin.candon@proton.ch>
2025-07-28 09:47:04 +0200

    Fix missing signature validation when opening invite keys
    [IDTEAM-5063]

--- packages/pass/lib/crypto/processes/invite/open-invite-key.ts
+++ packages/pass/lib/crypto/processes/invite/open-invite-key.ts
@@ -1,5 +1,6 @@
 import type { PrivateKeyReference, PublicKeyReference } from '@proton/crypto';
-import { CryptoProxy } from '@proton/crypto';
+import { CryptoProxy, VERIFICATION_STATUS } from '@proton/crypto';
+import { PassCryptoError } from '@proton/pass/lib/crypto/utils/errors';
 import type { KeyRotationKeyPair } from '@proton/pass/types';
 import { PassSignatureContext } from '@proton/pass/types';
 import { base64StringToUint8Array } from '@proton/shared/lib/helpers/encoding';
@@ -14,16 +15,18 @@ export const openInviteKey = async ({
     inviteKey,
     invitedPrivateKey,
     inviterPublicKeys,
-}: OpenInviteKeyProcessParams): Promise<Uint8Array> =>
-    (
-        await CryptoProxy.decryptMessage({
-            binaryMessage: base64StringToUint8Array(inviteKey.Key),
-            decryptionKeys: invitedPrivateKey,
-            verificationKeys: inviterPublicKeys,
-            format: 'binary',
-            signatureContext: {
-                value: PassSignatureContext.VaultInviteExistingUser,
-                required: true,
-            },
-        })
-    ).data;
+}: OpenInviteKeyProcessParams): Promise<Uint8Array> => {
+    const { data, verificationStatus } = await CryptoProxy.decryptMessage({
+        binaryMessage: base64StringToUint8Array(inviteKey.Key),
+        decryptionKeys: invitedPrivateKey,
+        verificationKeys: inviterPublicKeys,
+        format: 'binary',
+        signatureContext: {
+            value: PassSignatureContext.VaultInviteExistingUser,
+            required: true,
+        },
+    });
+
+    if (verificationStatus !== VERIFICATION_STATUS.SIGNED_AND_VALID) throw new PassCryptoError('Invalid invite key');
+    return data;
+};
