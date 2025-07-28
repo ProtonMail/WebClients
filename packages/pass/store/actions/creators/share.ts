755635460c1f0a0a4c2877ce2be4224190a4ac9f
Matthieu Lux <matthieu.lux@proton.ch>
2025-07-28 12:27:28 +0200

    Data layer for updating share visibility
    [IDTEAM-4714]

--- packages/pass/store/actions/creators/share.ts
+++ packages/pass/store/actions/creators/share.ts
@@ -4,7 +4,9 @@ import { c } from 'ttag';
 import { isVaultShare } from '@proton/pass/lib/shares/share.predicates';
 import { withCache } from '@proton/pass/store/actions/enhancers/cache';
 import { withNotification } from '@proton/pass/store/actions/enhancers/notification';
+import { requestActionsFactory } from '@proton/pass/store/request/flow';
 import type { SynchronizationResult } from '@proton/pass/store/sagas/client/sync';
+import type { ShareHiddenMap, ShareId, ShareType } from '@proton/pass/types';
 import { type Share, type ShareSyncKeys } from '@proton/pass/types';
 import { pipe } from '@proton/pass/utils/fp/pipe';
 
@@ -28,3 +30,28 @@ export const sharesEventNew = createAction('shares::event::new', (payload: Synch
 export const sharesEventSync = createAction('shares::event::sync', (payload: Pick<Share, ShareSyncKeys>) =>
     withCache({ payload })
 );
+
+export const sharesHide = requestActionsFactory<
+    { hideMap: ShareHiddenMap },
+    { shares: Record<ShareId, Share<ShareType.Vault>> }
+>('shares::hide')({
+    success: {
+        prepare: (payload: { shares: Record<ShareId, Share<ShareType.Vault>> }) =>
+            pipe(
+                withCache,
+                withNotification({
+                    type: 'info',
+                    text: c('Info').t`Vault organization successfully updated`,
+                })
+            )({ payload }),
+    },
+    failure: {
+        prepare: (error: unknown, payload) =>
+            withNotification({
+                type: 'error',
+                // FIXME (@ecandon): we could pass the number of failures here
+                text: c('Error').t`Could not resolve share members`,
+                error,
+            })({ payload, error }),
+    },
+});
