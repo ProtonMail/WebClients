632b9e78c4fbb07f14b158fa0a88fc5d6543f43d
Matthieu Lux <matthieu.lux@protonmail.com>
2025-07-30 17:26:33 +0200

    Use shares and items root selectors filtered with visibility
    Deal with redirection when something on screen is hidden
    
    [IDTEAM-4714]

--- packages/pass/components/Navigation/Autoselect.tsx
+++ packages/pass/components/Navigation/Autoselect.tsx
@@ -7,6 +7,7 @@ import { useNavigationActions } from '@proton/pass/components/Navigation/Navigat
 import { useNavigationFilters } from '@proton/pass/components/Navigation/NavigationFilters';
 import { useItemScope } from '@proton/pass/components/Navigation/NavigationMatches';
 import { getItemRoute, getLocalPath } from '@proton/pass/components/Navigation/routing';
+import { isShareVisible } from '@proton/pass/lib/shares/share.predicates';
 import { selectShare } from '@proton/pass/store/selectors';
 
 export const Autoselect: FC = () => {
@@ -18,9 +19,11 @@ export const Autoselect: FC = () => {
 
     /** Check if we should preserve the current filters (selected vault exists)  */
     const selectedVault = useSelector(selectShare(filters.selectedShareId));
+    const shareHidden = selectedVault !== undefined && !isShareVisible(selectedVault);
     const clearFilters = filters.selectedShareId !== null && selectedVault === undefined;
 
     const to = (() => {
+        if (shareHidden) return getLocalPath();
         if (autoselect) return preserveSearch(getItemRoute(autoselect.shareId, autoselect.itemId, { scope }));
         if (clearFilters) return getLocalPath();
         return null;
--- packages/pass/store/selectors/items.ts
+++ packages/pass/store/selectors/items.ts
@@ -7,7 +7,7 @@ import { unwrapOptimisticState } from '@proton/pass/store/optimistic/utils/trans
 import type { ItemsByShareId } from '@proton/pass/store/reducers/items';
 import { withOptimisticItemsByShareId } from '@proton/pass/store/reducers/items';
 import { SelectorError } from '@proton/pass/store/selectors/errors';
-import { selectVisibleShares } from '@proton/pass/store/selectors/shares';
+import { selectAllShareIds } from '@proton/pass/store/selectors/shares';
 import type { State } from '@proton/pass/store/types';
 import type {
     ItemRevision,
@@ -31,9 +31,10 @@ export const selectItemDrafts = (state: State) => state.items.drafts;
 export const selectNonFailedItems = createSelector(selectItemsState, asIfNotFailed);
 export const selectNonOptimisticItems = createSelector(selectItemsState, asIfNotOptimistic);
 export const selectItems = createSelector(selectItemsState, unwrapOptimisticState);
-export const selectAllItems = createSelector(selectItems, flattenItemsByShareId);
-export const selectAllVisibleItems = createSelector([selectAllItems, selectVisibleShares], (items, shares) =>
-    items.filter(({ shareId }) => shares.has(shareId))
+// Intentionally boring name to push for using visibility filter by default
+export const selectAllIncludingHiddenItems = createSelector(selectItems, flattenItemsByShareId);
+export const selectAllItems = createSelector([selectAllIncludingHiddenItems, selectAllShareIds], (items, shareIds) =>
+    items.filter(({ shareId }) => shareIds.has(shareId))
 );
 export const selectTrashedItems = createSelector(selectAllItems, (items) => items.filter(isTrashed));
 export const selectPinnedItems = createSelector(selectAllItems, (items) => items.filter(and(isActive, isPinned)));
