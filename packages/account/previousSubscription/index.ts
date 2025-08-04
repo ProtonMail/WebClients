61448b76f1d165c393fa42001341eb80163a90c5
Jack Crompton <jack.crompton@proton.ch>
2025-08-04 18:55:16 +0200

    Add PreviousSubscription redux async thunk and hooks
    CP-10174

---
+++ packages/account/previousSubscription/index.ts
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { type LatestSubscription, getLastCancelledSubscription } from '@proton/payments';
+import type { ProtonThunkArguments } from '@proton/redux-shared-store-types';
+import { createAsyncModelThunk, handleAsyncModel, previousSelector } from '@proton/redux-utilities';
+
+import { getInitialModelState } from '../initialModelState';
+import type { ModelState } from '../interface';
+import { type UserState, userThunk } from '../user';
+
+const name = 'previousSubscription' as const;
+
+export interface PreviousSubscriptionState extends UserState {
+    [name]: ModelState<{ hasHadSubscription: boolean; previousSubscriptionEndTime: number }>;
+}
+
+type SliceState = PreviousSubscriptionState[typeof name];
+type Model = NonNullable<SliceState['value']>;
+
+export const selectPreviousSubscription = (state: PreviousSubscriptionState) => state[name];
+
+const modelThunk = createAsyncModelThunk<Model, PreviousSubscriptionState, ProtonThunkArguments>(`${name}/fetch`, {
+    miss: async ({ extraArgument, dispatch }) => {
+        const user = await dispatch(userThunk());
+
+        if (user.isPaid) {
+            return { hasHadSubscription: true, previousSubscriptionEndTime: 0 };
+        }
+
+        const response = await extraArgument.api<LatestSubscription>(getLastCancelledSubscription());
+        const lastSubscriptionEnd = response.LastSubscriptionEnd || 0;
+
+        return { hasHadSubscription: lastSubscriptionEnd > 0, previousSubscriptionEndTime: lastSubscriptionEnd };
+    },
+    previous: previousSelector(selectPreviousSubscription),
+});
+
+const initialState: SliceState = getInitialModelState<Model>();
+
+const slice = createSlice({
+    name,
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        handleAsyncModel(builder, modelThunk);
+    },
+});
+
+export const previousSubscriptionReducer = { [name]: slice.reducer };
+export const previousSubscriptionThunk = modelThunk.thunk;
