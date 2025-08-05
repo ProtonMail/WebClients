a032749d2bd956e74b2443d349376e6b5f1cc871
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-08-05 08:11:46 +0000

    Add loader in the button of the mark as legitimate modal

--- applications/mail/src/app/components/message/extrasHeader/components/ExtraSpamScore.tsx
+++ applications/mail/src/app/components/message/extrasHeader/components/ExtraSpamScore.tsx
@@ -25,7 +25,6 @@ const ExtraSpamScore = ({ message }: Props) => {
     const api = useApi();
     const { createNotification } = useNotifications();
     const [spamScoreModalProps, setSpamScoreModalOpen] = useModalState();
-    const isSuspiciousFlagged = isSuspicious(message.data);
 
     if (isDMARCValidationFailure(message.data)) {
         return (
@@ -39,6 +38,7 @@ const ExtraSpamScore = ({ message }: Props) => {
                     </Href>
                 }
                 variant="danger-outline"
+                data-testid="spam-banner:dmarc-validation-failure"
             >
                 {c('Info')
                     .t`This email has failed its domain's authentication requirements. It may be spoofed or improperly forwarded.`}
@@ -46,17 +46,26 @@ const ExtraSpamScore = ({ message }: Props) => {
         );
     }
 
-    if (
-        (isAutoFlaggedPhishing(message.data) || isSuspiciousFlagged) &&
-        (!isManualFlaggedHam(message.data) || LabelIDs.includes(MAILBOX_LABEL_IDS.SPAM))
-    ) {
-        const markAsLegitimate = async () => {
-            await api(markAsHam(message.data.ID));
-            await call();
-            createNotification({ text: c('Success').t`Message marked as legitimate` });
-        };
+    const isSuspiciousFlagged = isSuspicious(message.data);
 
-        return (
+    if (
+        !(
+            (isAutoFlaggedPhishing(message.data) || isSuspiciousFlagged) &&
+            (!isManualFlaggedHam(message.data) || LabelIDs.includes(MAILBOX_LABEL_IDS.SPAM))
+        )
+    ) {
+        return null;
+    }
+
+    const markAsLegitimate = async () => {
+        await api(markAsHam(message.data.ID));
+        await call();
+        setSpamScoreModalOpen(false);
+        createNotification({ text: c('Success').t`Message marked as legitimate` });
+    };
+
+    return (
+        <>
             <Banner
                 link={
                     <Href className="inline-block" href={getBlogURL('/prevent-phishing-attacks')}>
@@ -64,45 +73,37 @@ const ExtraSpamScore = ({ message }: Props) => {
                     </Href>
                 }
                 action={
-                    <Button
-                        onClick={() => setSpamScoreModalOpen(true)}
-                        disabled={loading}
-                        data-testid="spam-banner:mark-legitimate"
-                    >
+                    <Button onClick={() => setSpamScoreModalOpen(true)} data-testid="spam-banner:mark-legitimate">
                         {c('Action').t`Mark legitimate`}
                     </Button>
                 }
                 data-testid="spam-banner:phishing-banner"
                 variant="danger"
             >
-                {isSuspiciousFlagged ? (
-                    <>
-                        {c('Info')
-                            .t`Our system flagged this message as a phishing attempt. Please check to ensure that it is legitimate.`}
-                    </>
-                ) : (
-                    <>
-                        {c('Info')
-                            .t`Our system flagged this as suspicious. If it is not a phishing or scam email, mark as legitimate.`}
-                    </>
-                )}
-                <Prompt
-                    title={c('Title').t`Mark email as legitimate`}
-                    buttons={[
-                        <Button color="norm" onClick={() => withLoading(markAsLegitimate())}>{c('Action')
-                            .t`Mark legitimate`}</Button>,
-                        <Button onClick={spamScoreModalProps.onClose}>{c('Action').t`Cancel`}</Button>,
-                    ]}
-                    {...spamScoreModalProps}
-                >
-                    {c('Info')
-                        .t`We apologize. This might have been a mistake from our side. Can you please confirm that you want to mark this email as a legitimate one?`}
-                </Prompt>
+                <>
+                    {isSuspiciousFlagged
+                        ? c('Info')
+                              .t`Our system flagged this message as a phishing attempt. Please check to ensure that it is legitimate.`
+                        : c('Info')
+                              .t`Our system flagged this as suspicious. If it is not a phishing or scam email, mark as legitimate.`}
+                </>
             </Banner>
-        );
-    }
 
-    return null;
+            <Prompt
+                data-testid="spam-banner:mark-legitimate-prompt"
+                title={c('Title').t`Mark email as legitimate`}
+                buttons={[
+                    <Button loading={loading} color="norm" onClick={() => withLoading(markAsLegitimate())}>{c('Action')
+                        .t`Mark legitimate`}</Button>,
+                    <Button onClick={spamScoreModalProps.onClose}>{c('Action').t`Cancel`}</Button>,
+                ]}
+                {...spamScoreModalProps}
+            >
+                {c('Info')
+                    .t`We apologize. This might have been a mistake from our side. Can you please confirm that you want to mark this email as a legitimate one?`}
+            </Prompt>
+        </>
+    );
 };
 
 export default ExtraSpamScore;
