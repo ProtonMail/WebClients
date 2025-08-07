942565c0be4b5a801bef4567924d0ef9e770cf7f
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-08-07 15:49:12 +0200

    Add top banner

--- packages/account/delegatedAccess/emergencyAccess/incoming/IncomingController.tsx
+++ packages/account/delegatedAccess/emergencyAccess/incoming/IncomingController.tsx
@@ -1,42 +1,26 @@
-import { type ReactNode, createContext, useCallback, useContext, useEffect, useRef } from 'react';
+import { type ReactNode, createContext, useCallback, useContext, useRef } from 'react';
 
-import { type Action, type ThunkDispatch } from '@reduxjs/toolkit';
-
-import { type ContactEmailsState, contactEmailsThunk } from '@proton/mail/store/contactEmails';
-import { baseUseDispatch, baseUseSelector } from '@proton/react-redux-store';
-import type { ProtonThunkArguments } from '@proton/redux-shared-store-types';
 import type { APP_NAMES } from '@proton/shared/lib/constants';
 import createListeners, { type Listeners } from '@proton/shared/lib/helpers/listeners';
-import noop from '@proton/utils/noop';
 
-import { listIncomingDelegatedAccess } from '../../incomingActions';
-import { type DelegatedAccessState } from '../../index';
 import { AccessIncomingEmergencyContactAction } from './AccessIncomingEmergencyContactAction';
 import { DeleteIncomingEmergencyContactAction } from './DeleteIncomingEmergencyContactAction';
 import { RequestAccessIncomingEmergencyContactAction } from './RequestAccessIncomingEmergencyContactAction';
 import type { ActionListener, ActionPayload } from './interface';
-import { type EnrichedIncomingDelegatedAccessReturnValue, selectEnrichedIncomingDelegatedAccess } from './selector';
+import { useIncomingItems } from './useIncomingItems';
 
 export interface IncomingController {
     notify: (payload: ActionPayload) => void;
     subscribe: (cb: ActionListener) => void;
-    items: EnrichedIncomingDelegatedAccessReturnValue['items'];
+    items: ReturnType<typeof useIncomingItems>['items'];
     loading: boolean;
 }
 
 export const IncomingControllerContext = createContext<IncomingController>({} as any);
-type RequiredState = DelegatedAccessState & ContactEmailsState;
 
 export const IncomingControllerProvider = ({ app, children }: { app: APP_NAMES; children: ReactNode }) => {
     const listenersRef = useRef<Listeners<[ActionPayload], undefined> | null>(null);
-    const dispatch = baseUseDispatch<ThunkDispatch<RequiredState, ProtonThunkArguments, Action>>();
-    const { items, loading } = baseUseSelector<RequiredState, EnrichedIncomingDelegatedAccessReturnValue>(
-        selectEnrichedIncomingDelegatedAccess
-    );
-
-    useEffect(() => {
-        Promise.all([dispatch(listIncomingDelegatedAccess()), dispatch(contactEmailsThunk())]).catch(noop);
-    }, []);
+    const { items, loading } = useIncomingItems();
 
     const incomingController: IncomingController = {
         notify: useCallback((payload) => {
--- packages/account/delegatedAccess/emergencyAccess/outgoing/OutgoingController.tsx
+++ packages/account/delegatedAccess/emergencyAccess/outgoing/OutgoingController.tsx
@@ -1,25 +1,17 @@
-import { type ReactNode, createContext, useCallback, useContext, useEffect, useRef } from 'react';
+import { type ReactNode, createContext, useCallback, useContext, useRef } from 'react';
 
-import type { Action, ThunkDispatch } from '@reduxjs/toolkit';
-
-import { type ContactEmailsState, contactEmailsThunk } from '@proton/mail/store/contactEmails';
-import { baseUseDispatch, baseUseSelector } from '@proton/react-redux-store';
-import type { ProtonThunkArguments } from '@proton/redux-shared-store-types';
 import createListeners, { type Listeners } from '@proton/shared/lib/helpers/listeners';
-import noop from '@proton/utils/noop';
 
 import { maxOutgoingDelegatedAccessContacts } from '../../constants';
-import { type DelegatedAccessState } from '../../index';
-import { listOutgoingDelegatedAccess } from '../../outgoingActions';
 import { AddOutgoingEmergencyContactAction } from './AddOutgoingEmergencyContactAction';
 import { DeleteOutgoingEmergencyContactAction } from './DeleteOutgoingEmergencyContactAction';
 import { EditOutgoingEmergencyContactAction } from './EditOutgoingEmergencyContactAction';
 import { GrantAccessOutgoingEmergencyContactAction } from './GrantAccessOutgoingEmergencyContactAction';
 import type { ActionListener, ActionPayload } from './interface';
-import { type EnrichedOutgoingDelegatedAccessReturnValue, selectEnrichedOutgoingDelegatedAccess } from './selector';
+import { useOutgoingItems } from './useOutgoingItems';
 
 export interface OutgoingController {
-    items: EnrichedOutgoingDelegatedAccessReturnValue['items'];
+    items: ReturnType<typeof useOutgoingItems>['items'];
     loading: boolean;
     notify: (payload: ActionPayload) => void;
     subscribe: (cb: ActionListener) => void;
@@ -31,18 +23,9 @@ export interface OutgoingController {
 
 export const OutgoingControllerContext = createContext<OutgoingController>({} as any);
 
-type RequiredState = DelegatedAccessState & ContactEmailsState;
-
 export const OutgoingControllerProvider = ({ children }: { children: ReactNode }) => {
     const listenersRef = useRef<Listeners<[ActionPayload], undefined> | null>(null);
-    const dispatch = baseUseDispatch<ThunkDispatch<RequiredState, ProtonThunkArguments, Action>>();
-    const { items, loading } = baseUseSelector<RequiredState, EnrichedOutgoingDelegatedAccessReturnValue>(
-        selectEnrichedOutgoingDelegatedAccess
-    );
-
-    useEffect(() => {
-        Promise.all([dispatch(listOutgoingDelegatedAccess()), dispatch(contactEmailsThunk())]).catch(noop);
-    }, []);
+    const { items, loading } = useOutgoingItems();
 
     const outgoingDelegatedAccessCount = items.length;
     const hasReachedLimit = outgoingDelegatedAccessCount === maxOutgoingDelegatedAccessContacts;
