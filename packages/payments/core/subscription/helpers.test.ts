e8d3f42d24c57e70ca86e7a49406fb85adb88ba4
Alexey Karpov <alexey.karpov@proton.ch>
2025-08-12 10:44:28 +0200

    Fix plan-addon mappings

--- packages/payments/core/subscription/helpers.test.ts
+++ packages/payments/core/subscription/helpers.test.ts
@@ -9,7 +9,9 @@ import {
     canModify,
     getAvailableSubscriptionActions,
     getIsPlanTransitionForbidden,
+    getLumoAddonNameByPlan,
     getPlanIDs,
+    getScribeAddonNameByPlan,
     getSubscriptionPlanTitle,
     isCheckForbidden,
     isManagedExternally,
@@ -626,5 +628,50 @@ describe('forbidden plan transitions', () => {
         const planIDs: PlanIDs = { [PLANS.LUMO]: 1 };
         const result = getIsPlanTransitionForbidden({ subscription: undefined, planIDs, plansMap });
         expect(result).toEqual(null);
+    });
+});
+
+describe('getLumoAddonNameByPlan', () => {
+    it.each<[plan: PLANS, expectedAddon: ADDON_NAMES]>([
+        // B2C
+        [PLANS.MAIL, ADDON_NAMES.LUMO_MAIL],
+        [PLANS.DRIVE, ADDON_NAMES.LUMO_DRIVE],
+        [PLANS.DRIVE_1TB, ADDON_NAMES.LUMO_DRIVE_1TB],
+        [PLANS.PASS, ADDON_NAMES.LUMO_PASS],
+        [PLANS.PASS_FAMILY, ADDON_NAMES.LUMO_PASS_FAMILY],
+        [PLANS.VPN2024, ADDON_NAMES.LUMO_VPN2024],
+        [PLANS.BUNDLE, ADDON_NAMES.LUMO_BUNDLE],
+        [PLANS.FAMILY, ADDON_NAMES.LUMO_FAMILY],
+        [PLANS.DUO, ADDON_NAMES.LUMO_DUO],
+
+        // B2B
+        [PLANS.MAIL_PRO, ADDON_NAMES.LUMO_MAIL_PRO],
+        [PLANS.MAIL_BUSINESS, ADDON_NAMES.LUMO_MAIL_BUSINESS],
+        [PLANS.DRIVE_PRO, ADDON_NAMES.LUMO_DRIVE_PRO],
+        [PLANS.DRIVE_BUSINESS, ADDON_NAMES.LUMO_DRIVE_BUSINESS],
+        [PLANS.BUNDLE_PRO, ADDON_NAMES.LUMO_BUNDLE_PRO],
+        [PLANS.BUNDLE_PRO_2024, ADDON_NAMES.LUMO_BUNDLE_PRO_2024],
+        [PLANS.VPN_PRO, ADDON_NAMES.LUMO_VPN_PRO],
+        [PLANS.VPN_BUSINESS, ADDON_NAMES.LUMO_VPN_BUSINESS],
+        [PLANS.PASS_PRO, ADDON_NAMES.LUMO_PASS_PRO],
+        [PLANS.PASS_BUSINESS, ADDON_NAMES.LUMO_PASS_BUSINESS],
+    ])('returns %s -> %s', (plan, expectedAddon) => {
+        expect(getLumoAddonNameByPlan(plan)).toBe(expectedAddon);
+    });
+});
+
+describe('getScribeAddonNameByPlan', () => {
+    it.each<[plan: PLANS, expectedAddon: ADDON_NAMES]>([
+        [PLANS.MAIL_PRO, ADDON_NAMES.MEMBER_SCRIBE_MAIL_PRO],
+        [PLANS.BUNDLE_PRO, ADDON_NAMES.MEMBER_SCRIBE_BUNDLE_PRO],
+        [PLANS.BUNDLE_PRO_2024, ADDON_NAMES.MEMBER_SCRIBE_BUNDLE_PRO_2024],
+        [PLANS.MAIL_BUSINESS, ADDON_NAMES.MEMBER_SCRIBE_MAIL_BUSINESS],
+    ])('returns %s -> %s', (plan, expectedAddon) => {
+        expect(getScribeAddonNameByPlan(plan)).toBe(expectedAddon);
+    });
+
+    it('returns undefined for plans without scribe addon mapping', () => {
+        expect(getScribeAddonNameByPlan(PLANS.MAIL)).toBeUndefined();
+        expect(getScribeAddonNameByPlan(PLANS.VPN2024)).toBeUndefined();
     });
 });
