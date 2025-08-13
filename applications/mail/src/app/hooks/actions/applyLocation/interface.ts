390a4d14dfed86c7b5be4ff8a461dce2182a1e56
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-08-13 09:49:26 +0000

    Move action regression environment

---
+++ applications/mail/src/app/hooks/actions/applyLocation/interface.ts
@@ -0,0 +1,33 @@
+import type { SPAM_ACTION } from '@proton/shared/lib/mail/mailSettings';
+
+import type { Element } from 'proton-mail/models/element';
+
+export interface ApplyLocationParams {
+    elements: Element[];
+    targetLabelID: string;
+    removeLabel?: boolean;
+    // This is used to avoid sending a unsubscribe request to a phishing email
+    askUnsubscribe?: boolean;
+    createFilters?: boolean;
+    showSuccessNotification?: boolean;
+    spamAction?: SPAM_ACTION;
+}
+
+export enum APPLY_LOCATION_TYPES {
+    MOVE = 'MOVE',
+    APPLY_LABEL = 'APPLY_LABEL',
+    STAR = 'STAR',
+}
+
+export interface ApplyLocationMoveProps extends ApplyLocationParams {
+    type: APPLY_LOCATION_TYPES.MOVE;
+}
+
+export interface ApplyLocationLabelProps extends ApplyLocationParams {
+    type: APPLY_LOCATION_TYPES.APPLY_LABEL;
+    changes: { [labelID: string]: boolean };
+}
+
+export interface ApplyLocationStarProps extends ApplyLocationParams {
+    type: APPLY_LOCATION_TYPES.STAR;
+}
