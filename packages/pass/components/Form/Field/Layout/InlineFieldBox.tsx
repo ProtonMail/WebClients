2a9935ec631db1f5a1cf729ec81fd253e5c8cdbb
Edvin CANDON <edvin.candon@proton.ch>
2025-08-14 18:11:06 +0200

    Refactor backup export modal
    - Fix UI when long backup directory
    
    [IDTEAM-5145]
    
    Improve automatic backup modal for encrypted password support

--- packages/pass/components/Form/Field/Layout/InlineFieldBox.tsx
+++ packages/pass/components/Form/Field/Layout/InlineFieldBox.tsx
@@ -4,11 +4,16 @@ import clsx from '@proton/utils/clsx';
 
 import './InlineFieldBox.scss';
 
-type Props = PropsWithChildren & { className?: string; label: ReactNode; size?: 'sm' | 'md' | 'lg' };
+type Props = PropsWithChildren & {
+    className?: string;
+    label: ReactNode;
+    size?: 'sm' | 'md' | 'lg';
+    childrenClassName?: string;
+};
 
-export const InlineFieldBox: FC<Props> = ({ label, children, size = 'sm', className = '' }) => (
+export const InlineFieldBox: FC<Props> = ({ label, children, size = 'sm', className = '', childrenClassName = '' }) => (
     <div className={clsx('pass-inline-field-box', size, className)}>
         <label>{label}</label>
-        <div className="flex items-center justify-end">{children}</div>
+        <div className={clsx('flex items-center justify-end', childrenClassName)}>{children}</div>
     </div>
 );
