cfa91cbb08252316830e2399a67fc9f99a5c51c6
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-08-18 13:23:41 +0200

    Update .eslintrc.js

--- applications/docs-editor/.eslintrc.js
+++ applications/docs-editor/.eslintrc.js
@@ -1,20 +1,19 @@
 const isFixMode = process.argv.includes('--fix')
 module.exports = {
+  root: true,
   extends: ['@proton/eslint-config-proton'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: './tsconfig.json',
+    projectService: true,
   },
   rules: {
     'react/prop-types': 'off',
     ...(!isFixMode && {
       'react-hooks/exhaustive-deps': 'warn',
     }),
-    '@typescript-eslint/lines-between-class-members': 'off',
     '@typescript-eslint/naming-convention': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
     'monorepo-cop/no-disable-monorepo-no-relative-rule': 'off',
     'monorepo-cop/no-relative-import-outside-package': 'warn',
     'no-console': [
