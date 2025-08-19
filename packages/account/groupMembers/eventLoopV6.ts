64c84fdf689fe4d5cc0ce76b1eee2d460c196e0e
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-08-19 17:45:10 +0200

    Fix group memberships for event loop v6

--- packages/account/groupMembers/eventLoopV6.ts
+++ packages/account/groupMembers/eventLoopV6.ts
@@ -1,8 +1,19 @@
+import { CacheType } from '@proton/redux-utilities';
+import noop from '@proton/utils/noop';
+
 import type { CoreEventLoopV6Callback } from '../coreEventLoop/interface';
+import { groupMembershipsThunk } from '../groupMemberships';
 import { groupMembersEventLoopV6Thunk } from './index';
 
 export const groupMembersLoop: CoreEventLoopV6Callback = ({ event, dispatch, api }) => {
     if (event.GroupMembers?.length) {
+        // Force refresh group memberships when group members are updated. This is due to the fact that:
+        // 1) no group memberships are returned in the event loop.
+        // 2) getting group members requires organization scope, so listening to that event loop v6 update doesn't work for free users.
+        // This is done in a fire-and-forget way, so it's not a big deal if it fails.
+        // This is a best effort `groupMembershipsListener` for event loop v6.
+        dispatch(groupMembershipsThunk({ cache: CacheType.None })).catch(noop);
+
         return dispatch(groupMembersEventLoopV6Thunk({ event, api }));
     }
 };
