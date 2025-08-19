73d96a962ea421015e335d91257d595231f797f1
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2025-08-19 09:01:28 +0000

    Vpn dashboard 24 month plans

--- packages/components/components/price/helper.test.ts
+++ packages/components/components/price/helper.test.ts
@@ -1,12 +1,14 @@
 import { getSimplePriceString, humanPrice } from './helper';
 
 describe('getSimplePriceString', () => {
+    const NBSP = '\u00A0';
+
     it('should return a String', () => {
-        expect(getSimplePriceString('EUR', 111)).toBe('1.11 €');
+        expect(getSimplePriceString('EUR', 111)).toBe(`1.11${NBSP}€`);
     });
 
     it('should support CHF', () => {
-        expect(getSimplePriceString('CHF', 111)).toBe('CHF 1.11');
+        expect(getSimplePriceString('CHF', 111)).toBe(`CHF${NBSP}1.11`);
     });
 
     it('should support USD', () => {
@@ -14,25 +16,25 @@ describe('getSimplePriceString', () => {
     });
 
     it('should support BRL', () => {
-        expect(getSimplePriceString('BRL', 111)).toBe('BRL 1.11');
+        expect(getSimplePriceString('BRL', 111)).toBe(`BRL${NBSP}1.11`);
     });
 
     it('should remove .00 when needed', () => {
-        expect(getSimplePriceString('EUR', 100)).toBe('1 €');
-        expect(getSimplePriceString('EUR', 10000)).toBe('100 €');
-        expect(getSimplePriceString('EUR', 10001)).toBe('100.01 €');
-        expect(getSimplePriceString('EUR', 10010)).toBe('100.10 €');
+        expect(getSimplePriceString('EUR', 100)).toBe(`1${NBSP}€`);
+        expect(getSimplePriceString('EUR', 10000)).toBe(`100${NBSP}€`);
+        expect(getSimplePriceString('EUR', 10001)).toBe(`100.01${NBSP}€`);
+        expect(getSimplePriceString('EUR', 10010)).toBe(`100.10${NBSP}€`);
     });
 
     it('should return negative amount', () => {
-        expect(getSimplePriceString('EUR', -123)).toBe('-1.23 €');
-        expect(getSimplePriceString('CHF', -111)).toBe('-CHF 1.11');
+        expect(getSimplePriceString('EUR', -123)).toBe(`-1.23${NBSP}€`);
+        expect(getSimplePriceString('CHF', -111)).toBe(`-CHF${NBSP}1.11`);
         expect(getSimplePriceString('USD', -111)).toBe('-US$1.11');
-        expect(getSimplePriceString('BRL', -111)).toBe('-BRL 1.11');
+        expect(getSimplePriceString('BRL', -111)).toBe(`-BRL${NBSP}1.11`);
     });
 
     it('should support suffix', () => {
-        expect(getSimplePriceString('EUR', 111, ' / month')).toBe('1.11 € / month');
+        expect(getSimplePriceString('EUR', 111, ' / month')).toBe(`1.11${NBSP}€ / month`);
     });
 });
 
