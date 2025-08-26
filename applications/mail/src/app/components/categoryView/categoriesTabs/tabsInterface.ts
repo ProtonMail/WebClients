0f60b741358b211cc145e7f9b7059876c59badc7
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-08-26 08:54:42 +0000

    P3-49 drag and drop categories

--- applications/mail/src/app/components/categoryView/categoriesTabs/tabsInterface.ts
+++ applications/mail/src/app/components/categoryView/categoriesTabs/tabsInterface.ts
@@ -1,1 +1,8 @@
 export type TabSize = 'default' | 'small' | 'tiny';
+
+export enum TabState {
+    ACTIVE = 'active',
+    INACTIVE = 'inactive',
+    DRAGGING_OVER = 'dragging-over',
+    DRAGGING_NEIGHBOR = 'dragging-neighbor',
+}
