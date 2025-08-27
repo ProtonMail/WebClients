bbe62309d17740ac393e8bc9287448fd1d1d965d
István Barna <istvan.barna@proton.ch>
2025-08-27 11:41:10 +0000

    Participant name fix

--- applications/meet/src/app/hooks/useDefaultDisplayName.tsx
+++ applications/meet/src/app/hooks/useDefaultDisplayName.tsx
@@ -2,22 +2,9 @@ import { useUser } from '@proton/account/user/hooks';
 
 import { getRandomParticipantName } from '../utils/getRandomParticipantName';
 
-export function useDefaultDisplayName() {
-    let displayName: string | undefined;
-
-    try {
-        const [user] = useUser();
-        displayName = user?.DisplayName || user?.Name || user?.Email || '';
-    } catch {
-        displayName = undefined;
-    }
-
-    return displayName || '';
-}
-
 const useDefaultDisplayNameAuthenticated = () => {
     const [user] = useUser();
-    return user?.Name || '';
+    return user?.DisplayName || user?.Name || user?.Email || '';
 };
 
 const useDefaultDisplayNameUnauthenticated = () => {
