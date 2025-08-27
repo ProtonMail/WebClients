7d7ec31af7c58ee5b633d2f63fad474c3d18c137
Will Hsu <will.hsu@proton.ch>
2025-08-27 17:37:10 +0800

    Websocket reconnection and mls check

---
+++ applications/meet/src/app/components/ConnectionLostModal/ConnectionLostModal.tsx
@@ -0,0 +1,42 @@
+import { c } from 'ttag';
+
+import { Button } from '@proton/atoms';
+import { ModalTwo, ModalTwoContent } from '@proton/components';
+
+import './ConnectionLostModal.scss';
+
+interface ConnectionLostModalProps {
+    onClose: () => void;
+}
+
+export const ConnectionLostModal = ({ onClose }: ConnectionLostModalProps) => {
+    return (
+        <ModalTwo open={true} rootClassName="bg-transparent connection-lost-modal" className="meet-radius">
+            <ModalTwoContent
+                className="flex flex-column justify-space-between p-4 mx-4 pb-0 gap-4 text-center bg-norm pt-custom"
+                style={{ '--pt-custom': '3rem' }}
+            >
+                <div className="text-3xl text-semibold">{c('meet_2025').t`Connection lost`}</div>
+                <div className="color-weak">{c('meet_2025').t`Connection lost, please join meeting again`}</div>
+
+                <div className="w-full flex flex-column gap-2 mt-4">
+                    <Button
+                        className="rounded-full color-invert reload-button py-4"
+                        onClick={() => window.location.reload()}
+                        color="norm"
+                        size="large"
+                    >{c('meet_2025 Action').t`OK`}</Button>
+
+                    <Button
+                        className="rounded-full py-4 bg-weak close-button border-none"
+                        onClick={onClose}
+                        color="weak"
+                        size="large"
+                    >
+                        {c('meet_2025 Action').t`Close`}
+                    </Button>
+                </div>
+            </ModalTwoContent>
+        </ModalTwo>
+    );
+};
