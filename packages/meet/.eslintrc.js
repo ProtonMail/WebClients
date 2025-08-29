d0e185c40305f25fd9ee658dc4ddc50293b49b04
Istvan Barna <istvan.barna@proton.ch>
2025-08-29 10:42:13 +0200

    fixing pipeline

--- packages/meet/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: ['@proton/eslint-config-proton'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: './tsconfig.json',
-    },
-    ignorePatterns: ['.eslintrc.js'],
-};
