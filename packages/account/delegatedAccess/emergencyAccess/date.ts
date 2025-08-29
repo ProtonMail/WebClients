520c8f90bdc48f94a2f5005096091e2942bb91c1
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-08-29 15:36:47 +0200

    Fix emergency access time left granularity

--- packages/account/delegatedAccess/emergencyAccess/date.ts
+++ packages/account/delegatedAccess/emergencyAccess/date.ts
@@ -2,13 +2,13 @@ import { format } from 'date-fns';
 
 import { dateLocale } from '@proton/shared/lib/i18n';
 
-import { getDaysFromMilliseconds, getDaysLeftLabel, getTriggerDelayLabel } from './helper';
+import { getTriggerDelayLabel } from './helper';
 
 export const getFormattedAccessibleAtDate = (date: Date | null) => {
     if (date === null) {
         return null;
     }
-    return format(date, 'PP', { locale: dateLocale });
+    return format(date, 'PPp', { locale: dateLocale });
 };
 
 export const getFormattedCreateTime = (date: Date | null) => {
@@ -16,13 +16,6 @@ export const getFormattedCreateTime = (date: Date | null) => {
         return null;
     }
     return format(date, 'PP', { locale: dateLocale });
-};
-
-export const getFormattedRemainingDays = (diff: number | null) => {
-    if (diff === null) {
-        return null;
-    }
-    return getDaysLeftLabel(getDaysFromMilliseconds(diff));
 };
 
 export const getFormattedTriggerDelay = (diff: number | null) => {
