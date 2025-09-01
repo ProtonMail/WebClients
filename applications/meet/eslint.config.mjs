1e8fe35814656dbc97339ae2ada499ac0c4649d0
Istvan Barna <istvan.barna@proton.ch>
2025-09-01 15:11:14 +0200

    addressing review comments

--- applications/meet/eslint.config.mjs
+++ applications/meet/eslint.config.mjs
@@ -1,15 +1,3 @@
-import { defineConfig } from 'eslint/config';
+import config from '@proton/eslint-config-proton/all';
 
-import defaultConfig from '@proton/eslint-config-proton/all';
-
-export default defineConfig([
-    defaultConfig,
-    {
-        files: ['**/*.test.ts'],
-        rules: {
-            'max-classes-per-file': 'off',
-            'class-methods-use-this': 'off',
-        },
-    },
-    { ignores: ['**/.eslintrc.js', 'assets/sandbox.js', '**/pkg/'] },
-]);
+export default config;
