1d86af3713a1338a9a4491f721f96cc311454c9a
Istvan Barna <istvan.barna@proton.ch>
2025-09-01 14:41:24 +0200

    addressing review comment

--- packages/srp/lib/keys.ts
+++ packages/srp/lib/keys.ts
@@ -11,11 +11,8 @@ export const computeKeyPassword = async (password: string, salt: string) => {
     if (!password || !salt || salt.length !== 24 || password.length < 1) {
         throw new Error('Password and salt required.');
     }
-
     const saltBinary = binaryStringToArray(decodeBase64(salt));
-
     const hash = await bcryptHash(password, BCRYPT_PREFIX + bcryptEncodeBase64(saltBinary, 16));
-
     // Remove bcrypt prefix and salt (first 29 characters)
     return hash.slice(29);
 };
