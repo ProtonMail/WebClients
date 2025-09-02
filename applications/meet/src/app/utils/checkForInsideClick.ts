af0b17d7a02fcb4afdcfcf9c54e82b5a54f46f18
István Barna <istvan.barna@proton.ch>
2025-09-02 13:10:40 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Richard Tetaz <richard.tetaz@proton.ch>

--- applications/meet/src/app/utils/checkForInsideClick.ts
+++ applications/meet/src/app/utils/checkForInsideClick.ts
@@ -1,13 +1,4 @@
 export const checkForInsideClick = (event: React.MouseEvent | React.FocusEvent, targetClassName: string) => {
-    let el = event.relatedTarget as HTMLElement | null;
-    let found = false;
-    while (el) {
-        if (el.classList && el.classList.contains(targetClassName)) {
-            found = true;
-            break;
-        }
-        el = el.parentElement;
-    }
-
-    return found;
+    const element = event.relatedTarget as HTMLElement | null;
+    return element?.closest(`.${targetClassName}`) !== null;
 };
