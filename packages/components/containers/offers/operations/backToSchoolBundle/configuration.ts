00d1a8c0f562f0f46012a9e8fd477ec4041de131
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-09-03 12:59:58 +0000

    Merge branch 'back-to-school' into 'main'
    [DRVWEB-4862] Q3 acquisition campaign episode 1: unblocking the translations
    
    See merge request web/clients!19098

---
+++ packages/components/containers/offers/operations/backToSchoolBundle/configuration.ts
@@ -0,0 +1,46 @@
+import { c } from 'ttag';
+
+import { FeatureCode } from '@proton/features/interface';
+import { COUPON_CODES, CYCLE, PLANS, PLAN_NAMES } from '@proton/payments';
+
+import { BackToSchoolLayout } from '../../components/backToSchool/BackToSchoolLayout';
+import { getBackToSchoolTitle } from '../../helpers/backToSchool';
+import type { OfferConfig } from '../../interface';
+
+export const backToSchoolBundle: OfferConfig = {
+    ID: 'back-to-school-bundle',
+    title: getBackToSchoolTitle,
+    featureCode: FeatureCode.OfferBackToSchoolBundle,
+    autoPopUp: 'one-time',
+    canBeDisabled: true,
+    deals: [
+        {
+            ref: 'proton_sep_25_bundle_web',
+            dealName: PLAN_NAMES[PLANS.BUNDLE],
+            couponCode: COUPON_CODES.SEP25BUNDLESALE,
+            planIDs: {
+                [PLANS.BUNDLE]: 1,
+            },
+            popular: 1,
+            cycle: CYCLE.YEARLY,
+            features: () => [
+                { name: c('q3campaign_2025: Info').t`All premium features of Mail, Pass, Drive, VPN, and Calendar` },
+                { name: c('q3campaign_2025: Info').t`500 GB storage` },
+                { name: c('q3campaign_2025: Info').t`Stronger protection against cyber threats` },
+            ],
+        },
+    ],
+    topButton: {
+        // TODO: appearance, variant
+        shape: 'outline',
+        getCTAContent: () => {
+            // translator: button in the top right corner of the app (outside the modal)
+            return c('q3campaign_2025: Action').t`END-OF-SUMMER SALE`;
+        },
+        icon: 'gift',
+        iconSize: 4,
+        gradient: false,
+        variant: '',
+    },
+    layout: BackToSchoolLayout,
+};
