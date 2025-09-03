75d4a2efb6469af6ce72e2180667b7110694803c
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-09-03 14:26:42 +0000

    Merge branch 'back-to-school-2' into 'main'
    [DRVWEB-4862] Q3 acquisition campaign episode 2: upgrading the layout
    
    See merge request web/clients!19131

--- packages/components/containers/offers/components/backToSchool/BackToSchoolLayout.tsx
+++ packages/components/containers/offers/components/backToSchool/BackToSchoolLayout.tsx
@@ -13,8 +13,8 @@ export function BackToSchoolLayout(props: OfferLayoutProps) {
 
     // TODO example data, use props instead
     const discountPercent = 60;
-    const promoPricePerMonth = getSimplePriceString(props.currency, 3.33);
-    const normalPricePerMonth = getSimplePriceString(props.currency, 9.99);
+    const promoPricePerMonth = getSimplePriceString(props.currency, 330);
+    const normalPricePerMonth = getSimplePriceString(props.currency, 990);
 
     const isToYearlyUpsell = props.offer.ID.includes('-to-yearly');
     const totalSavings = null;
@@ -22,53 +22,68 @@ export function BackToSchoolLayout(props: OfferLayoutProps) {
     const features = props.offer.deals[0].features?.() || [];
 
     return (
-        <div className="flex flex-column">
-            <h1>{props.offer.title?.()}</h1>
+        // TODO "style" attr is temporary
+        <div className="flex flex-column" style={{ width: '480px', height: '800px', border: '1px solid grey' }}>
+            <h1 className="mb-6">{props.offer.title?.()}</h1>
 
-            <span>
-                {/* TODO: why does it say "drive plus 200gb" and not just "drive plus"? */}
-                <span>{props.offer.deals[0].dealName}</span>
-                <br />
-                <span>{
-                    // translator: full sentence is e.g. "Proton VPN Plus for 12 months", "Proton Duo for 12 months"
-                    c('q3campaign_2025: Title').t`for 12 months`
-                }</span>
-            </span>
+            <div className="flex items-center mb-4">
+                <div className="flex-1 flex flex-column">
+                    {/* TODO: why does it say "drive plus 200gb" and not just "drive plus"? */}
+                    <span className="text-4xl">{props.offer.deals[0].dealName}</span>
+                    <span className="text-lg">{
+                        // translator: full sentence is e.g. "Proton VPN Plus for 12 months", "Proton Duo for 12 months"
+                        c('q3campaign_2025: Title').t`for 12 months`
+                    }</span>
+                </div>
 
-            {/* Square badge */}
-            <div>{
-                // translator: in English we have "50%" off, but with other languages, to save space, "-50%" could work better
-                c('q3campaign_2025: Info').jt`${discountPercent}% off`
-            }</div>
+                {/* Square badge */}
+                <div className="grow-0">{
+                    // translator: in English we have "50%" off, but with other languages, to save space, "-50%" could work better
+                    c('q3campaign_2025: Info').jt`${discountPercent}% off`
+                }</div>
+            </div>
 
-            <span>{
-                // translator: current promotion price per month e.g. "$3.33 / month"
-                c('q3campaign_2025: Info').jt`${promoPricePerMonth} /month`
-            }</span>
-            <span>
-                <s>{
-                    // translator: regular price per month e.g. "$9.99 / month" with STRIKETHROUGH
-                    c('q3campaign_2025: Info').jt`${normalPricePerMonth} /month`
-                }</s>
-            </span>
+            <div className="flex flex-column mb-4">
+                <div>
+                    <span className="mr-1" style={{ fontSize: '48px' }}>
+                        {promoPricePerMonth}
+                    </span>
+                    <span className="text-lg">{
+                        // translator: current promotion price per month e.g. "$3.33 / month"; price not part of this string
+                        c('q3campaign_2025: Info').t`/month`
+                    }</span>
+                </div>
+
+                <span className="text-lg">
+                    <s>{
+                        // translator: regular price per month e.g. "$9.99 / month" with STRIKETHROUGH
+                        c('q3campaign_2025: Info').jt`${normalPricePerMonth} /month`
+                    }</s>
+                </span>
+            </div>
 
             {isToYearlyUpsell && (
-                <span>{c('q3campaign_2025: Info').jt`Save ${totalSavings} and keep all your current features!`}</span>
+                <span className="mb-4">{c('q3campaign_2025: Info')
+                    .jt`Save ${totalSavings} and keep all your current features!`}</span>
             )}
 
-            <Button>{c('q3campaign_2025: Action').t`Get the deal`}</Button>
+            <Button color="warning" className="mb-5" style={{ backgroundColor: '#FFD000' }}>{c(
+                'q3campaign_2025: Action'
+            ).t`Get the deal`}</Button>
 
-            <ul>
+            <ul className="mt-0 mb-6">
                 {features.map((feature) => (
                     <li>{feature.name}</li>
                 ))}
             </ul>
 
-            <p>
-                {c('q3campaign_2025: Info')
-                    .t`Discounts are based on standard monthly pricing. Your subscription will renew at the standard annual rate when the billing cycle ends.`}
-            </p>
-            <a href="#">{c('q3campaign_2025: Action').t`Don’t show this offer again.`}</a>
+            <div className="flex flex-column gap-2 items-center text-sm">
+                <span>
+                    {c('q3campaign_2025: Info')
+                        .t`Discounts are based on standard monthly pricing. Your subscription will renew at the standard annual rate when the billing cycle ends.`}
+                </span>
+                <a href="#">{c('q3campaign_2025: Action').t`Don’t show this offer again.`}</a>
+            </div>
 
             {/* TODO: pumpkin image */}
         </div>
