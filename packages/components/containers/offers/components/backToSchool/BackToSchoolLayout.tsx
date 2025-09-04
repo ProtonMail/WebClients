404f6341deca1438fdce65d512b716d1417e3cb4
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-09-04 13:19:01 +0000

    Merge branch 'back-to-school-3' into 'main'
    [DRVWEB-4862] Q3 acquisition campaign episode 3: make the modal pretty
    
    See merge request web/clients!19157

--- packages/components/containers/offers/components/backToSchool/BackToSchoolLayout.tsx
+++ packages/components/containers/offers/components/backToSchool/BackToSchoolLayout.tsx
@@ -1,10 +1,14 @@
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms';
+import Icon from '@proton/components/components/icon/Icon';
 import { getSimplePriceString } from '@proton/components/components/price/helper';
 
 import hasOffer from '../../helpers/hasOffer';
 import type { OfferLayoutProps } from '../../interface';
+import pumpkin from './pumpkin.png';
+
+import './BackToSchoolLayout.scss';
 
 export function BackToSchoolLayout(props: OfferLayoutProps) {
     if (!hasOffer(props)) {
@@ -22,70 +26,83 @@ export function BackToSchoolLayout(props: OfferLayoutProps) {
     const features = props.offer.deals[0].features?.() || [];
 
     return (
-        // TODO "style" attr is temporary
-        <div className="flex flex-column" style={{ width: '480px', height: '800px', border: '1px solid grey' }}>
-            <h1 className="mb-6">{props.offer.title?.()}</h1>
+        <>
+            <div className="backToSchoolModalContent flex-1 flex flex-column">
+                <h1 className="mb-6 text-bold lh100">{props.offer.title?.()}</h1>
 
-            <div className="flex items-center mb-4">
-                <div className="flex-1 flex flex-column">
-                    {/* TODO: why does it say "drive plus 200gb" and not just "drive plus"? */}
-                    <span className="text-4xl">{props.offer.deals[0].dealName}</span>
-                    <span className="text-lg">{
-                        // translator: full sentence is e.g. "Proton VPN Plus for 12 months", "Proton Duo for 12 months"
-                        c('q3campaign_2025: Title').t`for 12 months`
-                    }</span>
+                <div className="flex items-center mb-4">
+                    <div className="flex-1 flex flex-column">
+                        {/* TODO: why does it say "drive plus 200gb" and not just "drive plus"? */}
+                        <span className="text-4xl text-bold">{props.offer.deals[0].dealName}</span>
+                        <span className="text-lg">{
+                            // translator: full sentence is e.g. "Proton VPN Plus for 12 months", "Proton Duo for 12 months"
+                            c('q3campaign_2025: Title').t`for 12 months`
+                        }</span>
+                    </div>
+
+                    {/* Square badge */}
+                    <div className="backToSchoolSquareBadge rounded flex items-center">
+                        <span className="h2 text-bold ml-3">{
+                            // translator: in English we have "50%" off, but with other languages, to save space, "-50%" could work better
+                            c('q3campaign_2025: Info').jt`${discountPercent}% off`
+                        }</span>
+                    </div>
                 </div>
 
-                {/* Square badge */}
-                <div className="grow-0">{
-                    // translator: in English we have "50%" off, but with other languages, to save space, "-50%" could work better
-                    c('q3campaign_2025: Info').jt`${discountPercent}% off`
-                }</div>
-            </div>
+                {/* Prices */}
+                <div className="flex flex-column mb-4">
+                    <div>
+                        <span className="backToSchoolPromoPrice">{promoPricePerMonth}</span>
+                        <span className="text-lg">{
+                            // translator: current promotion price per month e.g. "$3.33 / month"; price not part of this string
+                            c('q3campaign_2025: Info').t`/month`
+                        }</span>
+                    </div>
 
-            <div className="flex flex-column mb-4">
-                <div>
-                    <span className="mr-1" style={{ fontSize: '48px' }}>
-                        {promoPricePerMonth}
+                    <span className="text-lg">
+                        <s>{
+                            // translator: regular price per month e.g. "$9.99 / month" with STRIKETHROUGH
+                            c('q3campaign_2025: Info').jt`${normalPricePerMonth} /month`
+                        }</s>
                     </span>
-                    <span className="text-lg">{
-                        // translator: current promotion price per month e.g. "$3.33 / month"; price not part of this string
-                        c('q3campaign_2025: Info').t`/month`
-                    }</span>
                 </div>
 
-                <span className="text-lg">
-                    <s>{
-                        // translator: regular price per month e.g. "$9.99 / month" with STRIKETHROUGH
-                        c('q3campaign_2025: Info').jt`${normalPricePerMonth} /month`
-                    }</s>
-                </span>
+                {isToYearlyUpsell && (
+                    <span className="mb-4">{c('q3campaign_2025: Info')
+                        .jt`Save ${totalSavings} and keep all your current features!`}</span>
+                )}
+
+                <Button size="large" className="backToSchoolCTA mb-5 text-semibold">{c('q3campaign_2025: Action')
+                    .t`Get the deal`}</Button>
+
+                {/* Features */}
+                <ul className="backToSchoolFeatures mt-0 mb-6">
+                    {features.map((feature) => (
+                        <li key={feature.name} className="rounded">
+                            <Icon name="checkmark" />
+                            <span>{feature.name}</span>
+                        </li>
+                    ))}
+                </ul>
+
+                {/* Disclaimer */}
+                <div className="flex flex-column items-center text-center text-sm">
+                    <span>
+                        {c('q3campaign_2025: Info')
+                            .t`Discounts are based on standard monthly pricing. Your subscription will renew at the standard annual rate when the billing cycle ends.`}
+                    </span>
+
+                    <Button color="weak" shape="underline" className="backToSchoolFeaturesDismiss">{c(
+                        'q3campaign_2025: Action'
+                    ).t`Don’t show this offer again.`}</Button>
+                </div>
             </div>
-
-            {isToYearlyUpsell && (
-                <span className="mb-4">{c('q3campaign_2025: Info')
-                    .jt`Save ${totalSavings} and keep all your current features!`}</span>
-            )}
-
-            <Button color="warning" className="mb-5" style={{ backgroundColor: '#FFD000' }}>{c(
-                'q3campaign_2025: Action'
-            ).t`Get the deal`}</Button>
-
-            <ul className="mt-0 mb-6">
-                {features.map((feature) => (
-                    <li>{feature.name}</li>
-                ))}
-            </ul>
-
-            <div className="flex flex-column gap-2 items-center text-sm">
-                <span>
-                    {c('q3campaign_2025: Info')
-                        .t`Discounts are based on standard monthly pricing. Your subscription will renew at the standard annual rate when the billing cycle ends.`}
-                </span>
-                <a href="#">{c('q3campaign_2025: Action').t`Don’t show this offer again.`}</a>
-            </div>
-
-            {/* TODO: pumpkin image */}
-        </div>
+            {/* Footer: pumpkin */}
+            <img
+                className="grow-0 self-center mt-4"
+                src={pumpkin}
+                alt={c('q3campaign_2025: Label').t`Pumpkin with sunglasses on`}
+            />
+        </>
     );
 }
