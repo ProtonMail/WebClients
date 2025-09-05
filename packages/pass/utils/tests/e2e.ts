274f8c8486215ac81148e8e0781c451d56693d08
Edvin CANDON <edvin.candon@proton.ch>
2025-09-05 09:10:39 +0200

    Lint pass codebase

--- packages/pass/utils/tests/e2e.ts
+++ packages/pass/utils/tests/e2e.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 type E2eMessageType = 'shadowRoot';
 
 /**
@@ -5,9 +6,6 @@ type E2eMessageType = 'shadowRoot';
  * It uses console.debug as playwright is able to listen to it and catch handlers
  * Main use case is to send closed shadow roots pointers as they are not accessible otherwise
  */
-export const sendToE2eTests = async (type: E2eMessageType, ...args: any[]) => {
-    if (E2E_TESTS) {
-        // eslint-disable-next-line no-console
-        console.debug(`E2E.${type}`, ...args);
-    }
+export const sendToE2eTests = (type: E2eMessageType, ...args: any[]) => {
+    if (E2E_TESTS) console.debug(`E2E.${type}`, ...args);
 };
