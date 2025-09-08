3ae846564e7027121b1fde01736710a5463f0030
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-09-08 08:39:14 +0000

    Merge branch 'back-to-school-5' into 'main'
    [DRVWEB-4862] Q3 acquisition campaign episode 5: offer eligibility
    
    See merge request web/clients!19193

--- packages/components/containers/offers/operations/backToSchoolPassPlusToYearly/eligibility.ts
+++ packages/components/containers/offers/operations/backToSchoolPassPlusToYearly/eligibility.ts
@@ -1,6 +1,10 @@
-import type { Subscription } from '@proton/payments';
+import { CYCLE, PLANS, type Subscription } from '@proton/payments';
+import { getAppFromPathnameSafe } from '@proton/shared/lib/apps/slugHelper';
+import { APPS } from '@proton/shared/lib/constants';
 import type { ProtonConfig, UserModel } from '@proton/shared/lib/interfaces';
+import { hasPassLifetime, hasPassViaSimpleLogin } from '@proton/shared/lib/user/helpers';
 
+import OfferSubscription from '../../helpers/offerSubscription';
 import type { OfferConfig } from '../../interface';
 
 interface Props {
@@ -10,10 +14,30 @@ interface Props {
     offerConfig: OfferConfig;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getIsEligible = ({ user, subscription, protonConfig, offerConfig }: Props) => {
-    if (!subscription) {
+export const getIsEligible = ({ user, subscription, protonConfig }: Props) => {
+    if (user.isDelinquent || !user.canPay || user.isFree || subscription?.UpcomingSubscription) {
         return false;
+    }
+
+    if (subscription) {
+        const offerSubscription = new OfferSubscription(subscription);
+        if (offerSubscription.usedBackToSchoolPromo()) {
+            return false;
+        }
+    }
+
+    const parentApp = getAppFromPathnameSafe(window.location.pathname);
+    const hasValidApp =
+        protonConfig.APP_NAME === APPS.PROTONPASS ||
+        (protonConfig.APP_NAME === APPS.PROTONACCOUNT && parentApp === APPS.PROTONPASS);
+    const passSubscription = subscription?.Plans?.find((plan) => plan.Name === PLANS.PASS);
+    if (
+        hasValidApp &&
+        passSubscription?.Cycle === CYCLE.MONTHLY &&
+        !hasPassLifetime(user) &&
+        !hasPassViaSimpleLogin(user)
+    ) {
+        return true;
     }
 
     return false;
