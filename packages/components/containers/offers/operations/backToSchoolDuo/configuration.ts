3edfd6aa01a47560bfd8f3ebf644a5e3c476e1e2
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-09-08 16:26:40 +0000

    Merge branch 'back-to-school-7' into 'main'
    [DRVWEB-4862] Q3 acquisition campaign episode 6: top nav button style
    
    See merge request web/clients!19227

--- packages/components/containers/offers/operations/backToSchoolDuo/configuration.ts
+++ packages/components/containers/offers/operations/backToSchoolDuo/configuration.ts
@@ -30,7 +30,6 @@ export const backToSchoolDuo: OfferConfig = {
         },
     ],
     topButton: {
-        // TODO: appearance, variant
         shape: 'outline',
         getCTAContent: () => {
             // translator: button in the top right corner of the app (outside the modal)
@@ -39,7 +38,6 @@ export const backToSchoolDuo: OfferConfig = {
         icon: 'gift',
         iconSize: 4,
         gradient: false,
-        variant: '',
     },
     layout: BackToSchoolLayout,
 };
