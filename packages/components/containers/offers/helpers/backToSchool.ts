b4ff964ad58d8f60d96b431bf9143dba201a49da
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-09-09 15:17:29 +0000

    Merge branch 'back-to-school-10' into 'main'
    [DRVWEB-4862] Q3 acquisition campaign episode 10: rename feature flags and fix eligibility
    
    See merge request web/clients!19261

--- packages/components/containers/offers/helpers/backToSchool.ts
+++ packages/components/containers/offers/helpers/backToSchool.ts
@@ -1,16 +1,19 @@
 import { c } from 'ttag';
 
-import { PLANS, type Subscription } from '@proton/payments';
+import { CYCLE, PLANS, type Subscription } from '@proton/payments';
 
 export const getBackToSchoolTitle = (): string => {
     return c('q3campaign_2025: Title').t`End-of-summer sale`;
 };
 
-export function userHasSupportedProduct(subscription?: Subscription) {
-    const hasMail = subscription?.Plans?.some((plan) => plan.Name === PLANS.MAIL) ?? false;
+export function whichAppsUserBought(subscription?: Subscription) {
+    const hasMail =
+        subscription?.Plans?.some((plan) => plan.Name === PLANS.MAIL && plan.Cycle === CYCLE.YEARLY) ?? false;
     const hasDrive = subscription?.Plans?.some((plan) => plan.Name === PLANS.DRIVE) ?? false;
-    const hasPass = subscription?.Plans?.some((plan) => plan.Name === PLANS.PASS) ?? false;
-    const hasVPN = subscription?.Plans?.some((plan) => plan.Name === PLANS.VPN2024) ?? false;
+    const hasPass =
+        subscription?.Plans?.some((plan) => plan.Name === PLANS.PASS && plan.Cycle === CYCLE.YEARLY) ?? false;
+    const hasVPN =
+        subscription?.Plans?.some((plan) => plan.Name === PLANS.VPN2024 && plan.Cycle === CYCLE.YEARLY) ?? false;
 
-    return hasMail || hasDrive || hasPass || hasVPN;
+    return { hasMail, hasDrive, hasPass, hasVPN };
 }
