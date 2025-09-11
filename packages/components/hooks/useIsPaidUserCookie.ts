d706a0b27476cc4c90fafa827d4174c3c4c1627d
Jack Crompton <jack.crompton@proton.ch>
2025-09-11 16:18:32 +0200

    Remove unused no-offer cookie
    CP-10174

--- packages/components/hooks/useIsPaidUserCookie.ts
+++ packages/components/hooks/useIsPaidUserCookie.ts
@@ -1,49 +1,26 @@
 import { useEffect } from 'react';
 
-import { useUser } from '@proton/account/user/hooks';
-import { getCookie, setCookie } from '@proton/shared/lib/helpers/cookies';
+import { setCookie } from '@proton/shared/lib/helpers/cookies';
 import { getSecondLevelDomain } from '@proton/shared/lib/helpers/url';
 
 const COOKIE_NAME = 'no-offer';
-const today = new Date();
-const lastDayOfTheYear = new Date(today.getFullYear(), 11, 31, 23, 59, 59);
+
 const cookieDomain = `.${getSecondLevelDomain(window.location.hostname)}`;
 
 /**
- * Set a cookie for non eligible user to BF offer
- * Used by proton.me website to hide BF banner
+ * Cookie is no longer used.
+ * Let's clean it up
  */
 const useIsPaidUserCookie = () => {
-    const [user, loadingUser] = useUser();
-    const loading = loadingUser;
-
     useEffect(() => {
-        if (loading) {
-            return;
-        }
-
-        const cookie = getCookie(COOKIE_NAME);
-        const isPaid = user.isPaid;
-        const shouldSet = isPaid;
-
-        if (shouldSet && cookie !== '1') {
-            setCookie({
-                cookieName: COOKIE_NAME,
-                cookieValue: '1',
-                cookieDomain,
-                expirationDate: lastDayOfTheYear.toUTCString(),
-                path: '/',
-            });
-        } else if (!shouldSet && cookie === '1') {
-            setCookie({
-                cookieName: COOKIE_NAME,
-                cookieValue: undefined,
-                cookieDomain,
-                expirationDate: new Date(0).toUTCString(),
-                path: '/',
-            });
-        }
-    }, [user, loading]);
+        setCookie({
+            cookieName: COOKIE_NAME,
+            cookieValue: undefined,
+            cookieDomain,
+            expirationDate: new Date(0).toUTCString(),
+            path: '/',
+        });
+    }, []);
 };
 
 export default useIsPaidUserCookie;
