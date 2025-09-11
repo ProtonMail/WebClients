fa24b3898ef850c9787b6bc48c9ed7a83f23a7cb
Michal Horejsek <michal.horejsek@proton.ch>
2025-09-11 08:08:20 +0000

    Upgrade to Drive SDK v0.3.1

--- packages/drive/lib/useSrpModule.ts
+++ packages/drive/lib/useSrpModule.ts
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 
 import { useApi } from '@proton/components';
 import { srpGetVerify } from '@proton/shared/lib/srp';
-import { computeKeyPassword } from '@proton/srp/lib';
+import { computeKeyPassword, getSrp as srpModuleGetSrp } from '@proton/srp/lib';
 
 export type SRPVerifier = {
     modulusId: string;
@@ -13,6 +13,30 @@ export type SRPVerifier = {
 
 export const useSrpModule = () => {
     const api = useApi();
+
+    const getSrp = async (
+        version: number,
+        modulus: string,
+        serverEphemeral: string,
+        salt: string,
+        password: string
+    ): Promise<{
+        expectedServerProof: string;
+        clientProof: string;
+        clientEphemeral: string;
+    }> => {
+        return srpModuleGetSrp(
+            {
+                Version: version,
+                Modulus: modulus,
+                ServerEphemeral: serverEphemeral,
+                Salt: salt,
+            },
+            {
+                password,
+            }
+        );
+    };
 
     const getSrpVerifier = async (password: string): Promise<SRPVerifier> => {
         const { Auth } = await srpGetVerify({ api, credentials: { password } });
@@ -26,10 +50,12 @@ export const useSrpModule = () => {
 
     // Ensure the reference is stable across renders. Never update the whole object.
     const srpModule = useRef({
+        getSrp,
         getSrpVerifier,
         computeKeyPassword,
     });
 
+    srpModule.current.getSrp = getSrp;
     srpModule.current.getSrpVerifier = getSrpVerifier;
     srpModule.current.computeKeyPassword = computeKeyPassword;
 
