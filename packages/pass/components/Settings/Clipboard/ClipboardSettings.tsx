6608ab5b2b41b7f098e569b11a3d060a7950180e
Edvin CANDON <edvin.candon@proton.ch>
2025-09-13 20:02:18 +0200

    Fine-tune `ClipboardProvider` context
    - Remove unnused exposed modal/setModal methods
    - Simplify context & naming
    - Remove unused `initializeSetting` in `ClipboardSettings`
    
    [IDTEAM-4760]

--- packages/pass/components/Settings/Clipboard/ClipboardSettings.tsx
+++ packages/pass/components/Settings/Clipboard/ClipboardSettings.tsx
@@ -1,4 +1,4 @@
-import { type FC, useEffect, useMemo } from 'react';
+import { type FC, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 import { c } from 'ttag';
@@ -6,46 +6,34 @@ import { c } from 'ttag';
 import { Option, SelectTwo } from '@proton/components';
 import { useOnClipboardSettingsChange } from '@proton/pass/components/Settings/Clipboard/ClipboardProvider';
 import { selectClipboardTTL } from '@proton/pass/store/selectors';
+import { PASS_APP_NAME } from '@proton/shared/lib/constants';
 
-export const getClipboardTTLOptions = (): [value: number, label: string][] => [
+export const getClipboardTTLOptions = (): [ttl: number, label: string][] => [
     [-1, c('Label').t`Never`],
     [15_000, c('Label').t`15 seconds`],
     [60_000, c('Label').t`1 minute`],
     [120_000, c('Label').t`2 minutes`],
 ];
 
-export const getClipboardTTLDefault = () => getClipboardTTLOptions()[0][0];
-
-type Props = {
-    // Whether or not to initialize the settings value even if there are no changes
-    initializeSetting?: boolean;
-};
-
-export const ClipboardSettings: FC<Props> = ({ initializeSetting = false }) => {
+export const ClipboardSettings: FC = () => {
     const storedValue = useSelector(selectClipboardTTL);
     const onSettingsChange = useOnClipboardSettingsChange();
 
     const options = getClipboardTTLOptions();
-    const defaultValue = getClipboardTTLDefault();
-
-    const value = useMemo(() => options.find(([value]) => value === storedValue)?.[0] ?? defaultValue, [storedValue]);
-
-    useEffect(() => {
-        if (initializeSetting && storedValue === undefined) {
-            void onSettingsChange(defaultValue, true);
-        }
-    }, [initializeSetting, storedValue]);
+    const value = useMemo(() => options.find(([ttl]) => ttl === storedValue)?.[0] ?? -1, [storedValue]);
 
     return (
         <>
             <SelectTwo onValue={onSettingsChange} value={value}>
-                {options.map(([value, label]) => (
-                    <Option key={value} value={value} title={label} />
+                {options.map(([ttl, label]) => (
+                    <Option key={ttl} value={ttl} title={label} />
                 ))}
             </SelectTwo>
             {value !== -1 && (
                 <span className="block color-weak text-sm">
-                    {c('Info').t`Please keep your browser open so the clipboard can be cleared automatically.`}
+                    {DESKTOP_BUILD
+                        ? c('Info').t`Please keep ${PASS_APP_NAME} open so the clipboard can be cleared automatically.`
+                        : c('Info').t`Please keep your browser open so the clipboard can be cleared automatically.`}
                 </span>
             )}
         </>
