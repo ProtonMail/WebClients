4bdf6d0f8ff2aba3350518dabfca57059b100b1e
Edvin CANDON <edvin.candon@proton.ch>
2025-09-14 16:22:56 +0200

    Use extension alarms for clipboard clearing
    - Use `AlarmFactory` to survive service worker restarts
    - Use browser extension alarms for delays ≥1min, setTimeout for <1min delays
    - Add unit-tests for core clipboard service
    - Allow timeoutMs = 0 for immediate clipboard clearing (was previously ignored)
    
    [IDTEAM-4760]

--- packages/pass/lib/clipboard/service.ts
+++ packages/pass/lib/clipboard/service.ts
@@ -1,21 +1,31 @@
 import type { ClipboardApi, ClipboardService } from '@proton/pass/lib/clipboard/types';
-import type { Maybe } from '@proton/pass/types';
+import type { MaybeNull } from '@proton/pass/types';
+import type { AlarmFactory } from '@proton/pass/utils/time/alarm';
+import { createTimeoutAlarm } from '@proton/pass/utils/time/alarm';
 import noop from '@proton/utils/noop';
 
-export const createClipboardService = (clipboard: ClipboardApi): ClipboardService => {
-    let clipboardTimer: Maybe<NodeJS.Timeout>;
+export const createClipboardService = (
+    clipboard: ClipboardApi,
+    alarmFactory: AlarmFactory = createTimeoutAlarm
+): ClipboardService => {
+    let clipboardValue: MaybeNull<string> = null;
+
+    const alarm = alarmFactory('clipboard::clear', async () => {
+        const current = await clipboard.read().catch(noop);
+        if (current === clipboardValue) clipboard.write('').catch(noop);
+        clipboardValue = null;
+    });
 
     return {
         ...clipboard,
         autoClear: (timeoutMs, value) => {
-            if (clipboardTimer !== undefined) clearTimeout(clipboardTimer);
+            void alarm.reset();
+            clipboardValue = null;
 
-            if (timeoutMs <= 0) return;
-
-            clipboardTimer = setTimeout(async () => {
-                const current = await clipboard.read().catch(noop());
-                if (current === value) clipboard.write('').catch(noop);
-            }, timeoutMs);
+            if (timeoutMs >= 0) {
+                clipboardValue = value;
+                void alarm.set(Date.now() + timeoutMs);
+            }
         },
     };
 };
