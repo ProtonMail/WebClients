f7bfffe74de77eb15d061c8b96f1e37d9d903926
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2025-09-16 16:39:19 +0200

    Small code improvements for auto-type
    [IDTEAM-4336]

--- packages/pass/components/Item/Autotype/AutotypeDropdownLogin.tsx
+++ packages/pass/components/Item/Autotype/AutotypeDropdownLogin.tsx
@@ -1,8 +1,8 @@
 import type { FC } from 'react';
 
+import { WithFeatureFlag } from '@proton/pass/components/Core/WithFeatureFlag';
 import { AutotypeDropdown } from '@proton/pass/components/Item/Autotype/AutotypeDropdown';
 import { useAutotypeActions } from '@proton/pass/hooks/autotype/useAutotypeActions';
-import { useFeatureFlag } from '@proton/pass/hooks/useFeatureFlag';
 import type { Item } from '@proton/pass/types';
 import { PassFeature } from '@proton/pass/types/api/features';
 
@@ -10,11 +10,11 @@ type AutotypeDropdownLoginProps = {
     data: Item<'login'>;
 };
 
-export const AutotypeDropdownLogin: FC<AutotypeDropdownLoginProps> = ({ data }) => {
-    const autotypeEnabled = useFeatureFlag(PassFeature.PassDesktopAutotype);
+export const AutotypeDropdownLoginCore: FC<AutotypeDropdownLoginProps> = ({ data }) => {
     const { actions } = useAutotypeActions(data);
-
-    if (!autotypeEnabled || actions.length === 0) return null;
+    if (actions.length === 0) return null;
 
     return <AutotypeDropdown actions={actions} />;
 };
+
+export const AutotypeDropdownLogin = WithFeatureFlag(AutotypeDropdownLoginCore, PassFeature.PassDesktopAutotype);
