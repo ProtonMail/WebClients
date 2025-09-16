9d63b3bee556feb53d528d640b4049d5f6f77508
István Barna <istvan.barna@proton.ch>
2025-09-16 15:03:31 +0000

    MEET-204: Noise filtering

--- applications/meet/src/app/components/AudioSettings/AudioSettings.tsx
+++ applications/meet/src/app/components/AudioSettings/AudioSettings.tsx
@@ -3,7 +3,6 @@ import type { RefObject } from 'react';
 import { useRoomContext } from '@livekit/components-react';
 
 import { useMeetContext } from '../../contexts/MeetContext';
-import { useAudioToggle } from '../../hooks/useAudioToggle';
 import { useDevices } from '../../hooks/useDevices';
 import { supportsSetSinkId } from '../../utils/browser';
 import { AudioSettingsDropdown } from './AudioSettingsDropdown';
@@ -16,9 +15,8 @@ export const AudioSettings = ({ anchorRef }: AudioSettingsProps) => {
     const { microphones, speakers } = useDevices();
     const room = useRoomContext();
 
-    const { audioDeviceId, setAudioDeviceId, audioOutputDeviceId, setAudioOutputDeviceId } = useMeetContext();
-
-    const toggleAudio = useAudioToggle();
+    const { audioDeviceId, setAudioDeviceId, audioOutputDeviceId, setAudioOutputDeviceId, toggleAudio } =
+        useMeetContext();
 
     const handleInputDeviceChange = async (value: string | null) => {
         if (!value) {
