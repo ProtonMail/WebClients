156839aba80cf319c723cc9dd4efa776ffdc61df
Christian Owusu <christian.owusu@proton.ch>
2025-09-16 12:41:15 +0000

    Sheets: Add new Dialogs component

---
+++ applications/docs-editor/src/app/Containers/Spreadsheet/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,89 @@
+import {
+  CellFormatEditor,
+  CellFormatEditorDialog,
+  ConditionalFormatDialog,
+  ConditionalFormatEditor,
+  DataValidationEditor,
+  DataValidationEditorDialog,
+  DeleteSheetConfirmation,
+  NamedRangeEditor,
+  TableEditor,
+} from '@rowsncolumns/spreadsheet-state'
+import type { ProtonSheetsState } from '../../state'
+import { functionDescriptions } from '@rowsncolumns/functions'
+import { SheetSearch } from '@rowsncolumns/spreadsheet'
+import { ChartEditor, ChartEditorDialog } from '@rowsncolumns/charts'
+
+export type DialogsProps = {
+  state: ProtonSheetsState
+}
+
+export function Dialogs({ state }: DialogsProps) {
+  return (
+    <>
+      /* All dialogs below are legacy and will be incrementally replaced */
+      <ChartEditorDialog>
+        <ChartEditor
+          sheetId={state.activeSheetId}
+          chart={state.chartsState.selectedChart}
+          onSubmit={state.chartsState.onUpdateChart}
+        />
+      </ChartEditorDialog>
+      <ConditionalFormatDialog>
+        <ConditionalFormatEditor
+          sheetId={state.activeSheetId}
+          theme={state.theme}
+          conditionalFormats={state.conditionalFormats}
+          functionDescriptions={functionDescriptions}
+          onCreateRule={state.onCreateConditionalFormattingRule}
+          onDeleteRule={state.onDeleteConditionalFormattingRule}
+          onUpdateRule={state.onUpdateConditionalFormattingRule}
+          onPreviewRule={state.onPreviewConditionalFormattingRule}
+        />
+      </ConditionalFormatDialog>
+      <TableEditor sheetId={state.activeSheetId} onSubmit={state.onUpdateTable} theme={state.theme} />
+      <DeleteSheetConfirmation sheetId={state.activeSheetId} onDeleteSheet={state.onDeleteSheet} />
+      <NamedRangeEditor
+        sheetId={state.activeSheetId}
+        onCreateNamedRange={state.onCreateNamedRange}
+        onUpdateNamedRange={state.onUpdateNamedRange}
+      />
+      <SheetSearch
+        isActive={state.searchState.isSearchActive}
+        onSubmit={state.searchState.onSearch}
+        onReset={state.searchState.onResetSearch}
+        onNext={state.searchState.onFocusNextResult}
+        onPrevious={state.searchState.onFocusPreviousResult}
+        disableNext={!state.searchState.hasNextResult}
+        disablePrevious={!state.searchState.hasPreviousResult}
+        currentResult={state.searchState.currentResult}
+        totalResults={state.searchState.totalResults}
+        searchQuery={state.searchState.searchQuery}
+      />
+      <DataValidationEditorDialog>
+        <DataValidationEditor
+          dataValidations={state.dataValidations}
+          sheetId={state.activeSheetId}
+          functionDescriptions={functionDescriptions}
+          onDeleteRules={state.onDeleteDataValidationRules}
+          onDeleteRule={state.onDeleteDataValidationRule}
+          onCreateRule={state.onCreateDataValidationRule}
+          onUpdateRule={state.onUpdateDataValidationRule}
+        />
+      </DataValidationEditorDialog>
+      <CellFormatEditorDialog>
+        <CellFormatEditor
+          sheetId={state.activeSheetId}
+          activeCell={state.activeCell}
+          selections={state.selections}
+          onChangeFormatting={state.onChangeFormatting}
+          cellFormat={state.currentCellFormat}
+          getEffectiveValue={state.getEffectiveValue}
+          onMergeCells={state.onMergeCells}
+          theme={state.theme}
+          onChangeBorder={state.onChangeBorder}
+        />
+      </CellFormatEditorDialog>
+    </>
+  )
+}
