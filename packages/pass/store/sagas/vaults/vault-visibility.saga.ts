eb9b02c5b85bdb5b62903cf0a0fd027e488d0087
Edvin CANDON <edvin.candon@proton.ch>
2025-09-17 12:53:44 +0200

    Fix vault visibility toggle not reflecting in tabs

--- packages/pass/store/sagas/vaults/vault-visibility.saga.ts
+++ packages/pass/store/sagas/vaults/vault-visibility.saga.ts
@@ -1,18 +1,24 @@
-import { select } from 'redux-saga/effects';
+import { select, takeEvery } from 'redux-saga/effects';
 
 import { toggleVisibility } from '@proton/pass/lib/shares/share.requests';
 import { sharesVisibilityEdit } from '@proton/pass/store/actions';
 import type { SharesState } from '@proton/pass/store/reducers';
 import { createRequestSaga } from '@proton/pass/store/request/sagas';
 import { selectShareState } from '@proton/pass/store/selectors';
+import type { RootSagaOptions } from '@proton/pass/store/types';
 import type { Share, ShareType } from '@proton/pass/types';
 import { toMap } from '@proton/shared/lib/helpers/object';
 
-export default createRequestSaga({
-    actions: sharesVisibilityEdit,
-    call: function* ({ sharesToHide, sharesToUnhide }) {
-        const state: SharesState = yield select(selectShareState);
-        const shares: Share<ShareType.Vault>[] = yield toggleVisibility(sharesToHide, sharesToUnhide, state);
-        return toMap(shares, 'shareId');
+export default [
+    createRequestSaga({
+        actions: sharesVisibilityEdit,
+        call: function* ({ sharesToHide, sharesToUnhide }) {
+            const state: SharesState = yield select(selectShareState);
+            const shares: Share<ShareType.Vault>[] = yield toggleVisibility(sharesToHide, sharesToUnhide, state);
+            return toMap(shares, 'shareId');
+        },
+    }),
+    function* (options: RootSagaOptions) {
+        yield takeEvery(sharesVisibilityEdit.success.match, () => options.onItemsUpdated?.());
     },
-});
+] as const;
