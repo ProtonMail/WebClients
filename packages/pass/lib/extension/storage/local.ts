c1b2d303879069863f89c6f84a2c0613954f41d9
Edvin CANDON <edvin.candon@proton.ch>
2025-09-17 08:23:21 +0200

    Make storage operations explicitly `async` to prevent synchronous failures

--- packages/pass/lib/extension/storage/local.ts
+++ packages/pass/lib/extension/storage/local.ts
@@ -9,13 +9,13 @@ import type {
     StorageInterface,
 } from '@proton/pass/types';
 
-const getItems: GetItems = (keys) => browser.storage.local.get(keys);
+const getItems: GetItems = async (keys) => browser.storage.local.get(keys);
 const getItem: GetItem = async (key) => (await getItems([key]))?.[key] ?? null;
-const setItems: SetItems = (items) => browser.storage.local.set(items);
-const setItem: SetItem = (key, value) => setItems({ [key]: value });
-const removeItems: RemoveItems = (keys) => browser.storage.local.remove(keys);
-const removeItem: RemoveItem = (key) => removeItems([key]);
-const clear = (): Promise<void> => browser.storage.local.clear();
+const setItems: SetItems = async (items) => browser.storage.local.set(items);
+const setItem: SetItem = async (key, value) => setItems({ [key]: value });
+const removeItems: RemoveItems = async (keys) => browser.storage.local.remove(keys);
+const removeItem: RemoveItem = async (key) => removeItems([key]);
+const clear = async (): Promise<void> => browser.storage.local.clear();
 
 const browserLocalStorage: StorageInterface = {
     getItems,
