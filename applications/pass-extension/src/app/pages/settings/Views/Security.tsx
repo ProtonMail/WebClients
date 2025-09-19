fff8d7d3152f68f78533c569fe8e6c60a63b59ca
Edvin CANDON <edvin.candon@proton.ch>
2025-09-19 12:08:53 +0200

    Improve permission prompt triggers for clipboard
    [IDTEAM-4760]

--- applications/pass-extension/src/app/pages/settings/Views/Security.tsx
+++ applications/pass-extension/src/app/pages/settings/Views/Security.tsx
@@ -1,11 +1,18 @@
 import type { FC } from 'react';
 
+import { usePermissions } from 'proton-pass-extension/lib/hooks/usePermissions';
+import { CLIPBOARD_PERMISSIONS } from 'proton-pass-extension/lib/utils/permissions';
+
 import { Clipboard } from '@proton/pass/components/Settings/Clipboard';
 import { ExtraPassword } from '@proton/pass/components/Settings/ExtraPassword';
 import { LockSettings } from '@proton/pass/components/Settings/LockSettings';
 
-export const Security: FC = () => [
-    <LockSettings key="lock" />,
-    <ExtraPassword key="extra-pwd" />,
-    <Clipboard key="clipboard" />,
-];
+export const Security: FC = () => {
+    const { enabled, request } = usePermissions(CLIPBOARD_PERMISSIONS);
+
+    return [
+        <LockSettings key="lock" />,
+        <ExtraPassword key="extra-pwd" />,
+        <Clipboard key="clipboard" disabled={!enabled} activate={request} />,
+    ];
+};
--- packages/pass/components/Settings/Clipboard/ClipboardSettingsModal.tsx
+++ packages/pass/components/Settings/Clipboard/ClipboardSettingsModal.tsx
@@ -6,6 +6,7 @@ import { Button } from '@proton/atoms';
 import { Checkbox, ModalTwoContent, ModalTwoFooter, ModalTwoHeader } from '@proton/components';
 import { PassModal } from '@proton/pass/components/Layout/Modal/PassModal';
 import { useSetClipboardTTL } from '@proton/pass/components/Settings/Clipboard/ClipboardProvider';
+import { getDefaultClipboardTTLOption } from '@proton/pass/components/Settings/Clipboard/ClipboardSettings';
 
 type Props = { onClose: (overrideClipboardTTL: number) => void };
 
@@ -13,17 +14,27 @@ export const ClipboardSettingsModal: FC<Props> = ({ onClose }) => {
     const [notAgain, setNotAgain] = useState(false);
     const setClipboardTTL = useSetClipboardTTL();
 
-    const handleClose = async (overrideClipboardTTL: number) => {
-        if (notAgain) setClipboardTTL(overrideClipboardTTL);
+    const handleClose = (overrideClipboardTTL: number) => {
+        /** set the clipboard TTL on close if user discards the modal
+         * permanently or agrees on setting the default clipboard TTL. */
+        if (notAgain || overrideClipboardTTL > 0) setClipboardTTL(overrideClipboardTTL);
         onClose(overrideClipboardTTL);
     };
 
+    const timeoutDurationHumanReadable = getDefaultClipboardTTLOption()[1];
+
     return (
-        <PassModal onClose={() => onClose(-1)} open>
+        <PassModal onClose={() => onClose(-1)} size="small" open>
             <ModalTwoHeader title={c('Title').t`Clear clipboard`} />
             <ModalTwoContent>
-                <p>{c('Confirm').t`Do you want your clipboard automatically cleared after 2 minutes?`}</p>
-                <p className="color-weak">{c('Permission').t`Will require a permission from your browser`}</p>
+                <p>{
+                    // translator: `timeoutDurationHumanReadable` may be 15 seconds, 1 minute or 2 minutes
+                    c('Confirm')
+                        .t`Do you want your clipboard automatically cleared after ${timeoutDurationHumanReadable}?`
+                }</p>
+                {!DESKTOP_BUILD && (
+                    <p className="color-weak">{c('Permission').t`Will require a permission from your browser`}</p>
+                )}
                 <p>
                     <Checkbox checked={notAgain} onChange={() => setNotAgain(!notAgain)}>
                         {c('Confirm').t`Do not ask me again`}
