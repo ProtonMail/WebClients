0c9d09a776721500ae89d887823c14e9db77b1dc
Kévin Ta <kevin.ta@proton.ch>
2025-09-19 17:17:01 +0200

    MEET-330 Return better error message if WebRTC is unavailable

--- applications/meet/src/app/components/WasmUnsupportedError.tsx
+++ applications/meet/src/app/components/WasmUnsupportedError.tsx
@@ -4,7 +4,7 @@ import { MEET_APP_NAME } from '@proton/shared/lib/constants';
 
 export const WasmUnsupportedError = () => {
     return (
-        <div className="unsupported-wasm-container">
+        <div className="unsupported-browser-container">
             <span>{c('Meet')
                 .t`Your browser or device software does not support ${MEET_APP_NAME}. ${MEET_APP_NAME} requires WebAssembly (Wasm) to run. Please update your browser or device to a version that supports Wasm, or try using a different browser.`}</span>
         </div>
