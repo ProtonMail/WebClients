68f8be19aac44ad7c663b5fdd43725750b905220
Nico Hoffmann <nicolas.hoffmann@proton.ch>
2025-09-22 14:50:01 +0000

    Improved accessibility of loading for blind people

--- applications/meet/src/app/components/JoiningRoomLoader.tsx
+++ applications/meet/src/app/components/JoiningRoomLoader.tsx
@@ -17,7 +17,7 @@ export const JoiningRoomLoader = ({ participantCount, participantsLoaded }: Join
             heading: (participantCount) =>
                 !participantCount
                     ? c('Title').t`Starting MLS as the first participant...`
-                    : c('Title').t`Negotiating new MLS key with ${participantCount} other participant(s)....`,
+                    : c('Title').t`Negotiating new MLS key with ${participantCount} other participant(s)…`,
             description: !participantCount
                 ? c('Description').t`Generating new encryption key`
                 : c('Description').t`Rotating encryption key`,
@@ -28,13 +28,15 @@ export const JoiningRoomLoader = ({ participantCount, participantsLoaded }: Join
 
     return (
         <div
-            className="flex flex-column flex-nowrap items-center justify-center md:justify-start  md:h-custom md:w-custom pt-6"
+            className="flex flex-column flex-nowrap items-center justify-center md:justify-start md:h-custom md:w-custom pt-6"
             style={{ '--md-w-custom': '22.625rem', '--md-h-custom': '24rem' }}
         >
-            <h2 className="mt-6 mb-4 text-center text-semibold">
+            <h2 className="mt-6 mb-4 text-center text-semibold" aria-live="polite" aria-atomic="true">
                 {joiningRoomTitles[titleIndex].heading(participantCount)}
             </h2>
-            <div className="color-weak mb-8 text-center">{joiningRoomTitles[titleIndex].description}</div>
+            <div className="color-weak mb-8 text-center" aria-live="polite" aria-atomic="true">
+                {joiningRoomTitles[titleIndex].description}
+            </div>
             <CircleLoader
                 className="color-primary w-custom h-custom"
                 style={{ '--w-custom': '5.3rem', '--h-custom': '5.3rem', '--stroke-width': 1.3 }}
