e8c5c2bd1418f0a2a7147c7ea333be98555b5361
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-09-24 15:39:28 +0000

    Notifications settings

--- applications/mail/src/app/components/categoryView/editCategories/ModalEditCategories.tsx
+++ applications/mail/src/app/components/categoryView/editCategories/ModalEditCategories.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 import { c } from 'ttag';
 
@@ -6,7 +6,8 @@ import { Button } from '@proton/atoms';
 import type { ModalProps } from '@proton/components';
 import { ModalContent, ModalTwo, ModalTwoFooter, useApi } from '@proton/components';
 import useLoading from '@proton/hooks/useLoading';
-import type { CategoryTab } from '@proton/mail';
+import { type CategoryTab, categoriesActions } from '@proton/mail';
+import { useDispatch } from '@proton/redux-shared-store/sharedProvider';
 import { updateLabel } from '@proton/shared/lib/api/labels';
 import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
 import { LABEL_IDS_TO_HUMAN } from '@proton/shared/lib/mail/constants';
@@ -22,7 +23,7 @@ export const ModalEditCategories = ({ onDisableAll, ...rest }: Props) => {
     const api = useApi();
     const { categoriesTabs, categoriesStore } = useCategoriesView();
 
-    const initialCheckedCategoriesIdsRef = useRef<string[] | undefined>(undefined);
+    const dispatch = useDispatch();
 
     const [categoriesListState, setCategoriesListState] = useState<CategoryTab[]>(
         categoriesTabs?.filter((category) => category.id !== MAILBOX_LABEL_IDS.CATEGORY_DEFAULT) || []
@@ -30,28 +31,29 @@ export const ModalEditCategories = ({ onDisableAll, ...rest }: Props) => {
 
     const [loading, withLoading] = useLoading(false);
 
-    // This is used to keep track of the initial state of the checked categories
-    if (!initialCheckedCategoriesIdsRef.current) {
-        initialCheckedCategoriesIdsRef.current = categoriesListState
-            .filter((category) => category.checked)
-            .map((category) => category.id);
-    }
+    const handleSaveCategories = async () => {
+        if (categoriesListState.every((cat) => !cat.checked)) {
+            onDisableAll();
+            return;
+        }
 
-    const toggleCategories = async () => {
+        let shouldReload = false;
         const promises: Promise<any>[] = [];
 
-        const initialCategories = initialCheckedCategoriesIdsRef.current || [];
-
+        // We loop over all categories to know what changed and update them accordingly
         categoriesListState.forEach((category) => {
-            const isNowChecked = category.checked && !initialCategories.includes(category.id);
-            const isNowUnchecked = !category.checked && initialCategories.includes(category.id);
+            const initialCategory = categoriesTabs.find((cat) => cat.id === category.id);
+            const categoryFromStore = categoriesStore?.find((cat) => cat.ID === category.id);
+            if (!categoryFromStore) {
+                return;
+            }
 
-            if (isNowChecked || isNowUnchecked) {
-                const categoryFromStore = categoriesStore?.find((cat) => cat.ID === category.id);
-                if (!categoryFromStore) {
-                    return;
-                }
+            const checkChanged = initialCategory?.checked !== category.checked;
+            const notifyChanged = initialCategory?.notify !== category.notify;
 
+            shouldReload = checkChanged;
+
+            if (checkChanged || notifyChanged) {
                 promises.push(
                     api(
                         // TODO use new system label endpoint and remove Name / Color once created
@@ -59,40 +61,32 @@ export const ModalEditCategories = ({ onDisableAll, ...rest }: Props) => {
                             Name: categoryFromStore.Name,
                             Color: categoryFromStore.Color,
                             Display: category.checked ? 1 : 0,
+                            Notify: category.notify ? 1 : 0,
                         })
                     )
+                );
+
+                dispatch(
+                    categoriesActions.upsertCategory({
+                        ...categoryFromStore,
+                        Display: category.checked ? 1 : 0,
+                        Notify: category.notify ? 1 : 0,
+                    })
                 );
             }
         });
 
         await withLoading(Promise.all(promises));
-    };
 
-    const handleSaveCategories = async () => {
-        if (categoriesListState.every((cat) => !cat.checked)) {
-            onDisableAll();
-            return;
-        }
-
-        const currentChecked = categoriesListState
-            .filter((category) => category.checked)
-            .map((category) => category.id);
-
-        const areSameSize = currentChecked.length === initialCheckedCategoriesIdsRef.current?.length;
-        const areSameContent = currentChecked.every((id) => initialCheckedCategoriesIdsRef.current?.includes(id));
-
-        // We reload if the user made any change in the list of categories
-        if (currentChecked && (!areSameSize || !areSameContent)) {
-            await toggleCategories();
+        if (shouldReload) {
             window.location.replace(`/${LABEL_IDS_TO_HUMAN[MAILBOX_LABEL_IDS.CATEGORY_DEFAULT]}`);
+        } else {
+            rest.onClose?.();
         }
     };
 
-    const handleCategoryToggle = async (category: CategoryTab) => {
-        const newState = categoriesListState.map((cat) =>
-            cat.id === category.id ? { ...cat, checked: !cat.checked } : cat
-        );
-
+    const handleCategoryUpdate = (newCategory: CategoryTab) => {
+        const newState = categoriesListState.map((cat) => (cat.id === newCategory.id ? newCategory : cat));
         setCategoriesListState(newState);
     };
 
@@ -104,7 +98,12 @@ export const ModalEditCategories = ({ onDisableAll, ...rest }: Props) => {
                     .t`Select the categories to include, and enable notifications to be alerted about new emails in a category.`}</p>
                 <EditCategoriesList
                     categoriesToDisplay={categoriesListState}
-                    handleCategoryChange={handleCategoryToggle}
+                    handleCategoryCheckChange={(category) =>
+                        handleCategoryUpdate({ ...category, checked: !category.checked })
+                    }
+                    handleCategoryNotifyChange={(category) =>
+                        handleCategoryUpdate({ ...category, notify: !category.notify })
+                    }
                 />
             </ModalContent>
             <ModalTwoFooter>
