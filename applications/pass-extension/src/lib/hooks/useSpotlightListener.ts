fd80f2fa3f2469a4a1a24b9589e414b5fcb765c1
Edvin CANDON <edvin.candon@proton.ch>
2025-09-28 09:49:39 +0200

    Refactor `PERMISSIONS_REQUIRED` spotlight message triggers
    - Move definition to extension specific code
    - Remove unnecessary `promptForPermissions` on `PassCoreProvider`
    - This allows preserving proper error messaging on grant errors

--- applications/pass-extension/src/lib/hooks/useSpotlightListener.ts
+++ applications/pass-extension/src/lib/hooks/useSpotlightListener.ts
@@ -1,25 +1,54 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 import { useExtensionContext } from 'proton-pass-extension/lib/components/Extension/ExtensionSetup';
+import { useRequestHostPermissions } from 'proton-pass-extension/lib/hooks/useHostPermissions';
 import { popupMessage, sendMessage } from 'proton-pass-extension/lib/message/send-message';
 import { matchExtensionMessage } from 'proton-pass-extension/lib/message/utils';
 import { WorkerMessageType } from 'proton-pass-extension/types/messages';
+import { c } from 'ttag';
 
 import { usePassCore } from '@proton/pass/components/Core/PassCoreProvider';
+import { SubTheme } from '@proton/pass/components/Layout/Theme/types';
+import type { SpotlightMessageDefinition } from '@proton/pass/components/Spotlight/SpotlightContent';
 import { useSpotlight } from '@proton/pass/components/Spotlight/SpotlightProvider';
 import { useSpotlightMessages } from '@proton/pass/hooks/useSpotlightMessages';
 import { selectCreatedItemsCount } from '@proton/pass/store/selectors';
 import { SpotlightMessage } from '@proton/pass/types';
+import { PASS_APP_NAME } from '@proton/shared/lib/constants';
 import { wait } from '@proton/shared/lib/helpers/promise';
 import noop from '@proton/utils/noop';
+
+const useExtensionSpotlightMessages = () => {
+    const requestHostPermissions = useRequestHostPermissions();
+    return useMemo(
+        (): SpotlightMessageDefinition[] => [
+            {
+                type: SpotlightMessage.PERMISSIONS_REQUIRED,
+                mode: 'default',
+                id: 'permissions',
+                title: c('Title').t`Grant permissions`,
+                message: c('Info')
+                    .t`In order to get the best experience out of ${PASS_APP_NAME}, please grant the necessary extension permissions`,
+                className: SubTheme.ORANGE,
+                action: {
+                    label: c('Label').t`Grant`,
+                    type: 'button',
+                    onClick: () => requestHostPermissions(),
+                },
+            },
+        ],
+        [requestHostPermissions]
+    );
+};
 
 export const useSpotlightListener = () => {
     const core = usePassCore();
     const { setSpotlight, setPendingShareAccess } = useSpotlight();
     const { port } = useExtensionContext();
     const createdItemsCount = useSelector(selectCreatedItemsCount);
-    const definitions = useSpotlightMessages();
+    const extraDefinitions = useExtensionSpotlightMessages();
+    const definitions = useSpotlightMessages(extraDefinitions);
 
     useEffect(() => {
         const handleMessage = (message: unknown) => {
