d2b3ac9fdc22444b8f047e19632521f3219bdb81
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-09-29 12:28:20 +0000

    Show counters on categories tabs

--- applications/mail/src/app/components/categoryView/categoriesTabs/Tab.tsx
+++ applications/mail/src/app/components/categoryView/categoriesTabs/Tab.tsx
@@ -1,28 +1,29 @@
 import { NavLink } from 'react-router-dom';
 
 import { clsx } from 'clsx';
+import { c, msgid } from 'ttag';
 
 import { Icon } from '@proton/components';
-import type { IconName } from '@proton/icons';
-import type { CATEGORIES_COLOR_SHADES } from '@proton/mail';
-import { type CategoryLabelID, MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
+import type { CategoryTab } from '@proton/mail';
+import { useMailSettings } from '@proton/mail/store/mailSettings/hooks';
+import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
 import { LABEL_IDS_TO_HUMAN } from '@proton/shared/lib/mail/constants';
+import { VIEW_MODE } from '@proton/shared/lib/mail/mailSettings';
 
 import { getLabelFromCategoryId } from '../categoriesStringHelpers';
 import { TabState } from './tabsInterface';
 
 interface Props {
-    id: CategoryLabelID;
-    count?: number;
-    icon: IconName;
-    colorShade: CATEGORIES_COLOR_SHADES;
+    category: CategoryTab;
     tabState: TabState;
+    count: number;
 }
 
-export const Tab = ({ id, count, icon, tabState, colorShade }: Props) => {
+export const Tab = ({ category, count, tabState }: Props) => {
+    const [mailSettings] = useMailSettings();
     return (
         <NavLink
-            to={LABEL_IDS_TO_HUMAN[id] || LABEL_IDS_TO_HUMAN[MAILBOX_LABEL_IDS.CATEGORY_DEFAULT]}
+            to={LABEL_IDS_TO_HUMAN[category.id] || LABEL_IDS_TO_HUMAN[MAILBOX_LABEL_IDS.CATEGORY_DEFAULT]}
             className={clsx(
                 'tab-container flex flex-nowrap items-center text-no-decoration color-hint hover:mail-category-color',
                 tabState === TabState.ACTIVE &&
@@ -33,23 +34,37 @@ export const Tab = ({ id, count, icon, tabState, colorShade }: Props) => {
             )}
             role="tab"
             aria-selected={tabState === TabState.ACTIVE}
-            aria-label={getLabelFromCategoryId(id)}
-            data-testid={`category-tab-${id}`}
-            data-color={colorShade}
+            aria-label={getLabelFromCategoryId(category.id)}
+            data-testid={`category-tab-${category.id}`}
+            data-color={category.colorShade}
         >
-            <Icon className={clsx('shrink-0', tabState === TabState.ACTIVE && 'mail-category-color')} name={icon} />
+            <Icon
+                className={clsx('shrink-0', tabState === TabState.ACTIVE && 'mail-category-color')}
+                name={category.icon}
+            />
             <span
-                title={getLabelFromCategoryId(id)}
+                title={getLabelFromCategoryId(category.id)}
                 className={clsx('tag-label tag-label-text', tabState === TabState.ACTIVE ? 'color-norm' : 'color-weak')}
             >
-                {getLabelFromCategoryId(id)}
+                {getLabelFromCategoryId(category.id)}
             </span>
-            {/* TODO: clarify how the count is supposed to work */}
-            {count && tabState === TabState.ACTIVE && (
-                <span className={clsx('tag-count px-1.5 py-0.5 text-sm mail-category-color mail-category-count-bg')}>
+
+            {category.notify && count > 0 && tabState === TabState.ACTIVE ? (
+                <span
+                    aria-label={
+                        mailSettings?.ViewMode === VIEW_MODE.GROUP
+                            ? c('Label').ngettext(
+                                  msgid`${count} unread conversation`,
+                                  `${count} unread conversations`,
+                                  count
+                              )
+                            : c('Label').ngettext(msgid`${count} unread message`, `${count} unread messages`, count)
+                    }
+                    className="tag-count px-1.5 py-0.5 text-sm mail-category-color mail-category-count-bg"
+                >
                     {count}
                 </span>
-            )}
+            ) : null}
         </NavLink>
     );
 };
