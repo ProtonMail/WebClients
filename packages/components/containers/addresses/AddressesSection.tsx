e1cc91a9e36fd27d546ed85afa58a6552c097dcd
Romain Sanchez <romain.sanchez@proton.ch>
2025-09-30 10:38:27 +0000

    Prevent reconnecting BYOE addresses when limit has been reached

--- packages/components/containers/addresses/AddressesSection.tsx
+++ packages/components/containers/addresses/AddressesSection.tsx
@@ -2,16 +2,14 @@ import { useOrganization } from '@proton/account/organization/hooks';
 import useBYOEFeatureStatus from '@proton/activation/src/hooks/useBYOEFeatureStatus';
 import Loader from '@proton/components/components/loader/Loader';
 import SettingsSectionWide from '@proton/components/containers/account/SettingsSectionWide';
-import type { APP_NAMES } from '@proton/shared/lib/constants';
 
 import Addresses from './Addresses';
 
 interface Props {
     isOnlySelf?: boolean;
-    app?: APP_NAMES;
 }
 
-const AddressesSection = ({ isOnlySelf, app }: Props) => {
+const AddressesSection = ({ isOnlySelf }: Props) => {
     const [organization, loadingOrganization] = useOrganization();
     const hasAccessToBYOE = useBYOEFeatureStatus();
 
@@ -20,12 +18,7 @@ const AddressesSection = ({ isOnlySelf, app }: Props) => {
             {!organization || loadingOrganization ? (
                 <Loader />
             ) : (
-                <Addresses
-                    isOnlySelf={isOnlySelf}
-                    organization={organization}
-                    hasAccessToBYOE={hasAccessToBYOE}
-                    app={app}
-                />
+                <Addresses isOnlySelf={isOnlySelf} organization={organization} hasAccessToBYOE={hasAccessToBYOE} />
             )}
         </SettingsSectionWide>
     );
