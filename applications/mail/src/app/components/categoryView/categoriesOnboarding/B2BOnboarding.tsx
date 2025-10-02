ac4735e2db5da22299465becf57bc275e8595d28
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-10-02 09:49:46 +0200

    Add B2B onboarding buttons logic

--- applications/mail/src/app/components/categoryView/categoriesOnboarding/B2BOnboarding.tsx
+++ applications/mail/src/app/components/categoryView/categoriesOnboarding/B2BOnboarding.tsx
@@ -1,4 +1,8 @@
-// Placeholder for B2BOnboarding component
+import { useModalState } from '@proton/components';
+
+import { B2BOnboardingModal } from './B2BOnboardingModal';
+
 export const B2BOnboarding = () => {
-    return null;
+    const [modalProps, , renderModal] = useModalState({ open: true });
+    return renderModal ? <B2BOnboardingModal {...modalProps} /> : null;
 };
--- applications/mail/src/app/components/categoryView/categoriesOnboarding/B2BOnboardingModal.tsx
+++ applications/mail/src/app/components/categoryView/categoriesOnboarding/B2BOnboardingModal.tsx
@@ -1,14 +1,35 @@
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms';
-import { Icon, type ModalProps, ModalTwo, ModalTwoContent, ModalTwoHeader } from '@proton/components';
-import { MAIL_APP_NAME } from '@proton/shared/lib/constants';
+import { Icon, type ModalProps, ModalTwo, ModalTwoContent, ModalTwoHeader, useApi } from '@proton/components';
+import { FeatureCode } from '@proton/features/interface';
+import useFeature from '@proton/features/useFeature';
+import useLoading from '@proton/hooks/useLoading';
+import { updateMailCategoryView } from '@proton/shared/lib/api/mailSettings';
+import { MAILBOX_LABEL_IDS, MAIL_APP_NAME } from '@proton/shared/lib/constants';
+import { LABEL_IDS_TO_HUMAN } from '@proton/shared/lib/mail/constants';
 
 import onboardingImage from './b2bOnboardingCategories.svg';
 
 import './B2BOnboardingModal.scss';
 
 export const B2BOnboardingModal = (props: ModalProps) => {
+    const api = useApi();
+    const { update } = useFeature(FeatureCode.CategoryViewB2BOnboardingView);
+
+    const [loading, withLoading] = useLoading();
+
+    const handleClick = async (enableCategories: boolean) => {
+        const promises = [api(updateMailCategoryView(enableCategories)), update(true)];
+
+        await Promise.all(promises);
+        props.onClose?.();
+
+        if (enableCategories) {
+            window.location.replace(`/${LABEL_IDS_TO_HUMAN[MAILBOX_LABEL_IDS.INBOX]}`);
+        }
+    };
+
     return (
         <ModalTwo {...props} size="small">
             <ModalTwoHeader
@@ -23,10 +44,17 @@ export const B2BOnboardingModal = (props: ModalProps) => {
                         .t`With new email categories, only important messages land in your primary inbox. Everything else is organized into categories.`}
                 </p>
                 <div className="flex justify-center mb-4">
-                    <Button color="norm" className="mb-2" fullWidth>
+                    <Button
+                        disabled={loading}
+                        color="norm"
+                        className="mb-2"
+                        fullWidth
+                        onClick={() => withLoading(handleClick(true))}
+                    >
                         {c('Action').t`Try categories`}
                     </Button>
-                    <Button fullWidth>{c('Action').t`Continue without categories`}</Button>
+                    <Button disabled={loading} fullWidth onClick={() => withLoading(handleClick(false))}>{c('Action')
+                        .t`Continue without categories`}</Button>
                 </div>
                 <div className="flex justify-center">
                     <Icon name="lock-filled" className="color-hint" />
