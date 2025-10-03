6f5945dd4e052dad0bf2e66d948c085b9bb16a2e
Elena Atencio <elena.atencio@proton.ch>
2025-10-03 10:26:56 +0000

    Update lumo theme select to new designs

---
+++ applications/lumo/src/app/ui/components/LumoThemeButton/LumoThemeCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { c } from 'ttag';
+
+import { Button } from '@proton/atoms';
+import clsx from '@proton/utils/clsx';
+
+export type LumoThemeCardProps = {
+    src: string;
+    label: string;
+    value: string | number;
+};
+
+type Props = LumoThemeCardProps & {
+    selected: boolean;
+    onChange: (value: string | number) => void;
+};
+
+const LumoThemeCard = ({ src, label, selected, onChange, value }: Props) => {
+    return (
+        <div className="flex flex-nowrap flex-column gap-1 items-center">
+            <Button
+                className={clsx('lumo-theme-card-button rounded', selected && 'is-active pointer-events-none')}
+                onClick={() => onChange(value)}
+                aria-label={c('Action').t`Use ${label} theme`}
+            >
+                <img src={src} alt="" />
+            </Button>
+            <span>{label}</span>
+        </div>
+    );
+};
+
+export default LumoThemeCard;
