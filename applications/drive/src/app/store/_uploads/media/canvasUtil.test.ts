557f8e177b8611ef10ea7f6ba57ab8b72d37b117
Lucien Peslier <lucien.peslier@proton.ch>
2025-10-06 07:04:14 +0000

    Move the iOS/Safari detection out of the worker

--- applications/drive/src/app/store/_uploads/media/canvasUtil.test.ts
+++ applications/drive/src/app/store/_uploads/media/canvasUtil.test.ts
@@ -1,4 +1,4 @@
-import { HD_THUMBNAIL_MAX_SIZE, SupportedMimeTypes, THUMBNAIL_MAX_SIZE } from '@proton/shared/lib/drive/constants';
+import { HD_THUMBNAIL_MAX_SIZE, THUMBNAIL_MAX_SIZE } from '@proton/shared/lib/drive/constants';
 
 import { canvasToThumbnail } from './canvasUtil';
 import { ThumbnailType } from './interface';
@@ -50,67 +50,5 @@ describe('canvasToThumbnail', () => {
         });
 
         await expect(canvasToThumbnail(canvas)).rejects.toThrow('Cannot create small enough thumbnail');
-    });
-
-    describe('Safari/iOS WebP fallback', () => {
-        beforeEach(() => {
-            global.HTMLCanvasElement.prototype.toBlob = jest.fn((callback) => {
-                callback(new Blob(['x'.repeat(THUMBNAIL_MAX_SIZE * 0.8)]));
-            });
-        });
-
-        afterEach(() => {
-            mockIsSafari.mockReturnValue(false);
-            mockIsIos.mockReturnValue(false);
-        });
-
-        it('should use JPEG instead of WebP on Safari when WebP is requested', async () => {
-            mockIsSafari.mockReturnValue(true);
-
-            await canvasToThumbnail(canvas, ThumbnailType.PREVIEW, SupportedMimeTypes.webp);
-
-            expect(global.HTMLCanvasElement.prototype.toBlob).toHaveBeenCalledWith(
-                expect.any(Function),
-                SupportedMimeTypes.jpg,
-                expect.any(Number)
-            );
-        });
-
-        it('should use JPEG instead of WebP on iOS when WebP is requested', async () => {
-            mockIsIos.mockReturnValue(true);
-
-            await canvasToThumbnail(canvas, ThumbnailType.PREVIEW, SupportedMimeTypes.webp);
-
-            expect(global.HTMLCanvasElement.prototype.toBlob).toHaveBeenCalledWith(
-                expect.any(Function),
-                SupportedMimeTypes.jpg,
-                expect.any(Number)
-            );
-        });
-
-        it('should keep JPEG as JPEG on Safari/iOS', async () => {
-            mockIsSafari.mockReturnValue(true);
-
-            await canvasToThumbnail(canvas, ThumbnailType.PREVIEW, SupportedMimeTypes.jpg);
-
-            expect(global.HTMLCanvasElement.prototype.toBlob).toHaveBeenCalledWith(
-                expect.any(Function),
-                SupportedMimeTypes.jpg,
-                expect.any(Number)
-            );
-        });
-
-        it('should use WebP on non-Safari/iOS browsers when WebP is requested', async () => {
-            mockIsSafari.mockReturnValue(false);
-            mockIsIos.mockReturnValue(false);
-
-            await canvasToThumbnail(canvas, ThumbnailType.PREVIEW, SupportedMimeTypes.webp);
-
-            expect(global.HTMLCanvasElement.prototype.toBlob).toHaveBeenCalledWith(
-                expect.any(Function),
-                SupportedMimeTypes.webp,
-                expect.any(Number)
-            );
-        });
     });
 });
