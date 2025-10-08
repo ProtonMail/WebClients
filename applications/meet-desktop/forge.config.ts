eeaa5f2a6cd8821311f04c79552bb00e79a23d7e
István Barna <istvan.barna@proton.ch>
2025-10-08 10:18:54 +0200

    removing unused code

--- applications/meet-desktop/forge.config.ts
+++ applications/meet-desktop/forge.config.ts
@@ -141,7 +141,6 @@ const config: ForgeConfig = {
                 entryPoints: [
                     {
                         html: "./src/index.html",
-                        js: "./src/renderer.ts",
                         name: "main_window",
                         preload: {
                             js: "./src/preload.ts",
