3f1fde190894a929ed309f05048928c6663b1b22
István Barna <istvan.barna@proton.ch>
2025-10-08 10:54:58 +0200

    refactoring

--- applications/meet-desktop/forge.config.ts
+++ applications/meet-desktop/forge.config.ts
@@ -141,6 +141,7 @@ const config: ForgeConfig = {
                 entryPoints: [
                     {
                         html: "./src/index.html",
+                        js: "./src/renderer.ts",
                         name: "main_window",
                         preload: {
                             js: "./src/preload.ts",
