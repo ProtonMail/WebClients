a4c4f27ab9c180e442bf347f18ea19a805359fac
Edvin CANDON <edvin.candon@proton.ch>
2025-10-08 13:44:45 +0200

    Add @proton/types for ambient declarations

--- packages/types/assets.d.ts
+++ packages/types/assets.d.ts
@@ -1,25 +1,16 @@
 declare module '*.svg';
-
 declare module '*.jpg';
 declare module '*.png';
-
 declare module '*.webm';
 declare module '*.mp4';
-
 declare module '*.md';
+declare module '*.raw.scss';
+declare module '*.csv';
+declare module '*.webp';
 
 // TODO: Import from @proton/shared
 declare module 'ical.js';
-
 declare module 'pm-srp';
-
 declare module 'is-valid-domain';
-
 // Used to replace Node bundled punycode library which is now deprecated
 declare module 'punycode.js';
-
-declare module '*.raw.scss';
-
-declare module '*.csv';
-
-declare module '*.webp';
