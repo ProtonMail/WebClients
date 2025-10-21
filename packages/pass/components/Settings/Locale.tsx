72b8db18f7862ebd0a83bae8caad6e7c414a868a
Edvin CANDON <edvin.candon@proton.ch>
2025-10-21 12:08:28 +0200

    Move in-app notifications revalidation to settings saga on locale change
    [IDTEAM-5328]

--- packages/pass/components/Settings/Locale.tsx
+++ packages/pass/components/Settings/Locale.tsx
@@ -4,8 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { c } from 'ttag';
 
 import { LocaleSelect } from '@proton/pass/components/Form/legacy/LocaleSelect';
-import { getInAppNotifications, settingsEditIntent } from '@proton/pass/store/actions';
-import { withRevalidate } from '@proton/pass/store/request/enhancers';
+import { settingsEditIntent } from '@proton/pass/store/actions';
 import { selectLocale } from '@proton/pass/store/selectors';
 
 import { SettingsPanel } from './SettingsPanel';
@@ -18,14 +17,7 @@ export const Locale: FC = () => {
         <SettingsPanel title={c('Label').t`Language`}>
             <LocaleSelect
                 value={locale}
-                onChange={(locale) => {
-                    dispatch(settingsEditIntent('locale', { locale }, true));
-                    /* Update in-app notifications translations. Add timeout so BE can get
-                     * the updated language before returning translated notifications */
-                    setTimeout(() => {
-                        dispatch(withRevalidate(getInAppNotifications.intent()));
-                    }, 1000);
-                }}
+                onChange={(locale) => dispatch(settingsEditIntent('locale', { locale }, true))}
             />
         </SettingsPanel>
     );
