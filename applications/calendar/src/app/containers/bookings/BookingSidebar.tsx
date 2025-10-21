82595ea887048fc209c002010c199bd5ab7b74f2
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-10-21 14:03:12 +0200

    Move the form inside the sidebar

--- applications/calendar/src/app/containers/bookings/BookingSidebar.tsx
+++ applications/calendar/src/app/containers/bookings/BookingSidebar.tsx
@@ -1,11 +1,8 @@
 import { BookingManagement } from './BookingsPageManagement';
 
-export const BookingSidePanel = () => {
+export const BookingSidebar = () => {
     return (
-        <div
-            className="w-full sm:w-custom h-full flex flex-column absolute top-0 bg-norm px-6 py-4 sm:shadow-lifted "
-            style={{ '--w-custom': '15rem' }}
-        >
+        <div className="w-full sm:w-custom flex flex-column bg-norm px-6 py-4" style={{ '--w-custom': '15rem' }}>
             <BookingManagement.Header />
             <div className="grow">
                 <BookingManagement.Form />
