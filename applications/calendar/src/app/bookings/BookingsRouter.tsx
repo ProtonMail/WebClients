fd76aef232598dff9be4ec93a4456b2188c5ba9c
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-10-22 16:43:29 +0200

    Add a simple catch-all component for non-valid URLS

--- applications/calendar/src/app/bookings/BookingsRouter.tsx
+++ applications/calendar/src/app/bookings/BookingsRouter.tsx
@@ -1,10 +1,19 @@
 import { useEffect } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import { useFlag } from '@proton/unleash';
 
 import { BookingDetails } from './components/BookingDetails/BookingDetails';
 import { BookingPageLayout } from './components/BookingPageLaout';
+
+// Catch-all that redirects non-supported URL to the origin
+const RedirectToOrigin = () => {
+    useEffect(() => {
+        window.location.replace(window.location.origin);
+    }, []);
+
+    return null;
+};
 
 export const BookingsRouter = () => {
     const isEnabled = useFlag('CalendarExternalBookings');
@@ -21,7 +30,10 @@ export const BookingsRouter = () => {
 
     return (
         <BookingPageLayout>
-            <Route path="/:bookingId" exact component={BookingDetails} />
+            <Switch>
+                <Route path="/:bookingId" exact component={BookingDetails} />
+                <Route component={RedirectToOrigin} />
+            </Switch>
         </BookingPageLayout>
     );
 };
