86ec31a8d8b7db41c13c01590e24d326fa1ad300
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-10-22 09:21:16 +0200

    Redirect the user to the calendar app if disabled

--- applications/calendar/src/app/bookings/BookingsRouter.tsx
+++ applications/calendar/src/app/bookings/BookingsRouter.tsx
@@ -1,10 +1,24 @@
+import { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 
+import { useFlag } from '@proton/unleash';
+
 export const BookingsRouter = () => {
+    const isEnabled = useFlag('CalendarExternalBookings');
+
+    useEffect(() => {
+        if (!isEnabled) {
+            window.location.replace(window.location.origin);
+        }
+    }, [isEnabled]);
+
+    if (!isEnabled) {
+        return null;
+    }
+
     return (
         <>
-            <Route path="/" exact render={() => <p>Root route</p>} />
-            <Route path="/:bookingID" render={() => <p>Bookings detail route</p>} />
+            <Route path="/" exact render={() => <></>} />
         </>
     );
 };
