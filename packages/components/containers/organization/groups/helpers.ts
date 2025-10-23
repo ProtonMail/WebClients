83f507fb9295b0eb05db427c63318f6e6fca7daa
Yang Lin <yang.lin@proton.ch>
2025-10-23 04:08:18 +0000

    Enhance: group bulk management

--- packages/components/containers/organization/groups/helpers.ts
+++ packages/components/containers/organization/groups/helpers.ts
@@ -1,3 +1,5 @@
+import type { EnhancedMember, GroupMember, PartialMemberAddress, Recipient } from '@proton/shared/lib/interfaces';
+import type { ContactEmail } from '@proton/shared/lib/interfaces/contacts';
 import getRandomString, { DEFAULT_LOWERCASE_CHARSET } from '@proton/utils/getRandomString';
 import isTruthy from '@proton/utils/isTruthy';
 
@@ -15,3 +17,27 @@ export const getAddressSuggestedLocalPart = (
         .replace(/[^a-z0-9-_\.\s+]/g, '')
         .replace(/\s+/g, '-');
 };
+
+export const convertEnhancedMembersToContactEmails = (members?: EnhancedMember[]): ContactEmail[] => {
+    if (!members) {
+        return [];
+    }
+    const createContactEmail = (member: EnhancedMember) => (address: PartialMemberAddress) => ({
+        ID: address.ID,
+        Email: address.Email,
+        Name: member.Name,
+        Type: [],
+        Defaults: 0,
+        Order: 0,
+        ContactID: member.ID,
+        LabelIDs: [],
+        LastUsedTime: 0,
+    });
+    return members.flatMap((member) => member.Addresses?.map(createContactEmail(member)) || []);
+};
+
+export const convertGroupMemberToRecipient = (groupMembers: (GroupMember | ContactEmail)[]): Recipient[] =>
+    groupMembers.map((member) => ({
+        Name: member.Email || '',
+        Address: member.Email || '',
+    }));
