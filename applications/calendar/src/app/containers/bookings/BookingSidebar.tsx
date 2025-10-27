ca26c3cd2f2cf26d87fd7c2c2a4153fdc0214062
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-10-27 16:18:38 +0100

    Improve padding and add scroll over the form

--- applications/calendar/src/app/containers/bookings/BookingSidebar.tsx
+++ applications/calendar/src/app/containers/bookings/BookingSidebar.tsx
@@ -1,14 +1,23 @@
+import { Scroll } from '@proton/atoms/Scroll/Scroll';
+
 import { BookingManagement } from './BookingsPageManagement';
 
 export const BookingSidebar = () => {
     return (
-        <div className="w-full sm:w-custom flex flex-column bg-norm px-6 py-4" style={{ '--w-custom': '15rem' }}>
-            <BookingManagement.Header />
-            <div className="grow">
-                <BookingManagement.Form />
+        <div className="w-full sm:w-custom flex flex-column" style={{ '--w-custom': '15rem' }}>
+            <div className="p-3 self-end">
+                <BookingManagement.Header />
             </div>
 
-            <BookingManagement.Buttons />
+            <Scroll className="flex-1 w-full">
+                <div className="px-6 py-4">
+                    <BookingManagement.Form />
+                </div>
+            </Scroll>
+
+            <div className="px-6 py-4">
+                <BookingManagement.Buttons />
+            </div>
         </div>
     );
 };
