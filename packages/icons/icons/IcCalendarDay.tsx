6c45998564b2e79a046f064ff7797097ddd5b24b
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-10-28 17:08:48 +0100

    update calendar and map pin icons

--- packages/icons/icons/IcCalendarDay.tsx
+++ packages/icons/icons/IcCalendarDay.tsx
@@ -33,8 +33,10 @@ export const IcCalendarDay = ({ alt, title, size = 4, className = '', viewBox = 
 
                 <path
                     fillRule="evenodd"
-                    d="M11 3H5V1.5a.5.5 0 0 0-1 0V3a3 3 0 0 0-3 3v6a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3V1.5a.5.5 0 0 0-1 0V3ZM4 4h8a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2Z"
+                    d="M13 2a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2 .5.5 0 0 1 0 1 1 1 0 0 0-1 1v2h12V4a1 1 0 0 0-1-1 .5.5 0 0 1 0-1ZM2 13a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V7H2v6Z"
+                    clipRule="evenodd"
                 ></path>
+                <path d="M4.5 1a.5.5 0 0 1 .5.5V3a.5.5 0 0 1-1 0V1.5a.5.5 0 0 1 .5-.5Zm7 0a.5.5 0 0 1 .5.5V3a.5.5 0 0 1-1 0V1.5a.5.5 0 0 1 .5-.5ZM10 2a.5.5 0 0 1 0 1H6a.5.5 0 0 1 0-1h4Z"></path>
             </svg>
             {alt ? <span className="sr-only">{alt}</span> : null}
         </>
