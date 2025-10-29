8efac8ddff03b79cd75d2982f92a0e541db03d0e
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2025-10-29 17:28:34 +0100

    Move autotype from JS to Rust
    [IDTEAM-4336]

--- applications/pass-desktop/native/src/autotypes.rs
+++ applications/pass-desktop/native/src/autotypes.rs
@@ -1,3 +1,5 @@
+#[cfg(target_os = "linux")]
+use crate::clipboards::{Clipboard, ClipboardTrait};
 use anyhow::{Error, Result};
 use enigo::{
     Direction::{Click, Press, Release},
@@ -15,22 +17,24 @@ impl Autotype {
         })
     }
 
-    pub fn text(&mut self, text: &str) -> Result<(), Error> {
+    #[cfg_attr(target_os = "linux", allow(dead_code))]
+    fn text(&mut self, text: &str) -> Result<(), Error> {
         self.enigo.text(text)?;
         Ok(())
     }
 
-    pub fn tab(&mut self) -> Result<(), Error> {
+    fn tab(&mut self) -> Result<(), Error> {
         self.enigo.key(Key::Tab, Click)?;
         Ok(())
     }
 
-    pub fn enter(&mut self) -> Result<(), Error> {
+    fn enter(&mut self) -> Result<(), Error> {
         self.enigo.key(Key::Return, Click)?;
         Ok(())
     }
 
-    pub fn paste(&mut self) -> Result<(), Error> {
+    #[cfg_attr(not(target_os = "linux"), allow(dead_code))]
+    fn paste(&mut self) -> Result<(), Error> {
         #[cfg(target_os = "macos")]
         let modifier = Key::Meta;
         #[cfg(not(target_os = "macos"))]
@@ -39,6 +43,48 @@ impl Autotype {
         self.enigo.key(modifier, Press)?;
         self.enigo.key(Key::Unicode('v'), Click)?;
         self.enigo.key(modifier, Release)?;
+        Ok(())
+    }
+
+    fn perform_separator(&mut self, idx: usize, length: usize, enter_at_the_end: bool) -> Result<(), Error> {
+        if idx < length - 1 {
+            self.tab()?;
+        } else if enter_at_the_end {
+            self.enter()?;
+        }
+        Ok(())
+    }
+
+    pub fn perform_autotype(&mut self, fields: Vec<String>, enter_at_the_end: Option<bool>) -> Result<(), Error> {
+        let enter_at_the_end = enter_at_the_end.unwrap_or(false);
+        let fields_len = fields.len();
+
+        /* On Linux, enigo with libei feature (needed to support Wayland)
+        cannot type special characters with enigo.text(),
+        so we copy the field value in clipboard and paste it instead.
+        Related issue: https://github.com/enigo-rs/enigo/issues/404. */
+        #[cfg(target_os = "linux")]
+        {
+            // Save current clipboard value to restore it after autotype is done
+            let initial_clipboard_value = Clipboard::read()?;
+
+            for (idx, field) in fields.iter().enumerate() {
+                Clipboard::write(field, true, true)?;
+                self.paste()?;
+                self.perform_separator(idx, fields_len, enter_at_the_end)?;
+            }
+
+            Clipboard::write(&initial_clipboard_value, true, true)?;
+        }
+
+        #[cfg(not(target_os = "linux"))]
+        {
+            for (idx, field) in fields.iter().enumerate() {
+                self.text(field)?;
+                self.perform_separator(idx, fields_len, enter_at_the_end)?;
+            }
+        }
+
         Ok(())
     }
 }
