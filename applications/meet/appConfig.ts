bb2321e88c8d1add65dbc12fcb6af3e0fc4a46d1
István Barna <istvan.barna@proton.ch>
2025-11-10 12:52:31 +0000

    Separating meet error reporting

--- applications/meet/appConfig.ts
+++ applications/meet/appConfig.ts
@@ -1,6 +1,5 @@
 const appConfig = {
-    clientType: 11,
-    sentry: 'https://8f130b1135494dc8a92a231103dc34e3@sentry/85',
+    sentry: 'https://ea25ab3492624fcc9c2d55921c5c733b@sentry/84',
     sentryDesktop: 'https://8f130b1135494dc8a92a231103dc34e3@sentry/85',
 };
 
