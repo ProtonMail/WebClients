3ddfcd65db898675cda6209260de1ed42763e601
Nico Hoffmann <nicolas.hoffmann@proton.ch>
2025-11-10 17:16:49 +0100

    Exclude experiment users from Drive/Pass unlimited offers

--- packages/components/containers/offers/operations/blackFriday2025DrivePlusYearly/useOffer.ts
+++ packages/components/containers/offers/operations/blackFriday2025DrivePlusYearly/useOffer.ts
@@ -2,6 +2,8 @@ import { useSubscription } from '@proton/account/subscription/hooks';
 import { useUser } from '@proton/account/user/hooks';
 import useConfig from '@proton/components/hooks/useConfig';
 import { useAutomaticCurrency } from '@proton/components/payments/client-extensions';
+import { FeatureCode } from '@proton/features/interface';
+import useFeature from '@proton/features/useFeature';
 
 import useOfferFlags from '../../hooks/useOfferFlags';
 import type { Operation } from '../../interface';
@@ -13,6 +15,9 @@ export const useBlackFriday2025DrivePlusYearly = (): Operation => {
     const [subscription, subscriptionLoading] = useSubscription();
     const { isActive, loading: flagsLoading } = useOfferFlags(blackFriday2025DrivePlusYearlyConfig);
     const [preferredCurrency, loadingCurrency] = useAutomaticCurrency();
+    const userInInboxExperiment = useFeature(FeatureCode.BlackFridayWave2InboxExperiment);
+    const userInVPNExperiment = useFeature(FeatureCode.BlackFridayWave2VPNExperiment);
+    const userInExperiment = userInInboxExperiment?.feature?.Value === 1 || userInVPNExperiment?.feature?.Value === 1;
     const protonConfig = useConfig();
     const isLoading = flagsLoading || userLoading || subscriptionLoading || loadingCurrency;
     const isEligible = getIsEligible({
@@ -21,6 +26,7 @@ export const useBlackFriday2025DrivePlusYearly = (): Operation => {
         user,
         offerConfig: blackFriday2025DrivePlusYearlyConfig,
         preferredCurrency,
+        userInExperiment,
     });
     const isValid = isEligible && isActive;
 
