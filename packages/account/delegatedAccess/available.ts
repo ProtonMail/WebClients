4e0f6aca42f518137d249c2724d110cb36536368
Mattias Svanström <mattias.svanstrom@proton.ch>
2025-11-14 15:11:26 +0100

    Fix emergency access permissions for non-private admins

--- packages/account/delegatedAccess/available.ts
+++ packages/account/delegatedAccess/available.ts
@@ -1,4 +1,4 @@
-import type { UserModel } from '@proton/shared/lib/interfaces';
+import type { User } from '@proton/shared/lib/interfaces';
 import { isAdmin, isPrivate, isSelf } from '@proton/shared/lib/user/helpers';
 
 // Route that is in the email links
@@ -12,12 +12,12 @@ export const getViewTrustedContactRoute = (id: string) => {
     return `/recovery?${search}#emergency-access`;
 };
 
-export const getIsIncomingDelegatedAccessAvailable = (user: UserModel) => {
+export const getIsIncomingDelegatedAccessAvailable = (user: User) => {
     // Incoming delegated access, need to have keys setup and not impersonating
     return user.Keys.length > 0 && isSelf(user);
 };
 
-export const getIsOutgoingDelegatedAccessAvailable = (user: UserModel) => {
+export const getIsOutgoingDelegatedAccessAvailable = (user: User) => {
     // Outgoing delegated access, need to manage your own keys (isPrivate or be admin)
     return getIsIncomingDelegatedAccessAvailable(user) && (isPrivate(user) || isAdmin(user));
 };
