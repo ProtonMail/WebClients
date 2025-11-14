51d998dbd734978846554ad3cd117acf0a29a352
Nico Hoffmann <nicolas.hoffmann@proton.ch>
2025-11-14 15:10:30 +0100

    Booking empty container styles

--- applications/calendar/src/app/bookings/components/BookingsView.tsx
+++ applications/calendar/src/app/bookings/components/BookingsView.tsx
@@ -124,7 +124,7 @@ export const BookingsView = () => {
                 })}
             </div>
             {!canShowSlots && (
-                <div className="border rounded flex flex-column items-center justify-center p-8 mt-2 text-center">
+                <div className="border rounded-xl flex flex-column items-center justify-center p-20 mt-2 text-center booking-no-booking-container">
                     <div>{c('Info').t`No appointments available this week`}</div>
                     <Button shape="underline" color="norm" onClick={handleGoToNextAvailableAppointment}>{c('Action')
                         .t`Go to the next available appointment`}</Button>
