01e926cae2df438b2382b9600c0b7d3bd88278d8
Eamonn Maguire <eamonn.maguire@proton.ch>
2025-11-17 17:13:09 +0100

    [LUMO-324] Fixes for mobile etc.

--- applications/lumo/src/app/ui/MainLayout.tsx
+++ applications/lumo/src/app/ui/MainLayout.tsx
@@ -2,6 +2,8 @@ import type { ReactNode } from 'react';
 
 import { GhostChatProvider } from '../providers/GhostChatProvider';
 import { SidebarProvider, useSidebar } from '../providers/SidebarProvider';
+import { SearchModalProvider, useSearchModal } from '../providers/SearchModalProvider';
+import { useKeyboardShortcuts } from '../hooks/useKeyboardShortcuts';
 import HighLoadWarning from './components/HighLoadWarning';
 import LumoSidebar from './sidebar/LumoSidebar';
 
@@ -14,6 +16,11 @@ interface Props {
 
 const MainLayoutContent = ({ children, HeaderComponent }: Props) => {
     const { isSmallScreen } = useSidebar();
+    const { openSearchModal } = useSearchModal();
+    
+    // Set up keyboard shortcuts
+    useKeyboardShortcuts({ onOpenSearch: openSearchModal });
+
     return (
         <div className="relative reset4print flex flex-row h-full w-full overflow-hidden">
             <div className="absolute top-0 left-0 w-full h-full no-print">
@@ -36,7 +43,9 @@ export const MainLayout = ({ children, HeaderComponent }: Props) => {
     return (
         <GhostChatProvider>
             <SidebarProvider>
-                <MainLayoutContent children={children} HeaderComponent={HeaderComponent} />
+                <SearchModalProvider>
+                    <MainLayoutContent children={children} HeaderComponent={HeaderComponent} />
+                </SearchModalProvider>
             </SidebarProvider>
         </GhostChatProvider>
     );
