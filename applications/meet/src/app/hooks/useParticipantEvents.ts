3735f72c14c4458527e3139181b2be5a9bd354d4
István Barna <istvan.barna@proton.ch>
2025-11-19 14:34:01 +0000

    Sound notification upon meeting join up to 5 participants

--- applications/meet/src/app/hooks/useParticipantEvents.ts
+++ applications/meet/src/app/hooks/useParticipantEvents.ts
@@ -5,10 +5,13 @@ import type { Participant } from 'livekit-client';
 
 import isTruthy from '@proton/utils/isTruthy';
 
+import { JOIN_SOUND_NOTIFICATION_PARTICIPANT_LIMIT } from '../constants';
 import { ParticipantEvent, type ParticipantEventRecord } from '../types';
+import { useAudioPlayer } from './useAudioPlayer';
 
 export const useParticipantEvents = (participantNameMap: Record<string, string>) => {
     const room = useRoomContext();
+    const { playAudio } = useAudioPlayer('/assets/sounds/join_notification.wav');
 
     const [events, setEvents] = useState<ParticipantEventRecord[]>([]);
 
@@ -34,6 +37,10 @@ export const useParticipantEvents = (participantNameMap: Record<string, string>)
                     type: 'event',
                 },
             ]);
+
+            if (room.numParticipants <= JOIN_SOUND_NOTIFICATION_PARTICIPANT_LIMIT) {
+                playAudio();
+            }
         };
 
         const handleParticipantDisconnected = (participant: Participant) => {
@@ -56,7 +63,7 @@ export const useParticipantEvents = (participantNameMap: Record<string, string>)
             room.off('participantConnected', handleParticipantConnected);
             room.off('participantDisconnected', handleParticipantDisconnected);
         };
-    }, [room, participantNameMap]);
+    }, [room, participantNameMap, playAudio]);
 
     return eventsHavingNamedParticipants;
 };
