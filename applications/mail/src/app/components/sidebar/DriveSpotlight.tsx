192b843ee59764ae88c22716eb20b0f28b0a77d0
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-11-24 15:40:58 +0000

    Merge branch 'bug/drive-get-started' into 'main'
    Add condition around get-started request
    
    See merge request web/clients!20858

--- applications/mail/src/app/components/sidebar/DriveSpotlight.tsx
+++ applications/mail/src/app/components/sidebar/DriveSpotlight.tsx
@@ -1,17 +1,42 @@
+import { useEffect, useState } from 'react';
+
 import { c } from 'ttag';
 
-import { Spotlight } from '@proton/components';
+import { useUser } from '@proton/account/user/hooks';
+import { Spotlight, useApi } from '@proton/components';
 import ProductIcon from '@proton/components/containers/app/ProductIcon';
 import { FeatureCode, useFeature } from '@proton/features';
+import { getDriveChecklist } from '@proton/shared/lib/api/checklist';
+import { getSilentApi } from '@proton/shared/lib/api/helpers/customConfig';
 import { APPS, DRIVE_APP_NAME } from '@proton/shared/lib/constants';
+import type { ChecklistApiResponse } from '@proton/shared/lib/interfaces';
 import noop from '@proton/utils/noop';
 
-import { useDriveSpotlight } from './useDriveSpotlight';
+const FIRST_OF_DECEMBER = new Date(2025, 11, 1).valueOf();
 
 export function DriveSpotlight({ children }: { children: any }) {
-    const canShowSpotlight = useDriveSpotlight();
+    const normalApi = useApi();
+    const silentApi = getSilentApi(normalApi);
+    const [user] = useUser();
+    const [driveChecklist, setDriveChecklist] = useState<ChecklistApiResponse>();
     const { feature, update } = useFeature(FeatureCode.DriveSpotlightInMail);
+
+    const isEligibleForSpotlight = FIRST_OF_DECEMBER > Date.now() && user.ProductUsedSpace.Drive === 0;
+    const canShowSpotlight = isEligibleForSpotlight && !!driveChecklist?.Completed;
     const spotlightAlreadyShown = (feature?.Value ?? false) === false;
+
+    useEffect(() => {
+        if (!isEligibleForSpotlight) {
+            return;
+        }
+
+        const effect = async () => {
+            const checklist = await silentApi<ChecklistApiResponse>(getDriveChecklist('get-started'));
+            setDriveChecklist(checklist);
+        };
+
+        void effect();
+    }, [silentApi, isEligibleForSpotlight]);
 
     return (
         <Spotlight
