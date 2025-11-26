471b312b8470f6d66791eb8e4b3413d52fee74f7
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-11-26 13:22:18 +0100

    Create custom upsell config for bookings

---
+++ packages/components/components/upsell/UpsellModal/configs/getUpsellModalBookingsConfig.ts
@@ -0,0 +1,33 @@
+import { CYCLE, PLANS } from '@proton/payments';
+
+import { getUpsellModalFooterText } from '../helpers/getUpsellModalFooterText';
+import { getUpsellModalSubmitText } from '../helpers/getUpsellModalSubmitText';
+import { getUpsellPlanMonthlyPrice } from '../helpers/getUpsellPlanMonthlyPrice';
+import type { UpsellModalConfigCase } from '../interface';
+
+export const getUpsellModalBookingsConfig: UpsellModalConfigCase = async (props) => {
+    const { currency, paymentsApi, plans, user } = props;
+    // TODO change this value to the workspace plan
+    const plan = user.hasPaidMail ? PLANS.BUNDLE_PRO_2024 : PLANS.MAIL;
+    const planIDs = { [plan]: 1 };
+    const cycle = CYCLE.YEARLY;
+
+    const monthlyPrice = await getUpsellPlanMonthlyPrice({
+        currency,
+        cycle,
+        paymentsApi,
+        planIDs,
+        plans,
+    });
+
+    return {
+        planIDs,
+        cycle,
+        footerText: getUpsellModalFooterText({
+            planIDs,
+            currency,
+            monthlyPrice: monthlyPrice.regularPrice,
+        }),
+        submitText: getUpsellModalSubmitText({ planIDs }),
+    };
+};
