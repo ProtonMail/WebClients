fb6ca6b2f6de4bf1a98fa052397a1df655915b34
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-11-26 11:40:52 +0100

    Extract upsell logic in a hook

--- applications/calendar/src/app/containers/bookings/upsells/upsellHelpers.ts
+++ applications/calendar/src/app/containers/bookings/upsells/upsellHelpers.ts
@@ -1,3 +1,42 @@
-export const hasUserReachedBookingLimit = (): boolean => {
-    return true;
+import { PLANS, type Subscription, getPlan } from '@proton/payments';
+import type { UserModel } from '@proton/shared/lib/interfaces';
+
+import type { InternalBookingPage } from '../../../store/internalBooking/interface';
+
+export const hasUserReachPlanLimit = (
+    user: UserModel,
+    subscription: Subscription,
+    bookingsPages?: InternalBookingPage[]
+): boolean => {
+    if (!user.hasPaidMail) {
+        return true;
+    }
+
+    const planName = getPlan(subscription)?.Name;
+    if (!planName) {
+        return true;
+    }
+
+    if (!bookingsPages) {
+        return false;
+    }
+
+    switch (planName) {
+        case PLANS.MAIL:
+        case PLANS.BUNDLE:
+        case PLANS.DUO:
+        case PLANS.FAMILY:
+        case PLANS.MAIL_PRO:
+            return bookingsPages.length >= 1;
+    }
+
+    return false;
+};
+
+export const hasUserReachBookingsLimit = (bookingsPages?: InternalBookingPage[]): boolean => {
+    if (!bookingsPages) {
+        return false;
+    }
+
+    return bookingsPages.length >= 25;
 };
