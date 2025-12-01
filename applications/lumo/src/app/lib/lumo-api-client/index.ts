30afe753ef72dc4a4055d8773fdc9d5ab92b81cf
Marc Dupont <marc.dupont@proton.ch>
2025-12-01 13:55:00 +0100

    Refactor response decryption 1

--- applications/lumo/src/app/lib/lumo-api-client/index.ts
+++ applications/lumo/src/app/lib/lumo-api-client/index.ts
@@ -18,7 +18,7 @@ export type {
     RequestableGenerationTarget,
     GenerationTarget,
     LumoApiGenerationRequest,
-    GenerationToFrontendMessage,
+    GenerationResponseMessage,
     Status,
     LumoApiClientConfig,
     ChunkCallback,
