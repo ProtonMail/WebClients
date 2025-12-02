c4487fbacb668893e8b1062455ea11d392c1b26b
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2025-12-02 09:16:59 +0000

    Merge branch 'centralize-bookings-errors-messages' into 'main'
    Centralize bookings error messages and change form UI
    
    See merge request web/clients!21002

---
+++ applications/calendar/src/app/containers/bookings/utils/bookingCopy.ts
@@ -0,0 +1,27 @@
+import { c } from 'ttag';
+
+import { MAX_BOOKING_SLOTS } from '../bookingsProvider/interface';
+
+export const BookingErrorMessages = {
+    get RANGE_ALREADY_EXIST() {
+        return c('Info').t`Range already exists.`;
+    },
+    get RANGE_OVERLAP() {
+        return c('Info').t`Range overlaps with an existing range.`;
+    },
+    get RANGE_IN_PAST() {
+        return c('Info').t`Cannot create a range in the past.`;
+    },
+    get RANGE_MULTIPLE_DAYS() {
+        return c('Info').t`Cannot create a range across days.`;
+    },
+    get RANGE_LIMIT_EXCEEDED() {
+        return c('Info').t`You can’t have more than ${MAX_BOOKING_SLOTS} booking slots on a page.`;
+    },
+    get RANGE_TOO_SHORT() {
+        return c('Info').t`Range is too short.`;
+    },
+    get NO_CALENDAR_AVAILABLE() {
+        return c('Info').t`No calendar available; please delete the booking page.`;
+    },
+} as const;
