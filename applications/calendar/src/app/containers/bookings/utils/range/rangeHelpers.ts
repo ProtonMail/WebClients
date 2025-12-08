e0156ed06aa65d891c9d29466d01bcf36f272fbe
Flavien Bonvin <flavien.bonvin@proton.ch>
2025-12-08 14:19:56 +0100

    BOOK-347 Round to next half hour when creating today range

--- applications/calendar/src/app/containers/bookings/utils/range/rangeHelpers.ts
+++ applications/calendar/src/app/containers/bookings/utils/range/rangeHelpers.ts
@@ -45,10 +45,18 @@ export const createTodayBookingRange = (date: Date, timezone: string, today: Dat
         return undefined;
     }
 
+    const roundedTime = roundToNextHalfHour(today);
+
     const start =
         nextHour < DEFAULT_RANGE_START_HOUR
             ? set(date, { hours: DEFAULT_RANGE_START_HOUR, minutes: 0, seconds: 0, milliseconds: 0 })
-            : set(date, { hours: nextHour, minutes: 0, seconds: 0, milliseconds: 0 });
+            : set(date, {
+                  hours: roundedTime.getHours(),
+                  minutes: roundedTime.getMinutes(),
+                  seconds: 0,
+                  milliseconds: 0,
+              });
+
     const end = set(date, { hours: DEFAULT_RANGE_END_HOUR, minutes: 0, seconds: 0, milliseconds: 0 });
 
     return {
