9e6615e529c4675ffd234ce98cf81f0a384cfb43
Mattias Svanström <mmso@mmso.se>
2025-12-09 16:22:12 +0100

    Merge branch 'CP-10381-referral-og-text-and-image' into 'main'
    Referral og text and image
    
    See merge request web/clients!19378

--- applications/account/src/pages/refer-a-friend.ts
+++ applications/account/src/pages/refer-a-friend.ts
@@ -5,9 +5,13 @@ import type { Parameters } from './interface';
 // NOTE: This is hardcoded to avoid an import which breaks TypeScript compilation at this stage
 const BRAND_NAME = 'Proton';
 
+const url = 'https://account.proton.me/';
+
 const data = (): Parameters => ({
-    title: c('Metadata title').t`Try ${BRAND_NAME} for free with this link`,
-    description: c('Metadata title').t`Take control of your data with end-to-end encryption`,
+    title: c('Metadata title').t`Enjoy two weeks of ${BRAND_NAME} for free`,
+    description: c('Metadata title')
+        .t`Experience true online privacy. Protect your data with ${BRAND_NAME} for free for the next 14 days.`,
+    ogImage: `${url}assets/refer-a-friend-og-image.jpg`,
 });
 
 export default data;
