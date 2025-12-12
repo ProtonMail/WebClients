b7da711d37bc4ac5b8b4c7f503c6a4de2d9c36aa
Alexey Karpov <alexey.karpov@proton.ch>
2025-12-12 13:52:04 +0000

    Enable vpnpassbiz2025 on V2 signup

--- applications/account/src/app/single-signup/Step1B.tsx
+++ applications/account/src/app/single-signup/Step1B.tsx
@@ -1178,23 +1178,6 @@ const Step1B = ({
                         })}
                     </div>
                 )}
-                {(() => {
-                    if (!actualCheckout.discountPercent || checkTrial) {
-                        return;
-                    }
-
-                    const sharedClasses = 'mb-2';
-                    if (model.loadingDependencies) {
-                        return <SkeletonLoader height="2.5rem" width="30rem" className={sharedClasses} />;
-                    }
-                    return !isVpnPassBundleBusinessPlan ? (
-                        <DiscountBanner
-                            discountPercent={actualCheckout.discountPercent}
-                            selectedPlanTitle={selectedPlan.Title}
-                            className={sharedClasses}
-                        />
-                    ) : null;
-                })()}
                 {showCycleAndSelectors && (
                     <Box className={`mt-8 w-full ${padding}`}>
                         <BoxHeader
@@ -1464,13 +1447,29 @@ const Step1B = ({
                                             startTrial={checkTrial}
                                             onCurrencyChange={handleChangeCurrency}
                                         />
-                                        {isVpnPassBundleBusinessPlan && (
-                                            <DiscountBanner
-                                                discountPercent={actualCheckout.discountPercent}
-                                                selectedPlanTitle={selectedPlan.Title}
-                                                className="mb-2"
-                                            />
-                                        )}
+                                        {(() => {
+                                            if (!actualCheckout.discountPercent || checkTrial || signupTrial) {
+                                                return;
+                                            }
+
+                                            const sharedClasses = 'mb-2';
+                                            if (model.loadingDependencies) {
+                                                return (
+                                                    <SkeletonLoader
+                                                        height="2.5rem"
+                                                        width="30rem"
+                                                        className={sharedClasses}
+                                                    />
+                                                );
+                                            }
+                                            return (
+                                                <DiscountBanner
+                                                    discountPercent={actualCheckout.discountPercent}
+                                                    selectedPlanTitle={selectedPlan.Title}
+                                                    className={sharedClasses}
+                                                />
+                                            );
+                                        })()}
 
                                         {(() => {
                                             if (loadingPaymentsForm) {
