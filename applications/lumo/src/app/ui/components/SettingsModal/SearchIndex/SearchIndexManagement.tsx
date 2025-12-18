b63aa4833346eee0d20a1ce6ba79c708d5978397
Eamonn Maguire <eamonn.maguire@proton.ch>
2025-12-18 16:29:48 +0100

    Fix to the crypto and key used to encrypt the index.

--- applications/lumo/src/app/ui/components/SettingsModal/SearchIndex/SearchIndexManagement.tsx
+++ applications/lumo/src/app/ui/components/SettingsModal/SearchIndex/SearchIndexManagement.tsx
@@ -1,23 +1,20 @@
 import { Button } from '@proton/atoms/Button/Button';
-import { CircleLoader } from '@proton/atoms/CircleLoader/CircleLoader';
 import type { FunctionComponent } from 'react';
-import { Icon } from '@proton/components';
 
 import { c } from 'ttag';
 
 interface Props {
     onReindex: () => void;
-    onClear: () => void;
     disabled: boolean;
 }
 
-export const SearchIndexManagement: FunctionComponent<Props> = ({ onReindex, onClear, disabled }) => (
+export const SearchIndexManagement: FunctionComponent<Props> = ({ onReindex, disabled }) => (
     <div className="flex gap-2">
         {!disabled &&
             <Button color="norm"
                     onClick={onReindex}
                     title={c('Action').t`Re-index all conversations`}>
-                    c('Action').t`Reindex`
+                    {c('Action').t`Reindex`}
             </Button>
         }
     </div>
