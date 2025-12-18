30272b3235df03b1027f040e91302cfc6838a24b
Eamonn Maguire <eamonn.maguire@proton.ch>
2025-12-18 15:26:00 +0100

    More cleanups on the drive flows.

--- applications/lumo/src/app/ui/components/Files/DriveBrowser/DriveEmptyState.tsx
+++ applications/lumo/src/app/ui/components/Files/DriveBrowser/DriveEmptyState.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms/Button/Button';
+import { Icon } from '@proton/components';
 import { IcArrowUpLine } from '@proton/icons/icons/IcArrowUpLine';
 import lumoDrive from '@proton/styles/assets/img/lumo/lumo-drive.svg';
 
 interface DriveEmptyStateProps {
     onUpload: () => void;
+    onCreateFolder?: () => void;
     loading?: boolean;
     isRefreshing?: boolean;
     disabled?: boolean;
@@ -15,10 +17,13 @@ interface DriveEmptyStateProps {
 
 export const DriveEmptyState: React.FC<DriveEmptyStateProps> = ({
     onUpload,
+    onCreateFolder,
     loading = false,
     isRefreshing = false,
     disabled = false,
 }) => {
+    const isDisabled = loading || isRefreshing || disabled;
+
     return (
         <div className="text-center py-8 text-gray-500">
             <img
@@ -27,17 +32,32 @@ export const DriveEmptyState: React.FC<DriveEmptyStateProps> = ({
                 alt="Lumo + Proton Drive"
                 style={{ '--w-custom': '11.5rem' } as React.CSSProperties}
             />
-            <p>{c('collider_2025: Info').t`This folder is empty`}</p>
+            <p className="mb-4">{c('collider_2025: Info').t`This folder is empty`}</p>
 
-            <Button
-                onClick={onUpload}
-                size="medium"
-                color="norm"
-                disabled={loading || isRefreshing || disabled}
-                title={c('collider_2025: Action').t`Upload file and add to knowledge base`}
-            >
-                <IcArrowUpLine /> {c('collider_2025: Action').t`Upload a file`}
-            </Button>
+            <div className="flex flex-row items-center justify-center gap-2">
+                <Button
+                    onClick={onUpload}
+                    size="medium"
+                    color="norm"
+                    disabled={isDisabled}
+                    title={c('collider_2025: Action').t`Upload files`}
+                >
+                    <IcArrowUpLine className="mr-1" />
+                    {c('collider_2025: Action').t`Upload files`}
+                </Button>
+                {onCreateFolder && (
+                    <Button
+                        onClick={onCreateFolder}
+                        size="medium"
+                        shape="outline"
+                        disabled={isDisabled}
+                        title={c('collider_2025: Action').t`Create folder`}
+                    >
+                        <Icon name="folder-plus" className="mr-1" />
+                        {c('collider_2025: Action').t`Create folder`}
+                    </Button>
+                )}
+            </div>
         </div>
     );
 };
