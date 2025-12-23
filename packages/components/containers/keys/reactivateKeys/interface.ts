fc8018ebb01b589f3fe1714fe17c999724b0cca4
Mattias Svanström <mmso@mmso.se>
2025-12-23 18:11:19 +0100

    Fix loading prop in recover keys modal

--- packages/components/containers/keys/reactivateKeys/interface.ts
+++ packages/components/containers/keys/reactivateKeys/interface.ts
@@ -2,6 +2,7 @@ import type { KeyReactivationRequestState } from '@proton/shared/lib/keys';
 
 export interface ReactivateKeysContentProps {
     keyReactivationStates: KeyReactivationRequestState[];
+    loading: boolean;
     onLoading: (value: boolean) => void;
     onClose?: () => void;
 }
