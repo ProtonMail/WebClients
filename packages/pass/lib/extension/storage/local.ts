22eaa34b25291d9f6439c0e57f73ad54b8a3f18d
Edvin CANDON <edvin.candon@proton.ch>
2025-12-23 07:10:07 +0100

    Allow preserving storage keys on extension local storage clear
    [IDTEAM-2590]

--- packages/pass/lib/extension/storage/local.ts
+++ packages/pass/lib/extension/storage/local.ts
@@ -1,5 +1,6 @@
 import browser from '@proton/pass/lib/globals/browser';
 import type {
+    ClearItems,
     GetItem,
     GetItems,
     RemoveItem,
@@ -8,6 +9,7 @@ import type {
     SetItems,
     StorageInterface,
 } from '@proton/pass/types';
+import { notIn } from '@proton/pass/utils/fp/predicates';
 
 const getItems: GetItems = async (keys) => browser.storage.local.get(keys);
 const getItem: GetItem = async (key) => (await getItems([key]))?.[key] ?? null;
@@ -15,7 +17,14 @@ const setItems: SetItems = async (items) => browser.storage.local.set(items);
 const setItem: SetItem = async (key, value) => setItems({ [key]: value });
 const removeItems: RemoveItems = async (keys) => browser.storage.local.remove(keys);
 const removeItem: RemoveItem = async (key) => removeItems([key]);
-const clear = async (): Promise<void> => browser.storage.local.clear();
+const clear: ClearItems = async (options) => {
+    if (!options || options.preserve.length === 0) return browser.storage.local.clear();
+    else {
+        const store = await browser.storage.local.get();
+        const keysToRemove = Object.keys(store).filter(notIn(options.preserve));
+        return browser.storage.local.remove(keysToRemove);
+    }
+};
 
 const browserLocalStorage: StorageInterface = {
     getItems,
