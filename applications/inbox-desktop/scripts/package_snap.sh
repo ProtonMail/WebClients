11fe59cbc25e280c2c953dd041ceb28889c890e9
Atanas Janeshliev <atanas.janeshliev@proton.ch>
2025-12-24 12:41:07 +0100

    chore: let's try

--- applications/inbox-desktop/scripts/package_snap.sh
+++ applications/inbox-desktop/scripts/package_snap.sh
@@ -4,10 +4,20 @@ set -e
 APP_VERSION=$(node -p "require('./package.json').version")
 APP_DEB_PATH="./out/make/deb/x64/proton-mail_${APP_VERSION}_amd64.deb"
 
+if [ -n "$BUILD_TAG" ]; then
+  SNAP_VERSION="${APP_VERSION}+${BUILD_TAG}"
+else
+  SNAP_VERSION="${APP_VERSION}"
+fi
+
+
 echo "Building snap"
 echo "APP_VERSION: $APP_VERSION"
+echo "BUILD_TAG: $BUILD_TAG"
+echo "SNAP_VERSION: $SNAP_VERSION"
 echo "APP_DEB_PATH: $APP_DEB_PATH"
 
 sed -e "s/\$APP_VERSION/$APP_VERSION/g" \
     -e "s|\$APP_DEB_PATH|$APP_DEB_PATH|g" \
+    -e "s|\$SNAP_VERSION|$SNAP_VERSION|g" \
     ./snapcraft.yaml.template > ./snap/snapcraft.yaml
