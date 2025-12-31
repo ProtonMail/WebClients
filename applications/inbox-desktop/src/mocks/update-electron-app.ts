094bda69530e3820e4df207a4f81db7dbf6f5f1b
Atanas Janeshliev <atanas.janeshliev@proton.ch>
2025-12-31 12:31:55 +0100

    chore: fix INDA test job on CI

---
+++ applications/inbox-desktop/src/mocks/update-electron-app.ts
@@ -0,0 +1,5 @@
+export const updateElectronApp = jest.fn();
+export const UpdateSourceType = {
+    ElectronPublicUpdateService: "electron-public-update-service",
+    StaticStorage: "static-storage",
+};
