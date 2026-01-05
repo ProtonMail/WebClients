67e0242416cac9a72fecebf3bbee0929a80b5daa
Marc Dupont <marc.dupont@proton.ch>
2026-01-05 12:43:29 +0100

    Decryption transform stream

--- applications/lumo/src/app/util/nullable.ts
+++ applications/lumo/src/app/util/nullable.ts
@@ -2,3 +2,11 @@
 export function isNonNullable<T>(value: NonNullable<T> | undefined): value is NonNullable<T> {
     return value !== undefined;
 }
+
+// Wrap a function to allow undefined
+export function consider<T, U>(f: (x: T) => U): (x: T | undefined) => U | undefined;
+export function consider<T, U>(f: (x: T) => U): (x: undefined) => undefined;
+export function consider<T, U>(f: (x: T) => U): (x: T) => U;
+export function consider<T, U>(f: (x: T) => U): (x: T | undefined) => U | undefined {
+    return (x: T | undefined) => (x !== undefined ? f(x) : undefined);
+}
