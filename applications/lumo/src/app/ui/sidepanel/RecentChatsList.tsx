c2a308e37f0904d9664a0cc8f2e2d0c738d80456
Elena Atencio <elena.atencio@proton.ch>
2026-01-08 11:24:09 +0100

    prevent horizontal scrolling in sidebar

--- applications/lumo/src/app/ui/sidepanel/RecentChatsList.tsx
+++ applications/lumo/src/app/ui/sidepanel/RecentChatsList.tsx
@@ -8,11 +8,11 @@ import ChatDropdownMenu from './ChatDropdownMenu';
 interface ChatsListProps {
     conversations: Conversation[];
     selectedConversationId?: ConversationId;
-    disabled?: boolean; //used to disabled links for guest users
+    disabled?: boolean; //used to disabled links for guest users TODO: check if this is still true
     onItemClick?: () => void;
 }
 
-const RecentChatsList = ({ conversations, selectedConversationId, disabled = false, onItemClick }: ChatsListProps) => {
+const RecentChatsList = ({ conversations, selectedConversationId, onItemClick }: ChatsListProps) => {
     return (
         <ul className="flex flex-column flex-nowrap gap-0.5 shrink-0 pl-1 my-0">
             {conversations.map((conversation) => {
@@ -25,8 +25,10 @@ const RecentChatsList = ({ conversations, selectedConversationId, disabled = fal
                             'relative group-hover-hide-container group-hover-opacity-container flex items-center shrink-0',
                             'flex navigation-link w-full items-center flex-nowrap justify-space-between',
                             'py-0 w-full pl-2 text-sm',
+                            'max-w-full w-custom',
                             isSelected && 'is-active'
                         )}
+                        style={{ '--w-custom': '95%' }}
                     >
                         <LumoLink
                             to={`/c/${conversation.id}`}
