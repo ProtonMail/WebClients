e2a63b52bc9755d3d525db71e2197d55300c4796
István Barna <istvan.barna@proton.ch>
2026-01-16 11:28:44 +0000

    Subscribing to all audio tracks and handling invalidation if limit is reached

--- applications/meet/src/app/contexts/SubscriptionManagementProvider.tsx
+++ applications/meet/src/app/contexts/SubscriptionManagementProvider.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 
-import { useParticipantAudioControls } from '../hooks/useParticipantAudioControls';
+import { useParticipantAudioControls } from '../hooks/useParticipantAudioControls/useParticipantAudioControls';
 import { CameraTrackSubscriptionCacheProvider } from './CameraTrackSubscriptionCacheProvider/CameraTrackSubscriptionCacheProvider';
 
 export const SubscriptionManagementProvider = ({ children }: { children: ReactNode }) => {
