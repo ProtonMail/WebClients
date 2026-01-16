ea00fb875b4280db4983a605ee984b2e3fc90c3b
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-01-16 09:34:10 +0100

    Remove memo and callback from the useSelect all hook

--- applications/mail/src/app/hooks/useSelectAll.tsx
+++ applications/mail/src/app/hooks/useSelectAll.tsx
@@ -1,5 +1,3 @@
-import { useCallback, useMemo } from 'react';
-
 import getBoldFormattedText from '@proton/components/helpers/getBoldFormattedText';
 import { useConversationCounts, useFolders, useLabels, useMessageCounts } from '@proton/mail';
 import { useMailSettings } from '@proton/mail/store/mailSettings/hooks';
@@ -32,34 +30,33 @@ export const useSelectAll = ({ labelID }: Props) => {
     const selectAll = useMailSelector(selectSelectAll);
     const selectAllAvailable = useFlag('SelectAll');
 
-    const locationCount = useMemo(() => {
+    const locationCount = () => {
         return getLocationElementsCount(labelID, conversationCounts || [], messageCounts || [], isConversation);
-        // eslint-disable-next-line react-hooks/exhaustive-deps -- autofix-eslint-CFA412
-    }, [conversationCounts, messageCounts, labelID]);
+    };
 
-    const getBannerText = useCallback(() => {
-        return getBoldFormattedText(getSelectAllBannerText(isConversation, selectAll ? locationCount : mailPageSize));
-    }, [isConversation, locationCount, mailPageSize, selectAll]);
+    const getBannerText = () => {
+        return getBoldFormattedText(getSelectAllBannerText(isConversation, selectAll ? locationCount() : mailPageSize));
+    };
 
-    const getBannerTextWithLocation = useCallback(() => {
+    const getBannerTextWithLocation = () => {
         return getSelectAllBannerTextWithLocation({
             conversationMode: isConversation,
-            elementsCount: selectAll ? locationCount : mailPageSize,
+            elementsCount: selectAll ? locationCount() : mailPageSize,
             labelID,
             customLabels: labels,
             customFolders: folders,
         });
-    }, [isConversation, locationCount, mailPageSize, selectAll, labelID, labels, folders]);
+    };
 
-    const getButtonText = useCallback(() => {
+    const getButtonText = () => {
         return getSelectAllButtonText({
             selectAll,
-            elementsCount: locationCount,
+            elementsCount: locationCount(),
             labelID,
             customLabels: labels,
             customFolders: folders,
         });
-    }, [selectAll, locationCount, labelID, labels, folders]);
+    };
 
     const setSelectAll = (value: boolean) => {
         dispatch(layoutActions.setSelectAll(value));
