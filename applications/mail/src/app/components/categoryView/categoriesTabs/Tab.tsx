48341c6bdc870460145e08c75f56c707e9b0ee14
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-01-19 16:20:29 +0100

    Trigger event-loop when clicking on already active tab

--- applications/mail/src/app/components/categoryView/categoriesTabs/Tab.tsx
+++ applications/mail/src/app/components/categoryView/categoriesTabs/Tab.tsx
@@ -1,14 +1,19 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 
 import { clsx } from 'clsx';
 import { c, msgid } from 'ttag';
 
-import { Icon } from '@proton/components';
+import { Icon, useEventManager } from '@proton/components';
+import useLoading from '@proton/hooks/useLoading';
 import type { CategoryTab } from '@proton/mail';
 import { useMailSettings } from '@proton/mail/store/mailSettings/hooks';
 import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
+import { wait } from '@proton/shared/lib/helpers/promise';
 import { LABEL_IDS_TO_HUMAN } from '@proton/shared/lib/mail/constants';
 import { VIEW_MODE } from '@proton/shared/lib/mail/mailSettings';
+
+import { params } from 'proton-mail/store/elements/elementsSelectors';
+import { useMailSelector } from 'proton-mail/store/hooks';
 
 import { getLabelFromCategoryId } from '../categoriesStringHelpers';
 import { TabState, categoryColorClassName } from './tabsInterface';
@@ -21,9 +26,24 @@ interface Props {
 
 export const Tab = ({ category, count, tabState }: Props) => {
     const [mailSettings] = useMailSettings();
+
+    const history = useHistory();
+    const mailParams = useMailSelector(params);
+    const { call } = useEventManager();
+
+    const [refreshing, withRefreshing] = useLoading(false);
+
+    const handleClick = () => {
+        if (category.id === mailParams.labelID && history.location.hash === '' && !refreshing) {
+            void withRefreshing(Promise.all([call(), wait(1000)]));
+        }
+    };
+
+    const to = `/${LABEL_IDS_TO_HUMAN[category.id] || LABEL_IDS_TO_HUMAN[MAILBOX_LABEL_IDS.CATEGORY_DEFAULT]}`;
+
     return (
         <NavLink
-            to={LABEL_IDS_TO_HUMAN[category.id] || LABEL_IDS_TO_HUMAN[MAILBOX_LABEL_IDS.CATEGORY_DEFAULT]}
+            to={to}
             className={clsx(
                 'tab-container h-full flex flex-nowrap items-center text-no-decoration color-hint hover:mail-category-color',
                 tabState === TabState.ACTIVE &&
@@ -37,6 +57,7 @@ export const Tab = ({ category, count, tabState }: Props) => {
             aria-label={getLabelFromCategoryId(category.id)}
             data-testid={`category-tab-${category.id}`}
             data-color={category.colorShade}
+            onClick={handleClick}
         >
             <Icon
                 className={clsx('shrink-0', tabState === TabState.ACTIVE && categoryColorClassName)}
