dc3030a159642476eaad78522cb6c582396beb6a
Edvin CANDON <edvin.candon@proton.ch>
2026-01-19 10:36:45 +0100

    Improve message bridge typings
    [IDTEAM-5609]

--- applications/pass-extension/src/app/content/bridge/types.ts
+++ applications/pass-extension/src/app/content/bridge/types.ts
@@ -6,15 +6,12 @@ import type { ALLOWED_MESSAGES, BRIDGE_REQUEST, BRIDGE_RESPONSE } from './consta
 
 export type BridgeMessageType = Unpack<typeof ALLOWED_MESSAGES>;
 export type BridgeMessage<T extends BridgeMessageType = BridgeMessageType> = Extract<WorkerMessage, { type: T }>;
+export type AbstractBridgeMessage<T extends string = string> = { token: string; type: T };
 
-export type BridgeRequest<T extends BridgeMessageType = BridgeMessageType> = {
-    request: BridgeMessage<T>;
-    token: string;
-    type: typeof BRIDGE_REQUEST;
-};
+export type BridgeRequest<T extends BridgeMessageType = BridgeMessageType> = AbstractBridgeMessage<
+    typeof BRIDGE_REQUEST
+> & { request: BridgeMessage<T> };
 
-export type BridgeResponse<T extends BridgeMessageType = BridgeMessageType> = {
-    response: WorkerResponse<BridgeMessage<T>> | MessageFailure;
-    token: string;
-    type: typeof BRIDGE_RESPONSE;
-};
+export type BridgeResponse<T extends BridgeMessageType = BridgeMessageType> = AbstractBridgeMessage<
+    typeof BRIDGE_RESPONSE
+> & { response: WorkerResponse<BridgeMessage<T>> | MessageFailure };
