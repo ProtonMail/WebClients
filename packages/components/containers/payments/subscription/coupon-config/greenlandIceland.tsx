e969154a15bbac41b60b4121e12ddc8c1f35d04a
Alexey Karpov <alexey.karpov@proton.ch>
2026-01-26 09:50:44 +0100

    Add basic Greenland coupon

---
+++ packages/components/containers/payments/subscription/coupon-config/greenlandIceland.tsx
@@ -0,0 +1,8 @@
+import { COUPON_CODES } from '@proton/payments/core/constants';
+
+import type { CouponConfig } from './interface';
+
+export const greenlandIcelandConfig: CouponConfig = {
+    coupons: [COUPON_CODES.PLUS12FOR1],
+    hidden: true,
+};
