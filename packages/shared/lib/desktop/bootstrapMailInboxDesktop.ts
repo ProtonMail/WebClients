a881d182f2fa4945170a00c64be8a76bde08c7ee
Atanas Janeshliev <atanas.janeshliev@proton.ch>
2026-01-27 12:33:21 +0000

    fix(INDA-600): Fix zoom oauth links

--- packages/shared/lib/desktop/bootstrapMailInboxDesktop.ts
+++ packages/shared/lib/desktop/bootstrapMailInboxDesktop.ts
@@ -1,9 +1,11 @@
 import type { createAuthentication } from '@proton/account/bootstrap';
 
 import type createApi from '../api/createApi';
+import { getIsIframe } from '../helpers/browser';
 import { initElectronClassnames } from '../helpers/initElectronClassnames';
 import type { ProtonConfig } from '../interfaces';
 import { listenFreeTrialSessionExpiration } from './endOfTrialHelpers';
+import { handleInboxDesktopIPCPostMessages } from './ipcHelpers';
 
 export function bootstrapMailInboxDesktop({
     config,
@@ -16,4 +18,9 @@ export function bootstrapMailInboxDesktop({
 }) {
     initElectronClassnames();
     listenFreeTrialSessionExpiration(config.APP_NAME, authentication, api);
+
+    // Required for sidebar-calendar iframe
+    if (!getIsIframe()) {
+        handleInboxDesktopIPCPostMessages();
+    }
 }
