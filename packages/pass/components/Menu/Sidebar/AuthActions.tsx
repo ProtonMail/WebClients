a5a89b6c73aa73f5c4e1655b59a28d09a23a6070
Edvin CANDON <edvin.candon@proton.ch>
2026-01-28 13:44:56 +0100

    Block session lock during offline-resume sequence

--- packages/pass/components/Menu/Sidebar/AuthActions.tsx
+++ packages/pass/components/Menu/Sidebar/AuthActions.tsx
@@ -5,6 +5,7 @@ import { c } from 'ttag';
 
 import { DropdownMenuButton } from '@proton/pass/components/Layout/Dropdown/DropdownMenuButton';
 import { LockMode } from '@proton/pass/lib/auth/lock/types';
+import { offlineResume } from '@proton/pass/store/actions';
 import { lockCreateRequest } from '@proton/pass/store/actions/requests';
 import { selectLockMode, selectRequestInFlight } from '@proton/pass/store/selectors';
 import { PASS_APP_NAME } from '@proton/shared/lib/constants';
@@ -14,13 +15,15 @@ type Props = { onLock: () => void };
 export const AuthActions = memo(({ onLock }: Props) => {
     const lockMode = useSelector(selectLockMode);
     const lockCreateInFlight = useSelector(selectRequestInFlight(lockCreateRequest()));
+    const offlineResumeInFlight = useSelector(selectRequestInFlight(offlineResume.requestID()));
+    const disabled = lockCreateInFlight || offlineResumeInFlight;
     const canLock = lockMode !== LockMode.NONE;
 
     return (
         canLock && (
             <DropdownMenuButton
                 onClick={onLock}
-                disabled={lockCreateInFlight}
+                disabled={disabled}
                 label={c('Action').t`Lock ${PASS_APP_NAME}`}
                 icon="lock"
                 parentClassName="mx-3"
