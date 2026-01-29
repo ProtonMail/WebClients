43e6fae067257b0b578fa4e3a50e6352a949eb33
István Barna <istvan.barna@proton.ch>
2026-01-29 09:16:47 +0000

    Opening Meet desktop in a smaller window

--- applications/meet-desktop/src/utils/view/windowHelpers.ts
+++ applications/meet-desktop/src/utils/view/windowHelpers.ts
@@ -4,7 +4,6 @@ import { isLinux, isMac, isWindows } from "../helpers";
 import { appSession } from "../session";
 import { MEET_APP_NAME } from "@proton/shared/lib/constants";
 import { isProdEnv } from "../isProdEnv";
-import { DEFAULT_ZOOM_FACTOR } from "../../constants/zoom";
 import { getFeatureFlagManager } from "../flags/manager";
 import { FeatureFlag } from "../flags/flags";
 
@@ -13,17 +12,8 @@ declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 export const MINIMUM_WIDTH = 900;
 export const MINIMUM_HEIGHT = 300;
 
-export const DEFAULT_WIDTH = 1200;
-export const DEFAULT_HEIGHT = 900;
-
-const DEFAULT_WINDOW_BOUNDS = {
-    zoom: DEFAULT_ZOOM_FACTOR,
-    maximized: true,
-    width: DEFAULT_WIDTH,
-    height: DEFAULT_HEIGHT,
-    x: -1,
-    y: -1,
-};
+export const DEFAULT_WIDTH = 1080;
+export const DEFAULT_HEIGHT = 608; // 16:9 aspect ratio (1080 * 9/16 = 607.5, rounded to 608)
 
 const getOSSpecificConfig = (): BrowserWindowConstructorOptions => {
     if (isMac) {
@@ -46,18 +36,20 @@ const getOSSpecificConfig = (): BrowserWindowConstructorOptions => {
 };
 
 export const getWindowConfig = (): BrowserWindowConstructorOptions => {
-    const { x, y } = DEFAULT_WINDOW_BOUNDS;
-
     const primaryDisplay = screen.getPrimaryDisplay();
     const { width: screenWidth, height: screenHeight } = primaryDisplay.workAreaSize;
+
+    // Center the window on screen
+    const x = Math.round((screenWidth - Math.min(DEFAULT_WIDTH, screenWidth)) / 2);
+    const y = Math.round((screenHeight - Math.min(DEFAULT_HEIGHT, screenHeight)) / 2);
 
     return {
         title: isProdEnv() ? MEET_APP_NAME : `${MEET_APP_NAME} Dev`,
         icon: join(app.getAppPath(), "assets/icon.png"),
         x,
         y,
-        width: screenWidth,
-        height: screenHeight,
+        width: DEFAULT_WIDTH,
+        height: DEFAULT_HEIGHT,
         minWidth: MINIMUM_WIDTH,
         minHeight: MINIMUM_HEIGHT,
         autoHideMenuBar: true,
