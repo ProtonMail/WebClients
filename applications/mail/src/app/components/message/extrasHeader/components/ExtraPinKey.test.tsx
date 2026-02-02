e0cfb8c927f081159a062075c4662a728bfb704d
Redacted <redacted@invalid.proton.ch>
2026-02-02 19:37:28 +0100

    Tests: ExtraPinKey: drop unused key transparency setup

--- applications/mail/src/app/components/message/extrasHeader/components/ExtraPinKey.test.tsx
+++ applications/mail/src/app/components/message/extrasHeader/components/ExtraPinKey.test.tsx
@@ -3,7 +3,7 @@ import { fireEvent, screen } from '@testing-library/react';
 import { getModelState } from '@proton/account/test';
 import type { PublicKeyReference } from '@proton/crypto';
 import type { MessageVerification } from '@proton/mail/store/messages/messagesTypes';
-import { KT_VERIFICATION_STATUS, type MailSettings } from '@proton/shared/lib/interfaces';
+import type { MailSettings } from '@proton/shared/lib/interfaces';
 import type { Message } from '@proton/shared/lib/interfaces/mail/Message';
 import { MAIL_VERIFICATION_STATUS } from '@proton/shared/lib/mail/constants';
 import { PROMPT_PIN } from '@proton/shared/lib/mail/mailSettings';
@@ -60,7 +60,7 @@ describe('Extra pin key banner not displayed', () => {
             verificationStatus: MAIL_VERIFICATION_STATUS.SIGNED_AND_VALID,
         } as MessageVerification;
 
-        await setup(message, messageVerification, true);
+        await setup(message, messageVerification);
         const banner = screen.queryByTestId('extra-pin-key:banner');
 
         expect(banner).toBeNull();
@@ -107,131 +107,125 @@ describe('Extra pin key banner displayed', () => {
         screen.getByText('Trust public key?');
     };
 
-    [true, false].forEach((ktEnabled) => {
-        // AUTOPROMPT
-        it.each`
-            verificationStatus                             | shouldDisablePrompt
-            ${MAIL_VERIFICATION_STATUS.NOT_VERIFIED}       | ${true}
-            ${MAIL_VERIFICATION_STATUS.NOT_VERIFIED}       | ${false}
-            ${MAIL_VERIFICATION_STATUS.SIGNED_AND_INVALID} | ${true}
-            ${MAIL_VERIFICATION_STATUS.SIGNED_AND_INVALID} | ${false}
-        `(
-            `should render the banner when prompt key pinning is AUTOPROMPT (ktEnabled=${ktEnabled})`,
-            async ({ verificationStatus, shouldDisablePrompt }) => {
-                const signingKey = await generateKeys('signing', message.Sender.Address);
-
-                const messageVerification = {
-                    signingPublicKey: signingKey.publicKeys[0],
-                    verificationStatus,
-                    ktVerificationResult: { status: KT_VERIFICATION_STATUS.VERIFIED_KEYS },
-                } as MessageVerification;
-
-                await setup(message, messageVerification, false, true);
-                screen.getByTestId('extra-pin-key:banner');
-                // Expected text is displayed
-                screen.getByText("This sender's public key has not been trusted yet.");
-
-                // Test disable prompt button
-                if (shouldDisablePrompt) {
-                    const updateSpy = jest.fn(() => Promise.resolve({}));
-                    addApiMock('mail/v4/settings/promptpin', updateSpy, 'put');
-
-                    const disableAutopromtButton = screen.getByText('Never show');
-                    fireEvent.click(disableAutopromtButton);
-                    await tick();
-
-                    expect(updateSpy).toHaveBeenCalled();
-                } else {
-                    // Open trust public key modal and trust the key
-                    await openTrustKeyModal();
-                }
-            }
-        );
-
-        // PIN_UNSEEN
-        it(`should render the banner when prompt key pinning is PIN_UNSEEN (ktEnabled=${ktEnabled})`, async () => {
-            const senderKey = await generateKeys('sender', message.Sender.Address);
+    // AUTOPROMPT
+    it.each`
+        verificationStatus                             | shouldDisablePrompt
+        ${MAIL_VERIFICATION_STATUS.NOT_VERIFIED}       | ${true}
+        ${MAIL_VERIFICATION_STATUS.NOT_VERIFIED}       | ${false}
+        ${MAIL_VERIFICATION_STATUS.SIGNED_AND_INVALID} | ${true}
+        ${MAIL_VERIFICATION_STATUS.SIGNED_AND_INVALID} | ${false}
+    `(
+        'should render the banner when prompt key pinning is AUTOPROMPT',
+        async ({ verificationStatus, shouldDisablePrompt }) => {
             const signingKey = await generateKeys('signing', message.Sender.Address);
 
             const messageVerification = {
                 signingPublicKey: signingKey.publicKeys[0],
-                senderPinnedKeys: [...senderKey.publicKeys] as PublicKeyReference[],
-                senderPinnableKeys: [...signingKey.publicKeys] as PublicKeyReference[],
-                verificationStatus: MAIL_VERIFICATION_STATUS.NOT_VERIFIED,
+                verificationStatus,
             } as MessageVerification;
 
-            await setup(message, messageVerification);
+            await setup(message, messageVerification, false, true);
             screen.getByTestId('extra-pin-key:banner');
             // Expected text is displayed
-            screen.getByText('This message is signed by a key that has not been trusted yet.');
+            screen.getByText("This sender's public key has not been trusted yet.");
 
-            await openTrustKeyModal();
-        });
+            // Test disable prompt button
+            if (shouldDisablePrompt) {
+                const updateSpy = jest.fn(() => Promise.resolve({}));
+                addApiMock('mail/v4/settings/promptpin', updateSpy, 'put');
 
-        it(`should not render the banner when prompt key pinning is PIN_UNSEEN but the signature is invalid (ktEnabled=${ktEnabled})`, async () => {
-            const senderKey = await generateKeys('sender', message.Sender.Address);
+                const disableAutopromtButton = screen.getByText('Never show');
+                fireEvent.click(disableAutopromtButton);
+                await tick();
+
+                expect(updateSpy).toHaveBeenCalled();
+            } else {
+                // Open trust public key modal and trust the key
+                await openTrustKeyModal();
+            }
+        }
+    );
+
+    // PIN_UNSEEN
+    it('should render the banner when prompt key pinning is PIN_UNSEEN', async () => {
+        const senderKey = await generateKeys('sender', message.Sender.Address);
+        const signingKey = await generateKeys('signing', message.Sender.Address);
+
+        const messageVerification = {
+            signingPublicKey: signingKey.publicKeys[0],
+            senderPinnedKeys: [...senderKey.publicKeys] as PublicKeyReference[],
+            senderPinnableKeys: [...signingKey.publicKeys] as PublicKeyReference[],
+            verificationStatus: MAIL_VERIFICATION_STATUS.NOT_VERIFIED,
+        } as MessageVerification;
+
+        await setup(message, messageVerification);
+        screen.getByTestId('extra-pin-key:banner');
+        // Expected text is displayed
+        screen.getByText('This message is signed by a key that has not been trusted yet.');
+
+        await openTrustKeyModal();
+    });
+
+    it('should not render the banner when prompt key pinning is PIN_UNSEEN but the signature is invalid', async () => {
+        const senderKey = await generateKeys('sender', message.Sender.Address);
+        const signingKey = await generateKeys('signing', message.Sender.Address);
+
+        const messageVerification = {
+            signingPublicKey: signingKey.publicKeys[0],
+            senderPinnedKeys: [...senderKey.publicKeys] as PublicKeyReference[],
+            verificationStatus: MAIL_VERIFICATION_STATUS.SIGNED_AND_INVALID,
+        } as MessageVerification;
+
+        await setup(message, messageVerification);
+        expect(screen.queryByTestId('extra-pin-key:banner')).toBeNull();
+    });
+
+    // PIN_ATTACHED_SIGNING
+    it.each`
+        verificationStatus
+        ${MAIL_VERIFICATION_STATUS.NOT_VERIFIED}
+        ${MAIL_VERIFICATION_STATUS.SIGNED_AND_INVALID}
+    `('should render the banner when prompt key pinning is PIN_ATTACHED_SIGNING', async ({ verificationStatus }) => {
+        const signingKey = await generateKeys('signing', message.Sender.Address);
+
+        const messageVerification = {
+            signingPublicKey: signingKey.publicKeys[0],
+            attachedPublicKeys: [...signingKey.publicKeys] as PublicKeyReference[],
+            verificationStatus,
+        } as MessageVerification;
+
+        await setup(message, messageVerification);
+        screen.getByTestId('extra-pin-key:banner');
+        // Expected text is displayed
+        screen.getByText('This message is signed by the key attached, that has not been trusted yet.');
+
+        await openTrustKeyModal();
+    });
+
+    // PIN_ATTACHED
+    it.each`
+        verificationStatus                             | hasSigningKey
+        ${MAIL_VERIFICATION_STATUS.NOT_SIGNED}         | ${false}
+        ${MAIL_VERIFICATION_STATUS.NOT_SIGNED}         | ${true}
+        ${MAIL_VERIFICATION_STATUS.NOT_VERIFIED}       | ${false}
+        ${MAIL_VERIFICATION_STATUS.SIGNED_AND_INVALID} | ${false}
+    `(
+        'should render the banner when prompt key pinning is PIN_ATTACHED',
+        async ({ verificationStatus, hasSigningKey }) => {
             const signingKey = await generateKeys('signing', message.Sender.Address);
 
             const messageVerification = {
-                signingPublicKey: signingKey.publicKeys[0],
-                senderPinnedKeys: [...senderKey.publicKeys] as PublicKeyReference[],
-                verificationStatus: MAIL_VERIFICATION_STATUS.SIGNED_AND_INVALID,
+                signingPublicKey: hasSigningKey ? signingKey.publicKeys[0] : undefined,
+                attachedPublicKeys: [...signingKey.publicKeys] as PublicKeyReference[],
+                verificationStatus,
             } as MessageVerification;
 
-            await setup(message, messageVerification);
-            expect(screen.queryByTestId('extra-pin-key:banner')).toBeNull();
-        });
+            await setup(message, messageVerification, false, true);
+            screen.getByTestId('extra-pin-key:banner');
+            // Expected text is displayed
+            screen.getByText('An unknown public key has been detected for this recipient.');
 
-        // PIN_ATTACHED_SIGNING
-        it.each`
-            verificationStatus
-            ${MAIL_VERIFICATION_STATUS.NOT_VERIFIED}
-            ${MAIL_VERIFICATION_STATUS.SIGNED_AND_INVALID}
-        `(
-            `should render the banner when prompt key pinning is PIN_ATTACHED_SIGNING (ktEnabled=${ktEnabled})`,
-            async ({ verificationStatus }) => {
-                const signingKey = await generateKeys('signing', message.Sender.Address);
-
-                const messageVerification = {
-                    signingPublicKey: signingKey.publicKeys[0],
-                    attachedPublicKeys: [...signingKey.publicKeys] as PublicKeyReference[],
-                    verificationStatus,
-                } as MessageVerification;
-
-                await setup(message, messageVerification);
-                screen.getByTestId('extra-pin-key:banner');
-                // Expected text is displayed
-                screen.getByText('This message is signed by the key attached, that has not been trusted yet.');
-
-                await openTrustKeyModal();
-            }
-        );
-
-        // PIN_ATTACHED
-        it.each`
-            verificationStatus                             | hasSigningKey
-            ${MAIL_VERIFICATION_STATUS.NOT_SIGNED}         | ${false}
-            ${MAIL_VERIFICATION_STATUS.NOT_SIGNED}         | ${true}
-            ${MAIL_VERIFICATION_STATUS.NOT_VERIFIED}       | ${false}
-            ${MAIL_VERIFICATION_STATUS.SIGNED_AND_INVALID} | ${false}
-        `(
-            `should render the banner when prompt key pinning is PIN_ATTACHED (ktEnabled=${ktEnabled})`,
-            async ({ verificationStatus, hasSigningKey }) => {
-                const signingKey = await generateKeys('signing', message.Sender.Address);
-
-                const messageVerification = {
-                    signingPublicKey: hasSigningKey ? signingKey.publicKeys[0] : undefined,
-                    attachedPublicKeys: [...signingKey.publicKeys] as PublicKeyReference[],
-                    verificationStatus,
-                } as MessageVerification;
-
-                await setup(message, messageVerification, false, true);
-                screen.getByTestId('extra-pin-key:banner');
-                // Expected text is displayed
-                screen.getByText('An unknown public key has been detected for this recipient.');
-
-                await openTrustKeyModal();
-            }
-        );
-    });
+            await openTrustKeyModal();
+        }
+    );
 });
