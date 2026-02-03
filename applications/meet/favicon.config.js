0912eb2f6bd42a5aaa757653febe509c61808363
Kévin Ta <kevin.ta@proton.ch>
2026-02-03 22:01:00 +0100

    Update favicon configuration for Meet

--- applications/meet/favicon.config.js
+++ applications/meet/favicon.config.js
@@ -3,11 +3,13 @@ const path = require('path');
 const url = 'https://meet.proton.me/';
 
 module.exports = {
-    logo: path.resolve('./src/favicon.png'),
-    logoMaskable: path.resolve('./src/favicon.png'),
+    logo: path.resolve('./src/favicon.svg'),
+    logoMaskable: path.resolve('./src/icon-maskable.svg'),
     favicons: {
         appName: 'Proton Meet',
-        appDescription: 'Proton Meet',
+        appDescription:
+            'Proton Meet is a video conferencing tool that brings industry-leading encryption to your online calls, ensuring your most sensitive meetings remain private.',
     },
     url,
+    locales: Object.keys(require('./locales/config/locales.json')),
 };
