c508310aa19a27bd1ad577f273f0347e00cbf686
Edvin CANDON <edvin.candon@proton.ch>
2026-02-04 15:10:53 +0100

    Define base user events v2 requests
    [IDTEAM-4638]

---
+++ packages/pass/lib/events/v2/user-events.requests.ts
@@ -0,0 +1,14 @@
+import { api } from '@proton/pass/lib/api/api';
+import type { SyncEventListOutput } from '@proton/pass/types';
+
+/** Retrieves the latest user event ID */
+export const getUserEventLatestID = async (): Promise<string> => {
+    const res = await api({ url: `pass/v1/user/sync_event`, method: 'get' });
+    return res.EventID;
+};
+
+/** Retrieves all user events since `lastEventID` */
+export const getUserEventsSince = async (lastEventID: string): Promise<SyncEventListOutput> => {
+    const res = await api({ url: `pass/v1/user/sync_event/${lastEventID}`, method: 'get' });
+    return res.Events;
+};
