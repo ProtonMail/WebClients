3090956005c63c3590d8d8f8dc024f02e781d67a
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2026-02-05 16:46:51 +0100

    Fix Proton Unlimited displayed as Pass Trial
    [IDTEAM-5697]

--- packages/pass/lib/user/user.plan.ts
+++ packages/pass/lib/user/user.plan.ts
@@ -1,11 +1,14 @@
 import { type MaybeNull, type PassPlanResponse, PlanType } from '@proton/pass/types';
 import { UserPassPlan } from '@proton/pass/types/api/plan';
 import { getEpoch } from '@proton/pass/utils/time/epoch';
+import { PLANS } from '@proton/payments/index';
 
 export const getPassPlan = (plan?: MaybeNull<PassPlanResponse>): UserPassPlan => {
     switch (plan?.Type) {
         case PlanType.PLUS:
-            return plan.TrialEnd && getEpoch() < plan.TrialEnd ? UserPassPlan.TRIAL : UserPassPlan.PLUS;
+            return plan.InternalName === PLANS.PASS && plan.TrialEnd && getEpoch() < plan.TrialEnd
+                ? UserPassPlan.TRIAL
+                : UserPassPlan.PLUS;
         case PlanType.BUSINESS:
             return UserPassPlan.BUSINESS;
         default: {
