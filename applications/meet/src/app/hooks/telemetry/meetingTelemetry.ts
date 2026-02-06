56a8b01e1a2ed6004b4934155d596aa027ea755a
István Barna <istvan.barna@proton.ch>
2026-02-06 22:58:57 +0000

    Removing telemetry

--- applications/meet/src/app/hooks/telemetry/meetingTelemetry.ts
+++
@@ -1,7 +0,0 @@
-import { telemetry } from '@proton/shared/lib/telemetry';
-
-import type { ParticipantQualityStats } from './types';
-
-export const logParticipantQuality = (qualityStats: ParticipantQualityStats) => {
-    telemetry.sendCustomEvent('meet-participant-quality', qualityStats);
-};
