cc0c3336ec6e3254e82adf4401f7cbd11a852de9
Edvin CANDON <edvin.candon@proton.ch>
2026-02-06 11:23:07 +0100

    Fine-tune `itemsUpdated` action
    [IDTEAM-4638]

--- packages/pass/store/sagas/items/item-autofill.saga.ts
+++ packages/pass/store/sagas/items/item-autofill.saga.ts
@@ -2,7 +2,7 @@ import { put, takeEvery } from 'redux-saga/effects';
 
 import { parseItemRevision } from '@proton/pass/lib/items/item.parser';
 import { updateItemLastUseTime } from '@proton/pass/lib/items/item.requests';
-import { itemAutofilled, itemsEditEvent } from '@proton/pass/store/actions';
+import { itemAutofilled, itemsUpdated } from '@proton/pass/store/actions';
 import type { RootSagaOptions } from '@proton/pass/store/types';
 import type { ItemRevision, ItemRevisionContentsResponse } from '@proton/pass/types';
 import { logId, logger } from '@proton/pass/utils/logger';
@@ -14,7 +14,7 @@ function* itemAutofilledWorker({ onItemsUpdated }: RootSagaOptions, { payload: {
 
         const encryptedItem: ItemRevisionContentsResponse = yield updateItemLastUseTime(shareId, itemId);
         const item: ItemRevision = yield parseItemRevision(shareId, encryptedItem);
-        yield put(itemsEditEvent([item]));
+        yield put(itemsUpdated([item]));
     } catch (err: unknown) {
         logger.warn(`[Item::Autofill] lastUseTime update failed for item ${logId(itemId)} on share ${logId(shareId)}`, err);
     }
