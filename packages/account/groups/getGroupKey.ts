3b40a0f9bb2be501f928bef5c70aa6504d7b76c9
Pedro Lavarinhas Amaro <pedro.lavarinhas.amaro@proton.ch>
2026-02-09 13:47:52 +0100

    fix: Use user key for group address key as group owner

--- packages/account/groups/getGroupKey.ts
+++ packages/account/groups/getGroupKey.ts
@@ -3,6 +3,7 @@ import type { ThunkAction, UnknownAction } from '@reduxjs/toolkit';
 import type { AddressesState } from '@proton/account/addresses';
 import type { KtState } from '@proton/account/kt';
 import type { UserKeysState } from '@proton/account/userKeys';
+import { userKeysThunk } from '@proton/account/userKeys';
 import type { ProtonThunkArguments } from '@proton/redux-shared-store-types';
 import type { Address, DecryptedAddressKey } from '@proton/shared/lib/interfaces';
 import { getDecryptedGroupAddressKey } from '@proton/shared/lib/keys/groupKeys';
@@ -17,13 +18,33 @@ export const getGroupKey = ({
 }): ThunkAction<Promise<DecryptedAddressKey>, RequiredState, ProtonThunkArguments, UnknownAction> => {
     return async (dispatch) => {
         const organizationKey = await dispatch(organizationKeyThunk());
-        if (organizationKey.privateKey === undefined) {
-            throw new Error('Organization key is undefined');
+        if (organizationKey.privateKey) {
+            // Use organization key if we have it
+            const forwarderKey = await getDecryptedGroupAddressKey(groupAddress.Keys, organizationKey.privateKey);
+
+            if (!forwarderKey) {
+                throw new Error('Missing group address keys');
+            }
+
+            return forwarderKey;
         }
-        const forwarderKey = await getDecryptedGroupAddressKey(groupAddress.Keys, organizationKey.privateKey);
+
+        // If we don't have the organization key, use the user key
+        const userKeys = await dispatch(userKeysThunk());
+        if (!userKeys) {
+            throw new Error('No organization keys nor user keys available');
+        }
+
+        const primaryUserKey = userKeys[0];
+        const forwarderKey = await getDecryptedGroupAddressKey(groupAddress.Keys, primaryUserKey.privateKey, {
+            required: true,
+            value: 'account.key-token.address',
+        });
+
         if (!forwarderKey) {
             throw new Error('Missing group address keys');
         }
+
         return forwarderKey;
     };
 };
