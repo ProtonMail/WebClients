4f3d57514e8aeb88c8863e63b2c3f01a2f72ccce
Will Hsu <will.hsu@proton.ch>
2026-02-12 20:47:01 +0800

    Trigger useBridges() in useMeetingInitialisation to make sure we have useParticipantEvents()

--- applications/meet/src/app/hooks/useMeetingInitialisation.ts
+++ applications/meet/src/app/hooks/useMeetingInitialisation.ts
@@ -1,8 +1,10 @@
+import { useBridges } from './bridges/useBridges';
 import { useMeetingTimeout } from './useMeetingTimeout';
 import { usePaginationSizeUpdates } from './usePaginationSizeUpdates';
 import { useScreenShareUpdates } from './useScreenShareUpdates';
 
 export const useMeetingInitialisation = () => {
+    useBridges();
     usePaginationSizeUpdates();
     useScreenShareUpdates();
     useMeetingTimeout();
