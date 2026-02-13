d052260eb9d611f825de0f663dc79ebd4d49d0ca
Nico Hoffmann <nicolas.hoffmann@proton.ch>
2026-02-13 08:59:32 +0000

    Unify logo in modal/interface

--- applications/meet/src/app/components/TranslucentModal/TranslucentModal.tsx
+++ applications/meet/src/app/components/TranslucentModal/TranslucentModal.tsx
@@ -1,6 +1,6 @@
 import { ModalTwo } from '@proton/components/index';
 import { MEET_APP_NAME } from '@proton/shared/lib/constants';
-import brand from '@proton/styles/assets/img/meet/brand-dual-colors.png';
+import brand from '@proton/styles/assets/img/meet/brand-dual-colors.svg';
 
 import { CloseButton } from '../../atoms/CloseButton/CloseButton';
 
@@ -18,7 +18,7 @@ export const TranslucentModal = ({
     <ModalTwo open={open} onClose={onClose} rootClassName="translucent-modal" size="full" fullscreen>
         <div className="w-full meet-container-padding-x overflow-y-auto h-full">
             <div className="flex justify-space-between items-center pt-5 pb-5">
-                <img src={brand} alt={MEET_APP_NAME} className="h-custom" style={{ '--h-custom': '3rem' }} />
+                <img src={brand} alt={MEET_APP_NAME} className="h-custom" style={{ '--h-custom': '2.5rem' }} />
                 <CloseButton onClose={onClose} />
             </div>
             <div className="translucent-modal_container flex">
