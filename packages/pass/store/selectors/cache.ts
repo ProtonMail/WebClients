3c83a825bc7358e3c2ddb4153fb62c058ecccaf7
Edvin CANDON <edvin.candon@proton.ch>
2026-02-20 14:10:16 +0100

    Adapt state caching based on sync strategy
    [IDTEAM-4638]

--- packages/pass/store/selectors/cache.ts
+++ packages/pass/store/selectors/cache.ts
@@ -1,22 +1,29 @@
+import { SyncStrategy } from '@proton/pass/lib/events/types';
 import { asIfNotOptimistic } from '@proton/pass/store/optimistic/selectors/select-is-optimistic';
 import { getInitialPATState, reducerMap } from '@proton/pass/store/reducers';
+import { selectSyncStrategy } from '@proton/pass/store/selectors/settings';
 import type { State } from '@proton/pass/store/types';
 import { objectFilter } from '@proton/pass/utils/object/filter';
 import { partialMerge } from '@proton/pass/utils/object/merge';
 
 export const selectCachableState = (state: State) => {
     const cachable = asIfNotOptimistic(state, reducerMap);
+    const legacySync = selectSyncStrategy(state) === SyncStrategy.LEGACY;
 
     /** Remove "hot" data that should never be cached.
      * These should be revalidated on boot or via polling. */
-    cachable.monitor = null;
     cachable.items.secureLinks = {};
-    cachable.invites = {};
     cachable.user = partialMerge(cachable.user, { devices: [] });
     cachable.access = {};
     cachable.alias = { aliasDetails: {}, aliasOptions: null, mailboxes: null };
     cachable.files = {};
     cachable.accessTokens = getInitialPATState();
+
+    /** V2 user events provide granular invite updates, safe to cache */
+    if (legacySync) {
+        cachable.invites = {};
+        cachable.monitor = null;
+    }
 
     /** Filter stale request metadata and optimisticIds */
     cachable.items.byOptimisticId = {};
