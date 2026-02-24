8e018bf2b588efdfedacf80c9bc05e1548c858ae
Edvin CANDON <edvin.candon@proton.ch>
2026-02-24 15:03:19 +0100

    Fix user events channel stopping on error
    [IDTEAM-4638]

--- packages/pass/store/sagas/events/v2/channel.user-events.ts
+++ packages/pass/store/sagas/events/v2/channel.user-events.ts
@@ -14,28 +14,32 @@ const CHANNEL_ID = 'Polling::UserEventsV2';
 export function* userEventsChannel(_: Api, options: RootSagaOptions): Generator {
     try {
         while (true) {
-            /** 1. Ensure valid `userEventID` */
-            const lastUserEventID: MaybeNull<Id> = yield select(selectLatestUserEventId);
+            try {
+                /** 1. Ensure valid `userEventID` */
+                const lastUserEventID: MaybeNull<Id> = yield select(selectLatestUserEventId);
 
-            if (!lastUserEventID) {
-                logger.warn(`[${CHANNEL_ID}] No userEventID, skipping poll`);
-                yield delay(options.getPollingInterval());
-                continue;
+                if (!lastUserEventID) {
+                    logger.warn(`[${CHANNEL_ID}] No userEventID, skipping poll`);
+                    yield delay(options.getPollingInterval());
+                    continue;
+                }
+
+                /** 2. Fetch events since `userEventID` */
+                logger.debug(`[${CHANNEL_ID}] Polling events since ${logId(lastUserEventID)}`);
+                const events: SyncEventListOutput = yield call(getUserEventsSince, lastUserEventID);
+
+                /** 3. Process user events */
+                logger.debug(`[${CHANNEL_ID}] Processing events up to ${logId(events.LastEventID)} `);
+                const processed: boolean = yield call(processUserEvents, events, options);
+
+                /** 4. Update state with new eventID only if all events were processed */
+                if (processed) yield put(setUserEventID(events.LastEventID));
+
+                /** 5. If more events pending -> poll immediately */
+                if (processed && events.EventsPending) continue;
+            } catch (err) {
+                logger.warn(`[${CHANNEL_ID}] Channel error`, err);
             }
-
-            /** 2. Fetch events since `userEventID` */
-            logger.debug(`[${CHANNEL_ID}] Polling events since ${logId(lastUserEventID)}`);
-            const events: SyncEventListOutput = yield call(getUserEventsSince, lastUserEventID);
-
-            /** 3. Process user events */
-            logger.debug(`[${CHANNEL_ID}] Processing events up to ${logId(events.LastEventID)} `);
-            const processed: boolean = yield call(processUserEvents, events, options);
-
-            /** 4. Update state with new eventID only if all events were processed */
-            if (processed) yield put(setUserEventID(events.LastEventID));
-
-            /** 5. If more events pending -> poll immediately */
-            if (processed && events.EventsPending) continue;
 
             /** 6. Wait for next poll interval. `forcePollV2` short-circuits
              * the delay to trigger an immediate re-poll. If a poll is already
@@ -46,7 +50,6 @@ export function* userEventsChannel(_: Api, options: RootSagaOptions): Generator 
                 force: take(forcePollV2.match),
             });
         }
-    } catch {
     } finally {
         if (yield cancelled()) logger.info(`[${CHANNEL_ID}] Channel cancelled`);
     }
