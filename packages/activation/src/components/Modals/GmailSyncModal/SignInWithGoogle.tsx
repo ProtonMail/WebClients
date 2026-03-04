cfecef618c4e5def7ed6975c6ef85e20f1065078
Romain Sanchez <romain.sanchez@proton.ch>
2026-03-04 14:26:46 +0000

    Update add BYOE address modal

--- packages/activation/src/components/Modals/GmailSyncModal/SignInWithGoogle.tsx
+++ packages/activation/src/components/Modals/GmailSyncModal/SignInWithGoogle.tsx
@@ -12,9 +12,17 @@ interface Props {
     disabled?: boolean;
     onClick: () => void;
     reduceHeight?: boolean;
+    buttonText?: string;
 }
 
-const SignInWithGoogle = ({ loading, disabled, onClick, fullWidth, reduceHeight }: Props) => {
+const SignInWithGoogle = ({
+    loading,
+    disabled,
+    onClick,
+    fullWidth,
+    reduceHeight,
+    buttonText = c('Gmail forwarding').t`Sign in with Google`,
+}: Props) => {
     const { viewportWidth } = useActiveBreakpoint();
     const buttonHeight = !viewportWidth['<=small'] && reduceHeight ? '2.5rem' : '3rem';
     const googleLogoSize = !viewportWidth['<=small'] && reduceHeight ? '2rem' : '2.5rem';
@@ -27,17 +35,16 @@ const SignInWithGoogle = ({ loading, disabled, onClick, fullWidth, reduceHeight 
             loading={loading}
             fullWidth={fullWidth}
             disabled={loading || disabled}
-            className="flex items-center p-1 rounded google-button"
+            className="flex items-center p-2 rounded-lg google-button"
             style={{ '--h-custom': buttonHeight }}
         >
             <span
-                className="bg-norm rounded-sm flex justify-center items-center w-custom min-h-custom"
+                className="bg-norm rounded flex justify-center items-center w-custom min-h-custom"
                 style={{ '--w-custom': googleLogoSize, '--min-h-custom': googleLogoSize }}
             >
                 <img src={googleLogo} alt="" width={googleImageSize} height={googleImageSize} aria-hidden="true" />
             </span>
-            <span className="text-semibold flex-1 text-left pl-4 pr-8">{c('Gmail forwarding')
-                .t`Sign in with Google`}</span>
+            <span className="text-semibold flex-1 text-left pl-4 pr-8">{buttonText}</span>
         </Button>
     );
 };
