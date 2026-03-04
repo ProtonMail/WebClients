af482879579dfe4cfc4c78f01a568193a74edfcd
Elena Atencio <elena.atencio@proton.ch>
2026-03-04 11:05:48 +0000

    start on removing top navbar and integrating logo, app switcher, and new ghost chat button into sidebar for V2

--- applications/lumo/src/app/layouts/MainLayout.tsx
+++ applications/lumo/src/app/layouts/MainLayout.tsx
@@ -1,13 +1,13 @@
 import { type ReactNode, lazy } from 'react';
 
+import HighLoadWarning from '../components/Notifications/HighLoadWarning';
 import { useKeyboardShortcuts } from '../hooks/useKeyboardShortcuts';
 import { GhostChatProvider } from '../providers/GhostChatProvider';
 import { useIsGuest } from '../providers/IsGuestProvider';
 import { SearchModalProvider, useSearchModal } from '../providers/SearchModalProvider';
-import { SidebarProvider, useSidebar } from '../providers/SidebarProvider';
-import HighLoadWarning from '../components/Notifications/HighLoadWarning';
-import { PrivateHeader } from './header/PrivateHeader';
-import { PublicHeader } from './header/PublicHeader';
+import { SidebarProvider } from '../providers/SidebarProvider';
+// import { PrivateHeader } from './header/PrivateHeader';
+// import { PublicHeader } from './header/PublicHeader';
 import LumoSidebar from './sidebar/LumoSidebar';
 
 export type ActivePanel = 'chatHistory' | 'favoriteChats' | null;
@@ -17,20 +17,20 @@ interface Props {
 }
 
 const MainLayoutContent = ({ children }: Props) => {
-    const { isSmallScreen } = useSidebar();
+    // const { isSmallScreen } = useSidebar();
     const { openSearchModal } = useSearchModal();
-    const isGuest = useIsGuest();
+    // const isGuest = useIsGuest();
 
 
     // Set up keyboard shortcuts
     useKeyboardShortcuts({ onOpenSearch: openSearchModal });
-    const HeaderComponent = isGuest ? PublicHeader : PrivateHeader;
+    // const HeaderComponent = isGuest ? PublicHeader : PrivateHeader;
 
     return (
         <div className="relative reset4print flex flex-row h-full w-full overflow-hidden">
             <div className="absolute top-0 left-0 w-full h-full no-print">
                 <div className="flex flex-column flex-nowrap h-full flex-1 reset4print">
-                    {!isSmallScreen && <HeaderComponent />}
+                    {/* {!isSmallScreen && <HeaderComponent />} */}
                     <div className="flex flex-row flex-nowrap flex-1 min-h-0 w-full reset4print relative">
                         <LumoSidebar />
                         <main className="flex-1 flex flex-column flex-nowrap border-top border-weak reset4print">
