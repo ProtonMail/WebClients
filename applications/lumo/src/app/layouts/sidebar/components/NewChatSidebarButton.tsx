b6712e38204097664b5c52f1da2576f81301a251
Elena Atencio <elena.atencio@proton.ch>
2026-03-05 12:07:38 +0000

    add useSidebarVisibility hoook and split components into own files to improve readability of LumoSidebar component

---
+++ applications/lumo/src/app/layouts/sidebar/components/NewChatSidebarButton.tsx
@@ -0,0 +1,50 @@
+import { useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { c } from 'ttag';
+
+import { metaKey } from '@proton/shared/lib/helpers/browser';
+
+import { GuestChatDisclaimerModal } from '../../../components/Modals/GuestChatDisclaimerModal';
+import { useGuestChatHandler } from '../../../hooks/useGuestChatHandler';
+import { useGhostChat } from '../../../providers/GhostChatProvider';
+import { useIsGuest } from '../../../providers/IsGuestProvider';
+import { SidebarItem } from './SidebarItem';
+
+interface Props {
+    showText: boolean;
+    isSmallScreen: boolean;
+}
+
+export const NewChatSidebarButton = ({ showText, isSmallScreen }: Props) => {
+    const isGuest = useIsGuest();
+    const history = useHistory();
+    const { setGhostChatMode } = useGhostChat();
+    const { handleGuestClick, handleDisclaimerClose, disclaimerModalProps } = useGuestChatHandler();
+
+    const handleNewChat = useCallback(() => {
+        setGhostChatMode(false);
+        history.push('/');
+    }, [setGhostChatMode, history]);
+
+    const handleModalClose = useCallback(() => {
+        handleDisclaimerClose();
+        handleNewChat();
+    }, [handleNewChat, handleDisclaimerClose]);
+
+    return (
+        <>
+            <SidebarItem
+                icon="pen-square"
+                label={c('collider_2025:Button').t`New chat`}
+                onClick={isGuest ? handleGuestClick : handleNewChat}
+                showText={showText}
+                shortcut={!isSmallScreen ? `${metaKey}+J` : undefined}
+                showShortcutOnHover={true}
+            />
+            {isGuest && disclaimerModalProps.render && (
+                <GuestChatDisclaimerModal onClick={handleModalClose} {...disclaimerModalProps.modalProps} />
+            )}
+        </>
+    );
+};
