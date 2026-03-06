6e2db2be75455727ed0acf91e76ded63ef7a0d0b
Elena Atencio <elena.atencio@proton.ch>
2026-03-06 08:59:03 +0000

    update lumo plus icon svg color for collaposed sidebar

--- applications/lumo/src/app/components/CustomPlusIcon/CustomPlusIcon.tsx
+++ applications/lumo/src/app/components/CustomPlusIcon/CustomPlusIcon.tsx
@@ -18,23 +18,8 @@ const CustomPlusIcon = ({ size = 16, className, style }: CustomPlusIconProps) =>
     >
         <path
             d="M9.85156 6.40332H16L15.3652 9.59668H9.21582L7.83789 16H4.77051L6.14941 9.59668H0L0.634766 6.40332H6.78418L8.16309 0H11.2295L9.85156 6.40332Z"
-            fill="url(#paint0_linear_13209_13095)"
+            fill="#6D4AFF"
         />
-        <defs>
-            <linearGradient
-                id="paint0_linear_13209_13095"
-                x1="-0.0999997"
-                y1="23.4329"
-                x2="20.0816"
-                y2="15.8297"
-                gradientUnits="userSpaceOnUse"
-            >
-                <stop offset="0.157704" stopColor="#FFAC2E" />
-                <stop offset="0.383967" stopColor="#DD5DCC" />
-                <stop offset="0.759336" stopColor="#1BA3FD" />
-                <stop offset="0.998442" stopColor="var(--lumo-plus-logo-plus-sign-text)" />
-            </linearGradient>
-        </defs>
     </svg>
 );
 
