4c7e382054fa62a6fb26263edbffe40fbe30596e
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2026-03-06 13:22:10 +0000

    Merge branch 'feature/DRVWEB-5117-spring-sale-2026-coupon-configuration' into 'main'
    [DRVWEB-5117] Spring sale 2026 coupon configuration
    
    See merge request web/clients!23054

---
+++ packages/components/containers/payments/subscription/coupon-config/springSale2026.tsx
@@ -0,0 +1,27 @@
+import { c } from 'ttag';
+
+import Price from '@proton/components/components/price/Price';
+import { getCheckoutUi } from '@proton/payments/core/checkout';
+import { COUPON_CODES, CYCLE } from '@proton/payments/core/constants';
+
+import type { CouponConfig } from './interface';
+
+export const springSale2026Config: CouponConfig = {
+    coupons: [COUPON_CODES.MAR26SALE, COUPON_CODES.MAR26OFFER],
+    checkoutSubtitle: () => c('Title').t`Spring Sale`,
+    payCTA: () => c('Label').t`Get the deal`,
+    hidden: true,
+    cyclePriceComparePosition: 'before',
+    cyclePriceCompare: ({ suffix }, config) => {
+        const checkout = getCheckoutUi(config);
+
+        return (
+            <Price className="mr-2 text-strike" currency={checkout.currency} suffix={suffix}>
+                {checkout.withoutDiscountPerMonth}
+            </Price>
+        );
+    },
+    availableCycles: [CYCLE.YEARLY],
+    disableCurrencySelector: true,
+    hideLumoAddonBanner: true,
+};
