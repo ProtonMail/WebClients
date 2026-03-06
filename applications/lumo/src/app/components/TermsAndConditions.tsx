47725d47c9d03e0c70f377c117ba68e16505fb5d
Elena Atencio <elena.atencio@proton.ch>
2026-03-06 16:02:13 +0000

    move terms and conditions to underneath composer for guest users

--- applications/lumo/src/app/components/TermsAndConditions.tsx
+++ applications/lumo/src/app/components/TermsAndConditions.tsx
@@ -6,7 +6,6 @@ import clsx from '@proton/utils/clsx';
 
 const TermsAndConditions = ({ className }: { className?: string }) => {
     return (
-        // <div className="fixed bottom-0 left-0 right-0 text-center py-5 z-1">
         <p className={clsx('text-sm color-weak', className)}>
             {c('collider_2025: Legal disclaimer').t`By using ${LUMO_SHORT_APP_NAME}, you agree to our`}{' '}
             <InlineLinkButton
@@ -24,7 +23,6 @@ const TermsAndConditions = ({ className }: { className?: string }) => {
             </InlineLinkButton>
             .
         </p>
-        // </div>
     );
 };
 
