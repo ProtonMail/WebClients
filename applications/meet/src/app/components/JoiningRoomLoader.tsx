d40c071e692bb33b20ae7bdea9fe7ca0d1c30fdc
Will Hsu <will.hsu@proton.ch>
2026-03-16 11:51:50 +0800

    [Meet] Add log and show warning to user when livekit.connect take longer than expected

--- applications/meet/src/app/components/JoiningRoomLoader.tsx
+++ applications/meet/src/app/components/JoiningRoomLoader.tsx
@@ -4,9 +4,11 @@ import { CircleLoader } from '@proton/atoms/CircleLoader/CircleLoader';
 
 interface JoiningRoomLoaderProps {
     participantCount: number | null;
+    header?: string;
+    subtitle?: string;
 }
 
-export const JoiningRoomLoader = ({ participantCount }: JoiningRoomLoaderProps) => {
+export const JoiningRoomLoader = ({ participantCount, header, subtitle }: JoiningRoomLoaderProps) => {
     const participantsLoaded = participantCount !== null;
 
     const joiningRoomTitles: { heading: (participantCount: number | null) => string; description: string }[] = [
@@ -31,17 +33,22 @@ export const JoiningRoomLoader = ({ participantCount }: JoiningRoomLoaderProps) 
 
     const titleIndex = participantsLoaded ? 1 : 0;
 
+    const displayHeader = header ?? joiningRoomTitles[titleIndex].heading(participantCount);
+    const displaySubtitle = subtitle ?? joiningRoomTitles[titleIndex].description;
+
     return (
         <div
             className="flex flex-column flex-nowrap items-center justify-center md:justify-start md:h-custom md:w-custom pt-6"
             style={{ '--md-w-custom': '24.625rem', '--md-h-custom': '24rem' }}
         >
             <h2 className="mt-6 mb-4 text-center text-semibold" aria-live="polite" aria-atomic="true">
-                {joiningRoomTitles[titleIndex].heading(participantCount)}
+                {displayHeader}
             </h2>
-            <div className="color-weak mb-8 text-center" aria-live="polite" aria-atomic="true">
-                {joiningRoomTitles[titleIndex].description}
-            </div>
+            {displaySubtitle && (
+                <div className="color-weak mb-8 text-center" aria-live="polite" aria-atomic="true">
+                    {displaySubtitle}
+                </div>
+            )}
             <CircleLoader
                 className="color-primary w-custom h-custom"
                 style={{ '--w-custom': '5.3rem', '--h-custom': '5.3rem', '--stroke-width': 1.3 }}
