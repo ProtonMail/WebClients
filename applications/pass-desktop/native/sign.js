02e9d93096d578cbbe0c6a298d6955b8084cfa87
Edvin CANDON <edvin.candon@proton.ch>
2026-03-17 14:20:13 +0100

    Sign native binaries in pass desktop windows pipeline

---
+++ applications/pass-desktop/native/sign.js
@@ -0,0 +1,19 @@
+const { execSync } = require('child_process');
+const { readdirSync } = require('fs');
+const path = require('path');
+
+if (process.platform === 'win32') {
+    const signtoolArgs = process.env.SQUIRREL_SIGNTOOL_ARGS;
+    if (!signtoolArgs) throw new Error('SQUIRREL_SIGNTOOL_ARGS is not set');
+
+    const sign = (file) => {
+        console.warn(`Signing ${file}...`);
+        execSync(`signtool sign ${signtoolArgs} "${file}"`, { stdio: 'inherit' });
+    };
+
+    const nodeFiles = readdirSync('.').filter((f) => f.endsWith('.node'));
+    if (nodeFiles.length === 0) throw new Error('No .node files found');
+
+    for (const file of nodeFiles) sign(file);
+    sign(path.join('..', 'assets', 'proton_pass_nm_host.exe'));
+}
