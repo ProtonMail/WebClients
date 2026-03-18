56991c2d455b9afef55f72e32187167625a821af
Elena Atencio <elena.atencio@proton.ch>
2026-03-18 17:24:50 -0600

    design review fix - favorites and chat history should be collapsible like projects in sidebar

--- applications/lumo/src/app/layouts/sidebar/ProjectsSidebarSection.tsx
+++ applications/lumo/src/app/layouts/sidebar/ProjectsSidebarSection.tsx
@@ -1,12 +1,11 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 
-import { clsx } from 'clsx';
 import { c } from 'ttag';
 
 import { Tooltip } from '@proton/atoms/Tooltip/Tooltip';
 import { Icon, useModalStateObject } from '@proton/components';
-import { IcFolder } from '@proton/icons/icons/IcFolder';
+import { IcFolderOpen } from '@proton/icons/icons/IcFolderOpen';
 import { IcPlus } from '@proton/icons/icons/IcPlus';
 
 import { DismissedFeaturePill } from '../../components/DismissedFeaturePill';
@@ -17,6 +16,7 @@ import { ProjectLimitModal } from '../../features/projects/modals/ProjectLimitMo
 import { useIsGuest } from '../../providers/IsGuestProvider';
 import { useLumoPlan } from '../../providers/LumoPlanProvider';
 import { useSidebar } from '../../providers/SidebarProvider';
+import { CollapsibleSidebarSection } from './components/CollapsibleSidebarSection';
 import { SidebarNavList } from './components/SidebarNavList';
 
 import './ProjectsSidebarSection.scss';
@@ -27,42 +27,20 @@ interface ProjectsSidebarSectionProps {
 }
 
 export const ProjectsSidebarSection = ({ showText, onItemClick }: ProjectsSidebarSectionProps) => {
-    const [isExpanded, setIsExpanded] = useState(true);
-    const [isHovered, setIsHovered] = useState(false);
     const projects = useProjects();
     const history = useHistory();
     const location = useLocation();
 
     const isGuest = useIsGuest();
     const { hasLumoPlus } = useLumoPlan();
-    const { isCollapsed, isVisible, isSmallScreen, toggle } = useSidebar();
+    const { isCollapsed, toggle } = useSidebar();
     const newProjectModal = useModalStateObject();
     const projectLimitModal = useModalStateObject();
-
-    useEffect(() => {
-        if (isSmallScreen) {
-            setIsExpanded(isVisible);
-        } else if (isCollapsed) {
-            setIsExpanded(false);
-        } else {
-            const timer = setTimeout(() => {
-                setIsExpanded(true);
-            }, 200);
-            return () => clearTimeout(timer);
-        }
-    }, [isCollapsed, isVisible, isSmallScreen]);
 
     const currentProjectId = useMemo(() => {
         const match = location.pathname.match(/^\/projects\/([^/]+)/);
         return match ? match[1] : null;
     }, [location.pathname]);
-
-    const isProjectsPage = location.pathname === '/projects';
-
-    const handleToggle = (e: React.MouseEvent) => {
-        e.stopPropagation();
-        setIsExpanded(!isExpanded);
-    };
 
     const handleProjectsHeaderClick = () => {
         if (isGuest) {
@@ -90,15 +68,8 @@ export const ProjectsSidebarSection = ({ showText, onItemClick }: ProjectsSideba
         history.push('/projects');
     };
 
-    const handleCreateProject = () => {
-        console.log('handleCreateProject');
-        if (isGuest) {
-            if (onItemClick) {
-                onItemClick();
-            }
-            history.push('/projects');
-            return;
-        }
+    const handleCreateProject = (e: React.MouseEvent) => {
+        e.stopPropagation();
 
         if (!hasLumoPlus && projects.length >= 1) {
             projectLimitModal.openModal(true);
@@ -109,154 +80,97 @@ export const ProjectsSidebarSection = ({ showText, onItemClick }: ProjectsSideba
     };
 
     return (
-        <div className="projects-sidebar-section">
-            {isCollapsed ? (
-                <Tooltip title={c('collider_2025:Button').t`Projects`} originalPlacement="right">
-                    <button
-                        className="sidebar-item"
-                        onClick={handleProjectsHeaderClick}
-                        aria-label={c('collider_2025:Button').t`Projects`}
-                    >
-                        <div className="sidebar-item-icon">
-                            <IcFolder size={4} className="rtl:mirror" />
-                        </div>
-                    </button>
-                </Tooltip>
-            ) : (
-                <>
-                    {/* Projects Header - Grok-style with visible chevron and create button */}
-                    {isGuest ? (
-                        // Guest mode: simple button that navigates to projects
+        <>
+            {isGuest ? (
+                <div className="projects-sidebar-section">
+                    {isCollapsed ? (
                         <Tooltip title={c('collider_2025:Button').t`Projects`} originalPlacement="right">
                             <button
-                                className={clsx(
-                                    'sidebar-item projects-header-button flex-1',
-                                    isProjectsPage && 'is-active',
-                                    !showText && 'collapsed'
-                                )}
+                                className="sidebar-item"
                                 onClick={handleProjectsHeaderClick}
                                 aria-label={c('collider_2025:Button').t`Projects`}
                             >
                                 <div className="sidebar-item-icon">
-                                    <IcFolder size={4} />
+                                    <IcFolderOpen size={4} className="rtl:mirror" />
                                 </div>
-                                <span className={clsx('sidebar-item-text', !showText && 'hidden')}>
+                            </button>
+                        </Tooltip>
+                    ) : (
+                        <Tooltip title={c('collider_2025:Button').t`Projects`} originalPlacement="right">
+                            <button
+                                className="sidebar-item projects-header-button"
+                                onClick={handleProjectsHeaderClick}
+                                aria-label={c('collider_2025:Button').t`Projects`}
+                            >
+                                <div className="sidebar-item-icon">
+                                    <IcFolderOpen size={4} />
+                                </div>
+                                <span className="sidebar-item-text">
                                     {c('collider_2025:Button').t`Projects`}
                                     <DismissedFeaturePill featureId="projects" versionFlag="WhatsNewV1p3" />
                                 </span>
                             </button>
                         </Tooltip>
-                    ) : (
-                        // Authenticated mode: expandable with chevron and create button
-                        <>
-                            {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
-                            <div
-                                className={clsx(
-                                    'projects-header-container flex items-center gap-1',
-                                    isProjectsPage && 'is-active'
-                                )}
-                                onClick={handleProjectsHeaderClick}
-                                onMouseEnter={() => setIsHovered(true)}
-                                onMouseLeave={() => setIsHovered(false)}
-                                aria-label={c('collider_2025:Button').t`Projects`}
-                            >
-                                <div
-                                    className={clsx(
-                                        'sidebar-item projects-header-button flex-1',
-                                        !showText && 'collapsed'
-                                    )}
-                                >
-                                    <button
-                                        className="projects-icon-button"
-                                        onClick={handleToggle}
-                                        aria-label={
-                                            isExpanded
-                                                ? c('collider_2025:Button').t`Collapse projects`
-                                                : c('collider_2025:Button').t`Expand projects`
-                                        }
-                                        aria-expanded={isExpanded}
-                                    >
-                                        <div className="sidebar-item-icon projects-folder-icon">
-                                            {!isHovered && showText && (
-                                                <IcFolder
-                                                    size={4}
-                                                    className="rtl:mirror projects-folder-icon-default"
-                                                />
-                                            )}
-
-                                            {isHovered && (
-                                                <Icon
-                                                    name={isExpanded ? 'chevron-down' : 'chevron-right'}
-                                                    size={4}
-                                                    className="projects-chevron-hover"
-                                                />
-                                            )}
-                                        </div>
-                                    </button>
-                                    <div className="projects-text-div flex flex-nowrap items-center gap-2">
-                                        <span className={clsx('sidebar-item-text', !showText && 'hidden')}>
-                                            {c('collider_2025:Button').t`Projects`}
-                                            <DismissedFeaturePill featureId="projects" versionFlag="WhatsNewV1p3" />
-                                        </span>
-                                    </div>
-
-                                    {!isSmallScreen && showText && (
-                                        <button
-                                            className="projects-create-button"
-                                            onClick={handleCreateProject}
-                                            aria-label={c('collider_2025:Button').t`Create project`}
-                                            title={c('collider_2025:Button').t`Create project`}
-                                        >
-                                            <IcPlus size={3} />
-                                        </button>
-                                    )}
-                                </div>
-                            </div>
-                        </>
-                    )}
-                </>
-            )}
-
-            {/* Projects List - shown when expanded (only for authenticated users) */}
-            {!isGuest && isExpanded && showText && (
-                <div className="projects-list">
-                    {projects.length > 0 && (
-                        <SidebarNavList
-                            items={projects.slice(0, 5).map((project) => {
-                                const category = getProjectCategory(project.icon);
-                                return {
-                                    id: project.id,
-                                    to: `/projects/${project.id}`,
-                                    label: project.name,
-                                    isSelected: currentProjectId === project.id,
-                                    leadingContent: (
-                                        <div className="project-icon-small color-norm flex-shrink-0">
-                                            <Icon name={category.icon as any} size={4} className="color-white" />
-                                        </div>
-                                    ),
-                                    trailingContent: <ProjectActionsDropdown project={project} />,
-                                };
-                            })}
-                            onItemClick={onItemClick}
-                        />
-                    )}
-                    {projects.length > 5 && (
-                        <button
-                            className="project-sidebar-item-see-all flex items-center justify-center px-3 py-2 text-sm color-weak hover:color-norm transition-colors"
-                            onClick={handleProjectsClick}
-                        >
-                            {c('collider_2025:Button').t`See all`}
-                        </button>
-                    )}
-
-                    {projects.length === 0 && (
-                        <div className="px-3 py-2 text-sm color-weak">{c('collider_2025:Info').t`No projects yet`}</div>
                     )}
                 </div>
+            ) : (
+                <CollapsibleSidebarSection
+                    label={c('collider_2025:Button').t`Projects`}
+                    icon={<IcFolderOpen size={4} className="rtl:mirror" />}
+                    showText={showText}
+                    onHeaderClick={handleProjectsHeaderClick}
+                    labelExtra={<DismissedFeaturePill featureId="projects" versionFlag="WhatsNewV1p3" />}
+                    actionButton={
+                        <button
+                            className="projects-create-button"
+                            onClick={handleCreateProject}
+                            aria-label={c('collider_2025:Button').t`Create project`}
+                            title={c('collider_2025:Button').t`Create project`}
+                        >
+                            <IcPlus size={3} />
+                        </button>
+                    }
+                    className="projects-sidebar-section"
+                >
+                    <div className="projects-list">
+                        {projects.length > 0 && (
+                            <SidebarNavList
+                                items={projects.slice(0, 5).map((project) => {
+                                    const category = getProjectCategory(project.icon);
+                                    return {
+                                        id: project.id,
+                                        to: `/projects/${project.id}`,
+                                        label: project.name,
+                                        isSelected: currentProjectId === project.id,
+                                        leadingContent: (
+                                            <div className="project-icon-small color-norm flex-shrink-0">
+                                                <Icon name={category.icon as any} size={4} className="color-white" />
+                                            </div>
+                                        ),
+                                        trailingContent: <ProjectActionsDropdown project={project} />,
+                                    };
+                                })}
+                                onItemClick={onItemClick}
+                            />
+                        )}
+                        {projects.length > 5 && (
+                            <button
+                                className="project-sidebar-item-see-all flex items-center justify-center px-3 py-2 text-sm color-weak hover:color-norm transition-colors"
+                                onClick={handleProjectsClick}
+                            >
+                                {c('collider_2025:Button').t`See all`}
+                            </button>
+                        )}
+                        {projects.length === 0 && (
+                            <div className="px-3 py-2 text-sm color-weak">
+                                {c('collider_2025:Info').t`No projects yet`}
+                            </div>
+                        )}
+                    </div>
+                </CollapsibleSidebarSection>
             )}
-
             {newProjectModal.render && <NewProjectModal {...newProjectModal.modalProps} />}
             {projectLimitModal.render && <ProjectLimitModal {...projectLimitModal.modalProps} />}
-        </div>
+        </>
     );
 };
