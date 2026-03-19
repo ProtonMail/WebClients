b04e3a464ad1e68c9e0d6463f50ad41450e391d2
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2026-03-19 10:49:31 +0000

    Add feature flag for SSH agent
    [IDTEAM-5027]

--- packages/pass/components/Settings/Views/Tabs/General.tsx
+++ packages/pass/components/Settings/Views/Tabs/General.tsx
@@ -9,9 +9,13 @@ import { Offline } from '@proton/pass/components/Settings/Offline';
 import { Theme } from '@proton/pass/components/Settings/Theme';
 import { Beta } from '@proton/pass/components/Settings/Update/Beta.web';
 import { Update } from '@proton/pass/components/Settings/Update/Update.desktop';
+import { useFeatureFlag } from '@proton/pass/hooks/useFeatureFlag';
+import { PassFeature } from '@proton/pass/types/api/features';
 import isTruthy from '@proton/utils/isTruthy';
 
 export const General: FC = () => {
+    const showSshAgent = useFeatureFlag(PassFeature.PassDesktopSSHAgent);
+
     return [
         BUILD_TARGET === 'web' && <Beta key="web-beta" />,
         DESKTOP_BUILD && BUILD_TARGET !== 'linux' && <Update key="desktop-update" />,
@@ -19,7 +23,7 @@ export const General: FC = () => {
         <Theme key="theme" />,
         <Display key="display" />,
         OFFLINE_SUPPORTED && <Offline key="offline" />,
-        <SshAgent key="ssh-agent" />,
+        showSshAgent && <SshAgent key="ssh-agent" />,
         <ApplicationLogs style={{ '--h-custom': '18.75rem' }} key="logs" />,
     ].filter(isTruthy);
 };
