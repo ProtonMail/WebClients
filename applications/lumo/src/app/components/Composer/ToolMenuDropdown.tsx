c47c22bd5d5164e94e52fd3f1811192c5b4adb20
Elena Atencio <elena.atencio@proton.ch>
2026-03-23 12:56:24 -0600

    design review update - composer toolbar downdown menu

--- applications/lumo/src/app/components/Composer/ToolMenuDropdown.tsx
+++ applications/lumo/src/app/components/Composer/ToolMenuDropdown.tsx
@@ -9,7 +9,7 @@ import type { IconName } from '@proton/icons/types';
 import { useWebSearch } from '../../providers/WebSearchProvider';
 import { MenuDropdown, type MenuDropdownProps, MenuItem } from './components/MenuDropdown';
 
-import './UploadMenuDropdown.scss';
+import './ToolMenuDropdown.scss';
 
 interface ToolMenuDropdownProps extends Pick<MenuDropdownProps, 'isOpen' | 'anchorRef' | 'onClose'> {
     // Additional props specific to ToolMenuDropdown can be added here such as handleCreateImageTool click
@@ -44,14 +44,14 @@ export const ToolMenuDropdown = ({ isOpen, anchorRef, onClose }: ToolMenuDropdow
             isOpen={isOpen}
             anchorRef={anchorRef}
             onClose={onClose}
-            className="upload-menu-dropdown"
+            className="tool-menu-dropdown rounded-xl"
             width="200px"
         >
             {toolMenuItems.map((item) => (
                 <MenuItem key={item.iconName} {...item} />
             ))}
 
-            <hr className="my-1" />
+            <hr className="my-1 w-custom mx-auto" style={{ '--w-custom': '90%' }} />
 
             {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
             <div
@@ -59,7 +59,7 @@ export const ToolMenuDropdown = ({ isOpen, anchorRef, onClose }: ToolMenuDropdow
                 onClick={(e) => e.stopPropagation()}
             >
                 <div className="flex items-center gap-3">
-                    <Icon name="globe" size={5} className="color-weak" />
+                    <Icon name="globe" size={4} className="" />
                     <div className="flex flex-column">
                         <span className="text-sm font-medium">{c('collider_2025: Action').t`Web search`}</span>
                     </div>
