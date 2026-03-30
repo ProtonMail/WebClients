06265c96a76820191cdd5f177e892ad4b44f9425
Duc-Bao Trinh <duc-bao.trinh@proton.ch>
2026-03-30 17:15:29 +0200

    Split ssh_agent into separate files
    [IDTEAM-5027]

--- applications/pass-desktop/native/napi/src/ssh_agent/mod.rs
+++ applications/pass-desktop/native/napi/src/ssh_agent/mod.rs
@@ -12,69 +12,10 @@ use tokio::net::UnixListener;
 #[cfg(unix)]
 use tokio_stream;
 #[cfg(windows)]
-use tokio::net::windows::named_pipe::NamedPipeServer;
-#[cfg(windows)]
 use tokio_util::sync::CancellationToken;
 
 #[cfg(windows)]
-struct NamedPipeListener {
-    rx: tokio::sync::mpsc::Receiver<std::io::Result<NamedPipeServer>>,
-    cancel_token: CancellationToken,
-}
-
-#[cfg(windows)]
-impl NamedPipeListener {
-    fn new(path: String) -> std::io::Result<Self> {
-        use tokio::net::windows::named_pipe::ServerOptions;
-        
-        let (tx, rx) = tokio::sync::mpsc::channel(10);
-        let cancel_token = CancellationToken::new();
-        let cancel_clone = cancel_token.clone();
-        let mut listener = ServerOptions::new().first_pipe_instance(true).create(&path)?;
-
-        tokio::spawn(async move {
-            loop {
-                tokio::select! {
-                    _ = cancel_clone.cancelled() => {
-                        break;
-                    }
-                    connect_result = listener.connect() => {
-                        match connect_result {
-                            Ok(_) => {
-                                if let Err(e) = tx.send(Ok(listener)).await {
-                                    eprintln!("[SSH Agent] Receiver connection failed: {}", e);
-                                    break;
-                                }
-                                listener = match ServerOptions::new().create(&path) {
-                                    Ok(l) => l,
-                                    Err(e) => {
-                                        eprintln!("[SSH Agent] Failed to create next named pipe: {}", e);
-                                        break;
-                                    }
-                                };
-                            }
-                            Err(e) => {
-                                eprintln!("[SSH Agent] Failed to connect to named pipe: {}", e);
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        });
-
-        Ok(Self { rx, cancel_token })
-    }
-}
-
-#[cfg(windows)]
-impl futures::stream::Stream for NamedPipeListener {
-    type Item = std::io::Result<NamedPipeServer>;
-
-    fn poll_next(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Option<Self::Item>> {
-        self.rx.poll_recv(cx)
-    }
-}
+mod windows_named_pipe;
 
 #[napi_derive::napi(object)]
 pub struct SshKeyData {
@@ -193,7 +134,7 @@ impl SshAgentManager {
         let (task_handle, named_pipe_cancel_token) = {
             let pipe_path = socket_path.to_string_lossy().to_string();
             let agent_handler = PassSshAgent { is_unlocked_callback: Arc::new(is_unlocked_callback) };
-            let stream = NamedPipeListener::new(pipe_path)
+            let stream = windows_named_pipe::NamedPipeListener::new(pipe_path)
                 .map_err(|e| anyhow::anyhow!("Failed to create named pipe listener: {}", e))?;
 
             let cancel_token = stream.cancel_token.clone();
