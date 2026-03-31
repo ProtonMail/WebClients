f78820ec73f010881abc3c1378702699deaa0868
Facundo Pedrazzini <facundo.pedrazzini@proton.ch>
2026-03-31 14:18:51 +0200

    add post meeting modal buttons functionality
    (cherry picked from commit 05f029740807ec699947c25e11b6bd353447e74e)

--- applications/meet/src/app/components/AnonymousModal/end-call/FreeAccountModal.tsx
+++ applications/meet/src/app/components/AnonymousModal/end-call/FreeAccountModal.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router-dom';
+
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms/Button/Button';
@@ -6,25 +8,30 @@ import { MEET_APP_NAME } from '@proton/shared/lib/constants';
 import type { CTAModalBaseProps } from '../shared/types';
 import { EndCallModalShell } from './EndCallModalShell';
 
-export const FreeAccountModal = ({ open, onClose, action, rejoin }: CTAModalBaseProps) => (
-    <EndCallModalShell
-        open={open}
-        onClose={onClose}
-        actions={
-            <Button
-                className="create-account-low-pressure-button rounded-full px-10 py-4 text-semibold w-full"
-                onClick={() => {
-                    onClose();
-                    action();
-                }}
-                size="medium"
-            >
-                {c('Action').t`Start your own meeting`}
-            </Button>
-        }
-        rejoin={rejoin}
-        title={c('Info').t`You left the meeting`}
-        subtitle={c('Info')
-            .t`Host your own secure meeting. Start a call in ${MEET_APP_NAME} and share the link to invite anyone to join. Simple, secure, and free.`}
-    />
-);
+export const FreeAccountModal = ({ open, onClose, action, rejoin }: CTAModalBaseProps) => {
+    const history = useHistory();
+
+    return (
+        <EndCallModalShell
+            open={open}
+            onClose={onClose}
+            actions={
+                <Button
+                    className="create-account-low-pressure-button rounded-full px-10 py-4 text-semibold w-full"
+                    onClick={() => {
+                        onClose();
+                        history.push('/join');
+                        action();
+                    }}
+                    size="medium"
+                >
+                    {c('Action').t`Start your own meeting`}
+                </Button>
+            }
+            rejoin={rejoin}
+            title={c('Info').t`You left the meeting`}
+            subtitle={c('Info')
+                .t`Host your own secure meeting. Start a call in ${MEET_APP_NAME} and share the link to invite anyone to join. Simple, secure, and free.`}
+        />
+    );
+};
