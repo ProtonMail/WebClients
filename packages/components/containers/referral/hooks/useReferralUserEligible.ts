255034c15313517149df7d4c2ab5833e26348a79
Raphael Rychetsky <raphael.rychetsky@proton.ch>
2026-04-01 11:08:18 +0000

    Referral: Improve eligibility hook

--- packages/components/containers/referral/hooks/useReferralUserEligible.ts
+++ packages/components/containers/referral/hooks/useReferralUserEligible.ts
@@ -2,6 +2,7 @@ import { useLocation } from 'react-router-dom';
 
 import { useUser } from '@proton/account/user/hooks';
 import { useUserSettings } from '@proton/account/userSettings/hooks';
+import useConfig from '@proton/components/hooks/useConfig';
 import { getAppFromPathnameSafe } from '@proton/shared/lib/apps/slugHelper';
 import type { APP_NAMES } from '@proton/shared/lib/constants';
 import { APPS } from '@proton/shared/lib/constants';
@@ -10,18 +11,22 @@ import type { UserSettings } from '@proton/shared/lib/interfaces';
 export const getIsReferralUserEligible = (
     userSettings: UserSettings | undefined,
     isFree: boolean,
-    app: APP_NAMES | undefined
-) => !!userSettings?.Referral?.Eligible && !(isFree && app === APPS.PROTONVPN_SETTINGS);
+    app: APP_NAMES | undefined,
+    appName: APP_NAMES
+) =>
+    !!userSettings?.Referral?.Eligible &&
+    !(isFree && (app === APPS.PROTONVPN_SETTINGS || appName === APPS.PROTONVPN_SETTINGS));
 
 export const useReferralUserEligible = () => {
     const location = useLocation();
     const [userSettings] = useUserSettings();
     const [user] = useUser();
     const { isFree } = user;
+    const { APP_NAME } = useConfig();
 
     const app = getAppFromPathnameSafe(location.pathname);
 
-    const isUserEligible = getIsReferralUserEligible(userSettings, isFree, app);
+    const isUserEligible = getIsReferralUserEligible(userSettings, isFree, app, APP_NAME);
 
     return { isUserEligible };
 };
