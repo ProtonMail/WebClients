629a9f27a170c055d9554d9ffefff97df6c890c6
Son Nguyen <son.nguyen@proton.ch>
2026-04-02 09:14:04 +0200

    add exclude from monitoring in the extension

--- packages/pass/hooks/items/useItemState.ts
+++ packages/pass/hooks/items/useItemState.ts
@@ -73,7 +73,7 @@ export const useItemState: UseItemState = (item, share) => {
         const canItemShare = canShare && !orgItemSharingDisabled;
         const canManageAccess = shared && !readOnly;
         const canLeave = !isVault && !owner;
-        const canMonitor = !EXTENSION_BUILD && !trashed && data.type === 'login' && !readOnly;
+        const canMonitor = !trashed && data.type === 'login' && !readOnly;
         const canTogglePinned = !(pinInFlight || unpinInFlight);
 
         return {
