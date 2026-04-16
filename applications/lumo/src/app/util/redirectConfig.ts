82d689ecf91ff1039a612c09f39b3fbf13bc15ac
Elena Atencio <elena.atencio@proton.ch>
2026-04-16 13:02:35 +0000

    Revert redirecting links functionality added to support migration to new lumo marketing links

--- applications/lumo/src/app/util/redirectConfig.ts
+++
@@ -1,5 +0,0 @@
-export const REDIRECT_CONFIG = {
-    // Domains that should trigger redirects
-    SOURCE_DOMAINS: ['lumo.proton.me', 'lumo.proton.dev'] as const,
-    TARGET_DOMAIN: 'https://proton.me',
-} as const;
