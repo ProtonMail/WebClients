2878695b81c822a82ff45a075919523c59f5c2d3
Matthieu Lux <matthieu.lux@proton.ch>
2026-04-28 11:20:04 +0200

    [IDTEAM-4633] Fix native messaging timeout during login on account

--- applications/pass-desktop/src/lib/native-messaging/electron-ipc.ts
+++ applications/pass-desktop/src/lib/native-messaging/electron-ipc.ts
@@ -1,11 +1,13 @@
 import type { BrowserWindow } from 'electron';
 
+import { NativeMessageErrorType } from '@proton/pass/types';
 import type { MaybeNull, NativeMessagePayload, NativeMessageRequest, NativeMessageResponse } from '@proton/pass/types';
 
 import logger from '../../utils/logger';
 import { setupIpcHandler } from '../ipc';
 
 const log = (...content: any[]) => logger.debug('[NativeMessaging]', ...content);
+const info = (...content: any[]) => logger.info('[NativeMessaging]', ...content);
 
 declare module 'proton-pass-desktop/lib/ipc' {
     interface IPCChannels {
@@ -22,12 +24,22 @@ export const setupElectronIpcHandlers = (getWindow: () => MaybeNull<BrowserWindo
         request: NativeMessagePayload<NativeMessageRequest>,
         sendResponse: (response: NativeMessagePayload<NativeMessageResponse>) => void
     ) => {
+        if (!getWindow()?.webContents.getURL().startsWith(MAIN_WINDOW_WEBPACK_ENTRY)) {
+            info('App not on main URL, responding not logged in');
+            sendResponse({
+                type: request.type,
+                error: NativeMessageErrorType.DESKTOP_APP_NOT_LOGGED_IN,
+                messageId: request.messageId,
+            } as NativeMessagePayload<NativeMessageResponse>);
+            return;
+        }
+
         try {
             log('Sending request to view', request.type);
             pendingResponses.set(request.messageId, sendResponse);
             getWindow()?.webContents.send('nm:request', request);
         } catch (error) {
-            log('Sending to to view went wrong', error);
+            info('Sending to to view went wrong', error);
         }
     };
 
@@ -38,7 +50,7 @@ export const setupElectronIpcHandlers = (getWindow: () => MaybeNull<BrowserWindo
             pendingResponses.delete(response.messageId);
             sendResponse(response);
         } else {
-            log('No pending request for messageId', response.messageId);
+            info('No pending request for messageId', response.messageId);
         }
     });
 
