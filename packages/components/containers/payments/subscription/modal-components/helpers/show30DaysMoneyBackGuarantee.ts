cc538c33b1d7d9c437a5c5321fc706635a723371
Jonathan Bosi <jonathan.bosi@proton.ch>
2026-04-29 13:18:34 +0000

    Tech: remove useless duplicated property

--- packages/components/containers/payments/subscription/modal-components/helpers/show30DaysMoneyBackGuarantee.ts
+++ packages/components/containers/payments/subscription/modal-components/helpers/show30DaysMoneyBackGuarantee.ts
@@ -1,33 +1,28 @@
 import {
     type FreeSubscription,
-    type PlanIDs,
     type PlansMap,
     SelectedPlan,
     type Subscription,
     type SubscriptionCheckForbiddenReason,
     getIsVpnPlan,
-    getPlanNameFromIDs,
 } from '@proton/payments/index';
 
 export function show30DaysMoneyBackGuarantee({
-    planIDs,
     plansMap,
     subscription,
     paymentForbiddenReason,
     selectedPlan,
 }: {
-    planIDs: PlanIDs;
     paymentForbiddenReason: SubscriptionCheckForbiddenReason;
     plansMap: PlansMap;
     subscription: Subscription | FreeSubscription | undefined;
     selectedPlan: SelectedPlan;
 }): boolean {
-    const planName = getPlanNameFromIDs(planIDs);
-
     const currentPlan = SelectedPlan.createFromSubscription(subscription, plansMap);
 
     const addonsModification =
         selectedPlan.getPlanName() === currentPlan.getPlanName() && !selectedPlan.isEqualTo(currentPlan);
-    const hasGuarantee = getIsVpnPlan(planName) && !addonsModification && !paymentForbiddenReason.forbidden;
+    const hasGuarantee =
+        getIsVpnPlan(selectedPlan.getPlanName()) && !addonsModification && !paymentForbiddenReason.forbidden;
     return hasGuarantee;
 }
