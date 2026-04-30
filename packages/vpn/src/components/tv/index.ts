d1785a6f80e36f71535442acbf4b4745c4ecd78b
Joel Prat Vilanova <joel.pratvilanova@proton.ch>
2026-04-30 14:00:27 +0200

    VPNPLG-40: Create redirect for tv checkout

--- packages/vpn/src/components/tv/index.ts
+++ packages/vpn/src/components/tv/index.ts
@@ -1,6 +1,7 @@
 export { TVContainer } from './containers/TVContainer';
 export { TvContainerSignedIn } from './containers/TvContainerSignedIn';
 export { TvNotSignedIn } from './components/TvNotSignedIn';
+export { TvCheckout } from './containers/TvCheckoutContainer';
 
 export { getChildClientId } from './utils/getChildClientId';
 export { forkSession } from './utils/forkSession';
