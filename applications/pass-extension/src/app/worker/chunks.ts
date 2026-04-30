e79a1fad738b6667beae940935e76f1ffce36f80
Redacted <redacted@invalid.proton.ch>
2026-04-30 17:31:56 +0200

    pass-extension: trigger build failure on unhandled dynamic imports; handle new crypto chunks
    Note that if any of the chunks tracked in `CRYPTO_DYNAMIC_IMPORTS_CHUNKS_MAP` is no longer present,
    no error is thrown (same as before)

---
+++ applications/pass-extension/src/app/worker/chunks.ts
@@ -0,0 +1,25 @@
+/**
+ * Dynamic imports need to be manually included in the extension's SW via importScript.
+ * This file keeps track of the node_modules that are dynamically imported.
+ * The build script will trigger errors when it encounters new unhandled chunks.
+ */
+
+const getCryptoChunkName = (chunkDesc: string) => `chunk.crypto-${chunkDesc}.js`;
+
+export const CRYPTO_DYNAMIC_IMPORTS_CHUNKS_MAP: Record<string, string> = {
+    node_modules_openpgp_dist_lightweight_argon2id_min_mjs: getCryptoChunkName('argon2'),
+    vendor_argon2id_loader_ts: 'vendor_argon2id_loader_ts', // special case due to wasm handling
+    'node_modules_openpgp_dist_lightweight_nacl-fast_min_mjs': getCryptoChunkName('nacl'),
+    node_modules_openpgp_dist_lightweight_noble_post_quantum_min_mjs: getCryptoChunkName('pqc'),
+    node_modules_openpgp_dist_lightweight_noble_hashes_min_mjs: getCryptoChunkName('noblehashes'),
+    node_modules_openpgp_dist_lightweight_noble_curves_min_mjs: getCryptoChunkName('noblecurves'),
+    node_modules_protontech_crypto_node_modules_noble_hashes_legacy_js: getCryptoChunkName('noblehashes_legacy'),
+};
+
+/** These are unused by the Pass extension and don't need to be manually handled */
+export const IGNORED_DYNAMIC_IMPORTS_CHUNKS = new Set([
+    'node_modules_x2js_x2js_js',
+    'node_modules_jsmimeparser_index_js',
+    'node_modules_openpgp_dist_lightweight_legacy_ciphers_min_mjs',
+    'node_modules_openpgp_dist_lightweight_unbzip2-stream_min_mjs',
+]);
