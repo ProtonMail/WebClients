e0c4e4c1f1780de460fc827d8d3b0d7376897303
Elena Atencio <elena.atencio@proton.ch>
2026-04-30 12:25:04 +0200

    initial commit with new ConversationACtionsProvider and LumoLayout to standardize layout

--- applications/lumo/src/app/features/projects/components/ProjectDetailHeader.tsx
+++ applications/lumo/src/app/features/projects/components/ProjectDetailHeader.tsx
@@ -1,15 +1,11 @@
-import { useEffect, useRef, useState } from 'react';
-
 import { clsx } from 'clsx';
 import { c } from 'ttag';
 
 import { Button } from '@proton/atoms/Button/Button';
-import { Dropdown, DropdownMenu, DropdownMenuButton, Icon, usePopperAnchor } from '@proton/components';
 import { IcArrowLeft } from '@proton/icons/icons/IcArrowLeft';
-import { IcThreeDotsHorizontal } from '@proton/icons/icons/IcThreeDotsHorizontal';
-import { IcTrash } from '@proton/icons/icons/IcTrash';
 
 import ProjectSettingsButton from '../ProjectSettingsButton';
+import { ProjectTitleSection } from './ProjectTitleSection';
 
 interface ProjectDetailHeaderProps {
     projectName: string;
@@ -32,41 +28,6 @@ export const ProjectDetailHeader = ({
     onSaveTitle,
     onDeleteProject,
 }: ProjectDetailHeaderProps) => {
-    const [isEditingTitle, setIsEditingTitle] = useState(false);
-    const [editedTitle, setEditedTitle] = useState('');
-    const titleInputRef = useRef<HTMLInputElement>(null);
-    const { anchorRef, isOpen, toggle, close } = usePopperAnchor<HTMLButtonElement>();
-
-    useEffect(() => {
-        if (isEditingTitle && titleInputRef.current) {
-            titleInputRef.current.focus();
-            titleInputRef.current.select();
-        }
-    }, [isEditingTitle]);
-
-    const handleStartEditing = () => {
-        setEditedTitle(projectName);
-        setIsEditingTitle(true);
-    };
-
-    const handleSave = () => {
-        const trimmedTitle = editedTitle.trim();
-        if (trimmedTitle && trimmedTitle !== projectName) {
-            onSaveTitle(trimmedTitle);
-        }
-        setIsEditingTitle(false);
-    };
-
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            handleSave();
-        } else if (e.key === 'Escape') {
-            setIsEditingTitle(false);
-            setEditedTitle(projectName);
-        }
-    };
-
     return (
         <div
             className={clsx(
@@ -97,55 +58,12 @@ export const ProjectDetailHeader = ({
                     </div>
                 </div>
 
-                <div className="project-detail-title-section flex items-center flex-nowrap w-full">
-                    <Icon name={category.icon as any} size={6} className="project-detail-title-icon shrink-0" />
-                    {isEditingTitle ? (
-                        <input
-                            ref={titleInputRef}
-                            type="text"
-                            className="project-detail-title-input text-2xl"
-                            value={editedTitle}
-                            onChange={(e) => setEditedTitle(e.target.value)}
-                            onBlur={handleSave}
-                            onKeyDown={handleKeyDown}
-                            maxLength={100}
-                        />
-                    ) : (
-                        <>
-                            {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
-                            <h1
-                                className="project-detail-title text-2xl text-ellipsis"
-                                onClick={handleStartEditing}
-                                title={c('collider_2025:Action').t`Click to edit title`}
-                            >
-                                {projectName}
-                            </h1>
-                        </>
-                    )}
-                    <Button
-                        ref={anchorRef}
-                        icon
-                        shape="ghost"
-                        onClick={toggle}
-                        title={c('collider_2025:Action').t`More options`}
-                    >
-                        <IcThreeDotsHorizontal />
-                    </Button>
-                    <Dropdown isOpen={isOpen} anchorRef={anchorRef} onClose={close} className="chat-dropdown-menu">
-                        <DropdownMenu>
-                            <DropdownMenuButton
-                                className="text-left color-danger"
-                                onClick={() => {
-                                    close();
-                                    onDeleteProject();
-                                }}
-                            >
-                                <IcTrash className="mr-2" />
-                                {c('collider_2025:Action').t`Delete project`}
-                            </DropdownMenuButton>
-                        </DropdownMenu>
-                    </Dropdown>
-                </div>
+                <ProjectTitleSection
+                    projectName={projectName}
+                    categoryIcon={category.icon}
+                    onSaveTitle={onSaveTitle}
+                    onDeleteProject={onDeleteProject}
+                />
             </div>
         </div>
     );
