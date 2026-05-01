0139fdcd44574a23b5c1280776369ed1a0c58663
Elena Atencio <elena.atencio@proton.ch>
2026-05-01 12:17:53 +0200

    overrirde toggle styles to match new v2.0 designs

--- applications/lumo/src/app/upsells/composed/LumoComposerToggleUpsell.tsx
+++ applications/lumo/src/app/upsells/composed/LumoComposerToggleUpsell.tsx
@@ -1,34 +1,34 @@
-import { LUMO_UPSELL_PATHS } from '@proton/shared/lib/constants';
+// import { LUMO_UPSELL_PATHS } from '@proton/shared/lib/constants';
 
-import PressEnterToReturn from '../../components/Composer/PressEnterToReturn';
-import { useLumoPlan } from '../../hooks/useLumoPlan';
-import { useIsGuest } from '../../providers/IsGuestProvider';
-import GuestLumoPlusUpsellModal from '../GuestLumoPlusUpsellModal';
-import ToggleUpsell from '../primitives/ToggleUpsell';
-import useLumoPlusUpsellButtonConfig from '../useLumoPlusUpsellButtonConfig';
+// import PressEnterToReturn from '../../components/Composer/PressEnterToReturn';
+// import { useLumoPlan } from '../../hooks/useLumoPlan';
+// import { useIsGuest } from '../../providers/IsGuestProvider';
+// import GuestLumoPlusUpsellModal from '../GuestLumoPlusUpsellModal';
+// import ToggleUpsell from '../primitives/ToggleUpsell';
+// import useLumoPlusUpsellButtonConfig from '../useLumoPlusUpsellButtonConfig';
 
-const LumoPlusToggle = () => {
-    const isGuest = useIsGuest();
-    const { canShowLumoUpsellFree } = useLumoPlan();
-    const config = useLumoPlusUpsellButtonConfig(LUMO_UPSELL_PATHS.LUMO_PLUS_UPGRADE_TOGGLE, {
-        enableGuestModal: true,
-    });
+// const LumoPlusToggle = () => {
+//     const isGuest = useIsGuest();
+//     const { canShowLumoUpsellFree } = useLumoPlan();
+//     const config = useLumoPlusUpsellButtonConfig(LUMO_UPSELL_PATHS.LUMO_PLUS_UPGRADE_TOGGLE, {
+//         enableGuestModal: true,
+//     });
 
-    // Show upsell for guests or free users
-    const showUpsell = isGuest || canShowLumoUpsellFree;
+//     // Show upsell for guests or free users
+//     const showUpsell = isGuest || canShowLumoUpsellFree;
 
-    if (!showUpsell) {
-        return <PressEnterToReturn />;
-    }
+//     if (!showUpsell) {
+//         return <PressEnterToReturn />;
+//     }
 
-    return (
-        <>
-            <ToggleUpsell onUpgrade={config?.onUpgrade} className={config?.className} />
-            {config?.guestModal?.render && <GuestLumoPlusUpsellModal {...config.guestModal.modalProps} />}
-        </>
-    );
-};
+//     return (
+//         <>
+//             <ToggleUpsell onUpgrade={config?.onUpgrade} className={config?.className} />
+//             {config?.guestModal?.render && <GuestLumoPlusUpsellModal {...config.guestModal.modalProps} />}
+//         </>
+//     );
+// };
 
-LumoPlusToggle.displayName = 'LumoComposerToggleUpsell';
+// LumoPlusToggle.displayName = 'LumoComposerToggleUpsell';
 
-export default LumoPlusToggle;
+// export default LumoPlusToggle;
