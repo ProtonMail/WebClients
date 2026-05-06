0c4b473a4a5eb94c25e828459daa51335b55a8a0
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-05-06 11:25:45 +0200

    Add mail toolbar header actions buttons

---
+++ applications/mail/src/app/components/toolbar/actions/MoveBackButton.tsx
@@ -0,0 +1,28 @@
+import { useHistory } from 'react-router-dom';
+
+import { c } from 'ttag';
+
+import ToolbarButton from '@proton/components/components/toolbar/ToolbarButton';
+import { IcArrowLeft } from '@proton/icons/icons/IcArrowLeft';
+
+import { setParamsInLocation } from 'proton-mail/helpers/mailboxUrl';
+import { selectLabelID } from 'proton-mail/store/elements/elementsSelectors';
+import { useMailSelector } from 'proton-mail/store/hooks';
+
+export const MoveBackButton = () => {
+    const labelID = useMailSelector(selectLabelID);
+
+    const history = useHistory();
+
+    const handleBack = () => {
+        history.push(setParamsInLocation(history.location, { labelID }));
+    };
+
+    return (
+        <ToolbarButton
+            icon={<IcArrowLeft alt={c('Action').t`Back`} />}
+            onClick={handleBack}
+            data-testid="toolbar:back-button"
+        />
+    );
+};
