06f10e09d737b376400f07848ac2d9648c7300ad
Thong-Nguyen The <thong-nguyen.the@proton.ch>
2026-05-07 08:47:20 +0000

    AdminRoles(Account): Show AdminRole spotlight on MemberTable

--- packages/components/containers/members/UsersAndAddressesSection/UsersAndAddressesSectionHeader.tsx
+++ packages/components/containers/members/UsersAndAddressesSection/UsersAndAddressesSectionHeader.tsx
@@ -3,9 +3,15 @@ import type { CSSProperties, JSX } from 'react';
 import { c } from 'ttag';
 
 import Info from '@proton/components/components/link/Info';
+import useSpotlightShow from '@proton/components/components/spotlight/useSpotlightShow';
 import TableCell from '@proton/components/components/table/TableCell';
 import { adminTooltipText } from '@proton/components/containers/members/constants';
+import AdminRolesSpotlight from '@proton/components/containers/members/rolesAndPermissions/AdminRolesSpotlight';
+import useSpotlightOnFeature from '@proton/components/hooks/useSpotlightOnFeature';
+import { FeatureCode, useFeature } from '@proton/features';
+import { SECOND } from '@proton/shared/lib/constants';
 import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
+import { useFlag } from '@proton/unleash/useFlag';
 import isTruthy from '@proton/utils/isTruthy';
 
 type HeaderCellItem = {
@@ -21,12 +27,40 @@ interface Props {
 }
 
 const UsersAndAddressesSectionHeader = ({ useEmail, showFeaturesColumn }: Props) => {
+    const hasAdminRoles = useFlag('AdminRoleMVP');
+    const { feature: adminRolesModalFeature, loading: adminRolesModalLoading } = useFeature(
+        FeatureCode.AdminRolesOnboardingModal
+    );
+    const isAdminRolesModalDismissed = !adminRolesModalLoading && !adminRolesModalFeature?.Value;
+    const {
+        show: showSpotlight,
+        onDisplayed: onSpotlightDisplayed,
+        onClose: onSpotlightClose,
+    } = useSpotlightOnFeature(FeatureCode.AdminRolesTableSpotlight, hasAdminRoles && isAdminRolesModalDismissed);
+    const shouldShowSpotlight = useSpotlightShow(showSpotlight, 3 * SECOND);
+
     const addressesTitle = useEmail
         ? c('Title header for members table').t`Email`
         : c('Title header for members table').t`Addresses`;
 
     const headerCells: HeaderCellItem[] = [
-        { key: 'name', node: c('Title header for members table').t`Name`, className: 'w-3/10' },
+        {
+            key: 'name',
+            node: (
+                <AdminRolesSpotlight
+                    show={shouldShowSpotlight}
+                    onDisplayed={onSpotlightDisplayed}
+                    onClose={onSpotlightClose}
+                    originalPlacement="top-start"
+                    title={c('Spotlight').t`User roles available`}
+                    description={c('Spotlight').t`Click a user's name to manage their role and permissions.`}
+                    kbLink={getKnowledgeBaseUrl('/admin-roles')}
+                >
+                    <span>{c('Title header for members table').t`Name`}</span>
+                </AdminRolesSpotlight>
+            ),
+            className: 'w-3/10',
+        },
         {
             key: 'role',
             node: (
