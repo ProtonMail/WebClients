85e7f804f94a21f17d5af4531cc2f12d0e326575
Lucien Peslier <lucien.peslier@proton.ch>
2026-05-12 09:05:40 +0000

    Fix Share button not showing for photos

--- applications/drive/src/app/sections/transferManager/subscribeToUploadEvents.ts
+++ applications/drive/src/app/sections/transferManager/subscribeToUploadEvents.ts
@@ -1,8 +1,6 @@
-import { getDriveForPhotos } from '@proton/drive/index';
 import { BusDriverEventName, getBusDriver } from '@proton/drive/internal/BusDriver';
 import { getNodeEffectiveRole } from '@proton/drive/internal/sdkUtils/getNodeEffectiveRole';
 import { uploadManager } from '@proton/drive/modules/upload';
-import { getNodeEntityFromMaybeNode } from '@proton/drive/modules/upload/utils/getNodeEntityFromMaybeNode';
 
 import { getNodeEntity } from '../../utils/sdk/getNodeEntity';
 import { useTransferManagerStore } from './transferManager.store';
@@ -11,25 +9,15 @@ export const subscribeToUploadEvents = (): (() => void) => {
     const busDriver = getBusDriver();
     uploadManager.subscribeToEvents('transfer-manager', async (event, driveClient) => {
         if (event.type === 'file:complete') {
-            if (event.parentUid) {
-                const maybeNode = await driveClient.getNode(event.parentUid);
-                const { node } = await getNodeEntityFromMaybeNode(maybeNode);
-                const role = await getNodeEffectiveRole(node, driveClient);
-                useTransferManagerStore.getState().addItem(event.uploadId, { role, type: 'upload' });
-            }
+            const maybeNode = await driveClient.getNode(event.nodeUid);
+            const { node } = getNodeEntity(maybeNode);
+            const role = await getNodeEffectiveRole(node, driveClient);
+            useTransferManagerStore.getState().addItem(event.uploadId, { role, type: 'upload' });
 
-            const photosRootNode = event.isForPhotos
-                ? getNodeEntity(await getDriveForPhotos().getMyPhotosRootFolder()).node
-                : undefined;
             await busDriver.emit(
                 {
                     type: event.isUpdatedNode ? BusDriverEventName.UPDATED_NODES : BusDriverEventName.CREATED_NODES,
-                    items: [
-                        // TODO: Quick hack to know it is for photos section
-                        // We need to improve that somehow as we listen for the event in photos section,
-                        // and we need to know if it's for this section
-                        { uid: event.nodeUid, parentUid: event.isForPhotos ? photosRootNode?.uid : event.parentUid },
-                    ],
+                    items: [{ uid: event.nodeUid, parentUid: event.parentUid }],
                 },
                 driveClient
             );
