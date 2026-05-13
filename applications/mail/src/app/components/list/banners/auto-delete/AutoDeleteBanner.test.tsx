932535315b4de572844a569e7985e8eb03773b79
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-05-13 09:21:30 +0200

    test: remove mailTestRenderer in AutoDeleteBanner

--- applications/mail/src/app/components/list/banners/auto-delete/AutoDeleteBanner.test.tsx
+++ applications/mail/src/app/components/list/banners/auto-delete/AutoDeleteBanner.test.tsx
@@ -1,13 +1,16 @@
-import { screen } from '@testing-library/react';
-
-import { mailTestRender } from 'proton-mail/helpers/test/render';
+import { render, screen } from '@testing-library/react';
 
 import AutoDeleteBanner from './AutoDeleteBanner';
 
+jest.mock('@proton/components/hooks/useApi', () => () => jest.fn());
+jest.mock('@proton/redux-shared-store/sharedProvider', () => ({
+    useDispatch: () => jest.fn(),
+}));
+
 describe('AutoDeleteBanner', () => {
     describe('when bannerType is paid', () => {
-        it('should render correct banner', async () => {
-            await mailTestRender(<AutoDeleteBanner bannerType="paid-banner" />);
+        it('should render correct banner', () => {
+            render(<AutoDeleteBanner bannerType="paid-banner" />);
             expect(
                 screen.getByText(
                     /Automatically delete messages that have been in trash and spam for more than 30 days./i
@@ -17,8 +20,8 @@ describe('AutoDeleteBanner', () => {
     });
 
     describe('when bannerType is free', () => {
-        it('should render correct banner', async () => {
-            await mailTestRender(<AutoDeleteBanner bannerType="free-banner" />);
+        it('should render correct banner', () => {
+            render(<AutoDeleteBanner bannerType="free-banner" />);
             expect(
                 screen.getByText(
                     /Upgrade to automatically delete messages that have been in Trash or Spam for more than 30 days./i
@@ -28,8 +31,8 @@ describe('AutoDeleteBanner', () => {
     });
 
     describe('when bannerType is enabled', () => {
-        it('should render correct banner', async () => {
-            await mailTestRender(<AutoDeleteBanner bannerType="enabled" />);
+        it('should render correct banner', () => {
+            render(<AutoDeleteBanner bannerType="enabled" />);
             expect(
                 screen.getByText(
                     /Messages that have been in trash and spam more than 30 days will be automatically deleted./i
