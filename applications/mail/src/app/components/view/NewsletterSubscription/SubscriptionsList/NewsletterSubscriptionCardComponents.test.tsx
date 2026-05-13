7470bc9fbcb001042ff2fb9dafbfae1b0a477363
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-05-13 08:28:43 +0200

    test: remove mailTestRender in NewsletterCard tests

--- applications/mail/src/app/components/view/NewsletterSubscription/SubscriptionsList/NewsletterSubscriptionCardComponents.test.tsx
+++ applications/mail/src/app/components/view/NewsletterSubscription/SubscriptionsList/NewsletterSubscriptionCardComponents.test.tsx
@@ -1,33 +1,23 @@
-import { fireEvent, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
-import { mailTestRender } from 'proton-mail/helpers/test/render';
+import { useMailSelector } from 'proton-mail/store/hooks';
 import { SubscriptionTabs } from 'proton-mail/store/newsletterSubscriptions/interface';
-import * as mailboxSelectors from 'proton-mail/store/newsletterSubscriptions/newsletterSubscriptionsSelector';
 
 import { unsubscribedSubscription } from '../testData';
 import { SubscriptionCardButtons } from './NewsletterSubscriptionCardComponents';
 
-jest.mock('proton-mail/store/newsletterSubscriptions/newsletterSubscriptionsSelector', () => ({
-    ...jest.requireActual('proton-mail/store/newsletterSubscriptions/newsletterSubscriptionsSelector'),
-    selectedTab: jest.fn(),
-}));
-const mockedSelectedTab = mailboxSelectors.selectedTab as jest.Mock;
+jest.mock('proton-mail/store/hooks');
+jest.mocked(useMailSelector).mockReturnValue(SubscriptionTabs.Unsubscribe);
 
 const handleFilterClick = jest.fn();
 
 describe('SubscriptionCardButtons', () => {
-    beforeEach(() => {
-        mockedSelectedTab.mockReturnValue(SubscriptionTabs.Active);
-    });
-
     it('should call handleFilterClick with the correct filter type when the unsubscribe button is clicked', async () => {
-        mockedSelectedTab.mockReturnValue(SubscriptionTabs.Unsubscribe);
-
-        await mailTestRender(
+        render(
             <SubscriptionCardButtons subscription={unsubscribedSubscription} handleFilterClick={handleFilterClick} />
         );
 
-        const unsubscribeButton = screen.getByText('Move to Trash');
+        const unsubscribeButton = screen.getByTestId('move-to-trash');
         fireEvent.click(unsubscribeButton);
 
         expect(handleFilterClick).toHaveBeenCalledWith('MoveToTrash');
