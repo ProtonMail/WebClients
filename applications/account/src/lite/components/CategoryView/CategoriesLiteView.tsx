686f902c8aa65e0f51e620cee17fda1c8ebf7495
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-05-19 15:52:37 +0200

    feat: create category lite settings main layout

---
+++ applications/account/src/lite/components/CategoryView/CategoriesLiteView.tsx
@@ -0,0 +1,43 @@
+import { FeatureCode } from '@proton/features/interface';
+import useFeature from '@proton/features/useFeature';
+import { useFolders } from '@proton/mail/store/labels/hooks';
+import { useMailSettings } from '@proton/mail/store/mailSettings/hooks';
+import { useFlag } from '@proton/unleash/useFlag';
+
+import { CategoriesHeader } from './CategoriesHeader';
+import { CategoriesSettings } from './CategoriesSettings';
+import { CategoriesToggle } from './CategoriesToggle';
+
+interface CategoryViewProps {
+    layout: (children: React.ReactNode, props?: any) => React.ReactNode;
+    loader: React.ReactNode;
+}
+
+export const CategoriesLiteView = ({ layout, loader }: CategoryViewProps) => {
+    // Preaload needed data for children
+    const [, mailSettingsLoading] = useMailSettings();
+    const [, foldersLoading] = useFolders();
+
+    const categoryViewFlag = useFlag('CategoryView');
+    const betaFlag = useFeature<boolean>(FeatureCode.CategoryViewBeta);
+
+    const hasAccess = categoryViewFlag || (betaFlag.feature?.Value ?? false);
+    const loading = !!(mailSettingsLoading || foldersLoading || betaFlag.loading);
+
+    if (!hasAccess) {
+        return null;
+    }
+
+    if (loading) {
+        return loader;
+    }
+
+    return layout(
+        <div className="mobile-settings">
+            <CategoriesHeader />
+            <CategoriesToggle />
+            <CategoriesSettings />
+        </div>,
+        { className: 'overflow-auto' }
+    );
+};
