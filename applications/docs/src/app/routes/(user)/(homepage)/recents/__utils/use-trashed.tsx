cf32631d26735782d97f9f6cbfee6d334d129944
Maciej Lenartowicz <maciej.lenartowicz@proton.ch>
2026-05-22 11:55:43 +0200

    [REALTIME-240] Subscribe to SDK events in Docs homepage

--- applications/docs/src/app/routes/(user)/(homepage)/recents/__utils/use-trashed.tsx
+++ applications/docs/src/app/routes/(user)/(homepage)/recents/__utils/use-trashed.tsx
@@ -1,13 +1,12 @@
-import type { RecentDocumentsItemValue } from '@proton/docs-core/lib/Services/recent-documents'
-import { ServerTime } from '@proton/docs-shared'
-import { splitNodeUid } from '@proton/drive'
-import { mimeTypeToProtonDocumentType } from '@proton/shared/lib/helpers/mimetype'
-import type { ProtonDriveClient, NodeEntity, DegradedNode } from '@protontech/drive-sdk'
-import { useState, useMemo } from 'react'
-import { getNodeName, getAuthorName } from './create-document-items'
-import { useApplication } from '~/utils/application-context'
-import { c } from 'ttag'
 import useNotifications from '@proton/components/hooks/useNotifications'
+import type { RecentDocumentsItemValue } from '@proton/docs-core/lib/Services/recent-documents'
+import { generateNodeUid, getDrive } from '@proton/drive'
+import { mimeTypeToProtonDocumentType } from '@proton/shared/lib/helpers/mimetype'
+import type { DegradedNode, DriveEvent, NodeEntity, ProtonDriveClient } from '@protontech/drive-sdk'
+import { useCallback, useState } from 'react'
+import { c } from 'ttag'
+import { useApplication } from '~/utils/application-context'
+import { nodeToTrashedItemValue } from './create-document-items'
 
 export function useTrashed(drive: ProtonDriveClient) {
   const app = useApplication()
@@ -17,56 +16,70 @@ export function useTrashed(drive: ProtonDriveClient) {
   const [trashedDocumentItems, setTrashedDocumentItems] = useState<RecentDocumentsItemValue[]>([])
   const [isTrashLoading, setIsTrashLoading] = useState(false)
 
-  const fetchTrashed = useMemo(
-    () => async () => {
-      setIsTrashLoading(true)
+  const fetchTrashed = useCallback(async () => {
+    setIsTrashLoading(true)
 
-      const nodes: (NodeEntity | DegradedNode)[] = []
-      try {
-        for await (const maybeNode of drive.iterateTrashedNodes()) {
-          const node = maybeNode.ok ? maybeNode.value : maybeNode.error
-          if (!mimeTypeToProtonDocumentType(node.mediaType)) {
-            continue
-          }
-          nodes.push(node)
+    const nodes: (NodeEntity | DegradedNode)[] = []
+    try {
+      for await (const maybeNode of drive.iterateTrashedNodes()) {
+        const node = maybeNode.ok ? maybeNode.value : maybeNode.error
+        if (!mimeTypeToProtonDocumentType(node.mediaType)) {
+          continue
         }
-      } catch (error: any) {
-        logger.error('Failed to load trashed document with SDK', error)
-        createNotification({
-          type: 'error',
-          text: c('Error').t`Some trashed documents could not be loaded`,
-        })
+        nodes.push(node)
       }
+    } catch (error: any) {
+      logger.error('Failed to load trashed document with SDK', error)
+      createNotification({
+        type: 'error',
+        text: c('Error').t`Some trashed documents could not be loaded`,
+      })
+    }
 
-      setTrashedDocumentItems(nodes.map(nodeToDocumentsItemValue))
-      setIsTrashLoading(false)
-    },
-    [createNotification, drive, logger],
-  )
+    setTrashedDocumentItems(nodes.map(nodeToTrashedItemValue))
+    setIsTrashLoading(false)
+  }, [createNotification, drive, logger])
+
+  const handleEvent = useCallback(async (event: DriveEvent) => {
+    const drive = getDrive()
+
+    if (event.type === 'node_updated') {
+      if (event.isTrashed) {
+        const maybeNode = await drive.getNode(event.nodeUid)
+        const node = maybeNode.ok ? maybeNode.value : maybeNode.error
+        if (!mimeTypeToProtonDocumentType(node.mediaType)) {
+          return
+        }
+        setTrashedDocumentItems((items) => createOrUpdateItem(items, node))
+      } else {
+        setTrashedDocumentItems((items) => removeMissingItems(items, event))
+      }
+    }
+
+    if (event.type === 'node_deleted') {
+      setTrashedDocumentItems((items) => removeMissingItems(items, event))
+    }
+  }, [])
 
   return {
     fetchTrashed,
     isTrashLoading,
     trashedDocumentItems,
+    handleEvent,
   }
 }
 
-function nodeToDocumentsItemValue(node: NodeEntity | DegradedNode): RecentDocumentsItemValue {
-  const { volumeId, nodeId: linkId } = splitNodeUid(node.uid)
-  const { nodeId: parentLinkId } = node.parentUid ? splitNodeUid(node.parentUid) : {}
-  return {
-    name: getNodeName(node) ?? '',
-    type: mimeTypeToProtonDocumentType(node.mediaType) ?? 'document',
-    linkId,
-    parentLinkId,
-    volumeId,
-    // In case trashTime is missing it will use Sept 2001, but that's HIGHLY unlikely
-    lastViewed: new ServerTime(node.trashTime?.getTime() ?? 1000000000),
-    lastModified: new ServerTime(node.trashTime?.getTime() ?? 1000000000),
-    createdBy: getAuthorName(node),
-    // Properties below are irrelevant for trashed items
-    isSharedWithMe: false,
-    shareId: '',
-    location: { type: 'root' },
+function createOrUpdateItem(previousItems: RecentDocumentsItemValue[], node: NodeEntity | DegradedNode) {
+  const existingItemIndex = previousItems.findIndex((item) => node.uid === generateNodeUid(item.volumeId, item.linkId))
+  if (existingItemIndex >= 0) {
+    const updatedItems = [...previousItems]
+    updatedItems[existingItemIndex] = nodeToTrashedItemValue(node)
+    return updatedItems
+  } else {
+    return [...previousItems, nodeToTrashedItemValue(node)]
   }
+}
+
+function removeMissingItems(previousItems: RecentDocumentsItemValue[], event: { nodeUid: string }) {
+  return previousItems.filter((item) => event.nodeUid !== generateNodeUid(item.volumeId, item.linkId))
 }
