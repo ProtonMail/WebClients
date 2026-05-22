669dba2315bf7fb63cdf27235896069a5bf43d7b
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-05-22 16:14:21 +0200

    refac: remove the include15Minutes parameter

--- packages/shared/lib/calendar/eventDefaults.ts
+++ packages/shared/lib/calendar/eventDefaults.ts
@@ -5,23 +5,17 @@ export interface DurationOption {
     value: number;
 }
 
-export const getCalendarEventDefaultDuration = (options?: {
-    shortLabels?: boolean;
-    include15Minutes?: boolean;
-}): DurationOption[] => {
-    const durations: DurationOption[] = [];
-    const { shortLabels = false, include15Minutes = false } = options || {};
+export const getCalendarEventDefaultDuration = (options?: { shortLabels?: boolean }): DurationOption[] => {
+    const { shortLabels = false } = options || {};
 
-    if (include15Minutes) {
-        durations.push({ text: shortLabels ? c('Duration').t`15m` : c('Duration').t`15 minutes`, value: 15 });
-    }
-
-    durations.push(
+    return [
         { text: shortLabels ? c('Duration').t`30m` : c('Duration').t`30 minutes`, value: 30 },
         { text: shortLabels ? c('Duration').t`60m` : c('Duration').t`60 minutes`, value: 60 },
         { text: shortLabels ? c('Duration').t`90m` : c('Duration').t`90 minutes`, value: 90 },
-        { text: shortLabels ? c('Duration').t`120m` : c('Duration').t`120 minutes`, value: 120 }
-    );
+        { text: shortLabels ? c('Duration').t`120m` : c('Duration').t`120 minutes`, value: 120 },
+    ];
+};
 
-    return durations;
+export const getBookingEventDurationOptions = (): DurationOption[] => {
+    return [{ text: c('Duration').t`15m`, value: 15 }, ...getCalendarEventDefaultDuration({ shortLabels: true })];
 };
