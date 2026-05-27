50e6b81424562f637ac477887964b392f2d00cb9
Edvin CANDON <edvin.candon@proton.ch>
2026-05-27 15:55:54 +0200

    Eagerly init `registerStoreEffect` cache

--- packages/pass/store/connect/effect.ts
+++ packages/pass/store/connect/effect.ts
@@ -1,15 +1,9 @@
 import type { Store } from 'redux';
 
-import type { Maybe } from '@proton/pass/types';
-
 type Selector<S, T> = (state: S) => T;
 
-export const registerStoreEffect = <S, T>(
-    store: Store<S>,
-    selector: Selector<S, T>,
-    effect: (value: T) => void
-): (() => void) => {
-    let cache: Maybe<T>;
+export const registerStoreEffect = <S, T>(store: Store<S>, selector: Selector<S, T>, effect: (value: T) => void): (() => void) => {
+    let cache: T = selector(store.getState());
 
     return store.subscribe(() => {
         const value = selector(store.getState());
