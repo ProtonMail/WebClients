2c3778f897682127b9b158c6cabb6bf905b19fb3
Maciej Lenartowicz <maciej.lenartowicz@proton.ch>
2026-05-28 13:48:58 +0200

    [REALTIME-240] Homepage SDK - folder events

---
+++ applications/docs/src/app/routes/(user)/(homepage)/recents/__utils/use-recents-store.ts
@@ -0,0 +1,61 @@
+import type { RecentDocumentsItemValue } from '@proton/docs-core/lib/Services/recent-documents'
+import { generateNodeUid } from '@protontech/drive-sdk'
+import { create } from 'zustand'
+
+interface RecentsStore {
+  recentDocuments: { [nodeUid: string]: RecentDocumentsItemValue }
+  setRecentDocuments: (documents: RecentDocumentsItemValue[]) => void
+  setDocument: (updatedDocument: RecentDocumentsItemValue) => void
+  removeDocument: (nodeUid: string) => void
+  removeChildrenOf: (parentFolderUid: string) => void
+  recentDocumentsInitialized: boolean
+  setInitialized: () => void
+}
+
+export const useRecentsStore = create(
+  (set): RecentsStore => ({
+    recentDocuments: {},
+
+    setRecentDocuments: (documents) =>
+      set(() => {
+        const recentDocuments: { [nodeUid: string]: RecentDocumentsItemValue } = {}
+        for (const document of documents) {
+          recentDocuments[generateNodeUid(document.volumeId, document.linkId)] = document
+        }
+        return { recentDocuments }
+      }),
+
+    setDocument: (updatedDocument) =>
+      set((state) => ({
+        recentDocuments: {
+          ...state.recentDocuments,
+          [generateNodeUid(updatedDocument.volumeId, updatedDocument.linkId)]: updatedDocument,
+        },
+      })),
+
+    removeDocument: (nodeUid) =>
+      set((state) => {
+        const { [nodeUid]: _, ...rest } = state.recentDocuments
+        return { recentDocuments: rest }
+      }),
+
+    removeChildrenOf: (parentFolderUid) =>
+      set((state) => {
+        const filteredDocuments: { [nodeUid: string]: RecentDocumentsItemValue } = {}
+
+        for (const documentNodeUid in state.recentDocuments) {
+          const document = state.recentDocuments[documentNodeUid]
+          if (document.ancestorsNodeUids?.includes(parentFolderUid)) {
+            continue
+          }
+          filteredDocuments[documentNodeUid] = document
+        }
+
+        return { recentDocuments: filteredDocuments }
+      }),
+
+    recentDocumentsInitialized: false,
+
+    setInitialized: () => set(() => ({ recentDocumentsInitialized: true })),
+  }),
+)
--- applications/docs/src/app/utils/drive-sdk.ts
+++ applications/docs/src/app/utils/drive-sdk.ts
@@ -1,5 +1,19 @@
 import type { MaybeNode, ProtonDriveClient } from '@proton/drive'
 
+export async function getFullPath(drive: ProtonDriveClient, nodeUid: string) {
+  const path: string[] = []
+
+  const ancestry = await getNodeAncestry(drive, nodeUid, false)
+  const [_root, ...children] = ancestry
+  for (const ancestor of children) {
+    if (ancestor.ok) {
+      path.push(ancestor.value.name)
+    }
+  }
+
+  return { path, ancestry }
+}
+
 export async function getNodeAncestry(drive: ProtonDriveClient, nodeUid: string, includeSelf: boolean = true) {
   const ancestors: MaybeNode[] = []
 
@@ -31,4 +45,16 @@ export async function getNodeParent(drive: ProtonDriveClient, maybeNode: MaybeNo
 
 export function getParentUid(node: MaybeNode) {
   return node.ok ? node.value.parentUid : node.error.parentUid
+}
+
+export function extractNodeUid(node: MaybeNode) {
+  return node.ok ? node.value.uid : node.error.uid
+}
+
+export async function isSharedWithUser(drive: ProtonDriveClient, user: { Email: string }, nodeUid: string) {
+  const sharingInfo = await drive.getSharingInfo(nodeUid)
+  const currentUserSharingMembership = sharingInfo?.members.find((member) => {
+    return member.inviteeEmail === user.Email
+  })
+  return !!currentUserSharingMembership
 }
