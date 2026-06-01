4a8aca868a626e072387e90c9c456d7410f7c01b
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-06-01 07:44:39 +0200

    fix(mail-settings): improve loading state for web page ET settings

--- applications/account/src/lite/actions/LabelsSettings.tsx
+++ applications/account/src/lite/actions/LabelsSettings.tsx
@@ -1,12 +1,26 @@
-import type { ReactNode } from 'react';
-
 import { FoldersSection, LabelsSection } from '@proton/components';
+import { useFolders, useLabels } from '@proton/mail/store/labels/hooks';
+import { useMailSettings } from '@proton/mail/store/mailSettings/hooks';
 
 import MobileSection from '../components/MobileSection';
 
 import './MobileSettings.scss';
 
-const LabelsSettings = ({ layout }: { layout: (children: ReactNode, props?: any) => ReactNode }) => {
+interface LabelsSettingsProps {
+    layout: (children: React.ReactNode, props?: any) => React.ReactNode;
+    loader: React.ReactNode;
+}
+
+const LabelsSettings = ({ layout, loader }: LabelsSettingsProps) => {
+    const [, loadingFolders] = useFolders();
+    const [, loadingMailSettings] = useMailSettings();
+    const [, loadingLabels] = useLabels();
+
+    const loading = loadingFolders || loadingMailSettings || loadingLabels;
+    if (loading) {
+        return loader;
+    }
+
     return layout(
         <div className="mobile-settings">
             <MobileSection>
