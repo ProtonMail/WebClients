d19ab4c44288546053a27ac6c1cb7db07a2ee576
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-06-05 09:40:55 +0200

    feat(category): add link to categories settings in quick settings

--- applications/mail/src/app/components/categoryView/CategoriesQuickSettings.tsx
+++ applications/mail/src/app/components/categoryView/CategoriesQuickSettings.tsx
@@ -1,19 +1,27 @@
 import { c } from 'ttag';
 
+import { Href } from '@proton/atoms/Href/Href';
 import QuickSettingsSectionRow from '@proton/components/components/drawer/views/quickSettings/QuickSettingsSectionRow';
 import DrawerAppSection from '@proton/components/components/drawer/views/shared/DrawerAppSection';
 import Info from '@proton/components/components/link/Info';
 import Toggle from '@proton/components/components/toggle/Toggle';
+import useAuthentication from '@proton/components/hooks/useAuthentication';
 import { useCategoriesData } from '@proton/mail/features/categoriesView/useCategoriesData';
 import { useCategoryToggle } from '@proton/mail/features/categoriesView/useCategoryToggle';
+import { getAppHref } from '@proton/shared/lib/apps/helper';
+import { APPS } from '@proton/shared/lib/constants';
 
 export const CategoriesQuickSettings = () => {
     const { hasAccessToCategoryView } = useCategoriesData();
     const { handleChange, state, loading } = useCategoryToggle();
 
+    const authentication = useAuthentication();
+
     if (!hasAccessToCategoryView) {
         return null;
     }
+
+    const href = getAppHref('mail/general#categories', APPS.PROTONACCOUNT, authentication.localID);
 
     return (
         <DrawerAppSection>
@@ -30,6 +38,7 @@ export const CategoriesQuickSettings = () => {
                     />
                 }
             />
+            <Href href={href} className="color-weak text-sm">{c('Label').t`Manage categories`}</Href>
         </DrawerAppSection>
     );
 };
