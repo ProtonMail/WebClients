a8f7382868350bbe67596a36ece9de0675f36854
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-06-05 09:55:59 +0200

    refactor(category): use common hook to handle categories view toggle

--- applications/account/src/lite/components/CategoryView/CategoriesToggle.tsx
+++ applications/account/src/lite/components/CategoryView/CategoriesToggle.tsx
@@ -1,37 +1,14 @@
 import { c } from 'ttag';
 
 import Toggle from '@proton/components/components/toggle/Toggle';
-import useApi from '@proton/components/hooks/useApi';
-import useNotifications from '@proton/components/hooks/useNotifications';
-import useToggle from '@proton/components/hooks/useToggle';
-import useLoading from '@proton/hooks/useLoading';
-import { mailSettingsActions } from '@proton/mail/store/mailSettings';
-import { useMailSettings } from '@proton/mail/store/mailSettings/hooks';
-import { useDispatch } from '@proton/redux-shared-store/sharedProvider';
-import { updateMailCategoryView } from '@proton/shared/lib/api/mailSettings';
-import type { MailSettings } from '@proton/shared/lib/interfaces/MailSettings';
+import { useCategoryToggle } from '@proton/mail/features/categoriesView/useCategoryToggle';
 
 import MobileSection from '../MobileSection';
 import MobileSectionLabel from '../MobileSectionLabel';
 import MobileSectionRow from '../MobileSectionRow';
 
 export const CategoriesToggle = () => {
-    const api = useApi();
-    const dispatch = useDispatch();
-
-    const [mailSettings] = useMailSettings();
-
-    const { createNotification } = useNotifications();
-    const [toggleCategory, withLoading] = useLoading();
-    const { state, toggle } = useToggle(mailSettings.MailCategoryView);
-
-    const handleToggle = async (checked: boolean) => {
-        const response = await api<{ MailSettings: MailSettings }>(updateMailCategoryView(checked));
-        dispatch(mailSettingsActions.updateMailSettings(response.MailSettings));
-        createNotification({ text: c('Success').t`Preference saved` });
-
-        toggle();
-    };
+    const { handleChange, loading, state } = useCategoryToggle();
 
     return (
         <MobileSection>
@@ -40,8 +17,8 @@ export const CategoriesToggle = () => {
                 <Toggle
                     id="category-view"
                     checked={state}
-                    onChange={({ target }) => withLoading(handleToggle(target.checked))}
-                    loading={toggleCategory}
+                    onChange={({ target }) => handleChange({ checked: target.checked, notification: true })}
+                    loading={loading}
                 />
             </MobileSectionRow>
         </MobileSection>
