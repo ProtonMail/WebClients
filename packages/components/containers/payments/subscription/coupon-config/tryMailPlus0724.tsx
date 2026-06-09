8275dd852b0de63285e973201e4493f46c80faa9
Corentin Reungoat <corentin.reungoat@proton.ch>
2026-06-09 16:01:05 +0000

    Hide addons and irrelevant cycles when Mail Plus intro pricing coupon is applied

---
+++ packages/components/containers/payments/subscription/coupon-config/tryMailPlus0724.tsx
@@ -0,0 +1,12 @@
+import { COUPON_CODES, CYCLE } from '@proton/payments/core/constants';
+
+import type { CouponConfig } from './interface';
+
+// Hide distractions for users checking out with the Mail Plus intro pricing coupon
+export const tryMailPlus0724Config: CouponConfig = {
+    coupons: COUPON_CODES.TRYMAILPLUS0724,
+    hidden: false,
+    availableCycles: [CYCLE.MONTHLY],
+    hideLumoAddonBanner: true,
+    hideMeetAddonBanner: true,
+};
