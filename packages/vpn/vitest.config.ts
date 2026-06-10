61a7267cc9a6bf8a7f4f8eec224d13dcb2109755
Paolo Chiabrera <paolo.chiabrera@proton.ch>
2026-06-10 10:58:38 +0200

    chore: centralize vitest config

--- packages/vpn/vitest.config.ts
+++ packages/vpn/vitest.config.ts
@@ -1,28 +1,14 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import { mergeConfig } from 'vitest/config';
 
-export default defineConfig({
-    plugins: [react() as any],
+import { sharedVitestConfig } from '@proton/testing/configs/vitest.config';
+
+export default mergeConfig(sharedVitestConfig, {
+    plugins: [react()],
     test: {
-        globals: true,
-        environment: 'happy-dom',
-        reporters: [
-            [
-                'default',
-                {
-                    summary: false,
-                },
-            ],
-        ],
         setupFiles: './vitest.setup.ts',
         coverage: {
-            provider: 'v8',
-            reporter: ['text-summary', 'json', 'clover', 'html', 'cobertura'],
-            include: ['src/**/*.{js,jsx,ts,tsx}'],
             exclude: ['**/*.d.ts', '**/*.test.ts', '**/*.test.tsx', '**/*.stories.tsx'],
         },
-    },
-    resolve: {
-        conditions: ['browser'],
     },
 });
--- packages/wallet/vitest.config.ts
+++ packages/wallet/vitest.config.ts
@@ -1,30 +1,12 @@
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
-import { defineConfig } from 'vitest/config';
+import { mergeConfig } from 'vitest/config';
 
-export default defineConfig({
+import { sharedVitestConfig } from '@proton/testing/configs/vitest.config';
+
+export default mergeConfig(sharedVitestConfig, {
     plugins: [react(), wasm()],
     test: {
-        globals: true,
-        environment: 'happy-dom',
-        reporters: [
-            [
-                'default',
-                {
-                    summary: false,
-                },
-            ],
-        ],
         setupFiles: './vitest.setup.ts',
-        coverage: {
-            provider: 'v8',
-            reporter: ['text-summary', 'json', 'clover', 'html', 'cobertura'],
-            include: ['src/**/*.{js,jsx,ts,tsx}'],
-            exclude: ['**/*.d.ts', '**/*.test.ts', '**/*.test.tsx'],
-        },
-        maxWorkers: 1,
-    },
-    resolve: {
-        conditions: ['browser'],
     },
 });
