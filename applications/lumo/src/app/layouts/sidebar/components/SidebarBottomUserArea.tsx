e4b3b972b621ecafedcdad12d2790c435cb8021e
Elena Atencio <elena.atencio@proton.ch>
2026-06-19 10:16:13 +0000

    Unify lumo user dropdown for web and native mobile with lumo 2.0 styling

--- applications/lumo/src/app/layouts/sidebar/components/SidebarBottomUserArea.tsx
+++ applications/lumo/src/app/layouts/sidebar/components/SidebarBottomUserArea.tsx
@@ -1,6 +1,7 @@
 import { UserDropdown, useConfig } from '@proton/components';
 
 import LumoUserDropdown from '../../../components/LumoUserDropdown/LumoUserDropdown';
+import LumoUserDropdownContent from '../../../components/LumoUserDropdown/LumoUserDropdownContent';
 import { useLumoAuthAction } from '../../../hooks/useLumoAuthAction';
 import { useIsGuest } from '../../../providers/IsGuestProvider';
 import { useSidebar } from '../../../providers/SidebarProvider';
@@ -15,21 +16,27 @@ export const SidebarBottomUserArea = () => {
         return null;
     }
 
-    const DropdownComponent = isNativeAuthEnabled ? LumoUserDropdown : UserDropdown;
+    const dropdownProps = {
+        app: APP_NAME,
+        dropdownIcon: undefined as undefined,
+        className: isSmallScreen ? 'border-none' : undefined,
+    };
+
+    const userDropdown = isNativeAuthEnabled ? (
+        <LumoUserDropdown {...dropdownProps} />
+    ) : (
+        <UserDropdown {...dropdownProps}>
+            <LumoUserDropdownContent />
+        </UserDropdown>
+    );
 
     if (isSmallScreen) {
-        return (
-            <div className="sidebar-bottom-user-dropdown mobile-user-dropdown shrink-0 w-full">
-                <DropdownComponent app={APP_NAME} dropdownIcon={undefined} className="border-none" />
-            </div>
-        );
+        return <div className="sidebar-bottom-user-dropdown mobile-user-dropdown shrink-0 w-full">{userDropdown}</div>;
     }
 
     return (
         <div className="desktop-sidebar-user-dropdown flex flex-row flex-nowrap items-center gap-2 justify-space-between">
-            <div className="sidebar-item-text w-full">
-                <DropdownComponent app={APP_NAME} dropdownIcon={undefined} />
-            </div>
+            <div className="sidebar-item-text w-full">{userDropdown}</div>
         </div>
     );
 };
