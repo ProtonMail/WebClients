122f07b77666e4072ef3f5bee52b64f31bb8ab6a
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-06-24 16:51:27 +0200

    chore(mail): fix lookbehind compat issue on mail app

--- applications/mail/eslint.config.mjs
+++ applications/mail/eslint.config.mjs
@@ -49,9 +49,6 @@ export default defineConfig([
             '@typescript-eslint/no-restricted-imports': ['error', { paths: iconRestrictedImports }],
             // TODO: Remove this rule once the cycle dependency is fixed
             'import/no-cycle': 'off',
-
-            // TODO: Remove this rule once the compat issue is resolved
-            'compat/compat': 'off',
         },
     },
 ]);
