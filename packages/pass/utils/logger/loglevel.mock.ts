04ae61b792ad64034628756da222b6cda48989d6
Edvin CANDON <edvin.candon@proton.ch>
2026-06-24 14:12:18 +0200

    Simplify log mock

--- packages/pass/utils/logger/loglevel.mock.ts
+++ packages/pass/utils/logger/loglevel.mock.ts
@@ -1,29 +1,7 @@
 /* eslint-env jest */
+import type log from 'loglevel';
 
-/**
- * Manual mock for `loglevel` used by pass jest setups (`jest.mock('loglevel', () => require(...))`).
- */
-const createLoglevelMock = () => {
-    const logger: any = {
-        methodFactory: jest.fn(),
-        setLevel: jest.fn(),
-        setDefaultLevel: jest.fn(),
-        getLevel: jest.fn(),
-        enableAll: jest.fn(),
-        disableAll: jest.fn(),
-        rebuild: jest.fn(),
-        trace: jest.fn(),
-        debug: jest.fn(),
-        info: jest.fn(),
-        warn: jest.fn(),
-        error: jest.fn(),
-        log: jest.fn(),
-    };
+const loglevel = jest.createMockFromModule<typeof log>('loglevel');
+loglevel.getLogger = jest.fn(() => loglevel);
 
-    logger.getLogger = jest.fn(() => logger);
-    logger.getLoggers = jest.fn(() => ({}));
-
-    return logger;
-};
-
-export default createLoglevelMock();
+export default loglevel;
