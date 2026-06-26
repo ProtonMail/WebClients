6d1029284845147864fc51ea7d0d4ae934d5a08c
Vijayasingam Thanasekaran <vijayasingam.thanasekaran@proton.ch>
2026-06-26 12:14:42 +0100

    Add currency switcher to referral signup page

--- applications/account/src/app/signupCtx/flows/referral/components/Layout/Footer.tsx
+++ applications/account/src/app/signupCtx/flows/referral/components/Layout/Footer.tsx
@@ -1,6 +1,8 @@
 import { c } from 'ttag';
 
 import { Href } from '@proton/atoms/Href/Href';
+import CurrencySelector from '@proton/components/containers/payments/CurrencySelector';
+import { usePaymentOptimistic } from '@proton/payments/ui';
 import { APPS } from '@proton/shared/lib/constants';
 import { getPrivacyPolicyURL } from '@proton/shared/lib/helpers/url';
 import { locales } from '@proton/shared/lib/i18n/locales';
@@ -10,9 +12,11 @@ import LanguageSelect from '../../../../../public/LanguageSelect';
 import SignupSupportDropdown from '../../../../../signup/SignupSupportDropdown';
 
 export const Footer = () => {
+    const payments = usePaymentOptimistic();
+
     return (
         <footer
-            className="flex justify-center items-center p-4 gap-8 mt-auto"
+            className="flex justify-center items-center p-4 mt-auto"
             style={{
                 '--link-norm': 'var(--text-weak)',
                 '--primary': 'var(--text-weak)',
@@ -21,18 +25,27 @@ export const Footer = () => {
         >
             <Href
                 key="privacy"
-                className="signup-link link-focus text-no-decoration"
+                className="signup-link link-focus text-no-decoration mr-6"
                 href={getPrivacyPolicyURL(APPS.PROTONDRIVE)}
             >{c('Link').t`Privacy policy`}</Href>
             <Href
                 key="terms"
-                className="signup-link link-focus text-no-decoration"
+                className="signup-link link-focus text-no-decoration mr-6"
                 href={getLocaleTermsURL(APPS.PROTONDRIVE)}
             >
                 {c('Link').t`Terms`}
             </Href>
             <SignupSupportDropdown />
-            <LanguageSelect locales={locales} globe={true} />
+            <div>
+                <CurrencySelector
+                    currencies={payments.availableCurrencies}
+                    currency={payments.checkoutUi.currency}
+                    onSelect={payments.selectCurrency}
+                    mode="select-two"
+                    className="color-primary interactive-pseudo interactive--no-background border-none ml-2"
+                />
+            </div>
+            <LanguageSelect locales={locales} globe={true} className="ml-3" />
         </footer>
     );
 };
