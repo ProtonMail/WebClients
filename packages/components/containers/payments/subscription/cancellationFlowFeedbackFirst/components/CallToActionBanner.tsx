431951914d15c7f8b9d1663a481b81c6adfe6768
Fraser Dempster <fraser.dempster@proton.ch>
2026-06-29 13:03:22 +0200

    fixed settings link so that multiple logged in accounts do not lead to switch page

--- packages/components/containers/payments/subscription/cancellationFlowFeedbackFirst/components/CallToActionBanner.tsx
+++ packages/components/containers/payments/subscription/cancellationFlowFeedbackFirst/components/CallToActionBanner.tsx
@@ -1,5 +1,9 @@
 import { Href } from '@proton/atoms/Href/Href';
+import SettingsLink from '@proton/components/components/link/SettingsLink';
+import { APPS } from '@proton/shared/lib/constants';
 import clsx from '@proton/utils/clsx';
+
+const ctaClassName = 'shrink-0 text-no-decoration hover:text-underline text-no-wrap text-lg w-full md:w-auto';
 
 interface Props {
     image: string;
@@ -8,27 +12,35 @@ interface Props {
     ctaText: string;
     ctaHref: string;
     index: number;
+    isSettingsLink?: boolean;
 }
 
-export const CallToActionBanner = ({ image, title, description, ctaText, ctaHref, index }: Props) => {
+export const CallToActionBanner = ({
+    image,
+    title,
+    description,
+    ctaText,
+    ctaHref,
+    index,
+    isSettingsLink = false,
+}: Props) => {
     return (
-        <div
-            key={title}
-            className={clsx('flex flex-wrap items-center gap-4 p-6', index !== 0 && 'border-top border-weak')}
-        >
+        <div className={clsx('flex flex-wrap items-center gap-4 p-6', index !== 0 && 'border-top border-weak')}>
             {image && <img src={image} alt="" width={56} height={56} className="shrink-0" />}
             <div className="flex-1 flex flex-column md:flex-row flex-nowrap gap-2 items-center">
                 <div className="flex flex-column flex-nowrap md:flex-1">
                     <p className="text-bold m-0">{title}</p>
                     <p className="color-weak m-0">{description}</p>
                 </div>
-                <Href
-                    target="_self"
-                    href={ctaHref}
-                    className="shrink-0 text-no-decoration hover:text-underline text-no-wrap text-lg w-full md:w-auto"
-                >
-                    {ctaText}
-                </Href>
+                {isSettingsLink ? (
+                    <SettingsLink app={APPS.PROTONMAIL} path={ctaHref} target="_self" className={ctaClassName}>
+                        {ctaText}
+                    </SettingsLink>
+                ) : (
+                    <Href target="_self" href={ctaHref} className={ctaClassName}>
+                        {ctaText}
+                    </Href>
+                )}
             </div>
         </div>
     );
