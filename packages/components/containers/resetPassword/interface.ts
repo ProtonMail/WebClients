a7d6e36fc58aeeec971d9c8b681129db73a5ae9d
Vijayasingam Thanasekaran <vijayasingam.thanasekaran@proton.ch>
2026-06-30 09:50:43 +0100

    Move reset password params close to usage

--- packages/components/containers/resetPassword/interface.ts
+++
@@ -1,8 +0,0 @@
-import type { AuthResponse } from '@proton/shared/lib/authentication/interface';
-import type { Api, DecryptedKey } from '@proton/shared/lib/interfaces';
-
-export interface MnemonicData {
-    api: Api;
-    decryptedUserKeys: DecryptedKey[];
-    authResponse: AuthResponse;
-}
