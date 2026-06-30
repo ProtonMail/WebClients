b267e7aab5dd7f5e5ff51fbb96247ce82188c148
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-06-30 08:48:54 +0200

    feat(mail): create spotlight content for categories view onboarding

--- applications/mail/src/app/components/categoryView/categoriesOnboarding/onboardingInterface.ts
+++ applications/mail/src/app/components/categoryView/categoriesOnboarding/onboardingInterface.ts
@@ -11,6 +11,10 @@ export interface OnboardingInfo {
 
 export enum CategoriesOnboardingFlags {
     INITIAL_MODAL = 1 << 0,
+    SPOTLIGHT_MESSAGE = 1 << 1,
+    SPOTLIGHT_CATEGORIZE = 1 << 2,
+    SPOTLIGHT_CUSTOMIZE = 1 << 3,
+    SPOTLIGHT_FREE_USERS = 1 << 4,
 }
 
 export const FeatureValueDefault = -1 as const;
