c59c996dbb9af09bf32d9d4961a6273bfe2204cc
Vijayasingam Thanasekaran <vijayasingam.thanasekaran@proton.ch>
2026-06-30 10:10:39 +0100

    Update ASR to skip auto ownership verification

---
+++ packages/account/safetyReview/components/actions/accountRecovery/email/UpdateRecoveryEmail.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+
+import { c } from 'ttag';
+
+import { updateRecoveryEmailValue } from '@proton/account/recovery/accountRecoveryActions';
+import { SafetyReviewCta } from '@proton/account/safetyReview/components/SafetyReviewCta';
+import { SafetyReviewCardHeader } from '@proton/account/safetyReview/components/cards/SafetyReviewCardHeader';
+import type { SafetyReviewAllProps } from '@proton/account/safetyReview/components/interface';
+import type { ExtractRecoveryActionItem } from '@proton/account/safetyReview/recoveryState/recoveryState.ts';
+import InputFieldTwo from '@proton/components/components/v2/field/InputField';
+import useFormErrors from '@proton/components/components/v2/useFormErrors';
+import { useTheme } from '@proton/components/containers/themes/ThemeProvider';
+import useLoading from '@proton/hooks/useLoading';
+import { useDispatch } from '@proton/redux-shared-store/sharedProvider';
+import { emailValidator, requiredValidator } from '@proton/shared/lib/helpers/formValidators';
+import noop from '@proton/utils/noop';
+
+import darkRecoveryEmailIllustration from '../../../assets/recovery-email-dark.svg';
+import recoveryEmailIllustration from '../../../assets/recovery-email.svg';
+
+type Props = SafetyReviewAllProps & {
+    recoveryItem: ExtractRecoveryActionItem<'verifyRecoveryEmail'>;
+    onCompleted: () => void;
+    onSkip: () => void;
+};
+export const UpdateRecoveryEmail = (props: Props) => {
+    const theme = useTheme();
+    const isDarkTheme = theme.information.dark;
+    const [recoveryEmail, setRecoveryEmail] = useState(props.recoveryItem.recoveryItem.data.value);
+    const dispatch = useDispatch();
+    const [loading, withLoading] = useLoading();
+
+    const { validator, onFormSubmit } = useFormErrors();
+
+    return (
+        <form
+            id={props.firstItemId}
+            onSubmit={(event) => {
+                event.preventDefault();
+                if (!onFormSubmit()) {
+                    return;
+                }
+                withLoading(
+                    (async function () {
+                        await dispatch(updateRecoveryEmailValue({ value: recoveryEmail, persistPasswordScope: true }));
+                        props.onCompleted();
+                    })()
+                ).catch(noop);
+            }}
+        >
+            <SafetyReviewCardHeader>
+                <SafetyReviewCardHeader.Illustration>
+                    <img
+                        src={isDarkTheme ? darkRecoveryEmailIllustration : recoveryEmailIllustration}
+                        alt=""
+                        width={64}
+                        height={64}
+                    />
+                </SafetyReviewCardHeader.Illustration>
+                <SafetyReviewCardHeader.Title>{c('safety_review')
+                    .t`Update recovery email`}</SafetyReviewCardHeader.Title>
+            </SafetyReviewCardHeader>
+
+            <InputFieldTwo
+                label={c('Label').t`Recovery email address`}
+                disableChange={loading}
+                type="email"
+                autoComplete="email"
+                autoFocus
+                bigger
+                id="recovery-email-input"
+                value={recoveryEmail}
+                onValue={setRecoveryEmail}
+                error={validator([requiredValidator(recoveryEmail), emailValidator(recoveryEmail)])}
+            />
+
+            <p className="m-0 mt-2">{c('safety_review')
+                .t`To make sure this address is really yours, we’ll send an email with a verification code.`}</p>
+
+            <SafetyReviewCta {...props} onSkip={props.onSkip} loading={loading} cta={c('safety_review').t`Update`} />
+        </form>
+    );
+};
---
+++ packages/account/safetyReview/components/actions/accountRecovery/phone/SendCodeToVerifyPhone.tsx
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+
+import { c } from 'ttag';
+
+import { SafetyReviewCta } from '@proton/account/safetyReview/components/SafetyReviewCta';
+import type { SafetyReviewAllProps } from '@proton/account/safetyReview/components/interface';
+import type { ExtractRecoveryActionItem } from '@proton/account/safetyReview/recoveryState/recoveryState';
+import { InlineLinkButton } from '@proton/atoms/InlineLinkButton/InlineLinkButton';
+import { useTheme } from '@proton/components/containers/themes/ThemeProvider';
+
+import darkRecoveryPhoneIllustration from '../../../assets/recovery-phone-dark.svg';
+import recoveryPhoneIllustration from '../../../assets/recovery-phone.svg';
+import { SafetyReviewCardHeader } from '../../../cards/SafetyReviewCardHeader';
+import { UpdateRecoveryPhone } from './UpdateRecoveryPhone';
+
+type Props = SafetyReviewAllProps & {
+    recoveryItem: ExtractRecoveryActionItem<'verifyRecoveryPhone'>;
+    onSkip: () => void;
+    onSendCode: () => void;
+    onUpdate: () => void;
+};
+export const SendCodeToVerifyPhone = (props: Props) => {
+    const theme = useTheme();
+    const isDarkTheme = theme.information.dark;
+
+    const [updateRecoveryPhone, setShouldUpdateRecoveryPhone] = useState(false);
+
+    const updatePhoneLink = (
+        <InlineLinkButton key="resend" onClick={() => setShouldUpdateRecoveryPhone(true)} className="mt-2">
+            {c('Safety review').t`update it now`}
+        </InlineLinkButton>
+    );
+
+    if (updateRecoveryPhone) {
+        return (
+            <UpdateRecoveryPhone
+                {...props}
+                onCompleted={props.onSendCode}
+                onSkip={() => {
+                    setShouldUpdateRecoveryPhone(false);
+                }}
+            />
+        );
+    }
+
+    return (
+        <form
+            id={props.firstItemId}
+            onSubmit={(event) => {
+                event.preventDefault();
+                props.onSendCode();
+            }}
+        >
+            <SafetyReviewCardHeader>
+                <SafetyReviewCardHeader.Illustration>
+                    <img
+                        src={isDarkTheme ? darkRecoveryPhoneIllustration : recoveryPhoneIllustration}
+                        alt=""
+                        width={64}
+                        height={64}
+                    />
+                </SafetyReviewCardHeader.Illustration>
+                <SafetyReviewCardHeader.Title>{c('safety_review')
+                    .t`Verify recovery phone`}</SafetyReviewCardHeader.Title>
+                <SafetyReviewCardHeader.Description>
+                    {c('safety_review')
+                        .t`Verifying your phone number increases your account security and allows additional options for recovery.`}
+                </SafetyReviewCardHeader.Description>
+            </SafetyReviewCardHeader>
+            <div className="mb-8 p-6 rounded-lg bg-weak border text-semibold text-center text-lg">
+                {props.recoveryItem.recoveryItem.data.value}
+            </div>
+
+            <div className="flex gap-8">
+                <div>
+                    {c('safety_review')
+                        .t`To make sure this number is really yours, we’ll send an SMS with a verification code.`}
+                </div>
+                <div>{c('safety_review').jt`If your recovery phone has changed, ${updatePhoneLink}.`}</div>
+            </div>
+            <SafetyReviewCta {...props} onSkip={props.onSkip} cta={c('safety_review').t`Send verification code`} />
+        </form>
+    );
+};
