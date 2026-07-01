ea14c49905a873ff14540d1aaa024521ccff1ab9
Joel Prat Vilanova <joel.pratvilanova@proton.ch>
2026-07-01 11:59:44 +0000

    VPNPLG-95: Modify page load event

--- packages/components/hooks/useDashboardPageLoadTelemetry.ts
+++ packages/components/hooks/useDashboardPageLoadTelemetry.ts
@@ -5,25 +5,28 @@ import type { APP_NAMES } from '@proton/shared/lib/constants';
 import { sendTelemetryReport } from '@proton/shared/lib/helpers/metrics';
 import type { FeatureFlagsWithVariant } from '@proton/unleash/UnleashFeatureFlagsVariants';
 
+import type { TelemetryUserTier } from '../helpers/getTelemetryUserTier';
 import { default as useShowDashboard } from './accounts/useShowDashboard';
 import useApi from './useApi';
 
 const useDashboardPageLoadTelemetry = ({
     app,
     dashboardName,
+    userTier,
 }: {
     app: APP_NAMES;
     dashboardName: FeatureFlagsWithVariant;
+    userTier: TelemetryUserTier;
 }) => {
     const api = useApi();
-    const { variant, canShowDashboard } = useShowDashboard(app, dashboardName);
+    const { canShowDashboard } = useShowDashboard(app, dashboardName);
 
     const sendTelemetry = async () => {
         await sendTelemetryReport({
             api,
             measurementGroup: TelemetryMeasurementGroups.accountDashboard,
             event: TelemetryAccountDashboardEvents.pageLoad,
-            dimensions: { variant: variant.name, app },
+            dimensions: { app, user_tier: userTier },
             delay: false,
         });
     };
@@ -32,7 +35,7 @@ const useDashboardPageLoadTelemetry = ({
         if (canShowDashboard) {
             void sendTelemetry();
         }
-    }, [variant, canShowDashboard]);
+    }, [canShowDashboard]);
 };
 
 export default useDashboardPageLoadTelemetry;
