1eb3a58c8730becbc19f9a11ef4e2b0fa09c4754
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-07-02 15:29:06 +0200

    feat(mail): remove the unread count from the category tab

--- applications/mail/src/app/components/categoryView/categoriesTabs/TabNewCount.tsx
+++ applications/mail/src/app/components/categoryView/categoriesTabs/TabNewCount.tsx
@@ -1,27 +1,9 @@
 import { c } from 'ttag';
 
-import type { CategoryTab } from '@proton/mail/features/categoriesView/categoriesConstants';
-
-import { selectLabelIDUnreadCount } from 'proton-mail/hooks/mailboxCounter/useMaiboxCounter.selector';
-import { useMailSelector } from 'proton-mail/store/hooks';
-
-interface Props {
-    category: CategoryTab;
-}
-
-export const TabNewCount = ({ category }: Props) => {
-    const count = useMailSelector((state) => selectLabelIDUnreadCount(state, category.id));
-    if (count <= 0) {
-        return null;
-    }
-
-    const displayCount = count > 99 ? '99+' : count;
+export const TabNewCount = () => {
     return (
         <span className="tab-new-count text-semibold mail-category-color" aria-hidden="true">
-            {
-                // translator: number of new mails, displayed on hover, e.g. "3 new" or "99+ new"
-                c('Info').t`${displayCount} new`
-            }
+            {c('Info').t`New messages`}
         </span>
     );
 };
