816dc86a0ddbb381ef38c4b1f7d2a365cb12742b
Redacted <redacted@invalid.proton.ch>
2026-07-06 17:36:02 +0200

    AddKeyModal: automatically re-enable delegated access on new account key

--- packages/account/userKeys/addUserKeyAction.ts
+++ packages/account/userKeys/addUserKeyAction.ts
@@ -4,11 +4,14 @@ import type { ProtonThunkArguments } from '@proton/redux-shared-store-types';
 import { CacheType } from '@proton/redux-utilities/interface';
 import { getSilentApi } from '@proton/shared/lib/api/helpers/customConfig';
 import type { KeyGenConfig, KeyGenConfigV6 } from '@proton/shared/lib/interfaces';
+import { DelegatedAccessStateEnum } from '@proton/shared/lib/interfaces/DelegatedAccess';
 import { addUserKeysProcess } from '@proton/shared/lib/keys';
 import { getIsDeviceRecoveryEnabled } from '@proton/shared/lib/recoveryFile/deviceRecovery';
 import { getIsRecoveryFileAvailable } from '@proton/shared/lib/recoveryFile/recoveryFile';
 
 import { type AddressesState, addressesThunk } from '../addresses';
+import type { DelegatedAccessState } from '../delegatedAccess';
+import { listOutgoingDelegatedAccess, updateDelegatedAccess } from '../delegatedAccess/outgoingActions';
 import type { KtState } from '../kt';
 import { type OrganizationKeyState, organizationKeyThunk } from '../organizationKey';
 import { userThunk } from '../user';
@@ -21,7 +24,7 @@ export const addUserKeyAction = ({
     keyGenConfig: KeyGenConfig | KeyGenConfigV6;
 }): ThunkAction<
     Promise<{ fingerprint: string }>,
-    UserKeysState & AddressesState & UserSettingsState & OrganizationKeyState & KtState,
+    UserKeysState & AddressesState & UserSettingsState & OrganizationKeyState & KtState & DelegatedAccessState,
     ProtonThunkArguments,
     UnknownAction
 > => {
@@ -43,6 +46,9 @@ export const addUserKeyAction = ({
                 userKeys,
             });
             const isDeviceRecoveryEnabled = getIsDeviceRecoveryEnabled(userSettings, extra.authentication);
+            const previouslyEnabledDelegatedAccesses = (await dispatch(listOutgoingDelegatedAccess())).filter(
+                (delegatedAccess) => delegatedAccess.State === DelegatedAccessStateEnum.Enabled
+            );
 
             const newKey = await addUserKeysProcess({
                 api,
@@ -56,6 +62,18 @@ export const addUserKeyAction = ({
                 passphrase: extra.authentication.getPassword(),
             });
             await dispatch(userThunk({ cache: CacheType.None }));
+
+            // re-enable delegated access recovery
+            await Promise.all(
+                previouslyEnabledDelegatedAccesses.map((delegatedAccess) => {
+                    return dispatch(
+                        updateDelegatedAccess({
+                            delegatedAccess,
+                            api: getSilentApi(extra.api),
+                        })
+                    );
+                })
+            );
             return { fingerprint: newKey.getFingerprint() };
         } finally {
             extra.eventManager.start();
