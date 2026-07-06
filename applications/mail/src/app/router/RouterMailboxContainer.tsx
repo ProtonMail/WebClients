09e14abdf884be77b837f37bd2a4eca98838e43b
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-07-06 09:54:53 +0200

    fix(mail): scroll to top when category change and refactor scroll to top hook

--- applications/mail/src/app/router/RouterMailboxContainer.tsx
+++ applications/mail/src/app/router/RouterMailboxContainer.tsx
@@ -1,5 +1,5 @@
-import { type RefObject, useEffect, useMemo, useState } from 'react';
-import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { useRetentionPolicies } from '@proton/account/retentionPolicies/hooks';
 import { useUser } from '@proton/account/user/hooks';
@@ -10,21 +10,18 @@ import PrivateMainArea from '@proton/components/containers/layout/PrivateMainAre
 import useActiveBreakpoint from '@proton/components/hooks/useActiveBreakpoint';
 import { useCategoriesData } from '@proton/mail/features/categoriesView/useCategoriesData';
 import { MAILBOX_LABEL_IDS } from '@proton/shared/lib/constants';
-import { getSearchParams } from '@proton/shared/lib/helpers/url';
 import { CUSTOM_VIEWS, CUSTOM_VIEWS_LABELS, LABEL_IDS_TO_HUMAN } from '@proton/shared/lib/mail/constants';
-import type { Filter, Sort } from '@proton/shared/lib/mail/search';
 import { isAdminOrLoginAsAdmin } from '@proton/shared/lib/user/helpers';
 import clsx from '@proton/utils/clsx';
 
 import MailHeader from 'proton-mail/components/header/MailHeader';
-import useScrollToTop from 'proton-mail/components/list/useScrollToTop';
 import { NewsletterSubscriptionView } from 'proton-mail/components/view/NewsletterSubscription/NewsletterSubscriptionView';
 import { ROUTE_LABEL } from 'proton-mail/constants';
 import { MailboxContainerContextProvider } from 'proton-mail/containers/mailbox/MailboxContainerProvider';
-import { filterFromUrl, pageFromUrl, sortFromUrl } from 'proton-mail/helpers/mailboxUrl';
 import useMailDrawer from 'proton-mail/hooks/drawer/useMailDrawer';
 import { useElements } from 'proton-mail/hooks/mailbox/useElements';
-import { selectParams } from 'proton-mail/store/elements/elementsSelectors';
+import { useScrollListToTopOnViewChange } from 'proton-mail/router/hooks/useScrollListToTopOnViewChange';
+import { selectElementID, selectIsSearching, selectLabelID } from 'proton-mail/store/elements/elementsSelectors';
 import { useMailDispatch, useMailSelector } from 'proton-mail/store/hooks';
 import { layoutActions } from 'proton-mail/store/layout/layoutSlice';
 
@@ -38,7 +35,10 @@ import { useMailboxContainerSideEffects } from './sideEffects/useMailboxContaine
 
 export const RouterMailboxContainer = () => {
     // We get most of the data here to avoid unnecessary re-renders
-    const { labelID, isSearching, elementID } = useMailSelector(selectParams);
+    const labelID = useMailSelector(selectLabelID);
+    const elementID = useMailSelector(selectElementID);
+    const isSearching = useMailSelector(selectIsSearching);
+
     const navigation = useRouterNavigation({ labelID: labelID });
     const elementsParams = useGetElementParams({ navigation });
     const elementsData = useElements(elementsParams);
@@ -46,7 +46,8 @@ export const RouterMailboxContainer = () => {
 
     const [user] = useUser();
     const [retentionRules] = useRetentionPolicies();
-    const { isColumnModeActive, messageContainerRef, mainAreaRef, scrollContainerRef } = useMailboxLayoutProvider();
+    const { isColumnModeActive, messageContainerRef, mainAreaRef } = useMailboxLayoutProvider();
+    useScrollListToTopOnViewChange();
 
     const { drawerSidebarButtons, showDrawerSidebar } = useMailDrawer();
 
@@ -69,23 +70,7 @@ export const RouterMailboxContainer = () => {
         labelID: labelID,
     });
 
-    const dispatch = useMailDispatch(); // You're already using useMailSelector, so this is consistent
-
-    // TODO this will need to be improved to avoid depending on the URL, fix the test for the moment
-    const location = useLocation();
-    const urlPage = pageFromUrl(location);
-    const searchParams = getSearchParams(location.hash);
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- autofix-eslint-649484
-    const sort = useMemo<Sort>(() => sortFromUrl(location, labelID), [searchParams.sort, labelID]);
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- autofix-eslint-C5320A
-    const filter = useMemo<Filter>(() => filterFromUrl(location), [searchParams.filter]);
-    useScrollToTop(scrollContainerRef as RefObject<HTMLElement>, [
-        urlPage,
-        labelID,
-        sort,
-        filter,
-        elementsParams.search,
-    ]);
+    const dispatch = useMailDispatch();
     const breakpoints = useActiveBreakpoint();
 
     // When the labelID is updated, reset the select all value
