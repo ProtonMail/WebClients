cec79600523b6ef13570daf4f9e144cc174c5abb
Mattias Svanström <mattias.svanstrom@proton.ch>
2026-07-09 09:52:53 +0200

    Drop SystemGroupFlag FF

--- packages/account/groups/groupKeysListener.ts
+++ packages/account/groups/groupKeysListener.ts
@@ -1,7 +1,5 @@
 import { generateGroupKeysIfNeeded, groupsToGenerateFilter } from '@proton/account/groups/generateGroupKeys';
 import type { SharedStartListening } from '@proton/redux-shared-store-types';
-import { getUnleashReadyPromise } from '@proton/unleash/getUnleashReadyPromise';
-import noop from '@proton/utils/noop';
 
 import type { GroupsState } from '../index';
 
@@ -23,11 +21,6 @@ export const groupKeysListener = (startListening: SharedStartListening<GroupsSta
         effect: async (_action, listenerApi) => {
             // Unsubscribe to prevent race conditions.
             listenerApi.unsubscribe();
-            await getUnleashReadyPromise(listenerApi.extra.unleashClient).catch(noop);
-            // In case the flag is not enabled, just unsubscribe the listener and don't try again until next reload.
-            if (!listenerApi.extra.unleashClient.isEnabled('SystemGroupFlag')) {
-                return;
-            }
             try {
                 await listenerApi.dispatch(generateGroupKeysIfNeeded());
             } catch {
