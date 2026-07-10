3c46b4f624a4b359f336c80f9e3fdfd2e3ad4e51
Asier Aduriz <asier.aduriz@proton.ch>
2026-07-10 11:29:56 +0200

    Add small story to illustrate how anchors can look like buttons, but are semantically 'a' tags

--- applications/storybook/src/stories/atoms/Href.stories.tsx
+++ applications/storybook/src/stories/atoms/Href.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react-webpack5';
 
+import { ButtonLike } from '@proton/atoms/Button/ButtonLike';
 import { Href } from '@proton/atoms/Href/Href';
+import { ThemeColor } from '@proton/colors';
 
 const meta: Meta<typeof Href> = {
     title: 'Atoms/Href',
@@ -25,3 +27,18 @@ export default meta;
 type Story = StoryObj<typeof Href>;
 
 export const Default: Story = {};
+
+export const HrefAsButton: StoryObj<typeof ButtonLike> = {
+    render: () => (
+        <ButtonLike as={Href} href="https://proton.me" shape="solid" color={ThemeColor.Norm}>
+            Read the docs
+        </ButtonLike>
+    ),
+    parameters: {
+        docs: {
+            description: {
+                story: 'The `Href` atom styled like a button by passing it as the `as` element of `ButtonLike`. Still a real `<a href>`. For the inverse (a `<button>` that looks like a link), see Atoms/Button → LinkLikeButton.',
+            },
+        },
+    },
+};
