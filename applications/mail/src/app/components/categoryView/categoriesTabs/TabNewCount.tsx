6d49011187d42189c851fd95a54a8c6ae3f16a46
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-07-13 13:47:26 +0200

    refactor(mail): remove the unused new count badge and animation

--- applications/mail/src/app/components/categoryView/categoriesTabs/TabNewCount.tsx
+++
@@ -1,9 +0,0 @@
-import { c } from 'ttag';
-
-export const TabNewCount = () => {
-    return (
-        <span className="tab-new-count text-semibold mail-category-color" aria-hidden="true">
-            {c('Info').t`New messages`}
-        </span>
-    );
-};
