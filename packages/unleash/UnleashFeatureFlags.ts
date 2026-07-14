648ec1cf3b71f55a86de52b9d39c32748ff320ef
Sebastijan Zindl <sebastijan.zindl@proton.ch>
2026-07-14 14:35:22 +0200

    fix(INDA-722): CategoriesViewToggle reloads InboxDesktopApp after change

--- packages/unleash/UnleashFeatureFlags.ts
+++ packages/unleash/UnleashFeatureFlags.ts
@@ -19,6 +19,7 @@ export enum CommonFeatureFlag {
     InboxDesktopMultiAccountSupport = 'InboxDesktopMultiAccountSupport',
     InboxDesktopThemeSelection = 'InboxDesktopThemeSelection',
     InboxDesktopManualUpdateBannerDisabled = 'InboxDesktopManualUpdateBannerDisabled',
+    InboxDesktopCategoryViewSettingsToggleReloadDisabled = 'InboxDesktopCategoryViewSettingsToggleReloadDisabled',
     BreachAlertsNotificationsCommon = 'BreachAlertsNotificationsCommon',
     WalletAutoSetup = 'WalletAutoSetup',
     InboxDesktopWinLinNewAppSwitcher = 'InboxDesktopWinLinNewAppSwitcher',
