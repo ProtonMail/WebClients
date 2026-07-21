5635a50116d6fac0f91ec997fe07d5b63d246090
Paolo Chiabrera <paolo.chiabrera@proton.ch>
2026-07-21 14:42:51 +0200

    INWEB-1037: Add custom eslint rule to prevent nested npm packages

--- packages/eslint-plugin-custom-rules/index.js
+++ packages/eslint-plugin-custom-rules/index.js
@@ -3,6 +3,7 @@ import deprecateClasses from './deprecate-classes.js';
 import deprecateResponsiveUtilityClasses from './deprecate-responsive-utility-classes.js';
 import deprecateSizingClasses from './deprecate-sizing-classes.js';
 import deprecateSpacingUtilityClasses from './deprecate-spacing-utility-classes.js';
+import noNestedPackages from './no-nested-packages.js';
 import noPlaywrightStringTag from './no-playwright-string-tag.js';
 import noTemplateInTranslatorContext from './no-template-in-translator-context.js';
 import useSubscriptionModalLoading from './use-subscription-modal-loading.js';
@@ -20,6 +21,7 @@ export default {
         'deprecate-responsive-utility-classes': deprecateResponsiveUtilityClasses,
         'deprecate-sizing-classes': deprecateSizingClasses,
         'deprecate-spacing-utility-classes': deprecateSpacingUtilityClasses,
+        'no-nested-packages': noNestedPackages,
         'no-playwright-string-tag': noPlaywrightStringTag,
         'no-template-in-translator-context': noTemplateInTranslatorContext,
         'use-subscription-modal-loading': useSubscriptionModalLoading,
