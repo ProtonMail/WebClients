6331dbf3eb9827630ce74e75a299dc011404d1d9
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-07-21 14:34:46 +0200

    fix(mail,search): clean the bucketed search

--- applications/mail/src/app/components/header/search/AdvancedSearch.tsx
+++ applications/mail/src/app/components/header/search/AdvancedSearch.tsx
@@ -197,7 +197,6 @@ const AdvancedSearch = ({
             page: UNDEFINED, // Reset page parameter when performing an advanced search so that search results are shown from the first page
         });
 
-        performance.mark('search-submit');
         history.push(newURL);
 
         onClose();
