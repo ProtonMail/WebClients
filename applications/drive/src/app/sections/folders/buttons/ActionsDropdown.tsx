97e73acfdb0fbcdc27af322088dc0bd560094341
Lucien Peslier <lucien.peslier@proton.ch>
2026-07-21 07:25:08 +0000

    [DRVWEB-5569] Update ActionsDropdown to be equal with other menus

--- applications/drive/src/app/sections/folders/buttons/ActionsDropdown.tsx
+++ applications/drive/src/app/sections/folders/buttons/ActionsDropdown.tsx
@@ -1,145 +1,50 @@
-import { type ReactElement, useState } from 'react';
+import { useState } from 'react';
 
 import { c } from 'ttag';
 
-import { Dropdown, DropdownMenu, DropdownMenuButton, ToolbarButton, usePopperAnchor } from '@proton/components';
-import { MemberRole, generateNodeUid, getDrive } from '@proton/drive/index';
-import { useMoveItemsModal } from '@proton/drive/modals/moveItemsModal';
-import { useSharingModal } from '@proton/drive/modals/sharingModal';
-import { IcArrowsCross } from '@proton/icons/icons/IcArrowsCross';
+import { ContextSeparator, Dropdown, DropdownMenu, ToolbarButton, usePopperAnchor } from '@proton/components';
 import { IcChevronDownFilled } from '@proton/icons/icons/IcChevronDownFilled';
-import { IcInfoCircle } from '@proton/icons/icons/IcInfoCircle';
-import { IcPenSquare } from '@proton/icons/icons/IcPenSquare';
-import { IcTrash } from '@proton/icons/icons/IcTrash';
-import { IcUserPlus } from '@proton/icons/icons/IcUserPlus';
 import clsx from '@proton/utils/clsx';
 import generateUID from '@proton/utils/generateUID';
 
-import { useDetailsModal } from '../../../modals/DetailsModal';
-import { useFilesDetailsModal } from '../../../modals/FilesDetailsModal';
-import { useRenameModal } from '../../../modals/RenameModal';
-import { useTrashActions } from '../../commonActions/useTrashActions';
+import { RenameActionButton } from '../../buttons/RenameActionButton';
+import { ReportAbuseButton } from '../../commonButtons/ReportAbuseButton';
+import type { FolderPermissions, FolderViewItem } from '../useFolder.store';
+import type { FolderActions } from '../useFolderActions';
+import { CopyButton } from './CopyButton';
+import { CopyLinkContextButton } from './CopyLinkContextButton';
+import { DetailsButton } from './DetailsButton';
+import { MoveButton } from './MoveButton';
+import { RevisionsContextButton } from './RevisionsContextButton';
+import { ShareLinkButton } from './ShareLinkButton';
+import { TrashButton } from './TrashButton';
 
-export const toNodeUidsHelper = <T extends { volumeId: string; linkId: string }>(items: T[]): string[] =>
-    items.map((item) => generateNodeUid(item.volumeId, item.linkId));
-
-type Item = {
-    isFile: boolean;
-    name: string;
-    mimeType: string;
-    linkId: string;
-    volumeId: string;
-    rootShareId: string;
-    parentLinkId: string;
-};
 interface Props {
-    volumeId: string;
-    selectedItems: Item[];
-    role: MemberRole;
+    selectedItems: FolderViewItem[];
+    permissions: FolderPermissions;
     canShareSelectedItem: boolean;
+    actions: FolderActions;
 }
 
-export const ActionsDropdown = ({ volumeId, selectedItems, role, canShareSelectedItem }: Props) => {
+export const ActionsDropdown = ({ selectedItems, permissions, canShareSelectedItem, actions }: Props) => {
     const [uid] = useState(generateUID('actions-dropdown'));
     const { anchorRef, isOpen, toggle, close } = usePopperAnchor<HTMLButtonElement>();
-    const { filesDetailsModal, showFilesDetailsModal } = useFilesDetailsModal();
-    const { detailsModal, showDetailsModal } = useDetailsModal();
-    const { moveItemsModal, showMoveItemsModal } = useMoveItemsModal();
-    const { renameModal, showRenameModal } = useRenameModal();
-    const { sharingModal, showSharingModal } = useSharingModal();
-    const isEditor = role === MemberRole.Editor;
-    const { trashItems } = useTrashActions();
-    const hasFoldersSelected = selectedItems.some((item) => !item.isFile);
-    const isMultiSelect = selectedItems.length > 1;
-    const selectedLinkIds = selectedItems.map(({ linkId }) => linkId);
 
-    const menuItems: {
-        hidden: boolean;
-        name: string;
-        icon: ReactElement;
-        testId: string;
-        action: () => void;
-    }[] = [
-        {
-            hidden: !canShareSelectedItem,
-            name: c('Action').t`Share`,
-            icon: <IcUserPlus />,
-            testId: 'actions-dropdown-share-link',
-            action: () =>
-                // This is only used for standard folder view and not photos so we can force getDrive
-                showSharingModal({ nodeUid: generateNodeUid(volumeId, selectedLinkIds[0]), drive: getDrive() }),
-        },
-        {
-            hidden: !isEditor,
-            name: c('Action').t`Move to folder`,
-            icon: <IcArrowsCross />,
-            testId: 'actions-dropdown-move',
-            action: () => showMoveItemsModal({ nodeUids: toNodeUidsHelper(selectedItems) }),
-        },
-        {
-            hidden: isMultiSelect || !isEditor,
-            name: c('Action').t`Rename`,
-            icon: <IcPenSquare />,
-            testId: 'actions-dropdown-rename',
-            action: () => {
-                showRenameModal({
-                    nodeUid: generateNodeUid(selectedItems[0].volumeId, selectedItems[0].linkId),
-                });
-            },
-        },
-        {
-            hidden: isMultiSelect,
-            name: c('Action').t`Details`,
-            icon: <IcInfoCircle />,
-            testId: 'actions-dropdown-details',
-            action: () =>
-                showDetailsModal({
-                    nodeUid: generateNodeUid(selectedItems[0].volumeId, selectedLinkIds[0]),
-                }),
-        },
-        {
-            hidden: !isMultiSelect || hasFoldersSelected,
-            name: c('Action').t`Details`,
-            icon: <IcInfoCircle />,
-            testId: 'actions-dropdown-details',
-            action: () => showFilesDetailsModal({ nodeUids: toNodeUidsHelper(selectedItems) }),
-        },
-        {
-            hidden: !isEditor,
-            name: c('Action').t`Move to trash`,
-            icon: <IcTrash />,
-            testId: 'actions-dropdown-trash',
-            action: () =>
-                trashItems(
-                    getDrive(),
-                    selectedItems.map((item) => ({
-                        uid: generateNodeUid(item.volumeId, item.linkId),
-                        parentUid: generateNodeUid(item.volumeId, item.parentLinkId),
-                        name: item.name,
-                    }))
-                ),
-        },
-    ];
+    const selectedItem = selectedItems[0];
+    const isOnlyOneItem = selectedItems.length === 1 && !!selectedItem;
+    const isOnlyOneFileItem = isOnlyOneItem && !!selectedItem?.isFile;
+    const canCopyPublicLink = canShareSelectedItem && !!selectedItem?.isSharedPublicly;
 
-    const dropdownMenuButtons = menuItems
-        .filter((menuItem) => !menuItem.hidden)
-        .map((item) => (
-            <DropdownMenuButton
-                key={item.name}
-                hidden={item.hidden}
-                onContextMenu={(e) => e.stopPropagation()}
-                className="flex flex-nowrap gap-2 items-center text-left"
-                onClick={(e) => {
-                    e.stopPropagation();
-                    item.action();
-                    close();
-                }}
-                data-testid={item.testId}
-            >
-                {item.icon}
-                {item.name}
-            </DropdownMenuButton>
-        ));
+    const {
+        showSharingModal,
+        showMoveModal,
+        showCopyModal,
+        showRenameModal,
+        showDetailsModal,
+        showRevisionsModal,
+        showReportAbuseModal,
+        getPublicLinkInfo,
+    } = actions;
 
     return (
         <>
@@ -156,13 +61,49 @@ export const ActionsDropdown = ({ volumeId, selectedItems, role, canShareSelecte
                 title={c('Title').t`Show actions`}
             />
             <Dropdown id={uid} isOpen={isOpen} anchorRef={anchorRef} onClose={close} originalPlacement="bottom">
-                <DropdownMenu>{dropdownMenuButtons}</DropdownMenu>
+                <DropdownMenu>
+                    {canCopyPublicLink && <CopyLinkContextButton getPublicLinkInfo={getPublicLinkInfo} close={close} />}
+                    {canShareSelectedItem && (
+                        <ShareLinkButton type="context" onClick={showSharingModal} close={close} />
+                    )}
+                    <ContextSeparator />
+                    {permissions.canMove && (
+                        <MoveButton
+                            type="context"
+                            selectedItems={selectedItems}
+                            onClick={showMoveModal}
+                            close={close}
+                        />
+                    )}
+                    {permissions.canCopy && <CopyButton type="context" onClick={showCopyModal} close={close} />}
+                    {permissions.canRename && isOnlyOneItem && (
+                        <RenameActionButton type="context" onClick={showRenameModal} close={close} />
+                    )}
+                    <DetailsButton
+                        type="context"
+                        selectedItems={selectedItems}
+                        onClick={showDetailsModal}
+                        close={close}
+                    />
+                    {(permissions.canEdit || permissions.canReportAbuse) && <ContextSeparator />}
+                    {permissions.canEdit && isOnlyOneFileItem && selectedItem && (
+                        <>
+                            <RevisionsContextButton
+                                nodeUid={selectedItem.uid}
+                                rootShareId={selectedItem.rootShareId}
+                                mediaType={selectedItem.mimeType}
+                                showRevisionsModal={showRevisionsModal}
+                                close={close}
+                            />
+                            <ContextSeparator />
+                        </>
+                    )}
+                    {permissions.canTrash && <TrashButton type="context" selectedItems={selectedItems} close={close} />}
+                    {permissions.canReportAbuse && isOnlyOneItem && (
+                        <ReportAbuseButton buttonType="contextMenu" onClick={showReportAbuseModal} close={close} />
+                    )}
+                </DropdownMenu>
             </Dropdown>
-            {filesDetailsModal}
-            {detailsModal}
-            {moveItemsModal}
-            {renameModal}
-            {sharingModal}
         </>
     );
 };
