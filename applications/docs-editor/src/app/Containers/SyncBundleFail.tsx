e12d25c6d0ec83ac0850b9e0cddc8c0a9305503e
Nicholas Mueller <nicholas.mueller@proton.ch>
2026-07-28 08:41:21 +0000

    Docs: Fix bundle mismatch due to stale cookie

---
+++ applications/docs-editor/src/app/Containers/SyncBundleFail.tsx
@@ -0,0 +1,3 @@
+export function SyncBundleFail() {
+  return <div>Unable to load the application. Please try opening the document again from the homepage.</div>
+}
--- applications/docs-editor/src/app/index.tsx
+++ applications/docs-editor/src/app/index.tsx
@@ -3,37 +3,43 @@ import { BridgeOriginProvider, EDITOR_IFRAME_FOCUS_EVENT } from '@proton/docs-sh
 import '@proton/polyfill'
 import { createRoot } from 'react-dom/client'
 import { RootContainer } from './Containers/RootContainer'
+import { SyncBundleFail } from './Containers/SyncBundleFail'
 import './style'
 import { reportErrorToSentry } from './Utils/errorMessage'
 import type { EditorSystemMode } from '@proton/docs-shared/lib/EditorSystemMode'
 import type { DocumentType } from '@proton/drive-store/store/_documents'
-
-const searchParams = new URLSearchParams(window.location.search)
-
-const documentType = (searchParams.get('type') as DocumentType) || 'doc'
-const systemMode = searchParams.get('mode')
-
-// Global Error Listener that pushes Error messages to Sentry
-window.addEventListener('error', (event: ErrorEvent) => {
-  reportErrorToSentry(new Error(event.message))
-})
+import { syncBundle, SyncBundleResult } from './Utils/syncBundle'
 
 const container = document.querySelector('.app-root')
 const root = createRoot(container!)
-root.render(
-  <ErrorBoundary
-    onError={(error) => {
-      reportErrorToSentry(error)
-    }}
-  >
-    <RootContainer documentType={documentType} systemMode={systemMode as EditorSystemMode} />
-  </ErrorBoundary>,
-)
 
-window.addEventListener('focus', () => {
-  window.parent.postMessage(EDITOR_IFRAME_FOCUS_EVENT, BridgeOriginProvider.GetClientOrigin())
-})
+const syncResult = syncBundle()
+if (syncResult === SyncBundleResult.READY) {
+  const searchParams = new URLSearchParams(window.location.search)
+  const documentType = (searchParams.get('type') as DocumentType) || 'doc'
+  const systemMode = searchParams.get('mode')
 
-window.addEventListener('blur', () => {
-  document.dispatchEvent(new CustomEvent('dropdownclose'))
-})
+  // Global Error Listener that pushes Error messages to Sentry
+  window.addEventListener('error', (event: ErrorEvent) => {
+    reportErrorToSentry(new Error(event.message))
+  })
+  root.render(
+    <ErrorBoundary
+      onError={(error) => {
+        reportErrorToSentry(error)
+      }}
+    >
+      <RootContainer documentType={documentType} systemMode={systemMode as EditorSystemMode} />
+    </ErrorBoundary>,
+  )
+  window.addEventListener('focus', () => {
+    window.parent.postMessage(EDITOR_IFRAME_FOCUS_EVENT, BridgeOriginProvider.GetClientOrigin())
+  })
+  window.addEventListener('blur', () => {
+    document.dispatchEvent(new CustomEvent('dropdownclose'))
+  })
+} else if (syncResult === SyncBundleResult.FAILED) {
+  root.render(<SyncBundleFail />)
+} else if (syncResult === SyncBundleResult.RELOADING) {
+  // do nothing, wait for the page to reload
+}
