db60f9a0b48850d5fd88eccacfe44351e8446c66
Thong-Nguyen The <thong-nguyen.the@proton.ch>
2026-08-04 08:07:43 +0000

    Account: invalidate admin role to refresh after promotion/demotion

--- packages/account/members/actions.ts
+++ packages/account/members/actions.ts
@@ -91,6 +91,7 @@ import {
     MemberCreationValidationError,
     type MembersState,
     getMemberAddresses,
+    invalidateMemberRoles,
     membersThunk,
     updateMemberRoles,
     upsertMember,
@@ -313,6 +314,7 @@ export const setRole = ({
                 return;
             }
             await api(updateRoleConfig(member.ID, MEMBER_ROLE.ORGANIZATION_MEMBER));
+            dispatch(invalidateMemberRoles({ member }));
             return;
         }
 
@@ -320,6 +322,7 @@ export const setRole = ({
 
         if (!getIsPasswordless(organizationKey?.Key)) {
             await api(updateRoleConfig(member.ID, MEMBER_ROLE.ORGANIZATION_ADMIN));
+            dispatch(invalidateMemberRoles({ member }));
             return;
         }
 
@@ -337,6 +340,7 @@ export const setRole = ({
         }
 
         await dispatch(setAdminRoles({ memberKeyPayloads: [payload], api }));
+        dispatch(invalidateMemberRoles({ member }));
     };
 };
 
