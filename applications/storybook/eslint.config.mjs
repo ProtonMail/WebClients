f41e80c7a4552d530597291c33fe4ad370b3db16
Paolo Chiabrera <paolo.chiabrera@proton.ch>
2026-08-07 12:46:14 +0200

    chore: fix storybook linting for static assets

--- applications/storybook/eslint.config.mjs
+++ applications/storybook/eslint.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 
 import defaultConfig from '@proton/eslint-config-proton/all';
 
-export default defineConfig(defaultConfig, storybookConfig.configs['flat/recommended'], globalIgnores(['.storybook']), {
-    rules: {
-        // TODO: Add the missing explicit deps and remove this rule
-        'import/no-extraneous-dependencies': 'off',
-    },
-});
+export default defineConfig(
+    defaultConfig,
+    storybookConfig.configs['flat/recommended'],
+    globalIgnores(['.storybook', 'storybook-static']),
+    {
+        rules: {
+            // TODO: Add the missing explicit deps and remove this rule
+            'import/no-extraneous-dependencies': 'off',
+        },
+    }
+);
