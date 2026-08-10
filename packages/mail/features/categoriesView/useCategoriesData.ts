9b6324f0760a97582a3ff0f1a699f08bb80e6097
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2026-08-10 07:43:59 +0000

    Merge branch 'P3-411-enable-category-view-experiment-user' into 'main'
    fix(mail): show category view for users with correct variant
    
    See merge request web/clients!26996

--- packages/mail/features/categoriesView/useCategoriesData.ts
+++ packages/mail/features/categoriesView/useCategoriesData.ts
@@ -6,12 +6,25 @@ import {
 } from '@proton/mail/store/categoriesView/categoriesViewSelector';
 import { selectActiveCategoriesTabs, selectCategoriesLabel } from '@proton/mail/store/labels/selector';
 import { useSelector } from '@proton/redux-shared-store/sharedProvider';
+import type { CategoryViewVariantVariant } from '@proton/unleash/UnleashFeatureFlagsVariants';
 import { useFlagsStatus } from '@proton/unleash/proxy';
 import { useFlag } from '@proton/unleash/useFlag';
+import { useVariant } from '@proton/unleash/useVariant';
 
 import type { CategoryTab } from './categoriesConstants';
 
 const EMPTY_ARRAY: CategoryTab[] = [];
+
+type CategoryViewVariantName = CategoryViewVariantVariant | 'disabled';
+
+const variantWithAccess = new Set<CategoryViewVariantName>([
+    'FeatureAccessOn',
+    'PrimaryFiltering45',
+    'PrimaryFiltering60',
+    'RecategorizationButton',
+    'RecategorizationNoButton',
+    'GradualRollout',
+]);
 
 export const useCategoriesData = () => {
     const { flagsReady } = useFlagsStatus();
@@ -19,12 +32,15 @@ export const useCategoriesData = () => {
     const betaFlag = useFeature<boolean>(FeatureCode.CategoryViewBeta);
     const hasBetaAccess = betaFlag.feature?.Value ?? false;
 
+    const flagVariant = useVariant('CategoryViewVariant');
+    const variantWithCategoryViewAccess = flagVariant.name ? variantWithAccess.has(flagVariant.name) : false;
+
     const isLoading = useSelector(selectCategoryViewLoading);
     const settingAccess = useSelector(selectCategoryViewSettingAccess);
     const categoriesStore = useSelector(selectCategoriesLabel);
     const activeCategoriesTabs = useSelector(selectActiveCategoriesTabs);
 
-    const canUseCategoryView = categoryViewFlag || hasBetaAccess;
+    const canUseCategoryView = categoryViewFlag || hasBetaAccess || variantWithCategoryViewAccess;
     const isCategoryViewEnabled = canUseCategoryView && settingAccess;
 
     const isRefreshedToolbarUIDisabled = useFlag('NewToolbarKillSwitch');
