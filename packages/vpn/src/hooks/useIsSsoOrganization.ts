ed6e24b682e8b34c3b918c3f377aca8541fb9807
Asier Aduriz <asier.aduriz@proton.ch>
2026-08-11 11:02:46 +0200

    [VPNB2B-209] Add SSO warning back to SSO-enabled organizations

---
+++ packages/vpn/src/hooks/useIsSsoOrganization.ts
@@ -0,0 +1,13 @@
+import { useSamlSSO } from '@proton/account/samlSSO/hooks';
+
+/**
+ * Whether the organization signs its members in through SSO, i.e. it has at least one enabled SAML config.
+ */
+export const useIsSsoOrganization = (): { isSsoOrganization: boolean; isLoading: boolean } => {
+    const [samlSSO, isLoading] = useSamlSSO();
+
+    return {
+        isSsoOrganization: Boolean(samlSSO?.configs.some((config) => config.Enabled)),
+        isLoading,
+    };
+};
