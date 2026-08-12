227c93fafeee9d11faddaf0347606b948f819cd4
Mattias Svanström <mmso@mmso.se>
2026-08-12 13:14:04 +0200

    Add search keywords to mail settings subsections

--- applications/account/src/app/containers/calendar/routes.ts
+++ applications/account/src/app/containers/calendar/routes.ts
@@ -39,8 +39,24 @@ export const getCalendarAppRoutes = ({ app, user, organization, flags }: Organiz
                 to: CALENDAR_SETTINGS_ROUTE.GET_APPS,
                 icon: 'arrow-down-line',
                 subsections: [
-                    { id: CALENDAR_SETTINGS_SECTION_ID.MOBILE_APP, text: c('Title').t`Download the mobile apps` },
-                    { id: CALENDAR_SETTINGS_SECTION_ID.DESKTOP_APP, text: c('Title').t`Download the desktop app` },
+                    {
+                        id: CALENDAR_SETTINGS_SECTION_ID.MOBILE_APP,
+                        text: c('Title').t`Download the mobile apps`,
+                        keywords: [
+                            c('account_search_index').t`App Store`,
+                            c('account_search_index').t`Play Store`,
+                            c('account_search_index').t`iOS and Android apps`,
+                        ],
+                    },
+                    {
+                        id: CALENDAR_SETTINGS_SECTION_ID.DESKTOP_APP,
+                        text: c('Title').t`Download the desktop app`,
+                        keywords: [
+                            c('Action').t`Download for Windows`,
+                            c('Action').t`Download for macOS`,
+                            c('account_search_index').t`Linux desktop app`,
+                        ],
+                    },
                 ],
             },
             general: {
@@ -52,24 +68,44 @@ export const getCalendarAppRoutes = ({ app, user, organization, flags }: Organiz
                     {
                         text: c('Title').t`Time zone`,
                         id: CALENDAR_SETTINGS_SECTION_ID.TIME_ZONE,
+                        keywords: [
+                            c('Primary timezone').t`Primary time zone`,
+                            c('Primary timezone').t`Secondary time zone`,
+                            c('Label').t`Auto-detect primary time zone`,
+                        ],
                     },
                     {
                         text: c('Title').t`Layout`,
                         id: CALENDAR_SETTINGS_SECTION_ID.LAYOUT,
+                        keywords: [
+                            c('Label').t`Default view`,
+                            c('Label').t`Week start`,
+                            c('Label').t`Show week numbers`,
+                        ],
                     },
                     {
                         text: c('Title').t`Invitations`,
                         id: CALENDAR_SETTINGS_SECTION_ID.INVITATIONS,
+                        keywords: [
+                            c('account_search_index').t`Invitation language`,
+                            c('account_search_index').t`Auto-add invitations to calendar`,
+                            c('Label').t`Auto-add Meet links`,
+                        ],
                     },
                     {
                         text: c('Title').t`Integrations`,
                         id: CALENDAR_SETTINGS_SECTION_ID.INTEGRATIONS,
                         available:
                             (isZoomIntegrationEnabled || isProtonMeetIntegrationEnabled) && canDisableIntegrations,
+                        keywords: [
+                            c('Label').t`Video conferencing with Zoom`,
+                            c('account_search_index').t`Zoom meeting links`,
+                        ],
                     },
                     {
                         text: c('Title').t`Other preferences`,
                         id: CALENDAR_SETTINGS_SECTION_ID.OTHER_PREFERENCES,
+                        keywords: [c('Label').t`Keyboard shortcuts`],
                     },
                 ],
             },
@@ -82,10 +118,20 @@ export const getCalendarAppRoutes = ({ app, user, organization, flags }: Organiz
                     {
                         text: c('Title').t`My calendars`,
                         id: CALENDAR_SETTINGS_SECTION_ID.PERSONAL_CALENDARS,
+                        keywords: [
+                            c('Action').t`Create calendar`,
+                            c('Action').t`Set as default`,
+                            c('Title').t`Delete calendar`,
+                        ],
                     },
                     {
                         text: c('Title').t`Other calendars`,
                         id: CALENDAR_SETTINGS_SECTION_ID.OTHER_CALENDARS,
+                        keywords: [
+                            c('Action').t`Add calendar from URL`,
+                            c('Action').t`Add public holidays`,
+                            c('Header; Table with list of calendars arranged by type').t`Subscribed`,
+                        ],
                     },
                 ],
             },
@@ -99,10 +145,12 @@ export const getCalendarAppRoutes = ({ app, user, organization, flags }: Organiz
                     {
                         text: c('Title').t`Import`,
                         id: CALENDAR_SETTINGS_SECTION_ID.IMPORT,
+                        keywords: [c('Action').t`Import from ICS`, c('account_search_index').t`Import events`],
                     },
                     {
                         text: c('Title').t`Export`,
                         id: CALENDAR_SETTINGS_SECTION_ID.EXPORT,
+                        keywords: [c('Action').t`Download ICS`, c('Action').t`Select a calendar to export`],
                     },
                 ],
             },
--- applications/account/src/app/containers/mail/routes.tsx
+++ applications/account/src/app/containers/mail/routes.tsx
@@ -3,7 +3,7 @@ import { c } from 'ttag';
 import { Href } from '@proton/atoms/Href/Href';
 import type { SidebarConfig } from '@proton/components';
 import { getMailRouteTitles } from '@proton/components/containers/account/constants/settingsRouteTitles';
-import { ADDRESS_TYPE, APPS, MAIL_APP_NAME } from '@proton/shared/lib/constants';
+import { ADDRESS_TYPE, APPS, MAIL_APP_NAME, PASS_APP_NAME } from '@proton/shared/lib/constants';
 import { isElectronMail } from '@proton/shared/lib/helpers/desktop';
 import { hasOrganizationSetupWithKeys } from '@proton/shared/lib/helpers/organization';
 import { getKnowledgeBaseUrl } from '@proton/shared/lib/helpers/url';
@@ -55,8 +55,24 @@ export const getMailAppRoutes = ({
                 to: '/get-the-apps',
                 icon: 'arrow-down-line',
                 subsections: [
-                    { id: 'proton-mail-mobile-apps', text: c('Title').t`Download the mobile apps` },
-                    { id: 'proton-mail-desktop-apps', text: c('Title').t`Download the desktop app` },
+                    {
+                        id: 'proton-mail-mobile-apps',
+                        text: c('Title').t`Download the mobile apps`,
+                        keywords: [
+                            c('account_search_index').t`App Store`,
+                            c('account_search_index').t`Play Store`,
+                            c('account_search_index').t`iOS and Android apps`,
+                        ],
+                    },
+                    {
+                        id: 'proton-mail-desktop-apps',
+                        text: c('Title').t`Download the desktop app`,
+                        keywords: [
+                            c('Action').t`Download for Windows`,
+                            c('Action').t`Download for macOS`,
+                            c('account_search_index').t`Linux desktop app`,
+                        ],
+                    },
                 ],
             },
             general: {
@@ -68,27 +84,53 @@ export const getMailAppRoutes = ({
                     {
                         text: c('Title').t`General`,
                         id: 'general',
+                        keywords: [c('Label').t`Daily email notifications`, c('Label').t`Allow notifications by email`],
                     },
                     {
                         text: c('Title').t`Email categories`,
                         id: 'categories',
                         available: isCategoryViewEnabled && isOrgAllowingCategoryView,
+                        keywords: [
+                            c('Label').t`Use email categories`,
+                            c('Label').t`Show unread count`,
+                            c('account_search_index').t`Inbox tabs`,
+                        ],
                     },
                     {
                         text: c('Title').t`Layout`,
                         id: 'layout',
+                        keywords: [
+                            c('account_search_index').t`Inbox layout`,
+                            c('Label').t`Density`,
+                            c('Label to change density').t`Compact`,
+                        ],
                     },
                     {
                         text: c('Title').t`Messages`,
                         id: 'messages',
+                        keywords: [
+                            c('Label').t`Conversation grouping`,
+                            c('Label').t`Auto show embedded images`,
+                            c('Label').t`Auto-unsubscribe`,
+                        ],
                     },
                     {
                         text: c('Title').t`Composing`,
                         id: 'composing',
+                        keywords: [
+                            c('Label').t`Composer default font/size`,
+                            c('Label').t`Undo send`,
+                            c('Label').t`Composer text direction`,
+                        ],
                     },
                     {
                         text: c('Title').t`Other preferences`,
                         id: 'other-preferences',
+                        keywords: [
+                            c('Title').t`Keyboard shortcuts`,
+                            c('Label').t`Show sender images`,
+                            c('Label').t`Automatically save contacts`,
+                        ],
                     },
                 ],
             },
@@ -97,7 +139,16 @@ export const getMailAppRoutes = ({
                 text: mailRouteTitles.privacy,
                 to: '/email-privacy',
                 icon: 'shield',
-                subsections: [{ id: 'email-privacy' }],
+                subsections: [
+                    {
+                        id: 'email-privacy',
+                        keywords: [
+                            c('Label').t`Auto show remote images`,
+                            c('Label').t`Block email tracking`,
+                            c('Label').t`Protection mode`,
+                        ],
+                    },
+                ],
             },
             identity: {
                 id: 'identity',
@@ -109,17 +160,33 @@ export const getMailAppRoutes = ({
                         text: c('Title').t`Short domain (@pm.me)`,
                         id: 'pmme',
                         available: getShowPmMeSection(user, addresses),
+                        keywords: [c('Action').t`Send messages with @pm.me`],
                     },
                     {
                         text: c('Title').t`Display name and signature`,
                         id: 'name-signature',
+                        keywords: [
+                            c('Label').t`Display name`,
+                            c('Label').t`Signature`,
+                            c('Label').t`${MAIL_APP_NAME} footer`,
+                        ],
                     },
                     {
                         id: 'alias-promotion',
+                        keywords: [
+                            c('Alias promotion').t`Try ${PASS_APP_NAME}`,
+                            c('account_search_index').t`Email aliases`,
+                            c('account_search_index').t`Hide my email`,
+                        ],
                     },
                     {
                         text: c('Title').t`My addresses`,
                         id: 'addresses',
+                        keywords: [
+                            c('Action').t`Get more addresses`,
+                            c('Action').t`Connect Gmail address`,
+                            c('account_search_index').t`Add address`,
+                        ],
                     },
                 ],
             },
@@ -134,10 +201,20 @@ export const getMailAppRoutes = ({
                     {
                         text: c('Title').t`Folders`,
                         id: 'folderlist',
+                        keywords: [
+                            c('Action').t`Add folder`,
+                            c('Label').t`Use folder colors`,
+                            c('Label').t`Inherit color from parent folder`,
+                        ],
                     },
                     {
                         text: c('Title').t`Labels`,
                         id: 'labellist',
+                        keywords: [
+                            c('Action').t`Add label`,
+                            c('Action').t`Get more labels`,
+                            c('Title').t`Sort labels alphabetically`,
+                        ],
                     },
                 ],
             },
@@ -150,10 +227,20 @@ export const getMailAppRoutes = ({
                     {
                         text: c('Title').t`Custom filters`,
                         id: 'custom',
+                        keywords: [
+                            c('Action').t`Add filter`,
+                            c('Action').t`Add sieve filter`,
+                            c('Action').t`Get more filters`,
+                        ],
                     },
                     {
                         text: c('Title').t`Spam, block, and allow lists`,
                         id: 'spam',
+                        keywords: [
+                            c('Action').t`Add address or domain`,
+                            c('account_search_index').t`Block sender`,
+                            c('account_search_index').t`Allow list`,
+                        ],
                     },
                 ],
             },
@@ -163,8 +250,24 @@ export const getMailAppRoutes = ({
                 to: '/auto-reply',
                 icon: 'envelope-arrow-up-and-right',
                 subsections: [
-                    { text: c('Title').t`Forward emails`, id: 'forward' },
-                    { text: c('Title').t`Auto-reply`, id: 'auto-reply' },
+                    {
+                        text: c('Title').t`Forward emails`,
+                        id: 'forward',
+                        keywords: [
+                            c('email_forwarding_2023: Action').t`Add forwarding rule`,
+                            c('email_forwarding_2023: Action').t`Set up email forwarding`,
+                            c('account_search_index').t`Redirect incoming emails`,
+                        ],
+                    },
+                    {
+                        text: c('Title').t`Auto-reply`,
+                        id: 'auto-reply',
+                        keywords: [
+                            c('account_search_index').t`Out of office`,
+                            c('account_search_index').t`Vacation responder`,
+                            c('Label').t`Start date`,
+                        ],
+                    },
                 ],
             },
             domainNames: {
@@ -175,10 +278,21 @@ export const getMailAppRoutes = ({
                 // NOTE: This configuration is tied with the organization/routes.tsx domains availability
                 available: !user.isMember && !hasOrganizationKey && user.isSelf,
                 subsections: [
-                    { id: 'domains' },
+                    {
+                        id: 'domains',
+                        keywords: [
+                            c('Action').t`Add domain`,
+                            c('account_search_index').t`Custom domain`,
+                            c('Label in domain modal').t`Verify`,
+                        ],
+                    },
                     {
                         text: c('Title').t`Catch-all address`,
                         id: 'catch-all',
+                        keywords: [
+                            c('account_search_index').t`Receive misaddressed emails`,
+                            c('account_search_index').t`Catch-all`,
+                        ],
                     },
                 ],
             },
@@ -191,24 +305,44 @@ export const getMailAppRoutes = ({
                     {
                         text: c('Title').t`Address and key verification`,
                         id: 'address-verification',
+                        keywords: [c('Label').t`Prompt to trust keys`, c('Label').t`Verify keys with Key Transparency`],
                     },
                     {
                         text: c('Title').t`External PGP settings`,
                         id: 'pgp-settings',
+                        keywords: [
+                            c('Label').t`Sign external messages`,
+                            c('Label').t`Attach public key`,
+                            c('Label').t`Default PGP scheme`,
+                        ],
                     },
                     {
                         // title rendering is temporarily handled in the PostQuantumKeysOptInSection
                         text: undefined, // c('Title').t`Post-quantum protection`,
                         id: 'pqc-optin',
                         available: isCryptoPostQuantumOptInEnabled && user.isSelf,
+                        keywords: [
+                            c('Title').t`Post-quantum protection`,
+                            c('Action').t`Enable post-quantum protection`,
+                        ],
                     },
                     {
                         text: c('Title').t`Email encryption keys`,
                         id: 'addresses',
+                        keywords: [
+                            c('Action').t`Reactivate keys`,
+                            c('Keys actions').t`Export public key`,
+                            c('Action').t`Import key`,
+                        ],
                     },
                     {
                         text: c('Title').t`Account keys`,
                         id: 'user',
+                        keywords: [
+                            c('Action').t`Generate key`,
+                            c('Keys actions').t`Export private key`,
+                            c('Title header for keys table').t`Fingerprint`,
+                        ],
                     },
                 ],
             },
@@ -221,10 +355,15 @@ export const getMailAppRoutes = ({
                     {
                         text: c('Title').t`${MAIL_APP_NAME} Bridge`,
                         id: 'protonmail-bridge',
+                        keywords: [
+                            c('account_search_index').t`IMAP and SMTP`,
+                            c('account_search_index').t`Outlook, Apple Mail, and Thunderbird`,
+                        ],
                     },
                     {
                         text: c('Title').t`SMTP submission`,
                         id: 'smtp-tokens',
+                        keywords: [c('Action').t`Generate token`, c('Header for table').t`Token name`],
                     },
                 ],
             },
@@ -238,6 +377,11 @@ export const getMailAppRoutes = ({
                     {
                         text: c('Title').t`${MAIL_APP_NAME} Export Tool`,
                         id: 'import-export',
+                        keywords: [
+                            c('Action').t`Download the ${MAIL_APP_NAME} Export Tool`,
+                            c('account_search_index').t`Back up emails`,
+                            c('account_search_index').t`Export messages`,
+                        ],
                     },
                 ],
             },
