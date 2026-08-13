6817dbbd729b43a5dfaeaa07e5035bd383e5a8b4
Vlad Cipariu <vlad.cipariu@proton.ch>
2026-08-13 11:59:34 +0000

    fix(lumo): Hooks the Sign in and Create account buttons to the native account

--- applications/lumo/src/app/upsells/composed/LumoTierErrorUpsellButtons.tsx
+++ applications/lumo/src/app/upsells/composed/LumoTierErrorUpsellButtons.tsx
@@ -5,42 +5,53 @@ import { SettingsLink } from '@proton/components';
 import { PromotionButton } from '@proton/components/components/button/PromotionButton';
 import { LUMO_UPSELL_PATHS } from '@proton/shared/lib/constants';
 
+import { useAuthActionProps } from '../../hooks/useAuthActionProps';
 import { useIsGuest } from '../../providers/IsGuestProvider';
 import { useLumoPlan } from '../../providers/LumoPlanProvider';
 import BasicUpgradeButton from '../primitives/BasicUpgradeButton';
 import useLumoPlusUpsellButtonConfig from '../useLumoPlusUpsellButtonConfig';
+
+const GuestUpsellButtons = () => {
+    const signUp = useAuthActionProps('signup');
+    const signIn = useAuthActionProps('signin');
+
+    return (
+        <div className="flex flex-column gap-2 shrink-0">
+            <PromotionButton
+                as={SettingsLink}
+                path={signUp.path}
+                onClick={signUp.onClick}
+                shape="solid"
+                color="norm"
+                buttonGradient={false}
+                className="text-center justify-center"
+            >
+                {signUp.text}
+            </PromotionButton>
+            <PromotionButton
+                buttonGradient={false}
+                as={SettingsLink}
+                path={signIn.path}
+                onClick={signIn.onClick}
+                shape="ghost"
+                className="upsell-card-button justify-center"
+            >
+                {signIn.text}
+            </PromotionButton>
+        </div>
+    );
+};
+
+GuestUpsellButtons.displayName = 'GuestUpsellButtons';
 
 const LumoTierErrorUpsellButtons = () => {
     // TODO: update to generic question limi feature
     const config = useLumoPlusUpsellButtonConfig(LUMO_UPSELL_PATHS.QUESTION_LIMIT_FREE);
     const isGuest = useIsGuest();
     const { canShowTalkToAdminLumoUpsell } = useLumoPlan();
-    console.log('debug: canShowTalkToAdminLumoUpsell ', canShowTalkToAdminLumoUpsell);
 
     if (isGuest) {
-        return (
-            <div className="flex flex-column gap-2 shrink-0">
-                <PromotionButton
-                    as={SettingsLink}
-                    path="/signup"
-                    shape="solid"
-                    color="norm"
-                    buttonGradient={false}
-                    className="text-center justify-center"
-                >
-                    {c('collider_2025: Link').t`Create a free account`}
-                </PromotionButton>
-                <PromotionButton
-                    buttonGradient={false}
-                    as={SettingsLink}
-                    path=""
-                    shape="ghost"
-                    className="upsell-card-button justify-center"
-                >
-                    {c('collider_2025: Link').t`Sign in`}
-                </PromotionButton>
-            </div>
-        );
+        return <GuestUpsellButtons />;
     }
 
     if (canShowTalkToAdminLumoUpsell) {
