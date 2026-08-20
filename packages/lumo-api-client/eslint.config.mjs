be1ec9d23299c20d7835aa2e9e0efc37176653e6
Paolo Chiabrera <paolo.chiabrera@proton.ch>
2026-08-20 16:56:10 +0200

    INWEB-1112: Remove extraneous dependencies for packages/lumo-api-client

--- packages/lumo-api-client/eslint.config.mjs
+++ packages/lumo-api-client/eslint.config.mjs
@@ -19,10 +19,6 @@ export default defineConfig([
                 },
             ],
             '@typescript-eslint/no-use-before-define': 'off',
-            'monorepo-cop/no-disable-monorepo-no-relative-rule': 'off',
-            'monorepo-cop/no-relative-import-outside-package': 'off',
-            // TODO: Add the missing explicit deps and remove this rule
-            'import/no-extraneous-dependencies': 'off',
             // TODO: Remove this rule once the compat issue is resolved
             'compat/compat': 'off',
         },
