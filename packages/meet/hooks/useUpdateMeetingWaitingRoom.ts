d9958a026e0527244730478059fef528d1fa257d
Facundo Pedrazzini <facundo.pedrazzini@proton.ch>
2026-08-21 11:00:32 +0200

    Avoid reporting to sentry when users try to edit a waiting room from a meeting already started

--- packages/meet/hooks/useUpdateMeetingWaitingRoom.ts
+++ packages/meet/hooks/useUpdateMeetingWaitingRoom.ts
@@ -1,5 +1,7 @@
 import { useApi } from '@proton/components';
+import { getApiError } from '@proton/shared/lib/api/helpers/apiErrorHelper';
 import { toggleWaitingRoom } from '@proton/shared/lib/api/meet';
+import { API_CUSTOM_ERROR_CODES } from '@proton/shared/lib/errors';
 import type { CreateMeetingResponse, WaitingRoomState } from '@proton/shared/lib/interfaces/Meet';
 
 import { useMeetErrorReporting } from './useMeetErrorReporting';
@@ -26,7 +28,16 @@ export const useUpdateMeetingWaitingRoom = () => {
 
             return Meeting;
         } catch (error) {
-            reportMeetError('Error updating meeting waiting room status', error);
+            const { code } = getApiError(error);
+
+            const DONT_REPORT_TO_SENTRY_ERROR_CODES = [
+                // Meeting is active. Update waiting room setting inside the meeting
+                API_CUSTOM_ERROR_CODES.INVALID_REQUIREMENT,
+            ];
+
+            if (!DONT_REPORT_TO_SENTRY_ERROR_CODES.includes(code)) {
+                reportMeetError(`Error updating meeting waiting room status: ${code}`, error);
+            }
 
             throw error;
         }
