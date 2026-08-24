c159a9410af502dfc06e165652392e9400dc501a
Paolo Chiabrera <paolo.chiabrera@proton.ch>
2026-08-24 18:17:21 +0200

    INWEB-1116: Remove extraneous dependencies for packages/meet

--- packages/meet/eslint.config.mjs
+++ packages/meet/eslint.config.mjs
@@ -6,13 +6,11 @@ export default defineConfig([
     config,
     {
         rules: {
-            // TODO: Add the missing explicit deps and remove this rule
-            'import/no-extraneous-dependencies': 'off',
             'react-hooks/exhaustive-deps': 'warn',
             // TODO: Remove this rule once the compat issue is resolved
             'compat/compat': 'off',
-            // TODO: Migrate same-package imports to relative paths and remove this rule
-            'custom-rules/no-package-self-import': 'off',
+            // Cyclic dependency with @proton/calendar via @proton/components and @proton/redux-shared-store
+            'import/no-extraneous-dependencies': 'off',
         },
     },
 ]);
