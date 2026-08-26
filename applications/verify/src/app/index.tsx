559a6f9da7c7c1b7c6a1063f4b076d9c399dd0d3
Mattias Svanström <mmso@mmso.se>
2026-08-26 16:16:44 +0200

    Remove ResizeObserver polyfill from @proton/polyfill

--- applications/verify/src/app/index.tsx
+++ applications/verify/src/app/index.tsx
@@ -1,10 +1,18 @@
 import { createRoot } from 'react-dom/client';
 
+import { install as installResizeObserver } from 'resize-observer';
+
 import '@proton/polyfill';
 
 import './0';
 import App from './App';
 import './style';
+
+// This app targets older browsers than the rest of the monorepo (see the [verify]
+// section in .browserslistrc), some of which lack ResizeObserver.
+if (!window.ResizeObserver) {
+    installResizeObserver();
+}
 
 const container = document.querySelector('.app-root');
 const root = createRoot(container!);
