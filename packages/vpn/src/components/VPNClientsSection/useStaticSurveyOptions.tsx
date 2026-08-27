1dd15dfb9a30e58000cf300380591851c66cc69f
Joel Prat Vilanova <joel.pratvilanova@proton.ch>
2026-08-27 14:57:41 +0200

    NOISSUE: Extend podcast list

--- packages/vpn/src/components/VPNClientsSection/useStaticSurveyOptions.tsx
+++ packages/vpn/src/components/VPNClientsSection/useStaticSurveyOptions.tsx
@@ -4,108 +4,248 @@ import Option from '@proton/components/components/option/Option';
 
 const STATIC_PODCASTS: { name: string; value: string }[] = [
     {
-        name: 'The Kit & Krysta Podcast',
-        value: 'the_kit_krysta_podcast',
-    },
-    {
-        name: 'Locked On MLB - Daily Podcast On Major League Baseball',
-        value: 'locked_on_mlb_daily_podcast_on_major_league_baseball',
-    },
-    {
-        name: 'My World with Jeff Jarrett',
-        value: 'my_world_with_jeff_jarrett',
-    },
-    {
-        name: 'WIRED | Uncanny Valley',
-        value: 'wired_uncanny_valley',
-    },
-    {
-        name: 'Worklife with Molly Graham',
-        value: 'worklife_with_molly_graham',
-    },
-    {
-        name: 'What Next: TBD',
-        value: 'what_next_tbd',
-    },
-    {
-        name: 'Total Soccer Show',
-        value: 'total_soccer_show',
-    },
-    {
-        name: 'The Gist',
-        value: 'the_gist',
-    },
-    {
-        name: "Founder's Story",
-        value: 'founders_story',
-    },
-    {
-        name: 'Freelance to Founder',
-        value: 'freelance_to_founder',
-    },
-    {
-        name: 'Search Engine',
-        value: 'search_engine',
-    },
-    {
-        name: 'Take Your Shoes Off',
-        value: 'take_your_shoes_off',
-    },
-    {
-        name: 'The Prof G Show with Scott Galloway',
-        value: 'the_prof_g_show_with_scott_galloway',
-    },
-    {
-        name: 'The Vergecast',
-        value: 'the_vergecast',
-    },
-    {
-        name: 'Today Explained',
-        value: 'today_explained',
-    },
-    {
-        name: 'The Besties',
-        value: 'the_besties',
-    },
-    {
-        name: 'Finding Mastery',
-        value: 'finding_mastery',
-    },
-    {
-        name: 'Factually! with Adam Conover',
-        value: 'factually_with_adam_conover',
-    },
-    {
-        name: 'The HoneyDew with Ryan Sickler',
-        value: 'the_honeydew_with_ryan_sickler',
-    },
-    {
-        name: 'Door Handle Podcast',
-        value: 'door_handle_podcast',
-    },
-    {
-        name: 'Armchair Expert',
-        value: 'armchair_expert',
-    },
-    {
-        name: 'How I Built This',
-        value: 'how_i_built_this',
-    },
-    {
-        name: 'Mom Uncharted',
-        value: 'mom_uncharted',
+        name: 'La Ingobernable',
+        value: 'la_ingobernable',
     },
     {
         name: 'Soul Boom',
         value: 'soul_boom',
     },
     {
+        name: 'Only If You Get Caught',
+        value: 'only_if_you_get_caught',
+    },
+    {
+        name: 'Rompiendo el Mercado',
+        value: 'rompiendo_el_mercado',
+    },
+    {
+        name: 'Mom Uncharted',
+        value: 'mom_uncharted',
+    },
+    {
+        name: 'The MinnMax Show',
+        value: 'the_minnmax_show',
+    },
+    {
+        name: 'Athletic FC podcast',
+        value: 'athletic_fc_podcast',
+    },
+    {
+        name: 'Objetivo Libertad Financiera',
+        value: 'objetivo_libertad_financiera',
+    },
+    {
+        name: 'Tengo un Plan',
+        value: 'tengo_un_plan',
+    },
+    {
+        name: 'The Gist',
+        value: 'the_gist',
+    },
+    {
+        name: 'Today Explained',
+        value: 'today_explained',
+    },
+    {
+        name: 'Expreso de Medianoche',
+        value: 'expreso_de_medianoche',
+    },
+    {
+        name: 'How I Built This',
+        value: 'how_i_built_this',
+    },
+    {
+        name: '9to5 Mac Happy Hour',
+        value: '9to5_mac_happy_hour',
+    },
+    {
+        name: 'The Vergecast',
+        value: 'the_vergecast',
+    },
+    {
+        name: 'What Next: TBD',
+        value: 'what_next_tbd',
+    },
+    {
+        name: 'Take Your Shoes Off',
+        value: 'take_your_shoes_off',
+    },
+    {
+        name: 'Missed Apex Formula 1 Podcast',
+        value: 'missed_apex_formula_1_podcast',
+    },
+    {
+        name: 'The HoneyDew with Ryan Sickler',
+        value: 'the_honeydew_with_ryan_sickler',
+    },
+    {
+        name: 'Podcast de Juan Ramón Rallo',
+        value: 'podcast_de_juan_ramon_rallo',
+    },
+    {
+        name: 'Door Handle Podcast',
+        value: 'door_handle_podcast',
+    },
+    {
+        name: 'J vs Ben',
+        value: 'j_vs_ben',
+    },
+    {
+        name: 'Mac Geek Gab — Apple Tips, Tricks, and Troubleshooting',
+        value: 'mac_geek_gab_apple_tips_tricks_and_troubleshooting',
+    },
+    {
+        name: 'Total Soccer Show',
+        value: 'total_soccer_show',
+    },
+    {
+        name: "Let's Learn Everything",
+        value: 'lets_learn_everything',
+    },
+    {
+        name: 'Pew Pew Bang',
+        value: 'pew_pew_bang',
+    },
+    {
+        name: 'Cuéntanos',
+        value: 'cuentanos',
+    },
+    {
+        name: 'Un Podcast Sobre Bitcoin',
+        value: 'un_podcast_sobre_bitcoin',
+    },
+    {
+        name: 'The Race F1 Podcast',
+        value: 'the_race_f1_podcast',
+    },
+    {
+        name: 'Search Engine',
+        value: 'search_engine',
+    },
+    {
+        name: 'No es el fin del mundo',
+        value: 'no_es_el_fin_del_mundo',
+    },
+    {
+        name: 'Alan Barroso',
+        value: 'alan_barroso',
+    },
+    {
+        name: 'Factually! with Adam Conover',
+        value: 'factually_with_adam_conover',
+    },
+    {
+        name: 'Real Politik FC',
+        value: 'real_politik_fc',
+    },
+    {
+        name: 'No Obstante',
+        value: 'no_obstante',
+    },
+    {
         name: 'Crashing Out',
         value: 'crashing_out',
     },
     {
+        name: 'Tierra de Hackers',
+        value: 'tierra_de_hackers',
+    },
+    {
+        name: 'The Besties',
+        value: 'the_besties',
+    },
+    {
+        name: 'My World with Jeff Jarrett',
+        value: 'my_world_with_jeff_jarrett',
+    },
+    {
+        name: 'Data Skeptic',
+        value: 'data_skeptic',
+    },
+    {
+        name: 'Kinda Funny',
+        value: 'kinda_funny',
+    },
+    {
+        name: "Founder's Story",
+        value: 'founders_story',
+    },
+    {
+        name: 'WhatCulture Gaming',
+        value: 'whatculture_gaming',
+    },
+    {
+        name: 'Snake & Banter',
+        value: 'snake_banter',
+    },
+    {
+        name: 'WORLDCAST',
+        value: 'worldcast',
+    },
+    {
+        name: 'WIRED | Uncanny Valley',
+        value: 'wired_uncanny_valley',
+    },
+    {
+        name: 'Spicy 4 tuna',
+        value: 'spicy_4_tuna',
+    },
+    {
+        name: 'OpTic Podcast',
+        value: 'optic_podcast',
+    },
+    {
+        name: 'Wrestling is Cool!',
+        value: 'wrestling_is_cool',
+    },
+    {
         name: 'TechLinked',
         value: 'techlinked',
+    },
+    {
+        name: 'The Kit & Krysta Podcast',
+        value: 'the_kit_krysta_podcast',
+    },
+    {
+        name: 'Worklife with Molly Graham',
+        value: 'worklife_with_molly_graham',
+    },
+    {
+        name: 'Finding Mastery',
+        value: 'finding_mastery',
+    },
+    {
+        name: 'Locked On MLB - Daily Podcast On Major League Baseball',
+        value: 'locked_on_mlb_daily_podcast_on_major_league_baseball',
+    },
+    {
+        name: 'The Prof G Show with Scott Galloway',
+        value: 'the_prof_g_show_with_scott_galloway',
+    },
+    {
+        name: 'Monos Estocásticos',
+        value: 'monos_estocasticos',
+    },
+    {
+        name: 'Gente Interesante',
+        value: 'gente_interesante',
+    },
+    {
+        name: 'Hacked',
+        value: 'hacked',
+    },
+    {
+        name: 'Freelance to Founder',
+        value: 'freelance_to_founder',
+    },
+    {
+        name: 'Armchair Expert',
+        value: 'armchair_expert',
+    },
+    {
+        name: 'Never Post',
+        value: 'never_post',
     },
 ] as const;
 
