84ad2109679622a1a6027a2fc445b5da5b2f2e3a
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-09-01 17:31:14 +0200

    chore(knip): enable knip on vpn-settings app

--- applications/vpn-settings/src/pages/pricing.ts
+++
@@ -1,8 +0,0 @@
-import type { Parameters } from 'proton-account/src/pages/interface';
-import pricing from 'proton-account/src/pages/vpn.pricing';
-
-const data = (): Parameters => ({
-    ...pricing(),
-});
-
-export default data;
