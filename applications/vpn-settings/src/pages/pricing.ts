65155b9aab5af3546bdd8858a48476a45f1d6c44
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-09-01 18:53:49 +0200

    fix(knip): update vpn-settings app entry

---
+++ applications/vpn-settings/src/pages/pricing.ts
@@ -0,0 +1,8 @@
+import type { Parameters } from 'proton-account/src/pages/interface';
+import pricing from 'proton-account/src/pages/vpn.pricing';
+
+const data = (): Parameters => ({
+    ...pricing(),
+});
+
+export default data;
