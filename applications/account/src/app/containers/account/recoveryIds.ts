5ee177d150ad57cd2e179dbb995cdad62befe935
Jack Crompton <jack.crompton@proton.ch>
2026-09-02 10:01:13 +0100

    Remove unused recoveryIds

--- applications/account/src/app/containers/account/recoveryIds.ts
+++ applications/account/src/app/containers/account/recoveryIds.ts
@@ -1,8 +1,6 @@
 import { emergencyAccessId, recoveryContactsId } from '@proton/account/delegatedAccess/routes';
 
 export const recoveryIds = {
-    account: 'account',
-    data: 'data',
     emergencyAccess: emergencyAccessId,
     recoveryContacts: recoveryContactsId,
 };
