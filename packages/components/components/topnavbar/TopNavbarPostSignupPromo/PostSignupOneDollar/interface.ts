ce518f8999a884a570de9409105e3cd1398d29da
Fraser Dempster <fraser.dempster@proton.ch>
2026-09-04 16:59:01 +0000

    Reduced mail one dollar promo to five hours and removed number of mail requirement.

--- packages/components/components/topnavbar/TopNavbarPostSignupPromo/PostSignupOneDollar/interface.ts
+++ packages/components/components/topnavbar/TopNavbarPostSignupPromo/PostSignupOneDollar/interface.ts
@@ -3,16 +3,17 @@ import type { ReactNode } from 'react';
 export const LAST_REMINDER_DAY = 29;
 export const EXTENDED_REMINDER_DAY = 25;
 
-export const POST_SIGNUP_ONE_DOLLAR_ACCOUNT_AGE = 3;
+export const DRIVE_POST_SIGNUP_ONE_DOLLAR_ACCOUNT_AGE = 3;
+export const MAIL_POST_SIGNUP_ONE_DOLLAR_ACCOUNT_AGE = 5;
 export const POST_SIGNUP_ONE_DOLLAR_DURATION = 30;
 
 export type SUPPORTED_PRODUCTS = 'mail' | 'drive';
 
 export enum AUTOMATIC_OFFER_STATE {
-    'notStarted' = 0,
-    'firstSpotlight' = 1,
-    'secondSpotlight' = 2,
-    'lastReminder' = 3,
+    notStarted = 0,
+    firstSpotlight = 1,
+    secondSpotlight = 2,
+    lastReminder = 3,
 }
 
 export interface PostSubscriptionOneDollarOfferState {
