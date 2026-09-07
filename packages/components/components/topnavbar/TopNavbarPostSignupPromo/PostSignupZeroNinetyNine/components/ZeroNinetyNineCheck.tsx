fa4343a40c537dcffb0f573251637a08099b8370
Fraser Dempster <fraser.dempster@proton.ch>
2026-09-07 18:42:32 +0100

    Added components mimicking the original one dollar components.
    Did this because the alternative was reusing the components but since this is a temporary offer and they are permanent I didn't think it's worth extending existing ones to then unpick specific props and UI from them.

---
+++ packages/components/components/topnavbar/TopNavbarPostSignupPromo/PostSignupZeroNinetyNine/components/ZeroNinetyNineCheck.tsx
@@ -0,0 +1,7 @@
+import { IcCheckmark } from '@proton/icons/icons/IcCheckmark';
+
+export const ZeroNinetyNineCheck = (
+    <span className="rounded-full bg-norm shadow-lifted flex items-center justify-center w-6 h-6">
+        <IcCheckmark size={3} />
+    </span>
+);
