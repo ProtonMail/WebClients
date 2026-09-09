6b91bf7afdbe23b18058771e80dcd4172e89d562
Fraser Dempster <fraser.dempster@proton.ch>
2026-09-09 18:10:54 +0100

    Made the UI of the check match the Figma.

--- packages/components/components/topnavbar/TopNavbarPostSignupPromo/PostSignupZeroNinetyNine/components/ZeroNinetyNineCheck.tsx
+++ packages/components/components/topnavbar/TopNavbarPostSignupPromo/PostSignupZeroNinetyNine/components/ZeroNinetyNineCheck.tsx
@@ -1,7 +1,9 @@
 import { IcCheckmark } from '@proton/icons/icons/IcCheckmark';
 
+import './ZeroNinetyNineOffer.scss';
+
 export const ZeroNinetyNineCheck = (
-    <span className="rounded-full bg-norm shadow-lifted flex items-center justify-center w-6 h-6">
+    <span className="zero-ninety-nine-check rounded-full bg-norm flex items-center justify-center w-6 h-6">
         <IcCheckmark size={3} />
     </span>
 );
