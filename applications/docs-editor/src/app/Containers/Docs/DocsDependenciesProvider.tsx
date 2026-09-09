a2575f603d43880718fd114f82dfcf05acd7a2ee
Aman Harwara <aman.harwara@proton.ch>
2026-09-09 12:28:14 +0000

    Docs: Standalone docs - move some clientInvoker calls to deps

--- applications/docs-editor/src/app/Containers/Docs/DocsDependenciesProvider.tsx
+++ applications/docs-editor/src/app/Containers/Docs/DocsDependenciesProvider.tsx
@@ -1,9 +1,23 @@
+import type { CommentThreadInterface, SuggestionSummaryType } from '@proton/docs-shared'
+import type { TelemetryDocsEditorEvents } from '@proton/shared/lib/api/telemetry'
 import type { PropsWithChildren } from 'react'
 import { createContext, useContext } from 'react'
 
 export type DocsDependencies = {
   isDevOrBlack: () => boolean
   openLink: (url: string) => void
+  showGenericAlertModal: (message: string) => void
+  createSuggestionThread(
+    suggestionID: string,
+    commentContent: string,
+    suggestionType: SuggestionSummaryType,
+  ): Promise<CommentThreadInterface | undefined>
+  getAllThreads: () => Promise<CommentThreadInterface[]>
+  reopenSuggestion: (threadId: string) => Promise<boolean>
+  rejectSuggestion: (threadId: string, summary?: string | undefined) => Promise<boolean>
+  getDocumentUrl: () => Promise<string>
+  replaceDocumentUrl: (url: string) => Promise<void>
+  reportTelemetry: (event: TelemetryDocsEditorEvents) => void
 }
 
 const DocsDependenciesContext = createContext<DocsDependencies | undefined>(undefined)
