18c5afcafb1bf8f06f5479d0353e221f41886086
MargeBot <service_account_32425739d193d4576ca09ebf2d772104@noreply.gitlab.protontech.ch>
2026-09-17 16:07:35 +0000

    Merge branch 'meet-force-disable-noise-cancellation-on-windows-edge' into 'main'
    Disabling noise cancellation on Windows Edge
    
    See merge request web/clients!28293

--- packages/unleash/UnleashFeatureFlags.ts
+++ packages/unleash/UnleashFeatureFlags.ts
@@ -355,6 +355,11 @@ enum MeetFeatureFlag {
     MeetUseCachedServerTime = 'MeetUseCachedServerTime',
     MeetBackgroundEffectsOnMobileBrowsers = 'MeetBackgroundEffectsOnMobileBrowsers',
     MeetParticipantsLayouts = 'MeetParticipantsLayouts',
+    MeetCpuOptimizations = 'MeetCpuOptimizations',
+    MeetScreenShareAudioSupportedElectronVersion = 'MeetScreenShareAudioSupportedElectronVersion',
+    MeetChatMentions = 'MeetChatMentions',
+    MeetOpenMessageFromPreview = 'MeetOpenMessageFromPreview',
+    MeetForceDisableNoiseCancellationOnEdge = 'MeetForceDisableNoiseCancellationOnEdge',
 }
 
 enum LumoFeatureFlag {
