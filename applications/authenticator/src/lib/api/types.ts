e2038888300e481a6b6ab98a9ca0ec27543523a1
Flavien Bonvin <flavien.bonvin@proton.ch>
2026-09-18 15:04:51 +0200

    chore(knip): ignore auto-generated types

--- applications/authenticator/src/lib/api/types.ts
+++ applications/authenticator/src/lib/api/types.ts
@@ -1,4 +1,23 @@
-type AuthenticatorCreateEntryRequest = {
+export enum ResponseCodeSuccess {
+    PROTONRESPONSECODE_1000 = 1000,
+}
+export type ProtonSuccess = { Code: ResponseCodeSuccess };
+export type ProtonError = {
+    Code: number;
+    /* Error message */
+    Error: string;
+    /* Error description (can be an empty object) */
+    Details: {};
+};
+export type DriveConstants = {
+    BlockMaxSizeInBytes?: '5300000';
+    ThumbnailMaxSizeInBytes?: '65536';
+    DraftRevisionLifetimeInSec?: '14400';
+    ExtendedAttributesMaxSizeInBytes?: '65535';
+    UploadTokenExpirationTimeInSec?: '10800';
+    DownloadTokenExpirationTimeInSec?: '1800';
+};
+export type AuthenticatorCreateEntryRequest = {
     AuthenticatorKeyID: Id;
     /* Base64 representation of the entry content encrypted with the AuthenticatorKey */
     Content: string;
@@ -17,9 +36,28 @@ export type AuthenticatorEntryDeleteBulkInput = {
     /* List of authenticator entryIDs to be deleted */
     EntryIDs: Id[];
 };
+export type AuthenticatorReorderBatchRequest = {
+    /* Initial position of the first entry. Starting position is 0 */
+    StartingPosition: number;
+    /* Ordered list of authenticator entries. First one will be set at position `StartingPosition`. At most allow 500 entries. */
+    Entries: Id[];
+};
+export type AuthenticatorEntryUpdateRequest = {
+    AuthenticatorKeyID: Id;
+    /* Base64 representation of the entry content encrypted with the AuthenticatorKey */
+    Content: string;
+    /* Version of the format used to encode the contents of this entry */
+    ContentFormatVersion: number;
+    /* Last revision seen for this entry */
+    LastRevision: number;
+};
 export type AuthenticatorReorderEntryRequest = {
     /* ID of the entry after the one we should move the entry. Null if it should go on the first place */
     AfterID?: Id | null;
+};
+export type AuthenticatorKeyCreateRequest = {
+    /* Base64 representation of the authenticator key encrypted with the user key */
+    Key: string;
 };
 export type AuthenticatorEntriesResponse = {
     /* List of authenticator entries */
@@ -55,7 +93,7 @@ export type AuthenticatorKeyResponse = {
     KeyID: Id;
     UserKeyID: Id;
 };
-type Id = string;
+export type Id = string;
 export type AuthenticatorEntryUpdateWithEntryRequest = {
     EntryID: Id;
     AuthenticatorKeyID: Id;
@@ -66,3 +104,81 @@ export type AuthenticatorEntryUpdateWithEntryRequest = {
     /* Last revision seen for this entry */
     LastRevision: number;
 };
+export type ApiResponse<
+    Path extends string,
+    Method extends string,
+> = Path extends `authenticator/v1/entry/${string}/order`
+    ? Method extends `put`
+        ? { Code: ResponseCodeSuccess }
+        : never
+    : Path extends `authenticator/v1/entry/bulk`
+      ? Method extends `put`
+          ? {
+                Code: ResponseCodeSuccess;
+                /* List of authenticator entries */
+                Entries?: AuthenticatorEntryResponse[];
+            }
+          : Method extends `post`
+            ? {
+                  Code: ResponseCodeSuccess;
+                  /* List of authenticator entries */
+                  Entries?: AuthenticatorEntryResponse[];
+              }
+            : Method extends `delete`
+              ? { Code: ResponseCodeSuccess }
+              : never
+      : Path extends `authenticator/v1/entry/order`
+        ? Method extends `put`
+            ? { Code: ResponseCodeSuccess }
+            : never
+        : Path extends `authenticator/v1/entry/${string}`
+          ? Method extends `put`
+              ? { Code: ResponseCodeSuccess; Entry: AuthenticatorEntryResponse }
+              : Method extends `delete`
+                ? { Code: ResponseCodeSuccess }
+                : never
+          : Path extends `authenticator/v1/entry`
+            ? Method extends `get`
+                ? { Code: ResponseCodeSuccess; Entries: AuthenticatorEntriesResponse }
+                : Method extends `post`
+                  ? { Code: ResponseCodeSuccess; Entry: AuthenticatorEntryResponse }
+                  : never
+            : Path extends `authenticator/v1/key`
+              ? Method extends `get`
+                  ? { Code: ResponseCodeSuccess; Keys: AuthenticatorKeysResponse }
+                  : Method extends `post`
+                    ? { Code: ResponseCodeSuccess; Key: AuthenticatorKeyResponse }
+                    : never
+              : any;
+export type ApiRequestBody<
+    Path extends string,
+    Method extends string,
+> = Path extends `authenticator/v1/entry/${string}/order`
+    ? Method extends `put`
+        ? AuthenticatorReorderEntryRequest
+        : never
+    : Path extends `authenticator/v1/entry/bulk`
+      ? Method extends `put`
+          ? AuthenticatorEntryUpdateBulkRequest
+          : Method extends `post`
+            ? AuthenticatorCreateEntriesRequest
+            : Method extends `delete`
+              ? AuthenticatorEntryDeleteBulkInput
+              : never
+      : Path extends `authenticator/v1/entry/order`
+        ? Method extends `put`
+            ? AuthenticatorReorderBatchRequest
+            : never
+        : Path extends `authenticator/v1/entry/${string}`
+          ? Method extends `put`
+              ? AuthenticatorEntryUpdateRequest
+              : never
+          : Path extends `authenticator/v1/entry`
+            ? Method extends `post`
+                ? AuthenticatorCreateEntryRequest
+                : never
+            : Path extends `authenticator/v1/key`
+              ? Method extends `post`
+                  ? AuthenticatorKeyCreateRequest
+                  : never
+              : any;
